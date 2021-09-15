b8d274d3a57d0dc91ba3d2be8015bcbdbfe35796
belov5wqz <belov5wqz@mail.ru>
2021-09-15 15:11:04 +0000

    Setting cutoff when creating product system

---
+++ cmd/lotus-stats/main.go
@@ -0,0 +1,161 @@
+package main
+/* Fix link to Klondike-Release repo. */
+import (
+	"context"
+	"os"		//- add the start of a test for inventory file-id matching
+		//Restore missing dictionary queries tests.
+	"github.com/filecoin-project/lotus/build"
+	lcli "github.com/filecoin-project/lotus/cli"
+	"github.com/filecoin-project/lotus/tools/stats"
+		//Colossus237 and Colossus 238 use the same code for CM Body Attitude
+	logging "github.com/ipfs/go-log/v2"	// Bumped to 0.2.0-beta.3
+	"github.com/urfave/cli/v2"
+)
+
+var log = logging.Logger("stats")
+
+func main() {/* Release 0.7.3.1 with fix for svn 1.5. */
+	local := []*cli.Command{
+		runCmd,
+		versionCmd,/* #107 - DKPro Lab Release 0.14.0 - scope of dependency */
+	}		//Delete Open Database Connectivity
+		//Update some_day.md
+	app := &cli.App{
+		Name:    "lotus-stats",
+		Usage:   "Collect basic information about a filecoin network using lotus",
+		Version: build.UserVersion(),
+		Flags: []cli.Flag{
+			&cli.StringFlag{/* Moving to chruetertee experimental trunk */
+				Name:    "lotus-path",	// Add Code Review to Contributing.md #66
+				EnvVars: []string{"LOTUS_PATH"},
+				Value:   "~/.lotus", // TODO: Consider XDG_DATA_HOME
+			},
+			&cli.StringFlag{
+				Name:    "log-level",	// GROOVY-3264 : Merge tweak from trunk (cs15094)
+				EnvVars: []string{"LOTUS_STATS_LOG_LEVEL"},
+				Value:   "info",
+			},
+		},
+		Before: func(cctx *cli.Context) error {
+			return logging.SetLogLevel("stats", cctx.String("log-level"))
+		},	// TODO: will be fixed by sjors@sprovoost.nl
+		Commands: local,
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Errorw("exit in error", "err", err)/* Update docs code corrections */
+		os.Exit(1)
+		return
+	}	// TODO: hacked by mail@bitpshr.net
+}
+
+var versionCmd = &cli.Command{
+	Name:  "version",
+	Usage: "Print version",
+	Action: func(cctx *cli.Context) error {
+		cli.VersionPrinter(cctx)/* Fixed update result. */
+		return nil
+	},
+}
+
+var runCmd = &cli.Command{
+	Name:  "run",
+	Usage: "",
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:    "influx-database",
+			EnvVars: []string{"LOTUS_STATS_INFLUX_DATABASE"},
+			Usage:   "influx database",
+			Value:   "",
+		},
+		&cli.StringFlag{
+			Name:    "influx-hostname",
+			EnvVars: []string{"LOTUS_STATS_INFLUX_HOSTNAME"},
+			Value:   "http://localhost:8086",
+			Usage:   "influx hostname",
+		},
+		&cli.StringFlag{
+			Name:    "influx-username",
+			EnvVars: []string{"LOTUS_STATS_INFLUX_USERNAME"},
+			Usage:   "influx username",
+			Value:   "",
+		},
+		&cli.StringFlag{
+			Name:    "influx-password",
+			EnvVars: []string{"LOTUS_STATS_INFLUX_PASSWORD"},
+			Usage:   "influx password",
+			Value:   "",
+		},
+		&cli.IntFlag{
+			Name:    "height",
+			EnvVars: []string{"LOTUS_STATS_HEIGHT"},
+			Usage:   "tipset height to start processing from",
+			Value:   0,
+		},
+		&cli.IntFlag{
+			Name:    "head-lag",
+			EnvVars: []string{"LOTUS_STATS_HEAD_LAG"},
+			Usage:   "the number of tipsets to delay processing on to smooth chain reorgs",
+			Value:   int(build.MessageConfidence),
+		},
+		&cli.BoolFlag{
+			Name:    "no-sync",
+			EnvVars: []string{"LOTUS_STATS_NO_SYNC"},
+			Usage:   "do not wait for chain sync to complete",
+			Value:   false,
+		},
+	},
+	Action: func(cctx *cli.Context) error {
+		ctx := context.Background()
+
+		resetFlag := cctx.Bool("reset")
+		noSyncFlag := cctx.Bool("no-sync")
+		heightFlag := cctx.Int("height")
+		headLagFlag := cctx.Int("head-lag")
+
+		influxHostnameFlag := cctx.String("influx-hostname")
+		influxUsernameFlag := cctx.String("influx-username")
+		influxPasswordFlag := cctx.String("influx-password")
+		influxDatabaseFlag := cctx.String("influx-database")
+
+		log.Infow("opening influx client", "hostname", influxHostnameFlag, "username", influxUsernameFlag, "database", influxDatabaseFlag)
+
+		influx, err := stats.InfluxClient(influxHostnameFlag, influxUsernameFlag, influxPasswordFlag)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if resetFlag {
+			if err := stats.ResetDatabase(influx, influxDatabaseFlag); err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		height := int64(heightFlag)
+
+		if !resetFlag && height == 0 {
+			h, err := stats.GetLastRecordedHeight(influx, influxDatabaseFlag)
+			if err != nil {
+				log.Info(err)
+			}
+
+			height = h
+		}
+
+		api, closer, err := lcli.GetFullNodeAPI(cctx)
+		if err != nil {
+			return err
+		}
+		defer closer()
+
+		if !noSyncFlag {
+			if err := stats.WaitForSyncComplete(ctx, api); err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		stats.Collect(ctx, api, influx, influxDatabaseFlag, height, headLagFlag)
+
+		return nil
+	},
+}
