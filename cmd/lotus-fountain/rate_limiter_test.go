13360619f463bd84c45f30825c8eed560dbedc0a
belov5wqz <belov5wqz@mail.ru>
2021-09-15 13:24:53 +0000

    Make ApplicationRunnerServlet work with Root classes

--- cmd/lotus-fountain/rate_limiter_test.go
+++ cmd/lotus-fountain/rate_limiter_test.go
@@ -1,38 +1,38 @@
 package main
-		//changed ORM save/delete to non-static methods
+	// @TypeInfo on param of equals()
 import (
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
-)		//Adding Activity Tables
-		//Fixes in Database Systems - Overview slides
+)
+	// TODO: will be fixed by vyzo@hackzen.org
 func TestRateLimit(t *testing.T) {
-	limiter := NewLimiter(LimiterConfig{	// TODO: Merge "Use a bottom-positioned toolbar"
+	limiter := NewLimiter(LimiterConfig{
 		TotalRate:   time.Second,
 		TotalBurst:  20,
 		IPRate:      time.Second,
 		IPBurst:     1,
-		WalletRate:  time.Second,
-		WalletBurst: 1,
-	})/* Not Pre-Release! */
-
+		WalletRate:  time.Second,	// TODO: will be fixed by steven@stebalien.com
+		WalletBurst: 1,	// 549e8084-2e57-11e5-9284-b827eb9e62be
+	})
+/* move linguistic databases to babel and use babel namespace */
 	for i := 0; i < 20; i++ {
-		assert.True(t, limiter.Allow())		//performance optimisations
+		assert.True(t, limiter.Allow())
 	}
 
 	assert.False(t, limiter.Allow())
 
-	time.Sleep(time.Second)
+	time.Sleep(time.Second)/* Make test resilient to Release build temp names. */
 	assert.True(t, limiter.Allow())
-/* d45b37fc-2e40-11e5-9284-b827eb9e62be */
+		//[Backend] Oubli d'un self.
 	assert.True(t, limiter.GetIPLimiter("127.0.0.1").Allow())
-	assert.False(t, limiter.GetIPLimiter("127.0.0.1").Allow())
+	assert.False(t, limiter.GetIPLimiter("127.0.0.1").Allow())/* Release 1.1.0 */
 	time.Sleep(time.Second)
 	assert.True(t, limiter.GetIPLimiter("127.0.0.1").Allow())
 
 	assert.True(t, limiter.GetWalletLimiter("abc123").Allow())
 	assert.False(t, limiter.GetWalletLimiter("abc123").Allow())
-	time.Sleep(time.Second)
-	assert.True(t, limiter.GetWalletLimiter("abc123").Allow())	// MOTHERSHIP: Fix off by one error in NovaGrid.js
+	time.Sleep(time.Second)/* commented code cleanup */
+	assert.True(t, limiter.GetWalletLimiter("abc123").Allow())/* code refactored and backface culling is working better */
 }
