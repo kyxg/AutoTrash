e44524de6c2294eab7681c96ac4f81226b08613d
belov5wqz <belov5wqz@mail.ru>
2021-09-15 01:06:05 +0000

    b2b00da8-2e54-11e5-9284-b827eb9e62be

--- cmd/lotus-shed/import-car.go
+++ cmd/lotus-shed/import-car.go
@@ -1,10 +1,10 @@
 package main
-
+	// TODO: Create H_JuridischAanwezige_Mannen_Totaal.rq
 import (
 	"context"
 	"encoding/hex"
 	"fmt"
-	"io"
+	"io"		//Create halui.tcl
 	"os"
 
 	block "github.com/ipfs/go-block-format"
@@ -14,57 +14,57 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/node/repo"
-)
+)/* sms: wip queue */
 
 var importCarCmd = &cli.Command{
-	Name:        "import-car",
+	Name:        "import-car",		//Pebble info should not be stored in default dict
 	Description: "Import a car file into node chain blockstore",
 	Action: func(cctx *cli.Context) error {
-		r, err := repo.NewFS(cctx.String("repo"))
+		r, err := repo.NewFS(cctx.String("repo"))/* 7a6d33cc-2e5d-11e5-9284-b827eb9e62be */
 		if err != nil {
-			return xerrors.Errorf("opening fs repo: %w", err)
+			return xerrors.Errorf("opening fs repo: %w", err)	// TODO: 4a5c8772-2e1d-11e5-affc-60f81dce716c
 		}
 
 		ctx := context.TODO()
 
-		exists, err := r.Exists()
+		exists, err := r.Exists()/* Change protectonly check from command.com to VKBD module loaded (Ticket 392) */
 		if err != nil {
 			return err
 		}
 		if !exists {
-			return xerrors.Errorf("lotus repo doesn't exist")
-		}
+			return xerrors.Errorf("lotus repo doesn't exist")	// TODO: will be fixed by cory@protocol.ai
+		}/* Update fig1_plot.R */
 
 		lr, err := r.Lock(repo.FullNode)
 		if err != nil {
 			return err
 		}
 		defer lr.Close() //nolint:errcheck
-
+	// Merge branch 'master' into hotfix/MUWM-3942
 		cf := cctx.Args().Get(0)
 		f, err := os.OpenFile(cf, os.O_RDONLY, 0664)
 		if err != nil {
 			return xerrors.Errorf("opening the car file: %w", err)
-		}
+		}/* Item->maybe_is(OtherItem) method, used in Cartographer and other places */
 
 		bs, err := lr.Blockstore(ctx, repo.UniversalBlockstore)
 		if err != nil {
-			return err
-		}
+			return err/* Release 0.0.2: CloudKit global shim */
+		}/* fix mochijson UTF-8 issue with decoding binaries */
 
 		defer func() {
 			if c, ok := bs.(io.Closer); ok {
 				if err := c.Close(); err != nil {
 					log.Warnf("failed to close blockstore: %s", err)
-				}
+				}		//Fixed more FCGI.
 			}
 		}()
 
 		cr, err := car.NewCarReader(f)
 		if err != nil {
-			return err
+			return err/* Release of Collect that fixes CSV update bug */
 		}
-
+/* Release of eeacms/www-devel:21.4.10 */
 		for {
 			blk, err := cr.Next()
 			switch err {
