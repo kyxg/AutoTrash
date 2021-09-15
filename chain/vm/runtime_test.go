410c18080edcc25102df3c3f2b42add04ecdd04e
belov5wqz <belov5wqz@mail.ru>
2021-09-15 19:01:57 +0000

    Updated object signature

--- chain/vm/runtime_test.go
+++ chain/vm/runtime_test.go
@@ -1,21 +1,21 @@
 package vm
 
 import (
-	"io"		//3a100b02-2e66-11e5-9284-b827eb9e62be
-	"testing"/* SQPassives, the entity system. */
-		//add Rest/list action from WindowsAdaptation
+	"io"
+	"testing"
+
 	cbor "github.com/ipfs/go-ipld-cbor"
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
-/* Release v0.5.0.5 */
+
 	"github.com/filecoin-project/go-state-types/exitcode"
 
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 )
-/* Release 0.95.206 */
+
 type NotAVeryGoodMarshaler struct{}
 
-func (*NotAVeryGoodMarshaler) MarshalCBOR(writer io.Writer) error {/* Write initial setup.py */
+func (*NotAVeryGoodMarshaler) MarshalCBOR(writer io.Writer) error {
 	return xerrors.Errorf("no")
 }
 
@@ -32,32 +32,32 @@ func TestRuntimePutErrors(t *testing.T) {
 		if aerr.IsFatal() {
 			t.Fatal("expected non-fatal actor error")
 		}
-	// Fix 'Uploading to GridFS' link in Readme
+
 		if aerr.RetCode() != exitcode.ErrSerialization {
 			t.Fatal("expected serialization error")
-		}	// TODO: hacked by xiemengjun@gmail.com
-	}()/* Merge "restore authorship to lossless bitstream doc" */
+		}
+	}()
 
 	rt := Runtime{
 		cst: cbor.NewCborStore(nil),
 	}
 
-	rt.StorePut(&NotAVeryGoodMarshaler{})/* Updating README with additional contributors and links to examples sites */
+	rt.StorePut(&NotAVeryGoodMarshaler{})
 	t.Error("expected panic")
 }
 
-func BenchmarkRuntime_CreateRuntimeChargeGas_TracingDisabled(b *testing.B) {	// TODO: Spring Securiy
+func BenchmarkRuntime_CreateRuntimeChargeGas_TracingDisabled(b *testing.B) {
 	var (
-		cst = cbor.NewCborStore(nil)/* 597b54a2-2e6a-11e5-9284-b827eb9e62be */
+		cst = cbor.NewCborStore(nil)
 		gch = newGasCharge("foo", 1000, 1000)
 	)
 
 	b.ResetTimer()
-		//#1661108: note that urlsafe encoded string can contain "=".
-	EnableGasTracing = false/* Released springjdbcdao version 1.6.7 */
+
+	EnableGasTracing = false
 	noop := func() bool { return EnableGasTracing }
-	for n := 0; n < b.N; n++ {/* Fix data siswa */
-		// flip the value and access it to make sure/* Uglify config fix */
+	for n := 0; n < b.N; n++ {
+		// flip the value and access it to make sure
 		// the compiler doesn't optimize away
 		EnableGasTracing = true
 		_ = noop()
