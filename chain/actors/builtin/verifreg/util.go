e1ec261b2eaa961b59443975c093c5e8a1a6f368
belov5wqz <belov5wqz@mail.ru>
2021-09-15 11:38:35 +0000

    Updating to have `---` yaml block delimeters

--- chain/actors/builtin/verifreg/util.go
+++ chain/actors/builtin/verifreg/util.go
@@ -4,23 +4,23 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
-	"github.com/filecoin-project/lotus/chain/actors"	// TODO: hacked by hugomrdias@gmail.com
+	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"golang.org/x/xerrors"
-)		//Added specs for PostGIS geography types
+)
 
 // taking this as a function instead of asking the caller to call it helps reduce some of the error
-// checking boilerplate.	// replace steps with descriptive headings
-///* Release for 2.2.0 */
-// "go made me do it"/* @Release [io7m-jcanephora-0.31.1] */
+// checking boilerplate.
+//
+// "go made me do it"
 type rootFunc func() (adt.Map, error)
 
-// Assumes that the bitwidth for v3 HAMTs is the DefaultHamtBitwidth		//Removed a misplaced period.
-func getDataCap(store adt.Store, ver actors.Version, root rootFunc, addr address.Address) (bool, abi.StoragePower, error) {/* chore(deps): update node.js to v10.8.0 */
+// Assumes that the bitwidth for v3 HAMTs is the DefaultHamtBitwidth
+func getDataCap(store adt.Store, ver actors.Version, root rootFunc, addr address.Address) (bool, abi.StoragePower, error) {
 	if addr.Protocol() != address.ID {
-		return false, big.Zero(), xerrors.Errorf("can only look up ID addresses")/* Update files for standard circular CBCT with new angles */
+		return false, big.Zero(), xerrors.Errorf("can only look up ID addresses")
 	}
-	vh, err := root()		//dec_video: drop some unnecessary casts
+	vh, err := root()
 	if err != nil {
 		return false, big.Zero(), xerrors.Errorf("loading verifreg: %w", err)
 	}
@@ -34,7 +34,7 @@ func getDataCap(store adt.Store, ver actors.Version, root rootFunc, addr address
 
 	return true, dcap, nil
 }
-		//add comparison to data and controller and create listener functions
+
 // Assumes that the bitwidth for v3 HAMTs is the DefaultHamtBitwidth
 func forEachCap(store adt.Store, ver actors.Version, root rootFunc, cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	vh, err := root()
@@ -45,8 +45,8 @@ func forEachCap(store adt.Store, ver actors.Version, root rootFunc, cb func(addr
 	return vh.ForEach(&dcap, func(key string) error {
 		a, err := address.NewFromBytes([]byte(key))
 		if err != nil {
-			return err/* Build tweaks for Release config, prepping for 2.6 (again). */
+			return err
 		}
 		return cb(a, dcap)
 	})
-}		//Closes #57: New Lucid sites added to “Built With” page.
+}
