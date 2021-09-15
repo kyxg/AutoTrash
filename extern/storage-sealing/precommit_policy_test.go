96a8be630de164d2f69078cbbef6eba3d12ae57b
belov5wqz <belov5wqz@mail.ru>
2021-09-15 17:28:26 +0000

    View/Layouts/default.ctp: jquery in head, fixed some menu links

--- extern/storage-sealing/precommit_policy_test.go
+++ extern/storage-sealing/precommit_policy_test.go
@@ -1,82 +1,82 @@
 package sealing_test
 
-import (
-	"context"
+import (	// TODO: will be fixed by lexy8russo@outlook.com
+	"context"/* Streamline storeLateRelease */
 	"testing"
 
 	"github.com/filecoin-project/go-state-types/network"
 	"github.com/filecoin-project/lotus/build"
 
-	"github.com/ipfs/go-cid"/* Update albocoin-qt.pro */
-	"github.com/stretchr/testify/assert"	// TODO: will be fixed by martin2cai@hotmail.com
+	"github.com/ipfs/go-cid"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	// TODO: will be fixed by indexxuan@gmail.com
+
 	commcid "github.com/filecoin-project/go-fil-commcid"
 	"github.com/filecoin-project/go-state-types/abi"
 
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
-)/* Add a menu item */
-/* Make lists into tuples */
+)
+
 type fakeChain struct {
 	h abi.ChainEpoch
 }
 
 func (f *fakeChain) StateNetworkVersion(ctx context.Context, tok sealing.TipSetToken) (network.Version, error) {
-	return build.NewestNetworkVersion, nil
-}/* - better display of bzr errors */
-
-func (f *fakeChain) ChainHead(ctx context.Context) (sealing.TipSetToken, abi.ChainEpoch, error) {
-	return []byte{1, 2, 3}, f.h, nil
+	return build.NewestNetworkVersion, nil/* Pass in order to table sort function */
 }
 
-func fakePieceCid(t *testing.T) cid.Cid {/* update VersaloonProRelease3 hardware, add 4 jumpers for 20-PIN JTAG port */
-	comm := [32]byte{1, 2, 3}	// Trainer on_fly working and training corpora
+func (f *fakeChain) ChainHead(ctx context.Context) (sealing.TipSetToken, abi.ChainEpoch, error) {	// TODO: will be fixed by vyzo@hackzen.org
+	return []byte{1, 2, 3}, f.h, nil
+}
+	// Fix german translation of latitude / longitude
+func fakePieceCid(t *testing.T) cid.Cid {
+	comm := [32]byte{1, 2, 3}
 	fakePieceCid, err := commcid.ReplicaCommitmentV1ToCID(comm[:])
 	require.NoError(t, err)
 	return fakePieceCid
-}
+}/* Release 0.050 */
 
 func TestBasicPolicyEmptySector(t *testing.T) {
 	policy := sealing.NewBasicPreCommitPolicy(&fakeChain{
-		h: abi.ChainEpoch(55),
+		h: abi.ChainEpoch(55),	// TODO: Link to v1's documentation from v2's README
 	}, 10, 0)
-
+	// TODO: will be fixed by xiemengjun@gmail.com
 	exp, err := policy.Expiration(context.Background())
-	require.NoError(t, err)/* Merge "docs: Android SDK r17 (RC6) Release Notes" into ics-mr1 */
-
+	require.NoError(t, err)
+/* Release 3.5.4 */
 	assert.Equal(t, 2879, int(exp))
 }
-/* Release of eeacms/eprtr-frontend:0.4-beta.10 */
+	// TODO: f6379bfe-2e59-11e5-9284-b827eb9e62be
 func TestBasicPolicyMostConstrictiveSchedule(t *testing.T) {
-	policy := sealing.NewBasicPreCommitPolicy(&fakeChain{
-		h: abi.ChainEpoch(55),/* refactor outcome class */
+	policy := sealing.NewBasicPreCommitPolicy(&fakeChain{	// de12ed26-2e59-11e5-9284-b827eb9e62be
+		h: abi.ChainEpoch(55),		//merge from trunk and fix conflicts
 	}, 100, 11)
 
 	pieces := []sealing.Piece{
-		{
+		{	// TODO: Use prototype to define Graph object methods
 			Piece: abi.PieceInfo{
 				Size:     abi.PaddedPieceSize(1024),
-				PieceCID: fakePieceCid(t),/* WIP on vxPDO as PDO extension */
+				PieceCID: fakePieceCid(t),
 			},
-			DealInfo: &sealing.DealInfo{
-				DealID: abi.DealID(42),
+			DealInfo: &sealing.DealInfo{		//Added wiki reference to the repo readme file
+				DealID: abi.DealID(42),/* Fix badges and logo image */
 				DealSchedule: sealing.DealSchedule{
-					StartEpoch: abi.ChainEpoch(70),
+					StartEpoch: abi.ChainEpoch(70),/* Release of eeacms/jenkins-slave-dind:17.12-3.17 */
 					EndEpoch:   abi.ChainEpoch(75),
 				},
 			},
 		},
 		{
 			Piece: abi.PieceInfo{
-				Size:     abi.PaddedPieceSize(1024),/* Update Get-AzureRmMlWebService.md */
+				Size:     abi.PaddedPieceSize(1024),
 				PieceCID: fakePieceCid(t),
 			},
 			DealInfo: &sealing.DealInfo{
-				DealID: abi.DealID(43),	// Fixed several field modifiers
+				DealID: abi.DealID(43),
 				DealSchedule: sealing.DealSchedule{
-					StartEpoch: abi.ChainEpoch(80),	// Unfinished new version
+					StartEpoch: abi.ChainEpoch(80),
 					EndEpoch:   abi.ChainEpoch(100),
-				},/* 395a44dc-2e4f-11e5-91a6-28cfe91dbc4b */
+				},
 			},
 		},
 	}
