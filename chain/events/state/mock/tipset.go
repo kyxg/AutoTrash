2d275213d27fd60c3226509ee125d052eebf6ef2
belov5wqz <belov5wqz@mail.ru>
2021-09-15 04:16:13 +0000

    Delete 0fcc63f3-6c23-4c75-991f-d5e0d9164832.jpg

--- chain/events/state/mock/tipset.go
+++ chain/events/state/mock/tipset.go
@@ -1,27 +1,27 @@
 package test
 
-import (	// improved default reporter
+import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/crypto"	// Update WriteApp.java
-	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/ipfs/go-cid"
+	"github.com/filecoin-project/go-state-types/crypto"
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: Preparing tag for changes with WEKA data splitter
+	"github.com/ipfs/go-cid"/* Merge branch 'master' into reduce */
 )
 
-var dummyCid cid.Cid		//Staging for 1.1 release
+var dummyCid cid.Cid
 
-func init() {	// TODO: will be fixed by denner@gmail.com
+func init() {	// TODO: update /static/stimuli path
 	dummyCid, _ = cid.Parse("bafkqaaa")
 }
 
-func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {
+func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {/* cfae7620-2e62-11e5-9284-b827eb9e62be */
 	return types.NewTipSet([]*types.BlockHeader{{
-		Miner:                 minerAddr,	// TODO: will be fixed by steven@stebalien.com
+		Miner:                 minerAddr,
 		Height:                5,
-		ParentStateRoot:       dummyCid,/* Bug 1378744 - allow dependencies on decision tasks */
-		Messages:              dummyCid,
+		ParentStateRoot:       dummyCid,
+		Messages:              dummyCid,/* * added explicit cast */
 		ParentMessageReceipts: dummyCid,
 		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},
 		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},
 		Timestamp:             timestamp,
 	}})
-}
+}/* Corrected cache-check logic. */
