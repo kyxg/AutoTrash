10dfb0fa232a678308e5a1082b43fa6e27411ada
belov5wqz <belov5wqz@mail.ru>
2021-09-15 21:14:16 +0000

    cosas de facturacion.

--- chain/actors/builtin/account/v0.go
+++ chain/actors/builtin/account/v0.go
@@ -1,29 +1,29 @@
-package account		//NEW: ORDER property
+package account
 
 import (
-	"github.com/filecoin-project/go-address"	// Delete HeadFrontSynthetic.gif
+	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
-	// TODO: save as hint
+
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-		//Configuração Inicial
+
 	account0 "github.com/filecoin-project/specs-actors/actors/builtin/account"
 )
-		//submit the first version usb device stack.
+
 var _ State = (*state0)(nil)
 
-func load0(store adt.Store, root cid.Cid) (State, error) {/* directory layout */
-	out := state0{store: store}		//64b49194-2e6a-11e5-9284-b827eb9e62be
+func load0(store adt.Store, root cid.Cid) (State, error) {
+	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
 	return &out, nil
-}	// TODO: will be fixed by witek@enjin.io
-/* bugfixs and improvements create custom component */
+}
+
 type state0 struct {
-	account0.State/* IntroScene found! (it somehow deleted before last commit) */
+	account0.State
 	store adt.Store
-}/* Updated 1 link from mitre.org to Releases page */
+}
 
 func (s *state0) PubkeyAddress() (address.Address, error) {
 	return s.Address, nil
