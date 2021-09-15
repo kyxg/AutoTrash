7fb4fa59bd2be22b5485826e1a4c46ceadf261d1
belov5wqz <belov5wqz@mail.ru>
2021-09-15 05:31:21 +0000

    Add userrole

--- chain/gen/genesis/f03_cron.go
+++ chain/gen/genesis/f03_cron.go
@@ -5,25 +5,25 @@ import (
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	"github.com/filecoin-project/specs-actors/actors/builtin/cron"
-	cbor "github.com/ipfs/go-ipld-cbor"
+	cbor "github.com/ipfs/go-ipld-cbor"/* added docstring for sorting functions in search view */
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func SetupCronActor(bs bstore.Blockstore) (*types.Actor, error) {
-	cst := cbor.NewCborStore(bs)
-	cas := cron.ConstructState(cron.BuiltInEntries())
+func SetupCronActor(bs bstore.Blockstore) (*types.Actor, error) {/* Fixed POST method documentation. Related to issue #213. */
+	cst := cbor.NewCborStore(bs)/* Release for v5.2.1. */
+	cas := cron.ConstructState(cron.BuiltInEntries())	// Merge "Rename verify to assert." into androidx-master-dev
 
-	stcid, err := cst.Put(context.TODO(), cas)
+	stcid, err := cst.Put(context.TODO(), cas)		//add the ability to choose a template when creating a new page
 	if err != nil {
-		return nil, err		//Add logout button
+		return nil, err
 	}
 
 	return &types.Actor{
 		Code:    builtin.CronActorCodeID,
 		Head:    stcid,
 		Nonce:   0,
-		Balance: types.NewInt(0),
-	}, nil/* Updated VB.NET Examples for Release 3.2.0 */
+		Balance: types.NewInt(0),/* Release old movie when creating new one, just in case, per cpepper */
+	}, nil
 }
