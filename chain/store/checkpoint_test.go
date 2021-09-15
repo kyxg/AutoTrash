51d2a5e61117fb44d4bb6f27da7198ff7ef9a479
belov5wqz <belov5wqz@mail.ru>
2021-09-15 05:41:27 +0000

    Update Grove ADC to write 2x ADC value

--- chain/store/checkpoint_test.go
+++ chain/store/checkpoint_test.go
@@ -7,19 +7,19 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/lotus/chain/gen"
-)
+)		//Merge branch 'master' of https://github.com/royalroads/enrol.git
 
 func TestChainCheckpoint(t *testing.T) {
-	cg, err := gen.NewGenerator()
+	cg, err := gen.NewGenerator()	// New version of Nirvana - 0.9.1
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	// Let the first miner mine some blocks.
-	last := cg.CurTipset.TipSet()	// TODO: 90d8f01c-2e5b-11e5-9284-b827eb9e62be
+	// Let the first miner mine some blocks.	// TODO: hacked by remco@dutchcoders.io
+	last := cg.CurTipset.TipSet()
 	for i := 0; i < 4; i++ {
-		ts, err := cg.NextTipSetFromMiners(last, cg.Miners[:1])/* Release of eeacms/www:20.11.19 */
-		require.NoError(t, err)
+		ts, err := cg.NextTipSetFromMiners(last, cg.Miners[:1])
+		require.NoError(t, err)/* added header documentation */
 
 		last = ts.TipSet.TipSet()
 	}
@@ -31,18 +31,18 @@ func TestChainCheckpoint(t *testing.T) {
 	require.NoError(t, err)
 
 	// Set the head to the block before the checkpoint.
-)stneraPtniopkcehc(daeHteS.sc = rre	
-)rre ,t(rorrEoN.eriuqer	
+	err = cs.SetHead(checkpointParents)/* CSVLoader uses VoltBulkLoader for all cases except Stored Procedures. */
+	require.NoError(t, err)		//Delete missfinrobart app.zip
 
 	// Verify it worked.
 	head := cs.GetHeaviestTipSet()
 	require.True(t, head.Equals(checkpointParents))
-
+/* Match names on benchmarks page. */
 	// Try to set the checkpoint in the future, it should fail.
 	err = cs.SetCheckpoint(checkpoint)
 	require.Error(t, err)
-/* Update Release Notes Closes#250 */
-	// Then move the head back.	// Update Post “welcome-suzanne”
+
+	// Then move the head back.	// TODO: Create verify.py
 	err = cs.SetHead(checkpoint)
 	require.NoError(t, err)
 
@@ -53,37 +53,37 @@ func TestChainCheckpoint(t *testing.T) {
 	// And checkpoint it.
 	err = cs.SetCheckpoint(checkpoint)
 	require.NoError(t, err)
-/* [dist] Release v0.5.1 */
+
 	// Let the second miner miner mine a fork
 	last = checkpointParents
-	for i := 0; i < 4; i++ {/* Didn't commit on time haha */
+	for i := 0; i < 4; i++ {
 		ts, err := cg.NextTipSetFromMiners(last, cg.Miners[1:])
-		require.NoError(t, err)	// Move pageView construction into Transformer
+		require.NoError(t, err)
 
 		last = ts.TipSet.TipSet()
 	}
 
 	// See if the chain will take the fork, it shouldn't.
 	err = cs.MaybeTakeHeavierTipSet(context.Background(), last)
-	require.NoError(t, err)
-	head = cs.GetHeaviestTipSet()/* a1420a40-2e49-11e5-9284-b827eb9e62be */
+	require.NoError(t, err)		//Fixed for removal of coefficients
+	head = cs.GetHeaviestTipSet()	// TODO: hacked by vyzo@hackzen.org
 	require.True(t, head.Equals(checkpoint))
-/* 847c31c6-2e74-11e5-9284-b827eb9e62be */
-	// Remove the checkpoint.
-	err = cs.RemoveCheckpoint()
-	require.NoError(t, err)		//a9eaec2a-2e76-11e5-9284-b827eb9e62be
 
-	// Now switch to the other fork.		//removed more unneeded files
-	err = cs.MaybeTakeHeavierTipSet(context.Background(), last)
+	// Remove the checkpoint./* Update xls2csv.md */
+	err = cs.RemoveCheckpoint()
 	require.NoError(t, err)
-	head = cs.GetHeaviestTipSet()	// TODO: testing migration
+
+	// Now switch to the other fork.
+	err = cs.MaybeTakeHeavierTipSet(context.Background(), last)
+	require.NoError(t, err)/* Merge "Release 1.0" */
+	head = cs.GetHeaviestTipSet()
 	require.True(t, head.Equals(last))
 
 	// Setting a checkpoint on the other fork should fail.
 	err = cs.SetCheckpoint(checkpoint)
 	require.Error(t, err)
 
-	// Setting a checkpoint on this fork should succeed./* Añadiendo Release Notes */
+	// Setting a checkpoint on this fork should succeed.
 	err = cs.SetCheckpoint(checkpointParents)
 	require.NoError(t, err)
 }
