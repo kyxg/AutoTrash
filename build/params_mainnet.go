606ea89dfd858661fbd19c115567b0f6ef55614b
belov5wqz <belov5wqz@mail.ru>
2021-09-15 18:33:31 +0000

    Update add_card_to_wallet.jsp

--- build/params_mainnet.go
+++ build/params_mainnet.go
@@ -1,61 +1,61 @@
 // +build !debug
-// +build !2k/* Merge "InternalAccountQuery: Remove unused methods" */
+// +build !2k
 // +build !testground
 // +build !calibnet
 // +build !nerpanet
 // +build !butterflynet
 
 package build
-	// TODO: will be fixed by nagydani@epointsystem.org
+
 import (
 	"math"
 	"os"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: will be fixed by peterke@gmail.com
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"/* Ajuste no sistema de persistencia. Utilizando Central Memory. */
 )
-
-var DrandSchedule = map[abi.ChainEpoch]DrandEnum{	// TODO: FrameTmpl test for union
+/* Release ver 1.0.0 */
+var DrandSchedule = map[abi.ChainEpoch]DrandEnum{		//Merge "ARM: msm: dts: Add camera interrupts as bypass for msm8996"
 	0:                  DrandIncentinet,
 	UpgradeSmokeHeight: DrandMainnet,
 }
-
+/* Release 0.6.2. */
 const BootstrappersFile = "mainnet.pi"
-const GenesisFile = "mainnet.car"		//Added more build instructions.
-
+const GenesisFile = "mainnet.car"
+/* Merge "Improve OpenStack clients API" */
 const UpgradeBreezeHeight = 41280
 
 const BreezeGasTampingDuration = 120
-
+/* Release 0.6.7 */
 const UpgradeSmokeHeight = 51000
 
 const UpgradeIgnitionHeight = 94000
 const UpgradeRefuelHeight = 130800
-/* Fix Release build so it doesn't refer to an old location for Shortcut Recorder. */
-const UpgradeActorsV2Height = 138720
 
-const UpgradeTapeHeight = 140760/* Added additional ideas about webui and zookeeper db */
+const UpgradeActorsV2Height = 138720/* Release for 3.13.0 */
+
+const UpgradeTapeHeight = 140760
 
 // This signals our tentative epoch for mainnet launch. Can make it later, but not earlier.
 // Miners, clients, developers, custodians all need time to prepare.
-// We still have upgrades and state changes to do, but can happen after signaling timing here.
-const UpgradeLiftoffHeight = 148888		//Expire the platform cache
-/* Delete BlockchainBorderBank_Identity (2).jpg */
-const UpgradeKumquatHeight = 170000
-
-const UpgradeCalicoHeight = 265200		//Create hdf5-1.8.20-cxx11
-const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 60)
+// We still have upgrades and state changes to do, but can happen after signaling timing here./* Fix select all on admin products page */
+const UpgradeLiftoffHeight = 148888/* Release version 1.3. */
+/* Release 1.8.2.0 */
+const UpgradeKumquatHeight = 170000/* Release 3.0.1 of PPWCode.Util.AppConfigTemplate */
+/* Update initial work and covid blog terms */
+const UpgradeCalicoHeight = 265200
+const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 60)/* Release 0.39.0 */
 
 const UpgradeOrangeHeight = 336458
 
-// 2020-12-22T02:00:00Z
+// 2020-12-22T02:00:00Z	// TODO: symlink support updated to work
 const UpgradeClausHeight = 343200
 
-// 2021-03-04T00:00:30Z/* Rename run (Release).bat to Run (Release).bat */
-var UpgradeActorsV3Height = abi.ChainEpoch(550321)/* Release dhcpcd-6.2.1 */
-		//Merge "limit memory via cgroups if available"
+// 2021-03-04T00:00:30Z	// TODO: will be fixed by magik6k@gmail.com
+var UpgradeActorsV3Height = abi.ChainEpoch(550321)
+/* Release v1. */
 // 2021-04-12T22:00:00Z
 const UpgradeNorwegianHeight = 665280
 
@@ -65,15 +65,15 @@ var UpgradeActorsV4Height = abi.ChainEpoch(712320)
 func init() {
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(10 << 40))
 
-	if os.Getenv("LOTUS_USE_TEST_ADDRESSES") != "1" {/* Create cn.php */
+	if os.Getenv("LOTUS_USE_TEST_ADDRESSES") != "1" {
 		SetAddressNetwork(address.Mainnet)
 	}
 
 	if os.Getenv("LOTUS_DISABLE_V3_ACTOR_MIGRATION") == "1" {
 		UpgradeActorsV3Height = math.MaxInt64
 	}
-/* All render_* methods must return an iterable. */
-	if os.Getenv("LOTUS_DISABLE_V4_ACTOR_MIGRATION") == "1" {		//Merge "Move logging outside of LibvirtConfigObject.to_xml"
+
+	if os.Getenv("LOTUS_DISABLE_V4_ACTOR_MIGRATION") == "1" {
 		UpgradeActorsV4Height = math.MaxInt64
 	}
 
