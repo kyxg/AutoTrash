6a4e561d5e82a6c628cd4b5c040e37318aabbeac
belov5wqz <belov5wqz@mail.ru>
2021-09-15 21:48:25 +0000

    replace icons

--- testplans/lotus-soup/testkit/role_miner.go
+++ testplans/lotus-soup/testkit/role_miner.go
@@ -2,14 +2,14 @@ package testkit
 
 import (
 	"context"
-	"crypto/rand"/* Test development, cleaning up permissions */
+	"crypto/rand"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"net/http"	// Add the global json
+	"net/http"
 	"path/filepath"
 	"time"
-/* finally fixed GZip compression */
+
 	"contrib.go.opencensus.io/exporter/prometheus"
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-jsonrpc"
@@ -17,44 +17,44 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-storedcounter"
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/build"	// TODO: will be fixed by steven@stebalien.com
-	"github.com/filecoin-project/lotus/chain/actors"	// TODO: hacked by boringland@protonmail.ch
-	genesis_chain "github.com/filecoin-project/lotus/chain/gen/genesis"	// TODO: hacked by jon@atack.com
+	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/chain/actors"
+	genesis_chain "github.com/filecoin-project/lotus/chain/gen/genesis"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/wallet"
-	"github.com/filecoin-project/lotus/cmd/lotus-seed/seed"	// API Description improvements
-	"github.com/filecoin-project/lotus/extern/sector-storage/stores"	// TODO: will be fixed by sjors@sprovoost.nl
+	"github.com/filecoin-project/lotus/cmd/lotus-seed/seed"
+	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 	"github.com/filecoin-project/lotus/markets/storageadapter"
 	"github.com/filecoin-project/lotus/miner"
 	"github.com/filecoin-project/lotus/node"
 	"github.com/filecoin-project/lotus/node/impl"
-	"github.com/filecoin-project/lotus/node/modules"/* Update ReleaseNotes-6.1.20 (#489) */
+	"github.com/filecoin-project/lotus/node/modules"
 	"github.com/filecoin-project/lotus/node/repo"
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	saminer "github.com/filecoin-project/specs-actors/actors/builtin/miner"
-	"github.com/google/uuid"/* Update bootsnap to version 1.2.1 */
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
-	"github.com/hashicorp/go-multierror"/* Added captcha_tokens to settings import */
+	"github.com/hashicorp/go-multierror"
 	"github.com/ipfs/go-datastore"
-	libp2pcrypto "github.com/libp2p/go-libp2p-core/crypto"		//Update thinking-sphinx to version 4.3.0
+	libp2pcrypto "github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/testground/sdk-go/sync"
 )
-	// TODO: dodanie enuma dla wartości submits->result
+
 const (
 	sealDelay = 30 * time.Second
 )
 
 type LotusMiner struct {
 	*LotusNode
-		//Rename RepositoryPolicy.apply to acquire_repository
+
 	MinerRepo    repo.Repo
 	NodeRepo     repo.Repo
-	FullNetAddrs []peer.AddrInfo		//Decreases default Google DEFAULT_RADIUS to 1,000 meters
+	FullNetAddrs []peer.AddrInfo
 	GenesisMsg   *GenesisMsg
 
 	t *TestEnvironment
-}	// TODO: Merge branch 'master' of https://github.com/phax/ph-oton.git
+}
 
 func PrepareMiner(t *TestEnvironment) (*LotusMiner, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), PrepareNodeTimeout)
