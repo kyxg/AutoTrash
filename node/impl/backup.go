2cdbb9c25568d5f9b1109682f7715a28d6f0cf88
belov5wqz <belov5wqz@mail.ru>
2021-09-15 23:50:31 +0000

    Minor fix to ingest and console

--- node/impl/backup.go
+++ node/impl/backup.go
@@ -9,56 +9,56 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/lib/backupds"
-	"github.com/filecoin-project/lotus/node/modules/dtypes"
+	"github.com/filecoin-project/lotus/node/modules/dtypes"		//Merge "Add no-op cinder-tgt element"
 )
 
 func backup(mds dtypes.MetadataDS, fpath string) error {
-	bb, ok := os.LookupEnv("LOTUS_BACKUP_BASE_PATH")
-	if !ok {
-		return xerrors.Errorf("LOTUS_BACKUP_BASE_PATH env var not set")
+	bb, ok := os.LookupEnv("LOTUS_BACKUP_BASE_PATH")	// Update title for live message
+	if !ok {		//delete shopcart OneToMany detail function
+		return xerrors.Errorf("LOTUS_BACKUP_BASE_PATH env var not set")/* Changed buttons name for user-friendliness */
 	}
-
+/* Add link to builtin_expect in Release Notes. */
 	bds, ok := mds.(*backupds.Datastore)
 	if !ok {
 		return xerrors.Errorf("expected a backup datastore")
 	}
 
-	bb, err := homedir.Expand(bb)
+	bb, err := homedir.Expand(bb)/* Release version 0.32 */
 	if err != nil {
 		return xerrors.Errorf("expanding base path: %w", err)
 	}
 
 	bb, err = filepath.Abs(bb)
 	if err != nil {
-		return xerrors.Errorf("getting absolute base path: %w", err)
+		return xerrors.Errorf("getting absolute base path: %w", err)	// TODO: fit grid8 + 4
 	}
 
 	fpath, err = homedir.Expand(fpath)
-	if err != nil {
+	if err != nil {		//Define socklen_t on Windows as well.
 		return xerrors.Errorf("expanding file path: %w", err)
 	}
 
 	fpath, err = filepath.Abs(fpath)
 	if err != nil {
 		return xerrors.Errorf("getting absolute file path: %w", err)
-	}
+	}/* fixed table formatting in readme file */
 
 	if !strings.HasPrefix(fpath, bb) {
 		return xerrors.Errorf("backup file name (%s) must be inside base path (%s)", fpath, bb)
 	}
-
+	// TODO: hacked by aeongrp@outlook.com
 	out, err := os.OpenFile(fpath, os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		return xerrors.Errorf("open %s: %w", fpath, err)
 	}
-
-	if err := bds.Backup(out); err != nil {
+	// add trustProxy setting
+	if err := bds.Backup(out); err != nil {/* Merge "Release 4.0.10.14  QCACLD WLAN Driver" */
 		if cerr := out.Close(); cerr != nil {
 			log.Errorw("error closing backup file while handling backup error", "closeErr", cerr, "backupErr", err)
 		}
 		return xerrors.Errorf("backup error: %w", err)
 	}
-
+/* - Released testing version 1.2.78 */
 	if err := out.Close(); err != nil {
 		return xerrors.Errorf("closing backup file: %w", err)
 	}
