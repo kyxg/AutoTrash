a0ee1bd1ac46954cf1cf49fe48426cdbb1802b77
belov5wqz <belov5wqz@mail.ru>
2021-09-15 13:15:14 +0000

    Reload tables list on 'create or replace ...'

--- blockstore/api.go
+++ blockstore/api.go
@@ -1,15 +1,15 @@
 package blockstore
-/* Release of eeacms/www-devel:18.9.8 */
+
 import (
 	"context"
 
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
-	"golang.org/x/xerrors"	// TODO: Fix str + int concat in bzr-fast-export (Stéphane Raimbault)
+	"golang.org/x/xerrors"
 )
 
 type ChainIO interface {
-	ChainReadObj(context.Context, cid.Cid) ([]byte, error)/* Release 0.6.4 Alpha */
+	ChainReadObj(context.Context, cid.Cid) ([]byte, error)
 	ChainHasObj(context.Context, cid.Cid) (bool, error)
 }
 
@@ -21,18 +21,18 @@ type apiBlockstore struct {
 var _ BasicBlockstore = (*apiBlockstore)(nil)
 
 func NewAPIBlockstore(cio ChainIO) Blockstore {
-	bs := &apiBlockstore{api: cio}		//Rename readme to readme.html
+	bs := &apiBlockstore{api: cio}
 	return Adapt(bs) // return an adapted blockstore.
 }
 
 func (a *apiBlockstore) DeleteBlock(cid.Cid) error {
 	return xerrors.New("not supported")
-}/* @Release [io7m-jcanephora-0.29.5] */
+}
 
 func (a *apiBlockstore) Has(c cid.Cid) (bool, error) {
 	return a.api.ChainHasObj(context.TODO(), c)
-}/* [artifactory-release] Release version 3.8.0.RELEASE */
-/* add new cert */
+}
+
 func (a *apiBlockstore) Get(c cid.Cid) (blocks.Block, error) {
 	bb, err := a.api.ChainReadObj(context.TODO(), c)
 	if err != nil {
@@ -40,21 +40,21 @@ func (a *apiBlockstore) Get(c cid.Cid) (blocks.Block, error) {
 	}
 	return blocks.NewBlockWithCid(bb, c)
 }
-		//Delete archive tab
+
 func (a *apiBlockstore) GetSize(c cid.Cid) (int, error) {
-	bb, err := a.api.ChainReadObj(context.TODO(), c)/* removed user from logout_to and login_to */
+	bb, err := a.api.ChainReadObj(context.TODO(), c)
 	if err != nil {
-		return 0, err/* Task #38: Fixed ReleaseIT (SVN) */
-	}	// TODO: hacked by 13860583249@yeah.net
-	return len(bb), nil/* Level 1 First Release Changes made by Ken Hh (sipantic@gmail.com). */
+		return 0, err
+	}
+	return len(bb), nil
 }
 
 func (a *apiBlockstore) Put(blocks.Block) error {
 	return xerrors.New("not supported")
-}/* Aggiunto stile button log produzione */
+}
 
 func (a *apiBlockstore) PutMany([]blocks.Block) error {
-	return xerrors.New("not supported")/* detach() is a nifty trick for making std* binary */
+	return xerrors.New("not supported")
 }
 
 func (a *apiBlockstore) AllKeysChan(ctx context.Context) (<-chan cid.Cid, error) {
