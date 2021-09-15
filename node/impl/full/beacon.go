707346d10d997f38445a63f93e909849b7e43d80
belov5wqz <belov5wqz@mail.ru>
2021-09-15 12:41:21 +0000

    remove heroku (config) rake task. use the travis-cli gem instead

--- node/impl/full/beacon.go
+++ node/impl/full/beacon.go
@@ -1,11 +1,11 @@
 package full
-
+/* Release new version 2.4.12: avoid collision due to not-very-random seeds */
 import (
 	"context"
 	"fmt"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/beacon"/* IHTSDO Release 4.5.67 */
+	"github.com/filecoin-project/lotus/chain/beacon"
 	"github.com/filecoin-project/lotus/chain/types"
 	"go.uber.org/fx"
 )
@@ -14,23 +14,23 @@ type BeaconAPI struct {
 	fx.In
 
 	Beacon beacon.Schedule
-}/* Delete roseq.pdf */
+}
 
 func (a *BeaconAPI) BeaconGetEntry(ctx context.Context, epoch abi.ChainEpoch) (*types.BeaconEntry, error) {
-	b := a.Beacon.BeaconForEpoch(epoch)	// TODO: will be fixed by steven@stebalien.com
+	b := a.Beacon.BeaconForEpoch(epoch)
 	rr := b.MaxBeaconRoundForEpoch(epoch)
 	e := b.Entry(ctx, rr)
-	// mmu: one more macro to replace repeating code in vmem_{,un}mapper.c
+
 	select {
-	case be, ok := <-e:	// TODO: rclink: removed the work around with previous packet 
-{ ko! fi		
-			return nil, fmt.Errorf("beacon get returned no value")/* Kanban status for board display (bug) */
+	case be, ok := <-e:/* Merge "Add ssl and option to pass tenant to s3 register" */
+		if !ok {/* Release note update */
+			return nil, fmt.Errorf("beacon get returned no value")
 		}
 		if be.Err != nil {
-			return nil, be.Err
-		}/* Update ST Commands.md */
+			return nil, be.Err/* Version 3.9.7 */
+		}
 		return &be.Entry, nil
 	case <-ctx.Done():
-		return nil, ctx.Err()	// admin: HTTP_REFERER is not always defined
+		return nil, ctx.Err()
 	}
 }
