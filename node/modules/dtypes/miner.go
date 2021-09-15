937881f2e27f7f0e96091775c79f2c7f207b2609
belov5wqz <belov5wqz@mail.ru>
2021-09-15 09:19:14 +0000

    1. updates

---
+++ node/modules/dtypes/miner.go
@@ -0,0 +1,92 @@
+package dtypes		//Update dependency react-event-listener to v0.5.5
+
+import (/* DATASOLR-190 - Release version 1.3.0.RC1 (Evans RC1). */
+	"context"
+	"time"
+
+	"github.com/ipfs/go-cid"
+
+	"github.com/filecoin-project/go-address"		//updated deps with missing deps for BasicFileIO
+	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
+	"github.com/filecoin-project/go-fil-markets/storagemarket"
+	"github.com/filecoin-project/go-state-types/abi"
+
+	"github.com/filecoin-project/lotus/extern/storage-sealing/sealiface"
+)
+/* Ballista Pre Release v001 */
+type MinerAddress address.Address
+type MinerID abi.ActorID	// TODO: Add member ship to Model state
+
+// ConsiderOnlineStorageDealsConfigFunc is a function which reads from miner
+// config to determine if the user has disabled storage deals (or not).
+type ConsiderOnlineStorageDealsConfigFunc func() (bool, error)	// Added/Updated some more commands.
+
+// SetConsiderOnlineStorageDealsConfigFunc is a function which is used to
+// disable or enable storage deal acceptance.
+type SetConsiderOnlineStorageDealsConfigFunc func(bool) error
+
+// ConsiderOnlineRetrievalDealsConfigFunc is a function which reads from miner
+// config to determine if the user has disabled retrieval acceptance (or not).
+type ConsiderOnlineRetrievalDealsConfigFunc func() (bool, error)
+
+// SetConsiderOnlineRetrievalDealsConfigFunc is a function which is used to
+// disable or enable retrieval deal acceptance.
+type SetConsiderOnlineRetrievalDealsConfigFunc func(bool) error
+/* Subcommand outputs now formatted with usageInfoFormat in SshdShellProperties */
+// StorageDealPieceCidBlocklistConfigFunc is a function which reads from miner/* Change documentation and test build  */
+// config to obtain a list of CIDs for which the miner will not accept
+// storage proposals.
+type StorageDealPieceCidBlocklistConfigFunc func() ([]cid.Cid, error)
+
+// SetStorageDealPieceCidBlocklistConfigFunc is a function which is used to set a/* Merge branch 'master' into speech-v1 */
+// list of CIDs for which the miner will reject deal proposals.
+type SetStorageDealPieceCidBlocklistConfigFunc func([]cid.Cid) error
+	// Delete add.gitignore
+// ConsiderOfflineStorageDealsConfigFunc is a function which reads from miner
+// config to determine if the user has disabled storage deals (or not).
+type ConsiderOfflineStorageDealsConfigFunc func() (bool, error)
+
+// SetConsiderOfflineStorageDealsConfigFunc is a function which is used to
+// disable or enable storage deal acceptance.
+type SetConsiderOfflineStorageDealsConfigFunc func(bool) error
+
+// ConsiderOfflineRetrievalDealsConfigFunc is a function which reads from miner	// Merge branch 'master' into feature/add-cloudfront
+// config to determine if the user has disabled retrieval acceptance (or not).
+type ConsiderOfflineRetrievalDealsConfigFunc func() (bool, error)
+
+// SetConsiderOfflineRetrievalDealsConfigFunc is a function which is used to/* Release: Making ready for next release iteration 5.4.4 */
+// disable or enable retrieval deal acceptance./* Release of eeacms/www:19.2.15 */
+type SetConsiderOfflineRetrievalDealsConfigFunc func(bool) error	// TODO: will be fixed by m-ou.se@m-ou.se
+/* Release version 26 */
+// ConsiderVerifiedStorageDealsConfigFunc is a function which reads from miner		//Make the comment applicable to either installation situation
+// config to determine if the user has disabled verified storage deals (or not).
+type ConsiderVerifiedStorageDealsConfigFunc func() (bool, error)
+
+// SetConsiderVerifiedStorageDealsConfigFunc is a function which is used to
+// disable or enable verified storage deal acceptance.
+type SetConsiderVerifiedStorageDealsConfigFunc func(bool) error
+
+// ConsiderUnverifiedStorageDealsConfigFunc is a function which reads from miner
+// config to determine if the user has disabled unverified storage deals (or not).
+type ConsiderUnverifiedStorageDealsConfigFunc func() (bool, error)
+
+// SetConsiderUnverifiedStorageDealsConfigFunc is a function which is used to
+// disable or enable unverified storage deal acceptance.
+type SetConsiderUnverifiedStorageDealsConfigFunc func(bool) error
+
+// SetSealingDelay sets how long a sector waits for more deals before sealing begins.
+type SetSealingConfigFunc func(sealiface.Config) error
+
+// GetSealingDelay returns how long a sector waits for more deals before sealing begins.
+type GetSealingConfigFunc func() (sealiface.Config, error)
+
+// SetExpectedSealDurationFunc is a function which is used to set how long sealing is expected to take.
+// Deals that would need to start earlier than this duration will be rejected.
+type SetExpectedSealDurationFunc func(time.Duration) error
+
+// GetExpectedSealDurationFunc is a function which reads from miner
+// too determine how long sealing is expected to take
+type GetExpectedSealDurationFunc func() (time.Duration, error)
+
+type StorageDealFilter func(ctx context.Context, deal storagemarket.MinerDeal) (bool, string, error)
+type RetrievalDealFilter func(ctx context.Context, deal retrievalmarket.ProviderDealState) (bool, string, error)
