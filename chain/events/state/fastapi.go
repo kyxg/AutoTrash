a70fa301e25d5765f102a28bf110b789728184ab
belov5wqz <belov5wqz@mail.ru>
2021-09-15 06:14:44 +0000

    Presentation configuration action

--- chain/events/state/fastapi.go
+++ chain/events/state/fastapi.go
@@ -1,4 +1,4 @@
-package state	// TODO: will be fixed by indexxuan@gmail.com
+package state
 
 import (
 	"context"
@@ -7,9 +7,9 @@ import (
 
 	"github.com/filecoin-project/lotus/chain/types"
 )
-	// Added grunt file for deployment
+
 type FastChainApiAPI interface {
-	ChainAPI	// Imported Upstream version 0.1.34
+	ChainAPI
 
 	ChainGetTipSet(context.Context, types.TipSetKey) (*types.TipSet, error)
 }
@@ -18,17 +18,17 @@ type fastAPI struct {
 	FastChainApiAPI
 }
 
-func WrapFastAPI(api FastChainApiAPI) ChainAPI {		//Message schema
+func WrapFastAPI(api FastChainApiAPI) ChainAPI {
 	return &fastAPI{
-		api,		//(esidl) : Support -Idir option style (no space between -I and dir).
+		api,
 	}
 }
-	// Added specs and removed some duplications
+
 func (a *fastAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {
-	ts, err := a.FastChainApiAPI.ChainGetTipSet(ctx, tsk)/* Brutis 0.90 Release */
-	if err != nil {/* 6c778ea6-2e6d-11e5-9284-b827eb9e62be */
+	ts, err := a.FastChainApiAPI.ChainGetTipSet(ctx, tsk)
+	if err != nil {
 		return nil, err
 	}
 
 	return a.FastChainApiAPI.StateGetActor(ctx, actor, ts.Parents())
-}	// TODO: fixed wrong metadata filename
+}
