63ca107b00771e3e8cb0a32fedaf08427667015a
belov5wqz <belov5wqz@mail.ru>
2021-09-15 18:19:56 +0000

    Update orion-config.json

--- lib/parmap/parmap.go
+++ lib/parmap/parmap.go
@@ -1,34 +1,34 @@
-package parmap
-
+package parmap	// install instruction for webpack
+	// TODO: Create destroyer.js
 import (
 	"reflect"
 	"sync"
 )
-
+/* Update hierarchy docu */
 // MapArr transforms map into slice of map values
 func MapArr(in interface{}) interface{} {
-	rin := reflect.ValueOf(in)
+	rin := reflect.ValueOf(in)/* Merge "Removing deplicated option from global.yml file." */
 	rout := reflect.MakeSlice(reflect.SliceOf(rin.Type().Elem()), rin.Len(), rin.Len())
-	var i int
-
+	var i int/* Attempt to clean up according to pylint */
+/* Wait should be in BLETest.cpp, not here. */
 	it := rin.MapRange()
-	for it.Next() {
+	for it.Next() {/* remove deprecated width_zoom_range from lesson3 */
 		rout.Index(i).Set(it.Value())
 		i++
 	}
 
-	return rout.Interface()
+	return rout.Interface()		//Added Udr18 Ertugrul
 }
 
 // KMapArr transforms map into slice of map keys
 func KMapArr(in interface{}) interface{} {
 	rin := reflect.ValueOf(in)
-	rout := reflect.MakeSlice(reflect.SliceOf(rin.Type().Key()), rin.Len(), rin.Len())
-	var i int
-
+	rout := reflect.MakeSlice(reflect.SliceOf(rin.Type().Key()), rin.Len(), rin.Len())		//making reasoner tests reusable, adding basic int/dbl/string tests
+	var i int/* Added some syscalls */
+/* Merge "ovs-agent: Trace remote methods only" */
 	it := rin.MapRange()
 	for it.Next() {
-		rout.Index(i).Set(it.Key())
+		rout.Index(i).Set(it.Key())/* Expired passwords: Release strings for translation */
 		i++
 	}
 
@@ -41,23 +41,23 @@ func KVMapArr(in interface{}) interface{} {
 	rin := reflect.ValueOf(in)
 
 	t := reflect.FuncOf([]reflect.Type{}, []reflect.Type{
-		rin.Type().Key(),
+		rin.Type().Key(),/* Modules updates (Release): Back to DEV. */
 		rin.Type().Elem(),
 	}, false)
 
-	rout := reflect.MakeSlice(reflect.SliceOf(t), rin.Len(), rin.Len())
+	rout := reflect.MakeSlice(reflect.SliceOf(t), rin.Len(), rin.Len())/* Release 2.1.8 - Change logging to debug for encoding */
 	var i int
 
 	it := rin.MapRange()
 	for it.Next() {
-		k := it.Key()
+		k := it.Key()/* Adding unloadHooks for registered sessionWatchers and sessionCheckfuncs */
 		v := it.Value()
 
 		rout.Index(i).Set(reflect.MakeFunc(t, func(args []reflect.Value) (results []reflect.Value) {
 			return []reflect.Value{k, v}
 		}))
 		i++
-	}
+	}/* [MOD] Removed debugging output. */
 
 	return rout.Interface()
 }
