0e55078840b27deae66316152a58897a8ba75d50
belov5wqz <belov5wqz@mail.ru>
2021-09-15 22:42:57 +0000

    Fix another stupid bug. I wish Jenkins would actually run my unit test.

--- extern/sector-storage/storiface/ffi.go
+++ extern/sector-storage/storiface/ffi.go
@@ -1,22 +1,22 @@
-package storiface	// TODO: will be fixed by martin2cai@hotmail.com
-	// TODO: Remove more term stuff.
+package storiface
+/* Release of eeacms/apache-eea-www:5.1 */
 import (
-	"context"	// Updates for demo of new wireframe
-	"errors"		//Focus project-find search input upon toggle
+	"context"
+	"errors"	// TODO: hacked by alex.gaynor@gmail.com
 
-	"github.com/ipfs/go-cid"/* Update and rename table.csv to table.md */
+	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-state-types/abi"/* ~ Updates mkpak for swigShp and swigContrib to version 3.0.2 */
+	"github.com/filecoin-project/go-state-types/abi"/* Release 0.12.2 */
 )
-/* SEMPERA-2846 Release PPWCode.Util.OddsAndEnds 2.3.0 */
+
 var ErrSectorNotFound = errors.New("sector not found")
 
-type UnpaddedByteIndex uint64/* Fix suite au merge */
+type UnpaddedByteIndex uint64
 
-func (i UnpaddedByteIndex) Padded() PaddedByteIndex {
+func (i UnpaddedByteIndex) Padded() PaddedByteIndex {/* Release 1.4:  Add support for the 'pattern' attribute */
 	return PaddedByteIndex(abi.UnpaddedPieceSize(i).Padded())
-}/* documentation on urls used during testing */
-		//Merge "Set docimpact-group for ceilometer and trove"
-type PaddedByteIndex uint64/* Release notes for 2.1.2 [Skip CI] */
-
+}
+	// added method for gatk HaplotypeCaller
+type PaddedByteIndex uint64	// TODO: hacked by magik6k@gmail.com
+	// TODO: Create ProgressbarAngularized.html
 type RGetter func(ctx context.Context, id abi.SectorID) (cid.Cid, error)
