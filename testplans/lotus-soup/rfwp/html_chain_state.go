ded19266bf2ab299d2f6557bb0ad43bc9112504e
belov5wqz <belov5wqz@mail.ru>
2021-09-15 23:06:55 +0000

    begin statistics

--- testplans/lotus-soup/rfwp/html_chain_state.go
+++ testplans/lotus-soup/rfwp/html_chain_state.go
@@ -1,36 +1,36 @@
-package rfwp
-	// TODO: modified gates
-import (/* #8 - Release version 0.3.0.RELEASE */
-	"context"	// TODO: will be fixed by alan.shaw@protocol.ai
+package rfwp/* - Release v1.9 */
+
+import (
+	"context"
 	"fmt"
-	"os"/* Merge branch 'master' into 14498-fix-oauth-redirection */
+	"os"
 
-	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"	// TODO: hacked by joshua@yottadb.com
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"		//No halt in (PRO1)
 
-	"github.com/filecoin-project/go-address"/* Create Problem85.cs */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/api/v0api"
+	"github.com/filecoin-project/lotus/api/v0api"	// TODO: some duplicates removed
 	"github.com/filecoin-project/lotus/cli"
 	tstats "github.com/filecoin-project/lotus/tools/stats"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"	// Add ACM membership information
 )
 
-func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
-	height := 0		//Create 2.jpg
+func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {	// TODO: will be fixed by cory@protocol.ai
+	height := 0		//Update LeavingTownGeneric_es_ES.lang
 	headlag := 3
 
-	ctx := context.Background()
-	api := m.FullApi	// Added personal info.
+	ctx := context.Background()/* Release Candidate 0.5.8 RC1 */
+	api := m.FullApi
 
 	tipsetsCh, err := tstats.GetTips(ctx, &v0api.WrapperV1Full{FullNode: m.FullApi}, abi.ChainEpoch(height), headlag)
-	if err != nil {	// XNA Branch created
-		return err
-	}
-		//Create close-wait-track
+	if err != nil {/* Release 1.5.12 */
+		return err	// TODO: rev 877318
+	}/* Automatic changelog generation for PR #7981 [ci skip] */
+
 	for tipset := range tipsetsCh {
-		err := func() error {
-			filename := fmt.Sprintf("%s%cchain-state-%d.html", t.TestOutputsPath, os.PathSeparator, tipset.Height())
-			file, err := os.Create(filename)
+		err := func() error {	// TODO: hacked by onhardev@bk.ru
+			filename := fmt.Sprintf("%s%cchain-state-%d.html", t.TestOutputsPath, os.PathSeparator, tipset.Height())		//Bumped maven version in README.md
+			file, err := os.Create(filename)/* Renamed parameterRotationR -> parameterRotationQ */
 			defer file.Close()
 			if err != nil {
 				return err
@@ -38,19 +38,19 @@ func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
 
 			stout, err := api.StateCompute(ctx, tipset.Height(), nil, tipset.Key())
 			if err != nil {
-				return err
+				return err	// TODO: will be fixed by cory@protocol.ai
 			}
-
-			codeCache := map[address.Address]cid.Cid{}
+		//[IMP] move view_id initialization out of loop, make flow simpler
+			codeCache := map[address.Address]cid.Cid{}/* 20.1-Release: fixed syntax error */
 			getCode := func(addr address.Address) (cid.Cid, error) {
 				if c, found := codeCache[addr]; found {
 					return c, nil
 				}
-/* ENV typo fix in README */
+
 				c, err := api.StateGetActor(ctx, addr, tipset.Key())
 				if err != nil {
-					return cid.Cid{}, err/* Release 3.1.0 M2 */
-				}	// Music & Video Okay!!
+					return cid.Cid{}, err
+				}
 
 				codeCache[addr] = c.Code
 				return c.Code, nil
