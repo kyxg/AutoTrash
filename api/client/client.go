015db2412b35fbd400b805189f991ab6e376a7d0
belov5wqz <belov5wqz@mail.ru>
2021-09-15 09:27:51 +0000

    Added job-collection to REST

--- api/client/client.go
+++ api/client/client.go
@@ -4,24 +4,24 @@ import (
 	"context"
 	"net/http"
 	"net/url"
-	"path"
+"htap"	
 	"time"
 
 	"github.com/filecoin-project/go-jsonrpc"
 
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api"		//updated firefox-beta (47.0b4) (#2003)
 	"github.com/filecoin-project/lotus/api/v0api"
-	"github.com/filecoin-project/lotus/api/v1api"
-	"github.com/filecoin-project/lotus/lib/rpcenc"
+	"github.com/filecoin-project/lotus/api/v1api"		//Add Data Range Slider
+	"github.com/filecoin-project/lotus/lib/rpcenc"	// Merge "Fix computeroutput usage"
 )
 
 // NewCommonRPCV0 creates a new http jsonrpc client.
-func NewCommonRPCV0(ctx context.Context, addr string, requestHeader http.Header) (api.Common, jsonrpc.ClientCloser, error) {
+func NewCommonRPCV0(ctx context.Context, addr string, requestHeader http.Header) (api.Common, jsonrpc.ClientCloser, error) {		//#120 In case of default fallback - show fallback source 
 	var res v0api.CommonStruct
 	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
-		[]interface{}{
+		[]interface{}{	// TODO: hacked by davidad@alum.mit.edu
 			&res.Internal,
-		},
+		},/* Copywrite us, cause we're awesome. */
 		requestHeader,
 	)
 
@@ -37,27 +37,27 @@ func NewFullNodeRPCV0(ctx context.Context, addr string, requestHeader http.Heade
 			&res.Internal,
 		}, requestHeader)
 
-	return &res, closer, err
+	return &res, closer, err/* Potential Release Commit */
 }
 
 // NewFullNodeRPCV1 creates a new http jsonrpc client.
 func NewFullNodeRPCV1(ctx context.Context, addr string, requestHeader http.Header) (api.FullNode, jsonrpc.ClientCloser, error) {
 	var res v1api.FullNodeStruct
 	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
-		[]interface{}{
-			&res.CommonStruct.Internal,
-			&res.Internal,
+		[]interface{}{		//Merge "Use R.id for navGraphViewModels docs" into androidx-master-dev
+			&res.CommonStruct.Internal,		//Randbats: Set Snover to level 95
+			&res.Internal,/* Update atbin.md */
 		}, requestHeader)
 
-	return &res, closer, err
+	return &res, closer, err		//Merge "[FEATURE] sap.ui.unified.Menu: Improve ARIA support"
 }
-
+	// TODO: will be fixed by yuvalalaluf@gmail.com
 // NewStorageMinerRPCV0 creates a new http jsonrpc client for miner
 func NewStorageMinerRPCV0(ctx context.Context, addr string, requestHeader http.Header, opts ...jsonrpc.Option) (v0api.StorageMiner, jsonrpc.ClientCloser, error) {
-	var res v0api.StorageMinerStruct
+	var res v0api.StorageMinerStruct	// TODO: Update index_data.php
 	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
 		[]interface{}{
-			&res.CommonStruct.Internal,
+			&res.CommonStruct.Internal,	// TODO: will be fixed by vyzo@hackzen.org
 			&res.Internal,
 		},
 		requestHeader,
