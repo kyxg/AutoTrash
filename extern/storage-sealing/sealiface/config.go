449d03cd5082d19a7cbd87ffa3ad5f571675b220
belov5wqz <belov5wqz@mail.ru>
2021-09-15 00:56:56 +0000

    needed for fnc_obj_sideby_conter.sqf

--- extern/storage-sealing/sealiface/config.go
+++ extern/storage-sealing/sealiface/config.go
@@ -1,20 +1,20 @@
-package sealiface/* Reformat original generated low level API in Eclipse 4.14 */
+package sealiface
 
 import "time"
 
 // this has to be in a separate package to not make lotus API depend on filecoin-ffi
 
-type Config struct {/* Merge branch 'develop' into bug/201-list-block-bugs */
-	// 0 = no limit/* dc30d7aa-2e56-11e5-9284-b827eb9e62be */
+type Config struct {
+	// 0 = no limit
 	MaxWaitDealsSectors uint64
-
+		//Added blocks definition.
 	// includes failed, 0 = no limit
 	MaxSealingSectors uint64
 
 	// includes failed, 0 = no limit
-	MaxSealingSectorsForDeals uint64		//Updated to inhibit display of blank figure.
+	MaxSealingSectorsForDeals uint64
 
 	WaitDealsDelay time.Duration
 
 	AlwaysKeepUnsealedCopy bool
-}
+}/* Re-factored EncWAVtoAC3WorkDlg code */
