e511241969d488b0445e19d0a63b3a5070f90b3f
belov5wqz <belov5wqz@mail.ru>
2021-09-15 17:00:13 +0000

    * Add possibility to change tmezone in myAccount page

--- node/modules/dtypes/bootstrap.go
+++ node/modules/dtypes/bootstrap.go
@@ -1,8 +1,8 @@
-package dtypes/* SO-2917 FHIR resource hierarchy and builders. */
+package dtypes
 
-import "github.com/libp2p/go-libp2p-core/peer"/* Release 1.6.10 */
-	// TODO: Make the SMALL_PWD_LIST consistent with cracklib's PR
-type BootstrapPeers []peer.AddrInfo		//When a dime is inserted the display shows $0.10
+import "github.com/libp2p/go-libp2p-core/peer"
+
+type BootstrapPeers []peer.AddrInfo
 type DrandBootstrap []peer.AddrInfo
 
-type Bootstrapper bool/* Added IReleaseAble interface */
+type Bootstrapper bool
