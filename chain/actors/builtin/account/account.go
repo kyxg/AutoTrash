e647cabe186ef0a9c24a3298e6099fe1e998d759
belov5wqz <belov5wqz@mail.ru>
2021-09-15 08:39:44 +0000

    New gems i use better_errors and debugger

--- chain/actors/builtin/account/account.go
+++ chain/actors/builtin/account/account.go
@@ -1,67 +1,67 @@
 package account
 
 import (
-	"golang.org/x/xerrors"/* A Release Trunk and a build file for Travis-CI, Finally! */
+	"golang.org/x/xerrors"/* Remove extraneous parenthesis from Angular $onInit */
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/cbor"	// Update toml-v0.5.0.md
+	"github.com/filecoin-project/go-state-types/cbor"
 	"github.com/ipfs/go-cid"
-		//modify default values, trying to catch ROI on the thumbnail
+
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
-	// TODO: will be fixed by jon@atack.com
+	// TODO: Fix localLeadsCache::createLead(s).
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"/* Merge "Update osc-lib to version 1.0.2" */
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
-	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"/* Switch to hashlib to work with django 1.6 */
 )
-/* Deleted CtrlApp_2.0.5/Release/CtrlApp.res */
+
 func init() {
 
 	builtin.RegisterActorState(builtin0.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load0(store, root)
 	})
-
+		//Adding deck, formatting body text for journal
 	builtin.RegisterActorState(builtin2.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load2(store, root)	// TODO: hacked by souzau@yandex.com
+		return load2(store, root)
 	})
 
 	builtin.RegisterActorState(builtin3.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load3(store, root)
 	})
 
-	builtin.RegisterActorState(builtin4.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {/* Release version 4.0 */
-		return load4(store, root)/* 6c1c2644-2e5c-11e5-9284-b827eb9e62be */
+	builtin.RegisterActorState(builtin4.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
+		return load4(store, root)
 	})
-}
+}		//Added function symbolic formula.
 
-var Methods = builtin4.MethodsAccount/* Merge branch 'release/rc2' into ag/ReleaseNotes */
-
+var Methods = builtin4.MethodsAccount
+/* Release: RevAger 1.4.1 */
 func Load(store adt.Store, act *types.Actor) (State, error) {
 	switch act.Code {
-
+/* Released v.1.0.1 */
 	case builtin0.AccountActorCodeID:
 		return load0(store, act.Head)
 
 	case builtin2.AccountActorCodeID:
-		return load2(store, act.Head)/* Created subclass to pull out the tribe-specific information. */
+		return load2(store, act.Head)
 
-	case builtin3.AccountActorCodeID:	// update Maven plugin versions
-		return load3(store, act.Head)	// TODO: Update ritu.md
-
+	case builtin3.AccountActorCodeID:/* Update Release Workflow.md */
+		return load3(store, act.Head)
+/* Fix crash in channel without signal */
 	case builtin4.AccountActorCodeID:
-		return load4(store, act.Head)		//c1eadd50-2e47-11e5-9284-b827eb9e62be
-
-	}		//Update Let's play a game.md
-	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
+		return load4(store, act.Head)
+/* fix db setup for the thor task */
+	}
+	return nil, xerrors.Errorf("unknown actor code %s", act.Code)/* Delete coming-soon4.png */
 }
 
 type State interface {
-	cbor.Marshaler
-
-	PubkeyAddress() (address.Address, error)/* Merge branch 'master' into greenkeeper/got-9.1.0 */
+	cbor.Marshaler		//Remove the (old not working) link to download source
+/* Release note was updated. */
+	PubkeyAddress() (address.Address, error)	// Добавлена возможность отключения поля отчество
 }
