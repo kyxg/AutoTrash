0fe268b40c68dcab185c6e7729da3a1d8319d68d
belov5wqz <belov5wqz@mail.ru>
2021-09-15 16:08:36 +0000

    Use self.NAME in File#_info.

---
+++ chain/actors/builtin/cron/cron.go
@@ -0,0 +1,10 @@
+package cron
+
+import (		//Update list-resource.markdown
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
+)
+
+var (/* f5b68432-2e6e-11e5-9284-b827eb9e62be */
+	Address = builtin4.CronActorAddr
+	Methods = builtin4.MethodsCron/* Release 0.95.150: model improvements, lab of planet in the listing. */
+)
