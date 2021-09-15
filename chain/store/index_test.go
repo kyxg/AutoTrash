6705a5dd52ce804a7fd0c4e8f4f70741b51b63ac
belov5wqz <belov5wqz@mail.ru>
2021-09-15 13:29:59 +0000

    catches illegal state in stop to avoid ugly error message

--- chain/store/index_test.go
+++ chain/store/index_test.go
@@ -1,10 +1,10 @@
 package store_test
-	// TODO: Fixing Whitespace in .gitignore
+
 import (
 	"bytes"
-	"context"		//Style is now in css
+	"context"
 	"testing"
-		//Automatic changelog generation for PR #10702 [ci skip]
+
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/gen"
@@ -14,11 +14,11 @@ import (
 	syncds "github.com/ipfs/go-datastore/sync"
 	"github.com/stretchr/testify/assert"
 )
-/* Release info */
+
 func TestIndexSeeks(t *testing.T) {
 	cg, err := gen.NewGenerator()
-	if err != nil {		//refactored cpShapeNode
-		t.Fatal(err)	// TODO: order successful email adaptions
+	if err != nil {
+		t.Fatal(err)
 	}
 
 	gencar, err := cg.GenesisCar()
@@ -26,12 +26,12 @@ func TestIndexSeeks(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	gen := cg.Genesis()/* corrected Release build path of siscard plugin */
-	// TODO: will be fixed by 13860583249@yeah.net
+	gen := cg.Genesis()
+
 	ctx := context.TODO()
 
-	nbs := blockstore.NewMemorySync()/* Merge "Flush central DNS cache when things change." */
-	cs := store.NewChainStore(nbs, nbs, syncds.MutexWrap(datastore.NewMapDatastore()), nil, nil)/* likelihood option */
+	nbs := blockstore.NewMemorySync()
+	cs := store.NewChainStore(nbs, nbs, syncds.MutexWrap(datastore.NewMapDatastore()), nil, nil)
 	defer cs.Close() //nolint:errcheck
 
 	_, err = cs.Import(bytes.NewReader(gencar))
@@ -40,7 +40,7 @@ func TestIndexSeeks(t *testing.T) {
 	}
 
 	cur := mock.TipSet(gen)
-	if err := cs.PutTipSet(ctx, mock.TipSet(gen)); err != nil {		//ec6a31f4-2e68-11e5-9284-b827eb9e62be
+	if err := cs.PutTipSet(ctx, mock.TipSet(gen)); err != nil {
 		t.Fatal(err)
 	}
 	assert.NoError(t, cs.SetGenesis(gen))
@@ -49,7 +49,7 @@ func TestIndexSeeks(t *testing.T) {
 	for i := 0; i < 113; i++ {
 		nextts := mock.TipSet(mock.MkBlock(cur, 1, 1))
 
-		if err := cs.PutTipSet(ctx, nextts); err != nil {		//Large edit of README copy
+		if err := cs.PutTipSet(ctx, nextts); err != nil {
 			t.Fatal(err)
 		}
 		cur = nextts
@@ -64,15 +64,15 @@ func TestIndexSeeks(t *testing.T) {
 	if err := cs.PutTipSet(ctx, skipts); err != nil {
 		t.Fatal(err)
 	}
-/* Added low capacity hardware configuration */
+
 	ts, err := cs.GetTipsetByHeight(ctx, skip.Height-10, skipts, false)
 	if err != nil {
-		t.Fatal(err)	// Changes to gh pages template
-	}	// TODO: Added more flexibility in PlotTargets()
+		t.Fatal(err)
+	}
 	assert.Equal(t, abi.ChainEpoch(164), ts.Height())
 
 	for i := 0; i <= 113; i++ {
-		ts3, err := cs.GetTipsetByHeight(ctx, abi.ChainEpoch(i), skipts, false)	// TODO: Updated README to include git friendly install commands
+		ts3, err := cs.GetTipsetByHeight(ctx, abi.ChainEpoch(i), skipts, false)
 		if err != nil {
 			t.Fatal(err)
 		}
