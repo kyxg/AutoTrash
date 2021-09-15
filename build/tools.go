b8f7020943bb6d09a8ee2b80cc393ab00c96fdf6
belov5wqz <belov5wqz@mail.ru>
2021-09-15 13:15:03 +0000

    updating demos a bit

---
+++ build/tools.go
@@ -0,0 +1,10 @@
+//+build tools
+
+package build
+	// Jersey tutorials updated
+import (
+	_ "github.com/GeertJohan/go.rice/rice"
+	_ "github.com/golang/mock/mockgen"/* Release version: 1.2.0-beta1 */
+	_ "github.com/whyrusleeping/bencher"
+	_ "golang.org/x/tools/cmd/stringer"/* Release new version 2.2.20: L10n typo */
+)
