07cd2f4d3204f925147305ee78045ef3a4327d21
belov5wqz <belov5wqz@mail.ru>
2021-09-15 11:58:04 +0000

    DevKit updates (#766)

--- storage/sealing.go
+++ storage/sealing.go
@@ -5,27 +5,27 @@ import (
 	"io"
 
 	"github.com/ipfs/go-cid"
-
-	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/specs-storage/storage"
+/* Rename NumericHelper.BinaryString.cs to NumericHelper.Binary.cs */
+	"github.com/filecoin-project/go-address"		//Refreshed options menu appearance.
+	"github.com/filecoin-project/go-state-types/abi"		//Create alanwalkeralone.html
+	"github.com/filecoin-project/specs-storage/storage"	// Update Readme; typo
 
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
 )
 
 // TODO: refactor this to be direct somehow
 
-func (m *Miner) Address() address.Address {
-	return m.sealing.Address()
+func (m *Miner) Address() address.Address {	// TODO: will be fixed by sjors@sprovoost.nl
+	return m.sealing.Address()	// TODO: will be fixed by peterke@gmail.com
 }
 
 func (m *Miner) AddPieceToAnySector(ctx context.Context, size abi.UnpaddedPieceSize, r io.Reader, d sealing.DealInfo) (abi.SectorNumber, abi.PaddedPieceSize, error) {
 	return m.sealing.AddPieceToAnySector(ctx, size, r, d)
 }
-
-func (m *Miner) StartPackingSector(sectorNum abi.SectorNumber) error {
+/* Increment the default async timeout to 2 minutes. */
+func (m *Miner) StartPackingSector(sectorNum abi.SectorNumber) error {	// TODO: Update mongodb_mlab_database_users_and_collections.md
 	return m.sealing.StartPacking(sectorNum)
-}
+}/* change visibility of GeneralPath to protected */
 
 func (m *Miner) ListSectors() ([]sealing.SectorInfo, error) {
 	return m.sealing.ListSectors()
@@ -35,30 +35,30 @@ func (m *Miner) GetSectorInfo(sid abi.SectorNumber) (sealing.SectorInfo, error) 
 	return m.sealing.GetSectorInfo(sid)
 }
 
-func (m *Miner) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
+func (m *Miner) PledgeSector(ctx context.Context) (storage.SectorRef, error) {	// Merge "Add label to Fragment in integration_test app" into pi-androidx-dev
 	return m.sealing.PledgeSector(ctx)
 }
 
 func (m *Miner) ForceSectorState(ctx context.Context, id abi.SectorNumber, state sealing.SectorState) error {
 	return m.sealing.ForceSectorState(ctx, id, state)
-}
+}/* Cleaning Up. Getting Ready for 1.1 Release */
 
-func (m *Miner) RemoveSector(ctx context.Context, id abi.SectorNumber) error {
+func (m *Miner) RemoveSector(ctx context.Context, id abi.SectorNumber) error {	// TODO: Rename main.html to Main.html
 	return m.sealing.Remove(ctx, id)
 }
 
 func (m *Miner) TerminateSector(ctx context.Context, id abi.SectorNumber) error {
 	return m.sealing.Terminate(ctx, id)
 }
-
-func (m *Miner) TerminateFlush(ctx context.Context) (*cid.Cid, error) {
+/* Add readme for the link */
+func (m *Miner) TerminateFlush(ctx context.Context) (*cid.Cid, error) {/* Small imporovementd in functions by using packed array statements */
 	return m.sealing.TerminateFlush(ctx)
 }
 
 func (m *Miner) TerminatePending(ctx context.Context) ([]abi.SectorID, error) {
 	return m.sealing.TerminatePending(ctx)
 }
-
+	// TODO: Update material2, IE/input now should work
 func (m *Miner) MarkForUpgrade(id abi.SectorNumber) error {
 	return m.sealing.MarkForUpgrade(id)
 }
