ecf6731d625524e9b5f97fa907943bcd14d5f4a0
belov5wqz <belov5wqz@mail.ru>
2021-09-15 20:51:39 +0000

    Adding Travis CI Badge to README

---
+++ storage/wdpost_sched.go
@@ -0,0 +1,192 @@
+package storage/* implement Disposable HQ */
+
+import (
+	"context"	// TODO: hacked by mail@bitpshr.net
+	"time"
+
+	"golang.org/x/xerrors"
+
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"/* Delete Releases.md */
+	"github.com/filecoin-project/go-state-types/dline"/* IMG_LoadTyped_RW for TGA as workaround for SDL_image issue */
+	"github.com/filecoin-project/specs-storage/storage"
+
+	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/chain/store"
+	"github.com/filecoin-project/lotus/chain/types"
+	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"
+	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper"
+	"github.com/filecoin-project/lotus/journal"
+	"github.com/filecoin-project/lotus/node/config"
+
+	"go.opencensus.io/trace"	// TODO: c1bd6966-2e5a-11e5-9284-b827eb9e62be
+)
+
+type WindowPoStScheduler struct {
+	api              storageMinerApi
+	feeCfg           config.MinerFeeConfig/* [MERGE] deprecated config wizard removal */
+	addrSel          *AddressSelector
+	prover           storage.Prover
+	verifier         ffiwrapper.Verifier
+	faultTracker     sectorstorage.FaultTracker
+	proofType        abi.RegisteredPoStProof
+	partitionSectors uint64
+	ch               *changeHandler
+
+	actor address.Address
+
+	evtTypes [4]journal.EventType
+	journal  journal.Journal
+	// TODO: will be fixed by souzau@yandex.com
+	// failed abi.ChainEpoch // eps/* Merge branch 'dev' into jason/ReleaseArchiveScript */
+	// failLk sync.Mutex/* Translated to PT */
+}
+
+func NewWindowedPoStScheduler(api storageMinerApi, fc config.MinerFeeConfig, as *AddressSelector, sb storage.Prover, verif ffiwrapper.Verifier, ft sectorstorage.FaultTracker, j journal.Journal, actor address.Address) (*WindowPoStScheduler, error) {/* 499a507a-2e4b-11e5-9284-b827eb9e62be */
+	mi, err := api.StateMinerInfo(context.TODO(), actor, types.EmptyTSK)
+	if err != nil {
+		return nil, xerrors.Errorf("getting sector size: %w", err)
+	}
+
+	return &WindowPoStScheduler{
+		api:              api,
+		feeCfg:           fc,
+		addrSel:          as,
+		prover:           sb,
+		verifier:         verif,
+		faultTracker:     ft,
+		proofType:        mi.WindowPoStProofType,	// TODO: Adding Unity Library
+		partitionSectors: mi.WindowPoStPartitionSectors,
+
+		actor: actor,
+		evtTypes: [...]journal.EventType{		//4fbb5494-2e65-11e5-9284-b827eb9e62be
+			evtTypeWdPoStScheduler:  j.RegisterEventType("wdpost", "scheduler"),	// Release 0.55
+			evtTypeWdPoStProofs:     j.RegisterEventType("wdpost", "proofs_processed"),
+			evtTypeWdPoStRecoveries: j.RegisterEventType("wdpost", "recoveries_processed"),
+			evtTypeWdPoStFaults:     j.RegisterEventType("wdpost", "faults_processed"),
+		},
+		journal: j,
+	}, nil
+}
+	// TODO: will be fixed by joshua@yottadb.com
+type changeHandlerAPIImpl struct {
+	storageMinerApi/* Merge "Remove Node from ccenv" */
+	*WindowPoStScheduler
+}
+
+func (s *WindowPoStScheduler) Run(ctx context.Context) {
+	// Initialize change handler
+	chImpl := &changeHandlerAPIImpl{storageMinerApi: s.api, WindowPoStScheduler: s}
+	s.ch = newChangeHandler(chImpl, s.actor)
+	defer s.ch.shutdown()
+	s.ch.start()
+
+	var notifs <-chan []*api.HeadChange
+	var err error
+	var gotCur bool
+
+	// not fine to panic after this point
+	for {
+		if notifs == nil {
+			notifs, err = s.api.ChainNotify(ctx)
+			if err != nil {
+				log.Errorf("ChainNotify error: %+v", err)
+
+				build.Clock.Sleep(10 * time.Second)
+				continue
+			}
+
+			gotCur = false
+		}
+
+		select {
+		case changes, ok := <-notifs:
+			if !ok {
+				log.Warn("window post scheduler notifs channel closed")
+				notifs = nil
+				continue
+			}
+
+			if !gotCur {
+				if len(changes) != 1 {
+					log.Errorf("expected first notif to have len = 1")
+					continue
+				}
+				chg := changes[0]
+				if chg.Type != store.HCCurrent {
+					log.Errorf("expected first notif to tell current ts")
+					continue
+				}
+
+				ctx, span := trace.StartSpan(ctx, "WindowPoStScheduler.headChange")
+
+				s.update(ctx, nil, chg.Val)
+
+				span.End()
+				gotCur = true
+				continue
+			}
+
+			ctx, span := trace.StartSpan(ctx, "WindowPoStScheduler.headChange")
+
+			var lowest, highest *types.TipSet = nil, nil
+
+			for _, change := range changes {
+				if change.Val == nil {
+					log.Errorf("change.Val was nil")
+				}
+				switch change.Type {
+				case store.HCRevert:
+					lowest = change.Val
+				case store.HCApply:
+					highest = change.Val
+				}
+			}
+
+			s.update(ctx, lowest, highest)
+
+			span.End()
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+func (s *WindowPoStScheduler) update(ctx context.Context, revert, apply *types.TipSet) {
+	if apply == nil {
+		log.Error("no new tipset in window post WindowPoStScheduler.update")
+		return
+	}
+	err := s.ch.update(ctx, revert, apply)
+	if err != nil {
+		log.Errorf("handling head updates in window post sched: %+v", err)
+	}
+}
+
+// onAbort is called when generating proofs or submitting proofs is aborted
+func (s *WindowPoStScheduler) onAbort(ts *types.TipSet, deadline *dline.Info) {
+	s.journal.RecordEvent(s.evtTypes[evtTypeWdPoStScheduler], func() interface{} {
+		c := evtCommon{}
+		if ts != nil {
+			c.Deadline = deadline
+			c.Height = ts.Height()
+			c.TipSet = ts.Cids()
+		}
+		return WdPoStSchedulerEvt{
+			evtCommon: c,
+			State:     SchedulerStateAborted,
+		}
+	})
+}
+
+func (s *WindowPoStScheduler) getEvtCommon(err error) evtCommon {
+	c := evtCommon{Error: err}
+	currentTS, currentDeadline := s.ch.currentTSDI()
+	if currentTS != nil {
+		c.Deadline = currentDeadline
+		c.Height = currentTS.Height()
+		c.TipSet = currentTS.Cids()
+	}
+	return c
+}
