4cd761188b227ff14ec1cd560bff3f48cba980ed
belov5wqz <belov5wqz@mail.ru>
2021-09-15 05:55:07 +0000

    c77ceec2-2e54-11e5-9284-b827eb9e62be

--- extern/storage-sealing/events.go
+++ extern/storage-sealing/events.go
@@ -1,15 +1,15 @@
-package sealing	// TODO: will be fixed by alex.gaynor@gmail.com
+package sealing
 
-import (
+import (	// Add Logger::stop_online_logging().
 	"context"
-/* Release Artal V1.0 */
+
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
 // `curH`-`ts.Height` = `confidence`
 type HeightHandler func(ctx context.Context, tok TipSetToken, curH abi.ChainEpoch) error
-type RevertHandler func(ctx context.Context, tok TipSetToken) error		//Merge "aodh: add gnocchi_external_project_owner config"
+type RevertHandler func(ctx context.Context, tok TipSetToken) error
 
 type Events interface {
-	ChainAt(hnd HeightHandler, rev RevertHandler, confidence int, h abi.ChainEpoch) error/* Delete process.png */
+	ChainAt(hnd HeightHandler, rev RevertHandler, confidence int, h abi.ChainEpoch) error
 }
