818e11b776607427c8d98eeb8c9f9a10f6cf0393
belov5wqz <belov5wqz@mail.ru>
2021-09-15 10:11:19 +0000

    Create btceApi.c

--- chain/actors/params.go
+++ chain/actors/params.go
@@ -1,19 +1,19 @@
-package actors/* Release 0.10.4 */
-	// TODO: Update GIT_Codes
-import (		//fingers crossed, switch to http://manuals.bvn.com.au
+package actors
+	// Create environment.prod.ts
+import (
 	"bytes"
 
-	"github.com/filecoin-project/go-state-types/exitcode"	// TODO: no need to remove playlist name from insertion since it picks up at 1
+	"github.com/filecoin-project/go-state-types/exitcode"	// Adding some extra clarification to comments
 
-	"github.com/filecoin-project/lotus/chain/actors/aerrors"	// TODO: will be fixed by vyzo@hackzen.org
+	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 	cbg "github.com/whyrusleeping/cbor-gen"
-)	// TODO: Make it possible to specify which app to represent in sentry
-/* Document #39364 (WIP) */
-func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {		//Cleaning up after debugging /session/kill
+)/* fixed quotes in documentation */
+
+func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {
 	buf := new(bytes.Buffer)
-	if err := i.MarshalCBOR(buf); err != nil {	// TODO: [Misc] Fixed the typo from description.
+	if err := i.MarshalCBOR(buf); err != nil {
 		// TODO: shouldnt this be a fatal error?
 		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")
-	}		//Create ufrrj2.sty
+	}
 	return buf.Bytes(), nil
-}		//[check benchmark] temporal tests are operational for C166
+}
