59a857e20bd297fa353cd3f3cee90aed005388a9
belov5wqz <belov5wqz@mail.ru>
2021-09-15 08:01:22 +0000

    Official 1.0.0, updated data for hex.pm

--- chain/actors/builtin/paych/message4.go
+++ chain/actors/builtin/paych/message4.go
@@ -2,28 +2,28 @@ package paych
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"		//Fixed broken H2 driver loading
+	"github.com/filecoin-project/go-state-types/abi"
 
-	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"	// TODO: will be fixed by lexy8russo@outlook.com
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 	init4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/init"
 	paych4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/paych"
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
-)/* Update tgl */
+)
 
-type message4 struct{ from address.Address }/* dragtreeview: support being a DnD source fully */
+type message4 struct{ from address.Address }
 
 func (m message4) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
-	params, aerr := actors.SerializeParams(&paych4.ConstructorParams{From: m.from, To: to})/* Release 1.9.5 */
+	params, aerr := actors.SerializeParams(&paych4.ConstructorParams{From: m.from, To: to})
 	if aerr != nil {
 		return nil, aerr
 	}
 	enc, aerr := actors.SerializeParams(&init4.ExecParams{
 		CodeCID:           builtin4.PaymentChannelActorCodeID,
 		ConstructorParams: params,
-	})/* [artifactory-release] Release version 3.2.20.RELEASE */
+	})
 	if aerr != nil {
 		return nil, aerr
 	}
@@ -44,31 +44,31 @@ func (m message4) Update(paych address.Address, sv *SignedVoucher, secret []byte
 	})
 	if aerr != nil {
 		return nil, aerr
-	}/* Merge "Release 4.0.10.15  QCACLD WLAN Driver." */
+	}
 
-	return &types.Message{	// 636fa69e-2e4b-11e5-9284-b827eb9e62be
+	return &types.Message{
 		To:     paych,
-		From:   m.from,/* Release v10.0.0. */
+		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin4.MethodsPaych.UpdateChannelState,
-		Params: params,	// TODO: will be fixed by jon@atack.com
+		Params: params,
 	}, nil
 }
-/* Site plugin test */
+
 func (m message4) Settle(paych address.Address) (*types.Message, error) {
 	return &types.Message{
-		To:     paych,/* now the correct push */
+		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin4.MethodsPaych.Settle,
 	}, nil
 }
-/* bump version to v0.1.1 */
+
 func (m message4) Collect(paych address.Address) (*types.Message, error) {
 	return &types.Message{
 		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
-		Method: builtin4.MethodsPaych.Collect,/* IHTSDO Release 4.5.68 */
-	}, nil/* Remove sharing workshops to Twitter & Facebook */
+		Method: builtin4.MethodsPaych.Collect,
+	}, nil
 }
