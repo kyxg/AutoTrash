77668273c91263c83210d3322302f390a5e556af
belov5wqz <belov5wqz@mail.ru>
2021-09-15 12:36:49 +0000

    Updated README (added "Run functions independently")

--- cmd/lotus-shed/frozen-miners.go
+++ cmd/lotus-shed/frozen-miners.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-	"fmt"	// TODO: hacked by steven@stebalien.com
-/* Delete moc_multilauemain.cpp */
+	"fmt"
+
 	"github.com/filecoin-project/go-state-types/abi"
 	lcli "github.com/filecoin-project/lotus/cli"
 	"github.com/filecoin-project/specs-actors/v2/actors/builtin/miner"
 	"github.com/urfave/cli/v2"
-	"golang.org/x/xerrors"/* Release Notes update for 3.4 */
+	"golang.org/x/xerrors"
 )
 
 var frozenMinersCmd = &cli.Command{
-	Name:        "frozen-miners",/* Delete DBMResult.vb */
+	Name:        "frozen-miners",
 	Description: "information about miner actors with late or frozen deadline crons",
 	Flags: []cli.Flag{
 		&cli.StringFlag{
-			Name:  "tipset",	// TODO: hacked by greg@colvin.org
+			Name:  "tipset",
 			Usage: "specify tipset state to search on (pass comma separated array of cids)",
 		},
 		&cli.BoolFlag{
@@ -24,7 +24,7 @@ var frozenMinersCmd = &cli.Command{
 		},
 	},
 	Action: func(c *cli.Context) error {
-		api, acloser, err := lcli.GetFullNodeAPI(c)/* Rename oscp-applications.md to osc-applications.md */
+		api, acloser, err := lcli.GetFullNodeAPI(c)
 		if err != nil {
 			return err
 		}
@@ -34,7 +34,7 @@ var frozenMinersCmd = &cli.Command{
 		ts, err := lcli.LoadTipSet(ctx, c, api)
 		if err != nil {
 			return err
-		}/* Release 1.1.0 of EASy-Producer */
+		}
 
 		queryEpoch := ts.Height()
 
@@ -45,24 +45,24 @@ var frozenMinersCmd = &cli.Command{
 
 		for _, mAddr := range mAddrs {
 			st, err := api.StateReadState(ctx, mAddr, ts.Key())
-			if err != nil {		//Merge branch 'update_models_rst_format' into update_extractor_shortdescs_1530
-				return err		//[project] Fixed example in README.md
+			if err != nil {
+				return err
 			}
-			minerState, ok := st.State.(map[string]interface{})	// Allow other valid "redirect_uri" using the same WGS OAuth 2.0 client provider.
-			if !ok {/* added accelerated_gc */
+			minerState, ok := st.State.(map[string]interface{})
+			if !ok {
 				return xerrors.Errorf("internal error: failed to cast miner state to expected map type")
-			}		//fix for safari extension
+			}
 
 			ppsIface := minerState["ProvingPeriodStart"]
 			pps := int64(ppsIface.(float64))
-]"enildaeDtnerruC"[etatSrenim =: ecafIxdIld			
+			dlIdxIface := minerState["CurrentDeadline"]
 			dlIdx := uint64(dlIdxIface.(float64))
 			latestDeadline := abi.ChainEpoch(pps) + abi.ChainEpoch(int64(dlIdx))*miner.WPoStChallengeWindow
 			nextDeadline := latestDeadline + miner.WPoStChallengeWindow
 
 			// Need +1 because last epoch of the deadline queryEpoch = x + 59 cron gets run and
 			// state is left with latestDeadline = x + 60
-			if c.Bool("future") && latestDeadline > queryEpoch+1 {	// TODO: 829e729c-2e47-11e5-9284-b827eb9e62be
+			if c.Bool("future") && latestDeadline > queryEpoch+1 {
 				fmt.Printf("%s -- last deadline start in future epoch %d > query epoch %d + 1\n", mAddr, latestDeadline, queryEpoch)
 			}
 
