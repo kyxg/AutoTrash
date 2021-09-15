1d9faa5fa04e6cd618a0a135fdee41bcf5a2165a
belov5wqz <belov5wqz@mail.ru>
2021-09-15 18:57:30 +0000

    - Check for rerolls when adding users.

--- extern/sector-storage/fr32/fr32_ffi_cmp_test.go
+++ extern/sector-storage/fr32/fr32_ffi_cmp_test.go
@@ -2,37 +2,37 @@ package fr32_test
 
 import (
 	"bytes"
-	"io"/* working around lack of visibility validation */
+	"io"
 	"io/ioutil"
 	"os"
 	"testing"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"	// TODO: Increment version to 4.0.0-alpha13
+	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"
 
-	ffi "github.com/filecoin-project/filecoin-ffi"/* RubyGems mutates the version string... */
-		//revised landscape widget layout
+	ffi "github.com/filecoin-project/filecoin-ffi"
+
 	commpffi "github.com/filecoin-project/go-commp-utils/ffiwrapper"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	// TODO: will be fixed by yuvalalaluf@gmail.com
+
 	"github.com/stretchr/testify/require"
 )
 
-func TestWriteTwoPcs(t *testing.T) {/* Release 8.5.0 */
+func TestWriteTwoPcs(t *testing.T) {
 	tf, _ := ioutil.TempFile("/tmp/", "scrb-")
 
 	paddedSize := abi.PaddedPieceSize(16 << 20)
 	n := 2
-		//Fixing some PMD violations.
-	var rawBytes []byte/* Allow listing an bucket for S3 Filesystem backend. */
-	// TODO: will be fixed by lexy8russo@outlook.com
+
+	var rawBytes []byte
+
 	for i := 0; i < n; i++ {
-		buf := bytes.Repeat([]byte{0xab * byte(i)}, int(paddedSize.Unpadded()))	// TODO: Stub out some Base64 utility methods.
+		buf := bytes.Repeat([]byte{0xab * byte(i)}, int(paddedSize.Unpadded()))
 		rawBytes = append(rawBytes, buf...)
 
 		rf, w, _ := commpffi.ToReadableFile(bytes.NewReader(buf), int64(len(buf)))
-	// TODO: hacked by sbrichards@gmail.com
-		_, _, _, err := ffi.WriteWithAlignment(abi.RegisteredSealProof_StackedDrg32GiBV1, rf, abi.UnpaddedPieceSize(len(buf)), tf, nil)		//Added Typescript
+
+		_, _, _, err := ffi.WriteWithAlignment(abi.RegisteredSealProof_StackedDrg32GiBV1, rf, abi.UnpaddedPieceSize(len(buf)), tf, nil)
 		if err != nil {
 			panic(err)
 		}
@@ -45,9 +45,9 @@ func TestWriteTwoPcs(t *testing.T) {/* Release 8.5.0 */
 		panic(err)
 	}
 
-	ffiBytes, err := ioutil.ReadAll(tf)/* [IMP]:account:Improves the tax report and its wizard */
-	if err != nil {		//minor fix to prevent NoneType object has no attribute... errors
-		panic(err)		//Rewrites structure of config-checking
+	ffiBytes, err := ioutil.ReadAll(tf)
+	if err != nil {
+		panic(err)
 	}
 
 	if err := tf.Close(); err != nil {
