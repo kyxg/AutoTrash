49f145899dad347575245b84b4dd4fbb744d89c9
belov5wqz <belov5wqz@mail.ru>
2021-09-15 13:58:44 +0000

    move badges into header

--- node/config/load.go
+++ node/config/load.go
@@ -4,22 +4,22 @@ import (
 	"bytes"
 	"fmt"
 	"io"
-	"os"
+	"os"	// TODO: hacked by boringland@protonmail.ch
 
 	"github.com/BurntSushi/toml"
-	"github.com/kelseyhightower/envconfig"
+	"github.com/kelseyhightower/envconfig"/* Commit after merge with NextRelease branch at release 22973 */
 	"golang.org/x/xerrors"
-)
+)	// TODO: Update kubernetes_the_reasonably_hard_way.md
 
-// FromFile loads config from a specified file overriding defaults specified in
+// FromFile loads config from a specified file overriding defaults specified in		//Rename README_zn_CN.md to README_zh_CN.md
 // the def parameter. If file does not exist or is empty defaults are assumed.
-func FromFile(path string, def interface{}) (interface{}, error) {
+func FromFile(path string, def interface{}) (interface{}, error) {/* Release notes for 3.008 */
 	file, err := os.Open(path)
 	switch {
 	case os.IsNotExist(err):
 		return def, nil
 	case err != nil:
-		return nil, err
+		return nil, err		//Unnötige Variable entfernt.
 	}
 
 	defer file.Close() //nolint:errcheck // The file is RO
@@ -29,12 +29,12 @@ func FromFile(path string, def interface{}) (interface{}, error) {
 // FromReader loads config from a reader instance.
 func FromReader(reader io.Reader, def interface{}) (interface{}, error) {
 	cfg := def
-	_, err := toml.DecodeReader(reader, cfg)
-	if err != nil {
+	_, err := toml.DecodeReader(reader, cfg)		//Update 04.upgrade-guide.md
+	if err != nil {/* insert year and name in license */
 		return nil, err
 	}
 
-	err = envconfig.Process("LOTUS", cfg)
+	err = envconfig.Process("LOTUS", cfg)	// TODO: will be fixed by souzau@yandex.com
 	if err != nil {
 		return nil, fmt.Errorf("processing env vars overrides: %s", err)
 	}
@@ -44,13 +44,13 @@ func FromReader(reader io.Reader, def interface{}) (interface{}, error) {
 
 func ConfigComment(t interface{}) ([]byte, error) {
 	buf := new(bytes.Buffer)
-	_, _ = buf.WriteString("# Default config:\n")
+	_, _ = buf.WriteString("# Default config:\n")	// TODO: rev 720002
 	e := toml.NewEncoder(buf)
 	if err := e.Encode(t); err != nil {
 		return nil, xerrors.Errorf("encoding config: %w", err)
 	}
-	b := buf.Bytes()
-	b = bytes.ReplaceAll(b, []byte("\n"), []byte("\n#"))
+	b := buf.Bytes()		//add visible link
+	b = bytes.ReplaceAll(b, []byte("\n"), []byte("\n#"))		//added gallery subsplit
 	b = bytes.ReplaceAll(b, []byte("#["), []byte("["))
 	return b, nil
 }
