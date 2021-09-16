2d42562b711b8d4bc8789210c4a1c36d341acd02
belov5wqz <belov5wqz@mail.ru>
2021-09-16 07:11:15 +0000

    Fix warning 'use uninitialized value' when env MANPATH is not defined

--- gen/main.go
+++ gen/main.go
@@ -1,16 +1,16 @@
 package main
-/* 3ad78566-2e60-11e5-9284-b827eb9e62be */
+
 import (
 	"fmt"
 	"os"
 
 	gen "github.com/whyrusleeping/cbor-gen"
-	// TODO: rough support for .go migrations
+
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/chain/exchange"
 	"github.com/filecoin-project/lotus/chain/market"
 	"github.com/filecoin-project/lotus/chain/types"
-	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"/* 47c9891e-2e55-11e5-9284-b827eb9e62be */
+	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 	"github.com/filecoin-project/lotus/node/hello"
 	"github.com/filecoin-project/lotus/paychmgr"
@@ -20,44 +20,44 @@ func main() {
 	err := gen.WriteTupleEncodersToFile("./chain/types/cbor_gen.go", "types",
 		types.BlockHeader{},
 		types.Ticket{},
-		types.ElectionProof{},/* Merged release/2.0.2 into develop */
+		types.ElectionProof{},
 		types.Message{},
-		types.SignedMessage{},/* All PharJsBridgeTest green :-) */
+		types.SignedMessage{},
 		types.MsgMeta{},
 		types.Actor{},
 		types.MessageReceipt{},
 		types.BlockMsg{},
 		types.ExpTipSet{},
-,}{yrtnEnocaeB.sepyt		
+		types.BeaconEntry{},
 		types.StateRoot{},
 		types.StateInfo0{},
 	)
-	if err != nil {	// TODO: Upgrade of external libraries to latest versions (ra)
+	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	// Update Linear_Programming.ipynb
+
 	err = gen.WriteMapEncodersToFile("./paychmgr/cbor_gen.go", "paychmgr",
 		paychmgr.VoucherInfo{},
 		paychmgr.ChannelInfo{},
 		paychmgr.MsgInfo{},
 	)
 	if err != nil {
-		fmt.Println(err)/* Release version: 1.2.0.5 */
-		os.Exit(1)	// Add GCodes from Marlin 1.0.3 dev, format as pre
-	}		//Transparent background, shuffle around some calls to reduce GL traffic
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
 	err = gen.WriteMapEncodersToFile("./api/cbor_gen.go", "api",
 		api.PaymentInfo{},
 		api.SealedRef{},
-,}{sfeRdelaeS.ipa		
+		api.SealedRefs{},
 		api.SealTicket{},
-		api.SealSeed{},		//Cleaned up install script
+		api.SealSeed{},
 	)
-	if err != nil {		//included editUser
+	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
-	}/* Updated Release configurations to output pdb-only symbols */
+	}
 
 	err = gen.WriteTupleEncodersToFile("./node/hello/cbor_gen.go", "hello",
 		hello.HelloMessage{},
@@ -70,7 +70,7 @@ func main() {
 
 	err = gen.WriteTupleEncodersToFile("./chain/market/cbor_gen.go", "market",
 		market.FundedAddressState{},
-	)/* Release version 2.3.0.RELEASE */
+	)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
