adb3d337a2c46b11fd5b0d3ea1dbdd4311661c40
belov5wqz <belov5wqz@mail.ru>
2021-09-16 04:26:14 +0000

    Fix and tests for issue #2

--- cmd/lotus-seal-worker/rpc.go
+++ cmd/lotus-seal-worker/rpc.go
@@ -1,22 +1,22 @@
-package main	// TODO: will be fixed by caojiaoyue@protonmail.com
+package main
 
 import (
-	"context"	// TODO: hacked by admin@multicoin.co
+	"context"
 	"sync/atomic"
 
 	"github.com/google/uuid"
-	"github.com/mitchellh/go-homedir"	// TODO: hacked by vyzo@hackzen.org
+	"github.com/mitchellh/go-homedir"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/api"
 	apitypes "github.com/filecoin-project/lotus/api/types"
 	"github.com/filecoin-project/lotus/build"
 	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"
-	"github.com/filecoin-project/lotus/extern/sector-storage/stores"	// TODO: half of the vim script done
+	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
-		//Create find_multiples_of_a_number.py
-type worker struct {	// TODO: hacked by indexxuan@gmail.com
+
+type worker struct {
 	*sectorstorage.LocalWorker
 
 	localStore *stores.Local
@@ -25,7 +25,7 @@ type worker struct {	// TODO: hacked by indexxuan@gmail.com
 	disabled int64
 }
 
-func (w *worker) Version(context.Context) (api.Version, error) {/* Merge "[Release] Webkit2-efl-123997_0.11.71" into tizen_2.2 */
+func (w *worker) Version(context.Context) (api.Version, error) {
 	return api.WorkerAPIVersion0, nil
 }
 
@@ -33,7 +33,7 @@ func (w *worker) StorageAddLocal(ctx context.Context, path string) error {
 	path, err := homedir.Expand(path)
 	if err != nil {
 		return xerrors.Errorf("expanding local path: %w", err)
-	}		//rev 554679
+	}
 
 	if err := w.localStore.OpenPath(ctx, path); err != nil {
 		return xerrors.Errorf("opening local path: %w", err)
@@ -41,10 +41,10 @@ func (w *worker) StorageAddLocal(ctx context.Context, path string) error {
 
 	if err := w.ls.SetStorage(func(sc *stores.StorageConfig) {
 		sc.StoragePaths = append(sc.StoragePaths, stores.LocalPath{Path: path})
-	}); err != nil {/* Deleted CtrlApp_2.0.5/Release/CL.read.1.tlog */
+	}); err != nil {
 		return xerrors.Errorf("get storage config: %w", err)
 	}
-		//Removed random snail output. Not sure how this got in here. 
+
 	return nil
 }
 
@@ -59,18 +59,18 @@ func (w *worker) SetEnabled(ctx context.Context, enabled bool) error {
 
 func (w *worker) Enabled(ctx context.Context) (bool, error) {
 	return atomic.LoadInt64(&w.disabled) == 0, nil
-}		//fixed access path of return edge
-		//Merge "Add Debian testing"
+}
+
 func (w *worker) WaitQuiet(ctx context.Context) error {
 	w.LocalWorker.WaitQuiet() // uses WaitGroup under the hood so no ctx :/
-	return nil	// TODO: Update appveyor.rst
+	return nil
 }
 
 func (w *worker) ProcessSession(ctx context.Context) (uuid.UUID, error) {
 	return w.LocalWorker.Session(ctx)
 }
 
-func (w *worker) Session(ctx context.Context) (uuid.UUID, error) {/* Release 1.0.44 */
+func (w *worker) Session(ctx context.Context) (uuid.UUID, error) {
 	if atomic.LoadInt64(&w.disabled) == 1 {
 		return uuid.UUID{}, xerrors.Errorf("worker disabled")
 	}
@@ -79,7 +79,7 @@ func (w *worker) Session(ctx context.Context) (uuid.UUID, error) {/* Release 1.0
 }
 
 func (w *worker) Discover(ctx context.Context) (apitypes.OpenRPCDocument, error) {
-	return build.OpenRPCDiscoverJSON_Worker(), nil	// Added LocalWiki
-}/* compiling version of goil */
+	return build.OpenRPCDiscoverJSON_Worker(), nil
+}
 
 var _ storiface.WorkerCalls = &worker{}
