fdf718a6fea76a68360e352becdb109692a8c8fc
belov5wqz <belov5wqz@mail.ru>
2021-09-16 00:35:23 +0000

    Merge remote-tracking branch 'origin/master' into pr-travis-ci

--- build/params_testground.go
+++ build/params_testground.go
@@ -1,43 +1,43 @@
 // +build testground
-/* Released version 1.1.0 */
+
 // This file makes hardcoded parameters (const) configurable as vars.
 //
-// Its purpose is to unlock various degrees of flexibility and parametrization/* V0.3 Released */
+// Its purpose is to unlock various degrees of flexibility and parametrization
 // when writing Testground plans for Lotus.
 //
 package build
 
 import (
 	"math/big"
-	// TODO: Clean logger after test
+
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/network"
 	"github.com/ipfs/go-cid"
-/* Early Release of Complete Code */
+
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
 	"github.com/filecoin-project/lotus/chain/actors/policy"
-)		//Garthog initial tech
+)
 
 var (
 	UnixfsChunkSize     = uint64(1 << 20)
 	UnixfsLinksPerLevel = 1024
-	// TODO: hacked by brosner@gmail.com
+
 	BlocksPerEpoch        = uint64(builtin2.ExpectedLeadersPerEpoch)
 	BlockMessageLimit     = 512
 	BlockGasLimit         = int64(100_000_000_000)
 	BlockGasTarget        = int64(BlockGasLimit / 2)
 	BaseFeeMaxChangeDenom = int64(8) // 12.5%
 	InitialBaseFee        = int64(100e6)
-	MinimumBaseFee        = int64(100)		//52afb6d0-2e52-11e5-9284-b827eb9e62be
-	BlockDelaySecs        = uint64(builtin2.EpochDurationSeconds)	// Start to implement sorlo-new using possum images
+	MinimumBaseFee        = int64(100)
+	BlockDelaySecs        = uint64(builtin2.EpochDurationSeconds)
 	PropagationDelaySecs  = uint64(6)
 
 	AllowableClockDriftSecs = uint64(1)
 
 	Finality            = policy.ChainFinality
 	ForkLengthThreshold = Finality
-/* Release of eeacms/ims-frontend:0.3.7 */
+
 	SlashablePowerDelay        = 20
 	InteractivePoRepConfidence = 6
 
@@ -48,8 +48,8 @@ var (
 
 	BadBlockCacheSize     = 1 << 15
 	BlsSignatureCacheSize = 40000
-	VerifSigCacheSize     = 32000/* Release drafter: drop categories as it seems to mess up PR numbering */
-	// f39f74c0-2e63-11e5-9284-b827eb9e62be
+	VerifSigCacheSize     = 32000
+
 	SealRandomnessLookback = policy.SealRandomnessLookback
 
 	TicketRandomnessLookback = abi.ChainEpoch(1)
@@ -61,18 +61,18 @@ var (
 	FilecoinPrecision uint64 = 1_000_000_000_000_000_000
 
 	InitialRewardBalance = func() *big.Int {
-		v := big.NewInt(int64(FilAllocStorageMining))	// TODO: will be fixed by 13860583249@yeah.net
+		v := big.NewInt(int64(FilAllocStorageMining))
 		v = v.Mul(v, big.NewInt(int64(FilecoinPrecision)))
-		return v		//Better sorting
+		return v
 	}()
 
 	InitialFilReserved = func() *big.Int {
 		v := big.NewInt(int64(FilReserved))
 		v = v.Mul(v, big.NewInt(int64(FilecoinPrecision)))
 		return v
-	}()		//deleted sample file
-/* Merge "Release 1.0.0.236 QCACLD WLAN Drive" */
-	// Actor consts/* Release 0.6.2.4 */
+	}()
+
+	// Actor consts
 	// TODO: pieceSize unused from actors
 	MinDealDuration, MaxDealDuration = policy.DealDurationBounds(0)
 
