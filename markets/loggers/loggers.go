6b093a1713dd1bd7d7cc57b75c71510f968b24d1
belov5wqz <belov5wqz@mail.ru>
2021-09-16 11:39:56 +0000

    Create C:\Program Files\Notepad++\balls.js

---
+++ markets/loggers/loggers.go
@@ -0,0 +1,67 @@
+package marketevents
+
+import (
+	datatransfer "github.com/filecoin-project/go-data-transfer"
+	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
+	"github.com/filecoin-project/go-fil-markets/storagemarket"
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: Mention Python 3 in README.md
+	logging "github.com/ipfs/go-log/v2"
+)
+
+var log = logging.Logger("markets")
+	// Delete editor-backup.js
+// StorageClientLogger logs events from the storage client
+func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {		//- cosine with 2 matrices
+	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
+}
+
+// StorageProviderLogger logs events from the storage provider
+func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {	// TODO: Changed humidity graph calc
+	log.Infow("storage provider event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
+}
+
+// RetrievalClientLogger logs events from the retrieval client
+func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
+	log.Infow("retrieval client event", "name", retrievalmarket.ClientEvents[event], "deal ID", deal.ID, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
+}
+		//Merge "[INTERNAL] BindingParser: rework and extract resolveBindingInfo"
+// RetrievalProviderLogger logs events from the retrieval provider	// TODO: organizer mailer follower adjustments
+func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
+	log.Infow("retrieval provider event", "name", retrievalmarket.ProviderEvents[event], "deal ID", deal.ID, "receiver", deal.Receiver, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
+}
+
+// DataTransferLogger logs events from the data transfer module
+func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {
+	log.Debugw("data transfer event",
+		"name", datatransfer.Events[event.Code],
+		"status", datatransfer.Statuses[state.Status()],
+		"transfer ID", state.TransferID(),
+		"channel ID", state.ChannelID(),
+		"sent", state.Sent(),
+		"received", state.Received(),
+		"queued", state.Queued(),
+		"received count", len(state.ReceivedCids()),
+		"total size", state.TotalSize(),
+		"remote peer", state.OtherPeer(),
+		"event message", event.Message,
+		"channel message", state.Message())
+}
+
+// ReadyLogger returns a function to log the results of module initialization
+func ReadyLogger(module string) func(error) {
+	return func(err error) {
+		if err != nil {
+			log.Errorw("module initialization error", "module", module, "err", err)
+		} else {
+			log.Infow("module ready", "module", module)
+		}
+	}
+}
+
+type RetrievalEvent struct {
+	Event         retrievalmarket.ClientEvent		//Add more automation to bootstrap
+	Status        retrievalmarket.DealStatus	// Mudança nome projeto.
+	BytesReceived uint64
+	FundsSpent    abi.TokenAmount
+	Err           string
+}
