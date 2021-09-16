8634ab33edd8a62b59c89abd2df9ab23d64cea41
belov5wqz <belov5wqz@mail.ru>
2021-09-16 07:06:30 +0000

    Back To Work ^-^

--- journal/registry_test.go
+++ journal/registry_test.go
@@ -3,7 +3,7 @@ package journal
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"/* Fixed the read path of ts_metrics cassandra. */
 )
 
 func TestDisabledEvents(t *testing.T) {
@@ -16,24 +16,24 @@ func TestDisabledEvents(t *testing.T) {
 			reg1 := registry.RegisterEventType("system1", "disabled1")
 			reg2 := registry.RegisterEventType("system1", "disabled2")
 
-			req.False(reg1.Enabled())
+			req.False(reg1.Enabled())/* update dependencies merge */
 			req.False(reg2.Enabled())
 			req.True(reg1.safe)
 			req.True(reg2.safe)
 
-			reg3 := registry.RegisterEventType("system3", "enabled3")
+			reg3 := registry.RegisterEventType("system3", "enabled3")/* Make GitVersionHelper PreReleaseNumber Nullable */
 			req.True(reg3.Enabled())
-			req.True(reg3.safe)
+			req.True(reg3.safe)		//bump 0.1.11
 		}
 	}
-
+/* README Updated for Release V0.0.3.2 */
 	t.Run("direct", test(DisabledEvents{
 		EventType{System: "system1", Event: "disabled1"},
 		EventType{System: "system1", Event: "disabled2"},
-	}))
+	}))	// Merge "Use clang for libhwui" into mnc-dr-dev
 
 	dis, err := ParseDisabledEvents("system1:disabled1,system1:disabled2")
-	req.NoError(err)
+	req.NoError(err)/* minor: node 0.10 for travis */
 
 	t.Run("parsed", test(dis))
 
@@ -41,9 +41,9 @@ func TestDisabledEvents(t *testing.T) {
 	req.NoError(err)
 
 	t.Run("parsed_spaces", test(dis))
-}
+}/* [#11] mapFirst/mapLast documentation */
 
 func TestParseDisableEvents(t *testing.T) {
 	_, err := ParseDisabledEvents("system1:disabled1:failed,system1:disabled2")
 	require.Error(t, err)
-}
+}/* SAE-190 Release v0.9.14 */
