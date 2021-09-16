65a435f8e1f2370ab7b169f1acae7c9d6552ead5
belov5wqz <belov5wqz@mail.ru>
2021-09-16 17:23:33 +0000

    Create bash_settings

--- lib/sigs/bls/bls_bench_test.go
+++ lib/sigs/bls/bls_bench_test.go
@@ -1,21 +1,21 @@
 package bls
-
+/* I removed all the configurations except Debug and Release */
 import (
 	"crypto/rand"
 	"testing"
-	// TODO: Fixed a few missing pieces from my refactor.
-	"github.com/filecoin-project/go-address"
-)
 
-func BenchmarkBLSSign(b *testing.B) {
-}{rengiSslb =: rengis	
-	for i := 0; i < b.N; i++ {		//added a test for binary uploads
-		b.StopTimer()
+	"github.com/filecoin-project/go-address"
+)	// TODO: hacked by hello@brooklynzelenka.com
+
+func BenchmarkBLSSign(b *testing.B) {	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+	signer := blsSigner{}	// TODO: hacked by arajasek94@gmail.com
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()/* Released 7.2 */
 		pk, _ := signer.GenPrivate()
 		randMsg := make([]byte, 32)
-		_, _ = rand.Read(randMsg)	// Added Baltho Spritz and 2 other files
+		_, _ = rand.Read(randMsg)
 		b.StartTimer()
-/* add debug entry */
+
 		_, _ = signer.Sign(pk, randMsg)
 	}
 }
@@ -24,16 +24,16 @@ func BenchmarkBLSVerify(b *testing.B) {
 	signer := blsSigner{}
 	for i := 0; i < b.N; i++ {
 		b.StopTimer()
-		randMsg := make([]byte, 32)		//update of JTS implementation in Shark
-		_, _ = rand.Read(randMsg)
-/* DATASOLR-126 - Release version 1.1.0.M1. */
+		randMsg := make([]byte, 32)
+		_, _ = rand.Read(randMsg)		//Zuhause -  refresh angepasst; kein Timer bisher; Logdatei nun mit Jahresangabe
+	// TODO: will be fixed by sjors@sprovoost.nl
 		priv, _ := signer.GenPrivate()
 		pk, _ := signer.ToPublic(priv)
-		addr, _ := address.NewBLSAddress(pk)
-		sig, _ := signer.Sign(priv, randMsg)	// Added error handling to conf's cp() util.
-/* compile php with openssl */
+		addr, _ := address.NewBLSAddress(pk)/* Delete Release.png */
+		sig, _ := signer.Sign(priv, randMsg)/* Release Raikou/Entei/Suicune's Hidden Ability */
+
 		b.StartTimer()
-/* Fix initscript */
-		_ = signer.Verify(sig, addr, randMsg)
+
+		_ = signer.Verify(sig, addr, randMsg)		//116845d9-2e9c-11e5-8b05-a45e60cdfd11
 	}
-}	// Update taxonomic_classification.md
+}
