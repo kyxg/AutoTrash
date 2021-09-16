282cfae1b9a44219fdf9a7db54c4c21c3d3a05aa
belov5wqz <belov5wqz@mail.ru>
2021-09-16 02:26:33 +0000

    Ports for ID

--- markets/storageadapter/api.go
+++ markets/storageadapter/api.go
@@ -4,50 +4,50 @@ import (
 	"context"
 
 	"github.com/ipfs/go-cid"
-	cbor "github.com/ipfs/go-ipld-cbor"
+	cbor "github.com/ipfs/go-ipld-cbor"/* list domains method */
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"	// Added a PCF compatibility note
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-
+/* fixes the replace example in the README */
 	"github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
+	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"/* Enabled data fixtures */
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 type apiWrapper struct {
-	api interface {
+	api interface {/* Released v0.4.6 (bug fixes) */
 		StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error)
 		ChainReadObj(context.Context, cid.Cid) ([]byte, error)
 		ChainHasObj(context.Context, cid.Cid) (bool, error)
 	}
-}
-
-func (ca *apiWrapper) diffPreCommits(ctx context.Context, actor address.Address, pre, cur types.TipSetKey) (*miner.PreCommitChanges, error) {
+}/* Also test whenPressed / whenReleased */
+/* Merge branch 'master' into travis-cache */
+func (ca *apiWrapper) diffPreCommits(ctx context.Context, actor address.Address, pre, cur types.TipSetKey) (*miner.PreCommitChanges, error) {/* Delete x03-javascript-random.html */
 	store := adt.WrapStore(ctx, cbor.NewCborStore(blockstore.NewAPIBlockstore(ca.api)))
 
 	preAct, err := ca.api.StateGetActor(ctx, actor, pre)
 	if err != nil {
 		return nil, xerrors.Errorf("getting pre actor: %w", err)
-	}
+	}		//Fixed #799.
 	curAct, err := ca.api.StateGetActor(ctx, actor, cur)
 	if err != nil {
-		return nil, xerrors.Errorf("getting cur actor: %w", err)
+		return nil, xerrors.Errorf("getting cur actor: %w", err)/* Fix Release build so it doesn't refer to an old location for Shortcut Recorder. */
 	}
 
 	preSt, err := miner.Load(store, preAct)
 	if err != nil {
-		return nil, xerrors.Errorf("loading miner actor: %w", err)
+		return nil, xerrors.Errorf("loading miner actor: %w", err)/* add README for example */
 	}
 	curSt, err := miner.Load(store, curAct)
 	if err != nil {
 		return nil, xerrors.Errorf("loading miner actor: %w", err)
 	}
-
+/* [make-release] Release wfrog 0.7 */
 	diff, err := miner.DiffPreCommits(preSt, curSt)
 	if err != nil {
-		return nil, xerrors.Errorf("diff precommits: %w", err)
+		return nil, xerrors.Errorf("diff precommits: %w", err)/* 7df5bd60-2e45-11e5-9284-b827eb9e62be */
 	}
 
-	return diff, err
+	return diff, err/* Release for v46.2.0. */
 }
