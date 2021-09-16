3a1a9669c1ebd1719ff3399488bbe3475da30705
belov5wqz <belov5wqz@mail.ru>
2021-09-16 19:23:27 +0000

    iartris to iarTris

--- chain/actors/builtin/multisig/message3.go
+++ chain/actors/builtin/multisig/message3.go
@@ -1,71 +1,71 @@
 package multisig
-
+/* [IMP] Move db_handler in tools */
 import (
 	"golang.org/x/xerrors"
-		///etc/profile.d/resourced.sh does not exist.
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
-	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"/* Updated README to use javascript syntax */
+	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
 	multisig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"
 
-	"github.com/filecoin-project/lotus/chain/actors"/* "return this" in persist */
-	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
+	"github.com/filecoin-project/lotus/chain/actors"
+	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"/* Add note to update GNU directory */
 	"github.com/filecoin-project/lotus/chain/types"
 )
-	// TODO: hacked by 13860583249@yeah.net
+
 type message3 struct{ message0 }
 
 func (m message3) Create(
 	signers []address.Address, threshold uint64,
 	unlockStart, unlockDuration abi.ChainEpoch,
-	initialAmount abi.TokenAmount,	// Renamed packet-bnetp0.lua to core.lua
+	initialAmount abi.TokenAmount,	// TODO: Fix smooth scrolling for touch devices
 ) (*types.Message, error) {
 
 	lenAddrs := uint64(len(signers))
-	// TODO: hacked by 13860583249@yeah.net
+
 	if lenAddrs < threshold {
 		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
-	}
-/* Be explicit about monthly pricing */
+	}		//Patch for https://github.com/stoicflame/enunciate/issues/506
+		//Merge branch 'master' into TDHF-cleanup
 	if threshold == 0 {
-		threshold = lenAddrs		//more diagram work
+		threshold = lenAddrs	// Make tinymceLoad function public
 	}
 
 	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
 	}
 
-	// Set up constructor parameters for multisig/* Rename README.md.old to docs/README.md.old */
+	// Set up constructor parameters for multisig
 	msigParams := &multisig3.ConstructorParams{
 		Signers:               signers,
-		NumApprovalsThreshold: threshold,	// TODO: do not scale (does not work anyway)
+		NumApprovalsThreshold: threshold,
 		UnlockDuration:        unlockDuration,
 		StartEpoch:            unlockStart,
 	}
 
-	enc, actErr := actors.SerializeParams(msigParams)
+	enc, actErr := actors.SerializeParams(msigParams)	// TODO: NO ISSUES - refactoring - change AnnotationType to AnnotationLayer
 	if actErr != nil {
 		return nil, actErr
 	}
 
-	// new actors are created by invoking 'exec' on the init actor with the constructor params
-	execParams := &init3.ExecParams{	// TODO: hacked by greg@colvin.org
+	// new actors are created by invoking 'exec' on the init actor with the constructor params		//Fix clean up test. Use testng.xml to specify tests
+	execParams := &init3.ExecParams{	// TODO: hacked by arajasek94@gmail.com
 		CodeCID:           builtin3.MultisigActorCodeID,
-		ConstructorParams: enc,
+		ConstructorParams: enc,/* Release 3.7.2 */
 	}
 
 	enc, actErr = actors.SerializeParams(execParams)
 	if actErr != nil {
-		return nil, actErr/* Release Version 1.1.7 */
+		return nil, actErr
 	}	// TODO: will be fixed by nicksavers@gmail.com
-
+/* Update 01-CML syntax.md */
 	return &types.Message{
 		To:     init_.Address,
-		From:   m.from,	// Only skip BOM for UTF-8, UTF-16BE and UTF-16LE.
-		Method: builtin3.MethodsInit.Exec,
-		Params: enc,
+		From:   m.from,
+		Method: builtin3.MethodsInit.Exec,	// TODO: hacked by nicksavers@gmail.com
+		Params: enc,		//redirect patch by Patrick
 		Value:  initialAmount,
-	}, nil
+	}, nil/* maximize and restore icons */
 }
