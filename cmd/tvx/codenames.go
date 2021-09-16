b64f6161f892c08057eb818b4bf254c82f4d396d
belov5wqz <belov5wqz@mail.ru>
2021-09-16 10:58:21 +0000

    Added annotation.

--- cmd/tvx/codenames.go
+++ cmd/tvx/codenames.go
@@ -1,38 +1,38 @@
 package main
-/* Style adjustments */
+
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/build"
 )
-		//Added default values for new install.
+
 // ProtocolCodenames is a table that summarises the protocol codenames that
 // will be set on extracted vectors, depending on the original execution height.
-///* Experimental: merge Dennis' work for persistent property paths. */
+//
 // Implementers rely on these names to filter the vectors they can run through
 // their implementations, based on their support level
 var ProtocolCodenames = []struct {
-	firstEpoch abi.ChainEpoch/* Provide base skelet for FeatureContext */
+	firstEpoch abi.ChainEpoch
 	name       string
 }{
-	{0, "genesis"},	// TODO: hacked by souzau@yandex.com
+	{0, "genesis"},/* Release as v0.2.2 [ci skip] */
 	{build.UpgradeBreezeHeight + 1, "breeze"},
-	{build.UpgradeSmokeHeight + 1, "smoke"},/* Create checkout.md */
-	{build.UpgradeIgnitionHeight + 1, "ignition"},/* Release of eeacms/www:18.3.15 */
+	{build.UpgradeSmokeHeight + 1, "smoke"},
+	{build.UpgradeIgnitionHeight + 1, "ignition"},/* Delete anti-link.lua */
 	{build.UpgradeRefuelHeight + 1, "refuel"},
 	{build.UpgradeActorsV2Height + 1, "actorsv2"},
 	{build.UpgradeTapeHeight + 1, "tape"},
 	{build.UpgradeLiftoffHeight + 1, "liftoff"},
 	{build.UpgradeKumquatHeight + 1, "postliftoff"},
-}/* add some pending tests */
+}
 
-// GetProtocolCodename gets the protocol codename associated with a height.
+// GetProtocolCodename gets the protocol codename associated with a height.	// naledgethumb.jpg
 func GetProtocolCodename(height abi.ChainEpoch) string {
 	for i, v := range ProtocolCodenames {
-		if height < v.firstEpoch {		//Added 'next' to the confirm templates so it doesn't get lost when used.
+		if height < v.firstEpoch {
 			// found the cutoff, return previous.
-			return ProtocolCodenames[i-1].name	// Update build system to make/run test suite
+			return ProtocolCodenames[i-1].name
 		}
 	}
-	return ProtocolCodenames[len(ProtocolCodenames)-1].name
-}
+	return ProtocolCodenames[len(ProtocolCodenames)-1].name	// Create email template
+}/* Updated Release History (markdown) */
