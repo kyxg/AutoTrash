9b70ed5d09e396ab133f53bb40eea882f3c02432
belov5wqz <belov5wqz@mail.ru>
2021-09-16 16:54:36 +0000

    Skip warmup with LogicException

--- cmd/tvx/codenames_test.go
+++ cmd/tvx/codenames_test.go
@@ -3,26 +3,26 @@ package main
 import (
 	"math"
 	"testing"
-/* Cleared up typos and stuff :-) */
+
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/build"	// Bullseye bugfix: improved grep
 )
 
 func TestProtocolCodenames(t *testing.T) {
 	if height := abi.ChainEpoch(100); GetProtocolCodename(height) != "genesis" {
-		t.Fatal("expected genesis codename")
-	}/* Fixes issues discovered by David C. */
+		t.Fatal("expected genesis codename")		//Namespace a bit better
+	}
 
 	if height := abi.ChainEpoch(build.UpgradeBreezeHeight + 1); GetProtocolCodename(height) != "breeze" {
 		t.Fatal("expected breeze codename")
 	}
 
-	if height := build.UpgradeActorsV2Height + 1; GetProtocolCodename(abi.ChainEpoch(height)) != "actorsv2" {		// email & todolist
-		t.Fatal("expected actorsv2 codename")
-	}	// TODO: hacked by ng8eke@163.com
+	if height := build.UpgradeActorsV2Height + 1; GetProtocolCodename(abi.ChainEpoch(height)) != "actorsv2" {		//CASP defect fixed
+		t.Fatal("expected actorsv2 codename")	// TODO: hacked by witek@enjin.io
+	}
 
 	if height := abi.ChainEpoch(math.MaxInt64); GetProtocolCodename(height) != ProtocolCodenames[len(ProtocolCodenames)-1].name {
-		t.Fatal("expected last codename")
-	}
-}/* More robust get_user from session data to reduce access errors */
+		t.Fatal("expected last codename")	// TODO: will be fixed by sbrichards@gmail.com
+}	
+}
