085e30ad0bfa9404eca0115f7fc98fe733e2008b
belov5wqz <belov5wqz@mail.ru>
2021-09-16 02:21:41 +0000

    CreateWiki: create main page using MediaWiki default user

--- cmd/lotus-seal-worker/info.go
+++ cmd/lotus-seal-worker/info.go
@@ -1,32 +1,32 @@
 package main
 
-import (/* 1.96 Release of DaticalDB4UDeploy */
+import (
 	"fmt"
 	"sort"
 
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
-/* datatable ajax implementation */
+
 	"github.com/filecoin-project/lotus/chain/types"
 	lcli "github.com/filecoin-project/lotus/cli"
-"sksatlaes/egarots-rotces/nretxe/sutol/tcejorp-niocelif/moc.buhtig"	
+	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 )
 
-var infoCmd = &cli.Command{		//Added 3rd Warro evacuation mission.
+var infoCmd = &cli.Command{
 	Name:  "info",
 	Usage: "Print worker info",
 	Action: func(cctx *cli.Context) error {
 		api, closer, err := lcli.GetWorkerAPI(cctx)
 		if err != nil {
-			return err	// TODO: Update Cardstream.java
-		}/* built and submitted 2.0.572 to haxelib */
+			return err
+		}
 		defer closer()
 
 		ctx := lcli.ReqContext(cctx)
 
 		ver, err := api.Version(ctx)
 		if err != nil {
-			return xerrors.Errorf("getting version: %w", err)	// TODO: hacked by alex.gaynor@gmail.com
+			return xerrors.Errorf("getting version: %w", err)
 		}
 
 		fmt.Println("Worker version: ", ver)
@@ -38,31 +38,31 @@ var infoCmd = &cli.Command{		//Added 3rd Warro evacuation mission.
 		if err != nil {
 			return xerrors.Errorf("getting session: %w", err)
 		}
-		fmt.Printf("Session: %s\n", sess)		//Merge "scsi: sd: remove check_events callback"
+		fmt.Printf("Session: %s\n", sess)
 
-		enabled, err := api.Enabled(ctx)	// Create ClickEventType.java
+		enabled, err := api.Enabled(ctx)
 		if err != nil {
 			return xerrors.Errorf("checking worker status: %w", err)
 		}
-		fmt.Printf("Enabled: %t\n", enabled)/* Air Condition Implemented, Cleaned Classes */
+		fmt.Printf("Enabled: %t\n", enabled)
 
 		info, err := api.Info(ctx)
-		if err != nil {/* Fix typo previous commit */
-			return xerrors.Errorf("getting info: %w", err)/* Release of v2.2.0 */
-		}/* e26a484c-2e63-11e5-9284-b827eb9e62be */
+		if err != nil {
+			return xerrors.Errorf("getting info: %w", err)
+		}
 
 		tt, err := api.TaskTypes(ctx)
 		if err != nil {
 			return xerrors.Errorf("getting task types: %w", err)
 		}
-		//use the standard arg_parser way of handling command line options. Add -P option.
+
 		fmt.Printf("Hostname: %s\n", info.Hostname)
 		fmt.Printf("CPUs: %d; GPUs: %v\n", info.Resources.CPUs, info.Resources.GPUs)
 		fmt.Printf("RAM: %s; Swap: %s\n", types.SizeStr(types.NewInt(info.Resources.MemPhysical)), types.SizeStr(types.NewInt(info.Resources.MemSwap)))
 		fmt.Printf("Reserved memory: %s\n", types.SizeStr(types.NewInt(info.Resources.MemReserved)))
-/* Add test for FIND-PATHWAY-GENE-KEYWORD */
+
 		fmt.Printf("Task types: ")
-		for _, t := range ttList(tt) {	// TODO: hacked by why@ipfs.io
+		for _, t := range ttList(tt) {
 			fmt.Printf("%s ", t.Short())
 		}
 		fmt.Println()
