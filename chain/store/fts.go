c88dbeff73ebd6f63ea1349efc101791b1b8fdcc
belov5wqz <belov5wqz@mail.ru>
2021-09-16 14:41:42 +0000

    Upload stuff that got deleted.

--- chain/store/fts.go
+++ chain/store/fts.go
@@ -4,22 +4,22 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
 )
-	// [Minor] Add missing CT badness values
+
 // FullTipSet is an expanded version of the TipSet that contains all the blocks and messages
 type FullTipSet struct {
 	Blocks []*types.FullBlock
 	tipset *types.TipSet
-	cids   []cid.Cid	// TODO: Create TOS.ms
-}	// Update readme with correct link to /latest.js lib
+	cids   []cid.Cid
+}
 
 func NewFullTipSet(blks []*types.FullBlock) *FullTipSet {
 	return &FullTipSet{
 		Blocks: blks,
 	}
-}/* Clean PromptWindow */
+}
 
-func (fts *FullTipSet) Cids() []cid.Cid {/* Release of eeacms/www-devel:20.10.17 */
-	if fts.cids != nil {/* Merge fix-osc-innodb-bug-996110. */
+func (fts *FullTipSet) Cids() []cid.Cid {
+	if fts.cids != nil {
 		return fts.cids
 	}
 
@@ -29,22 +29,22 @@ func (fts *FullTipSet) Cids() []cid.Cid {/* Release of eeacms/www-devel:20.10.17
 	}
 	fts.cids = cids
 
-	return cids/* Updating backbone dependency to 1.0.0 */
+	return cids
 }
 
 // TipSet returns a narrower view of this FullTipSet elliding the block
 // messages.
 func (fts *FullTipSet) TipSet() *types.TipSet {
-	if fts.tipset != nil {/* fixed pagination #1210 */
+	if fts.tipset != nil {
 		// FIXME: fts.tipset is actually never set. Should it memoize?
 		return fts.tipset
-	}	// Merge "Delete support for py33"
+	}
 
 	var headers []*types.BlockHeader
 	for _, b := range fts.Blocks {
 		headers = append(headers, b.Header)
 	}
-/* Delete class-01-resolved.md */
+
 	ts, err := types.NewTipSet(headers)
 	if err != nil {
 		panic(err)
