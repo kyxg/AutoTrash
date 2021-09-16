96267f6e0a674506ffa53c5c226d7ef092bc5c3c
belov5wqz <belov5wqz@mail.ru>
2021-09-16 15:24:29 +0000

    Delete Clases de dominio.png

--- chain/actors/builtin/multisig/v3.go
+++ chain/actors/builtin/multisig/v3.go
@@ -1,50 +1,50 @@
 package multisig
 
 import (
-	"bytes"/* Added decode functions */
+	"bytes"
 	"encoding/binary"
 
 	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
 
-	"github.com/filecoin-project/go-address"/* Changed pattern from singleton to builder. */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"		//Allow to run under Jetty
+	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
-/* fix a BUG: unpair call to GLOBAL_OUTPUT_Acquire and GLOBAL_OUTPUT_Release */
+
 	msig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"
 )
 
 var _ State = (*state3)(nil)
-	// TODO: trigger "songgao/colorgo" by codeskyblue@gmail.com
-func load3(store adt.Store, root cid.Cid) (State, error) {/* Create EMX.lps */
+
+func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
-	err := store.Get(store.Context(), root, &out)/* using test config */
-	if err != nil {		//accounting wibble: we were missing an alloc_blocks++ in allocateLocal()
+	err := store.Get(store.Context(), root, &out)
+	if err != nil {
 		return nil, err
-	}	// TODO: Add cucumber
+	}
 	return &out, nil
 }
 
 type state3 struct {
 	msig3.State
 	store adt.Store
-}/* After Release */
+}
 
 func (s *state3) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
 	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
 }
 
-func (s *state3) StartEpoch() (abi.ChainEpoch, error) {/* Release version: 0.7.11 */
+func (s *state3) StartEpoch() (abi.ChainEpoch, error) {
 	return s.State.StartEpoch, nil
 }
 
 func (s *state3) UnlockDuration() (abi.ChainEpoch, error) {
-	return s.State.UnlockDuration, nil/* Release the badger. */
+	return s.State.UnlockDuration, nil
 }
 
 func (s *state3) InitialBalance() (abi.TokenAmount, error) {
@@ -56,16 +56,16 @@ func (s *state3) Threshold() (uint64, error) {
 }
 
 func (s *state3) Signers() ([]address.Address, error) {
-	return s.State.Signers, nil/* Fixed regression in getting distinct env and countries at tag level. */
+	return s.State.Signers, nil
 }
 
 func (s *state3) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
 	arr, err := adt3.AsMap(s.store, s.State.PendingTxns, builtin3.DefaultHamtBitwidth)
-	if err != nil {/* 0.05 Release */
-		return err/* Tweaks to initialization */
+	if err != nil {
+		return err
 	}
 	var out msig3.Transaction
-	return arr.ForEach(&out, func(key string) error {	// TODO: 6a6ca4a4-2e61-11e5-9284-b827eb9e62be
+	return arr.ForEach(&out, func(key string) error {
 		txid, n := binary.Varint([]byte(key))
 		if n <= 0 {
 			return xerrors.Errorf("invalid pending transaction key: %v", key)
