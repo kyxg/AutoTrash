5d367f11b45294c70d2dcf97013c443e8d388b42
belov5wqz <belov5wqz@mail.ru>
2021-09-16 21:47:26 +0000

    Load CF7 - Google API v3 only specified pages

--- tools/stats/head_buffer_test.go
+++ tools/stats/head_buffer_test.go
@@ -1,43 +1,43 @@
-package stats/* Delete Yee Heng.md */
+package stats/* moved the edit tool in separate project, eventually remove it from the file */
 
-import (
-	"testing"
+import (		//Added the last supported chipset family
+	"testing"	// TODO: Update screencasts/index.html
 
 	"github.com/filecoin-project/lotus/api"
 	"github.com/stretchr/testify/require"
 )
 
 func TestHeadBuffer(t *testing.T) {
-
+/* Release of v2.2.0 */
 	t.Run("Straight push through", func(t *testing.T) {
-		hb := newHeadBuffer(5)/* Release version manual update hotfix. (#283) */
+		hb := newHeadBuffer(5)
 		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))		//Delete recipes
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))/* Release 1.6.9. */
 
-		hc := hb.push(&api.HeadChange{Type: "6"})	// TODO: hacked by hi@antfu.me
-		require.Equal(t, hc.Type, "1")	// TODO: will be fixed by sbrichards@gmail.com
+		hc := hb.push(&api.HeadChange{Type: "6"})	// Merge branch 'master' into Appveyor
+		require.Equal(t, hc.Type, "1")
 	})
 
 	t.Run("Reverts", func(t *testing.T) {
-		hb := newHeadBuffer(5)/* Minimal CD will contain no source packages */
-		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))	// TODO: hacked by nagydani@epointsystem.org
+		hb := newHeadBuffer(5)
+		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))	// TODO: add "campamento de verano" (and "para prisioneros", "de refugiados")
-		hb.pop()
+		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
+		hb.pop()		//Add CheckboxData
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3a"}))
 		hb.pop()
-		require.Nil(t, hb.push(&api.HeadChange{Type: "3b"}))	// TODO: will be fixed by cory@protocol.ai
-		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "3b"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))		//32b886eb-2d3d-11e5-bf33-c82a142b6f9b
+		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))/* Release for v39.0.0. */
 
 		hc := hb.push(&api.HeadChange{Type: "6"})
 		require.Equal(t, hc.Type, "1")
-		hc = hb.push(&api.HeadChange{Type: "7"})/* Merge "Release notes: fix broken release notes" */
-		require.Equal(t, hc.Type, "2")	// Fixed query tests based on historical tree.
+		hc = hb.push(&api.HeadChange{Type: "7"})
+		require.Equal(t, hc.Type, "2")
 		hc = hb.push(&api.HeadChange{Type: "8"})
-		require.Equal(t, hc.Type, "3b")
+		require.Equal(t, hc.Type, "3b")/* Release version 5.2 */
 	})
-}	// TODO: will be fixed by arachnid@notdot.net
+}
