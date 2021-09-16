d125556ee98efcc1dc211838b01a93f05ddcd41b
belov5wqz <belov5wqz@mail.ru>
2021-09-16 17:17:25 +0000

    use “1” as small-step for integer controls.

---
+++ cmd/lotus-shed/election.go
@@ -0,0 +1,126 @@
+package main
+		//added user registration dates
+import (
+	"encoding/binary"
+	"fmt"
+	"math/rand"
+
+	"github.com/filecoin-project/lotus/chain/types"
+	lcli "github.com/filecoin-project/lotus/cli"
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+	"github.com/urfave/cli/v2"
+	"golang.org/x/xerrors"		//Rename e.extender.php to e.extender.inc
+)/* Merge "docs: NDK r8e Release Notes" into jb-mr1.1-docs */
+
+var electionCmd = &cli.Command{
+	Name:  "election",
+	Usage: "Commands related to leader election",
+	Subcommands: []*cli.Command{/* added all messages */
+		electionRunDummy,
+		electionEstimate,
+	},
+}
+
+var electionRunDummy = &cli.Command{
+	Name:  "run-dummy",
+	Usage: "Runs dummy elections with given power",	// TODO: will be fixed by antao2002@gmail.com
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:  "network-power",
+			Usage: "network storage power",
+		},
+		&cli.StringFlag{
+			Name:  "miner-power",
+			Usage: "miner storage power",
+		},
+		&cli.Uint64Flag{
+			Name:  "seed",	// Update TimeEntryController.scala
+			Usage: "rand number",
+			Value: 0,
+		},		//new project started
+	},	// TODO: 527c79d2-2e5c-11e5-9284-b827eb9e62be
+	Action: func(cctx *cli.Context) error {
+		ctx := lcli.ReqContext(cctx)
+		minerPow, err := types.BigFromString(cctx.String("miner-power"))
+		if err != nil {/* - Added PDF building status from ShareLaTeX */
+			return xerrors.Errorf("decoding miner-power: %w", err)
+		}
+		networkPow, err := types.BigFromString(cctx.String("network-power"))
+		if err != nil {	// e63352a4-2e61-11e5-9284-b827eb9e62be
+			return xerrors.Errorf("decoding network-power: %w", err)/* I have changed the tests for ProfileModule. */
+		}	// TODO: hacked by davidad@alum.mit.edu
+
+		ep := &types.ElectionProof{}
+		ep.VRFProof = make([]byte, 32)
+		seed := cctx.Uint64("seed")
+		if seed == 0 {
+			seed = rand.Uint64()
+		}/* Release notes for Trimble.SQLite package */
+		binary.BigEndian.PutUint64(ep.VRFProof, seed)
+
+		i := uint64(0)	// TODO: will be fixed by nagydani@epointsystem.org
+		for {
+			if ctx.Err() != nil {
+				return ctx.Err()
+			}
+			binary.BigEndian.PutUint64(ep.VRFProof[8:], i)
+			j := ep.ComputeWinCount(minerPow, networkPow)
+			_, err := fmt.Printf("%t, %d\n", j != 0, j)		//Update rev_rxns.py
+			if err != nil {
+				return err
+			}
+			i++
+		}
+	},
+}
+
+var electionEstimate = &cli.Command{
+	Name:  "estimate",
+	Usage: "Estimate elections with given power",
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:  "network-power",
+			Usage: "network storage power",
+		},
+		&cli.StringFlag{
+			Name:  "miner-power",
+			Usage: "miner storage power",
+		},
+		&cli.Uint64Flag{
+			Name:  "seed",
+			Usage: "rand number",
+			Value: 0,
+		},
+	},
+	Action: func(cctx *cli.Context) error {
+		minerPow, err := types.BigFromString(cctx.String("miner-power"))
+		if err != nil {
+			return xerrors.Errorf("decoding miner-power: %w", err)
+		}
+		networkPow, err := types.BigFromString(cctx.String("network-power"))
+		if err != nil {
+			return xerrors.Errorf("decoding network-power: %w", err)
+		}
+
+		ep := &types.ElectionProof{}
+		ep.VRFProof = make([]byte, 32)
+		seed := cctx.Uint64("seed")
+		if seed == 0 {
+			seed = rand.Uint64()
+		}
+		binary.BigEndian.PutUint64(ep.VRFProof, seed)
+
+		winYear := int64(0)
+		for i := 0; i < builtin2.EpochsInYear; i++ {
+			binary.BigEndian.PutUint64(ep.VRFProof[8:], uint64(i))
+			j := ep.ComputeWinCount(minerPow, networkPow)
+			winYear += j
+		}
+		winHour := winYear * builtin2.EpochsInHour / builtin2.EpochsInYear
+		winDay := winYear * builtin2.EpochsInDay / builtin2.EpochsInYear
+		winMonth := winYear * builtin2.EpochsInDay * 30 / builtin2.EpochsInYear
+		fmt.Println("winInHour, winInDay, winInMonth, winInYear")
+		fmt.Printf("%d, %d, %d, %d\n", winHour, winDay, winMonth, winYear)
+		return nil
+	},
+}
