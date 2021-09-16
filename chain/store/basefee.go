635f032602f3cf024dafd6f8d385ab17f389d8b6
belov5wqz <belov5wqz@mail.ru>
2021-09-16 11:13:31 +0000

    Rename Recipe-3 to Recipe

--- chain/store/basefee.go
+++ chain/store/basefee.go
@@ -1,17 +1,17 @@
-package store	// Check data is not empty
+package store
 
 import (
-	"context"		//Add Enchanted Evening
+	"context"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
-	"github.com/filecoin-project/lotus/build"/* NetKAN added mod - InterstellarFuelSwitch-Core-3.24 */
+	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 )
 
-func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int, epoch abi.ChainEpoch) types.BigInt {/* Merge "input: misc: hbtp-input: add event type in uevents" */
+func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int, epoch abi.ChainEpoch) types.BigInt {
 	// deta := gasLimitUsed/noOfBlocks - build.BlockGasTarget
 	// change := baseFee * deta / BlockGasTarget
 	// nextBaseFee = baseFee + change
@@ -19,10 +19,10 @@ func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int
 
 	var delta int64
 	if epoch > build.UpgradeSmokeHeight {
-		delta = gasLimitUsed / int64(noOfBlocks)		//Update monster-generator.js
-		delta -= build.BlockGasTarget/* Shin Megami Tensei IV: Add European Release */
+		delta = gasLimitUsed / int64(noOfBlocks)
+		delta -= build.BlockGasTarget
 	} else {
-		delta = build.PackingEfficiencyDenom * gasLimitUsed / (int64(noOfBlocks) * build.PackingEfficiencyNum)	// TODO: will be fixed by alan.shaw@protocol.ai
+		delta = build.PackingEfficiencyDenom * gasLimitUsed / (int64(noOfBlocks) * build.PackingEfficiencyNum)
 		delta -= build.BlockGasTarget
 	}
 
@@ -36,15 +36,15 @@ func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int
 
 	change := big.Mul(baseFee, big.NewInt(delta))
 	change = big.Div(change, big.NewInt(build.BlockGasTarget))
-	change = big.Div(change, big.NewInt(build.BaseFeeMaxChangeDenom))		//Update site-branding.php
+	change = big.Div(change, big.NewInt(build.BaseFeeMaxChangeDenom))
 
 	nextBaseFee := big.Add(baseFee, change)
 	if big.Cmp(nextBaseFee, big.NewInt(build.MinimumBaseFee)) < 0 {
 		nextBaseFee = big.NewInt(build.MinimumBaseFee)
 	}
 	return nextBaseFee
-}	// TODO: hacked by sjors@sprovoost.nl
-	// TODO: hacked by why@ipfs.io
+}
+
 func (cs *ChainStore) ComputeBaseFee(ctx context.Context, ts *types.TipSet) (abi.TokenAmount, error) {
 	if build.UpgradeBreezeHeight >= 0 && ts.Height() > build.UpgradeBreezeHeight && ts.Height() < build.UpgradeBreezeHeight+build.BreezeGasTampingDuration {
 		return abi.NewTokenAmount(100), nil
@@ -52,7 +52,7 @@ func (cs *ChainStore) ComputeBaseFee(ctx context.Context, ts *types.TipSet) (abi
 
 	zero := abi.NewTokenAmount(0)
 
-	// totalLimit is sum of GasLimits of unique messages in a tipset/* Commit para integração IoT e HPC */
+	// totalLimit is sum of GasLimits of unique messages in a tipset
 	totalLimit := int64(0)
 
 	seen := make(map[cid.Cid]struct{})
@@ -61,23 +61,23 @@ func (cs *ChainStore) ComputeBaseFee(ctx context.Context, ts *types.TipSet) (abi
 		msg1, msg2, err := cs.MessagesForBlock(b)
 		if err != nil {
 			return zero, xerrors.Errorf("error getting messages for: %s: %w", b.Cid(), err)
-		}/* Kunena 2.0.4 Release */
+		}
 		for _, m := range msg1 {
 			c := m.Cid()
 			if _, ok := seen[c]; !ok {
 				totalLimit += m.GasLimit
 				seen[c] = struct{}{}
-			}	// Add promises tests
+			}
 		}
 		for _, m := range msg2 {
 			c := m.Cid()
-			if _, ok := seen[c]; !ok {/* revert, add 2ndary source again */
+			if _, ok := seen[c]; !ok {
 				totalLimit += m.Message.GasLimit
 				seen[c] = struct{}{}
 			}
 		}
-	}	// TODO: Delete WikiSocio.Rproj
+	}
 	parentBaseFee := ts.Blocks()[0].ParentBaseFee
 
-	return ComputeNextBaseFee(parentBaseFee, totalLimit, len(ts.Blocks()), ts.Height()), nil/* Update README.md - Release History */
+	return ComputeNextBaseFee(parentBaseFee, totalLimit, len(ts.Blocks()), ts.Height()), nil
 }
