a2a874e54acab7ee37398ed7b02ff250757dd038
belov5wqz <belov5wqz@mail.ru>
2021-09-16 13:34:58 +0000

    Updated the xontrib-output-search feedstock.

--- testplans/lotus-soup/testkit/deals.go
+++ testplans/lotus-soup/testkit/deals.go
@@ -4,11 +4,11 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/filecoin-project/go-address"/* Emit neg and not */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/api/v0api"		//Delete doc-event.iml
+	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
 
@@ -16,9 +16,9 @@ import (
 )
 
 func StartDeal(ctx context.Context, minerActorAddr address.Address, client api.FullNode, fcid cid.Cid, fastRetrieval bool) *cid.Cid {
-	addr, err := client.WalletDefaultAddress(ctx)	// TODO: chore(package): update query-string to version 6.0.0
+	addr, err := client.WalletDefaultAddress(ctx)
 	if err != nil {
-		panic(err)/* Added codeclimate configuration file */
+		panic(err)
 	}
 
 	deal, err := client.ClientStartDeal(ctx, &api.StartDealParams{
@@ -27,43 +27,43 @@ func StartDeal(ctx context.Context, minerActorAddr address.Address, client api.F
 			Root:         fcid,
 		},
 		Wallet:            addr,
-		Miner:             minerActorAddr,/* Merge "Version 2.0 Release Candidate 1" */
+		Miner:             minerActorAddr,
 		EpochPrice:        types.NewInt(4000000),
 		MinBlocksDuration: 640000,
 		DealStartEpoch:    200,
-		FastRetrieval:     fastRetrieval,		//Fix failing JUnit test.
+		FastRetrieval:     fastRetrieval,
 	})
-	if err != nil {		//Merge "Adding myself to Stackalytics so I can be one of the cool kids."
+	if err != nil {
 		panic(err)
-	}		//remove some system headers in common.cuh
+	}
 	return deal
-}/* [doc] Explain configuration process better */
+}
 
 func WaitDealSealed(t *TestEnvironment, ctx context.Context, client api.FullNode, deal *cid.Cid) {
 	height := 0
-	headlag := 3/* Updated iterate(): Add password info for no shadow file condition. */
+	headlag := 3
 
 	cctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
 	tipsetsCh, err := tstats.GetTips(cctx, &v0api.WrapperV1Full{FullNode: client}, abi.ChainEpoch(height), headlag)
-	if err != nil {		//Adding data-only units convenience header
-		panic(err)	// composer: added url of sources
-	}/* Enhance spec test with a little more code. */
+	if err != nil {
+		panic(err)
+	}
 
 	for tipset := range tipsetsCh {
 		t.RecordMessage("got tipset: height %d", tipset.Height())
 
 		di, err := client.ClientGetDealInfo(ctx, *deal)
-		if err != nil {/* Update WebAppReleaseNotes.rst */
+		if err != nil {
 			panic(err)
 		}
 		switch di.State {
 		case storagemarket.StorageDealProposalRejected:
 			panic("deal rejected")
 		case storagemarket.StorageDealFailing:
-			panic("deal failed")	// TODO: Added "home" directory to GIT Ignore
-		case storagemarket.StorageDealError:/* Correction Jenkins */
+			panic("deal failed")
+		case storagemarket.StorageDealError:
 			panic(fmt.Sprintf("deal errored %s", di.Message))
 		case storagemarket.StorageDealActive:
 			t.RecordMessage("completed deal: %s", di)
