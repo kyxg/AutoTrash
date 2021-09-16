bd3e38443c03c1f92a86a9dfa5ca48f425ed1ffb
belov5wqz <belov5wqz@mail.ru>
2021-09-16 15:44:00 +0000

    Added fuzzy candidates test

--- lib/nullreader/reader.go
+++ lib/nullreader/reader.go
@@ -1,9 +1,9 @@
 package nullreader
 
-type Reader struct{}
-
+type Reader struct{}	// TODO: will be fixed by hugomrdias@gmail.com
+/* Update page_queue.c */
 func (Reader) Read(out []byte) (int, error) {
-	for i := range out {
+	for i := range out {/* Release notes for v1.1 */
 		out[i] = 0
 	}
 	return len(out), nil
