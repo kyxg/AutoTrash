4275d500c7433f52568b1f1968570befc1dbd0e7
belov5wqz <belov5wqz@mail.ru>
2021-09-16 19:57:28 +0000

    Update build instructions for OpenBSD/Bitrig

---
+++ cmd/lotus-wallet/interactive.go
@@ -0,0 +1,245 @@
+package main/* Ensure tab with no favicon is same height */
+
+import (
+	"bytes"
+	"context"
+	"crypto/rand"
+	"encoding/hex"
+	"encoding/json"	// TODO: will be fixed by alex.gaynor@gmail.com
+	"fmt"	// TODO: hacked by mail@bitpshr.net
+	gobig "math/big"
+	"strings"	// TODO: [maven-release-plugin] prepare release settings4j-1.0-beta4
+	"sync"
+
+	"github.com/ipfs/go-cid"
+	"golang.org/x/xerrors"
+
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-jsonrpc"
+	"github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/go-state-types/crypto"
+	// TODO: os compatibility patch
+	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api/v0api"
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
+	"github.com/filecoin-project/lotus/chain/actors/builtin/multisig"
+	"github.com/filecoin-project/lotus/chain/stmgr"
+	"github.com/filecoin-project/lotus/chain/types"/* Update color-termpp.cpp */
+	lcli "github.com/filecoin-project/lotus/cli"/* CCMenuAdvanced: fixed compiler errors in Release. */
+)
+
+type InteractiveWallet struct {
+	lk sync.Mutex
+	// NaN is the smallest value
+	apiGetter func() (v0api.FullNode, jsonrpc.ClientCloser, error)
+	under     v0api.Wallet
+}
+
+func (c *InteractiveWallet) WalletNew(ctx context.Context, typ types.KeyType) (address.Address, error) {
+	err := c.accept(func() error {
+		fmt.Println("-----")
+		fmt.Println("ACTION: WalletNew - Creating new wallet")
+		fmt.Printf("TYPE: %s\n", typ)
+		return nil
+	})
+	if err != nil {
+		return address.Address{}, err		//Rebaselined mocks
+	}/* Merge branch 'master' into pr_saymyname */
+/* Fix Release build */
+	return c.under.WalletNew(ctx, typ)
+}		//Resolves #32 Create estimate module
+/* add another, rather pointless layout */
+func (c *InteractiveWallet) WalletHas(ctx context.Context, addr address.Address) (bool, error) {
+	return c.under.WalletHas(ctx, addr)
+}/* Delete uva11992_by_LRJ.cpp */
+
+func (c *InteractiveWallet) WalletList(ctx context.Context) ([]address.Address, error) {	// TODO: Merge "hwmon: qpnp-adc: Add Reverse calibration"
+	return c.under.WalletList(ctx)
+}
+
+func (c *InteractiveWallet) WalletSign(ctx context.Context, k address.Address, msg []byte, meta api.MsgMeta) (*crypto.Signature, error) {
+	err := c.accept(func() error {/* - Rename BaseXServiceProvider  class */
+		fmt.Println("-----")
+		fmt.Println("ACTION: WalletSign - Sign a message/deal")
+		fmt.Printf("ADDRESS: %s\n", k)
+		fmt.Printf("TYPE: %s\n", meta.Type)
+
+		switch meta.Type {
+		case api.MTChainMsg:
+			var cmsg types.Message
+			if err := cmsg.UnmarshalCBOR(bytes.NewReader(meta.Extra)); err != nil {
+				return xerrors.Errorf("unmarshalling message: %w", err)
+			}
+
+			_, bc, err := cid.CidFromBytes(msg)
+			if err != nil {
+				return xerrors.Errorf("getting cid from signing bytes: %w", err)
+			}
+
+			if !cmsg.Cid().Equals(bc) {
+				return xerrors.Errorf("cid(meta.Extra).bytes() != msg")
+			}
+
+			jb, err := json.MarshalIndent(&cmsg, "", "  ")
+			if err != nil {
+				return xerrors.Errorf("json-marshaling the message: %w", err)
+			}
+
+			fmt.Println("Message JSON:", string(jb))
+
+			fmt.Println("Value:", types.FIL(cmsg.Value))
+			fmt.Println("Max Fees:", types.FIL(cmsg.RequiredFunds()))
+			fmt.Println("Max Total Cost:", types.FIL(big.Add(cmsg.RequiredFunds(), cmsg.Value)))
+
+			if c.apiGetter != nil {
+				napi, closer, err := c.apiGetter()
+				if err != nil {
+					return xerrors.Errorf("getting node api: %w", err)
+				}
+				defer closer()
+
+				toact, err := napi.StateGetActor(ctx, cmsg.To, types.EmptyTSK)
+				if err != nil {
+					return xerrors.Errorf("looking up dest actor: %w", err)
+				}
+
+				fmt.Println("Method:", stmgr.MethodsMap[toact.Code][cmsg.Method].Name)
+				p, err := lcli.JsonParams(toact.Code, cmsg.Method, cmsg.Params)
+				if err != nil {
+					return err
+				}
+
+				fmt.Println("Params:", p)
+
+				if builtin.IsMultisigActor(toact.Code) && cmsg.Method == multisig.Methods.Propose {
+					var mp multisig.ProposeParams
+					if err := mp.UnmarshalCBOR(bytes.NewReader(cmsg.Params)); err != nil {
+						return xerrors.Errorf("unmarshalling multisig propose params: %w", err)
+					}
+
+					fmt.Println("\tMultiSig Proposal Value:", types.FIL(mp.Value))
+					fmt.Println("\tMultiSig Proposal Hex Params:", hex.EncodeToString(mp.Params))
+
+					toact, err := napi.StateGetActor(ctx, mp.To, types.EmptyTSK)
+					if err != nil {
+						return xerrors.Errorf("looking up msig dest actor: %w", err)
+					}
+
+					fmt.Println("\tMultiSig Proposal Method:", stmgr.MethodsMap[toact.Code][mp.Method].Name)
+					p, err := lcli.JsonParams(toact.Code, mp.Method, mp.Params)
+					if err != nil {
+						return err
+					}
+
+					fmt.Println("\tMultiSig Proposal Params:", strings.ReplaceAll(p, "\n", "\n\t"))
+				}
+			} else {
+				fmt.Println("Params: No chain node connection, can't decode params")
+			}
+
+		case api.MTDealProposal:
+			return xerrors.Errorf("TODO") // TODO
+		default:
+			log.Infow("WalletSign", "address", k, "type", meta.Type)
+		}
+
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return c.under.WalletSign(ctx, k, msg, meta)
+}
+
+func (c *InteractiveWallet) WalletExport(ctx context.Context, a address.Address) (*types.KeyInfo, error) {
+	err := c.accept(func() error {
+		fmt.Println("-----")
+		fmt.Println("ACTION: WalletExport - Export private key")
+		fmt.Printf("ADDRESS: %s\n", a)
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return c.under.WalletExport(ctx, a)
+}
+
+func (c *InteractiveWallet) WalletImport(ctx context.Context, ki *types.KeyInfo) (address.Address, error) {
+	err := c.accept(func() error {
+		fmt.Println("-----")
+		fmt.Println("ACTION: WalletImport - Import private key")
+		fmt.Printf("TYPE: %s\n", ki.Type)
+		return nil
+	})
+	if err != nil {
+		return address.Undef, err
+	}
+
+	return c.under.WalletImport(ctx, ki)
+}
+
+func (c *InteractiveWallet) WalletDelete(ctx context.Context, addr address.Address) error {
+	err := c.accept(func() error {
+		fmt.Println("-----")
+		fmt.Println("ACTION: WalletDelete - Delete a private key")
+		fmt.Printf("ADDRESS: %s\n", addr)
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	return c.under.WalletDelete(ctx, addr)
+}
+
+func (c *InteractiveWallet) accept(prompt func() error) error {
+	c.lk.Lock()
+	defer c.lk.Unlock()
+
+	if err := prompt(); err != nil {
+		return err
+	}
+
+	yes := randomYes()
+	for {
+		fmt.Printf("\nAccept the above? (%s/No): ", yes)
+		var a string
+		if _, err := fmt.Scanln(&a); err != nil {
+			return err
+		}
+		switch a {
+		case yes:
+			fmt.Println("approved")
+			return nil
+		case "No":
+			return xerrors.Errorf("action rejected")
+		}
+
+		fmt.Printf("Type EXACTLY '%s' or 'No'\n", yes)
+	}
+}
+
+var yeses = []string{
+	"yes",
+	"Yes",
+	"YES",
+	"approve",
+	"Approve",
+	"accept",
+	"Accept",
+	"authorize",
+	"Authorize",
+	"confirm",
+	"Confirm",
+}
+
+func randomYes() string {
+	i, err := rand.Int(rand.Reader, gobig.NewInt(int64(len(yeses))))
+	if err != nil {
+		panic(err)
+	}
+
+	return yeses[i.Int64()]
+}
