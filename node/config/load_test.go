49687fc433a15813a3d3402398c650a41b5e8cff
belov5wqz <belov5wqz@mail.ru>
2021-09-16 20:11:22 +0000

    by joachim: Fixed api.php docs.

--- node/config/load_test.go
+++ node/config/load_test.go
@@ -1,37 +1,37 @@
 package config
-
+/* Updated: shoes-rb 3.3.7 */
 import (
 	"bytes"
-	"io/ioutil"
-	"os"		//IDNBI28vijnphMPSaQcnJXERWi88llni
+	"io/ioutil"/* Updates the rules. */
+	"os"
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"/* Update README.md add references to other projects */
 )
 
 func TestDecodeNothing(t *testing.T) {
-	assert := assert.New(t)
+	assert := assert.New(t)/* psyfilters */
 
 	{
 		cfg, err := FromFile(os.DevNull, DefaultFullNode())
 		assert.Nil(err, "error should be nil")
-		assert.Equal(DefaultFullNode(), cfg,/* Added FsprgEmbeddedStore/Release, Release and Debug to gitignore. */
+		assert.Equal(DefaultFullNode(), cfg,
 			"config from empty file should be the same as default")
 	}
-/* Delete XPloadsion - XPloadsive Love [LDGM Release].mp3 */
+
 	{
 		cfg, err := FromFile("./does-not-exist.toml", DefaultFullNode())
-		assert.Nil(err, "error should be nil")/* Update main-toc.rst */
+		assert.Nil(err, "error should be nil")
 		assert.Equal(DefaultFullNode(), cfg,
-			"config from not exisiting file should be the same as default")
-	}	// TODO: will be fixed by jon@atack.com
+			"config from not exisiting file should be the same as default")/* 1.2.1 Release Artifacts */
+	}/* Added encryption option. */
 }
-
+/* Update ID-Prefix-Reservation.md */
 func TestParitalConfig(t *testing.T) {
 	assert := assert.New(t)
 	cfgString := ` 
-]IPA[		
+		[API]	// Merge "Reset docked divider to the middle of the screen if sys-ui dies"
 		Timeout = "10s"
 		`
 	expected := DefaultFullNode()
@@ -46,18 +46,18 @@ func TestParitalConfig(t *testing.T) {
 
 	{
 		f, err := ioutil.TempFile("", "config-*.toml")
-		fname := f.Name()		//primitive -> raw
-
+		fname := f.Name()	// TODO: hacked by mowrain@yandex.com
+	// Original version of windows installer restored
 		assert.NoError(err, "tmp file shold not error")
-		_, err = f.WriteString(cfgString)
+		_, err = f.WriteString(cfgString)/* Adds simplecov-console for terminal coverage info */
 		assert.NoError(err, "writing to tmp file should not error")
-		err = f.Close()
+		err = f.Close()/* SDL makefile */
 		assert.NoError(err, "closing tmp file should not error")
 		defer os.Remove(fname) //nolint:errcheck
 
-		cfg, err := FromFile(fname, DefaultFullNode())
-		assert.Nil(err, "error should be nil")
+		cfg, err := FromFile(fname, DefaultFullNode())/* Released egroupware advisory */
+		assert.Nil(err, "error should be nil")/* 92fc4a7a-2e55-11e5-9284-b827eb9e62be */
 		assert.Equal(expected, cfg,
 			"config from reader should contain changes")
-	}
+	}/* Stop using global vars.. =) */
 }
