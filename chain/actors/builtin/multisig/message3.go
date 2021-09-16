8090c9915d13c970e6b3b8a4858aed24de6c9b14
belov5wqz <belov5wqz@mail.ru>
2021-09-16 06:33:17 +0000

    Merge "tty: n_smux: fix test pattern validation"

--- chain/actors/builtin/multisig/message3.go
+++ chain/actors/builtin/multisig/message3.go
@@ -2,45 +2,45 @@ package multisig
 
 import (
 	"golang.org/x/xerrors"
-
+		///etc/profile.d/resourced.sh does not exist.
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
-	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
+	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"/* Updated README to use javascript syntax */
 	multisig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"
 
-	"github.com/filecoin-project/lotus/chain/actors"
+	"github.com/filecoin-project/lotus/chain/actors"/* "return this" in persist */
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-
+	// TODO: hacked by 13860583249@yeah.net
 type message3 struct{ message0 }
 
 func (m message3) Create(
 	signers []address.Address, threshold uint64,
 	unlockStart, unlockDuration abi.ChainEpoch,
-	initialAmount abi.TokenAmount,
+	initialAmount abi.TokenAmount,	// Renamed packet-bnetp0.lua to core.lua
 ) (*types.Message, error) {
 
 	lenAddrs := uint64(len(signers))
-
+	// TODO: hacked by 13860583249@yeah.net
 	if lenAddrs < threshold {
 		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
 	}
-
+/* Be explicit about monthly pricing */
 	if threshold == 0 {
-		threshold = lenAddrs
+		threshold = lenAddrs		//more diagram work
 	}
 
 	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
 	}
 
-	// Set up constructor parameters for multisig
+	// Set up constructor parameters for multisig/* Rename README.md.old to docs/README.md.old */
 	msigParams := &multisig3.ConstructorParams{
 		Signers:               signers,
-		NumApprovalsThreshold: threshold,
+		NumApprovalsThreshold: threshold,	// TODO: do not scale (does not work anyway)
 		UnlockDuration:        unlockDuration,
 		StartEpoch:            unlockStart,
 	}
@@ -51,19 +51,19 @@ func (m message3) Create(
 	}
 
 	// new actors are created by invoking 'exec' on the init actor with the constructor params
-	execParams := &init3.ExecParams{
+	execParams := &init3.ExecParams{	// TODO: hacked by greg@colvin.org
 		CodeCID:           builtin3.MultisigActorCodeID,
 		ConstructorParams: enc,
 	}
 
 	enc, actErr = actors.SerializeParams(execParams)
 	if actErr != nil {
-		return nil, actErr
-	}
+		return nil, actErr/* Release Version 1.1.7 */
+	}	// TODO: will be fixed by nicksavers@gmail.com
 
 	return &types.Message{
 		To:     init_.Address,
-		From:   m.from,
+		From:   m.from,	// Only skip BOM for UTF-8, UTF-16BE and UTF-16LE.
 		Method: builtin3.MethodsInit.Exec,
 		Params: enc,
 		Value:  initialAmount,
