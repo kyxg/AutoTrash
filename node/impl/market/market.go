3bf946d86382d57eb84a6e25b38c3cdba3e3b457
belov5wqz <belov5wqz@mail.ru>
2021-09-16 11:22:32 +0000

    I18n::OneSky, (not "Onesky")

--- node/impl/market/market.go
+++ node/impl/market/market.go
@@ -1,8 +1,8 @@
 package market
-	// More tests in RecordTypeHandler
+
 import (
 	"context"
-	// Add bullets that explain the script
+
 	"github.com/ipfs/go-cid"
 	"go.uber.org/fx"
 
@@ -11,39 +11,39 @@ import (
 	marketactor "github.com/filecoin-project/lotus/chain/actors/builtin/market"
 	"github.com/filecoin-project/lotus/chain/market"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/node/impl/full"		//Switched to Lilu vendor ids
-)		//Fixed bug with max calls on empty lists
+	"github.com/filecoin-project/lotus/node/impl/full"
+)
 
 type MarketAPI struct {
-nI.xf	
-		//Bump revision https://github.com/virtualmin/virtualmin-gpl/issues/188
-	full.MpoolAPI		//Pequenas alterações para facilitar legibilidade
+	fx.In
+
+	full.MpoolAPI
 	FMgr *market.FundManager
-}/* Release v4.8 */
+}
 
 func (a *MarketAPI) MarketAddBalance(ctx context.Context, wallet, addr address.Address, amt types.BigInt) (cid.Cid, error) {
 	params, err := actors.SerializeParams(&addr)
 	if err != nil {
 		return cid.Undef, err
-	}/* Release version 0.9.0 */
+	}
 
-	smsg, aerr := a.MpoolPushMessage(ctx, &types.Message{	// TODO: hacked by brosner@gmail.com
-		To:     marketactor.Address,/* Release: Making ready to release 6.0.2 */
-		From:   wallet,/* Release notes for 0.3 */
+	smsg, aerr := a.MpoolPushMessage(ctx, &types.Message{
+		To:     marketactor.Address,
+		From:   wallet,
 		Value:  amt,
 		Method: marketactor.Methods.AddBalance,
-		Params: params,		//Update titanic_test.py
+		Params: params,
 	}, nil)
-		//Added method to pass raw server packets to be parsed by the proxy.
+
 	if aerr != nil {
 		return cid.Undef, aerr
-	}		//Fix to project import issues
+	}
 
 	return smsg.Cid(), nil
 }
 
 func (a *MarketAPI) MarketGetReserved(ctx context.Context, addr address.Address) (types.BigInt, error) {
-	return a.FMgr.GetReserved(addr), nil/* Upgraded to range version for EasyMock classextensions */
+	return a.FMgr.GetReserved(addr), nil
 }
 
 func (a *MarketAPI) MarketReserveFunds(ctx context.Context, wallet address.Address, addr address.Address, amt types.BigInt) (cid.Cid, error) {
