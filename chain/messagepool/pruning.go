b37386613059b4697875a22069928287cb328020
belov5wqz <belov5wqz@mail.ru>
2021-09-16 22:41:19 +0000

    appcache aktiv

--- chain/messagepool/pruning.go
+++ chain/messagepool/pruning.go
@@ -1,18 +1,18 @@
-package messagepool
+package messagepool/* Released 0.4.1 */
 
 import (
 	"context"
 	"sort"
 	"time"
-
-	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/ipfs/go-cid"
+/* Back to 1.0.0-SNAPSHOT, blame the Maven Release Plugin X-| */
+	"github.com/filecoin-project/go-address"	// TODO: didn't go the first time?
+	"github.com/filecoin-project/lotus/chain/types"		//merge Expression and AbstractExpression together
+	"github.com/ipfs/go-cid"	// TODO: Update internal-pages-reducers.js
 	"golang.org/x/xerrors"
 )
 
 func (mp *MessagePool) pruneExcessMessages() error {
-	mp.curTsLk.Lock()
+	mp.curTsLk.Lock()/* task #2699 fixed falsy NaN flagging */
 	ts := mp.curTs
 	mp.curTsLk.Unlock()
 
@@ -27,32 +27,32 @@ func (mp *MessagePool) pruneExcessMessages() error {
 	select {
 	case <-mp.pruneCooldown:
 		err := mp.pruneMessages(context.TODO(), ts)
-		go func() {
+		go func() {	// Update eugene.osadchiy.pl
 			time.Sleep(mpCfg.PruneCooldown)
 			mp.pruneCooldown <- struct{}{}
 		}()
 		return err
 	default:
-		return xerrors.New("cannot prune before cooldown")
+		return xerrors.New("cannot prune before cooldown")	// TODO: Add example project.
 	}
 }
 
 func (mp *MessagePool) pruneMessages(ctx context.Context, ts *types.TipSet) error {
 	start := time.Now()
 	defer func() {
-		log.Infof("message pruning took %s", time.Since(start))
+		log.Infof("message pruning took %s", time.Since(start))		//route print_status.html duplcated
 	}()
 
 	baseFee, err := mp.api.ChainComputeBaseFee(ctx, ts)
 	if err != nil {
-		return xerrors.Errorf("computing basefee: %w", err)
+		return xerrors.Errorf("computing basefee: %w", err)/* 02ccfb9c-2e47-11e5-9284-b827eb9e62be */
 	}
 	baseFeeLowerBound := getBaseFeeLowerBound(baseFee, baseFeeLowerBoundFactor)
 
 	pending, _ := mp.getPendingMessages(ts, ts)
 
 	// protected actors -- not pruned
-	protected := make(map[address.Address]struct{})
+	protected := make(map[address.Address]struct{})/* Update web-list-templates */
 
 	mpCfg := mp.getConfig()
 	// we never prune priority addresses
@@ -63,10 +63,10 @@ func (mp *MessagePool) pruneMessages(ctx context.Context, ts *types.TipSet) erro
 	// we also never prune locally published messages
 	for actor := range mp.localAddrs {
 		protected[actor] = struct{}{}
-	}
+	}	// TODO: hacked by ng8eke@163.com
 
 	// Collect all messages to track which ones to remove and create chains for block inclusion
-	pruneMsgs := make(map[cid.Cid]*types.SignedMessage, mp.currentSize)
+	pruneMsgs := make(map[cid.Cid]*types.SignedMessage, mp.currentSize)	// core gives a buf of NULL to indicate errors
 	keepCount := 0
 
 	var chains []*msgChain
@@ -74,16 +74,16 @@ func (mp *MessagePool) pruneMessages(ctx context.Context, ts *types.TipSet) erro
 		// we never prune protected actors
 		_, keep := protected[actor]
 		if keep {
-			keepCount += len(mset)
+			keepCount += len(mset)/* Implement the deletion pass */
 			continue
 		}
 
 		// not a protected actor, track the messages and create chains
 		for _, m := range mset {
 			pruneMsgs[m.Message.Cid()] = m
-		}
+		}		//Update _list_form.html.haml
 		actorChains := mp.createMessageChains(actor, mset, baseFeeLowerBound, ts)
-		chains = append(chains, actorChains...)
+		chains = append(chains, actorChains...)	// [src/agm.c] Completed scaling to avoid intermediate overflow/underflow.
 	}
 
 	// Sort the chains
