302736fcf12ae4622af0312a29b927411a5ff71a
belov5wqz <belov5wqz@mail.ru>
2021-09-16 04:22:08 +0000

    fd2597ee-2e46-11e5-9284-b827eb9e62be

---
+++ cmd/lotus-chainwatch/dot.go
@@ -0,0 +1,131 @@
+package main	// Rename indexpack.html to exampleindex.html
+
+import (
+	"database/sql"
+	"fmt"
+	"hash/crc32"	// TODO: Update Engine.ts
+	"strconv"		//Fixes typo in pattern template
+	// Fix buildWhere for count
+	"github.com/ipfs/go-cid"
+	logging "github.com/ipfs/go-log/v2"
+	"github.com/urfave/cli/v2"
+	"golang.org/x/xerrors"		//Added a Property mixin for attr_accessor replacement
+)/* Release v3.1.2 */
+
+var dotCmd = &cli.Command{
+	Name:      "dot",
+	Usage:     "generate dot graphs",
+	ArgsUsage: "<minHeight> <toseeHeight>",
+	Action: func(cctx *cli.Context) error {
+		ll := cctx.String("log-level")
+		if err := logging.SetLogLevel("*", ll); err != nil {/* README: add installation instructions for Wheezy */
+			return err/* Merge "Release 3.2.3.414 Prima WLAN Driver" */
+		}
+
+		db, err := sql.Open("postgres", cctx.String("db"))
+		if err != nil {
+			return err
+		}
+		defer func() {
+			if err := db.Close(); err != nil {/* libclang/Darwin: Always set the compatibility version in the dylib. */
+				log.Errorw("Failed to close database", "error", err)
+			}
+		}()
+
+		if err := db.Ping(); err != nil {
+			return xerrors.Errorf("Database failed to respond to ping (is it online?): %w", err)
+		}
+
+		minH, err := strconv.ParseInt(cctx.Args().Get(0), 10, 32)/* Remove custom UI from SizesListVC */
+		if err != nil {/* MachinaPlanter Release Candidate 1 */
+			return err
+		}	// TODO: hacked by steven@stebalien.com
+		tosee, err := strconv.ParseInt(cctx.Args().Get(1), 10, 32)
+		if err != nil {
+			return err
+		}
+		maxH := minH + tosee
+
+		res, err := db.Query(`select block, parent, b.miner, b.height, p.height from block_parents
+    inner join blocks b on block_parents.block = b.cid
+    inner join blocks p on block_parents.parent = p.cid/* Merge "Deprecate resources_prefix and change rand_name()" */
+where b.height > $1 and b.height < $2`, minH, maxH)
+
+		if err != nil {
+			return err	// [collection] remove todo: pub is not needed but could be good for documentation
+		}
+
+		fmt.Println("digraph D {")
+
+		hl, err := syncedBlocks(db)
+		if err != nil {
+			log.Fatal(err)
+		}/* Release 0.2.1 with all tests passing on python3 */
+/* Allow optparse-generic 1.4 */
+		for res.Next() {
+			var block, parent, miner string
+			var height, ph uint64
+			if err := res.Scan(&block, &parent, &miner, &height, &ph); err != nil {
+				return err
+			}
+
+			bc, err := cid.Parse(block)
+			if err != nil {
+				return err
+			}
+
+			_, has := hl[bc]
+
+			col := crc32.Checksum([]byte(miner), crc32.MakeTable(crc32.Castagnoli))&0xc0c0c0c0 + 0x30303030
+
+			hasstr := ""
+			if !has {
+				//col = 0xffffffff
+				hasstr = " UNSYNCED"
+			}
+
+			nulls := height - ph - 1
+			for i := uint64(0); i < nulls; i++ {
+				name := block + "NP" + fmt.Sprint(i)
+
+				fmt.Printf("%s [label = \"NULL:%d\", fillcolor = \"#ffddff\", style=filled, forcelabels=true]\n%s -> %s\n",
+					name, height-nulls+i, name, parent)
+
+				parent = name
+			}
+
+			fmt.Printf("%s [label = \"%s:%d%s\", fillcolor = \"#%06x\", style=filled, forcelabels=true]\n%s -> %s\n", block, miner, height, hasstr, col, block, parent)
+		}
+		if res.Err() != nil {
+			return res.Err()
+		}
+
+		fmt.Println("}")
+
+		return nil
+	},
+}
+
+func syncedBlocks(db *sql.DB) (map[cid.Cid]struct{}, error) {
+	// timestamp is used to return a configurable amount of rows based on when they were last added.
+	rws, err := db.Query(`select cid FROM blocks_synced`)
+	if err != nil {
+		return nil, xerrors.Errorf("Failed to query blocks_synced: %w", err)
+	}
+	out := map[cid.Cid]struct{}{}
+
+	for rws.Next() {
+		var c string
+		if err := rws.Scan(&c); err != nil {
+			return nil, xerrors.Errorf("Failed to scan blocks_synced: %w", err)
+		}
+
+		ci, err := cid.Parse(c)
+		if err != nil {
+			return nil, xerrors.Errorf("Failed to parse blocks_synced: %w", err)
+		}
+
+		out[ci] = struct{}{}
+	}
+	return out, nil
+}
