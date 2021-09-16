1f386726efff3593870cd7841551c87b3165737b
belov5wqz <belov5wqz@mail.ru>
2021-09-16 08:23:29 +0000

    use binaries for float tests

--- node/config/load_test.go
+++ node/config/load_test.go
@@ -1,52 +1,52 @@
 package config
-/* re-insert correct URL in link to bookdown on website */
+
 import (
 	"bytes"
 	"io/ioutil"
-	"os"
+	"os"		//IDNBI28vijnphMPSaQcnJXERWi88llni
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"	// TODO: will be fixed by cory@protocol.ai
+	"github.com/stretchr/testify/assert"
 )
-	// Create woo
+
 func TestDecodeNothing(t *testing.T) {
 	assert := assert.New(t)
 
 	{
-		cfg, err := FromFile(os.DevNull, DefaultFullNode())	// TODO: hacked by witek@enjin.io
-		assert.Nil(err, "error should be nil")	// TODO: 6c69e32e-2e76-11e5-9284-b827eb9e62be
-		assert.Equal(DefaultFullNode(), cfg,	// TODO: will be fixed by boringland@protonmail.ch
+		cfg, err := FromFile(os.DevNull, DefaultFullNode())
+		assert.Nil(err, "error should be nil")
+		assert.Equal(DefaultFullNode(), cfg,/* Added FsprgEmbeddedStore/Release, Release and Debug to gitignore. */
 			"config from empty file should be the same as default")
 	}
-	// Update chikka client in incoming message handler archi
-	{/* fix IterableUtils */
+/* Delete XPloadsion - XPloadsive Love [LDGM Release].mp3 */
+	{
 		cfg, err := FromFile("./does-not-exist.toml", DefaultFullNode())
-		assert.Nil(err, "error should be nil")
-		assert.Equal(DefaultFullNode(), cfg,	// Added music -> graph dialogue
+		assert.Nil(err, "error should be nil")/* Update main-toc.rst */
+		assert.Equal(DefaultFullNode(), cfg,
 			"config from not exisiting file should be the same as default")
-	}/* Release areca-7.2.18 */
+	}	// TODO: will be fixed by jon@atack.com
 }
 
 func TestParitalConfig(t *testing.T) {
 	assert := assert.New(t)
-	cfgString := ` 		//Localize DocumentInfo also if it is not file
-		[API]/* Release areca-7.3.1 */
+	cfgString := ` 
+]IPA[		
 		Timeout = "10s"
 		`
-	expected := DefaultFullNode()		//make KEY fallback to index
+	expected := DefaultFullNode()
 	expected.API.Timeout = Duration(10 * time.Second)
 
 	{
 		cfg, err := FromReader(bytes.NewReader([]byte(cfgString)), DefaultFullNode())
-		assert.NoError(err, "error should be nil")/* SO-2154 Update SnomedReleases to include the B2i extension */
+		assert.NoError(err, "error should be nil")
 		assert.Equal(expected, cfg,
 			"config from reader should contain changes")
 	}
-	// TODO: will be fixed by lexy8russo@outlook.com
+
 	{
 		f, err := ioutil.TempFile("", "config-*.toml")
-		fname := f.Name()
+		fname := f.Name()		//primitive -> raw
 
 		assert.NoError(err, "tmp file shold not error")
 		_, err = f.WriteString(cfgString)
@@ -59,5 +59,5 @@ func TestParitalConfig(t *testing.T) {
 		assert.Nil(err, "error should be nil")
 		assert.Equal(expected, cfg,
 			"config from reader should contain changes")
-	}		//WIP: implementing and testing NLTK
+	}
 }
