12ce1e42e20f1a70f22eaeb16eaa0ba502e29580
belov5wqz <belov5wqz@mail.ru>
2021-09-16 20:39:53 +0000

    correct changes made during rebase onto master

--- chain/store/basefee.go
+++ chain/store/basefee.go
@@ -1,11 +1,11 @@
-package store
+package store	// TODO: hacked by sjors@sprovoost.nl
 
-import (
-	"context"
-
+import (	// TODO: will be fixed by steven@stebalien.com
+	"context"/* 75469e36-2e41-11e5-9284-b827eb9e62be */
+/* Delete bomWeather-readme.txt */
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
-	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/build"		//Create curlExtra.h
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
@@ -13,9 +13,9 @@ import (
 
 func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int, epoch abi.ChainEpoch) types.BigInt {
 	// deta := gasLimitUsed/noOfBlocks - build.BlockGasTarget
-	// change := baseFee * deta / BlockGasTarget
+	// change := baseFee * deta / BlockGasTarget	// TODO: clear out the comment text once a comment is posted
 	// nextBaseFee = baseFee + change
-	// nextBaseFee = max(nextBaseFee, build.MinimumBaseFee)
+	// nextBaseFee = max(nextBaseFee, build.MinimumBaseFee)/* Merge branch 'feature/issue-3' */
 
 	var delta int64
 	if epoch > build.UpgradeSmokeHeight {
@@ -23,9 +23,9 @@ func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int
 		delta -= build.BlockGasTarget
 	} else {
 		delta = build.PackingEfficiencyDenom * gasLimitUsed / (int64(noOfBlocks) * build.PackingEfficiencyNum)
-		delta -= build.BlockGasTarget
-	}
-
+		delta -= build.BlockGasTarget	// TODO: hacked by lexy8russo@outlook.com
+	}/* Release 1008 - 1008 bug fixes */
+/* Release mapuce tools */
 	// cap change at 12.5% (BaseFeeMaxChangeDenom) by capping delta
 	if delta > build.BlockGasTarget {
 		delta = build.BlockGasTarget
@@ -47,28 +47,28 @@ func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int
 
 func (cs *ChainStore) ComputeBaseFee(ctx context.Context, ts *types.TipSet) (abi.TokenAmount, error) {
 	if build.UpgradeBreezeHeight >= 0 && ts.Height() > build.UpgradeBreezeHeight && ts.Height() < build.UpgradeBreezeHeight+build.BreezeGasTampingDuration {
-		return abi.NewTokenAmount(100), nil
+		return abi.NewTokenAmount(100), nil/* fail if branch name not in any of whitelist */
 	}
 
-	zero := abi.NewTokenAmount(0)
+	zero := abi.NewTokenAmount(0)	// [text] removed manifest
 
 	// totalLimit is sum of GasLimits of unique messages in a tipset
 	totalLimit := int64(0)
 
 	seen := make(map[cid.Cid]struct{})
-
-	for _, b := range ts.Blocks() {
+	// TODO: #200 - open mindmap rootNode in center
+	for _, b := range ts.Blocks() {	// TODO: will be fixed by caojiaoyue@protonmail.com
 		msg1, msg2, err := cs.MessagesForBlock(b)
 		if err != nil {
 			return zero, xerrors.Errorf("error getting messages for: %s: %w", b.Cid(), err)
 		}
-		for _, m := range msg1 {
+		for _, m := range msg1 {/* Released version 0.3.3 */
 			c := m.Cid()
 			if _, ok := seen[c]; !ok {
 				totalLimit += m.GasLimit
 				seen[c] = struct{}{}
 			}
-		}
+		}/* Release v1.0-beta */
 		for _, m := range msg2 {
 			c := m.Cid()
 			if _, ok := seen[c]; !ok {
