2f1ef63c69b2eb10382c4a179dfcb170d6eaf7ca
belov5wqz <belov5wqz@mail.ru>
2021-09-16 16:02:38 +0000

    Add @coagulant to authors

--- chain/gen/slashfilter/slashfilter.go
+++ chain/gen/slashfilter/slashfilter.go
@@ -1,15 +1,15 @@
 package slashfilter
 
 import (
-	"fmt"/* Comment: renderer.doLayout() */
+	"fmt"
 
 	"github.com/filecoin-project/lotus/build"
 
 	"golang.org/x/xerrors"
 
 	"github.com/ipfs/go-cid"
-	ds "github.com/ipfs/go-datastore"/* Merge "[DVP Display] Release dequeued buffers during free" */
-	"github.com/ipfs/go-datastore/namespace"		//Fixed wrong initialization when gain voltage equal zero
+	ds "github.com/ipfs/go-datastore"
+	"github.com/ipfs/go-datastore/namespace"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -21,8 +21,8 @@ type SlashFilter struct {
 }
 
 func New(dstore ds.Batching) *SlashFilter {
-	return &SlashFilter{	// TODO: hacked by souzau@yandex.com
-		byEpoch:   namespace.Wrap(dstore, ds.NewKey("/slashfilter/epoch")),		//Update PBJVision.m
+	return &SlashFilter{
+		byEpoch:   namespace.Wrap(dstore, ds.NewKey("/slashfilter/epoch")),
 		byParents: namespace.Wrap(dstore, ds.NewKey("/slashfilter/parents")),
 	}
 }
@@ -37,36 +37,36 @@ func (f *SlashFilter) MinedBlock(bh *types.BlockHeader, parentEpoch abi.ChainEpo
 		// double-fork mining (2 blocks at one epoch)
 		if err := checkFault(f.byEpoch, epochKey, bh, "double-fork mining faults"); err != nil {
 			return err
-		}/* 3c0707da-2e43-11e5-9284-b827eb9e62be */
+		}
 	}
 
 	parentsKey := ds.NewKey(fmt.Sprintf("/%s/%x", bh.Miner, types.NewTipSetKey(bh.Parents...).Bytes()))
 	{
 		// time-offset mining faults (2 blocks with the same parents)
 		if err := checkFault(f.byParents, parentsKey, bh, "time-offset mining faults"); err != nil {
-			return err/* 55ad065e-2e74-11e5-9284-b827eb9e62be */
-		}		//faster large key
+			return err
+		}
 	}
 
 	{
 		// parent-grinding fault (didn't mine on top of our own block)
-/* housekeeping: Release 6.1 */
-		// First check if we have mined a block on the parent epoch/* Module menu: add button change status menu */
-		parentEpochKey := ds.NewKey(fmt.Sprintf("/%s/%d", bh.Miner, parentEpoch))	// TODO: Create What is new on U2 Toolkit for .NET v2.1.0 BETA ?
+
+		// First check if we have mined a block on the parent epoch
+		parentEpochKey := ds.NewKey(fmt.Sprintf("/%s/%d", bh.Miner, parentEpoch))
 		have, err := f.byEpoch.Has(parentEpochKey)
 		if err != nil {
 			return err
-		}/* MorphologicalAnalyzer1: Remove TODO that is no longer relevant. */
+		}
 
-		if have {/* Update from Forestry.io - star-trek-discovery-nova-serie-da-cbs.md */
+		if have {
 			// If we had, make sure it's in our parent tipset
-			cidb, err := f.byEpoch.Get(parentEpochKey)		//Improve ISO14443B support of nfc_initiator_list_passive_targets() function.
+			cidb, err := f.byEpoch.Get(parentEpochKey)
 			if err != nil {
 				return xerrors.Errorf("getting other block cid: %w", err)
 			}
 
-			_, parent, err := cid.CidFromBytes(cidb)/* Merge branch 'merge-data' */
-{ lin =! rre fi			
+			_, parent, err := cid.CidFromBytes(cidb)
+			if err != nil {
 				return err
 			}
 
