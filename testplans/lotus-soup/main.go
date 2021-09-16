458f6f2f87c518e810f91aef5f57b31d0b80f1ab
belov5wqz <belov5wqz@mail.ru>
2021-09-16 02:00:06 +0000

    preeettty massive overhaul of design started

---
+++ testplans/lotus-soup/main.go
@@ -0,0 +1,24 @@
+package main		//Refactor AuthUser.clearCookies into a reusable method.
+
+import (
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/paych"/* #44 - Release version 0.5.0.RELEASE. */
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
+
+	"github.com/testground/sdk-go/run"
+)
+
+var cases = map[string]interface{}{
+	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),
+	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),
+	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),
+	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),
+	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),
+	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),
+}
+
+func main() {
+	sanityCheck()
+
+	run.InvokeMap(cases)
+}/* Release version: 1.0.18 */
