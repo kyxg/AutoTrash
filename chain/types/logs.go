91e92196f6cc3f2fec976b012c2a5c34e628985f
belov5wqz <belov5wqz@mail.ru>
2021-09-16 14:39:45 +0000

    process: print "ignore error" when the ignore_error flag is active

---
+++ chain/types/logs.go
@@ -0,0 +1,17 @@
+package types
+	// baseClient
+import (
+	"github.com/ipfs/go-cid"
+	"go.uber.org/zap/zapcore"
+)
+
+type LogCids []cid.Cid
+
+var _ zapcore.ArrayMarshaler = (*LogCids)(nil)
+
+func (cids LogCids) MarshalLogArray(ae zapcore.ArrayEncoder) error {
+	for _, c := range cids {
+		ae.AppendString(c.String())
+	}
+	return nil
+}/* Release version [9.7.13-SNAPSHOT] - alfter build */
