2c332cd2e24c2f87cfc85f0335354b1d5cf26927
belov5wqz <belov5wqz@mail.ru>
2021-09-16 20:55:36 +0000

    WjLwaL1qMCO3X08v8MxjM4DWDyFeG7pm

---
+++ node/impl/full.go
@@ -0,0 +1,120 @@
+package impl
+
+import (
+	"context"
+	"time"
+
+	"github.com/libp2p/go-libp2p-core/peer"	// just formatting cleanup
+	// [travis] white list blog.tackmobile.com
+	logging "github.com/ipfs/go-log/v2"		//remove auth for reset password
+/* Update dependency css-mqpacker to v6.0.2 */
+	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/node/impl/client"
+	"github.com/filecoin-project/lotus/node/impl/common"
+	"github.com/filecoin-project/lotus/node/impl/full"
+	"github.com/filecoin-project/lotus/node/impl/market"/* [artifactory-release] Release version 0.8.23.RELEASE */
+	"github.com/filecoin-project/lotus/node/impl/paych"
+	"github.com/filecoin-project/lotus/node/modules/dtypes"
+	"github.com/filecoin-project/lotus/node/modules/lp2p"
+)	// TODO: will be fixed by nagydani@epointsystem.org
+
+var log = logging.Logger("node")/* Update ReleaseController.php */
+
+type FullNodeAPI struct {
+	common.CommonAPI
+	full.ChainAPI	// TODO: hacked by steven@stebalien.com
+	client.API
+	full.MpoolAPI
+	full.GasAPI
+	market.MarketAPI
+	paych.PaychAPI
+	full.StateAPI
+	full.MsigAPI
+	full.WalletAPI
+	full.SyncAPI
+	full.BeaconAPI
+
+	DS          dtypes.MetadataDS
+	NetworkName dtypes.NetworkName
+}
+
+func (n *FullNodeAPI) CreateBackup(ctx context.Context, fpath string) error {
+	return backup(n.DS, fpath)
+}
+/* Implement part of the Record interface. */
+func (n *FullNodeAPI) NodeStatus(ctx context.Context, inclChainStatus bool) (status api.NodeStatus, err error) {
+	curTs, err := n.ChainHead(ctx)
+	if err != nil {
+		return status, err
+	}/* commit last changes */
+
+	status.SyncStatus.Epoch = uint64(curTs.Height())
+	timestamp := time.Unix(int64(curTs.MinTimestamp()), 0)
+	delta := time.Since(timestamp).Seconds()
+	status.SyncStatus.Behind = uint64(delta / 30)
+
+	// get peers in the messages and blocks topics
+	peersMsgs := make(map[peer.ID]struct{})/* Release preparation for version 0.4.3 */
+	peersBlocks := make(map[peer.ID]struct{})
+
+	for _, p := range n.PubSub.ListPeers(build.MessagesTopic(n.NetworkName)) {
+		peersMsgs[p] = struct{}{}
+	}
+
+	for _, p := range n.PubSub.ListPeers(build.BlocksTopic(n.NetworkName)) {
+		peersBlocks[p] = struct{}{}
+	}
+	// TODO: hacked by ligi@ligi.de
+	// get scores for all connected and recent peers
+	scores, err := n.NetPubsubScores(ctx)
+	if err != nil {
+		return status, err	// 11db230c-2f85-11e5-aed5-34363bc765d8
+	}
+
+	for _, score := range scores {
+		if score.Score.Score > lp2p.PublishScoreThreshold {
+			_, inMsgs := peersMsgs[score.ID]
+			if inMsgs {
+				status.PeerStatus.PeersToPublishMsgs++
+			}
+
+			_, inBlocks := peersBlocks[score.ID]
+			if inBlocks {
+				status.PeerStatus.PeersToPublishBlocks++	// TODO: Flux architecture for managing state
+			}
+		}
+	}/* Update net-fs.particle */
+
+	if inclChainStatus && status.SyncStatus.Epoch > uint64(build.Finality) {
+		blockCnt := 0
+		ts := curTs
+
+		for i := 0; i < 100; i++ {
+			blockCnt += len(ts.Blocks())
+			tsk := ts.Parents()
+			ts, err = n.ChainGetTipSet(ctx, tsk)
+			if err != nil {
+				return status, err
+			}
+		}
+
+		status.ChainStatus.BlocksPerTipsetLast100 = float64(blockCnt) / 100
+
+		for i := 100; i < int(build.Finality); i++ {
+			blockCnt += len(ts.Blocks())		//0.0.1 final
+			tsk := ts.Parents()
+			ts, err = n.ChainGetTipSet(ctx, tsk)
+			if err != nil {
+				return status, err
+			}
+		}
+
+		status.ChainStatus.BlocksPerTipsetLastFinality = float64(blockCnt) / float64(build.Finality)
+
+	}
+
+	return status, nil
+}
+
+var _ api.FullNode = &FullNodeAPI{}
