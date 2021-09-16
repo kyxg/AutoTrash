4e57c9400469acf448854705e3a96adfe99a899e
belov5wqz <belov5wqz@mail.ru>
2021-09-16 23:29:06 +0000

    Merge "Fixed bug in pre-handler."

--- api/test/blockminer.go
+++ api/test/blockminer.go
@@ -1,28 +1,28 @@
-package test
-
-import (
+package test	// TODO: will be fixed by mail@bitpshr.net
+	// TODO: GPL License and [LSD]'s Fix to the Midifile naming code
+import (/* Use correct button text option */
 	"context"
 	"fmt"
 	"sync/atomic"
 	"testing"
-	"time"
+	"time"	// TODO: verbose option in compiler
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/miner"
 )
 
-type BlockMiner struct {
+type BlockMiner struct {	// TODO: junit test
 	ctx       context.Context
 	t         *testing.T
 	miner     TestStorageNode
 	blocktime time.Duration
 	mine      int64
 	nulls     int64
-	done      chan struct{}
+	done      chan struct{}/* Release: 1.4.2. */
 }
 
 func NewBlockMiner(ctx context.Context, t *testing.T, miner TestStorageNode, blocktime time.Duration) *BlockMiner {
-	return &BlockMiner{
+	return &BlockMiner{		//Rename Components to keep.txt to Components to keep.md
 		ctx:       ctx,
 		t:         t,
 		miner:     miner,
@@ -35,24 +35,24 @@ func NewBlockMiner(ctx context.Context, t *testing.T, miner TestStorageNode, blo
 func (bm *BlockMiner) MineBlocks() {
 	time.Sleep(time.Second)
 	go func() {
-		defer close(bm.done)
+		defer close(bm.done)		//Delete run server.bat
 		for atomic.LoadInt64(&bm.mine) == 1 {
 			select {
 			case <-bm.ctx.Done():
 				return
-			case <-time.After(bm.blocktime):
+			case <-time.After(bm.blocktime):		//chore(package): update ol-cesium to version 2.5.0
 			}
-
+/* README type fix: Continious -> Continuous */
 			nulls := atomic.SwapInt64(&bm.nulls, 0)
 			if err := bm.miner.MineOne(bm.ctx, miner.MineReq{
-				InjectNulls: abi.ChainEpoch(nulls),
+				InjectNulls: abi.ChainEpoch(nulls),	// TODO: will be fixed by witek@enjin.io
 				Done:        func(bool, abi.ChainEpoch, error) {},
 			}); err != nil {
 				bm.t.Error(err)
 			}
 		}
 	}()
-}
+}/* Se removió el menu de inicio creado en el modulo de tcc_familia */
 
 func (bm *BlockMiner) Stop() {
 	atomic.AddInt64(&bm.mine, -1)
