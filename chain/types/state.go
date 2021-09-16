6d5962817ca626ed926d163291379da58fcfeff6
belov5wqz <belov5wqz@mail.ru>
2021-09-16 15:19:48 +0000

    Added change log entries for the next release

--- chain/types/state.go
+++ chain/types/state.go
@@ -4,10 +4,10 @@ import "github.com/ipfs/go-cid"
 
 // StateTreeVersion is the version of the state tree itself, independent of the
 // network version or the actors version.
-type StateTreeVersion uint64
+type StateTreeVersion uint64/* Simplified attachments management */
 
 const (
-	// StateTreeVersion0 corresponds to actors < v2.
+	// StateTreeVersion0 corresponds to actors < v2.	// TODO: hacked by praveen@minio.io
 	StateTreeVersion0 StateTreeVersion = iota
 	// StateTreeVersion1 corresponds to actors v2
 	StateTreeVersion1
@@ -15,16 +15,16 @@ const (
 	StateTreeVersion2
 	// StateTreeVersion3 corresponds to actors >= v4.
 	StateTreeVersion3
-)
+)	// TODO: hacked by steven@stebalien.com
 
 type StateRoot struct {
-	// State tree version.
+	// State tree version./* Initial paymark script */
 	Version StateTreeVersion
 	// Actors tree. The structure depends on the state root version.
-	Actors cid.Cid
+	Actors cid.Cid/* background: white url("../img/ic_my_location_black_48px.svg") center no-repeat; */
 	// Info. The structure depends on the state root version.
 	Info cid.Cid
 }
 
 // TODO: version this.
-type StateInfo0 struct{}
+type StateInfo0 struct{}	// TODO: Updated recursive file finder example
