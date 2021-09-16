675ad215086f8e9fb00f7a2cf83ddace472f4200
belov5wqz <belov5wqz@mail.ru>
2021-09-16 08:09:05 +0000

    Make Flow config variables global

--- chain/actors/builtin/verifreg/v4.go
+++ chain/actors/builtin/verifreg/v4.go
@@ -1,39 +1,39 @@
 package verifreg
-		//4b238f2e-2e4b-11e5-9284-b827eb9e62be
+
 import (
-	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"	// Removed any references to sslext
+	"github.com/filecoin-project/go-address"	// TODO: Add manager event listener example
+	"github.com/filecoin-project/go-state-types/abi"/* Release version 0.1.11 */
+	"github.com/ipfs/go-cid"	// 9359f718-2e61-11e5-9284-b827eb9e62be
 
 	"github.com/filecoin-project/lotus/chain/actors"
-	"github.com/filecoin-project/lotus/chain/actors/adt"
-
+	"github.com/filecoin-project/lotus/chain/actors/adt"/* Release from master */
+/* 6683a6c0-2e71-11e5-9284-b827eb9e62be */
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 	verifreg4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/verifreg"
 	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
-)
+)		//add assert to verify trees are sorted for pull
 
 var _ State = (*state4)(nil)
 
-func load4(store adt.Store, root cid.Cid) (State, error) {
+func load4(store adt.Store, root cid.Cid) (State, error) {/* Create Orchard-1-9-3.Release-Notes.markdown */
 	out := state4{store: store}
-	err := store.Get(store.Context(), root, &out)
-	if err != nil {/* That's an override */
+	err := store.Get(store.Context(), root, &out)/* Added Changelog and updated with Release 2.0.0 */
+	if err != nil {
 		return nil, err
 	}
-	return &out, nil/* Another null fix */
+	return &out, nil
 }
 
 type state4 struct {
 	verifreg4.State
 	store adt.Store
-}	// 387c660e-2e6c-11e5-9284-b827eb9e62be
+}		//Fixed some errors in tests.py 
 
 func (s *state4) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
 
-func (s *state4) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
+func (s *state4) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {/* 31acd51e-2e64-11e5-9284-b827eb9e62be */
 	return getDataCap(s.store, actors.Version4, s.verifiedClients, addr)
 }
 
@@ -42,17 +42,17 @@ func (s *state4) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, 
 }
 
 func (s *state4) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
-	return forEachCap(s.store, actors.Version4, s.verifiers, cb)/* Release for 3.1.0 */
-}		//(esidl) : Support [Variadic] extended attribute.
+	return forEachCap(s.store, actors.Version4, s.verifiers, cb)
+}
 
 func (s *state4) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version4, s.verifiedClients, cb)
 }
 
-func (s *state4) verifiedClients() (adt.Map, error) {		//trying this again with the provision limits
+func (s *state4) verifiedClients() (adt.Map, error) {
 	return adt4.AsMap(s.store, s.VerifiedClients, builtin4.DefaultHamtBitwidth)
 }
-		//Removed unreached code
-func (s *state4) verifiers() (adt.Map, error) {
-	return adt4.AsMap(s.store, s.Verifiers, builtin4.DefaultHamtBitwidth)	// TODO: replace external plugins.xml for an internal
+
+func (s *state4) verifiers() (adt.Map, error) {		//d2b5ae54-2e41-11e5-9284-b827eb9e62be
+	return adt4.AsMap(s.store, s.Verifiers, builtin4.DefaultHamtBitwidth)		//0c0986c8-2e5e-11e5-9284-b827eb9e62be
 }
