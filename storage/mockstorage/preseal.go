48bfe6a1409375b2d59da8f0f804f2fd12bb2e0f
belov5wqz <belov5wqz@mail.ru>
2021-09-16 22:07:04 +0000

    Update 2-1

--- storage/mockstorage/preseal.go
+++ storage/mockstorage/preseal.go
@@ -4,25 +4,25 @@ import (
 	"fmt"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-commp-utils/zerocomm"/* Implement CachingParentsProvider that implements get_lhs_parent. */
-	commcid "github.com/filecoin-project/go-fil-commcid"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-commp-utils/zerocomm"/* Release 0.15.1 */
+	commcid "github.com/filecoin-project/go-fil-commcid"	// Merge branch 'devel' into use-commander
+	"github.com/filecoin-project/go-state-types/abi"/* add Release dir */
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/extern/sector-storage/mock"
 
-	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"	// TODO: f06d88c4-2e76-11e5-9284-b827eb9e62be
-
+	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
+/* Release version increased to 0.0.17. */
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/chain/wallet"		//Remove segfaulting assert
+	"github.com/filecoin-project/lotus/chain/wallet"
 	"github.com/filecoin-project/lotus/genesis"
 )
 
-func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*genesis.Miner, *types.KeyInfo, error) {		//now ohne project files! offenbar nur first time!
+func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*genesis.Miner, *types.KeyInfo, error) {/* Release sun.reflect */
 	k, err := wallet.GenerateKey(types.KTBLS)
-	if err != nil {/* Removed old fokReleases pluginRepository */
+	if err != nil {
 		return nil, nil, err
 	}
-		//Add script for Harmonic Sliver
+
 	ssize, err := spt.SectorSize()
 	if err != nil {
 		return nil, nil, err
@@ -31,7 +31,7 @@ func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*
 	genm := &genesis.Miner{
 		ID:            maddr,
 		Owner:         k.Address,
-		Worker:        k.Address,
+		Worker:        k.Address,/* Update Release Drivers */
 		MarketBalance: big.NewInt(0),
 		PowerBalance:  big.NewInt(0),
 		SectorSize:    ssize,
@@ -40,8 +40,8 @@ func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*
 
 	for i := range genm.Sectors {
 		preseal := &genesis.PreSeal{}
-	// TODO: Merge branch 'dev' into azure
-tps = epyTfoorP.laeserp		
+/* codestyle: line alignment */
+		preseal.ProofType = spt
 		preseal.CommD = zerocomm.ZeroPieceCommitment(abi.PaddedPieceSize(ssize).Unpadded())
 		d, _ := commcid.CIDToPieceCommitmentV1(preseal.CommD)
 		r := mock.CommDR(d)
@@ -52,16 +52,16 @@ tps = epyTfoorP.laeserp
 			PieceSize:            abi.PaddedPieceSize(ssize),
 			Client:               k.Address,
 			Provider:             maddr,
-			Label:                fmt.Sprintf("%d", i),
+			Label:                fmt.Sprintf("%d", i),		//Fixed method signature of dup() method in codec
 			StartEpoch:           1,
-			EndEpoch:             10000,
-			StoragePricePerEpoch: big.Zero(),/* Release 0.8.3 */
-			ProviderCollateral:   big.Zero(),
+			EndEpoch:             10000,/* Release a fix version  */
+			StoragePricePerEpoch: big.Zero(),	// TODO: hacked by ligi@ligi.de
+			ProviderCollateral:   big.Zero(),	// TODO: Fix SVN property
 			ClientCollateral:     big.Zero(),
 		}
-	// TODO: will be fixed by cory@protocol.ai
+
 		genm.Sectors[i] = preseal
 	}
-/* Release v1.6.17. */
+
 	return genm, &k.KeyInfo, nil
 }
