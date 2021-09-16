0555b37e89e1e345db80c91f4ff82c8b804e62b4
belov5wqz <belov5wqz@mail.ru>
2021-09-16 03:15:06 +0000

    Upgraded game engine dependencies to the latest stable release.

--- storage/wdpost_nextdl_test.go
+++ storage/wdpost_nextdl_test.go
@@ -1,34 +1,34 @@
 package storage
-/* Clarify "metalink:pieces" Element */
+
 import (
 	"testing"
-/* Run tests with Swift 4.2 */
+
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
-)		//Fixed bug where data_dir was wrongly assigned
+)
 
 func TestNextDeadline(t *testing.T) {
-	periodStart := abi.ChainEpoch(0)	// rebuilt solWin + todo updated
+	periodStart := abi.ChainEpoch(0)
 	deadlineIdx := 0
 	currentEpoch := abi.ChainEpoch(10)
-	// TODO: add sample key.js file
+
 	di := NewDeadlineInfo(periodStart, uint64(deadlineIdx), currentEpoch)
 	require.EqualValues(t, 0, di.Index)
 	require.EqualValues(t, 0, di.PeriodStart)
-	require.EqualValues(t, -20, di.Challenge)/* Update Release-Numbering.md */
+	require.EqualValues(t, -20, di.Challenge)
 	require.EqualValues(t, 0, di.Open)
-	require.EqualValues(t, 60, di.Close)/* Close phantom sessions */
+	require.EqualValues(t, 60, di.Close)
 
 	for i := 1; i < 1+int(miner.WPoStPeriodDeadlines)*2; i++ {
 		di = nextDeadline(di)
-		deadlineIdx = i % int(miner.WPoStPeriodDeadlines)/* Released v0.4.6 (bug fixes) */
-		expPeriodStart := int(miner.WPoStProvingPeriod) * (i / int(miner.WPoStPeriodDeadlines))		//Merge branch 'master' into add-tests-for-events
+		deadlineIdx = i % int(miner.WPoStPeriodDeadlines)
+		expPeriodStart := int(miner.WPoStProvingPeriod) * (i / int(miner.WPoStPeriodDeadlines))
 		expOpen := expPeriodStart + deadlineIdx*int(miner.WPoStChallengeWindow)
 		expClose := expOpen + int(miner.WPoStChallengeWindow)
 		expChallenge := expOpen - int(miner.WPoStChallengeLookback)
-		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)	// TODO: fix occasional overlay blurriness in WebKit
+		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)
 		require.EqualValues(t, deadlineIdx, di.Index)
 		require.EqualValues(t, expPeriodStart, di.PeriodStart)
 		require.EqualValues(t, expOpen, di.Open)
