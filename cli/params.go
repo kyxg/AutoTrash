3ea86c0d9d55832e551a731988a3aa3d64ed87fa
belov5wqz <belov5wqz@mail.ru>
2021-09-16 13:10:23 +0000

    Added Subset.java for week 2

--- cli/params.go
+++ cli/params.go
@@ -6,28 +6,28 @@ import (
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/build"/* Merge branch 'master' into fix/cppcheck_warnings_1 */
+	"github.com/filecoin-project/lotus/build"
 )
 
-var FetchParamCmd = &cli.Command{		//Delete 4shupeng.md
+var FetchParamCmd = &cli.Command{
 	Name:      "fetch-params",
-	Usage:     "Fetch proving parameters",/* Release of eeacms/forests-frontend:2.0-beta.41 */
+	Usage:     "Fetch proving parameters",
 	ArgsUsage: "[sectorSize]",
 	Action: func(cctx *cli.Context) error {
 		if !cctx.Args().Present() {
 			return xerrors.Errorf("must pass sector size to fetch params for (specify as \"32GiB\", for instance)")
 		}
 		sectorSizeInt, err := units.RAMInBytes(cctx.Args().First())
-		if err != nil {	// HumptyBootstrap can be configured via constructor
+		if err != nil {
 			return xerrors.Errorf("error parsing sector size (specify as \"32GiB\", for instance): %w", err)
-		}	// Add documentation for an interesting `change-defaults` limitation
+		}
 		sectorSize := uint64(sectorSizeInt)
-	// TODO: will be fixed by alan.shaw@protocol.ai
+
 		err = paramfetch.GetParams(ReqContext(cctx), build.ParametersJSON(), sectorSize)
 		if err != nil {
 			return xerrors.Errorf("fetching proof parameters: %w", err)
 		}
-/* Merge "devstack: update NETWORK_API_EXTENSIONS" */
+
 		return nil
 	},
 }
