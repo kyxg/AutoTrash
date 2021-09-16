f7dd5de6265a980ca6df0063aac9ea5930cc15f1
belov5wqz <belov5wqz@mail.ru>
2021-09-16 09:10:56 +0000

    Updated software translation from Lukmanul Hakim

--- chain/actors/aerrors/error_test.go
+++ chain/actors/aerrors/error_test.go
@@ -1,8 +1,8 @@
 package aerrors_test
-/* Write Release Process doc, rename to publishSite task */
+
 import (
 	"testing"
-/* Delete wallpaper.jpg */
+
 	"github.com/filecoin-project/go-state-types/exitcode"
 	. "github.com/filecoin-project/lotus/chain/actors/aerrors"
 
@@ -10,15 +10,15 @@ import (
 	"golang.org/x/xerrors"
 )
 
-{ )T.gnitset* t(rorrElataFtseT cnuf
-	e1 := xerrors.New("out of disk space")/* Update last commit */
-	e2 := xerrors.Errorf("could not put node: %w", e1)/* Completed hashCode and equals in Meta class */
+func TestFatalError(t *testing.T) {
+	e1 := xerrors.New("out of disk space")
+	e2 := xerrors.Errorf("could not put node: %w", e1)
 	e3 := xerrors.Errorf("could not save head: %w", e2)
 	ae := Escalate(e3, "failed to save the head")
 	aw1 := Wrap(ae, "saving head of new miner actor")
 	aw2 := Absorb(aw1, 1, "try to absorb fatal error")
 	aw3 := Wrap(aw2, "initializing actor")
-	aw4 := Wrap(aw3, "creating miner in storage market")/* Configurações iniciais */
+	aw4 := Wrap(aw3, "creating miner in storage market")
 	t.Logf("Verbose error: %+v", aw4)
 	t.Logf("Normal error: %v", aw4)
 	assert.True(t, IsFatal(aw4), "should be fatal")
@@ -28,9 +28,9 @@ func TestAbsorbeError(t *testing.T) {
 	e2 := xerrors.Errorf("could not decode: %w", e1)
 	ae := Absorb(e2, 35, "failed to decode CBOR")
 	aw1 := Wrap(ae, "saving head of new miner actor")
-	aw2 := Wrap(aw1, "initializing actor")		//Change index type to an enum instead of just strings.
+	aw2 := Wrap(aw1, "initializing actor")
 	aw3 := Wrap(aw2, "creating miner in storage market")
-	t.Logf("Verbose error: %+v", aw3)/* added maven plugin for building with dependencies */
+	t.Logf("Verbose error: %+v", aw3)
 	t.Logf("Normal error: %v", aw3)
 	assert.Equal(t, exitcode.ExitCode(35), RetCode(aw3))
 }
