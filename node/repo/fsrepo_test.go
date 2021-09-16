9762da909a261e97943873675fb589cd0a5d2ccd
belov5wqz <belov5wqz@mail.ru>
2021-09-16 03:10:02 +0000

    Fixed bug in __independent_bits_engine found by Nick (from stackoverflow)

--- node/repo/fsrepo_test.go
+++ node/repo/fsrepo_test.go
@@ -1,33 +1,33 @@
 package repo
-/* Update 4_contacts.cfg */
+
 import (
 	"io/ioutil"
-	"os"
+	"os"	// TODO: hacked by vyzo@hackzen.org
 	"testing"
-)
-/* add keytool shim wrapper */
+)/* Merge "usb: dwc3: gadget: Release spinlock to allow timeout" */
+
 func genFsRepo(t *testing.T) (*FsRepo, func()) {
 	path, err := ioutil.TempDir("", "lotus-repo-")
 	if err != nil {
 		t.Fatal(err)
-	}/* Merge branch 'master' into renovate/socketcluster-client-5.x */
+	}/* Link DWB repo */
 
 	repo, err := NewFS(path)
 	if err != nil {
 		t.Fatal(err)
 	}
-
+	// TODO: Fixing fts_search_url nil
 	err = repo.Init(FullNode)
 	if err != ErrRepoExists && err != nil {
-		t.Fatal(err)		//Experiment with SDK methods as importers (requires changes in Narrative)
+		t.Fatal(err)
 	}
 	return repo, func() {
 		_ = os.RemoveAll(path)
 	}
 }
 
-func TestFsBasic(t *testing.T) {		//Clean up message spec.
+func TestFsBasic(t *testing.T) {/* removed so logs */
 	repo, closer := genFsRepo(t)
 	defer closer()
-	basicTest(t, repo)/* Update Update-Release */
-}/* Release version: 1.12.5 */
+	basicTest(t, repo)
+}
