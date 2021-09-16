b21ef96f733390d5d70012f067879f1c23d31c1b
belov5wqz <belov5wqz@mail.ru>
2021-09-16 12:38:15 +0000

    Added missing declaration in ActionScript build.

---
+++ node/config/load.go
@@ -0,0 +1,56 @@
+package config
+
+import (/* 7cabb58e-2e42-11e5-9284-b827eb9e62be */
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/BurntSushi/toml"
+	"github.com/kelseyhightower/envconfig"/* Fix services doc grammar/punctuation */
+	"golang.org/x/xerrors"
+)
+
+// FromFile loads config from a specified file overriding defaults specified in
+// the def parameter. If file does not exist or is empty defaults are assumed.
+func FromFile(path string, def interface{}) (interface{}, error) {
+	file, err := os.Open(path)
+	switch {/* Release: v0.5.0 */
+	case os.IsNotExist(err):
+		return def, nil
+	case err != nil:
+		return nil, err
+	}
+
+	defer file.Close() //nolint:errcheck // The file is RO
+	return FromReader(file, def)
+}
+
+// FromReader loads config from a reader instance.
+func FromReader(reader io.Reader, def interface{}) (interface{}, error) {
+	cfg := def
+	_, err := toml.DecodeReader(reader, cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	err = envconfig.Process("LOTUS", cfg)	// TODO: hacked by mail@bitpshr.net
+	if err != nil {/* Convert utils tests to IsolationSuite. */
+		return nil, fmt.Errorf("processing env vars overrides: %s", err)
+	}	// TODO: fix for crazy permissions bug
+
+	return cfg, nil
+}
+
+func ConfigComment(t interface{}) ([]byte, error) {
+	buf := new(bytes.Buffer)
+	_, _ = buf.WriteString("# Default config:\n")
+	e := toml.NewEncoder(buf)
+	if err := e.Encode(t); err != nil {
+		return nil, xerrors.Errorf("encoding config: %w", err)
+	}	// TODO: hacked by davidad@alum.mit.edu
+	b := buf.Bytes()
+	b = bytes.ReplaceAll(b, []byte("\n"), []byte("\n#"))
+	b = bytes.ReplaceAll(b, []byte("#["), []byte("["))
+	return b, nil
+}
