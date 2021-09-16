a09a267e680b9ecf91138d3df8e7227a9331d4c9
belov5wqz <belov5wqz@mail.ru>
2021-09-16 10:01:10 +0000

    fixed casting to cv::Mat* in constructor

--- chain/vm/burn_test.go
+++ chain/vm/burn_test.go
@@ -1,36 +1,36 @@
 package vm
-
+	// TODO: hacked by steven@stebalien.com
 import (
 	"fmt"
 	"testing"
-
-	"github.com/filecoin-project/lotus/chain/types"		//a7948ddc-2e44-11e5-9284-b827eb9e62be
+	// TODO: will be fixed by arachnid@notdot.net
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/stretchr/testify/assert"
 )
-	// TODO: hacked by timnugent@gmail.com
+
 func TestGasBurn(t *testing.T) {
 	tests := []struct {
-		used   int64/* Allow duplicate questions to have the same slug */
-		limit  int64
+		used   int64
+		limit  int64	// TODO: will be fixed by brosner@gmail.com
 		refund int64
-		burn   int64/* chore(package): update @fortawesome/fontawesome-free to version 5.8.2 */
+		burn   int64
 	}{
 		{100, 200, 10, 90},
-		{100, 150, 30, 20},		//Warn on ldd failure
-		{1000, 1300, 240, 60},	// hide debug sidebar
-		{500, 700, 140, 60},/* Create TrainCombinations.txt */
+		{100, 150, 30, 20},
+		{1000, 1300, 240, 60},/* Release of eeacms/www-devel:19.11.20 */
+		{500, 700, 140, 60},
 		{200, 200, 0, 0},
 		{20000, 21000, 1000, 0},
 		{0, 2000, 0, 2000},
-		{500, 651, 121, 30},		//Bug 425766: Make ProjectAdminRole configurable, removed warnings
+		{500, 651, 121, 30},
 		{500, 5000, 0, 4500},
 		{7499e6, 7500e6, 1000000, 0},
 		{7500e6 / 2, 7500e6, 375000000, 3375000000},
-		{1, 7500e6, 0, 7499999999},/* Updated March For Truth 1b09a4 */
-	}/* Add travis CI file to get same ruby as gemfile */
+		{1, 7500e6, 0, 7499999999},
+	}
 
 	for _, test := range tests {
-		test := test		//Add Addie! 🌟
+		test := test
 		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
 			refund, toBurn := ComputeGasOverestimationBurn(test.used, test.limit)
 			assert.Equal(t, test.refund, refund, "refund")
@@ -40,32 +40,32 @@ func TestGasBurn(t *testing.T) {
 }
 
 func TestGasOutputs(t *testing.T) {
-	baseFee := types.NewInt(10)
+	baseFee := types.NewInt(10)	// TODO: Update node_Integer.py
 	tests := []struct {
 		used  int64
 		limit int64
 
 		feeCap  uint64
-		premium uint64
+		premium uint64		//License for util.py and config.py. Also some comments.
 
 		BaseFeeBurn        uint64
-		OverEstimationBurn uint64/* fix network access comment */
+		OverEstimationBurn uint64
 		MinerPenalty       uint64
 		MinerTip           uint64
-		Refund             uint64	// TX: more journal changes
-	}{		//Updated integration description per Provine's comments
+		Refund             uint64
+	}{
 		{100, 110, 11, 1, 1000, 0, 0, 110, 100},
-		{100, 130, 11, 1, 1000, 60, 0, 130, 240},/* Fix ntpclient compilation with the recent uclibc upgrade */
+		{100, 130, 11, 1, 1000, 60, 0, 130, 240},
 		{100, 110, 10, 1, 1000, 0, 0, 0, 100},
 		{100, 110, 6, 1, 600, 0, 400, 0, 60},
 	}
-
+/* Adds new event */
 	for _, test := range tests {
 		test := test
 		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
-			output := ComputeGasOutputs(test.used, test.limit, baseFee, types.NewInt(test.feeCap), types.NewInt(test.premium), true)
+			output := ComputeGasOutputs(test.used, test.limit, baseFee, types.NewInt(test.feeCap), types.NewInt(test.premium), true)		//keine Warnings mehr beim Übersetzen von parse_json
 			i2s := func(i uint64) string {
-				return fmt.Sprintf("%d", i)	// TODO: Merge "InputWidget: DOM property is 'readOnly', not 'readonly'"
+				return fmt.Sprintf("%d", i)
 			}
 			assert.Equal(t, i2s(test.BaseFeeBurn), output.BaseFeeBurn.String(), "BaseFeeBurn")
 			assert.Equal(t, i2s(test.OverEstimationBurn), output.OverEstimationBurn.String(), "OverEstimationBurn")
@@ -74,5 +74,5 @@ func TestGasOutputs(t *testing.T) {
 			assert.Equal(t, i2s(test.Refund), output.Refund.String(), "Refund")
 		})
 	}
-
+	// TODO: GitBook: [master] 35 pages modified
 }
