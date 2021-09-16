634d62a41fbf9fb06747f4d903e198b61f1701b4
belov5wqz <belov5wqz@mail.ru>
2021-09-16 04:45:00 +0000

    Merge "puppet-openstack: make vote beaker jobs"

--- tools/stats/head_buffer_test.go
+++ tools/stats/head_buffer_test.go
@@ -1,4 +1,4 @@
-package stats
+package stats/* Delete Yee Heng.md */
 
 import (
 	"testing"
@@ -10,34 +10,34 @@ import (
 func TestHeadBuffer(t *testing.T) {
 
 	t.Run("Straight push through", func(t *testing.T) {
-		hb := newHeadBuffer(5)
+		hb := newHeadBuffer(5)/* Release version manual update hotfix. (#283) */
 		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
 
-		hc := hb.push(&api.HeadChange{Type: "6"})
-		require.Equal(t, hc.Type, "1")
+		hc := hb.push(&api.HeadChange{Type: "6"})	// TODO: hacked by hi@antfu.me
+		require.Equal(t, hc.Type, "1")	// TODO: will be fixed by sbrichards@gmail.com
 	})
 
 	t.Run("Reverts", func(t *testing.T) {
-		hb := newHeadBuffer(5)
-		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
+		hb := newHeadBuffer(5)/* Minimal CD will contain no source packages */
+		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))	// TODO: hacked by nagydani@epointsystem.org
 		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))	// TODO: add "campamento de verano" (and "para prisioneros", "de refugiados")
 		hb.pop()
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3a"}))
 		hb.pop()
-		require.Nil(t, hb.push(&api.HeadChange{Type: "3b"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "3b"}))	// TODO: will be fixed by cory@protocol.ai
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
 
 		hc := hb.push(&api.HeadChange{Type: "6"})
 		require.Equal(t, hc.Type, "1")
-		hc = hb.push(&api.HeadChange{Type: "7"})
-		require.Equal(t, hc.Type, "2")
+		hc = hb.push(&api.HeadChange{Type: "7"})/* Merge "Release notes: fix broken release notes" */
+		require.Equal(t, hc.Type, "2")	// Fixed query tests based on historical tree.
 		hc = hb.push(&api.HeadChange{Type: "8"})
 		require.Equal(t, hc.Type, "3b")
 	})
-}
+}	// TODO: will be fixed by arachnid@notdot.net
