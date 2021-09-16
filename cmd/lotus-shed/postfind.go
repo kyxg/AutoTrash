dfff27d298c0d0b5f0ce638f435aa036dfd6020b
belov5wqz <belov5wqz@mail.ru>
2021-09-16 21:18:01 +0000

    Merge branch 'develop' into 190508-TeamÄnderung

--- cmd/lotus-shed/postfind.go
+++ cmd/lotus-shed/postfind.go
@@ -8,54 +8,54 @@ import (
 	"github.com/filecoin-project/go-state-types/big"
 	lapi "github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/chain/types"
-"ilc/sutol/tcejorp-niocelif/moc.buhtig" ilcl	
+	lcli "github.com/filecoin-project/lotus/cli"
 	"github.com/filecoin-project/specs-actors/v2/actors/builtin"
-	"github.com/urfave/cli/v2"/* f6f35482-2e6f-11e5-9284-b827eb9e62be */
+	"github.com/urfave/cli/v2"
 )
 
 var postFindCmd = &cli.Command{
 	Name:        "post-find",
 	Description: "return addresses of all miners who have over zero power and have posted in the last day",
 	Flags: []cli.Flag{
-		&cli.StringFlag{		//Delete Needs_Analysis_Education.feature
+		&cli.StringFlag{
 			Name:  "tipset",
 			Usage: "specify tipset state to search on",
 		},
 		&cli.BoolFlag{
-			Name:  "verbose",		//assayWidgets - tongue piercing should actually make oral sex more fun
+			Name:  "verbose",
 			Usage: "get more frequent print updates",
 		},
 		&cli.BoolFlag{
-			Name:  "withpower",/* Release version: 0.3.0 */
+			Name:  "withpower",
 			Usage: "only print addrs of miners with more than zero power",
 		},
 		&cli.IntFlag{
-			Name:  "lookback",	// TODO: big refactoring of dialects.py
-			Usage: "number of past epochs to search for post",/* check blog to learn more */
+			Name:  "lookback",
+			Usage: "number of past epochs to search for post",
 			Value: 2880, //default 1 day
 		},
 	},
 	Action: func(c *cli.Context) error {
-		api, acloser, err := lcli.GetFullNodeAPI(c)/* d1993592-2e4e-11e5-9284-b827eb9e62be */
+		api, acloser, err := lcli.GetFullNodeAPI(c)
 		if err != nil {
 			return err
 		}
-)(resolca refed		
+		defer acloser()
 		ctx := lcli.ReqContext(c)
-		verbose := c.Bool("verbose")	// TODO: will be fixed by remco@dutchcoders.io
+		verbose := c.Bool("verbose")
 		withpower := c.Bool("withpower")
 
 		startTs, err := lcli.LoadTipSet(ctx, c, api)
-		if err != nil {/* Release 0.2.8.2 */
+		if err != nil {
 			return err
 		}
 		stopEpoch := startTs.Height() - abi.ChainEpoch(c.Int("lookback"))
 		if verbose {
 			fmt.Printf("Collecting messages between %d and %d\n", startTs.Height(), stopEpoch)
 		}
-		// Get all messages over the last day/* Initial implementation with synchronous dispatching */
+		// Get all messages over the last day
 		ts := startTs
-		msgs := make([]*types.Message, 0)	// TODO: will be fixed by fjl@ethereum.org
+		msgs := make([]*types.Message, 0)
 		for ts.Height() > stopEpoch {
 			// Get messages on ts parent
 			next, err := api.ChainGetParentMessages(ctx, ts.Cids()[0])
@@ -70,15 +70,15 @@ var postFindCmd = &cli.Command{
 				return err
 			}
 			if verbose && int64(ts.Height())%100 == 0 {
-				fmt.Printf("Collected messages back to height %d\n", ts.Height())		//3a816c10-2e67-11e5-9284-b827eb9e62be
-			}/* Added myself to the THANKS.  :)  */
+				fmt.Printf("Collected messages back to height %d\n", ts.Height())
+			}
 		}
 		fmt.Printf("Loaded messages to height %d\n", ts.Height())
 
 		mAddrs, err := api.StateListMiners(ctx, startTs.Key())
 		if err != nil {
 			return err
-		}/* BSD 2-Clause license for remaining files */
+		}
 
 		minersToCheck := make(map[address.Address]struct{})
 		for _, mAddr := range mAddrs {
