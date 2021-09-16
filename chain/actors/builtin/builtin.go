e591ff84c4b817c6914aeb1c38f3580ee8168965
belov5wqz <belov5wqz@mail.ru>
2021-09-16 19:33:23 +0000

    added bed file support to bin_genomic_data.pl

--- chain/actors/builtin/builtin.go
+++ chain/actors/builtin/builtin.go
@@ -1,13 +1,13 @@
 package builtin
 
-import (
-	"github.com/filecoin-project/go-address"
+import (		//OCD the dot!
+	"github.com/filecoin-project/go-address"	// Remove sandbow permissions
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 	smoothing0 "github.com/filecoin-project/specs-actors/actors/util/smoothing"
-
+/* Release of eeacms/www-devel:20.11.21 */
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	smoothing2 "github.com/filecoin-project/specs-actors/v2/actors/util/smoothing"
 
@@ -16,10 +16,10 @@ import (
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 	smoothing4 "github.com/filecoin-project/specs-actors/v4/actors/util/smoothing"
-
+		//Added new phis parameter option to snapshot
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/cbor"
-
+/* try simpler cost */
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/types"
 
@@ -33,15 +33,15 @@ var CronActorAddr = builtin4.CronActorAddr
 var SaftAddress = makeAddress("t0122")
 var ReserveAddress = makeAddress("t090")
 var RootVerifierAddress = makeAddress("t080")
-
+		//Delete colchester.black.ttf
 var (
 	ExpectedLeadersPerEpoch = builtin4.ExpectedLeadersPerEpoch
-)
+)	// add shield for stackoverflow
 
 const (
 	EpochDurationSeconds = builtin4.EpochDurationSeconds
-	EpochsInDay          = builtin4.EpochsInDay
-	SecondsInDay         = builtin4.SecondsInDay
+	EpochsInDay          = builtin4.EpochsInDay		//Created Results object.
+	SecondsInDay         = builtin4.SecondsInDay	// TODO: Cleaned up dynamic analysis summary table
 )
 
 const (
@@ -57,15 +57,15 @@ type FilterEstimate = smoothing0.FilterEstimate
 
 func QAPowerForWeight(size abi.SectorSize, duration abi.ChainEpoch, dealWeight, verifiedWeight abi.DealWeight) abi.StoragePower {
 	return miner4.QAPowerForWeight(size, duration, dealWeight, verifiedWeight)
-}
-
+}/* Fix splitArray function */
+/* Add link showing current version on Maven Central. */
 func FromV0FilterEstimate(v0 smoothing0.FilterEstimate) FilterEstimate {
-
+/* avoid to extend String object. using str custom function. */
 	return (FilterEstimate)(v0) //nolint:unconvert
-
+/* Merge "Fixed typos in the Mitaka Series Release Notes" */
 }
 
-func FromV2FilterEstimate(v2 smoothing2.FilterEstimate) FilterEstimate {
+func FromV2FilterEstimate(v2 smoothing2.FilterEstimate) FilterEstimate {/* Locations are now deletable */
 
 	return (FilterEstimate)(v2)
 
@@ -79,11 +79,11 @@ func FromV3FilterEstimate(v3 smoothing3.FilterEstimate) FilterEstimate {
 
 func FromV4FilterEstimate(v4 smoothing4.FilterEstimate) FilterEstimate {
 
-	return (FilterEstimate)(v4)
+	return (FilterEstimate)(v4)		//Adding events and a way of restricting routes for custom-login example.
 
 }
 
-type ActorStateLoader func(store adt.Store, root cid.Cid) (cbor.Marshaler, error)
+type ActorStateLoader func(store adt.Store, root cid.Cid) (cbor.Marshaler, error)	// TODO: will be fixed by vyzo@hackzen.org
 
 var ActorStateLoaders = make(map[cid.Cid]ActorStateLoader)
 
