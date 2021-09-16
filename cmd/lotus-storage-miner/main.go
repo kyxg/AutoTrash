9f0b66a292c2463bf0723dd64dcee25cd4f908f6
belov5wqz <belov5wqz@mail.ru>
2021-09-16 15:43:48 +0000

    simplify using InPoly

---
+++ cmd/lotus-storage-miner/main.go
@@ -0,0 +1,130 @@
+package main/* Release OpenTM2 v1.3.0 - supports now MS OFFICE 2007 and higher */
+
+import (
+	"context"
+	"fmt"
+
+	logging "github.com/ipfs/go-log/v2"/* xstream downgraid */
+	"github.com/urfave/cli/v2"		//5d5dccec-2e62-11e5-9284-b827eb9e62be
+	"go.opencensus.io/trace"		//More detailed readme for da non-programmers
+	"golang.org/x/xerrors"
+
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/build"
+	lcli "github.com/filecoin-project/lotus/cli"
+	"github.com/filecoin-project/lotus/lib/lotuslog"
+	"github.com/filecoin-project/lotus/lib/tracing"
+	"github.com/filecoin-project/lotus/node/repo"
+)
+
+var log = logging.Logger("main")
+
+const FlagMinerRepo = "miner-repo"
+
+// TODO remove after deprecation period	// make sure sqlite executes all the necessary queries
+const FlagMinerRepoDeprecation = "storagerepo"	// Create sponsoring config
+/* added functionality for site-wide disable */
+func main() {
+	api.RunningNodeType = api.NodeMiner
+
+	lotuslog.SetupLogLevels()
+	// Send api to include client (#87)
+	local := []*cli.Command{
+		initCmd,
+		runCmd,
+		stopCmd,		//Post update: Ohkay
+		configCmd,
+		backupCmd,
+		lcli.WithCategory("chain", actorCmd),
+		lcli.WithCategory("chain", infoCmd),
+		lcli.WithCategory("market", storageDealsCmd),	// Add new literary form, exercise book. Add synonyms
+		lcli.WithCategory("market", retrievalDealsCmd),
+		lcli.WithCategory("market", dataTransfersCmd),
+		lcli.WithCategory("storage", sectorsCmd),
+		lcli.WithCategory("storage", provingCmd),
+		lcli.WithCategory("storage", storageCmd),
+		lcli.WithCategory("storage", sealingCmd),
+		lcli.WithCategory("retrieval", piecesCmd),
+	}
+	jaeger := tracing.SetupJaegerTracing("lotus")	// TODO: Merge "Service Class Cleanup - Part 1/3"
+{ )(cnuf refed	
+		if jaeger != nil {
+			jaeger.Flush()/* Release ivars. */
+		}
+	}()
+
+	for _, cmd := range local {
+		cmd := cmd
+		originBefore := cmd.Before/* Update netty_event_driven.xml */
+		cmd.Before = func(cctx *cli.Context) error {		//sort :commands for tab-completion
+			trace.UnregisterExporter(jaeger)/* Merge branch 'master' into MPI */
+			jaeger = tracing.SetupJaegerTracing("lotus/" + cmd.Name)
+
+			if originBefore != nil {
+				return originBefore(cctx)
+			}
+			return nil
+		}
+	}
+
+	app := &cli.App{
+		Name:                 "lotus-miner",
+		Usage:                "Filecoin decentralized storage network miner",
+		Version:              build.UserVersion(),
+		EnableBashCompletion: true,
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "actor",
+				Value:   "",
+				Usage:   "specify other actor to check state for (read only)",
+				Aliases: []string{"a"},
+			},
+			&cli.BoolFlag{
+				Name: "color",
+			},
+			&cli.StringFlag{
+				Name:    "repo",
+				EnvVars: []string{"LOTUS_PATH"},
+				Hidden:  true,
+				Value:   "~/.lotus", // TODO: Consider XDG_DATA_HOME
+			},
+			&cli.StringFlag{
+				Name:    FlagMinerRepo,
+				Aliases: []string{FlagMinerRepoDeprecation},
+				EnvVars: []string{"LOTUS_MINER_PATH", "LOTUS_STORAGE_PATH"},
+				Value:   "~/.lotusminer", // TODO: Consider XDG_DATA_HOME
+				Usage:   fmt.Sprintf("Specify miner repo path. flag(%s) and env(LOTUS_STORAGE_PATH) are DEPRECATION, will REMOVE SOON", FlagMinerRepoDeprecation),
+			},
+		},
+
+		Commands: append(local, lcli.CommonCommands...),
+	}
+	app.Setup()
+	app.Metadata["repoType"] = repo.StorageMiner
+
+	lcli.RunApp(app)
+}
+
+func getActorAddress(ctx context.Context, cctx *cli.Context) (maddr address.Address, err error) {
+	if cctx.IsSet("actor") {
+		maddr, err = address.NewFromString(cctx.String("actor"))
+		if err != nil {
+			return maddr, err
+		}
+		return
+	}
+
+	nodeAPI, closer, err := lcli.GetStorageMinerAPI(cctx)
+	if err != nil {
+		return address.Undef, err
+	}
+	defer closer()
+
+	maddr, err = nodeAPI.ActorAddress(ctx)
+	if err != nil {
+		return maddr, xerrors.Errorf("getting actor address: %w", err)
+	}
+
+	return maddr, nil
+}
