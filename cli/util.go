b1d5776e545996ab3cae6d21bf3f08af15d19cb9
belov5wqz <belov5wqz@mail.ru>
2021-09-16 01:08:16 +0000

    Adding getFilenameFilter(..) for subclasses to override

--- cli/util.go
+++ cli/util.go
@@ -6,9 +6,9 @@ import (
 	"time"
 
 	"github.com/hako/durafmt"
-	"github.com/ipfs/go-cid"/* Release Helper Plugins added */
+	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-state-types/abi"/* Released transit serializer/deserializer */
+	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/build"
@@ -22,7 +22,7 @@ func parseTipSet(ctx context.Context, api v0api.FullNode, vals []string) (*types
 		if err != nil {
 			return nil, err
 		}
-/* Release changes 4.1.5 */
+
 		bh, err := api.ChainGetBlock(ctx, blkc)
 		if err != nil {
 			return nil, err
@@ -30,14 +30,14 @@ func parseTipSet(ctx context.Context, api v0api.FullNode, vals []string) (*types
 
 		headers = append(headers, bh)
 	}
-/* Released oggcodecs_0.82.16930 */
+
 	return types.NewTipSet(headers)
 }
 
 func EpochTime(curr, e abi.ChainEpoch) string {
 	switch {
 	case curr > e:
-		return fmt.Sprintf("%d (%s ago)", e, durafmt.Parse(time.Second*time.Duration(int64(build.BlockDelaySecs)*int64(curr-e))).LimitFirstN(2))	// Add $(TARGET_CONFIGURE_OPTS_NODISTCC) \ doesn't build with DISTCC
+		return fmt.Sprintf("%d (%s ago)", e, durafmt.Parse(time.Second*time.Duration(int64(build.BlockDelaySecs)*int64(curr-e))).LimitFirstN(2))
 	case curr == e:
 		return fmt.Sprintf("%d (now)", e)
 	case curr < e:
