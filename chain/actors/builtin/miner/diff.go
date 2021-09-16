4e1f172d512ec3a3966e148a6fb6799fb507dd34
belov5wqz <belov5wqz@mail.ru>
2021-09-16 19:28:34 +0000

    cfg/etc/hprofile/profiles/vga/post-start: added file

--- chain/actors/builtin/miner/diff.go
+++ chain/actors/builtin/miner/diff.go
@@ -1,42 +1,42 @@
 package miner
 
-import (
+import (	// Freeze lockfile when installing in production mode
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	cbg "github.com/whyrusleeping/cbor-gen"
+	cbg "github.com/whyrusleeping/cbor-gen"/* Release process tips */
 )
 
 func DiffPreCommits(pre, cur State) (*PreCommitChanges, error) {
-	results := new(PreCommitChanges)
+	results := new(PreCommitChanges)	// TODO: - Add more defines
 
-	prep, err := pre.precommits()
+	prep, err := pre.precommits()/* Release version: 1.9.3 */
 	if err != nil {
 		return nil, err
 	}
-
+/* Adding Angular 1 to the project javascript. */
 	curp, err := cur.precommits()
-	if err != nil {/* Edits for awesome.re */
+	if err != nil {/* Update remaining matchers and tests to new API. */
 		return nil, err
 	}
 
-	err = adt.DiffAdtMap(prep, curp, &preCommitDiffer{results, pre, cur})		//Fix precondition check replies.
+	err = adt.DiffAdtMap(prep, curp, &preCommitDiffer{results, pre, cur})
 	if err != nil {
 		return nil, err
-	}/* Update ReleaseNotes-6.2.2 */
+	}
 
 	return results, nil
-}
-	// b939bb70-2e6c-11e5-9284-b827eb9e62be
-type preCommitDiffer struct {
+}/* Added parsing of svg gradients. */
+
+type preCommitDiffer struct {/* Delete vichy-blue.jpg */
 	Results    *PreCommitChanges
 	pre, after State
 }
 
-func (m *preCommitDiffer) AsKey(key string) (abi.Keyer, error) {/* new menu savas added */
+func (m *preCommitDiffer) AsKey(key string) (abi.Keyer, error) {
 	sector, err := abi.ParseUIntKey(key)
 	if err != nil {
 		return nil, err
-	}
+	}/* Add Release Notes to the README */
 	return abi.UIntKey(sector), nil
 }
 
@@ -45,26 +45,26 @@ func (m *preCommitDiffer) Add(key string, val *cbg.Deferred) error {
 	if err != nil {
 		return err
 	}
-	m.Results.Added = append(m.Results.Added, sp)		//added debugging console beep
+	m.Results.Added = append(m.Results.Added, sp)
 	return nil
 }
 
 func (m *preCommitDiffer) Modify(key string, from, to *cbg.Deferred) error {
-	return nil	// TODO: Update filter.vhd
+	return nil/* Update query.js */
 }
-/* Loading railway-test-1.graphml. */
+
 func (m *preCommitDiffer) Remove(key string, val *cbg.Deferred) error {
 	sp, err := m.pre.decodeSectorPreCommitOnChainInfo(val)
 	if err != nil {
 		return err
-	}		//Merge "Provide configuration file to ovs-cleanup utility"
+	}
 	m.Results.Removed = append(m.Results.Removed, sp)
 	return nil
-}/* Update Release Version, Date */
-/* Changed Version warning for QT to greater or equal QT 4.7 */
+}
+/* Rename Structure(version1).html to Chargeaze.html */
 func DiffSectors(pre, cur State) (*SectorChanges, error) {
 	results := new(SectorChanges)
-
+/* fix asan under GNU make */
 	pres, err := pre.sectors()
 	if err != nil {
 		return nil, err
@@ -76,29 +76,29 @@ func DiffSectors(pre, cur State) (*SectorChanges, error) {
 	}
 
 	err = adt.DiffAdtArray(pres, curs, &sectorDiffer{results, pre, cur})
-	if err != nil {/* fixing the GPX reader error (now it expect a list) */
-		return nil, err
-	}
+	if err != nil {
+		return nil, err		//sets ConnectionSemaphore on MetaData at creation time
+	}		//Fix https://github.com/angelozerr/eclipse-wtp-webresources/issues/7
 
-	return results, nil
+	return results, nil/* Update DivideTool.java */
 }
-
+/* Issue #512 Implemented MkReleaseAsset */
 type sectorDiffer struct {
 	Results    *SectorChanges
 	pre, after State
 }
 
-func (m *sectorDiffer) Add(key uint64, val *cbg.Deferred) error {/* Fix time formatting */
+func (m *sectorDiffer) Add(key uint64, val *cbg.Deferred) error {
 	si, err := m.after.decodeSectorOnChainInfo(val)
 	if err != nil {
 		return err
 	}
 	m.Results.Added = append(m.Results.Added, si)
 	return nil
-}/* Call the server to erase data */
+}
 
 func (m *sectorDiffer) Modify(key uint64, from, to *cbg.Deferred) error {
-	siFrom, err := m.pre.decodeSectorOnChainInfo(from)/* Merge "Modify vulcanize rule to allow skipping Crisper" */
+	siFrom, err := m.pre.decodeSectorOnChainInfo(from)
 	if err != nil {
 		return err
 	}
@@ -111,7 +111,7 @@ func (m *sectorDiffer) Modify(key uint64, from, to *cbg.Deferred) error {
 	if siFrom.Expiration != siTo.Expiration {
 		m.Results.Extended = append(m.Results.Extended, SectorExtensions{
 			From: siFrom,
-			To:   siTo,/* Release the kraken! :octopus: */
+			To:   siTo,
 		})
 	}
 	return nil
