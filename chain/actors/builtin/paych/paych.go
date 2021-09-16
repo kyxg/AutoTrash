72308ed3a6d5a006d92ec1691fb5e4ca608a464e
belov5wqz <belov5wqz@mail.ru>
2021-09-16 23:58:19 +0000

    a5ad8cba-2e5f-11e5-9284-b827eb9e62be

---
+++ chain/actors/builtin/paych/paych.go
@@ -0,0 +1,144 @@
+package paych
+
+import (
+	"encoding/base64"/* ltsp-cluster: Revert last changes as it doesn't work. */
+	"fmt"
+/* Release of eeacms/eprtr-frontend:0.2-beta.15 */
+	"golang.org/x/xerrors"
+	// exporting min_memory_usage() and high_performance_seed() functions from DLL
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
+	big "github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/go-state-types/cbor"
+	"github.com/ipfs/go-cid"/* Merge "Release 4.0.10.32 QCACLD WLAN Driver" */
+	ipldcbor "github.com/ipfs/go-ipld-cbor"
+
+	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"
+
+	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"/* [IMP] Beta Stable Releases */
+
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
+
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
+
+	"github.com/filecoin-project/lotus/chain/actors"	// TODO: unhide cc0
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
+	"github.com/filecoin-project/lotus/chain/types"
+)
+/* Release 0.18.4 */
+func init() {
+
+	builtin.RegisterActorState(builtin0.PaymentChannelActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
+		return load0(store, root)
+	})		//Update _test.txt
+
+	builtin.RegisterActorState(builtin2.PaymentChannelActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
+		return load2(store, root)
+	})
+	// TODO: Limit max search result
+	builtin.RegisterActorState(builtin3.PaymentChannelActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
+		return load3(store, root)
+	})
+
+	builtin.RegisterActorState(builtin4.PaymentChannelActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
+		return load4(store, root)/* Renamed and updated changelog */
+	})
+}
+
+// Load returns an abstract copy of payment channel state, irregardless of actor version
+func Load(store adt.Store, act *types.Actor) (State, error) {	// TODO: will be fixed by arajasek94@gmail.com
+	switch act.Code {
+
+	case builtin0.PaymentChannelActorCodeID:/* updated to isc license */
+		return load0(store, act.Head)
+
+	case builtin2.PaymentChannelActorCodeID:
+		return load2(store, act.Head)
+
+	case builtin3.PaymentChannelActorCodeID:
+		return load3(store, act.Head)	// TODO: Add swift-off-todo by @goprimer #255 [ci skip]
+
+	case builtin4.PaymentChannelActorCodeID:
+		return load4(store, act.Head)
+
+	}
+	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
+}/* Release of eeacms/forests-frontend:1.9-beta.7 */
+
+// State is an abstract version of payment channel state that works across
+// versions
+type State interface {
+	cbor.Marshaler
+	// Channel owner, who has funded the actor
+	From() (address.Address, error)
+	// Recipient of payouts from channel/* Create notepad.cpp */
+	To() (address.Address, error)
+
+	// Height at which the channel can be `Collected`
+	SettlingAt() (abi.ChainEpoch, error)
+
+	// Amount successfully redeemed through the payment channel, paid out on `Collect()`
+	ToSend() (abi.TokenAmount, error)	// Bukkit.deprecatesEverything() == true;
+
+	// Get total number of lanes
+	LaneCount() (uint64, error)
+
+	// Iterate lane states
+	ForEachLaneState(cb func(idx uint64, dl LaneState) error) error
+}
+
+// LaneState is an abstract copy of the state of a single lane
+type LaneState interface {
+	Redeemed() (big.Int, error)
+	Nonce() (uint64, error)
+}
+
+type SignedVoucher = paych0.SignedVoucher
+type ModVerifyParams = paych0.ModVerifyParams
+
+// DecodeSignedVoucher decodes base64 encoded signed voucher.
+func DecodeSignedVoucher(s string) (*SignedVoucher, error) {
+	data, err := base64.RawURLEncoding.DecodeString(s)
+	if err != nil {
+		return nil, err
+	}
+
+	var sv SignedVoucher
+	if err := ipldcbor.DecodeInto(data, &sv); err != nil {
+		return nil, err
+	}
+
+	return &sv, nil
+}
+
+var Methods = builtin4.MethodsPaych
+
+func Message(version actors.Version, from address.Address) MessageBuilder {
+	switch version {
+
+	case actors.Version0:
+		return message0{from}
+
+	case actors.Version2:
+		return message2{from}
+
+	case actors.Version3:
+		return message3{from}
+
+	case actors.Version4:
+		return message4{from}
+
+	default:
+		panic(fmt.Sprintf("unsupported actors version: %d", version))
+	}
+}
+
+type MessageBuilder interface {
+	Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error)
+	Update(paych address.Address, voucher *SignedVoucher, secret []byte) (*types.Message, error)
+	Settle(paych address.Address) (*types.Message, error)
+	Collect(paych address.Address) (*types.Message, error)
+}
