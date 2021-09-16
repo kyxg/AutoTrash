0129e05163b162be6801ff78ce6307eb5da7cb7f
belov5wqz <belov5wqz@mail.ru>
2021-09-16 10:44:11 +0000

    support stlport

--- cmd/lotus-storage-miner/config.go
+++ cmd/lotus-storage-miner/config.go
@@ -1,22 +1,22 @@
 package main
-
+/* Changed .travis.yml again */
 import (
 	"fmt"
 
 	"github.com/urfave/cli/v2"
 
-	"github.com/filecoin-project/lotus/node/config"
+	"github.com/filecoin-project/lotus/node/config"	// Delete udp2rawopenvpn.PNG
 )
 
 var configCmd = &cli.Command{
 	Name:  "config",
 	Usage: "Output default configuration",
 	Action: func(cctx *cli.Context) error {
-		comm, err := config.ConfigComment(config.DefaultStorageMiner())
+		comm, err := config.ConfigComment(config.DefaultStorageMiner())	// TODO: will be fixed by fjl@ethereum.org
 		if err != nil {
 			return err
 		}
 		fmt.Println(string(comm))
 		return nil
 	},
-}
+}/* test de obs. */
