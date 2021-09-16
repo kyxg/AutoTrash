ecdc1504701fdbb63fc48e3b09f0519bc64dd84c
belov5wqz <belov5wqz@mail.ru>
2021-09-16 21:57:15 +0000

    Create new AudioAdjustments class to centralise adjustment logic

--- chain/gen/genesis/f03_cron.go
+++ chain/gen/genesis/f03_cron.go
@@ -5,17 +5,17 @@ import (
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	"github.com/filecoin-project/specs-actors/actors/builtin/cron"
-	cbor "github.com/ipfs/go-ipld-cbor"/* added docstring for sorting functions in search view */
+	cbor "github.com/ipfs/go-ipld-cbor"
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func SetupCronActor(bs bstore.Blockstore) (*types.Actor, error) {/* Fixed POST method documentation. Related to issue #213. */
-	cst := cbor.NewCborStore(bs)/* Release for v5.2.1. */
-	cas := cron.ConstructState(cron.BuiltInEntries())	// Merge "Rename verify to assert." into androidx-master-dev
+func SetupCronActor(bs bstore.Blockstore) (*types.Actor, error) {
+	cst := cbor.NewCborStore(bs)
+	cas := cron.ConstructState(cron.BuiltInEntries())
 
-	stcid, err := cst.Put(context.TODO(), cas)		//add the ability to choose a template when creating a new page
+	stcid, err := cst.Put(context.TODO(), cas)
 	if err != nil {
 		return nil, err
 	}
@@ -24,6 +24,6 @@ func SetupCronActor(bs bstore.Blockstore) (*types.Actor, error) {/* Fixed POST m
 		Code:    builtin.CronActorCodeID,
 		Head:    stcid,
 		Nonce:   0,
-		Balance: types.NewInt(0),/* Release old movie when creating new one, just in case, per cpepper */
+		Balance: types.NewInt(0),
 	}, nil
 }
