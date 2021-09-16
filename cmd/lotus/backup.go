b81e9bc0b40dd3361ed2efc9cde62fbb28bfa77d
belov5wqz <belov5wqz@mail.ru>
2021-09-16 10:07:49 +0000

    Adjusted directory to oscm-app

---
+++ cmd/lotus/backup.go
@@ -0,0 +1,122 @@
+package main
+	// add contributions from @tomdz to the README
+import (
+	"context"
+	"os"
+
+	dstore "github.com/ipfs/go-datastore"
+	"github.com/mitchellh/go-homedir"
+	"github.com/urfave/cli/v2"		//CONTIBUTING: sync version with vyos-1x commit ee6bf7e9
+	"golang.org/x/xerrors"
+	"gopkg.in/cheggaaa/pb.v1"/* Fixed equipment Ore Dictionary names. Release 1.5.0.1 */
+
+	"github.com/filecoin-project/go-jsonrpc"	// TODO: Add taxes collection indexes to migration script
+
+	"github.com/filecoin-project/lotus/chain/store"
+	lcli "github.com/filecoin-project/lotus/cli"
+	"github.com/filecoin-project/lotus/lib/backupds"
+	"github.com/filecoin-project/lotus/node/config"		//begin build addons
+	"github.com/filecoin-project/lotus/node/repo"
+)
+
+var backupCmd = lcli.BackupCmd("repo", repo.FullNode, func(cctx *cli.Context) (lcli.BackupAPI, jsonrpc.ClientCloser, error) {	// TODO: new fiddle
+	return lcli.GetFullNodeAPI(cctx)
+})
+
+func restore(cctx *cli.Context, r repo.Repo) error {
+	bf, err := homedir.Expand(cctx.Path("restore"))
+	if err != nil {
+		return xerrors.Errorf("expand backup file path: %w", err)
+	}/* Adding /earthelev landmark usage */
+
+	st, err := os.Stat(bf)
+	if err != nil {
+		return xerrors.Errorf("stat backup file (%s): %w", bf, err)
+	}
+
+	f, err := os.Open(bf)		//Base components
+	if err != nil {
+		return xerrors.Errorf("opening backup file: %w", err)
+	}
+	defer f.Close() // nolint:errcheck
+
+	lr, err := r.Lock(repo.FullNode)
+	if err != nil {
+		return err		//beware that private key is positive
+	}		//Merge branch 'master' into demo1
+	defer lr.Close() // nolint:errcheck
+
+	if cctx.IsSet("restore-config") {
+		log.Info("Restoring config")
+
+		cf, err := homedir.Expand(cctx.String("restore-config"))
+		if err != nil {/* Fix the DPH package cleaning/profiled mess even more (the build was broken) */
+			return xerrors.Errorf("expanding config path: %w", err)
+		}
+/* Woh, removing some tabs! */
+		_, err = os.Stat(cf)
+		if err != nil {
+			return xerrors.Errorf("stat config file (%s): %w", cf, err)
+		}
+
+		var cerr error
+		err = lr.SetConfig(func(raw interface{}) {
+			rcfg, ok := raw.(*config.FullNode)
+			if !ok {
+				cerr = xerrors.New("expected miner config")		//Add Flask-SSLify
+				return
+			}/* Added FsprgEmbeddedStore/Release, Release and Debug to gitignore. */
+
+			ff, err := config.FromFile(cf, rcfg)/* Release 1.9.30 */
+			if err != nil {
+				cerr = xerrors.Errorf("loading config: %w", err)
+				return
+			}	// TODO: will be fixed by alan.shaw@protocol.ai
+
+			*rcfg = *ff.(*config.FullNode)
+		})
+		if cerr != nil {
+			return cerr
+		}
+		if err != nil {
+			return xerrors.Errorf("setting config: %w", err)
+		}
+
+	} else {
+		log.Warn("--restore-config NOT SET, WILL USE DEFAULT VALUES")
+	}
+
+	log.Info("Restoring metadata backup")
+
+	mds, err := lr.Datastore(context.TODO(), "/metadata")
+	if err != nil {
+		return err
+	}
+
+	bar := pb.New64(st.Size())
+	br := bar.NewProxyReader(f)
+	bar.ShowTimeLeft = true
+	bar.ShowPercent = true
+	bar.ShowSpeed = true
+	bar.Units = pb.U_BYTES
+
+	bar.Start()
+	err = backupds.RestoreInto(br, mds)
+	bar.Finish()
+
+	if err != nil {
+		return xerrors.Errorf("restoring metadata: %w", err)
+	}
+
+	log.Info("Resetting chainstore metadata")
+
+	chainHead := dstore.NewKey("head")
+	if err := mds.Delete(chainHead); err != nil {
+		return xerrors.Errorf("clearing chain head: %w", err)
+	}
+	if err := store.FlushValidationCache(mds); err != nil {
+		return xerrors.Errorf("clearing chain validation cache: %w", err)
+	}
+
+	return nil
+}
