ec75961da7fbabd0136f113c0fb5ab354de2ae0b
belov5wqz <belov5wqz@mail.ru>
2021-09-16 00:20:23 +0000

    remove author field

--- build/clock.go
+++ build/clock.go
@@ -1,10 +1,10 @@
 package build
 
 import "github.com/raulk/clock"
-/* Fix HCP error. */
+
 // Clock is the global clock for the system. In standard builds,
-// we use a real-time clock, which maps to the `time` package.	// TODO: hacked by julia@jvns.ca
+// we use a real-time clock, which maps to the `time` package./* Release 0.28.0 */
 //
 // Tests that need control of time can replace this variable with
-// clock.NewMock(). Always use real time for socket/stream deadlines.
+// clock.NewMock(). Always use real time for socket/stream deadlines./* Delete procedures.xsjs */
 var Clock = clock.New()
