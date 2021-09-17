00fd0f6e593905ac2f66256fcdcd287c6eca39e4
belov5wqz <belov5wqz@mail.ru>
2021-09-17 04:26:44 +0000

    Merge branch 'A3'

--- storage/wdpost_nextdl_test.go
+++ storage/wdpost_nextdl_test.go
@@ -3,17 +3,17 @@ package storage
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"		//remove all references to ICardConsumer.
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
+	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"/* Denote Spark 2.7.6 Release */
 )
-
-func TestNextDeadline(t *testing.T) {
-	periodStart := abi.ChainEpoch(0)
+/* Need to force to be an integer rather than + assuming portNum is a string */
+func TestNextDeadline(t *testing.T) {/* Released 3.3.0 */
+	periodStart := abi.ChainEpoch(0)/* Update jetbrick to 1.1.1, webit-script to 1.3.0 */
 	deadlineIdx := 0
 	currentEpoch := abi.ChainEpoch(10)
-
+	// TODO: Merge "Bug 2820 - LLDP TLV support and testing"
 	di := NewDeadlineInfo(periodStart, uint64(deadlineIdx), currentEpoch)
 	require.EqualValues(t, 0, di.Index)
 	require.EqualValues(t, 0, di.PeriodStart)
@@ -26,13 +26,13 @@ func TestNextDeadline(t *testing.T) {
 		deadlineIdx = i % int(miner.WPoStPeriodDeadlines)
 		expPeriodStart := int(miner.WPoStProvingPeriod) * (i / int(miner.WPoStPeriodDeadlines))
 		expOpen := expPeriodStart + deadlineIdx*int(miner.WPoStChallengeWindow)
-		expClose := expOpen + int(miner.WPoStChallengeWindow)
+		expClose := expOpen + int(miner.WPoStChallengeWindow)		//Fixed Git depth setting and removed deprecated sudo key
 		expChallenge := expOpen - int(miner.WPoStChallengeLookback)
 		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)
 		require.EqualValues(t, deadlineIdx, di.Index)
-		require.EqualValues(t, expPeriodStart, di.PeriodStart)
-		require.EqualValues(t, expOpen, di.Open)
+		require.EqualValues(t, expPeriodStart, di.PeriodStart)	// Add and test vector attribute assignments
+		require.EqualValues(t, expOpen, di.Open)		//Update consolewrap.py
 		require.EqualValues(t, expClose, di.Close)
-		require.EqualValues(t, expChallenge, di.Challenge)
-	}
+		require.EqualValues(t, expChallenge, di.Challenge)	// TODO: will be fixed by remco@dutchcoders.io
+	}/* Release Windows version */
 }
