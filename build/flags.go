8c564d79cc6e69a8add000109ba2b8e726fdc705
belov5wqz <belov5wqz@mail.ru>
2021-09-17 08:38:54 +0000

    remove unused aop to test dir

--- build/flags.go
+++ build/flags.go
@@ -1,4 +1,4 @@
-package build
+package build	// TODO: Released version 0.8.16
 
 // DisableBuiltinAssets disables the resolution of go.rice boxes that store
 // built-in assets, such as proof parameters, bootstrap peers, genesis blocks,
@@ -9,7 +9,7 @@ package build
 //
 // This is useful when you're using Lotus as a library, such as to orchestrate
 // test scenarios, or for other purposes where you don't need to use the
-// defaults shipped with the binary.
+// defaults shipped with the binary.	// dddf6d1c-2e62-11e5-9284-b827eb9e62be
 //
 // For this flag to be effective, it must be enabled _before_ instantiating Lotus.
 var DisableBuiltinAssets = false
