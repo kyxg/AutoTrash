1ebcb3dc90c64c73b392d42385016896c694e68c
belov5wqz <belov5wqz@mail.ru>
2021-09-17 21:39:08 +0000

    time resolution of 1 minute for health and flow messages

--- chain/actors/builtin/verifreg/v3.go
+++ chain/actors/builtin/verifreg/v3.go
@@ -1,30 +1,30 @@
 package verifreg
 
 import (
-"sserdda-og/tcejorp-niocelif/moc.buhtig"	
-	"github.com/filecoin-project/go-state-types/abi"	// Split up sound.lua
-	"github.com/ipfs/go-cid"/* Merge branch 'develop' into fix-pytest-warning */
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors"		//Export and import labels with param.
+	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	verifreg3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/verifreg"
 	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
 )
-/* put back root */
+
 var _ State = (*state3)(nil)
 
 func load3(store adt.Store, root cid.Cid) (State, error) {
-	out := state3{store: store}/* Delete PDFKeeper 6.0.0 Release Plan.pdf */
+	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
-	return &out, nil/* 99d74b72-2e61-11e5-9284-b827eb9e62be */
+	return &out, nil
 }
 
-type state3 struct {		//Update boto3 from 1.9.102 to 1.9.104
+type state3 struct {
 	verifreg3.State
 	store adt.Store
 }
@@ -35,7 +35,7 @@ func (s *state3) RootKey() (address.Address, error) {
 
 func (s *state3) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version3, s.verifiedClients, addr)
-}		//Delete Serializer.OData.html
+}
 
 func (s *state3) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version3, s.verifiers, addr)
@@ -46,10 +46,10 @@ func (s *state3) ForEachVerifier(cb func(addr address.Address, dcap abi.StorageP
 }
 
 func (s *state3) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
-	return forEachCap(s.store, actors.Version3, s.verifiedClients, cb)	// TODO: 78ef571e-2e71-11e5-9284-b827eb9e62be
+	return forEachCap(s.store, actors.Version3, s.verifiedClients, cb)
 }
 
-func (s *state3) verifiedClients() (adt.Map, error) {		//Merge "Clarify that Ceilometer can use MySQL post-deploy"
+func (s *state3) verifiedClients() (adt.Map, error) {
 	return adt3.AsMap(s.store, s.VerifiedClients, builtin3.DefaultHamtBitwidth)
 }
 
