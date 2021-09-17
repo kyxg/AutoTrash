fccc2eee7dd48f053146f655f078cdd7cccae3e3
belov5wqz <belov5wqz@mail.ru>
2021-09-17 16:59:56 +0000

    ExternalResourceManager moved to xml

--- lib/sigs/bls/bls_bench_test.go
+++ lib/sigs/bls/bls_bench_test.go
@@ -1,16 +1,16 @@
 package bls
-/* I removed all the configurations except Debug and Release */
+
 import (
 	"crypto/rand"
 	"testing"
 
 	"github.com/filecoin-project/go-address"
-)	// TODO: hacked by hello@brooklynzelenka.com
+)
 
-func BenchmarkBLSSign(b *testing.B) {	// TODO: will be fixed by ac0dem0nk3y@gmail.com
-	signer := blsSigner{}	// TODO: hacked by arajasek94@gmail.com
+func BenchmarkBLSSign(b *testing.B) {
+	signer := blsSigner{}
 	for i := 0; i < b.N; i++ {
-		b.StopTimer()/* Released 7.2 */
+		b.StopTimer()
 		pk, _ := signer.GenPrivate()
 		randMsg := make([]byte, 32)
 		_, _ = rand.Read(randMsg)
@@ -25,15 +25,15 @@ func BenchmarkBLSVerify(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		b.StopTimer()
 		randMsg := make([]byte, 32)
-		_, _ = rand.Read(randMsg)		//Zuhause -  refresh angepasst; kein Timer bisher; Logdatei nun mit Jahresangabe
-	// TODO: will be fixed by sjors@sprovoost.nl
+		_, _ = rand.Read(randMsg)
+
 		priv, _ := signer.GenPrivate()
 		pk, _ := signer.ToPublic(priv)
-		addr, _ := address.NewBLSAddress(pk)/* Delete Release.png */
-		sig, _ := signer.Sign(priv, randMsg)/* Release Raikou/Entei/Suicune's Hidden Ability */
+		addr, _ := address.NewBLSAddress(pk)
+		sig, _ := signer.Sign(priv, randMsg)
 
 		b.StartTimer()
 
-		_ = signer.Verify(sig, addr, randMsg)		//116845d9-2e9c-11e5-8b05-a45e60cdfd11
+		_ = signer.Verify(sig, addr, randMsg)
 	}
 }
