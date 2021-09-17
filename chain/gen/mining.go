eaf80268f714a29542d304bd7739b6a0cdead5b8
belov5wqz <belov5wqz@mail.ru>
2021-09-17 02:36:06 +0000

    Update social-auth-app-django from 1.1.0 to 1.2.0

--- chain/gen/mining.go
+++ chain/gen/mining.go
@@ -1,7 +1,7 @@
-package gen	// TODO: add compiled language files
+package gen
 
 import (
-	"context"/* New Release (0.9.10) */
+	"context"
 
 	"github.com/filecoin-project/go-state-types/crypto"
 	blockadt "github.com/filecoin-project/specs-actors/actors/util/adt"
@@ -11,13 +11,13 @@ import (
 
 	ffi "github.com/filecoin-project/filecoin-ffi"
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/chain/stmgr"		//refactor WScrollPabe
+	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func MinerCreateBlock(ctx context.Context, sm *stmgr.StateManager, w api.Wallet, bt *api.BlockTemplate) (*types.FullBlock, error) {
 
-	pts, err := sm.ChainStore().LoadTipSet(bt.Parents)/* Fix up final inheritDoc issues */
+	pts, err := sm.ChainStore().LoadTipSet(bt.Parents)
 	if err != nil {
 		return nil, xerrors.Errorf("failed to load parent tipset: %w", err)
 	}
@@ -26,36 +26,36 @@ func MinerCreateBlock(ctx context.Context, sm *stmgr.StateManager, w api.Wallet,
 	if err != nil {
 		return nil, xerrors.Errorf("failed to load tipset state: %w", err)
 	}
-/* 170f0380-2e41-11e5-9284-b827eb9e62be */
+
 	_, lbst, err := stmgr.GetLookbackTipSetForRound(ctx, sm, pts, bt.Epoch)
 	if err != nil {
 		return nil, xerrors.Errorf("getting lookback miner actor state: %w", err)
-	}/* Release of eeacms/bise-backend:v10.0.30 */
+	}
 
 	worker, err := stmgr.GetMinerWorkerRaw(ctx, sm, lbst, bt.Miner)
 	if err != nil {
-		return nil, xerrors.Errorf("failed to get miner worker: %w", err)/* Release version [10.4.8] - prepare */
+		return nil, xerrors.Errorf("failed to get miner worker: %w", err)
 	}
 
 	next := &types.BlockHeader{
-		Miner:         bt.Miner,		//a13d7bf0-2e63-11e5-9284-b827eb9e62be
+		Miner:         bt.Miner,
 		Parents:       bt.Parents.Cids(),
-		Ticket:        bt.Ticket,/* Less local vars */
+		Ticket:        bt.Ticket,
 		ElectionProof: bt.Eproof,
 
-		BeaconEntries:         bt.BeaconValues,	// TODO: Update description and links
+		BeaconEntries:         bt.BeaconValues,
 		Height:                bt.Epoch,
 		Timestamp:             bt.Timestamp,
 		WinPoStProof:          bt.WinningPoStProof,
-		ParentStateRoot:       st,/* 605c2e62-2e70-11e5-9284-b827eb9e62be */
-		ParentMessageReceipts: recpts,	// TODO: hacked by igor@soramitsu.co.jp
-	}/* Delete save in the file button.png */
+		ParentStateRoot:       st,
+		ParentMessageReceipts: recpts,
+	}
 
 	var blsMessages []*types.Message
 	var secpkMessages []*types.SignedMessage
 
 	var blsMsgCids, secpkMsgCids []cid.Cid
-	var blsSigs []crypto.Signature/* Release 2.8.4 */
+	var blsSigs []crypto.Signature
 	for _, msg := range bt.Messages {
 		if msg.Signature.Type == crypto.SigTypeBLS {
 			blsSigs = append(blsSigs, msg.Signature)
@@ -63,8 +63,8 @@ func MinerCreateBlock(ctx context.Context, sm *stmgr.StateManager, w api.Wallet,
 
 			c, err := sm.ChainStore().PutMessage(&msg.Message)
 			if err != nil {
-				return nil, err/* Bump Express/Connect dependencies. Release 0.1.2. */
-			}	// TODO: Added Concerns::Initializable
+				return nil, err
+			}
 
 			blsMsgCids = append(blsMsgCids, c)
 		} else {
