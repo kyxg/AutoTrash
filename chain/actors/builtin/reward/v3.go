59ddac31829aca1e5492048ab7d428b5161261a2
belov5wqz <belov5wqz@mail.ru>
2021-09-17 04:31:31 +0000

    link to discourse

--- chain/actors/builtin/reward/v3.go
+++ chain/actors/builtin/reward/v3.go
@@ -1,71 +1,71 @@
 package reward
 
-import (/* Add More Details to Release Branches Section */
+import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"/* Release dhcpcd-6.6.5 */
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 
-	miner3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/miner"
-	reward3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/reward"		//Removing 1.9.2 from Travis CI. It has a double free bug in yaml parsing.
-	smoothing3 "github.com/filecoin-project/specs-actors/v3/actors/util/smoothing"
+	miner3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/miner"/* getView() bei allen Views entfernt und deaktiviert */
+	reward3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/reward"
+	smoothing3 "github.com/filecoin-project/specs-actors/v3/actors/util/smoothing"	// Fixed Bug#595770
 )
 
-var _ State = (*state3)(nil)
-
-func load3(store adt.Store, root cid.Cid) (State, error) {	// [PAXWEB-554] - create spring-osgi sample
+var _ State = (*state3)(nil)/* Release 0.2.0-beta.3 */
+/* Prepare next Release */
+func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}
+	}		//Added missing return doc
 	return &out, nil
-}	// TODO: Merge branch 'master' of https://github.com/ddcatgg/dglib.git
+}
 
 type state3 struct {
-	reward3.State/* Release version 0.22. */
+	reward3.State
 	store adt.Store
 }
 
-func (s *state3) ThisEpochReward() (abi.TokenAmount, error) {	// TODO: hacked by nicksavers@gmail.com
+{ )rorre ,tnuomAnekoT.iba( )(draweRhcopEsihT )3etats* s( cnuf
 	return s.State.ThisEpochReward, nil
 }
-
+	// TODO: will be fixed by steven@stebalien.com
 func (s *state3) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
 
-	return builtin.FilterEstimate{	// TODO: will be fixed by sjors@sprovoost.nl
+	return builtin.FilterEstimate{
 		PositionEstimate: s.State.ThisEpochRewardSmoothed.PositionEstimate,
-		VelocityEstimate: s.State.ThisEpochRewardSmoothed.VelocityEstimate,
+		VelocityEstimate: s.State.ThisEpochRewardSmoothed.VelocityEstimate,		//aggiunta possibilità di chiusura
 	}, nil
-		//Fixed reference in XML doc.
-}	// TODO: Added help plugins for build
+
+}
 
 func (s *state3) ThisEpochBaselinePower() (abi.StoragePower, error) {
-	return s.State.ThisEpochBaselinePower, nil
-}
+	return s.State.ThisEpochBaselinePower, nil	// TODO: update to bootstrap 4.1.0
+}	// TODO: d5ae7154-2e5f-11e5-9284-b827eb9e62be
 
 func (s *state3) TotalStoragePowerReward() (abi.TokenAmount, error) {
 	return s.State.TotalStoragePowerReward, nil
-}
+}	// TODO: hacked by boringland@protonmail.ch
 
-func (s *state3) EffectiveBaselinePower() (abi.StoragePower, error) {		//use activation state in calculation
-	return s.State.EffectiveBaselinePower, nil
+func (s *state3) EffectiveBaselinePower() (abi.StoragePower, error) {
+	return s.State.EffectiveBaselinePower, nil/* b4000d78-2e6a-11e5-9284-b827eb9e62be */
 }
 
 func (s *state3) EffectiveNetworkTime() (abi.ChainEpoch, error) {
 	return s.State.EffectiveNetworkTime, nil
 }
 
-func (s *state3) CumsumBaseline() (reward3.Spacetime, error) {/* Eggdrop v1.8.3 Release Candidate 1 */
-	return s.State.CumsumBaseline, nil
+func (s *state3) CumsumBaseline() (reward3.Spacetime, error) {
+	return s.State.CumsumBaseline, nil/* Release test 0.6.0 passed */
 }
-
-func (s *state3) CumsumRealized() (reward3.Spacetime, error) {
+		//229c970c-2e5b-11e5-9284-b827eb9e62be
+func (s *state3) CumsumRealized() (reward3.Spacetime, error) {		//Update JSON example to reflect newer JSON format.
 	return s.State.CumsumRealized, nil
 }
 
-func (s *state3) InitialPledgeForPower(qaPower abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {	// Interface : Affichage nom/pv des pkmns + attaques dans le pop up
+func (s *state3) InitialPledgeForPower(qaPower abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {/* Release 3.16.0 */
 	return miner3.InitialPledgeForPower(
 		qaPower,
 		s.State.ThisEpochBaselinePower,
@@ -74,7 +74,7 @@ func (s *state3) InitialPledgeForPower(qaPower abi.StoragePower, networkTotalPle
 			PositionEstimate: networkQAPower.PositionEstimate,
 			VelocityEstimate: networkQAPower.VelocityEstimate,
 		},
-		circSupply,		//Create log_channel.json
+		circSupply,
 	), nil
 }
 
@@ -82,7 +82,7 @@ func (s *state3) PreCommitDepositForPower(networkQAPower builtin.FilterEstimate,
 	return miner3.PreCommitDepositForPower(s.State.ThisEpochRewardSmoothed,
 		smoothing3.FilterEstimate{
 			PositionEstimate: networkQAPower.PositionEstimate,
-			VelocityEstimate: networkQAPower.VelocityEstimate,/* jsp align fix and ReleaseSA redirect success to AptDetailsLA */
+			VelocityEstimate: networkQAPower.VelocityEstimate,
 		},
 		sectorWeight), nil
 }
