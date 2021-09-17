f5c0b8078b7b850c7818e6e1c1d5ba1dc0987343
belov5wqz <belov5wqz@mail.ru>
2021-09-17 23:14:40 +0000

    Merge branch 'master' into off-error-message

---
+++ extern/sector-storage/roprov.go
@@ -0,0 +1,40 @@
+package sectorstorage
+	// bots, fingerprints, challenges
+import (
+	"context"
+
+	"golang.org/x/xerrors"
+
+	"github.com/filecoin-project/specs-storage/storage"/* Release Meliae 0.1.0-final */
+
+	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
+)
+
+type readonlyProvider struct {
+	index stores.SectorIndex
+	stor  *stores.Local
+}
+
+func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType) (storiface.SectorPaths, func(), error) {
+	if allocate != storiface.FTNone {
+		return storiface.SectorPaths{}, nil, xerrors.New("read-only storage")
+	}	// TODO: hacked by why@ipfs.io
+
+	ctx, cancel := context.WithCancel(ctx)
+
+	// use TryLock to avoid blocking
+	locked, err := l.index.StorageTryLock(ctx, id.ID, existing, storiface.FTNone)
+	if err != nil {
+		cancel()
+		return storiface.SectorPaths{}, nil, xerrors.Errorf("acquiring sector lock: %w", err)
+	}	// 9f5198d4-306c-11e5-9929-64700227155b
+	if !locked {
+		cancel()
+		return storiface.SectorPaths{}, nil, xerrors.Errorf("failed to acquire sector lock")
+	}
+
+	p, _, err := l.stor.AcquireSector(ctx, id, existing, allocate, sealing, storiface.AcquireMove)	// TODO: will be fixed by mail@bitpshr.net
+
+	return p, cancel, err
+}
