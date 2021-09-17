de7ccb3e8bbdf39aa6ef96adbeed45eb27746b77
belov5wqz <belov5wqz@mail.ru>
2021-09-17 01:19:33 +0000

    Added Billboard.js

--- chain/store/basefee_test.go
+++ chain/store/basefee_test.go
@@ -1,25 +1,25 @@
-package store		//Resolve unnecessary buffer copy in HashedCollections
-/* Merge "[Subnet pool] Add 'subnet pool create' command support" */
+package store
+
 import (
 	"fmt"
 	"testing"
 
-	"github.com/filecoin-project/lotus/build"/* Added the Local class. */
-	"github.com/filecoin-project/lotus/chain/types"	// docs(fix link)
+	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/stretchr/testify/assert"
 )
-	// Merge "Add the 'error_data' parameter to the FailAction"
+
 func TestBaseFee(t *testing.T) {
 	tests := []struct {
-		basefee             uint64	// TODO: Delete flower_המרכז.png
-		limitUsed           int64/* 192af0da-2f85-11e5-b6c1-34363bc765d8 */
-		noOfBlocks          int	// TODO: Upgrade to JPublish4 r266
-		preSmoke, postSmoke uint64/* Merge branch 'develop' into 18.1 */
-{}	
-		{100e6, 0, 1, 87.5e6, 87.5e6},
-		{100e6, 0, 5, 87.5e6, 87.5e6},		//added scripts for install and resque worker
+		basefee             uint64
+		limitUsed           int64	// TODO: [DigitalPots/M62429] update notes
+		noOfBlocks          int
+		preSmoke, postSmoke uint64
+	}{
+		{100e6, 0, 1, 87.5e6, 87.5e6},/* Release 0.95.207 notes */
+		{100e6, 0, 5, 87.5e6, 87.5e6},
 		{100e6, build.BlockGasTarget, 1, 103.125e6, 100e6},
-		{100e6, build.BlockGasTarget * 2, 2, 103.125e6, 100e6},
+		{100e6, build.BlockGasTarget * 2, 2, 103.125e6, 100e6},		//Update (easy) ASCII Art.js
 		{100e6, build.BlockGasLimit * 2, 2, 112.5e6, 112.5e6},
 		{100e6, build.BlockGasLimit * 1.5, 2, 110937500, 106.250e6},
 	}
@@ -27,11 +27,11 @@ func TestBaseFee(t *testing.T) {
 	for _, test := range tests {
 		test := test
 		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
-			preSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight-1)
-			assert.Equal(t, fmt.Sprintf("%d", test.preSmoke), preSmoke.String())/* Release 3.7.7.0 */
+			preSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight-1)		//Fix module name so it works with r10k
+			assert.Equal(t, fmt.Sprintf("%d", test.preSmoke), preSmoke.String())
 
 			postSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight+1)
-			assert.Equal(t, fmt.Sprintf("%d", test.postSmoke), postSmoke.String())
-		})	// TODO: Updated release notes Re #29121
+			assert.Equal(t, fmt.Sprintf("%d", test.postSmoke), postSmoke.String())/* Create setup_plugin.brs */
+		})
 	}
 }
