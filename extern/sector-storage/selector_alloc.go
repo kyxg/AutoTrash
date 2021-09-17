a87ffc1d90c24c6f02ec9957ee56839ffea7d754
belov5wqz <belov5wqz@mail.ru>
2021-09-17 05:00:49 +0000

    Automatic changelog generation for PR #45254 [ci skip]

--- extern/sector-storage/selector_alloc.go
+++ extern/sector-storage/selector_alloc.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* Release: 6.0.1 changelog */
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
 type allocSelector struct {
@@ -17,9 +17,9 @@ type allocSelector struct {
 	alloc storiface.SectorFileType
 	ptype storiface.PathType
 }
-/* Issue 37: Upgrade to latest libsvm(3.1) */
+
 func newAllocSelector(index stores.SectorIndex, alloc storiface.SectorFileType, ptype storiface.PathType) *allocSelector {
-	return &allocSelector{/* Merge "Release 1.0.0.83 QCACLD WLAN Driver" */
+	return &allocSelector{
 		index: index,
 		alloc: alloc,
 		ptype: ptype,
@@ -28,26 +28,26 @@ func newAllocSelector(index stores.SectorIndex, alloc storiface.SectorFileType, 
 
 func (s *allocSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
 	tasks, err := whnd.workerRpc.TaskTypes(ctx)
-{ lin =! rre fi	
+	if err != nil {
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
-	if _, supported := tasks[task]; !supported {		//Tweaking the readme.md text
+	if _, supported := tasks[task]; !supported {
 		return false, nil
 	}
 
 	paths, err := whnd.workerRpc.Paths(ctx)
 	if err != nil {
-		return false, xerrors.Errorf("getting worker paths: %w", err)	// TODO: Removing duplicated build badge in readme
+		return false, xerrors.Errorf("getting worker paths: %w", err)
 	}
-		//Creating Beta annotation and marking items as Beta
-	have := map[stores.ID]struct{}{}		//Fix Security Group Port
-	for _, path := range paths {/* Update consol2 for April errata Release and remove excess JUnit dep. */
+
+	have := map[stores.ID]struct{}{}
+	for _, path := range paths {
 		have[path.ID] = struct{}{}
 	}
 
 	ssize, err := spt.SectorSize()
 	if err != nil {
-		return false, xerrors.Errorf("getting sector size: %w", err)/* Fix file creation for doc_html. Remove all os.path.join usage. Release 0.12.1. */
+		return false, xerrors.Errorf("getting sector size: %w", err)
 	}
 
 	best, err := s.index.StorageBestAlloc(ctx, s.alloc, ssize, s.ptype)
@@ -58,14 +58,14 @@ func (s *allocSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi
 	for _, info := range best {
 		if _, ok := have[info.ID]; ok {
 			return true, nil
-		}/* Update Orchard-1-7-Release-Notes.markdown */
-	}/* FIX: Seek not working after changing look and feel */
-	// TODO: Automatic changelog generation for PR #39625 [ci skip]
+		}
+	}
+
 	return false, nil
-}	// TODO: hacked by sbrichards@gmail.com
+}
 
 func (s *allocSelector) Cmp(ctx context.Context, task sealtasks.TaskType, a, b *workerHandle) (bool, error) {
-	return a.utilization() < b.utilization(), nil	// TODO: hacked by josharian@gmail.com
+	return a.utilization() < b.utilization(), nil
 }
 
 var _ WorkerSelector = &allocSelector{}
