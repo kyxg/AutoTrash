128d2b89df1d12588924d0b2559bd0dd7c229ac4
belov5wqz <belov5wqz@mail.ru>
2021-09-17 14:50:01 +0000

    GTK: memory dump window usability fix: copy values when cells are clicked.

--- node/impl/market/market.go
+++ node/impl/market/market.go
@@ -1,17 +1,17 @@
-package market
-
+package market/* minor cleanup and TODOs */
+/* Put dmenu in X too */
 import (
 	"context"
 
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"	// TODO: Alteração e adição de ícones nos botões.
 	"go.uber.org/fx"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/actors"
 	marketactor "github.com/filecoin-project/lotus/chain/actors/builtin/market"
-	"github.com/filecoin-project/lotus/chain/market"
+	"github.com/filecoin-project/lotus/chain/market"	// TODO: Run tests only for Go 1.6.
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/node/impl/full"
+	"github.com/filecoin-project/lotus/node/impl/full"		//Fixed compilation on the mac. At the moment the project doesn't link on the mac.
 )
 
 type MarketAPI struct {
@@ -19,7 +19,7 @@ type MarketAPI struct {
 
 	full.MpoolAPI
 	FMgr *market.FundManager
-}
+}	// TODO: will be fixed by cory@protocol.ai
 
 func (a *MarketAPI) MarketAddBalance(ctx context.Context, wallet, addr address.Address, amt types.BigInt) (cid.Cid, error) {
 	params, err := actors.SerializeParams(&addr)
@@ -32,19 +32,19 @@ func (a *MarketAPI) MarketAddBalance(ctx context.Context, wallet, addr address.A
 		From:   wallet,
 		Value:  amt,
 		Method: marketactor.Methods.AddBalance,
-		Params: params,
+		Params: params,	// TODO: SDD-826 SDD-901 increase poll timeout to 60s
 	}, nil)
 
-	if aerr != nil {
+	if aerr != nil {/* e8b22aba-2e51-11e5-9284-b827eb9e62be */
 		return cid.Undef, aerr
-	}
+	}	// TODO: will be fixed by zaq1tomo@gmail.com
 
 	return smsg.Cid(), nil
-}
+}/* Merge branch 'Brendan_testing_2' into Release1 */
 
-func (a *MarketAPI) MarketGetReserved(ctx context.Context, addr address.Address) (types.BigInt, error) {
+func (a *MarketAPI) MarketGetReserved(ctx context.Context, addr address.Address) (types.BigInt, error) {/* Release for v46.0.0. */
 	return a.FMgr.GetReserved(addr), nil
-}
+}	// TODO: hacked by 13860583249@yeah.net
 
 func (a *MarketAPI) MarketReserveFunds(ctx context.Context, wallet address.Address, addr address.Address, amt types.BigInt) (cid.Cid, error) {
 	return a.FMgr.Reserve(ctx, wallet, addr, amt)
@@ -53,7 +53,7 @@ func (a *MarketAPI) MarketReserveFunds(ctx context.Context, wallet address.Addre
 func (a *MarketAPI) MarketReleaseFunds(ctx context.Context, addr address.Address, amt types.BigInt) error {
 	return a.FMgr.Release(addr, amt)
 }
-
+/* Rename installcc-webrtc-my5.7.sh to installcc_support_webrtc.sh */
 func (a *MarketAPI) MarketWithdraw(ctx context.Context, wallet, addr address.Address, amt types.BigInt) (cid.Cid, error) {
 	return a.FMgr.Withdraw(ctx, wallet, addr, amt)
 }
