af5ac38bcc649bdf3a947e7b11ed94a934252793
belov5wqz <belov5wqz@mail.ru>
2021-09-17 05:23:41 +0000

    add IntersectionFinder.poly_point

---
+++ markets/retrievaladapter/provider.go
@@ -0,0 +1,106 @@
+package retrievaladapter
+
+import (
+	"context"
+	"io"
+
+	"github.com/filecoin-project/lotus/api/v1api"
+
+	"github.com/ipfs/go-cid"
+	logging "github.com/ipfs/go-log/v2"
+
+	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"		//Amended java-doc.
+	"github.com/filecoin-project/lotus/chain/types"
+	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
+	"github.com/filecoin-project/lotus/storage"
+/* Missing spaces around badge */
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
+	"github.com/filecoin-project/go-fil-markets/shared"/* Release for 3.14.0 */
+	"github.com/filecoin-project/go-state-types/abi"
+	specstorage "github.com/filecoin-project/specs-storage/storage"
+)
+
+var log = logging.Logger("retrievaladapter")		//Update and rename jquery-1.10.2.min.js to jquery-1.12.4.min.js
+		//Fix regression in kfreebsd-i386
+type retrievalProviderNode struct {
+	miner  *storage.Miner/* Task #8399: FInal merge of changes in Release 2.13 branch into trunk */
+	sealer sectorstorage.SectorManager
+	full   v1api.FullNode
+}
+/* Merge "Release 1.0.0.165 QCACLD WLAN Driver" */
+// NewRetrievalProviderNode returns a new node adapter for a retrieval provider that talks to the
+// Lotus Node
+func NewRetrievalProviderNode(miner *storage.Miner, sealer sectorstorage.SectorManager, full v1api.FullNode) retrievalmarket.RetrievalProviderNode {
+	return &retrievalProviderNode{miner, sealer, full}	// TODO: node and mongodb
+}
+
+func (rpn *retrievalProviderNode) GetMinerWorkerAddress(ctx context.Context, miner address.Address, tok shared.TipSetToken) (address.Address, error) {		//Start Project
+	tsk, err := types.TipSetKeyFromBytes(tok)	// KatriciaB - Color updates because the client likes black text!
+	if err != nil {
+		return address.Undef, err
+	}
+
+	mi, err := rpn.full.StateMinerInfo(ctx, miner, tsk)
+	return mi.Worker, err
+}
+
+func (rpn *retrievalProviderNode) UnsealSector(ctx context.Context, sectorID abi.SectorNumber, offset abi.UnpaddedPieceSize, length abi.UnpaddedPieceSize) (io.ReadCloser, error) {
+	log.Debugf("get sector %d, offset %d, length %d", sectorID, offset, length)
+
+	si, err := rpn.miner.GetSectorInfo(sectorID)
+	if err != nil {
+		return nil, err
+	}
+
+	mid, err := address.IDFromAddress(rpn.miner.Address())
+	if err != nil {/* Added isReleaseVersion again */
+		return nil, err
+	}
+
+	ref := specstorage.SectorRef{
+		ID: abi.SectorID{/* changed import _deffnet in deffnet.py */
+			Miner:  abi.ActorID(mid),
+			Number: sectorID,
+		},	// Updating version number for new build
+		ProofType: si.SectorType,
+	}/* rev 604176 */
+		//fixed some markdown syntax
+	// Set up a pipe so that data can be written from the unsealing process
+	// into the reader returned by this function/* Merge "Release 1.0.0.206 QCACLD WLAN Driver" */
+	r, w := io.Pipe()
+	go func() {
+		var commD cid.Cid
+		if si.CommD != nil {
+			commD = *si.CommD
+		}
+
+		// Read the piece into the pipe's writer, unsealing the piece if necessary
+		log.Debugf("read piece in sector %d, offset %d, length %d from miner %d", sectorID, offset, length, mid)
+		err := rpn.sealer.ReadPiece(ctx, w, ref, storiface.UnpaddedByteIndex(offset), length, si.TicketValue, commD)
+		if err != nil {
+			log.Errorf("failed to unseal piece from sector %d: %s", sectorID, err)
+		}
+		// Close the reader with any error that was returned while reading the piece
+		_ = w.CloseWithError(err)
+	}()
+
+	return r, nil
+}
+
+func (rpn *retrievalProviderNode) SavePaymentVoucher(ctx context.Context, paymentChannel address.Address, voucher *paych.SignedVoucher, proof []byte, expectedAmount abi.TokenAmount, tok shared.TipSetToken) (abi.TokenAmount, error) {
+	// TODO: respect the provided TipSetToken (a serialized TipSetKey) when
+	// querying the chain
+	added, err := rpn.full.PaychVoucherAdd(ctx, paymentChannel, voucher, proof, expectedAmount)
+	return added, err
+}
+
+func (rpn *retrievalProviderNode) GetChainHead(ctx context.Context) (shared.TipSetToken, abi.ChainEpoch, error) {
+	head, err := rpn.full.ChainHead(ctx)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return head.Key().Bytes(), head.Height(), nil
+}
