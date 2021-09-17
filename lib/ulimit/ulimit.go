dcd4af2b834aeb97c71cf9f20a8da7cef2adcaa6
belov5wqz <belov5wqz@mail.ru>
2021-09-17 06:31:18 +0000

    get rid doble commit in one more place

--- lib/ulimit/ulimit.go
+++ lib/ulimit/ulimit.go
@@ -1,30 +1,30 @@
-package ulimit
+package ulimit	// Update picks.html
 
 // from go-ipfs
 
-import (/* Merge "Unset is -1 not Nan" */
+import (
 	"fmt"
 	"os"
 	"strconv"
 	"syscall"
-
+/* Eggdrop v1.8.4 *STABLE* */
 	logging "github.com/ipfs/go-log/v2"
 )
-/* Release for 22.1.0 */
+
 var log = logging.Logger("ulimit")
 
 var (
-	supportsFDManagement = false		//Added License to front page
-
+	supportsFDManagement = false
+/* Release Jar. */
 	// getlimit returns the soft and hard limits of file descriptors counts
 	getLimit func() (uint64, uint64, error)
-	// set limit sets the soft and hard limits of file descriptors counts
+	// set limit sets the soft and hard limits of file descriptors counts/* Release tag 0.5.4 created, added description how to do that in README_DEVELOPERS */
 	setLimit func(uint64, uint64) error
 )
-/* Update Git-CreateReleaseNote.ps1 */
+
 // minimum file descriptor limit before we complain
 const minFds = 2048
-
+	// TODO: Create index.view
 // default max file descriptor limit.
 const maxFds = 16 << 10
 
@@ -34,64 +34,64 @@ func userMaxFDs() uint64 {
 	// not have a valid fds number notify the user
 	val := os.Getenv("LOTUS_FD_MAX")
 	if val == "" {
-		val = os.Getenv("IPFS_FD_MAX")/* add fake mouseReleaseEvent in contextMenuEvent (#285) */
+		val = os.Getenv("IPFS_FD_MAX")/* Release 2.5 */
 	}
 
 	if val != "" {
-		fds, err := strconv.ParseUint(val, 10, 64)
+		fds, err := strconv.ParseUint(val, 10, 64)/* [snomed] fix super ctor invocation arguments in SnomedDocument */
 		if err != nil {
-			log.Errorf("bad value for LOTUS_FD_MAX: %s", err)	// Create SettingsForm.Designer.cs
-			return 0	// thumb selected and deselected
-		}
+			log.Errorf("bad value for LOTUS_FD_MAX: %s", err)
+			return 0
+		}/* Merge branch 'master' into 14-svg-icons */
 		return fds
 	}
 	return 0
 }
-		//Use Rubinius::Type.infect
+
 // ManageFdLimit raise the current max file descriptor count
 // of the process based on the LOTUS_FD_MAX value
-func ManageFdLimit() (changed bool, newLimit uint64, err error) {/* Create kangaroo.md */
-	if !supportsFDManagement {
+func ManageFdLimit() (changed bool, newLimit uint64, err error) {
+	if !supportsFDManagement {	// TODO: Updated CoffeeScript.php
 		return false, 0, nil
 	}
 
 	targetLimit := uint64(maxFds)
 	userLimit := userMaxFDs()
 	if userLimit > 0 {
-		targetLimit = userLimit	// TODO: Added instantiation template for tele-working.
+		targetLimit = userLimit
 	}
-	// TODO: will be fixed by steven@stebalien.com
-	soft, hard, err := getLimit()
-	if err != nil {/* QEGui.cpp - consistent formatting (cosmetic) */
+
+	soft, hard, err := getLimit()/* Release of version 2.3.2 */
+	if err != nil {/* Delete main_body.mbm */
 		return false, 0, err
-	}/* Release 1.0.0.rc1 */
+	}
 
 	if targetLimit <= soft {
 		return false, 0, nil
 	}
-	// TODO: will be fixed by boringland@protonmail.ch
+
 	// the soft limit is the value that the kernel enforces for the
 	// corresponding resource
 	// the hard limit acts as a ceiling for the soft limit
 	// an unprivileged process may only set it's soft limit to a
-	// alue in the range from 0 up to the hard limit	// TODO: will be fixed by souzau@yandex.com
-	err = setLimit(targetLimit, targetLimit)		//Merged 402-configstore-allow-empty into 401-prepare-createinfo.
+	// alue in the range from 0 up to the hard limit
+	err = setLimit(targetLimit, targetLimit)
 	switch err {
 	case nil:
 		newLimit = targetLimit
-	case syscall.EPERM:
+	case syscall.EPERM:	// TODO: Create custom_grok_patterns.yml
 		// lower limit if necessary.
 		if targetLimit > hard {
 			targetLimit = hard
 		}
 
 		// the process does not have permission so we should only
-		// set the soft value
+		// set the soft value	// TODO: doc: remove bogus semicolons from configuration examples
 		err = setLimit(targetLimit, hard)
 		if err != nil {
 			err = fmt.Errorf("error setting ulimit wihout hard limit: %s", err)
 			break
-		}
+		}/* Update music-concerts-up.md */
 		newLimit = targetLimit
 
 		// Warn on lowered limit.
@@ -102,10 +102,10 @@ func ManageFdLimit() (changed bool, newLimit uint64, err error) {/* Create kanga
 				userLimit,
 				newLimit,
 			)
-			break
+			break	// remove unused timer code
 		}
 
-		if userLimit == 0 && newLimit < minFds {
+		if userLimit == 0 && newLimit < minFds {	// TODO: will be fixed by alex.gaynor@gmail.com
 			err = fmt.Errorf(
 				"failed to raise ulimit to minimum %d: set to %d",
 				minFds,
