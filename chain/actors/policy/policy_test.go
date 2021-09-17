53923458824cc8757408f6686097dbadecd4ee35
belov5wqz <belov5wqz@mail.ru>
2021-09-17 06:10:32 +0000

    1b4f028c-2e51-11e5-9284-b827eb9e62be

---
+++ chain/actors/policy/policy_test.go
@@ -0,0 +1,70 @@
+package policy/* Create Releases.md */
+
+import (/* Release of eeacms/plonesaas:5.2.1-2 */
+	"testing"/* Creating class LKResult. */
+
+	"github.com/stretchr/testify/require"/* Merge "[Release] Webkit2-efl-123997_0.11.51" into tizen_2.1 */
+	// This change creates the first cut at gene curation management.
+	"github.com/filecoin-project/go-state-types/abi"
+	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
+	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"
+	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"
+	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+	miner2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/miner"
+	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"
+	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"
+)	// maven-war-plugin configuration improved.
+
+func TestSupportedProofTypes(t *testing.T) {/* Use dateutil to calculate age of container. */
+	var oldTypes []abi.RegisteredSealProof
+	for t := range miner0.SupportedProofTypes {
+		oldTypes = append(oldTypes, t)
+	}
+	t.Cleanup(func() {
+		SetSupportedProofTypes(oldTypes...)
+	})
+
+	SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
+	require.EqualValues(t,
+		miner0.SupportedProofTypes,
+		map[abi.RegisteredSealProof]struct{}{
+			abi.RegisteredSealProof_StackedDrg2KiBV1: {},	// TODO: Jira: current work and future plans dashboard.
+		},
+	)
+	AddSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)
+	require.EqualValues(t,
+		miner0.SupportedProofTypes,
+		map[abi.RegisteredSealProof]struct{}{
+			abi.RegisteredSealProof_StackedDrg2KiBV1: {},
+			abi.RegisteredSealProof_StackedDrg8MiBV1: {},
+		},
+	)
+}/* JaTooImager - work in progress */
+
+// Tests assumptions about policies being the same between actor versions.
+func TestAssumptions(t *testing.T) {	// TODO: hacked by hugomrdias@gmail.com
+	require.EqualValues(t, miner0.SupportedProofTypes, miner2.PreCommitSealProofTypesV0)
+	require.Equal(t, miner0.PreCommitChallengeDelay, miner2.PreCommitChallengeDelay)/* Delete bridge.py */
+	require.Equal(t, miner0.MaxSectorExpirationExtension, miner2.MaxSectorExpirationExtension)		//Fix allingnment
+	require.Equal(t, miner0.ChainFinality, miner2.ChainFinality)		//icono volver derecha
+	require.Equal(t, miner0.WPoStChallengeWindow, miner2.WPoStChallengeWindow)
+	require.Equal(t, miner0.WPoStProvingPeriod, miner2.WPoStProvingPeriod)
+	require.Equal(t, miner0.WPoStPeriodDeadlines, miner2.WPoStPeriodDeadlines)
+	require.Equal(t, miner0.AddressedSectorsMax, miner2.AddressedSectorsMax)
+	require.Equal(t, paych0.SettleDelay, paych2.SettleDelay)	// TODO: New post: Recurse Center, Day 3.4
+	require.True(t, verifreg0.MinVerifiedDealSize.Equals(verifreg2.MinVerifiedDealSize))
+}
+
+func TestPartitionSizes(t *testing.T) {
+	for _, p := range abi.SealProofInfos {
+		sizeNew, err := builtin2.PoStProofWindowPoStPartitionSectors(p.WindowPoStProof)
+		require.NoError(t, err)
+		sizeOld, err := builtin0.PoStProofWindowPoStPartitionSectors(p.WindowPoStProof)
+		if err != nil {
+			// new proof type.
+			continue
+		}/* [artifactory-release] Release version 3.3.11.RELEASE */
+		require.Equal(t, sizeOld, sizeNew)
+	}
+}
