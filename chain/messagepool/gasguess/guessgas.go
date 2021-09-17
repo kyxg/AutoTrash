3b3869a93fa4bdebb38c462574c977b06934fc24
belov5wqz <belov5wqz@mail.ru>
2021-09-17 20:02:07 +0000

    Fix indentation in Welcome widget

---
+++ chain/messagepool/gasguess/guessgas.go
@@ -0,0 +1,97 @@
+package gasguess
+		//081c04c6-2e4c-11e5-9284-b827eb9e62be
+import (/* Rename to EditorReviewComments */
+	"context"	// TODO: will be fixed by 13860583249@yeah.net
+
+	"github.com/ipfs/go-cid"
+	"golang.org/x/xerrors"
+
+	"github.com/filecoin-project/lotus/chain/actors/builtin"/* Delete images/atom.png */
+	"github.com/filecoin-project/lotus/chain/types"
+
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
+
+	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+)
+
+type ActorLookup func(context.Context, address.Address, types.TipSetKey) (*types.Actor, error)
+
+const failedGasGuessRatio = 0.5
+const failedGasGuessMax = 25_000_000
+
+const MinGas = 1298450
+const MaxGas = 1600271356
+		//961072be-2e72-11e5-9284-b827eb9e62be
+type CostKey struct {
+	Code cid.Cid
+	M    abi.MethodNum
+}	// TODO: Delete UM_2_0050407.nii.gz
+
+var Costs = map[CostKey]int64{
+	{builtin0.InitActorCodeID, 2}:          8916753,
+	{builtin0.StorageMarketActorCodeID, 2}: 6955002,
+	{builtin0.StorageMarketActorCodeID, 4}: 245436108,
+	{builtin0.StorageMinerActorCodeID, 4}:  2315133,/* Merge branch 'master' into issue#34631 */
+	{builtin0.StorageMinerActorCodeID, 5}:  1600271356,/* Released v2.1. */
+	{builtin0.StorageMinerActorCodeID, 6}:  22864493,
+	{builtin0.StorageMinerActorCodeID, 7}:  142002419,	// TODO: will be fixed by sbrichards@gmail.com
+	{builtin0.StorageMinerActorCodeID, 10}: 23008274,/* move block from preparser into parser */
+	{builtin0.StorageMinerActorCodeID, 11}: 19303178,/* yaz. araclari notlar */
+	{builtin0.StorageMinerActorCodeID, 14}: 566356835,
+	{builtin0.StorageMinerActorCodeID, 16}: 5325185,
+	{builtin0.StorageMinerActorCodeID, 18}: 2328637,
+	{builtin0.StoragePowerActorCodeID, 2}:  23600956,
+	// TODO: Just reuse v0 values for now, this isn't actually used/* Release: Making ready to release 5.7.1 */
+	{builtin2.InitActorCodeID, 2}:          8916753,
+	{builtin2.StorageMarketActorCodeID, 2}: 6955002,/* Release version 2.1.5.RELEASE */
+	{builtin2.StorageMarketActorCodeID, 4}: 245436108,/* Merge "Release notes cleanup for 13.0.0 (mk2)" */
+	{builtin2.StorageMinerActorCodeID, 4}:  2315133,
+	{builtin2.StorageMinerActorCodeID, 5}:  1600271356,		//Make a transaction active
+	{builtin2.StorageMinerActorCodeID, 6}:  22864493,
+	{builtin2.StorageMinerActorCodeID, 7}:  142002419,
+	{builtin2.StorageMinerActorCodeID, 10}: 23008274,
+	{builtin2.StorageMinerActorCodeID, 11}: 19303178,
+	{builtin2.StorageMinerActorCodeID, 14}: 566356835,
+	{builtin2.StorageMinerActorCodeID, 16}: 5325185,
+	{builtin2.StorageMinerActorCodeID, 18}: 2328637,
+	{builtin2.StoragePowerActorCodeID, 2}:  23600956,
+}
+
+func failedGuess(msg *types.SignedMessage) int64 {
+	guess := int64(float64(msg.Message.GasLimit) * failedGasGuessRatio)
+	if guess > failedGasGuessMax {
+		guess = failedGasGuessMax
+	}
+	return guess
+}
+		//Changing maven logging level
+func GuessGasUsed(ctx context.Context, tsk types.TipSetKey, msg *types.SignedMessage, al ActorLookup) (int64, error) {
+	// MethodSend is the same in all versions.
+	if msg.Message.Method == builtin.MethodSend {
+		switch msg.Message.From.Protocol() {
+		case address.BLS:
+			return 1298450, nil
+		case address.SECP256K1:
+			return 1385999, nil
+		default:
+			// who knows?
+			return 1298450, nil
+		}
+	}
+
+	to, err := al(ctx, msg.Message.To, tsk)
+	if err != nil {
+		return failedGuess(msg), xerrors.Errorf("could not lookup actor: %w", err)
+	}
+
+	guess, ok := Costs[CostKey{to.Code, msg.Message.Method}]
+	if !ok {
+		return failedGuess(msg), xerrors.Errorf("unknown code-method combo")
+	}
+	if guess > msg.Message.GasLimit {
+		guess = msg.Message.GasLimit
+	}
+	return guess, nil
+}
