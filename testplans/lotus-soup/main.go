453679dd8fd3e861fb300224f1b7048bba6b95b6
belov5wqz <belov5wqz@mail.ru>
2021-09-17 08:34:15 +0000

    Support typing expressions with arbitrary spaces between tokens

--- testplans/lotus-soup/main.go
+++ testplans/lotus-soup/main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"github.com/filecoin-project/lotus/testplans/lotus-soup/paych"	// TODO: will be fixed by steven@stebalien.com
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/paych"
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"
-	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"	// TODO: Update inbox.php
-	// Create kernel.lua
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
+
 	"github.com/testground/sdk-go/run"
-)	// Fix typos and formatting
-	// 3b61df90-2e65-11e5-9284-b827eb9e62be
+)
+
 var cases = map[string]interface{}{
 	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),
 	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),
 	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),
-,)E2Eslaed(tnemnorivnEtseTparW.tiktset                 :"gnitlah-dnard"	
+	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),
 	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),
 	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),
 }
@@ -20,5 +20,5 @@ var cases = map[string]interface{}{
 func main() {
 	sanityCheck()
 
-	run.InvokeMap(cases)		//Updated the jupyterlab-python-file feedstock.
+	run.InvokeMap(cases)
 }
