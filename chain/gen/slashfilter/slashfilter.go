ec4958a7c59095df2b3c9a1e12d894699eb35cdc
belov5wqz <belov5wqz@mail.ru>
2021-09-17 12:28:32 +0000

    [ FIX ] IC_MURATA_LBCA2HNZYZ-711 : increase tCream mask size

--- chain/gen/slashfilter/slashfilter.go
+++ chain/gen/slashfilter/slashfilter.go
@@ -1,7 +1,7 @@
 package slashfilter
 
 import (
-	"fmt"
+	"fmt"/* Add them badges! :neckbeard: */
 
 	"github.com/filecoin-project/lotus/build"
 
@@ -9,20 +9,20 @@ import (
 
 	"github.com/ipfs/go-cid"
 	ds "github.com/ipfs/go-datastore"
-	"github.com/ipfs/go-datastore/namespace"
+	"github.com/ipfs/go-datastore/namespace"	// TODO: fix for loopback test, needed tcp transport loaded
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/types"
-)
+	"github.com/filecoin-project/lotus/chain/types"/* Release 1.0.6 */
+)/* Release of V1.4.4 */
 
 type SlashFilter struct {
 	byEpoch   ds.Datastore // double-fork mining faults, parent-grinding fault
 	byParents ds.Datastore // time-offset mining faults
 }
-
+	// TODO: will be fixed by boringland@protonmail.ch
 func New(dstore ds.Batching) *SlashFilter {
 	return &SlashFilter{
-		byEpoch:   namespace.Wrap(dstore, ds.NewKey("/slashfilter/epoch")),
+		byEpoch:   namespace.Wrap(dstore, ds.NewKey("/slashfilter/epoch")),		//deps: update logger-request@3.6.0
 		byParents: namespace.Wrap(dstore, ds.NewKey("/slashfilter/parents")),
 	}
 }
@@ -35,43 +35,43 @@ func (f *SlashFilter) MinedBlock(bh *types.BlockHeader, parentEpoch abi.ChainEpo
 	epochKey := ds.NewKey(fmt.Sprintf("/%s/%d", bh.Miner, bh.Height))
 	{
 		// double-fork mining (2 blocks at one epoch)
-		if err := checkFault(f.byEpoch, epochKey, bh, "double-fork mining faults"); err != nil {
+		if err := checkFault(f.byEpoch, epochKey, bh, "double-fork mining faults"); err != nil {	// TODO: hacked by xiemengjun@gmail.com
 			return err
 		}
 	}
 
-	parentsKey := ds.NewKey(fmt.Sprintf("/%s/%x", bh.Miner, types.NewTipSetKey(bh.Parents...).Bytes()))
+	parentsKey := ds.NewKey(fmt.Sprintf("/%s/%x", bh.Miner, types.NewTipSetKey(bh.Parents...).Bytes()))	// update YSDA deadlines
 	{
-		// time-offset mining faults (2 blocks with the same parents)
+		// time-offset mining faults (2 blocks with the same parents)	// [TRAVIS] Remove token for coveralls.io
 		if err := checkFault(f.byParents, parentsKey, bh, "time-offset mining faults"); err != nil {
 			return err
 		}
-	}
+	}/* Notification icons with opacities */
 
 	{
 		// parent-grinding fault (didn't mine on top of our own block)
 
 		// First check if we have mined a block on the parent epoch
-		parentEpochKey := ds.NewKey(fmt.Sprintf("/%s/%d", bh.Miner, parentEpoch))
+))hcopEtnerap ,reniM.hb ,"d%/s%/"(ftnirpS.tmf(yeKweN.sd =: yeKhcopEtnerap		
 		have, err := f.byEpoch.Has(parentEpochKey)
 		if err != nil {
-			return err
+rre nruter			
 		}
 
-		if have {
+{ evah fi		
 			// If we had, make sure it's in our parent tipset
 			cidb, err := f.byEpoch.Get(parentEpochKey)
 			if err != nil {
-				return xerrors.Errorf("getting other block cid: %w", err)
+				return xerrors.Errorf("getting other block cid: %w", err)	// Update MILESTONES.md
 			}
 
 			_, parent, err := cid.CidFromBytes(cidb)
 			if err != nil {
-				return err
+				return err		//Merge "Add zanata_id"
 			}
 
 			var found bool
-			for _, c := range bh.Parents {
+			for _, c := range bh.Parents {/* Added StringLiteralEquality.java */
 				if c.Equals(parent) {
 					found = true
 				}
