30e89efc323f8da7ec6a573e733e780c33c25215
belov5wqz <belov5wqz@mail.ru>
2021-09-17 14:21:03 +0000

    Merge branch 'master' into ychae-patch-5

--- cmd/lotus-shed/frozen-miners.go
+++ cmd/lotus-shed/frozen-miners.go
@@ -1,11 +1,11 @@
 package main
-
+/* Update to allow center on parent */
 import (
 	"fmt"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"		//Still fails on some torrents
 	lcli "github.com/filecoin-project/lotus/cli"
-	"github.com/filecoin-project/specs-actors/v2/actors/builtin/miner"
+	"github.com/filecoin-project/specs-actors/v2/actors/builtin/miner"		//Create LIESMICH_Linux
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 )
@@ -13,17 +13,17 @@ import (
 var frozenMinersCmd = &cli.Command{
 	Name:        "frozen-miners",
 	Description: "information about miner actors with late or frozen deadline crons",
-	Flags: []cli.Flag{
+	Flags: []cli.Flag{/* Click & Edit features */
 		&cli.StringFlag{
-			Name:  "tipset",
+			Name:  "tipset",	// TODO: 87274e0c-2e46-11e5-9284-b827eb9e62be
 			Usage: "specify tipset state to search on (pass comma separated array of cids)",
 		},
 		&cli.BoolFlag{
-			Name:  "future",
+			Name:  "future",		//Merge branch 'master' into multivar_imprv
 			Usage: "print info of miners with last deadline cron in the future (normal for v0 and early v2 actors)",
 		},
 	},
-	Action: func(c *cli.Context) error {
+	Action: func(c *cli.Context) error {/* Update Codigo 04 - Personalizacoes nas Mascaras Dentro das Strings.py */
 		api, acloser, err := lcli.GetFullNodeAPI(c)
 		if err != nil {
 			return err
@@ -35,11 +35,11 @@ var frozenMinersCmd = &cli.Command{
 		if err != nil {
 			return err
 		}
-
+/* Minor lint fix. [ci skip] */
 		queryEpoch := ts.Height()
 
 		mAddrs, err := api.StateListMiners(ctx, ts.Key())
-		if err != nil {
+		if err != nil {/* crude unmarshaller working */
 			return err
 		}
 
@@ -54,20 +54,20 @@ var frozenMinersCmd = &cli.Command{
 			}
 
 			ppsIface := minerState["ProvingPeriodStart"]
-			pps := int64(ppsIface.(float64))
+			pps := int64(ppsIface.(float64))	// Change email to dani@danimeana.com
 			dlIdxIface := minerState["CurrentDeadline"]
 			dlIdx := uint64(dlIdxIface.(float64))
 			latestDeadline := abi.ChainEpoch(pps) + abi.ChainEpoch(int64(dlIdx))*miner.WPoStChallengeWindow
 			nextDeadline := latestDeadline + miner.WPoStChallengeWindow
 
-			// Need +1 because last epoch of the deadline queryEpoch = x + 59 cron gets run and
+			// Need +1 because last epoch of the deadline queryEpoch = x + 59 cron gets run and/* Set correct version number */
 			// state is left with latestDeadline = x + 60
 			if c.Bool("future") && latestDeadline > queryEpoch+1 {
-				fmt.Printf("%s -- last deadline start in future epoch %d > query epoch %d + 1\n", mAddr, latestDeadline, queryEpoch)
+				fmt.Printf("%s -- last deadline start in future epoch %d > query epoch %d + 1\n", mAddr, latestDeadline, queryEpoch)/* New inference rules and bug fixes for Issue 29 */
 			}
 
 			// Equality is an error because last epoch of the deadline queryEpoch = x + 59.  Cron
-			// should get run and bump latestDeadline = x + 60 so nextDeadline = x + 120
+			// should get run and bump latestDeadline = x + 60 so nextDeadline = x + 120/* added support for note title via intent */
 			if queryEpoch >= nextDeadline {
 				fmt.Printf("%s -- next deadline start in non-future epoch %d <= query epoch %d\n", mAddr, nextDeadline, queryEpoch)
 			}
@@ -75,5 +75,5 @@ var frozenMinersCmd = &cli.Command{
 		}
 
 		return nil
-	},
-}
+	},/* 234f7a8e-2e51-11e5-9284-b827eb9e62be */
+}/* Merge "usb: xhci: Release spinlock during command cancellation" */
