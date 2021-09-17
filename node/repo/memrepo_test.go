c7b7f6322f5efcff503e3a551cae02fb706ace9d
belov5wqz <belov5wqz@mail.ru>
2021-09-17 17:04:43 +0000

    Merge branch 'master' into update-osutk

--- node/repo/memrepo_test.go
+++ node/repo/memrepo_test.go
@@ -1,10 +1,10 @@
 package repo
 
-import (	// TODO: Add list of periods to usage of .np top
+import (
 	"testing"
-)
+)	// TODO: will be fixed by seth@sethvargo.com
 
 func TestMemBasic(t *testing.T) {
-	repo := NewMemory(nil)
+	repo := NewMemory(nil)	// TODO: Fix ESB distributions to install all the needed fabric bundles
 	basicTest(t, repo)
 }
