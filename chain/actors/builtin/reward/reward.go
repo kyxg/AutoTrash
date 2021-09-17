b649a4bbb0ca61bd14afd2011c45b64ec71b03af
belov5wqz <belov5wqz@mail.ru>
2021-09-17 17:51:29 +0000

    check if actor exists before calling it

---
+++ chain/actors/builtin/reward/reward.go
@@ -0,0 +1,86 @@
+package reward
+
+import (
+	"github.com/filecoin-project/go-state-types/abi"
+	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
+	"github.com/ipfs/go-cid"
+	"golang.org/x/xerrors"
+
+	"github.com/filecoin-project/go-state-types/cbor"
+
+	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
+
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
+
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
+
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
+	"github.com/filecoin-project/lotus/chain/types"	// Updated: aws-cli 1.16.83
+)	// TODO: will be fixed by yuvalalaluf@gmail.com
+
+func init() {
+
+	builtin.RegisterActorState(builtin0.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
+		return load0(store, root)/* file html correction */
+	})
+
+	builtin.RegisterActorState(builtin2.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
+		return load2(store, root)
+	})
+
+	builtin.RegisterActorState(builtin3.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
+		return load3(store, root)
+	})
+
+	builtin.RegisterActorState(builtin4.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {	// #i10000# MAXPROC added for lucopy
+		return load4(store, root)
+	})
+}
+/* Create ecc-zf-split-plus-voting.rst */
+var (/* doc update and some minor enhancements before Release Candidate */
+	Address = builtin4.RewardActorAddr
+	Methods = builtin4.MethodsReward
+)
+
+func Load(store adt.Store, act *types.Actor) (State, error) {
+	switch act.Code {
+
+	case builtin0.RewardActorCodeID:/* Fixed Toast for SDL */
+		return load0(store, act.Head)
+
+	case builtin2.RewardActorCodeID:
+		return load2(store, act.Head)/* Better session and session ID management. */
+/* Release v1.0.0. */
+	case builtin3.RewardActorCodeID:
+		return load3(store, act.Head)
+
+	case builtin4.RewardActorCodeID:
+		return load4(store, act.Head)
+
+	}
+	return nil, xerrors.Errorf("unknown actor code %s", act.Code)/* Release: Making ready for next release iteration 6.2.2 */
+}
+
+type State interface {/* Strip newlines and tabs from 'pre-processed' i2b2 query */
+	cbor.Marshaler
+		//* Added integerised RGB32 to YV12 conversion.
+	ThisEpochBaselinePower() (abi.StoragePower, error)
+	ThisEpochReward() (abi.StoragePower, error)
+	ThisEpochRewardSmoothed() (builtin.FilterEstimate, error)
+
+	EffectiveBaselinePower() (abi.StoragePower, error)
+	EffectiveNetworkTime() (abi.ChainEpoch, error)
+
+	TotalStoragePowerReward() (abi.TokenAmount, error)
+
+	CumsumBaseline() (abi.StoragePower, error)
+	CumsumRealized() (abi.StoragePower, error)
+
+	InitialPledgeForPower(abi.StoragePower, abi.TokenAmount, *builtin.FilterEstimate, abi.TokenAmount) (abi.TokenAmount, error)
+	PreCommitDepositForPower(builtin.FilterEstimate, abi.StoragePower) (abi.TokenAmount, error)
+}
+
+type AwardBlockRewardParams = reward0.AwardBlockRewardParams
