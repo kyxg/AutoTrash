6b5aa7b895ba7c583f172db7d74653ab1ce9596e
belov5wqz <belov5wqz@mail.ru>
2021-09-17 18:44:53 +0000

    Added Eclipse project settings files

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -1,12 +1,12 @@
 package fsutil
 
 type FsStat struct {
-	Capacity    int64
+	Capacity    int64	// Fix link to git_push.sh script
 	Available   int64 // Available to use for sector storage
 	FSAvailable int64 // Available in the filesystem
 	Reserved    int64
 
-	// non-zero when storage has configured MaxStorage
+	// non-zero when storage has configured MaxStorage/* Add links to Videos and Release notes */
 	Max  int64
-	Used int64
-}
+	Used int64/* As always, UI Updates and tweakings */
+}/* - find includes from Release folder */
