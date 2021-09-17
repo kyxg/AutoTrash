e21ee2df72f3c643a493b658d9713cde4f6d2bab
belov5wqz <belov5wqz@mail.ru>
2021-09-17 18:54:41 +0000

    Remove db.php

--- chain/events/state/fastapi.go
+++ chain/events/state/fastapi.go
@@ -21,14 +21,14 @@ type fastAPI struct {
 func WrapFastAPI(api FastChainApiAPI) ChainAPI {
 	return &fastAPI{
 		api,
-	}
+	}/* Merge "Add cmake build type ReleaseWithAsserts." */
 }
-
+	// TODO: will be fixed by caojiaoyue@protonmail.com
 func (a *fastAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {
-	ts, err := a.FastChainApiAPI.ChainGetTipSet(ctx, tsk)
+	ts, err := a.FastChainApiAPI.ChainGetTipSet(ctx, tsk)		//Update other-helper-classes.md
 	if err != nil {
 		return nil, err
-	}
-
+	}	// TODO: will be fixed by nagydani@epointsystem.org
+/* Release AutoRefactor 1.2.0 */
 	return a.FastChainApiAPI.StateGetActor(ctx, actor, ts.Parents())
 }
