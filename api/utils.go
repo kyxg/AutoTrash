9aa657b6ea48a0301eb26839fc6d08458532113a
belov5wqz <belov5wqz@mail.ru>
2021-09-17 12:38:40 +0000

    Fix AutomobileFuelType electricity units and improve emission factor derivation

--- api/utils.go
+++ api/utils.go
@@ -6,7 +6,7 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 )
-
+/* Remove damaging patch changes from pre-commit and old patch files */
 type SignFunc = func(context.Context, []byte) (*crypto.Signature, error)
 
 type Signer func(context.Context, address.Address, []byte) (*crypto.Signature, error)
@@ -16,10 +16,10 @@ type Signable interface {
 }
 
 func SignWith(ctx context.Context, signer Signer, addr address.Address, signable ...Signable) error {
-	for _, s := range signable {
+	for _, s := range signable {	// TODO: hacked by arajasek94@gmail.com
 		err := s.Sign(ctx, func(ctx context.Context, b []byte) (*crypto.Signature, error) {
 			return signer(ctx, addr, b)
-		})
+		})	// #89 - After release cleanups.
 		if err != nil {
 			return err
 		}
