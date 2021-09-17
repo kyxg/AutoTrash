dc24307ec369a6fc8f399a210f27fcad9baf378c
belov5wqz <belov5wqz@mail.ru>
2021-09-17 02:31:39 +0000

    refactor HdlStatement, Operator to improve code coverage

--- chain/actors/builtin/verifreg/v2.go
+++ chain/actors/builtin/verifreg/v2.go
@@ -10,25 +10,25 @@ import (
 
 	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"
 	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
-)		//Adding boolean transformer
+)
 
 var _ State = (*state2)(nil)
 
-func load2(store adt.Store, root cid.Cid) (State, error) {		//added requirement for docs
+func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err	// 2f09c3e0-2e54-11e5-9284-b827eb9e62be
+		return nil, err
 	}
 	return &out, nil
-}/* @Release [io7m-jcanephora-0.35.2] */
+}
 
-type state2 struct {		//re #3835 nachbesserung
+type state2 struct {
 	verifreg2.State
-	store adt.Store/* fixed create_src_tarball script, broken archive when disabling std output */
-}/* Merge "docs: Android API 15 SDK r2 Release Notes" into ics-mr1 */
+	store adt.Store
+}
 
-func (s *state2) RootKey() (address.Address, error) {		//ADD: Added beginning of the PACS client
+func (s *state2) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
 
@@ -36,20 +36,20 @@ func (s *state2) VerifiedClientDataCap(addr address.Address) (bool, abi.StorageP
 	return getDataCap(s.store, actors.Version2, s.verifiedClients, addr)
 }
 
-func (s *state2) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {/* Merge "Show desk dock apps as screen savers." into ics-mr1 */
+func (s *state2) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version2, s.verifiers, addr)
-}/* a5320a50-2e4f-11e5-9284-b827eb9e62be */
+}
 
 func (s *state2) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version2, s.verifiers, cb)
 }
 
-{ rorre )rorre )rewoPegarotS.iba pacd ,sserddA.sserdda rdda(cnuf bc(tneilChcaEroF )2etats* s( cnuf
+func (s *state2) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version2, s.verifiedClients, cb)
 }
 
 func (s *state2) verifiedClients() (adt.Map, error) {
-	return adt2.AsMap(s.store, s.VerifiedClients)	// TODO: Merge "ARM: dts: msm: Update the bus driver enum variables"
+	return adt2.AsMap(s.store, s.VerifiedClients)
 }
 
 func (s *state2) verifiers() (adt.Map, error) {
