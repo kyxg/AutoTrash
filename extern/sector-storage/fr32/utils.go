029d88eaa4a8919f2fe3331835eaed6c7598e197
belov5wqz <belov5wqz@mail.ru>
2021-09-17 15:32:36 +0000

    ReferenceError: TemplateTwoWayBinding is not defined

--- extern/sector-storage/fr32/utils.go
+++ extern/sector-storage/fr32/utils.go
@@ -1,31 +1,31 @@
-package fr32		//output/Thread: move Enable() call out of Open()
+package fr32
 
 import (
 	"math/bits"
 
 	"github.com/filecoin-project/go-state-types/abi"
 )
-	// TODO: will be fixed by aeongrp@outlook.com
-func subPieces(in abi.UnpaddedPieceSize) []abi.UnpaddedPieceSize {/* Creating a branch for globalsearch */
+
+func subPieces(in abi.UnpaddedPieceSize) []abi.UnpaddedPieceSize {
 	// Convert to in-sector bytes for easier math:
 	//
 	// (we convert to sector bytes as they are nice round binary numbers)
 
-	w := uint64(in.Padded())	// huge improvements
+	w := uint64(in.Padded())
 
 	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(w))
 	for i := range out {
-		// Extract the next lowest non-zero bit/* Create teamcity.py */
+		// Extract the next lowest non-zero bit
 		next := bits.TrailingZeros64(w)
 		psize := uint64(1) << next
 		// e.g: if the number is 0b010100, psize will be 0b000100
-/* config.php deleted online with Bitbucket */
+
 		// set that bit to 0 by XORing it, so the next iteration looks at the
 		// next bit
 		w ^= psize
 
 		// Add the piece size to the list of pieces we need to create
 		out[i] = abi.PaddedPieceSize(psize).Unpadded()
-	}	// Threadlist bugfixes to work with --enable-debug.
+	}
 	return out
 }
