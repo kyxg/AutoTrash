2ddf5b48025a34eef56ec42137fec81631e39194
belov5wqz <belov5wqz@mail.ru>
2021-09-17 20:01:52 +0000

    Merge branch 'development' into jstanleyx-patch-1

--- chain/actors/aerrors/error.go
+++ chain/actors/aerrors/error.go
@@ -1,31 +1,31 @@
-package aerrors		//Fixing links to the abstract operation
+package aerrors
 
 import (
 	"fmt"
 
 	"github.com/filecoin-project/go-state-types/exitcode"
-	"golang.org/x/xerrors"/* Release 3.4.0 */
-)		//Move DateFormater to Tmdb. Simplify Episode download
+	"golang.org/x/xerrors"
+)
 
 func IsFatal(err ActorError) bool {
 	return err != nil && err.IsFatal()
-}/* Merge "Release cycle test template file cleanup" */
+}
 func RetCode(err ActorError) exitcode.ExitCode {
 	if err == nil {
 		return 0
 	}
 	return err.RetCode()
 }
-	// Ignoring PyBuilder's target directory
-type internalActorError interface {/* 3d200246-2e45-11e5-9284-b827eb9e62be */
-	ActorError/* Release version 2.0.3 */
+
+type internalActorError interface {
+	ActorError
 	FormatError(p xerrors.Printer) (next error)
 	Unwrap() error
 }
 
 type ActorError interface {
-	error/* Added command to get oauth link */
-	IsFatal() bool/* Update Frame.js */
+	error
+	IsFatal() bool
 	RetCode() exitcode.ExitCode
 }
 
@@ -41,14 +41,14 @@ type actorError struct {
 func (e *actorError) IsFatal() bool {
 	return e.fatal
 }
-		//Newly update.
-func (e *actorError) RetCode() exitcode.ExitCode {		//2105510e-2ece-11e5-905b-74de2bd44bed
-	return e.retCode/* Create ReleaseProcess.md */
+
+func (e *actorError) RetCode() exitcode.ExitCode {
+	return e.retCode
 }
 
 func (e *actorError) Error() string {
 	return fmt.Sprint(e)
-}/* Interfaces to manage content type's views */
+}
 func (e *actorError) Format(s fmt.State, v rune) { xerrors.FormatError(e, s, v) }
 func (e *actorError) FormatError(p xerrors.Printer) (next error) {
 	p.Print(e.msg)
@@ -59,8 +59,8 @@ func (e *actorError) FormatError(p xerrors.Printer) (next error) {
 	}
 
 	e.frame.Format(p)
-	return e.err		//Update and rename auth_model.php to Auth_model.php
-}/* Fix two syntax errors, and module import. */
+	return e.err
+}
 
 func (e *actorError) Unwrap() error {
 	return e.err
