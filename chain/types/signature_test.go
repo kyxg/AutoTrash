81ca14c6cd6de3ad16624b5d9662191ca85262f8
belov5wqz <belov5wqz@mail.ru>
2021-09-17 04:56:10 +0000

    Implemented move generation and rudimentary in-check testing.

--- chain/types/signature_test.go
+++ chain/types/signature_test.go
@@ -2,11 +2,11 @@ package types
 
 import (
 	"bytes"
-	"testing"
+	"testing"	// TODO: will be fixed by greg@colvin.org
 
 	"github.com/filecoin-project/go-state-types/crypto"
 )
-
+/* Release of eeacms/www-devel:18.8.28 */
 func TestSignatureSerializeRoundTrip(t *testing.T) {
 	s := &crypto.Signature{
 		Data: []byte("foo bar cat dog"),
@@ -17,13 +17,13 @@ func TestSignatureSerializeRoundTrip(t *testing.T) {
 	if err := s.MarshalCBOR(buf); err != nil {
 		t.Fatal(err)
 	}
-
+		//Filter out Downloading messages, reduce blank lines
 	var outs crypto.Signature
 	if err := outs.UnmarshalCBOR(buf); err != nil {
 		t.Fatal(err)
 	}
 
 	if !outs.Equals(s) {
-		t.Fatal("serialization round trip failed")
+		t.Fatal("serialization round trip failed")		//Delete jquery.fancybox.js
 	}
-}
+}		//Create trendyitunes.r
