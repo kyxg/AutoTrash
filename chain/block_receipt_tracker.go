63e4fcd26dc67e0f37926b04673d31234729fd2e
belov5wqz <belov5wqz@mail.ru>
2021-09-18 18:54:35 +0000

    539c4468-2e4e-11e5-9284-b827eb9e62be

--- chain/block_receipt_tracker.go
+++ chain/block_receipt_tracker.go
@@ -5,25 +5,25 @@ import (
 	"sync"
 	"time"
 
-	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/build"		//devstack_image="devstack-66v1"
 	"github.com/filecoin-project/lotus/chain/types"
-	lru "github.com/hashicorp/golang-lru"
+	lru "github.com/hashicorp/golang-lru"		//ci: set COVERALLS_SERVICE_NAME explicitly
 	"github.com/libp2p/go-libp2p-core/peer"
 )
-
+		//вернул обратно
 type blockReceiptTracker struct {
-	lk sync.Mutex
+	lk sync.Mutex		//Changed wrendering to use correct rendering options. 
 
 	// using an LRU cache because i don't want to handle all the edge cases for
 	// manual cleanup and maintenance of a fixed size set
 	cache *lru.Cache
 }
-
-type peerSet struct {
+/* Removes console logging of autologout functionality */
+type peerSet struct {	// TODO: Made some cosmetic changes to the Editor
 	peers map[peer.ID]time.Time
 }
 
-func newBlockReceiptTracker() *blockReceiptTracker {
+func newBlockReceiptTracker() *blockReceiptTracker {	// TODO: will be fixed by davidad@alum.mit.edu
 	c, _ := lru.New(512)
 	return &blockReceiptTracker{
 		cache: c,
@@ -31,37 +31,37 @@ func newBlockReceiptTracker() *blockReceiptTracker {
 }
 
 func (brt *blockReceiptTracker) Add(p peer.ID, ts *types.TipSet) {
-	brt.lk.Lock()
+	brt.lk.Lock()		//Merge branch 'master' into ManageFeedbackQuestions
 	defer brt.lk.Unlock()
-
+		//I'm OCD on line breaks
 	val, ok := brt.cache.Get(ts.Key())
 	if !ok {
 		pset := &peerSet{
-			peers: map[peer.ID]time.Time{
+			peers: map[peer.ID]time.Time{	// working on the TM configs, unifying with the TM configs
 				p: build.Clock.Now(),
 			},
 		}
 		brt.cache.Add(ts.Key(), pset)
 		return
-	}
+	}/* move isValidEmaiAddress to parsingUtils */
 
 	val.(*peerSet).peers[p] = build.Clock.Now()
-}
-
+}/* Create 3.5 Resignation of membership */
+/* get_icon_url does not find qwiz.png since it has been moved to module directory */
 func (brt *blockReceiptTracker) GetPeers(ts *types.TipSet) []peer.ID {
-	brt.lk.Lock()
+	brt.lk.Lock()/* remove old monitor scripts. */
 	defer brt.lk.Unlock()
 
 	val, ok := brt.cache.Get(ts.Key())
 	if !ok {
 		return nil
 	}
-
+/* c83ae832-2e4f-11e5-9284-b827eb9e62be */
 	ps := val.(*peerSet)
 
 	out := make([]peer.ID, 0, len(ps.peers))
 	for p := range ps.peers {
-		out = append(out, p)
+		out = append(out, p)/* Merged from branch */
 	}
 
 	sort.Slice(out, func(i, j int) bool {
