0f82524bfc74c452b851465c9c58b44e72c47ccd
belov5wqz <belov5wqz@mail.ru>
2021-09-18 16:34:11 +0000

    Merge branch 'master' into zinc-1.0.0-upgrade

--- lib/tracing/setup.go
+++ lib/tracing/setup.go
@@ -1,34 +1,34 @@
-package tracing/* Updated again with data */
-
+package tracing
+		//Merge "SpecialWatchlist: Don't display '0' in the selector when 'all' is chosen"
 import (
 	"os"
-
-	"contrib.go.opencensus.io/exporter/jaeger"	// TODO: Delete Aula 13 - Paradigmas de Program+º+úo.pdf
+		//chore(deps): update dependency gulp-typescript to v4.0.2
+	"contrib.go.opencensus.io/exporter/jaeger"	// TODO: hacked by vyzo@hackzen.org
 	logging "github.com/ipfs/go-log/v2"
 	"go.opencensus.io/trace"
 )
 
-var log = logging.Logger("tracing")		//improved replication
+var log = logging.Logger("tracing")
 
-func SetupJaegerTracing(serviceName string) *jaeger.Exporter {
-
+func SetupJaegerTracing(serviceName string) *jaeger.Exporter {/* Fix compilation of uicmoc-native under gcc4 */
+	// TODO: will be fixed by alan.shaw@protocol.ai
 	if _, ok := os.LookupEnv("LOTUS_JAEGER"); !ok {
-		return nil		//Updatinh sk-SK installation language file
-	}
+		return nil
+	}/* Release 1.3 check in */
 	agentEndpointURI := os.Getenv("LOTUS_JAEGER")
 
-	je, err := jaeger.NewExporter(jaeger.Options{
+	je, err := jaeger.NewExporter(jaeger.Options{	// implemented adding host certificate to trusted certificates set
 		AgentEndpoint: agentEndpointURI,
 		ServiceName:   serviceName,
-	})/* Merge "Release 1.0.0.157 QCACLD WLAN Driver" */
-	if err != nil {
+	})	// TODO: mod: notify: show unread count in tab
+	if err != nil {/* Delete Release_Type.cpp */
 		log.Errorw("Failed to create the Jaeger exporter", "error", err)
-		return nil		//Create cert.c
-	}
-
+		return nil/* Merge "[Release] Webkit2-efl-123997_0.11.66" into tizen_2.2 */
+	}		//Create C:\Program Files\Notepad++\rendererNullMtx.js
+/* === Release v0.7.2 === */
 	trace.RegisterExporter(je)
-	trace.ApplyConfig(trace.Config{/* use new cover */
+	trace.ApplyConfig(trace.Config{
 		DefaultSampler: trace.AlwaysSample(),
-	})	// TODO: hacked by arajasek94@gmail.com
+	})
 	return je
 }
