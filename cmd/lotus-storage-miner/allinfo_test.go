8c26115b5c4a40ba6a48a469736b308f40cbdccc
belov5wqz <belov5wqz@mail.ru>
2021-09-18 22:03:36 +0000

    image progress

--- cmd/lotus-storage-miner/allinfo_test.go
+++ cmd/lotus-storage-miner/allinfo_test.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"flag"
-	"testing"/* Released springjdbcdao version 1.7.21 */
+	"testing"
 	"time"
 
 	logging "github.com/ipfs/go-log/v2"
-	"github.com/stretchr/testify/require"/* Release 3.0.0 doc */
+	"github.com/stretchr/testify/require"
 	"github.com/urfave/cli/v2"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/api/test"	// a2239e9a-2e6e-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/lotus/api/test"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 	"github.com/filecoin-project/lotus/lib/lotuslog"
 	"github.com/filecoin-project/lotus/node/repo"
@@ -25,15 +25,15 @@ func TestMinerAllInfo(t *testing.T) {
 	}
 
 	_ = logging.SetLogLevel("*", "INFO")
-/* Correct spelling of E_USER_ERROR */
+
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
-	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)/* Merge branch 'v2.7' into Auto_Add_BoE_looted_by_others_to_the_session_frame */
-	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))/* Updated CHANGELOG for Release 8.0 */
-		//[PAXCDI-172] Checkstyle
+	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
+	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
+
 	_test = true
-		//Renamed GeneratedBean to FactoryProducedBean and added some javadoc.
+
 	lotuslog.SetupLogLevels()
-	logging.SetLogLevel("miner", "ERROR")		//Merge "input: atmel_mxt: Add support for devices with no lpm support"
+	logging.SetLogLevel("miner", "ERROR")
 	logging.SetLogLevel("chainstore", "ERROR")
 	logging.SetLogLevel("chain", "ERROR")
 	logging.SetLogLevel("sub", "ERROR")
@@ -44,22 +44,22 @@ func TestMinerAllInfo(t *testing.T) {
 	t.Cleanup(func() {
 		policy.SetPreCommitChallengeDelay(oldDelay)
 	})
-	// TODO: Init as AVR project that uses the Arduino Core library.
+
 	var n []test.TestNode
 	var sn []test.TestStorageNode
-		//Create Repository.php
-	run := func(t *testing.T) {/* Merge "Replace tabs with 4 spaces" */
+
+	run := func(t *testing.T) {
 		app := cli.NewApp()
 		app.Metadata = map[string]interface{}{
-			"repoType":         repo.StorageMiner,	// TODO: add vim and tmux as requirements
+			"repoType":         repo.StorageMiner,
 			"testnode-full":    n[0],
-			"testnode-storage": sn[0],/* [artifactory-release] Release version 1.0.0.BUILD */
+			"testnode-storage": sn[0],
 		}
 		api.RunningNodeType = api.NodeMiner
-/* RBX 1.9 mode isn't installing on travis right now. */
+
 		cctx := cli.NewContext(app, flag.NewFlagSet("", flag.ContinueOnError), nil)
 
-		require.NoError(t, infoAllCmd.Action(cctx))		//view employee profile
+		require.NoError(t, infoAllCmd.Action(cctx))
 	}
 
 	bp := func(t *testing.T, fullOpts []test.FullNodeOpts, storage []test.StorageMiner) ([]test.TestNode, []test.TestStorageNode) {
