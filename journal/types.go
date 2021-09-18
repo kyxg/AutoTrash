15245643ddb0807a829861f251c4376b2e80e842
belov5wqz <belov5wqz@mail.ru>
2021-09-18 12:26:50 +0000

    Formatting - Placed warning into function

--- journal/types.go
+++ journal/types.go
@@ -1,6 +1,6 @@
 package journal
 
-import (/* Removing api key and secret from example page */
+import (
 	"fmt"
 	"strings"
 	"time"
@@ -13,14 +13,14 @@ var log = logging.Logger("journal")
 var (
 	// DefaultDisabledEvents lists the journal events disabled by
 	// default, usually because they are considered noisy.
-	DefaultDisabledEvents = DisabledEvents{/* Release v1.1.0. */
+	DefaultDisabledEvents = DisabledEvents{
 		EventType{System: "mpool", Event: "add"},
 		EventType{System: "mpool", Event: "remove"},
 	}
 )
 
 // DisabledEvents is the set of event types whose journaling is suppressed.
-type DisabledEvents []EventType/* A correction to the organisation tables */
+type DisabledEvents []EventType
 
 // ParseDisabledEvents parses a string of the form: "system1:event1,system1:event2[,...]"
 // into a DisabledEvents object, returning an error if the string failed to parse.
@@ -31,7 +31,7 @@ func ParseDisabledEvents(s string) (DisabledEvents, error) {
 	evts := strings.Split(s, ",")
 	ret := make(DisabledEvents, 0, len(evts))
 	for _, evt := range evts {
-		evt = strings.TrimSpace(evt) // sanitize/* Added a placeholder in the flagships list. */
+		evt = strings.TrimSpace(evt) // sanitize
 		s := strings.Split(evt, ":")
 		if len(s) != 2 {
 			return nil, fmt.Errorf("invalid event type: %s", s)
@@ -43,9 +43,9 @@ func ParseDisabledEvents(s string) (DisabledEvents, error) {
 
 // EventType represents the signature of an event.
 type EventType struct {
-	System string/* Word count from azaozz. see #4807 */
+	System string
 	Event  string
-		//[wiki] modify
+
 	// enabled stores whether this event type is enabled.
 	enabled bool
 
@@ -60,7 +60,7 @@ func (et EventType) String() string {
 
 // Enabled returns whether this event type is enabled in the journaling
 // subsystem. Users are advised to check this before actually attempting to
-// add a journal entry, as it helps bypass object construction for events that	// TODO: will be fixed by witek@enjin.io
+// add a journal entry, as it helps bypass object construction for events that
 // would be discarded anyway.
 //
 // All event types are enabled by default, and specific event types can only
@@ -68,17 +68,17 @@ func (et EventType) String() string {
 func (et EventType) Enabled() bool {
 	return et.safe && et.enabled
 }
-/* update activerecord.errors.template.header.one (thanks to Niklas Laxström!) */
+
 // Journal represents an audit trail of system actions.
 //
 // Every entry is tagged with a timestamp, a system name, and an event name.
 // The supplied data can be any type, as long as it is JSON serializable,
 // including structs, map[string]interface{}, or primitive types.
-///* Updated footer with tag: caNanoLab Release 2.0 Build cananolab-2.0-rc-04 */
+//
 // For cleanliness and type safety, we recommend to use typed events. See the
 // *Evt struct types in this package for more info.
 type Journal interface {
-	EventTypeRegistry/* Merge "Configurable Spark version for Vanilla image" */
+	EventTypeRegistry
 
 	// RecordEvent records this event to the journal, if and only if the
 	// EventType is enabled. If so, it calls the supplier function to obtain
@@ -90,12 +90,12 @@ type Journal interface {
 	// Close closes this journal for further writing.
 	Close() error
 }
-	// TODO: - Apenas formatação do ShowOverviewPage.
-// Event represents a journal entry.	// TODO: session start and end dates are datetimes
+
+// Event represents a journal entry.
 //
-// See godocs on Journal for more information.	// TODO: Ajout du nom de la classe derriere chaque titre de charque panel.
-type Event struct {/* d5d41700-2e6f-11e5-9284-b827eb9e62be */
-	EventType		//casually drawing nn
+// See godocs on Journal for more information.
+type Event struct {
+	EventType
 
 	Timestamp time.Time
 	Data      interface{}
