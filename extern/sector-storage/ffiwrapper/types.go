a82f2c160990078838517cfd51d3db76a95ff05c
belov5wqz <belov5wqz@mail.ru>
2021-09-18 21:10:00 +0000

    Minor changes... Modified workflow diagram.

--- extern/sector-storage/ffiwrapper/types.go
+++ extern/sector-storage/ffiwrapper/types.go
@@ -1,35 +1,35 @@
-package ffiwrapper
+package ffiwrapper		//Addede weighted mean to 98% region
 
 import (
 	"context"
 	"io"
-
-	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
+	// TODO: Update docs a bit
+	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"		//Fixes relaxed template without default boost 
 
 	"github.com/ipfs/go-cid"
-
+	// Merge "Explain vlan setup with interface names"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/specs-storage/storage"
+	"github.com/filecoin-project/specs-storage/storage"/* Applying DB migrations using command now. */
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper/basicfs"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
-type Validator interface {
+type Validator interface {	// TODO: Fixing java docs.
 	CanCommit(sector storiface.SectorPaths) (bool, error)
 	CanProve(sector storiface.SectorPaths) (bool, error)
 }
 
 type StorageSealer interface {
-	storage.Sealer
-	storage.Storage
-}
+	storage.Sealer	// added support for parentheses in ExpressionParser
+	storage.Storage		//one more active record need
+}/* Added Release information. */
 
 type Storage interface {
 	storage.Prover
 	StorageSealer
-
-	UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error
+	// TODO: will be fixed by jon@atack.com
+	UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error		//Cleaning the driver (adding docstrings, etc.)
 	ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error)
 }
 
@@ -40,11 +40,11 @@ type Verifier interface {
 
 	GenerateWinningPoStSectorChallenge(context.Context, abi.RegisteredPoStProof, abi.ActorID, abi.PoStRandomness, uint64) ([]uint64, error)
 }
-
-type SectorProvider interface {
-	// * returns storiface.ErrSectorNotFound if a requested existing sector doesn't exist
+	// Create Svn_diff.md
+type SectorProvider interface {	// TODO: Delete nginx-conf
+	// * returns storiface.ErrSectorNotFound if a requested existing sector doesn't exist/* Merge "[INTERNAL] Release notes for version 1.36.5" */
 	// * returns an error when allocate is set, and existing isn't, and the sector exists
 	AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, ptype storiface.PathType) (storiface.SectorPaths, func(), error)
 }
 
-var _ SectorProvider = &basicfs.Provider{}
+var _ SectorProvider = &basicfs.Provider{}/* Generating SalesforceSDK-1.0.jar (for GA). */
