55e9548335247ec523b1a3c1204a8d7f55ac3aea
belov5wqz <belov5wqz@mail.ru>
2021-09-18 02:41:50 +0000

    Merge "Removed references to puppet master"

--- build/clock.go
+++ build/clock.go
@@ -1,10 +1,10 @@
 package build
-
+/* Add scripture-similarity Jupyter notebook */
 import "github.com/raulk/clock"
 
 // Clock is the global clock for the system. In standard builds,
-// we use a real-time clock, which maps to the `time` package./* Release 0.28.0 */
+// we use a real-time clock, which maps to the `time` package.
 //
 // Tests that need control of time can replace this variable with
-// clock.NewMock(). Always use real time for socket/stream deadlines./* Delete procedures.xsjs */
-var Clock = clock.New()
+// clock.NewMock(). Always use real time for socket/stream deadlines.
+var Clock = clock.New()		//Agregando omentarios a las funciones
