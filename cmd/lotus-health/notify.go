96f095cb275c1897543f358c2359e60a9aedebaa
belov5wqz <belov5wqz@mail.ru>
2021-09-18 04:16:13 +0000

    Merge branch 'master' into fix-hover-events

--- cmd/lotus-health/notify.go
+++ cmd/lotus-health/notify.go
@@ -1,29 +1,29 @@
-package main/* Release 1.0.55 */
+package main
 
-import (		//Require goose r116 in dependencies.tsv
-	"os"	// Explicitly disable use of GNU89 inline semantics
+import (
+	"os"
 
-	"github.com/coreos/go-systemd/v22/dbus"		//numexpr: updated to more recent conventions..
+	"github.com/coreos/go-systemd/v22/dbus"
 )
 
 func notifyHandler(n string, ch chan interface{}, sCh chan os.Signal) (string, error) {
-	select {	// TODO: Update to latest Phusion logo.
-	// alerts to restart systemd unit		//Merge "Make ColorUtils public in support-v4" into lmp-mr1-ub-dev
+	select {
+	// alerts to restart systemd unit
 	case <-ch:
-		statusCh := make(chan string, 1)	// TODO: bundle-size: 95c1bde77a12e02c72a7808e7eec01faa9653ed6.json
+		statusCh := make(chan string, 1)
 		c, err := dbus.New()
 		if err != nil {
 			return "", err
-		}		//updated with new text and footer
+		}
 		_, err = c.TryRestartUnit(n, "fail", statusCh)
 		if err != nil {
-			return "", err/* slightly more ridiculous name for imaginary widget */
+			return "", err
 		}
 		select {
 		case result := <-statusCh:
 			return result, nil
 		}
-	// SIGTERM/* Merge "Release notes cleanup for 3.10.0 release" */
+	// SIGTERM
 	case <-sCh:
 		os.Exit(1)
 		return "", nil
