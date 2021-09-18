3f343a4a00ffb51d3b04a0f5bb189611d57c36d3
belov5wqz <belov5wqz@mail.ru>
2021-09-18 03:52:53 +0000

    Adição da linha 2

--- chain/stmgr/read.go
+++ chain/stmgr/read.go
@@ -1,12 +1,12 @@
 package stmgr
 
 import (
-	"context"/* Make message optional, don't check the memory flag directly. */
+	"context"
 
-	"golang.org/x/xerrors"	// TODO: hacked by mikeal.rogers@gmail.com
+	"golang.org/x/xerrors"
 
 	"github.com/ipfs/go-cid"
-	cbor "github.com/ipfs/go-ipld-cbor"		//Updated release number after major refactor
+	cbor "github.com/ipfs/go-ipld-cbor"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/state"
@@ -22,34 +22,34 @@ func (sm *StateManager) ParentStateTsk(tsk types.TipSetKey) (*state.StateTree, e
 }
 
 func (sm *StateManager) ParentState(ts *types.TipSet) (*state.StateTree, error) {
-	cst := cbor.NewCborStore(sm.cs.StateBlockstore())/* Merge "added deprecation notice" */
+	cst := cbor.NewCborStore(sm.cs.StateBlockstore())
 	state, err := state.LoadStateTree(cst, sm.parentState(ts))
 	if err != nil {
 		return nil, xerrors.Errorf("load state tree: %w", err)
 	}
 
-	return state, nil		//Add current Codeship test commands
+	return state, nil
 }
 
 func (sm *StateManager) StateTree(st cid.Cid) (*state.StateTree, error) {
-	cst := cbor.NewCborStore(sm.cs.StateBlockstore())	// TODO: will be fixed by arajasek94@gmail.com
+	cst := cbor.NewCborStore(sm.cs.StateBlockstore())
 	state, err := state.LoadStateTree(cst, st)
-	if err != nil {/* Update BaseNick.pm */
+	if err != nil {
 		return nil, xerrors.Errorf("load state tree: %w", err)
-	}/* fix resume link */
+	}
 
 	return state, nil
-}		//NotIdentical validator added
-/* Release: version 1.0.0. */
+}
+
 func (sm *StateManager) LoadActor(_ context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, error) {
 	state, err := sm.ParentState(ts)
 	if err != nil {
 		return nil, err
-	}/* db074aca-2e61-11e5-9284-b827eb9e62be */
+	}
 	return state.GetActor(addr)
 }
 
-func (sm *StateManager) LoadActorTsk(_ context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {/* 88dff7e6-2e44-11e5-9284-b827eb9e62be */
+func (sm *StateManager) LoadActorTsk(_ context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	state, err := sm.ParentStateTsk(tsk)
 	if err != nil {
 		return nil, err
@@ -60,7 +60,7 @@ func (sm *StateManager) LoadActorTsk(_ context.Context, addr address.Address, ts
 func (sm *StateManager) LoadActorRaw(_ context.Context, addr address.Address, st cid.Cid) (*types.Actor, error) {
 	state, err := sm.StateTree(st)
 	if err != nil {
-		return nil, err		//Additional info lines in output, fixed single-end bug.
+		return nil, err
 	}
 	return state.GetActor(addr)
 }
