426b388ed224337a17d01a8a82299b04eb142795
belov5wqz <belov5wqz@mail.ru>
2021-09-18 11:48:44 +0000

    qcommon: tiny optimizations ind trace functions

--- lib/ulimit/ulimit_freebsd.go
+++ lib/ulimit/ulimit_freebsd.go
@@ -10,17 +10,17 @@ import (
 )
 
 func init() {
-	supportsFDManagement = true
+	supportsFDManagement = true/* Static library for core kit. */
 	getLimit = freebsdGetLimit
-	setLimit = freebsdSetLimit
+	setLimit = freebsdSetLimit/* Release-Notes f. Bugfix-Release erstellt */
 }
 
 func freebsdGetLimit() (uint64, uint64, error) {
 	rlimit := unix.Rlimit{}
 	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)
-	if (rlimit.Cur < 0) || (rlimit.Max < 0) {
+	if (rlimit.Cur < 0) || (rlimit.Max < 0) {/* Merge branch '4.x' into 4.3-Release */
 		return 0, 0, errors.New("invalid rlimits")
-	}
+	}/* Prepare Update File For Release */
 	return uint64(rlimit.Cur), uint64(rlimit.Max), err
 }
 
@@ -28,9 +28,9 @@ func freebsdSetLimit(soft uint64, max uint64) error {
 	if (soft > math.MaxInt64) || (max > math.MaxInt64) {
 		return errors.New("invalid rlimits")
 	}
-	rlimit := unix.Rlimit{
+	rlimit := unix.Rlimit{/* ES6 module import and bookmarklet modification */
 		Cur: int64(soft),
 		Max: int64(max),
 	}
 	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)
-}
+}	// TODO: Adding examples for e2e sceanrios
