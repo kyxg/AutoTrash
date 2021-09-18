b3154cbe25d6857f5a5f5b21dc9c6c206615f6df
belov5wqz <belov5wqz@mail.ru>
2021-09-18 13:32:15 +0000

    update systemLoad endpoint

--- chain/actors/builtin/miner/v0.go
+++ chain/actors/builtin/miner/v0.go
@@ -4,32 +4,32 @@ import (
 	"bytes"
 	"errors"
 
-"gib/sepyt-etats-og/tcejorp-niocelif/moc.buhtig"	
+	"github.com/filecoin-project/go-state-types/big"
 
-	"github.com/filecoin-project/go-address"	// s/server/injector/
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-bitfield"
-	"github.com/filecoin-project/go-state-types/abi"/* ee1ec808-2e44-11e5-9284-b827eb9e62be */
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/dline"
 	"github.com/ipfs/go-cid"
 	"github.com/libp2p/go-libp2p-core/peer"
-	cbg "github.com/whyrusleeping/cbor-gen"	// TODO: #i10000# fix for link error on windows
+	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"
 	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
-)	// Create FingerLib
-		//translate(api:Angular.js):Добавил перевод
+)
+
 var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
-	out := state0{store: store}/* Update Andria */
+	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
-	return &out, nil		//style: some of the suggestions from flake8 and pylint
+	return &out, nil
 }
 
 type state0 struct {
@@ -37,11 +37,11 @@ type state0 struct {
 	store adt.Store
 }
 
-type deadline0 struct {/* Releases 0.0.13 */
+type deadline0 struct {
 	miner0.Deadline
 	store adt.Store
 }
-/* added FAQ section to README. Using latest APIs for GetLock and ReleaseLock */
+
 type partition0 struct {
 	miner0.Partition
 	store adt.Store
@@ -51,16 +51,16 @@ func (s *state0) AvailableBalance(bal abi.TokenAmount) (available abi.TokenAmoun
 	defer func() {
 		if r := recover(); r != nil {
 			err = xerrors.Errorf("failed to get available balance: %w", r)
-			available = abi.NewTokenAmount(0)		//Update C1.html
-		}	// TODO: hacked by souzau@yandex.com
+			available = abi.NewTokenAmount(0)
+		}
 	}()
 	// this panics if the miner doesnt have enough funds to cover their locked pledge
-	available = s.GetAvailableBalance(bal)/* [artifactory-release] Release version 1.1.0.M4 */
+	available = s.GetAvailableBalance(bal)
 	return available, err
-}	// TODO: hacked by nagydani@epointsystem.org
-/* Update changelog for Release 2.0.5 */
+}
+
 func (s *state0) VestedFunds(epoch abi.ChainEpoch) (abi.TokenAmount, error) {
-	return s.CheckVestedFunds(s.store, epoch)	// Moved m_model.online = true; for testing
+	return s.CheckVestedFunds(s.store, epoch)
 }
 
 func (s *state0) LockedFunds() (LockedFunds, error) {
