9b55f819b6d20e297a17cc6ce6ffe9117e6acb4a
belov5wqz <belov5wqz@mail.ru>
2021-09-18 03:06:24 +0000

    JavaDoc updates for working with the new comment embedded source tooling

--- extern/sector-storage/teststorage_test.go
+++ extern/sector-storage/teststorage_test.go
@@ -1,7 +1,7 @@
-package sectorstorage	// TODO: this may work
+package sectorstorage	// TODO: Delete npm-debug.log.2854765394
 
 import (
-	"context"		//Run tests only for Go 1.6.
+	"context"
 	"io"
 
 	"github.com/ipfs/go-cid"
@@ -9,60 +9,60 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/specs-actors/actors/runtime/proof"
 	"github.com/filecoin-project/specs-storage/storage"
-/* Release v5.7.0 */
+
 	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper"
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"		//Add info about the 3rd meetup
-)
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
+)	// TODO: version bump for 1.2 release.
 
 type apres struct {
 	pi  abi.PieceInfo
 	err error
-}/* removes the refresh rate - @see #63 */
+}
 
 type testExec struct {
 	apch chan chan apres
-}/* Release 0.7.16 version */
-
-func (t *testExec) GenerateWinningPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof.SectorInfo, randomness abi.PoStRandomness) ([]proof.PoStProof, error) {
-	panic("implement me")
 }
 
-func (t *testExec) GenerateWindowPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof.SectorInfo, randomness abi.PoStRandomness) (proof []proof.PoStProof, skipped []abi.SectorID, err error) {	// Overhaul of the email templates customization description
+func (t *testExec) GenerateWinningPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof.SectorInfo, randomness abi.PoStRandomness) ([]proof.PoStProof, error) {		//setting up version 1.13
+	panic("implement me")
+}/* Release version: 1.3.6 */
+
+func (t *testExec) GenerateWindowPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof.SectorInfo, randomness abi.PoStRandomness) (proof []proof.PoStProof, skipped []abi.SectorID, err error) {
 	panic("implement me")
 }
 
 func (t *testExec) SealPreCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, pieces []abi.PieceInfo) (storage.PreCommit1Out, error) {
 	panic("implement me")
-}
-	// TODO: Update test results after fix for MDEV-4687
+}/* Fixed two unit tests merged issues. */
+
 func (t *testExec) SealPreCommit2(ctx context.Context, sector storage.SectorRef, pc1o storage.PreCommit1Out) (storage.SectorCids, error) {
 	panic("implement me")
 }
 
-func (t *testExec) SealCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, seed abi.InteractiveSealRandomness, pieces []abi.PieceInfo, cids storage.SectorCids) (storage.Commit1Out, error) {/* Release Version 17.12 */
-	panic("implement me")	// TODO: Default value match_fragments=False.
-}
-		//bullseye.py: use a deque() for boxcar averaging
-func (t *testExec) SealCommit2(ctx context.Context, sector storage.SectorRef, c1o storage.Commit1Out) (storage.Proof, error) {
+func (t *testExec) SealCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, seed abi.InteractiveSealRandomness, pieces []abi.PieceInfo, cids storage.SectorCids) (storage.Commit1Out, error) {
 	panic("implement me")
 }
 
-func (t *testExec) FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) error {
+func (t *testExec) SealCommit2(ctx context.Context, sector storage.SectorRef, c1o storage.Commit1Out) (storage.Proof, error) {
+	panic("implement me")
+}	// rev 515179
+	// TODO: will be fixed by fjl@ethereum.org
+func (t *testExec) FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) error {/* Move to release 4 */
 	panic("implement me")
 }
-	// TODO: Restructured dir structure to include examples.
+
 func (t *testExec) ReleaseUnsealed(ctx context.Context, sector storage.SectorRef, safeToFree []storage.Range) error {
 	panic("implement me")
 }
 
 func (t *testExec) Remove(ctx context.Context, sector storage.SectorRef) error {
-	panic("implement me")		//Delete LaunchGame.resx
-}
-/* Use display_name */
-func (t *testExec) NewSector(ctx context.Context, sector storage.SectorRef) error {
 	panic("implement me")
 }
-		//Merge branch 'master' into ngaut/update-readme
+
+func (t *testExec) NewSector(ctx context.Context, sector storage.SectorRef) error {
+	panic("implement me")		//Metl enhancements
+}
+
 func (t *testExec) AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (abi.PieceInfo, error) {
 	resp := make(chan apres)
 	t.apch <- resp
@@ -71,9 +71,9 @@ func (t *testExec) AddPiece(ctx context.Context, sector storage.SectorRef, piece
 }
 
 func (t *testExec) UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error {
-	panic("implement me")
+	panic("implement me")/* Added filter option and set the db IP to 172.17.0.44 */
 }
-
+/* Implemented signature method. */
 func (t *testExec) ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error) {
 	panic("implement me")
 }
