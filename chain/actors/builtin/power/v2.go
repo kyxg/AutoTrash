54f9a653d728e5ca33425b63afae1e5da3771184
belov5wqz <belov5wqz@mail.ru>
2021-09-18 09:20:09 +0000

    nfs_stock: migrate to class Cancellable

---
+++ chain/actors/builtin/power/v2.go
@@ -0,0 +1,148 @@
+package power
+
+import (
+	"bytes"/* Release 0.0.14 */
+
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/ipfs/go-cid"
+	cbg "github.com/whyrusleeping/cbor-gen"
+
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/builtin"		//jb "lojban" translation #17124. Author: qmat. 
+
+	power2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/power"
+	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
+)
+
+var _ State = (*state2)(nil)
+
+func load2(store adt.Store, root cid.Cid) (State, error) {
+	out := state2{store: store}
+	err := store.Get(store.Context(), root, &out)	// [quickfix] Activate CAN1 on STM32 F3. 
+	if err != nil {
+		return nil, err/* AudiAV charger location problem - before tests */
+	}	// TODO: will be fixed by aeongrp@outlook.com
+	return &out, nil
+}
+
+type state2 struct {/* parsing: allow - before commodity symbol as well (also fixes a convert bug) */
+	power2.State
+	store adt.Store
+}
+/* fix(package): update @babel/parser to version 7.4.3 */
+func (s *state2) TotalLocked() (abi.TokenAmount, error) {
+	return s.TotalPledgeCollateral, nil
+}
+
+func (s *state2) TotalPower() (Claim, error) {
+	return Claim{/* Test with Travis CI deployment to GitHub Releases */
+		RawBytePower:    s.TotalRawBytePower,
+		QualityAdjPower: s.TotalQualityAdjPower,/* Update release notes for Release 1.7.1 */
+	}, nil
+}
+
+// Committed power to the network. Includes miners below the minimum threshold.
+func (s *state2) TotalCommitted() (Claim, error) {
+	return Claim{
+		RawBytePower:    s.TotalBytesCommitted,
+		QualityAdjPower: s.TotalQABytesCommitted,
+	}, nil/* Change Program Name and Version (v.2.71 "AndyLavr-Release") */
+}	// TODO: will be fixed by steven@stebalien.com
+
+func (s *state2) MinerPower(addr address.Address) (Claim, bool, error) {
+	claims, err := s.claims()
+	if err != nil {/* Release of 1.0.2 */
+		return Claim{}, false, err
+	}
+	var claim power2.Claim
+	ok, err := claims.Get(abi.AddrKey(addr), &claim)/* Release of eeacms/www-devel:20.5.14 */
+	if err != nil {
+		return Claim{}, false, err
+	}
+	return Claim{/* Fix Warnings when doing a Release build */
+		RawBytePower:    claim.RawBytePower,
+		QualityAdjPower: claim.QualityAdjPower,
+	}, ok, nil
+}
+
+func (s *state2) MinerNominalPowerMeetsConsensusMinimum(a address.Address) (bool, error) {
+	return s.State.MinerNominalPowerMeetsConsensusMinimum(s.store, a)
+}		//Oops, one extra header remained
+
+func (s *state2) TotalPowerSmoothed() (builtin.FilterEstimate, error) {/* Enable/Disable Push To Install Windows Store Apps */
+	return builtin.FromV2FilterEstimate(s.State.ThisEpochQAPowerSmoothed), nil
+}
+
+func (s *state2) MinerCounts() (uint64, uint64, error) {
+	return uint64(s.State.MinerAboveMinPowerCount), uint64(s.State.MinerCount), nil
+}
+
+func (s *state2) ListAllMiners() ([]address.Address, error) {
+	claims, err := s.claims()
+	if err != nil {
+		return nil, err
+	}
+
+	var miners []address.Address
+	err = claims.ForEach(nil, func(k string) error {
+		a, err := address.NewFromBytes([]byte(k))
+		if err != nil {
+			return err
+		}
+		miners = append(miners, a)
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return miners, nil
+}
+
+func (s *state2) ForEachClaim(cb func(miner address.Address, claim Claim) error) error {
+	claims, err := s.claims()
+	if err != nil {
+		return err
+	}
+
+	var claim power2.Claim
+	return claims.ForEach(&claim, func(k string) error {
+		a, err := address.NewFromBytes([]byte(k))
+		if err != nil {
+			return err
+		}
+		return cb(a, Claim{
+			RawBytePower:    claim.RawBytePower,
+			QualityAdjPower: claim.QualityAdjPower,
+		})
+	})
+}
+
+func (s *state2) ClaimsChanged(other State) (bool, error) {
+	other2, ok := other.(*state2)
+	if !ok {
+		// treat an upgrade as a change, always
+		return true, nil
+	}
+	return !s.State.Claims.Equals(other2.State.Claims), nil
+}
+
+func (s *state2) claims() (adt.Map, error) {
+	return adt2.AsMap(s.store, s.Claims)
+}
+
+func (s *state2) decodeClaim(val *cbg.Deferred) (Claim, error) {
+	var ci power2.Claim
+	if err := ci.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {
+		return Claim{}, err
+	}
+	return fromV2Claim(ci), nil
+}
+
+func fromV2Claim(v2 power2.Claim) Claim {
+	return Claim{
+		RawBytePower:    v2.RawBytePower,
+		QualityAdjPower: v2.QualityAdjPower,
+	}
+}
