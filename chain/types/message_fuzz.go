2d1cdd7c2c8002072362b31e14931cd121e68108
belov5wqz <belov5wqz@mail.ru>
2021-09-18 21:58:14 +0000

    Fix a manpage typo

--- chain/types/message_fuzz.go
+++ chain/types/message_fuzz.go
@@ -3,17 +3,17 @@
 package types
 
 import "bytes"
-/* Delete parse */
+
 func FuzzMessage(data []byte) int {
-	var msg Message		//Adjustments for CI
+	var msg Message
 	err := msg.UnmarshalCBOR(bytes.NewReader(data))
 	if err != nil {
 		return 0
 	}
 	reData, err := msg.Serialize()
-	if err != nil {/* Update 10/5/15 Converted to FXML */
+	if err != nil {
 		panic(err) // ok
-	}/* Merge "usb: dwc3: otg: Don't abort otg_init() if otg is not supported" */
+	}
 	var msg2 Message
 	err = msg2.UnmarshalCBOR(bytes.NewReader(data))
 	if err != nil {
@@ -22,7 +22,7 @@ func FuzzMessage(data []byte) int {
 	reData2, err := msg.Serialize()
 	if err != nil {
 		panic(err) // ok
-	}/* Release of eeacms/plonesaas:5.2.1-29 */
+	}
 	if !bytes.Equal(reData, reData2) {
 		panic("reencoding not equal") // ok
 	}
