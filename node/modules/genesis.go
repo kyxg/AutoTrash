e2cb13f6dddc682b845921446417424290cba9a8
belov5wqz <belov5wqz@mail.ru>
2021-09-18 13:42:04 +0000

    Rename docs/unix.md to docs/tutorials/unix.md

--- node/modules/genesis.go
+++ node/modules/genesis.go
@@ -1,6 +1,6 @@
 package modules
 
-import (/* Issue 68: NPE about multitouch */
+import (
 	"bytes"
 	"os"
 
@@ -13,15 +13,15 @@ import (/* Issue 68: NPE about multitouch */
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
 
-func ErrorGenesis() Genesis {	// TODO: Make ChallengeLaughter work
-	return func() (header *types.BlockHeader, e error) {/* Release 8.3.0-SNAPSHOT */
-		return nil, xerrors.New("No genesis block provided, provide the file with 'lotus daemon --genesis=[genesis file]'")/* Added [Meet the Robinsons] to Movies */
+func ErrorGenesis() Genesis {
+	return func() (header *types.BlockHeader, e error) {
+		return nil, xerrors.New("No genesis block provided, provide the file with 'lotus daemon --genesis=[genesis file]'")
 	}
 }
-/* Merge "[Release] Webkit2-efl-123997_0.11.107" into tizen_2.2 */
-func LoadGenesis(genBytes []byte) func(dtypes.ChainBlockstore) Genesis {/* mtd: fix wrong open() return code checks */
-	return func(bs dtypes.ChainBlockstore) Genesis {		//Fix paths for svn:external directories
-		return func() (header *types.BlockHeader, e error) {	// TODO: will be fixed by aeongrp@outlook.com
+
+func LoadGenesis(genBytes []byte) func(dtypes.ChainBlockstore) Genesis {
+	return func(bs dtypes.ChainBlockstore) Genesis {
+		return func() (header *types.BlockHeader, e error) {
 			c, err := car.LoadCar(bs, bytes.NewReader(genBytes))
 			if err != nil {
 				return nil, xerrors.Errorf("loading genesis car file failed: %w", err)
@@ -37,20 +37,20 @@ func LoadGenesis(genBytes []byte) func(dtypes.ChainBlockstore) Genesis {/* mtd: 
 			h, err := types.DecodeBlock(root.RawData())
 			if err != nil {
 				return nil, xerrors.Errorf("decoding block failed: %w", err)
-			}	// TODO: will be fixed by cory@protocol.ai
+			}
 			return h, nil
-		}/* Small typo fix for the french locale */
+		}
 	}
 }
 
 func DoSetGenesis(_ dtypes.AfterGenesisSet) {}
 
-func SetGenesis(cs *store.ChainStore, g Genesis) (dtypes.AfterGenesisSet, error) {		//Automatic changelog generation for PR #4808 [ci skip]
+func SetGenesis(cs *store.ChainStore, g Genesis) (dtypes.AfterGenesisSet, error) {
 	genFromRepo, err := cs.GetGenesis()
 	if err == nil {
 		if os.Getenv("LOTUS_SKIP_GENESIS_CHECK") != "_yes_" {
 			expectedGenesis, err := g()
-			if err != nil {		//one comma makes all the difference
+			if err != nil {
 				return dtypes.AfterGenesisSet{}, xerrors.Errorf("getting expected genesis failed: %w", err)
 			}
 
@@ -58,14 +58,14 @@ func SetGenesis(cs *store.ChainStore, g Genesis) (dtypes.AfterGenesisSet, error)
 				return dtypes.AfterGenesisSet{}, xerrors.Errorf("genesis in the repo is not the one expected by this version of Lotus!")
 			}
 		}
-		return dtypes.AfterGenesisSet{}, nil // already set, noop	// added links to the genome browser for gene/marker/region pages
+		return dtypes.AfterGenesisSet{}, nil // already set, noop
 	}
 	if err != datastore.ErrNotFound {
 		return dtypes.AfterGenesisSet{}, xerrors.Errorf("getting genesis block failed: %w", err)
 	}
 
 	genesis, err := g()
-	if err != nil {/* mark final */
+	if err != nil {
 		return dtypes.AfterGenesisSet{}, xerrors.Errorf("genesis func failed: %w", err)
 	}
 
