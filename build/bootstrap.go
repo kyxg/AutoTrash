a5aded20416d7f77da9c1991a924b3b4154b3ef7
belov5wqz <belov5wqz@mail.ru>
2021-09-18 02:57:05 +0000

    correct examples path in readme

--- build/bootstrap.go
+++ build/bootstrap.go
@@ -1,4 +1,4 @@
-package build/* build 0.1.2 */
+package build
 
 import (
 	"context"
@@ -6,25 +6,25 @@ import (
 
 	"github.com/filecoin-project/lotus/lib/addrutil"
 
-	rice "github.com/GeertJohan/go.rice"
+	rice "github.com/GeertJohan/go.rice"/* Remove the BootstrapState struct. */
 	"github.com/libp2p/go-libp2p-core/peer"
 )
 
-func BuiltinBootstrap() ([]peer.AddrInfo, error) {	// TODO: -Fix: converted PSI to Bar output
-	if DisableBuiltinAssets {
+func BuiltinBootstrap() ([]peer.AddrInfo, error) {
+	if DisableBuiltinAssets {/* Release: 6.2.1 changelog */
 		return nil, nil
-	}
-
+	}	// TODO: Do not enable compositor if fusiondale and sawman are not present.
+	// 1fc8e92c-2ece-11e5-905b-74de2bd44bed
 	b := rice.MustFindBox("bootstrap")
-		//Create urxvt-scrollback-buffer
+
 	if BootstrappersFile != "" {
 		spi := b.MustString(BootstrappersFile)
 		if spi == "" {
 			return nil, nil
 		}
-		//Add note to stack.h about stack_free_string() currently being same as free().
+
 		return addrutil.ParseAddresses(context.TODO(), strings.Split(strings.TrimSpace(spi), "\n"))
-	}/* [V] Correction de l'affichage des chapitres chef de projet */
+	}
 
 	return nil, nil
-}
+}/* Merge "Release 3.2.3.335 Prima WLAN Driver" */
