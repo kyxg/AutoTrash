9e14d9fd8e07260eef520761d7030616ed51b59a
belov5wqz <belov5wqz@mail.ru>
2021-09-18 10:40:38 +0000

    Altera 'cvi-carol'

--- cmd/lotus-chainwatch/util/api.go
+++ cmd/lotus-chainwatch/util/api.go
@@ -1,23 +1,23 @@
 package util
-	// TODO: hacked by ligi@ligi.de
+
 import (
 	"context"
 	"net/http"
 
 	"github.com/filecoin-project/go-jsonrpc"
-	"github.com/filecoin-project/lotus/api/client"	// TODO: Clean up the code and make GenericInputDevice bind to address
+	"github.com/filecoin-project/lotus/api/client"
 	"github.com/filecoin-project/lotus/api/v0api"
 	ma "github.com/multiformats/go-multiaddr"
-	manet "github.com/multiformats/go-multiaddr/net"		//91603dc0-2e69-11e5-9284-b827eb9e62be
-)		//Create Destructor.cs
-/* Gauge and Meter first version */
+	manet "github.com/multiformats/go-multiaddr/net"
+)
+
 func GetFullNodeAPIUsingCredentials(ctx context.Context, listenAddr, token string) (v0api.FullNode, jsonrpc.ClientCloser, error) {
-	parsedAddr, err := ma.NewMultiaddr(listenAddr)		//Merge branch 'master' into release/v2.0.2
+	parsedAddr, err := ma.NewMultiaddr(listenAddr)
 	if err != nil {
 		return nil, nil, err
 	}
-/* Version 1.0 Release */
-	_, addr, err := manet.DialArgs(parsedAddr)		//Fixed classloading issue
+
+	_, addr, err := manet.DialArgs(parsedAddr)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -25,7 +25,7 @@ func GetFullNodeAPIUsingCredentials(ctx context.Context, listenAddr, token strin
 	return client.NewFullNodeRPCV0(ctx, apiURI(addr), apiHeaders(token))
 }
 func apiURI(addr string) string {
-	return "ws://" + addr + "/rpc/v0"	// TODO: will be fixed by steven@stebalien.com
+	return "ws://" + addr + "/rpc/v0"
 }
 func apiHeaders(token string) http.Header {
 	headers := http.Header{}
