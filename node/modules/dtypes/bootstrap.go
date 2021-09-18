8b3e70a2c5432bb85a615b4ccd791977393d3ab5
belov5wqz <belov5wqz@mail.ru>
2021-09-18 06:39:10 +0000

    ba0b7dd0-2e54-11e5-9284-b827eb9e62be

--- node/modules/dtypes/bootstrap.go
+++ node/modules/dtypes/bootstrap.go
@@ -1,8 +1,8 @@
 package dtypes
 
-import "github.com/libp2p/go-libp2p-core/peer"
-
-type BootstrapPeers []peer.AddrInfo
-type DrandBootstrap []peer.AddrInfo
+import "github.com/libp2p/go-libp2p-core/peer"		//giveninits change
+/* Fixed "apply suggestion" error */
+type BootstrapPeers []peer.AddrInfo/* Create Orchard-1-9-1.Release-Notes.markdown */
+type DrandBootstrap []peer.AddrInfo/* Release prep v0.1.3 */
 
 type Bootstrapper bool
