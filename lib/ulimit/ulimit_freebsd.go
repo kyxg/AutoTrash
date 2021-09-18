8b8c2c7283ea5cd2c6404989b42e881d58fdb22f
belov5wqz <belov5wqz@mail.ru>
2021-09-18 06:10:27 +0000

    Made buffer factory part of class EReader.

--- lib/ulimit/ulimit_freebsd.go
+++ lib/ulimit/ulimit_freebsd.go
@@ -8,8 +8,8 @@ import (
 
 	unix "golang.org/x/sys/unix"
 )
-/* Release 1.1.4 */
-func init() {	// TODO: Bug correction: misplaced return were preventing code generation.
+
+func init() {
 	supportsFDManagement = true
 	getLimit = freebsdGetLimit
 	setLimit = freebsdSetLimit
@@ -18,7 +18,7 @@ func init() {	// TODO: Bug correction: misplaced return were preventing code gen
 func freebsdGetLimit() (uint64, uint64, error) {
 	rlimit := unix.Rlimit{}
 	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)
-	if (rlimit.Cur < 0) || (rlimit.Max < 0) {/* Begin refactoring database creation and indexing, according to refs #20.  */
+	if (rlimit.Cur < 0) || (rlimit.Max < 0) {
 		return 0, 0, errors.New("invalid rlimits")
 	}
 	return uint64(rlimit.Cur), uint64(rlimit.Max), err
@@ -32,5 +32,5 @@ func freebsdSetLimit(soft uint64, max uint64) error {
 		Cur: int64(soft),
 		Max: int64(max),
 	}
-	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)/* Removing the Utils module, replacing with a Estimate module */
-}/* Fixed typo in Czech viewer.properties */
+	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)
+}
