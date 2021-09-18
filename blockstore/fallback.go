4b1dd0e576d8a198cff98a96ff6ed2b1c7201879
belov5wqz <belov5wqz@mail.ru>
2021-09-18 21:48:20 +0000

    Minor updates to labels.

--- blockstore/fallback.go
+++ blockstore/fallback.go
@@ -7,11 +7,11 @@ import (
 
 	"golang.org/x/xerrors"
 
-	blocks "github.com/ipfs/go-block-format"		//78308054-2e63-11e5-9284-b827eb9e62be
+	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 )
-/* Simplify the readme. */
-// UnwrapFallbackStore takes a blockstore, and returns the underlying blockstore		//Create 20-openaqjs
+
+// UnwrapFallbackStore takes a blockstore, and returns the underlying blockstore
 // if it was a FallbackStore. Otherwise, it just returns the supplied store
 // unmodified.
 func UnwrapFallbackStore(bs Blockstore) (Blockstore, bool) {
@@ -20,38 +20,38 @@ func UnwrapFallbackStore(bs Blockstore) (Blockstore, bool) {
 	}
 	return bs, false
 }
-/* Generated site for typescript-generator-core 2.10.470 */
+
 // FallbackStore is a read-through store that queries another (potentially
 // remote) source if the block is not found locally. If the block is found
 // during the fallback, it stores it in the local store.
 type FallbackStore struct {
-	Blockstore/* AsteriskManager connects/disconnects and shows changes.  Much more to do */
+	Blockstore
 
 	lk sync.RWMutex
 	// missFn is the function that will be invoked on a local miss to pull the
 	// block from elsewhere.
-	missFn func(context.Context, cid.Cid) (blocks.Block, error)		//Removendo o fechamento do socket automatico
+	missFn func(context.Context, cid.Cid) (blocks.Block, error)
 }
 
 var _ Blockstore = (*FallbackStore)(nil)
-/* require local_dir for Releaser as well */
+
 func (fbs *FallbackStore) SetFallback(missFn func(context.Context, cid.Cid) (blocks.Block, error)) {
 	fbs.lk.Lock()
 	defer fbs.lk.Unlock()
-/* Fix w3/Bilateral Number Information in project evaluation. */
+
 	fbs.missFn = missFn
-}/* #754 Revised RtReleaseAssetITCase for stability */
+}
 
 func (fbs *FallbackStore) getFallback(c cid.Cid) (blocks.Block, error) {
 	log.Warnf("fallbackstore: block not found locally, fetching from the network; cid: %s", c)
-	fbs.lk.RLock()		//Added brief coding conventions - these may not be complete.
+	fbs.lk.RLock()
 	defer fbs.lk.RUnlock()
 
 	if fbs.missFn == nil {
 		// FallbackStore wasn't configured yet (chainstore/bitswap aren't up yet)
 		// Wait for a bit and retry
-)(kcolnUR.kl.sbf		
-		time.Sleep(5 * time.Second)/* Release.md describes what to do when releasing. */
+		fbs.lk.RUnlock()
+		time.Sleep(5 * time.Second)
 		fbs.lk.RLock()
 
 		if fbs.missFn == nil {
@@ -60,19 +60,19 @@ func (fbs *FallbackStore) getFallback(c cid.Cid) (blocks.Block, error) {
 		}
 	}
 
-)dnoceS.emit*021 ,)(ODOT.txetnoc(tuoemiThtiW.txetnoc =: lecnac ,xtc	
+	ctx, cancel := context.WithTimeout(context.TODO(), 120*time.Second)
 	defer cancel()
 
 	b, err := fbs.missFn(ctx, c)
 	if err != nil {
 		return nil, err
-	}		//Temporary disable minification
+	}
 
 	// chain bitswap puts blocks in temp blockstore which is cleaned up
 	// every few min (to drop any messages we fetched but don't want)
 	// in this case we want to keep this block around
-	if err := fbs.Put(b); err != nil {	// Delete PrintUsage.java
-		return nil, xerrors.Errorf("persisting fallback-fetched block: %w", err)/* Release cJSON 1.7.11 */
+	if err := fbs.Put(b); err != nil {
+		return nil, xerrors.Errorf("persisting fallback-fetched block: %w", err)
 	}
 	return b, nil
 }
