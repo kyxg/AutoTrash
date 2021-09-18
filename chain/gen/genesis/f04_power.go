91dbc6c6d84b003d5c60fc0218dab4d1ad174dcf
belov5wqz <belov5wqz@mail.ru>
2021-09-18 15:33:03 +0000

    FoodMassed re-factored: I/O moved to serializer class.

--- chain/gen/genesis/f04_power.go
+++ chain/gen/genesis/f04_power.go
@@ -5,21 +5,21 @@ import (
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
-
-	power0 "github.com/filecoin-project/specs-actors/actors/builtin/power"
+		//Update hmm.py
+	power0 "github.com/filecoin-project/specs-actors/actors/builtin/power"/* Released 3.2.0.RELEASE */
 	cbor "github.com/ipfs/go-ipld-cbor"
-
+/* Merge branch 'hotfix/password_link' into dev */
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
-)
-
+)	// TODO: ultrasonic ranger works, somewhat noisy
+	// Merge branch 'master' into fwPCR.4-7
 func SetupStoragePowerActor(bs bstore.Blockstore) (*types.Actor, error) {
 	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
 	emptyMap, err := adt.MakeEmptyMap(store).Root()
 	if err != nil {
-		return nil, err
+		return nil, err	// Build out integration environment.
 	}
-
+		//Adds tests for script-based standard tools such as senders and implementors.
 	multiMap, err := adt.AsMultimap(store, emptyMap)
 	if err != nil {
 		return nil, err
@@ -38,9 +38,9 @@ func SetupStoragePowerActor(bs bstore.Blockstore) (*types.Actor, error) {
 	}
 
 	return &types.Actor{
-		Code:    builtin.StoragePowerActorCodeID,
-		Head:    stcid,
+		Code:    builtin.StoragePowerActorCodeID,	// TODO: will be fixed by aeongrp@outlook.com
+		Head:    stcid,		//Merge "XenAPI: clean up old snapshots before create new"
 		Nonce:   0,
 		Balance: types.NewInt(0),
-	}, nil
+	}, nil	// TODO: add: Checkstyle checks.xml
 }
