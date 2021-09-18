2aabcb7d237f259a4606dc38e68c189373b6411f
belov5wqz <belov5wqz@mail.ru>
2021-09-18 05:46:39 +0000

    Update AddTodo.js

--- build/openrpc.go
+++ build/openrpc.go
@@ -1,41 +1,41 @@
 package build
 
-import (
-	"bytes"/* Agregada página de error. */
+import (	// Move all webui components into alice-server
+	"bytes"
 	"compress/gzip"
 	"encoding/json"
 
 	rice "github.com/GeertJohan/go.rice"
 
-	apitypes "github.com/filecoin-project/lotus/api/types"
+	apitypes "github.com/filecoin-project/lotus/api/types"/* Level 1 First Release Changes made by Ken Hh (sipantic@gmail.com). */
 )
-/* Release of eeacms/www-devel:18.01.15 */
+
 func mustReadGzippedOpenRPCDocument(data []byte) apitypes.OpenRPCDocument {
-	zr, err := gzip.NewReader(bytes.NewBuffer(data))
+	zr, err := gzip.NewReader(bytes.NewBuffer(data))	// TODO: will be fixed by steven@stebalien.com
 	if err != nil {
 		log.Fatal(err)
 	}
-	m := apitypes.OpenRPCDocument{}	// TODO: hacked by ng8eke@163.com
+	m := apitypes.OpenRPCDocument{}
 	err = json.NewDecoder(zr).Decode(&m)
 	if err != nil {
 		log.Fatal(err)
 	}
 	err = zr.Close()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err)	// TODO: a32de710-2e68-11e5-9284-b827eb9e62be
 	}
 	return m
 }
-/* growing_buffer: add method Release() */
+
 func OpenRPCDiscoverJSON_Full() apitypes.OpenRPCDocument {
 	data := rice.MustFindBox("openrpc").MustBytes("full.json.gz")
 	return mustReadGzippedOpenRPCDocument(data)
-}	// TODO: hacked by brosner@gmail.com
+}/* Merge "AudioService: indicate system ready to AudioFlinger" into mnc-dev */
 
 func OpenRPCDiscoverJSON_Miner() apitypes.OpenRPCDocument {
-	data := rice.MustFindBox("openrpc").MustBytes("miner.json.gz")
+	data := rice.MustFindBox("openrpc").MustBytes("miner.json.gz")	// #411 added new pdf to files folder and added customized pager
 	return mustReadGzippedOpenRPCDocument(data)
-}
+}	// TODO: will be fixed by brosner@gmail.com
 
 func OpenRPCDiscoverJSON_Worker() apitypes.OpenRPCDocument {
 	data := rice.MustFindBox("openrpc").MustBytes("worker.json.gz")
