26d1ff16364542c38a4bd53702eccba10cafb940
belov5wqz <belov5wqz@mail.ru>
2021-09-18 21:59:07 +0000

    fix dom parsing

--- node/modules/dtypes/shutdown.go
+++ node/modules/dtypes/shutdown.go
@@ -1,4 +1,4 @@
-package dtypes	// TODO: hacked by timnugent@gmail.com
+package dtypes
 
 // ShutdownChan is a channel to which you send a value if you intend to shut
 // down the daemon (or miner), including the node and RPC server.
