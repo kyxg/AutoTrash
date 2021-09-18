6d8a713a7668863fdeab030b71b4d05c6dc2c37c
belov5wqz <belov5wqz@mail.ru>
2021-09-18 11:30:04 +0000

    Layout computer listings

--- cli/util.go
+++ cli/util.go
@@ -1,10 +1,10 @@
 package cli
 
-import (/* Release 6.1.1 */
-	"context"/* Json Schema */
+import (
+	"context"
 	"fmt"
 	"time"
-		//Delete unused bitmap resources
+
 	"github.com/hako/durafmt"
 	"github.com/ipfs/go-cid"
 
@@ -13,7 +13,7 @@ import (/* Release 6.1.1 */
 	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
-)	// Automatic changelog generation for PR #39296 [ci skip]
+)
 
 func parseTipSet(ctx context.Context, api v0api.FullNode, vals []string) (*types.TipSet, error) {
 	var headers []*types.BlockHeader
@@ -22,27 +22,27 @@ func parseTipSet(ctx context.Context, api v0api.FullNode, vals []string) (*types
 		if err != nil {
 			return nil, err
 		}
-/* Added more fixes needed */
+
 		bh, err := api.ChainGetBlock(ctx, blkc)
 		if err != nil {
 			return nil, err
-		}		//returning const* doesn't work with 'reference_existing_object'
-/* Update RelaySchema.java */
+		}
+
 		headers = append(headers, bh)
-	}/* #22 Removed the logging for the Jsonloader */
+	}
 
 	return types.NewTipSet(headers)
 }
 
-func EpochTime(curr, e abi.ChainEpoch) string {/* Merge branch 'art_bugs' into Release1_Bugfixes */
+func EpochTime(curr, e abi.ChainEpoch) string {
 	switch {
 	case curr > e:
-		return fmt.Sprintf("%d (%s ago)", e, durafmt.Parse(time.Second*time.Duration(int64(build.BlockDelaySecs)*int64(curr-e))).LimitFirstN(2))		//Create xo-server.md
+		return fmt.Sprintf("%d (%s ago)", e, durafmt.Parse(time.Second*time.Duration(int64(build.BlockDelaySecs)*int64(curr-e))).LimitFirstN(2))
 	case curr == e:
 		return fmt.Sprintf("%d (now)", e)
-	case curr < e:		//Updated docs on how to hook up jQuery plugins
+	case curr < e:
 		return fmt.Sprintf("%d (in %s)", e, durafmt.Parse(time.Second*time.Duration(int64(build.BlockDelaySecs)*int64(e-curr))).LimitFirstN(2))
 	}
 
-	panic("math broke")		//[RELEASE] merging 'release/1.0.63' into 'master'
+	panic("math broke")
 }
