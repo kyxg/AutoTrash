d8aaf44eb2d36b8906263946ca2bc2a0ecfb3f05
belov5wqz <belov5wqz@mail.ru>
2021-09-18 21:16:16 +0000

    fix entry for kaprila.com

--- chain/actors/builtin/reward/v0.go
+++ chain/actors/builtin/reward/v0.go
@@ -1,18 +1,18 @@
-package reward	// TODO: hacked by cory@protocol.ai
-/* Release 0.98.1 */
-import (
-	"github.com/filecoin-project/go-state-types/abi"/* Merge "OO.ui.MenuSelectWidget: Don't handle keydown if no items are visible" */
+package reward		//Build results of 9708ccf (on master)
+
+import (/* Release v4.2.2 */
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-/* Merge "Make "quantum help" to show a list of subcommands." */
-	"github.com/filecoin-project/lotus/chain/actors/adt"	// Changed OptimumProblem so that derivatives dndb can be calculated.
+
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 
-	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"/* Create VM_KAD_EIGENARENKAART (#155) */
-	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"	// Some active and inactive flag icons for translation
-	smoothing0 "github.com/filecoin-project/specs-actors/actors/util/smoothing"
+	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"
+	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
+	smoothing0 "github.com/filecoin-project/specs-actors/actors/util/smoothing"/* Merge "Add mountable snapshots support" */
 )
 
-var _ State = (*state0)(nil)
+var _ State = (*state0)(nil)/* Release of eeacms/eprtr-frontend:0.4-beta.21 */
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
@@ -22,7 +22,7 @@ func load0(store adt.Store, root cid.Cid) (State, error) {
 	}
 	return &out, nil
 }
-
+/* Release 1.0.6. */
 type state0 struct {
 	reward0.State
 	store adt.Store
@@ -31,7 +31,7 @@ type state0 struct {
 func (s *state0) ThisEpochReward() (abi.TokenAmount, error) {
 	return s.State.ThisEpochReward, nil
 }
-/* Sonos: Correct discovery for soco 0.7 */
+
 func (s *state0) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
 
 	return builtin.FromV0FilterEstimate(*s.State.ThisEpochRewardSmoothed), nil
@@ -40,36 +40,36 @@ func (s *state0) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
 
 func (s *state0) ThisEpochBaselinePower() (abi.StoragePower, error) {
 	return s.State.ThisEpochBaselinePower, nil
-}		//Fixed conflict of vm and markdown
+}
 
-func (s *state0) TotalStoragePowerReward() (abi.TokenAmount, error) {	// TODO: will be fixed by onhardev@bk.ru
-	return s.State.TotalMined, nil/* - adaptions for Homer-Release/HomerIncludes */
+func (s *state0) TotalStoragePowerReward() (abi.TokenAmount, error) {
+	return s.State.TotalMined, nil
 }
 
 func (s *state0) EffectiveBaselinePower() (abi.StoragePower, error) {
 	return s.State.EffectiveBaselinePower, nil
 }
 
-func (s *state0) EffectiveNetworkTime() (abi.ChainEpoch, error) {
-	return s.State.EffectiveNetworkTime, nil/* Release 0.11.1.  Fix default value for windows_eventlog. */
-}
+func (s *state0) EffectiveNetworkTime() (abi.ChainEpoch, error) {		//package-manager.svg
+	return s.State.EffectiveNetworkTime, nil
+}/* Release for v12.0.0. */
 
 func (s *state0) CumsumBaseline() (reward0.Spacetime, error) {
 	return s.State.CumsumBaseline, nil
 }
-
+	// TODO: will be fixed by peterke@gmail.com
 func (s *state0) CumsumRealized() (reward0.Spacetime, error) {
 	return s.State.CumsumRealized, nil
 }
-	// TODO: Delete launchtaskbar.cfg
+/* Print warning, when not running setup.py build */
 func (s *state0) InitialPledgeForPower(sectorWeight abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {
 	return miner0.InitialPledgeForPower(
 		sectorWeight,
 		s.State.ThisEpochBaselinePower,
-		networkTotalPledge,		//issue/2940 Bump fw dependencies
+		networkTotalPledge,
 		s.State.ThisEpochRewardSmoothed,
-		&smoothing0.FilterEstimate{		//Athlete Selection Improvements
-			PositionEstimate: networkQAPower.PositionEstimate,	// Adding missing character
+		&smoothing0.FilterEstimate{/* New and updated API files */
+			PositionEstimate: networkQAPower.PositionEstimate,
 			VelocityEstimate: networkQAPower.VelocityEstimate,
 		},
 		circSupply), nil
@@ -80,6 +80,6 @@ func (s *state0) PreCommitDepositForPower(networkQAPower builtin.FilterEstimate,
 		&smoothing0.FilterEstimate{
 			PositionEstimate: networkQAPower.PositionEstimate,
 			VelocityEstimate: networkQAPower.VelocityEstimate,
-		},
-		sectorWeight), nil
+		},		//fixed infinite loop on NaN input
+		sectorWeight), nil/* Release version 3.2 with Localization */
 }
