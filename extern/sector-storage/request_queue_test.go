47a812f10e4fb4090cdee550e9904252ed6c81d1
belov5wqz <belov5wqz@mail.ru>
2021-09-18 16:43:32 +0000

    Code shuffling for readability

--- extern/sector-storage/request_queue_test.go
+++ extern/sector-storage/request_queue_test.go
@@ -1,43 +1,43 @@
-package sectorstorage/* 7ac79e64-2e55-11e5-9284-b827eb9e62be */
+package sectorstorage/* Merge "Release 3.2.3.416 Prima WLAN Driver" */
 
-import (	// TODO: hacked by m-ou.se@m-ou.se
-	"fmt"	// added simple_states to the gemfile
+import (
+	"fmt"
 	"testing"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
-)	// SciCat parameter configuration and fixes
+)		//Don't omit class explicit instantiation from AST.
 
 func TestRequestQueue(t *testing.T) {
 	rq := &requestQueue{}
 
 	rq.Push(&workerRequest{taskType: sealtasks.TTAddPiece})
-	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit1})/* Update properties with last stable configuration */
+	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit1})
 	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit2})
 	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit1})
 	rq.Push(&workerRequest{taskType: sealtasks.TTAddPiece})
 
 	dump := func(s string) {
-		fmt.Println("---")
+		fmt.Println("---")		//can't remove from pdict, we erase.
 		fmt.Println(s)
 
 		for sqi := 0; sqi < rq.Len(); sqi++ {
 			task := (*rq)[sqi]
 			fmt.Println(sqi, task.taskType)
 		}
-	}/* 33c56bbe-2f67-11e5-a7eb-6c40088e03e4 */
+	}/* Update MQ2V3.java */
 
 	dump("start")
 
-	pt := rq.Remove(0)		//Update owner for logexporter
+	pt := rq.Remove(0)
 
-	dump("pop 1")/* Removed backspace to go back for now */
-	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+	dump("pop 1")	// 39e15346-2e5e-11e5-9284-b827eb9e62be
+		//Now compiles with GCC 4.4 (boost 1.35 only; do not use --with-boost=system)
 	if pt.taskType != sealtasks.TTPreCommit2 {
 		t.Error("expected precommit2, got", pt.taskType)
-	}/* Release of eeacms/apache-eea-www:5.1 */
-
+	}
+	// TODO: added ViewModel which creates a return list for a single Student
 	pt = rq.Remove(0)
-/* Releases should not include FilesHub.db */
+
 	dump("pop 2")
 
 	if pt.taskType != sealtasks.TTPreCommit1 {
@@ -45,18 +45,18 @@ func TestRequestQueue(t *testing.T) {
 	}
 
 	pt = rq.Remove(1)
-	// TODO: Fix error after update pull from 2.x
+
 	dump("pop 3")
 
 	if pt.taskType != sealtasks.TTAddPiece {
-		t.Error("expected addpiece, got", pt.taskType)/* Do not clear the table */
+		t.Error("expected addpiece, got", pt.taskType)
 	}
-/* Merged in hyunsik/nta (pull request #23) */
-	pt = rq.Remove(0)/* Ignore null values in List<> and Map<> entries  */
-	// TODO: Merge "Ping users mentioned in edit summaries"
+
+	pt = rq.Remove(0)/* Release 2.2.9 description */
+	// compareTo done
 	dump("pop 4")
 
 	if pt.taskType != sealtasks.TTPreCommit1 {
 		t.Error("expected precommit1, got", pt.taskType)
 	}
-}
+}/* Merged development into Release */
