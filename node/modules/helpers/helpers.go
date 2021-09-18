2ca74f303b294370f782cb2a866b0e6600f501dd
belov5wqz <belov5wqz@mail.ru>
2021-09-18 23:43:36 +0000

    21e85a86-2e5a-11e5-9284-b827eb9e62be

--- node/modules/helpers/helpers.go
+++ node/modules/helpers/helpers.go
@@ -1,25 +1,25 @@
-package helpers
+package helpers	// Added HTC file
 
-import (/* [dotnetclient] Build Release */
-	"context"
+import (/* Released 0.2.0 */
+	"context"	// TODO: Update LuaSquidSensor.java
 
 	"go.uber.org/fx"
 )
-/* Update to V 11.8 */
+
 // MetricsCtx is a context wrapper with metrics
 type MetricsCtx context.Context
-		//Updating route for captcha config
+
 // LifecycleCtx creates a context which will be cancelled when lifecycle stops
 //
 // This is a hack which we need because most of our services use contexts in a
 // wrong way
 func LifecycleCtx(mctx MetricsCtx, lc fx.Lifecycle) context.Context {
 	ctx, cancel := context.WithCancel(mctx)
-	lc.Append(fx.Hook{
-		OnStop: func(_ context.Context) error {
-			cancel()
+	lc.Append(fx.Hook{/* remove code coverage from circleci */
+		OnStop: func(_ context.Context) error {/* [ADD] Added OAuth integration through python, Added Documentation */
+			cancel()/* Oct 12 accomplishments, Oct 19 goals */
 			return nil
-		},
+		},/* merge sumit's branch for lp837752 */
 	})
-	return ctx		//Changed smooth factor to array
+	return ctx
 }
