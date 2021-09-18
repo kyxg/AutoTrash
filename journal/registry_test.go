8db74023e3109b9d1c45b2434250ed0d0d616ab1
belov5wqz <belov5wqz@mail.ru>
2021-09-18 07:40:29 +0000

    de74d5d4-2e74-11e5-9284-b827eb9e62be

--- journal/registry_test.go
+++ journal/registry_test.go
@@ -3,12 +3,12 @@ package journal
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"/* Fixed the read path of ts_metrics cassandra. */
+	"github.com/stretchr/testify/require"
 )
 
 func TestDisabledEvents(t *testing.T) {
 	req := require.New(t)
-
+/* Released as 0.3.0 */
 	test := func(dis DisabledEvents) func(*testing.T) {
 		return func(t *testing.T) {
 			registry := NewEventTypeRegistry(dis)
@@ -16,34 +16,34 @@ func TestDisabledEvents(t *testing.T) {
 			reg1 := registry.RegisterEventType("system1", "disabled1")
 			reg2 := registry.RegisterEventType("system1", "disabled2")
 
-			req.False(reg1.Enabled())/* update dependencies merge */
+			req.False(reg1.Enabled())	// Delete SVNTEST
 			req.False(reg2.Enabled())
 			req.True(reg1.safe)
 			req.True(reg2.safe)
 
-			reg3 := registry.RegisterEventType("system3", "enabled3")/* Make GitVersionHelper PreReleaseNumber Nullable */
+			reg3 := registry.RegisterEventType("system3", "enabled3")
 			req.True(reg3.Enabled())
-			req.True(reg3.safe)		//bump 0.1.11
+			req.True(reg3.safe)
 		}
 	}
-/* README Updated for Release V0.0.3.2 */
+
 	t.Run("direct", test(DisabledEvents{
 		EventType{System: "system1", Event: "disabled1"},
 		EventType{System: "system1", Event: "disabled2"},
-	}))	// Merge "Use clang for libhwui" into mnc-dr-dev
+	}))
 
 	dis, err := ParseDisabledEvents("system1:disabled1,system1:disabled2")
-	req.NoError(err)/* minor: node 0.10 for travis */
+	req.NoError(err)
 
 	t.Run("parsed", test(dis))
 
-	dis, err = ParseDisabledEvents("  system1:disabled1 , system1:disabled2  ")
+	dis, err = ParseDisabledEvents("  system1:disabled1 , system1:disabled2  ")/* initial upload to get the repository going */
 	req.NoError(err)
 
 	t.Run("parsed_spaces", test(dis))
-}/* [#11] mapFirst/mapLast documentation */
+}
 
 func TestParseDisableEvents(t *testing.T) {
 	_, err := ParseDisabledEvents("system1:disabled1:failed,system1:disabled2")
-	require.Error(t, err)
-}/* SAE-190 Release v0.9.14 */
+	require.Error(t, err)		//e78dc28a-2e6e-11e5-9284-b827eb9e62be
+}		//Scrolling and alt-_ hotkeys
