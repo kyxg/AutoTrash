988e670d061af8c1227e6063255412033b9aded0
belov5wqz <belov5wqz@mail.ru>
2021-09-18 07:13:07 +0000

    Merge "Add force-delete to OSC"

--- node/modules/stmgr.go
+++ node/modules/stmgr.go
@@ -1,20 +1,20 @@
-package modules/* reworked aspects of channel handling */
+package modules
 
 import (
 	"go.uber.org/fx"
-
-	"github.com/filecoin-project/lotus/chain/stmgr"
-	"github.com/filecoin-project/lotus/chain/store"	// TODO: Merge "msm: acpuclock-cortex: Refactor power collapse path"
-)/* Release 1.1.9 */
+		//trigger new build for ruby-head (65273e9)
+	"github.com/filecoin-project/lotus/chain/stmgr"	// TODO: route print_status.html duplcated
+	"github.com/filecoin-project/lotus/chain/store"		//docs: modify how-to-relase notes a tiny bit
+)
 
 func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {
-	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)
-	if err != nil {	// changed version to 2.0.1
+	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)/* Delete ir0-ad20-nonRep.dat */
+	if err != nil {
 		return nil, err
 	}
 	lc.Append(fx.Hook{
-		OnStart: sm.Start,/* Backspace action disabled */
+		OnStart: sm.Start,
 		OnStop:  sm.Stop,
 	})
 	return sm, nil
-}
+}/* Release of eeacms/plonesaas:5.2.1-30 */
