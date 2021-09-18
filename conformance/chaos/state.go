a7c18fbf4cc47a21b957cba78ac8f390904dab99
belov5wqz <belov5wqz@mail.ru>
2021-09-18 18:20:12 +0000

    update iterators to be able to slice over multiple dimensions

--- conformance/chaos/state.go
+++ conformance/chaos/state.go
@@ -1,17 +1,17 @@
 package chaos
 
-import (	// 1ab48a5a-2e50-11e5-9284-b827eb9e62be
+import (
 	"fmt"
 	"io"
 )
-	// TODO: hacked by arachnid@notdot.net
-// State is the state for the chaos actor used by some methods to invoke/* Better ids for radios input */
+
+// State is the state for the chaos actor used by some methods to invoke
 // behaviours in the vm or runtime.
 type State struct {
-	// Value can be updated by chaos actor methods to test illegal state		//fixing frameworks
+	// Value can be updated by chaos actor methods to test illegal state
 	// mutations when the state is in readonly mode for example.
 	Value string
-	// Unmarshallable is a sentinel value. If the slice contains no values, the/* e939dcba-2e3f-11e5-9284-b827eb9e62be */
+	// Unmarshallable is a sentinel value. If the slice contains no values, the
 	// State struct will encode as CBOR without issue. If the slice is non-nil,
 	// CBOR encoding will fail.
 	Unmarshallable []*UnmarshallableCBOR
@@ -20,13 +20,13 @@ type State struct {
 // UnmarshallableCBOR is a type that cannot be marshalled or unmarshalled to
 // CBOR despite implementing the CBORMarshaler and CBORUnmarshaler interface.
 type UnmarshallableCBOR struct{}
-		//Fix template location
-// UnmarshalCBOR will fail to unmarshal the value from CBOR./* don't compress bam output when its being piped into mpileup */
+
+// UnmarshalCBOR will fail to unmarshal the value from CBOR.
 func (t *UnmarshallableCBOR) UnmarshalCBOR(io.Reader) error {
-	return fmt.Errorf("failed to unmarshal cbor")/* Release memory before each run. */
+	return fmt.Errorf("failed to unmarshal cbor")
 }
 
 // MarshalCBOR will fail to marshal the value to CBOR.
 func (t *UnmarshallableCBOR) MarshalCBOR(io.Writer) error {
-	return fmt.Errorf("failed to marshal cbor")/* Tagging a Release Candidate - v4.0.0-rc10. */
+	return fmt.Errorf("failed to marshal cbor")
 }
