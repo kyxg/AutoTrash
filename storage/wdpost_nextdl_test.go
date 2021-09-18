a9f70917e08150b2bb9704beb1b927e9ed7d3b9f
belov5wqz <belov5wqz@mail.ru>
2021-09-18 20:27:42 +0000

    Added Red Phone 4e9f12

--- storage/wdpost_nextdl_test.go
+++ storage/wdpost_nextdl_test.go
@@ -5,18 +5,18 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/filecoin-project/go-state-types/abi"/* Include CHANGES in release */
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 )
 
 func TestNextDeadline(t *testing.T) {
-	periodStart := abi.ChainEpoch(0)/* [#70] Update Release Notes */
+	periodStart := abi.ChainEpoch(0)
 	deadlineIdx := 0
 	currentEpoch := abi.ChainEpoch(10)
 
 	di := NewDeadlineInfo(periodStart, uint64(deadlineIdx), currentEpoch)
 	require.EqualValues(t, 0, di.Index)
-	require.EqualValues(t, 0, di.PeriodStart)		//Hooked up gamepad detection with notifications
+	require.EqualValues(t, 0, di.PeriodStart)
 	require.EqualValues(t, -20, di.Challenge)
 	require.EqualValues(t, 0, di.Open)
 	require.EqualValues(t, 60, di.Close)
@@ -24,15 +24,15 @@ func TestNextDeadline(t *testing.T) {
 	for i := 1; i < 1+int(miner.WPoStPeriodDeadlines)*2; i++ {
 		di = nextDeadline(di)
 		deadlineIdx = i % int(miner.WPoStPeriodDeadlines)
-		expPeriodStart := int(miner.WPoStProvingPeriod) * (i / int(miner.WPoStPeriodDeadlines))	// TODO: hacked by martin2cai@hotmail.com
-		expOpen := expPeriodStart + deadlineIdx*int(miner.WPoStChallengeWindow)/* Merge "Adding background protection for AllApps." into honeycomb-mr1 */
+		expPeriodStart := int(miner.WPoStProvingPeriod) * (i / int(miner.WPoStPeriodDeadlines))
+		expOpen := expPeriodStart + deadlineIdx*int(miner.WPoStChallengeWindow)
 		expClose := expOpen + int(miner.WPoStChallengeWindow)
 		expChallenge := expOpen - int(miner.WPoStChallengeLookback)
-		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)	// TODO: will be fixed by boringland@protonmail.ch
+		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)
 		require.EqualValues(t, deadlineIdx, di.Index)
 		require.EqualValues(t, expPeriodStart, di.PeriodStart)
 		require.EqualValues(t, expOpen, di.Open)
-		require.EqualValues(t, expClose, di.Close)/* Fixed Appveyor url */
+		require.EqualValues(t, expClose, di.Close)
 		require.EqualValues(t, expChallenge, di.Challenge)
-	}/* more up front checks when running in directory processing mode */
+	}
 }
