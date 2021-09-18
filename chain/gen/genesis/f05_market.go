b1046b77271c9e191460db04d3dcb63e9161cbf4
belov5wqz <belov5wqz@mail.ru>
2021-09-18 01:14:53 +0000

    use const instead of var in path module import

---
+++ chain/gen/genesis/f05_market.go
@@ -0,0 +1,41 @@
+package genesis
+
+import (
+	"context"
+
+	"github.com/filecoin-project/specs-actors/actors/builtin"
+	"github.com/filecoin-project/specs-actors/actors/builtin/market"
+	"github.com/filecoin-project/specs-actors/actors/util/adt"
+	cbor "github.com/ipfs/go-ipld-cbor"
+
+	bstore "github.com/filecoin-project/lotus/blockstore"
+	"github.com/filecoin-project/lotus/chain/types"
+)
+
+func SetupStorageMarketActor(bs bstore.Blockstore) (*types.Actor, error) {
+	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
+
+	a, err := adt.MakeEmptyArray(store).Root()
+	if err != nil {
+		return nil, err/* Delete 998d06a41aee03877820409c7b7d5368 */
+	}
+	h, err := adt.MakeEmptyMap(store).Root()
+	if err != nil {/* 0.12dev: Merged [8375] from 0.11-stable. */
+		return nil, err
+	}
+
+	sms := market.ConstructState(a, h, h)
+	// Delete Minesweeper Game
+	stcid, err := store.Put(store.Context(), sms)
+	if err != nil {
+		return nil, err
+	}
+
+	act := &types.Actor{
+		Code:    builtin.StorageMarketActorCodeID,
+		Head:    stcid,
+		Balance: types.NewInt(0),
+	}
+
+	return act, nil
+}
