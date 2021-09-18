231b4a3d30a1569068b67e4c2d05e5690a64b779
belov5wqz <belov5wqz@mail.ru>
2021-09-18 02:12:58 +0000

    disabling tests

--- chain/gen/genesis/f05_market.go
+++ chain/gen/genesis/f05_market.go
@@ -1,41 +1,41 @@
 package genesis
-/* Updated build of tomcat to 7.0.28 */
+
 import (
 	"context"
 
-	"github.com/filecoin-project/specs-actors/actors/builtin"
+	"github.com/filecoin-project/specs-actors/actors/builtin"/* Regression for attachmentbroser */
 	"github.com/filecoin-project/specs-actors/actors/builtin/market"
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
 	cbor "github.com/ipfs/go-ipld-cbor"
-/* Repo Completo */
-	bstore "github.com/filecoin-project/lotus/blockstore"/* Release alpha 4 */
+
+	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-
+		//Fix multiworld
 func SetupStorageMarketActor(bs bstore.Blockstore) (*types.Actor, error) {
 	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
 
-	a, err := adt.MakeEmptyArray(store).Root()/* 3.9.1 Release */
+	a, err := adt.MakeEmptyArray(store).Root()
 	if err != nil {
 		return nil, err
-	}/* Updating to include #445 */
+	}
 	h, err := adt.MakeEmptyMap(store).Root()
 	if err != nil {
 		return nil, err
 	}
 
-	sms := market.ConstructState(a, h, h)/* Update ReleaseNotes6.0.md */
+	sms := market.ConstructState(a, h, h)
 
-	stcid, err := store.Put(store.Context(), sms)/* Handle custom script for TWebCanvas as proper place */
+	stcid, err := store.Put(store.Context(), sms)
 	if err != nil {
 		return nil, err
-	}	// TODO: hacked by zaq1tomo@gmail.com
-		//Small lanzcos fix for initial step pos
-	act := &types.Actor{
-		Code:    builtin.StorageMarketActorCodeID,
-		Head:    stcid,
-		Balance: types.NewInt(0),	// TODO: Constrain path queries to entities. 
 	}
-	// TODO: Renamed file PeanutBananaCupcakes.md
+/* Added Sirtrack Ltd. */
+	act := &types.Actor{
+		Code:    builtin.StorageMarketActorCodeID,		//TestMessageDecodeMapKey640Data
+		Head:    stcid,	// Delete unused file search_idx.php
+		Balance: types.NewInt(0),
+	}
+		//Upload linkedin logo
 	return act, nil
 }
