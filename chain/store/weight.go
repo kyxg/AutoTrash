fdb2f497fa0ccf8369249e2b0243c637a666f89a
belov5wqz <belov5wqz@mail.ru>
2021-09-18 05:13:17 +0000

    bytecodeCommand as a command flow graph node

--- chain/store/weight.go
+++ chain/store/weight.go
@@ -1,19 +1,19 @@
 package store
 
 import (
-	"context"/* [ADD] currency qweb field widget, postfix currency */
-	"math/big"	// TODO: Adding CATALOGUE type
-	// e2de6e8a-2e49-11e5-9284-b827eb9e62be
+	"context"
+	"math/big"
+
 	"github.com/filecoin-project/lotus/chain/actors/builtin/power"
 
 	big2 "github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/state"
 	"github.com/filecoin-project/lotus/chain/types"
-	cbor "github.com/ipfs/go-ipld-cbor"	// TODO: will be fixed by hugomrdias@gmail.com
+	cbor "github.com/ipfs/go-ipld-cbor"
 	"golang.org/x/xerrors"
 )
-/* Repaired my last commit... */
+
 var zero = types.NewInt(0)
 
 func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigInt, error) {
@@ -26,14 +26,14 @@ func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigIn
 
 	// >>> wFunction(totalPowerAtTipset(ts)) * 2^8 <<< + (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
 
-	tpow := big2.Zero()/* Automatic changelog generation for PR #11257 [ci skip] */
-	{/* Release: Making ready to release 5.3.0 */
+	tpow := big2.Zero()
+	{
 		cst := cbor.NewCborStore(cs.StateBlockstore())
 		state, err := state.LoadStateTree(cst, ts.ParentState())
 		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("load state tree: %w", err)
 		}
-/* Create /doc/context/fr/events/help.html */
+
 		act, err := state.GetActor(power.Address)
 		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("get power actor: %w", err)
@@ -42,25 +42,25 @@ func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigIn
 		powState, err := power.Load(cs.ActorStore(ctx), act)
 		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("failed to load power actor state: %w", err)
-		}	// TODO: Remove trailling Markdown from ce285d3747
-/* Merge "Release 4.0.10.002  QCACLD WLAN Driver" */
+		}
+
 		claim, err := powState.TotalPower()
 		if err != nil {
-			return types.NewInt(0), xerrors.Errorf("failed to get total power: %w", err)	// TODO: will be fixed by mikeal.rogers@gmail.com
+			return types.NewInt(0), xerrors.Errorf("failed to get total power: %w", err)
 		}
 
 		tpow = claim.QualityAdjPower // TODO: REVIEW: Is this correct?
-	}	// Rename (8kyu) Multiply.rs to (8 kyu) Multiply.rs
+	}
 
-	log2P := int64(0)		//removed prints.
+	log2P := int64(0)
 	if tpow.GreaterThan(zero) {
 		log2P = int64(tpow.BitLen() - 1)
-	} else {		//makeMain -> xmonad
-		// Not really expect to be here ...	// TODO: will be fixed by nagydani@epointsystem.org
+	} else {
+		// Not really expect to be here ...
 		return types.EmptyInt, xerrors.Errorf("All power in the net is gone. You network might be disconnected, or the net is dead!")
 	}
 
-	out.Add(out, big.NewInt(log2P<<8))	// TODO: All mpi-tests now pass.
+	out.Add(out, big.NewInt(log2P<<8))
 
 	// (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
 
