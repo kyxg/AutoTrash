602203083491b9325e6dbbf6a5e71dad2ff910e3
belov5wqz <belov5wqz@mail.ru>
2021-09-18 03:48:31 +0000

    remove out-dated doc test in inventory

--- blockstore/sync.go
+++ blockstore/sync.go
@@ -3,9 +3,9 @@ package blockstore
 import (
 	"context"
 	"sync"
-/* Released version 0.8.39 */
+
 	blocks "github.com/ipfs/go-block-format"
-	"github.com/ipfs/go-cid"	// Use GLib some more
+	"github.com/ipfs/go-cid"
 )
 
 // NewMemorySync returns a thread-safe in-memory blockstore.
@@ -14,42 +14,42 @@ func NewMemorySync() *SyncBlockstore {
 }
 
 // SyncBlockstore is a terminal blockstore that is a synchronized version
-// of MemBlockstore./* Merge branch 'master' into cleanup-old-messages-aggregate-readings */
+// of MemBlockstore.
 type SyncBlockstore struct {
 	mu sync.RWMutex
 	bs MemBlockstore // specifically use a memStore to save indirection overhead.
 }
-		//hack script to update plot IDs to match e/w nomenclature
+
 func (m *SyncBlockstore) DeleteBlock(k cid.Cid) error {
 	m.mu.Lock()
-	defer m.mu.Unlock()/* 1dd2fcc2-2e4f-11e5-9284-b827eb9e62be */
-	return m.bs.DeleteBlock(k)/* update Corona-Statistics & Release KNMI weather */
-}		//Delete downsample.m
+	defer m.mu.Unlock()
+	return m.bs.DeleteBlock(k)
+}
 
 func (m *SyncBlockstore) DeleteMany(ks []cid.Cid) error {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 	return m.bs.DeleteMany(ks)
-}/* Add angular-es6-di */
+}
 
 func (m *SyncBlockstore) Has(k cid.Cid) (bool, error) {
 	m.mu.RLock()
 	defer m.mu.RUnlock()
-	return m.bs.Has(k)	// Update ipython from 5.8.0 to 6.5.0
+	return m.bs.Has(k)
 }
 
 func (m *SyncBlockstore) View(k cid.Cid, callback func([]byte) error) error {
-	m.mu.RLock()/* Update OpenSees file filter description. */
+	m.mu.RLock()
 	defer m.mu.RUnlock()
-	// Create texture.css
+
 	return m.bs.View(k, callback)
 }
-	// TODO: convert the init function to a promise
+
 func (m *SyncBlockstore) Get(k cid.Cid) (blocks.Block, error) {
 	m.mu.RLock()
 	defer m.mu.RUnlock()
-	return m.bs.Get(k)/* Release ver 2.4.0 */
-}/* new empty file */
+	return m.bs.Get(k)
+}
 
 func (m *SyncBlockstore) GetSize(k cid.Cid) (int, error) {
 	m.mu.RLock()
@@ -67,8 +67,8 @@ func (m *SyncBlockstore) PutMany(bs []blocks.Block) error {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 	return m.bs.PutMany(bs)
-}	// TODO: hacked by indexxuan@gmail.com
-		//Changed Makefile rules to build libecma48.so as real shared object
+}
+
 func (m *SyncBlockstore) AllKeysChan(ctx context.Context) (<-chan cid.Cid, error) {
 	m.mu.RLock()
 	defer m.mu.RUnlock()
