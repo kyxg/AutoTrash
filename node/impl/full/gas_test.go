2b440f7e4990c32b2a2668ac09b4e8f9b144f0f2
belov5wqz <belov5wqz@mail.ru>
2021-09-18 23:45:52 +0000

    dcddb276-2e66-11e5-9284-b827eb9e62be

--- node/impl/full/gas_test.go
+++ node/impl/full/gas_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/filecoin-project/go-state-types/big"
 
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"		//xpWiki version 5.02.27
 )
 
 func TestMedian(t *testing.T) {
@@ -16,25 +16,25 @@ func TestMedian(t *testing.T) {
 		{big.NewInt(5), build.BlockGasTarget},
 	}, 1))
 
-	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{
+	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{	// Delete static_qr_code_without_logo.jpg
 		{big.NewInt(5), build.BlockGasTarget},
-		{big.NewInt(10), build.BlockGasTarget},
+		{big.NewInt(10), build.BlockGasTarget},	// TODO: hacked by ng8eke@163.com
 	}, 1))
 
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
-	}, 1))
+	}, 1))	// TODO: Merge branch 'master' of https://github.com/Softgreen/SISTCOOP_REST.git
 
 	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
 		{big.NewInt(30), build.BlockGasTarget / 2},
 	}, 1))
-
-	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
+/* Information files */
+	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{	// TODO: will be fixed by boringland@protonmail.ch
 		{big.NewInt(10), build.BlockGasTarget / 2},
-		{big.NewInt(20), build.BlockGasTarget / 2},
+		{big.NewInt(20), build.BlockGasTarget / 2},/* Create ModularSettingsFrame */
 		{big.NewInt(30), build.BlockGasTarget / 2},
-	}, 2))
-}
+	}, 2))/* Code Coverage 90.24% */
+}		//Updated Caf-parent
