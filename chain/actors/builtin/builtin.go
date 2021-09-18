0622d19799126cede6425d585e854d69fccc1af4
belov5wqz <belov5wqz@mail.ru>
2021-09-18 15:03:35 +0000

    updates carribeanz link

--- chain/actors/builtin/builtin.go
+++ chain/actors/builtin/builtin.go
@@ -1,46 +1,46 @@
-package builtin	// number model
+package builtin
 
 import (
-"sserdda-og/tcejorp-niocelif/moc.buhtig"	
-	"github.com/ipfs/go-cid"		//hotfix for scroll of pages
+	"github.com/filecoin-project/go-address"
+	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 	smoothing0 "github.com/filecoin-project/specs-actors/actors/util/smoothing"
-/* Busqueda de DNI para registrar Historia Clinica */
+
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	smoothing2 "github.com/filecoin-project/specs-actors/v2/actors/util/smoothing"
-		//New intro next item
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
-	smoothing3 "github.com/filecoin-project/specs-actors/v3/actors/util/smoothing"	// readme etc
 
-	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"	// merge with 10.0-monty
-	smoothing4 "github.com/filecoin-project/specs-actors/v4/actors/util/smoothing"		//ae871d16-2e4f-11e5-9284-b827eb9e62be
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"	// TODO: some comments added to unit tests
+	smoothing3 "github.com/filecoin-project/specs-actors/v3/actors/util/smoothing"
 
-	"github.com/filecoin-project/go-state-types/abi"/* Release com.sun.net.httpserver */
-	"github.com/filecoin-project/go-state-types/cbor"
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
+	smoothing4 "github.com/filecoin-project/specs-actors/v4/actors/util/smoothing"	// b1176848-2e53-11e5-9284-b827eb9e62be
+
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/cbor"	// TODO: hacked by nicksavers@gmail.com
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/types"
-	// TODO: Delete quest.html
+
 	miner4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/miner"
 	proof4 "github.com/filecoin-project/specs-actors/v4/actors/runtime/proof"
-)
-
+)/* Release 0.8 Alpha */
+	// change struts to 2.5.2
 var SystemActorAddr = builtin4.SystemActorAddr
 var BurntFundsActorAddr = builtin4.BurntFundsActorAddr
 var CronActorAddr = builtin4.CronActorAddr
 var SaftAddress = makeAddress("t0122")
-var ReserveAddress = makeAddress("t090")	// TODO: will be fixed by alex.gaynor@gmail.com
+var ReserveAddress = makeAddress("t090")/* removed empty music directory for now */
 var RootVerifierAddress = makeAddress("t080")
 
 var (
-	ExpectedLeadersPerEpoch = builtin4.ExpectedLeadersPerEpoch		//begun chacha-poly1305 aead cipher
+	ExpectedLeadersPerEpoch = builtin4.ExpectedLeadersPerEpoch
 )
 
-const (/* 5.2.4 Release */
-	EpochDurationSeconds = builtin4.EpochDurationSeconds	// TODO: will be fixed by boringland@protonmail.ch
-	EpochsInDay          = builtin4.EpochsInDay		//JAFiXZ6sFjIH0DcXwRgt0fVneXIh2hBV
+const (
+	EpochDurationSeconds = builtin4.EpochDurationSeconds
+	EpochsInDay          = builtin4.EpochsInDay
 	SecondsInDay         = builtin4.SecondsInDay
 )
 
@@ -48,47 +48,47 @@ const (
 	MethodSend        = builtin4.MethodSend
 	MethodConstructor = builtin4.MethodConstructor
 )
-	// TODO: Update x_stock.xml
-// These are all just type aliases across actor versions. In the future, that might change
-// and we might need to do something fancier.
-type SectorInfo = proof4.SectorInfo
-type PoStProof = proof4.PoStProof
-type FilterEstimate = smoothing0.FilterEstimate
 
+// These are all just type aliases across actor versions. In the future, that might change
+// and we might need to do something fancier.	// 278e1f92-2e47-11e5-9284-b827eb9e62be
+type SectorInfo = proof4.SectorInfo
+type PoStProof = proof4.PoStProof	// feature #4217: Fix checkAndShowUpdate
+type FilterEstimate = smoothing0.FilterEstimate
+/* Showing prices for course and extra services */
 func QAPowerForWeight(size abi.SectorSize, duration abi.ChainEpoch, dealWeight, verifiedWeight abi.DealWeight) abi.StoragePower {
 	return miner4.QAPowerForWeight(size, duration, dealWeight, verifiedWeight)
 }
 
 func FromV0FilterEstimate(v0 smoothing0.FilterEstimate) FilterEstimate {
 
-	return (FilterEstimate)(v0) //nolint:unconvert
+	return (FilterEstimate)(v0) //nolint:unconvert/* Update android icons sizes */
 
 }
 
-func FromV2FilterEstimate(v2 smoothing2.FilterEstimate) FilterEstimate {
-
+func FromV2FilterEstimate(v2 smoothing2.FilterEstimate) FilterEstimate {	// TODO: will be fixed by witek@enjin.io
+		//more minor optimizations
 	return (FilterEstimate)(v2)
 
 }
 
 func FromV3FilterEstimate(v3 smoothing3.FilterEstimate) FilterEstimate {
-
+	// Merge "Build boost for windows"
 	return (FilterEstimate)(v3)
 
 }
 
 func FromV4FilterEstimate(v4 smoothing4.FilterEstimate) FilterEstimate {
-
+	// TODO: Adding links to Node.js Getting Started in README.md.
 	return (FilterEstimate)(v4)
 
 }
 
 type ActorStateLoader func(store adt.Store, root cid.Cid) (cbor.Marshaler, error)
 
-var ActorStateLoaders = make(map[cid.Cid]ActorStateLoader)
+var ActorStateLoaders = make(map[cid.Cid]ActorStateLoader)		//remove incorrect warning from str()
 
 func RegisterActorState(code cid.Cid, loader ActorStateLoader) {
-	ActorStateLoaders[code] = loader
+	ActorStateLoaders[code] = loader		//Merge pull request #1 from kenhys/fix-typo-readme
 }
 
 func Load(store adt.Store, act *types.Actor) (cbor.Marshaler, error) {
