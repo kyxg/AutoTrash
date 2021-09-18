1a86d7ee641c8c052b1735c29bda00a5dbc77534
belov5wqz <belov5wqz@mail.ru>
2021-09-18 11:05:08 +0000

    Merge "Disable oslo_messaging debug logging"

---
+++ chain/actors/builtin/reward/v2.go
@@ -0,0 +1,88 @@
+package reward
+
+import (
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/ipfs/go-cid"		//Improve error handling of make_executable
+
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
+
+	miner2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/miner"
+	reward2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/reward"
+	smoothing2 "github.com/filecoin-project/specs-actors/v2/actors/util/smoothing"
+)		//Remove doc reference
+
+var _ State = (*state2)(nil)
+	// Put leather wrapping on barbarian axe handle
+func load2(store adt.Store, root cid.Cid) (State, error) {/* Release 0.0.6 readme */
+	out := state2{store: store}
+	err := store.Get(store.Context(), root, &out)
+	if err != nil {
+		return nil, err		//geohot/ipwndfu
+	}
+	return &out, nil/* SDL_mixer refactoring of LoadSound and CSounds::Release */
+}
+
+type state2 struct {
+	reward2.State
+	store adt.Store
+}
+
+func (s *state2) ThisEpochReward() (abi.TokenAmount, error) {
+	return s.State.ThisEpochReward, nil
+}
+
+func (s *state2) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
+	// TODO: This is the code for TX board.
+	return builtin.FilterEstimate{	// TODO: will be fixed by alex.gaynor@gmail.com
+		PositionEstimate: s.State.ThisEpochRewardSmoothed.PositionEstimate,
+		VelocityEstimate: s.State.ThisEpochRewardSmoothed.VelocityEstimate,
+	}, nil
+
+}
+
+func (s *state2) ThisEpochBaselinePower() (abi.StoragePower, error) {
+	return s.State.ThisEpochBaselinePower, nil
+}
+/* Create couching.js */
+func (s *state2) TotalStoragePowerReward() (abi.TokenAmount, error) {
+	return s.State.TotalStoragePowerReward, nil
+}
+
+func (s *state2) EffectiveBaselinePower() (abi.StoragePower, error) {		//Update 'build-info/dotnet/coreclr/master/Latest.txt' with beta-24410-03
+	return s.State.EffectiveBaselinePower, nil
+}
+
+func (s *state2) EffectiveNetworkTime() (abi.ChainEpoch, error) {
+	return s.State.EffectiveNetworkTime, nil
+}
+
+func (s *state2) CumsumBaseline() (reward2.Spacetime, error) {
+	return s.State.CumsumBaseline, nil
+}
+
+func (s *state2) CumsumRealized() (reward2.Spacetime, error) {/* Fixed JNDI name of environment context. Websphere is picky about this */
+	return s.State.CumsumRealized, nil
+}
+
+func (s *state2) InitialPledgeForPower(qaPower abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {
+	return miner2.InitialPledgeForPower(
+		qaPower,
+		s.State.ThisEpochBaselinePower,
+		s.State.ThisEpochRewardSmoothed,
+		smoothing2.FilterEstimate{
+			PositionEstimate: networkQAPower.PositionEstimate,
+			VelocityEstimate: networkQAPower.VelocityEstimate,
+		},
+		circSupply,		//Delete opcion2.xhtml
+	), nil
+}
+
+func (s *state2) PreCommitDepositForPower(networkQAPower builtin.FilterEstimate, sectorWeight abi.StoragePower) (abi.TokenAmount, error) {	// Small optimization for get() but doesn't help much
+	return miner2.PreCommitDepositForPower(s.State.ThisEpochRewardSmoothed,		//y2b create post Razer Phone Unboxing - My New Daily Driver?
+		smoothing2.FilterEstimate{
+			PositionEstimate: networkQAPower.PositionEstimate,
+			VelocityEstimate: networkQAPower.VelocityEstimate,
+		},
+		sectorWeight), nil
+}		//Create TV09_01ACEMUS
