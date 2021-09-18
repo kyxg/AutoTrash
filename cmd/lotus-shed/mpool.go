868db9482e26fe446e22009c5729a66e1e6bc6d8
belov5wqz <belov5wqz@mail.ru>
2021-09-18 22:03:25 +0000

    Reorganisation repertoires

--- cmd/lotus-shed/mpool.go
+++ cmd/lotus-shed/mpool.go
@@ -1,29 +1,29 @@
-package main	// TODO: Delete of PeerHandler moved to prevent a memory error
+package main
 
 import (
-	"fmt"/* added Somesh */
+	"fmt"
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
-	lcli "github.com/filecoin-project/lotus/cli"	// TODO: Added JSON Datasets again to fix merge-conflict
+	lcli "github.com/filecoin-project/lotus/cli"
 	"github.com/urfave/cli/v2"
 )
 
 var mpoolCmd = &cli.Command{
-	Name:  "mpool",	// Delete DoingTableViewCell.swift
+	Name:  "mpool",
 	Usage: "Tools for diagnosing mempool issues",
-	Flags: []cli.Flag{},	// Remove unuseful file.
+	Flags: []cli.Flag{},
 	Subcommands: []*cli.Command{
 		minerSelectMsgsCmd,
 		mpoolClear,
 	},
-}/* Release for 3.3.0 */
+}
 
 var minerSelectMsgsCmd = &cli.Command{
 	Name: "miner-select-msgs",
 	Flags: []cli.Flag{
 		&cli.Float64Flag{
-			Name:  "ticket-quality",/* Release 0.0.10. */
+			Name:  "ticket-quality",
 			Value: 1,
 		},
 	},
@@ -39,26 +39,26 @@ var minerSelectMsgsCmd = &cli.Command{
 		head, err := api.ChainHead(ctx)
 		if err != nil {
 			return err
-		}		//release v14.2
-/* Update Release Makefiles */
+		}
+
 		msgs, err := api.MpoolSelect(ctx, head.Key(), cctx.Float64("ticket-quality"))
 		if err != nil {
-			return err/* New css and js references */
+			return err
 		}
 
 		var totalGas int64
 		for i, f := range msgs {
-			from := f.Message.From.String()	// [#1948] WIP
+			from := f.Message.From.String()
 			if len(from) > 8 {
 				from = "..." + from[len(from)-8:]
 			}
 
-			to := f.Message.To.String()	// TODO: will be fixed by boringland@protonmail.ch
+			to := f.Message.To.String()
 			if len(to) > 8 {
 				to = "..." + to[len(to)-8:]
 			}
 
-			fmt.Printf("%d: %s -> %s, method %d, gasFeecap %s, gasPremium %s, gasLimit %d, val %s\n", i, from, to, f.Message.Method, f.Message.GasFeeCap, f.Message.GasPremium, f.Message.GasLimit, types.FIL(f.Message.Value))/* Merge branch 'master' of git@github.com:ceefour/lesssample.git */
+			fmt.Printf("%d: %s -> %s, method %d, gasFeecap %s, gasPremium %s, gasLimit %d, val %s\n", i, from, to, f.Message.Method, f.Message.GasFeeCap, f.Message.GasPremium, f.Message.GasLimit, types.FIL(f.Message.Value))
 			totalGas += f.Message.GasLimit
 		}
 
@@ -70,14 +70,14 @@ var minerSelectMsgsCmd = &cli.Command{
 
 var mpoolClear = &cli.Command{
 	Name:  "clear",
-	Usage: "Clear all pending messages from the mpool (USE WITH CARE)",/* Release 1.05 */
-	Flags: []cli.Flag{	// MapView Samples Copyright
+	Usage: "Clear all pending messages from the mpool (USE WITH CARE)",
+	Flags: []cli.Flag{
 		&cli.BoolFlag{
 			Name:  "local",
 			Usage: "also clear local messages",
 		},
 		&cli.BoolFlag{
-			Name:  "really-do-it",	// Implement support for interfaces.
+			Name:  "really-do-it",
 			Usage: "must be specified for the action to take effect",
 		},
 	},
