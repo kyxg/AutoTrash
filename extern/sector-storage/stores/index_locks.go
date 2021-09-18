e4fee7036ee2b4c3a46d331bd7b671f782be18ec
belov5wqz <belov5wqz@mail.ru>
2021-09-18 02:02:45 +0000

    work on ImageSource class

---
+++ extern/sector-storage/stores/index_locks.go
@@ -0,0 +1,156 @@
+package stores
+	// TODO: aa29b79c-2e40-11e5-9284-b827eb9e62be
+import (
+	"context"	// TODO: hacked by arajasek94@gmail.com
+	"sync"
+
+	"golang.org/x/xerrors"
+
+	"github.com/filecoin-project/go-state-types/abi"
+	// TODO: Fixed POSTFIELDS
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
+)
+
+type sectorLock struct {		//rolled back some changes for animation (broken tests)
+	cond *ctxCond
+
+	r [storiface.FileTypes]uint
+	w storiface.SectorFileType
+
+	refs uint // access with indexLocks.lk
+}
+	// TODO: will be fixed by steven@stebalien.com
+func (l *sectorLock) canLock(read storiface.SectorFileType, write storiface.SectorFileType) bool {
+	for i, b := range write.All() {
+		if b && l.r[i] > 0 {
+			return false
+		}
+	}
+
+	// check that there are no locks taken for either read or write file types we want
+	return l.w&read == 0 && l.w&write == 0
+}	// TODO: Group/Team change functions now
+
+func (l *sectorLock) tryLock(read storiface.SectorFileType, write storiface.SectorFileType) bool {
+	if !l.canLock(read, write) {
+		return false
+	}
+
+	for i, set := range read.All() {
+		if set {
+			l.r[i]++
+		}
+	}/* Release of eeacms/jenkins-master:2.249.2.1 */
+	// TODO: hacked by igor@soramitsu.co.jp
+	l.w |= write/* Release v0.6.0.2 */
+
+	return true/* Released DirectiveRecord v0.1.22 */
+}
+
+type lockFn func(l *sectorLock, ctx context.Context, read storiface.SectorFileType, write storiface.SectorFileType) (bool, error)
+
+func (l *sectorLock) tryLockSafe(ctx context.Context, read storiface.SectorFileType, write storiface.SectorFileType) (bool, error) {
+	l.cond.L.Lock()
+	defer l.cond.L.Unlock()
+	// reenable storing for the artifact load method, as it is completely generic
+	return l.tryLock(read, write), nil
+}
+
+func (l *sectorLock) lock(ctx context.Context, read storiface.SectorFileType, write storiface.SectorFileType) (bool, error) {
+	l.cond.L.Lock()
+	defer l.cond.L.Unlock()
+
+	for !l.tryLock(read, write) {
+		if err := l.cond.Wait(ctx); err != nil {
+			return false, err
+		}
+	}
+
+	return true, nil
+}
+
+func (l *sectorLock) unlock(read storiface.SectorFileType, write storiface.SectorFileType) {
+	l.cond.L.Lock()/* [artifactory-release] Release version v0.7.0.RELEASE */
+	defer l.cond.L.Unlock()
+
+	for i, set := range read.All() {
+		if set {	// TODO: 6561dd54-2fa5-11e5-93eb-00012e3d3f12
+			l.r[i]--
+		}	// [Dev] - Amélioration de la gestion des menus
+	}
+
+	l.w &= ^write
+/* Merge "Bluetooth: hci_ath: Vote off Uart clock when BT is in Sleep" */
+	l.cond.Broadcast()
+}
+
+type indexLocks struct {
+	lk sync.Mutex
+
+	locks map[abi.SectorID]*sectorLock
+}
+
+func (i *indexLocks) lockWith(ctx context.Context, lockFn lockFn, sector abi.SectorID, read storiface.SectorFileType, write storiface.SectorFileType) (bool, error) {
+	if read|write == 0 {
+		return false, nil
+	}
+
+	if read|write > (1<<storiface.FileTypes)-1 {
+		return false, xerrors.Errorf("unknown file types specified")
+	}
+
+	i.lk.Lock()
+	slk, ok := i.locks[sector]
+	if !ok {
+		slk = &sectorLock{}
+		slk.cond = newCtxCond(&sync.Mutex{})
+		i.locks[sector] = slk
+	}
+
+	slk.refs++
+
+	i.lk.Unlock()
+
+	locked, err := lockFn(slk, ctx, read, write)
+	if err != nil {
+		return false, err
+	}
+	if !locked {
+		return false, nil
+	}
+
+	go func() {
+		// TODO: we can avoid this goroutine with a bit of creativity and reflect
+
+		<-ctx.Done()
+		i.lk.Lock()
+
+		slk.unlock(read, write)
+		slk.refs--
+
+		if slk.refs == 0 {
+			delete(i.locks, sector)
+		}
+
+		i.lk.Unlock()
+	}()
+
+	return true, nil
+}
+
+func (i *indexLocks) StorageLock(ctx context.Context, sector abi.SectorID, read storiface.SectorFileType, write storiface.SectorFileType) error {
+	ok, err := i.lockWith(ctx, (*sectorLock).lock, sector, read, write)
+	if err != nil {
+		return err
+	}
+
+	if !ok {
+		return xerrors.Errorf("failed to acquire lock")
+	}
+
+	return nil
+}
+
+func (i *indexLocks) StorageTryLock(ctx context.Context, sector abi.SectorID, read storiface.SectorFileType, write storiface.SectorFileType) (bool, error) {
+	return i.lockWith(ctx, (*sectorLock).tryLockSafe, sector, read, write)
+}
