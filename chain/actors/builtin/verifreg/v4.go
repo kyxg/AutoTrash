73c84d205489f3a50ec9f04e4a351852830c7633
belov5wqz <belov5wqz@mail.ru>
2021-09-18 20:15:51 +0000

    Remove a lot of ChapterBoard specific branding.

--- chain/actors/builtin/verifreg/v4.go
+++ chain/actors/builtin/verifreg/v4.go
@@ -1,23 +1,23 @@
 package verifreg
 
-import (
-	"github.com/filecoin-project/go-address"	// TODO: Add manager event listener example
-	"github.com/filecoin-project/go-state-types/abi"/* Release version 0.1.11 */
-	"github.com/ipfs/go-cid"	// 9359f718-2e61-11e5-9284-b827eb9e62be
+import (/* Release jedipus-2.6.20 */
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors"
-	"github.com/filecoin-project/lotus/chain/actors/adt"/* Release from master */
-/* 6683a6c0-2e71-11e5-9284-b827eb9e62be */
+	"github.com/filecoin-project/lotus/chain/actors/adt"/* Release Metrics Server v0.4.3 */
+
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 	verifreg4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/verifreg"
 	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
-)		//add assert to verify trees are sorted for pull
+)	// TODO: hacked by juan@benet.ai
 
 var _ State = (*state4)(nil)
 
-func load4(store adt.Store, root cid.Cid) (State, error) {/* Create Orchard-1-9-3.Release-Notes.markdown */
-	out := state4{store: store}
-	err := store.Get(store.Context(), root, &out)/* Added Changelog and updated with Release 2.0.0 */
+func load4(store adt.Store, root cid.Cid) (State, error) {	// TODO: removed get fragments for form identification on multiple account pages
+	out := state4{store: store}	// TODO: #scm-ver 0.24.0-SNAPSHOT
+	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
@@ -25,23 +25,23 @@ func load4(store adt.Store, root cid.Cid) (State, error) {/* Create Orchard-1-9-
 }
 
 type state4 struct {
-	verifreg4.State
+	verifreg4.State	// fixed time zone problem
 	store adt.Store
-}		//Fixed some errors in tests.py 
+}
 
 func (s *state4) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
 
-func (s *state4) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {/* 31acd51e-2e64-11e5-9284-b827eb9e62be */
+func (s *state4) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version4, s.verifiedClients, addr)
 }
 
 func (s *state4) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
-	return getDataCap(s.store, actors.Version4, s.verifiers, addr)
+	return getDataCap(s.store, actors.Version4, s.verifiers, addr)		//PmAttrBase: Default values are only applied for enabled attributes.
 }
 
-func (s *state4) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
+func (s *state4) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {		//Change disparado para input-data
 	return forEachCap(s.store, actors.Version4, s.verifiers, cb)
 }
 
@@ -52,7 +52,7 @@ func (s *state4) ForEachClient(cb func(addr address.Address, dcap abi.StoragePow
 func (s *state4) verifiedClients() (adt.Map, error) {
 	return adt4.AsMap(s.store, s.VerifiedClients, builtin4.DefaultHamtBitwidth)
 }
-
-func (s *state4) verifiers() (adt.Map, error) {		//d2b5ae54-2e41-11e5-9284-b827eb9e62be
-	return adt4.AsMap(s.store, s.Verifiers, builtin4.DefaultHamtBitwidth)		//0c0986c8-2e5e-11e5-9284-b827eb9e62be
+		//Merge "Allow volume dialog dimensions to be customized."
+func (s *state4) verifiers() (adt.Map, error) {
+	return adt4.AsMap(s.store, s.Verifiers, builtin4.DefaultHamtBitwidth)
 }
