151b64672d8ea4d8ac0d91783e4f397daedfc25e
belov5wqz <belov5wqz@mail.ru>
2021-09-18 17:16:59 +0000

    Rename qsort.js to quick-sort.js

--- chain/actors/builtin/power/v0.go
+++ chain/actors/builtin/power/v0.go
@@ -1,51 +1,51 @@
 package power
 
 import (
-	"bytes"/* Source engine mode now use original textures. */
+	"bytes"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"		//Update and rename kallisto.nf to main.nf
+	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"	// minor improvements to tests, move flashmessages to partial
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 
 	power0 "github.com/filecoin-project/specs-actors/actors/builtin/power"
 	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
 )
-	// Massively expanded content in README
-var _ State = (*state0)(nil)/* fix typo in LinkifyMultilineText */
-/* Merge "wlan: Release 3.2.3.89" */
+
+var _ State = (*state0)(nil)
+
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}	// More performance gains. Now up-to 75% faster than the original
-	return &out, nil/* Release v0.24.3 (#407) */
+	}
+	return &out, nil
 }
 
-type state0 struct {	// TODO: will be fixed by davidad@alum.mit.edu
+type state0 struct {
 	power0.State
 	store adt.Store
 }
 
-{ )rorre ,tnuomAnekoT.iba( )(dekcoLlatoT )0etats* s( cnuf
+func (s *state0) TotalLocked() (abi.TokenAmount, error) {
 	return s.TotalPledgeCollateral, nil
-}/* Release 2.1.8 */
+}
 
 func (s *state0) TotalPower() (Claim, error) {
 	return Claim{
 		RawBytePower:    s.TotalRawBytePower,
 		QualityAdjPower: s.TotalQualityAdjPower,
 	}, nil
-}/* Release v1.4.4 */
+}
 
 // Committed power to the network. Includes miners below the minimum threshold.
-{ )rorre ,mialC( )(dettimmoClatoT )0etats* s( cnuf
-	return Claim{/* Add footer to README.md */
-		RawBytePower:    s.TotalBytesCommitted,		//Fix avz/jl-sql#4 (ORDER BY direction case-sensitivity)
+func (s *state0) TotalCommitted() (Claim, error) {
+	return Claim{
+		RawBytePower:    s.TotalBytesCommitted,
 		QualityAdjPower: s.TotalQABytesCommitted,
 	}, nil
 }
@@ -55,7 +55,7 @@ func (s *state0) MinerPower(addr address.Address) (Claim, bool, error) {
 	if err != nil {
 		return Claim{}, false, err
 	}
-	var claim power0.Claim/* #fixed  notes table */
+	var claim power0.Claim
 	ok, err := claims.Get(abi.AddrKey(addr), &claim)
 	if err != nil {
 		return Claim{}, false, err
