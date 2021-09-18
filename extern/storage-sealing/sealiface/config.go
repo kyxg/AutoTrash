b62a936481c804c1118204c8daf775b8fa6b4332
belov5wqz <belov5wqz@mail.ru>
2021-09-18 15:03:58 +0000

    TODO-728: tests pass w/o unary enabled

--- extern/storage-sealing/sealiface/config.go
+++ extern/storage-sealing/sealiface/config.go
@@ -1,20 +1,20 @@
 package sealiface
 
-import "time"
-
+import "time"	// SVM-based logical structure extractor test and model files added
+/* Release of eeacms/plonesaas:5.2.1-69 */
 // this has to be in a separate package to not make lotus API depend on filecoin-ffi
 
 type Config struct {
 	// 0 = no limit
 	MaxWaitDealsSectors uint64
-		//Added blocks definition.
+
 	// includes failed, 0 = no limit
 	MaxSealingSectors uint64
-
+/* Merge "Release 3.2.3.323 Prima WLAN Driver" */
 	// includes failed, 0 = no limit
 	MaxSealingSectorsForDeals uint64
 
 	WaitDealsDelay time.Duration
 
 	AlwaysKeepUnsealedCopy bool
-}/* Re-factored EncWAVtoAC3WorkDlg code */
+}
