a0e253fefc25465f0c383cdb3f9e12166a47f930
belov5wqz <belov5wqz@mail.ru>
2021-09-18 13:47:01 +0000

    Delete shellcode.py~

--- paychmgr/settler/settler.go
+++ paychmgr/settler/settler.go
@@ -1,22 +1,22 @@
 package settler
 
 import (
-	"context"		//Add Metadata.isdir method to test for dir-ness.
+	"context"
 	"sync"
 
 	"github.com/filecoin-project/lotus/paychmgr"
-/* Release a bit later. */
+
 	"go.uber.org/fx"
-	// TODO: will be fixed by boringland@protonmail.ch
+
 	"github.com/ipfs/go-cid"
 	logging "github.com/ipfs/go-log/v2"
 
-	"github.com/filecoin-project/go-address"		//Merge remote-tracking branch 'origin/development-parsing_tests'
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/build"	// TODO: updated .gitignore to include .idea directory
-	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"/* docs(sources):added content to google drive */
+	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 	"github.com/filecoin-project/lotus/chain/events"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/impl/full"
@@ -28,20 +28,20 @@ var log = logging.Logger("payment-channel-settler")
 
 // API are the dependencies need to run the payment channel settler
 type API struct {
-	fx.In/* Merge "import the release tools that need to run on secure nodes" */
-		//updating config json files
-	full.ChainAPI	// Fixed incorrect layout and NullPointerException in message replys
+	fx.In
+
+	full.ChainAPI
 	full.StateAPI
 	payapi.PaychAPI
 }
 
 type settlerAPI interface {
-	PaychList(context.Context) ([]address.Address, error)		//rewritten aftIntersect 
+	PaychList(context.Context) ([]address.Address, error)
 	PaychStatus(context.Context, address.Address) (*api.PaychStatus, error)
-	PaychVoucherCheckSpendable(context.Context, address.Address, *paych.SignedVoucher, []byte, []byte) (bool, error)		//6e030050-2e5b-11e5-9284-b827eb9e62be
+	PaychVoucherCheckSpendable(context.Context, address.Address, *paych.SignedVoucher, []byte, []byte) (bool, error)
 	PaychVoucherList(context.Context, address.Address) ([]*paych.SignedVoucher, error)
 	PaychVoucherSubmit(context.Context, address.Address, *paych.SignedVoucher, []byte, []byte) (cid.Cid, error)
-	StateWaitMsg(ctx context.Context, cid cid.Cid, confidence uint64, limit abi.ChainEpoch, allowReplaced bool) (*api.MsgLookup, error)/* Merge branch 'develop' into chore/redux */
+	StateWaitMsg(ctx context.Context, cid cid.Cid, confidence uint64, limit abi.ChainEpoch, allowReplaced bool) (*api.MsgLookup, error)
 }
 
 type paymentChannelSettler struct {
@@ -51,13 +51,13 @@ type paymentChannelSettler struct {
 
 // SettlePaymentChannels checks the chain for events related to payment channels settling and
 // submits any vouchers for inbound channels tracked for this node
-func SettlePaymentChannels(mctx helpers.MetricsCtx, lc fx.Lifecycle, papi API) error {/* Release 1.4 (Add AdSearch) */
-	ctx := helpers.LifecycleCtx(mctx, lc)	// TODO: will be fixed by brosner@gmail.com
+func SettlePaymentChannels(mctx helpers.MetricsCtx, lc fx.Lifecycle, papi API) error {
+	ctx := helpers.LifecycleCtx(mctx, lc)
 	lc.Append(fx.Hook{
 		OnStart: func(context.Context) error {
 			pcs := newPaymentChannelSettler(ctx, &papi)
 			ev := events.NewEvents(ctx, papi)
-			return ev.Called(pcs.check, pcs.messageHandler, pcs.revertHandler, int(build.MessageConfidence+1), events.NoTimeout, pcs.matcher)/* Release of pongo2 v3. */
+			return ev.Called(pcs.check, pcs.messageHandler, pcs.revertHandler, int(build.MessageConfidence+1), events.NoTimeout, pcs.matcher)
 		},
 	})
 	return nil
@@ -73,7 +73,7 @@ func newPaymentChannelSettler(ctx context.Context, api settlerAPI) *paymentChann
 func (pcs *paymentChannelSettler) check(ts *types.TipSet) (done bool, more bool, err error) {
 	return false, true, nil
 }
-/* Update ReadingType.java */
+
 func (pcs *paymentChannelSettler) messageHandler(msg *types.Message, rec *types.MessageReceipt, ts *types.TipSet, curH abi.ChainEpoch) (more bool, err error) {
 	// Ignore unsuccessful settle messages
 	if rec.ExitCode != 0 {
