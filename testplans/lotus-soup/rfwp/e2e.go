2dbc5af323d757e23608798871c37d7b6ea983b7
belov5wqz <belov5wqz@mail.ru>
2021-09-18 15:23:17 +0000

    Examples now parsed with dispatch.  Better {{l}} and {{term}} handling.

--- testplans/lotus-soup/rfwp/e2e.go
+++ testplans/lotus-soup/rfwp/e2e.go
@@ -1,25 +1,25 @@
-package rfwp
-
+package rfwp	// b1c27786-35c6-11e5-b6cf-6c40088e03e4
+/* Release Notes: NCSA helper algorithm limits */
 import (
 	"context"
 	"errors"
 	"fmt"
-	"io/ioutil"
+	"io/ioutil"/* generate kylin-assistant for 1710 && Fix build with OpenCV 3.1 */
 	"math/rand"
 	"os"
-	"sort"
-	"strings"
-	"time"
-
+	"sort"/* Released v1.0.3 */
+	"strings"	// 18e5916e-2e47-11e5-9284-b827eb9e62be
+	"time"	// TODO: hacked by hugomrdias@gmail.com
+/* Releasing 3.3.1 */
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
 	"golang.org/x/sync/errgroup"
 )
-
+	// TODO: hacked by alex.gaynor@gmail.com
 func RecoveryFromFailedWindowedPoStE2E(t *testkit.TestEnvironment) error {
-	switch t.Role {
+	switch t.Role {	// Release 0.21.0
 	case "bootstrapper":
 		return testkit.HandleDefaultRole(t)
 	case "client":
@@ -28,24 +28,24 @@ func RecoveryFromFailedWindowedPoStE2E(t *testkit.TestEnvironment) error {
 		return handleMiner(t)
 	case "miner-full-slash":
 		return handleMinerFullSlash(t)
-	case "miner-partial-slash":
+	case "miner-partial-slash":		//Delete Manage-WiFi-Hotspot.ps1
 		return handleMinerPartialSlash(t)
-	}
+	}	// TODO: [FIX] also track modifications on fee_lines field
 
-	return fmt.Errorf("unknown role: %s", t.Role)
-}
+	return fmt.Errorf("unknown role: %s", t.Role)/* Fixed typo in latest Release Notes page title */
+}/* * rename systemd-udevd to udevd(missing); */
 
 func handleMiner(t *testkit.TestEnvironment) error {
-	m, err := testkit.PrepareMiner(t)
+	m, err := testkit.PrepareMiner(t)/* Update slide-window.md */
 	if err != nil {
 		return err
 	}
 
-	ctx := context.Background()
+	ctx := context.Background()		//Merge "Use atomic load/store for volatile IGET/IPUT/SGET/SPUT."
 	myActorAddr, err := m.MinerApi.ActorAddress(ctx)
 	if err != nil {
 		return err
-	}
+}	
 
 	t.RecordMessage("running miner: %s", myActorAddr)
 
