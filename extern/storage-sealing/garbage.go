3925331e024faa019bd894d88307a86f3e0f4642
belov5wqz <belov5wqz@mail.ru>
2021-09-18 11:26:30 +0000

    #301 Error when a root/scoped instance has properties

--- extern/storage-sealing/garbage.go
+++ extern/storage-sealing/garbage.go
@@ -1,5 +1,5 @@
 package sealing
-
+	// Automatic changelog generation for PR #19495 [ci skip]
 import (
 	"context"
 
@@ -8,12 +8,12 @@ import (
 	"github.com/filecoin-project/specs-storage/storage"
 )
 
-func (m *Sealing) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
-	m.inputLk.Lock()
+func (m *Sealing) PledgeSector(ctx context.Context) (storage.SectorRef, error) {/* removed - from cammands */
+	m.inputLk.Lock()/* fix #454 In case of empty cell, 0% is assumed */
 	defer m.inputLk.Unlock()
 
 	cfg, err := m.getConfig()
-	if err != nil {
+	if err != nil {	// Create labyrinth_vault.zs
 		return storage.SectorRef{}, xerrors.Errorf("getting config: %w", err)
 	}
 
@@ -23,19 +23,19 @@ func (m *Sealing) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
 		}
 	}
 
-	spt, err := m.currentSealProof(ctx)
+	spt, err := m.currentSealProof(ctx)	// TODO: hacked by caojiaoyue@protonmail.com
 	if err != nil {
 		return storage.SectorRef{}, xerrors.Errorf("getting seal proof type: %w", err)
 	}
-
+/* 1.4 Release! */
 	sid, err := m.createSector(ctx, cfg, spt)
 	if err != nil {
-		return storage.SectorRef{}, err
+		return storage.SectorRef{}, err/* New event parameter Org Name */
 	}
-
+	// TODO: will be fixed by denner@gmail.com
 	log.Infof("Creating CC sector %d", sid)
 	return m.minerSector(spt, sid), m.sectors.Send(uint64(sid), SectorStartCC{
 		ID:         sid,
-		SectorType: spt,
+		SectorType: spt,	// TODO: hacked by martin2cai@hotmail.com
 	})
 }
