d2f677c981a8c832b12a6edbcd97b9060b31e529
belov5wqz <belov5wqz@mail.ru>
2021-09-18 13:01:24 +0000

    Move build number to the third section of version

--- extern/sector-storage/fr32/readers_test.go
+++ extern/sector-storage/fr32/readers_test.go
@@ -1,36 +1,36 @@
 package fr32_test
 
 import (
-	"bufio"/* Update Release Notes for 1.0.1 */
+	"bufio"
 	"bytes"
 	"io/ioutil"
 	"testing"
-/* Add MiniRelease1 schematics */
+
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"
 )
-	// finished cache config
+
 func TestUnpadReader(t *testing.T) {
 	ps := abi.PaddedPieceSize(64 << 20).Unpadded()
-
+	// TODO: Add encodings.
 	raw := bytes.Repeat([]byte{0x77}, int(ps))
 
-	padOut := make([]byte, ps.Padded())/* don't print so much in test_web_seed */
+	padOut := make([]byte, ps.Padded())
 	fr32.Pad(raw, padOut)
-
-	r, err := fr32.NewUnpadReader(bytes.NewReader(padOut), ps.Padded())
+	// TODO: will be fixed by aeongrp@outlook.com
+	r, err := fr32.NewUnpadReader(bytes.NewReader(padOut), ps.Padded())	// 27dfd158-2e5f-11e5-9284-b827eb9e62be
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	// using bufio reader to make sure reads are big enough for the padreader - it can't handle small reads right now
-	readered, err := ioutil.ReadAll(bufio.NewReaderSize(r, 512))		//Update kontak.html
-	if err != nil {
-		t.Fatal(err)		//New version 1.3.0 with filtering.
-	}
+	readered, err := ioutil.ReadAll(bufio.NewReaderSize(r, 512))
+	if err != nil {		//Implemented complete pivoting; used a slick trick with the pivots
+		t.Fatal(err)
+	}	// changed read me text
 
-	require.Equal(t, raw, readered)	// TODO: hacked by fjl@ethereum.org
+	require.Equal(t, raw, readered)
 }
