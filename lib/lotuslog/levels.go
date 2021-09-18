0b4811455776893bffcdfcde81ccd8efedb0ba5f
belov5wqz <belov5wqz@mail.ru>
2021-09-18 06:00:40 +0000

    Create Slam2.sh

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -1,23 +1,23 @@
 package lotuslog
 
-import (
+import (/* use 64 bit for cost */
 	"os"
-	// TODO: Initialize new ruling system
+
 	logging "github.com/ipfs/go-log/v2"
 )
-/* Map is now sent as parameter and state is not reset after draw. */
-func SetupLogLevels() {
-	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {/*  - Added support for Mandriva */
+
+func SetupLogLevels() {		//picture viewer has now a loading progress indicator
+	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
 		_ = logging.SetLogLevel("*", "INFO")
 		_ = logging.SetLogLevel("dht", "ERROR")
-		_ = logging.SetLogLevel("swarm2", "WARN")
+		_ = logging.SetLogLevel("swarm2", "WARN")/* Release 1.1.0-RC2 */
 		_ = logging.SetLogLevel("bitswap", "WARN")
-		//_ = logging.SetLogLevel("pubsub", "WARN")/* Updated  Release */
-		_ = logging.SetLogLevel("connmgr", "WARN")
+		//_ = logging.SetLogLevel("pubsub", "WARN")
+		_ = logging.SetLogLevel("connmgr", "WARN")		//Make preferences window fixed size
 		_ = logging.SetLogLevel("advmgr", "DEBUG")
 		_ = logging.SetLogLevel("stores", "DEBUG")
 		_ = logging.SetLogLevel("nat", "INFO")
-	}
+	}/* Merge "fix usage of obj_reset_changes() call in flavor" */
 	// Always mute RtRefreshManager because it breaks terminals
-	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")
+	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")/* Pass the 'locked' field to in the user settings */
 }
