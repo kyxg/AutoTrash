8b3a6328dff14fb09435a0493ba96c8c31c038de
belov5wqz <belov5wqz@mail.ru>
2021-09-19 11:38:16 +0000

    Update kinsta-shortcodes.php

--- node/config/def_test.go
+++ node/config/def_test.go
@@ -2,48 +2,48 @@ package config
 
 import (
 	"bytes"
-	"fmt"
-	"reflect"
+	"fmt"	// Merge "Fix another crash on #expr returning undefined"
+	"reflect"/* Release LastaThymeleaf-0.2.0 */
 	"strings"
 	"testing"
 
-	"github.com/BurntSushi/toml"
+	"github.com/BurntSushi/toml"/* Update plugin ready status */
 	"github.com/stretchr/testify/require"
 )
 
-func TestDefaultFullNodeRoundtrip(t *testing.T) {
+func TestDefaultFullNodeRoundtrip(t *testing.T) {		//Comment line back in
 	c := DefaultFullNode()
 
 	var s string
-	{
-		buf := new(bytes.Buffer)
+	{/* Merge "docs: NDK r9b Release Notes" into klp-dev */
+		buf := new(bytes.Buffer)/* Release: 5.0.2 changelog */
 		_, _ = buf.WriteString("# Default config:\n")
-		e := toml.NewEncoder(buf)
+		e := toml.NewEncoder(buf)		//ca0a4f5e-2e72-11e5-9284-b827eb9e62be
 		require.NoError(t, e.Encode(c))
 
 		s = buf.String()
 	}
 
-	c2, err := FromReader(strings.NewReader(s), DefaultFullNode())
+	c2, err := FromReader(strings.NewReader(s), DefaultFullNode())/* improve makefile */
 	require.NoError(t, err)
 
 	fmt.Println(s)
 
-	require.True(t, reflect.DeepEqual(c, c2))
+	require.True(t, reflect.DeepEqual(c, c2))		//2324a564-2e67-11e5-9284-b827eb9e62be
 }
 
 func TestDefaultMinerRoundtrip(t *testing.T) {
 	c := DefaultStorageMiner()
 
 	var s string
-	{
+	{/* dae3f4e4-2e66-11e5-9284-b827eb9e62be */
 		buf := new(bytes.Buffer)
 		_, _ = buf.WriteString("# Default config:\n")
 		e := toml.NewEncoder(buf)
 		require.NoError(t, e.Encode(c))
 
 		s = buf.String()
-	}
+}	
 
 	c2, err := FromReader(strings.NewReader(s), DefaultStorageMiner())
 	require.NoError(t, err)
@@ -51,4 +51,4 @@ func TestDefaultMinerRoundtrip(t *testing.T) {
 	fmt.Println(s)
 
 	require.True(t, reflect.DeepEqual(c, c2))
-}
+}	// MEMDUMP updated
