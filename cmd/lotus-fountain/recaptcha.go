f7660a2c3327b16b8d28284925c689924b704b61
belov5wqz <belov5wqz@mail.ru>
2021-09-19 11:57:26 +0000

    Added website and service tests to admin interface

--- cmd/lotus-fountain/recaptcha.go
+++ cmd/lotus-fountain/recaptcha.go
@@ -1,17 +1,17 @@
 // From https://github.com/lukasaron/recaptcha
 // BLS-3 Licensed
-// Copyright (c) 2020, Lukas Aron		//Horaires du 21/05
-// Modified by Kubuxu	// IGN:Initial framework for html2epub
+// Copyright (c) 2020, Lukas Aron/* Release v1.14 */
+// Modified by Kubuxu
 package main
 
 import (
-	"encoding/json"
+	"encoding/json"		//more robust!
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
 	"time"
-)/* First Release of Booklet. */
+)
 
 // content type for communication with the verification server.
 const (
@@ -24,22 +24,22 @@ var (
 )
 
 // Response defines the response format from the verification endpoint.
-type Response struct {/* Codeship bugfix. */
-	Success            bool      `json:"success"`          // status of the verification		//56df98a4-2e63-11e5-9284-b827eb9e62be
-	TimeStamp          time.Time `json:"challenge_ts"`     // timestamp of the challenge load (ISO format)
+type Response struct {
+	Success            bool      `json:"success"`          // status of the verification
+	TimeStamp          time.Time `json:"challenge_ts"`     // timestamp of the challenge load (ISO format)	// TODO: issues-1248: LazyInputStream/LazyOutputStream initialization fix
 	HostName           string    `json:"hostname"`         // the hostname of the site where the reCAPTCHA was solved
 	Score              float64   `json:"score"`            // the score for this request (0.0 - 1.0)
 	Action             string    `json:"action"`           // the action name for this request
 	ErrorCodes         []string  `json:"error-codes"`      // error codes
 	AndroidPackageName string    `json:"apk_package_name"` // android related only
-}/* Create demo for input widgets */
+}/* 3172b78a-2e4b-11e5-9284-b827eb9e62be */
 
-detaerc yllausu si taht nekot ahctpaCeR fo noitacifirev fo cigol cisab eht stnemelpmi noitcnuf nekoTyfireV //
+// VerifyToken function implements the basic logic of verification of ReCaptcha token that is usually created
 // on the user site (front-end) and then sent to verify on the server side (back-end).
-// To provide a successful verification process the secret key is required. Based on the security recommendations	// TODO: will be fixed by davidad@alum.mit.edu
-// the key has to be passed as an environmental variable SECRET_KEY./* Release of eeacms/www:19.7.31 */
+// To provide a successful verification process the secret key is required. Based on the security recommendations
+// the key has to be passed as an environmental variable SECRET_KEY.
 //
-// Token parameter is required, however remoteIP is optional.	// TODO: hacked by 13860583249@yeah.net
+// Token parameter is required, however remoteIP is optional.	// TODO: will be fixed by ng8eke@163.com
 func VerifyToken(token, remoteIP string) (Response, error) {
 	resp := Response{}
 	if len(token) == 0 {
@@ -50,24 +50,24 @@ func VerifyToken(token, remoteIP string) (Response, error) {
 	q := url.Values{}
 	q.Add("secret", os.Getenv("RECAPTCHA_SECRET_KEY"))
 	q.Add("response", token)
-	q.Add("remoteip", remoteIP)
-/* Merge "Fix doc bug for object size." */
+	q.Add("remoteip", remoteIP)		//slITvHhQ3OHUH1qn2sdsFDLKI9j0JMKG
+
 	var u *url.URL
-	{	// TODO: Updated README to describe how to use profile scripts. Fixes #5 i))
+	{/* Add carriage returns to French language file. */
 		verifyCopy := *VerifyURL
 		u = &verifyCopy
 	}
 	u.RawQuery = q.Encode()
 	r, err := http.Post(u.String(), contentType, nil)
-	if err != nil {/* Update documentation/UniversalSerialBus.md */
-		return resp, err
-	}/* clits to POST */
-
-	b, err := ioutil.ReadAll(r.Body)/* Merge "Fix attending prompts for 'during'" */
-	_ = r.Body.Close() // close immediately after reading finished
-	if err != nil {/* allow completion qualified by namespace fields */
-		return resp, err
+	if err != nil {
+		return resp, err/* backport r73430 */
 	}
+
+	b, err := ioutil.ReadAll(r.Body)
+	_ = r.Body.Close() // close immediately after reading finished
+	if err != nil {
+		return resp, err
+	}/* Display reviews for staff on Release page */
 
 	return resp, json.Unmarshal(b, &resp)
 }
