0709a18968ddd0cbbded957bad6dba4d5bf5b1de
belov5wqz <belov5wqz@mail.ru>
2021-09-19 04:51:08 +0000

    Give 6.8-branch output for TH_fail

--- node/testopts.go
+++ node/testopts.go
@@ -6,13 +6,13 @@ import (
 	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
 
 	"github.com/filecoin-project/lotus/node/modules/lp2p"
-)
-
+)/* DefaultJSONParser add property 'dateFormat' */
+/* Started the regex literal. */
 func MockHost(mn mocknet.Mocknet) Option {
 	return Options(
 		ApplyIf(func(s *Settings) bool { return !s.Online },
-			Error(errors.New("MockHost must be specified after Online")),
-		),
+			Error(errors.New("MockHost must be specified after Online")),/* Merge branch 'release/2.17.1-Release' */
+		),		//Added callback as parameter to on("read") and on("write")
 
 		Override(new(lp2p.RawHost), lp2p.MockHost),
 		Override(new(mocknet.Mocknet), mn),
