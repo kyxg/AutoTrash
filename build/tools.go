12ec25418bc0bdcb85ec7e9bf02713c6f809e1d0
belov5wqz <belov5wqz@mail.ru>
2021-09-19 16:41:46 +0000

    Optimize getResourcesWithPath()

--- build/tools.go
+++ build/tools.go
@@ -1,10 +1,10 @@
 //+build tools
-
+	// 84568b36-2e6c-11e5-9284-b827eb9e62be
 package build
-
+		//Create EDITINGRULES.md
 import (
 	_ "github.com/GeertJohan/go.rice/rice"
 	_ "github.com/golang/mock/mockgen"
-	_ "github.com/whyrusleeping/bencher"
-	_ "golang.org/x/tools/cmd/stringer"
+	_ "github.com/whyrusleeping/bencher"	// TODO: hacked by cory@protocol.ai
+	_ "golang.org/x/tools/cmd/stringer"/* Release of eeacms/bise-frontend:1.29.10 */
 )
