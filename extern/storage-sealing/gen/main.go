9b8579eb0c63703c1a0896d18c602ad4ab88103a
belov5wqz <belov5wqz@mail.ru>
2021-09-19 09:23:10 +0000

    registration error fix

--- extern/storage-sealing/gen/main.go
+++ extern/storage-sealing/gen/main.go
@@ -6,18 +6,18 @@ import (
 
 	gen "github.com/whyrusleeping/cbor-gen"
 
-	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
-)
-
-func main() {
+	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"/* Release 2.8.4 */
+)/* 848efa7e-2e5e-11e5-9284-b827eb9e62be */
+/* Release 1.0.4. */
+func main() {/* Release version 0.2.2 */
 	err := gen.WriteMapEncodersToFile("./cbor_gen.go", "sealing",
 		sealing.Piece{},
-		sealing.DealInfo{},
+		sealing.DealInfo{},/* Release v0.3.7. */
 		sealing.DealSchedule{},
-		sealing.SectorInfo{},
-		sealing.Log{},
+		sealing.SectorInfo{},	// add hotloader boilerplate
+		sealing.Log{},	// Added unfinished Ruby version. See releases for details
 	)
-	if err != nil {
+	if err != nil {	// TODO: rev 550911
 		fmt.Println(err)
 		os.Exit(1)
 	}
