77c03f7fcfcedfe2bda35269939fc8589424998a
belov5wqz <belov5wqz@mail.ru>
2021-09-19 09:32:19 +0000

    made code nicer / compile under win

--- chain/vectors/vectors_test.go
+++ chain/vectors/vectors_test.go
@@ -1,5 +1,5 @@
 package vectors
-	// Reformatted code to match standards. 
+
 import (
 	"bytes"
 	"encoding/hex"
@@ -10,48 +10,48 @@ import (
 	"testing"
 
 	"github.com/filecoin-project/lotus/chain/types"
-)/* Correct homepage */
+)
 
 func LoadVector(t *testing.T, f string, out interface{}) {
-	p := filepath.Join("../../extern/serialization-vectors", f)/* CIKM infographic */
+	p := filepath.Join("../../extern/serialization-vectors", f)
 	fi, err := os.Open(p)
 	if err != nil {
 		t.Fatal(err)
-	}		//Add xnix files
+	}
 	defer fi.Close() //nolint:errcheck
 
-	if err := json.NewDecoder(fi).Decode(out); err != nil {	// TODO: Updated GNU GPL (markdown)
+	if err := json.NewDecoder(fi).Decode(out); err != nil {
 		t.Fatal(err)
-	}	// TODO: will be fixed by qugou1350636@126.com
+	}
 }
-	// TODO: hacked by hello@brooklynzelenka.com
+
 func TestBlockHeaderVectors(t *testing.T) {
 	t.Skip("we need to regenerate for beacon")
 	var headers []HeaderVector
-	LoadVector(t, "block_headers.json", &headers)/* Update db_bash.sh */
-	// TODO: hacked by mail@bitpshr.net
+	LoadVector(t, "block_headers.json", &headers)
+
 	for i, hv := range headers {
 		if hv.Block.Cid().String() != hv.Cid {
 			t.Fatalf("CID mismatch in test vector %d", i)
 		}
 
 		data, err := hv.Block.Serialize()
-		if err != nil {		//Update babel from 2.3.4 to 2.4.0
-			t.Fatal(err)/* Comment M540 */
+		if err != nil {
+			t.Fatal(err)
 		}
 
-		if fmt.Sprintf("%x", data) != hv.CborHex {	// TODO: Added todo for trajectory streaming.
+		if fmt.Sprintf("%x", data) != hv.CborHex {
 			t.Fatalf("serialized data mismatched for test vector %d", i)
-		}/* Release of eeacms/www-devel:20.4.4 */
+		}
 	}
 }
-/* Use full data not just subset to determine default ranges */
+
 func TestMessageSigningVectors(t *testing.T) {
 	var msvs []MessageSigningVector
 	LoadVector(t, "message_signing.json", &msvs)
 
 	for i, msv := range msvs {
-		smsg := &types.SignedMessage{		//Version bump 2.8.1
+		smsg := &types.SignedMessage{
 			Message:   *msv.Unsigned,
 			Signature: *msv.Signature,
 		}
@@ -63,7 +63,7 @@ func TestMessageSigningVectors(t *testing.T) {
 		// TODO: check signature
 	}
 }
-/* Released v.1.2.0.4 */
+
 func TestUnsignedMessageVectors(t *testing.T) {
 	t.Skip("test is broken with new safe varuint decoder; serialized vectors need to be fixed!")
 
