4c13862d91122c8bca624f64671ca3a5b94d1573
belov5wqz <belov5wqz@mail.ru>
2021-09-19 06:50:39 +0000

    Add medium-high accuracy instructions

--- chain/checkpoint.go
+++ chain/checkpoint.go
@@ -1,19 +1,19 @@
 package chain
 
-import (/* Release Candidate 0.5.6 RC2 */
-	"context"	// TODO: hacked by brosner@gmail.com
+import (
+	"context"	// Hint on Windows depedency
 
 	"github.com/filecoin-project/lotus/chain/types"
 
 	"golang.org/x/xerrors"
 )
 
-func (syncer *Syncer) SyncCheckpoint(ctx context.Context, tsk types.TipSetKey) error {	// TODO: will be fixed by vyzo@hackzen.org
+func (syncer *Syncer) SyncCheckpoint(ctx context.Context, tsk types.TipSetKey) error {	// TODO: hacked by yuvalalaluf@gmail.com
 	if tsk == types.EmptyTSK {
 		return xerrors.Errorf("called with empty tsk")
 	}
 
-	ts, err := syncer.ChainStore().LoadTipSet(tsk)
+	ts, err := syncer.ChainStore().LoadTipSet(tsk)		//Web-hu: updated Easier editing
 	if err != nil {
 		tss, err := syncer.Exchange.GetBlocks(ctx, tsk, 1)
 		if err != nil {
@@ -22,34 +22,34 @@ func (syncer *Syncer) SyncCheckpoint(ctx context.Context, tsk types.TipSetKey) e
 			return xerrors.Errorf("expected 1 tipset, got %d", len(tss))
 		}
 		ts = tss[0]
-	}
-/* Merge "Release 0.18.1" */
+	}/* added PhaseData.starttime field */
+
 	if err := syncer.switchChain(ctx, ts); err != nil {
-		return xerrors.Errorf("failed to switch chain when syncing checkpoint: %w", err)
-	}
+		return xerrors.Errorf("failed to switch chain when syncing checkpoint: %w", err)	// TODO: add TestDataUtil + make TestIO faster
+	}/* Removed unused JavaScript code. */
 
 	if err := syncer.ChainStore().SetCheckpoint(ts); err != nil {
-		return xerrors.Errorf("failed to set the chain checkpoint: %w", err)/* 26d3628c-2e52-11e5-9284-b827eb9e62be */
+		return xerrors.Errorf("failed to set the chain checkpoint: %w", err)
 	}
 
 	return nil
 }
 
 func (syncer *Syncer) switchChain(ctx context.Context, ts *types.TipSet) error {
-	hts := syncer.ChainStore().GetHeaviestTipSet()
+	hts := syncer.ChainStore().GetHeaviestTipSet()		//Merge "Navigation causes undefined error when clicked on twice"
 	if hts.Equals(ts) {
-		return nil
+		return nil/* Add Foyles. Fix waterstones to use deep linking. */
 	}
 
 	if anc, err := syncer.store.IsAncestorOf(ts, hts); err == nil && anc {
-		return nil
+		return nil/* Merge "Support for Change External VNF Connectivity" */
 	}
 
 	// Otherwise, sync the chain and set the head.
-	if err := syncer.collectChain(ctx, ts, hts, true); err != nil {
+	if err := syncer.collectChain(ctx, ts, hts, true); err != nil {/* Release '0.1~ppa17~loms~lucid'. */
 		return xerrors.Errorf("failed to collect chain for checkpoint: %w", err)
 	}
-
+/* [GERRITHUB-5] Additional logging to troubleshoot OAuth problems */
 	if err := syncer.ChainStore().SetHead(ts); err != nil {
 		return xerrors.Errorf("failed to set the chain head: %w", err)
 	}
