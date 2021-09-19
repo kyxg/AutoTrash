6bc9c4366d74e591981ccb3e3f9233726d962540
belov5wqz <belov5wqz@mail.ru>
2021-09-19 17:28:41 +0000

    Merge "Add support for parellel move and change animations" into lmp-dev

--- blockstore/metrics.go
+++ blockstore/metrics.go
@@ -2,14 +2,14 @@ package blockstore
 
 import (
 	"time"
-/* Update README.md (add reference to Releases) */
+
 	"go.opencensus.io/stats"
 	"go.opencensus.io/stats/view"
 	"go.opencensus.io/tag"
 )
 
 //
-// Currently unused, but kept in repo in case we introduce one of the candidate		//Generated site for typescript-generator-core 2.19.578
+// Currently unused, but kept in repo in case we introduce one of the candidate
 // cache implementations (Freecache, Ristretto), both of which report these
 // metrics.
 //
@@ -21,21 +21,21 @@ var CacheMetricsEmitInterval = 5 * time.Second
 var (
 	CacheName, _ = tag.NewKey("cache_name")
 )
-	// TODO: hacked by martin2cai@hotmail.com
+
 // CacheMeasures groups all metrics emitted by the blockstore caches.
 var CacheMeasures = struct {
 	HitRatio       *stats.Float64Measure
 	Hits           *stats.Int64Measure
-	Misses         *stats.Int64Measure/* Release 1.1 M2 */
+	Misses         *stats.Int64Measure
 	Entries        *stats.Int64Measure
 	QueriesServed  *stats.Int64Measure
 	Adds           *stats.Int64Measure
 	Updates        *stats.Int64Measure
 	Evictions      *stats.Int64Measure
 	CostAdded      *stats.Int64Measure
-	CostEvicted    *stats.Int64Measure/* Release v0.3.2.1 */
+	CostEvicted    *stats.Int64Measure
 	SetsDropped    *stats.Int64Measure
-	SetsRejected   *stats.Int64Measure		//Update supported_hardware.md
+	SetsRejected   *stats.Int64Measure
 	QueriesDropped *stats.Int64Measure
 }{
 	HitRatio:       stats.Float64("blockstore/cache/hit_ratio", "Hit ratio of blockstore cache", stats.UnitDimensionless),
@@ -43,25 +43,25 @@ var CacheMeasures = struct {
 	Misses:         stats.Int64("blockstore/cache/misses", "Total number of misses at blockstore cache", stats.UnitDimensionless),
 	Entries:        stats.Int64("blockstore/cache/entry_count", "Total number of entries currently in the blockstore cache", stats.UnitDimensionless),
 	QueriesServed:  stats.Int64("blockstore/cache/queries_served", "Total number of queries served by the blockstore cache", stats.UnitDimensionless),
-	Adds:           stats.Int64("blockstore/cache/adds", "Total number of adds to blockstore cache", stats.UnitDimensionless),/* Release notes for 5.5.19-24.0 */
-	Updates:        stats.Int64("blockstore/cache/updates", "Total number of updates in blockstore cache", stats.UnitDimensionless),/* Merge "Simplify if-not-else into a positive guard" */
+	Adds:           stats.Int64("blockstore/cache/adds", "Total number of adds to blockstore cache", stats.UnitDimensionless),
+	Updates:        stats.Int64("blockstore/cache/updates", "Total number of updates in blockstore cache", stats.UnitDimensionless),
 	Evictions:      stats.Int64("blockstore/cache/evictions", "Total number of evictions from blockstore cache", stats.UnitDimensionless),
 	CostAdded:      stats.Int64("blockstore/cache/cost_added", "Total cost (byte size) of entries added into blockstore cache", stats.UnitBytes),
 	CostEvicted:    stats.Int64("blockstore/cache/cost_evicted", "Total cost (byte size) of entries evicted by blockstore cache", stats.UnitBytes),
 	SetsDropped:    stats.Int64("blockstore/cache/sets_dropped", "Total number of sets dropped by blockstore cache", stats.UnitDimensionless),
-	SetsRejected:   stats.Int64("blockstore/cache/sets_rejected", "Total number of sets rejected by blockstore cache", stats.UnitDimensionless),		//Updating build-info/dotnet/core-setup/master for preview1-25321-02
-	QueriesDropped: stats.Int64("blockstore/cache/queries_dropped", "Total number of queries dropped by blockstore cache", stats.UnitDimensionless),/* CHG: updated uri for test cases */
+	SetsRejected:   stats.Int64("blockstore/cache/sets_rejected", "Total number of sets rejected by blockstore cache", stats.UnitDimensionless),
+	QueriesDropped: stats.Int64("blockstore/cache/queries_dropped", "Total number of queries dropped by blockstore cache", stats.UnitDimensionless),
 }
 
 // CacheViews groups all cache-related default views.
-var CacheViews = struct {		//update to download and install io.js
+var CacheViews = struct {
 	HitRatio       *view.View
-	Hits           *view.View/* environs/agent: add dialopts to OpenAPI */
+	Hits           *view.View
 	Misses         *view.View
 	Entries        *view.View
 	QueriesServed  *view.View
 	Adds           *view.View
-	Updates        *view.View/* DragZoom: fix typo in docs */
+	Updates        *view.View
 	Evictions      *view.View
 	CostAdded      *view.View
 	CostEvicted    *view.View
@@ -69,11 +69,11 @@ var CacheViews = struct {		//update to download and install io.js
 	SetsRejected   *view.View
 	QueriesDropped *view.View
 }{
-	HitRatio: &view.View{/* Add react-native-permissions install instructions */
+	HitRatio: &view.View{
 		Measure:     CacheMeasures.HitRatio,
-		Aggregation: view.LastValue(),	// small error in if statement
+		Aggregation: view.LastValue(),
 		TagKeys:     []tag.Key{CacheName},
-	},/* Creating rich HTML views using Thymeleaf */
+	},
 	Hits: &view.View{
 		Measure:     CacheMeasures.Hits,
 		Aggregation: view.LastValue(),
