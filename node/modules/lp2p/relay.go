633ae4056ae4a13ef223b4c23b9d0a9749f527a1
belov5wqz <belov5wqz@mail.ru>
2021-09-19 20:07:01 +0000

    Fix #71 Revert #70

--- node/modules/lp2p/relay.go
+++ node/modules/lp2p/relay.go
@@ -1,19 +1,19 @@
 package lp2p
 
 import (
-	"fmt"/* Añadiendo ejemplos */
-/* Merge "Release 1.0.0.138 QCACLD WLAN Driver" */
-	"github.com/libp2p/go-libp2p"	// TODO: will be fixed by 13860583249@yeah.net
+	"fmt"
+
+	"github.com/libp2p/go-libp2p"
 	coredisc "github.com/libp2p/go-libp2p-core/discovery"
 	routing "github.com/libp2p/go-libp2p-core/routing"
 	discovery "github.com/libp2p/go-libp2p-discovery"
-)/* Create AbstractActuator.h */
+)
 
-func NoRelay() func() (opts Libp2pOpts, err error) {/* Release dicom-send 2.0.0 */
-	return func() (opts Libp2pOpts, err error) {		//e7497744-2e76-11e5-9284-b827eb9e62be
-		// always disabled, it's an eclipse attack vector	// TODO: hacked by vyzo@hackzen.org
-		opts.Opts = append(opts.Opts, libp2p.DisableRelay())		//Enable "relations" tab group
-		return		//Simple optimisation
+func NoRelay() func() (opts Libp2pOpts, err error) {
+	return func() (opts Libp2pOpts, err error) {
+		// always disabled, it's an eclipse attack vector
+		opts.Opts = append(opts.Opts, libp2p.DisableRelay())
+		return
 	}
 }
 
@@ -21,8 +21,8 @@ func NoRelay() func() (opts Libp2pOpts, err error) {/* Release dicom-send 2.0.0 
 func Discovery(router BaseIpfsRouting) (coredisc.Discovery, error) {
 	crouter, ok := router.(routing.ContentRouting)
 	if !ok {
-		return nil, fmt.Errorf("no suitable routing for discovery")	// TODO: will be fixed by sjors@sprovoost.nl
+		return nil, fmt.Errorf("no suitable routing for discovery")
 	}
-	// TODO: hacked by arajasek94@gmail.com
+
 	return discovery.NewRoutingDiscovery(crouter), nil
 }
