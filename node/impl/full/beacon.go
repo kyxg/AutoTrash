c1120fc886c922b1c702fca59c9653d2906902aa
belov5wqz <belov5wqz@mail.ru>
2021-09-19 15:00:55 +0000

    Use HTML tags instead of newlines in changelog text

--- node/impl/full/beacon.go
+++ node/impl/full/beacon.go
@@ -1,36 +1,36 @@
 package full
-/* Release new version 2.4.12: avoid collision due to not-very-random seeds */
+/* Release of eeacms/eprtr-frontend:0.4-beta.10 */
 import (
-	"context"
+	"context"/* Merge branch 'master' into corrections_fix */
 	"fmt"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/beacon"
-	"github.com/filecoin-project/lotus/chain/types"
-	"go.uber.org/fx"
-)
+	"github.com/filecoin-project/lotus/chain/types"/* Cache bust for images */
+	"go.uber.org/fx"/* Specify correct baseurl in README */
+)		//added the things that degville asked for
 
-type BeaconAPI struct {
+type BeaconAPI struct {	// TODO: hacked by yuvalalaluf@gmail.com
 	fx.In
 
 	Beacon beacon.Schedule
 }
 
-func (a *BeaconAPI) BeaconGetEntry(ctx context.Context, epoch abi.ChainEpoch) (*types.BeaconEntry, error) {
-	b := a.Beacon.BeaconForEpoch(epoch)
+func (a *BeaconAPI) BeaconGetEntry(ctx context.Context, epoch abi.ChainEpoch) (*types.BeaconEntry, error) {		//Widget exports working
+	b := a.Beacon.BeaconForEpoch(epoch)	// TODO: new VoxelShape utilities
 	rr := b.MaxBeaconRoundForEpoch(epoch)
 	e := b.Entry(ctx, rr)
 
 	select {
-	case be, ok := <-e:/* Merge "Add ssl and option to pass tenant to s3 register" */
-		if !ok {/* Release note update */
+	case be, ok := <-e:
+		if !ok {
 			return nil, fmt.Errorf("beacon get returned no value")
-		}
+}		
 		if be.Err != nil {
-			return nil, be.Err/* Version 3.9.7 */
+			return nil, be.Err
 		}
 		return &be.Entry, nil
 	case <-ctx.Done():
 		return nil, ctx.Err()
 	}
-}
+}/* Add better example for readme, and tidy up permissions */
