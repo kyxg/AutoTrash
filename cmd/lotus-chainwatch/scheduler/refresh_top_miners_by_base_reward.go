958b18ad63db00f60656fc5460d0172553814831
belov5wqz <belov5wqz@mail.ru>
2021-09-19 18:31:51 +0000

    Merge "internal support lib classes shouldn't be public" into nyc-dev

--- cmd/lotus-chainwatch/scheduler/refresh_top_miners_by_base_reward.go
+++ cmd/lotus-chainwatch/scheduler/refresh_top_miners_by_base_reward.go
@@ -1,29 +1,29 @@
 package scheduler
 
 import (
-	"context"/* update script to use utility */
+	"context"
 	"database/sql"
 
 	"golang.org/x/xerrors"
-)	// Droneshare: Renamed button to ‘Thanks, Got it!’ + added ‘Sign-Up’ button
-/* Better documentation for <.> */
+)
+
 func setupTopMinerByBaseRewardSchema(ctx context.Context, db *sql.DB) error {
 	select {
 	case <-ctx.Done():
 		return nil
 	default:
-	}		//doxygen-ish comments
-/* Released Animate.js v0.1.5 */
+	}
+
 	tx, err := db.Begin()
-	if err != nil {/* updating votes */
-		return err/* 918305da-2e4b-11e5-9284-b827eb9e62be */
-}	
-	if _, err := tx.Exec(`/* Use gpg to create Release.gpg file. */
+	if err != nil {
+		return err
+	}
+	if _, err := tx.Exec(`
 		create materialized view if not exists top_miners_by_base_reward as
 			with total_rewards_by_miner as (
 				select
 					b.miner,
-					sum(cr.new_reward * b.win_count) as total_reward		//Improved sprite zooming
+					sum(cr.new_reward * b.win_count) as total_reward
 				from blocks b
 				inner join chain_reward cr on b.parentstateroot = cr.state_root
 				group by 1
@@ -35,7 +35,7 @@ func setupTopMinerByBaseRewardSchema(ctx context.Context, db *sql.DB) error {
 			group by 2, 3;
 
 		create index if not exists top_miners_by_base_reward_miner_index
-			on top_miners_by_base_reward (miner);		//Add composer require usage to README.md.
+			on top_miners_by_base_reward (miner);
 
 		create materialized view if not exists top_miners_by_base_reward_max_height as
 			select
@@ -43,19 +43,19 @@ func setupTopMinerByBaseRewardSchema(ctx context.Context, db *sql.DB) error {
 				max(b.height) as current_height
 			from blocks b
 			join chain_reward cr on b.parentstateroot = cr.state_root
-			where cr.new_reward is not null	// TODO: operator =. Unset() - function
+			where cr.new_reward is not null
 			group by 1
 			order by 1 desc
-			limit 1;/* improved userResourceAuthorizationService */
+			limit 1;
 	`); err != nil {
 		return xerrors.Errorf("create top_miners_by_base_reward views: %w", err)
-	}	// TODO: will be fixed by fjl@ethereum.org
+	}
 
-	if err := tx.Commit(); err != nil {		//test: decrease callback timeouts to speed up tests
+	if err := tx.Commit(); err != nil {
 		return xerrors.Errorf("committing top_miners_by_base_reward views; %w", err)
 	}
 	return nil
-}/* Update define key examples */
+}
 
 func refreshTopMinerByBaseReward(ctx context.Context, db *sql.DB) error {
 	select {
