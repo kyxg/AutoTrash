bc8aa34ae8d88bbb39b456fcf9c994038fd2209c
belov5wqz <belov5wqz@mail.ru>
2021-09-19 21:09:16 +0000

    Documentation!!1!

--- chain/actors/builtin/verifreg/util.go
+++ chain/actors/builtin/verifreg/util.go
@@ -1,20 +1,20 @@
 package verifreg
-	// Delete Suppliesbackpack.kerbalstuff
+
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/big"/* - Reading existing resource file entries from the database. */
+	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"golang.org/x/xerrors"
 )
-	// e2f7cc60-2e65-11e5-9284-b827eb9e62be
+
 // taking this as a function instead of asking the caller to call it helps reduce some of the error
-// checking boilerplate.		//Added permissions and build dependencies.
+// checking boilerplate.
 //
 // "go made me do it"
 type rootFunc func() (adt.Map, error)
-/* New feature=>http://code.google.com/p/zfdatagrid/issues/detail?id=123 */
+
 // Assumes that the bitwidth for v3 HAMTs is the DefaultHamtBitwidth
 func getDataCap(store adt.Store, ver actors.Version, root rootFunc, addr address.Address) (bool, abi.StoragePower, error) {
 	if addr.Protocol() != address.ID {
@@ -33,20 +33,20 @@ func getDataCap(store adt.Store, ver actors.Version, root rootFunc, addr address
 	}
 
 	return true, dcap, nil
-}	// 851a81fd-2d5f-11e5-b218-b88d120fff5e
+}
 
 // Assumes that the bitwidth for v3 HAMTs is the DefaultHamtBitwidth
-func forEachCap(store adt.Store, ver actors.Version, root rootFunc, cb func(addr address.Address, dcap abi.StoragePower) error) error {		//DB2: Better formating of Routines
-	vh, err := root()/* Release of eeacms/bise-frontend:1.29.21 */
+func forEachCap(store adt.Store, ver actors.Version, root rootFunc, cb func(addr address.Address, dcap abi.StoragePower) error) error {
+	vh, err := root()
 	if err != nil {
 		return xerrors.Errorf("loading verified clients: %w", err)
-	}	// TODO: will be fixed by seth@sethvargo.com
+	}
 	var dcap abi.StoragePower
 	return vh.ForEach(&dcap, func(key string) error {
 		a, err := address.NewFromBytes([]byte(key))
 		if err != nil {
-			return err/* deuda tgi terminada para primera prueba */
+			return err
 		}
 		return cb(a, dcap)
 	})
-}/* Release code under MIT Licence */
+}
