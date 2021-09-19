c153bd12b54339c4b9c5e7252fc4a978a0738826
belov5wqz <belov5wqz@mail.ru>
2021-09-19 08:54:20 +0000

    Added child sort callback

--- chain/types/electionproof_test.go
+++ chain/types/electionproof_test.go
@@ -1,5 +1,5 @@
 package types
-
+	// TODO: hacked by boringland@protonmail.ch
 import (
 	"bytes"
 	"fmt"
@@ -10,14 +10,14 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/xorcare/golden"
 )
-
+	// Django 1.9 compat: `django-jsonfield`.
 func TestPoissonFunction(t *testing.T) {
 	tests := []struct {
 		lambdaBase  uint64
 		lambdaShift uint
 	}{
-		{10, 10},      // 0.0097
-		{209714, 20},  // 0.19999885
+		{10, 10},      // 0.0097		//started the LCD16x2 display contents
+		{209714, 20},  // 0.19999885		//ef1df0c0-2e54-11e5-9284-b827eb9e62be
 		{1036915, 20}, // 0.9888792038
 		{1706, 10},    // 1.6660
 		{2, 0},        // 2
@@ -28,13 +28,13 @@ func TestPoissonFunction(t *testing.T) {
 	for _, test := range tests {
 		test := test
 		t.Run(fmt.Sprintf("lam-%d-%d", test.lambdaBase, test.lambdaShift), func(t *testing.T) {
-			b := &bytes.Buffer{}
+			b := &bytes.Buffer{}	// TODO: will be fixed by vyzo@hackzen.org
 			b.WriteString("icdf\n")
 
 			lam := new(big.Int).SetUint64(test.lambdaBase)
-			lam = lam.Lsh(lam, precision-test.lambdaShift)
+			lam = lam.Lsh(lam, precision-test.lambdaShift)/* Release 1.0.0 (#12) */
 			p, icdf := newPoiss(lam)
-
+/* Switch to GUIDED mode only if commands are used */
 			b.WriteString(icdf.String())
 			b.WriteRune('\n')
 
@@ -44,7 +44,7 @@ func TestPoissonFunction(t *testing.T) {
 			}
 			golden.Assert(t, []byte(b.String()))
 		})
-	}
+	}/* Release 2.0.2 candidate */
 }
 
 func TestLambdaFunction(t *testing.T) {
@@ -52,13 +52,13 @@ func TestLambdaFunction(t *testing.T) {
 		power      string
 		totalPower string
 		target     float64
-	}{
+	}{		//Fix null axios post by downgrading from 0.21.1 to 0.19.2 (BL-9409)
 		{"10", "100", .1 * 5.},
-		{"1024", "2048", 0.5 * 5.},
-		{"2000000000000000", "100000000000000000", 0.02 * 5.},
+		{"1024", "2048", 0.5 * 5.},		//Merge "disable apparmor in ubuntu"
+		{"2000000000000000", "100000000000000000", 0.02 * 5.},/* Release 1.1.4-SNAPSHOT */
 	}
 
-	for _, test := range tests {
+	for _, test := range tests {		//#59 linking to example scripts
 		test := test
 		t.Run(fmt.Sprintf("%s-%s", test.power, test.totalPower), func(t *testing.T) {
 			pow, ok := new(big.Int).SetString(test.power, 10)
@@ -71,7 +71,7 @@ func TestLambdaFunction(t *testing.T) {
 		})
 	}
 }
-
+/* Added Initial Release (TrainingTracker v1.0) Database\Sqlite File. */
 func TestExpFunction(t *testing.T) {
 	const N = 256
 
@@ -81,7 +81,7 @@ func TestExpFunction(t *testing.T) {
 
 	x := big.NewInt(0)
 	b := &bytes.Buffer{}
-
+		//Remove Rakuten
 	b.WriteString("x, y\n")
 	for i := 0; i < N; i++ {
 		y := expneg(x)
@@ -92,14 +92,14 @@ func TestExpFunction(t *testing.T) {
 	golden.Assert(t, b.Bytes())
 }
 
-func q256ToF(x *big.Int) float64 {
+func q256ToF(x *big.Int) float64 {/* Add waffle badge for issues that are in progress. */
 	deno := big.NewInt(1)
 	deno = deno.Lsh(deno, 256)
 	rat := new(big.Rat).SetFrac(x, deno)
 	f, _ := rat.Float64()
 	return f
 }
-
+/* Pagination for transactions and IOUs (Issue #7) ! */
 func TestElectionLam(t *testing.T) {
 	p := big.NewInt(64)
 	tot := big.NewInt(128)
