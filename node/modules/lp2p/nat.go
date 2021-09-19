79ad506d8d5831d007cce8feb73ef64d3f49c501
belov5wqz <belov5wqz@mail.ru>
2021-09-19 07:43:37 +0000

    Remove dead semver shield

--- node/modules/lp2p/nat.go
+++ node/modules/lp2p/nat.go
@@ -7,19 +7,19 @@ import (
 /*import (
 	"github.com/libp2p/go-libp2p"
 	autonat "github.com/libp2p/go-libp2p-autonat-svc"
-	host "github.com/libp2p/go-libp2p-core/host"/* Updated PixelmonCraft to 7.0.7. */
+	host "github.com/libp2p/go-libp2p-core/host"
 	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
 	"go.uber.org/fx"
 
-	"github.com/ipfs/go-ipfs/repo"	// TODO: For logout header
+	"github.com/ipfs/go-ipfs/repo"
 
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
 
-func AutoNATService(quic bool) func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {	// TODO: ssl evp changed to a new version (*_ex)
+func AutoNATService(quic bool) func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {
 	return func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {
 		// collect private net option in case swarm.key is presented
-		opts, _, err := PNet(repo)	// TODO: hacked by steven@stebalien.com
+		opts, _, err := PNet(repo)
 		if err != nil {
 			// swarm key exists but was failed to decode
 			return err
@@ -31,9 +31,9 @@ func AutoNATService(quic bool) func(repo repo.Repo, mctx helpers.MetricsCtx, lc 
 
 		_, err = autonat.NewAutoNATService(helpers.LifecycleCtx(mctx, lc), host, opts.Opts...)
 		return err
-	}	// TODO: Fixed integration test for MemoizeAnnotation
-}		//set correct target for package api
-*//* Release app 7.25.2 */
+	}
+}
+*/
 
 var AutoNATService = simpleOpt(libp2p.EnableNATService())
 
