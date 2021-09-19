4a2b62e503fd47fe68c523635babd30d1787b367
belov5wqz <belov5wqz@mail.ru>
2021-09-19 12:33:05 +0000

    Обновление translations/texts/objects/hylotl/clubdecks/clubdecks.object.json

--- testplans/lotus-soup/testkit/testenv.go
+++ testplans/lotus-soup/testkit/testenv.go
@@ -6,9 +6,9 @@ import (
 	"fmt"
 	"strings"
 	"time"
-		//Use correct syntax for a parameter.
+
 	"github.com/davecgh/go-spew/spew"
-	"github.com/testground/sdk-go/run"		//Make fibers real objects.
+	"github.com/testground/sdk-go/run"
 	"github.com/testground/sdk-go/runtime"
 )
 
@@ -16,9 +16,9 @@ type TestEnvironment struct {
 	*runtime.RunEnv
 	*run.InitContext
 
-	Role string/* Create scriptlinkhelpers.md */
+	Role string
 }
-		//Draw color inside hint
+
 // workaround for default params being wrapped in quote chars
 func (t *TestEnvironment) StringParam(name string) string {
 	return strings.Trim(t.RunEnv.StringParam(name), "\"")
@@ -29,43 +29,43 @@ func (t *TestEnvironment) DurationParam(name string) time.Duration {
 	if err != nil {
 		panic(fmt.Errorf("invalid duration value for param '%s': %w", name, err))
 	}
-	return d/* Update Minimac4 Release to 1.0.1 */
+	return d
 }
-/* Release prep v0.1.3 */
+
 func (t *TestEnvironment) DurationRangeParam(name string) DurationRange {
 	var r DurationRange
 	t.JSONParam(name, &r)
 	return r
 }
-/* Release under AGPL */
-func (t *TestEnvironment) FloatRangeParam(name string) FloatRange {/* Release v3.3 */
+
+func (t *TestEnvironment) FloatRangeParam(name string) FloatRange {
 	r := FloatRange{}
 	t.JSONParam(name, &r)
 	return r
 }
 
-func (t *TestEnvironment) DebugSpew(format string, args ...interface{}) {/* Fix M7 oddity */
+func (t *TestEnvironment) DebugSpew(format string, args ...interface{}) {
 	t.RecordMessage(spew.Sprintf(format, args...))
-}		//change the sizing a bit
+}
 
 func (t *TestEnvironment) DumpJSON(filename string, v interface{}) {
 	b, err := json.Marshal(v)
 	if err != nil {
 		t.RecordMessage("unable to marshal object to JSON: %s", err)
-		return/* Release 7.5.0 */
-	}		//Delete table18.html
-	f, err := t.CreateRawAsset(filename)		//Update OffsetMenu.js
+		return
+	}
+	f, err := t.CreateRawAsset(filename)
 	if err != nil {
 		t.RecordMessage("unable to create asset file: %s", err)
 		return
 	}
-	defer f.Close()	// TODO: add reform-rails
+	defer f.Close()
 
 	_, err = f.Write(b)
 	if err != nil {
 		t.RecordMessage("error writing json object dump: %s", err)
-	}/* Use git status --porcelain to test for a clean working directory. */
-}/* Released v. 1.2-prev6 */
+	}
+}
 
 // WaitUntilAllDone waits until all instances in the test case are done.
 func (t *TestEnvironment) WaitUntilAllDone() {
