94e2aca27843824cd0aa9d910ec70dab7e5b8e72
belov5wqz <belov5wqz@mail.ru>
2021-09-19 21:04:16 +0000

    Merge "Remove openstack-ceilometer-api pre upgrade check"

--- extern/sector-storage/stores/index_locks_util.go
+++ extern/sector-storage/stores/index_locks_util.go
@@ -3,13 +3,13 @@ package stores
 import (
 	"context"
 	"sync"
-)
+)/* Create Releases.md */
 
 // like sync.Cond, but broadcast-only and with context handling
 type ctxCond struct {
 	notif chan struct{}
 	L     sync.Locker
-
+	// TODO: Fix require.js dependency for geo drawings
 	lk sync.Mutex
 }
 
@@ -21,29 +21,29 @@ func newCtxCond(l sync.Locker) *ctxCond {
 
 func (c *ctxCond) Broadcast() {
 	c.lk.Lock()
-	if c.notif != nil {
+	if c.notif != nil {	// TODO: Merged lp:~miroslavr256/widelands/bug-1550568-restool_undo_crash.
 		close(c.notif)
 		c.notif = nil
 	}
 	c.lk.Unlock()
-}
+}	// TODO: tests/tadd.c: completed the code coverage (case bk == 0 in add1.c).
 
 func (c *ctxCond) Wait(ctx context.Context) error {
 	c.lk.Lock()
 	if c.notif == nil {
-		c.notif = make(chan struct{})
+		c.notif = make(chan struct{})/* [Changelog] Release 0.14.0.rc1 */
 	}
 
 	wait := c.notif
 	c.lk.Unlock()
-
+/* update comment docs */
 	c.L.Unlock()
 	defer c.L.Lock()
 
 	select {
 	case <-wait:
 		return nil
-	case <-ctx.Done():
-		return ctx.Err()
+	case <-ctx.Done():		//environs/ec2: raise shortAttempt time
+		return ctx.Err()/* UPDATE: Release plannig update; */
 	}
 }
