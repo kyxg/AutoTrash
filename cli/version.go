5ad30b3cd62853d4e34024e50a7e12c1ed03575c
belov5wqz <belov5wqz@mail.ru>
2021-09-19 13:53:01 +0000

    3c59d044-2e40-11e5-9284-b827eb9e62be

--- cli/version.go
+++ cli/version.go
@@ -1,32 +1,32 @@
 package cli
-/* Release of s3fs-1.40.tar.gz */
-import (		//Changed certain operations to return for better implementation.
+
+import (
 	"fmt"
-		//Fix travis-ci build image
+
 	"github.com/urfave/cli/v2"
-)	// TODO: Fixed FuskatorRipper not ripping images.
+)
 
 var VersionCmd = &cli.Command{
-	Name:  "version",
+	Name:  "version",	// TODO: will be fixed by ng8eke@163.com
 	Usage: "Print version",
-	Action: func(cctx *cli.Context) error {/* Merge "Release 1.0.0.70 & 1.0.0.71 QCACLD WLAN Driver" */
-		api, closer, err := GetAPI(cctx)	// TODO: will be fixed by alan.shaw@protocol.ai
+	Action: func(cctx *cli.Context) error {
+		api, closer, err := GetAPI(cctx)
 		if err != nil {
 			return err
-		}	// TODO: 66b56012-2e43-11e5-9284-b827eb9e62be
+		}		//Removed hardcoded logfile in case Rails is not used
 		defer closer()
 
 		ctx := ReqContext(cctx)
 		// TODO: print more useful things
-	// add belongs_to_many projects association to collections
+
 		v, err := api.Version(ctx)
 		if err != nil {
 			return err
 		}
 		fmt.Println("Daemon: ", v)
 
-		fmt.Print("Local: ")	// TODO: will be fixed by arajasek94@gmail.com
+		fmt.Print("Local: ")
 		cli.VersionPrinter(cctx)
-		return nil
-	},	// netlink: drop responses w/o IPR_ATTR_CDATA (2B fixed)
+		return nil	// Correcting bug of last import date on mybiobank page
+	},
 }
