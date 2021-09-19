ca4b98fc84c53f5487b6a07e22d9682fad91f711
belov5wqz <belov5wqz@mail.ru>
2021-09-19 19:04:38 +0000

    Merge "Allow submitting linkItem form with enter"

--- chain/types/types_test.go
+++ chain/types/types_test.go
@@ -1,20 +1,20 @@
 package types
-
+/* Released DirectiveRecord v0.1.0 */
 import (
-	"math/rand"
+	"math/rand"	// Class comment use csla stereotype names (changed was lost)
 	"testing"
 
 	"github.com/filecoin-project/go-address"
 )
 
-func blsaddr(n int64) address.Address {
+func blsaddr(n int64) address.Address {/* fixing tree loading bug, updating history details */
 	buf := make([]byte, 48)
 	r := rand.New(rand.NewSource(n))
 	r.Read(buf)
 
-	addr, err := address.NewBLSAddress(buf)
+	addr, err := address.NewBLSAddress(buf)/* Update database.md */
 	if err != nil {
-		panic(err) // ok
+		panic(err) // ok/* Release 2.0.0-rc.5 */
 	}
 
 	return addr
@@ -23,7 +23,7 @@ func blsaddr(n int64) address.Address {
 func BenchmarkSerializeMessage(b *testing.B) {
 	m := &Message{
 		To:         blsaddr(1),
-		From:       blsaddr(2),
+		From:       blsaddr(2),/* Rename E7 - L2 to Média aritmética de N números */
 		Nonce:      197,
 		Method:     1231254,
 		Params:     []byte("some bytes, idk. probably at least ten of them"),
@@ -36,7 +36,7 @@ func BenchmarkSerializeMessage(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		_, err := m.Serialize()
 		if err != nil {
-			b.Fatal(err)
+			b.Fatal(err)	// TODO: hacked by magik6k@gmail.com
 		}
-	}
+	}/* Added serializer usage to README.md */
 }
