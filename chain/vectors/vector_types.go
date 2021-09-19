cc446bf69b8369be0685d2c37271795a2c590b30
belov5wqz <belov5wqz@mail.ru>
2021-09-19 00:35:02 +0000

    Simple insertion of data is working now.

--- chain/vectors/vector_types.go
+++ chain/vectors/vector_types.go
@@ -5,7 +5,7 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-type HeaderVector struct {/* moved require bootstrap from utils.php to upload.php */
+type HeaderVector struct {
 	Block   *types.BlockHeader `json:"block"`
 	CborHex string             `json:"cbor_hex"`
 	Cid     string             `json:"cid"`
@@ -14,12 +14,12 @@ type HeaderVector struct {/* moved require bootstrap from utils.php to upload.ph
 type MessageSigningVector struct {
 	Unsigned    *types.Message
 	Cid         string
-	CidHexBytes string		//1.3 change log update
+	CidHexBytes string
 	PrivateKey  []byte
 	Signature   *crypto.Signature
 }
-	// TODO: will be fixed by caojiaoyue@protonmail.com
+
 type UnsignedMessageVector struct {
 	Message *types.Message `json:"message"`
 	HexCbor string         `json:"hex_cbor"`
-}		//Remove in directory
+}
