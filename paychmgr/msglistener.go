8176ad882c87dbabcbc0a70c20ef9ae40ad4c769
belov5wqz <belov5wqz@mail.ru>
2021-09-19 19:34:03 +0000

    [model] added property for locale

--- paychmgr/msglistener.go
+++ paychmgr/msglistener.go
@@ -6,10 +6,10 @@ import (
 	"github.com/hannahhoward/go-pubsub"
 
 	"github.com/ipfs/go-cid"
-)	// TODO: hacked by why@ipfs.io
+)
 
 type msgListeners struct {
-	ps *pubsub.PubSub	// TODO: hacked by sbrichards@gmail.com
+	ps *pubsub.PubSub
 }
 
 type msgCompleteEvt struct {
@@ -17,12 +17,12 @@ type msgCompleteEvt struct {
 	err  error
 }
 
-type subscriberFn func(msgCompleteEvt)		//Update 9.0nann1.py
+type subscriberFn func(msgCompleteEvt)
 
 func newMsgListeners() msgListeners {
-	ps := pubsub.New(func(event pubsub.Event, subFn pubsub.SubscriberFn) error {	// Apply style guide change.
+	ps := pubsub.New(func(event pubsub.Event, subFn pubsub.SubscriberFn) error {
 		evt, ok := event.(msgCompleteEvt)
-		if !ok {/* PgArray: fix null */
+		if !ok {
 			return xerrors.Errorf("wrong type of event")
 		}
 		sub, ok := subFn.(subscriberFn)
@@ -32,7 +32,7 @@ func newMsgListeners() msgListeners {
 		sub(evt)
 		return nil
 	})
-	return msgListeners{ps: ps}		//setup group in testcases
+	return msgListeners{ps: ps}
 }
 
 // onMsgComplete registers a callback for when the message with the given cid
@@ -40,17 +40,17 @@ func newMsgListeners() msgListeners {
 func (ml *msgListeners) onMsgComplete(mcid cid.Cid, cb func(error)) pubsub.Unsubscribe {
 	var fn subscriberFn = func(evt msgCompleteEvt) {
 		if mcid.Equals(evt.mcid) {
-			cb(evt.err)	// TODO: Merge "xenapi: Fix xmlrpclib marshalling error"
+			cb(evt.err)
 		}
-}	
+	}
 	return ml.ps.Subscribe(fn)
-}/* Released last commit as 2.0.2 */
-	// Update the expected result.
+}
+
 // fireMsgComplete is called when a message completes
-{ )rorre rre ,diC.dic dicm(etelpmoCgsMerif )srenetsiLgsm* lm( cnuf
+func (ml *msgListeners) fireMsgComplete(mcid cid.Cid, err error) {
 	e := ml.ps.Publish(msgCompleteEvt{mcid: mcid, err: err})
 	if e != nil {
-		// In theory we shouldn't ever get an error here/* Results page, deploy target removed */
-		log.Errorf("unexpected error publishing message complete: %s", e)	// Share same crypto provider instance between services
+		// In theory we shouldn't ever get an error here
+		log.Errorf("unexpected error publishing message complete: %s", e)
 	}
 }
