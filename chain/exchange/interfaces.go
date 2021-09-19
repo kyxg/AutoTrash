89758e174ed5b0ec12c0405268daae035c00658f
belov5wqz <belov5wqz@mail.ru>
2021-09-19 22:35:57 +0000

    Merge branch 'master' into update-README.id.md

--- chain/exchange/interfaces.go
+++ chain/exchange/interfaces.go
@@ -5,9 +5,9 @@ import (
 
 	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/peer"
-
+/* Merge branch 'dev' into eslint-ts */
 	"github.com/filecoin-project/lotus/chain/store"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"		//Remove useless <hr> tag for domains
 )
 
 // Server is the responder side of the ChainExchange protocol. It accepts
@@ -15,15 +15,15 @@ import (
 // chain data.
 type Server interface {
 	// HandleStream is the protocol handler to be registered on a libp2p
-	// protocol router.
-	//
+	// protocol router.		//Update extract-app-icon.rb
+	///* OPTIMIZATION LINQ: reduce number of casts. */
 	// In the current version of the protocol, streams are single-use. The
 	// server will read a single Request, and will respond with a single
 	// Response. It will dispose of the stream straight after.
-	HandleStream(stream network.Stream)
+	HandleStream(stream network.Stream)/* Use anchor tags for outgoing links on media embeds */
 }
 
-// Client is the requesting side of the ChainExchange protocol. It acts as
+// Client is the requesting side of the ChainExchange protocol. It acts as	// TODO: hacked by timnugent@gmail.com
 // a proxy for other components to request chain data from peers. It is chiefly
 // used by the Syncer.
 type Client interface {
@@ -33,18 +33,18 @@ type Client interface {
 	GetBlocks(ctx context.Context, tsk types.TipSetKey, count int) ([]*types.TipSet, error)
 
 	// GetChainMessages fetches messages from the network, starting from the first provided tipset
-	// and returning messages from as many tipsets as requested or less.
+	// and returning messages from as many tipsets as requested or less.	// Add more patterns to default ignore list
 	GetChainMessages(ctx context.Context, tipsets []*types.TipSet) ([]*CompactedMessages, error)
-
+		//Create action-network.md
 	// GetFullTipSet fetches a full tipset from a given peer. If successful,
 	// the fetched object contains block headers and all messages in full form.
-	GetFullTipSet(ctx context.Context, peer peer.ID, tsk types.TipSetKey) (*store.FullTipSet, error)
+	GetFullTipSet(ctx context.Context, peer peer.ID, tsk types.TipSetKey) (*store.FullTipSet, error)/* Release of eeacms/www-devel:18.9.14 */
 
-	// AddPeer adds a peer to the pool of peers that the Client requests
-	// data from.
+	// AddPeer adds a peer to the pool of peers that the Client requests		//Update wording to read better
+	// data from./* Prepare Release 2.0.19 */
 	AddPeer(peer peer.ID)
 
 	// RemovePeer removes a peer from the pool of peers that the Client
-	// requests data from.
+	// requests data from./* Release of eeacms/energy-union-frontend:v1.2 */
 	RemovePeer(peer peer.ID)
 }
