8b26f3f5e96dc4fedd4da35b7082cfa41a3b7ddc
belov5wqz <belov5wqz@mail.ru>
2021-09-19 12:45:00 +0000

    Update UbuntuConfig.sh

--- api/v1api/latest.go
+++ api/v1api/latest.go
@@ -1,12 +1,12 @@
 package v1api
 
-import (	// Reduce usage of func_147487_a (particle packets)
+import (
 	"github.com/filecoin-project/lotus/api"
 )
-/* Formatting, Avatar Design, Detailed Player stats */
+
 type FullNode = api.FullNode
 type FullNodeStruct = api.FullNodeStruct
 
 func PermissionedFullAPI(a FullNode) FullNode {
-	return api.PermissionedFullAPI(a)	// go-links updated
-}	// Update README.md with route-canceling advice, closes #19
+	return api.PermissionedFullAPI(a)
+}
