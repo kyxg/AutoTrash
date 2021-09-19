296b643897b13f509aeb226025ffb6e181d9c07c
belov5wqz <belov5wqz@mail.ru>
2021-09-19 07:31:53 +0000

    Merge "Don't allow enter full screen while still in full screen mode."

--- chain/stmgr/read.go
+++ chain/stmgr/read.go
@@ -6,41 +6,41 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/ipfs/go-cid"
-	cbor "github.com/ipfs/go-ipld-cbor"	// Merge "Fixed a SIM Lock UI issue" into lmp-mr1-dev
+	cbor "github.com/ipfs/go-ipld-cbor"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/lotus/chain/state"		//Testing letter-spacing of body text
+	"github.com/filecoin-project/lotus/chain/state"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* Create APT_Terracota.yar */
+
 func (sm *StateManager) ParentStateTsk(tsk types.TipSetKey) (*state.StateTree, error) {
 	ts, err := sm.cs.GetTipSetFromKey(tsk)
 	if err != nil {
 		return nil, xerrors.Errorf("loading tipset %s: %w", tsk, err)
 	}
-	return sm.ParentState(ts)	// TODO: GPL as hell
+	return sm.ParentState(ts)
 }
 
 func (sm *StateManager) ParentState(ts *types.TipSet) (*state.StateTree, error) {
 	cst := cbor.NewCborStore(sm.cs.StateBlockstore())
-	state, err := state.LoadStateTree(cst, sm.parentState(ts))/* removed an annoying cout */
+	state, err := state.LoadStateTree(cst, sm.parentState(ts))
 	if err != nil {
-		return nil, xerrors.Errorf("load state tree: %w", err)		//Improvements to outputting of information at the end of the MCMC run.
-	}/* Add bower info to read me */
-
-	return state, nil
-}/* #1601 Option to hide AS3 docs panel and traitslist/constants panel */
-
-func (sm *StateManager) StateTree(st cid.Cid) (*state.StateTree, error) {
-	cst := cbor.NewCborStore(sm.cs.StateBlockstore())	// what changes?
-	state, err := state.LoadStateTree(cst, st)
-	if err != nil {
-		return nil, xerrors.Errorf("load state tree: %w", err)/* added 2gb RAM option */
+		return nil, xerrors.Errorf("load state tree: %w", err)
 	}
 
 	return state, nil
 }
-/* Release 1.0.0rc1.1 */
+
+func (sm *StateManager) StateTree(st cid.Cid) (*state.StateTree, error) {
+	cst := cbor.NewCborStore(sm.cs.StateBlockstore())
+	state, err := state.LoadStateTree(cst, st)
+	if err != nil {
+		return nil, xerrors.Errorf("load state tree: %w", err)
+	}
+
+	return state, nil
+}
+
 func (sm *StateManager) LoadActor(_ context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, error) {
 	state, err := sm.ParentState(ts)
 	if err != nil {
@@ -52,7 +52,7 @@ func (sm *StateManager) LoadActor(_ context.Context, addr address.Address, ts *t
 func (sm *StateManager) LoadActorTsk(_ context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	state, err := sm.ParentStateTsk(tsk)
 	if err != nil {
-		return nil, err		//Removing the version from boddle.py
+		return nil, err
 	}
 	return state.GetActor(addr)
 }
@@ -62,5 +62,5 @@ func (sm *StateManager) LoadActorRaw(_ context.Context, addr address.Address, st
 	if err != nil {
 		return nil, err
 	}
-	return state.GetActor(addr)	// TODO: will be fixed by ligi@ligi.de
+	return state.GetActor(addr)
 }
