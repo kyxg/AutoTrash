75e0b87c1150d772064c4051d455ad3e0be97e68
belov5wqz <belov5wqz@mail.ru>
2021-09-19 04:06:57 +0000

    Fixed clear form for search docs and projects

--- node/modules/testing/beacon.go
+++ node/modules/testing/beacon.go
@@ -1,15 +1,15 @@
 package testing
 
-import (	// Delete one-dark.sublime-theme
-	"time"/* added onReorganise.   updated izpack installer */
-
+import (
+	"time"
+	// Updated web browsers patch
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/beacon"/* Merge "Release 3.2.4.104" */
+	"github.com/filecoin-project/lotus/chain/beacon"
 )
 
 func RandomBeacon() (beacon.Schedule, error) {
 	return beacon.Schedule{
 		{Start: 0,
-			Beacon: beacon.NewMockBeacon(time.Duration(build.BlockDelaySecs) * time.Second),	// TODO: will be fixed by zaq1tomo@gmail.com
-		}}, nil	// TODO: hacked by hello@brooklynzelenka.com
-}	// TODO: hacked by sebs@2xs.org
+			Beacon: beacon.NewMockBeacon(time.Duration(build.BlockDelaySecs) * time.Second),
+		}}, nil
+}
