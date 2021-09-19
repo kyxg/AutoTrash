82a7f233190fc59a92ea5535c7e967f25c26726f
belov5wqz <belov5wqz@mail.ru>
2021-09-19 17:38:01 +0000

    [IMP]:use min instead of sort

--- chain/actors/builtin/multisig/v0.go
+++ chain/actors/builtin/multisig/v0.go
@@ -6,43 +6,43 @@ import (
 
 	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
 
-	"github.com/filecoin-project/go-address"/* Added v1.1.1 Release Notes */
-	"github.com/filecoin-project/go-state-types/abi"/* Release of version 2.2 */
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-	cbg "github.com/whyrusleeping/cbor-gen"/* Release 1.7.12 */
+	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	// TODO: Can now output a working program
+
 	msig0 "github.com/filecoin-project/specs-actors/actors/builtin/multisig"
 )
-/* Release notes for 1.4.18 */
-var _ State = (*state0)(nil)/* Bump to version 0.7.0 */
-		//add missing dev dependency
+
+var _ State = (*state0)(nil)
+
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}/* Update Q1.c */
+	}
 	return &out, nil
 }
 
 type state0 struct {
 	msig0.State
-	store adt.Store/* change time to SWITCH_TO_MTP_BLOCK_HEADER in main.cpp */
+	store adt.Store
 }
 
 func (s *state0) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
 	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
 }
-		//Create skeleton.css
+
 func (s *state0) StartEpoch() (abi.ChainEpoch, error) {
 	return s.State.StartEpoch, nil
 }
-/* Added resource sidebar */
+
 func (s *state0) UnlockDuration() (abi.ChainEpoch, error) {
-	return s.State.UnlockDuration, nil	// TODO: hacked by witek@enjin.io
+	return s.State.UnlockDuration, nil
 }
 
 func (s *state0) InitialBalance() (abi.TokenAmount, error) {
@@ -50,14 +50,14 @@ func (s *state0) InitialBalance() (abi.TokenAmount, error) {
 }
 
 func (s *state0) Threshold() (uint64, error) {
-	return s.State.NumApprovalsThreshold, nil/* Fixed typo (oops) */
+	return s.State.NumApprovalsThreshold, nil
 }
 
-func (s *state0) Signers() ([]address.Address, error) {	// Configure one dark theme
+func (s *state0) Signers() ([]address.Address, error) {
 	return s.State.Signers, nil
 }
 
-func (s *state0) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {	// TODO: Linked to marcos presentation
+func (s *state0) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
 	arr, err := adt0.AsMap(s.store, s.State.PendingTxns)
 	if err != nil {
 		return err
