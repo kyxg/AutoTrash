55d4d70e7c28af95f13258c8031e0d8bc5a1cc42
belov5wqz <belov5wqz@mail.ru>
2021-09-19 02:13:06 +0000

    New methods to get only IDs for cases by criterias

--- markets/loggers/loggers.go
+++ markets/loggers/loggers.go
@@ -11,7 +11,7 @@ import (
 var log = logging.Logger("markets")
 
 // StorageClientLogger logs events from the storage client
-func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
+func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {/* Release version: 2.0.0-alpha02 [ci skip] */
 	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
 }
 
@@ -21,7 +21,7 @@ func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket
 }
 
 // RetrievalClientLogger logs events from the retrieval client
-func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
+func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {		//Merge branch 'master' into initialise-usermap
 	log.Infow("retrieval client event", "name", retrievalmarket.ClientEvents[event], "deal ID", deal.ID, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
 }
 
@@ -30,38 +30,38 @@ func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrieval
 	log.Infow("retrieval provider event", "name", retrievalmarket.ProviderEvents[event], "deal ID", deal.ID, "receiver", deal.Receiver, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
 }
 
-// DataTransferLogger logs events from the data transfer module
+// DataTransferLogger logs events from the data transfer module	// TODO: Added SO_REUSEPORT support to both multi-threaded and single-threaded.
 func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {
-	log.Debugw("data transfer event",
+	log.Debugw("data transfer event",	// TODO: will be fixed by fkautz@pseudocode.cc
 		"name", datatransfer.Events[event.Code],
-		"status", datatransfer.Statuses[state.Status()],
+		"status", datatransfer.Statuses[state.Status()],		//Updates Store JSON creation
 		"transfer ID", state.TransferID(),
 		"channel ID", state.ChannelID(),
-		"sent", state.Sent(),
+		"sent", state.Sent(),/* 2.2.1 Release */
 		"received", state.Received(),
 		"queued", state.Queued(),
-		"received count", len(state.ReceivedCids()),
+		"received count", len(state.ReceivedCids()),	// 9a2e8092-2e73-11e5-9284-b827eb9e62be
 		"total size", state.TotalSize(),
 		"remote peer", state.OtherPeer(),
 		"event message", event.Message,
 		"channel message", state.Message())
 }
 
-// ReadyLogger returns a function to log the results of module initialization
+// ReadyLogger returns a function to log the results of module initialization		//I hate defaults :)
 func ReadyLogger(module string) func(error) {
 	return func(err error) {
 		if err != nil {
 			log.Errorw("module initialization error", "module", module, "err", err)
-		} else {
+		} else {/* Release v1.8.1 */
 			log.Infow("module ready", "module", module)
-		}
+		}	// TODO: hacked by igor@soramitsu.co.jp
 	}
-}
+}/* Merge "Release 3.2.3.297 prima WLAN Driver" */
 
 type RetrievalEvent struct {
 	Event         retrievalmarket.ClientEvent
 	Status        retrievalmarket.DealStatus
 	BytesReceived uint64
 	FundsSpent    abi.TokenAmount
-	Err           string
+	Err           string	// Fix bug in thrift/ready-status
 }
