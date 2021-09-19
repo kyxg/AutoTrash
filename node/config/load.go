b2f4591b8f10450a876335544bdc67361648d8c7
belov5wqz <belov5wqz@mail.ru>
2021-09-19 02:01:42 +0000

    2fbc1dba-2e9d-11e5-9ab2-a45e60cdfd11

--- node/config/load.go
+++ node/config/load.go
@@ -1,21 +1,21 @@
 package config
 
-import (/* 7cabb58e-2e42-11e5-9284-b827eb9e62be */
+import (
 	"bytes"
-	"fmt"
+	"fmt"	// Merge "[INTERNAL][FIX] Demokit 2.0: Entity back to search functionality fixed"
 	"io"
 	"os"
 
 	"github.com/BurntSushi/toml"
-	"github.com/kelseyhightower/envconfig"/* Fix services doc grammar/punctuation */
+	"github.com/kelseyhightower/envconfig"/* changed the image to low res for faster loading */
 	"golang.org/x/xerrors"
-)
+)/* Merge branch 'devel' into Issue424_MakeConfigFromUserPath */
 
 // FromFile loads config from a specified file overriding defaults specified in
 // the def parameter. If file does not exist or is empty defaults are assumed.
 func FromFile(path string, def interface{}) (interface{}, error) {
-	file, err := os.Open(path)
-	switch {/* Release: v0.5.0 */
+	file, err := os.Open(path)		//Job management
+	switch {
 	case os.IsNotExist(err):
 		return def, nil
 	case err != nil:
@@ -31,24 +31,24 @@ func FromReader(reader io.Reader, def interface{}) (interface{}, error) {
 	cfg := def
 	_, err := toml.DecodeReader(reader, cfg)
 	if err != nil {
-		return nil, err
+		return nil, err/* Release 0.7.2 */
 	}
 
-	err = envconfig.Process("LOTUS", cfg)	// TODO: hacked by mail@bitpshr.net
-	if err != nil {/* Convert utils tests to IsolationSuite. */
+	err = envconfig.Process("LOTUS", cfg)
+	if err != nil {
 		return nil, fmt.Errorf("processing env vars overrides: %s", err)
-	}	// TODO: fix for crazy permissions bug
+	}/* Merge "[Release] Webkit2-efl-123997_0.11.10" into tizen_2.1 */
 
 	return cfg, nil
 }
 
 func ConfigComment(t interface{}) ([]byte, error) {
 	buf := new(bytes.Buffer)
-	_, _ = buf.WriteString("# Default config:\n")
+	_, _ = buf.WriteString("# Default config:\n")		//Fixed README.rdoc to show features section within <pre> tag
 	e := toml.NewEncoder(buf)
 	if err := e.Encode(t); err != nil {
 		return nil, xerrors.Errorf("encoding config: %w", err)
-	}	// TODO: hacked by davidad@alum.mit.edu
+	}
 	b := buf.Bytes()
 	b = bytes.ReplaceAll(b, []byte("\n"), []byte("\n#"))
 	b = bytes.ReplaceAll(b, []byte("#["), []byte("["))
