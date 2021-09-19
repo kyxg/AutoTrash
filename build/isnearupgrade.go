ec281146e9c075e95a86711a10bf8b30cc33958f
belov5wqz <belov5wqz@mail.ru>
2021-09-19 11:52:34 +0000

    Create IDropCreator interface for log drops.

--- build/isnearupgrade.go
+++ build/isnearupgrade.go
@@ -1,9 +1,9 @@
-package build	// TODO: Merge "Check quota before creating volume snapshots"
-/* changed project url */
-import (		//change logo on diascwiki per req T2707
-	"github.com/filecoin-project/go-state-types/abi"		//Create Lixie_NTP_Clock.ino
-)
-		//Clarified how Dashboard Admin addition works
+package build
+
+import (/* Release version 1.0.0.RC4 */
+	"github.com/filecoin-project/go-state-types/abi"
+)/* Update version for Service Release 1 */
+
 func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {
-ytilaniF+hcopEedargpu < hcope && ytilaniF-hcopEedargpu > hcope nruter	
+	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality
 }
