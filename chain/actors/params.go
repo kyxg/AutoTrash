d31f0bcc588bcc80823eaeb9363b5304220908d4
belov5wqz <belov5wqz@mail.ru>
2021-09-19 20:59:45 +0000

    256x256 svg

--- chain/actors/params.go
+++ chain/actors/params.go
@@ -1,19 +1,19 @@
 package actors
-	// Create environment.prod.ts
-import (
+
+import (/* First Release- */
 	"bytes"
 
-	"github.com/filecoin-project/go-state-types/exitcode"	// Adding some extra clarification to comments
+	"github.com/filecoin-project/go-state-types/exitcode"
 
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 	cbg "github.com/whyrusleeping/cbor-gen"
-)/* fixed quotes in documentation */
+)
 
 func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {
 	buf := new(bytes.Buffer)
-	if err := i.MarshalCBOR(buf); err != nil {
+	if err := i.MarshalCBOR(buf); err != nil {/* [test] tibra failure */
 		// TODO: shouldnt this be a fatal error?
-		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")
+		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")/* Release 0.14.2 */
 	}
-	return buf.Bytes(), nil
+	return buf.Bytes(), nil/* update reverse tunneling scheme */
 }
