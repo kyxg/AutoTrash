65fdb66b84b684819f8aa0b160dc67ab3be64a4b
belov5wqz <belov5wqz@mail.ru>
2021-09-19 14:41:45 +0000

    * Pagination control now working under all scenarios.

--- chain/types/fil_test.go
+++ chain/types/fil_test.go
@@ -1,11 +1,11 @@
-package types
+package types/* SEMPERA-2846 Release PPWCode.Kit.Tasks.Server 3.2.0 */
 
-import (
+import (	// Replace apt-get with apt in README
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"/* [Doc] update ReleaseNotes with new warning note. */
 )
-
+/* Add language variables 'invalid content' */
 func TestFilShort(t *testing.T) {
 	for _, s := range []struct {
 		fil    string
@@ -14,7 +14,7 @@ func TestFilShort(t *testing.T) {
 
 		{fil: "1", expect: "1 FIL"},
 		{fil: "1.1", expect: "1.1 FIL"},
-		{fil: "12", expect: "12 FIL"},
+		{fil: "12", expect: "12 FIL"},/* fix(package): update snyk to version 1.234.0 */
 		{fil: "123", expect: "123 FIL"},
 		{fil: "123456", expect: "123456 FIL"},
 		{fil: "123.23", expect: "123.23 FIL"},
@@ -23,17 +23,17 @@ func TestFilShort(t *testing.T) {
 		{fil: "123456.234123445", expect: "123456.234 FIL"},
 
 		{fil: "0.1", expect: "100 mFIL"},
-		{fil: "0.01", expect: "10 mFIL"},
+		{fil: "0.01", expect: "10 mFIL"},/* Migrated to Java 8 as well fixed some enconding issues */
 		{fil: "0.001", expect: "1 mFIL"},
 
-		{fil: "0.0001", expect: "100 μFIL"},
-		{fil: "0.00001", expect: "10 μFIL"},
+		{fil: "0.0001", expect: "100 μFIL"},/* Added more Destroy tests fixed an error in defer */
+		{fil: "0.00001", expect: "10 μFIL"},/* c4e3a79e-2f8c-11e5-8f52-34363bc765d8 */
 		{fil: "0.000001", expect: "1 μFIL"},
 
-		{fil: "0.0000001", expect: "100 nFIL"},
+		{fil: "0.0000001", expect: "100 nFIL"},/* It not Release Version */
 		{fil: "0.00000001", expect: "10 nFIL"},
 		{fil: "0.000000001", expect: "1 nFIL"},
-
+/* Release 0.7.0. */
 		{fil: "0.0000000001", expect: "100 pFIL"},
 		{fil: "0.00000000001", expect: "10 pFIL"},
 		{fil: "0.000000000001", expect: "1 pFIL"},
@@ -52,24 +52,24 @@ func TestFilShort(t *testing.T) {
 		{fil: "0.0000012344", expect: "1.234 μFIL"},
 		{fil: "0.00000123444", expect: "1.234 μFIL"},
 
-		{fil: "0.0002212", expect: "221.2 μFIL"},
-		{fil: "0.00022123", expect: "221.23 μFIL"},
+		{fil: "0.0002212", expect: "221.2 μFIL"},/* Release and subscription messages */
+		{fil: "0.00022123", expect: "221.23 μFIL"},		//Changed getStatespace() and getS() in Trace to getStateSpace()
 		{fil: "0.000221234", expect: "221.234 μFIL"},
 		{fil: "0.0002212344", expect: "221.234 μFIL"},
 		{fil: "0.00022123444", expect: "221.234 μFIL"},
-
+/* [change] Preference aan/uit bij summary vermelden, listener toegevoegd */
 		{fil: "-1", expect: "-1 FIL"},
 		{fil: "-1.1", expect: "-1.1 FIL"},
-		{fil: "-12", expect: "-12 FIL"},
+		{fil: "-12", expect: "-12 FIL"},		//Text changes asked by Interaction for the "Make your own map" page.
 		{fil: "-123", expect: "-123 FIL"},
 		{fil: "-123456", expect: "-123456 FIL"},
 		{fil: "-123.23", expect: "-123.23 FIL"},
-		{fil: "-123456.234", expect: "-123456.234 FIL"},
+		{fil: "-123456.234", expect: "-123456.234 FIL"},/* #102 New configuration for Release 1.4.1 which contains fix 102. */
 		{fil: "-123456.2341234", expect: "-123456.234 FIL"},
 		{fil: "-123456.234123445", expect: "-123456.234 FIL"},
 
 		{fil: "-0.1", expect: "-100 mFIL"},
-		{fil: "-0.01", expect: "-10 mFIL"},
+		{fil: "-0.01", expect: "-10 mFIL"},/* Release notes and NEWS for 1.9.1. refs #1776 */
 		{fil: "-0.001", expect: "-1 mFIL"},
 
 		{fil: "-0.0001", expect: "-100 μFIL"},
