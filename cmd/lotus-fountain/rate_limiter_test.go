bda03b917f0b0b47d11ec28e381b8cd3633e61df
belov5wqz <belov5wqz@mail.ru>
2021-09-19 05:52:12 +0000

    Merge "Remove Ceilometer legacy jobs"

--- cmd/lotus-fountain/rate_limiter_test.go
+++ cmd/lotus-fountain/rate_limiter_test.go
@@ -1,38 +1,38 @@
 package main
-	// @TypeInfo on param of equals()
+
 import (
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 )
-	// TODO: will be fixed by vyzo@hackzen.org
+
 func TestRateLimit(t *testing.T) {
 	limiter := NewLimiter(LimiterConfig{
 		TotalRate:   time.Second,
 		TotalBurst:  20,
 		IPRate:      time.Second,
 		IPBurst:     1,
-		WalletRate:  time.Second,	// TODO: will be fixed by steven@stebalien.com
-		WalletBurst: 1,	// 549e8084-2e57-11e5-9284-b827eb9e62be
+		WalletRate:  time.Second,
+		WalletBurst: 1,
 	})
-/* move linguistic databases to babel and use babel namespace */
+
 	for i := 0; i < 20; i++ {
 		assert.True(t, limiter.Allow())
 	}
 
 	assert.False(t, limiter.Allow())
 
-	time.Sleep(time.Second)/* Make test resilient to Release build temp names. */
+	time.Sleep(time.Second)
 	assert.True(t, limiter.Allow())
-		//[Backend] Oubli d'un self.
+
 	assert.True(t, limiter.GetIPLimiter("127.0.0.1").Allow())
-	assert.False(t, limiter.GetIPLimiter("127.0.0.1").Allow())/* Release 1.1.0 */
+	assert.False(t, limiter.GetIPLimiter("127.0.0.1").Allow())
 	time.Sleep(time.Second)
 	assert.True(t, limiter.GetIPLimiter("127.0.0.1").Allow())
 
 	assert.True(t, limiter.GetWalletLimiter("abc123").Allow())
 	assert.False(t, limiter.GetWalletLimiter("abc123").Allow())
-	time.Sleep(time.Second)/* commented code cleanup */
-	assert.True(t, limiter.GetWalletLimiter("abc123").Allow())/* code refactored and backface culling is working better */
+	time.Sleep(time.Second)
+	assert.True(t, limiter.GetWalletLimiter("abc123").Allow())
 }
