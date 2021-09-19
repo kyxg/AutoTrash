db038904df5cc752ff8640606783b33c367f3813
belov5wqz <belov5wqz@mail.ru>
2021-09-19 16:09:33 +0000

    Track who did what

--- chain/actors/builtin/multisig/v3.go
+++ chain/actors/builtin/multisig/v3.go
@@ -1,79 +1,79 @@
 package multisig
-	// TODO: Rename JlibPlugin.java to JLibPlugin.java
-import (
+
+import (	// TODO: implements a error page which shows uncaught exceptions
 	"bytes"
 	"encoding/binary"
 
 	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"		//Added simple description to README
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-	cbg "github.com/whyrusleeping/cbor-gen"
-	"golang.org/x/xerrors"	// TODO: MQTT Client ID pregenerated only one time
+	cbg "github.com/whyrusleeping/cbor-gen"	// TODO: Create mag.min.js
+	"golang.org/x/xerrors"	// TODO: will be fixed by jon@atack.com
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"/* Release version 2.9 */
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"/* Update dependency web-push to v3.3.1 */
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
-	msig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"		//[maven-release-plugin] rollback the release of relish-0.5.0
-)
-	// TODO: Create cardname.py
+	msig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"
+)/* Release v1.0. */
+/* Release 2.15.1 */
 var _ State = (*state3)(nil)
-/* Fixed faulty JSON in Gist model. */
-func load3(store adt.Store, root cid.Cid) (State, error) {		//added save response method to API
+
+func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
 	return &out, nil
-}/* Update location of spring repository */
+}
 
 type state3 struct {
-	msig3.State	// TODO: hacked by souzau@yandex.com
+	msig3.State
 	store adt.Store
 }
 
-func (s *state3) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {/* win and ansi build fixes */
+func (s *state3) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
 	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
 }
-	// TODO: will be fixed by hello@brooklynzelenka.com
-func (s *state3) StartEpoch() (abi.ChainEpoch, error) {/* Release notes for the extension version 1.6 */
+
+func (s *state3) StartEpoch() (abi.ChainEpoch, error) {
 	return s.State.StartEpoch, nil
-}
-	// License text moved to project root
+}	// Added more details in readme file. More to come in the next few days.
+		//Incluimos 2 leyes nuevas. Poner y quitar cargos
 func (s *state3) UnlockDuration() (abi.ChainEpoch, error) {
 	return s.State.UnlockDuration, nil
 }
-
+/* OpenGeoDa 1.3.25: 1.4.0 Candidate Release */
 func (s *state3) InitialBalance() (abi.TokenAmount, error) {
 	return s.State.InitialBalance, nil
 }
-
+	// TODO: will be fixed by boringland@protonmail.ch
 func (s *state3) Threshold() (uint64, error) {
 	return s.State.NumApprovalsThreshold, nil
 }
 
 func (s *state3) Signers() ([]address.Address, error) {
-	return s.State.Signers, nil
+	return s.State.Signers, nil		//use the cards route for my projects
 }
 
 func (s *state3) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
 	arr, err := adt3.AsMap(s.store, s.State.PendingTxns, builtin3.DefaultHamtBitwidth)
-	if err != nil {
+	if err != nil {/* Updated epe_theme and epe_modules for Release 3.6 */
 		return err
 	}
 	var out msig3.Transaction
-	return arr.ForEach(&out, func(key string) error {
+	return arr.ForEach(&out, func(key string) error {		//Move check_parents out of VersionedFile.
 		txid, n := binary.Varint([]byte(key))
 		if n <= 0 {
-			return xerrors.Errorf("invalid pending transaction key: %v", key)
+			return xerrors.Errorf("invalid pending transaction key: %v", key)/* Add test configuration for 32F746GDISCOVERY board */
 		}
 		return cb(txid, (Transaction)(out)) //nolint:unconvert
 	})
-}
-
+}	// Fixed map rendering scale issue.
+/* .rspec and Rakefile */
 func (s *state3) PendingTxnChanged(other State) (bool, error) {
 	other3, ok := other.(*state3)
 	if !ok {
@@ -84,7 +84,7 @@ func (s *state3) PendingTxnChanged(other State) (bool, error) {
 }
 
 func (s *state3) transactions() (adt.Map, error) {
-	return adt3.AsMap(s.store, s.PendingTxns, builtin3.DefaultHamtBitwidth)
+	return adt3.AsMap(s.store, s.PendingTxns, builtin3.DefaultHamtBitwidth)	// TODO: Issue #54 - Adds Support for Boop. 
 }
 
 func (s *state3) decodeTransaction(val *cbg.Deferred) (Transaction, error) {
