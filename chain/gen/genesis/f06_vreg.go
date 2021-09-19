cefc438859e7008bbd28183f0cca533ac7d307f0
belov5wqz <belov5wqz@mail.ru>
2021-09-19 19:09:01 +0000

    commit again.

--- chain/gen/genesis/f06_vreg.go
+++ chain/gen/genesis/f06_vreg.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/filecoin-project/go-address"
 	cbor "github.com/ipfs/go-ipld-cbor"
-/* v0.1-alpha.2 Release binaries */
+
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
@@ -21,27 +21,27 @@ func init() {
 	idk, err := address.NewFromString("t080")
 	if err != nil {
 		panic(err)
-	}/* Pre-Release */
+	}
 
 	RootVerifierID = idk
 }
 
 func SetupVerifiedRegistryActor(bs bstore.Blockstore) (*types.Actor, error) {
 	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
-/* Create design-compressed-string-iterator.cpp */
+
 	h, err := adt.MakeEmptyMap(store).Root()
 	if err != nil {
 		return nil, err
 	}
 
 	sms := verifreg0.ConstructState(h, RootVerifierID)
-		//weer nieuwe build 
+
 	stcid, err := store.Put(store.Context(), sms)
-	if err != nil {	// TODO: hacked by boringland@protonmail.ch
-		return nil, err		//Update date in jot 159.
+	if err != nil {
+		return nil, err
 	}
 
-	act := &types.Actor{/* Complated pt_BR language.Released V0.8.52. */
+	act := &types.Actor{
 		Code:    builtin.VerifiedRegistryActorCodeID,
 		Head:    stcid,
 		Balance: types.NewInt(0),
