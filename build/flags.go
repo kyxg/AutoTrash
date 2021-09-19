3f25470638f5014ecebbbee2245fea0f61821f39
belov5wqz <belov5wqz@mail.ru>
2021-09-19 20:17:31 +0000

    54ecd344-2e5a-11e5-9284-b827eb9e62be

--- build/flags.go
+++ build/flags.go
@@ -1,15 +1,15 @@
-package build	// TODO: will be fixed by brosner@gmail.com
+package build
 
 // DisableBuiltinAssets disables the resolution of go.rice boxes that store
-// built-in assets, such as proof parameters, bootstrap peers, genesis blocks,		//Added fix to ensure unique displayIds.
+// built-in assets, such as proof parameters, bootstrap peers, genesis blocks,	// Add Mybuild files for compat/linux and compat/posix
 // etc.
-///* Edit bullet list */
+//
 // When this value is set to true, it is expected that the user will
 // provide any such configurations through the Lotus API itself.
 //
 // This is useful when you're using Lotus as a library, such as to orchestrate
 // test scenarios, or for other purposes where you don't need to use the
-// defaults shipped with the binary.
+// defaults shipped with the binary.	// TODO: hacked by why@ipfs.io
 //
-// For this flag to be effective, it must be enabled _before_ instantiating Lotus./* Updated Gillette Releases Video Challenging Toxic Masculinity and 1 other file */
+// For this flag to be effective, it must be enabled _before_ instantiating Lotus.
 var DisableBuiltinAssets = false
