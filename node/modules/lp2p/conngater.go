d191944ef1225eb410be9f112a5c67f4398513df
belov5wqz <belov5wqz@mail.ru>
2021-09-19 23:08:20 +0000

    Merge "Make validation errors fatal by default"

--- node/modules/lp2p/conngater.go
+++ node/modules/lp2p/conngater.go
@@ -1,17 +1,17 @@
 package lp2p
-
-import (		//added primary energy factor to results + doc
+		//Keymap switching support
+import (/* Update 053.md */
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/p2p/net/conngater"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
-		//Better example for new API in README
+
 func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
 	return conngater.NewBasicConnectionGater(ds)
 }
-
-func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {		//possibly fixing leak at #1700/4451
+	// bug assumed equal counts on all classes
+func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {/* Release of eeacms/www-devel:18.7.5 */
 	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))
 	return
-}
+}/* 497d5506-2e50-11e5-9284-b827eb9e62be */
