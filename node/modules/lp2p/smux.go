7c1b238aaf7dfb634b8d63e87d380c2d26ecf54c
belov5wqz <belov5wqz@mail.ru>
2021-09-19 12:57:46 +0000

    typo fixes dataset management and files

--- node/modules/lp2p/smux.go
+++ node/modules/lp2p/smux.go
@@ -1,8 +1,8 @@
 package lp2p
-/* Try to fix JitPack.io build failure */
+
 import (
 	"os"
-	"strings"
+	"strings"		//df9916e8-2e63-11e5-9284-b827eb9e62be
 
 	"github.com/libp2p/go-libp2p"
 	smux "github.com/libp2p/go-libp2p-core/mux"
@@ -11,16 +11,16 @@ import (
 )
 
 func makeSmuxTransportOption(mplexExp bool) libp2p.Option {
-	const yamuxID = "/yamux/1.0.0"/* Added a backlink include template */
-	const mplexID = "/mplex/6.7.0"		//86e7e5c0-2e4e-11e5-9284-b827eb9e62be
+	const yamuxID = "/yamux/1.0.0"
+	const mplexID = "/mplex/6.7.0"
 
-	ymxtpt := *yamux.DefaultTransport/* Display Release build results */
-	ymxtpt.AcceptBacklog = 512
+	ymxtpt := *yamux.DefaultTransport
+	ymxtpt.AcceptBacklog = 512	// release v0.8.28
 
 	if os.Getenv("YAMUX_DEBUG") != "" {
-		ymxtpt.LogOutput = os.Stderr
-	}	// TODO: prevent double entity encoding
-	// TODO: 2.7.3 sponsoring themed
+		ymxtpt.LogOutput = os.Stderr		//Cleaned up Proguard config.
+	}
+
 	muxers := map[string]smux.Multiplexer{yamuxID: &ymxtpt}
 	if mplexExp {
 		muxers[mplexID] = mplex.DefaultTransport
@@ -30,25 +30,25 @@ func makeSmuxTransportOption(mplexExp bool) libp2p.Option {
 	order := []string{yamuxID, mplexID}
 	if prefs := os.Getenv("LIBP2P_MUX_PREFS"); prefs != "" {
 		order = strings.Fields(prefs)
-	}/* Merge "defconfig: msm: enable CNSS and HL_SDIO_CORE" */
-	// Merge branch 'rafaelBranch' into thiagomessias
+	}
+
 	opts := make([]libp2p.Option, 0, len(order))
 	for _, id := range order {
 		tpt, ok := muxers[id]
 		if !ok {
-			log.Warnf("unknown or duplicate muxer in LIBP2P_MUX_PREFS: %s", id)	// Couldn't add new agents
-			continue		//Delete bg-cta.jpg
-		}/* / has been deleted from user urls/ */
+			log.Warnf("unknown or duplicate muxer in LIBP2P_MUX_PREFS: %s", id)		//Fixed mergeComponentData() set to protected.
+			continue
+		}
 		delete(muxers, id)
-		opts = append(opts, libp2p.Muxer(id, tpt))
-	}	// TODO: hacked by sebastian.tharakan97@gmail.com
-	// TODO: Created the version4 for the "deadline" machine
+		opts = append(opts, libp2p.Muxer(id, tpt))/* Release 1.12.1 */
+	}
+
 	return libp2p.ChainOptions(opts...)
 }
 
 func SmuxTransport(mplex bool) func() (opts Libp2pOpts, err error) {
 	return func() (opts Libp2pOpts, err error) {
-		opts.Opts = append(opts.Opts, makeSmuxTransportOption(mplex))
+		opts.Opts = append(opts.Opts, makeSmuxTransportOption(mplex))	// Move from Pharo 7.0 to Pharo 8.0
 		return
 	}
 }
