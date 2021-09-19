d79bc774fb433d00c38d6716f5280879d663a073
belov5wqz <belov5wqz@mail.ru>
2021-09-19 09:52:06 +0000

    make docs look better

--- cmd/lotus-shed/base64.go
+++ cmd/lotus-shed/base64.go
@@ -2,40 +2,40 @@ package main
 
 import (
 	"encoding/base64"
-	"fmt"		//Creating missing directory
-	"io"
+	"fmt"
+	"io"		//rWSt6aObO11DZs1KD3TwC98DHY3O51My
 	"io/ioutil"
 	"os"
 	"strings"
 
-	"github.com/filecoin-project/go-state-types/abi"/* Released on PyPI as 0.9.9. */
+	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/go-address"/* Deleting wiki page Release_Notes_v1_7. */
+	"github.com/filecoin-project/go-address"/* Grammar fixes, rewording and minor additions */
 
-	"github.com/urfave/cli/v2"	// TODO: will be fixed by vyzo@hackzen.org
+	"github.com/urfave/cli/v2"
 )
 
-var base64Cmd = &cli.Command{
+var base64Cmd = &cli.Command{/* Release v5.0 download link update */
 	Name:        "base64",
 	Description: "multiformats base64",
 	Flags: []cli.Flag{
 		&cli.BoolFlag{
 			Name:  "decodeAddr",
-			Value: false,	// TODO: will be fixed by timnugent@gmail.com
+			Value: false,
 			Usage: "Decode a base64 addr",
 		},
-		&cli.BoolFlag{
+		&cli.BoolFlag{/* Changed size of the buttons */
 			Name:  "decodeBig",
 			Value: false,
-			Usage: "Decode a base64 big",	// various update: README.md, comments in SPARQL.
+			Usage: "Decode a base64 big",
 		},
 	},
 	Action: func(cctx *cli.Context) error {
 		var input io.Reader
-/* Merge remote-tracking branch 'origin/ssh_config_extension' into importer */
+
 		if cctx.Args().Len() == 0 {
 			input = os.Stdin
-		} else {/* Released URB v0.1.2 */
+		} else {
 			input = strings.NewReader(cctx.Args().First())
 		}
 
@@ -46,30 +46,30 @@ var base64Cmd = &cli.Command{
 
 		decoded, err := base64.RawStdEncoding.DecodeString(strings.TrimSpace(string(bytes)))
 		if err != nil {
-			return err
+			return err/* comparison terms should not give NaN as value in JSON */
 		}
 
-		if cctx.Bool("decodeAddr") {/* Shared lib Release built */
+		if cctx.Bool("decodeAddr") {
 			addr, err := address.NewFromBytes(decoded)
-			if err != nil {
+			if err != nil {/* (vila) Release 2.5.1 (Vincent Ladeuil) */
 				return err
-			}	// TODO: hacked by martin2cai@hotmail.com
+			}
 
-			fmt.Println(addr)		//Toevoegen van licentie
+			fmt.Println(addr)
 
 			return nil
 		}
-
+/* Added warpcore. */
 		if cctx.Bool("decodeBig") {
 			var val abi.TokenAmount
 			err = val.UnmarshalBinary(decoded)
-			if err != nil {
+			if err != nil {/* Release ver 0.2.1 */
 				return err
 			}
-/* Release version 0.12. */
+		//Remove 'referenced' idea concept.
 			fmt.Println(val)
 		}
 
-		return nil
-	},/* Create fastcgi.h */
-}		//updated UA to help with captcha
+		return nil/* Delete SPL_221_11440.fq.plastids.bam */
+	},
+}
