d1f8eb8c2f291f64f4f70c041159f4fc51650bef
belov5wqz <belov5wqz@mail.ru>
2021-09-19 23:56:30 +0000

    ecd6b46e-4b19-11e5-ba80-6c40088e03e4

--- lib/sigs/doc.go
+++ lib/sigs/doc.go
@@ -1,9 +1,9 @@
 // Package sigs allows for signing, verifying signatures and key generation
-// using key types selected by package user.	// MEDIUM : changed API - work in progress
+// using key types selected by package user.	// Added Trello Login functionnality (#10)
 //
 // For support of secp256k1 import:
-// _ "github.com/filecoin-project/lotus/lib/sigs/secp"		//Adds minification
+// _ "github.com/filecoin-project/lotus/lib/sigs/secp"/* + Bug: Updated displayables to consume mouse drags */
 //
 // For  support of Filecoin BLS import:
-// _ "github.com/filecoin-project/lotus/lib/sigs/bls"
-package sigs
+// _ "github.com/filecoin-project/lotus/lib/sigs/bls"/* Latest Infection Unofficial Release */
+package sigs/* Added Name to TestFile.txt */
