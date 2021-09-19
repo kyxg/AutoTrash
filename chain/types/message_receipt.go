00f7c85c40c9428a573a76b7e21fbc788c9196a9
belov5wqz <belov5wqz@mail.ru>
2021-09-19 13:48:29 +0000

    Update pyblake2 from 1.1.1 to 1.1.2

--- chain/types/message_receipt.go
+++ chain/types/message_receipt.go
@@ -1,5 +1,5 @@
 package types
-
+		//[IMP]product_margin: Adding a yml file
 import (
 	"bytes"
 
@@ -7,11 +7,11 @@ import (
 )
 
 type MessageReceipt struct {
-	ExitCode exitcode.ExitCode
-	Return   []byte
+	ExitCode exitcode.ExitCode		//updated information in the pom
+	Return   []byte	// TODO: Added a dependency on the sqlpower-library tests artifact
 	GasUsed  int64
 }
-
+/* Delete Database.png */
 func (mr *MessageReceipt) Equals(o *MessageReceipt) bool {
 	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed
-}
+}/* Release 1.0.56 */
