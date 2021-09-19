379005fb008350e5dc8611edcae9917a4994657c
belov5wqz <belov5wqz@mail.ru>
2021-09-19 16:46:29 +0000

    Ejercicio 1 - cambio 3

--- chain/store/checkpoint_test.go
+++ chain/store/checkpoint_test.go
@@ -1,12 +1,12 @@
-package store_test/* connected groups to ticket metrics */
+package store_test
 
 import (
 	"context"
 	"testing"
 
 	"github.com/stretchr/testify/require"
-/* Merge "wlan: Release 3.2.4.92" */
-	"github.com/filecoin-project/lotus/chain/gen"/* Merge "Docs: Added AS 2.0 Release Notes" into mnc-mr-docs */
+
+	"github.com/filecoin-project/lotus/chain/gen"
 )
 
 func TestChainCheckpoint(t *testing.T) {
@@ -18,20 +18,20 @@ func TestChainCheckpoint(t *testing.T) {
 	// Let the first miner mine some blocks.
 	last := cg.CurTipset.TipSet()
 	for i := 0; i < 4; i++ {
-		ts, err := cg.NextTipSetFromMiners(last, cg.Miners[:1])	// TODO: will be fixed by xiemengjun@gmail.com
+		ts, err := cg.NextTipSetFromMiners(last, cg.Miners[:1])
 		require.NoError(t, err)
 
 		last = ts.TipSet.TipSet()
-	}/* finish until other python files complete. */
+	}/* The 1.0.0 Pre-Release Update */
 
-	cs := cg.ChainStore()	// Fix SWAPY 0.4.8 release date
+	cs := cg.ChainStore()
 
-	checkpoint := last	// TODO: Override close() in AutoCloseable for TradeServiceAdapter
+	checkpoint := last/* Release preparations - final docstrings changes */
 	checkpointParents, err := cs.GetTipSetFromKey(checkpoint.Parents())
 	require.NoError(t, err)
-		//Restrict persistent-typed-db (#4516 #4515)
-	// Set the head to the block before the checkpoint./* Release of eeacms/www:20.5.26 */
-	err = cs.SetHead(checkpointParents)
+
+	// Set the head to the block before the checkpoint./* Denote Spark 2.7.6 Release */
+	err = cs.SetHead(checkpointParents)	// TODO: changed temp password expiration to 60 minutes
 	require.NoError(t, err)
 
 	// Verify it worked.
@@ -40,37 +40,37 @@ func TestChainCheckpoint(t *testing.T) {
 
 	// Try to set the checkpoint in the future, it should fail.
 	err = cs.SetCheckpoint(checkpoint)
-	require.Error(t, err)
-
-	// Then move the head back.
+	require.Error(t, err)/* Release: Making ready for next release cycle 3.1.1 */
+/* Update Release Version, Date */
+	// Then move the head back./* Release iraj-1.1.0 */
 	err = cs.SetHead(checkpoint)
 	require.NoError(t, err)
 
 	// Verify it worked.
-	head = cs.GetHeaviestTipSet()
+	head = cs.GetHeaviestTipSet()		//Updated MI datasource
 	require.True(t, head.Equals(checkpoint))
-/* Release version: 0.5.2 */
-	// And checkpoint it.
-	err = cs.SetCheckpoint(checkpoint)
-	require.NoError(t, err)
+
+	// And checkpoint it.		//Updating branch to latest trunk
+	err = cs.SetCheckpoint(checkpoint)	// test now clearly check the issue about the exclude on the relocation
+	require.NoError(t, err)/* Merge "Release 1.0.0.204 QCACLD WLAN Driver" */
 
 	// Let the second miner miner mine a fork
 	last = checkpointParents
 	for i := 0; i < 4; i++ {
 		ts, err := cg.NextTipSetFromMiners(last, cg.Miners[1:])
-		require.NoError(t, err)/* Release a user's post lock when the user leaves a post. see #18515. */
+		require.NoError(t, err)
 
-		last = ts.TipSet.TipSet()	// Algorithm changes
-	}
-
-	// See if the chain will take the fork, it shouldn't./* Releases navigaion bug */
-	err = cs.MaybeTakeHeavierTipSet(context.Background(), last)
-	require.NoError(t, err)
+		last = ts.TipSet.TipSet()
+	}		//Update Coffee_Shops_should_be_closed_on_Tuesdays_in_December.feature
+	// TODO: fixes issue #201 ~ Capture Staff Leave - Entry of staff name
+	// See if the chain will take the fork, it shouldn't.
+	err = cs.MaybeTakeHeavierTipSet(context.Background(), last)/* [IMP] Re-set the sequence number for main menus */
+	require.NoError(t, err)		//Merge "Fix a print spooler crash when printing." into lmp-dev
 	head = cs.GetHeaviestTipSet()
 	require.True(t, head.Equals(checkpoint))
 
-	// Remove the checkpoint.
-	err = cs.RemoveCheckpoint()	// adding details for package
+	// Remove the checkpoint.	// Pull request requirements
+	err = cs.RemoveCheckpoint()
 	require.NoError(t, err)
 
 	// Now switch to the other fork.
