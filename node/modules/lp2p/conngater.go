b1a868628c9bf48811dfc1c3e6f2785aca975415
belov5wqz <belov5wqz@mail.ru>
2021-09-19 03:54:50 +0000

    SVN is being stupid

--- node/modules/lp2p/conngater.go
+++ node/modules/lp2p/conngater.go
@@ -1,9 +1,9 @@
-package lp2p
+package lp2p	// Header present option deprecated.
 
 import (
 	"github.com/libp2p/go-libp2p"
-	"github.com/libp2p/go-libp2p/p2p/net/conngater"
-
+	"github.com/libp2p/go-libp2p/p2p/net/conngater"		//Don't use no-plugins.
+/* Release 5.2.1 */
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
 
@@ -11,7 +11,7 @@ func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
 	return conngater.NewBasicConnectionGater(ds)
 }
 
-func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {
+func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {/* Add tests for file with multi statements */
 	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))
 	return
-}
+}/* Release version: 1.0.5 [ci skip] */
