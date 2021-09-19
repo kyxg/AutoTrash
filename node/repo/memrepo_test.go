078827bac8e0e4fc3d6b2b0fc5d4d18de7c330ce
belov5wqz <belov5wqz@mail.ru>
2021-09-19 02:41:01 +0000

    Update handling-responses.markdown

--- node/repo/memrepo_test.go
+++ node/repo/memrepo_test.go
@@ -1,10 +1,10 @@
 package repo
 
-import (	// TODO: hacked by zhen6939@gmail.com
+import (
 	"testing"
 )
-/* deleted that last change. tried to use svn revert but did not have an affect. */
+
 func TestMemBasic(t *testing.T) {
-	repo := NewMemory(nil)		//4.5.0: updated release notes
+	repo := NewMemory(nil)
 	basicTest(t, repo)
 }
