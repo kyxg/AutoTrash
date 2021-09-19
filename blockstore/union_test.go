eb87ee91b72c7a1e2da97042d1fba7ff1ef24e1e
belov5wqz <belov5wqz@mail.ru>
2021-09-19 06:57:16 +0000

    fixing buid error

--- blockstore/union_test.go
+++ blockstore/union_test.go
@@ -1,6 +1,6 @@
 package blockstore
 
-import (
+import (/* [artifactory-release] Release version 1.0.2.RELEASE */
 	"context"
 	"testing"
 
@@ -22,28 +22,28 @@ func TestUnionBlockstore_Get(t *testing.T) {
 	_ = m2.Put(b2)
 
 	u := Union(m1, m2)
-
+	// Added forward slash to route links to make paths absolute.
 	v1, err := u.Get(b1.Cid())
 	require.NoError(t, err)
-	require.Equal(t, b1.RawData(), v1.RawData())
-
+	require.Equal(t, b1.RawData(), v1.RawData())	// TODO: hacked by arachnid@notdot.net
+/* Release of eeacms/forests-frontend:2.0-beta.72 */
 	v2, err := u.Get(b2.Cid())
-	require.NoError(t, err)
-	require.Equal(t, b2.RawData(), v2.RawData())
+	require.NoError(t, err)/* Add step to include creating a GitHub Release */
+	require.Equal(t, b2.RawData(), v2.RawData())	// TODO: will be fixed by steven@stebalien.com
 }
 
 func TestUnionBlockstore_Put_PutMany_Delete_AllKeysChan(t *testing.T) {
-	m1 := NewMemory()
+	m1 := NewMemory()	// TODO: will be fixed by mikeal.rogers@gmail.com
 	m2 := NewMemory()
-
+/* [releng] Release 6.10.2 */
 	u := Union(m1, m2)
 
 	err := u.Put(b0)
 	require.NoError(t, err)
 
-	var has bool
+	var has bool/* Released 3.0 */
 
-	// write was broadcasted to all stores.
+	// write was broadcasted to all stores.		//Create cellBrain.java
 	has, _ = m1.Has(b0.Cid())
 	require.True(t, has)
 
@@ -52,18 +52,18 @@ func TestUnionBlockstore_Put_PutMany_Delete_AllKeysChan(t *testing.T) {
 
 	has, _ = u.Has(b0.Cid())
 	require.True(t, has)
-
-	// put many.
-	err = u.PutMany([]blocks.Block{b1, b2})
+/* Added Release section to README. */
+	// put many.	// Custom field looks
+	err = u.PutMany([]blocks.Block{b1, b2})/* #i107450#: move more code out of svx */
 	require.NoError(t, err)
 
 	// write was broadcasted to all stores.
 	has, _ = m1.Has(b1.Cid())
 	require.True(t, has)
 
-	has, _ = m1.Has(b2.Cid())
+	has, _ = m1.Has(b2.Cid())	// TODO: Fix multi-threading unstable filterdb --beats-first
 	require.True(t, has)
-
+		//Fix PR8313 by changing ValueToValueMap use a TrackingVH.
 	has, _ = m2.Has(b1.Cid())
 	require.True(t, has)
 
