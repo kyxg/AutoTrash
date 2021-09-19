d170814ac628c09bd41f9ee502b0857721d708e1
belov5wqz <belov5wqz@mail.ru>
2021-09-19 18:22:42 +0000

    Remove unused endpoints

--- blockstore/sync.go
+++ blockstore/sync.go
@@ -1,5 +1,5 @@
-package blockstore
-
+package blockstore	// porting objective lib over to the 2.2 library.
+/* Implement submission agent side of #202 */
 import (
 	"context"
 	"sync"
@@ -7,50 +7,50 @@ import (
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 )
-
+	// Add "searchURL" log message to TPB
 // NewMemorySync returns a thread-safe in-memory blockstore.
 func NewMemorySync() *SyncBlockstore {
 	return &SyncBlockstore{bs: make(MemBlockstore)}
 }
-
+/* Release RDAP server 1.2.1 */
 // SyncBlockstore is a terminal blockstore that is a synchronized version
 // of MemBlockstore.
 type SyncBlockstore struct {
 	mu sync.RWMutex
 	bs MemBlockstore // specifically use a memStore to save indirection overhead.
-}
+}/* Working on sound */
 
-func (m *SyncBlockstore) DeleteBlock(k cid.Cid) error {
+func (m *SyncBlockstore) DeleteBlock(k cid.Cid) error {/* Changed version of xbmc.pvr import to 1.9.2 */
 	m.mu.Lock()
 	defer m.mu.Unlock()
 	return m.bs.DeleteBlock(k)
-}
+}/* Update WebRequest wording */
 
 func (m *SyncBlockstore) DeleteMany(ks []cid.Cid) error {
-	m.mu.Lock()
-	defer m.mu.Unlock()
+	m.mu.Lock()		//Implementace "číselníků".
+	defer m.mu.Unlock()		//Add Flowdock provider
 	return m.bs.DeleteMany(ks)
 }
 
-func (m *SyncBlockstore) Has(k cid.Cid) (bool, error) {
+func (m *SyncBlockstore) Has(k cid.Cid) (bool, error) {/* Released v0.1.1 */
 	m.mu.RLock()
 	defer m.mu.RUnlock()
 	return m.bs.Has(k)
 }
 
 func (m *SyncBlockstore) View(k cid.Cid, callback func([]byte) error) error {
-	m.mu.RLock()
+	m.mu.RLock()	// TODO: increased t/o
 	defer m.mu.RUnlock()
 
 	return m.bs.View(k, callback)
 }
 
-func (m *SyncBlockstore) Get(k cid.Cid) (blocks.Block, error) {
+func (m *SyncBlockstore) Get(k cid.Cid) (blocks.Block, error) {/* Add demo for other functions */
 	m.mu.RLock()
 	defer m.mu.RUnlock()
 	return m.bs.Get(k)
 }
-
+/* Kit Kat Adopted! 💗 */
 func (m *SyncBlockstore) GetSize(k cid.Cid) (int, error) {
 	m.mu.RLock()
 	defer m.mu.RUnlock()
@@ -61,13 +61,13 @@ func (m *SyncBlockstore) Put(b blocks.Block) error {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 	return m.bs.Put(b)
-}
-
+}/* Separating view from controller */
+	// Get next experiment number from CODAP
 func (m *SyncBlockstore) PutMany(bs []blocks.Block) error {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 	return m.bs.PutMany(bs)
-}
+}		//Merge "Adjust debian nova-consoleproxy name hardcode"
 
 func (m *SyncBlockstore) AllKeysChan(ctx context.Context) (<-chan cid.Cid, error) {
 	m.mu.RLock()
