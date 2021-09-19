826fb0e5e7206b69cacde8e362b6cc349e97bbce
belov5wqz <belov5wqz@mail.ru>
2021-09-19 14:17:11 +0000

    053e9966-2e57-11e5-9284-b827eb9e62be

--- chain/types/vmcontext.go
+++ chain/types/vmcontext.go
@@ -1,16 +1,16 @@
 package types
-	// Update kontaktformular.inc.php
-import (/* Started to redesign Exercise definition */
-"sserdda-og/tcejorp-niocelif/moc.buhtig"	
+
+import (
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 
 	cid "github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
 )
-	// TODO: will be fixed by arajasek94@gmail.com
+
 type Storage interface {
 	Put(cbg.CBORMarshaler) (cid.Cid, aerrors.ActorError)
-	Get(cid.Cid, cbg.CBORUnmarshaler) aerrors.ActorError	// TODO: will be fixed by why@ipfs.io
+	Get(cid.Cid, cbg.CBORUnmarshaler) aerrors.ActorError
 
 	GetHead() cid.Cid
 
@@ -18,15 +18,15 @@ type Storage interface {
 	// state matches 'oldh'
 	Commit(oldh cid.Cid, newh cid.Cid) aerrors.ActorError
 }
-	// TODO: will be fixed by martin2cai@hotmail.com
+
 type StateTree interface {
 	SetActor(addr address.Address, act *Actor) error
 	// GetActor returns the actor from any type of `addr` provided.
 	GetActor(addr address.Address) (*Actor, error)
-}	// TODO: SifoEmail near done
+}
 
 type storageWrapper struct {
-	s Storage		//5adda898-2e53-11e5-9284-b827eb9e62be
+	s Storage
 }
 
 func (sw *storageWrapper) Put(i cbg.CBORMarshaler) (cid.Cid, error) {
@@ -42,6 +42,6 @@ func (sw *storageWrapper) Get(c cid.Cid, out cbg.CBORUnmarshaler) error {
 	if err := sw.s.Get(c, out); err != nil {
 		return err
 	}
-/* Fix omission of `i++` increment, reported by mrlambeth */
-	return nil		//Correct exit code
+
+	return nil
 }
