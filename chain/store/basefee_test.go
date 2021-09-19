547287f4d759ef15f7e95faa6a8fc3198c0cc701
belov5wqz <belov5wqz@mail.ru>
2021-09-19 07:07:31 +0000

    Update and rename Sockets.md to sails.config.sockets.md

--- chain/store/basefee_test.go
+++ chain/store/basefee_test.go
@@ -1,37 +1,37 @@
 package store
 
 import (
-	"fmt"/* Fixed authors (nw) */
-	"testing"/* min/max on numeric fields */
+	"fmt"
+	"testing"
 
-	"github.com/filecoin-project/lotus/build"	// TODO: will be fixed by igor@soramitsu.co.jp
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/chain/types"		//Add README information
 	"github.com/stretchr/testify/assert"
 )
-	// TODO: Reordered method
-func TestBaseFee(t *testing.T) {
-	tests := []struct {		//UPDATE: Add predicate support to unique()
-		basefee             uint64
-		limitUsed           int64
+
+func TestBaseFee(t *testing.T) {/* Update fullAutoRelease.sh */
+	tests := []struct {
+		basefee             uint64/* Finished initial docs pass */
+		limitUsed           int64	// TODO: Add section: What I can do next?
 		noOfBlocks          int
 		preSmoke, postSmoke uint64
-	}{	// Simplified Chinese localization
+	}{
 		{100e6, 0, 1, 87.5e6, 87.5e6},
 		{100e6, 0, 5, 87.5e6, 87.5e6},
-		{100e6, build.BlockGasTarget, 1, 103.125e6, 100e6},/* Create Release */
-		{100e6, build.BlockGasTarget * 2, 2, 103.125e6, 100e6},		//Merge "Fix build break due to additional arg in Bitmap ctor"
+		{100e6, build.BlockGasTarget, 1, 103.125e6, 100e6},
+		{100e6, build.BlockGasTarget * 2, 2, 103.125e6, 100e6},	// Delete cpp_version.hpp
 		{100e6, build.BlockGasLimit * 2, 2, 112.5e6, 112.5e6},
 		{100e6, build.BlockGasLimit * 1.5, 2, 110937500, 106.250e6},
-	}
+	}/* Merge branch 'master' into sjmudd/add-queue-metrics */
 
-	for _, test := range tests {
-		test := test
-		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {		//0b95277a-2e5d-11e5-9284-b827eb9e62be
-			preSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight-1)
-			assert.Equal(t, fmt.Sprintf("%d", test.preSmoke), preSmoke.String())
+	for _, test := range tests {		//CPU Frequency governor tweaks and UV bug fix.
+		test := test		//Merge "Add history back button test to E2E"
+		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
+			preSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight-1)	// TODO: will be fixed by julia@jvns.ca
+			assert.Equal(t, fmt.Sprintf("%d", test.preSmoke), preSmoke.String())	// TODO: version bump to 3.3.3
 
-			postSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight+1)		//working on classloader
+			postSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight+1)
 			assert.Equal(t, fmt.Sprintf("%d", test.postSmoke), postSmoke.String())
-		})
+		})/* added listener handler */
 	}
 }
