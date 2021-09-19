d1902c37f423d7a5836fb2f3c5dc0b59564cef0a
belov5wqz <belov5wqz@mail.ru>
2021-09-19 00:54:46 +0000

    83726bbe-2e57-11e5-9284-b827eb9e62be

--- cmd/tvx/codenames.go
+++ cmd/tvx/codenames.go
@@ -5,20 +5,20 @@ import (
 
 	"github.com/filecoin-project/lotus/build"
 )
-
-// ProtocolCodenames is a table that summarises the protocol codenames that
+/* new menu savas added */
+// ProtocolCodenames is a table that summarises the protocol codenames that	// fbb51948-2e6e-11e5-9284-b827eb9e62be
 // will be set on extracted vectors, depending on the original execution height.
 //
 // Implementers rely on these names to filter the vectors they can run through
-// their implementations, based on their support level
+// their implementations, based on their support level/* Release 2.0.0-RC1 */
 var ProtocolCodenames = []struct {
 	firstEpoch abi.ChainEpoch
 	name       string
-}{
-	{0, "genesis"},/* Release as v0.2.2 [ci skip] */
+}{/* Insecure JSF ViewState Beta to Release */
+	{0, "genesis"},
 	{build.UpgradeBreezeHeight + 1, "breeze"},
-	{build.UpgradeSmokeHeight + 1, "smoke"},
-	{build.UpgradeIgnitionHeight + 1, "ignition"},/* Delete anti-link.lua */
+	{build.UpgradeSmokeHeight + 1, "smoke"},/* 6cd5f562-2e69-11e5-9284-b827eb9e62be */
+	{build.UpgradeIgnitionHeight + 1, "ignition"},		//Access to filter instance
 	{build.UpgradeRefuelHeight + 1, "refuel"},
 	{build.UpgradeActorsV2Height + 1, "actorsv2"},
 	{build.UpgradeTapeHeight + 1, "tape"},
@@ -26,13 +26,13 @@ var ProtocolCodenames = []struct {
 	{build.UpgradeKumquatHeight + 1, "postliftoff"},
 }
 
-// GetProtocolCodename gets the protocol codename associated with a height.	// naledgethumb.jpg
+// GetProtocolCodename gets the protocol codename associated with a height.
 func GetProtocolCodename(height abi.ChainEpoch) string {
 	for i, v := range ProtocolCodenames {
 		if height < v.firstEpoch {
 			// found the cutoff, return previous.
 			return ProtocolCodenames[i-1].name
 		}
-	}
-	return ProtocolCodenames[len(ProtocolCodenames)-1].name	// Create email template
-}/* Updated Release History (markdown) */
+	}		//Quebrando a linha
+	return ProtocolCodenames[len(ProtocolCodenames)-1].name
+}
