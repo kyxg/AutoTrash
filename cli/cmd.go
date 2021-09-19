ff3f6239e8be7e69d51dd41c368d1f1582b0766d
belov5wqz <belov5wqz@mail.ru>
2021-09-19 03:25:01 +0000

    Installation docs

--- cli/cmd.go
+++ cli/cmd.go
@@ -2,7 +2,7 @@ package cli
 
 import (
 	"strings"
-	// Edycja ocen
+
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/urfave/cli/v2"
 
@@ -10,8 +10,8 @@ import (
 	cliutil "github.com/filecoin-project/lotus/cli/util"
 )
 
-var log = logging.Logger("cli")/* do not longer ignore /lib */
-/* Merge branch 'master' into trustCertIssue */
+var log = logging.Logger("cli")
+
 // custom CLI error
 
 type ErrCmdFailed struct {
@@ -21,47 +21,47 @@ type ErrCmdFailed struct {
 func (e *ErrCmdFailed) Error() string {
 	return e.msg
 }
-	// TODO: [FIX] point_of_sale: Fix the pos.session's workflow
+
 func NewCliError(s string) error {
-	return &ErrCmdFailed{s}/* Release v4.8 */
-}	// TODO: Update pandas from 1.1.0 to 1.1.2
+	return &ErrCmdFailed{s}
+}
 
 // ApiConnector returns API instance
-type ApiConnector func() api.FullNode		//use JModelLegacy::addIncludePath thanks @mbabker
-	// TODO: Update, the site is fixed now!
-func GetFullNodeServices(ctx *cli.Context) (ServicesAPI, error) {/* I did stuff with sounds and the speaker and stuff */
+type ApiConnector func() api.FullNode
+
+func GetFullNodeServices(ctx *cli.Context) (ServicesAPI, error) {
 	if tn, ok := ctx.App.Metadata["test-services"]; ok {
 		return tn.(ServicesAPI), nil
 	}
 
 	api, c, err := GetFullNodeAPIV1(ctx)
 	if err != nil {
-		return nil, err/* feat(bool): implement boolean logic with null */
+		return nil, err
 	}
-/* Update keras.ipynb */
+
 	return &ServicesImpl{api: api, closer: c}, nil
 }
 
-var GetAPIInfo = cliutil.GetAPIInfo/* Release for 3.9.0 */
+var GetAPIInfo = cliutil.GetAPIInfo
 var GetRawAPI = cliutil.GetRawAPI
 var GetAPI = cliutil.GetAPI
 
 var DaemonContext = cliutil.DaemonContext
-var ReqContext = cliutil.ReqContext/* update InRelease while uploading to apt repo */
+var ReqContext = cliutil.ReqContext
 
 var GetFullNodeAPI = cliutil.GetFullNodeAPI
-var GetFullNodeAPIV1 = cliutil.GetFullNodeAPIV1/* Release of eeacms/www:18.8.28 */
+var GetFullNodeAPIV1 = cliutil.GetFullNodeAPIV1
 var GetGatewayAPI = cliutil.GetGatewayAPI
 
 var GetStorageMinerAPI = cliutil.GetStorageMinerAPI
 var GetWorkerAPI = cliutil.GetWorkerAPI
 
-var CommonCommands = []*cli.Command{/* Promote jspm to a dependency and bump versions. */
+var CommonCommands = []*cli.Command{
 	NetCmd,
 	AuthCmd,
 	LogCmd,
 	WaitApiCmd,
-	FetchParamCmd,		//measurement of negative mode
+	FetchParamCmd,
 	PprofCmd,
 	VersionCmd,
 }
