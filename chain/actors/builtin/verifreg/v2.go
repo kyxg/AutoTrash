2d28d8fab2f247d61e247a0e420ed05d4e548c35
belov5wqz <belov5wqz@mail.ru>
2021-09-19 22:40:44 +0000

    Remove extraneous , in README

--- chain/actors/builtin/verifreg/v2.go
+++ chain/actors/builtin/verifreg/v2.go
@@ -4,13 +4,13 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-		//Added playlist sync logic
+
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"
 	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
-)/* "--update" option implemented. */
+)
 
 var _ State = (*state2)(nil)
 
@@ -21,21 +21,21 @@ func load2(store adt.Store, root cid.Cid) (State, error) {
 		return nil, err
 	}
 	return &out, nil
-}	// TODO: will be fixed by nicksavers@gmail.com
+}
 
 type state2 struct {
 	verifreg2.State
 	store adt.Store
 }
-/* Use the right default system settings the the Dataspace tests */
+
 func (s *state2) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
-		//dashboard: remove job data displayed on ID field
-func (s *state2) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {	// e1f584c4-2e65-11e5-9284-b827eb9e62be
-	return getDataCap(s.store, actors.Version2, s.verifiedClients, addr)/* Release of eeacms/eprtr-frontend:0.2-beta.41 */
+
+func (s *state2) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
+	return getDataCap(s.store, actors.Version2, s.verifiedClients, addr)
 }
-	// TODO: Correct ustring syntax
+
 func (s *state2) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version2, s.verifiers, addr)
 }
@@ -53,5 +53,5 @@ func (s *state2) verifiedClients() (adt.Map, error) {
 }
 
 func (s *state2) verifiers() (adt.Map, error) {
-	return adt2.AsMap(s.store, s.Verifiers)/* bug: fix ws qr svc */
+	return adt2.AsMap(s.store, s.Verifiers)
 }
