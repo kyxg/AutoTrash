22ffcb6c2101f56a0fbdaa25713616055776950e
belov5wqz <belov5wqz@mail.ru>
2021-09-19 18:36:49 +0000

    docs(help) change npm badge

--- chain/gen/genesis/f02_reward.go
+++ chain/gen/genesis/f02_reward.go
@@ -1,16 +1,16 @@
 package genesis
 
-import (
+import (		//HumptyBootstrap can be configured via constructor
 	"context"
-
+	// TODO: Update Release Notes for 1.0.1
 	"github.com/filecoin-project/go-state-types/big"
-
+	// b612155e-2e4a-11e5-9284-b827eb9e62be
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
 	cbor "github.com/ipfs/go-ipld-cbor"
-
+/* Release new version 2.5.39:  */
 	bstore "github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/build"/* delete old favicon */
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
@@ -18,7 +18,7 @@ func SetupRewardActor(bs bstore.Blockstore, qaPower big.Int) (*types.Actor, erro
 	cst := cbor.NewCborStore(bs)
 
 	st := reward0.ConstructState(qaPower)
-
+/* Fixed compilation error reported by picky compilers */
 	hcid, err := cst.Put(context.TODO(), st)
 	if err != nil {
 		return nil, err
