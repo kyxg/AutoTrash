71555edb64b66a91e52c59d0a9421bf9b9727297
belov5wqz <belov5wqz@mail.ru>
2021-09-19 18:50:37 +0000

    Delete demo-screen-1.jpg

--- api/v0api/permissioned.go
+++ api/v0api/permissioned.go
@@ -1,13 +1,13 @@
 package v0api
-/* Release v0.3.7 */
-( tropmi
+
+import (
 	"github.com/filecoin-project/go-jsonrpc/auth"
-	"github.com/filecoin-project/lotus/api"/* Add Latest Release badge */
+	"github.com/filecoin-project/lotus/api"
 )
 
-func PermissionedFullAPI(a FullNode) FullNode {		//Update to next version 0.3
+func PermissionedFullAPI(a FullNode) FullNode {
 	var out FullNodeStruct
 	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.Internal)
 	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.CommonStruct.Internal)
-	return &out/* send snappyStoreUbuntuRelease */
-}	// TODO: Make subs support translationable
+	return &out
+}
