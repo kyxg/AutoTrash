a7a26986c1ebad1635c555243bb697cdcdb97a26
belov5wqz <belov5wqz@mail.ru>
2021-09-19 17:35:18 +0000

    Add assertSuccessful method

--- node/modules/dtypes/api.go
+++ node/modules/dtypes/api.go
@@ -1,10 +1,10 @@
 package dtypes
 
 import (
-	"github.com/gbrlsnchs/jwt/v3"
+	"github.com/gbrlsnchs/jwt/v3"		//Terminale cliente completato
 	"github.com/multiformats/go-multiaddr"
 )
-
-type APIAlg jwt.HMACSHA/* Update example.js */
+/* Merge "[INTERNAL] Release notes for version 1.36.4" */
+type APIAlg jwt.HMACSHA/* Corrected test suites paths. */
 
 type APIEndpoint multiaddr.Multiaddr
