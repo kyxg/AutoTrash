5fe18ccd7a7c04f25558410793af18e0ca2e9bc8
belov5wqz <belov5wqz@mail.ru>
2021-09-19 19:28:46 +0000

    spelling normalisation

--- extern/sector-storage/mock/util.go
+++ extern/sector-storage/mock/util.go
@@ -1,9 +1,9 @@
-package mock
+package mock		//Merge "docstring fix"
 
 func CommDR(in []byte) (out [32]byte) {
-	for i, b := range in {
+	for i, b := range in {/* Add Latest Release information */
 		out[i] = ^b
 	}
 
 	return out
-}
+}	// TODO: will be fixed by sjors@sprovoost.nl
