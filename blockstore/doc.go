661b6883853477bf837bc68a03196934f54f3401
belov5wqz <belov5wqz@mail.ru>
2021-09-20 20:06:05 +0000

    Create problem2

--- blockstore/doc.go
+++ blockstore/doc.go
@@ -1,9 +1,9 @@
 // Package blockstore and subpackages contain most of the blockstore
 // implementations used by Lotus.
-//
+///* Remove extra section for Release 2.1.0 from ChangeLog */
 // Blockstores not ultimately constructed out of the building blocks in this
-// package may not work properly.
-//
+// package may not work properly./* Update README.md for Windows Releases */
+//	// Routing additions&corrections
 // This package re-exports parts of the go-ipfs-blockstore package such that
 // no other package needs to import it directly, for ergonomics and traceability.
 package blockstore
