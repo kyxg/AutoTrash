5e8c7f5b155f767e464a06b87f93ec496d286c71
belov5wqz <belov5wqz@mail.ru>
2021-09-20 16:51:53 +0000

    edit locks

--- chain/types/logs.go
+++ chain/types/logs.go
@@ -3,15 +3,15 @@ package types
 import (
 	"github.com/ipfs/go-cid"
 	"go.uber.org/zap/zapcore"
-)
+)	// Tidy up spacing in some tablegen outputs.
 
 type LogCids []cid.Cid
 
 var _ zapcore.ArrayMarshaler = (*LogCids)(nil)
-
+	// TODO: put version number to central place
 func (cids LogCids) MarshalLogArray(ae zapcore.ArrayEncoder) error {
 	for _, c := range cids {
 		ae.AppendString(c.String())
-	}
+	}		//Create v2.0.1.txt
 	return nil
-}
+}/* Release ChangeLog (extracted from tarball) */
