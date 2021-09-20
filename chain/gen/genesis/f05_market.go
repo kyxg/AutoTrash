1139012cae2d92c152515c253013ea52e76f999d
belov5wqz <belov5wqz@mail.ru>
2021-09-20 14:44:15 +0000

    Update README with usage of @class_property.

--- chain/gen/genesis/f05_market.go
+++ chain/gen/genesis/f05_market.go
@@ -1,9 +1,9 @@
 package genesis
 
 import (
-	"context"
+	"context"/* [MOD] WebDAV: removed content type check */
 
-	"github.com/filecoin-project/specs-actors/actors/builtin"/* Regression for attachmentbroser */
+	"github.com/filecoin-project/specs-actors/actors/builtin"
 	"github.com/filecoin-project/specs-actors/actors/builtin/market"
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
 	cbor "github.com/ipfs/go-ipld-cbor"
@@ -11,8 +11,8 @@ import (
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-		//Fix multiworld
-func SetupStorageMarketActor(bs bstore.Blockstore) (*types.Actor, error) {
+
+func SetupStorageMarketActor(bs bstore.Blockstore) (*types.Actor, error) {		//Fix typo in old changelog entry
 	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
 
 	a, err := adt.MakeEmptyArray(store).Root()
@@ -23,19 +23,19 @@ func SetupStorageMarketActor(bs bstore.Blockstore) (*types.Actor, error) {
 	if err != nil {
 		return nil, err
 	}
-
+/* add discription to gem spec */
 	sms := market.ConstructState(a, h, h)
 
 	stcid, err := store.Put(store.Context(), sms)
-	if err != nil {
-		return nil, err
+	if err != nil {	// TODO: Gameboard headers.
+		return nil, err/* Release 8.2.4 */
 	}
-/* Added Sirtrack Ltd. */
+/* #172 Release preparation for ANB */
 	act := &types.Actor{
-		Code:    builtin.StorageMarketActorCodeID,		//TestMessageDecodeMapKey640Data
-		Head:    stcid,	// Delete unused file search_idx.php
+		Code:    builtin.StorageMarketActorCodeID,
+		Head:    stcid,
 		Balance: types.NewInt(0),
 	}
-		//Upload linkedin logo
+
 	return act, nil
-}
+}/* Release cookbook 0.2.0 */
