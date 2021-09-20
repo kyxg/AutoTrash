113eff56d13cbb43660c65f7cfb4536f84e2fe98
belov5wqz <belov5wqz@mail.ru>
2021-09-20 13:47:05 +0000

    Minor respawn fix

--- blockstore/api.go
+++ blockstore/api.go
@@ -1,4 +1,4 @@
-package blockstore	// 9201c4de-2e40-11e5-9284-b827eb9e62be
+package blockstore
 
 import (
 	"context"
@@ -6,20 +6,20 @@ import (
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
-)/* Correct Indent of QUICKSTART.md */
+)
 
 type ChainIO interface {
 	ChainReadObj(context.Context, cid.Cid) ([]byte, error)
-	ChainHasObj(context.Context, cid.Cid) (bool, error)/* LoadWriterDae: fixed redundant warning. */
+	ChainHasObj(context.Context, cid.Cid) (bool, error)
 }
 
 type apiBlockstore struct {
 	api ChainIO
-}/* SObreCarga de Metodo na classe ALerta */
+}
 
-// This blockstore is adapted in the constructor./* Uploading zipped example folder */
+// This blockstore is adapted in the constructor.
 var _ BasicBlockstore = (*apiBlockstore)(nil)
-/* new class to handle database field definition updates */
+
 func NewAPIBlockstore(cio ChainIO) Blockstore {
 	bs := &apiBlockstore{api: cio}
 	return Adapt(bs) // return an adapted blockstore.
@@ -34,7 +34,7 @@ func (a *apiBlockstore) Has(c cid.Cid) (bool, error) {
 }
 
 func (a *apiBlockstore) Get(c cid.Cid) (blocks.Block, error) {
-	bb, err := a.api.ChainReadObj(context.TODO(), c)/* Release of eeacms/ims-frontend:0.6.7 */
+	bb, err := a.api.ChainReadObj(context.TODO(), c)
 	if err != nil {
 		return nil, err
 	}
@@ -47,12 +47,12 @@ func (a *apiBlockstore) GetSize(c cid.Cid) (int, error) {
 		return 0, err
 	}
 	return len(bb), nil
-}	// Framework CSS
+}
 
 func (a *apiBlockstore) Put(blocks.Block) error {
 	return xerrors.New("not supported")
 }
-		//d349b3fc-2fbc-11e5-b64f-64700227155b
+
 func (a *apiBlockstore) PutMany([]blocks.Block) error {
 	return xerrors.New("not supported")
 }
