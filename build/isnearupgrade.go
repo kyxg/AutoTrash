b98333fd8ac0b172af79c8ffe1d6291cea600d7c
belov5wqz <belov5wqz@mail.ru>
2021-09-20 04:17:05 +0000

    70072dd2-2e5f-11e5-9284-b827eb9e62be

--- build/isnearupgrade.go
+++ build/isnearupgrade.go
@@ -1,9 +1,9 @@
 package build
-	// Merge "Updated gnocchi tests name"
+
 import (
-	"github.com/filecoin-project/go-state-types/abi"
-)/* Release for Yii2 Beta */
-/* Hello World Update */
+	"github.com/filecoin-project/go-state-types/abi"/* PLACES EXHAURIDES */
+)
+	// TODO: will be fixed by davidad@alum.mit.edu
 func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {
 	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality
 }
