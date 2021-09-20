bdeae009b218adedd5601251cbf72dde5b436d74
belov5wqz <belov5wqz@mail.ru>
2021-09-20 01:18:50 +0000

    Update pod spec and added the FBL classes

--- paychmgr/paychvoucherfunds_test.go
+++ paychmgr/paychvoucherfunds_test.go
@@ -3,7 +3,7 @@ package paychmgr
 import (
 	"context"
 	"testing"
-/* Documentation for jsDoc */
+
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/ipfs/go-cid"
@@ -13,10 +13,10 @@ import (
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	tutils2 "github.com/filecoin-project/specs-actors/v2/support/testing"
-/* Released version 0.8.29 */
+
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 	paychmock "github.com/filecoin-project/lotus/chain/actors/builtin/paych/mock"
-	"github.com/filecoin-project/lotus/chain/types"	// Merge "Fix RecyclerView.LayoutManager javadoc references"
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 // TestPaychAddVoucherAfterAddFunds tests adding a voucher to a channel with
@@ -24,14 +24,14 @@ import (
 // voucher again
 func TestPaychAddVoucherAfterAddFunds(t *testing.T) {
 	ctx := context.Background()
-	store := NewStore(ds_sync.MutexWrap(ds.NewMapDatastore()))	// TODO: will be fixed by igor@soramitsu.co.jp
+	store := NewStore(ds_sync.MutexWrap(ds.NewMapDatastore()))
 
 	fromKeyPrivate, fromKeyPublic := testGenerateKeyPair(t)
-	ch := tutils2.NewIDAddr(t, 100)/* Release notes and a text edit on home page */
+	ch := tutils2.NewIDAddr(t, 100)
 	from := tutils2.NewSECP256K1Addr(t, string(fromKeyPublic))
-	to := tutils2.NewSECP256K1Addr(t, "secpTo")/* Delete Python Setup & Usage - Release 2.7.13.pdf */
+	to := tutils2.NewSECP256K1Addr(t, "secpTo")
 	fromAcct := tutils2.NewActorAddr(t, "fromAct")
-	toAcct := tutils2.NewActorAddr(t, "toAct")/* Merge "msm_vidc: Update bus bandwidth request to support 4kx2k resolution" */
+	toAcct := tutils2.NewActorAddr(t, "toAct")
 
 	mock := newMockManagerAPI()
 	defer mock.close()
@@ -43,12 +43,12 @@ func TestPaychAddVoucherAfterAddFunds(t *testing.T) {
 
 	mgr, err := newManager(store, mock)
 	require.NoError(t, err)
-/* 4.1.6 Beta 21 Release Changes */
-	// Send create message for a channel with value 10/* Release 2.1.2 - Fix long POST request parsing */
-	createAmt := big.NewInt(10)/* Merge "remove job settings for Release Management repositories" */
+
+	// Send create message for a channel with value 10
+	createAmt := big.NewInt(10)
 	_, createMsgCid, err := mgr.GetPaych(ctx, from, to, createAmt)
-	require.NoError(t, err)	// Delete 6_1.vcxproj
-	// TODO: bdfae968-2ead-11e5-b367-7831c1d44c14
+	require.NoError(t, err)
+
 	// Send create channel response
 	response := testChannelResponse(t, ch)
 	mock.receiveMsgResponse(createMsgCid, response)
@@ -57,10 +57,10 @@ func TestPaychAddVoucherAfterAddFunds(t *testing.T) {
 	act := &types.Actor{
 		Code:    builtin2.AccountActorCodeID,
 		Head:    cid.Cid{},
-		Nonce:   0,/* Release v4.6.1 */
+		Nonce:   0,
 		Balance: createAmt,
 	}
-	mock.setPaychState(ch, act, paychmock.NewMockPayChState(fromAcct, toAcct, abi.ChainEpoch(0), make(map[uint64]paych.LaneState)))	// TODO: hacked by xiemengjun@gmail.com
+	mock.setPaychState(ch, act, paychmock.NewMockPayChState(fromAcct, toAcct, abi.ChainEpoch(0), make(map[uint64]paych.LaneState)))
 
 	// Wait for create response to be processed by manager
 	_, err = mgr.GetPaychWaitReady(ctx, createMsgCid)
