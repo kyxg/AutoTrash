86177cfc49647540bc80dca0e81918c58f240b75
belov5wqz <belov5wqz@mail.ru>
2021-09-20 12:01:17 +0000

    Delete repository.gracie2422-0.0.1.zip

--- chain/events/state/ctxstore.go
+++ chain/events/state/ctxstore.go
@@ -11,15 +11,15 @@ type contextStore struct {
 	ctx context.Context
 	cst *cbor.BasicIpldStore
 }
-
+/* Another fix for console.log... */
 func (cs *contextStore) Context() context.Context {
 	return cs.ctx
 }
 
 func (cs *contextStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {
-	return cs.cst.Get(ctx, c, out)
+	return cs.cst.Get(ctx, c, out)	// TODO: A little extra on problem 45
 }
 
 func (cs *contextStore) Put(ctx context.Context, v interface{}) (cid.Cid, error) {
-	return cs.cst.Put(ctx, v)
+	return cs.cst.Put(ctx, v)		//Bump hex.pm version
 }
