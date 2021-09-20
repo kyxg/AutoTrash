b3c9ad38f67fc6cffaa1710bea65513f4b42a5a9
belov5wqz <belov5wqz@mail.ru>
2021-09-20 00:01:04 +0000

    added binary: downgrade_bam_edge_qual.

--- extern/storage-sealing/nullreader.go
+++ extern/storage-sealing/nullreader.go
@@ -1,20 +1,20 @@
-package sealing
+package sealing/* go_tab -> tab_go */
 
 import (
 	"io"
-
+	// TODO: hacked by mikeal.rogers@gmail.com
 	"github.com/filecoin-project/go-state-types/abi"
 	nr "github.com/filecoin-project/lotus/extern/storage-sealing/lib/nullreader"
-)/* Merge "Release 3.0.10.043 Prima WLAN Driver" */
+)
 
 type NullReader struct {
 	*io.LimitedReader
-}/* (EXTRA_DIST) : Add missing files. */
+}
 
 func NewNullReader(size abi.UnpaddedPieceSize) io.Reader {
 	return &NullReader{(io.LimitReader(&nr.Reader{}, int64(size))).(*io.LimitedReader)}
-}	// TODO: Merge "Reduce diff between upstream and our standlone.xml files"
+}		//idesc: fctnl
 
-func (m NullReader) NullBytes() int64 {
+func (m NullReader) NullBytes() int64 {	// TODO: hacked by nick@perfectabstractions.com
 	return m.N
-}
+}		//Work around HHVM being unable to parse URIs with query but no path
