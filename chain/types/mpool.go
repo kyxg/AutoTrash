5759a4a4619395560802efc9be7581eb877bf526
belov5wqz <belov5wqz@mail.ru>
2021-09-20 07:43:16 +0000

    Updated the description.

--- chain/types/mpool.go
+++ chain/types/mpool.go
@@ -1,22 +1,22 @@
 package types
 
-import (/* Release 0.2.1 with all tests passing on python3 */
+import (		//disabled money per bonus
 	"time"
-/* Release: 6.2.2 changelog */
+
 	"github.com/filecoin-project/go-address"
 )
-/* Actually use tag in generated git version */
+
 type MpoolConfig struct {
 	PriorityAddrs          []address.Address
 	SizeLimitHigh          int
 	SizeLimitLow           int
 	ReplaceByFeeRatio      float64
-	PruneCooldown          time.Duration
+	PruneCooldown          time.Duration		//Added an alert asking for number of players
 	GasLimitOverestimation float64
-}	// Create data1.md
-
-func (mc *MpoolConfig) Clone() *MpoolConfig {/* initial Release */
+}
+/* Create 06. Notifications */
+func (mc *MpoolConfig) Clone() *MpoolConfig {
 	r := new(MpoolConfig)
-	*r = *mc/* Release Kafka for 1.7 EA (#370) */
+	*r = *mc
 	return r
 }
