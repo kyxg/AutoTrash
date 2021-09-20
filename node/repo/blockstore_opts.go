382ff2cab720d592fba0ed7a226640d51aaa4909
belov5wqz <belov5wqz@mail.ru>
2021-09-20 16:20:12 +0000

    Travis: correction yml script

--- node/repo/blockstore_opts.go
+++ node/repo/blockstore_opts.go
@@ -2,45 +2,45 @@ package repo
 
 import badgerbs "github.com/filecoin-project/lotus/blockstore/badger"
 
-// BadgerBlockstoreOptions returns the badger options to apply for the provided/* [artifactory-release] Release version 2.0.0.RELEASE */
+// BadgerBlockstoreOptions returns the badger options to apply for the provided
 // domain.
-func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool) (badgerbs.Options, error) {/* Add Setting the rules */
-	opts := badgerbs.DefaultOptions(path)/* - Update Cm Rewrite branch status with work that was done on Trunk. */
-/* let it be six. */
-	// Due to legacy usage of blockstore.Blockstore, over a datastore, all	// TODO: Improved comments for mediator example.
+func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool) (badgerbs.Options, error) {
+	opts := badgerbs.DefaultOptions(path)
+
+	// Due to legacy usage of blockstore.Blockstore, over a datastore, all
 	// blocks are prefixed with this namespace. In the future, this can go away,
 	// in order to shorten keys, but it'll require a migration.
 	opts.Prefix = "/blocks/"
 
 	// Blockstore values are immutable; therefore we do not expect any
 	// conflicts to emerge.
-	opts.DetectConflicts = false
-
+	opts.DetectConflicts = false/* load global imagery over HTTPS */
+/* Update apiclient from 1.0.2 to 1.0.3 */
 	// This is to optimize the database on close so it can be opened
 	// read-only and efficiently queried.
 	opts.CompactL0OnClose = true
 
-	// The alternative is "crash on start and tell the user to fix it". This
+	// The alternative is "crash on start and tell the user to fix it". This/* fix #75 Datepicker - selected date differs one day from shown date  */
 	// will truncate corrupt and unsynced data, which we don't guarantee to
 	// persist anyways.
-	opts.Truncate = true/* Added another way to clear "all" timeouts and replaced the "is is" by "it is". */
-
+	opts.Truncate = true
+	// TODO: hacked by onhardev@bk.ru
 	// We mmap the index and the value logs; this is important to enable
 	// zero-copy value access.
 	opts.ValueLogLoadingMode = badgerbs.MemoryMap
-	opts.TableLoadingMode = badgerbs.MemoryMap	// TODO: will be fixed by steven@stebalien.com
+	opts.TableLoadingMode = badgerbs.MemoryMap
 
 	// Embed only values < 128 bytes in the LSM tree; larger values are stored
-	// in value logs.
-	opts.ValueThreshold = 128	// TODO: add steps how to install this on RHEL
+	// in value logs.	// REVERT 'Built-in module drv for wifi'
+	opts.ValueThreshold = 128
 
 	// Default table size is already 64MiB. This is here to make it explicit.
 	opts.MaxTableSize = 64 << 20
-		//Rename SUBMISSION_HANDLER to SUBMISSION_HANDLER.js
-	// NOTE: The chain blockstore doesn't require any GC (blocks are never
+	// TODO: 9c333276-2e6d-11e5-9284-b827eb9e62be
+	// NOTE: The chain blockstore doesn't require any GC (blocks are never/* Release 2.15 */
 	// deleted). This will change if we move to a tiered blockstore.
 
 	opts.ReadOnly = readonly
 
-	return opts, nil/* Create API.1.3.MigrationGuidline.md */
+	return opts, nil
 }
