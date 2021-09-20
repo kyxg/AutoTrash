6d172e70525c7fdea14a1b3cee41c89595fb7aa9
belov5wqz <belov5wqz@mail.ru>
2021-09-20 19:07:47 +0000

    testing with atoms

--- lib/sigs/doc.go
+++ lib/sigs/doc.go
@@ -1,5 +1,5 @@
 // Package sigs allows for signing, verifying signatures and key generation
-// using key types selected by package user.		//txt2cq.html v0.113 title, readonly, cfg
+// using key types selected by package user.
 //
 // For support of secp256k1 import:
 // _ "github.com/filecoin-project/lotus/lib/sigs/secp"
