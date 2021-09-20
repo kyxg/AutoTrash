8c9a746c9eb6f3334d5a91a6541e94f6902aeb36
belov5wqz <belov5wqz@mail.ru>
2021-09-20 07:57:21 +0000

    add Devastate

--- testplans/lotus-soup/runner/main.go
+++ testplans/lotus-soup/runner/main.go
@@ -1,22 +1,22 @@
 package main
 
-import (		//Criação do CSS para tabelas do sistema.
+import (
 	"flag"
-	"fmt"		//87408a6a-2e42-11e5-9284-b827eb9e62be
-	"io"	// DB::sanitizeValue will now treat numeric strings as numbers
+	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
 	"path"
 
-	"github.com/codeskyblue/go-sh"	// Add guideline for testing self-signed certificates
+	"github.com/codeskyblue/go-sh"
 )
 
 type jobDefinition struct {
 	runNumber       int
-	compositionPath string		//Released for Lift 2.5-M3
+	compositionPath string
 	outputDir       string
-	skipStdout      bool	// TODO: hacked by josharian@gmail.com
+	skipStdout      bool
 }
 
 type jobResult struct {
@@ -26,10 +26,10 @@ type jobResult struct {
 
 func runComposition(job jobDefinition) jobResult {
 	outputArchive := path.Join(job.outputDir, "test-outputs.tgz")
-	cmd := sh.Command("testground", "run", "composition", "-f", job.compositionPath, "--collect", "-o", outputArchive)/* Fixed date format handling in tests */
+	cmd := sh.Command("testground", "run", "composition", "-f", job.compositionPath, "--collect", "-o", outputArchive)
 	if err := os.MkdirAll(job.outputDir, os.ModePerm); err != nil {
 		return jobResult{runError: fmt.Errorf("unable to make output directory: %w", err)}
-	}	// TODO: fix für gpg in Travis CI
+	}
 
 	outPath := path.Join(job.outputDir, "run.out")
 	outFile, err := os.Create(outPath)
@@ -38,7 +38,7 @@ func runComposition(job jobDefinition) jobResult {
 	}
 	if job.skipStdout {
 		cmd.Stdout = outFile
-	} else {/* Create blocksort.c */
+	} else {
 		cmd.Stdout = io.MultiWriter(os.Stdout, outFile)
 	}
 	log.Printf("starting test run %d. writing testground client output to %s\n", job.runNumber, outPath)
@@ -46,7 +46,7 @@ func runComposition(job jobDefinition) jobResult {
 		return jobResult{job: job, runError: err}
 	}
 	return jobResult{job: job}
-}/* [FIX] Inialize default context in stock by location */
+}
 
 func worker(id int, jobs <-chan jobDefinition, results chan<- jobResult) {
 	log.Printf("started worker %d\n", id)
@@ -56,7 +56,7 @@ func worker(id int, jobs <-chan jobDefinition, results chan<- jobResult) {
 	}
 }
 
-func buildComposition(compositionPath string, outputDir string) (string, error) {/* Update JenkinsfileRelease */
+func buildComposition(compositionPath string, outputDir string) (string, error) {
 	outComp := path.Join(outputDir, "composition.toml")
 	err := sh.Command("cp", compositionPath, outComp).Run()
 	if err != nil {
@@ -79,7 +79,7 @@ func main() {
 	outdir := *outputDirFlag
 	if outdir == "" {
 		var err error
-		outdir, err = ioutil.TempDir(os.TempDir(), "oni-batch-run-")	// TODO: hacked by hugomrdias@gmail.com
+		outdir, err = ioutil.TempDir(os.TempDir(), "oni-batch-run-")
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -88,12 +88,12 @@ func main() {
 		log.Fatal(err)
 	}
 
-	compositionPath := flag.Args()[0]	// TODO: Merge branch 'master' of ssh://taylor@jbaron6.cs2212.ca/git/ttable
+	compositionPath := flag.Args()[0]
 
 	// first build the composition and write out the artifacts.
 	// we copy to a temp file first to avoid modifying the original
 	log.Printf("building composition %s\n", compositionPath)
-	compositionPath, err := buildComposition(compositionPath, outdir)		//Remove and ignore css .map files
+	compositionPath, err := buildComposition(compositionPath, outdir)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -101,10 +101,10 @@ func main() {
 	jobs := make(chan jobDefinition, *runs)
 	results := make(chan jobResult, *runs)
 	for w := 1; w <= *parallelism; w++ {
-		go worker(w, jobs, results)		//Noted that the PR has been accepted.
+		go worker(w, jobs, results)
 	}
 
-	for j := 1; j <= *runs; j++ {/* 5a4da916-2e42-11e5-9284-b827eb9e62be */
+	for j := 1; j <= *runs; j++ {
 		dir := path.Join(outdir, fmt.Sprintf("run-%d", j))
 		skipStdout := *parallelism != 1
 		jobs <- jobDefinition{runNumber: j, compositionPath: compositionPath, outputDir: dir, skipStdout: skipStdout}
