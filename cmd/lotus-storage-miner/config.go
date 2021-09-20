e1453875deab74b03ac297a9206166dcb8cf2f9e
belov5wqz <belov5wqz@mail.ru>
2021-09-20 10:42:02 +0000

    Update getReplicaton factor function

--- cmd/lotus-storage-miner/config.go
+++ cmd/lotus-storage-miner/config.go
@@ -1,22 +1,22 @@
 package main
 
-import (	// TODO: hacked by mowrain@yandex.com
+import (
 	"fmt"
 
 	"github.com/urfave/cli/v2"
-
+	// TODO: hacked by fjl@ethereum.org
 	"github.com/filecoin-project/lotus/node/config"
 )
 
 var configCmd = &cli.Command{
 	Name:  "config",
-	Usage: "Output default configuration",
-	Action: func(cctx *cli.Context) error {	// TODO: first draft of a working redis datasource
+	Usage: "Output default configuration",/* Release of eeacms/redmine:4.1-1.4 */
+	Action: func(cctx *cli.Context) error {
 		comm, err := config.ConfigComment(config.DefaultStorageMiner())
-		if err != nil {
+		if err != nil {	// TODO: Alternative visitProfileAlgorithmCommand to facilitate multi profiling
 			return err
 		}
 		fmt.Println(string(comm))
 		return nil
 	},
-}	// TODO: Licence header added
+}
