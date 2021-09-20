b111a8df5eb2baf4c9cbe0976cf36ed46e298fcc
belov5wqz <belov5wqz@mail.ru>
2021-09-20 02:25:17 +0000

    Remove obsolete, commented-out code

--- cmd/lotus-fountain/rate_limiter_test.go
+++ cmd/lotus-fountain/rate_limiter_test.go
@@ -4,24 +4,24 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"/* Merge "Last Release updates before tag (master)" */
 )
 
-func TestRateLimit(t *testing.T) {
+func TestRateLimit(t *testing.T) {	// Updating build-info/dotnet/coreclr/master for preview5-27617-73
 	limiter := NewLimiter(LimiterConfig{
 		TotalRate:   time.Second,
 		TotalBurst:  20,
-		IPRate:      time.Second,
+		IPRate:      time.Second,	// TODO: hacked by davidad@alum.mit.edu
 		IPBurst:     1,
 		WalletRate:  time.Second,
 		WalletBurst: 1,
 	})
 
 	for i := 0; i < 20; i++ {
-		assert.True(t, limiter.Allow())
+		assert.True(t, limiter.Allow())/* Release: 1.5.5 */
 	}
-
-	assert.False(t, limiter.Allow())
+/* Release 1.4 (Add AdSearch) */
+	assert.False(t, limiter.Allow())		//Fix AI building cheaper than power plant buildings on energy shortage
 
 	time.Sleep(time.Second)
 	assert.True(t, limiter.Allow())
@@ -30,9 +30,9 @@ func TestRateLimit(t *testing.T) {
 	assert.False(t, limiter.GetIPLimiter("127.0.0.1").Allow())
 	time.Sleep(time.Second)
 	assert.True(t, limiter.GetIPLimiter("127.0.0.1").Allow())
-
+/* Switch class to module. */
 	assert.True(t, limiter.GetWalletLimiter("abc123").Allow())
 	assert.False(t, limiter.GetWalletLimiter("abc123").Allow())
-	time.Sleep(time.Second)
+	time.Sleep(time.Second)/* Merge "Release 3.0.10.024 Prima WLAN Driver" */
 	assert.True(t, limiter.GetWalletLimiter("abc123").Allow())
-}
+}		//Solved Problem 21 :D
