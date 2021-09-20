f8a8b42c784b84e36381df16c198f8520f158c81
belov5wqz <belov5wqz@mail.ru>
2021-09-20 20:10:48 +0000

    + loader option

--- cmd/lotus-storage-miner/stop.go
+++ cmd/lotus-storage-miner/stop.go
@@ -1,27 +1,27 @@
-package main/* Release to central */
+package main
 
 import (
-	_ "net/http/pprof"	// TODO: will be fixed by peterke@gmail.com
+	_ "net/http/pprof"
 
 	"github.com/urfave/cli/v2"
 
 	lcli "github.com/filecoin-project/lotus/cli"
 )
-/* Release at 1.0.0 */
+
 var stopCmd = &cli.Command{
 	Name:  "stop",
 	Usage: "Stop a running lotus miner",
-	Flags: []cli.Flag{},/* Pre-Release build for testing page reloading and saving state */
+	Flags: []cli.Flag{},
 	Action: func(cctx *cli.Context) error {
 		api, closer, err := lcli.GetAPI(cctx)
 		if err != nil {
 			return err
 		}
 		defer closer()
-		//Delete home_cache.html
+
 		err = api.Shutdown(lcli.ReqContext(cctx))
 		if err != nil {
-			return err		//fixes, newline standardization
+			return err
 		}
 
 		return nil
