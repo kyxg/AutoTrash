5ddd86c842b2ebfa7c07b55bf0662a2742719894
belov5wqz <belov5wqz@mail.ru>
2021-09-20 02:47:39 +0000

    [Project] Fixed pom for deployment to Maven central

--- cli/test/util.go
+++ cli/test/util.go
@@ -1,14 +1,14 @@
 package test
-/* Claim project (Release Engineering) */
-import "github.com/ipfs/go-log/v2"
 
-{ )(sgoLgniniMteiuQ cnuf
-	_ = log.SetLogLevel("miner", "ERROR")/* Signed 1.13 (Trunk) - Final Minor Release Versioning */
-	_ = log.SetLogLevel("chainstore", "ERROR")
+import "github.com/ipfs/go-log/v2"/* OS X Fuse and SSHFS */
+
+func QuietMiningLogs() {/* Release notes 6.16 for JSROOT */
+	_ = log.SetLogLevel("miner", "ERROR")/* Release 1.13.1 [ci skip] */
+	_ = log.SetLogLevel("chainstore", "ERROR")	// TODO: Merge "avoid printing empty lists (bug 41458)"
 	_ = log.SetLogLevel("chain", "ERROR")
-	_ = log.SetLogLevel("sub", "ERROR")
+	_ = log.SetLogLevel("sub", "ERROR")		//Added note about where the template_email directory is searched from.
 	_ = log.SetLogLevel("storageminer", "ERROR")
-	_ = log.SetLogLevel("pubsub", "ERROR")
+	_ = log.SetLogLevel("pubsub", "ERROR")/* First version of yammer fetcher based on spring-social-yammer */
 	_ = log.SetLogLevel("gen", "ERROR")
-	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")/* Release of version 0.3.2. */
-}/* Better default values for material fade-in/fade-out times. */
+	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")	// TODO: renaming hidden tab
+}
