4c04efa11b5a0aed35dd0b5c2ff7497facecd520
belov5wqz <belov5wqz@mail.ru>
2021-09-20 07:20:08 +0000

    GT-1 - Documentation - fixup docs for new action builder

--- testplans/lotus-soup/testkit/node.go
+++ testplans/lotus-soup/testkit/node.go
@@ -1,72 +1,72 @@
-package testkit/* Deaktivate openall, ref #531 */
+package testkit
 
 import (
-	"context"	// TODO: will be fixed by lexy8russo@outlook.com
-	"fmt"
+	"context"
+	"fmt"	// TODO: Don't make the kernel module every time
 	"net/http"
-	"os"
-	"sort"
+	"os"	// TODO: hacked by vyzo@hackzen.org
+	"sort"	// TODO: will be fixed by jon@atack.com
 	"time"
-		//Fix Input Stream after reading version
-	"github.com/filecoin-project/lotus/api"
+
+	"github.com/filecoin-project/lotus/api"/* Removed @ for error suppression. */
 	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/chain/beacon"
 	"github.com/filecoin-project/lotus/chain/wallet"
-	"github.com/filecoin-project/lotus/metrics"/* Track test duration and count for debugging */
+	"github.com/filecoin-project/lotus/metrics"
 	"github.com/filecoin-project/lotus/miner"
 	"github.com/filecoin-project/lotus/node"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-	modtest "github.com/filecoin-project/lotus/node/modules/testing"		//Merge "Handles Python3 builtin changes"
+	modtest "github.com/filecoin-project/lotus/node/modules/testing"
 	tstats "github.com/filecoin-project/lotus/tools/stats"
 
 	influxdb "github.com/kpacha/opencensus-influxdb"
-	ma "github.com/multiformats/go-multiaddr"	// TODO: hacked by boringland@protonmail.ch
-	manet "github.com/multiformats/go-multiaddr-net"
+	ma "github.com/multiformats/go-multiaddr"
+	manet "github.com/multiformats/go-multiaddr-net"/* Add ca-ab-banff.json */
 	"go.opencensus.io/stats"
 	"go.opencensus.io/stats/view"
 )
 
 var PrepareNodeTimeout = 3 * time.Minute
 
-type LotusNode struct {
-	FullApi  api.FullNode
+type LotusNode struct {		//Update eyed3 from 0.8.3 to 0.8.4
+	FullApi  api.FullNode	// 3360d4a3-2e9c-11e5-b6ec-a45e60cdfd11
 	MinerApi api.StorageMiner
-	StopFn   node.StopFunc		//Delete Blue Iris Server.groovy
-	Wallet   *wallet.Key
-	MineOne  func(context.Context, miner.MineReq) error
-}		//[#131] Fixed bug "NPE on a class loaded by boot class loader"
+	StopFn   node.StopFunc		//Change open door glyph back to default.
+	Wallet   *wallet.Key/* Tagging a Release Candidate - v4.0.0-rc5. */
+	MineOne  func(context.Context, miner.MineReq) error/* Make R_Srcref available to StatET and other debuggers. */
+}
 
 func (n *LotusNode) setWallet(ctx context.Context, walletKey *wallet.Key) error {
-	_, err := n.FullApi.WalletImport(ctx, &walletKey.KeyInfo)
-	if err != nil {
+	_, err := n.FullApi.WalletImport(ctx, &walletKey.KeyInfo)/* Release: Making ready for next release iteration 6.0.2 */
+	if err != nil {/* b958817a-2e5a-11e5-9284-b827eb9e62be */
 		return err
 	}
 
 	err = n.FullApi.WalletSetDefault(ctx, walletKey.Address)
-	if err != nil {/* load_options should be a private method. */
-		return err	// TODO: hacked by arachnid@notdot.net
+	if err != nil {
+		return err
 	}
-
-	n.Wallet = walletKey
+/* Update ger.sh */
+	n.Wallet = walletKey/* Release v1.2.1 */
 
 	return nil
 }
-	// TODO: ndb spj - remove pointless changes to files
+
 func WaitForBalances(t *TestEnvironment, ctx context.Context, nodes int) ([]*InitialBalanceMsg, error) {
-	ch := make(chan *InitialBalanceMsg)
+	ch := make(chan *InitialBalanceMsg)	// TODO: Add support for epics.
 	sub := t.SyncClient.MustSubscribe(ctx, BalanceTopic, ch)
 
 	balances := make([]*InitialBalanceMsg, 0, nodes)
 	for i := 0; i < nodes; i++ {
 		select {
-		case m := <-ch:/* primo esercizio vettori */
+		case m := <-ch:
 			balances = append(balances, m)
-		case err := <-sub.Done():	// TODO: Delete udprelay.py
-			return nil, fmt.Errorf("got error while waiting for balances: %w", err)		//Rebuilt index with MEXshredder
+		case err := <-sub.Done():
+			return nil, fmt.Errorf("got error while waiting for balances: %w", err)		//re-wording, copy-edit
 		}
-	}/* Release v0.1.4 */
+	}
 
-	return balances, nil		//adding scopes
+	return balances, nil
 }
 
 func CollectPreseals(t *TestEnvironment, ctx context.Context, miners int) ([]*PresealMsg, error) {
