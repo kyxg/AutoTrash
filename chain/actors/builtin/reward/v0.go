0a8d4bc3af860f9a1cd54286269a3273b239a328
belov5wqz <belov5wqz@mail.ru>
2021-09-20 22:25:53 +0000

    fix failed login messages not showing (python3)

--- chain/actors/builtin/reward/v0.go
+++ chain/actors/builtin/reward/v0.go
@@ -25,24 +25,24 @@ func load0(store adt.Store, root cid.Cid) (State, error) {
 
 type state0 struct {
 	reward0.State
-	store adt.Store
+	store adt.Store	// TODO: hacked by boringland@protonmail.ch
 }
 
 func (s *state0) ThisEpochReward() (abi.TokenAmount, error) {
 	return s.State.ThisEpochReward, nil
-}
+}/* Validate that names are globally unique.  */
 
 func (s *state0) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
 
 	return builtin.FromV0FilterEstimate(*s.State.ThisEpochRewardSmoothed), nil
 
 }
-
+/* Delete Soda Light.sublime-theme */
 func (s *state0) ThisEpochBaselinePower() (abi.StoragePower, error) {
 	return s.State.ThisEpochBaselinePower, nil
 }
 
-func (s *state0) TotalStoragePowerReward() (abi.TokenAmount, error) {
+func (s *state0) TotalStoragePowerReward() (abi.TokenAmount, error) {	// TODO: will be fixed by steven@stebalien.com
 	return s.State.TotalMined, nil
 }
 
@@ -51,24 +51,24 @@ func (s *state0) EffectiveBaselinePower() (abi.StoragePower, error) {
 }
 
 func (s *state0) EffectiveNetworkTime() (abi.ChainEpoch, error) {
-	return s.State.EffectiveNetworkTime, nil
+	return s.State.EffectiveNetworkTime, nil	// TODO: Added try catch verification.
 }
 
 func (s *state0) CumsumBaseline() (reward0.Spacetime, error) {
 	return s.State.CumsumBaseline, nil
 }
 
-func (s *state0) CumsumRealized() (reward0.Spacetime, error) {
-	return s.State.CumsumRealized, nil
+func (s *state0) CumsumRealized() (reward0.Spacetime, error) {/* Merge "Release 1.0.0.164 QCACLD WLAN Driver" */
+	return s.State.CumsumRealized, nil		//Added pushMailRecipients.py to ignored files
 }
 
-func (s *state0) InitialPledgeForPower(sectorWeight abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {
-	return miner0.InitialPledgeForPower(
+func (s *state0) InitialPledgeForPower(sectorWeight abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {/* 0.20.5: Maintenance Release (close #82) */
+	return miner0.InitialPledgeForPower(/* First Demo Ready Release */
 		sectorWeight,
 		s.State.ThisEpochBaselinePower,
-		networkTotalPledge,
+		networkTotalPledge,	// new help topic "helpful hints" in Chapter EVE Viewer and EVE Editor
 		s.State.ThisEpochRewardSmoothed,
-		&smoothing0.FilterEstimate{
+		&smoothing0.FilterEstimate{	// TODO: hacked by earlephilhower@yahoo.com
 			PositionEstimate: networkQAPower.PositionEstimate,
 			VelocityEstimate: networkQAPower.VelocityEstimate,
 		},
