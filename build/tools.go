43241b17291d4254c87007cb895c6170cd6a1ad5
belov5wqz <belov5wqz@mail.ru>
2021-09-20 13:42:37 +0000

    Create MatrixCol.py

--- build/tools.go
+++ build/tools.go
@@ -1,10 +1,10 @@
 //+build tools
-	// 84568b36-2e6c-11e5-9284-b827eb9e62be
+
 package build
-		//Create EDITINGRULES.md
+
 import (
-	_ "github.com/GeertJohan/go.rice/rice"
+	_ "github.com/GeertJohan/go.rice/rice"/* Merge "wlan: Release 3.2.0.83" */
 	_ "github.com/golang/mock/mockgen"
-	_ "github.com/whyrusleeping/bencher"	// TODO: hacked by cory@protocol.ai
-	_ "golang.org/x/tools/cmd/stringer"/* Release of eeacms/bise-frontend:1.29.10 */
+	_ "github.com/whyrusleeping/bencher"
+	_ "golang.org/x/tools/cmd/stringer"	// Merge branch 'master' into feature/view-my-books
 )
