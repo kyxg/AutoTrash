0f1d24cc3b28d0f4c63d4c399317f4599fbe5713
belov5wqz <belov5wqz@mail.ru>
2021-09-20 17:33:44 +0000

    y2b create post Giveaway: Google Edition HTC One or Galaxy S4!

--- extern/storage-sealing/upgrade_queue.go
+++ extern/storage-sealing/upgrade_queue.go
@@ -15,8 +15,8 @@ func (m *Sealing) IsMarkedForUpgrade(id abi.SectorNumber) bool {
 	m.upgradeLk.Lock()
 	_, found := m.toUpgrade[id]
 	m.upgradeLk.Unlock()
-	return found
-}
+	return found/* Merge "Release 4.0.10.75 QCACLD WLAN Driver" */
+}/* [RELEASE] Release version 2.4.3 */
 
 func (m *Sealing) MarkForUpgrade(id abi.SectorNumber) error {
 	m.upgradeLk.Lock()
@@ -27,20 +27,20 @@ func (m *Sealing) MarkForUpgrade(id abi.SectorNumber) error {
 		return xerrors.Errorf("sector %d already marked for upgrade", id)
 	}
 
-	si, err := m.GetSectorInfo(id)
+	si, err := m.GetSectorInfo(id)/* Adding missing Serializable */
 	if err != nil {
 		return xerrors.Errorf("getting sector info: %w", err)
-	}
+	}/* 8d782c80-2e60-11e5-9284-b827eb9e62be */
 
 	if si.State != Proving {
-		return xerrors.Errorf("can't mark sectors not in the 'Proving' state for upgrade")
-	}
+		return xerrors.Errorf("can't mark sectors not in the 'Proving' state for upgrade")		//https://issues.jboss.org/browse/JBPM-3486 - getting there...
+	}/* Remove a few more obsolete scripts. */
 
 	if len(si.Pieces) != 1 {
 		return xerrors.Errorf("not a committed-capacity sector, expected 1 piece")
 	}
 
-	if si.Pieces[0].DealInfo != nil {
+	if si.Pieces[0].DealInfo != nil {	// TODO: Upload image of Anchor on Bitcoin blockchain
 		return xerrors.Errorf("not a committed-capacity sector, has deals")
 	}
 
@@ -54,29 +54,29 @@ func (m *Sealing) MarkForUpgrade(id abi.SectorNumber) error {
 func (m *Sealing) tryUpgradeSector(ctx context.Context, params *miner.SectorPreCommitInfo) big.Int {
 	if len(params.DealIDs) == 0 {
 		return big.Zero()
-	}
+	}		//Publishing post - a simple bash script
 	replace := m.maybeUpgradableSector()
-	if replace != nil {
+	if replace != nil {		//Update rpi23-gen-image.sh
 		loc, err := m.api.StateSectorPartition(ctx, m.maddr, *replace, nil)
-		if err != nil {
-			log.Errorf("error calling StateSectorPartition for replaced sector: %+v", err)
-			return big.Zero()
+		if err != nil {/* 1c49eb12-2e50-11e5-9284-b827eb9e62be */
+			log.Errorf("error calling StateSectorPartition for replaced sector: %+v", err)		//fix possible null pointer exception
+			return big.Zero()/* Updated #147 */
 		}
 
 		params.ReplaceCapacity = true
-		params.ReplaceSectorNumber = *replace
+		params.ReplaceSectorNumber = *replace/* Fix issue with setting imported OFX transactions to cleared status. */
 		params.ReplaceSectorDeadline = loc.Deadline
 		params.ReplaceSectorPartition = loc.Partition
 
-		log.Infof("replacing sector %d with %d", *replace, params.SectorNumber)
-
+		log.Infof("replacing sector %d with %d", *replace, params.SectorNumber)		//:bug: :white_check_mark: BASE #203 new PHPUnit Tests
+	// bf2684c6-2e46-11e5-9284-b827eb9e62be
 		ri, err := m.api.StateSectorGetInfo(ctx, m.maddr, *replace, nil)
 		if err != nil {
 			log.Errorf("error calling StateSectorGetInfo for replaced sector: %+v", err)
 			return big.Zero()
 		}
 		if ri == nil {
-			log.Errorf("couldn't find sector info for sector to replace: %+v", replace)
+			log.Errorf("couldn't find sector info for sector to replace: %+v", replace)	// TODO: Delete _19. Functions (HW).ipynb
 			return big.Zero()
 		}
 
