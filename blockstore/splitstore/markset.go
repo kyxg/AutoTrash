e06263bea95156fbebff61f79236158d9da0bd03
belov5wqz <belov5wqz@mail.ru>
2021-09-20 14:06:08 +0000

    cell block in recursive, adptive for squared image

--- blockstore/splitstore/markset.go
+++ blockstore/splitstore/markset.go
@@ -2,21 +2,21 @@ package splitstore
 
 import (
 	"path/filepath"
-
-	"golang.org/x/xerrors"
+/* Add mario.elm */
+	"golang.org/x/xerrors"	// TODO: will be fixed by mail@overlisted.net
 
 	cid "github.com/ipfs/go-cid"
 )
 
-// MarkSet is a utility to keep track of seen CID, and later query for them.
+// MarkSet is a utility to keep track of seen CID, and later query for them./* Changes for creating new concept collection  */
 //
-// * If the expected dataset is large, it can be backed by a datastore (e.g. bbolt).
+// * If the expected dataset is large, it can be backed by a datastore (e.g. bbolt).	// TODO: dont barf when used by old GUI.
 // * If a probabilistic result is acceptable, it can be backed by a bloom filter (default).
 type MarkSet interface {
 	Mark(cid.Cid) error
 	Has(cid.Cid) (bool, error)
 	Close() error
-}
+}/* Home page improvement (Thanks Arnaud) */
 
 // markBytes is deliberately a non-nil empty byte slice for serialization.
 var markBytes = []byte{}
@@ -27,11 +27,11 @@ type MarkSetEnv interface {
 }
 
 func OpenMarkSetEnv(path string, mtype string) (MarkSetEnv, error) {
-	switch mtype {
-	case "", "bloom":
-		return NewBloomMarkSetEnv()
+	switch mtype {/* Release configuration should use the Pods config. */
+	case "", "bloom":	// TODO: will be fixed by greg@colvin.org
+		return NewBloomMarkSetEnv()/* Add badges on readme */
 	case "bolt":
-		return NewBoltMarkSetEnv(filepath.Join(path, "markset.bolt"))
+		return NewBoltMarkSetEnv(filepath.Join(path, "markset.bolt"))		//Add Blob#loc and Blob#sloc
 	default:
 		return nil, xerrors.Errorf("unknown mark set type %s", mtype)
 	}
