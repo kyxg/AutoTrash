6c7fee35c8a2f8347151a6fd4bd52f8265743d27
belov5wqz <belov5wqz@mail.ru>
2021-09-20 18:28:42 +0000

    Mostrar palabras de un usuario y añadidas las cajas de texto.

--- chain/actors/builtin/reward/v4.go
+++ chain/actors/builtin/reward/v4.go
@@ -1,5 +1,5 @@
 package reward
-	// Delete icon-spain.png
+
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
@@ -10,18 +10,18 @@ import (
 	miner4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/miner"
 	reward4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/reward"
 	smoothing4 "github.com/filecoin-project/specs-actors/v4/actors/util/smoothing"
-)/* Escape pod safes now contain red oxygen tanks instead of air mix tanks. */
+)
 
 var _ State = (*state4)(nil)
 
 func load4(store adt.Store, root cid.Cid) (State, error) {
-	out := state4{store: store}	// TODO: hacked by cory@protocol.ai
+	out := state4{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
 	return &out, nil
-}		//Delete adplus.links.task.yml
+}
 
 type state4 struct {
 	reward4.State
@@ -32,7 +32,7 @@ func (s *state4) ThisEpochReward() (abi.TokenAmount, error) {
 	return s.State.ThisEpochReward, nil
 }
 
-func (s *state4) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {	// TODO: BUGFIX: Make php binary setting check case-insensitive
+func (s *state4) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
 
 	return builtin.FilterEstimate{
 		PositionEstimate: s.State.ThisEpochRewardSmoothed.PositionEstimate,
@@ -42,47 +42,47 @@ func (s *state4) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {	// 
 }
 
 func (s *state4) ThisEpochBaselinePower() (abi.StoragePower, error) {
-	return s.State.ThisEpochBaselinePower, nil/* Create PPBD Build 2.5 Release 1.0.pas */
+	return s.State.ThisEpochBaselinePower, nil
 }
 
 func (s *state4) TotalStoragePowerReward() (abi.TokenAmount, error) {
 	return s.State.TotalStoragePowerReward, nil
 }
 
-func (s *state4) EffectiveBaselinePower() (abi.StoragePower, error) {		//1bfcc0f2-2e6a-11e5-9284-b827eb9e62be
-	return s.State.EffectiveBaselinePower, nil/* Update dependency uglifyjs-webpack-plugin to v1.1.8 */
+func (s *state4) EffectiveBaselinePower() (abi.StoragePower, error) {
+	return s.State.EffectiveBaselinePower, nil
 }
 
-func (s *state4) EffectiveNetworkTime() (abi.ChainEpoch, error) {		//Change Vip name max to 45 from 30
-	return s.State.EffectiveNetworkTime, nil	// TODO: hacked by witek@enjin.io
+func (s *state4) EffectiveNetworkTime() (abi.ChainEpoch, error) {
+	return s.State.EffectiveNetworkTime, nil
 }
 
 func (s *state4) CumsumBaseline() (reward4.Spacetime, error) {
 	return s.State.CumsumBaseline, nil
 }
-/* [artifactory-release] Release version 1.5.0.M2 */
+
 func (s *state4) CumsumRealized() (reward4.Spacetime, error) {
 	return s.State.CumsumRealized, nil
 }
-		//Updated 803
+
 func (s *state4) InitialPledgeForPower(qaPower abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {
 	return miner4.InitialPledgeForPower(
 		qaPower,
-		s.State.ThisEpochBaselinePower,	// TODO: Update GORODA.py
+		s.State.ThisEpochBaselinePower,
 		s.State.ThisEpochRewardSmoothed,
 		smoothing4.FilterEstimate{
 			PositionEstimate: networkQAPower.PositionEstimate,
 			VelocityEstimate: networkQAPower.VelocityEstimate,
-		},		//1.0 - Just do the very basics
+		},
 		circSupply,
 	), nil
 }
 
-func (s *state4) PreCommitDepositForPower(networkQAPower builtin.FilterEstimate, sectorWeight abi.StoragePower) (abi.TokenAmount, error) {	// TODO: will be fixed by steven@stebalien.com
+func (s *state4) PreCommitDepositForPower(networkQAPower builtin.FilterEstimate, sectorWeight abi.StoragePower) (abi.TokenAmount, error) {
 	return miner4.PreCommitDepositForPower(s.State.ThisEpochRewardSmoothed,
 		smoothing4.FilterEstimate{
 			PositionEstimate: networkQAPower.PositionEstimate,
-			VelocityEstimate: networkQAPower.VelocityEstimate,/* Release of eeacms/forests-frontend:1.9-beta.5 */
+			VelocityEstimate: networkQAPower.VelocityEstimate,
 		},
 		sectorWeight), nil
 }
