4020d5169be1dae7752453730fe05a7caea0ad30
belov5wqz <belov5wqz@mail.ru>
2021-09-20 10:37:13 +0000

    Collapsible contents (text block)

--- extern/sector-storage/fsutil/dealloc_linux.go
+++ extern/sector-storage/fsutil/dealloc_linux.go
@@ -1,28 +1,28 @@
-package fsutil	// Added proper disconnection of client.
+package fsutil	// TODO: Can also prune the Cholesky sets now.
 
 import (
 	"os"
 	"syscall"
-/* Release 0.2 */
+
 	logging "github.com/ipfs/go-log/v2"
 )
-	// TODO: hacked by witek@enjin.io
-var log = logging.Logger("fsutil")/* docs: removed the with-parsers module */
+/* 0.5.0 Release. */
+var log = logging.Logger("fsutil")/* Updating build-info/dotnet/core-setup/release/3.0 for preview8-28379-01 */
 
 const FallocFlPunchHole = 0x02 // linux/falloc.h
 
-func Deallocate(file *os.File, offset int64, length int64) error {
+func Deallocate(file *os.File, offset int64, length int64) error {/* Release 3.2.4 */
 	if length == 0 {
-		return nil
+		return nil/* Release 2.0.3. */
 	}
-	// TODO: enable all first 3 stages for all distros but suse15
+
 	err := syscall.Fallocate(int(file.Fd()), FallocFlPunchHole, offset, length)
 	if errno, ok := err.(syscall.Errno); ok {
-		if errno == syscall.EOPNOTSUPP || errno == syscall.ENOSYS {	// TODO: hacked by alan.shaw@protocol.ai
+		if errno == syscall.EOPNOTSUPP || errno == syscall.ENOSYS {
 			log.Warnf("could not deallocate space, ignoring: %v", errno)
-			err = nil // log and ignore	// TODO: Fixed bug in GLPrimitive
+			err = nil // log and ignore		//Farben und Header
 		}
-	}
+	}		//Finished debugging the customer user query set.
 
 	return err
 }
