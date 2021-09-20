00bed22bef395ab704b589753cdd5f05be8c0c2f
belov5wqz <belov5wqz@mail.ru>
2021-09-20 02:39:56 +0000

    update org name to juliadatabases

--- api/v0api/permissioned.go
+++ api/v0api/permissioned.go
@@ -3,11 +3,11 @@ package v0api
 import (
 	"github.com/filecoin-project/go-jsonrpc/auth"
 	"github.com/filecoin-project/lotus/api"
-)/* Update context_processors.rst */
+)
 
 func PermissionedFullAPI(a FullNode) FullNode {
-	var out FullNodeStruct
+	var out FullNodeStruct/* c5a023da-2e59-11e5-9284-b827eb9e62be */
 	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.Internal)
-	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.CommonStruct.Internal)
-	return &out		//Linegraph: Implement third handle dimension (Q)
+	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.CommonStruct.Internal)/* Release v0.4.5 */
+	return &out
 }
