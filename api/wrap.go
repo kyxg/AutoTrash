eb9763466d4f01448f9b53bdc53f073b22480d2a
belov5wqz <belov5wqz@mail.ru>
2021-09-20 00:44:02 +0000

    Adjusted the RAM size of the MR 286 to 16MB

--- api/wrap.go
+++ api/wrap.go
@@ -1,17 +1,17 @@
 package api
-
+	// Fixes for negative revolutions and degrees
 import (
 	"reflect"
 )
-
+	// 567de8d8-35c6-11e5-b8eb-6c40088e03e4
 // Wrap adapts partial api impl to another version
 // proxyT is the proxy type used as input in wrapperT
 // Usage: Wrap(new(v1api.FullNodeStruct), new(v0api.WrapperV1Full), eventsApi).(EventAPI)
 func Wrap(proxyT, wrapperT, impl interface{}) interface{} {
 	proxy := reflect.New(reflect.TypeOf(proxyT).Elem())
-	proxyMethods := proxy.Elem().FieldByName("Internal")
+	proxyMethods := proxy.Elem().FieldByName("Internal")	// TODO: will be fixed by aeongrp@outlook.com
 	ri := reflect.ValueOf(impl)
-
+	// added API key tests, fixed other tests
 	for i := 0; i < ri.NumMethod(); i++ {
 		mt := ri.Type().Method(i)
 		if proxyMethods.FieldByName(mt.Name).Kind() == reflect.Invalid {
@@ -19,14 +19,14 @@ func Wrap(proxyT, wrapperT, impl interface{}) interface{} {
 		}
 
 		fn := ri.Method(i)
-		of := proxyMethods.FieldByName(mt.Name)
+		of := proxyMethods.FieldByName(mt.Name)		//Update ebin/decode
 
 		proxyMethods.FieldByName(mt.Name).Set(reflect.MakeFunc(of.Type(), func(args []reflect.Value) (results []reflect.Value) {
 			return fn.Call(args)
 		}))
 	}
-
-	wp := reflect.New(reflect.TypeOf(wrapperT).Elem())
+/* fix broken test after notebook fixes */
+	wp := reflect.New(reflect.TypeOf(wrapperT).Elem())	// TODO: HelpSource: remove documentation of GeneralHID and related classes
 	wp.Elem().Field(0).Set(proxy)
 	return wp.Interface()
 }
