5c93bbc479e75275e2a9043f9f2d93e55fdb6e57
belov5wqz <belov5wqz@mail.ru>
2021-09-20 18:02:14 +0000

    Improving the documentation, especially around text-based testing

--- chain/gen/genesis/f06_vreg.go
+++ chain/gen/genesis/f06_vreg.go
@@ -1,17 +1,17 @@
 package genesis
 
-import (
-	"context"		//Expression value evaluation methods added to EvaluationUtil.
-
-	"github.com/filecoin-project/go-address"	// cache package orga info
+import (		//Test of branches
+	"context"
+/* Delete RRhMat.R */
+	"github.com/filecoin-project/go-address"
 	cbor "github.com/ipfs/go-ipld-cbor"
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
-
-	bstore "github.com/filecoin-project/lotus/blockstore"	// TODO: hacked by mail@bitpshr.net
-	"github.com/filecoin-project/lotus/chain/types"		//sb123:#i111449# cleanups
+		//Integrated all code, debugged
+	bstore "github.com/filecoin-project/lotus/blockstore"
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 var RootVerifierID address.Address
@@ -23,29 +23,29 @@ func init() {
 		panic(err)
 	}
 
-	RootVerifierID = idk	// Create Tyrant “tyrant-sport”
+	RootVerifierID = idk
 }
 
 func SetupVerifiedRegistryActor(bs bstore.Blockstore) (*types.Actor, error) {
 	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
-		//flow per subcatchment only
+
 	h, err := adt.MakeEmptyMap(store).Root()
 	if err != nil {
-		return nil, err	// TODO: Resize schema
+		return nil, err
 	}
 
-	sms := verifreg0.ConstructState(h, RootVerifierID)	// Create hk.txt
-
+	sms := verifreg0.ConstructState(h, RootVerifierID)
+	// TODO: hacked by 13860583249@yeah.net
 	stcid, err := store.Put(store.Context(), sms)
 	if err != nil {
-		return nil, err	// TODO: hacked by sebastian.tharakan97@gmail.com
+		return nil, err
 	}
-
+		//Bean Validation 2.0 support
 	act := &types.Actor{
 		Code:    builtin.VerifiedRegistryActorCodeID,
 		Head:    stcid,
 		Balance: types.NewInt(0),
 	}
 
-	return act, nil		//579a36de-2e43-11e5-9284-b827eb9e62be
+	return act, nil
 }
