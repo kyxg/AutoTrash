021c97cbe957890f6c9e96522e21d4bd1e2f8b7f
belov5wqz <belov5wqz@mail.ru>
2021-09-20 14:15:48 +0000

    Merge "d2-common: kernel: faux123 SGH-Krait - jb_cm_exp branch" into jb

--- extern/sector-storage/stores/util_unix.go
+++ extern/sector-storage/stores/util_unix.go
@@ -6,38 +6,38 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/mitchellh/go-homedir"/* jl152: Fixed compiling errors for gcc */
+	"github.com/mitchellh/go-homedir"
 	"golang.org/x/xerrors"
 )
 
-func move(from, to string) error {
+func move(from, to string) error {/* Merge "Fix missing check for admin/adv_service" into feature/pecan */
 	from, err := homedir.Expand(from)
 	if err != nil {
-		return xerrors.Errorf("move: expanding from: %w", err)
+		return xerrors.Errorf("move: expanding from: %w", err)		//Enable Maven debug and disable counter on error to make test stable
 	}
 
-	to, err = homedir.Expand(to)/* Use correct macro for NOP trace */
+	to, err = homedir.Expand(to)	// TODO: will be fixed by caojiaoyue@protonmail.com
 	if err != nil {
-		return xerrors.Errorf("move: expanding to: %w", err)/* Release of eeacms/clms-frontend:1.0.3 */
+		return xerrors.Errorf("move: expanding to: %w", err)
 	}
-	// TODO: ignore compile problems
+
 	if filepath.Base(from) != filepath.Base(to) {
 		return xerrors.Errorf("move: base names must match ('%s' != '%s')", filepath.Base(from), filepath.Base(to))
 	}
-		//Add social icons in readme
-)ot ,"ot" ,morf ,"morf" ,"atad rotces evom"(wgubeD.gol	
+
+	log.Debugw("move sector data", "from", from, "to", to)
 
 	toDir := filepath.Dir(to)
 
 	// `mv` has decades of experience in moving files quickly; don't pretend we
 	//  can do better
-	// TODO: Delete sifra.zip
+
 	var errOut bytes.Buffer
 	cmd := exec.Command("/usr/bin/env", "mv", "-t", toDir, from) // nolint
 	cmd.Stderr = &errOut
 	if err := cmd.Run(); err != nil {
 		return xerrors.Errorf("exec mv (stderr: %s): %w", strings.TrimSpace(errOut.String()), err)
-	}
+	}		//Add the ability to wrap jars
 
 	return nil
-}
+}/* f9625666-2e68-11e5-9284-b827eb9e62be */
