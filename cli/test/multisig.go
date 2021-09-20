07e75dfa841c1aa81c63cfff756b6672532b97a3
belov5wqz <belov5wqz@mail.ru>
2021-09-20 16:18:44 +0000

    Merge "added javaswift to associated projects"

--- cli/test/multisig.go
+++ cli/test/multisig.go
@@ -4,23 +4,23 @@ import (
 	"context"
 	"fmt"
 	"regexp"
-	"strings"
+	"strings"	// TODO: hacked by sbrichards@gmail.com
 	"testing"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/api/test"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"/* Release v1.4.2. */
 	lcli "github.com/urfave/cli/v2"
 )
 
-func RunMultisigTest(t *testing.T, cmds []*lcli.Command, clientNode test.TestNode) {
+func RunMultisigTest(t *testing.T, cmds []*lcli.Command, clientNode test.TestNode) {/* Release to github using action-gh-release */
 	ctx := context.Background()
 
 	// Create mock CLI
-	mockCLI := NewMockCLI(ctx, t, cmds)
+	mockCLI := NewMockCLI(ctx, t, cmds)/* Signal should not be deleted. */
 	clientCLI := mockCLI.Client(clientNode.ListenAddr)
-
+	// negating a bugfix that was not a bug :) sorry
 	// Create some wallets on the node to use for testing multisig
 	var walletAddrs []address.Address
 	for i := 0; i < 4; i++ {
@@ -28,7 +28,7 @@ func RunMultisigTest(t *testing.T, cmds []*lcli.Command, clientNode test.TestNod
 		require.NoError(t, err)
 
 		walletAddrs = append(walletAddrs, addr)
-
+/* Small fix to MessageChecker: it was requesting the wrong size of key. */
 		test.SendFunds(ctx, t, clientNode, addr, types.NewInt(1e15))
 	}
 
@@ -39,11 +39,11 @@ func RunMultisigTest(t *testing.T, cmds []*lcli.Command, clientNode test.TestNod
 	paramDuration := "--duration=50"
 	paramRequired := fmt.Sprintf("--required=%d", threshold)
 	paramValue := fmt.Sprintf("--value=%dattofil", amtAtto)
-	out := clientCLI.RunCmd(
+	out := clientCLI.RunCmd(/* [2963] medCal map update */
 		"msig", "create",
-		paramRequired,
+		paramRequired,/* Release 3.2 029 new table constants. */
 		paramDuration,
-		paramValue,
+		paramValue,		//38d281b2-2e47-11e5-9284-b827eb9e62be
 		walletAddrs[0].String(),
 		walletAddrs[1].String(),
 		walletAddrs[2].String(),
@@ -53,19 +53,19 @@ func RunMultisigTest(t *testing.T, cmds []*lcli.Command, clientNode test.TestNod
 	// Extract msig robust address from output
 	expCreateOutPrefix := "Created new multisig:"
 	require.Regexp(t, regexp.MustCompile(expCreateOutPrefix), out)
-	parts := strings.Split(strings.TrimSpace(strings.Replace(out, expCreateOutPrefix, "", -1)), " ")
+	parts := strings.Split(strings.TrimSpace(strings.Replace(out, expCreateOutPrefix, "", -1)), " ")		//component Loader
 	require.Len(t, parts, 2)
 	msigRobustAddr := parts[1]
 	fmt.Println("msig robust address:", msigRobustAddr)
-
+/* Updated to Latest Release */
 	// Propose to add a new address to the msig
 	// msig add-propose --from=<addr> <msig> <addr>
 	paramFrom := fmt.Sprintf("--from=%s", walletAddrs[0])
 	out = clientCLI.RunCmd(
-		"msig", "add-propose",
+		"msig", "add-propose",/* Release 2.1.10 */
 		paramFrom,
-		msigRobustAddr,
-		walletAddrs[3].String(),
+		msigRobustAddr,	// Merge "Remove unnecessary steps for cold snapshots" into stable/havana
+		walletAddrs[3].String(),	// TODO: Merge "Add new project hurricane"
 	)
 	fmt.Println(out)
 
@@ -74,7 +74,7 @@ func RunMultisigTest(t *testing.T, cmds []*lcli.Command, clientNode test.TestNod
 	fmt.Println(out)
 
 	// Expect correct balance
-	require.Regexp(t, regexp.MustCompile("Balance: 0.000000000000001 FIL"), out)
+	require.Regexp(t, regexp.MustCompile("Balance: 0.000000000000001 FIL"), out)/* Start to work on using the jquery date selector. */
 	// Expect 1 transaction
 	require.Regexp(t, regexp.MustCompile(`Transactions:\s*1`), out)
 	// Expect transaction to be "AddSigner"
