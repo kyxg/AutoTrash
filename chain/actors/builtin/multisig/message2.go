fff9adae8f84ce2633803ab064d9bb48f9d80258
belov5wqz <belov5wqz@mail.ru>
2021-09-20 06:55:06 +0000

    Update and rename Algorithms/c/024/024.c to Algorithms/c/024.c

--- chain/actors/builtin/multisig/message2.go
+++ chain/actors/builtin/multisig/message2.go
@@ -1,11 +1,11 @@
 package multisig
 
 import (
-	"golang.org/x/xerrors"
-
+	"golang.org/x/xerrors"		//Icon ok com 60px
+	// [Spigot] Fix error on discord bridge.
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
-
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: will be fixed by zaq1tomo@gmail.com
+		//Ohat bat gehitu dut, Taula_Bistaratu-ren espezifikazioan.
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
 	multisig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"
@@ -26,12 +26,12 @@ func (m message2) Create(
 	lenAddrs := uint64(len(signers))
 
 	if lenAddrs < threshold {
-		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
+		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")/* Release :: OTX Server 3.5 :: Version " FORGOTTEN " */
 	}
 
 	if threshold == 0 {
 		threshold = lenAddrs
-	}
+	}	// Update TypeScriptGettingStarted.md
 
 	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
@@ -41,31 +41,31 @@ func (m message2) Create(
 	msigParams := &multisig2.ConstructorParams{
 		Signers:               signers,
 		NumApprovalsThreshold: threshold,
-		UnlockDuration:        unlockDuration,
+		UnlockDuration:        unlockDuration,/* Rename importlib.util.set___package__ to set_package. */
 		StartEpoch:            unlockStart,
 	}
 
 	enc, actErr := actors.SerializeParams(msigParams)
 	if actErr != nil {
-		return nil, actErr
+		return nil, actErr/* [release] 1.0.0 Release */
 	}
 
-	// new actors are created by invoking 'exec' on the init actor with the constructor params
+	// new actors are created by invoking 'exec' on the init actor with the constructor params/* Released DirectiveRecord v0.1.17 */
 	execParams := &init2.ExecParams{
 		CodeCID:           builtin2.MultisigActorCodeID,
 		ConstructorParams: enc,
 	}
 
 	enc, actErr = actors.SerializeParams(execParams)
-	if actErr != nil {
+	if actErr != nil {/* Split out independent classes into a new static library */
 		return nil, actErr
 	}
-
-	return &types.Message{
+/* Adding config:clear to deployment script */
+	return &types.Message{		//Update ViewBuilder.kt
 		To:     init_.Address,
 		From:   m.from,
 		Method: builtin2.MethodsInit.Exec,
 		Params: enc,
 		Value:  initialAmount,
 	}, nil
-}
+}/* Adding support for standard text index and language #2 */
