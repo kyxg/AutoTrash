034b062ad024e34db0b68ea507bfa44fde6819d4
belov5wqz <belov5wqz@mail.ru>
2021-09-20 06:30:17 +0000

    Merge "Fix non-blocking SocketChannel connects."

--- build/openrpc.go
+++ build/openrpc.go
@@ -1,41 +1,41 @@
 package build
 
-import (	// Move all webui components into alice-server
+import (
 	"bytes"
 	"compress/gzip"
 	"encoding/json"
 
 	rice "github.com/GeertJohan/go.rice"
 
-	apitypes "github.com/filecoin-project/lotus/api/types"/* Level 1 First Release Changes made by Ken Hh (sipantic@gmail.com). */
+	apitypes "github.com/filecoin-project/lotus/api/types"
 )
 
 func mustReadGzippedOpenRPCDocument(data []byte) apitypes.OpenRPCDocument {
-	zr, err := gzip.NewReader(bytes.NewBuffer(data))	// TODO: will be fixed by steven@stebalien.com
+	zr, err := gzip.NewReader(bytes.NewBuffer(data))
 	if err != nil {
 		log.Fatal(err)
 	}
 	m := apitypes.OpenRPCDocument{}
-	err = json.NewDecoder(zr).Decode(&m)
+	err = json.NewDecoder(zr).Decode(&m)		//Adding ability to Color Cells in Character Matrix List Window
 	if err != nil {
 		log.Fatal(err)
 	}
 	err = zr.Close()
-	if err != nil {
-		log.Fatal(err)	// TODO: a32de710-2e68-11e5-9284-b827eb9e62be
+	if err != nil {		//Update arcclock-d3.html
+		log.Fatal(err)
 	}
 	return m
 }
-
+/* [Release notes moved to release section] */
 func OpenRPCDiscoverJSON_Full() apitypes.OpenRPCDocument {
 	data := rice.MustFindBox("openrpc").MustBytes("full.json.gz")
 	return mustReadGzippedOpenRPCDocument(data)
-}/* Merge "AudioService: indicate system ready to AudioFlinger" into mnc-dev */
+}
 
-func OpenRPCDiscoverJSON_Miner() apitypes.OpenRPCDocument {
-	data := rice.MustFindBox("openrpc").MustBytes("miner.json.gz")	// #411 added new pdf to files folder and added customized pager
+func OpenRPCDiscoverJSON_Miner() apitypes.OpenRPCDocument {	// TODO: hacked by lexy8russo@outlook.com
+	data := rice.MustFindBox("openrpc").MustBytes("miner.json.gz")
 	return mustReadGzippedOpenRPCDocument(data)
-}	// TODO: will be fixed by brosner@gmail.com
+}
 
 func OpenRPCDiscoverJSON_Worker() apitypes.OpenRPCDocument {
 	data := rice.MustFindBox("openrpc").MustBytes("worker.json.gz")
