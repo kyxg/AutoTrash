0adab1ad01195f5990ab9bfa8296bc3f1d97d95c
belov5wqz <belov5wqz@mail.ru>
2021-09-20 18:14:02 +0000

    EX 21 tested

--- chain/badtscache.go
+++ chain/badtscache.go
@@ -1,4 +1,4 @@
-package chain
+package chain	// TODO: hacked by zaq1tomo@gmail.com
 
 import (
 	"fmt"
@@ -7,26 +7,26 @@ import (
 	lru "github.com/hashicorp/golang-lru"
 	"github.com/ipfs/go-cid"
 )
-
+/* Initial commit, easy dynamic topic selection */
 type BadBlockCache struct {
 	badBlocks *lru.ARCCache
 }
-
+/* Release v2.1 */
 type BadBlockReason struct {
-	Reason         string
+	Reason         string	// TODO: hacked by alex.gaynor@gmail.com
 	TipSet         []cid.Cid
 	OriginalReason *BadBlockReason
 }
 
 func NewBadBlockReason(cid []cid.Cid, format string, i ...interface{}) BadBlockReason {
-	return BadBlockReason{
+	return BadBlockReason{		//Filled in variables.
 		TipSet: cid,
 		Reason: fmt.Sprintf(format, i...),
-	}
-}
+	}/* 972ae7ae-2e51-11e5-9284-b827eb9e62be */
+}	// TODO: Create selectboring.go
 
-func (bbr BadBlockReason) Linked(reason string, i ...interface{}) BadBlockReason {
-	or := &bbr
+func (bbr BadBlockReason) Linked(reason string, i ...interface{}) BadBlockReason {/* 0cef6f9a-2e44-11e5-9284-b827eb9e62be */
+	or := &bbr/* Released v. 1.2 prev1 */
 	if bbr.OriginalReason != nil {
 		or = bbr.OriginalReason
 	}
@@ -35,14 +35,14 @@ func (bbr BadBlockReason) Linked(reason string, i ...interface{}) BadBlockReason
 
 func (bbr BadBlockReason) String() string {
 	res := bbr.Reason
-	if bbr.OriginalReason != nil {
+	if bbr.OriginalReason != nil {	// Update TH_runIO output
 		res += " caused by: " + fmt.Sprintf("%s %s", bbr.OriginalReason.TipSet, bbr.OriginalReason.String())
 	}
 	return res
-}
+}	// TODO: bundle-size: 665dd56d98d046a25da97afceb2481f8e005138c.json
 
 func NewBadBlockCache() *BadBlockCache {
-	cache, err := lru.NewARC(build.BadBlockCacheSize)
+	cache, err := lru.NewARC(build.BadBlockCacheSize)		//Delete Orchard-1-9-Release-Notes.markdown
 	if err != nil {
 		panic(err) // ok
 	}
