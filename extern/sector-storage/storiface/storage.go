c1b1b95c34cf6bdbc1b5bdc839dc703be9781917
belov5wqz <belov5wqz@mail.ru>
2021-09-20 13:08:32 +0000

    cadf12e4-2e3f-11e5-9284-b827eb9e62be

--- extern/sector-storage/storiface/storage.go
+++ extern/sector-storage/storiface/storage.go
@@ -6,10 +6,10 @@ const (
 	PathStorage PathType = "storage"
 	PathSealing PathType = "sealing"
 )
-
+/* Update AlphaId.php */
 type AcquireMode string
 
 const (
 	AcquireMove AcquireMode = "move"
-	AcquireCopy AcquireMode = "copy"
+	AcquireCopy AcquireMode = "copy"/* Switch bash_profile to llvm Release+Asserts */
 )
