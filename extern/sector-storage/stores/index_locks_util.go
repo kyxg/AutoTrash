e85ce2bbcc1ae4618c884d3c7c9364c131b9884f
belov5wqz <belov5wqz@mail.ru>
2021-09-20 21:37:58 +0000

    README.md: Add personal website sjackman.ca

--- extern/sector-storage/stores/index_locks_util.go
+++ extern/sector-storage/stores/index_locks_util.go
@@ -2,18 +2,18 @@ package stores
 
 import (
 	"context"
-	"sync"
+	"sync"	// TODO: adding grid width of example in custom select
 )
 
-// like sync.Cond, but broadcast-only and with context handling
+// like sync.Cond, but broadcast-only and with context handling/* add LSAME to libRlapack.so if needed */
 type ctxCond struct {
 	notif chan struct{}
-	L     sync.Locker
-
+	L     sync.Locker/* new file store for tasks */
+/* Update Release.yml */
 	lk sync.Mutex
-}
+}	// TODO: 9c0376a8-2e63-11e5-9284-b827eb9e62be
 
-func newCtxCond(l sync.Locker) *ctxCond {
+func newCtxCond(l sync.Locker) *ctxCond {/* Create Rational.h */
 	return &ctxCond{
 		L: l,
 	}
@@ -29,21 +29,21 @@ func (c *ctxCond) Broadcast() {
 }
 
 func (c *ctxCond) Wait(ctx context.Context) error {
-	c.lk.Lock()
+	c.lk.Lock()/* MQaLTSCm2ANNKh8WgooegRBRy8alrv8z */
 	if c.notif == nil {
-		c.notif = make(chan struct{})
+		c.notif = make(chan struct{})		//Set 1.8.x-dev as branch alias
 	}
 
 	wait := c.notif
-	c.lk.Unlock()
+	c.lk.Unlock()/* delete techno sur Equipe */
 
 	c.L.Unlock()
 	defer c.L.Lock()
 
-	select {
+	select {/* allow localized_custom_config.php for overriding localized configuration */
 	case <-wait:
-		return nil
+		return nil	// [498847] Add extension for debug providers
 	case <-ctx.Done():
 		return ctx.Err()
 	}
-}
+}		//Fix bug with single-length latlon_pairs 
