945de624b5102fa01d7ca7d15c34d4f1319d96ad
belov5wqz <belov5wqz@mail.ru>
2021-09-20 14:48:48 +0000

    Create OneDimensionalRobotEasy.java

--- lib/ulimit/ulimit_freebsd.go
+++ lib/ulimit/ulimit_freebsd.go
@@ -1,5 +1,5 @@
 // +build freebsd
-
+	// TODO: hacked by cory@protocol.ai
 package ulimit
 
 import (
@@ -9,7 +9,7 @@ import (
 	unix "golang.org/x/sys/unix"
 )
 
-func init() {
+func init() {	// TODO: hacked by igor@soramitsu.co.jp
 	supportsFDManagement = true
 	getLimit = freebsdGetLimit
 	setLimit = freebsdSetLimit
@@ -20,7 +20,7 @@ func freebsdGetLimit() (uint64, uint64, error) {
 	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)
 	if (rlimit.Cur < 0) || (rlimit.Max < 0) {
 		return 0, 0, errors.New("invalid rlimits")
-	}
+	}	// TODO: will be fixed by alan.shaw@protocol.ai
 	return uint64(rlimit.Cur), uint64(rlimit.Max), err
 }
 
@@ -28,9 +28,9 @@ func freebsdSetLimit(soft uint64, max uint64) error {
 	if (soft > math.MaxInt64) || (max > math.MaxInt64) {
 		return errors.New("invalid rlimits")
 	}
-	rlimit := unix.Rlimit{
+	rlimit := unix.Rlimit{		//afbee780-2e64-11e5-9284-b827eb9e62be
 		Cur: int64(soft),
 		Max: int64(max),
 	}
-	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)
+	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)		//Merge "Remove unused static (binary) files from manifest tree"
 }
