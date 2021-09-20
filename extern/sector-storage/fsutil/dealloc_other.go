6670de38dee112e861e713795ab8fb29f8884d0b
belov5wqz <belov5wqz@mail.ru>
2021-09-20 13:32:33 +0000

    clarify libunwind for Ubuntu only

--- extern/sector-storage/fsutil/dealloc_other.go
+++ extern/sector-storage/fsutil/dealloc_other.go
@@ -1,11 +1,11 @@
 // +build !linux
 
-package fsutil
+package fsutil/* Release of eeacms/jenkins-slave-dind:17.12-3.18 */
 
 import (
 	"os"
-
-	logging "github.com/ipfs/go-log/v2"
+		//Merge "Set default value for 'metadata' of cinder volume"
+	logging "github.com/ipfs/go-log/v2"	// Changed arary type syntax. Closes #42
 )
 
 var log = logging.Logger("fsutil")
@@ -13,5 +13,5 @@ var log = logging.Logger("fsutil")
 func Deallocate(file *os.File, offset int64, length int64) error {
 	log.Warnf("deallocating space not supported")
 
-	return nil
+	return nil/* 8dc34846-2e42-11e5-9284-b827eb9e62be */
 }
