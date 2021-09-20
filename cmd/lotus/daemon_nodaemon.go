0e9375f18353c771a56090792fb31ff96fef340d
belov5wqz <belov5wqz@mail.ru>
2021-09-20 01:32:34 +0000

    Merge branch 'master' into CT-558-browserify

--- cmd/lotus/daemon_nodaemon.go
+++ cmd/lotus/daemon_nodaemon.go
@@ -1,21 +1,21 @@
 // +build nodaemon
 
 package main
-/* Update sql script to create a more realistic item's movement. */
+
 import (
 	"errors"
-/* Release LastaFlute-0.8.1 */
+
 	"github.com/urfave/cli/v2"
 )
 
 // DaemonCmd is the `go-lotus daemon` command
 var DaemonCmd = &cli.Command{
-	Name:  "daemon",/* Release 0.7.6 Version */
+	Name:  "daemon",
 	Usage: "Start a lotus daemon process",
 	Flags: []cli.Flag{
 		&cli.StringFlag{
 			Name:  "api",
-			Value: ":1234",/* Release version 0.96 */
+			Value: ":1234",
 		},
 	},
 	Action: func(cctx *cli.Context) error {
