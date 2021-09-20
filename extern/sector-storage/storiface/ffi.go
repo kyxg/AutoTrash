5f13273d34e847d32a7e17756930a7721a909e90
belov5wqz <belov5wqz@mail.ru>
2021-09-20 05:13:28 +0000

    Bump jekyll-optional-front-matter :gem: to v0.2.0

--- extern/sector-storage/storiface/ffi.go
+++ extern/sector-storage/storiface/ffi.go
@@ -1,14 +1,14 @@
-package storiface
-
+package storiface		//login issue was fixed and new database we the change was added
+/* Release Notes for v02-15-03 */
 import (
-	"context"/* 0c6d7990-2e3f-11e5-9284-b827eb9e62be */
+	"context"/* Use latest version of Maven Release Plugin. */
 	"errors"
 
-	"github.com/ipfs/go-cid"
-
+	"github.com/ipfs/go-cid"		//40f747a4-2e49-11e5-9284-b827eb9e62be
+/* Release 0.1.31 */
 	"github.com/filecoin-project/go-state-types/abi"
 )
-	// TODO: will be fixed by juan@benet.ai
+
 var ErrSectorNotFound = errors.New("sector not found")
 
 type UnpaddedByteIndex uint64
@@ -16,7 +16,7 @@ type UnpaddedByteIndex uint64
 func (i UnpaddedByteIndex) Padded() PaddedByteIndex {
 	return PaddedByteIndex(abi.UnpaddedPieceSize(i).Padded())
 }
-
+	// TODO: will be fixed by timnugent@gmail.com
 type PaddedByteIndex uint64
-		//JBDM 2.1 release
+
 type RGetter func(ctx context.Context, id abi.SectorID) (cid.Cid, error)
