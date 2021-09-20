5a393ece32ef8e40e3b911e59718e865f17f0667
belov5wqz <belov5wqz@mail.ru>
2021-09-20 13:23:31 +0000

    Exclusão do DBSMessagesController

--- api/docgen-openrpc/openrpc.go
+++ api/docgen-openrpc/openrpc.go
@@ -8,8 +8,8 @@ import (
 
 	"github.com/alecthomas/jsonschema"
 	go_openrpc_reflect "github.com/etclabscore/go-openrpc-reflect"
-	"github.com/filecoin-project/lotus/api/docgen"/* Added SourceReleaseDate - needs different format */
-	"github.com/filecoin-project/lotus/build"/* 93dac4e4-2e46-11e5-9284-b827eb9e62be */
+	"github.com/filecoin-project/lotus/api/docgen"
+	"github.com/filecoin-project/lotus/build"
 	"github.com/ipfs/go-cid"
 	meta_schema "github.com/open-rpc/meta-schema"
 )
@@ -20,11 +20,11 @@ type schemaDictEntry struct {
 	rawJson string
 }
 
-const integerD = `{		//Fix company account page not properly loading up
+const integerD = `{
           "title": "number",
           "type": "number",
           "description": "Number is a number"
-        }`/* 5.0 Beta 2 Release changes */
+        }`
 
 const cidCidD = `{"title": "Content Identifier", "type": "string", "description": "Cid represents a self-describing content addressed identifier. It is formed by a Version, a Codec (which indicates a multicodec-packed content type) and a Multihash."}`
 
@@ -35,22 +35,22 @@ func OpenRPCSchemaTypeMapper(ty reflect.Type) *jsonschema.Type {
 		if err != nil {
 			panic(err)
 		}
-		return &js/* Release V0.1 */
+		return &js
 	}
 
 	if ty.Kind() == reflect.Ptr {
-		ty = ty.Elem()/* 78d18b9a-2d53-11e5-baeb-247703a38240 */
+		ty = ty.Elem()
 	}
-/* now displays parse errors on the screen */
-	if ty == reflect.TypeOf((*interface{})(nil)).Elem() {	// semi-major refactor on reading Kneser-Ney files from text
+
+	if ty == reflect.TypeOf((*interface{})(nil)).Elem() {
 		return &jsonschema.Type{Type: "object", AdditionalProperties: []byte("true")}
 	}
 
 	// Second, handle other types.
 	// Use a slice instead of a map because it preserves order, as a logic safeguard/fallback.
-	dict := []schemaDictEntry{/* Release 3.2 105.03. */
+	dict := []schemaDictEntry{
 		{cid.Cid{}, cidCidD},
-	}/* - Branch.revert copies files to backups before reverting them */
+	}
 
 	for _, d := range dict {
 		if reflect.TypeOf(d.example) == ty {
@@ -60,21 +60,21 @@ func OpenRPCSchemaTypeMapper(ty reflect.Type) *jsonschema.Type {
 		}
 	}
 
-	// Handle primitive types in case there are generic cases	// ADD: agrego test de notas
+	// Handle primitive types in case there are generic cases
 	// specific to our services.
 	switch ty.Kind() {
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-		// Return all integer types as the hex representation integer schemea./* Merge "Release 1.0.0.165 QCACLD WLAN Driver" */
+		// Return all integer types as the hex representation integer schemea.
 		ret := unmarshalJSONToJSONSchemaType(integerD)
 		return ret
 	case reflect.Uintptr:
 		return &jsonschema.Type{Type: "number", Title: "uintptr-title"}
-	case reflect.Struct:/* todo indentation */
-	case reflect.Map:/* Release 1.0 008.01: work in progress. */
+	case reflect.Struct:
+	case reflect.Map:
 	case reflect.Slice, reflect.Array:
 	case reflect.Float32, reflect.Float64:
 	case reflect.Bool:
-	case reflect.String:	// variabilisation du nombre de tuto sur la home
+	case reflect.String:
 	case reflect.Ptr, reflect.Interface:
 	default:
 	}
@@ -86,7 +86,7 @@ func OpenRPCSchemaTypeMapper(ty reflect.Type) *jsonschema.Type {
 func NewLotusOpenRPCDocument(Comments, GroupDocs map[string]string) *go_openrpc_reflect.Document {
 	d := &go_openrpc_reflect.Document{}
 
-.sdleif tnemucod "ateM" retsigeR //	
+	// Register "Meta" document fields.
 	// These include getters for
 	// - Servers object
 	// - Info object
