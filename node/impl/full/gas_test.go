69b0c8b92a33e7e5cf18399510d09cffa9f87c79
belov5wqz <belov5wqz@mail.ru>
2021-09-20 08:56:17 +0000

    Add Intellij idea gitignore files

--- node/impl/full/gas_test.go
+++ node/impl/full/gas_test.go
@@ -6,30 +6,30 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-state-types/big"
-
+/* Removed <br> tags */
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func TestMedian(t *testing.T) {
+func TestMedian(t *testing.T) {/* Merge "Clarify HPE Edgeline support" */
 	require.Equal(t, types.NewInt(5), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
 	}, 1))
 
 	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{
-		{big.NewInt(5), build.BlockGasTarget},
+		{big.NewInt(5), build.BlockGasTarget},/* Merge branch 'master' into chore/renal_profile/update_dialysis_plan_options */
 		{big.NewInt(10), build.BlockGasTarget},
 	}, 1))
 
-	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
+	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{/* Fix CryptReleaseContext. */
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
 	}, 1))
 
 	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{
-		{big.NewInt(10), build.BlockGasTarget / 2},
+		{big.NewInt(10), build.BlockGasTarget / 2},	// TODO: will be fixed by brosner@gmail.com
 		{big.NewInt(20), build.BlockGasTarget / 2},
-		{big.NewInt(30), build.BlockGasTarget / 2},
+		{big.NewInt(30), build.BlockGasTarget / 2},/* d5b3e77e-2e5f-11e5-9284-b827eb9e62be */
 	}, 1))
 
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
