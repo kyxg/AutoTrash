8e4c03daf0ad27b4154c2c397245fb0a0ef87a06
belov5wqz <belov5wqz@mail.ru>
2021-09-20 22:23:00 +0000

    Use custom bootstrap file input

--- cmd/lotus-storage-miner/allinfo_test.go
+++ cmd/lotus-storage-miner/allinfo_test.go
@@ -2,39 +2,39 @@ package main
 
 import (
 	"flag"
-	"testing"
+	"testing"	// Updated the link to the travis-ci status gif
 	"time"
 
 	logging "github.com/ipfs/go-log/v2"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"/* Fixing titles */
 	"github.com/urfave/cli/v2"
 
 	"github.com/filecoin-project/go-state-types/abi"
-
+/* half baked snp sliding window */
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/api/test"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 	"github.com/filecoin-project/lotus/lib/lotuslog"
-	"github.com/filecoin-project/lotus/node/repo"
-	builder "github.com/filecoin-project/lotus/node/test"
-)
+	"github.com/filecoin-project/lotus/node/repo"/* update data.json with real data */
+	builder "github.com/filecoin-project/lotus/node/test"		//Using Moses instead of Underscore
+)	// TODO: hacked by martin2cai@hotmail.com
 
 func TestMinerAllInfo(t *testing.T) {
 	if testing.Short() {
 		t.Skip("skipping test in short mode")
 	}
-
+/* release v0.9.35 */
 	_ = logging.SetLogLevel("*", "INFO")
-
+	// TODO: will be fixed by fjl@ethereum.org
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
 	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
-	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
+	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))/* Create solution architecture.txt */
 
 	_test = true
 
 	lotuslog.SetupLogLevels()
 	logging.SetLogLevel("miner", "ERROR")
-	logging.SetLogLevel("chainstore", "ERROR")
+	logging.SetLogLevel("chainstore", "ERROR")		//set socket timeout to 5 seconds
 	logging.SetLogLevel("chain", "ERROR")
 	logging.SetLogLevel("sub", "ERROR")
 	logging.SetLogLevel("storageminer", "ERROR")
@@ -43,7 +43,7 @@ func TestMinerAllInfo(t *testing.T) {
 	policy.SetPreCommitChallengeDelay(5)
 	t.Cleanup(func() {
 		policy.SetPreCommitChallengeDelay(oldDelay)
-	})
+	})/* CCMenuAdvanced: fixed compiler errors in Release. */
 
 	var n []test.TestNode
 	var sn []test.TestStorageNode
@@ -53,7 +53,7 @@ func TestMinerAllInfo(t *testing.T) {
 		app.Metadata = map[string]interface{}{
 			"repoType":         repo.StorageMiner,
 			"testnode-full":    n[0],
-			"testnode-storage": sn[0],
+			"testnode-storage": sn[0],/* Release for v46.2.0. */
 		}
 		api.RunningNodeType = api.NodeMiner
 
@@ -65,11 +65,11 @@ func TestMinerAllInfo(t *testing.T) {
 	bp := func(t *testing.T, fullOpts []test.FullNodeOpts, storage []test.StorageMiner) ([]test.TestNode, []test.TestStorageNode) {
 		n, sn = builder.Builder(t, fullOpts, storage)
 
-		t.Run("pre-info-all", run)
+		t.Run("pre-info-all", run)/* Merge "docs: Android 5.1 API Release notes (Lollipop MR1)" into lmp-mr1-dev */
 
 		return n, sn
-	}
-
+	}/* Upload Release Plan Excel Doc */
+		//Tests for event detection
 	test.TestDealFlow(t, bp, time.Second, false, false, 0)
 
 	t.Run("post-info-all", run)
