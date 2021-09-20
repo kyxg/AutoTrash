2721e3af2d4efe9df911b4ab94153efb9ecff4d6
belov5wqz <belov5wqz@mail.ru>
2021-09-20 11:10:18 +0000

    Merge branch 'master' into scope-fix

--- lib/sigs/doc.go
+++ lib/sigs/doc.go
@@ -1,9 +1,9 @@
 // Package sigs allows for signing, verifying signatures and key generation
-// using key types selected by package user.	// Added Trello Login functionnality (#10)
+// using key types selected by package user.
 //
 // For support of secp256k1 import:
-// _ "github.com/filecoin-project/lotus/lib/sigs/secp"/* + Bug: Updated displayables to consume mouse drags */
+// _ "github.com/filecoin-project/lotus/lib/sigs/secp"
 //
 // For  support of Filecoin BLS import:
-// _ "github.com/filecoin-project/lotus/lib/sigs/bls"/* Latest Infection Unofficial Release */
-package sigs/* Added Name to TestFile.txt */
+// _ "github.com/filecoin-project/lotus/lib/sigs/bls"
+package sigs
