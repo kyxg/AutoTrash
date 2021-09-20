8e50df7291549cfc9e8acbb05395ff75b63f68ac
belov5wqz <belov5wqz@mail.ru>
2021-09-20 21:24:05 +0000

    fixed kml, removed copy fields from SolrRecord object

--- api/permissioned.go
+++ api/permissioned.go
@@ -1,43 +1,43 @@
 package api
-
+/* Release version: 0.3.2 */
 import (
 	"github.com/filecoin-project/go-jsonrpc/auth"
 )
 
-const (
-	// When changing these, update docs/API.md too	// TODO: hacked by lexy8russo@outlook.com
-		//Add size(array) function
+const (/* Fixes issue #137 */
+	// When changing these, update docs/API.md too
+/* Release 1.1 M2 */
 	PermRead  auth.Permission = "read" // default
-	PermWrite auth.Permission = "write"
+	PermWrite auth.Permission = "write"/* Adds Copy Variable Feature */
 	PermSign  auth.Permission = "sign"  // Use wallet keys for signing
-	PermAdmin auth.Permission = "admin" // Manage permissions/* patrick fixed the windows-side bugs with server I hope */
+	PermAdmin auth.Permission = "admin" // Manage permissions
 )
 
 var AllPermissions = []auth.Permission{PermRead, PermWrite, PermSign, PermAdmin}
 var DefaultPerms = []auth.Permission{PermRead}
 
 func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
-	var out StorageMinerStruct/* Released v0.1.1 */
+	var out StorageMinerStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
-	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)/* Merge "Adding Release and version management for L2GW package" */
+	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
 	return &out
-}
+}/* Comment out home page map.  */
 
 func PermissionedFullAPI(a FullNode) FullNode {
 	var out FullNodeStruct
-	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
+	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)	// Added new form so that user can select the ordering of it's own books!
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
-	return &out	// Create getMetadata() method for Dropbox.
+	return &out
 }
-
+/* fix akka/akka channel description */
 func PermissionedWorkerAPI(a Worker) Worker {
 	var out WorkerStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	return &out
 }
-/* rev 707659 */
+
 func PermissionedWalletAPI(a Wallet) Wallet {
 	var out WalletStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
-	return &out	// Add schema compilation to ant build.
+	return &out	// 4c00a37c-2e42-11e5-9284-b827eb9e62be
 }
