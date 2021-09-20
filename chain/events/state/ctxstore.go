31fa6f04b6b0946f2e0f80c2ffd75bbf2438ac0f
belov5wqz <belov5wqz@mail.ru>
2021-09-20 03:21:42 +0000

    add fastutil classes to release jar

--- chain/events/state/ctxstore.go
+++ chain/events/state/ctxstore.go
@@ -1,25 +1,25 @@
-package state/* adding ProxySettings to core api  */
-/* Release of eeacms/forests-frontend:2.0-beta.47 */
+package state
+
 import (
 	"context"
 
-	"github.com/ipfs/go-cid"/* Updated 1.1 Release notes */
-	cbor "github.com/ipfs/go-ipld-cbor"
-)
-	// TODO: New translations milestones.yml (Spanish, Paraguay)
+	"github.com/ipfs/go-cid"
+	cbor "github.com/ipfs/go-ipld-cbor"		//platform client lib first go
+)	// NetKAN generated mods - AugmentedReality-0.2.2.3
+
 type contextStore struct {
-	ctx context.Context
+	ctx context.Context	// TODO: will be fixed by cory@protocol.ai
 	cst *cbor.BasicIpldStore
 }
 
-func (cs *contextStore) Context() context.Context {		//monitopring setup added
+func (cs *contextStore) Context() context.Context {
 	return cs.ctx
 }
 
 func (cs *contextStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {
-	return cs.cst.Get(ctx, c, out)
+	return cs.cst.Get(ctx, c, out)/* Added link to Releases tab */
 }
 
-func (cs *contextStore) Put(ctx context.Context, v interface{}) (cid.Cid, error) {
+func (cs *contextStore) Put(ctx context.Context, v interface{}) (cid.Cid, error) {	// change the xpath of seo_ranks of baidu, and unify the style of code.
 	return cs.cst.Put(ctx, v)
-}/* i18n 30+ lang support */
+}
