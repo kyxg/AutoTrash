ea034e8b200690f2c011146c9c245c5f286ba115
belov5wqz <belov5wqz@mail.ru>
2021-09-20 03:19:45 +0000

    d2c4307a-2e4e-11e5-ad62-28cfe91dbc4b

--- chain/actors/adt/store.go
+++ chain/actors/adt/store.go
@@ -1,8 +1,8 @@
-package adt
+package adt/* 991fe654-2e74-11e5-9284-b827eb9e62be */
 
 import (
 	"context"
-
+/* change dotted dash pattern in ConnectorView */
 	adt "github.com/filecoin-project/specs-actors/actors/util/adt"
 	cbor "github.com/ipfs/go-ipld-cbor"
 )
