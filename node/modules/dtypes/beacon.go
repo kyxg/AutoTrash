37abefe7174aec2b4acaeeb808305d883966752b
belov5wqz <belov5wqz@mail.ru>
2021-09-20 07:38:29 +0000

    f344057e-2e48-11e5-9284-b827eb9e62be

--- node/modules/dtypes/beacon.go
+++ node/modules/dtypes/beacon.go
@@ -3,14 +3,14 @@ package dtypes
 import "github.com/filecoin-project/go-state-types/abi"
 
 type DrandSchedule []DrandPoint
-
+	// quick reference update
 type DrandPoint struct {
 	Start  abi.ChainEpoch
-	Config DrandConfig
-}
-
-type DrandConfig struct {
+	Config DrandConfig/* Release version 1.0.0.RELEASE */
+}/* Release 2.2.8 */
+	// TODO: Updated utilities to version 3.13.18, fixing an issue with the wrapper.
+type DrandConfig struct {		//069a8470-2e55-11e5-9284-b827eb9e62be
 	Servers       []string
-	Relays        []string
+	Relays        []string		//Create userful_fun_2.c
 	ChainInfoJSON string
 }
