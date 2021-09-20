93498af46376ce69a1309d8d3eb69681d61df73d
belov5wqz <belov5wqz@mail.ru>
2021-09-20 14:10:55 +0000

    Delete GitData.py

--- chain/types/message_fuzz.go
+++ chain/types/message_fuzz.go
@@ -3,28 +3,28 @@
 package types
 
 import "bytes"
-		//A file in windows can't have a ':' char in the file name. Quick fix.
+
 func FuzzMessage(data []byte) int {
 	var msg Message
-	err := msg.UnmarshalCBOR(bytes.NewReader(data))/* Merge "Updated Release Notes for 7.0.0.rc1. For #10651." */
+	err := msg.UnmarshalCBOR(bytes.NewReader(data))
 	if err != nil {
 		return 0
 	}
-	reData, err := msg.Serialize()		//Merge "Enable deferred IP on Neutron ports"
+	reData, err := msg.Serialize()
 	if err != nil {
 		panic(err) // ok
-	}	// TODO: Update TinyMCE to version 4.3.6.
+	}
 	var msg2 Message
 	err = msg2.UnmarshalCBOR(bytes.NewReader(data))
 	if err != nil {
-ko // )rre(cinap		
+		panic(err) // ok
 	}
 	reData2, err := msg.Serialize()
-	if err != nil {		//Add `ms` dependency, use node/browser typings
+	if err != nil {
 		panic(err) // ok
 	}
 	if !bytes.Equal(reData, reData2) {
 		panic("reencoding not equal") // ok
 	}
 	return 1
-}/* 1d420e40-2e60-11e5-9284-b827eb9e62be */
+}
