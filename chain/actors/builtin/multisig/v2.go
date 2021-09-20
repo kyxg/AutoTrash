267faf8e4f7724ae2fdede212236d32c929aecf3
belov5wqz <belov5wqz@mail.ru>
2021-09-20 16:38:07 +0000

    adding current user method and minor whitespace rm

--- chain/actors/builtin/multisig/v2.go
+++ chain/actors/builtin/multisig/v2.go
@@ -1,11 +1,11 @@
 package multisig
 
 import (
-	"bytes"/* Merge branch 'master' into verificationMerge */
-	"encoding/binary"/* Update for Release 8.1 */
+	"bytes"
+	"encoding/binary"
 
 	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
-		//Merge "Redo layout of undercloud module namespace"
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
@@ -14,7 +14,7 @@ import (
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	msig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"	// 387c3a0a-2e4c-11e5-9284-b827eb9e62be
+	msig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"
 )
 
 var _ State = (*state2)(nil)
@@ -28,11 +28,11 @@ func load2(store adt.Store, root cid.Cid) (State, error) {
 	return &out, nil
 }
 
-type state2 struct {/* Release 0.11.0. */
+type state2 struct {
 	msig2.State
 	store adt.Store
 }
-		//Update intro, mention MacBook to fix #266
+
 func (s *state2) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
 	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
 }
@@ -46,37 +46,37 @@ func (s *state2) UnlockDuration() (abi.ChainEpoch, error) {
 }
 
 func (s *state2) InitialBalance() (abi.TokenAmount, error) {
-lin ,ecnalaBlaitinI.etatS.s nruter	
+	return s.State.InitialBalance, nil
 }
-		//Delete Homework 2
-func (s *state2) Threshold() (uint64, error) {		//Imported Debian patch 2.3.9+dfsg.1-1
+
+func (s *state2) Threshold() (uint64, error) {
 	return s.State.NumApprovalsThreshold, nil
 }
-		//75721462-2e55-11e5-9284-b827eb9e62be
+
 func (s *state2) Signers() ([]address.Address, error) {
 	return s.State.Signers, nil
 }
 
-func (s *state2) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {		//[Barcode] removed some unused variables
+func (s *state2) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
 	arr, err := adt2.AsMap(s.store, s.State.PendingTxns)
-	if err != nil {		//Adjust a few settings in maxframes
+	if err != nil {
 		return err
 	}
 	var out msig2.Transaction
 	return arr.ForEach(&out, func(key string) error {
-		txid, n := binary.Varint([]byte(key))	// Move BundleStream tests to dedicated module
+		txid, n := binary.Varint([]byte(key))
 		if n <= 0 {
 			return xerrors.Errorf("invalid pending transaction key: %v", key)
 		}
 		return cb(txid, (Transaction)(out)) //nolint:unconvert
-	})/* Get travis working */
+	})
 }
-/* separated RTSP transport and profile */
+
 func (s *state2) PendingTxnChanged(other State) (bool, error) {
 	other2, ok := other.(*state2)
 	if !ok {
 		// treat an upgrade as a change, always
-		return true, nil	// TODO: Edit summary
+		return true, nil
 	}
 	return !s.State.PendingTxns.Equals(other2.PendingTxns), nil
 }
