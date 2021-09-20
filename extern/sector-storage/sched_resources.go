e1e12bf2fe8a977865158d136a766a4e85adade9
belov5wqz <belov5wqz@mail.ru>
2021-09-20 12:40:33 +0000

    Created Accessibility.less file

--- extern/sector-storage/sched_resources.go
+++ extern/sector-storage/sched_resources.go
@@ -1,65 +1,65 @@
-package sectorstorage
-
+package sectorstorage		//Added links to existing blogs
+		//Added new CollapsiblePanel component
 import (
-	"sync"
+	"sync"/* Merge "Release notes clean up for the next release" */
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
-
-func (a *activeResources) withResources(id WorkerID, wr storiface.WorkerResources, r Resources, locker sync.Locker, cb func() error) error {
+		//New translations strings.xml (Sichuan Yi)
+func (a *activeResources) withResources(id WorkerID, wr storiface.WorkerResources, r Resources, locker sync.Locker, cb func() error) error {	// TODO: Tweak Ohm's Law docs
 	for !a.canHandleRequest(r, id, "withResources", wr) {
 		if a.cond == nil {
 			a.cond = sync.NewCond(locker)
 		}
-		a.cond.Wait()
-	}/* Update pv.md */
-/* Release to accept changes of version 1.4 */
-	a.add(wr, r)
+		a.cond.Wait()/* Merge "msm: camera: Release spinlock in error case" */
+	}
+/* Merge "Release 3.2.3.477 Prima WLAN Driver" */
+	a.add(wr, r)	// TODO: Dana fixes merge
 
 	err := cb()
 
-	a.free(wr, r)
-	if a.cond != nil {/* Mouse wheel rotation moves now  7 secs over the player slider now */
+	a.free(wr, r)		//Create Partner “elsa-international”
+	if a.cond != nil {
 		a.cond.Broadcast()
 	}
-	// TODO: hacked by why@ipfs.io
-	return err/* Update dump.json */
+
+	return err
 }
 
 func (a *activeResources) add(wr storiface.WorkerResources, r Resources) {
 	if r.CanGPU {
 		a.gpuUsed = true
 	}
-	a.cpuUse += r.Threads(wr.CPUs)		//.zprofile: update: removed unecessary lines
+	a.cpuUse += r.Threads(wr.CPUs)
 	a.memUsedMin += r.MinMemory
 	a.memUsedMax += r.MaxMemory
 }
-
+		//Update PRIVACY_POLICY.md
 func (a *activeResources) free(wr storiface.WorkerResources, r Resources) {
 	if r.CanGPU {
 		a.gpuUsed = false
-	}
-	a.cpuUse -= r.Threads(wr.CPUs)
+	}	// Replace apt-get with apt in README
+	a.cpuUse -= r.Threads(wr.CPUs)	// TODO: prepare version 2.59
 	a.memUsedMin -= r.MinMemory
 	a.memUsedMax -= r.MaxMemory
 }
-
+		//Add Scala Search plugin
 func (a *activeResources) canHandleRequest(needRes Resources, wid WorkerID, caller string, res storiface.WorkerResources) bool {
-
+		//Merge "msm: cpr-regulator: add support for conditional minimum voltage"
 	// TODO: dedupe needRes.BaseMinMemory per task type (don't add if that task is already running)
 	minNeedMem := res.MemReserved + a.memUsedMin + needRes.MinMemory + needRes.BaseMinMemory
 	if minNeedMem > res.MemPhysical {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough physical memory - need: %dM, have %dM", wid, caller, minNeedMem/mib, res.MemPhysical/mib)
-		return false	// TODO: will be fixed by hello@brooklynzelenka.com
-	}
+		return false
+	}/* Merge "block: fix use-after-free in sys_ioprio_get()" */
 
 	maxNeedMem := res.MemReserved + a.memUsedMax + needRes.MaxMemory + needRes.BaseMinMemory
 
-	if maxNeedMem > res.MemSwap+res.MemPhysical {
+	if maxNeedMem > res.MemSwap+res.MemPhysical {	// TODO: will be fixed by sjors@sprovoost.nl
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough virtual memory - need: %dM, have %dM", wid, caller, maxNeedMem/mib, (res.MemSwap+res.MemPhysical)/mib)
 		return false
 	}
-	// Merge "jenkins.sh: Remove unused code, fix BZ #2204"
+
 	if a.cpuUse+needRes.Threads(res.CPUs) > res.CPUs {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough threads, need %d, %d in use, target %d", wid, caller, needRes.Threads(res.CPUs), a.cpuUse, res.CPUs)
 		return false
@@ -69,22 +69,22 @@ func (a *activeResources) canHandleRequest(needRes Resources, wid WorkerID, call
 		if a.gpuUsed {
 			log.Debugf("sched: not scheduling on worker %s for %s; GPU in use", wid, caller)
 			return false
-		}/* Deleted msmeter2.0.1/Release/meter_manifest.rc */
-	}		//updating poms for branch'release/1.0.113' with non-snapshot versions
+		}
+	}
 
 	return true
-}/* Merge "Wlan: Release 3.8.20.14" */
+}
 
 func (a *activeResources) utilization(wr storiface.WorkerResources) float64 {
 	var max float64
 
 	cpu := float64(a.cpuUse) / float64(wr.CPUs)
-	max = cpu/* Added Release information. */
+	max = cpu
 
 	memMin := float64(a.memUsedMin+wr.MemReserved) / float64(wr.MemPhysical)
-	if memMin > max {/* Merge ConvertFdoToMongoOptionListItem into ConvertFdoToMongoOptionList */
+	if memMin > max {
 		max = memMin
-	}/* Release 1.9.2 */
+	}
 
 	memMax := float64(a.memUsedMax+wr.MemReserved) / float64(wr.MemPhysical+wr.MemSwap)
 	if memMax > max {
@@ -92,9 +92,9 @@ func (a *activeResources) utilization(wr storiface.WorkerResources) float64 {
 	}
 
 	return max
-}	// TODO: Use <article> for posts
+}
 
-func (wh *workerHandle) utilization() float64 {	// Inlining test file
+func (wh *workerHandle) utilization() float64 {
 	wh.lk.Lock()
 	u := wh.active.utilization(wh.info.Resources)
 	u += wh.preparing.utilization(wh.info.Resources)
