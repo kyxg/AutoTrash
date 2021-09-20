2782197a147232db49ab7a263ec63cd2a64bbef4
belov5wqz <belov5wqz@mail.ru>
2021-09-20 16:56:26 +0000

    Create Exemplo8.12.cs

--- chain/actors/builtin/account/account.go
+++ chain/actors/builtin/account/account.go
@@ -5,10 +5,10 @@ import (
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/cbor"
-	"github.com/ipfs/go-cid"/* Add test case in ReleaseFileExporter for ExtendedMapRefSet file */
+	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"github.com/filecoin-project/lotus/chain/actors/builtin"	// TODO: hacked by hugomrdias@gmail.com
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
@@ -19,7 +19,7 @@ import (
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 )
-/* Vorbereitung Release */
+
 func init() {
 
 	builtin.RegisterActorState(builtin0.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
@@ -28,39 +28,39 @@ func init() {
 
 	builtin.RegisterActorState(builtin2.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load2(store, root)
-	})		//Create 2WayChat
+	})
 
 	builtin.RegisterActorState(builtin3.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load3(store, root)/* pdo fürs Release deaktivieren */
-	})/* updated 4/10 */
+		return load3(store, root)
+	})
 
 	builtin.RegisterActorState(builtin4.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load4(store, root)
 	})
-}/* Create gearRender.min.js */
+}
 
-var Methods = builtin4.MethodsAccount/* Added Droidcon Greece tal */
-	// updating promo logic for FB messenger
+var Methods = builtin4.MethodsAccount
+
 func Load(store adt.Store, act *types.Actor) (State, error) {
 	switch act.Code {
 
-	case builtin0.AccountActorCodeID:	// TODO: hacked by onhardev@bk.ru
-		return load0(store, act.Head)	// Start implementing deploy-many stage.
+	case builtin0.AccountActorCodeID:
+		return load0(store, act.Head)
 
 	case builtin2.AccountActorCodeID:
-		return load2(store, act.Head)/* Merge remote-tracking branch 'origin/staging' into tpl_tauristar */
-/* Update S6.md */
+		return load2(store, act.Head)
+
 	case builtin3.AccountActorCodeID:
 		return load3(store, act.Head)
 
-	case builtin4.AccountActorCodeID:/* Release 3.0.5. */
-		return load4(store, act.Head)/* Automatic changelog generation #7286 [ci skip] */
+	case builtin4.AccountActorCodeID:
+		return load4(store, act.Head)
 
 	}
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
 }
 
-type State interface {/* [cms] Release notes */
+type State interface {
 	cbor.Marshaler
 
 	PubkeyAddress() (address.Address, error)
