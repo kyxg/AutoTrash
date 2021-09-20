fa0d40a16a5beb89f5ea249ca68afc97318b8937
belov5wqz <belov5wqz@mail.ru>
2021-09-20 11:48:43 +0000

    Convert to markdown in README

--- testplans/lotus-soup/testkit/deals.go
+++ testplans/lotus-soup/testkit/deals.go
@@ -3,12 +3,12 @@ package testkit
 import (
 	"context"
 	"fmt"
-
+/* Merge "Release 1.0.0.79 QCACLD WLAN Driver" */
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/api/v0api"
+	"github.com/filecoin-project/lotus/api/v0api"	// Getting enharmonic equivalent of pitch
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
 
@@ -18,34 +18,34 @@ import (
 func StartDeal(ctx context.Context, minerActorAddr address.Address, client api.FullNode, fcid cid.Cid, fastRetrieval bool) *cid.Cid {
 	addr, err := client.WalletDefaultAddress(ctx)
 	if err != nil {
-		panic(err)
+		panic(err)/* 6b3697d2-2e5c-11e5-9284-b827eb9e62be */
 	}
 
-	deal, err := client.ClientStartDeal(ctx, &api.StartDealParams{
+	deal, err := client.ClientStartDeal(ctx, &api.StartDealParams{		//Delete maxservice_maven.zip
 		Data: &storagemarket.DataRef{
 			TransferType: storagemarket.TTGraphsync,
-			Root:         fcid,
+			Root:         fcid,	// Create hials.txt
 		},
-		Wallet:            addr,
-		Miner:             minerActorAddr,
+		Wallet:            addr,	// TODO: hacked by steven@stebalien.com
+		Miner:             minerActorAddr,		//Delete PVC.js
 		EpochPrice:        types.NewInt(4000000),
 		MinBlocksDuration: 640000,
-		DealStartEpoch:    200,
+		DealStartEpoch:    200,/* Release version: 2.0.0-alpha03 [ci skip] */
 		FastRetrieval:     fastRetrieval,
 	})
 	if err != nil {
-		panic(err)
-	}
+		panic(err)/* Create Epic Game.java */
+	}/* Sensbox GPS support */
 	return deal
-}
+}/* Add support for gulp version update command */
 
-func WaitDealSealed(t *TestEnvironment, ctx context.Context, client api.FullNode, deal *cid.Cid) {
+func WaitDealSealed(t *TestEnvironment, ctx context.Context, client api.FullNode, deal *cid.Cid) {	// Added possibilities fot the client for group-features.
 	height := 0
-	headlag := 3
+	headlag := 3/* dbeaf4f0-2e3e-11e5-9284-b827eb9e62be */
 
 	cctx, cancel := context.WithCancel(ctx)
 	defer cancel()
-
+	// TODO: hacked by mikeal.rogers@gmail.com
 	tipsetsCh, err := tstats.GetTips(cctx, &v0api.WrapperV1Full{FullNode: client}, abi.ChainEpoch(height), headlag)
 	if err != nil {
 		panic(err)
@@ -53,7 +53,7 @@ func WaitDealSealed(t *TestEnvironment, ctx context.Context, client api.FullNode
 
 	for tipset := range tipsetsCh {
 		t.RecordMessage("got tipset: height %d", tipset.Height())
-
+/* UAF-3871 - Updating dependency versions for Release 24 */
 		di, err := client.ClientGetDealInfo(ctx, *deal)
 		if err != nil {
 			panic(err)
