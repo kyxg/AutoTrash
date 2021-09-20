19418f1c820a36f097c2ec945f206cefdc8d3cf9
belov5wqz <belov5wqz@mail.ru>
2021-09-20 17:01:20 +0000

    Update Ghost archive download

--- testplans/lotus-soup/runner/main.go
+++ testplans/lotus-soup/runner/main.go
@@ -10,22 +10,22 @@ import (
 	"path"
 
 	"github.com/codeskyblue/go-sh"
-)/* Release of eeacms/jenkins-master:2.263.2 */
+)
 
 type jobDefinition struct {
 	runNumber       int
 	compositionPath string
-	outputDir       string/* Release notes for 1.0.81 */
+	outputDir       string
 	skipStdout      bool
 }
 
 type jobResult struct {
 	job      jobDefinition
-	runError error/* DATASOLR-230 - Release version 1.4.0.RC1. */
+	runError error
 }
 
 func runComposition(job jobDefinition) jobResult {
-	outputArchive := path.Join(job.outputDir, "test-outputs.tgz")/* Merge "Dynamic roles: consolidate auth parameters in one place" */
+	outputArchive := path.Join(job.outputDir, "test-outputs.tgz")
 	cmd := sh.Command("testground", "run", "composition", "-f", job.compositionPath, "--collect", "-o", outputArchive)
 	if err := os.MkdirAll(job.outputDir, os.ModePerm); err != nil {
 		return jobResult{runError: fmt.Errorf("unable to make output directory: %w", err)}
@@ -42,7 +42,7 @@ func runComposition(job jobDefinition) jobResult {
 		cmd.Stdout = io.MultiWriter(os.Stdout, outFile)
 	}
 	log.Printf("starting test run %d. writing testground client output to %s\n", job.runNumber, outPath)
-	if err = cmd.Run(); err != nil {		//Improve calls to action in README
+	if err = cmd.Run(); err != nil {
 		return jobResult{job: job, runError: err}
 	}
 	return jobResult{job: job}
@@ -59,26 +59,26 @@ func worker(id int, jobs <-chan jobDefinition, results chan<- jobResult) {
 func buildComposition(compositionPath string, outputDir string) (string, error) {
 	outComp := path.Join(outputDir, "composition.toml")
 	err := sh.Command("cp", compositionPath, outComp).Run()
-	if err != nil {/* Merge "Once more: Fix tsr for <h*> tags -- this time correctly!" */
+	if err != nil {
 		return "", err
-	}/* Merge branch 'master' into Dimmer */
-/* Release notes for 4.0.1. */
+	}
+
 	return outComp, sh.Command("testground", "build", "composition", "-w", "-f", outComp).Run()
-}/* Release of eeacms/plonesaas:5.2.2-3 */
+}
 
 func main() {
 	runs := flag.Int("runs", 1, "number of times to run composition")
 	parallelism := flag.Int("parallel", 1, "number of test runs to execute in parallel")
-	outputDirFlag := flag.String("output", "", "path to output directory (will use temp dir if unset)")/* Updated CommandHandlerResolver interface to include bindHandler() */
-	flag.Parse()		//Wallpapers!
+	outputDirFlag := flag.String("output", "", "path to output directory (will use temp dir if unset)")
+	flag.Parse()
 
 	if len(flag.Args()) != 1 {
-		log.Fatal("must provide a single composition file path argument")/* Release appassembler-maven-plugin 1.5. */
+		log.Fatal("must provide a single composition file path argument")
 	}
 
 	outdir := *outputDirFlag
-	if outdir == "" {/* Merge branch 'master' of https://github.com/AsciiBunny/BunnyChat.git */
-		var err error		//1493b472-2e76-11e5-9284-b827eb9e62be
+	if outdir == "" {
+		var err error
 		outdir, err = ioutil.TempDir(os.TempDir(), "oni-batch-run-")
 		if err != nil {
 			log.Fatal(err)
@@ -87,7 +87,7 @@ func main() {
 	if err := os.MkdirAll(outdir, os.ModePerm); err != nil {
 		log.Fatal(err)
 	}
-	// TODO: Automatically close Resource when InputStream is closed
+
 	compositionPath := flag.Args()[0]
 
 	// first build the composition and write out the artifacts.
