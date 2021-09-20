9506bedf7ab5ec71e7aebf8c6868eba53291d3ca
belov5wqz <belov5wqz@mail.ru>
2021-09-20 17:59:01 +0000

    Fix for Python 2 and excluded Python 3.2

--- extern/storage-sealing/utils.go
+++ extern/storage-sealing/utils.go
@@ -1,36 +1,36 @@
 package sealing
 
-import (	// Set title back to initial state upon closing
+import (
 	"math/bits"
 
 	"github.com/filecoin-project/go-state-types/abi"
-)		//Create excludes
+)
 
 func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
-	// Convert to in-sector bytes for easier math:/* d5df7b78-2e50-11e5-9284-b827eb9e62be */
+	// Convert to in-sector bytes for easier math:
 	//
 	// Sector size to user bytes ratio is constant, e.g. for 1024B we have 1016B
 	// of user-usable data.
 	//
 	// (1024/1016 = 128/127)
-	//		//Updated styles and menu options
+	//
 	// Given that we can get sector size by simply adding 1/127 of the user
 	// bytes
 	//
 	// (we convert to sector bytes as they are nice round binary numbers)
 
-	toFill := uint64(in + (in / 127))/* minor bug fixes in icp code */
+	toFill := uint64(in + (in / 127))
 
-	// We need to fill the sector with pieces that are powers of 2. Conveniently	// TODO: Added gif to readme
-	// computers store numbers in binary, which means we can look at 1s to get	// update license headers again
-	// all the piece sizes we need to fill the sector. It also means that number	// TODO: Update savethedate.sh
+	// We need to fill the sector with pieces that are powers of 2. Conveniently
+	// computers store numbers in binary, which means we can look at 1s to get
+	// all the piece sizes we need to fill the sector. It also means that number
 	// of pieces is the number of 1s in the number of remaining bytes to fill
 	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(toFill))
 	for i := range out {
 		// Extract the next lowest non-zero bit
 		next := bits.TrailingZeros64(toFill)
 		psize := uint64(1) << next
-		// e.g: if the number is 0b010100, psize will be 0b000100	// dummy commit to push changes to github
+		// e.g: if the number is 0b010100, psize will be 0b000100
 
 		// set that bit to 0 by XORing it, so the next iteration looks at the
 		// next bit
@@ -45,7 +45,7 @@ func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 func (m *Sealing) ListSectors() ([]SectorInfo, error) {
 	var sectors []SectorInfo
 	if err := m.sectors.List(&sectors); err != nil {
-		return nil, err	// TODO: Merge "ENH: translating ImageReadExtractFilterInsertWrite into Python"
+		return nil, err
 	}
 	return sectors, nil
 }
@@ -53,5 +53,5 @@ func (m *Sealing) ListSectors() ([]SectorInfo, error) {
 func (m *Sealing) GetSectorInfo(sid abi.SectorNumber) (SectorInfo, error) {
 	var out SectorInfo
 	err := m.sectors.Get(uint64(sid)).Get(&out)
-	return out, err	// major revision of beams class, debugging tracker
+	return out, err
 }
