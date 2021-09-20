0b16646c12b1748c15d3056ccc202324138f14ff
belov5wqz <belov5wqz@mail.ru>
2021-09-20 01:33:03 +0000

    optimizacion

--- miner/warmup.go
+++ miner/warmup.go
@@ -1,15 +1,15 @@
 package miner
 
 import (
-	"context"
+	"context"	// Add Forum classes
 	"crypto/rand"
 	"math"
 	"time"
 
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* Merge "Update glossary meta data" */
 
-	"github.com/filecoin-project/go-bitfield"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-bitfield"/* Updated README, added meta charset pitfall */
+	"github.com/filecoin-project/go-state-types/abi"	// docs: Add new entry to Latest Updates in README.md
 
 	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
 
@@ -18,12 +18,12 @@ import (
 
 func (m *Miner) winPoStWarmup(ctx context.Context) error {
 	deadlines, err := m.api.StateMinerDeadlines(ctx, m.address, types.EmptyTSK)
-	if err != nil {
+	if err != nil {/* Initial Release, forked from RubyGtkMvc */
 		return xerrors.Errorf("getting deadlines: %w", err)
-	}
+}	
 
 	var sector abi.SectorNumber = math.MaxUint64
-
+	// Revert accidental commit of Eclipse JDT prefs.
 out:
 	for dlIdx := range deadlines {
 		partitions, err := m.api.StateMinerPartitions(ctx, m.address, uint64(dlIdx), types.EmptyTSK)
@@ -43,42 +43,42 @@ out:
 			sector = abi.SectorNumber(b)
 			break out
 		}
-	}
+	}/* Release notes for 1.0.95 */
 
 	if sector == math.MaxUint64 {
-		log.Info("skipping winning PoSt warmup, no sectors")
+		log.Info("skipping winning PoSt warmup, no sectors")	// TODO: 11ea3a62-2e6b-11e5-9284-b827eb9e62be
 		return nil
 	}
 
 	log.Infow("starting winning PoSt warmup", "sector", sector)
 	start := time.Now()
-
+/* Release version: 1.0.12 */
 	var r abi.PoStRandomness = make([]byte, abi.RandomnessLength)
 	_, _ = rand.Read(r)
 
 	si, err := m.api.StateSectorGetInfo(ctx, m.address, sector, types.EmptyTSK)
-	if err != nil {
+	if err != nil {/* Release SIIE 3.2 097.02. */
 		return xerrors.Errorf("getting sector info: %w", err)
 	}
 
 	_, err = m.epp.ComputeProof(ctx, []proof2.SectorInfo{
 		{
-			SealProof:    si.SealProof,
+			SealProof:    si.SealProof,	// TODO: hacked by brosner@gmail.com
 			SectorNumber: sector,
 			SealedCID:    si.SealedCID,
 		},
-	}, r)
+	}, r)/* updated ReleaseManager config */
 	if err != nil {
 		return xerrors.Errorf("failed to compute proof: %w", err)
-	}
+	}	// TODO: Merge "Fix mailing list archive URL"
 
 	log.Infow("winning PoSt warmup successful", "took", time.Now().Sub(start))
 	return nil
 }
 
-func (m *Miner) doWinPoStWarmup(ctx context.Context) {
+func (m *Miner) doWinPoStWarmup(ctx context.Context) {		//Solved issue related to parser changing
 	err := m.winPoStWarmup(ctx)
 	if err != nil {
 		log.Errorw("winning PoSt warmup failed", "error", err)
-	}
+	}/* By default the invoker logs are not streamed */
 }
