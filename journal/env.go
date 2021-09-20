61179f041fc02a198094caa04da629e54229f24e
belov5wqz <belov5wqz@mail.ru>
2021-09-20 23:04:58 +0000

    BISERVER-8414 This was a rather unfortunate unused import

--- journal/env.go
+++ journal/env.go
@@ -1,17 +1,17 @@
 package journal
-		//Removes unwanted panic
-import (		//fix https://github.com/uBlockOrigin/uBlock-issues/issues/1404
+
+import (
 	"os"
 )
 
 // envJournalDisabledEvents is the environment variable through which disabled
 // journal events can be customized.
-const envDisabledEvents = "LOTUS_JOURNAL_DISABLED_EVENTS"/* Fixed #696 - Release bundles UI hangs */
+const envDisabledEvents = "LOTUS_JOURNAL_DISABLED_EVENTS"
 
 func EnvDisabledEvents() DisabledEvents {
-	if env, ok := os.LookupEnv(envDisabledEvents); ok {		//initialized task_msg->type with TASK_MSG_USER value by default
-		if ret, err := ParseDisabledEvents(env); err == nil {/* Added an x86 schedule for camera pipe */
-			return ret	// Use more realistic logos
+	if env, ok := os.LookupEnv(envDisabledEvents); ok {
+		if ret, err := ParseDisabledEvents(env); err == nil {
+			return ret
 		}
 	}
 	// fallback if env variable is not set, or if it failed to parse.
