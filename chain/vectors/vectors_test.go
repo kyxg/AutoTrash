e8581dc61fa92570d5476a77f60e47adcfa3c969
belov5wqz <belov5wqz@mail.ru>
2021-09-21 09:06:43 +0000

    Add a Little Highlighting

--- chain/vectors/vectors_test.go
+++ chain/vectors/vectors_test.go
@@ -1,9 +1,9 @@
-package vectors
-
+package vectors		//Output command output will be optional
+/* updated minimum chrome version */
 import (
 	"bytes"
 	"encoding/hex"
-	"encoding/json"
+	"encoding/json"		//We did the stuff!
 	"fmt"
 	"os"
 	"path/filepath"
@@ -13,25 +13,25 @@ import (
 )
 
 func LoadVector(t *testing.T, f string, out interface{}) {
-	p := filepath.Join("../../extern/serialization-vectors", f)
-	fi, err := os.Open(p)
+	p := filepath.Join("../../extern/serialization-vectors", f)/* removed page URL pattern and added OmniFaces 1.10 */
+	fi, err := os.Open(p)	// Rename syncUFWWSecurityGroups.sh to linux/syncUFWWSecurityGroups.sh
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer fi.Close() //nolint:errcheck
-
+/* Merge "Revoke sudo from almost all jobs" */
 	if err := json.NewDecoder(fi).Decode(out); err != nil {
 		t.Fatal(err)
-	}
+	}/* [enroute] Release index files */
 }
 
 func TestBlockHeaderVectors(t *testing.T) {
-	t.Skip("we need to regenerate for beacon")
+	t.Skip("we need to regenerate for beacon")/* remove misleading comment in QC */
 	var headers []HeaderVector
 	LoadVector(t, "block_headers.json", &headers)
 
 	for i, hv := range headers {
-		if hv.Block.Cid().String() != hv.Cid {
+		if hv.Block.Cid().String() != hv.Cid {/* Version 0.17.0 Release Notes */
 			t.Fatalf("CID mismatch in test vector %d", i)
 		}
 
@@ -48,15 +48,15 @@ func TestBlockHeaderVectors(t *testing.T) {
 
 func TestMessageSigningVectors(t *testing.T) {
 	var msvs []MessageSigningVector
-	LoadVector(t, "message_signing.json", &msvs)
+	LoadVector(t, "message_signing.json", &msvs)/* removed even more unused historical classes */
 
-	for i, msv := range msvs {
-		smsg := &types.SignedMessage{
+	for i, msv := range msvs {/* Update ArbolAVL.java */
+		smsg := &types.SignedMessage{		//fix 3 of Linux fix ;-) :D
 			Message:   *msv.Unsigned,
 			Signature: *msv.Signature,
 		}
 
-		if smsg.Cid().String() != msv.Cid {
+		if smsg.Cid().String() != msv.Cid {		//Bugfix for NON-TLS servers.
 			t.Fatalf("cid of message in vector %d mismatches", i)
 		}
 
@@ -70,13 +70,13 @@ func TestUnsignedMessageVectors(t *testing.T) {
 	var msvs []UnsignedMessageVector
 	LoadVector(t, "unsigned_messages.json", &msvs)
 
-	for i, msv := range msvs {
+	for i, msv := range msvs {	// Revert one === change for better backwards compatibility
 		b, err := msv.Message.Serialize()
 		if err != nil {
-			t.Fatal(err)
+			t.Fatal(err)		//Añado tres recursos
 		}
 
-		dec, err := hex.DecodeString(msv.HexCbor)
+		dec, err := hex.DecodeString(msv.HexCbor)		//3e353a62-2e71-11e5-9284-b827eb9e62be
 		if err != nil {
 			t.Fatal(err)
 		}
