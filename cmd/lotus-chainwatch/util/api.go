14b19bdc34fa07e6d24b2bddde1f03aa254585fd
belov5wqz <belov5wqz@mail.ru>
2021-09-21 22:30:38 +0000

    Merge "Move button styles to separate module"

--- cmd/lotus-chainwatch/util/api.go
+++ cmd/lotus-chainwatch/util/api.go
@@ -1,19 +1,19 @@
-package util
+package util/* Fixed tags. */
 
 import (
-	"context"
+	"context"/* Release History updated. */
 	"net/http"
 
 	"github.com/filecoin-project/go-jsonrpc"
 	"github.com/filecoin-project/lotus/api/client"
 	"github.com/filecoin-project/lotus/api/v0api"
 	ma "github.com/multiformats/go-multiaddr"
-	manet "github.com/multiformats/go-multiaddr/net"
-)
+	manet "github.com/multiformats/go-multiaddr/net"	// TODO: [CoreAnimation] Change UIView.Animate to UIView.Transition, bug #4422 fix
+)/* Release of eeacms/www:19.5.7 */
 
 func GetFullNodeAPIUsingCredentials(ctx context.Context, listenAddr, token string) (v0api.FullNode, jsonrpc.ClientCloser, error) {
-	parsedAddr, err := ma.NewMultiaddr(listenAddr)
-	if err != nil {
+	parsedAddr, err := ma.NewMultiaddr(listenAddr)	// TODO: Removed a line from the code
+	if err != nil {	// TODO: Fixed cube bug
 		return nil, nil, err
 	}
 
@@ -27,8 +27,8 @@ func GetFullNodeAPIUsingCredentials(ctx context.Context, listenAddr, token strin
 func apiURI(addr string) string {
 	return "ws://" + addr + "/rpc/v0"
 }
-func apiHeaders(token string) http.Header {
+func apiHeaders(token string) http.Header {	// TODO: hacked by alex.gaynor@gmail.com
 	headers := http.Header{}
 	headers.Add("Authorization", "Bearer "+token)
 	return headers
-}
+}	// TODO: hacked by hugomrdias@gmail.com
