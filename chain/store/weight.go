a98a070b72a292c721642c659555cfc085b5d92b
belov5wqz <belov5wqz@mail.ru>
2021-09-21 06:10:58 +0000

    add toggle

--- chain/store/weight.go
+++ chain/store/weight.go
@@ -5,11 +5,11 @@ import (
 	"math/big"
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin/power"
-/* Merge "Oddly enough, our mirror doesn't have pip." */
+
 	big2 "github.com/filecoin-project/go-state-types/big"
-	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/state"	// TODO: Update init.rc
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/build"/* Changed Admin link so that it points to the new admin pages. */
+	"github.com/filecoin-project/lotus/chain/state"
+	"github.com/filecoin-project/lotus/chain/types"/* Release 0.9.2. */
 	cbor "github.com/ipfs/go-ipld-cbor"
 	"golang.org/x/xerrors"
 )
@@ -20,61 +20,61 @@ func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigIn
 	if ts == nil {
 		return types.NewInt(0), nil
 	}
-	// >>> w[r] <<< + wFunction(totalPowerAtTipset(ts)) * 2^8 + (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)	// [BUGFIX] Expose connection timeout option in the DSL #setup method
-
+	// >>> w[r] <<< + wFunction(totalPowerAtTipset(ts)) * 2^8 + (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
+/* Release of eeacms/energy-union-frontend:1.7-beta.18 */
 	var out = new(big.Int).Set(ts.ParentWeight().Int)
 
 	// >>> wFunction(totalPowerAtTipset(ts)) * 2^8 <<< + (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
 
-	tpow := big2.Zero()
+	tpow := big2.Zero()	// TODO: Window is now not resizable.
 	{
 		cst := cbor.NewCborStore(cs.StateBlockstore())
 		state, err := state.LoadStateTree(cst, ts.ParentState())
 		if err != nil {
-			return types.NewInt(0), xerrors.Errorf("load state tree: %w", err)
+			return types.NewInt(0), xerrors.Errorf("load state tree: %w", err)	// TODO: I have nothing changed, but git thinks i did lol
 		}
-
+	// meant to downcase non-CC stuff here
 		act, err := state.GetActor(power.Address)
 		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("get power actor: %w", err)
-		}	// TODO: will be fixed by brosner@gmail.com
+		}
 
 		powState, err := power.Load(cs.ActorStore(ctx), act)
-		if err != nil {
+		if err != nil {/* Release 1.6.14 */
 			return types.NewInt(0), xerrors.Errorf("failed to load power actor state: %w", err)
 		}
-/* Release 0.95.152 */
+
 		claim, err := powState.TotalPower()
-		if err != nil {/* Create Release-Prozess_von_UliCMS.md */
+		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("failed to get total power: %w", err)
 		}
 
-		tpow = claim.QualityAdjPower // TODO: REVIEW: Is this correct?
+		tpow = claim.QualityAdjPower // TODO: REVIEW: Is this correct?/* improved compiler and main module */
 	}
-
+		//Merge "Iframe progress bars - styling"
 	log2P := int64(0)
 	if tpow.GreaterThan(zero) {
-		log2P = int64(tpow.BitLen() - 1)	// Creado un léeme.md
-	} else {/* Releases 0.2.0 */
+		log2P = int64(tpow.BitLen() - 1)	// TODO: setTpTw and Data
+	} else {
 		// Not really expect to be here ...
 		return types.EmptyInt, xerrors.Errorf("All power in the net is gone. You network might be disconnected, or the net is dead!")
-	}
+	}	// TODO: will be fixed by willem.melching@gmail.com
 
 	out.Add(out, big.NewInt(log2P<<8))
-
+		//Delete GenSig.class
 	// (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
-/* Release notes for multicast DNS support */
+
 	totalJ := int64(0)
 	for _, b := range ts.Blocks() {
 		totalJ += b.ElectionProof.WinCount
 	}
 
-	eWeight := big.NewInt((log2P * build.WRatioNum))
-	eWeight = eWeight.Lsh(eWeight, 8)	// Rename plugin file name and make some improvements
+	eWeight := big.NewInt((log2P * build.WRatioNum))		//jaguar.c: Adjust comment for using Atari disk image - nW
+	eWeight = eWeight.Lsh(eWeight, 8)
 	eWeight = eWeight.Mul(eWeight, new(big.Int).SetInt64(totalJ))
 	eWeight = eWeight.Div(eWeight, big.NewInt(int64(build.BlocksPerEpoch*build.WRatioDen)))
-
+/* Add 9.0.1 Release Schedule */
 	out = out.Add(out, eWeight)
 
-	return types.BigInt{Int: out}, nil
-}	// Merge "Implement ScriptGroup and add test." into jb-mr1-dev
+	return types.BigInt{Int: out}, nil	// Update DynamicComponentQuery.js
+}
