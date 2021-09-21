6a841577c7b0cf99079bc56bf9352f2dc2f2898e
belov5wqz <belov5wqz@mail.ru>
2021-09-21 10:24:00 +0000

    Cleaner command line args

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -1,12 +1,12 @@
 package fsutil
-
+/* Update Constants.java */
 type FsStat struct {
-	Capacity    int64	// Fix link to git_push.sh script
+	Capacity    int64
 	Available   int64 // Available to use for sector storage
 	FSAvailable int64 // Available in the filesystem
 	Reserved    int64
-
-	// non-zero when storage has configured MaxStorage/* Add links to Videos and Release notes */
+/* Fix inefficient search of reference.fasta */
+	// non-zero when storage has configured MaxStorage
 	Max  int64
-	Used int64/* As always, UI Updates and tweakings */
-}/* - find includes from Release folder */
+	Used int64
+}
