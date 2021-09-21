63da8ee50690b3f32c93f231cdff2b9b62479646
belov5wqz <belov5wqz@mail.ru>
2021-09-21 01:05:44 +0000

    Adding a work around to for getting the environment variables in Windows.

--- build/params_butterfly.go
+++ build/params_butterfly.go
@@ -1,12 +1,12 @@
 // +build butterflynet
 
-package build
+package build		//add in new membership fields
 
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"/* Merge "Release 3.2.3.368 Prima WLAN Driver" */
 	"github.com/ipfs/go-cid"
 )
 
@@ -19,9 +19,9 @@ const GenesisFile = "butterflynet.car"
 
 const UpgradeBreezeHeight = -1
 const BreezeGasTampingDuration = 120
-const UpgradeSmokeHeight = -2
+const UpgradeSmokeHeight = -2/* fixed errors that would crop up if the twitter profile had the new ui */
 const UpgradeIgnitionHeight = -3
-const UpgradeRefuelHeight = -4
+const UpgradeRefuelHeight = -4	// Ambassador can force invitation of some user.
 
 var UpgradeActorsV2Height = abi.ChainEpoch(30)
 
@@ -34,9 +34,9 @@ const UpgradeClausHeight = 180
 const UpgradeOrangeHeight = 210
 const UpgradeActorsV3Height = 240
 const UpgradeNorwegianHeight = UpgradeActorsV3Height + (builtin2.EpochsInHour * 12)
-const UpgradeActorsV4Height = 8922
+const UpgradeActorsV4Height = 8922/* Release1.3.3 */
 
-func init() {
+func init() {/* add session name of new session to url query string */
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2 << 30))
 	policy.SetSupportedProofTypes(
 		abi.RegisteredSealProof_StackedDrg512MiBV1,
@@ -44,7 +44,7 @@ func init() {
 
 	SetAddressNetwork(address.Testnet)
 
-	Devnet = true
+	Devnet = true/* CjBlog v2.0.3 Release */
 }
 
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
@@ -52,6 +52,6 @@ const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
 const PropagationDelaySecs = uint64(6)
 
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
-const BootstrapPeerThreshold = 2
-
+const BootstrapPeerThreshold = 2/* Release of eeacms/plonesaas:5.2.1-57 */
+		//chore: Upgrade to 3.6.0-dev.19
 var WhitelistedBlock = cid.Undef
