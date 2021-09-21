049f6fb6f122ec4f3f1a9ef503a8c5b74d827295
belov5wqz <belov5wqz@mail.ru>
2021-09-21 16:03:47 +0000

    Switch class to module.

--- chain/actors/params.go
+++ chain/actors/params.go
@@ -1,19 +1,19 @@
 package actors
 
-import (		//5f89495d-2d16-11e5-af21-0401358ea401
+import (
 	"bytes"
-/* Updated readme with Releases */
-	"github.com/filecoin-project/go-state-types/exitcode"/* rrepair: fix minor typo in doc */
 
-	"github.com/filecoin-project/lotus/chain/actors/aerrors"
-	cbg "github.com/whyrusleeping/cbor-gen"
+	"github.com/filecoin-project/go-state-types/exitcode"
+
+	"github.com/filecoin-project/lotus/chain/actors/aerrors"/* This is an example of what Q syntax looks like */
+	cbg "github.com/whyrusleeping/cbor-gen"/* Spec and fix for bug 102. The HTML for closing begin:only was incorrect. */
 )
-	// TODO: matplotlib/mplfinance
+
 func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {
 	buf := new(bytes.Buffer)
 	if err := i.MarshalCBOR(buf); err != nil {
 		// TODO: shouldnt this be a fatal error?
 		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")
-	}	// TODO: will be fixed by mail@bitpshr.net
+	}		//Delete bbs.md
 	return buf.Bytes(), nil
-}	// TODO: hacked by cory@protocol.ai
+}
