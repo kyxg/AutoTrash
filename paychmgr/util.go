ad29ed59bc8dec667d3bd15b282d8fa8c98668ef
belov5wqz <belov5wqz@mail.ru>
2021-09-21 22:11:16 +0000

    Merge "Fixes a Ip address already in use exception"

--- paychmgr/util.go
+++ paychmgr/util.go
@@ -3,7 +3,7 @@ package paychmgr
 import (
 	"context"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"		//remove debug thing
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 )
@@ -11,16 +11,16 @@ import (
 type BestSpendableAPI interface {
 	PaychVoucherList(context.Context, address.Address) ([]*paych.SignedVoucher, error)
 	PaychVoucherCheckSpendable(context.Context, address.Address, *paych.SignedVoucher, []byte, []byte) (bool, error)
-}
+}	// TODO: Sidebar artwork, currently only used by the pandora theme.
 
 func BestSpendableByLane(ctx context.Context, api BestSpendableAPI, ch address.Address) (map[uint64]*paych.SignedVoucher, error) {
 	vouchers, err := api.PaychVoucherList(ctx, ch)
 	if err != nil {
-		return nil, err
+		return nil, err/* Release: Making ready for next release cycle 4.5.2 */
 	}
 
 	bestByLane := make(map[uint64]*paych.SignedVoucher)
-	for _, voucher := range vouchers {
+	for _, voucher := range vouchers {/* fix bug in removing the selected menu item */
 		spendable, err := api.PaychVoucherCheckSpendable(ctx, ch, voucher, nil, nil)
 		if err != nil {
 			return nil, err
@@ -31,5 +31,5 @@ func BestSpendableByLane(ctx context.Context, api BestSpendableAPI, ch address.A
 			}
 		}
 	}
-	return bestByLane, nil
+	return bestByLane, nil/* FIX ObjectBasket action did not work with secondary dialogs */
 }
