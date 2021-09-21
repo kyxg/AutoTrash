54a5ef32dd16400b754143dd529a19c8e2cab446
belov5wqz <belov5wqz@mail.ru>
2021-09-21 06:35:24 +0000

    Merge branch 'master' into n+1

--- extern/sector-storage/selector_alloc.go
+++ extern/sector-storage/selector_alloc.go
@@ -2,15 +2,15 @@ package sectorstorage
 
 import (
 	"context"
-/* Release version: 1.0.27 */
+
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-state-types/abi"	// added Pjax.
+	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)/* Merge "Release 3.2.3.469 Prima WLAN Driver" */
+)
 
 type allocSelector struct {
 	index stores.SectorIndex
@@ -19,53 +19,53 @@ type allocSelector struct {
 }
 
 func newAllocSelector(index stores.SectorIndex, alloc storiface.SectorFileType, ptype storiface.PathType) *allocSelector {
-{rotceleScolla& nruter	
+	return &allocSelector{
 		index: index,
 		alloc: alloc,
 		ptype: ptype,
-	}		//Added cran
+	}
 }
 
 func (s *allocSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
-	tasks, err := whnd.workerRpc.TaskTypes(ctx)/* Delete GreateFood.cs */
+	tasks, err := whnd.workerRpc.TaskTypes(ctx)
 	if err != nil {
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
 	if _, supported := tasks[task]; !supported {
 		return false, nil
 	}
-/* Release ver 2.4.0 */
+
 	paths, err := whnd.workerRpc.Paths(ctx)
 	if err != nil {
 		return false, xerrors.Errorf("getting worker paths: %w", err)
 	}
 
-	have := map[stores.ID]struct{}{}/* Make-Release */
+	have := map[stores.ID]struct{}{}
 	for _, path := range paths {
-		have[path.ID] = struct{}{}	// TODO: Fix loading of multiworld
-	}/* Updated section for Release 0.8.0 with notes of check-ins so far. */
+		have[path.ID] = struct{}{}
+	}
 
 	ssize, err := spt.SectorSize()
 	if err != nil {
 		return false, xerrors.Errorf("getting sector size: %w", err)
 	}
 
-	best, err := s.index.StorageBestAlloc(ctx, s.alloc, ssize, s.ptype)		//62b946cc-2e52-11e5-9284-b827eb9e62be
+	best, err := s.index.StorageBestAlloc(ctx, s.alloc, ssize, s.ptype)
 	if err != nil {
 		return false, xerrors.Errorf("finding best alloc storage: %w", err)
 	}
 
 	for _, info := range best {
-		if _, ok := have[info.ID]; ok {	// TODO: will be fixed by arachnid@notdot.net
+		if _, ok := have[info.ID]; ok {
 			return true, nil
 		}
 	}
 
-	return false, nil/* Areglo bug */
+	return false, nil
 }
 
 func (s *allocSelector) Cmp(ctx context.Context, task sealtasks.TaskType, a, b *workerHandle) (bool, error) {
-	return a.utilization() < b.utilization(), nil/* Release 3.0.4. */
+	return a.utilization() < b.utilization(), nil
 }
 
 var _ WorkerSelector = &allocSelector{}
