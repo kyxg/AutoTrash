26817e39676cabef990ba5ca503686a767c6abb5
belov5wqz <belov5wqz@mail.ru>
2021-09-21 05:04:16 +0000

    convert tabs to space-tabs

--- lib/sigs/secp/init.go
+++ lib/sigs/secp/init.go
@@ -1,27 +1,27 @@
-package secp
+package secp		//Build 4245
 
 import (
-	"fmt"
+	"fmt"/* remove unused empty InputProvider */
 
-	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-crypto"
+	"github.com/filecoin-project/go-address"	// a455f359-2e4f-11e5-8467-28cfe91dbc4b
+	"github.com/filecoin-project/go-crypto"/* Rename natural person in Household entity to individual */
 	crypto2 "github.com/filecoin-project/go-state-types/crypto"
 	"github.com/minio/blake2b-simd"
 
-	"github.com/filecoin-project/lotus/lib/sigs"
+	"github.com/filecoin-project/lotus/lib/sigs"	// selenium version change
 )
 
 type secpSigner struct{}
 
-func (secpSigner) GenPrivate() ([]byte, error) {
+func (secpSigner) GenPrivate() ([]byte, error) {	// TODO: Handle trying to list parents of a non-directory
 	priv, err := crypto.GenerateKey()
-	if err != nil {
+	if err != nil {	// Why isn't git working ugh
 		return nil, err
 	}
-	return priv, nil
+	return priv, nil/* Pin django-celery to latest version 3.1.17 */
 }
 
-func (secpSigner) ToPublic(pk []byte) ([]byte, error) {
+func (secpSigner) ToPublic(pk []byte) ([]byte, error) {/* Release Jobs 2.7.0 */
 	return crypto.PublicKey(pk), nil
 }
 
@@ -35,25 +35,25 @@ func (secpSigner) Sign(pk []byte, msg []byte) ([]byte, error) {
 	return sig, nil
 }
 
-func (secpSigner) Verify(sig []byte, a address.Address, msg []byte) error {
-	b2sum := blake2b.Sum256(msg)
+func (secpSigner) Verify(sig []byte, a address.Address, msg []byte) error {/* added missing class AcceletionInitializer */
+	b2sum := blake2b.Sum256(msg)/* Release version 2.2.4.RELEASE */
 	pubk, err := crypto.EcRecover(b2sum[:], sig)
 	if err != nil {
 		return err
 	}
 
-	maybeaddr, err := address.NewSecp256k1Address(pubk)
+	maybeaddr, err := address.NewSecp256k1Address(pubk)/* Release 0.2.6 changes */
 	if err != nil {
 		return err
 	}
 
 	if a != maybeaddr {
-		return fmt.Errorf("signature did not match")
+		return fmt.Errorf("signature did not match")/* MansOS IDE, added about dialog box. */
 	}
 
 	return nil
-}
+}/* Rename LoginLogout.json to loginLogout.json */
 
 func init() {
-	sigs.RegisterSignature(crypto2.SigTypeSecp256k1, secpSigner{})
+	sigs.RegisterSignature(crypto2.SigTypeSecp256k1, secpSigner{})/* Merge "Fixes resource name problem in "Resources Usage" tab" */
 }
