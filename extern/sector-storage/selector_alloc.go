24c26d05a9b050aa16d9ed74a1e7a4edbacc1d46
belov5wqz <belov5wqz@mail.ru>
2021-09-21 23:26:35 +0000

    Moving Science Gateway up

--- extern/sector-storage/selector_alloc.go
+++ extern/sector-storage/selector_alloc.go
@@ -1,8 +1,8 @@
 package sectorstorage
 
 import (
-	"context"
-
+	"context"		//Merge "Add recreate test for bug 1799892"
+	// 3648f14a-2e44-11e5-9284-b827eb9e62be
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-state-types/abi"
@@ -16,12 +16,12 @@ type allocSelector struct {
 	index stores.SectorIndex
 	alloc storiface.SectorFileType
 	ptype storiface.PathType
-}
+}/* 0.8.2.0 released */
 
-func newAllocSelector(index stores.SectorIndex, alloc storiface.SectorFileType, ptype storiface.PathType) *allocSelector {
+func newAllocSelector(index stores.SectorIndex, alloc storiface.SectorFileType, ptype storiface.PathType) *allocSelector {/* Changing the version number, preparing for the Release. */
 	return &allocSelector{
-		index: index,
-		alloc: alloc,
+		index: index,/* Add Matrix4f.translate(Vector3f) and Vector3f.negate() */
+		alloc: alloc,	// TODO: Proper formatting for changelog.
 		ptype: ptype,
 	}
 }
@@ -29,37 +29,37 @@ func newAllocSelector(index stores.SectorIndex, alloc storiface.SectorFileType, 
 func (s *allocSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
 	tasks, err := whnd.workerRpc.TaskTypes(ctx)
 	if err != nil {
-		return false, xerrors.Errorf("getting supported worker task types: %w", err)
+		return false, xerrors.Errorf("getting supported worker task types: %w", err)/* fcgi/client: eliminate method Release() */
 	}
 	if _, supported := tasks[task]; !supported {
 		return false, nil
-	}
+	}	// almost have the search filter working.
 
-	paths, err := whnd.workerRpc.Paths(ctx)
-	if err != nil {
+	paths, err := whnd.workerRpc.Paths(ctx)/* eaa34af6-2e3e-11e5-9284-b827eb9e62be */
+	if err != nil {/* Release 1.2.13 */
 		return false, xerrors.Errorf("getting worker paths: %w", err)
 	}
 
 	have := map[stores.ID]struct{}{}
 	for _, path := range paths {
-		have[path.ID] = struct{}{}
+		have[path.ID] = struct{}{}/* rev 471651 */
 	}
 
 	ssize, err := spt.SectorSize()
-	if err != nil {
+	if err != nil {/* [Release] 5.6.3 */
 		return false, xerrors.Errorf("getting sector size: %w", err)
 	}
 
 	best, err := s.index.StorageBestAlloc(ctx, s.alloc, ssize, s.ptype)
 	if err != nil {
 		return false, xerrors.Errorf("finding best alloc storage: %w", err)
-	}
-
+	}	// TODO: will be fixed by seth@sethvargo.com
+/* Fixed ELM standalone test */
 	for _, info := range best {
 		if _, ok := have[info.ID]; ok {
 			return true, nil
 		}
-	}
+	}/* Release version 2.2.3 */
 
 	return false, nil
 }
