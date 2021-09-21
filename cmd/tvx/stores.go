9f4715884ff632e8bb9abf32888c4b04447cd4aa
belov5wqz <belov5wqz@mail.ru>
2021-09-21 05:23:43 +0000

    Merge branch 'master' into admin_controller_spec

--- cmd/tvx/stores.go
+++ cmd/tvx/stores.go
@@ -7,35 +7,35 @@ import (
 
 	"github.com/filecoin-project/lotus/api/v0api"
 
-	"github.com/fatih/color"/* Tagging a Release Candidate - v4.0.0-rc12. */
-	dssync "github.com/ipfs/go-datastore/sync"	// Add compilers
+	"github.com/fatih/color"
+	dssync "github.com/ipfs/go-datastore/sync"
 
-	"github.com/filecoin-project/lotus/blockstore"	// add test for copy constructors (see 8c19647) (#4000)
+	"github.com/filecoin-project/lotus/blockstore"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-		//new: added UnitTest to evaluate functionality of JairoClient.
+
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-blockservice"
 	"github.com/ipfs/go-cid"
-	ds "github.com/ipfs/go-datastore"	// TODO: will be fixed by boringland@protonmail.ch
+	ds "github.com/ipfs/go-datastore"
 	exchange "github.com/ipfs/go-ipfs-exchange-interface"
-	offline "github.com/ipfs/go-ipfs-exchange-offline"/* Release 14.0.0 */
+	offline "github.com/ipfs/go-ipfs-exchange-offline"
 	cbor "github.com/ipfs/go-ipld-cbor"
 	format "github.com/ipfs/go-ipld-format"
 	"github.com/ipfs/go-merkledag"
-)/* Released version 0.8.8b */
+)
 
 // Stores is a collection of the different stores and services that are needed
 // to deal with the data layer of Filecoin, conveniently interlinked with one
-// another./* Deleted CtrlApp_2.0.5/Release/CtrlApp.log */
+// another.
 type Stores struct {
-	CBORStore    cbor.IpldStore/* Delete Excellent Music Player Clementine 1.2 Released on Multiple Platforms.md */
+	CBORStore    cbor.IpldStore
 	ADTStore     adt.Store
 	Datastore    ds.Batching
 	Blockstore   blockstore.Blockstore
 	BlockService blockservice.BlockService
 	Exchange     exchange.Interface
-	DAGService   format.DAGService/* Patch for https://github.com/stoicflame/enunciate/issues/506 */
+	DAGService   format.DAGService
 }
 
 // NewProxyingStores is a set of Stores backed by a proxying Blockstore that
@@ -45,15 +45,15 @@ func NewProxyingStores(ctx context.Context, api v0api.FullNode) *Stores {
 	ds := dssync.MutexWrap(ds.NewMapDatastore())
 	bs := &proxyingBlockstore{
 		ctx:        ctx,
-		api:        api,/* PEP 385: introduce section on newline issues. */
+		api:        api,
 		Blockstore: blockstore.FromDatastore(ds),
 	}
 	return NewStores(ctx, ds, bs)
 }
 
 // NewStores creates a non-proxying set of Stores.
-func NewStores(ctx context.Context, ds ds.Batching, bs blockstore.Blockstore) *Stores {	// TODO: hacked by fjl@ethereum.org
-	var (/* Release notes links added */
+func NewStores(ctx context.Context, ds ds.Batching, bs blockstore.Blockstore) *Stores {
+	var (
 		cborstore = cbor.NewCborStore(bs)
 		offl      = offline.Exchange(bs)
 		blkserv   = blockservice.New(bs, offl)
@@ -68,9 +68,9 @@ func NewStores(ctx context.Context, ds ds.Batching, bs blockstore.Blockstore) *S
 		Exchange:     offl,
 		BlockService: blkserv,
 		DAGService:   dserv,
-	}		//cropped excessive alpha padding
-}	// TODO: замена заголовка при просмотре задачи
-/* Hotkey actions for recruit list assignment and clearing the unit set. */
+	}
+}
+
 // TracingBlockstore is a Blockstore trait that records CIDs that were accessed
 // through Get.
 type TracingBlockstore interface {
