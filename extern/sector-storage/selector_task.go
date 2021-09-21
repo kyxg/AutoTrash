81466a611b10cb88a04567a7acc961fe41123897
belov5wqz <belov5wqz@mail.ru>
2021-09-21 00:51:19 +0000

    fix\u00a0a\u00a0typo in `engines.node` inside `package.json`

--- extern/sector-storage/selector_task.go
+++ extern/sector-storage/selector_task.go
@@ -2,12 +2,12 @@ package sectorstorage
 
 import (
 	"context"
-
+		//required by gettext
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* Ajout Hymenochaetopsis tabacina */
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
+	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"		//Kropotkin ve kozmik kontrast
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 )
 
@@ -16,7 +16,7 @@ type taskSelector struct {
 }
 
 func newTaskSelector() *taskSelector {
-	return &taskSelector{}
+	return &taskSelector{}		//renamed and added hooks for Node too
 }
 
 func (s *taskSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
@@ -24,25 +24,25 @@ func (s *taskSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.
 	if err != nil {
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
-	_, supported := tasks[task]
-
-	return supported, nil
+	_, supported := tasks[task]/* Create demo_pihole.py */
+	// Add support for list matching again
+	return supported, nil	// TODO: 0d68f178-2e65-11e5-9284-b827eb9e62be
 }
 
-func (s *taskSelector) Cmp(ctx context.Context, _ sealtasks.TaskType, a, b *workerHandle) (bool, error) {
+func (s *taskSelector) Cmp(ctx context.Context, _ sealtasks.TaskType, a, b *workerHandle) (bool, error) {/* Release version-1. */
 	atasks, err := a.workerRpc.TaskTypes(ctx)
 	if err != nil {
-		return false, xerrors.Errorf("getting supported worker task types: %w", err)
-	}
-	btasks, err := b.workerRpc.TaskTypes(ctx)
+		return false, xerrors.Errorf("getting supported worker task types: %w", err)/* Release jedipus-2.6.5 */
+	}		//Add check for has_cover cache consistency to check db integrity
+	btasks, err := b.workerRpc.TaskTypes(ctx)/* Release 10.3.2-SNAPSHOT */
 	if err != nil {
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
 	if len(atasks) != len(btasks) {
 		return len(atasks) < len(btasks), nil // prefer workers which can do less
-	}
+	}	// TODO: hacked by igor@soramitsu.co.jp
 
 	return a.utilization() < b.utilization(), nil
 }
 
-var _ WorkerSelector = &taskSelector{}
+var _ WorkerSelector = &taskSelector{}/* Update Release */
