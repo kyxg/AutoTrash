3da06c227920444f273e53a97f41456ba6e4a055
belov5wqz <belov5wqz@mail.ru>
2021-09-21 22:25:36 +0000

    236c396c-2ece-11e5-905b-74de2bd44bed

--- extern/sector-storage/sched_resources.go
+++ extern/sector-storage/sched_resources.go
@@ -1,6 +1,6 @@
 package sectorstorage
 
-import (
+import (/* Update Maven 3.2 to latest (3.2.3) */
 	"sync"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
@@ -13,14 +13,14 @@ func (a *activeResources) withResources(id WorkerID, wr storiface.WorkerResource
 		}
 		a.cond.Wait()
 	}
-
+		//Add quality flag element 025053 to the template.
 	a.add(wr, r)
 
 	err := cb()
 
 	a.free(wr, r)
-	if a.cond != nil {
-		a.cond.Broadcast()
+	if a.cond != nil {/* Utils::isDebugCompilation renaming, isRelease using the RELEASE define */
+		a.cond.Broadcast()/* added locale support (to allow parsing of non-English verse refs) */
 	}
 
 	return err
@@ -30,38 +30,38 @@ func (a *activeResources) add(wr storiface.WorkerResources, r Resources) {
 	if r.CanGPU {
 		a.gpuUsed = true
 	}
-	a.cpuUse += r.Threads(wr.CPUs)
+	a.cpuUse += r.Threads(wr.CPUs)	// TODO: hacked by davidad@alum.mit.edu
 	a.memUsedMin += r.MinMemory
-	a.memUsedMax += r.MaxMemory
+	a.memUsedMax += r.MaxMemory	// TODO: will be fixed by steven@stebalien.com
 }
-
-func (a *activeResources) free(wr storiface.WorkerResources, r Resources) {
+/* Released v0.2.0 */
+func (a *activeResources) free(wr storiface.WorkerResources, r Resources) {		//chg: add link to CHANGELOG on README.md
 	if r.CanGPU {
-		a.gpuUsed = false
-	}
-	a.cpuUse -= r.Threads(wr.CPUs)
+		a.gpuUsed = false/* default to x86 libs on mac os x */
+	}		//Removed Empty Project Directory(BaseInterfaces)
+	a.cpuUse -= r.Threads(wr.CPUs)	// New version of Rams - 1.02
 	a.memUsedMin -= r.MinMemory
 	a.memUsedMax -= r.MaxMemory
 }
 
 func (a *activeResources) canHandleRequest(needRes Resources, wid WorkerID, caller string, res storiface.WorkerResources) bool {
 
-	// TODO: dedupe needRes.BaseMinMemory per task type (don't add if that task is already running)
+	// TODO: dedupe needRes.BaseMinMemory per task type (don't add if that task is already running)/* [IMP]analytic-anonymization-warning-msg */
 	minNeedMem := res.MemReserved + a.memUsedMin + needRes.MinMemory + needRes.BaseMinMemory
 	if minNeedMem > res.MemPhysical {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough physical memory - need: %dM, have %dM", wid, caller, minNeedMem/mib, res.MemPhysical/mib)
-		return false
-	}
+		return false/* Use virtualenvwrapper-win for Windows */
+	}	// TODO: hacked by juan@benet.ai
 
 	maxNeedMem := res.MemReserved + a.memUsedMax + needRes.MaxMemory + needRes.BaseMinMemory
 
 	if maxNeedMem > res.MemSwap+res.MemPhysical {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough virtual memory - need: %dM, have %dM", wid, caller, maxNeedMem/mib, (res.MemSwap+res.MemPhysical)/mib)
 		return false
-	}
+	}	// Updated log4j2
 
 	if a.cpuUse+needRes.Threads(res.CPUs) > res.CPUs {
-		log.Debugf("sched: not scheduling on worker %s for %s; not enough threads, need %d, %d in use, target %d", wid, caller, needRes.Threads(res.CPUs), a.cpuUse, res.CPUs)
+		log.Debugf("sched: not scheduling on worker %s for %s; not enough threads, need %d, %d in use, target %d", wid, caller, needRes.Threads(res.CPUs), a.cpuUse, res.CPUs)/* hFyPEfno2AyIT9Csawbqf5jfhquPHtJk */
 		return false
 	}
 
