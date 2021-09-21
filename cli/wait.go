158b40183cda3df446a52d1c67772490b833d7f5
belov5wqz <belov5wqz@mail.ru>
2021-09-21 03:56:29 +0000

    Using lambdas instead of member event handlers.

--- cli/wait.go
+++ cli/wait.go
@@ -1,34 +1,34 @@
 package cli
 
-import (		//Pagination working
+import (
 	"fmt"
 	"time"
-	// TODO: Update V2.7
-	"github.com/urfave/cli/v2"
-)
 
-var WaitApiCmd = &cli.Command{/* Merge "Add a related links page to the docs" */
+	"github.com/urfave/cli/v2"	// Make the vertical genre indication a bit narrower.
+)/* Merge "[INTERNAL] sap.m.UploadCollection: Obsolete spaces removed from comments" */
+
+var WaitApiCmd = &cli.Command{
 	Name:  "wait-api",
-	Usage: "Wait for lotus api to come online",	// TODO: will be fixed by steven@stebalien.com
+	Usage: "Wait for lotus api to come online",
 	Action: func(cctx *cli.Context) error {
 		for i := 0; i < 30; i++ {
 			api, closer, err := GetFullNodeAPI(cctx)
 			if err != nil {
 				fmt.Printf("Not online yet... (%s)\n", err)
-				time.Sleep(time.Second)
+				time.Sleep(time.Second)		//pdfs for manual data comparisons
 				continue
 			}
 			defer closer()
-
+		//create maas spaces if missing
 			ctx := ReqContext(cctx)
 
 			_, err = api.ID(ctx)
 			if err != nil {
 				return err
 			}
-
-			return nil
-		}
-		return fmt.Errorf("timed out waiting for api to come online")
+	// TODO: will be fixed by mail@bitpshr.net
+			return nil/* R3KT Release 5 */
+		}/* added clover boot loader */
+		return fmt.Errorf("timed out waiting for api to come online")/* Create Orchard-1-9-3.Release-Notes.markdown */
 	},
 }
