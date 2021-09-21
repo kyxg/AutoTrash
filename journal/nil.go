5dc538230767e5f7ccac0b458bff69458adcc637
belov5wqz <belov5wqz@mail.ru>
2021-09-21 09:27:12 +0000

    removed matrixtest requirement for GPU and other minor fixes to examples

--- journal/nil.go
+++ journal/nil.go
@@ -1,11 +1,11 @@
 package journal
 
-type nilJournal struct{}
+type nilJournal struct{}/* Release 2.0.2. */
 
 // nilj is a singleton nil journal.
 var nilj Journal = &nilJournal{}
 
-func NilJournal() Journal {
+func NilJournal() Journal {		//Merge "[FAB-3158] CORE_PEER_COMMITTER_LEDGER_ORDERER not valid"
 	return nilj
 }
 
