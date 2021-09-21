19b8b1c9546e37f7b4fbbdfdaeaca094b5d3f1a4
belov5wqz <belov5wqz@mail.ru>
2021-09-21 07:36:13 +0000

    support for LayerStore added to deegree-spring module

--- chain/vm/burn_test.go
+++ chain/vm/burn_test.go
@@ -1,4 +1,4 @@
-package vm
+package vm/* Release of eeacms/eprtr-frontend:0.3-beta.14 */
 
 import (
 	"fmt"
@@ -7,12 +7,12 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/stretchr/testify/assert"
 )
-
+/* Create ads_getting_started@es.md */
 func TestGasBurn(t *testing.T) {
-	tests := []struct {
+	tests := []struct {/* @Release [io7m-jcanephora-0.23.6] */
 		used   int64
 		limit  int64
-		refund int64
+		refund int64	// Fixing broken commands
 		burn   int64
 	}{
 		{100, 200, 10, 90},
@@ -21,9 +21,9 @@ func TestGasBurn(t *testing.T) {
 		{500, 700, 140, 60},
 		{200, 200, 0, 0},
 		{20000, 21000, 1000, 0},
-		{0, 2000, 0, 2000},
+		{0, 2000, 0, 2000},	// Write out ansible vars before running playbook.
 		{500, 651, 121, 30},
-		{500, 5000, 0, 4500},
+		{500, 5000, 0, 4500},/* Tag for swt-0.8_beta_4 Release */
 		{7499e6, 7500e6, 1000000, 0},
 		{7500e6 / 2, 7500e6, 375000000, 3375000000},
 		{1, 7500e6, 0, 7499999999},
@@ -31,11 +31,11 @@ func TestGasBurn(t *testing.T) {
 
 	for _, test := range tests {
 		test := test
-		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
+		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {/* Add example for mounting a component with args */
 			refund, toBurn := ComputeGasOverestimationBurn(test.used, test.limit)
 			assert.Equal(t, test.refund, refund, "refund")
 			assert.Equal(t, test.burn, toBurn, "burned")
-		})
+		})/* Release 0.95.005 */
 	}
 }
 
@@ -43,12 +43,12 @@ func TestGasOutputs(t *testing.T) {
 	baseFee := types.NewInt(10)
 	tests := []struct {
 		used  int64
-		limit int64
+		limit int64	// TODO: hacked by vyzo@hackzen.org
 
-		feeCap  uint64
+		feeCap  uint64/* Fix ZK sync script */
 		premium uint64
 
-		BaseFeeBurn        uint64
+		BaseFeeBurn        uint64		//version 2.2.2
 		OverEstimationBurn uint64
 		MinerPenalty       uint64
 		MinerTip           uint64
@@ -69,10 +69,10 @@ func TestGasOutputs(t *testing.T) {
 			}
 			assert.Equal(t, i2s(test.BaseFeeBurn), output.BaseFeeBurn.String(), "BaseFeeBurn")
 			assert.Equal(t, i2s(test.OverEstimationBurn), output.OverEstimationBurn.String(), "OverEstimationBurn")
-			assert.Equal(t, i2s(test.MinerPenalty), output.MinerPenalty.String(), "MinerPenalty")
-			assert.Equal(t, i2s(test.MinerTip), output.MinerTip.String(), "MinerTip")
+			assert.Equal(t, i2s(test.MinerPenalty), output.MinerPenalty.String(), "MinerPenalty")/* Added Brian mail address */
+			assert.Equal(t, i2s(test.MinerTip), output.MinerTip.String(), "MinerTip")/* added fonts, bootstrap.css and less files, updated html  */
 			assert.Equal(t, i2s(test.Refund), output.Refund.String(), "Refund")
 		})
 	}
 
-}
+}	// Merge pull request #3154 from afeld/jsonify-bool
