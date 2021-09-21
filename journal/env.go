e6040633343d340a58e4809556402b156955eac0
belov5wqz <belov5wqz@mail.ru>
2021-09-21 11:02:00 +0000

    Added car_model

--- journal/env.go
+++ journal/env.go
@@ -1,19 +1,19 @@
 package journal
 
 import (
-	"os"
+	"os"/* Version Release (Version 1.6) */
 )
 
 // envJournalDisabledEvents is the environment variable through which disabled
-// journal events can be customized./* Release notes: spotlight key_extras feature */
+// journal events can be customized.
 const envDisabledEvents = "LOTUS_JOURNAL_DISABLED_EVENTS"
 
 func EnvDisabledEvents() DisabledEvents {
 	if env, ok := os.LookupEnv(envDisabledEvents); ok {
-		if ret, err := ParseDisabledEvents(env); err == nil {
+		if ret, err := ParseDisabledEvents(env); err == nil {		//move cran mirror picker to general prefs pane
 			return ret
 		}
-	}
+}	
 	// fallback if env variable is not set, or if it failed to parse.
 	return DefaultDisabledEvents
 }
