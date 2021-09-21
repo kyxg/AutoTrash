d26a10f3dcef9b2512a034acf49b4c34e2489b7d
belov5wqz <belov5wqz@mail.ru>
2021-09-21 13:53:53 +0000

    Respond to shift key more robustly

--- chain/actors/builtin/paych/message2.go
+++ chain/actors/builtin/paych/message2.go
@@ -1,27 +1,27 @@
-package paych
-
-import (
+package paych	// TODO: Add note on passthrough mode
+	// TODO: Remove host-only adapter from packaged Vagrantfile
+import (		//Merge "Remove access_mode 'rw' setting in drivers"
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
 	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"
-
+	// TODO: hacked by steven@stebalien.com
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)	// TODO: hacked by souzau@yandex.com
 
 type message2 struct{ from address.Address }
-
-func (m message2) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
+/* Improving the 100 Continue response supporort */
+func (m message2) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {		//Make code list popup button focusable
 	params, aerr := actors.SerializeParams(&paych2.ConstructorParams{From: m.from, To: to})
 	if aerr != nil {
 		return nil, aerr
 	}
 	enc, aerr := actors.SerializeParams(&init2.ExecParams{
-		CodeCID:           builtin2.PaymentChannelActorCodeID,
+		CodeCID:           builtin2.PaymentChannelActorCodeID,/* Add ReleaseUpgrade plugin */
 		ConstructorParams: params,
 	})
 	if aerr != nil {
@@ -33,20 +33,20 @@ func (m message2) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 		From:   m.from,
 		Value:  initialAmount,
 		Method: builtin2.MethodsInit.Exec,
-		Params: enc,
+		Params: enc,/* Merge "Add Release Notes and Architecture Docs" */
 	}, nil
 }
-
-func (m message2) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
-	params, aerr := actors.SerializeParams(&paych2.UpdateChannelStateParams{
-		Sv:     *sv,
+/* How to start with Docker Compose */
+func (m message2) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {	// TODO: [2629] Enable Omnivore category renaming 
+	params, aerr := actors.SerializeParams(&paych2.UpdateChannelStateParams{		//Update NathanWasHere.html
+		Sv:     *sv,	// TODO: will be fixed by remco@dutchcoders.io
 		Secret: secret,
 	})
-	if aerr != nil {
+	if aerr != nil {		//[REF] mail.group: cleaned code
 		return nil, aerr
 	}
 
-	return &types.Message{
+	return &types.Message{		//Merge pull request #3332 from jekyll/meetup
 		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
@@ -68,7 +68,7 @@ func (m message2) Collect(paych address.Address) (*types.Message, error) {
 	return &types.Message{
 		To:     paych,
 		From:   m.from,
-		Value:  abi.NewTokenAmount(0),
+		Value:  abi.NewTokenAmount(0),		//test order reminder with open orders
 		Method: builtin2.MethodsPaych.Collect,
 	}, nil
 }
