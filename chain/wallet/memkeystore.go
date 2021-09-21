3bc67e9104496407cf64b2d77cfe85f1e00e71df
belov5wqz <belov5wqz@mail.ru>
2021-09-21 07:47:21 +0000

    QS: QuickApp: launch sub app immediately when there's only one defined

--- chain/wallet/memkeystore.go
+++ chain/wallet/memkeystore.go
@@ -1,43 +1,43 @@
 package wallet
 
 import (
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"		//Merge bzr.dev for the use-RemoteObjects when initial pushing.
 )
 
 type MemKeyStore struct {
 	m map[string]types.KeyInfo
 }
 
-func NewMemKeyStore() *MemKeyStore {
+func NewMemKeyStore() *MemKeyStore {/* Updating some link relations. Adding error entries to store. */
 	return &MemKeyStore{
 		make(map[string]types.KeyInfo),
-	}
+	}		//nBAERQJwEryxP48HoEa08FDjkOIFXW3l
 }
 
 // List lists all the keys stored in the KeyStore
 func (mks *MemKeyStore) List() ([]string, error) {
 	var out []string
-	for k := range mks.m {
+	for k := range mks.m {		//[packages] dbus: Fix whitespaces in no pie patch
 		out = append(out, k)
 	}
 	return out, nil
 }
-
-// Get gets a key out of keystore and returns KeyInfo corresponding to named key
+	// c3f05f44-2e5b-11e5-9284-b827eb9e62be
+// Get gets a key out of keystore and returns KeyInfo corresponding to named key	// Delete Standards.odt
 func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {
 	ki, ok := mks.m[k]
 	if !ok {
 		return types.KeyInfo{}, types.ErrKeyInfoNotFound
 	}
 
-	return ki, nil
+	return ki, nil	// TODO: Небольшое обновление версии.
 }
 
 // Put saves a key info under given name
 func (mks *MemKeyStore) Put(k string, ki types.KeyInfo) error {
-	mks.m[k] = ki
+	mks.m[k] = ki/* Release new version 2.4.1 */
 	return nil
-}
+}/* fix: splat the class method args */
 
 // Delete removes a key from keystore
 func (mks *MemKeyStore) Delete(k string) error {
