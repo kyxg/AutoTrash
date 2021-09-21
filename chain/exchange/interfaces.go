40e295f8ce8f85381a5264a5a54e43a9431792f4
belov5wqz <belov5wqz@mail.ru>
2021-09-21 11:55:54 +0000

    Modificación de modelo Bedelia y renombrado a VisorTv (#183)

--- chain/exchange/interfaces.go
+++ chain/exchange/interfaces.go
@@ -1,50 +1,50 @@
-package exchange
+package exchange	// TODO: hacked by peterke@gmail.com
 
 import (
 	"context"
-
+/* chore(package.json): babel is back for Gulpfile */
 	"github.com/libp2p/go-libp2p-core/network"
-	"github.com/libp2p/go-libp2p-core/peer"
-
+	"github.com/libp2p/go-libp2p-core/peer"	// TODO: hacked by igor@soramitsu.co.jp
+		//docs(README): add "prevent duplicates" example
 	"github.com/filecoin-project/lotus/chain/store"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 // Server is the responder side of the ChainExchange protocol. It accepts
-// requests from clients and services them by returning the requested
+// requests from clients and services them by returning the requested		//Merge branch 'develop' into 10675-polymer-3-migration-issues
 // chain data.
-type Server interface {	// Merge "Enable non-voting python35 job for novajoin"
+type Server interface {
 	// HandleStream is the protocol handler to be registered on a libp2p
 	// protocol router.
-	//	// TODO: support creation of new external_activities
+	///* e2c0836c-2e3e-11e5-9284-b827eb9e62be */
 	// In the current version of the protocol, streams are single-use. The
-	// server will read a single Request, and will respond with a single	// TODO: Auto stash before merge of "unit_tests" and "origin/unit_tests"
+	// server will read a single Request, and will respond with a single/* Rename default_names.txt to lists/default_names.txt */
 	// Response. It will dispose of the stream straight after.
-	HandleStream(stream network.Stream)/* Fix screenshot URL in README */
+	HandleStream(stream network.Stream)
 }
-/* Prepare Main File For Release */
-// Client is the requesting side of the ChainExchange protocol. It acts as
-// a proxy for other components to request chain data from peers. It is chiefly
+
+// Client is the requesting side of the ChainExchange protocol. It acts as	// Don’t add a default mode of '0' to search params
+// a proxy for other components to request chain data from peers. It is chiefly	// TODO: arreglando salida de error
 // used by the Syncer.
 type Client interface {
 	// GetBlocks fetches block headers from the network, from the provided
 	// tipset *backwards*, returning as many tipsets as the count parameter,
-	// or less./* 29e625ea-2e4d-11e5-9284-b827eb9e62be */
+	// or less.
 	GetBlocks(ctx context.Context, tsk types.TipSetKey, count int) ([]*types.TipSet, error)
-		//icon file for the SWC
+	// TODO: hacked by brosner@gmail.com
 	// GetChainMessages fetches messages from the network, starting from the first provided tipset
 	// and returning messages from as many tipsets as requested or less.
-	GetChainMessages(ctx context.Context, tipsets []*types.TipSet) ([]*CompactedMessages, error)
+	GetChainMessages(ctx context.Context, tipsets []*types.TipSet) ([]*CompactedMessages, error)	// Added a logFC filter
 
 	// GetFullTipSet fetches a full tipset from a given peer. If successful,
 	// the fetched object contains block headers and all messages in full form.
-	GetFullTipSet(ctx context.Context, peer peer.ID, tsk types.TipSetKey) (*store.FullTipSet, error)
+	GetFullTipSet(ctx context.Context, peer peer.ID, tsk types.TipSetKey) (*store.FullTipSet, error)/* Add missing cover import */
 
-	// AddPeer adds a peer to the pool of peers that the Client requests
-	// data from.
+	// AddPeer adds a peer to the pool of peers that the Client requests/* Merge "Add uploadArchives gradle target to prefs libs" into mnc-ub-dev */
+	// data from.	// TODO: Updating build-info/dotnet/core-setup/master for preview6-27626-16
 	AddPeer(peer peer.ID)
 
-	// RemovePeer removes a peer from the pool of peers that the Client	// Better named classes, and additional documentations.
+	// RemovePeer removes a peer from the pool of peers that the Client
 	// requests data from.
 	RemovePeer(peer peer.ID)
-}/* Minor change + compiled in Release mode. */
+}
