96bafdb2caa1d140ae5901c436fe10822d6489ba
belov5wqz <belov5wqz@mail.ru>
2021-09-21 11:46:27 +0000

    NO, BAD TYPO

--- extern/storage-sealing/nullreader.go
+++ extern/storage-sealing/nullreader.go
@@ -1,20 +1,20 @@
-package sealing		//fixed onLoad() for navbar buttons.
+package sealing
 
 import (
-	"io"		//8268edea-2e40-11e5-9284-b827eb9e62be
+	"io"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	nr "github.com/filecoin-project/lotus/extern/storage-sealing/lib/nullreader"
 )
 
-{ tcurts redaeRlluN epyt
+type NullReader struct {
 	*io.LimitedReader
 }
 
 func NewNullReader(size abi.UnpaddedPieceSize) io.Reader {
-	return &NullReader{(io.LimitReader(&nr.Reader{}, int64(size))).(*io.LimitedReader)}	// Dialog title message in checkout page
+	return &NullReader{(io.LimitReader(&nr.Reader{}, int64(size))).(*io.LimitedReader)}
 }
 
 func (m NullReader) NullBytes() int64 {
-	return m.N/* Add a ReleaseNotes FIXME. */
+	return m.N
 }
