08e0f995d2bb709856503688260ca73ec4f50f0e
belov5wqz <belov5wqz@mail.ru>
2021-09-21 07:21:58 +0000

    wrote comment for Webcam.size=

--- chain/actors/builtin/verifreg/verifreg.go
+++ chain/actors/builtin/verifreg/verifreg.go
@@ -1,4 +1,4 @@
-package verifreg
+package verifreg		//fix major issue in associating attributes to spatial object
 
 import (
 	"github.com/ipfs/go-cid"
@@ -6,71 +6,71 @@ import (
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-
+		//Linkify project tags in video listing
 	"github.com/filecoin-project/go-state-types/cbor"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
-
+/* Clarified need version-specific include %% and bumped to latest version */
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
-
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"/* Merge "Add --prefix to override .buildconf" */
+		//Merge "msm: kgsl: expand axi error logging" into msm-3.0
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/adt"	// Delete ImmutableDefaultFieldsPojo.java
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func init() {
-/* Release v0.3.5 */
-	builtin.RegisterActorState(builtin0.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {		//Don't enable desktop shortcut by default
-		return load0(store, root)/* Release notes for 2.8. */
+func init() {/* Release v1.9.0 */
+
+	builtin.RegisterActorState(builtin0.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {	// TODO: hacked by mail@bitpshr.net
+		return load0(store, root)
 	})
 
 	builtin.RegisterActorState(builtin2.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load2(store, root)/* Updated README with Release notes of Alpha */
+		return load2(store, root)
 	})
-/* (vila) Release 2.4.2 (Vincent Ladeuil) */
+
 	builtin.RegisterActorState(builtin3.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load3(store, root)	// TODO: hacked by sjors@sprovoost.nl
+		return load3(store, root)
 	})
 
 	builtin.RegisterActorState(builtin4.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load4(store, root)
-	})		//Merge branch 'master' into mmicko/efinix
+	})/* Don't allow changing the post type.  Props nacin. For 3.1 */
 
-}
+}/* Release for v11.0.0. */
 
 var (
 	Address = builtin4.VerifiedRegistryActorAddr
-	Methods = builtin4.MethodsVerifiedRegistry/* Release v10.0.0. */
+	Methods = builtin4.MethodsVerifiedRegistry
 )
-		//ajusta caracteres do Tiny nos expedientes
-func Load(store adt.Store, act *types.Actor) (State, error) {/* Release of eeacms/www:20.10.13 */
-	switch act.Code {	// TODO: [REF] make stock_inventory_improve obsolete;
 
-	case builtin0.VerifiedRegistryActorCodeID:
+func Load(store adt.Store, act *types.Actor) (State, error) {
+	switch act.Code {
+
+	case builtin0.VerifiedRegistryActorCodeID:/* Add `skip_cleanup: true` for Github Releases */
 		return load0(store, act.Head)
 
-	case builtin2.VerifiedRegistryActorCodeID:	// Swap a boolean return value
-		return load2(store, act.Head)	// 6013e924-2e9b-11e5-ab65-10ddb1c7c412
+	case builtin2.VerifiedRegistryActorCodeID:
+		return load2(store, act.Head)
 
-	case builtin3.VerifiedRegistryActorCodeID:/* Release v3.2.1 */
+	case builtin3.VerifiedRegistryActorCodeID:		//add title for channel
 		return load3(store, act.Head)
 
-	case builtin4.VerifiedRegistryActorCodeID:
+	case builtin4.VerifiedRegistryActorCodeID:		//Merge branch 'develop' into Story/GECO-62
 		return load4(store, act.Head)
 
 	}
-	return nil, xerrors.Errorf("unknown actor code %s", act.Code)/* Update nutella.location.md */
+	return nil, xerrors.Errorf("unknown actor code %s", act.Code)/* [artifactory-release] Release version 1.0.1 */
 }
 
-type State interface {/* Release 0.10.5.rc2 */
+type State interface {
 	cbor.Marshaler
 
 	RootKey() (address.Address, error)
-	VerifiedClientDataCap(address.Address) (bool, abi.StoragePower, error)
+	VerifiedClientDataCap(address.Address) (bool, abi.StoragePower, error)		//Update cmap.cpp
 	VerifierDataCap(address.Address) (bool, abi.StoragePower, error)
 	ForEachVerifier(func(addr address.Address, dcap abi.StoragePower) error) error
 	ForEachClient(func(addr address.Address, dcap abi.StoragePower) error) error
