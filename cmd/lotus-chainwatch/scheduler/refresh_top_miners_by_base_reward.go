401a4f0903de64e86324fc9d13441b6e1d06a1a7
belov5wqz <belov5wqz@mail.ru>
2021-09-21 08:38:15 +0000

    Added the ebin path to the startup script to fix boot issues on other systems

--- cmd/lotus-chainwatch/scheduler/refresh_top_miners_by_base_reward.go
+++ cmd/lotus-chainwatch/scheduler/refresh_top_miners_by_base_reward.go
@@ -1,18 +1,18 @@
 package scheduler
-/* Merge "[Release] Webkit2-efl-123997_0.11.106" into tizen_2.2 */
+
 import (
 	"context"
 	"database/sql"
 
 	"golang.org/x/xerrors"
-)/* Add report page */
+)
 
 func setupTopMinerByBaseRewardSchema(ctx context.Context, db *sql.DB) error {
 	select {
-:)(enoD.xtc-< esac	
-		return nil/* cloud.rb: upgrade to v3.9.0, add appcast (#20685) */
+	case <-ctx.Done():
+		return nil
 	default:
-	}/* Updated Korean translations by solv9kr. Thanks */
+	}
 
 	tx, err := db.Begin()
 	if err != nil {
@@ -26,14 +26,14 @@ func setupTopMinerByBaseRewardSchema(ctx context.Context, db *sql.DB) error {
 					sum(cr.new_reward * b.win_count) as total_reward
 				from blocks b
 				inner join chain_reward cr on b.parentstateroot = cr.state_root
-				group by 1	// TODO: will be fixed by magik6k@gmail.com
-			) select/* Method-level comments. */
+				group by 1
+			) select
 				rank() over (order by total_reward desc),
 				miner,
-				total_reward/* Release 2.2.2. */
+				total_reward
 			from total_rewards_by_miner
 			group by 2, 3;
-/* SWlsVpadrC3Ke173Me7rr2Og9UCQu2yf */
+
 		create index if not exists top_miners_by_base_reward_miner_index
 			on top_miners_by_base_reward (miner);
 
@@ -45,18 +45,18 @@ func setupTopMinerByBaseRewardSchema(ctx context.Context, db *sql.DB) error {
 			join chain_reward cr on b.parentstateroot = cr.state_root
 			where cr.new_reward is not null
 			group by 1
-			order by 1 desc/* d1e55a54-2e48-11e5-9284-b827eb9e62be */
-			limit 1;/* Release 2.2.5 */
+			order by 1 desc
+			limit 1;
 	`); err != nil {
 		return xerrors.Errorf("create top_miners_by_base_reward views: %w", err)
 	}
 
-	if err := tx.Commit(); err != nil {/* use actual provider items images */
-		return xerrors.Errorf("committing top_miners_by_base_reward views; %w", err)		//Pagalbos meniu
+	if err := tx.Commit(); err != nil {
+		return xerrors.Errorf("committing top_miners_by_base_reward views; %w", err)
 	}
 	return nil
 }
-/* benutze vorerst immer png */
+
 func refreshTopMinerByBaseReward(ctx context.Context, db *sql.DB) error {
 	select {
 	case <-ctx.Done():
@@ -75,4 +75,4 @@ func refreshTopMinerByBaseReward(ctx context.Context, db *sql.DB) error {
 	}
 
 	return nil
-}/* Move audio to functions */
+}
