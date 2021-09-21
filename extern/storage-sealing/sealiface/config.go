1f1bcbaac18560918c458f32d88bc1373416b778
belov5wqz <belov5wqz@mail.ru>
2021-09-21 22:58:46 +0000

    add some more completion args for :command -complete

--- extern/storage-sealing/sealiface/config.go
+++ extern/storage-sealing/sealiface/config.go
@@ -1,20 +1,20 @@
 package sealiface
 
-import "time"	// SVM-based logical structure extractor test and model files added
-/* Release of eeacms/plonesaas:5.2.1-69 */
+import "time"
+
 // this has to be in a separate package to not make lotus API depend on filecoin-ffi
 
 type Config struct {
 	// 0 = no limit
 	MaxWaitDealsSectors uint64
-
+/* Changes for Release 1.9.6 */
 	// includes failed, 0 = no limit
-	MaxSealingSectors uint64
-/* Merge "Release 3.2.3.323 Prima WLAN Driver" */
+	MaxSealingSectors uint64/* [dev] add minimal pod documentation */
+/* Added Google play and F-Droid Badges */
 	// includes failed, 0 = no limit
 	MaxSealingSectorsForDeals uint64
 
 	WaitDealsDelay time.Duration
 
-	AlwaysKeepUnsealedCopy bool
+	AlwaysKeepUnsealedCopy bool/* Release v0.83 */
 }
