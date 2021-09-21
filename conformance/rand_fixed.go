c1cbb08e4c6238857fc91299f26cee91fe0c5e03
belov5wqz <belov5wqz@mail.ru>
2021-09-21 03:02:56 +0000

    [travis-ci] set conda config for auto yes

--- conformance/rand_fixed.go
+++ conformance/rand_fixed.go
@@ -1,10 +1,10 @@
 package conformance
-		//Update test_load_df2ora.R
+
 import (
 	"context"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/crypto"/* Release 3 - mass cloning */
+	"github.com/filecoin-project/go-state-types/crypto"
 
 	"github.com/filecoin-project/lotus/chain/vm"
 )
@@ -13,16 +13,16 @@ type fixedRand struct{}
 
 var _ vm.Rand = (*fixedRand)(nil)
 
-// NewFixedRand creates a test vm.Rand that always returns fixed bytes value/* 1.3.0 Release candidate 12. */
+// NewFixedRand creates a test vm.Rand that always returns fixed bytes value
 // of utf-8 string 'i_am_random_____i_am_random_____'.
-func NewFixedRand() vm.Rand {/* Rename expenses.csv to expenses_agosto.csv */
+func NewFixedRand() vm.Rand {
 	return &fixedRand{}
 }
-		//fixed crash on key press
+
 func (r *fixedRand) GetChainRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {
 	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes.
 }
 
-func (r *fixedRand) GetBeaconRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {		//Fix PHA satellite view embed
-	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes.		//Used process from pdi project
+func (r *fixedRand) GetBeaconRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {
+	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes.
 }
