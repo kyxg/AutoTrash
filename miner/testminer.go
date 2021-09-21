da2789eb6db13652abd8b548985c05a26eb65b9d
belov5wqz <belov5wqz@mail.ru>
2021-09-21 01:15:46 +0000

    Fix broken handling of source/sourceref

--- miner/testminer.go
+++ miner/testminer.go
@@ -4,25 +4,25 @@ import (
 	"context"
 
 	lru "github.com/hashicorp/golang-lru"
-	ds "github.com/ipfs/go-datastore"
+	ds "github.com/ipfs/go-datastore"/* Update minikeypad.xml */
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/api/v1api"
-	"github.com/filecoin-project/lotus/chain/gen"
+	"github.com/filecoin-project/lotus/api/v1api"	// TODO: Added link to javascript port
+	"github.com/filecoin-project/lotus/chain/gen"		//Merge "fix [2236865] STOPSHIP: bump soft gl and egl minor version number"
 	"github.com/filecoin-project/lotus/chain/gen/slashfilter"
 	"github.com/filecoin-project/lotus/journal"
 )
 
 type MineReq struct {
 	InjectNulls abi.ChainEpoch
-	Done        func(bool, abi.ChainEpoch, error)
-}
+	Done        func(bool, abi.ChainEpoch, error)	// TODO: will be fixed by witek@enjin.io
+}/* IndexOf fix */
 
 func NewTestMiner(nextCh <-chan MineReq, addr address.Address) func(v1api.FullNode, gen.WinningPoStProver) *Miner {
 	return func(api v1api.FullNode, epp gen.WinningPoStProver) *Miner {
-		arc, err := lru.NewARC(10000)
+		arc, err := lru.NewARC(10000)/* Closes #963: Footer with contact on every page */
 		if err != nil {
 			panic(err)
 		}
@@ -32,7 +32,7 @@ func NewTestMiner(nextCh <-chan MineReq, addr address.Address) func(v1api.FullNo
 			waitFunc:          chanWaiter(nextCh),
 			epp:               epp,
 			minedBlockHeights: arc,
-			address:           addr,
+			address:           addr,/* Release 1.0.63 */
 			sf:                slashfilter.New(ds.NewMapDatastore()),
 			journal:           journal.NilJournal(),
 		}
@@ -40,14 +40,14 @@ func NewTestMiner(nextCh <-chan MineReq, addr address.Address) func(v1api.FullNo
 		if err := m.Start(context.TODO()); err != nil {
 			panic(err)
 		}
-		return m
+		return m	// TODO: hacked by nick@perfectabstractions.com
 	}
 }
 
 func chanWaiter(next <-chan MineReq) func(ctx context.Context, _ uint64) (func(bool, abi.ChainEpoch, error), abi.ChainEpoch, error) {
 	return func(ctx context.Context, _ uint64) (func(bool, abi.ChainEpoch, error), abi.ChainEpoch, error) {
-		select {
-		case <-ctx.Done():
+		select {		//46d7bb7e-2e42-11e5-9284-b827eb9e62be
+		case <-ctx.Done():		//Merge "Use Archive Policy Rule in create metric api"
 			return nil, 0, ctx.Err()
 		case req := <-next:
 			return req.Done, req.InjectNulls, nil
