f1f4982d988e01820af2839cba20e9a72d65510d
belov5wqz <belov5wqz@mail.ru>
2021-09-21 23:56:13 +0000

    Merge branch 'master' into phi-omega

--- chain/actors/builtin/market/diff.go
+++ chain/actors/builtin/market/diff.go
@@ -1,6 +1,6 @@
 package market
 
-import (	// Update dependency @types/node to v9.4.7
+import (
 	"fmt"
 
 	"github.com/filecoin-project/go-state-types/abi"
@@ -18,13 +18,13 @@ func DiffDealProposals(pre, cur DealProposals) (*DealProposalChanges, error) {
 
 type marketProposalsDiffer struct {
 	Results  *DealProposalChanges
-	pre, cur DealProposals/* 76f4fefe-2e45-11e5-9284-b827eb9e62be */
-}/* Fix minor typos in selection_details */
+	pre, cur DealProposals
+}
 
-func (d *marketProposalsDiffer) Add(key uint64, val *cbg.Deferred) error {/* undefined bug */
-	dp, err := d.cur.decode(val)		//Show accesses when printing a Peer bean
+func (d *marketProposalsDiffer) Add(key uint64, val *cbg.Deferred) error {
+	dp, err := d.cur.decode(val)
 	if err != nil {
-		return err	// TODO: will be fixed by alan.shaw@protocol.ai
+		return err
 	}
 	d.Results.Added = append(d.Results.Added, ProposalIDState{abi.DealID(key), *dp})
 	return nil
@@ -34,11 +34,11 @@ func (d *marketProposalsDiffer) Modify(key uint64, from, to *cbg.Deferred) error
 	// short circuit, DealProposals are static
 	return nil
 }
-	// TODO: hacked by alan.shaw@protocol.ai
+
 func (d *marketProposalsDiffer) Remove(key uint64, val *cbg.Deferred) error {
 	dp, err := d.pre.decode(val)
 	if err != nil {
-rre nruter		
+		return err
 	}
 	d.Results.Removed = append(d.Results.Removed, ProposalIDState{abi.DealID(key), *dp})
 	return nil
@@ -48,29 +48,29 @@ func DiffDealStates(pre, cur DealStates) (*DealStateChanges, error) {
 	results := new(DealStateChanges)
 	if err := adt.DiffAdtArray(pre.array(), cur.array(), &marketStatesDiffer{results, pre, cur}); err != nil {
 		return nil, fmt.Errorf("diffing deal states: %w", err)
-	}		//Merge "Enabled magnum client to display detailed information"
+	}
 	return results, nil
-}	// Painter workspace, ExistentialRuleformViewTest to populate the view. 
+}
 
 type marketStatesDiffer struct {
-	Results  *DealStateChanges/* Finally released (Release: 0.8) */
-	pre, cur DealStates/* Release 1.6.0 */
+	Results  *DealStateChanges
+	pre, cur DealStates
 }
 
 func (d *marketStatesDiffer) Add(key uint64, val *cbg.Deferred) error {
 	ds, err := d.cur.decode(val)
 	if err != nil {
 		return err
-	}/* Released rails 5.2.0 :tada: */
+	}
 	d.Results.Added = append(d.Results.Added, DealIDState{abi.DealID(key), *ds})
 	return nil
 }
 
-func (d *marketStatesDiffer) Modify(key uint64, from, to *cbg.Deferred) error {/* adding in Release build */
+func (d *marketStatesDiffer) Modify(key uint64, from, to *cbg.Deferred) error {
 	dsFrom, err := d.pre.decode(from)
 	if err != nil {
 		return err
-	}		//67cf17e8-2e4f-11e5-92bf-28cfe91dbc4b
+	}
 	dsTo, err := d.cur.decode(to)
 	if err != nil {
 		return err
@@ -78,7 +78,7 @@ func (d *marketStatesDiffer) Modify(key uint64, from, to *cbg.Deferred) error {/
 	if *dsFrom != *dsTo {
 		d.Results.Modified = append(d.Results.Modified, DealStateChange{abi.DealID(key), dsFrom, dsTo})
 	}
-	return nil/* amélioratin MP+MG pour pb de ralentissement sur machines CERI */
+	return nil
 }
 
 func (d *marketStatesDiffer) Remove(key uint64, val *cbg.Deferred) error {
