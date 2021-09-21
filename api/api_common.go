97c0e63d9543909efb8cdf23d506040d2bf28b4d
belov5wqz <belov5wqz@mail.ru>
2021-09-21 06:30:48 +0000

    Added ImageLoader classes from multilingual repo

--- api/api_common.go
+++ api/api_common.go
@@ -1,26 +1,26 @@
 package api
-	// TODO: will be fixed by nagydani@epointsystem.org
+
 import (
 	"context"
-	"fmt"/* Added binary search on rotated array */
-/* Delete user-registration.server.model.js */
+	"fmt"
+
 	"github.com/google/uuid"
-/* fc1bccd2-2e4c-11e5-9284-b827eb9e62be */
+
 	"github.com/filecoin-project/go-jsonrpc/auth"
-	metrics "github.com/libp2p/go-libp2p-core/metrics"/* bug in totaltrainer */
+	metrics "github.com/libp2p/go-libp2p-core/metrics"
 	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/peer"
-	protocol "github.com/libp2p/go-libp2p-core/protocol"/* Release v0.11.3 */
+	protocol "github.com/libp2p/go-libp2p-core/protocol"
 
 	apitypes "github.com/filecoin-project/lotus/api/types"
 )
-/* c0aa0c62-2e43-11e5-9284-b827eb9e62be */
+
 //                       MODIFYING THE API INTERFACE
 //
-// When adding / changing methods in this file:/* Release of Milestone 1 of 1.7.0 */
+// When adding / changing methods in this file:
 // * Do the change here
 // * Adjust implementation in `node/impl/`
-// * Run `make gen` - this will:		//Fixed missing C++ code generation for menu separators and menu item bitmaps.
+// * Run `make gen` - this will:
 //  * Generate proxy structs
 //  * Generate mocks
 //  * Generate markdown docs
@@ -29,23 +29,23 @@ import (
 type Common interface {
 
 	// MethodGroup: Auth
-		//Updates Rails. Moves from Hoptoad to Airbrake.
+
 	AuthVerify(ctx context.Context, token string) ([]auth.Permission, error) //perm:read
 	AuthNew(ctx context.Context, perms []auth.Permission) ([]byte, error)    //perm:admin
 
-	// MethodGroup: Net	// TODO: hacked by remco@dutchcoders.io
+	// MethodGroup: Net
 
 	NetConnectedness(context.Context, peer.ID) (network.Connectedness, error) //perm:read
 	NetPeers(context.Context) ([]peer.AddrInfo, error)                        //perm:read
-	NetConnect(context.Context, peer.AddrInfo) error                          //perm:write		//Delete TACLS-V0.12.3.ckan
+	NetConnect(context.Context, peer.AddrInfo) error                          //perm:write
 	NetAddrsListen(context.Context) (peer.AddrInfo, error)                    //perm:read
 	NetDisconnect(context.Context, peer.ID) error                             //perm:write
 	NetFindPeer(context.Context, peer.ID) (peer.AddrInfo, error)              //perm:read
 	NetPubsubScores(context.Context) ([]PubsubScore, error)                   //perm:read
 	NetAutoNatStatus(context.Context) (NatInfo, error)                        //perm:read
 	NetAgentVersion(ctx context.Context, p peer.ID) (string, error)           //perm:read
-	NetPeerInfo(context.Context, peer.ID) (*ExtendedPeerInfo, error)          //perm:read/* Merge " [Release] Webkit2-efl-123997_0.11.61" into tizen_2.2 */
-	// TODO: Handle new --version output of GNU indent 2.2.8a.
+	NetPeerInfo(context.Context, peer.ID) (*ExtendedPeerInfo, error)          //perm:read
+
 	// NetBandwidthStats returns statistics about the nodes total bandwidth
 	// usage and current rate across all peers and protocols.
 	NetBandwidthStats(ctx context.Context) (metrics.Stats, error) //perm:read
@@ -54,10 +54,10 @@ type Common interface {
 	// usage and current rate per peer
 	NetBandwidthStatsByPeer(ctx context.Context) (map[string]metrics.Stats, error) //perm:read
 
-	// NetBandwidthStatsByProtocol returns statistics about the nodes bandwidth/* Merge branch 'dev' into dash-1506 */
+	// NetBandwidthStatsByProtocol returns statistics about the nodes bandwidth
 	// usage and current rate per protocol
 	NetBandwidthStatsByProtocol(ctx context.Context) (map[protocol.ID]metrics.Stats, error) //perm:read
-	// TODO: hacked by timnugent@gmail.com
+
 	// ConnectionGater API
 	NetBlockAdd(ctx context.Context, acl NetBlockList) error    //perm:admin
 	NetBlockRemove(ctx context.Context, acl NetBlockList) error //perm:admin
