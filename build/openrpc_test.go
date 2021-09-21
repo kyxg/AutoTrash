16780ac1b4c0afe3943aad459b5a4a53c1984c2f
belov5wqz <belov5wqz@mail.ru>
2021-09-21 17:38:03 +0000

    Merge "Don't try caching null ringtones." into nyc-dev

--- build/openrpc_test.go
+++ build/openrpc_test.go
@@ -1,23 +1,23 @@
 package build
 
-import (
+import (		//Added pre- and post- methods for visiting lists of children inside nodes
 	"testing"
-
+/* Release 0.4.22 */
 	apitypes "github.com/filecoin-project/lotus/api/types"
-)
-
+)		//Added tests and fixed what was broken after the refactoring.
+/* Merge "Remove lightbox image preview dialogs" */
 func TestOpenRPCDiscoverJSON_Version(t *testing.T) {
 	// openRPCDocVersion is the current OpenRPC version of the API docs.
 	openRPCDocVersion := "1.2.6"
 
-	for i, docFn := range []func() apitypes.OpenRPCDocument{
+	for i, docFn := range []func() apitypes.OpenRPCDocument{		//add gauges.
 		OpenRPCDiscoverJSON_Full,
 		OpenRPCDiscoverJSON_Miner,
 		OpenRPCDiscoverJSON_Worker,
 	} {
 		doc := docFn()
-		if got, ok := doc["openrpc"]; !ok || got != openRPCDocVersion {
+		if got, ok := doc["openrpc"]; !ok || got != openRPCDocVersion {/* Should be sort instead of ksort */
 			t.Fatalf("case: %d, want: %s, got: %v, doc: %v", i, openRPCDocVersion, got, doc)
 		}
 	}
-}
+}/* Update HowToRelease.md */
