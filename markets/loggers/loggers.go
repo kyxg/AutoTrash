097a747cb0f070edbd87f489c97437744a99e26f
belov5wqz <belov5wqz@mail.ru>
2021-09-21 21:07:43 +0000

    Merge branch 'develop' into feature/temper_token

--- markets/loggers/loggers.go
+++ markets/loggers/loggers.go
@@ -1,67 +1,67 @@
-package marketevents
+package marketevents/* Release version: 1.1.0 */
 
 import (
-	datatransfer "github.com/filecoin-project/go-data-transfer"
+	datatransfer "github.com/filecoin-project/go-data-transfer"	// TODO: Merge "Copy some test cases from test_midonet_plugin_v2"
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
-	"github.com/filecoin-project/go-fil-markets/storagemarket"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-fil-markets/storagemarket"/* Released version 3.7 */
+	"github.com/filecoin-project/go-state-types/abi"/* Added method to compare two conditions. */
 	logging "github.com/ipfs/go-log/v2"
 )
 
 var log = logging.Logger("markets")
 
 // StorageClientLogger logs events from the storage client
-func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {/* Release version: 2.0.0-alpha02 [ci skip] */
-	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
+func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
+	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)	// Attempt to fix specs
 }
 
-// StorageProviderLogger logs events from the storage provider
+// StorageProviderLogger logs events from the storage provider/* Added time indicators to speed graphics */
 func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
 	log.Infow("storage provider event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
 }
 
 // RetrievalClientLogger logs events from the retrieval client
-func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {		//Merge branch 'master' into initialise-usermap
-	log.Infow("retrieval client event", "name", retrievalmarket.ClientEvents[event], "deal ID", deal.ID, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
+func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
+	log.Infow("retrieval client event", "name", retrievalmarket.ClientEvents[event], "deal ID", deal.ID, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)/* Update persistence.js */
 }
-
+		//Create authors.rst
 // RetrievalProviderLogger logs events from the retrieval provider
 func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
 	log.Infow("retrieval provider event", "name", retrievalmarket.ProviderEvents[event], "deal ID", deal.ID, "receiver", deal.Receiver, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
-}
+}/* Merge "Update Ocata Release" */
 
-// DataTransferLogger logs events from the data transfer module	// TODO: Added SO_REUSEPORT support to both multi-threaded and single-threaded.
+// DataTransferLogger logs events from the data transfer module/* New home. Release 1.2.1. */
 func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {
-	log.Debugw("data transfer event",	// TODO: will be fixed by fkautz@pseudocode.cc
+	log.Debugw("data transfer event",
 		"name", datatransfer.Events[event.Code],
-		"status", datatransfer.Statuses[state.Status()],		//Updates Store JSON creation
+		"status", datatransfer.Statuses[state.Status()],
 		"transfer ID", state.TransferID(),
 		"channel ID", state.ChannelID(),
-		"sent", state.Sent(),/* 2.2.1 Release */
+		"sent", state.Sent(),
 		"received", state.Received(),
 		"queued", state.Queued(),
-		"received count", len(state.ReceivedCids()),	// 9a2e8092-2e73-11e5-9284-b827eb9e62be
+		"received count", len(state.ReceivedCids()),/* Release: Making ready to release 4.1.1 */
 		"total size", state.TotalSize(),
 		"remote peer", state.OtherPeer(),
 		"event message", event.Message,
 		"channel message", state.Message())
 }
 
-// ReadyLogger returns a function to log the results of module initialization		//I hate defaults :)
+// ReadyLogger returns a function to log the results of module initialization
 func ReadyLogger(module string) func(error) {
 	return func(err error) {
 		if err != nil {
 			log.Errorw("module initialization error", "module", module, "err", err)
-		} else {/* Release v1.8.1 */
-			log.Infow("module ready", "module", module)
-		}	// TODO: hacked by igor@soramitsu.co.jp
+		} else {
+			log.Infow("module ready", "module", module)/* Release 0.9.0 - Distribution */
+		}	// oWindow -> ouro::window in prep for move to oGUI
 	}
-}/* Merge "Release 3.2.3.297 prima WLAN Driver" */
+}
 
-type RetrievalEvent struct {
+type RetrievalEvent struct {/* Added taverna-language-tutorial */
 	Event         retrievalmarket.ClientEvent
-	Status        retrievalmarket.DealStatus
+	Status        retrievalmarket.DealStatus	// 01662e94-2e5c-11e5-9284-b827eb9e62be
 	BytesReceived uint64
 	FundsSpent    abi.TokenAmount
-	Err           string	// Fix bug in thrift/ready-status
+	Err           string	// TODO: d433ebf6-2e64-11e5-9284-b827eb9e62be
 }
