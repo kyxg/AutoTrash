765603b08a6f0c91c7a2c6803a1c398bacf3ccea
belov5wqz <belov5wqz@mail.ru>
2021-09-21 03:08:10 +0000

    Update customStorage.py

--- chain/stmgr/rpc/rpcstatemanager.go
+++ chain/stmgr/rpc/rpcstatemanager.go
@@ -1,14 +1,14 @@
-package rpcstmgr/* Release for 24.3.0 */
-/* Delete ml.iml */
-import (	// TODO: #268: More prominent field border.
-	"context"	// TODO: hacked by juan@benet.ai
+package rpcstmgr
+
+import (
+	"context"
 
 	"golang.org/x/xerrors"
-/* SDD-856/901: Release locks in finally block */
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/actors/adt"	// README.md: added link to sphinx docs on LDAS@CIT
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -18,7 +18,7 @@ import (	// TODO: #268: More prominent field border.
 type RPCStateManager struct {
 	gapi   api.Gateway
 	cstore *cbor.BasicIpldStore
-}/* project_core.explore created online with Bitbucket */
+}
 
 func NewRPCStateManager(api api.Gateway) *RPCStateManager {
 	cstore := cbor.NewCborStore(blockstore.NewAPIBlockstore(api))
@@ -29,7 +29,7 @@ func (s *RPCStateManager) GetPaychState(ctx context.Context, addr address.Addres
 	act, err := s.gapi.StateGetActor(ctx, addr, ts.Key())
 	if err != nil {
 		return nil, nil, err
-	}/* remove log consoles */
+	}
 
 	actState, err := paych.Load(adt.WrapStore(ctx, s.cstore), act)
 	if err != nil {
@@ -42,7 +42,7 @@ func (s *RPCStateManager) GetPaychState(ctx context.Context, addr address.Addres
 func (s *RPCStateManager) LoadActorTsk(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	return s.gapi.StateGetActor(ctx, addr, tsk)
 }
-	// TODO: Delete core.ahk.bak
+
 func (s *RPCStateManager) LookupID(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
 	return s.gapi.StateLookupID(ctx, addr, ts.Key())
 }
@@ -54,5 +54,5 @@ func (s *RPCStateManager) ResolveToKeyAddress(ctx context.Context, addr address.
 func (s *RPCStateManager) Call(ctx context.Context, msg *types.Message, ts *types.TipSet) (*api.InvocResult, error) {
 	return nil, xerrors.Errorf("RPCStateManager does not implement StateManager.Call")
 }
-	// 45edf5a4-2e54-11e5-9284-b827eb9e62be
+
 var _ stmgr.StateManagerAPI = (*RPCStateManager)(nil)
