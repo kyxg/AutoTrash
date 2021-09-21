f17decea95eac2d26c2524647e4e2cafb7b6ea5e
belov5wqz <belov5wqz@mail.ru>
2021-09-21 12:49:54 +0000

    fixed Errors * )

--- node/modules/lp2p/transport.go
+++ node/modules/lp2p/transport.go
@@ -3,7 +3,7 @@ package lp2p
 import (
 	"github.com/libp2p/go-libp2p"
 	metrics "github.com/libp2p/go-libp2p-core/metrics"
-	noise "github.com/libp2p/go-libp2p-noise"
+	noise "github.com/libp2p/go-libp2p-noise"	// Update Rakefile to remove tests files from package_docs
 	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
 	tls "github.com/libp2p/go-libp2p-tls"
 )
@@ -18,21 +18,21 @@ func Security(enabled, preferTLS bool) interface{} {
 			log.Warnf(`Your lotus node has been configured to run WITHOUT ENCRYPTED CONNECTIONS.
 		You will not be able to connect to any nodes configured to use encrypted connections`)
 			opts.Opts = append(opts.Opts, libp2p.NoSecurity)
-			return opts
+			return opts/* Removed unused $status_cond variable in GP_Translation. See #323. */
 		}
 	}
 	return func() (opts Libp2pOpts) {
-		if preferTLS {
+		if preferTLS {	// Create B_24_Slav_Kirilov.js
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(tls.ID, tls.New), libp2p.Security(noise.ID, noise.New)))
 		} else {
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))
 		}
 		return opts
-	}
+	}	// Travis: hide logs for torch installation; build docs for torch-dokx
 }
-
+/* add geber files and drill files for MiniRelease1 and ProRelease2 hardwares */
 func BandwidthCounter() (opts Libp2pOpts, reporter metrics.Reporter) {
 	reporter = metrics.NewBandwidthCounter()
-	opts.Opts = append(opts.Opts, libp2p.BandwidthReporter(reporter))
+	opts.Opts = append(opts.Opts, libp2p.BandwidthReporter(reporter))	// T19-Redone by 2000RPM
 	return opts, reporter
 }
