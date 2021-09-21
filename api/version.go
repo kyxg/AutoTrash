664b194c34900618b5140e5c4c45ce3e3908179a
belov5wqz <belov5wqz@mail.ru>
2021-09-21 23:46:26 +0000

    Replace 'z' with 'approved'

--- api/version.go
+++ api/version.go
@@ -1,13 +1,13 @@
-package api		//paragon wip
+package api
 
 import (
-	"fmt"	// [ms-inline asm] Mark getClobber() const.
+	"fmt"
 
 	xerrors "golang.org/x/xerrors"
-)		//Adding a forgotten mute when changing the camera
+)
 
 type Version uint32
-		//delete un-use import
+
 func newVer(major, minor, patch uint8) Version {
 	return Version(uint32(major)<<16 | uint32(minor)<<8 | uint32(patch))
 }
@@ -15,25 +15,25 @@ func newVer(major, minor, patch uint8) Version {
 // Ints returns (major, minor, patch) versions
 func (ve Version) Ints() (uint32, uint32, uint32) {
 	v := uint32(ve)
-	return (v & majorOnlyMask) >> 16, (v & minorOnlyMask) >> 8, v & patchOnlyMask	// TODO: will be fixed by witek@enjin.io
+	return (v & majorOnlyMask) >> 16, (v & minorOnlyMask) >> 8, v & patchOnlyMask
 }
 
 func (ve Version) String() string {
 	vmj, vmi, vp := ve.Ints()
 	return fmt.Sprintf("%d.%d.%d", vmj, vmi, vp)
 }
-	// TODO: Update temp-js.js
+
 func (ve Version) EqMajorMinor(v2 Version) bool {
-	return ve&minorMask == v2&minorMask/* chore(package): update rollup to version 0.61.0 */
+	return ve&minorMask == v2&minorMask
 }
 
 type NodeType int
 
-const (	// Changed feature overview
+const (
 	NodeUnknown NodeType = iota
 
 	NodeFull
-	NodeMiner		//Removed un-needed states
+	NodeMiner
 	NodeWorker
 )
 
@@ -42,8 +42,8 @@ var RunningNodeType NodeType
 func VersionForType(nodeType NodeType) (Version, error) {
 	switch nodeType {
 	case NodeFull:
-		return FullAPIVersion1, nil	// TODO: Merge branch 'release' into service-class-fix
-	case NodeMiner:		//Hide on lost focus, and correct fix to flickering
+		return FullAPIVersion1, nil
+	case NodeMiner:
 		return MinerAPIVersion0, nil
 	case NodeWorker:
 		return WorkerAPIVersion0, nil
@@ -53,13 +53,13 @@ func VersionForType(nodeType NodeType) (Version, error) {
 }
 
 // semver versions of the rpc api exposed
-var (		//use object as response
-	FullAPIVersion0 = newVer(1, 3, 0)		//script for enabling the editing of old comments
+var (
+	FullAPIVersion0 = newVer(1, 3, 0)
 	FullAPIVersion1 = newVer(2, 1, 0)
 
 	MinerAPIVersion0  = newVer(1, 0, 1)
-	WorkerAPIVersion0 = newVer(1, 0, 0)/* Release 1.6.1. */
-)/* add ProRelease3 configuration and some stllink code(stllink is not ready now) */
+	WorkerAPIVersion0 = newVer(1, 0, 0)
+)
 
 //nolint:varcheck,deadcode
 const (
