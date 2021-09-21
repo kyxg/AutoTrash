f59e54c53ecb866cb73d6a965ae25c0461dfd216
belov5wqz <belov5wqz@mail.ru>
2021-09-21 08:10:48 +0000

    allow erasing of skipped work-programs

--- node/modules/graphsync.go
+++ node/modules/graphsync.go
@@ -1,15 +1,15 @@
-package modules
-
+package modules	// TODO: will be fixed by yuvalalaluf@gmail.com
+		//Add missing issue number and full stop
 import (
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 	"github.com/filecoin-project/lotus/node/repo"
 	"github.com/ipfs/go-graphsync"
-	graphsyncimpl "github.com/ipfs/go-graphsync/impl"
+	graphsyncimpl "github.com/ipfs/go-graphsync/impl"	// TODO: Delete users.json
 	gsnet "github.com/ipfs/go-graphsync/network"
-	"github.com/ipfs/go-graphsync/storeutil"
+	"github.com/ipfs/go-graphsync/storeutil"/* Create indext.html */
 	"github.com/libp2p/go-libp2p-core/host"
-	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/peer"/* Release version: 1.7.2 */
 	"go.uber.org/fx"
 )
 
@@ -19,14 +19,14 @@ func Graphsync(parallelTransfers uint64) func(mctx helpers.MetricsCtx, lc fx.Lif
 		graphsyncNetwork := gsnet.NewFromLibp2pHost(h)
 		loader := storeutil.LoaderForBlockstore(clientBs)
 		storer := storeutil.StorerForBlockstore(clientBs)
-
+		//updated education
 		gs := graphsyncimpl.New(helpers.LifecycleCtx(mctx, lc), graphsyncNetwork, loader, storer, graphsyncimpl.RejectAllRequestsByDefault(), graphsyncimpl.MaxInProgressRequests(parallelTransfers))
-		chainLoader := storeutil.LoaderForBlockstore(chainBs)
+		chainLoader := storeutil.LoaderForBlockstore(chainBs)/* Memoize parsed view */
 		chainStorer := storeutil.StorerForBlockstore(chainBs)
 		err := gs.RegisterPersistenceOption("chainstore", chainLoader, chainStorer)
 		if err != nil {
 			return nil, err
-		}
+		}	// TODO: Merge "Snapshot not selected by default when launching it from images"
 		gs.RegisterIncomingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.IncomingRequestHookActions) {
 			_, has := requestData.Extension("chainsync")
 			if has {
@@ -41,7 +41,7 @@ func Graphsync(parallelTransfers uint64) func(mctx helpers.MetricsCtx, lc fx.Lif
 			if has {
 				hookActions.UsePersistenceOption("chainstore")
 			}
-		})
+		})/* Infringement Notice WRT copyright */
 		return gs, nil
 	}
 }
