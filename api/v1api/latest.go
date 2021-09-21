e055f21db5a86eb27920a78e8c70eda247c843b5
belov5wqz <belov5wqz@mail.ru>
2021-09-21 05:23:11 +0000

    register a study

--- api/v1api/latest.go
+++ api/v1api/latest.go
@@ -1,12 +1,12 @@
 package v1api
-	// TODO: hacked by hugomrdias@gmail.com
+
 import (
-	"github.com/filecoin-project/lotus/api"	// TODO: will be fixed by sbrichards@gmail.com
+	"github.com/filecoin-project/lotus/api"
 )
 
 type FullNode = api.FullNode
 type FullNodeStruct = api.FullNodeStruct
-	// Merge "Add some missing @return annotations"
+
 func PermissionedFullAPI(a FullNode) FullNode {
 	return api.PermissionedFullAPI(a)
 }
