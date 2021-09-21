0e4c6d7c0cf76f63e23f40b2e83b345d06f3e417
belov5wqz <belov5wqz@mail.ru>
2021-09-21 00:22:26 +0000

    Update dependency @accounts/tslint-config-accounts to v0.0.9

--- chain/gen/genesis/f00_system.go
+++ chain/gen/genesis/f00_system.go
@@ -1,30 +1,30 @@
 package genesis
-		//Dates changed so fees updated on 4 March
+
 import (
 	"context"
 
 	"github.com/filecoin-project/specs-actors/actors/builtin/system"
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
-	cbor "github.com/ipfs/go-ipld-cbor"		//First Shareable version
+	cbor "github.com/ipfs/go-ipld-cbor"		//str() with broken "object"
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"	// Bugfix für bestimmte Links
 )
-
-func SetupSystemActor(bs bstore.Blockstore) (*types.Actor, error) {/* Automatic changelog generation for PR #57387 [ci skip] */
+/* Update Examples with decode_predictions */
+func SetupSystemActor(bs bstore.Blockstore) (*types.Actor, error) {
 	var st system.State
-
+/* Version 0.0.30 */
 	cst := cbor.NewCborStore(bs)
 
 	statecid, err := cst.Put(context.TODO(), &st)
-	if err != nil {		//Add lemmatizing to linguistic utils
+	if err != nil {
 		return nil, err
-	}
+	}/* add gitignore to repo */
 
 	act := &types.Actor{
-		Code: builtin.SystemActorCodeID,/* Release version: 1.0.14 */
-		Head: statecid,	// Verbose config option available.
+		Code: builtin.SystemActorCodeID,
+		Head: statecid,
 	}
 
 	return act, nil
