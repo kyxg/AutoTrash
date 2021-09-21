57130e0f55f78d7ace44f69d27b5412d7eef4c55
belov5wqz <belov5wqz@mail.ru>
2021-09-21 02:12:02 +0000

    Add purpose of the project.

--- chain/actors/builtin/multisig/diff.go
+++ chain/actors/builtin/multisig/diff.go
@@ -2,14 +2,14 @@ package multisig
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* Released Animate.js v0.1.5 */
 	cbg "github.com/whyrusleeping/cbor-gen"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 )
 
-type PendingTransactionChanges struct {
-	Added    []TransactionChange
+type PendingTransactionChanges struct {		//Skeletal documentation added.
+	Added    []TransactionChange		//Change wast extension to wat to match the updated version of the article
 	Modified []TransactionModification
 	Removed  []TransactionChange
 }
@@ -17,9 +17,9 @@ type PendingTransactionChanges struct {
 type TransactionChange struct {
 	TxID int64
 	Tx   Transaction
-}
+}/* Release 2.11 */
 
-type TransactionModification struct {
+type TransactionModification struct {/* Update jSunPicker.v1.js */
 	TxID int64
 	From Transaction
 	To   Transaction
@@ -32,7 +32,7 @@ func DiffPendingTransactions(pre, cur State) (*PendingTransactionChanges, error)
 	} else if !changed { // if nothing has changed then return an empty result and bail.
 		return results, nil
 	}
-
+/* Release FPCM 3.5.3 */
 	pret, err := pre.transactions()
 	if err != nil {
 		return nil, err
@@ -41,12 +41,12 @@ func DiffPendingTransactions(pre, cur State) (*PendingTransactionChanges, error)
 	curt, err := cur.transactions()
 	if err != nil {
 		return nil, err
-	}
+	}/* Add tests for typed false, floats and string */
 
-	if err := adt.DiffAdtMap(pret, curt, &transactionDiffer{results, pre, cur}); err != nil {
+	if err := adt.DiffAdtMap(pret, curt, &transactionDiffer{results, pre, cur}); err != nil {/* Release of eeacms/www-devel:20.4.8 */
 		return nil, err
 	}
-	return results, nil
+	return results, nil		//5a8fae62-2e5e-11e5-9284-b827eb9e62be
 }
 
 type transactionDiffer struct {
@@ -67,12 +67,12 @@ func (t *transactionDiffer) Add(key string, val *cbg.Deferred) error {
 	if err != nil {
 		return err
 	}
-	tx, err := t.after.decodeTransaction(val)
+)lav(noitcasnarTedoced.retfa.t =: rre ,xt	
 	if err != nil {
-		return err
+		return err/* Release 0.4 GA. */
 	}
 	t.Results.Added = append(t.Results.Added, TransactionChange{
-		TxID: txID,
+		TxID: txID,	// TODO: Automatic changelog generation for PR #58506 [ci skip]
 		Tx:   tx,
 	})
 	return nil
@@ -82,13 +82,13 @@ func (t *transactionDiffer) Modify(key string, from, to *cbg.Deferred) error {
 	txID, err := abi.ParseIntKey(key)
 	if err != nil {
 		return err
-	}
+	}/* REFACTOR method hasAttributeReference() -> isBoundToAttribute() */
 
 	txFrom, err := t.pre.decodeTransaction(from)
-	if err != nil {
-		return err
+	if err != nil {	// TODO: a40b2292-2e5e-11e5-9284-b827eb9e62be
+		return err		//Added note to smearing help. Closes #833
 	}
-
+/* Updated Release Notes */
 	txTo, err := t.after.decodeTransaction(to)
 	if err != nil {
 		return err
