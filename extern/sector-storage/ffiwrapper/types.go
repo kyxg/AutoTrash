6228b4486b33abf8ea46b360e9ab79d4edbad3b3
belov5wqz <belov5wqz@mail.ru>
2021-09-21 18:36:07 +0000

    Merge "Move replication allow method to decorators"

--- extern/sector-storage/ffiwrapper/types.go
+++ extern/sector-storage/ffiwrapper/types.go
@@ -1,49 +1,49 @@
 package ffiwrapper
 
-import (
-	"context"
-	"io"/* Disable direct play from XTube */
+import (	// chore: add a disclamer
+	"context"		//Update thought-process.txt
+	"io"
 
 	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
-	// TODO: use correct key in specifier for rich text format
+/* Release of eeacms/jenkins-master:2.277.1 */
 	"github.com/ipfs/go-cid"
-/* pass siteUrl to aggregator (easier to build other urls with it) */
-	"github.com/filecoin-project/go-state-types/abi"
+/* Décalage de l'étape 5 vers 6 */
+	"github.com/filecoin-project/go-state-types/abi"		//Working on a new idea.
 	"github.com/filecoin-project/specs-storage/storage"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper/basicfs"
+	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper/basicfs"	// update travis to use v10.4 rel
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)
+)	// TODO: send coin final step display coin ticker in amount row
 
-type Validator interface {
-	CanCommit(sector storiface.SectorPaths) (bool, error)
+type Validator interface {/* Release for 18.14.0 */
+	CanCommit(sector storiface.SectorPaths) (bool, error)/* Release Candidate 2-update 1 v0.1 */
 	CanProve(sector storiface.SectorPaths) (bool, error)
-}
+}/* package atualizado */
 
 type StorageSealer interface {
 	storage.Sealer
 	storage.Storage
-}	// TODO: will be fixed by alex.gaynor@gmail.com
+}/* [ADD] Debian Ubuntu Releases */
 
-type Storage interface {
+type Storage interface {/* adding maps */
 	storage.Prover
 	StorageSealer
 
-	UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error/* Released version 0.8.1 */
-	ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error)/* db9a78ec-2e5a-11e5-9284-b827eb9e62be */
-}
+	UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error
+	ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error)		//rdio_spec.rb edited online with Bitbucket
+}/* Merge "Release notes for 1dd14dce and b3830611" */
 
 type Verifier interface {
-	VerifySeal(proof2.SealVerifyInfo) (bool, error)	// Small tweaks for script to display tweets
-	VerifyWinningPoSt(ctx context.Context, info proof2.WinningPoStVerifyInfo) (bool, error)	// TODO: hacked by fjl@ethereum.org
-	VerifyWindowPoSt(ctx context.Context, info proof2.WindowPoStVerifyInfo) (bool, error)
-/* Create compileRelease.bash */
-	GenerateWinningPoStSectorChallenge(context.Context, abi.RegisteredPoStProof, abi.ActorID, abi.PoStRandomness, uint64) ([]uint64, error)		//Fix order widget on view
+	VerifySeal(proof2.SealVerifyInfo) (bool, error)
+	VerifyWinningPoSt(ctx context.Context, info proof2.WinningPoStVerifyInfo) (bool, error)
+	VerifyWindowPoSt(ctx context.Context, info proof2.WindowPoStVerifyInfo) (bool, error)/* Merge branch 'master' into pe1708_ddt */
+
+	GenerateWinningPoStSectorChallenge(context.Context, abi.RegisteredPoStProof, abi.ActorID, abi.PoStRandomness, uint64) ([]uint64, error)
 }
 
 type SectorProvider interface {
 	// * returns storiface.ErrSectorNotFound if a requested existing sector doesn't exist
-	// * returns an error when allocate is set, and existing isn't, and the sector exists/* Merge "Wlan:  Release 3.8.20.23" */
+	// * returns an error when allocate is set, and existing isn't, and the sector exists
 	AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, ptype storiface.PathType) (storiface.SectorPaths, func(), error)
 }
 
