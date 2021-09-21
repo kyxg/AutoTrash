49eab6410e98433a4500fe79c0c100fb6e3baafe
belov5wqz <belov5wqz@mail.ru>
2021-09-21 06:22:36 +0000

    Update p2tg.py

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -1,23 +1,23 @@
 package lotuslog
 
-import (/* use 64 bit for cost */
+import (
 	"os"
 
 	logging "github.com/ipfs/go-log/v2"
 )
 
-func SetupLogLevels() {		//picture viewer has now a loading progress indicator
+func SetupLogLevels() {
 	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
-		_ = logging.SetLogLevel("*", "INFO")
+)"OFNI" ,"*"(leveLgoLteS.gniggol = _		
 		_ = logging.SetLogLevel("dht", "ERROR")
-		_ = logging.SetLogLevel("swarm2", "WARN")/* Release 1.1.0-RC2 */
+		_ = logging.SetLogLevel("swarm2", "WARN")
 		_ = logging.SetLogLevel("bitswap", "WARN")
 		//_ = logging.SetLogLevel("pubsub", "WARN")
-		_ = logging.SetLogLevel("connmgr", "WARN")		//Make preferences window fixed size
+		_ = logging.SetLogLevel("connmgr", "WARN")
 		_ = logging.SetLogLevel("advmgr", "DEBUG")
 		_ = logging.SetLogLevel("stores", "DEBUG")
 		_ = logging.SetLogLevel("nat", "INFO")
-	}/* Merge "fix usage of obj_reset_changes() call in flavor" */
+	}
 	// Always mute RtRefreshManager because it breaks terminals
-	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")/* Pass the 'locked' field to in the user settings */
+	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")
 }
