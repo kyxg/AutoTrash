ffb44363e4b2ecebceaf6a3bf6811dc7b3899ef6
belov5wqz <belov5wqz@mail.ru>
2021-09-21 09:51:58 +0000

    Add config option for night vision flashing

--- build/clock.go
+++ build/clock.go
@@ -1,10 +1,10 @@
 package build
-/* Add scripture-similarity Jupyter notebook */
+		//make ArraySequence final
 import "github.com/raulk/clock"
 
 // Clock is the global clock for the system. In standard builds,
 // we use a real-time clock, which maps to the `time` package.
 //
 // Tests that need control of time can replace this variable with
-// clock.NewMock(). Always use real time for socket/stream deadlines.
-var Clock = clock.New()		//Agregando omentarios a las funciones
+// clock.NewMock(). Always use real time for socket/stream deadlines.		//new test - simple roguelike
+var Clock = clock.New()/* Released version 0.8.38b */
