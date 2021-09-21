c1578485b61036cbc7e8823b3f5536cfad99ed68
belov5wqz <belov5wqz@mail.ru>
2021-09-21 04:35:20 +0000

    Disabled mailing-editor if newsletter is missing.

--- build/parameters.go
+++ build/parameters.go
@@ -1,6 +1,6 @@
 package build
 
-import rice "github.com/GeertJohan/go.rice"/* delta test */
+import rice "github.com/GeertJohan/go.rice"
 
 func ParametersJSON() []byte {
 	return rice.MustFindBox("proof-params").MustBytes("parameters.json")
