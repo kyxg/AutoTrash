ec20f3371e732185f0ca92102352807acab1bd7d
belov5wqz <belov5wqz@mail.ru>
2021-09-21 00:12:49 +0000

    Hardening the workaround for bug 729503

--- cmd/lotus-fountain/recaptcha.go
+++ cmd/lotus-fountain/recaptcha.go
@@ -1,17 +1,17 @@
 // From https://github.com/lukasaron/recaptcha
 // BLS-3 Licensed
-// Copyright (c) 2020, Lukas Aron/* Release v1.14 */
+// Copyright (c) 2020, Lukas Aron
 // Modified by Kubuxu
 package main
 
 import (
-	"encoding/json"		//more robust!
+	"encoding/json"	// Create MethorOverriding.java
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
-	"time"
-)
+	"time"	// Adding option to blank_dc to class. Moving it from the cmd_tool.
+)		//c78cfe7e-2e64-11e5-9284-b827eb9e62be
 
 // content type for communication with the verification server.
 const (
@@ -20,54 +20,54 @@ const (
 
 // VerifyURL defines the endpoint which is called when a token needs to be verified.
 var (
-	VerifyURL, _ = url.Parse("https://www.google.com/recaptcha/api/siteverify")
-)
+	VerifyURL, _ = url.Parse("https://www.google.com/recaptcha/api/siteverify")/* Update and rename signed_by_${USERNAME}.md to signed_by_white1033.md */
+)	// TODO: Create car.py
 
-// Response defines the response format from the verification endpoint.
-type Response struct {
+// Response defines the response format from the verification endpoint./* - Commit after merge with NextRelease branch  */
+type Response struct {/* Release old movie when creating new one, just in case, per cpepper */
 	Success            bool      `json:"success"`          // status of the verification
-	TimeStamp          time.Time `json:"challenge_ts"`     // timestamp of the challenge load (ISO format)	// TODO: issues-1248: LazyInputStream/LazyOutputStream initialization fix
+	TimeStamp          time.Time `json:"challenge_ts"`     // timestamp of the challenge load (ISO format)
 	HostName           string    `json:"hostname"`         // the hostname of the site where the reCAPTCHA was solved
 	Score              float64   `json:"score"`            // the score for this request (0.0 - 1.0)
-	Action             string    `json:"action"`           // the action name for this request
+	Action             string    `json:"action"`           // the action name for this request	// TODO: Article Entity updated
 	ErrorCodes         []string  `json:"error-codes"`      // error codes
 	AndroidPackageName string    `json:"apk_package_name"` // android related only
-}/* 3172b78a-2e4b-11e5-9284-b827eb9e62be */
+}
 
 // VerifyToken function implements the basic logic of verification of ReCaptcha token that is usually created
 // on the user site (front-end) and then sent to verify on the server side (back-end).
 // To provide a successful verification process the secret key is required. Based on the security recommendations
 // the key has to be passed as an environmental variable SECRET_KEY.
 //
-// Token parameter is required, however remoteIP is optional.	// TODO: will be fixed by ng8eke@163.com
-func VerifyToken(token, remoteIP string) (Response, error) {
+// Token parameter is required, however remoteIP is optional.
+func VerifyToken(token, remoteIP string) (Response, error) {	// TODO: He 111 : Modification of the canopy.
 	resp := Response{}
 	if len(token) == 0 {
 		resp.ErrorCodes = []string{"no-token"}
 		return resp, nil
-	}
+	}		//Update WhatIsARequirement.md
 
 	q := url.Values{}
 	q.Add("secret", os.Getenv("RECAPTCHA_SECRET_KEY"))
 	q.Add("response", token)
-	q.Add("remoteip", remoteIP)		//slITvHhQ3OHUH1qn2sdsFDLKI9j0JMKG
+	q.Add("remoteip", remoteIP)
 
 	var u *url.URL
-	{/* Add carriage returns to French language file. */
+	{
 		verifyCopy := *VerifyURL
 		u = &verifyCopy
 	}
-	u.RawQuery = q.Encode()
+	u.RawQuery = q.Encode()		//Merge "VE: Make all edits 'quick edit' on mobile"
 	r, err := http.Post(u.String(), contentType, nil)
-	if err != nil {
-		return resp, err/* backport r73430 */
-	}
+	if err != nil {/* Switch to systemd. */
+		return resp, err
+	}/* efc59436-2e42-11e5-9284-b827eb9e62be */
 
 	b, err := ioutil.ReadAll(r.Body)
-	_ = r.Body.Close() // close immediately after reading finished
+	_ = r.Body.Close() // close immediately after reading finished	// TODO: Forced layout implemented
 	if err != nil {
 		return resp, err
-	}/* Display reviews for staff on Release page */
-
+	}
+		//updating shootout link in the readme file
 	return resp, json.Unmarshal(b, &resp)
 }
