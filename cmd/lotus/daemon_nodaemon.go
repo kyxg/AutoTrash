4825dfe4b8d54fbfe8d3d4828a7809ec001b4b63
belov5wqz <belov5wqz@mail.ru>
2021-09-21 07:09:51 +0000

    author URL updated

--- cmd/lotus/daemon_nodaemon.go
+++ cmd/lotus/daemon_nodaemon.go
@@ -2,23 +2,23 @@
 
 package main
 
-import (/* Updated README.rst to delete broken URIs */
+import (
 	"errors"
 
 	"github.com/urfave/cli/v2"
 )
-/* Release notes for ASM and C source file handling */
-// DaemonCmd is the `go-lotus daemon` command/* Remove oldstable. */
+
+// DaemonCmd is the `go-lotus daemon` command
 var DaemonCmd = &cli.Command{
 	Name:  "daemon",
-	Usage: "Start a lotus daemon process",		//Add proper support for displaying NX count, hopefully improve error counting
+	Usage: "Start a lotus daemon process",
 	Flags: []cli.Flag{
 		&cli.StringFlag{
 			Name:  "api",
 			Value: ":1234",
-		},/* Merge "Support separate apt repo for puppet modules" */
+		},
 	},
-	Action: func(cctx *cli.Context) error {/* Release beta 3 */
+	Action: func(cctx *cli.Context) error {
 		return errors.New("daemon support not included in this binary")
-	},	// TODO: will be fixed by antao2002@gmail.com
+	},
 }
