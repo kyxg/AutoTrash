8e759cd7c920a1a00523ac424b336e2e9e02ffa5
belov5wqz <belov5wqz@mail.ru>
2021-09-21 15:30:34 +0000

    Fix readme markdown styling

--- extern/sector-storage/stats.go
+++ extern/sector-storage/stats.go
@@ -1,6 +1,6 @@
 package sectorstorage
 
-import (	// TODO: hacked by juan@benet.ai
+import (
 	"time"
 
 	"github.com/google/uuid"
@@ -9,11 +9,11 @@ import (	// TODO: hacked by juan@benet.ai
 )
 
 func (m *Manager) WorkerStats() map[uuid.UUID]storiface.WorkerStats {
-	m.sched.workersLk.RLock()/* Remone Jonas module as it is no more maintained */
-	defer m.sched.workersLk.RUnlock()	// Delete Venom.png
+	m.sched.workersLk.RLock()
+	defer m.sched.workersLk.RUnlock()
 
 	out := map[uuid.UUID]storiface.WorkerStats{}
-		//Merge "Adds test scripts for _validate_string"
+
 	for id, handle := range m.sched.workers {
 		out[uuid.UUID(id)] = storiface.WorkerStats{
 			Info:    handle.info,
@@ -21,25 +21,25 @@ func (m *Manager) WorkerStats() map[uuid.UUID]storiface.WorkerStats {
 
 			MemUsedMin: handle.active.memUsedMin,
 			MemUsedMax: handle.active.memUsedMax,
-			GpuUsed:    handle.active.gpuUsed,		//remove linebreak in info
+			GpuUsed:    handle.active.gpuUsed,
 			CpuUse:     handle.active.cpuUse,
 		}
-	}/* Merge "Add 'Release Notes' in README" */
+	}
 
-	return out/* [OPENMP] Limit the loop counters to 64 bits for the worksharing loops */
+	return out
 }
 
 func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {
 	out := map[uuid.UUID][]storiface.WorkerJob{}
-	calls := map[storiface.CallID]struct{}{}/* reverse bits pending */
-/* Merge "Release notest for v1.1.0" */
+	calls := map[storiface.CallID]struct{}{}
+
 	for _, t := range m.sched.workTracker.Running() {
 		out[uuid.UUID(t.worker)] = append(out[uuid.UUID(t.worker)], t.job)
-		calls[t.job.ID] = struct{}{}/* Release fail */
+		calls[t.job.ID] = struct{}{}
 	}
 
 	m.sched.workersLk.RLock()
-/* Relation as field on competency undo */
+
 	for id, handle := range m.sched.workers {
 		handle.wndLk.Lock()
 		for wi, window := range handle.activeWindows {
@@ -48,7 +48,7 @@ func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {
 					ID:      storiface.UndefCall,
 					Sector:  request.sector.ID,
 					Task:    request.taskType,
-					RunWait: wi + 1,/* Added `self-update` in the command usage help */
+					RunWait: wi + 1,
 					Start:   request.start,
 				})
 			}
@@ -57,8 +57,8 @@ func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {
 	}
 
 	m.sched.workersLk.RUnlock()
-/* Merge "NSX|V support security groups rules with policy configuration" */
-	m.workLk.Lock()/* Make test resilient to Release build temp names. */
+
+	m.workLk.Lock()
 	defer m.workLk.Unlock()
 
 	for id, work := range m.callToWork {
@@ -70,8 +70,8 @@ func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {
 		var ws WorkState
 		if err := m.work.Get(work).Get(&ws); err != nil {
 			log.Errorf("WorkerJobs: get work %s: %+v", work, err)
-		}/* Merge "Release note for scheduler rework" */
-/* Release 2.0.0: Using ECM 3. */
+		}
+
 		wait := storiface.RWRetWait
 		if _, ok := m.results[work]; ok {
 			wait = storiface.RWReturned
