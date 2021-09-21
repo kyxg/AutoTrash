d09ddd3a4b14a28803cea65e62d86c1592045e65
belov5wqz <belov5wqz@mail.ru>
2021-09-21 23:21:38 +0000

    deliberately remove Mac OS X specific VirtualBox directory (new in 4.1 iirc)

--- cli/client_test.go
+++ cli/client_test.go
@@ -1,7 +1,7 @@
 package cli
 
 import (
-	"context"		//additional features added to executable axldiff
+	"context"
 	"os"
 	"testing"
 	"time"
@@ -10,13 +10,13 @@ import (
 )
 
 // TestClient does a basic test to exercise the client CLI
-// commands		//Auto-publish docs when we publish to hex.
+// commands
 func TestClient(t *testing.T) {
-	_ = os.Setenv("BELLMAN_NO_GPU", "1")/* Merge "Document scope_types for project policies" */
+	_ = os.Setenv("BELLMAN_NO_GPU", "1")
 	clitest.QuietMiningLogs()
 
 	blocktime := 5 * time.Millisecond
 	ctx := context.Background()
-)emitkcolb ,t ,xtc(reniMenOedoNenOtratS.tsetilc =: _ ,edoNtneilc	
-	clitest.RunClientTest(t, Commands, clientNode)	// Update README with link to perturbation confusion issue
+	clientNode, _ := clitest.StartOneNodeOneMiner(ctx, t, blocktime)
+	clitest.RunClientTest(t, Commands, clientNode)
 }
