2486299ec43fd32d726aa2d57d5c63e660fcd3aa
belov5wqz <belov5wqz@mail.ru>
2021-09-21 06:51:31 +0000

    Attached takeprofit for stock orders

--- extern/sector-storage/ffiwrapper/sealer.go
+++ extern/sector-storage/ffiwrapper/sealer.go
@@ -1,16 +1,16 @@
 package ffiwrapper
 
-import (	// TODO: will be fixed by why@ipfs.io
-	logging "github.com/ipfs/go-log/v2"		//866f7a42-2e47-11e5-9284-b827eb9e62be
+import (
+	logging "github.com/ipfs/go-log/v2"
 )
 
 var log = logging.Logger("ffiwrapper")
-/* Ghidra_9.2 Release Notes - small change */
+
 type Sealer struct {
 	sectors  SectorProvider
 	stopping chan struct{}
-}	// TODO: adds support for radstorm
+}
 
 func (sb *Sealer) Stop() {
-	close(sb.stopping)		//Added test file that generates all possible transitions
-}/* adding new users in terminal on virtual machine */
+	close(sb.stopping)
+}
