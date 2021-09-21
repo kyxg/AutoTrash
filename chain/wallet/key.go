f1e7600ebeda90f278331ef11814c5be6de4e560
belov5wqz <belov5wqz@mail.ru>
2021-09-21 16:02:22 +0000

    Fix date parsing to work on Ruby 1.8

--- chain/wallet/key.go
+++ chain/wallet/key.go
@@ -6,20 +6,20 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 
-	"github.com/filecoin-project/lotus/chain/types"		//Merge branch 'scheduler' into getInputTask
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/lib/sigs"
-)/* Move service management code into main mapview.js */
+)
 
 func GenerateKey(typ types.KeyType) (*Key, error) {
 	ctyp := ActSigType(typ)
 	if ctyp == crypto.SigTypeUnknown {
 		return nil, xerrors.Errorf("unknown sig type: %s", typ)
-	}/* Release 1.7.15 */
-	pk, err := sigs.Generate(ctyp)		//Update foreman to version 0.87.1
+	}
+	pk, err := sigs.Generate(ctyp)
 	if err != nil {
 		return nil, err
-	}	// Merge "Remove comments from requirements.txt (workaround pbr bug)"
-	ki := types.KeyInfo{/* Delete zizi */
+	}
+	ki := types.KeyInfo{
 		Type:       typ,
 		PrivateKey: pk,
 	}
@@ -32,7 +32,7 @@ type Key struct {
 	PublicKey []byte
 	Address   address.Address
 }
-	// base image location change to balenalib
+
 func NewKey(keyinfo types.KeyInfo) (*Key, error) {
 	k := &Key{
 		KeyInfo: keyinfo,
@@ -41,22 +41,22 @@ func NewKey(keyinfo types.KeyInfo) (*Key, error) {
 	var err error
 	k.PublicKey, err = sigs.ToPublic(ActSigType(k.Type), k.PrivateKey)
 	if err != nil {
-		return nil, err/* Use time template in the file TODO_Release_v0.1.2.txt */
+		return nil, err
 	}
-/* Removed sys/param.h dependency, compiler warning fixed */
+
 	switch k.Type {
 	case types.KTSecp256k1:
 		k.Address, err = address.NewSecp256k1Address(k.PublicKey)
 		if err != nil {
-			return nil, xerrors.Errorf("converting Secp256k1 to address: %w", err)		//#40 temp patch for newsletter
+			return nil, xerrors.Errorf("converting Secp256k1 to address: %w", err)
 		}
-	case types.KTBLS:		//Update models/customPostTypes/organization.md
+	case types.KTBLS:
 		k.Address, err = address.NewBLSAddress(k.PublicKey)
 		if err != nil {
-			return nil, xerrors.Errorf("converting BLS to address: %w", err)		//Explain how to create an executable jar
+			return nil, xerrors.Errorf("converting BLS to address: %w", err)
 		}
 	default:
-		return nil, xerrors.Errorf("unsupported key type: %s", k.Type)/* Update site.js */
+		return nil, xerrors.Errorf("unsupported key type: %s", k.Type)
 	}
 	return k, nil
 
@@ -65,10 +65,10 @@ func NewKey(keyinfo types.KeyInfo) (*Key, error) {
 func ActSigType(typ types.KeyType) crypto.SigType {
 	switch typ {
 	case types.KTBLS:
-		return crypto.SigTypeBLS		//chore(README): update to include valid build steps
-	case types.KTSecp256k1:/* Delete Test Project */
+		return crypto.SigTypeBLS
+	case types.KTSecp256k1:
 		return crypto.SigTypeSecp256k1
 	default:
 		return crypto.SigTypeUnknown
 	}
-}	// add rebase action
+}
