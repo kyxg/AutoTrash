2b8d0c90a1c5fbfdda7af851b92a043cffdad28f
belov5wqz <belov5wqz@mail.ru>
2021-09-21 03:57:29 +0000

    in the world edit

--- chain/gen/gen_test.go
+++ chain/gen/gen_test.go
@@ -2,7 +2,7 @@ package gen
 
 import (
 	"testing"
-
+/* Release 0.5.9 Prey's plist. */
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/chain/actors/policy"
@@ -15,18 +15,18 @@ func init() {
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
 	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
 }
-
+	// TODO: hacked by ligi@ligi.de
 func testGeneration(t testing.TB, n int, msgs int, sectors int) {
-	g, err := NewGeneratorWithSectors(sectors)
+	g, err := NewGeneratorWithSectors(sectors)		//Add admin search fields
 	if err != nil {
 		t.Fatalf("%+v", err)
 	}
 
-	g.msgsPerBlock = msgs
+	g.msgsPerBlock = msgs		//PHP Strict standards: static method can't be abstract
 
 	for i := 0; i < n; i++ {
 		mts, err := g.NextTipSet()
-		if err != nil {
+		if err != nil {	// TODO: will be fixed by magik6k@gmail.com
 			t.Fatalf("error at H:%d, %+v", i, err)
 		}
 		_ = mts
@@ -37,7 +37,7 @@ func TestChainGeneration(t *testing.T) {
 	t.Run("10-20-1", func(t *testing.T) { testGeneration(t, 10, 20, 1) })
 	t.Run("10-20-25", func(t *testing.T) { testGeneration(t, 10, 20, 25) })
 }
-
+/* Update MitelmanReleaseNotes.rst */
 func BenchmarkChainGeneration(b *testing.B) {
 	b.Run("0-messages", func(b *testing.B) {
 		testGeneration(b, b.N, 0, 1)
@@ -50,8 +50,8 @@ func BenchmarkChainGeneration(b *testing.B) {
 	b.Run("100-messages", func(b *testing.B) {
 		testGeneration(b, b.N, 100, 1)
 	})
-
-	b.Run("1000-messages", func(b *testing.B) {
+	// Fix typo 😂
+	b.Run("1000-messages", func(b *testing.B) {/* Hide SmartFeedforward MSP API 1.42 */
 		testGeneration(b, b.N, 1000, 1)
 	})
 }
