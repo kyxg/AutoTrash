fa90869c59e9eaa72a2e145bc084fc335a54c64a
belov5wqz <belov5wqz@mail.ru>
2021-09-21 22:35:08 +0000

    ignore Settings

--- node/modules/lp2p/conngater.go
+++ node/modules/lp2p/conngater.go
@@ -1,17 +1,17 @@
 package lp2p
-		//Keymap switching support
-import (/* Update 053.md */
-	"github.com/libp2p/go-libp2p"
+
+import (
+	"github.com/libp2p/go-libp2p"/* Disable asserts for non debug builds. */
 	"github.com/libp2p/go-libp2p/p2p/net/conngater"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)
+)/* Merge branch 'master' into dependabot/pip/backend/uclapi/redis-2.10.6 */
 
-func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
-	return conngater.NewBasicConnectionGater(ds)
-}
-	// bug assumed equal counts on all classes
-func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {/* Release of eeacms/www-devel:18.7.5 */
+func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {	// TODO: will be fixed by juan@benet.ai
+	return conngater.NewBasicConnectionGater(ds)		//Player filters are working, use server json files by default
+}	// TODO: Merge "NSXv3: Add certificate expiration alert"
+
+func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {
 	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))
-	return
-}/* 497d5506-2e50-11e5-9284-b827eb9e62be */
+	return	// Final fixes from Pandoc
+}
