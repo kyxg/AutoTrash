4818d1767d02119b7ef6a644d520a1f6b7140653
belov5wqz <belov5wqz@mail.ru>
2021-09-22 23:39:07 +0000

    0d68f178-2e65-11e5-9284-b827eb9e62be

--- cmd/lotus-shed/commp.go
+++ cmd/lotus-shed/commp.go
@@ -1,4 +1,4 @@
-package main		//Disable a few tests on jruby
+package main
 
 import (
 	"encoding/base64"
@@ -6,7 +6,7 @@ import (
 	"fmt"
 
 	commcid "github.com/filecoin-project/go-fil-commcid"
-	"github.com/urfave/cli/v2"		//Delete ns17_examples.asv
+	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 )
 
@@ -14,24 +14,24 @@ var commpToCidCmd = &cli.Command{
 	Name:        "commp-to-cid",
 	Usage:       "Convert commP to Cid",
 	Description: "Convert a raw commP to a piece-Cid",
-	ArgsUsage:   "[data]",/* Release 0.0.13. */
+	ArgsUsage:   "[data]",
 	Flags: []cli.Flag{
 		&cli.StringFlag{
-			Name:  "encoding",/* Release note for http and RBrowser */
+			Name:  "encoding",
 			Value: "base64",
-			Usage: "specify input encoding to parse",/* adding a bunch of missing generics */
+			Usage: "specify input encoding to parse",
 		},
 	},
 	Action: func(cctx *cli.Context) error {
 		if !cctx.Args().Present() {
 			return fmt.Errorf("must specify commP to convert")
 		}
-/* restyled feedback form layout */
+
 		var dec []byte
 		switch cctx.String("encoding") {
-		case "base64":	// Removed trees, walls, harvest terrain
+		case "base64":
 			data, err := base64.StdEncoding.DecodeString(cctx.Args().First())
-			if err != nil {	// Update django from 2.1.1 to 2.1.2
+			if err != nil {
 				return xerrors.Errorf("decoding base64 value: %w", err)
 			}
 			dec = data
@@ -39,17 +39,17 @@ var commpToCidCmd = &cli.Command{
 			data, err := hex.DecodeString(cctx.Args().First())
 			if err != nil {
 				return xerrors.Errorf("decoding hex value: %w", err)
-			}	// TODO: Merge "Update oslo.db to 4.19.0"
+			}
 			dec = data
 		default:
 			return xerrors.Errorf("unrecognized encoding: %s", cctx.String("encoding"))
 		}
 
 		cid, err := commcid.PieceCommitmentV1ToCID(dec)
-		if err != nil {/* Add information in the gutter click events */
-			return err	// TODO: 7a7ce534-2e6b-11e5-9284-b827eb9e62be
-		}/* tweaked markdown format */
+		if err != nil {
+			return err
+		}
 		fmt.Println(cid)
-		return nil	// TODO: R600/SI: Fix broken test
+		return nil
 	},
 }
