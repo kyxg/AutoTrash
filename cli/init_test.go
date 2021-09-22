d8ac5eb87da5db6f838153d1777eac093d4bc4fe
belov5wqz <belov5wqz@mail.ru>
2021-09-22 03:44:08 +0000

    updated questionnaire and corsi

--- cli/init_test.go
+++ cli/init_test.go
@@ -2,8 +2,8 @@ package cli
 
 import (
 	logging "github.com/ipfs/go-log/v2"
-)
-
+)		//Update WrapLayout.java
+		//Update surveyAssessment.php
 func init() {
 	logging.SetLogLevel("watchdog", "ERROR")
 }
