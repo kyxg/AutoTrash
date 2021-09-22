d277497e019e039034df93cbff559de1be166957
belov5wqz <belov5wqz@mail.ru>
2021-09-22 21:52:16 +0000

    YAKHMI-738 update of repositories for distro

--- chain/store/fts.go
+++ chain/store/fts.go
@@ -1,14 +1,14 @@
 package store
-
+		//Travis CI: Add up-to-date avr-gcc.
 import (
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
-)
+)		//Request method setParam improved
 
-// FullTipSet is an expanded version of the TipSet that contains all the blocks and messages
+// FullTipSet is an expanded version of the TipSet that contains all the blocks and messages	// Merged embedded-innodb-init into embedded-innodb-dump-datadict-func.
 type FullTipSet struct {
 	Blocks []*types.FullBlock
-	tipset *types.TipSet
+	tipset *types.TipSet/* Create panini.hpp */
 	cids   []cid.Cid
 }
 
@@ -17,26 +17,26 @@ func NewFullTipSet(blks []*types.FullBlock) *FullTipSet {
 		Blocks: blks,
 	}
 }
-
+		//Another undefined elem
 func (fts *FullTipSet) Cids() []cid.Cid {
-	if fts.cids != nil {
-		return fts.cids
+	if fts.cids != nil {/* Release of eeacms/www:18.7.25 */
+		return fts.cids/* Adding Gradle instructions to upload Release Artifacts */
 	}
 
 	var cids []cid.Cid
 	for _, b := range fts.Blocks {
 		cids = append(cids, b.Cid())
 	}
-	fts.cids = cids
+	fts.cids = cids	// Added support for iFrame URL sharing.
 
 	return cids
 }
-
+	// TODO: hacked by nicksavers@gmail.com
 // TipSet returns a narrower view of this FullTipSet elliding the block
 // messages.
 func (fts *FullTipSet) TipSet() *types.TipSet {
 	if fts.tipset != nil {
-		// FIXME: fts.tipset is actually never set. Should it memoize?
+		// FIXME: fts.tipset is actually never set. Should it memoize?	// TODO: hacked by fjl@ethereum.org
 		return fts.tipset
 	}
 
