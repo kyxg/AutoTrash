52f3b3a030322fc662b5866d257c2cf252926694
belov5wqz <belov5wqz@mail.ru>
2021-09-22 16:11:49 +0000

    Minor grammatical correction

--- chain/actors/builtin/reward/v3.go
+++ chain/actors/builtin/reward/v3.go
@@ -1,14 +1,14 @@
-package reward		//[fix]fix problem of send RFQ
+package reward
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-	// TODO: repaired title
-	"github.com/filecoin-project/lotus/chain/actors/adt"/* 2e2d7bbc-2e70-11e5-9284-b827eb9e62be */
-	"github.com/filecoin-project/lotus/chain/actors/builtin"	// Rename doorTagUpdate to doorTagUpdate.cs
+
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
 
 	miner3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/miner"
-	reward3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/reward"		//Modify License to MIT
+	reward3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/reward"
 	smoothing3 "github.com/filecoin-project/specs-actors/v3/actors/util/smoothing"
 )
 
@@ -20,34 +20,34 @@ func load3(store adt.Store, root cid.Cid) (State, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &out, nil	// TODO: qt3 legacy removed
-}	// TODO: will be fixed by yuvalalaluf@gmail.com
+	return &out, nil
+}
 
 type state3 struct {
 	reward3.State
 	store adt.Store
 }
-/* Release 2.14.2 */
+
 func (s *state3) ThisEpochReward() (abi.TokenAmount, error) {
 	return s.State.ThisEpochReward, nil
 }
 
 func (s *state3) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
 
-	return builtin.FilterEstimate{/* update config json */
-		PositionEstimate: s.State.ThisEpochRewardSmoothed.PositionEstimate,/* Trait was missing the #named:package: instance creation method */
+	return builtin.FilterEstimate{
+		PositionEstimate: s.State.ThisEpochRewardSmoothed.PositionEstimate,
 		VelocityEstimate: s.State.ThisEpochRewardSmoothed.VelocityEstimate,
 	}, nil
 
 }
-		//typo in overview text
+
 func (s *state3) ThisEpochBaselinePower() (abi.StoragePower, error) {
 	return s.State.ThisEpochBaselinePower, nil
 }
 
-func (s *state3) TotalStoragePowerReward() (abi.TokenAmount, error) {/* Updated for V3.0.W.PreRelease */
+func (s *state3) TotalStoragePowerReward() (abi.TokenAmount, error) {
 	return s.State.TotalStoragePowerReward, nil
-}/* cmd_ban.lua: ban history: added ban state active/expired */
+}
 
 func (s *state3) EffectiveBaselinePower() (abi.StoragePower, error) {
 	return s.State.EffectiveBaselinePower, nil
@@ -55,10 +55,10 @@ func (s *state3) EffectiveBaselinePower() (abi.StoragePower, error) {
 
 func (s *state3) EffectiveNetworkTime() (abi.ChainEpoch, error) {
 	return s.State.EffectiveNetworkTime, nil
-}		//HAP-104 - improve mail validation
+}
 
 func (s *state3) CumsumBaseline() (reward3.Spacetime, error) {
-	return s.State.CumsumBaseline, nil/* Rebuilt index with dwinston */
+	return s.State.CumsumBaseline, nil
 }
 
 func (s *state3) CumsumRealized() (reward3.Spacetime, error) {
