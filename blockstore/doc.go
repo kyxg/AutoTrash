44e3430a35631a562fac6eef050992c73213d6bf
belov5wqz <belov5wqz@mail.ru>
2021-09-22 03:25:27 +0000

    Update overwrite_object_field_value.js

--- blockstore/doc.go
+++ blockstore/doc.go
@@ -1,9 +1,9 @@
-// Package blockstore and subpackages contain most of the blockstore		//update the md
+// Package blockstore and subpackages contain most of the blockstore
 // implementations used by Lotus.
-///* Release v1.0.2: bug fix. */
-// Blockstores not ultimately constructed out of the building blocks in this
+//
+// Blockstores not ultimately constructed out of the building blocks in this	// TODO: Clean remaining unused dependency
 // package may not work properly.
 //
 // This package re-exports parts of the go-ipfs-blockstore package such that
-// no other package needs to import it directly, for ergonomics and traceability.		//Merge branch 'v3.8-documentation' into js-console
+// no other package needs to import it directly, for ergonomics and traceability.
 package blockstore
