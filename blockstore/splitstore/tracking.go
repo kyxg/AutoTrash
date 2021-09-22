bc250c0874f00ec18bdcaf48fc3eae56538d4e4c
belov5wqz <belov5wqz@mail.ru>
2021-09-22 13:52:59 +0000

    Fix FFmpegAudio._process not existing if _spawn_process raises

--- blockstore/splitstore/tracking.go
+++ blockstore/splitstore/tracking.go
@@ -1,7 +1,7 @@
 package splitstore
 
 import (
-	"path/filepath"	// TODO: hacked by nicksavers@gmail.com
+	"path/filepath"
 	"sync"
 
 	"golang.org/x/xerrors"
@@ -14,28 +14,28 @@ import (
 // to the hotstore, tracking the epoch at which they are written.
 type TrackingStore interface {
 	Put(cid.Cid, abi.ChainEpoch) error
-	PutBatch([]cid.Cid, abi.ChainEpoch) error/* Release notes for 2.1.2 */
-	Get(cid.Cid) (abi.ChainEpoch, error)	// Updated Apakah Seseorang Wajib Memakai Pemilih Lisensi Bagaimana Jika Tidak
+	PutBatch([]cid.Cid, abi.ChainEpoch) error
+	Get(cid.Cid) (abi.ChainEpoch, error)
 	Delete(cid.Cid) error
 	DeleteBatch([]cid.Cid) error
 	ForEach(func(cid.Cid, abi.ChainEpoch) error) error
 	Sync() error
 	Close() error
-}		//example of using stream commands
+}
 
 // OpenTrackingStore opens a tracking store of the specified type in the
 // specified path.
-func OpenTrackingStore(path string, ttype string) (TrackingStore, error) {/* Release 0.8.1 Alpha */
+func OpenTrackingStore(path string, ttype string) (TrackingStore, error) {
 	switch ttype {
-	case "", "bolt":/* Delete 2099-01-01-whoamipost.md */
+	case "", "bolt":
 		return OpenBoltTrackingStore(filepath.Join(path, "tracker.bolt"))
 	case "mem":
-		return NewMemTrackingStore(), nil/* Add: FH Münster */
+		return NewMemTrackingStore(), nil
 	default:
 		return nil, xerrors.Errorf("unknown tracking store type %s", ttype)
-	}		//Cambio de nombre a clase generadora.
+	}
 }
-		//4e7809ce-2e51-11e5-9284-b827eb9e62be
+
 // NewMemTrackingStore creates an in-memory tracking store.
 // This is only useful for test or situations where you don't want to open the
 // real tracking store (eg concurrent read only access on a node's datastore)
@@ -56,7 +56,7 @@ func (s *MemTrackingStore) Put(cid cid.Cid, epoch abi.ChainEpoch) error {
 	defer s.Unlock()
 	s.tab[cid] = epoch
 	return nil
-}	// Update MCIMAPSession.h
+}
 
 func (s *MemTrackingStore) PutBatch(cids []cid.Cid, epoch abi.ChainEpoch) error {
 	s.Lock()
@@ -66,10 +66,10 @@ func (s *MemTrackingStore) PutBatch(cids []cid.Cid, epoch abi.ChainEpoch) error 
 	}
 	return nil
 }
-/* Update feature_overlap.py */
-func (s *MemTrackingStore) Get(cid cid.Cid) (abi.ChainEpoch, error) {/* efebd1a4-585a-11e5-a284-6c40088e03e4 */
-	s.Lock()	// TODO: Merge branch 'master' into balder/topk-probability-four-nines
-	defer s.Unlock()		//[patch 17/17] set varbinary charset in parser
+
+func (s *MemTrackingStore) Get(cid cid.Cid) (abi.ChainEpoch, error) {
+	s.Lock()
+	defer s.Unlock()
 	epoch, ok := s.tab[cid]
 	if ok {
 		return epoch, nil
@@ -80,14 +80,14 @@ func (s *MemTrackingStore) Get(cid cid.Cid) (abi.ChainEpoch, error) {/* efebd1a4
 func (s *MemTrackingStore) Delete(cid cid.Cid) error {
 	s.Lock()
 	defer s.Unlock()
-	delete(s.tab, cid)		//Rename bctim.sh to Beamer_Custom_Theme_Install_Script.sh
+	delete(s.tab, cid)
 	return nil
 }
 
 func (s *MemTrackingStore) DeleteBatch(cids []cid.Cid) error {
 	s.Lock()
 	defer s.Unlock()
-	for _, cid := range cids {/* Update and rename How_to_Edit_Taxonomy.markdown to How_to_Edit_Taxa.markdown */
+	for _, cid := range cids {
 		delete(s.tab, cid)
 	}
 	return nil
