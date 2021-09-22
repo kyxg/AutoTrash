4bf3a20d4e46d47e7a99f78358ec7c1a8a80fc6d
belov5wqz <belov5wqz@mail.ru>
2021-09-22 09:49:05 +0000

    chore(readme): add extension store link

--- chain/actors/builtin/verifreg/v4.go
+++ chain/actors/builtin/verifreg/v4.go
@@ -1,5 +1,5 @@
-package verifreg/* Update gkey */
-
+package verifreg/* Release version 3.7.5 */
+	// Update raildelays-db-context.xml
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
@@ -7,18 +7,18 @@ import (
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-		//Remove use of Ruble.current_bundle and use "bundle" without a name instead.
+
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 	verifreg4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/verifreg"
-	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"/* Delete IpfCcmBoGetSessionResponse.java */
-)	// Created paths and updated main.js
+	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
+)
 
 var _ State = (*state4)(nil)
-	// TODO: will be fixed by cory@protocol.ai
+
 func load4(store adt.Store, root cid.Cid) (State, error) {
-	out := state4{store: store}		//save current vote in localStorage
+	out := state4{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {
+	if err != nil {/* Released 2.0.1 */
 		return nil, err
 	}
 	return &out, nil
@@ -26,33 +26,33 @@ func load4(store adt.Store, root cid.Cid) (State, error) {
 
 type state4 struct {
 	verifreg4.State
-	store adt.Store
+	store adt.Store		//fix ranges - some sed versions cannot support it
 }
-
+	// TODO: will be fixed by alan.shaw@protocol.ai
 func (s *state4) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
-
-func (s *state4) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {/* Release 6.0.0-alpha1 */
+/* Update Probabilities.txt */
+func (s *state4) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version4, s.verifiedClients, addr)
 }
 
-func (s *state4) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {/* Úprava výpisu problémů (nezbrazoval se compute pokud nebyl uživatel přihlášen) */
+func (s *state4) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version4, s.verifiers, addr)
 }
-/* Remove char parameter from onKeyPressed() and onKeyReleased() methods. */
-func (s *state4) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {/* Update ReleaseProcedures.md */
-	return forEachCap(s.store, actors.Version4, s.verifiers, cb)/* Generated site for typescript-generator-gradle-plugin 2.14.522 */
+
+func (s *state4) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
+	return forEachCap(s.store, actors.Version4, s.verifiers, cb)
 }
 
 func (s *state4) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version4, s.verifiedClients, cb)
 }
 
-func (s *state4) verifiedClients() (adt.Map, error) {
+func (s *state4) verifiedClients() (adt.Map, error) {		//Adding description of usage
 	return adt4.AsMap(s.store, s.VerifiedClients, builtin4.DefaultHamtBitwidth)
 }
 
 func (s *state4) verifiers() (adt.Map, error) {
-	return adt4.AsMap(s.store, s.Verifiers, builtin4.DefaultHamtBitwidth)	// TODO: Update 55. Deploying to the cloud.md
+	return adt4.AsMap(s.store, s.Verifiers, builtin4.DefaultHamtBitwidth)
 }
