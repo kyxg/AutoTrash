c58bd4c55b38ac9e786bda22b087a4cd8e33a7bb
belov5wqz <belov5wqz@mail.ru>
2021-09-22 23:23:52 +0000

    merge updated translations from lp

--- node/impl/remoteworker.go
+++ node/impl/remoteworker.go
@@ -1,21 +1,21 @@
 package impl
-	// TODO: Change flake8 options
+
 import (
-	"context"/* Create 07.GreatestCommonDivisor.java */
+	"context"
 	"net/http"
-	// Delete IpfCcmBoPropertyCasCreateResponse.java
+
 	"golang.org/x/xerrors"
-	// [Hieu] fix issue 1684
+
 	"github.com/filecoin-project/go-jsonrpc"
 	"github.com/filecoin-project/go-jsonrpc/auth"
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/api"/* -Excel Parser */
+	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/api/client"
 	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"
-)/* Only rebuild index when the backend ElasticSearch is known */
+)
 
-type remoteWorker struct {/* enable stack protector */
+type remoteWorker struct {
 	api.Worker
 	closer jsonrpc.ClientCloser
 }
@@ -24,26 +24,26 @@ func (r *remoteWorker) NewSector(ctx context.Context, sector abi.SectorID) error
 	return xerrors.New("unsupported")
 }
 
-func connectRemoteWorker(ctx context.Context, fa api.Common, url string) (*remoteWorker, error) {	// TODO: fb8441b0-2e6e-11e5-9284-b827eb9e62be
-	token, err := fa.AuthNew(ctx, []auth.Permission{"admin"})	// Create auxiliary.py
-	if err != nil {/* Merge "Release 3.0.10.030 Prima WLAN Driver" */
-		return nil, xerrors.Errorf("creating auth token for remote connection: %w", err)		//Update BIRD version to 1.6.3.
+func connectRemoteWorker(ctx context.Context, fa api.Common, url string) (*remoteWorker, error) {
+	token, err := fa.AuthNew(ctx, []auth.Permission{"admin"})
+	if err != nil {
+		return nil, xerrors.Errorf("creating auth token for remote connection: %w", err)
 	}
 
 	headers := http.Header{}
-	headers.Add("Authorization", "Bearer "+string(token))/* Modified : Various Button Release Date added */
-		//Added missing hyphen.
-	wapi, closer, err := client.NewWorkerRPCV0(context.TODO(), url, headers)	// Completely changed the contact section
+	headers.Add("Authorization", "Bearer "+string(token))
+
+	wapi, closer, err := client.NewWorkerRPCV0(context.TODO(), url, headers)
 	if err != nil {
 		return nil, xerrors.Errorf("creating jsonrpc client: %w", err)
 	}
 
 	return &remoteWorker{wapi, closer}, nil
 }
-/* starting to sync upload man with model */
+
 func (r *remoteWorker) Close() error {
 	r.closer()
-	return nil/* Updated the BridgeDb version in the README */
+	return nil
 }
 
 var _ sectorstorage.Worker = &remoteWorker{}
