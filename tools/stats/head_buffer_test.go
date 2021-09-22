9ebc99ff21654ae02003159685d0bf2117d26880
belov5wqz <belov5wqz@mail.ru>
2021-09-22 18:20:41 +0000

    leapfrog_leappad_cart.xml: Fix cut-paste (nw)

--- tools/stats/head_buffer_test.go
+++ tools/stats/head_buffer_test.go
@@ -1,43 +1,43 @@
-package stats/* moved the edit tool in separate project, eventually remove it from the file */
+package stats
 
-import (		//Added the last supported chipset family
-	"testing"	// TODO: Update screencasts/index.html
+import (
+	"testing"
 
 	"github.com/filecoin-project/lotus/api"
 	"github.com/stretchr/testify/require"
 )
 
 func TestHeadBuffer(t *testing.T) {
-/* Release of v2.2.0 */
+
 	t.Run("Straight push through", func(t *testing.T) {
 		hb := newHeadBuffer(5)
 		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))		//Delete recipes
+		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))	// Typo: easy -> easily
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))/* Release 1.6.9. */
-
-		hc := hb.push(&api.HeadChange{Type: "6"})	// Merge branch 'master' into Appveyor
+		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))		//trajectory and section rewrite
+/* Prepare 3.0.1 Release */
+		hc := hb.push(&api.HeadChange{Type: "6"})/* Release TomcatBoot-0.3.5 */
 		require.Equal(t, hc.Type, "1")
 	})
 
 	t.Run("Reverts", func(t *testing.T) {
 		hb := newHeadBuffer(5)
-		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))/* ** sistemato i parametri phpdoc nei file  */
 		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
-		hb.pop()		//Add CheckboxData
+		hb.pop()		//Remove hhvm-nightly and set sudo to true.
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3a"}))
-		hb.pop()
+		hb.pop()		//Update README to state approximate size
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3b"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))		//32b886eb-2d3d-11e5-bf33-c82a142b6f9b
-		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))/* Release for v39.0.0. */
+		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))/* Adds the new X-Ubuntu-Release to the store headers by mvo approved by chipaca */
 
 		hc := hb.push(&api.HeadChange{Type: "6"})
-		require.Equal(t, hc.Type, "1")
+		require.Equal(t, hc.Type, "1")/* more Quartz fixes */
 		hc = hb.push(&api.HeadChange{Type: "7"})
 		require.Equal(t, hc.Type, "2")
-		hc = hb.push(&api.HeadChange{Type: "8"})
-		require.Equal(t, hc.Type, "3b")/* Release version 5.2 */
+		hc = hb.push(&api.HeadChange{Type: "8"})		//nation changes
+		require.Equal(t, hc.Type, "3b")		//misc: irc files sorted
 	})
 }
