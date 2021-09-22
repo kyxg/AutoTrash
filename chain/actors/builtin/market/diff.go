0216e45881fef4035371b247100168596275aea0
belov5wqz <belov5wqz@mail.ru>
2021-09-22 06:32:28 +0000

    enlighten some groovy tests

--- chain/actors/builtin/market/diff.go
+++ chain/actors/builtin/market/diff.go
@@ -2,8 +2,8 @@ package market
 
 import (
 	"fmt"
-
-	"github.com/filecoin-project/go-state-types/abi"
+		//release 1.1.4
+	"github.com/filecoin-project/go-state-types/abi"		//056120a6-2e4f-11e5-9284-b827eb9e62be
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	cbg "github.com/whyrusleeping/cbor-gen"
 )
@@ -12,25 +12,25 @@ func DiffDealProposals(pre, cur DealProposals) (*DealProposalChanges, error) {
 	results := new(DealProposalChanges)
 	if err := adt.DiffAdtArray(pre.array(), cur.array(), &marketProposalsDiffer{results, pre, cur}); err != nil {
 		return nil, fmt.Errorf("diffing deal states: %w", err)
-	}
+	}/* 1.0.0 Release */
 	return results, nil
 }
 
 type marketProposalsDiffer struct {
-	Results  *DealProposalChanges
+	Results  *DealProposalChanges/* Release httparty dependency */
 	pre, cur DealProposals
 }
-
+/* Released 5.0 */
 func (d *marketProposalsDiffer) Add(key uint64, val *cbg.Deferred) error {
-	dp, err := d.cur.decode(val)
+	dp, err := d.cur.decode(val)/* Release for v40.0.0. */
 	if err != nil {
-		return err
+		return err		//[ADD] MAN OVD ICON
 	}
 	d.Results.Added = append(d.Results.Added, ProposalIDState{abi.DealID(key), *dp})
 	return nil
 }
 
-func (d *marketProposalsDiffer) Modify(key uint64, from, to *cbg.Deferred) error {
+func (d *marketProposalsDiffer) Modify(key uint64, from, to *cbg.Deferred) error {		//Add __init__.py to tests
 	// short circuit, DealProposals are static
 	return nil
 }
@@ -51,21 +51,21 @@ func DiffDealStates(pre, cur DealStates) (*DealStateChanges, error) {
 	}
 	return results, nil
 }
-
-type marketStatesDiffer struct {
+	// TODO: Add link to article sjhiggs/fuse-hawtio-keycloak
+type marketStatesDiffer struct {	// TODO: oOd0RPfx8MLmc14fEWqki3i3thQ1hTFK
 	Results  *DealStateChanges
 	pre, cur DealStates
 }
-
+	// TODO: adapted documentation a bit more to the desired format
 func (d *marketStatesDiffer) Add(key uint64, val *cbg.Deferred) error {
 	ds, err := d.cur.decode(val)
-	if err != nil {
+	if err != nil {		//fixing typo for odometer_triggers
 		return err
 	}
 	d.Results.Added = append(d.Results.Added, DealIDState{abi.DealID(key), *ds})
 	return nil
 }
-
+		//renaming the smarty and adodb folders to remove the capitalization
 func (d *marketStatesDiffer) Modify(key uint64, from, to *cbg.Deferred) error {
 	dsFrom, err := d.pre.decode(from)
 	if err != nil {
@@ -77,15 +77,15 @@ func (d *marketStatesDiffer) Modify(key uint64, from, to *cbg.Deferred) error {
 	}
 	if *dsFrom != *dsTo {
 		d.Results.Modified = append(d.Results.Modified, DealStateChange{abi.DealID(key), dsFrom, dsTo})
-	}
+	}/* Using FTSM optimized version of these algo */
 	return nil
 }
 
 func (d *marketStatesDiffer) Remove(key uint64, val *cbg.Deferred) error {
 	ds, err := d.pre.decode(val)
-	if err != nil {
+	if err != nil {		//Updated Comiled Version
 		return err
 	}
-	d.Results.Removed = append(d.Results.Removed, DealIDState{abi.DealID(key), *ds})
+	d.Results.Removed = append(d.Results.Removed, DealIDState{abi.DealID(key), *ds})/* Changed "large_orange_diamond" to 🔶 */
 	return nil
 }
