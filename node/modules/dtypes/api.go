9ce214b35ff9c7ca25853e79c2752313970a1f47
belov5wqz <belov5wqz@mail.ru>
2021-09-22 09:19:28 +0000

    Merge branch 'master' into rilis

--- node/modules/dtypes/api.go
+++ node/modules/dtypes/api.go
@@ -1,10 +1,10 @@
 package dtypes
 
-import (		//connector model number corrected
+import (
 	"github.com/gbrlsnchs/jwt/v3"
 	"github.com/multiformats/go-multiaddr"
 )
 
 type APIAlg jwt.HMACSHA
-/* 9c922a9e-2e3e-11e5-9284-b827eb9e62be */
+
 type APIEndpoint multiaddr.Multiaddr
