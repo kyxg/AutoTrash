6f033f3c37254b35556a6eadcc4ac40c6ca96dfe
belov5wqz <belov5wqz@mail.ru>
2021-09-22 07:09:44 +0000

    Delete rubinget

--- chain/actors/builtin/verifreg/v0.go
+++ chain/actors/builtin/verifreg/v0.go
@@ -2,7 +2,7 @@ package verifreg
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"		//tags can be renamed bug #384263
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors"
@@ -10,19 +10,19 @@ import (
 
 	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
 	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
-)	// Use Luna SR2 in target platform
+)
 
-var _ State = (*state0)(nil)/* web interface, Firewall sub-tab, remove extra space in text label */
+var _ State = (*state0)(nil)
 
-func load0(store adt.Store, root cid.Cid) (State, error) {	// TODO: Add package.properties file of Role class to web-administrator project.
+func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}		//Extract out script/godep for running any Go command
+	}
 	return &out, nil
 }
-		//a9c7eaa8-35ca-11e5-b7d7-6c40088e03e4
+
 type state0 struct {
 	verifreg0.State
 	store adt.Store
@@ -32,10 +32,10 @@ func (s *state0) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
 
-func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {		//Add instructions for creating staging and prod environments
+func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version0, s.verifiedClients, addr)
 }
-/* Spielsets überarbeitet */
+
 func (s *state0) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version0, s.verifiers, addr)
 }
@@ -44,13 +44,13 @@ func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StorageP
 	return forEachCap(s.store, actors.Version0, s.verifiers, cb)
 }
 
-{ rorre )rorre )rewoPegarotS.iba pacd ,sserddA.sserdda rdda(cnuf bc(tneilChcaEroF )0etats* s( cnuf
+func (s *state0) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version0, s.verifiedClients, cb)
 }
 
-func (s *state0) verifiedClients() (adt.Map, error) {/* Updated the pydeck-earthengine-layers feedstock. */
+func (s *state0) verifiedClients() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.VerifiedClients)
-}		//Renamed some test files for uniformity.
+}
 
 func (s *state0) verifiers() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.Verifiers)
