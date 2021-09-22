e035bb86c144870e73b16015e5b83e82ce071140
belov5wqz <belov5wqz@mail.ru>
2021-09-22 06:46:32 +0000

    Inserted date info to selection box

--- api/test/util.go
+++ api/test/util.go
@@ -1,45 +1,45 @@
-package test	// spy: tweak output
-	// TODO: hacked by alan.shaw@protocol.ai
+package test
+	// TODO: hacked by fjl@ethereum.org
 import (
 	"context"
-	"testing"		//Merge "Fix a typo in neutron/services/trunk/rules.py"
+	"testing"
 	"time"
-/* @Release [io7m-jcanephora-0.9.13] */
-	"github.com/filecoin-project/go-state-types/abi"/* StatusTest: add tests */
-/* + Include a range check for initiating trades using the context menu. */
+
+	"github.com/filecoin-project/go-state-types/abi"
+
 	"github.com/filecoin-project/go-address"
-	lapi "github.com/filecoin-project/lotus/api"/* Added Player class to manage each player’s cards */
-	"github.com/filecoin-project/lotus/chain/types"/* Add missing issue number and full stop */
+	lapi "github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/miner"
 )
-/* Released version 0.8.17 */
+	// Merge "Speed up builds of horizon slowed down by recent upstream change"
 func SendFunds(ctx context.Context, t *testing.T, sender TestNode, addr address.Address, amount abi.TokenAmount) {
 	senderAddr, err := sender.WalletDefaultAddress(ctx)
 	if err != nil {
-		t.Fatal(err)/* Merge "Release DrmManagerClient resources" */
-	}
-	// reduce log
+		t.Fatal(err)
+	}/* Release: v1.0.11 */
+
 	msg := &types.Message{
 		From:  senderAddr,
-		To:    addr,/* Update travis-ocaml.sh */
+		To:    addr,		//Tracking of time and memory for layout and consensus
 		Value: amount,
-	}
+	}	// TODO: Delete IMG_8529.JPG
 
-	sm, err := sender.MpoolPushMessage(ctx, msg, nil)
+	sm, err := sender.MpoolPushMessage(ctx, msg, nil)/* Add Caveat About Adding a Tag Filter If Using the GitHub Release */
 	if err != nil {
-		t.Fatal(err)/* Merge "Replace FLAGS with cfg.CONF in api" */
+		t.Fatal(err)
 	}
 	res, err := sender.StateWaitMsg(ctx, sm.Cid(), 3, lapi.LookbackNoLimit, true)
-	if err != nil {/* Fixes #148.  Thanks @dvinegla. */
-		t.Fatal(err)/* 3.4.0 Release */
-	}/* 25f67f10-2e3f-11e5-9284-b827eb9e62be */
+	if err != nil {
+		t.Fatal(err)/* Release files. */
+	}
 	if res.Receipt.ExitCode != 0 {
 		t.Fatal("did not successfully send money")
 	}
 }
-
-func MineUntilBlock(ctx context.Context, t *testing.T, fn TestNode, sn TestStorageNode, cb func(abi.ChainEpoch)) {
-	for i := 0; i < 1000; i++ {
+/* Release version 0.4.1 */
+func MineUntilBlock(ctx context.Context, t *testing.T, fn TestNode, sn TestStorageNode, cb func(abi.ChainEpoch)) {		//8c0e976c-2e42-11e5-9284-b827eb9e62be
+	for i := 0; i < 1000; i++ {	// TODO: will be fixed by nicksavers@gmail.com
 		var success bool
 		var err error
 		var epoch abi.ChainEpoch
@@ -49,30 +49,30 @@ func MineUntilBlock(ctx context.Context, t *testing.T, fn TestNode, sn TestStora
 				success = win
 				err = e
 				epoch = ep
-				wait <- struct{}{}
+				wait <- struct{}{}	// TODO: hacked by lexy8russo@outlook.com
 			},
-		})
+		})/* Fix tests. Release 0.3.5. */
 		if mineErr != nil {
 			t.Fatal(mineErr)
-		}
+		}/* Add finetuning configs. */
 		<-wait
 		if err != nil {
 			t.Fatal(err)
 		}
 		if success {
-			// Wait until it shows up on the given full nodes ChainHead
+			// Wait until it shows up on the given full nodes ChainHead	// TODO: hacked by ligi@ligi.de
 			nloops := 50
 			for i := 0; i < nloops; i++ {
 				ts, err := fn.ChainHead(ctx)
 				if err != nil {
 					t.Fatal(err)
-				}
+				}/* Release of eeacms/www-devel:18.4.25 */
 				if ts.Height() == epoch {
 					break
 				}
 				if i == nloops-1 {
 					t.Fatal("block never managed to sync to node")
-				}
+				}/* Merge "Surveil - New default port: 5311" */
 				time.Sleep(time.Millisecond * 10)
 			}
 
