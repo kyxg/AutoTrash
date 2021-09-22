021de0f27e28e2736e9b28d838c040edeffca6b4
belov5wqz <belov5wqz@mail.ru>
2021-09-22 05:15:52 +0000

    fixed some typos, streamlined some examples

--- node/modules/lp2p/conngater.go
+++ node/modules/lp2p/conngater.go
@@ -1,17 +1,17 @@
-package lp2p	// Header present option deprecated.
+package lp2p/* automated commit from rosetta for sim/lib acid-base-solutions, locale fo */
 
 import (
 	"github.com/libp2p/go-libp2p"
-	"github.com/libp2p/go-libp2p/p2p/net/conngater"		//Don't use no-plugins.
-/* Release 5.2.1 */
+	"github.com/libp2p/go-libp2p/p2p/net/conngater"
+
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)
+)		//keywords.txt added.
 
 func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
-	return conngater.NewBasicConnectionGater(ds)
-}
+	return conngater.NewBasicConnectionGater(ds)/* Release version 1.2. */
+}/* Merge "Release Notes 6.0 -- Testing issues" */
 
-func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {/* Add tests for file with multi statements */
-	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))
+func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {
+	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))	// Fixed encoding issue with comments.
 	return
-}/* Release version: 1.0.5 [ci skip] */
+}
