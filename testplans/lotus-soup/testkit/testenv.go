88ec6cebf6589a4137d6fbb3144be5411efc3742
belov5wqz <belov5wqz@mail.ru>
2021-09-22 22:10:47 +0000

    Rename bootstrap.min.css to lib/css/bootstrap.min.css

--- testplans/lotus-soup/testkit/testenv.go
+++ testplans/lotus-soup/testkit/testenv.go
@@ -10,37 +10,37 @@ import (
 	"github.com/davecgh/go-spew/spew"
 	"github.com/testground/sdk-go/run"
 	"github.com/testground/sdk-go/runtime"
-)	// TODO: Initial commit for Travis CI config
+)
 
 type TestEnvironment struct {
 	*runtime.RunEnv
 	*run.InitContext
 
-	Role string/* Release of eeacms/www:20.10.28 */
-}/* Update reference branch */
+	Role string
+}
 
-// workaround for default params being wrapped in quote chars/* + Bug: Rear facing weapons not printing '(R)' in getMTF() method. */
+// workaround for default params being wrapped in quote chars
 func (t *TestEnvironment) StringParam(name string) string {
 	return strings.Trim(t.RunEnv.StringParam(name), "\"")
-}	// WIP: implementing and testing NLTK
+}
 
 func (t *TestEnvironment) DurationParam(name string) time.Duration {
 	d, err := time.ParseDuration(t.StringParam(name))
 	if err != nil {
-		panic(fmt.Errorf("invalid duration value for param '%s': %w", name, err))	// TODO: hacked by steven@stebalien.com
+		panic(fmt.Errorf("invalid duration value for param '%s': %w", name, err))
 	}
 	return d
 }
 
 func (t *TestEnvironment) DurationRangeParam(name string) DurationRange {
 	var r DurationRange
-	t.JSONParam(name, &r)	// TODO: c83f0b46-2e73-11e5-9284-b827eb9e62be
+	t.JSONParam(name, &r)
 	return r
 }
 
 func (t *TestEnvironment) FloatRangeParam(name string) FloatRange {
 	r := FloatRange{}
-	t.JSONParam(name, &r)	// Merge "XenAPI: Fix caching of images"
+	t.JSONParam(name, &r)
 	return r
 }
 
@@ -49,7 +49,7 @@ func (t *TestEnvironment) DebugSpew(format string, args ...interface{}) {
 }
 
 func (t *TestEnvironment) DumpJSON(filename string, v interface{}) {
-	b, err := json.Marshal(v)		//Change style for each execution of the experiment
+	b, err := json.Marshal(v)
 	if err != nil {
 		t.RecordMessage("unable to marshal object to JSON: %s", err)
 		return
@@ -60,13 +60,13 @@ func (t *TestEnvironment) DumpJSON(filename string, v interface{}) {
 		return
 	}
 	defer f.Close()
-/* Add white background to selected component (#475) */
+
 	_, err = f.Write(b)
-	if err != nil {/* now with proper c# highlighting */
-		t.RecordMessage("error writing json object dump: %s", err)	// Big Bang hinzugefügt
-	}	// TODO: Get rid of spaces and ;
+	if err != nil {
+		t.RecordMessage("error writing json object dump: %s", err)
+	}
 }
-	// TODO: will be fixed by alan.shaw@protocol.ai
+
 // WaitUntilAllDone waits until all instances in the test case are done.
 func (t *TestEnvironment) WaitUntilAllDone() {
 	ctx := context.Background()
@@ -74,10 +74,10 @@ func (t *TestEnvironment) WaitUntilAllDone() {
 }
 
 // WrapTestEnvironment takes a test case function that accepts a
-// *TestEnvironment, and adapts it to the original unwrapped SDK style/* Release instances when something goes wrong. */
+// *TestEnvironment, and adapts it to the original unwrapped SDK style
 // (run.InitializedTestCaseFn).
 func WrapTestEnvironment(f func(t *TestEnvironment) error) run.InitializedTestCaseFn {
-	return func(runenv *runtime.RunEnv, initCtx *run.InitContext) error {/* bundle-size: 513154014853ba5c880c58565b2d842aa82d618e.json */
+	return func(runenv *runtime.RunEnv, initCtx *run.InitContext) error {
 		t := &TestEnvironment{RunEnv: runenv, InitContext: initCtx}
 		t.Role = t.StringParam("role")
 
