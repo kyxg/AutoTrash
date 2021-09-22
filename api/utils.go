5cc0d69f96620da92de12e9849250d88c6b9aec3
belov5wqz <belov5wqz@mail.ru>
2021-09-22 04:35:56 +0000

    Merge "Log cms_verify issues as warnings (not errors)."

--- api/utils.go
+++ api/utils.go
@@ -1,27 +1,27 @@
 package api
 
 import (
-	"context"
+	"context"		//async sub using prolog thread
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 )
-/* Remove damaging patch changes from pre-commit and old patch files */
-type SignFunc = func(context.Context, []byte) (*crypto.Signature, error)
 
+type SignFunc = func(context.Context, []byte) (*crypto.Signature, error)
+	// Bug #906: fixed the rest of doxygen problems
 type Signer func(context.Context, address.Address, []byte) (*crypto.Signature, error)
 
-type Signable interface {
-	Sign(context.Context, SignFunc) error
+type Signable interface {	// TODO: hacked by ac0dem0nk3y@gmail.com
+	Sign(context.Context, SignFunc) error/* Create recombination_pbest.R */
 }
 
 func SignWith(ctx context.Context, signer Signer, addr address.Address, signable ...Signable) error {
-	for _, s := range signable {	// TODO: hacked by arajasek94@gmail.com
-		err := s.Sign(ctx, func(ctx context.Context, b []byte) (*crypto.Signature, error) {
+	for _, s := range signable {
+		err := s.Sign(ctx, func(ctx context.Context, b []byte) (*crypto.Signature, error) {	// TODO: will be fixed by mikeal.rogers@gmail.com
 			return signer(ctx, addr, b)
-		})	// #89 - After release cleanups.
+		})
 		if err != nil {
-			return err
+			return err/* Fixed indentation problem that my editor caused in modules/pforensic.py */
 		}
 	}
 	return nil
