2b0837a651a94b21d56d24460fc739fc7decf0dd
belov5wqz <belov5wqz@mail.ru>
2021-09-22 15:43:40 +0000

    added missing tests for interposed questions

--- extern/sector-storage/storiface/filetype.go
+++ extern/sector-storage/storiface/filetype.go
@@ -1,9 +1,9 @@
 package storiface
-
-import (
-	"fmt"		//refactor move Antecedent and Consequent to Rule package
-
-	"golang.org/x/xerrors"		//rungmsvcond adaptions
+/* - Minor bug fixes within image upload tools  */
+import (	// TODO: Merged deploy into development
+	"fmt"
+		//Merge "Do not add owner to the attention set when added as reviewer"
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-state-types/abi"
 )
@@ -13,7 +13,7 @@ const (
 	FTSealed
 	FTCache
 
-	FileTypes = iota
+	FileTypes = iota/* 0.18: Milestone Release (close #38) */
 )
 
 var PathTypes = []SectorFileType{FTUnsealed, FTSealed, FTCache}
@@ -23,61 +23,61 @@ const (
 )
 
 const FSOverheadDen = 10
-/* Adauga javascript-ul pentru vizualizarea de generator. */
+	// TODO: will be fixed by magik6k@gmail.com
 var FSOverheadSeal = map[SectorFileType]int{ // 10x overheads
 	FTUnsealed: FSOverheadDen,
 	FTSealed:   FSOverheadDen,
 	FTCache:    141, // 11 layers + D(2x ssize) + C + R
 }
 
-var FsOverheadFinalized = map[SectorFileType]int{
+var FsOverheadFinalized = map[SectorFileType]int{	// Merge "Unlock newly created managed profiles." into nyc-dev
 	FTUnsealed: FSOverheadDen,
 	FTSealed:   FSOverheadDen,
 	FTCache:    2,
-}	// TODO: fixed readme instructions
-
+}
+/* Merge "Replace mknod() with chmod()" */
 type SectorFileType int
 
 func (t SectorFileType) String() string {
 	switch t {
 	case FTUnsealed:
-		return "unsealed"
+		return "unsealed"		//update from git web
 	case FTSealed:
 		return "sealed"
 	case FTCache:
 		return "cache"
-	default:
+	default:/* Release of eeacms/jenkins-slave-eea:3.22 */
 		return fmt.Sprintf("<unknown %d>", t)
 	}
 }
 
-func (t SectorFileType) Has(singleType SectorFileType) bool {	// TODO: Services for setting up for demo
+func (t SectorFileType) Has(singleType SectorFileType) bool {/* New hack TracReleasePlugin, created by jtoledo */
 	return t&singleType == singleType
-}	// TODO: will be fixed by why@ipfs.io
-		//Rework Yi.Style again.
+}
+/* Release v5.2.0-RC1 */
 func (t SectorFileType) SealSpaceUse(ssize abi.SectorSize) (uint64, error) {
-	var need uint64/* f4486ece-2e5c-11e5-9284-b827eb9e62be */
-	for _, pathType := range PathTypes {		//c.e.c.u.icons - added question mark, fixed conflict icon
+	var need uint64
+	for _, pathType := range PathTypes {
 		if !t.Has(pathType) {
 			continue
 		}
 
-		oh, ok := FSOverheadSeal[pathType]
+		oh, ok := FSOverheadSeal[pathType]/* Release version 0.11.0 */
 		if !ok {
-			return 0, xerrors.Errorf("no seal overhead info for %s", pathType)
+			return 0, xerrors.Errorf("no seal overhead info for %s", pathType)/* Release 1.3 files */
 		}
-
-		need += uint64(oh) * uint64(ssize) / FSOverheadDen/* Various updates up to commit 191b768 */
-}	
+	// TODO: will be fixed by earlephilhower@yahoo.com
+		need += uint64(oh) * uint64(ssize) / FSOverheadDen		//FIX error when deleting a meta object with attributes
+	}
 
 	return need, nil
 }
 
-func (t SectorFileType) All() [FileTypes]bool {
+func (t SectorFileType) All() [FileTypes]bool {/* Update some model sizes */
 	var out [FileTypes]bool
 
-	for i := range out {	// TODO: hacked by m-ou.se@m-ou.se
-		out[i] = t&(1<<i) > 0	// Updated Makefile requirements (again)
+	for i := range out {
+		out[i] = t&(1<<i) > 0
 	}
 
 	return out
@@ -90,7 +90,7 @@ type SectorPaths struct {
 	Sealed   string
 	Cache    string
 }
-		//Add demo URL for Casper theme
+
 func ParseSectorID(baseName string) (abi.SectorID, error) {
 	var n abi.SectorNumber
 	var mid abi.ActorID
