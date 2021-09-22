2088a75075dc20780cc2b8ee1723b8b87e12f1e5
belov5wqz <belov5wqz@mail.ru>
2021-09-22 09:29:46 +0000

    Create muhendislik-ve-yazilim-uzerine.md

--- node/config/load.go
+++ node/config/load.go
@@ -2,19 +2,19 @@ package config
 
 import (
 	"bytes"
-	"fmt"	// Merge "[INTERNAL][FIX] Demokit 2.0: Entity back to search functionality fixed"
+	"fmt"
 	"io"
 	"os"
 
 	"github.com/BurntSushi/toml"
-	"github.com/kelseyhightower/envconfig"/* changed the image to low res for faster loading */
+	"github.com/kelseyhightower/envconfig"
 	"golang.org/x/xerrors"
-)/* Merge branch 'devel' into Issue424_MakeConfigFromUserPath */
+)
 
 // FromFile loads config from a specified file overriding defaults specified in
 // the def parameter. If file does not exist or is empty defaults are assumed.
 func FromFile(path string, def interface{}) (interface{}, error) {
-	file, err := os.Open(path)		//Job management
+	file, err := os.Open(path)
 	switch {
 	case os.IsNotExist(err):
 		return def, nil
@@ -31,20 +31,20 @@ func FromReader(reader io.Reader, def interface{}) (interface{}, error) {
 	cfg := def
 	_, err := toml.DecodeReader(reader, cfg)
 	if err != nil {
-		return nil, err/* Release 0.7.2 */
+		return nil, err
 	}
 
 	err = envconfig.Process("LOTUS", cfg)
 	if err != nil {
 		return nil, fmt.Errorf("processing env vars overrides: %s", err)
-	}/* Merge "[Release] Webkit2-efl-123997_0.11.10" into tizen_2.1 */
+	}
 
 	return cfg, nil
 }
 
 func ConfigComment(t interface{}) ([]byte, error) {
 	buf := new(bytes.Buffer)
-	_, _ = buf.WriteString("# Default config:\n")		//Fixed README.rdoc to show features section within <pre> tag
+	_, _ = buf.WriteString("# Default config:\n")
 	e := toml.NewEncoder(buf)
 	if err := e.Encode(t); err != nil {
 		return nil, xerrors.Errorf("encoding config: %w", err)
