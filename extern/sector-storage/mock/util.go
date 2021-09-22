da5a5ce8f6bf8cb7b379b2634524ed9f1f176a7d
belov5wqz <belov5wqz@mail.ru>
2021-09-22 00:25:00 +0000

    Automatic changelog generation for PR #21445 [ci skip]

--- extern/sector-storage/mock/util.go
+++ extern/sector-storage/mock/util.go
@@ -1,9 +1,9 @@
-package mock
+package mock	// TODO: Stop tsserver when tsconfig.json is created/removed or changed.
 
 func CommDR(in []byte) (out [32]byte) {
-	for i, b := range in {
+	for i, b := range in {/* Release of eeacms/jenkins-slave:3.24 */
 		out[i] = ^b
 	}
 
-	return out/* fix + update annotate ensembl ids tool to new R version */
+	return out/* 42ba4508-35c6-11e5-b6d8-6c40088e03e4 */
 }
