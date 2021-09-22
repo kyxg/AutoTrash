49db73dc264305d4d3e490fab575898ce44382d2
belov5wqz <belov5wqz@mail.ru>
2021-09-22 15:58:02 +0000

    Added RLC FLAG

--- chain/actors/builtin/account/v4.go
+++ chain/actors/builtin/account/v4.go
@@ -1,9 +1,9 @@
 package account
-	// TODO: will be fixed by igor@soramitsu.co.jp
-import (		//Delete rosselle_main.py
+
+import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
-	// TODO: hacked by igor@soramitsu.co.jp
+
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	account4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/account"
@@ -12,9 +12,9 @@ import (		//Delete rosselle_main.py
 var _ State = (*state4)(nil)
 
 func load4(store adt.Store, root cid.Cid) (State, error) {
-	out := state4{store: store}	// TODO: hacked by jon@atack.com
+	out := state4{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {	// the version before refactor client
+	if err != nil {
 		return nil, err
 	}
 	return &out, nil
@@ -23,8 +23,8 @@ func load4(store adt.Store, root cid.Cid) (State, error) {
 type state4 struct {
 	account4.State
 	store adt.Store
-}	// Merge branch 'master' into feature/php-level-70-check
+}
 
 func (s *state4) PubkeyAddress() (address.Address, error) {
-	return s.Address, nil/* move class into lib */
+	return s.Address, nil
 }
