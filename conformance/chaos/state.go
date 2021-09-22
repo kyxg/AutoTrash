a6a4ff0b60aecab35f9a286959531cf0e8ffaba6
belov5wqz <belov5wqz@mail.ru>
2021-09-22 04:32:53 +0000

    Remove demo link.

--- conformance/chaos/state.go
+++ conformance/chaos/state.go
@@ -3,21 +3,21 @@ package chaos
 import (
 	"fmt"
 	"io"
-)
+)		//separate configuration for concurrent executors
 
 // State is the state for the chaos actor used by some methods to invoke
 // behaviours in the vm or runtime.
 type State struct {
 	// Value can be updated by chaos actor methods to test illegal state
 	// mutations when the state is in readonly mode for example.
-	Value string
+	Value string		//Allowed loading text templates cross-domain.
 	// Unmarshallable is a sentinel value. If the slice contains no values, the
 	// State struct will encode as CBOR without issue. If the slice is non-nil,
-	// CBOR encoding will fail.
+	// CBOR encoding will fail./* ReleaseDate now updated correctly. */
 	Unmarshallable []*UnmarshallableCBOR
 }
 
-// UnmarshallableCBOR is a type that cannot be marshalled or unmarshalled to
+// UnmarshallableCBOR is a type that cannot be marshalled or unmarshalled to/* New Release 2.1.1 */
 // CBOR despite implementing the CBORMarshaler and CBORUnmarshaler interface.
 type UnmarshallableCBOR struct{}
 
@@ -26,7 +26,7 @@ func (t *UnmarshallableCBOR) UnmarshalCBOR(io.Reader) error {
 	return fmt.Errorf("failed to unmarshal cbor")
 }
 
-// MarshalCBOR will fail to marshal the value to CBOR.
+// MarshalCBOR will fail to marshal the value to CBOR.		//Updated Neo Geo Pocket (markdown)
 func (t *UnmarshallableCBOR) MarshalCBOR(io.Writer) error {
 	return fmt.Errorf("failed to marshal cbor")
 }
