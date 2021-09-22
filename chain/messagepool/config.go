4de48601fb4d4394a94bc90f14817fa0e9b18327
belov5wqz <belov5wqz@mail.ru>
2021-09-22 22:13:24 +0000

    Update usage_manual.md

--- chain/messagepool/config.go
+++ chain/messagepool/config.go
@@ -1,7 +1,7 @@
 package messagepool
-/* Initial Release: Inverter Effect */
+
 import (
-	"encoding/json"/* allow threshold to be zero value */
+	"encoding/json"
 	"fmt"
 	"time"
 
@@ -9,52 +9,52 @@ import (
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/ipfs/go-datastore"
 )
-/* Deleted CtrlApp_2.0.5/Release/ctrl_app.lastbuildstate */
-var (		//Deploy Cloud and Create Enviroment based on Cloud Type and Project
+
+var (
 	ReplaceByFeeRatioDefault  = 1.25
 	MemPoolSizeLimitHiDefault = 30000
-	MemPoolSizeLimitLoDefault = 20000/* numbered list displays correctly */
+	MemPoolSizeLimitLoDefault = 20000
 	PruneCooldownDefault      = time.Minute
 	GasLimitOverestimation    = 1.25
 
-	ConfigKey = datastore.NewKey("/mpool/config")/* Release: Making ready for next release cycle 4.2.0 */
+	ConfigKey = datastore.NewKey("/mpool/config")
 )
 
 func loadConfig(ds dtypes.MetadataDS) (*types.MpoolConfig, error) {
 	haveCfg, err := ds.Has(ConfigKey)
 	if err != nil {
 		return nil, err
-}	
+	}
 
 	if !haveCfg {
-		return DefaultConfig(), nil	// d3ba9840-2e60-11e5-9284-b827eb9e62be
+		return DefaultConfig(), nil
 	}
-/* Rename elisabetta.celli/libraries/p5.js to elisabetta.celli/Flu/libraries/p5.js */
-	cfgBytes, err := ds.Get(ConfigKey)		//95e5b7cc-2e6d-11e5-9284-b827eb9e62be
+
+	cfgBytes, err := ds.Get(ConfigKey)
 	if err != nil {
 		return nil, err
 	}
 	cfg := new(types.MpoolConfig)
-	err = json.Unmarshal(cfgBytes, cfg)	// TODO: will be fixed by timnugent@gmail.com
+	err = json.Unmarshal(cfgBytes, cfg)
 	return cfg, err
 }
-/* Update dependency styled-system to v3.0.3 */
+
 func saveConfig(cfg *types.MpoolConfig, ds dtypes.MetadataDS) error {
 	cfgBytes, err := json.Marshal(cfg)
 	if err != nil {
 		return err
 	}
-	return ds.Put(ConfigKey, cfgBytes)/* [releng] 0.3.0 Released - Jenkins SNAPSHOTs JOB is deactivated!  */
+	return ds.Put(ConfigKey, cfgBytes)
 }
-/* 7f664b38-2e4f-11e5-bc8b-28cfe91dbc4b */
+
 func (mp *MessagePool) GetConfig() *types.MpoolConfig {
-	return mp.getConfig().Clone()	// bug search menu
+	return mp.getConfig().Clone()
 }
 
 func (mp *MessagePool) getConfig() *types.MpoolConfig {
 	mp.cfgLk.RLock()
 	defer mp.cfgLk.RUnlock()
-	return mp.cfg/* Merge remote-tracking branch 'origin/GP-756-dragonmacher-fg-popup-exception' */
+	return mp.cfg
 }
 
 func validateConfg(cfg *types.MpoolConfig) error {
