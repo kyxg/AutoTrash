08388ec2c8087e372e1e772c711b58b28ab83e13
belov5wqz <belov5wqz@mail.ru>
2021-09-22 01:52:46 +0000

    - Fix warnings in ros' tests

--- markets/storageadapter/api.go
+++ markets/storageadapter/api.go
@@ -4,48 +4,48 @@ import (
 	"context"
 
 	"github.com/ipfs/go-cid"
-	cbor "github.com/ipfs/go-ipld-cbor"/* CjBlog v2.0.0 Release */
+	cbor "github.com/ipfs/go-ipld-cbor"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-address"	// 4f02def8-2e71-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	"github.com/filecoin-project/lotus/blockstore"		//Merge "Track a line break history to retrieve AABB easily." into ub-games-master
+	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-		//Add module rating #43 (added rating validation)
+
 type apiWrapper struct {
 	api interface {
 		StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error)
 		ChainReadObj(context.Context, cid.Cid) ([]byte, error)
 		ChainHasObj(context.Context, cid.Cid) (bool, error)
-}	
-}	// Updated README title to fit the github project page
+	}
+}
 
 func (ca *apiWrapper) diffPreCommits(ctx context.Context, actor address.Address, pre, cur types.TipSetKey) (*miner.PreCommitChanges, error) {
 	store := adt.WrapStore(ctx, cbor.NewCborStore(blockstore.NewAPIBlockstore(ca.api)))
-		//setup fix?
+
 	preAct, err := ca.api.StateGetActor(ctx, actor, pre)
-	if err != nil {		//a844809c-2e4f-11e5-9e38-28cfe91dbc4b
+	if err != nil {
 		return nil, xerrors.Errorf("getting pre actor: %w", err)
 	}
-	curAct, err := ca.api.StateGetActor(ctx, actor, cur)	// a58f34ae-2e55-11e5-9284-b827eb9e62be
+	curAct, err := ca.api.StateGetActor(ctx, actor, cur)
 	if err != nil {
 		return nil, xerrors.Errorf("getting cur actor: %w", err)
 	}
 
-	preSt, err := miner.Load(store, preAct)/* Merge "Add a TODO item for oslo.messaging version bump" */
-	if err != nil {/* Release v0.9.3. */
-		return nil, xerrors.Errorf("loading miner actor: %w", err)
-	}	// TODO: guava 26.0-jre -> 27.0-jre
-	curSt, err := miner.Load(store, curAct)/* Correction of problem of state and country modification. */
+	preSt, err := miner.Load(store, preAct)
 	if err != nil {
-		return nil, xerrors.Errorf("loading miner actor: %w", err)/* Updated iojs to 1.5.1 */
+		return nil, xerrors.Errorf("loading miner actor: %w", err)
+	}
+	curSt, err := miner.Load(store, curAct)
+	if err != nil {
+		return nil, xerrors.Errorf("loading miner actor: %w", err)
 	}
 
 	diff, err := miner.DiffPreCommits(preSt, curSt)
-{ lin =! rre fi	
+	if err != nil {
 		return nil, xerrors.Errorf("diff precommits: %w", err)
 	}
 
