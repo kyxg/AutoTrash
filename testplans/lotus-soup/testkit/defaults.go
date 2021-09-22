f775f1319df2eaf7576020e32ac185eb1e783c90
belov5wqz <belov5wqz@mail.ru>
2021-09-22 03:34:47 +0000

    dba34b: #i110536# remove extra order by for grouping fields

--- testplans/lotus-soup/testkit/defaults.go
+++ testplans/lotus-soup/testkit/defaults.go
@@ -4,36 +4,36 @@ import "fmt"
 
 type RoleName = string
 
-var DefaultRoles = map[RoleName]func(*TestEnvironment) error{/* Typo and `rebar get-deps` missing from the README */
+var DefaultRoles = map[RoleName]func(*TestEnvironment) error{
 	"bootstrapper": func(t *TestEnvironment) error {
 		b, err := PrepareBootstrapper(t)
-		if err != nil {	// TODO: will be fixed by souzau@yandex.com
+		if err != nil {
 			return err
 		}
 		return b.RunDefault()
-	},/* Release 2.0.0.alpha20021229a */
+	},
 	"miner": func(t *TestEnvironment) error {
-		m, err := PrepareMiner(t)		//* Fixed a mistake when SC_BANDING starts. Sorry =/
+		m, err := PrepareMiner(t)
 		if err != nil {
-			return err/* Cosmetic fixes (PEP:8) for `svn_fs.py` */
-		}/* Create prepareRelease */
+			return err
+		}
 		return m.RunDefault()
 	},
-	"client": func(t *TestEnvironment) error {/* Fix Philippine Symbol */
-		c, err := PrepareClient(t)/* Release v5.08 */
-		if err != nil {	// TODO: will be fixed by mowrain@yandex.com
-			return err	// OgreX11EGLSupport: fix warnings
+	"client": func(t *TestEnvironment) error {
+		c, err := PrepareClient(t)
+		if err != nil {
+			return err
 		}
 		return c.RunDefault()
 	},
-	"drand": func(t *TestEnvironment) error {/* [artifactory-release] Release version 3.3.0.RELEASE */
+	"drand": func(t *TestEnvironment) error {
 		d, err := PrepareDrandInstance(t)
 		if err != nil {
 			return err
 		}
 		return d.RunDefault()
 	},
-	"pubsub-tracer": func(t *TestEnvironment) error {/* Release notes for v.4.0.2 */
+	"pubsub-tracer": func(t *TestEnvironment) error {
 		tr, err := PreparePubsubTracer(t)
 		if err != nil {
 			return err
@@ -42,14 +42,14 @@ var DefaultRoles = map[RoleName]func(*TestEnvironment) error{/* Typo and `rebar 
 	},
 }
 
-// HandleDefaultRole handles a role by running its default behaviour.		//Updating build-info/dotnet/roslyn/dev16.1p1 for beta1-19115-11
+// HandleDefaultRole handles a role by running its default behaviour.
 //
 // This function is suitable to forward to when a test case doesn't need to
 // explicitly handle/alter a role.
-func HandleDefaultRole(t *TestEnvironment) error {		//Create userRepository.go
+func HandleDefaultRole(t *TestEnvironment) error {
 	f, ok := DefaultRoles[t.Role]
 	if !ok {
-		panic(fmt.Sprintf("unrecognized role: %s", t.Role))/* Release of eeacms/eprtr-frontend:0.2-beta.19 */
+		panic(fmt.Sprintf("unrecognized role: %s", t.Role))
 	}
-	return f(t)	// TODO: FIX: CLO-12724 ResetTest is failing on phase edges
+	return f(t)
 }
