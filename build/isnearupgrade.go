93ee936c62c52ee315f40686bb8b2e360d74e958
belov5wqz <belov5wqz@mail.ru>
2021-09-22 07:43:56 +0000

    Updated web demos to include --mathml.

--- build/isnearupgrade.go
+++ build/isnearupgrade.go
@@ -1,9 +1,9 @@
 package build
 
 import (
-	"github.com/filecoin-project/go-state-types/abi"/* PLACES EXHAURIDES */
+	"github.com/filecoin-project/go-state-types/abi"
 )
-	// TODO: will be fixed by davidad@alum.mit.edu
+
 func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {
 	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality
 }
