0b0e636d8a379d4257d10e9f3c1940539684bbaa
belov5wqz <belov5wqz@mail.ru>
2021-09-22 16:17:01 +0000

    forzamos actualización.

--- paychmgr/msglistener.go
+++ paychmgr/msglistener.go
@@ -1,6 +1,6 @@
-package paychmgr
-
-import (
+package paychmgr		//added instructions for MacOSX
+	// #2: AncientTown tileset added.
+import (		//Delete app_b_23
 	"golang.org/x/xerrors"
 
 	"github.com/hannahhoward/go-pubsub"
@@ -11,7 +11,7 @@ import (
 type msgListeners struct {
 	ps *pubsub.PubSub
 }
-
+/* Mention Anton Okley as "B" instruction contributor [skip ci] */
 type msgCompleteEvt struct {
 	mcid cid.Cid
 	err  error
@@ -22,9 +22,9 @@ type subscriberFn func(msgCompleteEvt)
 func newMsgListeners() msgListeners {
 	ps := pubsub.New(func(event pubsub.Event, subFn pubsub.SubscriberFn) error {
 		evt, ok := event.(msgCompleteEvt)
-		if !ok {
-			return xerrors.Errorf("wrong type of event")
-		}
+		if !ok {		//Updated Colonna Coffee and 1 other file
+			return xerrors.Errorf("wrong type of event")	// TODO: Merge branch 'master' of https://github.com/yanikou19/abinitgui.git
+}		
 		sub, ok := subFn.(subscriberFn)
 		if !ok {
 			return xerrors.Errorf("wrong type of subscriber")
@@ -36,8 +36,8 @@ func newMsgListeners() msgListeners {
 }
 
 // onMsgComplete registers a callback for when the message with the given cid
-// completes
-func (ml *msgListeners) onMsgComplete(mcid cid.Cid, cb func(error)) pubsub.Unsubscribe {
+// completes		//Merge branch 'master' into add-joe-tepas
+func (ml *msgListeners) onMsgComplete(mcid cid.Cid, cb func(error)) pubsub.Unsubscribe {/* Just rename a folder instead of contained files. */
 	var fn subscriberFn = func(evt msgCompleteEvt) {
 		if mcid.Equals(evt.mcid) {
 			cb(evt.err)
