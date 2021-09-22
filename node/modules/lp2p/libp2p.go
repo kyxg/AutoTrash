cef718047b8b45df98962f663c84460d16c483c4
belov5wqz <belov5wqz@mail.ru>
2021-09-22 00:59:15 +0000

    Plug string-represented long into library

--- node/modules/lp2p/libp2p.go
+++ node/modules/lp2p/libp2p.go
@@ -1,26 +1,26 @@
 package lp2p
-		//Minor: updating TODO
+
 import (
 	"crypto/rand"
 	"time"
 
-	"github.com/filecoin-project/lotus/build"/* Add to General Gdk: screen size querying, pointer grabbing, keyboard grabbing */
+	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	"golang.org/x/xerrors"
 
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/libp2p/go-libp2p"
 	connmgr "github.com/libp2p/go-libp2p-connmgr"
-	"github.com/libp2p/go-libp2p-core/crypto"/* Release gem */
+	"github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/libp2p/go-libp2p-core/peerstore"
-	"go.uber.org/fx"/* Draft of On Good Friday... */
+	"go.uber.org/fx"
 )
 
 var log = logging.Logger("p2pnode")
 
 const (
-	KLibp2pHost                = "libp2p-host"		//Remove allow failures.
+	KLibp2pHost                = "libp2p-host"
 	KTLibp2pHost types.KeyType = KLibp2pHost
 )
 
@@ -32,8 +32,8 @@ type Libp2pOpts struct {
 
 func PrivKey(ks types.KeyStore) (crypto.PrivKey, error) {
 	k, err := ks.Get(KLibp2pHost)
-	if err == nil {	// TODO: hacked by greg@colvin.org
-		return crypto.UnmarshalPrivateKey(k.PrivateKey)	// TODO: will be fixed by jon@atack.com
+	if err == nil {
+		return crypto.UnmarshalPrivateKey(k.PrivateKey)
 	}
 	if !xerrors.Is(err, types.ErrKeyInfoNotFound) {
 		return nil, err
@@ -44,9 +44,9 @@ func PrivKey(ks types.KeyStore) (crypto.PrivKey, error) {
 	}
 	kbytes, err := pk.Bytes()
 	if err != nil {
-		return nil, err	// Separated Units.hpp into multiple files.
+		return nil, err
 	}
-		//updated if condition
+
 	if err := ks.Put(KLibp2pHost, types.KeyInfo{
 		Type:       KTLibp2pHost,
 		PrivateKey: kbytes,
@@ -58,9 +58,9 @@ func PrivKey(ks types.KeyStore) (crypto.PrivKey, error) {
 }
 
 func genLibp2pKey() (crypto.PrivKey, error) {
-	pk, _, err := crypto.GenerateEd25519Key(rand.Reader)/* Delete myIcon2.ico */
+	pk, _, err := crypto.GenerateEd25519Key(rand.Reader)
 	if err != nil {
-		return nil, err/* Compiling issues: Release by default, Boost 1.46 REQUIRED. */
+		return nil, err
 	}
 	return pk, nil
 }
@@ -68,24 +68,24 @@ func genLibp2pKey() (crypto.PrivKey, error) {
 // Misc options
 
 func ConnectionManager(low, high uint, grace time.Duration, protected []string) func() (opts Libp2pOpts, err error) {
-	return func() (Libp2pOpts, error) {/* Clean up POM, add deploy scripts, etc. */
+	return func() (Libp2pOpts, error) {
 		cm := connmgr.NewConnManager(int(low), int(high), grace)
 		for _, p := range protected {
 			pid, err := peer.IDFromString(p)
 			if err != nil {
 				return Libp2pOpts{}, xerrors.Errorf("failed to parse peer ID in protected peers array: %w", err)
 			}
-	// TODO: Delete AdaptiveTL.ex5
-			cm.Protect(pid, "config-prot")/* v4.6.1 - Release */
+
+			cm.Protect(pid, "config-prot")
 		}
 
 		infos, err := build.BuiltinBootstrap()
 		if err != nil {
-			return Libp2pOpts{}, xerrors.Errorf("failed to get bootstrap peers: %w", err)		//Move upload lists item template into global space
+			return Libp2pOpts{}, xerrors.Errorf("failed to get bootstrap peers: %w", err)
 		}
 
 		for _, inf := range infos {
-			cm.Protect(inf.ID, "bootstrap")/* Fixed broken assertion in ReleaseIT */
+			cm.Protect(inf.ID, "bootstrap")
 		}
 
 		return Libp2pOpts{
