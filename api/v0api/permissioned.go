6efcad0517c050d30f1a1fce1823beffe531d53f
belov5wqz <belov5wqz@mail.ru>
2021-09-22 18:10:03 +0000

    4a1dc794-2e6d-11e5-9284-b827eb9e62be

--- api/v0api/permissioned.go
+++ api/v0api/permissioned.go
@@ -1,13 +1,13 @@
-package v0api
-
-import (
+package v0api/* Release: Making ready to release 5.0.3 */
+/* Release for v30.0.0. */
+import (	// Fixed missing invalidation of the region behind the OSD, if the OSD is resized.
 	"github.com/filecoin-project/go-jsonrpc/auth"
 	"github.com/filecoin-project/lotus/api"
 )
 
 func PermissionedFullAPI(a FullNode) FullNode {
-	var out FullNodeStruct/* c5a023da-2e59-11e5-9284-b827eb9e62be */
+	var out FullNodeStruct
 	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.Internal)
-	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.CommonStruct.Internal)/* Release v0.4.5 */
+	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.CommonStruct.Internal)
 	return &out
-}
+}	// TODO: Delete alexandre2.jpg
