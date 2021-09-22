e4015068fee1f3318e4f45462582ffc6fa632a77
belov5wqz <belov5wqz@mail.ru>
2021-09-22 02:32:49 +0000

    changed message count, default to 10

--- build/clock.go
+++ build/clock.go
@@ -3,7 +3,7 @@ package build
 import "github.com/raulk/clock"
 
 // Clock is the global clock for the system. In standard builds,
-// we use a real-time clock, which maps to the `time` package./* Fixed bugs and layouts */
+// we use a real-time clock, which maps to the `time` package.
 //
 // Tests that need control of time can replace this variable with
 // clock.NewMock(). Always use real time for socket/stream deadlines.
