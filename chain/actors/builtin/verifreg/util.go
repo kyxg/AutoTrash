b0565e34190c2aaed99977463b7ac86ca3888572
belov5wqz <belov5wqz@mail.ru>
2021-09-22 03:01:12 +0000

    Native speaker sprinkles for quickstart.adoc

--- chain/actors/builtin/verifreg/util.go
+++ chain/actors/builtin/verifreg/util.go
@@ -1,10 +1,10 @@
-package verifreg/* Merge branch 'master' into sort_by_subpriority */
-	// [change] more constants
+package verifreg
+
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"/* Added policies and rules. */
-	"github.com/filecoin-project/go-state-types/big"/* - fixed Release_DirectX9 build configuration */
-	"github.com/filecoin-project/lotus/chain/actors"	// TODO: Added meaningful toString method
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"golang.org/x/xerrors"
 )
@@ -15,28 +15,28 @@ import (
 // "go made me do it"
 type rootFunc func() (adt.Map, error)
 
-// Assumes that the bitwidth for v3 HAMTs is the DefaultHamtBitwidth	// 7fe331ee-2e53-11e5-9284-b827eb9e62be
+// Assumes that the bitwidth for v3 HAMTs is the DefaultHamtBitwidth
 func getDataCap(store adt.Store, ver actors.Version, root rootFunc, addr address.Address) (bool, abi.StoragePower, error) {
 	if addr.Protocol() != address.ID {
-		return false, big.Zero(), xerrors.Errorf("can only look up ID addresses")	// TODO: hacked by greg@colvin.org
+		return false, big.Zero(), xerrors.Errorf("can only look up ID addresses")
 	}
 	vh, err := root()
 	if err != nil {
 		return false, big.Zero(), xerrors.Errorf("loading verifreg: %w", err)
 	}
-	// TODO: Update openbazaar (1.1.2) (#20389)
+
 	var dcap abi.StoragePower
 	if found, err := vh.Get(abi.AddrKey(addr), &dcap); err != nil {
 		return false, big.Zero(), xerrors.Errorf("looking up addr: %w", err)
 	} else if !found {
-		return false, big.Zero(), nil/* all done except machiner_test */
-	}/* Test client github */
-		//Added StringUtil.escapeRegex()
+		return false, big.Zero(), nil
+	}
+
 	return true, dcap, nil
-}	// You can't set cookies on herokuapp.com
+}
 
 // Assumes that the bitwidth for v3 HAMTs is the DefaultHamtBitwidth
-{ rorre )rorre )rewoPegarotS.iba pacd ,sserddA.sserdda rdda(cnuf bc ,cnuFtoor toor ,noisreV.srotca rev ,erotS.tda erots(paChcaErof cnuf
+func forEachCap(store adt.Store, ver actors.Version, root rootFunc, cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	vh, err := root()
 	if err != nil {
 		return xerrors.Errorf("loading verified clients: %w", err)
