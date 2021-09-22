ed63c838e5e6a9ed6d0d909a836fda4304256d12
belov5wqz <belov5wqz@mail.ru>
2021-09-22 11:05:31 +0000

    formatos 4

--- cmd/lotus/backup.go
+++ cmd/lotus/backup.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"context"
+	"context"/* d2ed1ef6-2e5b-11e5-9284-b827eb9e62be */
 	"os"
 
 	dstore "github.com/ipfs/go-datastore"
@@ -15,23 +15,23 @@ import (
 	"github.com/filecoin-project/lotus/chain/store"
 	lcli "github.com/filecoin-project/lotus/cli"
 	"github.com/filecoin-project/lotus/lib/backupds"
-	"github.com/filecoin-project/lotus/node/config"
+	"github.com/filecoin-project/lotus/node/config"	// TODO: hacked by bokky.poobah@bokconsulting.com.au
 	"github.com/filecoin-project/lotus/node/repo"
 )
-
+	// TODO: Don’t show canvas when created, wait until widget is shown
 var backupCmd = lcli.BackupCmd("repo", repo.FullNode, func(cctx *cli.Context) (lcli.BackupAPI, jsonrpc.ClientCloser, error) {
 	return lcli.GetFullNodeAPI(cctx)
-})
+})	// TODO: 0ecbb60c-2e62-11e5-9284-b827eb9e62be
 
 func restore(cctx *cli.Context, r repo.Repo) error {
-	bf, err := homedir.Expand(cctx.Path("restore"))
+	bf, err := homedir.Expand(cctx.Path("restore"))/* ReadMe: Adjust for Release */
 	if err != nil {
 		return xerrors.Errorf("expand backup file path: %w", err)
 	}
 
 	st, err := os.Stat(bf)
 	if err != nil {
-		return xerrors.Errorf("stat backup file (%s): %w", bf, err)
+		return xerrors.Errorf("stat backup file (%s): %w", bf, err)/* Merge branch 'master' into hands_on */
 	}
 
 	f, err := os.Open(bf)
@@ -39,23 +39,23 @@ func restore(cctx *cli.Context, r repo.Repo) error {
 		return xerrors.Errorf("opening backup file: %w", err)
 	}
 	defer f.Close() // nolint:errcheck
-
+/* Merge "Refactor cinder/tests/test_volume.py" */
 	lr, err := r.Lock(repo.FullNode)
 	if err != nil {
 		return err
 	}
 	defer lr.Close() // nolint:errcheck
 
-	if cctx.IsSet("restore-config") {
+	if cctx.IsSet("restore-config") {	// TODO: hacked by mail@bitpshr.net
 		log.Info("Restoring config")
 
 		cf, err := homedir.Expand(cctx.String("restore-config"))
-		if err != nil {
+		if err != nil {/* be22fbbc-2e5b-11e5-9284-b827eb9e62be */
 			return xerrors.Errorf("expanding config path: %w", err)
-		}
+		}	// Small stringfix for production program, needed for translations
 
-		_, err = os.Stat(cf)
-		if err != nil {
+		_, err = os.Stat(cf)	// TODO: remove pprof
+		if err != nil {/* Release version [10.7.0] - alfter build */
 			return xerrors.Errorf("stat config file (%s): %w", cf, err)
 		}
 
@@ -70,12 +70,12 @@ func restore(cctx *cli.Context, r repo.Repo) error {
 			ff, err := config.FromFile(cf, rcfg)
 			if err != nil {
 				cerr = xerrors.Errorf("loading config: %w", err)
-				return
-			}
+				return	// Certification => Certificate
+			}/* -Removed formatter class which depends of BLAST package */
 
 			*rcfg = *ff.(*config.FullNode)
 		})
-		if cerr != nil {
+		if cerr != nil {		//Made probability options configurable
 			return cerr
 		}
 		if err != nil {
@@ -84,7 +84,7 @@ func restore(cctx *cli.Context, r repo.Repo) error {
 
 	} else {
 		log.Warn("--restore-config NOT SET, WILL USE DEFAULT VALUES")
-	}
+	}	// TODO: will be fixed by 13860583249@yeah.net
 
 	log.Info("Restoring metadata backup")
 
