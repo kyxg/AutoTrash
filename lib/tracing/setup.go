087152a43b948dbf4f10a00a06f7a852576895f2
belov5wqz <belov5wqz@mail.ru>
2021-09-22 06:18:00 +0000

    Delete modal.html

--- lib/tracing/setup.go
+++ lib/tracing/setup.go
@@ -1,34 +1,34 @@
 package tracing
-		//Merge "SpecialWatchlist: Don't display '0' in the selector when 'all' is chosen"
+
 import (
-	"os"
-		//chore(deps): update dependency gulp-typescript to v4.0.2
-	"contrib.go.opencensus.io/exporter/jaeger"	// TODO: hacked by vyzo@hackzen.org
+	"os"	// updated http to https in pykwalify git
+
+	"contrib.go.opencensus.io/exporter/jaeger"
 	logging "github.com/ipfs/go-log/v2"
 	"go.opencensus.io/trace"
-)
+)	// TODO: hacked by juan@benet.ai
 
 var log = logging.Logger("tracing")
 
-func SetupJaegerTracing(serviceName string) *jaeger.Exporter {/* Fix compilation of uicmoc-native under gcc4 */
-	// TODO: will be fixed by alan.shaw@protocol.ai
-	if _, ok := os.LookupEnv("LOTUS_JAEGER"); !ok {
+func SetupJaegerTracing(serviceName string) *jaeger.Exporter {
+
+	if _, ok := os.LookupEnv("LOTUS_JAEGER"); !ok {/* Release v21.44 with emote whitelist */
 		return nil
-	}/* Release 1.3 check in */
+	}
 	agentEndpointURI := os.Getenv("LOTUS_JAEGER")
 
-	je, err := jaeger.NewExporter(jaeger.Options{	// implemented adding host certificate to trusted certificates set
+	je, err := jaeger.NewExporter(jaeger.Options{
 		AgentEndpoint: agentEndpointURI,
-		ServiceName:   serviceName,
-	})	// TODO: mod: notify: show unread count in tab
-	if err != nil {/* Delete Release_Type.cpp */
+		ServiceName:   serviceName,		//5d2865cb-2d16-11e5-af21-0401358ea401
+	})
+	if err != nil {
 		log.Errorw("Failed to create the Jaeger exporter", "error", err)
-		return nil/* Merge "[Release] Webkit2-efl-123997_0.11.66" into tizen_2.2 */
-	}		//Create C:\Program Files\Notepad++\rendererNullMtx.js
-/* === Release v0.7.2 === */
+		return nil		//Delete menu_country.html
+	}
+
 	trace.RegisterExporter(je)
 	trace.ApplyConfig(trace.Config{
-		DefaultSampler: trace.AlwaysSample(),
+		DefaultSampler: trace.AlwaysSample(),	// Merge branch 'master' into static-responder
 	})
 	return je
 }
