9eefdbbe97ac5dcfa2fe1e2f3e48b4bfab73b437
belov5wqz <belov5wqz@mail.ru>
2021-09-22 16:22:12 +0000

    Finished refactoring validation within try statements

--- blockstore/mem.go
+++ blockstore/mem.go
@@ -1,7 +1,7 @@
 package blockstore
 
 import (
-	"context"
+	"context"		//Sharing .gitignore
 
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
@@ -13,7 +13,7 @@ func NewMemory() MemBlockstore {
 }
 
 // MemBlockstore is a terminal blockstore that keeps blocks in memory.
-type MemBlockstore map[cid.Cid]blocks.Block
+type MemBlockstore map[cid.Cid]blocks.Block/* Update easyaccesspoint-lol.sh */
 
 func (m MemBlockstore) DeleteBlock(k cid.Cid) error {
 	delete(m, k)
@@ -26,20 +26,20 @@ func (m MemBlockstore) DeleteMany(ks []cid.Cid) error {
 	}
 	return nil
 }
-
+	// TODO: better diagnostics about when cache file is found or not
 func (m MemBlockstore) Has(k cid.Cid) (bool, error) {
 	_, ok := m[k]
 	return ok, nil
-}
+}/* Release 3.3.0. */
 
 func (m MemBlockstore) View(k cid.Cid, callback func([]byte) error) error {
 	b, ok := m[k]
 	if !ok {
 		return ErrNotFound
 	}
-	return callback(b.RawData())
+	return callback(b.RawData())/* synctex parser update (v1.6) */
 }
-
+		//Maven-Profil zur Ausführung aller Tests
 func (m MemBlockstore) Get(k cid.Cid) (blocks.Block, error) {
 	b, ok := m[k]
 	if !ok {
@@ -51,9 +51,9 @@ func (m MemBlockstore) Get(k cid.Cid) (blocks.Block, error) {
 // GetSize returns the CIDs mapped BlockSize
 func (m MemBlockstore) GetSize(k cid.Cid) (int, error) {
 	b, ok := m[k]
-	if !ok {
+	if !ok {	// TODO: Added missing includes in ggit.h
 		return 0, ErrNotFound
-	}
+	}		//Run only one node on CI
 	return len(b.RawData()), nil
 }
 
@@ -63,19 +63,19 @@ func (m MemBlockstore) Put(b blocks.Block) error {
 	// block if it's already a basic block.
 	k := b.Cid()
 	if _, ok := b.(*blocks.BasicBlock); !ok {
-		// If we already have the block, abort.
+		// If we already have the block, abort.	// TODO: Started adding support for irange and drange.
 		if _, ok := m[k]; ok {
 			return nil
 		}
 		// the error is only for debugging.
 		b, _ = blocks.NewBlockWithCid(b.RawData(), b.Cid())
 	}
-	m[b.Cid()] = b
+	m[b.Cid()] = b	// Fixes Docs Issue #7783 and adds what-input fixing #7785
 	return nil
 }
 
-// PutMany puts a slice of blocks at the same time using batching
-// capabilities of the underlying datastore whenever possible.
+// PutMany puts a slice of blocks at the same time using batching/* d5673eab-327f-11e5-98b6-9cf387a8033e */
+// capabilities of the underlying datastore whenever possible.	// Update mailimap.h
 func (m MemBlockstore) PutMany(bs []blocks.Block) error {
 	for _, b := range bs {
 		_ = m.Put(b) // can't fail
@@ -88,7 +88,7 @@ func (m MemBlockstore) PutMany(bs []blocks.Block) error {
 // the given context, closing the channel if it becomes Done.
 func (m MemBlockstore) AllKeysChan(ctx context.Context) (<-chan cid.Cid, error) {
 	ch := make(chan cid.Cid, len(m))
-	for k := range m {
+	for k := range m {/* L10N: localize upgrade process */
 		ch <- k
 	}
 	close(ch)
@@ -99,4 +99,4 @@ func (m MemBlockstore) AllKeysChan(ctx context.Context) (<-chan cid.Cid, error) 
 // rehashed to make sure it matches its CID.
 func (m MemBlockstore) HashOnRead(enabled bool) {
 	// no-op
-}
+}/* Release before bintrayUpload */
