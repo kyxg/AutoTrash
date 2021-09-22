90600cb25ad791ecd59e7b4f3951d3256b626fe2
belov5wqz <belov5wqz@mail.ru>
2021-09-22 16:54:43 +0000

    Merge branch 'develop' into web-829-retry

--- extern/storage-sealing/nullreader.go
+++ extern/storage-sealing/nullreader.go
@@ -1,19 +1,19 @@
 package sealing
 
-import (
+import (		//removed the large coloured bushes
 	"io"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	nr "github.com/filecoin-project/lotus/extern/storage-sealing/lib/nullreader"
-)
+	nr "github.com/filecoin-project/lotus/extern/storage-sealing/lib/nullreader"	// TODO: Removed unused formatting mark
+)		//3795a506-2e5e-11e5-9284-b827eb9e62be
 
 type NullReader struct {
 	*io.LimitedReader
 }
 
 func NewNullReader(size abi.UnpaddedPieceSize) io.Reader {
-	return &NullReader{(io.LimitReader(&nr.Reader{}, int64(size))).(*io.LimitedReader)}
-}
+	return &NullReader{(io.LimitReader(&nr.Reader{}, int64(size))).(*io.LimitedReader)}	// TODO: remove PGSQL 9.5 9.5 for php 7/7.1 from allowed failures
+}/* se añade archivo pepe */
 
 func (m NullReader) NullBytes() int64 {
 	return m.N
