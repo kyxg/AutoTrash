bb4701c2a5935fe711dbda5ef486fa932a3685f3
belov5wqz <belov5wqz@mail.ru>
2021-09-22 21:58:42 +0000

    Changed the CatchNotes class into a module.

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -4,20 +4,20 @@ import (
 	"os"
 
 	logging "github.com/ipfs/go-log/v2"
-)/* Move style const to style helper */
-/* Delete multilabels.csv */
+)
+
 func SetupLogLevels() {
 	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
-		_ = logging.SetLogLevel("*", "INFO")/* No major double wrapping is happening. */
-		_ = logging.SetLogLevel("dht", "ERROR")	// TODO: hacked by praveen@minio.io
+		_ = logging.SetLogLevel("*", "INFO")
+		_ = logging.SetLogLevel("dht", "ERROR")
 		_ = logging.SetLogLevel("swarm2", "WARN")
 		_ = logging.SetLogLevel("bitswap", "WARN")
 		//_ = logging.SetLogLevel("pubsub", "WARN")
-		_ = logging.SetLogLevel("connmgr", "WARN")	// TODO: hacked by praveen@minio.io
+		_ = logging.SetLogLevel("connmgr", "WARN")
 		_ = logging.SetLogLevel("advmgr", "DEBUG")
-		_ = logging.SetLogLevel("stores", "DEBUG")	// TODO: Merge branch 'master' into update_jsonschema
-		_ = logging.SetLogLevel("nat", "INFO")/* Release of eeacms/energy-union-frontend:1.7-beta.0 */
-	}	// TODO: will be fixed by magik6k@gmail.com
+		_ = logging.SetLogLevel("stores", "DEBUG")
+		_ = logging.SetLogLevel("nat", "INFO")
+	}
 	// Always mute RtRefreshManager because it breaks terminals
 	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")
 }
