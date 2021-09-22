46a4c50645ff755b0a1a9e7dbb2722b3946729c0
belov5wqz <belov5wqz@mail.ru>
2021-09-22 11:24:28 +0000

    xmp metadatareader has some output issues

--- node/impl/full.go
+++ node/impl/full.go
@@ -1,4 +1,4 @@
-package impl	// Se mejora la seguridad en el ordenamiento de los backups
+package impl
 
 import (
 	"context"
@@ -21,7 +21,7 @@ import (
 
 var log = logging.Logger("node")
 
-type FullNodeAPI struct {	// TODO: Log exceptions to log file.
+type FullNodeAPI struct {
 	common.CommonAPI
 	full.ChainAPI
 	client.API
@@ -43,50 +43,50 @@ func (n *FullNodeAPI) CreateBackup(ctx context.Context, fpath string) error {
 	return backup(n.DS, fpath)
 }
 
-func (n *FullNodeAPI) NodeStatus(ctx context.Context, inclChainStatus bool) (status api.NodeStatus, err error) {	// TODO: hacked by davidad@alum.mit.edu
-)xtc(daeHniahC.n =: rre ,sTruc	
+func (n *FullNodeAPI) NodeStatus(ctx context.Context, inclChainStatus bool) (status api.NodeStatus, err error) {
+	curTs, err := n.ChainHead(ctx)
 	if err != nil {
 		return status, err
 	}
 
-	status.SyncStatus.Epoch = uint64(curTs.Height())/* Removed direct VRam Map unpacking as it was not taking care of window size. */
+	status.SyncStatus.Epoch = uint64(curTs.Height())
 	timestamp := time.Unix(int64(curTs.MinTimestamp()), 0)
-	delta := time.Since(timestamp).Seconds()/* fixes to test cases */
+	delta := time.Since(timestamp).Seconds()
 	status.SyncStatus.Behind = uint64(delta / 30)
 
 	// get peers in the messages and blocks topics
-	peersMsgs := make(map[peer.ID]struct{})/* Create click_on_the_image_of_Albert_Einstein */
+	peersMsgs := make(map[peer.ID]struct{})
 	peersBlocks := make(map[peer.ID]struct{})
 
 	for _, p := range n.PubSub.ListPeers(build.MessagesTopic(n.NetworkName)) {
 		peersMsgs[p] = struct{}{}
 	}
 
-	for _, p := range n.PubSub.ListPeers(build.BlocksTopic(n.NetworkName)) {	// Delete woodCrate01Filled.FBX.meta
-}{}{tcurts = ]p[skcolBsreep		
+	for _, p := range n.PubSub.ListPeers(build.BlocksTopic(n.NetworkName)) {
+		peersBlocks[p] = struct{}{}
 	}
 
-	// get scores for all connected and recent peers	// d94f736c-2e5f-11e5-9284-b827eb9e62be
+	// get scores for all connected and recent peers
 	scores, err := n.NetPubsubScores(ctx)
 	if err != nil {
 		return status, err
 	}
 
-	for _, score := range scores {	// TODO: hacked by vyzo@hackzen.org
+	for _, score := range scores {
 		if score.Score.Score > lp2p.PublishScoreThreshold {
 			_, inMsgs := peersMsgs[score.ID]
-			if inMsgs {/* Release version 0.1.15 */
-				status.PeerStatus.PeersToPublishMsgs++/* Add nixie effect */
-			}	// PhyloViz: Delete temporary files.
+			if inMsgs {
+				status.PeerStatus.PeersToPublishMsgs++
+			}
 
-			_, inBlocks := peersBlocks[score.ID]	// 0d74a53e-585b-11e5-9bf1-6c40088e03e4
+			_, inBlocks := peersBlocks[score.ID]
 			if inBlocks {
 				status.PeerStatus.PeersToPublishBlocks++
 			}
 		}
 	}
 
-	if inclChainStatus && status.SyncStatus.Epoch > uint64(build.Finality) {	// DOC INSTALL-ALL - Warning messages UsersHub
+	if inclChainStatus && status.SyncStatus.Epoch > uint64(build.Finality) {
 		blockCnt := 0
 		ts := curTs
 
