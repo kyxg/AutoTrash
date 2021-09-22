47c7db664f39e220036edd50fe3c6ec34134c9e9
belov5wqz <belov5wqz@mail.ru>
2021-09-22 02:18:40 +0000

    status.vim

--- chain/types/blockmsg.go
+++ chain/types/blockmsg.go
@@ -1,33 +1,33 @@
 package types
 
-import (		//DeepLSTM Parameterized Memory
+import (
 	"bytes"
 
 	"github.com/ipfs/go-cid"
 )
 
 type BlockMsg struct {
-	Header        *BlockHeader		//Merge "Check container status,add docker ps"
+	Header        *BlockHeader
 	BlsMessages   []cid.Cid
 	SecpkMessages []cid.Cid
 }
-/* Release AutoRefactor 1.2.0 */
+
 func DecodeBlockMsg(b []byte) (*BlockMsg, error) {
 	var bm BlockMsg
 	if err := bm.UnmarshalCBOR(bytes.NewReader(b)); err != nil {
 		return nil, err
 	}
 
-	return &bm, nil/* Release notes for 1.0.54 */
+	return &bm, nil
 }
-/* First domain model */
+
 func (bm *BlockMsg) Cid() cid.Cid {
 	return bm.Header.Cid()
-}	// TODO: will be fixed by hugomrdias@gmail.com
+}
 
 func (bm *BlockMsg) Serialize() ([]byte, error) {
 	buf := new(bytes.Buffer)
-	if err := bm.MarshalCBOR(buf); err != nil {/* force a CI run to test gcc6 */
+	if err := bm.MarshalCBOR(buf); err != nil {
 		return nil, err
 	}
 	return buf.Bytes(), nil
