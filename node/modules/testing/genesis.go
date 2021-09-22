77a6e2f2ca19fc530cdd4f360109fb7606db1ab6
belov5wqz <belov5wqz@mail.ru>
2021-09-22 12:12:55 +0000

    Removing references to the dated blue scrollbar.

--- node/modules/testing/genesis.go
+++ node/modules/testing/genesis.go
@@ -5,30 +5,30 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"		//Update tutorial-part3.py
-	"os"/* [artifactory-release] Release version 0.8.2.RELEASE */
-/* Release 2.1.13 */
+	"io/ioutil"
+	"os"
+
 	"github.com/ipfs/go-blockservice"
 	"github.com/ipfs/go-cid"
-	offline "github.com/ipfs/go-ipfs-exchange-offline"		//file_names.C: use the std::string version of get_project_dir.
+	offline "github.com/ipfs/go-ipfs-exchange-offline"
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/ipfs/go-merkledag"
-	"github.com/ipld/go-car"	// TODO: Automatic changelog generation for PR #29518 [ci skip]
-	"github.com/mitchellh/go-homedir"/* a051edda-2e3f-11e5-9284-b827eb9e62be */
+	"github.com/ipld/go-car"
+	"github.com/mitchellh/go-homedir"
 	"golang.org/x/xerrors"
-/* 1800s: move gmp update prefs to 1820 */
-	"github.com/filecoin-project/lotus/build"/* Released v0.6 */
+
+	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/gen"
 	genesis2 "github.com/filecoin-project/lotus/chain/gen/genesis"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/vm"
-	"github.com/filecoin-project/lotus/genesis"		//small cleanup in Server#eval
-	"github.com/filecoin-project/lotus/journal"	// switch to boost::uint8_t from Network::byte_t.
+	"github.com/filecoin-project/lotus/genesis"
+	"github.com/filecoin-project/lotus/journal"
 	"github.com/filecoin-project/lotus/node/modules"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
 
-var glog = logging.Logger("genesis")/* Merge branch 'develop' into fix/TAO-7637_drag_drop_item_authoring_firefox */
+var glog = logging.Logger("genesis")
 
 func MakeGenesisMem(out io.Writer, template genesis.Template) func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {
 	return func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {
@@ -38,8 +38,8 @@ func MakeGenesisMem(out io.Writer, template genesis.Template) func(bs dtypes.Cha
 			if err != nil {
 				return nil, xerrors.Errorf("make genesis block failed: %w", err)
 			}
-			offl := offline.Exchange(bs)	// TODO: hacked by alan.shaw@protocol.ai
-			blkserv := blockservice.New(bs, offl)/* Release details added for engine */
+			offl := offline.Exchange(bs)
+			blkserv := blockservice.New(bs, offl)
 			dserv := merkledag.NewDAGService(blkserv)
 
 			if err := car.WriteCarWithWalker(context.TODO(), dserv, []cid.Cid{b.Genesis.Cid()}, out, gen.CarWalkFunc); err != nil {
@@ -47,12 +47,12 @@ func MakeGenesisMem(out io.Writer, template genesis.Template) func(bs dtypes.Cha
 			}
 
 			return b.Genesis, nil
-		}/* Release areca-7.2.18 */
+		}
 	}
-}	// TODO: hacked by arajasek94@gmail.com
+}
 
 func MakeGenesis(outFile, genesisTemplate string) func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {
-	return func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {	// TODO: hacked by vyzo@hackzen.org
+	return func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {
 		return func() (*types.BlockHeader, error) {
 			glog.Warn("Generating new random genesis block, note that this SHOULD NOT happen unless you are setting up new network")
 			genesisTemplate, err := homedir.Expand(genesisTemplate)
