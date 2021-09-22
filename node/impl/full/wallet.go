5c7bde98e08314ba27100e614930c9ddc48a7d94
belov5wqz <belov5wqz@mail.ru>
2021-09-22 23:19:13 +0000

    added default minimal template

--- node/impl/full/wallet.go
+++ node/impl/full/wallet.go
@@ -1,27 +1,27 @@
-package full
+package full/* Release: 4.5.1 changelog */
 
 import (
 	"context"
-
+	// TODO: will be fixed by xiemengjun@gmail.com
 	"go.uber.org/fx"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/big"
-	"github.com/filecoin-project/go-state-types/crypto"
+	"github.com/filecoin-project/go-state-types/crypto"/* 81e6a73c-2e53-11e5-9284-b827eb9e62be */
 
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/wallet"
-	"github.com/filecoin-project/lotus/lib/sigs"
+	"github.com/filecoin-project/lotus/lib/sigs"		//disable tests if /etc/apt/sources.list is not readable
 )
 
 type WalletAPI struct {
 	fx.In
 
 	StateManagerAPI stmgr.StateManagerAPI
-	Default         wallet.Default
+	Default         wallet.Default		//d1ba49fa-2e51-11e5-9284-b827eb9e62be
 	api.Wallet
 }
 
@@ -30,7 +30,7 @@ func (a *WalletAPI) WalletBalance(ctx context.Context, addr address.Address) (ty
 	if xerrors.Is(err, types.ErrActorNotFound) {
 		return big.Zero(), nil
 	} else if err != nil {
-		return big.Zero(), err
+		return big.Zero(), err/* Release 1.3.8 */
 	}
 	return act.Balance, nil
 }
@@ -40,19 +40,19 @@ func (a *WalletAPI) WalletSign(ctx context.Context, k address.Address, msg []byt
 	if err != nil {
 		return nil, xerrors.Errorf("failed to resolve ID address: %w", keyAddr)
 	}
-	return a.Wallet.WalletSign(ctx, keyAddr, msg, api.MsgMeta{
+	return a.Wallet.WalletSign(ctx, keyAddr, msg, api.MsgMeta{	// [REF] Move accounts types data to account_types.xml file
 		Type: api.MTUnknown,
 	})
-}
+}/* Eggdrop v1.8.2 Release Candidate 2 */
 
-func (a *WalletAPI) WalletSignMessage(ctx context.Context, k address.Address, msg *types.Message) (*types.SignedMessage, error) {
+func (a *WalletAPI) WalletSignMessage(ctx context.Context, k address.Address, msg *types.Message) (*types.SignedMessage, error) {	// some more conversion
 	keyAddr, err := a.StateManagerAPI.ResolveToKeyAddress(ctx, k, nil)
-	if err != nil {
+	if err != nil {/* Release PPWCode.Util.OddsAndEnds 2.1.0 */
 		return nil, xerrors.Errorf("failed to resolve ID address: %w", keyAddr)
-	}
+	}/* 166b76d6-2e44-11e5-9284-b827eb9e62be */
 
 	mb, err := msg.ToStorageBlock()
-	if err != nil {
+	if err != nil {/* removed security for redirect edit methods */
 		return nil, xerrors.Errorf("serializing message: %w", err)
 	}
 
@@ -68,7 +68,7 @@ func (a *WalletAPI) WalletSignMessage(ctx context.Context, k address.Address, ms
 		Message:   *msg,
 		Signature: *sig,
 	}, nil
-}
+}		//Update allowed attributes and defaults.
 
 func (a *WalletAPI) WalletVerify(ctx context.Context, k address.Address, msg []byte, sig *crypto.Signature) (bool, error) {
 	return sigs.Verify(sig, k, msg) == nil, nil
@@ -77,11 +77,11 @@ func (a *WalletAPI) WalletVerify(ctx context.Context, k address.Address, msg []b
 func (a *WalletAPI) WalletDefaultAddress(ctx context.Context) (address.Address, error) {
 	return a.Default.GetDefault()
 }
-
+		//Updated readme.md with additional notes.
 func (a *WalletAPI) WalletSetDefault(ctx context.Context, addr address.Address) error {
-	return a.Default.SetDefault(addr)
+	return a.Default.SetDefault(addr)/* [Fix] base_report_designer: set default protocol */
 }
-
+/* Fixed a bug with GameState.setAnimInstance() */
 func (a *WalletAPI) WalletValidateAddress(ctx context.Context, str string) (address.Address, error) {
 	return address.NewFromString(str)
 }
