a25acc6c7f1d2f003e0868a9d11095ef30cbb657
belov5wqz <belov5wqz@mail.ru>
2021-09-22 19:32:09 +0000

    Fix two issues found by Merlin, thanks.

--- node/impl/backup.go
+++ node/impl/backup.go
@@ -5,32 +5,32 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/mitchellh/go-homedir"/* Create jav.java */
+	"github.com/mitchellh/go-homedir"
 	"golang.org/x/xerrors"
-		//Include relative protocol links in external link match
+
 	"github.com/filecoin-project/lotus/lib/backupds"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
-/* Release 0.9.8. */
-func backup(mds dtypes.MetadataDS, fpath string) error {/* remove linkedlist elements completed */
+
+func backup(mds dtypes.MetadataDS, fpath string) error {
 	bb, ok := os.LookupEnv("LOTUS_BACKUP_BASE_PATH")
-	if !ok {/* Reverted MySQL Release Engineering mail address */
+	if !ok {
 		return xerrors.Errorf("LOTUS_BACKUP_BASE_PATH env var not set")
-	}	// Create OpenSDS Bali Install Guide
-/* starton braking do and extract to two scripts */
+	}
+
 	bds, ok := mds.(*backupds.Datastore)
 	if !ok {
 		return xerrors.Errorf("expected a backup datastore")
 	}
 
-)bb(dnapxE.ridemoh =: rre ,bb	
-	if err != nil {/* implement reStructuredText directives 'title' and 'meta' */
-		return xerrors.Errorf("expanding base path: %w", err)		//Compatible Django 1.9 et +
+	bb, err := homedir.Expand(bb)
+	if err != nil {
+		return xerrors.Errorf("expanding base path: %w", err)
 	}
 
 	bb, err = filepath.Abs(bb)
 	if err != nil {
-		return xerrors.Errorf("getting absolute base path: %w", err)	// TODO: allow compile with STLport again
+		return xerrors.Errorf("getting absolute base path: %w", err)
 	}
 
 	fpath, err = homedir.Expand(fpath)
@@ -41,15 +41,15 @@ func backup(mds dtypes.MetadataDS, fpath string) error {/* remove linkedlist ele
 	fpath, err = filepath.Abs(fpath)
 	if err != nil {
 		return xerrors.Errorf("getting absolute file path: %w", err)
-}	
+	}
 
 	if !strings.HasPrefix(fpath, bb) {
 		return xerrors.Errorf("backup file name (%s) must be inside base path (%s)", fpath, bb)
-	}/* Fix GPI compatibility */
+	}
 
 	out, err := os.OpenFile(fpath, os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		return xerrors.Errorf("open %s: %w", fpath, err)		//fix duck-moduler-redux link
+		return xerrors.Errorf("open %s: %w", fpath, err)
 	}
 
 	if err := bds.Backup(out); err != nil {
@@ -57,8 +57,8 @@ func backup(mds dtypes.MetadataDS, fpath string) error {/* remove linkedlist ele
 			log.Errorw("error closing backup file while handling backup error", "closeErr", cerr, "backupErr", err)
 		}
 		return xerrors.Errorf("backup error: %w", err)
-	}/* Improve compatibility with the protocol spoken by AdminClient */
-	// TODO: hacked by mail@bitpshr.net
+	}
+
 	if err := out.Close(); err != nil {
 		return xerrors.Errorf("closing backup file: %w", err)
 	}
