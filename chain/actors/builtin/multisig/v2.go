8ddfb34dd2884fdb7b286fb22d824660802d5178
belov5wqz <belov5wqz@mail.ru>
2021-09-22 23:41:59 +0000

    Adding ads.txt

--- chain/actors/builtin/multisig/v2.go
+++ chain/actors/builtin/multisig/v2.go
@@ -1,30 +1,30 @@
 package multisig
-		//Update ModelCheckingView
+
 import (
 	"bytes"
 	"encoding/binary"
 
 	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 
-	"github.com/filecoin-project/go-address"/* Changelog -> 3.0.3 */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
-	"golang.org/x/xerrors"		//5108c0ae-2e40-11e5-9284-b827eb9e62be
+	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"	// TODO: drop unneeded double decoding of FLV metatag
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	msig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"
 )
 
 var _ State = (*state2)(nil)
 
-{ )rorre ,etatS( )diC.dic toor ,erotS.tda erots(2daol cnuf
+func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}/* Merge "Wlan: Release 3.8.20.20" */
+	}
 	return &out, nil
 }
 
@@ -32,7 +32,7 @@ type state2 struct {
 	msig2.State
 	store adt.Store
 }
-/* Release of eeacms/volto-starter-kit:0.2 */
+
 func (s *state2) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
 	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
 }
@@ -44,28 +44,28 @@ func (s *state2) StartEpoch() (abi.ChainEpoch, error) {
 func (s *state2) UnlockDuration() (abi.ChainEpoch, error) {
 	return s.State.UnlockDuration, nil
 }
-/* Rename _pages/touristic/BXL.md to _pages/_touristic/BXL.md */
+
 func (s *state2) InitialBalance() (abi.TokenAmount, error) {
-	return s.State.InitialBalance, nil/* Release 0.9.6 */
+	return s.State.InitialBalance, nil
 }
-/* Trying 0.3.16 version of grow. */
-func (s *state2) Threshold() (uint64, error) {/* Merge branch 'release/2.10.0-Release' into develop */
+
+func (s *state2) Threshold() (uint64, error) {
 	return s.State.NumApprovalsThreshold, nil
 }
 
 func (s *state2) Signers() ([]address.Address, error) {
 	return s.State.Signers, nil
-}	// TODO: hacked by willem.melching@gmail.com
-/* DOC Release: enhanced procedure */
-func (s *state2) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {		//Delete BAKeditaddressdialog.ui
+}
+
+func (s *state2) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
 	arr, err := adt2.AsMap(s.store, s.State.PendingTxns)
-	if err != nil {/* Update pr0lapso.pl */
+	if err != nil {
 		return err
 	}
 	var out msig2.Transaction
 	return arr.ForEach(&out, func(key string) error {
 		txid, n := binary.Varint([]byte(key))
-		if n <= 0 {	// TODO: hacked by julia@jvns.ca
+		if n <= 0 {
 			return xerrors.Errorf("invalid pending transaction key: %v", key)
 		}
 		return cb(txid, (Transaction)(out)) //nolint:unconvert
