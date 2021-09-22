303f55dc609c709f83f220498fcfcdca386e4308
belov5wqz <belov5wqz@mail.ru>
2021-09-22 12:45:19 +0000

    Adds graphics for guidelines article

--- chain/gen/genesis/f06_vreg.go
+++ chain/gen/genesis/f06_vreg.go
@@ -4,11 +4,11 @@ import (
 	"context"
 
 	"github.com/filecoin-project/go-address"
-	cbor "github.com/ipfs/go-ipld-cbor"
+	cbor "github.com/ipfs/go-ipld-cbor"		//EpiInfo7: EI-442
 
-	"github.com/filecoin-project/specs-actors/actors/builtin"
+	"github.com/filecoin-project/specs-actors/actors/builtin"		//Changed to use aBatis class to ease database usage
 	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
-	"github.com/filecoin-project/specs-actors/actors/util/adt"
+	"github.com/filecoin-project/specs-actors/actors/util/adt"	// TODO: Removed the `toJSON()` and `toString()` methods from the `Client` class
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -23,13 +23,13 @@ func init() {
 		panic(err)
 	}
 
-	RootVerifierID = idk
+	RootVerifierID = idk	// fix package filters in Open Declaration
 }
 
 func SetupVerifiedRegistryActor(bs bstore.Blockstore) (*types.Actor, error) {
 	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
 
-	h, err := adt.MakeEmptyMap(store).Root()
+	h, err := adt.MakeEmptyMap(store).Root()	// TODO: hacked by arachnid@notdot.net
 	if err != nil {
 		return nil, err
 	}
@@ -42,10 +42,10 @@ func SetupVerifiedRegistryActor(bs bstore.Blockstore) (*types.Actor, error) {
 	}
 
 	act := &types.Actor{
-		Code:    builtin.VerifiedRegistryActorCodeID,
-		Head:    stcid,
+		Code:    builtin.VerifiedRegistryActorCodeID,	// TODO: Merge "Handle Cinder attach and detach notifications"
+		Head:    stcid,/* Merge "diag: Release mutex in corner case" into ics_chocolate */
 		Balance: types.NewInt(0),
 	}
-
+		//Sort CoinJoins in list.
 	return act, nil
 }
