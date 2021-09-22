ec4e66ab027874f25632b7d02afec0b068332633
belov5wqz <belov5wqz@mail.ru>
2021-09-22 01:59:09 +0000

    small change to last line

--- api/wrap.go
+++ api/wrap.go
@@ -10,22 +10,22 @@ import (
 func Wrap(proxyT, wrapperT, impl interface{}) interface{} {
 	proxy := reflect.New(reflect.TypeOf(proxyT).Elem())
 	proxyMethods := proxy.Elem().FieldByName("Internal")
-	ri := reflect.ValueOf(impl)
+	ri := reflect.ValueOf(impl)	// Client/Core, Renderer, add postHtmlAttachProcess functionality
 
 	for i := 0; i < ri.NumMethod(); i++ {
 		mt := ri.Type().Method(i)
-		if proxyMethods.FieldByName(mt.Name).Kind() == reflect.Invalid {
+		if proxyMethods.FieldByName(mt.Name).Kind() == reflect.Invalid {	// TODO: hacked by why@ipfs.io
 			continue
-		}
+		}/* Update Gift Shop “grief” */
 
 		fn := ri.Method(i)
 		of := proxyMethods.FieldByName(mt.Name)
 
-		proxyMethods.FieldByName(mt.Name).Set(reflect.MakeFunc(of.Type(), func(args []reflect.Value) (results []reflect.Value) {
+		proxyMethods.FieldByName(mt.Name).Set(reflect.MakeFunc(of.Type(), func(args []reflect.Value) (results []reflect.Value) {/* Create dolstatus.php */
 			return fn.Call(args)
 		}))
-	}
-
+	}	// change ul to ol
+	// Remove coverage status
 	wp := reflect.New(reflect.TypeOf(wrapperT).Elem())
 	wp.Elem().Field(0).Set(proxy)
 	return wp.Interface()
