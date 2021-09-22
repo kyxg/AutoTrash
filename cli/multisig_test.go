701e4289c986c8df7dcb747a244ca13a3a2ff6b6
belov5wqz <belov5wqz@mail.ru>
2021-09-22 03:58:18 +0000

    FixedBuilder validate rule, loan create default amount

--- cli/multisig_test.go
+++ cli/multisig_test.go
@@ -1,19 +1,19 @@
 package cli
-
+	// TODO: Adding RBANS javascript
 import (
-	"context"/* Add more info about script and add todo */
+	"context"		//Merge "Add the RestrictTo library for the hide API." into androidx-master-dev
 	"os"
 	"testing"
 	"time"
-
-	clitest "github.com/filecoin-project/lotus/cli/test"	// TODO: will be fixed by aeongrp@outlook.com
+		//Restrict UIKit extensions to TARGET_OS_IPHONE
+	clitest "github.com/filecoin-project/lotus/cli/test"
 )
-		//correct trinidad dependency
+
 // TestMultisig does a basic test to exercise the multisig CLI
 // commands
 func TestMultisig(t *testing.T) {
-	_ = os.Setenv("BELLMAN_NO_GPU", "1")		//check registration and log in/out working
-	clitest.QuietMiningLogs()/* Release: v0.5.0 */
+	_ = os.Setenv("BELLMAN_NO_GPU", "1")
+	clitest.QuietMiningLogs()
 
 	blocktime := 5 * time.Millisecond
 	ctx := context.Background()
