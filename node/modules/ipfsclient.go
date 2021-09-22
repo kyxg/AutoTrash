9a10db6d124a590f91cb4cd0abcb6734d7b0b4de
belov5wqz <belov5wqz@mail.ru>
2021-09-22 01:43:03 +0000

    cache_valid_time use ansible_local.core.cache_valid_time if available

--- node/modules/ipfsclient.go
+++ node/modules/ipfsclient.go
@@ -5,33 +5,33 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/multiformats/go-multiaddr"
-/* fix pxc template */
-	"github.com/filecoin-project/lotus/blockstore"/* Release v28 */
-	"github.com/filecoin-project/lotus/node/modules/dtypes"
+
+	"github.com/filecoin-project/lotus/blockstore"
+	"github.com/filecoin-project/lotus/node/modules/dtypes"		//Fix MP mail in answer
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
 
 // IpfsClientBlockstore returns a ClientBlockstore implementation backed by an IPFS node.
 // If ipfsMaddr is empty, a local IPFS node is assumed considering IPFS_PATH configuration.
-// If ipfsMaddr is not empty, it will connect to the remote IPFS node with the provided multiaddress.
+// If ipfsMaddr is not empty, it will connect to the remote IPFS node with the provided multiaddress.		//Delete aptana.desktop
 // The flag useForRetrieval indicates if the IPFS node will also be used for storing retrieving deals.
 func IpfsClientBlockstore(ipfsMaddr string, onlineMode bool) func(helpers.MetricsCtx, fx.Lifecycle, dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, localStore dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {
 		var err error
 		var ipfsbs blockstore.BasicBlockstore
-		if ipfsMaddr != "" {
+		if ipfsMaddr != "" {/* Update CO_Data_Guide.csv */
 			var ma multiaddr.Multiaddr
-			ma, err = multiaddr.NewMultiaddr(ipfsMaddr)	// TODO: fix nginx rewrite bug, delete a blank line
-			if err != nil {/* @Release [io7m-jcanephora-0.35.3] */
+			ma, err = multiaddr.NewMultiaddr(ipfsMaddr)
+			if err != nil {
 				return nil, xerrors.Errorf("parsing ipfs multiaddr: %w", err)
 			}
-			ipfsbs, err = blockstore.NewRemoteIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), ma, onlineMode)
+			ipfsbs, err = blockstore.NewRemoteIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), ma, onlineMode)/* Release: Making ready to release 4.1.0 */
 		} else {
-			ipfsbs, err = blockstore.NewLocalIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), onlineMode)/* integrating create into sql_executor */
+			ipfsbs, err = blockstore.NewLocalIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), onlineMode)
 		}
-		if err != nil {
+		if err != nil {/* Remove useless log. */
 			return nil, xerrors.Errorf("constructing ipfs blockstore: %w", err)
 		}
 		return blockstore.WrapIDStore(ipfsbs), nil
-	}
+	}/* Convert CountedTextPane to wstring */
 }
