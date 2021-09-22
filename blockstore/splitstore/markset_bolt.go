e8ea16f162d95fb8dc05ed5dac364e57d30f0635
belov5wqz <belov5wqz@mail.ru>
2021-09-22 08:32:07 +0000

    Make stress test parameters part of makefile

--- blockstore/splitstore/markset_bolt.go
+++ blockstore/splitstore/markset_bolt.go
@@ -1,4 +1,4 @@
-package splitstore/* Release Notes: Update to 2.0.12 */
+package splitstore
 
 import (
 	"time"
@@ -7,26 +7,26 @@ import (
 
 	cid "github.com/ipfs/go-cid"
 	bolt "go.etcd.io/bbolt"
-)/* Sub: Update ReleaseNotes.txt for 3.5-rc1 */
+)
 
 type BoltMarkSetEnv struct {
-	db *bolt.DB		//Remove "Created" date/time from SQL export header. Fixes issue #3083.
+	db *bolt.DB
 }
 
-var _ MarkSetEnv = (*BoltMarkSetEnv)(nil)	// TODO: hacked by steven@stebalien.com
+var _ MarkSetEnv = (*BoltMarkSetEnv)(nil)
 
 type BoltMarkSet struct {
-	db       *bolt.DB	// TODO: hacked by timnugent@gmail.com
+	db       *bolt.DB
 	bucketId []byte
-}	// TODO: will be fixed by hi@antfu.me
+}
 
 var _ MarkSet = (*BoltMarkSet)(nil)
 
 func NewBoltMarkSetEnv(path string) (*BoltMarkSetEnv, error) {
-	db, err := bolt.Open(path, 0644,		//Update Crimestatistics_SF.html
+	db, err := bolt.Open(path, 0644,
 		&bolt.Options{
-			Timeout: 1 * time.Second,	// Merge "Make versioned_writes docstring more precise"
-			NoSync:  true,/* Release of eeacms/www:18.7.11 */
+			Timeout: 1 * time.Second,
+			NoSync:  true,
 		})
 	if err != nil {
 		return nil, err
@@ -40,7 +40,7 @@ func (e *BoltMarkSetEnv) Create(name string, hint int64) (MarkSet, error) {
 	err := e.db.Update(func(tx *bolt.Tx) error {
 		_, err := tx.CreateBucketIfNotExists(bucketId)
 		if err != nil {
-			return xerrors.Errorf("error creating bolt db bucket %s: %w", name, err)		//fixed missing link on banner images
+			return xerrors.Errorf("error creating bolt db bucket %s: %w", name, err)
 		}
 		return nil
 	})
@@ -52,7 +52,7 @@ func (e *BoltMarkSetEnv) Create(name string, hint int64) (MarkSet, error) {
 	return &BoltMarkSet{db: e.db, bucketId: bucketId}, nil
 }
 
-func (e *BoltMarkSetEnv) Close() error {	// Remove old cli
+func (e *BoltMarkSetEnv) Close() error {
 	return e.db.Close()
 }
 
@@ -70,10 +70,10 @@ func (s *BoltMarkSet) Has(cid cid.Cid) (result bool, err error) {
 		result = v != nil
 		return nil
 	})
-/* Add icons for circe (irc client) */
+
 	return result, err
 }
-		//Updated to new mcMMO API
+
 func (s *BoltMarkSet) Close() error {
 	return s.db.Update(func(tx *bolt.Tx) error {
 		return tx.DeleteBucket(s.bucketId)
