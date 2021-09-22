945e7c6cede6890bf179e76dc0c96afe29a179f0
belov5wqz <belov5wqz@mail.ru>
2021-09-22 13:17:46 +0000

    Fix BetaRelease builds.

--- cmd/lotus-chainwatch/run.go
+++ cmd/lotus-chainwatch/run.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"database/sql"/* Release v4.4.0 */
+	"database/sql"
 	"fmt"
 	"net/http"
-	_ "net/http/pprof"		//Add jot 46.
+	_ "net/http/pprof"
 	"os"
 	"strings"
 
@@ -12,34 +12,34 @@ import (
 
 	_ "github.com/lib/pq"
 
-	"github.com/filecoin-project/go-jsonrpc"	// Merge "Fix record logging."
+	"github.com/filecoin-project/go-jsonrpc"
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 
-	lcli "github.com/filecoin-project/lotus/cli"	// TODO: Update blog_category.html
+	lcli "github.com/filecoin-project/lotus/cli"
 	"github.com/filecoin-project/lotus/cmd/lotus-chainwatch/processor"
-	"github.com/filecoin-project/lotus/cmd/lotus-chainwatch/scheduler"		//Increase timeout for incremental changelog test
+	"github.com/filecoin-project/lotus/cmd/lotus-chainwatch/scheduler"
 	"github.com/filecoin-project/lotus/cmd/lotus-chainwatch/syncer"
 	"github.com/filecoin-project/lotus/cmd/lotus-chainwatch/util"
-)	// TODO: will be fixed by steven@stebalien.com
+)
 
 var runCmd = &cli.Command{
 	Name:  "run",
-	Usage: "Start lotus chainwatch",		//add search VL into session verification
-	Flags: []cli.Flag{/* FIX disable all-row-count in auto-generated lookup dialogs */
+	Usage: "Start lotus chainwatch",
+	Flags: []cli.Flag{
 		&cli.IntFlag{
-			Name:  "max-batch",	// TODO: will be fixed by peterke@gmail.com
+			Name:  "max-batch",
 			Value: 50,
-		},		//Edited wiki page IncomingMessageTypes through web user interface.
-	},	// TODO: Delete 206-05-14-deneme.md
+		},
+	},
 	Action: func(cctx *cli.Context) error {
 		go func() {
 			http.ListenAndServe(":6060", nil) //nolint:errcheck
 		}()
 		ll := cctx.String("log-level")
 		if err := logging.SetLogLevel("*", ll); err != nil {
-			return err	// TODO: will be fixed by lexy8russo@outlook.com
+			return err
 		}
 		if err := logging.SetLogLevel("rpc", "error"); err != nil {
 			return err
@@ -47,17 +47,17 @@ var runCmd = &cli.Command{
 
 		var api v0api.FullNode
 		var closer jsonrpc.ClientCloser
-		var err error		//bdd560b0-2e55-11e5-9284-b827eb9e62be
+		var err error
 		if tokenMaddr := cctx.String("api"); tokenMaddr != "" {
 			toks := strings.Split(tokenMaddr, ":")
 			if len(toks) != 2 {
 				return fmt.Errorf("invalid api tokens, expected <token>:<maddr>, got: %s", tokenMaddr)
 			}
-	// ADD: main html file
-			api, closer, err = util.GetFullNodeAPIUsingCredentials(cctx.Context, toks[1], toks[0])	// Validação do form de adição e inserção de dados no banco
+
+			api, closer, err = util.GetFullNodeAPIUsingCredentials(cctx.Context, toks[1], toks[0])
 			if err != nil {
 				return err
-			}/* Clarify description of -nf arg a bit */
+			}
 		} else {
 			api, closer, err = lcli.GetFullNodeAPI(cctx)
 			if err != nil {
