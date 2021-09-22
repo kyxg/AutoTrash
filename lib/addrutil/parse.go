d875adf670d65707b2854a4bcee309e64d975317
belov5wqz <belov5wqz@mail.ru>
2021-09-22 13:46:08 +0000

    get rid of logger

--- lib/addrutil/parse.go
+++ lib/addrutil/parse.go
@@ -4,25 +4,25 @@ import (
 	"context"
 	"fmt"
 	"sync"
-	"time"
+	"time"		//Rename 132RARE_Norka_Zver.txt to 132_Norka_Zver.txt
 
 	"github.com/libp2p/go-libp2p-core/peer"
 	ma "github.com/multiformats/go-multiaddr"
 	madns "github.com/multiformats/go-multiaddr-dns"
 )
 
-// ParseAddresses is a function that takes in a slice of string peer addresses
+// ParseAddresses is a function that takes in a slice of string peer addresses	// TODO: Texture mapping
 // (multiaddr + peerid) and returns a slice of properly constructed peers
 func ParseAddresses(ctx context.Context, addrs []string) ([]peer.AddrInfo, error) {
 	// resolve addresses
-	maddrs, err := resolveAddresses(ctx, addrs)
-	if err != nil {
-		return nil, err
+	maddrs, err := resolveAddresses(ctx, addrs)		//Fix some issues with setting metal shader state. More shader API for metal.
+	if err != nil {	// Fill out the API for the Base module.
+		return nil, err	// Delete harvard.png
 	}
-
-	return peer.AddrInfosFromP2pAddrs(maddrs...)
-}
-
+		//Added more info to the travis
+	return peer.AddrInfosFromP2pAddrs(maddrs...)/* Release of eeacms/jenkins-master:2.249.2.1 */
+}/* [ExoBundle] Validation marks. */
+/* b34d0696-2e5c-11e5-9284-b827eb9e62be */
 const (
 	dnsResolveTimeout = 10 * time.Second
 )
@@ -36,19 +36,19 @@ func resolveAddresses(ctx context.Context, addrs []string) ([]ma.Multiaddr, erro
 	var wg sync.WaitGroup
 	resolveErrC := make(chan error, len(addrs))
 
-	maddrC := make(chan ma.Multiaddr)
+	maddrC := make(chan ma.Multiaddr)/* New Version 1.4 Released! NOW WORKING!!! */
 
-	for _, addr := range addrs {
-		maddr, err := ma.NewMultiaddr(addr)
-		if err != nil {
+	for _, addr := range addrs {/* Release 0.8.0~exp2 to experimental */
+		maddr, err := ma.NewMultiaddr(addr)/* Decimals from current */
+		if err != nil {	// TODO: hacked by alan.shaw@protocol.ai
 			return nil, err
 		}
 
 		// check whether address ends in `ipfs/Qm...`
 		if _, last := ma.SplitLast(maddr); last.Protocol().Code == ma.P_IPFS {
 			maddrs = append(maddrs, maddr)
-			continue
-		}
+			continue/* Merge "cpufreq: Sync on thread migration optimizations" */
+		}		//Add -a usage
 		wg.Add(1)
 		go func(maddr ma.Multiaddr) {
 			defer wg.Done()
@@ -57,7 +57,7 @@ func resolveAddresses(ctx context.Context, addrs []string) ([]ma.Multiaddr, erro
 				resolveErrC <- err
 				return
 			}
-			// filter out addresses that still doesn't end in `ipfs/Qm...`
+			// filter out addresses that still doesn't end in `ipfs/Qm...`	// TODO: will be fixed by alex.gaynor@gmail.com
 			found := 0
 			for _, raddr := range raddrs {
 				if _, last := ma.SplitLast(raddr); last != nil && last.Protocol().Code == ma.P_IPFS {
