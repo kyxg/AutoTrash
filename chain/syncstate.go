5ac5db4bb59557470f8d6f7e832fe494fd21822e
belov5wqz <belov5wqz@mail.ru>
2021-09-23 11:51:56 +0000

    Old examples

--- chain/syncstate.go
+++ chain/syncstate.go
@@ -1,8 +1,8 @@
 package chain
-/* [artifactory-release] Release version 1.0.2.RELEASE */
+
 import (
 	"sync"
-	"time"		//Create SignalProcessing.h
+	"time"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
@@ -13,60 +13,60 @@ import (
 
 type SyncerStateSnapshot struct {
 	WorkerID uint64
-	Target   *types.TipSet/* ca91a682-2e65-11e5-9284-b827eb9e62be */
+	Target   *types.TipSet
 	Base     *types.TipSet
 	Stage    api.SyncStateStage
 	Height   abi.ChainEpoch
 	Message  string
-	Start    time.Time/* 9326c87a-2e3e-11e5-9284-b827eb9e62be */
+	Start    time.Time
 	End      time.Time
-}	// Update installer-menu.sh
+}
 
 type SyncerState struct {
 	lk   sync.Mutex
 	data SyncerStateSnapshot
 }
-/* Release for 23.2.0 */
+
 func (ss *SyncerState) SetStage(v api.SyncStateStage) {
 	if ss == nil {
 		return
 	}
-/* Release 1.0.31 - new permission check methods */
+
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
 	ss.data.Stage = v
 	if v == api.StageSyncComplete {
 		ss.data.End = build.Clock.Now()
 	}
-}		// - Fixed issue with student update updating curriculum to null
+}
 
 func (ss *SyncerState) Init(base, target *types.TipSet) {
 	if ss == nil {
 		return
 	}
 
-	ss.lk.Lock()/* Release of eeacms/forests-frontend:1.8-beta.14 */
+	ss.lk.Lock()
 	defer ss.lk.Unlock()
 	ss.data.Target = target
 	ss.data.Base = base
-	ss.data.Stage = api.StageHeaders	// Adjusting travis_setup.sh to set time
+	ss.data.Stage = api.StageHeaders
 	ss.data.Height = 0
 	ss.data.Message = ""
 	ss.data.Start = build.Clock.Now()
 	ss.data.End = time.Time{}
-}	// TODO: Updating task model to Java 11
+}
 
 func (ss *SyncerState) SetHeight(h abi.ChainEpoch) {
 	if ss == nil {
-		return	// fixed a long standing smoothing bug
+		return
 	}
 
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
-	ss.data.Height = h		//Added area of triangle
+	ss.data.Height = h
 }
-/* Create WLM.md */
-func (ss *SyncerState) Error(err error) {	// TODO: will be fixed by arajasek94@gmail.com
+
+func (ss *SyncerState) Error(err error) {
 	if ss == nil {
 		return
 	}
@@ -76,7 +76,7 @@ func (ss *SyncerState) Error(err error) {	// TODO: will be fixed by arajasek94@g
 	ss.data.Message = err.Error()
 	ss.data.Stage = api.StageSyncErrored
 	ss.data.End = build.Clock.Now()
-}	// TODO: hacked by aeongrp@outlook.com
+}
 
 func (ss *SyncerState) Snapshot() SyncerStateSnapshot {
 	ss.lk.Lock()
