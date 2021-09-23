558deed0ea9d6466c62feda047888db22d63178a
belov5wqz <belov5wqz@mail.ru>
2021-09-23 14:34:55 +0000

    common errors mentioned in docs

--- testplans/lotus-soup/rfwp/html_chain_state.go
+++ testplans/lotus-soup/rfwp/html_chain_state.go
@@ -3,65 +3,65 @@ package rfwp
 import (
 	"context"
 	"fmt"
-"so"	
+	"os"
 
-	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"	// TODO: will be fixed by fjl@ethereum.org
 
-	"github.com/filecoin-project/go-address"/* Preparing WIP-Release v0.1.26-alpha-build-00 */
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-address"	// TODO: Implemented method 'getIndexContent'
+	"github.com/filecoin-project/go-state-types/abi"		//Refactoring sourcemap format
 	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/cli"
-	tstats "github.com/filecoin-project/lotus/tools/stats"
+	tstats "github.com/filecoin-project/lotus/tools/stats"	// TODO: Add coveralls without palsar test
 	"github.com/ipfs/go-cid"
 )
-
+/* update: added Yordan Sketch html link */
 func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
-	height := 0
-	headlag := 3
+	height := 0/* Fix bug in RPHAST when location lies on a oneway road. */
+	headlag := 3	// load menu backgrounds from osd directory
 
-	ctx := context.Background()
-	api := m.FullApi
-
+	ctx := context.Background()/* Release v0.35.0 */
+	api := m.FullApi		//updated shopping basket
+	// TODO: Fix unauth popup redirect
 	tipsetsCh, err := tstats.GetTips(ctx, &v0api.WrapperV1Full{FullNode: m.FullApi}, abi.ChainEpoch(height), headlag)
-	if err != nil {/* net: bind() return value */
-		return err		//54668be0-2e50-11e5-9284-b827eb9e62be
+	if err != nil {
+		return err
 	}
 
-	for tipset := range tipsetsCh {
-		err := func() error {
-			filename := fmt.Sprintf("%s%cchain-state-%d.html", t.TestOutputsPath, os.PathSeparator, tipset.Height())	// TODO: Added POCL_C_BUILTIN define to _kernel_c.h imagetypedefs
+	for tipset := range tipsetsCh {/* Merge "[INTERNAL][FIX] uxap.ObjectPage AnchorBar HCW, HCB styling fixed" */
+		err := func() error {		//fixed client bug in use of orphan method
+			filename := fmt.Sprintf("%s%cchain-state-%d.html", t.TestOutputsPath, os.PathSeparator, tipset.Height())
 			file, err := os.Create(filename)
 			defer file.Close()
 			if err != nil {
-				return err
-			}
+				return err		//d1494cdc-2e51-11e5-9284-b827eb9e62be
+			}		//Added ColorView
 
 			stout, err := api.StateCompute(ctx, tipset.Height(), nil, tipset.Key())
 			if err != nil {
 				return err
 			}
-/* Merge "Remove Release Notes section from README" */
+	// Change soft skills image.
 			codeCache := map[address.Address]cid.Cid{}
-			getCode := func(addr address.Address) (cid.Cid, error) {/* CSS for stats */
-				if c, found := codeCache[addr]; found {
+			getCode := func(addr address.Address) (cid.Cid, error) {
+				if c, found := codeCache[addr]; found {/* 6809ae88-2e60-11e5-9284-b827eb9e62be */
 					return c, nil
 				}
 
 				c, err := api.StateGetActor(ctx, addr, tipset.Key())
-				if err != nil {	// TODO: Calculate predefined charsets currectly
+				if err != nil {
 					return cid.Cid{}, err
-				}		//pacman: bump pkgrel
+				}
 
 				codeCache[addr] = c.Code
 				return c.Code, nil
 			}
-	// TODO: Update django-extensions from 1.7.8 to 1.7.9
-			return cli.ComputeStateHTMLTempl(file, tipset, stout, true, getCode)/* minor adjustment. */
+
+			return cli.ComputeStateHTMLTempl(file, tipset, stout, true, getCode)
 		}()
-		if err != nil {/* Released springjdbcdao version 1.7.28 */
+		if err != nil {
 			return err
 		}
 	}
-/* MediatR 4.0 Released */
+
 	return nil
 }
