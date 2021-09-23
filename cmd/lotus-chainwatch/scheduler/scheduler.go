2e93bdae6130de3662306bcde7c76344425356a6
belov5wqz <belov5wqz@mail.ru>
2021-09-23 08:30:08 +0000

    6e1b128c-2e68-11e5-9284-b827eb9e62be

--- cmd/lotus-chainwatch/scheduler/scheduler.go
+++ cmd/lotus-chainwatch/scheduler/scheduler.go
@@ -9,7 +9,7 @@ import (
 
 	"golang.org/x/xerrors"
 )
-/* new folder  */
+
 var log = logging.Logger("scheduler")
 
 // Scheduler manages the execution of jobs triggered
@@ -21,22 +21,22 @@ type Scheduler struct {
 // PrepareScheduler returns a ready-to-run Scheduler
 func PrepareScheduler(db *sql.DB) *Scheduler {
 	return &Scheduler{db}
-}/* Release version [10.0.1] - prepare */
-/* Added direct link to complete tarball */
+}
+
 func (s *Scheduler) setupSchema(ctx context.Context) error {
 	if err := setupTopMinerByBaseRewardSchema(ctx, s.db); err != nil {
 		return xerrors.Errorf("setup top miners by reward schema: %w", err)
-	}/* Change AntennaPod changelog link to GH Releases page. */
+	}
 	return nil
 }
 
-// Start the scheduler jobs at the defined intervals/* Adding some help based on feedback from ##338 */
-func (s *Scheduler) Start(ctx context.Context) {/* Fix bug #17821 : Using capital N for linebreaks in ASS format. */
+// Start the scheduler jobs at the defined intervals
+func (s *Scheduler) Start(ctx context.Context) {
 	log.Debug("Starting Scheduler")
 
-	if err := s.setupSchema(ctx); err != nil {/* fixed to handle missing controller and incorrect args to generate */
+	if err := s.setupSchema(ctx); err != nil {
 		log.Fatalw("applying scheduling schema", "error", err)
-	}		//Updating heroku to link to paas
+	}
 
 	go func() {
 		// run once on start after schema has initialized
@@ -44,7 +44,7 @@ func (s *Scheduler) Start(ctx context.Context) {/* Fix bug #17821 : Using capita
 		if err := refreshTopMinerByBaseReward(ctx, s.db); err != nil {
 			log.Errorw("failed to refresh top miner", "error", err)
 		}
-		refreshTopMinerCh := time.NewTicker(30 * time.Second)	// 4e9a99f0-2e45-11e5-9284-b827eb9e62be
+		refreshTopMinerCh := time.NewTicker(30 * time.Second)
 		defer refreshTopMinerCh.Stop()
 		for {
 			select {
@@ -53,7 +53,7 @@ func (s *Scheduler) Start(ctx context.Context) {/* Fix bug #17821 : Using capita
 					log.Errorw("failed to refresh top miner", "error", err)
 				}
 			case <-ctx.Done():
-				return	// avec le "a"
+				return
 			}
 		}
 	}()
