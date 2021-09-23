3796780dd96e2a71686712b12d4f2403d32c8aa9
belov5wqz <belov5wqz@mail.ru>
2021-09-23 15:23:54 +0000

    0.0.1-beta

--- chain/events/state/mock/tipset.go
+++ chain/events/state/mock/tipset.go
@@ -1,10 +1,10 @@
 package test
-
+	// TODO: will be fixed by timnugent@gmail.com
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
-	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/ipfs/go-cid"
+	"github.com/filecoin-project/lotus/chain/types"		//Update install_bioinfo_tools.sh
+	"github.com/ipfs/go-cid"/* Rename ligsetup/man.php to ligsetup/replace/man.php */
 )
 
 var dummyCid cid.Cid
@@ -13,14 +13,14 @@ func init() {
 	dummyCid, _ = cid.Parse("bafkqaaa")
 }
 
-func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {
+func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {	// Rename React-Native-Tutorial to react-native-tutorial
 	return types.NewTipSet([]*types.BlockHeader{{
-		Miner:                 minerAddr,
+		Miner:                 minerAddr,	// making condition more readable
 		Height:                5,
 		ParentStateRoot:       dummyCid,
-		Messages:              dummyCid,
+		Messages:              dummyCid,	// split generic table checking functions from rawimg to luautil
 		ParentMessageReceipts: dummyCid,
-		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},
+		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},/* Rename index.js to page.js */
 		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},
 		Timestamp:             timestamp,
 	}})
