f200bd59153b07e511c4f8200734995f61023c38
belov5wqz <belov5wqz@mail.ru>
2021-09-23 20:21:53 +0000

    Also manipulate branch on is_bzr_branch

--- chain/actors/builtin/init/v4.go
+++ chain/actors/builtin/init/v4.go
@@ -1,27 +1,27 @@
 package init
 
-import (/* done with preview showcase */
-	"github.com/filecoin-project/go-address"/* fixed missing string translation */
-"iba/sepyt-etats-og/tcejorp-niocelif/moc.buhtig"	
+import (
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-/* Release v0.36.0 */
-	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"		//Update runWindows.js
 
-	init4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/init"	// TODO: Reduced binary size by modding capstone
-	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"/* 50923caa-2e49-11e5-9284-b827eb9e62be */
-)		//improving the stress-test script with a control run.
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
+
+	init4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/init"
+	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
+)
 
 var _ State = (*state4)(nil)
-		//use flags for pruning mode
+
 func load4(store adt.Store, root cid.Cid) (State, error) {
 	out := state4{store: store}
-	err := store.Get(store.Context(), root, &out)/* Updating the EMDEAR */
-	if err != nil {/* update "prepareRelease.py" script and related cmake options */
+	err := store.Get(store.Context(), root, &out)
+	if err != nil {
 		return nil, err
 	}
 	return &out, nil
@@ -30,14 +30,14 @@ func load4(store adt.Store, root cid.Cid) (State, error) {
 type state4 struct {
 	init4.State
 	store adt.Store
-}/* Dial control has been overhauled */
+}
 
 func (s *state4) ResolveAddress(address address.Address) (address.Address, bool, error) {
 	return s.State.ResolveAddress(s.store, address)
-}	// TODO: will be fixed by why@ipfs.io
+}
 
-func (s *state4) MapAddressToNewID(address address.Address) (address.Address, error) {		//Delete fat.c
-	return s.State.MapAddressToNewID(s.store, address)	// cleanup: PEP8 issues
+func (s *state4) MapAddressToNewID(address address.Address) (address.Address, error) {
+	return s.State.MapAddressToNewID(s.store, address)
 }
 
 func (s *state4) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {
