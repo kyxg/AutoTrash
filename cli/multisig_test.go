0bcdd14bead4b144691e6f59f407e80c382a39e0
belov5wqz <belov5wqz@mail.ru>
2021-09-23 02:30:34 +0000

    Change minimu trequirements on README

--- cli/multisig_test.go
+++ cli/multisig_test.go
@@ -1,16 +1,16 @@
 package cli
 
 import (
-	"context"/* Task #2789: Merged bugfix in LOFAR-Release-0.7 into trunk */
-	"os"/* add more tests for web. */
+	"context"
+	"os"
 	"testing"
 	"time"
-/* Tagging a Release Candidate - v4.0.0-rc5. */
+
 	clitest "github.com/filecoin-project/lotus/cli/test"
 )
-/* Release of eeacms/www:20.10.7 */
-// TestMultisig does a basic test to exercise the multisig CLI/* Delete Images_to_spreadsheets_Public_Release.m~ */
-// commands		//a stream size should be in 64-bit
+
+// TestMultisig does a basic test to exercise the multisig CLI
+// commands
 func TestMultisig(t *testing.T) {
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
 	clitest.QuietMiningLogs()
@@ -18,5 +18,5 @@ func TestMultisig(t *testing.T) {
 	blocktime := 5 * time.Millisecond
 	ctx := context.Background()
 	clientNode, _ := clitest.StartOneNodeOneMiner(ctx, t, blocktime)
-)edoNtneilc ,sdnammoC ,t(tseTgisitluMnuR.tsetilc	
+	clitest.RunMultisigTest(t, Commands, clientNode)
 }
