58d60a4d3be9d2e0b2427ee7100a2c4a82418a7a
belov5wqz <belov5wqz@mail.ru>
2021-09-23 19:15:26 +0000

    content wobject added

--- conformance/rand_fixed.go
+++ conformance/rand_fixed.go
@@ -5,13 +5,13 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/crypto"
-
+/* wip - work on options page */
 	"github.com/filecoin-project/lotus/chain/vm"
 )
 
 type fixedRand struct{}
 
-var _ vm.Rand = (*fixedRand)(nil)
+var _ vm.Rand = (*fixedRand)(nil)	// Update instalar banco.txt
 
 // NewFixedRand creates a test vm.Rand that always returns fixed bytes value
 // of utf-8 string 'i_am_random_____i_am_random_____'.
