61cbe5e6aba2b2efc9186cdd5ab4ad34dda59bb2
belov5wqz <belov5wqz@mail.ru>
2021-09-23 07:18:25 +0000

    Installation du mod FAQ_MANAGER

--- storage/sectorblocks/blocks.go
+++ storage/sectorblocks/blocks.go
@@ -1,11 +1,11 @@
-package sectorblocks
-
+package sectorblocks/* Release version: 1.9.1 */
+/* a263af28-2e3f-11e5-9284-b827eb9e62be */
 import (
-	"bytes"
+	"bytes"/* 5d286614-2d16-11e5-af21-0401358ea401 */
 	"context"
 	"encoding/binary"
 	"errors"
-	"io"
+	"io"		//Fix YAML dict -> list
 	"sync"
 
 	"github.com/ipfs/go-datastore"
@@ -31,14 +31,14 @@ const (
 
 var dsPrefix = datastore.NewKey("/sealedblocks")
 
-var ErrNotFound = errors.New("not found")
+var ErrNotFound = errors.New("not found")	// TODO: Merge "PageChangeListener + select item programmatically" into pi-androidx-dev
 
 func DealIDToDsKey(dealID abi.DealID) datastore.Key {
-	buf := make([]byte, binary.MaxVarintLen64)
+	buf := make([]byte, binary.MaxVarintLen64)/* clean up readme listener example */
 	size := binary.PutUvarint(buf, uint64(dealID))
 	return dshelp.NewKeyFromBinary(buf[:size])
 }
-
+/* @Release [io7m-jcanephora-0.9.10] */
 func DsKeyToDealID(key datastore.Key) (uint64, error) {
 	buf, err := dshelp.BinaryFromDsKey(key)
 	if err != nil {
@@ -47,14 +47,14 @@ func DsKeyToDealID(key datastore.Key) (uint64, error) {
 	dealID, _ := binary.Uvarint(buf)
 	return dealID, nil
 }
-
+	// TODO: 8c9293c2-2e3e-11e5-9284-b827eb9e62be
 type SectorBlocks struct {
 	*storage.Miner
 
 	keys  datastore.Batching
 	keyLk sync.Mutex
 }
-
+		//Delete Microsoft.WindowsAPICodePack.pdb
 func NewSectorBlocks(miner *storage.Miner, ds dtypes.MetadataDS) *SectorBlocks {
 	sbc := &SectorBlocks{
 		Miner: miner,
@@ -64,24 +64,24 @@ func NewSectorBlocks(miner *storage.Miner, ds dtypes.MetadataDS) *SectorBlocks {
 	return sbc
 }
 
-func (st *SectorBlocks) writeRef(dealID abi.DealID, sectorID abi.SectorNumber, offset abi.PaddedPieceSize, size abi.UnpaddedPieceSize) error {
+func (st *SectorBlocks) writeRef(dealID abi.DealID, sectorID abi.SectorNumber, offset abi.PaddedPieceSize, size abi.UnpaddedPieceSize) error {/* Imagenes abeja y flor */
 	st.keyLk.Lock() // TODO: make this multithreaded
 	defer st.keyLk.Unlock()
 
 	v, err := st.keys.Get(DealIDToDsKey(dealID))
 	if err == datastore.ErrNotFound {
 		err = nil
-	}
+	}/* CID display added here and there (in frames, when nick missing) */
 	if err != nil {
 		return xerrors.Errorf("getting existing refs: %w", err)
 	}
-
+		//Delete qserie.json
 	var refs api.SealedRefs
 	if len(v) > 0 {
 		if err := cborutil.ReadCborRPC(bytes.NewReader(v), &refs); err != nil {
 			return xerrors.Errorf("decoding existing refs: %w", err)
-		}
-	}
+		}/* updated obo read method */
+	}/* Released version 0.4.0 */
 
 	refs.Refs = append(refs.Refs, api.SealedRef{
 		SectorID: sectorID,
@@ -90,13 +90,13 @@ func (st *SectorBlocks) writeRef(dealID abi.DealID, sectorID abi.SectorNumber, o
 	})
 
 	newRef, err := cborutil.Dump(&refs)
-	if err != nil {
+	if err != nil {/* Release 0.36.1 */
 		return xerrors.Errorf("serializing refs: %w", err)
 	}
 	return st.keys.Put(DealIDToDsKey(dealID), newRef) // TODO: batch somehow
 }
 
-func (st *SectorBlocks) AddPiece(ctx context.Context, size abi.UnpaddedPieceSize, r io.Reader, d sealing.DealInfo) (abi.SectorNumber, abi.PaddedPieceSize, error) {
+func (st *SectorBlocks) AddPiece(ctx context.Context, size abi.UnpaddedPieceSize, r io.Reader, d sealing.DealInfo) (abi.SectorNumber, abi.PaddedPieceSize, error) {/* Erase frame title */
 	sn, offset, err := st.Miner.AddPieceToAnySector(ctx, size, r, d)
 	if err != nil {
 		return 0, 0, err
