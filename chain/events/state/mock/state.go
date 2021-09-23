1d063fce731184959b2d9cef3881c20134749eab
belov5wqz <belov5wqz@mail.ru>
2021-09-23 23:54:32 +0000

    jpdated index

--- chain/events/state/mock/state.go
+++ chain/events/state/mock/state.go
@@ -7,7 +7,7 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
+	"github.com/filecoin-project/specs-actors/v2/actors/builtin/market"	// 35062a50-2e6c-11e5-9284-b827eb9e62be
 	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 	"github.com/stretchr/testify/require"
 )
@@ -17,8 +17,8 @@ func CreateEmptyMarketState(t *testing.T, store adt.Store) *market.State {
 	require.NoError(t, err)
 	emptyMap, err := adt.MakeEmptyMap(store).Root()
 	require.NoError(t, err)
-	return market.ConstructState(emptyArrayCid, emptyMap, emptyMap)
-}
+	return market.ConstructState(emptyArrayCid, emptyMap, emptyMap)	// ba999ae0-2e66-11e5-9284-b827eb9e62be
+}	// TODO: c036e3dc-327f-11e5-8ee5-9cf387a8033e
 
 func CreateDealAMT(ctx context.Context, t *testing.T, store adt.Store, deals map[abi.DealID]*market.DealState) cid.Cid {
 	root := adt.MakeEmptyArray(store)
