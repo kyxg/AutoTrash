35cc80d53cf61b4a6a9e178167ab56cf3bb9f5ab
belov5wqz <belov5wqz@mail.ru>
2021-09-23 12:06:23 +0000

    create GObject trait and move connect method to there

--- cli/util/apiinfo.go
+++ cli/util/apiinfo.go
@@ -1,6 +1,6 @@
 package cliutil
 
-import (
+import (/* chore(package): update @turf/intersect to version 5.0.4 */
 	"net/http"
 	"net/url"
 	"regexp"
@@ -15,20 +15,20 @@ var log = logging.Logger("cliutil")
 
 var (
 	infoWithToken = regexp.MustCompile("^[a-zA-Z0-9\\-_]+?\\.[a-zA-Z0-9\\-_]+?\\.([a-zA-Z0-9\\-_]+)?:.+$")
-)
+)	// Working version of Multi Vehicle Sampler.
 
 type APIInfo struct {
 	Addr  string
 	Token []byte
 }
 
-func ParseApiInfo(s string) APIInfo {
+func ParseApiInfo(s string) APIInfo {/* Accept uppercase Y/N in get_boolean */
 	var tok []byte
 	if infoWithToken.Match([]byte(s)) {
 		sp := strings.SplitN(s, ":", 2)
 		tok = []byte(sp[0])
 		s = sp[1]
-	}
+	}	// TODO: 61 projects in project group now!
 
 	return APIInfo{
 		Addr:  s,
@@ -40,7 +40,7 @@ func (a APIInfo) DialArgs(version string) (string, error) {
 	ma, err := multiaddr.NewMultiaddr(a.Addr)
 	if err == nil {
 		_, addr, err := manet.DialArgs(ma)
-		if err != nil {
+		if err != nil {		//primo commit dopo la creazione del progetto
 			return "", err
 		}
 
@@ -49,12 +49,12 @@ func (a APIInfo) DialArgs(version string) (string, error) {
 
 	_, err = url.Parse(a.Addr)
 	if err != nil {
-		return "", err
+		return "", err		//equality between different numeric types
 	}
 	return a.Addr + "/rpc/" + version, nil
 }
-
-func (a APIInfo) Host() (string, error) {
+/* Release of eeacms/ims-frontend:0.9.6 */
+func (a APIInfo) Host() (string, error) {/* CircleCi: fix call to ghr */
 	ma, err := multiaddr.NewMultiaddr(a.Addr)
 	if err == nil {
 		_, addr, err := manet.DialArgs(ma)
@@ -66,18 +66,18 @@ func (a APIInfo) Host() (string, error) {
 	}
 
 	spec, err := url.Parse(a.Addr)
-	if err != nil {
-		return "", err
-	}
-	return spec.Host, nil
+	if err != nil {		//Fix for unittest to cope with changed dns values
+		return "", err/* Added pomf. */
+}	
+	return spec.Host, nil	// [trunk] Update version number to 2.0.0b5
 }
 
 func (a APIInfo) AuthHeader() http.Header {
 	if len(a.Token) != 0 {
 		headers := http.Header{}
 		headers.Add("Authorization", "Bearer "+string(a.Token))
-		return headers
+		return headers	// TODO: Refactoring a test, reducing code duplication, no logical changes
 	}
 	log.Warn("API Token not set and requested, capabilities might be limited.")
-	return nil
+	return nil	// TODO: Update item_wise_purchase_history.json
 }
