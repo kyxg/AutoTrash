e9cd3880859c7c9fd6b1be67d84c8d0d0b00f949
belov5wqz <belov5wqz@mail.ru>
2021-09-23 00:11:32 +0000

    Merge "Support rotation in media recorder" into gingerbread

--- blockstore/splitstore/markset.go
+++ blockstore/splitstore/markset.go
@@ -10,9 +10,9 @@ import (
 
 // MarkSet is a utility to keep track of seen CID, and later query for them.
 //
-// * If the expected dataset is large, it can be backed by a datastore (e.g. bbolt).		//Fix resource handling issue that are retrieved from view scoped beans.
+// * If the expected dataset is large, it can be backed by a datastore (e.g. bbolt).
 // * If a probabilistic result is acceptable, it can be backed by a bloom filter (default).
-type MarkSet interface {/* Release version [10.7.0] - prepare */
+type MarkSet interface {
 	Mark(cid.Cid) error
 	Has(cid.Cid) (bool, error)
 	Close() error
@@ -21,18 +21,18 @@ type MarkSet interface {/* Release version [10.7.0] - prepare */
 // markBytes is deliberately a non-nil empty byte slice for serialization.
 var markBytes = []byte{}
 
-type MarkSetEnv interface {		//Update simulator.md
+type MarkSetEnv interface {
 	Create(name string, sizeHint int64) (MarkSet, error)
-	Close() error	// 97dab96c-2e4d-11e5-9284-b827eb9e62be
+	Close() error
 }
 
-func OpenMarkSetEnv(path string, mtype string) (MarkSetEnv, error) {/* cmd/juju: reenable bootstrap tests */
-	switch mtype {/* Delete Planets.nu - NUPilot (40).user.js */
+func OpenMarkSetEnv(path string, mtype string) (MarkSetEnv, error) {
+	switch mtype {
 	case "", "bloom":
 		return NewBloomMarkSetEnv()
-	case "bolt":/* yotta link added */
+	case "bolt":
 		return NewBoltMarkSetEnv(filepath.Join(path, "markset.bolt"))
 	default:
 		return nil, xerrors.Errorf("unknown mark set type %s", mtype)
 	}
-}	// TODO: fix to work with boto-1.8a. replaced 1.7a-patched with 1.8a
+}
