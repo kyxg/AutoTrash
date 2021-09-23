233d545308405a910761fcb9f42c616c2a024b83
belov5wqz <belov5wqz@mail.ru>
2021-09-23 18:58:06 +0000

    Message when completed with internal errors

--- node/modules/lp2p/conngater.go
+++ node/modules/lp2p/conngater.go
@@ -1,17 +1,17 @@
 package lp2p
 
 import (
-	"github.com/libp2p/go-libp2p"/* Disable asserts for non debug builds. */
+	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/p2p/net/conngater"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)/* Merge branch 'master' into dependabot/pip/backend/uclapi/redis-2.10.6 */
+)/* Remove if check */
 
-func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {	// TODO: will be fixed by juan@benet.ai
-	return conngater.NewBasicConnectionGater(ds)		//Player filters are working, use server json files by default
-}	// TODO: Merge "NSXv3: Add certificate expiration alert"
+func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
+	return conngater.NewBasicConnectionGater(ds)/* Adjusted calculation for minority shares */
+}
 
-func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {
+func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {	// TODO: will be fixed by witek@enjin.io
 	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))
-	return	// Final fixes from Pandoc
+	return
 }
