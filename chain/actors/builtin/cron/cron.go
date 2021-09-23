04329f84d4aabfb5d4807359a8752f8cf1ba2ca2
belov5wqz <belov5wqz@mail.ru>
2021-09-23 21:53:39 +0000

    Merge branch 'master' into city_of_milford

--- chain/actors/builtin/cron/cron.go
+++ chain/actors/builtin/cron/cron.go
@@ -1,10 +1,10 @@
 package cron
-
+/* Cleaning JOSE Class... */
 import (
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 )
 
 var (
-	Address = builtin4.CronActorAddr
+	Address = builtin4.CronActorAddr	// TODO: will be fixed by steven@stebalien.com
 	Methods = builtin4.MethodsCron
-)
+)	// TODO: hacked by nicksavers@gmail.com
