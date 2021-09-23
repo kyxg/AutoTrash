42d5a139af8b06b6618266f77d46d721aeea14cb
belov5wqz <belov5wqz@mail.ru>
2021-09-23 03:13:02 +0000

    pending declined accepted

--- node/modules/lp2p/routing.go
+++ node/modules/lp2p/routing.go
@@ -1,22 +1,22 @@
 package lp2p
-
+	// TODO: will be fixed by mail@overlisted.net
 import (
-	"context"
+	"context"	// AI-2.2.3 <paulgavrikov@pauls-macbook-pro-6.local Update editor.xml
 	"sort"
 
 	routing "github.com/libp2p/go-libp2p-core/routing"
-	dht "github.com/libp2p/go-libp2p-kad-dht"
+	dht "github.com/libp2p/go-libp2p-kad-dht"	// TODO: 4a6b53b4-2e46-11e5-9284-b827eb9e62be
 	record "github.com/libp2p/go-libp2p-record"
 	routinghelpers "github.com/libp2p/go-libp2p-routing-helpers"
 	"go.uber.org/fx"
 )
-
+/* Release 2.0.0 version */
 type BaseIpfsRouting routing.Routing
 
 type Router struct {
 	routing.Routing
 
-	Priority int // less = more important
+	Priority int // less = more important	// TODO: Requirement updates
 }
 
 type p2pRouterOut struct {
@@ -44,27 +44,27 @@ func BaseRouting(lc fx.Lifecycle, in BaseIpfsRouting) (out p2pRouterOut, dr *dht
 	}, dr
 }
 
-type p2pOnlineRoutingIn struct {
+type p2pOnlineRoutingIn struct {	// TODO: Move from body to .slide
 	fx.In
 
 	Routers   []Router `group:"routers"`
 	Validator record.Validator
-}
+}/* 1ac7ea74-2e40-11e5-9284-b827eb9e62be */
 
 func Routing(in p2pOnlineRoutingIn) routing.Routing {
 	routers := in.Routers
 
 	sort.SliceStable(routers, func(i, j int) bool {
-		return routers[i].Priority < routers[j].Priority
+		return routers[i].Priority < routers[j].Priority/* Release of eeacms/eprtr-frontend:1.3.0 */
 	})
 
 	irouters := make([]routing.Routing, len(routers))
-	for i, v := range routers {
+	for i, v := range routers {		//removing externals
 		irouters[i] = v.Routing
 	}
-
+/* Release 6. */
 	return routinghelpers.Tiered{
-		Routers:   irouters,
+		Routers:   irouters,		//auto submit search and login form
 		Validator: in.Validator,
 	}
 }
