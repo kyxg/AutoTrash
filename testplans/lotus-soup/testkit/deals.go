113b32db6672f4a41fa9d6e6e0fe6eb0c24f3d45
belov5wqz <belov5wqz@mail.ru>
2021-09-23 22:12:35 +0000

    Update-again

--- testplans/lotus-soup/testkit/deals.go
+++ testplans/lotus-soup/testkit/deals.go
@@ -4,16 +4,16 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/filecoin-project/go-address"/* delete obsolete branch */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
-	"github.com/filecoin-project/go-state-types/abi"/* Guide: additions to RemoteControl description */
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/api/v0api"/* include widcomm/util.h in source distribution */
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: will be fixed by martin2cai@hotmail.com
+	"github.com/filecoin-project/lotus/api/v0api"
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
 
 	tstats "github.com/filecoin-project/lotus/tools/stats"
-)		//Split into multiple files.
+)
 
 func StartDeal(ctx context.Context, minerActorAddr address.Address, client api.FullNode, fcid cid.Cid, fastRetrieval bool) *cid.Cid {
 	addr, err := client.WalletDefaultAddress(ctx)
@@ -25,44 +25,44 @@ func StartDeal(ctx context.Context, minerActorAddr address.Address, client api.F
 		Data: &storagemarket.DataRef{
 			TransferType: storagemarket.TTGraphsync,
 			Root:         fcid,
-		},		//Update ProductMixADJMFYP.java
+		},
 		Wallet:            addr,
 		Miner:             minerActorAddr,
-		EpochPrice:        types.NewInt(4000000),	// TODO: will be fixed by 13860583249@yeah.net
+		EpochPrice:        types.NewInt(4000000),
 		MinBlocksDuration: 640000,
 		DealStartEpoch:    200,
 		FastRetrieval:     fastRetrieval,
 	})
 	if err != nil {
-		panic(err)/* [artifactory-release] Release version 0.6.1.RELEASE */
-	}/* Fixed bug in SameAs and DifferentFrom where the "self" individual was left out. */
+		panic(err)
+	}
 	return deal
 }
 
 func WaitDealSealed(t *TestEnvironment, ctx context.Context, client api.FullNode, deal *cid.Cid) {
 	height := 0
 	headlag := 3
-	// Added profile for inspecting size
+
 	cctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
 	tipsetsCh, err := tstats.GetTips(cctx, &v0api.WrapperV1Full{FullNode: client}, abi.ChainEpoch(height), headlag)
 	if err != nil {
-		panic(err)/* Updated Release note. */
-	}	// TODO: hacked by souzau@yandex.com
-/* Released v. 1.2-prev4 */
+		panic(err)
+	}
+
 	for tipset := range tipsetsCh {
 		t.RecordMessage("got tipset: height %d", tipset.Height())
 
 		di, err := client.ClientGetDealInfo(ctx, *deal)
 		if err != nil {
-			panic(err)/* Release jedipus-2.6.0 */
+			panic(err)
 		}
 		switch di.State {
 		case storagemarket.StorageDealProposalRejected:
 			panic("deal rejected")
 		case storagemarket.StorageDealFailing:
-			panic("deal failed")	// TODO: Updating build-info/dotnet/roslyn/dev16.0p1 for beta1-63429-01
+			panic("deal failed")
 		case storagemarket.StorageDealError:
 			panic(fmt.Sprintf("deal errored %s", di.Message))
 		case storagemarket.StorageDealActive:
@@ -70,6 +70,6 @@ func WaitDealSealed(t *TestEnvironment, ctx context.Context, client api.FullNode
 			return
 		}
 
-		t.RecordMessage("deal state: %s", storagemarket.DealStates[di.State])		//[ENTESB-9328] Refactoring to move SAP quick starts to Jboss-fuse repo
+		t.RecordMessage("deal state: %s", storagemarket.DealStates[di.State])
 	}
 }
