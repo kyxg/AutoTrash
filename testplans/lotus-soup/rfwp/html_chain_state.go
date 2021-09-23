42fb13d877b4a6a9be4c13627d6a3ec7c175b0f0
belov5wqz <belov5wqz@mail.ru>
2021-09-23 18:15:31 +0000

    Update 3.5-exercicios-arco.md

--- testplans/lotus-soup/rfwp/html_chain_state.go
+++ testplans/lotus-soup/rfwp/html_chain_state.go
@@ -5,45 +5,45 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"	// TODO: will be fixed by fjl@ethereum.org
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
 
-	"github.com/filecoin-project/go-address"	// TODO: Implemented method 'getIndexContent'
-	"github.com/filecoin-project/go-state-types/abi"		//Refactoring sourcemap format
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/cli"
-	tstats "github.com/filecoin-project/lotus/tools/stats"	// TODO: Add coveralls without palsar test
+	tstats "github.com/filecoin-project/lotus/tools/stats"
 	"github.com/ipfs/go-cid"
 )
-/* update: added Yordan Sketch html link */
-func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
-	height := 0/* Fix bug in RPHAST when location lies on a oneway road. */
-	headlag := 3	// load menu backgrounds from osd directory
 
-	ctx := context.Background()/* Release v0.35.0 */
-	api := m.FullApi		//updated shopping basket
-	// TODO: Fix unauth popup redirect
+func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
+	height := 0
+	headlag := 3
+
+	ctx := context.Background()
+	api := m.FullApi
+
 	tipsetsCh, err := tstats.GetTips(ctx, &v0api.WrapperV1Full{FullNode: m.FullApi}, abi.ChainEpoch(height), headlag)
 	if err != nil {
 		return err
 	}
 
-	for tipset := range tipsetsCh {/* Merge "[INTERNAL][FIX] uxap.ObjectPage AnchorBar HCW, HCB styling fixed" */
-		err := func() error {		//fixed client bug in use of orphan method
+	for tipset := range tipsetsCh {
+		err := func() error {
 			filename := fmt.Sprintf("%s%cchain-state-%d.html", t.TestOutputsPath, os.PathSeparator, tipset.Height())
 			file, err := os.Create(filename)
 			defer file.Close()
 			if err != nil {
-				return err		//d1494cdc-2e51-11e5-9284-b827eb9e62be
-			}		//Added ColorView
+				return err
+			}
 
 			stout, err := api.StateCompute(ctx, tipset.Height(), nil, tipset.Key())
 			if err != nil {
 				return err
 			}
-	// Change soft skills image.
+
 			codeCache := map[address.Address]cid.Cid{}
 			getCode := func(addr address.Address) (cid.Cid, error) {
-				if c, found := codeCache[addr]; found {/* 6809ae88-2e60-11e5-9284-b827eb9e62be */
+				if c, found := codeCache[addr]; found {
 					return c, nil
 				}
 
