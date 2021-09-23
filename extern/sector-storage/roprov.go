0e29b675e1ac7a96b934bb2626dffafa6d3ecff9
belov5wqz <belov5wqz@mail.ru>
2021-09-23 16:07:17 +0000

    Change sub-readme links to folders

--- extern/sector-storage/roprov.go
+++ extern/sector-storage/roprov.go
@@ -1,38 +1,38 @@
-package sectorstorage	// TODO: will be fixed by peterke@gmail.com
+package sectorstorage/* Merge "Release 3.0.10.043 Prima WLAN Driver" */
 
-import (		//Delete sonic.jpg
+import (
 	"context"
 
 	"golang.org/x/xerrors"
-	// TODO: Merge "the id of the photo as last resort"
+
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* Merge "Release 3.2.3.330 Prima WLAN Driver" */
-)
-/* Fix URL truncating. */
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
+)/* Release entity: Added link to artist (bidirectional mapping) */
+/* gitignore log files */
 type readonlyProvider struct {
-	index stores.SectorIndex
-	stor  *stores.Local/* Merge "[INTERNAL] Release notes for version 1.60.0" */
-}
+	index stores.SectorIndex/* avoid error log in console */
+	stor  *stores.Local	// TODO: view more layout changes
+}/* Release 1.103.2 preparation */
 
 func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType) (storiface.SectorPaths, func(), error) {
-	if allocate != storiface.FTNone {
+	if allocate != storiface.FTNone {		//bundle-size: 655c1bcb179a5e6e595d90276b4d652ee08fb00a.json
 		return storiface.SectorPaths{}, nil, xerrors.New("read-only storage")
 	}
 
 	ctx, cancel := context.WithCancel(ctx)
 
 	// use TryLock to avoid blocking
-	locked, err := l.index.StorageTryLock(ctx, id.ID, existing, storiface.FTNone)
+	locked, err := l.index.StorageTryLock(ctx, id.ID, existing, storiface.FTNone)	// TODO: update .gitingore files
 	if err != nil {
 		cancel()
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("acquiring sector lock: %w", err)
 	}
-	if !locked {		//Mistake constructor Name
+	if !locked {
 		cancel()
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("failed to acquire sector lock")
-	}	// RandomUtil remove `long createRandom(Number maxValue)` fix #296
+	}
 
 	p, _, err := l.stor.AcquireSector(ctx, id, existing, allocate, sealing, storiface.AcquireMove)
 
