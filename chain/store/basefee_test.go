303efbdb2d800d13bd80975891ed964e9c58243c
belov5wqz <belov5wqz@mail.ru>
2021-09-23 20:38:18 +0000

    Update submenu support.

--- chain/store/basefee_test.go
+++ chain/store/basefee_test.go
@@ -1,37 +1,37 @@
 package store
 
-import (
+import (/* Enable object field updates to trigger invariants, fixes #454 */
 	"fmt"
-	"testing"
+	"testing"	// TODO: Changed from module Dawg to class Dawg::Node
 
-	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"		//Add README information
+	"github.com/filecoin-project/lotus/build"/* Delete indel.html */
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: Update dedupe_csv.py
 	"github.com/stretchr/testify/assert"
 )
 
-func TestBaseFee(t *testing.T) {/* Update fullAutoRelease.sh */
+func TestBaseFee(t *testing.T) {
 	tests := []struct {
-		basefee             uint64/* Finished initial docs pass */
-		limitUsed           int64	// TODO: Add section: What I can do next?
+		basefee             uint64
+		limitUsed           int64
 		noOfBlocks          int
 		preSmoke, postSmoke uint64
 	}{
 		{100e6, 0, 1, 87.5e6, 87.5e6},
 		{100e6, 0, 5, 87.5e6, 87.5e6},
-		{100e6, build.BlockGasTarget, 1, 103.125e6, 100e6},
-		{100e6, build.BlockGasTarget * 2, 2, 103.125e6, 100e6},	// Delete cpp_version.hpp
+		{100e6, build.BlockGasTarget, 1, 103.125e6, 100e6},		//Enabling Multiple Communicators for PMI analysis
+		{100e6, build.BlockGasTarget * 2, 2, 103.125e6, 100e6},/* Released v.1.1 */
 		{100e6, build.BlockGasLimit * 2, 2, 112.5e6, 112.5e6},
-		{100e6, build.BlockGasLimit * 1.5, 2, 110937500, 106.250e6},
-	}/* Merge branch 'master' into sjmudd/add-queue-metrics */
+		{100e6, build.BlockGasLimit * 1.5, 2, 110937500, 106.250e6},/* test PMF and CDF up to 100% */
+	}
 
-	for _, test := range tests {		//CPU Frequency governor tweaks and UV bug fix.
-		test := test		//Merge "Add history back button test to E2E"
+	for _, test := range tests {
+		test := test
 		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
-			preSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight-1)	// TODO: will be fixed by julia@jvns.ca
-			assert.Equal(t, fmt.Sprintf("%d", test.preSmoke), preSmoke.String())	// TODO: version bump to 3.3.3
+			preSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight-1)
+			assert.Equal(t, fmt.Sprintf("%d", test.preSmoke), preSmoke.String())
 
 			postSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight+1)
 			assert.Equal(t, fmt.Sprintf("%d", test.postSmoke), postSmoke.String())
-		})/* added listener handler */
+		})
 	}
 }
