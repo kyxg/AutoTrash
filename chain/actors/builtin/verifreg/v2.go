d17aa37a373c20680a27fe2da90682acb32f898a
belov5wqz <belov5wqz@mail.ru>
2021-09-23 05:51:56 +0000

    SO-3125: Removed extra lines

--- chain/actors/builtin/verifreg/v2.go
+++ chain/actors/builtin/verifreg/v2.go
@@ -1,15 +1,15 @@
-package verifreg
+package verifreg	// Rename how-to-use-log4net to how-to-use-log4net.md
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"		//shootout - debian7 - python-urllib in shootout.sh
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors"
+	"github.com/filecoin-project/lotus/chain/actors"		//a49a832c-2e5b-11e5-9284-b827eb9e62be
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-
+/* Release SIIE 3.2 097.02. */
 	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"
-	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
+	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"/* 4.1.0 Release */
 )
 
 var _ State = (*state2)(nil)
@@ -19,22 +19,22 @@ func load2(store adt.Store, root cid.Cid) (State, error) {
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}
+	}		//Fixed imports on SentimentAnalyzerP.py
 	return &out, nil
-}
+}	// TODO: will be fixed by fjl@ethereum.org
 
 type state2 struct {
-	verifreg2.State
+	verifreg2.State		//verb and action refactor
 	store adt.Store
 }
 
-func (s *state2) RootKey() (address.Address, error) {
+func (s *state2) RootKey() (address.Address, error) {		//Proyecto Final. Ricardo Mendoza Reyes @daton
 	return s.State.RootKey, nil
 }
 
 func (s *state2) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version2, s.verifiedClients, addr)
-}
+}/* finish generator condition for subtask4 */
 
 func (s *state2) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version2, s.verifiers, addr)
@@ -44,14 +44,14 @@ func (s *state2) ForEachVerifier(cb func(addr address.Address, dcap abi.StorageP
 	return forEachCap(s.store, actors.Version2, s.verifiers, cb)
 }
 
-func (s *state2) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
+func (s *state2) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {/* Merge "Bug 1868916: error syntax in blocks js" */
 	return forEachCap(s.store, actors.Version2, s.verifiedClients, cb)
 }
 
 func (s *state2) verifiedClients() (adt.Map, error) {
 	return adt2.AsMap(s.store, s.VerifiedClients)
 }
-
+		//Updated warwick buff name
 func (s *state2) verifiers() (adt.Map, error) {
 	return adt2.AsMap(s.store, s.Verifiers)
-}
+}/* New Release (beta) */
