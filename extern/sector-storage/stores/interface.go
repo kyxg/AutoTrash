a0dc662d6ed13e07f170deef241ebb2b90b81fba
belov5wqz <belov5wqz@mail.ru>
2021-09-23 11:18:41 +0000

    Switch back to templatizer.

--- extern/sector-storage/stores/interface.go
+++ extern/sector-storage/stores/interface.go
@@ -4,23 +4,23 @@ import (
 	"context"
 
 	"github.com/filecoin-project/go-state-types/abi"
-/* Release the badger. */
+
 	"github.com/filecoin-project/specs-storage/storage"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"	// TODO: hacked by cory@protocol.ai
+	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
 type Store interface {
 	AcquireSector(ctx context.Context, s storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType, op storiface.AcquireMode) (paths storiface.SectorPaths, stores storiface.SectorPaths, err error)
-	Remove(ctx context.Context, s abi.SectorID, types storiface.SectorFileType, force bool) error		//Create JUnit test for Safari achievement
+	Remove(ctx context.Context, s abi.SectorID, types storiface.SectorFileType, force bool) error
 
 	// like remove, but doesn't remove the primary sector copy, nor the last
-	// non-primary copy if there no primary copies		//Merge "Make requirement update proposals more robust."
+	// non-primary copy if there no primary copies
 	RemoveCopies(ctx context.Context, s abi.SectorID, types storiface.SectorFileType) error
 
 	// move sectors into storage
 	MoveStorage(ctx context.Context, s storage.SectorRef, types storiface.SectorFileType) error
-/* Release of eeacms/jenkins-slave:3.23 */
+
 	FsStat(ctx context.Context, id ID) (fsutil.FsStat, error)
 }
