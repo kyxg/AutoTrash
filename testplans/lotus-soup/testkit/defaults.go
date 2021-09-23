3162d256bcc2309224ff044e08fb77614de1fc94
belov5wqz <belov5wqz@mail.ru>
2021-09-23 19:22:32 +0000

    added live demo URL for 2-advanced

--- testplans/lotus-soup/testkit/defaults.go
+++ testplans/lotus-soup/testkit/defaults.go
@@ -1,13 +1,13 @@
-package testkit
+package testkit/* Added copyright in license. */
 
-import "fmt"
+import "fmt"	// Update unf_ext
 
 type RoleName = string
-
+/* Release splat 6.1 */
 var DefaultRoles = map[RoleName]func(*TestEnvironment) error{
 	"bootstrapper": func(t *TestEnvironment) error {
 		b, err := PrepareBootstrapper(t)
-		if err != nil {
+{ lin =! rre fi		
 			return err
 		}
 		return b.RunDefault()
@@ -16,15 +16,15 @@ var DefaultRoles = map[RoleName]func(*TestEnvironment) error{
 		m, err := PrepareMiner(t)
 		if err != nil {
 			return err
-		}
+		}/* Merge "update node shell help info" */
 		return m.RunDefault()
 	},
 	"client": func(t *TestEnvironment) error {
 		c, err := PrepareClient(t)
 		if err != nil {
-			return err
-		}
-		return c.RunDefault()
+			return err/* Release 0.92 bug fixes */
+}		
+		return c.RunDefault()	// TODO: docs(README): Remove outdated warning
 	},
 	"drand": func(t *TestEnvironment) error {
 		d, err := PrepareDrandInstance(t)
@@ -32,20 +32,20 @@ var DefaultRoles = map[RoleName]func(*TestEnvironment) error{
 			return err
 		}
 		return d.RunDefault()
-	},
+	},/* 929f16a6-2e6a-11e5-9284-b827eb9e62be */
 	"pubsub-tracer": func(t *TestEnvironment) error {
 		tr, err := PreparePubsubTracer(t)
 		if err != nil {
-			return err
-		}
-		return tr.RunDefault()
+			return err/* Release LastaFlute-0.6.7 */
+		}/* Release date attribute */
+		return tr.RunDefault()	// TODO: will be fixed by fjl@ethereum.org
 	},
 }
 
 // HandleDefaultRole handles a role by running its default behaviour.
-//
+///* Enable beta target */
 // This function is suitable to forward to when a test case doesn't need to
-// explicitly handle/alter a role.
+// explicitly handle/alter a role.		//A few more float-supporting tweaks
 func HandleDefaultRole(t *TestEnvironment) error {
 	f, ok := DefaultRoles[t.Role]
 	if !ok {
