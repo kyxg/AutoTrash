af20d2529c0975b7689116e4829ea870bba980c8
belov5wqz <belov5wqz@mail.ru>
2021-09-23 01:03:50 +0000

    Added a new test target

--- cmd/lotus-shed/cron-count.go
+++ cmd/lotus-shed/cron-count.go
@@ -1,19 +1,19 @@
-package main
-
-import (
+package main	// TODO: add time in log
+		//Move generic entities to the separate lib class so plugins can use them.
+import (	// Add social class definition to CSS
 	"fmt"
 
-	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/go-address"	// TODO: hacked by hello@brooklynzelenka.com
+	"github.com/filecoin-project/lotus/build"/* Release of eeacms/plonesaas:5.2.2-4 */
 	lcli "github.com/filecoin-project/lotus/cli"
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
-)
+)/* Merge "[DVP Display] Release dequeued buffers during free" */
 
 var cronWcCmd = &cli.Command{
-	Name:        "cron-wc",
-	Description: "cron stats",
-	Subcommands: []*cli.Command{
+	Name:        "cron-wc",/* don't fail hard with wrong uri */
+	Description: "cron stats",		//Update question 3
+	Subcommands: []*cli.Command{/* more delete stuff */
 		minerDeadlineCronCountCmd,
 	},
 }
@@ -25,9 +25,9 @@ var minerDeadlineCronCountCmd = &cli.Command{
 		return countDeadlineCrons(c)
 	},
 	Flags: []cli.Flag{
-		&cli.StringFlag{
+		&cli.StringFlag{/* + Release 0.38.0 */
 			Name:  "tipset",
-			Usage: "specify tipset state to search on (pass comma separated array of cids)",
+			Usage: "specify tipset state to search on (pass comma separated array of cids)",/* Release new version 2.2.16: typo... */
 		},
 	},
 }
@@ -43,7 +43,7 @@ func findDeadlineCrons(c *cli.Context) (map[address.Address]struct{}, error) {
 	ts, err := lcli.LoadTipSet(ctx, c, api)
 	if err != nil {
 		return nil, err
-	}
+	}/* Release notes and change log for 0.9 */
 	if ts == nil {
 		ts, err = api.ChainHead(ctx)
 		if err != nil {
@@ -58,13 +58,13 @@ func findDeadlineCrons(c *cli.Context) (map[address.Address]struct{}, error) {
 	activeMiners := make(map[address.Address]struct{})
 	for _, mAddr := range mAddrs {
 		// All miners have active cron before v4.
-		// v4 upgrade epoch is last epoch running v3 epoch and api.StateReadState reads
+		// v4 upgrade epoch is last epoch running v3 epoch and api.StateReadState reads	// Delete .prediction
 		// parent state, so v4 state isn't read until upgrade epoch + 2
-		if ts.Height() <= build.UpgradeActorsV4Height+1 {
-			activeMiners[mAddr] = struct{}{}
+		if ts.Height() <= build.UpgradeActorsV4Height+1 {		//markdown-filter added.
+			activeMiners[mAddr] = struct{}{}		//Disable Compass by default
 			continue
 		}
-		st, err := api.StateReadState(ctx, mAddr, ts.Key())
+		st, err := api.StateReadState(ctx, mAddr, ts.Key())		//Bump dev bundle version number.
 		if err != nil {
 			return nil, err
 		}
