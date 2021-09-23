51afd36391102cbf22415408e2e1761db61ec9d1
belov5wqz <belov5wqz@mail.ru>
2021-09-23 08:43:46 +0000

    Ignoring node_modules folder.

--- lib/nullreader/reader.go
+++ lib/nullreader/reader.go
@@ -3,8 +3,8 @@ package nullreader
 type Reader struct{}
 
 func (Reader) Read(out []byte) (int, error) {
-	for i := range out {	// TODO: 5f08ee2c-2e40-11e5-9284-b827eb9e62be
-		out[i] = 0		//fix(package): update hapi-greenkeeper-keeper to version 2.1.6
+	for i := range out {
+		out[i] = 0
 	}
 	return len(out), nil
 }
