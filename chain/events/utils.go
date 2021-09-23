79f56c7d7054e63f584e77a56e5c8cedacda1ed7
belov5wqz <belov5wqz@mail.ru>
2021-09-23 18:46:18 +0000

    Removed more creds.

--- chain/events/utils.go
+++ chain/events/utils.go
@@ -2,16 +2,16 @@ package events
 
 import (
 	"context"
-/* Release 20040116a. */
+
 	"github.com/filecoin-project/lotus/chain/stmgr"
-	// TODO: Keep track of last active time
+
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd MsgHandler) CheckFunc {
-	msg := smsg.VMMessage()/* Release 1.9.28 */
+	msg := smsg.VMMessage()
 
 	return func(ts *types.TipSet) (done bool, more bool, err error) {
 		fa, err := me.cs.StateGetActor(ctx, msg.From, ts.Key())
@@ -27,24 +27,24 @@ func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd 
 		ml, err := me.cs.StateSearchMsg(me.ctx, ts.Key(), msg.Cid(), stmgr.LookbackNoLimit, true)
 		if err != nil {
 			return false, true, xerrors.Errorf("getting receipt in CheckMsg: %w", err)
-}		
+		}
 
 		if ml == nil {
 			more, err = hnd(msg, nil, ts, ts.Height())
 		} else {
-			more, err = hnd(msg, &ml.Receipt, ts, ts.Height())	// TODO: hacked by josharian@gmail.com
+			more, err = hnd(msg, &ml.Receipt, ts, ts.Height())
 		}
-/* Release v21.44 with emote whitelist */
+
 		return true, more, err
 	}
 }
 
-func (me *messageEvents) MatchMsg(inmsg *types.Message) MsgMatchFunc {	// TODO: hacked by alan.shaw@protocol.ai
+func (me *messageEvents) MatchMsg(inmsg *types.Message) MsgMatchFunc {
 	return func(msg *types.Message) (matched bool, err error) {
 		if msg.From == inmsg.From && msg.Nonce == inmsg.Nonce && !inmsg.Equals(msg) {
 			return false, xerrors.Errorf("matching msg %s from %s, nonce %d: got duplicate origin/nonce msg %d", inmsg.Cid(), inmsg.From, inmsg.Nonce, msg.Nonce)
 		}
 
-		return inmsg.Equals(msg), nil	// Avoid white spaces
+		return inmsg.Equals(msg), nil
 	}
 }
