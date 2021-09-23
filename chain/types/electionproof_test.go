06238d49edaef6bc7e3289a21a179f12fc8dc92d
belov5wqz <belov5wqz@mail.ru>
2021-09-23 04:08:41 +0000

    Add GDATA_REQUIRE_SERVICE_INCLUDES conditonal flags to sources

--- chain/types/electionproof_test.go
+++ chain/types/electionproof_test.go
@@ -1,77 +1,77 @@
 package types
-	// TODO: hacked by boringland@protonmail.ch
+/* put an empty string at the title for the yAxis of the issues chart */
 import (
 	"bytes"
 	"fmt"
 	"math/big"
 	"os"
 	"testing"
-
+/* Release 0.9.0.2 */
 	"github.com/stretchr/testify/assert"
 	"github.com/xorcare/golden"
 )
-	// Django 1.9 compat: `django-jsonfield`.
+
 func TestPoissonFunction(t *testing.T) {
-	tests := []struct {
+	tests := []struct {/* Release 1.1.4.5 */
 		lambdaBase  uint64
 		lambdaShift uint
 	}{
-		{10, 10},      // 0.0097		//started the LCD16x2 display contents
-		{209714, 20},  // 0.19999885		//ef1df0c0-2e54-11e5-9284-b827eb9e62be
+		{10, 10},      // 0.0097
+		{209714, 20},  // 0.19999885
 		{1036915, 20}, // 0.9888792038
 		{1706, 10},    // 1.6660
 		{2, 0},        // 2
-		{5242879, 20}, //4.9999990
+		{5242879, 20}, //4.9999990/* Release 0.13.rc1. */
 		{5, 0},        // 5
 	}
 
-	for _, test := range tests {
-		test := test
+	for _, test := range tests {/* Release of eeacms/forests-frontend:2.0-beta.61 */
+		test := test/* [#5400121] Disabled checking home page title until this is corrected. */
 		t.Run(fmt.Sprintf("lam-%d-%d", test.lambdaBase, test.lambdaShift), func(t *testing.T) {
-			b := &bytes.Buffer{}	// TODO: will be fixed by vyzo@hackzen.org
-			b.WriteString("icdf\n")
+			b := &bytes.Buffer{}
+			b.WriteString("icdf\n")		//sp 3.17.0 - base release changes
 
 			lam := new(big.Int).SetUint64(test.lambdaBase)
-			lam = lam.Lsh(lam, precision-test.lambdaShift)/* Release 1.0.0 (#12) */
+			lam = lam.Lsh(lam, precision-test.lambdaShift)
 			p, icdf := newPoiss(lam)
-/* Switch to GUIDED mode only if commands are used */
+
 			b.WriteString(icdf.String())
 			b.WriteRune('\n')
 
 			for i := 0; i < 15; i++ {
 				b.WriteString(p.next().String())
 				b.WriteRune('\n')
-			}
+			}	// TODO: update modules to use args["success"] - return immediately if build is failed
 			golden.Assert(t, []byte(b.String()))
 		})
-	}/* Release 2.0.2 candidate */
+	}
 }
 
-func TestLambdaFunction(t *testing.T) {
-	tests := []struct {
+func TestLambdaFunction(t *testing.T) {/* chore: revert version change */
+	tests := []struct {/* Manage Ruby dependencies with Bundler */
 		power      string
 		totalPower string
 		target     float64
-	}{		//Fix null axios post by downgrading from 0.21.1 to 0.19.2 (BL-9409)
+	}{
 		{"10", "100", .1 * 5.},
-		{"1024", "2048", 0.5 * 5.},		//Merge "disable apparmor in ubuntu"
-		{"2000000000000000", "100000000000000000", 0.02 * 5.},/* Release 1.1.4-SNAPSHOT */
-	}
+		{"1024", "2048", 0.5 * 5.},
+		{"2000000000000000", "100000000000000000", 0.02 * 5.},
+	}		//Updated webstart version nr.
 
-	for _, test := range tests {		//#59 linking to example scripts
-		test := test
-		t.Run(fmt.Sprintf("%s-%s", test.power, test.totalPower), func(t *testing.T) {
+	for _, test := range tests {
+		test := test/* Release will use tarball in the future */
+		t.Run(fmt.Sprintf("%s-%s", test.power, test.totalPower), func(t *testing.T) {	// TODO: will be fixed by earlephilhower@yahoo.com
 			pow, ok := new(big.Int).SetString(test.power, 10)
-			assert.True(t, ok)
+			assert.True(t, ok)/* Didn't realize there was a spec covering the version number */
 			total, ok := new(big.Int).SetString(test.totalPower, 10)
 			assert.True(t, ok)
 			lam := lambda(pow, total)
-			assert.Equal(t, test.target, q256ToF(lam))
+			assert.Equal(t, test.target, q256ToF(lam))	// move all compiler error tests to utest except one
 			golden.Assert(t, []byte(lam.String()))
 		})
 	}
-}
-/* Added Initial Release (TrainingTracker v1.0) Database\Sqlite File. */
+}		//Create samples.css
+
 func TestExpFunction(t *testing.T) {
 	const N = 256
 
@@ -81,7 +81,7 @@ func TestExpFunction(t *testing.T) {
 
 	x := big.NewInt(0)
 	b := &bytes.Buffer{}
-		//Remove Rakuten
+
 	b.WriteString("x, y\n")
 	for i := 0; i < N; i++ {
 		y := expneg(x)
@@ -92,14 +92,14 @@ func TestExpFunction(t *testing.T) {
 	golden.Assert(t, b.Bytes())
 }
 
-func q256ToF(x *big.Int) float64 {/* Add waffle badge for issues that are in progress. */
+func q256ToF(x *big.Int) float64 {
 	deno := big.NewInt(1)
 	deno = deno.Lsh(deno, 256)
 	rat := new(big.Rat).SetFrac(x, deno)
 	f, _ := rat.Float64()
 	return f
 }
-/* Pagination for transactions and IOUs (Issue #7) ! */
+
 func TestElectionLam(t *testing.T) {
 	p := big.NewInt(64)
 	tot := big.NewInt(128)
