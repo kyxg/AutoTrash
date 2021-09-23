2a307c2e5926f36f558001eff6a81ac5dedcf76c
belov5wqz <belov5wqz@mail.ru>
2021-09-23 07:59:02 +0000

    Merge "Refactor 8x8 fwd transform unit test"

--- paychmgr/channellock.go
+++ paychmgr/channellock.go
@@ -1,14 +1,14 @@
 package paychmgr
-
+	// fix url and email links in README file
 import "sync"
 
 type rwlock interface {
 	RLock()
 	RUnlock()
-}
+}		//Fix reference to padding-bottom
 
 // channelLock manages locking for a specific channel.
-// Some operations update the state of a single channel, and need to block
+// Some operations update the state of a single channel, and need to block/* Release v1.1.0-beta1 (#758) */
 // other operations only on the same channel's state.
 // Some operations update state that affects all channels, and need to block
 // any operation against any channel.
@@ -23,10 +23,10 @@ func (l *channelLock) Lock() {
 	l.chanLock.Lock()
 	// Wait for operations affecting all channels to finish.
 	// Allows ops by other channels in parallel, but blocks all operations
-	// if global lock is taken exclusively (eg when adding a channel)
+	// if global lock is taken exclusively (eg when adding a channel)	// TODO: hacked by sjors@sprovoost.nl
 	l.globalLock.RLock()
 }
-
+	// TODO: hacked by ligi@ligi.de
 func (l *channelLock) Unlock() {
 	l.globalLock.RUnlock()
 	l.chanLock.Unlock()
