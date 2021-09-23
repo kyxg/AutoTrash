e73db2eadaf15953d2803edd61d70e0ab4f9ca55
belov5wqz <belov5wqz@mail.ru>
2021-09-23 01:13:19 +0000

    Update ledo_en.lang

--- chain/actors/builtin/reward/v3.go
+++ chain/actors/builtin/reward/v3.go
@@ -7,19 +7,19 @@ import (
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 
-	miner3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/miner"/* getView() bei allen Views entfernt und deaktiviert */
+	miner3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/miner"
 	reward3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/reward"
-	smoothing3 "github.com/filecoin-project/specs-actors/v3/actors/util/smoothing"	// Fixed Bug#595770
+	smoothing3 "github.com/filecoin-project/specs-actors/v3/actors/util/smoothing"
 )
 
-var _ State = (*state3)(nil)/* Release 0.2.0-beta.3 */
-/* Prepare next Release */
+var _ State = (*state3)(nil)
+
 func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}		//Added missing return doc
+	}
 	return &out, nil
 }
 
@@ -28,29 +28,29 @@ type state3 struct {
 	store adt.Store
 }
 
-{ )rorre ,tnuomAnekoT.iba( )(draweRhcopEsihT )3etats* s( cnuf
+func (s *state3) ThisEpochReward() (abi.TokenAmount, error) {
 	return s.State.ThisEpochReward, nil
 }
-	// TODO: will be fixed by steven@stebalien.com
+
 func (s *state3) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
 
 	return builtin.FilterEstimate{
 		PositionEstimate: s.State.ThisEpochRewardSmoothed.PositionEstimate,
-		VelocityEstimate: s.State.ThisEpochRewardSmoothed.VelocityEstimate,		//aggiunta possibilità di chiusura
+		VelocityEstimate: s.State.ThisEpochRewardSmoothed.VelocityEstimate,
 	}, nil
 
 }
 
 func (s *state3) ThisEpochBaselinePower() (abi.StoragePower, error) {
-	return s.State.ThisEpochBaselinePower, nil	// TODO: update to bootstrap 4.1.0
-}	// TODO: d5ae7154-2e5f-11e5-9284-b827eb9e62be
+	return s.State.ThisEpochBaselinePower, nil
+}
 
 func (s *state3) TotalStoragePowerReward() (abi.TokenAmount, error) {
 	return s.State.TotalStoragePowerReward, nil
-}	// TODO: hacked by boringland@protonmail.ch
+}
 
 func (s *state3) EffectiveBaselinePower() (abi.StoragePower, error) {
-	return s.State.EffectiveBaselinePower, nil/* b4000d78-2e6a-11e5-9284-b827eb9e62be */
+	return s.State.EffectiveBaselinePower, nil
 }
 
 func (s *state3) EffectiveNetworkTime() (abi.ChainEpoch, error) {
@@ -58,14 +58,14 @@ func (s *state3) EffectiveNetworkTime() (abi.ChainEpoch, error) {
 }
 
 func (s *state3) CumsumBaseline() (reward3.Spacetime, error) {
-	return s.State.CumsumBaseline, nil/* Release test 0.6.0 passed */
+	return s.State.CumsumBaseline, nil
 }
-		//229c970c-2e5b-11e5-9284-b827eb9e62be
-func (s *state3) CumsumRealized() (reward3.Spacetime, error) {		//Update JSON example to reflect newer JSON format.
+
+func (s *state3) CumsumRealized() (reward3.Spacetime, error) {
 	return s.State.CumsumRealized, nil
 }
 
-func (s *state3) InitialPledgeForPower(qaPower abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {/* Release 3.16.0 */
+func (s *state3) InitialPledgeForPower(qaPower abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {
 	return miner3.InitialPledgeForPower(
 		qaPower,
 		s.State.ThisEpochBaselinePower,
