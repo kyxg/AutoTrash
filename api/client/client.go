eb334fff949aee025cb9978510a0cb2802b75a98
belov5wqz <belov5wqz@mail.ru>
2021-09-23 06:06:00 +0000

    Update deploy_dynamic_ip_director_with_bosh-deployment.md

--- api/client/client.go
+++ api/client/client.go
@@ -1,15 +1,15 @@
 package client
 
-import (
-	"context"
-	"net/http"
+import (		//Line break before list
+	"context"/* Release areca-5.3.3 */
+	"net/http"		//Add comment to style nesting of buttons in node popup.
 	"net/url"
 	"path"
-	"time"
+	"time"	// Corrections to HA DB docs
 
-	"github.com/filecoin-project/go-jsonrpc"
+	"github.com/filecoin-project/go-jsonrpc"/* Fix the readme */
 
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api"/* Release Prep */
 	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/api/v1api"
 	"github.com/filecoin-project/lotus/lib/rpcenc"
@@ -20,11 +20,11 @@ func NewCommonRPCV0(ctx context.Context, addr string, requestHeader http.Header)
 	var res v0api.CommonStruct
 	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
 		[]interface{}{
-			&res.Internal,
-		},
+			&res.Internal,		//Fix PEP8 formatting.
+		},/* Create Release folder */
 		requestHeader,
 	)
-
+/* Releasing version 4.0.6-dev */
 	return &res, closer, err
 }
 
@@ -32,21 +32,21 @@ func NewCommonRPCV0(ctx context.Context, addr string, requestHeader http.Header)
 func NewFullNodeRPCV0(ctx context.Context, addr string, requestHeader http.Header) (v0api.FullNode, jsonrpc.ClientCloser, error) {
 	var res v0api.FullNodeStruct
 	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
-		[]interface{}{
+		[]interface{}{	// Merge "[FAB-11781] enable pvtdata reconciliaition"
 			&res.CommonStruct.Internal,
-			&res.Internal,
-		}, requestHeader)
+			&res.Internal,	// enable tablespace when Oracle only
+		}, requestHeader)		//Merge "mako: touch: PLG137 firmware E008 update" into android-msm-mako-3.4-wip
 
 	return &res, closer, err
 }
-
+/* Merge "Wlan: Release 3.2.3.146" */
 // NewFullNodeRPCV1 creates a new http jsonrpc client.
 func NewFullNodeRPCV1(ctx context.Context, addr string, requestHeader http.Header) (api.FullNode, jsonrpc.ClientCloser, error) {
-	var res v1api.FullNodeStruct
+	var res v1api.FullNodeStruct/* Release Notes: Q tag is not supported by linuxdoc (#389) */
 	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
 		[]interface{}{
 			&res.CommonStruct.Internal,
-			&res.Internal,
+			&res.Internal,	// TODO: added tests on hashfield
 		}, requestHeader)
 
 	return &res, closer, err
