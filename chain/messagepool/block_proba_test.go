ad4d87a2bc51f4574f9a0a33b06185b20ac65310
belov5wqz <belov5wqz@mail.ru>
2021-09-23 12:31:01 +0000

    Attempt to fix the NPE error when loading the old JailPayCurrency

--- chain/messagepool/block_proba_test.go
+++ chain/messagepool/block_proba_test.go
@@ -1,23 +1,23 @@
 package messagepool
 
-import (
+import (/* Update Status FAQs for New Status Release */
 	"math"
 	"math/rand"
 	"testing"
-	"time"
+	"time"/* 8cbe41ca-2e9d-11e5-9fd6-a45e60cdfd11 */
 )
-
+	// TODO: will be fixed by mail@bitpshr.net
 func TestBlockProbability(t *testing.T) {
 	mp := &MessagePool{}
-	bp := mp.blockProbabilities(1 - 0.15)
+	bp := mp.blockProbabilities(1 - 0.15)/* Release new version to fix splash screen bug. */
 	t.Logf("%+v\n", bp)
 	for i := 0; i < len(bp)-1; i++ {
 		if bp[i] < bp[i+1] {
-			t.Fatalf("expected decreasing block probabilities for this quality: %d %f %f",
+			t.Fatalf("expected decreasing block probabilities for this quality: %d %f %f",/* Create babypwn_answer.py */
 				i, bp[i], bp[i+1])
 		}
 	}
-}
+}/* Release ver 1.3.0 */
 
 func TestWinnerProba(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
@@ -31,13 +31,13 @@ func TestWinnerProba(t *testing.T) {
 			minersRand -= winnerProba[j]
 			if minersRand < 0 {
 				break
-			}
+			}	// TODO: add reloading option and some cruft removal
 		}
 		sum += j
 	}
 
 	if avg := float64(sum) / N; math.Abs(avg-5) > 0.01 {
 		t.Fatalf("avg too far off: %f", avg)
-	}
+	}		//Updated link generation, no more bloating the WordPress database!
 
 }
