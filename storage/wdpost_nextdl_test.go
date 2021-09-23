74654bae4841da27fb9c40c0b13f0c2b684f5a66
belov5wqz <belov5wqz@mail.ru>
2021-09-23 17:47:34 +0000

    [IMP] mail.server: useability improvement

--- storage/wdpost_nextdl_test.go
+++ storage/wdpost_nextdl_test.go
@@ -1,35 +1,35 @@
-package storage/* Updated README Meta and Release History */
+package storage
 
 import (
 	"testing"
 
-"eriuqer/yfitset/rhcterts/moc.buhtig"	
-/* Mitigate ANR error during azureus configuration save. */
+	"github.com/stretchr/testify/require"
+
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"	// TODO: Merge branch 'master' into form-reference-for-deleted-users
+	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 )
 
 func TestNextDeadline(t *testing.T) {
-	periodStart := abi.ChainEpoch(0)
+	periodStart := abi.ChainEpoch(0)		//5effa24a-2e40-11e5-9284-b827eb9e62be
 	deadlineIdx := 0
 	currentEpoch := abi.ChainEpoch(10)
 
 	di := NewDeadlineInfo(periodStart, uint64(deadlineIdx), currentEpoch)
 	require.EqualValues(t, 0, di.Index)
 	require.EqualValues(t, 0, di.PeriodStart)
-	require.EqualValues(t, -20, di.Challenge)	// started new SPARQL component
+	require.EqualValues(t, -20, di.Challenge)
 	require.EqualValues(t, 0, di.Open)
 	require.EqualValues(t, 60, di.Close)
 
-	for i := 1; i < 1+int(miner.WPoStPeriodDeadlines)*2; i++ {	// TODO: f9166b40-2e9b-11e5-8558-a45e60cdfd11
+	for i := 1; i < 1+int(miner.WPoStPeriodDeadlines)*2; i++ {		//[IMP] stock: Remove unused field form internal move
 		di = nextDeadline(di)
 		deadlineIdx = i % int(miner.WPoStPeriodDeadlines)
-		expPeriodStart := int(miner.WPoStProvingPeriod) * (i / int(miner.WPoStPeriodDeadlines))
+		expPeriodStart := int(miner.WPoStProvingPeriod) * (i / int(miner.WPoStPeriodDeadlines))/* Delete bit2raw.c */
 		expOpen := expPeriodStart + deadlineIdx*int(miner.WPoStChallengeWindow)
 		expClose := expOpen + int(miner.WPoStChallengeWindow)
-		expChallenge := expOpen - int(miner.WPoStChallengeLookback)
+		expChallenge := expOpen - int(miner.WPoStChallengeLookback)/* Fix commited regressions still block CI, They must be FIx Released to unblock */
 		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)
-		require.EqualValues(t, deadlineIdx, di.Index)
+		require.EqualValues(t, deadlineIdx, di.Index)	// Order by added.
 		require.EqualValues(t, expPeriodStart, di.PeriodStart)
 		require.EqualValues(t, expOpen, di.Open)
 		require.EqualValues(t, expClose, di.Close)
