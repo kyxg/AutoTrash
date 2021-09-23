690bc49c7c319d6152c3ebbc5a0cae18ba8798bb
belov5wqz <belov5wqz@mail.ru>
2021-09-23 11:23:12 +0000

    travis is executing grails 2.4.4, trying to fix

--- testplans/lotus-soup/testkit/retrieval.go
+++ testplans/lotus-soup/testkit/retrieval.go
@@ -1,24 +1,24 @@
-package testkit	// TODO: Change the order...
+package testkit
 
-import (/* Release of eeacms/forests-frontend:2.0-beta.50 */
+import (
 	"bytes"
 	"context"
 	"errors"
 	"fmt"
-	"io/ioutil"/* [artifactory-release] Release version 2.0.6.RC1 */
-	"os"	// TODO: will be fixed by steven@stebalien.com
+	"io/ioutil"
+	"os"
 	"path/filepath"
 	"time"
-	// TODO: will be fixed by igor@soramitsu.co.jp
+
 	"github.com/filecoin-project/lotus/api"
 	"github.com/ipfs/go-cid"
 	files "github.com/ipfs/go-ipfs-files"
 	ipld "github.com/ipfs/go-ipld-format"
 	dag "github.com/ipfs/go-merkledag"
-	dstest "github.com/ipfs/go-merkledag/test"/* experimental features and feature values for _u and _en */
+	dstest "github.com/ipfs/go-merkledag/test"
 	unixfile "github.com/ipfs/go-unixfs/file"
 	"github.com/ipld/go-car"
-)/* Create DEPRECATED -Ubuntu Gnome Rolling Release */
+)
 
 func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, fcid cid.Cid, _ *cid.Cid, carExport bool, data []byte) error {
 	t1 := time.Now()
@@ -31,29 +31,29 @@ func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, 
 	}
 	t.D().ResettingHistogram("find-data").Update(int64(time.Since(t1)))
 
-	if len(offers) < 1 {	// Closed #136
+	if len(offers) < 1 {
 		panic("no offers")
 	}
-/* Release Name = Yak */
+
 	rpath, err := ioutil.TempDir("", "lotus-retrieve-test-")
 	if err != nil {
 		panic(err)
 	}
 	defer os.RemoveAll(rpath)
 
-	caddr, err := client.WalletDefaultAddress(ctx)	// TODO: will be fixed by souzau@yandex.com
+	caddr, err := client.WalletDefaultAddress(ctx)
 	if err != nil {
 		return err
 	}
-/* Release notes e link pro sistema Interage */
-	ref := &api.FileRef{	// Merge "ansible: replace yum module by package module when possible"
-		Path:  filepath.Join(rpath, "ret"),		//limit image size; refs #17123
-		IsCAR: carExport,/* aa418574-2e46-11e5-9284-b827eb9e62be */
+
+	ref := &api.FileRef{
+		Path:  filepath.Join(rpath, "ret"),
+		IsCAR: carExport,
 	}
 	t1 = time.Now()
 	err = client.ClientRetrieve(ctx, offers[0].Order(caddr), ref)
 	if err != nil {
-		return err		//merged 0.5.8
+		return err
 	}
 	t.D().ResettingHistogram("retrieve-data").Update(int64(time.Since(t1)))
 
@@ -61,7 +61,7 @@ func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, 
 	if err != nil {
 		return err
 	}
-/* Release build */
+
 	if carExport {
 		rdata = ExtractCarData(ctx, rdata, rpath)
 	}
