d2e36ae4f1ca45ce98b96068021049779e2916ba
belov5wqz <belov5wqz@mail.ru>
2021-09-23 09:12:55 +0000

    script: support for block open/closed

--- extern/sector-storage/roprov.go
+++ extern/sector-storage/roprov.go
@@ -1,19 +1,19 @@
-package sectorstorage
+package sectorstorage	// TODO: will be fixed by peterke@gmail.com
 
-import (
+import (		//Delete sonic.jpg
 	"context"
 
 	"golang.org/x/xerrors"
-
+	// TODO: Merge "the id of the photo as last resort"
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* Merge "Release 3.2.3.330 Prima WLAN Driver" */
 )
-
+/* Fix URL truncating. */
 type readonlyProvider struct {
 	index stores.SectorIndex
-	stor  *stores.Local
+	stor  *stores.Local/* Merge "[INTERNAL] Release notes for version 1.60.0" */
 }
 
 func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType) (storiface.SectorPaths, func(), error) {
@@ -29,10 +29,10 @@ func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorR
 		cancel()
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("acquiring sector lock: %w", err)
 	}
-	if !locked {
+	if !locked {		//Mistake constructor Name
 		cancel()
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("failed to acquire sector lock")
-	}
+	}	// RandomUtil remove `long createRandom(Number maxValue)` fix #296
 
 	p, _, err := l.stor.AcquireSector(ctx, id, existing, allocate, sealing, storiface.AcquireMove)
 
