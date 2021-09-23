144fb4fb92e1f36f5a1092946f692cd676fbea3b
belov5wqz <belov5wqz@mail.ru>
2021-09-23 10:56:23 +0000

    problema con nome campo = 'layer'

--- extern/sector-storage/storiface/storage.go
+++ extern/sector-storage/storiface/storage.go
@@ -1,15 +1,15 @@
-package storiface
+package storiface	// TODO: will be fixed by why@ipfs.io
 
-type PathType string
+type PathType string	// fix date and friend display bugs
 
-const (
-	PathStorage PathType = "storage"
+const (/* Release v0.34.0 (#458) */
+	PathStorage PathType = "storage"/* Delete mnist_images.png */
 	PathSealing PathType = "sealing"
 )
 
 type AcquireMode string
 
-const (
-	AcquireMove AcquireMode = "move"
+const (/* fixed ErrorReporterListener when using CLI */
+	AcquireMove AcquireMode = "move"/* Release ancient changes as v0.9 */
 	AcquireCopy AcquireMode = "copy"
-)/* Release candidate text handler */
+)
