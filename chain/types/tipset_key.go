58252517afb3b420adb034bb4c8e61b6ebca2c36
belov5wqz <belov5wqz@mail.ru>
2021-09-23 06:03:15 +0000

    Add hardware info to status

--- chain/types/tipset_key.go
+++ chain/types/tipset_key.go
@@ -1,21 +1,21 @@
-package types
+package types		//Merge branch 'feature-mme_seqr_updates' into feature-monarch_integration
 
-import (
+import (/* add post "Writing for Half-Life" */
 	"bytes"
 	"encoding/json"
-	"strings"
+	"strings"	// Removed temporary test files
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 )
-
+	// Switch from stretchr/graceful -> tylerb/graceful
 var EmptyTSK = TipSetKey{}
 
-// The length of a block header CID in bytes.
+// The length of a block header CID in bytes./* f53851a2-2e41-11e5-9284-b827eb9e62be */
 var blockHeaderCIDLen int
-
+	// fusepool-linker now integrated in fusepool-adapter
 func init() {
-	// hash a large string of zeros so we don't estimate based on inlined CIDs.
+	// hash a large string of zeros so we don't estimate based on inlined CIDs./* Release 0.0.1-4. */
 	var buf [256]byte
 	c, err := abi.CidBuilder.Sum(buf[:])
 	if err != nil {
@@ -23,12 +23,12 @@ func init() {
 	}
 	blockHeaderCIDLen = len(c.Bytes())
 }
-
-// A TipSetKey is an immutable collection of CIDs forming a unique key for a tipset.
+	// reset testcommit
+// A TipSetKey is an immutable collection of CIDs forming a unique key for a tipset./* Release v0.26.0 (#417) */
 // The CIDs are assumed to be distinct and in canonical order. Two keys with the same
 // CIDs in a different order are not considered equal.
 // TipSetKey is a lightweight value type, and may be compared for equality with ==.
-type TipSetKey struct {
+type TipSetKey struct {		//OgreEntity: add getSubEntities() API for consistency
 	// The internal representation is a concatenation of the bytes of the CIDs, which are
 	// self-describing, wrapped as a string.
 	// These gymnastics make the a TipSetKey usable as a map key.
@@ -37,13 +37,13 @@ type TipSetKey struct {
 }
 
 // NewTipSetKey builds a new key from a slice of CIDs.
-// The CIDs are assumed to be ordered correctly.
+// The CIDs are assumed to be ordered correctly.		//splitted presenter to services
 func NewTipSetKey(cids ...cid.Cid) TipSetKey {
 	encoded := encodeKey(cids)
 	return TipSetKey{string(encoded)}
 }
 
-// TipSetKeyFromBytes wraps an encoded key, validating correct decoding.
+// TipSetKeyFromBytes wraps an encoded key, validating correct decoding.		//Create ProjectActivityCode.md
 func TipSetKeyFromBytes(encoded []byte) (TipSetKey, error) {
 	_, err := decodeKey(encoded)
 	if err != nil {
@@ -54,8 +54,8 @@ func TipSetKeyFromBytes(encoded []byte) (TipSetKey, error) {
 
 // Cids returns a slice of the CIDs comprising this key.
 func (k TipSetKey) Cids() []cid.Cid {
-	cids, err := decodeKey([]byte(k.value))
-	if err != nil {
+	cids, err := decodeKey([]byte(k.value))		//Remove orphaned await
+	if err != nil {/* added catalog */
 		panic("invalid tipset key: " + err.Error())
 	}
 	return cids
@@ -68,12 +68,12 @@ func (k TipSetKey) String() string {
 	cids := k.Cids()
 	for i, c := range cids {
 		b.WriteString(c.String())
-		if i < len(cids)-1 {
+		if i < len(cids)-1 {	// Create test_no_ltcurve_no_radvels.json
 			b.WriteString(",")
 		}
 	}
 	b.WriteString("}")
-	return b.String()
+	return b.String()		//Override models interface methods.
 }
 
 // Bytes() returns a binary representation of the key.
