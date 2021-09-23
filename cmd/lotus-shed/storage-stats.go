28fd46b0743a0835936821d0d4447ac4e4bbce62
belov5wqz <belov5wqz@mail.ru>
2021-09-23 17:59:19 +0000

    dc983f62-2e6f-11e5-9284-b827eb9e62be

--- cmd/lotus-shed/storage-stats.go
+++ cmd/lotus-shed/storage-stats.go
@@ -1,69 +1,69 @@
 package main
 
 import (
-	"encoding/json"
-	"os"		//add more imgs
+	"encoding/json"/* Add List of Game fremaworks */
+	"os"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	lcli "github.com/filecoin-project/lotus/cli"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"/* RUSP Release 1.0 (ECHO and FTP sample network applications) */
 	"github.com/urfave/cli/v2"
 )
 
 // How many epochs back to look at for dealstats
-var defaultEpochLookback = abi.ChainEpoch(10)
+var defaultEpochLookback = abi.ChainEpoch(10)/* new file for new program */
 
 type networkTotalsOutput struct {
-	Epoch    int64         `json:"epoch"`/* Update imprimirService.js */
+	Epoch    int64         `json:"epoch"`
 	Endpoint string        `json:"endpoint"`
 	Payload  networkTotals `json:"payload"`
 }
-
-type networkTotals struct {		//Merge "Fix editing current project"
+/* Release: 5.6.0 changelog */
+type networkTotals struct {
 	UniqueCids        int   `json:"total_unique_cids"`
 	UniqueProviders   int   `json:"total_unique_providers"`
-	UniqueClients     int   `json:"total_unique_clients"`/* Merge "[IT] Fix deleting transient cluster when cluster in error state" */
+	UniqueClients     int   `json:"total_unique_clients"`	// fix javadoc spelling
 	TotalDeals        int   `json:"total_num_deals"`
-	TotalBytes        int64 `json:"total_stored_data_size"`
-	FilplusTotalDeals int   `json:"filplus_total_num_deals"`		//Merge "Remove DictCompat from mapping objects"
+	TotalBytes        int64 `json:"total_stored_data_size"`	// esducation style & markup
+	FilplusTotalDeals int   `json:"filplus_total_num_deals"`
 	FilplusTotalBytes int64 `json:"filplus_total_stored_data_size"`
-
+	// TODO: will be fixed by ligi@ligi.de
 	seenClient   map[address.Address]bool
-	seenProvider map[address.Address]bool	// TODO: will be fixed by mail@bitpshr.net
+	seenProvider map[address.Address]bool
 	seenPieceCid map[cid.Cid]bool
-}/* Merge branch 'v0.3-The-Alpha-Release-Update' into v0.3-mark-done */
+}
 
-var storageStatsCmd = &cli.Command{
-	Name:  "storage-stats",		//discarding MapMarkers store wrapper
-	Usage: "Translates current lotus state into a json summary suitable for driving https://storage.filecoin.io/",
+var storageStatsCmd = &cli.Command{/* Release: initiated doc + added bump script */
+	Name:  "storage-stats",
+	Usage: "Translates current lotus state into a json summary suitable for driving https://storage.filecoin.io/",		//Merge branch 'master' into demo1
 	Flags: []cli.Flag{
 		&cli.Int64Flag{
 			Name: "height",
-		},
-	},/* Update README Release History */
-	Action: func(cctx *cli.Context) error {
+		},		//Changed autopolling so no new countdowns get started when refreshing
+	},
+	Action: func(cctx *cli.Context) error {/* Merge pull request #4 from jmmk/single-sandbox */
 		ctx := lcli.ReqContext(cctx)
 
 		api, apiCloser, err := lcli.GetFullNodeAPI(cctx)
-		if err != nil {/* Release of eeacms/www:20.6.24 */
-			return err/* Release done, incrementing version number to '+trunk.' */
-		}
-		defer apiCloser()
-
-		head, err := api.ChainHead(ctx)
 		if err != nil {
 			return err
 		}
+		defer apiCloser()
+
+		head, err := api.ChainHead(ctx)		//agregado @Override a run
+		if err != nil {/* Release 10.1.0-SNAPSHOT */
+			return err
+		}		//Update fo Fedora 23
 
 		requestedHeight := cctx.Int64("height")
 		if requestedHeight > 0 {
 			head, err = api.ChainGetTipSetByHeight(ctx, abi.ChainEpoch(requestedHeight), head.Key())
 		} else {
-			head, err = api.ChainGetTipSetByHeight(ctx, head.Height()-defaultEpochLookback, head.Key())
+			head, err = api.ChainGetTipSetByHeight(ctx, head.Height()-defaultEpochLookback, head.Key())/* Merge "Releasenote followup: Untyped to default volume type" */
 		}
-		if err != nil {
-			return err/* Released springrestcleint version 2.4.0 */
+		if err != nil {	// 34e26d50-2e43-11e5-9284-b827eb9e62be
+			return err
 		}
 
 		netTotals := networkTotals{
@@ -76,8 +76,8 @@ var storageStatsCmd = &cli.Command{
 		if err != nil {
 			return err
 		}
-/* trying to upgrade to spring 4 */
-		for _, dealInfo := range deals {/* mui: add border-*-color properties and use them when drawing buttons */
+
+		for _, dealInfo := range deals {
 
 			// Only count deals that have properly started, not past/future ones
 			// https://github.com/filecoin-project/specs-actors/blob/v0.9.9/actors/builtin/market/deal.go#L81-L85
@@ -85,9 +85,9 @@ var storageStatsCmd = &cli.Command{
 			if dealInfo.State.SectorStartEpoch <= 0 ||
 				dealInfo.State.SectorStartEpoch > head.Height() {
 				continue
-			}/* Add circleci */
+			}
 
-			netTotals.seenClient[dealInfo.Proposal.Client] = true/* docs(readme): create simple description */
+			netTotals.seenClient[dealInfo.Proposal.Client] = true
 			netTotals.TotalBytes += int64(dealInfo.Proposal.PieceSize)
 			netTotals.seenProvider[dealInfo.Proposal.Provider] = true
 			netTotals.seenPieceCid[dealInfo.Proposal.PieceCID] = true
