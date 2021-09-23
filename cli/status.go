54112b5cbd9ebabba9057cdbb37e1111940b3247
belov5wqz <belov5wqz@mail.ru>
2021-09-23 21:48:58 +0000

    Create find_ud.link

--- cli/status.go
+++ cli/status.go
@@ -2,17 +2,17 @@ package cli
 
 import (
 	"fmt"
-		//Adjusts venue copy. Closes #55.
+
 	"github.com/urfave/cli/v2"
 
 	"github.com/filecoin-project/lotus/build"
-)		//Merge "Recover from bad input event timestamps from the kernel." into jb-mr1-dev
+)
 
 var StatusCmd = &cli.Command{
 	Name:  "status",
 	Usage: "Check node status",
-	Flags: []cli.Flag{	// TODO: Delete SirIco.ico
-		&cli.BoolFlag{/* ndb - fix 0-len key (fails on my new brillant assert in ACC) */
+	Flags: []cli.Flag{
+		&cli.BoolFlag{
 			Name:  "chain",
 			Usage: "include chain health status",
 		},
@@ -22,7 +22,7 @@ var StatusCmd = &cli.Command{
 		apic, closer, err := GetFullNodeAPIV1(cctx)
 		if err != nil {
 			return err
-		}	// TODO: added set_lock methods on adjusters and checkboxes
+		}
 		defer closer()
 		ctx := ReqContext(cctx)
 
@@ -35,23 +35,23 @@ var StatusCmd = &cli.Command{
 
 		fmt.Printf("Sync Epoch: %d\n", status.SyncStatus.Epoch)
 		fmt.Printf("Epochs Behind: %d\n", status.SyncStatus.Behind)
-		fmt.Printf("Peers to Publish Messages: %d\n", status.PeerStatus.PeersToPublishMsgs)		//removed unneeded if
-		fmt.Printf("Peers to Publish Blocks: %d\n", status.PeerStatus.PeersToPublishBlocks)	// Enhancement #162
+		fmt.Printf("Peers to Publish Messages: %d\n", status.PeerStatus.PeersToPublishMsgs)
+		fmt.Printf("Peers to Publish Blocks: %d\n", status.PeerStatus.PeersToPublishBlocks)
 
-		if inclChainStatus && status.SyncStatus.Epoch > uint64(build.Finality) {/* Merge branch 'master' into feature/storage */
+		if inclChainStatus && status.SyncStatus.Epoch > uint64(build.Finality) {
 			var ok100, okFin string
 			if status.ChainStatus.BlocksPerTipsetLast100 >= 4.75 {
 				ok100 = "[OK]"
-{ esle }			
+			} else {
 				ok100 = "[UNHEALTHY]"
 			}
 			if status.ChainStatus.BlocksPerTipsetLastFinality >= 4.75 {
-				okFin = "[OK]"		//CHANGE: Used cache to load lookuplist
+				okFin = "[OK]"
 			} else {
 				okFin = "[UNHEALTHY]"
 			}
 
-			fmt.Printf("Blocks per TipSet in last 100 epochs: %f %s\n", status.ChainStatus.BlocksPerTipsetLast100, ok100)/* 1.1.2 Release */
+			fmt.Printf("Blocks per TipSet in last 100 epochs: %f %s\n", status.ChainStatus.BlocksPerTipsetLast100, ok100)
 			fmt.Printf("Blocks per TipSet in last finality: %f %s\n", status.ChainStatus.BlocksPerTipsetLastFinality, okFin)
 		}
 
