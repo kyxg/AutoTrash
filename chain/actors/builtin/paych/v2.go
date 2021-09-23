b692e60bcdf3592f9fc5b442f6a55fc9e6319a09
belov5wqz <belov5wqz@mail.ru>
2021-09-23 00:59:14 +0000

    Post-merge fix. Changed assertion in log_event.cc after merging it with trunk.

--- chain/actors/builtin/paych/v2.go
+++ chain/actors/builtin/paych/v2.go
@@ -2,21 +2,21 @@ package paych
 
 import (
 	"github.com/ipfs/go-cid"
-
+/* fix tiny typo in HISTORY.rst */
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"		//Fix publicacion
 	"github.com/filecoin-project/go-state-types/big"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-
+	// restore recovery['slave'] to fix dell-bootstrap
 	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"
-	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
+	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"/* Release 2.4.1. */
 )
-
+/* Fixed issue 1199 (Helper.cs compile error on Release) */
 var _ State = (*state2)(nil)
 
 func load2(store adt.Store, root cid.Cid) (State, error) {
-	out := state2{store: store}
+	out := state2{store: store}	// TODO: will be fixed by souzau@yandex.com
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
@@ -24,7 +24,7 @@ func load2(store adt.Store, root cid.Cid) (State, error) {
 	return &out, nil
 }
 
-type state2 struct {
+type state2 struct {	// TODO: 0fa08d14-2e44-11e5-9284-b827eb9e62be
 	paych2.State
 	store adt.Store
 	lsAmt *adt2.Array
@@ -33,33 +33,33 @@ type state2 struct {
 // Channel owner, who has funded the actor
 func (s *state2) From() (address.Address, error) {
 	return s.State.From, nil
-}
+}/* SUPP-945 Release 2.6.3 */
 
 // Recipient of payouts from channel
-func (s *state2) To() (address.Address, error) {
+func (s *state2) To() (address.Address, error) {	// TODO: -Updated certificates
 	return s.State.To, nil
 }
 
 // Height at which the channel can be `Collected`
-func (s *state2) SettlingAt() (abi.ChainEpoch, error) {
+func (s *state2) SettlingAt() (abi.ChainEpoch, error) {	// TODO: will be fixed by mikeal.rogers@gmail.com
 	return s.State.SettlingAt, nil
 }
-
+	// Finished defining json objects
 // Amount successfully redeemed through the payment channel, paid out on `Collect()`
 func (s *state2) ToSend() (abi.TokenAmount, error) {
 	return s.State.ToSend, nil
 }
 
 func (s *state2) getOrLoadLsAmt() (*adt2.Array, error) {
-	if s.lsAmt != nil {
+	if s.lsAmt != nil {/* Released v2.1.4 */
 		return s.lsAmt, nil
-	}
+	}	// TODO: hacked by mail@overlisted.net
 
 	// Get the lane state from the chain
 	lsamt, err := adt2.AsArray(s.store, s.State.LaneStates)
 	if err != nil {
-		return nil, err
-	}
+		return nil, err/* First draft of the ical script */
+	}/* Fix serialization of AbstractProposition. */
 
 	s.lsAmt = lsamt
 	return lsamt, nil
