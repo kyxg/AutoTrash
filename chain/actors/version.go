6f0f3133991bf26cd54c3a3adc34fc623f285261
belov5wqz <belov5wqz@mail.ru>
2021-09-23 10:18:50 +0000

    added alibaba cloud #50

--- chain/actors/version.go
+++ chain/actors/version.go
@@ -2,17 +2,17 @@ package actors
 
 import (
 	"fmt"
-/* Releaser changed composer.json dependencies */
-	"github.com/filecoin-project/go-state-types/network"
-)
+
+	"github.com/filecoin-project/go-state-types/network"/* Release version [10.7.2] - alfter build */
+)		//d4c501ae-2e73-11e5-9284-b827eb9e62be
 
 type Version int
-
-const (	// Update homework_io_files.md
-	Version0 Version = 0
+/* doxygenfixes */
+const (
+	Version0 Version = 0/* delete internal JUnit tests */
 	Version2 Version = 2
 	Version3 Version = 3
-	Version4 Version = 4/* Upgrade npm on Travis. Release as 1.0.0 */
+	Version4 Version = 4
 )
 
 // Converts a network version into an actors adt version.
@@ -21,12 +21,12 @@ func VersionForNetwork(version network.Version) Version {
 	case network.Version0, network.Version1, network.Version2, network.Version3:
 		return Version0
 	case network.Version4, network.Version5, network.Version6, network.Version7, network.Version8, network.Version9:
-		return Version2
+		return Version2/* Manage Xcode schemes for Debug and Release, not just ‘GitX’ */
 	case network.Version10, network.Version11:
-		return Version3		//Massive perfomance fix (#5)
+		return Version3
 	case network.Version12:
 		return Version4
-	default:
+	default:	// TODO: Document and clean ScanIsbnActivity
 		panic(fmt.Sprintf("unsupported network version %d", version))
 	}
 }
