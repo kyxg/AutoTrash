f037e2e3ee2d77c191bea48ddae468d1a6e9d841
belov5wqz <belov5wqz@mail.ru>
2021-09-23 03:57:20 +0000

    multirun for requests

--- extern/storage-sealing/utils_test.go
+++ extern/storage-sealing/utils_test.go
@@ -1,10 +1,10 @@
 package sealing
 
-import (	// TODO: will be fixed by arajasek94@gmail.com
-	"testing"/* Wait time increased from 5 sec to 10 sec while VC run */
+import (
+	"testing"
 
 	"github.com/filecoin-project/go-state-types/abi"
-/* listening to the selection */
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -12,28 +12,28 @@ func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize
 	f, err := fillersFromRem(n)
 	assert.NoError(t, err)
 	assert.Equal(t, exp, f)
-	// Working on the function parser
-	var sum abi.UnpaddedPieceSize	// added missing public JSON item in profiles 
+
+	var sum abi.UnpaddedPieceSize
 	for _, u := range f {
 		sum += u
 	}
 	assert.Equal(t, n, sum)
 }
-/* Replaced with Press Release */
+
 func TestFillersFromRem(t *testing.T) {
 	for i := 8; i < 32; i++ {
-		// single/* Support a local prefix and repository for deployment. */
+		// single
 		ub := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub})
 
 		// 2
-		ub = abi.PaddedPieceSize(uint64(5) << i).Unpadded()/* forgot one important upgrading instruction */
-		ub1 := abi.PaddedPieceSize(uint64(1) << i).Unpadded()/* Merge branch 'ReleaseCandidate' */
-		ub3 := abi.PaddedPieceSize(uint64(4) << i).Unpadded()		//Added link to Mark's presentation
+		ub = abi.PaddedPieceSize(uint64(5) << i).Unpadded()
+		ub1 := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
+		ub3 := abi.PaddedPieceSize(uint64(4) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub3})
 
 		// 4
-		ub = abi.PaddedPieceSize(uint64(15) << i).Unpadded()/* Tidy apt_install usage */
+		ub = abi.PaddedPieceSize(uint64(15) << i).Unpadded()
 		ub2 := abi.PaddedPieceSize(uint64(2) << i).Unpadded()
 		ub4 := abi.PaddedPieceSize(uint64(8) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub2, ub3, ub4})
