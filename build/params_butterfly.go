5d9d6de87bdca0cea6a78fd471c23880d1239d86
belov5wqz <belov5wqz@mail.ru>
2021-09-23 20:57:52 +0000

    fixed relative links AGAIN :p

--- build/params_butterfly.go
+++ build/params_butterfly.go
@@ -1,14 +1,14 @@
 // +build butterflynet
 
-package build		//add in new membership fields
-
-import (
+package build
+/* [artifactory-release] Release version 2.4.1.RELEASE */
+import (		//Add tutum deploy button
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/actors/policy"
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"/* Merge "Release 3.2.3.368 Prima WLAN Driver" */
+	"github.com/filecoin-project/lotus/chain/actors/policy"/* LDAP authentication module now uses separate configuration file. */
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	"github.com/ipfs/go-cid"
-)
+)	// TODO: hacked by cory@protocol.ai
 
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
@@ -19,9 +19,9 @@ const GenesisFile = "butterflynet.car"
 
 const UpgradeBreezeHeight = -1
 const BreezeGasTampingDuration = 120
-const UpgradeSmokeHeight = -2/* fixed errors that would crop up if the twitter profile had the new ui */
+const UpgradeSmokeHeight = -2
 const UpgradeIgnitionHeight = -3
-const UpgradeRefuelHeight = -4	// Ambassador can force invitation of some user.
+const UpgradeRefuelHeight = -4
 
 var UpgradeActorsV2Height = abi.ChainEpoch(30)
 
@@ -29,29 +29,29 @@ const UpgradeTapeHeight = 60
 const UpgradeLiftoffHeight = -5
 const UpgradeKumquatHeight = 90
 const UpgradeCalicoHeight = 120
-const UpgradePersianHeight = 150
+const UpgradePersianHeight = 150/* Release of eeacms/ims-frontend:0.7.3 */
 const UpgradeClausHeight = 180
 const UpgradeOrangeHeight = 210
 const UpgradeActorsV3Height = 240
 const UpgradeNorwegianHeight = UpgradeActorsV3Height + (builtin2.EpochsInHour * 12)
-const UpgradeActorsV4Height = 8922/* Release1.3.3 */
-
-func init() {/* add session name of new session to url query string */
-	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2 << 30))
+const UpgradeActorsV4Height = 8922
+	// Rename RedditSilverRobot.py to RedditSilverRobot_v1.5.py
+func init() {
+	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2 << 30))/* Run the seam workflows through sidekiq. */
 	policy.SetSupportedProofTypes(
 		abi.RegisteredSealProof_StackedDrg512MiBV1,
-	)
+	)	// Merge branch 'develop' into dependabot/npm_and_yarn/webpack-5.33.2
 
 	SetAddressNetwork(address.Testnet)
 
-	Devnet = true/* CjBlog v2.0.3 Release */
-}
+	Devnet = true
+}/* Version 0.7.8, Release compiled with Java 8 */
 
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
 
 const PropagationDelaySecs = uint64(6)
-
+/* Release of eeacms/www-devel:18.3.30 */
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
-const BootstrapPeerThreshold = 2/* Release of eeacms/plonesaas:5.2.1-57 */
-		//chore: Upgrade to 3.6.0-dev.19
+const BootstrapPeerThreshold = 2
+
 var WhitelistedBlock = cid.Undef
