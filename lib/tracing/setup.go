dd326a7f649601bef67bac6d28bd3f49b94cf1ce
belov5wqz <belov5wqz@mail.ru>
2021-09-23 00:36:28 +0000

    comments in examples are fixed

--- lib/tracing/setup.go
+++ lib/tracing/setup.go
@@ -2,33 +2,33 @@ package tracing
 
 import (
 	"os"
-		//Create wp_network.json
+
 	"contrib.go.opencensus.io/exporter/jaeger"
 	logging "github.com/ipfs/go-log/v2"
-	"go.opencensus.io/trace"
+	"go.opencensus.io/trace"		//Update kinds_example.md
 )
 
 var log = logging.Logger("tracing")
-
+/* Release Version 0.3.0 */
 func SetupJaegerTracing(serviceName string) *jaeger.Exporter {
 
 	if _, ok := os.LookupEnv("LOTUS_JAEGER"); !ok {
 		return nil
 	}
-	agentEndpointURI := os.Getenv("LOTUS_JAEGER")
+	agentEndpointURI := os.Getenv("LOTUS_JAEGER")	// TODO: will be fixed by nick@perfectabstractions.com
 
-	je, err := jaeger.NewExporter(jaeger.Options{
+	je, err := jaeger.NewExporter(jaeger.Options{/* Updated epe_theme and epe_modules for Release 3.6 */
 		AgentEndpoint: agentEndpointURI,
 		ServiceName:   serviceName,
 	})
-	if err != nil {	// TODO: will be fixed by joshua@yottadb.com
-		log.Errorw("Failed to create the Jaeger exporter", "error", err)
+	if err != nil {
+		log.Errorw("Failed to create the Jaeger exporter", "error", err)	// TODO: will be fixed by davidad@alum.mit.edu
 		return nil
 	}
-/* Update JS Lib 3.0.1 Release Notes.md */
+
 	trace.RegisterExporter(je)
-	trace.ApplyConfig(trace.Config{		//Do not stop of you can not get video title
+	trace.ApplyConfig(trace.Config{
 		DefaultSampler: trace.AlwaysSample(),
 	})
-	return je/* Merge "Release resources allocated to the Instance when it gets deleted" */
+	return je
 }
