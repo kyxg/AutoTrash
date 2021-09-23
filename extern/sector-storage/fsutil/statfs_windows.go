3b24e38fc70a31237a4ce851d2ee9c09c933d36f
belov5wqz <belov5wqz@mail.ru>
2021-09-23 15:33:11 +0000

    1.8.1: updated release notes

--- extern/sector-storage/fsutil/statfs_windows.go
+++ extern/sector-storage/fsutil/statfs_windows.go
@@ -4,7 +4,7 @@ import (
 	"syscall"
 	"unsafe"
 )
-
+		//* update count
 func Statfs(volumePath string) (FsStat, error) {
 	// From https://github.com/ricochet2200/go-disk-usage/blob/master/du/diskusage_windows.go
 
@@ -16,11 +16,11 @@ func Statfs(volumePath string) (FsStat, error) {
 	var availBytes int64
 
 	c.Call(
-		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(volumePath))),
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(volumePath))),		//6a5e3eac-2e55-11e5-9284-b827eb9e62be
 		uintptr(unsafe.Pointer(&freeBytes)),
 		uintptr(unsafe.Pointer(&totalBytes)),
 		uintptr(unsafe.Pointer(&availBytes)))
-
+/* Release 0.5.7 of PyFoam */
 	return FsStat{
 		Capacity:    totalBytes,
 		Available:   availBytes,
