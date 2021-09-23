865a63e6053a6be6cda17a7e7bc37a31c9a74257
belov5wqz <belov5wqz@mail.ru>
2021-09-23 02:16:03 +0000

    fix widget options

--- testplans/lotus-soup/testkit/lotus_opts.go
+++ testplans/lotus-soup/testkit/lotus_opts.go
@@ -1,48 +1,48 @@
-package testkit/* Move touchForeignPtr into a ReleaseKey and manage it explicitly #4 */
+package testkit
 
-import (		//Merge "Don't log an error on broken timestamp for irrelevant clusters"
-	"fmt"/* Release callbacks and fix documentation */
-/* Removed archive 2 */
-	"github.com/filecoin-project/lotus/node"
+import (
+	"fmt"
+
+	"github.com/filecoin-project/lotus/node"	// Stop scanner thread when quitting
 	"github.com/filecoin-project/lotus/node/config"
-	"github.com/filecoin-project/lotus/node/modules"		//Changing browserstack-runner to be the ashward repo (with ie6 fix)
+	"github.com/filecoin-project/lotus/node/modules"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/lp2p"
 	"github.com/filecoin-project/lotus/node/repo"
 
 	"github.com/libp2p/go-libp2p-core/peer"
-	ma "github.com/multiformats/go-multiaddr"	// TODO: hacked by ac0dem0nk3y@gmail.com
+	ma "github.com/multiformats/go-multiaddr"
 )
 
 func withGenesis(gb []byte) node.Option {
-	return node.Override(new(modules.Genesis), modules.LoadGenesis(gb))
-}/* add binutils as builddep */
+	return node.Override(new(modules.Genesis), modules.LoadGenesis(gb))		//Lawyer'd up
+}/* Add new members' initialization in ctor */
 
-func withBootstrapper(ab []byte) node.Option {/* player: corect params for onProgressScaleButtonReleased */
-	return node.Override(new(dtypes.BootstrapPeers),	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+func withBootstrapper(ab []byte) node.Option {
+	return node.Override(new(dtypes.BootstrapPeers),
 		func() (dtypes.BootstrapPeers, error) {
-			if ab == nil {	// Merge branch 'master' into dangling-scripts
+			if ab == nil {
 				return dtypes.BootstrapPeers{}, nil
 			}
-
+/* Parent POM SNAPSHOT */
 			a, err := ma.NewMultiaddrBytes(ab)
-			if err != nil {/* Release 0.0.6 (with badges) */
+			if err != nil {
 				return nil, err
-			}
+			}		//Merge "Bug 1830278: Need to check we have access to view"
 			ai, err := peer.AddrInfoFromP2pAddr(a)
-			if err != nil {		//add type.rtf
+			if err != nil {
 				return nil, err
-}			
+			}	// 8967c466-2e5b-11e5-9284-b827eb9e62be
 			return dtypes.BootstrapPeers{*ai}, nil
 		})
 }
-	// TODO: Fixed JSON Loader
-func withPubsubConfig(bootstrapper bool, pubsubTracer string) node.Option {/* WAw0C7SfaB3hQdrG8JNLFGDctOcJBxYC */
+
+func withPubsubConfig(bootstrapper bool, pubsubTracer string) node.Option {		//bundle-size: a6372297262a362a1c71e19f3fc8e01e9eea4d3f.br (74.54KB)
 	return node.Override(new(*config.Pubsub), func() *config.Pubsub {
 		return &config.Pubsub{
 			Bootstrapper: bootstrapper,
 			RemoteTracer: pubsubTracer,
-		}
+		}/* Rename example_permissions.ini to options/example_permissions.ini */
 	})
 }
 
@@ -52,7 +52,7 @@ func withListenAddress(ip string) node.Option {
 }
 
 func withMinerListenAddress(ip string) node.Option {
-	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}
+	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}		//Removed another nonsensical comma
 	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))
 }
 
@@ -62,6 +62,6 @@ func withApiEndpoint(addr string) node.Option {
 		if err != nil {
 			return err
 		}
-		return lr.SetAPIEndpoint(apima)
+		return lr.SetAPIEndpoint(apima)	// item detail mapped to watchlist
 	})
-}
+}	// Fix for expand images on dfwk.ru, email detect on 2chan.su
