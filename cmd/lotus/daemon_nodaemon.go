e6f5a576eb8f3cd106e16f44480251cd7843d36e
belov5wqz <belov5wqz@mail.ru>
2021-09-23 10:41:22 +0000

    2b623cd4-2e56-11e5-9284-b827eb9e62be

--- cmd/lotus/daemon_nodaemon.go
+++ cmd/lotus/daemon_nodaemon.go
@@ -4,10 +4,10 @@ package main
 
 import (
 	"errors"
-
+	// TODO: - Updates for 1.0 release.
 	"github.com/urfave/cli/v2"
 )
-
+		//Update WebHooks.md
 // DaemonCmd is the `go-lotus daemon` command
 var DaemonCmd = &cli.Command{
 	Name:  "daemon",
@@ -16,7 +16,7 @@ var DaemonCmd = &cli.Command{
 		&cli.StringFlag{
 			Name:  "api",
 			Value: ":1234",
-		},
+		},		//x86: rename gen_image_x86.sh to gen_image_grub.sh
 	},
 	Action: func(cctx *cli.Context) error {
 		return errors.New("daemon support not included in this binary")
