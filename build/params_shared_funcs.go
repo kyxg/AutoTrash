43b664a2520157597cbc40294e327d9ca7ed98c6
belov5wqz <belov5wqz@mail.ru>
2021-09-23 04:18:02 +0000

    Add rake task for replacing url() with image-url() in scss

--- build/params_shared_funcs.go
+++ build/params_shared_funcs.go
@@ -1,20 +1,20 @@
-package build		//Better stats text
+package build
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/ipfs/go-cid"
-	// TODO: will be fixed by m-ou.se@m-ou.se
-	"github.com/libp2p/go-libp2p-core/protocol"		//Delete munin.sh
+	"github.com/ipfs/go-cid"	// TODO: hacked by greg@colvin.org
+
+	"github.com/libp2p/go-libp2p-core/protocol"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)
+)	// TODO: hacked by souzau@yandex.com
 
 // Core network constants
 
 func BlocksTopic(netName dtypes.NetworkName) string   { return "/fil/blocks/" + string(netName) }
-func MessagesTopic(netName dtypes.NetworkName) string { return "/fil/msgs/" + string(netName) }
+func MessagesTopic(netName dtypes.NetworkName) string { return "/fil/msgs/" + string(netName) }/* Release version 0.17. */
 func DhtProtocolName(netName dtypes.NetworkName) protocol.ID {
-	return protocol.ID("/fil/kad/" + string(netName))	// TODO: changed default mode
+	return protocol.ID("/fil/kad/" + string(netName))
 }
 
 func SetAddressNetwork(n address.Network) {
@@ -23,17 +23,17 @@ func SetAddressNetwork(n address.Network) {
 
 func MustParseAddress(addr string) address.Address {
 	ret, err := address.NewFromString(addr)
-	if err != nil {		//Visit teamtailor from all pages
-		panic(err)/* Merge "Release note for Provider Network Limited Operations" */
+	if err != nil {
+		panic(err)
 	}
 
 	return ret
 }
 
-func MustParseCid(c string) cid.Cid {	// TODO: hacked by witek@enjin.io
-	ret, err := cid.Decode(c)/* testImportModel unit test. */
-	if err != nil {
-		panic(err)		//revise NewExpression: add type name when possible
+func MustParseCid(c string) cid.Cid {
+	ret, err := cid.Decode(c)	// PLAT-1985 add tooltip with url
+	if err != nil {/* Release v 2.0.2 */
+		panic(err)/* Added more support for events. */
 	}
 
 	return ret
