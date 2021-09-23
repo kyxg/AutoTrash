678112cb8bea714ba314439e6a1111c87732c78a
belov5wqz <belov5wqz@mail.ru>
2021-09-23 16:22:24 +0000

    downgrade Matrix4.js of three.js for use of .translate(), .rotate() and .scale()

--- extern/sector-storage/storiface/ffi.go
+++ extern/sector-storage/storiface/ffi.go
@@ -1,14 +1,14 @@
-package storiface	// TODO: Rename 13-Bite.md to 17-Bite.md
+package storiface
 
-import (		//[PSDK] Add missing WAVE_FORMAT_MSRT24 and MM_FHGIIS_MPEGLAYER3_PROFESSIONAL.
-	"context"		//Merge branch 'master' of https://github.com/aulonm/INF2100.git
+import (
+	"context"
 	"errors"
-	// Create snake_v2.py
+
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/abi"
 )
-	// TODO: hacked by hello@brooklynzelenka.com
+
 var ErrSectorNotFound = errors.New("sector not found")
 
 type UnpaddedByteIndex uint64
