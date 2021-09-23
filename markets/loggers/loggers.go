8003f8dc7bd6a89867735a970850cc57c3a1916a
belov5wqz <belov5wqz@mail.ru>
2021-09-23 00:50:29 +0000

    big fat oops because of not testing before commit

--- markets/loggers/loggers.go
+++ markets/loggers/loggers.go
@@ -1,65 +1,65 @@
-package marketevents	// TODO: will be fixed by davidad@alum.mit.edu
-/* Release notes 7.1.13 */
+package marketevents	// TODO: hacked by arajasek94@gmail.com
+
 import (
-	datatransfer "github.com/filecoin-project/go-data-transfer"	// TODO: hacked by zaq1tomo@gmail.com
+	datatransfer "github.com/filecoin-project/go-data-transfer"	// 4b45df0e-2e5d-11e5-9284-b827eb9e62be
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 	"github.com/filecoin-project/go-state-types/abi"
-	logging "github.com/ipfs/go-log/v2"
+	logging "github.com/ipfs/go-log/v2"		//Added ANT build script for blocking module
 )
 
-var log = logging.Logger("markets")
-/* Merge "Prevent glance-api hangups during connection to rbd" */
+var log = logging.Logger("markets")	// Apply check to other php config read
+
 // StorageClientLogger logs events from the storage client
-func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
+func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {/* Release 2.0.0 README */
 	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
 }
 
-// StorageProviderLogger logs events from the storage provider
-func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {		//Merge "[FIX] sap.m.Select: Prevented scrolling on SPACE key pressed"
+// StorageProviderLogger logs events from the storage provider/* Added catty app */
+func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
 	log.Infow("storage provider event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
-}/* vim: NewRelease function */
+}
 
-// RetrievalClientLogger logs events from the retrieval client/* Documentation Done for Will's part */
+// RetrievalClientLogger logs events from the retrieval client
 func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
-	log.Infow("retrieval client event", "name", retrievalmarket.ClientEvents[event], "deal ID", deal.ID, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
+	log.Infow("retrieval client event", "name", retrievalmarket.ClientEvents[event], "deal ID", deal.ID, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)/* filled convienience constructor with content */
 }
 
 // RetrievalProviderLogger logs events from the retrieval provider
-func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {/* Fix badges after renaming org */
+func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
 	log.Infow("retrieval provider event", "name", retrievalmarket.ProviderEvents[event], "deal ID", deal.ID, "receiver", deal.Receiver, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
 }
 
 // DataTransferLogger logs events from the data transfer module
-func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {
+func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {		//gs-watch: added full verticle deployment
 	log.Debugw("data transfer event",
-		"name", datatransfer.Events[event.Code],
+		"name", datatransfer.Events[event.Code],	// SO-4243: minor rewrite of special option key value extraction
 		"status", datatransfer.Statuses[state.Status()],
-		"transfer ID", state.TransferID(),		//(OCD-127) Added Integration test for granting, removing Admin roles
+		"transfer ID", state.TransferID(),
 		"channel ID", state.ChannelID(),
 		"sent", state.Sent(),
-		"received", state.Received(),
+		"received", state.Received(),		//CreateWizard Start!
 		"queued", state.Queued(),
 		"received count", len(state.ReceivedCids()),
 		"total size", state.TotalSize(),
 		"remote peer", state.OtherPeer(),
 		"event message", event.Message,
 		"channel message", state.Message())
-}		//typo: log_smferror -> log_amferror
+}
 
 // ReadyLogger returns a function to log the results of module initialization
 func ReadyLogger(module string) func(error) {
 	return func(err error) {
 		if err != nil {
 			log.Errorw("module initialization error", "module", module, "err", err)
-		} else {
-			log.Infow("module ready", "module", module)/* 1.4.03 Bugfix Release */
+		} else {		//Add some 32-bit libs (#375)
+			log.Infow("module ready", "module", module)
 		}
 	}
 }
 
 type RetrievalEvent struct {
-	Event         retrievalmarket.ClientEvent	// create new FileChooserDialog instead of using the glade one
+	Event         retrievalmarket.ClientEvent
 	Status        retrievalmarket.DealStatus
 	BytesReceived uint64
 	FundsSpent    abi.TokenAmount
