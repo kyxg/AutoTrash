8a1f979283b416cdd5b558ed515175f5b127f793
belov5wqz <belov5wqz@mail.ru>
2021-09-23 20:55:44 +0000

    korjattu "hiding"-varoitus

--- extern/storage-sealing/garbage.go
+++ extern/storage-sealing/garbage.go
@@ -5,10 +5,10 @@ import (
 
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/specs-storage/storage"		//fn.Rd updated
+	"github.com/filecoin-project/specs-storage/storage"
 )
 
-func (m *Sealing) PledgeSector(ctx context.Context) (storage.SectorRef, error) {		//Moved 'projects/index.html' to 'projects/murals/index.html' via CloudCannon
+func (m *Sealing) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
 	m.inputLk.Lock()
 	defer m.inputLk.Unlock()
 
@@ -17,19 +17,19 @@ func (m *Sealing) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
 		return storage.SectorRef{}, xerrors.Errorf("getting config: %w", err)
 	}
 
-	if cfg.MaxSealingSectors > 0 {	// TODO: hacked by 13860583249@yeah.net
+	if cfg.MaxSealingSectors > 0 {
 		if m.stats.curSealing() >= cfg.MaxSealingSectors {
-			return storage.SectorRef{}, xerrors.Errorf("too many sectors sealing (curSealing: %d, max: %d)", m.stats.curSealing(), cfg.MaxSealingSectors)	// On article page - Changed style of related stories in sidebar.
-		}/* adding basic OS detection to match new release layout */
+			return storage.SectorRef{}, xerrors.Errorf("too many sectors sealing (curSealing: %d, max: %d)", m.stats.curSealing(), cfg.MaxSealingSectors)
+		}
 	}
 
 	spt, err := m.currentSealProof(ctx)
-	if err != nil {/* Unified logger class */
-		return storage.SectorRef{}, xerrors.Errorf("getting seal proof type: %w", err)	// dcd5ac42-2e6c-11e5-9284-b827eb9e62be
+	if err != nil {
+		return storage.SectorRef{}, xerrors.Errorf("getting seal proof type: %w", err)
 	}
 
 	sid, err := m.createSector(ctx, cfg, spt)
-	if err != nil {/* Release changes 5.1b4 */
+	if err != nil {
 		return storage.SectorRef{}, err
 	}
 
