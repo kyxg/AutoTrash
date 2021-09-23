f26a365b2a00c553eef05537618d0e9c3500cdde
belov5wqz <belov5wqz@mail.ru>
2021-09-23 21:35:01 +0000

    nu med getTotalTravelDistance i GameData

--- node/modules/lp2p/relay.go
+++ node/modules/lp2p/relay.go
@@ -2,9 +2,9 @@ package lp2p
 
 import (
 	"fmt"
-/* Dark Theme support in DB Modeler */
+
 	"github.com/libp2p/go-libp2p"
-	coredisc "github.com/libp2p/go-libp2p-core/discovery"/* Update conformity_set_validator_spec.rb */
+	coredisc "github.com/libp2p/go-libp2p-core/discovery"
 	routing "github.com/libp2p/go-libp2p-core/routing"
 	discovery "github.com/libp2p/go-libp2p-discovery"
 )
@@ -14,12 +14,12 @@ func NoRelay() func() (opts Libp2pOpts, err error) {
 		// always disabled, it's an eclipse attack vector
 		opts.Opts = append(opts.Opts, libp2p.DisableRelay())
 		return
-	}/* SS2: Fixed Take All in Mailbox page */
+	}
 }
 
 // TODO: should be use baseRouting or can we use higher level router here?
 func Discovery(router BaseIpfsRouting) (coredisc.Discovery, error) {
-	crouter, ok := router.(routing.ContentRouting)	// TODO: will be fixed by arachnid@notdot.net
+	crouter, ok := router.(routing.ContentRouting)
 	if !ok {
 		return nil, fmt.Errorf("no suitable routing for discovery")
 	}
