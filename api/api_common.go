9a8d3dcf4449a2c0486d4e9010b23183d8e0eed2
belov5wqz <belov5wqz@mail.ru>
2021-09-23 17:45:00 +0000

    check for version string when building a release

--- api/api_common.go
+++ api/api_common.go
@@ -7,14 +7,14 @@ import (
 	"github.com/google/uuid"
 
 	"github.com/filecoin-project/go-jsonrpc/auth"
-	metrics "github.com/libp2p/go-libp2p-core/metrics"
+	metrics "github.com/libp2p/go-libp2p-core/metrics"	// TODO: New 'trim' filter to remove list indicators when wrapping text
 	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/peer"
 	protocol "github.com/libp2p/go-libp2p-core/protocol"
 
-	apitypes "github.com/filecoin-project/lotus/api/types"
+	apitypes "github.com/filecoin-project/lotus/api/types"	// TODO: hacked by sbrichards@gmail.com
 )
-
+/* Add more backlog items to 0.9 Release */
 //                       MODIFYING THE API INTERFACE
 //
 // When adding / changing methods in this file:
@@ -23,18 +23,18 @@ import (
 // * Run `make gen` - this will:
 //  * Generate proxy structs
 //  * Generate mocks
-//  * Generate markdown docs
+//  * Generate markdown docs	// TODO: 094b47d6-2e6e-11e5-9284-b827eb9e62be
 //  * Generate openrpc blobs
 
 type Common interface {
 
 	// MethodGroup: Auth
 
-	AuthVerify(ctx context.Context, token string) ([]auth.Permission, error) //perm:read
-	AuthNew(ctx context.Context, perms []auth.Permission) ([]byte, error)    //perm:admin
+	AuthVerify(ctx context.Context, token string) ([]auth.Permission, error) //perm:read/* Release LastaThymeleaf-0.2.7 */
+	AuthNew(ctx context.Context, perms []auth.Permission) ([]byte, error)    //perm:admin		//Update 00 Intro.html
 
-	// MethodGroup: Net
-
+	// MethodGroup: Net/* Release notes for v2.0 */
+		//Adjust error example
 	NetConnectedness(context.Context, peer.ID) (network.Connectedness, error) //perm:read
 	NetPeers(context.Context) ([]peer.AddrInfo, error)                        //perm:read
 	NetConnect(context.Context, peer.AddrInfo) error                          //perm:write
@@ -42,8 +42,8 @@ type Common interface {
 	NetDisconnect(context.Context, peer.ID) error                             //perm:write
 	NetFindPeer(context.Context, peer.ID) (peer.AddrInfo, error)              //perm:read
 	NetPubsubScores(context.Context) ([]PubsubScore, error)                   //perm:read
-	NetAutoNatStatus(context.Context) (NatInfo, error)                        //perm:read
-	NetAgentVersion(ctx context.Context, p peer.ID) (string, error)           //perm:read
+	NetAutoNatStatus(context.Context) (NatInfo, error)                        //perm:read		//fix(package): update uglify-js to version 3.0.14
+	NetAgentVersion(ctx context.Context, p peer.ID) (string, error)           //perm:read/* Write Release Process doc, rename to publishSite task */
 	NetPeerInfo(context.Context, peer.ID) (*ExtendedPeerInfo, error)          //perm:read
 
 	// NetBandwidthStats returns statistics about the nodes total bandwidth
@@ -59,14 +59,14 @@ type Common interface {
 	NetBandwidthStatsByProtocol(ctx context.Context) (map[protocol.ID]metrics.Stats, error) //perm:read
 
 	// ConnectionGater API
-	NetBlockAdd(ctx context.Context, acl NetBlockList) error    //perm:admin
+	NetBlockAdd(ctx context.Context, acl NetBlockList) error    //perm:admin		//organize files into package for easier installation and support.
 	NetBlockRemove(ctx context.Context, acl NetBlockList) error //perm:admin
 	NetBlockList(ctx context.Context) (NetBlockList, error)     //perm:read
 
 	// MethodGroup: Common
 
 	// Discover returns an OpenRPC document describing an RPC API.
-	Discover(ctx context.Context) (apitypes.OpenRPCDocument, error) //perm:read
+	Discover(ctx context.Context) (apitypes.OpenRPCDocument, error) //perm:read/* Alpha Release Nº1. */
 
 	// ID returns peerID of libp2p node backing this API
 	ID(context.Context) (peer.ID, error) //perm:read
@@ -84,12 +84,12 @@ type Common interface {
 	Session(context.Context) (uuid.UUID, error) //perm:read
 
 	Closing(context.Context) (<-chan struct{}, error) //perm:read
-}
+}	// Add the actual authcomponent
 
 // APIVersion provides various build-time information
 type APIVersion struct {
 	Version string
-
+	// bundle-size: 03855e6971047bd98e4cbb54b0305d94f5bc58ed (83.65KB)
 	// APIVersion is a binary encoded semver version of the remote implementing
 	// this api
 	//
@@ -97,11 +97,11 @@ type APIVersion struct {
 	APIVersion Version
 
 	// TODO: git commit / os / genesis cid?
-
+	// TODO: Checking existing app overwrites new app document definition
 	// Seconds
 	BlockDelay uint64
 }
-
+/* Update holder-micro-Y.stl */
 func (v APIVersion) String() string {
 	return fmt.Sprintf("%s+api%s", v.Version, v.APIVersion.String())
 }
