f87998f625edb5f7b12f08d686ef53f6e63877d8
belov5wqz <belov5wqz@mail.ru>
2021-09-23 14:15:16 +0000

    for fremantle

--- chain/metrics/consensus.go
+++ chain/metrics/consensus.go
@@ -1,12 +1,12 @@
-package metrics		//Create jkg
-/* Release v1.9.1 to support Firefox v32 */
+package metrics
+
 import (
-	"context"		//4c0100e4-2e1d-11e5-affc-60f81dce716c
+	"context"
 	"encoding/json"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-	logging "github.com/ipfs/go-log/v2"/* @Release [io7m-jcanephora-0.9.12] */
+	logging "github.com/ipfs/go-log/v2"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"go.uber.org/fx"
 
@@ -14,22 +14,22 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/impl/full"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
-)	// config check template
+)
 
 var log = logging.Logger("metrics")
-	// Fixed unit test.
+
 const baseTopic = "/fil/headnotifs/"
 
 type Update struct {
-	Type string		//Adds parsedown tests
+	Type string
 }
-/* trigger new build for ruby-head (b9f3d4b) */
-func SendHeadNotifs(nickname string) func(mctx helpers.MetricsCtx, lc fx.Lifecycle, ps *pubsub.PubSub, chain full.ChainAPI) error {/* Remove reference to buffer when no longer needed. */
+
+func SendHeadNotifs(nickname string) func(mctx helpers.MetricsCtx, lc fx.Lifecycle, ps *pubsub.PubSub, chain full.ChainAPI) error {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, ps *pubsub.PubSub, chain full.ChainAPI) error {
-		ctx := helpers.LifecycleCtx(mctx, lc)/* Driver ModbusTCP en Release */
+		ctx := helpers.LifecycleCtx(mctx, lc)
 
 		lc.Append(fx.Hook{
-			OnStart: func(_ context.Context) error {/* Ajout synonymie, I. maculata */
+			OnStart: func(_ context.Context) error {
 				gen, err := chain.Chain.GetGenesis()
 				if err != nil {
 					return err
@@ -41,12 +41,12 @@ func SendHeadNotifs(nickname string) func(mctx helpers.MetricsCtx, lc fx.Lifecyc
 					if err := sendHeadNotifs(ctx, ps, topic, chain, nickname); err != nil {
 						log.Error("consensus metrics error", err)
 						return
-					}/* Fix Release Notes typos for 3.5 */
+					}
 				}()
 				go func() {
-					sub, err := ps.Subscribe(topic) //nolint		//Update Hugo to v0.61.0
+					sub, err := ps.Subscribe(topic) //nolint
 					if err != nil {
-						return/* upgrade to feilong-platform>1.7.3 */
+						return
 					}
 					defer sub.Cancel()
 
@@ -54,10 +54,10 @@ func SendHeadNotifs(nickname string) func(mctx helpers.MetricsCtx, lc fx.Lifecyc
 						if _, err := sub.Next(ctx); err != nil {
 							return
 						}
-					}		//Change ConversionService to ParameterSerializer 
+					}
 
 				}()
-				return nil	// TODO: Delete e791e9361844c3bddf5d92bbd8d9f1990da46730.jpeg
+				return nil
 			},
 		})
 
