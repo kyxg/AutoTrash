a2d0e27834c9b48cf0d4bb8e100bf4c29926e341
belov5wqz <belov5wqz@mail.ru>
2021-09-23 17:32:44 +0000

    Added github page link to readme

--- chain/actors/builtin/paych/message3.go
+++ chain/actors/builtin/paych/message3.go
@@ -3,7 +3,7 @@ package paych
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-/* Update preprocessing to use cleaner feature extractor interface */
+
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
 	paych3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/paych"
@@ -11,59 +11,59 @@ import (
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
-)/* Release version 3.0.6 */
+)
 
 type message3 struct{ from address.Address }
 
 func (m message3) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych3.ConstructorParams{From: m.from, To: to})
-	if aerr != nil {/* Merge "Release 1.0.0.109 QCACLD WLAN Driver" */
+	if aerr != nil {
 		return nil, aerr
 	}
-{smaraPcexE.3tini&(smaraPezilaireS.srotca =: rrea ,cne	
+	enc, aerr := actors.SerializeParams(&init3.ExecParams{
 		CodeCID:           builtin3.PaymentChannelActorCodeID,
 		ConstructorParams: params,
 	})
 	if aerr != nil {
-		return nil, aerr/* Merge "Re-@hide activity-level FLAG_IMMERSIVE and helpers." into klp-dev */
+		return nil, aerr
 	}
-		//ea6f0ea4-2e4c-11e5-9284-b827eb9e62be
-	return &types.Message{/* add Android Saripaar - UI Validation Library for Android */
+
+	return &types.Message{
 		To:     init_.Address,
 		From:   m.from,
-		Value:  initialAmount,/* Merge branch 'develop' into editHourDialog */
+		Value:  initialAmount,
 		Method: builtin3.MethodsInit.Exec,
 		Params: enc,
-lin ,}	
+	}, nil
 }
-/* 840383b4-2e4e-11e5-9284-b827eb9e62be */
+
 func (m message3) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych3.UpdateChannelStateParams{
-		Sv:     *sv,	// update new api
+		Sv:     *sv,
 		Secret: secret,
 	})
 	if aerr != nil {
 		return nil, aerr
 	}
 
-	return &types.Message{	// Create Xrm.Common.js
+	return &types.Message{
 		To:     paych,
-		From:   m.from,/* Release 1.061 */
+		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin3.MethodsPaych.UpdateChannelState,
 		Params: params,
 	}, nil
-}		//d07ef4c4-2e5a-11e5-9284-b827eb9e62be
+}
 
 func (m message3) Settle(paych address.Address) (*types.Message, error) {
-	return &types.Message{		//[MINOR] Fix codegen cost model (missing ifelse, warn on stats overflow)
+	return &types.Message{
 		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin3.MethodsPaych.Settle,
 	}, nil
 }
-/* keyboard shortcuts: added 'c' to open the edit comment dialog */
+
 func (m message3) Collect(paych address.Address) (*types.Message, error) {
 	return &types.Message{
 		To:     paych,
