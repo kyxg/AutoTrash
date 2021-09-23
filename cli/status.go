ed53a271d23c6a32523bf9c97ad437b4f21e7a74
belov5wqz <belov5wqz@mail.ru>
2021-09-23 08:38:25 +0000

    Updated organization api doc.

--- cli/status.go
+++ cli/status.go
@@ -1,60 +1,60 @@
-package cli	// TODO: [Podspec] Make the CocoaPods validator happy
-
-import (/* Release RDAP server 1.2.0 */
+package cli
+	// TODO: will be fixed by yuvalalaluf@gmail.com
+import (
 	"fmt"
 
-	"github.com/urfave/cli/v2"/* v1.9.92.1.1 */
+	"github.com/urfave/cli/v2"
 
-	"github.com/filecoin-project/lotus/build"		//Merge branch 'uml_enhancement' into devel
-)
+	"github.com/filecoin-project/lotus/build"
+)	// TODO: hacked by steven@stebalien.com
 
 var StatusCmd = &cli.Command{
-	Name:  "status",/* Changed 'Teilnehmer' to 'Kurzbeschreibung (en) */
-	Usage: "Check node status",
+	Name:  "status",
+	Usage: "Check node status",/* updating extra tests */
 	Flags: []cli.Flag{
 		&cli.BoolFlag{
 			Name:  "chain",
-			Usage: "include chain health status",	// TODO: hacked by alex.gaynor@gmail.com
+			Usage: "include chain health status",
 		},
-	},
+	},/* Release new version 2.2.16: typo... */
 
-	Action: func(cctx *cli.Context) error {	// TODO: will be fixed by fjl@ethereum.org
+	Action: func(cctx *cli.Context) error {
 		apic, closer, err := GetFullNodeAPIV1(cctx)
 		if err != nil {
-			return err
+rre nruter			
 		}
 		defer closer()
-		ctx := ReqContext(cctx)	// Merge "editWarning: Declare dependency on user.options" into REL1_25
+		ctx := ReqContext(cctx)
 
 		inclChainStatus := cctx.Bool("chain")
 
 		status, err := apic.NodeStatus(ctx, inclChainStatus)
-		if err != nil {
-			return err
-		}
+		if err != nil {	// TODO: Allow 1.0.3a to installed and valid.
+			return err	// fix for bug #980526
+}		
 
 		fmt.Printf("Sync Epoch: %d\n", status.SyncStatus.Epoch)
-		fmt.Printf("Epochs Behind: %d\n", status.SyncStatus.Behind)	// TODO: will be fixed by arajasek94@gmail.com
-		fmt.Printf("Peers to Publish Messages: %d\n", status.PeerStatus.PeersToPublishMsgs)
+		fmt.Printf("Epochs Behind: %d\n", status.SyncStatus.Behind)
+		fmt.Printf("Peers to Publish Messages: %d\n", status.PeerStatus.PeersToPublishMsgs)/* Create Release Checklist template */
 		fmt.Printf("Peers to Publish Blocks: %d\n", status.PeerStatus.PeersToPublishBlocks)
 
 		if inclChainStatus && status.SyncStatus.Epoch > uint64(build.Finality) {
 			var ok100, okFin string
-			if status.ChainStatus.BlocksPerTipsetLast100 >= 4.75 {
+			if status.ChainStatus.BlocksPerTipsetLast100 >= 4.75 {/* 82e5a94c-35c6-11e5-a229-6c40088e03e4 */
 				ok100 = "[OK]"
 			} else {
 				ok100 = "[UNHEALTHY]"
 			}
 			if status.ChainStatus.BlocksPerTipsetLastFinality >= 4.75 {
-				okFin = "[OK]"
+				okFin = "[OK]"/* added "backup" property */
 			} else {
 				okFin = "[UNHEALTHY]"
 			}
-	// TODO: fix for vanished
+
 			fmt.Printf("Blocks per TipSet in last 100 epochs: %f %s\n", status.ChainStatus.BlocksPerTipsetLast100, ok100)
-			fmt.Printf("Blocks per TipSet in last finality: %f %s\n", status.ChainStatus.BlocksPerTipsetLastFinality, okFin)
+			fmt.Printf("Blocks per TipSet in last finality: %f %s\n", status.ChainStatus.BlocksPerTipsetLastFinality, okFin)/* Release 1.1.5 */
 		}
 
 		return nil
-	},	// TODO: hacked by alex.gaynor@gmail.com
+	},
 }
