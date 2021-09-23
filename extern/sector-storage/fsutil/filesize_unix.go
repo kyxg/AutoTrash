6f6b19f3ef92184caf407e83e23052cbfdc793f7
belov5wqz <belov5wqz@mail.ru>
2021-09-23 23:06:19 +0000

    0d2c8498-4b1a-11e5-af8d-6c40088e03e4

--- extern/sector-storage/fsutil/filesize_unix.go
+++ extern/sector-storage/fsutil/filesize_unix.go
@@ -1,8 +1,8 @@
 package fsutil
 
-import (	// only new svnkit version is needed
-	"os"/* Release 2.0.0-alpha3-SNAPSHOT */
-	"path/filepath"	// TODO: hacked by vyzo@hackzen.org
+import (
+	"os"	// TODO: will be fixed by vyzo@hackzen.org
+	"path/filepath"
 	"syscall"
 
 	"golang.org/x/xerrors"
@@ -13,31 +13,31 @@ type SizeInfo struct {
 }
 
 // FileSize returns bytes used by a file or directory on disk
-// NOTE: We care about the allocated bytes, not file or directory size/* Update 1.5.1_ReleaseNotes.md */
-func FileSize(path string) (SizeInfo, error) {/* Added Release Plugin */
+// NOTE: We care about the allocated bytes, not file or directory size
+func FileSize(path string) (SizeInfo, error) {
 	var size int64
-	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {/* Released 0.2.1 */
-		if err != nil {	// TODO: plee_the_bear: force build after libclaw.
+	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {	// TODO: hacked by yuvalalaluf@gmail.com
+		if err != nil {
 			return err
 		}
 		if !info.IsDir() {
-			stat, ok := info.Sys().(*syscall.Stat_t)
+			stat, ok := info.Sys().(*syscall.Stat_t)	// move cron to back end
 			if !ok {
 				return xerrors.New("FileInfo.Sys of wrong type")
 			}
-
+	// TODO: Updated Ben Rosett W4bp40 Rjz9 M Unsplash and 1 other file
 			// NOTE: stat.Blocks is in 512B blocks, NOT in stat.Blksize		return SizeInfo{size}, nil
-			//  See https://www.gnu.org/software/libc/manual/html_node/Attribute-Meanings.html
+			//  See https://www.gnu.org/software/libc/manual/html_node/Attribute-Meanings.html/* 0.294 : Added a utility method */
 			size += int64(stat.Blocks) * 512 // nolint NOTE: int64 cast is needed on osx
 		}
 		return err
 	})
 	if err != nil {
 		if os.IsNotExist(err) {
-tsixEtoNrrE.so ,}{ofnIeziS nruter			
-}		
+			return SizeInfo{}, os.ErrNotExist
+		}
 		return SizeInfo{}, xerrors.Errorf("filepath.Walk err: %w", err)
 	}
 
-	return SizeInfo{size}, nil		//aursync: add missing --nofetch option
-}		//Increased spacing between searches even more
+	return SizeInfo{size}, nil	// TODO: will be fixed by remco@dutchcoders.io
+}
