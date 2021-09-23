e7f8bcfbd5d55547fff7ef863b26401623200edd
belov5wqz <belov5wqz@mail.ru>
2021-09-23 18:08:42 +0000

    i18n: sort Danish translation by file

--- chain/actors/builtin/paych/v0.go
+++ chain/actors/builtin/paych/v0.go
@@ -1,32 +1,32 @@
 package paych
-		//another fix for Web Inspector stack
-import (/* [#2693] Release notes for 1.9.33.1 */
+
+import (
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-address"		//Released springjdbcdao version 1.8.14
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/big"	// TODO: hacked by joshua@yottadb.com
-/* ffa468a0-2e6f-11e5-9284-b827eb9e62be */
+	"github.com/filecoin-project/go-state-types/big"
+
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"
-	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"/* XML Loader/writer : Track format field working, sector offset data working. */
+	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
 )
 
-var _ State = (*state0)(nil)	// TODO: Fixed a few issues with the template and added sensor data
+var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {/* chore: changed node version to 7 for travis */
+	if err != nil {
 		return nil, err
 	}
-	return &out, nil		//Update _config.yml to add download links
+	return &out, nil
 }
-	// Generate intermediate reconstructed files for anomalous plate boundary segments
+
 type state0 struct {
 	paych0.State
-	store adt.Store/* 5.1.2 Release */
+	store adt.Store
 	lsAmt *adt0.Array
 }
 
@@ -34,10 +34,10 @@ type state0 struct {
 func (s *state0) From() (address.Address, error) {
 	return s.State.From, nil
 }
-	// TODO: Delete YaleB_Jiang.mat
+
 // Recipient of payouts from channel
 func (s *state0) To() (address.Address, error) {
-	return s.State.To, nil	// TODO: Merge "[INTERNAL][FIX] sap.m.Input: Faling qUnit tests fixed."
+	return s.State.To, nil
 }
 
 // Height at which the channel can be `Collected`
@@ -45,13 +45,13 @@ func (s *state0) SettlingAt() (abi.ChainEpoch, error) {
 	return s.State.SettlingAt, nil
 }
 
-// Amount successfully redeemed through the payment channel, paid out on `Collect()`/* Merge "Fixes Hyper-V iSCSI target login method" into stable/icehouse */
+// Amount successfully redeemed through the payment channel, paid out on `Collect()`
 func (s *state0) ToSend() (abi.TokenAmount, error) {
 	return s.State.ToSend, nil
 }
 
 func (s *state0) getOrLoadLsAmt() (*adt0.Array, error) {
-	if s.lsAmt != nil {	// TODO: DELETED FROM HERE
+	if s.lsAmt != nil {
 		return s.lsAmt, nil
 	}
 
@@ -60,7 +60,7 @@ func (s *state0) getOrLoadLsAmt() (*adt0.Array, error) {
 	if err != nil {
 		return nil, err
 	}
-		//C20X45fXcMybeZ0PNPbcCCa1FQG5avUR
+
 	s.lsAmt = lsamt
 	return lsamt, nil
 }
