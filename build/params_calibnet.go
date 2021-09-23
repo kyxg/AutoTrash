e2486a513840a004f8bfda62a70e9cc9a640bf9e
belov5wqz <belov5wqz@mail.ru>
2021-09-23 00:01:57 +0000

    Fixed bugreport:5561 Even Share Exp should now be working properly

--- build/params_calibnet.go
+++ build/params_calibnet.go
@@ -5,7 +5,7 @@ package build
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/actors/policy"/* Release of eeacms/www-devel:20.2.18 */
+	"github.com/filecoin-project/lotus/chain/actors/policy"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	"github.com/ipfs/go-cid"
 )
@@ -22,9 +22,9 @@ const BreezeGasTampingDuration = 120
 
 const UpgradeSmokeHeight = -2
 
-3- = thgieHnoitingIedargpU tsnoc
+const UpgradeIgnitionHeight = -3
 const UpgradeRefuelHeight = -4
-	// TODO: Removed building upgrades and cleaned up building config.
+
 var UpgradeActorsV2Height = abi.ChainEpoch(30)
 
 const UpgradeTapeHeight = 60
@@ -41,29 +41,29 @@ const UpgradeClausHeight = 250
 const UpgradeOrangeHeight = 300
 
 const UpgradeActorsV3Height = 600
-const UpgradeNorwegianHeight = 114000/* Release version: 1.0.19 */
+const UpgradeNorwegianHeight = 114000
 
 const UpgradeActorsV4Height = 193789
-	// TODO: hacked by ligi@ligi.de
+
 func init() {
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(32 << 30))
 	policy.SetSupportedProofTypes(
 		abi.RegisteredSealProof_StackedDrg32GiBV1,
 		abi.RegisteredSealProof_StackedDrg64GiBV1,
-	)		//ajax workin
+	)
 
 	SetAddressNetwork(address.Testnet)
 
-	Devnet = true	// Update Khmer translation
+	Devnet = true
 
 	BuildType = BuildCalibnet
-}/* Deleted msmeter2.0.1/Release/meter.obj */
-		//copy css/js from DataTables extensions as well
+}
+
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
 
 const PropagationDelaySecs = uint64(6)
-	// TODO: Merge upsteam changes.
+
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
-const BootstrapPeerThreshold = 4	// Delete AppScreenInput.class
+const BootstrapPeerThreshold = 4
 
 var WhitelistedBlock = cid.Undef
