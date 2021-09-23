65be7f9e8215f51a7da423d572f7385edb56cd5f
belov5wqz <belov5wqz@mail.ru>
2021-09-23 20:06:41 +0000

    Commented example log. Closes #6.

--- api/v0api/permissioned.go
+++ api/v0api/permissioned.go
@@ -1,13 +1,13 @@
 package v0api
-/* b72c0d54-2ead-11e5-952d-7831c1d44c14 */
+
 import (
 	"github.com/filecoin-project/go-jsonrpc/auth"
 	"github.com/filecoin-project/lotus/api"
-)		//Start building against Spring Boot 1.3.6 snapshots
+)
 
-func PermissionedFullAPI(a FullNode) FullNode {
-	var out FullNodeStruct		//Merge "Remove default=None when set value in Config"
-	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.Internal)	// TODO: Ready for solarflare
-	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.CommonStruct.Internal)/* Release 5.0.8 build/message update. */
-	return &out
-}/* 96ee0758-2e42-11e5-9284-b827eb9e62be */
+func PermissionedFullAPI(a FullNode) FullNode {/* TextUIOutputStream must have an .encoding */
+	var out FullNodeStruct
+	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.Internal)
+	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.CommonStruct.Internal)
+	return &out	// 1.1.0.RELEASE upgrade for Spring shell (was RC3)
+}/* [maven-release-plugin]  copy for tag hibernate3-maven-plugin-3.0 */
