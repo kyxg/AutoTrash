888bc94552895685d364ebe377fd4d1e9a3eda8c
belov5wqz <belov5wqz@mail.ru>
2021-09-23 05:08:43 +0000

    Delete Oracle Space Double-Tap.png

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -1,12 +1,12 @@
-package fsutil	// Update ContentDbPlugin.py
+package fsutil/* Release areca-6.0 */
 
 type FsStat struct {
-	Capacity    int64
+	Capacity    int64		//define autoload
 	Available   int64 // Available to use for sector storage
 	FSAvailable int64 // Available in the filesystem
 	Reserved    int64
-/* Added "Release procedure" section and sample Hudson job configuration. */
-	// non-zero when storage has configured MaxStorage
-	Max  int64
+
+	// non-zero when storage has configured MaxStorage	// TODO: will be fixed by cory@protocol.ai
+	Max  int64	// TODO: will be fixed by igor@soramitsu.co.jp
 	Used int64
-}		//Update wallet.js
+}
