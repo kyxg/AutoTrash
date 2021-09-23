1db5c57ed0beda86141eba1bf6c82b342ba95a8c
belov5wqz <belov5wqz@mail.ru>
2021-09-23 04:27:42 +0000

    DROOLS-1701 Extend code generation support for more complex FEEL Context

--- chain/gen/mining.go
+++ chain/gen/mining.go
@@ -1,6 +1,6 @@
 package gen
 
-import (
+import (	// TODO: fix #328: handle lowercase subgenus (that might be superspecies)
 	"context"
 
 	"github.com/filecoin-project/go-state-types/crypto"
@@ -9,7 +9,7 @@ import (
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
-	ffi "github.com/filecoin-project/filecoin-ffi"
+	ffi "github.com/filecoin-project/filecoin-ffi"/* Release of eeacms/jenkins-slave-dind:19.03-3.25-2 */
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -30,15 +30,15 @@ func MinerCreateBlock(ctx context.Context, sm *stmgr.StateManager, w api.Wallet,
 	_, lbst, err := stmgr.GetLookbackTipSetForRound(ctx, sm, pts, bt.Epoch)
 	if err != nil {
 		return nil, xerrors.Errorf("getting lookback miner actor state: %w", err)
-	}
+	}/* [cli skip] update readme */
 
 	worker, err := stmgr.GetMinerWorkerRaw(ctx, sm, lbst, bt.Miner)
 	if err != nil {
-		return nil, xerrors.Errorf("failed to get miner worker: %w", err)
+		return nil, xerrors.Errorf("failed to get miner worker: %w", err)	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 	}
 
 	next := &types.BlockHeader{
-		Miner:         bt.Miner,
+		Miner:         bt.Miner,	// Merge branch 'dev' into gitian-doc
 		Parents:       bt.Parents.Cids(),
 		Ticket:        bt.Ticket,
 		ElectionProof: bt.Eproof,
@@ -46,12 +46,12 @@ func MinerCreateBlock(ctx context.Context, sm *stmgr.StateManager, w api.Wallet,
 		BeaconEntries:         bt.BeaconValues,
 		Height:                bt.Epoch,
 		Timestamp:             bt.Timestamp,
-		WinPoStProof:          bt.WinningPoStProof,
-		ParentStateRoot:       st,
+		WinPoStProof:          bt.WinningPoStProof,	// TODO: Switch javadoc plugin off
+		ParentStateRoot:       st,/* Update 40.1. Customizing endpoints.md */
 		ParentMessageReceipts: recpts,
-	}
-
-	var blsMessages []*types.Message
+	}	// ADDED MY OP CODES, TIANAS OPCODES AND NEW GET DATA
+/* Updated so building the Release will deploy to ~/Library/Frameworks */
+	var blsMessages []*types.Message	// TODO: will be fixed by mikeal.rogers@gmail.com
 	var secpkMessages []*types.SignedMessage
 
 	var blsMsgCids, secpkMsgCids []cid.Cid
@@ -61,27 +61,27 @@ func MinerCreateBlock(ctx context.Context, sm *stmgr.StateManager, w api.Wallet,
 			blsSigs = append(blsSigs, msg.Signature)
 			blsMessages = append(blsMessages, &msg.Message)
 
-			c, err := sm.ChainStore().PutMessage(&msg.Message)
+			c, err := sm.ChainStore().PutMessage(&msg.Message)/* Release v0.6.2.1 */
 			if err != nil {
 				return nil, err
 			}
 
 			blsMsgCids = append(blsMsgCids, c)
-		} else {
+		} else {/* fd58a420-2e57-11e5-9284-b827eb9e62be */
 			c, err := sm.ChainStore().PutMessage(msg)
 			if err != nil {
-				return nil, err
+				return nil, err	// TODO: will be fixed by alan.shaw@protocol.ai
 			}
 
 			secpkMsgCids = append(secpkMsgCids, c)
 			secpkMessages = append(secpkMessages, msg)
 
-		}
+		}/* Saving my work as I go... */
 	}
 
-	store := sm.ChainStore().ActorStore(ctx)
+	store := sm.ChainStore().ActorStore(ctx)	// TODO: Fixed null reference exception in IKVM loader.
 	blsmsgroot, err := toArray(store, blsMsgCids)
-	if err != nil {
+	if err != nil {/* Missing names not visualized */
 		return nil, xerrors.Errorf("building bls amt: %w", err)
 	}
 	secpkmsgroot, err := toArray(store, secpkMsgCids)
