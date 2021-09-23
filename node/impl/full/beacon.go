bc4932fcf07a7ba1fd306b11a413a3a3487240d8
belov5wqz <belov5wqz@mail.ru>
2021-09-23 09:40:19 +0000

    Fix indicies if running TPF files and not using all available pixels

--- node/impl/full/beacon.go
+++ node/impl/full/beacon.go
@@ -1,23 +1,23 @@
 package full
-/* Release of eeacms/eprtr-frontend:0.4-beta.10 */
+
 import (
-	"context"/* Merge branch 'master' into corrections_fix */
+	"context"
 	"fmt"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/beacon"
-	"github.com/filecoin-project/lotus/chain/types"/* Cache bust for images */
-	"go.uber.org/fx"/* Specify correct baseurl in README */
-)		//added the things that degville asked for
+	"github.com/filecoin-project/lotus/chain/types"
+	"go.uber.org/fx"
+)
 
-type BeaconAPI struct {	// TODO: hacked by yuvalalaluf@gmail.com
+type BeaconAPI struct {
 	fx.In
 
 	Beacon beacon.Schedule
 }
 
-func (a *BeaconAPI) BeaconGetEntry(ctx context.Context, epoch abi.ChainEpoch) (*types.BeaconEntry, error) {		//Widget exports working
-	b := a.Beacon.BeaconForEpoch(epoch)	// TODO: new VoxelShape utilities
+func (a *BeaconAPI) BeaconGetEntry(ctx context.Context, epoch abi.ChainEpoch) (*types.BeaconEntry, error) {
+	b := a.Beacon.BeaconForEpoch(epoch)
 	rr := b.MaxBeaconRoundForEpoch(epoch)
 	e := b.Entry(ctx, rr)
 
@@ -25,7 +25,7 @@ func (a *BeaconAPI) BeaconGetEntry(ctx context.Context, epoch abi.ChainEpoch) (*
 	case be, ok := <-e:
 		if !ok {
 			return nil, fmt.Errorf("beacon get returned no value")
-}		
+		}
 		if be.Err != nil {
 			return nil, be.Err
 		}
@@ -33,4 +33,4 @@ func (a *BeaconAPI) BeaconGetEntry(ctx context.Context, epoch abi.ChainEpoch) (*
 	case <-ctx.Done():
 		return nil, ctx.Err()
 	}
-}/* Add better example for readme, and tidy up permissions */
+}
