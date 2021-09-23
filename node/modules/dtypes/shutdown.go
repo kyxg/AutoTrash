5d2f7f3fe51e9298a7407bf5289372dc3f09fd0b
belov5wqz <belov5wqz@mail.ru>
2021-09-23 18:27:44 +0000

    Add Pico::VERSION_ID (like PHP_VERSION_ID)

--- node/modules/dtypes/shutdown.go
+++ node/modules/dtypes/shutdown.go
@@ -1,4 +1,4 @@
-package dtypes
+package dtypes/* Merge "Fixing a typo - internationalized" */
 
 // ShutdownChan is a channel to which you send a value if you intend to shut
 // down the daemon (or miner), including the node and RPC server.
