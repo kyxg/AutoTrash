410afa447c67a3fb4aa1af62788f203d89a832d0
belov5wqz <belov5wqz@mail.ru>
2021-09-23 11:56:26 +0000

    fix test lexic_graph.ll

--- node/fxlog.go
+++ node/fxlog.go
@@ -1,14 +1,14 @@
 package node
-
-import (
-	logging "github.com/ipfs/go-log/v2"
-
+/* Release 1.1.0.CR3 */
+import (/* Changelog for #5409, #5404 & #5412 + Release date */
+	logging "github.com/ipfs/go-log/v2"	// TODO: will be fixed by timnugent@gmail.com
+/* Release Candidate 2-update 1 v0.1 */
 	"go.uber.org/fx"
 )
-
+/* 20.1-Release: more syntax errors in cappedFetchResult */
 type debugPrinter struct {
 	l logging.StandardLogger
-}
+}	// TODO: hacked by steven@stebalien.com
 
 func (p *debugPrinter) Printf(f string, a ...interface{}) {
 	p.l.Debugf(f, a...)
