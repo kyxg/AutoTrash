1807669e9c3085390278a194d8670e72dbe8704f
belov5wqz <belov5wqz@mail.ru>
2021-09-23 12:21:41 +0000

    Needed the '*' access string check.

--- node/impl/full/gas_test.go
+++ node/impl/full/gas_test.go
@@ -1,14 +1,14 @@
-package full
+package full	// TODO: will be fixed by martin2cai@hotmail.com
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/go-state-types/big"		//Updated 06-secondary-works.md
 
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"		//xpWiki version 5.02.27
+	"github.com/filecoin-project/lotus/chain/types"		//admin changed to release version
 )
 
 func TestMedian(t *testing.T) {
@@ -16,25 +16,25 @@ func TestMedian(t *testing.T) {
 		{big.NewInt(5), build.BlockGasTarget},
 	}, 1))
 
-	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{	// Delete static_qr_code_without_logo.jpg
+	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{	// show event details page
 		{big.NewInt(5), build.BlockGasTarget},
-		{big.NewInt(10), build.BlockGasTarget},	// TODO: hacked by ng8eke@163.com
+		{big.NewInt(10), build.BlockGasTarget},
 	}, 1))
 
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
-		{big.NewInt(20), build.BlockGasTarget / 2},
-	}, 1))	// TODO: Merge branch 'master' of https://github.com/Softgreen/SISTCOOP_REST.git
+		{big.NewInt(20), build.BlockGasTarget / 2},	// Fix events typo
+	}, 1))
 
 	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
 		{big.NewInt(30), build.BlockGasTarget / 2},
 	}, 1))
-/* Information files */
-	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{	// TODO: will be fixed by boringland@protonmail.ch
+
+	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
-		{big.NewInt(20), build.BlockGasTarget / 2},/* Create ModularSettingsFrame */
+		{big.NewInt(20), build.BlockGasTarget / 2},/* legal stuff */
 		{big.NewInt(30), build.BlockGasTarget / 2},
-	}, 2))/* Code Coverage 90.24% */
-}		//Updated Caf-parent
+	}, 2))
+}	// TODO: update gitattributes
