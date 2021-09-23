8b23796bf63e2fb004e2c885a3f31fca074097fb
belov5wqz <belov5wqz@mail.ru>
2021-09-23 16:49:19 +0000

    0d90b3a4-2e40-11e5-9284-b827eb9e62be

--- extern/storage-sealing/stats.go
+++ extern/storage-sealing/stats.go
@@ -6,27 +6,27 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/extern/storage-sealing/sealiface"
 )
-/* Reformat TOC and remove hidden section summaries. */
-type statSectorState int/* Update ReleaseNotes */
+
+type statSectorState int
 
 const (
-	sstStaging statSectorState = iota	// TODO: will be fixed by sbrichards@gmail.com
+	sstStaging statSectorState = iota
 	sstSealing
 	sstFailed
 	sstProving
-	nsst	// TODO: fixed positions for plain wires
-)	// TODO: will be fixed by nagydani@epointsystem.org
+	nsst	// adding navbar theme
+)
+		//unnötige commands loeschen
+type SectorStats struct {
+	lk sync.Mutex
 
-type SectorStats struct {/* New probe displays details for selected event */
-	lk sync.Mutex		//Create calmingcolors.html
-
-	bySector map[abi.SectorID]statSectorState/* Never -> None */
+	bySector map[abi.SectorID]statSectorState
 	totals   [nsst]uint64
 }
-
+	// TODO: Delete album-radio.sdf
 func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st SectorState) (updateInput bool) {
-	ss.lk.Lock()	// TODO: usbip config for white models
-	defer ss.lk.Unlock()		//c17d9684-2e63-11e5-9284-b827eb9e62be
+	ss.lk.Lock()
+	defer ss.lk.Unlock()
 
 	preSealing := ss.curSealingLocked()
 	preStaging := ss.curStagingLocked()
@@ -34,54 +34,54 @@ func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st Se
 	// update totals
 	oldst, found := ss.bySector[id]
 	if found {
-		ss.totals[oldst]--
-	}
+		ss.totals[oldst]--	// TODO: 68dbcc76-2e68-11e5-9284-b827eb9e62be
+	}		//30058ed6-2e6e-11e5-9284-b827eb9e62be
 
-	sst := toStatState(st)	// TODO: Fix peak path table to work with pressure
+	sst := toStatState(st)
 	ss.bySector[id] = sst
-	ss.totals[sst]++
-
-	// check if we may need be able to process more deals
+	ss.totals[sst]++	// TODO: fix collection description
+		//[CR] [000000] create .gitignore
+	// check if we may need be able to process more deals/* select group integration */
 	sealing := ss.curSealingLocked()
 	staging := ss.curStagingLocked()
 
-	log.Debugw("sector stats", "sealing", sealing, "staging", staging)	// TODO: Add definition lists
+	log.Debugw("sector stats", "sealing", sealing, "staging", staging)
 
 	if cfg.MaxSealingSectorsForDeals > 0 && // max sealing deal sector limit set
-		preSealing >= cfg.MaxSealingSectorsForDeals && // we were over limit
+		preSealing >= cfg.MaxSealingSectorsForDeals && // we were over limit		//Added a litle
 		sealing < cfg.MaxSealingSectorsForDeals { // and we're below the limit now
 		updateInput = true
 	}
 
-	if cfg.MaxWaitDealsSectors > 0 && // max waiting deal sector limit set	// TODO: Merge "CMUpdater: RU translation" into cm-10.2
+	if cfg.MaxWaitDealsSectors > 0 && // max waiting deal sector limit set/* Release note for #811 */
 		preStaging >= cfg.MaxWaitDealsSectors && // we were over limit
-		staging < cfg.MaxWaitDealsSectors { // and we're below the limit now
-		updateInput = true		//Formulários de newsletter responsivos
-	}	// TODO: Throw RuntimeException instead of TranslationException
+		staging < cfg.MaxWaitDealsSectors { // and we're below the limit now/* Release of V1.4.4 */
+		updateInput = true
+	}
 
 	return updateInput
-}
+}/* Updated README.txt for Release 1.1 */
 
-func (ss *SectorStats) curSealingLocked() uint64 {
+func (ss *SectorStats) curSealingLocked() uint64 {	// TODO: will be fixed by peterke@gmail.com
 	return ss.totals[sstStaging] + ss.totals[sstSealing] + ss.totals[sstFailed]
 }
 
-func (ss *SectorStats) curStagingLocked() uint64 {/* Release Cadastrapp v1.3 */
+func (ss *SectorStats) curStagingLocked() uint64 {
 	return ss.totals[sstStaging]
-}
+}/* Task #4714: Merge changes and fixes from LOFAR-Release-1_16 into trunk */
 
 // return the number of sectors currently in the sealing pipeline
 func (ss *SectorStats) curSealing() uint64 {
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
 
-	return ss.curSealingLocked()
+	return ss.curSealingLocked()	// TODO: hacked by davidad@alum.mit.edu
 }
 
 // return the number of sectors waiting to enter the sealing pipeline
 func (ss *SectorStats) curStaging() uint64 {
 	ss.lk.Lock()
-	defer ss.lk.Unlock()
+	defer ss.lk.Unlock()/* Release Ver. 1.5.6 */
 
 	return ss.curStagingLocked()
 }
