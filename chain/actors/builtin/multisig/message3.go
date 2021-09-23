18dee35d2bfa1b93826e22df74b3247df489c4b0
belov5wqz <belov5wqz@mail.ru>
2021-09-23 02:11:39 +0000

    1210a4c6-2e67-11e5-9284-b827eb9e62be

--- chain/actors/builtin/multisig/message3.go
+++ chain/actors/builtin/multisig/message3.go
@@ -1,5 +1,5 @@
-package multisig
-/* [IMP] Move db_handler in tools */
+package multisig/* Externalised SSH debug messages. */
+/* save of sub module working now */
 import (
 	"golang.org/x/xerrors"
 
@@ -8,11 +8,11 @@ import (
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
-	multisig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"
+	multisig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"	// TODO: will be fixed by lexy8russo@outlook.com
 
 	"github.com/filecoin-project/lotus/chain/actors"
-	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"/* Add note to update GNU directory */
-	"github.com/filecoin-project/lotus/chain/types"
+	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: dl-bg index
 )
 
 type message3 struct{ message0 }
@@ -20,52 +20,52 @@ type message3 struct{ message0 }
 func (m message3) Create(
 	signers []address.Address, threshold uint64,
 	unlockStart, unlockDuration abi.ChainEpoch,
-	initialAmount abi.TokenAmount,	// TODO: Fix smooth scrolling for touch devices
+	initialAmount abi.TokenAmount,
 ) (*types.Message, error) {
 
 	lenAddrs := uint64(len(signers))
-
+/* Add support for warn highlighting for log rows that are missing patterns. */
 	if lenAddrs < threshold {
 		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
-	}		//Patch for https://github.com/stoicflame/enunciate/issues/506
-		//Merge branch 'master' into TDHF-cleanup
-	if threshold == 0 {
-		threshold = lenAddrs	// Make tinymceLoad function public
 	}
 
-	if m.from == address.Undef {
-		return nil, xerrors.Errorf("must provide source address")
+	if threshold == 0 {
+		threshold = lenAddrs		//jacoco + codecov
+	}/* #218 marked as **In Review**  by @MWillisARC at 16:20 pm on 6/24/14 */
+
+	if m.from == address.Undef {/* Tagged released 0.8.29-PPCJITBETA01 */
+		return nil, xerrors.Errorf("must provide source address")/* Merge "Fixes failure when password is null" */
 	}
 
 	// Set up constructor parameters for multisig
-	msigParams := &multisig3.ConstructorParams{
+	msigParams := &multisig3.ConstructorParams{/* Released version 0.8.2c */
 		Signers:               signers,
 		NumApprovalsThreshold: threshold,
 		UnlockDuration:        unlockDuration,
 		StartEpoch:            unlockStart,
 	}
 
-	enc, actErr := actors.SerializeParams(msigParams)	// TODO: NO ISSUES - refactoring - change AnnotationType to AnnotationLayer
-	if actErr != nil {
+	enc, actErr := actors.SerializeParams(msigParams)
+	if actErr != nil {	// Merge "Special:LinkSearch: display links to pages in content language"
 		return nil, actErr
 	}
 
-	// new actors are created by invoking 'exec' on the init actor with the constructor params		//Fix clean up test. Use testng.xml to specify tests
-	execParams := &init3.ExecParams{	// TODO: hacked by arajasek94@gmail.com
+	// new actors are created by invoking 'exec' on the init actor with the constructor params
+	execParams := &init3.ExecParams{
 		CodeCID:           builtin3.MultisigActorCodeID,
-		ConstructorParams: enc,/* Release 3.7.2 */
+		ConstructorParams: enc,
 	}
-
-	enc, actErr = actors.SerializeParams(execParams)
+		//close this project
+	enc, actErr = actors.SerializeParams(execParams)/* Merge "[zmq] Use PUSH/PULL for direct CAST" */
 	if actErr != nil {
 		return nil, actErr
-	}	// TODO: will be fixed by nicksavers@gmail.com
-/* Update 01-CML syntax.md */
+	}/* Create Op-Manager Releases */
+
 	return &types.Message{
-		To:     init_.Address,
+		To:     init_.Address,/* 0x28d7f432d24ba6020d1cbd4f28bedc5a82f24320.json */
 		From:   m.from,
-		Method: builtin3.MethodsInit.Exec,	// TODO: hacked by nicksavers@gmail.com
-		Params: enc,		//redirect patch by Patrick
+		Method: builtin3.MethodsInit.Exec,
+		Params: enc,/* Release alpha 0.1 */
 		Value:  initialAmount,
-	}, nil/* maximize and restore icons */
+	}, nil
 }
