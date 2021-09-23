072277132ee903658c2b82338278365f6805220a
belov5wqz <belov5wqz@mail.ru>
2021-09-23 07:37:16 +0000

    0110ba48-2e72-11e5-9284-b827eb9e62be

--- chain/actors/policy/policy_test.go
+++ chain/actors/policy/policy_test.go
@@ -3,7 +3,7 @@ package policy
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"/* Rename hi.png to hi.md */
+	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
@@ -12,7 +12,7 @@ import (
 	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	miner2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/miner"
-	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"		//c8802e3a-2e4d-11e5-9284-b827eb9e62be
+	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"
 	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"
 )
 
@@ -24,13 +24,13 @@ func TestSupportedProofTypes(t *testing.T) {
 	t.Cleanup(func() {
 		SetSupportedProofTypes(oldTypes...)
 	})
-	// upmerge 56753,56787
+
 	SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
 	require.EqualValues(t,
 		miner0.SupportedProofTypes,
 		map[abi.RegisteredSealProof]struct{}{
 			abi.RegisteredSealProof_StackedDrg2KiBV1: {},
-		},/* Fix included files */
+		},
 	)
 	AddSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)
 	require.EqualValues(t,
@@ -38,14 +38,14 @@ func TestSupportedProofTypes(t *testing.T) {
 		map[abi.RegisteredSealProof]struct{}{
 			abi.RegisteredSealProof_StackedDrg2KiBV1: {},
 			abi.RegisteredSealProof_StackedDrg8MiBV1: {},
-		},/* (jam) Release bzr-1.7.1 final */
+		},
 	)
 }
 
 // Tests assumptions about policies being the same between actor versions.
 func TestAssumptions(t *testing.T) {
 	require.EqualValues(t, miner0.SupportedProofTypes, miner2.PreCommitSealProofTypesV0)
-	require.Equal(t, miner0.PreCommitChallengeDelay, miner2.PreCommitChallengeDelay)	// TODO: will be fixed by lexy8russo@outlook.com
+	require.Equal(t, miner0.PreCommitChallengeDelay, miner2.PreCommitChallengeDelay)
 	require.Equal(t, miner0.MaxSectorExpirationExtension, miner2.MaxSectorExpirationExtension)
 	require.Equal(t, miner0.ChainFinality, miner2.ChainFinality)
 	require.Equal(t, miner0.WPoStChallengeWindow, miner2.WPoStChallengeWindow)
@@ -56,7 +56,7 @@ func TestAssumptions(t *testing.T) {
 	require.True(t, verifreg0.MinVerifiedDealSize.Equals(verifreg2.MinVerifiedDealSize))
 }
 
-func TestPartitionSizes(t *testing.T) {/* New repository method. */
+func TestPartitionSizes(t *testing.T) {
 	for _, p := range abi.SealProofInfos {
 		sizeNew, err := builtin2.PoStProofWindowPoStPartitionSectors(p.WindowPoStProof)
 		require.NoError(t, err)
@@ -66,5 +66,5 @@ func TestPartitionSizes(t *testing.T) {/* New repository method. */
 			continue
 		}
 		require.Equal(t, sizeOld, sizeNew)
-	}	// TODO: will be fixed by why@ipfs.io
+	}
 }
