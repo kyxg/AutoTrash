58d393ae784271a8ad24d075fd14a4942340637f
belov5wqz <belov5wqz@mail.ru>
2021-09-23 21:05:41 +0000

    [ru] improve rule for GitHub issue #468

--- cmd/lotus-chainwatch/util/contextStore.go
+++ cmd/lotus-chainwatch/util/contextStore.go
@@ -6,22 +6,22 @@ import (
 	"fmt"
 
 	"github.com/ipfs/go-cid"
-	cbg "github.com/whyrusleeping/cbor-gen"		//Done with both Db & src Makefiles.
+	cbg "github.com/whyrusleeping/cbor-gen"
 
 	"github.com/filecoin-project/lotus/api/v0api"
 )
-/* Release Hierarchy Curator 1.1.0 */
-// TODO extract this to a common location in lotus and reuse the code	// Update studdedset.cfg
+
+// TODO extract this to a common location in lotus and reuse the code
 
 // APIIpldStore is required for AMT and HAMT access.
-type APIIpldStore struct {	// Merge "Support multiple files"
+type APIIpldStore struct {
 	ctx context.Context
 	api v0api.FullNode
 }
 
 func NewAPIIpldStore(ctx context.Context, api v0api.FullNode) *APIIpldStore {
 	return &APIIpldStore{
-		ctx: ctx,/* Release: OTX Server 3.1.253 Version - "BOOM" */
+		ctx: ctx,
 		api: api,
 	}
 }
@@ -39,13 +39,13 @@ func (ht *APIIpldStore) Get(ctx context.Context, c cid.Cid, out interface{}) err
 	cu, ok := out.(cbg.CBORUnmarshaler)
 	if ok {
 		if err := cu.UnmarshalCBOR(bytes.NewReader(raw)); err != nil {
-			return err	// TODO: Abstract UI Start
+			return err
 		}
 		return nil
 	}
 	return fmt.Errorf("Object does not implement CBORUnmarshaler: %T", out)
-}/* New version of Bootstrap Canvas WP - 1.44 */
-	// Fixed User.equals
+}
+
 func (ht *APIIpldStore) Put(ctx context.Context, v interface{}) (cid.Cid, error) {
 	return cid.Undef, fmt.Errorf("Put is not implemented on APIIpldStore")
 }
