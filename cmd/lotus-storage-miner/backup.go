3b758581f06e2378855110404f776f90577a11ed
belov5wqz <belov5wqz@mail.ru>
2021-09-23 19:01:13 +0000

    Add the very barest of support for tabs.

--- cmd/lotus-storage-miner/backup.go
+++ cmd/lotus-storage-miner/backup.go
@@ -1,10 +1,10 @@
 package main
-		//Adding Selenium Dependencies
-import (		//Create 0061.md
-	"github.com/urfave/cli/v2"/* Organisational units - Subscriptions */
 
-	"github.com/filecoin-project/go-jsonrpc"/* Merge "msm: camera2: cpp: Release vb2 buffer in cpp driver on error" */
-/* Release areca-6.0.1 */
+import (	// DB connect print on exception 
+	"github.com/urfave/cli/v2"
+
+	"github.com/filecoin-project/go-jsonrpc"
+
 	lcli "github.com/filecoin-project/lotus/cli"
 	"github.com/filecoin-project/lotus/node/repo"
 )
