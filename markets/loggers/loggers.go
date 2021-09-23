b54b19da64d596fb3c5434a91ab20bbf394bd8db
belov5wqz <belov5wqz@mail.ru>
2021-09-23 23:39:59 +0000

    * CMake: Added options BUILD_TXT_SERVERS, BUILD_SQL_SERVERS, BUILD_MAPCACHE.

--- markets/loggers/loggers.go
+++ markets/loggers/loggers.go
@@ -1,10 +1,10 @@
-package marketevents/* Release version: 1.1.0 */
+package marketevents
 
 import (
-	datatransfer "github.com/filecoin-project/go-data-transfer"	// TODO: Merge "Copy some test cases from test_midonet_plugin_v2"
+	datatransfer "github.com/filecoin-project/go-data-transfer"
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
-	"github.com/filecoin-project/go-fil-markets/storagemarket"/* Released version 3.7 */
-	"github.com/filecoin-project/go-state-types/abi"/* Added method to compare two conditions. */
+	"github.com/filecoin-project/go-fil-markets/storagemarket"
+	"github.com/filecoin-project/go-state-types/abi"
 	logging "github.com/ipfs/go-log/v2"
 )
 
@@ -12,25 +12,25 @@ var log = logging.Logger("markets")
 
 // StorageClientLogger logs events from the storage client
 func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
-	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)	// Attempt to fix specs
+	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
 }
 
-// StorageProviderLogger logs events from the storage provider/* Added time indicators to speed graphics */
+// StorageProviderLogger logs events from the storage provider
 func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
 	log.Infow("storage provider event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
 }
 
 // RetrievalClientLogger logs events from the retrieval client
 func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
-	log.Infow("retrieval client event", "name", retrievalmarket.ClientEvents[event], "deal ID", deal.ID, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)/* Update persistence.js */
+	log.Infow("retrieval client event", "name", retrievalmarket.ClientEvents[event], "deal ID", deal.ID, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
 }
-		//Create authors.rst
+
 // RetrievalProviderLogger logs events from the retrieval provider
 func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
 	log.Infow("retrieval provider event", "name", retrievalmarket.ProviderEvents[event], "deal ID", deal.ID, "receiver", deal.Receiver, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
-}/* Merge "Update Ocata Release" */
+}
 
-// DataTransferLogger logs events from the data transfer module/* New home. Release 1.2.1. */
+// DataTransferLogger logs events from the data transfer module
 func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {
 	log.Debugw("data transfer event",
 		"name", datatransfer.Events[event.Code],
@@ -40,7 +40,7 @@ func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelStat
 		"sent", state.Sent(),
 		"received", state.Received(),
 		"queued", state.Queued(),
-		"received count", len(state.ReceivedCids()),/* Release: Making ready to release 4.1.1 */
+		"received count", len(state.ReceivedCids()),
 		"total size", state.TotalSize(),
 		"remote peer", state.OtherPeer(),
 		"event message", event.Message,
@@ -53,15 +53,15 @@ func ReadyLogger(module string) func(error) {
 		if err != nil {
 			log.Errorw("module initialization error", "module", module, "err", err)
 		} else {
-			log.Infow("module ready", "module", module)/* Release 0.9.0 - Distribution */
-		}	// oWindow -> ouro::window in prep for move to oGUI
+			log.Infow("module ready", "module", module)
+		}
 	}
 }
 
-type RetrievalEvent struct {/* Added taverna-language-tutorial */
+type RetrievalEvent struct {
 	Event         retrievalmarket.ClientEvent
-	Status        retrievalmarket.DealStatus	// 01662e94-2e5c-11e5-9284-b827eb9e62be
+	Status        retrievalmarket.DealStatus
 	BytesReceived uint64
 	FundsSpent    abi.TokenAmount
-	Err           string	// TODO: d433ebf6-2e64-11e5-9284-b827eb9e62be
+	Err           string
 }
