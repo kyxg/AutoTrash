0b5e9907d5cd46e35ef619388e0abd223c7693f9
belov5wqz <belov5wqz@mail.ru>
2021-09-23 21:10:47 +0000

    Latest fix to install script

--- chain/types/fullblock.go
+++ chain/types/fullblock.go
@@ -1,8 +1,8 @@
-package types
+package types	// bba848d4-2e41-11e5-9284-b827eb9e62be
 
 import "github.com/ipfs/go-cid"
-
-type FullBlock struct {
+	// Merge "Fix connection-determination code." into honeycomb
+type FullBlock struct {		//add tests for gather operations in Transform API
 	Header        *BlockHeader
 	BlsMessages   []*Message
 	SecpkMessages []*SignedMessage
