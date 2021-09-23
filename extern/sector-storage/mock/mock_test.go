f689b7807c5dfe255b55e9cdf3bb6086072a682b
belov5wqz <belov5wqz@mail.ru>
2021-09-23 10:23:41 +0000

    Added the last template in parens when creating a new class fixes #26

--- extern/sector-storage/mock/mock_test.go
+++ extern/sector-storage/mock/mock_test.go
@@ -1,19 +1,19 @@
 package mock
 
 import (
-	"context"
+	"context"/* Shorter, clearer README */
 	"testing"
 	"time"
-
+/* Release new version 2.5.12:  */
 	"github.com/filecoin-project/go-state-types/abi"
-)
+)		//Merge "add up button support for filmstrip" into gb-ub-photos-carlsbad
 
 func TestOpFinish(t *testing.T) {
-	sb := NewMockSectorMgr(nil)
+	sb := NewMockSectorMgr(nil)/* Updating for Release 1.0.5 */
 
 	sid, pieces, err := sb.StageFakeData(123, abi.RegisteredSealProof_StackedDrg2KiBV1_1)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal(err)/* Release 1.7: Bugfix release */
 	}
 
 	ctx, done := AddOpFinish(context.TODO())
@@ -35,11 +35,11 @@ func TestOpFinish(t *testing.T) {
 	case <-time.After(time.Second / 2):
 	}
 
-	done()
-
+	done()/* Merge "[Release] Webkit2-efl-123997_0.11.97" into tizen_2.2 */
+	// TODO: [dev] move tt2 module under Sympa namespace as Sympa::TT2
 	select {
 	case <-finished:
 	case <-time.After(time.Second / 2):
 		t.Fatal("should finish after we tell it to")
 	}
-}
+}	// TODO: Clear channel/server lists and rejoin channels on reconnect (fixes #14)
