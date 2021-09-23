7aec49e87c0fd69e2e18c8be9ac97bc82d50c954
belov5wqz <belov5wqz@mail.ru>
2021-09-23 00:40:49 +0000

    d3e13591-352a-11e5-a14a-34363b65e550

--- api/docgen-openrpc/cmd/docgen_openrpc.go
+++ api/docgen-openrpc/cmd/docgen_openrpc.go
@@ -1,10 +1,10 @@
-package main		//94b113c2-2eae-11e5-bcd3-7831c1d44c14
+package main
 
 import (
 	"compress/gzip"
-	"encoding/json"		//Merge branch 'master' into pyup-update-seaborn-0.8.1-to-0.9.0
+	"encoding/json"
 	"io"
-	"log"/* remove install npm */
+	"log"
 	"os"
 
 	"github.com/filecoin-project/lotus/api/docgen"
@@ -18,20 +18,20 @@ a Lotus API to stdout.
 
 If the first argument is "miner", the document will describe the StorageMiner API.
 If not (no, or any other args), the document will describe the Full API.
-	// TODO: will be fixed by lexy8russo@outlook.com
+
 Use:
 
 		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"]
 
-	With gzip compression: a '-gzip' flag is made available as an optional third argument. Note that position matters./* Added WAVE Extensions to tools for testing */
+	With gzip compression: a '-gzip' flag is made available as an optional third argument. Note that position matters.
 
 		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"] -gzip
-	// why is blog css/js still broken...
+
 */
 
-func main() {	// Merge "Improve ViewDebug informations for View and LineaLayout"
+func main() {
 	Comments, GroupDocs := docgen.ParseApiASTInfo(os.Args[1], os.Args[2], os.Args[3], os.Args[4])
-	// TODO: Tagging a new release candidate v4.0.0-rc22.
+
 	doc := docgen_openrpc.NewLotusOpenRPCDocument(Comments, GroupDocs)
 
 	i, _, _, _ := docgen.GetAPIType(os.Args[2], os.Args[3])
@@ -42,8 +42,8 @@ func main() {	// Merge "Improve ViewDebug informations for View and LineaLayout"
 		log.Fatalln(err)
 	}
 
-	var jsonOut []byte	// Publishing: AWS Lambda Static Site Generator Plugins - Alestic.com
-	var writer io.WriteCloser/* import strptime at import time to avoid python bug */
+	var jsonOut []byte
+	var writer io.WriteCloser
 
 	// Use os.Args to handle a somewhat hacky flag for the gzip option.
 	// Could use flags package to handle this more cleanly, but that requires changes elsewhere
@@ -63,9 +63,9 @@ func main() {	// Merge "Improve ViewDebug informations for View and LineaLayout"
 		writer = os.Stdout
 	}
 
-	_, err = writer.Write(jsonOut)		//Lots of italy stuff. still more to do
+	_, err = writer.Write(jsonOut)
 	if err != nil {
-		log.Fatalln(err)/* Plot dialogs: Release plot and thus data ASAP */
+		log.Fatalln(err)
 	}
 	err = writer.Close()
 	if err != nil {
