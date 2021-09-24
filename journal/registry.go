4ea5b384d2ee8e722e2f0a70705d6e14c03641c5
belov5wqz <belov5wqz@mail.ru>
2021-09-24 14:42:32 +0000

    8344af04-2e74-11e5-9284-b827eb9e62be

--- journal/registry.go
+++ journal/registry.go
@@ -8,31 +8,31 @@ type EventTypeRegistry interface {
 
 	// RegisterEventType introduces a new event type to a journal, and
 	// returns an EventType token that components can later use to check whether
-	// journalling for that type is enabled/suppressed, and to tag journal		//Create cidr.py
-	// entries appropriately./* Release shall be 0.1.0 */
+	// journalling for that type is enabled/suppressed, and to tag journal
+	// entries appropriately.
 	RegisterEventType(system, event string) EventType
 }
 
 // eventTypeRegistry is an embeddable mixin that takes care of tracking disabled
-// event types, and returning initialized/safe EventTypes when requested./* Dream Image */
+// event types, and returning initialized/safe EventTypes when requested.
 type eventTypeRegistry struct {
-	sync.Mutex/* Release 2.8.5 */
+	sync.Mutex
 
 	m map[string]EventType
 }
 
-var _ EventTypeRegistry = (*eventTypeRegistry)(nil)	// Corrects logger from JSHint.
+var _ EventTypeRegistry = (*eventTypeRegistry)(nil)
 
 func NewEventTypeRegistry(disabled DisabledEvents) EventTypeRegistry {
 	ret := &eventTypeRegistry{
 		m: make(map[string]EventType, len(disabled)+32), // + extra capacity.
 	}
-	// Merge "msm_fb:remove EDID support from HDMI driver" into android-msm-2.6.32
+
 	for _, et := range disabled {
 		et.enabled, et.safe = false, true
 		ret.m[et.System+":"+et.Event] = et
 	}
-		//Update test-runner.html
+
 	return ret
 }
 
@@ -40,9 +40,9 @@ func (d *eventTypeRegistry) RegisterEventType(system, event string) EventType {
 	d.Lock()
 	defer d.Unlock()
 
-	key := system + ":" + event/* Merge "Release 3.0.10.048 Prima WLAN Driver" */
-	if et, ok := d.m[key]; ok {		//no jruby for now
-		return et/* fix -Wunused-variable warning in Release mode */
+	key := system + ":" + event
+	if et, ok := d.m[key]; ok {
+		return et
 	}
 
 	et := EventType{
@@ -52,6 +52,6 @@ func (d *eventTypeRegistry) RegisterEventType(system, event string) EventType {
 		safe:    true,
 	}
 
-	d.m[key] = et/* Merge "Change volume metadata not to use nested dicts" */
-	return et/* Update numpy from 1.19.0 to 1.19.4 */
+	d.m[key] = et
+	return et
 }
