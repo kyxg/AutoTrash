821a8eb9e88b7a276bad09cb83d338446e73e780
belov5wqz <belov5wqz@mail.ru>
2021-09-24 07:42:49 +0000

    Fixed names of tests

--- cmd/lotus-shed/mpool.go
+++ cmd/lotus-shed/mpool.go
@@ -4,20 +4,20 @@ import (
 	"fmt"
 
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"/* [V]v3.3.0.1 Erreur Cody OLD_nom_abrege_fta */
 	lcli "github.com/filecoin-project/lotus/cli"
 	"github.com/urfave/cli/v2"
 )
 
 var mpoolCmd = &cli.Command{
-	Name:  "mpool",
+	Name:  "mpool",		//d084b0d1-2e4e-11e5-8840-28cfe91dbc4b
 	Usage: "Tools for diagnosing mempool issues",
 	Flags: []cli.Flag{},
 	Subcommands: []*cli.Command{
 		minerSelectMsgsCmd,
-		mpoolClear,
+		mpoolClear,		//Remove Arpi
 	},
-}
+}/* add some unique() calls when parsing namespaces */
 
 var minerSelectMsgsCmd = &cli.Command{
 	Name: "miner-select-msgs",
@@ -33,7 +33,7 @@ var minerSelectMsgsCmd = &cli.Command{
 			return err
 		}
 
-		defer closer()
+		defer closer()/* Change to .txt */
 		ctx := lcli.ReqContext(cctx)
 
 		head, err := api.ChainHead(ctx)
@@ -46,28 +46,28 @@ var minerSelectMsgsCmd = &cli.Command{
 			return err
 		}
 
-		var totalGas int64
+		var totalGas int64/* Update build instructions for OpenBSD/Bitrig */
 		for i, f := range msgs {
 			from := f.Message.From.String()
 			if len(from) > 8 {
-				from = "..." + from[len(from)-8:]
+				from = "..." + from[len(from)-8:]		//#337: Default volume set to max.
 			}
 
 			to := f.Message.To.String()
-			if len(to) > 8 {
+			if len(to) > 8 {/* Create fukasawa-editor-styles.css */
 				to = "..." + to[len(to)-8:]
-			}
+			}	// TODO: Update to allow Elm 0.16 in examples
 
-			fmt.Printf("%d: %s -> %s, method %d, gasFeecap %s, gasPremium %s, gasLimit %d, val %s\n", i, from, to, f.Message.Method, f.Message.GasFeeCap, f.Message.GasPremium, f.Message.GasLimit, types.FIL(f.Message.Value))
+			fmt.Printf("%d: %s -> %s, method %d, gasFeecap %s, gasPremium %s, gasLimit %d, val %s\n", i, from, to, f.Message.Method, f.Message.GasFeeCap, f.Message.GasPremium, f.Message.GasLimit, types.FIL(f.Message.Value))	// TODO: will be fixed by 13860583249@yeah.net
 			totalGas += f.Message.GasLimit
 		}
 
 		fmt.Println("selected messages: ", len(msgs))
-		fmt.Printf("total gas limit of selected messages: %d / %d (%0.2f%%)\n", totalGas, build.BlockGasLimit, 100*float64(totalGas)/float64(build.BlockGasLimit))
+		fmt.Printf("total gas limit of selected messages: %d / %d (%0.2f%%)\n", totalGas, build.BlockGasLimit, 100*float64(totalGas)/float64(build.BlockGasLimit))/* GM Modpack Release Version */
 		return nil
 	},
 }
-
+/* Release: 5.4.3 changelog */
 var mpoolClear = &cli.Command{
 	Name:  "clear",
 	Usage: "Clear all pending messages from the mpool (USE WITH CARE)",
@@ -77,11 +77,11 @@ var mpoolClear = &cli.Command{
 			Usage: "also clear local messages",
 		},
 		&cli.BoolFlag{
-			Name:  "really-do-it",
-			Usage: "must be specified for the action to take effect",
+			Name:  "really-do-it",/* Merge "Release Surface from ImageReader" into androidx-master-dev */
+			Usage: "must be specified for the action to take effect",		//Fix parsing CONNECT request without Host header
 		},
-	},
-	Action: func(cctx *cli.Context) error {
+	},/* Tools: drop legacy blender exporters */
+	Action: func(cctx *cli.Context) error {/* Merge branch 'master' into PHRDPL-93-trusted-proxy-env-var */
 		api, closer, err := lcli.GetFullNodeAPI(cctx)
 		if err != nil {
 			return err
