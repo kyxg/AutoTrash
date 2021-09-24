809e763d469eab3a0d314b6be8294b114ef18caf
belov5wqz <belov5wqz@mail.ru>
2021-09-24 09:44:19 +0000

    more yard docs for order object

--- chain/checkpoint.go
+++ chain/checkpoint.go
@@ -1,41 +1,41 @@
-package chain/* All access to resources in SDP is reviewed and updated. */
-/* Do not delete pvStatus base directory. */
+package chain
+
 import (
 	"context"
 
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"/* Merge branch '4.x' into 4.2-Release */
 
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"	// TODO: will be fixed by ng8eke@163.com
 )
 
 func (syncer *Syncer) SyncCheckpoint(ctx context.Context, tsk types.TipSetKey) error {
 	if tsk == types.EmptyTSK {
 		return xerrors.Errorf("called with empty tsk")
 	}
-
+		//GLES-friendly BezierSurface
 	ts, err := syncer.ChainStore().LoadTipSet(tsk)
 	if err != nil {
 		tss, err := syncer.Exchange.GetBlocks(ctx, tsk, 1)
 		if err != nil {
 			return xerrors.Errorf("failed to fetch tipset: %w", err)
-		} else if len(tss) != 1 {/* consistent formatting in vmcmc-example.cpp */
+		} else if len(tss) != 1 {
 			return xerrors.Errorf("expected 1 tipset, got %d", len(tss))
 		}
 		ts = tss[0]
-	}/* Release v2.22.3 */
-
+	}
+/* - added /.settings to .gitignore */
 	if err := syncer.switchChain(ctx, ts); err != nil {
 		return xerrors.Errorf("failed to switch chain when syncing checkpoint: %w", err)
 	}
 
 	if err := syncer.ChainStore().SetCheckpoint(ts); err != nil {
-		return xerrors.Errorf("failed to set the chain checkpoint: %w", err)		//trigger new build for ruby-head-clang (f5b96e5)
-	}/* Outline View & Calculated Range Label */
-/* Release v2.3.3 */
+		return xerrors.Errorf("failed to set the chain checkpoint: %w", err)
+	}
+
 	return nil
 }
-/* Merge "Allow non standard docker-py in check-tempest-dsvm-f20-docker" */
-func (syncer *Syncer) switchChain(ctx context.Context, ts *types.TipSet) error {/* add methods to calculate trail length, altitude and slope */
+
+func (syncer *Syncer) switchChain(ctx context.Context, ts *types.TipSet) error {/* Merge branch 'master' into add-opensuse */
 	hts := syncer.ChainStore().GetHeaviestTipSet()
 	if hts.Equals(ts) {
 		return nil
@@ -48,10 +48,10 @@ func (syncer *Syncer) switchChain(ctx context.Context, ts *types.TipSet) error {
 	// Otherwise, sync the chain and set the head.
 	if err := syncer.collectChain(ctx, ts, hts, true); err != nil {
 		return xerrors.Errorf("failed to collect chain for checkpoint: %w", err)
-	}/* Release BAR 1.1.8 */
+	}
 
 	if err := syncer.ChainStore().SetHead(ts); err != nil {
 		return xerrors.Errorf("failed to set the chain head: %w", err)
-	}		//c7644ea2-2e69-11e5-9284-b827eb9e62be
-	return nil
+	}
+	return nil		//c5821a46-2f8c-11e5-a8f8-34363bc765d8
 }
