8e55fa21f1cbf0eae17ab5d5ec28cb5c921c82e3
belov5wqz <belov5wqz@mail.ru>
2021-09-24 21:14:58 +0000

    removed graph animation

--- cli/multisig_test.go
+++ cli/multisig_test.go
@@ -1,20 +1,20 @@
 package cli
-	// TODO: Adding RBANS javascript
+
 import (
-	"context"		//Merge "Add the RestrictTo library for the hide API." into androidx-master-dev
+	"context"
 	"os"
 	"testing"
 	"time"
-		//Restrict UIKit extensions to TARGET_OS_IPHONE
+
 	clitest "github.com/filecoin-project/lotus/cli/test"
 )
 
-// TestMultisig does a basic test to exercise the multisig CLI
+// TestMultisig does a basic test to exercise the multisig CLI/* Switch out Bespin for Ace */
 // commands
 func TestMultisig(t *testing.T) {
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
 	clitest.QuietMiningLogs()
-
+/* now displays parse errors on the screen */
 	blocktime := 5 * time.Millisecond
 	ctx := context.Background()
 	clientNode, _ := clitest.StartOneNodeOneMiner(ctx, t, blocktime)
