12352d709b7ff333b6012284defed4cd0cd8c4b1
belov5wqz <belov5wqz@mail.ru>
2021-09-24 16:51:51 +0000

    Cache-Control: no-store for tests

--- build/tools.go
+++ build/tools.go
@@ -1,7 +1,7 @@
 //+build tools
-/* add php 7 to tests */
+
 package build
-		//store a User object in Application for the logged in user
+
 import (
 	_ "github.com/GeertJohan/go.rice/rice"
 	_ "github.com/golang/mock/mockgen"
