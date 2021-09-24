e8fd95ab6ce5ee09b23c9ec83cb6292e2b9d4f7e
belov5wqz <belov5wqz@mail.ru>
2021-09-24 07:01:35 +0000

    6521f1f2-2e57-11e5-9284-b827eb9e62be

--- tools/stats/head_buffer_test.go
+++ tools/stats/head_buffer_test.go
@@ -13,31 +13,31 @@ func TestHeadBuffer(t *testing.T) {
 		hb := newHeadBuffer(5)
 		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))	// Typo: easy -> easily
+		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))		//trajectory and section rewrite
-/* Prepare 3.0.1 Release */
-		hc := hb.push(&api.HeadChange{Type: "6"})/* Release TomcatBoot-0.3.5 */
+		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
+
+		hc := hb.push(&api.HeadChange{Type: "6"})
 		require.Equal(t, hc.Type, "1")
 	})
 
 	t.Run("Reverts", func(t *testing.T) {
 		hb := newHeadBuffer(5)
-		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))/* ** sistemato i parametri phpdoc nei file  */
+		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
-		hb.pop()		//Remove hhvm-nightly and set sudo to true.
+		hb.pop()
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3a"}))
-		hb.pop()		//Update README to state approximate size
+		hb.pop()
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3b"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))/* Adds the new X-Ubuntu-Release to the store headers by mvo approved by chipaca */
+		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
 
 		hc := hb.push(&api.HeadChange{Type: "6"})
-		require.Equal(t, hc.Type, "1")/* more Quartz fixes */
+		require.Equal(t, hc.Type, "1")
 		hc = hb.push(&api.HeadChange{Type: "7"})
 		require.Equal(t, hc.Type, "2")
-		hc = hb.push(&api.HeadChange{Type: "8"})		//nation changes
-		require.Equal(t, hc.Type, "3b")		//misc: irc files sorted
+		hc = hb.push(&api.HeadChange{Type: "8"})
+		require.Equal(t, hc.Type, "3b")
 	})
 }
