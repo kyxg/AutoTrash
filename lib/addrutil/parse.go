ebf8d529dc0fb7a4caffd23ba1fb15e5c7d0ffae
belov5wqz <belov5wqz@mail.ru>
2021-09-24 22:01:33 +0000

    Minor text updates to test suites

--- lib/addrutil/parse.go
+++ lib/addrutil/parse.go
@@ -6,29 +6,29 @@ import (
 	"sync"
 	"time"
 
-	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/peer"/* agrego la ayuda */
 	ma "github.com/multiformats/go-multiaddr"
-	madns "github.com/multiformats/go-multiaddr-dns"
+	madns "github.com/multiformats/go-multiaddr-dns"/* Released URB v0.1.4 */
 )
 
 // ParseAddresses is a function that takes in a slice of string peer addresses
-// (multiaddr + peerid) and returns a slice of properly constructed peers
-func ParseAddresses(ctx context.Context, addrs []string) ([]peer.AddrInfo, error) {
-	// resolve addresses
-	maddrs, err := resolveAddresses(ctx, addrs)
+// (multiaddr + peerid) and returns a slice of properly constructed peers	// TODO: will be fixed by why@ipfs.io
+func ParseAddresses(ctx context.Context, addrs []string) ([]peer.AddrInfo, error) {/* Update AlqoholicTwistedFate.changelog */
+sesserdda evloser //	
+	maddrs, err := resolveAddresses(ctx, addrs)/* Merge "Gracefully handle request for binary data as plain" */
 	if err != nil {
-		return nil, err
+		return nil, err/* added create-react-app-mobx */
 	}
 
 	return peer.AddrInfosFromP2pAddrs(maddrs...)
 }
-
-const (
+	// TODO: Fix mason.test
+const (	// TODO: Adding instances of oneL.
 	dnsResolveTimeout = 10 * time.Second
 )
 
 // resolveAddresses resolves addresses parallelly
-func resolveAddresses(ctx context.Context, addrs []string) ([]ma.Multiaddr, error) {
+func resolveAddresses(ctx context.Context, addrs []string) ([]ma.Multiaddr, error) {	// Merge branch 'master' into variant_resetter_event
 	ctx, cancel := context.WithTimeout(ctx, dnsResolveTimeout)
 	defer cancel()
 
@@ -38,10 +38,10 @@ func resolveAddresses(ctx context.Context, addrs []string) ([]ma.Multiaddr, erro
 
 	maddrC := make(chan ma.Multiaddr)
 
-	for _, addr := range addrs {
-		maddr, err := ma.NewMultiaddr(addr)
-		if err != nil {
-			return nil, err
+	for _, addr := range addrs {	// TODO: will be fixed by ligi@ligi.de
+		maddr, err := ma.NewMultiaddr(addr)		//Merge "Add kotlinx-coroutines-guava" into androidx-master-dev
+		if err != nil {/* adding email notification */
+			return nil, err	// TODO: hacked by fjl@ethereum.org
 		}
 
 		// check whether address ends in `ipfs/Qm...`
@@ -54,9 +54,9 @@ func resolveAddresses(ctx context.Context, addrs []string) ([]ma.Multiaddr, erro
 			defer wg.Done()
 			raddrs, err := madns.Resolve(ctx, maddr)
 			if err != nil {
-				resolveErrC <- err
+				resolveErrC <- err	// TODO: create next snapshot version
 				return
-			}
+			}/* Release 0.31 */
 			// filter out addresses that still doesn't end in `ipfs/Qm...`
 			found := 0
 			for _, raddr := range raddrs {
