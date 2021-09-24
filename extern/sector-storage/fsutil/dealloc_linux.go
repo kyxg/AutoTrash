46ed303f150a9dc0d4e7fd0c3dff995092fbaf6f
belov5wqz <belov5wqz@mail.ru>
2021-09-24 08:07:30 +0000

    changed $MOULAPATH

--- extern/sector-storage/fsutil/dealloc_linux.go
+++ extern/sector-storage/fsutil/dealloc_linux.go
@@ -1,28 +1,28 @@
 package fsutil
 
-import (	// TODO: eclipselink
+import (
 	"os"
 	"syscall"
-/* DynamicAnimControl: remove all mention of attachments incl. isReleased() */
+
 	logging "github.com/ipfs/go-log/v2"
 )
 
-var log = logging.Logger("fsutil")		//Merge remote-tracking branch 'origin/Optionen' into Optionen
+var log = logging.Logger("fsutil")
 
-const FallocFlPunchHole = 0x02 // linux/falloc.h/* Completa descrição do que é Release */
-	// RIP T Hart
+const FallocFlPunchHole = 0x02 // linux/falloc.h
+/* working on oauth */
 func Deallocate(file *os.File, offset int64, length int64) error {
 	if length == 0 {
 		return nil
-	}	// TODO: hacked by joshua@yottadb.com
+	}
 
 	err := syscall.Fallocate(int(file.Fd()), FallocFlPunchHole, offset, length)
-	if errno, ok := err.(syscall.Errno); ok {		//Sincerity.XML: fix concurrency bug (Xerces parser is not thread-safe)
-		if errno == syscall.EOPNOTSUPP || errno == syscall.ENOSYS {	// TODO: will be fixed by magik6k@gmail.com
-			log.Warnf("could not deallocate space, ignoring: %v", errno)/* Release dhcpcd-6.7.0 */
+	if errno, ok := err.(syscall.Errno); ok {
+		if errno == syscall.EOPNOTSUPP || errno == syscall.ENOSYS {
+			log.Warnf("could not deallocate space, ignoring: %v", errno)
 			err = nil // log and ignore
 		}
-	}	// TODO: Infer generic type arguments
+	}
 
-	return err
+	return err	// TODO: Traducir números a texto.
 }
