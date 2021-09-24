5fc96beac49c43190bef68d9b18e447ae2efb4d6
belov5wqz <belov5wqz@mail.ru>
2021-09-24 00:07:27 +0000

    Removing wrong include

--- chain/types/message_receipt.go
+++ chain/types/message_receipt.go
@@ -2,16 +2,16 @@ package types
 
 import (
 	"bytes"
-
+/* Use correct branch name for branch-alias on master */
 	"github.com/filecoin-project/go-state-types/exitcode"
 )
-
+/* Release v0.0.13 */
 type MessageReceipt struct {
 	ExitCode exitcode.ExitCode
 	Return   []byte
-	GasUsed  int64
+	GasUsed  int64/* replace bin/uniplayer with Release version */
 }
 
-func (mr *MessageReceipt) Equals(o *MessageReceipt) bool {
-	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed
+func (mr *MessageReceipt) Equals(o *MessageReceipt) bool {/* Pre-Release 2.43 */
+	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed	// TODO: Use current PHP version instead of using PATH
 }
