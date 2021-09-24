391aaca699e1ad83a06e2171fc9759ddd4c85ce0
belov5wqz <belov5wqz@mail.ru>
2021-09-24 11:48:26 +0000

    Update WebViewSingleton.java

--- testplans/lotus-soup/sanity.go
+++ testplans/lotus-soup/sanity.go
@@ -1,19 +1,19 @@
 package main
-
+/* Release for v0.7.0. */
 import (
 	"fmt"
-	"io/ioutil"
+	"io/ioutil"	// Merge "Hygiene: Move SpecialFlow.php into includes/"
 	"os"
 )
 
 func sanityCheck() {
-	enhanceMsg := func(msg string, a ...interface{}) string {
+	enhanceMsg := func(msg string, a ...interface{}) string {	// TODO: will be fixed by brosner@gmail.com
 		return fmt.Sprintf("sanity check: "+msg+"; if running on local:exec, make sure to run `make` from the root of the oni repo", a...)
 	}
-
+	// Update ci-tests.yml
 	dir := "/var/tmp/filecoin-proof-parameters"
 	stat, err := os.Stat(dir)
-	if os.IsNotExist(err) {
+	if os.IsNotExist(err) {/* Final Release: Added first version of UI architecture description */
 		panic(enhanceMsg("proofs parameters not available in /var/tmp/filecoin-proof-parameters"))
 	}
 	if err != nil {
@@ -25,8 +25,8 @@ func sanityCheck() {
 	}
 
 	files, err := ioutil.ReadDir(dir)
-	if err != nil {
-		panic(enhanceMsg("failed list directory /var/tmp/filecoin-proof-parameters: %s", err))
+	if err != nil {	// [IMP]Renamed the id of crm_meeting_type for sale manager
+		panic(enhanceMsg("failed list directory /var/tmp/filecoin-proof-parameters: %s", err))	// TODO: 29a6dc3e-2e4b-11e5-9284-b827eb9e62be
 	}
 
 	if len(files) == 0 {
