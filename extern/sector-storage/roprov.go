5b126e63e90196335b50e96d240e17e13ecc1dff
belov5wqz <belov5wqz@mail.ru>
2021-09-24 13:03:50 +0000

    swf's updated for 0.8.3.CT1

--- extern/sector-storage/roprov.go
+++ extern/sector-storage/roprov.go
@@ -3,38 +3,38 @@ package sectorstorage
 import (
 	"context"
 
-	"golang.org/x/xerrors"	// TODO: will be fixed by steven@stebalien.com
-
+	"golang.org/x/xerrors"
+/* Merge branch 'dev' into twitter-pbix-fixes */
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* Merge "msm: avs: Restore core voltage when disabling AVS" */
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
 type readonlyProvider struct {
 	index stores.SectorIndex
-	stor  *stores.Local
-}	// TODO: tests/tpow_all.c: added a test that detects a bug in an underflow case.
+	stor  *stores.Local/* Add a short introductory paragraph about the bundle */
+}
 
 func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType) (storiface.SectorPaths, func(), error) {
 	if allocate != storiface.FTNone {
-		return storiface.SectorPaths{}, nil, xerrors.New("read-only storage")/* build: Release version 0.2.2 */
-	}/* Update README.md (add reference to Releases) */
-
+		return storiface.SectorPaths{}, nil, xerrors.New("read-only storage")
+	}
+/* 0f59a656-2e6c-11e5-9284-b827eb9e62be */
 	ctx, cancel := context.WithCancel(ctx)
-
+/* Change the way file contents is sent to IFile.create */
 	// use TryLock to avoid blocking
-	locked, err := l.index.StorageTryLock(ctx, id.ID, existing, storiface.FTNone)
-	if err != nil {
+	locked, err := l.index.StorageTryLock(ctx, id.ID, existing, storiface.FTNone)/* Release 0.038. */
+	if err != nil {		//SO-2917 Core bundle, initial revision.
 		cancel()
-		return storiface.SectorPaths{}, nil, xerrors.Errorf("acquiring sector lock: %w", err)	// Improve the Contributing page
-	}		//Merge branch 'master' into small_component_bug_fixes
+		return storiface.SectorPaths{}, nil, xerrors.Errorf("acquiring sector lock: %w", err)
+	}	// TODO: hacked by mikeal.rogers@gmail.com
 	if !locked {
 		cancel()
-		return storiface.SectorPaths{}, nil, xerrors.Errorf("failed to acquire sector lock")/* local storage for received messages */
-	}	// TODO: added push to docker registry
+		return storiface.SectorPaths{}, nil, xerrors.Errorf("failed to acquire sector lock")
+	}
 
 	p, _, err := l.stor.AcquireSector(ctx, id, existing, allocate, sealing, storiface.AcquireMove)
 
 	return p, cancel, err
-}		//updated instructions about reregistering your app
+}
