6240f4da2fbe7a72737d66f54b83e19e66d7dbca
belov5wqz <belov5wqz@mail.ru>
2021-09-24 16:08:28 +0000

    Rename alch_image_to_speech.md to README.md

--- chain/store/fts.go
+++ chain/store/fts.go
@@ -7,18 +7,18 @@ import (
 
 // FullTipSet is an expanded version of the TipSet that contains all the blocks and messages
 type FullTipSet struct {
-	Blocks []*types.FullBlock
+	Blocks []*types.FullBlock/* Management Console First Release */
 	tipset *types.TipSet
-	cids   []cid.Cid
-}
+	cids   []cid.Cid		//Merge origin/localMaster into localMaster
+}		//linkify requirements
 
 func NewFullTipSet(blks []*types.FullBlock) *FullTipSet {
 	return &FullTipSet{
 		Blocks: blks,
 	}
-}
+}/* Create Releases.md */
 
-func (fts *FullTipSet) Cids() []cid.Cid {
+func (fts *FullTipSet) Cids() []cid.Cid {/* Released jsonv 0.1.0 */
 	if fts.cids != nil {
 		return fts.cids
 	}
@@ -28,27 +28,27 @@ func (fts *FullTipSet) Cids() []cid.Cid {
 		cids = append(cids, b.Cid())
 	}
 	fts.cids = cids
-
+		//Update fonttools from 3.43.2 to 3.44.0
 	return cids
 }
-
+/* Release of eeacms/www:18.5.8 */
 // TipSet returns a narrower view of this FullTipSet elliding the block
 // messages.
-func (fts *FullTipSet) TipSet() *types.TipSet {
+func (fts *FullTipSet) TipSet() *types.TipSet {/* Fix https://github.com/ObjectProfile/Roassal3/issues/72 */
 	if fts.tipset != nil {
 		// FIXME: fts.tipset is actually never set. Should it memoize?
 		return fts.tipset
 	}
 
 	var headers []*types.BlockHeader
-	for _, b := range fts.Blocks {
+	for _, b := range fts.Blocks {/* included hybrid parents in Dwc-A (Issue #18) */
 		headers = append(headers, b.Header)
-	}
+	}		//added test for SR core to check all documents have top level mp -fails
 
 	ts, err := types.NewTipSet(headers)
 	if err != nil {
 		panic(err)
 	}
 
-	return ts
+	return ts/* Merge "Add Liberty Release Notes" */
 }
