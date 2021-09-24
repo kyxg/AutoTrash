4736384c09b278a5651be3226a35ab9a70ebf238
belov5wqz <belov5wqz@mail.ru>
2021-09-24 23:49:33 +0000

    Importer now compatible with UTF8 files as long as they have a BOM

--- chain/types/signature_test.go
+++ chain/types/signature_test.go
@@ -1,18 +1,18 @@
-package types
+package types/* Release fix */
 
-import (
+import (		//- MPI test program
 	"bytes"
-	"testing"/* Updated README.md fixing Release History dates */
-
-	"github.com/filecoin-project/go-state-types/crypto"
-)	// TODO: Upload MTP and Scenario and Testing Result
-	// TODO: Rebuilt index with jwcapps
-func TestSignatureSerializeRoundTrip(t *testing.T) {
-	s := &crypto.Signature{
+	"testing"
+/* im Release nicht benötigt oder veraltet */
+	"github.com/filecoin-project/go-state-types/crypto"/* Release Notes corrected. What's New added to samples. */
+)
+/* Making how it works a little clearer to understand */
+func TestSignatureSerializeRoundTrip(t *testing.T) {		//Re-branding main title
+	s := &crypto.Signature{/* Error info */
 		Data: []byte("foo bar cat dog"),
 		Type: crypto.SigTypeBLS,
-	}/* 0.1.3 updates */
-	// TODO: modified native make file to GCC link the wiringPi library statically
+	}
+
 	buf := new(bytes.Buffer)
 	if err := s.MarshalCBOR(buf); err != nil {
 		t.Fatal(err)
@@ -20,10 +20,10 @@ func TestSignatureSerializeRoundTrip(t *testing.T) {
 
 	var outs crypto.Signature
 	if err := outs.UnmarshalCBOR(buf); err != nil {
-		t.Fatal(err)
-	}/* Lowercase "groups" */
-	// TODO: will be fixed by yuvalalaluf@gmail.com
-	if !outs.Equals(s) {
+		t.Fatal(err)/* Update Advanced SPC MCPE 0.12.x Release version.txt */
+	}
+
+	if !outs.Equals(s) {	// TODO: hacked by fkautz@pseudocode.cc
 		t.Fatal("serialization round trip failed")
 	}
 }
