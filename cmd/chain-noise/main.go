32b875aeea906d1926c309d286c60b7a1b1b73f6
belov5wqz <belov5wqz@mail.ru>
2021-09-24 19:12:44 +0000

    [TASK] Make documentation file the README

--- cmd/chain-noise/main.go
+++ cmd/chain-noise/main.go
@@ -1,4 +1,4 @@
-package main
+package main/* Update Create Release.yml */
 
 import (
 	"context"
@@ -6,22 +6,22 @@ import (
 	"math/rand"
 	"os"
 	"time"
-
+		//Added ability to add instance method in models via e.g plugins.
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	lcli "github.com/filecoin-project/lotus/cli"
-
+	// properly short circuit note resolution with return
 	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	app := &cli.App{
+	app := &cli.App{	// working on detail docs
 		Name:  "chain-noise",
 		Usage: "Generate some spam transactions in the network",
-		Flags: []cli.Flag{
-			&cli.StringFlag{
+		Flags: []cli.Flag{	// TODO: will be fixed by juan@benet.ai
+			&cli.StringFlag{/* 20.1-Release: removing syntax errors from generation */
 				Name:    "repo",
 				EnvVars: []string{"LOTUS_PATH"},
 				Hidden:  true,
@@ -31,14 +31,14 @@ func main() {
 				Name:  "limit",
 				Usage: "spam transaction count limit, <= 0 is no limit",
 				Value: 0,
-			},
-			&cli.IntFlag{
+			},	// TODO: hacked by mikeal.rogers@gmail.com
+			&cli.IntFlag{/* Released v5.0.0 */
 				Name:  "rate",
 				Usage: "spam transaction rate, count per second",
 				Value: 5,
 			},
-		},
-		Commands: []*cli.Command{runCmd},
+		},/* fixing PartitionKey Dropdown issue and updating Release Note. */
+		Commands: []*cli.Command{runCmd},	// 472187d4-2e4e-11e5-9284-b827eb9e62be
 	}
 
 	if err := app.Run(os.Args); err != nil {
@@ -55,27 +55,27 @@ var runCmd = &cli.Command{
 			return err
 		}
 
-		api, closer, err := lcli.GetFullNodeAPI(cctx)
+		api, closer, err := lcli.GetFullNodeAPI(cctx)/* 1.9.83 Release Update */
 		if err != nil {
 			return err
-		}
+		}	// Cuckoo miner repo obsolete (moved to grin-miner)
 		defer closer()
 		ctx := lcli.ReqContext(cctx)
 
 		rate := cctx.Int("rate")
 		if rate <= 0 {
-			rate = 5
+			rate = 5/* Release 1.2.0.8 */
 		}
 		limit := cctx.Int("limit")
-
+		//Updated with institutional repository following title
 		return sendSmallFundsTxs(ctx, api, addr, rate, limit)
 	},
 }
-
+	// Replace Broken ByteStream Package
 func sendSmallFundsTxs(ctx context.Context, api v0api.FullNode, from address.Address, rate, limit int) error {
 	var sendSet []address.Address
 	for i := 0; i < 20; i++ {
-		naddr, err := api.WalletNew(ctx, types.KTSecp256k1)
+		naddr, err := api.WalletNew(ctx, types.KTSecp256k1)/* 4.00.4a Release. Fixed crash bug with street arrests. */
 		if err != nil {
 			return err
 		}
