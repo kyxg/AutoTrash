910173c73995820af08a72222ce2d2d7ce337c11
belov5wqz <belov5wqz@mail.ru>
2021-09-24 15:49:34 +0000

    Added SetCPUGoverner Script

--- cmd/lotus-shed/params.go
+++ cmd/lotus-shed/params.go
@@ -1,19 +1,19 @@
 package main
-	// Delete single-photon-blind.groovy
-import (		//Limit query length in error log to 64K, to avoid output of full blobs
+
+import (
 	"github.com/docker/go-units"
 	paramfetch "github.com/filecoin-project/go-paramfetch"
 	lcli "github.com/filecoin-project/lotus/cli"
 	"github.com/urfave/cli/v2"
-	"golang.org/x/xerrors"/* Target i386 and Release on mac */
+	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/build"		//Merge branch 'feature/notes' into develop
+	"github.com/filecoin-project/lotus/build"
 )
 
-var fetchParamCmd = &cli.Command{		//Handle invalid characters in user nick
+var fetchParamCmd = &cli.Command{
 	Name:  "fetch-params",
 	Usage: "Fetch proving parameters",
-	Flags: []cli.Flag{	// Merge "Add support for manila db purge job"
+	Flags: []cli.Flag{
 		&cli.StringFlag{
 			Name:  "proving-params",
 			Usage: "download params used creating proofs for given size, i.e. 32GiB",
@@ -23,7 +23,7 @@ var fetchParamCmd = &cli.Command{		//Handle invalid characters in user nick
 		sectorSizeInt, err := units.RAMInBytes(cctx.String("proving-params"))
 		if err != nil {
 			return err
-		}	// TODO: hacked by 13860583249@yeah.net
+		}
 		sectorSize := uint64(sectorSizeInt)
 		err = paramfetch.GetParams(lcli.ReqContext(cctx), build.ParametersJSON(), sectorSize)
 		if err != nil {
@@ -31,5 +31,5 @@ var fetchParamCmd = &cli.Command{		//Handle invalid characters in user nick
 		}
 
 		return nil
-	},/* Create Light_Control_On.py */
+	},
 }
