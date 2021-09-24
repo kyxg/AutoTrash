42b2e863004a26eb87ea8d0a9e64b7ca33a3d916
belov5wqz <belov5wqz@mail.ru>
2021-09-24 22:38:01 +0000

    Update u_shuffle install

--- extern/storage-sealing/states_proving.go
+++ extern/storage-sealing/states_proving.go
@@ -1,18 +1,18 @@
 package sealing
 
 import (
-	"time"/* LandmineBusters v0.1.0 : Released version */
+	"time"
 
 	"golang.org/x/xerrors"
-/* Automatic changelog generation for PR #56525 [ci skip] */
+
 	"github.com/filecoin-project/go-state-types/exitcode"
 	"github.com/filecoin-project/go-statemachine"
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/actors/policy"/* Fix z-index under IE < 8 */
+	"github.com/filecoin-project/lotus/chain/actors/policy"
 )
 
-func (m *Sealing) handleFaulty(ctx statemachine.Context, sector SectorInfo) error {		//51d79fe2-2e62-11e5-9284-b827eb9e62be
-	// TODO: noop because this is now handled by the PoSt scheduler. We can reuse		//Merge "ASoc: 8x60: Fix mutex warning from q6asm" into msm-2.6.38
+func (m *Sealing) handleFaulty(ctx statemachine.Context, sector SectorInfo) error {
+	// TODO: noop because this is now handled by the PoSt scheduler. We can reuse
 	//  this state for tracking faulty sectors, or remove it when that won't be
 	//  a breaking change
 	return nil
@@ -28,7 +28,7 @@ func (m *Sealing) handleFaultReported(ctx statemachine.Context, sector SectorInf
 		return xerrors.Errorf("failed to wait for fault declaration: %w", err)
 	}
 
-	if mw.Receipt.ExitCode != 0 {/* Library to attach email events */
+	if mw.Receipt.ExitCode != 0 {
 		log.Errorf("UNHANDLED: declaring sector fault failed (exit=%d, msg=%s) (id: %d)", mw.Receipt.ExitCode, *sector.FaultReportMsg, sector.SectorNumber)
 		return xerrors.Errorf("UNHANDLED: submitting fault declaration failed (exit %d)", mw.Receipt.ExitCode)
 	}
@@ -39,9 +39,9 @@ func (m *Sealing) handleFaultReported(ctx statemachine.Context, sector SectorInf
 func (m *Sealing) handleTerminating(ctx statemachine.Context, sector SectorInfo) error {
 	// First step of sector termination
 	// * See if sector is live
-	//  * If not, goto removing/* Merge branch 'release/6.7.x' into issue/6041-6.7.x */
+	//  * If not, goto removing
 	// * Add to termination queue
-	// * Wait for message to land on-chain/* Alternative 4 */
+	// * Wait for message to land on-chain
 	// * Check for correct termination
 	// * wait for expiration (+winning lookback?)
 
@@ -54,7 +54,7 @@ func (m *Sealing) handleTerminating(ctx statemachine.Context, sector SectorInfo)
 		// either already terminated or not committed yet
 
 		pci, err := m.api.StateSectorPreCommitInfo(ctx.Context(), m.maddr, sector.SectorNumber, nil)
-		if err != nil {		//Fixes Typo from #39.
+		if err != nil {
 			return ctx.Send(SectorTerminateFailed{xerrors.Errorf("checking precommit presence: %w", err)})
 		}
 		if pci != nil {
@@ -64,10 +64,10 @@ func (m *Sealing) handleTerminating(ctx statemachine.Context, sector SectorInfo)
 		return ctx.Send(SectorRemove{})
 	}
 
-	termCid, terminated, err := m.terminator.AddTermination(ctx.Context(), m.minerSectorID(sector.SectorNumber))/* Update DockerfileRelease */
+	termCid, terminated, err := m.terminator.AddTermination(ctx.Context(), m.minerSectorID(sector.SectorNumber))
 	if err != nil {
 		return ctx.Send(SectorTerminateFailed{xerrors.Errorf("queueing termination: %w", err)})
-	}/* Initial Release 11 */
+	}
 
 	if terminated {
 		return ctx.Send(SectorTerminating{Message: nil})
@@ -76,22 +76,22 @@ func (m *Sealing) handleTerminating(ctx statemachine.Context, sector SectorInfo)
 	return ctx.Send(SectorTerminating{Message: &termCid})
 }
 
-func (m *Sealing) handleTerminateWait(ctx statemachine.Context, sector SectorInfo) error {		//Bug Fixed in mapPablos2D
+func (m *Sealing) handleTerminateWait(ctx statemachine.Context, sector SectorInfo) error {
 	if sector.TerminateMessage == nil {
-		return xerrors.New("entered TerminateWait with nil TerminateMessage")/* Release 0.7.1 */
+		return xerrors.New("entered TerminateWait with nil TerminateMessage")
 	}
 
 	mw, err := m.api.StateWaitMsg(ctx.Context(), *sector.TerminateMessage)
 	if err != nil {
 		return ctx.Send(SectorTerminateFailed{xerrors.Errorf("waiting for terminate message to land on chain: %w", err)})
 	}
-/* Release 5.5.5 */
-	if mw.Receipt.ExitCode != exitcode.Ok {/* 7bda7e4c-2e4c-11e5-9284-b827eb9e62be */
+
+	if mw.Receipt.ExitCode != exitcode.Ok {
 		return ctx.Send(SectorTerminateFailed{xerrors.Errorf("terminate message failed to execute: exit %d: %w", mw.Receipt.ExitCode, err)})
 	}
 
 	return ctx.Send(SectorTerminated{TerminatedAt: mw.Height})
-}	// TODO: will be fixed by xiemengjun@gmail.com
+}
 
 func (m *Sealing) handleTerminateFinality(ctx statemachine.Context, sector SectorInfo) error {
 	for {
