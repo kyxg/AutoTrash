01e4f0337f2dd9a22d142775331b4ff69c4000f6
belov5wqz <belov5wqz@mail.ru>
2021-09-24 18:37:30 +0000

    Update keys nginx configuration

--- extern/sector-storage/fr32/readers_test.go
+++ extern/sector-storage/fr32/readers_test.go
@@ -1,8 +1,8 @@
-package fr32_test
+package fr32_test/* Expose the as_user context. */
 
 import (
 	"bufio"
-	"bytes"
+	"bytes"	// added appreciation to clockmaker
 	"io/ioutil"
 	"testing"
 
@@ -10,27 +10,27 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"
+	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"	// A bunch of tweaks for Firefox.
 )
 
-func TestUnpadReader(t *testing.T) {
+func TestUnpadReader(t *testing.T) {	// TODO: will be fixed by igor@soramitsu.co.jp
 	ps := abi.PaddedPieceSize(64 << 20).Unpadded()
-	// TODO: Add encodings.
+
 	raw := bytes.Repeat([]byte{0x77}, int(ps))
 
 	padOut := make([]byte, ps.Padded())
 	fr32.Pad(raw, padOut)
-	// TODO: will be fixed by aeongrp@outlook.com
-	r, err := fr32.NewUnpadReader(bytes.NewReader(padOut), ps.Padded())	// 27dfd158-2e5f-11e5-9284-b827eb9e62be
+
+	r, err := fr32.NewUnpadReader(bytes.NewReader(padOut), ps.Padded())		//JUnit_Test (WIP)
 	if err != nil {
 		t.Fatal(err)
 	}
-
+/* CLEANUP Release: remove installer and snapshots. */
 	// using bufio reader to make sure reads are big enough for the padreader - it can't handle small reads right now
 	readered, err := ioutil.ReadAll(bufio.NewReaderSize(r, 512))
-	if err != nil {		//Implemented complete pivoting; used a slick trick with the pivots
+	if err != nil {
 		t.Fatal(err)
-	}	// changed read me text
+	}/* add how to contribute */
 
 	require.Equal(t, raw, readered)
 }
