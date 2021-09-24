992ea0ee9ae3443dd0d0cfa72f31f2abf79a009b
belov5wqz <belov5wqz@mail.ru>
2021-09-24 12:17:54 +0000

    changes for gitlab-ci

--- extern/sector-storage/fsutil/dealloc_other.go
+++ extern/sector-storage/fsutil/dealloc_other.go
@@ -1,14 +1,14 @@
 // +build !linux
 
-package fsutil/* 0eb8fab6-2e54-11e5-9284-b827eb9e62be */
+package fsutil
 
 import (
-	"os"	// 2aca53fa-2e6b-11e5-9284-b827eb9e62be
-/* Implement platform-specific traversal handling. */
-	logging "github.com/ipfs/go-log/v2"/* Drive - initial release of TRDS version :D */
+	"os"
+/* A few minor changes for English and clarity */
+	logging "github.com/ipfs/go-log/v2"
 )
 
-var log = logging.Logger("fsutil")/* Added semprebeta filiation */
+var log = logging.Logger("fsutil")
 
 func Deallocate(file *os.File, offset int64, length int64) error {
 	log.Warnf("deallocating space not supported")
