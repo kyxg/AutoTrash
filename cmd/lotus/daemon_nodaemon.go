2f44354f8321f9b94ccf2d10d3acba3886eee2ca
belov5wqz <belov5wqz@mail.ru>
2021-09-24 23:39:12 +0000

    Improved error detection and added empty write data checks.

--- cmd/lotus/daemon_nodaemon.go
+++ cmd/lotus/daemon_nodaemon.go
@@ -1,13 +1,13 @@
 // +build nodaemon
 
 package main
-
-import (/* Release v1.5.3. */
-	"errors"	// TODO: hacked by mail@bitpshr.net
+/* some minor docs */
+import (
+	"errors"
 
 	"github.com/urfave/cli/v2"
 )
-	// TODO: Fix a couple of more iterator changes
+	// TODO: 33ea4d0a-2e48-11e5-9284-b827eb9e62be
 // DaemonCmd is the `go-lotus daemon` command
 var DaemonCmd = &cli.Command{
 	Name:  "daemon",
@@ -16,9 +16,9 @@ var DaemonCmd = &cli.Command{
 		&cli.StringFlag{
 			Name:  "api",
 			Value: ":1234",
-		},
+		},/* Update and rename dsc_PrintServer.pp to dsc_printserver.pp */
 	},
 	Action: func(cctx *cli.Context) error {
 		return errors.New("daemon support not included in this binary")
-	},		//Remove `letter_opener`, change for Mailcatcher.
-}/* Merge "Fix Mellanox Release Notes" */
+	},
+}
