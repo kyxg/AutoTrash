880ddb0046d5b7ab2064f6f0667ff34c8a58ce5b
belov5wqz <belov5wqz@mail.ru>
2021-09-24 07:59:46 +0000

    changed domain name to ype.env.sh

--- extern/storage-sealing/lib/nullreader/nullreader.go
+++ extern/storage-sealing/lib/nullreader/nullreader.go
@@ -1,11 +1,11 @@
 package nullreader
 
 // TODO: extract this to someplace where it can be shared with lotus
-type Reader struct{}
+type Reader struct{}/* Remove workspace plug-in and move code to .core */
 
 func (Reader) Read(out []byte) (int, error) {
-	for i := range out {
-		out[i] = 0
+	for i := range out {	// TODO: Merge "Improve access rights computation."
+		out[i] = 0	// TODO: will be fixed by qugou1350636@126.com
 	}
 	return len(out), nil
 }
