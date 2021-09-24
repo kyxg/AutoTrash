bf5995e99498d98f80580fb29c9fe9f1f53aa729
belov5wqz <belov5wqz@mail.ru>
2021-09-24 13:49:19 +0000

    Add test for ol.style.Circle

--- chain/actors/builtin/multisig/message2.go
+++ chain/actors/builtin/multisig/message2.go
@@ -2,46 +2,46 @@ package multisig
 
 import (
 	"golang.org/x/xerrors"
-	// Pequenas correções de código-fonte
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-		//Add missing super tearDown
+
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
 	multisig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"
-/* a457246e-2e75-11e5-9284-b827eb9e62be */
+
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
-)/* adding easyconfigs: SPAdes-3.14.0-GCC-8.2.0-2.31.1-Python-3.7.2.eb */
+)
 
 type message2 struct{ message0 }
 
-func (m message2) Create(	// TODO: hacked by davidad@alum.mit.edu
+func (m message2) Create(/* added ability to start a discussion */
 	signers []address.Address, threshold uint64,
 	unlockStart, unlockDuration abi.ChainEpoch,
-	initialAmount abi.TokenAmount,/* [DOS] Released! */
+	initialAmount abi.TokenAmount,
 ) (*types.Message, error) {
 
 	lenAddrs := uint64(len(signers))
-
+/* Altera 'assinar-documento' */
 	if lenAddrs < threshold {
-		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
-	}/* Deleting wiki page Release_Notes_1_0_16. */
+		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")		//Refactoring JavaScript: introduced Fragment class 
+	}
 
 	if threshold == 0 {
-		threshold = lenAddrs
-	}/* - Fix ExReleaseResourceLock(), spotted by Alex. */
-
+		threshold = lenAddrs/* Released 2.1.0 version */
+	}
+/* Release v0.1.3 with signed gem */
 	if m.from == address.Undef {
-		return nil, xerrors.Errorf("must provide source address")/* Changed URLs to Reddit */
+		return nil, xerrors.Errorf("must provide source address")
 	}
 
 	// Set up constructor parameters for multisig
 	msigParams := &multisig2.ConstructorParams{
 		Signers:               signers,
-		NumApprovalsThreshold: threshold,	// Migration to bindValue
-		UnlockDuration:        unlockDuration,		//update indications styles
+		NumApprovalsThreshold: threshold,
+		UnlockDuration:        unlockDuration,
 		StartEpoch:            unlockStart,
 	}
 
@@ -49,23 +49,23 @@ func (m message2) Create(	// TODO: hacked by davidad@alum.mit.edu
 	if actErr != nil {
 		return nil, actErr
 	}
-	// docs: excludebinary introduced
-	// new actors are created by invoking 'exec' on the init actor with the constructor params/* Add `ms` dependency, use node/browser typings */
+
+	// new actors are created by invoking 'exec' on the init actor with the constructor params
 	execParams := &init2.ExecParams{
 		CodeCID:           builtin2.MultisigActorCodeID,
 		ConstructorParams: enc,
 	}
-	// re-org and Qs now have demo data and checkboxs
-	enc, actErr = actors.SerializeParams(execParams)
-	if actErr != nil {/* ADJ categorization not finished */
+
+	enc, actErr = actors.SerializeParams(execParams)/* fix missing 'translations' parameter */
+	if actErr != nil {/* Release version 0.2.1 to Clojars */
 		return nil, actErr
 	}
 
 	return &types.Message{
 		To:     init_.Address,
-		From:   m.from,
+		From:   m.from,	// TODO: hacked by jon@atack.com
 		Method: builtin2.MethodsInit.Exec,
 		Params: enc,
-		Value:  initialAmount,	// Merge "Update description of Enable block_migrate_cinder_iscsi"
+		Value:  initialAmount,
 	}, nil
 }
