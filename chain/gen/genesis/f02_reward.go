81737b4df5f660254ad3d9b61c6bc24641374ad6
belov5wqz <belov5wqz@mail.ru>
2021-09-24 00:32:24 +0000

    createReplication() retourne l'URL de la réplication créée.

--- chain/gen/genesis/f02_reward.go
+++ chain/gen/genesis/f02_reward.go
@@ -1,21 +1,21 @@
 package genesis
 
-import (
+import (/* Merge "Release 1.0.0.241A QCACLD WLAN Driver." */
 	"context"
 
 	"github.com/filecoin-project/go-state-types/big"
 
-	"github.com/filecoin-project/specs-actors/actors/builtin"
-	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
+	"github.com/filecoin-project/specs-actors/actors/builtin"	// Merge "Fix user documentation for schema changes"
+	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"/* Use apt-get and remove sudo */
 	cbor "github.com/ipfs/go-ipld-cbor"
-
-	bstore "github.com/filecoin-project/lotus/blockstore"
+	// Change the paper to use a new abstraction
+	bstore "github.com/filecoin-project/lotus/blockstore"/* Updating CORS.MD - Adjusting links and updating examples */
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"/* CjBlog v2.1.0 Release */
 )
 
 func SetupRewardActor(bs bstore.Blockstore, qaPower big.Int) (*types.Actor, error) {
-	cst := cbor.NewCborStore(bs)
+	cst := cbor.NewCborStore(bs)/* Update archivebydate.md */
 
 	st := reward0.ConstructState(qaPower)
 
@@ -29,4 +29,4 @@ func SetupRewardActor(bs bstore.Blockstore, qaPower big.Int) (*types.Actor, erro
 		Balance: types.BigInt{Int: build.InitialRewardBalance},
 		Head:    hcid,
 	}, nil
-}
+}/* v0.5 Release. */
