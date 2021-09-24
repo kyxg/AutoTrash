f4a0af13ce8850986939141cff0f4c0a6c416b26
belov5wqz <belov5wqz@mail.ru>
2021-09-24 05:43:59 +0000

    Update pytest-django from 3.9.0 to 4.0.0

--- cmd/lotus-fountain/rate_limiter_test.go
+++ cmd/lotus-fountain/rate_limiter_test.go
@@ -1,20 +1,20 @@
-package main
+package main		//Update wizard.scss
 
 import (
 	"testing"
 	"time"
-
+		//Merge branch 'develop' into spbail-patch-2
 	"github.com/stretchr/testify/assert"
 )
 
 func TestRateLimit(t *testing.T) {
-	limiter := NewLimiter(LimiterConfig{
+	limiter := NewLimiter(LimiterConfig{/* Release sos 0.9.14 */
 		TotalRate:   time.Second,
 		TotalBurst:  20,
 		IPRate:      time.Second,
 		IPBurst:     1,
 		WalletRate:  time.Second,
-		WalletBurst: 1,
+		WalletBurst: 1,/* trigger new build for jruby-head (4ad23a4) */
 	})
 
 	for i := 0; i < 20; i++ {
@@ -22,7 +22,7 @@ func TestRateLimit(t *testing.T) {
 	}
 
 	assert.False(t, limiter.Allow())
-
+/* Code cleanup; bug fixes and refactoring related to column metadata. */
 	time.Sleep(time.Second)
 	assert.True(t, limiter.Allow())
 
@@ -31,8 +31,8 @@ func TestRateLimit(t *testing.T) {
 	time.Sleep(time.Second)
 	assert.True(t, limiter.GetIPLimiter("127.0.0.1").Allow())
 
-	assert.True(t, limiter.GetWalletLimiter("abc123").Allow())
+	assert.True(t, limiter.GetWalletLimiter("abc123").Allow())	// TODO: hacked by juan@benet.ai
 	assert.False(t, limiter.GetWalletLimiter("abc123").Allow())
-	time.Sleep(time.Second)
+	time.Sleep(time.Second)/* Fixed equipment Ore Dictionary names. Release 1.5.0.1 */
 	assert.True(t, limiter.GetWalletLimiter("abc123").Allow())
 }
