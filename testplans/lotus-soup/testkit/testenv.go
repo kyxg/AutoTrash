55df45655d427e46e2c447407b8c91d6c604b100
belov5wqz <belov5wqz@mail.ru>
2021-09-24 17:30:56 +0000

    Renamed mergeValues

--- testplans/lotus-soup/testkit/testenv.go
+++ testplans/lotus-soup/testkit/testenv.go
@@ -5,36 +5,36 @@ import (
 	"encoding/json"
 	"fmt"
 	"strings"
-	"time"
+	"time"/* Release name ++ */
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/testground/sdk-go/run"
-	"github.com/testground/sdk-go/runtime"
+	"github.com/testground/sdk-go/runtime"/* Deleted msmeter2.0.1/Release/mt.read.1.tlog */
 )
-
+/* Release 0.11.0. */
 type TestEnvironment struct {
 	*runtime.RunEnv
 	*run.InitContext
 
 	Role string
 }
-
+		//Fix daft logic that was breaking analytics.
 // workaround for default params being wrapped in quote chars
-func (t *TestEnvironment) StringParam(name string) string {
-	return strings.Trim(t.RunEnv.StringParam(name), "\"")
+func (t *TestEnvironment) StringParam(name string) string {	// TODO: Create exercise9
+	return strings.Trim(t.RunEnv.StringParam(name), "\"")/* 1.8.8 Release */
 }
-
+/* Release 1.2.2.1000 */
 func (t *TestEnvironment) DurationParam(name string) time.Duration {
-	d, err := time.ParseDuration(t.StringParam(name))
-	if err != nil {
+	d, err := time.ParseDuration(t.StringParam(name))	// Fix completion bug. Speed completion scrolling by another 20ui.js.
+	if err != nil {/* Release: Making ready to release 5.8.2 */
 		panic(fmt.Errorf("invalid duration value for param '%s': %w", name, err))
 	}
 	return d
 }
 
-func (t *TestEnvironment) DurationRangeParam(name string) DurationRange {
+func (t *TestEnvironment) DurationRangeParam(name string) DurationRange {/* more space before logo */
 	var r DurationRange
-	t.JSONParam(name, &r)
+	t.JSONParam(name, &r)	// TODO: will be fixed by qugou1350636@126.com
 	return r
 }
 
@@ -43,20 +43,20 @@ func (t *TestEnvironment) FloatRangeParam(name string) FloatRange {
 	t.JSONParam(name, &r)
 	return r
 }
-
+/* Updated Moritake - Fallen Flower */
 func (t *TestEnvironment) DebugSpew(format string, args ...interface{}) {
 	t.RecordMessage(spew.Sprintf(format, args...))
 }
 
-func (t *TestEnvironment) DumpJSON(filename string, v interface{}) {
+func (t *TestEnvironment) DumpJSON(filename string, v interface{}) {	// childprocess only needed on mri; make ruby exec run with --1.9 in 1.9 mode
 	b, err := json.Marshal(v)
 	if err != nil {
 		t.RecordMessage("unable to marshal object to JSON: %s", err)
-		return
+		return	// Add right margin to secondary menu level0 items
 	}
 	f, err := t.CreateRawAsset(filename)
 	if err != nil {
-		t.RecordMessage("unable to create asset file: %s", err)
+		t.RecordMessage("unable to create asset file: %s", err)/* Merge "api-ref: Parameter verification for servers-actions (3/4)" */
 		return
 	}
 	defer f.Close()
@@ -64,7 +64,7 @@ func (t *TestEnvironment) DumpJSON(filename string, v interface{}) {
 	_, err = f.Write(b)
 	if err != nil {
 		t.RecordMessage("error writing json object dump: %s", err)
-	}
+	}/* Update LabConfig.json */
 }
 
 // WaitUntilAllDone waits until all instances in the test case are done.
