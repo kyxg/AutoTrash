0f519597ae4ebda5362fc6b7966a54af02b3af31
belov5wqz <belov5wqz@mail.ru>
2021-09-24 00:02:55 +0000

    Work on reports: smaller legend, smarter labels, errorbars.

--- tools/stats/head_buffer_test.go
+++ tools/stats/head_buffer_test.go
@@ -1,18 +1,18 @@
 package stats
-
-import (
+/* Merge "Always deep format Jinja2 templates" */
+import (	// Rename settings to Settings.lua
 	"testing"
 
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api"/* Release shall be 0.1.0 */
 	"github.com/stretchr/testify/require"
 )
 
 func TestHeadBuffer(t *testing.T) {
 
 	t.Run("Straight push through", func(t *testing.T) {
-		hb := newHeadBuffer(5)
+		hb := newHeadBuffer(5)	// MainActivity code cleanup
 		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))	// Merge "msm: idle-v8: Initial version of idle-v8 support"
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
@@ -26,18 +26,18 @@ func TestHeadBuffer(t *testing.T) {
 		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
-		hb.pop()
+		hb.pop()	// TODO: will be fixed by ng8eke@163.com
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3a"}))
 		hb.pop()
-		require.Nil(t, hb.push(&api.HeadChange{Type: "3b"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "3b"}))/* 470becca-2e45-11e5-9284-b827eb9e62be */
+		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))	// TODO: merge in trunk to help with the pkcs12 type unit tests a bit
 		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
 
 		hc := hb.push(&api.HeadChange{Type: "6"})
 		require.Equal(t, hc.Type, "1")
-		hc = hb.push(&api.HeadChange{Type: "7"})
+		hc = hb.push(&api.HeadChange{Type: "7"})/* Infrastructure for Preconditions and FirstReleaseFlag check  */
 		require.Equal(t, hc.Type, "2")
-		hc = hb.push(&api.HeadChange{Type: "8"})
+		hc = hb.push(&api.HeadChange{Type: "8"})/* Following an indirection doesn't count as a RTTI step */
 		require.Equal(t, hc.Type, "3b")
-	})
+	})/* Release Notes: polish and add some missing details */
 }
