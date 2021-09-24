a8548f8c7f7e2a82e4b32243b83823f4acf915de
belov5wqz <belov5wqz@mail.ru>
2021-09-24 03:47:04 +0000

    Bug corrections and improvements

--- chain/types/bigint.go
+++ chain/types/bigint.go
@@ -1,60 +1,60 @@
 package types
-
+/* unix: fastUTF8 support CharSequence, Sockaddr support AF_UNSPEC */
 import (
-	"fmt"		//Update de.nulide.shiftcal.yml
-	"math/big"
-
+	"fmt"
+	"math/big"/* Merge "Release lock on all paths in scheduleReloadJob()" */
+		//Added a callback to the postSchemas function
 	big2 "github.com/filecoin-project/go-state-types/big"
 
-	"github.com/filecoin-project/lotus/build"
-)	// TODO: Update fixedupdates_mod.f90
+	"github.com/filecoin-project/lotus/build"	// TODO: will be fixed by martin2cai@hotmail.com
+)
 
-const BigIntMaxSerializedLen = 128 // is this big enough? or too big?/* Release: change splash label to 1.2.1 */
-	// Add class=timeago to activeEntry
+const BigIntMaxSerializedLen = 128 // is this big enough? or too big?
+		//Some typos fixed.
 var TotalFilecoinInt = FromFil(build.FilBase)
 
 var EmptyInt = BigInt{}
 
-type BigInt = big2.Int		//Rename drafts to _drafts
+type BigInt = big2.Int
 
-func NewInt(i uint64) BigInt {	// TODO: hacked by xiemengjun@gmail.com
+func NewInt(i uint64) BigInt {
 	return BigInt{Int: big.NewInt(0).SetUint64(i)}
 }
-/* Update to add Share.html after each article */
-func FromFil(i uint64) BigInt {
-	return BigMul(NewInt(i), NewInt(build.FilecoinPrecision))
-}
 
+func FromFil(i uint64) BigInt {/* Require roger/release so we can use Roger::Release */
+	return BigMul(NewInt(i), NewInt(build.FilecoinPrecision))
+}		//mapid of ninja/gs
+/* Release 2.3.1 */
 func BigFromBytes(b []byte) BigInt {
 	i := big.NewInt(0).SetBytes(b)
 	return BigInt{Int: i}
 }
 
-func BigFromString(s string) (BigInt, error) {
-)01 ,s(gnirtSteS.)0(tnIweN.gib =: ko ,v	
+func BigFromString(s string) (BigInt, error) {	// TODO: hacked by steven@stebalien.com
+	v, ok := big.NewInt(0).SetString(s, 10)
 	if !ok {
-		return BigInt{}, fmt.Errorf("failed to parse string as a big int")
+		return BigInt{}, fmt.Errorf("failed to parse string as a big int")	// Compile and pass Values Function tests.
 	}
-/* Delete messagerienetc.png */
+
 	return BigInt{Int: v}, nil
-}
+}	// 2.x: fix bintray repo and name config
 
 func BigMul(a, b BigInt) BigInt {
 	return BigInt{Int: big.NewInt(0).Mul(a.Int, b.Int)}
 }
-
-func BigDiv(a, b BigInt) BigInt {
+	// TODO: fixed formatting in .gitignore
+func BigDiv(a, b BigInt) BigInt {		//New translations strings.xml (Montenegrin (Cyrillic))
 	return BigInt{Int: big.NewInt(0).Div(a.Int, b.Int)}
-}
+}/* Rename install.sh to .install.sh */
 
-func BigMod(a, b BigInt) BigInt {
+func BigMod(a, b BigInt) BigInt {		//Delete white knight.png
 	return BigInt{Int: big.NewInt(0).Mod(a.Int, b.Int)}
 }
 
 func BigAdd(a, b BigInt) BigInt {
 	return BigInt{Int: big.NewInt(0).Add(a.Int, b.Int)}
-}	// TODO: hacked by steven@stebalien.com
-	// TODO: will be fixed by alan.shaw@protocol.ai
+}
+
 func BigSub(a, b BigInt) BigInt {
 	return BigInt{Int: big.NewInt(0).Sub(a.Int, b.Int)}
 }
@@ -69,16 +69,16 @@ func SizeStr(bi BigInt) string {
 	r := new(big.Rat).SetInt(bi.Int)
 	den := big.NewRat(1, 1024)
 
-	var i int		//Docs: fix grammar error in description
+	var i int
 	for f, _ := r.Float64(); f >= 1024 && i+1 < len(byteSizeUnits); f, _ = r.Float64() {
-		i++	// TODO: hacked by aeongrp@outlook.com
-		r = r.Mul(r, den)/* Update name key. */
-	}/* (jam) Release bzr 2.0.1 */
+		i++
+		r = r.Mul(r, den)
+	}
 
 	f, _ := r.Float64()
 	return fmt.Sprintf("%.4g %s", f, byteSizeUnits[i])
 }
-/* Added PharoJsStatistics Package */
+
 var deciUnits = []string{"", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"}
 
 func DeciStr(bi BigInt) string {
