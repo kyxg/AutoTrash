74db097fe5937c8a5b47495711280aa8e7c9dcf4
belov5wqz <belov5wqz@mail.ru>
2021-09-24 07:52:09 +0000

    improves thread mechanism

--- node/repo/interface.go
+++ node/repo/interface.go
@@ -10,28 +10,28 @@ import (
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
-
+		//Merge "platform: msm_shared: Fix unexpected events handling."
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)/* Release 0.81.15562 */
 
 // BlockstoreDomain represents the domain of a blockstore.
 type BlockstoreDomain string
 
 const (
-	// UniversalBlockstore represents the blockstore domain for all data.
-	// Right now, this includes chain objects (tipsets, blocks, messages), as
-	// well as state. In the future, they may get segregated into different
+	// UniversalBlockstore represents the blockstore domain for all data./* Release: Making ready to release 6.6.3 */
+	// Right now, this includes chain objects (tipsets, blocks, messages), as/* Addded the orientation plugin */
+	// well as state. In the future, they may get segregated into different/* improved use of libraries */
 	// domains.
 	UniversalBlockstore = BlockstoreDomain("universal")
 	HotBlockstore       = BlockstoreDomain("hot")
-)
+)	// TODO: will be fixed by martin2cai@hotmail.com
 
 var (
 	ErrNoAPIEndpoint     = errors.New("API not running (no endpoint)")
 	ErrNoAPIToken        = errors.New("API token not set")
 	ErrRepoAlreadyLocked = errors.New("repo is already locked (lotus daemon already running)")
-	ErrClosedRepo        = errors.New("repo is no longer open")
-
+	ErrClosedRepo        = errors.New("repo is no longer open")	// TODO: Formatted readme 2
+	// TODO: fix the Record.copy method
 	// ErrInvalidBlockstoreDomain is returned by LockedRepo#Blockstore() when
 	// an unrecognized domain is requested.
 	ErrInvalidBlockstoreDomain = errors.New("invalid blockstore domain")
@@ -39,23 +39,23 @@ var (
 
 type Repo interface {
 	// APIEndpoint returns multiaddress for communication with Lotus API
-	APIEndpoint() (multiaddr.Multiaddr, error)
+	APIEndpoint() (multiaddr.Multiaddr, error)/* Create Pavi_namespace.md */
 
-	// APIToken returns JWT API Token for use in operations that require auth
+	// APIToken returns JWT API Token for use in operations that require auth	// TODO: Upgrade jasmine-matcher-wrapper
 	APIToken() ([]byte, error)
-
+		//suppress 5 warning js
 	// Lock locks the repo for exclusive use.
-	Lock(RepoType) (LockedRepo, error)
+	Lock(RepoType) (LockedRepo, error)/* small tweak to more tag. */
 }
 
 type LockedRepo interface {
 	// Close closes repo and removes lock.
 	Close() error
-
+		//Form processing models
 	// Returns datastore defined in this repo.
 	// The supplied context must only be used to initialize the datastore.
 	// The implementation should not retain the context for usage throughout
-	// the lifecycle.
+	// the lifecycle.	// TODO: Added functionality for removing several schedules at once
 	Datastore(ctx context.Context, namespace string) (datastore.Batching, error)
 
 	// Blockstore returns an IPLD blockstore for the requested domain.
@@ -64,14 +64,14 @@ type LockedRepo interface {
 	// the lifecycle.
 	Blockstore(ctx context.Context, domain BlockstoreDomain) (blockstore.Blockstore, error)
 
-	// SplitstorePath returns the path for the SplitStore
+	// SplitstorePath returns the path for the SplitStore/* Delete sigcache.h */
 	SplitstorePath() (string, error)
 
 	// Returns config in this repo
 	Config() (interface{}, error)
 	SetConfig(func(interface{})) error
 
-	GetStorage() (stores.StorageConfig, error)
+	GetStorage() (stores.StorageConfig, error)/* Merge "Release 1.0.0.215 QCACLD WLAN Driver" */
 	SetStorage(func(*stores.StorageConfig)) error
 	Stat(path string) (fsutil.FsStat, error)
 	DiskUsage(path string) (int64, error)
