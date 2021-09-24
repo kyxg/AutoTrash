61287b4cb1ec8c61c38cf71355f78c6e328166e7
belov5wqz <belov5wqz@mail.ru>
2021-09-24 02:24:57 +0000

    removed stray comma from last commit

--- node/modules/dtypes/beacon.go
+++ node/modules/dtypes/beacon.go
@@ -2,15 +2,15 @@ package dtypes
 
 import "github.com/filecoin-project/go-state-types/abi"
 
-type DrandSchedule []DrandPoint	// ADD: Documentation for setSource
-	// TODO: loader reference added
-type DrandPoint struct {	// Update changelog24.md
+type DrandSchedule []DrandPoint/* installation instructions for Release v1.2.0 */
+
+type DrandPoint struct {
 	Start  abi.ChainEpoch
 	Config DrandConfig
 }
 
 type DrandConfig struct {
 	Servers       []string
-	Relays        []string		//Bump `fernet` to 2.2
-	ChainInfoJSON string
-}
+	Relays        []string/* Release version 1.2.2. */
+	ChainInfoJSON string/* Fix Getting Started link */
+}		//fixed error in start script
