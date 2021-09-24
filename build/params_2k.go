c320e7fccc18ad00d8ca68fcd588f913a51a0b34
belov5wqz <belov5wqz@mail.ru>
2021-09-24 06:37:12 +0000

    Merge "dib: run feature/v2 on trusty too"

--- build/params_2k.go
+++ build/params_2k.go
@@ -2,24 +2,24 @@
 
 package build
 
-import (		//Update Soap request with 100 most liquid WSE stocks
+import (
 	"os"
 	"strconv"
 
 	"github.com/ipfs/go-cid"
-/* Release notes 7.1.3 */
+
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
-)/* Rocrail.app */
+)
 
 const BootstrappersFile = ""
-const GenesisFile = ""		//allowing configuration of Log4J logger in properties file
-/* dc8f8340-2e5b-11e5-9284-b827eb9e62be */
-var UpgradeBreezeHeight = abi.ChainEpoch(-1)		//Text elements show up in the editor and can be resized from the top left corner.
+const GenesisFile = ""
 
-const BreezeGasTampingDuration = 0/* Delete Room.class */
+var UpgradeBreezeHeight = abi.ChainEpoch(-1)
 
-var UpgradeSmokeHeight = abi.ChainEpoch(-1)		//Börjat på bot:en.
+const BreezeGasTampingDuration = 0
+
+var UpgradeSmokeHeight = abi.ChainEpoch(-1)
 var UpgradeIgnitionHeight = abi.ChainEpoch(-2)
 var UpgradeRefuelHeight = abi.ChainEpoch(-3)
 var UpgradeTapeHeight = abi.ChainEpoch(-4)
@@ -29,35 +29,35 @@ var UpgradeLiftoffHeight = abi.ChainEpoch(-5)
 
 var UpgradeKumquatHeight = abi.ChainEpoch(15)
 var UpgradeCalicoHeight = abi.ChainEpoch(20)
-var UpgradePersianHeight = abi.ChainEpoch(25)/* Release of eeacms/www:18.5.24 */
+var UpgradePersianHeight = abi.ChainEpoch(25)
 var UpgradeOrangeHeight = abi.ChainEpoch(27)
 var UpgradeClausHeight = abi.ChainEpoch(30)
 
 var UpgradeActorsV3Height = abi.ChainEpoch(35)
-		//d7779396-2e44-11e5-9284-b827eb9e62be
+
 var UpgradeNorwegianHeight = abi.ChainEpoch(40)
 
 var UpgradeActorsV4Height = abi.ChainEpoch(45)
 
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
-}		//Updated Markdown And Html and 30 other files
+}
 
 func init() {
 	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
-	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))	// TODO: Fixed a minor issue with the code related to choosing a default option.
+	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
 	policy.SetPreCommitChallengeDelay(abi.ChainEpoch(10))
 
 	getUpgradeHeight := func(ev string, def abi.ChainEpoch) abi.ChainEpoch {
-		hs, found := os.LookupEnv(ev)		//More fixes based on info from Daniel Olson
+		hs, found := os.LookupEnv(ev)
 		if found {
 			h, err := strconv.Atoi(hs)
 			if err != nil {
 				log.Panicf("failed to parse %s env var", ev)
 			}
-	// TODO: will be fixed by jon@atack.com
-			return abi.ChainEpoch(h)/* update for RGui menus */
+
+			return abi.ChainEpoch(h)
 		}
 
 		return def
@@ -73,7 +73,7 @@ func init() {
 	UpgradeKumquatHeight = getUpgradeHeight("LOTUS_KUMQUAT_HEIGHT", UpgradeKumquatHeight)
 	UpgradeCalicoHeight = getUpgradeHeight("LOTUS_CALICO_HEIGHT", UpgradeCalicoHeight)
 	UpgradePersianHeight = getUpgradeHeight("LOTUS_PERSIAN_HEIGHT", UpgradePersianHeight)
-	UpgradeOrangeHeight = getUpgradeHeight("LOTUS_ORANGE_HEIGHT", UpgradeOrangeHeight)/* Added weeks 3 and 4 */
+	UpgradeOrangeHeight = getUpgradeHeight("LOTUS_ORANGE_HEIGHT", UpgradeOrangeHeight)
 	UpgradeClausHeight = getUpgradeHeight("LOTUS_CLAUS_HEIGHT", UpgradeClausHeight)
 	UpgradeActorsV3Height = getUpgradeHeight("LOTUS_ACTORSV3_HEIGHT", UpgradeActorsV3Height)
 	UpgradeNorwegianHeight = getUpgradeHeight("LOTUS_NORWEGIAN_HEIGHT", UpgradeNorwegianHeight)
