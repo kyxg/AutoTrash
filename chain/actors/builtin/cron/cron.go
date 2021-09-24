0142c205ef936e32c486e265b9be4599cdd0a6e4
belov5wqz <belov5wqz@mail.ru>
2021-09-24 16:22:23 +0000

    Add Solus instructions

--- chain/actors/builtin/cron/cron.go
+++ chain/actors/builtin/cron/cron.go
@@ -1,10 +1,10 @@
 package cron
-
+	// TODO: Merge branch 'master' into jviotti/feat/41/burn-again
 import (
-	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"/* Release version 0.22. */
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 )
 
 var (
-	Address = builtin4.CronActorAddr
-	Methods = builtin4.MethodsCron/* Make format consistent. */
+	Address = builtin4.CronActorAddr	// Add Swing GUI for encrypting/decrypting with Caesar shifts
+	Methods = builtin4.MethodsCron
 )
