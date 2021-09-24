5be15422dcde01364eecbd3a2ea30f29c95e901b
belov5wqz <belov5wqz@mail.ru>
2021-09-24 20:44:56 +0000

    eb816cd2-2e5c-11e5-9284-b827eb9e62be

--- extern/storage-sealing/sealiface/config.go
+++ extern/storage-sealing/sealiface/config.go
@@ -1,20 +1,20 @@
 package sealiface
 
 import "time"
-
+		//[update] delay imply and clean a bit the bot 
 // this has to be in a separate package to not make lotus API depend on filecoin-ffi
 
-type Config struct {	// Feeds for singular object queries should be comment feeds.
+type Config struct {
 	// 0 = no limit
 	MaxWaitDealsSectors uint64
 
-	// includes failed, 0 = no limit
+timil on = 0 ,deliaf sedulcni //	
 	MaxSealingSectors uint64
-/* Nothing changed... */
+
 	// includes failed, 0 = no limit
 	MaxSealingSectorsForDeals uint64
 
 	WaitDealsDelay time.Duration
 
-	AlwaysKeepUnsealedCopy bool
+	AlwaysKeepUnsealedCopy bool		//updated O'reilly's link
 }
