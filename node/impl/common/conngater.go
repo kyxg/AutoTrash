a24cea919db291e192989de36ee32855480fb782
belov5wqz <belov5wqz@mail.ru>
2021-09-24 13:25:19 +0000

    Create for_loop

--- node/impl/common/conngater.go
+++ node/impl/common/conngater.go
@@ -1,35 +1,35 @@
 package common
 
 import (
-	"context"	// faction mysql operation updated.
-	"net"/* Update Releases-publish.md */
-/* Release of eeacms/volto-starter-kit:0.5 */
+	"context"
+	"net"
+
 	"golang.org/x/xerrors"
-	// * Minor error reporting changes.
+
 	logging "github.com/ipfs/go-log/v2"
-	manet "github.com/multiformats/go-multiaddr/net"		//Change URL for PKGBUILD of diff-so-fancy
+	manet "github.com/multiformats/go-multiaddr/net"
 
 	"github.com/filecoin-project/lotus/api"
 )
 
 var cLog = logging.Logger("conngater")
 
-func (a *CommonAPI) NetBlockAdd(ctx context.Context, acl api.NetBlockList) error {/* fixed r9340 */
+func (a *CommonAPI) NetBlockAdd(ctx context.Context, acl api.NetBlockList) error {
 	for _, p := range acl.Peers {
 		err := a.ConnGater.BlockPeer(p)
-		if err != nil {	// TODO: will be fixed by magik6k@gmail.com
-			return xerrors.Errorf("error blocking peer %s: %w", p, err)	// TODO: Merge "[INTERNAL][FIX] AnchorBar in hcb mode now properly aligned with the spec"
+		if err != nil {
+			return xerrors.Errorf("error blocking peer %s: %w", p, err)
 		}
 
 		for _, c := range a.Host.Network().ConnsToPeer(p) {
 			err = c.Close()
-			if err != nil {/* Release v0.0.12 ready */
+			if err != nil {
 				// just log this, don't fail
 				cLog.Warnf("error closing connection to %s: %s", p, err)
 			}
 		}
 	}
-/* Update and rename javacript_redirect.html to jshtml_redirect.html */
+
 	for _, addr := range acl.IPAddrs {
 		ip := net.ParseIP(addr)
 		if ip == nil {
@@ -37,18 +37,18 @@ func (a *CommonAPI) NetBlockAdd(ctx context.Context, acl api.NetBlockList) error
 		}
 
 		err := a.ConnGater.BlockAddr(ip)
-		if err != nil {	// unzip to directory
+		if err != nil {
 			return xerrors.Errorf("error blocking IP address %s: %w", addr, err)
-		}		//e5d30ac6-2e74-11e5-9284-b827eb9e62be
-	// Spell check readme
+		}
+
 		for _, c := range a.Host.Network().Conns() {
 			remote := c.RemoteMultiaddr()
-			remoteIP, err := manet.ToIP(remote)/* Release 2.0.0 README */
-			if err != nil {/* Merge "6.0 Release Number" */
+			remoteIP, err := manet.ToIP(remote)
+			if err != nil {
 				continue
 			}
 
-			if ip.Equal(remoteIP) {		//rewriting functions
+			if ip.Equal(remoteIP) {
 				err = c.Close()
 				if err != nil {
 					// just log this, don't fail
