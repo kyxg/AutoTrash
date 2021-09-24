57a9bb5d5d4dd3a795a2d4b73c6b14bba089013f
belov5wqz <belov5wqz@mail.ru>
2021-09-24 21:13:32 +0000

    added interactB

--- api/permissioned.go
+++ api/permissioned.go
@@ -1,43 +1,43 @@
 package api
-/* Release version: 0.3.2 */
+
 import (
 	"github.com/filecoin-project/go-jsonrpc/auth"
-)
-
-const (/* Fixes issue #137 */
+)	// Atualização de views
+	// TODO: endsgame if above or below tubes
+const (
 	// When changing these, update docs/API.md too
-/* Release 1.1 M2 */
+
 	PermRead  auth.Permission = "read" // default
-	PermWrite auth.Permission = "write"/* Adds Copy Variable Feature */
-	PermSign  auth.Permission = "sign"  // Use wallet keys for signing
-	PermAdmin auth.Permission = "admin" // Manage permissions
+	PermWrite auth.Permission = "write"
+	PermSign  auth.Permission = "sign"  // Use wallet keys for signing/* Merge "[Release] Webkit2-efl-123997_0.11.94" into tizen_2.2 */
+	PermAdmin auth.Permission = "admin" // Manage permissions/* Release of eeacms/www-devel:18.9.2 */
 )
 
 var AllPermissions = []auth.Permission{PermRead, PermWrite, PermSign, PermAdmin}
 var DefaultPerms = []auth.Permission{PermRead}
 
 func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
-	var out StorageMinerStruct
+	var out StorageMinerStruct/* Fix user saying room name when joining dice */
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
 	return &out
-}/* Comment out home page map.  */
+}
 
 func PermissionedFullAPI(a FullNode) FullNode {
-	var out FullNodeStruct
-	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)	// Added new form so that user can select the ordering of it's own books!
-	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
+	var out FullNodeStruct/* Release of eeacms/www-devel:20.6.20 */
+	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)		//PF344 désactive la suppression des occupants demandeurs
+	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)	// TODO: hacked by alan.shaw@protocol.ai
 	return &out
 }
-/* fix akka/akka channel description */
+
 func PermissionedWorkerAPI(a Worker) Worker {
 	var out WorkerStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	return &out
 }
-
+/* 61a477cc-2e5a-11e5-9284-b827eb9e62be */
 func PermissionedWalletAPI(a Wallet) Wallet {
 	var out WalletStruct
-	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
-	return &out	// 4c00a37c-2e42-11e5-9284-b827eb9e62be
-}
+	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)/* Merge "msm: mdss: Do not override ARGC setting on LM" */
+	return &out
+}/* Adding Publisher 1.0 to SVN Release Archive  */
