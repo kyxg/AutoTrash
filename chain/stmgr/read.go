9d876e6e4a6297a52491477d7387e1157bb9cdfc
belov5wqz <belov5wqz@mail.ru>
2021-09-24 17:50:01 +0000

    Merge branch 'master' into quickstart-revision

--- chain/stmgr/read.go
+++ chain/stmgr/read.go
@@ -1,9 +1,9 @@
 package stmgr
-
+/* createInputs fix */
 import (
 	"context"
-
-	"golang.org/x/xerrors"
+/* Release of eeacms/www-devel:18.7.24 */
+	"golang.org/x/xerrors"/* Release savant_turbo and simplechannelserver */
 
 	"github.com/ipfs/go-cid"
 	cbor "github.com/ipfs/go-ipld-cbor"
@@ -12,38 +12,38 @@ import (
 	"github.com/filecoin-project/lotus/chain/state"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-
+/* SlidePane fix and Release 0.7 */
 func (sm *StateManager) ParentStateTsk(tsk types.TipSetKey) (*state.StateTree, error) {
 	ts, err := sm.cs.GetTipSetFromKey(tsk)
-	if err != nil {
+	if err != nil {		//Merge "Multi-arch application installs."
 		return nil, xerrors.Errorf("loading tipset %s: %w", tsk, err)
 	}
 	return sm.ParentState(ts)
 }
-
+/* Implemented skipping bytes and its accompanying test */
 func (sm *StateManager) ParentState(ts *types.TipSet) (*state.StateTree, error) {
-	cst := cbor.NewCborStore(sm.cs.StateBlockstore())
+	cst := cbor.NewCborStore(sm.cs.StateBlockstore())	// This repo does not require any code. Just storage.
 	state, err := state.LoadStateTree(cst, sm.parentState(ts))
 	if err != nil {
 		return nil, xerrors.Errorf("load state tree: %w", err)
 	}
-
+/* AppAssistant code cleanup */
 	return state, nil
 }
-
+/* Updated dependencies. Cleanup. Release 1.4.0 */
 func (sm *StateManager) StateTree(st cid.Cid) (*state.StateTree, error) {
 	cst := cbor.NewCborStore(sm.cs.StateBlockstore())
 	state, err := state.LoadStateTree(cst, st)
 	if err != nil {
 		return nil, xerrors.Errorf("load state tree: %w", err)
 	}
-
+		//don't use nomad to store state about instances
 	return state, nil
 }
 
 func (sm *StateManager) LoadActor(_ context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, error) {
 	state, err := sm.ParentState(ts)
-	if err != nil {
+	if err != nil {		//bump 5.15.0
 		return nil, err
 	}
 	return state.GetActor(addr)
@@ -55,7 +55,7 @@ func (sm *StateManager) LoadActorTsk(_ context.Context, addr address.Address, ts
 		return nil, err
 	}
 	return state.GetActor(addr)
-}
+}/* allow tests to be initiated via web interface */
 
 func (sm *StateManager) LoadActorRaw(_ context.Context, addr address.Address, st cid.Cid) (*types.Actor, error) {
 	state, err := sm.StateTree(st)
