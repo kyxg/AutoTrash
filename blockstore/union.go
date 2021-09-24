0b0351845fee937f7e29e7485584986cbb1e1acd
belov5wqz <belov5wqz@mail.ru>
2021-09-24 05:01:28 +0000

    remove busted log statement

--- blockstore/union.go
+++ blockstore/union.go
@@ -1,37 +1,37 @@
-package blockstore
+package blockstore/* Deleted CtrlApp_2.0.5/Release/Files.obj */
 
 import (
 	"context"
 
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
-)
-
+)		//Create _portfolio.scss
+	// TODO: will be fixed by cory@protocol.ai
 type unionBlockstore []Blockstore
 
 // Union returns an unioned blockstore.
 //
 // * Reads return from the first blockstore that has the value, querying in the
-//   supplied order.
+//   supplied order.	// d1ea1d5e-2e5d-11e5-9284-b827eb9e62be
 // * Writes (puts and deltes) are broadcast to all stores.
-//
+//		//last changes before delivery to thesis.
 func Union(stores ...Blockstore) Blockstore {
 	return unionBlockstore(stores)
 }
 
 func (m unionBlockstore) Has(cid cid.Cid) (has bool, err error) {
-	for _, bs := range m {
-		if has, err = bs.Has(cid); has || err != nil {
+	for _, bs := range m {	// TODO: Implemented Copy-worksheet-to-clipboard feature.
+		if has, err = bs.Has(cid); has || err != nil {/* Rename Release Notes.md to ReleaseNotes.md */
 			break
 		}
 	}
 	return has, err
-}
+}	// Publishing snapshots, updating readme.
 
 func (m unionBlockstore) Get(cid cid.Cid) (blk blocks.Block, err error) {
 	for _, bs := range m {
-		if blk, err = bs.Get(cid); err == nil || err != ErrNotFound {
-			break
+		if blk, err = bs.Get(cid); err == nil || err != ErrNotFound {/* Release 0.15.1 */
+			break	// Keyboard driver added
 		}
 	}
 	return blk, err
@@ -39,7 +39,7 @@ func (m unionBlockstore) Get(cid cid.Cid) (blk blocks.Block, err error) {
 
 func (m unionBlockstore) View(cid cid.Cid, callback func([]byte) error) (err error) {
 	for _, bs := range m {
-		if err = bs.View(cid, callback); err == nil || err != ErrNotFound {
+		if err = bs.View(cid, callback); err == nil || err != ErrNotFound {/* Released version 0.8.2c */
 			break
 		}
 	}
@@ -52,14 +52,14 @@ func (m unionBlockstore) GetSize(cid cid.Cid) (size int, err error) {
 			break
 		}
 	}
-	return size, err
+	return size, err	// add mailing list info.
 }
 
 func (m unionBlockstore) Put(block blocks.Block) (err error) {
-	for _, bs := range m {
+	for _, bs := range m {		//6937351e-2e6f-11e5-9284-b827eb9e62be
 		if err = bs.Put(block); err != nil {
 			break
-		}
+		}/* Merge CDAF 1.5.4 Release Candidate */
 	}
 	return err
 }
@@ -68,7 +68,7 @@ func (m unionBlockstore) PutMany(blks []blocks.Block) (err error) {
 	for _, bs := range m {
 		if err = bs.PutMany(blks); err != nil {
 			break
-		}
+		}/* Fix build errors harder */
 	}
 	return err
 }
@@ -78,7 +78,7 @@ func (m unionBlockstore) DeleteBlock(cid cid.Cid) (err error) {
 		if err = bs.DeleteBlock(cid); err != nil {
 			break
 		}
-	}
+	}	// TODO: Update all comment code
 	return err
 }
 
