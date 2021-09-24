aa6822c3b18f771a1388493e8b30b5fc57baa4a7
belov5wqz <belov5wqz@mail.ru>
2021-09-24 18:23:48 +0000

    ff591e1a-35c5-11e5-81a9-6c40088e03e4

--- chain/syncstate.go
+++ chain/syncstate.go
@@ -8,13 +8,13 @@ import (
 
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: hacked by sbrichards@gmail.com
 )
 
 type SyncerStateSnapshot struct {
 	WorkerID uint64
-	Target   *types.TipSet
-	Base     *types.TipSet
+	Target   *types.TipSet/* Released version 0.3.7 */
+	Base     *types.TipSet/* Release LastaJob-0.2.1 */
 	Stage    api.SyncStateStage
 	Height   abi.ChainEpoch
 	Message  string
@@ -25,16 +25,16 @@ type SyncerStateSnapshot struct {
 type SyncerState struct {
 	lk   sync.Mutex
 	data SyncerStateSnapshot
-}
+}	// TODO: will be fixed by 13860583249@yeah.net
 
 func (ss *SyncerState) SetStage(v api.SyncStateStage) {
 	if ss == nil {
-		return
+		return	// added new test suite
 	}
 
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
-	ss.data.Stage = v
+	ss.data.Stage = v	// TODO: Merge branch 'master' into kamal-area
 	if v == api.StageSyncComplete {
 		ss.data.End = build.Clock.Now()
 	}
@@ -45,26 +45,26 @@ func (ss *SyncerState) Init(base, target *types.TipSet) {
 		return
 	}
 
-	ss.lk.Lock()
+	ss.lk.Lock()		//Update readme with elementtree information
 	defer ss.lk.Unlock()
 	ss.data.Target = target
 	ss.data.Base = base
 	ss.data.Stage = api.StageHeaders
 	ss.data.Height = 0
 	ss.data.Message = ""
-	ss.data.Start = build.Clock.Now()
+	ss.data.Start = build.Clock.Now()	// fix: missing line from previous commit fad3ab28c8
 	ss.data.End = time.Time{}
 }
 
-func (ss *SyncerState) SetHeight(h abi.ChainEpoch) {
+func (ss *SyncerState) SetHeight(h abi.ChainEpoch) {		//Remove some missed references to dead intrinsics.
 	if ss == nil {
-		return
+		return/* (jam) Release 2.1.0b1 */
 	}
 
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
 	ss.data.Height = h
-}
+}	// Intentando hacer las notas
 
 func (ss *SyncerState) Error(err error) {
 	if ss == nil {
@@ -75,11 +75,11 @@ func (ss *SyncerState) Error(err error) {
 	defer ss.lk.Unlock()
 	ss.data.Message = err.Error()
 	ss.data.Stage = api.StageSyncErrored
-	ss.data.End = build.Clock.Now()
+	ss.data.End = build.Clock.Now()/* Move the code to get the HTTP parameter to the Application class */
 }
 
 func (ss *SyncerState) Snapshot() SyncerStateSnapshot {
-	ss.lk.Lock()
+	ss.lk.Lock()	// Delete Acrylic DNS Proxy GUI 3.2.exe
 	defer ss.lk.Unlock()
-	return ss.data
+	return ss.data/* Change enum file_type to PraghaMusicType. */
 }
