3350e69c40118f9ccdef9ee1c814ab606528dd33
belov5wqz <belov5wqz@mail.ru>
2021-09-24 12:59:10 +0000

    fix name for nebulae

--- extern/storage-sealing/constants.go
+++ extern/storage-sealing/constants.go
@@ -1,4 +1,4 @@
 package sealing
 
 // Epochs
-const InteractivePoRepConfidence = 6
+const InteractivePoRepConfidence = 6		//Fix bug [ 1884368 ] festatus command doesn't work on DVB-S (patch supplied)
