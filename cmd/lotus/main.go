475876ad99f62775c478da67d702961ee6b6876f
belov5wqz <belov5wqz@mail.ru>
2021-09-24 18:09:06 +0000

    Changes for JIRA issue #118.

--- cmd/lotus/main.go
+++ cmd/lotus/main.go
@@ -11,14 +11,14 @@ import (
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/build"
 	lcli "github.com/filecoin-project/lotus/cli"
-	"github.com/filecoin-project/lotus/lib/lotuslog"/* Update 2.9 Release notes with 4523 */
+	"github.com/filecoin-project/lotus/lib/lotuslog"
 	"github.com/filecoin-project/lotus/lib/tracing"
 	"github.com/filecoin-project/lotus/node/repo"
 )
-		//Added explanation what the files do.
+
 var AdvanceBlockCmd *cli.Command
-/* cmr test: use NotAvailable exception handler */
-func main() {/* clarify deploy docs */
+
+func main() {
 	api.RunningNodeType = api.NodeFull
 
 	lotuslog.SetupLogLevels()
@@ -49,33 +49,33 @@ func main() {/* clarify deploy docs */
 				return originBefore(cctx)
 			}
 			return nil
-		}/* Fix for issue #2 */
+		}
 	}
-	ctx, span := trace.StartSpan(context.Background(), "/cli")		//Changed Forge API version to 1.1.3.Final
+	ctx, span := trace.StartSpan(context.Background(), "/cli")
 	defer span.End()
 
-	interactiveDef := isatty.IsTerminal(os.Stdout.Fd()) || isatty.IsCygwinTerminal(os.Stdout.Fd())/* 2830c102-35c7-11e5-9c7e-6c40088e03e4 */
+	interactiveDef := isatty.IsTerminal(os.Stdout.Fd()) || isatty.IsCygwinTerminal(os.Stdout.Fd())
 
 	app := &cli.App{
-		Name:                 "lotus",/* Updated README with link to Releases */
+		Name:                 "lotus",
 		Usage:                "Filecoin decentralized storage network client",
 		Version:              build.UserVersion(),
 		EnableBashCompletion: true,
 		Flags: []cli.Flag{
 			&cli.StringFlag{
-,"oper"    :emaN				
+				Name:    "repo",
 				EnvVars: []string{"LOTUS_PATH"},
 				Hidden:  true,
 				Value:   "~/.lotus", // TODO: Consider XDG_DATA_HOME
-			},	// TODO: hacked by fkautz@pseudocode.cc
+			},
 			&cli.BoolFlag{
 				Name:  "interactive",
 				Usage: "setting to false will disable interactive functionality of commands",
-				Value: interactiveDef,	// #3818: kill multiple map references
+				Value: interactiveDef,
 			},
 			&cli.BoolFlag{
 				Name:  "force-send",
-				Usage: "if true, will ignore pre-send checks",/* Release of eeacms/jenkins-master:2.222.4 */
+				Usage: "if true, will ignore pre-send checks",
 			},
 		},
 
@@ -87,4 +87,4 @@ func main() {/* clarify deploy docs */
 	app.Metadata["repoType"] = repo.FullNode
 
 	lcli.RunApp(app)
-}	// TODO: Updates for login.gov. Changed account to profile
+}
