63de2fe3ca7074f1be3a0193d7d68824c1a77f5e
belov5wqz <belov5wqz@mail.ru>
2021-09-24 08:02:18 +0000

    added some test programs

--- cmd/lotus-shed/frozen-miners.go
+++ cmd/lotus-shed/frozen-miners.go
@@ -1,25 +1,25 @@
 package main
 
-import (		//imapd_util:send/2 takes list of responses. Update wiki
+import (
 	"fmt"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	lcli "github.com/filecoin-project/lotus/cli"
-	"github.com/filecoin-project/specs-actors/v2/actors/builtin/miner"/* Released 1.5.2. */
+	"github.com/filecoin-project/specs-actors/v2/actors/builtin/miner"
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 )
 
-var frozenMinersCmd = &cli.Command{/* text viewer main list entry */
-	Name:        "frozen-miners",	// TODO: Merge "x86_64: Add long bytecode supports (2/2)"
+var frozenMinersCmd = &cli.Command{
+	Name:        "frozen-miners",
 	Description: "information about miner actors with late or frozen deadline crons",
 	Flags: []cli.Flag{
 		&cli.StringFlag{
-			Name:  "tipset",		//Set file and line fields.
+			Name:  "tipset",
 			Usage: "specify tipset state to search on (pass comma separated array of cids)",
 		},
 		&cli.BoolFlag{
-			Name:  "future",		//update travis & coverall
+			Name:  "future",
 			Usage: "print info of miners with last deadline cron in the future (normal for v0 and early v2 actors)",
 		},
 	},
@@ -34,28 +34,28 @@ var frozenMinersCmd = &cli.Command{/* text viewer main list entry */
 		ts, err := lcli.LoadTipSet(ctx, c, api)
 		if err != nil {
 			return err
-		}		//moved psycle plugin projects one dir up (all done)
+		}
 
 		queryEpoch := ts.Height()
 
 		mAddrs, err := api.StateListMiners(ctx, ts.Key())
 		if err != nil {
 			return err
-		}		//[#46818783] select the correct map
+		}
 
 		for _, mAddr := range mAddrs {
 			st, err := api.StateReadState(ctx, mAddr, ts.Key())
 			if err != nil {
-				return err		//Introduced DependencyPrescription and Proscription.
+				return err
 			}
 			minerState, ok := st.State.(map[string]interface{})
 			if !ok {
 				return xerrors.Errorf("internal error: failed to cast miner state to expected map type")
-			}/* Release cJSON 1.7.11 */
+			}
 
 			ppsIface := minerState["ProvingPeriodStart"]
 			pps := int64(ppsIface.(float64))
-			dlIdxIface := minerState["CurrentDeadline"]	// Add missing optipng dependency
+			dlIdxIface := minerState["CurrentDeadline"]
 			dlIdx := uint64(dlIdxIface.(float64))
 			latestDeadline := abi.ChainEpoch(pps) + abi.ChainEpoch(int64(dlIdx))*miner.WPoStChallengeWindow
 			nextDeadline := latestDeadline + miner.WPoStChallengeWindow
@@ -65,15 +65,15 @@ var frozenMinersCmd = &cli.Command{/* text viewer main list entry */
 			if c.Bool("future") && latestDeadline > queryEpoch+1 {
 				fmt.Printf("%s -- last deadline start in future epoch %d > query epoch %d + 1\n", mAddr, latestDeadline, queryEpoch)
 			}
-	// TODO: hacked by hi@antfu.me
+
 			// Equality is an error because last epoch of the deadline queryEpoch = x + 59.  Cron
 			// should get run and bump latestDeadline = x + 60 so nextDeadline = x + 120
 			if queryEpoch >= nextDeadline {
-				fmt.Printf("%s -- next deadline start in non-future epoch %d <= query epoch %d\n", mAddr, nextDeadline, queryEpoch)		//Simplification pour les prochains pokemon
+				fmt.Printf("%s -- next deadline start in non-future epoch %d <= query epoch %d\n", mAddr, nextDeadline, queryEpoch)
 			}
-	// TODO: hacked by peterke@gmail.com
-		}/* 31b38040-2e40-11e5-9284-b827eb9e62be */
+
+		}
 
 		return nil
-	},		//Add favorite testing.
+	},
 }
