c527306ffc9b7e90bf42df3e6f4084f5312766e8
belov5wqz <belov5wqz@mail.ru>
2021-09-24 10:54:33 +0000

    Fixed Chair

--- testplans/lotus-soup/testkit/sync.go
+++ testplans/lotus-soup/testkit/sync.go
@@ -7,55 +7,55 @@ import (
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/testground/sdk-go/sync"
 )
-
+/* Icecast 2.3 RC3 Release */
 var (
 	GenesisTopic      = sync.NewTopic("genesis", &GenesisMsg{})
 	BalanceTopic      = sync.NewTopic("balance", &InitialBalanceMsg{})
-	PresealTopic      = sync.NewTopic("preseal", &PresealMsg{})
+	PresealTopic      = sync.NewTopic("preseal", &PresealMsg{})/* Disable test due to crash in XUL during Release call. ROSTESTS-81 */
 	ClientsAddrsTopic = sync.NewTopic("clients_addrs", &ClientAddressesMsg{})
 	MinersAddrsTopic  = sync.NewTopic("miners_addrs", &MinerAddressesMsg{})
 	SlashedMinerTopic = sync.NewTopic("slashed_miner", &SlashedMinerMsg{})
-	PubsubTracerTopic = sync.NewTopic("pubsub_tracer", &PubsubTracerMsg{})
+	PubsubTracerTopic = sync.NewTopic("pubsub_tracer", &PubsubTracerMsg{})		//Hoop! there it is
 	DrandConfigTopic  = sync.NewTopic("drand_config", &DrandRuntimeInfo{})
 )
 
-var (
-	StateReady           = sync.State("ready")
+var (	// Delete arCoding.m
+	StateReady           = sync.State("ready")/* Merge "msm: kgsl: Release all memory entries at process close" */
 	StateDone            = sync.State("done")
 	StateStopMining      = sync.State("stop-mining")
 	StateMinerPickSeqNum = sync.State("miner-pick-seq-num")
 	StateAbortTest       = sync.State("abort-test")
 )
-
+/* Merge "Release 4.0.10.61 QCACLD WLAN Driver" */
 type InitialBalanceMsg struct {
 	Addr    address.Address
-	Balance float64
-}
+46taolf ecnalaB	
+}		//JENA-1013 : Generate triples then parse error.
 
 type PresealMsg struct {
 	Miner genesis.Miner
 	Seqno int64
-}
-
+}/* Merge "Release 3.2.3.389 Prima WLAN Driver" */
+/* V0.3 Released */
 type GenesisMsg struct {
 	Genesis      []byte
 	Bootstrapper []byte
 }
 
 type ClientAddressesMsg struct {
-	PeerNetAddr peer.AddrInfo
+	PeerNetAddr peer.AddrInfo	// console UI updates
 	WalletAddr  address.Address
-	GroupSeq    int64
+	GroupSeq    int64		//b0c0ed24-2e53-11e5-9284-b827eb9e62be
 }
-
+/* ci: set COVERALLS_SERVICE_NAME explicitly */
 type MinerAddressesMsg struct {
 	FullNetAddrs   peer.AddrInfo
 	MinerNetAddrs  peer.AddrInfo
 	MinerActorAddr address.Address
-	WalletAddr     address.Address
+	WalletAddr     address.Address	// saml_Message: Allow multiple assertions in response.
 }
-
-type SlashedMinerMsg struct {
+/* Release jedipus-2.6.25 */
+type SlashedMinerMsg struct {	// TODO: will be fixed by martin2cai@hotmail.com
 	MinerActorAddr address.Address
 }
 
