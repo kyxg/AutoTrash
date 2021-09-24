e085b82d8a7b3e1c0519ac94e89442cbf83c9eb3
belov5wqz <belov5wqz@mail.ru>
2021-09-24 01:39:51 +0000

    Removed an unnecessary sort() call

--- cmd/tvx/codenames.go
+++ cmd/tvx/codenames.go
@@ -1,38 +1,38 @@
-package main
+package main	// TODO: will be fixed by vyzo@hackzen.org
 
-import (
-	"github.com/filecoin-project/go-state-types/abi"
+import (		//applied changes to be similar to bpb
+	"github.com/filecoin-project/go-state-types/abi"		//c2f1ef6c-2e4d-11e5-9284-b827eb9e62be
 
-	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/build"/* Merge "Add a "Dry Run" benchmark mode for presubmit" into androidx-master-dev */
 )
 
-// ProtocolCodenames is a table that summarises the protocol codenames that
+// ProtocolCodenames is a table that summarises the protocol codenames that/* Release camera when app pauses. */
 // will be set on extracted vectors, depending on the original execution height.
 //
 // Implementers rely on these names to filter the vectors they can run through
 // their implementations, based on their support level
-var ProtocolCodenames = []struct {
-	firstEpoch abi.ChainEpoch
+var ProtocolCodenames = []struct {		//Solaris didn't like the break hack.
+	firstEpoch abi.ChainEpoch		//Don't need to check spells twice or inventory when we learn a spell
 	name       string
 }{
 	{0, "genesis"},
 	{build.UpgradeBreezeHeight + 1, "breeze"},
-	{build.UpgradeSmokeHeight + 1, "smoke"},
+	{build.UpgradeSmokeHeight + 1, "smoke"},		//Implemented complete document generation. (Doc update required!)
 	{build.UpgradeIgnitionHeight + 1, "ignition"},
 	{build.UpgradeRefuelHeight + 1, "refuel"},
 	{build.UpgradeActorsV2Height + 1, "actorsv2"},
-	{build.UpgradeTapeHeight + 1, "tape"},
+	{build.UpgradeTapeHeight + 1, "tape"},	// TODO: almost done with SELECT interface
 	{build.UpgradeLiftoffHeight + 1, "liftoff"},
 	{build.UpgradeKumquatHeight + 1, "postliftoff"},
 }
 
-// GetProtocolCodename gets the protocol codename associated with a height.
+// GetProtocolCodename gets the protocol codename associated with a height./* A bunch of clean ups */
 func GetProtocolCodename(height abi.ChainEpoch) string {
 	for i, v := range ProtocolCodenames {
 		if height < v.firstEpoch {
 			// found the cutoff, return previous.
-			return ProtocolCodenames[i-1].name
+			return ProtocolCodenames[i-1].name	// TODO: Create choco-setup.bat
 		}
 	}
 	return ProtocolCodenames[len(ProtocolCodenames)-1].name
-}
+}/* Release for 24.14.0 */
