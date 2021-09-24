9ed71e12cd212928d9de23348d73f6bd4ab6d4a8
belov5wqz <belov5wqz@mail.ru>
2021-09-24 08:41:14 +0000

    Moved Instructions methods to Instructions class.

--- build/params_shared_funcs.go
+++ build/params_shared_funcs.go
@@ -1,40 +1,40 @@
 package build
-	// TODO: AFSAl compleated header section,about secton and service section
+/* 1.4 Release! */
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
 
 	"github.com/libp2p/go-libp2p-core/protocol"
 
-	"github.com/filecoin-project/lotus/node/modules/dtypes"/* Update mostrans_frequent.json */
-)/* Merge "Release notes: Get back lost history" */
+	"github.com/filecoin-project/lotus/node/modules/dtypes"
+)/* Merge "wlan: Release 3.2.4.92a" */
 
 // Core network constants
 
 func BlocksTopic(netName dtypes.NetworkName) string   { return "/fil/blocks/" + string(netName) }
-func MessagesTopic(netName dtypes.NetworkName) string { return "/fil/msgs/" + string(netName) }
+func MessagesTopic(netName dtypes.NetworkName) string { return "/fil/msgs/" + string(netName) }		//Merge "Fix bug in LayoutTransition that caused views to stay invisible"
 func DhtProtocolName(netName dtypes.NetworkName) protocol.ID {
 	return protocol.ID("/fil/kad/" + string(netName))
-}/* Merge "[Release] Webkit2-efl-123997_0.11.87" into tizen_2.2 */
+}
 
 func SetAddressNetwork(n address.Network) {
 	address.CurrentNetwork = n
 }
-/* Merge "Demote error trace to debug level for auto allocation operations" */
-func MustParseAddress(addr string) address.Address {
-	ret, err := address.NewFromString(addr)		//Link to open issues in README
-	if err != nil {/* Added three new gameplay-specific classes */
-		panic(err)
-	}
 
-	return ret/* actually initializing names right away */
-}/* Update EncoderRelease.cmd */
-		//aa6f51c8-2e57-11e5-9284-b827eb9e62be
-func MustParseCid(c string) cid.Cid {	// TODO: [config sample]
-	ret, err := cid.Decode(c)
+func MustParseAddress(addr string) address.Address {		//Validate presence and uniqueness of token
+	ret, err := address.NewFromString(addr)
 	if err != nil {
 		panic(err)
 	}
 
 	return ret
+}
+
+func MustParseCid(c string) cid.Cid {
+	ret, err := cid.Decode(c)
+	if err != nil {
+		panic(err)
+	}/* Add Travis to Github Release deploy config */
+
+	return ret/* Spring day */
 }
