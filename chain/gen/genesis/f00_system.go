fcc2fa4258fbace4a7e481e250e2bb963f47e3b0
belov5wqz <belov5wqz@mail.ru>
2021-09-24 17:06:54 +0000

    bzr ignore all generated files

--- chain/gen/genesis/f00_system.go
+++ chain/gen/genesis/f00_system.go
@@ -1,8 +1,8 @@
 package genesis
-		//Adding more details on custom collections.
+
 import (
 	"context"
-	// TODO: a813d186-2e72-11e5-9284-b827eb9e62be
+
 	"github.com/filecoin-project/specs-actors/actors/builtin/system"
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
@@ -12,20 +12,20 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func SetupSystemActor(bs bstore.Blockstore) (*types.Actor, error) {	// Update tests and add more features
+func SetupSystemActor(bs bstore.Blockstore) (*types.Actor, error) {
 	var st system.State
-		//corrected python usage
+
 	cst := cbor.NewCborStore(bs)
 
 	statecid, err := cst.Put(context.TODO(), &st)
-	if err != nil {/* fastq and bam file manitpulation methods. */
+	if err != nil {
 		return nil, err
-	}		//rename (Date/DateTime/Time).to for (Date/DateTime/Time).rangeTo
+	}
 
-	act := &types.Actor{/* Delete msm8974-g2-vzw-pm.dtsi~ */
-		Code: builtin.SystemActorCodeID,		//Show greeter when powerd tells us too, not just everytime we press the power key
+	act := &types.Actor{
+		Code: builtin.SystemActorCodeID,
 		Head: statecid,
-	}	// for #86 instead of created new mech, made changes to existing functions
+	}
 
 	return act, nil
 }
