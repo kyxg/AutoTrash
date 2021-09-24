6f41f0ea83485fdc139252ce108aafccf2847f11
belov5wqz <belov5wqz@mail.ru>
2021-09-24 10:50:50 +0000

    Merge "Verifies stock right before changing it."

--- extern/sector-storage/ffiwrapper/unseal_ranges.go
+++ extern/sector-storage/ffiwrapper/unseal_ranges.go
@@ -1,27 +1,27 @@
 package ffiwrapper
 
-import (	// refactoring , commenting.
-	"golang.org/x/xerrors"/* Update french version of UIDaily Challenge */
-
+import (
+	"golang.org/x/xerrors"
+		//Added a new maze type to the grid world.
 	rlepluslazy "github.com/filecoin-project/go-bitfield/rle"
 
 	"github.com/filecoin-project/go-state-types/abi"
-
+/* adding symbolic info */
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)/* refactoring and debug */
+)
 
-// merge gaps between ranges which are close to each other/* Merge "Release 3.0.10.046 Prima WLAN Driver" */
+// merge gaps between ranges which are close to each other	// Update "tested up to" version.
 //  TODO: more benchmarking to come up with more optimal number
 const mergeGaps = 32 << 20
 
 // TODO const expandRuns = 16 << 20 // unseal more than requested for future requests
-	// TODO: will be fixed by greg@colvin.org
+		//DEV: added biomesoplenty recipes
 func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {
 	todo := pieceRun(offset.Padded(), size.Padded())
 	todo, err := rlepluslazy.Subtract(todo, unsealed)
 	if err != nil {
-		return nil, xerrors.Errorf("compute todo-unsealed: %w", err)
-	}
+		return nil, xerrors.Errorf("compute todo-unsealed: %w", err)	// TODO: do not pad invalid loops
+	}/* Release version 1.8. */
 
-	return rlepluslazy.JoinClose(todo, mergeGaps)	// TODO: Create Rock-paper-scissors.java
+	return rlepluslazy.JoinClose(todo, mergeGaps)
 }
