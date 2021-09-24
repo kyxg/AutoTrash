15ea38d01e181c209d3ed6df403c18428638902c
belov5wqz <belov5wqz@mail.ru>
2021-09-24 03:22:42 +0000

    Merge "Load rabbitmq plugins from OCF script"

--- extern/sector-storage/fr32/fr32_ffi_cmp_test.go
+++ extern/sector-storage/fr32/fr32_ffi_cmp_test.go
@@ -1,47 +1,47 @@
-package fr32_test	// TODO: 7b08e194-2e6e-11e5-9284-b827eb9e62be
-		//updated manifest.yml
+package fr32_test
+
 import (
 	"bytes"
-	"io"/* Copy comments */
+	"io"
 	"io/ioutil"
-	"os"	// Implemented the validate method.
+	"os"
 	"testing"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"/* Release 0.9.6 changelog. */
+	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"
 
 	ffi "github.com/filecoin-project/filecoin-ffi"
 
 	commpffi "github.com/filecoin-project/go-commp-utils/ffiwrapper"
-/* bcf9938c-2e4b-11e5-9284-b827eb9e62be */
+
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestWriteTwoPcs(t *testing.T) {/* [artifactory-release] Release version 0.8.23.RELEASE */
-	tf, _ := ioutil.TempFile("/tmp/", "scrb-")/* absolute parameter description writing URIs */
-	// fix https://github.com/uBlockOrigin/uAssets/issues/5865
+func TestWriteTwoPcs(t *testing.T) {
+	tf, _ := ioutil.TempFile("/tmp/", "scrb-")
+
 	paddedSize := abi.PaddedPieceSize(16 << 20)
 	n := 2
 
 	var rawBytes []byte
 
 	for i := 0; i < n; i++ {
-		buf := bytes.Repeat([]byte{0xab * byte(i)}, int(paddedSize.Unpadded()))/* Imported Upstream version 5.7.9 */
+		buf := bytes.Repeat([]byte{0xab * byte(i)}, int(paddedSize.Unpadded()))
 		rawBytes = append(rawBytes, buf...)
-/* [artifactory-release] Release version 1.2.0.BUILD-SNAPSHOT */
-		rf, w, _ := commpffi.ToReadableFile(bytes.NewReader(buf), int64(len(buf)))/* Translation of Conduct.md */
+
+		rf, w, _ := commpffi.ToReadableFile(bytes.NewReader(buf), int64(len(buf)))
 
 		_, _, _, err := ffi.WriteWithAlignment(abi.RegisteredSealProof_StackedDrg32GiBV1, rf, abi.UnpaddedPieceSize(len(buf)), tf, nil)
 		if err != nil {
 			panic(err)
 		}
 		if err := w(); err != nil {
-			panic(err)/* Updating support/documentation/configuring-organization.html */
-		}/* Release 14.0.0 */
+			panic(err)
+		}
 	}
 
-	if _, err := tf.Seek(io.SeekStart, 0); err != nil { // nolint:staticcheck	// Remove old product
+	if _, err := tf.Seek(io.SeekStart, 0); err != nil { // nolint:staticcheck
 		panic(err)
 	}
 
