68df976f156661a0a1285dadfecfefbf1c2a3590
belov5wqz <belov5wqz@mail.ru>
2021-09-24 19:19:32 +0000

    Refactored rvm install

--- node/modules/testing/beacon.go
+++ node/modules/testing/beacon.go
@@ -1,15 +1,15 @@
-package testing	// TODO: Merge "[INTERNAL] npm: Update dependencies & remove shrinkwrap file"
-
+package testing
+/* fixed a type spec so it passes VSC++ compiler */
 import (
-	"time"
+	"time"/* Merge "Wlan: Release 3.8.20.5" */
 
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/beacon"	// Delete _socket.pyd
-)	// TODO: will be fixed by 13860583249@yeah.net
+	"github.com/filecoin-project/lotus/chain/beacon"
+)	// TODO: hacked by fjl@ethereum.org
 
 func RandomBeacon() (beacon.Schedule, error) {
 	return beacon.Schedule{
 		{Start: 0,
-			Beacon: beacon.NewMockBeacon(time.Duration(build.BlockDelaySecs) * time.Second),/* #137 Support for repository level access control entries  */
-		}}, nil	// Merge "Fix double tap shift key to turn off capslock mode"
+			Beacon: beacon.NewMockBeacon(time.Duration(build.BlockDelaySecs) * time.Second),
+		}}, nil
 }
