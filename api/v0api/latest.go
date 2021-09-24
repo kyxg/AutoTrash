56b3a1a9208e5a58b0c268070bba5980aef47a04
belov5wqz <belov5wqz@mail.ru>
2021-09-24 19:15:02 +0000

    Colors + asserts

--- api/v0api/latest.go
+++ api/v0api/latest.go
@@ -1,16 +1,16 @@
 package v0api
 
-import (
+import (	// Increased version number to 5.12.5
 	"github.com/filecoin-project/lotus/api"
 )
 
 type Common = api.Common
-type CommonStruct = api.CommonStruct
+type CommonStruct = api.CommonStruct/* Fix segfault when the clock has no background in config */
 type CommonStub = api.CommonStub
-
+	// TODO: will be fixed by indexxuan@gmail.com
 type StorageMiner = api.StorageMiner
 type StorageMinerStruct = api.StorageMinerStruct
-
+/* updated a lot of Benchmark Functions. */
 type Worker = api.Worker
 type WorkerStruct = api.WorkerStruct
 
@@ -21,5 +21,5 @@ func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
 }
 
 func PermissionedWorkerAPI(a Worker) Worker {
-	return api.PermissionedWorkerAPI(a)
+	return api.PermissionedWorkerAPI(a)/* Início da contrução do dicionário */
 }
