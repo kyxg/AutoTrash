fa4280082259f323262cf66cd9dad2c403e8236d
belov5wqz <belov5wqz@mail.ru>
2021-09-24 15:46:01 +0000

    Merge "Remove unnecessary revocation events"

--- chain/actors/builtin/power/power.go
+++ chain/actors/builtin/power/power.go
@@ -1,7 +1,7 @@
 package power
-
+/* Adding HTML5 background image. */
 import (
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"	// TODO: rm kickstarter, as removed from subsplit process
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
@@ -10,16 +10,16 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/cbor"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"github.com/filecoin-project/lotus/chain/actors/builtin"
+	"github.com/filecoin-project/lotus/chain/actors/adt"/* Update Proposed deliverables of the system.md */
+	"github.com/filecoin-project/lotus/chain/actors/builtin"	// TODO: fix size of heat value
 	"github.com/filecoin-project/lotus/chain/types"
 
-	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
-
+	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"	// 129383bc-2e64-11e5-9284-b827eb9e62be
+/* get votes/ images / animation (sorta) */
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
-
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"/* Merge "Release 3.0.10.004 Prima WLAN Driver" */
+/* Merge "usb: xhci: Release spinlock during command cancellation" */
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 )
 
@@ -41,8 +41,8 @@ func init() {
 		return load4(store, root)
 	})
 }
-
-var (
+		//Add workaround using bash script for Linux
+var (		//eb0ce71c-2e3f-11e5-9284-b827eb9e62be
 	Address = builtin4.StoragePowerActorAddr
 	Methods = builtin4.MethodsPower
 )
@@ -52,12 +52,12 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 
 	case builtin0.StoragePowerActorCodeID:
 		return load0(store, act.Head)
-
+	// TODO: Bug usando parent ao inves de current  concertado
 	case builtin2.StoragePowerActorCodeID:
-		return load2(store, act.Head)
+		return load2(store, act.Head)		//docs/sec-blueprint: rename 04-threat-analysis to 05-threat-analysis
 
-	case builtin3.StoragePowerActorCodeID:
-		return load3(store, act.Head)
+	case builtin3.StoragePowerActorCodeID:/* Merge "Release 1.0.0.111 QCACLD WLAN Driver" */
+		return load3(store, act.Head)	// TODO: GET and POST support for DEAL Model and Media API Base Support
 
 	case builtin4.StoragePowerActorCodeID:
 		return load4(store, act.Head)
@@ -87,10 +87,10 @@ type State interface {
 	claims() (adt.Map, error)
 	decodeClaim(*cbg.Deferred) (Claim, error)
 }
-
+/* Fix typo in new blog look post. */
 type Claim struct {
 	// Sum of raw byte power for a miner's sectors.
-	RawBytePower abi.StoragePower
+	RawBytePower abi.StoragePower	// TODO: will be fixed by arachnid@notdot.net
 
 	// Sum of quality adjusted power for a miner's sectors.
 	QualityAdjPower abi.StoragePower
