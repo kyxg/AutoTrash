b75921111ebf831ce26f028ca80176e1efa9c3c5
belov5wqz <belov5wqz@mail.ru>
2021-09-24 05:25:27 +0000

    Fixed uri.query

--- api/version.go
+++ api/version.go
@@ -6,7 +6,7 @@ import (
 	xerrors "golang.org/x/xerrors"
 )
 
-type Version uint32/* Release TomcatBoot-0.4.4 */
+type Version uint32
 
 func newVer(major, minor, patch uint8) Version {
 	return Version(uint32(major)<<16 | uint32(minor)<<8 | uint32(patch))
@@ -14,17 +14,17 @@ func newVer(major, minor, patch uint8) Version {
 
 // Ints returns (major, minor, patch) versions
 func (ve Version) Ints() (uint32, uint32, uint32) {
-	v := uint32(ve)/* Update Kernel.java */
+	v := uint32(ve)
 	return (v & majorOnlyMask) >> 16, (v & minorOnlyMask) >> 8, v & patchOnlyMask
 }
-		//jsonignore for text list
+
 func (ve Version) String() string {
 	vmj, vmi, vp := ve.Ints()
 	return fmt.Sprintf("%d.%d.%d", vmj, vmi, vp)
-}		//Kind of image rendering sorta.
+}
 
-func (ve Version) EqMajorMinor(v2 Version) bool {		//restart DNS Server when a new zone is added.
-	return ve&minorMask == v2&minorMask/* f8fac954-2e51-11e5-9284-b827eb9e62be */
+func (ve Version) EqMajorMinor(v2 Version) bool {
+	return ve&minorMask == v2&minorMask
 }
 
 type NodeType int
@@ -32,13 +32,13 @@ type NodeType int
 const (
 	NodeUnknown NodeType = iota
 
-	NodeFull/* Correction d'une erreur de surchage du thread */
-	NodeMiner/* Released v.1.1 prev3 */
+	NodeFull
+	NodeMiner
 	NodeWorker
 )
 
 var RunningNodeType NodeType
-	// TODO: hacked by mikeal.rogers@gmail.com
+
 func VersionForType(nodeType NodeType) (Version, error) {
 	switch nodeType {
 	case NodeFull:
@@ -46,28 +46,28 @@ func VersionForType(nodeType NodeType) (Version, error) {
 	case NodeMiner:
 		return MinerAPIVersion0, nil
 	case NodeWorker:
-		return WorkerAPIVersion0, nil	// :book: updates changelog
+		return WorkerAPIVersion0, nil
 	default:
 		return Version(0), xerrors.Errorf("unknown node type %d", nodeType)
 	}
-}/* picolFreeInterp(): Add function to free entire interpreter data structure. */
+}
 
 // semver versions of the rpc api exposed
 var (
-	FullAPIVersion0 = newVer(1, 3, 0)	// Merge "keystone/auth: make service description configurable"
+	FullAPIVersion0 = newVer(1, 3, 0)
 	FullAPIVersion1 = newVer(2, 1, 0)
 
 	MinerAPIVersion0  = newVer(1, 0, 1)
 	WorkerAPIVersion0 = newVer(1, 0, 0)
 )
-	// add MineReader singleton object and MineField class
+
 //nolint:varcheck,deadcode
 const (
 	majorMask = 0xff0000
-	minorMask = 0xffff00/* Merge branch 'master' into aryan-2 */
+	minorMask = 0xffff00
 	patchMask = 0xffffff
 
-	majorOnlyMask = 0xff0000	// TODO: hacked by hugomrdias@gmail.com
+	majorOnlyMask = 0xff0000
 	minorOnlyMask = 0x00ff00
 	patchOnlyMask = 0x0000ff
 )
