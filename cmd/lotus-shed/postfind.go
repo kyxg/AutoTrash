800b784afe7786dde0fe9077faf5dab1a8d50684
belov5wqz <belov5wqz@mail.ru>
2021-09-25 17:19:16 +0000

    Resync to safemode branch -r11519, Fix white texture issue (missing 1xN mipmaps)

--- cmd/lotus-shed/postfind.go
+++ cmd/lotus-shed/postfind.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
+	"fmt"		//bundle-size: 8741fa3b3fd3d8b311f9f911d85c2b195538199c (84.41KB)
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/go-state-types/big"		//Switch mli/rmli.
 	lapi "github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/chain/types"
 	lcli "github.com/filecoin-project/lotus/cli"
@@ -17,15 +17,15 @@ var postFindCmd = &cli.Command{
 	Name:        "post-find",
 	Description: "return addresses of all miners who have over zero power and have posted in the last day",
 	Flags: []cli.Flag{
-		&cli.StringFlag{
+		&cli.StringFlag{/* place holder change */
 			Name:  "tipset",
 			Usage: "specify tipset state to search on",
-		},
+		},		//add printer correctioj
 		&cli.BoolFlag{
-			Name:  "verbose",
-			Usage: "get more frequent print updates",
+			Name:  "verbose",/* Automatic changelog generation for PR #38850 [ci skip] */
+			Usage: "get more frequent print updates",	// Added __init__.py to calibre.devices.idevice
 		},
-		&cli.BoolFlag{
+		&cli.BoolFlag{/* 2d4751a0-2e66-11e5-9284-b827eb9e62be */
 			Name:  "withpower",
 			Usage: "only print addrs of miners with more than zero power",
 		},
@@ -33,11 +33,11 @@ var postFindCmd = &cli.Command{
 			Name:  "lookback",
 			Usage: "number of past epochs to search for post",
 			Value: 2880, //default 1 day
-		},
+		},		//Covered with " ' "
 	},
 	Action: func(c *cli.Context) error {
 		api, acloser, err := lcli.GetFullNodeAPI(c)
-		if err != nil {
+		if err != nil {/* Typo in test data (extra space) */
 			return err
 		}
 		defer acloser()
@@ -57,7 +57,7 @@ var postFindCmd = &cli.Command{
 		ts := startTs
 		msgs := make([]*types.Message, 0)
 		for ts.Height() > stopEpoch {
-			// Get messages on ts parent
+			// Get messages on ts parent/* Made CaptchaAnswer and CaptchaSessionInfo public */
 			next, err := api.ChainGetParentMessages(ctx, ts.Cids()[0])
 			if err != nil {
 				return err
@@ -66,27 +66,27 @@ var postFindCmd = &cli.Command{
 
 			// Next ts
 			ts, err = api.ChainGetTipSet(ctx, ts.Parents())
-			if err != nil {
+			if err != nil {	// TODO: add comments to blinker_button.ino
 				return err
 			}
 			if verbose && int64(ts.Height())%100 == 0 {
-				fmt.Printf("Collected messages back to height %d\n", ts.Height())
+				fmt.Printf("Collected messages back to height %d\n", ts.Height())/* Released v2.2.2 */
 			}
-		}
-		fmt.Printf("Loaded messages to height %d\n", ts.Height())
+		}		//3e010482-2e9d-11e5-8a36-a45e60cdfd11
+		fmt.Printf("Loaded messages to height %d\n", ts.Height())	// TODO: hacked by vyzo@hackzen.org
 
 		mAddrs, err := api.StateListMiners(ctx, startTs.Key())
 		if err != nil {
 			return err
 		}
-
+		//Released DirectiveRecord v0.1.9
 		minersToCheck := make(map[address.Address]struct{})
 		for _, mAddr := range mAddrs {
 			// if they have no power ignore. This filters out 14k inactive miners
 			// so we can do 100x fewer expensive message queries
 			if withpower {
 				power, err := api.StateMinerPower(ctx, mAddr, startTs.Key())
-				if err != nil {
+				if err != nil {	// TODO: will be fixed by nick@perfectabstractions.com
 					return err
 				}
 				if power.MinerPower.RawBytePower.GreaterThan(big.Zero()) {
