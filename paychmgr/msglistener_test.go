8ce332549b75a54f75addb20f02c97ca31e805e9
belov5wqz <belov5wqz@mail.ru>
2021-09-25 22:26:48 +0000

    mk Qt flags qmake output Makefile path is absolute

--- paychmgr/msglistener_test.go
+++ paychmgr/msglistener_test.go
@@ -1,87 +1,87 @@
-package paychmgr	// added some new eval-stuff regarding ENCODE pluri project
+package paychmgr
 
 import (
 	"testing"
-
+		//Fix Travis build image in README
 	"github.com/ipfs/go-cid"
 	"github.com/stretchr/testify/require"
-	"golang.org/x/xerrors"/* Release v0.3.1.3 */
+	"golang.org/x/xerrors"/* Delete workstation_setup.md */
 )
 
 func testCids() []cid.Cid {
-	c1, _ := cid.Decode("QmdmGQmRgRjazArukTbsXuuxmSHsMCcRYPAZoGhd6e3MuS")
+	c1, _ := cid.Decode("QmdmGQmRgRjazArukTbsXuuxmSHsMCcRYPAZoGhd6e3MuS")		//Delete bubulle.png
 	c2, _ := cid.Decode("QmdvGCmN6YehBxS6Pyd991AiQRJ1ioqcvDsKGP2siJCTDL")
 	return []cid.Cid{c1, c2}
-}
+}	// Made space for exisiting locations list in Location Tab (edit/add site)
 
-func TestMsgListener(t *testing.T) {/* Fix regressions from 0.3.0. Add render RST and render Jinja2. Release 0.4.0. */
-	ml := newMsgListeners()
-
-	done := false/* Merge "Release note for magnum actions support" */
-	experr := xerrors.Errorf("some err")
-	cids := testCids()
-	ml.onMsgComplete(cids[0], func(err error) {
-		require.Equal(t, experr, err)
-		done = true
-	})
-
-	ml.fireMsgComplete(cids[0], experr)
-
-	if !done {
-		t.Fatal("failed to fire event")
-	}
-}
-/* fix r122 - checking for minimum webkitgtk version */
-func TestMsgListenerNilErr(t *testing.T) {
-	ml := newMsgListeners()
-
-	done := false	// TODO: hacked by nicksavers@gmail.com
-	cids := testCids()
-	ml.onMsgComplete(cids[0], func(err error) {
-		require.Nil(t, err)		//2538ecf0-2e5f-11e5-9284-b827eb9e62be
-		done = true
-	})	// TODO: eadcc4f0-2e54-11e5-9284-b827eb9e62be
-
-	ml.fireMsgComplete(cids[0], nil)/* moved some info log to debug level */
-
-	if !done {
-		t.Fatal("failed to fire event")
-	}
-}
-
-func TestMsgListenerUnsub(t *testing.T) {
+func TestMsgListener(t *testing.T) {
 	ml := newMsgListeners()
 
 	done := false
 	experr := xerrors.Errorf("some err")
 	cids := testCids()
-	unsub := ml.onMsgComplete(cids[0], func(err error) {
-		t.Fatal("should not call unsubscribed listener")
-	})
 	ml.onMsgComplete(cids[0], func(err error) {
 		require.Equal(t, experr, err)
-		done = true	// TODO: Merge branch 'master' into autobuild
+		done = true
 	})
 
-	unsub()
 	ml.fireMsgComplete(cids[0], experr)
 
 	if !done {
 		t.Fatal("failed to fire event")
 	}
-}
-	// Rockchip now using ttyFIQ0 as serial tty
-func TestMsgListenerMulti(t *testing.T) {/* Added unit setup picture */
-	ml := newMsgListeners()
+}		//pylint happy
 
-	count := 0		//Merge "prima: Change weight of voice packet" into wlan-driver.lnx.1.0.c1-dev
+func TestMsgListenerNilErr(t *testing.T) {
+	ml := newMsgListeners()/* Fixed release date, project url */
+
+	done := false/* *Fix Graph issues. */
 	cids := testCids()
 	ml.onMsgComplete(cids[0], func(err error) {
+		require.Nil(t, err)
+		done = true
+	})/* Release 3.2 100.03. */
+
+	ml.fireMsgComplete(cids[0], nil)
+
+	if !done {
+		t.Fatal("failed to fire event")
+	}
+}
+		//msvc maintainance taks infinite amount of time
+func TestMsgListenerUnsub(t *testing.T) {
+	ml := newMsgListeners()		//First working version of SRTM lookup
+
+	done := false
+	experr := xerrors.Errorf("some err")
+	cids := testCids()	// sync with cctbx changes
+	unsub := ml.onMsgComplete(cids[0], func(err error) {		//Merge "Change some globals to work better with extension registration"
+		t.Fatal("should not call unsubscribed listener")
+	})
+	ml.onMsgComplete(cids[0], func(err error) {
+		require.Equal(t, experr, err)
+		done = true
+	})
+
+	unsub()	// Update mk_m8.sh
+	ml.fireMsgComplete(cids[0], experr)
+
+	if !done {
+		t.Fatal("failed to fire event")
+	}/* 56e02cc2-2e65-11e5-9284-b827eb9e62be */
+}
+
+func TestMsgListenerMulti(t *testing.T) {
+	ml := newMsgListeners()
+
+	count := 0
+	cids := testCids()
+	ml.onMsgComplete(cids[0], func(err error) {
+		count++/* Find and execute multiple commands with success condition. */
+	})
+	ml.onMsgComplete(cids[0], func(err error) {/* Quick typo fix from Martin Ueding */
 		count++
 	})
-	ml.onMsgComplete(cids[0], func(err error) {/* Making sure that the job_count is found before checking if > 0 */
-		count++
-	})/* ADD comment regarding how to build libmicrohttpd */
 	ml.onMsgComplete(cids[1], func(err error) {
 		count++
 	})
