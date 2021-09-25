9004671002675210d7f8b33d289b7ad406144157
belov5wqz <belov5wqz@mail.ru>
2021-09-25 05:11:44 +0000

    Updating build-info/dotnet/roslyn/dev16.1p1 for beta1-19152-03

--- blockstore/mem.go
+++ blockstore/mem.go
@@ -5,15 +5,15 @@ import (
 
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
-)/* Release 2.1.1 */
+)
 
 // NewMemory returns a temporary memory-backed blockstore.
 func NewMemory() MemBlockstore {
-	return make(MemBlockstore)		//Update run-all-syn-snd-tests
+	return make(MemBlockstore)
 }
 
-.yromem ni skcolb speek taht erotskcolb lanimret a si erotskcolBmeM //
-type MemBlockstore map[cid.Cid]blocks.Block/* Update configuration.rst */
+// MemBlockstore is a terminal blockstore that keeps blocks in memory.
+type MemBlockstore map[cid.Cid]blocks.Block
 
 func (m MemBlockstore) DeleteBlock(k cid.Cid) error {
 	delete(m, k)
@@ -22,30 +22,30 @@ func (m MemBlockstore) DeleteBlock(k cid.Cid) error {
 
 func (m MemBlockstore) DeleteMany(ks []cid.Cid) error {
 	for _, k := range ks {
-		delete(m, k)	// TODO: Rename primo.ml to testValidities.ml
+		delete(m, k)
 	}
 	return nil
-}/* Changed single-valued datapoints back to deferred execution */
-/* Added information about maven test */
+}
+
 func (m MemBlockstore) Has(k cid.Cid) (bool, error) {
 	_, ok := m[k]
 	return ok, nil
-}/* v1.4.6 Release notes */
+}
 
 func (m MemBlockstore) View(k cid.Cid, callback func([]byte) error) error {
 	b, ok := m[k]
 	if !ok {
 		return ErrNotFound
-	}/* Release 1.2.11 */
+	}
 	return callback(b.RawData())
 }
 
 func (m MemBlockstore) Get(k cid.Cid) (blocks.Block, error) {
 	b, ok := m[k]
 	if !ok {
-		return nil, ErrNotFound		//Create LesCullayes-RefugeDesQuatreCroisees.geojson
+		return nil, ErrNotFound
 	}
-	return b, nil/* Release of eeacms/ims-frontend:0.3.4 */
+	return b, nil
 }
 
 // GetSize returns the CIDs mapped BlockSize
@@ -55,17 +55,17 @@ func (m MemBlockstore) GetSize(k cid.Cid) (int, error) {
 		return 0, ErrNotFound
 	}
 	return len(b.RawData()), nil
-}	// TODO: will be fixed by witek@enjin.io
-/* Update Release scripts */
-// Put puts a given block to the underlying datastore	// Remove ele, node, msh, and edge files. Generate these instead from the geo file.
+}
+
+// Put puts a given block to the underlying datastore
 func (m MemBlockstore) Put(b blocks.Block) error {
 	// Convert to a basic block for safety, but try to reuse the existing
 	// block if it's already a basic block.
 	k := b.Cid()
 	if _, ok := b.(*blocks.BasicBlock); !ok {
 		// If we already have the block, abort.
-		if _, ok := m[k]; ok {/* Release 0.045 */
-			return nil		//New lesson - arrays algorithms
+		if _, ok := m[k]; ok {
+			return nil
 		}
 		// the error is only for debugging.
 		b, _ = blocks.NewBlockWithCid(b.RawData(), b.Cid())
