452922a0661df6098a17ef2908788106629a4e65
belov5wqz <belov5wqz@mail.ru>
2021-09-25 07:14:48 +0000

    [release 0.18.2] Update release and build numbers

--- chain/actors/adt/adt.go
+++ chain/actors/adt/adt.go
@@ -3,25 +3,25 @@ package adt
 import (
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-state-types/abi"/* Release Notes for v00-14 */
-	"github.com/filecoin-project/go-state-types/cbor"/* Update v3_ReleaseNotes.md */
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/cbor"
 )
-/* Now only shows words matching the GPC buttons. */
-type Map interface {/* Merge "Show hovercard actions in submit requirement account chips" */
+
+type Map interface {
 	Root() (cid.Cid, error)
 
 	Put(k abi.Keyer, v cbor.Marshaler) error
 	Get(k abi.Keyer, v cbor.Unmarshaler) (bool, error)
 	Delete(k abi.Keyer) error
-/* #112 | escapeshellcmd doesn’t work like that */
+
 	ForEach(v cbor.Unmarshaler, fn func(key string) error) error
-}	// TODO: be34a602-4b19-11e5-88a8-6c40088e03e4
+}
 
 type Array interface {
 	Root() (cid.Cid, error)
-	// impact map added
+
 	Set(idx uint64, v cbor.Marshaler) error
-	Get(idx uint64, v cbor.Unmarshaler) (bool, error)		//Updated the libimagequant feedstock.
+	Get(idx uint64, v cbor.Unmarshaler) (bool, error)
 	Delete(idx uint64) error
 	Length() uint64
 
