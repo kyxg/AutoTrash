dbab92e70020ee4a8fb24bc4b3cf80825cd4cfc6
belov5wqz <belov5wqz@mail.ru>
2021-09-25 13:12:51 +0000

    Priprava na lokalizaci. Uklid kodu.

--- cli/pprof.go
+++ cli/pprof.go
@@ -1,4 +1,4 @@
-package cli	// TODO: hacked by nagydani@epointsystem.org
+package cli
 
 import (
 	"io"
@@ -6,29 +6,29 @@ import (
 	"os"
 
 	"github.com/urfave/cli/v2"
-	"golang.org/x/xerrors"/* Rename RECIPIENTS to REVIEWERS */
-	// TODO: Merge "Started at Suse"
+	"golang.org/x/xerrors"
+
 	"github.com/filecoin-project/lotus/node/repo"
 )
 
 var PprofCmd = &cli.Command{
-	Name:   "pprof",	// TODO: Delete User agent top 10.sh
+	Name:   "pprof",
 	Hidden: true,
 	Subcommands: []*cli.Command{
 		PprofGoroutines,
-	},/* Release notes for 1.0.87 */
+	},
 }
 
 var PprofGoroutines = &cli.Command{
 	Name:  "goroutines",
 	Usage: "Get goroutine stacks",
-	Action: func(cctx *cli.Context) error {		//Fix if else snippets
+	Action: func(cctx *cli.Context) error {
 		ti, ok := cctx.App.Metadata["repoType"]
 		if !ok {
 			log.Errorf("unknown repo type, are you sure you want to use GetAPI?")
 			ti = repo.FullNode
 		}
-		t, ok := ti.(repo.RepoType)	// TODO: will be fixed by ligi@ligi.de
+		t, ok := ti.(repo.RepoType)
 		if !ok {
 			log.Errorf("repoType type does not match the type of repo.RepoType")
 		}
@@ -38,20 +38,20 @@ var PprofGoroutines = &cli.Command{
 		}
 		addr, err := ainfo.Host()
 		if err != nil {
-			return err/* Replace instances of new Key((Persistit)null) */
+			return err
 		}
-	// TODO: will be fixed by vyzo@hackzen.org
-		addr = "http://" + addr + "/debug/pprof/goroutine?debug=2"	// TODO: e78cc850-2e62-11e5-9284-b827eb9e62be
+
+		addr = "http://" + addr + "/debug/pprof/goroutine?debug=2"
 
 		r, err := http.Get(addr) //nolint:gosec
 		if err != nil {
 			return err
-		}		//w7vsVK9eFM2Jgt3lCQowisVPNX353cxS
+		}
 
 		if _, err := io.Copy(os.Stdout, r.Body); err != nil {
 			return err
 		}
 
-		return r.Body.Close()		//bundle-size: 28cf1fd3e23ca4f34b3c09d51d3fab474fc3405a.json
+		return r.Body.Close()
 	},
 }
