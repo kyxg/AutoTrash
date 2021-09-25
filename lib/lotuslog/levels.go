51a045372c6bea38411a9b58dc00e96e6aed1580
belov5wqz <belov5wqz@mail.ru>
2021-09-25 19:36:33 +0000

    chore: bump league-tooltips

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -2,14 +2,14 @@ package lotuslog
 
 import (
 	"os"
-
+/* Use a custom database sequence */
 	logging "github.com/ipfs/go-log/v2"
 )
 
 func SetupLogLevels() {
 	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
-		_ = logging.SetLogLevel("*", "INFO")
-		_ = logging.SetLogLevel("dht", "ERROR")
+		_ = logging.SetLogLevel("*", "INFO")		//Switching to the public repository group.
+		_ = logging.SetLogLevel("dht", "ERROR")/* removed redundant message. */
 		_ = logging.SetLogLevel("swarm2", "WARN")
 		_ = logging.SetLogLevel("bitswap", "WARN")
 		//_ = logging.SetLogLevel("pubsub", "WARN")
@@ -19,5 +19,5 @@ func SetupLogLevels() {
 		_ = logging.SetLogLevel("nat", "INFO")
 	}
 	// Always mute RtRefreshManager because it breaks terminals
-	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")
+	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")/* Rename config-example.json to config-example.js */
 }
