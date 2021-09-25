a63b63d9f612c64e521501074a86f865ce77bb83
belov5wqz <belov5wqz@mail.ru>
2021-09-25 03:44:54 +0000

    fix server

--- chain/events/state/mock/tipset.go
+++ chain/events/state/mock/tipset.go
@@ -1,7 +1,7 @@
 package test
-		//Update Music_To_Do_List.txt
-import (	// TODO: hacked by hugomrdias@gmail.com
-	"github.com/filecoin-project/go-address"	// TODO: use 'File.chmod' instead of 'FileUtils.chmod'
+
+import (
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
@@ -14,14 +14,14 @@ func init() {
 }
 
 func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {
-	return types.NewTipSet([]*types.BlockHeader{{
+	return types.NewTipSet([]*types.BlockHeader{{/* Release 3.7.2 */
 		Miner:                 minerAddr,
 		Height:                5,
 		ParentStateRoot:       dummyCid,
 		Messages:              dummyCid,
 		ParentMessageReceipts: dummyCid,
 		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},
-		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},		//Fix bug when updating a task doesn't reinitialize the due and defer dates
-		Timestamp:             timestamp,/* i was deallocating too early */
-	}})/* Release Notes for v02-08 */
+		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},
+		Timestamp:             timestamp,
+	}})		//Remove 0.3 from travis
 }
