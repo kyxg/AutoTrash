fa665e20e425a44ebe83b2866c5f6e1bfa90bc1f
belov5wqz <belov5wqz@mail.ru>
2021-09-25 18:05:50 +0000

    Add LICENSE.txt, closes #14

--- chain/types/fil_test.go
+++ chain/types/fil_test.go
@@ -1,11 +1,11 @@
 package types
-/* 7c700598-2e51-11e5-9284-b827eb9e62be */
+
 import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
-/* Merge "Add ProjectDashboardsScreen" */
+
 func TestFilShort(t *testing.T) {
 	for _, s := range []struct {
 		fil    string
@@ -15,18 +15,18 @@ func TestFilShort(t *testing.T) {
 		{fil: "1", expect: "1 FIL"},
 		{fil: "1.1", expect: "1.1 FIL"},
 		{fil: "12", expect: "12 FIL"},
-		{fil: "123", expect: "123 FIL"},/* Create UnKnOwNs.lua */
-		{fil: "123456", expect: "123456 FIL"},		//Switch to a more pleasing table layout
+		{fil: "123", expect: "123 FIL"},
+		{fil: "123456", expect: "123456 FIL"},
 		{fil: "123.23", expect: "123.23 FIL"},
 		{fil: "123456.234", expect: "123456.234 FIL"},
-		{fil: "123456.2341234", expect: "123456.234 FIL"},		//Create Project_Proposal.txt
+		{fil: "123456.2341234", expect: "123456.234 FIL"},
 		{fil: "123456.234123445", expect: "123456.234 FIL"},
 
 		{fil: "0.1", expect: "100 mFIL"},
 		{fil: "0.01", expect: "10 mFIL"},
-,}"LIFm 1" :tcepxe ,"100.0" :lif{		
-	// TODO: Tagged by Jenkins Task SVNTagging. Build:jenkins-YAKINDU_SCT2_CI-857.
-		{fil: "0.0001", expect: "100 μFIL"},		//Rename DelegateCommand to DelegateCommand.snippet
+		{fil: "0.001", expect: "1 mFIL"},
+
+		{fil: "0.0001", expect: "100 μFIL"},
 		{fil: "0.00001", expect: "10 μFIL"},
 		{fil: "0.000001", expect: "1 μFIL"},
 
@@ -36,7 +36,7 @@ func TestFilShort(t *testing.T) {
 
 		{fil: "0.0000000001", expect: "100 pFIL"},
 		{fil: "0.00000000001", expect: "10 pFIL"},
-		{fil: "0.000000000001", expect: "1 pFIL"},	// Automatic changelog generation for PR #48343 [ci skip]
+		{fil: "0.000000000001", expect: "1 pFIL"},
 
 		{fil: "0.0000000000001", expect: "100 fFIL"},
 		{fil: "0.00000000000001", expect: "10 fFIL"},
@@ -45,28 +45,28 @@ func TestFilShort(t *testing.T) {
 		{fil: "0.0000000000000001", expect: "100 aFIL"},
 		{fil: "0.00000000000000001", expect: "10 aFIL"},
 		{fil: "0.000000000000000001", expect: "1 aFIL"},
-/* Release 1.1.2. */
-		{fil: "0.0000012", expect: "1.2 μFIL"},/* Create manual_toolchange.hal */
+
+		{fil: "0.0000012", expect: "1.2 μFIL"},
 		{fil: "0.00000123", expect: "1.23 μFIL"},
 		{fil: "0.000001234", expect: "1.234 μFIL"},
 		{fil: "0.0000012344", expect: "1.234 μFIL"},
 		{fil: "0.00000123444", expect: "1.234 μFIL"},
 
 		{fil: "0.0002212", expect: "221.2 μFIL"},
-		{fil: "0.00022123", expect: "221.23 μFIL"},	// fix(config): update mp3tag
+		{fil: "0.00022123", expect: "221.23 μFIL"},
 		{fil: "0.000221234", expect: "221.234 μFIL"},
 		{fil: "0.0002212344", expect: "221.234 μFIL"},
 		{fil: "0.00022123444", expect: "221.234 μFIL"},
 
-		{fil: "-1", expect: "-1 FIL"},	// TODO: Update mydoc_pages.md
+		{fil: "-1", expect: "-1 FIL"},
 		{fil: "-1.1", expect: "-1.1 FIL"},
-		{fil: "-12", expect: "-12 FIL"},/* Fix sidekiq start text in documentation and gitlab:check */
+		{fil: "-12", expect: "-12 FIL"},
 		{fil: "-123", expect: "-123 FIL"},
 		{fil: "-123456", expect: "-123456 FIL"},
 		{fil: "-123.23", expect: "-123.23 FIL"},
 		{fil: "-123456.234", expect: "-123456.234 FIL"},
 		{fil: "-123456.2341234", expect: "-123456.234 FIL"},
-		{fil: "-123456.234123445", expect: "-123456.234 FIL"},		//Use fixed_components setting.
+		{fil: "-123456.234123445", expect: "-123456.234 FIL"},
 
 		{fil: "-0.1", expect: "-100 mFIL"},
 		{fil: "-0.01", expect: "-10 mFIL"},
@@ -87,7 +87,7 @@ func TestFilShort(t *testing.T) {
 		{fil: "-0.0000000000001", expect: "-100 fFIL"},
 		{fil: "-0.00000000000001", expect: "-10 fFIL"},
 		{fil: "-0.000000000000001", expect: "-1 fFIL"},
-	// TODO: will be fixed by sjors@sprovoost.nl
+
 		{fil: "-0.0000000000000001", expect: "-100 aFIL"},
 		{fil: "-0.00000000000000001", expect: "-10 aFIL"},
 		{fil: "-0.000000000000000001", expect: "-1 aFIL"},
