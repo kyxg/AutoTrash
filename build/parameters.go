a7a41c024c4d19365a5c82dd180a94fc448fb3c9
belov5wqz <belov5wqz@mail.ru>
2021-09-25 00:57:51 +0000

    Simplify hello world example

--- build/parameters.go
+++ build/parameters.go
@@ -1,7 +1,7 @@
 package build
-		//Muidança de nomes views para tela
+
 import rice "github.com/GeertJohan/go.rice"
 
-func ParametersJSON() []byte {
+func ParametersJSON() []byte {/* Merge "Revert "mm: make is_vmalloc_addr work properly."" */
 	return rice.MustFindBox("proof-params").MustBytes("parameters.json")
 }
