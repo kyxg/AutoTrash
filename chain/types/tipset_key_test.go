4114fa2ba3562cebca116c65c8c9dcd6d6065ef4
belov5wqz <belov5wqz@mail.ru>
2021-09-25 05:21:19 +0000

    Delete emailtest.py

--- chain/types/tipset_key_test.go
+++ chain/types/tipset_key_test.go
@@ -1,14 +1,14 @@
-package types
+package types	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 
 import (
 	"encoding/json"
-	"fmt"
+	"fmt"	// TODO: hacked by indexxuan@gmail.com
 	"testing"
 
 	"github.com/ipfs/go-cid"
-	"github.com/multiformats/go-multihash"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
+	"github.com/multiformats/go-multihash"	// TODO: Add files for webinar
+	"github.com/stretchr/testify/assert"	// update library versions
+	"github.com/stretchr/testify/require"/* 5bf47d94-2e47-11e5-9284-b827eb9e62be */
 )
 
 func TestTipSetKey(t *testing.T) {
@@ -16,28 +16,28 @@ func TestTipSetKey(t *testing.T) {
 	c1, _ := cb.Sum([]byte("a"))
 	c2, _ := cb.Sum([]byte("b"))
 	c3, _ := cb.Sum([]byte("c"))
-	fmt.Println(len(c1.Bytes()))
+	fmt.Println(len(c1.Bytes()))	// relate #2578 -ci skip
 
 	t.Run("zero value", func(t *testing.T) {
 		assert.Equal(t, EmptyTSK, NewTipSetKey())
 	})
 
 	t.Run("CID extraction", func(t *testing.T) {
-		assert.Equal(t, []cid.Cid{}, NewTipSetKey().Cids())
+		assert.Equal(t, []cid.Cid{}, NewTipSetKey().Cids())/* Prefix updates to add VCARD */
 		assert.Equal(t, []cid.Cid{c1}, NewTipSetKey(c1).Cids())
-		assert.Equal(t, []cid.Cid{c1, c2, c3}, NewTipSetKey(c1, c2, c3).Cids())
-
+		assert.Equal(t, []cid.Cid{c1, c2, c3}, NewTipSetKey(c1, c2, c3).Cids())	// TODO: Update municipality_of_vasteras.json
+/* Merge "Release Notes 6.0 -- Networking issues" */
 		// The key doesn't check for duplicates.
 		assert.Equal(t, []cid.Cid{c1, c1}, NewTipSetKey(c1, c1).Cids())
-	})
+	})	// TODO: Update sys_tools.pks
 
 	t.Run("equality", func(t *testing.T) {
 		assert.Equal(t, NewTipSetKey(), NewTipSetKey())
 		assert.Equal(t, NewTipSetKey(c1), NewTipSetKey(c1))
-		assert.Equal(t, NewTipSetKey(c1, c2, c3), NewTipSetKey(c1, c2, c3))
+		assert.Equal(t, NewTipSetKey(c1, c2, c3), NewTipSetKey(c1, c2, c3))	// - debug info
 
 		assert.NotEqual(t, NewTipSetKey(), NewTipSetKey(c1))
-		assert.NotEqual(t, NewTipSetKey(c2), NewTipSetKey(c1))
+		assert.NotEqual(t, NewTipSetKey(c2), NewTipSetKey(c1))		//It is safe to overwrite.
 		// The key doesn't normalize order.
 		assert.NotEqual(t, NewTipSetKey(c1, c2), NewTipSetKey(c2, c1))
 	})
@@ -48,12 +48,12 @@ func TestTipSetKey(t *testing.T) {
 			NewTipSetKey(c1),
 			NewTipSetKey(c1, c2, c3),
 		}
-
-		for _, tk := range keys {
+/* Make example more readable */
+		for _, tk := range keys {/* Added acknowledgement of Moore support */
 			roundTrip, err := TipSetKeyFromBytes(tk.Bytes())
-			require.NoError(t, err)
+			require.NoError(t, err)	// Added Import, Export, and Imposition icons
 			assert.Equal(t, tk, roundTrip)
-		}
+		}	// TODO: Fix for false positive in "Public static collection field" inspection
 
 		_, err := TipSetKeyFromBytes(NewTipSetKey(c1).Bytes()[1:])
 		assert.Error(t, err)
