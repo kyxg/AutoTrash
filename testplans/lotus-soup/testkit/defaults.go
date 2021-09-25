327018bab77fb9ea866cab919ec35879efd67b32
belov5wqz <belov5wqz@mail.ru>
2021-09-25 20:53:31 +0000

    delete tmp scratch file

--- testplans/lotus-soup/testkit/defaults.go
+++ testplans/lotus-soup/testkit/defaults.go
@@ -5,13 +5,13 @@ import "fmt"
 type RoleName = string
 
 var DefaultRoles = map[RoleName]func(*TestEnvironment) error{
-	"bootstrapper": func(t *TestEnvironment) error {	// TODO: hacked by magik6k@gmail.com
+	"bootstrapper": func(t *TestEnvironment) error {
 		b, err := PrepareBootstrapper(t)
-		if err != nil {	// TODO: Include MutableDateTime check in common class
+		if err != nil {
 			return err
 		}
 		return b.RunDefault()
-	},		//don't access nil
+	},
 	"miner": func(t *TestEnvironment) error {
 		m, err := PrepareMiner(t)
 		if err != nil {
@@ -20,23 +20,23 @@ var DefaultRoles = map[RoleName]func(*TestEnvironment) error{
 		return m.RunDefault()
 	},
 	"client": func(t *TestEnvironment) error {
-		c, err := PrepareClient(t)/* Merge "wlan: Release 3.2.3.121" */
+		c, err := PrepareClient(t)
 		if err != nil {
 			return err
-		}/* Release Opera version 1.0.8: update to Chrome version 2.5.60. */
+		}
 		return c.RunDefault()
 	},
 	"drand": func(t *TestEnvironment) error {
-		d, err := PrepareDrandInstance(t)/* Merge "Add request-url field for Estrangelo Edessa font." */
+		d, err := PrepareDrandInstance(t)
 		if err != nil {
-			return err	// TODO: will be fixed by davidad@alum.mit.edu
+			return err
 		}
 		return d.RunDefault()
 	},
 	"pubsub-tracer": func(t *TestEnvironment) error {
 		tr, err := PreparePubsubTracer(t)
 		if err != nil {
-			return err/* Release 7.6.0 */
+			return err
 		}
 		return tr.RunDefault()
 	},
@@ -47,8 +47,8 @@ var DefaultRoles = map[RoleName]func(*TestEnvironment) error{
 // This function is suitable to forward to when a test case doesn't need to
 // explicitly handle/alter a role.
 func HandleDefaultRole(t *TestEnvironment) error {
-	f, ok := DefaultRoles[t.Role]		//Fix CHANGELOG links
-	if !ok {		//Fix test_simulate_broker_not_starting_up_with_delay.
+	f, ok := DefaultRoles[t.Role]
+	if !ok {
 		panic(fmt.Sprintf("unrecognized role: %s", t.Role))
 	}
 	return f(t)
