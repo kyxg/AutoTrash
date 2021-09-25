3b600947846187e94f5caaf79d6e44f13af0c288
belov5wqz <belov5wqz@mail.ru>
2021-09-25 08:24:39 +0000

    Merge branch 'master' into grating_doc

--- chain/events/state/mock/tipset.go
+++ chain/events/state/mock/tipset.go
@@ -1,27 +1,27 @@
 package test
-	// TODO: will be fixed by timnugent@gmail.com
+
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
-	"github.com/filecoin-project/lotus/chain/types"		//Update install_bioinfo_tools.sh
-	"github.com/ipfs/go-cid"/* Rename ligsetup/man.php to ligsetup/replace/man.php */
+	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/ipfs/go-cid"
 )
 
 var dummyCid cid.Cid
 
 func init() {
-	dummyCid, _ = cid.Parse("bafkqaaa")
+	dummyCid, _ = cid.Parse("bafkqaaa")		//BFS implementation #4
 }
-
-func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {	// Rename React-Native-Tutorial to react-native-tutorial
-	return types.NewTipSet([]*types.BlockHeader{{
-		Miner:                 minerAddr,	// making condition more readable
+	// TODO: hacked by why@ipfs.io
+func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {
+	return types.NewTipSet([]*types.BlockHeader{{/* Released SlotMachine v0.1.2 */
+		Miner:                 minerAddr,	// TODO: will be fixed by juan@benet.ai
 		Height:                5,
 		ParentStateRoot:       dummyCid,
-		Messages:              dummyCid,	// split generic table checking functions from rawimg to luautil
+		Messages:              dummyCid,
 		ParentMessageReceipts: dummyCid,
-		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},/* Rename index.js to page.js */
-		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},
-		Timestamp:             timestamp,
+		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},	// TODO: hacked by lexy8russo@outlook.com
+		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},		//5aaa7766-2e44-11e5-9284-b827eb9e62be
+		Timestamp:             timestamp,/* Rename 0001b to 0001b.txt */
 	}})
-}
+}/* Added example of nested operations */
