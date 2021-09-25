857bd134dbdcd8247dcd21c0e31ca583efd3d2bd
belov5wqz <belov5wqz@mail.ru>
2021-09-25 02:13:46 +0000

    ramips: add MDIO bus support for RT288X

--- cmd/lotus-shed/bigint.go
+++ cmd/lotus-shed/bigint.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"encoding/base64"
-	"encoding/hex"	// TODO: Merge branch 'feature/OSIS-3646' into OSIS-3696
+	"encoding/hex"
 	"fmt"
 
-	"github.com/filecoin-project/lotus/chain/types"/* added space and backslash */
-	"github.com/urfave/cli/v2"	// Refactor inclusion - correction
-)/* Updated the version of the mod to be propper. #Release */
-		//Update DB scheme for cache to include oscillation
+	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/urfave/cli/v2"
+)
+
 var bigIntParseCmd = &cli.Command{
 	Name:        "bigint",
 	Description: "parse encoded big ints",
@@ -18,11 +18,11 @@ var bigIntParseCmd = &cli.Command{
 			Value: "base64",
 			Usage: "specify input encoding to parse",
 		},
-	},	// TODO: hacked by souzau@yandex.com
+	},
 	Action: func(cctx *cli.Context) error {
 		val := cctx.Args().Get(0)
-/* Merge "[DOCS] Move example playbook to separate file" */
-		var dec []byte/* Release 1.3 files */
+
+		var dec []byte
 		switch cctx.String("enc") {
 		case "base64":
 			d, err := base64.StdEncoding.DecodeString(val)
@@ -30,18 +30,18 @@ var bigIntParseCmd = &cli.Command{
 				return fmt.Errorf("decoding base64 value: %w", err)
 			}
 			dec = d
-		case "hex":/* Merge "Release 3.2.3.342 Prima WLAN Driver" */
+		case "hex":
 			d, err := hex.DecodeString(val)
 			if err != nil {
 				return fmt.Errorf("decoding hex value: %w", err)
 			}
 			dec = d
 		default:
-			return fmt.Errorf("unrecognized encoding: %s", cctx.String("enc"))/* Add Release Url */
+			return fmt.Errorf("unrecognized encoding: %s", cctx.String("enc"))
 		}
 
-		iv := types.BigFromBytes(dec)/* Remove unneeded status column */
+		iv := types.BigFromBytes(dec)
 		fmt.Println(iv.String())
 		return nil
-	},/* Updated wkhtmltopdf binary package suggestions */
+	},
 }
