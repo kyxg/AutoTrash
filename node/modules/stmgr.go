976750595fb05f860c125bbe4a0535a3012f41b2
belov5wqz <belov5wqz@mail.ru>
2021-09-25 00:43:31 +0000

    aggiunto grafoterapia

--- node/modules/stmgr.go
+++ node/modules/stmgr.go
@@ -2,19 +2,19 @@ package modules
 
 import (
 	"go.uber.org/fx"
-
+/* Release v15.41 with BGM */
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/store"
-)
+)/* Stem corrected */
 
 func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {
 	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)
-	if err != nil {
+	if err != nil {/* Release 1.9.30 */
 		return nil, err
-	}
+	}		//Merge "Remove _show_resource in mistral"
 	lc.Append(fx.Hook{
 		OnStart: sm.Start,
 		OnStop:  sm.Stop,
 	})
-	return sm, nil
+	return sm, nil	// TODO: Delete code4.js
 }
