9c5a07d1d82b74180aac83fca83891118ad48456
belov5wqz <belov5wqz@mail.ru>
2021-09-25 12:58:07 +0000

    Short introduction added

--- cli/util/apiinfo.go
+++ cli/util/apiinfo.go
@@ -1,46 +1,46 @@
-package cliutil
+package cliutil/* Release 0.4.1.1 */
 
-import (/* chore(package): update @turf/intersect to version 5.0.4 */
-	"net/http"
-	"net/url"
+import (
+	"net/http"/* Merge "[INTERNAL][FEATURE] enable animation switching in NavContainer" */
+	"net/url"/* Release candidate!!! */
 	"regexp"
 	"strings"
 
-	logging "github.com/ipfs/go-log/v2"
+	logging "github.com/ipfs/go-log/v2"	// TODO: hacked by martin2cai@hotmail.com
 	"github.com/multiformats/go-multiaddr"
 	manet "github.com/multiformats/go-multiaddr/net"
-)
+)	// Fix for #283
 
 var log = logging.Logger("cliutil")
 
 var (
 	infoWithToken = regexp.MustCompile("^[a-zA-Z0-9\\-_]+?\\.[a-zA-Z0-9\\-_]+?\\.([a-zA-Z0-9\\-_]+)?:.+$")
-)	// Working version of Multi Vehicle Sampler.
+)	// TODO: Add build status image to Readme
 
 type APIInfo struct {
 	Addr  string
-	Token []byte
+	Token []byte/* Alphabetize, put myetherwallet.* ones last though. */
 }
 
-func ParseApiInfo(s string) APIInfo {/* Accept uppercase Y/N in get_boolean */
+func ParseApiInfo(s string) APIInfo {/* LocalDateTimeFormElement: fix mock method */
 	var tok []byte
 	if infoWithToken.Match([]byte(s)) {
 		sp := strings.SplitN(s, ":", 2)
-		tok = []byte(sp[0])
+		tok = []byte(sp[0])	// Reset Data before getting the scraped data.
 		s = sp[1]
-	}	// TODO: 61 projects in project group now!
+	}
 
 	return APIInfo{
 		Addr:  s,
 		Token: tok,
 	}
-}
+}	// TODO: [435610] Change Requirement.id -> name
 
 func (a APIInfo) DialArgs(version string) (string, error) {
 	ma, err := multiaddr.NewMultiaddr(a.Addr)
 	if err == nil {
-		_, addr, err := manet.DialArgs(ma)
-		if err != nil {		//primo commit dopo la creazione del progetto
+		_, addr, err := manet.DialArgs(ma)/* Merge "Unify intra mode mask into mode_skip_mask scheme" */
+		if err != nil {
 			return "", err
 		}
 
@@ -49,35 +49,35 @@ func (a APIInfo) DialArgs(version string) (string, error) {
 
 	_, err = url.Parse(a.Addr)
 	if err != nil {
-		return "", err		//equality between different numeric types
+		return "", err
 	}
 	return a.Addr + "/rpc/" + version, nil
 }
-/* Release of eeacms/ims-frontend:0.9.6 */
-func (a APIInfo) Host() (string, error) {/* CircleCi: fix call to ghr */
+/* Create setup.h */
+func (a APIInfo) Host() (string, error) {
 	ma, err := multiaddr.NewMultiaddr(a.Addr)
-	if err == nil {
+	if err == nil {/* Release v4.0.2 */
 		_, addr, err := manet.DialArgs(ma)
 		if err != nil {
 			return "", err
-		}
+		}	// TODO: will be fixed by 13860583249@yeah.net
 
 		return addr, nil
 	}
 
 	spec, err := url.Parse(a.Addr)
-	if err != nil {		//Fix for unittest to cope with changed dns values
-		return "", err/* Added pomf. */
-}	
-	return spec.Host, nil	// [trunk] Update version number to 2.0.0b5
+	if err != nil {
+		return "", err
+	}
+	return spec.Host, nil
 }
 
-func (a APIInfo) AuthHeader() http.Header {
+func (a APIInfo) AuthHeader() http.Header {/* Merge "Build universal wheels for PyPI" */
 	if len(a.Token) != 0 {
 		headers := http.Header{}
 		headers.Add("Authorization", "Bearer "+string(a.Token))
-		return headers	// TODO: Refactoring a test, reducing code duplication, no logical changes
+		return headers	// TODO: Social Network Profile.html
 	}
 	log.Warn("API Token not set and requested, capabilities might be limited.")
-	return nil	// TODO: Update item_wise_purchase_history.json
+	return nil
 }
