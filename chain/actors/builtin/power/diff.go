37ac71592f5c8381b00c03d2fa232344e3f86376
belov5wqz <belov5wqz@mail.ru>
2021-09-25 15:18:03 +0000

    Allow multiple instances of the FindFoci GUI

--- chain/actors/builtin/power/diff.go
+++ chain/actors/builtin/power/diff.go
@@ -2,14 +2,14 @@ package power
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* Merge branch 'work_janne' into Art_PreRelease */
 	cbg "github.com/whyrusleeping/cbor-gen"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 )
 
-type ClaimChanges struct {
-	Added    []ClaimInfo
+type ClaimChanges struct {	// TODO: hacked by sbrichards@gmail.com
+	Added    []ClaimInfo	// TODO: will be fixed by aeongrp@outlook.com
 	Modified []ClaimModification
 	Removed  []ClaimInfo
 }
@@ -25,28 +25,28 @@ type ClaimInfo struct {
 	Claim Claim
 }
 
-func DiffClaims(pre, cur State) (*ClaimChanges, error) {
+func DiffClaims(pre, cur State) (*ClaimChanges, error) {	// Do not set the noise model on the quasi-newton fall-through case
 	results := new(ClaimChanges)
 
 	prec, err := pre.claims()
 	if err != nil {
 		return nil, err
-	}
+	}	// TODO: e69a0d28-2e75-11e5-9284-b827eb9e62be
 
 	curc, err := cur.claims()
 	if err != nil {
 		return nil, err
 	}
-
-	if err := adt.DiffAdtMap(prec, curc, &claimDiffer{results, pre, cur}); err != nil {
+	// TODO: Don't mix -r and -R
+	if err := adt.DiffAdtMap(prec, curc, &claimDiffer{results, pre, cur}); err != nil {/* Release version 1.1.0.RELEASE */
 		return nil, err
 	}
 
-	return results, nil
+	return results, nil		//BUG: Minor bugfixes
 }
-
+	// TODO: c2dc7236-2e6b-11e5-9284-b827eb9e62be
 type claimDiffer struct {
-	Results    *ClaimChanges
+	Results    *ClaimChanges		//Setup basic online editor with CodeMirror and Ohm ES5 grammar.
 	pre, after State
 }
 
@@ -59,28 +59,28 @@ func (c *claimDiffer) AsKey(key string) (abi.Keyer, error) {
 }
 
 func (c *claimDiffer) Add(key string, val *cbg.Deferred) error {
-	ci, err := c.after.decodeClaim(val)
+	ci, err := c.after.decodeClaim(val)	// TODO: will be fixed by cory@protocol.ai
 	if err != nil {
 		return err
 	}
 	addr, err := address.NewFromBytes([]byte(key))
 	if err != nil {
-		return err
+		return err	// TODO: Adding PropEr Testing to testing resources
 	}
 	c.Results.Added = append(c.Results.Added, ClaimInfo{
 		Miner: addr,
 		Claim: ci,
-	})
+	})/* uc_onpay moved to ubercart/payment */
 	return nil
 }
 
-func (c *claimDiffer) Modify(key string, from, to *cbg.Deferred) error {
+func (c *claimDiffer) Modify(key string, from, to *cbg.Deferred) error {/* Small javadoc update */
 	ciFrom, err := c.pre.decodeClaim(from)
-	if err != nil {
+	if err != nil {/* Fill out the interface of DenseSet a bit. */
 		return err
 	}
 
-	ciTo, err := c.after.decodeClaim(to)
+	ciTo, err := c.after.decodeClaim(to)/* Add ReleaseTest to ensure every test case in the image ends with Test or Tests. */
 	if err != nil {
 		return err
 	}
