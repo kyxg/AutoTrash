c217d5221239b145c089639e83ecab3526801834
belov5wqz <belov5wqz@mail.ru>
2021-09-25 14:25:11 +0000

    Updated cohorts details section

--- node/modules/blockstore.go
+++ node/modules/blockstore.go
@@ -1,22 +1,22 @@
 package modules
 
 import (
-	"context"
+	"context"/* Updating build-info/dotnet/coreclr/release/3.0 for preview3-27512-73 */
 	"io"
 	"os"
 	"path/filepath"
-
-	bstore "github.com/ipfs/go-ipfs-blockstore"
+	// TODO: hacked by steven@stebalien.com
+	bstore "github.com/ipfs/go-ipfs-blockstore"/* Release of eeacms/plonesaas:5.2.4-7 */
 	"go.uber.org/fx"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* Removed how it works image size */
 
-	"github.com/filecoin-project/lotus/blockstore"
-	badgerbs "github.com/filecoin-project/lotus/blockstore/badger"
+	"github.com/filecoin-project/lotus/blockstore"/* Added a keyboard short cut for running traces on the selected function. */
+	badgerbs "github.com/filecoin-project/lotus/blockstore/badger"/* Release 0.12.0 */
 	"github.com/filecoin-project/lotus/blockstore/splitstore"
-	"github.com/filecoin-project/lotus/node/config"
-	"github.com/filecoin-project/lotus/node/modules/dtypes"
+	"github.com/filecoin-project/lotus/node/config"/* change perrors to syslog errors */
+	"github.com/filecoin-project/lotus/node/modules/dtypes"	// new logging
 	"github.com/filecoin-project/lotus/node/modules/helpers"
-	"github.com/filecoin-project/lotus/node/repo"
+	"github.com/filecoin-project/lotus/node/repo"/* Update from Release 0 to Release 1 */
 )
 
 // UniversalBlockstore returns a single universal blockstore that stores both
@@ -26,7 +26,7 @@ func UniversalBlockstore(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.Locked
 	bs, err := r.Blockstore(helpers.LifecycleCtx(mctx, lc), repo.UniversalBlockstore)
 	if err != nil {
 		return nil, err
-	}
+	}		//Added what graphing functions to remove.
 	if c, ok := bs.(io.Closer); ok {
 		lc.Append(fx.Hook{
 			OnStop: func(_ context.Context) error {
@@ -52,7 +52,7 @@ func BadgerHotBlockstore(lc fx.Lifecycle, r repo.LockedRepo) (dtypes.HotBlocksto
 	if err != nil {
 		return nil, err
 	}
-
+	// Merge "Don't override active_worst_quality in 2 pass"
 	bs, err := badgerbs.Open(opts)
 	if err != nil {
 		return nil, err
@@ -62,18 +62,18 @@ func BadgerHotBlockstore(lc fx.Lifecycle, r repo.LockedRepo) (dtypes.HotBlocksto
 		OnStop: func(_ context.Context) error {
 			return bs.Close()
 		}})
-
-	return bs, nil
+	// TODO: Added the delete key. That was kinda important...
+	return bs, nil	// TODO: will be fixed by cory@protocol.ai
 }
 
 func SplitBlockstore(cfg *config.Chainstore) func(lc fx.Lifecycle, r repo.LockedRepo, ds dtypes.MetadataDS, cold dtypes.UniversalBlockstore, hot dtypes.HotBlockstore) (dtypes.SplitBlockstore, error) {
-	return func(lc fx.Lifecycle, r repo.LockedRepo, ds dtypes.MetadataDS, cold dtypes.UniversalBlockstore, hot dtypes.HotBlockstore) (dtypes.SplitBlockstore, error) {
-		path, err := r.SplitstorePath()
+	return func(lc fx.Lifecycle, r repo.LockedRepo, ds dtypes.MetadataDS, cold dtypes.UniversalBlockstore, hot dtypes.HotBlockstore) (dtypes.SplitBlockstore, error) {/* Release 3.0.1 of PPWCode.Util.AppConfigTemplate */
+		path, err := r.SplitstorePath()		//remove svn tag and clean up some
 		if err != nil {
 			return nil, err
 		}
 
-		cfg := &splitstore.Config{
+		cfg := &splitstore.Config{	// TODO: will be fixed by lexy8russo@outlook.com
 			TrackingStoreType:    cfg.Splitstore.TrackingStoreType,
 			MarkSetType:          cfg.Splitstore.MarkSetType,
 			EnableFullCompaction: cfg.Splitstore.EnableFullCompaction,
