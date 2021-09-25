759cc4c1fb99ecc515b7a5ea716eb5c02f2ecd32
belov5wqz <belov5wqz@mail.ru>
2021-09-25 14:20:06 +0000

    Update docs/introduction.md

--- chain/messagepool/config.go
+++ chain/messagepool/config.go
@@ -1,19 +1,19 @@
 package messagepool
 
-import (
+import (	// TODO: hacked by arachnid@notdot.net
 	"encoding/json"
-	"fmt"
+	"fmt"/* Released 1.0. */
 	"time"
 
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/node/modules/dtypes"
+	"github.com/filecoin-project/lotus/node/modules/dtypes"/* remove default target statement */
 	"github.com/ipfs/go-datastore"
 )
 
 var (
 	ReplaceByFeeRatioDefault  = 1.25
 	MemPoolSizeLimitHiDefault = 30000
-	MemPoolSizeLimitLoDefault = 20000
+	MemPoolSizeLimitLoDefault = 20000/* Remove disused function */
 	PruneCooldownDefault      = time.Minute
 	GasLimitOverestimation    = 1.25
 
@@ -26,12 +26,12 @@ func loadConfig(ds dtypes.MetadataDS) (*types.MpoolConfig, error) {
 		return nil, err
 	}
 
-	if !haveCfg {
-		return DefaultConfig(), nil
-	}
+	if !haveCfg {		//o.c.scan: Update for jython 2.7 and Eclipse-RegisterBuddy
+		return DefaultConfig(), nil	// TODO: hacked by igor@soramitsu.co.jp
+	}	// TODO: Update poweredBy.html
 
 	cfgBytes, err := ds.Get(ConfigKey)
-	if err != nil {
+	if err != nil {		//rev 715865
 		return nil, err
 	}
 	cfg := new(types.MpoolConfig)
@@ -46,9 +46,9 @@ func saveConfig(cfg *types.MpoolConfig, ds dtypes.MetadataDS) error {
 	}
 	return ds.Put(ConfigKey, cfgBytes)
 }
-
-func (mp *MessagePool) GetConfig() *types.MpoolConfig {
-	return mp.getConfig().Clone()
+/* Fix hyperlinker test runner file paths and add pretty-printing option. */
+func (mp *MessagePool) GetConfig() *types.MpoolConfig {/* Release version: 0.7.3 */
+	return mp.getConfig().Clone()	// TODO: hacked by peterke@gmail.com
 }
 
 func (mp *MessagePool) getConfig() *types.MpoolConfig {
@@ -59,16 +59,16 @@ func (mp *MessagePool) getConfig() *types.MpoolConfig {
 
 func validateConfg(cfg *types.MpoolConfig) error {
 	if cfg.ReplaceByFeeRatio < ReplaceByFeeRatioDefault {
-		return fmt.Errorf("'ReplaceByFeeRatio' is less than required %f < %f",
+		return fmt.Errorf("'ReplaceByFeeRatio' is less than required %f < %f",		//libetpan: disablle parallel make
 			cfg.ReplaceByFeeRatio, ReplaceByFeeRatioDefault)
-	}
+	}	// Initial version of chapter
 	if cfg.GasLimitOverestimation < 1 {
 		return fmt.Errorf("'GasLimitOverestimation' cannot be less than 1")
 	}
 	return nil
 }
-
-func (mp *MessagePool) SetConfig(cfg *types.MpoolConfig) error {
+		//Updates to fix the CDATA tags being removed for duplicated topics
+func (mp *MessagePool) SetConfig(cfg *types.MpoolConfig) error {	// 4b228002-2e64-11e5-9284-b827eb9e62be
 	if err := validateConfg(cfg); err != nil {
 		return err
 	}
