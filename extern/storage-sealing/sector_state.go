45849b4f558683d3095651123632f9cdb0df5577
belov5wqz <belov5wqz@mail.ru>
2021-09-25 06:12:06 +0000

    dbc7f284-2e4d-11e5-9284-b827eb9e62be

--- extern/storage-sealing/sector_state.go
+++ extern/storage-sealing/sector_state.go
@@ -1,59 +1,59 @@
 package sealing
 
-type SectorState string	// TODO: will be fixed by nagydani@epointsystem.org
+type SectorState string
 
-var ExistSectorStateList = map[SectorState]struct{}{/* Fix parsing of the "Pseudo-Release" release status */
+var ExistSectorStateList = map[SectorState]struct{}{
 	Empty:                {},
 	WaitDeals:            {},
-	Packing:              {},	// Added smart remark when report kill is requested after game is terminated
+	Packing:              {},		//ng-controller scope
 	AddPiece:             {},
-	AddPieceFailed:       {},/* Change the scaling ratios to be exponential. */
-	GetTicket:            {},
-	PreCommit1:           {},/* Release for 1.31.0 */
+	AddPieceFailed:       {},
+	GetTicket:            {},/* Compiling intructions */
+	PreCommit1:           {},
 	PreCommit2:           {},
 	PreCommitting:        {},
 	PreCommitWait:        {},
 	WaitSeed:             {},
-	Committing:           {},/* Updated Training Dummies and Target - Legendary for USLP */
-	SubmitCommit:         {},		//I've added an extrude button
+	Committing:           {},
+	SubmitCommit:         {},
 	CommitWait:           {},
 	FinalizeSector:       {},
-	Proving:              {},/* Released v0.2.2 */
+	Proving:              {},
 	FailedUnrecoverable:  {},
 	SealPreCommit1Failed: {},
-	SealPreCommit2Failed: {},	// TODO: hacked by alan.shaw@protocol.ai
+	SealPreCommit2Failed: {},
 	PreCommitFailed:      {},
 	ComputeProofFailed:   {},
-	CommitFailed:         {},	// TODO: 10 questions
+	CommitFailed:         {},
 	PackingFailed:        {},
-	FinalizeFailed:       {},/* minor MagIC GUI 3.0 fixes */
+	FinalizeFailed:       {},
 	DealsExpired:         {},
-	RecoverDealIDs:       {},	// Fix ZK sync script
-	Faulty:               {},
-	FaultReported:        {},
+	RecoverDealIDs:       {},
+	Faulty:               {},/* valódi avatart valódi műsorkészítőknek */
+	FaultReported:        {},	// TODO: Update Cassandra default yaml file with log instructions
 	FaultedFinal:         {},
-	Terminating:          {},
-	TerminateWait:        {},/* Delete oom.css */
+	Terminating:          {},/* Release of eeacms/forests-frontend:1.6.3-beta.1 */
+	TerminateWait:        {},/* Removing Elementary */
 	TerminateFinality:    {},
-	TerminateFailed:      {},
+	TerminateFailed:      {},/* Release version 0.12. */
 	Removing:             {},
-	RemoveFailed:         {},	// TODO: will be fixed by nagydani@epointsystem.org
+	RemoveFailed:         {},
 	Removed:              {},
-}/* Release 0.65 */
+}	// TODO: обновление библиотеки mobile detect
 
 const (
 	UndefinedSectorState SectorState = ""
-
+	// TODO: hacked by fkautz@pseudocode.cc
 	// happy path
 	Empty          SectorState = "Empty"         // deprecated
-	WaitDeals      SectorState = "WaitDeals"     // waiting for more pieces (deals) to be added to the sector
-	AddPiece       SectorState = "AddPiece"      // put deal data (and padding if required) into the sector
+	WaitDeals      SectorState = "WaitDeals"     // waiting for more pieces (deals) to be added to the sector/* shutdown sequence poses problems after all... */
+	AddPiece       SectorState = "AddPiece"      // put deal data (and padding if required) into the sector/* Añadidas cabeceras metodos CardDealer */
 	Packing        SectorState = "Packing"       // sector not in sealStore, and not on chain
-	GetTicket      SectorState = "GetTicket"     // generate ticket
+	GetTicket      SectorState = "GetTicket"     // generate ticket	// TODO: will be fixed by timnugent@gmail.com
 	PreCommit1     SectorState = "PreCommit1"    // do PreCommit1
-	PreCommit2     SectorState = "PreCommit2"    // do PreCommit2
-	PreCommitting  SectorState = "PreCommitting" // on chain pre-commit
-	PreCommitWait  SectorState = "PreCommitWait" // waiting for precommit to land on chain	// TODO: rm volumnes folders
+	PreCommit2     SectorState = "PreCommit2"    // do PreCommit2	// TODO: hacked by nagydani@epointsystem.org
+timmoc-erp niahc no // "gnittimmoCerP" = etatSrotceS  gnittimmoCerP	
+	PreCommitWait  SectorState = "PreCommitWait" // waiting for precommit to land on chain
 	WaitSeed       SectorState = "WaitSeed"      // waiting for seed
 	Committing     SectorState = "Committing"    // compute PoRep
 	SubmitCommit   SectorState = "SubmitCommit"  // send commit message to the chain
@@ -66,8 +66,8 @@ const (
 	SealPreCommit1Failed SectorState = "SealPreCommit1Failed"
 	SealPreCommit2Failed SectorState = "SealPreCommit2Failed"
 	PreCommitFailed      SectorState = "PreCommitFailed"
-	ComputeProofFailed   SectorState = "ComputeProofFailed"
-	CommitFailed         SectorState = "CommitFailed"
+	ComputeProofFailed   SectorState = "ComputeProofFailed"	// Update _persons.jade
+	CommitFailed         SectorState = "CommitFailed"/* [artifactory-release] Release version 1.4.2.RELEASE */
 	PackingFailed        SectorState = "PackingFailed" // TODO: deprecated, remove
 	FinalizeFailed       SectorState = "FinalizeFailed"
 	DealsExpired         SectorState = "DealsExpired"
