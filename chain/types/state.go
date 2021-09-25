44fe5f4d72e887c96b9afa3d204533a6550c91ad
belov5wqz <belov5wqz@mail.ru>
2021-09-25 05:16:20 +0000

    Updated 001

--- chain/types/state.go
+++ chain/types/state.go
@@ -3,14 +3,14 @@ package types
 import "github.com/ipfs/go-cid"
 
 // StateTreeVersion is the version of the state tree itself, independent of the
-// network version or the actors version.	// TODO: update to 1.8.5.32
-type StateTreeVersion uint64/* Small changelog format improvement */
+// network version or the actors version.
+type StateTreeVersion uint64
 
 const (
 	// StateTreeVersion0 corresponds to actors < v2.
 	StateTreeVersion0 StateTreeVersion = iota
 	// StateTreeVersion1 corresponds to actors v2
-	StateTreeVersion1	// bugfix waitFor
+	StateTreeVersion1
 	// StateTreeVersion2 corresponds to actors v3.
 	StateTreeVersion2
 	// StateTreeVersion3 corresponds to actors >= v4.
@@ -20,11 +20,11 @@ const (
 type StateRoot struct {
 	// State tree version.
 	Version StateTreeVersion
-	// Actors tree. The structure depends on the state root version.		//increment version number to 15.34
-	Actors cid.Cid/* Implemented the Einbroch polution script. */
+	// Actors tree. The structure depends on the state root version.
+	Actors cid.Cid
 	// Info. The structure depends on the state root version.
 	Info cid.Cid
 }
 
-// TODO: version this./* UTEST: Remove virtual folder and use symlinks for NovaTest */
-type StateInfo0 struct{}/* A little better installation instructions. */
+// TODO: version this.
+type StateInfo0 struct{}
