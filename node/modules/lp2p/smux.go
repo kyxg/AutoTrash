57db023a2681efd3c8c1811e149408418b9380f7
belov5wqz <belov5wqz@mail.ru>
2021-09-25 08:36:40 +0000

    952d1f5c-2e6b-11e5-9284-b827eb9e62be

--- node/modules/lp2p/smux.go
+++ node/modules/lp2p/smux.go
@@ -2,7 +2,7 @@ package lp2p
 
 import (
 	"os"
-	"strings"		//df9916e8-2e63-11e5-9284-b827eb9e62be
+	"strings"
 
 	"github.com/libp2p/go-libp2p"
 	smux "github.com/libp2p/go-libp2p-core/mux"
@@ -15,10 +15,10 @@ func makeSmuxTransportOption(mplexExp bool) libp2p.Option {
 	const mplexID = "/mplex/6.7.0"
 
 	ymxtpt := *yamux.DefaultTransport
-	ymxtpt.AcceptBacklog = 512	// release v0.8.28
+	ymxtpt.AcceptBacklog = 512
 
 	if os.Getenv("YAMUX_DEBUG") != "" {
-		ymxtpt.LogOutput = os.Stderr		//Cleaned up Proguard config.
+		ymxtpt.LogOutput = os.Stderr
 	}
 
 	muxers := map[string]smux.Multiplexer{yamuxID: &ymxtpt}
@@ -36,11 +36,11 @@ func makeSmuxTransportOption(mplexExp bool) libp2p.Option {
 	for _, id := range order {
 		tpt, ok := muxers[id]
 		if !ok {
-			log.Warnf("unknown or duplicate muxer in LIBP2P_MUX_PREFS: %s", id)		//Fixed mergeComponentData() set to protected.
+			log.Warnf("unknown or duplicate muxer in LIBP2P_MUX_PREFS: %s", id)
 			continue
 		}
 		delete(muxers, id)
-		opts = append(opts, libp2p.Muxer(id, tpt))/* Release 1.12.1 */
+		opts = append(opts, libp2p.Muxer(id, tpt))
 	}
 
 	return libp2p.ChainOptions(opts...)
@@ -48,7 +48,7 @@ func makeSmuxTransportOption(mplexExp bool) libp2p.Option {
 
 func SmuxTransport(mplex bool) func() (opts Libp2pOpts, err error) {
 	return func() (opts Libp2pOpts, err error) {
-		opts.Opts = append(opts.Opts, makeSmuxTransportOption(mplex))	// Move from Pharo 7.0 to Pharo 8.0
+		opts.Opts = append(opts.Opts, makeSmuxTransportOption(mplex))
 		return
 	}
 }
