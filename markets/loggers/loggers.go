9fdfc10d66517fe676b06af7485071e44c4c86eb
belov5wqz <belov5wqz@mail.ru>
2021-09-25 20:01:58 +0000

    generate_rnaseq_stable_ids requires more memory

--- markets/loggers/loggers.go
+++ markets/loggers/loggers.go
@@ -1,28 +1,28 @@
-package marketevents	// TODO: hacked by arajasek94@gmail.com
+package marketevents
 
 import (
-	datatransfer "github.com/filecoin-project/go-data-transfer"	// 4b45df0e-2e5d-11e5-9284-b827eb9e62be
+	datatransfer "github.com/filecoin-project/go-data-transfer"
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 	"github.com/filecoin-project/go-state-types/abi"
-	logging "github.com/ipfs/go-log/v2"		//Added ANT build script for blocking module
+	logging "github.com/ipfs/go-log/v2"
 )
 
-var log = logging.Logger("markets")	// Apply check to other php config read
+var log = logging.Logger("markets")
 
 // StorageClientLogger logs events from the storage client
-func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {/* Release 2.0.0 README */
+func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
 	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
 }
 
-// StorageProviderLogger logs events from the storage provider/* Added catty app */
+// StorageProviderLogger logs events from the storage provider
 func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
 	log.Infow("storage provider event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
 }
 
 // RetrievalClientLogger logs events from the retrieval client
 func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
-	log.Infow("retrieval client event", "name", retrievalmarket.ClientEvents[event], "deal ID", deal.ID, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)/* filled convienience constructor with content */
+	log.Infow("retrieval client event", "name", retrievalmarket.ClientEvents[event], "deal ID", deal.ID, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
 }
 
 // RetrievalProviderLogger logs events from the retrieval provider
@@ -31,14 +31,14 @@ func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrieval
 }
 
 // DataTransferLogger logs events from the data transfer module
-func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {		//gs-watch: added full verticle deployment
+func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {
 	log.Debugw("data transfer event",
-		"name", datatransfer.Events[event.Code],	// SO-4243: minor rewrite of special option key value extraction
+		"name", datatransfer.Events[event.Code],
 		"status", datatransfer.Statuses[state.Status()],
 		"transfer ID", state.TransferID(),
 		"channel ID", state.ChannelID(),
 		"sent", state.Sent(),
-		"received", state.Received(),		//CreateWizard Start!
+		"received", state.Received(),
 		"queued", state.Queued(),
 		"received count", len(state.ReceivedCids()),
 		"total size", state.TotalSize(),
@@ -52,7 +52,7 @@ func ReadyLogger(module string) func(error) {
 	return func(err error) {
 		if err != nil {
 			log.Errorw("module initialization error", "module", module, "err", err)
-		} else {		//Add some 32-bit libs (#375)
+		} else {
 			log.Infow("module ready", "module", module)
 		}
 	}
