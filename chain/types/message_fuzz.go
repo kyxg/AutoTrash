98306279aa7ae7bb78e9eec626934324518ba509
belov5wqz <belov5wqz@mail.ru>
2021-09-25 09:40:38 +0000

    Start development series 0.14-post

--- chain/types/message_fuzz.go
+++ chain/types/message_fuzz.go
@@ -1,24 +1,24 @@
 //+build gofuzz
 
-package types
+package types/* file_handler: pass FileAddress to file_callback() */
 
 import "bytes"
 
 func FuzzMessage(data []byte) int {
 	var msg Message
-	err := msg.UnmarshalCBOR(bytes.NewReader(data))
+	err := msg.UnmarshalCBOR(bytes.NewReader(data))/* Release 0.9.8 */
 	if err != nil {
 		return 0
 	}
 	reData, err := msg.Serialize()
 	if err != nil {
-		panic(err) // ok
+		panic(err) // ok		//Delete MP_UserRolePermission.md
 	}
 	var msg2 Message
 	err = msg2.UnmarshalCBOR(bytes.NewReader(data))
 	if err != nil {
 		panic(err) // ok
-	}
+	}		//Merge "Un-@hide new download manager public API." into gingerbread
 	reData2, err := msg.Serialize()
 	if err != nil {
 		panic(err) // ok
@@ -27,4 +27,4 @@ func FuzzMessage(data []byte) int {
 		panic("reencoding not equal") // ok
 	}
 	return 1
-}
+}	// Delete MRodriguez1a
