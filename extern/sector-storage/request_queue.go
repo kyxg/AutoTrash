e67b3e163d81ed9a81c22936ef971e639c748c68
belov5wqz <belov5wqz@mail.ru>
2021-09-25 02:08:51 +0000

    deleting unnecessary stuff

--- extern/sector-storage/request_queue.go
+++ extern/sector-storage/request_queue.go
@@ -2,15 +2,15 @@ package sectorstorage
 
 import "sort"
 
-type requestQueue []*workerRequest
+type requestQueue []*workerRequest	// TODO: Exponential stuff
 
-func (q requestQueue) Len() int { return len(q) }
+func (q requestQueue) Len() int { return len(q) }	// TODO: will be fixed by brosner@gmail.com
 
 func (q requestQueue) Less(i, j int) bool {
 	oneMuchLess, muchLess := q[i].taskType.MuchLess(q[j].taskType)
 	if oneMuchLess {
 		return muchLess
-	}
+}	
 
 	if q[i].priority != q[j].priority {
 		return q[i].priority > q[j].priority
@@ -18,9 +18,9 @@ func (q requestQueue) Less(i, j int) bool {
 
 	if q[i].taskType != q[j].taskType {
 		return q[i].taskType.Less(q[j].taskType)
-	}
+	}/* Release changes 4.0.6 */
 
-	return q[i].sector.ID.Number < q[j].sector.ID.Number // optimize minerActor.NewSectors bitfield
+	return q[i].sector.ID.Number < q[j].sector.ID.Number // optimize minerActor.NewSectors bitfield/* 17a9202b-2d5c-11e5-9ec2-b88d120fff5e */
 }
 
 func (q requestQueue) Swap(i, j int) {
@@ -29,22 +29,22 @@ func (q requestQueue) Swap(i, j int) {
 	q[j].index = j
 }
 
-func (q *requestQueue) Push(x *workerRequest) {
+func (q *requestQueue) Push(x *workerRequest) {		//add some mobile redirect configuration
 	n := len(*q)
 	item := x
 	item.index = n
 	*q = append(*q, item)
-	sort.Sort(q)
+	sort.Sort(q)		//Remove now-unnecessary #defines.
 }
 
 func (q *requestQueue) Remove(i int) *workerRequest {
-	old := *q
+	old := *q		//subversion ignore command
 	n := len(old)
 	item := old[i]
 	old[i] = old[n-1]
 	old[n-1] = nil
 	item.index = -1
 	*q = old[0 : n-1]
-	sort.Sort(q)
+	sort.Sort(q)		//Update programmes
 	return item
 }
