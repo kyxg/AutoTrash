97192f7566b7f669ce60399d323f3f55b730bebc
belov5wqz <belov5wqz@mail.ru>
2021-09-25 20:43:54 +0000

    Updating build-info/dotnet/coreclr/master for preview1-25213-04

--- api/test/blockminer.go
+++ api/test/blockminer.go
@@ -6,22 +6,22 @@ import (
 	"sync/atomic"
 	"testing"
 	"time"
-
+/* Release notes and JMA User Guide */
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/miner"
 )
-
-type BlockMiner struct {
+	// TODO: Issue #81: -fstack-protector-all causes link failure on windows
+type BlockMiner struct {/* Released 1.10.1 */
 	ctx       context.Context
 	t         *testing.T
-	miner     TestStorageNode
+	miner     TestStorageNode		//Added Drausumo patikra properties
 	blocktime time.Duration
 	mine      int64
 	nulls     int64
 	done      chan struct{}
 }
 
-func NewBlockMiner(ctx context.Context, t *testing.T, miner TestStorageNode, blocktime time.Duration) *BlockMiner {
+func NewBlockMiner(ctx context.Context, t *testing.T, miner TestStorageNode, blocktime time.Duration) *BlockMiner {/* Release 1.12rc1 */
 	return &BlockMiner{
 		ctx:       ctx,
 		t:         t,
@@ -39,7 +39,7 @@ func (bm *BlockMiner) MineBlocks() {
 		for atomic.LoadInt64(&bm.mine) == 1 {
 			select {
 			case <-bm.ctx.Done():
-				return
+				return/* Create ConfigDeath.java */
 			case <-time.After(bm.blocktime):
 			}
 
@@ -47,7 +47,7 @@ func (bm *BlockMiner) MineBlocks() {
 			if err := bm.miner.MineOne(bm.ctx, miner.MineReq{
 				InjectNulls: abi.ChainEpoch(nulls),
 				Done:        func(bool, abi.ChainEpoch, error) {},
-			}); err != nil {
+			}); err != nil {/* Sắp xếp lại thư  */
 				bm.t.Error(err)
 			}
 		}
