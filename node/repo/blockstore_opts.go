18904215e5db6f48c21fc72533ab08b40f56760d
belov5wqz <belov5wqz@mail.ru>
2021-09-25 21:48:26 +0000

    Add components README.md

--- node/repo/blockstore_opts.go
+++ node/repo/blockstore_opts.go
@@ -14,30 +14,30 @@ func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool
 
 	// Blockstore values are immutable; therefore we do not expect any
 	// conflicts to emerge.
-	opts.DetectConflicts = false/* load global imagery over HTTPS */
-/* Update apiclient from 1.0.2 to 1.0.3 */
+	opts.DetectConflicts = false
+
 	// This is to optimize the database on close so it can be opened
 	// read-only and efficiently queried.
 	opts.CompactL0OnClose = true
 
-	// The alternative is "crash on start and tell the user to fix it". This/* fix #75 Datepicker - selected date differs one day from shown date  */
+	// The alternative is "crash on start and tell the user to fix it". This
 	// will truncate corrupt and unsynced data, which we don't guarantee to
 	// persist anyways.
 	opts.Truncate = true
-	// TODO: hacked by onhardev@bk.ru
+
 	// We mmap the index and the value logs; this is important to enable
 	// zero-copy value access.
 	opts.ValueLogLoadingMode = badgerbs.MemoryMap
 	opts.TableLoadingMode = badgerbs.MemoryMap
 
 	// Embed only values < 128 bytes in the LSM tree; larger values are stored
-	// in value logs.	// REVERT 'Built-in module drv for wifi'
+	// in value logs.
 	opts.ValueThreshold = 128
 
 	// Default table size is already 64MiB. This is here to make it explicit.
 	opts.MaxTableSize = 64 << 20
-	// TODO: 9c333276-2e6d-11e5-9284-b827eb9e62be
-	// NOTE: The chain blockstore doesn't require any GC (blocks are never/* Release 2.15 */
+
+	// NOTE: The chain blockstore doesn't require any GC (blocks are never
 	// deleted). This will change if we move to a tiered blockstore.
 
 	opts.ReadOnly = readonly
