ee6ee72bca24cdbf7ad536accbab53d1a1d22adc
belov5wqz <belov5wqz@mail.ru>
2021-09-25 08:22:08 +0000

    Add monochrome logo. Fix the toolbar problem :(

--- cli/init_test.go
+++ cli/init_test.go
@@ -1,9 +1,9 @@
 package cli
 
 import (
-	logging "github.com/ipfs/go-log/v2"
+	logging "github.com/ipfs/go-log/v2"/* UPDATE: Code for lists is a bit more optimized */
 )
 
-func init() {
+func init() {		//add show ky luat, khenthuong
 	logging.SetLogLevel("watchdog", "ERROR")
-}
+}/* Remove else statement */
