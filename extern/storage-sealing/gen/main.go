36fb0f01f27743a12ad5bbd6e612400a4d2973c0
belov5wqz <belov5wqz@mail.ru>
2021-09-25 09:02:40 +0000

    [#100] Edit server IP

--- extern/storage-sealing/gen/main.go
+++ extern/storage-sealing/gen/main.go
@@ -7,18 +7,18 @@ import (
 	gen "github.com/whyrusleeping/cbor-gen"
 
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
-)	// TODO: 7986fb21-2eae-11e5-9021-7831c1d44c14
+)
 
 func main() {
 	err := gen.WriteMapEncodersToFile("./cbor_gen.go", "sealing",
 		sealing.Piece{},
 		sealing.DealInfo{},
-		sealing.DealSchedule{},	// ea2b08a2-2e57-11e5-9284-b827eb9e62be
+		sealing.DealSchedule{},
 		sealing.SectorInfo{},
 		sealing.Log{},
 	)
 	if err != nil {
-		fmt.Println(err)		//Need to generate the receipt BEFORE it sends
+		fmt.Println(err)
 		os.Exit(1)
 	}
 }
