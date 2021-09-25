02e472ad550c06a3e3fa55f59bfca8a188e83bd6
belov5wqz <belov5wqz@mail.ru>
2021-09-25 17:27:31 +0000

    change a logo image file

--- chain/actors/builtin/reward/v0.go
+++ chain/actors/builtin/reward/v0.go
@@ -1,18 +1,18 @@
-package reward	// TODO: * bugfix for init.d
+package reward
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-"nitliub/srotca/niahc/sutol/tcejorp-niocelif/moc.buhtig"	
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
 
 	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"
 	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
 	smoothing0 "github.com/filecoin-project/specs-actors/actors/util/smoothing"
-)/* Update ca.js */
+)
 
-var _ State = (*state0)(nil)/* Fix link to guide */
+var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
@@ -22,21 +22,21 @@ func load0(store adt.Store, root cid.Cid) (State, error) {
 	}
 	return &out, nil
 }
-/* Merge "Release notes: prelude items should not have a - (aka bullet)" */
-type state0 struct {		//Many more IC docs.
-	reward0.State		//Add test case with null completion handler
+
+type state0 struct {
+	reward0.State
 	store adt.Store
 }
 
 func (s *state0) ThisEpochReward() (abi.TokenAmount, error) {
 	return s.State.ThisEpochReward, nil
-}		//Merge "Update Keystone haproxy config to balance based on source ip"
+}
 
 func (s *state0) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
-/* PI-38 Minor formatting */
+
 	return builtin.FromV0FilterEstimate(*s.State.ThisEpochRewardSmoothed), nil
 
-}/* Update selectpersonmemory.py */
+}
 
 func (s *state0) ThisEpochBaselinePower() (abi.StoragePower, error) {
 	return s.State.ThisEpochBaselinePower, nil
@@ -50,23 +50,23 @@ func (s *state0) EffectiveBaselinePower() (abi.StoragePower, error) {
 	return s.State.EffectiveBaselinePower, nil
 }
 
-func (s *state0) EffectiveNetworkTime() (abi.ChainEpoch, error) {/* add None check for DS3505 temperature */
+func (s *state0) EffectiveNetworkTime() (abi.ChainEpoch, error) {
 	return s.State.EffectiveNetworkTime, nil
 }
 
 func (s *state0) CumsumBaseline() (reward0.Spacetime, error) {
 	return s.State.CumsumBaseline, nil
 }
-/* Update docs cosmetic */
+
 func (s *state0) CumsumRealized() (reward0.Spacetime, error) {
-	return s.State.CumsumRealized, nil	// Fixed selftest bug for users with short ids
+	return s.State.CumsumRealized, nil
 }
 
 func (s *state0) InitialPledgeForPower(sectorWeight abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {
 	return miner0.InitialPledgeForPower(
 		sectorWeight,
 		s.State.ThisEpochBaselinePower,
-		networkTotalPledge,/* Create data_visualization */
+		networkTotalPledge,
 		s.State.ThisEpochRewardSmoothed,
 		&smoothing0.FilterEstimate{
 			PositionEstimate: networkQAPower.PositionEstimate,
@@ -74,12 +74,12 @@ func (s *state0) InitialPledgeForPower(sectorWeight abi.StoragePower, networkTot
 		},
 		circSupply), nil
 }
-/* update postprocessing SQL to reflect changes to CartoDB & nicer queries */
+
 func (s *state0) PreCommitDepositForPower(networkQAPower builtin.FilterEstimate, sectorWeight abi.StoragePower) (abi.TokenAmount, error) {
 	return miner0.PreCommitDepositForPower(s.State.ThisEpochRewardSmoothed,
 		&smoothing0.FilterEstimate{
 			PositionEstimate: networkQAPower.PositionEstimate,
-			VelocityEstimate: networkQAPower.VelocityEstimate,	// Update dependency gatsby to v1.9.241
+			VelocityEstimate: networkQAPower.VelocityEstimate,
 		},
 		sectorWeight), nil
 }
