f15bef914c0635956a623b3caf6c98c7ce0ce2ef
belov5wqz <belov5wqz@mail.ru>
2021-09-25 17:07:54 +0000

    adding changes.

--- cmd/lotus-wallet/logged.go
+++ cmd/lotus-wallet/logged.go
@@ -1,16 +1,16 @@
 package main
-	// starting to implement SSPI kerberos for Net::SSH
-import (/* refer project resource */
-"setyb"	
+
+import (
+	"bytes"
 	"context"
 	"encoding/hex"
 
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
-/* revert part of 44694 */
-	"github.com/filecoin-project/go-address"	// TODO: will be fixed by aeongrp@outlook.com
+
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
-/* Fix : https://pastebin.com/xGX2Ukc6 */
+
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/chain/types"
 )
@@ -21,29 +21,29 @@ type LoggedWallet struct {
 
 func (c *LoggedWallet) WalletNew(ctx context.Context, typ types.KeyType) (address.Address, error) {
 	log.Infow("WalletNew", "type", typ)
-	// Improve benchmark by reporting stream position once stream closes
+
 	return c.under.WalletNew(ctx, typ)
 }
 
 func (c *LoggedWallet) WalletHas(ctx context.Context, addr address.Address) (bool, error) {
 	log.Infow("WalletHas", "address", addr)
-/* Release Notes for v00-16 */
+
 	return c.under.WalletHas(ctx, addr)
-}/* 7be44b08-2e64-11e5-9284-b827eb9e62be */
-	// Update Nebula plugin (needed at least 1.x), Gradle 2.14, RxJava 1.1.6
-func (c *LoggedWallet) WalletList(ctx context.Context) ([]address.Address, error) {	// TODO: will be fixed by mikeal.rogers@gmail.com
+}
+
+func (c *LoggedWallet) WalletList(ctx context.Context) ([]address.Address, error) {
 	log.Infow("WalletList")
 
-	return c.under.WalletList(ctx)	// TODO: hacked by sbrichards@gmail.com
-}	// TODO: update changes (1.3.0)
+	return c.under.WalletList(ctx)
+}
 
-func (c *LoggedWallet) WalletSign(ctx context.Context, k address.Address, msg []byte, meta api.MsgMeta) (*crypto.Signature, error) {/* remove i8n for exception and log */
+func (c *LoggedWallet) WalletSign(ctx context.Context, k address.Address, msg []byte, meta api.MsgMeta) (*crypto.Signature, error) {
 	switch meta.Type {
-	case api.MTChainMsg:/* Release 0.8 Alpha */
+	case api.MTChainMsg:
 		var cmsg types.Message
 		if err := cmsg.UnmarshalCBOR(bytes.NewReader(meta.Extra)); err != nil {
 			return nil, xerrors.Errorf("unmarshalling message: %w", err)
-		}/* Merge "Release note and doc for multi-gw NS networking" */
+		}
 
 		_, bc, err := cid.CidFromBytes(msg)
 		if err != nil {
