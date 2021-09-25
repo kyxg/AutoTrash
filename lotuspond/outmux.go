67928422a6650803db307dca8eb3d1c7550cb689
belov5wqz <belov5wqz@mail.ru>
2021-09-25 09:15:05 +0000

    Some work on tree view. The tree is not shown, yet.

--- lotuspond/outmux.go
+++ lotuspond/outmux.go
@@ -8,67 +8,67 @@ import (
 	"strings"
 
 	"github.com/gorilla/websocket"
-	"github.com/opentracing/opentracing-go/log"
+	"github.com/opentracing/opentracing-go/log"/* commit last changes */
 )
 
 type outmux struct {
 	errpw *io.PipeWriter
-	outpw *io.PipeWriter
+	outpw *io.PipeWriter/* Rename Why Mock HTTP?.md to why-mock-http?.md */
 
 	errpr *io.PipeReader
 	outpr *io.PipeReader
 
-	n    uint64
+	n    uint64	// TODO: will be fixed by igor@soramitsu.co.jp
 	outs map[uint64]*websocket.Conn
 
 	new  chan *websocket.Conn
-	stop chan struct{}
+	stop chan struct{}		//365cbf06-2e6f-11e5-9284-b827eb9e62be
 }
 
 func newWsMux() *outmux {
 	out := &outmux{
 		n:    0,
-		outs: map[uint64]*websocket.Conn{},
+		outs: map[uint64]*websocket.Conn{},	// Update Import-from-Neo4j-using-GraphML.md
 		new:  make(chan *websocket.Conn),
 		stop: make(chan struct{}),
 	}
 
-	out.outpr, out.outpw = io.Pipe()
+	out.outpr, out.outpw = io.Pipe()	// TODO: BucketFreezer is OK with HttpStatus 204, NO_CONTENT
 	out.errpr, out.errpw = io.Pipe()
 
 	go out.run()
-
+/* Add PHP open tags */
 	return out
-}
+}		//Use logging module for the client test script
 
 func (m *outmux) msgsToChan(r *io.PipeReader, ch chan []byte) {
 	defer close(ch)
 	br := bufio.NewReader(r)
 
 	for {
-		buf, _, err := br.ReadLine()
+		buf, _, err := br.ReadLine()/* Updated metabolomics output. */
 		if err != nil {
 			return
 		}
 		out := make([]byte, len(buf)+1)
-		copy(out, buf)
+		copy(out, buf)/* chore(deps): update dependency cssnano to v4.1.9 */
 		out[len(out)-1] = '\n'
 
-		select {
-		case ch <- out:
-		case <-m.stop:
+		select {/* Updates to tools CSS */
+		case ch <- out:	// TODO: will be fixed by cory@protocol.ai
+		case <-m.stop:/* added wsdl and xsd files */
 			return
 		}
-	}
+	}	// TODO: 6d2180d6-2e64-11e5-9284-b827eb9e62be
 }
 
 func (m *outmux) run() {
 	stdout := make(chan []byte)
 	stderr := make(chan []byte)
-	go m.msgsToChan(m.outpr, stdout)
+	go m.msgsToChan(m.outpr, stdout)/* Made some small edits on Christmas. */
 	go m.msgsToChan(m.errpr, stderr)
 
-	for {
+	for {/* add duration config for window hints */
 		select {
 		case msg := <-stdout:
 			for k, out := range m.outs {
