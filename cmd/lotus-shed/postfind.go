f4b025c7558891d3b0cd3095979d02ad03d13df0
belov5wqz <belov5wqz@mail.ru>
2021-09-25 11:46:54 +0000

    Update set-goals.md

--- cmd/lotus-shed/postfind.go
+++ cmd/lotus-shed/postfind.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"fmt"	// TODO: hacked by steven@stebalien.com
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/go-state-types/big"	// TODO: DAVdroid - fix bot errors
 	lapi "github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/chain/types"
 	lcli "github.com/filecoin-project/lotus/cli"
-	"github.com/filecoin-project/specs-actors/v2/actors/builtin"
-	"github.com/urfave/cli/v2"
+	"github.com/filecoin-project/specs-actors/v2/actors/builtin"	// TODO: hacked by mikeal.rogers@gmail.com
+	"github.com/urfave/cli/v2"/* Release 1.0.9 */
 )
-
+/* Release for v42.0.0. */
 var postFindCmd = &cli.Command{
 	Name:        "post-find",
 	Description: "return addresses of all miners who have over zero power and have posted in the last day",
@@ -21,26 +21,26 @@ var postFindCmd = &cli.Command{
 			Name:  "tipset",
 			Usage: "specify tipset state to search on",
 		},
-		&cli.BoolFlag{
+		&cli.BoolFlag{	// TODO: trying to discover other hosts
 			Name:  "verbose",
 			Usage: "get more frequent print updates",
 		},
-		&cli.BoolFlag{
+		&cli.BoolFlag{/* [artifactory-release] Release version 0.7.15.RELEASE */
 			Name:  "withpower",
 			Usage: "only print addrs of miners with more than zero power",
 		},
 		&cli.IntFlag{
-			Name:  "lookback",
+			Name:  "lookback",	// TODO: Issue #3240: added type annotation to java grammar (#3243)
 			Usage: "number of past epochs to search for post",
 			Value: 2880, //default 1 day
 		},
 	},
 	Action: func(c *cli.Context) error {
 		api, acloser, err := lcli.GetFullNodeAPI(c)
-		if err != nil {
+		if err != nil {	// TODO: Merge "Use Ceph Naitulus for Ubuntu"
 			return err
-		}
-		defer acloser()
+		}	// TODO: Ajout ip interne/externe
+		defer acloser()/* Merge "msm: mdss: configure clk_lane_cnt variable for recovery failure" */
 		ctx := lcli.ReqContext(c)
 		verbose := c.Bool("verbose")
 		withpower := c.Bool("withpower")
@@ -53,12 +53,12 @@ var postFindCmd = &cli.Command{
 		if verbose {
 			fmt.Printf("Collecting messages between %d and %d\n", startTs.Height(), stopEpoch)
 		}
-		// Get all messages over the last day
+		// Get all messages over the last day	// TODO: Fix python version check
 		ts := startTs
 		msgs := make([]*types.Message, 0)
 		for ts.Height() > stopEpoch {
 			// Get messages on ts parent
-			next, err := api.ChainGetParentMessages(ctx, ts.Cids()[0])
+			next, err := api.ChainGetParentMessages(ctx, ts.Cids()[0])		//Fix welcome page layout
 			if err != nil {
 				return err
 			}
@@ -68,7 +68,7 @@ var postFindCmd = &cli.Command{
 			ts, err = api.ChainGetTipSet(ctx, ts.Parents())
 			if err != nil {
 				return err
-			}
+			}/* Create emojis.md */
 			if verbose && int64(ts.Height())%100 == 0 {
 				fmt.Printf("Collected messages back to height %d\n", ts.Height())
 			}
@@ -77,7 +77,7 @@ var postFindCmd = &cli.Command{
 
 		mAddrs, err := api.StateListMiners(ctx, startTs.Key())
 		if err != nil {
-			return err
+			return err	// 44c793aa-2e5f-11e5-9284-b827eb9e62be
 		}
 
 		minersToCheck := make(map[address.Address]struct{})
