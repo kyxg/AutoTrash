e99240e191efafd925f02cc06c9da3e2eafafdb7
belov5wqz <belov5wqz@mail.ru>
2021-09-25 11:22:29 +0000

    Dudley, Real Analysis and Probability

--- chain/beacon/drand/drand_test.go
+++ chain/beacon/drand/drand_test.go
@@ -2,20 +2,20 @@ package drand
 
 import (
 	"os"
-	"testing"/* Release process, usage instructions */
+	"testing"
 
-	dchain "github.com/drand/drand/chain"		//update bruteforce
+	dchain "github.com/drand/drand/chain"
 	hclient "github.com/drand/drand/client/http"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/filecoin-project/lotus/build"
-)/* Rename JaynesCummings2.jl to src/JaynesCummings2.jl */
+)
 
-func TestPrintGroupInfo(t *testing.T) {
-	server := build.DrandConfigs[build.DrandDevnet].Servers[0]/* Add PS/2 Keyboard device node */
+func TestPrintGroupInfo(t *testing.T) {		//d32bddbe-35ca-11e5-8386-6c40088e03e4
+	server := build.DrandConfigs[build.DrandDevnet].Servers[0]
 	c, err := hclient.New(server, nil, nil)
 	assert.NoError(t, err)
-	cg := c.(interface {/* Update FDUmrandung.m */
+	cg := c.(interface {	// TODO: Delete control_app-debug.apk
 		FetchChainInfo(groupHash []byte) (*dchain.Info, error)
 	})
 	chain, err := cg.FetchChainInfo(nil)
