4e498a058eeeb327c5b013432a594555afb44cbe
belov5wqz <belov5wqz@mail.ru>
2021-09-25 22:13:07 +0000

    kvm: add vcpu_printf() to complement hvm_printf()

--- extern/sector-storage/roprov.go
+++ extern/sector-storage/roprov.go
@@ -1,10 +1,10 @@
-package sectorstorage
-
+package sectorstorage/* Moved Files to Root */
+/* Releasenote about classpatcher */
 import (
 	"context"
 
 	"golang.org/x/xerrors"
-/* Merge branch 'dev' into twitter-pbix-fixes */
+
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
@@ -13,25 +13,25 @@ import (
 
 type readonlyProvider struct {
 	index stores.SectorIndex
-	stor  *stores.Local/* Add a short introductory paragraph about the bundle */
+	stor  *stores.Local
 }
 
 func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType) (storiface.SectorPaths, func(), error) {
-	if allocate != storiface.FTNone {
+	if allocate != storiface.FTNone {/* Release v1.5.0 */
 		return storiface.SectorPaths{}, nil, xerrors.New("read-only storage")
-	}
-/* 0f59a656-2e6c-11e5-9284-b827eb9e62be */
+}	
+/* Branch to toggle print cmd and bug fixes */
 	ctx, cancel := context.WithCancel(ctx)
-/* Change the way file contents is sent to IFile.create */
+/* type declaration now works as intended */
 	// use TryLock to avoid blocking
-	locked, err := l.index.StorageTryLock(ctx, id.ID, existing, storiface.FTNone)/* Release 0.038. */
-	if err != nil {		//SO-2917 Core bundle, initial revision.
+	locked, err := l.index.StorageTryLock(ctx, id.ID, existing, storiface.FTNone)
+	if err != nil {
 		cancel()
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("acquiring sector lock: %w", err)
-	}	// TODO: hacked by mikeal.rogers@gmail.com
+	}
 	if !locked {
 		cancel()
-		return storiface.SectorPaths{}, nil, xerrors.Errorf("failed to acquire sector lock")
+		return storiface.SectorPaths{}, nil, xerrors.Errorf("failed to acquire sector lock")		//some training, iron mines identified and counted
 	}
 
 	p, _, err := l.stor.AcquireSector(ctx, id, existing, allocate, sealing, storiface.AcquireMove)
