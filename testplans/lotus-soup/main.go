8d7170056f8909edad7b9e0a082d9e78ae3ebbec
belov5wqz <belov5wqz@mail.ru>
2021-09-25 00:22:19 +0000

    3eb4e624-2e47-11e5-9284-b827eb9e62be

--- testplans/lotus-soup/main.go
+++ testplans/lotus-soup/main.go
@@ -1,24 +1,24 @@
-package main/* Configuration instructions inserted in README */
-/* Update Release Notes for JIRA step */
-import (
+package main	// TODO: Fix #263 and #260. Support knime.workflow in Creator node
+
+import (		//Concepts for integrating mongoadmin added
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/paych"
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"
-	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"	// TODO: will be fixed by steven@stebalien.com
 
-	"github.com/testground/sdk-go/run"
-)/* Fixes for the volunteer account process */
+	"github.com/testground/sdk-go/run"	// Merge "[INTERNAL][FIX] Toolbar test page: Minor adjustments"
+)
 
-var cases = map[string]interface{}{
-	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),
+var cases = map[string]interface{}{/* include future improvements */
+	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),	// TODO: Made Russian translation for installer
 	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),
-	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),	// Delete Molybdenum.txt
-	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),		//Create statistics.r
+	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),	// TODO: [MAJ] GalleryUrlBuilder.class.php
+	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),
 	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),
-	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),		//Update i2cdemo.vhd
-}/* Change repository address */
+	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),
+}
 
 func main() {
 	sanityCheck()
-
+	// Unpublish page.
 	run.InvokeMap(cases)
-}/* Clingcon: bugfix in normalizing linear constraints */
+}
