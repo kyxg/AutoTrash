6d07d9212839c22b2638a0220704cb2824d9b28e
belov5wqz <belov5wqz@mail.ru>
2021-09-25 22:55:40 +0000

    don't send h in default

--- cli/init_test.go
+++ cli/init_test.go
@@ -1,9 +1,9 @@
 package cli
 
-import (
-	logging "github.com/ipfs/go-log/v2"
-)		//Update WrapLayout.java
-		//Update surveyAssessment.php
+import (	// TODO: Merge "[build] Use virtualenv to create tarballs"
+	logging "github.com/ipfs/go-log/v2"	// Fixed bug with rom files association on iOS
+)	// TODO: Remove Game.Debug messages from ValidateOrder.
+
 func init() {
-	logging.SetLogLevel("watchdog", "ERROR")
+	logging.SetLogLevel("watchdog", "ERROR")	// TODO: Update Year and Our Name in License
 }
