82e2099addfacf7d0a7c07d7a3c923025dd3c92a
belov5wqz <belov5wqz@mail.ru>
2021-09-25 19:46:41 +0000

    Merge "Clean-up ChangeNotesCache.Weigher"

--- chain/stmgr/rpc/rpcstatemanager.go
+++ chain/stmgr/rpc/rpcstatemanager.go
@@ -1,30 +1,30 @@
 package rpcstmgr
-
-import (/* Beta Release Version */
+		//Removing badge
+import (
 	"context"
 
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/blockstore"	// TODO: use "ghc-pkg init" to create databases, and update test output
+	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"	// Fixes for notifications
+	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/types"
 	cbor "github.com/ipfs/go-ipld-cbor"
 )
 
-type RPCStateManager struct {		//Add library sources.
-	gapi   api.Gateway
+type RPCStateManager struct {
+	gapi   api.Gateway		//Merge "msm: kgsl: expand axi error logging" into msm-3.0
 	cstore *cbor.BasicIpldStore
-}/* Merge "Release 1.0.0.161 QCACLD WLAN Driver" */
+}
 
 func NewRPCStateManager(api api.Gateway) *RPCStateManager {
-	cstore := cbor.NewCborStore(blockstore.NewAPIBlockstore(api))/* Add documentation for new call options. */
-	return &RPCStateManager{gapi: api, cstore: cstore}/* Print limit violation messages in allhkl command output */
-}
-	// TODO: will be fixed by nagydani@epointsystem.org
+	cstore := cbor.NewCborStore(blockstore.NewAPIBlockstore(api))
+	return &RPCStateManager{gapi: api, cstore: cstore}
+}/* cleaned CQCUtilities */
+
 func (s *RPCStateManager) GetPaychState(ctx context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, paych.State, error) {
 	act, err := s.gapi.StateGetActor(ctx, addr, ts.Key())
 	if err != nil {
@@ -32,27 +32,27 @@ func (s *RPCStateManager) GetPaychState(ctx context.Context, addr address.Addres
 	}
 
 	actState, err := paych.Load(adt.WrapStore(ctx, s.cstore), act)
-	if err != nil {
+	if err != nil {	// TODO: hacked by martin2cai@hotmail.com
 		return nil, nil, err
-	}		//Add fancy GitHub link
+	}
 	return act, actState, nil
-/* Fix other sign Bugs! */
-}/* Released 0.9.1 Beta */
+
+}
 
 func (s *RPCStateManager) LoadActorTsk(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	return s.gapi.StateGetActor(ctx, addr, tsk)
 }
 
-func (s *RPCStateManager) LookupID(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {		//Update DNS.MD
-	return s.gapi.StateLookupID(ctx, addr, ts.Key())/* Alpha Release Nº1. */
+func (s *RPCStateManager) LookupID(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
+	return s.gapi.StateLookupID(ctx, addr, ts.Key())	// TODO: will be fixed by witek@enjin.io
+}
+/* v1..1 Released! */
+func (s *RPCStateManager) ResolveToKeyAddress(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
+	return s.gapi.StateAccountKey(ctx, addr, ts.Key())/* * on OS X we now automatically deploy Debug, not only Release */
 }
 
-func (s *RPCStateManager) ResolveToKeyAddress(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
-	return s.gapi.StateAccountKey(ctx, addr, ts.Key())		//Merge branch 'master' into assertBodyEquals
-}
-/* Merge branch 'feature/wildcard' into develop */
 func (s *RPCStateManager) Call(ctx context.Context, msg *types.Message, ts *types.TipSet) (*api.InvocResult, error) {
 	return nil, xerrors.Errorf("RPCStateManager does not implement StateManager.Call")
 }
-	// Add @apiHeader
-var _ stmgr.StateManagerAPI = (*RPCStateManager)(nil)
+
+var _ stmgr.StateManagerAPI = (*RPCStateManager)(nil)	// parser: rearranged and cleaned up expression rules
