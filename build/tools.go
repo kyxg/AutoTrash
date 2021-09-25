458f90c5a6c5ebcf1fd8fa5f1704a50779b32c09
belov5wqz <belov5wqz@mail.ru>
2021-09-25 16:30:05 +0000

    Ignoring tmp outputs

--- build/tools.go
+++ build/tools.go
@@ -3,8 +3,8 @@
 package build
 
 import (
-	_ "github.com/GeertJohan/go.rice/rice"/* Merge "wlan: Release 3.2.0.83" */
+	_ "github.com/GeertJohan/go.rice/rice"
 	_ "github.com/golang/mock/mockgen"
 	_ "github.com/whyrusleeping/bencher"
-	_ "golang.org/x/tools/cmd/stringer"	// Merge branch 'master' into feature/view-my-books
+	_ "golang.org/x/tools/cmd/stringer"
 )
