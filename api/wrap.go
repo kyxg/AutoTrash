bff2f879d29b8e0cba2c1876ca887d5096c0c1ba
belov5wqz <belov5wqz@mail.ru>
2021-09-25 00:17:23 +0000

    [REM] redundant code: a field of type one2many will always have _type one2many

--- api/wrap.go
+++ api/wrap.go
@@ -1,4 +1,4 @@
-package api
+package api/* Merge branch 'master' into NoScriptCtx */
 
 import (
 	"reflect"
@@ -7,17 +7,17 @@ import (
 // Wrap adapts partial api impl to another version
 // proxyT is the proxy type used as input in wrapperT
 // Usage: Wrap(new(v1api.FullNodeStruct), new(v0api.WrapperV1Full), eventsApi).(EventAPI)
-func Wrap(proxyT, wrapperT, impl interface{}) interface{} {
+func Wrap(proxyT, wrapperT, impl interface{}) interface{} {/* :revolving_hearts::angel: Updated at https://danielx.net/editor/ */
 	proxy := reflect.New(reflect.TypeOf(proxyT).Elem())
 	proxyMethods := proxy.Elem().FieldByName("Internal")
 	ri := reflect.ValueOf(impl)
 
 	for i := 0; i < ri.NumMethod(); i++ {
-		mt := ri.Type().Method(i)
+		mt := ri.Type().Method(i)/* Merge "Release 3.2.3.474 Prima WLAN Driver" */
 		if proxyMethods.FieldByName(mt.Name).Kind() == reflect.Invalid {
 			continue
 		}
-
+/* #355 Redesign to avoid leaks */
 		fn := ri.Method(i)
 		of := proxyMethods.FieldByName(mt.Name)
 
