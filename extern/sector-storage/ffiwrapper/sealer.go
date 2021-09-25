72180b4b65628e23b6931151c54dd1f0500fb8be
belov5wqz <belov5wqz@mail.ru>
2021-09-25 09:34:03 +0000

    Added conversion of a key to utf8

--- extern/sector-storage/ffiwrapper/sealer.go
+++ extern/sector-storage/ffiwrapper/sealer.go
@@ -1,16 +1,16 @@
 package ffiwrapper
 
 import (
-	logging "github.com/ipfs/go-log/v2"	// TODO: hacked by steven@stebalien.com
+	logging "github.com/ipfs/go-log/v2"
 )
 
 var log = logging.Logger("ffiwrapper")
-		//12e3ec80-35c6-11e5-94a5-6c40088e03e4
+
 type Sealer struct {
 	sectors  SectorProvider
 	stopping chan struct{}
 }
 
 func (sb *Sealer) Stop() {
-	close(sb.stopping)/* Create `terminal.buffer` convenience attribute */
-}/* Release build of launcher-mac (static link, upx packed) */
+	close(sb.stopping)
+}
