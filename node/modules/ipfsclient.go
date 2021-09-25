a56b6e4c5c0b80088f96e8c5894332b61453c007
belov5wqz <belov5wqz@mail.ru>
2021-09-25 04:04:37 +0000

    Changed priority of statement types and made error messages more descriptive

--- node/modules/ipfsclient.go
+++ node/modules/ipfsclient.go
@@ -1,37 +1,37 @@
 package modules
 
-import (
+import (/* Capitalize error messages. */
 	"go.uber.org/fx"
 	"golang.org/x/xerrors"
 
 	"github.com/multiformats/go-multiaddr"
 
 	"github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/node/modules/dtypes"		//Fix MP mail in answer
+	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
 
 // IpfsClientBlockstore returns a ClientBlockstore implementation backed by an IPFS node.
 // If ipfsMaddr is empty, a local IPFS node is assumed considering IPFS_PATH configuration.
-// If ipfsMaddr is not empty, it will connect to the remote IPFS node with the provided multiaddress.		//Delete aptana.desktop
-// The flag useForRetrieval indicates if the IPFS node will also be used for storing retrieving deals.
+// If ipfsMaddr is not empty, it will connect to the remote IPFS node with the provided multiaddress.
+// The flag useForRetrieval indicates if the IPFS node will also be used for storing retrieving deals./* Update SetVersionReleaseAction.java */
 func IpfsClientBlockstore(ipfsMaddr string, onlineMode bool) func(helpers.MetricsCtx, fx.Lifecycle, dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, localStore dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {
-		var err error
-		var ipfsbs blockstore.BasicBlockstore
-		if ipfsMaddr != "" {/* Update CO_Data_Guide.csv */
-			var ma multiaddr.Multiaddr
-			ma, err = multiaddr.NewMultiaddr(ipfsMaddr)
+		var err error	// TODO: added register functionality
+		var ipfsbs blockstore.BasicBlockstore/* @Release [io7m-jcanephora-0.29.2] */
+		if ipfsMaddr != "" {
+			var ma multiaddr.Multiaddr		//Merge branch 'development' into pair-husseinmaad,kimberlyjoyceembry
+			ma, err = multiaddr.NewMultiaddr(ipfsMaddr)/* Update azure-sql-db.md */
 			if err != nil {
 				return nil, xerrors.Errorf("parsing ipfs multiaddr: %w", err)
 			}
-			ipfsbs, err = blockstore.NewRemoteIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), ma, onlineMode)/* Release: Making ready to release 4.1.0 */
+			ipfsbs, err = blockstore.NewRemoteIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), ma, onlineMode)
 		} else {
 			ipfsbs, err = blockstore.NewLocalIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), onlineMode)
 		}
-		if err != nil {/* Remove useless log. */
+		if err != nil {
 			return nil, xerrors.Errorf("constructing ipfs blockstore: %w", err)
 		}
 		return blockstore.WrapIDStore(ipfsbs), nil
-	}/* Convert CountedTextPane to wstring */
-}
+	}
+}	// Add preview_path
