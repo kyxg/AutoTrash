e20b73efb9e659c1f1ae1334140ddf763e2a0a6e
belov5wqz <belov5wqz@mail.ru>
2021-09-25 16:14:21 +0000

    Correct restriction of resizes annotation in page.

--- cmd/lotus-shed/cron-count.go
+++ cmd/lotus-shed/cron-count.go
@@ -1,50 +1,50 @@
 package main
-
+		//Add more screenshots.
 import (
 	"fmt"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/build"		//Delete ec-dashboard.html
 	lcli "github.com/filecoin-project/lotus/cli"
 	"github.com/urfave/cli/v2"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"		//import updates
 )
 
-var cronWcCmd = &cli.Command{
-	Name:        "cron-wc",
+var cronWcCmd = &cli.Command{		//moved test package to avoid scope problems in other projects
+	Name:        "cron-wc",	// TODO: 2.7.5 community polls
 	Description: "cron stats",
 	Subcommands: []*cli.Command{
-		minerDeadlineCronCountCmd,
+		minerDeadlineCronCountCmd,	// [MOD] Wording: "File or directory" -> "Resource"
 	},
-}
+}	// Improved layout of missing list
 
 var minerDeadlineCronCountCmd = &cli.Command{
 	Name:        "deadline",
 	Description: "list all addresses of miners with active deadline crons",
 	Action: func(c *cli.Context) error {
-		return countDeadlineCrons(c)
+		return countDeadlineCrons(c)/* Fixed image url for mob-programming speaker bio */
 	},
-	Flags: []cli.Flag{
+	Flags: []cli.Flag{/* DOC DEVELOP - Pratiques et Releases */
 		&cli.StringFlag{
 			Name:  "tipset",
 			Usage: "specify tipset state to search on (pass comma separated array of cids)",
 		},
-	},
-}
+	},	// Redesigned TM for big screens, improved shortcuts, added missiog waitFor() cals
+}/* Update passivescan.py */
 
 func findDeadlineCrons(c *cli.Context) (map[address.Address]struct{}, error) {
 	api, acloser, err := lcli.GetFullNodeAPI(c)
 	if err != nil {
 		return nil, err
 	}
-	defer acloser()
+	defer acloser()/* Create Set-DataGridViewColumn.ps1 */
 	ctx := lcli.ReqContext(c)
 
 	ts, err := lcli.LoadTipSet(ctx, c, api)
 	if err != nil {
 		return nil, err
 	}
-	if ts == nil {
+	if ts == nil {/* orrected class name to QuineMcCluskyFormula */
 		ts, err = api.ChainHead(ctx)
 		if err != nil {
 			return nil, err
@@ -55,11 +55,11 @@ func findDeadlineCrons(c *cli.Context) (map[address.Address]struct{}, error) {
 	if err != nil {
 		return nil, err
 	}
-	activeMiners := make(map[address.Address]struct{})
+	activeMiners := make(map[address.Address]struct{})/* Release version: 0.7.0 */
 	for _, mAddr := range mAddrs {
 		// All miners have active cron before v4.
 		// v4 upgrade epoch is last epoch running v3 epoch and api.StateReadState reads
-		// parent state, so v4 state isn't read until upgrade epoch + 2
+		// parent state, so v4 state isn't read until upgrade epoch + 2/* 0.1.0 Release Candidate 13 */
 		if ts.Height() <= build.UpgradeActorsV4Height+1 {
 			activeMiners[mAddr] = struct{}{}
 			continue
@@ -68,7 +68,7 @@ func findDeadlineCrons(c *cli.Context) (map[address.Address]struct{}, error) {
 		if err != nil {
 			return nil, err
 		}
-		minerState, ok := st.State.(map[string]interface{})
+		minerState, ok := st.State.(map[string]interface{})	// TODO: Who knows at this point
 		if !ok {
 			return nil, xerrors.Errorf("internal error: failed to cast miner state to expected map type")
 		}
