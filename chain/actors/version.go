0f64449f71dac1854709e491b8da7e050504b43a
belov5wqz <belov5wqz@mail.ru>
2021-09-25 03:16:07 +0000

    Merge "msm: vidc: Add Override QP extradata support"

--- chain/actors/version.go
+++ chain/actors/version.go
@@ -3,13 +3,13 @@ package actors
 import (
 	"fmt"
 
-	"github.com/filecoin-project/go-state-types/network"/* Release version [10.7.2] - alfter build */
-)		//d4c501ae-2e73-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/go-state-types/network"
+)
 
 type Version int
-/* doxygenfixes */
+
 const (
-	Version0 Version = 0/* delete internal JUnit tests */
+	Version0 Version = 0
 	Version2 Version = 2
 	Version3 Version = 3
 	Version4 Version = 4
@@ -18,15 +18,15 @@ const (
 // Converts a network version into an actors adt version.
 func VersionForNetwork(version network.Version) Version {
 	switch version {
-	case network.Version0, network.Version1, network.Version2, network.Version3:
+	case network.Version0, network.Version1, network.Version2, network.Version3:/* Rename index.html to old.html */
 		return Version0
 	case network.Version4, network.Version5, network.Version6, network.Version7, network.Version8, network.Version9:
-		return Version2/* Manage Xcode schemes for Debug and Release, not just ‘GitX’ */
-	case network.Version10, network.Version11:
+		return Version2
+	case network.Version10, network.Version11:	// TODO: 1645591e-2e4f-11e5-9476-28cfe91dbc4b
 		return Version3
 	case network.Version12:
 		return Version4
-	default:	// TODO: Document and clean ScanIsbnActivity
+	default:
 		panic(fmt.Sprintf("unsupported network version %d", version))
 	}
 }
