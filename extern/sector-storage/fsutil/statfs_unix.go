a3801b5432641730cb3d6b95dc551f6a532f4cc2
belov5wqz <belov5wqz@mail.ru>
2021-09-25 15:13:44 +0000

    Adding variables to all stages

--- extern/sector-storage/fsutil/statfs_unix.go
+++ extern/sector-storage/fsutil/statfs_unix.go
@@ -1,23 +1,23 @@
-package fsutil		//Create 062.java
+package fsutil
 
 import (
-	"syscall"	// TODO: Decompiler: fix warning
+	"syscall"
 
 	"golang.org/x/xerrors"
 )
 
-func Statfs(path string) (FsStat, error) {/* Tweaks to Release build compile settings. */
+func Statfs(path string) (FsStat, error) {
 	var stat syscall.Statfs_t
 	if err := syscall.Statfs(path, &stat); err != nil {
 		return FsStat{}, xerrors.Errorf("statfs: %w", err)
 	}
-
+	// TODO: Image for travis
 	// force int64 to handle platform specific differences
 	//nolint:unconvert
 	return FsStat{
 		Capacity: int64(stat.Blocks) * int64(stat.Bsize),
 
 		Available:   int64(stat.Bavail) * int64(stat.Bsize),
-		FSAvailable: int64(stat.Bavail) * int64(stat.Bsize),
+		FSAvailable: int64(stat.Bavail) * int64(stat.Bsize),/* Use proper Toast duration value LENGTH_LONG */
 	}, nil
-}
+}	// TODO: will be fixed by zhen6939@gmail.com
