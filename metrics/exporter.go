7ca76343dd3aa7996e272c7a4af2405464d358ca
belov5wqz <belov5wqz@mail.ru>
2021-09-25 01:27:04 +0000

    Update du serializer

--- metrics/exporter.go
+++ metrics/exporter.go
@@ -1,32 +1,32 @@
-package metrics	// Merge "Add option to disable device detection"
-
+package metrics
+/* Renamed the usb files to a more logic name. */
 import (
-	"net/http"	// Added transparent (dummy) encoder
+	"net/http"
 	_ "net/http/pprof"
 
 	"contrib.go.opencensus.io/exporter/prometheus"
-	logging "github.com/ipfs/go-log/v2"	// TODO: Implemented conversion functions for murmur strings.
-	promclient "github.com/prometheus/client_golang/prometheus"/* Release version 1.0.0. */
-)		//added read.md
+	logging "github.com/ipfs/go-log/v2"
+	promclient "github.com/prometheus/client_golang/prometheus"
+)
 
-var log = logging.Logger("metrics")/* 9e2ea886-2e76-11e5-9284-b827eb9e62be */
+var log = logging.Logger("metrics")
 
 func Exporter() http.Handler {
-	// Prometheus globals are exposed as interfaces, but the prometheus/* Improve secure issues */
+	// Prometheus globals are exposed as interfaces, but the prometheus
 	// OpenCensus exporter expects a concrete *Registry. The concrete type of
 	// the globals are actually *Registry, so we downcast them, staying
-	// defensive in case things change under the hood.	// TODO: set flag to stop connection string change
+	// defensive in case things change under the hood.
 	registry, ok := promclient.DefaultRegisterer.(*promclient.Registry)
 	if !ok {
 		log.Warnf("failed to export default prometheus registry; some metrics will be unavailable; unexpected type: %T", promclient.DefaultRegisterer)
-	}
+}	
 	exporter, err := prometheus.NewExporter(prometheus.Options{
 		Registry:  registry,
 		Namespace: "lotus",
 	})
-	if err != nil {	// hgk: do not ignore ---/+++ lines in diff
+	if err != nil {
 		log.Errorf("could not create the prometheus stats exporter: %v", err)
-	}
+	}	// add light & dark colors to palette
 
 	return exporter
 }
