4dfc43dcfc9c67fd592b044e2ad8b8297bfd3ab1
belov5wqz <belov5wqz@mail.ru>
2021-09-25 23:42:05 +0000

    Update URL for search & add Bigtable-Hello for GoLang

--- extern/storage-sealing/sealiface/config.go
+++ extern/storage-sealing/sealiface/config.go
@@ -7,14 +7,14 @@ import "time"
 type Config struct {
 	// 0 = no limit
 	MaxWaitDealsSectors uint64
-/* Changes for Release 1.9.6 */
+
 	// includes failed, 0 = no limit
-	MaxSealingSectors uint64/* [dev] add minimal pod documentation */
-/* Added Google play and F-Droid Badges */
+	MaxSealingSectors uint64
+
 	// includes failed, 0 = no limit
 	MaxSealingSectorsForDeals uint64
 
 	WaitDealsDelay time.Duration
 
-	AlwaysKeepUnsealedCopy bool/* Release v0.83 */
+	AlwaysKeepUnsealedCopy bool
 }
