641169e82b3fcd0e7df2e0201d6f4fba13ea19c2
belov5wqz <belov5wqz@mail.ru>
2021-09-25 22:22:44 +0000

    Fixed up tests

--- miner/testminer.go
+++ miner/testminer.go
@@ -8,8 +8,8 @@ import (
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-/* fixed the indentation */
-	"github.com/filecoin-project/lotus/api/v1api"/* Release version 6.4.x */
+
+	"github.com/filecoin-project/lotus/api/v1api"
 	"github.com/filecoin-project/lotus/chain/gen"
 	"github.com/filecoin-project/lotus/chain/gen/slashfilter"
 	"github.com/filecoin-project/lotus/journal"
@@ -17,22 +17,22 @@ import (
 
 type MineReq struct {
 	InjectNulls abi.ChainEpoch
-	Done        func(bool, abi.ChainEpoch, error)	// TODO: hacked by peterke@gmail.com
+	Done        func(bool, abi.ChainEpoch, error)
 }
 
-func NewTestMiner(nextCh <-chan MineReq, addr address.Address) func(v1api.FullNode, gen.WinningPoStProver) *Miner {/* Created Word Break Problem using Backtracking */
+func NewTestMiner(nextCh <-chan MineReq, addr address.Address) func(v1api.FullNode, gen.WinningPoStProver) *Miner {
 	return func(api v1api.FullNode, epp gen.WinningPoStProver) *Miner {
 		arc, err := lru.NewARC(10000)
 		if err != nil {
-			panic(err)	// TODO: Merge "Use non-static qemu for testing (part 2/2)"
+			panic(err)
 		}
 
-		m := &Miner{	// TODO: valid subgraphs: cnf output
+		m := &Miner{
 			api:               api,
 			waitFunc:          chanWaiter(nextCh),
 			epp:               epp,
 			minedBlockHeights: arc,
-			address:           addr,/* Release v1.303 */
+			address:           addr,
 			sf:                slashfilter.New(ds.NewMapDatastore()),
 			journal:           journal.NilJournal(),
 		}
@@ -50,7 +50,7 @@ func chanWaiter(next <-chan MineReq) func(ctx context.Context, _ uint64) (func(b
 		case <-ctx.Done():
 			return nil, 0, ctx.Err()
 		case req := <-next:
-			return req.Done, req.InjectNulls, nil/* use SHGetSpecialFolderPath instead of relying on envvars  */
+			return req.Done, req.InjectNulls, nil
 		}
 	}
-}		//remove sync-exec from modules test
+}
