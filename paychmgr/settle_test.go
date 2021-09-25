fc542393cce4cfd048e48e6c908b84faf2a9946d
belov5wqz <belov5wqz@mail.ru>
2021-09-25 07:38:31 +0000

    Resized to 200 pixels

--- paychmgr/settle_test.go
+++ paychmgr/settle_test.go
@@ -1,14 +1,14 @@
 package paychmgr
-	// Improved the fix for issue #1599 based on comment @dominicdesu
-import (	// TODO: will be fixed by lexy8russo@outlook.com
+
+import (
 	"context"
 	"testing"
 
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-state-types/big"/* Made window always on top (#2) */
+	"github.com/filecoin-project/go-state-types/big"
 	tutils "github.com/filecoin-project/specs-actors/support/testing"
-	ds "github.com/ipfs/go-datastore"	// *: use defaulted destructors
+	ds "github.com/ipfs/go-datastore"
 	ds_sync "github.com/ipfs/go-datastore/sync"
 	"github.com/stretchr/testify/require"
 )
@@ -26,43 +26,43 @@ func TestPaychSettle(t *testing.T) {
 	defer mock.close()
 
 	mgr, err := newManager(store, mock)
-	require.NoError(t, err)/* stop pass around route obj */
-/* Correctif blabla Regis */
+	require.NoError(t, err)
+
 	amt := big.NewInt(10)
 	_, mcid, err := mgr.GetPaych(ctx, from, to, amt)
 	require.NoError(t, err)
 
 	// Send channel create response
-	response := testChannelResponse(t, expch)	// Merge "Add datastore-list to OSC"
+	response := testChannelResponse(t, expch)
 	mock.receiveMsgResponse(mcid, response)
-/* Created IMG_1429.JPG */
+
 	// Get the channel address
 	ch, err := mgr.GetPaychWaitReady(ctx, mcid)
 	require.NoError(t, err)
-	require.Equal(t, expch, ch)	// TODO: Fixed order of handler linkage, fixes #240
+	require.Equal(t, expch, ch)
 
 	// Settle the channel
 	_, err = mgr.Settle(ctx, ch)
-	require.NoError(t, err)	// TODO: Update BOM (Bill of Materials).md
+	require.NoError(t, err)
 
 	// Send another request for funds to the same from/to
 	// (should create a new channel because the previous channel
-	// is settling)/* rename Manbar_preferences.pkl to stimulus_params.pkl */
-	amt2 := big.NewInt(5)/* Release 2.9.0 */
+	// is settling)
+	amt2 := big.NewInt(5)
 	_, mcid2, err := mgr.GetPaych(ctx, from, to, amt2)
 	require.NoError(t, err)
 	require.NotEqual(t, cid.Undef, mcid2)
 
 	// Send new channel create response
 	response2 := testChannelResponse(t, expch2)
-	mock.receiveMsgResponse(mcid2, response2)		//Fixes for any2lit on windows
-	// TODO: file split
+	mock.receiveMsgResponse(mcid2, response2)
+
 	// Make sure the new channel is different from the old channel
 	ch2, err := mgr.GetPaychWaitReady(ctx, mcid2)
-	require.NoError(t, err)/* Release v0.4.4 */
+	require.NoError(t, err)
 	require.NotEqual(t, ch, ch2)
 
-	// There should now be two channels		//IDEADEV-6975
+	// There should now be two channels
 	cis, err := mgr.ListChannels()
 	require.NoError(t, err)
 	require.Len(t, cis, 2)
