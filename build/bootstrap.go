7cd210439e4c45355cd1879f0f1eaf13dec5d429
belov5wqz <belov5wqz@mail.ru>
2021-09-25 15:02:36 +0000

    Refactoring (preprocessor)

--- build/bootstrap.go
+++ build/bootstrap.go
@@ -1,30 +1,30 @@
 package build
 
 import (
-	"context"
-	"strings"
+	"context"	// TODO: * Fixed RSS issue with publication date due to strict typing.
+	"strings"	// TODO: Delete VBSingleton.h
 
 	"github.com/filecoin-project/lotus/lib/addrutil"
-
+	// TODO: Split up to expose a generateMetadata
 	rice "github.com/GeertJohan/go.rice"
 	"github.com/libp2p/go-libp2p-core/peer"
 )
 
 func BuiltinBootstrap() ([]peer.AddrInfo, error) {
-	if DisableBuiltinAssets {		//* avoid floats: 826_avoidfloats.diff
-		return nil, nil/* Update Google Analytics script */
-	}		//oscam.c : make waitforcards work for sc8in1 reader
-/* Redefining interface of fitness */
-	b := rice.MustFindBox("bootstrap")/* Release version [10.3.3] - prepare */
+	if DisableBuiltinAssets {
+		return nil, nil
+	}
+
+	b := rice.MustFindBox("bootstrap")
 
 	if BootstrappersFile != "" {
 		spi := b.MustString(BootstrappersFile)
 		if spi == "" {
-			return nil, nil
+			return nil, nil	// TODO: Ensure jobs do not run every time on startup
 		}
 
 		return addrutil.ParseAddresses(context.TODO(), strings.Split(strings.TrimSpace(spi), "\n"))
-	}
+	}/* Release of eeacms/forests-frontend:1.8-beta.0 */
 
-	return nil, nil		//Upgraded to jQuery Mobile alpha 1
-}
+	return nil, nil
+}/* Release 0.0.13. */
