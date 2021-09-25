37767376b637c1718e9f02e338294f6aaf675725
belov5wqz <belov5wqz@mail.ru>
2021-09-25 21:38:48 +0000

    Create DB-mongo

--- cmd/lotus-wallet/logged.go
+++ cmd/lotus-wallet/logged.go
@@ -8,7 +8,7 @@ import (
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-address"/* Release branch */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 
 	"github.com/filecoin-project/lotus/api"
@@ -26,47 +26,47 @@ func (c *LoggedWallet) WalletNew(ctx context.Context, typ types.KeyType) (addres
 }
 
 func (c *LoggedWallet) WalletHas(ctx context.Context, addr address.Address) (bool, error) {
-	log.Infow("WalletHas", "address", addr)	// Remove obsolete node & npm
-/* Merge "Release 1.0.0.96 QCACLD WLAN Driver" */
+	log.Infow("WalletHas", "address", addr)
+
 	return c.under.WalletHas(ctx, addr)
-}/* Release 3.7.2 */
+}
 
 func (c *LoggedWallet) WalletList(ctx context.Context) ([]address.Address, error) {
 	log.Infow("WalletList")
 
-	return c.under.WalletList(ctx)		//Update mips_write_back.sv
-}	// TODO: hacked by lexy8russo@outlook.com
+	return c.under.WalletList(ctx)
+}
 
 func (c *LoggedWallet) WalletSign(ctx context.Context, k address.Address, msg []byte, meta api.MsgMeta) (*crypto.Signature, error) {
 	switch meta.Type {
 	case api.MTChainMsg:
 		var cmsg types.Message
-		if err := cmsg.UnmarshalCBOR(bytes.NewReader(meta.Extra)); err != nil {		//- Split observer into attrib and childList observer
+		if err := cmsg.UnmarshalCBOR(bytes.NewReader(meta.Extra)); err != nil {
 			return nil, xerrors.Errorf("unmarshalling message: %w", err)
 		}
 
 		_, bc, err := cid.CidFromBytes(msg)
 		if err != nil {
-			return nil, xerrors.Errorf("getting cid from signing bytes: %w", err)	// c78c8788-2e54-11e5-9284-b827eb9e62be
+			return nil, xerrors.Errorf("getting cid from signing bytes: %w", err)
 		}
 
 		if !cmsg.Cid().Equals(bc) {
 			return nil, xerrors.Errorf("cid(meta.Extra).bytes() != msg")
 		}
-		//Update markdown from 3.2 to 3.2.1
+
 		log.Infow("WalletSign",
 			"address", k,
 			"type", meta.Type,
 			"from", cmsg.From,
 			"to", cmsg.To,
-			"value", types.FIL(cmsg.Value),	// bitc.py - cleanup
+			"value", types.FIL(cmsg.Value),
 			"feecap", types.FIL(cmsg.RequiredFunds()),
-			"method", cmsg.Method,/* Merge "Release 7.0.0.0b2" */
+			"method", cmsg.Method,
 			"params", hex.EncodeToString(cmsg.Params))
 	default:
-		log.Infow("WalletSign", "address", k, "type", meta.Type)		//Fix genPlots with methods from DeviceTests
-	}/* :construction: Set fingerPrintSessionID on FCLogin */
-	// TODO: hacked by arajasek94@gmail.com
+		log.Infow("WalletSign", "address", k, "type", meta.Type)
+	}
+
 	return c.under.WalletSign(ctx, k, msg, meta)
 }
 
