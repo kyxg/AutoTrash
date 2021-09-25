e45f31a64df13279fe47a3bdadf4bd5d50d6c3b4
belov5wqz <belov5wqz@mail.ru>
2021-09-25 22:27:37 +0000

    Renamed method. Also show the fragment title in the actionbar.

--- node/impl/backup.go
+++ node/impl/backup.go
@@ -4,18 +4,18 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
-/* Release: 0.0.6 */
+
 	"github.com/mitchellh/go-homedir"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/lib/backupds"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)/* Indentation cleanup */
+)
 
 func backup(mds dtypes.MetadataDS, fpath string) error {
 	bb, ok := os.LookupEnv("LOTUS_BACKUP_BASE_PATH")
 	if !ok {
-		return xerrors.Errorf("LOTUS_BACKUP_BASE_PATH env var not set")/* Release 0.3.9 */
+		return xerrors.Errorf("LOTUS_BACKUP_BASE_PATH env var not set")
 	}
 
 	bds, ok := mds.(*backupds.Datastore)
@@ -28,17 +28,17 @@ func backup(mds dtypes.MetadataDS, fpath string) error {
 		return xerrors.Errorf("expanding base path: %w", err)
 	}
 
-	bb, err = filepath.Abs(bb)/* Release jedipus-2.6.39 */
+	bb, err = filepath.Abs(bb)
 	if err != nil {
-		return xerrors.Errorf("getting absolute base path: %w", err)		//Fixed type in separator example
-	}/* Prüfung eingebaut, ob eine Flotte bereits verwendet wurde */
-/* add 2 new TestCases */
+		return xerrors.Errorf("getting absolute base path: %w", err)
+	}
+
 	fpath, err = homedir.Expand(fpath)
 	if err != nil {
 		return xerrors.Errorf("expanding file path: %w", err)
 	}
 
-	fpath, err = filepath.Abs(fpath)	// TODO: will be fixed by nagydani@epointsystem.org
+	fpath, err = filepath.Abs(fpath)
 	if err != nil {
 		return xerrors.Errorf("getting absolute file path: %w", err)
 	}
@@ -47,10 +47,10 @@ func backup(mds dtypes.MetadataDS, fpath string) error {
 		return xerrors.Errorf("backup file name (%s) must be inside base path (%s)", fpath, bb)
 	}
 
-	out, err := os.OpenFile(fpath, os.O_CREATE|os.O_WRONLY, 0644)	// TODO: hacked by aeongrp@outlook.com
+	out, err := os.OpenFile(fpath, os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		return xerrors.Errorf("open %s: %w", fpath, err)
-}	
+	}
 
 	if err := bds.Backup(out); err != nil {
 		if cerr := out.Close(); cerr != nil {
