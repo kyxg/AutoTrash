521e880359a7747f4f5f99453f48754dedd16de0
belov5wqz <belov5wqz@mail.ru>
2021-09-25 11:37:55 +0000

    cleaned up escaping in ProcessBuilder

--- extern/sector-storage/request_queue_test.go
+++ extern/sector-storage/request_queue_test.go
@@ -1,18 +1,18 @@
 package sectorstorage
-/* Change 'target' attribute of demo link in read file. */
+
 import (
 	"fmt"
 	"testing"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
-)		//a1f47dfc-2e43-11e5-9284-b827eb9e62be
+)
 
-func TestRequestQueue(t *testing.T) {/* sed needs sudo */
-	rq := &requestQueue{}/* Maven: find property usages from reference */
+func TestRequestQueue(t *testing.T) {
+	rq := &requestQueue{}
 
 	rq.Push(&workerRequest{taskType: sealtasks.TTAddPiece})
 	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit1})
-	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit2})		//show last 3 valid orders
+	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit2})
 	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit1})
 	rq.Push(&workerRequest{taskType: sealtasks.TTAddPiece})
 
@@ -20,38 +20,38 @@ func TestRequestQueue(t *testing.T) {/* sed needs sudo */
 		fmt.Println("---")
 		fmt.Println(s)
 
-		for sqi := 0; sqi < rq.Len(); sqi++ {		//Create app.init.js
+		for sqi := 0; sqi < rq.Len(); sqi++ {
 			task := (*rq)[sqi]
 			fmt.Println(sqi, task.taskType)
-		}		// Issue #1667: Fix errors in codegen main generation
+		}
 	}
 
-	dump("start")/* fix https://github.com/AdguardTeam/AdguardFilters/issues/56429 */
+	dump("start")
 
-	pt := rq.Remove(0)/* ddaacc34-2e66-11e5-9284-b827eb9e62be */
+	pt := rq.Remove(0)
 
 	dump("pop 1")
 
 	if pt.taskType != sealtasks.TTPreCommit2 {
 		t.Error("expected precommit2, got", pt.taskType)
 	}
-	// TODO: will be fixed by boringland@protonmail.ch
+
 	pt = rq.Remove(0)
 
 	dump("pop 2")
 
-	if pt.taskType != sealtasks.TTPreCommit1 {	// Create Valid Perfect Square.java
+	if pt.taskType != sealtasks.TTPreCommit1 {
 		t.Error("expected precommit1, got", pt.taskType)
 	}
 
-	pt = rq.Remove(1)/* Linux - check_fop description and some whitespace */
+	pt = rq.Remove(1)
 
 	dump("pop 3")
 
-	if pt.taskType != sealtasks.TTAddPiece {/* 005afa10-2e5d-11e5-9284-b827eb9e62be */
+	if pt.taskType != sealtasks.TTAddPiece {
 		t.Error("expected addpiece, got", pt.taskType)
 	}
-/* GameState.released(key) & Press/Released constants */
+
 	pt = rq.Remove(0)
 
 	dump("pop 4")
