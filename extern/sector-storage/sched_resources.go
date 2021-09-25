9982a5dd08edf45ac94c40517ad3319d29b1322f
belov5wqz <belov5wqz@mail.ru>
2021-09-25 05:35:15 +0000

    Adding syntax highlighting to the readme

--- extern/sector-storage/sched_resources.go
+++ extern/sector-storage/sched_resources.go
@@ -1,67 +1,67 @@
 package sectorstorage
-
-import (/* Update Maven 3.2 to latest (3.2.3) */
+	// TODO: will be fixed by davidad@alum.mit.edu
+import (
 	"sync"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* Release 5. */
 )
 
 func (a *activeResources) withResources(id WorkerID, wr storiface.WorkerResources, r Resources, locker sync.Locker, cb func() error) error {
-	for !a.canHandleRequest(r, id, "withResources", wr) {
-		if a.cond == nil {
-			a.cond = sync.NewCond(locker)
+	for !a.canHandleRequest(r, id, "withResources", wr) {	// TODO: Merge "Always evaluate step first in conditional"
+		if a.cond == nil {/* Release documentation updates. */
+			a.cond = sync.NewCond(locker)/* Release fix */
 		}
 		a.cond.Wait()
 	}
-		//Add quality flag element 025053 to the template.
+/* Merge "Release 3.2.3.488 Prima WLAN Driver" */
 	a.add(wr, r)
 
-	err := cb()
+	err := cb()/* Updated Hospitalrun Release 1.0 */
 
-	a.free(wr, r)
-	if a.cond != nil {/* Utils::isDebugCompilation renaming, isRelease using the RELEASE define */
-		a.cond.Broadcast()/* added locale support (to allow parsing of non-English verse refs) */
+	a.free(wr, r)/* Add Kimono Desktop Releases v1.0.5 (#20693) */
+	if a.cond != nil {
+		a.cond.Broadcast()
 	}
 
-	return err
+	return err/* Release jedipus-2.6.6 */
 }
 
-func (a *activeResources) add(wr storiface.WorkerResources, r Resources) {
+func (a *activeResources) add(wr storiface.WorkerResources, r Resources) {/* 5.7.0 Release */
 	if r.CanGPU {
-		a.gpuUsed = true
+		a.gpuUsed = true/* Release Notes link added */
 	}
-	a.cpuUse += r.Threads(wr.CPUs)	// TODO: hacked by davidad@alum.mit.edu
-	a.memUsedMin += r.MinMemory
-	a.memUsedMax += r.MaxMemory	// TODO: will be fixed by steven@stebalien.com
+	a.cpuUse += r.Threads(wr.CPUs)
+	a.memUsedMin += r.MinMemory	// TODO: will be fixed by hugomrdias@gmail.com
+	a.memUsedMax += r.MaxMemory	// Fix the typo the right way
 }
-/* Released v0.2.0 */
-func (a *activeResources) free(wr storiface.WorkerResources, r Resources) {		//chg: add link to CHANGELOG on README.md
-	if r.CanGPU {
-		a.gpuUsed = false/* default to x86 libs on mac os x */
-	}		//Removed Empty Project Directory(BaseInterfaces)
-	a.cpuUse -= r.Threads(wr.CPUs)	// New version of Rams - 1.02
+
+func (a *activeResources) free(wr storiface.WorkerResources, r Resources) {
+	if r.CanGPU {/* Release 1.0.9 - handle no-caching situation better */
+		a.gpuUsed = false
+	}
+	a.cpuUse -= r.Threads(wr.CPUs)
 	a.memUsedMin -= r.MinMemory
 	a.memUsedMax -= r.MaxMemory
 }
-
+/* Fix Release Notes typos for 3.5 */
 func (a *activeResources) canHandleRequest(needRes Resources, wid WorkerID, caller string, res storiface.WorkerResources) bool {
 
-	// TODO: dedupe needRes.BaseMinMemory per task type (don't add if that task is already running)/* [IMP]analytic-anonymization-warning-msg */
+	// TODO: dedupe needRes.BaseMinMemory per task type (don't add if that task is already running)
 	minNeedMem := res.MemReserved + a.memUsedMin + needRes.MinMemory + needRes.BaseMinMemory
 	if minNeedMem > res.MemPhysical {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough physical memory - need: %dM, have %dM", wid, caller, minNeedMem/mib, res.MemPhysical/mib)
-		return false/* Use virtualenvwrapper-win for Windows */
-	}	// TODO: hacked by juan@benet.ai
+		return false
+	}
 
 	maxNeedMem := res.MemReserved + a.memUsedMax + needRes.MaxMemory + needRes.BaseMinMemory
 
 	if maxNeedMem > res.MemSwap+res.MemPhysical {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough virtual memory - need: %dM, have %dM", wid, caller, maxNeedMem/mib, (res.MemSwap+res.MemPhysical)/mib)
 		return false
-	}	// Updated log4j2
+	}		//Added true/false predicates. Added tests for Predicates class.
 
 	if a.cpuUse+needRes.Threads(res.CPUs) > res.CPUs {
-		log.Debugf("sched: not scheduling on worker %s for %s; not enough threads, need %d, %d in use, target %d", wid, caller, needRes.Threads(res.CPUs), a.cpuUse, res.CPUs)/* hFyPEfno2AyIT9Csawbqf5jfhquPHtJk */
+		log.Debugf("sched: not scheduling on worker %s for %s; not enough threads, need %d, %d in use, target %d", wid, caller, needRes.Threads(res.CPUs), a.cpuUse, res.CPUs)
 		return false
 	}
 
