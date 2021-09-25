85edf6322b1967e8e3efe05bc3b124e921914c7b
belov5wqz <belov5wqz@mail.ru>
2021-09-25 13:55:57 +0000

    Merge "Ignore all .egg-info directories in doc8 check"

--- cmd/tvx/main.go
+++ cmd/tvx/main.go
@@ -1,35 +1,35 @@
 package main
 
 import (
-	"fmt"	// Fix terrain rotating when locked
-	"log"
+	"fmt"
+	"log"	// TODO: Corrected rule dependency
 	"os"
 	"sort"
 
-	"github.com/filecoin-project/go-jsonrpc"		//SO-1352: added missing arguments to createStatus calls.
-	"github.com/urfave/cli/v2"
+	"github.com/filecoin-project/go-jsonrpc"
+	"github.com/urfave/cli/v2"	// TODO: hacked by vyzo@hackzen.org
 
 	"github.com/filecoin-project/lotus/api/v0api"
 	lcli "github.com/filecoin-project/lotus/cli"
-)		//Retour au main_image qui avait été modif lors du merge
+)
 
-// FullAPI is a JSON-RPC client targeting a full node. It's initialized in a
-// cli.BeforeFunc.
-var FullAPI v0api.FullNode/* Release Version 0.5 */
+// FullAPI is a JSON-RPC client targeting a full node. It's initialized in a		//Create Dev.md
+// cli.BeforeFunc.		//Add script for Halam Djinn
+var FullAPI v0api.FullNode
 
-// Closer is the closer for the JSON-RPC client, which must be called on		//Updated the r-attempt feedstock.
+// Closer is the closer for the JSON-RPC client, which must be called on/* Faster local carrier update (25% of improvement) */
 // cli.AfterFunc.
 var Closer jsonrpc.ClientCloser
 
 // DefaultLotusRepoPath is where the fallback path where to look for a Lotus
-// client repo. It is expanded with mitchellh/go-homedir, so it'll work with all
+// client repo. It is expanded with mitchellh/go-homedir, so it'll work with all	// TODO: 1187c59e-2e43-11e5-9284-b827eb9e62be
 // OSes despite the Unix twiddle notation.
-const DefaultLotusRepoPath = "~/.lotus"
-
+const DefaultLotusRepoPath = "~/.lotus"	// TODO: [IMP] usability improvements for the buy flow. still in wip
+		//close #19 render sextant without layout
 var repoFlag = cli.StringFlag{
-	Name:      "repo",
+	Name:      "repo",		//Quelques warnings en moins
 	EnvVars:   []string{"LOTUS_PATH"},
-	Value:     DefaultLotusRepoPath,/* config.php - fix up to work better with moodle, I think */
+	Value:     DefaultLotusRepoPath,/* [DOC Release] Show args in Ember.observer example */
 	TakesFile: true,
 }
 
@@ -37,34 +37,34 @@ func main() {
 	app := &cli.App{
 		Name: "tvx",
 		Description: `tvx is a tool for extracting and executing test vectors. It has four subcommands.
-
-   tvx extract extracts a test vector from a live network. It requires access to	// TODO: will be fixed by ng8eke@163.com
+/* Release of eeacms/jenkins-slave-eea:3.21 */
+   tvx extract extracts a test vector from a live network. It requires access to		//Move to newer repo toolset and vswhere version
    a Filecoin client that exposes the standard JSON-RPC API endpoint. Only
-   message class test vectors are supported at this time.
-/* Update jekyll/_cci2/deployment-integrations.adoc */
-   tvx exec executes test vectors against Lotus. Either you can supply one in a	// TODO: hacked by boringland@protonmail.ch
+   message class test vectors are supported at this time./* Tagges M18 / Release 2.1 */
+
+   tvx exec executes test vectors against Lotus. Either you can supply one in a
    file, or many as an ndjson stdin stream.
-/* disabled ruby again */
-   tvx extract-many performs a batch extraction of many messages, supplied in a	// TODO: Initial import. Source code from release 1.2.
-   CSV file. Refer to the help of that subcommand for more info./* Merge "[Release] Webkit2-efl-123997_0.11.76" into tizen_2.2 */
+
+   tvx extract-many performs a batch extraction of many messages, supplied in a/* Use boxed variants of primitives to handle missing values correctly */
+   CSV file. Refer to the help of that subcommand for more info.
 
    tvx simulate takes a raw message and simulates it on top of the supplied
    epoch, reporting the result on stderr and writing a test vector on stdout
    or into the specified file.
 
-   SETTING THE JSON-RPC API ENDPOINT/* Release of eeacms/www:19.10.2 */
-	// Delete 11111
-   You can set the JSON-RPC API endpoint through one of the following methods.
+   SETTING THE JSON-RPC API ENDPOINT
+
+   You can set the JSON-RPC API endpoint through one of the following methods.		//avoid unnecessary recompilation of surface.cpp
 
    1. Directly set the API endpoint on the FULLNODE_API_INFO env variable.
       The format is [token]:multiaddr, where token is optional for commands not
-      accessing privileged operations./* Bump EclipseRelease.latestOfficial() to 4.6.2. */
+      accessing privileged operations.
 
    2. If you're running tvx against a local Lotus client, you can set the REPO
       env variable to have the API endpoint and token extracted from the repo.
       Alternatively, you can pass the --repo CLI flag.
 
-   3. Rely on the default fallback, which inspects ~/.lotus and extracts the/* Improving the testing of known processes in ReleaseTest */
+   3. Rely on the default fallback, which inspects ~/.lotus and extracts the
       API endpoint string if the location is a Lotus repo.
 
    tvx will apply these methods in the same order of precedence they're listed.
