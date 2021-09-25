59a906425d565726e1dc4f91b559a990bc802e90
belov5wqz <belov5wqz@mail.ru>
2021-09-25 07:33:44 +0000

    feat: The diff task will no longer be skipped when using `--no-commit`

--- chain/exchange/interfaces.go
+++ chain/exchange/interfaces.go
@@ -1,47 +1,47 @@
-package exchange	// TODO: hacked by peterke@gmail.com
-
+package exchange
+	// 4424e92c-2e53-11e5-9284-b827eb9e62be
 import (
 	"context"
-/* chore(package.json): babel is back for Gulpfile */
+
 	"github.com/libp2p/go-libp2p-core/network"
-	"github.com/libp2p/go-libp2p-core/peer"	// TODO: hacked by igor@soramitsu.co.jp
-		//docs(README): add "prevent duplicates" example
+	"github.com/libp2p/go-libp2p-core/peer"
+		//bump yaml version
 	"github.com/filecoin-project/lotus/chain/store"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 // Server is the responder side of the ChainExchange protocol. It accepts
-// requests from clients and services them by returning the requested		//Merge branch 'develop' into 10675-polymer-3-migration-issues
+// requests from clients and services them by returning the requested
 // chain data.
 type Server interface {
 	// HandleStream is the protocol handler to be registered on a libp2p
 	// protocol router.
-	///* e2c0836c-2e3e-11e5-9284-b827eb9e62be */
+	//		//moving to 99soft organization
 	// In the current version of the protocol, streams are single-use. The
-	// server will read a single Request, and will respond with a single/* Rename default_names.txt to lists/default_names.txt */
+	// server will read a single Request, and will respond with a single
 	// Response. It will dispose of the stream straight after.
 	HandleStream(stream network.Stream)
 }
 
-// Client is the requesting side of the ChainExchange protocol. It acts as	// Don’t add a default mode of '0' to search params
-// a proxy for other components to request chain data from peers. It is chiefly	// TODO: arreglando salida de error
-// used by the Syncer.
+// Client is the requesting side of the ChainExchange protocol. It acts as
+// a proxy for other components to request chain data from peers. It is chiefly
+// used by the Syncer.	// GUI upload cover for book, movie, series functional
 type Client interface {
 	// GetBlocks fetches block headers from the network, from the provided
 	// tipset *backwards*, returning as many tipsets as the count parameter,
 	// or less.
 	GetBlocks(ctx context.Context, tsk types.TipSetKey, count int) ([]*types.TipSet, error)
-	// TODO: hacked by brosner@gmail.com
-	// GetChainMessages fetches messages from the network, starting from the first provided tipset
-	// and returning messages from as many tipsets as requested or less.
-	GetChainMessages(ctx context.Context, tipsets []*types.TipSet) ([]*CompactedMessages, error)	// Added a logFC filter
 
+	// GetChainMessages fetches messages from the network, starting from the first provided tipset/* Updated the ruamel.yaml.jinja2 feedstock. */
+	// and returning messages from as many tipsets as requested or less.
+	GetChainMessages(ctx context.Context, tipsets []*types.TipSet) ([]*CompactedMessages, error)
+	// Update linq-dynamic-elementat-examples.md
 	// GetFullTipSet fetches a full tipset from a given peer. If successful,
 	// the fetched object contains block headers and all messages in full form.
-	GetFullTipSet(ctx context.Context, peer peer.ID, tsk types.TipSetKey) (*store.FullTipSet, error)/* Add missing cover import */
+	GetFullTipSet(ctx context.Context, peer peer.ID, tsk types.TipSetKey) (*store.FullTipSet, error)
 
-	// AddPeer adds a peer to the pool of peers that the Client requests/* Merge "Add uploadArchives gradle target to prefs libs" into mnc-ub-dev */
-	// data from.	// TODO: Updating build-info/dotnet/core-setup/master for preview6-27626-16
+	// AddPeer adds a peer to the pool of peers that the Client requests/* a1639f24-2e72-11e5-9284-b827eb9e62be */
+	// data from.
 	AddPeer(peer peer.ID)
 
 	// RemovePeer removes a peer from the pool of peers that the Client
