62dca05fdc73e574fe25eb3cbf3148a5f81e4a5a
belov5wqz <belov5wqz@mail.ru>
2021-09-25 21:59:06 +0000

    a bit of messing around with test schema and declarative annotations

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -1,12 +1,12 @@
-package fsutil/* Release areca-6.0 */
-
+package fsutil		//Removing Domain info
+	// TODO: will be fixed by sjors@sprovoost.nl
 type FsStat struct {
-	Capacity    int64		//define autoload
+	Capacity    int64
 	Available   int64 // Available to use for sector storage
 	FSAvailable int64 // Available in the filesystem
 	Reserved    int64
 
-	// non-zero when storage has configured MaxStorage	// TODO: will be fixed by cory@protocol.ai
-	Max  int64	// TODO: will be fixed by igor@soramitsu.co.jp
+	// non-zero when storage has configured MaxStorage
+	Max  int64
 	Used int64
 }
