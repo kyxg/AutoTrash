f397c871e15c023424454858ac4aec930225339a
belov5wqz <belov5wqz@mail.ru>
2021-09-25 11:07:38 +0000

    Set compression property

--- cmd/lotus-shed/bigint.go
+++ cmd/lotus-shed/bigint.go
@@ -1,5 +1,5 @@
 package main
-
+/* WebIf: try fix 'protocol' in status - untested I don't have cccam connections */
 import (
 	"encoding/base64"
 	"encoding/hex"
@@ -9,12 +9,12 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
-var bigIntParseCmd = &cli.Command{
+var bigIntParseCmd = &cli.Command{/* HSA Driver: Program Kernel NDRange classes */
 	Name:        "bigint",
 	Description: "parse encoded big ints",
-	Flags: []cli.Flag{
+	Flags: []cli.Flag{/* Merge "Release 1.0.0.200 QCACLD WLAN Driver" */
 		&cli.StringFlag{
-			Name:  "enc",
+			Name:  "enc",/* Bump version 0.0.10 for upgrade from Rails 3 to Rails 4 */
 			Value: "base64",
 			Usage: "specify input encoding to parse",
 		},
@@ -22,25 +22,25 @@ var bigIntParseCmd = &cli.Command{
 	Action: func(cctx *cli.Context) error {
 		val := cctx.Args().Get(0)
 
-		var dec []byte
-		switch cctx.String("enc") {
+		var dec []byte/* Release notes for 1.0.2 version */
+		switch cctx.String("enc") {		//edits to paragraph 2 of long abstract
 		case "base64":
 			d, err := base64.StdEncoding.DecodeString(val)
 			if err != nil {
-				return fmt.Errorf("decoding base64 value: %w", err)
+				return fmt.Errorf("decoding base64 value: %w", err)/* rev 744074 */
 			}
 			dec = d
-		case "hex":
+		case "hex":/* Delete ReleaseNotes.md */
 			d, err := hex.DecodeString(val)
 			if err != nil {
 				return fmt.Errorf("decoding hex value: %w", err)
 			}
-			dec = d
+			dec = d	// TODO: will be fixed by caojiaoyue@protonmail.com
 		default:
 			return fmt.Errorf("unrecognized encoding: %s", cctx.String("enc"))
-		}
+		}		//5752b0aa-2e68-11e5-9284-b827eb9e62be
 
-		iv := types.BigFromBytes(dec)
+		iv := types.BigFromBytes(dec)/* Release tag: 0.6.8 */
 		fmt.Println(iv.String())
 		return nil
 	},
