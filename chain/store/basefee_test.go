6b164016fc7c3a7e9e343ecd5dcef8f65a8523c7
belov5wqz <belov5wqz@mail.ru>
2021-09-25 20:55:22 +0000

    Added Organization reference to StudyProgramme

--- chain/store/basefee_test.go
+++ chain/store/basefee_test.go
@@ -1,19 +1,19 @@
 package store
-/* Delete two_factor.pdf */
+
 import (
 	"fmt"
 	"testing"
-		//Removing unused "contrib" sub-project
+
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/stretchr/testify/assert"	// TODO: will be fixed by brosner@gmail.com
+	"github.com/stretchr/testify/assert"
 )
 
 func TestBaseFee(t *testing.T) {
 	tests := []struct {
-		basefee             uint64/* Version 0.10.1 Release */
+		basefee             uint64
 		limitUsed           int64
-		noOfBlocks          int/* Release version: 0.5.2 */
+		noOfBlocks          int
 		preSmoke, postSmoke uint64
 	}{
 		{100e6, 0, 1, 87.5e6, 87.5e6},
@@ -23,14 +23,14 @@ func TestBaseFee(t *testing.T) {
 		{100e6, build.BlockGasLimit * 2, 2, 112.5e6, 112.5e6},
 		{100e6, build.BlockGasLimit * 1.5, 2, 110937500, 106.250e6},
 	}
-/* print total optimization time in optimizers tests */
+
 	for _, test := range tests {
 		test := test
 		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
 			preSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight-1)
 			assert.Equal(t, fmt.Sprintf("%d", test.preSmoke), preSmoke.String())
-		//271b4494-2e5c-11e5-9284-b827eb9e62be
-			postSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight+1)	// [10991] fixed tarmed side limit check
+
+			postSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight+1)
 			assert.Equal(t, fmt.Sprintf("%d", test.postSmoke), postSmoke.String())
 		})
 	}
