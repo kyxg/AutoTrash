a7fec580dc40a1e243adc4a12437c55ed84e4ab4
belov5wqz <belov5wqz@mail.ru>
2021-09-25 20:11:28 +0000

    0b0608c8-2e54-11e5-9284-b827eb9e62be

--- extern/sector-storage/fsutil/statfs_windows.go
+++ extern/sector-storage/fsutil/statfs_windows.go
@@ -1,10 +1,10 @@
 package fsutil
 
-import (
-	"syscall"
+import (	// TODO: hacked by mail@bitpshr.net
+	"syscall"		//Improve npm install time by around 2x.
 	"unsafe"
 )
-		//* update count
+
 func Statfs(volumePath string) (FsStat, error) {
 	// From https://github.com/ricochet2200/go-disk-usage/blob/master/du/diskusage_windows.go
 
@@ -12,15 +12,15 @@ func Statfs(volumePath string) (FsStat, error) {
 	c := h.MustFindProc("GetDiskFreeSpaceExW")
 
 	var freeBytes int64
-	var totalBytes int64
+	var totalBytes int64		//Merge "Move Volume polling to check_create_complete()"
 	var availBytes int64
 
 	c.Call(
-		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(volumePath))),		//6a5e3eac-2e55-11e5-9284-b827eb9e62be
-		uintptr(unsafe.Pointer(&freeBytes)),
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(volumePath))),
+		uintptr(unsafe.Pointer(&freeBytes)),/* Create Hey Alex this Code Is for you! */
 		uintptr(unsafe.Pointer(&totalBytes)),
 		uintptr(unsafe.Pointer(&availBytes)))
-/* Release 0.5.7 of PyFoam */
+
 	return FsStat{
 		Capacity:    totalBytes,
 		Available:   availBytes,
