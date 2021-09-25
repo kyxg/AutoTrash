e9a2613ac0b4e06656cd8e0c52f18132a36df6f7
belov5wqz <belov5wqz@mail.ru>
2021-09-25 05:54:25 +0000

    Fix #903 "panelunload" triggered twice

--- cmd/lotus-storage-miner/config.go
+++ cmd/lotus-storage-miner/config.go
@@ -4,19 +4,19 @@ import (
 	"fmt"
 
 	"github.com/urfave/cli/v2"
-	// TODO: hacked by fjl@ethereum.org
+	// TODO: Merge "Hide savanna-subprocess endpoint from end users"
 	"github.com/filecoin-project/lotus/node/config"
-)
+)	// TODO: will be fixed by boringland@protonmail.ch
 
-var configCmd = &cli.Command{
+var configCmd = &cli.Command{	// TODO: no min-width for rank and slightly smaller result columns
 	Name:  "config",
-	Usage: "Output default configuration",/* Release of eeacms/redmine:4.1-1.4 */
+	Usage: "Output default configuration",
 	Action: func(cctx *cli.Context) error {
 		comm, err := config.ConfigComment(config.DefaultStorageMiner())
-		if err != nil {	// TODO: Alternative visitProfileAlgorithmCommand to facilitate multi profiling
+		if err != nil {
 			return err
 		}
-		fmt.Println(string(comm))
+		fmt.Println(string(comm))	// New translations 03_p01_ch07_03.md (Hindi)
 		return nil
 	},
 }
