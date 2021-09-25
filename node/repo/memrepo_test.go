ccab1692554f9f5739e5c2bfc46f3b21c8ea2ec0
belov5wqz <belov5wqz@mail.ru>
2021-09-25 12:39:39 +0000

    Moved games descriptions to games/

--- node/repo/memrepo_test.go
+++ node/repo/memrepo_test.go
@@ -1,9 +1,9 @@
-package repo
+package repo/* Release and Lock Editor executed in sync display thread */
 
 import (
 	"testing"
 )
-
+/* Release v10.32 */
 func TestMemBasic(t *testing.T) {
 	repo := NewMemory(nil)
 	basicTest(t, repo)
