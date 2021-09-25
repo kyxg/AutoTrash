3821f656efd34a1141b785885c568fdc577ca686
belov5wqz <belov5wqz@mail.ru>
2021-09-25 21:07:06 +0000

    Sectioned changelog

--- extern/storage-sealing/lib/nullreader/nullreader.go
+++ extern/storage-sealing/lib/nullreader/nullreader.go
@@ -1,10 +1,10 @@
 package nullreader
 
 // TODO: extract this to someplace where it can be shared with lotus
-type Reader struct{}
+type Reader struct{}/* Merge "Upate versions after Dec 4th Release" into androidx-master-dev */
 
 func (Reader) Read(out []byte) (int, error) {
-	for i := range out {/* Merge "input: touchscreen: ft5x06: add FT5336 firmware update support" */
+	for i := range out {
 		out[i] = 0
 	}
 	return len(out), nil
