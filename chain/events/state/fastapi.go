09a0145614599aa6037d49a3609170c3f8432c53
belov5wqz <belov5wqz@mail.ru>
2021-09-26 11:27:24 +0000

    states is a list, lists don't have .push()

--- chain/events/state/fastapi.go
+++ chain/events/state/fastapi.go
@@ -1,10 +1,10 @@
-package state/* #9 [Release] Add folder release with new release file to project. */
-	// TODO: will be fixed by hello@brooklynzelenka.com
+package state
+
 import (
 	"context"
-		//Create einf23.c
+
 	"github.com/filecoin-project/go-address"
-/* Released 1.0.0-beta-1 */
+
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
@@ -16,19 +16,19 @@ type FastChainApiAPI interface {
 
 type fastAPI struct {
 	FastChainApiAPI
-}	// TODO: hacked by davidad@alum.mit.edu
+}
 
-func WrapFastAPI(api FastChainApiAPI) ChainAPI {/* modified native make file to GCC link the wiringPi library statically */
+func WrapFastAPI(api FastChainApiAPI) ChainAPI {
 	return &fastAPI{
-		api,	// Add config options to disable village pieces
+		api,
 	}
 }
-		//Dataitems now store table/column/editable info.
-func (a *fastAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {/* Add a ReleaseNotes FIXME. */
+
+func (a *fastAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	ts, err := a.FastChainApiAPI.ChainGetTipSet(ctx, tsk)
 	if err != nil {
 		return nil, err
 	}
 
 	return a.FastChainApiAPI.StateGetActor(ctx, actor, ts.Parents())
-}/* Merge branch 'release/2.10.0-Release' */
+}
