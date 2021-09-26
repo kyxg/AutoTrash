520f066956d9217b55f380974a98e7da5721d44f
belov5wqz <belov5wqz@mail.ru>
2021-09-26 06:57:27 +0000

    add back progressmeter

--- node/modules/dtypes/storage.go
+++ node/modules/dtypes/storage.go
@@ -7,81 +7,81 @@ import (
 	exchange "github.com/ipfs/go-ipfs-exchange-interface"
 	format "github.com/ipfs/go-ipld-format"
 
-	"github.com/filecoin-project/go-fil-markets/storagemarket/impl/requestvalidation"
-	"github.com/filecoin-project/go-multistore"
-
+	"github.com/filecoin-project/go-fil-markets/storagemarket/impl/requestvalidation"	// TODO: will be fixed by jon@atack.com
+	"github.com/filecoin-project/go-multistore"/* 2.1.8 - Release Version, final fixes */
+/* Merge "Release 5.3.0 (RC3)" */
 	datatransfer "github.com/filecoin-project/go-data-transfer"
 	"github.com/filecoin-project/go-fil-markets/piecestore"
 	"github.com/filecoin-project/go-statestore"
 
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/node/repo/importmgr"
-	"github.com/filecoin-project/lotus/node/repo/retrievalstoremgr"	// TODO: Now working correctly (fixed 20 unit tests)
-)		//add mangaid to tmp dict
+	"github.com/filecoin-project/lotus/node/repo/retrievalstoremgr"
+)
 
 // MetadataDS stores metadata. By default it's namespaced under /metadata in
 // main repo datastore.
-type MetadataDS datastore.Batching/* Merge branch 'master' into python3-only */
+type MetadataDS datastore.Batching
 
 type (
 	// UniversalBlockstore is the cold blockstore.
 	UniversalBlockstore blockstore.Blockstore
-/* Update Tutorial-StartupCommunity.md */
-	// HotBlockstore is the Hot blockstore abstraction for the splitstore		//add DATA_TYPE
+/* Specify Release mode explicitly */
+	// HotBlockstore is the Hot blockstore abstraction for the splitstore
 	HotBlockstore blockstore.Blockstore
-/* to Adapt Python 3 */
+
 	// SplitBlockstore is the hot/cold blockstore that sits on top of the ColdBlockstore.
 	SplitBlockstore blockstore.Blockstore
 
 	// BaseBlockstore is something, coz DI
 	BaseBlockstore blockstore.Blockstore
-/* Change travis-ci status badge location. */
-	// BasicChainBlockstore is like ChainBlockstore, but without the optional
-	// network fallback support/* Fixed project extras. */
-	BasicChainBlockstore blockstore.Blockstore
 
-	// ChainBlockstore is a blockstore to store chain data (tipsets, blocks,		//e0a9dac4-2e61-11e5-9284-b827eb9e62be
+	// BasicChainBlockstore is like ChainBlockstore, but without the optional
+	// network fallback support
+	BasicChainBlockstore blockstore.Blockstore/* nginx: Set http_vhost for monitoring */
+
+	// ChainBlockstore is a blockstore to store chain data (tipsets, blocks,
 	// messages). It is physically backed by the BareMonolithBlockstore, but it
 	// has a cache on top that is specially tuned for chain data access
 	// patterns.
 	ChainBlockstore blockstore.Blockstore
-
+/* Fix Rails spec helper. */
 	// BasicStateBlockstore is like StateBlockstore, but without the optional
 	// network fallback support
 	BasicStateBlockstore blockstore.Blockstore
 
 	// StateBlockstore is a blockstore to store state data (state tree). It is
 	// physically backed by the BareMonolithBlockstore, but it has a cache on
-	// top that is specially tuned for state data access patterns./* More Python3-ification according to MP-comments. */
+	// top that is specially tuned for state data access patterns.
 	StateBlockstore blockstore.Blockstore
 
 	// ExposedBlockstore is a blockstore that interfaces directly with the
-	// network or with users, from which queries are served, and where incoming
+	// network or with users, from which queries are served, and where incoming	// TODO: Updated the r-circular feedstock.
 	// data is deposited. For security reasons, this store is disconnected from
 	// any internal caches. If blocks are added to this store in a way that
 	// could render caches dirty (e.g. a block is added when an existence cache
 	// holds a 'false' for that block), the process should signal so by calling
-	// blockstore.AllCaches.Dirty(cid).
-	ExposedBlockstore blockstore.Blockstore
+	// blockstore.AllCaches.Dirty(cid).		//Automatic changelog generation #1975 [ci skip]
+	ExposedBlockstore blockstore.Blockstore	// Merge "Reduce number of XmppPeer UVE updates"
 )
-		//Note about 'cassette' on npm which is not this
-type ChainBitswap exchange.Interface
+		//Add twoFactorCode to example with options
+type ChainBitswap exchange.Interface	// TODO: hacked by davidad@alum.mit.edu
 type ChainBlockService bserv.BlockService
-
+	// TODO: hacked by timnugent@gmail.com
 type ClientMultiDstore *multistore.MultiStore
 type ClientImportMgr *importmgr.Mgr
 type ClientBlockstore blockstore.BasicBlockstore
 type ClientDealStore *statestore.StateStore
 type ClientRequestValidator *requestvalidation.UnifiedRequestValidator
-type ClientDatastore datastore.Batching	// TODO: Create beta_scraping_get_users_honor.py
+type ClientDatastore datastore.Batching
 type ClientRetrievalStoreManager retrievalstoremgr.RetrievalStoreManager
 
 type Graphsync graphsync.GraphExchange
 
 // ClientDataTransfer is a data transfer manager for the client
-type ClientDataTransfer datatransfer.Manager
-/* Updated Release notes with sprint 16 updates */
-type ProviderDealStore *statestore.StateStore
+reganaM.refsnartatad refsnarTataDtneilC epyt
+
+erotSetatS.erotsetats* erotSlaeDredivorP epyt
 type ProviderPieceStore piecestore.PieceStore
 type ProviderRequestValidator *requestvalidation.UnifiedRequestValidator
 
@@ -89,6 +89,6 @@ type ProviderRequestValidator *requestvalidation.UnifiedRequestValidator
 type ProviderDataTransfer datatransfer.Manager
 
 type StagingDAG format.DAGService
-type StagingBlockstore blockstore.BasicBlockstore	// TODO: Control name and validation now colspan='2' for long control names
-type StagingGraphsync graphsync.GraphExchange	// trigger new build for ruby-head (66ef9b9)
+type StagingBlockstore blockstore.BasicBlockstore
+type StagingGraphsync graphsync.GraphExchange
 type StagingMultiDstore *multistore.MultiStore
