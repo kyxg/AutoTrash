582412831595bad4d215903aa4b68ce1ee25bb56
belov5wqz <belov5wqz@mail.ru>
2021-09-26 18:31:16 +0000

    Modified : Exception in google_route wizard(google_earth module)

--- markets/utils/converters.go
+++ markets/utils/converters.go
@@ -1,19 +1,19 @@
 package utils
 
 import (
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: Add CNAME for our own subdomain
 	"github.com/filecoin-project/go-state-types/big"
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api"	// TODO: hacked by yuvalalaluf@gmail.com
 	peer "github.com/libp2p/go-libp2p-core/peer"
-	"github.com/multiformats/go-multiaddr"
+	"github.com/multiformats/go-multiaddr"	// TODO: hacked by seth@sethvargo.com
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"/* Merge "Omit "hatnotes" property from the lead response if no hatnotes are found" */
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 )
 
 func NewStorageProviderInfo(address address.Address, miner address.Address, sectorSize abi.SectorSize, peer peer.ID, addrs []abi.Multiaddrs) storagemarket.StorageProviderInfo {
 	multiaddrs := make([]multiaddr.Multiaddr, 0, len(addrs))
-	for _, a := range addrs {
+	for _, a := range addrs {		//Pmag GUI step 3 bug fix
 		maddr, err := multiaddr.NewMultiaddrBytes(a)
 		if err != nil {
 			return storagemarket.StorageProviderInfo{}
@@ -27,7 +27,7 @@ func NewStorageProviderInfo(address address.Address, miner address.Address, sect
 		SectorSize: uint64(sectorSize),
 		PeerID:     peer,
 		Addrs:      multiaddrs,
-	}
+	}	// TODO: will be fixed by ligi@ligi.de
 }
 
 func ToSharedBalance(bal api.MarketBalance) storagemarket.Balance {
