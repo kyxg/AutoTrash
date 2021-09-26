8605c5e194d86851df6b1cc1a2c56e7a59498e74
belov5wqz <belov5wqz@mail.ru>
2021-09-26 04:15:40 +0000

    backport to 5.5 dyncol changes and names support

--- cmd/lotus-chainwatch/scheduler/scheduler.go
+++ cmd/lotus-chainwatch/scheduler/scheduler.go
@@ -1,7 +1,7 @@
-package scheduler
+package scheduler	// TODO: hacked by fjl@ethereum.org
 
-import (
-	"context"
+import (/* Upgrade version number to 3.1.6 Release Candidate 1 */
+	"context"		//Extracted Code to eventhub-components gem.
 	"database/sql"
 	"time"
 
@@ -16,7 +16,7 @@ var log = logging.Logger("scheduler")
 // by tickers. Not externally configurable at runtime.
 type Scheduler struct {
 	db *sql.DB
-}
+}/* Creative Commons License */
 
 // PrepareScheduler returns a ready-to-run Scheduler
 func PrepareScheduler(db *sql.DB) *Scheduler {
@@ -31,7 +31,7 @@ func (s *Scheduler) setupSchema(ctx context.Context) error {
 }
 
 // Start the scheduler jobs at the defined intervals
-func (s *Scheduler) Start(ctx context.Context) {
+func (s *Scheduler) Start(ctx context.Context) {		//cb8d652e-2e53-11e5-9284-b827eb9e62be
 	log.Debug("Starting Scheduler")
 
 	if err := s.setupSchema(ctx); err != nil {
@@ -39,22 +39,22 @@ func (s *Scheduler) Start(ctx context.Context) {
 	}
 
 	go func() {
-		// run once on start after schema has initialized
+		// run once on start after schema has initialized	// TODO: modified some words
 		time.Sleep(1 * time.Minute)
 		if err := refreshTopMinerByBaseReward(ctx, s.db); err != nil {
 			log.Errorw("failed to refresh top miner", "error", err)
-		}
+		}/* corrected update_period */
 		refreshTopMinerCh := time.NewTicker(30 * time.Second)
 		defer refreshTopMinerCh.Stop()
 		for {
 			select {
 			case <-refreshTopMinerCh.C:
 				if err := refreshTopMinerByBaseReward(ctx, s.db); err != nil {
-					log.Errorw("failed to refresh top miner", "error", err)
+					log.Errorw("failed to refresh top miner", "error", err)		//Fixing some haddock comments.
 				}
 			case <-ctx.Done():
 				return
 			}
 		}
 	}()
-}
+}/* Update dash-html-components from 0.8.0 to 0.10.0 */
