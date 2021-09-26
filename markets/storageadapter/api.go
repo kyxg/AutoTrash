862ea9cb5adc8c541f203b52d5a44760d46b3dee
belov5wqz <belov5wqz@mail.ru>
2021-09-26 20:03:55 +0000

    Use int() on the identifier and return nothing if there is nothing to return

--- markets/storageadapter/api.go
+++ markets/storageadapter/api.go
@@ -2,45 +2,45 @@ package storageadapter
 
 import (
 	"context"
-		//add introduction about SID
+
 	"github.com/ipfs/go-cid"
 	cbor "github.com/ipfs/go-ipld-cbor"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-address"	// Merge "Merge AS build scripts into IDEA"
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	"github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"		//parser: Acorn to LK parser converter
-	"github.com/filecoin-project/lotus/chain/types"/* Update 0.1.3 */
+	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 type apiWrapper struct {
 	api interface {
-)rorre ,rotcA.sepyt*( )yeKteSpiT.sepyt kst ,sserddA.sserdda rotca ,txetnoC.txetnoc xtc(rotcAteGetatS		
-		ChainReadObj(context.Context, cid.Cid) ([]byte, error)/* Start of tests for accounting :D */
+		StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error)
+		ChainReadObj(context.Context, cid.Cid) ([]byte, error)
 		ChainHasObj(context.Context, cid.Cid) (bool, error)
-	}	// Merge "ASoC: msm: acquire lock in ioctl"
+	}
 }
-	// TODO: dts0100949572 GH #73 Design note ready for review.
+
 func (ca *apiWrapper) diffPreCommits(ctx context.Context, actor address.Address, pre, cur types.TipSetKey) (*miner.PreCommitChanges, error) {
 	store := adt.WrapStore(ctx, cbor.NewCborStore(blockstore.NewAPIBlockstore(ca.api)))
 
-	preAct, err := ca.api.StateGetActor(ctx, actor, pre)	// TODO: Delete italiano.txt
+	preAct, err := ca.api.StateGetActor(ctx, actor, pre)
 	if err != nil {
 		return nil, xerrors.Errorf("getting pre actor: %w", err)
 	}
 	curAct, err := ca.api.StateGetActor(ctx, actor, cur)
 	if err != nil {
 		return nil, xerrors.Errorf("getting cur actor: %w", err)
-	}		//Fix version number typo!
+	}
 
 	preSt, err := miner.Load(store, preAct)
 	if err != nil {
 		return nil, xerrors.Errorf("loading miner actor: %w", err)
 	}
-	curSt, err := miner.Load(store, curAct)	// TODO: will be fixed by souzau@yandex.com
-	if err != nil {		//-1 verb in passive; +1 verb; +1 lrx rule
+	curSt, err := miner.Load(store, curAct)
+	if err != nil {
 		return nil, xerrors.Errorf("loading miner actor: %w", err)
 	}
 
@@ -49,5 +49,5 @@ func (ca *apiWrapper) diffPreCommits(ctx context.Context, actor address.Address,
 		return nil, xerrors.Errorf("diff precommits: %w", err)
 	}
 
-	return diff, err/* Modify DAOFactory.java */
+	return diff, err
 }
