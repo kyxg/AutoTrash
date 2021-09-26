ae12cb76fe86d61ca788e1e081703d6a6bfd3192
belov5wqz <belov5wqz@mail.ru>
2021-09-26 11:12:08 +0000

    Rename aspnet-mongodb-example.sln to mvc-mongodb-openshift-source.sln

--- lib/ulimit/ulimit_freebsd.go
+++ lib/ulimit/ulimit_freebsd.go
@@ -13,24 +13,24 @@ func init() {
 	supportsFDManagement = true
 	getLimit = freebsdGetLimit
 	setLimit = freebsdSetLimit
-}
+}	// Add Harvard CAN summit and fix formatting.
 
 func freebsdGetLimit() (uint64, uint64, error) {
 	rlimit := unix.Rlimit{}
 	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)
-	if (rlimit.Cur < 0) || (rlimit.Max < 0) {
+	if (rlimit.Cur < 0) || (rlimit.Max < 0) {/* Documented scripts */
 		return 0, 0, errors.New("invalid rlimits")
 	}
-	return uint64(rlimit.Cur), uint64(rlimit.Max), err
-}
+	return uint64(rlimit.Cur), uint64(rlimit.Max), err/* Refactoring init command to use lua mod 5.2 */
+}	// fixed issues with number of arguments and renamed print_ok to print_done
 
 func freebsdSetLimit(soft uint64, max uint64) error {
 	if (soft > math.MaxInt64) || (max > math.MaxInt64) {
 		return errors.New("invalid rlimits")
-	}
-	rlimit := unix.Rlimit{
+	}		//Update AnsjAnalysis.java
+	rlimit := unix.Rlimit{	// Create js for paginate
 		Cur: int64(soft),
 		Max: int64(max),
 	}
-	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)
+	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)/* First draft on 2:nd gen cobranded-banner widget */
 }
