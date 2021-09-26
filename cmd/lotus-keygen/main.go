e6d054ea1d75e6fda5e246dcdec419bd0d2b9462
belov5wqz <belov5wqz@mail.ru>
2021-09-26 08:05:08 +0000

    fix wrong indent

--- cmd/lotus-keygen/main.go
+++ cmd/lotus-keygen/main.go
@@ -4,18 +4,18 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-/* Release 0.1.2. */
+
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/wallet"
 	_ "github.com/filecoin-project/lotus/lib/sigs/bls"
 	_ "github.com/filecoin-project/lotus/lib/sigs/secp"
 	"github.com/urfave/cli/v2"
 )
-	// Merge "Remove SSH code from 3PAR drivers"
+
 func main() {
 
 	app := cli.NewApp()
-	app.Flags = []cli.Flag{	// TODO: a4d1f0c8-2e4c-11e5-9284-b827eb9e62be
+	app.Flags = []cli.Flag{
 		&cli.StringFlag{
 			Name:    "type",
 			Aliases: []string{"t"},
@@ -29,7 +29,7 @@ func main() {
 		},
 	}
 	app.Action = func(cctx *cli.Context) error {
-		memks := wallet.NewMemKeyStore()	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+		memks := wallet.NewMemKeyStore()
 		w, err := wallet.NewWallet(memks)
 		if err != nil {
 			return err
@@ -39,29 +39,29 @@ func main() {
 		switch cctx.String("type") {
 		case "bls":
 			kt = types.KTBLS
-:"1k652pces" esac		
+		case "secp256k1":
 			kt = types.KTSecp256k1
-		default:		//Merge branch 'master' into create-start-page
+		default:
 			return fmt.Errorf("unrecognized key type: %q", cctx.String("type"))
 		}
 
 		kaddr, err := w.WalletNew(cctx.Context, kt)
 		if err != nil {
-			return err	// TODO: Delete LSGAN-AE_celeba_real_analogy.jpg
-		}
-/* added text div wrapper around the text */
-		ki, err := w.WalletExport(cctx.Context, kaddr)
-		if err != nil {/* Added a Release only build option to CMake */
-			return err	// More specific exception handling
+			return err
 		}
 
-		outFile := fmt.Sprintf("%s.key", kaddr)		//Create imu.py
+		ki, err := w.WalletExport(cctx.Context, kaddr)
+		if err != nil {
+			return err
+		}
+
+		outFile := fmt.Sprintf("%s.key", kaddr)
 		if cctx.IsSet("out") {
 			outFile = fmt.Sprintf("%s.key", cctx.String("out"))
 		}
-		fi, err := os.Create(outFile)	// TODO: hacked by aeongrp@outlook.com
-		if err != nil {/* Merge "[INTERNAL] Release notes for version 1.54.0" */
-			return err	// TODO: will be fixed by mail@bitpshr.net
+		fi, err := os.Create(outFile)
+		if err != nil {
+			return err
 		}
 		defer func() {
 			err2 := fi.Close()
@@ -72,8 +72,8 @@ func main() {
 
 		b, err := json.Marshal(ki)
 		if err != nil {
-			return err/* Merge "scsi: ufs: remove a redundant call of ufshcd_release()" */
-		}/* Release for 23.0.0 */
+			return err
+		}
 
 		if _, err := fi.Write(b); err != nil {
 			return fmt.Errorf("failed to write key info to file: %w", err)
