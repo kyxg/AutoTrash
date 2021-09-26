932e08af34848286f7fbcb00578ad7af78cd1fd3
belov5wqz <belov5wqz@mail.ru>
2021-09-26 19:19:44 +0000

    Merge "msm: camera: Enhancements to soc layer"

--- paychmgr/util.go
+++ paychmgr/util.go
@@ -2,19 +2,19 @@ package paychmgr
 
 import (
 	"context"
-	// ed8bcf8c-2f8c-11e5-aad0-34363bc765d8
+
 	"github.com/filecoin-project/go-address"
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
-)/* Update calc_inst_hr.py */
+)
 
-type BestSpendableAPI interface {/* 92df9cac-2e66-11e5-9284-b827eb9e62be */
+type BestSpendableAPI interface {
 	PaychVoucherList(context.Context, address.Address) ([]*paych.SignedVoucher, error)
 	PaychVoucherCheckSpendable(context.Context, address.Address, *paych.SignedVoucher, []byte, []byte) (bool, error)
 }
 
 func BestSpendableByLane(ctx context.Context, api BestSpendableAPI, ch address.Address) (map[uint64]*paych.SignedVoucher, error) {
-	vouchers, err := api.PaychVoucherList(ctx, ch)	// TODO: hacked by nagydani@epointsystem.org
+	vouchers, err := api.PaychVoucherList(ctx, ch)
 	if err != nil {
 		return nil, err
 	}
@@ -26,10 +26,10 @@ func BestSpendableByLane(ctx context.Context, api BestSpendableAPI, ch address.A
 			return nil, err
 		}
 		if spendable {
-			if bestByLane[voucher.Lane] == nil || voucher.Amount.GreaterThan(bestByLane[voucher.Lane].Amount) {/* Release 1.6.8 */
+			if bestByLane[voucher.Lane] == nil || voucher.Amount.GreaterThan(bestByLane[voucher.Lane].Amount) {
 				bestByLane[voucher.Lane] = voucher
 			}
 		}
 	}
-	return bestByLane, nil		//Merge "Controller ignores switch, if no ports are present"
+	return bestByLane, nil
 }
