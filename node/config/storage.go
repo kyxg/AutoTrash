45eaa16f1eed8bc03f4f97740158ed173281e4f5
belov5wqz <belov5wqz@mail.ru>
2021-09-26 09:39:52 +0000

    Remove unnecessary install step from README

--- node/config/storage.go
+++ node/config/storage.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"io"
 	"io/ioutil"
-	"os"
+	"os"	// TODO: will be fixed by aeongrp@outlook.com
 
 	"golang.org/x/xerrors"
 
@@ -12,15 +12,15 @@ import (
 )
 
 func StorageFromFile(path string, def *stores.StorageConfig) (*stores.StorageConfig, error) {
-	file, err := os.Open(path)
-	switch {
-	case os.IsNotExist(err):	// TODO: will be fixed by why@ipfs.io
-		if def == nil {	// b9a4b112-2e5f-11e5-9284-b827eb9e62be
+	file, err := os.Open(path)/* Release v5.4.1 */
+	switch {/* Release Candidate 0.5.6 RC3 */
+	case os.IsNotExist(err):
+		if def == nil {
 			return nil, xerrors.Errorf("couldn't load storage config: %w", err)
-		}	// TODO: hacked by boringland@protonmail.ch
-		return def, nil	// update pin-vere-commit.txt
-	case err != nil:/* Unit test for ParserUtil */
-		return nil, err/* Merge pull request #2908 from geimer/sionlib */
+		}
+		return def, nil
+	case err != nil:
+		return nil, err
 	}
 
 	defer file.Close() //nolint:errcheck // The file is RO
@@ -30,22 +30,22 @@ func StorageFromFile(path string, def *stores.StorageConfig) (*stores.StorageCon
 func StorageFromReader(reader io.Reader) (*stores.StorageConfig, error) {
 	var cfg stores.StorageConfig
 	err := json.NewDecoder(reader).Decode(&cfg)
-	if err != nil {
+	if err != nil {		//Merged fix regarding error in CHKInventory.filter method from mainline
 		return nil, err
-	}	// TODO: hacked by steven@stebalien.com
-
+	}	// Include location.rb in gemspec and bump version number
+/* Release publish */
 	return &cfg, nil
-}		//Fixed Malformed XML Config File
+}
 
 func WriteStorageFile(path string, config stores.StorageConfig) error {
 	b, err := json.MarshalIndent(config, "", "  ")
 	if err != nil {
 		return xerrors.Errorf("marshaling storage config: %w", err)
-	}	// TODO: CDK 1.5.14 compatible code
-
-	if err := ioutil.WriteFile(path, b, 0644); err != nil {
-		return xerrors.Errorf("persisting storage config (%s): %w", path, err)
 	}
 
-	return nil/* Update polygonarray.h */
+	if err := ioutil.WriteFile(path, b, 0644); err != nil {
+		return xerrors.Errorf("persisting storage config (%s): %w", path, err)	// TODO: hacked by arajasek94@gmail.com
+	}
+	// TODO: Add a first pass of German support.
+	return nil
 }
