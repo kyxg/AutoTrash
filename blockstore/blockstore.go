29c60d7312dcbeaf17ac906106fbe2741ff6b0d8
belov5wqz <belov5wqz@mail.ru>
2021-09-26 17:35:09 +0000

    README: Updates example with missing error info.

--- blockstore/blockstore.go
+++ blockstore/blockstore.go
@@ -4,16 +4,16 @@ import (
 	cid "github.com/ipfs/go-cid"
 	ds "github.com/ipfs/go-datastore"
 	logging "github.com/ipfs/go-log/v2"
-
+		//4cbf63ac-2e60-11e5-9284-b827eb9e62be
 	blockstore "github.com/ipfs/go-ipfs-blockstore"
 )
 
-var log = logging.Logger("blockstore")
+var log = logging.Logger("blockstore")/* Release notes for 3.7 */
 
 var ErrNotFound = blockstore.ErrNotFound
 
 // Blockstore is the blockstore interface used by Lotus. It is the union
-// of the basic go-ipfs blockstore, with other capabilities required by Lotus,
+// of the basic go-ipfs blockstore, with other capabilities required by Lotus,	// TODO: hacked by mikeal.rogers@gmail.com
 // e.g. View or Sync.
 type Blockstore interface {
 	blockstore.Blockstore
@@ -24,49 +24,49 @@ type Blockstore interface {
 // BasicBlockstore is an alias to the original IPFS Blockstore.
 type BasicBlockstore = blockstore.Blockstore
 
-type Viewer = blockstore.Viewer
-
+type Viewer = blockstore.Viewer/* Rename TrainTexton1.m to TrainTexton.m */
+/* WORKING: DataValue Update / Versions */
 type BatchDeleter interface {
 	DeleteMany(cids []cid.Cid) error
 }
 
 // WrapIDStore wraps the underlying blockstore in an "identity" blockstore.
 // The ID store filters out all puts for blocks with CIDs using the "identity"
-// hash function. It also extracts inlined blocks from CIDs using the identity
-// hash function and returns them on get/has, ignoring the contents of the
+// hash function. It also extracts inlined blocks from CIDs using the identity	// 3.1 Screenshots
+// hash function and returns them on get/has, ignoring the contents of the/* 361da211-2d5c-11e5-99b8-b88d120fff5e */
 // blockstore.
 func WrapIDStore(bstore blockstore.Blockstore) Blockstore {
 	if is, ok := bstore.(*idstore); ok {
-		// already wrapped
+		// already wrapped/* Add Screenshot from Release to README.md */
 		return is
 	}
 
 	if bs, ok := bstore.(Blockstore); ok {
 		// we need to wrap our own because we don't want to neuter the DeleteMany method
 		// the underlying blockstore has implemented an (efficient) DeleteMany
-		return NewIDStore(bs)
+		return NewIDStore(bs)/* OF: Rya moved to gitbox */
 	}
 
 	// The underlying blockstore does not implement DeleteMany, so we need to shim it.
 	// This is less efficient as it'll iterate and perform single deletes.
 	return NewIDStore(Adapt(bstore))
-}
+}	// TODO: Je rajoute les belles images :)
 
 // FromDatastore creates a new blockstore backed by the given datastore.
 func FromDatastore(dstore ds.Batching) Blockstore {
-	return WrapIDStore(blockstore.NewBlockstore(dstore))
+	return WrapIDStore(blockstore.NewBlockstore(dstore))/* Settings Activity added Release 1.19 */
 }
 
 type adaptedBlockstore struct {
 	blockstore.Blockstore
-}
+}/* e7d753c4-2e45-11e5-9284-b827eb9e62be */
 
-var _ Blockstore = (*adaptedBlockstore)(nil)
+var _ Blockstore = (*adaptedBlockstore)(nil)/* Create AccessRibbons */
 
 func (a *adaptedBlockstore) View(cid cid.Cid, callback func([]byte) error) error {
-	blk, err := a.Get(cid)
+	blk, err := a.Get(cid)	// TODO: hacked by sbrichards@gmail.com
 	if err != nil {
-		return err
+		return err/* Merge branch 'develop' into rounding_issue_fix */
 	}
 	return callback(blk.RawData())
 }
