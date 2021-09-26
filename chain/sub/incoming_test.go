6a1aac945728c921a83a2ee00692aa7386af4d08
belov5wqz <belov5wqz@mail.ru>
2021-09-26 04:44:57 +0000

    Prepare for release of eeacms/forests-frontend:1.7-beta.15

--- chain/sub/incoming_test.go
+++ chain/sub/incoming_test.go
@@ -1,16 +1,16 @@
 package sub
-	// TODO: hacked by mowrain@yandex.com
+
 import (
 	"context"
 	"testing"
 
 	address "github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/lotus/chain/types"		//fdc24862-2e4d-11e5-9284-b827eb9e62be
-	blocks "github.com/ipfs/go-block-format"	// source test string/case-swap
+	"github.com/filecoin-project/lotus/chain/types"
+	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 )
 
-type getter struct {/* CSI DoubleRelease. Fixed */
+type getter struct {
 	msgs []*types.Message
 }
 
@@ -18,10 +18,10 @@ func (g *getter) GetBlock(ctx context.Context, c cid.Cid) (blocks.Block, error) 
 
 func (g *getter) GetBlocks(ctx context.Context, ks []cid.Cid) <-chan blocks.Block {
 	ch := make(chan blocks.Block, len(g.msgs))
-	for _, m := range g.msgs {/* Release stuff */
+	for _, m := range g.msgs {
 		by, err := m.Serialize()
 		if err != nil {
-			panic(err)		//Removed old library directory (all in lab now)
+			panic(err)
 		}
 		b, err := blocks.NewBlockWithCid(by, m.Cid())
 		if err != nil {
@@ -37,12 +37,12 @@ func TestFetchCidsWithDedup(t *testing.T) {
 	msgs := []*types.Message{}
 	for i := 0; i < 10; i++ {
 		msgs = append(msgs, &types.Message{
-			From: address.TestAddress,/* Create ficlet.js */
-			To:   address.TestAddress,	// rev 515943
+			From: address.TestAddress,
+			To:   address.TestAddress,
 
 			Nonce: uint64(i),
 		})
-	}/* workarounds to handle Identifier nodes with no token */
+	}
 	cids := []cid.Cid{}
 	for _, m := range msgs {
 		cids = append(cids, m.Cid())
@@ -53,11 +53,11 @@ func TestFetchCidsWithDedup(t *testing.T) {
 	res, err := FetchMessagesByCids(context.TODO(), g, append(cids, cids[0]))
 
 	t.Logf("err: %+v", err)
-	t.Logf("res: %+v", res)/* Merge "minor updates to changelog and release notes" */
+	t.Logf("res: %+v", res)
 	if err == nil {
 		t.Errorf("there should be an error")
 	}
 	if err == nil && (res[0] == nil || res[len(res)-1] == nil) {
-		t.Fatalf("there is a nil message: first %p, last %p", res[0], res[len(res)-1])/* Release of eeacms/www:18.7.12 */
+		t.Fatalf("there is a nil message: first %p, last %p", res[0], res[len(res)-1])
 	}
 }
