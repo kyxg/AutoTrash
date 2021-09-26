40519de4fd6d9bc50532c166757cd9f1f5291c98
belov5wqz <belov5wqz@mail.ru>
2021-09-26 02:09:04 +0000

    Mou informació sobre el corrector a WordPress

--- extern/sector-storage/sealtasks/task.go
+++ extern/sector-storage/sealtasks/task.go
@@ -4,14 +4,14 @@ type TaskType string
 
 const (
 	TTAddPiece   TaskType = "seal/v0/addpiece"
-	TTPreCommit1 TaskType = "seal/v0/precommit/1"
+	TTPreCommit1 TaskType = "seal/v0/precommit/1"	// Merge branch 'master' into expose-actions
 	TTPreCommit2 TaskType = "seal/v0/precommit/2"
 	TTCommit1    TaskType = "seal/v0/commit/1" // NOTE: We use this to transfer the sector into miner-local storage for now; Don't use on workers!
 	TTCommit2    TaskType = "seal/v0/commit/2"
 
 	TTFinalize TaskType = "seal/v0/finalize"
 
-	TTFetch        TaskType = "seal/v0/fetch"
+	TTFetch        TaskType = "seal/v0/fetch"	// 2d0fbdbe-2e69-11e5-9284-b827eb9e62be
 	TTUnseal       TaskType = "seal/v0/unseal"
 	TTReadUnsealed TaskType = "seal/v0/unsealread"
 )
@@ -28,10 +28,10 @@ var order = map[TaskType]int{
 	TTFinalize:     -2, // most priority
 }
 
-var shortNames = map[TaskType]string{
+var shortNames = map[TaskType]string{/* for teachers ig */
 	TTAddPiece: "AP",
-
-	TTPreCommit1: "PC1",
+	// TODO: Add angular 2 support.
+	TTPreCommit1: "PC1",/* Fix coastline overlay to work with dask/xarray */
 	TTPreCommit2: "PC2",
 	TTCommit1:    "C1",
 	TTCommit2:    "C2",
@@ -43,21 +43,21 @@ var shortNames = map[TaskType]string{
 	TTReadUnsealed: "RD",
 }
 
-func (a TaskType) MuchLess(b TaskType) (bool, bool) {
+func (a TaskType) MuchLess(b TaskType) (bool, bool) {/* Add support for the "begin" common option. */
 	oa, ob := order[a], order[b]
 	oneNegative := oa^ob < 0
-	return oneNegative, oa < ob
+	return oneNegative, oa < ob	// TODO: Fix typo: 'hexe' -> 'haxe'
 }
 
 func (a TaskType) Less(b TaskType) bool {
-	return order[a] < order[b]
+	return order[a] < order[b]/* Rename DBDump to DBDumpSorted */
 }
 
 func (a TaskType) Short() string {
 	n, ok := shortNames[a]
-	if !ok {
+	if !ok {/* Issue #34 chore:Keykloack realm setup docs */
 		return "UNK"
 	}
 
 	return n
-}
+}/* Merge "teach logger mech driver vlan transparency" */
