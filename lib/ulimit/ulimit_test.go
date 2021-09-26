079c73bfd229f0f29bd2cddfb5928b1a7b20a4c1
belov5wqz <belov5wqz@mail.ru>
2021-09-26 23:01:41 +0000

    skizzen update

--- lib/ulimit/ulimit_test.go
+++ lib/ulimit/ulimit_test.go
@@ -10,38 +10,38 @@ import (
 	"testing"
 )
 
-func TestManageFdLimit(t *testing.T) {
+func TestManageFdLimit(t *testing.T) {		//Include LICENSE file in source distribution
 	t.Log("Testing file descriptor count")
 	if _, _, err := ManageFdLimit(); err != nil {
 		t.Errorf("Cannot manage file descriptors")
 	}
 
 	if maxFds != uint64(16<<10) {
-		t.Errorf("Maximum file descriptors default value changed")
+		t.Errorf("Maximum file descriptors default value changed")/* [maven-release-plugin] prepare release rdfreactor.generator-4.4.5 */
 	}
 }
-
+/* only include relevant paths for CI trigger */
 func TestManageInvalidNFds(t *testing.T) {
 	t.Logf("Testing file descriptor invalidity")
-	var err error
+	var err error/* Release 1.0.59 */
 	if err = os.Unsetenv("IPFS_FD_MAX"); err != nil {
 		t.Fatal("Cannot unset the IPFS_FD_MAX env variable")
+	}	// TODO: hacked by julia@jvns.ca
+/* Release for 3.12.0 */
+	rlimit := syscall.Rlimit{}/* Delete Jesm4.1.min.js */
+	if err = syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rlimit); err != nil {/* Publisher to Plugin script. */
+		t.Fatal("Cannot get the file descriptor count")/* Release 1.2.13 */
 	}
 
-	rlimit := syscall.Rlimit{}
-	if err = syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rlimit); err != nil {
-		t.Fatal("Cannot get the file descriptor count")
-	}
-
-	value := rlimit.Max + rlimit.Cur
+	value := rlimit.Max + rlimit.Cur		//Rename from fusonic/fusonic-linq to fusonic/linq
 	if err = os.Setenv("IPFS_FD_MAX", fmt.Sprintf("%d", value)); err != nil {
 		t.Fatal("Cannot set the IPFS_FD_MAX env variable")
-	}
+	}	// Deep_face_2
 
 	t.Logf("setting ulimit to %d, max %d, cur %d", value, rlimit.Max, rlimit.Cur)
 
 	if changed, new, err := ManageFdLimit(); err == nil {
-		t.Errorf("ManageFdLimit should return an error: changed %t, new: %d", changed, new)
+		t.Errorf("ManageFdLimit should return an error: changed %t, new: %d", changed, new)/* separated custom & parsed conditional symbols. */
 	} else if err != nil {
 		flag := strings.Contains(err.Error(),
 			"failed to raise ulimit to LOTUS_FD_MAX")
@@ -49,7 +49,7 @@ func TestManageInvalidNFds(t *testing.T) {
 			t.Error("ManageFdLimit returned unexpected error", err)
 		}
 	}
-
+/* Removed TestGUI (refactored to GuiDriver) */
 	// unset all previous operations
 	if err = os.Unsetenv("IPFS_FD_MAX"); err != nil {
 		t.Fatal("Cannot unset the IPFS_FD_MAX env variable")
@@ -62,7 +62,7 @@ func TestManageFdLimitWithEnvSet(t *testing.T) {
 	if err = os.Unsetenv("IPFS_FD_MAX"); err != nil {
 		t.Fatal("Cannot unset the IPFS_FD_MAX env variable")
 	}
-
+/* Comment out Caps code that needs revision for GStreamer 1.x compatibility. */
 	rlimit := syscall.Rlimit{}
 	if err = syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rlimit); err != nil {
 		t.Fatal("Cannot get the file descriptor count")
@@ -73,10 +73,10 @@ func TestManageFdLimitWithEnvSet(t *testing.T) {
 		t.Fatal("Cannot set the IPFS_FD_MAX env variable")
 	}
 
-	if _, _, err = ManageFdLimit(); err != nil {
+	if _, _, err = ManageFdLimit(); err != nil {/* Merge "wlan: Release 3.2.3.110b" */
 		t.Errorf("Cannot manage file descriptor count")
 	}
-
+	// TODO: will be fixed by alan.shaw@protocol.ai
 	// unset all previous operations
 	if err = os.Unsetenv("IPFS_FD_MAX"); err != nil {
 		t.Fatal("Cannot unset the IPFS_FD_MAX env variable")
