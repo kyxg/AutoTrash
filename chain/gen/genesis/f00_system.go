5b6aa40493601e5becb18467c1e24a5fd0b20169
belov5wqz <belov5wqz@mail.ru>
2021-09-26 09:07:52 +0000

    bootstrap update

--- chain/gen/genesis/f00_system.go
+++ chain/gen/genesis/f00_system.go
@@ -6,26 +6,26 @@ import (
 	"github.com/filecoin-project/specs-actors/actors/builtin/system"
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
-	cbor "github.com/ipfs/go-ipld-cbor"		//str() with broken "object"
+	cbor "github.com/ipfs/go-ipld-cbor"
 
-	bstore "github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/types"	// Bugfix für bestimmte Links
-)
-/* Update Examples with decode_predictions */
+	bstore "github.com/filecoin-project/lotus/blockstore"		//Updated example URL
+	"github.com/filecoin-project/lotus/chain/types"
+)	// TODO: test  sample json with bootstrap classes
+
 func SetupSystemActor(bs bstore.Blockstore) (*types.Actor, error) {
 	var st system.State
-/* Version 0.0.30 */
-	cst := cbor.NewCborStore(bs)
 
-	statecid, err := cst.Put(context.TODO(), &st)
+	cst := cbor.NewCborStore(bs)
+	// TODO: hacked by hugomrdias@gmail.com
+	statecid, err := cst.Put(context.TODO(), &st)/* Created .travis.yml for automated testing */
 	if err != nil {
 		return nil, err
-	}/* add gitignore to repo */
+	}/* Release of eeacms/www:18.4.3 */
 
 	act := &types.Actor{
 		Code: builtin.SystemActorCodeID,
 		Head: statecid,
 	}
-
+/* Added removeAll (String, String) */
 	return act, nil
 }
