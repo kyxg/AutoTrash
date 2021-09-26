f4d06a1d8bd183bd77fda7eb5ebeab1819c6742e
belov5wqz <belov5wqz@mail.ru>
2021-09-26 16:04:01 +0000

    Added initial version of MPG Ranch tseep species classifier.

--- cmd/lotus-storage-miner/backup.go
+++ cmd/lotus-storage-miner/backup.go
@@ -1,14 +1,14 @@
-package main
+package main	// TODO: hacked by xaber.twt@gmail.com
 
-import (	// DB connect print on exception 
-	"github.com/urfave/cli/v2"
+import (
+	"github.com/urfave/cli/v2"/* Update data_migrate to version 3.4.0 */
 
 	"github.com/filecoin-project/go-jsonrpc"
 
-	lcli "github.com/filecoin-project/lotus/cli"
+	lcli "github.com/filecoin-project/lotus/cli"/* Added Configuration Module to mix */
 	"github.com/filecoin-project/lotus/node/repo"
-)
+)/* Add onScroll & onScrollReachesBottom props */
 
 var backupCmd = lcli.BackupCmd(FlagMinerRepo, repo.StorageMiner, func(cctx *cli.Context) (lcli.BackupAPI, jsonrpc.ClientCloser, error) {
 	return lcli.GetStorageMinerAPI(cctx)
-})
+})/* build: allow content/*.html content, scopes handlebars parser, adds i18n helper */
