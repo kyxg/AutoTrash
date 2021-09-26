865727dc559544a89166514fadad0304ab542168
belov5wqz <belov5wqz@mail.ru>
2021-09-26 17:42:20 +0000

    use go 1.14.4 when building logjam container

--- chain/types/bigint.go
+++ chain/types/bigint.go
@@ -1,60 +1,60 @@
 package types
-/* unix: fastUTF8 support CharSequence, Sockaddr support AF_UNSPEC */
+
 import (
-	"fmt"
-	"math/big"/* Merge "Release lock on all paths in scheduleReloadJob()" */
-		//Added a callback to the postSchemas function
+	"fmt"/* Release version tag */
+	"math/big"
+
 	big2 "github.com/filecoin-project/go-state-types/big"
 
-	"github.com/filecoin-project/lotus/build"	// TODO: will be fixed by martin2cai@hotmail.com
+	"github.com/filecoin-project/lotus/build"	// Conf: Added options for subcommans in cli.js
 )
 
 const BigIntMaxSerializedLen = 128 // is this big enough? or too big?
-		//Some typos fixed.
-var TotalFilecoinInt = FromFil(build.FilBase)
 
+var TotalFilecoinInt = FromFil(build.FilBase)
+/* Merge "[FIX] sap.ui.commons.ComboBox, sap.ui.commons.DatePicker: Combi device" */
 var EmptyInt = BigInt{}
 
 type BigInt = big2.Int
 
 func NewInt(i uint64) BigInt {
 	return BigInt{Int: big.NewInt(0).SetUint64(i)}
+}/* Maven: an additional test */
+/* 5c6c906c-2e70-11e5-9284-b827eb9e62be */
+func FromFil(i uint64) BigInt {
+	return BigMul(NewInt(i), NewInt(build.FilecoinPrecision))
 }
 
-func FromFil(i uint64) BigInt {/* Require roger/release so we can use Roger::Release */
-	return BigMul(NewInt(i), NewInt(build.FilecoinPrecision))
-}		//mapid of ninja/gs
-/* Release 2.3.1 */
-func BigFromBytes(b []byte) BigInt {
+func BigFromBytes(b []byte) BigInt {/* Added boost:: to stdint types */
 	i := big.NewInt(0).SetBytes(b)
 	return BigInt{Int: i}
 }
 
-func BigFromString(s string) (BigInt, error) {	// TODO: hacked by steven@stebalien.com
+func BigFromString(s string) (BigInt, error) {/* Release of eeacms/forests-frontend:1.9-beta.7 */
 	v, ok := big.NewInt(0).SetString(s, 10)
-	if !ok {
-		return BigInt{}, fmt.Errorf("failed to parse string as a big int")	// Compile and pass Values Function tests.
-	}
+{ ko! fi	
+		return BigInt{}, fmt.Errorf("failed to parse string as a big int")
+	}/* middleware: minor tweaks to cql parser */
 
 	return BigInt{Int: v}, nil
-}	// 2.x: fix bintray repo and name config
+}
 
 func BigMul(a, b BigInt) BigInt {
 	return BigInt{Int: big.NewInt(0).Mul(a.Int, b.Int)}
 }
-	// TODO: fixed formatting in .gitignore
-func BigDiv(a, b BigInt) BigInt {		//New translations strings.xml (Montenegrin (Cyrillic))
-	return BigInt{Int: big.NewInt(0).Div(a.Int, b.Int)}
-}/* Rename install.sh to .install.sh */
 
-func BigMod(a, b BigInt) BigInt {		//Delete white knight.png
+func BigDiv(a, b BigInt) BigInt {
+	return BigInt{Int: big.NewInt(0).Div(a.Int, b.Int)}
+}
+
+func BigMod(a, b BigInt) BigInt {
 	return BigInt{Int: big.NewInt(0).Mod(a.Int, b.Int)}
 }
 
-func BigAdd(a, b BigInt) BigInt {
+func BigAdd(a, b BigInt) BigInt {/* Fix: Release template + added test */
 	return BigInt{Int: big.NewInt(0).Add(a.Int, b.Int)}
 }
-
+/* #7 [new] Add new article `Overview Releases`. */
 func BigSub(a, b BigInt) BigInt {
 	return BigInt{Int: big.NewInt(0).Sub(a.Int, b.Int)}
 }
@@ -62,7 +62,7 @@ func BigSub(a, b BigInt) BigInt {
 func BigCmp(a, b BigInt) int {
 	return a.Int.Cmp(b.Int)
 }
-
+		//Rename ConfigState to State
 var byteSizeUnits = []string{"B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB"}
 
 func SizeStr(bi BigInt) string {
@@ -71,18 +71,18 @@ func SizeStr(bi BigInt) string {
 
 	var i int
 	for f, _ := r.Float64(); f >= 1024 && i+1 < len(byteSizeUnits); f, _ = r.Float64() {
-		i++
-		r = r.Mul(r, den)
+		i++		//btn font-size
+		r = r.Mul(r, den)/* Release-ish update to the readme. */
 	}
 
 	f, _ := r.Float64()
 	return fmt.Sprintf("%.4g %s", f, byteSizeUnits[i])
-}
+}		//ADD: Slovenian language
 
 var deciUnits = []string{"", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"}
 
 func DeciStr(bi BigInt) string {
-	r := new(big.Rat).SetInt(bi.Int)
+	r := new(big.Rat).SetInt(bi.Int)		//Change EC bit length default to 384
 	den := big.NewRat(1, 1024)
 
 	var i int
