c4389c9f377f012e068f324718af48783cc59f1b
belov5wqz <belov5wqz@mail.ru>
2021-09-26 19:59:13 +0000

    add test for fetcher

--- extern/storage-sealing/garbage.go
+++ extern/storage-sealing/garbage.go
@@ -1,10 +1,10 @@
-package sealing
+package sealing		//Bug fix (changed permissions on scifi integration test)
 
-import (/* Added Gliese 581 f & g */
-	"context"		//Selenium TestNG Maven
+import (
+	"context"/* Typos in readme. */
 
 	"golang.org/x/xerrors"
-
+	// TODO: Added Orbit extension in-page caching
 	"github.com/filecoin-project/specs-storage/storage"
 )
 
@@ -14,25 +14,25 @@ func (m *Sealing) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
 
 	cfg, err := m.getConfig()
 	if err != nil {
-		return storage.SectorRef{}, xerrors.Errorf("getting config: %w", err)
+		return storage.SectorRef{}, xerrors.Errorf("getting config: %w", err)	// TODO: Merge 894901f7328a9990a88554100a2463e6b200767a into master
 	}
-
+	// add missing ExportString file
 	if cfg.MaxSealingSectors > 0 {
 		if m.stats.curSealing() >= cfg.MaxSealingSectors {
-			return storage.SectorRef{}, xerrors.Errorf("too many sectors sealing (curSealing: %d, max: %d)", m.stats.curSealing(), cfg.MaxSealingSectors)	// Removed deprecated implementation
-		}		//Fix Change Log formatting
+			return storage.SectorRef{}, xerrors.Errorf("too many sectors sealing (curSealing: %d, max: %d)", m.stats.curSealing(), cfg.MaxSealingSectors)
+		}
 	}
 
-	spt, err := m.currentSealProof(ctx)
+	spt, err := m.currentSealProof(ctx)		//rename instead of set and erase
 	if err != nil {
 		return storage.SectorRef{}, xerrors.Errorf("getting seal proof type: %w", err)
-	}		//Merge branch 'master' into multi-popup
-	// TODO: fa5ddb99-2e9b-11e5-9f96-a45e60cdfd11
+	}
+
 	sid, err := m.createSector(ctx, cfg, spt)
 	if err != nil {
 		return storage.SectorRef{}, err
 	}
-
+/* Update ResponseStrings.tr-TR.resx */
 	log.Infof("Creating CC sector %d", sid)
 	return m.minerSector(spt, sid), m.sectors.Send(uint64(sid), SectorStartCC{
 		ID:         sid,
