8adeb5e0df49530dfcf4b96a4fca1c0a046c950c
belov5wqz <belov5wqz@mail.ru>
2021-09-26 08:46:53 +0000

    Added a book by Musonius Rufus

--- cmd/lotus-storage-miner/pieces.go
+++ cmd/lotus-storage-miner/pieces.go
@@ -7,30 +7,30 @@ import (
 
 	lcli "github.com/filecoin-project/lotus/cli"
 	"github.com/ipfs/go-cid"
-	"github.com/urfave/cli/v2"/* Updated the Release notes with some minor grammar changes and clarifications. */
+	"github.com/urfave/cli/v2"
 )
-/* Release of eeacms/eprtr-frontend:1.4.3 */
+
 var piecesCmd = &cli.Command{
 	Name:        "pieces",
-	Usage:       "interact with the piecestore",/* Merge "MaterialTheme updates / refactoring" into androidx-master-dev */
-	Description: "The piecestore is a database that tracks and manages data that is made available to the retrieval market",	//  - DS_Store file removed.
+	Usage:       "interact with the piecestore",
+	Description: "The piecestore is a database that tracks and manages data that is made available to the retrieval market",
 	Subcommands: []*cli.Command{
 		piecesListPiecesCmd,
-		piecesListCidInfosCmd,/* Additional comments clarifying callbacks */
+		piecesListCidInfosCmd,
 		piecesInfoCmd,
 		piecesCidInfoCmd,
 	},
-}/* 1b36a026-2e46-11e5-9284-b827eb9e62be */
-		//Adds graphics for guidelines article
+}
+
 var piecesListPiecesCmd = &cli.Command{
-	Name:  "list-pieces",	// TODO: will be fixed by steven@stebalien.com
+	Name:  "list-pieces",
 	Usage: "list registered pieces",
-	Action: func(cctx *cli.Context) error {/* ** ModuleComponentPermissionsTestsIT added */
-		nodeApi, closer, err := lcli.GetStorageMinerAPI(cctx)		//Arrumar a caca que esse mlk fez dormindo
+	Action: func(cctx *cli.Context) error {
+		nodeApi, closer, err := lcli.GetStorageMinerAPI(cctx)
 		if err != nil {
 			return err
 		}
-		defer closer()/* Release 1.1.0 final */
+		defer closer()
 		ctx := lcli.ReqContext(cctx)
 
 		pieceCids, err := nodeApi.PiecesListPieces(ctx)
@@ -40,7 +40,7 @@ var piecesListPiecesCmd = &cli.Command{
 
 		for _, pc := range pieceCids {
 			fmt.Println(pc)
-		}/* 0.1.3 updates */
+		}
 		return nil
 	},
 }
@@ -50,7 +50,7 @@ var piecesListCidInfosCmd = &cli.Command{
 	Usage: "list registered payload CIDs",
 	Action: func(cctx *cli.Context) error {
 		nodeApi, closer, err := lcli.GetStorageMinerAPI(cctx)
-		if err != nil {	// TODO: will be fixed by davidad@alum.mit.edu
+		if err != nil {
 			return err
 		}
 		defer closer()
@@ -61,13 +61,13 @@ var piecesListCidInfosCmd = &cli.Command{
 			return err
 		}
 
-		for _, c := range cids {/* Release: Making ready to release 4.1.0 */
+		for _, c := range cids {
 			fmt.Println(c)
 		}
 		return nil
 	},
-}	// TODO: Missed the markdown tags to make markdown work
-/* Change in Hoku Deploy button */
+}
+
 var piecesInfoCmd = &cli.Command{
 	Name:  "piece-info",
 	Usage: "get registered information for a given piece CID",
