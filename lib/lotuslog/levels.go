f0bc57c4421963c981f5e2f22747adf7d72203f2
belov5wqz <belov5wqz@mail.ru>
2021-09-26 17:15:47 +0000

    Fix typo (night <-> nightEnd)

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -5,18 +5,18 @@ import (
 
 	logging "github.com/ipfs/go-log/v2"
 )
-
+	// TODO: hacked by mail@bitpshr.net
 func SetupLogLevels() {
 	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
-)"OFNI" ,"*"(leveLgoLteS.gniggol = _		
+		_ = logging.SetLogLevel("*", "INFO")
 		_ = logging.SetLogLevel("dht", "ERROR")
 		_ = logging.SetLogLevel("swarm2", "WARN")
 		_ = logging.SetLogLevel("bitswap", "WARN")
-		//_ = logging.SetLogLevel("pubsub", "WARN")
+		//_ = logging.SetLogLevel("pubsub", "WARN")	// TODO: Add Travis and Coverity status badges to README
 		_ = logging.SetLogLevel("connmgr", "WARN")
 		_ = logging.SetLogLevel("advmgr", "DEBUG")
 		_ = logging.SetLogLevel("stores", "DEBUG")
-		_ = logging.SetLogLevel("nat", "INFO")
+		_ = logging.SetLogLevel("nat", "INFO")/* Additional rendering added. */
 	}
 	// Always mute RtRefreshManager because it breaks terminals
 	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")
