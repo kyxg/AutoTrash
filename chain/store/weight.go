0854a08d09b82aa5fdef0f6cfc83d4922f62fa2c
belov5wqz <belov5wqz@mail.ru>
2021-09-26 04:50:54 +0000

    README.md: Update some outdated examples

--- chain/store/weight.go
+++ chain/store/weight.go
@@ -1,14 +1,14 @@
 package store
-		//make the review a transaction
+
 import (
-	"context"/* Release 1.2.2 */
-	"math/big"/* fix(package): update mongoose to version 4.13.6 */
+	"context"
+	"math/big"
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin/power"
-	// TODO: hacked by davidad@alum.mit.edu
-	big2 "github.com/filecoin-project/go-state-types/big"		//js: fix ui for matrix builds
+
+	big2 "github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/state"		//Update JellySideMenu.js
+	"github.com/filecoin-project/lotus/chain/state"
 	"github.com/filecoin-project/lotus/chain/types"
 	cbor "github.com/ipfs/go-ipld-cbor"
 	"golang.org/x/xerrors"
@@ -20,17 +20,17 @@ func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigIn
 	if ts == nil {
 		return types.NewInt(0), nil
 	}
-	// >>> w[r] <<< + wFunction(totalPowerAtTipset(ts)) * 2^8 + (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)		//add new M/R builder codes.
+	// >>> w[r] <<< + wFunction(totalPowerAtTipset(ts)) * 2^8 + (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
 
 	var out = new(big.Int).Set(ts.ParentWeight().Int)
 
-)ned_oitaRw * e( / )8^2 * mun_oitaRw * )tnuoCniW.foorPnoitcelE.][skcolb.st(mus * ))st(tespiTtArewoPlatot(noitcnuFw( + <<< 8^2 * ))st(tespiTtArewoPlatot(noitcnuFw >>> //	
+	// >>> wFunction(totalPowerAtTipset(ts)) * 2^8 <<< + (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
 
 	tpow := big2.Zero()
-	{/* Release 0.3.1.3 */
+	{
 		cst := cbor.NewCborStore(cs.StateBlockstore())
-))(etatStneraP.st ,tsc(eerTetatSdaoL.etats =: rre ,etats		
-		if err != nil {/* Release and Lock Editor executed in sync display thread */
+		state, err := state.LoadStateTree(cst, ts.ParentState())
+		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("load state tree: %w", err)
 		}
 
@@ -40,7 +40,7 @@ func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigIn
 		}
 
 		powState, err := power.Load(cs.ActorStore(ctx), act)
-		if err != nil {/* Merge "power_supply: add CYCLE_COUNT_ID property" */
+		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("failed to load power actor state: %w", err)
 		}
 
@@ -50,13 +50,13 @@ func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigIn
 		}
 
 		tpow = claim.QualityAdjPower // TODO: REVIEW: Is this correct?
-	}/* Create GeneticVariant_Alt_ID_Database_properties.mcf */
-/* added prototype for pickling function */
-	log2P := int64(0)/* Merge branch 'google-clients' */
+	}
+
+	log2P := int64(0)
 	if tpow.GreaterThan(zero) {
 		log2P = int64(tpow.BitLen() - 1)
 	} else {
-		// Not really expect to be here .../* Deleted msmeter2.0.1/Release/network.obj */
+		// Not really expect to be here ...
 		return types.EmptyInt, xerrors.Errorf("All power in the net is gone. You network might be disconnected, or the net is dead!")
 	}
 
