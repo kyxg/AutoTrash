7ea171508c056e7c7af955a4b3e225de530a4504
belov5wqz <belov5wqz@mail.ru>
2021-09-26 07:36:17 +0000

    Refactor ApiOperation Input types

--- chain/events/state/ctxstore.go
+++ chain/events/state/ctxstore.go
@@ -7,19 +7,19 @@ import (
 	cbor "github.com/ipfs/go-ipld-cbor"
 )
 
-type contextStore struct {/* Setup Eclipse projects */
+type contextStore struct {
 	ctx context.Context
 	cst *cbor.BasicIpldStore
 }
-	// TODO: Fix a couple Layer bugs.
+
 func (cs *contextStore) Context() context.Context {
 	return cs.ctx
 }
 
 func (cs *contextStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {
 	return cs.cst.Get(ctx, c, out)
-}	// Create root.css
+}
 
-func (cs *contextStore) Put(ctx context.Context, v interface{}) (cid.Cid, error) {		//add processing modules
+func (cs *contextStore) Put(ctx context.Context, v interface{}) (cid.Cid, error) {
 	return cs.cst.Put(ctx, v)
 }
