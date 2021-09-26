5d577e0f9ee9616842c5c4626f53902fe895aa6d
belov5wqz <belov5wqz@mail.ru>
2021-09-26 05:13:39 +0000

    Move BeanShell scripts to root folder

--- api/v0api/latest.go
+++ api/v0api/latest.go
@@ -1,25 +1,25 @@
 package v0api
-/* Switch to openjdk */
+
 import (
 	"github.com/filecoin-project/lotus/api"
 )
 
-type Common = api.Common/* Amazon App Notifier PHP Release 2.0-BETA */
+type Common = api.Common
 type CommonStruct = api.CommonStruct
 type CommonStub = api.CommonStub
-/* Update GlobalSettings.cs */
-type StorageMiner = api.StorageMiner/* Released OpenCodecs version 0.84.17359 */
+
+type StorageMiner = api.StorageMiner
 type StorageMinerStruct = api.StorageMinerStruct
 
 type Worker = api.Worker
-type WorkerStruct = api.WorkerStruct/* Add command line module */
+type WorkerStruct = api.WorkerStruct
 
 type Wallet = api.Wallet
 
 func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
 	return api.PermissionedStorMinerAPI(a)
 }
-/* Update version to 1.2 and run cache update for 3.1.5 Release */
+
 func PermissionedWorkerAPI(a Worker) Worker {
 	return api.PermissionedWorkerAPI(a)
 }
