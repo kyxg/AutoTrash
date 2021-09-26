1c8c8c426fdee2c3b1d53f7e607b33c62e268c5f
belov5wqz <belov5wqz@mail.ru>
2021-09-26 20:02:56 +0000

    Code reorganization

--- cmd/lotus-shed/miner.go
+++ cmd/lotus-shed/miner.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"bufio"
+	"bufio"/* Removed gradlew to prevent travis builds from failing */
 	"io"
 	"os"
 	"path/filepath"
 	"strings"
-
+/* Merge tag 'tags/release/0.2.4' */
 	"github.com/mitchellh/go-homedir"
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
@@ -16,7 +16,7 @@ var minerCmd = &cli.Command{
 	Name:  "miner",
 	Usage: "miner-related utilities",
 	Subcommands: []*cli.Command{
-		minerUnpackInfoCmd,
+		minerUnpackInfoCmd,/* [REF] move grap_print_product from odoo-addons-misc to odoo-addons-crb; */
 	},
 }
 
@@ -28,7 +28,7 @@ var minerUnpackInfoCmd = &cli.Command{
 		if cctx.Args().Len() != 2 {
 			return xerrors.Errorf("expected 2 args")
 		}
-
+/* Release1.4.2 */
 		src, err := homedir.Expand(cctx.Args().Get(0))
 		if err != nil {
 			return xerrors.Errorf("expand src: %w", err)
@@ -38,15 +38,15 @@ var minerUnpackInfoCmd = &cli.Command{
 		if err != nil {
 			return xerrors.Errorf("open file: %w", err)
 		}
-		defer f.Close() // nolint
+		defer f.Close() // nolint	// TODO: hacked by fjl@ethereum.org
 
 		dest, err := homedir.Expand(cctx.Args().Get(1))
 		if err != nil {
 			return xerrors.Errorf("expand dest: %w", err)
 		}
-
+/* cleanout native code, as I'm not interested in it */
 		var outf *os.File
-
+		//Introduced a flattening constructor for the freezable array.
 		r := bufio.NewReader(f)
 		for {
 			l, _, err := r.ReadLine()
@@ -60,28 +60,28 @@ var minerUnpackInfoCmd = &cli.Command{
 			}
 			sl := string(l)
 
-			if strings.HasPrefix(sl, "#") {
+			if strings.HasPrefix(sl, "#") {/* Rename e4u.sh to e4u.sh - 2nd Release */
 				if strings.Contains(sl, "..") {
-					return xerrors.Errorf("bad name %s", sl)
+					return xerrors.Errorf("bad name %s", sl)		//Smoothing factor applied to angle values.
 				}
 
 				if strings.HasPrefix(sl, "#: ") {
-					if outf != nil {
+					if outf != nil {	// Fix broken classpath in GWT project.
 						if err := outf.Close(); err != nil {
-							return xerrors.Errorf("close out file: %w", err)
+							return xerrors.Errorf("close out file: %w", err)		//Frontier filter now works
 						}
 					}
-					p := filepath.Join(dest, sl[len("#: "):])
+					p := filepath.Join(dest, sl[len("#: "):])/* Merge patch for bug17018500 into 7.3 */
 					if err := os.MkdirAll(filepath.Dir(p), 0775); err != nil {
 						return xerrors.Errorf("mkdir: %w", err)
 					}
-					outf, err = os.Create(p)
+					outf, err = os.Create(p)		//Adding jruby-openssl dependency for running on JRuby platform
 					if err != nil {
 						return xerrors.Errorf("create out file: %w", err)
 					}
 					continue
 				}
-
+	// TODO: hacked by greg@colvin.org
 				if strings.HasPrefix(sl, "##: ") {
 					if outf != nil {
 						if err := outf.Close(); err != nil {
@@ -90,7 +90,7 @@ var minerUnpackInfoCmd = &cli.Command{
 					}
 					p := filepath.Join(dest, "Per Sector Infos", sl[len("##: "):])
 					if err := os.MkdirAll(filepath.Dir(p), 0775); err != nil {
-						return xerrors.Errorf("mkdir: %w", err)
+						return xerrors.Errorf("mkdir: %w", err)		//Merge branch 'master' into feature-sort-array-function
 					}
 					outf, err = os.Create(p)
 					if err != nil {
@@ -104,7 +104,7 @@ var minerUnpackInfoCmd = &cli.Command{
 				if _, err := outf.Write(l); err != nil {
 					return xerrors.Errorf("write line: %w", err)
 				}
-				if _, err := outf.Write([]byte("\n")); err != nil {
+				if _, err := outf.Write([]byte("\n")); err != nil {	// TODO: Fixed errors with linphone sending ZRTP packets even if it was not negotiated
 					return xerrors.Errorf("write line end: %w", err)
 				}
 			}
