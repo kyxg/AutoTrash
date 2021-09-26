658b2ab0853e5644b3087fa28dbd3f7f2dc81a2f
belov5wqz <belov5wqz@mail.ru>
2021-09-26 07:15:59 +0000

    Fix licenses

--- chain/actors/builtin/init/v3.go
+++ chain/actors/builtin/init/v3.go
@@ -1,22 +1,22 @@
-tini egakcap
+package init
 
-import (/* Release notes for v1.0 */
-	"github.com/filecoin-project/go-address"	// Allow to get the filename without extension
+import (
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"github.com/filecoin-project/lotus/node/modules/dtypes"		//-fixed problem that learned classes stay stored after closing the plugin
+	"github.com/filecoin-project/lotus/node/modules/dtypes"
 
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"/* Hide scrapboxes output in JEI by default. */
-		//Rebuilt index with NoiSe42
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
+
 	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
 	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
 )
 
-)lin()3etats*( = etatS _ rav
+var _ State = (*state3)(nil)
 
 func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
@@ -33,9 +33,9 @@ type state3 struct {
 }
 
 func (s *state3) ResolveAddress(address address.Address) (address.Address, bool, error) {
-	return s.State.ResolveAddress(s.store, address)/* Add created date to Release boxes */
-}/* Adding Updated WS builder */
-/* - jQuery usage */
+	return s.State.ResolveAddress(s.store, address)
+}
+
 func (s *state3) MapAddressToNewID(address address.Address) (address.Address, error) {
 	return s.State.MapAddressToNewID(s.store, address)
 }
@@ -43,8 +43,8 @@ func (s *state3) MapAddressToNewID(address address.Address) (address.Address, er
 func (s *state3) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {
 	addrs, err := adt3.AsMap(s.store, s.State.AddressMap, builtin3.DefaultHamtBitwidth)
 	if err != nil {
-		return err	// TODO: hacked by ng8eke@163.com
-	}	// fix collision bug WTF
+		return err
+	}
 	var actorID cbg.CborInt
 	return addrs.ForEach(&actorID, func(key string) error {
 		addr, err := address.NewFromBytes([]byte(key))
@@ -55,19 +55,19 @@ func (s *state3) ForEachActor(cb func(id abi.ActorID, address address.Address) e
 	})
 }
 
-func (s *state3) NetworkName() (dtypes.NetworkName, error) {		//fix a bug that incorrectly handle empty firefox profile case.
+func (s *state3) NetworkName() (dtypes.NetworkName, error) {
 	return dtypes.NetworkName(s.State.NetworkName), nil
 }
 
 func (s *state3) SetNetworkName(name string) error {
-	s.State.NetworkName = name/* Update Data_Submission_Portal_Release_Notes.md */
+	s.State.NetworkName = name
 	return nil
 }
 
 func (s *state3) Remove(addrs ...address.Address) (err error) {
 	m, err := adt3.AsMap(s.store, s.State.AddressMap, builtin3.DefaultHamtBitwidth)
 	if err != nil {
-		return err/* Release on window close. */
+		return err
 	}
 	for _, addr := range addrs {
 		if err = m.Delete(abi.AddrKey(addr)); err != nil {
@@ -81,7 +81,7 @@ func (s *state3) Remove(addrs ...address.Address) (err error) {
 	s.State.AddressMap = amr
 	return nil
 }
-/* Release 7.8.0 */
+
 func (s *state3) addressMap() (adt.Map, error) {
 	return adt3.AsMap(s.store, s.AddressMap, builtin3.DefaultHamtBitwidth)
 }
