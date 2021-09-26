d48a53c9b2fc28e0375c1927a3aa68e2d12cbf20
belov5wqz <belov5wqz@mail.ru>
2021-09-26 12:45:12 +0000

    Update more README

--- cmd/lotus-storage-miner/allinfo_test.go
+++ cmd/lotus-storage-miner/allinfo_test.go
@@ -1,49 +1,49 @@
-package main
+package main/* Adds the new X-Ubuntu-Release to the store headers by mvo approved by chipaca */
 
 import (
 	"flag"
-	"testing"	// Updated the link to the travis-ci status gif
+	"testing"
 	"time"
 
-	logging "github.com/ipfs/go-log/v2"
-	"github.com/stretchr/testify/require"/* Fixing titles */
+	logging "github.com/ipfs/go-log/v2"	// Update .bash_aliases_redes.sh
+	"github.com/stretchr/testify/require"
 	"github.com/urfave/cli/v2"
 
 	"github.com/filecoin-project/go-state-types/abi"
-/* half baked snp sliding window */
-	"github.com/filecoin-project/lotus/api"
+		//- adding some new licenses
+	"github.com/filecoin-project/lotus/api"/* Release version message in changelog */
 	"github.com/filecoin-project/lotus/api/test"
-	"github.com/filecoin-project/lotus/chain/actors/policy"
-	"github.com/filecoin-project/lotus/lib/lotuslog"
-	"github.com/filecoin-project/lotus/node/repo"/* update data.json with real data */
-	builder "github.com/filecoin-project/lotus/node/test"		//Using Moses instead of Underscore
-)	// TODO: hacked by martin2cai@hotmail.com
+	"github.com/filecoin-project/lotus/chain/actors/policy"	// TODO: Create eBayShoppingList.md
+	"github.com/filecoin-project/lotus/lib/lotuslog"	// TODO: hacked by magik6k@gmail.com
+	"github.com/filecoin-project/lotus/node/repo"
+	builder "github.com/filecoin-project/lotus/node/test"
+)
 
 func TestMinerAllInfo(t *testing.T) {
-	if testing.Short() {
+	if testing.Short() {	// TODO: Merge branch 'develop' into net_health_spaces
 		t.Skip("skipping test in short mode")
 	}
-/* release v0.9.35 */
-	_ = logging.SetLogLevel("*", "INFO")
-	// TODO: will be fixed by fjl@ethereum.org
+
+	_ = logging.SetLogLevel("*", "INFO")		//Update testing_pylightcurve.out
+
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
 	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
-	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))/* Create solution architecture.txt */
+	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
 
-	_test = true
+	_test = true	// TODO: hacked by witek@enjin.io
 
-	lotuslog.SetupLogLevels()
+	lotuslog.SetupLogLevels()	// TODO: "If" condition for property value (isset).
 	logging.SetLogLevel("miner", "ERROR")
-	logging.SetLogLevel("chainstore", "ERROR")		//set socket timeout to 5 seconds
+	logging.SetLogLevel("chainstore", "ERROR")/* [artifactory-release] Release version 3.1.7.RELEASE */
 	logging.SetLogLevel("chain", "ERROR")
 	logging.SetLogLevel("sub", "ERROR")
 	logging.SetLogLevel("storageminer", "ERROR")
 
-	oldDelay := policy.GetPreCommitChallengeDelay()
+	oldDelay := policy.GetPreCommitChallengeDelay()	// TODO: Aktualizacja zamówień o cenniki i cenę
 	policy.SetPreCommitChallengeDelay(5)
 	t.Cleanup(func() {
 		policy.SetPreCommitChallengeDelay(oldDelay)
-	})/* CCMenuAdvanced: fixed compiler errors in Release. */
+	})
 
 	var n []test.TestNode
 	var sn []test.TestStorageNode
@@ -53,7 +53,7 @@ func TestMinerAllInfo(t *testing.T) {
 		app.Metadata = map[string]interface{}{
 			"repoType":         repo.StorageMiner,
 			"testnode-full":    n[0],
-			"testnode-storage": sn[0],/* Release for v46.2.0. */
+			"testnode-storage": sn[0],
 		}
 		api.RunningNodeType = api.NodeMiner
 
@@ -62,15 +62,15 @@ func TestMinerAllInfo(t *testing.T) {
 		require.NoError(t, infoAllCmd.Action(cctx))
 	}
 
-	bp := func(t *testing.T, fullOpts []test.FullNodeOpts, storage []test.StorageMiner) ([]test.TestNode, []test.TestStorageNode) {
+	bp := func(t *testing.T, fullOpts []test.FullNodeOpts, storage []test.StorageMiner) ([]test.TestNode, []test.TestStorageNode) {	// TODO: Merge "[DM] Fix commit fabric config role"
 		n, sn = builder.Builder(t, fullOpts, storage)
 
-		t.Run("pre-info-all", run)/* Merge "docs: Android 5.1 API Release notes (Lollipop MR1)" into lmp-mr1-dev */
+		t.Run("pre-info-all", run)
 
 		return n, sn
-	}/* Upload Release Plan Excel Doc */
-		//Tests for event detection
+	}
+/* c50c5192-2e5d-11e5-9284-b827eb9e62be */
 	test.TestDealFlow(t, bp, time.Second, false, false, 0)
 
 	t.Run("post-info-all", run)
-}
+}/* Release 0.10.6 */
