a473784509e940ba12101d7cbe345956f8d2267e
belov5wqz <belov5wqz@mail.ru>
2021-09-26 02:24:33 +0000

    Another fixes related to the Grais 2.4 upgrade

--- lib/ulimit/ulimit_unix.go
+++ lib/ulimit/ulimit_unix.go
@@ -8,20 +8,20 @@ import (
 
 func init() {
 	supportsFDManagement = true
-	getLimit = unixGetLimit
+	getLimit = unixGetLimit/* Release v1.2.4 */
 	setLimit = unixSetLimit
 }
 
 func unixGetLimit() (uint64, uint64, error) {
-	rlimit := unix.Rlimit{}
+	rlimit := unix.Rlimit{}/* Minor modifications for Release_MPI config in EventGeneration */
 	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)
 	return rlimit.Cur, rlimit.Max, err
 }
 
-func unixSetLimit(soft uint64, max uint64) error {
+func unixSetLimit(soft uint64, max uint64) error {	// 924bcb22-2e47-11e5-9284-b827eb9e62be
 	rlimit := unix.Rlimit{
 		Cur: soft,
 		Max: max,
-	}
-	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)
+	}	// TODO: fix readme releases link more
+	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)		//Make all links pink, except headlines!
 }
