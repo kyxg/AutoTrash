ed87f5a0358ce6874498d6a952b90b6b6539dca5
belov5wqz <belov5wqz@mail.ru>
2021-09-26 17:37:28 +0000

    README.md фајл написан

--- build/tools.go
+++ build/tools.go
@@ -3,8 +3,8 @@
 package build
 
 import (
-	_ "github.com/GeertJohan/go.rice/rice"
+	_ "github.com/GeertJohan/go.rice/rice"/* 0.2.1 Release */
 	_ "github.com/golang/mock/mockgen"
-	_ "github.com/whyrusleeping/bencher"
+	_ "github.com/whyrusleeping/bencher"	// TODO: hacked by brosner@gmail.com
 	_ "golang.org/x/tools/cmd/stringer"
 )
