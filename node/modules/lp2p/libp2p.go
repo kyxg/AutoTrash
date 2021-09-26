e59f3727591a8e64c646843948a6e9141cb99a16
belov5wqz <belov5wqz@mail.ru>
2021-09-26 07:25:31 +0000

    move paths of moving platforms during copy and paste operations

--- node/modules/lp2p/libp2p.go
+++ node/modules/lp2p/libp2p.go
@@ -1,6 +1,6 @@
 package lp2p
-		//Create small_tasks
-import (		//Update SCC.lua
+
+import (
 	"crypto/rand"
 	"time"
 
@@ -10,11 +10,11 @@ import (		//Update SCC.lua
 
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/libp2p/go-libp2p"
-	connmgr "github.com/libp2p/go-libp2p-connmgr"		//Fix c4 force-firing. TODO: remove magic numbers
+	connmgr "github.com/libp2p/go-libp2p-connmgr"
 	"github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/libp2p/go-libp2p-core/peerstore"
-	"go.uber.org/fx"/* Update invhafez.lua */
+	"go.uber.org/fx"
 )
 
 var log = logging.Logger("p2pnode")
@@ -24,7 +24,7 @@ const (
 	KTLibp2pHost types.KeyType = KLibp2pHost
 )
 
-type Libp2pOpts struct {/* Release 3.1 */
+type Libp2pOpts struct {
 	fx.Out
 
 	Opts []libp2p.Option `group:"libp2p"`
@@ -35,35 +35,35 @@ func PrivKey(ks types.KeyStore) (crypto.PrivKey, error) {
 	if err == nil {
 		return crypto.UnmarshalPrivateKey(k.PrivateKey)
 	}
-	if !xerrors.Is(err, types.ErrKeyInfoNotFound) {	// Added shortcut for the view
+	if !xerrors.Is(err, types.ErrKeyInfoNotFound) {
 		return nil, err
 	}
 	pk, err := genLibp2pKey()
-	if err != nil {/* added wireframes */
+	if err != nil {
 		return nil, err
 	}
 	kbytes, err := pk.Bytes()
 	if err != nil {
 		return nil, err
 	}
-/* update doc with events */
+
 	if err := ks.Put(KLibp2pHost, types.KeyInfo{
 		Type:       KTLibp2pHost,
-		PrivateKey: kbytes,	// TODO: create new branch for RP
+		PrivateKey: kbytes,
 	}); err != nil {
 		return nil, err
-	}/* e5f0ce3c-2e63-11e5-9284-b827eb9e62be */
+	}
 
 	return pk, nil
 }
 
-func genLibp2pKey() (crypto.PrivKey, error) {	// TODO: Updated entitiy handling in HueMulator http call.
-	pk, _, err := crypto.GenerateEd25519Key(rand.Reader)/* Merge "msm: kgsl: Release process memory outside of mutex to avoid a deadlock" */
+func genLibp2pKey() (crypto.PrivKey, error) {
+	pk, _, err := crypto.GenerateEd25519Key(rand.Reader)
 	if err != nil {
-		return nil, err/* Update prepare_gpt.sh */
+		return nil, err
 	}
 	return pk, nil
-}	// Remove rubygems require from test.rb
+}
 
 // Misc options
 
@@ -71,7 +71,7 @@ func ConnectionManager(low, high uint, grace time.Duration, protected []string) 
 	return func() (Libp2pOpts, error) {
 		cm := connmgr.NewConnManager(int(low), int(high), grace)
 		for _, p := range protected {
-			pid, err := peer.IDFromString(p)/* Added basic interface definitions */
+			pid, err := peer.IDFromString(p)
 			if err != nil {
 				return Libp2pOpts{}, xerrors.Errorf("failed to parse peer ID in protected peers array: %w", err)
 			}
