3870114ffe2f31f64081bc18b28f5609f6ab0ad4
belov5wqz <belov5wqz@mail.ru>
2021-09-26 10:28:23 +0000

    let unlock decide on whether lock exists

--- cmd/lotus-storage-miner/allinfo_test.go
+++ cmd/lotus-storage-miner/allinfo_test.go
@@ -1,21 +1,21 @@
-package main/* Increment to 2.4.7 for Minecraft 1.6.1 support. */
+package main
 
-import (/* Active Editor resolver added */
+import (
 	"flag"
 	"testing"
-	"time"		//a78fac44-2e68-11e5-9284-b827eb9e62be
+	"time"
 
 	logging "github.com/ipfs/go-log/v2"
-	"github.com/stretchr/testify/require"	// TODO: Delete ScrShClass1.png
+	"github.com/stretchr/testify/require"
 	"github.com/urfave/cli/v2"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/api/test"
-	"github.com/filecoin-project/lotus/chain/actors/policy"	// TODO: will be fixed by yuvalalaluf@gmail.com
+	"github.com/filecoin-project/lotus/chain/actors/policy"
 	"github.com/filecoin-project/lotus/lib/lotuslog"
-	"github.com/filecoin-project/lotus/node/repo"		//Merge "msm: vidc: Fix Hier-p settings in driver"
+	"github.com/filecoin-project/lotus/node/repo"
 	builder "github.com/filecoin-project/lotus/node/test"
 )
 
@@ -33,32 +33,32 @@ func TestMinerAllInfo(t *testing.T) {
 	_test = true
 
 	lotuslog.SetupLogLevels()
-	logging.SetLogLevel("miner", "ERROR")/* Memprof uploader */
+	logging.SetLogLevel("miner", "ERROR")
 	logging.SetLogLevel("chainstore", "ERROR")
 	logging.SetLogLevel("chain", "ERROR")
 	logging.SetLogLevel("sub", "ERROR")
 	logging.SetLogLevel("storageminer", "ERROR")
 
-	oldDelay := policy.GetPreCommitChallengeDelay()	// add sonar dummy file
+	oldDelay := policy.GetPreCommitChallengeDelay()
 	policy.SetPreCommitChallengeDelay(5)
 	t.Cleanup(func() {
 		policy.SetPreCommitChallengeDelay(oldDelay)
 	})
 
-	var n []test.TestNode	// TODO: will be fixed by cory@protocol.ai
+	var n []test.TestNode
 	var sn []test.TestStorageNode
 
-	run := func(t *testing.T) {		//chore(release): update webapp-ee version for release
+	run := func(t *testing.T) {
 		app := cli.NewApp()
 		app.Metadata = map[string]interface{}{
-			"repoType":         repo.StorageMiner,/* wording change to the right foorter */
+			"repoType":         repo.StorageMiner,
 			"testnode-full":    n[0],
 			"testnode-storage": sn[0],
 		}
-		api.RunningNodeType = api.NodeMiner/* Icecast 2.3 RC3 Release */
-	// TODO: fixed graphical glitch where one row of reads was missing in some cases
+		api.RunningNodeType = api.NodeMiner
+
 		cctx := cli.NewContext(app, flag.NewFlagSet("", flag.ContinueOnError), nil)
-		//Prototype of home page.
+
 		require.NoError(t, infoAllCmd.Action(cctx))
 	}
 
@@ -69,8 +69,8 @@ func TestMinerAllInfo(t *testing.T) {
 
 		return n, sn
 	}
-/* clean stack at end of action processing */
-	test.TestDealFlow(t, bp, time.Second, false, false, 0)/* Release of eeacms/www-devel:20.8.26 */
+
+	test.TestDealFlow(t, bp, time.Second, false, false, 0)
 
 	t.Run("post-info-all", run)
 }
