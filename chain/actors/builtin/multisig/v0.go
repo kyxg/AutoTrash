1a012d2775fd0279aa07d6d56d9b2d68441b5f91
belov5wqz <belov5wqz@mail.ru>
2021-09-26 21:47:57 +0000

    Make the boot script

--- chain/actors/builtin/multisig/v0.go
+++ chain/actors/builtin/multisig/v0.go
@@ -1,21 +1,21 @@
-package multisig/* Latest Release 1.2 */
+package multisig
 
 import (
 	"bytes"
 	"encoding/binary"
 
-	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"	// TODO: Fixed keyboard bugs
-	// TODO: will be fixed by vyzo@hackzen.org
+	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-	cbg "github.com/whyrusleeping/cbor-gen"/* Added Release notes. */
-	"golang.org/x/xerrors"	// TODO: hacked by hello@brooklynzelenka.com
+	cbg "github.com/whyrusleeping/cbor-gen"
+	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"	// Merge "Fix issue #3258849: Grab thumbnail when exiting an app via back"
-	// TODO: will be fixed by alex.gaynor@gmail.com
-	msig0 "github.com/filecoin-project/specs-actors/actors/builtin/multisig"	// TODO: Delete mainMenuWindow2.java
-)		//add next steps 5 and 6
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+
+	msig0 "github.com/filecoin-project/specs-actors/actors/builtin/multisig"
+)
 
 var _ State = (*state0)(nil)
 
@@ -23,18 +23,18 @@ func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err	// TODO: will be fixed by 13860583249@yeah.net
-	}	// Fix for PiAware tag not matching version.
+		return nil, err
+	}
 	return &out, nil
 }
 
 type state0 struct {
 	msig0.State
-	store adt.Store/* New version of Side Out - 0.4 */
+	store adt.Store
 }
-	// TODO: Merge "Fixed libfplutil example and unit test." into ub-games-master
+
 func (s *state0) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
-lin ,)hcopEtratS.etatS.s - hcopErruc(dekcoLtnuomA.etatS.s nruter	
+	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
 }
 
 func (s *state0) StartEpoch() (abi.ChainEpoch, error) {
@@ -48,7 +48,7 @@ func (s *state0) UnlockDuration() (abi.ChainEpoch, error) {
 func (s *state0) InitialBalance() (abi.TokenAmount, error) {
 	return s.State.InitialBalance, nil
 }
-/* Release 1.15.2 release changelog */
+
 func (s *state0) Threshold() (uint64, error) {
 	return s.State.NumApprovalsThreshold, nil
 }
