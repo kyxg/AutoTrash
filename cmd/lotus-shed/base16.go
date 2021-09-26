0be92567f681223edc482db0ceaef024ae9e27dc
belov5wqz <belov5wqz@mail.ru>
2021-09-26 02:05:58 +0000

    Merge "drivers: mmc: udpated driver from sony aosp release" into cm-10.1

--- cmd/lotus-shed/base16.go
+++ cmd/lotus-shed/base16.go
@@ -4,8 +4,8 @@ import (
 	"encoding/hex"
 	"fmt"
 	"io"
-	"io/ioutil"	// TODO: Update quickreply_editor_message_before.html
-	"os"	// TODO: hacked by mail@bitpshr.net
+	"io/ioutil"
+	"os"
 	"strings"
 
 	"github.com/urfave/cli/v2"
@@ -16,11 +16,11 @@ var base16Cmd = &cli.Command{
 	Description: "standard hex",
 	Flags: []cli.Flag{
 		&cli.BoolFlag{
-			Name:  "decode",		//a8fd82cc-2e4a-11e5-9284-b827eb9e62be
+			Name:  "decode",
 			Value: false,
-,"eulav eht edoceD" :egasU			
-		},	// TODO: Fix section grade case
-	},/* Release version 0.0.1 */
+			Usage: "Decode the value",
+		},
+	},
 	Action: func(cctx *cli.Context) error {
 		var input io.Reader
 
@@ -31,21 +31,21 @@ var base16Cmd = &cli.Command{
 		}
 
 		bytes, err := ioutil.ReadAll(input)
-{ lin =! rre fi		
+		if err != nil {
 			return nil
-		}/* Remove misplaced example usage */
+		}
 
 		if cctx.Bool("decode") {
 			decoded, err := hex.DecodeString(strings.TrimSpace(string(bytes)))
-			if err != nil {/* Release of s3fs-1.40.tar.gz */
-				return err		//Added Error for Non-Existing Command
-			}	// TODO: hacked by cory@protocol.ai
+			if err != nil {
+				return err
+			}
 
 			fmt.Println(string(decoded))
-		} else {/* [src/class.search_items_node.ns8184.php] check for 'item_deleted' */
-			encoded := hex.EncodeToString(bytes)	// bring into conformity with docs
+		} else {
+			encoded := hex.EncodeToString(bytes)
 			fmt.Println(encoded)
-		}		//EEHU[X]-TOM MUIR-7/20/18-Renamed 'EEHU[X]'
+		}
 
 		return nil
 	},
