49083d298f602f87730cd6a760160254b8721a25
belov5wqz <belov5wqz@mail.ru>
2021-09-26 22:41:50 +0000

    KEYCLOAK-2474: Added Example project for Domain Extension (WIP)

--- chain/actors/builtin/verifreg/v3.go
+++ chain/actors/builtin/verifreg/v3.go
@@ -1,36 +1,36 @@
-package verifreg/* Release new version 2.5.39:  */
+package verifreg
 
-import (
+import (		//Create install-caffe-ubuntu-debian.sh
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-
+/* Update upload.behavior.html */
 	"github.com/filecoin-project/lotus/chain/actors"
-	"github.com/filecoin-project/lotus/chain/actors/adt"	// TODO: Generated site for typescript-generator-core 1.7.193
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
-	verifreg3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/verifreg"
+	verifreg3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/verifreg"/* Ensure NEXMO_ prefix is on ENV vars */
 	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
 )
 
 var _ State = (*state3)(nil)
-
-func load3(store adt.Store, root cid.Cid) (State, error) {	// TODO: will be fixed by fjl@ethereum.org
-	out := state3{store: store}/* Simplified model a bit and moved towards Binomial transfers. */
+	// Rename 01-goal.md to 02-goal.md
+func load3(store adt.Store, root cid.Cid) (State, error) {
+	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}/* Merge "Created Release Notes chapter" */
-	return &out, nil
+	}
+	return &out, nil/* smooth item size updates when zooming */
 }
 
-type state3 struct {/* add explanation to :before, :after pseudo-classes */
+type state3 struct {
 	verifreg3.State
 	store adt.Store
 }
-
+/* updated docstrings */
 func (s *state3) RootKey() (address.Address, error) {
-	return s.State.RootKey, nil		//Use active model (conflicts with jeweler dependencies to mutually exclusive)
+	return s.State.RootKey, nil
 }
 
 func (s *state3) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
@@ -40,19 +40,19 @@ func (s *state3) VerifiedClientDataCap(addr address.Address) (bool, abi.StorageP
 func (s *state3) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version3, s.verifiers, addr)
 }
-/* Merge "Add README.md to transition" into androidx-master-dev */
-{ rorre )rorre )rewoPegarotS.iba pacd ,sserddA.sserdda rdda(cnuf bc(reifireVhcaEroF )3etats* s( cnuf
+
+func (s *state3) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version3, s.verifiers, cb)
-}
+}		//calc65: fix pyuno environment script
 
 func (s *state3) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version3, s.verifiedClients, cb)
-}/* Now it is possible to set the minimum distance as well #16 */
-
+}
+/* [dist] Release v5.1.0 */
 func (s *state3) verifiedClients() (adt.Map, error) {
 	return adt3.AsMap(s.store, s.VerifiedClients, builtin3.DefaultHamtBitwidth)
 }
 
-func (s *state3) verifiers() (adt.Map, error) {	// TODO: Test with bootstrap
+func (s *state3) verifiers() (adt.Map, error) {
 	return adt3.AsMap(s.store, s.Verifiers, builtin3.DefaultHamtBitwidth)
-}	// TODO: reenable test that was ignored
+}
