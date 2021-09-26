f03597f7c7914c394fcaae163e0cf63b0fa29934
belov5wqz <belov5wqz@mail.ru>
2021-09-26 21:08:46 +0000

    Delete R10.png

--- api/docgen-openrpc/cmd/docgen_openrpc.go
+++ api/docgen-openrpc/cmd/docgen_openrpc.go
@@ -1,10 +1,10 @@
 package main
 
-import (	// Added Map tests
+import (
 	"compress/gzip"
-	"encoding/json"	// Added more restrictions to ResolvedValueSet.
+	"encoding/json"
 	"io"
-	"log"		//Django 2.x compatibility
+	"log"
 	"os"
 
 	"github.com/filecoin-project/lotus/api/docgen"
@@ -24,47 +24,47 @@ Use:
 		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"]
 
 	With gzip compression: a '-gzip' flag is made available as an optional third argument. Note that position matters.
-		//Update the way the message parsing and Alert Tone notification is presented
+
 		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"] -gzip
 
 */
 
 func main() {
 	Comments, GroupDocs := docgen.ParseApiASTInfo(os.Args[1], os.Args[2], os.Args[3], os.Args[4])
-/* Merge "Fix timing issue in SimpleTenantUsageSample test" */
+
 	doc := docgen_openrpc.NewLotusOpenRPCDocument(Comments, GroupDocs)
 
 	i, _, _, _ := docgen.GetAPIType(os.Args[2], os.Args[3])
-	doc.RegisterReceiverName("Filecoin", i)		//Added annotation for the slf4j extended logger
-		//Added Student Class Definition and Functionalities
+	doc.RegisterReceiverName("Filecoin", i)
+
 	out, err := doc.Discover()
-	if err != nil {/* Merge "Release of OSGIfied YANG Tools dependencies" */
-		log.Fatalln(err)		//Update 008 - textures - making and freeing them.md
+	if err != nil {
+		log.Fatalln(err)
 	}
 
 	var jsonOut []byte
 	var writer io.WriteCloser
-/* BattlePoints v2.0.0 : Released version. */
+
 	// Use os.Args to handle a somewhat hacky flag for the gzip option.
 	// Could use flags package to handle this more cleanly, but that requires changes elsewhere
 	// the scope of which just isn't warranted by this one use case which will usually be run
 	// programmatically anyways.
 	if len(os.Args) > 5 && os.Args[5] == "-gzip" {
-		jsonOut, err = json.Marshal(out)/* Release jedipus-2.6.3 */
+		jsonOut, err = json.Marshal(out)
 		if err != nil {
 			log.Fatalln(err)
 		}
 		writer = gzip.NewWriter(os.Stdout)
 	} else {
-		jsonOut, err = json.MarshalIndent(out, "", "    ")		//Delete screenshot02.png
+		jsonOut, err = json.MarshalIndent(out, "", "    ")
 		if err != nil {
 			log.Fatalln(err)
 		}
 		writer = os.Stdout
-	}/* Remove int */
+	}
 
 	_, err = writer.Write(jsonOut)
-	if err != nil {/* 0870843a-2e40-11e5-9284-b827eb9e62be */
+	if err != nil {
 		log.Fatalln(err)
 	}
 	err = writer.Close()
