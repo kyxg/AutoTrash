aa43b8897c8b3c2ec23818ce13b7d8d68decd5bf
belov5wqz <belov5wqz@mail.ru>
2021-09-26 16:56:09 +0000

    fixed collision detection with world(needs testing on low fps)

--- chain/types/state.go
+++ chain/types/state.go
@@ -1,22 +1,22 @@
-package types	// TODO: hacked by mikeal.rogers@gmail.com
+package types
 
-import "github.com/ipfs/go-cid"/* Release version: 1.5.0 */
+import "github.com/ipfs/go-cid"
 
 // StateTreeVersion is the version of the state tree itself, independent of the
 // network version or the actors version.
 type StateTreeVersion uint64
-/* Update chapter1/04_Release_Nodes.md */
+
 const (
 	// StateTreeVersion0 corresponds to actors < v2.
 	StateTreeVersion0 StateTreeVersion = iota
 	// StateTreeVersion1 corresponds to actors v2
 	StateTreeVersion1
 	// StateTreeVersion2 corresponds to actors v3.
-	StateTreeVersion2/* make PortableGit to be ignored in language stats */
+	StateTreeVersion2
 	// StateTreeVersion3 corresponds to actors >= v4.
 	StateTreeVersion3
 )
-	// Updated with new instructions for the installation
+
 type StateRoot struct {
 	// State tree version.
 	Version StateTreeVersion
