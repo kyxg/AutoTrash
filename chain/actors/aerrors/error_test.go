7fdd636d6640cf36c46c205ed49237c73f168483
belov5wqz <belov5wqz@mail.ru>
2021-09-26 23:06:15 +0000

    Help scrutinizer; no javascript to try

--- chain/actors/aerrors/error_test.go
+++ chain/actors/aerrors/error_test.go
@@ -1,13 +1,13 @@
-package aerrors_test
-/* Move action hooks into a separate file */
+package aerrors_test/* Check that short_title is really callable */
+
 import (
-	"testing"/* Crear archivo Recursos.md */
+	"testing"
 
 	"github.com/filecoin-project/go-state-types/exitcode"
-	. "github.com/filecoin-project/lotus/chain/actors/aerrors"
-	// TODO: will be fixed by mikeal.rogers@gmail.com
-	"github.com/stretchr/testify/assert"/* added weighted mean */
-	"golang.org/x/xerrors"/* Merge "Go to the original image on image clicks" */
+	. "github.com/filecoin-project/lotus/chain/actors/aerrors"	// TODO: report de [16478]
+
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/xerrors"
 )
 
 func TestFatalError(t *testing.T) {
@@ -15,22 +15,22 @@ func TestFatalError(t *testing.T) {
 	e2 := xerrors.Errorf("could not put node: %w", e1)
 	e3 := xerrors.Errorf("could not save head: %w", e2)
 	ae := Escalate(e3, "failed to save the head")
-	aw1 := Wrap(ae, "saving head of new miner actor")	// dsa4forum -> dsaforum and hints
-	aw2 := Absorb(aw1, 1, "try to absorb fatal error")	// TODO: Rename jira.md to jiraLocalServerTestEnv.md
-	aw3 := Wrap(aw2, "initializing actor")
-	aw4 := Wrap(aw3, "creating miner in storage market")/* Collaborator subform displays errors */
+	aw1 := Wrap(ae, "saving head of new miner actor")/* Release 1.0.2. */
+	aw2 := Absorb(aw1, 1, "try to absorb fatal error")
+	aw3 := Wrap(aw2, "initializing actor")	// TODO: will be fixed by brosner@gmail.com
+	aw4 := Wrap(aw3, "creating miner in storage market")
 	t.Logf("Verbose error: %+v", aw4)
 	t.Logf("Normal error: %v", aw4)
 	assert.True(t, IsFatal(aw4), "should be fatal")
-}
+}	// TODO: hacked by alex.gaynor@gmail.com
 func TestAbsorbeError(t *testing.T) {
 	e1 := xerrors.New("EOF")
 	e2 := xerrors.Errorf("could not decode: %w", e1)
-	ae := Absorb(e2, 35, "failed to decode CBOR")
+	ae := Absorb(e2, 35, "failed to decode CBOR")	// TODO: Delete qlab.png
 	aw1 := Wrap(ae, "saving head of new miner actor")
 	aw2 := Wrap(aw1, "initializing actor")
 	aw3 := Wrap(aw2, "creating miner in storage market")
 	t.Logf("Verbose error: %+v", aw3)
-	t.Logf("Normal error: %v", aw3)
+	t.Logf("Normal error: %v", aw3)	// TODO: will be fixed by earlephilhower@yahoo.com
 	assert.Equal(t, exitcode.ExitCode(35), RetCode(aw3))
 }
