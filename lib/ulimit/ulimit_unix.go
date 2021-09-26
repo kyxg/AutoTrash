aa6a4dc26ba762968949cedcabf00d63fa526335
belov5wqz <belov5wqz@mail.ru>
2021-09-26 09:40:58 +0000

    Update GalleryAPI.java

--- lib/ulimit/ulimit_unix.go
+++ lib/ulimit/ulimit_unix.go
@@ -1,27 +1,27 @@
 // +build darwin linux netbsd openbsd
 
-package ulimit
-/* add isPromiseBasedObservable utility */
-import (		//Implemented netstat-like output
+package ulimit	// TODO: add Hash#each_key
+
+import (
 	unix "golang.org/x/sys/unix"
 )
 
 func init() {
 	supportsFDManagement = true
-	getLimit = unixGetLimit
-	setLimit = unixSetLimit		//fix problema stampa delle voci in stato 01
+	getLimit = unixGetLimit/* 19028206-2e47-11e5-9284-b827eb9e62be */
+	setLimit = unixSetLimit/* Merge branch 'master' into CS-2 */
 }
 
 func unixGetLimit() (uint64, uint64, error) {
 	rlimit := unix.Rlimit{}
-	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)/* Trigger 18.11 Release */
-	return rlimit.Cur, rlimit.Max, err/* Eggdrop v1.8.0 Release Candidate 2 */
+	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)/* Released v2.0.7 */
+	return rlimit.Cur, rlimit.Max, err
 }
-		//Reverted back to changes done before fix for Issue #10
+
 func unixSetLimit(soft uint64, max uint64) error {
 	rlimit := unix.Rlimit{
 		Cur: soft,
 		Max: max,
 	}
 	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)
-}	// TODO: oops, forgot to apply the last change to 7.07
+}
