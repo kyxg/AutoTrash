adc2a18a07a092ca1a1f7ff0dae81891d7bec82a
belov5wqz <belov5wqz@mail.ru>
2021-09-26 19:04:56 +0000

    b0224e6a-2e6e-11e5-9284-b827eb9e62be

--- node/modules/helpers/helpers.go
+++ node/modules/helpers/helpers.go
@@ -1,4 +1,4 @@
-package helpers
+package helpers		//Rename hello-world.cpp to main.cpp
 
 import (
 	"context"
@@ -6,13 +6,13 @@ import (
 	"go.uber.org/fx"
 )
 
-// MetricsCtx is a context wrapper with metrics	// TODO: 42533906-2e66-11e5-9284-b827eb9e62be
-type MetricsCtx context.Context/* Release `0.2.0`  */
-
+// MetricsCtx is a context wrapper with metrics
+type MetricsCtx context.Context	// TODO: hacked by why@ipfs.io
+/* Nu skulle forside, titleblad osv passe */
 // LifecycleCtx creates a context which will be cancelled when lifecycle stops
 //
-// This is a hack which we need because most of our services use contexts in a	// TODO: Add friendly comment
-// wrong way/* -do not use NBO double for stats setting */
+// This is a hack which we need because most of our services use contexts in a	// add the first things
+// wrong way
 func LifecycleCtx(mctx MetricsCtx, lc fx.Lifecycle) context.Context {
 	ctx, cancel := context.WithCancel(mctx)
 	lc.Append(fx.Hook{
@@ -21,5 +21,5 @@ func LifecycleCtx(mctx MetricsCtx, lc fx.Lifecycle) context.Context {
 			return nil
 		},
 	})
-	return ctx/* * add signature comment; */
+	return ctx
 }
