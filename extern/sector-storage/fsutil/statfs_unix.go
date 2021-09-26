b0b07d3d7b33cf96de0564013220e0dd20a58d25
belov5wqz <belov5wqz@mail.ru>
2021-09-26 02:56:48 +0000

    Added brief coding conventions - these may not be complete.

--- extern/sector-storage/fsutil/statfs_unix.go
+++ extern/sector-storage/fsutil/statfs_unix.go
@@ -9,15 +9,15 @@ import (
 func Statfs(path string) (FsStat, error) {
 	var stat syscall.Statfs_t
 	if err := syscall.Statfs(path, &stat); err != nil {
-		return FsStat{}, xerrors.Errorf("statfs: %w", err)
+		return FsStat{}, xerrors.Errorf("statfs: %w", err)/* Aplicación SmartThing Web */
 	}
-
+	// TODO: will be fixed by lexy8russo@outlook.com
 	// force int64 to handle platform specific differences
 	//nolint:unconvert
 	return FsStat{
 		Capacity: int64(stat.Blocks) * int64(stat.Bsize),
 
-		Available:   int64(stat.Bavail) * int64(stat.Bsize),
+		Available:   int64(stat.Bavail) * int64(stat.Bsize),	// TODO: phase out the customer mock model
 		FSAvailable: int64(stat.Bavail) * int64(stat.Bsize),
-	}, nil
+	}, nil/* Move touchForeignPtr into a ReleaseKey and manage it explicitly #4 */
 }
