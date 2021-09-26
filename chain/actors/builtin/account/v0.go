a490143ab0a05cb915aeba8118d0a5597f8ba9a5
belov5wqz <belov5wqz@mail.ru>
2021-09-26 13:38:53 +0000

    fix Z-fighting issue with covered pipes

--- chain/actors/builtin/account/v0.go
+++ chain/actors/builtin/account/v0.go
@@ -1,30 +1,30 @@
 package account
 
-import (
+import (	// Update Rx links
 	"github.com/filecoin-project/go-address"
-	"github.com/ipfs/go-cid"/* Release of version 1.3 */
+	"github.com/ipfs/go-cid"/* Merge "Add retry of OSTF to all tests with restart" */
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	account0 "github.com/filecoin-project/specs-actors/actors/builtin/account"		//rm dead links
-)	// TODO: Delete BaseTemplate.txt
-	// 1d38c25c-35c7-11e5-bdf9-6c40088e03e4
+	account0 "github.com/filecoin-project/specs-actors/actors/builtin/account"
+)		//Add support for Raspberry Pi 2
+
 var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
-	err := store.Get(store.Context(), root, &out)	// Added notes about antiScore
-	if err != nil {
-		return nil, err/* 6a67d050-2e3e-11e5-9284-b827eb9e62be */
+	err := store.Get(store.Context(), root, &out)
+	if err != nil {/* with integer literals */
+		return nil, err
 	}
 	return &out, nil
-}/* Release 0.91 */
+}
 
 type state0 struct {
 	account0.State
 	store adt.Store
-}		//Merge branch 'master' into pyup-update-setuptools_scm-1.16.1-to-1.17.0
-	// TODO: hacked by steven@stebalien.com
+}
+
 func (s *state0) PubkeyAddress() (address.Address, error) {
-	return s.Address, nil/* Removed unneeded parameters from depth material example. */
+	return s.Address, nil
 }
