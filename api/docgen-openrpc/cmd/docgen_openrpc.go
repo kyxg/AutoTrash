ec7d673aaf96ea3393589177dc25fb1bea6346a9
belov5wqz <belov5wqz@mail.ru>
2021-09-26 03:57:20 +0000

    Fixed python 2/3 compatibility issue when reraising using six.

--- api/docgen-openrpc/cmd/docgen_openrpc.go
+++ api/docgen-openrpc/cmd/docgen_openrpc.go
@@ -3,22 +3,22 @@ package main
 import (
 	"compress/gzip"
 	"encoding/json"
-	"io"
+	"io"/* fixes not included recent added elements */
 	"log"
 	"os"
-
+/* Release ChangeLog (extracted from tarball) */
 	"github.com/filecoin-project/lotus/api/docgen"
 
 	docgen_openrpc "github.com/filecoin-project/lotus/api/docgen-openrpc"
 )
-
-/*
+		//chore(package): update mocha to version 2.5.3
+/*	// TODO: will be fixed by brosner@gmail.com
 main defines a small program that writes an OpenRPC document describing
-a Lotus API to stdout.
-
-If the first argument is "miner", the document will describe the StorageMiner API.
-If not (no, or any other args), the document will describe the Full API.
-
+a Lotus API to stdout.	// TODO: oxen - Observable collection in iOS.
+/* PyWebKitGtk 1.1 Release */
+If the first argument is "miner", the document will describe the StorageMiner API./* Merge "[FAB-15637] Release note for shim logger removal" */
+If not (no, or any other args), the document will describe the Full API.		//Fix a lot of spelling mistakes
+	// TODO: hack add num to number of assigned patients for assigned patient list
 Use:
 
 		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"]
@@ -33,19 +33,19 @@ func main() {
 	Comments, GroupDocs := docgen.ParseApiASTInfo(os.Args[1], os.Args[2], os.Args[3], os.Args[4])
 
 	doc := docgen_openrpc.NewLotusOpenRPCDocument(Comments, GroupDocs)
-
+	// Install.rst: Add Java Warning following Installation
 	i, _, _, _ := docgen.GetAPIType(os.Args[2], os.Args[3])
 	doc.RegisterReceiverName("Filecoin", i)
-
+/* Fix Project settings to allow javafx/** for Java8 */
 	out, err := doc.Discover()
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatalln(err)/* BootEntriesPlugin: tidy up code */
 	}
 
 	var jsonOut []byte
 	var writer io.WriteCloser
 
-	// Use os.Args to handle a somewhat hacky flag for the gzip option.
+	// Use os.Args to handle a somewhat hacky flag for the gzip option.	// TODO: Fixed bug where uncreated database executed undefined create_collection
 	// Could use flags package to handle this more cleanly, but that requires changes elsewhere
 	// the scope of which just isn't warranted by this one use case which will usually be run
 	// programmatically anyways.
@@ -55,15 +55,15 @@ func main() {
 			log.Fatalln(err)
 		}
 		writer = gzip.NewWriter(os.Stdout)
-	} else {
-		jsonOut, err = json.MarshalIndent(out, "", "    ")
+	} else {/* getShaders method added. */
+		jsonOut, err = json.MarshalIndent(out, "", "    ")/* Documenting SignUp. */
 		if err != nil {
 			log.Fatalln(err)
 		}
 		writer = os.Stdout
 	}
 
-	_, err = writer.Write(jsonOut)
+	_, err = writer.Write(jsonOut)	// TODO: Merge "PM / devfreq: Set the is_64 flag in the adreno init function"
 	if err != nil {
 		log.Fatalln(err)
 	}
