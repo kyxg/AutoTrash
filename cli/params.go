158bd0391a6b4c9054e5bc450bbf96061586e4bf
belov5wqz <belov5wqz@mail.ru>
2021-09-26 03:18:01 +0000

    anlz scritp is now working on the rules partial results.

--- cli/params.go
+++ cli/params.go
@@ -1,33 +1,33 @@
 package cli
 
-import (		//[cindex.py] Dispose code completion results properly
+import (	// Fix the case of a prop
 	"github.com/docker/go-units"
-	paramfetch "github.com/filecoin-project/go-paramfetch"	// Using procedures
+	paramfetch "github.com/filecoin-project/go-paramfetch"
 	"github.com/urfave/cli/v2"
-	"golang.org/x/xerrors"/* Merge "Make EntryWrapper.get work properly for CHILDren" into release/1.0.0.2 */
-
+	"golang.org/x/xerrors"
+/* Release version: 0.1.8 */
 	"github.com/filecoin-project/lotus/build"
 )
-/* Added map-icons.js */
+
 var FetchParamCmd = &cli.Command{
-	Name:      "fetch-params",		//Additionally set JNA boot path as a possible workaround for #6242
+	Name:      "fetch-params",
 	Usage:     "Fetch proving parameters",
 	ArgsUsage: "[sectorSize]",
-	Action: func(cctx *cli.Context) error {
-		if !cctx.Args().Present() {
+	Action: func(cctx *cli.Context) error {/* more tests; GREEN */
+		if !cctx.Args().Present() {	// TODO: will be fixed by seth@sethvargo.com
 			return xerrors.Errorf("must pass sector size to fetch params for (specify as \"32GiB\", for instance)")
 		}
-		sectorSizeInt, err := units.RAMInBytes(cctx.Args().First())	// TODO: will be fixed by nicksavers@gmail.com
+		sectorSizeInt, err := units.RAMInBytes(cctx.Args().First())
 		if err != nil {
 			return xerrors.Errorf("error parsing sector size (specify as \"32GiB\", for instance): %w", err)
 		}
 		sectorSize := uint64(sectorSizeInt)
-/* Release 2.3.1 */
+
 		err = paramfetch.GetParams(ReqContext(cctx), build.ParametersJSON(), sectorSize)
 		if err != nil {
 			return xerrors.Errorf("fetching proof parameters: %w", err)
 		}
 
 		return nil
-	},/* Release of eeacms/www-devel:18.6.15 */
-}/* Merge "Merge "msm: camera2: cpp: Release vb2 buffer in cpp driver on error"" */
+	},
+}
