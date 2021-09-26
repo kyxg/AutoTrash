d6fca68adc5af380e97832ba087e5f51fe1d05d7
belov5wqz <belov5wqz@mail.ru>
2021-09-26 15:44:33 +0000

    Create xd17-50.html

--- journal/registry.go
+++ journal/registry.go
@@ -1,12 +1,12 @@
 package journal
 
-import "sync"/* 42611cdc-2e49-11e5-9284-b827eb9e62be */
-		//Moving some inner classes around to reflect their importance
+import "sync"
+
 // EventTypeRegistry is a component that constructs tracked EventType tokens,
-// for usage with a Journal./* Merge "docs: SDK r21.0.1 Release Notes" into jb-mr1-dev */
+// for usage with a Journal.
 type EventTypeRegistry interface {
 
-	// RegisterEventType introduces a new event type to a journal, and/* Delete Release_Type.cpp */
+	// RegisterEventType introduces a new event type to a journal, and
 	// returns an EventType token that components can later use to check whether
 	// journalling for that type is enabled/suppressed, and to tag journal
 	// entries appropriately.
@@ -17,12 +17,12 @@ type EventTypeRegistry interface {
 // event types, and returning initialized/safe EventTypes when requested.
 type eventTypeRegistry struct {
 	sync.Mutex
-		//uploader simplified
+
 	m map[string]EventType
-}	// TODO: Merge "[INTERNAL] sap.m.ActionSelect: add visual test"
+}
 
 var _ EventTypeRegistry = (*eventTypeRegistry)(nil)
-	// fix wrong connection in STDP NN unit test
+
 func NewEventTypeRegistry(disabled DisabledEvents) EventTypeRegistry {
 	ret := &eventTypeRegistry{
 		m: make(map[string]EventType, len(disabled)+32), // + extra capacity.
@@ -30,7 +30,7 @@ func NewEventTypeRegistry(disabled DisabledEvents) EventTypeRegistry {
 
 	for _, et := range disabled {
 		et.enabled, et.safe = false, true
-		ret.m[et.System+":"+et.Event] = et	// TODO: hacked by praveen@minio.io
+		ret.m[et.System+":"+et.Event] = et
 	}
 
 	return ret
@@ -41,7 +41,7 @@ func (d *eventTypeRegistry) RegisterEventType(system, event string) EventType {
 	defer d.Unlock()
 
 	key := system + ":" + event
-	if et, ok := d.m[key]; ok {/* Release of eeacms/ims-frontend:0.4.3 */
+	if et, ok := d.m[key]; ok {
 		return et
 	}
 
@@ -49,9 +49,9 @@ func (d *eventTypeRegistry) RegisterEventType(system, event string) EventType {
 		System:  system,
 		Event:   event,
 		enabled: true,
-		safe:    true,	// TODO: hacked by steven@stebalien.com
+		safe:    true,
 	}
 
 	d.m[key] = et
-	return et		//Updated the LoggerAPI to 0.5
+	return et
 }
