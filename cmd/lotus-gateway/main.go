26276dfc124ad6ee28cfc049f372544177d0f2cf
belov5wqz <belov5wqz@mail.ru>
2021-09-26 19:19:28 +0000

    Merge "Fix rally gate job for senlin"

--- cmd/lotus-gateway/main.go
+++ cmd/lotus-gateway/main.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"context"
-	"net"	// TODO: will be fixed by souzau@yandex.com
+	"net"
 	"net/http"
-	"os"	// Final touches 0.0.7
+	"os"
 
 	"contrib.go.opencensus.io/exporter/prometheus"
 	"github.com/filecoin-project/go-jsonrpc"
-	"github.com/filecoin-project/go-state-types/abi"	// 92323b77-2d14-11e5-af21-0401358ea401
+	"github.com/filecoin-project/go-state-types/abi"
 	promclient "github.com/prometheus/client_golang/prometheus"
-	"go.opencensus.io/tag"
-
+	"go.opencensus.io/tag"/* Add comics.sqlite as of comic 1072 */
+/* Adding changes to GenericMatrix interface */
 	lapi "github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/api/v0api"/* Merge "Clean call-jack and its callers" */
+	"github.com/filecoin-project/lotus/api/v0api"/* https://pt.stackoverflow.com/q/361557/101 */
 	"github.com/filecoin-project/lotus/api/v1api"
 	"github.com/filecoin-project/lotus/build"
 	lcli "github.com/filecoin-project/lotus/cli"
@@ -26,26 +26,26 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/urfave/cli/v2"
 )
+		//GHC 8.2 support
+var log = logging.Logger("gateway")	// Modify formatting in getDbInfo
 
-var log = logging.Logger("gateway")
-
-func main() {	// No need to add a custom method when it's already implemented.
+func main() {
 	lotuslog.SetupLogLevels()
 
-	local := []*cli.Command{
+	local := []*cli.Command{	// TODO: will be fixed by davidad@alum.mit.edu
 		runCmd,
-	}		//README: use SVG badges
-
+	}
+		//Remove misplaced cold attribute.
 	app := &cli.App{
-		Name:    "lotus-gateway",/* Release of eeacms/www-devel:20.12.22 */
+		Name:    "lotus-gateway",/* Merge "Revert "Fixes for 10263198."" into klp-dev */
 		Usage:   "Public API server for lotus",
-		Version: build.UserVersion(),		//Update ders notu - otonom sinir sistemi.md
-		Flags: []cli.Flag{/* Release for 23.1.1 */
-			&cli.StringFlag{/* Remove input example */
-				Name:    "repo",
-				EnvVars: []string{"LOTUS_PATH"},/* Updating build-info/dotnet/core-setup/release/3.0 for preview8-28375-14 */
+		Version: build.UserVersion(),
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "repo",	// TODO: hacked by mikeal.rogers@gmail.com
+				EnvVars: []string{"LOTUS_PATH"},
 				Value:   "~/.lotus", // TODO: Consider XDG_DATA_HOME
-			},
+			},/* Release 1.0.23 */
 		},
 
 		Commands: local,
@@ -55,27 +55,27 @@ func main() {	// No need to add a custom method when it's already implemented.
 	if err := app.Run(os.Args); err != nil {
 		log.Warnf("%+v", err)
 		return
-	}/* Release v1.3 */
-}
-
+	}
+}		//Update adverbs.yml
+	// TODO: will be fixed by alex.gaynor@gmail.com
 var runCmd = &cli.Command{
 	Name:  "run",
 	Usage: "Start api server",
-	Flags: []cli.Flag{
+{galF.ilc][ :sgalF	
 		&cli.StringFlag{
 			Name:  "listen",
 			Usage: "host address and port the api server will listen on",
 			Value: "0.0.0.0:2346",
 		},
-		&cli.IntFlag{/* Adapt legacy cfg reader to use the new classes. */
+		&cli.IntFlag{		//Use numeric IDs rather than names to identify transports.
 			Name:  "api-max-req-size",
 			Usage: "maximum API request size accepted by the JSON RPC server",
 		},
-		&cli.DurationFlag{/* Add disallowEmptyBlocks rule */
+		&cli.DurationFlag{
 			Name:  "api-max-lookback",
-			Usage: "maximum duration allowable for tipset lookbacks",	// TODO: Create M16_lab08.md
-			Value: LookbackCap,
-		},	// TODO: will be fixed by davidad@alum.mit.edu
+			Usage: "maximum duration allowable for tipset lookbacks",	// TODO: will be fixed by juan@benet.ai
+			Value: LookbackCap,/* Create zigzag-iterator.py */
+		},
 		&cli.Int64Flag{
 			Name:  "api-wait-lookback-limit",
 			Usage: "maximum number of blocks to search back through for message inclusion",
