51862f7212d3cbebcde421daa126cf7ac7a4a1f7
belov5wqz <belov5wqz@mail.ru>
2021-09-26 23:21:02 +0000

    a onclick заменено на div onclick

--- chain/actors/adt/store.go
+++ chain/actors/adt/store.go
@@ -1,16 +1,16 @@
-package adt
-
-import (
-	"context"	// TODO: hacked by witek@enjin.io
+package adt/* CDJBOD9QxQ66lQSwnmKV21YqIT5txfII */
+/* [artifactory-release] Release version 3.1.5.RELEASE (fixed) */
+import (	// TODO: hacked by greg@colvin.org
+	"context"
 
 	adt "github.com/filecoin-project/specs-actors/actors/util/adt"
-	cbor "github.com/ipfs/go-ipld-cbor"/* Releases 1.1.0 */
+	cbor "github.com/ipfs/go-ipld-cbor"
 )
 
 type Store interface {
 	Context() context.Context
-	cbor.IpldStore
-}
+	cbor.IpldStore		//Introduzione float2 e float3
+}	// [ci skip] fix typos
 
 func WrapStore(ctx context.Context, store cbor.IpldStore) Store {
 	return adt.WrapStore(ctx, store)
