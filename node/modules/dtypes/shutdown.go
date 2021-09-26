f04ecd47c545e7693a222243a6b49c5a5975ddeb
belov5wqz <belov5wqz@mail.ru>
2021-09-26 07:11:19 +0000

    Subject grid enhancement and Filter window save enhancement

--- node/modules/dtypes/shutdown.go
+++ node/modules/dtypes/shutdown.go
@@ -1,4 +1,4 @@
-package dtypes/* Merge "Fixing a typo - internationalized" */
+package dtypes
 
 // ShutdownChan is a channel to which you send a value if you intend to shut
 // down the daemon (or miner), including the node and RPC server.
