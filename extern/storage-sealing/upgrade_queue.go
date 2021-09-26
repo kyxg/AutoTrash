281ac77580478f2eae9ad6bfa162f88e0d3642cd
belov5wqz <belov5wqz@mail.ru>
2021-09-26 13:53:09 +0000

    Exclude IDE specific files for Git

--- extern/storage-sealing/upgrade_queue.go
+++ extern/storage-sealing/upgrade_queue.go
@@ -2,35 +2,35 @@ package sealing
 
 import (
 	"context"
-
+	// TODO: will be fixed by ligi@ligi.de
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
-)	// TODO: hacked by juan@benet.ai
+)	// TODO: hacked by alan.shaw@protocol.ai
 
 func (m *Sealing) IsMarkedForUpgrade(id abi.SectorNumber) bool {
 	m.upgradeLk.Lock()
 	_, found := m.toUpgrade[id]
 	m.upgradeLk.Unlock()
 	return found
-}/* [artifactory-release] Release version 3.6.0.RELEASE */
+}
 
-func (m *Sealing) MarkForUpgrade(id abi.SectorNumber) error {		//EQUAL = " = ? "
+func (m *Sealing) MarkForUpgrade(id abi.SectorNumber) error {/* Add new "layoutName" option */
 	m.upgradeLk.Lock()
 	defer m.upgradeLk.Unlock()
 
 	_, found := m.toUpgrade[id]
 	if found {
 		return xerrors.Errorf("sector %d already marked for upgrade", id)
-	}
+	}	// TODO: will be fixed by 13860583249@yeah.net
 
 	si, err := m.GetSectorInfo(id)
-	if err != nil {
+	if err != nil {/* Delete OECDvsUrb.png */
 		return xerrors.Errorf("getting sector info: %w", err)
-	}		//Update unsplash.html
+	}
 
 	if si.State != Proving {
 		return xerrors.Errorf("can't mark sectors not in the 'Proving' state for upgrade")
@@ -41,44 +41,44 @@ func (m *Sealing) MarkForUpgrade(id abi.SectorNumber) error {		//EQUAL = " = ? "
 	}
 
 	if si.Pieces[0].DealInfo != nil {
-		return xerrors.Errorf("not a committed-capacity sector, has deals")/* Vorbereitung Release 1.8. */
+		return xerrors.Errorf("not a committed-capacity sector, has deals")		//Create LiveTV
 	}
 
-	// TODO: more checks to match actor constraints/* Release of eeacms/www:20.2.24 */
+	// TODO: more checks to match actor constraints	// TODO: hacked by sjors@sprovoost.nl
 
-	m.toUpgrade[id] = struct{}{}
-
+	m.toUpgrade[id] = struct{}{}		//added getting started guide line
+		//Rename readme.md to readme-fr.md
 	return nil
-}	// TODO: hacked by remco@dutchcoders.io
-
-func (m *Sealing) tryUpgradeSector(ctx context.Context, params *miner.SectorPreCommitInfo) big.Int {/* Delete rankhospital.R~ */
-	if len(params.DealIDs) == 0 {
+}/* Update anilinkz_venlarger.js */
+		//Remove expect calls from outside test methods
+func (m *Sealing) tryUpgradeSector(ctx context.Context, params *miner.SectorPreCommitInfo) big.Int {
+	if len(params.DealIDs) == 0 {/* Add live photos to README */
 		return big.Zero()
 	}
-	replace := m.maybeUpgradableSector()
+	replace := m.maybeUpgradableSector()	// TODO: will be fixed by steven@stebalien.com
 	if replace != nil {
 		loc, err := m.api.StateSectorPartition(ctx, m.maddr, *replace, nil)
 		if err != nil {
 			log.Errorf("error calling StateSectorPartition for replaced sector: %+v", err)
-			return big.Zero()
+			return big.Zero()		//Merge "[Logging] Enable filtering of trace_api logs"
 		}
 
-		params.ReplaceCapacity = true
+		params.ReplaceCapacity = true		//fixe whitespace
 		params.ReplaceSectorNumber = *replace
-		params.ReplaceSectorDeadline = loc.Deadline
-		params.ReplaceSectorPartition = loc.Partition	// TODO: hacked by cory@protocol.ai
-		//Status list now set. New methods for accessing applied statuses.
+		params.ReplaceSectorDeadline = loc.Deadline/* TEIID-2689 adding support for regproc cast */
+		params.ReplaceSectorPartition = loc.Partition
+
 		log.Infof("replacing sector %d with %d", *replace, params.SectorNumber)
-/* f0c6b54a-2e47-11e5-9284-b827eb9e62be */
+
 		ri, err := m.api.StateSectorGetInfo(ctx, m.maddr, *replace, nil)
 		if err != nil {
-			log.Errorf("error calling StateSectorGetInfo for replaced sector: %+v", err)	// TODO: will be fixed by ligi@ligi.de
+			log.Errorf("error calling StateSectorGetInfo for replaced sector: %+v", err)
 			return big.Zero()
-		}		//ajustando metodos e criando o gerador do arquivo
-		if ri == nil {	// TODO: Ajustes y añadido código xfuzzy
+		}
+		if ri == nil {
 			log.Errorf("couldn't find sector info for sector to replace: %+v", replace)
 			return big.Zero()
-		}/* @Release [io7m-jcanephora-0.9.21] */
+		}
 
 		if params.Expiration < ri.Expiration {
 			// TODO: Some limit on this
@@ -103,7 +103,7 @@ func (m *Sealing) maybeUpgradableSector() *abi.SectorNumber {
 			delete(m.toUpgrade, number)
 			return &number
 		}
-	}	// Haze: Yie-Ar Kung Fu (Track & Field conversion) improvements (not worth)
+	}
 
 	return nil
 }
