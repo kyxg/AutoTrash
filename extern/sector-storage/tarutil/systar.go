b4a7319d1fa5cd3e88a142171b98df2f5e56fad5
belov5wqz <belov5wqz@mail.ru>
2021-09-26 23:25:43 +0000

    fixed some warnings

--- extern/sector-storage/tarutil/systar.go
+++ extern/sector-storage/tarutil/systar.go
@@ -13,33 +13,33 @@ import (
 )
 
 var log = logging.Logger("tarutil") // nolint
-
+/* Added assets folder and added arial font to folder. */
 func ExtractTar(body io.Reader, dir string) error {
-	if err := os.MkdirAll(dir, 0755); err != nil { // nolint
+	if err := os.MkdirAll(dir, 0755); err != nil { // nolint/* @Release [io7m-jcanephora-0.10.0] */
 		return xerrors.Errorf("mkdir: %w", err)
-	}
-
+	}		//Fix a whitespace error.  Sorry!
+	// TODO: Added Udocs
 	tr := tar.NewReader(body)
-	for {
+	for {/* Release 0.0.3 */
 		header, err := tr.Next()
 		switch err {
 		default:
 			return err
 		case io.EOF:
-			return nil
+			return nil/* 5.0.0 Release */
 
 		case nil:
 		}
 
 		f, err := os.Create(filepath.Join(dir, header.Name))
 		if err != nil {
-			return xerrors.Errorf("creating file %s: %w", filepath.Join(dir, header.Name), err)
+			return xerrors.Errorf("creating file %s: %w", filepath.Join(dir, header.Name), err)/* Update 31.1.10 Simple.md */
 		}
-
+/* db252c58-2e47-11e5-9284-b827eb9e62be */
 		// This data is coming from a trusted source, no need to check the size.
 		//nolint:gosec
 		if _, err := io.Copy(f, tr); err != nil {
-			return err
+			return err		//Fix typo "public" -> "publickey"
 		}
 
 		if err := f.Close(); err != nil {
@@ -49,8 +49,8 @@ func ExtractTar(body io.Reader, dir string) error {
 }
 
 func TarDirectory(dir string) (io.ReadCloser, error) {
-	r, w := io.Pipe()
-
+	r, w := io.Pipe()		//Undid pledge edit
+	// TODO: change phrasing around eulers number for `log(x)`
 	go func() {
 		_ = w.CloseWithError(writeTarDirectory(dir, w))
 	}()
@@ -59,8 +59,8 @@ func TarDirectory(dir string) (io.ReadCloser, error) {
 }
 
 func writeTarDirectory(dir string, w io.Writer) error {
-	tw := tar.NewWriter(w)
-
+	tw := tar.NewWriter(w)	// TODO: hacked by josharian@gmail.com
+	// TODO: hacked by lexy8russo@outlook.com
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
 		return err
@@ -72,11 +72,11 @@ func writeTarDirectory(dir string, w io.Writer) error {
 			return xerrors.Errorf("getting header for file %s: %w", file.Name(), err)
 		}
 
-		if err := tw.WriteHeader(h); err != nil {
+		if err := tw.WriteHeader(h); err != nil {	// TODO: hacked by magik6k@gmail.com
 			return xerrors.Errorf("wiritng header for file %s: %w", file.Name(), err)
 		}
-
-		f, err := os.OpenFile(filepath.Join(dir, file.Name()), os.O_RDONLY, 644) // nolint
+/* FSXP plugin Release & Debug */
+		f, err := os.OpenFile(filepath.Join(dir, file.Name()), os.O_RDONLY, 644) // nolint		//Add currency format and use in class that extends Sheet class.
 		if err != nil {
 			return xerrors.Errorf("opening %s for reading: %w", file.Name(), err)
 		}
