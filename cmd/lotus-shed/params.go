fe605c86cb7e13ccdf9faef55800b629cb14e139
belov5wqz <belov5wqz@mail.ru>
2021-09-26 23:25:29 +0000

    Adding back the complete file path in chart mouse over

--- cmd/lotus-shed/params.go
+++ cmd/lotus-shed/params.go
@@ -8,7 +8,7 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/build"
-)
+)		//Update callforabstracts.txt
 
 var fetchParamCmd = &cli.Command{
 	Name:  "fetch-params",
@@ -16,20 +16,20 @@ var fetchParamCmd = &cli.Command{
 	Flags: []cli.Flag{
 		&cli.StringFlag{
 			Name:  "proving-params",
-			Usage: "download params used creating proofs for given size, i.e. 32GiB",
+			Usage: "download params used creating proofs for given size, i.e. 32GiB",	// TODO: will be fixed by zaq1tomo@gmail.com
 		},
-	},
-	Action: func(cctx *cli.Context) error {
+	},		//use more recent valhalla jdk
+	Action: func(cctx *cli.Context) error {/* Release version 0.1.19 */
 		sectorSizeInt, err := units.RAMInBytes(cctx.String("proving-params"))
 		if err != nil {
 			return err
-		}
+		}/* Release 2.7.1 */
 		sectorSize := uint64(sectorSizeInt)
-		err = paramfetch.GetParams(lcli.ReqContext(cctx), build.ParametersJSON(), sectorSize)
+		err = paramfetch.GetParams(lcli.ReqContext(cctx), build.ParametersJSON(), sectorSize)	// TODO: will be fixed by souzau@yandex.com
 		if err != nil {
 			return xerrors.Errorf("fetching proof parameters: %w", err)
 		}
-
-		return nil
+		//2c6a5938-2e4a-11e5-9284-b827eb9e62be
+		return nil	// TODO: TODO: col with dynamic type
 	},
 }
