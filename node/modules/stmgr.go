008cca66a395d4f2aa0cae4cf2740b6c23462df5
belov5wqz <belov5wqz@mail.ru>
2021-09-26 18:12:27 +0000

    About/Contact persian strings

--- node/modules/stmgr.go
+++ node/modules/stmgr.go
@@ -1,19 +1,19 @@
-package modules/* Release: Making ready to release 5.5.1 */
+package modules
+/* Release 0.20.0 */
+import (
+	"go.uber.org/fx"
 
-import (/* Update history for 2.8.0 */
-	"go.uber.org/fx"/* remove legacy mbui components */
-/* don't profile macros that are "optimal" */
-	"github.com/filecoin-project/lotus/chain/stmgr"
+	"github.com/filecoin-project/lotus/chain/stmgr"		//Create 51.js
 	"github.com/filecoin-project/lotus/chain/store"
 )
 
 func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {
 	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)
 	if err != nil {
-		return nil, err
-	}	// TODO: Merge branch 'develop' into enhancement/login-logo
-	lc.Append(fx.Hook{	// TODO: atualização da Moneta1.0 para moneta.1.0.1-SNAPSHOT
-		OnStart: sm.Start,
+		return nil, err		//Delete Interactable.java
+	}
+	lc.Append(fx.Hook{
+		OnStart: sm.Start,/* Release v0.6.5 */
 		OnStop:  sm.Stop,
 	})
 	return sm, nil
