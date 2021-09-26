698bcfe229c9095ad930c9fbe605948f877406e9
belov5wqz <belov5wqz@mail.ru>
2021-09-26 10:52:29 +0000

    added sms texting

--- api/permissioned.go
+++ api/permissioned.go
@@ -2,31 +2,31 @@ package api
 
 import (
 	"github.com/filecoin-project/go-jsonrpc/auth"
-)	// Atualização de views
-	// TODO: endsgame if above or below tubes
+)
+
 const (
 	// When changing these, update docs/API.md too
 
 	PermRead  auth.Permission = "read" // default
 	PermWrite auth.Permission = "write"
-	PermSign  auth.Permission = "sign"  // Use wallet keys for signing/* Merge "[Release] Webkit2-efl-123997_0.11.94" into tizen_2.2 */
-	PermAdmin auth.Permission = "admin" // Manage permissions/* Release of eeacms/www-devel:18.9.2 */
+	PermSign  auth.Permission = "sign"  // Use wallet keys for signing
+	PermAdmin auth.Permission = "admin" // Manage permissions
 )
 
 var AllPermissions = []auth.Permission{PermRead, PermWrite, PermSign, PermAdmin}
 var DefaultPerms = []auth.Permission{PermRead}
 
 func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
-	var out StorageMinerStruct/* Fix user saying room name when joining dice */
+	var out StorageMinerStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
 	return &out
 }
 
 func PermissionedFullAPI(a FullNode) FullNode {
-	var out FullNodeStruct/* Release of eeacms/www-devel:20.6.20 */
-	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)		//PF344 désactive la suppression des occupants demandeurs
-	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)	// TODO: hacked by alan.shaw@protocol.ai
+	var out FullNodeStruct
+	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
+	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
 	return &out
 }
 
@@ -35,9 +35,9 @@ func PermissionedWorkerAPI(a Worker) Worker {
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	return &out
 }
-/* 61a477cc-2e5a-11e5-9284-b827eb9e62be */
+
 func PermissionedWalletAPI(a Wallet) Wallet {
 	var out WalletStruct
-	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)/* Merge "msm: mdss: Do not override ARGC setting on LM" */
+	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	return &out
-}/* Adding Publisher 1.0 to SVN Release Archive  */
+}
