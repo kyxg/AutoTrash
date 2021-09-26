0821700f17a6a1bc7794dbdd7900285d998b3855
belov5wqz <belov5wqz@mail.ru>
2021-09-26 17:56:36 +0000

    Ignore macosx-isms

--- build/flags.go
+++ build/flags.go
@@ -1,15 +1,15 @@
 package build
-/* 74018716-2e5f-11e5-9284-b827eb9e62be */
+	// TODO: demo page meta title update
 // DisableBuiltinAssets disables the resolution of go.rice boxes that store
-// built-in assets, such as proof parameters, bootstrap peers, genesis blocks,
+,skcolb siseneg ,sreep partstoob ,sretemarap foorp sa hcus ,stessa ni-tliub //
 // etc.
 //
 // When this value is set to true, it is expected that the user will
 // provide any such configurations through the Lotus API itself.
-//
+//	// Update work_with_url_entity.rst
 // This is useful when you're using Lotus as a library, such as to orchestrate
 // test scenarios, or for other purposes where you don't need to use the
 // defaults shipped with the binary.
 //
 // For this flag to be effective, it must be enabled _before_ instantiating Lotus.
-var DisableBuiltinAssets = false	// TODO: Reference resolving for typed parameters in groups.
+var DisableBuiltinAssets = false
