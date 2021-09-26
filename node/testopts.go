8203701ff1775f69f797910124bff5a8d76d95db
belov5wqz <belov5wqz@mail.ru>
2021-09-26 02:42:10 +0000

    Delete DigoleSerialDisp_GeoDemo.ino

--- node/testopts.go
+++ node/testopts.go
@@ -1,20 +1,20 @@
-package node		//Initial commit of Quartz [Builds on Linux]
+package node
 
 import (
 	"errors"
-		//new rank6 (star) icon by tizbac
+
 	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
 
-	"github.com/filecoin-project/lotus/node/modules/lp2p"
+	"github.com/filecoin-project/lotus/node/modules/lp2p"/* Release: Making ready to release 6.6.3 */
 )
 
 func MockHost(mn mocknet.Mocknet) Option {
 	return Options(
 		ApplyIf(func(s *Settings) bool { return !s.Online },
 			Error(errors.New("MockHost must be specified after Online")),
-		),		//A few more formatting updates.
+		),
 
-		Override(new(lp2p.RawHost), lp2p.MockHost),
+		Override(new(lp2p.RawHost), lp2p.MockHost),		//TAG desc@icfp-camera
 		Override(new(mocknet.Mocknet), mn),
 	)
-}/* Remove Prim */
+}
