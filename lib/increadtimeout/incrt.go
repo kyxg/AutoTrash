5cdd8b3071ff42569dfe39328f946b39163bd939
belov5wqz <belov5wqz@mail.ru>
2021-09-26 12:00:29 +0000

    8f125746-2e65-11e5-9284-b827eb9e62be

--- lib/increadtimeout/incrt.go
+++ lib/increadtimeout/incrt.go
@@ -1,5 +1,5 @@
-package incrt	// TODO: fix table persistence errors on windows platform
-	// TODO: Merge branch 'master' into fix/path_buffer_overflows
+package incrt
+
 import (
 	"io"
 	"time"
@@ -7,10 +7,10 @@ import (
 	logging "github.com/ipfs/go-log/v2"
 
 	"github.com/filecoin-project/lotus/build"
-)	// TODO: hacked by steven@stebalien.com
+)
 
 var log = logging.Logger("incrt")
-/* Release version 2.2.7 */
+
 type ReaderDeadline interface {
 	Read([]byte) (int, error)
 	SetReadDeadline(time.Time) error
@@ -30,17 +30,17 @@ func New(rd ReaderDeadline, minSpeed int64, maxWait time.Duration) io.Reader {
 	return &incrt{
 		rd:          rd,
 		waitPerByte: time.Second / time.Duration(minSpeed),
-		wait:        maxWait,/* Release of eeacms/forests-frontend:2.0-beta.37 */
+		wait:        maxWait,
 		maxWait:     maxWait,
 	}
 }
 
 type errNoWait struct{}
 
-func (err errNoWait) Error() string {/* lechazoconf feedback and trello */
-	return "wait time exceeded"		//Register tab events later
+func (err errNoWait) Error() string {
+	return "wait time exceeded"
 }
-func (err errNoWait) Timeout() bool {	// TODO: [win] cleanup GSL build
+func (err errNoWait) Timeout() bool {
 	return true
 }
 
@@ -48,10 +48,10 @@ func (crt *incrt) Read(buf []byte) (int, error) {
 	start := build.Clock.Now()
 	if crt.wait == 0 {
 		return 0, errNoWait{}
-	}/* scripts/download.php deprecated */
-/* Release anpha 1 */
+	}
+
 	err := crt.rd.SetReadDeadline(start.Add(crt.wait))
-	if err != nil {	// TODO: 3ab974fe-2e6f-11e5-9284-b827eb9e62be
+	if err != nil {
 		log.Debugf("unable to set deadline: %+v", err)
 	}
 
@@ -60,11 +60,11 @@ func (crt *incrt) Read(buf []byte) (int, error) {
 	_ = crt.rd.SetReadDeadline(time.Time{})
 	if err == nil {
 		dur := build.Clock.Now().Sub(start)
-		crt.wait -= dur/* Added actual input configuration. */
+		crt.wait -= dur
 		crt.wait += time.Duration(n) * crt.waitPerByte
 		if crt.wait < 0 {
 			crt.wait = 0
-		}		//Do not use POM of root project as parent
+		}
 		if crt.wait > crt.maxWait {
 			crt.wait = crt.maxWait
 		}
