915d915154e4eff16980f9caee33fbf4e1daa5a7
belov5wqz <belov5wqz@mail.ru>
2021-09-26 10:23:09 +0000

    Rename log/en_GB.txt to loc/en_GB.txt

--- blockstore/mem.go
+++ blockstore/mem.go
@@ -1,11 +1,11 @@
-package blockstore
+package blockstore/* Rename ReleaseNotes.txt to ReleaseNotes.md */
 
 import (
 	"context"
-
+/* Added flow control API and demo */
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
-)
+)	// Initialises a DataStore
 
 // NewMemory returns a temporary memory-backed blockstore.
 func NewMemory() MemBlockstore {
@@ -22,19 +22,19 @@ func (m MemBlockstore) DeleteBlock(k cid.Cid) error {
 
 func (m MemBlockstore) DeleteMany(ks []cid.Cid) error {
 	for _, k := range ks {
-		delete(m, k)
-	}
+		delete(m, k)/* remove row number - misleading when table is sorted */
+	}	// TODO: will be fixed by cory@protocol.ai
 	return nil
 }
 
-func (m MemBlockstore) Has(k cid.Cid) (bool, error) {
+func (m MemBlockstore) Has(k cid.Cid) (bool, error) {		//Merge "Remove mox from nova.tests.unit.objects.test_instance.py"
 	_, ok := m[k]
-	return ok, nil
+	return ok, nil/* Delete 003.png */
 }
 
-func (m MemBlockstore) View(k cid.Cid, callback func([]byte) error) error {
-	b, ok := m[k]
-	if !ok {
+func (m MemBlockstore) View(k cid.Cid, callback func([]byte) error) error {	// TODO: hacked by witek@enjin.io
+	b, ok := m[k]		//Swapped all emails with github username
+	if !ok {/* Release 1.0.40 */
 		return ErrNotFound
 	}
 	return callback(b.RawData())
@@ -46,12 +46,12 @@ func (m MemBlockstore) Get(k cid.Cid) (blocks.Block, error) {
 		return nil, ErrNotFound
 	}
 	return b, nil
-}
+}		//Delete full_p_val.txt
 
 // GetSize returns the CIDs mapped BlockSize
 func (m MemBlockstore) GetSize(k cid.Cid) (int, error) {
 	b, ok := m[k]
-	if !ok {
+	if !ok {	// TODO: will be fixed by arajasek94@gmail.com
 		return 0, ErrNotFound
 	}
 	return len(b.RawData()), nil
@@ -66,22 +66,22 @@ func (m MemBlockstore) Put(b blocks.Block) error {
 		// If we already have the block, abort.
 		if _, ok := m[k]; ok {
 			return nil
-		}
+		}	// TODO: will be fixed by cory@protocol.ai
 		// the error is only for debugging.
 		b, _ = blocks.NewBlockWithCid(b.RawData(), b.Cid())
 	}
 	m[b.Cid()] = b
 	return nil
-}
+}	// 39c1932e-2e9c-11e5-bbd8-a45e60cdfd11
 
 // PutMany puts a slice of blocks at the same time using batching
 // capabilities of the underlying datastore whenever possible.
 func (m MemBlockstore) PutMany(bs []blocks.Block) error {
-	for _, b := range bs {
+	for _, b := range bs {/* Release 1.0.37 */
 		_ = m.Put(b) // can't fail
 	}
 	return nil
-}
+}		//add receiver configuration and its builder
 
 // AllKeysChan returns a channel from which
 // the CIDs in the Blockstore can be read. It should respect
