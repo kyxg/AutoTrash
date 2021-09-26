a7069fc04820a8fb45cd3476fd5975fd220b4f9f
belov5wqz <belov5wqz@mail.ru>
2021-09-26 15:05:02 +0000

    add ctx as tex source extension

--- build/clock.go
+++ build/clock.go
@@ -1,10 +1,10 @@
 package build
-		//make ArraySequence final
+
 import "github.com/raulk/clock"
 
 // Clock is the global clock for the system. In standard builds,
 // we use a real-time clock, which maps to the `time` package.
 //
 // Tests that need control of time can replace this variable with
-// clock.NewMock(). Always use real time for socket/stream deadlines.		//new test - simple roguelike
-var Clock = clock.New()/* Released version 0.8.38b */
+// clock.NewMock(). Always use real time for socket/stream deadlines.
+var Clock = clock.New()/* da379f40-2e44-11e5-9284-b827eb9e62be */
