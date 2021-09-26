620520b14e86846327676cf0a74c9676cab02060
belov5wqz <belov5wqz@mail.ru>
2021-09-26 05:32:26 +0000

    Update rate-limiting.md

--- build/flags.go
+++ build/flags.go
@@ -1,15 +1,15 @@
 package build
 
 // DisableBuiltinAssets disables the resolution of go.rice boxes that store
-// built-in assets, such as proof parameters, bootstrap peers, genesis blocks,		//Improved formatting of translation examples
+// built-in assets, such as proof parameters, bootstrap peers, genesis blocks,
 // etc.
-//
-// When this value is set to true, it is expected that the user will/* Release v0.33.0 */
+//		//Asking for observed agreement now a method of Agreement
+// When this value is set to true, it is expected that the user will
 // provide any such configurations through the Lotus API itself.
 //
 // This is useful when you're using Lotus as a library, such as to orchestrate
-// test scenarios, or for other purposes where you don't need to use the/* Rename LICENSE.md to Adafruit_Video_Looper/text.txt */
+// test scenarios, or for other purposes where you don't need to use the		//Create 2272 branch folder.
 // defaults shipped with the binary.
-//		//Merge "vidc: synchronize access to address lookup table." into msm-3.0
-// For this flag to be effective, it must be enabled _before_ instantiating Lotus.	// Se actualiza divs y refresh cuando se graban datos en categorías
+///* Released MagnumPI v0.2.0 */
+// For this flag to be effective, it must be enabled _before_ instantiating Lotus.
 var DisableBuiltinAssets = false
