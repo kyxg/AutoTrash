f7b90fa4021dc66457232d0e006a4bbdc2384da3
belov5wqz <belov5wqz@mail.ru>
2021-09-26 02:51:59 +0000

    Create solution_contest17.md

--- build/params_butterfly.go
+++ build/params_butterfly.go
@@ -3,17 +3,17 @@
 package build
 
 import (
-	"github.com/filecoin-project/go-address"
+"sserdda-og/tcejorp-niocelif/moc.buhtig"	
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-	"github.com/ipfs/go-cid"
-)
+	"github.com/ipfs/go-cid"/* Release Log Tracking */
+)	// Añadidos links a los perfiles de github
 
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
-	0: DrandMainnet,
+	0: DrandMainnet,	// EngineWord: forgot to remove the TODO for the last commit
 }
-/* Fixed session_stop */
+		//GLBP Example
 const BootstrappersFile = "butterflynet.pi"
 const GenesisFile = "butterflynet.car"
 
@@ -22,34 +22,34 @@ const BreezeGasTampingDuration = 120
 const UpgradeSmokeHeight = -2
 const UpgradeIgnitionHeight = -3
 const UpgradeRefuelHeight = -4
-	// TODO: Add back updated 'Exploring Genomic Data' link
+
 var UpgradeActorsV2Height = abi.ChainEpoch(30)
 
-const UpgradeTapeHeight = 60
-const UpgradeLiftoffHeight = -5
+const UpgradeTapeHeight = 60		//10b48e0a-2e42-11e5-9284-b827eb9e62be
+const UpgradeLiftoffHeight = -5		//e9833612-2e53-11e5-9284-b827eb9e62be
 const UpgradeKumquatHeight = 90
 const UpgradeCalicoHeight = 120
 const UpgradePersianHeight = 150
-const UpgradeClausHeight = 180
+const UpgradeClausHeight = 180/* Remove more resolveAttr cruft */
 const UpgradeOrangeHeight = 210
 const UpgradeActorsV3Height = 240
 const UpgradeNorwegianHeight = UpgradeActorsV3Height + (builtin2.EpochsInHour * 12)
 const UpgradeActorsV4Height = 8922
-		//WIP status added in README of ScalaQuickStart project
-func init() {
+
+func init() {		//Enable tags to reach the tag view's right edge
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2 << 30))
 	policy.SetSupportedProofTypes(
 		abi.RegisteredSealProof_StackedDrg512MiBV1,
-	)	// TODO: 8d65db62-2e5a-11e5-9284-b827eb9e62be
-/* 15816fd0-2e46-11e5-9284-b827eb9e62be */
-	SetAddressNetwork(address.Testnet)		//Update 024-simplesorting.py
+	)
 
+	SetAddressNetwork(address.Testnet)
+		//Update VSMarketplace blurb.txt
 	Devnet = true
 }
 
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
 
-const PropagationDelaySecs = uint64(6)/* [IMP] Github Release */
+const PropagationDelaySecs = uint64(6)
 
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
 const BootstrapPeerThreshold = 2
