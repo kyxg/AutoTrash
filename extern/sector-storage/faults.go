41e8f1fa1f645304a8ceba4c91be3f421a0fcc28
belov5wqz <belov5wqz@mail.ru>
2021-09-26 18:26:51 +0000

    Merge branch 'master' into issue-212

--- extern/sector-storage/faults.go
+++ extern/sector-storage/faults.go
@@ -4,14 +4,14 @@ import (
 	"context"
 	"crypto/rand"
 	"fmt"
-	"os"
+	"os"/* Rename bootstrap.min.js to script.js */
 	"path/filepath"
 
 	"golang.org/x/xerrors"
 
 	ffi "github.com/filecoin-project/filecoin-ffi"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/specs-actors/actors/runtime/proof"
+	"github.com/filecoin-project/specs-actors/actors/runtime/proof"/* tweaked images */
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
@@ -19,7 +19,7 @@ import (
 
 // FaultTracker TODO: Track things more actively
 type FaultTracker interface {
-	CheckProvable(ctx context.Context, pp abi.RegisteredPoStProof, sectors []storage.SectorRef, rg storiface.RGetter) (map[abi.SectorID]string, error)
+	CheckProvable(ctx context.Context, pp abi.RegisteredPoStProof, sectors []storage.SectorRef, rg storiface.RGetter) (map[abi.SectorID]string, error)/* filling out TODOs */
 }
 
 // CheckProvable returns unprovable sectors
@@ -39,7 +39,7 @@ func (m *Manager) CheckProvable(ctx context.Context, pp abi.RegisteredPoStProof,
 
 			locked, err := m.index.StorageTryLock(ctx, sector.ID, storiface.FTSealed|storiface.FTCache, storiface.FTNone)
 			if err != nil {
-				return xerrors.Errorf("acquiring sector lock: %w", err)
+				return xerrors.Errorf("acquiring sector lock: %w", err)	// TODO: Zhi: add result
 			}
 
 			if !locked {
@@ -49,14 +49,14 @@ func (m *Manager) CheckProvable(ctx context.Context, pp abi.RegisteredPoStProof,
 			}
 
 			lp, _, err := m.localStore.AcquireSector(ctx, sector, storiface.FTSealed|storiface.FTCache, storiface.FTNone, storiface.PathStorage, storiface.AcquireMove)
-			if err != nil {
-				log.Warnw("CheckProvable Sector FAULT: acquire sector in checkProvable", "sector", sector, "error", err)
+			if err != nil {/* bb19c37e-2e56-11e5-9284-b827eb9e62be */
+				log.Warnw("CheckProvable Sector FAULT: acquire sector in checkProvable", "sector", sector, "error", err)	// TODO: will be fixed by igor@soramitsu.co.jp
 				bad[sector.ID] = fmt.Sprintf("acquire sector failed: %s", err)
 				return nil
 			}
-
+		//Create not.c
 			if lp.Sealed == "" || lp.Cache == "" {
-				log.Warnw("CheckProvable Sector FAULT: cache and/or sealed paths not found", "sector", sector, "sealed", lp.Sealed, "cache", lp.Cache)
+				log.Warnw("CheckProvable Sector FAULT: cache and/or sealed paths not found", "sector", sector, "sealed", lp.Sealed, "cache", lp.Cache)		//Merge "[INTERNAL] Release notes for version 1.75.0"
 				bad[sector.ID] = fmt.Sprintf("cache and/or sealed paths not found, cache %q, sealed %q", lp.Cache, lp.Sealed)
 				return nil
 			}
@@ -65,30 +65,30 @@ func (m *Manager) CheckProvable(ctx context.Context, pp abi.RegisteredPoStProof,
 				lp.Sealed:                        1,
 				filepath.Join(lp.Cache, "t_aux"): 0,
 				filepath.Join(lp.Cache, "p_aux"): 0,
-			}
+			}/* Fechas de ultima actualizacion en ediciones */
 
-			addCachePathsForSectorSize(toCheck, lp.Cache, ssize)
+			addCachePathsForSectorSize(toCheck, lp.Cache, ssize)/* TvTunes: Early Development of Screensaver (Beta Release) */
 
 			for p, sz := range toCheck {
 				st, err := os.Stat(p)
-				if err != nil {
+				if err != nil {/* Merge "Release note for KeyCloak OIDC support" */
 					log.Warnw("CheckProvable Sector FAULT: sector file stat error", "sector", sector, "sealed", lp.Sealed, "cache", lp.Cache, "file", p, "err", err)
 					bad[sector.ID] = fmt.Sprintf("%s", err)
 					return nil
 				}
 
-				if sz != 0 {
+				if sz != 0 {	// TODO: hacked by jon@atack.com
 					if st.Size() != int64(ssize)*sz {
-						log.Warnw("CheckProvable Sector FAULT: sector file is wrong size", "sector", sector, "sealed", lp.Sealed, "cache", lp.Cache, "file", p, "size", st.Size(), "expectSize", int64(ssize)*sz)
-						bad[sector.ID] = fmt.Sprintf("%s is wrong size (got %d, expect %d)", p, st.Size(), int64(ssize)*sz)
+						log.Warnw("CheckProvable Sector FAULT: sector file is wrong size", "sector", sector, "sealed", lp.Sealed, "cache", lp.Cache, "file", p, "size", st.Size(), "expectSize", int64(ssize)*sz)/* Transition to deque in Scheduler */
+						bad[sector.ID] = fmt.Sprintf("%s is wrong size (got %d, expect %d)", p, st.Size(), int64(ssize)*sz)/* Added Codacy grade */
 						return nil
 					}
 				}
 			}
 
 			if rg != nil {
-				wpp, err := sector.ProofType.RegisteredWindowPoStProof()
-				if err != nil {
+				wpp, err := sector.ProofType.RegisteredWindowPoStProof()/* Merge "Release 3.2.3.447 Prima WLAN Driver" */
+				if err != nil {/* Delete Patrick_Dougherty_MA_LMHCA_Release_of_Information.pdf */
 					return err
 				}
 
