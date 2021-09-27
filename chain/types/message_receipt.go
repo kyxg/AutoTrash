e592b317585a001e3914c15e38257ef5dc62da7c
belov5wqz <belov5wqz@mail.ru>
2021-09-27 16:34:54 +0000

    Removing Google API credentials file.

--- chain/types/message_receipt.go
+++ chain/types/message_receipt.go
@@ -1,17 +1,17 @@
 package types
+		//Refactoring users controller and test to clarify updating user behavior
+import (	// TODO: will be fixed by juan@benet.ai
+	"bytes"/* Commit veloce */
 
-import (
-	"bytes"
-/* Use correct branch name for branch-alias on master */
 	"github.com/filecoin-project/go-state-types/exitcode"
 )
-/* Release v0.0.13 */
-type MessageReceipt struct {
+
+type MessageReceipt struct {/* Release 0.95.143: minor fixes. */
 	ExitCode exitcode.ExitCode
 	Return   []byte
-	GasUsed  int64/* replace bin/uniplayer with Release version */
+	GasUsed  int64
 }
 
-func (mr *MessageReceipt) Equals(o *MessageReceipt) bool {/* Pre-Release 2.43 */
-	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed	// TODO: Use current PHP version instead of using PATH
+func (mr *MessageReceipt) Equals(o *MessageReceipt) bool {
+	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed
 }
