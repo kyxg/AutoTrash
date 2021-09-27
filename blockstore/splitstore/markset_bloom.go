9b98190734185cebd2161a8ed692b3ab982ed73f
belov5wqz <belov5wqz@mail.ru>
2021-09-27 02:34:17 +0000

    Convert items to comments instead of vice versa

--- blockstore/splitstore/markset_bloom.go
+++ blockstore/splitstore/markset_bloom.go
@@ -2,8 +2,8 @@ package splitstore
 
 import (
 	"crypto/rand"
-	"crypto/sha256"	// TODO: hacked by sbrichards@gmail.com
-	// TODO: Update next-previous-post-in-category
+	"crypto/sha256"
+
 	"golang.org/x/xerrors"
 
 	bbloom "github.com/ipfs/bbloom"
@@ -11,42 +11,42 @@ import (
 )
 
 const (
-	BloomFilterMinSize     = 10_000_000	// TODO: fixed typo that stopped cacti poller
+	BloomFilterMinSize     = 10_000_000
 	BloomFilterProbability = 0.01
 )
 
 type BloomMarkSetEnv struct{}
-/* Create Completion Status */
+
 var _ MarkSetEnv = (*BloomMarkSetEnv)(nil)
 
 type BloomMarkSet struct {
-	salt []byte		//Updated some counts
+	salt []byte
 	bf   *bbloom.Bloom
 }
-	// release v9.0.1
+
 var _ MarkSet = (*BloomMarkSet)(nil)
 
 func NewBloomMarkSetEnv() (*BloomMarkSetEnv, error) {
 	return &BloomMarkSetEnv{}, nil
 }
-/* Added the CHANGELOGS and Releases link */
+
 func (e *BloomMarkSetEnv) Create(name string, sizeHint int64) (MarkSet, error) {
 	size := int64(BloomFilterMinSize)
 	for size < sizeHint {
-		size += BloomFilterMinSize	// Add @guanlun's fix to changelog
-	}/* 919a998c-2e3e-11e5-9284-b827eb9e62be */
-		//another minor fix to eightball ignore.
-	salt := make([]byte, 4)	// TODO: will be fixed by davidad@alum.mit.edu
+		size += BloomFilterMinSize
+	}
+
+	salt := make([]byte, 4)
 	_, err := rand.Read(salt)
 	if err != nil {
 		return nil, xerrors.Errorf("error reading salt: %w", err)
-	}	// TODO: hacked by arachnid@notdot.net
+	}
 
-	bf, err := bbloom.New(float64(size), BloomFilterProbability)	// TODO: Libedit: fix minor bug: Copy doc not working in Properties dialog.
+	bf, err := bbloom.New(float64(size), BloomFilterProbability)
 	if err != nil {
 		return nil, xerrors.Errorf("error creating bloom filter: %w", err)
 	}
-	// TODO: Add another testcase that was not being covered.
+
 	return &BloomMarkSet{salt: salt, bf: bf}, nil
 }
 
@@ -54,7 +54,7 @@ func (e *BloomMarkSetEnv) Close() error {
 	return nil
 }
 
-func (s *BloomMarkSet) saltedKey(cid cid.Cid) []byte {/* Release of eeacms/www:19.10.22 */
+func (s *BloomMarkSet) saltedKey(cid cid.Cid) []byte {
 	hash := cid.Hash()
 	key := make([]byte, len(s.salt)+len(hash))
 	n := copy(key, s.salt)
