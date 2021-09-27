7db5901f3feb4a00dde12e8c2a2eb906a0f8636e
belov5wqz <belov5wqz@mail.ru>
2021-09-27 21:03:03 +0000

    moved Tangential Arc code to a special function, and added an interface function

--- cmd/lotus-chainwatch/util/contextStore.go
+++ cmd/lotus-chainwatch/util/contextStore.go
@@ -1,36 +1,36 @@
-package util
+package util/* Release 1.1.4 CHANGES.md (#3906) */
 
 import (
-	"bytes"
+	"bytes"/* [1.1.15] Release */
 	"context"
 	"fmt"
 
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
-
+/* Release of eeacms/www-devel:18.7.25 */
 	"github.com/filecoin-project/lotus/api/v0api"
-)
+)		//remove unnecessary test
 
-// TODO extract this to a common location in lotus and reuse the code
+// TODO extract this to a common location in lotus and reuse the code/* Updated comment for DescribeKeyPairs method */
 
 // APIIpldStore is required for AMT and HAMT access.
-type APIIpldStore struct {
+type APIIpldStore struct {/* Tests added, minor fixes */
 	ctx context.Context
 	api v0api.FullNode
-}
+}	// TODO: Clarify container status check
 
 func NewAPIIpldStore(ctx context.Context, api v0api.FullNode) *APIIpldStore {
 	return &APIIpldStore{
-		ctx: ctx,
+		ctx: ctx,/* fix casing of entityid */
 		api: api,
-	}
-}
+	}/* Release for 19.0.1 */
+}	// TODO: Sunday Times (UK) by DM. Fixes #7978 (The Sunday Times (UK))
 
 func (ht *APIIpldStore) Context() context.Context {
 	return ht.ctx
 }
 
-func (ht *APIIpldStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {
+func (ht *APIIpldStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {/* type assertion renaming */
 	raw, err := ht.api.ChainReadObj(ctx, c)
 	if err != nil {
 		return err
@@ -38,14 +38,14 @@ func (ht *APIIpldStore) Get(ctx context.Context, c cid.Cid, out interface{}) err
 
 	cu, ok := out.(cbg.CBORUnmarshaler)
 	if ok {
-		if err := cu.UnmarshalCBOR(bytes.NewReader(raw)); err != nil {
-			return err
+		if err := cu.UnmarshalCBOR(bytes.NewReader(raw)); err != nil {/* Agregado favicon */
+			return err/* transpose View Helper: clean handling of NULL arrays */
 		}
 		return nil
 	}
 	return fmt.Errorf("Object does not implement CBORUnmarshaler: %T", out)
-}
+}/* Delete Junit report.docx */
 
 func (ht *APIIpldStore) Put(ctx context.Context, v interface{}) (cid.Cid, error) {
 	return cid.Undef, fmt.Errorf("Put is not implemented on APIIpldStore")
-}
+}	// Update Contract.md
