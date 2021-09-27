aba4278e80c488e8d638b9ba800d1926d527218f
belov5wqz <belov5wqz@mail.ru>
2021-09-27 08:18:50 +0000

    Fix Mystic skills double-casting at high ping

--- build/params_debug.go
+++ build/params_debug.go
@@ -1,10 +1,10 @@
 // +build debug
-/* Update Release Information */
+/* Delete lambda_test.txt */
 package build
 
 func init() {
 	InsecurePoStValidation = true
 	BuildType |= BuildDebug
-}/* added Cycles and Cycles Delta columns */
+}
 
 // NOTE: Also includes settings from params_2k
