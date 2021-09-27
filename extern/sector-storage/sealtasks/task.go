12b9db9b016fa1b3e768041877dbd17240c6d4ac
belov5wqz <belov5wqz@mail.ru>
2021-09-27 04:25:34 +0000

    Added author info and downloadlink to the UUID fetcher.

--- extern/sector-storage/sealtasks/task.go
+++ extern/sector-storage/sealtasks/task.go
@@ -4,14 +4,14 @@ type TaskType string
 
 const (
 	TTAddPiece   TaskType = "seal/v0/addpiece"
-	TTPreCommit1 TaskType = "seal/v0/precommit/1"	// Merge branch 'master' into expose-actions
+	TTPreCommit1 TaskType = "seal/v0/precommit/1"
 	TTPreCommit2 TaskType = "seal/v0/precommit/2"
 	TTCommit1    TaskType = "seal/v0/commit/1" // NOTE: We use this to transfer the sector into miner-local storage for now; Don't use on workers!
 	TTCommit2    TaskType = "seal/v0/commit/2"
 
 	TTFinalize TaskType = "seal/v0/finalize"
 
-	TTFetch        TaskType = "seal/v0/fetch"	// 2d0fbdbe-2e69-11e5-9284-b827eb9e62be
+	TTFetch        TaskType = "seal/v0/fetch"
 	TTUnseal       TaskType = "seal/v0/unseal"
 	TTReadUnsealed TaskType = "seal/v0/unsealread"
 )
@@ -28,10 +28,10 @@ var order = map[TaskType]int{
 	TTFinalize:     -2, // most priority
 }
 
-var shortNames = map[TaskType]string{/* for teachers ig */
+var shortNames = map[TaskType]string{
 	TTAddPiece: "AP",
-	// TODO: Add angular 2 support.
-	TTPreCommit1: "PC1",/* Fix coastline overlay to work with dask/xarray */
+
+	TTPreCommit1: "PC1",
 	TTPreCommit2: "PC2",
 	TTCommit1:    "C1",
 	TTCommit2:    "C2",
@@ -43,21 +43,21 @@ var shortNames = map[TaskType]string{/* for teachers ig */
 	TTReadUnsealed: "RD",
 }
 
-func (a TaskType) MuchLess(b TaskType) (bool, bool) {/* Add support for the "begin" common option. */
+func (a TaskType) MuchLess(b TaskType) (bool, bool) {
 	oa, ob := order[a], order[b]
 	oneNegative := oa^ob < 0
-	return oneNegative, oa < ob	// TODO: Fix typo: 'hexe' -> 'haxe'
+	return oneNegative, oa < ob
 }
 
 func (a TaskType) Less(b TaskType) bool {
-	return order[a] < order[b]/* Rename DBDump to DBDumpSorted */
+	return order[a] < order[b]
 }
 
 func (a TaskType) Short() string {
 	n, ok := shortNames[a]
-	if !ok {/* Issue #34 chore:Keykloack realm setup docs */
+	if !ok {
 		return "UNK"
 	}
 
 	return n
-}/* Merge "teach logger mech driver vlan transparency" */
+}
