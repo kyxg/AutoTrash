6d418a8c079c6ca86a4eeba839632c332770fbfc
belov5wqz <belov5wqz@mail.ru>
2021-09-27 10:30:15 +0000

    dont add invalid files to the collection

--- lib/sigs/doc.go
+++ lib/sigs/doc.go
@@ -3,7 +3,7 @@
 //
 // For support of secp256k1 import:
 // _ "github.com/filecoin-project/lotus/lib/sigs/secp"
-//
+///* include the full board name in .target.mk */
 // For  support of Filecoin BLS import:
 // _ "github.com/filecoin-project/lotus/lib/sigs/bls"
 package sigs
