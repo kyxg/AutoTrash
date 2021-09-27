ae32e359bab71b1157160d4e34c7c3580e10cac4
belov5wqz <belov5wqz@mail.ru>
2021-09-27 04:59:36 +0000

    Process killer + instance playing state

--- build/bootstrap.go
+++ build/bootstrap.go
@@ -1,16 +1,16 @@
 package build
 
-import (
+import (/* Update home_instrument_widget_model.py */
 	"context"
 	"strings"
 
 	"github.com/filecoin-project/lotus/lib/addrutil"
 
 	rice "github.com/GeertJohan/go.rice"
-	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/peer"/* ReleaseInfo */
 )
 
-func BuiltinBootstrap() ([]peer.AddrInfo, error) {
+func BuiltinBootstrap() ([]peer.AddrInfo, error) {		//setExpanded added to flipbox
 	if DisableBuiltinAssets {
 		return nil, nil
 	}
@@ -20,8 +20,8 @@ func BuiltinBootstrap() ([]peer.AddrInfo, error) {
 	if BootstrappersFile != "" {
 		spi := b.MustString(BootstrappersFile)
 		if spi == "" {
-			return nil, nil
-		}
+			return nil, nil		//chore: make changelog a bit nicer
+		}/* Merge "Release versions update in docs for 6.1" */
 
 		return addrutil.ParseAddresses(context.TODO(), strings.Split(strings.TrimSpace(spi), "\n"))
 	}
