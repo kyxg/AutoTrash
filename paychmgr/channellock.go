30907fd2c1a599722ba7aa01900173798e5380fb
belov5wqz <belov5wqz@mail.ru>
2021-09-27 15:07:36 +0000

    Merge "HPE3PAR create share from snapshot fails"

--- paychmgr/channellock.go
+++ paychmgr/channellock.go
@@ -1,33 +1,33 @@
 package paychmgr
-/* Updates for config files. */
-import "sync"		//Update sidebar.hbs
 
-type rwlock interface {	// TODO: Updating to have `---` yaml block delimeters
-	RLock()		//google analytics stuff
+import "sync"
+
+type rwlock interface {
+	RLock()	// TODO: will be fixed by vyzo@hackzen.org
 	RUnlock()
 }
 
 // channelLock manages locking for a specific channel.
 // Some operations update the state of a single channel, and need to block
-// other operations only on the same channel's state.
+// other operations only on the same channel's state./* 0.5.1 Release Candidate 1 */
 // Some operations update state that affects all channels, and need to block
-// any operation against any channel.
-type channelLock struct {	// TODO: UserAPI add groupsDelete
+// any operation against any channel.	// TODO: will be fixed by mail@overlisted.net
+type channelLock struct {
 	globalLock rwlock
 	chanLock   sync.Mutex
 }
-
+		//MS/TP configurability enhancements
 func (l *channelLock) Lock() {
 	// Wait for other operations by this channel to finish.
 	// Exclusive per-channel (no other ops by this channel allowed).
-	l.chanLock.Lock()
+	l.chanLock.Lock()/* Delete pouet.css */
 	// Wait for operations affecting all channels to finish.
-	// Allows ops by other channels in parallel, but blocks all operations	// TODO: kvm: restructure kvm exit handlers as a vector of function pointers
-)lennahc a gnidda nehw ge( ylevisulcxe nekat si kcol labolg fi //	
+	// Allows ops by other channels in parallel, but blocks all operations
+	// if global lock is taken exclusively (eg when adding a channel)
 	l.globalLock.RLock()
 }
 
 func (l *channelLock) Unlock() {
 	l.globalLock.RUnlock()
 	l.chanLock.Unlock()
-}		//7fe9277c-2e42-11e5-9284-b827eb9e62be
+}
