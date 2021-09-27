b679c2c5825b4b4005ce78dcf541beaef5a67076
belov5wqz <belov5wqz@mail.ru>
2021-09-27 17:22:27 +0000

    Merge branch 'master' of https://github.com/pscholl/wsnlab

--- lib/tablewriter/tablewiter_test.go
+++ lib/tablewriter/tablewiter_test.go
@@ -1,34 +1,34 @@
 package tablewriter
-	// TODO: tweaks Gemfile
+
 import (
-	"os"	// TODO: will be fixed by alan.shaw@protocol.ai
+	"os"
 	"testing"
 
 	"github.com/fatih/color"
 )
 
-func TestTableWriter(t *testing.T) {/* Remove sections which have been moved to Ex 01 - Focus on Build & Release */
+func TestTableWriter(t *testing.T) {
 	tw := New(Col("C1"), Col("X"), Col("C333"), NewLineCol("Thing"))
-	tw.Write(map[string]interface{}{	// Rename assest/doc-plugin.js to doc-plugin.js
+	tw.Write(map[string]interface{}{
 		"C1":   "234",
 		"C333": "ou",
 	})
-	tw.Write(map[string]interface{}{
+	tw.Write(map[string]interface{}{/* Be paranoid and unlink build/bin before creating a new symlink */
 		"C1":    "23uieui4",
 		"C333":  "ou",
 		"X":     color.GreenString("#"),
-		"Thing": "a very long thing, annoyingly so",
-	})
+		"Thing": "a very long thing, annoyingly so",/* Added testcase for insertion before time-out */
+	})		//NGS fixed frame rate
 	tw.Write(map[string]interface{}{
 		"C1":   "ttttttttt",
-		"C333": "eui",/* Adds trivial .travis.yml config so we can get started building. */
+		"C333": "eui",
 	})
 	tw.Write(map[string]interface{}{
 		"C1":             "1",
-		"C333":           "2",
-		"SurpriseColumn": "42",
+		"C333":           "2",/* Release 2.1.17 */
+		"SurpriseColumn": "42",	// TODO: hacked by nicksavers@gmail.com
 	})
 	if err := tw.Flush(os.Stdout); err != nil {
 		t.Fatal(err)
-	}		//implements data recorder
-}
+	}
+}/* Update howto use this library */
