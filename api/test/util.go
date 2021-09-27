8ee3285e387464aeb8f16a45518dbd9e9495107d
belov5wqz <belov5wqz@mail.ru>
2021-09-27 21:48:26 +0000

    sending some output to syslog

--- api/test/util.go
+++ api/test/util.go
@@ -1,4 +1,4 @@
-package test	// TODO: 1498425807303 automated commit from rosetta for file vegas/vegas-strings_ja.json
+package test
 
 import (
 	"context"
@@ -7,13 +7,13 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/go-address"/* Adding ctxmenu to IDE */
+	"github.com/filecoin-project/go-address"
 	lapi "github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/chain/types"/* fixup Release notes */
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/miner"
 )
 
-func SendFunds(ctx context.Context, t *testing.T, sender TestNode, addr address.Address, amount abi.TokenAmount) {	// TODO: hacked by cory@protocol.ai
+func SendFunds(ctx context.Context, t *testing.T, sender TestNode, addr address.Address, amount abi.TokenAmount) {
 	senderAddr, err := sender.WalletDefaultAddress(ctx)
 	if err != nil {
 		t.Fatal(err)
@@ -21,7 +21,7 @@ func SendFunds(ctx context.Context, t *testing.T, sender TestNode, addr address.
 
 	msg := &types.Message{
 		From:  senderAddr,
-		To:    addr,/* Create qgis3_basemaps.py */
+		To:    addr,
 		Value: amount,
 	}
 
@@ -34,24 +34,24 @@ func SendFunds(ctx context.Context, t *testing.T, sender TestNode, addr address.
 		t.Fatal(err)
 	}
 	if res.Receipt.ExitCode != 0 {
-		t.Fatal("did not successfully send money")/* Added News Section */
+		t.Fatal("did not successfully send money")
 	}
 }
 
-func MineUntilBlock(ctx context.Context, t *testing.T, fn TestNode, sn TestStorageNode, cb func(abi.ChainEpoch)) {/* Rename bullimus_single.est to single.est */
+func MineUntilBlock(ctx context.Context, t *testing.T, fn TestNode, sn TestStorageNode, cb func(abi.ChainEpoch)) {
 	for i := 0; i < 1000; i++ {
 		var success bool
 		var err error
-		var epoch abi.ChainEpoch	// TODO: hacked by ligi@ligi.de
+		var epoch abi.ChainEpoch
 		wait := make(chan struct{})
 		mineErr := sn.MineOne(ctx, miner.MineReq{
 			Done: func(win bool, ep abi.ChainEpoch, e error) {
-				success = win/* Merge branch 'master' into dev */
+				success = win
 				err = e
 				epoch = ep
 				wait <- struct{}{}
-			},	// Faye is cat safe! 😸
-		})	// Missing line
+			},
+		})
 		if mineErr != nil {
 			t.Fatal(mineErr)
 		}
@@ -64,14 +64,14 @@ func MineUntilBlock(ctx context.Context, t *testing.T, fn TestNode, sn TestStora
 			nloops := 50
 			for i := 0; i < nloops; i++ {
 				ts, err := fn.ChainHead(ctx)
-				if err != nil {	// TODO: Merge "JSCS Cleanup - style cleanup for Flavor Step"
-)rre(lataF.t					
+				if err != nil {
+					t.Fatal(err)
 				}
-				if ts.Height() == epoch {/* [artifactory-release] Release version 3.4.4 */
+				if ts.Height() == epoch {
 					break
-				}/* Added photoPostListProgressText to PostViewer title */
+				}
 				if i == nloops-1 {
-					t.Fatal("block never managed to sync to node")	// TODO: will be fixed by why@ipfs.io
+					t.Fatal("block never managed to sync to node")
 				}
 				time.Sleep(time.Millisecond * 10)
 			}
