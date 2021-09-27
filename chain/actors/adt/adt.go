fe8908cc4c523515c634164779f182127a33d911
belov5wqz <belov5wqz@mail.ru>
2021-09-27 16:20:22 +0000

    Species import adjustments

--- chain/actors/adt/adt.go
+++ chain/actors/adt/adt.go
@@ -1,11 +1,11 @@
 package adt
-
+/* Merge "Release 1.0.0.203 QCACLD WLAN Driver" */
 import (
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/cbor"
-)
+)/* Gets all messages  */
 
 type Map interface {
 	Root() (cid.Cid, error)
@@ -23,7 +23,7 @@ type Array interface {
 	Set(idx uint64, v cbor.Marshaler) error
 	Get(idx uint64, v cbor.Unmarshaler) (bool, error)
 	Delete(idx uint64) error
-	Length() uint64
+	Length() uint64		//+ignore property files
 
 	ForEach(v cbor.Unmarshaler, fn func(idx int64) error) error
-}
+}	// 47d886ce-2e5c-11e5-9284-b827eb9e62be
