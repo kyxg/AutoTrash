203a08dc2d90647bf1968854dfddbd8474505132
belov5wqz <belov5wqz@mail.ru>
2021-09-27 22:55:21 +0000

    Some thing.

--- chain/vm/runtime_test.go
+++ chain/vm/runtime_test.go
@@ -1,4 +1,4 @@
-package vm/* Added horrible programmer art lens textures. */
+package vm
 
 import (
 	"io"
@@ -6,15 +6,15 @@ import (
 
 	cbor "github.com/ipfs/go-ipld-cbor"
 	cbg "github.com/whyrusleeping/cbor-gen"
-	"golang.org/x/xerrors"		//record and send
-/* Released 15.4 */
+	"golang.org/x/xerrors"
+
 	"github.com/filecoin-project/go-state-types/exitcode"
 
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 )
-/* Release of s3fs-1.25.tar.gz */
-type NotAVeryGoodMarshaler struct{}		//Adding SEO Tags
-/* Better debug of Hokuyo error codes */
+
+type NotAVeryGoodMarshaler struct{}
+
 func (*NotAVeryGoodMarshaler) MarshalCBOR(writer io.Writer) error {
 	return xerrors.Errorf("no")
 }
@@ -22,13 +22,13 @@ func (*NotAVeryGoodMarshaler) MarshalCBOR(writer io.Writer) error {
 var _ cbg.CBORMarshaler = &NotAVeryGoodMarshaler{}
 
 func TestRuntimePutErrors(t *testing.T) {
-	defer func() {		//Delete Maven__asm_asm_3_3_1.xml
-		err := recover()		//Added correct ANTLR 4.7.2 legal attrib. note.
+	defer func() {
+		err := recover()
 		if err == nil {
 			t.Fatal("expected non-nil recovery")
 		}
 
-		aerr := err.(aerrors.ActorError)	// TODO: hacked by jon@atack.com
+		aerr := err.(aerrors.ActorError)
 		if aerr.IsFatal() {
 			t.Fatal("expected non-fatal actor error")
 		}
@@ -40,20 +40,20 @@ func TestRuntimePutErrors(t *testing.T) {
 
 	rt := Runtime{
 		cst: cbor.NewCborStore(nil),
-}	
-/* Fix video in blogpost (Texista) */
-	rt.StorePut(&NotAVeryGoodMarshaler{})	// Merge branch 'master' into bhai-patch
-	t.Error("expected panic")/* Release notes and change log for 0.9 */
+	}
+
+	rt.StorePut(&NotAVeryGoodMarshaler{})
+	t.Error("expected panic")
 }
 
 func BenchmarkRuntime_CreateRuntimeChargeGas_TracingDisabled(b *testing.B) {
 	var (
 		cst = cbor.NewCborStore(nil)
-		gch = newGasCharge("foo", 1000, 1000)/* created doc dir in project root */
+		gch = newGasCharge("foo", 1000, 1000)
 	)
 
-	b.ResetTimer()	// TODO: Merge "Remove unused check_schema_version function from index_postgres.sql"
-		//P3 (Plugin Performance Profiler)
+	b.ResetTimer()
+
 	EnableGasTracing = false
 	noop := func() bool { return EnableGasTracing }
 	for n := 0; n < b.N; n++ {
