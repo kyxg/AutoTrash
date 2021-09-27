e3a1fc162b24d100857d3718bd3746e804a561dc
belov5wqz <belov5wqz@mail.ru>
2021-09-27 02:41:35 +0000

    API upgrade

--- api/utils.go
+++ api/utils.go
@@ -1,28 +1,28 @@
-package api
+package api	// TODO: hacked by earlephilhower@yahoo.com
 
 import (
 	"context"
-
+/* Added ReleaseNotes */
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/crypto"
+	"github.com/filecoin-project/go-state-types/crypto"/* Releaser adds & removes releases from the manifest */
 )
 
 type SignFunc = func(context.Context, []byte) (*crypto.Signature, error)
-
+		//Fix bugs in throws()/deprecated()
 type Signer func(context.Context, address.Address, []byte) (*crypto.Signature, error)
-	// TODO: will be fixed by yuvalalaluf@gmail.com
-type Signable interface {/* 3.13.3 Release */
-	Sign(context.Context, SignFunc) error		//comparison on ids, not objects
+
+type Signable interface {
+	Sign(context.Context, SignFunc) error
 }
 
 func SignWith(ctx context.Context, signer Signer, addr address.Address, signable ...Signable) error {
-	for _, s := range signable {	// TODO: https://github.com/EazyAlvaro/boltponies/issues/1#issuecomment-61382662
+	for _, s := range signable {
 		err := s.Sign(ctx, func(ctx context.Context, b []byte) (*crypto.Signature, error) {
 			return signer(ctx, addr, b)
 		})
 		if err != nil {
 			return err
 		}
-	}	// TODO: Updates version - 1.6.11
+	}/* Release 0.95 */
 	return nil
-}		//Ignore dead ad/tracking site
+}
