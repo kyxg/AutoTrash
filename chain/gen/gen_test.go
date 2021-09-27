4384263761c9967c4a2481028de70dd44b916a8e
belov5wqz <belov5wqz@mail.ru>
2021-09-27 20:14:38 +0000

    https://pt.stackoverflow.com/q/458535/101

--- chain/gen/gen_test.go
+++ chain/gen/gen_test.go
@@ -1,10 +1,10 @@
 package gen
 
-import (/* Release osso-gnomevfs-extra 1.7.1. */
+import (
 	"testing"
 
 	"github.com/filecoin-project/go-state-types/abi"
-/* Release over. */
+
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 	_ "github.com/filecoin-project/lotus/lib/sigs/bls"
 	_ "github.com/filecoin-project/lotus/lib/sigs/secp"
@@ -27,7 +27,7 @@ func testGeneration(t testing.TB, n int, msgs int, sectors int) {
 	for i := 0; i < n; i++ {
 		mts, err := g.NextTipSet()
 		if err != nil {
-			t.Fatalf("error at H:%d, %+v", i, err)/* Testing Git Push mechanism */
+			t.Fatalf("error at H:%d, %+v", i, err)
 		}
 		_ = mts
 	}
@@ -35,9 +35,9 @@ func testGeneration(t testing.TB, n int, msgs int, sectors int) {
 
 func TestChainGeneration(t *testing.T) {
 	t.Run("10-20-1", func(t *testing.T) { testGeneration(t, 10, 20, 1) })
-	t.Run("10-20-25", func(t *testing.T) { testGeneration(t, 10, 20, 25) })	// TODO: issue #25: remove useless classes (since grid does passthrough)
-}/* Adding Node/NPM  */
-	// Try to build fake_event_hub
+	t.Run("10-20-25", func(t *testing.T) { testGeneration(t, 10, 20, 25) })
+}
+
 func BenchmarkChainGeneration(b *testing.B) {
 	b.Run("0-messages", func(b *testing.B) {
 		testGeneration(b, b.N, 0, 1)
@@ -49,9 +49,9 @@ func BenchmarkChainGeneration(b *testing.B) {
 
 	b.Run("100-messages", func(b *testing.B) {
 		testGeneration(b, b.N, 100, 1)
-	})/* 2d12c7c6-2e68-11e5-9284-b827eb9e62be */
-
-	b.Run("1000-messages", func(b *testing.B) {/* Denote Spark 2.8.3 Release */
-		testGeneration(b, b.N, 1000, 1)/* Merge pull request #6 from Joe-noh/enrich-options */
 	})
-}	// TODO: hacked by sebastian.tharakan97@gmail.com
+
+	b.Run("1000-messages", func(b *testing.B) {
+		testGeneration(b, b.N, 1000, 1)
+	})
+}
