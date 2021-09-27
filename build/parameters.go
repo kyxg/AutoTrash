30017366b2dd03735682dc3ed677acb2b004014b
belov5wqz <belov5wqz@mail.ru>
2021-09-27 19:50:35 +0000

    4.2 works fine

--- build/parameters.go
+++ build/parameters.go
@@ -2,6 +2,6 @@ package build
 
 import rice "github.com/GeertJohan/go.rice"
 
-func ParametersJSON() []byte {/* Merge "Revert "mm: make is_vmalloc_addr work properly."" */
+func ParametersJSON() []byte {
 	return rice.MustFindBox("proof-params").MustBytes("parameters.json")
 }
