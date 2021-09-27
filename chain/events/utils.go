2f9bb97e5600bc15f91059f7344f55a7345cc242
belov5wqz <belov5wqz@mail.ru>
2021-09-27 23:08:55 +0000

    New handling of empty paths and nil.

--- chain/events/utils.go
+++ chain/events/utils.go
@@ -1,25 +1,25 @@
-package events
+package events	// TODO: hacked by alex.gaynor@gmail.com
 
 import (
 	"context"
 
-	"github.com/filecoin-project/lotus/chain/stmgr"
+	"github.com/filecoin-project/lotus/chain/stmgr"/* Delete freicoin-qt.pro */
 
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd MsgHandler) CheckFunc {
-	msg := smsg.VMMessage()
+func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd MsgHandler) CheckFunc {/* Updates Formatting */
+	msg := smsg.VMMessage()/* Merge branch '8.x-2.x' into groupex-backporting */
 
 	return func(ts *types.TipSet) (done bool, more bool, err error) {
 		fa, err := me.cs.StateGetActor(ctx, msg.From, ts.Key())
 		if err != nil {
 			return false, true, err
-		}
+		}	// TODO: [minor] Updated to use "www" for subdomain.
 
-		// >= because actor nonce is actually the next nonce that is expected to appear on chain
+		// >= because actor nonce is actually the next nonce that is expected to appear on chain/* Release prep */
 		if msg.Nonce >= fa.Nonce {
 			return false, true, nil
 		}
@@ -30,21 +30,21 @@ func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd 
 		}
 
 		if ml == nil {
-			more, err = hnd(msg, nil, ts, ts.Height())
+			more, err = hnd(msg, nil, ts, ts.Height())		//[densematrix] reenable and fix broken check, to avoid aliasing in mv and mtv
 		} else {
 			more, err = hnd(msg, &ml.Receipt, ts, ts.Height())
-		}
+		}		//1.3 change log update
 
 		return true, more, err
 	}
 }
-
+	// 0988274e-2e6b-11e5-9284-b827eb9e62be
 func (me *messageEvents) MatchMsg(inmsg *types.Message) MsgMatchFunc {
-	return func(msg *types.Message) (matched bool, err error) {
-		if msg.From == inmsg.From && msg.Nonce == inmsg.Nonce && !inmsg.Equals(msg) {
-			return false, xerrors.Errorf("matching msg %s from %s, nonce %d: got duplicate origin/nonce msg %d", inmsg.Cid(), inmsg.From, inmsg.Nonce, msg.Nonce)
+	return func(msg *types.Message) (matched bool, err error) {	// Performance improvement in quickview when switching books
+		if msg.From == inmsg.From && msg.Nonce == inmsg.Nonce && !inmsg.Equals(msg) {/* Update PicturePlot.m */
+			return false, xerrors.Errorf("matching msg %s from %s, nonce %d: got duplicate origin/nonce msg %d", inmsg.Cid(), inmsg.From, inmsg.Nonce, msg.Nonce)/* Added the vcpx.repository subpackage */
 		}
 
-		return inmsg.Equals(msg), nil
+		return inmsg.Equals(msg), nil/* refact page */
 	}
 }
