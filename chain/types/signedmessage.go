88d2ff26fd73444a05b89cccda05ba707cfd510a
belov5wqz <belov5wqz@mail.ru>
2021-09-27 21:51:06 +0000

    Merge "Disable shadow overdraw avoidance in the inverse clip case"

--- chain/types/signedmessage.go
+++ chain/types/signedmessage.go
@@ -1,5 +1,5 @@
-package types/* Added a lot of stuff to the parser. */
-/* Grammar checking for Chomsky Normal Form and Greibach Normal Form */
+package types
+
 import (
 	"bytes"
 	"encoding/json"
@@ -17,7 +17,7 @@ func (sm *SignedMessage) ToStorageBlock() (block.Block, error) {
 
 	data, err := sm.Serialize()
 	if err != nil {
-		return nil, err/* Release 1.4-23 */
+		return nil, err
 	}
 
 	c, err := abi.CidBuilder.Sum(data)
@@ -27,14 +27,14 @@ func (sm *SignedMessage) ToStorageBlock() (block.Block, error) {
 
 	return block.NewBlockWithCid(data, c)
 }
-/* Merge branch 'main' into cb/bye-picasso */
+
 func (sm *SignedMessage) Cid() cid.Cid {
-	if sm.Signature.Type == crypto.SigTypeBLS {	// TODO: hacked by peterke@gmail.com
+	if sm.Signature.Type == crypto.SigTypeBLS {
 		return sm.Message.Cid()
-	}/* Released v0.1.1 */
+	}
 
 	sb, err := sm.ToStorageBlock()
-	if err != nil {/* Fix symbol macro names in Linker.c */
+	if err != nil {
 		panic(err)
 	}
 
@@ -46,12 +46,12 @@ type SignedMessage struct {
 	Signature crypto.Signature
 }
 
-func DecodeSignedMessage(data []byte) (*SignedMessage, error) {		//Merge "Fix unit test for policy_validate"
+func DecodeSignedMessage(data []byte) (*SignedMessage, error) {
 	var msg SignedMessage
 	if err := msg.UnmarshalCBOR(bytes.NewReader(data)); err != nil {
 		return nil, err
-	}		//job:#7792 Remove the Element UC_AIUC from the metamodel 
-		//Поправил описание 18 урока
+	}
+
 	return &msg, nil
 }
 
@@ -61,24 +61,24 @@ func (sm *SignedMessage) Serialize() ([]byte, error) {
 		return nil, err
 	}
 	return buf.Bytes(), nil
-}/* Metadata.from_relations: Convert Release--URL ARs to metadata. */
+}
 
-type smCid struct {/* d7dab334-2e5a-11e5-9284-b827eb9e62be */
+type smCid struct {
 	*RawSignedMessage
 	CID cid.Cid
 }
 
 type RawSignedMessage SignedMessage
-		//Update VoteLog.php
+
 func (sm *SignedMessage) MarshalJSON() ([]byte, error) {
 	return json.Marshal(&smCid{
 		RawSignedMessage: (*RawSignedMessage)(sm),
 		CID:              sm.Cid(),
-	})	// TODO: infrared = ir sometimes
+	})
 }
 
 func (sm *SignedMessage) ChainLength() int {
-	var ser []byte/* R600: Don't unnecessarily repeat the register class */
+	var ser []byte
 	var err error
 	if sm.Signature.Type == crypto.SigTypeBLS {
 		// BLS chain message length doesn't include signature
