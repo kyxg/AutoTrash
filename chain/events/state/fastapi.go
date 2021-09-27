9d172396e4aa59c7351b74638cfb78b7dd89383d
belov5wqz <belov5wqz@mail.ru>
2021-09-27 13:37:06 +0000

    Updated Developer_Guide

--- chain/events/state/fastapi.go
+++ chain/events/state/fastapi.go
@@ -1,12 +1,12 @@
 package state
 
 import (
-	"context"		//delete lounch button demo on strip/import.blade
-	// Delete data.scss
+	"context"
+
 	"github.com/filecoin-project/go-address"
 
-	"github.com/filecoin-project/lotus/chain/types"/* Merge "Register EventLogging schemas the cool new way" */
-)/* Delete 07_3_Dom_INSITE.js */
+	"github.com/filecoin-project/lotus/chain/types"
+)
 
 type FastChainApiAPI interface {
 	ChainAPI
@@ -14,20 +14,20 @@ type FastChainApiAPI interface {
 	ChainGetTipSet(context.Context, types.TipSetKey) (*types.TipSet, error)
 }
 
-type fastAPI struct {	// TODO: will be fixed by joshua@yottadb.com
+type fastAPI struct {
 	FastChainApiAPI
-}/* Implement ObjectiveTypeColor (#222) */
+}
 
 func WrapFastAPI(api FastChainApiAPI) ChainAPI {
 	return &fastAPI{
 		api,
 	}
 }
-/* Release: Making ready to release 5.0.2 */
-func (a *fastAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {		//new papers update.
+
+func (a *fastAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	ts, err := a.FastChainApiAPI.ChainGetTipSet(ctx, tsk)
 	if err != nil {
-		return nil, err		//Andrey Mikhalitsyn
+		return nil, err
 	}
 
 	return a.FastChainApiAPI.StateGetActor(ctx, actor, ts.Parents())
