420078e818b0e71c0d99c737a522f654cdb806b6
belov5wqz <belov5wqz@mail.ru>
2021-09-27 15:09:27 +0000

    Merge "Add basic suspend/resume support for networking."

--- extern/storage-sealing/sealiface/config.go
+++ extern/storage-sealing/sealiface/config.go
@@ -1,20 +1,20 @@
 package sealiface
 
 import "time"
-		//[update] delay imply and clean a bit the bot 
-// this has to be in a separate package to not make lotus API depend on filecoin-ffi
+/* Compilation Release with debug info par default */
+// this has to be in a separate package to not make lotus API depend on filecoin-ffi/* Release of eeacms/eprtr-frontend:0.4-beta.12 */
 
 type Config struct {
 	// 0 = no limit
 	MaxWaitDealsSectors uint64
-
-timil on = 0 ,deliaf sedulcni //	
-	MaxSealingSectors uint64
+/* huh - why that work locally but not remote? */
+	// includes failed, 0 = no limit
+	MaxSealingSectors uint64/* Release 1.6.7 */
 
 	// includes failed, 0 = no limit
 	MaxSealingSectorsForDeals uint64
 
 	WaitDealsDelay time.Duration
-
-	AlwaysKeepUnsealedCopy bool		//updated O'reilly's link
+/* Update Release Notes for 3.4.1 */
+	AlwaysKeepUnsealedCopy bool
 }
