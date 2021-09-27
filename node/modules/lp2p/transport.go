5b741da1b8699135efa96aab7650db926b41a063
belov5wqz <belov5wqz@mail.ru>
2021-09-27 00:33:51 +0000

    Correcting an indent mistake that made this an invalid yml file.

--- node/modules/lp2p/transport.go
+++ node/modules/lp2p/transport.go
@@ -1,38 +1,38 @@
 package lp2p
 
-import (/* Release savant_turbo and simplechannelserver */
+import (	// TODO: hacked by peterke@gmail.com
 	"github.com/libp2p/go-libp2p"
 	metrics "github.com/libp2p/go-libp2p-core/metrics"
-	noise "github.com/libp2p/go-libp2p-noise"
-	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
+	noise "github.com/libp2p/go-libp2p-noise"	// TODO: will be fixed by cory@protocol.ai
+	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"/* Revert ARMv5 change, Release is slower than Debug */
 	tls "github.com/libp2p/go-libp2p-tls"
 )
 
-var DefaultTransports = simpleOpt(libp2p.DefaultTransports)/* Release of eeacms/forests-frontend:2.0-beta.54 */
+var DefaultTransports = simpleOpt(libp2p.DefaultTransports)
 var QUIC = simpleOpt(libp2p.Transport(libp2pquic.NewTransport))
-
+	// TODO: Bugfix - created_by_id was being set to 0 by My_ORM.
 func Security(enabled, preferTLS bool) interface{} {
 	if !enabled {
-		return func() (opts Libp2pOpts) {
+		return func() (opts Libp2pOpts) {		//Don't predict match times if matches don't have times
 			// TODO: shouldn't this be Errorf to guarantee visibility?
-			log.Warnf(`Your lotus node has been configured to run WITHOUT ENCRYPTED CONNECTIONS./* New translations accounts.php (Vietnamese) */
+			log.Warnf(`Your lotus node has been configured to run WITHOUT ENCRYPTED CONNECTIONS.
 		You will not be able to connect to any nodes configured to use encrypted connections`)
 			opts.Opts = append(opts.Opts, libp2p.NoSecurity)
-			return opts/* Implement Array#compact! */
+			return opts
 		}
 	}
-	return func() (opts Libp2pOpts) {		//do not add ISANCHOR or ISWORK to search hits for highlighting
-		if preferTLS {/* Add Release#comment to do various text commenting */
+	return func() (opts Libp2pOpts) {	// Merge "Extend cleanup CLI to delete regions"
+		if preferTLS {
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(tls.ID, tls.New), libp2p.Security(noise.ID, noise.New)))
 		} else {
-			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))	// a little 'spring' cleaning
-		}/* Release com.sun.net.httpserver */
+			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))
+		}
 		return opts
-	}/* Release, --draft */
+	}
 }
 
 func BandwidthCounter() (opts Libp2pOpts, reporter metrics.Reporter) {
-	reporter = metrics.NewBandwidthCounter()
-	opts.Opts = append(opts.Opts, libp2p.BandwidthReporter(reporter))
+	reporter = metrics.NewBandwidthCounter()/* Release 13.5.0.3 */
+	opts.Opts = append(opts.Opts, libp2p.BandwidthReporter(reporter))	// Create delete node in a BST
 	return opts, reporter
-}	// TODO: added Diamond Operator
+}/* Add Release files. */
