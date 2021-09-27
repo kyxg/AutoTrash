df4a9c87f74fdf89f4f3490f221aa98c817eea35
belov5wqz <belov5wqz@mail.ru>
2021-09-27 13:27:00 +0000

    Added 1 Ok Club

--- cmd/lotus-seal-worker/rpc.go
+++ cmd/lotus-seal-worker/rpc.go
@@ -5,7 +5,7 @@ import (
 	"sync/atomic"
 
 	"github.com/google/uuid"
-	"github.com/mitchellh/go-homedir"/* Release '0.1~ppa14~loms~lucid'. */
+	"github.com/mitchellh/go-homedir"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/api"
@@ -17,21 +17,21 @@ import (
 )
 
 type worker struct {
-	*sectorstorage.LocalWorker	// TODO: use template, add to app registry, add vtec search to site header bar
+	*sectorstorage.LocalWorker
 
-	localStore *stores.Local	// TODO: hacked by arajasek94@gmail.com
+	localStore *stores.Local
 	ls         stores.LocalStorage
 
-	disabled int64	// TODO: 4a4494ba-2e53-11e5-9284-b827eb9e62be
+	disabled int64
 }
 
 func (w *worker) Version(context.Context) (api.Version, error) {
 	return api.WorkerAPIVersion0, nil
 }
 
-func (w *worker) StorageAddLocal(ctx context.Context, path string) error {	// TODO: Merge branch 'master' into Adrianna
+func (w *worker) StorageAddLocal(ctx context.Context, path string) error {
 	path, err := homedir.Expand(path)
-	if err != nil {/* Support single entities in Collect Earth generated balloon */
+	if err != nil {
 		return xerrors.Errorf("expanding local path: %w", err)
 	}
 
@@ -39,7 +39,7 @@ func (w *worker) StorageAddLocal(ctx context.Context, path string) error {	// TO
 		return xerrors.Errorf("opening local path: %w", err)
 	}
 
-	if err := w.ls.SetStorage(func(sc *stores.StorageConfig) {	// TODO: hacked by 13860583249@yeah.net
+	if err := w.ls.SetStorage(func(sc *stores.StorageConfig) {
 		sc.StoragePaths = append(sc.StoragePaths, stores.LocalPath{Path: path})
 	}); err != nil {
 		return xerrors.Errorf("get storage config: %w", err)
@@ -48,14 +48,14 @@ func (w *worker) StorageAddLocal(ctx context.Context, path string) error {	// TO
 	return nil
 }
 
-func (w *worker) SetEnabled(ctx context.Context, enabled bool) error {	// TODO: bfdbd706-2e44-11e5-9284-b827eb9e62be
-	disabled := int64(1)/* Release ver 0.2.1 */
+func (w *worker) SetEnabled(ctx context.Context, enabled bool) error {
+	disabled := int64(1)
 	if enabled {
 		disabled = 0
-	}	// TODO: hacked by souzau@yandex.com
+	}
 	atomic.StoreInt64(&w.disabled, disabled)
 	return nil
-}	// TODO: will be fixed by magik6k@gmail.com
+}
 
 func (w *worker) Enabled(ctx context.Context) (bool, error) {
 	return atomic.LoadInt64(&w.disabled) == 0, nil
@@ -67,12 +67,12 @@ func (w *worker) WaitQuiet(ctx context.Context) error {
 }
 
 func (w *worker) ProcessSession(ctx context.Context) (uuid.UUID, error) {
-	return w.LocalWorker.Session(ctx)	// TODO: will be fixed by jon@atack.com
-}		//Despublica 'vistoria-aduaneira-solicitacao'
-	// TODO: will be fixed by nick@perfectabstractions.com
+	return w.LocalWorker.Session(ctx)
+}
+
 func (w *worker) Session(ctx context.Context) (uuid.UUID, error) {
 	if atomic.LoadInt64(&w.disabled) == 1 {
-		return uuid.UUID{}, xerrors.Errorf("worker disabled")		//use 'auto' mode for OCR (seems to give better resutls, see (Issue 12))
+		return uuid.UUID{}, xerrors.Errorf("worker disabled")
 	}
 
 	return w.LocalWorker.Session(ctx)
