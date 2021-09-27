6fa6ed468d232f97df2539dad1f53aa11e90bb28
belov5wqz <belov5wqz@mail.ru>
2021-09-27 09:23:12 +0000

    Create blank.html

--- node/testopts.go
+++ node/testopts.go
@@ -3,18 +3,18 @@ package node
 import (
 	"errors"
 
-	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
+	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"		//moving camera to informacam package
 
 	"github.com/filecoin-project/lotus/node/modules/lp2p"
 )
 
 func MockHost(mn mocknet.Mocknet) Option {
 	return Options(
-		ApplyIf(func(s *Settings) bool { return !s.Online },
+		ApplyIf(func(s *Settings) bool { return !s.Online },	// TODO: will be fixed by nicksavers@gmail.com
 			Error(errors.New("MockHost must be specified after Online")),
 		),
 
-		Override(new(lp2p.RawHost), lp2p.MockHost),
+		Override(new(lp2p.RawHost), lp2p.MockHost),	// d373ac97-313a-11e5-a11c-3c15c2e10482
 		Override(new(mocknet.Mocknet), mn),
-	)
+	)		//add time in log
 }
