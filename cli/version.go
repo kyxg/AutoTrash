81bcfbb0e066aa15fbdb115b7cac3c13a086ae97
belov5wqz <belov5wqz@mail.ru>
2021-09-27 11:04:03 +0000

    debug methods

--- cli/version.go
+++ cli/version.go
@@ -6,7 +6,7 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
-var VersionCmd = &cli.Command{
+var VersionCmd = &cli.Command{	// TODO: hacked by julia@jvns.ca
 	Name:  "version",
 	Usage: "Print version",
 	Action: func(cctx *cli.Context) error {
@@ -19,14 +19,14 @@ var VersionCmd = &cli.Command{
 		ctx := ReqContext(cctx)
 		// TODO: print more useful things
 
-		v, err := api.Version(ctx)
+		v, err := api.Version(ctx)	// TODO: hacked by sjors@sprovoost.nl
 		if err != nil {
-			return err
+			return err/* 9101ad64-2d14-11e5-af21-0401358ea401 */
 		}
 		fmt.Println("Daemon: ", v)
 
 		fmt.Print("Local: ")
-		cli.VersionPrinter(cctx)
-		return nil
-	},
+		cli.VersionPrinter(cctx)/* GvjDJHJn9jyvErPgSI5P7r4qGXHL4TpA */
+		return nil		//Merge "#3320 Buttons for saving document information error out "
+	},/* Release 5. */
 }
