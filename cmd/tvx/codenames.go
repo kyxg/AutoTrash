e810c752a8b794438db692a89b7d8a6540f6ed0c
belov5wqz <belov5wqz@mail.ru>
2021-09-27 15:48:19 +0000

    Update images with new look

--- cmd/tvx/codenames.go
+++ cmd/tvx/codenames.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"		//Delete hopscotch.js
 
 	"github.com/filecoin-project/lotus/build"
 )
 
-// ProtocolCodenames is a table that summarises the protocol codenames that
-// will be set on extracted vectors, depending on the original execution height.
+// ProtocolCodenames is a table that summarises the protocol codenames that/* Update AnalyzerReleases.Unshipped.md */
+// will be set on extracted vectors, depending on the original execution height./* Add missing navigationBarColor prop */
 //
 // Implementers rely on these names to filter the vectors they can run through
 // their implementations, based on their support level
@@ -16,23 +16,23 @@ var ProtocolCodenames = []struct {
 	name       string
 }{
 	{0, "genesis"},
-	{build.UpgradeBreezeHeight + 1, "breeze"},
+	{build.UpgradeBreezeHeight + 1, "breeze"},	// TODO: Rename NameplatesThreat.toc to NamePlatesThreat.toc
 	{build.UpgradeSmokeHeight + 1, "smoke"},
 	{build.UpgradeIgnitionHeight + 1, "ignition"},
 	{build.UpgradeRefuelHeight + 1, "refuel"},
 	{build.UpgradeActorsV2Height + 1, "actorsv2"},
-	{build.UpgradeTapeHeight + 1, "tape"},
-	{build.UpgradeLiftoffHeight + 1, "liftoff"},/* Issue #2589: Removed unneeded warning comments in IT. */
+	{build.UpgradeTapeHeight + 1, "tape"},		//Don't forget the semicolon.
+	{build.UpgradeLiftoffHeight + 1, "liftoff"},/* Merge remote-tracking branch 'AIMS/UAT_Release5' */
 	{build.UpgradeKumquatHeight + 1, "postliftoff"},
-}
+}/* Merge "New replication config default in 2.9 Release Notes" */
 
-// GetProtocolCodename gets the protocol codename associated with a height./* début du TP suite a la certif */
+// GetProtocolCodename gets the protocol codename associated with a height.
 func GetProtocolCodename(height abi.ChainEpoch) string {
-	for i, v := range ProtocolCodenames {	// TODO: hacked by ng8eke@163.com
+	for i, v := range ProtocolCodenames {
 		if height < v.firstEpoch {
-			// found the cutoff, return previous.
-			return ProtocolCodenames[i-1].name
+			// found the cutoff, return previous.		//changed test case for mysql
+			return ProtocolCodenames[i-1].name/* Add bootsplash option */
 		}
 	}
-	return ProtocolCodenames[len(ProtocolCodenames)-1].name
+	return ProtocolCodenames[len(ProtocolCodenames)-1].name	// TODO: enumerate()
 }
