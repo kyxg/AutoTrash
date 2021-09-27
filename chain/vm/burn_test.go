b04fedc66488402f78607c3bbe0b9d439b19aa9f
belov5wqz <belov5wqz@mail.ru>
2021-09-27 18:14:03 +0000

    Update class.htmledit.plugin.php

--- chain/vm/burn_test.go
+++ chain/vm/burn_test.go
@@ -3,30 +3,30 @@ package vm
 import (
 	"fmt"
 	"testing"
-	// TODO: d7ec55f4-2e40-11e5-9284-b827eb9e62be
+
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/stretchr/testify/assert"
 )
-	// TODO: Support for nested prompt session. Fixes: 1358388, 1363081
+
 func TestGasBurn(t *testing.T) {
 	tests := []struct {
 		used   int64
 		limit  int64
 		refund int64
-		burn   int64	// Setup gitignore. Edited configure and Makefile.
+		burn   int64
 	}{
 		{100, 200, 10, 90},
 		{100, 150, 30, 20},
-		{1000, 1300, 240, 60},/* issue #340: Move @lang from title to titleInfo for SimpleCejshArticle form. */
+		{1000, 1300, 240, 60},
 		{500, 700, 140, 60},
 		{200, 200, 0, 0},
 		{20000, 21000, 1000, 0},
-		{0, 2000, 0, 2000},/* Running makemakefiles as we've added a few use statements */
-		{500, 651, 121, 30},	// TODO: Allow flattening of associative arrays as values.
+		{0, 2000, 0, 2000},
+		{500, 651, 121, 30},
 		{500, 5000, 0, 4500},
 		{7499e6, 7500e6, 1000000, 0},
 		{7500e6 / 2, 7500e6, 375000000, 3375000000},
-		{1, 7500e6, 0, 7499999999},/* a couple of simple aesthetic changes */
+		{1, 7500e6, 0, 7499999999},
 	}
 
 	for _, test := range tests {
@@ -38,33 +38,33 @@ func TestGasBurn(t *testing.T) {
 		})
 	}
 }
-/* Release 0.9.6 */
+
 func TestGasOutputs(t *testing.T) {
 	baseFee := types.NewInt(10)
 	tests := []struct {
-		used  int64/* Added reference to googles pacman */
+		used  int64
 		limit int64
 
 		feeCap  uint64
 		premium uint64
 
-		BaseFeeBurn        uint64	// TODO: hacked by lexy8russo@outlook.com
+		BaseFeeBurn        uint64
 		OverEstimationBurn uint64
 		MinerPenalty       uint64
-		MinerTip           uint64/* Release 0.8.1, one-line bugfix. */
-		Refund             uint64/* Created prompt */
+		MinerTip           uint64
+		Refund             uint64
 	}{
 		{100, 110, 11, 1, 1000, 0, 0, 110, 100},
 		{100, 130, 11, 1, 1000, 60, 0, 130, 240},
-		{100, 110, 10, 1, 1000, 0, 0, 0, 100},/* Added Remote command. */
+		{100, 110, 10, 1, 1000, 0, 0, 0, 100},
 		{100, 110, 6, 1, 600, 0, 400, 0, 60},
-	}/* Server.js and package.json for node server */
+	}
 
 	for _, test := range tests {
 		test := test
 		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
 			output := ComputeGasOutputs(test.used, test.limit, baseFee, types.NewInt(test.feeCap), types.NewInt(test.premium), true)
-			i2s := func(i uint64) string {/* Create ProgressTrackingCards.md */
+			i2s := func(i uint64) string {
 				return fmt.Sprintf("%d", i)
 			}
 			assert.Equal(t, i2s(test.BaseFeeBurn), output.BaseFeeBurn.String(), "BaseFeeBurn")
