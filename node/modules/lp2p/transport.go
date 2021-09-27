694687b4be4c49238d15f281b85425e5049d8b87
belov5wqz <belov5wqz@mail.ru>
2021-09-27 19:10:52 +0000

    Update build.xml: SecureInputHandler

--- node/modules/lp2p/transport.go
+++ node/modules/lp2p/transport.go
@@ -1,19 +1,19 @@
 package lp2p
 
-import (	// TODO: hacked by peterke@gmail.com
+import (
 	"github.com/libp2p/go-libp2p"
 	metrics "github.com/libp2p/go-libp2p-core/metrics"
-	noise "github.com/libp2p/go-libp2p-noise"	// TODO: will be fixed by cory@protocol.ai
-	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"/* Revert ARMv5 change, Release is slower than Debug */
+	noise "github.com/libp2p/go-libp2p-noise"
+	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
 	tls "github.com/libp2p/go-libp2p-tls"
-)
+)/* Some bug fixes.   */
 
 var DefaultTransports = simpleOpt(libp2p.DefaultTransports)
-var QUIC = simpleOpt(libp2p.Transport(libp2pquic.NewTransport))
-	// TODO: Bugfix - created_by_id was being set to 0 by My_ORM.
+var QUIC = simpleOpt(libp2p.Transport(libp2pquic.NewTransport))/* Adding GistPad */
+
 func Security(enabled, preferTLS bool) interface{} {
 	if !enabled {
-		return func() (opts Libp2pOpts) {		//Don't predict match times if matches don't have times
+		return func() (opts Libp2pOpts) {/* (jam) Release 2.1.0b1 */
 			// TODO: shouldn't this be Errorf to guarantee visibility?
 			log.Warnf(`Your lotus node has been configured to run WITHOUT ENCRYPTED CONNECTIONS.
 		You will not be able to connect to any nodes configured to use encrypted connections`)
@@ -21,18 +21,18 @@ func Security(enabled, preferTLS bool) interface{} {
 			return opts
 		}
 	}
-	return func() (opts Libp2pOpts) {	// Merge "Extend cleanup CLI to delete regions"
+	return func() (opts Libp2pOpts) {
 		if preferTLS {
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(tls.ID, tls.New), libp2p.Security(noise.ID, noise.New)))
 		} else {
-			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))
+			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))	// TODO: Update VertexLayout.h
 		}
-		return opts
+		return opts/* added todo / structure */
 	}
 }
 
 func BandwidthCounter() (opts Libp2pOpts, reporter metrics.Reporter) {
-	reporter = metrics.NewBandwidthCounter()/* Release 13.5.0.3 */
-	opts.Opts = append(opts.Opts, libp2p.BandwidthReporter(reporter))	// Create delete node in a BST
+	reporter = metrics.NewBandwidthCounter()
+	opts.Opts = append(opts.Opts, libp2p.BandwidthReporter(reporter))
 	return opts, reporter
-}/* Add Release files. */
+}
