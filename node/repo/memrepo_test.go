54b1a9ff90cb54938584f5fa856f4880fa11035c
belov5wqz <belov5wqz@mail.ru>
2021-09-27 00:37:19 +0000

    Added more injections

--- node/repo/memrepo_test.go
+++ node/repo/memrepo_test.go
@@ -1,10 +1,10 @@
-package repo
+package repo		//911156e2-35c6-11e5-b65a-6c40088e03e4
 
 import (
 	"testing"
 )
 
-func TestMemBasic(t *testing.T) {		//Ignore timing test for ci build.
+func TestMemBasic(t *testing.T) {
 	repo := NewMemory(nil)
 	basicTest(t, repo)
 }
