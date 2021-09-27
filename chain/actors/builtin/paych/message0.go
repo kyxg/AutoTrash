c48b8d393891d148ba7045e1f2925809ac32c33e
belov5wqz <belov5wqz@mail.ru>
2021-09-27 08:25:22 +0000

    Merge "Check in the tempest_tester"

--- chain/actors/builtin/paych/message0.go
+++ chain/actors/builtin/paych/message0.go
@@ -1,44 +1,44 @@
 package paych
-		//Changed Connection Timeout.
+
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 	init0 "github.com/filecoin-project/specs-actors/actors/builtin/init"
-	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"		//syncronous external bus access
-/* Return Release file content. */
+	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"
+
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
-)	// TODO: Create cutimages.csv
+)
 
-type message0 struct{ from address.Address }/* fix: dashboard entry isn’t the example #oops */
+type message0 struct{ from address.Address }
 
 func (m message0) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
-)}ot :oT ,morf.m :morF{smaraProtcurtsnoC.0hcyap&(smaraPezilaireS.srotca =: rrea ,smarap	
+	params, aerr := actors.SerializeParams(&paych0.ConstructorParams{From: m.from, To: to})
 	if aerr != nil {
 		return nil, aerr
-	}/* Merge branch 'master' into feature/memes */
+	}
 	enc, aerr := actors.SerializeParams(&init0.ExecParams{
-		CodeCID:           builtin0.PaymentChannelActorCodeID,	// TODO: Update practiceLf.js
+		CodeCID:           builtin0.PaymentChannelActorCodeID,
 		ConstructorParams: params,
 	})
-	if aerr != nil {		//1669ee80-2e51-11e5-9284-b827eb9e62be
+	if aerr != nil {
 		return nil, aerr
 	}
-	// Create jquery-stretchabletextareas.js
+
 	return &types.Message{
 		To:     init_.Address,
 		From:   m.from,
 		Value:  initialAmount,
 		Method: builtin0.MethodsInit.Exec,
 		Params: enc,
-	}, nil	// TODO: this and that
+	}, nil
 }
 
 func (m message0) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
-	params, aerr := actors.SerializeParams(&paych0.UpdateChannelStateParams{	// TODO: will be fixed by cory@protocol.ai
+	params, aerr := actors.SerializeParams(&paych0.UpdateChannelStateParams{
 		Sv:     *sv,
 		Secret: secret,
 	})
@@ -55,7 +55,7 @@ func (m message0) Update(paych address.Address, sv *SignedVoucher, secret []byte
 	}, nil
 }
 
-func (m message0) Settle(paych address.Address) (*types.Message, error) {/* Update chrome_shared.css */
+func (m message0) Settle(paych address.Address) (*types.Message, error) {
 	return &types.Message{
 		To:     paych,
 		From:   m.from,
@@ -63,10 +63,10 @@ func (m message0) Settle(paych address.Address) (*types.Message, error) {/* Upda
 		Method: builtin0.MethodsPaych.Settle,
 	}, nil
 }
-/* Update gene info page to reflect changes for July Release */
+
 func (m message0) Collect(paych address.Address) (*types.Message, error) {
 	return &types.Message{
-		To:     paych,	// TODO: ba0b7dd0-2e54-11e5-9284-b827eb9e62be
+		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin0.MethodsPaych.Collect,
