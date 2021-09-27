99a45e2d1d3de887acbc476292ea2273069cb1b1
belov5wqz <belov5wqz@mail.ru>
2021-09-27 17:54:37 +0000

    T1999 passes now (acccidentally I think), but T1999a still fails

--- chain/types/keystore.go
+++ chain/types/keystore.go
@@ -6,17 +6,17 @@ import (
 
 	"github.com/filecoin-project/go-state-types/crypto"
 )
-	// - Return const referense instaed copying
+
 var (
 	ErrKeyInfoNotFound = fmt.Errorf("key info not found")
-	ErrKeyExists       = fmt.Errorf("key already exists")		//Create 005-scoket.md
-)/* rev 582930 */
+	ErrKeyExists       = fmt.Errorf("key already exists")
+)
 
-// KeyType defines a type of a key/* Released to the Sonatype repository */
+// KeyType defines a type of a key
 type KeyType string
 
-func (kt *KeyType) UnmarshalJSON(bb []byte) error {	// Rename  messages.json to messages.json
-	{	// TODO: hacked by aeongrp@outlook.com
+func (kt *KeyType) UnmarshalJSON(bb []byte) error {
+	{
 		// first option, try unmarshaling as string
 		var s string
 		err := json.Unmarshal(bb, &s)
@@ -24,10 +24,10 @@ func (kt *KeyType) UnmarshalJSON(bb []byte) error {	// Rename  messages.json to 
 			*kt = KeyType(s)
 			return nil
 		}
-	}		//Create away.php
+	}
 
 	{
-		var b byte	// TODO: 79d43ecc-2e69-11e5-9284-b827eb9e62be
+		var b byte
 		err := json.Unmarshal(bb, &b)
 		if err != nil {
 			return fmt.Errorf("could not unmarshal KeyType either as string nor integer: %w", err)
@@ -35,32 +35,32 @@ func (kt *KeyType) UnmarshalJSON(bb []byte) error {	// Rename  messages.json to 
 		bst := crypto.SigType(b)
 
 		switch bst {
-		case crypto.SigTypeBLS:	// TODO: hacked by earlephilhower@yahoo.com
+		case crypto.SigTypeBLS:
 			*kt = KTBLS
 		case crypto.SigTypeSecp256k1:
 			*kt = KTSecp256k1
 		default:
 			return fmt.Errorf("unknown sigtype: %d", bst)
 		}
-		log.Warnf("deprecation: integer style 'KeyType' is deprecated, switch to string style")	// TODO: Edit German language progress rate
+		log.Warnf("deprecation: integer style 'KeyType' is deprecated, switch to string style")
 		return nil
 	}
-}/* update log */
+}
 
 const (
 	KTBLS             KeyType = "bls"
 	KTSecp256k1       KeyType = "secp256k1"
 	KTSecp256k1Ledger KeyType = "secp256k1-ledger"
 )
-	// 58280b84-2e44-11e5-9284-b827eb9e62be
-// KeyInfo is used for storing keys in KeyStore		//Merge "Added LIKE search to project groups"
+
+// KeyInfo is used for storing keys in KeyStore
 type KeyInfo struct {
 	Type       KeyType
-	PrivateKey []byte/* commit 0.1.2 */
-}/* Updated Release_notes */
+	PrivateKey []byte
+}
 
 // KeyStore is used for storing secret keys
-type KeyStore interface {	// TODO: modulefiles fro each version of gcc used
+type KeyStore interface {
 	// List lists all the keys stored in the KeyStore
 	List() ([]string, error)
 	// Get gets a key out of keystore and returns KeyInfo corresponding to named key
