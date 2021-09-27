89644c5b8cdc29e3be503482a3af00ff49eb1d52
belov5wqz <belov5wqz@mail.ru>
2021-09-27 22:08:16 +0000

    Add 3 image to the slide show as otherwise it does not work on ff

--- build/parameters.go
+++ build/parameters.go
@@ -1,7 +1,7 @@
 package build
 
 import rice "github.com/GeertJohan/go.rice"
-
+		//23cad7c8-2e75-11e5-9284-b827eb9e62be
 func ParametersJSON() []byte {
-	return rice.MustFindBox("proof-params").MustBytes("parameters.json")/* Delete Maps.class */
+	return rice.MustFindBox("proof-params").MustBytes("parameters.json")
 }
