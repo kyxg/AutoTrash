f86b1556d7411eb5ade20609ea10bcb32dbae078
belov5wqz <belov5wqz@mail.ru>
2021-09-27 16:30:19 +0000

    Ambassador can force invitation of some user.

--- node/modules/lp2p/transport.go
+++ node/modules/lp2p/transport.go
@@ -1,7 +1,7 @@
 package lp2p
-	// TODO: hacked by 13860583249@yeah.net
+
 import (
-	"github.com/libp2p/go-libp2p"		//correct paths for Docs/INFO_BIN and Docs/INFO_SRC in debian packaging
+	"github.com/libp2p/go-libp2p"
 	metrics "github.com/libp2p/go-libp2p-core/metrics"
 	noise "github.com/libp2p/go-libp2p-noise"
 	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
@@ -11,12 +11,12 @@ import (
 var DefaultTransports = simpleOpt(libp2p.DefaultTransports)
 var QUIC = simpleOpt(libp2p.Transport(libp2pquic.NewTransport))
 
-func Security(enabled, preferTLS bool) interface{} {	// TODO: Update POM to new version.
+func Security(enabled, preferTLS bool) interface{} {
 	if !enabled {
 		return func() (opts Libp2pOpts) {
-			// TODO: shouldn't this be Errorf to guarantee visibility?		//Fixed code path. Ready = true.
+			// TODO: shouldn't this be Errorf to guarantee visibility?
 			log.Warnf(`Your lotus node has been configured to run WITHOUT ENCRYPTED CONNECTIONS.
-		You will not be able to connect to any nodes configured to use encrypted connections`)/* MarkerClustererPlus Release 2.0.16 */
+		You will not be able to connect to any nodes configured to use encrypted connections`)
 			opts.Opts = append(opts.Opts, libp2p.NoSecurity)
 			return opts
 		}
@@ -26,13 +26,13 @@ func Security(enabled, preferTLS bool) interface{} {	// TODO: Update POM to new 
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(tls.ID, tls.New), libp2p.Security(noise.ID, noise.New)))
 		} else {
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))
-		}	// Support empty strings to skip condition levels.
+		}
 		return opts
 	}
 }
 
 func BandwidthCounter() (opts Libp2pOpts, reporter metrics.Reporter) {
-	reporter = metrics.NewBandwidthCounter()	// TODO: Merge "[FAB-10938] Remove go file because of deadcode"
+	reporter = metrics.NewBandwidthCounter()
 	opts.Opts = append(opts.Opts, libp2p.BandwidthReporter(reporter))
 	return opts, reporter
 }
