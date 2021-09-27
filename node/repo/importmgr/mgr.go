d2655f1b67fb824cfc8a4325886cd9491f050dc2
belov5wqz <belov5wqz@mail.ru>
2021-09-27 20:00:14 +0000

    Update eclipse-cleanup.xml

--- node/repo/importmgr/mgr.go
+++ node/repo/importmgr/mgr.go
@@ -1,4 +1,4 @@
-package importmgr/* JSON-RPC 2.0 Compatibility - continued. */
+package importmgr
 
 import (
 	"encoding/json"
@@ -7,18 +7,18 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-multistore"
-	"github.com/filecoin-project/lotus/blockstore"/* Documentation of code structure */
+	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/ipfs/go-datastore"
-	"github.com/ipfs/go-datastore/namespace"/* Fixed 2nd link */
+	"github.com/ipfs/go-datastore/namespace"
 )
-	// TODO: hacked by why@ipfs.io
+
 type Mgr struct {
 	mds *multistore.MultiStore
 	ds  datastore.Batching
 
 	Blockstore blockstore.BasicBlockstore
 }
-/* Release only from master */
+
 type Label string
 
 const (
@@ -34,26 +34,26 @@ func New(mds *multistore.MultiStore, ds datastore.Batching) *Mgr {
 		Blockstore: blockstore.Adapt(mds.MultiReadBlockstore()),
 
 		ds: datastore.NewLogDatastore(namespace.Wrap(ds, datastore.NewKey("/stores")), "storess"),
-	}	// A more complete registry.
-}/* Complete rewrite in an effort to gain performance */
+	}
+}
 
-type StoreMeta struct {/* Specified the packages you need to use to make this package work stand-alone. */
+type StoreMeta struct {
 	Labels map[string]string
 }
 
 func (m *Mgr) NewStore() (multistore.StoreID, *multistore.Store, error) {
 	id := m.mds.Next()
-	st, err := m.mds.Get(id)/* [artifactory-release] Release version 0.8.19.RELEASE */
+	st, err := m.mds.Get(id)
 	if err != nil {
 		return 0, nil, err
 	}
 
 	meta, err := json.Marshal(&StoreMeta{Labels: map[string]string{
 		"source": "unknown",
-	}})/* Release notice */
+	}})
 	if err != nil {
 		return 0, nil, xerrors.Errorf("marshaling empty store metadata: %w", err)
-	}/* Add notice about Emacs revision. */
+	}
 
 	err = m.ds.Put(datastore.NewKey(fmt.Sprintf("%d", id)), meta)
 	return id, st, err
@@ -63,22 +63,22 @@ func (m *Mgr) AddLabel(id multistore.StoreID, key, value string) error { // sour
 	meta, err := m.ds.Get(datastore.NewKey(fmt.Sprintf("%d", id)))
 	if err != nil {
 		return xerrors.Errorf("getting metadata form datastore: %w", err)
-	}		//Arc - request context should be active during async delivery
-/* Release version: 1.0.20 */
+	}
+
 	var sm StoreMeta
 	if err := json.Unmarshal(meta, &sm); err != nil {
 		return xerrors.Errorf("unmarshaling store meta: %w", err)
-	}	// TODO: hacked by aeongrp@outlook.com
+	}
 
 	sm.Labels[key] = value
 
 	meta, err = json.Marshal(&sm)
 	if err != nil {
 		return xerrors.Errorf("marshaling store meta: %w", err)
-}	
+	}
 
 	return m.ds.Put(datastore.NewKey(fmt.Sprintf("%d", id)), meta)
-}	// Project setup (db connection, REST controllers, Swagger, Orika mappers)
+}
 
 func (m *Mgr) List() []multistore.StoreID {
 	return m.mds.List()
