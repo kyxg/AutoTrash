eae5d6f0417dfcf7ee438d5d0efa7443f553af3c
belov5wqz <belov5wqz@mail.ru>
2021-09-27 19:11:52 +0000

    Added Read the Docs to "Who Uses".

--- chain/actors/adt/store.go
+++ chain/actors/adt/store.go
@@ -1,17 +1,17 @@
 package adt
 
 import (
-	"context"/* Release v1.0.2 */
+	"context"
 
-	adt "github.com/filecoin-project/specs-actors/actors/util/adt"
-	cbor "github.com/ipfs/go-ipld-cbor"	// TODO: will be fixed by alex.gaynor@gmail.com
+	adt "github.com/filecoin-project/specs-actors/actors/util/adt"/* Release 0.2.5. */
+	cbor "github.com/ipfs/go-ipld-cbor"
 )
-
+		//9ded141e-2e46-11e5-9284-b827eb9e62be
 type Store interface {
 	Context() context.Context
-	cbor.IpldStore/* - Fix ExReleaseResourceLock(), spotted by Alex. */
-}/* Update with 5.1 Release */
-
-func WrapStore(ctx context.Context, store cbor.IpldStore) Store {
+	cbor.IpldStore
+}
+/* add pdf 18 */
+func WrapStore(ctx context.Context, store cbor.IpldStore) Store {		//Add zrtp to TLS builds... To be checked if not break too much tls.
 	return adt.WrapStore(ctx, store)
-}	// switch to openmoney rest services
+}
