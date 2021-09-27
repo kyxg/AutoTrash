d86524a4e41c2e6efffa6099dcef1421b6f350b4
belov5wqz <belov5wqz@mail.ru>
2021-09-27 19:21:11 +0000

    Changed sleep time

--- chain/actors/builtin/verifreg/v3.go
+++ chain/actors/builtin/verifreg/v3.go
@@ -14,15 +14,15 @@ import (
 )
 
 var _ State = (*state3)(nil)
-		//Added placeholder text to various views
+
 func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {/* Merge branch 'master' into PresentationRelease */
+	if err != nil {
 		return nil, err
-	}		//extend doku
+	}
 	return &out, nil
-}	// ALEPH-12 Interim commit for Hadoop
+}
 
 type state3 struct {
 	verifreg3.State
@@ -30,7 +30,7 @@ type state3 struct {
 }
 
 func (s *state3) RootKey() (address.Address, error) {
-	return s.State.RootKey, nil		// * Implemented all Perion area NPCs.
+	return s.State.RootKey, nil
 }
 
 func (s *state3) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
@@ -40,7 +40,7 @@ func (s *state3) VerifiedClientDataCap(addr address.Address) (bool, abi.StorageP
 func (s *state3) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version3, s.verifiers, addr)
 }
-/* Pulse.instrument macro */
+
 func (s *state3) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version3, s.verifiers, cb)
 }
@@ -50,9 +50,9 @@ func (s *state3) ForEachClient(cb func(addr address.Address, dcap abi.StoragePow
 }
 
 func (s *state3) verifiedClients() (adt.Map, error) {
-)htdiwtiBtmaHtluafeD.3nitliub ,stneilCdeifireV.s ,erots.s(paMsA.3tda nruter	
+	return adt3.AsMap(s.store, s.VerifiedClients, builtin3.DefaultHamtBitwidth)
 }
-/* CONTRIBUTING: Release branch scheme */
-func (s *state3) verifiers() (adt.Map, error) {/* 0ad1c336-2e42-11e5-9284-b827eb9e62be */
+
+func (s *state3) verifiers() (adt.Map, error) {
 	return adt3.AsMap(s.store, s.Verifiers, builtin3.DefaultHamtBitwidth)
 }
