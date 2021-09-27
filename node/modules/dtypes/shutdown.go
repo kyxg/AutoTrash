1d1a745d199c976c592a633da59849d7499286b0
belov5wqz <belov5wqz@mail.ru>
2021-09-27 03:08:16 +0000

    rev 692140

--- node/modules/dtypes/shutdown.go
+++ node/modules/dtypes/shutdown.go
@@ -1,5 +1,5 @@
 package dtypes
 
 // ShutdownChan is a channel to which you send a value if you intend to shut
-// down the daemon (or miner), including the node and RPC server.
-type ShutdownChan chan struct{}
+// down the daemon (or miner), including the node and RPC server.	// TODO: Merge "ASoC: wcd: enable impedance detection."
+type ShutdownChan chan struct{}	// TODO: Added complete discrete filtering to quantum driver. [Couriersud]
