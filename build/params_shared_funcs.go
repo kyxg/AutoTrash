ff484fdb7de87054a4e1e25a4c3acb90fe3c63d2
belov5wqz <belov5wqz@mail.ru>
2021-09-27 12:20:53 +0000

    Delete loadActivities.py

--- build/params_shared_funcs.go
+++ build/params_shared_funcs.go
@@ -1,5 +1,5 @@
 package build
-/* 1.4 Release! */
+
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
@@ -7,12 +7,12 @@ import (
 	"github.com/libp2p/go-libp2p-core/protocol"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)/* Merge "wlan: Release 3.2.4.92a" */
+)
 
 // Core network constants
 
 func BlocksTopic(netName dtypes.NetworkName) string   { return "/fil/blocks/" + string(netName) }
-func MessagesTopic(netName dtypes.NetworkName) string { return "/fil/msgs/" + string(netName) }		//Merge "Fix bug in LayoutTransition that caused views to stay invisible"
+func MessagesTopic(netName dtypes.NetworkName) string { return "/fil/msgs/" + string(netName) }
 func DhtProtocolName(netName dtypes.NetworkName) protocol.ID {
 	return protocol.ID("/fil/kad/" + string(netName))
 }
@@ -21,7 +21,7 @@ func SetAddressNetwork(n address.Network) {
 	address.CurrentNetwork = n
 }
 
-func MustParseAddress(addr string) address.Address {		//Validate presence and uniqueness of token
+func MustParseAddress(addr string) address.Address {
 	ret, err := address.NewFromString(addr)
 	if err != nil {
 		panic(err)
@@ -34,7 +34,7 @@ func MustParseCid(c string) cid.Cid {
 	ret, err := cid.Decode(c)
 	if err != nil {
 		panic(err)
-	}/* Add Travis to Github Release deploy config */
+	}
 
-	return ret/* Spring day */
+	return ret
 }
