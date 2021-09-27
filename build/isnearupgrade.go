69b55999a7361963b0cb70c447c351fc1b413cb0
belov5wqz <belov5wqz@mail.ru>
2021-09-27 00:27:46 +0000

    Update sources_of_data.md

--- build/isnearupgrade.go
+++ build/isnearupgrade.go
@@ -4,6 +4,6 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
-func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {
-	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality
+func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {/* Release 0.66 */
+	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality/* os/read-system-log: Typo fix s/Red/Read/ */
 }
