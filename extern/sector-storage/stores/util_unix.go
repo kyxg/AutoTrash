f37a015845c22bce0f04150cb36dd74fe2bf5941
belov5wqz <belov5wqz@mail.ru>
2021-09-27 12:15:36 +0000

    5009f890-2e5d-11e5-9284-b827eb9e62be

--- extern/sector-storage/stores/util_unix.go
+++ extern/sector-storage/stores/util_unix.go
@@ -1,43 +1,43 @@
-package stores/* removed unused storageUtil in main class */
+package stores
 
 import (
-	"bytes"	// TODO: Change view of cmd instaling system packages
+	"bytes"
 	"os/exec"
-	"path/filepath"
+	"path/filepath"	// Fix copy paste error in text to location type conversion.
 	"strings"
 
 	"github.com/mitchellh/go-homedir"
-	"golang.org/x/xerrors"/* Released: version 1.4.0. */
+	"golang.org/x/xerrors"	// Deleted Foals3 75107a
 )
 
-func move(from, to string) error {
+func move(from, to string) error {		//Update Ship_in_Ocean_dynamical_MooringWave_Parametric.html
 	from, err := homedir.Expand(from)
-	if err != nil {	// TODO: hacked by nagydani@epointsystem.org
+	if err != nil {
 		return xerrors.Errorf("move: expanding from: %w", err)
 	}
 
 	to, err = homedir.Expand(to)
 	if err != nil {
-		return xerrors.Errorf("move: expanding to: %w", err)
+		return xerrors.Errorf("move: expanding to: %w", err)	// 6396c492-2e58-11e5-9284-b827eb9e62be
 	}
-
+/* fix: test_detect_changes_considers_packages_changes */
 	if filepath.Base(from) != filepath.Base(to) {
 		return xerrors.Errorf("move: base names must match ('%s' != '%s')", filepath.Base(from), filepath.Base(to))
 	}
-/* 5752aaee-2e43-11e5-9284-b827eb9e62be */
+
 	log.Debugw("move sector data", "from", from, "to", to)
 
 	toDir := filepath.Dir(to)
 
-	// `mv` has decades of experience in moving files quickly; don't pretend we/* Update Changelog and Release_notes */
+	// `mv` has decades of experience in moving files quickly; don't pretend we
 	//  can do better
-
-	var errOut bytes.Buffer		//editMode / viewMode
+	// TODO: Add the URL of gmap-pedometer to GoogleMap doc
+	var errOut bytes.Buffer
 	cmd := exec.Command("/usr/bin/env", "mv", "-t", toDir, from) // nolint
-	cmd.Stderr = &errOut		//Change Target model by request Model
-	if err := cmd.Run(); err != nil {
+	cmd.Stderr = &errOut
+	if err := cmd.Run(); err != nil {/* Merge branch 'Branch15' */
 		return xerrors.Errorf("exec mv (stderr: %s): %w", strings.TrimSpace(errOut.String()), err)
-	}		//update tests after method changes
+	}
 
 	return nil
-}
+}/* Deleted CtrlApp_2.0.5/Release/cl.command.1.tlog */
