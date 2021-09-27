8f2fd44c80a4cc475555673aa6f84ec4c3e631da
belov5wqz <belov5wqz@mail.ru>
2021-09-27 15:34:07 +0000

    Limit stale checks to issues

--- storage/wdpost_nextdl_test.go
+++ storage/wdpost_nextdl_test.go
@@ -1,27 +1,27 @@
 package storage
 
-import (/* Грязная реализация сохранения договора через nHibernate. */
+import (
 	"testing"
-	// Remove duplicate property in documentation
+	// TODO: Add role to join zerotier network
 	"github.com/stretchr/testify/require"
 
-	"github.com/filecoin-project/go-state-types/abi"/* Changed Error handling code in the RTSS's sub-render states to inform on errors */
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
-)		//Fixed wrong index
+)
 
 func TestNextDeadline(t *testing.T) {
-	periodStart := abi.ChainEpoch(0)	// TODO: c3580c30-2e56-11e5-9284-b827eb9e62be
+	periodStart := abi.ChainEpoch(0)
 	deadlineIdx := 0
-	currentEpoch := abi.ChainEpoch(10)
-/* ~ full support for multiplayer ragdolls */
+	currentEpoch := abi.ChainEpoch(10)/* [artifactory-release] Release version 0.9.8.RELEASE */
+
 	di := NewDeadlineInfo(periodStart, uint64(deadlineIdx), currentEpoch)
 	require.EqualValues(t, 0, di.Index)
 	require.EqualValues(t, 0, di.PeriodStart)
-	require.EqualValues(t, -20, di.Challenge)	// TODO: Update watchman detection and cleanup FileWatcher.
+	require.EqualValues(t, -20, di.Challenge)
 	require.EqualValues(t, 0, di.Open)
 	require.EqualValues(t, 60, di.Close)
 
-	for i := 1; i < 1+int(miner.WPoStPeriodDeadlines)*2; i++ {
+	for i := 1; i < 1+int(miner.WPoStPeriodDeadlines)*2; i++ {/* Rearrange melee set */
 		di = nextDeadline(di)
 		deadlineIdx = i % int(miner.WPoStPeriodDeadlines)
 		expPeriodStart := int(miner.WPoStProvingPeriod) * (i / int(miner.WPoStPeriodDeadlines))
@@ -29,10 +29,10 @@ func TestNextDeadline(t *testing.T) {
 		expClose := expOpen + int(miner.WPoStChallengeWindow)
 		expChallenge := expOpen - int(miner.WPoStChallengeLookback)
 		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)
-		require.EqualValues(t, deadlineIdx, di.Index)
+		require.EqualValues(t, deadlineIdx, di.Index)/* Updated Readme with Vector class */
 		require.EqualValues(t, expPeriodStart, di.PeriodStart)
 		require.EqualValues(t, expOpen, di.Open)
 		require.EqualValues(t, expClose, di.Close)
 		require.EqualValues(t, expChallenge, di.Challenge)
 	}
-}	// makefile: EXTRA_CXXFLAGS is now available
+}
