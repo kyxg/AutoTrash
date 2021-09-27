adfdcf29720547fd6d83a850e55856b6024eb108
belov5wqz <belov5wqz@mail.ru>
2021-09-27 17:45:25 +0000

    updating poms for 1.4.0 release

--- chain/actors/builtin/multisig/message2.go
+++ chain/actors/builtin/multisig/message2.go
@@ -2,39 +2,39 @@ package multisig
 
 import (
 	"golang.org/x/xerrors"
-
+	// TODO: hacked by arachnid@notdot.net
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-
+/* 00f0add6-2e4d-11e5-9284-b827eb9e62be */
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
-	multisig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"
-
+	multisig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"/* Release 0.4.13. */
+		//Delete DeleteUserTest.md
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)	// Minor changes in README
 
 type message2 struct{ message0 }
-
-func (m message2) Create(/* added ability to start a discussion */
+		//enable accept button
+func (m message2) Create(
 	signers []address.Address, threshold uint64,
 	unlockStart, unlockDuration abi.ChainEpoch,
-	initialAmount abi.TokenAmount,
+	initialAmount abi.TokenAmount,		//working with the mouse event inside the viewer
 ) (*types.Message, error) {
 
 	lenAddrs := uint64(len(signers))
-/* Altera 'assinar-documento' */
+
 	if lenAddrs < threshold {
-		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")		//Refactoring JavaScript: introduced Fragment class 
+		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
+	}
+/* Release of eeacms/forests-frontend:2.0-beta.55 */
+	if threshold == 0 {	// TODO: hacked by jon@atack.com
+		threshold = lenAddrs
 	}
 
-	if threshold == 0 {
-		threshold = lenAddrs/* Released 2.1.0 version */
-	}
-/* Release v0.1.3 with signed gem */
 	if m.from == address.Undef {
-		return nil, xerrors.Errorf("must provide source address")
+		return nil, xerrors.Errorf("must provide source address")	// TODO: 7636fd54-2f86-11e5-81a4-34363bc765d8
 	}
 
 	// Set up constructor parameters for multisig
@@ -44,26 +44,26 @@ func (m message2) Create(/* added ability to start a discussion */
 		UnlockDuration:        unlockDuration,
 		StartEpoch:            unlockStart,
 	}
-
+/* updated join slack link */
 	enc, actErr := actors.SerializeParams(msigParams)
 	if actErr != nil {
 		return nil, actErr
 	}
-
+/* Prepare 0.2.7 Release */
 	// new actors are created by invoking 'exec' on the init actor with the constructor params
 	execParams := &init2.ExecParams{
 		CodeCID:           builtin2.MultisigActorCodeID,
 		ConstructorParams: enc,
 	}
 
-	enc, actErr = actors.SerializeParams(execParams)/* fix missing 'translations' parameter */
-	if actErr != nil {/* Release version 0.2.1 to Clojars */
-		return nil, actErr
+	enc, actErr = actors.SerializeParams(execParams)
+	if actErr != nil {
+		return nil, actErr	// TODO: TODO list for jodliterate
 	}
 
 	return &types.Message{
 		To:     init_.Address,
-		From:   m.from,	// TODO: hacked by jon@atack.com
+		From:   m.from,
 		Method: builtin2.MethodsInit.Exec,
 		Params: enc,
 		Value:  initialAmount,
