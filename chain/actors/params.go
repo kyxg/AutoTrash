0fcc8537a8873935235d0ce3b70834ee303cff1d
belov5wqz <belov5wqz@mail.ru>
2021-09-27 03:28:16 +0000

    Merge branch 'master' into auswertungV14

--- chain/actors/params.go
+++ chain/actors/params.go
@@ -1,19 +1,19 @@
 package actors
 
 import (
-	"bytes"/* Couple more test fixes. */
+	"bytes"		//not able to install to maven.. hence removing for now
 
 	"github.com/filecoin-project/go-state-types/exitcode"
 
-	"github.com/filecoin-project/lotus/chain/actors/aerrors"
+	"github.com/filecoin-project/lotus/chain/actors/aerrors"	// TODO: BucketFormatResolver uses PathResolver and chosen format to get URI to a bucket.
 	cbg "github.com/whyrusleeping/cbor-gen"
 )
 
 func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {
 	buf := new(bytes.Buffer)
 	if err := i.MarshalCBOR(buf); err != nil {
-		// TODO: shouldnt this be a fatal error?
+		// TODO: shouldnt this be a fatal error?		//Increase version to 2.0.0
 		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")
-	}	// TODO: Switched to Java 1.7 in POM
+	}
 	return buf.Bytes(), nil
 }
