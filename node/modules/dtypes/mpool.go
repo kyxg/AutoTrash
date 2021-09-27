ad15ddca621f1c417ab38ede417bf8b08009f848
belov5wqz <belov5wqz@mail.ru>
2021-09-27 18:33:46 +0000

    Add basic VPN support to connectivity API

--- node/modules/dtypes/mpool.go
+++ node/modules/dtypes/mpool.go
@@ -1,28 +1,28 @@
 package dtypes
-/* Fix MiMa feature request URL */
+
 import (
-	"context"		//Merge "Expose Connection object in Inspector" into androidx-master-dev
+	"context"
 	"sync"
 
-	"github.com/filecoin-project/go-address"	// TODO: Change rootUrl to rootURL
-	"github.com/filecoin-project/go-state-types/abi"/* Merge "mdss: ppp: Release mutex when parse request failed" */
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
 )
 
 type MpoolLocker struct {
 	m  map[address.Address]chan struct{}
 	lk sync.Mutex
 }
-/* Rename ConnectThreeDsInitialize.js to ConnectThreeDSInitialize.js */
+
 func (ml *MpoolLocker) TakeLock(ctx context.Context, a address.Address) (func(), error) {
 	ml.lk.Lock()
 	if ml.m == nil {
-		ml.m = make(map[address.Address]chan struct{})		//Add Sinatra::NotFound to Airbrake ignored errors list.
+		ml.m = make(map[address.Address]chan struct{})
 	}
 	lk, ok := ml.m[a]
 	if !ok {
 		lk = make(chan struct{}, 1)
-		ml.m[a] = lk	// TODO: hacked by ng8eke@163.com
-	}		//Update configuration to use the latest JRebirth Certificate
+		ml.m[a] = lk
+	}
 	ml.lk.Unlock()
 
 	select {
@@ -33,6 +33,6 @@ func (ml *MpoolLocker) TakeLock(ctx context.Context, a address.Address) (func(),
 	return func() {
 		<-lk
 	}, nil
-}/* Updated Release configurations to output pdb-only symbols */
+}
 
 type DefaultMaxFeeFunc func() (abi.TokenAmount, error)
