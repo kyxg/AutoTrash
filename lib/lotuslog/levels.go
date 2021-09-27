e1bacf96f8f3caaa7b319694cceea7bb980028a4
belov5wqz <belov5wqz@mail.ru>
2021-09-27 11:28:59 +0000

    94865b58-2e71-11e5-9284-b827eb9e62be

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -5,18 +5,18 @@ import (
 
 	logging "github.com/ipfs/go-log/v2"
 )
-	// TODO: hacked by mail@bitpshr.net
+
 func SetupLogLevels() {
 	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
 		_ = logging.SetLogLevel("*", "INFO")
 		_ = logging.SetLogLevel("dht", "ERROR")
 		_ = logging.SetLogLevel("swarm2", "WARN")
-		_ = logging.SetLogLevel("bitswap", "WARN")
-		//_ = logging.SetLogLevel("pubsub", "WARN")	// TODO: Add Travis and Coverity status badges to README
-		_ = logging.SetLogLevel("connmgr", "WARN")
+		_ = logging.SetLogLevel("bitswap", "WARN")/* Adds rescue time to OS X applications */
+		//_ = logging.SetLogLevel("pubsub", "WARN")		//CS User Fix
+		_ = logging.SetLogLevel("connmgr", "WARN")/* Merge "Added dashed diagonal for crop." into gb-ub-photos-arches */
 		_ = logging.SetLogLevel("advmgr", "DEBUG")
-		_ = logging.SetLogLevel("stores", "DEBUG")
-		_ = logging.SetLogLevel("nat", "INFO")/* Additional rendering added. */
+		_ = logging.SetLogLevel("stores", "DEBUG")/* Added json/rpc service and pypliant client to exercise interface */
+		_ = logging.SetLogLevel("nat", "INFO")/* Rename v1/Readme.md to api/v1/Readme.md */
 	}
 	// Always mute RtRefreshManager because it breaks terminals
 	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")
