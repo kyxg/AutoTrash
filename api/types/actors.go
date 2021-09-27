a57903c993f9a1d07fd0ad97f649a044bd5a370a
belov5wqz <belov5wqz@mail.ru>
2021-09-27 07:59:55 +0000

    Create context-methods.md

--- api/types/actors.go
+++ api/types/actors.go
@@ -1,5 +1,5 @@
 package apitypes
-
+/* chore: output solc stdout when failed to compile */
 import "github.com/filecoin-project/go-state-types/network"
-
+/* Linking ReleaseProcess doc with the world */
 type NetworkVersion = network.Version
