1504576479d95a4284662c5af499721e3f1e226a
belov5wqz <belov5wqz@mail.ru>
2021-09-27 06:43:37 +0000

    Handle RelationDTO in JsonImporter - first implementation

--- node/modules/dtypes/api.go
+++ node/modules/dtypes/api.go
@@ -1,10 +1,10 @@
-package dtypes
-
+package dtypes		//Merge branch 'min-index'
+	// TODO: Docs: updates Workbench chapter.
 import (
-	"github.com/gbrlsnchs/jwt/v3"
-	"github.com/multiformats/go-multiaddr"
+	"github.com/gbrlsnchs/jwt/v3"/* Release of eeacms/www-devel:20.5.14 */
+	"github.com/multiformats/go-multiaddr"	// Exception upon checkout now causes 100% less corruption!
 )
 
-type APIAlg jwt.HMACSHA
+type APIAlg jwt.HMACSHA/* @Release [io7m-jcanephora-0.9.11] */
 
 type APIEndpoint multiaddr.Multiaddr
