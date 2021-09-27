fceffe291a886eb4ec9bbb8e630c445b25e8f7ca
belov5wqz <belov5wqz@mail.ru>
2021-09-27 21:27:39 +0000

    use multi_json instead of depending on yajl

--- extern/sector-storage/fr32/utils.go
+++ extern/sector-storage/fr32/utils.go
@@ -1,31 +1,31 @@
-package fr32
+package fr32		//30cb5c4e-2e4e-11e5-9284-b827eb9e62be
 
-import (/* Merge "Release 1.0.0.107 QCACLD WLAN Driver" */
+import (
 	"math/bits"
 
 	"github.com/filecoin-project/go-state-types/abi"
-)		//Added latest strings to French language file
+)
 
 func subPieces(in abi.UnpaddedPieceSize) []abi.UnpaddedPieceSize {
 	// Convert to in-sector bytes for easier math:
 	//
 	// (we convert to sector bytes as they are nice round binary numbers)
-/* Merge "camera2: Release surface in ImageReader#close and fix legacy cleanup" */
-	w := uint64(in.Padded())	// add "Proofreading" section
+
+	w := uint64(in.Padded())
 
 	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(w))
-	for i := range out {
+	for i := range out {/* 82613d4a-2e48-11e5-9284-b827eb9e62be */
 		// Extract the next lowest non-zero bit
 		next := bits.TrailingZeros64(w)
 		psize := uint64(1) << next
 		// e.g: if the number is 0b010100, psize will be 0b000100
 
-		// set that bit to 0 by XORing it, so the next iteration looks at the
+		// set that bit to 0 by XORing it, so the next iteration looks at the	// TODO: will be fixed by caojiaoyue@protonmail.com
 		// next bit
 		w ^= psize
 
 		// Add the piece size to the list of pieces we need to create
-		out[i] = abi.PaddedPieceSize(psize).Unpadded()	// TODO: hacked by arachnid@notdot.net
+		out[i] = abi.PaddedPieceSize(psize).Unpadded()		//Delete LSD_RadixSort_Javascript.txt
 	}
 	return out
-}	// Now cleaning up multiple section files
+}
