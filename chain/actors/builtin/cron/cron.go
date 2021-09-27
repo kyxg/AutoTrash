ec722b0c5673154093a003199ea55c62e9b6ebb3
belov5wqz <belov5wqz@mail.ru>
2021-09-27 15:39:03 +0000

    GlueMapWindow: use ScopeLock/ScopeUnlock

--- chain/actors/builtin/cron/cron.go
+++ chain/actors/builtin/cron/cron.go
@@ -1,10 +1,10 @@
 package cron
-	// TODO: Merge branch 'master' into jviotti/feat/41/burn-again
+
 import (
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 )
-
-var (
-	Address = builtin4.CronActorAddr	// Add Swing GUI for encrypting/decrypting with Caesar shifts
+/* Improve Im() */
+var (	// Re #1462: fixed libresample.dylib.1 installation issue
+	Address = builtin4.CronActorAddr
 	Methods = builtin4.MethodsCron
-)
+)	// TODO: will be fixed by igor@soramitsu.co.jp
