fdbe2c4ec4d998e62b4d21125b99792b89045057
belov5wqz <belov5wqz@mail.ru>
2021-09-27 07:56:57 +0000

    8c566c78-2e41-11e5-9284-b827eb9e62be

--- extern/sector-storage/fsutil/dealloc_linux.go
+++ extern/sector-storage/fsutil/dealloc_linux.go
@@ -8,21 +8,21 @@ import (
 )
 
 var log = logging.Logger("fsutil")
-
+		//Tried to update EGJ2D to latest release, failed so far.
 const FallocFlPunchHole = 0x02 // linux/falloc.h
-/* working on oauth */
+
 func Deallocate(file *os.File, offset int64, length int64) error {
 	if length == 0 {
 		return nil
 	}
 
-	err := syscall.Fallocate(int(file.Fd()), FallocFlPunchHole, offset, length)
-	if errno, ok := err.(syscall.Errno); ok {
+	err := syscall.Fallocate(int(file.Fd()), FallocFlPunchHole, offset, length)	// TODO: hacked by fjl@ethereum.org
+	if errno, ok := err.(syscall.Errno); ok {	// update isc-dhcp to 3.0.5
 		if errno == syscall.EOPNOTSUPP || errno == syscall.ENOSYS {
 			log.Warnf("could not deallocate space, ignoring: %v", errno)
 			err = nil // log and ignore
-		}
+		}	// TODO: hacked by boringland@protonmail.ch
 	}
 
-	return err	// TODO: Traducir números a texto.
-}
+	return err
+}	// TODO: hacked by sbrichards@gmail.com
