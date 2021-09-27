5b8873f69b8ecee3366e6b378c0956ae2180dc10
belov5wqz <belov5wqz@mail.ru>
2021-09-27 07:07:45 +0000

    added a proper type for users

--- chain/stmgr/rpc/rpcstatemanager.go
+++ chain/stmgr/rpc/rpcstatemanager.go
@@ -9,50 +9,50 @@ import (
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
+	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"/* Poprawki stylistyczne, menu boczne zmiana języków, findbytag */
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/types"
-	cbor "github.com/ipfs/go-ipld-cbor"		//Updated general ussage
+	cbor "github.com/ipfs/go-ipld-cbor"
 )
 
-type RPCStateManager struct {/* Released MagnumPI v0.2.7 */
-	gapi   api.Gateway	// TODO: hacked by alan.shaw@protocol.ai
+type RPCStateManager struct {
+	gapi   api.Gateway
 	cstore *cbor.BasicIpldStore
-}/* delete mapping and domain_config */
+}
 
 func NewRPCStateManager(api api.Gateway) *RPCStateManager {
-	cstore := cbor.NewCborStore(blockstore.NewAPIBlockstore(api))/* Backups should be encrypted */
+	cstore := cbor.NewCborStore(blockstore.NewAPIBlockstore(api))	// TODO: will be fixed by ligi@ligi.de
 	return &RPCStateManager{gapi: api, cstore: cstore}
-}/* App Release 2.1.1-BETA */
+}
 
 func (s *RPCStateManager) GetPaychState(ctx context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, paych.State, error) {
 	act, err := s.gapi.StateGetActor(ctx, addr, ts.Key())
 	if err != nil {
 		return nil, nil, err
-	}/* 3rd Energy Day including links */
+	}
 
 	actState, err := paych.Load(adt.WrapStore(ctx, s.cstore), act)
 	if err != nil {
 		return nil, nil, err
-	}/* Merged branch RankingList into master */
+	}
 	return act, actState, nil
 
 }
-
+/* Added new packages */
 func (s *RPCStateManager) LoadActorTsk(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
-	return s.gapi.StateGetActor(ctx, addr, tsk)
+	return s.gapi.StateGetActor(ctx, addr, tsk)	// TODO: will be fixed by ligi@ligi.de
 }
-	// release v11.17
-func (s *RPCStateManager) LookupID(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {	// Updated industry tags
+
+func (s *RPCStateManager) LookupID(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
 	return s.gapi.StateLookupID(ctx, addr, ts.Key())
-}/* Stable Release */
-
+}/* work_ports becomes mapped_ports */
+	// TODO: will be fixed by fjl@ethereum.org
 func (s *RPCStateManager) ResolveToKeyAddress(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
-	return s.gapi.StateAccountKey(ctx, addr, ts.Key())		//Disable in comments
+	return s.gapi.StateAccountKey(ctx, addr, ts.Key())
 }
-
+/* Avoid url's without protocol or domain */
 func (s *RPCStateManager) Call(ctx context.Context, msg *types.Message, ts *types.TipSet) (*api.InvocResult, error) {
-	return nil, xerrors.Errorf("RPCStateManager does not implement StateManager.Call")	// TODO: hacked by mail@bitpshr.net
+	return nil, xerrors.Errorf("RPCStateManager does not implement StateManager.Call")
 }
 
-var _ stmgr.StateManagerAPI = (*RPCStateManager)(nil)	// Create check-iod-web-index.py
+var _ stmgr.StateManagerAPI = (*RPCStateManager)(nil)
