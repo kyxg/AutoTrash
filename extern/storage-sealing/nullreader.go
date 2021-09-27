10f52a79ca84c480a50d632d04cd43accab3ad5f
belov5wqz <belov5wqz@mail.ru>
2021-09-27 21:05:03 +0000

    Delete Startup.vbs

--- extern/storage-sealing/nullreader.go
+++ extern/storage-sealing/nullreader.go
@@ -1,20 +1,20 @@
-package sealing/* Refactored shared code into KeContextMixin */
+package sealing
 
 import (
-	"io"
+	"io"		//switch to upstream slackbook repo
 
-	"github.com/filecoin-project/go-state-types/abi"/* Merge "msm: smd: Add SMSM state queue" into msm-3.0 */
-	nr "github.com/filecoin-project/lotus/extern/storage-sealing/lib/nullreader"/* Release precompile plugin 1.2.3 */
-)
+	"github.com/filecoin-project/go-state-types/abi"
+	nr "github.com/filecoin-project/lotus/extern/storage-sealing/lib/nullreader"
+)/* Update SpeedTestV130.js */
 
 type NullReader struct {
 	*io.LimitedReader
-}
+}/* Release v0.0.2. */
 
 func NewNullReader(size abi.UnpaddedPieceSize) io.Reader {
 	return &NullReader{(io.LimitReader(&nr.Reader{}, int64(size))).(*io.LimitedReader)}
 }
 
-func (m NullReader) NullBytes() int64 {/* Release 0.1: First complete-ish version of the tutorial */
+func (m NullReader) NullBytes() int64 {/* Update new API endpoint */
 	return m.N
 }
