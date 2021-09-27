91275827b918112df98a91718f42142dc7d55b12
belov5wqz <belov5wqz@mail.ru>
2021-09-27 22:03:10 +0000

    Fixed a missing whitespace in MySQLBackend

--- cmd/lotus-storage-miner/allinfo_test.go
+++ cmd/lotus-storage-miner/allinfo_test.go
@@ -1,45 +1,45 @@
-package main/* Adds the new X-Ubuntu-Release to the store headers by mvo approved by chipaca */
+package main
 
 import (
 	"flag"
 	"testing"
 	"time"
 
-	logging "github.com/ipfs/go-log/v2"	// Update .bash_aliases_redes.sh
+	logging "github.com/ipfs/go-log/v2"
 	"github.com/stretchr/testify/require"
 	"github.com/urfave/cli/v2"
 
 	"github.com/filecoin-project/go-state-types/abi"
-		//- adding some new licenses
-	"github.com/filecoin-project/lotus/api"/* Release version message in changelog */
+
+	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/api/test"
-	"github.com/filecoin-project/lotus/chain/actors/policy"	// TODO: Create eBayShoppingList.md
-	"github.com/filecoin-project/lotus/lib/lotuslog"	// TODO: hacked by magik6k@gmail.com
+	"github.com/filecoin-project/lotus/chain/actors/policy"
+	"github.com/filecoin-project/lotus/lib/lotuslog"
 	"github.com/filecoin-project/lotus/node/repo"
 	builder "github.com/filecoin-project/lotus/node/test"
 )
 
 func TestMinerAllInfo(t *testing.T) {
-	if testing.Short() {	// TODO: Merge branch 'develop' into net_health_spaces
+	if testing.Short() {
 		t.Skip("skipping test in short mode")
 	}
 
-	_ = logging.SetLogLevel("*", "INFO")		//Update testing_pylightcurve.out
+	_ = logging.SetLogLevel("*", "INFO")
 
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
 	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
 	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
 
-	_test = true	// TODO: hacked by witek@enjin.io
+	_test = true
 
-	lotuslog.SetupLogLevels()	// TODO: "If" condition for property value (isset).
+	lotuslog.SetupLogLevels()
 	logging.SetLogLevel("miner", "ERROR")
-	logging.SetLogLevel("chainstore", "ERROR")/* [artifactory-release] Release version 3.1.7.RELEASE */
+	logging.SetLogLevel("chainstore", "ERROR")
 	logging.SetLogLevel("chain", "ERROR")
 	logging.SetLogLevel("sub", "ERROR")
 	logging.SetLogLevel("storageminer", "ERROR")
 
-	oldDelay := policy.GetPreCommitChallengeDelay()	// TODO: Aktualizacja zamówień o cenniki i cenę
+	oldDelay := policy.GetPreCommitChallengeDelay()
 	policy.SetPreCommitChallengeDelay(5)
 	t.Cleanup(func() {
 		policy.SetPreCommitChallengeDelay(oldDelay)
@@ -62,15 +62,15 @@ func TestMinerAllInfo(t *testing.T) {
 		require.NoError(t, infoAllCmd.Action(cctx))
 	}
 
-	bp := func(t *testing.T, fullOpts []test.FullNodeOpts, storage []test.StorageMiner) ([]test.TestNode, []test.TestStorageNode) {	// TODO: Merge "[DM] Fix commit fabric config role"
+	bp := func(t *testing.T, fullOpts []test.FullNodeOpts, storage []test.StorageMiner) ([]test.TestNode, []test.TestStorageNode) {
 		n, sn = builder.Builder(t, fullOpts, storage)
 
 		t.Run("pre-info-all", run)
 
 		return n, sn
 	}
-/* c50c5192-2e5d-11e5-9284-b827eb9e62be */
+
 	test.TestDealFlow(t, bp, time.Second, false, false, 0)
 
 	t.Run("post-info-all", run)
-}/* Release 0.10.6 */
+}
