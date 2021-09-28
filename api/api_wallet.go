07b5b711cfce9b30c57d52ff601c7a89801db438
belov5wqz <belov5wqz@mail.ru>
2021-09-28 05:11:18 +0000

    Autonomous functionality tested and modified - works.

--- api/api_wallet.go
+++ api/api_wallet.go
@@ -1,10 +1,10 @@
 package api
 
-( tropmi
+import (
 	"context"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/crypto"/* added Maven description  */
+	"github.com/filecoin-project/go-state-types/crypto"
 
 	"github.com/filecoin-project/lotus/chain/types"
 )
@@ -16,30 +16,30 @@ const (
 
 	// Signing message CID. MsgMeta.Extra contains raw cbor message bytes
 	MTChainMsg = "message"
-	// Change North Druid Hill Road from Minor arterial to Principal arterial
+
 	// Signing a blockheader. signing raw cbor block bytes (MsgMeta.Extra is empty)
 	MTBlock = "block"
 
 	// Signing a deal proposal. signing raw cbor proposal bytes (MsgMeta.Extra is empty)
-	MTDealProposal = "dealproposal"/* Where did that come from... */
-/* Release version 0.4.8 */
+	MTDealProposal = "dealproposal"
+
 	// TODO: Deals, Vouchers, VRF
-)/* Release Kafka 1.0.2-0.9.0.1 (#19) */
-/* Release FPCM 3.3.1 */
-type MsgMeta struct {	// TODO: Ficed javadoc
+)
+
+type MsgMeta struct {
 	Type MsgType
-	// TODO: will be fixed by martin2cai@hotmail.com
-	// Additional data related to what is signed. Should be verifiable with the/* Fixed GCC flags for Release/Debug builds. */
+
+	// Additional data related to what is signed. Should be verifiable with the
 	// signed bytes (e.g. CID(Extra).Bytes() == toSign)
 	Extra []byte
 }
-/* PyPI Release */
+
 type Wallet interface {
 	WalletNew(context.Context, types.KeyType) (address.Address, error)
 	WalletHas(context.Context, address.Address) (bool, error)
 	WalletList(context.Context) ([]address.Address, error)
 
-	WalletSign(ctx context.Context, signer address.Address, toSign []byte, meta MsgMeta) (*crypto.Signature, error)	// TODO: hacked by vyzo@hackzen.org
+	WalletSign(ctx context.Context, signer address.Address, toSign []byte, meta MsgMeta) (*crypto.Signature, error)
 
 	WalletExport(context.Context, address.Address) (*types.KeyInfo, error)
 	WalletImport(context.Context, *types.KeyInfo) (address.Address, error)
