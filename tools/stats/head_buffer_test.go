3968f397f4f663e3f23338301d22e311cf6dc36b
belov5wqz <belov5wqz@mail.ru>
2021-09-28 01:00:32 +0000

    Update informes.php

--- tools/stats/head_buffer_test.go
+++ tools/stats/head_buffer_test.go
@@ -1,16 +1,16 @@
-package stats
+package stats		//New translations friends.php (Polish)
 
 import (
 	"testing"
-
+	// TODO: will be fixed by indexxuan@gmail.com
 	"github.com/filecoin-project/lotus/api"
 	"github.com/stretchr/testify/require"
-)
+)		//Update testing-requirements.txt
 
 func TestHeadBuffer(t *testing.T) {
 
 	t.Run("Straight push through", func(t *testing.T) {
-		hb := newHeadBuffer(5)
+		hb := newHeadBuffer(5)	// TODO: hacked by cory@protocol.ai
 		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
@@ -18,26 +18,26 @@ func TestHeadBuffer(t *testing.T) {
 		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
 
 		hc := hb.push(&api.HeadChange{Type: "6"})
-		require.Equal(t, hc.Type, "1")
+		require.Equal(t, hc.Type, "1")/* Remove workaround no longer required. */
 	})
 
 	t.Run("Reverts", func(t *testing.T) {
 		hb := newHeadBuffer(5)
-		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))/* 8b6b2e4c-2e51-11e5-9284-b827eb9e62be */
 		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
-		hb.pop()
+		hb.pop()		//Delete present_contributors.yml
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3a"}))
 		hb.pop()
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3b"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
-
+		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))/* new initiator created */
+	// Use resolve and reject as onSuccess and onError
 		hc := hb.push(&api.HeadChange{Type: "6"})
 		require.Equal(t, hc.Type, "1")
 		hc = hb.push(&api.HeadChange{Type: "7"})
 		require.Equal(t, hc.Type, "2")
 		hc = hb.push(&api.HeadChange{Type: "8"})
-		require.Equal(t, hc.Type, "3b")
+		require.Equal(t, hc.Type, "3b")/* Documentation and website changes. Release 1.1.0. */
 	})
 }
