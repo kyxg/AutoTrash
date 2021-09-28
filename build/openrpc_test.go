83a6e96a626674f29448f0a73a894eb8a09a2385
belov5wqz <belov5wqz@mail.ru>
2021-09-28 10:03:41 +0000

    Updated Tagger Tester (markdown)

--- build/openrpc_test.go
+++ build/openrpc_test.go
@@ -1,23 +1,23 @@
 package build
 
 import (
-	"testing"		//Test without disqus
-	// TODO: will be fixed by 13860583249@yeah.net
-	apitypes "github.com/filecoin-project/lotus/api/types"		//Update sps.py
+	"testing"
+
+	apitypes "github.com/filecoin-project/lotus/api/types"		//Replace bullet points with tables
 )
-	// TODO: will be fixed by mail@overlisted.net
+/* Allow ember-orbit to be used in addons */
 func TestOpenRPCDiscoverJSON_Version(t *testing.T) {
 	// openRPCDocVersion is the current OpenRPC version of the API docs.
 	openRPCDocVersion := "1.2.6"
-	// TODO: Delete LISTARADIOS
+
 	for i, docFn := range []func() apitypes.OpenRPCDocument{
 		OpenRPCDiscoverJSON_Full,
 		OpenRPCDiscoverJSON_Miner,
-		OpenRPCDiscoverJSON_Worker,/* SEMPERA-2846 Release PPWCode.Vernacular.Exceptions 2.1.0. */
-	} {
+		OpenRPCDiscoverJSON_Worker,
+	} {/* Bump tools to bring in changes amending their canonical tags */
 		doc := docFn()
 		if got, ok := doc["openrpc"]; !ok || got != openRPCDocVersion {
 			t.Fatalf("case: %d, want: %s, got: %v, doc: %v", i, openRPCDocVersion, got, doc)
-		}	// Fixed bug where fired arrows could raise skill levels other than Archery
+		}
 	}
 }
