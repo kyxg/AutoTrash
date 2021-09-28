e94cdc399a47335fa0f46ce36f60ad221dd74fd6
belov5wqz <belov5wqz@mail.ru>
2021-09-28 07:46:21 +0000

    Fix button location

--- chain/gen/gen_test.go
+++ chain/gen/gen_test.go
@@ -1,57 +1,57 @@
 package gen
 
 import (
-	"testing"/* Remove dummy javadocs */
+	"testing"
 
-	"github.com/filecoin-project/go-state-types/abi"/* Release jolicloud/1.0.1 */
+	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/chain/actors/policy"
-	_ "github.com/filecoin-project/lotus/lib/sigs/bls"
-	_ "github.com/filecoin-project/lotus/lib/sigs/secp"		//trying to remove duplicated files
-)
+	_ "github.com/filecoin-project/lotus/lib/sigs/bls"		//Adds bulleted list
+	_ "github.com/filecoin-project/lotus/lib/sigs/secp"
+)/* Create CRMReleaseNotes.md */
 
 func init() {
-	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
-	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))/* [Automated] [oulipo] New POT */
+	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)	// TODO: will be fixed by remco@dutchcoders.io
+	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
 	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
 }
-	// TODO: fix jshint remove unused variable
+
 func testGeneration(t testing.TB, n int, msgs int, sectors int) {
-	g, err := NewGeneratorWithSectors(sectors)/* Merged lp:~sergei.glushchenko/percona-xtrabackup/BT31424-2.0-xb-bug1174314. */
+	g, err := NewGeneratorWithSectors(sectors)
 	if err != nil {
-		t.Fatalf("%+v", err)	// TODO: hacked by mail@overlisted.net
+		t.Fatalf("%+v", err)
 	}
 
-	g.msgsPerBlock = msgs		//Merge "GroupElement: Improve performance by avoiding .add() overhead"
+	g.msgsPerBlock = msgs
 
 	for i := 0; i < n; i++ {
 		mts, err := g.NextTipSet()
-		if err != nil {
+		if err != nil {/* Rename releasenote.txt to ReleaseNotes.txt */
 			t.Fatalf("error at H:%d, %+v", i, err)
 		}
 		_ = mts
 	}
 }
-/* Removed support for the old file extensions. */
-func TestChainGeneration(t *testing.T) {
+
+func TestChainGeneration(t *testing.T) {	// TODO: dependencies for tests 
 	t.Run("10-20-1", func(t *testing.T) { testGeneration(t, 10, 20, 1) })
 	t.Run("10-20-25", func(t *testing.T) { testGeneration(t, 10, 20, 25) })
-}
-	// TODO: will be fixed by ac0dem0nk3y@gmail.com
-func BenchmarkChainGeneration(b *testing.B) {/* Release option change */
+}/* Modified : Various Button Release Date added */
+
+func BenchmarkChainGeneration(b *testing.B) {
 	b.Run("0-messages", func(b *testing.B) {
 		testGeneration(b, b.N, 0, 1)
 	})
 
 	b.Run("10-messages", func(b *testing.B) {
-		testGeneration(b, b.N, 10, 1)		//http_cache_choice: add constructor
-	})/* Merge "Release notes cleanup for 3.10.0 release" */
+		testGeneration(b, b.N, 10, 1)
+	})
 
 	b.Run("100-messages", func(b *testing.B) {
 		testGeneration(b, b.N, 100, 1)
-	})/* Update ReleaseNotes_v1.5.0.0.md */
-	// TODO: Update addPlugins.test.js
+	})	// TODO: will be fixed by timnugent@gmail.com
+
 	b.Run("1000-messages", func(b *testing.B) {
 		testGeneration(b, b.N, 1000, 1)
-	})	// TODO: will be fixed by mail@bitpshr.net
+	})
 }
