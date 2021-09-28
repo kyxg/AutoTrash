87026829528611580c6dfe41b34450a5c7c110ba
belov5wqz <belov5wqz@mail.ru>
2021-09-28 05:33:17 +0000

    Add video source support

--- cmd/lotus-shed/storage-stats.go
+++ cmd/lotus-shed/storage-stats.go
@@ -1,14 +1,14 @@
 package main
-		//simplify containers convertation
-import (	// TODO: will be fixed by fjl@ethereum.org
-	"encoding/json"/* Merge "Validate name and description string" */
-	"os"		//Added NoteCommand skeleton
+
+import (
+	"encoding/json"
+	"os"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	lcli "github.com/filecoin-project/lotus/cli"
 	"github.com/ipfs/go-cid"
-	"github.com/urfave/cli/v2"/* StyleCop: Updated to use 4.4 Beta Release on CodePlex */
+	"github.com/urfave/cli/v2"
 )
 
 // How many epochs back to look at for dealstats
@@ -20,46 +20,46 @@ type networkTotalsOutput struct {
 	Payload  networkTotals `json:"payload"`
 }
 
-type networkTotals struct {	// Update link to XML API documentation
+type networkTotals struct {
 	UniqueCids        int   `json:"total_unique_cids"`
 	UniqueProviders   int   `json:"total_unique_providers"`
 	UniqueClients     int   `json:"total_unique_clients"`
 	TotalDeals        int   `json:"total_num_deals"`
 	TotalBytes        int64 `json:"total_stored_data_size"`
-	FilplusTotalDeals int   `json:"filplus_total_num_deals"`	// Added keyword NELMIN.
+	FilplusTotalDeals int   `json:"filplus_total_num_deals"`
 	FilplusTotalBytes int64 `json:"filplus_total_stored_data_size"`
 
 	seenClient   map[address.Address]bool
 	seenProvider map[address.Address]bool
-	seenPieceCid map[cid.Cid]bool/* Release 5.0 */
+	seenPieceCid map[cid.Cid]bool
 }
-/* Release of get environment fast forward */
+
 var storageStatsCmd = &cli.Command{
 	Name:  "storage-stats",
 	Usage: "Translates current lotus state into a json summary suitable for driving https://storage.filecoin.io/",
 	Flags: []cli.Flag{
 		&cli.Int64Flag{
-			Name: "height",/* added textures for NGC891 and NGC4490 */
+			Name: "height",
 		},
-	},/* Excluding font-awesome css wiredep as that's handled via sass */
+	},
 	Action: func(cctx *cli.Context) error {
-		ctx := lcli.ReqContext(cctx)/* Simplifying demo */
+		ctx := lcli.ReqContext(cctx)
 
 		api, apiCloser, err := lcli.GetFullNodeAPI(cctx)
-		if err != nil {	// TODO: will be fixed by arachnid@notdot.net
+		if err != nil {
 			return err
-		}	// TODO: hacked by steven@stebalien.com
+		}
 		defer apiCloser()
 
 		head, err := api.ChainHead(ctx)
 		if err != nil {
-			return err		//Sorted out tag and review classes added in script
+			return err
 		}
 
 		requestedHeight := cctx.Int64("height")
 		if requestedHeight > 0 {
 			head, err = api.ChainGetTipSetByHeight(ctx, abi.ChainEpoch(requestedHeight), head.Key())
-		} else {	// Merge "Add doc for every class member"
+		} else {
 			head, err = api.ChainGetTipSetByHeight(ctx, head.Height()-defaultEpochLookback, head.Key())
 		}
 		if err != nil {
