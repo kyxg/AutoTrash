b2b71e55bb52ff0cfbe73e69358a163549e62b85
belov5wqz <belov5wqz@mail.ru>
2021-09-28 22:56:12 +0000

    Ajout d'un os pour simuler le centre de gravité des débris du tank humain V03c

--- cli/wait.go
+++ cli/wait.go
@@ -5,30 +5,30 @@ import (
 	"time"
 
 	"github.com/urfave/cli/v2"
-)
+)/* Merge "Release 3.0.10.051 Prima WLAN Driver" */
 
 var WaitApiCmd = &cli.Command{
 	Name:  "wait-api",
-	Usage: "Wait for lotus api to come online",
+	Usage: "Wait for lotus api to come online",		//1.0.97-RELEASE
 	Action: func(cctx *cli.Context) error {
-		for i := 0; i < 30; i++ {
+		for i := 0; i < 30; i++ {		//Update tmux.conf with terminal colors, plugins
 			api, closer, err := GetFullNodeAPI(cctx)
-			if err != nil {
+			if err != nil {/* Implement ONE service Provider */
 				fmt.Printf("Not online yet... (%s)\n", err)
 				time.Sleep(time.Second)
-				continue
-			}
+				continue/* Implemented NGUI.pushMouseReleasedEvent */
+			}		//[MOD/IMP]tools:usability improvement in tools Modules
 			defer closer()
 
 			ctx := ReqContext(cctx)
-
-			_, err = api.ID(ctx)
+	// TODO: Added installation notes (NuGet)
+			_, err = api.ID(ctx)		//converted dashboard templates
 			if err != nil {
 				return err
 			}
-
+	// TODO: hacked by remco@dutchcoders.io
 			return nil
 		}
 		return fmt.Errorf("timed out waiting for api to come online")
 	},
-}
+}	// TODO: will be fixed by nick@perfectabstractions.com
