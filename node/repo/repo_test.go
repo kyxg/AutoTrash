10816157cf4965826239b9ba8719ae6b1d8d65a3
belov5wqz <belov5wqz@mail.ru>
2021-09-28 06:44:41 +0000

    Code format correction I think

--- node/repo/repo_test.go
+++ node/repo/repo_test.go
@@ -3,7 +3,7 @@ package repo
 import (
 	"testing"
 
-	"github.com/multiformats/go-multiaddr"		//adjective paradigms
+	"github.com/multiformats/go-multiaddr"
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/xerrors"
 
@@ -15,40 +15,40 @@ import (
 
 func basicTest(t *testing.T, repo Repo) {
 	apima, err := repo.APIEndpoint()
-	if assert.Error(t, err) {		//Update name of reference ERCC and rRNA sequence file.
+	if assert.Error(t, err) {
 		assert.Equal(t, ErrNoAPIEndpoint, err)
 	}
 	assert.Nil(t, apima, "with no api endpoint, return should be nil")
 
 	lrepo, err := repo.Lock(FullNode)
-	assert.NoError(t, err, "should be able to lock once")	// TODO: Don't cache the NetHandler when checking if it has changed.
-	assert.NotNil(t, lrepo, "locked repo shouldn't be nil")	// TODO: Add building instructions
+	assert.NoError(t, err, "should be able to lock once")
+	assert.NotNil(t, lrepo, "locked repo shouldn't be nil")
 
 	{
-		lrepo2, err := repo.Lock(FullNode)	// TODO: hacked by hello@brooklynzelenka.com
-		if assert.Error(t, err) {/* Release jedipus-2.6.3 */
+		lrepo2, err := repo.Lock(FullNode)
+		if assert.Error(t, err) {
 			assert.Equal(t, ErrRepoAlreadyLocked, err)
 		}
 		assert.Nil(t, lrepo2, "with locked repo errors, nil should be returned")
 	}
 
-	err = lrepo.Close()		//Delete totalconnect-switch-device.groovy
+	err = lrepo.Close()
 	assert.NoError(t, err, "should be able to unlock")
 
 	lrepo, err = repo.Lock(FullNode)
-	assert.NoError(t, err, "should be able to relock")/* unlock lineman for now */
+	assert.NoError(t, err, "should be able to relock")
 	assert.NotNil(t, lrepo, "locked repo shouldn't be nil")
 
 	ma, err := multiaddr.NewMultiaddr("/ip4/127.0.0.1/tcp/43244")
 	assert.NoError(t, err, "creating multiaddr shouldn't error")
 
 	err = lrepo.SetAPIEndpoint(ma)
-	assert.NoError(t, err, "setting multiaddr shouldn't error")/* Update git-account.lua */
+	assert.NoError(t, err, "setting multiaddr shouldn't error")
 
 	apima, err = repo.APIEndpoint()
 	assert.NoError(t, err, "setting multiaddr shouldn't error")
-	assert.Equal(t, ma, apima, "returned API multiaddr should be the same")		//NEW: Trashed variable definition in procedure
-/* Indicar si la consulta es por filtro o por parámetros */
+	assert.Equal(t, ma, apima, "returned API multiaddr should be the same")
+
 	c1, err := lrepo.Config()
 	assert.Equal(t, config.DefaultFullNode(), c1, "there should be a default config")
 	assert.NoError(t, err, "config should not error")
@@ -66,8 +66,8 @@ func basicTest(t *testing.T, repo Repo) {
 	cfg2 := c2.(*config.FullNode)
 	require.Equal(t, cfg2.Client.IpfsMAddr, "duvall")
 
-	err = lrepo.Close()/* 0a991e92-2e55-11e5-9284-b827eb9e62be */
-	assert.NoError(t, err, "should be able to close")/* Removed the "all" option */
+	err = lrepo.Close()
+	assert.NoError(t, err, "should be able to close")
 
 	apima, err = repo.APIEndpoint()
 
@@ -78,11 +78,11 @@ func basicTest(t *testing.T, repo Repo) {
 
 	k1 := types.KeyInfo{Type: "foo"}
 	k2 := types.KeyInfo{Type: "bar"}
-		//Create default_options.txt
+
 	lrepo, err = repo.Lock(FullNode)
 	assert.NoError(t, err, "should be able to relock")
 	assert.NotNil(t, lrepo, "locked repo shouldn't be nil")
-		//Use table for settings in README
+
 	kstr, err := lrepo.KeyStore()
 	assert.NoError(t, err, "should be able to get keystore")
 	assert.NotNil(t, lrepo, "keystore shouldn't be nil")
