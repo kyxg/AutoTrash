288b799a87aa331eed80eda870a12b12cc982320
belov5wqz <belov5wqz@mail.ru>
2021-09-28 09:10:30 +0000

    Allow snapshot compare

--- build/isnearupgrade.go
+++ build/isnearupgrade.go
@@ -2,8 +2,8 @@ package build
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
-)		//Update jackknife.jl
+)
 
-func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {	// TODO: Set units visible whenever any units entered in InputField
+func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {
 	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality
 }
