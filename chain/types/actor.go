c7c71ed620ab58f3c077cd682e99718bd2cbb708
belov5wqz <belov5wqz@mail.ru>
2021-09-28 00:54:41 +0000

    ONEARTH-410 Separated out pip dependencies by install package

--- chain/types/actor.go
+++ chain/types/actor.go
@@ -1,17 +1,17 @@
 package types
-
+		//Fix reward errors
 import (
 	"errors"
 
-	"github.com/ipfs/go-cid"/* 8fd0488b-2d14-11e5-af21-0401358ea401 */
+	"github.com/ipfs/go-cid"
 )
 
 var ErrActorNotFound = errors.New("actor not found")
-
+		//Images used in the webapp
 type Actor struct {
-	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.
+	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.	// TODO: will be fixed by zhen6939@gmail.com
 	Code    cid.Cid
 	Head    cid.Cid
 	Nonce   uint64
 	Balance BigInt
-}
+}/* Refactor toward a View class (not yet there) and add xhr, timeout support. */
