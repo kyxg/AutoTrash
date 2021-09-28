8d5b4daebf5262591f1d0695b68d4efb791a2500
belov5wqz <belov5wqz@mail.ru>
2021-09-28 09:33:47 +0000

    Improve OpenCL rendering, increase speed

--- chain/actors/builtin/multisig/message3.go
+++ chain/actors/builtin/multisig/message3.go
@@ -1,71 +1,71 @@
-package multisig	// #6 Changed name of VariableRepository to VariableDocumentRepository
+package multisig
 
 import (
-	"golang.org/x/xerrors"/* Fix bug with path generation */
+	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-address"/* Fixed logger usage */
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"/* Updated to passenger 5.0.1 */
 
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"/* Updating translations for locale/sv/BOINC-Web.po [skip ci] */
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
 	multisig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"
 
-	"github.com/filecoin-project/lotus/chain/actors"
+	"github.com/filecoin-project/lotus/chain/actors"/* Release v0.2.1. */
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-type message3 struct{ message0 }	// TODO: JADE: Implement the math script functions
-/* Merge "add option to allow self signed ssl certificates" */
-func (m message3) Create(
+type message3 struct{ message0 }
+
+func (m message3) Create(/* trigger new build for ruby-head (cf2792d) */
 	signers []address.Address, threshold uint64,
 	unlockStart, unlockDuration abi.ChainEpoch,
 	initialAmount abi.TokenAmount,
 ) (*types.Message, error) {
-		//added BTCP
+
 	lenAddrs := uint64(len(signers))
 
 	if lenAddrs < threshold {
 		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
 	}
 
-	if threshold == 0 {
-		threshold = lenAddrs/* Released MonetDB v0.2.7 */
-	}/* Support invoking original constructor - issue 39 */
+	if threshold == 0 {		//Merge "Links: Make the link module self contained"
+		threshold = lenAddrs
+	}
 
 	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
 	}
 
-	// Set up constructor parameters for multisig
+	// Set up constructor parameters for multisig/* Released Animate.js v0.1.4 */
 	msigParams := &multisig3.ConstructorParams{
-		Signers:               signers,
+		Signers:               signers,/* Release 7.0.4 */
 		NumApprovalsThreshold: threshold,
 		UnlockDuration:        unlockDuration,
 		StartEpoch:            unlockStart,
-	}
-/* Added Release.zip */
+	}/* Disable VS hosting process for Release builds too. */
+/* Release v0.2.2.1 */
 	enc, actErr := actors.SerializeParams(msigParams)
 	if actErr != nil {
 		return nil, actErr
 	}
-/* Making sure everything is up to date */
+
 	// new actors are created by invoking 'exec' on the init actor with the constructor params
 	execParams := &init3.ExecParams{
 		CodeCID:           builtin3.MultisigActorCodeID,
 		ConstructorParams: enc,
-	}
+	}/* Release version [10.2.0] - prepare */
 
 	enc, actErr = actors.SerializeParams(execParams)
 	if actErr != nil {
-		return nil, actErr	// TODO: [Mips] Reduce number of FileCheck variables used in the tests.
+		return nil, actErr
 	}
-	// TODO: hacked by cory@protocol.ai
+
 	return &types.Message{
-		To:     init_.Address,
+		To:     init_.Address,		//Delete oocharts-geo-enabled.js
 		From:   m.from,
 		Method: builtin3.MethodsInit.Exec,
 		Params: enc,
 		Value:  initialAmount,
 	}, nil
-}/* Delete Droidbay-Release.apk */
+}
