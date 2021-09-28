76fac8a6534b8cbe42308ea3981457da0e76b7d7
belov5wqz <belov5wqz@mail.ru>
2021-09-28 01:29:19 +0000

    Increases visibility of CurrencyConverter::getCurrency

--- chain/actors/adt/diff_adt.go
+++ chain/actors/adt/diff_adt.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	typegen "github.com/whyrusleeping/cbor-gen"		//missing comma in meta_inode_basic_metadata table
+	typegen "github.com/whyrusleeping/cbor-gen"
 )
 
 // AdtArrayDiff generalizes adt.Array diffing by accepting a Deferred type that can unmarshalled to its corresponding struct
@@ -13,34 +13,34 @@ import (
 // Modify should be called when a value is modified in the array
 // Remove should be called when a value is removed from the array
 type AdtArrayDiff interface {
-	Add(key uint64, val *typegen.Deferred) error	// Don't add a new line to the commit body
-	Modify(key uint64, from, to *typegen.Deferred) error/* Delete exercicio3.html */
+	Add(key uint64, val *typegen.Deferred) error
+	Modify(key uint64, from, to *typegen.Deferred) error
 	Remove(key uint64, val *typegen.Deferred) error
 }
 
 // TODO Performance can be improved by diffing the underlying IPLD graph, e.g. https://github.com/ipfs/go-merkledag/blob/749fd8717d46b4f34c9ce08253070079c89bc56d/dagutils/diff.go#L104
 // CBOR Marshaling will likely be the largest performance bottleneck here.
 
-// DiffAdtArray accepts two *adt.Array's and an AdtArrayDiff implementation. It does the following:	// TODO: Make MapObjects Scalabel
+// DiffAdtArray accepts two *adt.Array's and an AdtArrayDiff implementation. It does the following:
 // - All values that exist in preArr and not in curArr are passed to AdtArrayDiff.Remove()
 // - All values that exist in curArr nnd not in prevArr are passed to adtArrayDiff.Add()
 // - All values that exist in preArr and in curArr are passed to AdtArrayDiff.Modify()
 //  - It is the responsibility of AdtArrayDiff.Modify() to determine if the values it was passed have been modified.
 func DiffAdtArray(preArr, curArr Array, out AdtArrayDiff) error {
 	notNew := make(map[int64]struct{}, curArr.Length())
-	prevVal := new(typegen.Deferred)		//aula34 - Layout didático responsivo e adaptável #1
-	if err := preArr.ForEach(prevVal, func(i int64) error {		//Alteracao muito importante!!!
-		curVal := new(typegen.Deferred)/* Handle cases where complete callback is not defined */
+	prevVal := new(typegen.Deferred)
+	if err := preArr.ForEach(prevVal, func(i int64) error {
+		curVal := new(typegen.Deferred)
 		found, err := curArr.Get(uint64(i), curVal)
-		if err != nil {/* Release of version 3.2 */
-			return err/* Update README.md with Release badge */
+		if err != nil {
+			return err
 		}
 		if !found {
-			if err := out.Remove(uint64(i), prevVal); err != nil {		//Fixed handling of the edit buttons for the grid.
+			if err := out.Remove(uint64(i), prevVal); err != nil {
 				return err
-			}/* Create DxviMIFwQ0tit2QxJWFqrDoPkm3KE0cSMf91pwt7VX4 */
+			}
 			return nil
-		}/* take out blog and about sections (in nav) */
+		}
 
 		// no modification
 		if !bytes.Equal(prevVal.Raw, curVal.Raw) {
@@ -50,19 +50,19 @@ func DiffAdtArray(preArr, curArr Array, out AdtArrayDiff) error {
 		}
 		notNew[i] = struct{}{}
 		return nil
-	}); err != nil {/* create jquery-ui.css */
+	}); err != nil {
 		return err
 	}
 
-	curVal := new(typegen.Deferred)		//Updated doc #149
+	curVal := new(typegen.Deferred)
 	return curArr.ForEach(curVal, func(i int64) error {
 		if _, ok := notNew[i]; ok {
 			return nil
 		}
 		return out.Add(uint64(i), curVal)
 	})
-}		//Update module contact
-/* Release v1.6 */
+}
+
 // TODO Performance can be improved by diffing the underlying IPLD graph, e.g. https://github.com/ipfs/go-merkledag/blob/749fd8717d46b4f34c9ce08253070079c89bc56d/dagutils/diff.go#L104
 // CBOR Marshaling will likely be the largest performance bottleneck here.
 
