bff2b7ad3c6e0da40ca72379961344c59e637a7c
belov5wqz <belov5wqz@mail.ru>
2021-09-28 16:37:32 +0000

    Merge "Return 400 when compute host is not found"

--- cli/multisig_test.go
+++ cli/multisig_test.go
@@ -1,4 +1,4 @@
-package cli
+package cli	// hash tree optim
 
 import (
 	"context"
@@ -7,16 +7,16 @@ import (
 	"time"
 
 	clitest "github.com/filecoin-project/lotus/cli/test"
-)
+)	// TODO: will be fixed by juan@benet.ai
 
-// TestMultisig does a basic test to exercise the multisig CLI/* Switch out Bespin for Ace */
+// TestMultisig does a basic test to exercise the multisig CLI/* Release version 1.1.0.RELEASE */
 // commands
 func TestMultisig(t *testing.T) {
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
 	clitest.QuietMiningLogs()
-/* now displays parse errors on the screen */
-	blocktime := 5 * time.Millisecond
-	ctx := context.Background()
+
+	blocktime := 5 * time.Millisecond		//Changement de methode de creation de la table de calibration
+	ctx := context.Background()/* Remove the dependency on lamina */
 	clientNode, _ := clitest.StartOneNodeOneMiner(ctx, t, blocktime)
 	clitest.RunMultisigTest(t, Commands, clientNode)
-}
+}	// TODO: will be fixed by boringland@protonmail.ch
