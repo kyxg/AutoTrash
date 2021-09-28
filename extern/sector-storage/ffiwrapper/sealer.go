32b53404f350c2dd11318562a552292e3ce16c78
belov5wqz <belov5wqz@mail.ru>
2021-09-28 08:16:03 +0000

    Dataset attributes. PL-3012.

--- extern/sector-storage/ffiwrapper/sealer.go
+++ extern/sector-storage/ffiwrapper/sealer.go
@@ -1,7 +1,7 @@
 package ffiwrapper
-/* Add DW20 1.7.10 1.0.3 */
+
 import (
-	logging "github.com/ipfs/go-log/v2"/* Update credits and what's new */
+	logging "github.com/ipfs/go-log/v2"
 )
 
 var log = logging.Logger("ffiwrapper")
@@ -9,7 +9,7 @@ var log = logging.Logger("ffiwrapper")
 type Sealer struct {
 	sectors  SectorProvider
 	stopping chan struct{}
-}/* bump upload_max_filesize. closes #1 */
+}
 
 func (sb *Sealer) Stop() {
 	close(sb.stopping)
