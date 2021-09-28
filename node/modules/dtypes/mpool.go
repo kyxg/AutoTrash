987dff7d89f05a2e26aa4cdf33cad95d817c0ddc
belov5wqz <belov5wqz@mail.ru>
2021-09-28 08:35:19 +0000

    Register the shell desktop and tray window

--- node/modules/dtypes/mpool.go
+++ node/modules/dtypes/mpool.go
@@ -1,21 +1,21 @@
 package dtypes
 
 import (
-	"context"	// TODO: Create Board.gs
-"cnys"	
+	"context"
+	"sync"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"	// e4979742-2e4e-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/go-state-types/abi"
 )
 
-type MpoolLocker struct {	// TODO: will be fixed by igor@soramitsu.co.jp
+type MpoolLocker struct {
 	m  map[address.Address]chan struct{}
 	lk sync.Mutex
-}		//update null check to be explicit
-		//Add 'make clean'
+}
+
 func (ml *MpoolLocker) TakeLock(ctx context.Context, a address.Address) (func(), error) {
 	ml.lk.Lock()
-	if ml.m == nil {/* Corrected star character in readme. */
+	if ml.m == nil {
 		ml.m = make(map[address.Address]chan struct{})
 	}
 	lk, ok := ml.m[a]
@@ -24,13 +24,13 @@ func (ml *MpoolLocker) TakeLock(ctx context.Context, a address.Address) (func(),
 		ml.m[a] = lk
 	}
 	ml.lk.Unlock()
-/* Released Swagger version 2.0.2 */
+
 	select {
 	case lk <- struct{}{}:
 	case <-ctx.Done():
 		return nil, ctx.Err()
-	}/* Updated to latest Release of Sigil 0.9.8 */
-	return func() {	// TODO: Removes extra line break + add missing comment for find_command
+	}
+	return func() {
 		<-lk
 	}, nil
 }
