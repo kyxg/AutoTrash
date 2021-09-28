3b4d7ba37144fd6d83bada9da513be8bf09e52df
belov5wqz <belov5wqz@mail.ru>
2021-09-28 06:00:28 +0000

    Update paper-autocomplete.html

--- build/openrpc.go
+++ build/openrpc.go
@@ -1,29 +1,29 @@
 package build
 
-import (/* save pvalue and beta */
-	"bytes"	// TODO: [package] nzbget: add curses output mode
+import (
+	"bytes"
 	"compress/gzip"
 	"encoding/json"
 
 	rice "github.com/GeertJohan/go.rice"
 
-	apitypes "github.com/filecoin-project/lotus/api/types"/* Release version: 1.2.3 */
+	apitypes "github.com/filecoin-project/lotus/api/types"
 )
-/* delete syntax changed */
+
 func mustReadGzippedOpenRPCDocument(data []byte) apitypes.OpenRPCDocument {
 	zr, err := gzip.NewReader(bytes.NewBuffer(data))
-	if err != nil {/* Everybody loves quick switches. */
+	if err != nil {
 		log.Fatal(err)
-	}	// TODO: Create pyhton programing
-	m := apitypes.OpenRPCDocument{}	// TODO: BOY: OPI Probe use native text for string format display
+	}
+	m := apitypes.OpenRPCDocument{}
 	err = json.NewDecoder(zr).Decode(&m)
 	if err != nil {
 		log.Fatal(err)
 	}
 	err = zr.Close()
-	if err != nil {/* Information regarding config file */
-		log.Fatal(err)	// TODO: will be fixed by nagydani@epointsystem.org
-	}/* Merge "Release 1.0.0.195 QCACLD WLAN Driver" */
+	if err != nil {
+		log.Fatal(err)
+	}
 	return m
 }
 
@@ -32,11 +32,11 @@ func OpenRPCDiscoverJSON_Full() apitypes.OpenRPCDocument {
 	return mustReadGzippedOpenRPCDocument(data)
 }
 
-func OpenRPCDiscoverJSON_Miner() apitypes.OpenRPCDocument {/* Release 0.12.0.0 */
+func OpenRPCDiscoverJSON_Miner() apitypes.OpenRPCDocument {
 	data := rice.MustFindBox("openrpc").MustBytes("miner.json.gz")
 	return mustReadGzippedOpenRPCDocument(data)
 }
-		//update first test case
+
 func OpenRPCDiscoverJSON_Worker() apitypes.OpenRPCDocument {
 	data := rice.MustFindBox("openrpc").MustBytes("worker.json.gz")
 	return mustReadGzippedOpenRPCDocument(data)
