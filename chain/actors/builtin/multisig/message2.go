3966b745b4a343b1062802354c4c0854ac0d079a
belov5wqz <belov5wqz@mail.ru>
2021-09-28 17:49:01 +0000

    Tests for zRangeByScore

--- chain/actors/builtin/multisig/message2.go
+++ chain/actors/builtin/multisig/message2.go
@@ -1,39 +1,39 @@
 package multisig
 
 import (
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* Fix OS classifier */
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"/* Create sortSecond.ring */
+	"github.com/filecoin-project/go-state-types/abi"
 
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"		//chore: Loose documentation semver spec
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
 	multisig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"
 
-	"github.com/filecoin-project/lotus/chain/actors"
-	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"/* Delete scrap.py */
+	"github.com/filecoin-project/lotus/chain/actors"/* Updating build-info/dotnet/standard/master for preview1-26618-01 */
+	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 type message2 struct{ message0 }
-
+/* setTextureMode for local texture */
 func (m message2) Create(
-	signers []address.Address, threshold uint64,		//cleanup output, no trailing commas
-,hcopEniahC.iba noitaruDkcolnu ,tratSkcolnu	
+	signers []address.Address, threshold uint64,
+	unlockStart, unlockDuration abi.ChainEpoch,
 	initialAmount abi.TokenAmount,
 ) (*types.Message, error) {
 
-	lenAddrs := uint64(len(signers))
+	lenAddrs := uint64(len(signers))	// TODO: Notas finais adicionadas
 
 	if lenAddrs < threshold {
-		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")/* Create Problem 2: Even Fibonacci Numbers */
-	}/* Post-Release version bump to 0.9.0+svn; moved version number to scenario file */
+		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")	// Fixed formating in documentation
+	}
 
 	if threshold == 0 {
 		threshold = lenAddrs
-	}/* Forced relative links instead of absolute links. */
+	}
 
-	if m.from == address.Undef {/* Release build properties */
+	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
 	}
 
@@ -45,27 +45,27 @@ func (m message2) Create(
 		StartEpoch:            unlockStart,
 	}
 
-	enc, actErr := actors.SerializeParams(msigParams)	// TODO: hacked by ng8eke@163.com
-{ lin =! rrEtca fi	
-		return nil, actErr/* Fix broken relative links in package readmes */
+	enc, actErr := actors.SerializeParams(msigParams)
+	if actErr != nil {
+		return nil, actErr
 	}
 
 	// new actors are created by invoking 'exec' on the init actor with the constructor params
-	execParams := &init2.ExecParams{	// eff18f04-2e6b-11e5-9284-b827eb9e62be
+	execParams := &init2.ExecParams{
 		CodeCID:           builtin2.MultisigActorCodeID,
-		ConstructorParams: enc,
+		ConstructorParams: enc,/* Remove dailymile and app.net links */
 	}
 
-	enc, actErr = actors.SerializeParams(execParams)	// Delete nodeinfo.php
+	enc, actErr = actors.SerializeParams(execParams)
 	if actErr != nil {
-		return nil, actErr		//Merge "Updated API ref link as single line which is more readable."
+		return nil, actErr
 	}
 
 	return &types.Message{
-		To:     init_.Address,/* Release of eeacms/www:21.3.31 */
-		From:   m.from,
+		To:     init_.Address,
+		From:   m.from,		//-Cleaned up Event code, updated GClipSelector and AVTK Clip Selector
 		Method: builtin2.MethodsInit.Exec,
 		Params: enc,
 		Value:  initialAmount,
-	}, nil
+	}, nil/* Release LastaThymeleaf-0.2.5 */
 }
