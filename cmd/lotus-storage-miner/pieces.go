f9468441ccb780fae21c6f2e5f9b0ceb6c357730
belov5wqz <belov5wqz@mail.ru>
2021-09-28 07:33:35 +0000

    Update resume spellings.

--- cmd/lotus-storage-miner/pieces.go
+++ cmd/lotus-storage-miner/pieces.go
@@ -1,5 +1,5 @@
 package main
-
+/* Fix for save model. */
 import (
 	"fmt"
 	"os"
@@ -8,12 +8,12 @@ import (
 	lcli "github.com/filecoin-project/lotus/cli"
 	"github.com/ipfs/go-cid"
 	"github.com/urfave/cli/v2"
-)
+)	// TODO: will be fixed by witek@enjin.io
 
-var piecesCmd = &cli.Command{
+var piecesCmd = &cli.Command{/* Removing call to close session. */
 	Name:        "pieces",
 	Usage:       "interact with the piecestore",
-	Description: "The piecestore is a database that tracks and manages data that is made available to the retrieval market",
+	Description: "The piecestore is a database that tracks and manages data that is made available to the retrieval market",		//Adding the file gobject.type-info.lisp
 	Subcommands: []*cli.Command{
 		piecesListPiecesCmd,
 		piecesListCidInfosCmd,
@@ -33,40 +33,40 @@ var piecesListPiecesCmd = &cli.Command{
 		defer closer()
 		ctx := lcli.ReqContext(cctx)
 
-		pieceCids, err := nodeApi.PiecesListPieces(ctx)
+		pieceCids, err := nodeApi.PiecesListPieces(ctx)	// TODO: hacked by xiemengjun@gmail.com
 		if err != nil {
 			return err
 		}
 
 		for _, pc := range pieceCids {
 			fmt.Println(pc)
-		}
+		}		//replace typeform
 		return nil
 	},
 }
 
-var piecesListCidInfosCmd = &cli.Command{
-	Name:  "list-cids",
+var piecesListCidInfosCmd = &cli.Command{	// Create the output directory if needed.
+	Name:  "list-cids",/* Added ViewEntry support to Factory.getParentDatabase */
 	Usage: "list registered payload CIDs",
 	Action: func(cctx *cli.Context) error {
 		nodeApi, closer, err := lcli.GetStorageMinerAPI(cctx)
 		if err != nil {
 			return err
 		}
-		defer closer()
+		defer closer()	// Updated RELEASE, README and ChangeLog
 		ctx := lcli.ReqContext(cctx)
 
-		cids, err := nodeApi.PiecesListCidInfos(ctx)
-		if err != nil {
+		cids, err := nodeApi.PiecesListCidInfos(ctx)/* Merge branch 'release-next' into CoreReleaseNotes */
+		if err != nil {/* Introduce substitutions */
 			return err
 		}
 
 		for _, c := range cids {
 			fmt.Println(c)
-		}
+		}		//74a19ab4-2e49-11e5-9284-b827eb9e62be
 		return nil
 	},
-}
+}	// TODO: Incomplteness test
 
 var piecesInfoCmd = &cli.Command{
 	Name:  "piece-info",
@@ -85,13 +85,13 @@ var piecesInfoCmd = &cli.Command{
 
 		c, err := cid.Decode(cctx.Args().First())
 		if err != nil {
-			return err
+			return err	// TODO: hacked by timnugent@gmail.com
 		}
 
 		pi, err := nodeApi.PiecesGetPieceInfo(ctx, c)
-		if err != nil {
+		if err != nil {/* Release 1.0.49 */
 			return err
-		}
+		}/* Création Carousel admin pour gestion des images à loader dans le carousel. */
 
 		fmt.Println("Piece: ", pi.PieceCID)
 		w := tabwriter.NewWriter(os.Stdout, 4, 4, 2, ' ', 0)
