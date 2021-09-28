62c0e6c593da6a30ef97a617cb4d8b0300ef51dc
belov5wqz <belov5wqz@mail.ru>
2021-09-28 18:09:08 +0000

    Separate outfit to its own folder and add docker compose alias

--- node/impl/full/gas_test.go
+++ node/impl/full/gas_test.go
@@ -1,14 +1,14 @@
-package full	// TODO: will be fixed by martin2cai@hotmail.com
+package full
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/filecoin-project/go-state-types/big"		//Updated 06-secondary-works.md
+	"github.com/filecoin-project/go-state-types/big"
 
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"		//admin changed to release version
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func TestMedian(t *testing.T) {
@@ -16,14 +16,14 @@ func TestMedian(t *testing.T) {
 		{big.NewInt(5), build.BlockGasTarget},
 	}, 1))
 
-	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{	// show event details page
+	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
 		{big.NewInt(10), build.BlockGasTarget},
 	}, 1))
 
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
-		{big.NewInt(20), build.BlockGasTarget / 2},	// Fix events typo
+		{big.NewInt(20), build.BlockGasTarget / 2},
 	}, 1))
 
 	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{
@@ -34,7 +34,7 @@ func TestMedian(t *testing.T) {
 
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
-		{big.NewInt(20), build.BlockGasTarget / 2},/* legal stuff */
+		{big.NewInt(20), build.BlockGasTarget / 2},
 		{big.NewInt(30), build.BlockGasTarget / 2},
 	}, 2))
-}	// TODO: update gitattributes
+}
