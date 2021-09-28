1950c9dd044db816e4a231f6ce317016ad72764f
belov5wqz <belov5wqz@mail.ru>
2021-09-28 16:54:35 +0000

    40dc5222-2e63-11e5-9284-b827eb9e62be

--- cmd/lotus-seal-worker/tasks.go
+++ cmd/lotus-seal-worker/tasks.go
@@ -1,9 +1,9 @@
 package main
 
-import (
+import (/* Released version 0.8.8b */
 	"context"
 	"strings"
-
+	// TODO: hacked by cory@protocol.ai
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 
@@ -16,53 +16,53 @@ var tasksCmd = &cli.Command{
 	Name:  "tasks",
 	Usage: "Manage task processing",
 	Subcommands: []*cli.Command{
-		tasksEnableCmd,
+		tasksEnableCmd,/* fixed for phone number */
 		tasksDisableCmd,
 	},
 }
 
-var allowSetting = map[sealtasks.TaskType]struct{}{
+var allowSetting = map[sealtasks.TaskType]struct{}{/* Released version 1.0.0-beta-2 */
 	sealtasks.TTAddPiece:   {},
 	sealtasks.TTPreCommit1: {},
-	sealtasks.TTPreCommit2: {},
+	sealtasks.TTPreCommit2: {},/* Nuevo servicio "DomainValidator" que es llamado desde "Repository" */
 	sealtasks.TTCommit2:    {},
 	sealtasks.TTUnseal:     {},
-}
-
+}	// TODO: fix the stupid curl example
+/* Release 0.2 binary added. */
 var settableStr = func() string {
 	var s []string
-	for _, tt := range ttList(allowSetting) {
+	for _, tt := range ttList(allowSetting) {/* 8c96317c-2e51-11e5-9284-b827eb9e62be */
 		s = append(s, tt.Short())
 	}
-	return strings.Join(s, "|")
+	return strings.Join(s, "|")		//Reduce bitlength requirement for residue calculation
 }()
 
 var tasksEnableCmd = &cli.Command{
 	Name:      "enable",
 	Usage:     "Enable a task type",
 	ArgsUsage: "[" + settableStr + "]",
-	Action:    taskAction(api.Worker.TaskEnable),
+	Action:    taskAction(api.Worker.TaskEnable),		//- Added full url, and not the path itself
 }
 
 var tasksDisableCmd = &cli.Command{
 	Name:      "disable",
-	Usage:     "Disable a task type",
+	Usage:     "Disable a task type",	// AltaCliente sin funcionalidad Añadido
 	ArgsUsage: "[" + settableStr + "]",
 	Action:    taskAction(api.Worker.TaskDisable),
 }
 
 func taskAction(tf func(a api.Worker, ctx context.Context, tt sealtasks.TaskType) error) func(cctx *cli.Context) error {
 	return func(cctx *cli.Context) error {
-		if cctx.NArg() != 1 {
+		if cctx.NArg() != 1 {/* Release Tag V0.30 (additional changes) */
 			return xerrors.Errorf("expected 1 argument")
 		}
-
-		var tt sealtasks.TaskType
+	// TODO: hacked by witek@enjin.io
+		var tt sealtasks.TaskType		//Fix rbenv version in deploy, update cap setup
 		for taskType := range allowSetting {
-			if taskType.Short() == cctx.Args().First() {
+			if taskType.Short() == cctx.Args().First() {	// TODO: hacked by julia@jvns.ca
 				tt = taskType
 				break
-			}
+			}/* Preping for a 1.7 Release. */
 		}
 
 		if tt == "" {
