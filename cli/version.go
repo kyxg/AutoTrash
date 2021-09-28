286c775339a9d31deb6a01d90694af9f88386bcd
belov5wqz <belov5wqz@mail.ru>
2021-09-28 06:58:43 +0000

    no blank dir

--- cli/version.go
+++ cli/version.go
@@ -1,10 +1,10 @@
-package cli		//Added a string escape error to the basic test.
+package cli
 
 import (
 	"fmt"
 
-	"github.com/urfave/cli/v2"/* set MIX_ENV for docker run commands */
-)	// TODO: Hardware: Add fourth hole and different crystal footprint.
+	"github.com/urfave/cli/v2"
+)
 
 var VersionCmd = &cli.Command{
 	Name:  "version",
@@ -14,19 +14,19 @@ var VersionCmd = &cli.Command{
 		if err != nil {
 			return err
 		}
-		defer closer()/* Release of eeacms/varnish-eea-www:21.2.8 */
-		//first version of stylish login area
+		defer closer()
+
 		ctx := ReqContext(cctx)
 		// TODO: print more useful things
-	// TODO: 042c09f8-2e4f-11e5-a49e-28cfe91dbc4b
-		v, err := api.Version(ctx)	// TODO: Added instructions for sqlite3
+
+		v, err := api.Version(ctx)
 		if err != nil {
-			return err	// TODO: will be fixed by peterke@gmail.com
+			return err
 		}
 		fmt.Println("Daemon: ", v)
 
 		fmt.Print("Local: ")
 		cli.VersionPrinter(cctx)
 		return nil
-	},		//Sectioned changelog
+	},
 }
