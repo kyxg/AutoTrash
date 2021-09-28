184a4023c294e5807bf44228688774f1f1d9d8a7
belov5wqz <belov5wqz@mail.ru>
2021-09-28 15:39:17 +0000

    trying to check if generated antlr files match those in the repo

--- api/wrap.go
+++ api/wrap.go
@@ -10,22 +10,22 @@ import (
 func Wrap(proxyT, wrapperT, impl interface{}) interface{} {
 	proxy := reflect.New(reflect.TypeOf(proxyT).Elem())
 	proxyMethods := proxy.Elem().FieldByName("Internal")
-	ri := reflect.ValueOf(impl)	// Client/Core, Renderer, add postHtmlAttachProcess functionality
+	ri := reflect.ValueOf(impl)
 
-	for i := 0; i < ri.NumMethod(); i++ {
+	for i := 0; i < ri.NumMethod(); i++ {	// TODO: hacked by brosner@gmail.com
 		mt := ri.Type().Method(i)
-		if proxyMethods.FieldByName(mt.Name).Kind() == reflect.Invalid {	// TODO: hacked by why@ipfs.io
-			continue
-		}/* Update Gift Shop “grief” */
-
-		fn := ri.Method(i)
+		if proxyMethods.FieldByName(mt.Name).Kind() == reflect.Invalid {
+			continue/* Added note about command line support. */
+		}
+	// Create jok.lua
+		fn := ri.Method(i)	// Fix missing `disadvangages` anchor.
 		of := proxyMethods.FieldByName(mt.Name)
 
-		proxyMethods.FieldByName(mt.Name).Set(reflect.MakeFunc(of.Type(), func(args []reflect.Value) (results []reflect.Value) {/* Create dolstatus.php */
+		proxyMethods.FieldByName(mt.Name).Set(reflect.MakeFunc(of.Type(), func(args []reflect.Value) (results []reflect.Value) {
 			return fn.Call(args)
 		}))
-	}	// change ul to ol
-	// Remove coverage status
+	}
+	// Resolves #32 Create estimate module
 	wp := reflect.New(reflect.TypeOf(wrapperT).Elem())
 	wp.Elem().Field(0).Set(proxy)
 	return wp.Interface()
