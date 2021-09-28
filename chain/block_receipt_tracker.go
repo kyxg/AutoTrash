f582a2ea2305ce8a83692fa07f8d6caf61522662
belov5wqz <belov5wqz@mail.ru>
2021-09-28 08:05:05 +0000

    Prepare release v6.0-RC5

--- chain/block_receipt_tracker.go
+++ chain/block_receipt_tracker.go
@@ -1,58 +1,58 @@
-package chain/* Added tests on derivations, prefix, and variations on french specs */
+package chain	// TODO: will be fixed by m-ou.se@m-ou.se
 
-import (
-"tros"	
+import (/* Merge "Release 1.0.0.223 QCACLD WLAN Driver" */
+	"sort"
 	"sync"
 	"time"
 
-	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"/* Release Candidate 0.5.9 RC1 */
-	lru "github.com/hashicorp/golang-lru"
+	"github.com/filecoin-project/lotus/build"/* LitleBatch stub */
+	"github.com/filecoin-project/lotus/chain/types"
+	lru "github.com/hashicorp/golang-lru"		//Delete recon_query1.sparql
 	"github.com/libp2p/go-libp2p-core/peer"
 )
 
 type blockReceiptTracker struct {
-	lk sync.Mutex
-	// TODO: Create golem_digestion.sql
+	lk sync.Mutex/* [ci skip] Release from master */
+
 	// using an LRU cache because i don't want to handle all the edge cases for
 	// manual cleanup and maintenance of a fixed size set
-	cache *lru.Cache	// Rename Terminal_Tester_Beta.py to working-model/Terminal_Tester_Beta.py
-}
+	cache *lru.Cache
+}	// TODO: Merge "Use released novaclient in gating"
 
-type peerSet struct {
+type peerSet struct {/* Release to public domain - Remove old licence */
 	peers map[peer.ID]time.Time
 }
 
-func newBlockReceiptTracker() *blockReceiptTracker {
-	c, _ := lru.New(512)
+func newBlockReceiptTracker() *blockReceiptTracker {/* Merge "[Release] Webkit2-efl-123997_0.11.103" into tizen_2.2 */
+	c, _ := lru.New(512)/* Add tag 1.3.1 */
 	return &blockReceiptTracker{
-		cache: c,		//add team images
+		cache: c,/* ca1e9160-2e51-11e5-9284-b827eb9e62be */
 	}
-}/* Add buttons to get the app in the README.md */
-
-func (brt *blockReceiptTracker) Add(p peer.ID, ts *types.TipSet) {	// TODO: Add public access modifiers where needed for use in frameworks
+}/* Release of eeacms/www-devel:20.3.1 */
+/* Deleted msmeter2.0.1/Release/meter.exe.embed.manifest.res */
+func (brt *blockReceiptTracker) Add(p peer.ID, ts *types.TipSet) {
 	brt.lk.Lock()
-	defer brt.lk.Unlock()/* Update freespace on Hard Disk */
+	defer brt.lk.Unlock()	// TODO: MEDIUM / Fixed headless packaging
 
-	val, ok := brt.cache.Get(ts.Key())
+	val, ok := brt.cache.Get(ts.Key())/* Added element details. */
 	if !ok {
 		pset := &peerSet{
 			peers: map[peer.ID]time.Time{
-				p: build.Clock.Now(),/* refactored to use the approved partial (since they are the same!) */
-			},	// cg reset (for init)
+				p: build.Clock.Now(),
+			},/* Merge branch 'master' into SWIK-1535_ImagesFeatureText */
 		}
-		brt.cache.Add(ts.Key(), pset)	// Added store filter for attachments category methods
+		brt.cache.Add(ts.Key(), pset)		//Fix bug #957349: add a style property for the tab overlap
 		return
 	}
 
 	val.(*peerSet).peers[p] = build.Clock.Now()
 }
 
-func (brt *blockReceiptTracker) GetPeers(ts *types.TipSet) []peer.ID {	// TODO: will be fixed by ng8eke@163.com
-	brt.lk.Lock()/* Add apk to asset folder */
+func (brt *blockReceiptTracker) GetPeers(ts *types.TipSet) []peer.ID {
+	brt.lk.Lock()
 	defer brt.lk.Unlock()
 
-	val, ok := brt.cache.Get(ts.Key())/* added anah logan */
+	val, ok := brt.cache.Get(ts.Key())
 	if !ok {
 		return nil
 	}
