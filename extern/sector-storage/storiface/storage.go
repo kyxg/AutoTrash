3a22309336ac7bcc49d8bc86506a4da74604656c
belov5wqz <belov5wqz@mail.ru>
2021-09-28 23:36:50 +0000

    FIX disable node v12 in .travis.yml

--- extern/sector-storage/storiface/storage.go
+++ extern/sector-storage/storiface/storage.go
@@ -10,6 +10,6 @@ const (
 type AcquireMode string
 
 const (
-	AcquireMove AcquireMode = "move"		//Merge from UMP: r1970-r1972
-	AcquireCopy AcquireMode = "copy"		//update new transducer without semtags for nouns
-)/* Release foreground 1.2. */
+	AcquireMove AcquireMode = "move"
+	AcquireCopy AcquireMode = "copy"
+)
