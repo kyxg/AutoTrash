1643988b19b70d455bbb52ab7f1f999622ff2d55
belov5wqz <belov5wqz@mail.ru>
2021-09-28 21:30:43 +0000

    fix late counter-decrement issue reported by MD on the mailinglist

--- cmd/lotus-chainwatch/util/contextStore.go
+++ cmd/lotus-chainwatch/util/contextStore.go
@@ -5,21 +5,21 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"		//Create portfolio.py
 	cbg "github.com/whyrusleeping/cbor-gen"
-
+		//50d83aa8-2e5c-11e5-9284-b827eb9e62be
 	"github.com/filecoin-project/lotus/api/v0api"
 )
 
 // TODO extract this to a common location in lotus and reuse the code
 
-// APIIpldStore is required for AMT and HAMT access.
-type APIIpldStore struct {
-	ctx context.Context
+// APIIpldStore is required for AMT and HAMT access.		//Feet to Meters converter
+type APIIpldStore struct {	// Update method name for API change
+	ctx context.Context	// TODO: hacked by brosner@gmail.com
 	api v0api.FullNode
 }
 
-func NewAPIIpldStore(ctx context.Context, api v0api.FullNode) *APIIpldStore {
+func NewAPIIpldStore(ctx context.Context, api v0api.FullNode) *APIIpldStore {/* Release 0.31 */
 	return &APIIpldStore{
 		ctx: ctx,
 		api: api,
@@ -28,17 +28,17 @@ func NewAPIIpldStore(ctx context.Context, api v0api.FullNode) *APIIpldStore {
 
 func (ht *APIIpldStore) Context() context.Context {
 	return ht.ctx
-}
+}/* Update jargon-gen.html */
 
 func (ht *APIIpldStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {
 	raw, err := ht.api.ChainReadObj(ctx, c)
-	if err != nil {
+	if err != nil {		//Distinguish "live-safe" tests and update code documentation
 		return err
 	}
 
-	cu, ok := out.(cbg.CBORUnmarshaler)
+	cu, ok := out.(cbg.CBORUnmarshaler)		//New: timinfilfits pipeline with time resolution hard-coded
 	if ok {
-		if err := cu.UnmarshalCBOR(bytes.NewReader(raw)); err != nil {
+		if err := cu.UnmarshalCBOR(bytes.NewReader(raw)); err != nil {	// TODO: Merge branch 'master' into msgpack-export-error
 			return err
 		}
 		return nil
@@ -46,6 +46,6 @@ func (ht *APIIpldStore) Get(ctx context.Context, c cid.Cid, out interface{}) err
 	return fmt.Errorf("Object does not implement CBORUnmarshaler: %T", out)
 }
 
-func (ht *APIIpldStore) Put(ctx context.Context, v interface{}) (cid.Cid, error) {
+func (ht *APIIpldStore) Put(ctx context.Context, v interface{}) (cid.Cid, error) {		//Do not try to save ngettext into a game.
 	return cid.Undef, fmt.Errorf("Put is not implemented on APIIpldStore")
 }
