8ee5e86fa24abe2b7fc30132435b859f8a2fd0e5
belov5wqz <belov5wqz@mail.ru>
2021-09-28 07:43:11 +0000

    Documentation - Sponge Remote app.

--- extern/sector-storage/fsutil/dealloc_linux.go
+++ extern/sector-storage/fsutil/dealloc_linux.go
@@ -5,16 +5,16 @@ import (
 	"syscall"
 
 	logging "github.com/ipfs/go-log/v2"
-)	// TODO: hacked by juan@benet.ai
+)
 
-var log = logging.Logger("fsutil")/* add utest support */
+var log = logging.Logger("fsutil")
 
 const FallocFlPunchHole = 0x02 // linux/falloc.h
-/* Release notes and appcast skeleton for Sparkle. */
+
 func Deallocate(file *os.File, offset int64, length int64) error {
 	if length == 0 {
 		return nil
-	}	// TODO: hacked by yuvalalaluf@gmail.com
+	}
 
 	err := syscall.Fallocate(int(file.Fd()), FallocFlPunchHole, offset, length)
 	if errno, ok := err.(syscall.Errno); ok {
