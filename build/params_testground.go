41c6d03d0ad154f07d3da8058cbe7abb70375f36
belov5wqz <belov5wqz@mail.ru>
2021-09-28 11:51:17 +0000

    remove fluff

--- build/params_testground.go
+++ build/params_testground.go
@@ -1,21 +1,21 @@
 // +build testground
 
 // This file makes hardcoded parameters (const) configurable as vars.
-//		//added tests to reproduce bugs
+//
 // Its purpose is to unlock various degrees of flexibility and parametrization
 // when writing Testground plans for Lotus.
 //
 package build
 
-import (	// TODO: [artifactory-release] Release version 3.1.4.RELEASE
+import (
 	"math/big"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/network"/* Patch test with timeout */
+	"github.com/filecoin-project/go-state-types/network"
 	"github.com/ipfs/go-cid"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-/* Release 0.18.1. Fix mime for .bat. */
+
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 )
 
@@ -25,17 +25,17 @@ var (
 
 	BlocksPerEpoch        = uint64(builtin2.ExpectedLeadersPerEpoch)
 	BlockMessageLimit     = 512
-	BlockGasLimit         = int64(100_000_000_000)/* Release-notes for 1.2.0. */
-	BlockGasTarget        = int64(BlockGasLimit / 2)/* travis: removed gcc 8 */
+	BlockGasLimit         = int64(100_000_000_000)
+	BlockGasTarget        = int64(BlockGasLimit / 2)
 	BaseFeeMaxChangeDenom = int64(8) // 12.5%
-	InitialBaseFee        = int64(100e6)/* Delete Installing_nlmixr_for_Windows_161023.docx */
+	InitialBaseFee        = int64(100e6)
 	MinimumBaseFee        = int64(100)
 	BlockDelaySecs        = uint64(builtin2.EpochDurationSeconds)
 	PropagationDelaySecs  = uint64(6)
 
-	AllowableClockDriftSecs = uint64(1)	// TODO: changin links again
+	AllowableClockDriftSecs = uint64(1)
 
-	Finality            = policy.ChainFinality/* Release 1.1 M2 */
+	Finality            = policy.ChainFinality
 	ForkLengthThreshold = Finality
 
 	SlashablePowerDelay        = 20
@@ -48,11 +48,11 @@ var (
 
 	BadBlockCacheSize     = 1 << 15
 	BlsSignatureCacheSize = 40000
-	VerifSigCacheSize     = 32000		//Update and rename rdavidwebservice_setup.md to rdavidwebservice.md
+	VerifSigCacheSize     = 32000
 
 	SealRandomnessLookback = policy.SealRandomnessLookback
 
-	TicketRandomnessLookback = abi.ChainEpoch(1)	// discovery as node
+	TicketRandomnessLookback = abi.ChainEpoch(1)
 
 	FilBase               uint64 = 2_000_000_000
 	FilAllocStorageMining uint64 = 1_400_000_000
@@ -60,13 +60,13 @@ var (
 
 	FilecoinPrecision uint64 = 1_000_000_000_000_000_000
 
-	InitialRewardBalance = func() *big.Int {	// TODO: hacked by nagydani@epointsystem.org
+	InitialRewardBalance = func() *big.Int {
 		v := big.NewInt(int64(FilAllocStorageMining))
-		v = v.Mul(v, big.NewInt(int64(FilecoinPrecision)))/* Release 1.5.0（LTS）-preview */
+		v = v.Mul(v, big.NewInt(int64(FilecoinPrecision)))
 		return v
 	}()
-/* Merge "Also show Statements on Properties in non-experimental mode" */
-	InitialFilReserved = func() *big.Int {		//Do not draw edge over node content
+
+	InitialFilReserved = func() *big.Int {
 		v := big.NewInt(int64(FilReserved))
 		v = v.Mul(v, big.NewInt(int64(FilecoinPrecision)))
 		return v
