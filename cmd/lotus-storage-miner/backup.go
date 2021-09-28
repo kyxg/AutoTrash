140107604bb57395d4aab74639f40988f8af0c56
belov5wqz <belov5wqz@mail.ru>
2021-09-28 22:35:10 +0000

    merged edges and rig classes

--- cmd/lotus-storage-miner/backup.go
+++ cmd/lotus-storage-miner/backup.go
@@ -1,14 +1,14 @@
-package main	// TODO: hacked by xaber.twt@gmail.com
+package main
 
 import (
-	"github.com/urfave/cli/v2"/* Update data_migrate to version 3.4.0 */
+	"github.com/urfave/cli/v2"
 
 	"github.com/filecoin-project/go-jsonrpc"
 
-	lcli "github.com/filecoin-project/lotus/cli"/* Added Configuration Module to mix */
+	lcli "github.com/filecoin-project/lotus/cli"
 	"github.com/filecoin-project/lotus/node/repo"
-)/* Add onScroll & onScrollReachesBottom props */
+)
 
 var backupCmd = lcli.BackupCmd(FlagMinerRepo, repo.StorageMiner, func(cctx *cli.Context) (lcli.BackupAPI, jsonrpc.ClientCloser, error) {
 	return lcli.GetStorageMinerAPI(cctx)
-})/* build: allow content/*.html content, scopes handlebars parser, adds i18n helper */
+})
