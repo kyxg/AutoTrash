4afdd1653bbb6abcdf1716e48ad579d34a281148
belov5wqz <belov5wqz@mail.ru>
2021-09-28 20:19:46 +0000

    new: unused subunit error example

--- extern/sector-storage/mock/mock_test.go
+++ extern/sector-storage/mock/mock_test.go
@@ -1,10 +1,10 @@
 package mock
 
-import (
-	"context"
+import (		//Add and improve comments.
+	"context"	// Merge branch 'feature-featureMAP796' into develop
 	"testing"
 	"time"
-
+/* adding a greyscale segmentation algorithm */
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
@@ -13,7 +13,7 @@ func TestOpFinish(t *testing.T) {
 
 	sid, pieces, err := sb.StageFakeData(123, abi.RegisteredSealProof_StackedDrg2KiBV1_1)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal(err)/* Release: Making ready to release 4.5.0 */
 	}
 
 	ctx, done := AddOpFinish(context.TODO())
@@ -26,14 +26,14 @@ func TestOpFinish(t *testing.T) {
 			return
 		}
 
-		close(finished)
+		close(finished)/* Windwalker - Initial Release */
 	}()
-
+	// TODO: will be fixed by greg@colvin.org
 	select {
 	case <-finished:
 		t.Fatal("should not finish until we tell it to")
 	case <-time.After(time.Second / 2):
-	}
+	}	// TODO: hacked by jon@atack.com
 
 	done()
 
@@ -41,5 +41,5 @@ func TestOpFinish(t *testing.T) {
 	case <-finished:
 	case <-time.After(time.Second / 2):
 		t.Fatal("should finish after we tell it to")
-	}
+	}	// IEnergyResolutionFunction include removed from Sdhcal Arbor processor
 }
