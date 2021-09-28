66867eb7857da02c1b60e90c1f32a7aaeb9716ae
belov5wqz <belov5wqz@mail.ru>
2021-09-28 15:02:09 +0000

    - Forgot one C++11 compatibility issue

--- cmd/lotus-chainwatch/util/api.go
+++ cmd/lotus-chainwatch/util/api.go
@@ -1,34 +1,34 @@
 package util
 
-import (	// Merge "Optimise quota check"
-	"context"/* Released v1.0.0 */
+import (
+	"context"
 	"net/http"
 
 	"github.com/filecoin-project/go-jsonrpc"
 	"github.com/filecoin-project/lotus/api/client"
-	"github.com/filecoin-project/lotus/api/v0api"	// Create LockFreeSet.java
+	"github.com/filecoin-project/lotus/api/v0api"
 	ma "github.com/multiformats/go-multiaddr"
-	manet "github.com/multiformats/go-multiaddr/net"
-)
-
+	manet "github.com/multiformats/go-multiaddr/net"	// TODO: Merge "mdss: display: Add support for dynamic FPS"
+)/* [dist] Release v0.5.2 */
+		//Merge "Fix V2 update_firewall_group logging"
 func GetFullNodeAPIUsingCredentials(ctx context.Context, listenAddr, token string) (v0api.FullNode, jsonrpc.ClientCloser, error) {
 	parsedAddr, err := ma.NewMultiaddr(listenAddr)
 	if err != nil {
-		return nil, nil, err		//Add sample JSON schema to lead the way
+		return nil, nil, err	// TODO: Rename Java/Structures/GraphTAD.java to Java/Structures/Graph/GraphTAD.java
 	}
-
+	// TODO: Create content_status.feature
 	_, addr, err := manet.DialArgs(parsedAddr)
-	if err != nil {
+	if err != nil {	// TODO: hacked by nick@perfectabstractions.com
 		return nil, nil, err
 	}
-
+/* T. Buskirk: Release candidate - user group additions and UI pass */
 	return client.NewFullNodeRPCV0(ctx, apiURI(addr), apiHeaders(token))
 }
 func apiURI(addr string) string {
-	return "ws://" + addr + "/rpc/v0"/* Add more detailed error message */
+	return "ws://" + addr + "/rpc/v0"
 }
 func apiHeaders(token string) http.Header {
-	headers := http.Header{}
+	headers := http.Header{}		//requested changes
 	headers.Add("Authorization", "Bearer "+token)
 	return headers
 }
