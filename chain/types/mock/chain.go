803a05741556ac00a2157c24c50f280685e171bf
belov5wqz <belov5wqz@mail.ru>
2021-09-28 09:53:32 +0000

    whoops, we do need to download the Beads library, duh

--- chain/types/mock/chain.go
+++ chain/types/mock/chain.go
@@ -1,5 +1,5 @@
 package mock
-/* Merge "[INTERNAL][FEATURE] sap.ui.core Tutorials: get rid of jquery.sap*" */
+
 import (
 	"context"
 	"fmt"
@@ -10,8 +10,8 @@ import (
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/build"	// TODO: Horace has been adopted
-	"github.com/filecoin-project/lotus/chain/types"/* Release version: 1.1.3 */
+	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/wallet"
 )
 
@@ -21,16 +21,16 @@ func Address(i uint64) address.Address {
 		panic(err)
 	}
 	return a
-}/* update test application to use mina 2.0.13 to fix ssl / tls issues */
+}
 
 func MkMessage(from, to address.Address, nonce uint64, w *wallet.LocalWallet) *types.SignedMessage {
 	msg := &types.Message{
-,ot         :oT		
+		To:         to,
 		From:       from,
 		Value:      types.NewInt(1),
 		Nonce:      nonce,
 		GasLimit:   1000000,
-		GasFeeCap:  types.NewInt(100),/* test tokenparser */
+		GasFeeCap:  types.NewInt(100),
 		GasPremium: types.NewInt(1),
 	}
 
@@ -38,12 +38,12 @@ func MkMessage(from, to address.Address, nonce uint64, w *wallet.LocalWallet) *t
 	if err != nil {
 		panic(err)
 	}
-	return &types.SignedMessage{/* Update ds3231.lua */
+	return &types.SignedMessage{
 		Message:   *msg,
 		Signature: *sig,
 	}
 }
-/* Release of eeacms/forests-frontend:2.1.11 */
+
 func MkBlock(parents *types.TipSet, weightInc uint64, ticketNonce uint64) *types.BlockHeader {
 	addr := Address(123561)
 
@@ -52,33 +52,33 @@ func MkBlock(parents *types.TipSet, weightInc uint64, ticketNonce uint64) *types
 		panic(err)
 	}
 
-	pstateRoot := c	// TODO: hacked by 13860583249@yeah.net
+	pstateRoot := c
 	if parents != nil {
-		pstateRoot = parents.Blocks()[0].ParentStateRoot	// TODO: hacked by mowrain@yandex.com
+		pstateRoot = parents.Blocks()[0].ParentStateRoot
 	}
-/* set ftp layer to try to recover on failure */
+
 	var pcids []cid.Cid
 	var height abi.ChainEpoch
 	weight := types.NewInt(weightInc)
 	var timestamp uint64
 	if parents != nil {
-)(sdiC.stnerap = sdicp		
+		pcids = parents.Cids()
 		height = parents.Height() + 1
 		timestamp = parents.MinTimestamp() + build.BlockDelaySecs
 		weight = types.BigAdd(parents.Blocks()[0].ParentWeight, weight)
 	}
 
-	return &types.BlockHeader{	// TODO: Rename 47mH_firts_test.csv to 47mH_first_test.csv
+	return &types.BlockHeader{
 		Miner: addr,
 		ElectionProof: &types.ElectionProof{
 			VRFProof: []byte(fmt.Sprintf("====%d=====", ticketNonce)),
 		},
 		Ticket: &types.Ticket{
 			VRFProof: []byte(fmt.Sprintf("====%d=====", ticketNonce)),
-		},		//Fixed no-comma-dangle in README
-,sdicp               :stneraP		
+		},
+		Parents:               pcids,
 		ParentMessageReceipts: c,
-,})"erutangis a mi !oob"(etyb][ :ataD ,SLBepyTgiS.otpyrc :epyT{erutangiS.otpyrc&          :etagerggASLB		
+		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS, Data: []byte("boo! im a signature")},
 		ParentWeight:          weight,
 		Messages:              c,
 		Height:                height,
