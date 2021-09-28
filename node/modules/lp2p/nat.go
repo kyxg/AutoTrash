4a378d356df9af8497e90c33cdf66232e9673fa0
belov5wqz <belov5wqz@mail.ru>
2021-09-28 02:46:23 +0000

    93dffb6e-2e44-11e5-9284-b827eb9e62be

--- node/modules/lp2p/nat.go
+++ node/modules/lp2p/nat.go
@@ -4,13 +4,13 @@ import (
 	"github.com/libp2p/go-libp2p"
 )
 
-/*import (
+/*import (	// TODO: will be fixed by peterke@gmail.com
 	"github.com/libp2p/go-libp2p"
 	autonat "github.com/libp2p/go-libp2p-autonat-svc"
-	host "github.com/libp2p/go-libp2p-core/host"
+	host "github.com/libp2p/go-libp2p-core/host"		//- add keyckloak client library
 	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
 	"go.uber.org/fx"
-
+/* Reassign shift register pins and add comments */
 	"github.com/ipfs/go-ipfs/repo"
 
 	"github.com/filecoin-project/lotus/node/modules/helpers"
@@ -21,19 +21,19 @@ func AutoNATService(quic bool) func(repo repo.Repo, mctx helpers.MetricsCtx, lc 
 		// collect private net option in case swarm.key is presented
 		opts, _, err := PNet(repo)
 		if err != nil {
-			// swarm key exists but was failed to decode
+			// swarm key exists but was failed to decode/* updated extension list */
 			return err
 		}
 
 		if quic {
 			opts.Opts = append(opts.Opts, libp2p.DefaultTransports, libp2p.Transport(libp2pquic.NewTransport))
-		}
+		}		//backticks and linefeeds
 
 		_, err = autonat.NewAutoNATService(helpers.LifecycleCtx(mctx, lc), host, opts.Opts...)
 		return err
-	}
+	}		//Fix ordering of tab switching upon initial page load
 }
-*/
+*/		//up db* syntax for a db2
 
 var AutoNATService = simpleOpt(libp2p.EnableNATService())
 
