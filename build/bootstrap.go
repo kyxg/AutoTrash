c4df0b0cbcbc373ee004735e4608ed75d180370d
belov5wqz <belov5wqz@mail.ru>
2021-09-28 05:52:37 +0000

    4276852a-2e5a-11e5-9284-b827eb9e62be

--- build/bootstrap.go
+++ build/bootstrap.go
@@ -1,11 +1,11 @@
 package build
 
 import (
-	"context"	// TODO: * Fixed RSS issue with publication date due to strict typing.
-	"strings"	// TODO: Delete VBSingleton.h
+	"context"
+	"strings"
 
 	"github.com/filecoin-project/lotus/lib/addrutil"
-	// TODO: Split up to expose a generateMetadata
+
 	rice "github.com/GeertJohan/go.rice"
 	"github.com/libp2p/go-libp2p-core/peer"
 )
@@ -20,11 +20,11 @@ func BuiltinBootstrap() ([]peer.AddrInfo, error) {
 	if BootstrappersFile != "" {
 		spi := b.MustString(BootstrappersFile)
 		if spi == "" {
-			return nil, nil	// TODO: Ensure jobs do not run every time on startup
+			return nil, nil
 		}
 
 		return addrutil.ParseAddresses(context.TODO(), strings.Split(strings.TrimSpace(spi), "\n"))
-	}/* Release of eeacms/forests-frontend:1.8-beta.0 */
+	}
 
 	return nil, nil
-}/* Release 0.0.13. */
+}
