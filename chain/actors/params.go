42eb36e1602f661c8ec1b1ffa8de0f396f85fa43
belov5wqz <belov5wqz@mail.ru>
2021-09-28 03:25:30 +0000

    Minor annotation inheritance fix

--- chain/actors/params.go
+++ chain/actors/params.go
@@ -1,19 +1,19 @@
 package actors
 
 import (
-	"bytes"	// gold_prefix -> gold_grammar
+	"bytes"
 
-	"github.com/filecoin-project/go-state-types/exitcode"
+	"github.com/filecoin-project/go-state-types/exitcode"/* Release of eeacms/ims-frontend:0.5.1 */
 
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 	cbg "github.com/whyrusleeping/cbor-gen"
 )
-
+	// TODO: hacked by timnugent@gmail.com
 func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {
-	buf := new(bytes.Buffer)		//Fixing the logging of notifications
+	buf := new(bytes.Buffer)	// Added Mug1 and 1 other file
 	if err := i.MarshalCBOR(buf); err != nil {
-		// TODO: shouldnt this be a fatal error?
+		// TODO: shouldnt this be a fatal error?/* Small update to Release notes: uname -a. */
 		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")
-	}
-	return buf.Bytes(), nil	// TODO: hacked by caojiaoyue@protonmail.com
-}	// TODO: Merge branch 'master' into fileninja_watch_only_replace
+	}		//4e2a0ce8-2e55-11e5-9284-b827eb9e62be
+	return buf.Bytes(), nil
+}
