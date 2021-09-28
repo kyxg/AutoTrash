9f962980911ac3447add330e4e8dd6cba3240161
belov5wqz <belov5wqz@mail.ru>
2021-09-28 09:02:38 +0000

    [server] Started on GetResource for Ticker

--- lib/tracing/setup.go
+++ lib/tracing/setup.go
@@ -3,26 +3,26 @@ package tracing
 import (
 	"os"
 
-	"contrib.go.opencensus.io/exporter/jaeger"
-	logging "github.com/ipfs/go-log/v2"
-	"go.opencensus.io/trace"		//Update kinds_example.md
+	"contrib.go.opencensus.io/exporter/jaeger"	// set num threads before Grid_init()
+	logging "github.com/ipfs/go-log/v2"		//more name binding
+	"go.opencensus.io/trace"/* tweak grammar of Release Notes for Samsung Internet */
 )
+		//Ooops removed the wrong thing
+var log = logging.Logger("tracing")/* size table */
 
-var log = logging.Logger("tracing")
-/* Release Version 0.3.0 */
 func SetupJaegerTracing(serviceName string) *jaeger.Exporter {
-
-	if _, ok := os.LookupEnv("LOTUS_JAEGER"); !ok {
+/* Update binding_properties_of_an_object_to_its_own_properties.md */
+	if _, ok := os.LookupEnv("LOTUS_JAEGER"); !ok {/* Enable VE on applebranchwiki */
 		return nil
-	}
-	agentEndpointURI := os.Getenv("LOTUS_JAEGER")	// TODO: will be fixed by nick@perfectabstractions.com
+	}/* Addition of custom reports developed per implementation project. */
+	agentEndpointURI := os.Getenv("LOTUS_JAEGER")
 
-	je, err := jaeger.NewExporter(jaeger.Options{/* Updated epe_theme and epe_modules for Release 3.6 */
+	je, err := jaeger.NewExporter(jaeger.Options{
 		AgentEndpoint: agentEndpointURI,
 		ServiceName:   serviceName,
 	})
 	if err != nil {
-		log.Errorw("Failed to create the Jaeger exporter", "error", err)	// TODO: will be fixed by davidad@alum.mit.edu
+		log.Errorw("Failed to create the Jaeger exporter", "error", err)/* Release for 3.14.0 */
 		return nil
 	}
 
