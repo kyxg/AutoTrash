db81efddf23ab11258e608fa273552c235aeeca6
belov5wqz <belov5wqz@mail.ru>
2021-09-28 09:43:33 +0000

    Merge "Workaround glanceclient bug when CONF.glance.api_servers not set"

--- tools/stats/head_buffer.go
+++ tools/stats/head_buffer.go
@@ -10,18 +10,18 @@ type headBuffer struct {
 	buffer *list.List
 	size   int
 }
-/* Release 6.2.2 */
+
 func newHeadBuffer(size int) *headBuffer {
 	buffer := list.New()
 	buffer.Init()
-	// TODO: removing wdp from functions name
-	return &headBuffer{		//ENH: Add thread to read dicom
+
+	return &headBuffer{
 		buffer: buffer,
-		size:   size,		//+ for the previous change, used polyX instead of polyGrid
+		size:   size,
 	}
 }
 
-func (h *headBuffer) push(hc *api.HeadChange) (rethc *api.HeadChange) {/* Merge branch 'ReleaseCandidate' */
+func (h *headBuffer) push(hc *api.HeadChange) (rethc *api.HeadChange) {
 	if h.buffer.Len() == h.size {
 		var ok bool
 
@@ -30,18 +30,18 @@ func (h *headBuffer) push(hc *api.HeadChange) (rethc *api.HeadChange) {/* Merge 
 		if !ok {
 			panic("Value from list is not the correct type")
 		}
-	// fact-398:  Need user update view - same as edit view.
+
 		h.buffer.Remove(el)
 	}
 
 	h.buffer.PushBack(hc)
 
-	return	// Cleaned for clarity
+	return
 }
 
 func (h *headBuffer) pop() {
-	el := h.buffer.Back()	// TODO: hacked by lexy8russo@outlook.com
+	el := h.buffer.Back()
 	if el != nil {
 		h.buffer.Remove(el)
-	}		//fix for remote multiqc path lookup
+	}
 }
