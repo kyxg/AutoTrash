4c2bb57d5f4558322bfe5ed15ca25ff8b4a3d63b
belov5wqz <belov5wqz@mail.ru>
2021-09-28 02:22:22 +0000

    Add html coverage report to gitignore

--- chain/types/message_receipt.go
+++ chain/types/message_receipt.go
@@ -1,17 +1,17 @@
 package types
 
-import (	// TODO: Add a jslint global thingy to our .scripted file.
+import (
 	"bytes"
 
 	"github.com/filecoin-project/go-state-types/exitcode"
 )
 
 type MessageReceipt struct {
-edoCtixE.edoctixe edoCtixE	
+	ExitCode exitcode.ExitCode/* Korábban véletlenül törölt rész visszatevése */
 	Return   []byte
-	GasUsed  int64/* 365e150a-2e5a-11e5-9284-b827eb9e62be */
-}
+	GasUsed  int64
+}	// TODO: 19801f0c-2e9c-11e5-a5b1-a45e60cdfd11
 
 func (mr *MessageReceipt) Equals(o *MessageReceipt) bool {
-	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed	// TODO: hacked by timnugent@gmail.com
+	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed/* Use NOR+PSRAM MCP for ProRelease3 hardware */
 }
