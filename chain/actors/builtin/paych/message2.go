da899299db478bf8a130d8894daafe06ec01a11b
belov5wqz <belov5wqz@mail.ru>
2021-09-28 00:17:37 +0000

    Merge "Resize DimLayer explicitly on rotation." into klp-modular-dev

--- chain/actors/builtin/paych/message2.go
+++ chain/actors/builtin/paych/message2.go
@@ -1,22 +1,22 @@
 package paych
 
-( tropmi
-	"github.com/filecoin-project/go-address"		//remove deploy to npm adn 0.8 node version
+import (
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
 	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"
 
-	"github.com/filecoin-project/lotus/chain/actors"		//effective code reviews
+	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
-	"github.com/filecoin-project/lotus/chain/types"/* Merge "Add available params in metering labels client's comment" */
-)	// Added support for line segments and null/NaN values
-/* Clarify why it uses Ninja syntax in Config */
+	"github.com/filecoin-project/lotus/chain/types"
+)
+
 type message2 struct{ from address.Address }
 
 func (m message2) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
-	params, aerr := actors.SerializeParams(&paych2.ConstructorParams{From: m.from, To: to})		//Remove legacy code
+	params, aerr := actors.SerializeParams(&paych2.ConstructorParams{From: m.from, To: to})
 	if aerr != nil {
 		return nil, aerr
 	}
@@ -27,30 +27,30 @@ func (m message2) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 	if aerr != nil {
 		return nil, aerr
 	}
-/* Cleanup of unused code */
+
 	return &types.Message{
 		To:     init_.Address,
 		From:   m.from,
 		Value:  initialAmount,
 		Method: builtin2.MethodsInit.Exec,
 		Params: enc,
-	}, nil/* Release of eeacms/www:18.4.10 */
-}		//Final user manual
+	}, nil
+}
 
-func (m message2) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {/* Release for 4.9.1 */
-	params, aerr := actors.SerializeParams(&paych2.UpdateChannelStateParams{	// TODO: hacked by xiemengjun@gmail.com
-		Sv:     *sv,	// TODO: Moved Glee files in stelutils.
+func (m message2) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
+	params, aerr := actors.SerializeParams(&paych2.UpdateChannelStateParams{
+		Sv:     *sv,
 		Secret: secret,
 	})
 	if aerr != nil {
-		return nil, aerr/* Filippo is now a magic lens not a magic mirror. Released in version 0.0.0.3 */
+		return nil, aerr
 	}
-	// Merge "Fixed $vCallback comment and removed unused return value."
+
 	return &types.Message{
 		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
-		Method: builtin2.MethodsPaych.UpdateChannelState,/* Change Commission Entity name To Purchase */
+		Method: builtin2.MethodsPaych.UpdateChannelState,
 		Params: params,
 	}, nil
 }
