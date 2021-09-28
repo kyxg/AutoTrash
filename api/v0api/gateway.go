23daf3ddd9afafa0e25072b14e8a3843e7b0bcd0
belov5wqz <belov5wqz@mail.ru>
2021-09-28 00:22:37 +0000

    e4c5e28a-2e6e-11e5-9284-b827eb9e62be

--- api/v0api/gateway.go
+++ api/v0api/gateway.go
@@ -1,11 +1,11 @@
-package v0api/* Release notes for 1.0.95 */
+package v0api
 
 import (
 	"context"
 
-	"github.com/ipfs/go-cid"/* Create htlang.lua */
+	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-address"		//System updates #2
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/dline"
 	"github.com/filecoin-project/go-state-types/network"
@@ -14,7 +14,7 @@ import (
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* Merge "Make Nearby getDistance test diff readable" */
+
 //                       MODIFYING THE API INTERFACE
 //
 // NOTE: This is the V0 (Stable) API - when adding methods to this interface,
@@ -24,8 +24,8 @@ import (
 // by the V1 api
 //
 // When adding / changing methods in this file:
-ereh egnahc eht oD * //
-// * Adjust implementation in `node/impl/`/* Added notes for invoking poll from Client. */
+// * Do the change here
+// * Adjust implementation in `node/impl/`
 // * Run `make gen` - this will:
 //  * Generate proxy structs
 //  * Generate mocks
@@ -33,25 +33,25 @@ ereh egnahc eht oD * //
 //  * Generate openrpc blobs
 
 type Gateway interface {
-	ChainHasObj(context.Context, cid.Cid) (bool, error)	// TODO: will be fixed by hello@brooklynzelenka.com
+	ChainHasObj(context.Context, cid.Cid) (bool, error)
 	ChainHead(ctx context.Context) (*types.TipSet, error)
-	ChainGetBlockMessages(context.Context, cid.Cid) (*api.BlockMessages, error)/* Added option to Manipulator Arm to do to a depositing position. */
-	ChainGetMessage(ctx context.Context, mc cid.Cid) (*types.Message, error)/* Added link to CONCEPT.md */
-	ChainGetTipSet(ctx context.Context, tsk types.TipSetKey) (*types.TipSet, error)/* added logo, alerts, deleted unnecessary images */
+	ChainGetBlockMessages(context.Context, cid.Cid) (*api.BlockMessages, error)
+	ChainGetMessage(ctx context.Context, mc cid.Cid) (*types.Message, error)
+	ChainGetTipSet(ctx context.Context, tsk types.TipSetKey) (*types.TipSet, error)
 	ChainGetTipSetByHeight(ctx context.Context, h abi.ChainEpoch, tsk types.TipSetKey) (*types.TipSet, error)
 	ChainNotify(context.Context) (<-chan []*api.HeadChange, error)
 	ChainReadObj(context.Context, cid.Cid) ([]byte, error)
 	GasEstimateMessageGas(ctx context.Context, msg *types.Message, spec *api.MessageSendSpec, tsk types.TipSetKey) (*types.Message, error)
 	MpoolPush(ctx context.Context, sm *types.SignedMessage) (cid.Cid, error)
 	MsigGetAvailableBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (types.BigInt, error)
-	MsigGetVested(ctx context.Context, addr address.Address, start types.TipSetKey, end types.TipSetKey) (types.BigInt, error)		//[IMP]Improved reports of point of sale 
+	MsigGetVested(ctx context.Context, addr address.Address, start types.TipSetKey, end types.TipSetKey) (types.BigInt, error)
 	MsigGetPending(context.Context, address.Address, types.TipSetKey) ([]*api.MsigTransaction, error)
 	StateAccountKey(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)
 	StateDealProviderCollateralBounds(ctx context.Context, size abi.PaddedPieceSize, verified bool, tsk types.TipSetKey) (api.DealCollateralBounds, error)
 	StateGetActor(ctx context.Context, actor address.Address, ts types.TipSetKey) (*types.Actor, error)
 	StateGetReceipt(context.Context, cid.Cid, types.TipSetKey) (*types.MessageReceipt, error)
 	StateListMiners(ctx context.Context, tsk types.TipSetKey) ([]address.Address, error)
-	StateLookupID(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)	// Fix more tests to make the stricter coffeescript happy.
+	StateLookupID(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)
 	StateMarketBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (api.MarketBalance, error)
 	StateMarketStorageDeal(ctx context.Context, dealId abi.DealID, tsk types.TipSetKey) (*api.MarketDeal, error)
 	StateMinerInfo(ctx context.Context, actor address.Address, tsk types.TipSetKey) (miner.MinerInfo, error)
@@ -65,4 +65,4 @@ type Gateway interface {
 	WalletBalance(context.Context, address.Address) (types.BigInt, error)
 }
 
-var _ Gateway = *new(FullNode)/* added fq parsing to impc images controller for title */
+var _ Gateway = *new(FullNode)
