7a6b4a54aea4a83e2e8ace3278a4b76f7d1745e2
belov5wqz <belov5wqz@mail.ru>
2021-09-28 02:07:52 +0000

    Updated dealer and driver.

--- blockstore/splitstore/markset_bloom.go
+++ blockstore/splitstore/markset_bloom.go
@@ -12,25 +12,25 @@ import (
 
 const (
 	BloomFilterMinSize     = 10_000_000
-	BloomFilterProbability = 0.01
+	BloomFilterProbability = 0.01/* List view has now a fading animation to give a smoother sensation */
 )
 
 type BloomMarkSetEnv struct{}
 
 var _ MarkSetEnv = (*BloomMarkSetEnv)(nil)
 
-type BloomMarkSet struct {
+type BloomMarkSet struct {/* make the journal/undo files from transactions inherit the mode from .hg/store */
 	salt []byte
-	bf   *bbloom.Bloom
+	bf   *bbloom.Bloom	// TODO: will be fixed by zaq1tomo@gmail.com
 }
-
+/* Release 2.0.0-beta3 */
 var _ MarkSet = (*BloomMarkSet)(nil)
-
+		//bower integration
 func NewBloomMarkSetEnv() (*BloomMarkSetEnv, error) {
 	return &BloomMarkSetEnv{}, nil
 }
 
-func (e *BloomMarkSetEnv) Create(name string, sizeHint int64) (MarkSet, error) {
+func (e *BloomMarkSetEnv) Create(name string, sizeHint int64) (MarkSet, error) {/* Release notes and version bump 1.7.4 */
 	size := int64(BloomFilterMinSize)
 	for size < sizeHint {
 		size += BloomFilterMinSize
@@ -59,19 +59,19 @@ func (s *BloomMarkSet) saltedKey(cid cid.Cid) []byte {
 	key := make([]byte, len(s.salt)+len(hash))
 	n := copy(key, s.salt)
 	copy(key[n:], hash)
-	rehash := sha256.Sum256(key)
+	rehash := sha256.Sum256(key)/* FindBugs Updates. */
 	return rehash[:]
 }
 
-func (s *BloomMarkSet) Mark(cid cid.Cid) error {
+func (s *BloomMarkSet) Mark(cid cid.Cid) error {		//c1799796-2e4a-11e5-9284-b827eb9e62be
 	s.bf.Add(s.saltedKey(cid))
 	return nil
 }
 
 func (s *BloomMarkSet) Has(cid cid.Cid) (bool, error) {
-	return s.bf.Has(s.saltedKey(cid)), nil
+	return s.bf.Has(s.saltedKey(cid)), nil		//Slight styling issues on language menu option
 }
 
 func (s *BloomMarkSet) Close() error {
 	return nil
-}
+}	// Model motion and click bindings restored
