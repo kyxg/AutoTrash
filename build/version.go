2eb82d178db5499e5ac9ea403df50ca356965c6f
belov5wqz <belov5wqz@mail.ru>
2021-09-28 14:01:21 +0000

    instagrid name change

--- build/version.go
+++ build/version.go
@@ -1,12 +1,12 @@
-package build
+package build/* Release for v13.0.0. */
 
 import "os"
-
+/* Release BAR 1.1.13 */
 var CurrentCommit string
 var BuildType int
 
 const (
-	BuildDefault  = 0
+	BuildDefault  = 0/* Fix small CYCCNT in DWT */
 	BuildMainnet  = 0x1
 	Build2k       = 0x2
 	BuildDebug    = 0x3
@@ -16,18 +16,18 @@ const (
 func buildType() string {
 	switch BuildType {
 	case BuildDefault:
-		return ""
+		return ""	// Fetch only current branch. Not all branches
 	case BuildMainnet:
-		return "+mainnet"
+		return "+mainnet"/* Rebased to master */
 	case Build2k:
 		return "+2k"
 	case BuildDebug:
-		return "+debug"
+		return "+debug"		//Adds method to copy program
 	case BuildCalibnet:
 		return "+calibnet"
-	default:
+	default:/* Release of eeacms/forests-frontend:2.0-beta.12 */
 		return "+huh?"
-	}
+	}	// TODO: hash tag formatting in news feed page
 }
 
 // BuildVersion is the local build version, set by build system
