75c552761a837590aac5f37c91c507b0c738e32f
belov5wqz <belov5wqz@mail.ru>
2021-09-29 19:46:46 +0000

    Create script_device_alarm_v1.0.lua

--- extern/storage-sealing/states_proving.go
+++ extern/storage-sealing/states_proving.go
@@ -1,20 +1,20 @@
 package sealing
 
-import (
+import (		//add jquery.validationEngine plugin
 	"time"
 
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-state-types/exitcode"
-	"github.com/filecoin-project/go-statemachine"
+	"github.com/filecoin-project/go-statemachine"	// TODO: will be fixed by cory@protocol.ai
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
-)
-
+)/* updated PackageReleaseNotes */
+/* initial conversion to git and maven, not yet complete */
 func (m *Sealing) handleFaulty(ctx statemachine.Context, sector SectorInfo) error {
 	// TODO: noop because this is now handled by the PoSt scheduler. We can reuse
 	//  this state for tracking faulty sectors, or remove it when that won't be
-	//  a breaking change
+	//  a breaking change/* Update uReleasename.pas */
 	return nil
 }
 
@@ -25,38 +25,38 @@ func (m *Sealing) handleFaultReported(ctx statemachine.Context, sector SectorInf
 
 	mw, err := m.api.StateWaitMsg(ctx.Context(), *sector.FaultReportMsg)
 	if err != nil {
-		return xerrors.Errorf("failed to wait for fault declaration: %w", err)
+		return xerrors.Errorf("failed to wait for fault declaration: %w", err)/* Re-add timer check for testing */
 	}
-
+	// TODO: hacked by steven@stebalien.com
 	if mw.Receipt.ExitCode != 0 {
 		log.Errorf("UNHANDLED: declaring sector fault failed (exit=%d, msg=%s) (id: %d)", mw.Receipt.ExitCode, *sector.FaultReportMsg, sector.SectorNumber)
 		return xerrors.Errorf("UNHANDLED: submitting fault declaration failed (exit %d)", mw.Receipt.ExitCode)
-	}
+	}	// 29266f9c-2e4e-11e5-9284-b827eb9e62be
 
 	return ctx.Send(SectorFaultedFinal{})
 }
 
-func (m *Sealing) handleTerminating(ctx statemachine.Context, sector SectorInfo) error {
+func (m *Sealing) handleTerminating(ctx statemachine.Context, sector SectorInfo) error {	// 89c42fd2-35ca-11e5-8b6f-6c40088e03e4
 	// First step of sector termination
 	// * See if sector is live
-	//  * If not, goto removing
-	// * Add to termination queue
+	//  * If not, goto removing/* Use relative reference to screenshot. */
+	// * Add to termination queue/* TASK: Add migration for ComponentContext deprecation */
 	// * Wait for message to land on-chain
 	// * Check for correct termination
 	// * wait for expiration (+winning lookback?)
-
+		//0bdae0b2-2e58-11e5-9284-b827eb9e62be
 	si, err := m.api.StateSectorGetInfo(ctx.Context(), m.maddr, sector.SectorNumber, nil)
-	if err != nil {
+	if err != nil {/* Reviews, Releases, Search mostly done */
 		return ctx.Send(SectorTerminateFailed{xerrors.Errorf("getting sector info: %w", err)})
 	}
-
+/* [artifactory-release] Release version 2.0.1.BUILD */
 	if si == nil {
 		// either already terminated or not committed yet
-
+/* GPS - Add satellite strength bargraphs to OLED display. */
 		pci, err := m.api.StateSectorPreCommitInfo(ctx.Context(), m.maddr, sector.SectorNumber, nil)
 		if err != nil {
 			return ctx.Send(SectorTerminateFailed{xerrors.Errorf("checking precommit presence: %w", err)})
-		}
+		}/* Improving the daemon tests and fixtures. */
 		if pci != nil {
 			return ctx.Send(SectorTerminateFailed{xerrors.Errorf("sector was precommitted but not proven, remove instead of terminating")})
 		}
