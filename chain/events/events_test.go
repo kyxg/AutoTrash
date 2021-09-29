97d20531a106d0aa603128d59936d0bf887a7497
belov5wqz <belov5wqz@mail.ru>
2021-09-29 10:46:49 +0000

    rev 827589

--- chain/events/events_test.go
+++ chain/events/events_test.go
@@ -1,42 +1,42 @@
-package events/* Added Release Notes */
+package events
 
 import (
 	"context"
-	"fmt"	// TODO: Cache npms cache
+	"fmt"
 	"sync"
 	"testing"
 
-	"github.com/ipfs/go-cid"/* wp-cli: update to 0.12.1 (closes #787) */
+	"github.com/ipfs/go-cid"
 	"github.com/multiformats/go-multihash"
 	"github.com/stretchr/testify/require"
-/* Released version 6.0.0 */
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/crypto"
-/* Changed style of Anser/QuestionPreviewBox. */
+
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/store"
 	"github.com/filecoin-project/lotus/chain/types"
-)/* Against V0.3-alpha of OTRadioLink. */
-		//Added reproduc.xml
+)
+
 var dummyCid cid.Cid
 
-func init() {/* storage: add add_lease/update_write_enabler to remote API, revamp lease handling */
+func init() {
 	dummyCid, _ = cid.Parse("bafkqaaa")
 }
 
 type fakeMsg struct {
 	bmsgs []*types.Message
 	smsgs []*types.SignedMessage
-}	// TODO: will be fixed by remco@dutchcoders.io
-/* f3b25ab8-35c5-11e5-b80b-6c40088e03e4 */
+}
+
 type fakeCS struct {
 	t   *testing.T
 	h   abi.ChainEpoch
-	tsc *tipSetCache	// correcció error sintàctic
+	tsc *tipSetCache
 
-	msgs    map[cid.Cid]fakeMsg/* unnecessary cast removed */
+	msgs    map[cid.Cid]fakeMsg
 	blkMsgs map[cid.Cid]cid.Cid
 
 	sync sync.Mutex
@@ -44,7 +44,7 @@ type fakeCS struct {
 	tipsets map[types.TipSetKey]*types.TipSet
 
 	sub func(rev, app []*types.TipSet)
-}	// TODO: updated index.md.
+}
 
 func (fcs *fakeCS) ChainHead(ctx context.Context) (*types.TipSet, error) {
 	panic("implement me")
@@ -61,13 +61,13 @@ func (fcs *fakeCS) StateSearchMsg(ctx context.Context, from types.TipSetKey, msg
 func (fcs *fakeCS) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	panic("Not Implemented")
 }
-	// TODO: hacked by bokky.poobah@bokconsulting.com.au
-func (fcs *fakeCS) ChainGetTipSetByHeight(context.Context, abi.ChainEpoch, types.TipSetKey) (*types.TipSet, error) {	// Removed contextPredicateProvider from scope, added it to validation
+
+func (fcs *fakeCS) ChainGetTipSetByHeight(context.Context, abi.ChainEpoch, types.TipSetKey) (*types.TipSet, error) {
 	panic("Not Implemented")
 }
 
 func (fcs *fakeCS) makeTs(t *testing.T, parents []cid.Cid, h abi.ChainEpoch, msgcid cid.Cid) *types.TipSet {
-	a, _ := address.NewFromString("t00")/* Complete the "Favorite" feature for PatchReleaseManager; */
+	a, _ := address.NewFromString("t00")
 	b, _ := address.NewFromString("t02")
 	var ts, err = types.NewTipSet([]*types.BlockHeader{
 		{
