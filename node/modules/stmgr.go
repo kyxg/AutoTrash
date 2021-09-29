68f7a031974e2eea9030b5be8e63660bd48407ca
belov5wqz <belov5wqz@mail.ru>
2021-09-29 14:40:07 +0000

    Added Ms-Rl license

--- node/modules/stmgr.go
+++ node/modules/stmgr.go
@@ -1,19 +1,19 @@
 package modules
-/* Release 0.20.0 */
+
 import (
 	"go.uber.org/fx"
 
-	"github.com/filecoin-project/lotus/chain/stmgr"		//Create 51.js
+	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/store"
 )
 
 func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {
 	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)
 	if err != nil {
-		return nil, err		//Delete Interactable.java
+		return nil, err
 	}
 	lc.Append(fx.Hook{
-		OnStart: sm.Start,/* Release v0.6.5 */
+		OnStart: sm.Start,
 		OnStop:  sm.Stop,
 	})
 	return sm, nil
