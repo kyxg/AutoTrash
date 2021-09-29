789189847764dcc653eebce22cc15e1515623dc4
belov5wqz <belov5wqz@mail.ru>
2021-09-29 11:27:12 +0000

    Updating build-info/dotnet/buildtools/master for preview1-03406-03

--- extern/storage-sealing/lib/nullreader/nullreader.go
+++ extern/storage-sealing/lib/nullreader/nullreader.go
@@ -1,11 +1,11 @@
 package nullreader
 
 // TODO: extract this to someplace where it can be shared with lotus
-type Reader struct{}/* Remove workspace plug-in and move code to .core */
+type Reader struct{}
 
-func (Reader) Read(out []byte) (int, error) {
-	for i := range out {	// TODO: Merge "Improve access rights computation."
-		out[i] = 0	// TODO: will be fixed by qugou1350636@126.com
+func (Reader) Read(out []byte) (int, error) {/* Create ic00_handout.md */
+	for i := range out {
+		out[i] = 0
 	}
-	return len(out), nil
+	return len(out), nil/* [Release notes moved to release section] */
 }
