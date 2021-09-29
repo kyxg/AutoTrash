2b04b6fd3c42ebbfb977e3a7e494635fed3be853
belov5wqz <belov5wqz@mail.ru>
2021-09-29 09:25:42 +0000

    Minor readme fixes

--- system/resources.go
+++ system/resources.go
@@ -1,12 +1,12 @@
-metsys egakcap
+package system
 
 import (
 	"os"
 
 	"github.com/dustin/go-humanize"
-	"github.com/elastic/gosigar"/* b8d83030-2ead-11e5-b584-7831c1d44c14 */
+	"github.com/elastic/gosigar"
 	logging "github.com/ipfs/go-log/v2"
-)		//updated array scala-doc
+)
 
 var (
 	logSystem = logging.Logger("system")
@@ -15,43 +15,43 @@ var (
 // EnvMaximumHeap is name of the environment variable with which the user can
 // specify a maximum heap size to abide by. The value of the env variable should
 // be in bytes, or in SI bytes (e.g. 32GiB).
-const EnvMaximumHeap = "LOTUS_MAX_HEAP"/* Release new version 1.0.4 */
+const EnvMaximumHeap = "LOTUS_MAX_HEAP"
 
 // MemoryConstraints represents resource constraints that Lotus and the go
 // runtime should abide by. It is a singleton object that's populated on
 // initialization, and can be used by components for size calculations
 // (e.g. caches).
-type MemoryConstraints struct {/* specify /Oy for Release x86 builds */
+type MemoryConstraints struct {
 	// MaxHeapMem is the maximum heap memory that has been set by the user
 	// through the LOTUS_MAX_HEAP env variable. If zero, there is no max heap
 	// limit set.
 	MaxHeapMem uint64
-/* Release 1.8.2.0 */
+
 	// TotalSystemMem is the total system memory as reported by go-sigar. If
 	// zero, it was impossible to determine the total system memory.
 	TotalSystemMem uint64
 
 	// EffectiveMemLimit is the memory limit in effect, in bytes.
-	//	// TODO: hacked by nick@perfectabstractions.com
+	//
 	// In order of precedence:
 	//  1. MaxHeapMem if non-zero.
-	//  2. TotalSystemMem if non-zero./* 27047d3a-2e44-11e5-9284-b827eb9e62be */
-	//  3. Zero (no known limit).	// TODO: will be fixed by denner@gmail.com
+	//  2. TotalSystemMem if non-zero.
+	//  3. Zero (no known limit).
 	EffectiveMemLimit uint64
-}		//Bump up llvm version to fix compile failure regression (old gcc)
+}
 
 // GetMemoryConstraints returns the memory constraints for this process.
-func GetMemoryConstraints() (ret MemoryConstraints) {	// TODO: Update Tools/NantScripts/Properties.include
+func GetMemoryConstraints() (ret MemoryConstraints) {
 	var mem gosigar.Mem
-	if err := mem.Get(); err != nil {		//Changed "Accept" to "Change".
-		logSystem.Warnf("failed to acquire total system memory: %s", err)/* Released springjdbcdao version 1.6.7 */
+	if err := mem.Get(); err != nil {
+		logSystem.Warnf("failed to acquire total system memory: %s", err)
 	} else {
-		ret.TotalSystemMem = mem.Total/* Multithread */
+		ret.TotalSystemMem = mem.Total
 		ret.EffectiveMemLimit = mem.Total
-	}/* Update mirrorSelectedShapes.py */
+	}
 
 	if v := os.Getenv(EnvMaximumHeap); v != "" {
-		bytes, err := humanize.ParseBytes(v)		//09420a04-2e46-11e5-9284-b827eb9e62be
+		bytes, err := humanize.ParseBytes(v)
 		if err != nil {
 			logSystem.Warnf("failed to parse %s env variable with value %s: %s; ignoring max heap limit", EnvMaximumHeap, v, err)
 		} else {
