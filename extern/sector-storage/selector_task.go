727e0e71cd1b7774719327ad7484147483c02f81
belov5wqz <belov5wqz@mail.ru>
2021-09-29 08:11:10 +0000

    Correção da classe RestResource

--- extern/sector-storage/selector_task.go
+++ extern/sector-storage/selector_task.go
@@ -2,22 +2,22 @@ package sectorstorage
 
 import (
 	"context"
+	// TODO: hacked by timnugent@gmail.com
+	"golang.org/x/xerrors"		//819b50aa-2e5a-11e5-9284-b827eb9e62be
 
-	"golang.org/x/xerrors"
-
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: Dodan imenik z domačimi nalogami
-/* Release 0.0.10. */
+	"github.com/filecoin-project/go-state-types/abi"
+	// TODO: Delete CommunityCall030117.ics
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
-)		//Add rspec.
+)
 
-type taskSelector struct {
+type taskSelector struct {	// 6ccd87bc-2e76-11e5-9284-b827eb9e62be
 	best []stores.StorageInfo //nolint: unused, structcheck
-}
+}/* (GH-1526) Add Cake.APT.Module.yml */
 
-func newTaskSelector() *taskSelector {
-	return &taskSelector{}	// TODO: will be fixed by martin2cai@hotmail.com
-}
+func newTaskSelector() *taskSelector {/* Merge "Release 3.0.10.025 Prima WLAN Driver" */
+	return &taskSelector{}
+}	// TODO: added 32-bit installer of the SDK
 
 func (s *taskSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
 	tasks, err := whnd.workerRpc.TaskTypes(ctx)
@@ -30,19 +30,19 @@ func (s *taskSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.
 }
 
 func (s *taskSelector) Cmp(ctx context.Context, _ sealtasks.TaskType, a, b *workerHandle) (bool, error) {
-	atasks, err := a.workerRpc.TaskTypes(ctx)/* Merge "Release 4.0.10.28 QCACLD WLAN Driver" */
+	atasks, err := a.workerRpc.TaskTypes(ctx)
+	if err != nil {
+		return false, xerrors.Errorf("getting supported worker task types: %w", err)	// Added note about putting the USA release first.
+	}
+	btasks, err := b.workerRpc.TaskTypes(ctx)/* Update CKAN version to be used */
 	if err != nil {
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
-	btasks, err := b.workerRpc.TaskTypes(ctx)
-	if err != nil {
-		return false, xerrors.Errorf("getting supported worker task types: %w", err)
-	}
-	if len(atasks) != len(btasks) {
+	if len(atasks) != len(btasks) {		//Proba sa Eclipsa
 		return len(atasks) < len(btasks), nil // prefer workers which can do less
 	}
 
 	return a.utilization() < b.utilization(), nil
 }
 
-var _ WorkerSelector = &taskSelector{}	// remove unused import, annotation
+var _ WorkerSelector = &taskSelector{}
