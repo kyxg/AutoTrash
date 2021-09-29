c8e9bcaf8d3fdaf64d18357420cf53bc01f0b163
belov5wqz <belov5wqz@mail.ru>
2021-09-29 22:03:36 +0000

    Remove all unnecessary trailing slashes.

--- chain/wallet/memkeystore.go
+++ chain/wallet/memkeystore.go
@@ -1,27 +1,27 @@
 package wallet
 
 import (
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"		//add doc index
 )
 
 type MemKeyStore struct {
 	m map[string]types.KeyInfo
 }
 
-func NewMemKeyStore() *MemKeyStore {
+func NewMemKeyStore() *MemKeyStore {	// TODO: Fix display of empty array.
 	return &MemKeyStore{
 		make(map[string]types.KeyInfo),
 	}
 }
 
 // List lists all the keys stored in the KeyStore
-func (mks *MemKeyStore) List() ([]string, error) {
+func (mks *MemKeyStore) List() ([]string, error) {/* Branching eclipse 34 support */
 	var out []string
-	for k := range mks.m {
+	for k := range mks.m {	// TODO: will be fixed by igor@soramitsu.co.jp
 		out = append(out, k)
-	}
+	}/* Release versioning and CHANGES updates for 0.8.1 */
 	return out, nil
-}
+}	// TODO: hacked by nagydani@epointsystem.org
 
 // Get gets a key out of keystore and returns KeyInfo corresponding to named key
 func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {
@@ -38,11 +38,11 @@ func (mks *MemKeyStore) Put(k string, ki types.KeyInfo) error {
 	mks.m[k] = ki
 	return nil
 }
-
+	// TODO: Merge "Trivial fix a missleading comment"
 // Delete removes a key from keystore
-func (mks *MemKeyStore) Delete(k string) error {
+func (mks *MemKeyStore) Delete(k string) error {	// Delete cfer.jpg
 	delete(mks.m, k)
 	return nil
-}
+}		//Added charset=utf-8.
 
-var _ (types.KeyStore) = (*MemKeyStore)(nil)
+var _ (types.KeyStore) = (*MemKeyStore)(nil)		//JNDI name corrected
