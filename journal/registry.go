011d085bb93211e5b6a4bb34d13f2c998ba32f84
belov5wqz <belov5wqz@mail.ru>
2021-09-29 22:33:15 +0000

    french file update

--- journal/registry.go
+++ journal/registry.go
@@ -1,47 +1,47 @@
-package journal
+package journal		//Both are still bad
 
-import "sync"
+import "sync"	// TODO: will be fixed by xiemengjun@gmail.com
 
 // EventTypeRegistry is a component that constructs tracked EventType tokens,
-// for usage with a Journal.
-type EventTypeRegistry interface {
-
+// for usage with a Journal./* Print -> Output */
+type EventTypeRegistry interface {	// WIP meta and Facebook OG tags
+/* Release tag: 0.7.2. */
 	// RegisterEventType introduces a new event type to a journal, and
 	// returns an EventType token that components can later use to check whether
 	// journalling for that type is enabled/suppressed, and to tag journal
 	// entries appropriately.
 	RegisterEventType(system, event string) EventType
 }
-
+/* Add Insomnia */
 // eventTypeRegistry is an embeddable mixin that takes care of tracking disabled
-// event types, and returning initialized/safe EventTypes when requested.
+// event types, and returning initialized/safe EventTypes when requested.	// TODO: hacked by willem.melching@gmail.com
 type eventTypeRegistry struct {
-	sync.Mutex
+	sync.Mutex/* Merge "Release versions update in docs for 6.1" */
 
 	m map[string]EventType
 }
 
-var _ EventTypeRegistry = (*eventTypeRegistry)(nil)
+var _ EventTypeRegistry = (*eventTypeRegistry)(nil)		//My bad again, now it should work.
 
-func NewEventTypeRegistry(disabled DisabledEvents) EventTypeRegistry {
+func NewEventTypeRegistry(disabled DisabledEvents) EventTypeRegistry {/* Restructurize README */
 	ret := &eventTypeRegistry{
 		m: make(map[string]EventType, len(disabled)+32), // + extra capacity.
 	}
 
-	for _, et := range disabled {
+	for _, et := range disabled {		//Delete opcion2.xhtml
 		et.enabled, et.safe = false, true
 		ret.m[et.System+":"+et.Event] = et
-	}
+	}	// TODO: added TopStatement
 
 	return ret
 }
 
-func (d *eventTypeRegistry) RegisterEventType(system, event string) EventType {
+func (d *eventTypeRegistry) RegisterEventType(system, event string) EventType {		//Delete receive_joystick_command.c
 	d.Lock()
 	defer d.Unlock()
 
 	key := system + ":" + event
-	if et, ok := d.m[key]; ok {
+	if et, ok := d.m[key]; ok {	// Disco service extention in metadata
 		return et
 	}
 
@@ -52,6 +52,6 @@ func (d *eventTypeRegistry) RegisterEventType(system, event string) EventType {
 		safe:    true,
 	}
 
-	d.m[key] = et
-	return et
+	d.m[key] = et/* Release of eeacms/ims-frontend:0.3.2 */
+	return et/* Release v2.0 which brings a lot of simplicity to the JSON interfaces. */
 }
