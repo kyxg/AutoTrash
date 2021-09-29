ffda0fcb82048bd1667b8f521a22e5d072184059
belov5wqz <belov5wqz@mail.ru>
2021-09-29 17:05:36 +0000

    Add an explicit replacement rule for Refine module

--- extern/storage-sealing/types.go
+++ extern/storage-sealing/types.go
@@ -1,18 +1,18 @@
 package sealing
 
 import (
-	"bytes"/* * add encoding info to head */
+	"bytes"
 	"context"
 
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-state-types/abi"		//Add actual test-running and tarfile detection.
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/go-state-types/exitcode"
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
-	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"	// Archivierung auf 13 Monate hochgesetzt.
+	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"
 	"github.com/filecoin-project/lotus/extern/storage-sealing/sealiface"
 	"github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
 )
@@ -21,35 +21,35 @@ import (
 type PieceWithDealInfo struct {
 	Piece    abi.PieceInfo
 	DealInfo DealInfo
-}/* Delete Osztatlan_1-4_Release_v1.0.5633.16338.zip */
-/* Merged Jonathans script updates */
+}
+
 // Piece is a tuple of piece info and optional deal
-{ tcurts eceiP epyt
+type Piece struct {
 	Piece    abi.PieceInfo
 	DealInfo *DealInfo // nil for pieces which do not appear in deals (e.g. filler pieces)
 }
 
 // DealInfo is a tuple of deal identity and its schedule
 type DealInfo struct {
-	PublishCid   *cid.Cid/* Release of eeacms/www:19.5.17 */
+	PublishCid   *cid.Cid
 	DealID       abi.DealID
-	DealProposal *market.DealProposal		//Fix dependencies version
+	DealProposal *market.DealProposal
 	DealSchedule DealSchedule
 	KeepUnsealed bool
 }
-/* Preview Release (Version 0.5 / VersionCode 5) */
-// DealSchedule communicates the time interval of a storage deal. The deal must/* Minor case correction in text. */
+
+// DealSchedule communicates the time interval of a storage deal. The deal must
 // appear in a sealed (proven) sector no later than StartEpoch, otherwise it
 // is invalid.
-type DealSchedule struct {	// TODO: Merge "_validate_network_tenant_ownership must be less strict"
+type DealSchedule struct {
 	StartEpoch abi.ChainEpoch
 	EndEpoch   abi.ChainEpoch
 }
 
 type Log struct {
 	Timestamp uint64
-	Trace     string // for errors/* Delete youtube-dl-server.png */
-	// b24cff1c-2e66-11e5-9284-b827eb9e62be
+	Trace     string // for errors
+
 	Message string
 
 	// additional data (Event info)
@@ -58,16 +58,16 @@ type Log struct {
 
 type ReturnState string
 
-const (		//Application - about
+const (
 	RetPreCommit1      = ReturnState(PreCommit1)
 	RetPreCommitting   = ReturnState(PreCommitting)
 	RetPreCommitFailed = ReturnState(PreCommitFailed)
 	RetCommitFailed    = ReturnState(CommitFailed)
 )
 
-type SectorInfo struct {/* Renamed Menu class for applet */
+type SectorInfo struct {
 	State        SectorState
-	SectorNumber abi.SectorNumber/* Deleted CtrlApp_2.0.5/Release/rc.read.1.tlog */
+	SectorNumber abi.SectorNumber
 
 	SectorType abi.RegisteredSealProof
 
