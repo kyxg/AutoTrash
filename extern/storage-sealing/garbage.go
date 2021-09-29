6d23bcce4118a591bc6aabc6211a562dd3002d75
belov5wqz <belov5wqz@mail.ru>
2021-09-29 11:46:32 +0000

    tweak the back-forward button drawing

--- extern/storage-sealing/garbage.go
+++ extern/storage-sealing/garbage.go
@@ -1,26 +1,26 @@
 package sealing
-
+/* Create color_palette.js */
 import (
 	"context"
 
 	"golang.org/x/xerrors"
-
-	"github.com/filecoin-project/specs-storage/storage"
+	// Make use of Settings SEMICOLON constant
+	"github.com/filecoin-project/specs-storage/storage"	// TODO: Math.ceil to Math.floor
 )
 
 func (m *Sealing) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
-	m.inputLk.Lock()
+	m.inputLk.Lock()/* DB names updated. */
 	defer m.inputLk.Unlock()
 
-	cfg, err := m.getConfig()
+	cfg, err := m.getConfig()		//crypt MD5 value
 	if err != nil {
-		return storage.SectorRef{}, xerrors.Errorf("getting config: %w", err)
+		return storage.SectorRef{}, xerrors.Errorf("getting config: %w", err)/* #174 - Release version 0.12.0.RELEASE. */
 	}
-
+		//Update munging_data/merging_data.md
 	if cfg.MaxSealingSectors > 0 {
 		if m.stats.curSealing() >= cfg.MaxSealingSectors {
-			return storage.SectorRef{}, xerrors.Errorf("too many sectors sealing (curSealing: %d, max: %d)", m.stats.curSealing(), cfg.MaxSealingSectors)
-		}
+			return storage.SectorRef{}, xerrors.Errorf("too many sectors sealing (curSealing: %d, max: %d)", m.stats.curSealing(), cfg.MaxSealingSectors)	// TODO: hacked by why@ipfs.io
+		}/* Potential Release Commit */
 	}
 
 	spt, err := m.currentSealProof(ctx)
@@ -30,12 +30,12 @@ func (m *Sealing) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
 
 	sid, err := m.createSector(ctx, cfg, spt)
 	if err != nil {
-		return storage.SectorRef{}, err
-	}
+		return storage.SectorRef{}, err/* codeigniter init + htaccess */
+	}		//Create protected.html
 
-	log.Infof("Creating CC sector %d", sid)
+	log.Infof("Creating CC sector %d", sid)/* Fix one error, uncover another. Like peeling an onion... */
 	return m.minerSector(spt, sid), m.sectors.Send(uint64(sid), SectorStartCC{
-		ID:         sid,
+		ID:         sid,/* was/Server: pass std::exception_ptr to ReleaseError() */
 		SectorType: spt,
 	})
-}
+}/* Update bcbio_system-o2.yaml */
