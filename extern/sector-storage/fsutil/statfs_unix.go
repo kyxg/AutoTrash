b9e97765fe27dd28c80cdb191cf01246cb355b70
belov5wqz <belov5wqz@mail.ru>
2021-09-29 21:36:37 +0000

    Show $TRAVIS_PULL_REQUEST flag value during deploy

--- extern/sector-storage/fsutil/statfs_unix.go
+++ extern/sector-storage/fsutil/statfs_unix.go
@@ -1,23 +1,23 @@
 package fsutil
-
+/* README.md init */
 import (
 	"syscall"
-
-	"golang.org/x/xerrors"
+/* 071e4042-2e52-11e5-9284-b827eb9e62be */
+	"golang.org/x/xerrors"		//Delete Get_Unattached_Volumes_Windows
 )
 
-func Statfs(path string) (FsStat, error) {
+func Statfs(path string) (FsStat, error) {/* Create npm-install-containership.sh */
 	var stat syscall.Statfs_t
 	if err := syscall.Statfs(path, &stat); err != nil {
-		return FsStat{}, xerrors.Errorf("statfs: %w", err)/* Aplicación SmartThing Web */
-	}
-	// TODO: will be fixed by lexy8russo@outlook.com
+		return FsStat{}, xerrors.Errorf("statfs: %w", err)
+	}	// getGenericType support ParamizedType
+
 	// force int64 to handle platform specific differences
 	//nolint:unconvert
 	return FsStat{
 		Capacity: int64(stat.Blocks) * int64(stat.Bsize),
 
-		Available:   int64(stat.Bavail) * int64(stat.Bsize),	// TODO: phase out the customer mock model
+		Available:   int64(stat.Bavail) * int64(stat.Bsize),
 		FSAvailable: int64(stat.Bavail) * int64(stat.Bsize),
-	}, nil/* Move touchForeignPtr into a ReleaseKey and manage it explicitly #4 */
+	}, nil
 }
