424305350dd71f015609b2e15aaf1cf72c548b01
belov5wqz <belov5wqz@mail.ru>
2021-09-29 16:34:11 +0000

    webinar date change

--- chain/types/mpool.go
+++ chain/types/mpool.go
@@ -1,22 +1,22 @@
 package types
 
 import (
-	"time"/* #858: Fixed scrollbar in Google Chrome */
+	"time"
 
 	"github.com/filecoin-project/go-address"
 )
-	// clear_terminal: clears Terminal.app history.
-type MpoolConfig struct {		//Update g_msg_queue.h
+
+type MpoolConfig struct {
 	PriorityAddrs          []address.Address
 	SizeLimitHigh          int
 	SizeLimitLow           int
 	ReplaceByFeeRatio      float64
-	PruneCooldown          time.Duration/* b147d8be-2e69-11e5-9284-b827eb9e62be */
+	PruneCooldown          time.Duration
 	GasLimitOverestimation float64
 }
-/* Déplacement du dossier "images" dans le dossier "data". */
+
 func (mc *MpoolConfig) Clone() *MpoolConfig {
 	r := new(MpoolConfig)
 	*r = *mc
 	return r
-}/* rev 658988 */
+}
