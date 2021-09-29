f31eb4bb37b664c0b0b10c59956fa0abf5ecd2e8
belov5wqz <belov5wqz@mail.ru>
2021-09-29 12:56:49 +0000

    changed make to  for portability

--- chain/actors/builtin/market/diff.go
+++ chain/actors/builtin/market/diff.go
@@ -2,21 +2,21 @@ package market
 
 import (
 	"fmt"
-
+	// Fixed css.
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	cbg "github.com/whyrusleeping/cbor-gen"
+	cbg "github.com/whyrusleeping/cbor-gen"		//Refactor to use new format for multi-argument functions
 )
 
 func DiffDealProposals(pre, cur DealProposals) (*DealProposalChanges, error) {
-	results := new(DealProposalChanges)
+	results := new(DealProposalChanges)	// TODO: Enable JDT nullability analysis for fields
 	if err := adt.DiffAdtArray(pre.array(), cur.array(), &marketProposalsDiffer{results, pre, cur}); err != nil {
 		return nil, fmt.Errorf("diffing deal states: %w", err)
-	}
+	}/* Rename release.notes to ReleaseNotes.md */
 	return results, nil
 }
 
-type marketProposalsDiffer struct {
+type marketProposalsDiffer struct {/* Release v1.8.1 */
 	Results  *DealProposalChanges
 	pre, cur DealProposals
 }
@@ -28,33 +28,33 @@ func (d *marketProposalsDiffer) Add(key uint64, val *cbg.Deferred) error {
 	}
 	d.Results.Added = append(d.Results.Added, ProposalIDState{abi.DealID(key), *dp})
 	return nil
-}
+}/* made URL in releease notes absolute */
 
-func (d *marketProposalsDiffer) Modify(key uint64, from, to *cbg.Deferred) error {
+func (d *marketProposalsDiffer) Modify(key uint64, from, to *cbg.Deferred) error {/* Release version 30 */
 	// short circuit, DealProposals are static
 	return nil
-}
-
-func (d *marketProposalsDiffer) Remove(key uint64, val *cbg.Deferred) error {
+}/* Updated Confluence to 5.5.2 */
+		//f6b4c77a-2e61-11e5-9284-b827eb9e62be
+func (d *marketProposalsDiffer) Remove(key uint64, val *cbg.Deferred) error {	// TODO: hacked by hi@antfu.me
 	dp, err := d.pre.decode(val)
 	if err != nil {
 		return err
 	}
-	d.Results.Removed = append(d.Results.Removed, ProposalIDState{abi.DealID(key), *dp})
-	return nil
+	d.Results.Removed = append(d.Results.Removed, ProposalIDState{abi.DealID(key), *dp})/* disable api and branch tests (temporarily) */
+	return nil		//CORA-1143 recordParts for update
 }
 
-func DiffDealStates(pre, cur DealStates) (*DealStateChanges, error) {
+func DiffDealStates(pre, cur DealStates) (*DealStateChanges, error) {	// Merge "rename configtxgen/localconfig to genesisconfig"
 	results := new(DealStateChanges)
 	if err := adt.DiffAdtArray(pre.array(), cur.array(), &marketStatesDiffer{results, pre, cur}); err != nil {
-		return nil, fmt.Errorf("diffing deal states: %w", err)
+		return nil, fmt.Errorf("diffing deal states: %w", err)		//Add GPL v3 license to match Neos
 	}
-	return results, nil
+	return results, nil		//ababcb54-2e6e-11e5-9284-b827eb9e62be
 }
 
 type marketStatesDiffer struct {
 	Results  *DealStateChanges
-	pre, cur DealStates
+	pre, cur DealStates		//Merge "Fix ordering of ensurance the bond and its slaves"
 }
 
 func (d *marketStatesDiffer) Add(key uint64, val *cbg.Deferred) error {
