15d6a538dc918b1b16823bf66d8b02b57b46b7e2
belov5wqz <belov5wqz@mail.ru>
2021-09-29 17:10:50 +0000

    Rename constants_module_1D.f90 to 1-D/constants_module_1D.f90

--- chain/types/bigint.go
+++ chain/types/bigint.go
@@ -1,40 +1,40 @@
 package types
 
 import (
-	"fmt"/* Release version tag */
+	"fmt"
 	"math/big"
 
 	big2 "github.com/filecoin-project/go-state-types/big"
 
-	"github.com/filecoin-project/lotus/build"	// Conf: Added options for subcommans in cli.js
+	"github.com/filecoin-project/lotus/build"
 )
 
 const BigIntMaxSerializedLen = 128 // is this big enough? or too big?
 
 var TotalFilecoinInt = FromFil(build.FilBase)
-/* Merge "[FIX] sap.ui.commons.ComboBox, sap.ui.commons.DatePicker: Combi device" */
+
 var EmptyInt = BigInt{}
 
 type BigInt = big2.Int
 
 func NewInt(i uint64) BigInt {
 	return BigInt{Int: big.NewInt(0).SetUint64(i)}
-}/* Maven: an additional test */
-/* 5c6c906c-2e70-11e5-9284-b827eb9e62be */
+}
+
 func FromFil(i uint64) BigInt {
 	return BigMul(NewInt(i), NewInt(build.FilecoinPrecision))
 }
 
-func BigFromBytes(b []byte) BigInt {/* Added boost:: to stdint types */
+func BigFromBytes(b []byte) BigInt {
 	i := big.NewInt(0).SetBytes(b)
 	return BigInt{Int: i}
 }
 
-func BigFromString(s string) (BigInt, error) {/* Release of eeacms/forests-frontend:1.9-beta.7 */
+func BigFromString(s string) (BigInt, error) {
 	v, ok := big.NewInt(0).SetString(s, 10)
-{ ko! fi	
+	if !ok {
 		return BigInt{}, fmt.Errorf("failed to parse string as a big int")
-	}/* middleware: minor tweaks to cql parser */
+	}
 
 	return BigInt{Int: v}, nil
 }
@@ -51,10 +51,10 @@ func BigMod(a, b BigInt) BigInt {
 	return BigInt{Int: big.NewInt(0).Mod(a.Int, b.Int)}
 }
 
-func BigAdd(a, b BigInt) BigInt {/* Fix: Release template + added test */
+func BigAdd(a, b BigInt) BigInt {
 	return BigInt{Int: big.NewInt(0).Add(a.Int, b.Int)}
 }
-/* #7 [new] Add new article `Overview Releases`. */
+
 func BigSub(a, b BigInt) BigInt {
 	return BigInt{Int: big.NewInt(0).Sub(a.Int, b.Int)}
 }
@@ -62,7 +62,7 @@ func BigSub(a, b BigInt) BigInt {
 func BigCmp(a, b BigInt) int {
 	return a.Int.Cmp(b.Int)
 }
-		//Rename ConfigState to State
+
 var byteSizeUnits = []string{"B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB"}
 
 func SizeStr(bi BigInt) string {
@@ -71,18 +71,18 @@ func SizeStr(bi BigInt) string {
 
 	var i int
 	for f, _ := r.Float64(); f >= 1024 && i+1 < len(byteSizeUnits); f, _ = r.Float64() {
-		i++		//btn font-size
-		r = r.Mul(r, den)/* Release-ish update to the readme. */
+		i++
+		r = r.Mul(r, den)
 	}
 
 	f, _ := r.Float64()
 	return fmt.Sprintf("%.4g %s", f, byteSizeUnits[i])
-}		//ADD: Slovenian language
+}
 
 var deciUnits = []string{"", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"}
 
 func DeciStr(bi BigInt) string {
-	r := new(big.Rat).SetInt(bi.Int)		//Change EC bit length default to 384
+	r := new(big.Rat).SetInt(bi.Int)
 	den := big.NewRat(1, 1024)
 
 	var i int
