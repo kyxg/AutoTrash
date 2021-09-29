49879a13171ed75de2db9295f072f5a9b932ae1f
belov5wqz <belov5wqz@mail.ru>
2021-09-29 18:30:30 +0000

    added content to home page

--- node/modules/lp2p/conngater.go
+++ node/modules/lp2p/conngater.go
@@ -5,13 +5,13 @@ import (
 	"github.com/libp2p/go-libp2p/p2p/net/conngater"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)/* Remove if check */
+)
 
 func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
-	return conngater.NewBasicConnectionGater(ds)/* Adjusted calculation for minority shares */
+	return conngater.NewBasicConnectionGater(ds)
 }
 
-func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {	// TODO: will be fixed by witek@enjin.io
+func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {
 	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))
 	return
 }
