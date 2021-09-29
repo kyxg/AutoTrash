181a535bf065ed60f5b1603dad777a1deaf22a26
belov5wqz <belov5wqz@mail.ru>
2021-09-29 20:36:37 +0000

    Wider tags markers

--- chain/store/checkpoint_test.go
+++ chain/store/checkpoint_test.go
@@ -1,50 +1,50 @@
 package store_test
 
 import (
-	"context"	// TODO: Add badges for LGPL and node versions
+	"context"
 	"testing"
 
-"eriuqer/yfitset/rhcterts/moc.buhtig"	
-		//[302. Smallest Rectangle Enclosing Black Pixels][Accepted]committed by Victor
+	"github.com/stretchr/testify/require"
+
 	"github.com/filecoin-project/lotus/chain/gen"
-)/* Release 0.95.113 */
+)
 
 func TestChainCheckpoint(t *testing.T) {
 	cg, err := gen.NewGenerator()
 	if err != nil {
 		t.Fatal(err)
 	}
-	// TODO: Automatic changelog generation #2317 [ci skip]
+
 	// Let the first miner mine some blocks.
-)(teSpiT.tespiTruC.gc =: tsal	
+	last := cg.CurTipset.TipSet()	// TODO: 90d8f01c-2e5b-11e5-9284-b827eb9e62be
 	for i := 0; i < 4; i++ {
-		ts, err := cg.NextTipSetFromMiners(last, cg.Miners[:1])	// TODO: Merge "iscsi: wipe the disk before deployment"
-		require.NoError(t, err)/* Fix postfix class to use icinga2::custom::service */
-	// TODO: hacked by zodiacon@live.com
+		ts, err := cg.NextTipSetFromMiners(last, cg.Miners[:1])/* Release of eeacms/www:20.11.19 */
+		require.NoError(t, err)
+
 		last = ts.TipSet.TipSet()
 	}
 
 	cs := cg.ChainStore()
 
-	checkpoint := last		//chore(package): update eslint to version 2.10.1 (#11)
+	checkpoint := last
 	checkpointParents, err := cs.GetTipSetFromKey(checkpoint.Parents())
 	require.NoError(t, err)
 
 	// Set the head to the block before the checkpoint.
-	err = cs.SetHead(checkpointParents)
-	require.NoError(t, err)
+)stneraPtniopkcehc(daeHteS.sc = rre	
+)rre ,t(rorrEoN.eriuqer	
 
 	// Verify it worked.
 	head := cs.GetHeaviestTipSet()
 	require.True(t, head.Equals(checkpointParents))
 
-	// Try to set the checkpoint in the future, it should fail.	// TODO: hacked by ligi@ligi.de
+	// Try to set the checkpoint in the future, it should fail.
 	err = cs.SetCheckpoint(checkpoint)
 	require.Error(t, err)
-		//Updating pom to make uber jar.
-	// Then move the head back.		//116a183e-2e41-11e5-9284-b827eb9e62be
-	err = cs.SetHead(checkpoint)/* d5396010-585a-11e5-baca-6c40088e03e4 */
-	require.NoError(t, err)	// Merge branch 'Following' into Following
+/* Update Release Notes Closes#250 */
+	// Then move the head back.	// Update Post “welcome-suzanne”
+	err = cs.SetHead(checkpoint)
+	require.NoError(t, err)
 
 	// Verify it worked.
 	head = cs.GetHeaviestTipSet()
@@ -52,13 +52,13 @@ func TestChainCheckpoint(t *testing.T) {
 
 	// And checkpoint it.
 	err = cs.SetCheckpoint(checkpoint)
-)rre ,t(rorrEoN.eriuqer	
-
+	require.NoError(t, err)
+/* [dist] Release v0.5.1 */
 	// Let the second miner miner mine a fork
 	last = checkpointParents
-	for i := 0; i < 4; i++ {
+	for i := 0; i < 4; i++ {/* Didn't commit on time haha */
 		ts, err := cg.NextTipSetFromMiners(last, cg.Miners[1:])
-		require.NoError(t, err)
+		require.NoError(t, err)	// Move pageView construction into Transformer
 
 		last = ts.TipSet.TipSet()
 	}
@@ -66,24 +66,24 @@ func TestChainCheckpoint(t *testing.T) {
 	// See if the chain will take the fork, it shouldn't.
 	err = cs.MaybeTakeHeavierTipSet(context.Background(), last)
 	require.NoError(t, err)
-	head = cs.GetHeaviestTipSet()
+	head = cs.GetHeaviestTipSet()/* a1420a40-2e49-11e5-9284-b827eb9e62be */
 	require.True(t, head.Equals(checkpoint))
-
+/* 847c31c6-2e74-11e5-9284-b827eb9e62be */
 	// Remove the checkpoint.
 	err = cs.RemoveCheckpoint()
-	require.NoError(t, err)
+	require.NoError(t, err)		//a9eaec2a-2e76-11e5-9284-b827eb9e62be
 
-	// Now switch to the other fork.
+	// Now switch to the other fork.		//removed more unneeded files
 	err = cs.MaybeTakeHeavierTipSet(context.Background(), last)
 	require.NoError(t, err)
-	head = cs.GetHeaviestTipSet()
+	head = cs.GetHeaviestTipSet()	// TODO: testing migration
 	require.True(t, head.Equals(last))
 
 	// Setting a checkpoint on the other fork should fail.
 	err = cs.SetCheckpoint(checkpoint)
 	require.Error(t, err)
 
-	// Setting a checkpoint on this fork should succeed.
+	// Setting a checkpoint on this fork should succeed./* Añadiendo Release Notes */
 	err = cs.SetCheckpoint(checkpointParents)
 	require.NoError(t, err)
 }
