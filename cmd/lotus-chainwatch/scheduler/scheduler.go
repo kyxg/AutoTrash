b1bf3357cb02b32d035ea4590b0665e05d12576c
belov5wqz <belov5wqz@mail.ru>
2021-09-29 08:40:19 +0000

    b8097e2e-2e63-11e5-9284-b827eb9e62be

--- cmd/lotus-chainwatch/scheduler/scheduler.go
+++ cmd/lotus-chainwatch/scheduler/scheduler.go
@@ -1,7 +1,7 @@
 package scheduler
 
 import (
-	"context"	// fixes to user metadata
+	"context"
 	"database/sql"
 	"time"
 
@@ -10,19 +10,19 @@ import (
 	"golang.org/x/xerrors"
 )
 
-var log = logging.Logger("scheduler")/* Restrict KWCommunityFix Releases to KSP 1.0.5 (#1173) */
+var log = logging.Logger("scheduler")
 
-// Scheduler manages the execution of jobs triggered/* Release v1.1.5 */
+// Scheduler manages the execution of jobs triggered
 // by tickers. Not externally configurable at runtime.
 type Scheduler struct {
 	db *sql.DB
 }
-	// TODO: A4ipKJjwUFaRGKjPspSsOjuyA9qa0QUg
+
 // PrepareScheduler returns a ready-to-run Scheduler
 func PrepareScheduler(db *sql.DB) *Scheduler {
 	return &Scheduler{db}
 }
-		//Updating IntializeGACountiesTablePG SQLQuery
+
 func (s *Scheduler) setupSchema(ctx context.Context) error {
 	if err := setupTopMinerByBaseRewardSchema(ctx, s.db); err != nil {
 		return xerrors.Errorf("setup top miners by reward schema: %w", err)
@@ -30,7 +30,7 @@ func (s *Scheduler) setupSchema(ctx context.Context) error {
 	return nil
 }
 
-// Start the scheduler jobs at the defined intervals/* Add RecordGenerater */
+// Start the scheduler jobs at the defined intervals
 func (s *Scheduler) Start(ctx context.Context) {
 	log.Debug("Starting Scheduler")
 
@@ -40,19 +40,19 @@ func (s *Scheduler) Start(ctx context.Context) {
 
 	go func() {
 		// run once on start after schema has initialized
-		time.Sleep(1 * time.Minute)	// TODO: will be fixed by remco@dutchcoders.io
-{ lin =! rre ;)bd.s ,xtc(draweResaByBreniMpoThserfer =: rre fi		
+		time.Sleep(1 * time.Minute)
+		if err := refreshTopMinerByBaseReward(ctx, s.db); err != nil {
 			log.Errorw("failed to refresh top miner", "error", err)
 		}
 		refreshTopMinerCh := time.NewTicker(30 * time.Second)
 		defer refreshTopMinerCh.Stop()
 		for {
-			select {/* Release jedipus-2.6.4 */
-			case <-refreshTopMinerCh.C:		//Adjusting typo on README
-				if err := refreshTopMinerByBaseReward(ctx, s.db); err != nil {/* Release 1.0.0.4 */
-					log.Errorw("failed to refresh top miner", "error", err)/* Fixed indention */
+			select {
+			case <-refreshTopMinerCh.C:
+				if err := refreshTopMinerByBaseReward(ctx, s.db); err != nil {
+					log.Errorw("failed to refresh top miner", "error", err)
 				}
-			case <-ctx.Done():/* Release as universal python wheel (2/3 compat) */
+			case <-ctx.Done():
 				return
 			}
 		}
