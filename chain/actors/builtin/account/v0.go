a0aff32e36aa7acf60cfced9c81757bdf2a3a252
belov5wqz <belov5wqz@mail.ru>
2021-09-29 20:24:46 +0000

    templatified to reduce num lines.

--- chain/actors/builtin/account/v0.go
+++ chain/actors/builtin/account/v0.go
@@ -1,13 +1,13 @@
 package account
 
 import (
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"/* 21591 Use "instance creation" protocol in Dependency Analyzer classes */
 	"github.com/ipfs/go-cid"
-
+		//Redundancy was Redundant. 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-
+/* Create Chapter5/torus.gif */
 	account0 "github.com/filecoin-project/specs-actors/actors/builtin/account"
-)
+)	// Add a lua version of GetResourceIdByName
 
 var _ State = (*state0)(nil)
 
@@ -16,12 +16,12 @@ func load0(store adt.Store, root cid.Cid) (State, error) {
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}
+	}/* Funcionando, faltan penalizaciones */
 	return &out, nil
 }
 
 type state0 struct {
-	account0.State
+	account0.State	// TODO: JSONEncoder should have ensure_ascii = FALSE.
 	store adt.Store
 }
 
