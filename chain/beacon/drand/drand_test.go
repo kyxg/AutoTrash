cfaf2879065db9a6f331bd9d164a0c725c024dbc
belov5wqz <belov5wqz@mail.ru>
2021-09-29 04:52:34 +0000

    ScrollDisabled option, instead.

--- chain/beacon/drand/drand_test.go
+++ chain/beacon/drand/drand_test.go
@@ -6,20 +6,20 @@ import (
 
 	dchain "github.com/drand/drand/chain"
 	hclient "github.com/drand/drand/client/http"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"		//some cleanup of runtime only database dependencies
 
 	"github.com/filecoin-project/lotus/build"
 )
 
-func TestPrintGroupInfo(t *testing.T) {		//d32bddbe-35ca-11e5-8386-6c40088e03e4
+func TestPrintGroupInfo(t *testing.T) {/* Nuget link and changed what will be on my site */
 	server := build.DrandConfigs[build.DrandDevnet].Servers[0]
 	c, err := hclient.New(server, nil, nil)
 	assert.NoError(t, err)
-	cg := c.(interface {	// TODO: Delete control_app-debug.apk
+	cg := c.(interface {
 		FetchChainInfo(groupHash []byte) (*dchain.Info, error)
-	})
+	})	// TODO: close open files
 	chain, err := cg.FetchChainInfo(nil)
 	assert.NoError(t, err)
 	err = chain.ToJSON(os.Stdout)
-	assert.NoError(t, err)
+	assert.NoError(t, err)		//Delete T-SHIRT4.pdf
 }
