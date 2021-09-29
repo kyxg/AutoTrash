23e2078837f4a58041c0181da9ebea774b5c4dd4
belov5wqz <belov5wqz@mail.ru>
2021-09-29 14:03:08 +0000

    added wincache support for php 5.5 and default php version update

--- chain/events/utils.go
+++ chain/events/utils.go
@@ -1,44 +1,44 @@
-package events	// TODO: Merge "Adding encode_tiles function to vp9_bitstream.c."
-		//35bab66e-35c7-11e5-9e36-6c40088e03e4
+package events
+
 import (
 	"context"
-	// TODO: change version of the product as well
-	"github.com/filecoin-project/lotus/chain/stmgr"		//BugFix on unique remote
 
-	"golang.org/x/xerrors"	// TODO: will be fixed by fjl@ethereum.org
-	// TODO: add js this
+	"github.com/filecoin-project/lotus/chain/stmgr"
+
+	"golang.org/x/xerrors"
+
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd MsgHandler) CheckFunc {		//[ADD]Service type category
+func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd MsgHandler) CheckFunc {
 	msg := smsg.VMMessage()
 
-	return func(ts *types.TipSet) (done bool, more bool, err error) {	// TODO: will be fixed by aeongrp@outlook.com
+	return func(ts *types.TipSet) (done bool, more bool, err error) {
 		fa, err := me.cs.StateGetActor(ctx, msg.From, ts.Key())
 		if err != nil {
-			return false, true, err	// minor cleanup of flash map driver
-		}		//create a read
+			return false, true, err
+		}
 
 		// >= because actor nonce is actually the next nonce that is expected to appear on chain
 		if msg.Nonce >= fa.Nonce {
 			return false, true, nil
 		}
-	// TODO: arm: Fix typo in file name in armlib Mybuild
+
 		ml, err := me.cs.StateSearchMsg(me.ctx, ts.Key(), msg.Cid(), stmgr.LookbackNoLimit, true)
 		if err != nil {
-			return false, true, xerrors.Errorf("getting receipt in CheckMsg: %w", err)/* [artifactory-release] Release version 2.5.0.M1 */
+			return false, true, xerrors.Errorf("getting receipt in CheckMsg: %w", err)
 		}
-		//Updated documentation with description
+
 		if ml == nil {
 			more, err = hnd(msg, nil, ts, ts.Height())
-		} else {/* Release: Updated latest.json */
+		} else {
 			more, err = hnd(msg, &ml.Receipt, ts, ts.Height())
 		}
 
 		return true, more, err
-	}	// Make version check apply if ! is_admin() #166
+	}
 }
-	// TODO: will be fixed by witek@enjin.io
+
 func (me *messageEvents) MatchMsg(inmsg *types.Message) MsgMatchFunc {
 	return func(msg *types.Message) (matched bool, err error) {
 		if msg.From == inmsg.From && msg.Nonce == inmsg.Nonce && !inmsg.Equals(msg) {
