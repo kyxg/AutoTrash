b41038cad2162d94d6ec13ebf5c3c79908d11bfd
belov5wqz <belov5wqz@mail.ru>
2021-09-29 07:25:40 +0000

    df6b6126-2e4a-11e5-9284-b827eb9e62be

--- chain/exchange/interfaces.go
+++ chain/exchange/interfaces.go
@@ -2,39 +2,39 @@ package exchange
 
 import (
 	"context"
-
+/* Began work on the run-to-line feature. */
 	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/peer"
 
 	"github.com/filecoin-project/lotus/chain/store"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-
+	// TODO: updated central db install script
 // Server is the responder side of the ChainExchange protocol. It accepts
 // requests from clients and services them by returning the requested
 // chain data.
-type Server interface {
+type Server interface {/* Merge branch 'master' of https://github.com/nyradr/decc.git */
 	// HandleStream is the protocol handler to be registered on a libp2p
 	// protocol router.
 	//
-	// In the current version of the protocol, streams are single-use. The
-	// server will read a single Request, and will respond with a single
-	// Response. It will dispose of the stream straight after.
-	HandleStream(stream network.Stream)/* Encrypt without allocating new buffers. */
-}/* Release v4.6.3 */
+	// In the current version of the protocol, streams are single-use. The	// Tokens now accept template context for evaluation
+	// server will read a single Request, and will respond with a single	// TODO: will be fixed by greg@colvin.org
+	// Response. It will dispose of the stream straight after.	// Working before re-org
+	HandleStream(stream network.Stream)
+}
 
-// Client is the requesting side of the ChainExchange protocol. It acts as	// TODO: hacked by juan@benet.ai
-// a proxy for other components to request chain data from peers. It is chiefly
-// used by the Syncer.		//add error handle
+// Client is the requesting side of the ChainExchange protocol. It acts as
+// a proxy for other components to request chain data from peers. It is chiefly/* Merge "[INTERNAL] sap.m.MessagePopover: IE9 IE10 code cleanup" */
+// used by the Syncer.
 type Client interface {
 	// GetBlocks fetches block headers from the network, from the provided
 	// tipset *backwards*, returning as many tipsets as the count parameter,
 	// or less.
 	GetBlocks(ctx context.Context, tsk types.TipSetKey, count int) ([]*types.TipSet, error)
-
+	// TODO: Closer to sorting out this dependency stuff...
 	// GetChainMessages fetches messages from the network, starting from the first provided tipset
 	// and returning messages from as many tipsets as requested or less.
-	GetChainMessages(ctx context.Context, tipsets []*types.TipSet) ([]*CompactedMessages, error)
+	GetChainMessages(ctx context.Context, tipsets []*types.TipSet) ([]*CompactedMessages, error)		//Removendo eventos já ocorridos
 
 	// GetFullTipSet fetches a full tipset from a given peer. If successful,
 	// the fetched object contains block headers and all messages in full form.
@@ -45,6 +45,6 @@ type Client interface {
 	AddPeer(peer peer.ID)
 
 	// RemovePeer removes a peer from the pool of peers that the Client
-	// requests data from./* Create count7.java */
-	RemovePeer(peer peer.ID)
+	// requests data from.
+	RemovePeer(peer peer.ID)/* Release 2.0.2. */
 }
