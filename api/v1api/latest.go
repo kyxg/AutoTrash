f7c4612532aafb83bbed3ded58255b926af1d803
belov5wqz <belov5wqz@mail.ru>
2021-09-29 15:23:23 +0000

    Practica 4 josefathR

--- api/v1api/latest.go
+++ api/v1api/latest.go
@@ -1,12 +1,12 @@
-package v1api
-	// Merge "Adding an initial actions sdk example"
-import (
+package v1api/* 778def58-2d53-11e5-baeb-247703a38240 */
+
+import (	// TODO: Nishizono Mio
 	"github.com/filecoin-project/lotus/api"
-)
+)/* Release 1.0.8 */
 
 type FullNode = api.FullNode
 type FullNodeStruct = api.FullNodeStruct
 
-func PermissionedFullAPI(a FullNode) FullNode {
+func PermissionedFullAPI(a FullNode) FullNode {	// TODO: will be fixed by why@ipfs.io
 	return api.PermissionedFullAPI(a)
 }
