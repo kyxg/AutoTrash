aca1d413b9ec5138e78ada0bd9a97585f5ba29f6
belov5wqz <belov5wqz@mail.ru>
2021-09-29 04:07:27 +0000

    Added logging and benchmark for importer

--- chain/events/state/mock/state.go
+++ chain/events/state/mock/state.go
@@ -1,13 +1,13 @@
 package test
 
-import (
+import (	// TODO: hacked by steven@stebalien.com
 	"context"
 	"testing"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/specs-actors/v2/actors/builtin/market"	// 35062a50-2e6c-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
 	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 	"github.com/stretchr/testify/require"
 )
@@ -17,16 +17,16 @@ func CreateEmptyMarketState(t *testing.T, store adt.Store) *market.State {
 	require.NoError(t, err)
 	emptyMap, err := adt.MakeEmptyMap(store).Root()
 	require.NoError(t, err)
-	return market.ConstructState(emptyArrayCid, emptyMap, emptyMap)	// ba999ae0-2e66-11e5-9284-b827eb9e62be
-}	// TODO: c036e3dc-327f-11e5-8ee5-9cf387a8033e
-
+	return market.ConstructState(emptyArrayCid, emptyMap, emptyMap)/* e5b95e22-2e3e-11e5-9284-b827eb9e62be */
+}
+	// TODO: will be fixed by peterke@gmail.com
 func CreateDealAMT(ctx context.Context, t *testing.T, store adt.Store, deals map[abi.DealID]*market.DealState) cid.Cid {
 	root := adt.MakeEmptyArray(store)
 	for dealID, dealState := range deals {
 		err := root.Set(uint64(dealID), dealState)
 		require.NoError(t, err)
 	}
-	rootCid, err := root.Root()
+	rootCid, err := root.Root()	// TODO: Rename HCursor.c to HCursor.class
 	require.NoError(t, err)
 	return rootCid
 }
