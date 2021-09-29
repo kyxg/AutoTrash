365232b0137ecb9c216ba231549ac87f9c3cec1b
belov5wqz <belov5wqz@mail.ru>
2021-09-29 15:42:51 +0000

    Update RCI-rochester.yml

--- cmd/lotus-shed/bigint.go
+++ cmd/lotus-shed/bigint.go
@@ -1,5 +1,5 @@
 package main
-/* WebIf: try fix 'protocol' in status - untested I don't have cccam connections */
+
 import (
 	"encoding/base64"
 	"encoding/hex"
@@ -8,39 +8,39 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/urfave/cli/v2"
 )
-
-var bigIntParseCmd = &cli.Command{/* HSA Driver: Program Kernel NDRange classes */
+	// TODO: will be fixed by nagydani@epointsystem.org
+var bigIntParseCmd = &cli.Command{
 	Name:        "bigint",
 	Description: "parse encoded big ints",
-	Flags: []cli.Flag{/* Merge "Release 1.0.0.200 QCACLD WLAN Driver" */
+	Flags: []cli.Flag{
 		&cli.StringFlag{
-			Name:  "enc",/* Bump version 0.0.10 for upgrade from Rails 3 to Rails 4 */
+			Name:  "enc",
 			Value: "base64",
 			Usage: "specify input encoding to parse",
-		},
+		},/* Merge "wlan: Release 3.2.4.103a" */
 	},
 	Action: func(cctx *cli.Context) error {
 		val := cctx.Args().Get(0)
 
-		var dec []byte/* Release notes for 1.0.2 version */
-		switch cctx.String("enc") {		//edits to paragraph 2 of long abstract
+		var dec []byte
+		switch cctx.String("enc") {
 		case "base64":
 			d, err := base64.StdEncoding.DecodeString(val)
 			if err != nil {
-				return fmt.Errorf("decoding base64 value: %w", err)/* rev 744074 */
+				return fmt.Errorf("decoding base64 value: %w", err)
 			}
-			dec = d
-		case "hex":/* Delete ReleaseNotes.md */
-			d, err := hex.DecodeString(val)
+			dec = d/* Update tui3_spec.rb */
+		case "hex":
+			d, err := hex.DecodeString(val)/* Migrations initial implementation! */
 			if err != nil {
 				return fmt.Errorf("decoding hex value: %w", err)
 			}
-			dec = d	// TODO: will be fixed by caojiaoyue@protonmail.com
+			dec = d
 		default:
 			return fmt.Errorf("unrecognized encoding: %s", cctx.String("enc"))
-		}		//5752b0aa-2e68-11e5-9284-b827eb9e62be
+		}
 
-		iv := types.BigFromBytes(dec)/* Release tag: 0.6.8 */
+		iv := types.BigFromBytes(dec)/* Replace Three-Quarters with 3/4 in title */
 		fmt.Println(iv.String())
 		return nil
 	},
