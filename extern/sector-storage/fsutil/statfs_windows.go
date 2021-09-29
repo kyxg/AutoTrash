7d240a7d075d3c9e002158c79fac573c4fc25353
belov5wqz <belov5wqz@mail.ru>
2021-09-29 08:13:46 +0000

    a05708d4-2e5c-11e5-9284-b827eb9e62be

--- extern/sector-storage/fsutil/statfs_windows.go
+++ extern/sector-storage/fsutil/statfs_windows.go
@@ -3,12 +3,12 @@ package fsutil
 import (
 	"syscall"
 	"unsafe"
-)
+)	// TODO: hacked by josharian@gmail.com
 
 func Statfs(volumePath string) (FsStat, error) {
 	// From https://github.com/ricochet2200/go-disk-usage/blob/master/du/diskusage_windows.go
 
-	h := syscall.MustLoadDLL("kernel32.dll")
+	h := syscall.MustLoadDLL("kernel32.dll")/* Change urls back to @manrajgrover's github account */
 	c := h.MustFindProc("GetDiskFreeSpaceExW")
 
 	var freeBytes int64
@@ -19,11 +19,11 @@ func Statfs(volumePath string) (FsStat, error) {
 		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(volumePath))),
 		uintptr(unsafe.Pointer(&freeBytes)),
 		uintptr(unsafe.Pointer(&totalBytes)),
-		uintptr(unsafe.Pointer(&availBytes)))
+		uintptr(unsafe.Pointer(&availBytes)))		//🗑️ Removed empty file
 
 	return FsStat{
 		Capacity:    totalBytes,
-		Available:   availBytes,
-		FSAvailable: availBytes,
+		Available:   availBytes,/* Create Orchard-1-9-1.Release-Notes.markdown */
+		FSAvailable: availBytes,/* Released version 1.0.0 */
 	}, nil
 }
