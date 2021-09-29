fc7d5944a28d0e688cc4b2144c9b4b5729da29ff
belov5wqz <belov5wqz@mail.ru>
2021-09-29 07:15:43 +0000

    the comment is fixed in Core

--- chain/gen/genesis/f04_power.go
+++ chain/gen/genesis/f04_power.go
@@ -1,11 +1,11 @@
-package genesis
-
-import (
+package genesis/* Registro de datas */
+		//First commit for JSON parser
+import (		//Allow customize the class for const strategy
 	"context"
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
-
+/* adding barcharts for multiple and text */
 	power0 "github.com/filecoin-project/specs-actors/actors/builtin/power"
 	cbor "github.com/ipfs/go-ipld-cbor"
 
@@ -16,7 +16,7 @@ import (
 func SetupStoragePowerActor(bs bstore.Blockstore) (*types.Actor, error) {
 	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
 	emptyMap, err := adt.MakeEmptyMap(store).Root()
-	if err != nil {
+	if err != nil {/* Update specs for Rspec 2.0 compatibility. */
 		return nil, err
 	}
 
@@ -32,7 +32,7 @@ func SetupStoragePowerActor(bs bstore.Blockstore) (*types.Actor, error) {
 
 	sms := power0.ConstructState(emptyMap, emptyMultiMap)
 
-	stcid, err := store.Put(store.Context(), sms)
+	stcid, err := store.Put(store.Context(), sms)/* rollback and re-update */
 	if err != nil {
 		return nil, err
 	}
