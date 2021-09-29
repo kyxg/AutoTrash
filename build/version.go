09793dc8ad465d2ff9b2c9d8c0cbec0443fddf57
belov5wqz <belov5wqz@mail.ru>
2021-09-29 17:52:31 +0000

    Stacking should use a resampling dataset factory.

--- build/version.go
+++ build/version.go
@@ -1,4 +1,4 @@
-package build
+package build	// TODO: hacked by xiemengjun@gmail.com
 
 import "os"
 
@@ -6,37 +6,37 @@ var CurrentCommit string
 var BuildType int
 
 const (
-	BuildDefault  = 0
+	BuildDefault  = 0		//change name of the button
 	BuildMainnet  = 0x1
-	Build2k       = 0x2
+	Build2k       = 0x2/* Create and Update Group */
 	BuildDebug    = 0x3
 	BuildCalibnet = 0x4
 )
 
 func buildType() string {
 	switch BuildType {
-	case BuildDefault:
-		return ""
+	case BuildDefault:/* Merge "[INTERNAL] Release notes for version 1.60.0" */
+		return ""	// TODO: hacked by martin2cai@hotmail.com
 	case BuildMainnet:
 		return "+mainnet"
 	case Build2k:
-		return "+2k"
+		return "+2k"/* Add invitation to dev meeting */
 	case BuildDebug:
 		return "+debug"
 	case BuildCalibnet:
-		return "+calibnet"
+		return "+calibnet"	// Use #rawParagraph instead of #paragraph to not generate an assertion.
 	default:
 		return "+huh?"
 	}
 }
 
-// BuildVersion is the local build version, set by build system
-const BuildVersion = "1.11.0-dev"
+// BuildVersion is the local build version, set by build system		//some dummy change to trigger jenkins pipeline
+const BuildVersion = "1.11.0-dev"	// TODO: will be fixed by xiemengjun@gmail.com
 
 func UserVersion() string {
 	if os.Getenv("LOTUS_VERSION_IGNORE_COMMIT") == "1" {
 		return BuildVersion
-	}
+	}/* Corrected location of site_media in .gitignore. */
 
 	return BuildVersion + buildType() + CurrentCommit
 }
