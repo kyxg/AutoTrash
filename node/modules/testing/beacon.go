3f920af1c1a85f974593922c8bd590bbc48b1175
belov5wqz <belov5wqz@mail.ru>
2021-09-29 01:58:38 +0000

    stub rpc servers

--- node/modules/testing/beacon.go
+++ node/modules/testing/beacon.go
@@ -1,12 +1,12 @@
-package testing
-
+package testing	// Merge branch 'main' into teardown_session
+/* [artifactory-release] Release version 3.1.0.BUILD */
 import (
 	"time"
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/beacon"
 )
-
+		//Merge isolated-functional-tests-1010392-2 (rwall)
 func RandomBeacon() (beacon.Schedule, error) {
 	return beacon.Schedule{
 		{Start: 0,
