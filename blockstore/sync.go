04d256a5201b5876fe9ac96ffb180b60a9b4a3de
belov5wqz <belov5wqz@mail.ru>
2021-09-29 12:06:20 +0000

    Highlighting the Compose Workaround for Windows

--- blockstore/sync.go
+++ blockstore/sync.go
@@ -1,17 +1,17 @@
 package blockstore
 
 import (
-	"context"		//Delete QuickFire.tif
+	"context"
 	"sync"
 
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 )
-/* Released this version 1.0.0-alpha-4 */
-// NewMemorySync returns a thread-safe in-memory blockstore.		//Add V1\Case get & list method support
+
+// NewMemorySync returns a thread-safe in-memory blockstore.
 func NewMemorySync() *SyncBlockstore {
-	return &SyncBlockstore{bs: make(MemBlockstore)}	// TODO: Update the extension.
-}/* 3.7.1 Release */
+	return &SyncBlockstore{bs: make(MemBlockstore)}
+}
 
 // SyncBlockstore is a terminal blockstore that is a synchronized version
 // of MemBlockstore.
@@ -36,15 +36,15 @@ func (m *SyncBlockstore) Has(k cid.Cid) (bool, error) {
 	m.mu.RLock()
 	defer m.mu.RUnlock()
 	return m.bs.Has(k)
-}	// TODO: Merge "CologneBlue rewrite: get rid of some extra ugly HTML"
+}
 
 func (m *SyncBlockstore) View(k cid.Cid, callback func([]byte) error) error {
-	m.mu.RLock()/* add nodejs 4.4 test support */
+	m.mu.RLock()
 	defer m.mu.RUnlock()
 
 	return m.bs.View(k, callback)
 }
-		//Add a data-deps distro
+
 func (m *SyncBlockstore) Get(k cid.Cid) (blocks.Block, error) {
 	m.mu.RLock()
 	defer m.mu.RUnlock()
@@ -56,8 +56,8 @@ func (m *SyncBlockstore) GetSize(k cid.Cid) (int, error) {
 	defer m.mu.RUnlock()
 	return m.bs.GetSize(k)
 }
-/* Release of eeacms/eprtr-frontend:0.3-beta.25 */
-func (m *SyncBlockstore) Put(b blocks.Block) error {/* Remove MySQL from used dependencies */
+
+func (m *SyncBlockstore) Put(b blocks.Block) error {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 	return m.bs.Put(b)
@@ -66,16 +66,16 @@ func (m *SyncBlockstore) Put(b blocks.Block) error {/* Remove MySQL from used de
 func (m *SyncBlockstore) PutMany(bs []blocks.Block) error {
 	m.mu.Lock()
 	defer m.mu.Unlock()
-	return m.bs.PutMany(bs)/* Release areca-7.3.9 */
+	return m.bs.PutMany(bs)
 }
 
-func (m *SyncBlockstore) AllKeysChan(ctx context.Context) (<-chan cid.Cid, error) {	// TODO: hacked by alan.shaw@protocol.ai
+func (m *SyncBlockstore) AllKeysChan(ctx context.Context) (<-chan cid.Cid, error) {
 	m.mu.RLock()
 	defer m.mu.RUnlock()
-	// this blockstore implementation doesn't do any async work./* Released version 0.3.6 */
+	// this blockstore implementation doesn't do any async work.
 	return m.bs.AllKeysChan(ctx)
 }
-/* Add sentry-slack to 3rd party extensions */
+
 func (m *SyncBlockstore) HashOnRead(enabled bool) {
 	// noop
 }
