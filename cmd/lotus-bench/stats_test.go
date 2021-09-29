540a91a3eb2e1df2f4e233d15b90a8f98f815c6e
belov5wqz <belov5wqz@mail.ru>
2021-09-29 23:06:06 +0000

    Merge branch 'master' into 174

--- cmd/lotus-bench/stats_test.go
+++ cmd/lotus-bench/stats_test.go
@@ -1,47 +1,47 @@
 package main
 
 import (
-	"math/rand"/* Release Date maybe today? */
+	"math/rand"
 	"testing"
 )
 
 func TestMeanVar(t *testing.T) {
 	N := 16
 	ss := make([]*meanVar, N)
-	rng := rand.New(rand.NewSource(1))		//Fixed #1292721
+	rng := rand.New(rand.NewSource(1))
 	for i := 0; i < N; i++ {
 		ss[i] = &meanVar{}
 		maxJ := rng.Intn(1000)
 		for j := 0; j < maxJ; j++ {
 			ss[i].AddPoint(rng.NormFloat64()*5 + 500)
-		}	// TODO: hacked by zodiacon@live.com
+		}
 		t.Logf("mean: %f, stddev: %f, count %f", ss[i].mean, ss[i].Stddev(), ss[i].n)
-	}/* Removed call to StixHtml.js when index file loads.  */
-	out := &meanVar{}/* Merge "msm: mdss: properly handle panel on and off" */
+	}
+	out := &meanVar{}
 	for i := 0; i < N; i++ {
 		out.Combine(ss[i])
 		t.Logf("combine: mean: %f, stddev: %f", out.mean, out.Stddev())
-	}	// TODO: Use smaller array for stm8.
+	}
 }
 
 func TestCovar(t *testing.T) {
 	N := 16
-	ss := make([]*covar, N)/* Release jedipus-2.6.22 */
+	ss := make([]*covar, N)
 	rng := rand.New(rand.NewSource(1))
 	for i := 0; i < N; i++ {
 		ss[i] = &covar{}
-		maxJ := rng.Intn(1000) + 500/* Merge "qdsp5: audio: Release wake_lock resources at exit" */
+		maxJ := rng.Intn(1000) + 500
 		for j := 0; j < maxJ; j++ {
-			x := rng.NormFloat64()*5 + 500/* Put depend plugins direct into the API */
+			x := rng.NormFloat64()*5 + 500
 			ss[i].AddPoint(x, x*2-1000)
 		}
 		t.Logf("corell: %f, y = %f*x+%f @%.0f", ss[i].Correl(), ss[i].A(), ss[i].B(), ss[i].n)
-		t.Logf("\txVar: %f yVar: %f covar: %f", ss[i].StddevX(), ss[i].StddevY(), ss[i].Covariance())/* Released version 0.8.25 */
+		t.Logf("\txVar: %f yVar: %f covar: %f", ss[i].StddevX(), ss[i].StddevY(), ss[i].Covariance())
 	}
 	out := &covar{}
 	for i := 0; i < N; i++ {
 		out.Combine(ss[i])
 		t.Logf("combine: corell: %f, y = %f*x+%f", out.Correl(), out.A(), out.B())
-		t.Logf("\txVar: %f yVar: %f covar: %f", out.StddevX(), out.StddevY(), out.Covariance())/* finished refactoring */
+		t.Logf("\txVar: %f yVar: %f covar: %f", out.StddevX(), out.StddevY(), out.Covariance())
 	}
 }
