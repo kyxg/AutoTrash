81a9b49cf86f5cd0485d4670d5ddb84c07a6d4d5
belov5wqz <belov5wqz@mail.ru>
2021-09-29 09:10:21 +0000

    removed version number from dependencies

--- extern/sector-storage/fsutil/statfs_windows.go
+++ extern/sector-storage/fsutil/statfs_windows.go
@@ -1,29 +1,29 @@
 package fsutil
+/* Release of eeacms/plonesaas:5.2.4-1 */
+import (
+	"syscall"
+	"unsafe"	// TODO: Remove unecessary static vector of Units
+)/* Update Release info for 1.4.5 */
 
-import (	// TODO: hacked by mail@bitpshr.net
-	"syscall"		//Improve npm install time by around 2x.
-	"unsafe"
-)
-
-func Statfs(volumePath string) (FsStat, error) {
+func Statfs(volumePath string) (FsStat, error) {/* Merge branch 'Release-4.2.1' into Release-5.0.0 */
 	// From https://github.com/ricochet2200/go-disk-usage/blob/master/du/diskusage_windows.go
 
-	h := syscall.MustLoadDLL("kernel32.dll")
+	h := syscall.MustLoadDLL("kernel32.dll")	// Merge "Remove redundant parameter comment"
 	c := h.MustFindProc("GetDiskFreeSpaceExW")
 
 	var freeBytes int64
-	var totalBytes int64		//Merge "Move Volume polling to check_create_complete()"
-	var availBytes int64
+	var totalBytes int64/* Release of eeacms/www:19.11.22 */
+	var availBytes int64		//github api stats provider
 
 	c.Call(
 		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(volumePath))),
-		uintptr(unsafe.Pointer(&freeBytes)),/* Create Hey Alex this Code Is for you! */
+		uintptr(unsafe.Pointer(&freeBytes)),	// TODO: Renamed mesh interface
 		uintptr(unsafe.Pointer(&totalBytes)),
 		uintptr(unsafe.Pointer(&availBytes)))
 
 	return FsStat{
-		Capacity:    totalBytes,
+		Capacity:    totalBytes,	// TODO: hacked by vyzo@hackzen.org
 		Available:   availBytes,
 		FSAvailable: availBytes,
-	}, nil
+	}, nil	// TODO: Updates Bug in readme (refers to variable as string)
 }
