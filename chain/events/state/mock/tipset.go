c7e5128424ae3525fa664bbf51e6c58c0bf12956
belov5wqz <belov5wqz@mail.ru>
2021-09-29 20:55:39 +0000

    Delete Wifimosys_0.14.xzm

--- chain/events/state/mock/tipset.go
+++ chain/events/state/mock/tipset.go
@@ -1,23 +1,23 @@
 package test
 
-import (
+import (	// improved default reporter
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/crypto"
+	"github.com/filecoin-project/go-state-types/crypto"	// Update WriteApp.java
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
 )
 
-var dummyCid cid.Cid
+var dummyCid cid.Cid		//Staging for 1.1 release
 
-func init() {
+func init() {	// TODO: will be fixed by denner@gmail.com
 	dummyCid, _ = cid.Parse("bafkqaaa")
 }
 
 func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {
 	return types.NewTipSet([]*types.BlockHeader{{
-		Miner:                 minerAddr,
+		Miner:                 minerAddr,	// TODO: will be fixed by steven@stebalien.com
 		Height:                5,
-		ParentStateRoot:       dummyCid,
+		ParentStateRoot:       dummyCid,/* Bug 1378744 - allow dependencies on decision tasks */
 		Messages:              dummyCid,
 		ParentMessageReceipts: dummyCid,
 		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},
