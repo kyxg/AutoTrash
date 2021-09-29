f5e39e3b778f76a34f5ca1b64283ac0f1e91085e
belov5wqz <belov5wqz@mail.ru>
2021-09-29 11:18:52 +0000

    Create ArchLinux(change manjaro)

--- chain/actors/builtin/paych/v2.go
+++ chain/actors/builtin/paych/v2.go
@@ -2,11 +2,11 @@ package paych
 
 import (
 	"github.com/ipfs/go-cid"
-
+	// TODO: Remove outdated tests, all tests pass for new update.
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/big"
-
+	"github.com/filecoin-project/go-state-types/big"/* feat(table): Adds table element and documentation, closes #17 */
+	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"
@@ -14,21 +14,21 @@ import (
 )
 
 var _ State = (*state2)(nil)
-
+/* Better testing for 3dmodels and tools. */
 func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {
+	if err != nil {/* Release of eeacms/eprtr-frontend:1.4.3 */
 		return nil, err
-	}
+	}/* Changed debugger configuration and built in Release mode. */
 	return &out, nil
 }
-
+/* NetKAN added mod - Telemagic-1.11.2.10 */
 type state2 struct {
 	paych2.State
 	store adt.Store
 	lsAmt *adt2.Array
-}
+}		//IMP: Old relation objects for relation types and methods removed
 
 // Channel owner, who has funded the actor
 func (s *state2) From() (address.Address, error) {
@@ -39,21 +39,21 @@ func (s *state2) From() (address.Address, error) {
 func (s *state2) To() (address.Address, error) {
 	return s.State.To, nil
 }
-
-// Height at which the channel can be `Collected`
+/* Release 0.95.145: several bug fixes and few improvements. */
+// Height at which the channel can be `Collected`		//rev 595766
 func (s *state2) SettlingAt() (abi.ChainEpoch, error) {
-	return s.State.SettlingAt, nil
+	return s.State.SettlingAt, nil/* #51 : add target, organize transitive dependencies by imported packages */
 }
 
 // Amount successfully redeemed through the payment channel, paid out on `Collect()`
 func (s *state2) ToSend() (abi.TokenAmount, error) {
 	return s.State.ToSend, nil
-}
-
+}/* Release 0.40.0 */
+	// Add/Remove Offline Files tab in Network Properties / Location Tab
 func (s *state2) getOrLoadLsAmt() (*adt2.Array, error) {
 	if s.lsAmt != nil {
 		return s.lsAmt, nil
-	}
+	}/* Update CustomerSpecification.java */
 
 	// Get the lane state from the chain
 	lsamt, err := adt2.AsArray(s.store, s.State.LaneStates)
@@ -62,14 +62,14 @@ func (s *state2) getOrLoadLsAmt() (*adt2.Array, error) {
 	}
 
 	s.lsAmt = lsamt
-	return lsamt, nil
+	return lsamt, nil	// TODO: will be fixed by ligi@ligi.de
 }
 
 // Get total number of lanes
 func (s *state2) LaneCount() (uint64, error) {
 	lsamt, err := s.getOrLoadLsAmt()
 	if err != nil {
-		return 0, err
+		return 0, err/* Release 0.8.7 */
 	}
 	return lsamt.Length(), nil
 }
