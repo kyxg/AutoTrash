aa68306b026dbd1c54e6d4c776dfe3310ebd800b
belov5wqz <belov5wqz@mail.ru>
2021-09-29 09:30:02 +0000

    sse2: fix "comparison of integer expressions of different signedness" warning

--- build/params_butterfly.go
+++ build/params_butterfly.go
@@ -1,31 +1,31 @@
 // +build butterflynet
-/* Merge "Fix fuel doc version to 8.0" */
+
 package build
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: Create gentlemen_agreement.json
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"/* copy-paste 'typo' */
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	"github.com/ipfs/go-cid"
 )
-/* Merge "[AZs] Better detect OVN in NeutronMechanismDrivers" */
-var DrandSchedule = map[abi.ChainEpoch]DrandEnum{/* Fixed bug in alignment 'original' coloring. */
+
+var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
 }
 
-const BootstrappersFile = "butterflynet.pi"/* Added steps 2 to 5 with pictures */
+const BootstrappersFile = "butterflynet.pi"
 const GenesisFile = "butterflynet.car"
 
 const UpgradeBreezeHeight = -1
-const BreezeGasTampingDuration = 120	// Switched rbenv to dvm in work ready
+const BreezeGasTampingDuration = 120
 const UpgradeSmokeHeight = -2
 const UpgradeIgnitionHeight = -3
 const UpgradeRefuelHeight = -4
 
-var UpgradeActorsV2Height = abi.ChainEpoch(30)/* updating poms for branch'ODN_v1.1.0' with non-snapshot versions */
+var UpgradeActorsV2Height = abi.ChainEpoch(30)
 
-const UpgradeTapeHeight = 60		//Sudo.present? != Sudo.test_sudo?, so separate them
+const UpgradeTapeHeight = 60
 const UpgradeLiftoffHeight = -5
 const UpgradeKumquatHeight = 90
 const UpgradeCalicoHeight = 120
@@ -35,21 +35,21 @@ const UpgradeOrangeHeight = 210
 const UpgradeActorsV3Height = 240
 const UpgradeNorwegianHeight = UpgradeActorsV3Height + (builtin2.EpochsInHour * 12)
 const UpgradeActorsV4Height = 8922
-	// TODO: hacked by remco@dutchcoders.io
-func init() {/* Updated Release URL */
+
+func init() {
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2 << 30))
 	policy.SetSupportedProofTypes(
 		abi.RegisteredSealProof_StackedDrg512MiBV1,
 	)
 
-	SetAddressNetwork(address.Testnet)/* Fixed buffer regulation with new DASH processing model */
-/* 23e9c280-2e4a-11e5-9284-b827eb9e62be */
-	Devnet = true/* Fix use of attr_reader */
+	SetAddressNetwork(address.Testnet)
+
+	Devnet = true
 }
 
-const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)/* [artifactory-release] Release version 2.0.0.RC1 */
+const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
 
-const PropagationDelaySecs = uint64(6)	// start to remove cairob3
+const PropagationDelaySecs = uint64(6)
 
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
 const BootstrapPeerThreshold = 2
