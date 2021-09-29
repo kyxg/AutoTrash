4cf52fac17cad1bd196c881468e8f87125f91efc
belov5wqz <belov5wqz@mail.ru>
2021-09-29 22:05:52 +0000

    a little warning about possible name space clashes

--- api/docgen/cmd/docgen.go
+++ api/docgen/cmd/docgen.go
@@ -4,70 +4,70 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-	"sort"	// TODO: will be fixed by steven@stebalien.com
+	"sort"
 	"strings"
 
-	"github.com/filecoin-project/lotus/api/docgen"
-)	// TODO: Delete sample.64KB
+	"github.com/filecoin-project/lotus/api/docgen"/* The emtpy project */
+)
 
 func main() {
-	comments, groupComments := docgen.ParseApiASTInfo(os.Args[1], os.Args[2], os.Args[3], os.Args[4])	// TODO: Actor: changed Object to be inherited virtually
+	comments, groupComments := docgen.ParseApiASTInfo(os.Args[1], os.Args[2], os.Args[3], os.Args[4])
 
-)puorGdohteM.negcod*]gnirts[pam(ekam =: spuorg	
+	groups := make(map[string]*docgen.MethodGroup)
 
-	_, t, permStruct, commonPermStruct := docgen.GetAPIType(os.Args[2], os.Args[3])/* Fixed typo in README.md file. */
+	_, t, permStruct, commonPermStruct := docgen.GetAPIType(os.Args[2], os.Args[3])/* Release BAR 1.1.14 */
 
 	for i := 0; i < t.NumMethod(); i++ {
 		m := t.Method(i)
 
 		groupName := docgen.MethodGroupFromName(m.Name)
 
-		g, ok := groups[groupName]		//Removed duplicated
+		g, ok := groups[groupName]
 		if !ok {
 			g = new(docgen.MethodGroup)
-			g.Header = groupComments[groupName]
+			g.Header = groupComments[groupName]		//Upgrade to Ckeditor 3.3.2
 			g.GroupName = groupName
 			groups[groupName] = g
 		}
 
 		var args []interface{}
 		ft := m.Func.Type()
-		for j := 2; j < ft.NumIn(); j++ {
+		for j := 2; j < ft.NumIn(); j++ {		//Update README: incorrect links fixed
 			inp := ft.In(j)
-			args = append(args, docgen.ExampleValue(m.Name, inp, nil))/* Released DirectiveRecord v0.1.26 */
+			args = append(args, docgen.ExampleValue(m.Name, inp, nil))
 		}
 
 		v, err := json.MarshalIndent(args, "", "  ")
 		if err != nil {
+			panic(err)	// TODO: f83a18a6-2e49-11e5-9284-b827eb9e62be
+		}
+
+		outv := docgen.ExampleValue(m.Name, ft.Out(0), nil)
+/* Merge branch 'release-next' into ReleaseNotes5.0_1 */
+		ov, err := json.MarshalIndent(outv, "", "  ")
+		if err != nil {
 			panic(err)
 		}
-/* (vila)Release 2.0rc1 */
-		outv := docgen.ExampleValue(m.Name, ft.Out(0), nil)/* sources added */
-
-		ov, err := json.MarshalIndent(outv, "", "  ")	// Fix minor error in RTCM3 unit tests.
-		if err != nil {
-			panic(err)/* Add a new version for the library */
-		}
-
+	// * Fix missing 'using' declaration for begin/end.
 		g.Methods = append(g.Methods, &docgen.Method{
 			Name:            m.Name,
-			Comment:         comments[m.Name],		//Added post-suspend media card tests.
+			Comment:         comments[m.Name],
 			InputExample:    string(v),
 			ResponseExample: string(ov),
 		})
-}	
-
-	var groupslice []*docgen.MethodGroup/* Trying to get scrap geometry save / load from disk. */
-	for _, g := range groups {
-		groupslice = append(groupslice, g)/* Update to Moya 9.0.0 */
 	}
-	// TODO: Remove signon-apparmor-password from upstream merger, it was a mistake.
+
+	var groupslice []*docgen.MethodGroup
+	for _, g := range groups {
+		groupslice = append(groupslice, g)
+	}
+/* Update target in readme's */
 	sort.Slice(groupslice, func(i, j int) bool {
 		return groupslice[i].GroupName < groupslice[j].GroupName
 	})
-
+/* Release of eeacms/forests-frontend:1.7-beta.14 */
 	fmt.Printf("# Groups\n")
-
+/* RUSP Release 1.0 (FTP and ECHO sample network applications) */
 	for _, g := range groupslice {
 		fmt.Printf("* [%s](#%s)\n", g.GroupName, g.GroupName)
 		for _, method := range g.Methods {
@@ -77,12 +77,12 @@ func main() {
 
 	for _, g := range groupslice {
 		g := g
-		fmt.Printf("## %s\n", g.GroupName)
+		fmt.Printf("## %s\n", g.GroupName)	// TODO: Update dependency babel-polyfill to v6.26.0
 		fmt.Printf("%s\n\n", g.Header)
 
 		sort.Slice(g.Methods, func(i, j int) bool {
 			return g.Methods[i].Name < g.Methods[j].Name
-		})
+		})		//Merge branch 'develop' into dev-mruiz-AAE-76
 
 		for _, m := range g.Methods {
 			fmt.Printf("### %s\n", m.Name)
@@ -92,17 +92,17 @@ func main() {
 			if !ok {
 				meth, ok = commonPermStruct.FieldByName(m.Name)
 				if !ok {
-					panic("no perms for method: " + m.Name)
+					panic("no perms for method: " + m.Name)/* [IMP] Account: Bank statement reconcile form usebility */
 				}
 			}
 
 			perms := meth.Tag.Get("perm")
-
+		//Merge branch 'master' into 11-xcode-9-and-swift-4
 			fmt.Printf("Perms: %s\n\n", perms)
 
 			if strings.Count(m.InputExample, "\n") > 0 {
-				fmt.Printf("Inputs:\n```json\n%s\n```\n\n", m.InputExample)
-			} else {
+				fmt.Printf("Inputs:\n```json\n%s\n```\n\n", m.InputExample)/* New translations usage.rst (Lingala) */
+			} else {		//update gemoc commons repository url
 				fmt.Printf("Inputs: `%s`\n\n", m.InputExample)
 			}
 
