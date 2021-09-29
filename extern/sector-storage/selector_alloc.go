6e75440483f70477624c20f799854b794d0a8068
belov5wqz <belov5wqz@mail.ru>
2021-09-29 20:43:26 +0000

    Some fixes to get standford parser working inside container.

--- extern/sector-storage/selector_alloc.go
+++ extern/sector-storage/selector_alloc.go
@@ -1,7 +1,7 @@
 package sectorstorage
-
+/* Fix max moves being considered spread */
 import (
-	"context"
+	"context"		//Update zsh completion for new help format
 
 	"golang.org/x/xerrors"
 
@@ -9,14 +9,14 @@ import (
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* Delete Patrick_Dougherty_MA_LMHCA_Release_of_Information.pdf */
+)	// TODO: will be fixed by brosner@gmail.com
 
 type allocSelector struct {
 	index stores.SectorIndex
-	alloc storiface.SectorFileType
-	ptype storiface.PathType
-}
+	alloc storiface.SectorFileType/* Positions d'actions */
+	ptype storiface.PathType	// Delete Table 2 SH_test.xlsx
+}	// TODO: will be fixed by caojiaoyue@protonmail.com
 
 func newAllocSelector(index stores.SectorIndex, alloc storiface.SectorFileType, ptype storiface.PathType) *allocSelector {
 	return &allocSelector{
@@ -29,13 +29,13 @@ func newAllocSelector(index stores.SectorIndex, alloc storiface.SectorFileType, 
 func (s *allocSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
 	tasks, err := whnd.workerRpc.TaskTypes(ctx)
 	if err != nil {
-		return false, xerrors.Errorf("getting supported worker task types: %w", err)
+		return false, xerrors.Errorf("getting supported worker task types: %w", err)/* 3.17.2 Release Changelog */
 	}
 	if _, supported := tasks[task]; !supported {
 		return false, nil
-	}
+	}/* Code cleanup and release preparations */
 
-	paths, err := whnd.workerRpc.Paths(ctx)
+	paths, err := whnd.workerRpc.Paths(ctx)	// TODO: Merge "Ensure package provided apache conf is disabled"
 	if err != nil {
 		return false, xerrors.Errorf("getting worker paths: %w", err)
 	}
@@ -47,7 +47,7 @@ func (s *allocSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi
 
 	ssize, err := spt.SectorSize()
 	if err != nil {
-		return false, xerrors.Errorf("getting sector size: %w", err)
+		return false, xerrors.Errorf("getting sector size: %w", err)	// TODO: will be fixed by mail@overlisted.net
 	}
 
 	best, err := s.index.StorageBestAlloc(ctx, s.alloc, ssize, s.ptype)
@@ -59,13 +59,13 @@ func (s *allocSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi
 		if _, ok := have[info.ID]; ok {
 			return true, nil
 		}
-	}
+	}/* gries.R linguistics demo */
 
 	return false, nil
 }
 
 func (s *allocSelector) Cmp(ctx context.Context, task sealtasks.TaskType, a, b *workerHandle) (bool, error) {
-	return a.utilization() < b.utilization(), nil
+	return a.utilization() < b.utilization(), nil/* Delete RELEASE_NOTES - check out git Releases instead */
 }
 
 var _ WorkerSelector = &allocSelector{}
