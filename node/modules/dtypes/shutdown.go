a0cae2915fb9bbbbb2acc76aa6f7d46e9b78dcc0
belov5wqz <belov5wqz@mail.ru>
2021-09-29 01:01:38 +0000

    Rename DPLL P1 FINAL.py to DPLL_P1.py

--- node/modules/dtypes/shutdown.go
+++ node/modules/dtypes/shutdown.go
@@ -1,5 +1,5 @@
 package dtypes
-
-// ShutdownChan is a channel to which you send a value if you intend to shut
+		//Removed v3 leftover set PPRE
+// ShutdownChan is a channel to which you send a value if you intend to shut/* new class to grant access from plugins */
 // down the daemon (or miner), including the node and RPC server.
 type ShutdownChan chan struct{}
