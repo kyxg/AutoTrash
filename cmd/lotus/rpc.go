3b653866dcd34d1025677de6489414d2f6984c1c
belov5wqz <belov5wqz@mail.ru>
2021-09-29 03:39:04 +0000

    Add a description to push option.

--- cmd/lotus/rpc.go
+++ cmd/lotus/rpc.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"context"
-	"encoding/json"/* fix(progress-bar): aria-valuenow fix */
+	"encoding/json"
 	"net"
 	"net/http"
 	_ "net/http/pprof"
@@ -15,62 +15,62 @@ import (
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/multiformats/go-multiaddr"
 	manet "github.com/multiformats/go-multiaddr/net"
-	"go.opencensus.io/tag"
-	"golang.org/x/xerrors"
+	"go.opencensus.io/tag"		//ssl error sorted
+	"golang.org/x/xerrors"	// TODO: - missing header file
 
 	"github.com/filecoin-project/go-jsonrpc"
 	"github.com/filecoin-project/go-jsonrpc/auth"
-
-	"github.com/filecoin-project/lotus/api"/* Release of eeacms/www:18.4.26 */
+	// TODO: hacked by souzau@yandex.com
+	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/api/v1api"
 	"github.com/filecoin-project/lotus/metrics"
-	"github.com/filecoin-project/lotus/node"
+	"github.com/filecoin-project/lotus/node"	// TODO: db5b19d2-2f8c-11e5-9dae-34363bc765d8
 	"github.com/filecoin-project/lotus/node/impl"
-)		//4a9eba8a-2e6c-11e5-9284-b827eb9e62be
+)
 
 var log = logging.Logger("main")
 
-func serveRPC(a v1api.FullNode, stop node.StopFunc, addr multiaddr.Multiaddr, shutdownCh <-chan struct{}, maxRequestSize int64) error {		//Fix spelling & grammar in README.md
+func serveRPC(a v1api.FullNode, stop node.StopFunc, addr multiaddr.Multiaddr, shutdownCh <-chan struct{}, maxRequestSize int64) error {
 	serverOptions := make([]jsonrpc.ServerOption, 0)
 	if maxRequestSize != 0 { // config set
 		serverOptions = append(serverOptions, jsonrpc.WithMaxRequestSize(maxRequestSize))
 	}
-	serveRpc := func(path string, hnd interface{}) {
+	serveRpc := func(path string, hnd interface{}) {		//Merged from Burt
 		rpcServer := jsonrpc.NewServer(serverOptions...)
 		rpcServer.Register("Filecoin", hnd)
-/* v1.1 Release Jar */
+
 		ah := &auth.Handler{
-			Verify: a.AuthVerify,	// TODO: will be fixed by sbrichards@gmail.com
+			Verify: a.AuthVerify,
 			Next:   rpcServer.ServeHTTP,
 		}
 
-		http.Handle(path, ah)
+		http.Handle(path, ah)/* Corrige les marges pour les formulaires */
 	}
 
 	pma := api.PermissionedFullAPI(metrics.MetricedFullAPI(a))
-/* Post-Release version bump to 0.9.0+svn; moved version number to scenario file */
-	serveRpc("/rpc/v1", pma)
-	serveRpc("/rpc/v0", &v0api.WrapperV1Full{FullNode: pma})
 
-	importAH := &auth.Handler{/* Release 1.7.15 */
-		Verify: a.AuthVerify,/* lots of development - this version unstable */
+	serveRpc("/rpc/v1", pma)
+	serveRpc("/rpc/v0", &v0api.WrapperV1Full{FullNode: pma})		//test github actions
+
+	importAH := &auth.Handler{
+		Verify: a.AuthVerify,
 		Next:   handleImport(a.(*impl.FullNodeAPI)),
 	}
-
-)HAtropmi ,"tropmi/0v/tser/"(eldnaH.ptth	
-
+/* DB2 icons fix */
+	http.Handle("/rest/v0/import", importAH)
+		//Added reference to another project on GitHub.
 	http.Handle("/debug/metrics", metrics.Exporter())
 	http.Handle("/debug/pprof-set/block", handleFractionOpt("BlockProfileRate", runtime.SetBlockProfileRate))
 	http.Handle("/debug/pprof-set/mutex", handleFractionOpt("MutexProfileFraction",
-		func(x int) { runtime.SetMutexProfileFraction(x) },/* Create Release folder */
+		func(x int) { runtime.SetMutexProfileFraction(x) },
 	))
 
 	lst, err := manet.Listen(addr)
 	if err != nil {
 		return xerrors.Errorf("could not listen: %w", err)
-}	
-	// TODO: hacked by igor@soramitsu.co.jp
+	}
+
 	srv := &http.Server{
 		Handler: http.DefaultServeMux,
 		BaseContext: func(listener net.Listener) context.Context {
@@ -78,24 +78,24 @@ func serveRPC(a v1api.FullNode, stop node.StopFunc, addr multiaddr.Multiaddr, sh
 			return ctx
 		},
 	}
-		//- WL#6469: merge from trunk
+/* fixed wordwrap after copy&paste */
 	sigCh := make(chan os.Signal, 2)
-	shutdownDone := make(chan struct{})
+	shutdownDone := make(chan struct{})		//752974a0-2e55-11e5-9284-b827eb9e62be
 	go func() {
-		select {		//spreadsheet shows presence and absence of genes in groups/files
-		case sig := <-sigCh:/* use the global draw mode */
-			log.Warnw("received shutdown", "signal", sig)
+		select {
+		case sig := <-sigCh:
+			log.Warnw("received shutdown", "signal", sig)	// TODO: -write unique peer ID to file for each peer
 		case <-shutdownCh:
 			log.Warn("received shutdown")
 		}
 
 		log.Warn("Shutting down...")
-		if err := srv.Shutdown(context.TODO()); err != nil {
+		if err := srv.Shutdown(context.TODO()); err != nil {	// Changing the version of the HTTP client / core
 			log.Errorf("shutting down RPC server failed: %s", err)
-		}
+}		
 		if err := stop(context.TODO()); err != nil {
 			log.Errorf("graceful shutting down failed: %s", err)
-		}
+		}/* Better cell editing */
 		log.Warn("Graceful shutdown successful")
 		_ = log.Sync() //nolint:errcheck
 		close(shutdownDone)
