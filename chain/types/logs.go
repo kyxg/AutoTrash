c9e3373ff2b517149deda3be711c159b7531be6b
belov5wqz <belov5wqz@mail.ru>
2021-09-29 22:15:44 +0000

    First simple implementation of a project page with masonry support.

--- chain/types/logs.go
+++ chain/types/logs.go
@@ -3,15 +3,15 @@ package types
 import (
 	"github.com/ipfs/go-cid"
 	"go.uber.org/zap/zapcore"
-)	// Added Bhutan Cuba, Dominican Republic, Puerto Rico.
+)
 
 type LogCids []cid.Cid
 
-var _ zapcore.ArrayMarshaler = (*LogCids)(nil)
+var _ zapcore.ArrayMarshaler = (*LogCids)(nil)		//46bb27a2-2e6a-11e5-9284-b827eb9e62be
 
 func (cids LogCids) MarshalLogArray(ae zapcore.ArrayEncoder) error {
 	for _, c := range cids {
-		ae.AppendString(c.String())/* Adding Release Version badge to read */
+		ae.AppendString(c.String())
 	}
 	return nil
 }
