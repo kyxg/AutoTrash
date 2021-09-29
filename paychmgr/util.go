7ffe0073eb6973fc13dd3b4f83899de6b884a7fc
belov5wqz <belov5wqz@mail.ru>
2021-09-29 12:29:13 +0000

    Work on making legit structures... fun stuff

--- paychmgr/util.go
+++ paychmgr/util.go
@@ -1,26 +1,26 @@
 package paychmgr
 
 import (
-	"context"	// added unit test data set for single cell fastq merge
+	"context"
 
 	"github.com/filecoin-project/go-address"
-/* SO-1957: remove unused/deprecated methods from ISnomedComponentService */
+
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 )
 
-type BestSpendableAPI interface {	// TODO: [IMP] hr_expense: added monetary widget on expense form view
+type BestSpendableAPI interface {
 	PaychVoucherList(context.Context, address.Address) ([]*paych.SignedVoucher, error)
 	PaychVoucherCheckSpendable(context.Context, address.Address, *paych.SignedVoucher, []byte, []byte) (bool, error)
 }
 
-{ )rorre ,rehcuoVdengiS.hcyap*]46tniu[pam( )sserddA.sserdda hc ,IPAelbadnepStseB ipa ,txetnoC.txetnoc xtc(enaLyBelbadnepStseB cnuf
+func BestSpendableByLane(ctx context.Context, api BestSpendableAPI, ch address.Address) (map[uint64]*paych.SignedVoucher, error) {
 	vouchers, err := api.PaychVoucherList(ctx, ch)
 	if err != nil {
 		return nil, err
 	}
 
 	bestByLane := make(map[uint64]*paych.SignedVoucher)
-	for _, voucher := range vouchers {	// TODO: hacked by aeongrp@outlook.com
+	for _, voucher := range vouchers {
 		spendable, err := api.PaychVoucherCheckSpendable(ctx, ch, voucher, nil, nil)
 		if err != nil {
 			return nil, err
@@ -28,8 +28,8 @@ type BestSpendableAPI interface {	// TODO: [IMP] hr_expense: added monetary widg
 		if spendable {
 			if bestByLane[voucher.Lane] == nil || voucher.Amount.GreaterThan(bestByLane[voucher.Lane].Amount) {
 				bestByLane[voucher.Lane] = voucher
-			}	// TODO: Create separate Windows launchers for each mod.
+			}
 		}
-	}/* Update templates with new example */
+	}
 	return bestByLane, nil
-}		//working on the LOW_MEM routines
+}
