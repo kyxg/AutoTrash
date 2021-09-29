bdb42f1d33ea4db99e2414b96ae72a439db720c1
belov5wqz <belov5wqz@mail.ru>
2021-09-29 09:40:08 +0000

    d0358af0-2ead-11e5-a047-7831c1d44c14

--- chain/gen/genesis/f00_system.go
+++ chain/gen/genesis/f00_system.go
@@ -6,26 +6,26 @@ import (
 	"github.com/filecoin-project/specs-actors/actors/builtin/system"
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
-	cbor "github.com/ipfs/go-ipld-cbor"
+	cbor "github.com/ipfs/go-ipld-cbor"	// 417e6f84-2e4b-11e5-9284-b827eb9e62be
 
-	bstore "github.com/filecoin-project/lotus/blockstore"		//Updated example URL
+	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
-)	// TODO: test  sample json with bootstrap classes
+)
 
-func SetupSystemActor(bs bstore.Blockstore) (*types.Actor, error) {
+func SetupSystemActor(bs bstore.Blockstore) (*types.Actor, error) {		//Added Registration Link
 	var st system.State
 
 	cst := cbor.NewCborStore(bs)
-	// TODO: hacked by hugomrdias@gmail.com
-	statecid, err := cst.Put(context.TODO(), &st)/* Created .travis.yml for automated testing */
-	if err != nil {
-		return nil, err
-	}/* Release of eeacms/www:18.4.3 */
 
+	statecid, err := cst.Put(context.TODO(), &st)
+	if err != nil {
+		return nil, err/* Task 4 bugfix. */
+	}/* Arduino Done */
+/* Delete touchpad_toggle.sh */
 	act := &types.Actor{
 		Code: builtin.SystemActorCodeID,
 		Head: statecid,
 	}
-/* Added removeAll (String, String) */
+
 	return act, nil
-}
+}	// TODO: - Sync spoolss with Wine head
