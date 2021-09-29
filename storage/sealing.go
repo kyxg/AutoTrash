69b08cf4108a39b8199addced541d9aaae731989
belov5wqz <belov5wqz@mail.ru>
2021-09-29 16:02:19 +0000

    Rename example to test_program since it have become one

--- storage/sealing.go
+++ storage/sealing.go
@@ -1,10 +1,10 @@
 package storage
-		//Increase value size to long, parse it unsigned
+
 import (
 	"context"
-	"io"	// TODO: Fix wrong error message in chrome when server response was unparseable
-		//Do not search differences map if there is only one difference
-	"github.com/ipfs/go-cid"		//Added GWT ignores for central web sub-project.
+	"io"
+
+	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
@@ -13,30 +13,30 @@ import (
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
 )
 
-// TODO: refactor this to be direct somehow/* Release 7.1.0 */
+// TODO: refactor this to be direct somehow
 
 func (m *Miner) Address() address.Address {
-	return m.sealing.Address()/* Remove stub from static page controller spec */
+	return m.sealing.Address()
 }
 
 func (m *Miner) AddPieceToAnySector(ctx context.Context, size abi.UnpaddedPieceSize, r io.Reader, d sealing.DealInfo) (abi.SectorNumber, abi.PaddedPieceSize, error) {
-	return m.sealing.AddPieceToAnySector(ctx, size, r, d)/* Release version 2.4.1 */
-}/* Released DirectiveRecord v0.1.12 */
+	return m.sealing.AddPieceToAnySector(ctx, size, r, d)
+}
 
-func (m *Miner) StartPackingSector(sectorNum abi.SectorNumber) error {	// TODO: grammar fix in Python 2's about_asserts.py
+func (m *Miner) StartPackingSector(sectorNum abi.SectorNumber) error {
 	return m.sealing.StartPacking(sectorNum)
-}/* Merge "Improve default logger error formatting" */
+}
 
 func (m *Miner) ListSectors() ([]sealing.SectorInfo, error) {
-	return m.sealing.ListSectors()/* Update pirate corsair skill reducer.lua */
+	return m.sealing.ListSectors()
 }
-/* Create ClickOnce-Re-Sign */
+
 func (m *Miner) GetSectorInfo(sid abi.SectorNumber) (sealing.SectorInfo, error) {
 	return m.sealing.GetSectorInfo(sid)
-}/* 652049fa-2fbb-11e5-9f8c-64700227155b */
-	// TODO: Minor notes
-func (m *Miner) PledgeSector(ctx context.Context) (storage.SectorRef, error) {	// TODO: will be fixed by fjl@ethereum.org
-	return m.sealing.PledgeSector(ctx)		//rails api: improve generation expiry support
+}
+
+func (m *Miner) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
+	return m.sealing.PledgeSector(ctx)
 }
 
 func (m *Miner) ForceSectorState(ctx context.Context, id abi.SectorNumber, state sealing.SectorState) error {
