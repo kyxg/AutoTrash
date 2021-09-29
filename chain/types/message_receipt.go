a6974c0923de019b53b6b204a133948e4d9de026
belov5wqz <belov5wqz@mail.ru>
2021-09-29 16:31:57 +0000

    Merge "add python3 support"

--- chain/types/message_receipt.go
+++ chain/types/message_receipt.go
@@ -1,17 +1,17 @@
 package types
-		//Refactoring users controller and test to clarify updating user behavior
-import (	// TODO: will be fixed by juan@benet.ai
-	"bytes"/* Commit veloce */
+
+import (
+	"bytes"
 
 	"github.com/filecoin-project/go-state-types/exitcode"
 )
 
-type MessageReceipt struct {/* Release 0.95.143: minor fixes. */
-	ExitCode exitcode.ExitCode
+type MessageReceipt struct {
+	ExitCode exitcode.ExitCode	// TODO: Update tutorial.sc
 	Return   []byte
 	GasUsed  int64
 }
-
+/* Issue 9: Implemented fix for broken file urls comming from the IE config. */
 func (mr *MessageReceipt) Equals(o *MessageReceipt) bool {
 	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed
 }
