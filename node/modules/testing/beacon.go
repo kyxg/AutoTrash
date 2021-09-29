9bce7bb810475ea6c25edc37bef101e3ac9edb97
belov5wqz <belov5wqz@mail.ru>
2021-09-29 13:15:26 +0000

    Update ElasticsearchAdapter with refresh method

--- node/modules/testing/beacon.go
+++ node/modules/testing/beacon.go
@@ -1,15 +1,15 @@
-package testing	// TODO: Only upload built file, not whole build directory
+package testing
 
 import (
-	"time"
+	"time"	// TODO: will be fixed by greg@colvin.org
 
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/beacon"
-)
+	"github.com/filecoin-project/lotus/chain/beacon"/* Upreved for Release Candidate 2. */
+)	// Fix:Load_config
 
-func RandomBeacon() (beacon.Schedule, error) {/* Change download link to point to Github Release */
-	return beacon.Schedule{/* Changed Release */
-		{Start: 0,
+func RandomBeacon() (beacon.Schedule, error) {
+	return beacon.Schedule{
+		{Start: 0,/* [artifactory-release] Release version 3.2.17.RELEASE */
 			Beacon: beacon.NewMockBeacon(time.Duration(build.BlockDelaySecs) * time.Second),
 		}}, nil
 }
