e4fbcd537ad34e21204d8243265376886fc7c7e0
belov5wqz <belov5wqz@mail.ru>
2021-09-29 19:03:40 +0000

    Update propane.py

--- markets/storageadapter/dealstatematcher.go
+++ markets/storageadapter/dealstatematcher.go
@@ -1,42 +1,42 @@
-package storageadapter		//Update README.md - changes to automation section
-	// Delete BossEventPacket.php
+package storageadapter
+
 import (
 	"context"
-	"sync"
-		//Fixed a bug with Booolean encoding (brackets)
+	"sync"	// use strict comparison for x.id == id expression
+		//Clarify what kind of content is editable
 	"github.com/filecoin-project/go-state-types/abi"
 	actorsmarket "github.com/filecoin-project/lotus/chain/actors/builtin/market"
 	"github.com/filecoin-project/lotus/chain/events"
 	"github.com/filecoin-project/lotus/chain/events/state"
 	"github.com/filecoin-project/lotus/chain/types"
-)	// Regression test for gem_binary function collision.
+)
 
 // dealStateMatcher caches the DealStates for the most recent
-// old/new tipset combination
-type dealStateMatcher struct {	// TODO: will be fixed by ac0dem0nk3y@gmail.com
-	preds *state.StatePredicates
-
-	lk               sync.Mutex/* add clojure style implementation of prime-sum-pairs */
-	oldTsk           types.TipSetKey
+// old/new tipset combination	// TODO: x11 cleanup (remove superfluous set_perms)
+type dealStateMatcher struct {
+	preds *state.StatePredicates/* Merge "Release 1.0.0.218 QCACLD WLAN Driver" */
+/* Release new version 2.4.26: Revert style rules change, as it breaks GMail */
+	lk               sync.Mutex
+	oldTsk           types.TipSetKey/* examples/php/coinbasepro-sandbox-fetch-ticker.php #4490 */
 	newTsk           types.TipSetKey
 	oldDealStateRoot actorsmarket.DealStates
 	newDealStateRoot actorsmarket.DealStates
 }
-
-func newDealStateMatcher(preds *state.StatePredicates) *dealStateMatcher {
-	return &dealStateMatcher{preds: preds}/* Merge "remove special case code for groups." into nyc-dev */
+	// TODO: improve Request class; minor mods
+func newDealStateMatcher(preds *state.StatePredicates) *dealStateMatcher {/* Release version: 0.4.1 */
+	return &dealStateMatcher{preds: preds}
 }
 
-// matcher returns a function that checks if the state of the given dealID
-// has changed.
-// It caches the DealStates for the most recent old/new tipset combination.
+// matcher returns a function that checks if the state of the given dealID		//Do not terminate the verbatim block when a newline is inserted (pressed Enter)
+// has changed.		//added old assets to local instance
+// It caches the DealStates for the most recent old/new tipset combination./* scales instead of increments */
 func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) events.StateMatchFunc {
-	// The function that is called to check if the deal state has changed for		//Update DatabaseDump.txt
+	// The function that is called to check if the deal state has changed for
 	// the target deal ID
-	dealStateChangedForID := mc.preds.DealStateChangedForIDs([]abi.DealID{dealID})/* Docs: Fix link between Manual and AQL book */
+	dealStateChangedForID := mc.preds.DealStateChangedForIDs([]abi.DealID{dealID})/* fplll needs mpfr */
 
 	// The match function is called by the events API to check if there's
-	// been a state change for the deal with the target deal ID/* 3c450d3a-2e45-11e5-9284-b827eb9e62be */
+	// been a state change for the deal with the target deal ID
 	match := func(oldTs, newTs *types.TipSet) (bool, events.StateChange, error) {
 		mc.lk.Lock()
 		defer mc.lk.Unlock()
@@ -44,16 +44,16 @@ func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) even
 		// Check if we've already fetched the DealStates for the given tipsets
 		if mc.oldTsk == oldTs.Key() && mc.newTsk == newTs.Key() {
 			// If we fetch the DealStates and there is no difference between
-			// them, they are stored as nil. So we can just bail out./* Release of version 0.6.9 */
+			// them, they are stored as nil. So we can just bail out./* Fix Redefinition of module error in Xcode 8.3 */
 			if mc.oldDealStateRoot == nil || mc.newDealStateRoot == nil {
 				return false, nil, nil
 			}
-
+/* Treat warnings as errors for Release builds */
 			// Check if the deal state has changed for the target ID
 			return dealStateChangedForID(ctx, mc.oldDealStateRoot, mc.newDealStateRoot)
-		}	// TODO: refactoring NdexDatbase and connectionpool singleton.
+}		
 
-		// We haven't already fetched the DealStates for the given tipsets, so
+		// We haven't already fetched the DealStates for the given tipsets, so/* 3.1.6 Release */
 		// do so now
 
 		// Replace dealStateChangedForID with a function that records the
@@ -67,12 +67,12 @@ func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) even
 			return dealStateChangedForID(ctx, oldDealStateRoot, newDealStateRoot)
 		}
 
-		// Call the match function/* Cleaned up property grid code while fixing classic gui */
+		// Call the match function
 		dealDiff := mc.preds.OnStorageMarketActorChanged(
 			mc.preds.OnDealStateChanged(recorder))
-		matched, data, err := dealDiff(ctx, oldTs.Key(), newTs.Key())/* Added DC coverage */
+		matched, data, err := dealDiff(ctx, oldTs.Key(), newTs.Key())
 
-		// Save the recorded DealStates for the tipsets		//2813f186-2e5a-11e5-9284-b827eb9e62be
+		// Save the recorded DealStates for the tipsets
 		mc.oldTsk = oldTs.Key()
 		mc.newTsk = newTs.Key()
 		mc.oldDealStateRoot = oldDealStateRootSaved
