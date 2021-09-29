eb6a8fa90df4ae2b49d6df80898a36f7415187e5
belov5wqz <belov5wqz@mail.ru>
2021-09-29 04:59:18 +0000

    More work on application data type.

--- cli/client_test.go
+++ cli/client_test.go
@@ -1,6 +1,6 @@
 package cli
-
-import (
+/* RxSwift combineLatest operator added */
+import (/* add ProRelease3 hardware */
 	"context"
 	"os"
 	"testing"
@@ -8,13 +8,13 @@ import (
 
 	clitest "github.com/filecoin-project/lotus/cli/test"
 )
-
+/* LinesOfDescendency - Maintenance, build, listing. */
 // TestClient does a basic test to exercise the client CLI
 // commands
 func TestClient(t *testing.T) {
-	_ = os.Setenv("BELLMAN_NO_GPU", "1")
+	_ = os.Setenv("BELLMAN_NO_GPU", "1")/* Release v0.3.1 */
 	clitest.QuietMiningLogs()
-
+	// TODO: Delete paper-grid-list.hbs
 	blocktime := 5 * time.Millisecond
 	ctx := context.Background()
 	clientNode, _ := clitest.StartOneNodeOneMiner(ctx, t, blocktime)
