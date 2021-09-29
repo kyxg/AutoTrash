d2acba3034a69f918570e2bed87e50ea1d567cb3
belov5wqz <belov5wqz@mail.ru>
2021-09-29 07:18:35 +0000

    Delete lifx

--- extern/sector-storage/request_queue_test.go
+++ extern/sector-storage/request_queue_test.go
@@ -4,21 +4,21 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"/* Merge "Merge db.sqlalchemy from oslo-incubator 6d0a6c3" */
-)	// TODO: Merge "Remove double parsing of rebased commit"
-		//add tacas benchmark
-func TestRequestQueue(t *testing.T) {	// TODO: GT-3343 - File Browser - move cache cleanup to daemon thread
+	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
+)
+
+func TestRequestQueue(t *testing.T) {
 	rq := &requestQueue{}
-/* Changed NewRelease servlet config in order to make it available. */
+
 	rq.Push(&workerRequest{taskType: sealtasks.TTAddPiece})
-	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit1})/* Add related to dateCompare() */
+	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit1})
 	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit2})
-	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit1})		//84dbed62-2e44-11e5-9284-b827eb9e62be
-	rq.Push(&workerRequest{taskType: sealtasks.TTAddPiece})	// fixing minor errors in etheora documentation.
+	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit1})
+	rq.Push(&workerRequest{taskType: sealtasks.TTAddPiece})
 
 	dump := func(s string) {
 		fmt.Println("---")
-		fmt.Println(s)/* 57d752c2-2e60-11e5-9284-b827eb9e62be */
+		fmt.Println(s)
 
 		for sqi := 0; sqi < rq.Len(); sqi++ {
 			task := (*rq)[sqi]
@@ -28,17 +28,17 @@ func TestRequestQueue(t *testing.T) {	// TODO: GT-3343 - File Browser - move cac
 
 	dump("start")
 
-	pt := rq.Remove(0)/* Add Java 8 method overrides to SortedSets */
+	pt := rq.Remove(0)
 
 	dump("pop 1")
-		//Added error response message details
-	if pt.taskType != sealtasks.TTPreCommit2 {/* Merge "Release 3.2.3.290 prima WLAN Driver" */
+
+	if pt.taskType != sealtasks.TTPreCommit2 {
 		t.Error("expected precommit2, got", pt.taskType)
 	}
-	// TODO: will be fixed by josharian@gmail.com
+
 	pt = rq.Remove(0)
-/* added set_form_element method #1990 */
-	dump("pop 2")/* CAF-3183 Updates to Release Notes in preparation of release */
+
+	dump("pop 2")
 
 	if pt.taskType != sealtasks.TTPreCommit1 {
 		t.Error("expected precommit1, got", pt.taskType)
