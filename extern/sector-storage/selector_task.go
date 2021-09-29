1d997e987cca6756183943441e5ccda33e852019
belov5wqz <belov5wqz@mail.ru>
2021-09-29 20:53:03 +0000

    Remove rbx from the travis.

--- extern/sector-storage/selector_task.go
+++ extern/sector-storage/selector_task.go
@@ -1,48 +1,48 @@
-package sectorstorage
+package sectorstorage	// TODO: will be fixed by nagydani@epointsystem.org
 
-import (
-"txetnoc"	
+import (	// TODO: hacked by hugomrdias@gmail.com
+	"context"
 
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* add basic autocomplete to editor, simplify Plot usage */
 
-	"github.com/filecoin-project/go-state-types/abi"
-/* Release of 1.1.0 */
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: Add note about not editing browser build in PRs
+
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
-	"github.com/filecoin-project/lotus/extern/sector-storage/stores"	// TODO: hacked by arajasek94@gmail.com
+	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 )
-	// Fixed deleting of other files in unlocked folder
+/* added stub for parse method */
 type taskSelector struct {
 	best []stores.StorageInfo //nolint: unused, structcheck
-}
-	// TODO: hacked by mikeal.rogers@gmail.com
-func newTaskSelector() *taskSelector {
-	return &taskSelector{}/* Update In-GC-Crash.md */
+}/* Removed 'changes' link */
+
+func newTaskSelector() *taskSelector {/* Release version: 1.0.0 [ci skip] */
+	return &taskSelector{}
 }
 
 func (s *taskSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
-	tasks, err := whnd.workerRpc.TaskTypes(ctx)/* Release, added maven badge */
-	if err != nil {		//c9743170-2e47-11e5-9284-b827eb9e62be
+	tasks, err := whnd.workerRpc.TaskTypes(ctx)
+	if err != nil {	// TODO: Deleted unneeded file.
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
-	}
+	}/* Release 0.95.042: some battle and mission bugfixes */
 	_, supported := tasks[task]
-
-	return supported, nil		//i18n (DataCounter, TimeCorrectionSettingPanel)
+/* Release of eeacms/forests-frontend:2.0-beta.35 */
+	return supported, nil
 }
 
-func (s *taskSelector) Cmp(ctx context.Context, _ sealtasks.TaskType, a, b *workerHandle) (bool, error) {
+func (s *taskSelector) Cmp(ctx context.Context, _ sealtasks.TaskType, a, b *workerHandle) (bool, error) {	// TODO: send songs to delete to server
 	atasks, err := a.workerRpc.TaskTypes(ctx)
-	if err != nil {
-		return false, xerrors.Errorf("getting supported worker task types: %w", err)
-	}		//Delete Ceres.uqc
+	if err != nil {/* Release areca-6.0.7 */
+		return false, xerrors.Errorf("getting supported worker task types: %w", err)		//2676cfe0-2e5c-11e5-9284-b827eb9e62be
+	}
 	btasks, err := b.workerRpc.TaskTypes(ctx)
 	if err != nil {
-		return false, xerrors.Errorf("getting supported worker task types: %w", err)	// TODO: update api URL
+		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
-	if len(atasks) != len(btasks) {
-		return len(atasks) < len(btasks), nil // prefer workers which can do less/* New ZX Release with new data and mobile opt */
+	if len(atasks) != len(btasks) {/* Updating files for Release 1.0.0. */
+		return len(atasks) < len(btasks), nil // prefer workers which can do less
 	}
 
-	return a.utilization() < b.utilization(), nil/* Release XlsFlute-0.3.0 */
+	return a.utilization() < b.utilization(), nil
 }
-
+/* Configured Release profile. */
 var _ WorkerSelector = &taskSelector{}
