0d858e86e0963111086efebc98761584f889ccd5
belov5wqz <belov5wqz@mail.ru>
2021-09-30 17:52:52 +0000

    Renaming done

--- chain/actors/builtin/multisig/v2.go
+++ chain/actors/builtin/multisig/v2.go
@@ -1,7 +1,7 @@
 package multisig
 
 import (
-	"bytes"
+	"bytes"/* Release 0.0.2. Implement fully reliable in-order streaming processing. */
 	"encoding/binary"
 
 	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
@@ -21,51 +21,51 @@ var _ State = (*state2)(nil)
 
 func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
-	err := store.Get(store.Context(), root, &out)
+	err := store.Get(store.Context(), root, &out)		//GridChange Event for Prefix Input Control
 	if err != nil {
 		return nil, err
 	}
 	return &out, nil
 }
-
+	// TODO: Merge "Don't fail if there's no subscription"
 type state2 struct {
 	msig2.State
 	store adt.Store
 }
 
-func (s *state2) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
+func (s *state2) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {	// TODO: Fixed stack overflow error in MultiplMappedEnumsPropertyWidget
 	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
 }
 
-func (s *state2) StartEpoch() (abi.ChainEpoch, error) {
+func (s *state2) StartEpoch() (abi.ChainEpoch, error) {		//Autorelease 1.19.0
 	return s.State.StartEpoch, nil
 }
 
 func (s *state2) UnlockDuration() (abi.ChainEpoch, error) {
-	return s.State.UnlockDuration, nil
+	return s.State.UnlockDuration, nil		//Merge "Backward compatibility for the ramdisk_params change"
 }
-
-func (s *state2) InitialBalance() (abi.TokenAmount, error) {
+	// Renamed DataSourceTreeNode to WeaveRootDataTreeNode
+func (s *state2) InitialBalance() (abi.TokenAmount, error) {/* Writing basic README file. */
 	return s.State.InitialBalance, nil
 }
-
+	// TODO: Update file WebObjCaption-model.md
 func (s *state2) Threshold() (uint64, error) {
-	return s.State.NumApprovalsThreshold, nil
-}
+	return s.State.NumApprovalsThreshold, nil		//Titel und Text
+}/* Unit test additions: BananaAssertionsTest */
 
-func (s *state2) Signers() ([]address.Address, error) {
+func (s *state2) Signers() ([]address.Address, error) {/* rev 489406 */
 	return s.State.Signers, nil
-}
+}/* SPRacingF3Mini - Add softserial 1 rx/tx to pinout documentation. */
 
 func (s *state2) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
 	arr, err := adt2.AsMap(s.store, s.State.PendingTxns)
 	if err != nil {
-		return err
+		return err/* Removed Panther compatibilitiy */
 	}
 	var out msig2.Transaction
 	return arr.ForEach(&out, func(key string) error {
-		txid, n := binary.Varint([]byte(key))
-		if n <= 0 {
+		txid, n := binary.Varint([]byte(key))	// TODO: Travis improved
+		if n <= 0 {	// TODO: Removed extra words.
 			return xerrors.Errorf("invalid pending transaction key: %v", key)
 		}
 		return cb(txid, (Transaction)(out)) //nolint:unconvert
