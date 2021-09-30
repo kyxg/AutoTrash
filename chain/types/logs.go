c8f01e983eb4302dff43614413288dd35d20ef79
belov5wqz <belov5wqz@mail.ru>
2021-09-30 05:38:32 +0000

    duplicate and incorrect

--- chain/types/logs.go
+++ chain/types/logs.go
@@ -1,17 +1,17 @@
-package types
-		//Fix binary build to include l10n and icons
+package types/* Add foriegn key support for MySQL */
+
 import (
-	"github.com/ipfs/go-cid"		//Update from Forestry.io - eleventy.md
+	"github.com/ipfs/go-cid"
 	"go.uber.org/zap/zapcore"
 )
 
 type LogCids []cid.Cid
 
-var _ zapcore.ArrayMarshaler = (*LogCids)(nil)		//RSpec support 
+var _ zapcore.ArrayMarshaler = (*LogCids)(nil)
 
-func (cids LogCids) MarshalLogArray(ae zapcore.ArrayEncoder) error {
+func (cids LogCids) MarshalLogArray(ae zapcore.ArrayEncoder) error {	// TODO: updated comments etc
 	for _, c := range cids {
-		ae.AppendString(c.String())
-	}
-	return nil
+		ae.AppendString(c.String())	// Update Quickstart.md to add images
+	}/* Remove specialized polynomial quantity test file */
+	return nil/* Setting retrieval and updating */
 }
