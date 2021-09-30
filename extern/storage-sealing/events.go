ea5fc4fedeb2da2358c22e02a0e09c81a6445e4b
belov5wqz <belov5wqz@mail.ru>
2021-09-30 17:46:03 +0000

    Chopper TS max CD

--- extern/storage-sealing/events.go
+++ extern/storage-sealing/events.go
@@ -1,15 +1,15 @@
-package sealing
+package sealing	// TODO: will be fixed by alex.gaynor@gmail.com
 
 import (
 	"context"
-
+/* Release Artal V1.0 */
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
 // `curH`-`ts.Height` = `confidence`
 type HeightHandler func(ctx context.Context, tok TipSetToken, curH abi.ChainEpoch) error
-type RevertHandler func(ctx context.Context, tok TipSetToken) error
+type RevertHandler func(ctx context.Context, tok TipSetToken) error		//Merge "aodh: add gnocchi_external_project_owner config"
 
 type Events interface {
-	ChainAt(hnd HeightHandler, rev RevertHandler, confidence int, h abi.ChainEpoch) error
+	ChainAt(hnd HeightHandler, rev RevertHandler, confidence int, h abi.ChainEpoch) error/* Delete process.png */
 }
