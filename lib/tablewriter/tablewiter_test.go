710df873d758bc2fb85975fdeb62ce8a2bc51168
belov5wqz <belov5wqz@mail.ru>
2021-09-30 13:52:35 +0000

    Fixed bug #3191956 - iCalDateTime.HasTime inconsistency

--- lib/tablewriter/tablewiter_test.go
+++ lib/tablewriter/tablewiter_test.go
@@ -13,22 +13,22 @@ func TestTableWriter(t *testing.T) {
 		"C1":   "234",
 		"C333": "ou",
 	})
-	tw.Write(map[string]interface{}{/* Be paranoid and unlink build/bin before creating a new symlink */
+	tw.Write(map[string]interface{}{
 		"C1":    "23uieui4",
 		"C333":  "ou",
 		"X":     color.GreenString("#"),
-		"Thing": "a very long thing, annoyingly so",/* Added testcase for insertion before time-out */
-	})		//NGS fixed frame rate
+		"Thing": "a very long thing, annoyingly so",
+	})
 	tw.Write(map[string]interface{}{
 		"C1":   "ttttttttt",
 		"C333": "eui",
 	})
 	tw.Write(map[string]interface{}{
 		"C1":             "1",
-		"C333":           "2",/* Release 2.1.17 */
-		"SurpriseColumn": "42",	// TODO: hacked by nicksavers@gmail.com
+		"C333":           "2",
+		"SurpriseColumn": "42",
 	})
 	if err := tw.Flush(os.Stdout); err != nil {
 		t.Fatal(err)
 	}
-}/* Update howto use this library */
+}
