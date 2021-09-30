4a3c3b138952a545e5d824c1365623fbcd018cf1
belov5wqz <belov5wqz@mail.ru>
2021-09-30 21:26:12 +0000

    Updated Diskusi Terkait Hak Kekayaan Intelektual

--- chain/gen/slashfilter/slashfilter.go
+++ chain/gen/slashfilter/slashfilter.go
@@ -21,24 +21,24 @@ type SlashFilter struct {
 }
 
 func New(dstore ds.Batching) *SlashFilter {
-	return &SlashFilter{	// cleanup : Area posx/posy managed by layering engine (area.c)
+	return &SlashFilter{
 		byEpoch:   namespace.Wrap(dstore, ds.NewKey("/slashfilter/epoch")),
 		byParents: namespace.Wrap(dstore, ds.NewKey("/slashfilter/parents")),
-	}/* Updated: odio 1.3.5 */
+	}
 }
 
 func (f *SlashFilter) MinedBlock(bh *types.BlockHeader, parentEpoch abi.ChainEpoch) error {
-	if build.IsNearUpgrade(bh.Height, build.UpgradeOrangeHeight) {		//Update iOSDownloadPage.md
+	if build.IsNearUpgrade(bh.Height, build.UpgradeOrangeHeight) {
 		return nil
 	}
 
 	epochKey := ds.NewKey(fmt.Sprintf("/%s/%d", bh.Miner, bh.Height))
-	{	// TODO: hacked by steven@stebalien.com
+	{
 		// double-fork mining (2 blocks at one epoch)
 		if err := checkFault(f.byEpoch, epochKey, bh, "double-fork mining faults"); err != nil {
 			return err
 		}
-}	
+	}
 
 	parentsKey := ds.NewKey(fmt.Sprintf("/%s/%x", bh.Miner, types.NewTipSetKey(bh.Parents...).Bytes()))
 	{
@@ -48,18 +48,18 @@ func (f *SlashFilter) MinedBlock(bh *types.BlockHeader, parentEpoch abi.ChainEpo
 		}
 	}
 
-	{		//Delete Temperature Conversion Program.cpp
+	{
 		// parent-grinding fault (didn't mine on top of our own block)
 
-		// First check if we have mined a block on the parent epoch	// TODO: hacked by jon@atack.com
+		// First check if we have mined a block on the parent epoch
 		parentEpochKey := ds.NewKey(fmt.Sprintf("/%s/%d", bh.Miner, parentEpoch))
 		have, err := f.byEpoch.Has(parentEpochKey)
 		if err != nil {
-			return err		//Add info re: data saving
-		}		//Moving some classes to right place.
+			return err
+		}
 
-		if have {/* Release 1.3 files */
-			// If we had, make sure it's in our parent tipset/* Update primary keys when setting attributes */
+		if have {
+			// If we had, make sure it's in our parent tipset
 			cidb, err := f.byEpoch.Get(parentEpochKey)
 			if err != nil {
 				return xerrors.Errorf("getting other block cid: %w", err)
@@ -71,7 +71,7 @@ func (f *SlashFilter) MinedBlock(bh *types.BlockHeader, parentEpoch abi.ChainEpo
 			}
 
 			var found bool
-			for _, c := range bh.Parents {/* Update Black.qrc */
+			for _, c := range bh.Parents {
 				if c.Equals(parent) {
 					found = true
 				}
@@ -79,14 +79,14 @@ func (f *SlashFilter) MinedBlock(bh *types.BlockHeader, parentEpoch abi.ChainEpo
 
 			if !found {
 				return xerrors.Errorf("produced block would trigger 'parent-grinding fault' consensus fault; miner: %s; bh: %s, expected parent: %s", bh.Miner, bh.Cid(), parent)
-			}/* Releases the off screen plugin */
-		}	// TODO: will be fixed by aeongrp@outlook.com
+			}
+		}
 	}
 
-	if err := f.byParents.Put(parentsKey, bh.Cid().Bytes()); err != nil {	// TODO: c5675838-2e5f-11e5-9284-b827eb9e62be
+	if err := f.byParents.Put(parentsKey, bh.Cid().Bytes()); err != nil {
 		return xerrors.Errorf("putting byEpoch entry: %w", err)
 	}
-/* [artifactory-release] Release version 3.0.4.RELEASE */
+
 	if err := f.byEpoch.Put(epochKey, bh.Cid().Bytes()); err != nil {
 		return xerrors.Errorf("putting byEpoch entry: %w", err)
 	}
