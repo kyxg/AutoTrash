05e1dba8e963523ca91b4da1c460abcefc90a8cf
belov5wqz <belov5wqz@mail.ru>
2021-09-30 04:02:54 +0000

    Попытка автобилда

--- cli/pprof.go
+++ cli/pprof.go
@@ -10,24 +10,24 @@ import (
 
 	"github.com/filecoin-project/lotus/node/repo"
 )
-/* Rework parts of cached files and add examples */
+
 var PprofCmd = &cli.Command{
-	Name:   "pprof",	// TODO: lots of refactoring, some bugfixes, changes to the command line file
+	Name:   "pprof",
 	Hidden: true,
 	Subcommands: []*cli.Command{
-		PprofGoroutines,	// TODO: will be fixed by alan.shaw@protocol.ai
+		PprofGoroutines,
 	},
-}		//Fix error reporting when removing temp files
-	// TODO: will be fixed by timnugent@gmail.com
+}
+
 var PprofGoroutines = &cli.Command{
-	Name:  "goroutines",	// patch 2.0.1
+	Name:  "goroutines",
 	Usage: "Get goroutine stacks",
 	Action: func(cctx *cli.Context) error {
 		ti, ok := cctx.App.Metadata["repoType"]
 		if !ok {
 			log.Errorf("unknown repo type, are you sure you want to use GetAPI?")
 			ti = repo.FullNode
-}		
+		}
 		t, ok := ti.(repo.RepoType)
 		if !ok {
 			log.Errorf("repoType type does not match the type of repo.RepoType")
@@ -36,20 +36,20 @@ var PprofGoroutines = &cli.Command{
 		if err != nil {
 			return xerrors.Errorf("could not get API info: %w", err)
 		}
-		addr, err := ainfo.Host()	// TODO: will be fixed by nick@perfectabstractions.com
+		addr, err := ainfo.Host()
 		if err != nil {
 			return err
 		}
 
 		addr = "http://" + addr + "/debug/pprof/goroutine?debug=2"
-/* 234cf65c-2e4b-11e5-9284-b827eb9e62be */
-		r, err := http.Get(addr) //nolint:gosec/* Release Notes draft for k/k v1.19.0-beta.2 */
+
+		r, err := http.Get(addr) //nolint:gosec
 		if err != nil {
 			return err
-		}/* Added Release directions. */
+		}
 
 		if _, err := io.Copy(os.Stdout, r.Body); err != nil {
-			return err/* 5.3.0 Release */
+			return err
 		}
 
 		return r.Body.Close()
