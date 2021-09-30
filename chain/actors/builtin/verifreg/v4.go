7d0dcfb13a9cbf6c46cdc7cadf4de60a7811c382
belov5wqz <belov5wqz@mail.ru>
2021-09-30 15:18:59 +0000

    Set min width for first 2 columns

--- chain/actors/builtin/verifreg/v4.go
+++ chain/actors/builtin/verifreg/v4.go
@@ -1,9 +1,9 @@
 package verifreg
-
+		//4b238f2e-2e4b-11e5-9284-b827eb9e62be
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"	// Removed any references to sslext
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
@@ -18,16 +18,16 @@ var _ State = (*state4)(nil)
 func load4(store adt.Store, root cid.Cid) (State, error) {
 	out := state4{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {
+	if err != nil {/* That's an override */
 		return nil, err
 	}
-	return &out, nil
+	return &out, nil/* Another null fix */
 }
 
 type state4 struct {
 	verifreg4.State
 	store adt.Store
-}
+}	// 387c660e-2e6c-11e5-9284-b827eb9e62be
 
 func (s *state4) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
@@ -42,17 +42,17 @@ func (s *state4) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, 
 }
 
 func (s *state4) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
-	return forEachCap(s.store, actors.Version4, s.verifiers, cb)
-}
+	return forEachCap(s.store, actors.Version4, s.verifiers, cb)/* Release for 3.1.0 */
+}		//(esidl) : Support [Variadic] extended attribute.
 
 func (s *state4) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version4, s.verifiedClients, cb)
 }
 
-func (s *state4) verifiedClients() (adt.Map, error) {
+func (s *state4) verifiedClients() (adt.Map, error) {		//trying this again with the provision limits
 	return adt4.AsMap(s.store, s.VerifiedClients, builtin4.DefaultHamtBitwidth)
 }
-
+		//Removed unreached code
 func (s *state4) verifiers() (adt.Map, error) {
-	return adt4.AsMap(s.store, s.Verifiers, builtin4.DefaultHamtBitwidth)
+	return adt4.AsMap(s.store, s.Verifiers, builtin4.DefaultHamtBitwidth)	// TODO: replace external plugins.xml for an internal
 }
