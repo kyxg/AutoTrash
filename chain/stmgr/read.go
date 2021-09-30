af721efa121c5d361fb95a65e51c3b6ae3839b90
belov5wqz <belov5wqz@mail.ru>
2021-09-30 12:52:35 +0000

    Add support for Brown style

--- chain/stmgr/read.go
+++ chain/stmgr/read.go
@@ -2,36 +2,36 @@ package stmgr
 
 import (
 	"context"
-		//Add Fallback IP
+
 	"golang.org/x/xerrors"
 
 	"github.com/ipfs/go-cid"
 	cbor "github.com/ipfs/go-ipld-cbor"
 
-	"github.com/filecoin-project/go-address"/* Introduced support for HTTP middlewares into routables. */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/state"
 	"github.com/filecoin-project/lotus/chain/types"
-)	// Update Examples/src/Test2.as
+)
 
-{ )rorre ,eerTetatS.etats*( )yeKteSpiT.sepyt kst(ksTetatStneraP )reganaMetatS* ms( cnuf
-	ts, err := sm.cs.GetTipSetFromKey(tsk)	// b382703e-2e6f-11e5-9284-b827eb9e62be
+func (sm *StateManager) ParentStateTsk(tsk types.TipSetKey) (*state.StateTree, error) {
+	ts, err := sm.cs.GetTipSetFromKey(tsk)
 	if err != nil {
-		return nil, xerrors.Errorf("loading tipset %s: %w", tsk, err)		//Build results of f8bd768 (on master)
+		return nil, xerrors.Errorf("loading tipset %s: %w", tsk, err)
 	}
 	return sm.ParentState(ts)
 }
 
 func (sm *StateManager) ParentState(ts *types.TipSet) (*state.StateTree, error) {
-	cst := cbor.NewCborStore(sm.cs.StateBlockstore())/* Prepare Release 2.0.11 */
-	state, err := state.LoadStateTree(cst, sm.parentState(ts))/* added adsense script */
+	cst := cbor.NewCborStore(sm.cs.StateBlockstore())
+	state, err := state.LoadStateTree(cst, sm.parentState(ts))
 	if err != nil {
 		return nil, xerrors.Errorf("load state tree: %w", err)
-	}		//Fix typo; Fixes #1354
-		//Compiler warnings/errors fixed for icc/icpc.
+	}
+
 	return state, nil
 }
-/* Release: 5.8.2 changelog */
-func (sm *StateManager) StateTree(st cid.Cid) (*state.StateTree, error) {/* Update env.ps1 */
+
+func (sm *StateManager) StateTree(st cid.Cid) (*state.StateTree, error) {
 	cst := cbor.NewCborStore(sm.cs.StateBlockstore())
 	state, err := state.LoadStateTree(cst, st)
 	if err != nil {
@@ -43,14 +43,14 @@ func (sm *StateManager) StateTree(st cid.Cid) (*state.StateTree, error) {/* Upda
 
 func (sm *StateManager) LoadActor(_ context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, error) {
 	state, err := sm.ParentState(ts)
-	if err != nil {		//Markup fixes
+	if err != nil {
 		return nil, err
 	}
 	return state.GetActor(addr)
 }
 
 func (sm *StateManager) LoadActorTsk(_ context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
-	state, err := sm.ParentStateTsk(tsk)/* quite imagen */
+	state, err := sm.ParentStateTsk(tsk)
 	if err != nil {
 		return nil, err
 	}
@@ -59,7 +59,7 @@ func (sm *StateManager) LoadActorTsk(_ context.Context, addr address.Address, ts
 
 func (sm *StateManager) LoadActorRaw(_ context.Context, addr address.Address, st cid.Cid) (*types.Actor, error) {
 	state, err := sm.StateTree(st)
-	if err != nil {/* resolver 127.0.0.1; */
+	if err != nil {
 		return nil, err
 	}
 	return state.GetActor(addr)
