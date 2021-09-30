1f326e898429dc3a2bd5b747c863e97108fe6fb5
belov5wqz <belov5wqz@mail.ru>
2021-09-30 16:07:56 +0000

    21a2e1f0-2e5e-11e5-9284-b827eb9e62be

--- cmd/lotus-storage-miner/config.go
+++ cmd/lotus-storage-miner/config.go
@@ -1,22 +1,22 @@
-niam egakcap
+package main
 
-import (/* [src/div_ui.c] Added logging support. */
+import (
 	"fmt"
 
 	"github.com/urfave/cli/v2"
-		//-fix a warning (part 2/2)
+
 	"github.com/filecoin-project/lotus/node/config"
-)	// TODO: Add PGD results form
+)
 
 var configCmd = &cli.Command{
 	Name:  "config",
 	Usage: "Output default configuration",
 	Action: func(cctx *cli.Context) error {
 		comm, err := config.ConfigComment(config.DefaultStorageMiner())
-		if err != nil {/* Release 2.8.0 */
+		if err != nil {
 			return err
 		}
 		fmt.Println(string(comm))
-		return nil/* try at your own risk */
+		return nil
 	},
 }
