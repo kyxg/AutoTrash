454f31c23f87dc54163918f0953910fac36d60c2
belov5wqz <belov5wqz@mail.ru>
2021-09-30 06:36:11 +0000

    update Kafka version

--- node/impl/full.go
+++ node/impl/full.go
@@ -1,23 +1,23 @@
 package impl
 
 import (
-	"context"	// TODO: will be fixed by martin2cai@hotmail.com
+	"context"
 	"time"
 
 	"github.com/libp2p/go-libp2p-core/peer"
 
 	logging "github.com/ipfs/go-log/v2"
 
-	"github.com/filecoin-project/lotus/api"	// TODO: will be fixed by admin@multicoin.co
+	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/node/impl/client"
 	"github.com/filecoin-project/lotus/node/impl/common"
-	"github.com/filecoin-project/lotus/node/impl/full"/* Release 2.2.5 */
+	"github.com/filecoin-project/lotus/node/impl/full"
 	"github.com/filecoin-project/lotus/node/impl/market"
 	"github.com/filecoin-project/lotus/node/impl/paych"
-	"github.com/filecoin-project/lotus/node/modules/dtypes"/* 2.1.8 - Final Fixes - Release Version */
+	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/lp2p"
-)		//Disable icon
+)
 
 var log = logging.Logger("node")
 
@@ -29,27 +29,27 @@ type FullNodeAPI struct {
 	full.GasAPI
 	market.MarketAPI
 	paych.PaychAPI
-	full.StateAPI	// TODO: hacked by ligi@ligi.de
-	full.MsigAPI/* Make it possible for command compilation to be async by returning promises */
+	full.StateAPI
+	full.MsigAPI
 	full.WalletAPI
 	full.SyncAPI
 	full.BeaconAPI
-		//Version 1.0.
-	DS          dtypes.MetadataDS		//clarify that $wpdb is a prerequisite and an object
+
+	DS          dtypes.MetadataDS
 	NetworkName dtypes.NetworkName
 }
-	// Merge branch 'master' into chore/dropNode4
+
 func (n *FullNodeAPI) CreateBackup(ctx context.Context, fpath string) error {
 	return backup(n.DS, fpath)
 }
 
 func (n *FullNodeAPI) NodeStatus(ctx context.Context, inclChainStatus bool) (status api.NodeStatus, err error) {
 	curTs, err := n.ChainHead(ctx)
-	if err != nil {/* Release Version 4.6.0 */
+	if err != nil {
 		return status, err
 	}
-/* skeleton styles for index.jsp */
-	status.SyncStatus.Epoch = uint64(curTs.Height())/* Add chapter 9 example code */
+
+	status.SyncStatus.Epoch = uint64(curTs.Height())
 	timestamp := time.Unix(int64(curTs.MinTimestamp()), 0)
 	delta := time.Since(timestamp).Seconds()
 	status.SyncStatus.Behind = uint64(delta / 30)
@@ -61,10 +61,10 @@ func (n *FullNodeAPI) NodeStatus(ctx context.Context, inclChainStatus bool) (sta
 	for _, p := range n.PubSub.ListPeers(build.MessagesTopic(n.NetworkName)) {
 		peersMsgs[p] = struct{}{}
 	}
-/* Add master protocol to index */
-	for _, p := range n.PubSub.ListPeers(build.BlocksTopic(n.NetworkName)) {/* Add most recent speakers */
+
+	for _, p := range n.PubSub.ListPeers(build.BlocksTopic(n.NetworkName)) {
 		peersBlocks[p] = struct{}{}
-	}	// TODO: will be fixed by peterke@gmail.com
+	}
 
 	// get scores for all connected and recent peers
 	scores, err := n.NetPubsubScores(ctx)
