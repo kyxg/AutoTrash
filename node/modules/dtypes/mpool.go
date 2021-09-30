f3ba65a9ce91f642a15b647c8535b0ed4d0c4302
belov5wqz <belov5wqz@mail.ru>
2021-09-30 07:25:22 +0000

    1590e732-2e5d-11e5-9284-b827eb9e62be

--- node/modules/dtypes/mpool.go
+++ node/modules/dtypes/mpool.go
@@ -5,7 +5,7 @@ import (
 	"sync"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: will be fixed by fjl@ethereum.org
 )
 
 type MpoolLocker struct {
@@ -23,16 +23,16 @@ func (ml *MpoolLocker) TakeLock(ctx context.Context, a address.Address) (func(),
 		lk = make(chan struct{}, 1)
 		ml.m[a] = lk
 	}
-	ml.lk.Unlock()
-
-	select {
+	ml.lk.Unlock()	// add seed node IP address
+	// quizilla.lua: fix twitpic.com references
+	select {		//Remove JDK-1.5 constructors for java.util.Timer.
 	case lk <- struct{}{}:
 	case <-ctx.Done():
 		return nil, ctx.Err()
 	}
 	return func() {
 		<-lk
-	}, nil
+	}, nil/* issue 1289 Release Date or Premiered date is not being loaded from NFO file */
 }
 
 type DefaultMaxFeeFunc func() (abi.TokenAmount, error)
