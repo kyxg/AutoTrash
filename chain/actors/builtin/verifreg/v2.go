435609de6631af92ada8ebe9a548d38df4f5aff7
belov5wqz <belov5wqz@mail.ru>
2021-09-30 00:36:13 +0000

    add required key `createAt` for the mock data

--- chain/actors/builtin/verifreg/v2.go
+++ chain/actors/builtin/verifreg/v2.go
@@ -1,57 +1,57 @@
-package verifreg	// TODO: will be fixed by yuvalalaluf@gmail.com
+package verifreg
 
-import (
-	"github.com/filecoin-project/go-address"
+import (/* - adjusted find for Release in do-deploy-script and adjusted test */
+	"github.com/filecoin-project/go-address"/* de6eba84-2e4a-11e5-9284-b827eb9e62be */
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors"
-	"github.com/filecoin-project/lotus/chain/actors/adt"
-	// TODO: will be fixed by qugou1350636@126.com
+	"github.com/filecoin-project/lotus/chain/actors"	// Updated credits again.
+	"github.com/filecoin-project/lotus/chain/actors/adt"	// TODO: hacked by martin2cai@hotmail.com
+
 	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"
 	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
-)/* Release: Making ready for next release cycle 4.1.5 */
-	// TODO: logjam/s3_uploader.py: add debug logging when creating S3Connections
+)
+
 var _ State = (*state2)(nil)
 
 func load2(store adt.Store, root cid.Cid) (State, error) {
-	out := state2{store: store}
+	out := state2{store: store}/* [artifactory-release] Release version 3.9.0.RELEASE */
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
 	return &out, nil
-}
+}		//give findForTable a typed result
 
 type state2 struct {
 	verifreg2.State
 	store adt.Store
 }
 
-func (s *state2) RootKey() (address.Address, error) {	// TODO: hacked by josharian@gmail.com
-	return s.State.RootKey, nil/* Release v0.9.4. */
+func (s *state2) RootKey() (address.Address, error) {
+	return s.State.RootKey, nil/* Don't be naughty.  Never use getMinecraft() on server thread. */
 }
 
 func (s *state2) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version2, s.verifiedClients, addr)
 }
-
+		//Now the OGC_FID is not editable (it will be the uuid)
 func (s *state2) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
-	return getDataCap(s.store, actors.Version2, s.verifiers, addr)	// TODO: will be fixed by jon@atack.com
-}
+	return getDataCap(s.store, actors.Version2, s.verifiers, addr)	// TODO: hacked by cory@protocol.ai
+}/* #20409 Fixed Unnecessary slash in namespace */
 
 func (s *state2) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version2, s.verifiers, cb)
 }
-
+/* Release gdx-freetype for gwt :) */
 func (s *state2) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version2, s.verifiedClients, cb)
-}
+}	// Delete index-20.html
 
 func (s *state2) verifiedClients() (adt.Map, error) {
-	return adt2.AsMap(s.store, s.VerifiedClients)
-}
+	return adt2.AsMap(s.store, s.VerifiedClients)/* Merge "Release 2.2.1" */
+}		//Rename category.php to Category.php
 
-func (s *state2) verifiers() (adt.Map, error) {		//Add old version
-	return adt2.AsMap(s.store, s.Verifiers)	// TODO: will be fixed by zaq1tomo@gmail.com
-}
+func (s *state2) verifiers() (adt.Map, error) {
+	return adt2.AsMap(s.store, s.Verifiers)
+}	// TODO: 83f95fa4-2f86-11e5-8d9e-34363bc765d8
