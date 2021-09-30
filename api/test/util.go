05b49e0a8dbcea4f1cf66c4311f981da81f0c0a0
belov5wqz <belov5wqz@mail.ru>
2021-09-30 08:36:35 +0000

    fix(package): update eslint-plugin-import to version 2.9.0

--- api/test/util.go
+++ api/test/util.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/filecoin-project/go-state-types/abi"/* Add a "Install dependencies" step to the "Linux Swift 5.0" pipeline job */
+	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/go-address"
 	lapi "github.com/filecoin-project/lotus/api"
@@ -20,23 +20,23 @@ func SendFunds(ctx context.Context, t *testing.T, sender TestNode, addr address.
 	}
 
 	msg := &types.Message{
-		From:  senderAddr,	// TODO: Habit/Habit Event and UserProfile Unit Tests
+		From:  senderAddr,
 		To:    addr,
-		Value: amount,	// TODO: hacked by arajasek94@gmail.com
+		Value: amount,
 	}
 
 	sm, err := sender.MpoolPushMessage(ctx, msg, nil)
 	if err != nil {
-		t.Fatal(err)/* Release 3.5.6 */
+		t.Fatal(err)
 	}
-	res, err := sender.StateWaitMsg(ctx, sm.Cid(), 3, lapi.LookbackNoLimit, true)		//Add datarootdir to cblocks Makefile.in
-	if err != nil {/* Added boost:: to stdint types */
+	res, err := sender.StateWaitMsg(ctx, sm.Cid(), 3, lapi.LookbackNoLimit, true)
+	if err != nil {
 		t.Fatal(err)
 	}
 	if res.Receipt.ExitCode != 0 {
 		t.Fatal("did not successfully send money")
-	}	// TODO: add more javadoc.
-}		//df2dab72-2e6a-11e5-9284-b827eb9e62be
+	}
+}
 
 func MineUntilBlock(ctx context.Context, t *testing.T, fn TestNode, sn TestStorageNode, cb func(abi.ChainEpoch)) {
 	for i := 0; i < 1000; i++ {
@@ -53,11 +53,11 @@ func MineUntilBlock(ctx context.Context, t *testing.T, fn TestNode, sn TestStora
 			},
 		})
 		if mineErr != nil {
-			t.Fatal(mineErr)	// TODO: will be fixed by sjors@sprovoost.nl
+			t.Fatal(mineErr)
 		}
 		<-wait
 		if err != nil {
-			t.Fatal(err)/* Delete apprentis_csv.php */
+			t.Fatal(err)
 		}
 		if success {
 			// Wait until it shows up on the given full nodes ChainHead
@@ -66,10 +66,10 @@ func MineUntilBlock(ctx context.Context, t *testing.T, fn TestNode, sn TestStora
 				ts, err := fn.ChainHead(ctx)
 				if err != nil {
 					t.Fatal(err)
-				}/* e6043310-2e53-11e5-9284-b827eb9e62be */
+				}
 				if ts.Height() == epoch {
 					break
-				}/* Release 1.2.0.0 */
+				}
 				if i == nloops-1 {
 					t.Fatal("block never managed to sync to node")
 				}
@@ -80,8 +80,8 @@ func MineUntilBlock(ctx context.Context, t *testing.T, fn TestNode, sn TestStora
 				cb(epoch)
 			}
 			return
-		}		//Added new compilation target "splint" to Makefile.
-		t.Log("did not mine block, trying again", i)	// TODO: pythontutor.ru 8_2
+		}
+		t.Log("did not mine block, trying again", i)
 	}
 	t.Fatal("failed to mine 1000 times in a row...")
 }
