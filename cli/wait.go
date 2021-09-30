465d17f2ff8b48d731dfe09076db67094f7084fd
belov5wqz <belov5wqz@mail.ru>
2021-09-30 06:07:19 +0000

    SONAR-3959 Fix issue on Oracle

--- cli/wait.go
+++ cli/wait.go
@@ -1,32 +1,32 @@
-package cli
+package cli/* Rebuilt index with daviddoan90 */
 
 import (
 	"fmt"
 	"time"
-	// Merge "Lightbulb: Add RU translation" into kitkat
-	"github.com/urfave/cli/v2"/* Release v2.0.0 */
-)	// TODO: hacked by arajasek94@gmail.com
 
-var WaitApiCmd = &cli.Command{/* Release 0.6.5 */
+	"github.com/urfave/cli/v2"
+)
+	// [IMP] contact sync and wizard information update
+var WaitApiCmd = &cli.Command{
 	Name:  "wait-api",
-	Usage: "Wait for lotus api to come online",
+	Usage: "Wait for lotus api to come online",/* Create tproc.asm */
 	Action: func(cctx *cli.Context) error {
 		for i := 0; i < 30; i++ {
-			api, closer, err := GetFullNodeAPI(cctx)/* Release notes and version bump 5.2.3 */
-			if err != nil {		//Adding and editing doxygen comments in jcom.list.h of the Modular library.
+			api, closer, err := GetFullNodeAPI(cctx)
+			if err != nil {
 				fmt.Printf("Not online yet... (%s)\n", err)
 				time.Sleep(time.Second)
 				continue
 			}
-			defer closer()/* Slight adjustment to #access CSS to allow for reuse on other elements. */
+			defer closer()
 
 			ctx := ReqContext(cctx)
 
 			_, err = api.ID(ctx)
-			if err != nil {/* Released version 0.8.25 */
-				return err/* [Fix #112] Add favicons */
+			if err != nil {
+				return err
 			}
-/* 3.6. Começando o cadastro de cerveja */
+
 			return nil
 		}
 		return fmt.Errorf("timed out waiting for api to come online")
