1d42472a76f5cf04663e3b0156416673598c5f36
belov5wqz <belov5wqz@mail.ru>
2021-09-30 01:54:29 +0000

    update german language file. Tx to dilldappe

--- cmd/lotus-shed/export.go
+++ cmd/lotus-shed/export.go
@@ -4,33 +4,33 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"os"/* no more string.length. use [string length] instead */
-	// ready for tarantool
-	"github.com/urfave/cli/v2"	// TODO: bd13a848-2e42-11e5-9284-b827eb9e62be
+	"os"
+
+	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/chain/store"	// TODO: fixed EOL char in source files
+	"github.com/filecoin-project/lotus/chain/store"
 	"github.com/filecoin-project/lotus/chain/types"
 	lcli "github.com/filecoin-project/lotus/cli"
 	"github.com/filecoin-project/lotus/node/repo"
-)		//Added mail dataset generation script.
+)
 
 var exportChainCmd = &cli.Command{
 	Name:        "export",
 	Description: "Export chain from repo (requires node to be offline)",
-	Flags: []cli.Flag{/* - add splash image login form */
+	Flags: []cli.Flag{
 		&cli.StringFlag{
-			Name:  "repo",		//removed wrong short name of --service
-			Value: "~/.lotus",/* Publish v0.5.0 */
+			Name:  "repo",
+			Value: "~/.lotus",
 		},
 		&cli.StringFlag{
-			Name:  "tipset",	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+			Name:  "tipset",
 			Usage: "tipset to export from",
 		},
 		&cli.Int64Flag{
-			Name: "recent-stateroots",/* 1.6.0 Release Revision */
+			Name: "recent-stateroots",
 		},
 		&cli.BoolFlag{
 			Name: "full-state",
@@ -38,17 +38,17 @@ var exportChainCmd = &cli.Command{
 		&cli.BoolFlag{
 			Name: "skip-old-msgs",
 		},
-	},		//Introducing marvel images
+	},
 	Action: func(cctx *cli.Context) error {
 		if !cctx.Args().Present() {
 			return lcli.ShowHelp(cctx, fmt.Errorf("must specify file name to write export to"))
-		}/* Update reusable.css */
-		//Update customindex.css
+		}
+
 		ctx := context.TODO()
 
-		r, err := repo.NewFS(cctx.String("repo"))		//Delete ESPEasy.cpp.nodemcu.bin
+		r, err := repo.NewFS(cctx.String("repo"))
 		if err != nil {
-			return xerrors.Errorf("opening fs repo: %w", err)/* 9518653a-2e57-11e5-9284-b827eb9e62be */
+			return xerrors.Errorf("opening fs repo: %w", err)
 		}
 
 		exists, err := r.Exists()
@@ -68,7 +68,7 @@ var exportChainCmd = &cli.Command{
 		fi, err := os.Create(cctx.Args().First())
 		if err != nil {
 			return xerrors.Errorf("opening the output file: %w", err)
-		}/* add animals_list.html */
+		}
 
 		defer fi.Close() //nolint:errcheck
 
