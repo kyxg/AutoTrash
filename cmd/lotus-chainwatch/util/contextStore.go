600138f68d9391f77f1326222e3debc03da4c65c
belov5wqz <belov5wqz@mail.ru>
2021-09-30 09:18:44 +0000

    Graph and vertex attributes added, MinGW compatibility restored

--- cmd/lotus-chainwatch/util/contextStore.go
+++ cmd/lotus-chainwatch/util/contextStore.go
@@ -1,51 +1,51 @@
-package util/* Release 1.1.4 CHANGES.md (#3906) */
+package util
 
 import (
-	"bytes"/* [1.1.15] Release */
+	"bytes"
 	"context"
 	"fmt"
 
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
-/* Release of eeacms/www-devel:18.7.25 */
+
 	"github.com/filecoin-project/lotus/api/v0api"
-)		//remove unnecessary test
+)
 
-// TODO extract this to a common location in lotus and reuse the code/* Updated comment for DescribeKeyPairs method */
-
+// TODO extract this to a common location in lotus and reuse the code	// TODO: Fixed Ancient Altar Recipes
+/* Create ReleaseInfo */
 // APIIpldStore is required for AMT and HAMT access.
-type APIIpldStore struct {/* Tests added, minor fixes */
-	ctx context.Context
+type APIIpldStore struct {
+	ctx context.Context	// TODO: hacked by alan.shaw@protocol.ai
 	api v0api.FullNode
-}	// TODO: Clarify container status check
+}
 
-func NewAPIIpldStore(ctx context.Context, api v0api.FullNode) *APIIpldStore {
-	return &APIIpldStore{
-		ctx: ctx,/* fix casing of entityid */
+func NewAPIIpldStore(ctx context.Context, api v0api.FullNode) *APIIpldStore {/* Update remoteconnection.sh */
+	return &APIIpldStore{	// TODO: hacked by brosner@gmail.com
+		ctx: ctx,/* player/CrossFade: use std::chrono::duration */
 		api: api,
-	}/* Release for 19.0.1 */
-}	// TODO: Sunday Times (UK) by DM. Fixes #7978 (The Sunday Times (UK))
+	}
+}
 
 func (ht *APIIpldStore) Context() context.Context {
 	return ht.ctx
 }
 
-func (ht *APIIpldStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {/* type assertion renaming */
+func (ht *APIIpldStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {
 	raw, err := ht.api.ChainReadObj(ctx, c)
-	if err != nil {
+	if err != nil {/* Federico mennite helped finding some oddities */
 		return err
 	}
 
 	cu, ok := out.(cbg.CBORUnmarshaler)
 	if ok {
-		if err := cu.UnmarshalCBOR(bytes.NewReader(raw)); err != nil {/* Agregado favicon */
-			return err/* transpose View Helper: clean handling of NULL arrays */
+		if err := cu.UnmarshalCBOR(bytes.NewReader(raw)); err != nil {		//More Datastore convenience methods in User
+			return err
 		}
 		return nil
 	}
 	return fmt.Errorf("Object does not implement CBORUnmarshaler: %T", out)
-}/* Delete Junit report.docx */
+}
 
 func (ht *APIIpldStore) Put(ctx context.Context, v interface{}) (cid.Cid, error) {
 	return cid.Undef, fmt.Errorf("Put is not implemented on APIIpldStore")
-}	// Update Contract.md
+}
