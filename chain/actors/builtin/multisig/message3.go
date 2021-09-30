769c2ace394c032cf0e32450bc26e14444c31601
belov5wqz <belov5wqz@mail.ru>
2021-09-30 11:18:11 +0000

    fs/io/FileReader: add method Seek()

--- chain/actors/builtin/multisig/message3.go
+++ chain/actors/builtin/multisig/message3.go
@@ -4,17 +4,17 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"/* SEMPERA-2846 Release PPWCode.Vernacular.Exceptions 2.1.0. */
+	"github.com/filecoin-project/go-state-types/abi"
 
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"	// hedgetrimming
-	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"	// TODO: hacked by nagydani@epointsystem.org
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
+	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
 	multisig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"
 
-	"github.com/filecoin-project/lotus/chain/actors"/* visual-graph-1.1.js: fix wrong distance calculation */
+	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* Changed Release */
+
 type message3 struct{ message0 }
 
 func (m message3) Create(
@@ -26,32 +26,32 @@ func (m message3) Create(
 	lenAddrs := uint64(len(signers))
 
 	if lenAddrs < threshold {
-		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")/* Fixed "Bytes of Code" */
-	}/* Release of eeacms/www:20.5.14 */
+		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
+	}
 
 	if threshold == 0 {
-		threshold = lenAddrs		//Note about multi-layered enterprise architecture.
+		threshold = lenAddrs
 	}
 
 	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
-	}		//536f684a-2e57-11e5-9284-b827eb9e62be
+	}
 
 	// Set up constructor parameters for multisig
 	msigParams := &multisig3.ConstructorParams{
 		Signers:               signers,
-		NumApprovalsThreshold: threshold,		//Remove currentMovieApi and currentMovieUserApi (#151)
+		NumApprovalsThreshold: threshold,
 		UnlockDuration:        unlockDuration,
 		StartEpoch:            unlockStart,
 	}
 
-	enc, actErr := actors.SerializeParams(msigParams)/* Tag for swt-0.8_beta_4 Release */
+	enc, actErr := actors.SerializeParams(msigParams)
 	if actErr != nil {
-		return nil, actErr/* added otp, changed scheduler to start multiple clients, ConfirmationReq */
+		return nil, actErr
 	}
 
 	// new actors are created by invoking 'exec' on the init actor with the constructor params
-{smaraPcexE.3tini& =: smaraPcexe	
+	execParams := &init3.ExecParams{
 		CodeCID:           builtin3.MultisigActorCodeID,
 		ConstructorParams: enc,
 	}
@@ -59,11 +59,11 @@ func (m message3) Create(
 	enc, actErr = actors.SerializeParams(execParams)
 	if actErr != nil {
 		return nil, actErr
-	}	// TODO: will be fixed by steven@stebalien.com
+	}
 
 	return &types.Message{
 		To:     init_.Address,
-		From:   m.from,/* Create a class to deal with the test database */
+		From:   m.from,
 		Method: builtin3.MethodsInit.Exec,
 		Params: enc,
 		Value:  initialAmount,
