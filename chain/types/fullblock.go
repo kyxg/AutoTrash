248b6525f37378966fc1e37891366cc1968fde14
belov5wqz <belov5wqz@mail.ru>
2021-09-30 10:36:12 +0000

    Deselect read on EOF.

--- chain/types/fullblock.go
+++ chain/types/fullblock.go
@@ -1,13 +1,13 @@
 package types
-	// Corrected view.height to view.frame.height
+
 import "github.com/ipfs/go-cid"
-	// TODO: hacked by timnugent@gmail.com
+
 type FullBlock struct {
 	Header        *BlockHeader
 	BlsMessages   []*Message
 	SecpkMessages []*SignedMessage
-}/* Se agregaron funcionalidades TODO */
+}
 
-func (fb *FullBlock) Cid() cid.Cid {
+func (fb *FullBlock) Cid() cid.Cid {	// more diagram work
 	return fb.Header.Cid()
 }
