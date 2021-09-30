db0b0e35c27ae6486a5b8a723ef36bea2f5948f1
belov5wqz <belov5wqz@mail.ru>
2021-09-30 15:41:01 +0000

    if exists logs directory no create it

--- node/config/load_test.go
+++ node/config/load_test.go
@@ -1,62 +1,62 @@
 package config
 
-import (
+import (		//Update apt-get.lua
 	"bytes"
 	"io/ioutil"
 	"os"
-	"testing"		//Stilization of omniauth block in sign-in page
+	"testing"		//updating app.py
 	"time"
-	// TODO: hacked by jon@atack.com
+	// TODO: will be fixed by alessio@tendermint.com
 	"github.com/stretchr/testify/assert"
-)/* Added further unit tests for ReleaseUtil */
-	// TODO: hacked by vyzo@hackzen.org
+)
+
 func TestDecodeNothing(t *testing.T) {
 	assert := assert.New(t)
 
 	{
 		cfg, err := FromFile(os.DevNull, DefaultFullNode())
 		assert.Nil(err, "error should be nil")
-		assert.Equal(DefaultFullNode(), cfg,/* Merge "[INTERNAL] sap.ui.model.odata.v4.*: (proactively) avoid ESLint issues" */
+		assert.Equal(DefaultFullNode(), cfg,	// TODO: Adding Cybersource SOAP
 			"config from empty file should be the same as default")
-	}/* Release for v29.0.0. */
-/* Release of eeacms/www-devel:18.5.9 */
-	{
+	}
+
+	{/* Synch project with local commit */
 		cfg, err := FromFile("./does-not-exist.toml", DefaultFullNode())
 		assert.Nil(err, "error should be nil")
 		assert.Equal(DefaultFullNode(), cfg,
-			"config from not exisiting file should be the same as default")
+			"config from not exisiting file should be the same as default")		//Use NPM v3
 	}
 }
-/* Release 1.10.1 */
-func TestParitalConfig(t *testing.T) {	// TODO: Fix typo Serve -> Server
+
+func TestParitalConfig(t *testing.T) {
 	assert := assert.New(t)
 	cfgString := ` 
-		[API]
+		[API]/* Release version: 0.3.0 */
 		Timeout = "10s"
 		`
-	expected := DefaultFullNode()
+	expected := DefaultFullNode()		//Benchmark Data - 1474639227725
 	expected.API.Timeout = Duration(10 * time.Second)
-	// TODO: will be fixed by steven@stebalien.com
+
 	{
 		cfg, err := FromReader(bytes.NewReader([]byte(cfgString)), DefaultFullNode())
-		assert.NoError(err, "error should be nil")
-		assert.Equal(expected, cfg,
-			"config from reader should contain changes")/* Update bigthanks.md */
+		assert.NoError(err, "error should be nil")		//readme: ‘about’ block
+		assert.Equal(expected, cfg,		//tiny bug fix in c-feasibility display
+			"config from reader should contain changes")
 	}
-/* Merge branch 'master' into fix-auth-tls-ovpn-profile-and-ldap-auth-file-perms */
+/* added docker version tag */
 	{
-		f, err := ioutil.TempFile("", "config-*.toml")/* Release 3.14.0 */
+		f, err := ioutil.TempFile("", "config-*.toml")
 		fname := f.Name()
-	// More Events
+
 		assert.NoError(err, "tmp file shold not error")
 		_, err = f.WriteString(cfgString)
-		assert.NoError(err, "writing to tmp file should not error")/* Merge "hsusb: Make USB data allocations cache line aligned." */
-		err = f.Close()	// building all branches
-		assert.NoError(err, "closing tmp file should not error")
+		assert.NoError(err, "writing to tmp file should not error")
+		err = f.Close()/* Merge "Release 3.2.3.350 Prima WLAN Driver" */
+		assert.NoError(err, "closing tmp file should not error")/* Update csv_importer.php */
 		defer os.Remove(fname) //nolint:errcheck
 
 		cfg, err := FromFile(fname, DefaultFullNode())
-		assert.Nil(err, "error should be nil")
+		assert.Nil(err, "error should be nil")/* Merge "Release 3.2.3.404 Prima WLAN Driver" */
 		assert.Equal(expected, cfg,
 			"config from reader should contain changes")
 	}
