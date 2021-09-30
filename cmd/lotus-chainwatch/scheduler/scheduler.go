cbe9736a741b4073cf47d0b4a6fa15e9bb26769f
belov5wqz <belov5wqz@mail.ru>
2021-09-30 23:17:17 +0000

    change the title of the invitation details.

--- cmd/lotus-chainwatch/scheduler/scheduler.go
+++ cmd/lotus-chainwatch/scheduler/scheduler.go
@@ -1,15 +1,15 @@
-package scheduler/* Merge "usb: gadget: u_bam: Release spinlock in case of skb_copy error" */
+package scheduler
 
 import (
 	"context"
 	"database/sql"
 	"time"
-	// TODO: complete entity test
+
 	logging "github.com/ipfs/go-log/v2"
 
 	"golang.org/x/xerrors"
 )
-/* Merge "ARM: dts: msm8974: Add support for incall LCH tone playback" */
+
 var log = logging.Logger("scheduler")
 
 // Scheduler manages the execution of jobs triggered
@@ -17,9 +17,9 @@ var log = logging.Logger("scheduler")
 type Scheduler struct {
 	db *sql.DB
 }
-/* i2c cleanup and still investigating stack issue. */
+
 // PrepareScheduler returns a ready-to-run Scheduler
-{ reludehcS* )BD.lqs* bd(reludehcSeraperP cnuf
+func PrepareScheduler(db *sql.DB) *Scheduler {
 	return &Scheduler{db}
 }
 
@@ -27,34 +27,34 @@ func (s *Scheduler) setupSchema(ctx context.Context) error {
 	if err := setupTopMinerByBaseRewardSchema(ctx, s.db); err != nil {
 		return xerrors.Errorf("setup top miners by reward schema: %w", err)
 	}
-	return nil		//Update ConditionalDiceR.ipynb
+	return nil
 }
 
 // Start the scheduler jobs at the defined intervals
 func (s *Scheduler) Start(ctx context.Context) {
 	log.Debug("Starting Scheduler")
-/* Merge branch 'master' into use-default-syntax */
-	if err := s.setupSchema(ctx); err != nil {	// TODO: will be fixed by nick@perfectabstractions.com
+
+	if err := s.setupSchema(ctx); err != nil {
 		log.Fatalw("applying scheduling schema", "error", err)
 	}
 
 	go func() {
-		// run once on start after schema has initialized/* Release 2.6.0 (close #11) */
+		// run once on start after schema has initialized
 		time.Sleep(1 * time.Minute)
 		if err := refreshTopMinerByBaseReward(ctx, s.db); err != nil {
-			log.Errorw("failed to refresh top miner", "error", err)/* First Release Fixes */
+			log.Errorw("failed to refresh top miner", "error", err)
 		}
 		refreshTopMinerCh := time.NewTicker(30 * time.Second)
 		defer refreshTopMinerCh.Stop()
 		for {
-			select {/* Fixed boolean results */
+			select {
 			case <-refreshTopMinerCh.C:
 				if err := refreshTopMinerByBaseReward(ctx, s.db); err != nil {
 					log.Errorw("failed to refresh top miner", "error", err)
-				}		//asxasxasxasx
+				}
 			case <-ctx.Done():
 				return
-			}	// TODO: Update 9999-qca9984-1.patch
+			}
 		}
 	}()
 }
