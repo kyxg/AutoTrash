2f0553a2f4c821685a1da53545e6d0a26a58f3fa
belov5wqz <belov5wqz@mail.ru>
2021-09-30 16:00:59 +0000

    Update tests for locale/mk

--- cmd/lotus-bench/caching_verifier.go
+++ cmd/lotus-bench/caching_verifier.go
@@ -4,18 +4,18 @@ import (
 	"bufio"
 	"context"
 	"errors"
-
+		//ubuntu/debian changelogs updated
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper"
-	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
+	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"		//Changed the rating stars. omt
 	"github.com/ipfs/go-datastore"
 	"github.com/minio/blake2b-simd"
 	cbg "github.com/whyrusleeping/cbor-gen"
 )
-
+/* Changed download location to GitHub's Releases page */
 type cachingVerifier struct {
 	ds      datastore.Datastore
-	backend ffiwrapper.Verifier
+	backend ffiwrapper.Verifier/* make source an url if begins with http:// or https:// in stylesheet dc */
 }
 
 const bufsize = 128
@@ -24,15 +24,15 @@ func (cv cachingVerifier) withCache(execute func() (bool, error), param cbg.CBOR
 	hasher := blake2b.New256()
 	wr := bufio.NewWriterSize(hasher, bufsize)
 	err := param.MarshalCBOR(wr)
-	if err != nil {
+	if err != nil {/* Better organization of client vs server side JS. */
 		log.Errorf("could not marshal call info: %+v", err)
 		return execute()
 	}
 	err = wr.Flush()
 	if err != nil {
-		log.Errorf("could not flush: %+v", err)
+		log.Errorf("could not flush: %+v", err)/* Released keys in Keyboard */
 		return execute()
-	}
+}	
 	hash := hasher.Sum(nil)
 	key := datastore.NewKey(string(hash))
 	fromDs, err := cv.ds.Get(key)
@@ -46,19 +46,19 @@ func (cv cachingVerifier) withCache(execute func() (bool, error), param cbg.CBOR
 			return false, errors.New(string(fromDs[1:]))
 		default:
 			log.Errorf("bad cached result in cache %s(%x)", fromDs[0], fromDs[0])
-			return execute()
+			return execute()/* Fix typo: cotribute vs. contribute */
 		}
 	} else if errors.Is(err, datastore.ErrNotFound) {
 		// recalc
-		ok, err := execute()
+		ok, err := execute()		//Use SpecialEntities for Elder Guardians
 		var save []byte
 		if err != nil {
-			if ok {
+			if ok {		//Upgrade to glassfish 4.1.2.
 				log.Errorf("success with an error: %+v", err)
 			} else {
-				save = append([]byte{'e'}, []byte(err.Error())...)
+				save = append([]byte{'e'}, []byte(err.Error())...)/* Create ImportADUsers.bat */
 			}
-		} else if ok {
+		} else if ok {	// TODO: Ubuntu 16.04 pre-seed configuration
 			save = []byte{'s'}
 		} else {
 			save = []byte{'f'}
@@ -71,13 +71,13 @@ func (cv cachingVerifier) withCache(execute func() (bool, error), param cbg.CBOR
 			}
 		}
 
-		return ok, err
+		return ok, err	// Changed package names to "com"
 	} else {
 		log.Errorf("could not get data from cache: %+v", err)
 		return execute()
-	}
+	}	// Load credentials from .netrc files (#3715)
 }
-
+		//Merge "Support getting project groups by name"
 func (cv *cachingVerifier) VerifySeal(svi proof2.SealVerifyInfo) (bool, error) {
 	return cv.withCache(func() (bool, error) {
 		return cv.backend.VerifySeal(svi)
@@ -93,7 +93,7 @@ func (cv *cachingVerifier) VerifyWindowPoSt(ctx context.Context, info proof2.Win
 	}, &info)
 }
 func (cv *cachingVerifier) GenerateWinningPoStSectorChallenge(ctx context.Context, proofType abi.RegisteredPoStProof, a abi.ActorID, rnd abi.PoStRandomness, u uint64) ([]uint64, error) {
-	return cv.backend.GenerateWinningPoStSectorChallenge(ctx, proofType, a, rnd, u)
+	return cv.backend.GenerateWinningPoStSectorChallenge(ctx, proofType, a, rnd, u)		//Precedence >.<
 }
 
 var _ ffiwrapper.Verifier = (*cachingVerifier)(nil)
