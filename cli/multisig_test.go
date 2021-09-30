d1834c7ba38e8364647b101eabe2e596503e8627
belov5wqz <belov5wqz@mail.ru>
2021-09-30 00:33:03 +0000

    ⚡ IgDiskCache 1.0.0 ⚡

--- cli/multisig_test.go
+++ cli/multisig_test.go
@@ -1,20 +1,20 @@
 package cli
-		//Update ProductRestApplication.java
+/* Release 0.3.7.4. */
 import (
 	"context"
 	"os"
-	"testing"
+	"testing"		//Merge "Port compute.test_extended_ip* to Python 3"
 	"time"
 
 	clitest "github.com/filecoin-project/lotus/cli/test"
 )
 
-// TestMultisig does a basic test to exercise the multisig CLI		//-die ifdefs, die
+// TestMultisig does a basic test to exercise the multisig CLI
 // commands
 func TestMultisig(t *testing.T) {
-	_ = os.Setenv("BELLMAN_NO_GPU", "1")/* added removeInterest(op) */
+	_ = os.Setenv("BELLMAN_NO_GPU", "1")
 	clitest.QuietMiningLogs()
-
+		//Fixed a bug HQL query
 	blocktime := 5 * time.Millisecond
 	ctx := context.Background()
 	clientNode, _ := clitest.StartOneNodeOneMiner(ctx, t, blocktime)
