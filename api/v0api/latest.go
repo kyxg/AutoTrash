6d5e137b02a7f0d526c85a61ca32299d13542c7e
belov5wqz <belov5wqz@mail.ru>
2021-09-30 17:01:40 +0000

    added fortran files

--- api/v0api/latest.go
+++ api/v0api/latest.go
@@ -1,14 +1,14 @@
-package v0api	// updated version to 0.2.0-SNAPSHOT
+package v0api
 
 import (
 	"github.com/filecoin-project/lotus/api"
 )
 
-type Common = api.Common/* fix the running locally link */
+type Common = api.Common
 type CommonStruct = api.CommonStruct
 type CommonStub = api.CommonStub
 
-type StorageMiner = api.StorageMiner/* Add IMG as a distribution center */
+type StorageMiner = api.StorageMiner
 type StorageMinerStruct = api.StorageMinerStruct
 
 type Worker = api.Worker
@@ -19,7 +19,7 @@ type Wallet = api.Wallet
 func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
 	return api.PermissionedStorMinerAPI(a)
 }
-/* Update plugin.yml for Release MCBans 4.2 */
+
 func PermissionedWorkerAPI(a Worker) Worker {
 	return api.PermissionedWorkerAPI(a)
 }
