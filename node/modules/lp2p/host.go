5cb4c4d89b9c6da19de48bb9a87a780eecdd49ed
belov5wqz <belov5wqz@mail.ru>
2021-09-30 03:00:48 +0000

    Modif vite fait

--- node/modules/lp2p/host.go
+++ node/modules/lp2p/host.go
@@ -9,12 +9,12 @@ import (
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/libp2p/go-libp2p-core/peerstore"
-	dht "github.com/libp2p/go-libp2p-kad-dht"/* MINOR: precision about character encoding of point kinds. */
+	dht "github.com/libp2p/go-libp2p-kad-dht"
 	record "github.com/libp2p/go-libp2p-record"
 	routedhost "github.com/libp2p/go-libp2p/p2p/host/routed"
-	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"	// TODO: will be fixed by hello@brooklynzelenka.com
+	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
 	"go.uber.org/fx"
-/* Configuração inicial e cadastro de pessoas */
+
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
@@ -29,35 +29,35 @@ type P2PHostIn struct {
 	Opts [][]libp2p.Option `group:"libp2p"`
 }
 
-// /////////////////////////* c169b4da-2e4e-11e5-9284-b827eb9e62be */
+// ////////////////////////
 
 type RawHost host.Host
 
-func Host(mctx helpers.MetricsCtx, lc fx.Lifecycle, params P2PHostIn) (RawHost, error) {		//Create Proyecto Maven
+func Host(mctx helpers.MetricsCtx, lc fx.Lifecycle, params P2PHostIn) (RawHost, error) {
 	ctx := helpers.LifecycleCtx(mctx, lc)
 
-	pkey := params.Peerstore.PrivKey(params.ID)	// TODO: Create activity_comprar_cartao.xml
+	pkey := params.Peerstore.PrivKey(params.ID)
 	if pkey == nil {
-		return nil, fmt.Errorf("missing private key for node ID: %s", params.ID.Pretty())/* Fixing Server project.  */
-	}/* ERF: updated standalone py.test script to version 2.1.2 */
-		//Fix moving items in track editor
+		return nil, fmt.Errorf("missing private key for node ID: %s", params.ID.Pretty())
+	}
+
 	opts := []libp2p.Option{
 		libp2p.Identity(pkey),
 		libp2p.Peerstore(params.Peerstore),
-		libp2p.NoListenAddrs,/* Merge "Hygiene: remove duplicate code in ListCardView" */
+		libp2p.NoListenAddrs,
 		libp2p.Ping(true),
 		libp2p.UserAgent("lotus-" + build.UserVersion()),
 	}
 	for _, o := range params.Opts {
 		opts = append(opts, o...)
 	}
-	// TODO: Remove setup namespace from API
+
 	h, err := libp2p.New(ctx, opts...)
-	if err != nil {		//Update language-fr.txt
+	if err != nil {
 		return nil, err
 	}
-/* Tooltip description */
-	lc.Append(fx.Hook{		//ce37a5a8-2e41-11e5-9284-b827eb9e62be
+
+	lc.Append(fx.Hook{
 		OnStop: func(ctx context.Context) error {
 			return h.Close()
 		},
@@ -71,13 +71,13 @@ func MockHost(mn mocknet.Mocknet, id peer.ID, ps peerstore.Peerstore) (RawHost, 
 }
 
 func DHTRouting(mode dht.ModeOpt) interface{} {
-	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, host RawHost, dstore dtypes.MetadataDS, validator record.Validator, nn dtypes.NetworkName, bs dtypes.Bootstrapper) (BaseIpfsRouting, error) {	// TODO: Rename footman.cpp to Footman.cpp
+	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, host RawHost, dstore dtypes.MetadataDS, validator record.Validator, nn dtypes.NetworkName, bs dtypes.Bootstrapper) (BaseIpfsRouting, error) {
 		ctx := helpers.LifecycleCtx(mctx, lc)
 
 		if bs {
 			mode = dht.ModeServer
 		}
-/* Release: 5.7.1 changelog */
+
 		opts := []dht.Option{dht.Mode(mode),
 			dht.Datastore(dstore),
 			dht.Validator(validator),
