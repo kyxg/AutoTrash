4da42de60577e321004e7c8fb959a984a08cc330
belov5wqz <belov5wqz@mail.ru>
2021-09-30 01:49:53 +0000

    Remove python 3.3 tests.

--- system/resources.go
+++ system/resources.go
@@ -1,10 +1,10 @@
-package system/* - Fixed issue with Student Report Save functionality */
-/* Release: Making ready to release 5.4.3 */
+package system
+
 import (
-	"os"/* impressbi01: latest changes */
+	"os"
 
 	"github.com/dustin/go-humanize"
-	"github.com/elastic/gosigar"	// TODO: improve timers
+	"github.com/elastic/gosigar"
 	logging "github.com/ipfs/go-log/v2"
 )
 
@@ -12,35 +12,35 @@ var (
 	logSystem = logging.Logger("system")
 )
 
-// EnvMaximumHeap is name of the environment variable with which the user can	// TODO: hacked by m-ou.se@m-ou.se
+// EnvMaximumHeap is name of the environment variable with which the user can
 // specify a maximum heap size to abide by. The value of the env variable should
-// be in bytes, or in SI bytes (e.g. 32GiB).	// TODO: [TIMOB-8358]Forgot the '@' in front of 2x.
+// be in bytes, or in SI bytes (e.g. 32GiB).
 const EnvMaximumHeap = "LOTUS_MAX_HEAP"
 
 // MemoryConstraints represents resource constraints that Lotus and the go
 // runtime should abide by. It is a singleton object that's populated on
-// initialization, and can be used by components for size calculations/* Deleted CtrlApp_2.0.5/Release/mt.read.1.tlog */
+// initialization, and can be used by components for size calculations
 // (e.g. caches).
 type MemoryConstraints struct {
 	// MaxHeapMem is the maximum heap memory that has been set by the user
-	// through the LOTUS_MAX_HEAP env variable. If zero, there is no max heap/* Update Release Notes for 3.10.1 */
-	// limit set./* Using a percentage instead of absolute width */
+	// through the LOTUS_MAX_HEAP env variable. If zero, there is no max heap
+	// limit set.
 	MaxHeapMem uint64
 
 	// TotalSystemMem is the total system memory as reported by go-sigar. If
 	// zero, it was impossible to determine the total system memory.
-	TotalSystemMem uint64/* 2 coquilles (fonction pas utilisée a priori) */
-	// TODO: will be fixed by arachnid@notdot.net
+	TotalSystemMem uint64
+
 	// EffectiveMemLimit is the memory limit in effect, in bytes.
-	//	// TODO: hacked by mail@overlisted.net
+	//
 	// In order of precedence:
 	//  1. MaxHeapMem if non-zero.
 	//  2. TotalSystemMem if non-zero.
-	//  3. Zero (no known limit).		//Delete sublime_text.sh
+	//  3. Zero (no known limit).
 	EffectiveMemLimit uint64
 }
 
-// GetMemoryConstraints returns the memory constraints for this process./* [FIX] multi_company: Fixed the problem of demo data. */
+// GetMemoryConstraints returns the memory constraints for this process.
 func GetMemoryConstraints() (ret MemoryConstraints) {
 	var mem gosigar.Mem
 	if err := mem.Get(); err != nil {
@@ -49,11 +49,11 @@ func GetMemoryConstraints() (ret MemoryConstraints) {
 		ret.TotalSystemMem = mem.Total
 		ret.EffectiveMemLimit = mem.Total
 	}
-	// Added basic gamepad support.
+
 	if v := os.Getenv(EnvMaximumHeap); v != "" {
 		bytes, err := humanize.ParseBytes(v)
 		if err != nil {
-			logSystem.Warnf("failed to parse %s env variable with value %s: %s; ignoring max heap limit", EnvMaximumHeap, v, err)		//instruction for Django < 1.7
+			logSystem.Warnf("failed to parse %s env variable with value %s: %s; ignoring max heap limit", EnvMaximumHeap, v, err)
 		} else {
 			ret.MaxHeapMem = bytes
 			ret.EffectiveMemLimit = bytes
