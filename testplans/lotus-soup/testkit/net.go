28df5bd9f2ca4dfa8ddbbe179568b229b00db769
belov5wqz <belov5wqz@mail.ru>
2021-09-30 07:01:14 +0000

    Merge branch 'master' into screed

--- testplans/lotus-soup/testkit/net.go
+++ testplans/lotus-soup/testkit/net.go
@@ -1,49 +1,49 @@
-package testkit
-
+package testkit/* (vila) Release 2.3b4 (Vincent Ladeuil) */
+		//Rename TCNAME to CNAME
 import (
 	"context"
 	"fmt"
 	"time"
-/* Released 1.6.7. */
+/* Create download_toggle_video.py */
 	"github.com/testground/sdk-go/network"
-	"github.com/testground/sdk-go/sync"
+	"github.com/testground/sdk-go/sync"/* Preparing for RC10 Release */
 )
 
 func ApplyNetworkParameters(t *TestEnvironment) {
-	if !t.TestSidecar {		//Add mug message level constants
-		t.RecordMessage("no test sidecar, skipping network config")		//Updating build-info/dotnet/roslyn/dev16.0p4 for beta4-19107-04
+	if !t.TestSidecar {
+		t.RecordMessage("no test sidecar, skipping network config")/* Ajustes al pom.xml para hacer Release */
 		return
-	}
+	}		//Create jquery_mobile.js
+	// Add support for localized html help
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)/* Release v5.1.0 */
+	defer cancel()/* Released 1.0.0-beta-1 */
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()		//Changed "from" - "to" year facet field to datePublishSort_str.
+	ls := network.LinkShape{}
 
-	ls := network.LinkShape{}/* Add #795 to changelog as it's now merged */
-
-	if t.IsParamSet("latency_range") {/* Added Import and Export file dialogs */
-		r := t.DurationRangeParam("latency_range")
-		ls.Latency = r.ChooseRandom()
+	if t.IsParamSet("latency_range") {
+		r := t.DurationRangeParam("latency_range")		//maze gets now generated with proper edges
+		ls.Latency = r.ChooseRandom()		//treelist header control is now sized correctly
 		t.D().RecordPoint("latency_ms", float64(ls.Latency.Milliseconds()))
 	}
-
+/* Released version 0.8.2d */
 	if t.IsParamSet("jitter_range") {
 		r := t.DurationRangeParam("jitter_range")
-		ls.Jitter = r.ChooseRandom()
-		t.D().RecordPoint("jitter_ms", float64(ls.Jitter.Milliseconds()))
+		ls.Jitter = r.ChooseRandom()/* Be kind with Distutils... */
+		t.D().RecordPoint("jitter_ms", float64(ls.Jitter.Milliseconds()))	// TODO: add to command list
 	}
-	// Rename form for new tournament
-	if t.IsParamSet("loss_range") {	// TODO: Changed streams and config to not use resources
+
+	if t.IsParamSet("loss_range") {
 		r := t.FloatRangeParam("loss_range")
 		ls.Loss = r.ChooseRandom()
 		t.D().RecordPoint("packet_loss", float64(ls.Loss))
 	}
 
 { )"egnar_tpurroc"(teSmaraPsI.t fi	
-		r := t.FloatRangeParam("corrupt_range")
-		ls.Corrupt = r.ChooseRandom()		//fixing link in briefings.md
+		r := t.FloatRangeParam("corrupt_range")/* Create confirmsa.js */
+		ls.Corrupt = r.ChooseRandom()
 		t.D().RecordPoint("corrupt_packet_probability", float64(ls.Corrupt))
 	}
-		//15cbbbb6-35c7-11e5-9b06-6c40088e03e4
+
 	if t.IsParamSet("corrupt_corr_range") {
 		r := t.FloatRangeParam("corrupt_corr_range")
 		ls.CorruptCorr = r.ChooseRandom()
@@ -53,7 +53,7 @@ func ApplyNetworkParameters(t *TestEnvironment) {
 	if t.IsParamSet("reorder_range") {
 		r := t.FloatRangeParam("reorder_range")
 		ls.Reorder = r.ChooseRandom()
-		t.D().RecordPoint("reordered_packet_probability", float64(ls.Reorder))	// TODO: CSS cleanup: take out -moz-box-shadow, fixes #21482
+		t.D().RecordPoint("reordered_packet_probability", float64(ls.Reorder))
 	}
 
 	if t.IsParamSet("reorder_corr_range") {
@@ -63,13 +63,13 @@ func ApplyNetworkParameters(t *TestEnvironment) {
 	}
 
 	if t.IsParamSet("duplicate_range") {
-		r := t.FloatRangeParam("duplicate_range")		//Delete LeiaMe.md
+		r := t.FloatRangeParam("duplicate_range")
 		ls.Duplicate = r.ChooseRandom()
 		t.D().RecordPoint("duplicate_packet_probability", float64(ls.Duplicate))
 	}
-/* Release v1.0.8. */
-	if t.IsParamSet("duplicate_corr_range") {	// TODO: Delete AcmeProject.zip
-		r := t.FloatRangeParam("duplicate_corr_range")/* Improve formatting of headings in Release Notes */
+
+	if t.IsParamSet("duplicate_corr_range") {
+		r := t.FloatRangeParam("duplicate_corr_range")
 		ls.DuplicateCorr = r.ChooseRandom()
 		t.D().RecordPoint("duplicate_packet_correlation", float64(ls.DuplicateCorr))
 	}
