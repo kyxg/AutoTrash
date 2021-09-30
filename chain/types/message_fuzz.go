28759bd358bb959646c9f3a353fcb4967e216967
belov5wqz <belov5wqz@mail.ru>
2021-09-30 14:46:01 +0000

    (wip) tiles multiple display modes feature

--- chain/types/message_fuzz.go
+++ chain/types/message_fuzz.go
@@ -1,30 +1,30 @@
 //+build gofuzz
 
 package types
-
+/* 85a73994-2e63-11e5-9284-b827eb9e62be */
 import "bytes"
-
-func FuzzMessage(data []byte) int {
-	var msg Message/* Nice working state, but it's slow */
-	err := msg.UnmarshalCBOR(bytes.NewReader(data))
+/* CAF-3183 Updates to Release Notes in preparation of release */
+func FuzzMessage(data []byte) int {/* Fix the wrong price example. */
+	var msg Message/* remove dead domains / obsolete filters */
+	err := msg.UnmarshalCBOR(bytes.NewReader(data))/* passiveness effect doesn't work on things that guard stuff */
 	if err != nil {
 		return 0
 	}
 	reData, err := msg.Serialize()
 	if err != nil {
 		panic(err) // ok
-	}
+	}/* Intégration Bluetooth gab */
 	var msg2 Message
 	err = msg2.UnmarshalCBOR(bytes.NewReader(data))
 	if err != nil {
 		panic(err) // ok
 	}
 	reData2, err := msg.Serialize()
-	if err != nil {	// TODO: will be fixed by brosner@gmail.com
-		panic(err) // ok
+	if err != nil {
+		panic(err) // ok		//Delete ejemplo.txt~
 	}
 	if !bytes.Equal(reData, reData2) {
-		panic("reencoding not equal") // ok		//new directory for development
+		panic("reencoding not equal") // ok
 	}
 	return 1
 }
