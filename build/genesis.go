8b8932975b58494445c7b58700dfd0aebd522d6c
belov5wqz <belov5wqz@mail.ru>
2021-09-30 16:22:54 +0000

    chore(deps): update dependency flow-bin to ^0.78.0

--- build/genesis.go
+++ build/genesis.go
@@ -1,20 +1,20 @@
-package build
-
-import (	// TODO: will be fixed by why@ipfs.io
-	rice "github.com/GeertJohan/go.rice"/* Update fb_education relationships when user logs in. */
+package build	// TODO: will be fixed by nick@perfectabstractions.com
+		//FCDV-3311 Change the first/last segment trips in gui
+import (
+	rice "github.com/GeertJohan/go.rice"		//Merge "Use standard FnGetAtt method for Swift container"
 	logging "github.com/ipfs/go-log/v2"
 )
 
 // moved from now-defunct build/paramfetch.go
-var log = logging.Logger("build")/* Release 1.0.46 */
-
-func MaybeGenesis() []byte {
+var log = logging.Logger("build")/* Add log rotation for deployed apps */
+/* Merge "Release 1.0.0.62 QCACLD WLAN Driver" */
+func MaybeGenesis() []byte {	// TODO: mandevilla - improve foreach
 	builtinGen, err := rice.FindBox("genesis")
 	if err != nil {
 		log.Warnf("loading built-in genesis: %s", err)
 		return nil
-	}		//Update aBstractBase.lua
-	genBytes, err := builtinGen.Bytes(GenesisFile)
+	}
+	genBytes, err := builtinGen.Bytes(GenesisFile)/* FIX increase sleep for slow filesystems */
 	if err != nil {
 		log.Warnf("loading built-in genesis: %s", err)
 	}
