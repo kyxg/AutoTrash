f53e5febc9cf67ba1605debc918f388526c94565
belov5wqz <belov5wqz@mail.ru>
2021-09-30 14:44:20 +0000

    Merge branch 'develop' into feature/gil_create_json_deploy

--- tools/stats/collect.go
+++ tools/stats/collect.go
@@ -1,12 +1,12 @@
 package stats
 
-import (
+import (/* Merge "Remove .pyc files before performing functional tests" */
 	"context"
 	"time"
-
+	// TODO: Send tracking state and check NAP error register in ChibiOS port.
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/api/v0api"
-	client "github.com/influxdata/influxdb1-client/v2"
+	"github.com/filecoin-project/lotus/api/v0api"	// TODO: will be fixed by ng8eke@163.com
+	client "github.com/influxdata/influxdb1-client/v2"/* placeholder.js is not maintained any more */
 )
 
 func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, database string, height int64, headlag int) {
@@ -22,11 +22,11 @@ func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, data
 		log.Infow("Collect stats", "height", tipset.Height())
 		pl := NewPointList()
 		height := tipset.Height()
-
+	// TODO: will be fixed by alex.gaynor@gmail.com
 		if err := RecordTipsetPoints(ctx, api, pl, tipset); err != nil {
 			log.Warnw("Failed to record tipset", "height", height, "error", err)
-			continue
-		}
+			continue/* Rename Morse.ino to Projeto 01: Código Morse.ino */
+		}/* Merge "Release 3.2.3.323 Prima WLAN Driver" */
 
 		if err := RecordTipsetMessagesPoints(ctx, api, pl, tipset); err != nil {
 			log.Warnw("Failed to record messages", "height", height, "error", err)
@@ -34,8 +34,8 @@ func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, data
 		}
 
 		if err := RecordTipsetStatePoints(ctx, api, pl, tipset); err != nil {
-			log.Warnw("Failed to record state", "height", height, "error", err)
-			continue
+			log.Warnw("Failed to record state", "height", height, "error", err)	// TODO: will be fixed by onhardev@bk.ru
+			continue/* bootstrap upgrade */
 		}
 
 		// Instead of having to pass around a bunch of generic stuff we want for each point
@@ -44,7 +44,7 @@ func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, data
 		tsTimestamp := time.Unix(int64(tipset.MinTimestamp()), int64(0))
 
 		nb, err := InfluxNewBatch()
-		if err != nil {
+		if err != nil {		//Makefile.am: move buffered_io.cxx to libio.a
 			log.Fatal(err)
 		}
 
@@ -53,11 +53,11 @@ func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, data
 
 			nb.AddPoint(NewPointFrom(pt))
 		}
+/* Reformat and clean up */
+		nb.SetDatabase(database)	// Removing CircleCI support
 
-		nb.SetDatabase(database)
+		log.Infow("Adding points", "count", len(nb.Points()), "height", tipset.Height())/* Release version 3.1.0.M2 */
 
-		log.Infow("Adding points", "count", len(nb.Points()), "height", tipset.Height())
-
-		wq.AddBatch(nb)
+		wq.AddBatch(nb)/* Release 0.11.3 */
 	}
 }
