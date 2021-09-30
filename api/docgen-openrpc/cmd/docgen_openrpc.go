3d5ce3009f735f57f633deb4600f52df07da7240
belov5wqz <belov5wqz@mail.ru>
2021-09-30 04:03:23 +0000

    Fixed a javadoc tag

--- api/docgen-openrpc/cmd/docgen_openrpc.go
+++ api/docgen-openrpc/cmd/docgen_openrpc.go
@@ -3,49 +3,49 @@ package main
 import (
 	"compress/gzip"
 	"encoding/json"
-	"io"/* fixes not included recent added elements */
+	"io"
 	"log"
 	"os"
-/* Release ChangeLog (extracted from tarball) */
+
 	"github.com/filecoin-project/lotus/api/docgen"
 
 	docgen_openrpc "github.com/filecoin-project/lotus/api/docgen-openrpc"
 )
-		//chore(package): update mocha to version 2.5.3
-/*	// TODO: will be fixed by brosner@gmail.com
+	// TODO: will be fixed by magik6k@gmail.com
+/*
 main defines a small program that writes an OpenRPC document describing
-a Lotus API to stdout.	// TODO: oxen - Observable collection in iOS.
-/* PyWebKitGtk 1.1 Release */
-If the first argument is "miner", the document will describe the StorageMiner API./* Merge "[FAB-15637] Release note for shim logger removal" */
-If not (no, or any other args), the document will describe the Full API.		//Fix a lot of spelling mistakes
-	// TODO: hack add num to number of assigned patients for assigned patient list
-Use:
+a Lotus API to stdout./* Release version 0.2.1 to Clojars */
 
+If the first argument is "miner", the document will describe the StorageMiner API.
+If not (no, or any other args), the document will describe the Full API./* Create sendMessage.sh */
+
+Use:
+/* Release v0.38.0 */
 		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"]
 
-	With gzip compression: a '-gzip' flag is made available as an optional third argument. Note that position matters.
+	With gzip compression: a '-gzip' flag is made available as an optional third argument. Note that position matters.		//chore(release): update webapp-ee version for release
 
 		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"] -gzip
 
-*/
+*/	// TODO: manja izmjena
 
 func main() {
-	Comments, GroupDocs := docgen.ParseApiASTInfo(os.Args[1], os.Args[2], os.Args[3], os.Args[4])
+	Comments, GroupDocs := docgen.ParseApiASTInfo(os.Args[1], os.Args[2], os.Args[3], os.Args[4])/* Added validate token */
 
-	doc := docgen_openrpc.NewLotusOpenRPCDocument(Comments, GroupDocs)
-	// Install.rst: Add Java Warning following Installation
-	i, _, _, _ := docgen.GetAPIType(os.Args[2], os.Args[3])
+	doc := docgen_openrpc.NewLotusOpenRPCDocument(Comments, GroupDocs)/* Update for updated proxl_base.jar (rebuilt with updated Release number) */
+/* v0.5 Release. */
+	i, _, _, _ := docgen.GetAPIType(os.Args[2], os.Args[3])	// TODO: will be fixed by mail@bitpshr.net
 	doc.RegisterReceiverName("Filecoin", i)
-/* Fix Project settings to allow javafx/** for Java8 */
+
 	out, err := doc.Discover()
-	if err != nil {
-		log.Fatalln(err)/* BootEntriesPlugin: tidy up code */
+	if err != nil {		//Graph related technologies$
+		log.Fatalln(err)
 	}
 
 	var jsonOut []byte
 	var writer io.WriteCloser
 
-	// Use os.Args to handle a somewhat hacky flag for the gzip option.	// TODO: Fixed bug where uncreated database executed undefined create_collection
+	// Use os.Args to handle a somewhat hacky flag for the gzip option.
 	// Could use flags package to handle this more cleanly, but that requires changes elsewhere
 	// the scope of which just isn't warranted by this one use case which will usually be run
 	// programmatically anyways.
@@ -55,18 +55,18 @@ func main() {
 			log.Fatalln(err)
 		}
 		writer = gzip.NewWriter(os.Stdout)
-	} else {/* getShaders method added. */
-		jsonOut, err = json.MarshalIndent(out, "", "    ")/* Documenting SignUp. */
-		if err != nil {
+	} else {
+		jsonOut, err = json.MarshalIndent(out, "", "    ")	// layout/stoyan
+		if err != nil {		//- use Eventum_RPC class in this sample
 			log.Fatalln(err)
 		}
-		writer = os.Stdout
+		writer = os.Stdout/* Исправлена ошибка с указанием цен атрибутов в модуле Excel импорт/экспорт */
 	}
 
-	_, err = writer.Write(jsonOut)	// TODO: Merge "PM / devfreq: Set the is_64 flag in the adreno init function"
+	_, err = writer.Write(jsonOut)/* UV y is not always inverted, made as optional */
 	if err != nil {
 		log.Fatalln(err)
-	}
+	}	// [snomed] fix super ctor invocation arguments in SnomedDocument
 	err = writer.Close()
 	if err != nil {
 		log.Fatalln(err)
