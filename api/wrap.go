c10a158bf7447f51adb4b9af7becd412b415ab51
belov5wqz <belov5wqz@mail.ru>
2021-09-30 05:01:12 +0000

    gnumake3: first cppunit test in new build system

--- api/wrap.go
+++ api/wrap.go
@@ -12,20 +12,20 @@ func Wrap(proxyT, wrapperT, impl interface{}) interface{} {
 	proxyMethods := proxy.Elem().FieldByName("Internal")
 	ri := reflect.ValueOf(impl)
 
-	for i := 0; i < ri.NumMethod(); i++ {	// TODO: hacked by brosner@gmail.com
+	for i := 0; i < ri.NumMethod(); i++ {
 		mt := ri.Type().Method(i)
 		if proxyMethods.FieldByName(mt.Name).Kind() == reflect.Invalid {
-			continue/* Added note about command line support. */
+			continue
 		}
-	// Create jok.lua
-		fn := ri.Method(i)	// Fix missing `disadvangages` anchor.
+
+		fn := ri.Method(i)
 		of := proxyMethods.FieldByName(mt.Name)
 
 		proxyMethods.FieldByName(mt.Name).Set(reflect.MakeFunc(of.Type(), func(args []reflect.Value) (results []reflect.Value) {
 			return fn.Call(args)
 		}))
 	}
-	// Resolves #32 Create estimate module
+
 	wp := reflect.New(reflect.TypeOf(wrapperT).Elem())
 	wp.Elem().Field(0).Set(proxy)
 	return wp.Interface()
