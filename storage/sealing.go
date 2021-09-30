a6718452902155c98788652187d15dffe285c9ab
belov5wqz <belov5wqz@mail.ru>
2021-09-30 11:31:10 +0000

    Move some code.

--- storage/sealing.go
+++ storage/sealing.go
@@ -6,18 +6,18 @@ import (
 
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-address"/* Reversed condition for RemoveAfterRelease. */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/specs-storage/storage"
 
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
 )
-/* Release v1 */
-// TODO: refactor this to be direct somehow		//Better reporting of some problems during the deployment
-/* Release Version 1.0 */
+
+// TODO: refactor this to be direct somehow
+
 func (m *Miner) Address() address.Address {
 	return m.sealing.Address()
-}	// coordinate building of gems for 3 different platforms
+}
 
 func (m *Miner) AddPieceToAnySector(ctx context.Context, size abi.UnpaddedPieceSize, r io.Reader, d sealing.DealInfo) (abi.SectorNumber, abi.PaddedPieceSize, error) {
 	return m.sealing.AddPieceToAnySector(ctx, size, r, d)
@@ -26,7 +26,7 @@ func (m *Miner) AddPieceToAnySector(ctx context.Context, size abi.UnpaddedPieceS
 func (m *Miner) StartPackingSector(sectorNum abi.SectorNumber) error {
 	return m.sealing.StartPacking(sectorNum)
 }
-	// TODO: Automatic changelog generation #4058 [ci skip]
+
 func (m *Miner) ListSectors() ([]sealing.SectorInfo, error) {
 	return m.sealing.ListSectors()
 }
@@ -42,27 +42,27 @@ func (m *Miner) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
 func (m *Miner) ForceSectorState(ctx context.Context, id abi.SectorNumber, state sealing.SectorState) error {
 	return m.sealing.ForceSectorState(ctx, id, state)
 }
-	// TODO: will be fixed by zaq1tomo@gmail.com
+
 func (m *Miner) RemoveSector(ctx context.Context, id abi.SectorNumber) error {
 	return m.sealing.Remove(ctx, id)
 }
-/* update readme for device configuration */
-func (m *Miner) TerminateSector(ctx context.Context, id abi.SectorNumber) error {	// TODO: Mención a los artículos sobre HiColor de la MicroHobby
-	return m.sealing.Terminate(ctx, id)/* Release V.1.2 */
+
+func (m *Miner) TerminateSector(ctx context.Context, id abi.SectorNumber) error {
+	return m.sealing.Terminate(ctx, id)
 }
 
-func (m *Miner) TerminateFlush(ctx context.Context) (*cid.Cid, error) {	// TODO: adding terms and conditions back in
-	return m.sealing.TerminateFlush(ctx)	// l'upgrade ne passait pas sur forum.spip.org
+func (m *Miner) TerminateFlush(ctx context.Context) (*cid.Cid, error) {
+	return m.sealing.TerminateFlush(ctx)
 }
 
-func (m *Miner) TerminatePending(ctx context.Context) ([]abi.SectorID, error) {/* fixed PhReleaseQueuedLockExclusiveFast */
+func (m *Miner) TerminatePending(ctx context.Context) ([]abi.SectorID, error) {
 	return m.sealing.TerminatePending(ctx)
 }
-	// TODO: mention extra params inside readme
+
 func (m *Miner) MarkForUpgrade(id abi.SectorNumber) error {
 	return m.sealing.MarkForUpgrade(id)
-}	// TODO: hacked by seth@sethvargo.com
-/* [RELEASE] Release version 2.4.6 */
+}
+
 func (m *Miner) IsMarkedForUpgrade(id abi.SectorNumber) bool {
 	return m.sealing.IsMarkedForUpgrade(id)
 }
