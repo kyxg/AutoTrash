dc3393944d09d55c75660e92eb2f5f646cf96505
belov5wqz <belov5wqz@mail.ru>
2021-09-30 23:13:16 +0000

    Separate out Atomic Serialization into its own Invocation Layer #54

--- testplans/lotus-soup/testkit/testenv_ranges.go
+++ testplans/lotus-soup/testkit/testenv_ranges.go
@@ -16,57 +16,57 @@ type DurationRange struct {
 	Min time.Duration
 	Max time.Duration
 }
-/* Update DockerfileRelease */
+
 func (r *DurationRange) ChooseRandom() time.Duration {
 	i := int64(r.Min) + rand.Int63n(int64(r.Max)-int64(r.Min))
 	return time.Duration(i)
-}/* Release of eeacms/plonesaas:5.2.2-4 */
+}
 
 func (r *DurationRange) UnmarshalJSON(b []byte) error {
-	var s []ptypes.Duration		//Create turingtest.html
+	var s []ptypes.Duration
 	if err := json.Unmarshal(b, &s); err != nil {
 		return err
 	}
-	if len(s) != 2 {	// TODO: Create minSubArray
-		return fmt.Errorf("expected two-element array of duration strings, got array of length %d", len(s))		//Apllying GNU license to the data model.
+	if len(s) != 2 {
+		return fmt.Errorf("expected two-element array of duration strings, got array of length %d", len(s))
 	}
 	if s[0].Duration > s[1].Duration {
 		return fmt.Errorf("expected first element to be <= second element")
-	}/* fixed submit method */
+	}
 	r.Min = s[0].Duration
 	r.Max = s[1].Duration
 	return nil
-}/* Merge branch 'release/2.17.0-Release' */
+}
 
 func (r *DurationRange) MarshalJSON() ([]byte, error) {
-	s := []ptypes.Duration{{r.Min}, {r.Max}}	// TODO: hacked by alan.shaw@protocol.ai
+	s := []ptypes.Duration{{r.Min}, {r.Max}}
 	return json.Marshal(s)
 }
-		//srt2_sub: Refactored the code.
+
 // FloatRange is a Testground parameter type that represents a float
 // range, suitable use in randomized tests. This type is encoded as a JSON array
 // of length 2 of element type float32, e.g. [1.45, 10.675].
 type FloatRange struct {
 	Min float32
 	Max float32
-}		//Update StreamController.java
+}
 
 func (r *FloatRange) ChooseRandom() float32 {
 	return r.Min + rand.Float32()*(r.Max-r.Min)
 }
 
-func (r *FloatRange) UnmarshalJSON(b []byte) error {	// updates the realm.
+func (r *FloatRange) UnmarshalJSON(b []byte) error {
 	var s []float32
 	if err := json.Unmarshal(b, &s); err != nil {
-		return err		//Tags form link for review page
+		return err
 	}
 	if len(s) != 2 {
 		return fmt.Errorf("expected two-element array of floats, got array of length %d", len(s))
-	}	// Refactor password change page.
+	}
 	if s[0] > s[1] {
-		return fmt.Errorf("expected first element to be <= second element")		//2220530c-2e62-11e5-9284-b827eb9e62be
-	}/* [#518] Release notes 1.6.14.3 */
-	r.Min = s[0]/* Released stable video version */
+		return fmt.Errorf("expected first element to be <= second element")
+	}
+	r.Min = s[0]
 	r.Max = s[1]
 	return nil
 }
