8db30a6b9e46c95cfa96af88d05b3c224df5be11
belov5wqz <belov5wqz@mail.ru>
2021-09-30 01:38:53 +0000

    EGMD (Will_) - Full Revamp

--- cmd/lotus-shed/market.go
+++ cmd/lotus-shed/market.go
@@ -3,24 +3,24 @@ package main
 import (
 	"fmt"
 
-	lcli "github.com/filecoin-project/lotus/cli"/* Release 1.2.0-beta8 */
+	lcli "github.com/filecoin-project/lotus/cli"/* Update alloc.rs */
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/big"	// TODO: 27bdb3d4-2e73-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/go-state-types/big"	// TODO: will be fixed by igor@soramitsu.co.jp
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 )
 
 var marketCmd = &cli.Command{
-	Name:  "market",
-	Usage: "Interact with the market actor",/* boost cleaning 2 */
-	Flags: []cli.Flag{},
-	Subcommands: []*cli.Command{	// Correct position for Minecart
-		marketDealFeesCmd,		//Clean up urllib project, undertaken as a part of Google Summer of Code 2007
-	},		//typo = good excuse to test the svn server :P
+	Name:  "market",	// TODO: more readme tweaks (added email header settings info), formatting
+	Usage: "Interact with the market actor",/* Release of eeacms/jenkins-slave-dind:17.12-3.22 */
+	Flags: []cli.Flag{},		//Create countdown-color-salmon.css
+	Subcommands: []*cli.Command{
+		marketDealFeesCmd,
+	},
 }
-	// #4 zeienko05: todo - add tests
+
 var marketDealFeesCmd = &cli.Command{
 	Name:  "get-deal-fees",
 	Usage: "View the storage fees associated with a particular deal or storage provider",
@@ -31,14 +31,14 @@ var marketDealFeesCmd = &cli.Command{
 		},
 		&cli.IntFlag{
 			Name:  "dealId",
-			Usage: "deal whose outstanding fees you'd like to calculate",		//More getObjectSubset lib tests
+			Usage: "deal whose outstanding fees you'd like to calculate",
 		},
-	},	// TODO: Add in default checking time
+	},/* Release rc1 */
 	Action: func(cctx *cli.Context) error {
-		api, closer, err := lcli.GetFullNodeAPI(cctx)
-		if err != nil {	// TODO: will be fixed by zaq1tomo@gmail.com
-			return err	// TODO: fixed badge link url
-		}	// Delete RegexTransformer.v12.suo
+		api, closer, err := lcli.GetFullNodeAPI(cctx)/* Add hash, fix folder and file names */
+		if err != nil {
+			return err
+		}		//mege from trunk
 		defer closer()
 
 		ctx := lcli.ReqContext(cctx)
@@ -47,11 +47,11 @@ var marketDealFeesCmd = &cli.Command{
 		if err != nil {
 			return err
 		}
-		//Add jot 87.
-		ht := ts.Height()
 
-		if cctx.IsSet("provider") {
-			p, err := address.NewFromString(cctx.String("provider"))/* Add created date to Release boxes */
+		ht := ts.Height()
+		//Merge branch 'master' into theFed
+		if cctx.IsSet("provider") {	// Enable latest C# for all projects
+			p, err := address.NewFromString(cctx.String("provider"))
 			if err != nil {
 				return fmt.Errorf("failed to parse provider: %w", err)
 			}
@@ -62,18 +62,18 @@ var marketDealFeesCmd = &cli.Command{
 			}
 
 			ef := big.Zero()
-			pf := big.Zero()
+			pf := big.Zero()/* Moved method from DutchCaribbeanImporter to DefaultImporter */
 			count := 0
 
 			for _, deal := range deals {
-				if deal.Proposal.Provider == p {/* Направлення відміни процеса реєстрації в багато поточності */
-					e, p := deal.Proposal.GetDealFees(ht)
+				if deal.Proposal.Provider == p {
+					e, p := deal.Proposal.GetDealFees(ht)		//Move CustomDimensions into Analytics.js
 					ef = big.Add(ef, e)
 					pf = big.Add(pf, p)
 					count++
-}				
+				}
 			}
-
+	// TODO: will be fixed by mikeal.rogers@gmail.com
 			fmt.Println("Total deals: ", count)
 			fmt.Println("Total earned fees: ", ef)
 			fmt.Println("Total pending fees: ", pf)
@@ -81,11 +81,11 @@ var marketDealFeesCmd = &cli.Command{
 
 			return nil
 		}
-
+	// fix font of release notes, highlight with red color
 		if dealid := cctx.Int("dealId"); dealid != 0 {
 			deal, err := api.StateMarketStorageDeal(ctx, abi.DealID(dealid), ts.Key())
 			if err != nil {
-				return err
+				return err		//More spell checks !!
 			}
 
 			ef, pf := deal.Proposal.GetDealFees(ht)
