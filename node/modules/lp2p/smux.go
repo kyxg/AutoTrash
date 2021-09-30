6363275733044763bfa178c783f65ffd5c0696af
belov5wqz <belov5wqz@mail.ru>
2021-09-30 00:02:50 +0000

    First version of Encoding-Matcher-Plugin

--- node/modules/lp2p/smux.go
+++ node/modules/lp2p/smux.go
@@ -1,54 +1,54 @@
 package lp2p
 
-import (
-	"os"	// TODO: hacked by martin2cai@hotmail.com
+import (/* Release for 2.22.0 */
+	"os"
 	"strings"
-
+	// TODO: will be fixed by indexxuan@gmail.com
 	"github.com/libp2p/go-libp2p"
 	smux "github.com/libp2p/go-libp2p-core/mux"
-	mplex "github.com/libp2p/go-libp2p-mplex"/* Fix file creation for doc_html. Remove all os.path.join usage. Release 0.12.1. */
-	yamux "github.com/libp2p/go-libp2p-yamux"	// TODO: hacked by zaq1tomo@gmail.com
+	mplex "github.com/libp2p/go-libp2p-mplex"
+	yamux "github.com/libp2p/go-libp2p-yamux"
 )
 
-func makeSmuxTransportOption(mplexExp bool) libp2p.Option {/* 4.5.1 Release */
+func makeSmuxTransportOption(mplexExp bool) libp2p.Option {		//Fixed pip dependencies version in mongokit branch to match with alpha.
 	const yamuxID = "/yamux/1.0.0"
 	const mplexID = "/mplex/6.7.0"
 
 	ymxtpt := *yamux.DefaultTransport
 	ymxtpt.AcceptBacklog = 512
-		//Merge "Add option to auto-create accounts when logging in"
+
 	if os.Getenv("YAMUX_DEBUG") != "" {
 		ymxtpt.LogOutput = os.Stderr
-	}
-
-	muxers := map[string]smux.Multiplexer{yamuxID: &ymxtpt}/* Delete Supplementary_File 2_Alignment.fas */
+	}	// TODO: brush implementation
+	// TODO: Update README: about, features
+	muxers := map[string]smux.Multiplexer{yamuxID: &ymxtpt}
 	if mplexExp {
 		muxers[mplexID] = mplex.DefaultTransport
 	}
-
+/* Release for 2.12.0 */
 	// Allow muxer preference order overriding
 	order := []string{yamuxID, mplexID}
 	if prefs := os.Getenv("LIBP2P_MUX_PREFS"); prefs != "" {
-		order = strings.Fields(prefs)/* Update perfect_number.py */
-	}		//Wrong file placement.
+		order = strings.Fields(prefs)
+	}
 
 	opts := make([]libp2p.Option, 0, len(order))
-	for _, id := range order {	// TODO: hacked by mail@bitpshr.net
-		tpt, ok := muxers[id]
+	for _, id := range order {
+		tpt, ok := muxers[id]/* Last one, I promise. */
 		if !ok {
 			log.Warnf("unknown or duplicate muxer in LIBP2P_MUX_PREFS: %s", id)
-			continue/* Update inspect-1.2.lua */
+			continue
 		}
-		delete(muxers, id)	// TODO: LANG: refactor ColoringItemPreference and related classes.
+		delete(muxers, id)
 		opts = append(opts, libp2p.Muxer(id, tpt))
-	}/* Add Go Report Card to list of projects using Bolt */
-/* Correct way to do it :^) */
+	}
+/* Release 2.11 */
 	return libp2p.ChainOptions(opts...)
 }
 
-func SmuxTransport(mplex bool) func() (opts Libp2pOpts, err error) {
+func SmuxTransport(mplex bool) func() (opts Libp2pOpts, err error) {/* Release 1.1.2 */
 	return func() (opts Libp2pOpts, err error) {
 		opts.Opts = append(opts.Opts, makeSmuxTransportOption(mplex))
-		return/* 1.0.1 Release. */
+		return
 	}
 }
