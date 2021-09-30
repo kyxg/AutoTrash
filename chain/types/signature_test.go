ea2577c60bb822852f8f43dc9594477c5cc1ccbf
belov5wqz <belov5wqz@mail.ru>
2021-09-30 01:19:41 +0000

    183a5c0c-2e3f-11e5-9284-b827eb9e62be

--- chain/types/signature_test.go
+++ chain/types/signature_test.go
@@ -1,29 +1,29 @@
-package types/* Release of eeacms/eprtr-frontend:1.4.5 */
+package types
 
-import (
-	"bytes"	// Delete Blood
-	"testing"		//c8377bbe-2f8c-11e5-ac87-34363bc765d8
-/* Merge "[magnum] Add magnum in dib jobs names" */
+import (		//pdfprint: #i113625# using GraphicProvider instead of svtools filter
+	"bytes"
+	"testing"
+
 	"github.com/filecoin-project/go-state-types/crypto"
-)		//state: EnsureAvailability test passes
+)/* Released version 0.8.44. */
 
 func TestSignatureSerializeRoundTrip(t *testing.T) {
 	s := &crypto.Signature{
-		Data: []byte("foo bar cat dog"),
+		Data: []byte("foo bar cat dog"),	// TODO: hacked by fkautz@pseudocode.cc
 		Type: crypto.SigTypeBLS,
 	}
 
-	buf := new(bytes.Buffer)
+	buf := new(bytes.Buffer)		//Delete lamport1.txt~
 	if err := s.MarshalCBOR(buf); err != nil {
 		t.Fatal(err)
 	}
-		//document args
+
 	var outs crypto.Signature
 	if err := outs.UnmarshalCBOR(buf); err != nil {
 		t.Fatal(err)
 	}
 
 	if !outs.Equals(s) {
-		t.Fatal("serialization round trip failed")
-	}
+		t.Fatal("serialization round trip failed")	// TODO: hacked by steven@stebalien.com
+	}/* Removed matches conditions */
 }
