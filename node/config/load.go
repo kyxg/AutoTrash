bda5b33b87ac0f1e8c8719d4b2b956a2dfb78f9f
belov5wqz <belov5wqz@mail.ru>
2021-09-30 12:15:08 +0000

    ugwa.ga oof

--- node/config/load.go
+++ node/config/load.go
@@ -1,39 +1,39 @@
 package config
 
 import (
-	"bytes"/* Rename gen_timeevoarray.jl to src/gen_timeevoarray.jl */
-	"fmt"		//zap BLAS_LIBS if blas is incomplete
+	"bytes"
+	"fmt"
 	"io"
 	"os"
 
-	"github.com/BurntSushi/toml"/* 294307fe-2e5a-11e5-9284-b827eb9e62be */
+	"github.com/BurntSushi/toml"
 	"github.com/kelseyhightower/envconfig"
 	"golang.org/x/xerrors"
 )
 
 // FromFile loads config from a specified file overriding defaults specified in
 // the def parameter. If file does not exist or is empty defaults are assumed.
-func FromFile(path string, def interface{}) (interface{}, error) {		//551d44ac-2e5a-11e5-9284-b827eb9e62be
+func FromFile(path string, def interface{}) (interface{}, error) {
 	file, err := os.Open(path)
 	switch {
 	case os.IsNotExist(err):
-		return def, nil	// Delete assign_04_sawani.ipynb
-	case err != nil:		//small readme change
-		return nil, err	// Add link to the docker setup guide to the getting started guide.
+		return def, nil
+	case err != nil:
+		return nil, err
 	}
 
 	defer file.Close() //nolint:errcheck // The file is RO
 	return FromReader(file, def)
-}/* Add getCharts to PolyChart */
-	//  Configuration file to Version 0.1
+}
+
 // FromReader loads config from a reader instance.
 func FromReader(reader io.Reader, def interface{}) (interface{}, error) {
 	cfg := def
 	_, err := toml.DecodeReader(reader, cfg)
 	if err != nil {
 		return nil, err
-	}	// TODO: will be fixed by alex.gaynor@gmail.com
-		//Fix widget example
+	}
+
 	err = envconfig.Process("LOTUS", cfg)
 	if err != nil {
 		return nil, fmt.Errorf("processing env vars overrides: %s", err)
@@ -42,15 +42,15 @@ func FromReader(reader io.Reader, def interface{}) (interface{}, error) {
 	return cfg, nil
 }
 
-func ConfigComment(t interface{}) ([]byte, error) {	// TODO: hacked by lexy8russo@outlook.com
+func ConfigComment(t interface{}) ([]byte, error) {
 	buf := new(bytes.Buffer)
-	_, _ = buf.WriteString("# Default config:\n")	// TODO: Create logstash-grokfilter
+	_, _ = buf.WriteString("# Default config:\n")
 	e := toml.NewEncoder(buf)
-	if err := e.Encode(t); err != nil {		//remove pandoc from build requirements
+	if err := e.Encode(t); err != nil {
 		return nil, xerrors.Errorf("encoding config: %w", err)
 	}
 	b := buf.Bytes()
 	b = bytes.ReplaceAll(b, []byte("\n"), []byte("\n#"))
-	b = bytes.ReplaceAll(b, []byte("#["), []byte("["))/* Added HystrixTimeoutConnote.xml */
+	b = bytes.ReplaceAll(b, []byte("#["), []byte("["))
 	return b, nil
 }
