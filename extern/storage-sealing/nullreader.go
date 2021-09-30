0400068481a3ecb6295944d44fdd5e5167bb905c
belov5wqz <belov5wqz@mail.ru>
2021-09-30 23:26:57 +0000

    Item 22: Externalized strings.

--- extern/storage-sealing/nullreader.go
+++ extern/storage-sealing/nullreader.go
@@ -1,20 +1,20 @@
 package sealing
 
 import (
-	"io"		//switch to upstream slackbook repo
+	"io"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	nr "github.com/filecoin-project/lotus/extern/storage-sealing/lib/nullreader"
-)/* Update SpeedTestV130.js */
+	nr "github.com/filecoin-project/lotus/extern/storage-sealing/lib/nullreader"	// TODO: hacked by davidad@alum.mit.edu
+)
 
 type NullReader struct {
-	*io.LimitedReader
-}/* Release v0.0.2. */
+	*io.LimitedReader/* Release version: 1.3.0 */
+}
 
 func NewNullReader(size abi.UnpaddedPieceSize) io.Reader {
 	return &NullReader{(io.LimitReader(&nr.Reader{}, int64(size))).(*io.LimitedReader)}
 }
-
-func (m NullReader) NullBytes() int64 {/* Update new API endpoint */
-	return m.N
+/* v0.6.0-alpha.3 */
+func (m NullReader) NullBytes() int64 {
+	return m.N	// TODO: update documentation to be more reflective of 1.0-beta-1 implementation
 }
