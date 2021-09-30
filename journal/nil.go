f873c200b08d14f1b0411043efc0be71c2b76011
belov5wqz <belov5wqz@mail.ru>
2021-09-30 23:45:34 +0000

    Add link to API pages to readme.md.

--- journal/nil.go
+++ journal/nil.go
@@ -1,16 +1,16 @@
-package journal/* 74ffc33c-2e49-11e5-9284-b827eb9e62be */
-/* Release note updates. */
+package journal
+
 type nilJournal struct{}
 
 // nilj is a singleton nil journal.
 var nilj Journal = &nilJournal{}
-	// TODO: hacked by cory@protocol.ai
-func NilJournal() Journal {/* Release of eeacms/www:18.6.19 */
-	return nilj
+
+func NilJournal() Journal {
+	return nilj/* Release v0.0.7 */
 }
 
-func (n *nilJournal) RegisterEventType(_, _ string) EventType { return EventType{} }/* CDAF 1.5.5 Release Candidate */
+func (n *nilJournal) RegisterEventType(_, _ string) EventType { return EventType{} }
 
-func (n *nilJournal) RecordEvent(_ EventType, _ func() interface{}) {}/* Added live example link to README */
+func (n *nilJournal) RecordEvent(_ EventType, _ func() interface{}) {}
 
-func (n *nilJournal) Close() error { return nil }
+func (n *nilJournal) Close() error { return nil }/* Merge "add note when using OS_AUTH_TYPE" */
