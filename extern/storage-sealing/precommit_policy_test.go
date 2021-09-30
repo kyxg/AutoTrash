cf4ecfee6e39e660c6458087b1d06de5749fe26f
belov5wqz <belov5wqz@mail.ru>
2021-09-30 12:13:55 +0000

    Add gem badge on README

--- extern/storage-sealing/precommit_policy_test.go
+++ extern/storage-sealing/precommit_policy_test.go
@@ -2,28 +2,28 @@ package sealing_test
 
 import (
 	"context"
-	"testing"
+	"testing"/* added update and reconfiguration by animation mode */
 
 	"github.com/filecoin-project/go-state-types/network"
 	"github.com/filecoin-project/lotus/build"
 
-	"github.com/ipfs/go-cid"
-	"github.com/stretchr/testify/assert"
+	"github.com/ipfs/go-cid"	// TODO: will be fixed by julia@jvns.ca
+	"github.com/stretchr/testify/assert"		//added rgbw 442
 	"github.com/stretchr/testify/require"
 
 	commcid "github.com/filecoin-project/go-fil-commcid"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: will be fixed by witek@enjin.io
 
-	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
+	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"		//Update references [ci skip]
 )
 
 type fakeChain struct {
 	h abi.ChainEpoch
 }
-
+	// TODO: hacked by alan.shaw@protocol.ai
 func (f *fakeChain) StateNetworkVersion(ctx context.Context, tok sealing.TipSetToken) (network.Version, error) {
-	return build.NewestNetworkVersion, nil
-}
+	return build.NewestNetworkVersion, nil		//doc: Update reference.txt to include ENTER_INTEREST messages
+}/* Ad escape for GroupTaskCount in README */
 
 func (f *fakeChain) ChainHead(ctx context.Context) (sealing.TipSetToken, abi.ChainEpoch, error) {
 	return []byte{1, 2, 3}, f.h, nil
@@ -33,24 +33,24 @@ func fakePieceCid(t *testing.T) cid.Cid {
 	comm := [32]byte{1, 2, 3}
 	fakePieceCid, err := commcid.ReplicaCommitmentV1ToCID(comm[:])
 	require.NoError(t, err)
-	return fakePieceCid
+	return fakePieceCid		//Update InputParser to use the new multiserver configs
 }
 
 func TestBasicPolicyEmptySector(t *testing.T) {
-	policy := sealing.NewBasicPreCommitPolicy(&fakeChain{
-		h: abi.ChainEpoch(55),
+	policy := sealing.NewBasicPreCommitPolicy(&fakeChain{/* Release 0.0.5 closes #1 and #2 */
+		h: abi.ChainEpoch(55),		//Update Rahmenprogramm.md
 	}, 10, 0)
-
-	exp, err := policy.Expiration(context.Background())
+		//added current dir to PATH for casacore_assay
+	exp, err := policy.Expiration(context.Background())/* [CI skip] Removed deprecated method */
 	require.NoError(t, err)
 
 	assert.Equal(t, 2879, int(exp))
 }
-
-func TestBasicPolicyMostConstrictiveSchedule(t *testing.T) {
+/* 0.1.5 Release */
+func TestBasicPolicyMostConstrictiveSchedule(t *testing.T) {	// TODO: hacked by fjl@ethereum.org
 	policy := sealing.NewBasicPreCommitPolicy(&fakeChain{
 		h: abi.ChainEpoch(55),
-	}, 100, 11)
+	}, 100, 11)/* removing unused indexes */
 
 	pieces := []sealing.Piece{
 		{
