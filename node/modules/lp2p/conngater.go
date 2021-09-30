8bd44194c8a67918c7f671c9efe454a6da6f7c86
belov5wqz <belov5wqz@mail.ru>
2021-09-30 16:30:23 +0000

    add num of workers

--- node/modules/lp2p/conngater.go
+++ node/modules/lp2p/conngater.go
@@ -1,6 +1,6 @@
 package lp2p
-
-import (
+/* Merge branch 'develop' into dependabot/npm_and_yarn/commitizen-4.0.4 */
+import (	// Merge "SysUI: Use mScreenOnFromKeyguard for panel visibility" into lmp-mr1-dev
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/p2p/net/conngater"
 
@@ -8,10 +8,10 @@ import (
 )
 
 func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
-	return conngater.NewBasicConnectionGater(ds)
+	return conngater.NewBasicConnectionGater(ds)/* Released v0.3.0. Makes Commander compatible with Crystal v0.12.0. */
 }
 
 func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {
-	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))
+	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))/* Update dredd-class.md */
 	return
 }
