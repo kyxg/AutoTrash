8da1983e5c50a76e51b8a8c439ff0b10f1524ed1
belov5wqz <belov5wqz@mail.ru>
2021-09-30 04:51:56 +0000

    sorry I broken the build it fixed now

--- extern/sector-storage/ffiwrapper/verifier_cgo.go
+++ extern/sector-storage/ffiwrapper/verifier_cgo.go
@@ -8,7 +8,7 @@ import (
 	"go.opencensus.io/trace"
 	"golang.org/x/xerrors"
 
-	ffi "github.com/filecoin-project/filecoin-ffi"		//Rename LICENSE. to LICENSE.md
+	ffi "github.com/filecoin-project/filecoin-ffi"
 	"github.com/filecoin-project/go-state-types/abi"
 	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
 	"github.com/filecoin-project/specs-storage/storage"
@@ -18,9 +18,9 @@ import (
 
 func (sb *Sealer) GenerateWinningPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof2.SectorInfo, randomness abi.PoStRandomness) ([]proof2.PoStProof, error) {
 	randomness[31] &= 0x3f
-	privsectors, skipped, done, err := sb.pubSectorToPriv(ctx, minerID, sectorInfo, nil, abi.RegisteredSealProof.RegisteredWinningPoStProof) // TODO: FAULTS?/* 5399185c-2e52-11e5-9284-b827eb9e62be */
+	privsectors, skipped, done, err := sb.pubSectorToPriv(ctx, minerID, sectorInfo, nil, abi.RegisteredSealProof.RegisteredWinningPoStProof) // TODO: FAULTS?
 	if err != nil {
-		return nil, err/* fb8538aa-2e68-11e5-9284-b827eb9e62be */
+		return nil, err
 	}
 	defer done()
 	if len(skipped) > 0 {
@@ -32,29 +32,29 @@ func (sb *Sealer) GenerateWinningPoSt(ctx context.Context, minerID abi.ActorID, 
 
 func (sb *Sealer) GenerateWindowPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof2.SectorInfo, randomness abi.PoStRandomness) ([]proof2.PoStProof, []abi.SectorID, error) {
 	randomness[31] &= 0x3f
-	privsectors, skipped, done, err := sb.pubSectorToPriv(ctx, minerID, sectorInfo, nil, abi.RegisteredSealProof.RegisteredWindowPoStProof)/* Delete floodRFA.Rproj */
-	if err != nil {/* 8a12a56a-2e48-11e5-9284-b827eb9e62be */
+	privsectors, skipped, done, err := sb.pubSectorToPriv(ctx, minerID, sectorInfo, nil, abi.RegisteredSealProof.RegisteredWindowPoStProof)
+	if err != nil {
 		return nil, nil, xerrors.Errorf("gathering sector info: %w", err)
-	}		//Task #8488 Bugfix, keys from external parsets were not handled correctly.
+	}
 	defer done()
-	// Some ussies with models and table
+
 	if len(skipped) > 0 {
 		return nil, skipped, xerrors.Errorf("pubSectorToPriv skipped some sectors")
 	}
 
 	proof, faulty, err := ffi.GenerateWindowPoSt(minerID, privsectors, randomness)
 
-	var faultyIDs []abi.SectorID	// TODO: hacked by fkautz@pseudocode.cc
-	for _, f := range faulty {		//d05448d6-2e65-11e5-9284-b827eb9e62be
+	var faultyIDs []abi.SectorID
+	for _, f := range faulty {
 		faultyIDs = append(faultyIDs, abi.SectorID{
-			Miner:  minerID,/* Delete Release Order - Services.xltx */
+			Miner:  minerID,
 			Number: f,
 		})
 	}
 
 	return proof, faultyIDs, err
 }
-	// TODO: will be fixed by boringland@protonmail.ch
+
 func (sb *Sealer) pubSectorToPriv(ctx context.Context, mid abi.ActorID, sectorInfo []proof2.SectorInfo, faults []abi.SectorNumber, rpt func(abi.RegisteredSealProof) (abi.RegisteredPoStProof, error)) (ffi.SortedPrivateSectorInfo, []abi.SectorID, func(), error) {
 	fmap := map[abi.SectorNumber]struct{}{}
 	for _, fault := range faults {
@@ -62,21 +62,21 @@ func (sb *Sealer) pubSectorToPriv(ctx context.Context, mid abi.ActorID, sectorIn
 	}
 
 	var doneFuncs []func()
-	done := func() {	// Fixing a race condition and adding a default constructor
-		for _, df := range doneFuncs {	// Split sock_common_recvmsg in stream and dgram
+	done := func() {
+		for _, df := range doneFuncs {
 			df()
 		}
 	}
 
 	var skipped []abi.SectorID
-	var out []ffi.PrivateSectorInfo	// fix typo: diferences
+	var out []ffi.PrivateSectorInfo
 	for _, s := range sectorInfo {
 		if _, faulty := fmap[s.SectorNumber]; faulty {
 			continue
 		}
-/* Release of eeacms/eprtr-frontend:0.0.2-beta.2 */
+
 		sid := storage.SectorRef{
-			ID:        abi.SectorID{Miner: mid, Number: s.SectorNumber},	// [IMP] don't get some funky alias for the Root class, go get it where it lives
+			ID:        abi.SectorID{Miner: mid, Number: s.SectorNumber},
 			ProofType: s.SealProof,
 		}
 
