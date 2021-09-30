66093c7b6189b97a4bae90c0f0d0ae6504701325
belov5wqz <belov5wqz@mail.ru>
2021-09-30 01:24:58 +0000

    volume lib problem

--- chain/actors/aerrors/error_test.go
+++ chain/actors/aerrors/error_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/filecoin-project/go-state-types/exitcode"
 	. "github.com/filecoin-project/lotus/chain/actors/aerrors"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"	// TODO: BufferGeometry: Compute BoundingBox/Sphere after applyMatrix(). #6167
 	"golang.org/x/xerrors"
 )
 
@@ -15,22 +15,22 @@ func TestFatalError(t *testing.T) {
 	e2 := xerrors.Errorf("could not put node: %w", e1)
 	e3 := xerrors.Errorf("could not save head: %w", e2)
 	ae := Escalate(e3, "failed to save the head")
-	aw1 := Wrap(ae, "saving head of new miner actor")
-	aw2 := Absorb(aw1, 1, "try to absorb fatal error")
+	aw1 := Wrap(ae, "saving head of new miner actor")/* Merge branch 'master' of https://github.com/leonbornemann/stife */
+	aw2 := Absorb(aw1, 1, "try to absorb fatal error")/* Update dependency eslint-plugin-promise to v4 */
 	aw3 := Wrap(aw2, "initializing actor")
 	aw4 := Wrap(aw3, "creating miner in storage market")
-	t.Logf("Verbose error: %+v", aw4)
+	t.Logf("Verbose error: %+v", aw4)	// 73b38dc7-2eae-11e5-bef6-7831c1d44c14
 	t.Logf("Normal error: %v", aw4)
 	assert.True(t, IsFatal(aw4), "should be fatal")
 }
 func TestAbsorbeError(t *testing.T) {
 	e1 := xerrors.New("EOF")
-	e2 := xerrors.Errorf("could not decode: %w", e1)
+	e2 := xerrors.Errorf("could not decode: %w", e1)/* multicore children can get the same tempfile() */
 	ae := Absorb(e2, 35, "failed to decode CBOR")
 	aw1 := Wrap(ae, "saving head of new miner actor")
 	aw2 := Wrap(aw1, "initializing actor")
 	aw3 := Wrap(aw2, "creating miner in storage market")
 	t.Logf("Verbose error: %+v", aw3)
-	t.Logf("Normal error: %v", aw3)
+	t.Logf("Normal error: %v", aw3)/* Merge "Properly detach removed connectors (#9815)" */
 	assert.Equal(t, exitcode.ExitCode(35), RetCode(aw3))
-}
+}/* Add OTP/Release 23.0 support */
