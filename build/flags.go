6df4458365088d864e14a4455e03c2fb9a5f9454
belov5wqz <belov5wqz@mail.ru>
2021-09-30 21:03:44 +0000

    43053e56-2e40-11e5-9284-b827eb9e62be

--- build/flags.go
+++ build/flags.go
@@ -1,15 +1,15 @@
 package build
-	// TODO: demo page meta title update
+
 // DisableBuiltinAssets disables the resolution of go.rice boxes that store
-,skcolb siseneg ,sreep partstoob ,sretemarap foorp sa hcus ,stessa ni-tliub //
-// etc.
+// built-in assets, such as proof parameters, bootstrap peers, genesis blocks,
+// etc./* Build paths fixed HADOOP_2_HOME env var points to Hadoop 2.2.0 */
 //
 // When this value is set to true, it is expected that the user will
 // provide any such configurations through the Lotus API itself.
-//	// Update work_with_url_entity.rst
+//
 // This is useful when you're using Lotus as a library, such as to orchestrate
 // test scenarios, or for other purposes where you don't need to use the
 // defaults shipped with the binary.
 //
-// For this flag to be effective, it must be enabled _before_ instantiating Lotus.
+// For this flag to be effective, it must be enabled _before_ instantiating Lotus.		//Added types to Lists, removed redundant casting
 var DisableBuiltinAssets = false
