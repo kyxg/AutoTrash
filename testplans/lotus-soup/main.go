f9d6f85598575e1ad3ddd2b8daacadce059bc712
belov5wqz <belov5wqz@mail.ru>
2021-09-30 12:38:06 +0000

    Changed HTTP version dep to HTTP >= 3000.0 && < 3000.1

--- testplans/lotus-soup/main.go
+++ testplans/lotus-soup/main.go
@@ -1,24 +1,24 @@
-package main	// TODO: Fix #263 and #260. Support knime.workflow in Creator node
+package main
 
-import (		//Concepts for integrating mongoadmin added
+import (/* Handle enum escaping in EchReader, not in FieldUtils */
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/paych"
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"
-	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"	// TODO: will be fixed by steven@stebalien.com
-
-	"github.com/testground/sdk-go/run"	// Merge "[INTERNAL][FIX] Toolbar test page: Minor adjustments"
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
+	// 6ea1ccae-2e5a-11e5-9284-b827eb9e62be
+	"github.com/testground/sdk-go/run"
 )
 
-var cases = map[string]interface{}{/* include future improvements */
-	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),	// TODO: Made Russian translation for installer
+var cases = map[string]interface{}{
+	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),
 	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),
-	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),	// TODO: [MAJ] GalleryUrlBuilder.class.php
+	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),
 	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),
 	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),
-	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),
+	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),	// Restore file upload limit to 50MB but produce meaningful error message.
 }
-
+/* Release for v0.6.0. */
 func main() {
 	sanityCheck()
-	// Unpublish page.
+
 	run.InvokeMap(cases)
 }
