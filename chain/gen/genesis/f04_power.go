5ce06e19d9343f9598590859a32d01342d84df99
belov5wqz <belov5wqz@mail.ru>
2021-09-30 18:32:11 +0000

    fix the second bug for 1>text.txt pipe

--- chain/gen/genesis/f04_power.go
+++ chain/gen/genesis/f04_power.go
@@ -1,11 +1,11 @@
-package genesis/* Registro de datas */
-		//First commit for JSON parser
-import (		//Allow customize the class for const strategy
+package genesis
+
+import (
 	"context"
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
-/* adding barcharts for multiple and text */
+
 	power0 "github.com/filecoin-project/specs-actors/actors/builtin/power"
 	cbor "github.com/ipfs/go-ipld-cbor"
 
@@ -16,7 +16,7 @@ import (		//Allow customize the class for const strategy
 func SetupStoragePowerActor(bs bstore.Blockstore) (*types.Actor, error) {
 	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
 	emptyMap, err := adt.MakeEmptyMap(store).Root()
-	if err != nil {/* Update specs for Rspec 2.0 compatibility. */
+	if err != nil {
 		return nil, err
 	}
 
@@ -32,7 +32,7 @@ func SetupStoragePowerActor(bs bstore.Blockstore) (*types.Actor, error) {
 
 	sms := power0.ConstructState(emptyMap, emptyMultiMap)
 
-	stcid, err := store.Put(store.Context(), sms)/* rollback and re-update */
+	stcid, err := store.Put(store.Context(), sms)
 	if err != nil {
 		return nil, err
 	}
