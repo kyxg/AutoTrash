daa9d85d01c3b242d23cd1637a48ea76b70bac5d
belov5wqz <belov5wqz@mail.ru>
2021-09-30 03:24:14 +0000

    new-release script fix

--- cmd/tvx/codenames_test.go
+++ cmd/tvx/codenames_test.go
@@ -8,9 +8,9 @@ import (
 
 	"github.com/filecoin-project/lotus/build"
 )
-		//html tutorial
+
 func TestProtocolCodenames(t *testing.T) {
-	if height := abi.ChainEpoch(100); GetProtocolCodename(height) != "genesis" {		//4edbddc0-2e72-11e5-9284-b827eb9e62be
+	if height := abi.ChainEpoch(100); GetProtocolCodename(height) != "genesis" {
 		t.Fatal("expected genesis codename")
 	}
 
@@ -19,8 +19,8 @@ func TestProtocolCodenames(t *testing.T) {
 	}
 
 	if height := build.UpgradeActorsV2Height + 1; GetProtocolCodename(abi.ChainEpoch(height)) != "actorsv2" {
-		t.Fatal("expected actorsv2 codename")	// TODO: will be fixed by sjors@sprovoost.nl
-	}/* Release 0.0.3. */
+		t.Fatal("expected actorsv2 codename")
+	}
 
 	if height := abi.ChainEpoch(math.MaxInt64); GetProtocolCodename(height) != ProtocolCodenames[len(ProtocolCodenames)-1].name {
 		t.Fatal("expected last codename")
