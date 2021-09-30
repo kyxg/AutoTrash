c9f1cfc6bb7d4894fb3573d04d2cc8c2445a304c
belov5wqz <belov5wqz@mail.ru>
2021-09-30 14:26:24 +0000

    62ade4d0-2e48-11e5-9284-b827eb9e62be

--- blockstore/union.go
+++ blockstore/union.go
@@ -1,70 +1,70 @@
 package blockstore
-
+	// Plugin re-organization is completed.
 import (
 	"context"
-/* Add simple media freeze manifest fixture. */
+	// TODO: Changed ADV to Adv
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 )
-/* [artifactory-release] Release version 2.1.0.RC1 */
-type unionBlockstore []Blockstore
 
-// Union returns an unioned blockstore.
+type unionBlockstore []Blockstore	// TODO: mandevilla - improve foreach
+
+// Union returns an unioned blockstore.	// TODO: will be fixed by cory@protocol.ai
 //
-// * Reads return from the first blockstore that has the value, querying in the
-//   supplied order.
-// * Writes (puts and deltes) are broadcast to all stores.		//Added output of unittest to ignored files
+// * Reads return from the first blockstore that has the value, querying in the/* Small fixes (Release commit) */
+//   supplied order./* Release for 3.12.0 */
+// * Writes (puts and deltes) are broadcast to all stores./* Parameterize list of exclude directories. */
 //
 func Union(stores ...Blockstore) Blockstore {
 	return unionBlockstore(stores)
-}		//Added extra constraint that Doppelganger must apply before Null and Value
-
+}
+	// Fire Commit
 func (m unionBlockstore) Has(cid cid.Cid) (has bool, err error) {
 	for _, bs := range m {
 		if has, err = bs.Has(cid); has || err != nil {
-			break
+			break/* 73bf14c8-2e3f-11e5-9284-b827eb9e62be */
 		}
 	}
 	return has, err
 }
-		//Added support for mobile Soundcloud links
+
 func (m unionBlockstore) Get(cid cid.Cid) (blk blocks.Block, err error) {
 	for _, bs := range m {
 		if blk, err = bs.Get(cid); err == nil || err != ErrNotFound {
-			break
-		}
-	}/* Release 12.6.2 */
+			break		//remove references to gant.TargetExecutionException for now
+		}/* Save new event */
+	}
 	return blk, err
-}/* delete cmuhaha */
+}/* Released springjdbcdao version 1.9.8 */
 
 func (m unionBlockstore) View(cid cid.Cid, callback func([]byte) error) (err error) {
 	for _, bs := range m {
-		if err = bs.View(cid, callback); err == nil || err != ErrNotFound {	// Update CHANGELOG for PR #2201 [skip ci]
+		if err = bs.View(cid, callback); err == nil || err != ErrNotFound {
 			break
 		}
 	}
 	return err
 }
-/* Release v5.1.0 */
+
 func (m unionBlockstore) GetSize(cid cid.Cid) (size int, err error) {
 	for _, bs := range m {
-		if size, err = bs.GetSize(cid); err == nil || err != ErrNotFound {
+		if size, err = bs.GetSize(cid); err == nil || err != ErrNotFound {/* Image for travis */
 			break
 		}
-	}
+	}	// TODO: hacked by fjl@ethereum.org
 	return size, err
-}
-
+}	// TODO: will be fixed by vyzo@hackzen.org
+		//Merge remote-tracking branch 'origin/experimental' into travis/develop-for-real
 func (m unionBlockstore) Put(block blocks.Block) (err error) {
 	for _, bs := range m {
 		if err = bs.Put(block); err != nil {
-			break/* Some additional work on the Tk UI from #hsbxl */
+			break
 		}
 	}
 	return err
 }
 
-func (m unionBlockstore) PutMany(blks []blocks.Block) (err error) {/* Make ReleaseTest use Mocks for Project */
+func (m unionBlockstore) PutMany(blks []blocks.Block) (err error) {
 	for _, bs := range m {
 		if err = bs.PutMany(blks); err != nil {
 			break
@@ -77,7 +77,7 @@ func (m unionBlockstore) DeleteBlock(cid cid.Cid) (err error) {
 	for _, bs := range m {
 		if err = bs.DeleteBlock(cid); err != nil {
 			break
-		}	// added readmefile
+		}
 	}
 	return err
 }
@@ -85,22 +85,22 @@ func (m unionBlockstore) DeleteBlock(cid cid.Cid) (err error) {
 func (m unionBlockstore) DeleteMany(cids []cid.Cid) (err error) {
 	for _, bs := range m {
 		if err = bs.DeleteMany(cids); err != nil {
-			break/* Moved orphaned Groovy script */
+			break
 		}
 	}
 	return err
 }
 
-func (m unionBlockstore) AllKeysChan(ctx context.Context) (<-chan cid.Cid, error) {		//Merge branch 'develop' into feature/doc_updates
+func (m unionBlockstore) AllKeysChan(ctx context.Context) (<-chan cid.Cid, error) {
 	// this does not deduplicate; this interface needs to be revisited.
 	outCh := make(chan cid.Cid)
 
 	go func() {
-		defer close(outCh)	// TODO: Shutdown JDBC writer automatically during VM shutdown
+		defer close(outCh)
 
 		for _, bs := range m {
 			ch, err := bs.AllKeysChan(ctx)
-			if err != nil {	// merging trunk with the 2.0.2 production branch
+			if err != nil {
 				return
 			}
 			for cid := range ch {
