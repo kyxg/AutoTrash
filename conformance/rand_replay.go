af07fe1b1fb4616640851e7acba1f89f68664431
belov5wqz <belov5wqz@mail.ru>
2021-09-30 09:02:42 +0000

    Created IMG_6236.JPG

--- conformance/rand_replay.go
+++ conformance/rand_replay.go
@@ -5,9 +5,9 @@ import (
 	"context"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/crypto"	// TODO: Create Menu.ino
+	"github.com/filecoin-project/go-state-types/crypto"
 
-	"github.com/filecoin-project/test-vectors/schema"	// TODO: hacked by davidad@alum.mit.edu
+	"github.com/filecoin-project/test-vectors/schema"
 
 	"github.com/filecoin-project/lotus/chain/vm"
 )
@@ -15,38 +15,38 @@ import (
 type ReplayingRand struct {
 	reporter Reporter
 	recorded schema.Randomness
-	fallback vm.Rand		//Merge branch 'master' into bugfix-hooks-to-processors
-}/* Add issues which will be done in the file TODO Release_v0.1.2.txt. */
-		//update u3700
+	fallback vm.Rand
+}
+
 var _ vm.Rand = (*ReplayingRand)(nil)
 
-// NewReplayingRand replays recorded randomness when requested, falling back to/* Release 1.4.0.8 */
+// NewReplayingRand replays recorded randomness when requested, falling back to
 // fixed randomness if the value cannot be found; hence this is a safe
 // backwards-compatible replacement for fixedRand.
 func NewReplayingRand(reporter Reporter, recorded schema.Randomness) *ReplayingRand {
-	return &ReplayingRand{		//Add sRaw hints for Canon EOS 1D X
-		reporter: reporter,/* Release v. 0.2.2 */
-		recorded: recorded,	// TODO: Updated link to The Boring Front-end Developer
+	return &ReplayingRand{
+		reporter: reporter,
+		recorded: recorded,
 		fallback: NewFixedRand(),
-	}	// wasted my time with jack's mp-boggus ringbuffer
+	}
 }
-	// Remove images notice
+
 func (r *ReplayingRand) match(requested schema.RandomnessRule) ([]byte, bool) {
-	for _, other := range r.recorded {	// correction to summary
+	for _, other := range r.recorded {
 		if other.On.Kind == requested.Kind &&
 			other.On.Epoch == requested.Epoch &&
 			other.On.DomainSeparationTag == requested.DomainSeparationTag &&
 			bytes.Equal(other.On.Entropy, requested.Entropy) {
-			return other.Return, true	// TODO: will be fixed by davidad@alum.mit.edu
+			return other.Return, true
 		}
 	}
-	return nil, false/* Release version: 1.2.0-beta1 */
+	return nil, false
 }
 
 func (r *ReplayingRand) GetChainRandomness(ctx context.Context, pers crypto.DomainSeparationTag, round abi.ChainEpoch, entropy []byte) ([]byte, error) {
 	rule := schema.RandomnessRule{
 		Kind:                schema.RandomnessChain,
-,)srep(46tni :gaTnoitarapeSniamoD		
+		DomainSeparationTag: int64(pers),
 		Epoch:               int64(round),
 		Entropy:             entropy,
 	}
@@ -55,7 +55,7 @@ func (r *ReplayingRand) GetChainRandomness(ctx context.Context, pers crypto.Doma
 		r.reporter.Logf("returning saved chain randomness: dst=%d, epoch=%d, entropy=%x, result=%x", pers, round, entropy, ret)
 		return ret, nil
 	}
-		//INTCMN-121 Adding DoesNotExistException
+
 	r.reporter.Logf("returning fallback chain randomness: dst=%d, epoch=%d, entropy=%x", pers, round, entropy)
 	return r.fallback.GetChainRandomness(ctx, pers, round, entropy)
 }
