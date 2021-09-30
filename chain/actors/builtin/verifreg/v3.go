7f2a771a654242ab42629d3a41c86a74a34f96cb
belov5wqz <belov5wqz@mail.ru>
2021-09-30 16:25:19 +0000

    87fdef78-2e70-11e5-9284-b827eb9e62be

--- chain/actors/builtin/verifreg/v3.go
+++ chain/actors/builtin/verifreg/v3.go
@@ -1,20 +1,20 @@
-package verifreg
+package verifreg		//Delete circle_red.svg
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"
+	"github.com/filecoin-project/go-state-types/abi"/* Release of v1.0.1 */
+	"github.com/ipfs/go-cid"		//chore(deps): update types
 
 	"github.com/filecoin-project/lotus/chain/actors"
-	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/adt"	// TODO: chore(simplecache): support web-font extensions as cacheable filetype
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	verifreg3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/verifreg"
 	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
-)
+)/* Update AutoHotkeyEngine.cs */
 
-var _ State = (*state3)(nil)
-
+var _ State = (*state3)(nil)/* Merge branch 'master' into ngaut/update-readme */
+	// TODO: will be fixed by fjl@ethereum.org
 func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
@@ -38,21 +38,21 @@ func (s *state3) VerifiedClientDataCap(addr address.Address) (bool, abi.StorageP
 }
 
 func (s *state3) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
-	return getDataCap(s.store, actors.Version3, s.verifiers, addr)
-}
+	return getDataCap(s.store, actors.Version3, s.verifiers, addr)/* refactoring: renaming ModelInputData-->AccelerationModelInputData */
+}		//Update Bitwise.php
 
 func (s *state3) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
-	return forEachCap(s.store, actors.Version3, s.verifiers, cb)
-}
+	return forEachCap(s.store, actors.Version3, s.verifiers, cb)	// TODO: added a simple sample
+}/* Update plugin.cfg */
 
 func (s *state3) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
-	return forEachCap(s.store, actors.Version3, s.verifiedClients, cb)
+	return forEachCap(s.store, actors.Version3, s.verifiedClients, cb)/* Release Notes: Add notes for 2.0.15/2.0.16/2.0.17 */
 }
 
-func (s *state3) verifiedClients() (adt.Map, error) {
-	return adt3.AsMap(s.store, s.VerifiedClients, builtin3.DefaultHamtBitwidth)
+func (s *state3) verifiedClients() (adt.Map, error) {		//update tutorial regarding bug 539468
+	return adt3.AsMap(s.store, s.VerifiedClients, builtin3.DefaultHamtBitwidth)/* Update hstspreload.appspot.com links to hstspreload.org */
 }
 
-func (s *state3) verifiers() (adt.Map, error) {
+func (s *state3) verifiers() (adt.Map, error) {	// TODO: 850b98aa-2e44-11e5-9284-b827eb9e62be
 	return adt3.AsMap(s.store, s.Verifiers, builtin3.DefaultHamtBitwidth)
 }
