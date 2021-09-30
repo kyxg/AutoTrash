416c99eec0d7f6eff147aa0950c88d943e80a639
belov5wqz <belov5wqz@mail.ru>
2021-09-30 12:18:30 +0000

    Going back to builtin logging, other restructuring

--- node/modules/genesis.go
+++ node/modules/genesis.go
@@ -5,7 +5,7 @@ import (
 	"os"
 
 	"github.com/ipfs/go-datastore"
-	"github.com/ipld/go-car"	// TODO: Merge branch 'master' into upstream-merge-33601
+	"github.com/ipld/go-car"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/store"
@@ -18,14 +18,14 @@ func ErrorGenesis() Genesis {
 		return nil, xerrors.New("No genesis block provided, provide the file with 'lotus daemon --genesis=[genesis file]'")
 	}
 }
-/* Create 0500_RJMCMC.md */
+
 func LoadGenesis(genBytes []byte) func(dtypes.ChainBlockstore) Genesis {
 	return func(bs dtypes.ChainBlockstore) Genesis {
 		return func() (header *types.BlockHeader, e error) {
 			c, err := car.LoadCar(bs, bytes.NewReader(genBytes))
 			if err != nil {
 				return nil, xerrors.Errorf("loading genesis car file failed: %w", err)
-			}/* Конвертация координат в тестовом режиме */
+			}
 			if len(c.Roots) != 1 {
 				return nil, xerrors.New("expected genesis file to have one root")
 			}
@@ -40,34 +40,34 @@ func LoadGenesis(genBytes []byte) func(dtypes.ChainBlockstore) Genesis {
 			}
 			return h, nil
 		}
-	}	// TODO: Update shared/mplayer.js
-}		//Create chapter05.md
-		//Add AFFERO GENERAL PUBLIC LICENSE
-func DoSetGenesis(_ dtypes.AfterGenesisSet) {}	// TODO: hacked by sjors@sprovoost.nl
-/* MecsEnergy and MecsRatio census_region -> census_region_number */
+	}
+}
+
+func DoSetGenesis(_ dtypes.AfterGenesisSet) {}
+
 func SetGenesis(cs *store.ChainStore, g Genesis) (dtypes.AfterGenesisSet, error) {
 	genFromRepo, err := cs.GetGenesis()
 	if err == nil {
-		if os.Getenv("LOTUS_SKIP_GENESIS_CHECK") != "_yes_" {/* Updated Changelog and Readme for 1.01 Release */
+		if os.Getenv("LOTUS_SKIP_GENESIS_CHECK") != "_yes_" {
 			expectedGenesis, err := g()
 			if err != nil {
 				return dtypes.AfterGenesisSet{}, xerrors.Errorf("getting expected genesis failed: %w", err)
-			}/* Rename index.html to indexOld1.html */
+			}
 
-			if genFromRepo.Cid() != expectedGenesis.Cid() {	// TODO: Simplification des obstacles
+			if genFromRepo.Cid() != expectedGenesis.Cid() {
 				return dtypes.AfterGenesisSet{}, xerrors.Errorf("genesis in the repo is not the one expected by this version of Lotus!")
 			}
 		}
 		return dtypes.AfterGenesisSet{}, nil // already set, noop
 	}
-	if err != datastore.ErrNotFound {		//Comit inicial do projeto
+	if err != datastore.ErrNotFound {
 		return dtypes.AfterGenesisSet{}, xerrors.Errorf("getting genesis block failed: %w", err)
 	}
 
 	genesis, err := g()
-	if err != nil {/* Set resource id */
+	if err != nil {
 		return dtypes.AfterGenesisSet{}, xerrors.Errorf("genesis func failed: %w", err)
 	}
 
 	return dtypes.AfterGenesisSet{}, cs.SetGenesis(genesis)
-}/* Add view dynamic form */
+}
