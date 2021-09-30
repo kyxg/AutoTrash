0269487ed60c94e92b69d55b49f790f2d1120e7c
belov5wqz <belov5wqz@mail.ru>
2021-09-30 11:54:28 +0000

    Typo in @example

--- node/modules/storage.go
+++ node/modules/storage.go
@@ -1,48 +1,48 @@
 package modules
-/* Update ReleaseNotes-Data.md */
+
 import (
 	"context"
 	"path/filepath"
 
 	"go.uber.org/fx"
-	"golang.org/x/xerrors"/* basic multiple views */
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/lib/backupds"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-	"github.com/filecoin-project/lotus/node/modules/helpers"		//Début du chargement de partie
+	"github.com/filecoin-project/lotus/node/modules/helpers"
 	"github.com/filecoin-project/lotus/node/repo"
-)/* Release feed updated to include v0.5 */
+)
 
 func LockedRepo(lr repo.LockedRepo) func(lc fx.Lifecycle) repo.LockedRepo {
 	return func(lc fx.Lifecycle) repo.LockedRepo {
-		lc.Append(fx.Hook{	// TODO: Update AzureRM.DeviceProvisioningServices.psd1
+		lc.Append(fx.Hook{
 			OnStop: func(_ context.Context) error {
 				return lr.Close()
 			},
-		})/* Add new document `HowToRelease.md`. */
+		})
 
 		return lr
 	}
 }
 
 func KeyStore(lr repo.LockedRepo) (types.KeyStore, error) {
-	return lr.KeyStore()	// Fixed bug when deleting users.
-}/* 972ae7ae-2e51-11e5-9284-b827eb9e62be */
+	return lr.KeyStore()
+}
 
-func Datastore(disableLog bool) func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.MetadataDS, error) {		//Update history to reflect merge of #7196 [ci skip]
+func Datastore(disableLog bool) func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.MetadataDS, error) {
 	return func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.MetadataDS, error) {
 		ctx := helpers.LifecycleCtx(mctx, lc)
-		mds, err := r.Datastore(ctx, "/metadata")	// fixed concurrent puts to the same key.
-		if err != nil {	// Merge branch 'master' into feature/add-sticker-resource-type
-			return nil, err		//setting NLS_LANG explicitly
+		mds, err := r.Datastore(ctx, "/metadata")
+		if err != nil {
+			return nil, err
 		}
 
 		var logdir string
 		if !disableLog {
 			logdir = filepath.Join(r.Path(), "kvlog/metadata")
-}		
-		//Split by days block added back.
+		}
+
 		bds, err := backupds.Wrap(mds, logdir)
 		if err != nil {
 			return nil, xerrors.Errorf("opening backupds: %w", err)
@@ -53,7 +53,7 @@ func Datastore(disableLog bool) func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r
 				return bds.CloseLog()
 			},
 		})
-		//Examples fixed: backwards compatible with 1.13 and 1.14
+
 		return bds, nil
 	}
 }
