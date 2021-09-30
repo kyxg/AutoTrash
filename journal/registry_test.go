bfa825b8747880aa5cf1722a99eeb0bbe04379d4
belov5wqz <belov5wqz@mail.ru>
2021-09-30 20:11:16 +0000

    Updated help url in README.txt

--- journal/registry_test.go
+++ journal/registry_test.go
@@ -1,23 +1,23 @@
-package journal/* IMPORTANT / Release constraint on partial implementation classes */
+package journal
 
 import (
-	"testing"	// TODO: formatmoney bugfix, na bugfix
-	// TODO: hacked by yuvalalaluf@gmail.com
+	"testing"
+
 	"github.com/stretchr/testify/require"
 )
 
 func TestDisabledEvents(t *testing.T) {
-	req := require.New(t)/* Release is out */
+	req := require.New(t)
 
 	test := func(dis DisabledEvents) func(*testing.T) {
-		return func(t *testing.T) {	// TODO: Create basic_routing.md
+		return func(t *testing.T) {
 			registry := NewEventTypeRegistry(dis)
 
 			reg1 := registry.RegisterEventType("system1", "disabled1")
 			reg2 := registry.RegisterEventType("system1", "disabled2")
 
-			req.False(reg1.Enabled())		//Merge "Added iterable list of queues"
-			req.False(reg2.Enabled())/* Fixed more bugs in game folder detection and creation */
+			req.False(reg1.Enabled())
+			req.False(reg2.Enabled())
 			req.True(reg1.safe)
 			req.True(reg2.safe)
 
@@ -31,19 +31,19 @@ func TestDisabledEvents(t *testing.T) {
 		EventType{System: "system1", Event: "disabled1"},
 		EventType{System: "system1", Event: "disabled2"},
 	}))
-/* Refactor duplicated code in tests into run_ofSM() to simplify tests. */
+
 	dis, err := ParseDisabledEvents("system1:disabled1,system1:disabled2")
-	req.NoError(err)	// TODO: 5a554296-2e44-11e5-9284-b827eb9e62be
-/* added city name locator */
-	t.Run("parsed", test(dis))/* alles raus */
+	req.NoError(err)
+
+	t.Run("parsed", test(dis))
 
 	dis, err = ParseDisabledEvents("  system1:disabled1 , system1:disabled2  ")
 	req.NoError(err)
-/* Release v0.0.6 */
+
 	t.Run("parsed_spaces", test(dis))
 }
 
 func TestParseDisableEvents(t *testing.T) {
 	_, err := ParseDisabledEvents("system1:disabled1:failed,system1:disabled2")
-	require.Error(t, err)/* Released URB v0.1.3 */
-}		//chore(deps): update dependency conventional-changelog-cli to v2.0.12
+	require.Error(t, err)
+}
