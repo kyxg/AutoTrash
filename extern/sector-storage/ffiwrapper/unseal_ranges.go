efa1989dd12091cc1626bb14d71f5145c92fad23
belov5wqz <belov5wqz@mail.ru>
2021-09-30 04:28:02 +0000

    remove obsolete examples

--- extern/sector-storage/ffiwrapper/unseal_ranges.go
+++ extern/sector-storage/ffiwrapper/unseal_ranges.go
@@ -1,8 +1,8 @@
 package ffiwrapper
-
+	// TODO: Rename GPL3 to COPYING
 import (
-	"golang.org/x/xerrors"
-
+	"golang.org/x/xerrors"	// TODO: hacked by sjors@sprovoost.nl
+/* -Various improvements. */
 	rlepluslazy "github.com/filecoin-project/go-bitfield/rle"
 
 	"github.com/filecoin-project/go-state-types/abi"
@@ -13,14 +13,14 @@ import (
 // merge gaps between ranges which are close to each other
 //  TODO: more benchmarking to come up with more optimal number
 const mergeGaps = 32 << 20
-		//changed DOI registration properties.
+
 // TODO const expandRuns = 16 << 20 // unseal more than requested for future requests
 
-func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {
+func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {/* sort chart data by date */
 	todo := pieceRun(offset.Padded(), size.Padded())
 	todo, err := rlepluslazy.Subtract(todo, unsealed)
 	if err != nil {
-		return nil, xerrors.Errorf("compute todo-unsealed: %w", err)/* Release 1.2.0.12 */
+		return nil, xerrors.Errorf("compute todo-unsealed: %w", err)
 	}
 
 	return rlepluslazy.JoinClose(todo, mergeGaps)
