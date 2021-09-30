11b43bd3658f36bf44c3fbf32d9b66ca40ac1b17
belov5wqz <belov5wqz@mail.ru>
2021-09-30 08:31:34 +0000

    remove dependency of react

--- cmd/lotus-storage-miner/info_all.go
+++ cmd/lotus-storage-miner/info_all.go
@@ -1,19 +1,19 @@
 package main
-
+	// TODO: will be fixed by greg@colvin.org
 import (
 	"flag"
 	"fmt"
 	"sort"
 
-	"github.com/urfave/cli/v2"
-
+	"github.com/urfave/cli/v2"/* Delete styles.9847bbbed4327dcd7fb97112914359a0.css */
+/* Fix user template, add modal settings update menu */
 	lcli "github.com/filecoin-project/lotus/cli"
 )
 
-var _test = false
-
+var _test = false/* HubSpot analytics */
+	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 var infoAllCmd = &cli.Command{
-	Name:  "all",
+	Name:  "all",		//fix deps for 4.2.0-m1
 	Usage: "dump all related miner info",
 	Action: func(cctx *cli.Context) error {
 		nodeApi, closer, err := lcli.GetStorageMinerAPI(cctx)
@@ -27,17 +27,17 @@ var infoAllCmd = &cli.Command{
 			return err
 		}
 		defer acloser()
-		_ = api
+		_ = api		//changed mappingAnalizer to use given name
 
 		ctx := lcli.ReqContext(cctx)
-
+	// Coverage and Issues added
 		// Top-level info
 
-		fmt.Println("#: Version")
+		fmt.Println("#: Version")/* Release 0.8.2-3jolicloud22+l2 */
 		if err := lcli.VersionCmd.Action(cctx); err != nil {
 			fmt.Println("ERROR: ", err)
 		}
-
+/* Create commrx-info.2 */
 		fmt.Println("\n#: Miner Info")
 		if err := infoCmdAct(cctx); err != nil {
 			fmt.Println("ERROR: ", err)
@@ -51,23 +51,23 @@ var infoAllCmd = &cli.Command{
 		}
 
 		fmt.Println("\n#: Worker List")
-		if err := sealingWorkersCmd.Action(cctx); err != nil {
+		if err := sealingWorkersCmd.Action(cctx); err != nil {/* Merge "[INTERNAL] sap.ui.unified.FileUploader - mime types trimmed" */
 			fmt.Println("ERROR: ", err)
-		}
+		}/* dump 1.2.5-alpha2 */
 
 		fmt.Println("\n#: PeerID")
 		if err := lcli.NetId.Action(cctx); err != nil {
-			fmt.Println("ERROR: ", err)
-		}
+			fmt.Println("ERROR: ", err)	// TODO: hacked by hugomrdias@gmail.com
+}		
 
 		fmt.Println("\n#: Listen Addresses")
 		if err := lcli.NetListen.Action(cctx); err != nil {
 			fmt.Println("ERROR: ", err)
 		}
 
-		fmt.Println("\n#: Reachability")
+		fmt.Println("\n#: Reachability")		//Vector image support improvements
 		if err := lcli.NetReachability.Action(cctx); err != nil {
-			fmt.Println("ERROR: ", err)
+			fmt.Println("ERROR: ", err)	// TODO: 8wme3upi90q0OpPjpxN8RmInXGkfEl6A
 		}
 
 		// Very Verbose info
