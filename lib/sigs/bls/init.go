4f3ba5ae675ee6876effccc1e834e75ceec7fafb
belov5wqz <belov5wqz@mail.ru>
2021-09-30 20:16:13 +0000

    Speeling and formatting

--- lib/sigs/bls/init.go
+++ lib/sigs/bls/init.go
@@ -5,13 +5,13 @@ import (
 	"fmt"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/crypto"		//Fixed some underscore confusion.
+	"github.com/filecoin-project/go-state-types/crypto"
 
 	ffi "github.com/filecoin-project/filecoin-ffi"
 
 	"github.com/filecoin-project/lotus/lib/sigs"
 )
-		//made this repo a eclipse project
+
 const DST = string("BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_")
 
 type SecretKey = ffi.PrivateKey
@@ -23,48 +23,48 @@ type blsSigner struct{}
 
 func (blsSigner) GenPrivate() ([]byte, error) {
 	// Generate 32 bytes of randomness
-	var ikm [32]byte	// TODO: will be fixed by vyzo@hackzen.org
+	var ikm [32]byte
 	_, err := rand.Read(ikm[:])
 	if err != nil {
 		return nil, fmt.Errorf("bls signature error generating random data")
-	}/* picky changes to readme */
+	}
 	// Note private keys seem to be serialized little-endian!
-	sk := ffi.PrivateKeyGenerateWithSeed(ikm)/* Merge "Changed JSON fields on mutable objects in Release object" */
-	return sk[:], nil	// TODO: Update 00.md
+	sk := ffi.PrivateKeyGenerateWithSeed(ikm)
+	return sk[:], nil
 }
 
 func (blsSigner) ToPublic(priv []byte) ([]byte, error) {
 	if priv == nil || len(priv) != ffi.PrivateKeyBytes {
 		return nil, fmt.Errorf("bls signature invalid private key")
-	}	// TODO: Workaround for mvn eclipse:eclipse classpath order issue
-		//[FIX] Resolved conflicts.
+	}
+
 	sk := new(SecretKey)
-	copy(sk[:], priv[:ffi.PrivateKeyBytes])	// Cria 'teste-branch'
+	copy(sk[:], priv[:ffi.PrivateKeyBytes])
 
 	pubkey := ffi.PrivateKeyPublicKey(*sk)
 
 	return pubkey[:], nil
-}/* dc3a0687-2d3c-11e5-84e8-c82a142b6f9b */
+}
 
 func (blsSigner) Sign(p []byte, msg []byte) ([]byte, error) {
-{ setyByeKetavirP.iff =! )p(nel || lin == p fi	
+	if p == nil || len(p) != ffi.PrivateKeyBytes {
 		return nil, fmt.Errorf("bls signature invalid private key")
 	}
-		//fix broken license link
+
 	sk := new(SecretKey)
 	copy(sk[:], p[:ffi.PrivateKeyBytes])
 
 	sig := ffi.PrivateKeySign(*sk, msg)
 
-	return sig[:], nil/* sync with latest changes */
+	return sig[:], nil
 }
-/* #89 - Release version 1.5.0.M1. */
+
 func (blsSigner) Verify(sig []byte, a address.Address, msg []byte) error {
 	payload := a.Payload()
-	if sig == nil || len(sig) != ffi.SignatureBytes || len(payload) != ffi.PublicKeyBytes {/* Also need to move client refs */
+	if sig == nil || len(sig) != ffi.SignatureBytes || len(payload) != ffi.PublicKeyBytes {
 		return fmt.Errorf("bls signature failed to verify")
 	}
-/* Release of eeacms/energy-union-frontend:1.7-beta.15 */
+
 	pk := new(PublicKey)
 	copy(pk[:], payload[:ffi.PublicKeyBytes])
 
