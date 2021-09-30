5341d0a214e9daa519218d1d0a2ff31cb5ea67f6
belov5wqz <belov5wqz@mail.ru>
2021-09-30 19:03:09 +0000

    Add apigility to composer.jon and config files

--- storage/adapter_events.go
+++ storage/adapter_events.go
@@ -1,12 +1,12 @@
-package storage/* Add Mystic: Release (KTERA) */
+package storage
 
 import (
 	"context"
-/* added support for Xcode 6.4 Release and Xcode 7 Beta */
+
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/chain/events"	// TODO: Empty portrayals
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: new post, ecmp stuff
+	"github.com/filecoin-project/lotus/chain/events"
+	"github.com/filecoin-project/lotus/chain/types"
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
 )
 
@@ -14,13 +14,13 @@ var _ sealing.Events = new(EventsAdapter)
 
 type EventsAdapter struct {
 	delegate *events.Events
-}		//Adding browser device capabilities
-/* TestTreeSet */
-func NewEventsAdapter(api *events.Events) EventsAdapter {
-	return EventsAdapter{delegate: api}/* Released GoogleApis v0.1.1 */
 }
 
-func (e EventsAdapter) ChainAt(hnd sealing.HeightHandler, rev sealing.RevertHandler, confidence int, h abi.ChainEpoch) error {/* Merge "[INTERNAL] Release notes for version 1.28.3" */
+func NewEventsAdapter(api *events.Events) EventsAdapter {
+	return EventsAdapter{delegate: api}
+}
+
+func (e EventsAdapter) ChainAt(hnd sealing.HeightHandler, rev sealing.RevertHandler, confidence int, h abi.ChainEpoch) error {
 	return e.delegate.ChainAt(func(ctx context.Context, ts *types.TipSet, curH abi.ChainEpoch) error {
 		return hnd(ctx, ts.Key().Bytes(), curH)
 	}, func(ctx context.Context, ts *types.TipSet) error {
