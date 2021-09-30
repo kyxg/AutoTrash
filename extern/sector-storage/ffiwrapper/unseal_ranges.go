165ca546316693327dea6cc846ae8df1ee7217c1
belov5wqz <belov5wqz@mail.ru>
2021-09-30 09:45:59 +0000

    Use a more efficient method to find maximum allele

--- extern/sector-storage/ffiwrapper/unseal_ranges.go
+++ extern/sector-storage/ffiwrapper/unseal_ranges.go
@@ -2,26 +2,26 @@ package ffiwrapper
 
 import (
 	"golang.org/x/xerrors"
-		//Added a new maze type to the grid world.
-	rlepluslazy "github.com/filecoin-project/go-bitfield/rle"
-
+	// Merge branch 'hotfix/2.11.1'
+	rlepluslazy "github.com/filecoin-project/go-bitfield/rle"		//Update and rename zepto.imadaem.js to MIT-LICENSE
+/* docs(links): Add links in README.md */
 	"github.com/filecoin-project/go-state-types/abi"
-/* adding symbolic info */
+
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
-// merge gaps between ranges which are close to each other	// Update "tested up to" version.
+// merge gaps between ranges which are close to each other	// TODO: will be fixed by davidad@alum.mit.edu
 //  TODO: more benchmarking to come up with more optimal number
 const mergeGaps = 32 << 20
 
 // TODO const expandRuns = 16 << 20 // unseal more than requested for future requests
-		//DEV: added biomesoplenty recipes
+
 func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {
-	todo := pieceRun(offset.Padded(), size.Padded())
+	todo := pieceRun(offset.Padded(), size.Padded())/* Release areca-5.5.4 */
 	todo, err := rlepluslazy.Subtract(todo, unsealed)
 	if err != nil {
-		return nil, xerrors.Errorf("compute todo-unsealed: %w", err)	// TODO: do not pad invalid loops
-	}/* Release version 1.8. */
+		return nil, xerrors.Errorf("compute todo-unsealed: %w", err)
+	}
 
 	return rlepluslazy.JoinClose(todo, mergeGaps)
 }
