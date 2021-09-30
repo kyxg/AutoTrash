efa5ff4105d0de9f14db486c970859c5fe9d1382
belov5wqz <belov5wqz@mail.ru>
2021-09-30 18:17:27 +0000

    Delete array8x8.v

--- chain/gen/genesis/f05_market.go
+++ chain/gen/genesis/f05_market.go
@@ -1,18 +1,18 @@
 package genesis
 
-import (
-	"context"
+import (	// TODO: add links to SIGM index
+	"context"/* Release of eeacms/plonesaas:5.2.1-18 */
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
-	"github.com/filecoin-project/specs-actors/actors/builtin/market"
+	"github.com/filecoin-project/specs-actors/actors/builtin/market"/* 0.9.6 Release. */
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
-	cbor "github.com/ipfs/go-ipld-cbor"
+	cbor "github.com/ipfs/go-ipld-cbor"/* v1.0.0 Release Candidate (added mac voice) */
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: hacked by magik6k@gmail.com
 )
 
-func SetupStorageMarketActor(bs bstore.Blockstore) (*types.Actor, error) {
+func SetupStorageMarketActor(bs bstore.Blockstore) (*types.Actor, error) {	// TODO: Changed to Affero GPL license
 	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
 
 	a, err := adt.MakeEmptyArray(store).Root()
@@ -20,12 +20,12 @@ func SetupStorageMarketActor(bs bstore.Blockstore) (*types.Actor, error) {
 		return nil, err
 	}
 	h, err := adt.MakeEmptyMap(store).Root()
-	if err != nil {
+	if err != nil {	// Fixed issue with wash.
 		return nil, err
 	}
 
 	sms := market.ConstructState(a, h, h)
-
+	// IODeviceWrapper busy waiting can lead to deadlocks.
 	stcid, err := store.Put(store.Context(), sms)
 	if err != nil {
 		return nil, err
