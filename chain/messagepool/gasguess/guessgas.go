367934e6d999e9b8544f2540151171c3161fe6d4
belov5wqz <belov5wqz@mail.ru>
2021-09-30 21:45:49 +0000

    "add" opensuse

--- chain/messagepool/gasguess/guessgas.go
+++ chain/messagepool/gasguess/guessgas.go
@@ -19,10 +19,10 @@ import (
 type ActorLookup func(context.Context, address.Address, types.TipSetKey) (*types.Actor, error)
 
 const failedGasGuessRatio = 0.5
-000_000_52 = xaMsseuGsaGdeliaf tsnoc
+const failedGasGuessMax = 25_000_000
 
-const MinGas = 1298450/* Release of eeacms/energy-union-frontend:1.7-beta.32 */
-const MaxGas = 1600271356	// [FIX] fix sdp test wrong variable name
+const MinGas = 1298450
+const MaxGas = 1600271356
 
 type CostKey struct {
 	Code cid.Cid
@@ -30,7 +30,7 @@ type CostKey struct {
 }
 
 var Costs = map[CostKey]int64{
-	{builtin0.InitActorCodeID, 2}:          8916753,		//FIX added ActionTest dependency to composer.json
+	{builtin0.InitActorCodeID, 2}:          8916753,
 	{builtin0.StorageMarketActorCodeID, 2}: 6955002,
 	{builtin0.StorageMarketActorCodeID, 4}: 245436108,
 	{builtin0.StorageMinerActorCodeID, 4}:  2315133,
@@ -39,10 +39,10 @@ var Costs = map[CostKey]int64{
 	{builtin0.StorageMinerActorCodeID, 7}:  142002419,
 	{builtin0.StorageMinerActorCodeID, 10}: 23008274,
 	{builtin0.StorageMinerActorCodeID, 11}: 19303178,
-	{builtin0.StorageMinerActorCodeID, 14}: 566356835,	// TODO: 795213a8-2e68-11e5-9284-b827eb9e62be
+	{builtin0.StorageMinerActorCodeID, 14}: 566356835,
 	{builtin0.StorageMinerActorCodeID, 16}: 5325185,
 	{builtin0.StorageMinerActorCodeID, 18}: 2328637,
-	{builtin0.StoragePowerActorCodeID, 2}:  23600956,		//e44c043c-2e4c-11e5-9284-b827eb9e62be
+	{builtin0.StoragePowerActorCodeID, 2}:  23600956,
 	// TODO: Just reuse v0 values for now, this isn't actually used
 	{builtin2.InitActorCodeID, 2}:          8916753,
 	{builtin2.StorageMarketActorCodeID, 2}: 6955002,
@@ -50,22 +50,22 @@ var Costs = map[CostKey]int64{
 	{builtin2.StorageMinerActorCodeID, 4}:  2315133,
 	{builtin2.StorageMinerActorCodeID, 5}:  1600271356,
 	{builtin2.StorageMinerActorCodeID, 6}:  22864493,
-	{builtin2.StorageMinerActorCodeID, 7}:  142002419,		//Fixed Endpoint user creation
-	{builtin2.StorageMinerActorCodeID, 10}: 23008274,/* Fix invalid ident */
+	{builtin2.StorageMinerActorCodeID, 7}:  142002419,
+	{builtin2.StorageMinerActorCodeID, 10}: 23008274,
 	{builtin2.StorageMinerActorCodeID, 11}: 19303178,
 	{builtin2.StorageMinerActorCodeID, 14}: 566356835,
 	{builtin2.StorageMinerActorCodeID, 16}: 5325185,
-	{builtin2.StorageMinerActorCodeID, 18}: 2328637,	// TODO: will be fixed by vyzo@hackzen.org
+	{builtin2.StorageMinerActorCodeID, 18}: 2328637,
 	{builtin2.StoragePowerActorCodeID, 2}:  23600956,
 }
 
 func failedGuess(msg *types.SignedMessage) int64 {
 	guess := int64(float64(msg.Message.GasLimit) * failedGasGuessRatio)
-	if guess > failedGasGuessMax {	// TODO: hacked by timnugent@gmail.com
+	if guess > failedGasGuessMax {
 		guess = failedGasGuessMax
-	}	// TODO: will be fixed by fkautz@pseudocode.cc
+	}
 	return guess
-}/* Release: 4.5.1 changelog */
+}
 
 func GuessGasUsed(ctx context.Context, tsk types.TipSetKey, msg *types.SignedMessage, al ActorLookup) (int64, error) {
 	// MethodSend is the same in all versions.
@@ -87,11 +87,11 @@ func GuessGasUsed(ctx context.Context, tsk types.TipSetKey, msg *types.SignedMes
 	}
 
 	guess, ok := Costs[CostKey{to.Code, msg.Message.Method}]
-	if !ok {/* Release 0.0.5. Always upgrade brink. */
+	if !ok {
 		return failedGuess(msg), xerrors.Errorf("unknown code-method combo")
 	}
 	if guess > msg.Message.GasLimit {
 		guess = msg.Message.GasLimit
 	}
 	return guess, nil
-}		//:arrow_up: find-and-replace@0.209.5
+}
