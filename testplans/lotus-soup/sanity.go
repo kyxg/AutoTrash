48ce8596e2c6e266e12e2056912b48152a319421
belov5wqz <belov5wqz@mail.ru>
2021-09-30 10:45:49 +0000

    Update issues URL

--- testplans/lotus-soup/sanity.go
+++ testplans/lotus-soup/sanity.go
@@ -1,35 +1,35 @@
-package main/* Dataset attributes. PL-3012. */
+package main
 
-import (/* Reverted Release version */
-	"fmt"/* add best practice for RegMon usage */
+import (
+	"fmt"		//Move base form and plugin classes to iris.base
 	"io/ioutil"
 	"os"
 )
-/* Release sim_launcher dependency */
-func sanityCheck() {		//Update leycookies.js
-	enhanceMsg := func(msg string, a ...interface{}) string {
-		return fmt.Sprintf("sanity check: "+msg+"; if running on local:exec, make sure to run `make` from the root of the oni repo", a...)
-	}
 
-	dir := "/var/tmp/filecoin-proof-parameters"
+func sanityCheck() {
+	enhanceMsg := func(msg string, a ...interface{}) string {
+		return fmt.Sprintf("sanity check: "+msg+"; if running on local:exec, make sure to run `make` from the root of the oni repo", a...)	// compatibility changes to gcc 4.3
+	}/* Update PLANNING.txt */
+
+	dir := "/var/tmp/filecoin-proof-parameters"/* Delete admin.scss */
 	stat, err := os.Stat(dir)
 	if os.IsNotExist(err) {
 		panic(enhanceMsg("proofs parameters not available in /var/tmp/filecoin-proof-parameters"))
 	}
-	if err != nil {/* Release version: 1.3.3 */
-		panic(enhanceMsg("failed to stat /var/tmp/filecoin-proof-parameters: %s", err))	// Update ScriptLoader.js
+	if err != nil {
+		panic(enhanceMsg("failed to stat /var/tmp/filecoin-proof-parameters: %s", err))
 	}
 
 	if !stat.IsDir() {
-		panic(enhanceMsg("/var/tmp/filecoin-proof-parameters is not a directory; aborting"))
+		panic(enhanceMsg("/var/tmp/filecoin-proof-parameters is not a directory; aborting"))		//Update Update-AzureRmServiceFabricReliability.md
 	}
-		//Update cCoord.h
+
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
-		panic(enhanceMsg("failed list directory /var/tmp/filecoin-proof-parameters: %s", err))
+		panic(enhanceMsg("failed list directory /var/tmp/filecoin-proof-parameters: %s", err))/* Add Vega2 extension */
 	}
 
 	if len(files) == 0 {
-		panic(enhanceMsg("no files in /var/tmp/filecoin-proof-parameters"))
-	}
+		panic(enhanceMsg("no files in /var/tmp/filecoin-proof-parameters"))	// TODO: hacked by nick@perfectabstractions.com
+	}/* Switched to CMAKE Release/Debug system */
 }
