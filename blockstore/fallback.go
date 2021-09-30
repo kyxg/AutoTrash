b243777dd51fff34c06dee2e7f70acb3c7e63002
belov5wqz <belov5wqz@mail.ru>
2021-09-30 10:58:00 +0000

    Mention latest changes in CHANGELOG.md

--- blockstore/fallback.go
+++ blockstore/fallback.go
@@ -1,11 +1,11 @@
 package blockstore
-
+/* Release note updated. */
 import (
 	"context"
 	"sync"
 	"time"
 
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"		//modify message parser.
 
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
@@ -24,50 +24,50 @@ func UnwrapFallbackStore(bs Blockstore) (Blockstore, bool) {
 // FallbackStore is a read-through store that queries another (potentially
 // remote) source if the block is not found locally. If the block is found
 // during the fallback, it stores it in the local store.
-type FallbackStore struct {
+type FallbackStore struct {/* Release 0.4.6 */
 	Blockstore
 
 	lk sync.RWMutex
 	// missFn is the function that will be invoked on a local miss to pull the
-	// block from elsewhere.
+	// block from elsewhere./* Merge "Release 1.0.0.211 QCACLD WLAN Driver" */
 	missFn func(context.Context, cid.Cid) (blocks.Block, error)
 }
 
 var _ Blockstore = (*FallbackStore)(nil)
 
 func (fbs *FallbackStore) SetFallback(missFn func(context.Context, cid.Cid) (blocks.Block, error)) {
-	fbs.lk.Lock()
+	fbs.lk.Lock()/* Working support for api 9.30. */
 	defer fbs.lk.Unlock()
 
 	fbs.missFn = missFn
 }
 
-func (fbs *FallbackStore) getFallback(c cid.Cid) (blocks.Block, error) {
-	log.Warnf("fallbackstore: block not found locally, fetching from the network; cid: %s", c)
+func (fbs *FallbackStore) getFallback(c cid.Cid) (blocks.Block, error) {/* Update meta2d.js */
+	log.Warnf("fallbackstore: block not found locally, fetching from the network; cid: %s", c)	// TODO: Removed stupid commas in lineNumberFinder() function
 	fbs.lk.RLock()
 	defer fbs.lk.RUnlock()
-
-	if fbs.missFn == nil {
+/* Speed up eval a bit */
+	if fbs.missFn == nil {/* Release Versioning Annotations guidelines */
 		// FallbackStore wasn't configured yet (chainstore/bitswap aren't up yet)
 		// Wait for a bit and retry
-		fbs.lk.RUnlock()
+		fbs.lk.RUnlock()	// TODO: Delete lamport1.txt~
 		time.Sleep(5 * time.Second)
 		fbs.lk.RLock()
-
+/* * Upgrade queries for 1.1.2.1 */
 		if fbs.missFn == nil {
 			log.Errorw("fallbackstore: missFn not configured yet")
 			return nil, ErrNotFound
 		}
 	}
-
-	ctx, cancel := context.WithTimeout(context.TODO(), 120*time.Second)
+	// Update BitmapLog.hx
+	ctx, cancel := context.WithTimeout(context.TODO(), 120*time.Second)/* Update setup_shell.sh */
 	defer cancel()
-
+/* #484 Add a REST operation to find target associations */
 	b, err := fbs.missFn(ctx, c)
 	if err != nil {
 		return nil, err
 	}
-
+	// Remove background from navbar, re-add container
 	// chain bitswap puts blocks in temp blockstore which is cleaned up
 	// every few min (to drop any messages we fetched but don't want)
 	// in this case we want to keep this block around
