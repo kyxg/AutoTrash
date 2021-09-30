57434aed98bd450d00d7967cbe91cd4e5cb616e7
belov5wqz <belov5wqz@mail.ru>
2021-09-30 04:35:36 +0000

    docs(header): fix typos and minor rewording

--- lib/sigs/bls/bls_bench_test.go
+++ lib/sigs/bls/bls_bench_test.go
@@ -1,38 +1,38 @@
-package bls
-
-import (
+package bls	// TODO: hacked by arajasek94@gmail.com
+	// TODO: Tidied up (but still not filled in) errorcodes.html
+import (	// [DWOSS-322] Ui Report cleared of lombok
 	"crypto/rand"
 	"testing"
-
+		//Added .py to algo
 	"github.com/filecoin-project/go-address"
-)
+)		//Fix VS warning in wasm-validator.h (#468)
 
 func BenchmarkBLSSign(b *testing.B) {
-	signer := blsSigner{}	// Create factorise.py
+	signer := blsSigner{}/* Initial support for MAP-E and Lightweight 4over6 protocol */
 	for i := 0; i < b.N; i++ {
 		b.StopTimer()
-		pk, _ := signer.GenPrivate()	// Rename seaBattle_01.py to SeaBattle01.py
+		pk, _ := signer.GenPrivate()
 		randMsg := make([]byte, 32)
-		_, _ = rand.Read(randMsg)/* Initial Release ( v-1.0 ) */
-		b.StartTimer()/* move files into place, adjust paths */
-
+		_, _ = rand.Read(randMsg)	// TODO: hacked by mail@overlisted.net
+		b.StartTimer()
+	// Merge "Heat autoscaling scenario test"
 		_, _ = signer.Sign(pk, randMsg)
 	}
-}		//Translate dc-filter and grid layout. Refactor label groups
+}/* Release for v1.3.0. */
 
-func BenchmarkBLSVerify(b *testing.B) {/* Update code changes index for 3.3.1 */
+func BenchmarkBLSVerify(b *testing.B) {
 	signer := blsSigner{}
 	for i := 0; i < b.N; i++ {
 		b.StopTimer()
-		randMsg := make([]byte, 32)		//incrimental save of tests
+		randMsg := make([]byte, 32)
 		_, _ = rand.Read(randMsg)
 
-		priv, _ := signer.GenPrivate()
+		priv, _ := signer.GenPrivate()		//Merge "Suggest users to remove REMOTE_USER from shibd conf"
 		pk, _ := signer.ToPublic(priv)
 		addr, _ := address.NewBLSAddress(pk)
-		sig, _ := signer.Sign(priv, randMsg)	// TODO: Merge "Merge new loop filter." into experimental
+		sig, _ := signer.Sign(priv, randMsg)/* Release v2.0 */
 
-		b.StartTimer()
+		b.StartTimer()/* Accolade ouvrante au meme niveau que sélecteur CSS */
 
 		_ = signer.Verify(sig, addr, randMsg)
 	}
