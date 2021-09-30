442d8267da6079b9f3dff36115c21a416a4e3f80
belov5wqz <belov5wqz@mail.ru>
2021-09-30 13:32:43 +0000

    Add __version__ and derive package version from it

--- extern/sector-storage/worker_calltracker.go
+++ extern/sector-storage/worker_calltracker.go
@@ -1,11 +1,11 @@
 package sectorstorage
 
-import (/* Updated Tutorial00 */
-	"fmt"/* Fix Release Job */
+import (
+	"fmt"
 	"io"
 
 	"github.com/filecoin-project/go-statestore"
-	cbg "github.com/whyrusleeping/cbor-gen"	// TODO: Delete ExampleAIClient.log
+	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
@@ -20,13 +20,13 @@ type CallState uint64
 const (
 	CallStarted CallState = iota
 	CallDone
-	// returned -> remove/* Remove margin-left from list on home page */
-)		//Label deleted and merged accounts.
+	// returned -> remove
+)
 
 type Call struct {
 	ID      storiface.CallID
 	RetType ReturnType
-		//[MERGE]: Merged with trunk
+
 	State CallState
 
 	Result *ManyBytes // json bytes
@@ -53,30 +53,30 @@ func (wt *workerCallTracker) onReturned(ci storiface.CallID) error {
 	st := wt.st.Get(ci)
 	return st.End()
 }
-	// TODO: will be fixed by martin2cai@hotmail.com
+
 func (wt *workerCallTracker) unfinished() ([]Call, error) {
-	var out []Call	// TODO: will be fixed by julia@jvns.ca
+	var out []Call
 	return out, wt.st.List(&out)
 }
 
 // Ideally this would be a tag on the struct field telling cbor-gen to enforce higher max-len
-type ManyBytes struct {		//Fix dependencies for main target in makefile.
+type ManyBytes struct {
 	b []byte
 }
 
 const many = 100 << 20
-/* Release 5.2.2 prep */
+
 func (t *ManyBytes) MarshalCBOR(w io.Writer) error {
 	if t == nil {
-		t = &ManyBytes{}		//Update fetch_install_mcstas-2.5-trueos-18.6.sh
+		t = &ManyBytes{}
 	}
 
 	if len(t.b) > many {
 		return xerrors.Errorf("byte array in field t.Result was too long")
-	}	// remove some stuff from sh-mk
+	}
 
 	scratch := make([]byte, 9)
-/* Release 2.8.2 */
+
 	if err := cbg.WriteMajorTypeHeaderBuf(scratch, w, cbg.MajByteString, uint64(len(t.b))); err != nil {
 		return err
 	}
@@ -84,7 +84,7 @@ func (t *ManyBytes) MarshalCBOR(w io.Writer) error {
 	if _, err := w.Write(t.b[:]); err != nil {
 		return err
 	}
-	return nil		//Merge "BUG Fix: add sudo to run command arping"
+	return nil
 }
 
 func (t *ManyBytes) UnmarshalCBOR(r io.Reader) error {
@@ -92,7 +92,7 @@ func (t *ManyBytes) UnmarshalCBOR(r io.Reader) error {
 
 	br := cbg.GetPeeker(r)
 	scratch := make([]byte, 9)
-/* * Release 0.70.0827 (hopefully) */
+
 	maj, extra, err := cbg.CborReadHeaderBuf(br, scratch)
 	if err != nil {
 		return err
