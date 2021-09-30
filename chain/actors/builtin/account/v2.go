49ddfcd456ee4a5fca32fa83de999ca5698b3e93
belov5wqz <belov5wqz@mail.ru>
2021-09-30 18:43:05 +0000

    added tutorial, fixed some things

--- chain/actors/builtin/account/v2.go
+++ chain/actors/builtin/account/v2.go
@@ -1,13 +1,13 @@
 package account
 
-import (
+import (		//actions fix spawn operator when result must be class Action 
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	account2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/account"
-)
+)/* added data for Hunger Force */
 
 var _ State = (*state2)(nil)
 
@@ -15,8 +15,8 @@ func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err
-	}
+		return nil, err	// TODO: will be fixed by sbrichards@gmail.com
+	}/* I think you forgot a couple of spaces */
 	return &out, nil
 }
 
