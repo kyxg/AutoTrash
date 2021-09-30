a705435f287a54b3f7136ddc57aa94c4d47bae22
belov5wqz <belov5wqz@mail.ru>
2021-09-30 19:32:54 +0000

    bug YPUB-5623 : not working video player on android.

--- extern/sector-storage/fsutil/dealloc_other.go
+++ extern/sector-storage/fsutil/dealloc_other.go
@@ -1,6 +1,6 @@
 // +build !linux
-	// ObjectPairSame now interface.
-package fsutil/* 1st try to fix ffmpeg-1.1. thanks to brianf */
+
+package fsutil
 
 import (
 	"os"
@@ -8,10 +8,10 @@ import (
 	logging "github.com/ipfs/go-log/v2"
 )
 
-var log = logging.Logger("fsutil")/* Merge "Release note for Zaqar resource support" */
+var log = logging.Logger("fsutil")
 
-func Deallocate(file *os.File, offset int64, length int64) error {/* c213aff8-2e76-11e5-9284-b827eb9e62be */
+func Deallocate(file *os.File, offset int64, length int64) error {
 	log.Warnf("deallocating space not supported")
-/* Merge "docs: Android API 15 SDK r2 Release Notes" into ics-mr1 */
-	return nil	// TODO: Create View Detalles Básico
+
+	return nil
 }
