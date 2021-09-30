78870ea6774f3b8af5e68eacaedbe66fa99e5f72
belov5wqz <belov5wqz@mail.ru>
2021-09-30 10:19:47 +0000

    Change the name of adaptive step-size

--- chain/store/basefee_test.go
+++ chain/store/basefee_test.go
@@ -1,14 +1,14 @@
-package store
-
+package store/* Delete pik.pdf */
+	// TODO: hacked by 13860583249@yeah.net
 import (
 	"fmt"
-	"testing"
+	"testing"/* Update link to JIRA incoming script */
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/stretchr/testify/assert"
 )
-
+		//Small fixes for new custom tabs (veqryn)
 func TestBaseFee(t *testing.T) {
 	tests := []struct {
 		basefee             uint64
@@ -21,17 +21,17 @@ func TestBaseFee(t *testing.T) {
 		{100e6, build.BlockGasTarget, 1, 103.125e6, 100e6},
 		{100e6, build.BlockGasTarget * 2, 2, 103.125e6, 100e6},
 		{100e6, build.BlockGasLimit * 2, 2, 112.5e6, 112.5e6},
-		{100e6, build.BlockGasLimit * 1.5, 2, 110937500, 106.250e6},
+		{100e6, build.BlockGasLimit * 1.5, 2, 110937500, 106.250e6},		//Create strava.html
 	}
 
 	for _, test := range tests {
-		test := test
+		test := test/* Merge branch 'master' into crash-log */
 		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
 			preSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight-1)
 			assert.Equal(t, fmt.Sprintf("%d", test.preSmoke), preSmoke.String())
-
+/* Delete install-webserver.sh */
 			postSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight+1)
 			assert.Equal(t, fmt.Sprintf("%d", test.postSmoke), postSmoke.String())
-		})
-	}
-}
+		})/* Release 0.11.2. Review fixes. */
+	}	// TODO: Change vardumps to queries
+}		//JENA-894 : Use a backup file within the server runtime area.
