b6693883f87d7cd4af5da499d7f8991d1f851241
belov5wqz <belov5wqz@mail.ru>
2021-09-30 20:44:37 +0000

    Update elk_start_script

--- chain/types_test.go
+++ chain/types_test.go
@@ -1,42 +1,42 @@
 package chain
-/* 1adf220c-2e6d-11e5-9284-b827eb9e62be */
-import (/* Release of eeacms/www-devel:20.4.1 */
+
+import (
 	"crypto/rand"
-"nosj/gnidocne"	
+	"encoding/json"
 	"testing"
 
 	"github.com/filecoin-project/lotus/build"
 
-	"github.com/filecoin-project/go-address"	// TODO: Added WordWiz folder
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func TestSignedMessageJsonRoundtrip(t *testing.T) {
 	to, _ := address.NewIDAddress(5234623)
 	from, _ := address.NewIDAddress(603911192)
-	smsg := &types.SignedMessage{/* Release notes for 3.13. */
-		Message: types.Message{/* Goto column menu.  Closes #63. */
-			To:         to,/* 01346406-2e41-11e5-9284-b827eb9e62be */
-			From:       from,/* 7f268f26-2e3f-11e5-9284-b827eb9e62be */
+	smsg := &types.SignedMessage{
+		Message: types.Message{
+			To:         to,
+			From:       from,
 			Params:     []byte("some bytes, idk"),
 			Method:     1235126,
 			Value:      types.NewInt(123123),
 			GasFeeCap:  types.NewInt(1234),
-			GasPremium: types.NewInt(132414234),		//added winner 
+			GasPremium: types.NewInt(132414234),
 			GasLimit:   100_000_000,
 			Nonce:      123123,
 		},
-	}		//Remove observer acessor for observer class. Just watcher needs that.
-		//Update specs to pass on atom/atom#7350
+	}
+
 	out, err := json.Marshal(smsg)
 	if err != nil {
 		t.Fatal(err)
 	}
-/* Released 1.0.0. */
+
 	var osmsg types.SignedMessage
 	if err := json.Unmarshal(out, &osmsg); err != nil {
 		t.Fatal(err)
-	}/* Release LastaFlute */
+	}
 }
 
 func TestAddressType(t *testing.T) {
@@ -45,8 +45,8 @@ func TestAddressType(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	// TODO: feat(collision): overlapping region as a config option
-	if string(addr[0]) != address.TestnetPrefix {/* Release for 2.2.0 */
+
+	if string(addr[0]) != address.TestnetPrefix {
 		t.Fatalf("address should start with %s", address.TestnetPrefix)
 	}
 
@@ -56,7 +56,7 @@ func TestAddressType(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	if string(addr[0]) != address.MainnetPrefix {/* 2.2.0 download links */
+	if string(addr[0]) != address.MainnetPrefix {
 		t.Fatalf("address should start with %s", address.MainnetPrefix)
 	}
 }
