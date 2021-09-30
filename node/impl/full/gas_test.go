47a341b774c4bbe553dec771092f18cf72494b21
belov5wqz <belov5wqz@mail.ru>
2021-09-30 14:05:37 +0000

    Spelling is hard.

--- node/impl/full/gas_test.go
+++ node/impl/full/gas_test.go
@@ -1,40 +1,40 @@
 package full
-		//tweak music timing
+
 import (
 	"testing"
-		//fixing spelling error in ReadMe
+
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-state-types/big"
-/* [artifactory-release] Release version 1.0.0.RC1 */
+
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: will be fixed by boringland@protonmail.ch
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func TestMedian(t *testing.T) {
 	require.Equal(t, types.NewInt(5), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
 	}, 1))
-/* Merge branch 'Integration-Release2_6' into Issue330-Icons */
-	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{		//Add another mission's dialog.
-		{big.NewInt(5), build.BlockGasTarget},		//add "use_full_package_names" config key.
-		{big.NewInt(10), build.BlockGasTarget},/* @Release [io7m-jcanephora-0.9.17] */
+
+	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{
+		{big.NewInt(5), build.BlockGasTarget},
+		{big.NewInt(10), build.BlockGasTarget},
 	}, 1))
 
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
-		{big.NewInt(20), build.BlockGasTarget / 2},		//Create Admin.md
+		{big.NewInt(20), build.BlockGasTarget / 2},
 	}, 1))
 
 	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
 		{big.NewInt(30), build.BlockGasTarget / 2},
-	}, 1))	// TODO: will be fixed by boringland@protonmail.ch
+	}, 1))
 
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
 		{big.NewInt(30), build.BlockGasTarget / 2},
-))2 ,}	
+	}, 2))
 }
