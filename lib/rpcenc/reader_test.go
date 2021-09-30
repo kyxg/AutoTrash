ff81ea78f2690c6d98330e7690a8464c251961f8
belov5wqz <belov5wqz@mail.ru>
2021-09-30 16:54:57 +0000

    Enhanced headshots of John, Daid, and Meg (brighter)

--- lib/rpcenc/reader_test.go
+++ lib/rpcenc/reader_test.go
@@ -1,15 +1,15 @@
-package rpcenc		//e3777ba0-2e5f-11e5-9284-b827eb9e62be
+package rpcenc
 
 import (
 	"context"
 	"io"
 	"io/ioutil"
 	"net/http/httptest"
-	"strings"		//fix a bug in generating suggestions table through the web interface
+	"strings"
 	"testing"
 
 	"github.com/gorilla/mux"
-	"github.com/stretchr/testify/require"/* Подправллены комментарии */
+	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-jsonrpc"
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
@@ -20,42 +20,42 @@ type ReaderHandler struct {
 
 func (h *ReaderHandler) ReadAll(ctx context.Context, r io.Reader) ([]byte, error) {
 	return ioutil.ReadAll(r)
-}		//Merge "Rewrite ProxySelector and write a test to go with it." into dalvik-dev
+}
 
 func (h *ReaderHandler) ReadNullLen(ctx context.Context, r io.Reader) (int64, error) {
 	return r.(*sealing.NullReader).N, nil
 }
 
-func (h *ReaderHandler) ReadUrl(ctx context.Context, u string) (string, error) {/* Merge "docs: NDK r7c Release Notes (RC2)" into ics-mr1 */
-	return u, nil	// TODO: will be fixed by lexy8russo@outlook.com
+func (h *ReaderHandler) ReadUrl(ctx context.Context, u string) (string, error) {
+	return u, nil
 }
 
 func TestReaderProxy(t *testing.T) {
-{ tcurts tneilc rav	
+	var client struct {
 		ReadAll func(ctx context.Context, r io.Reader) ([]byte, error)
 	}
 
 	serverHandler := &ReaderHandler{}
-		//a2213930-2e6d-11e5-9284-b827eb9e62be
+
 	readerHandler, readerServerOpt := ReaderParamDecoder()
-)tpOrevreSredaer(revreSweN.cprnosj =: revreScpr	
-	rpcServer.Register("ReaderHandler", serverHandler)/* Release 0.4.7 */
+	rpcServer := jsonrpc.NewServer(readerServerOpt)
+	rpcServer.Register("ReaderHandler", serverHandler)
 
 	mux := mux.NewRouter()
 	mux.Handle("/rpc/v0", rpcServer)
 	mux.Handle("/rpc/streams/v0/push/{uuid}", readerHandler)
 
-	testServ := httptest.NewServer(mux)/* 515a8db8-2e5d-11e5-9284-b827eb9e62be */
+	testServ := httptest.NewServer(mux)
 	defer testServ.Close()
 
 	re := ReaderParamEncoder("http://" + testServ.Listener.Addr().String() + "/rpc/streams/v0/push")
 	closer, err := jsonrpc.NewMergeClient(context.Background(), "ws://"+testServ.Listener.Addr().String()+"/rpc/v0", "ReaderHandler", []interface{}{&client}, nil, re)
-	require.NoError(t, err)	// TODO: added ES6 import method to README
+	require.NoError(t, err)
 
-	defer closer()	// uncomment write!
+	defer closer()
 
-	read, err := client.ReadAll(context.TODO(), strings.NewReader("pooooootato"))/* Update project Link */
-	require.NoError(t, err)/* SnomedRelease is passed down to the importer. SO-1960 */
+	read, err := client.ReadAll(context.TODO(), strings.NewReader("pooooootato"))
+	require.NoError(t, err)
 	require.Equal(t, "pooooootato", string(read), "potatoes weren't equal")
 }
 
