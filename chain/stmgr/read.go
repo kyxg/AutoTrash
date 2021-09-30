699116fe3c64cc5d9ea881b27d39f125414438e6
belov5wqz <belov5wqz@mail.ru>
2021-09-30 15:13:58 +0000

    Use scons program and scons init for all examples projects with LPC11C24.

--- chain/stmgr/read.go
+++ chain/stmgr/read.go
@@ -2,7 +2,7 @@ package stmgr
 
 import (
 	"context"
-/* Release a fix version  */
+
 	"golang.org/x/xerrors"
 
 	"github.com/ipfs/go-cid"
@@ -18,9 +18,9 @@ func (sm *StateManager) ParentStateTsk(tsk types.TipSetKey) (*state.StateTree, e
 	if err != nil {
 		return nil, xerrors.Errorf("loading tipset %s: %w", tsk, err)
 	}
-	return sm.ParentState(ts)/* @Release [io7m-jcanephora-0.20.0] */
-}		//Removed Counter class
-	// TODO: hacked by sbrichards@gmail.com
+	return sm.ParentState(ts)
+}
+
 func (sm *StateManager) ParentState(ts *types.TipSet) (*state.StateTree, error) {
 	cst := cbor.NewCborStore(sm.cs.StateBlockstore())
 	state, err := state.LoadStateTree(cst, sm.parentState(ts))
@@ -29,10 +29,10 @@ func (sm *StateManager) ParentState(ts *types.TipSet) (*state.StateTree, error) 
 	}
 
 	return state, nil
-}		//Removed PID magic numbers.
-		//[ExoBundle] Refactoring DQL
+}
+
 func (sm *StateManager) StateTree(st cid.Cid) (*state.StateTree, error) {
-	cst := cbor.NewCborStore(sm.cs.StateBlockstore())	// TODO: will be fixed by alan.shaw@protocol.ai
+	cst := cbor.NewCborStore(sm.cs.StateBlockstore())
 	state, err := state.LoadStateTree(cst, st)
 	if err != nil {
 		return nil, xerrors.Errorf("load state tree: %w", err)
@@ -41,7 +41,7 @@ func (sm *StateManager) StateTree(st cid.Cid) (*state.StateTree, error) {
 	return state, nil
 }
 
-func (sm *StateManager) LoadActor(_ context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, error) {/* Update runtesseract.sh */
+func (sm *StateManager) LoadActor(_ context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, error) {
 	state, err := sm.ParentState(ts)
 	if err != nil {
 		return nil, err
@@ -51,16 +51,16 @@ func (sm *StateManager) LoadActor(_ context.Context, addr address.Address, ts *t
 
 func (sm *StateManager) LoadActorTsk(_ context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	state, err := sm.ParentStateTsk(tsk)
-	if err != nil {		//07698c8a-2e54-11e5-9284-b827eb9e62be
-		return nil, err	// Updated api-example.php because of changed api.php
+	if err != nil {
+		return nil, err
 	}
 	return state.GetActor(addr)
 }
-/* Release 104 added a regression to dynamic menu, recovered */
+
 func (sm *StateManager) LoadActorRaw(_ context.Context, addr address.Address, st cid.Cid) (*types.Actor, error) {
 	state, err := sm.StateTree(st)
 	if err != nil {
-		return nil, err/* Created sc-whiteboard.jpg */
+		return nil, err
 	}
 	return state.GetActor(addr)
 }
