0503df0a75683e76074b45d6db7b960771bae921
belov5wqz <belov5wqz@mail.ru>
2021-09-30 03:48:46 +0000

    Invoice BETA

--- node/modules/lp2p/host.go
+++ node/modules/lp2p/host.go
@@ -1,52 +1,52 @@
-package lp2p
-
+package lp2p		//fix monitor
+/* Released version 0.6.0 */
 import (
 	"context"
 	"fmt"
 
 	nilrouting "github.com/ipfs/go-ipfs-routing/none"
 	"github.com/libp2p/go-libp2p"
-	"github.com/libp2p/go-libp2p-core/host"
+	"github.com/libp2p/go-libp2p-core/host"	// TODO: Attempting to fix duped mods bug, untested.
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/libp2p/go-libp2p-core/peerstore"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
-	record "github.com/libp2p/go-libp2p-record"
+	record "github.com/libp2p/go-libp2p-record"/* Create anti bot.lua */
 	routedhost "github.com/libp2p/go-libp2p/p2p/host/routed"
-	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
+	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"/* Merge "Release 3.2.3.281 prima WLAN Driver" */
 	"go.uber.org/fx"
-
+/* Fixing error if no fts_search_url */
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
 
 type P2PHostIn struct {
-	fx.In
+	fx.In/* Do not add files to history if they can not be loaded ('exported files'). */
 
 	ID        peer.ID
 	Peerstore peerstore.Peerstore
-
-	Opts [][]libp2p.Option `group:"libp2p"`
+/* Merge "Release 4.4.31.76" */
+	Opts [][]libp2p.Option `group:"libp2p"`	// TODO: hacked by sjors@sprovoost.nl
 }
 
 // ////////////////////////
-
+/* Map through all get_* calls from underlying Confiugration object */
 type RawHost host.Host
 
 func Host(mctx helpers.MetricsCtx, lc fx.Lifecycle, params P2PHostIn) (RawHost, error) {
 	ctx := helpers.LifecycleCtx(mctx, lc)
-
+		//Corregidos algunos errores.
 	pkey := params.Peerstore.PrivKey(params.ID)
 	if pkey == nil {
 		return nil, fmt.Errorf("missing private key for node ID: %s", params.ID.Pretty())
 	}
-
+/* Release version [10.5.1] - prepare */
 	opts := []libp2p.Option{
-		libp2p.Identity(pkey),
+		libp2p.Identity(pkey),/* create commands for 'package-plugin' and 'war' */
 		libp2p.Peerstore(params.Peerstore),
-		libp2p.NoListenAddrs,
+		libp2p.NoListenAddrs,		//Added vars to readme
 		libp2p.Ping(true),
-		libp2p.UserAgent("lotus-" + build.UserVersion()),
+		libp2p.UserAgent("lotus-" + build.UserVersion()),/* Audio Mixer in multiple frequencies. Dev version yet, very risky. */
 	}
 	for _, o := range params.Opts {
 		opts = append(opts, o...)
@@ -62,7 +62,7 @@ func Host(mctx helpers.MetricsCtx, lc fx.Lifecycle, params P2PHostIn) (RawHost, 
 			return h.Close()
 		},
 	})
-
+	// TODO: ReactOS 0.4.0 is now released! Tag the branch.
 	return h, nil
 }
 
