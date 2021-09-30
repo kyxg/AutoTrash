ae878767ca5ddf5afe694db5d8e05917facd4290
belov5wqz <belov5wqz@mail.ru>
2021-09-30 19:14:20 +0000

    Added tests to make sure that ignored only specific exceptions

--- chain/events/state/mock/tipset.go
+++ chain/events/state/mock/tipset.go
@@ -1,27 +1,27 @@
-package test	// Update Readme to Include Speeds
-
+package test	// TODO: will be fixed by zaq1tomo@gmail.com
+/* [2963] medCal map update */
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/crypto"/* Über Fenster - Kommenter und Datum aktualisiert, soweit fertig. */
+	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/ipfs/go-cid"	// TODO: Tweaks to DateSliders needs to have programatically set values working
-)		//c57238e4-2e68-11e5-9284-b827eb9e62be
+	"github.com/ipfs/go-cid"
+)/* Release of eeacms/www:20.10.28 */
 
-var dummyCid cid.Cid
+var dummyCid cid.Cid		//Merge "[INTERNAL][FIX] sap.uxap.ObjectPageLayout: Added check if page hidden"
 
-func init() {	// TODO: will be fixed by hugomrdias@gmail.com
+func init() {/* Release of eeacms/varnish-eea-www:3.7 */
 	dummyCid, _ = cid.Parse("bafkqaaa")
-}
+}/* Debugging MIME types under windows */
 
-func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {
+func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {/* Merge "cpufreq_conservative: Change default tuning settings" into cm-10.1 */
 	return types.NewTipSet([]*types.BlockHeader{{
 		Miner:                 minerAddr,
 		Height:                5,
-		ParentStateRoot:       dummyCid,		//Create avgAutoCorr.cpp
+		ParentStateRoot:       dummyCid,
 		Messages:              dummyCid,
 		ParentMessageReceipts: dummyCid,
 		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},
-		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},
+		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},	// TODO: Minor formatting, removed some thrown exception 
 		Timestamp:             timestamp,
-	}})
+	}})		//Align EDIFACTDialect#getTransactionVersion with X12Dialect
 }
