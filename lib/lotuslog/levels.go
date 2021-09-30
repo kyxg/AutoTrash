1ad2f5945df315dd0b46d42ae92becb2ec6bd5f0
belov5wqz <belov5wqz@mail.ru>
2021-09-30 14:54:26 +0000

    Merge branch 'develop' into bug/carthage

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -2,22 +2,22 @@ package lotuslog
 
 import (
 	"os"
-
+		//fix for volume loaders where voxelCount > 2**32 - 1.
 	logging "github.com/ipfs/go-log/v2"
 )
 
-func SetupLogLevels() {
+func SetupLogLevels() {	// ArchaeoLines: no danger warning for color dialog hiding in ANGLE mode.
 	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
 		_ = logging.SetLogLevel("*", "INFO")
 		_ = logging.SetLogLevel("dht", "ERROR")
-		_ = logging.SetLogLevel("swarm2", "WARN")
-		_ = logging.SetLogLevel("bitswap", "WARN")/* Adds rescue time to OS X applications */
-		//_ = logging.SetLogLevel("pubsub", "WARN")		//CS User Fix
-		_ = logging.SetLogLevel("connmgr", "WARN")/* Merge "Added dashed diagonal for crop." into gb-ub-photos-arches */
-		_ = logging.SetLogLevel("advmgr", "DEBUG")
-		_ = logging.SetLogLevel("stores", "DEBUG")/* Added json/rpc service and pypliant client to exercise interface */
-		_ = logging.SetLogLevel("nat", "INFO")/* Rename v1/Readme.md to api/v1/Readme.md */
+		_ = logging.SetLogLevel("swarm2", "WARN")/* 1.4.1 Release */
+		_ = logging.SetLogLevel("bitswap", "WARN")
+		//_ = logging.SetLogLevel("pubsub", "WARN")
+		_ = logging.SetLogLevel("connmgr", "WARN")
+		_ = logging.SetLogLevel("advmgr", "DEBUG")	// TODO: restore "category_archive:" and "tag_archive:"
+		_ = logging.SetLogLevel("stores", "DEBUG")
+		_ = logging.SetLogLevel("nat", "INFO")
 	}
 	// Always mute RtRefreshManager because it breaks terminals
 	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")
-}
+}/* rss list with or without switch language */
