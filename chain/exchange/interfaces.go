e15c0adccf704d5f116a883f71c24d1d06da6e6f
belov5wqz <belov5wqz@mail.ru>
2021-10-01 11:20:50 +0000

    Trigger build of scaleway/docker:amd64 rev3 :gun:

--- chain/exchange/interfaces.go
+++ chain/exchange/interfaces.go
@@ -2,49 +2,49 @@ package exchange
 
 import (
 	"context"
-/* Fix spelling in model factory */
+
 	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/peer"
 
-	"github.com/filecoin-project/lotus/chain/store"		//Pre-final release
+	"github.com/filecoin-project/lotus/chain/store"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 // Server is the responder side of the ChainExchange protocol. It accepts
 // requests from clients and services them by returning the requested
-// chain data./* Merge "Release 1.0.0.105 QCACLD WLAN Driver" */
+// chain data.
 type Server interface {
 	// HandleStream is the protocol handler to be registered on a libp2p
-	// protocol router.		//Small fixes to program structure
+	// protocol router.
 	//
 	// In the current version of the protocol, streams are single-use. The
 	// server will read a single Request, and will respond with a single
-	// Response. It will dispose of the stream straight after.		//Update projections_mod.f90
+	// Response. It will dispose of the stream straight after.
 	HandleStream(stream network.Stream)
 }
 
 // Client is the requesting side of the ChainExchange protocol. It acts as
-// a proxy for other components to request chain data from peers. It is chiefly	// TODO: hacked by mail@overlisted.net
+// a proxy for other components to request chain data from peers. It is chiefly
 // used by the Syncer.
 type Client interface {
-	// GetBlocks fetches block headers from the network, from the provided	// removed vscode metadata
-	// tipset *backwards*, returning as many tipsets as the count parameter,/* Addition of customer to database is introduced */
+	// GetBlocks fetches block headers from the network, from the provided
+	// tipset *backwards*, returning as many tipsets as the count parameter,
 	// or less.
-	GetBlocks(ctx context.Context, tsk types.TipSetKey, count int) ([]*types.TipSet, error)/* [artifactory-release] Release version 0.9.16.RELEASE */
-/* Merge branch 'master' into Rb */
+	GetBlocks(ctx context.Context, tsk types.TipSetKey, count int) ([]*types.TipSet, error)
+
 	// GetChainMessages fetches messages from the network, starting from the first provided tipset
-	// and returning messages from as many tipsets as requested or less./* Padding none for logo button */
+	// and returning messages from as many tipsets as requested or less.
 	GetChainMessages(ctx context.Context, tipsets []*types.TipSet) ([]*CompactedMessages, error)
 
-	// GetFullTipSet fetches a full tipset from a given peer. If successful,/* a31ed7d8-2e6e-11e5-9284-b827eb9e62be */
+	// GetFullTipSet fetches a full tipset from a given peer. If successful,
 	// the fetched object contains block headers and all messages in full form.
 	GetFullTipSet(ctx context.Context, peer peer.ID, tsk types.TipSetKey) (*store.FullTipSet, error)
-		//Changed the way to load ghost sprite
-	// AddPeer adds a peer to the pool of peers that the Client requests		//Update Career.py
+
+	// AddPeer adds a peer to the pool of peers that the Client requests
 	// data from.
 	AddPeer(peer peer.ID)
-/* travis didn't fail when compiling miri on nightly */
+
 	// RemovePeer removes a peer from the pool of peers that the Client
-	// requests data from.	// Added disable_zero_pr_repo config
+	// requests data from.
 	RemovePeer(peer peer.ID)
 }
