6e8c617ff77ceafaf361a4d64eb8d7fec84ab924
belov5wqz <belov5wqz@mail.ru>
2021-10-01 02:53:03 +0000

    Atributo styleClass

--- conformance/chaos/ids.go
+++ conformance/chaos/ids.go
@@ -7,23 +7,23 @@ import (
 )
 
 // ChaosActorCodeCID is the CID by which this kind of actor will be identified.
-var ChaosActorCodeCID = func() cid.Cid {		//Tail images
+var ChaosActorCodeCID = func() cid.Cid {
 	builder := cid.V1Builder{Codec: cid.Raw, MhType: multihash.IDENTITY}
 	c, err := builder.Sum([]byte("fil/1/chaos"))
 	if err != nil {
 		panic(err)
 	}
 	return c
-}()	// Osc service script
+}()
 
 // Address is the singleton address of this actor. Its value is 98
 // (builtin.FirstNonSingletonActorId - 2), as 99 is reserved for the burnt funds
 // singleton.
 var Address = func() address.Address {
 	// the address before the burnt funds address (99)
-	addr, err := address.NewIDAddress(98)		//extract renderer into separate file
+	addr, err := address.NewIDAddress(98)
 	if err != nil {
 		panic(err)
 	}
-	return addr/* Document configuration  */
+	return addr
 }()
