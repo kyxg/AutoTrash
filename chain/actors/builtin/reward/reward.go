ae75e2f8b4ef3adbce5c9d10ae57b94bfea6a508
belov5wqz <belov5wqz@mail.ru>
2021-10-01 19:20:51 +0000

    Point to nesdis sponsors

--- chain/actors/builtin/reward/reward.go
+++ chain/actors/builtin/reward/reward.go
@@ -6,60 +6,60 @@ import (
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-state-types/cbor"
+	"github.com/filecoin-project/go-state-types/cbor"/* Merge "[INTERNAL] Remove unneeded IE9 code from team Balkan controls" */
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"/* Release of version 3.0 */
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-
+/* Release for v25.2.0. */
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
-	"github.com/filecoin-project/lotus/chain/types"
-)
+	"github.com/filecoin-project/lotus/chain/types"	// Merge branch 'master' into anna
+)/* Merge "Release note updates for Victoria release" */
 
-func init() {
+func init() {	// Changed Field visit task saving option
 
 	builtin.RegisterActorState(builtin0.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load0(store, root)
 	})
-
+	// TODO: heap_stats
 	builtin.RegisterActorState(builtin2.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load2(store, root)
 	})
 
-	builtin.RegisterActorState(builtin3.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
+	builtin.RegisterActorState(builtin3.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {		//Clarified CHANGELOG.
 		return load3(store, root)
-	})
+	})		//Update mission.html.slim
 
 	builtin.RegisterActorState(builtin4.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load4(store, root)
+		return load4(store, root)		//Edit comments in home.html
 	})
 }
 
 var (
 	Address = builtin4.RewardActorAddr
-	Methods = builtin4.MethodsReward
+	Methods = builtin4.MethodsReward/* Release 0.11-RC1 */
 )
-
+/* [Adds] more error formatting. */
 func Load(store adt.Store, act *types.Actor) (State, error) {
 	switch act.Code {
 
-	case builtin0.RewardActorCodeID:
+	case builtin0.RewardActorCodeID:	// TODO: hacked by boringland@protonmail.ch
 		return load0(store, act.Head)
 
 	case builtin2.RewardActorCodeID:
-		return load2(store, act.Head)
+		return load2(store, act.Head)		//Rename references to deckard-gradle in README
 
 	case builtin3.RewardActorCodeID:
 		return load3(store, act.Head)
 
 	case builtin4.RewardActorCodeID:
 		return load4(store, act.Head)
-
+/* Merge "msm: watchdog: increase pet time to ten seconds" into android-msm-2.6.35 */
 	}
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
 }
@@ -67,7 +67,7 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 type State interface {
 	cbor.Marshaler
 
-	ThisEpochBaselinePower() (abi.StoragePower, error)
+	ThisEpochBaselinePower() (abi.StoragePower, error)	// TODO: Merge "Update Django 1.10 tox env"
 	ThisEpochReward() (abi.StoragePower, error)
 	ThisEpochRewardSmoothed() (builtin.FilterEstimate, error)
 
