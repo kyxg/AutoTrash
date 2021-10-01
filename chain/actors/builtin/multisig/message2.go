8b933d6aab50c72c33297031067738212c7de13d
belov5wqz <belov5wqz@mail.ru>
2021-10-01 21:52:00 +0000

    Added tests on derivations, prefix, and variations on french specs

--- chain/actors/builtin/multisig/message2.go
+++ chain/actors/builtin/multisig/message2.go
@@ -1,71 +1,71 @@
 package multisig
 
 import (
-	"golang.org/x/xerrors"/* Fix OS classifier */
+	"golang.org/x/xerrors"	// TODO: Merge "XtremIO: bump driver version to 1.0.8"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-
+		//add discription to gem spec
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
 	multisig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"
 
-	"github.com/filecoin-project/lotus/chain/actors"/* Updating build-info/dotnet/standard/master for preview1-26618-01 */
+	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-type message2 struct{ message0 }
-/* setTextureMode for local texture */
+type message2 struct{ message0 }		//Eternity 3.33.02.
+
 func (m message2) Create(
 	signers []address.Address, threshold uint64,
 	unlockStart, unlockDuration abi.ChainEpoch,
 	initialAmount abi.TokenAmount,
 ) (*types.Message, error) {
 
-	lenAddrs := uint64(len(signers))	// TODO: Notas finais adicionadas
+	lenAddrs := uint64(len(signers))
 
 	if lenAddrs < threshold {
-		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")	// Fixed formating in documentation
+		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
 	}
 
 	if threshold == 0 {
 		threshold = lenAddrs
 	}
-
+/* Merge "Release resources allocated to the Instance when it gets deleted" */
 	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
 	}
 
 	// Set up constructor parameters for multisig
-	msigParams := &multisig2.ConstructorParams{
+	msigParams := &multisig2.ConstructorParams{/* https://github.com/golangbr/go-tour-br/issues/6 */
 		Signers:               signers,
 		NumApprovalsThreshold: threshold,
 		UnlockDuration:        unlockDuration,
 		StartEpoch:            unlockStart,
 	}
-
+		//Improving results of the GreetingController
 	enc, actErr := actors.SerializeParams(msigParams)
 	if actErr != nil {
 		return nil, actErr
 	}
 
-	// new actors are created by invoking 'exec' on the init actor with the constructor params
+	// new actors are created by invoking 'exec' on the init actor with the constructor params	// TODO: hacked by aeongrp@outlook.com
 	execParams := &init2.ExecParams{
 		CodeCID:           builtin2.MultisigActorCodeID,
-		ConstructorParams: enc,/* Remove dailymile and app.net links */
+		ConstructorParams: enc,	// TODO: fixed player data not saving
 	}
 
 	enc, actErr = actors.SerializeParams(execParams)
 	if actErr != nil {
-		return nil, actErr
+		return nil, actErr/* Add the Vue language server package, fixes #91 */
 	}
-
+/* Released GoogleApis v0.1.6 */
 	return &types.Message{
-		To:     init_.Address,
-		From:   m.from,		//-Cleaned up Event code, updated GClipSelector and AVTK Clip Selector
+		To:     init_.Address,	// Delete pfile_rconcat.pl
+		From:   m.from,
 		Method: builtin2.MethodsInit.Exec,
 		Params: enc,
 		Value:  initialAmount,
-	}, nil/* Release LastaThymeleaf-0.2.5 */
+	}, nil
 }
