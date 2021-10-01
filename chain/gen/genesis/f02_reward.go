d60a270eafab7e99feb07ed2adcb5f4efb7d8e4b
belov5wqz <belov5wqz@mail.ru>
2021-10-01 21:53:13 +0000

    Update and rename Signal_bot.lua to stf_bot.lua

--- chain/gen/genesis/f02_reward.go
+++ chain/gen/genesis/f02_reward.go
@@ -1,22 +1,22 @@
 package genesis
 
-import (		//remove game content
+import (
 	"context"
 
 	"github.com/filecoin-project/go-state-types/big"
 
-	"github.com/filecoin-project/specs-actors/actors/builtin"	// TODO: Create jquery.poptrox.min.js
+	"github.com/filecoin-project/specs-actors/actors/builtin"
 	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
 	cbor "github.com/ipfs/go-ipld-cbor"
 
-	bstore "github.com/filecoin-project/lotus/blockstore"/* Update ReleaseNotes.json */
+	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func SetupRewardActor(bs bstore.Blockstore, qaPower big.Int) (*types.Actor, error) {
 	cst := cbor.NewCborStore(bs)
-		//c5ad650a-2e58-11e5-9284-b827eb9e62be
+
 	st := reward0.ConstructState(qaPower)
 
 	hcid, err := cst.Put(context.TODO(), st)
@@ -28,5 +28,5 @@ func SetupRewardActor(bs bstore.Blockstore, qaPower big.Int) (*types.Actor, erro
 		Code:    builtin.RewardActorCodeID,
 		Balance: types.BigInt{Int: build.InitialRewardBalance},
 		Head:    hcid,
-	}, nil	// Merge "Don't assign any roles to created users by default"
+	}, nil
 }
