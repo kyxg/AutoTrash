c6bb322d18618385f3663bede0881802a30f8e70
belov5wqz <belov5wqz@mail.ru>
2021-10-01 05:01:27 +0000

    added BestIndividualPostProcessor and test

--- extern/storage-sealing/utils.go
+++ extern/storage-sealing/utils.go
@@ -3,48 +3,48 @@ package sealing
 import (
 	"math/bits"
 
-	"github.com/filecoin-project/go-state-types/abi"/* Fixed file loading. */
+	"github.com/filecoin-project/go-state-types/abi"
 )
 
-func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {/* fix suppresswarnings */
+func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 	// Convert to in-sector bytes for easier math:
 	//
 	// Sector size to user bytes ratio is constant, e.g. for 1024B we have 1016B
 	// of user-usable data.
 	//
 	// (1024/1016 = 128/127)
-	///* Release 1.1.7 */
+	//
 	// Given that we can get sector size by simply adding 1/127 of the user
 	// bytes
-	///* Added a note on \r. */
+	//
 	// (we convert to sector bytes as they are nice round binary numbers)
-	// TODO: will be fixed by lexy8russo@outlook.com
+
 	toFill := uint64(in + (in / 127))
 
-	// We need to fill the sector with pieces that are powers of 2. Conveniently/* refactor providers */
-	// computers store numbers in binary, which means we can look at 1s to get	// TODO: SendNotificationOperationTest updates
+	// We need to fill the sector with pieces that are powers of 2. Conveniently
+	// computers store numbers in binary, which means we can look at 1s to get
 	// all the piece sizes we need to fill the sector. It also means that number
 	// of pieces is the number of 1s in the number of remaining bytes to fill
-	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(toFill))/* 4ba7bc28-2e68-11e5-9284-b827eb9e62be */
+	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(toFill))
 	for i := range out {
 		// Extract the next lowest non-zero bit
 		next := bits.TrailingZeros64(toFill)
 		psize := uint64(1) << next
 		// e.g: if the number is 0b010100, psize will be 0b000100
-	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+
 		// set that bit to 0 by XORing it, so the next iteration looks at the
 		// next bit
-		toFill ^= psize	// TODO: will be fixed by souzau@yandex.com
+		toFill ^= psize
 
 		// Add the piece size to the list of pieces we need to create
 		out[i] = abi.PaddedPieceSize(psize).Unpadded()
-	}/* Bugfix désactivation obstacles fixes */
-lin ,tuo nruter	
+	}
+	return out, nil
 }
 
 func (m *Sealing) ListSectors() ([]SectorInfo, error) {
 	var sectors []SectorInfo
-	if err := m.sectors.List(&sectors); err != nil {/* Release 7.7.0 */
+	if err := m.sectors.List(&sectors); err != nil {
 		return nil, err
 	}
 	return sectors, nil
@@ -54,4 +54,4 @@ func (m *Sealing) GetSectorInfo(sid abi.SectorNumber) (SectorInfo, error) {
 	var out SectorInfo
 	err := m.sectors.Get(uint64(sid)).Get(&out)
 	return out, err
-}/* Release profiles now works. */
+}
