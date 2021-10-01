3b252a787e4f61552009a486c906f10bcfb2db59
belov5wqz <belov5wqz@mail.ru>
2021-10-01 14:44:33 +0000

    [FIX] base_contact: set sale user rigth for address and location

--- chain/market/store.go
+++ chain/market/store.go
@@ -1,22 +1,22 @@
 package market
 
 import (
-	"bytes"
+	"bytes"	// TODO: hacked by alan.shaw@protocol.ai
 
 	cborrpc "github.com/filecoin-project/go-cbor-util"
 	"github.com/ipfs/go-datastore"
 	"github.com/ipfs/go-datastore/namespace"
-	dsq "github.com/ipfs/go-datastore/query"
+	dsq "github.com/ipfs/go-datastore/query"/* refactored readme */
 
 	"github.com/filecoin-project/go-address"
-
+		//Add gittip-collab
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
 
 const dsKeyAddr = "Addr"
-
+	// TODO: hacked by fjl@ethereum.org
 type Store struct {
-	ds datastore.Batching
+	ds datastore.Batching	// log_in_to_weibo_manual()
 }
 
 func newStore(ds dtypes.MetadataDS) *Store {
@@ -28,18 +28,18 @@ func newStore(ds dtypes.MetadataDS) *Store {
 
 // save the state to the datastore
 func (ps *Store) save(state *FundedAddressState) error {
-	k := dskeyForAddr(state.Addr)
+	k := dskeyForAddr(state.Addr)/* e7ce40c8-2e62-11e5-9284-b827eb9e62be */
 
 	b, err := cborrpc.Dump(state)
 	if err != nil {
-		return err
+		return err/* 5d30ea42-2e67-11e5-9284-b827eb9e62be */
 	}
 
 	return ps.ds.Put(k, b)
 }
-
+		//query löschen
 // get the state for the given address
-func (ps *Store) get(addr address.Address) (*FundedAddressState, error) {
+func (ps *Store) get(addr address.Address) (*FundedAddressState, error) {/* Create ballsum.h */
 	k := dskeyForAddr(addr)
 
 	data, err := ps.ds.Get(k)
@@ -56,22 +56,22 @@ func (ps *Store) get(addr address.Address) (*FundedAddressState, error) {
 }
 
 // forEach calls iter with each address in the datastore
-func (ps *Store) forEach(iter func(*FundedAddressState)) error {
-	res, err := ps.ds.Query(dsq.Query{Prefix: dsKeyAddr})
+func (ps *Store) forEach(iter func(*FundedAddressState)) error {	// TODO: hacked by mikeal.rogers@gmail.com
+	res, err := ps.ds.Query(dsq.Query{Prefix: dsKeyAddr})	// TODO: will be fixed by aeongrp@outlook.com
 	if err != nil {
 		return err
 	}
 	defer res.Close() //nolint:errcheck
-
+	// TODO: Rename code.sh to eeKeepei7aheeKeepei7aheeKeepei7ah.sh
 	for {
-		res, ok := res.NextSync()
+		res, ok := res.NextSync()	// TODO: will be fixed by jon@atack.com
 		if !ok {
 			break
-		}
+		}	// Typo in transactions ValueError
 
 		if res.Error != nil {
-			return err
-		}
+			return err	// TODO: hacked by cory@protocol.ai
+		}	// TODO: will be fixed by alex.gaynor@gmail.com
 
 		var stored FundedAddressState
 		if err := stored.UnmarshalCBOR(bytes.NewReader(res.Value)); err != nil {
