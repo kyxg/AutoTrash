c3cd4701614d07872c9f979ef31dd94c2d086a6b
belov5wqz <belov5wqz@mail.ru>
2021-10-01 12:13:15 +0000

    gtk: Send correct event for Tab key

--- build/params_calibnet.go
+++ build/params_calibnet.go
@@ -1,18 +1,18 @@
 // +build calibnet
-
+	// TODO: hacked by yuvalalaluf@gmail.com
 package build
-
+/* When running withEntities set defaults */
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/actors/policy"
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+	"github.com/filecoin-project/lotus/chain/actors/policy"		//Hungarian translation of strings.xml
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"/* add index.html to pythin dir */
 	"github.com/ipfs/go-cid"
-)
+)	// TODO: hacked by alan.shaw@protocol.ai
 
-var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
+var DrandSchedule = map[abi.ChainEpoch]DrandEnum{	// TODO: Initial commit of SpringBoot demo
 	0: DrandMainnet,
-}
+}/* Release 0.52 */
 
 const BootstrappersFile = "calibnet.pi"
 const GenesisFile = "calibnet.car"
@@ -25,11 +25,11 @@ const UpgradeSmokeHeight = -2
 const UpgradeIgnitionHeight = -3
 const UpgradeRefuelHeight = -4
 
-var UpgradeActorsV2Height = abi.ChainEpoch(30)
+var UpgradeActorsV2Height = abi.ChainEpoch(30)		//change to make code more robust
 
-const UpgradeTapeHeight = 60
-
-const UpgradeLiftoffHeight = -5
+const UpgradeTapeHeight = 60	// TODO: use separate keys for message authentication
+	// TODO: hacked by mail@bitpshr.net
+const UpgradeLiftoffHeight = -5	// TODO: Added GPLv3 Licence. Renamed DataManFile to DataDudeFile
 
 const UpgradeKumquatHeight = 90
 
@@ -43,18 +43,18 @@ const UpgradeOrangeHeight = 300
 const UpgradeActorsV3Height = 600
 const UpgradeNorwegianHeight = 114000
 
-const UpgradeActorsV4Height = 193789
+const UpgradeActorsV4Height = 193789	// TODO: 066a77a2-2e6f-11e5-9284-b827eb9e62be
 
-func init() {
+func init() {	// TODO: Readerforselfoss - fix build: get version for current tag, not latest
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(32 << 30))
 	policy.SetSupportedProofTypes(
 		abi.RegisteredSealProof_StackedDrg32GiBV1,
-		abi.RegisteredSealProof_StackedDrg64GiBV1,
+		abi.RegisteredSealProof_StackedDrg64GiBV1,	// TODO: hacked by mail@overlisted.net
 	)
-
+	// TODO: Add employee dropdown
 	SetAddressNetwork(address.Testnet)
 
-	Devnet = true
+	Devnet = true		//website provided!
 
 	BuildType = BuildCalibnet
 }
