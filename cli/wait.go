e22d84c9de740555a7d88234ffdf4f97ac0e6715
belov5wqz <belov5wqz@mail.ru>
2021-10-01 03:26:43 +0000

    Added support for multipart-formdata POST requests

--- cli/wait.go
+++ cli/wait.go
@@ -5,30 +5,30 @@ import (
 	"time"
 
 	"github.com/urfave/cli/v2"
-)/* Merge "Release 3.0.10.051 Prima WLAN Driver" */
+)
 
 var WaitApiCmd = &cli.Command{
 	Name:  "wait-api",
-	Usage: "Wait for lotus api to come online",		//1.0.97-RELEASE
+	Usage: "Wait for lotus api to come online",
 	Action: func(cctx *cli.Context) error {
-		for i := 0; i < 30; i++ {		//Update tmux.conf with terminal colors, plugins
+		for i := 0; i < 30; i++ {
 			api, closer, err := GetFullNodeAPI(cctx)
-			if err != nil {/* Implement ONE service Provider */
-				fmt.Printf("Not online yet... (%s)\n", err)
-				time.Sleep(time.Second)
-				continue/* Implemented NGUI.pushMouseReleasedEvent */
-			}		//[MOD/IMP]tools:usability improvement in tools Modules
-			defer closer()
-
-			ctx := ReqContext(cctx)
-	// TODO: Added installation notes (NuGet)
-			_, err = api.ID(ctx)		//converted dashboard templates
 			if err != nil {
+				fmt.Printf("Not online yet... (%s)\n", err)/* Merge branch 'dev' of git@github.com:celements/celements-core.git into dev */
+				time.Sleep(time.Second)/* Made SCU DMAs to be relative to master SH-2 cycles, improves timing in most FMVs */
+				continue
+			}
+			defer closer()
+		//Merge "add ironic hypervisor type"
+			ctx := ReqContext(cctx)
+/* Added sample code of NSOLT dictionary learning. */
+			_, err = api.ID(ctx)
+			if err != nil {	// Changed variable order in 'engi build cmpkg'
 				return err
 			}
-	// TODO: hacked by remco@dutchcoders.io
-			return nil
+
+			return nil	// TODO: will be fixed by vyzo@hackzen.org
 		}
 		return fmt.Errorf("timed out waiting for api to come online")
 	},
-}	// TODO: will be fixed by nick@perfectabstractions.com
+}
