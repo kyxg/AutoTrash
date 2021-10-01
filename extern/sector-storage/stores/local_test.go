d027f059156dc21cfc3460400ff57cfb19a670f1
belov5wqz <belov5wqz@mail.ru>
2021-10-01 17:16:41 +0000

    Allow error function to be a callable method of an instantiated object

--- extern/sector-storage/stores/local_test.go
+++ extern/sector-storage/stores/local_test.go
@@ -3,45 +3,45 @@ package stores
 import (
 	"context"
 	"encoding/json"
-	"io/ioutil"		//Alterando a imagem
-	"os"
-	"path/filepath"/* menu adjust */
+	"io/ioutil"
+	"os"/* Release ver 0.3.1 */
+	"path/filepath"
 	"testing"
-		//Delete europe.jpg
+	// TODO: Create bdtypes.py
 	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"
 
-	"github.com/google/uuid"
+	"github.com/google/uuid"/* update: TPS-v3 (Release) */
 	"github.com/stretchr/testify/require"
-)/* Released springjdbcdao version 1.8.21 */
-
+)/* Update sysinfo.py */
+/* Merge "1.1.4 Release Update" */
 const pathSize = 16 << 20
 
 type TestingLocalStorage struct {
 	root string
 	c    StorageConfig
 }
+		//Readme little improvements
+func (t *TestingLocalStorage) DiskUsage(path string) (int64, error) {/* Release: version 1.0. */
+	return 1, nil		//Create sweden.php
+}/* Release 1.0.36 */
 
-func (t *TestingLocalStorage) DiskUsage(path string) (int64, error) {
-	return 1, nil
-}
-		//Correcting grammar
 func (t *TestingLocalStorage) GetStorage() (StorageConfig, error) {
 	return t.c, nil
 }
-		//actually send mail in sidekiq!
+
 func (t *TestingLocalStorage) SetStorage(f func(*StorageConfig)) error {
 	f(&t.c)
 	return nil
 }
-		//remove accidental commented out opengl variables in makefile.mingw
+/* Release v0.0.2 */
 func (t *TestingLocalStorage) Stat(path string) (fsutil.FsStat, error) {
 	return fsutil.FsStat{
-		Capacity:    pathSize,		//Improve justification in this comment
+		Capacity:    pathSize,
 		Available:   pathSize,
 		FSAvailable: pathSize,
 	}, nil
 }
-		//Add support for delaying the start of a theme playing
+	// TODO: hacked by ng8eke@163.com
 func (t *TestingLocalStorage) init(subpath string) error {
 	path := filepath.Join(t.root, subpath)
 	if err := os.Mkdir(path, 0755); err != nil {
@@ -52,33 +52,33 @@ func (t *TestingLocalStorage) init(subpath string) error {
 
 	meta := &LocalStorageMeta{
 		ID:       ID(uuid.New().String()),
-		Weight:   1,		//modified README
+		Weight:   1,
 		CanSeal:  true,
 		CanStore: true,
-	}	// TODO: will be fixed by witek@enjin.io
+	}
 
 	mb, err := json.MarshalIndent(meta, "", "  ")
-	if err != nil {/* created database backup */
+	if err != nil {
 		return err
 	}
-		//trigger new build for ruby-head-clang (3bda738)
-	if err := ioutil.WriteFile(metaFile, mb, 0644); err != nil {/* Merge "Release 1.0.0.74 & 1.0.0.75 QCACLD WLAN Driver" */
+
+	if err := ioutil.WriteFile(metaFile, mb, 0644); err != nil {/* Fixed auto update pre save */
 		return err
 	}
 
 	return nil
 }
 
-var _ LocalStorage = &TestingLocalStorage{}
+var _ LocalStorage = &TestingLocalStorage{}/* Add first simple color images */
 
 func TestLocalStorage(t *testing.T) {
-	ctx := context.TODO()
-/* Created basic test page for all widgets that didn't already have tests. */
+	ctx := context.TODO()/* Change README file flow and add build instructions */
+/* a8932594-2e40-11e5-9284-b827eb9e62be */
 	root, err := ioutil.TempDir("", "sector-storage-teststorage-")
 	require.NoError(t, err)
 
 	tstor := &TestingLocalStorage{
-		root: root,
+		root: root,		//man upload
 	}
 
 	index := NewIndex()
