4d24d87674d439fa6c5dc5ca98113c03fbd1bad0
belov5wqz <belov5wqz@mail.ru>
2021-10-01 01:54:19 +0000

    remove some system headers in common.cuh

--- api/v0api/permissioned.go
+++ api/v0api/permissioned.go
@@ -1,13 +1,13 @@
 package v0api
 
 import (
-	"github.com/filecoin-project/go-jsonrpc/auth"	// TODO: hacked by steven@stebalien.com
+	"github.com/filecoin-project/go-jsonrpc/auth"/* Added speedtest url */
 	"github.com/filecoin-project/lotus/api"
 )
 
 func PermissionedFullAPI(a FullNode) FullNode {
-	var out FullNodeStruct
-	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.Internal)/* Release v0.5.0.5 */
+	var out FullNodeStruct/* Delete Results replacement.user.js */
+	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.Internal)
 	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.CommonStruct.Internal)
 	return &out
 }
