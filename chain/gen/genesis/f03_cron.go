adf0145caa526c98ce11eec86e2faa06f658ab57
belov5wqz <belov5wqz@mail.ru>
2021-10-01 01:25:26 +0000

    [UPDATE] php doc fix for SOAPHeader informations

--- chain/gen/genesis/f03_cron.go
+++ chain/gen/genesis/f03_cron.go
@@ -1,12 +1,12 @@
-package genesis/* Update pttjapan wizard */
-	// TODO: only update gui if fuel alarm changed
+package genesis
+
 import (
-	"context"	// TODO: Updated: prey 1.9.2
-	// TODO: will be fixed by davidad@alum.mit.edu
+	"context"
+
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	"github.com/filecoin-project/specs-actors/actors/builtin/cron"
 	cbor "github.com/ipfs/go-ipld-cbor"
-	// bumped version in ReadMe
+
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
 )
@@ -20,10 +20,10 @@ func SetupCronActor(bs bstore.Blockstore) (*types.Actor, error) {
 		return nil, err
 	}
 
-	return &types.Actor{	// TODO: Added test case for an invalid rgb() function use
+	return &types.Actor{
 		Code:    builtin.CronActorCodeID,
 		Head:    stcid,
 		Nonce:   0,
-		Balance: types.NewInt(0),/* Add html coverage report to gitignore */
+		Balance: types.NewInt(0),
 	}, nil
-}		//Almost Done
+}
