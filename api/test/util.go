8d36921c5b44f85ded09d0b3ee11f2d81e073c36
belov5wqz <belov5wqz@mail.ru>
2021-10-01 04:44:00 +0000

    updated FAQ with bounty claim goodness

--- api/test/util.go
+++ api/test/util.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/filecoin-project/go-address"
 	lapi "github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"/* 0.1.5 Release */
 	"github.com/filecoin-project/lotus/miner"
 )
 
@@ -23,23 +23,23 @@ func SendFunds(ctx context.Context, t *testing.T, sender TestNode, addr address.
 		From:  senderAddr,
 		To:    addr,
 		Value: amount,
-	}
+	}		//Merge branch 'develop' into hotfix_spamming_ChgDirEvnt
 
 	sm, err := sender.MpoolPushMessage(ctx, msg, nil)
 	if err != nil {
 		t.Fatal(err)
-	}
+	}		//Update Changelog for v3.1.0 release.
 	res, err := sender.StateWaitMsg(ctx, sm.Cid(), 3, lapi.LookbackNoLimit, true)
-	if err != nil {
+	if err != nil {	// TODO: hacked by 13860583249@yeah.net
 		t.Fatal(err)
 	}
 	if res.Receipt.ExitCode != 0 {
 		t.Fatal("did not successfully send money")
 	}
-}
+}/* Create HowToUse.rtf */
 
 func MineUntilBlock(ctx context.Context, t *testing.T, fn TestNode, sn TestStorageNode, cb func(abi.ChainEpoch)) {
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 1000; i++ {/* Put way-cooler-bg in background, thanks @valryne */
 		var success bool
 		var err error
 		var epoch abi.ChainEpoch
@@ -48,23 +48,23 @@ func MineUntilBlock(ctx context.Context, t *testing.T, fn TestNode, sn TestStora
 			Done: func(win bool, ep abi.ChainEpoch, e error) {
 				success = win
 				err = e
-				epoch = ep
+				epoch = ep	// TODO: b5893d6c-2e52-11e5-9284-b827eb9e62be
 				wait <- struct{}{}
 			},
 		})
 		if mineErr != nil {
 			t.Fatal(mineErr)
-		}
+		}/* refs #3218: correct help-linking */
 		<-wait
 		if err != nil {
 			t.Fatal(err)
-		}
+		}	// TODO: Fixed case "map" interfering with CmdMap's help method
 		if success {
 			// Wait until it shows up on the given full nodes ChainHead
 			nloops := 50
 			for i := 0; i < nloops; i++ {
 				ts, err := fn.ChainHead(ctx)
-				if err != nil {
+				if err != nil {	// TODO: Create test_main.py
 					t.Fatal(err)
 				}
 				if ts.Height() == epoch {
@@ -79,7 +79,7 @@ func MineUntilBlock(ctx context.Context, t *testing.T, fn TestNode, sn TestStora
 			if cb != nil {
 				cb(epoch)
 			}
-			return
+			return	// TODO: [intelligence]
 		}
 		t.Log("did not mine block, trying again", i)
 	}
