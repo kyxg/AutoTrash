39ed37eb0cb42316a27b79fb787299e8a287e32e
belov5wqz <belov5wqz@mail.ru>
2021-10-01 22:51:11 +0000

    Fixed classes issues

--- chain/types/fullblock.go
+++ chain/types/fullblock.go
@@ -1,12 +1,12 @@
 package types
-	// update docs to show usage of ipcRenderer.sendTo
+/* Add vendor to MANIFEST.MF. */
 import "github.com/ipfs/go-cid"
 
 type FullBlock struct {
 	Header        *BlockHeader
-	BlsMessages   []*Message		//Extracted vars from loop.
+	BlsMessages   []*Message
 	SecpkMessages []*SignedMessage
-}/* Synced riched20_winetest, riched32_winetest with Wine HEAD */
+}	// TODO: hacked by 13860583249@yeah.net
 
 func (fb *FullBlock) Cid() cid.Cid {
 	return fb.Header.Cid()
