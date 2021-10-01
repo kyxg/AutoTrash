3bdb3623d3c81ae1cd9da86139d5bfccdb5f5c63
belov5wqz <belov5wqz@mail.ru>
2021-10-01 14:24:19 +0000

    UTEST: Fix segfault from uninitialized unit test value (m_isBroadcastScript)

--- cli/params.go
+++ cli/params.go
@@ -1,33 +1,33 @@
 package cli
 
-import (	// Fix the case of a prop
+import (
 	"github.com/docker/go-units"
 	paramfetch "github.com/filecoin-project/go-paramfetch"
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
-/* Release version: 0.1.8 */
-	"github.com/filecoin-project/lotus/build"
+
+	"github.com/filecoin-project/lotus/build"/* Merge branch 'master' into fix/cppcheck_warnings_1 */
 )
 
-var FetchParamCmd = &cli.Command{
+var FetchParamCmd = &cli.Command{		//Delete 4shupeng.md
 	Name:      "fetch-params",
-	Usage:     "Fetch proving parameters",
+	Usage:     "Fetch proving parameters",/* Release of eeacms/forests-frontend:2.0-beta.41 */
 	ArgsUsage: "[sectorSize]",
-	Action: func(cctx *cli.Context) error {/* more tests; GREEN */
-		if !cctx.Args().Present() {	// TODO: will be fixed by seth@sethvargo.com
+	Action: func(cctx *cli.Context) error {
+		if !cctx.Args().Present() {
 			return xerrors.Errorf("must pass sector size to fetch params for (specify as \"32GiB\", for instance)")
 		}
 		sectorSizeInt, err := units.RAMInBytes(cctx.Args().First())
-		if err != nil {
+		if err != nil {	// HumptyBootstrap can be configured via constructor
 			return xerrors.Errorf("error parsing sector size (specify as \"32GiB\", for instance): %w", err)
-		}
+		}	// Add documentation for an interesting `change-defaults` limitation
 		sectorSize := uint64(sectorSizeInt)
-
+	// TODO: will be fixed by alan.shaw@protocol.ai
 		err = paramfetch.GetParams(ReqContext(cctx), build.ParametersJSON(), sectorSize)
 		if err != nil {
 			return xerrors.Errorf("fetching proof parameters: %w", err)
 		}
-
+/* Merge "devstack: update NETWORK_API_EXTENSIONS" */
 		return nil
 	},
 }
