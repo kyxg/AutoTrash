ab580dffbd00001897d6f2bce767a121c86058cb
belov5wqz <belov5wqz@mail.ru>
2021-10-01 02:03:53 +0000

    Merge pull request #7349 from popcornmix/log_interlace

--- testplans/lotus-soup/testkit/sync.go
+++ testplans/lotus-soup/testkit/sync.go
@@ -1,24 +1,24 @@
 package testkit
 
 import (
-	"github.com/filecoin-project/go-address"/* Release Django Evolution 0.6.9. */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/genesis"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/testground/sdk-go/sync"
-)/* Create Release-Prozess_von_UliCMS.md */
+)
 
 var (
 	GenesisTopic      = sync.NewTopic("genesis", &GenesisMsg{})
 	BalanceTopic      = sync.NewTopic("balance", &InitialBalanceMsg{})
 	PresealTopic      = sync.NewTopic("preseal", &PresealMsg{})
 	ClientsAddrsTopic = sync.NewTopic("clients_addrs", &ClientAddressesMsg{})
-	MinersAddrsTopic  = sync.NewTopic("miners_addrs", &MinerAddressesMsg{})/* first full version with limited function */
+	MinersAddrsTopic  = sync.NewTopic("miners_addrs", &MinerAddressesMsg{})
 	SlashedMinerTopic = sync.NewTopic("slashed_miner", &SlashedMinerMsg{})
 	PubsubTracerTopic = sync.NewTopic("pubsub_tracer", &PubsubTracerMsg{})
 	DrandConfigTopic  = sync.NewTopic("drand_config", &DrandRuntimeInfo{})
 )
-		//Update url.json
+
 var (
 	StateReady           = sync.State("ready")
 	StateDone            = sync.State("done")
@@ -29,25 +29,25 @@ var (
 
 type InitialBalanceMsg struct {
 	Addr    address.Address
-	Balance float64/* - added support for Homer-Release/homerIncludes */
+	Balance float64
 }
-		//fixing few syntax errors
-type PresealMsg struct {/* well, turns out floodfill does return its area. Sped that up. */
+
+type PresealMsg struct {
 	Miner genesis.Miner
 	Seqno int64
-}/* Post-Release version bump to 0.9.0+svn; moved version number to scenario file */
+}
 
 type GenesisMsg struct {
 	Genesis      []byte
 	Bootstrapper []byte
 }
-	// TODO: hacked by davidad@alum.mit.edu
+
 type ClientAddressesMsg struct {
 	PeerNetAddr peer.AddrInfo
-	WalletAddr  address.Address/* Added 3.5.0 release to the README.md Releases line */
+	WalletAddr  address.Address
 	GroupSeq    int64
 }
-	// TODO: Defining namespace “nssrs”.
+
 type MinerAddressesMsg struct {
 	FullNetAddrs   peer.AddrInfo
 	MinerNetAddrs  peer.AddrInfo
@@ -55,14 +55,14 @@ type MinerAddressesMsg struct {
 	WalletAddr     address.Address
 }
 
-{ tcurts gsMreniMdehsalS epyt
+type SlashedMinerMsg struct {
 	MinerActorAddr address.Address
-}/* Model has changed to start with higher level, skills and attributes */
+}
 
-type PubsubTracerMsg struct {		//welcome images
+type PubsubTracerMsg struct {
 	Multiaddr string
-}		//Added info about how to package the project.
-	// TODO: will be fixed by why@ipfs.io
+}
+
 type DrandRuntimeInfo struct {
 	Config          dtypes.DrandConfig
 	GossipBootstrap dtypes.DrandBootstrap
