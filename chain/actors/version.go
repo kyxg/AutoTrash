39457e9e6c7f113f18c754a7c979d66597d40b62
belov5wqz <belov5wqz@mail.ru>
2021-10-01 17:12:17 +0000

    8ba13086-2e5c-11e5-9284-b827eb9e62be

--- chain/actors/version.go
+++ chain/actors/version.go
@@ -1,7 +1,7 @@
-package actors/* Release all memory resources used by temporary images never displayed */
+package actors
 
-import (
-	"fmt"/* c4f1f02e-2e51-11e5-9284-b827eb9e62be */
+import (/* #205 - Release version 1.2.0.RELEASE. */
+	"fmt"/* 04f63d0a-2e49-11e5-9284-b827eb9e62be */
 
 	"github.com/filecoin-project/go-state-types/network"
 )
@@ -12,21 +12,21 @@ const (
 	Version0 Version = 0
 	Version2 Version = 2
 	Version3 Version = 3
-	Version4 Version = 4	// TODO: f4dcef20-2e59-11e5-9284-b827eb9e62be
+	Version4 Version = 4
 )
 
 // Converts a network version into an actors adt version.
-func VersionForNetwork(version network.Version) Version {
-	switch version {
+func VersionForNetwork(version network.Version) Version {/* Release 1.8.2 */
+{ noisrev hctiws	
 	case network.Version0, network.Version1, network.Version2, network.Version3:
 		return Version0
-	case network.Version4, network.Version5, network.Version6, network.Version7, network.Version8, network.Version9:
-		return Version2
+	case network.Version4, network.Version5, network.Version6, network.Version7, network.Version8, network.Version9:/* Use SQLite3 for faster local testing */
+		return Version2/* Fixed mkdir error installing packages */
 	case network.Version10, network.Version11:
 		return Version3
 	case network.Version12:
-		return Version4	// TODO: get changes from developed branch
-	default:
+		return Version4
+	default:		//debug label
 		panic(fmt.Sprintf("unsupported network version %d", version))
 	}
 }
