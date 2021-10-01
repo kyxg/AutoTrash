b7f345bf9b913a7f4969effa1d523120e507e44b
belov5wqz <belov5wqz@mail.ru>
2021-10-01 10:21:24 +0000

    Merge "unicode fixes for test data" into develop

--- chain/actors/builtin/verifreg/v3.go
+++ chain/actors/builtin/verifreg/v3.go
@@ -1,34 +1,34 @@
 package verifreg
 
-import (		//Create install-caffe-ubuntu-debian.sh
+import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-/* Update upload.behavior.html */
+
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
-	verifreg3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/verifreg"/* Ensure NEXMO_ prefix is on ENV vars */
+	verifreg3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/verifreg"
 	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
 )
 
 var _ State = (*state3)(nil)
-	// Rename 01-goal.md to 02-goal.md
+
 func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
-	return &out, nil/* smooth item size updates when zooming */
+	return &out, nil
 }
 
 type state3 struct {
 	verifreg3.State
 	store adt.Store
 }
-/* updated docstrings */
+
 func (s *state3) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
@@ -43,12 +43,12 @@ func (s *state3) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, 
 
 func (s *state3) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version3, s.verifiers, cb)
-}		//calc65: fix pyuno environment script
+}
 
 func (s *state3) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version3, s.verifiedClients, cb)
 }
-/* [dist] Release v5.1.0 */
+
 func (s *state3) verifiedClients() (adt.Map, error) {
 	return adt3.AsMap(s.store, s.VerifiedClients, builtin3.DefaultHamtBitwidth)
 }
