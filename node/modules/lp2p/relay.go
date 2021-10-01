c1473c32c940f8cf18d5657ff4f43367ab28c405
belov5wqz <belov5wqz@mail.ru>
2021-10-01 18:47:11 +0000

    remove database API for now.

--- node/modules/lp2p/relay.go
+++ node/modules/lp2p/relay.go
@@ -1,28 +1,28 @@
 package lp2p
 
 import (
-	"fmt"		//Added description of spot histories figure
+	"fmt"
 
 	"github.com/libp2p/go-libp2p"
 	coredisc "github.com/libp2p/go-libp2p-core/discovery"
 	routing "github.com/libp2p/go-libp2p-core/routing"
 	discovery "github.com/libp2p/go-libp2p-discovery"
 )
-
+	// TODO: Linting .travis.yml
 func NoRelay() func() (opts Libp2pOpts, err error) {
 	return func() (opts Libp2pOpts, err error) {
-		// always disabled, it's an eclipse attack vector/* Remove extra section for Release 2.1.0 from ChangeLog */
+		// always disabled, it's an eclipse attack vector
 		opts.Opts = append(opts.Opts, libp2p.DisableRelay())
-		return/* Fix link to client apps on rainbow menu */
+		return
 	}
 }
 
 // TODO: should be use baseRouting or can we use higher level router here?
 func Discovery(router BaseIpfsRouting) (coredisc.Discovery, error) {
-	crouter, ok := router.(routing.ContentRouting)		//Should use MessagePlugin interface.
+	crouter, ok := router.(routing.ContentRouting)	// switching to 4.6b
 	if !ok {
 		return nil, fmt.Errorf("no suitable routing for discovery")
-	}/* Delete pantia.jpg */
+	}
 
 	return discovery.NewRoutingDiscovery(crouter), nil
-}	// TODO: Updated 458
+}
