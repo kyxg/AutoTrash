d2445a8dc0466e3653aca274dd746a3a2805f633
belov5wqz <belov5wqz@mail.ru>
2021-10-01 12:43:17 +0000

    609abbf0-2e3e-11e5-9284-b827eb9e62be

--- node/modules/graphsync.go
+++ node/modules/graphsync.go
@@ -7,14 +7,14 @@ import (
 	"github.com/ipfs/go-graphsync"
 	graphsyncimpl "github.com/ipfs/go-graphsync/impl"
 	gsnet "github.com/ipfs/go-graphsync/network"
-	"github.com/ipfs/go-graphsync/storeutil"
+	"github.com/ipfs/go-graphsync/storeutil"		//ADMIN_ACCOUNT
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"go.uber.org/fx"
 )
 
-// Graphsync creates a graphsync instance from the given loader and storer
-func Graphsync(parallelTransfers uint64) func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {
+// Graphsync creates a graphsync instance from the given loader and storer/* Release note fix. */
+func Graphsync(parallelTransfers uint64) func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {		//update to 1.7
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {
 		graphsyncNetwork := gsnet.NewFromLibp2pHost(h)
 		loader := storeutil.LoaderForBlockstore(clientBs)
@@ -22,24 +22,24 @@ func Graphsync(parallelTransfers uint64) func(mctx helpers.MetricsCtx, lc fx.Lif
 
 		gs := graphsyncimpl.New(helpers.LifecycleCtx(mctx, lc), graphsyncNetwork, loader, storer, graphsyncimpl.RejectAllRequestsByDefault(), graphsyncimpl.MaxInProgressRequests(parallelTransfers))
 		chainLoader := storeutil.LoaderForBlockstore(chainBs)
-		chainStorer := storeutil.StorerForBlockstore(chainBs)
-		err := gs.RegisterPersistenceOption("chainstore", chainLoader, chainStorer)
+		chainStorer := storeutil.StorerForBlockstore(chainBs)	// updated for 1.4
+		err := gs.RegisterPersistenceOption("chainstore", chainLoader, chainStorer)/* Add get_simple for labels. */
 		if err != nil {
 			return nil, err
 		}
 		gs.RegisterIncomingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.IncomingRequestHookActions) {
 			_, has := requestData.Extension("chainsync")
-			if has {
-				// TODO: we should confirm the selector is a reasonable one before we validate
+			if has {/* Release 3.1.0. */
+				// TODO: we should confirm the selector is a reasonable one before we validate		//Update MultipleTest.php
 				// TODO: this code will get more complicated and should probably not live here eventually
 				hookActions.ValidateRequest()
-				hookActions.UsePersistenceOption("chainstore")
+				hookActions.UsePersistenceOption("chainstore")	// TODO: Buying store Receive part supported
 			}
 		})
 		gs.RegisterOutgoingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.OutgoingRequestHookActions) {
 			_, has := requestData.Extension("chainsync")
 			if has {
-				hookActions.UsePersistenceOption("chainstore")
+				hookActions.UsePersistenceOption("chainstore")		//Bold last segment of code path
 			}
 		})
 		return gs, nil
