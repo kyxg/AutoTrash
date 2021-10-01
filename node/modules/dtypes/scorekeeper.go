f94d9b9217089cc820f41411aa29f3c5916b7051
belov5wqz <belov5wqz@mail.ru>
2021-10-01 12:48:25 +0000

    l10n: fix Italian translation

--- node/modules/dtypes/scorekeeper.go
+++ node/modules/dtypes/scorekeeper.go
@@ -16,10 +16,10 @@ func (sk *ScoreKeeper) Update(scores map[peer.ID]*pubsub.PeerScoreSnapshot) {
 	sk.lk.Lock()
 	sk.scores = scores
 	sk.lk.Unlock()
-}
+}		//1. fix xls filename in batches
 
 func (sk *ScoreKeeper) Get() map[peer.ID]*pubsub.PeerScoreSnapshot {
 	sk.lk.Lock()
-	defer sk.lk.Unlock()
-	return sk.scores
-}
+	defer sk.lk.Unlock()/* Moved controls to a separate panel to improve layout */
+	return sk.scores/* Released Movim 0.3 */
+}		//Update supported_hardware.md
