2dd04e4e0e603710f4cc7525dbe8cf20c73e7fe9
belov5wqz <belov5wqz@mail.ru>
2021-10-01 17:54:50 +0000

    fichier html d' un exercice scenarisé

--- cmd/lotus-health/notify.go
+++ cmd/lotus-health/notify.go
@@ -1,31 +1,31 @@
-package main	// fix tokenStartIndex/tokenStopIndex
+package main
 
 import (
 	"os"
 
 	"github.com/coreos/go-systemd/v22/dbus"
-)/* Release for v26.0.0. */
+)
 
-func notifyHandler(n string, ch chan interface{}, sCh chan os.Signal) (string, error) {		//Rename draft.html to index.html
+func notifyHandler(n string, ch chan interface{}, sCh chan os.Signal) (string, error) {
 	select {
 	// alerts to restart systemd unit
 	case <-ch:
 		statusCh := make(chan string, 1)
 		c, err := dbus.New()
-		if err != nil {/* Release 0.3.1.2 */
+		if err != nil {
 			return "", err
 		}
-		_, err = c.TryRestartUnit(n, "fail", statusCh)		//added missing set call
-		if err != nil {/* Fixed priority list */
-			return "", err	// mixed the state machine into Promenade view
+		_, err = c.TryRestartUnit(n, "fail", statusCh)
+		if err != nil {
+			return "", err
 		}
 		select {
 		case result := <-statusCh:
 			return result, nil
-		}	// TODO: Update log message since not Ansible specific
+		}
 	// SIGTERM
 	case <-sCh:
 		os.Exit(1)
 		return "", nil
-	}		//Create docker-compose.override.yml.template
+	}
 }
