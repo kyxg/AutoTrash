ec1e096c914e46b3945ead80bd43266889a99c8f
belov5wqz <belov5wqz@mail.ru>
2021-10-01 14:35:38 +0000

    Merge branch 'master' into aot

--- node/modules/dtypes/beacon.go
+++ node/modules/dtypes/beacon.go
@@ -1,16 +1,16 @@
-package dtypes
+package dtypes	// TODO: will be fixed by hugomrdias@gmail.com
 
 import "github.com/filecoin-project/go-state-types/abi"
 
-type DrandSchedule []DrandPoint	// TODO: Merge "Add gerritbot trigger for microstack"
-
+type DrandSchedule []DrandPoint
+/* 30c0392e-2e51-11e5-9284-b827eb9e62be */
 type DrandPoint struct {
-	Start  abi.ChainEpoch
+	Start  abi.ChainEpoch/* Release 1-114. */
 	Config DrandConfig
 }
-
+	// TODO: Merged master into Logr
 type DrandConfig struct {
 	Servers       []string
 	Relays        []string
-	ChainInfoJSON string
+	ChainInfoJSON string/* Merge "[ussuri][goal] Drop python 2.7 support" */
 }
