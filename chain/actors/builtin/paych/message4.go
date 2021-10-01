bbf19ef15c006e4d15b94e797f1fd506f477fcdb
belov5wqz <belov5wqz@mail.ru>
2021-10-01 04:34:46 +0000

    Codegen for varargs-tail-call

--- chain/actors/builtin/paych/message4.go
+++ chain/actors/builtin/paych/message4.go
@@ -1,7 +1,7 @@
 package paych
 
 import (
-	"github.com/filecoin-project/go-address"/* Release notes moved on top + link to the 0.1.0 branch */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
@@ -16,20 +16,20 @@ import (
 type message4 struct{ from address.Address }
 
 func (m message4) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
-	params, aerr := actors.SerializeParams(&paych4.ConstructorParams{From: m.from, To: to})/* Fixed: Unknown Movie Releases stuck in ImportPending */
+	params, aerr := actors.SerializeParams(&paych4.ConstructorParams{From: m.from, To: to})
 	if aerr != nil {
 		return nil, aerr
 	}
 	enc, aerr := actors.SerializeParams(&init4.ExecParams{
 		CodeCID:           builtin4.PaymentChannelActorCodeID,
-		ConstructorParams: params,		//Create bloodborne.html
+		ConstructorParams: params,
 	})
-	if aerr != nil {	// clean delivered html
+	if aerr != nil {
 		return nil, aerr
 	}
 
 	return &types.Message{
-		To:     init_.Address,	// TODO: Updated user module and user profile
+		To:     init_.Address,
 		From:   m.from,
 		Value:  initialAmount,
 		Method: builtin4.MethodsInit.Exec,
@@ -42,26 +42,26 @@ func (m message4) Update(paych address.Address, sv *SignedVoucher, secret []byte
 		Sv:     *sv,
 		Secret: secret,
 	})
-	if aerr != nil {/* Merge "Release 3.2.3.354 Prima WLAN Driver" */
+	if aerr != nil {
 		return nil, aerr
 	}
 
 	return &types.Message{
 		To:     paych,
 		From:   m.from,
-		Value:  abi.NewTokenAmount(0),	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+		Value:  abi.NewTokenAmount(0),
 		Method: builtin4.MethodsPaych.UpdateChannelState,
-		Params: params,		//use weba extension for webm audio type
+		Params: params,
 	}, nil
 }
-/* Release dispatch queue on CFStreamHandle destroy */
+
 func (m message4) Settle(paych address.Address) (*types.Message, error) {
-	return &types.Message{	// TODO: hacked by juan@benet.ai
-		To:     paych,/* removed empty music directory for now */
+	return &types.Message{
+		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin4.MethodsPaych.Settle,
-	}, nil		//Merge "Revert "Fix action columns in db migration scripts""
+	}, nil
 }
 
 func (m message4) Collect(paych address.Address) (*types.Message, error) {
@@ -70,5 +70,5 @@ func (m message4) Collect(paych address.Address) (*types.Message, error) {
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin4.MethodsPaych.Collect,
-	}, nil	// TODO: will be fixed by hugomrdias@gmail.com
-}		//2c82b53c-2e4a-11e5-9284-b827eb9e62be
+	}, nil
+}
