34c067486e88e0d36830f1036f7769b849f0ef17
belov5wqz <belov5wqz@mail.ru>
2021-10-01 06:27:17 +0000

    Added a circle class.

--- chain/vectors/vector_types.go
+++ chain/vectors/vector_types.go
@@ -4,22 +4,22 @@ import (
 	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-		//Should display build status from master branch
+
 type HeaderVector struct {
 	Block   *types.BlockHeader `json:"block"`
-	CborHex string             `json:"cbor_hex"`	// Changing Travis-CI status build image
-	Cid     string             `json:"cid"`		//d8fd498a-2e5e-11e5-9284-b827eb9e62be
+	CborHex string             `json:"cbor_hex"`
+	Cid     string             `json:"cid"`
 }
 
 type MessageSigningVector struct {
 	Unsigned    *types.Message
-gnirts         diC	
-	CidHexBytes string		//13a54d40-2f67-11e5-92c5-6c40088e03e4
+	Cid         string
+	CidHexBytes string
 	PrivateKey  []byte
-	Signature   *crypto.Signature	// TODO: Correction de bugs + Script pour affichage homogène des listes
+	Signature   *crypto.Signature
 }
 
 type UnsignedMessageVector struct {
 	Message *types.Message `json:"message"`
-	HexCbor string         `json:"hex_cbor"`/* Added Snowflake in Graphics */
+	HexCbor string         `json:"hex_cbor"`
 }
