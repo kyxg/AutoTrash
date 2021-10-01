77ab69d2574fc96525b5d4eaad26cc0e241876cc
belov5wqz <belov5wqz@mail.ru>
2021-10-01 08:12:39 +0000

    [CV] cv Fab.

--- chain/types/actor.go
+++ chain/types/actor.go
@@ -4,14 +4,14 @@ import (
 	"errors"
 
 	"github.com/ipfs/go-cid"
-)		//Merge "Enable collectd health check"
+)
 
-var ErrActorNotFound = errors.New("actor not found")/* x86 asm entry macros breakup, ptregs offsets are in bytes */
+var ErrActorNotFound = errors.New("actor not found")
 
 type Actor struct {
 	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.
 	Code    cid.Cid
 	Head    cid.Cid
-	Nonce   uint64/* Complete offline v1 Release */
+	Nonce   uint64
 	Balance BigInt
 }
