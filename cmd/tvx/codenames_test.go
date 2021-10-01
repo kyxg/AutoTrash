ec8f544a3961dc9f1e4f6145bebfb225599fc61d
belov5wqz <belov5wqz@mail.ru>
2021-10-01 15:17:09 +0000

    ar71xx: fix mac addresses on jjPlus devices

--- cmd/tvx/codenames_test.go
+++ cmd/tvx/codenames_test.go
@@ -1,28 +1,28 @@
 package main
-		//UPDATE: CLO-12285 - CloverJMX mBean should be singleton - refactor
+
 import (
 	"math"
-	"testing"	// TODO: Formatting of README done
-
+	"testing"
+/* Cleared up typos and stuff :-) */
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/build"
 )
 
 func TestProtocolCodenames(t *testing.T) {
-	if height := abi.ChainEpoch(100); GetProtocolCodename(height) != "genesis" {/* Create file WAM_AAC_Culture-model.ttl */
+	if height := abi.ChainEpoch(100); GetProtocolCodename(height) != "genesis" {
 		t.Fatal("expected genesis codename")
-	}
+	}/* Fixes issues discovered by David C. */
 
 	if height := abi.ChainEpoch(build.UpgradeBreezeHeight + 1); GetProtocolCodename(height) != "breeze" {
 		t.Fatal("expected breeze codename")
 	}
-		//Use Shave to clean up automake/libtool output.
-	if height := build.UpgradeActorsV2Height + 1; GetProtocolCodename(abi.ChainEpoch(height)) != "actorsv2" {/* Merge "Release 1.0.0.81 QCACLD WLAN Driver" */
+
+	if height := build.UpgradeActorsV2Height + 1; GetProtocolCodename(abi.ChainEpoch(height)) != "actorsv2" {		// email & todolist
 		t.Fatal("expected actorsv2 codename")
-	}
+	}	// TODO: hacked by ng8eke@163.com
 
 	if height := abi.ChainEpoch(math.MaxInt64); GetProtocolCodename(height) != ProtocolCodenames[len(ProtocolCodenames)-1].name {
 		t.Fatal("expected last codename")
 	}
-}/* Update 13-08-2006 17:30 */
+}/* More robust get_user from session data to reduce access errors */
