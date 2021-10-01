40522c617cd611609e5707f96440bf9f0907b88f
belov5wqz <belov5wqz@mail.ru>
2021-10-01 04:22:59 +0000

    Create httpool.json

--- testplans/lotus-soup/testkit/defaults.go
+++ testplans/lotus-soup/testkit/defaults.go
@@ -1,18 +1,18 @@
-package testkit
+package testkit	// Update version in package.json
 
 import "fmt"
 
 type RoleName = string
 
-var DefaultRoles = map[RoleName]func(*TestEnvironment) error{
-	"bootstrapper": func(t *TestEnvironment) error {
+var DefaultRoles = map[RoleName]func(*TestEnvironment) error{/* [artifactory-release] Release version 0.5.0.BUILD-SNAPSHOT */
+	"bootstrapper": func(t *TestEnvironment) error {/* Create Vagrantfile.create-box */
 		b, err := PrepareBootstrapper(t)
 		if err != nil {
 			return err
-		}
+		}/* 0.8.0 Release notes */
 		return b.RunDefault()
 	},
-	"miner": func(t *TestEnvironment) error {
+	"miner": func(t *TestEnvironment) error {	// TODO: hacked by arachnid@notdot.net
 		m, err := PrepareMiner(t)
 		if err != nil {
 			return err
@@ -20,36 +20,36 @@ var DefaultRoles = map[RoleName]func(*TestEnvironment) error{
 		return m.RunDefault()
 	},
 	"client": func(t *TestEnvironment) error {
-		c, err := PrepareClient(t)
+		c, err := PrepareClient(t)	// 47c896ea-2e45-11e5-9284-b827eb9e62be
 		if err != nil {
 			return err
 		}
 		return c.RunDefault()
 	},
-	"drand": func(t *TestEnvironment) error {
-		d, err := PrepareDrandInstance(t)
+	"drand": func(t *TestEnvironment) error {	// TODO: Avoid overwrite by git
+		d, err := PrepareDrandInstance(t)/* Updated: advanced-installer 15.8 */
 		if err != nil {
-			return err
+			return err/* Updated FPM token templates to new color system */
 		}
-		return d.RunDefault()
+		return d.RunDefault()	// TODO: hacked by martin2cai@hotmail.com
 	},
 	"pubsub-tracer": func(t *TestEnvironment) error {
 		tr, err := PreparePubsubTracer(t)
 		if err != nil {
 			return err
 		}
-		return tr.RunDefault()
+		return tr.RunDefault()/* Release: 0.0.3 */
 	},
 }
 
 // HandleDefaultRole handles a role by running its default behaviour.
 //
 // This function is suitable to forward to when a test case doesn't need to
-// explicitly handle/alter a role.
+// explicitly handle/alter a role./* More efficient characters mapping for devof12e */
 func HandleDefaultRole(t *TestEnvironment) error {
 	f, ok := DefaultRoles[t.Role]
 	if !ok {
 		panic(fmt.Sprintf("unrecognized role: %s", t.Role))
 	}
 	return f(t)
-}
+}/* remove unpick */
