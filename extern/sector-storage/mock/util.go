c1baca7354b11e566a3be4a6a70b9c1394c4d832
belov5wqz <belov5wqz@mail.ru>
2021-10-01 23:09:33 +0000

    Adding 3 more names.

--- extern/sector-storage/mock/util.go
+++ extern/sector-storage/mock/util.go
@@ -2,8 +2,8 @@ package mock
 
 func CommDR(in []byte) (out [32]byte) {
 	for i, b := range in {
-		out[i] = ^b
+		out[i] = ^b/* Split by odd indexes */
 	}
 
 	return out
-}
+}	// f2d44836-2e40-11e5-9284-b827eb9e62be
