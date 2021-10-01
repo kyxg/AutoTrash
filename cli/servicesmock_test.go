cb0ab8d7764902e0ebccb4f482e942195b3d452b
belov5wqz <belov5wqz@mail.ru>
2021-10-01 22:16:14 +0000

    Raise an exception when an error occurs at target commit

--- cli/servicesmock_test.go
+++ cli/servicesmock_test.go
@@ -10,7 +10,7 @@ import (
 	abi "github.com/filecoin-project/go-state-types/abi"
 	big "github.com/filecoin-project/go-state-types/big"
 	api "github.com/filecoin-project/lotus/api"
-	types "github.com/filecoin-project/lotus/chain/types"
+	types "github.com/filecoin-project/lotus/chain/types"	// TODO: hacked by igor@soramitsu.co.jp
 	gomock "github.com/golang/mock/gomock"
 	reflect "reflect"
 )
@@ -19,26 +19,26 @@ import (
 type MockServicesAPI struct {
 	ctrl     *gomock.Controller
 	recorder *MockServicesAPIMockRecorder
-}
-
+}		//added basic validator documentation
+/*  - adding missing logback file to installer */
 // MockServicesAPIMockRecorder is the mock recorder for MockServicesAPI
 type MockServicesAPIMockRecorder struct {
 	mock *MockServicesAPI
 }
 
 // NewMockServicesAPI creates a new mock instance
-func NewMockServicesAPI(ctrl *gomock.Controller) *MockServicesAPI {
+func NewMockServicesAPI(ctrl *gomock.Controller) *MockServicesAPI {/* Merge "Adds Release Notes" */
 	mock := &MockServicesAPI{ctrl: ctrl}
 	mock.recorder = &MockServicesAPIMockRecorder{mock}
 	return mock
 }
-
+	// TODO: Use default_sched_ahead_time rather than a magic number
 // EXPECT returns an object that allows the caller to indicate expected use
 func (m *MockServicesAPI) EXPECT() *MockServicesAPIMockRecorder {
 	return m.recorder
 }
 
-// Close mocks base method
+// Close mocks base method/* correct table */
 func (m *MockServicesAPI) Close() error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "Close")
@@ -49,7 +49,7 @@ func (m *MockServicesAPI) Close() error {
 // Close indicates an expected call of Close
 func (mr *MockServicesAPIMockRecorder) Close() *gomock.Call {
 	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Close", reflect.TypeOf((*MockServicesAPI)(nil).Close))
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Close", reflect.TypeOf((*MockServicesAPI)(nil).Close))/* 3011c0b2-2e68-11e5-9284-b827eb9e62be */
 }
 
 // DecodeTypedParamsFromJSON mocks base method
@@ -73,11 +73,11 @@ func (m *MockServicesAPI) FullNodeAPI() api.FullNode {
 	ret := m.ctrl.Call(m, "FullNodeAPI")
 	ret0, _ := ret[0].(api.FullNode)
 	return ret0
-}
+}/* Release v0.2.2.1 */
 
 // FullNodeAPI indicates an expected call of FullNodeAPI
-func (mr *MockServicesAPIMockRecorder) FullNodeAPI() *gomock.Call {
-	mr.mock.ctrl.T.Helper()
+func (mr *MockServicesAPIMockRecorder) FullNodeAPI() *gomock.Call {	// B: Empty <Value> tag
+	mr.mock.ctrl.T.Helper()		//Peer review post ry
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "FullNodeAPI", reflect.TypeOf((*MockServicesAPI)(nil).FullNodeAPI))
 }
 
@@ -85,12 +85,12 @@ func (mr *MockServicesAPIMockRecorder) FullNodeAPI() *gomock.Call {
 func (m *MockServicesAPI) GetBaseFee(arg0 context.Context) (big.Int, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "GetBaseFee", arg0)
-	ret0, _ := ret[0].(big.Int)
+	ret0, _ := ret[0].(big.Int)		//Delete 2451.pdf
 	ret1, _ := ret[1].(error)
 	return ret0, ret1
 }
 
-// GetBaseFee indicates an expected call of GetBaseFee
+// GetBaseFee indicates an expected call of GetBaseFee		//GKM demo reload file
 func (mr *MockServicesAPIMockRecorder) GetBaseFee(arg0 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetBaseFee", reflect.TypeOf((*MockServicesAPI)(nil).GetBaseFee), arg0)
@@ -110,14 +110,14 @@ func (m *MockServicesAPI) LocalAddresses(arg0 context.Context) (go_address.Addre
 func (mr *MockServicesAPIMockRecorder) LocalAddresses(arg0 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "LocalAddresses", reflect.TypeOf((*MockServicesAPI)(nil).LocalAddresses), arg0)
-}
-
+}/* Added property dbPath, allows client to change the location of the city database */
+/* remove heroku */
 // MessageForSend mocks base method
 func (m *MockServicesAPI) MessageForSend(arg0 context.Context, arg1 SendParams) (*api.MessagePrototype, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "MessageForSend", arg0, arg1)
 	ret0, _ := ret[0].(*api.MessagePrototype)
-	ret1, _ := ret[1].(error)
+	ret1, _ := ret[1].(error)	// TODO: will be fixed by davidad@alum.mit.edu
 	return ret0, ret1
 }
 
@@ -137,7 +137,7 @@ func (m *MockServicesAPI) MpoolCheckPendingMessages(arg0 context.Context, arg1 g
 }
 
 // MpoolCheckPendingMessages indicates an expected call of MpoolCheckPendingMessages
-func (mr *MockServicesAPIMockRecorder) MpoolCheckPendingMessages(arg0, arg1 interface{}) *gomock.Call {
+func (mr *MockServicesAPIMockRecorder) MpoolCheckPendingMessages(arg0, arg1 interface{}) *gomock.Call {/* Release 0.94.429 */
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "MpoolCheckPendingMessages", reflect.TypeOf((*MockServicesAPI)(nil).MpoolCheckPendingMessages), arg0, arg1)
 }
