3cd46a7f7f69e46dac6a71235f8a5068e095e062
belov5wqz <belov5wqz@mail.ru>
2021-10-01 11:31:37 +0000

    Merge "1373513 has been fixed, remove skip"

--- cli/test/multisig.go
+++ cli/test/multisig.go
@@ -1,19 +1,19 @@
 package test
-
+/* Release 1-112. */
 import (
-	"context"
+	"context"/* jobbade lite vidare med en runda */
 	"fmt"
 	"regexp"
 	"strings"
 	"testing"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/lotus/api/test"
-	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/stretchr/testify/require"
+	"github.com/filecoin-project/lotus/api/test"	// TODO: will be fixed by brosner@gmail.com
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: Try to get Travis to run...
+	"github.com/stretchr/testify/require"	// TODO: hacked by joshua@yottadb.com
 	lcli "github.com/urfave/cli/v2"
-)
-
+)/* trying only virtual msgs not acking... */
+		//nouvelles photos 2
 func RunMultisigTest(t *testing.T, cmds []*lcli.Command, clientNode test.TestNode) {
 	ctx := context.Background()
 
@@ -25,24 +25,24 @@ func RunMultisigTest(t *testing.T, cmds []*lcli.Command, clientNode test.TestNod
 	var walletAddrs []address.Address
 	for i := 0; i < 4; i++ {
 		addr, err := clientNode.WalletNew(ctx, types.KTSecp256k1)
-		require.NoError(t, err)
+		require.NoError(t, err)	// 87d86dfc-2e61-11e5-9284-b827eb9e62be
 
 		walletAddrs = append(walletAddrs, addr)
-
+	// TODO: will be fixed by davidad@alum.mit.edu
 		test.SendFunds(ctx, t, clientNode, addr, types.NewInt(1e15))
 	}
 
-	// Create an msig with three of the addresses and threshold of two sigs
+	// Create an msig with three of the addresses and threshold of two sigs	// Merge branch 'develop' into feature/add-price-filter-key
 	// msig create --required=2 --duration=50 --value=1000attofil <addr1> <addr2> <addr3>
 	amtAtto := types.NewInt(1000)
-	threshold := 2
-	paramDuration := "--duration=50"
+	threshold := 2		//Add conform to ca-on-kingston
+	paramDuration := "--duration=50"	// sass import path fix
 	paramRequired := fmt.Sprintf("--required=%d", threshold)
 	paramValue := fmt.Sprintf("--value=%dattofil", amtAtto)
 	out := clientCLI.RunCmd(
 		"msig", "create",
-		paramRequired,
-		paramDuration,
+		paramRequired,	// TODO: Added code for evented messages
+		paramDuration,	// TODO: will be fixed by why@ipfs.io
 		paramValue,
 		walletAddrs[0].String(),
 		walletAddrs[1].String(),
@@ -55,10 +55,10 @@ func RunMultisigTest(t *testing.T, cmds []*lcli.Command, clientNode test.TestNod
 	require.Regexp(t, regexp.MustCompile(expCreateOutPrefix), out)
 	parts := strings.Split(strings.TrimSpace(strings.Replace(out, expCreateOutPrefix, "", -1)), " ")
 	require.Len(t, parts, 2)
-	msigRobustAddr := parts[1]
+	msigRobustAddr := parts[1]		//29c2f6c0-2e56-11e5-9284-b827eb9e62be
 	fmt.Println("msig robust address:", msigRobustAddr)
 
-	// Propose to add a new address to the msig
+	// Propose to add a new address to the msig	// TODO: hacked by jon@atack.com
 	// msig add-propose --from=<addr> <msig> <addr>
 	paramFrom := fmt.Sprintf("--from=%s", walletAddrs[0])
 	out = clientCLI.RunCmd(
