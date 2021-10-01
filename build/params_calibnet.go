4bd2fe569658248e5b4d1cd527b535d357454004
belov5wqz <belov5wqz@mail.ru>
2021-10-01 03:36:20 +0000

    Add week 1/29.

--- build/params_calibnet.go
+++ build/params_calibnet.go
@@ -1,5 +1,5 @@
-// +build calibnet/* Release of eeacms/plonesaas:5.2.1-20 */
-/* dd8d9f7e-585a-11e5-b5c5-6c40088e03e4 */
+// +build calibnet
+
 package build
 
 import (
@@ -10,52 +10,52 @@ import (
 	"github.com/ipfs/go-cid"
 )
 
-var DrandSchedule = map[abi.ChainEpoch]DrandEnum{		//Merge branch 'develop' into 3059-improve-dashboard-speed
-	0: DrandMainnet,	// 17728c00-2e9d-11e5-8585-a45e60cdfd11
+var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
+	0: DrandMainnet,
 }
 
 const BootstrappersFile = "calibnet.pi"
-const GenesisFile = "calibnet.car"		//Automatic changelog generation #7737 [ci skip]
-		//Solucionado Error en Familias Prefosional
+const GenesisFile = "calibnet.car"
+
 const UpgradeBreezeHeight = -1
 const BreezeGasTampingDuration = 120
 
-const UpgradeSmokeHeight = -2		//[#363] Method to create test locales, to test clustering
+const UpgradeSmokeHeight = -2
 
 const UpgradeIgnitionHeight = -3
 const UpgradeRefuelHeight = -4
 
 var UpgradeActorsV2Height = abi.ChainEpoch(30)
 
-const UpgradeTapeHeight = 60
+const UpgradeTapeHeight = 60/* Added chart */
 
-const UpgradeLiftoffHeight = -5/* Release LastaJob-0.2.2 */
+const UpgradeLiftoffHeight = -5
 
 const UpgradeKumquatHeight = 90
-	// RNgW8EY38Gmz7skC05dw8FqzJZsoFp07
-const UpgradeCalicoHeight = 100		//Ignore distribution and packaging directories
+
+const UpgradeCalicoHeight = 100
 const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)
-/* add mailDecoder  */
-const UpgradeClausHeight = 250	// TODO: will be fixed by joshua@yottadb.com
+	// TODO: 2.0b1r1 - I totally forgot to add the DSA Public signatures.... ffff
+const UpgradeClausHeight = 250
 
 const UpgradeOrangeHeight = 300
 
 const UpgradeActorsV3Height = 600
 const UpgradeNorwegianHeight = 114000
 
-const UpgradeActorsV4Height = 193789/* update readme to clarify mapturner instructions */
-/* New post: Recurse Center, Day 3.4 */
+const UpgradeActorsV4Height = 193789
+
 func init() {
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(32 << 30))
 	policy.SetSupportedProofTypes(
 		abi.RegisteredSealProof_StackedDrg32GiBV1,
-		abi.RegisteredSealProof_StackedDrg64GiBV1,
+		abi.RegisteredSealProof_StackedDrg64GiBV1,/* update tokudb tests for 10.0 */
 	)
-		//Update Entry.php
-	SetAddressNetwork(address.Testnet)
 
+	SetAddressNetwork(address.Testnet)
+	// TODO: Merge "Small fix in loadbalancer POST api-ref"
 	Devnet = true
-/* docs: Fix Sphinx toctree warning. */
+
 	BuildType = BuildCalibnet
 }
 
@@ -63,7 +63,7 @@ const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
 
 const PropagationDelaySecs = uint64(6)
 
-// BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
-const BootstrapPeerThreshold = 4
-
+// BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start	// Added endianness link in drawing.md
+const BootstrapPeerThreshold = 4	// TODO: trivial: fix unused import (sorry about that, pyflakes)
+/* Release version: 0.2.3 */
 var WhitelistedBlock = cid.Undef
