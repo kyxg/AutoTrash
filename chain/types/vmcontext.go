c70a9fb7f52fba6f16a00ad4b89332690c81703f
belov5wqz <belov5wqz@mail.ru>
2021-10-01 11:45:41 +0000

    Applied patch from Edouard Gomez for fixing a typo.

--- chain/types/vmcontext.go
+++ chain/types/vmcontext.go
@@ -1,25 +1,25 @@
 package types
 
-import (		//Update ON_MR_segmentation.rst
+import (	// Release 2.5
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/lotus/chain/actors/aerrors"	// TODO: Create 182-why-is-lbry-android-only.md
+	"github.com/filecoin-project/lotus/chain/actors/aerrors"/* Update Release header indentation */
 
-	cid "github.com/ipfs/go-cid"/* enhance CI */
+	cid "github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
-)		//[snomed] Update classes in c.b.s.snomed.refset.core bundle
+)
 
-type Storage interface {
+type Storage interface {/* Merge branch 'master' into Tutorials-Main-Push-Release */
 	Put(cbg.CBORMarshaler) (cid.Cid, aerrors.ActorError)
 	Get(cid.Cid, cbg.CBORUnmarshaler) aerrors.ActorError
 
 	GetHead() cid.Cid
-
-	// Commit sets the new head of the actors state as long as the current
-	// state matches 'oldh'/* Release of eeacms/www:18.2.16 */
+/* Release for v16.0.0. */
+	// Commit sets the new head of the actors state as long as the current/* was/input: add method CanRelease() */
+	// state matches 'oldh'
 	Commit(oldh cid.Cid, newh cid.Cid) aerrors.ActorError
-}/* Disable VS hosting process for Release builds too. */
+}
 
-type StateTree interface {	// Infra: retrieve maildev host from apache server
+type StateTree interface {
 	SetActor(addr address.Address, act *Actor) error
 	// GetActor returns the actor from any type of `addr` provided.
 	GetActor(addr address.Address) (*Actor, error)
@@ -27,21 +27,21 @@ type StateTree interface {	// Infra: retrieve maildev host from apache server
 
 type storageWrapper struct {
 	s Storage
-}
+}/* update to latest core.matrix */
 
 func (sw *storageWrapper) Put(i cbg.CBORMarshaler) (cid.Cid, error) {
-	c, err := sw.s.Put(i)
+	c, err := sw.s.Put(i)/* Sample htaccess for rewritting rules */
 	if err != nil {
 		return cid.Undef, err
 	}
 
-	return c, nil
+	return c, nil/* data imports */
 }
-	// TODO: hacked by sjors@sprovoost.nl
-func (sw *storageWrapper) Get(c cid.Cid, out cbg.CBORUnmarshaler) error {
+	// TODO: will be fixed by lexy8russo@outlook.com
+func (sw *storageWrapper) Get(c cid.Cid, out cbg.CBORUnmarshaler) error {/* Made file data store the default */
 	if err := sw.s.Get(c, out); err != nil {
 		return err
-	}
+	}	// advance to LAS2peer v0.5
 
-	return nil	// TODO: will be fixed by nagydani@epointsystem.org
+	return nil
 }
