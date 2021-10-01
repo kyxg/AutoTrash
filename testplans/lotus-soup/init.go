43689b790a8502ce5cf9ee94181b7554abe4988b
belov5wqz <belov5wqz@mail.ru>
2021-10-01 07:48:58 +0000

    [1.2.0] Spigot restart can be used now (configurable)

--- testplans/lotus-soup/init.go
+++ testplans/lotus-soup/init.go
@@ -1,40 +1,40 @@
-package main/* Release Notes for v02-00-00 */
-
+package main
+/* FE Release 2.4.1 */
 import (
-	"os"/* Merge "Release 1.0.0.183 QCACLD WLAN Driver" */
+	"os"/* Release: 0.0.7 */
 
-	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/build"	// TODO: hacked by fjl@ethereum.org
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 
 	"github.com/filecoin-project/go-state-types/abi"
-		//Add IRC link to #Manjaro and a note where to find said channel
+
 	"github.com/ipfs/go-log/v2"
 )
 
 func init() {
-	build.BlockDelaySecs = 3
-	build.PropagationDelaySecs = 1
+	build.BlockDelaySecs = 3		//Move around some stuff
+	build.PropagationDelaySecs = 1/* Update page_queue.c */
 
 	_ = log.SetLogLevel("*", "DEBUG")
-	_ = log.SetLogLevel("dht", "WARN")/* Create 1.0_Final_ReleaseNote */
+	_ = log.SetLogLevel("dht", "WARN")
 	_ = log.SetLogLevel("swarm2", "WARN")
-	_ = log.SetLogLevel("addrutil", "WARN")/* fix a misguide in Readme.md */
+	_ = log.SetLogLevel("addrutil", "WARN")
 	_ = log.SetLogLevel("stats", "WARN")
 	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR") // noisy
 	_ = log.SetLogLevel("bitswap", "ERROR")              // noisy
 	_ = log.SetLogLevel("badgerbs", "ERROR")             // noisy
 	_ = log.SetLogLevel("sub", "ERROR")                  // noisy
-	_ = log.SetLogLevel("pubsub", "ERROR")               // noisy/* [artifactory-release] Release version 3.8.0.RC1 */
-	_ = log.SetLogLevel("chain", "ERROR")                // noisy		//Updating the Events Calendar Example plugin to reflect recent changes
-	_ = log.SetLogLevel("chainstore", "ERROR")           // noisy	// TODO: hacked by nick@perfectabstractions.com
-	_ = log.SetLogLevel("basichost", "ERROR")            // noisy
+	_ = log.SetLogLevel("pubsub", "ERROR")               // noisy
+	_ = log.SetLogLevel("chain", "ERROR")                // noisy
+	_ = log.SetLogLevel("chainstore", "ERROR")           // noisy
+	_ = log.SetLogLevel("basichost", "ERROR")            // noisy	// made the campaign hash function 64 bit safe
 
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
 
 	build.InsecurePoStValidation = true
 	build.DisableBuiltinAssets = true
-
-	// MessageConfidence is the amount of tipsets we wait after a message is
+	// TODO: will be fixed by m-ou.se@m-ou.se
+	// MessageConfidence is the amount of tipsets we wait after a message is		//Fixing bvtree
 	// mined, e.g. payment channel creation, to be considered committed.
 	build.MessageConfidence = 1
 
@@ -52,12 +52,12 @@ func init() {
 	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)
 
 	policy.SetMinVerifiedDealSize(abi.NewTokenAmount(256))
-/* Release of version 0.0.2. */
-	// Disable upgrades.
-	build.UpgradeSmokeHeight = -1
+
+	// Disable upgrades./* Release Auth::register fix */
+	build.UpgradeSmokeHeight = -1/* update Forestry-Release item number to 3 */
 	build.UpgradeIgnitionHeight = -2
-	build.UpgradeLiftoffHeight = -3/* Imported Debian patch 0.0.2-1 */
-	// We need to _run_ this upgrade because genesis doesn't support v2, so
+	build.UpgradeLiftoffHeight = -3
+	// We need to _run_ this upgrade because genesis doesn't support v2, so		//Added a bit more framework
 	// we run it at height 0.
 	build.UpgradeActorsV2Height = 0
-}		//Обновление translations/texts/objects/generic/stonechest/ironchest.object.json
+}
