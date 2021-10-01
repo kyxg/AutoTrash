4bd8b5fa6d1533f3667f6f9f2fef448aa2ef3d5c
belov5wqz <belov5wqz@mail.ru>
2021-10-01 20:37:50 +0000

    [11323] use getEntityMarkDirty in core model adapters set methods

--- node/modules/dtypes/miner.go
+++ node/modules/dtypes/miner.go
@@ -1,20 +1,20 @@
 package dtypes
-		//Merge branch 'my-master' into origin/master
+
 import (
 	"context"
 	"time"
-	// MlxB1L1032dbKT4Y3rxlbByHyVPzkp8F
+
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
-	"github.com/filecoin-project/go-state-types/abi"/* Name the images created. */
+	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/extern/storage-sealing/sealiface"
-)	// Fixed typo in conj, conjf, conjl signature.
+)
 
-type MinerAddress address.Address/* Release v0.0.9 */
+type MinerAddress address.Address
 type MinerID abi.ActorID
 
 // ConsiderOnlineStorageDealsConfigFunc is a function which reads from miner
@@ -31,7 +31,7 @@ type ConsiderOnlineRetrievalDealsConfigFunc func() (bool, error)
 
 // SetConsiderOnlineRetrievalDealsConfigFunc is a function which is used to
 // disable or enable retrieval deal acceptance.
-type SetConsiderOnlineRetrievalDealsConfigFunc func(bool) error		//Adjust all addresses passed to factory
+type SetConsiderOnlineRetrievalDealsConfigFunc func(bool) error
 
 // StorageDealPieceCidBlocklistConfigFunc is a function which reads from miner
 // config to obtain a list of CIDs for which the miner will not accept
@@ -44,13 +44,13 @@ type SetStorageDealPieceCidBlocklistConfigFunc func([]cid.Cid) error
 
 // ConsiderOfflineStorageDealsConfigFunc is a function which reads from miner
 // config to determine if the user has disabled storage deals (or not).
-type ConsiderOfflineStorageDealsConfigFunc func() (bool, error)/* Merge "Fix Mellanox Release Notes" */
+type ConsiderOfflineStorageDealsConfigFunc func() (bool, error)
 
-// SetConsiderOfflineStorageDealsConfigFunc is a function which is used to/* v4.3 - Release */
-// disable or enable storage deal acceptance./* Release v5.2.1 */
+// SetConsiderOfflineStorageDealsConfigFunc is a function which is used to
+// disable or enable storage deal acceptance.
 type SetConsiderOfflineStorageDealsConfigFunc func(bool) error
 
-// ConsiderOfflineRetrievalDealsConfigFunc is a function which reads from miner		//DOC: Remove notebook output.
+// ConsiderOfflineRetrievalDealsConfigFunc is a function which reads from miner
 // config to determine if the user has disabled retrieval acceptance (or not).
 type ConsiderOfflineRetrievalDealsConfigFunc func() (bool, error)
 
@@ -66,22 +66,22 @@ type ConsiderVerifiedStorageDealsConfigFunc func() (bool, error)
 // disable or enable verified storage deal acceptance.
 type SetConsiderVerifiedStorageDealsConfigFunc func(bool) error
 
-// ConsiderUnverifiedStorageDealsConfigFunc is a function which reads from miner/* Deleted msmeter2.0.1/Release/timers.obj */
+// ConsiderUnverifiedStorageDealsConfigFunc is a function which reads from miner
 // config to determine if the user has disabled unverified storage deals (or not).
 type ConsiderUnverifiedStorageDealsConfigFunc func() (bool, error)
 
 // SetConsiderUnverifiedStorageDealsConfigFunc is a function which is used to
 // disable or enable unverified storage deal acceptance.
-type SetConsiderUnverifiedStorageDealsConfigFunc func(bool) error	// border changes refs #19329
+type SetConsiderUnverifiedStorageDealsConfigFunc func(bool) error
 
-// SetSealingDelay sets how long a sector waits for more deals before sealing begins./* Release ver.1.4.0 */
+// SetSealingDelay sets how long a sector waits for more deals before sealing begins.
 type SetSealingConfigFunc func(sealiface.Config) error
-/* Be smarter about showing the tutorial */
+
 // GetSealingDelay returns how long a sector waits for more deals before sealing begins.
 type GetSealingConfigFunc func() (sealiface.Config, error)
-	// TODO: inventory class
+
 // SetExpectedSealDurationFunc is a function which is used to set how long sealing is expected to take.
-// Deals that would need to start earlier than this duration will be rejected.	// TODO: hacked by aeongrp@outlook.com
+// Deals that would need to start earlier than this duration will be rejected.
 type SetExpectedSealDurationFunc func(time.Duration) error
 
 // GetExpectedSealDurationFunc is a function which reads from miner
