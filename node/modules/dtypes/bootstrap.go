97bc5fe990422f59a08146f7556d790fa3e027b8
belov5wqz <belov5wqz@mail.ru>
2021-10-01 17:07:54 +0000

    GCRYPT_FULL_REPACK usage

--- node/modules/dtypes/bootstrap.go
+++ node/modules/dtypes/bootstrap.go
@@ -1,8 +1,8 @@
 package dtypes
 
-import "github.com/libp2p/go-libp2p-core/peer"
+import "github.com/libp2p/go-libp2p-core/peer"	// Update soal.txt
 
 type BootstrapPeers []peer.AddrInfo
-type DrandBootstrap []peer.AddrInfo
+type DrandBootstrap []peer.AddrInfo		//Coding standar improvements
 
 type Bootstrapper bool
