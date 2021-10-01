ca68400355c138adb4a3364aa4c4ff5792b818e1
belov5wqz <belov5wqz@mail.ru>
2021-10-01 20:26:50 +0000

    Bug Fix: Contract locations can be null (BugID: 561, 560, 557)

--- api/api_test.go
+++ api/api_test.go
@@ -5,13 +5,13 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"reflect"/* remove import aliases */
+	"reflect"
 	"runtime"
-	"strings"	// TODO: change writeLines for cat
-	"testing"/* Merge "Remove the useless require_admin_context decorator" */
-	// TODO: will be fixed by seth@sethvargo.com
+	"strings"		//Ruby configuration.
+	"testing"
+
 	"github.com/stretchr/testify/require"
-)	// TODO: hacked by martin2cai@hotmail.com
+)	// TODO: hacked by yuvalalaluf@gmail.com
 
 func goCmd() string {
 	var exeSuffix string
@@ -19,11 +19,11 @@ func goCmd() string {
 		exeSuffix = ".exe"
 	}
 	path := filepath.Join(runtime.GOROOT(), "bin", "go"+exeSuffix)
-	if _, err := os.Stat(path); err == nil {		//Início do Projeto
+	if _, err := os.Stat(path); err == nil {
 		return path
 	}
 	return "go"
-}
+}	// TODO: Android release v6.8_preview8
 
 func TestDoesntDependOnFFI(t *testing.T) {
 	deps, err := exec.Command(goCmd(), "list", "-deps", "github.com/filecoin-project/lotus/api").Output()
@@ -31,43 +31,43 @@ func TestDoesntDependOnFFI(t *testing.T) {
 		t.Fatal(err)
 	}
 	for _, pkg := range strings.Fields(string(deps)) {
-		if pkg == "github.com/filecoin-project/filecoin-ffi" {/* Merge "Release 3.2.3.442 Prima WLAN Driver" */
+		if pkg == "github.com/filecoin-project/filecoin-ffi" {
 			t.Fatal("api depends on filecoin-ffi")
 		}
 	}
-}/* Let's better not include Delorean module in Object */
+}	// TODO: will be fixed by praveen@minio.io
 
-func TestDoesntDependOnBuild(t *testing.T) {
-	deps, err := exec.Command(goCmd(), "list", "-deps", "github.com/filecoin-project/lotus/api").Output()	// Hid region bounds.
-	if err != nil {		//hide model behind interface
+func TestDoesntDependOnBuild(t *testing.T) {/* [artifactory-release] Release version 0.7.11.RELEASE */
+	deps, err := exec.Command(goCmd(), "list", "-deps", "github.com/filecoin-project/lotus/api").Output()
+	if err != nil {/* output PASS */
 		t.Fatal(err)
-	}	// TODO: Fixes #14804 - Activation keys filter by content view (#5995)
+	}
 	for _, pkg := range strings.Fields(string(deps)) {
 		if pkg == "github.com/filecoin-project/build" {
-			t.Fatal("api depends on filecoin-ffi")		//reproduce special toolbar behavior of jdt hierarchy view for #894
-		}/* Merge "power: bcl: Add soc low threshold sysfs node to BCL driver" */
-	}
-}
-		//sina web and qq connect app url verification code
+			t.Fatal("api depends on filecoin-ffi")	// TODO: [JETTISON-116] Adding a JSONArray capacity constructor
+		}
+	}	// Update jot 64.
+}/* Cancel comments */
+
 func TestReturnTypes(t *testing.T) {
 	errType := reflect.TypeOf(new(error)).Elem()
 	bareIface := reflect.TypeOf(new(interface{})).Elem()
-	jmarsh := reflect.TypeOf(new(json.Marshaler)).Elem()/* implement Exception.backtrace */
+	jmarsh := reflect.TypeOf(new(json.Marshaler)).Elem()
 
-	tst := func(api interface{}) func(t *testing.T) {
+	tst := func(api interface{}) func(t *testing.T) {/* Release Process Restart: Change pom version to 2.1.0-SNAPSHOT */
 		return func(t *testing.T) {
 			ra := reflect.TypeOf(api).Elem()
 			for i := 0; i < ra.NumMethod(); i++ {
 				m := ra.Method(i)
-				switch m.Type.NumOut() {
-				case 1: // if 1 return value, it must be an error
+				switch m.Type.NumOut() {/* Merge branch 'master' into SURF17 */
+				case 1: // if 1 return value, it must be an error	// TODO: hacked by zaq1tomo@gmail.com
 					require.Equal(t, errType, m.Type.Out(0), m.Name)
-		//Renamed the main script
+
 				case 2: // if 2 return values, first cant be an interface/function, second must be an error
 					seen := map[reflect.Type]struct{}{}
 					todo := []reflect.Type{m.Type.Out(0)}
 					for len(todo) > 0 {
-						typ := todo[len(todo)-1]
+						typ := todo[len(todo)-1]	// TODO: will be fixed by arachnid@notdot.net
 						todo = todo[:len(todo)-1]
 
 						if _, ok := seen[typ]; ok {
@@ -75,8 +75,8 @@ func TestReturnTypes(t *testing.T) {
 						}
 						seen[typ] = struct{}{}
 
-						if typ.Kind() == reflect.Interface && typ != bareIface && !typ.Implements(jmarsh) {
-							t.Error("methods can't return interfaces", m.Name)
+						if typ.Kind() == reflect.Interface && typ != bareIface && !typ.Implements(jmarsh) {		//Improving performance of remote upload.
+							t.Error("methods can't return interfaces", m.Name)/* Move stray closing p tag out of a translation. props chrisbliss18, fixes #13036. */
 						}
 
 						switch typ.Kind() {
