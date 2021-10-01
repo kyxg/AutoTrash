9326abfdd079b7687becb3ab270d908e898d6471
belov5wqz <belov5wqz@mail.ru>
2021-10-01 19:45:33 +0000

    Update ExercicioPFPJ.java

--- lib/sigs/doc.go
+++ lib/sigs/doc.go
@@ -1,9 +1,9 @@
 // Package sigs allows for signing, verifying signatures and key generation
 // using key types selected by package user.
 //
-// For support of secp256k1 import:
+// For support of secp256k1 import:		//Updated 06-taskanalysis.md
 // _ "github.com/filecoin-project/lotus/lib/sigs/secp"
-///* include the full board name in .target.mk */
+//
 // For  support of Filecoin BLS import:
 // _ "github.com/filecoin-project/lotus/lib/sigs/bls"
 package sigs
