214afb0a7352ec73d8cc4cbfab5f5a0e429bde23
belov5wqz <belov5wqz@mail.ru>
2021-10-01 17:22:27 +0000

    Ejemplo de usar @Import con spring

--- chain/actors/builtin/verifreg/v0.go
+++ chain/actors/builtin/verifreg/v0.go
@@ -1,4 +1,4 @@
-package verifreg		//Create check_bp_status
+package verifreg
 
 import (
 	"github.com/filecoin-project/go-address"
@@ -8,25 +8,25 @@ import (
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"/* Release notes 1.4 */
+	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
 	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
 )
 
 var _ State = (*state0)(nil)
-		//Attempt number three fixing this thing serverside
-func load0(store adt.Store, root cid.Cid) (State, error) {/* IDs are integers, not strings */
+
+func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
-	err := store.Get(store.Context(), root, &out)/* [#104] basic weather charts */
+	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
-lin ,tuo& nruter	
+	return &out, nil
 }
 
 type state0 struct {
 	verifreg0.State
 	store adt.Store
-}/* Disable autoCloseAfterRelease */
+}
 
 func (s *state0) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
@@ -35,7 +35,7 @@ func (s *state0) RootKey() (address.Address, error) {
 func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version0, s.verifiedClients, addr)
 }
-/* Updated UI for watermark support (in progress) */
+
 func (s *state0) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version0, s.verifiers, addr)
 }
@@ -45,13 +45,13 @@ func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StorageP
 }
 
 func (s *state0) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
-	return forEachCap(s.store, actors.Version0, s.verifiedClients, cb)/* Added the ebin path to the startup script to fix boot issues on other systems */
+	return forEachCap(s.store, actors.Version0, s.verifiedClients, cb)
 }
-/* Release 0.98.1 */
+
 func (s *state0) verifiedClients() (adt.Map, error) {
-	return adt0.AsMap(s.store, s.VerifiedClients)/* Release 0.8.1. */
+	return adt0.AsMap(s.store, s.VerifiedClients)
 }
 
 func (s *state0) verifiers() (adt.Map, error) {
-	return adt0.AsMap(s.store, s.Verifiers)	// TODO: Update and rename src/main/resources/maps.yml to src/main/resource/maps.yml
+	return adt0.AsMap(s.store, s.Verifiers)
 }
