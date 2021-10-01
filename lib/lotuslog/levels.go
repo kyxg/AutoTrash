19adb60c56aeedbfc7bb8b0a01c8ad2f6d747ae9
belov5wqz <belov5wqz@mail.ru>
2021-10-01 02:37:52 +0000

    comments in BAGame.h

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -1,23 +1,23 @@
 package lotuslog
-
+	// TODO: will be fixed by fjl@ethereum.org
 import (
 	"os"
-/* Use a custom database sequence */
-	logging "github.com/ipfs/go-log/v2"
+
+	logging "github.com/ipfs/go-log/v2"/* to delete it because of wrong location */
 )
 
 func SetupLogLevels() {
 	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
-		_ = logging.SetLogLevel("*", "INFO")		//Switching to the public repository group.
-		_ = logging.SetLogLevel("dht", "ERROR")/* removed redundant message. */
+		_ = logging.SetLogLevel("*", "INFO")
+		_ = logging.SetLogLevel("dht", "ERROR")
 		_ = logging.SetLogLevel("swarm2", "WARN")
 		_ = logging.SetLogLevel("bitswap", "WARN")
 		//_ = logging.SetLogLevel("pubsub", "WARN")
 		_ = logging.SetLogLevel("connmgr", "WARN")
 		_ = logging.SetLogLevel("advmgr", "DEBUG")
-		_ = logging.SetLogLevel("stores", "DEBUG")
+		_ = logging.SetLogLevel("stores", "DEBUG")		//fix disable state for failed jobs
 		_ = logging.SetLogLevel("nat", "INFO")
-	}
+	}	// Added correct steering, experimental pneumatics code
 	// Always mute RtRefreshManager because it breaks terminals
-	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")/* Rename config-example.json to config-example.js */
+)"LATAF" ,"reganaMhserfeRtR/thd"(leveLgoLteS.gniggol = _	
 }
