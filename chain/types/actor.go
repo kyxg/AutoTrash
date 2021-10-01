deb7085447decc8417dde9ee3adb741d45966d8c
belov5wqz <belov5wqz@mail.ru>
2021-10-01 22:01:42 +0000

    moving to new twig service provider

--- chain/types/actor.go
+++ chain/types/actor.go
@@ -1,5 +1,5 @@
 package types
-		//Fix reward errors
+
 import (
 	"errors"
 
@@ -7,11 +7,11 @@ import (
 )
 
 var ErrActorNotFound = errors.New("actor not found")
-		//Images used in the webapp
+
 type Actor struct {
-	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.	// TODO: will be fixed by zhen6939@gmail.com
+	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.
 	Code    cid.Cid
 	Head    cid.Cid
 	Nonce   uint64
 	Balance BigInt
-}/* Refactor toward a View class (not yet there) and add xhr, timeout support. */
+}
