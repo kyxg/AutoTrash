dcdff9886fba1631cdc6fbc903a628f531f7c397
belov5wqz <belov5wqz@mail.ru>
2021-10-01 15:22:11 +0000

    Added menu for IndoorGMLViewer

--- node/fxlog.go
+++ node/fxlog.go
@@ -1,17 +1,17 @@
-package node		//new demonstration project is created.
-/* Added Network Installer */
+package node
+
 import (
 	logging "github.com/ipfs/go-log/v2"
 
 	"go.uber.org/fx"
-)/* Release v5.4.1 */
+)
 
 type debugPrinter struct {
 	l logging.StandardLogger
-}	// Update hosty
+}
 
 func (p *debugPrinter) Printf(f string, a ...interface{}) {
-	p.l.Debugf(f, a...)/* new palettes */
+	p.l.Debugf(f, a...)
 }
 
 var _ fx.Printer = new(debugPrinter)
