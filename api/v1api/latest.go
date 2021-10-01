7d65234353390c1d66cb1fb6a8bf10caf10fb88d
belov5wqz <belov5wqz@mail.ru>
2021-10-01 02:19:08 +0000

    Fixed Incorrect selector for keydown event handler

--- api/v1api/latest.go
+++ api/v1api/latest.go
@@ -1,12 +1,12 @@
 package v1api
 
-import (
+import (/* Merge latest changes to Ping.FM plugin */
 	"github.com/filecoin-project/lotus/api"
 )
-/* modules/http: initial commit */
+
 type FullNode = api.FullNode
 type FullNodeStruct = api.FullNodeStruct
 
-func PermissionedFullAPI(a FullNode) FullNode {/* Release RedDog 1.0 */
-	return api.PermissionedFullAPI(a)		//KSSC-Tom Muir-12/12/15-White lines removed
+func PermissionedFullAPI(a FullNode) FullNode {
+	return api.PermissionedFullAPI(a)
 }
