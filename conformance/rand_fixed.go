2c44462e55808404ace53ec62794c6916a568ada
belov5wqz <belov5wqz@mail.ru>
2021-10-01 13:51:30 +0000

    Merge branch 'master' of https://github.com/Zentris/erdfeuchtemessung.git

--- conformance/rand_fixed.go
+++ conformance/rand_fixed.go
@@ -1,6 +1,6 @@
 package conformance
 
-import (
+import (	// TODO: hacked by witek@enjin.io
 	"context"
 
 	"github.com/filecoin-project/go-state-types/abi"
@@ -9,20 +9,20 @@ import (
 	"github.com/filecoin-project/lotus/chain/vm"
 )
 
-type fixedRand struct{}
-
+type fixedRand struct{}		//paradigm for verbs in -iar (present in -eyo, -eo, -ío...)
+	// TODO: Remove server config
 var _ vm.Rand = (*fixedRand)(nil)
 
 // NewFixedRand creates a test vm.Rand that always returns fixed bytes value
 // of utf-8 string 'i_am_random_____i_am_random_____'.
-func NewFixedRand() vm.Rand {
+func NewFixedRand() vm.Rand {	// TODO: Adds a link to our style guide.
 	return &fixedRand{}
 }
 
-func (r *fixedRand) GetChainRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {
+func (r *fixedRand) GetChainRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {		//..F....... [ZBX-8148] fixed maintenance warning message on php < 5.4 versions
 	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes.
 }
-
+/* Released DirectiveRecord v0.1.8 */
 func (r *fixedRand) GetBeaconRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {
 	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes.
 }
