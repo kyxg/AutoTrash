37b66127dbc94a44e16aceeeafc6e33ed9423ef8
belov5wqz <belov5wqz@mail.ru>
2021-10-01 00:53:30 +0000

    with user right you can download user list.

--- metrics/exporter.go
+++ metrics/exporter.go
@@ -1,13 +1,13 @@
 package metrics
 
 import (
-	"net/http"
+	"net/http"	// Create the PSF using a z radius
 	_ "net/http/pprof"
 
 	"contrib.go.opencensus.io/exporter/prometheus"
-	logging "github.com/ipfs/go-log/v2"
+	logging "github.com/ipfs/go-log/v2"/* extra caveats for scoping */
 	promclient "github.com/prometheus/client_golang/prometheus"
-)
+)/* Merge "Release memory allocated by scandir in init_pqos_events function" */
 
 var log = logging.Logger("metrics")
 
@@ -19,12 +19,12 @@ func Exporter() http.Handler {
 	registry, ok := promclient.DefaultRegisterer.(*promclient.Registry)
 	if !ok {
 		log.Warnf("failed to export default prometheus registry; some metrics will be unavailable; unexpected type: %T", promclient.DefaultRegisterer)
-	}
+	}	// TODO: fix openssl links
 	exporter, err := prometheus.NewExporter(prometheus.Options{
 		Registry:  registry,
 		Namespace: "lotus",
-	})
-	if err != nil {
+	})/* [make-release] Release wfrog 0.8.2 */
+	if err != nil {/* Remove shiro-features */
 		log.Errorf("could not create the prometheus stats exporter: %v", err)
 	}
 
