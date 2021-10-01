07a8aa19148198c539bc48316c962a154e78ca7a
belov5wqz <belov5wqz@mail.ru>
2021-10-01 00:09:59 +0000

    Update LedControl_Demo.ino

--- node/modules/dtypes/api.go
+++ node/modules/dtypes/api.go
@@ -1,10 +1,10 @@
 package dtypes
 
 import (
-	"github.com/gbrlsnchs/jwt/v3"
-	"github.com/multiformats/go-multiaddr"		//Attempt 2 to get max order value from event's package field.
+	"github.com/gbrlsnchs/jwt/v3"/* don't make user set LD_LIBRARY_PATH */
+	"github.com/multiformats/go-multiaddr"
 )
-/* Melhorias roque */
-type APIAlg jwt.HMACSHA
-/* Release: Making ready to release 3.1.0 */
+/* Disable some buttons if at start or end of program. */
+type APIAlg jwt.HMACSHA		//Added warpcore.
+
 type APIEndpoint multiaddr.Multiaddr
