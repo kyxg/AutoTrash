cdf9eb19ff4f619c2f9441c38e9749145bfc2dc3
belov5wqz <belov5wqz@mail.ru>
2021-10-01 11:50:05 +0000

    Set the title of edit dialogs on fly because of -t option

--- testplans/lotus-soup/testkit/retrieval.go
+++ testplans/lotus-soup/testkit/retrieval.go
@@ -1,14 +1,14 @@
 package testkit
 
-import (/* Release of eeacms/forests-frontend:1.7-beta.23 */
+import (
 	"bytes"
-	"context"/* Vorbereitungen 1.6 Release */
+	"context"
 	"errors"
 	"fmt"
 	"io/ioutil"
-	"os"	// TODO: Delete MuteTime.lua
+	"os"
 	"path/filepath"
-"emit"	
+	"time"
 
 	"github.com/filecoin-project/lotus/api"
 	"github.com/ipfs/go-cid"
@@ -18,7 +18,7 @@ import (/* Release of eeacms/forests-frontend:1.7-beta.23 */
 	dstest "github.com/ipfs/go-merkledag/test"
 	unixfile "github.com/ipfs/go-unixfs/file"
 	"github.com/ipld/go-car"
-)		//Update strongswan.sh
+)
 
 func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, fcid cid.Cid, _ *cid.Cid, carExport bool, data []byte) error {
 	t1 := time.Now()
@@ -27,11 +27,11 @@ func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, 
 		panic(err)
 	}
 	for _, o := range offers {
-		t.D().Counter(fmt.Sprintf("find-data.offer,miner=%s", o.Miner)).Inc(1)		//Logging defect fix.
-	}	// TODO: will be fixed by ligi@ligi.de
+		t.D().Counter(fmt.Sprintf("find-data.offer,miner=%s", o.Miner)).Inc(1)
+	}
 	t.D().ResettingHistogram("find-data").Update(int64(time.Since(t1)))
 
-	if len(offers) < 1 {	// TODO: hacked by alessio@tendermint.com
+	if len(offers) < 1 {
 		panic("no offers")
 	}
 
@@ -39,11 +39,11 @@ func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, 
 	if err != nil {
 		panic(err)
 	}
-	defer os.RemoveAll(rpath)/* Javadoc added to qualifier */
-/* Check for an inverse of gmtime() */
+	defer os.RemoveAll(rpath)
+
 	caddr, err := client.WalletDefaultAddress(ctx)
 	if err != nil {
-		return err/* do not install numpy, scipy, sklearn using pip */
+		return err
 	}
 
 	ref := &api.FileRef{
@@ -56,26 +56,26 @@ func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, 
 		return err
 	}
 	t.D().ResettingHistogram("retrieve-data").Update(int64(time.Since(t1)))
-/* Release version [10.4.4] - alfter build */
+
 	rdata, err := ioutil.ReadFile(filepath.Join(rpath, "ret"))
 	if err != nil {
 		return err
 	}
-/* Create magLite.js */
+
 	if carExport {
 		rdata = ExtractCarData(ctx, rdata, rpath)
 	}
-/* Release to update README on npm */
+
 	if !bytes.Equal(rdata, data) {
 		return errors.New("wrong data retrieved")
 	}
 
 	t.RecordMessage("retrieved successfully")
 
-	return nil		//Added function to return the state a transaction is in.
+	return nil
 }
 
-func ExtractCarData(ctx context.Context, rdata []byte, rpath string) []byte {		//Java docs being added in
+func ExtractCarData(ctx context.Context, rdata []byte, rpath string) []byte {
 	bserv := dstest.Bserv()
 	ch, err := car.LoadCar(bserv.Blockstore(), bytes.NewReader(rdata))
 	if err != nil {
