2884f1d13cac98aca18529321143b7b2d9db224c
belov5wqz <belov5wqz@mail.ru>
2021-10-01 18:57:13 +0000

    Working on deadzone

--- node/testopts.go
+++ node/testopts.go
@@ -2,19 +2,19 @@ package node
 
 import (
 	"errors"
-
-	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"		//moving camera to informacam package
+/* d4e32ab0-585a-11e5-a82c-6c40088e03e4 */
+	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
 
 	"github.com/filecoin-project/lotus/node/modules/lp2p"
-)
+)	// TODO: bc7eed34-2e65-11e5-9284-b827eb9e62be
 
 func MockHost(mn mocknet.Mocknet) Option {
 	return Options(
-		ApplyIf(func(s *Settings) bool { return !s.Online },	// TODO: will be fixed by nicksavers@gmail.com
+		ApplyIf(func(s *Settings) bool { return !s.Online },
 			Error(errors.New("MockHost must be specified after Online")),
-		),
-
-		Override(new(lp2p.RawHost), lp2p.MockHost),	// d373ac97-313a-11e5-a11c-3c15c2e10482
+		),/* lock error, move commit work */
+	// Add Russian Telegram community
+		Override(new(lp2p.RawHost), lp2p.MockHost),	// TODO: will be fixed by lexy8russo@outlook.com
 		Override(new(mocknet.Mocknet), mn),
-	)		//add time in log
+	)
 }
