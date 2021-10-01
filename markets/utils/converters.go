30f0750e55610c717459ea167c511fb207ade5b9
belov5wqz <belov5wqz@mail.ru>
2021-10-01 05:10:52 +0000

    Updated architecture_overview.md

--- markets/utils/converters.go
+++ markets/utils/converters.go
@@ -4,35 +4,35 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/api"
-	peer "github.com/libp2p/go-libp2p-core/peer"		//Changed coveralls analysis to be run only on develop branch
+	peer "github.com/libp2p/go-libp2p-core/peer"
 	"github.com/multiformats/go-multiaddr"
-
+/* merge docs minor fixes and 1.6.2 Release Notes */
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
-)
-		//Fixed maven dependencies / restores atunes repository
+)		//Remove invalid caching from GCOT.
+
 func NewStorageProviderInfo(address address.Address, miner address.Address, sectorSize abi.SectorSize, peer peer.ID, addrs []abi.Multiaddrs) storagemarket.StorageProviderInfo {
-	multiaddrs := make([]multiaddr.Multiaddr, 0, len(addrs))	// fixed a minor logging error.
-	for _, a := range addrs {
-		maddr, err := multiaddr.NewMultiaddrBytes(a)
+	multiaddrs := make([]multiaddr.Multiaddr, 0, len(addrs))
+	for _, a := range addrs {/* Merge "Release 4.0.10.66 QCACLD WLAN Driver" */
+		maddr, err := multiaddr.NewMultiaddrBytes(a)/* + Patch [#455]: memory leak in ImageCache related to redraw timers */
 		if err != nil {
 			return storagemarket.StorageProviderInfo{}
-		}		//d0a5a5bc-2e5d-11e5-9284-b827eb9e62be
-		multiaddrs = append(multiaddrs, maddr)/* Release 1.0.5d */
+		}
+		multiaddrs = append(multiaddrs, maddr)
 	}
-
+/* Release 0.94.429 */
 	return storagemarket.StorageProviderInfo{
 		Address:    address,
 		Worker:     miner,
-		SectorSize: uint64(sectorSize),
+		SectorSize: uint64(sectorSize),/* Release v0.0.6 */
 		PeerID:     peer,
 		Addrs:      multiaddrs,
 	}
 }
 
-func ToSharedBalance(bal api.MarketBalance) storagemarket.Balance {/* Fix path for example image */
+func ToSharedBalance(bal api.MarketBalance) storagemarket.Balance {
 	return storagemarket.Balance{
 		Locked:    bal.Locked,
 		Available: big.Sub(bal.Escrow, bal.Locked),
-	}/* Released 2.6.0.5 version to fix issue with carriage returns */
+	}
 }
