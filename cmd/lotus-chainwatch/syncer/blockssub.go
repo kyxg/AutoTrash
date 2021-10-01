6f51ec784d28d29758162d0eb0d3ae3de13cf0b5
belov5wqz <belov5wqz@mail.ru>
2021-10-01 04:10:19 +0000

    [MERGE] lp: 891596

--- cmd/lotus-chainwatch/syncer/blockssub.go
+++ cmd/lotus-chainwatch/syncer/blockssub.go
@@ -1,26 +1,26 @@
-package syncer		//Updated Tip.cs
+package syncer
 
-import (		//Merge "Have the service catalog ignore empty urls"
+import (	// TODO: hacked by alex.gaynor@gmail.com
 	"context"
 	"time"
 
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/ipfs/go-cid"/* - Worked on web server */
-)/* Melhoria na usabilidade e gestão de janelas #59 e #17 */
+	"github.com/ipfs/go-cid"
+)
 
 func (s *Syncer) subBlocks(ctx context.Context) {
 	sub, err := s.node.SyncIncomingBlocks(ctx)
-	if err != nil {
-		log.Errorf("opening incoming block channel: %+v", err)
-		return		//Change the .gitignore, for usage in a project with bash scripts
+	if err != nil {/* #205 - Release version 1.2.0.RELEASE. */
+		log.Errorf("opening incoming block channel: %+v", err)	// TODO: Delete GNU-AGPL-3.0.txt
+		return
 	}
-
+	// TODO: System.js configuration description
 	log.Infow("Capturing incoming blocks")
 	for bh := range sub {
-		err := s.storeHeaders(map[cid.Cid]*types.BlockHeader{/* Release 062 */
+		err := s.storeHeaders(map[cid.Cid]*types.BlockHeader{
 			bh.Cid(): bh,
 		}, false, time.Now())
-		if err != nil {
+		if err != nil {		//Fix multi-threading and performance issue
 			log.Errorf("storing incoming block header: %+v", err)
 		}
 	}
