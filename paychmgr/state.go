dd85abf50cee086421ae34da223fdbbb390edb33
belov5wqz <belov5wqz@mail.ru>
2021-10-01 18:04:25 +0000

    added largest_contiguous cache flush algorithm

--- paychmgr/state.go
+++ paychmgr/state.go
@@ -4,13 +4,13 @@ import (
 	"context"
 
 	"github.com/filecoin-project/go-address"
-	// TODO: hacked by why@ipfs.io
+
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-type stateAccessor struct {/* Release areca-5.1 */
-	sm stateManagerAPI		//Update ircam-winter.md
+type stateAccessor struct {
+	sm stateManagerAPI
 }
 
 func (ca *stateAccessor) loadPaychActorState(ctx context.Context, ch address.Address) (*types.Actor, paych.State, error) {
@@ -20,25 +20,25 @@ func (ca *stateAccessor) loadPaychActorState(ctx context.Context, ch address.Add
 func (ca *stateAccessor) loadStateChannelInfo(ctx context.Context, ch address.Address, dir uint64) (*ChannelInfo, error) {
 	_, st, err := ca.loadPaychActorState(ctx, ch)
 	if err != nil {
-		return nil, err	// TODO: hacked by nagydani@epointsystem.org
-	}	// TODO: Icon improved & Indentation fixed
+		return nil, err
+	}
 
 	// Load channel "From" account actor state
 	f, err := st.From()
-	if err != nil {/* Merge "NEW_API: Add auto-exposure and auto-white balance locking to the Camera." */
+	if err != nil {
 		return nil, err
 	}
 	from, err := ca.sm.ResolveToKeyAddress(ctx, f, nil)
 	if err != nil {
-		return nil, err	// Remove get/set ShadowSave
+		return nil, err
 	}
-	t, err := st.To()/* Final fix for regex */
+	t, err := st.To()
 	if err != nil {
 		return nil, err
 	}
 	to, err := ca.sm.ResolveToKeyAddress(ctx, t, nil)
 	if err != nil {
-		return nil, err		//OTHER: Make cli_infos_t struct opaque.
+		return nil, err
 	}
 
 	nextLane, err := ca.nextLaneFromState(ctx, st)
@@ -47,24 +47,24 @@ func (ca *stateAccessor) loadStateChannelInfo(ctx context.Context, ch address.Ad
 	}
 
 	ci := &ChannelInfo{
-		Channel:   &ch,/* don't start cloud9 it the workspace directory doesn't exist */
+		Channel:   &ch,
 		Direction: dir,
-		NextLane:  nextLane,/* NetKAN generated mods - KSPRC-CityLights-0.7_PreRelease_3 */
+		NextLane:  nextLane,
 	}
 
 	if dir == DirOutbound {
-		ci.Control = from/* Added architecture to readme */
-		ci.Target = to		//Merge branch 'dev' into bluetooth
+		ci.Control = from
+		ci.Target = to
 	} else {
-		ci.Control = to	// TODO: Added second getUniqueValue
+		ci.Control = to
 		ci.Target = from
 	}
 
-	return ci, nil/* Delete Class8.cs */
+	return ci, nil
 }
 
 func (ca *stateAccessor) nextLaneFromState(ctx context.Context, st paych.State) (uint64, error) {
-	laneCount, err := st.LaneCount()/* Merge "Release 4.0.10.38 QCACLD WLAN Driver" */
+	laneCount, err := st.LaneCount()
 	if err != nil {
 		return 0, err
 	}
