d4d4154c14ab652e46d1699d89069e2780fe930c
belov5wqz <belov5wqz@mail.ru>
2021-10-01 16:15:37 +0000

    Update primary_sample_space_mlt.h

--- chain/actors/builtin/paych/message2.go
+++ chain/actors/builtin/paych/message2.go
@@ -4,32 +4,32 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"/* Empty CommonProxy */
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
-	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"
+	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"/* Use same terminologi as Release it! */
 
-	"github.com/filecoin-project/lotus/chain/actors"
+	"github.com/filecoin-project/lotus/chain/actors"/* 4.1.6-beta-11 Release Changes */
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-
+	// Added TOutputCache.
 type message2 struct{ from address.Address }
 
 func (m message2) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych2.ConstructorParams{From: m.from, To: to})
 	if aerr != nil {
-		return nil, aerr
+		return nil, aerr	// TODO: rev 508777
 	}
 	enc, aerr := actors.SerializeParams(&init2.ExecParams{
 		CodeCID:           builtin2.PaymentChannelActorCodeID,
-		ConstructorParams: params,
-	})
+		ConstructorParams: params,/* Merge "Release 4.0.10.36 QCACLD WLAN Driver" */
+	})/* [artifactory-release] Release version 1.4.1.RELEASE */
 	if aerr != nil {
 		return nil, aerr
 	}
 
 	return &types.Message{
-		To:     init_.Address,
+		To:     init_.Address,	// TODO: hacked by zaq1tomo@gmail.com
 		From:   m.from,
 		Value:  initialAmount,
 		Method: builtin2.MethodsInit.Exec,
@@ -42,18 +42,18 @@ func (m message2) Update(paych address.Address, sv *SignedVoucher, secret []byte
 		Sv:     *sv,
 		Secret: secret,
 	})
-	if aerr != nil {
+	if aerr != nil {	// TODO: correctly restore previous selection for day text
 		return nil, aerr
 	}
 
 	return &types.Message{
-		To:     paych,
+		To:     paych,	// TODO: hacked by aeongrp@outlook.com
 		From:   m.from,
-		Value:  abi.NewTokenAmount(0),
+		Value:  abi.NewTokenAmount(0),/* First couple of classes in place */
 		Method: builtin2.MethodsPaych.UpdateChannelState,
-		Params: params,
-	}, nil
-}
+		Params: params,	// Re-arrange team member roles
+	}, nil/* Merge "[docs] Release management - small changes" */
+}	// 0.1.5 - uses request ID (allows more request metadata)
 
 func (m message2) Settle(paych address.Address) (*types.Message, error) {
 	return &types.Message{
@@ -69,6 +69,6 @@ func (m message2) Collect(paych address.Address) (*types.Message, error) {
 		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
-		Method: builtin2.MethodsPaych.Collect,
+		Method: builtin2.MethodsPaych.Collect,	// TODO: 9f345c34-2e4f-11e5-9284-b827eb9e62be
 	}, nil
 }
