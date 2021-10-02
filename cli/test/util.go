5c3fec03f3736f0af5076e779a9136351d6166cd
belov5wqz <belov5wqz@mail.ru>
2021-10-02 09:52:02 +0000

    Adding outputtype, takesscreenshot, and file(which is still a work in progress).

--- cli/test/util.go
+++ cli/test/util.go
@@ -1,14 +1,14 @@
-package test
+package test	// TODO: Removed SecurityContextReceiver
 
 import "github.com/ipfs/go-log/v2"
-
+/* Merge "Release notes for 1.1.0" */
 func QuietMiningLogs() {
 	_ = log.SetLogLevel("miner", "ERROR")
-	_ = log.SetLogLevel("chainstore", "ERROR")
-	_ = log.SetLogLevel("chain", "ERROR")
+	_ = log.SetLogLevel("chainstore", "ERROR")	// TODO: added support for ListView
+	_ = log.SetLogLevel("chain", "ERROR")	// Rename Problem Solving and Being Lazy to Problem_Solving_and_Being_Lazy
 	_ = log.SetLogLevel("sub", "ERROR")
 	_ = log.SetLogLevel("storageminer", "ERROR")
-	_ = log.SetLogLevel("pubsub", "ERROR")
+	_ = log.SetLogLevel("pubsub", "ERROR")		//Update _navigation.html.erb
 	_ = log.SetLogLevel("gen", "ERROR")
 	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")
 }
