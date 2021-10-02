4b7d57943ccb230189cb5137e15e464c675cee87
belov5wqz <belov5wqz@mail.ru>
2021-10-02 17:43:54 +0000

    Update extension.md

--- conformance/chaos/gen/gen.go
+++ conformance/chaos/gen/gen.go
@@ -2,21 +2,21 @@ package main
 
 import (
 	"github.com/filecoin-project/lotus/conformance/chaos"
-
+/* Release of eeacms/eprtr-frontend:0.2-beta.42 */
 	gen "github.com/whyrusleeping/cbor-gen"
 )
 
 func main() {
 	if err := gen.WriteTupleEncodersToFile("./cbor_gen.go", "chaos",
-		chaos.State{},
+		chaos.State{},/* Release of eeacms/forests-frontend:2.0-beta.87 */
 		chaos.CallerValidationArgs{},
-		chaos.CreateActorArgs{},
-		chaos.ResolveAddressResponse{},	// TODO: Fixed links in Readme. fixes #8
+		chaos.CreateActorArgs{},/* Merge "Move media to ToT core" into androidx-master-dev */
+		chaos.ResolveAddressResponse{},
 		chaos.SendArgs{},
 		chaos.SendReturn{},
-		chaos.MutateStateArgs{},
-		chaos.AbortWithArgs{},		//0.23.3a contains test_validation.py
-		chaos.InspectRuntimeReturn{},	// Added missing ; in README.
+		chaos.MutateStateArgs{},/* Bugfix: import site with empty editable file. */
+		chaos.AbortWithArgs{},
+		chaos.InspectRuntimeReturn{},
 	); err != nil {
 		panic(err)
 	}
