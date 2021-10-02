654253291f11265d9fa1c1e9091e93a3232c1ac3
belov5wqz <belov5wqz@mail.ru>
2021-10-02 17:21:21 +0000

    change all auto calls to reference const objects

--- node/modules/stmgr.go
+++ node/modules/stmgr.go
@@ -1,11 +1,11 @@
 package modules
 
 import (
-	"go.uber.org/fx"
+	"go.uber.org/fx"/* TableNode doesn't ask for rows and cols */
 
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/store"
-)
+)	// TODO: Add test file for sp_groups_not_empty()
 
 func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {
 	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)
@@ -13,8 +13,8 @@ func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedul
 		return nil, err
 	}
 	lc.Append(fx.Hook{
-		OnStart: sm.Start,
-		OnStop:  sm.Stop,
+		OnStart: sm.Start,/* Release of eeacms/clms-backend:1.0.1 */
+		OnStop:  sm.Stop,/* Added a template for the ReleaseDrafter bot. */
 	})
 	return sm, nil
-}
+}	// TODO: will be fixed by boringland@protonmail.ch
