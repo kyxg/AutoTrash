18e9f0882b0ed326ccd6467b479cbc91a64d4baf
belov5wqz <belov5wqz@mail.ru>
2021-10-02 14:11:10 +0000

    Debugging the player instance code and converting more map to unordered_map

--- build/flags.go
+++ build/flags.go
@@ -1,11 +1,11 @@
-package build
-		//:arrow_up: whitespace@0.36.2
+package build/* Merge branch 'master' into ED_408_change_required_msg */
+
 // DisableBuiltinAssets disables the resolution of go.rice boxes that store
-// built-in assets, such as proof parameters, bootstrap peers, genesis blocks,	// Bump otter (again)
+// built-in assets, such as proof parameters, bootstrap peers, genesis blocks,
 // etc.
-//	// Remove unnecessary crons
+///* 4d7aaee6-2e4f-11e5-896c-28cfe91dbc4b */
 // When this value is set to true, it is expected that the user will
-// provide any such configurations through the Lotus API itself.		//Add Fidelity Media SSP
+// provide any such configurations through the Lotus API itself.
 //
 // This is useful when you're using Lotus as a library, such as to orchestrate
 // test scenarios, or for other purposes where you don't need to use the
