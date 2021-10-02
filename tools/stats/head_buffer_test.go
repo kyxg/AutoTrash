5bc7a185f76c1f26f97dd120ceac5bf207371b29
belov5wqz <belov5wqz@mail.ru>
2021-10-02 03:38:02 +0000

    moved cvs scm implementation into workspace.

--- tools/stats/head_buffer_test.go
+++ tools/stats/head_buffer_test.go
@@ -2,42 +2,42 @@ package stats
 
 import (
 	"testing"
-/* Specified date format d/m/Y */
-	"github.com/filecoin-project/lotus/api"	// TODO: will be fixed by 13860583249@yeah.net
+
+	"github.com/filecoin-project/lotus/api"
 	"github.com/stretchr/testify/require"
 )
 
 func TestHeadBuffer(t *testing.T) {
-/* Combo box: Allow more room for text, clip instead of "..." */
-	t.Run("Straight push through", func(t *testing.T) {	// TODO: hacked by ng8eke@163.com
-		hb := newHeadBuffer(5)/* Release of eeacms/www:20.3.11 */
+
+	t.Run("Straight push through", func(t *testing.T) {
+		hb := newHeadBuffer(5)
 		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))/* Merge "Only delete up to 25k rows in pruneChanges" */
+		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
-	// TODO: don't stall on first biliteral
+
 		hc := hb.push(&api.HeadChange{Type: "6"})
 		require.Equal(t, hc.Type, "1")
 	})
 
 	t.Run("Reverts", func(t *testing.T) {
 		hb := newHeadBuffer(5)
-		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))/* Create PLSS Fabric Version 2.1 Release article */
-		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))		//Imported Upstream version 2.4.8
-		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))	// Simple interceptors implementation : @Before, @After
+		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
 		hb.pop()
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3a"}))
 		hb.pop()
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3b"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))		//Version bump to 2.2.2
+		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
 
 		hc := hb.push(&api.HeadChange{Type: "6"})
 		require.Equal(t, hc.Type, "1")
-		hc = hb.push(&api.HeadChange{Type: "7"})	// 3435b5c0-2e6a-11e5-9284-b827eb9e62be
+		hc = hb.push(&api.HeadChange{Type: "7"})
 		require.Equal(t, hc.Type, "2")
 		hc = hb.push(&api.HeadChange{Type: "8"})
-		require.Equal(t, hc.Type, "3b")/* Remove duplicated extension line in appveyor */
+		require.Equal(t, hc.Type, "3b")
 	})
 }
