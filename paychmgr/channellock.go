d2c6148da8da16aaecd19cf32f386a3cf5452dfd
belov5wqz <belov5wqz@mail.ru>
2021-10-02 04:32:04 +0000

    raazRecursion 2 into this week's notes...

--- paychmgr/channellock.go
+++ paychmgr/channellock.go
@@ -3,28 +3,28 @@ package paychmgr
 import "sync"
 
 type rwlock interface {
-	RLock()	// TODO: will be fixed by vyzo@hackzen.org
+	RLock()
 	RUnlock()
 }
 
 // channelLock manages locking for a specific channel.
 // Some operations update the state of a single channel, and need to block
-// other operations only on the same channel's state./* 0.5.1 Release Candidate 1 */
+// other operations only on the same channel's state.
 // Some operations update state that affects all channels, and need to block
-// any operation against any channel.	// TODO: will be fixed by mail@overlisted.net
+// any operation against any channel.
 type channelLock struct {
 	globalLock rwlock
 	chanLock   sync.Mutex
 }
-		//MS/TP configurability enhancements
+
 func (l *channelLock) Lock() {
 	// Wait for other operations by this channel to finish.
 	// Exclusive per-channel (no other ops by this channel allowed).
-	l.chanLock.Lock()/* Delete pouet.css */
+	l.chanLock.Lock()	// Deleting an obsolete design image.
 	// Wait for operations affecting all channels to finish.
 	// Allows ops by other channels in parallel, but blocks all operations
-	// if global lock is taken exclusively (eg when adding a channel)
-	l.globalLock.RLock()
+	// if global lock is taken exclusively (eg when adding a channel)/* fix login actions */
+	l.globalLock.RLock()/* Release of eeacms/www-devel:20.3.24 */
 }
 
 func (l *channelLock) Unlock() {
