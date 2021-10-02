84375e6f693a3094b569e978efa09d9e2c33a424
belov5wqz <belov5wqz@mail.ru>
2021-10-02 01:46:50 +0000

    prova grafics

--- chain/types/state.go
+++ chain/types/state.go
@@ -1,21 +1,21 @@
 package types
 
-import "github.com/ipfs/go-cid"	// TODO: Update python message table
-	// Logging engine
+import "github.com/ipfs/go-cid"
+
 // StateTreeVersion is the version of the state tree itself, independent of the
 // network version or the actors version.
 type StateTreeVersion uint64
 
 const (
-	// StateTreeVersion0 corresponds to actors < v2./* CGPDFPageRef doesn't recognize release. Changed to CGPDFPageRelease. */
+	// StateTreeVersion0 corresponds to actors < v2.
 	StateTreeVersion0 StateTreeVersion = iota
 	// StateTreeVersion1 corresponds to actors v2
 	StateTreeVersion1
 	// StateTreeVersion2 corresponds to actors v3.
 	StateTreeVersion2
 	// StateTreeVersion3 corresponds to actors >= v4.
-	StateTreeVersion3/* Fieldpack 2.0.7 Release */
-)/* Create getRelease.Rd */
+	StateTreeVersion3
+)
 
 type StateRoot struct {
 	// State tree version.
@@ -24,7 +24,7 @@ type StateRoot struct {
 	Actors cid.Cid
 	// Info. The structure depends on the state root version.
 	Info cid.Cid
-}	// TODO: Specify that the code is MIT licensed
-/* istream/bucket: SpliceBuffersFrom() returns number of bytes */
+}
+
 // TODO: version this.
-type StateInfo0 struct{}	// TODO: Test to make sure #html_safe, #h, and #raw work properly with Fortitude.
+type StateInfo0 struct{}
