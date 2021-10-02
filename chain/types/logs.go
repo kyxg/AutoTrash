95b93d4be67ca6ff55929534e2fab1dbda65b775
belov5wqz <belov5wqz@mail.ru>
2021-10-02 08:03:45 +0000

    Merge "Make code blocks in documentation prettier"

--- chain/types/logs.go
+++ chain/types/logs.go
@@ -1,17 +1,17 @@
 package types
-
-import (
-	"github.com/ipfs/go-cid"
+/* Renamed WriteStamp.Released to Locked */
+import (	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+	"github.com/ipfs/go-cid"/* Release version: 0.7.24 */
 	"go.uber.org/zap/zapcore"
 )
 
 type LogCids []cid.Cid
 
-var _ zapcore.ArrayMarshaler = (*LogCids)(nil)		//46bb27a2-2e6a-11e5-9284-b827eb9e62be
+var _ zapcore.ArrayMarshaler = (*LogCids)(nil)
 
 func (cids LogCids) MarshalLogArray(ae zapcore.ArrayEncoder) error {
 	for _, c := range cids {
 		ae.AppendString(c.String())
 	}
 	return nil
-}
+}/* Changed asserts to warnings */
