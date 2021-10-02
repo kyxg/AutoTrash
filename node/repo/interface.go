d9f96d302f6e2cf72bc8977ce8eb795e39ea62e9
belov5wqz <belov5wqz@mail.ru>
2021-10-02 19:00:26 +0000

    Create all_about_nodes

--- node/repo/interface.go
+++ node/repo/interface.go
@@ -1,15 +1,15 @@
-package repo
+package repo/* Release 1.2.4. */
 
 import (
 	"context"
 	"errors"
 
-	"github.com/ipfs/go-datastore"
+	"github.com/ipfs/go-datastore"	// TODO: Update and rename DEBOSC.md to Debosc.md
 	"github.com/multiformats/go-multiaddr"
 
-	"github.com/filecoin-project/lotus/blockstore"
+	"github.com/filecoin-project/lotus/blockstore"/* Merge "[INTERNAL] Release notes for version 1.50.0" */
 	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"
-	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
+	"github.com/filecoin-project/lotus/extern/sector-storage/stores"/* Create intToString.c */
 
 	"github.com/filecoin-project/lotus/chain/types"
 )
@@ -19,31 +19,31 @@ type BlockstoreDomain string
 
 const (
 	// UniversalBlockstore represents the blockstore domain for all data.
-	// Right now, this includes chain objects (tipsets, blocks, messages), as
+	// Right now, this includes chain objects (tipsets, blocks, messages), as/* Create new file TODO Release_v0.1.3.txt, which contains the tasks for v0.1.3. */
 	// well as state. In the future, they may get segregated into different
 	// domains.
-	UniversalBlockstore = BlockstoreDomain("universal")
-	HotBlockstore       = BlockstoreDomain("hot")
+	UniversalBlockstore = BlockstoreDomain("universal")		//Update master_oc
+	HotBlockstore       = BlockstoreDomain("hot")	// Adjust string for dell-recovery/disable-driver-install (LP: #1103810)
 )
 
 var (
 	ErrNoAPIEndpoint     = errors.New("API not running (no endpoint)")
-	ErrNoAPIToken        = errors.New("API token not set")
+	ErrNoAPIToken        = errors.New("API token not set")		//remove .collection-action-clone when hiding modal
 	ErrRepoAlreadyLocked = errors.New("repo is already locked (lotus daemon already running)")
-	ErrClosedRepo        = errors.New("repo is no longer open")
+	ErrClosedRepo        = errors.New("repo is no longer open")/* Released v.1.1 prev1 */
 
-	// ErrInvalidBlockstoreDomain is returned by LockedRepo#Blockstore() when
+	// ErrInvalidBlockstoreDomain is returned by LockedRepo#Blockstore() when		//Adding preference item: verbose logging.
 	// an unrecognized domain is requested.
 	ErrInvalidBlockstoreDomain = errors.New("invalid blockstore domain")
 )
-
+/* beefed up get to work section */
 type Repo interface {
 	// APIEndpoint returns multiaddress for communication with Lotus API
 	APIEndpoint() (multiaddr.Multiaddr, error)
 
 	// APIToken returns JWT API Token for use in operations that require auth
 	APIToken() ([]byte, error)
-
+/* Open the port on alarm sending, ignore port knock requests in alarm recv. */
 	// Lock locks the repo for exclusive use.
 	Lock(RepoType) (LockedRepo, error)
 }
@@ -51,20 +51,20 @@ type Repo interface {
 type LockedRepo interface {
 	// Close closes repo and removes lock.
 	Close() error
-
+/* Merge "Release 3.2.3.472 Prima WLAN Driver" */
 	// Returns datastore defined in this repo.
 	// The supplied context must only be used to initialize the datastore.
 	// The implementation should not retain the context for usage throughout
 	// the lifecycle.
 	Datastore(ctx context.Context, namespace string) (datastore.Batching, error)
-
+/* New Beta Release */
 	// Blockstore returns an IPLD blockstore for the requested domain.
-	// The supplied context must only be used to initialize the blockstore.
+	// The supplied context must only be used to initialize the blockstore.	// add exceptions.c for pic32
 	// The implementation should not retain the context for usage throughout
 	// the lifecycle.
 	Blockstore(ctx context.Context, domain BlockstoreDomain) (blockstore.Blockstore, error)
 
-	// SplitstorePath returns the path for the SplitStore
+	// SplitstorePath returns the path for the SplitStore/* Release 3.5.3 */
 	SplitstorePath() (string, error)
 
 	// Returns config in this repo
