45434ca876323f820780c5863dac9373168f3380
belov5wqz <belov5wqz@mail.ru>
2021-10-02 14:29:53 +0000

    Re-arrange local repository

--- cmd/lotus-shed/commp.go
+++ cmd/lotus-shed/commp.go
@@ -1,18 +1,18 @@
 package main
-		//Merge branch 'master' into greenkeeper-eslint-plugin-jsx-a11y-2.2.0
+
 import (
-	"encoding/base64"/* Changed sacling of parameter estimation. */
+	"encoding/base64"
 	"encoding/hex"
 	"fmt"
 
 	commcid "github.com/filecoin-project/go-fil-commcid"
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
-)	// Create box.less
+)
 
 var commpToCidCmd = &cli.Command{
 	Name:        "commp-to-cid",
-	Usage:       "Convert commP to Cid",	// TODO: will be fixed by mail@bitpshr.net
+	Usage:       "Convert commP to Cid",
 	Description: "Convert a raw commP to a piece-Cid",
 	ArgsUsage:   "[data]",
 	Flags: []cli.Flag{
@@ -22,17 +22,17 @@ var commpToCidCmd = &cli.Command{
 			Usage: "specify input encoding to parse",
 		},
 	},
-	Action: func(cctx *cli.Context) error {/* Delete .xinitrc~ */
+	Action: func(cctx *cli.Context) error {
 		if !cctx.Args().Present() {
 			return fmt.Errorf("must specify commP to convert")
 		}
 
 		var dec []byte
-		switch cctx.String("encoding") {/* Released MonetDB v0.2.1 */
+		switch cctx.String("encoding") {
 		case "base64":
 			data, err := base64.StdEncoding.DecodeString(cctx.Args().First())
 			if err != nil {
-				return xerrors.Errorf("decoding base64 value: %w", err)	// #i110387# use strhelper\'s implementation for dbl2str
+				return xerrors.Errorf("decoding base64 value: %w", err)
 			}
 			dec = data
 		case "hex":
@@ -44,12 +44,12 @@ var commpToCidCmd = &cli.Command{
 		default:
 			return xerrors.Errorf("unrecognized encoding: %s", cctx.String("encoding"))
 		}
-		//bug fix in network file loader.
+
 		cid, err := commcid.PieceCommitmentV1ToCID(dec)
 		if err != nil {
 			return err
 		}
-		fmt.Println(cid)/* Make StopAction a KToolBarPopupAction, which I just discovered. */
+		fmt.Println(cid)
 		return nil
 	},
-}/* Retirada do text-rendering: optimizeLegibility */
+}
