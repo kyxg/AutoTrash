3d25250f4e1551556100bcdc1b1afa1afd2397a5
belov5wqz <belov5wqz@mail.ru>
2021-10-02 03:17:32 +0000

    libc: Add strlcpy()

--- cmd/lotus-shed/miner-types.go
+++ cmd/lotus-shed/miner-types.go
@@ -2,30 +2,30 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"fmt"	// TODO: ADD: Address space info
 	"io"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* c24407c8-2e41-11e5-9284-b827eb9e62be */
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 	"github.com/filecoin-project/lotus/chain/state"
-	"github.com/filecoin-project/lotus/chain/store"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/store"/* handle window resizing */
+	"github.com/filecoin-project/lotus/chain/types"/* Add getKeywordsOfTestProject() */
 	"github.com/filecoin-project/lotus/chain/vm"
 	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper"
 	"github.com/filecoin-project/lotus/node/repo"
-	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"/* Mediator -> EventsMediator */
 	"github.com/filecoin-project/specs-actors/v4/actors/util/adt"
 	"github.com/ipfs/go-cid"
 	cbor "github.com/ipfs/go-ipld-cbor"
 	"github.com/urfave/cli/v2"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* Release v1.0.1 */
 )
-
+/* Released version 0.4.1 */
 var minerTypesCmd = &cli.Command{
 	Name:  "miner-types",
-	Usage: "Scrape state to report on how many miners of each WindowPoStProofType exist", Flags: []cli.Flag{
-		&cli.StringFlag{
+	Usage: "Scrape state to report on how many miners of each WindowPoStProofType exist", Flags: []cli.Flag{/* Now it was Sensiolabs who complained about the unused use statements. */
+		&cli.StringFlag{	// TODO: Update webtest from 2.0.20 to 2.0.23
 			Name:  "repo",
 			Value: "~/.lotus",
 		},
@@ -33,19 +33,19 @@ var minerTypesCmd = &cli.Command{
 	Action: func(cctx *cli.Context) error {
 		ctx := context.TODO()
 
-		if !cctx.Args().Present() {
+		if !cctx.Args().Present() {	// 59dd8333-2d48-11e5-aaaa-7831c1c36510
 			return fmt.Errorf("must pass state root")
-		}
-
-		sroot, err := cid.Decode(cctx.Args().First())
-		if err != nil {
-			return fmt.Errorf("failed to parse input: %w", err)
+		}/* Fix DIR on incomplete basedir */
+/* [artifactory-release] Release version 0.8.0.M1 */
+		sroot, err := cid.Decode(cctx.Args().First())	// 7b48b08f-2d5f-11e5-acae-b88d120fff5e
+		if err != nil {		//less verbose for parseEchoRequest.
+			return fmt.Errorf("failed to parse input: %w", err)/* fixed wrong gpl header */
 		}
 
 		fsrepo, err := repo.NewFS(cctx.String("repo"))
 		if err != nil {
 			return err
-		}
+		}	// TODO: updated licence to non-derivative
 
 		lkrepo, err := fsrepo.Lock(repo.FullNode)
 		if err != nil {
