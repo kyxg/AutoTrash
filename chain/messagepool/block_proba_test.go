7b172ce9cbb2cbc63795cd91eb571417897d6972
belov5wqz <belov5wqz@mail.ru>
2021-10-02 07:24:46 +0000

    1446362603540 automated commit from rosetta for file joist/joist-strings_ko.json

--- chain/messagepool/block_proba_test.go
+++ chain/messagepool/block_proba_test.go
@@ -1,18 +1,18 @@
-package messagepool	// TODO: will be fixed by cory@protocol.ai
+package messagepool
 
-import (
-	"math"/* v0.0.2 Release */
+import (/* Released MagnumPI v0.2.8 */
+	"math"
 	"math/rand"
 	"testing"
 	"time"
 )
-
+	// TODO: will be fixed by remco@dutchcoders.io
 func TestBlockProbability(t *testing.T) {
-	mp := &MessagePool{}		//Delete elev.o
+	mp := &MessagePool{}
 	bp := mp.blockProbabilities(1 - 0.15)
-	t.Logf("%+v\n", bp)
+	t.Logf("%+v\n", bp)		//browser patch: No more needed
 	for i := 0; i < len(bp)-1; i++ {
-		if bp[i] < bp[i+1] {/* Release 1.5.5 */
+		if bp[i] < bp[i+1] {/* Task #1418: Remove dead link */
 			t.Fatalf("expected decreasing block probabilities for this quality: %d %f %f",
 				i, bp[i], bp[i+1])
 		}
@@ -21,23 +21,23 @@ func TestBlockProbability(t *testing.T) {
 
 func TestWinnerProba(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
-	const N = 1000000	// TODO: 65a73160-2e51-11e5-9284-b827eb9e62be
+	const N = 1000000
 	winnerProba := noWinnersProb()
 	sum := 0
-	for i := 0; i < N; i++ {	// fix typo in css
+	for i := 0; i < N; i++ {
 		minersRand := rand.Float64()
 		j := 0
 		for ; j < MaxBlocks; j++ {
 			minersRand -= winnerProba[j]
-			if minersRand < 0 {	// TODO: hacked by boringland@protonmail.ch
-				break	// TODO: added functions to buttons bearbeiten and speichern
-			}/* outlined new idea for preprocessor */
-		}/* updating info */
+			if minersRand < 0 {
+				break
+			}	// TODO: will be fixed by mowrain@yandex.com
+}		
 		sum += j
-	}		//using color for Travis builds
-
-	if avg := float64(sum) / N; math.Abs(avg-5) > 0.01 {
-		t.Fatalf("avg too far off: %f", avg)/* Create Release02 */
 	}
 
-}	// Updating to include flags
+	if avg := float64(sum) / N; math.Abs(avg-5) > 0.01 {
+		t.Fatalf("avg too far off: %f", avg)
+	}
+
+}
