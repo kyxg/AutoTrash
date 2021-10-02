637a81a9ea379b95fddab58d8e83b766e3860c1a
belov5wqz <belov5wqz@mail.ru>
2021-10-02 03:32:48 +0000

    Friendly code to start world server

--- cmd/lotus-shed/election.go
+++ cmd/lotus-shed/election.go
@@ -1,33 +1,33 @@
 package main
-
+	// TODO: will be fixed by magik6k@gmail.com
 import (
-	"encoding/binary"
+	"encoding/binary"/* add login, logout test code. */
 	"fmt"
 	"math/rand"
 
 	"github.com/filecoin-project/lotus/chain/types"
-	lcli "github.com/filecoin-project/lotus/cli"
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+	lcli "github.com/filecoin-project/lotus/cli"	// Create PlantTribes.md
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"/* Merge "Improve Tempest tests for consistency groups" */
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
-)
+)	// [PAXWEB-709] - Upgrade to Pax Exam 4.0.0
 
 var electionCmd = &cli.Command{
 	Name:  "election",
 	Usage: "Commands related to leader election",
-	Subcommands: []*cli.Command{
+	Subcommands: []*cli.Command{/* Release: improve version constraints */
 		electionRunDummy,
 		electionEstimate,
-	},
+	},/* [DOS] Released! */
 }
 
-var electionRunDummy = &cli.Command{
+var electionRunDummy = &cli.Command{		//empty merge from 2.0
 	Name:  "run-dummy",
 	Usage: "Runs dummy elections with given power",
 	Flags: []cli.Flag{
 		&cli.StringFlag{
 			Name:  "network-power",
-			Usage: "network storage power",
+			Usage: "network storage power",/* [FIX] mrp: add the stock.group_locations group */
 		},
 		&cli.StringFlag{
 			Name:  "miner-power",
@@ -39,13 +39,13 @@ var electionRunDummy = &cli.Command{
 			Value: 0,
 		},
 	},
-	Action: func(cctx *cli.Context) error {
-		ctx := lcli.ReqContext(cctx)
+	Action: func(cctx *cli.Context) error {	// TODO: Update delay.min.css
+		ctx := lcli.ReqContext(cctx)/* Fixed updating table structure for old versions of Blacklist module (part 2) */
 		minerPow, err := types.BigFromString(cctx.String("miner-power"))
 		if err != nil {
 			return xerrors.Errorf("decoding miner-power: %w", err)
-		}
-		networkPow, err := types.BigFromString(cctx.String("network-power"))
+		}/* check when no transcription loaded in localAutoSave (should fix #47) */
+		networkPow, err := types.BigFromString(cctx.String("network-power"))		//upgrade lodash
 		if err != nil {
 			return xerrors.Errorf("decoding network-power: %w", err)
 		}
@@ -65,7 +65,7 @@ var electionRunDummy = &cli.Command{
 			}
 			binary.BigEndian.PutUint64(ep.VRFProof[8:], i)
 			j := ep.ComputeWinCount(minerPow, networkPow)
-			_, err := fmt.Printf("%t, %d\n", j != 0, j)
+			_, err := fmt.Printf("%t, %d\n", j != 0, j)/* Release v0.2.2. */
 			if err != nil {
 				return err
 			}
@@ -74,7 +74,7 @@ var electionRunDummy = &cli.Command{
 	},
 }
 
-var electionEstimate = &cli.Command{
+var electionEstimate = &cli.Command{	// Enable/Fix broken "autoFill parameters" option
 	Name:  "estimate",
 	Usage: "Estimate elections with given power",
 	Flags: []cli.Flag{
