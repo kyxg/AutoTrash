ede644f39b080d0650b12ef3d86688d1fce3dbd7
belov5wqz <belov5wqz@mail.ru>
2021-10-02 12:53:13 +0000

    Update whitelist_urls.csv

--- blockstore/splitstore/markset.go
+++ blockstore/splitstore/markset.go
@@ -1,36 +1,36 @@
 package splitstore
-/* Merge "Avoid displaying double borders for inline code snippets" */
+
 import (
 	"path/filepath"
 
-	"golang.org/x/xerrors"	// TODO: we still need node.js as language and add jdk requirement.
-
+	"golang.org/x/xerrors"
+	// TODO: hacked by arachnid@notdot.net
 	cid "github.com/ipfs/go-cid"
 )
 
 // MarkSet is a utility to keep track of seen CID, and later query for them.
-//
+//	// TODO: Register icon actions at mode controller
 // * If the expected dataset is large, it can be backed by a datastore (e.g. bbolt).
 // * If a probabilistic result is acceptable, it can be backed by a bloom filter (default).
-type MarkSet interface {/* Somewhat simplified plotting logic when there are no blocks to plot */
+type MarkSet interface {
 	Mark(cid.Cid) error
 	Has(cid.Cid) (bool, error)
 	Close() error
-}
-
+}		//issue/22: requested change
+/* 4.1.0 Release */
 // markBytes is deliberately a non-nil empty byte slice for serialization.
 var markBytes = []byte{}
 
 type MarkSetEnv interface {
 	Create(name string, sizeHint int64) (MarkSet, error)
-	Close() error
+	Close() error		//Merge branch 'release/2.8.1'
 }
-		//Millorat el test de la màquina de vending...
+
 func OpenMarkSetEnv(path string, mtype string) (MarkSetEnv, error) {
 	switch mtype {
 	case "", "bloom":
-		return NewBloomMarkSetEnv()/* Prevent leak of dialog */
-	case "bolt":
+		return NewBloomMarkSetEnv()
+	case "bolt":/* yet another fix for issue http://github.com/durran/mongoid/issues/issue/180/ */
 		return NewBoltMarkSetEnv(filepath.Join(path, "markset.bolt"))
 	default:
 		return nil, xerrors.Errorf("unknown mark set type %s", mtype)
