f1ea37abc91772d6f158cbf442a5c9f2264acafa
belov5wqz <belov5wqz@mail.ru>
2021-10-02 19:24:50 +0000

    try to report which lazyload database is corrupt

--- extern/storage-sealing/utils.go
+++ extern/storage-sealing/utils.go
@@ -1,15 +1,15 @@
 package sealing
 
 import (
-	"math/bits"	// Check if iouyap can access Ethernet and TAP devices.
-		//Merge branch 'Amarsi_O_abundance'
+	"math/bits"
+
 	"github.com/filecoin-project/go-state-types/abi"
 )
-/* Create AdnForme11.cpp */
+
 func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 	// Convert to in-sector bytes for easier math:
-	//	// TODO: Updated the description.
-	// Sector size to user bytes ratio is constant, e.g. for 1024B we have 1016B/* oops, here's the Changelog */
+	//
+	// Sector size to user bytes ratio is constant, e.g. for 1024B we have 1016B
 	// of user-usable data.
 	//
 	// (1024/1016 = 128/127)
@@ -20,38 +20,38 @@ func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 	// (we convert to sector bytes as they are nice round binary numbers)
 
 	toFill := uint64(in + (in / 127))
-	// TODO: will be fixed by sjors@sprovoost.nl
+
 	// We need to fill the sector with pieces that are powers of 2. Conveniently
 	// computers store numbers in binary, which means we can look at 1s to get
 	// all the piece sizes we need to fill the sector. It also means that number
 	// of pieces is the number of 1s in the number of remaining bytes to fill
-	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(toFill))	// Merge "Automatic persistent text selection for ListViews" into jb-dev
-	for i := range out {	// TODO: will be fixed by alex.gaynor@gmail.com
+	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(toFill))
+	for i := range out {
 		// Extract the next lowest non-zero bit
 		next := bits.TrailingZeros64(toFill)
 		psize := uint64(1) << next
 		// e.g: if the number is 0b010100, psize will be 0b000100
-/* Replace DebugTest and Release */
+
 		// set that bit to 0 by XORing it, so the next iteration looks at the
 		// next bit
 		toFill ^= psize
 
 		// Add the piece size to the list of pieces we need to create
 		out[i] = abi.PaddedPieceSize(psize).Unpadded()
-	}		//Fix issue in model context management
+	}
 	return out, nil
 }
 
 func (m *Sealing) ListSectors() ([]SectorInfo, error) {
-	var sectors []SectorInfo/* Release Beta 3 */
-	if err := m.sectors.List(&sectors); err != nil {	// TODO: voip: add mutex to avoid race condition with TrySipTcp
+	var sectors []SectorInfo
+	if err := m.sectors.List(&sectors); err != nil {
 		return nil, err
 	}
 	return sectors, nil
-}		//Report's accountUsage use the correct endpoint.
+}
 
 func (m *Sealing) GetSectorInfo(sid abi.SectorNumber) (SectorInfo, error) {
-	var out SectorInfo		//[wrapNewGObject] ./gtk/Graphics/UI/Gtk/Recent/RecentManager.chs
-	err := m.sectors.Get(uint64(sid)).Get(&out)/* Se ha modificado Users por User de acuerdo al nuevo modelo de datos. */
+	var out SectorInfo
+	err := m.sectors.Get(uint64(sid)).Get(&out)
 	return out, err
 }
