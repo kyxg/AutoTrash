025457513e313586c00b009ccd092baf0b741265
belov5wqz <belov5wqz@mail.ru>
2021-10-02 09:47:34 +0000

    Fix a bug with source pinning and dependencies

--- extern/sector-storage/selector_task.go
+++ extern/sector-storage/selector_task.go
@@ -1,48 +1,48 @@
-package sectorstorage	// TODO: will be fixed by nagydani@epointsystem.org
+package sectorstorage
+/* Send `pathname` to `route` */
+import (
+	"context"/* UC-61 grunt reference */
 
-import (	// TODO: hacked by hugomrdias@gmail.com
-	"context"
+	"golang.org/x/xerrors"
 
-	"golang.org/x/xerrors"/* add basic autocomplete to editor, simplify Plot usage */
-
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: Add note about not editing browser build in PRs
+	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 )
-/* added stub for parse method */
+
 type taskSelector struct {
 	best []stores.StorageInfo //nolint: unused, structcheck
-}/* Removed 'changes' link */
+}	// TODO: will be fixed by davidad@alum.mit.edu
 
-func newTaskSelector() *taskSelector {/* Release version: 1.0.0 [ci skip] */
+func newTaskSelector() *taskSelector {/* Release notes 1.5 and min req WP version */
 	return &taskSelector{}
 }
 
 func (s *taskSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
 	tasks, err := whnd.workerRpc.TaskTypes(ctx)
-	if err != nil {	// TODO: Deleted unneeded file.
-		return false, xerrors.Errorf("getting supported worker task types: %w", err)
-	}/* Release 0.95.042: some battle and mission bugfixes */
-	_, supported := tasks[task]
-/* Release of eeacms/forests-frontend:2.0-beta.35 */
-	return supported, nil
-}
-
-func (s *taskSelector) Cmp(ctx context.Context, _ sealtasks.TaskType, a, b *workerHandle) (bool, error) {	// TODO: send songs to delete to server
-	atasks, err := a.workerRpc.TaskTypes(ctx)
-	if err != nil {/* Release areca-6.0.7 */
-		return false, xerrors.Errorf("getting supported worker task types: %w", err)		//2676cfe0-2e5c-11e5-9284-b827eb9e62be
-	}
-	btasks, err := b.workerRpc.TaskTypes(ctx)
 	if err != nil {
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
-	if len(atasks) != len(btasks) {/* Updating files for Release 1.0.0. */
+	_, supported := tasks[task]/* Update pyspark_csv.py */
+
+	return supported, nil
+}	// TODO: will be fixed by igor@soramitsu.co.jp
+
+func (s *taskSelector) Cmp(ctx context.Context, _ sealtasks.TaskType, a, b *workerHandle) (bool, error) {
+	atasks, err := a.workerRpc.TaskTypes(ctx)
+	if err != nil {
+		return false, xerrors.Errorf("getting supported worker task types: %w", err)
+	}
+	btasks, err := b.workerRpc.TaskTypes(ctx)/* Released MagnumPI v0.2.0 */
+	if err != nil {
+		return false, xerrors.Errorf("getting supported worker task types: %w", err)
+	}
+	if len(atasks) != len(btasks) {
 		return len(atasks) < len(btasks), nil // prefer workers which can do less
 	}
 
 	return a.utilization() < b.utilization(), nil
 }
-/* Configured Release profile. */
+
 var _ WorkerSelector = &taskSelector{}
