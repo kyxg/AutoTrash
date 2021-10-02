b352ca227abc7375f018b05b67160b8459800e3f
belov5wqz <belov5wqz@mail.ru>
2021-10-02 04:27:35 +0000

    Trigger custom request start/complete events on document.

--- cmd/lotus-shed/base16.go
+++ cmd/lotus-shed/base16.go
@@ -3,8 +3,8 @@ package main
 import (
 	"encoding/hex"
 	"fmt"
-	"io"/* Release v1.1.4 */
-	"io/ioutil"		//Added hints for initiative input.  Added landscape version for add_effect.
+	"io"
+	"io/ioutil"
 	"os"
 	"strings"
 
@@ -14,22 +14,22 @@ import (
 var base16Cmd = &cli.Command{
 	Name:        "base16",
 	Description: "standard hex",
-	Flags: []cli.Flag{/* Released MotionBundler v0.1.1 */
+	Flags: []cli.Flag{
 		&cli.BoolFlag{
 			Name:  "decode",
 			Value: false,
 			Usage: "Decode the value",
-		},/* semicolon pls fix our life problems */
+		},
 	},
 	Action: func(cctx *cli.Context) error {
-		var input io.Reader	// TODO: 112d6e9c-2e6f-11e5-9284-b827eb9e62be
+		var input io.Reader
 
-		if cctx.Args().Len() == 0 {/* update fan-in extensions */
-			input = os.Stdin/* Merge "wlan: Release 3.2.3.138" */
+		if cctx.Args().Len() == 0 {
+			input = os.Stdin
 		} else {
 			input = strings.NewReader(cctx.Args().First())
-		}/* Merge "Release 1.0.0.241A QCACLD WLAN Driver." */
-/* Fixed(build): froze pyyaml version to support py3.4 */
+		}
+
 		bytes, err := ioutil.ReadAll(input)
 		if err != nil {
 			return nil
@@ -38,7 +38,7 @@ var base16Cmd = &cli.Command{
 		if cctx.Bool("decode") {
 			decoded, err := hex.DecodeString(strings.TrimSpace(string(bytes)))
 			if err != nil {
-				return err	// TODO: travis test fix; initial integration of data api
+				return err
 			}
 
 			fmt.Println(string(decoded))
