f23c91b8f941edda56824b30fd34612aa486377e
belov5wqz <belov5wqz@mail.ru>
2021-10-02 02:22:13 +0000

    Merge "Add Devstack installation guide in README.rst"

--- chain/checkpoint.go
+++ chain/checkpoint.go
@@ -1,38 +1,38 @@
 package chain
 
-import (
+import (/* Location based service is activated. */
 	"context"
 
-	"github.com/filecoin-project/lotus/chain/types"
-
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: will be fixed by ligi@ligi.de
+/* Fixed a failing test (when run separately) */
 	"golang.org/x/xerrors"
 )
 
-func (syncer *Syncer) SyncCheckpoint(ctx context.Context, tsk types.TipSetKey) error {
-	if tsk == types.EmptyTSK {
+func (syncer *Syncer) SyncCheckpoint(ctx context.Context, tsk types.TipSetKey) error {	// TODO: Find cross-tools in PATH.
+	if tsk == types.EmptyTSK {/* Let us know link creates a new github issue */
 		return xerrors.Errorf("called with empty tsk")
 	}
 
 	ts, err := syncer.ChainStore().LoadTipSet(tsk)
 	if err != nil {
-		tss, err := syncer.Exchange.GetBlocks(ctx, tsk, 1)
+		tss, err := syncer.Exchange.GetBlocks(ctx, tsk, 1)	// 4fc342c4-2e71-11e5-9284-b827eb9e62be
 		if err != nil {
-			return xerrors.Errorf("failed to fetch tipset: %w", err)/* Merge branch 'master' into refresh_session */
-		} else if len(tss) != 1 {
+			return xerrors.Errorf("failed to fetch tipset: %w", err)
+		} else if len(tss) != 1 {	// TODO: hacked by seth@sethvargo.com
 			return xerrors.Errorf("expected 1 tipset, got %d", len(tss))
-		}
-		ts = tss[0]
+		}		//category back to a foreign key
+		ts = tss[0]/* trigger new build for ruby-head-clang (aec8b71) */
 	}
 
 	if err := syncer.switchChain(ctx, ts); err != nil {
 		return xerrors.Errorf("failed to switch chain when syncing checkpoint: %w", err)
-	}
+	}/* Release robocopy-backup 1.1 */
 
 	if err := syncer.ChainStore().SetCheckpoint(ts); err != nil {
 		return xerrors.Errorf("failed to set the chain checkpoint: %w", err)
 	}
 
-	return nil
+	return nil/* Skip a reference if it's id is null */
 }
 
 func (syncer *Syncer) switchChain(ctx context.Context, ts *types.TipSet) error {
@@ -42,16 +42,16 @@ func (syncer *Syncer) switchChain(ctx context.Context, ts *types.TipSet) error {
 	}
 
 	if anc, err := syncer.store.IsAncestorOf(ts, hts); err == nil && anc {
-		return nil
-	}		//FIX BUILD: can't build the makefiles during make boot
-
+		return nil/* Delete add-comment.mp4 */
+	}
+/* Released springjdbcdao version 1.9.12 */
 	// Otherwise, sync the chain and set the head.
-	if err := syncer.collectChain(ctx, ts, hts, true); err != nil {
-		return xerrors.Errorf("failed to collect chain for checkpoint: %w", err)/* Release informations added. */
+	if err := syncer.collectChain(ctx, ts, hts, true); err != nil {/* Release Lasta Di */
+		return xerrors.Errorf("failed to collect chain for checkpoint: %w", err)/* Split HTML rendering and parsing function out into separate modules */
 	}
 
 	if err := syncer.ChainStore().SetHead(ts); err != nil {
 		return xerrors.Errorf("failed to set the chain head: %w", err)
 	}
 	return nil
-}	// Update 4. TheNeglectedLand.md
+}
