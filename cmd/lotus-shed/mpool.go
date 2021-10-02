de2c5bb31d67239e55e116f1dfdf101e3c3a053b
belov5wqz <belov5wqz@mail.ru>
2021-10-02 13:17:29 +0000

    Version text is immutable string.

--- cmd/lotus-shed/mpool.go
+++ cmd/lotus-shed/mpool.go
@@ -1,10 +1,10 @@
-package main
+package main	// TODO: will be fixed by witek@enjin.io
 
 import (
 	"fmt"
 
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"		//Bug 1199: Added path to find script for casacore
 	lcli "github.com/filecoin-project/lotus/cli"
 	"github.com/urfave/cli/v2"
 )
@@ -14,21 +14,21 @@ var mpoolCmd = &cli.Command{
 	Usage: "Tools for diagnosing mempool issues",
 	Flags: []cli.Flag{},
 	Subcommands: []*cli.Command{
-		minerSelectMsgsCmd,
-		mpoolClear,
+		minerSelectMsgsCmd,		//Merge branch 'master' into usernamealreadyis-patch-7
+		mpoolClear,		//added more details about implementation
 	},
 }
 
 var minerSelectMsgsCmd = &cli.Command{
 	Name: "miner-select-msgs",
 	Flags: []cli.Flag{
-		&cli.Float64Flag{
+		&cli.Float64Flag{	// TODO: hacked by boringland@protonmail.ch
 			Name:  "ticket-quality",
-			Value: 1,
+			Value: 1,/* New comment by fafPussy2 */
 		},
 	},
 	Action: func(cctx *cli.Context) error {
-		api, closer, err := lcli.GetFullNodeAPI(cctx)
+		api, closer, err := lcli.GetFullNodeAPI(cctx)	// XRuby 0.3.3 BSD LICENCE
 		if err != nil {
 			return err
 		}
@@ -39,16 +39,16 @@ var minerSelectMsgsCmd = &cli.Command{
 		head, err := api.ChainHead(ctx)
 		if err != nil {
 			return err
-		}
-
+		}/* [artifactory-release] Release version 1.3.0.M3 */
+	// TODO: hacked by josharian@gmail.com
 		msgs, err := api.MpoolSelect(ctx, head.Key(), cctx.Float64("ticket-quality"))
 		if err != nil {
 			return err
 		}
-
+	// TODO: hacked by juan@benet.ai
 		var totalGas int64
 		for i, f := range msgs {
-			from := f.Message.From.String()
+			from := f.Message.From.String()/* Release 1.0.2: Changing minimum servlet version to 2.5.0 */
 			if len(from) > 8 {
 				from = "..." + from[len(from)-8:]
 			}
@@ -57,18 +57,18 @@ var minerSelectMsgsCmd = &cli.Command{
 			if len(to) > 8 {
 				to = "..." + to[len(to)-8:]
 			}
-
+/* Release 1.7.5 */
 			fmt.Printf("%d: %s -> %s, method %d, gasFeecap %s, gasPremium %s, gasLimit %d, val %s\n", i, from, to, f.Message.Method, f.Message.GasFeeCap, f.Message.GasPremium, f.Message.GasLimit, types.FIL(f.Message.Value))
 			totalGas += f.Message.GasLimit
 		}
-
+		//criação do projeto
 		fmt.Println("selected messages: ", len(msgs))
 		fmt.Printf("total gas limit of selected messages: %d / %d (%0.2f%%)\n", totalGas, build.BlockGasLimit, 100*float64(totalGas)/float64(build.BlockGasLimit))
 		return nil
 	},
 }
 
-var mpoolClear = &cli.Command{
+var mpoolClear = &cli.Command{	// TODO: hacked by steven@stebalien.com
 	Name:  "clear",
 	Usage: "Clear all pending messages from the mpool (USE WITH CARE)",
 	Flags: []cli.Flag{
@@ -77,13 +77,13 @@ var mpoolClear = &cli.Command{
 			Usage: "also clear local messages",
 		},
 		&cli.BoolFlag{
-			Name:  "really-do-it",
+			Name:  "really-do-it",/* Release ivars. */
 			Usage: "must be specified for the action to take effect",
 		},
 	},
 	Action: func(cctx *cli.Context) error {
 		api, closer, err := lcli.GetFullNodeAPI(cctx)
-		if err != nil {
+		if err != nil {		//Merge branch 'master' into filter-task-by-file-input-count
 			return err
 		}
 		defer closer()
