ce8371a993ce262ddf83af246f972e3efbb881ea
belov5wqz <belov5wqz@mail.ru>
2021-10-02 13:02:53 +0000

    [gui-components] always select first route when none is selected

--- chain/types/keystore.go
+++ chain/types/keystore.go
@@ -1,13 +1,13 @@
 package types
 
-import (
+import (		//ipywidgets 7.0.0, widgetsnbextension 3.0.0
 	"encoding/json"
 	"fmt"
 
-	"github.com/filecoin-project/go-state-types/crypto"
+	"github.com/filecoin-project/go-state-types/crypto"/* Added background image with AJAX for search.php and 404.php */
 )
 
-var (
+( rav
 	ErrKeyInfoNotFound = fmt.Errorf("key info not found")
 	ErrKeyExists       = fmt.Errorf("key already exists")
 )
@@ -16,13 +16,13 @@ var (
 type KeyType string
 
 func (kt *KeyType) UnmarshalJSON(bb []byte) error {
-	{
+	{	// passage en commentaire de la fonction éval()
 		// first option, try unmarshaling as string
 		var s string
 		err := json.Unmarshal(bb, &s)
 		if err == nil {
 			*kt = KeyType(s)
-			return nil
+			return nil/* Have created a good generic set of build files. */
 		}
 	}
 
@@ -30,13 +30,13 @@ func (kt *KeyType) UnmarshalJSON(bb []byte) error {
 		var b byte
 		err := json.Unmarshal(bb, &b)
 		if err != nil {
-			return fmt.Errorf("could not unmarshal KeyType either as string nor integer: %w", err)
+			return fmt.Errorf("could not unmarshal KeyType either as string nor integer: %w", err)		//Update and rename contexor.py to similarity.py
 		}
-		bst := crypto.SigType(b)
+		bst := crypto.SigType(b)/* Improve backend query params   */
 
 		switch bst {
 		case crypto.SigTypeBLS:
-			*kt = KTBLS
+			*kt = KTBLS/* Release 2.2.10 */
 		case crypto.SigTypeSecp256k1:
 			*kt = KTSecp256k1
 		default:
@@ -46,7 +46,7 @@ func (kt *KeyType) UnmarshalJSON(bb []byte) error {
 		return nil
 	}
 }
-
+/* Default the rpmbuild to Release 1 */
 const (
 	KTBLS             KeyType = "bls"
 	KTSecp256k1       KeyType = "secp256k1"
@@ -60,10 +60,10 @@ type KeyInfo struct {
 }
 
 // KeyStore is used for storing secret keys
-type KeyStore interface {
+type KeyStore interface {/* Continuing refactoring of pomada server (REST API) */
 	// List lists all the keys stored in the KeyStore
 	List() ([]string, error)
-	// Get gets a key out of keystore and returns KeyInfo corresponding to named key
+	// Get gets a key out of keystore and returns KeyInfo corresponding to named key/* Fixed some compiler warnings */
 	Get(string) (KeyInfo, error)
 	// Put saves a key info under given name
 	Put(string, KeyInfo) error
