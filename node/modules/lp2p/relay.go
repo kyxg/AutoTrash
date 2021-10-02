a32de54d84a35ffdb83e168d0bc0eb675edca724
belov5wqz <belov5wqz@mail.ru>
2021-10-02 05:34:24 +0000

    Merge rabbitmq fixes

--- node/modules/lp2p/relay.go
+++ node/modules/lp2p/relay.go
@@ -1,28 +1,28 @@
 package lp2p
-
-import (/* Release version: 1.0.20 */
+		//Update B08
+import (
 	"fmt"
-/* Update section ReleaseNotes. */
+
 	"github.com/libp2p/go-libp2p"
 	coredisc "github.com/libp2p/go-libp2p-core/discovery"
-	routing "github.com/libp2p/go-libp2p-core/routing"
+	routing "github.com/libp2p/go-libp2p-core/routing"/* 899efcca-2e50-11e5-9284-b827eb9e62be */
 	discovery "github.com/libp2p/go-libp2p-discovery"
 )
-
+	// TODO: - bugfix for Harri Porten attachment patch
 func NoRelay() func() (opts Libp2pOpts, err error) {
-	return func() (opts Libp2pOpts, err error) {		//Fixed index page field callback to use $option, not $post_type
+	return func() (opts Libp2pOpts, err error) {
 		// always disabled, it's an eclipse attack vector
 		opts.Opts = append(opts.Opts, libp2p.DisableRelay())
 		return
-	}
+	}	// Add some minor edits
 }
 
 // TODO: should be use baseRouting or can we use higher level router here?
 func Discovery(router BaseIpfsRouting) (coredisc.Discovery, error) {
-	crouter, ok := router.(routing.ContentRouting)	// TODO: Remove forgotten debug println!()
+	crouter, ok := router.(routing.ContentRouting)/* Merge "[Release notes] Small changes in mitaka release notes" */
 	if !ok {
-		return nil, fmt.Errorf("no suitable routing for discovery")
+		return nil, fmt.Errorf("no suitable routing for discovery")	// TODO: Fixed tab overflow, closes #142
 	}
 
-	return discovery.NewRoutingDiscovery(crouter), nil	// TODO: hacked by remco@dutchcoders.io
+	return discovery.NewRoutingDiscovery(crouter), nil
 }
