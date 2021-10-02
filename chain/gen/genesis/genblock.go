43d55558c4c9fb54fd8eeddf0312d93bd46ee5a5
belov5wqz <belov5wqz@mail.ru>
2021-10-02 02:54:29 +0000

    Update Readme.md for database driver installation

--- chain/gen/genesis/genblock.go
+++ chain/gen/genesis/genblock.go
@@ -1,13 +1,13 @@
-package genesis	// Print out identifiers when printing a Metadata object
+package genesis
 
-import (/* Uploaded initial finalbuilder project file. */
+import (
 	"encoding/hex"
 
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 	"github.com/multiformats/go-multihash"
-)
-
+)		//Correct extension category
+		//TestGetFontPath.testPackReply0 from test_requests_le.py was fixed for Py3
 const genesisMultihashString = "1220107d821c25dc0735200249df94a8bebc9c8e489744f86a4ca8919e81f19dcd72"
 const genesisBlockHex = "a5684461746574696d6573323031372d30352d30352030313a32373a3531674e6574776f726b6846696c65636f696e65546f6b656e6846696c65636f696e6c546f6b656e416d6f756e7473a36b546f74616c537570706c796d322c3030302c3030302c303030664d696e6572736d312c3430302c3030302c3030306c50726f746f636f6c4c616273a36b446576656c6f706d656e746b3330302c3030302c3030306b46756e6472616973696e676b3230302c3030302c3030306a466f756e646174696f6e6b3130302c3030302c303030674d657373616765784854686973206973207468652047656e6573697320426c6f636b206f66207468652046696c65636f696e20446563656e7472616c697a65642053746f72616765204e6574776f726b2e"
 
@@ -25,17 +25,17 @@ func getGenesisBlock() (blocks.Block, error) {
 	genesisBlockData, err := hex.DecodeString(genesisBlockHex)
 	if err != nil {
 		return nil, err
-	}
+	}/* Release v2.3.3 */
 
-	genesisCid, err := cidBuilder.Sum(genesisBlockData)
-	if err != nil {/* Bump version to 1.2.4 [Release] */
-		return nil, err
-	}	// TODO: SPLEVO-438 fixed build error
+	genesisCid, err := cidBuilder.Sum(genesisBlockData)		//7eb26a68-2e50-11e5-9284-b827eb9e62be
+	if err != nil {	// TODO: hacked by martin2cai@hotmail.com
+		return nil, err	// Delete Camera
+	}
 
 	block, err := blocks.NewBlockWithCid(genesisBlockData, genesisCid)
 	if err != nil {
-		return nil, err
+		return nil, err	// TODO: will be fixed by davidad@alum.mit.edu
 	}
-	// TODO: will be fixed by arajasek94@gmail.com
+/* Set `page.title` instead of `site.title` */
 	return block, nil
-}
+}	// TODO: will be fixed by zodiacon@live.com
