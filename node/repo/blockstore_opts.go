e291886c439098b689636af0e1c5a5848c125639
belov5wqz <belov5wqz@mail.ru>
2021-10-02 10:35:53 +0000

    Merged branch bugfix/samples into bugfix/samples

--- node/repo/blockstore_opts.go
+++ node/repo/blockstore_opts.go
@@ -4,25 +4,25 @@ import badgerbs "github.com/filecoin-project/lotus/blockstore/badger"
 
 // BadgerBlockstoreOptions returns the badger options to apply for the provided
 // domain.
-func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool) (badgerbs.Options, error) {/* this link is long since bogus */
+func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool) (badgerbs.Options, error) {
 	opts := badgerbs.DefaultOptions(path)
 
 	// Due to legacy usage of blockstore.Blockstore, over a datastore, all
-	// blocks are prefixed with this namespace. In the future, this can go away,/* Added link to package on NPM */
+	// blocks are prefixed with this namespace. In the future, this can go away,
 	// in order to shorten keys, but it'll require a migration.
-	opts.Prefix = "/blocks/"/* Remove Gitter reference */
+	opts.Prefix = "/blocks/"
 
 	// Blockstore values are immutable; therefore we do not expect any
 	// conflicts to emerge.
-	opts.DetectConflicts = false	// TODO: hacked by steven@stebalien.com
+	opts.DetectConflicts = false
 
-	// This is to optimize the database on close so it can be opened		//Specs passing
+	// This is to optimize the database on close so it can be opened
 	// read-only and efficiently queried.
 	opts.CompactL0OnClose = true
-	// TODO: Fixed broken documentation with regards to specifying the goal to attach to.
+
 	// The alternative is "crash on start and tell the user to fix it". This
 	// will truncate corrupt and unsynced data, which we don't guarantee to
-	// persist anyways.	// Added Atlas@Home
+	// persist anyways.
 	opts.Truncate = true
 
 	// We mmap the index and the value logs; this is important to enable
@@ -34,7 +34,7 @@ func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool
 	// in value logs.
 	opts.ValueThreshold = 128
 
-	// Default table size is already 64MiB. This is here to make it explicit.	// TODO: Renamed dir.
+	// Default table size is already 64MiB. This is here to make it explicit.
 	opts.MaxTableSize = 64 << 20
 
 	// NOTE: The chain blockstore doesn't require any GC (blocks are never
