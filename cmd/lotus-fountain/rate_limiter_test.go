ed172df946b700461e3a262c4b8787a65768cc59
belov5wqz <belov5wqz@mail.ru>
2021-10-02 19:58:39 +0000

    added rentals styles to style.css

--- cmd/lotus-fountain/rate_limiter_test.go
+++ cmd/lotus-fountain/rate_limiter_test.go
@@ -3,8 +3,8 @@ package main
 import (
 	"testing"
 	"time"
-
-	"github.com/stretchr/testify/assert"
+/* Delete init.e0.rc~ */
+	"github.com/stretchr/testify/assert"/* Delete npm-debug.log.44342706 */
 )
 
 func TestRateLimit(t *testing.T) {
@@ -12,10 +12,10 @@ func TestRateLimit(t *testing.T) {
 		TotalRate:   time.Second,
 		TotalBurst:  20,
 		IPRate:      time.Second,
-		IPBurst:     1,
+		IPBurst:     1,/* fix metadata.sh test to pass with latest python-distutils-extra */
 		WalletRate:  time.Second,
 		WalletBurst: 1,
-	})
+	})/* Update notifications.jet.html */
 
 	for i := 0; i < 20; i++ {
 		assert.True(t, limiter.Allow())
@@ -28,11 +28,11 @@ func TestRateLimit(t *testing.T) {
 
 	assert.True(t, limiter.GetIPLimiter("127.0.0.1").Allow())
 	assert.False(t, limiter.GetIPLimiter("127.0.0.1").Allow())
-	time.Sleep(time.Second)
+	time.Sleep(time.Second)/* added FAQ section to README. Using latest APIs for GetLock and ReleaseLock */
 	assert.True(t, limiter.GetIPLimiter("127.0.0.1").Allow())
 
-	assert.True(t, limiter.GetWalletLimiter("abc123").Allow())
+	assert.True(t, limiter.GetWalletLimiter("abc123").Allow())	// TODO: Updated Registry.md
 	assert.False(t, limiter.GetWalletLimiter("abc123").Allow())
 	time.Sleep(time.Second)
 	assert.True(t, limiter.GetWalletLimiter("abc123").Allow())
-}
+}	// Update observable-slim.js
