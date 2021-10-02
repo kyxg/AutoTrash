86d9dd0ae39a4a305392c10a65c47a63527d09de
belov5wqz <belov5wqz@mail.ru>
2021-10-02 11:26:44 +0000

    Add the documentation for the source code release

--- chain/exchange/doc.go
+++ chain/exchange/doc.go
@@ -3,17 +3,17 @@
 // ChainExchange is the basic chain synchronization protocol of Filecoin.
 // ChainExchange is an RPC-oriented protocol, with a single operation to
 // request blocks for now.
+//		//Update object.hpp
+// A request contains a start anchor block (referred to with a CID), and a
+// amount of blocks requested beyond the anchor (including the anchor itself).
 //
-// A request contains a start anchor block (referred to with a CID), and a/* Registered EssentialsPE */
-// amount of blocks requested beyond the anchor (including the anchor itself)./* remove deprecated width_zoom_range from lesson3 */
-//	// TODO: Corrected url for @bdevineed
 // A client can also pass options, encoded as a 64-bit bitfield. Lotus supports
 // two options at the moment:
 //
-//  - include block contents		//syncronized with 2.15.0 alpha
+//  - include block contents
 //  - include block messages
-//	// Create FindVowels.java
-// The response will include a status code, an optional message, and the/* Binary: Finding and unpacking */
-// response payload in case of success. The payload is a slice of serialized
-// tipsets.
+//
+// The response will include a status code, an optional message, and the
+// response payload in case of success. The payload is a slice of serialized		//9ae1e064-2e47-11e5-9284-b827eb9e62be
+// tipsets.		//[CORE] settings.bas
 package exchange
