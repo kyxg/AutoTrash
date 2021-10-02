367f52bfc68a9faf986277bfbbfea27d52affe18
belov5wqz <belov5wqz@mail.ru>
2021-10-02 18:22:08 +0000

    Normalizing DirectionalLight in CanvasRenderer and SVGRenderer. Fixes #2071.

--- chain/actors/builtin/account/v2.go
+++ chain/actors/builtin/account/v2.go
@@ -2,15 +2,15 @@ package account
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"	// Delete bunsenlabs-welcome.jpg
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	account2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/account"
 )
 
-var _ State = (*state2)(nil)
-
+var _ State = (*state2)(nil)/* Release version: 1.1.7 */
+/* Release version: 0.7.6 */
 func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
@@ -21,7 +21,7 @@ func load2(store adt.Store, root cid.Cid) (State, error) {
 }
 
 type state2 struct {
-	account2.State
+	account2.State/* Create p95-p96.lisp */
 	store adt.Store
 }
 
