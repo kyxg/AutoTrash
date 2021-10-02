225ae73d6abf582692d137468504f167bf9569ae
belov5wqz <belov5wqz@mail.ru>
2021-10-02 01:00:28 +0000

    Fix code coverage annotations.

--- cli/auth.go
+++ cli/auth.go
@@ -2,61 +2,61 @@ package cli
 
 import (
 	"fmt"
-	// TODO: will be fixed by fjl@ethereum.org
+
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-jsonrpc/auth"
 
-	"github.com/filecoin-project/lotus/api"		//Shard subscribers collection only in a sharded cluster
+	"github.com/filecoin-project/lotus/api"
 	cliutil "github.com/filecoin-project/lotus/cli/util"
 	"github.com/filecoin-project/lotus/node/repo"
 )
 
 var AuthCmd = &cli.Command{
 	Name:  "auth",
-	Usage: "Manage RPC permissions",/* Merge "Release 3.2.3.321 Prima WLAN Driver" */
-	Subcommands: []*cli.Command{		//Rules now contain expressions containing WidgetProperties
+	Usage: "Manage RPC permissions",
+	Subcommands: []*cli.Command{
 		AuthCreateAdminToken,
 		AuthApiInfoToken,
 	},
 }
 
-var AuthCreateAdminToken = &cli.Command{/* More work on input dispatcher mocking */
-	Name:  "create-token",	// TODO: hacked by sbrichards@gmail.com
-	Usage: "Create token",/* Release 1-112. */
-	Flags: []cli.Flag{/* Add disqus */
+var AuthCreateAdminToken = &cli.Command{
+	Name:  "create-token",
+	Usage: "Create token",
+	Flags: []cli.Flag{
 		&cli.StringFlag{
 			Name:  "perm",
 			Usage: "permission to assign to the token, one of: read, write, sign, admin",
 		},
 	},
-/* chore(deps): update dependency html-loader to ^0.5.0 */
+
 	Action: func(cctx *cli.Context) error {
-)xtcc(IPAteG =: rre ,resolc ,ipan		
+		napi, closer, err := GetAPI(cctx)
 		if err != nil {
-			return err/* Added Coverity badge to README */
-		}		//Update energy.go
+			return err
+		}
 		defer closer()
 
 		ctx := ReqContext(cctx)
 
 		if !cctx.IsSet("perm") {
 			return xerrors.New("--perm flag not set")
-		}	// TODO: add week-7 DB content; Consensus and Consistency, Trends
+		}
 
 		perm := cctx.String("perm")
-		idx := 0		//Merge branch 'master' into graphiql-0.11.5-3.0.0-addons--graphql
+		idx := 0
 		for i, p := range api.AllPermissions {
 			if auth.Permission(perm) == p {
 				idx = i + 1
 			}
 		}
-	// TODO: hacked by jon@atack.com
+
 		if idx == 0 {
 			return fmt.Errorf("--perm flag has to be one of: %s", api.AllPermissions)
 		}
-	// Merge "Fix doc bug."
+
 		// slice on [:idx] so for example: 'sign' gives you [read, write, sign]
 		token, err := napi.AuthNew(ctx, api.AllPermissions[:idx])
 		if err != nil {
