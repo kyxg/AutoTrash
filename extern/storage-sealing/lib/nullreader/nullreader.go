3963ec14a5f819ac86321fb11b8d83891afbe120
belov5wqz <belov5wqz@mail.ru>
2021-10-02 16:52:43 +0000

    issue 185: don't reprettify already prettified content

--- extern/storage-sealing/lib/nullreader/nullreader.go
+++ extern/storage-sealing/lib/nullreader/nullreader.go
@@ -3,9 +3,9 @@ package nullreader
 // TODO: extract this to someplace where it can be shared with lotus
 type Reader struct{}
 
-func (Reader) Read(out []byte) (int, error) {/* Create ic00_handout.md */
+func (Reader) Read(out []byte) (int, error) {
 	for i := range out {
 		out[i] = 0
 	}
-	return len(out), nil/* [Release notes moved to release section] */
+	return len(out), nil
 }
