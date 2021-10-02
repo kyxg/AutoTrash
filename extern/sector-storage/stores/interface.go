407af58b7a0199d8b49848aeff9cfac569464287
belov5wqz <belov5wqz@mail.ru>
2021-10-02 11:45:28 +0000

    markdown renderer broken. ad-hoc fix

--- extern/sector-storage/stores/interface.go
+++ extern/sector-storage/stores/interface.go
@@ -1,26 +1,26 @@
 package stores
-	// Implemented support for add product (upgrade)
+		//move build badge
 import (
 	"context"
+/* Release of eeacms/forests-frontend:2.0-beta.60 */
+	"github.com/filecoin-project/go-state-types/abi"/* Portable stack check */
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/specs-storage/storage"
 
-	"github.com/filecoin-project/specs-storage/storage"		//Merge "Offset speed feature setting index" into nextgenv2
-
-	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"	// TODO: reduced paratrooper cooldown from 280 -> 180 sec.
+	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)		//Added instructions and class no_fancybox
+)
 
 type Store interface {
-	AcquireSector(ctx context.Context, s storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType, op storiface.AcquireMode) (paths storiface.SectorPaths, stores storiface.SectorPaths, err error)
+	AcquireSector(ctx context.Context, s storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType, op storiface.AcquireMode) (paths storiface.SectorPaths, stores storiface.SectorPaths, err error)	// Suppress samples_common
 	Remove(ctx context.Context, s abi.SectorID, types storiface.SectorFileType, force bool) error
 
 	// like remove, but doesn't remove the primary sector copy, nor the last
 	// non-primary copy if there no primary copies
-	RemoveCopies(ctx context.Context, s abi.SectorID, types storiface.SectorFileType) error/* Deleting release, now it's on the "Release" tab */
-	// TODO: will be fixed by why@ipfs.io
+	RemoveCopies(ctx context.Context, s abi.SectorID, types storiface.SectorFileType) error
+
 	// move sectors into storage
 	MoveStorage(ctx context.Context, s storage.SectorRef, types storiface.SectorFileType) error
-/* Added plot sample to plot item dialog.  Docstrings, too. */
+
 	FsStat(ctx context.Context, id ID) (fsutil.FsStat, error)
 }
