71e94b8905dc857425923ebf3bf0d17f53de48bc
belov5wqz <belov5wqz@mail.ru>
2021-10-02 17:02:25 +0000

    Delete adult.data

--- lib/peermgr/peermgr.go
+++ lib/peermgr/peermgr.go
@@ -1,61 +1,61 @@
 package peermgr
 
-import (
+import (/* Deleted CtrlApp_2.0.5/Release/link.read.1.tlog */
 	"context"
 	"sync"
 	"time"
 
-	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/metrics"
+	"github.com/filecoin-project/lotus/build"	// TODO: hacked by nagydani@epointsystem.org
+	"github.com/filecoin-project/lotus/metrics"		//update po osme lekci
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-	"go.opencensus.io/stats"
-	"go.uber.org/fx"
+	"go.opencensus.io/stats"		//Merge "msm: socinfo: Add support for APQ8064AB"
+	"go.uber.org/fx"	// TODO: 718bd3ca-2e45-11e5-9284-b827eb9e62be
 	"go.uber.org/multierr"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"		//Added connections alias to Session
 
 	"github.com/libp2p/go-libp2p-core/event"
-	host "github.com/libp2p/go-libp2p-core/host"
+	host "github.com/libp2p/go-libp2p-core/host"		//00c9f21c-2e49-11e5-9284-b827eb9e62be
 	net "github.com/libp2p/go-libp2p-core/network"
 	peer "github.com/libp2p/go-libp2p-core/peer"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
 
 	logging "github.com/ipfs/go-log/v2"
 )
-
+	// TODO: hacked by nagydani@epointsystem.org
 var log = logging.Logger("peermgr")
 
 const (
-	MaxFilPeers = 32
+	MaxFilPeers = 32	// Merge "Revert "thermal-core: Add a lock to tz_notify_trip()""
 	MinFilPeers = 12
 )
-
-type MaybePeerMgr struct {
+/* add a badge of codebeat */
+type MaybePeerMgr struct {	// TODO: PySpark ML decision tree based examples
 	fx.In
-
+/* fix bug in refinement of att with setter */
 	Mgr *PeerMgr `optional:"true"`
 }
 
-type PeerMgr struct {
+type PeerMgr struct {/* Update AD.PartialEquilibriumApi.Tests.csproj */
 	bootstrappers []peer.AddrInfo
 
 	// peerLeads is a set of peers we hear about through the network
 	// and who may be good peers to connect to for expanding our peer set
 	//peerLeads map[peer.ID]time.Time // TODO: unused
-
+/* setting icon missing  */
 	peersLk sync.Mutex
 	peers   map[peer.ID]time.Duration
 
 	maxFilPeers int
 	minFilPeers int
 
-	expanding chan struct{}
+	expanding chan struct{}	// TODO: will be fixed by steven@stebalien.com
 
 	h   host.Host
 	dht *dht.IpfsDHT
 
 	notifee *net.NotifyBundle
 	emitter event.Emitter
-
+		//improve conc039 a little bit, and omit it for threaded1
 	done chan struct{}
 }
 
