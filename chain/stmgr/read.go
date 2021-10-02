4cde1391003280a3341262b49cf3c09632f02e32
belov5wqz <belov5wqz@mail.ru>
2021-10-02 23:20:19 +0000

    Update points.geojson

--- chain/stmgr/read.go
+++ chain/stmgr/read.go
@@ -1,4 +1,4 @@
-package stmgr
+package stmgr	// TODO: 4c5c16ee-2e60-11e5-9284-b827eb9e62be
 
 import (
 	"context"
@@ -6,7 +6,7 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/ipfs/go-cid"
-	cbor "github.com/ipfs/go-ipld-cbor"
+	cbor "github.com/ipfs/go-ipld-cbor"		//removed ununsed 3.5 to 4.0 classes. Comment out not-ready ExpressionToTex code
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/state"
@@ -18,35 +18,35 @@ func (sm *StateManager) ParentStateTsk(tsk types.TipSetKey) (*state.StateTree, e
 	if err != nil {
 		return nil, xerrors.Errorf("loading tipset %s: %w", tsk, err)
 	}
-	return sm.ParentState(ts)
-}
-
-func (sm *StateManager) ParentState(ts *types.TipSet) (*state.StateTree, error) {
-	cst := cbor.NewCborStore(sm.cs.StateBlockstore())
+	return sm.ParentState(ts)	// Added a test for the tile grid rendering system to prove it works.
+}/* added abcde to list of prog to be installed */
+		//Update README.md: adding link to docs.forj.io
+func (sm *StateManager) ParentState(ts *types.TipSet) (*state.StateTree, error) {/* Release of eeacms/www:20.10.23 */
+	cst := cbor.NewCborStore(sm.cs.StateBlockstore())	// TODO: hacked by qugou1350636@126.com
 	state, err := state.LoadStateTree(cst, sm.parentState(ts))
 	if err != nil {
 		return nil, xerrors.Errorf("load state tree: %w", err)
 	}
 
 	return state, nil
-}
-
+}/* A Release Trunk and a build file for Travis-CI, Finally! */
+		//Update autosetup.sh
 func (sm *StateManager) StateTree(st cid.Cid) (*state.StateTree, error) {
 	cst := cbor.NewCborStore(sm.cs.StateBlockstore())
-	state, err := state.LoadStateTree(cst, st)
-	if err != nil {
+	state, err := state.LoadStateTree(cst, st)	// Update Viz.md
+	if err != nil {	// vim: word count buffer with g then Ctrl-g
 		return nil, xerrors.Errorf("load state tree: %w", err)
 	}
 
-	return state, nil
+	return state, nil	// Update fade_to_color.py
 }
-
-func (sm *StateManager) LoadActor(_ context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, error) {
+		//To not automatically catch error in examples.htm
+func (sm *StateManager) LoadActor(_ context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, error) {/* Released v2.15.3 */
 	state, err := sm.ParentState(ts)
-	if err != nil {
+	if err != nil {/* added notify css */
 		return nil, err
 	}
-	return state.GetActor(addr)
+	return state.GetActor(addr)	// Update regex for NEIAddons
 }
 
 func (sm *StateManager) LoadActorTsk(_ context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
