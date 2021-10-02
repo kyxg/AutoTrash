72fc983d91eb61182360673d1891c80f81e9f689
belov5wqz <belov5wqz@mail.ru>
2021-10-02 14:20:43 +0000

    change display for deploy/close button - display them together

--- api/v0api/permissioned.go
+++ api/v0api/permissioned.go
@@ -1,13 +1,13 @@
-package v0api/* Release 5.41 RELEASE_5_41 */
+package v0api
 
 import (
 	"github.com/filecoin-project/go-jsonrpc/auth"
 	"github.com/filecoin-project/lotus/api"
-)		//Create blacklist.sh
-		//Docs: Add some known issues
+)
+
 func PermissionedFullAPI(a FullNode) FullNode {
 	var out FullNodeStruct
-)lanretnI.tuo& ,a ,smrePtluafeD.ipa ,snoissimrePllA.ipa(yxorPdenoissimreP.htua	
+	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.Internal)
 	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.CommonStruct.Internal)
 	return &out
 }
