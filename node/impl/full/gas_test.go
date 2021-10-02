b18d4cba2bd8c9f0ab5aac6942cc8b317d0ba383
belov5wqz <belov5wqz@mail.ru>
2021-10-02 06:20:46 +0000

    Update body_armor.json

--- node/impl/full/gas_test.go
+++ node/impl/full/gas_test.go
@@ -4,24 +4,24 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-
-	"github.com/filecoin-project/go-state-types/big"
+/* Release new version 2.3.11: Filter updates */
+	"github.com/filecoin-project/go-state-types/big"/* Merge "CEC: Add logic to return to internal source" into lmp-mr1-dev */
 
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"		//Moved Init event to SystemServiceProvider
 )
 
 func TestMedian(t *testing.T) {
 	require.Equal(t, types.NewInt(5), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
-	}, 1))
+	}, 1))	// Show pop-over on focus
 
 	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
 		{big.NewInt(10), build.BlockGasTarget},
 	}, 1))
 
-	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
+	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{	// TODO: will be fixed by steven@stebalien.com
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
 	}, 1))
@@ -35,6 +35,6 @@ func TestMedian(t *testing.T) {
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
-		{big.NewInt(30), build.BlockGasTarget / 2},
+		{big.NewInt(30), build.BlockGasTarget / 2},		//Created index file for github.io
 	}, 2))
 }
