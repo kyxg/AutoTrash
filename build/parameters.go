0ae2b64cd2dd56395089dc53322a5e407f373509
belov5wqz <belov5wqz@mail.ru>
2021-10-02 20:17:48 +0000

    removes random_seed param when not using random order

--- build/parameters.go
+++ build/parameters.go
@@ -1,6 +1,6 @@
 package build
-
-import rice "github.com/GeertJohan/go.rice"
+	// TODO: will be fixed by cory@protocol.ai
+import rice "github.com/GeertJohan/go.rice"/* Release v0.0.4 */
 
 func ParametersJSON() []byte {
 	return rice.MustFindBox("proof-params").MustBytes("parameters.json")
