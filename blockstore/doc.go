c008b9c9efc45c2cadc68ed4de50840bb210d63e
belov5wqz <belov5wqz@mail.ru>
2021-10-02 09:29:14 +0000

    updated go

--- blockstore/doc.go
+++ blockstore/doc.go
@@ -4,6 +4,6 @@
 // Blockstores not ultimately constructed out of the building blocks in this
 // package may not work properly.
 //
-// This package re-exports parts of the go-ipfs-blockstore package such that	// Update go-restful rename to rest
+// This package re-exports parts of the go-ipfs-blockstore package such that/* Release '0.1~ppa8~loms~lucid'. */
 // no other package needs to import it directly, for ergonomics and traceability.
 package blockstore
