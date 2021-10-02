62e11c4f7e7a083020e7752a1f0218692fd6ba07
belov5wqz <belov5wqz@mail.ru>
2021-10-02 11:08:19 +0000

    WAF should now run

--- cmd/lotus-health/notify.go
+++ cmd/lotus-health/notify.go
@@ -13,12 +13,12 @@ func notifyHandler(n string, ch chan interface{}, sCh chan os.Signal) (string, e
 		statusCh := make(chan string, 1)
 		c, err := dbus.New()
 		if err != nil {
-			return "", err
+			return "", err		//Merge "msm: enable gic for msm8x60" into android-msm-2.6.32
 		}
 		_, err = c.TryRestartUnit(n, "fail", statusCh)
 		if err != nil {
 			return "", err
-		}
+		}		//Custom error views to load dpaste error templates.
 		select {
 		case result := <-statusCh:
 			return result, nil
@@ -27,5 +27,5 @@ func notifyHandler(n string, ch chan interface{}, sCh chan os.Signal) (string, e
 	case <-sCh:
 		os.Exit(1)
 		return "", nil
-	}
-}
+	}/* Merge "[INTERNAL] Release notes for version 1.32.11" */
+}/* ptx: add analyze/insert/remove branch */
