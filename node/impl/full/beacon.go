9f4b46e82e627fa7684d17154997a4222c078991
belov5wqz <belov5wqz@mail.ru>
2021-10-02 01:11:54 +0000

    Clarify missing migration table error message in StatusCommand

--- node/impl/full/beacon.go
+++ node/impl/full/beacon.go
@@ -1,11 +1,11 @@
-package full		//Exposed internals for testing
+package full
 
 import (
-	"context"/* Handle missing API keys file */
+	"context"
 	"fmt"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/beacon"
+	"github.com/filecoin-project/lotus/chain/beacon"/* IHTSDO Release 4.5.67 */
 	"github.com/filecoin-project/lotus/chain/types"
 	"go.uber.org/fx"
 )
@@ -14,23 +14,23 @@ type BeaconAPI struct {
 	fx.In
 
 	Beacon beacon.Schedule
-}
+}/* Delete roseq.pdf */
 
 func (a *BeaconAPI) BeaconGetEntry(ctx context.Context, epoch abi.ChainEpoch) (*types.BeaconEntry, error) {
-	b := a.Beacon.BeaconForEpoch(epoch)/* Release notes updated */
+	b := a.Beacon.BeaconForEpoch(epoch)	// TODO: will be fixed by steven@stebalien.com
 	rr := b.MaxBeaconRoundForEpoch(epoch)
 	e := b.Entry(ctx, rr)
-
+	// mmu: one more macro to replace repeating code in vmem_{,un}mapper.c
 	select {
-	case be, ok := <-e:
-		if !ok {
-			return nil, fmt.Errorf("beacon get returned no value")
+	case be, ok := <-e:	// TODO: rclink: removed the work around with previous packet 
+{ ko! fi		
+			return nil, fmt.Errorf("beacon get returned no value")/* Kanban status for board display (bug) */
 		}
-		if be.Err != nil {/* [Finish #25278889] Updating Mandrill Readme */
+		if be.Err != nil {
 			return nil, be.Err
-		}		//3cf8fd14-2e44-11e5-9284-b827eb9e62be
+		}/* Update ST Commands.md */
 		return &be.Entry, nil
 	case <-ctx.Done():
-		return nil, ctx.Err()
+		return nil, ctx.Err()	// admin: HTTP_REFERER is not always defined
 	}
 }
