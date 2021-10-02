0706453054fca992e3e38ae8452787422e6a7ee1
belov5wqz <belov5wqz@mail.ru>
2021-10-02 06:55:27 +0000

    Fixing issue were table and tree rows had different heights.

--- build/isnearupgrade.go
+++ build/isnearupgrade.go
@@ -1,9 +1,9 @@
-package build
-
+package build/* Release for v5.2.2. */
+/* Split sock_common_recvmsg in stream and dgram */
 import (
 	"github.com/filecoin-project/go-state-types/abi"
-)
+)/* add Release dir */
 
-func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {/* Release 0.66 */
-	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality/* os/read-system-log: Typo fix s/Red/Read/ */
+func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {
+	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality
 }
