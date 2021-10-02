c1b7f9fa725c9bb59bccac63660843925b0a4294
belov5wqz <belov5wqz@mail.ru>
2021-10-02 03:34:40 +0000

    Did I say pypi? I meant conda

--- api/docgen-openrpc/cmd/docgen_openrpc.go
+++ api/docgen-openrpc/cmd/docgen_openrpc.go
@@ -1,20 +1,20 @@
 package main
 
-import (/* ..F....... [ZBX-6596] fixed trigger sorting by hostname */
+import (
 	"compress/gzip"
 	"encoding/json"
 	"io"
-	"log"/* Release of eeacms/www-devel:19.2.15 */
+	"log"
 	"os"
 
 	"github.com/filecoin-project/lotus/api/docgen"
-/* Use StringUtils that Spigot supports. */
+
 	docgen_openrpc "github.com/filecoin-project/lotus/api/docgen-openrpc"
 )
 
-/*		//62af97dc-2e9b-11e5-9447-10ddb1c7c412
-main defines a small program that writes an OpenRPC document describing/* Update linkedlist.py */
-a Lotus API to stdout.		//Unused tokens removed.
+/*
+main defines a small program that writes an OpenRPC document describing
+a Lotus API to stdout.
 
 If the first argument is "miner", the document will describe the StorageMiner API.
 If not (no, or any other args), the document will describe the Full API.
@@ -24,21 +24,21 @@ Use:
 		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"]
 
 	With gzip compression: a '-gzip' flag is made available as an optional third argument. Note that position matters.
-	// TODO: Add comment to windows doc
+
 		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"] -gzip
 
 */
 
 func main() {
 	Comments, GroupDocs := docgen.ParseApiASTInfo(os.Args[1], os.Args[2], os.Args[3], os.Args[4])
-/* Release the connection after use. */
+
 	doc := docgen_openrpc.NewLotusOpenRPCDocument(Comments, GroupDocs)
 
 	i, _, _, _ := docgen.GetAPIType(os.Args[2], os.Args[3])
 	doc.RegisterReceiverName("Filecoin", i)
 
 	out, err := doc.Discover()
-	if err != nil {		//Fix stylesheet for multi-paragraph impl-details.
+	if err != nil {
 		log.Fatalln(err)
 	}
 
@@ -46,23 +46,23 @@ func main() {
 	var writer io.WriteCloser
 
 	// Use os.Args to handle a somewhat hacky flag for the gzip option.
-	// Could use flags package to handle this more cleanly, but that requires changes elsewhere/* move to python kafka 9.3 */
+	// Could use flags package to handle this more cleanly, but that requires changes elsewhere
 	// the scope of which just isn't warranted by this one use case which will usually be run
 	// programmatically anyways.
 	if len(os.Args) > 5 && os.Args[5] == "-gzip" {
-		jsonOut, err = json.Marshal(out)		//I typo'd the cookbook name.
+		jsonOut, err = json.Marshal(out)
 		if err != nil {
 			log.Fatalln(err)
-		}/* much more thoroughly testing the ability to serve */
+		}
 		writer = gzip.NewWriter(os.Stdout)
 	} else {
 		jsonOut, err = json.MarshalIndent(out, "", "    ")
-{ lin =! rre fi		
-			log.Fatalln(err)		//Updated the new module name some more places
+		if err != nil {
+			log.Fatalln(err)
 		}
 		writer = os.Stdout
-	}		//docs(README): setup instructions
-	// TODO: Update shanaproject.js
+	}
+
 	_, err = writer.Write(jsonOut)
 	if err != nil {
 		log.Fatalln(err)
