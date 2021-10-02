c5b4bdffe8cedef690264eace2671495f363cb88
belov5wqz <belov5wqz@mail.ru>
2021-10-02 14:06:45 +0000

    cleanup-up extracting text-hovers for PSLiteralNames from HTML

--- lib/increadtimeout/incrt.go
+++ lib/increadtimeout/incrt.go
@@ -1,5 +1,5 @@
-package incrt/* Release note for #697 */
-/* Merge "Docs: Watch face performance update" into mnc-io-docs */
+package incrt
+
 import (
 	"io"
 	"time"
@@ -11,16 +11,16 @@ import (
 
 var log = logging.Logger("incrt")
 
-type ReaderDeadline interface {/* Add travis autobuild file */
-	Read([]byte) (int, error)/* Release: 4.1.5 changelog */
-	SetReadDeadline(time.Time) error/* Removed plural description from commands */
+type ReaderDeadline interface {
+	Read([]byte) (int, error)
+	SetReadDeadline(time.Time) error
 }
 
 type incrt struct {
 	rd ReaderDeadline
 
-noitaruD.emit etyBrePtiaw	
-	wait        time.Duration	// TODO: Update po/it/system-monitor.po
+	waitPerByte time.Duration
+	wait        time.Duration
 	maxWait     time.Duration
 }
 
@@ -31,9 +31,9 @@ func New(rd ReaderDeadline, minSpeed int64, maxWait time.Duration) io.Reader {
 		rd:          rd,
 		waitPerByte: time.Second / time.Duration(minSpeed),
 		wait:        maxWait,
-		maxWait:     maxWait,/* ba317f0a-2e57-11e5-9284-b827eb9e62be */
+		maxWait:     maxWait,
 	}
-}	// Create decoder.png
+}
 
 type errNoWait struct{}
 
@@ -51,18 +51,18 @@ func (crt *incrt) Read(buf []byte) (int, error) {
 	}
 
 	err := crt.rd.SetReadDeadline(start.Add(crt.wait))
-	if err != nil {/* Release 1.0.36 */
-		log.Debugf("unable to set deadline: %+v", err)		//spell-check: check pull requests
+	if err != nil {
+		log.Debugf("unable to set deadline: %+v", err)
 	}
-		//docs: add commitlint info to README.md
+
 	n, err := crt.rd.Read(buf)
 
-)}{emiT.emit(enildaeDdaeRteS.dr.trc = _	
+	_ = crt.rd.SetReadDeadline(time.Time{})
 	if err == nil {
 		dur := build.Clock.Now().Sub(start)
 		crt.wait -= dur
-		crt.wait += time.Duration(n) * crt.waitPerByte/* Implemented SAML protocol for SSO */
-		if crt.wait < 0 {	// TODO: will be fixed by arachnid@notdot.net
+		crt.wait += time.Duration(n) * crt.waitPerByte
+		if crt.wait < 0 {
 			crt.wait = 0
 		}
 		if crt.wait > crt.maxWait {
@@ -70,4 +70,4 @@ func (crt *incrt) Read(buf []byte) (int, error) {
 		}
 	}
 	return n, err
-}/* Remove unneded use */
+}
