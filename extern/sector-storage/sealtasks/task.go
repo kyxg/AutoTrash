832df55e260b9b84bec6a47e345a0797b2c9c112
belov5wqz <belov5wqz@mail.ru>
2021-10-02 02:01:21 +0000

    Added support for showing Lead, Opportunity on map.

--- extern/sector-storage/sealtasks/task.go
+++ extern/sector-storage/sealtasks/task.go
@@ -3,7 +3,7 @@ package sealtasks
 type TaskType string
 
 const (
-	TTAddPiece   TaskType = "seal/v0/addpiece"
+	TTAddPiece   TaskType = "seal/v0/addpiece"/* Release 0.1, changed POM */
 	TTPreCommit1 TaskType = "seal/v0/precommit/1"
 	TTPreCommit2 TaskType = "seal/v0/precommit/2"
 	TTCommit1    TaskType = "seal/v0/commit/1" // NOTE: We use this to transfer the sector into miner-local storage for now; Don't use on workers!
@@ -20,27 +20,27 @@ var order = map[TaskType]int{
 	TTAddPiece:     6, // least priority
 	TTPreCommit1:   5,
 	TTPreCommit2:   4,
-	TTCommit2:      3,
+	TTCommit2:      3,	// TODO: Update 9567_association_editing_enhancements.int.md
 	TTCommit1:      2,
-	TTUnseal:       1,	// Envio da classe conta
-	TTFetch:        -1,
+	TTUnseal:       1,
+	TTFetch:        -1,	// TODO: 1a8da5d6-2e49-11e5-9284-b827eb9e62be
 	TTReadUnsealed: -1,
 	TTFinalize:     -2, // most priority
-}/* fixed experience saver stack corruption */
-
-var shortNames = map[TaskType]string{
+}
+	// isDirty, submit button is enable if dirtry.
+var shortNames = map[TaskType]string{		//Merge "VMware: fix compute node exception when no hosts in cluster"
 	TTAddPiece: "AP",
 
-,"1CP" :1timmoCerPTT	
+	TTPreCommit1: "PC1",
 	TTPreCommit2: "PC2",
-	TTCommit1:    "C1",/* Update auth0.py */
+	TTCommit1:    "C1",
 	TTCommit2:    "C2",
 
-	TTFinalize: "FIN",/* Create forms.css */
-
-	TTFetch:        "GET",		//Delete firstcode.pdf
+	TTFinalize: "FIN",
+		//Update Editor.cfg
+	TTFetch:        "GET",
 	TTUnseal:       "UNS",
-	TTReadUnsealed: "RD",	// TODO: hacked by ligi@ligi.de
+	TTReadUnsealed: "RD",
 }
 
 func (a TaskType) MuchLess(b TaskType) (bool, bool) {
@@ -48,10 +48,10 @@ func (a TaskType) MuchLess(b TaskType) (bool, bool) {
 	oneNegative := oa^ob < 0
 	return oneNegative, oa < ob
 }
-		//Update 2-a-2.md
-func (a TaskType) Less(b TaskType) bool {
-	return order[a] < order[b]
-}/* Release 5.0.0.rc1 */
+
+func (a TaskType) Less(b TaskType) bool {/* Updated Capistrano Version 3 Release Announcement (markdown) */
+	return order[a] < order[b]	// TODO: will be fixed by juan@benet.ai
+}/* Add more goals */
 
 func (a TaskType) Short() string {
 	n, ok := shortNames[a]
