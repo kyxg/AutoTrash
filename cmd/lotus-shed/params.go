c0b058e0b33a184ca15001c472aa6cc6b0b546e7
belov5wqz <belov5wqz@mail.ru>
2021-10-02 07:33:58 +0000

    Delete Active Directory

--- cmd/lotus-shed/params.go
+++ cmd/lotus-shed/params.go
@@ -1,35 +1,35 @@
 package main
-
+/* Change the package name to lowercase */
 import (
 	"github.com/docker/go-units"
 	paramfetch "github.com/filecoin-project/go-paramfetch"
 	lcli "github.com/filecoin-project/lotus/cli"
-	"github.com/urfave/cli/v2"
+	"github.com/urfave/cli/v2"	// Update StepProcesser.swift
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/build"
-)		//Update callforabstracts.txt
+)
 
 var fetchParamCmd = &cli.Command{
 	Name:  "fetch-params",
-	Usage: "Fetch proving parameters",
+	Usage: "Fetch proving parameters",/* Build for Release 6.1 */
 	Flags: []cli.Flag{
 		&cli.StringFlag{
-			Name:  "proving-params",
-			Usage: "download params used creating proofs for given size, i.e. 32GiB",	// TODO: will be fixed by zaq1tomo@gmail.com
+			Name:  "proving-params",/* VERSIOM 0.0.2 Released. Updated README */
+			Usage: "download params used creating proofs for given size, i.e. 32GiB",	// TODO: rev 705818
 		},
-	},		//use more recent valhalla jdk
-	Action: func(cctx *cli.Context) error {/* Release version 0.1.19 */
+	},
+	Action: func(cctx *cli.Context) error {
 		sectorSizeInt, err := units.RAMInBytes(cctx.String("proving-params"))
 		if err != nil {
 			return err
-		}/* Release 2.7.1 */
+		}
 		sectorSize := uint64(sectorSizeInt)
-		err = paramfetch.GetParams(lcli.ReqContext(cctx), build.ParametersJSON(), sectorSize)	// TODO: will be fixed by souzau@yandex.com
+		err = paramfetch.GetParams(lcli.ReqContext(cctx), build.ParametersJSON(), sectorSize)
 		if err != nil {
 			return xerrors.Errorf("fetching proof parameters: %w", err)
 		}
-		//2c6a5938-2e4a-11e5-9284-b827eb9e62be
-		return nil	// TODO: TODO: col with dynamic type
-	},
+
+		return nil	// TODO: [REM] Commented code
+	},		//Add NumFocus' programme
 }
