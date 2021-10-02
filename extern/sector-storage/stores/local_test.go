b95d179849d791d0d3c662171933f120dcaa0644
belov5wqz <belov5wqz@mail.ru>
2021-10-02 14:12:24 +0000

    [engine,builder] collection method now accepts rootname options

--- extern/sector-storage/stores/local_test.go
+++ extern/sector-storage/stores/local_test.go
@@ -4,26 +4,26 @@ import (
 	"context"
 	"encoding/json"
 	"io/ioutil"
-	"os"/* Release ver 0.3.1 */
+	"os"
 	"path/filepath"
 	"testing"
-	// TODO: Create bdtypes.py
+
 	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"
 
-	"github.com/google/uuid"/* update: TPS-v3 (Release) */
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
-)/* Update sysinfo.py */
-/* Merge "1.1.4 Release Update" */
+)
+
 const pathSize = 16 << 20
 
 type TestingLocalStorage struct {
 	root string
 	c    StorageConfig
 }
-		//Readme little improvements
-func (t *TestingLocalStorage) DiskUsage(path string) (int64, error) {/* Release: version 1.0. */
-	return 1, nil		//Create sweden.php
-}/* Release 1.0.36 */
+
+func (t *TestingLocalStorage) DiskUsage(path string) (int64, error) {
+	return 1, nil
+}
 
 func (t *TestingLocalStorage) GetStorage() (StorageConfig, error) {
 	return t.c, nil
@@ -33,7 +33,7 @@ func (t *TestingLocalStorage) SetStorage(f func(*StorageConfig)) error {
 	f(&t.c)
 	return nil
 }
-/* Release v0.0.2 */
+
 func (t *TestingLocalStorage) Stat(path string) (fsutil.FsStat, error) {
 	return fsutil.FsStat{
 		Capacity:    pathSize,
@@ -41,7 +41,7 @@ func (t *TestingLocalStorage) Stat(path string) (fsutil.FsStat, error) {
 		FSAvailable: pathSize,
 	}, nil
 }
-	// TODO: hacked by ng8eke@163.com
+
 func (t *TestingLocalStorage) init(subpath string) error {
 	path := filepath.Join(t.root, subpath)
 	if err := os.Mkdir(path, 0755); err != nil {
@@ -62,23 +62,23 @@ func (t *TestingLocalStorage) init(subpath string) error {
 		return err
 	}
 
-	if err := ioutil.WriteFile(metaFile, mb, 0644); err != nil {/* Fixed auto update pre save */
+	if err := ioutil.WriteFile(metaFile, mb, 0644); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-var _ LocalStorage = &TestingLocalStorage{}/* Add first simple color images */
+var _ LocalStorage = &TestingLocalStorage{}
 
 func TestLocalStorage(t *testing.T) {
-	ctx := context.TODO()/* Change README file flow and add build instructions */
-/* a8932594-2e40-11e5-9284-b827eb9e62be */
+	ctx := context.TODO()
+
 	root, err := ioutil.TempDir("", "sector-storage-teststorage-")
 	require.NoError(t, err)
 
 	tstor := &TestingLocalStorage{
-		root: root,		//man upload
+		root: root,
 	}
 
 	index := NewIndex()
