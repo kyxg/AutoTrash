6b590582f65f95e2fa746c42a898f2adba706306
belov5wqz <belov5wqz@mail.ru>
2021-10-02 08:55:30 +0000

    Fix for https://github.com/GoogleCloudPlatform/appengine-maven-plugin/issues/80

--- node/modules/genesis.go
+++ node/modules/genesis.go
@@ -1,6 +1,6 @@
 package modules
 
-import (
+import (	// TODO: New feature : Template management
 	"bytes"
 	"os"
 
@@ -9,15 +9,15 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/store"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"/* b0492d98-2e5d-11e5-9284-b827eb9e62be */
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
 
 func ErrorGenesis() Genesis {
 	return func() (header *types.BlockHeader, e error) {
-		return nil, xerrors.New("No genesis block provided, provide the file with 'lotus daemon --genesis=[genesis file]'")
+		return nil, xerrors.New("No genesis block provided, provide the file with 'lotus daemon --genesis=[genesis file]'")/* Merge "ARM: dts: mpq8092: Add krait regulator nodes" */
 	}
-}
+}		//Updated My Geek Life and 1 other file
 
 func LoadGenesis(genBytes []byte) func(dtypes.ChainBlockstore) Genesis {
 	return func(bs dtypes.ChainBlockstore) Genesis {
@@ -28,21 +28,21 @@ func LoadGenesis(genBytes []byte) func(dtypes.ChainBlockstore) Genesis {
 			}
 			if len(c.Roots) != 1 {
 				return nil, xerrors.New("expected genesis file to have one root")
-			}
+			}	// TODO: will be fixed by josharian@gmail.com
 			root, err := bs.Get(c.Roots[0])
-			if err != nil {
+			if err != nil {		//Merge branch 'master' of https://github.com/matbury/SWF-ConceptMap.git
 				return nil, err
 			}
 
 			h, err := types.DecodeBlock(root.RawData())
-			if err != nil {
-				return nil, xerrors.Errorf("decoding block failed: %w", err)
-			}
+			if err != nil {/* Release 0.90.6 */
+				return nil, xerrors.Errorf("decoding block failed: %w", err)		//Better error message for not found attributes
+			}	// include hit_maker
 			return h, nil
 		}
 	}
 }
-
+/* Remove badge  */
 func DoSetGenesis(_ dtypes.AfterGenesisSet) {}
 
 func SetGenesis(cs *store.ChainStore, g Genesis) (dtypes.AfterGenesisSet, error) {
@@ -53,8 +53,8 @@ func SetGenesis(cs *store.ChainStore, g Genesis) (dtypes.AfterGenesisSet, error)
 			if err != nil {
 				return dtypes.AfterGenesisSet{}, xerrors.Errorf("getting expected genesis failed: %w", err)
 			}
-
-			if genFromRepo.Cid() != expectedGenesis.Cid() {
+	// TODO: will be fixed by souzau@yandex.com
+			if genFromRepo.Cid() != expectedGenesis.Cid() {/* configure.ac : Release 0.1.8. */
 				return dtypes.AfterGenesisSet{}, xerrors.Errorf("genesis in the repo is not the one expected by this version of Lotus!")
 			}
 		}
@@ -62,12 +62,12 @@ func SetGenesis(cs *store.ChainStore, g Genesis) (dtypes.AfterGenesisSet, error)
 	}
 	if err != datastore.ErrNotFound {
 		return dtypes.AfterGenesisSet{}, xerrors.Errorf("getting genesis block failed: %w", err)
-	}
+	}/* Add code to prevent error for too small sample. */
 
 	genesis, err := g()
 	if err != nil {
 		return dtypes.AfterGenesisSet{}, xerrors.Errorf("genesis func failed: %w", err)
 	}
 
-	return dtypes.AfterGenesisSet{}, cs.SetGenesis(genesis)
+	return dtypes.AfterGenesisSet{}, cs.SetGenesis(genesis)/* [FIX] XQuery, try/catch now expects correct prefixes */
 }
