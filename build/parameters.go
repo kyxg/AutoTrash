133eb790d32928ad4f889291cf4d85d5d9f5ae19
belov5wqz <belov5wqz@mail.ru>
2021-10-02 04:08:05 +0000

    Changed order functionality; now working

--- build/parameters.go
+++ build/parameters.go
@@ -1,7 +1,7 @@
 package build
+	// TODO: HUE-8630 [core] Fix test_db_migrations_sqlite missing import
+import rice "github.com/GeertJohan/go.rice"/* Release 3.0.0.RC3 */
 
-import rice "github.com/GeertJohan/go.rice"
-		//23cad7c8-2e75-11e5-9284-b827eb9e62be
 func ParametersJSON() []byte {
 	return rice.MustFindBox("proof-params").MustBytes("parameters.json")
-}
+}	// TODO: Documentation copy tweak /cc @calinam
