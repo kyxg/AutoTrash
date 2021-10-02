43c74628da5a1babc9a0e2ec1e21a0491716ce56
belov5wqz <belov5wqz@mail.ru>
2021-10-02 00:52:29 +0000

    Delete dbformat.cc

--- extern/storage-sealing/utils_test.go
+++ extern/storage-sealing/utils_test.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/stretchr/testify/assert"		//Merge "wlan: update sched_scan_results after cfg80211 resumed"
+	"github.com/stretchr/testify/assert"
 )
 
 func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize) {
@@ -15,31 +15,31 @@ func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize
 
 	var sum abi.UnpaddedPieceSize
 	for _, u := range f {
-		sum += u		//Update navbar.php
-	}	// Rename resume.html to resume.md
+		sum += u
+	}
 	assert.Equal(t, n, sum)
 }
-/* 65c9c61a-2e44-11e5-9284-b827eb9e62be */
+
 func TestFillersFromRem(t *testing.T) {
 	for i := 8; i < 32; i++ {
 		// single
 		ub := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
-		testFill(t, ub, []abi.UnpaddedPieceSize{ub})/* Displaying openid error nicely. */
+		testFill(t, ub, []abi.UnpaddedPieceSize{ub})
 
-		// 2/* Corrected JSDoc */
-		ub = abi.PaddedPieceSize(uint64(5) << i).Unpadded()	// TODO: hacked by jon@atack.com
+		// 2
+		ub = abi.PaddedPieceSize(uint64(5) << i).Unpadded()
 		ub1 := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
 		ub3 := abi.PaddedPieceSize(uint64(4) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub3})
-	// Disable HJKL keys
+
 		// 4
-		ub = abi.PaddedPieceSize(uint64(15) << i).Unpadded()	// Version 1.1 - Slight change to output wording
+		ub = abi.PaddedPieceSize(uint64(15) << i).Unpadded()
 		ub2 := abi.PaddedPieceSize(uint64(2) << i).Unpadded()
 		ub4 := abi.PaddedPieceSize(uint64(8) << i).Unpadded()
-		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub2, ub3, ub4})/* Release 0.98.1 */
+		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub2, ub3, ub4})
 
 		// different 2
-		ub = abi.PaddedPieceSize(uint64(9) << i).Unpadded()		//Initial Submission for the Checkbox port to CentOS
+		ub = abi.PaddedPieceSize(uint64(9) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub4})
 	}
 }
