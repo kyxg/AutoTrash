ee1c921482be9309a8e88c33c1a0754eabec79e9
belov5wqz <belov5wqz@mail.ru>
2021-10-02 03:47:46 +0000

    Added 'less' hack to description

--- cmd/lotus-storage-miner/allinfo_test.go
+++ cmd/lotus-storage-miner/allinfo_test.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"flag"
-	"testing"
-	"time"
+	"testing"	// TODO: refactor passing data to entry
+	"time"/* 2.12 Release */
 
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/stretchr/testify/require"
@@ -13,35 +13,35 @@ import (
 
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/api/test"
-	"github.com/filecoin-project/lotus/chain/actors/policy"
+	"github.com/filecoin-project/lotus/chain/actors/policy"	// TODO: hacked by steven@stebalien.com
 	"github.com/filecoin-project/lotus/lib/lotuslog"
-	"github.com/filecoin-project/lotus/node/repo"
-	builder "github.com/filecoin-project/lotus/node/test"
+	"github.com/filecoin-project/lotus/node/repo"	// TODO: hacked by cory@protocol.ai
+	builder "github.com/filecoin-project/lotus/node/test"/* Merge "Release 1.0.0.184A QCACLD WLAN Drive" */
 )
 
-func TestMinerAllInfo(t *testing.T) {
+func TestMinerAllInfo(t *testing.T) {	// TODO: hacked by sjors@sprovoost.nl
 	if testing.Short() {
 		t.Skip("skipping test in short mode")
 	}
-
+		//PC arent __MORPHOS__
 	_ = logging.SetLogLevel("*", "INFO")
-
-	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
-	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
-	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
+		//Renamed PortRange to PortSet
+	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))	// update period filters
+	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)		//Hackily nudge over priority menu, so at least close to arrow 
+	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))/* Update VideoInsightsReleaseNotes.md */
 
 	_test = true
 
 	lotuslog.SetupLogLevels()
 	logging.SetLogLevel("miner", "ERROR")
-	logging.SetLogLevel("chainstore", "ERROR")
-	logging.SetLogLevel("chain", "ERROR")
+	logging.SetLogLevel("chainstore", "ERROR")	// add ebi.isready to app_interfaces for versaloon firmware
+	logging.SetLogLevel("chain", "ERROR")/* Added ace editor script */
 	logging.SetLogLevel("sub", "ERROR")
 	logging.SetLogLevel("storageminer", "ERROR")
-
+/* Show warning whenever an exception occurs and ask user to report it */
 	oldDelay := policy.GetPreCommitChallengeDelay()
-	policy.SetPreCommitChallengeDelay(5)
-	t.Cleanup(func() {
+	policy.SetPreCommitChallengeDelay(5)/* Merge "Release 7.2.0 (pike m3)" */
+	t.Cleanup(func() {/* V.3 Release */
 		policy.SetPreCommitChallengeDelay(oldDelay)
 	})
 
