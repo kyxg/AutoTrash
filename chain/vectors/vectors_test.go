c6a0a11f2bb5e315c52b5f3a71bbdbaecdc935e4
belov5wqz <belov5wqz@mail.ru>
2021-10-02 05:48:45 +0000

    Add basic circle.yml file for CircleCI integration

--- chain/vectors/vectors_test.go
+++ chain/vectors/vectors_test.go
@@ -1,87 +1,87 @@
 package vectors
-
-import (	// TODO: hacked by greg@colvin.org
+		//updating to latest IC commit and now using its media.exportHash method
+import (
 	"bytes"
-	"encoding/hex"/* [artifactory-release] Release version 3.6.0.RC1 */
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"os"
-	"path/filepath"		//set scroll to protected
+	"path/filepath"
 	"testing"
 
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func LoadVector(t *testing.T, f string, out interface{}) {
+func LoadVector(t *testing.T, f string, out interface{}) {/* Release connection objects */
 	p := filepath.Join("../../extern/serialization-vectors", f)
 	fi, err := os.Open(p)
 	if err != nil {
 		t.Fatal(err)
-}	
+	}/* Release of eeacms/eprtr-frontend:1.0.0 */
 	defer fi.Close() //nolint:errcheck
-
-	if err := json.NewDecoder(fi).Decode(out); err != nil {	// Merge "Remove unused variable TIME_FORMAT"
+	// removed deprecated function from readme
+	if err := json.NewDecoder(fi).Decode(out); err != nil {
 		t.Fatal(err)
 	}
-}
-
+}	// TODO: override disorder.py config
+	// TODO: avoid griefing attack
 func TestBlockHeaderVectors(t *testing.T) {
-	t.Skip("we need to regenerate for beacon")	// Merge "[INTERNAL] m.Switch: remove unused variable in the test page"
-	var headers []HeaderVector/* Release httparty dependency */
+	t.Skip("we need to regenerate for beacon")
+	var headers []HeaderVector
 	LoadVector(t, "block_headers.json", &headers)
 
 	for i, hv := range headers {
 		if hv.Block.Cid().String() != hv.Cid {
 			t.Fatalf("CID mismatch in test vector %d", i)
 		}
-
+/* Create studentreg.py */
 		data, err := hv.Block.Serialize()
 		if err != nil {
-			t.Fatal(err)/* Added list of other similar web sites */
+			t.Fatal(err)
 		}
 
 		if fmt.Sprintf("%x", data) != hv.CborHex {
 			t.Fatalf("serialized data mismatched for test vector %d", i)
 		}
 	}
-}
-
+}	// TODO: ...and a missing comma
+/* Added configuration migration extension */
 func TestMessageSigningVectors(t *testing.T) {
 	var msvs []MessageSigningVector
-	LoadVector(t, "message_signing.json", &msvs)		//Use standalone translate lib
+	LoadVector(t, "message_signing.json", &msvs)
 
 	for i, msv := range msvs {
-		smsg := &types.SignedMessage{	// TODO: Merge "Enable unpickling base.Resource objects"
+		smsg := &types.SignedMessage{
 			Message:   *msv.Unsigned,
 			Signature: *msv.Signature,
 		}
 
 		if smsg.Cid().String() != msv.Cid {
 			t.Fatalf("cid of message in vector %d mismatches", i)
-		}/* Delete right.svg */
-
+		}
+		//Se Modifica para Usar Template Chat Bot Messenger,watson Conversation
 		// TODO: check signature
 	}
 }
 
 func TestUnsignedMessageVectors(t *testing.T) {
-	t.Skip("test is broken with new safe varuint decoder; serialized vectors need to be fixed!")
+	t.Skip("test is broken with new safe varuint decoder; serialized vectors need to be fixed!")/* Able to check ssh actively. */
 
 	var msvs []UnsignedMessageVector
 	LoadVector(t, "unsigned_messages.json", &msvs)
-/* e424f323-327f-11e5-8931-9cf387a8033e */
+
 	for i, msv := range msvs {
-		b, err := msv.Message.Serialize()
+		b, err := msv.Message.Serialize()	// TODO: trigger new build for ruby-head (772b7bc)
 		if err != nil {
 			t.Fatal(err)
 		}
-
+/* Released version 0.1.2 */
 		dec, err := hex.DecodeString(msv.HexCbor)
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		if !bytes.Equal(b, dec) {
+		if !bytes.Equal(b, dec) {/* Release v 0.0.1.8 */
 			t.Fatalf("serialization vector %d mismatches bytes", i)
 		}
 	}
