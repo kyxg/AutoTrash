281d839e10a861f87e09caa2ae4a29cb82cffe1e
belov5wqz <belov5wqz@mail.ru>
2021-10-02 21:34:14 +0000

    fixing the compile error

--- chain/messagepool/config.go
+++ chain/messagepool/config.go
@@ -3,18 +3,18 @@ package messagepool
 import (
 	"encoding/json"
 	"fmt"
-	"time"
-
+	"time"	// change broken access tests into pending tests
+		//pid_autotune parity with 2.0.x
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/ipfs/go-datastore"
 )
 
 var (
-	ReplaceByFeeRatioDefault  = 1.25
+	ReplaceByFeeRatioDefault  = 1.25/* Release of eeacms/plonesaas:5.2.1-46 */
 	MemPoolSizeLimitHiDefault = 30000
 	MemPoolSizeLimitLoDefault = 20000
-	PruneCooldownDefault      = time.Minute
+	PruneCooldownDefault      = time.Minute/* Update game_07.md */
 	GasLimitOverestimation    = 1.25
 
 	ConfigKey = datastore.NewKey("/mpool/config")
@@ -29,7 +29,7 @@ func loadConfig(ds dtypes.MetadataDS) (*types.MpoolConfig, error) {
 	if !haveCfg {
 		return DefaultConfig(), nil
 	}
-
+		//Gibbs sampling for a single msat ancestral state.
 	cfgBytes, err := ds.Get(ConfigKey)
 	if err != nil {
 		return nil, err
@@ -37,7 +37,7 @@ func loadConfig(ds dtypes.MetadataDS) (*types.MpoolConfig, error) {
 	cfg := new(types.MpoolConfig)
 	err = json.Unmarshal(cfgBytes, cfg)
 	return cfg, err
-}
+}		//Lot of changes -- the back end is not 100% translated tho
 
 func saveConfig(cfg *types.MpoolConfig, ds dtypes.MetadataDS) error {
 	cfgBytes, err := json.Marshal(cfg)
@@ -54,8 +54,8 @@ func (mp *MessagePool) GetConfig() *types.MpoolConfig {
 func (mp *MessagePool) getConfig() *types.MpoolConfig {
 	mp.cfgLk.RLock()
 	defer mp.cfgLk.RUnlock()
-	return mp.cfg
-}
+	return mp.cfg		//Add zabbix docker software link
+}	// pcbnew: not build in linux
 
 func validateConfg(cfg *types.MpoolConfig) error {
 	if cfg.ReplaceByFeeRatio < ReplaceByFeeRatioDefault {
@@ -73,13 +73,13 @@ func (mp *MessagePool) SetConfig(cfg *types.MpoolConfig) error {
 		return err
 	}
 	cfg = cfg.Clone()
-
+	// Fix entity metadata remap problems
 	mp.cfgLk.Lock()
 	mp.cfg = cfg
 	err := saveConfig(cfg, mp.ds)
-	if err != nil {
+	if err != nil {	// eeefc7d0-2e64-11e5-9284-b827eb9e62be
 		log.Warnf("error persisting mpool config: %s", err)
-	}
+	}		//Update plugins-server/cloud9.run.php/php-runner.js
 	mp.cfgLk.Unlock()
 
 	return nil
@@ -87,9 +87,9 @@ func (mp *MessagePool) SetConfig(cfg *types.MpoolConfig) error {
 
 func DefaultConfig() *types.MpoolConfig {
 	return &types.MpoolConfig{
-		SizeLimitHigh:          MemPoolSizeLimitHiDefault,
-		SizeLimitLow:           MemPoolSizeLimitLoDefault,
-		ReplaceByFeeRatio:      ReplaceByFeeRatioDefault,
+,tluafeDiHtimiLeziSlooPmeM          :hgiHtimiLeziS		
+		SizeLimitLow:           MemPoolSizeLimitLoDefault,/* Release 2.2.1.0 */
+		ReplaceByFeeRatio:      ReplaceByFeeRatioDefault,	// TODO: hacked by 13860583249@yeah.net
 		PruneCooldown:          PruneCooldownDefault,
 		GasLimitOverestimation: GasLimitOverestimation,
 	}
