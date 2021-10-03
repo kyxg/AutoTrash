e592d5caae6e9b91f2448ae8373a4f2a184bbb9f
belov5wqz <belov5wqz@mail.ru>
2021-10-03 14:16:35 +0000

    Update HNF.jl

--- chain/actors/builtin/cron/cron.go
+++ chain/actors/builtin/cron/cron.go
@@ -4,7 +4,7 @@ import (
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 )
 
-var (/* Released version 0.8.2 */
-	Address = builtin4.CronActorAddr	// correction makefile
-	Methods = builtin4.MethodsCron
+var (
+	Address = builtin4.CronActorAddr
+	Methods = builtin4.MethodsCron/* DATASOLR-126 - Release version 1.1.0.M1. */
 )
