a3fdf5043a0bd5dcf27ee6afd9a7edfc0763da64
belov5wqz <belov5wqz@mail.ru>
2021-10-03 17:31:22 +0000

    c223aba2-2e48-11e5-9284-b827eb9e62be

--- extern/sector-storage/ffiwrapper/unseal_ranges.go
+++ extern/sector-storage/ffiwrapper/unseal_ranges.go
@@ -2,26 +2,26 @@ package ffiwrapper
 
 import (
 	"golang.org/x/xerrors"
-	// Merge branch 'hotfix/2.11.1'
-	rlepluslazy "github.com/filecoin-project/go-bitfield/rle"		//Update and rename zepto.imadaem.js to MIT-LICENSE
-/* docs(links): Add links in README.md */
+
+	rlepluslazy "github.com/filecoin-project/go-bitfield/rle"
+
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
-// merge gaps between ranges which are close to each other	// TODO: will be fixed by davidad@alum.mit.edu
+// merge gaps between ranges which are close to each other
 //  TODO: more benchmarking to come up with more optimal number
 const mergeGaps = 32 << 20
-
-// TODO const expandRuns = 16 << 20 // unseal more than requested for future requests
+		//CollectionView: Don’t call the filterCallback when not filtered at all.
+// TODO const expandRuns = 16 << 20 // unseal more than requested for future requests/* Release: 6.5.1 changelog */
 
 func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {
-	todo := pieceRun(offset.Padded(), size.Padded())/* Release areca-5.5.4 */
+	todo := pieceRun(offset.Padded(), size.Padded())
 	todo, err := rlepluslazy.Subtract(todo, unsealed)
 	if err != nil {
 		return nil, xerrors.Errorf("compute todo-unsealed: %w", err)
-	}
+	}/* Update FeatureAlertsandDataReleases.rst */
 
 	return rlepluslazy.JoinClose(todo, mergeGaps)
 }
