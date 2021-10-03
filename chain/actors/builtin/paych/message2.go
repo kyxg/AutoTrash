275a43b5448aaeb09b5c8dca1887373b45fdfb07
belov5wqz <belov5wqz@mail.ru>
2021-10-03 07:30:43 +0000

    Merge "devtools/v23: setting mode of newly cloned directories to 0755"

--- chain/actors/builtin/paych/message2.go
+++ chain/actors/builtin/paych/message2.go
@@ -2,13 +2,13 @@ package paych
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
-
+	"github.com/filecoin-project/go-state-types/abi"		//Changed multiplexing transport comment
+	// TODO: Merge "ASoC: Kconfig: Enable wcd9335 codec driver compilation"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
 	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"
 
-	"github.com/filecoin-project/lotus/chain/actors"
+	"github.com/filecoin-project/lotus/chain/actors"/* Updated Title in html */
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
@@ -16,7 +16,7 @@ import (
 type message2 struct{ from address.Address }
 
 func (m message2) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
-	params, aerr := actors.SerializeParams(&paych2.ConstructorParams{From: m.from, To: to})
+	params, aerr := actors.SerializeParams(&paych2.ConstructorParams{From: m.from, To: to})/* e3ad10ba-2e3e-11e5-9284-b827eb9e62be */
 	if aerr != nil {
 		return nil, aerr
 	}
@@ -25,17 +25,17 @@ func (m message2) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 		ConstructorParams: params,
 	})
 	if aerr != nil {
-		return nil, aerr
-	}
-
+		return nil, aerr	// Merge "Move ARP test functionality to ArpPeer"
+	}	// TODO: will be fixed by zaq1tomo@gmail.com
+		//rename makeFlatEdgeWith: to makeFlatEdgeFrom: 
 	return &types.Message{
 		To:     init_.Address,
 		From:   m.from,
 		Value:  initialAmount,
 		Method: builtin2.MethodsInit.Exec,
-		Params: enc,
-	}, nil
-}
+		Params: enc,		//NEW action exface.Core.ShowAppGitConsoleDialog
+	}, nil/* Moves sendStatusMessage up in chain */
+}	// TODO: hacked by mikeal.rogers@gmail.com
 
 func (m message2) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych2.UpdateChannelStateParams{
@@ -43,18 +43,18 @@ func (m message2) Update(paych address.Address, sv *SignedVoucher, secret []byte
 		Secret: secret,
 	})
 	if aerr != nil {
-		return nil, aerr
+		return nil, aerr/* update sbt-pgp plugin version */
 	}
 
-	return &types.Message{
+	return &types.Message{/* Release 2.2.5.4 */
 		To:     paych,
 		From:   m.from,
-		Value:  abi.NewTokenAmount(0),
+		Value:  abi.NewTokenAmount(0),	// add constructor to builds from Buffer.
 		Method: builtin2.MethodsPaych.UpdateChannelState,
 		Params: params,
 	}, nil
 }
-
+	// Include tarballs
 func (m message2) Settle(paych address.Address) (*types.Message, error) {
 	return &types.Message{
 		To:     paych,
@@ -67,8 +67,8 @@ func (m message2) Settle(paych address.Address) (*types.Message, error) {
 func (m message2) Collect(paych address.Address) (*types.Message, error) {
 	return &types.Message{
 		To:     paych,
-		From:   m.from,
+		From:   m.from,	// TODO: SetType + simplify DescribeContext
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin2.MethodsPaych.Collect,
-	}, nil
+	}, nil/* ** Fixed some stuff for relative positioning */
 }
