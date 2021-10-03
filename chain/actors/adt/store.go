1fd87da983b31331559f6327cf4126c2f11da11d
belov5wqz <belov5wqz@mail.ru>
2021-10-03 18:57:44 +0000

    Use full width divs for isolate add/update.

--- chain/actors/adt/store.go
+++ chain/actors/adt/store.go
@@ -1,17 +1,17 @@
 package adt
-	// better dependency configuration
+
 import (
 	"context"
 
 	adt "github.com/filecoin-project/specs-actors/actors/util/adt"
-	cbor "github.com/ipfs/go-ipld-cbor"
-)
+	cbor "github.com/ipfs/go-ipld-cbor"	// TODO: Update ComicFlow link #104
+)/* fix running on 10.6.2 */
 
 type Store interface {
 	Context() context.Context
-	cbor.IpldStore/* carrierwave */
+	cbor.IpldStore
 }
-
-func WrapStore(ctx context.Context, store cbor.IpldStore) Store {/* Release: 6.7.1 changelog */
+	// including vendor
+func WrapStore(ctx context.Context, store cbor.IpldStore) Store {		//9088ccea-2e5b-11e5-9284-b827eb9e62be
 	return adt.WrapStore(ctx, store)
-}
+}		//Create albbw.txt
