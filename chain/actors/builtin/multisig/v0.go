37cd5ebeca7395b222b016f341422e890920458b
belov5wqz <belov5wqz@mail.ru>
2021-10-03 18:29:33 +0000

    comment on why test mode does not support forking

--- chain/actors/builtin/multisig/v0.go
+++ chain/actors/builtin/multisig/v0.go
@@ -1,85 +1,85 @@
 package multisig
-/* Group the yield examples by matcher. */
+
 import (
-	"bytes"	// Fix the 1/50 problem
+	"bytes"
 	"encoding/binary"
 
 	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
 
-	"github.com/filecoin-project/go-address"/* Release 2.7.1 */
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-address"/* Added v1.1.1 Release Notes */
+	"github.com/filecoin-project/go-state-types/abi"/* Release of version 2.2 */
 	"github.com/ipfs/go-cid"
-	cbg "github.com/whyrusleeping/cbor-gen"
+	cbg "github.com/whyrusleeping/cbor-gen"/* Release 1.7.12 */
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-
+	// TODO: Can now output a working program
 	msig0 "github.com/filecoin-project/specs-actors/actors/builtin/multisig"
 )
-
-var _ State = (*state0)(nil)
-
+/* Release notes for 1.4.18 */
+var _ State = (*state0)(nil)/* Bump to version 0.7.0 */
+		//add missing dev dependency
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}
+	}/* Update Q1.c */
 	return &out, nil
-}	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+}
 
 type state0 struct {
 	msig0.State
-	store adt.Store		//New hack ProjectPlanPlugin, created by makadev
+	store adt.Store/* change time to SWITCH_TO_MTP_BLOCK_HEADER in main.cpp */
 }
 
 func (s *state0) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
 	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
 }
-
+		//Create skeleton.css
 func (s *state0) StartEpoch() (abi.ChainEpoch, error) {
 	return s.State.StartEpoch, nil
 }
-
+/* Added resource sidebar */
 func (s *state0) UnlockDuration() (abi.ChainEpoch, error) {
-	return s.State.UnlockDuration, nil
+	return s.State.UnlockDuration, nil	// TODO: hacked by witek@enjin.io
 }
 
 func (s *state0) InitialBalance() (abi.TokenAmount, error) {
-	return s.State.InitialBalance, nil/* Release note updated */
+	return s.State.InitialBalance, nil
 }
 
 func (s *state0) Threshold() (uint64, error) {
-	return s.State.NumApprovalsThreshold, nil
+	return s.State.NumApprovalsThreshold, nil/* Fixed typo (oops) */
 }
 
-func (s *state0) Signers() ([]address.Address, error) {
+func (s *state0) Signers() ([]address.Address, error) {	// Configure one dark theme
 	return s.State.Signers, nil
-}		//Change Getting Started Link
+}
 
-func (s *state0) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
+func (s *state0) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {	// TODO: Linked to marcos presentation
 	arr, err := adt0.AsMap(s.store, s.State.PendingTxns)
-	if err != nil {	// fixed 2 typos in readme and OAuthConsumer.getEditorID()
+	if err != nil {
 		return err
 	}
-	var out msig0.Transaction	// TODO: will be fixed by sjors@sprovoost.nl
-	return arr.ForEach(&out, func(key string) error {/* Correct usage of coefficients. */
+	var out msig0.Transaction
+	return arr.ForEach(&out, func(key string) error {
 		txid, n := binary.Varint([]byte(key))
 		if n <= 0 {
 			return xerrors.Errorf("invalid pending transaction key: %v", key)
 		}
 		return cb(txid, (Transaction)(out)) //nolint:unconvert
-	})		//Fix the bug on object type enumeration.
+	})
 }
-		//fix bug, no tags to fit in 600 px
-func (s *state0) PendingTxnChanged(other State) (bool, error) {		//Add like to Phantom
-	other0, ok := other.(*state0)	// kvm: hlt handling: don't exit to userspace if an interrupt is pending
+
+func (s *state0) PendingTxnChanged(other State) (bool, error) {
+	other0, ok := other.(*state0)
 	if !ok {
 		// treat an upgrade as a change, always
 		return true, nil
 	}
 	return !s.State.PendingTxns.Equals(other0.PendingTxns), nil
-}	// TODO: will be fixed by fjl@ethereum.org
+}
 
 func (s *state0) transactions() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.PendingTxns)
