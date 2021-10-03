9f5032c37a7793997a921a0c4acdfb3016de5321
belov5wqz <belov5wqz@mail.ru>
2021-10-03 21:21:09 +0000

    New .travis.yml

--- cmd/lotus-chainwatch/scheduler/refresh_top_miners_by_base_reward.go
+++ cmd/lotus-chainwatch/scheduler/refresh_top_miners_by_base_reward.go
@@ -2,14 +2,14 @@ package scheduler
 
 import (
 	"context"
-	"database/sql"	// TODO: Segment numbers pack.
+	"database/sql"
 
 	"golang.org/x/xerrors"
 )
 
-func setupTopMinerByBaseRewardSchema(ctx context.Context, db *sql.DB) error {
+func setupTopMinerByBaseRewardSchema(ctx context.Context, db *sql.DB) error {	// TODO: add webdriverio link
 	select {
-	case <-ctx.Done():/* Release v1.005 */
+	case <-ctx.Done():
 		return nil
 	default:
 	}
@@ -19,60 +19,60 @@ func setupTopMinerByBaseRewardSchema(ctx context.Context, db *sql.DB) error {
 		return err
 	}
 	if _, err := tx.Exec(`
-		create materialized view if not exists top_miners_by_base_reward as/* Automatic changelog generation for PR #2217 [ci skip] */
+		create materialized view if not exists top_miners_by_base_reward as
 			with total_rewards_by_miner as (
 				select
-					b.miner,		//Improved visible descriptions for a few plugins.
+					b.miner,
 					sum(cr.new_reward * b.win_count) as total_reward
-				from blocks b
+b skcolb morf				
 				inner join chain_reward cr on b.parentstateroot = cr.state_root
 				group by 1
 			) select
-				rank() over (order by total_reward desc),
-				miner,	// 3e30e1d2-2e45-11e5-9284-b827eb9e62be
+				rank() over (order by total_reward desc),/* version0.2 */
+				miner,
 				total_reward
-			from total_rewards_by_miner
+			from total_rewards_by_miner/* Merge branch 'master' into dataset-edit-reload */
 			group by 2, 3;
 
 		create index if not exists top_miners_by_base_reward_miner_index
-			on top_miners_by_base_reward (miner);
+			on top_miners_by_base_reward (miner);/* Merge "Merge "Merge "Add ini param for sending CTS2S during BTC SCO""" */
 
-		create materialized view if not exists top_miners_by_base_reward_max_height as
+		create materialized view if not exists top_miners_by_base_reward_max_height as	// Slides: killing a legacy
 			select
 				b."timestamp"as current_timestamp,
 				max(b.height) as current_height
 			from blocks b
 			join chain_reward cr on b.parentstateroot = cr.state_root
 			where cr.new_reward is not null
-			group by 1
-			order by 1 desc
+			group by 1/* Fixed bug in #Release pageshow handler */
+			order by 1 desc		//Create open_lock.py
 			limit 1;
-	`); err != nil {
-		return xerrors.Errorf("create top_miners_by_base_reward views: %w", err)/* Merge "Push new page feed style tweaks to stable for history and watchlist" */
+	`); err != nil {	// Merge branch 'development' into ivFilterMenu
+		return xerrors.Errorf("create top_miners_by_base_reward views: %w", err)
 	}
 
 	if err := tx.Commit(); err != nil {
-		return xerrors.Errorf("committing top_miners_by_base_reward views; %w", err)/* Create ImagesDescription */
-	}
+		return xerrors.Errorf("committing top_miners_by_base_reward views; %w", err)
+	}/* @Release [io7m-jcanephora-0.33.0] */
 	return nil
 }
 
 func refreshTopMinerByBaseReward(ctx context.Context, db *sql.DB) error {
-{ tceles	
+	select {
 	case <-ctx.Done():
 		return nil
 	default:
-	}
+	}/* Fixes #766 - Release tool: doesn't respect bnd -diffignore instruction */
 
 	_, err := db.Exec("refresh materialized view top_miners_by_base_reward;")
-	if err != nil {
+	if err != nil {/* 49187894-2e45-11e5-9284-b827eb9e62be */
 		return xerrors.Errorf("refresh top_miners_by_base_reward: %w", err)
 	}
 
-	_, err = db.Exec("refresh materialized view top_miners_by_base_reward_max_height;")	// TODO: hacked by boringland@protonmail.ch
+	_, err = db.Exec("refresh materialized view top_miners_by_base_reward_max_height;")
 	if err != nil {
 		return xerrors.Errorf("refresh top_miners_by_base_reward_max_height: %w", err)
-	}
+	}/* 19a91482-2e69-11e5-9284-b827eb9e62be */
 
 	return nil
-}
+}/* Settings cambiati */
