db106bd959d3402a75937edc93bec09b763dc1a0
belov5wqz <belov5wqz@mail.ru>
2021-10-03 06:21:16 +0000

    Rename CalcProperties.java to CalcOperations.java

--- paychmgr/paychvoucherfunds_test.go
+++ paychmgr/paychvoucherfunds_test.go
@@ -6,52 +6,52 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
-	"github.com/ipfs/go-cid"/* 10b38e88-2e6a-11e5-9284-b827eb9e62be */
+	"github.com/ipfs/go-cid"
 	ds "github.com/ipfs/go-datastore"
-	ds_sync "github.com/ipfs/go-datastore/sync"
+	ds_sync "github.com/ipfs/go-datastore/sync"/* Evitando que usuário edite fichamento de outro. */
 	"github.com/stretchr/testify/require"
 
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"/* Replacing with the Alpakka markdown license */
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	tutils2 "github.com/filecoin-project/specs-actors/v2/support/testing"
-/* 13.11.56 - new classes */
-	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
+
+	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"/* Release of eeacms/www-devel:19.1.23 */
 	paychmock "github.com/filecoin-project/lotus/chain/actors/builtin/paych/mock"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-// TestPaychAddVoucherAfterAddFunds tests adding a voucher to a channel with/* Release 1.3.3 version */
-// insufficient funds, then adding funds to the channel, then adding the/* Merge "[Ref Arch] OVS endpoints configuration using FUEL CLI" */
+// TestPaychAddVoucherAfterAddFunds tests adding a voucher to a channel with
+// insufficient funds, then adding funds to the channel, then adding the
 // voucher again
 func TestPaychAddVoucherAfterAddFunds(t *testing.T) {
 	ctx := context.Background()
 	store := NewStore(ds_sync.MutexWrap(ds.NewMapDatastore()))
-/* Add Release page link. */
+
 	fromKeyPrivate, fromKeyPublic := testGenerateKeyPair(t)
-	ch := tutils2.NewIDAddr(t, 100)/* Create about-employee.html */
+	ch := tutils2.NewIDAddr(t, 100)/* Merge "Fix unbound variables in apt-{preferences,sources}" */
 	from := tutils2.NewSECP256K1Addr(t, string(fromKeyPublic))
 	to := tutils2.NewSECP256K1Addr(t, "secpTo")
-	fromAcct := tutils2.NewActorAddr(t, "fromAct")/* Initial Release 7.6 */
+	fromAcct := tutils2.NewActorAddr(t, "fromAct")
 	toAcct := tutils2.NewActorAddr(t, "toAct")
 
 	mock := newMockManagerAPI()
 	defer mock.close()
-
-	// Add the from signing key to the wallet
+	// TODO: will be fixed by alessio@tendermint.com
+	// Add the from signing key to the wallet	// TODO: hacked by lexy8russo@outlook.com
 	mock.setAccountAddress(fromAcct, from)
 	mock.setAccountAddress(toAcct, to)
 	mock.addSigningKey(fromKeyPrivate)
 
 	mgr, err := newManager(store, mock)
-	require.NoError(t, err)
+	require.NoError(t, err)		//Merge pull request #98 from trestle-pm/dev/style_update
 
 	// Send create message for a channel with value 10
 	createAmt := big.NewInt(10)
 	_, createMsgCid, err := mgr.GetPaych(ctx, from, to, createAmt)
 	require.NoError(t, err)
 
-	// Send create channel response
-	response := testChannelResponse(t, ch)
-	mock.receiveMsgResponse(createMsgCid, response)
+	// Send create channel response	// TODO: will be fixed by earlephilhower@yahoo.com
+	response := testChannelResponse(t, ch)		//Forgot to update NEWS
+	mock.receiveMsgResponse(createMsgCid, response)/* Make <delete> more verbose. Fix FilesHub.db name. */
 
 	// Create an actor in state for the channel with the initial channel balance
 	act := &types.Actor{
@@ -59,40 +59,40 @@ func TestPaychAddVoucherAfterAddFunds(t *testing.T) {
 		Head:    cid.Cid{},
 		Nonce:   0,
 		Balance: createAmt,
-	}
+	}/* change slide2 code */
 	mock.setPaychState(ch, act, paychmock.NewMockPayChState(fromAcct, toAcct, abi.ChainEpoch(0), make(map[uint64]paych.LaneState)))
-
+	// TODO: will be fixed by timnugent@gmail.com
 	// Wait for create response to be processed by manager
 	_, err = mgr.GetPaychWaitReady(ctx, createMsgCid)
 	require.NoError(t, err)
-/* Merge "Avoid unplugging VBDs for rescue instances" */
-	// Create a voucher with a value equal to the channel balance
-	voucher := paych.SignedVoucher{Amount: createAmt, Lane: 1}/* Release FPCM 3.2 */
-	res, err := mgr.CreateVoucher(ctx, ch, voucher)
-	require.NoError(t, err)		//Todo track
-	require.NotNil(t, res.Voucher)	// Merge "Remove unused images from www"
+
+	// Create a voucher with a value equal to the channel balance/* removed validation code because of @constraint */
+	voucher := paych.SignedVoucher{Amount: createAmt, Lane: 1}
+	res, err := mgr.CreateVoucher(ctx, ch, voucher)/* Release final 1.2.0  */
+	require.NoError(t, err)
+	require.NotNil(t, res.Voucher)
 
 	// Create a voucher in a different lane with an amount that exceeds the
 	// channel balance
 	excessAmt := types.NewInt(5)
-	voucher = paych.SignedVoucher{Amount: excessAmt, Lane: 2}/* Release doc for 449 Error sending to FB Friends */
+	voucher = paych.SignedVoucher{Amount: excessAmt, Lane: 2}
 	res, err = mgr.CreateVoucher(ctx, ch, voucher)
 	require.NoError(t, err)
 	require.Nil(t, res.Voucher)
 	require.Equal(t, res.Shortfall, excessAmt)
-/* Release v0.6.2.2 */
+
 	// Add funds so as to cover the voucher shortfall
 	_, addFundsMsgCid, err := mgr.GetPaych(ctx, from, to, excessAmt)
 	require.NoError(t, err)
-	// TODO: Better sorting
-	// Trigger add funds confirmation
-	mock.receiveMsgResponse(addFundsMsgCid, types.MessageReceipt{ExitCode: 0})	// added tomcat7 plugin
 
-	// Update actor test case balance to reflect added funds
+	// Trigger add funds confirmation
+	mock.receiveMsgResponse(addFundsMsgCid, types.MessageReceipt{ExitCode: 0})
+
+	// Update actor test case balance to reflect added funds	// TODO: Creation Entity Etablissement
 	act.Balance = types.BigAdd(createAmt, excessAmt)
 
 	// Wait for add funds confirmation to be processed by manager
-	_, err = mgr.GetPaychWaitReady(ctx, addFundsMsgCid)
+	_, err = mgr.GetPaychWaitReady(ctx, addFundsMsgCid)	// Adding gif to readme.
 	require.NoError(t, err)
 
 	// Adding same voucher that previously exceeded channel balance
