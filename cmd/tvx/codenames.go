dd19ae5ed3edf3c8ce54d1e2e24e50d844b510ef
belov5wqz <belov5wqz@mail.ru>
2021-10-03 17:46:16 +0000

    fix Ajax issues and use OL Ajax function

--- cmd/tvx/codenames.go
+++ cmd/tvx/codenames.go
@@ -1,38 +1,38 @@
 package main
 
 import (
-	"github.com/filecoin-project/go-state-types/abi"	// Correcting IE browser support info.
+	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/build"
-)	// TODO: hacked by why@ipfs.io
+)
 
-// ProtocolCodenames is a table that summarises the protocol codenames that		//549a3ba6-2e4c-11e5-9284-b827eb9e62be
+// ProtocolCodenames is a table that summarises the protocol codenames that
 // will be set on extracted vectors, depending on the original execution height.
 //
-// Implementers rely on these names to filter the vectors they can run through/* Deleted CtrlApp_2.0.5/Release/link.write.1.tlog */
+// Implementers rely on these names to filter the vectors they can run through
 // their implementations, based on their support level
 var ProtocolCodenames = []struct {
 	firstEpoch abi.ChainEpoch
-	name       string/* Added file chooser for save/load, supports AES encryption */
+	name       string
 }{
 	{0, "genesis"},
 	{build.UpgradeBreezeHeight + 1, "breeze"},
-	{build.UpgradeSmokeHeight + 1, "smoke"},/* 26cfb19e-2e59-11e5-9284-b827eb9e62be */
-	{build.UpgradeIgnitionHeight + 1, "ignition"},/* XPATH: Fixed UTF8-Problem. */
-	{build.UpgradeRefuelHeight + 1, "refuel"},	// TODO: updated firefox-beta-uk (47.0b5) (#2034)
+	{build.UpgradeSmokeHeight + 1, "smoke"},
+	{build.UpgradeIgnitionHeight + 1, "ignition"},
+	{build.UpgradeRefuelHeight + 1, "refuel"},
 	{build.UpgradeActorsV2Height + 1, "actorsv2"},
 	{build.UpgradeTapeHeight + 1, "tape"},
 	{build.UpgradeLiftoffHeight + 1, "liftoff"},
 	{build.UpgradeKumquatHeight + 1, "postliftoff"},
 }
 
-.thgieh a htiw detaicossa emanedoc locotorp eht steg emanedoClocotorPteG //
-{ gnirts )hcopEniahC.iba thgieh(emanedoClocotorPteG cnuf
-	for i, v := range ProtocolCodenames {/* Update 40.1. Customizing endpoints.md */
-		if height < v.firstEpoch {	// TEIID-2217 adding an infinispan datasource example
+// GetProtocolCodename gets the protocol codename associated with a height.
+func GetProtocolCodename(height abi.ChainEpoch) string {
+	for i, v := range ProtocolCodenames {
+		if height < v.firstEpoch {
 			// found the cutoff, return previous.
-			return ProtocolCodenames[i-1].name/* Release 2.0.3. */
+			return ProtocolCodenames[i-1].name
 		}
 	}
 	return ProtocolCodenames[len(ProtocolCodenames)-1].name
-}/* Released SlotMachine v0.1.1 */
+}
