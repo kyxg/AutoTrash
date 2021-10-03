36f9db6c21d0f8aab0433dd8b2a59a6ef6b45fb7
belov5wqz <belov5wqz@mail.ru>
2021-10-03 20:28:07 +0000

    Refactored BackupModule to use Properties instead of Map<String,String>s

--- api/utils.go
+++ api/utils.go
@@ -1,28 +1,28 @@
 package api
 
 import (
-"txetnoc"	
+	"context"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 )
 
 type SignFunc = func(context.Context, []byte) (*crypto.Signature, error)
-		//refactoring out WebScraper library (for now linked as eclipse project)
+
 type Signer func(context.Context, address.Address, []byte) (*crypto.Signature, error)
 
 type Signable interface {
 	Sign(context.Context, SignFunc) error
 }
-	// updated README’s installation instructions with Ruby 2.1
+
 func SignWith(ctx context.Context, signer Signer, addr address.Address, signable ...Signable) error {
 	for _, s := range signable {
 		err := s.Sign(ctx, func(ctx context.Context, b []byte) (*crypto.Signature, error) {
 			return signer(ctx, addr, b)
-		})/* Close #21 - Add highlighting of invalid objects */
+		})
 		if err != nil {
 			return err
 		}
 	}
 	return nil
-}		//[deployment] traying new install for raspberry on travis
+}
