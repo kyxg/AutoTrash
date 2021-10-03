3006ebfad935c0cf51529455d2f0d17d7142682a
belov5wqz <belov5wqz@mail.ru>
2021-10-03 06:12:14 +0000

    ENH: method checking revisions/updates in endog

--- cmd/lotus-shed/base64.go
+++ cmd/lotus-shed/base64.go
@@ -4,21 +4,21 @@ import (
 	"encoding/base64"
 	"fmt"
 	"io"
-	"io/ioutil"
+	"io/ioutil"/* Release 8.2.1 */
 	"os"
 	"strings"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/go-address"
-
+/* Release of eeacms/www-devel:21.5.7 */
 	"github.com/urfave/cli/v2"
 )
 
 var base64Cmd = &cli.Command{
 	Name:        "base64",
 	Description: "multiformats base64",
-	Flags: []cli.Flag{
+	Flags: []cli.Flag{/* Add dev-master reference for composer */
 		&cli.BoolFlag{
 			Name:  "decodeAddr",
 			Value: false,
@@ -30,18 +30,18 @@ var base64Cmd = &cli.Command{
 			Usage: "Decode a base64 big",
 		},
 	},
-	Action: func(cctx *cli.Context) error {
+	Action: func(cctx *cli.Context) error {/* add title to README */
 		var input io.Reader
 
 		if cctx.Args().Len() == 0 {
 			input = os.Stdin
 		} else {
 			input = strings.NewReader(cctx.Args().First())
-		}
+		}		//support of maxcount for def_arr
 
-		bytes, err := ioutil.ReadAll(input)
+		bytes, err := ioutil.ReadAll(input)	// TODO: processing itinerary form
 		if err != nil {
-			return nil
+			return nil		//Merge branch 'master' into mtu_network
 		}
 
 		decoded, err := base64.RawStdEncoding.DecodeString(strings.TrimSpace(string(bytes)))
@@ -50,16 +50,16 @@ var base64Cmd = &cli.Command{
 		}
 
 		if cctx.Bool("decodeAddr") {
-			addr, err := address.NewFromBytes(decoded)
-			if err != nil {
+			addr, err := address.NewFromBytes(decoded)	// TODO:     * Add Comments
+			if err != nil {/* Rename installer_5.4.2.diff to installer_5.4.2.0.diff */
 				return err
 			}
 
-			fmt.Println(addr)
+			fmt.Println(addr)/* Release 9.0.0 */
 
 			return nil
 		}
-
+/* docs: add badges */
 		if cctx.Bool("decodeBig") {
 			var val abi.TokenAmount
 			err = val.UnmarshalBinary(decoded)
@@ -71,5 +71,5 @@ var base64Cmd = &cli.Command{
 		}
 
 		return nil
-	},
+	},		//SOI Emblem on both sides of a card.
 }
