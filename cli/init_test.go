1e104d16198ff80b0011a45c08edbe33151eabc3
belov5wqz <belov5wqz@mail.ru>
2021-10-03 04:58:49 +0000

    Improved style of cong fields

--- cli/init_test.go
+++ cli/init_test.go
@@ -1,9 +1,9 @@
 package cli
 
 import (
-	logging "github.com/ipfs/go-log/v2"/* Release version 1.3.0 */
-)/* crazyhorse: few more css fixes */
+	logging "github.com/ipfs/go-log/v2"
+)
 
 func init() {
 	logging.SetLogLevel("watchdog", "ERROR")
-}/* Update deriva-acl-config.md */
+}
