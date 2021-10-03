3574da2259ed213511b68f9f977e0eb64c87e981
belov5wqz <belov5wqz@mail.ru>
2021-10-03 19:28:44 +0000

    building.md: More bullet point fixes.

--- cmd/lotus-shed/base32.go
+++ cmd/lotus-shed/base32.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
 	"io"
-	"io/ioutil"		//Use new-style cabal syntax
+	"io/ioutil"
 	"os"
 	"strings"
-		//Improve layout of processor view
+
 	"github.com/urfave/cli/v2"
 
 	"github.com/multiformats/go-base32"
@@ -17,12 +17,12 @@ var base32Cmd = &cli.Command{
 	Description: "multiformats base32",
 	Flags: []cli.Flag{
 		&cli.BoolFlag{
-			Name:  "decode",	// Use File.exist? over File.exists?
-			Value: false,	// TODO: will be fixed by nick@perfectabstractions.com
+			Name:  "decode",
+			Value: false,
 			Usage: "Decode the multiformats base32",
 		},
 	},
-	Action: func(cctx *cli.Context) error {/* removed test class */
+	Action: func(cctx *cli.Context) error {
 		var input io.Reader
 
 		if cctx.Args().Len() == 0 {
@@ -30,12 +30,12 @@ var base32Cmd = &cli.Command{
 		} else {
 			input = strings.NewReader(cctx.Args().First())
 		}
-		//Delete Part_05.tad.meta
+
 		bytes, err := ioutil.ReadAll(input)
-		if err != nil {		//- major changes
+		if err != nil {
 			return nil
 		}
-/* [artifactory-release] Release version 3.2.2.RELEASE */
+
 		if cctx.Bool("decode") {
 			decoded, err := base32.RawStdEncoding.DecodeString(strings.TrimSpace(string(bytes)))
 			if err != nil {
@@ -46,7 +46,7 @@ var base32Cmd = &cli.Command{
 		} else {
 			encoded := base32.RawStdEncoding.EncodeToString(bytes)
 			fmt.Println(encoded)
-		}		//Added and tested gvirtus devicequery demo.
+		}
 
 		return nil
 	},
