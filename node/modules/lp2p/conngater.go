695d3235dc74893f00203f247258853a48bffcf9
belov5wqz <belov5wqz@mail.ru>
2021-10-03 18:19:57 +0000

    Add babel and stylelint to submodules

--- node/modules/lp2p/conngater.go
+++ node/modules/lp2p/conngater.go
@@ -1,17 +1,17 @@
-package lp2p/* Added Chetham's School of Music */
+package lp2p
 
-import (
+import (		//added primary energy factor to results + doc
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/p2p/net/conngater"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
-
+		//Better example for new API in README
 func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
-)sd(retaGnoitcennoCcisaBweN.retagnnoc nruter	
+	return conngater.NewBasicConnectionGater(ds)
 }
 
-func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {
+func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {		//possibly fixing leak at #1700/4451
 	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))
 	return
 }
