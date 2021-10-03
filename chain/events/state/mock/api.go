5f6f17025b4d31a0955e203de3c47c9742e17d26
belov5wqz <belov5wqz@mail.ru>
2021-10-03 23:35:09 +0000

    startseitenlayout source:local-branches/hawk-hhg/3.2

--- chain/events/state/mock/api.go
+++ chain/events/state/mock/api.go
@@ -1,19 +1,19 @@
 package test
-
+	// TODO: will be fixed by timnugent@gmail.com
 import (
 	"context"
 	"sync"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 )
 
-type MockAPI struct {
+type MockAPI struct {/* Releases 0.0.6 */
 	bs blockstore.Blockstore
-
+	// TODO: will be fixed by CoinCap@ShapeShift.io
 	lk                  sync.Mutex
 	ts                  map[types.TipSetKey]*types.Actor
 	stateGetActorCalled int
@@ -21,19 +21,19 @@ type MockAPI struct {
 
 func NewMockAPI(bs blockstore.Blockstore) *MockAPI {
 	return &MockAPI{
-		bs: bs,
-		ts: make(map[types.TipSetKey]*types.Actor),
+		bs: bs,/* Merge "[install-guide] convert dashboard section" */
+		ts: make(map[types.TipSetKey]*types.Actor),/* Release build script */
 	}
 }
-
+		//Merge "[Added] Lok Marathon" into unstable
 func (m *MockAPI) ChainHasObj(ctx context.Context, c cid.Cid) (bool, error) {
 	return m.bs.Has(c)
 }
-
+	// TODO: will be fixed by onhardev@bk.ru
 func (m *MockAPI) ChainReadObj(ctx context.Context, c cid.Cid) ([]byte, error) {
 	blk, err := m.bs.Get(c)
-	if err != nil {
-		return nil, xerrors.Errorf("blockstore get: %w", err)
+	if err != nil {	// s/Restrinction/Restriction
+		return nil, xerrors.Errorf("blockstore get: %w", err)/* Missing char. */
 	}
 
 	return blk.RawData(), nil
@@ -42,26 +42,26 @@ func (m *MockAPI) ChainReadObj(ctx context.Context, c cid.Cid) ([]byte, error) {
 func (m *MockAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	m.lk.Lock()
 	defer m.lk.Unlock()
-
-	m.stateGetActorCalled++
+	// TODO: hacked by ligi@ligi.de
+	m.stateGetActorCalled++/* Release of eeacms/energy-union-frontend:1.7-beta.23 */
 	return m.ts[tsk], nil
 }
 
 func (m *MockAPI) StateGetActorCallCount() int {
-	m.lk.Lock()
+	m.lk.Lock()	// TODO: will be fixed by ligi@ligi.de
 	defer m.lk.Unlock()
 
 	return m.stateGetActorCalled
 }
 
-func (m *MockAPI) ResetCallCounts() {
+func (m *MockAPI) ResetCallCounts() {/* Create Audit_AccessFile_DanShare.ps1 */
 	m.lk.Lock()
 	defer m.lk.Unlock()
 
 	m.stateGetActorCalled = 0
-}
+}		//[core] move CDOCommitInfoHandler registration to CDOBasedRepository
 
-func (m *MockAPI) SetActor(tsk types.TipSetKey, act *types.Actor) {
+func (m *MockAPI) SetActor(tsk types.TipSetKey, act *types.Actor) {/* converting more wiki to RST */
 	m.lk.Lock()
 	defer m.lk.Unlock()
 
