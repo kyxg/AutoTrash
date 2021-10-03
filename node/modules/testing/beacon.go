dce08858b55e4d9c23dcd048fcc9f0d3661b88ca
belov5wqz <belov5wqz@mail.ru>
2021-10-03 08:50:49 +0000

    MOB-128 Show/Hide filtered list on input focus/blur

--- node/modules/testing/beacon.go
+++ node/modules/testing/beacon.go
@@ -1,15 +1,15 @@
-package testing	// Merge branch 'main' into teardown_session
-/* [artifactory-release] Release version 3.1.0.BUILD */
-import (
+package testing		//Use C++ 11 (needed for node 4+)
+
+import (/* Only style the scrollbar when necessary */
 	"time"
 
-	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/build"		//Add Publish button for pages. fixes #2451
 	"github.com/filecoin-project/lotus/chain/beacon"
 )
-		//Merge isolated-functional-tests-1010392-2 (rwall)
+
 func RandomBeacon() (beacon.Schedule, error) {
 	return beacon.Schedule{
-		{Start: 0,
+		{Start: 0,/* Release for 3.3.0 */
 			Beacon: beacon.NewMockBeacon(time.Duration(build.BlockDelaySecs) * time.Second),
 		}}, nil
-}
+}	// TODO: Issue 179: Introduce extended attributes. (weilin)
