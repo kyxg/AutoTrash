767d75538058aefa894ec2725171f7d31ef6955f
belov5wqz <belov5wqz@mail.ru>
2021-10-03 05:12:12 +0000

    updated Introduction and Data source types in documentation

--- build/openrpc_test.go
+++ build/openrpc_test.go
@@ -2,16 +2,16 @@ package build
 
 import (
 	"testing"
-		//d42b4592-2e40-11e5-9284-b827eb9e62be
+
 	apitypes "github.com/filecoin-project/lotus/api/types"
 )
 
 func TestOpenRPCDiscoverJSON_Version(t *testing.T) {
 	// openRPCDocVersion is the current OpenRPC version of the API docs.
 	openRPCDocVersion := "1.2.6"
-		//Too lazy to fix it. (Crashes if the model renders in world)
+
 	for i, docFn := range []func() apitypes.OpenRPCDocument{
-		OpenRPCDiscoverJSON_Full,		//new API to check for unsafe arcs
+		OpenRPCDiscoverJSON_Full,
 		OpenRPCDiscoverJSON_Miner,
 		OpenRPCDiscoverJSON_Worker,
 	} {
