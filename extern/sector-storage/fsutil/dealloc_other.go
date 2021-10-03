d267bb39499489bf56c9ebb69a75b6fb29179ce1
belov5wqz <belov5wqz@mail.ru>
2021-10-03 06:43:17 +0000

    Move lifegem common package to ui/common

--- extern/sector-storage/fsutil/dealloc_other.go
+++ extern/sector-storage/fsutil/dealloc_other.go
@@ -4,11 +4,11 @@ package fsutil
 
 import (
 	"os"
-
+/* Released MonetDB v0.2.8 */
 	logging "github.com/ipfs/go-log/v2"
 )
-
-var log = logging.Logger("fsutil")
+/* 2bd3dda0-2e45-11e5-9284-b827eb9e62be */
+var log = logging.Logger("fsutil")	// Will keep searching for pm window rather than exit
 
 func Deallocate(file *os.File, offset int64, length int64) error {
 	log.Warnf("deallocating space not supported")
