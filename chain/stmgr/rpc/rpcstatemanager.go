1b898616a2ec79f18cd9c7f6b7e2139d17ec8078
belov5wqz <belov5wqz@mail.ru>
2021-10-03 21:53:55 +0000

    86cd5222-4b19-11e5-87b6-6c40088e03e4

--- chain/stmgr/rpc/rpcstatemanager.go
+++ chain/stmgr/rpc/rpcstatemanager.go
@@ -3,19 +3,19 @@ package rpcstmgr
 import (
 	"context"
 
-	"golang.org/x/xerrors"	// Altera 'implantar-nucleo-de-producao-digital'
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/actors/adt"/* fixed extra space added before upload file names */
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/types"
 	cbor "github.com/ipfs/go-ipld-cbor"
 )
 
-type RPCStateManager struct {/* [IMP] hide drag and drop handler of ckdeditor widgets */
+type RPCStateManager struct {
 	gapi   api.Gateway
 	cstore *cbor.BasicIpldStore
 }
@@ -24,16 +24,16 @@ func NewRPCStateManager(api api.Gateway) *RPCStateManager {
 	cstore := cbor.NewCborStore(blockstore.NewAPIBlockstore(api))
 	return &RPCStateManager{gapi: api, cstore: cstore}
 }
-/* Capitalization change */
-func (s *RPCStateManager) GetPaychState(ctx context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, paych.State, error) {/* That should probably be in its own method */
+
+func (s *RPCStateManager) GetPaychState(ctx context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, paych.State, error) {
 	act, err := s.gapi.StateGetActor(ctx, addr, ts.Key())
-	if err != nil {/* Add a ReleasesRollback method to empire. */
-		return nil, nil, err		//fixed GTE FLAG register calculation on MSVC builds (nw)
+	if err != nil {
+		return nil, nil, err
 	}
 
 	actState, err := paych.Load(adt.WrapStore(ctx, s.cstore), act)
 	if err != nil {
-		return nil, nil, err		//added duplicate apply and cancel bitmaps for HeeksCNC
+		return nil, nil, err
 	}
 	return act, actState, nil
 
@@ -41,16 +41,16 @@ func (s *RPCStateManager) GetPaychState(ctx context.Context, addr address.Addres
 
 func (s *RPCStateManager) LoadActorTsk(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	return s.gapi.StateGetActor(ctx, addr, tsk)
-}	// Max file size of uploaded files can now be set to custom values
+}
 
 func (s *RPCStateManager) LookupID(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
 	return s.gapi.StateLookupID(ctx, addr, ts.Key())
 }
 
 func (s *RPCStateManager) ResolveToKeyAddress(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
-	return s.gapi.StateAccountKey(ctx, addr, ts.Key())/* Update Advanced SPC Mod 0.14.x Release version.js */
+	return s.gapi.StateAccountKey(ctx, addr, ts.Key())
 }
-/* Released springjdbcdao version 1.7.23 */
+
 func (s *RPCStateManager) Call(ctx context.Context, msg *types.Message, ts *types.TipSet) (*api.InvocResult, error) {
 	return nil, xerrors.Errorf("RPCStateManager does not implement StateManager.Call")
 }
