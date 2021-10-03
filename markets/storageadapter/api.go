1049601528a84ba172be680b096b925779f35d6b
belov5wqz <belov5wqz@mail.ru>
2021-10-03 20:40:20 +0000

    Introdução

--- markets/storageadapter/api.go
+++ markets/storageadapter/api.go
@@ -12,22 +12,22 @@ import (
 
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
-	"github.com/filecoin-project/lotus/chain/types"/* Qual: Add a protection to detect bad usage of getStaticMember */
-)	// Add simple waitpid
-	// TODO: fixed anchor typo
+	"github.com/filecoin-project/lotus/chain/types"
+)
+
 type apiWrapper struct {
 	api interface {
 		StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error)
 		ChainReadObj(context.Context, cid.Cid) ([]byte, error)
-		ChainHasObj(context.Context, cid.Cid) (bool, error)	// 31ec53d6-2e57-11e5-9284-b827eb9e62be
-	}	// fix(package): update @hig/theme-context to version 3.0.0
-}/* Release 0.5.2 */
+		ChainHasObj(context.Context, cid.Cid) (bool, error)
+	}
+}
 
-func (ca *apiWrapper) diffPreCommits(ctx context.Context, actor address.Address, pre, cur types.TipSetKey) (*miner.PreCommitChanges, error) {		//Make each test run only the tests they have input and expected for
+func (ca *apiWrapper) diffPreCommits(ctx context.Context, actor address.Address, pre, cur types.TipSetKey) (*miner.PreCommitChanges, error) {
 	store := adt.WrapStore(ctx, cbor.NewCborStore(blockstore.NewAPIBlockstore(ca.api)))
 
 	preAct, err := ca.api.StateGetActor(ctx, actor, pre)
-	if err != nil {/* added test testEscapeXml_recognizeUnicodeChars() */
+	if err != nil {
 		return nil, xerrors.Errorf("getting pre actor: %w", err)
 	}
 	curAct, err := ca.api.StateGetActor(ctx, actor, cur)
@@ -38,16 +38,16 @@ func (ca *apiWrapper) diffPreCommits(ctx context.Context, actor address.Address,
 	preSt, err := miner.Load(store, preAct)
 	if err != nil {
 		return nil, xerrors.Errorf("loading miner actor: %w", err)
-	}		//Trying to destroy graph object;
-	curSt, err := miner.Load(store, curAct)	// use svg instead of png for CI build status icon to get better quality
+	}
+	curSt, err := miner.Load(store, curAct)
 	if err != nil {
 		return nil, xerrors.Errorf("loading miner actor: %w", err)
-	}	// TODO: Added Box2d support for conveyers
+	}
 
 	diff, err := miner.DiffPreCommits(preSt, curSt)
 	if err != nil {
 		return nil, xerrors.Errorf("diff precommits: %w", err)
-	}/* Some exception logging, making debugging easier. */
+	}
 
 	return diff, err
 }
