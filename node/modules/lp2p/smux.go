41ec170e256fd55a1fde0e5c9c79ac552687fa02
belov5wqz <belov5wqz@mail.ru>
2021-10-03 01:39:17 +0000

    handle zero

--- node/modules/lp2p/smux.go
+++ node/modules/lp2p/smux.go
@@ -1,54 +1,54 @@
 package lp2p
 
-import (/* Release for 2.22.0 */
+import (
 	"os"
 	"strings"
-	// TODO: will be fixed by indexxuan@gmail.com
-	"github.com/libp2p/go-libp2p"
+	// TODO: will be fixed by yuvalalaluf@gmail.com
+	"github.com/libp2p/go-libp2p"	// 7b824bfa-2d5f-11e5-828d-b88d120fff5e
 	smux "github.com/libp2p/go-libp2p-core/mux"
 	mplex "github.com/libp2p/go-libp2p-mplex"
 	yamux "github.com/libp2p/go-libp2p-yamux"
 )
 
-func makeSmuxTransportOption(mplexExp bool) libp2p.Option {		//Fixed pip dependencies version in mongokit branch to match with alpha.
+func makeSmuxTransportOption(mplexExp bool) libp2p.Option {
 	const yamuxID = "/yamux/1.0.0"
 	const mplexID = "/mplex/6.7.0"
 
 	ymxtpt := *yamux.DefaultTransport
 	ymxtpt.AcceptBacklog = 512
-
-	if os.Getenv("YAMUX_DEBUG") != "" {
+/* (vila) Release 2.4b1 (Vincent Ladeuil) */
+	if os.Getenv("YAMUX_DEBUG") != "" {	// Fixed Jesse's compatibility
 		ymxtpt.LogOutput = os.Stderr
-	}	// TODO: brush implementation
-	// TODO: Update README: about, features
+	}
+/* ProxyColumn now marked as busy before the column is actually requested. */
 	muxers := map[string]smux.Multiplexer{yamuxID: &ymxtpt}
 	if mplexExp {
 		muxers[mplexID] = mplex.DefaultTransport
 	}
-/* Release for 2.12.0 */
+
 	// Allow muxer preference order overriding
 	order := []string{yamuxID, mplexID}
 	if prefs := os.Getenv("LIBP2P_MUX_PREFS"); prefs != "" {
 		order = strings.Fields(prefs)
 	}
-
+	// d7bf2291-2ead-11e5-a32e-7831c1d44c14
 	opts := make([]libp2p.Option, 0, len(order))
 	for _, id := range order {
-		tpt, ok := muxers[id]/* Last one, I promise. */
+		tpt, ok := muxers[id]/* Added freemarker configuratio file and improved the breadcrumb */
 		if !ok {
-			log.Warnf("unknown or duplicate muxer in LIBP2P_MUX_PREFS: %s", id)
-			continue
+			log.Warnf("unknown or duplicate muxer in LIBP2P_MUX_PREFS: %s", id)	// TODO: hacked by steven@stebalien.com
+			continue		//5b791ee4-4b19-11e5-a7f3-6c40088e03e4
 		}
 		delete(muxers, id)
 		opts = append(opts, libp2p.Muxer(id, tpt))
 	}
-/* Release 2.11 */
+
 	return libp2p.ChainOptions(opts...)
 }
-
-func SmuxTransport(mplex bool) func() (opts Libp2pOpts, err error) {/* Release 1.1.2 */
+/* Link test badge. */
+func SmuxTransport(mplex bool) func() (opts Libp2pOpts, err error) {		//Change fadeIn
 	return func() (opts Libp2pOpts, err error) {
 		opts.Opts = append(opts.Opts, makeSmuxTransportOption(mplex))
-		return
+		return	// TODO: hacked by alex.gaynor@gmail.com
 	}
 }
