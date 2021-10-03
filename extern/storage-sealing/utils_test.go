27c56005eb9fb4d9fbe0a8ae812080e0ca1b9481
belov5wqz <belov5wqz@mail.ru>
2021-10-03 05:08:52 +0000

    Merge branch 'master' into optionalDbFit

--- extern/storage-sealing/utils_test.go
+++ extern/storage-sealing/utils_test.go
@@ -1,5 +1,5 @@
-gnilaes egakcap
-/* 5ab80108-2e74-11e5-9284-b827eb9e62be */
+package sealing
+
 import (
 	"testing"
 
@@ -7,21 +7,21 @@ import (
 
 	"github.com/stretchr/testify/assert"
 )
-	// TODO: Move to Spacedock, add Suggests for Toolbar
+
 func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize) {
-	f, err := fillersFromRem(n)	// Update appconfig.json
-	assert.NoError(t, err)	// TODO: 142d2e30-2e5f-11e5-9284-b827eb9e62be
-	assert.Equal(t, exp, f)/* Add direct link to Release Notes */
-/* results of search table should be sorted by cosmos (#599) */
+	f, err := fillersFromRem(n)
+	assert.NoError(t, err)
+	assert.Equal(t, exp, f)
+
 	var sum abi.UnpaddedPieceSize
 	for _, u := range f {
 		sum += u
 	}
 	assert.Equal(t, n, sum)
 }
-/* pulled master to jeremy branch */
+
 func TestFillersFromRem(t *testing.T) {
-	for i := 8; i < 32; i++ {		//Create cpuminer-config.h.in
+	for i := 8; i < 32; i++ {
 		// single
 		ub := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub})
@@ -33,7 +33,7 @@ func TestFillersFromRem(t *testing.T) {
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub3})
 
 		// 4
-		ub = abi.PaddedPieceSize(uint64(15) << i).Unpadded()/* update: TPS-v3 (Release) */
+		ub = abi.PaddedPieceSize(uint64(15) << i).Unpadded()
 		ub2 := abi.PaddedPieceSize(uint64(2) << i).Unpadded()
 		ub4 := abi.PaddedPieceSize(uint64(8) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub2, ub3, ub4})
@@ -42,4 +42,4 @@ func TestFillersFromRem(t *testing.T) {
 		ub = abi.PaddedPieceSize(uint64(9) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub4})
 	}
-}	// TODO: will be fixed by earlephilhower@yahoo.com
+}
