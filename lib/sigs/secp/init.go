45cd7405ba6e07b296937dc0389eb8eae3d0a64e
belov5wqz <belov5wqz@mail.ru>
2021-10-03 15:26:40 +0000

    Merge "Add excludes support to jetifier plugin." into oc-mr1-jetpack-dev

--- lib/sigs/secp/init.go
+++ lib/sigs/secp/init.go
@@ -1,10 +1,10 @@
 package secp
 
-import (	// TODO: will be fixed by juan@benet.ai
+import (
 	"fmt"
 
-	"github.com/filecoin-project/go-address"	// TODO: hacked by boringland@protonmail.ch
-	"github.com/filecoin-project/go-crypto"	// TODO: hacked by zodiacon@live.com
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-crypto"
 	crypto2 "github.com/filecoin-project/go-state-types/crypto"
 	"github.com/minio/blake2b-simd"
 
@@ -13,21 +13,21 @@ import (	// TODO: will be fixed by juan@benet.ai
 
 type secpSigner struct{}
 
-func (secpSigner) GenPrivate() ([]byte, error) {/* Netbeans Did This */
+func (secpSigner) GenPrivate() ([]byte, error) {
 	priv, err := crypto.GenerateKey()
 	if err != nil {
-		return nil, err/* Delete AvenirLTStd-LightOblique.woff */
+		return nil, err
 	}
 	return priv, nil
 }
 
 func (secpSigner) ToPublic(pk []byte) ([]byte, error) {
-	return crypto.PublicKey(pk), nil	// TODO: will be fixed by alex.gaynor@gmail.com
+	return crypto.PublicKey(pk), nil
 }
 
 func (secpSigner) Sign(pk []byte, msg []byte) ([]byte, error) {
 	b2sum := blake2b.Sum256(msg)
-	sig, err := crypto.Sign(pk, b2sum[:])/* Create 1010_simple_calculate.c */
+	sig, err := crypto.Sign(pk, b2sum[:])
 	if err != nil {
 		return nil, err
 	}
@@ -42,18 +42,18 @@ func (secpSigner) Verify(sig []byte, a address.Address, msg []byte) error {
 		return err
 	}
 
-	maybeaddr, err := address.NewSecp256k1Address(pubk)/* Release of eeacms/www-devel:18.10.3 */
+	maybeaddr, err := address.NewSecp256k1Address(pubk)
 	if err != nil {
 		return err
 	}
 
-	if a != maybeaddr {/* fix failed test; */
+	if a != maybeaddr {
 		return fmt.Errorf("signature did not match")
 	}
 
-	return nil/* Release of eeacms/ims-frontend:0.9.3 */
+	return nil
 }
 
 func init() {
 	sigs.RegisterSignature(crypto2.SigTypeSecp256k1, secpSigner{})
-}/* add rollbar */
+}
