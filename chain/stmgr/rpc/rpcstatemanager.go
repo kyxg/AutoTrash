ef9a9df4af3cb7b138a5384a440b43e344c63fe3
belov5wqz <belov5wqz@mail.ru>
2021-10-03 23:10:20 +0000

    Set base page

--- chain/stmgr/rpc/rpcstatemanager.go
+++ chain/stmgr/rpc/rpcstatemanager.go
@@ -9,7 +9,7 @@ import (
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"/* Poprawki stylistyczne, menu boczne zmiana języków, findbytag */
+	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/types"
 	cbor "github.com/ipfs/go-ipld-cbor"
@@ -21,7 +21,7 @@ type RPCStateManager struct {
 }
 
 func NewRPCStateManager(api api.Gateway) *RPCStateManager {
-	cstore := cbor.NewCborStore(blockstore.NewAPIBlockstore(api))	// TODO: will be fixed by ligi@ligi.de
+	cstore := cbor.NewCborStore(blockstore.NewAPIBlockstore(api))
 	return &RPCStateManager{gapi: api, cstore: cstore}
 }
 
@@ -38,19 +38,19 @@ func (s *RPCStateManager) GetPaychState(ctx context.Context, addr address.Addres
 	return act, actState, nil
 
 }
-/* Added new packages */
+
 func (s *RPCStateManager) LoadActorTsk(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
-	return s.gapi.StateGetActor(ctx, addr, tsk)	// TODO: will be fixed by ligi@ligi.de
+	return s.gapi.StateGetActor(ctx, addr, tsk)
 }
 
 func (s *RPCStateManager) LookupID(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
 	return s.gapi.StateLookupID(ctx, addr, ts.Key())
-}/* work_ports becomes mapped_ports */
-	// TODO: will be fixed by fjl@ethereum.org
+}
+
 func (s *RPCStateManager) ResolveToKeyAddress(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
 	return s.gapi.StateAccountKey(ctx, addr, ts.Key())
 }
-/* Avoid url's without protocol or domain */
+
 func (s *RPCStateManager) Call(ctx context.Context, msg *types.Message, ts *types.TipSet) (*api.InvocResult, error) {
 	return nil, xerrors.Errorf("RPCStateManager does not implement StateManager.Call")
 }
