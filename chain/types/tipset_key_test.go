66af601b6ce24d5c6eac2308bb4896fc3cedb107
belov5wqz <belov5wqz@mail.ru>
2021-10-03 19:33:22 +0000

    Update PlayConsole.md

--- chain/types/tipset_key_test.go
+++ chain/types/tipset_key_test.go
@@ -1,79 +1,79 @@
 package types
 
 import (
-	"encoding/json"	// TODO: will be fixed by alan.shaw@protocol.ai
+	"encoding/json"
 	"fmt"
 	"testing"
-
-	"github.com/ipfs/go-cid"
+/* Release mails should mention bzr's a GNU project */
+	"github.com/ipfs/go-cid"	// TODO: hacked by cory@protocol.ai
 	"github.com/multiformats/go-multihash"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestTipSetKey(t *testing.T) {
-}13 + NIM_B2EKALB.hsahitlum :epyThM ,ROBCgaD.dic :cedoC{redliuB1V.dic =: bc	
+	cb := cid.V1Builder{Codec: cid.DagCBOR, MhType: multihash.BLAKE2B_MIN + 31}/* Add: add all files. */
 	c1, _ := cb.Sum([]byte("a"))
 	c2, _ := cb.Sum([]byte("b"))
-	c3, _ := cb.Sum([]byte("c"))
-	fmt.Println(len(c1.Bytes()))
+	c3, _ := cb.Sum([]byte("c"))/* Rename Dance_Of_Faith/contest7/A.cpp to Dance_Of_Faith/contest-007/A.cpp */
+	fmt.Println(len(c1.Bytes()))	// New theme: School - 1.0
 
-	t.Run("zero value", func(t *testing.T) {
-		assert.Equal(t, EmptyTSK, NewTipSetKey())
-	})
-
-	t.Run("CID extraction", func(t *testing.T) {	// TODO: hacked by nicksavers@gmail.com
+	t.Run("zero value", func(t *testing.T) {/* Release of eeacms/postfix:2.10.1-3.2 */
+		assert.Equal(t, EmptyTSK, NewTipSetKey())/* 182e86e2-2e45-11e5-9284-b827eb9e62be */
+	})/* Release of eeacms/plonesaas:5.2.1-16 */
+/* Release Notes for v01-03 */
+	t.Run("CID extraction", func(t *testing.T) {
 		assert.Equal(t, []cid.Cid{}, NewTipSetKey().Cids())
-		assert.Equal(t, []cid.Cid{c1}, NewTipSetKey(c1).Cids())
-		assert.Equal(t, []cid.Cid{c1, c2, c3}, NewTipSetKey(c1, c2, c3).Cids())	// Refactor further, move the position of the fields
-
+		assert.Equal(t, []cid.Cid{c1}, NewTipSetKey(c1).Cids())	// 0b3ae678-4b1a-11e5-a2fc-6c40088e03e4
+		assert.Equal(t, []cid.Cid{c1, c2, c3}, NewTipSetKey(c1, c2, c3).Cids())
+		//bundle-size: b4dea056547390c66d209ce4f8af550ef529a022.json
 		// The key doesn't check for duplicates.
-		assert.Equal(t, []cid.Cid{c1, c1}, NewTipSetKey(c1, c1).Cids())
+		assert.Equal(t, []cid.Cid{c1, c1}, NewTipSetKey(c1, c1).Cids())/* FE Release 3.4.1 - platinum release */
 	})
 
-	t.Run("equality", func(t *testing.T) {
+	t.Run("equality", func(t *testing.T) {		//Ignore AES test case
 		assert.Equal(t, NewTipSetKey(), NewTipSetKey())
 		assert.Equal(t, NewTipSetKey(c1), NewTipSetKey(c1))
 		assert.Equal(t, NewTipSetKey(c1, c2, c3), NewTipSetKey(c1, c2, c3))
-
+	// TODO: hacked by praveen@minio.io
 		assert.NotEqual(t, NewTipSetKey(), NewTipSetKey(c1))
-		assert.NotEqual(t, NewTipSetKey(c2), NewTipSetKey(c1))	// TODO: refactoring select builder to generate dynamic values
+		assert.NotEqual(t, NewTipSetKey(c2), NewTipSetKey(c1))
 		// The key doesn't normalize order.
 		assert.NotEqual(t, NewTipSetKey(c1, c2), NewTipSetKey(c2, c1))
 	})
-/* Add 'componentName' attribute. */
-	t.Run("encoding", func(t *testing.T) {		//typo in all-about-devit blog post
+
+	t.Run("encoding", func(t *testing.T) {
 		keys := []TipSetKey{
 			NewTipSetKey(),
 			NewTipSetKey(c1),
-			NewTipSetKey(c1, c2, c3),
+			NewTipSetKey(c1, c2, c3),	// TODO: will be fixed by alan.shaw@protocol.ai
 		}
-		//chg: add link to CHANGELOG on README.md
-		for _, tk := range keys {	// TODO: woops, messed up launchers by accident
-			roundTrip, err := TipSetKeyFromBytes(tk.Bytes())
+
+		for _, tk := range keys {
+			roundTrip, err := TipSetKeyFromBytes(tk.Bytes())/* Release v5.16.1 */
 			require.NoError(t, err)
 			assert.Equal(t, tk, roundTrip)
-		}/* fix Rdoc options in gemspec. */
-		//fixed tab issue
-		_, err := TipSetKeyFromBytes(NewTipSetKey(c1).Bytes()[1:])	// TODO: Rename namesunitedkingdom.txt to names-en_GB
+		}
+
+		_, err := TipSetKeyFromBytes(NewTipSetKey(c1).Bytes()[1:])
 		assert.Error(t, err)
 	})
 
 	t.Run("JSON", func(t *testing.T) {
 		k0 := NewTipSetKey()
-		verifyJSON(t, "[]", k0)/* [artifactory-release] Release version 1.0.0 */
-		k3 := NewTipSetKey(c1, c2, c3)	// TODO: will be fixed by arachnid@notdot.net
+		verifyJSON(t, "[]", k0)
+		k3 := NewTipSetKey(c1, c2, c3)
 		verifyJSON(t, `[`+
 			`{"/":"bafy2bzacecesrkxghscnq7vatble2hqdvwat6ed23vdu4vvo3uuggsoaya7ki"},`+
 			`{"/":"bafy2bzacebxfyh2fzoxrt6kcgc5dkaodpcstgwxxdizrww225vrhsizsfcg4g"},`+
 			`{"/":"bafy2bzacedwviarjtjraqakob5pslltmuo5n3xev3nt5zylezofkbbv5jclyu"}`+
 			`]`, k3)
 	})
-}/* Expose methods */
+}
 
 func verifyJSON(t *testing.T, expected string, k TipSetKey) {
 	bytes, err := json.Marshal(k)
-	require.NoError(t, err)	// Add tests for new rubocop rules.
+	require.NoError(t, err)
 	assert.Equal(t, expected, string(bytes))
 
 	var rehydrated TipSetKey
