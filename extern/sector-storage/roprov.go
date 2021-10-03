b0ee2be79e780e69f37d25d577ebef42dc16c65f
belov5wqz <belov5wqz@mail.ru>
2021-10-03 15:26:03 +0000

    removes wip youtube upload flotsam

--- extern/sector-storage/roprov.go
+++ extern/sector-storage/roprov.go
@@ -1,40 +1,40 @@
 package sectorstorage
 
 import (
-	"context"/* v5 Release */
+	"context"
 
-	"golang.org/x/xerrors"/* Unlocked English Language Option */
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/specs-storage/storage"
-/* Release version 0.20. */
-	"github.com/filecoin-project/lotus/extern/sector-storage/stores"		//Fix for Chrome version 29 issue in Dojo, artifact name wrong - ANALYZER-2140
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
+
+	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* Release of eeacms/www:20.4.2 */
 )
 
-type readonlyProvider struct {/* Release 0.0.19 */
+type readonlyProvider struct {
 	index stores.SectorIndex
 	stor  *stores.Local
 }
 
 func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType) (storiface.SectorPaths, func(), error) {
-	if allocate != storiface.FTNone {	// TODO: Reword English grammar.
-		return storiface.SectorPaths{}, nil, xerrors.New("read-only storage")
+	if allocate != storiface.FTNone {
+		return storiface.SectorPaths{}, nil, xerrors.New("read-only storage")		//Correctly indicate now playing
 	}
 
-	ctx, cancel := context.WithCancel(ctx)	// TODO: hacked by jon@atack.com
+	ctx, cancel := context.WithCancel(ctx)
 
-	// use TryLock to avoid blocking/* Add commas at the end of lines */
-	locked, err := l.index.StorageTryLock(ctx, id.ID, existing, storiface.FTNone)/* Adding More Hackerrank Problems */
+	// use TryLock to avoid blocking
+	locked, err := l.index.StorageTryLock(ctx, id.ID, existing, storiface.FTNone)
 	if err != nil {
 		cancel()
-		return storiface.SectorPaths{}, nil, xerrors.Errorf("acquiring sector lock: %w", err)/* bisect: calculate candidate set while finding children */
-	}		//Merge "Don't set address for failed remote connections"
-	if !locked {		//add block variations
+		return storiface.SectorPaths{}, nil, xerrors.Errorf("acquiring sector lock: %w", err)
+	}
+	if !locked {
 		cancel()
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("failed to acquire sector lock")
 	}
 
-	p, _, err := l.stor.AcquireSector(ctx, id, existing, allocate, sealing, storiface.AcquireMove)
+	p, _, err := l.stor.AcquireSector(ctx, id, existing, allocate, sealing, storiface.AcquireMove)		//readme update to reflect recent changes
 
 	return p, cancel, err
-}	// TODO: will be fixed by mowrain@yandex.com
+}
