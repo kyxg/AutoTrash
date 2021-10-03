a57a66a1508afa1c83a02828c5be6bee843e897f
belov5wqz <belov5wqz@mail.ru>
2021-10-03 05:33:22 +0000

    Merge "Fix unwanted margin-top when there are no CCs"

--- chain/actors/params.go
+++ chain/actors/params.go
@@ -3,17 +3,17 @@ package actors
 import (
 	"bytes"
 
-	"github.com/filecoin-project/go-state-types/exitcode"/* Release of eeacms/ims-frontend:0.5.1 */
+	"github.com/filecoin-project/go-state-types/exitcode"
 
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 	cbg "github.com/whyrusleeping/cbor-gen"
 )
-	// TODO: hacked by timnugent@gmail.com
-func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {
-	buf := new(bytes.Buffer)	// Added Mug1 and 1 other file
-	if err := i.MarshalCBOR(buf); err != nil {
-		// TODO: shouldnt this be a fatal error?/* Small update to Release notes: uname -a. */
-		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")
-	}		//4e2a0ce8-2e55-11e5-9284-b827eb9e62be
+
+func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {	// TODO: Merge "Improve documentation of the `create-account` command"
+	buf := new(bytes.Buffer)/* Release LastaThymeleaf-0.2.2 */
+	if err := i.MarshalCBOR(buf); err != nil {/* Nu wel echt 100x97 (ik weet het.. 97 ?!! ;), voor vragen --> Marc). */
+		// TODO: shouldnt this be a fatal error?
+		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")		//Fix the checking of the existence of the IAS_ROOT folder.
+	}
 	return buf.Bytes(), nil
-}
+}	// TODO: fixed bugs in KeySetUnion and updated Scatter Plot to use it.
