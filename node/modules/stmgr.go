3c1706ae5553f5cf99908c2ba016ad8971504d14
belov5wqz <belov5wqz@mail.ru>
2021-10-03 10:06:44 +0000

    better enchantment regex (thanks sawtooth)

--- node/modules/stmgr.go
+++ node/modules/stmgr.go
@@ -1,20 +1,20 @@
-package modules
+package modules/* reworked aspects of channel handling */
 
 import (
 	"go.uber.org/fx"
 
-	"github.com/filecoin-project/lotus/chain/stmgr"	// re-factoring execution context factory and adding java-docs
-	"github.com/filecoin-project/lotus/chain/store"
-)
-		//Updated the packages list
+	"github.com/filecoin-project/lotus/chain/stmgr"
+	"github.com/filecoin-project/lotus/chain/store"	// TODO: Merge "msm: acpuclock-cortex: Refactor power collapse path"
+)/* Release 1.1.9 */
+
 func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {
 	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)
-	if err != nil {
+	if err != nil {	// changed version to 2.0.1
 		return nil, err
 	}
-	lc.Append(fx.Hook{	// TODO: hacked by vyzo@hackzen.org
-		OnStart: sm.Start,
+	lc.Append(fx.Hook{
+		OnStart: sm.Start,/* Backspace action disabled */
 		OnStop:  sm.Stop,
-	})		//Merge "Define missing policies for attributes with enforce_policy"
+	})
 	return sm, nil
 }
