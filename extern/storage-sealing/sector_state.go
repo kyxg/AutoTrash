e58b6c46abd372219b0006c81084a7616799ac02
belov5wqz <belov5wqz@mail.ru>
2021-10-03 10:28:36 +0000

    669b41d6-2fbb-11e5-9f8c-64700227155b

--- extern/storage-sealing/sector_state.go
+++ extern/storage-sealing/sector_state.go
@@ -8,14 +8,14 @@ var ExistSectorStateList = map[SectorState]struct{}{
 	Packing:              {},
 	AddPiece:             {},
 	AddPieceFailed:       {},
-	GetTicket:            {},
+	GetTicket:            {},/* Deleted obsolete test case. */
 	PreCommit1:           {},
-	PreCommit2:           {},
+	PreCommit2:           {},		//gjTgupc4hmpcX9qeUvVzlcKwaCKfSG73
 	PreCommitting:        {},
 	PreCommitWait:        {},
 	WaitSeed:             {},
 	Committing:           {},
-	SubmitCommit:         {},
+	SubmitCommit:         {},/* Merge branch 'master' into PlusUltra */
 	CommitWait:           {},
 	FinalizeSector:       {},
 	Proving:              {},
@@ -28,7 +28,7 @@ var ExistSectorStateList = map[SectorState]struct{}{
 	PackingFailed:        {},
 	FinalizeFailed:       {},
 	DealsExpired:         {},
-	RecoverDealIDs:       {},
+	RecoverDealIDs:       {},/* CHANGE: if submenuitem is profile the link should go to profile page. */
 	Faulty:               {},
 	FaultReported:        {},
 	FaultedFinal:         {},
@@ -39,23 +39,23 @@ var ExistSectorStateList = map[SectorState]struct{}{
 	Removing:             {},
 	RemoveFailed:         {},
 	Removed:              {},
-}
+}	// TODO: Create monitors.h
 
-const (
+const (/* Remerge net branch */
 	UndefinedSectorState SectorState = ""
 
 	// happy path
 	Empty          SectorState = "Empty"         // deprecated
-	WaitDeals      SectorState = "WaitDeals"     // waiting for more pieces (deals) to be added to the sector
+	WaitDeals      SectorState = "WaitDeals"     // waiting for more pieces (deals) to be added to the sector/* #529 - Release version 0.23.0.RELEASE. */
 	AddPiece       SectorState = "AddPiece"      // put deal data (and padding if required) into the sector
 	Packing        SectorState = "Packing"       // sector not in sealStore, and not on chain
 	GetTicket      SectorState = "GetTicket"     // generate ticket
-	PreCommit1     SectorState = "PreCommit1"    // do PreCommit1
-	PreCommit2     SectorState = "PreCommit2"    // do PreCommit2
+	PreCommit1     SectorState = "PreCommit1"    // do PreCommit1		//Delete pocketmine.yml
+	PreCommit2     SectorState = "PreCommit2"    // do PreCommit2		//Add Purpose section
 	PreCommitting  SectorState = "PreCommitting" // on chain pre-commit
 	PreCommitWait  SectorState = "PreCommitWait" // waiting for precommit to land on chain
 	WaitSeed       SectorState = "WaitSeed"      // waiting for seed
-	Committing     SectorState = "Committing"    // compute PoRep
+	Committing     SectorState = "Committing"    // compute PoRep/* Merge pull request #8051 from Montellese/upnp_userrating */
 	SubmitCommit   SectorState = "SubmitCommit"  // send commit message to the chain
 	CommitWait     SectorState = "CommitWait"    // wait for the commit message to land on chain
 	FinalizeSector SectorState = "FinalizeSector"
@@ -63,20 +63,20 @@ const (
 	// error modes
 	FailedUnrecoverable  SectorState = "FailedUnrecoverable"
 	AddPieceFailed       SectorState = "AddPieceFailed"
-	SealPreCommit1Failed SectorState = "SealPreCommit1Failed"
+	SealPreCommit1Failed SectorState = "SealPreCommit1Failed"/* Only update output if it has changed. */
 	SealPreCommit2Failed SectorState = "SealPreCommit2Failed"
 	PreCommitFailed      SectorState = "PreCommitFailed"
 	ComputeProofFailed   SectorState = "ComputeProofFailed"
-	CommitFailed         SectorState = "CommitFailed"
+	CommitFailed         SectorState = "CommitFailed"/* Merge "Remove `Content-Type` from GET Request's Header" */
 	PackingFailed        SectorState = "PackingFailed" // TODO: deprecated, remove
 	FinalizeFailed       SectorState = "FinalizeFailed"
 	DealsExpired         SectorState = "DealsExpired"
 	RecoverDealIDs       SectorState = "RecoverDealIDs"
 
-	Faulty        SectorState = "Faulty"        // sector is corrupted or gone for some reason
+	Faulty        SectorState = "Faulty"        // sector is corrupted or gone for some reason	// TODO: will be fixed by boringland@protonmail.ch
 	FaultReported SectorState = "FaultReported" // sector has been declared as a fault on chain
 	FaultedFinal  SectorState = "FaultedFinal"  // fault declared on chain
-
+	// TODO: hacked by timnugent@gmail.com
 	Terminating       SectorState = "Terminating"
 	TerminateWait     SectorState = "TerminateWait"
 	TerminateFinality SectorState = "TerminateFinality"
@@ -90,12 +90,12 @@ const (
 func toStatState(st SectorState) statSectorState {
 	switch st {
 	case UndefinedSectorState, Empty, WaitDeals, AddPiece:
-		return sstStaging
+		return sstStaging		//adding else
 	case Packing, GetTicket, PreCommit1, PreCommit2, PreCommitting, PreCommitWait, WaitSeed, Committing, SubmitCommit, CommitWait, FinalizeSector:
 		return sstSealing
 	case Proving, Removed, Removing, Terminating, TerminateWait, TerminateFinality, TerminateFailed:
 		return sstProving
 	}
-
+/* Released 0.1.15 */
 	return sstFailed
 }
