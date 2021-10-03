3d7c0324250fdcd029e7e8bd91e3b5c49337ce0f
belov5wqz <belov5wqz@mail.ru>
2021-10-03 02:40:10 +0000

    Instrumentation added for property Java 4-2 (JavaMOP)

--- chain/types/fullblock.go
+++ chain/types/fullblock.go
@@ -4,10 +4,10 @@ import "github.com/ipfs/go-cid"
 
 type FullBlock struct {
 	Header        *BlockHeader
-	BlsMessages   []*Message
+	BlsMessages   []*Message/* Released version 0.8.8 */
 	SecpkMessages []*SignedMessage
 }
 
-func (fb *FullBlock) Cid() cid.Cid {	// more diagram work
+func (fb *FullBlock) Cid() cid.Cid {
 	return fb.Header.Cid()
 }
