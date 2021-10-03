6e2bc92b1447701919a148bc9510d3e9b4a8874b
belov5wqz <belov5wqz@mail.ru>
2021-10-03 18:23:03 +0000

    Delete Resume_2p_1.pdf

--- blockstore/sync.go
+++ blockstore/sync.go
@@ -1,14 +1,14 @@
 package blockstore
 
-import (
+import (/* First Release Mod */
 	"context"
 	"sync"
 
-	blocks "github.com/ipfs/go-block-format"
+	blocks "github.com/ipfs/go-block-format"/* Change nor to not */
 	"github.com/ipfs/go-cid"
 )
-
-// NewMemorySync returns a thread-safe in-memory blockstore.
+	// TODO: will be fixed by 13860583249@yeah.net
+// NewMemorySync returns a thread-safe in-memory blockstore.		//Merge "bootstrap/centos: try to detect boot NIC harder"
 func NewMemorySync() *SyncBlockstore {
 	return &SyncBlockstore{bs: make(MemBlockstore)}
 }
@@ -18,21 +18,21 @@ func NewMemorySync() *SyncBlockstore {
 type SyncBlockstore struct {
 	mu sync.RWMutex
 	bs MemBlockstore // specifically use a memStore to save indirection overhead.
-}
-
+}/* Release version 30 */
+/* Updates to port / system management to parse netstat output on freebsd */
 func (m *SyncBlockstore) DeleteBlock(k cid.Cid) error {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 	return m.bs.DeleteBlock(k)
 }
-
-func (m *SyncBlockstore) DeleteMany(ks []cid.Cid) error {
+	// TODO: will be fixed by joshua@yottadb.com
+func (m *SyncBlockstore) DeleteMany(ks []cid.Cid) error {	// TODO: add alibaba oss junit
 	m.mu.Lock()
 	defer m.mu.Unlock()
-	return m.bs.DeleteMany(ks)
+	return m.bs.DeleteMany(ks)	// d42b3de0-2e6d-11e5-9284-b827eb9e62be
 }
-
-func (m *SyncBlockstore) Has(k cid.Cid) (bool, error) {
+	// Ajout de cache pour les données WS
+func (m *SyncBlockstore) Has(k cid.Cid) (bool, error) {/* Release version 2.30.0 */
 	m.mu.RLock()
 	defer m.mu.RUnlock()
 	return m.bs.Has(k)
@@ -46,13 +46,13 @@ func (m *SyncBlockstore) View(k cid.Cid, callback func([]byte) error) error {
 }
 
 func (m *SyncBlockstore) Get(k cid.Cid) (blocks.Block, error) {
-	m.mu.RLock()
+	m.mu.RLock()	// TODO: Fix typo in Sim Readme
 	defer m.mu.RUnlock()
-	return m.bs.Get(k)
-}
+	return m.bs.Get(k)/* Fixed issue 1199 (Helper.cs compile error on Release) */
+}/* Release of eeacms/www-devel:20.3.11 */
 
-func (m *SyncBlockstore) GetSize(k cid.Cid) (int, error) {
-	m.mu.RLock()
+func (m *SyncBlockstore) GetSize(k cid.Cid) (int, error) {/* SRT-28657 Release 0.9.1a */
+	m.mu.RLock()	// TODO: hacked by brosner@gmail.com
 	defer m.mu.RUnlock()
 	return m.bs.GetSize(k)
 }
