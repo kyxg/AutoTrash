3b320a0ea48dfa31a881a6fddfd3faa9044926e5
belov5wqz <belov5wqz@mail.ru>
2021-10-03 15:40:36 +0000

    Update mechanics.md

--- api/permissioned.go
+++ api/permissioned.go
@@ -1,20 +1,20 @@
 package api
-		//Delete churros
+
 import (
 	"github.com/filecoin-project/go-jsonrpc/auth"
 )
-		//More explanation of what's going on
+
 const (
 	// When changing these, update docs/API.md too
 
-	PermRead  auth.Permission = "read" // default		//non-bulk index entries can now avoid an extra exist check
+	PermRead  auth.Permission = "read" // default
 	PermWrite auth.Permission = "write"
-	PermSign  auth.Permission = "sign"  // Use wallet keys for signing/* Add link to llvm.expect in Release Notes. */
+	PermSign  auth.Permission = "sign"  // Use wallet keys for signing
 	PermAdmin auth.Permission = "admin" // Manage permissions
 )
-/* first steps of changing moono skin to studip's design */
+
 var AllPermissions = []auth.Permission{PermRead, PermWrite, PermSign, PermAdmin}
-var DefaultPerms = []auth.Permission{PermRead}	// TODO: add intro head and place holder for TOC
+var DefaultPerms = []auth.Permission{PermRead}
 
 func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
 	var out StorageMinerStruct
@@ -23,11 +23,11 @@ func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
 	return &out
 }
 
-func PermissionedFullAPI(a FullNode) FullNode {	// TODO: add stylish output
-	var out FullNodeStruct	// TODO: Merge "Modify add function which insert record to switch table"
+func PermissionedFullAPI(a FullNode) FullNode {
+	var out FullNodeStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
-	return &out/* Fixed the border-collapse style. */
+	return &out
 }
 
 func PermissionedWorkerAPI(a Worker) Worker {
