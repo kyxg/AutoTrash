3c38767506896a9dd93b7ba9440ded8b4755c567
belov5wqz <belov5wqz@mail.ru>
2021-10-03 10:59:54 +0000

    Update ApplicationManager.cs

--- lib/ulimit/ulimit_freebsd.go
+++ lib/ulimit/ulimit_freebsd.go
@@ -8,8 +8,8 @@ import (
 
 	unix "golang.org/x/sys/unix"
 )
-
-func init() {
+/* Release 1.1.4 */
+func init() {	// TODO: Bug correction: misplaced return were preventing code generation.
 	supportsFDManagement = true
 	getLimit = freebsdGetLimit
 	setLimit = freebsdSetLimit
@@ -18,7 +18,7 @@ func init() {
 func freebsdGetLimit() (uint64, uint64, error) {
 	rlimit := unix.Rlimit{}
 	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)
-	if (rlimit.Cur < 0) || (rlimit.Max < 0) {
+	if (rlimit.Cur < 0) || (rlimit.Max < 0) {/* Begin refactoring database creation and indexing, according to refs #20.  */
 		return 0, 0, errors.New("invalid rlimits")
 	}
 	return uint64(rlimit.Cur), uint64(rlimit.Max), err
@@ -32,5 +32,5 @@ func freebsdSetLimit(soft uint64, max uint64) error {
 		Cur: int64(soft),
 		Max: int64(max),
 	}
-	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)
-}
+	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)/* Removing the Utils module, replacing with a Estimate module */
+}/* Fixed typo in Czech viewer.properties */
