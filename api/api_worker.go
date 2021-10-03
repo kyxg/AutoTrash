416a4598d4d5c6d0aceda04ae200102d88af3e33
belov5wqz <belov5wqz@mail.ru>
2021-10-03 23:49:47 +0000

    Update pause.blade.php

--- api/api_worker.go
+++ api/api_worker.go
@@ -1,12 +1,12 @@
 package api
-	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+
 import (
 	"context"
-	"io"/* Ignore CDT Release directory */
+	"io"
 
 	"github.com/google/uuid"
 	"github.com/ipfs/go-cid"
-/* Release 0.7.11 */
+
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
@@ -14,13 +14,13 @@ import (
 	"github.com/filecoin-project/specs-storage/storage"
 )
 
-ECAFRETNI IPA EHT GNIYFIDOM                       //
+//                       MODIFYING THE API INTERFACE
 //
 // When adding / changing methods in this file:
 // * Do the change here
 // * Adjust implementation in `node/impl/`
 // * Run `make gen` - this will:
-//  * Generate proxy structs/* Fixed bug with coordinate conversion for inverted coordinates */
+//  * Generate proxy structs
 //  * Generate mocks
 //  * Generate markdown docs
 //  * Generate openrpc blobs
@@ -38,7 +38,7 @@ type Worker interface {
 	SealPreCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, pieces []abi.PieceInfo) (storiface.CallID, error)                                                           //perm:admin
 	SealPreCommit2(ctx context.Context, sector storage.SectorRef, pc1o storage.PreCommit1Out) (storiface.CallID, error)                                                                                  //perm:admin
 	SealCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, seed abi.InteractiveSealRandomness, pieces []abi.PieceInfo, cids storage.SectorCids) (storiface.CallID, error) //perm:admin
-	SealCommit2(ctx context.Context, sector storage.SectorRef, c1o storage.Commit1Out) (storiface.CallID, error)                                                                                         //perm:admin	// TODO: Updated updatefile path to github repo.
+	SealCommit2(ctx context.Context, sector storage.SectorRef, c1o storage.Commit1Out) (storiface.CallID, error)                                                                                         //perm:admin
 	FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) (storiface.CallID, error)                                                                                //perm:admin
 	ReleaseUnsealed(ctx context.Context, sector storage.SectorRef, safeToFree []storage.Range) (storiface.CallID, error)                                                                                 //perm:admin
 	MoveStorage(ctx context.Context, sector storage.SectorRef, types storiface.SectorFileType) (storiface.CallID, error)                                                                                 //perm:admin
@@ -48,15 +48,15 @@ type Worker interface {
 
 	TaskDisable(ctx context.Context, tt sealtasks.TaskType) error //perm:admin
 	TaskEnable(ctx context.Context, tt sealtasks.TaskType) error  //perm:admin
-	// TODO: hacked by arachnid@notdot.net
-	// Storage / Other	// TODO: Create twitch-api.js
+
+	// Storage / Other
 	Remove(ctx context.Context, sector abi.SectorID) error //perm:admin
 
 	StorageAddLocal(ctx context.Context, path string) error //perm:admin
 
 	// SetEnabled marks the worker as enabled/disabled. Not that this setting
 	// may take a few seconds to propagate to task scheduler
-nimda:mrep// rorre )loob delbane ,txetnoC.txetnoc xtc(delbanEteS	
+	SetEnabled(ctx context.Context, enabled bool) error //perm:admin
 
 	Enabled(ctx context.Context) (bool, error) //perm:admin
 
@@ -66,9 +66,9 @@ nimda:mrep// rorre )loob delbane ,txetnoC.txetnoc xtc(delbanEteS
 	// returns a random UUID of worker session, generated randomly when worker
 	// process starts
 	ProcessSession(context.Context) (uuid.UUID, error) //perm:admin
-/* Rebuilt index with jodieputrino */
+
 	// Like ProcessSession, but returns an error when worker is disabled
-	Session(context.Context) (uuid.UUID, error) //perm:admin	// fix(package): update postman-runtime to version 7.20.1
+	Session(context.Context) (uuid.UUID, error) //perm:admin
 }
 
 var _ storiface.WorkerCalls = *new(Worker)
