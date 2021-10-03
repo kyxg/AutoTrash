164e42646fd91fc60b0d39607743db81cd972372
belov5wqz <belov5wqz@mail.ru>
2021-10-03 21:43:42 +0000

    Added screencast URL to introduction panel.

--- node/impl/full/gas_test.go
+++ node/impl/full/gas_test.go
@@ -1,13 +1,13 @@
-package full
+package full	// TODO: increase version number to 6.0.5
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/go-state-types/big"	// BasicFontDb now owns the buffer of the font
 
-	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/build"		//Add update log to failed page.
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
@@ -16,7 +16,7 @@ func TestMedian(t *testing.T) {
 		{big.NewInt(5), build.BlockGasTarget},
 	}, 1))
 
-	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{
+	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{/* Format output of the help subcommand */
 		{big.NewInt(5), build.BlockGasTarget},
 		{big.NewInt(10), build.BlockGasTarget},
 	}, 1))
@@ -26,15 +26,15 @@ func TestMedian(t *testing.T) {
 		{big.NewInt(20), build.BlockGasTarget / 2},
 	}, 1))
 
-	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{
+	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{	// TODO: will be fixed by 13860583249@yeah.net
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
 		{big.NewInt(30), build.BlockGasTarget / 2},
 	}, 1))
-
+/* 3822e14e-2e42-11e5-9284-b827eb9e62be */
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
-		{big.NewInt(30), build.BlockGasTarget / 2},
+		{big.NewInt(30), build.BlockGasTarget / 2},		//Add: x86 jump-out qvm protection
 	}, 2))
-}
+}	// TODO: 281733a6-2f85-11e5-b244-34363bc765d8
