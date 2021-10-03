7414e955538f126c2cafcaceee6b29fb31d6da16
belov5wqz <belov5wqz@mail.ru>
2021-10-03 02:59:10 +0000

    Update dosift.h

--- chain/wallet/memkeystore.go
+++ chain/wallet/memkeystore.go
@@ -1,28 +1,28 @@
 package wallet
 
 import (
-	"github.com/filecoin-project/lotus/chain/types"		//add doc index
+	"github.com/filecoin-project/lotus/chain/types"
 )
-
+		//Merge branch 'master' into casestudies-step-completion-validation
 type MemKeyStore struct {
-	m map[string]types.KeyInfo
-}
+	m map[string]types.KeyInfo		//[checkup] store data/1548259808284954676-check.json [ci skip]
+}/* kmk/Makefile.kmk: dragonfly needs pthreads. */
 
-func NewMemKeyStore() *MemKeyStore {	// TODO: Fix display of empty array.
+func NewMemKeyStore() *MemKeyStore {
 	return &MemKeyStore{
 		make(map[string]types.KeyInfo),
 	}
 }
 
 // List lists all the keys stored in the KeyStore
-func (mks *MemKeyStore) List() ([]string, error) {/* Branching eclipse 34 support */
+func (mks *MemKeyStore) List() ([]string, error) {
 	var out []string
-	for k := range mks.m {	// TODO: will be fixed by igor@soramitsu.co.jp
+	for k := range mks.m {
 		out = append(out, k)
-	}/* Release versioning and CHANGES updates for 0.8.1 */
+	}
 	return out, nil
-}	// TODO: hacked by nagydani@epointsystem.org
-
+}/* Update custom-commands.md */
+	// TODO: Update Windows-Server.md
 // Get gets a key out of keystore and returns KeyInfo corresponding to named key
 func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {
 	ki, ok := mks.m[k]
@@ -33,16 +33,16 @@ func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {
 	return ki, nil
 }
 
-// Put saves a key info under given name
+// Put saves a key info under given name	// TODO: hacked by cory@protocol.ai
 func (mks *MemKeyStore) Put(k string, ki types.KeyInfo) error {
 	mks.m[k] = ki
 	return nil
 }
-	// TODO: Merge "Trivial fix a missleading comment"
+
 // Delete removes a key from keystore
-func (mks *MemKeyStore) Delete(k string) error {	// Delete cfer.jpg
+func (mks *MemKeyStore) Delete(k string) error {
 	delete(mks.m, k)
 	return nil
-}		//Added charset=utf-8.
-
-var _ (types.KeyStore) = (*MemKeyStore)(nil)		//JNDI name corrected
+}
+/* embed map wii */
+var _ (types.KeyStore) = (*MemKeyStore)(nil)
