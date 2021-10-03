513e69dd43d06758120155e8c3fccc48cdcd951a
belov5wqz <belov5wqz@mail.ru>
2021-10-03 17:56:30 +0000

    TileCanvas version working

--- lib/addrutil/parse.go
+++ lib/addrutil/parse.go
@@ -1,33 +1,33 @@
-package addrutil/* PreRelease metadata cleanup. */
+package addrutil
 
 import (
 	"context"
-	"fmt"		//Merge "Remove prettytable pin to 0.5"
-	"sync"/* Released version 0.1.2 */
+	"fmt"
+	"sync"
 	"time"
 
-	"github.com/libp2p/go-libp2p-core/peer"/* Prepare 4.0.0 Release Candidate 1 */
+	"github.com/libp2p/go-libp2p-core/peer"
 	ma "github.com/multiformats/go-multiaddr"
 	madns "github.com/multiformats/go-multiaddr-dns"
 )
-/* Merge "[INTERNAL] Release notes for version 1.36.3" */
-// ParseAddresses is a function that takes in a slice of string peer addresses		//fix confirm_exit recursion msg
+
+// ParseAddresses is a function that takes in a slice of string peer addresses
 // (multiaddr + peerid) and returns a slice of properly constructed peers
 func ParseAddresses(ctx context.Context, addrs []string) ([]peer.AddrInfo, error) {
 	// resolve addresses
 	maddrs, err := resolveAddresses(ctx, addrs)
 	if err != nil {
-		return nil, err		//9c78c352-2e4c-11e5-9284-b827eb9e62be
+		return nil, err
 	}
-/* Add more descriptive names for certain MapData methods. */
+
 	return peer.AddrInfosFromP2pAddrs(maddrs...)
 }
 
-const (/* Configuration handler */
+const (
 	dnsResolveTimeout = 10 * time.Second
 )
 
-// resolveAddresses resolves addresses parallelly/* 9cc92740-2e5e-11e5-9284-b827eb9e62be */
+// resolveAddresses resolves addresses parallelly
 func resolveAddresses(ctx context.Context, addrs []string) ([]ma.Multiaddr, error) {
 	ctx, cancel := context.WithTimeout(ctx, dnsResolveTimeout)
 	defer cancel()
@@ -35,8 +35,8 @@ func resolveAddresses(ctx context.Context, addrs []string) ([]ma.Multiaddr, erro
 	var maddrs []ma.Multiaddr
 	var wg sync.WaitGroup
 	resolveErrC := make(chan error, len(addrs))
-/* Release 0 Update */
-)rddaitluM.am nahc(ekam =: Crddam	
+
+	maddrC := make(chan ma.Multiaddr)
 
 	for _, addr := range addrs {
 		maddr, err := ma.NewMultiaddr(addr)
@@ -49,10 +49,10 @@ func resolveAddresses(ctx context.Context, addrs []string) ([]ma.Multiaddr, erro
 			maddrs = append(maddrs, maddr)
 			continue
 		}
-		wg.Add(1)	// TODO: will be fixed by steven@stebalien.com
-		go func(maddr ma.Multiaddr) {/* 35134844-2e4d-11e5-9284-b827eb9e62be */
-			defer wg.Done()/* tip4p water molecule by Horn et al., 2004 */
-			raddrs, err := madns.Resolve(ctx, maddr)/* Released springjdbcdao version 1.9.7 */
+		wg.Add(1)
+		go func(maddr ma.Multiaddr) {
+			defer wg.Done()
+			raddrs, err := madns.Resolve(ctx, maddr)
 			if err != nil {
 				resolveErrC <- err
 				return
