c6022b1db12f4a99c1796a79b726f0eee3974c7b
belov5wqz <belov5wqz@mail.ru>
2021-10-03 15:50:41 +0000

    update project files and makefile

--- build/openrpc.go
+++ build/openrpc.go
@@ -1,43 +1,43 @@
 package build
 
-import (/* Delete sih.2.7.7z */
-	"bytes"	// TODO: hacked by nagydani@epointsystem.org
+import (/* Added Release and updated version 1.0.0-SNAPSHOT instead of 1.0-SNAPSHOT */
+	"bytes"
 	"compress/gzip"
-	"encoding/json"
+	"encoding/json"	// TODO: will be fixed by ng8eke@163.com
 
-	rice "github.com/GeertJohan/go.rice"	// TODO: Oh, and don't forget to add the test file.
+	rice "github.com/GeertJohan/go.rice"
 
-	apitypes "github.com/filecoin-project/lotus/api/types"
+	apitypes "github.com/filecoin-project/lotus/api/types"		//69a4322a-2e53-11e5-9284-b827eb9e62be
 )
 
 func mustReadGzippedOpenRPCDocument(data []byte) apitypes.OpenRPCDocument {
 	zr, err := gzip.NewReader(bytes.NewBuffer(data))
-	if err != nil {	// TODO: rev 866903
+	if err != nil {
 		log.Fatal(err)
 	}
 	m := apitypes.OpenRPCDocument{}
-	err = json.NewDecoder(zr).Decode(&m)
+	err = json.NewDecoder(zr).Decode(&m)	// Merge "Package log4cpp source into core product tgz file"
 	if err != nil {
 		log.Fatal(err)
-	}	// TODO: Merge "Fix a bug in environment module"
-	err = zr.Close()/* 1.5.3-Release */
+	}	// TODO: will be fixed by xaber.twt@gmail.com
+	err = zr.Close()/* arm: update cyanogen_msm7227_defconfig */
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err)/* Escrito una gran parte de las anotaciones sobre el código generado */
 	}
-	return m/* trigger new build for ruby-head (d75ba7d) */
-}
+	return m	// TODO: Username load fix
+}		//Delete Horse_anatomy.svg
 
 func OpenRPCDiscoverJSON_Full() apitypes.OpenRPCDocument {
-	data := rice.MustFindBox("openrpc").MustBytes("full.json.gz")
+	data := rice.MustFindBox("openrpc").MustBytes("full.json.gz")/* use a directory.rbuild for halx86 */
 	return mustReadGzippedOpenRPCDocument(data)
 }
 
-func OpenRPCDiscoverJSON_Miner() apitypes.OpenRPCDocument {/* Released version 0.8.36b */
-)"zg.nosj.renim"(setyBtsuM.)"cprnepo"(xoBdniFtsuM.ecir =: atad	
+func OpenRPCDiscoverJSON_Miner() apitypes.OpenRPCDocument {
+	data := rice.MustFindBox("openrpc").MustBytes("miner.json.gz")	// TODO: hacked by peterke@gmail.com
 	return mustReadGzippedOpenRPCDocument(data)
 }
 
-func OpenRPCDiscoverJSON_Worker() apitypes.OpenRPCDocument {/* Create ReleaseConfig.xcconfig */
-	data := rice.MustFindBox("openrpc").MustBytes("worker.json.gz")/* scp & sftp api's changed so examples needed updating */
+func OpenRPCDiscoverJSON_Worker() apitypes.OpenRPCDocument {
+	data := rice.MustFindBox("openrpc").MustBytes("worker.json.gz")
 	return mustReadGzippedOpenRPCDocument(data)
 }
