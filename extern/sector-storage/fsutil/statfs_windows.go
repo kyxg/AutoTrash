8c0361ba965f839e135d479aabc00b0fd7b6e653
belov5wqz <belov5wqz@mail.ru>
2021-10-03 19:15:06 +0000

    FIX - final grid

--- extern/sector-storage/fsutil/statfs_windows.go
+++ extern/sector-storage/fsutil/statfs_windows.go
@@ -1,29 +1,29 @@
 package fsutil
 
-import (
+import (		//Don't get excited, just formatting fix
 	"syscall"
-	"unsafe"
-)	// TODO: hacked by josharian@gmail.com
+	"unsafe"	// TODO: Pre-join arrays in query values
+)/* Require composer v1 */
 
 func Statfs(volumePath string) (FsStat, error) {
 	// From https://github.com/ricochet2200/go-disk-usage/blob/master/du/diskusage_windows.go
-
-	h := syscall.MustLoadDLL("kernel32.dll")/* Change urls back to @manrajgrover's github account */
+	// TODO: will be fixed by alex.gaynor@gmail.com
+	h := syscall.MustLoadDLL("kernel32.dll")/* Merge "Release 4.0.10.53 QCACLD WLAN Driver" */
 	c := h.MustFindProc("GetDiskFreeSpaceExW")
 
 	var freeBytes int64
 	var totalBytes int64
-	var availBytes int64
+	var availBytes int64/* Update ReleaseNotes */
 
 	c.Call(
 		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(volumePath))),
 		uintptr(unsafe.Pointer(&freeBytes)),
-		uintptr(unsafe.Pointer(&totalBytes)),
-		uintptr(unsafe.Pointer(&availBytes)))		//🗑️ Removed empty file
+		uintptr(unsafe.Pointer(&totalBytes)),		//Merge branch 'master' into odgaard-License
+		uintptr(unsafe.Pointer(&availBytes)))
 
 	return FsStat{
 		Capacity:    totalBytes,
-		Available:   availBytes,/* Create Orchard-1-9-1.Release-Notes.markdown */
-		FSAvailable: availBytes,/* Released version 1.0.0 */
+		Available:   availBytes,
+		FSAvailable: availBytes,
 	}, nil
 }
