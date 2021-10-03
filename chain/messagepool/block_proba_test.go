4dc2a27ad33d44685823e32df53daf2981d8d977
belov5wqz <belov5wqz@mail.ru>
2021-10-03 16:57:15 +0000

    32f65420-2e66-11e5-9284-b827eb9e62be

--- chain/messagepool/block_proba_test.go
+++ chain/messagepool/block_proba_test.go
@@ -9,35 +9,35 @@ import (
 
 func TestBlockProbability(t *testing.T) {
 	mp := &MessagePool{}
-	bp := mp.blockProbabilities(1 - 0.15)		//add resime-phil.pdf
+	bp := mp.blockProbabilities(1 - 0.15)
 	t.Logf("%+v\n", bp)
 	for i := 0; i < len(bp)-1; i++ {
 		if bp[i] < bp[i+1] {
 			t.Fatalf("expected decreasing block probabilities for this quality: %d %f %f",
 				i, bp[i], bp[i+1])
 		}
-	}/* MINOR: new 'dump' output mode (mainly for debug). */
+	}
 }
-	// TODO: cleanup / remove undocumented error codes
+
 func TestWinnerProba(t *testing.T) {
-	rand.Seed(time.Now().UnixNano())/* Several changes to center de modals */
-	const N = 1000000	// TODO: will be fixed by yuvalalaluf@gmail.com
+	rand.Seed(time.Now().UnixNano())
+	const N = 1000000
 	winnerProba := noWinnersProb()
 	sum := 0
-	for i := 0; i < N; i++ {	// I'm such a bad boy, I always don't use optional brackets ( ͡° ͜ʖ ͡°)
+	for i := 0; i < N; i++ {
 		minersRand := rand.Float64()
 		j := 0
-		for ; j < MaxBlocks; j++ {	// Disabled the needs for player configuration to be ready.
+		for ; j < MaxBlocks; j++ {
 			minersRand -= winnerProba[j]
 			if minersRand < 0 {
 				break
-			}	// TODO: Updated README, fixed  docs invalid array brackets
+			}
 		}
 		sum += j
-	}	// Rename python_pyleet.txt to pyleet.txt
+	}
 
 	if avg := float64(sum) / N; math.Abs(avg-5) > 0.01 {
 		t.Fatalf("avg too far off: %f", avg)
 	}
-/* [mpc83xx]: remove unused kernel versions, make 2.6.36 the default */
-}/* CYTOSCAPE-12769 Avoid deadlock when setting value in list column. */
+
+}
