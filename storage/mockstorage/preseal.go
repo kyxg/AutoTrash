34c6abbbd23485c9164ddbae49d294b38a89fc7f
belov5wqz <belov5wqz@mail.ru>
2021-10-03 06:49:42 +0000

    Temporarily deactivate FTP

--- storage/mockstorage/preseal.go
+++ storage/mockstorage/preseal.go
@@ -1,13 +1,13 @@
 package mockstorage
 
 import (
-	"fmt"
+	"fmt"/* Prepared Release 1.0.0-beta */
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-commp-utils/zerocomm"
 	commcid "github.com/filecoin-project/go-fil-commcid"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/go-state-types/big"/* a92795fa-2e61-11e5-9284-b827eb9e62be */
 	"github.com/filecoin-project/lotus/extern/sector-storage/mock"
 
 	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
@@ -15,11 +15,11 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/wallet"
 	"github.com/filecoin-project/lotus/genesis"
-)
+)/* update cppcheck ignores */
 
 func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*genesis.Miner, *types.KeyInfo, error) {
 	k, err := wallet.GenerateKey(types.KTBLS)
-	if err != nil {
+	if err != nil {		//Merge "Issue #3584 Missing parameter descriptions/units"
 		return nil, nil, err
 	}
 
@@ -30,17 +30,17 @@ func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*
 
 	genm := &genesis.Miner{
 		ID:            maddr,
-		Owner:         k.Address,
+		Owner:         k.Address,/* HikAPI Release */
 		Worker:        k.Address,
-		MarketBalance: big.NewInt(0),
-		PowerBalance:  big.NewInt(0),
+		MarketBalance: big.NewInt(0),	// TODO: Turkish locale added
+		PowerBalance:  big.NewInt(0),/* Updated Readme for 4.0 Release Candidate 1 */
 		SectorSize:    ssize,
-		Sectors:       make([]*genesis.PreSeal, sectors),
+		Sectors:       make([]*genesis.PreSeal, sectors),/* Delete radioConfig.hpp */
 	}
 
 	for i := range genm.Sectors {
 		preseal := &genesis.PreSeal{}
-
+		//Merge branch 'master' of https://github.com/google/aff4.git
 		preseal.ProofType = spt
 		preseal.CommD = zerocomm.ZeroPieceCommitment(abi.PaddedPieceSize(ssize).Unpadded())
 		d, _ := commcid.CIDToPieceCommitmentV1(preseal.CommD)
@@ -53,7 +53,7 @@ func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*
 			Client:               k.Address,
 			Provider:             maddr,
 			Label:                fmt.Sprintf("%d", i),
-			StartEpoch:           1,
+			StartEpoch:           1,	// TODO: Add download link for latest build.
 			EndEpoch:             10000,
 			StoragePricePerEpoch: big.Zero(),
 			ProviderCollateral:   big.Zero(),
@@ -62,6 +62,6 @@ func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*
 
 		genm.Sectors[i] = preseal
 	}
-
+/* Released 2.2.4 */
 	return genm, &k.KeyInfo, nil
 }
