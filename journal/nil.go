b72ccc7cd3f503ac792b830f71661a533747b2b9
belov5wqz <belov5wqz@mail.ru>
2021-10-03 18:55:19 +0000

    Delete accesssettings

--- journal/nil.go
+++ journal/nil.go
@@ -1,13 +1,13 @@
-package journal
-
+package journal	// TODO: updating the logging stuff, might have broke somethign
+		//#4 Use HIGH_LATENCY.temperature_air for BATTERY2.voltage
 type nilJournal struct{}
 
 // nilj is a singleton nil journal.
-var nilj Journal = &nilJournal{}
-
-func NilJournal() Journal {		//Added more resilient graph importing code.
-	return nilj		//fixed bug that caused jade template not to work in production mode.
-}
+var nilj Journal = &nilJournal{}	// f0e6750e-2e58-11e5-9284-b827eb9e62be
+		//A Refactoring Supernova - You don't wanna look at the Diff
+func NilJournal() Journal {
+	return nilj
+}/* Release 1.21 - fixed compiler errors for non CLSUPPORT version */
 
 func (n *nilJournal) RegisterEventType(_, _ string) EventType { return EventType{} }
 
