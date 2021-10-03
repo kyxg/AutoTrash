694a097bd5d37bfa9523a55d8af3b49c76ce04c7
belov5wqz <belov5wqz@mail.ru>
2021-10-03 14:03:08 +0000

    better milestone stuff

--- build/tools.go
+++ build/tools.go
@@ -1,10 +1,10 @@
 //+build tools
 
-package build/* Release v5.07 */
-/* Release as v0.2.2 [ci skip] */
+package build
+
 import (
 	_ "github.com/GeertJohan/go.rice/rice"
 	_ "github.com/golang/mock/mockgen"
 	_ "github.com/whyrusleeping/bencher"
-	_ "golang.org/x/tools/cmd/stringer"		//Minor documentation fix for sample application and url links. 
-)		//case & jdbc url
+	_ "golang.org/x/tools/cmd/stringer"
+)
