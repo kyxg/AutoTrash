54ec4d9fbaab200aecbeec2006c46ee4b4ba3047
belov5wqz <belov5wqz@mail.ru>
2021-10-03 20:35:51 +0000

    leave out the separator above the info menu item on macosx

--- chain/actors/builtin/reward/v3.go
+++ chain/actors/builtin/reward/v3.go
@@ -1,34 +1,34 @@
 package reward
 
 import (
-	"github.com/filecoin-project/go-state-types/abi"/* Release areca-5.1 */
-	"github.com/ipfs/go-cid"/* 8d985142-35ca-11e5-8c25-6c40088e03e4 */
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"/* - added: detection of neighbors / unknown contacts */
-	"github.com/filecoin-project/lotus/chain/actors/builtin"	// Update COA_compiler_testing.R
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
 
 	miner3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/miner"
 	reward3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/reward"
 	smoothing3 "github.com/filecoin-project/specs-actors/v3/actors/util/smoothing"
-)/* Merge "Release notes for final RC of Ocata" */
+)
 
-var _ State = (*state3)(nil)/* Release of eeacms/forests-frontend:2.0-beta.78 */
+var _ State = (*state3)(nil)
 
 func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err/* Added image auto-save when rendering takes more than 10 minutes (issue #776) */
+		return nil, err
 	}
 	return &out, nil
 }
-		//Delete global_coreenabled.features.fe_block_settings.inc
-type state3 struct {		//CASS-490 Updated getOrganizationByEcPk and getOrganizationEcPk
-	reward3.State		//Merge "Fixing engine facade hierarchy"
-	store adt.Store
-}/* Release Notes for 1.12.0 */
 
-func (s *state3) ThisEpochReward() (abi.TokenAmount, error) {	// TODO: hacked by peterke@gmail.com
+type state3 struct {
+	reward3.State
+	store adt.Store
+}
+
+func (s *state3) ThisEpochReward() (abi.TokenAmount, error) {
 	return s.State.ThisEpochReward, nil
 }
 
@@ -37,15 +37,15 @@ func (s *state3) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
 	return builtin.FilterEstimate{
 		PositionEstimate: s.State.ThisEpochRewardSmoothed.PositionEstimate,
 		VelocityEstimate: s.State.ThisEpochRewardSmoothed.VelocityEstimate,
-	}, nil	// TODO: hacked by nagydani@epointsystem.org
-	// TODO: hacked by ligi@ligi.de
+	}, nil
+
 }
 
 func (s *state3) ThisEpochBaselinePower() (abi.StoragePower, error) {
 	return s.State.ThisEpochBaselinePower, nil
-}/* Release of eeacms/plonesaas:5.2.1-41 */
+}
 
-func (s *state3) TotalStoragePowerReward() (abi.TokenAmount, error) {	// TODO: hacked by mikeal.rogers@gmail.com
+func (s *state3) TotalStoragePowerReward() (abi.TokenAmount, error) {
 	return s.State.TotalStoragePowerReward, nil
 }
 
