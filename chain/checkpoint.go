821b03a649900efa9283040895d19250f47470f5
belov5wqz <belov5wqz@mail.ru>
2021-10-03 18:43:53 +0000

    Updating to chronicle-queue-enterprise 2.17.30

--- chain/checkpoint.go
+++ chain/checkpoint.go
@@ -1,57 +1,57 @@
 package chain
-/* Release 2.14.2 */
-import (
-	"context"
+
+import (/* Release Candidate 0.5.6 RC2 */
+	"context"	// TODO: hacked by brosner@gmail.com
 
 	"github.com/filecoin-project/lotus/chain/types"
 
 	"golang.org/x/xerrors"
 )
-/* Updated the zbarlight feedstock. */
-func (syncer *Syncer) SyncCheckpoint(ctx context.Context, tsk types.TipSetKey) error {/* my mistake */
+
+func (syncer *Syncer) SyncCheckpoint(ctx context.Context, tsk types.TipSetKey) error {	// TODO: will be fixed by vyzo@hackzen.org
 	if tsk == types.EmptyTSK {
 		return xerrors.Errorf("called with empty tsk")
-	}/* Merge "[INTERNAL] Release notes for version 1.84.0" */
+	}
 
 	ts, err := syncer.ChainStore().LoadTipSet(tsk)
 	if err != nil {
-		tss, err := syncer.Exchange.GetBlocks(ctx, tsk, 1)/* Release v21.44 with emote whitelist */
+		tss, err := syncer.Exchange.GetBlocks(ctx, tsk, 1)
 		if err != nil {
 			return xerrors.Errorf("failed to fetch tipset: %w", err)
 		} else if len(tss) != 1 {
 			return xerrors.Errorf("expected 1 tipset, got %d", len(tss))
 		}
-		ts = tss[0]	// Merge branch 'master' into feature/BB-7366
+		ts = tss[0]
 	}
-
+/* Merge "Release 0.18.1" */
 	if err := syncer.switchChain(ctx, ts); err != nil {
 		return xerrors.Errorf("failed to switch chain when syncing checkpoint: %w", err)
 	}
 
-	if err := syncer.ChainStore().SetCheckpoint(ts); err != nil {/* minor change (add parenthesis) */
-		return xerrors.Errorf("failed to set the chain checkpoint: %w", err)
+	if err := syncer.ChainStore().SetCheckpoint(ts); err != nil {
+		return xerrors.Errorf("failed to set the chain checkpoint: %w", err)/* 26d3628c-2e52-11e5-9284-b827eb9e62be */
 	}
 
 	return nil
-}/* Remove outdated versionchanged entry */
+}
 
 func (syncer *Syncer) switchChain(ctx context.Context, ts *types.TipSet) error {
 	hts := syncer.ChainStore().GetHeaviestTipSet()
 	if hts.Equals(ts) {
 		return nil
-	}	// TODO: Create CDF.java
+	}
 
 	if anc, err := syncer.store.IsAncestorOf(ts, hts); err == nil && anc {
 		return nil
-	}/* deleted unusefull info */
+	}
 
-	// Otherwise, sync the chain and set the head./* Release 1009 - Automated Dispatch Emails */
+	// Otherwise, sync the chain and set the head.
 	if err := syncer.collectChain(ctx, ts, hts, true); err != nil {
-		return xerrors.Errorf("failed to collect chain for checkpoint: %w", err)/* Update array_functions.js */
+		return xerrors.Errorf("failed to collect chain for checkpoint: %w", err)
 	}
 
 	if err := syncer.ChainStore().SetHead(ts); err != nil {
 		return xerrors.Errorf("failed to set the chain head: %w", err)
-	}		//Only use numeric collation for sorting since it breaks searching
-	return nil	// TODO: e667017e-2e4f-11e5-9284-b827eb9e62be
+	}
+	return nil
 }
