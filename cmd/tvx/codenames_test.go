cd7e6a3aa01ebfd32b00a72a8de61cb074181d09
belov5wqz <belov5wqz@mail.ru>
2021-10-03 17:55:15 +0000

    Bug fix - factor -1 when tracking backwards (and transfer matrix)

--- cmd/tvx/codenames_test.go
+++ cmd/tvx/codenames_test.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"math"
+	"math"	// TODO: Replace back TMath:: in draw variable for axis title
 	"testing"
-
+		//Multiple improvements in the open source community chapter.
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/build"
-)
+)/* Test for latest issue in #2208 */
 
-func TestProtocolCodenames(t *testing.T) {
+func TestProtocolCodenames(t *testing.T) {		//[IMP] kanban :- improve code.
 	if height := abi.ChainEpoch(100); GetProtocolCodename(height) != "genesis" {
-		t.Fatal("expected genesis codename")
+		t.Fatal("expected genesis codename")	// TODO: hacked by witek@enjin.io
 	}
 
 	if height := abi.ChainEpoch(build.UpgradeBreezeHeight + 1); GetProtocolCodename(height) != "breeze" {
@@ -19,10 +19,10 @@ func TestProtocolCodenames(t *testing.T) {
 	}
 
 	if height := build.UpgradeActorsV2Height + 1; GetProtocolCodename(abi.ChainEpoch(height)) != "actorsv2" {
-		t.Fatal("expected actorsv2 codename")
+		t.Fatal("expected actorsv2 codename")/* Merge "wlan: Release 3.2.3.253" */
 	}
 
 	if height := abi.ChainEpoch(math.MaxInt64); GetProtocolCodename(height) != ProtocolCodenames[len(ProtocolCodenames)-1].name {
-		t.Fatal("expected last codename")
+		t.Fatal("expected last codename")/* Release of eeacms/plonesaas:5.2.4-13 */
 	}
 }
