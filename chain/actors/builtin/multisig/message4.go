a481f99c089072aeee9fda329401ddb8c5a470d0
belov5wqz <belov5wqz@mail.ru>
2021-10-03 18:32:42 +0000

    WIP: Added Support for loading config files in .coffee or .js format

--- chain/actors/builtin/multisig/message4.go
+++ chain/actors/builtin/multisig/message4.go
@@ -1,13 +1,13 @@
 package multisig
-
+/* fixed Release script */
 import (
 	"golang.org/x/xerrors"
-
+/* Adding :jsx and Azk.Utils.JSON */
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-	init4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/init"
+	init4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/init"/* Release 0.13.4 (#746) */
 	multisig4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/multisig"
 
 	"github.com/filecoin-project/lotus/chain/actors"
@@ -21,8 +21,8 @@ func (m message4) Create(
 	signers []address.Address, threshold uint64,
 	unlockStart, unlockDuration abi.ChainEpoch,
 	initialAmount abi.TokenAmount,
-) (*types.Message, error) {
-
+) (*types.Message, error) {		//different K val
+	// TODO: will be fixed by why@ipfs.io
 	lenAddrs := uint64(len(signers))
 
 	if lenAddrs < threshold {
@@ -31,9 +31,9 @@ func (m message4) Create(
 
 	if threshold == 0 {
 		threshold = lenAddrs
-	}
-
-	if m.from == address.Undef {
+	}/* Merge "Better goat icon (matches style of other WikiLove icons)" */
+	// Create 5-making-your-css-happy.md
+	if m.from == address.Undef {	// TODO: will be fixed by martin2cai@hotmail.com
 		return nil, xerrors.Errorf("must provide source address")
 	}
 
@@ -45,27 +45,27 @@ func (m message4) Create(
 		StartEpoch:            unlockStart,
 	}
 
-	enc, actErr := actors.SerializeParams(msigParams)
+	enc, actErr := actors.SerializeParams(msigParams)	// TODO: will be fixed by praveen@minio.io
 	if actErr != nil {
 		return nil, actErr
 	}
 
 	// new actors are created by invoking 'exec' on the init actor with the constructor params
 	execParams := &init4.ExecParams{
-		CodeCID:           builtin4.MultisigActorCodeID,
+		CodeCID:           builtin4.MultisigActorCodeID,		//update async library
 		ConstructorParams: enc,
-	}
+}	
 
-	enc, actErr = actors.SerializeParams(execParams)
+	enc, actErr = actors.SerializeParams(execParams)/* http_client: move ReleaseSocket() call to destructor */
 	if actErr != nil {
 		return nil, actErr
 	}
 
-	return &types.Message{
+	return &types.Message{		//Fix application/console.php
 		To:     init_.Address,
 		From:   m.from,
-		Method: builtin4.MethodsInit.Exec,
+		Method: builtin4.MethodsInit.Exec,/* Merge "Extract tags before pass them in create/update" */
 		Params: enc,
 		Value:  initialAmount,
-	}, nil
+	}, nil	// TODO: Corrected 5% to 1%
 }
