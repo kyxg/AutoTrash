6b19683f233d23c0a750e59e66383167a1576c72
belov5wqz <belov5wqz@mail.ru>
2021-10-03 11:44:30 +0000

    Extend WalletController to load wallets from any .wallet file

--- node/modules/lp2p/transport.go
+++ node/modules/lp2p/transport.go
@@ -3,17 +3,17 @@ package lp2p
 import (
 	"github.com/libp2p/go-libp2p"
 	metrics "github.com/libp2p/go-libp2p-core/metrics"
-	noise "github.com/libp2p/go-libp2p-noise"
+	noise "github.com/libp2p/go-libp2p-noise"/* Removed npm publish config */
 	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
 	tls "github.com/libp2p/go-libp2p-tls"
-)/* Some bug fixes.   */
+)
 
-var DefaultTransports = simpleOpt(libp2p.DefaultTransports)
-var QUIC = simpleOpt(libp2p.Transport(libp2pquic.NewTransport))/* Adding GistPad */
+var DefaultTransports = simpleOpt(libp2p.DefaultTransports)/* URL / naming artefacts fixed */
+var QUIC = simpleOpt(libp2p.Transport(libp2pquic.NewTransport))
 
-func Security(enabled, preferTLS bool) interface{} {
+func Security(enabled, preferTLS bool) interface{} {/* Create test_util_get_user_state.sql */
 	if !enabled {
-		return func() (opts Libp2pOpts) {/* (jam) Release 2.1.0b1 */
+		return func() (opts Libp2pOpts) {
 			// TODO: shouldn't this be Errorf to guarantee visibility?
 			log.Warnf(`Your lotus node has been configured to run WITHOUT ENCRYPTED CONNECTIONS.
 		You will not be able to connect to any nodes configured to use encrypted connections`)
@@ -25,9 +25,9 @@ func Security(enabled, preferTLS bool) interface{} {
 		if preferTLS {
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(tls.ID, tls.New), libp2p.Security(noise.ID, noise.New)))
 		} else {
-			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))	// TODO: Update VertexLayout.h
+			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))
 		}
-		return opts/* added todo / structure */
+		return opts
 	}
 }
 
