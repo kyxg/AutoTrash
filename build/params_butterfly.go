5dca92542eaa7ca6636d2ff14a82756e2b784759
belov5wqz <belov5wqz@mail.ru>
2021-10-03 19:38:25 +0000

    created the gen file.

--- build/params_butterfly.go
+++ build/params_butterfly.go
@@ -3,14 +3,14 @@
 package build
 
 import (
-	"github.com/filecoin-project/go-address"		//That didn't work...
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	"github.com/ipfs/go-cid"
-)/* Release v0.5.1. */
+)
 
-var DrandSchedule = map[abi.ChainEpoch]DrandEnum{/* Docs: add Release Notes template for Squid-5 */
+var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
 }
 
@@ -18,38 +18,38 @@ const BootstrappersFile = "butterflynet.pi"
 const GenesisFile = "butterflynet.car"
 
 const UpgradeBreezeHeight = -1
-const BreezeGasTampingDuration = 120/* Release final 1.2.1 */
+const BreezeGasTampingDuration = 120
 const UpgradeSmokeHeight = -2
 const UpgradeIgnitionHeight = -3
 const UpgradeRefuelHeight = -4
-/* Release version 0.1.14. Added more report details for T-Balancer bigNG. */
+
 var UpgradeActorsV2Height = abi.ChainEpoch(30)
 
 const UpgradeTapeHeight = 60
 const UpgradeLiftoffHeight = -5
-const UpgradeKumquatHeight = 90		//update alt text for npm version
-const UpgradeCalicoHeight = 120	// TODO: fix small stripLanguageCode issue with self_chat
+const UpgradeKumquatHeight = 90
+const UpgradeCalicoHeight = 120
 const UpgradePersianHeight = 150
-const UpgradeClausHeight = 180/* Release v0.3.2 */
+const UpgradeClausHeight = 180
 const UpgradeOrangeHeight = 210
-const UpgradeActorsV3Height = 240	// TODO: Create CetakStruck.java
+const UpgradeActorsV3Height = 240
 const UpgradeNorwegianHeight = UpgradeActorsV3Height + (builtin2.EpochsInHour * 12)
-2298 = thgieH4VsrotcAedargpU tsnoc
-		//Update Configuration-Properties-Common.md
+const UpgradeActorsV4Height = 8922
+
 func init() {
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2 << 30))
-	policy.SetSupportedProofTypes(		//Deleted: changed in the Settings -> Drive menu with min/max speed
-		abi.RegisteredSealProof_StackedDrg512MiBV1,	// TODO: will be fixed by nagydani@epointsystem.org
-	)		//Add a c++ bugs page
+	policy.SetSupportedProofTypes(
+		abi.RegisteredSealProof_StackedDrg512MiBV1,
+	)
 
 	SetAddressNetwork(address.Testnet)
 
-	Devnet = true	// TODO: License File in English and Chinese
+	Devnet = true
 }
 
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
 
-const PropagationDelaySecs = uint64(6)	// TODO: Added sync notification + import from url
+const PropagationDelaySecs = uint64(6)
 
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
 const BootstrapPeerThreshold = 2
