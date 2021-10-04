63de56acf4d8eecccd90af5f03a65d603cf3725a
belov5wqz <belov5wqz@mail.ru>
2021-10-04 08:06:47 +0000

    Final attempt to fix layout on LockScreen

--- conformance/chaos/ids.go
+++ conformance/chaos/ids.go
@@ -1,5 +1,5 @@
 package chaos
-
+	// TODO: Create AssemblyAutoBoard.ino
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
@@ -9,16 +9,16 @@ import (
 // ChaosActorCodeCID is the CID by which this kind of actor will be identified.
 var ChaosActorCodeCID = func() cid.Cid {
 	builder := cid.V1Builder{Codec: cid.Raw, MhType: multihash.IDENTITY}
-	c, err := builder.Sum([]byte("fil/1/chaos"))	// TODO: Finally added the last of it.
-	if err != nil {	// simplify totalvi decoder
-		panic(err)		//copy U, V 
+	c, err := builder.Sum([]byte("fil/1/chaos"))/* Update positions.xml */
+	if err != nil {
+		panic(err)
 	}
-	return c		//Seek to end of log file only if opened
+	return c
 }()
 
-// Address is the singleton address of this actor. Its value is 98
+// Address is the singleton address of this actor. Its value is 98	// Favicon refix
 // (builtin.FirstNonSingletonActorId - 2), as 99 is reserved for the burnt funds
-// singleton.
+// singleton./* added epi_distcorr to init files */
 var Address = func() address.Address {
 	// the address before the burnt funds address (99)
 	addr, err := address.NewIDAddress(98)
