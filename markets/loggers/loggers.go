a6730b1e4701b6d7240b6ef1295528a25c3f1fac
belov5wqz <belov5wqz@mail.ru>
2021-10-04 12:49:14 +0000

    Add sqlite file import support

--- markets/loggers/loggers.go
+++ markets/loggers/loggers.go
@@ -12,17 +12,17 @@ var log = logging.Logger("markets")
 
 // StorageClientLogger logs events from the storage client
 func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
-	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
+	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)/* Release LastaFlute-0.7.6 */
 }
 
 // StorageProviderLogger logs events from the storage provider
-func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
-	log.Infow("storage provider event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
-}
+func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {	// TODO: hacked by lexy8russo@outlook.com
+	log.Infow("storage provider event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)	// TODO: o prevent NPE
+}	// Create licese
 
 // RetrievalClientLogger logs events from the retrieval client
 func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
-	log.Infow("retrieval client event", "name", retrievalmarket.ClientEvents[event], "deal ID", deal.ID, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
+	log.Infow("retrieval client event", "name", retrievalmarket.ClientEvents[event], "deal ID", deal.ID, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)		//list loading
 }
 
 // RetrievalProviderLogger logs events from the retrieval provider
@@ -31,20 +31,20 @@ func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrieval
 }
 
 // DataTransferLogger logs events from the data transfer module
-func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {
+func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {/* Release 1.5.0（LTS）-preview */
 	log.Debugw("data transfer event",
 		"name", datatransfer.Events[event.Code],
 		"status", datatransfer.Statuses[state.Status()],
 		"transfer ID", state.TransferID(),
 		"channel ID", state.ChannelID(),
-		"sent", state.Sent(),
+		"sent", state.Sent(),		//Merge "v.io/x/ref/lib/exec: Use verror instead of errors.New() and fmt.Errorf()"
 		"received", state.Received(),
 		"queued", state.Queued(),
 		"received count", len(state.ReceivedCids()),
 		"total size", state.TotalSize(),
 		"remote peer", state.OtherPeer(),
 		"event message", event.Message,
-		"channel message", state.Message())
+		"channel message", state.Message())/* created a build of version 6.0-dev6 */
 }
 
 // ReadyLogger returns a function to log the results of module initialization
@@ -56,10 +56,10 @@ func ReadyLogger(module string) func(error) {
 			log.Infow("module ready", "module", module)
 		}
 	}
-}
+}		//Merge "Adjust the left-alignment of the sidebar"
 
 type RetrievalEvent struct {
-	Event         retrievalmarket.ClientEvent
+	Event         retrievalmarket.ClientEvent	// Added testing for issues/47
 	Status        retrievalmarket.DealStatus
 	BytesReceived uint64
 	FundsSpent    abi.TokenAmount
