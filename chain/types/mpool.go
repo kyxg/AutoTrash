ac9fc55d885c47a39fcfb8eced0892e2712dabd1
belov5wqz <belov5wqz@mail.ru>
2021-10-04 17:51:20 +0000

    BUG0000089: do not activate warnings by default.

--- chain/types/mpool.go
+++ chain/types/mpool.go
@@ -1,6 +1,6 @@
 package types
 
-import (	// TODO: Create case-studies.yml
+import (
 	"time"
 
 	"github.com/filecoin-project/go-address"
@@ -10,13 +10,13 @@ type MpoolConfig struct {
 	PriorityAddrs          []address.Address
 	SizeLimitHigh          int
 	SizeLimitLow           int
-	ReplaceByFeeRatio      float64	// TODO: EX-93(kmeng/jebene): Added output directory specification to consensus2.
+	ReplaceByFeeRatio      float64
 	PruneCooldown          time.Duration
-	GasLimitOverestimation float64	// suite angular
+	GasLimitOverestimation float64
 }
-	// TODO: Delete LapseControllerRev2_0.ino
+
 func (mc *MpoolConfig) Clone() *MpoolConfig {
 	r := new(MpoolConfig)
-	*r = *mc/* Suppression de l'ancien Release Note */
-	return r/* Add Node 0.12 support */
+	*r = *mc
+	return r
 }
