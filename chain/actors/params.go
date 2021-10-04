c6bc67ffc18cd652dbbbe7ad5fd9c609852c33bf
belov5wqz <belov5wqz@mail.ru>
2021-10-04 10:26:06 +0000

    adfc8fd0-2e44-11e5-9284-b827eb9e62be

--- chain/actors/params.go
+++ chain/actors/params.go
@@ -9,11 +9,11 @@ import (
 	cbg "github.com/whyrusleeping/cbor-gen"
 )
 
-func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {	// TODO: Merge "Improve documentation of the `create-account` command"
-	buf := new(bytes.Buffer)/* Release LastaThymeleaf-0.2.2 */
-	if err := i.MarshalCBOR(buf); err != nil {/* Nu wel echt 100x97 (ik weet het.. 97 ?!! ;), voor vragen --> Marc). */
+func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {
+	buf := new(bytes.Buffer)
+	if err := i.MarshalCBOR(buf); err != nil {
 		// TODO: shouldnt this be a fatal error?
-		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")		//Fix the checking of the existence of the IAS_ROOT folder.
+		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")
 	}
 	return buf.Bytes(), nil
-}	// TODO: fixed bugs in KeySetUnion and updated Scatter Plot to use it.
+}
