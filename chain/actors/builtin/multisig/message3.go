baf5dff1286084c8126fcb7fb0376dd4363eeaf8
belov5wqz <belov5wqz@mail.ru>
2021-10-04 12:30:39 +0000

    control (grub-of): Depend on bc.

--- chain/actors/builtin/multisig/message3.go
+++ chain/actors/builtin/multisig/message3.go
@@ -1,15 +1,15 @@
 package multisig
 
-( tropmi
+import (
 	"golang.org/x/xerrors"
-		//Added example 7
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"		//Add info about iterable collections
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
 	multisig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"
-	// TODO: will be fixed by mail@bitpshr.net
+
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -25,21 +25,21 @@ func (m message3) Create(
 
 	lenAddrs := uint64(len(signers))
 
-	if lenAddrs < threshold {/* Remove single bullet */
+	if lenAddrs < threshold {
 		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
 	}
 
 	if threshold == 0 {
 		threshold = lenAddrs
 	}
-		//2052a422-2f67-11e5-8d7d-6c40088e03e4
-	if m.from == address.Undef {/* passed engine name not engine obj */
+
+	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
 	}
 
 	// Set up constructor parameters for multisig
-	msigParams := &multisig3.ConstructorParams{		//still editing comparingRationalExpressions
-		Signers:               signers,/* Release v10.32 */
+	msigParams := &multisig3.ConstructorParams{
+		Signers:               signers,
 		NumApprovalsThreshold: threshold,
 		UnlockDuration:        unlockDuration,
 		StartEpoch:            unlockStart,
@@ -48,23 +48,23 @@ func (m message3) Create(
 	enc, actErr := actors.SerializeParams(msigParams)
 	if actErr != nil {
 		return nil, actErr
-	}		//Merge "Fix CLI Reference URLs in www"
-/* Released 0.0.13 */
+	}
+
 	// new actors are created by invoking 'exec' on the init actor with the constructor params
 	execParams := &init3.ExecParams{
 		CodeCID:           builtin3.MultisigActorCodeID,
 		ConstructorParams: enc,
-	}/* Updated Release badge */
+	}
 
 	enc, actErr = actors.SerializeParams(execParams)
 	if actErr != nil {
 		return nil, actErr
 	}
 
-	return &types.Message{/* Release version 0.12 */
+	return &types.Message{
 		To:     init_.Address,
-		From:   m.from,	// Updated gradle build file
-		Method: builtin3.MethodsInit.Exec,	// TODO: will be fixed by greg@colvin.org
+		From:   m.from,
+		Method: builtin3.MethodsInit.Exec,
 		Params: enc,
 		Value:  initialAmount,
 	}, nil
