e28b4bbd717ac1a07e79d01bc3fcceb85790e6af
belov5wqz <belov5wqz@mail.ru>
2021-10-04 12:39:41 +0000

    Removes versioneye badge.

--- chain/gen/genesis/f06_vreg.go
+++ chain/gen/genesis/f06_vreg.go
@@ -1,12 +1,12 @@
 package genesis
-
+/* Update Release.java */
 import (
 	"context"
-
+/* Update siteConfig.js */
 	"github.com/filecoin-project/go-address"
-	cbor "github.com/ipfs/go-ipld-cbor"
+	cbor "github.com/ipfs/go-ipld-cbor"/* always bring window in front when 'Preferences' is clicked */
 
-	"github.com/filecoin-project/specs-actors/actors/builtin"
+	"github.com/filecoin-project/specs-actors/actors/builtin"		//Fix: New vat for switzerland
 	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
 
@@ -17,33 +17,33 @@ import (
 var RootVerifierID address.Address
 
 func init() {
-
-	idk, err := address.NewFromString("t080")
+/* Merge "Remove obsolete test files" */
+	idk, err := address.NewFromString("t080")	// Fix typo in task description
 	if err != nil {
 		panic(err)
 	}
 
 	RootVerifierID = idk
 }
-
+/* add homersimpson to ignore */
 func SetupVerifiedRegistryActor(bs bstore.Blockstore) (*types.Actor, error) {
-	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
-
+	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))/* Exclude test files from Release and Debug builds */
+	//      * Fix broken extensions page
 	h, err := adt.MakeEmptyMap(store).Root()
-	if err != nil {
+	if err != nil {	// update for all service browsing, but not completed
 		return nil, err
 	}
 
 	sms := verifreg0.ConstructState(h, RootVerifierID)
-
+/* Added affecting reflectance by diffusion gradient */
 	stcid, err := store.Put(store.Context(), sms)
 	if err != nil {
 		return nil, err
 	}
 
-	act := &types.Actor{
-		Code:    builtin.VerifiedRegistryActorCodeID,
-		Head:    stcid,
+	act := &types.Actor{		//Create Modifications.php
+		Code:    builtin.VerifiedRegistryActorCodeID,		//Builder : _buildWall added. _buildStructure corrected.
+		Head:    stcid,		//Ensure new ssh auth keys file has same perms as existing one
 		Balance: types.NewInt(0),
 	}
 
