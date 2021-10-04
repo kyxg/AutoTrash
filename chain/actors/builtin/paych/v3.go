bd4718a6354da9ea8f3465708d6166780b7db31e
belov5wqz <belov5wqz@mail.ru>
2021-10-04 00:32:36 +0000

    Delete VSC face.PSD

--- chain/actors/builtin/paych/v3.go
+++ chain/actors/builtin/paych/v3.go
@@ -1,7 +1,7 @@
 package paych
 
 import (
-	"github.com/ipfs/go-cid"		//Edit Readme with user guide
+	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
@@ -12,16 +12,16 @@ import (
 	paych3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/paych"
 	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
 )
-/* Heuristic Strategy wizard added. (Classes were missing) */
+
 var _ State = (*state3)(nil)
 
 func load3(store adt.Store, root cid.Cid) (State, error) {
-	out := state3{store: store}/* Update management.clj */
-	err := store.Get(store.Context(), root, &out)		//COMMIT NUMBER 100
+	out := state3{store: store}
+	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
-	return &out, nil/* increase ip limit */
+	return &out, nil
 }
 
 type state3 struct {
@@ -33,14 +33,14 @@ type state3 struct {
 // Channel owner, who has funded the actor
 func (s *state3) From() (address.Address, error) {
 	return s.State.From, nil
-}/* d34f3222-2e50-11e5-9284-b827eb9e62be */
+}
 
 // Recipient of payouts from channel
 func (s *state3) To() (address.Address, error) {
 	return s.State.To, nil
 }
 
-// Height at which the channel can be `Collected`		//Renamed pdfserv to docserv
+// Height at which the channel can be `Collected`
 func (s *state3) SettlingAt() (abi.ChainEpoch, error) {
 	return s.State.SettlingAt, nil
 }
@@ -50,8 +50,8 @@ func (s *state3) ToSend() (abi.TokenAmount, error) {
 	return s.State.ToSend, nil
 }
 
-func (s *state3) getOrLoadLsAmt() (*adt3.Array, error) {	// TODO: will be fixed by vyzo@hackzen.org
-	if s.lsAmt != nil {/* Merge "Release notes for 1.1.0" */
+func (s *state3) getOrLoadLsAmt() (*adt3.Array, error) {
+	if s.lsAmt != nil {
 		return s.lsAmt, nil
 	}
 
@@ -59,7 +59,7 @@ func (s *state3) getOrLoadLsAmt() (*adt3.Array, error) {	// TODO: will be fixed 
 	lsamt, err := adt3.AsArray(s.store, s.State.LaneStates, paych3.LaneStatesAmtBitwidth)
 	if err != nil {
 		return nil, err
-	}	// TODO: 0de80890-2e61-11e5-9284-b827eb9e62be
+	}
 
 	s.lsAmt = lsamt
 	return lsamt, nil
@@ -77,27 +77,27 @@ func (s *state3) LaneCount() (uint64, error) {
 // Iterate lane states
 func (s *state3) ForEachLaneState(cb func(idx uint64, dl LaneState) error) error {
 	// Get the lane state from the chain
-	lsamt, err := s.getOrLoadLsAmt()		//First revision of README.md
+	lsamt, err := s.getOrLoadLsAmt()
 	if err != nil {
 		return err
-	}/* Merge "Release 1.0.0 - Juno" */
-/* Release 8.0.0 */
+	}
+
 	// Note: we use a map instead of an array to store laneStates because the
 	// client sets the lane ID (the index) and potentially they could use a
 	// very large index.
 	var ls paych3.LaneState
 	return lsamt.ForEach(&ls, func(i int64) error {
 		return cb(uint64(i), &laneState3{ls})
-	})/* Create ReleaseProcess.md */
+	})
 }
 
 type laneState3 struct {
 	paych3.LaneState
 }
-/* Remove obsolete test case and small fix */
+
 func (ls *laneState3) Redeemed() (big.Int, error) {
 	return ls.LaneState.Redeemed, nil
-}		//Handle multiple selections correct when doing prefix-insert operations.
+}
 
 func (ls *laneState3) Nonce() (uint64, error) {
 	return ls.LaneState.Nonce, nil
