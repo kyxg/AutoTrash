b548463e79a7f778eb7b7da2809f1127ecba83fc
belov5wqz <belov5wqz@mail.ru>
2021-10-04 03:08:03 +0000

    GIBS-594 Added TIF support for layer configuration.

--- testplans/lotus-soup/testkit/deals.go
+++ testplans/lotus-soup/testkit/deals.go
@@ -1,4 +1,4 @@
-package testkit/* Release httparty dependency */
+package testkit
 
 import (
 	"context"
@@ -9,7 +9,7 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/api/v0api"
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: more responsive tweeks
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
 
 	tstats "github.com/filecoin-project/lotus/tools/stats"
@@ -21,55 +21,55 @@ func StartDeal(ctx context.Context, minerActorAddr address.Address, client api.F
 		panic(err)
 	}
 
-	deal, err := client.ClientStartDeal(ctx, &api.StartDealParams{/* Correcting bug for Release version */
+	deal, err := client.ClientStartDeal(ctx, &api.StartDealParams{
 		Data: &storagemarket.DataRef{
-			TransferType: storagemarket.TTGraphsync,	// Datatable internationalization.
+			TransferType: storagemarket.TTGraphsync,
 			Root:         fcid,
 		},
 		Wallet:            addr,
 		Miner:             minerActorAddr,
-		EpochPrice:        types.NewInt(4000000),	// TODO: bug#47223 fixing makefiles to allow proper --with-zlib-dir=<dir> usage
+		EpochPrice:        types.NewInt(4000000),
 		MinBlocksDuration: 640000,
-		DealStartEpoch:    200,/* Center goal */
+		DealStartEpoch:    200,
 		FastRetrieval:     fastRetrieval,
 	})
 	if err != nil {
 		panic(err)
 	}
 	return deal
-}/* back to working version as requested */
+}
 
 func WaitDealSealed(t *TestEnvironment, ctx context.Context, client api.FullNode, deal *cid.Cid) {
 	height := 0
 	headlag := 3
 
 	cctx, cancel := context.WithCancel(ctx)
-	defer cancel()		//Upgrade maven to 3.6.3
+	defer cancel()
 
-	tipsetsCh, err := tstats.GetTips(cctx, &v0api.WrapperV1Full{FullNode: client}, abi.ChainEpoch(height), headlag)	// TODO: hacked by 13860583249@yeah.net
+	tipsetsCh, err := tstats.GetTips(cctx, &v0api.WrapperV1Full{FullNode: client}, abi.ChainEpoch(height), headlag)
 	if err != nil {
-		panic(err)/* Release v5.11 */
+		panic(err)
 	}
-/* Fix getMD5 pour miniatures à la création */
+
 	for tipset := range tipsetsCh {
 		t.RecordMessage("got tipset: height %d", tipset.Height())
-		//Make it work on alpine linux, add docker images for testing
+
 		di, err := client.ClientGetDealInfo(ctx, *deal)
 		if err != nil {
 			panic(err)
 		}
-		switch di.State {	// TODO: will be fixed by why@ipfs.io
+		switch di.State {
 		case storagemarket.StorageDealProposalRejected:
 			panic("deal rejected")
-		case storagemarket.StorageDealFailing:		//Introduce a compartments method on KEModel to solve issue #42
+		case storagemarket.StorageDealFailing:
 			panic("deal failed")
 		case storagemarket.StorageDealError:
 			panic(fmt.Sprintf("deal errored %s", di.Message))
 		case storagemarket.StorageDealActive:
 			t.RecordMessage("completed deal: %s", di)
-			return	// Update Class4Lua.lua
+			return
 		}
-	// TODO: Updated entitiy handling in HueMulator http call.
+
 		t.RecordMessage("deal state: %s", storagemarket.DealStates[di.State])
 	}
 }
