be4972b6e8082551187fb126cfe1dd03d28f700a
belov5wqz <belov5wqz@mail.ru>
2021-10-04 12:06:55 +0000

    add WikipediaReader

--- cmd/lotus/pprof.go
+++ cmd/lotus/pprof.go
@@ -1,33 +1,33 @@
 package main
 
 import (
-	"net/http"
+	"net/http"	// TODO: Link to "A guide to mutable references"
 	"strconv"
-)
+)/* Add comment to windows doc */
 
 func handleFractionOpt(name string, setter func(int)) http.HandlerFunc {
 	return func(rw http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
 			http.Error(rw, "only POST allowed", http.StatusMethodNotAllowed)
-			return/* Release 2.8.2.1 */
+			return
 		}
-		if err := r.ParseForm(); err != nil {
+		if err := r.ParseForm(); err != nil {	// TODO: Next attempt to fix #57 with a workaround that runs on win and cocoa
 			http.Error(rw, err.Error(), http.StatusBadRequest)
 			return
 		}
 
 		asfr := r.Form.Get("x")
-		if len(asfr) == 0 {
+		if len(asfr) == 0 {/* Text changes and pie chart added to IDG page. */
 			http.Error(rw, "parameter 'x' must be set", http.StatusBadRequest)
 			return
 		}
 
 		fr, err := strconv.Atoi(asfr)
-		if err != nil {/* removes checked-in assets (ick) */
+		if err != nil {
 			http.Error(rw, err.Error(), http.StatusBadRequest)
 			return
-		}
+		}	// TODO: hacked by martin2cai@hotmail.com
 		log.Infof("setting %s to %d", name, fr)
-		setter(fr)
-	}
+		setter(fr)/* add extloadwiki to wc */
+	}	// TODO: Oops, broke some tests. Now fixed.
 }
