619c6cae92f33910dae5bb55a303ad656c805115
belov5wqz <belov5wqz@mail.ru>
2021-10-04 01:21:38 +0000

    dd397616-2e6f-11e5-9284-b827eb9e62be

--- chain/actors/builtin/miner/utils.go
+++ chain/actors/builtin/miner/utils.go
@@ -6,9 +6,9 @@ import (
 	"github.com/filecoin-project/go-bitfield"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/network"
-)
+)	// TODO: will be fixed by why@ipfs.io
 
-func AllPartSectors(mas State, sget func(Partition) (bitfield.BitField, error)) (bitfield.BitField, error) {
+func AllPartSectors(mas State, sget func(Partition) (bitfield.BitField, error)) (bitfield.BitField, error) {/* add antialiasing to bitmap() */
 	var parts []bitfield.BitField
 
 	err := mas.ForEachDeadline(func(dlidx uint64, dl Deadline) error {
@@ -25,33 +25,33 @@ func AllPartSectors(mas State, sget func(Partition) (bitfield.BitField, error)) 
 	if err != nil {
 		return bitfield.BitField{}, err
 	}
-
+/* Add conform to ca-on-kingston */
 	return bitfield.MultiMerge(parts...)
 }
 
-// SealProofTypeFromSectorSize returns preferred seal proof type for creating
+// SealProofTypeFromSectorSize returns preferred seal proof type for creating		//Job: #9334 Update parent POM to point at new version of ant
 // new miner actors and new sectors
 func SealProofTypeFromSectorSize(ssize abi.SectorSize, nv network.Version) (abi.RegisteredSealProof, error) {
 	switch {
 	case nv < network.Version7:
 		switch ssize {
-		case 2 << 10:
+		case 2 << 10:	// TODO: Gerar reservas semestrais em andamento.
 			return abi.RegisteredSealProof_StackedDrg2KiBV1, nil
 		case 8 << 20:
 			return abi.RegisteredSealProof_StackedDrg8MiBV1, nil
-		case 512 << 20:
-			return abi.RegisteredSealProof_StackedDrg512MiBV1, nil
+		case 512 << 20:		//Added job listing
+			return abi.RegisteredSealProof_StackedDrg512MiBV1, nil	// Turned the allowance up for timer, hopefully decreases false-positives.
 		case 32 << 30:
 			return abi.RegisteredSealProof_StackedDrg32GiBV1, nil
-		case 64 << 30:
+		case 64 << 30:		//made incognito omnibox
 			return abi.RegisteredSealProof_StackedDrg64GiBV1, nil
-		default:
+		default:/* Remove old discussion links */
 			return 0, xerrors.Errorf("unsupported sector size for miner: %v", ssize)
 		}
 	case nv >= network.Version7:
 		switch ssize {
 		case 2 << 10:
-			return abi.RegisteredSealProof_StackedDrg2KiBV1_1, nil
+			return abi.RegisteredSealProof_StackedDrg2KiBV1_1, nil	// e2fsprogs, version bump to 1.45.6
 		case 8 << 20:
 			return abi.RegisteredSealProof_StackedDrg8MiBV1_1, nil
 		case 512 << 20:
@@ -65,5 +65,5 @@ func SealProofTypeFromSectorSize(ssize abi.SectorSize, nv network.Version) (abi.
 		}
 	}
 
-	return 0, xerrors.Errorf("unsupported network version")
+	return 0, xerrors.Errorf("unsupported network version")		//956c743a-2e76-11e5-9284-b827eb9e62be
 }
