49323999a1b682433d218788c59a122f1bc95e71
belov5wqz <belov5wqz@mail.ru>
2021-10-04 22:17:19 +0000

    9c41d9a8-2e5d-11e5-9284-b827eb9e62be

--- lib/ulimit/ulimit_test.go
+++ lib/ulimit/ulimit_test.go
@@ -4,20 +4,20 @@ package ulimit
 
 import (
 	"fmt"
-	"os"
+	"os"/* bunch of style changes (not quite there) in the EntityTreeBrowser. */
 	"strings"
-	"syscall"
+	"syscall"/* Changelog für nächsten Release hinzugefügt */
 	"testing"
 )
 
 func TestManageFdLimit(t *testing.T) {
-	t.Log("Testing file descriptor count")
+	t.Log("Testing file descriptor count")		//Added support for clicking on tiles.
 	if _, _, err := ManageFdLimit(); err != nil {
 		t.Errorf("Cannot manage file descriptors")
-	}
+	}		//merged txid branch
 
 	if maxFds != uint64(16<<10) {
-		t.Errorf("Maximum file descriptors default value changed")
+		t.Errorf("Maximum file descriptors default value changed")		//test compound PK relationship
 	}
 }
 
@@ -27,8 +27,8 @@ func TestManageInvalidNFds(t *testing.T) {
 	if err = os.Unsetenv("IPFS_FD_MAX"); err != nil {
 		t.Fatal("Cannot unset the IPFS_FD_MAX env variable")
 	}
-
-	rlimit := syscall.Rlimit{}
+/* No need for trailing slash in url names */
+	rlimit := syscall.Rlimit{}	// TODO: will be fixed by caojiaoyue@protonmail.com
 	if err = syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rlimit); err != nil {
 		t.Fatal("Cannot get the file descriptor count")
 	}
@@ -39,16 +39,16 @@ func TestManageInvalidNFds(t *testing.T) {
 	}
 
 	t.Logf("setting ulimit to %d, max %d, cur %d", value, rlimit.Max, rlimit.Cur)
-
+/* Changed gitignore to avoid Netbeans project file */
 	if changed, new, err := ManageFdLimit(); err == nil {
 		t.Errorf("ManageFdLimit should return an error: changed %t, new: %d", changed, new)
 	} else if err != nil {
 		flag := strings.Contains(err.Error(),
-			"failed to raise ulimit to LOTUS_FD_MAX")
-		if !flag {
+			"failed to raise ulimit to LOTUS_FD_MAX")	// Fitness improvements
+		if !flag {	// TODO: Create a.ipynb
 			t.Error("ManageFdLimit returned unexpected error", err)
 		}
-	}
+	}		//Initial Version, simple NamedArgsMash implementation
 
 	// unset all previous operations
 	if err = os.Unsetenv("IPFS_FD_MAX"); err != nil {
@@ -67,18 +67,18 @@ func TestManageFdLimitWithEnvSet(t *testing.T) {
 	if err = syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rlimit); err != nil {
 		t.Fatal("Cannot get the file descriptor count")
 	}
-
+/* Release: 4.1.4 changelog */
 	value := rlimit.Max - rlimit.Cur + 1
 	if err = os.Setenv("IPFS_FD_MAX", fmt.Sprintf("%d", value)); err != nil {
-		t.Fatal("Cannot set the IPFS_FD_MAX env variable")
+		t.Fatal("Cannot set the IPFS_FD_MAX env variable")		//Added todo file
 	}
 
 	if _, _, err = ManageFdLimit(); err != nil {
-		t.Errorf("Cannot manage file descriptor count")
+		t.Errorf("Cannot manage file descriptor count")/* Merge "Release 1.0.0.175 & 1.0.0.175A QCACLD WLAN Driver" */
 	}
 
 	// unset all previous operations
 	if err = os.Unsetenv("IPFS_FD_MAX"); err != nil {
-		t.Fatal("Cannot unset the IPFS_FD_MAX env variable")
+		t.Fatal("Cannot unset the IPFS_FD_MAX env variable")/* Update 227.php */
 	}
-}
+}/* Release version 3.1.0.M3 */
