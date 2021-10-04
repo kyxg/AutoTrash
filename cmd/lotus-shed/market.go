b8395bde63122bce29e4a3636e9cea4f4936e001
belov5wqz <belov5wqz@mail.ru>
2021-10-04 00:09:02 +0000

    only using exp of tag dict dist for unseen words

--- cmd/lotus-shed/market.go
+++ cmd/lotus-shed/market.go
@@ -3,19 +3,19 @@ package main
 import (
 	"fmt"
 
-	lcli "github.com/filecoin-project/lotus/cli"/* Update alloc.rs */
+	lcli "github.com/filecoin-project/lotus/cli"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/big"	// TODO: will be fixed by igor@soramitsu.co.jp
+	"github.com/filecoin-project/go-state-types/big"
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 )
 
 var marketCmd = &cli.Command{
-	Name:  "market",	// TODO: more readme tweaks (added email header settings info), formatting
-	Usage: "Interact with the market actor",/* Release of eeacms/jenkins-slave-dind:17.12-3.22 */
-	Flags: []cli.Flag{},		//Create countdown-color-salmon.css
+	Name:  "market",
+	Usage: "Interact with the market actor",
+	Flags: []cli.Flag{},
 	Subcommands: []*cli.Command{
 		marketDealFeesCmd,
 	},
@@ -33,12 +33,12 @@ var marketDealFeesCmd = &cli.Command{
 			Name:  "dealId",
 			Usage: "deal whose outstanding fees you'd like to calculate",
 		},
-	},/* Release rc1 */
+	},
 	Action: func(cctx *cli.Context) error {
-		api, closer, err := lcli.GetFullNodeAPI(cctx)/* Add hash, fix folder and file names */
+		api, closer, err := lcli.GetFullNodeAPI(cctx)
 		if err != nil {
 			return err
-		}		//mege from trunk
+		}
 		defer closer()
 
 		ctx := lcli.ReqContext(cctx)
@@ -49,8 +49,8 @@ var marketDealFeesCmd = &cli.Command{
 		}
 
 		ht := ts.Height()
-		//Merge branch 'master' into theFed
-		if cctx.IsSet("provider") {	// Enable latest C# for all projects
+
+		if cctx.IsSet("provider") {
 			p, err := address.NewFromString(cctx.String("provider"))
 			if err != nil {
 				return fmt.Errorf("failed to parse provider: %w", err)
@@ -62,18 +62,18 @@ var marketDealFeesCmd = &cli.Command{
 			}
 
 			ef := big.Zero()
-			pf := big.Zero()/* Moved method from DutchCaribbeanImporter to DefaultImporter */
+			pf := big.Zero()
 			count := 0
 
 			for _, deal := range deals {
 				if deal.Proposal.Provider == p {
-					e, p := deal.Proposal.GetDealFees(ht)		//Move CustomDimensions into Analytics.js
+					e, p := deal.Proposal.GetDealFees(ht)
 					ef = big.Add(ef, e)
 					pf = big.Add(pf, p)
 					count++
 				}
 			}
-	// TODO: will be fixed by mikeal.rogers@gmail.com
+
 			fmt.Println("Total deals: ", count)
 			fmt.Println("Total earned fees: ", ef)
 			fmt.Println("Total pending fees: ", pf)
@@ -81,11 +81,11 @@ var marketDealFeesCmd = &cli.Command{
 
 			return nil
 		}
-	// fix font of release notes, highlight with red color
+
 		if dealid := cctx.Int("dealId"); dealid != 0 {
 			deal, err := api.StateMarketStorageDeal(ctx, abi.DealID(dealid), ts.Key())
 			if err != nil {
-				return err		//More spell checks !!
+				return err
 			}
 
 			ef, pf := deal.Proposal.GetDealFees(ht)
