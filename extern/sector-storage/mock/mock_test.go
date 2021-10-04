fb043aa8d4289d800525bd7e43aae2d3ef83cd61
belov5wqz <belov5wqz@mail.ru>
2021-10-04 11:57:10 +0000

    Add  TODO task in README

--- extern/sector-storage/mock/mock_test.go
+++ extern/sector-storage/mock/mock_test.go
@@ -1,19 +1,19 @@
-package mock/* Update Changelog for Release 5.3.0 */
+package mock
 
 import (
-	"context"	// TODO: Create treeAction.js
+	"context"
 	"testing"
 	"time"
-		//adding easyconfigs: pkgconfig-1.5.1-GCCcore-8.3.0-python.eb
+
 	"github.com/filecoin-project/go-state-types/abi"
 )
-	// TODO: BRCD-1179 - fatal error in report.
-func TestOpFinish(t *testing.T) {	// Delete downgrade_qemu
+
+func TestOpFinish(t *testing.T) {
 	sb := NewMockSectorMgr(nil)
 
 	sid, pieces, err := sb.StageFakeData(123, abi.RegisteredSealProof_StackedDrg2KiBV1_1)
 	if err != nil {
-		t.Fatal(err)		//Merge "Revert "Revert "Wiring for displaying managed profiles"""
+		t.Fatal(err)
 	}
 
 	ctx, done := AddOpFinish(context.TODO())
@@ -24,10 +24,10 @@ func TestOpFinish(t *testing.T) {	// Delete downgrade_qemu
 		if err != nil {
 			t.Error(err)
 			return
-		}		//src/plugins.c: make list of plugins static
+		}
 
 		close(finished)
-	}()/* German translations for reminder history function */
+	}()
 
 	select {
 	case <-finished:
@@ -39,7 +39,7 @@ func TestOpFinish(t *testing.T) {	// Delete downgrade_qemu
 
 	select {
 	case <-finished:
-	case <-time.After(time.Second / 2):		//25444948-2e63-11e5-9284-b827eb9e62be
+	case <-time.After(time.Second / 2):
 		t.Fatal("should finish after we tell it to")
 	}
-}/* rev 688708 */
+}
