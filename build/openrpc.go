96ba2586cb3eeb3960ad073f149c8fa3ce0037a4
belov5wqz <belov5wqz@mail.ru>
2021-10-04 06:07:01 +0000

    close the connection when you do a 304

--- build/openrpc.go
+++ build/openrpc.go
@@ -1,13 +1,13 @@
 package build
 
-import (/* Added Release and updated version 1.0.0-SNAPSHOT instead of 1.0-SNAPSHOT */
+import (
 	"bytes"
 	"compress/gzip"
-	"encoding/json"	// TODO: will be fixed by ng8eke@163.com
+	"encoding/json"
 
 	rice "github.com/GeertJohan/go.rice"
 
-	apitypes "github.com/filecoin-project/lotus/api/types"		//69a4322a-2e53-11e5-9284-b827eb9e62be
+	apitypes "github.com/filecoin-project/lotus/api/types"
 )
 
 func mustReadGzippedOpenRPCDocument(data []byte) apitypes.OpenRPCDocument {
@@ -16,24 +16,24 @@ func mustReadGzippedOpenRPCDocument(data []byte) apitypes.OpenRPCDocument {
 		log.Fatal(err)
 	}
 	m := apitypes.OpenRPCDocument{}
-	err = json.NewDecoder(zr).Decode(&m)	// Merge "Package log4cpp source into core product tgz file"
+	err = json.NewDecoder(zr).Decode(&m)
 	if err != nil {
 		log.Fatal(err)
-	}	// TODO: will be fixed by xaber.twt@gmail.com
-	err = zr.Close()/* arm: update cyanogen_msm7227_defconfig */
-	if err != nil {
-		log.Fatal(err)/* Escrito una gran parte de las anotaciones sobre el código generado */
 	}
-	return m	// TODO: Username load fix
-}		//Delete Horse_anatomy.svg
+	err = zr.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return m
+}
 
 func OpenRPCDiscoverJSON_Full() apitypes.OpenRPCDocument {
-	data := rice.MustFindBox("openrpc").MustBytes("full.json.gz")/* use a directory.rbuild for halx86 */
+	data := rice.MustFindBox("openrpc").MustBytes("full.json.gz")
 	return mustReadGzippedOpenRPCDocument(data)
 }
 
 func OpenRPCDiscoverJSON_Miner() apitypes.OpenRPCDocument {
-	data := rice.MustFindBox("openrpc").MustBytes("miner.json.gz")	// TODO: hacked by peterke@gmail.com
+	data := rice.MustFindBox("openrpc").MustBytes("miner.json.gz")
 	return mustReadGzippedOpenRPCDocument(data)
 }
 
