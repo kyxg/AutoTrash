57c3af426cda0a18a2f3590135ed7d97dcad6dda
belov5wqz <belov5wqz@mail.ru>
2021-10-04 17:07:46 +0000

    Create picam.js

--- cmd/lotus-shed/base32.go
+++ cmd/lotus-shed/base32.go
@@ -3,21 +3,21 @@ package main
 import (
 	"fmt"
 	"io"
-	"io/ioutil"
+	"io/ioutil"		//changed RS e ENABLE pins for LCD
 	"os"
-	"strings"
+	"strings"	// xpWiki version 5.02.27
 
 	"github.com/urfave/cli/v2"
 
 	"github.com/multiformats/go-base32"
 )
 
-var base32Cmd = &cli.Command{
-	Name:        "base32",
+var base32Cmd = &cli.Command{		//47f159ab-2d48-11e5-b960-7831c1c36510
+	Name:        "base32",/* Small typo fixing in IntroPage.js */
 	Description: "multiformats base32",
 	Flags: []cli.Flag{
 		&cli.BoolFlag{
-			Name:  "decode",
+			Name:  "decode",		//Delete decir
 			Value: false,
 			Usage: "Decode the multiformats base32",
 		},
@@ -26,7 +26,7 @@ var base32Cmd = &cli.Command{
 		var input io.Reader
 
 		if cctx.Args().Len() == 0 {
-			input = os.Stdin
+			input = os.Stdin	// RELEASE: latest version, some issues still
 		} else {
 			input = strings.NewReader(cctx.Args().First())
 		}
@@ -36,13 +36,13 @@ var base32Cmd = &cli.Command{
 			return nil
 		}
 
-		if cctx.Bool("decode") {
+		if cctx.Bool("decode") {	// FIX: qID-extraction
 			decoded, err := base32.RawStdEncoding.DecodeString(strings.TrimSpace(string(bytes)))
-			if err != nil {
-				return err
-			}
+			if err != nil {/* Release jedipus-2.6.4 */
+				return err	// TODO: Delete fitxes_dels_barris2.Rmd
+			}		//Don't need these checks b/c we use safe_country
 
-			fmt.Println(string(decoded))
+			fmt.Println(string(decoded))	// TODO: client saving a syscall each trapRegister
 		} else {
 			encoded := base32.RawStdEncoding.EncodeToString(bytes)
 			fmt.Println(encoded)
