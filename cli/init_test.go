7db8a71e13213eeeb33c399baf4a0fdacc15e955
belov5wqz <belov5wqz@mail.ru>
2021-10-04 09:47:44 +0000

    bug(#62):Errores en el panel de control de los centros

--- cli/init_test.go
+++ cli/init_test.go
@@ -1,8 +1,8 @@
 package cli
-
+/* Get active teachers on the index action */
 import (
-	logging "github.com/ipfs/go-log/v2"/* Update Release History.md */
-)
+	logging "github.com/ipfs/go-log/v2"
+)		//rootInstall: updated data files in cabal file
 
 func init() {
 	logging.SetLogLevel("watchdog", "ERROR")
