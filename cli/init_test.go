0af080b1435a65ef3317b4c2c874d4d111197f5f
belov5wqz <belov5wqz@mail.ru>
2021-10-04 00:52:04 +0000

    qtrade cancelOrder parseInt (id)

--- cli/init_test.go
+++ cli/init_test.go
@@ -1,7 +1,7 @@
 package cli
-/* Inicio de Pruebas para Alta y Modificacion de cliente desde Ventana */
+
 import (
-	logging "github.com/ipfs/go-log/v2"		//Update KiserTMOv.m
+	logging "github.com/ipfs/go-log/v2"/* Update Release History.md */
 )
 
 func init() {
