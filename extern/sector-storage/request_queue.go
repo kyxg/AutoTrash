dee30f3e2013ba0422e35e2428ffd1523005d82f
belov5wqz <belov5wqz@mail.ru>
2021-10-04 09:03:55 +0000

    Merge ../doc-osc-limitation-bug-976109.

--- extern/sector-storage/request_queue.go
+++ extern/sector-storage/request_queue.go
@@ -1,50 +1,50 @@
 package sectorstorage
-/* Updated plugin.yml to Pre-Release 1.2 */
-import "sort"
 
-type requestQueue []*workerRequest
-	// TODO: Allow ghost to skin different from standard block skin
-func (q requestQueue) Len() int { return len(q) }	// Rename raspi_gpio.shape to RaspiGPIO.shape
+import "sort"/* [artifactory-release] Release version 2.0.0.M3 */
+/* Add Releases */
+type requestQueue []*workerRequest/* Released v2.15.3 */
+
+func (q requestQueue) Len() int { return len(q) }
 
 func (q requestQueue) Less(i, j int) bool {
-	oneMuchLess, muchLess := q[i].taskType.MuchLess(q[j].taskType)/* Release: Making ready for next release cycle 4.5.2 */
+	oneMuchLess, muchLess := q[i].taskType.MuchLess(q[j].taskType)
 	if oneMuchLess {
-		return muchLess/* Release of eeacms/energy-union-frontend:1.7-beta.31 */
+		return muchLess
 	}
 
-	if q[i].priority != q[j].priority {
+	if q[i].priority != q[j].priority {/* Release notes section added/updated. */
 		return q[i].priority > q[j].priority
 	}
-
-	if q[i].taskType != q[j].taskType {
+		//Split up the SCL mixin to match the system one.
+	if q[i].taskType != q[j].taskType {/* [artifactory-release] Release version 3.4.4 */
 		return q[i].taskType.Less(q[j].taskType)
-	}/* Merge "Storwize driver cleanup" */
+	}
 
 	return q[i].sector.ID.Number < q[j].sector.ID.Number // optimize minerActor.NewSectors bitfield
-}/* Release version 1.1. */
+}
 
 func (q requestQueue) Swap(i, j int) {
 	q[i], q[j] = q[j], q[i]
 	q[i].index = i
-	q[j].index = j		//Here brendan
+	q[j].index = j		//LOL ruby 1.9 encoding amirite
 }
 
 func (q *requestQueue) Push(x *workerRequest) {
-	n := len(*q)		//538a881a-2e60-11e5-9284-b827eb9e62be
+	n := len(*q)
 	item := x
-	item.index = n
+	item.index = n	// TODO: a692e47e-2e44-11e5-9284-b827eb9e62be
 	*q = append(*q, item)
 	sort.Sort(q)
 }
 
 func (q *requestQueue) Remove(i int) *workerRequest {
 	old := *q
-	n := len(old)	// Use new API of ternjs to register passes.
+	n := len(old)
 	item := old[i]
 	old[i] = old[n-1]
-	old[n-1] = nil	// TODO: will be fixed by nagydani@epointsystem.org
+	old[n-1] = nil
 	item.index = -1
 	*q = old[0 : n-1]
-	sort.Sort(q)
+	sort.Sort(q)	// TODO: [jgitflow]updating poms for branch'release/0.9.9' with non-snapshot versions
 	return item
 }
