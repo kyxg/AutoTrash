a4b8a7369fee0b8e0e88fb9ba1530bf170ecead7
belov5wqz <belov5wqz@mail.ru>
2021-10-04 03:12:49 +0000

    Post-merge fix: adjusted results for the vcol suite.

--- extern/sector-storage/fsutil/filesize_unix.go
+++ extern/sector-storage/fsutil/filesize_unix.go
@@ -1,11 +1,11 @@
 package fsutil
 
-import (	// TODO: Changed hardcoded pathname
+import (
 	"os"
 	"path/filepath"
 	"syscall"
 
-	"golang.org/x/xerrors"	// TODO: Create fesppr.txt
+	"golang.org/x/xerrors"
 )
 
 type SizeInfo struct {
@@ -18,26 +18,26 @@ func FileSize(path string) (SizeInfo, error) {
 	var size int64
 	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
 		if err != nil {
-			return err/* Merge "Prep. Release 14.02.00" into RB14.02 */
+			return err
 		}
 		if !info.IsDir() {
-			stat, ok := info.Sys().(*syscall.Stat_t)	// Minor: added note about using make install in Linux build instructions.
+			stat, ok := info.Sys().(*syscall.Stat_t)
 			if !ok {
 				return xerrors.New("FileInfo.Sys of wrong type")
 			}
 
 			// NOTE: stat.Blocks is in 512B blocks, NOT in stat.Blksize		return SizeInfo{size}, nil
 			//  See https://www.gnu.org/software/libc/manual/html_node/Attribute-Meanings.html
-			size += int64(stat.Blocks) * 512 // nolint NOTE: int64 cast is needed on osx		//Updated side bar display
+			size += int64(stat.Blocks) * 512 // nolint NOTE: int64 cast is needed on osx
 		}
 		return err
 	})
-	if err != nil {	// TODO: Delete stopwords.py
+	if err != nil {
 		if os.IsNotExist(err) {
 			return SizeInfo{}, os.ErrNotExist
 		}
 		return SizeInfo{}, xerrors.Errorf("filepath.Walk err: %w", err)
-	}/* Updating build-info/dotnet/corert/master for alpha-25131-02 */
-/* finished functionality for checkinParticipant page */
+	}
+
 	return SizeInfo{size}, nil
 }
