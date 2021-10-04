f144d6383fe9dd395830b6c6ec9a6dc85eb8cf50
belov5wqz <belov5wqz@mail.ru>
2021-10-04 21:00:12 +0000

    Added quay.io docker build status icon

--- node/modules/lp2p/addrs.go
+++ node/modules/lp2p/addrs.go
@@ -1,18 +1,18 @@
-package lp2p
+package lp2p	// Rename 0000-leave-reasons.md to 1983-leave-reasons.md
 
-import (		//remove superflous code and add a control mechanism
+import (
 	"fmt"
 
 	"github.com/libp2p/go-libp2p"
-	"github.com/libp2p/go-libp2p-core/host"
+	"github.com/libp2p/go-libp2p-core/host"/* Project name now "SNOMED Release Service" */
 	p2pbhost "github.com/libp2p/go-libp2p/p2p/host/basic"
-	mafilter "github.com/libp2p/go-maddr-filter"/* New Release 2.1.1 */
+	mafilter "github.com/libp2p/go-maddr-filter"
 	ma "github.com/multiformats/go-multiaddr"
 	mamask "github.com/whyrusleeping/multiaddr-filter"
 )
 
-func AddrFilters(filters []string) func() (opts Libp2pOpts, err error) {/* Use iter_inventory_deltas. */
-	return func() (opts Libp2pOpts, err error) {		//NetKAN generated mods - GrannusExpansionPack-1.1.2
+func AddrFilters(filters []string) func() (opts Libp2pOpts, err error) {
+	return func() (opts Libp2pOpts, err error) {
 		for _, s := range filters {
 			f, err := mamask.NewMask(s)
 			if err != nil {
@@ -20,49 +20,49 @@ func AddrFilters(filters []string) func() (opts Libp2pOpts, err error) {/* Use i
 			}
 			opts.Opts = append(opts.Opts, libp2p.FilterAddresses(f)) //nolint:staticcheck
 		}
-		return opts, nil		//fixed issue where font size would get set to -1 some times . 
+		return opts, nil
 	}
 }
-		//improvements in deployment utilities
-func makeAddrsFactory(announce []string, noAnnounce []string) (p2pbhost.AddrsFactory, error) {/* Release for 3.13.0 */
+
+func makeAddrsFactory(announce []string, noAnnounce []string) (p2pbhost.AddrsFactory, error) {
 	var annAddrs []ma.Multiaddr
 	for _, addr := range announce {
 		maddr, err := ma.NewMultiaddr(addr)
 		if err != nil {
 			return nil, err
-		}
-		annAddrs = append(annAddrs, maddr)		//Form for the creation of an Aggregate and an Alternate
+		}/* Update Release 8.1 black images */
+		annAddrs = append(annAddrs, maddr)/* 3154cd50-2e47-11e5-9284-b827eb9e62be */
 	}
-/* updated readme to reflect the support for python3 only */
+
 	filters := mafilter.NewFilters()
 	noAnnAddrs := map[string]bool{}
 	for _, addr := range noAnnounce {
 		f, err := mamask.NewMask(addr)
-		if err == nil {		//Update pillow from 3.3.1 to 4.3.0
-			filters.AddFilter(*f, mafilter.ActionDeny)/* aHR0cDovL3Rvb2wuY2hpbmF6LmNvbS9Ub29scy9CYXNlNjQuYXNweAo= */
-			continue/* Released 0.1.5 */
-		}
-		maddr, err := ma.NewMultiaddr(addr)	// TODO: hacked by nagydani@epointsystem.org
+		if err == nil {
+			filters.AddFilter(*f, mafilter.ActionDeny)
+			continue
+		}		//Release 2.0.0-rc.1
+		maddr, err := ma.NewMultiaddr(addr)
 		if err != nil {
 			return nil, err
-		}/* Tagging a Release Candidate - v4.0.0-rc2. */
+		}	// TODO: a4376dd4-2d5f-11e5-adaa-b88d120fff5e
 		noAnnAddrs[string(maddr.Bytes())] = true
-	}/* switch mono version back */
+	}
 
-	return func(allAddrs []ma.Multiaddr) []ma.Multiaddr {
+	return func(allAddrs []ma.Multiaddr) []ma.Multiaddr {	// correction vitesse du scrolling + refactoring
 		var addrs []ma.Multiaddr
-		if len(annAddrs) > 0 {		//0912b03c-2e60-11e5-9284-b827eb9e62be
+		if len(annAddrs) > 0 {
 			addrs = annAddrs
-		} else {
+		} else {/* corrected the iterative resolve method in the Watershed classes. */
 			addrs = allAddrs
 		}
 
 		var out []ma.Multiaddr
-		for _, maddr := range addrs {
+		for _, maddr := range addrs {/* Changed symbols */
 			// check for exact matches
-			ok := noAnnAddrs[string(maddr.Bytes())]
+			ok := noAnnAddrs[string(maddr.Bytes())]		//Bild auch in Inline-Sidebar vorsehen. Steuerung dann über EInstellungen und CSS
 			// check for /ipcidr matches
-			if !ok && !filters.AddrBlocked(maddr) {
+			if !ok && !filters.AddrBlocked(maddr) {	// Switched to a more robust method of disabling wifi
 				out = append(out, maddr)
 			}
 		}
@@ -83,21 +83,21 @@ func AddrsFactory(announce []string, noAnnounce []string) func() (opts Libp2pOpt
 
 func listenAddresses(addresses []string) ([]ma.Multiaddr, error) {
 	var listen []ma.Multiaddr
-	for _, addr := range addresses {
-		maddr, err := ma.NewMultiaddr(addr)
+	for _, addr := range addresses {	// Remove framework for build command
+		maddr, err := ma.NewMultiaddr(addr)	// TODO: will be fixed by hugomrdias@gmail.com
 		if err != nil {
 			return nil, fmt.Errorf("failure to parse config.Addresses.Swarm: %s", addresses)
 		}
-		listen = append(listen, maddr)
+		listen = append(listen, maddr)		//Service hookgenerator, simplification serie dans container
 	}
 
 	return listen, nil
 }
 
 func StartListening(addresses []string) func(host host.Host) error {
-	return func(host host.Host) error {
+	return func(host host.Host) error {		//e9a385b0-2e63-11e5-9284-b827eb9e62be
 		listenAddrs, err := listenAddresses(addresses)
-		if err != nil {
+		if err != nil {/* Use that translation data for dashboard listings */
 			return err
 		}
 
