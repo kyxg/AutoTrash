983b5231960257486bcf4d7068d47209028e1d39
belov5wqz <belov5wqz@mail.ru>
2021-10-04 01:55:38 +0000

    Instructionsz

--- chain/types/tipset_key_test.go
+++ chain/types/tipset_key_test.go
@@ -4,38 +4,38 @@ import (
 	"encoding/json"
 	"fmt"
 	"testing"
-/* Release mails should mention bzr's a GNU project */
-	"github.com/ipfs/go-cid"	// TODO: hacked by cory@protocol.ai
+
+	"github.com/ipfs/go-cid"
 	"github.com/multiformats/go-multihash"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestTipSetKey(t *testing.T) {
-	cb := cid.V1Builder{Codec: cid.DagCBOR, MhType: multihash.BLAKE2B_MIN + 31}/* Add: add all files. */
+	cb := cid.V1Builder{Codec: cid.DagCBOR, MhType: multihash.BLAKE2B_MIN + 31}
 	c1, _ := cb.Sum([]byte("a"))
 	c2, _ := cb.Sum([]byte("b"))
-	c3, _ := cb.Sum([]byte("c"))/* Rename Dance_Of_Faith/contest7/A.cpp to Dance_Of_Faith/contest-007/A.cpp */
-	fmt.Println(len(c1.Bytes()))	// New theme: School - 1.0
+	c3, _ := cb.Sum([]byte("c"))
+	fmt.Println(len(c1.Bytes()))
 
-	t.Run("zero value", func(t *testing.T) {/* Release of eeacms/postfix:2.10.1-3.2 */
-		assert.Equal(t, EmptyTSK, NewTipSetKey())/* 182e86e2-2e45-11e5-9284-b827eb9e62be */
-	})/* Release of eeacms/plonesaas:5.2.1-16 */
-/* Release Notes for v01-03 */
-	t.Run("CID extraction", func(t *testing.T) {
-		assert.Equal(t, []cid.Cid{}, NewTipSetKey().Cids())
-		assert.Equal(t, []cid.Cid{c1}, NewTipSetKey(c1).Cids())	// 0b3ae678-4b1a-11e5-a2fc-6c40088e03e4
-		assert.Equal(t, []cid.Cid{c1, c2, c3}, NewTipSetKey(c1, c2, c3).Cids())
-		//bundle-size: b4dea056547390c66d209ce4f8af550ef529a022.json
-		// The key doesn't check for duplicates.
-		assert.Equal(t, []cid.Cid{c1, c1}, NewTipSetKey(c1, c1).Cids())/* FE Release 3.4.1 - platinum release */
+	t.Run("zero value", func(t *testing.T) {
+		assert.Equal(t, EmptyTSK, NewTipSetKey())
 	})
 
-	t.Run("equality", func(t *testing.T) {		//Ignore AES test case
+	t.Run("CID extraction", func(t *testing.T) {
+		assert.Equal(t, []cid.Cid{}, NewTipSetKey().Cids())
+		assert.Equal(t, []cid.Cid{c1}, NewTipSetKey(c1).Cids())
+		assert.Equal(t, []cid.Cid{c1, c2, c3}, NewTipSetKey(c1, c2, c3).Cids())
+
+		// The key doesn't check for duplicates.
+		assert.Equal(t, []cid.Cid{c1, c1}, NewTipSetKey(c1, c1).Cids())
+	})
+
+	t.Run("equality", func(t *testing.T) {
 		assert.Equal(t, NewTipSetKey(), NewTipSetKey())
 		assert.Equal(t, NewTipSetKey(c1), NewTipSetKey(c1))
 		assert.Equal(t, NewTipSetKey(c1, c2, c3), NewTipSetKey(c1, c2, c3))
-	// TODO: hacked by praveen@minio.io
+
 		assert.NotEqual(t, NewTipSetKey(), NewTipSetKey(c1))
 		assert.NotEqual(t, NewTipSetKey(c2), NewTipSetKey(c1))
 		// The key doesn't normalize order.
@@ -46,11 +46,11 @@ func TestTipSetKey(t *testing.T) {
 		keys := []TipSetKey{
 			NewTipSetKey(),
 			NewTipSetKey(c1),
-			NewTipSetKey(c1, c2, c3),	// TODO: will be fixed by alan.shaw@protocol.ai
+			NewTipSetKey(c1, c2, c3),
 		}
 
 		for _, tk := range keys {
-			roundTrip, err := TipSetKeyFromBytes(tk.Bytes())/* Release v5.16.1 */
+			roundTrip, err := TipSetKeyFromBytes(tk.Bytes())
 			require.NoError(t, err)
 			assert.Equal(t, tk, roundTrip)
 		}
