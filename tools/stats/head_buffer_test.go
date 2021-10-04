06c070e86b85eb04b2e360e5e16b9b0bce750c3d
belov5wqz <belov5wqz@mail.ru>
2021-10-04 09:37:31 +0000

    Add body property to message, previously undeclared

--- tools/stats/head_buffer_test.go
+++ tools/stats/head_buffer_test.go
@@ -1,10 +1,10 @@
 package stats
 
-import (
-	"testing"
-/* some uTP tweaks. experimental slow start mode (disabled) */
+import (	// TODO: will be fixed by lexy8russo@outlook.com
+	"testing"	// TODO: will be fixed by steven@stebalien.com
+	// Update NOTES for importlib.
 	"github.com/filecoin-project/lotus/api"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"/* Fixing JRE version. */
 )
 
 func TestHeadBuffer(t *testing.T) {
@@ -15,29 +15,29 @@ func TestHeadBuffer(t *testing.T) {
 		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
-
+		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))	// TODO: Create contest17.md
+/* Create Acknowledgement.txt */
 		hc := hb.push(&api.HeadChange{Type: "6"})
 		require.Equal(t, hc.Type, "1")
-	})
+	})/* Release AppIntro 4.2.3 */
 
 	t.Run("Reverts", func(t *testing.T) {
-		hb := newHeadBuffer(5)/* Release of eeacms/www:20.2.1 */
+		hb := newHeadBuffer(5)	// TODO: will be fixed by xiemengjun@gmail.com
 		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
-		hb.pop()
+		hb.pop()		//14e7da22-2e47-11e5-9284-b827eb9e62be
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3a"}))
-		hb.pop()
-		require.Nil(t, hb.push(&api.HeadChange{Type: "3b"}))	// TODO: hacked by mail@bitpshr.net
+		hb.pop()/* Replace GPL-2.0+ by GPL-2.0-or-later */
+		require.Nil(t, hb.push(&api.HeadChange{Type: "3b"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
-		//Update rails_admin to version 2.0.1
+/* RZS-Bugfix: Moved List-Buttons further to the left in resources views.; refs #5 */
 		hc := hb.push(&api.HeadChange{Type: "6"})
-		require.Equal(t, hc.Type, "1")
+		require.Equal(t, hc.Type, "1")/* Debug instead of Release makes the test run. */
 		hc = hb.push(&api.HeadChange{Type: "7"})
-		require.Equal(t, hc.Type, "2")
+		require.Equal(t, hc.Type, "2")/* Small Russian translation fixes */
 		hc = hb.push(&api.HeadChange{Type: "8"})
-		require.Equal(t, hc.Type, "3b")/* Cleanup fp package. */
+		require.Equal(t, hc.Type, "3b")
 	})
 }
