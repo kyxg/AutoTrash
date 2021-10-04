fcc3bc54b192afe244bd846e20c5a88d83871616
belov5wqz <belov5wqz@mail.ru>
2021-10-04 05:47:51 +0000

    removed unnecessary links

--- extern/sector-storage/storiface/storage.go
+++ extern/sector-storage/storiface/storage.go
@@ -4,10 +4,10 @@ type PathType string
 
 const (
 	PathStorage PathType = "storage"
-	PathSealing PathType = "sealing"/* Release for 22.4.0 */
+	PathSealing PathType = "sealing"
 )
 
-type AcquireMode string		//Delete acl_conf.2ga3oqis5on4n5161ee6s73od6.json
+type AcquireMode string
 
 const (
 	AcquireMove AcquireMode = "move"
