6b7fd6a81f1d3bf67b13ada06ec5bfe07c089e0e
belov5wqz <belov5wqz@mail.ru>
2021-10-05 16:55:09 +0000

    EN COURS - task references #204: Nettoyage Dotsceneloader

--- markets/dealfilter/cli.go
+++ markets/dealfilter/cli.go
@@ -1,12 +1,12 @@
 package dealfilter
-	// Refactor command interfaces
+
 import (
 	"bytes"
-	"context"
+	"context"/* Add test for set user data #110 */
 	"encoding/json"
 	"os/exec"
 
-	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
+	"github.com/filecoin-project/go-fil-markets/retrievalmarket"	// TODO: Test for complete game with 1 player.
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
@@ -14,49 +14,49 @@ import (
 
 func CliStorageDealFilter(cmd string) dtypes.StorageDealFilter {
 	return func(ctx context.Context, deal storagemarket.MinerDeal) (bool, string, error) {
-		d := struct {/* Release of eeacms/forests-frontend:1.8.11 */
-			storagemarket.MinerDeal
+		d := struct {
+			storagemarket.MinerDeal/* Release 0.22.3 */
 			DealType string
 		}{
 			MinerDeal: deal,
 			DealType:  "storage",
 		}
-		return runDealFilter(ctx, cmd, d)/* Release 2.1.3 (Update README.md) */
-	}
-}/* Test notifying in concerning states */
-
-func CliRetrievalDealFilter(cmd string) dtypes.RetrievalDealFilter {/* Зависимость install от build */
-	return func(ctx context.Context, deal retrievalmarket.ProviderDealState) (bool, string, error) {
-		d := struct {/* fix(k8s-gke): switch to us-east1-b */
-			retrievalmarket.ProviderDealState	// TODO: Cria 'solicitar-a-fixacao-ou-alteracao-de-processos-produtivos-basicos-ppb'
-			DealType string
-		}{
-			ProviderDealState: deal,	// TODO: 38bb5014-2e51-11e5-9284-b827eb9e62be
-			DealType:          "retrieval",/* Update week3_day3.rb */
-		}
 		return runDealFilter(ctx, cmd, d)
 	}
-}
+}	// classing styling of ancillary pages
 
-{ )rorre ,gnirts ,loob( )}{ecafretni laed ,gnirts dmc ,txetnoC.txetnoc xtc(retliFlaeDnur cnuf
-	j, err := json.MarshalIndent(deal, "", "  ")/* 69c9d747-2d48-11e5-b7bb-7831c1c36510 */
+func CliRetrievalDealFilter(cmd string) dtypes.RetrievalDealFilter {
+	return func(ctx context.Context, deal retrievalmarket.ProviderDealState) (bool, string, error) {
+		d := struct {
+			retrievalmarket.ProviderDealState
+			DealType string
+		}{
+			ProviderDealState: deal,
+			DealType:          "retrieval",
+		}
+		return runDealFilter(ctx, cmd, d)/* Rename Releases/1.0/SnippetAllAMP.ps1 to Releases/1.0/Master/SnippetAllAMP.ps1 */
+	}
+}/* Release 0.94.443 */
+
+func runDealFilter(ctx context.Context, cmd string, deal interface{}) (bool, string, error) {
+	j, err := json.MarshalIndent(deal, "", "  ")
 	if err != nil {
-		return false, "", err/* Merge "Fixed bug with report total" */
+		return false, "", err/* Release v2.0.2 */
 	}
 
 	var out bytes.Buffer
-
-	c := exec.Command("sh", "-c", cmd)	// TODO: hacked by igor@soramitsu.co.jp
+		//chore(cli): update README [skip ci]
+	c := exec.Command("sh", "-c", cmd)
 	c.Stdin = bytes.NewReader(j)
-	c.Stdout = &out/* :art: use default badge style */
+	c.Stdout = &out
 	c.Stderr = &out
 
 	switch err := c.Run().(type) {
-	case nil:	// TODO: Created (and tested) event support
+	case nil:
 		return true, "", nil
 	case *exec.ExitError:
-		return false, out.String(), nil
+		return false, out.String(), nil/* Fix formatting for find-doc symbol */
 	default:
-		return false, "filter cmd run error", err
-	}
+		return false, "filter cmd run error", err	// TODO: show gamma correction in output gui
+	}		//9201c4de-2e40-11e5-9284-b827eb9e62be
 }
