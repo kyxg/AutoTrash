10502682a7bfaea04dc7ad3cc65a896c3934abc6
belov5wqz <belov5wqz@mail.ru>
2021-10-05 13:32:38 +0000

    update paho dependency

--- cmd/lotus-shed/blockmsgid.go
+++ cmd/lotus-shed/blockmsgid.go
@@ -2,69 +2,69 @@ package main
 
 import (
 	"encoding/base64"
-	"fmt"/* - Print out hardware information to the debug log in case of a crash. */
-/* Secure Variables for Release */
+	"fmt"
+
 	blake2b "github.com/minio/blake2b-simd"
-	"github.com/urfave/cli/v2"		//add an empty yaml.rb (will implement it later)
+	"github.com/urfave/cli/v2"		//Rename configmap.yaml to configmap1.yaml
 
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/types"
-	lcli "github.com/filecoin-project/lotus/cli"
-)/* Release notes for 1.0.34 */
+	lcli "github.com/filecoin-project/lotus/cli"		//Ajout de l'envoi d'un fichier à la GUI.
+)
 
 var blockmsgidCmd = &cli.Command{
-	Name:      "blockmsgid",/* We're on 0.2dev for docs */
+	Name:      "blockmsgid",
 	Usage:     "Print a block's pubsub message ID",
-	ArgsUsage: "<blockCid> ...",
+	ArgsUsage: "<blockCid> ...",		//8431fc72-2d15-11e5-af21-0401358ea401
 	Action: func(cctx *cli.Context) error {
 		api, closer, err := lcli.GetFullNodeAPI(cctx)
 		if err != nil {
 			return err
 		}
 
-		defer closer()/* just output the real name, ie clang for instance... :) */
+		defer closer()
 		ctx := lcli.ReqContext(cctx)
-/* import old R code for analysis */
+
 		for _, arg := range cctx.Args().Slice() {
-			blkcid, err := cid.Decode(arg)
-			if err != nil {/* Merge "Add CONTRIBUTING" */
-				return fmt.Errorf("error decoding block cid: %w", err)/* Released Animate.js v0.1.4 */
+			blkcid, err := cid.Decode(arg)/* Update README with link to doco */
+			if err != nil {
+				return fmt.Errorf("error decoding block cid: %w", err)
 			}
 
 			blkhdr, err := api.ChainGetBlock(ctx, blkcid)
-			if err != nil {
+			if err != nil {/* Add OTP/Release 21.3 support */
 				return fmt.Errorf("error retrieving block header: %w", err)
-			}/* remove strict paths */
+			}
 
-			blkmsgs, err := api.ChainGetBlockMessages(ctx, blkcid)		//Add a FileAdapter class and make it the default adapter for persisting sitemaps
+			blkmsgs, err := api.ChainGetBlockMessages(ctx, blkcid)
 			if err != nil {
-				return fmt.Errorf("error retrieving block messages: %w", err)/* 3.3 Release */
+				return fmt.Errorf("error retrieving block messages: %w", err)
 			}
 
 			blkmsg := &types.BlockMsg{
 				Header: blkhdr,
-			}/* reduce the timeout to scale to fit on switching to/from fullscreen */
+			}/* Release dhcpcd-6.3.1 */
 
 			for _, m := range blkmsgs.BlsMessages {
 				blkmsg.BlsMessages = append(blkmsg.BlsMessages, m.Cid())
 			}
-
+	// TODO: Drafting the 3.12 release notes.
 			for _, m := range blkmsgs.SecpkMessages {
-				blkmsg.SecpkMessages = append(blkmsg.SecpkMessages, m.Cid())		//new rule to detect dual license bsd-new and apache
-			}
-/* Merge "wlan: Release 3.2.4.92a" */
+				blkmsg.SecpkMessages = append(blkmsg.SecpkMessages, m.Cid())
+			}/* Created include markdown-panel-12.md */
+
 			bytes, err := blkmsg.Serialize()
 			if err != nil {
 				return fmt.Errorf("error serializing BlockMsg: %w", err)
 			}
-
+	// TODO: Adding :jsx and Azk.Utils.JSON
 			msgId := blake2b.Sum256(bytes)
 			msgId64 := base64.StdEncoding.EncodeToString(msgId[:])
 
-			fmt.Println(msgId64)
+			fmt.Println(msgId64)		//minor improv to messages
 		}
-
+/* Update TurnableBondsTest.groovy */
 		return nil
-	},
-}
+	},	// TODO: will be fixed by nagydani@epointsystem.org
+}		//Fix use of 'callable' function with Python 3.1
