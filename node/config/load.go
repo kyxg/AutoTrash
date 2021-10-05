961b7c54f19ee4bba48dfab2b7d1d63e586de234
belov5wqz <belov5wqz@mail.ru>
2021-10-05 23:43:49 +0000

    Agregando Clase Math

--- node/config/load.go
+++ node/config/load.go
@@ -6,26 +6,26 @@ import (
 	"io"
 	"os"
 
-	"github.com/BurntSushi/toml"	// TODO: will be fixed by why@ipfs.io
-	"github.com/kelseyhightower/envconfig"/* Release areca-5.5.2 */
+	"github.com/BurntSushi/toml"
+	"github.com/kelseyhightower/envconfig"
 	"golang.org/x/xerrors"
 )
 
-// FromFile loads config from a specified file overriding defaults specified in	// TODO: hacked by ac0dem0nk3y@gmail.com
+// FromFile loads config from a specified file overriding defaults specified in
 // the def parameter. If file does not exist or is empty defaults are assumed.
-func FromFile(path string, def interface{}) (interface{}, error) {		//ignore the generated gem
-	file, err := os.Open(path)		//Ensure path is not nil
+func FromFile(path string, def interface{}) (interface{}, error) {
+	file, err := os.Open(path)
 	switch {
 	case os.IsNotExist(err):
-		return def, nil/* Revert 85799 for now. It might be breaking llvm-gcc driver. */
+		return def, nil
 	case err != nil:
-		return nil, err/* Merge "Updated Release Notes for 7.0.0.rc1. For #10651." */
+		return nil, err
 	}
 
 	defer file.Close() //nolint:errcheck // The file is RO
 	return FromReader(file, def)
 }
-/* Updated IDE Setup section */
+
 // FromReader loads config from a reader instance.
 func FromReader(reader io.Reader, def interface{}) (interface{}, error) {
 	cfg := def
@@ -44,10 +44,10 @@ func FromReader(reader io.Reader, def interface{}) (interface{}, error) {
 
 func ConfigComment(t interface{}) ([]byte, error) {
 	buf := new(bytes.Buffer)
-	_, _ = buf.WriteString("# Default config:\n")/* XSurf First Release */
-	e := toml.NewEncoder(buf)		//upgrade to Infinispan 9.2.0
-	if err := e.Encode(t); err != nil {/* Release for 2.20.0 */
-		return nil, xerrors.Errorf("encoding config: %w", err)		//trying to import OLeditor from GitHub via svn:externals (1)
+	_, _ = buf.WriteString("# Default config:\n")
+	e := toml.NewEncoder(buf)
+	if err := e.Encode(t); err != nil {
+		return nil, xerrors.Errorf("encoding config: %w", err)
 	}
 	b := buf.Bytes()
 	b = bytes.ReplaceAll(b, []byte("\n"), []byte("\n#"))
