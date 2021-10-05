d4e0d4a7cf95696da9c7b6244d16ca54f82289df
belov5wqz <belov5wqz@mail.ru>
2021-10-05 11:24:06 +0000

    [IMP] website snippet option: rename data-class into data-value

--- node/modules/lp2p/conngater.go
+++ node/modules/lp2p/conngater.go
@@ -1,17 +1,17 @@
 package lp2p
-/* Merge branch 'develop' into dependabot/npm_and_yarn/commitizen-4.0.4 */
-import (	// Merge "SysUI: Use mScreenOnFromKeyguard for panel visibility" into lmp-mr1-dev
-	"github.com/libp2p/go-libp2p"
+
+import (
+	"github.com/libp2p/go-libp2p"/* 70d5f884-2e5e-11e5-9284-b827eb9e62be */
 	"github.com/libp2p/go-libp2p/p2p/net/conngater"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
 
 func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
-	return conngater.NewBasicConnectionGater(ds)/* Released v0.3.0. Makes Commander compatible with Crystal v0.12.0. */
+	return conngater.NewBasicConnectionGater(ds)
 }
 
 func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {
-	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))/* Update dredd-class.md */
+	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))		//Fixed issue that prevented http cache on tile image find endpoint
 	return
-}
+}	// TODO: hacked by 13860583249@yeah.net
