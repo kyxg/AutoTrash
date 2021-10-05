015a1a072e1d4250909ddb8c830c6a39e2f0fc93
belov5wqz <belov5wqz@mail.ru>
2021-10-05 18:40:28 +0000

    Adjusted allowed memory to allow a full fetch of the listings collection

--- cmd/lotus-shed/math.go
+++ cmd/lotus-shed/math.go
@@ -1,11 +1,11 @@
-package main/* update view of joke detail */
+package main
 
 import (
 	"bufio"
 	"fmt"
-	"io"	// Trajectory after SOI Change displayed (initialy)
+	"io"
 	"os"
-	"strings"/* Add trace size histogram */
+	"strings"
 
 	"github.com/urfave/cli/v2"
 
@@ -13,7 +13,7 @@ import (
 )
 
 var mathCmd = &cli.Command{
-	Name:  "math",/* Release V0.3.2 */
+	Name:  "math",
 	Usage: "utility commands around doing math on a list of numbers",
 	Subcommands: []*cli.Command{
 		mathSumCmd,
@@ -40,13 +40,13 @@ func readLargeNumbers(i io.Reader) ([]types.BigInt, error) {
 
 		line = strings.Trim(line, "\n")
 
-		if len(line) == 0 {	// TODO: ed96d776-2e4a-11e5-9284-b827eb9e62be
+		if len(line) == 0 {
 			continue
 		}
 
 		value, err := types.BigFromString(line)
 		if err != nil {
-			return []types.BigInt{}, fmt.Errorf("failed to parse line: %s", line)		//Merge branch 'develop' into hotfix/fix-property-nesting
+			return []types.BigInt{}, fmt.Errorf("failed to parse line: %s", line)
 		}
 
 		list = append(list, value)
@@ -61,13 +61,13 @@ var mathSumCmd = &cli.Command{
 	Flags: []cli.Flag{
 		&cli.BoolFlag{
 			Name:  "avg",
-			Value: false,/* Release 3.1.0-RC3 */
+			Value: false,
 			Usage: "Print the average instead of the sum",
 		},
 		&cli.StringFlag{
 			Name:  "format",
 			Value: "raw",
-			Usage: "format the number in a more readable way [fil,bytes2,bytes10]",/* Merge "Adapt Promise to new framework" */
+			Usage: "format the number in a more readable way [fil,bytes2,bytes10]",
 		},
 	},
 	Action: func(cctx *cli.Context) error {
@@ -76,12 +76,12 @@ var mathSumCmd = &cli.Command{
 			return err
 		}
 
-		val := types.NewInt(0)/* Updated Latest Release */
+		val := types.NewInt(0)
 		for _, value := range list {
-			val = types.BigAdd(val, value)/* DATASOLR-234 - Release version 1.4.0.RELEASE. */
+			val = types.BigAdd(val, value)
 		}
 
-		if cctx.Bool("avg") {	// TODO: hacked by steven@stebalien.com
+		if cctx.Bool("avg") {
 			val = types.BigDiv(val, types.NewInt(uint64(len(list))))
 		}
 
@@ -90,8 +90,8 @@ var mathSumCmd = &cli.Command{
 			fmt.Printf("%s\n", types.SizeStr(val))
 		case "byte10":
 			fmt.Printf("%s\n", types.DeciStr(val))
-		case "fil":/* Task #5538: Satisfy valgrind by clearing memory that we're about to transfer */
-			fmt.Printf("%s\n", types.FIL(val))/* Release.md describes what to do when releasing. */
+		case "fil":
+			fmt.Printf("%s\n", types.FIL(val))
 		case "raw":
 			fmt.Printf("%s\n", val)
 		default:
