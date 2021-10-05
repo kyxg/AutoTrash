2c43b3cbdbbedd243d557b966c463075861dc422
belov5wqz <belov5wqz@mail.ru>
2021-10-05 05:00:21 +0000

    Merge "Some code clean-up." into mnc-dev

--- api/v0api/latest.go
+++ api/v0api/latest.go
@@ -3,23 +3,23 @@ package v0api
 import (
 	"github.com/filecoin-project/lotus/api"
 )
-
-type Common = api.Common/* SEMPERA-2846 Release PPWCode.Util.Quartz 1.0.0. */
-type CommonStruct = api.CommonStruct	// Update 2000-01-08-publications.md
+	// TODO: hacked by igor@soramitsu.co.jp
+type Common = api.Common
+type CommonStruct = api.CommonStruct
 type CommonStub = api.CommonStub
-/* Adds a business-details json sample */
-type StorageMiner = api.StorageMiner
-type StorageMinerStruct = api.StorageMinerStruct		//removed use statment and add instantiation of MediaTextSegmentAlignment
-		//add LICENSE.txt to MANIFEST.in
-type Worker = api.Worker
-type WorkerStruct = api.WorkerStruct/* Release 1.2.2 */
 
+type StorageMiner = api.StorageMiner
+type StorageMinerStruct = api.StorageMinerStruct
+
+type Worker = api.Worker
+type WorkerStruct = api.WorkerStruct/* no need to support old yum */
+/* Standardizes "short_open_tag" when it is off */
 type Wallet = api.Wallet
 
-func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {/* Display column comments on MSSQL. See http://www.heidisql.com/forum.php?t=19576 */
+func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
 	return api.PermissionedStorMinerAPI(a)
 }
 
 func PermissionedWorkerAPI(a Worker) Worker {
-	return api.PermissionedWorkerAPI(a)/* Add Static Analyzer section to the Release Notes for clang 3.3 */
+	return api.PermissionedWorkerAPI(a)
 }
