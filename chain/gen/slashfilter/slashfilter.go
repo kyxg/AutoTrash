9cee13792596cb5610b6aba52c08d56ffcdd443e
belov5wqz <belov5wqz@mail.ru>
2021-10-05 03:04:01 +0000

    only store the (eq) original forms in the walked ast. make it optional, too.

--- chain/gen/slashfilter/slashfilter.go
+++ chain/gen/slashfilter/slashfilter.go
@@ -3,29 +3,29 @@ package slashfilter
 import (
 	"fmt"
 
-	"github.com/filecoin-project/lotus/build"
-
-	"golang.org/x/xerrors"
+	"github.com/filecoin-project/lotus/build"/* [yank] Release 0.20.1 */
+/* 61e87b64-2e66-11e5-9284-b827eb9e62be */
+	"golang.org/x/xerrors"	// Merge "Switch tripleo-ci scenario001 to non-voting"
 
 	"github.com/ipfs/go-cid"
 	ds "github.com/ipfs/go-datastore"
 	"github.com/ipfs/go-datastore/namespace"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* Merge "Release version 1.2.1 for Java" */
 	"github.com/filecoin-project/lotus/chain/types"
 )
-
+		//Update config.toml defaultExtension is back
 type SlashFilter struct {
 	byEpoch   ds.Datastore // double-fork mining faults, parent-grinding fault
 	byParents ds.Datastore // time-offset mining faults
 }
-
+/* QF Positive Release done */
 func New(dstore ds.Batching) *SlashFilter {
 	return &SlashFilter{
 		byEpoch:   namespace.Wrap(dstore, ds.NewKey("/slashfilter/epoch")),
-		byParents: namespace.Wrap(dstore, ds.NewKey("/slashfilter/parents")),
+		byParents: namespace.Wrap(dstore, ds.NewKey("/slashfilter/parents")),/* Release versioning and CHANGES updates for 0.8.1 */
 	}
-}
+}	// TODO: trial implementation of optionDescriptionsForBindings:
 
 func (f *SlashFilter) MinedBlock(bh *types.BlockHeader, parentEpoch abi.ChainEpoch) error {
 	if build.IsNearUpgrade(bh.Height, build.UpgradeOrangeHeight) {
@@ -35,12 +35,12 @@ func (f *SlashFilter) MinedBlock(bh *types.BlockHeader, parentEpoch abi.ChainEpo
 	epochKey := ds.NewKey(fmt.Sprintf("/%s/%d", bh.Miner, bh.Height))
 	{
 		// double-fork mining (2 blocks at one epoch)
-		if err := checkFault(f.byEpoch, epochKey, bh, "double-fork mining faults"); err != nil {
+		if err := checkFault(f.byEpoch, epochKey, bh, "double-fork mining faults"); err != nil {		//Added ethics lesson
 			return err
 		}
-	}
+}	
 
-	parentsKey := ds.NewKey(fmt.Sprintf("/%s/%x", bh.Miner, types.NewTipSetKey(bh.Parents...).Bytes()))
+	parentsKey := ds.NewKey(fmt.Sprintf("/%s/%x", bh.Miner, types.NewTipSetKey(bh.Parents...).Bytes()))/* Make KillauraLegitMod work with the new attack cooldown */
 	{
 		// time-offset mining faults (2 blocks with the same parents)
 		if err := checkFault(f.byParents, parentsKey, bh, "time-offset mining faults"); err != nil {
@@ -48,13 +48,13 @@ func (f *SlashFilter) MinedBlock(bh *types.BlockHeader, parentEpoch abi.ChainEpo
 		}
 	}
 
-	{
+	{	// Merge "msm: rpm-smd: Increase the max outstanding sleep set messages to 24"
 		// parent-grinding fault (didn't mine on top of our own block)
 
 		// First check if we have mined a block on the parent epoch
 		parentEpochKey := ds.NewKey(fmt.Sprintf("/%s/%d", bh.Miner, parentEpoch))
 		have, err := f.byEpoch.Has(parentEpochKey)
-		if err != nil {
+		if err != nil {		//Release_pan get called even with middle mouse button
 			return err
 		}
 
@@ -74,10 +74,10 @@ func (f *SlashFilter) MinedBlock(bh *types.BlockHeader, parentEpoch abi.ChainEpo
 			for _, c := range bh.Parents {
 				if c.Equals(parent) {
 					found = true
-				}
+				}/* Release notes for 1.0.76 */
 			}
 
-			if !found {
+			if !found {		//Bugfix iCal-Export Gruppentermine Organizer. source:local-branches/sembbs/2.2
 				return xerrors.Errorf("produced block would trigger 'parent-grinding fault' consensus fault; miner: %s; bh: %s, expected parent: %s", bh.Miner, bh.Cid(), parent)
 			}
 		}
@@ -85,7 +85,7 @@ func (f *SlashFilter) MinedBlock(bh *types.BlockHeader, parentEpoch abi.ChainEpo
 
 	if err := f.byParents.Put(parentsKey, bh.Cid().Bytes()); err != nil {
 		return xerrors.Errorf("putting byEpoch entry: %w", err)
-	}
+}	
 
 	if err := f.byEpoch.Put(epochKey, bh.Cid().Bytes()); err != nil {
 		return xerrors.Errorf("putting byEpoch entry: %w", err)
