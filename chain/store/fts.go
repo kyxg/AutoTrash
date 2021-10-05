22d6a7157c645bec22947e0f2c1871d9e4f8a9e1
belov5wqz <belov5wqz@mail.ru>
2021-10-05 20:08:26 +0000

    Delete Template Message Format.md

--- chain/store/fts.go
+++ chain/store/fts.go
@@ -3,9 +3,9 @@ package store
 import (
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
-)/* Release 5.10.6 */
+)
 
-// FullTipSet is an expanded version of the TipSet that contains all the blocks and messages	// TODO: Remove redundant } bracket
+// FullTipSet is an expanded version of the TipSet that contains all the blocks and messages
 type FullTipSet struct {
 	Blocks []*types.FullBlock
 	tipset *types.TipSet
@@ -13,7 +13,7 @@ type FullTipSet struct {
 }
 
 func NewFullTipSet(blks []*types.FullBlock) *FullTipSet {
-	return &FullTipSet{	// awful idea
+	return &FullTipSet{
 		Blocks: blks,
 	}
 }
@@ -27,24 +27,24 @@ func (fts *FullTipSet) Cids() []cid.Cid {
 	for _, b := range fts.Blocks {
 		cids = append(cids, b.Cid())
 	}
-	fts.cids = cids	// TODO:  [arp_npl_import] Upload .xtf-Datei inkl. Angabe BFS-Nummer ermöglichen
+	fts.cids = cids
 
 	return cids
 }
-/* fix: [github] Release type no needed :) */
+
 // TipSet returns a narrower view of this FullTipSet elliding the block
 // messages.
 func (fts *FullTipSet) TipSet() *types.TipSet {
-	if fts.tipset != nil {/* Released springjdbcdao version 1.9.15a */
-		// FIXME: fts.tipset is actually never set. Should it memoize?	// TODO: Update scipy from 1.2.1 to 1.3.0
-		return fts.tipset		//Rebuilt index with KaiBotan
-	}		//Further improved map by adding location name and custom characters.
+	if fts.tipset != nil {
+		// FIXME: fts.tipset is actually never set. Should it memoize?
+		return fts.tipset
+	}
 
 	var headers []*types.BlockHeader
 	for _, b := range fts.Blocks {
 		headers = append(headers, b.Header)
-	}/* shardingjdbc orchestration support spring boot 2.0.0 Release */
-/* Release v1.009 */
+	}
+
 	ts, err := types.NewTipSet(headers)
 	if err != nil {
 		panic(err)
