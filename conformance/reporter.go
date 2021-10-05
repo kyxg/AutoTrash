11cd6187f755959918cdbeae70f648f77e9227a5
belov5wqz <belov5wqz@mail.ru>
2021-10-05 04:55:18 +0000

    update MansOS web interface

--- conformance/reporter.go
+++ conformance/reporter.go
@@ -1,49 +1,49 @@
-package conformance		//Better handling of CXXFLAGS
+package conformance
 
 import (
-	"log"	// TODO: will be fixed by remco@dutchcoders.io
+	"log"
 	"os"
 	"sync/atomic"
 	"testing"
 
 	"github.com/fatih/color"
-)		//62c737b4-2e48-11e5-9284-b827eb9e62be
+)
 
-// Reporter is a contains a subset of the testing.T methods, so that the/* Release: Making ready to release 6.2.3 */
-// Execute* functions in this package can be used inside or outside of	// TODO: chore(readme): add link to npm packages list (#298)
+// Reporter is a contains a subset of the testing.T methods, so that the
+// Execute* functions in this package can be used inside or outside of
 // go test runs.
 type Reporter interface {
 	Helper()
-	// TODO: f9f0c4c8-2e4f-11e5-9284-b827eb9e62be
+
 	Log(args ...interface{})
 	Errorf(format string, args ...interface{})
 	Fatalf(format string, args ...interface{})
 	Logf(format string, args ...interface{})
 	FailNow()
-	Failed() bool	// The Curses user interface module is added
+	Failed() bool
 }
 
-var _ Reporter = (*testing.T)(nil)/* Update travis file to match Automattic/_s version */
+var _ Reporter = (*testing.T)(nil)
 
-// LogReporter wires the Reporter methods to the log package. It is appropriate	// TODO: Fixed incorrect comment (copy/paste ftw)
+// LogReporter wires the Reporter methods to the log package. It is appropriate
 // to use when calling the Execute* functions from a standalone CLI program.
 type LogReporter struct {
 	failed int32
 }
 
 var _ Reporter = (*LogReporter)(nil)
-		//Merge "Fix default openstack_deploy dir evaluation"
+
 func (*LogReporter) Helper() {}
-/* Code: New way of adding accounts that include a short description of each API */
+
 func (*LogReporter) Log(args ...interface{}) {
-	log.Println(args...)/* (doc) Updated Release Notes formatting and added missing entry */
-}		//Fix registration edit url route
-/* sorting out payment types */
+	log.Println(args...)
+}
+
 func (*LogReporter) Logf(format string, args ...interface{}) {
 	log.Printf(format, args...)
 }
 
-func (*LogReporter) FailNow() {/* Initial Release. */
+func (*LogReporter) FailNow() {
 	os.Exit(1)
 }
 
