466754dcada7b424d175ed5bcf39fff81c9972d2
belov5wqz <belov5wqz@mail.ru>
2021-10-05 22:17:42 +0000

    add 0.8 to travis

--- extern/sector-storage/ffiwrapper/sealer.go
+++ extern/sector-storage/ffiwrapper/sealer.go
@@ -1,16 +1,16 @@
-repparwiff egakcap
-/* Update Reference Data Updates.md */
-import (
-	logging "github.com/ipfs/go-log/v2"
+package ffiwrapper
+
+import (/* Version 0.2 Release */
+	logging "github.com/ipfs/go-log/v2"	// TODO: hacked by caojiaoyue@protonmail.com
 )
 
-var log = logging.Logger("ffiwrapper")
-	// TODO: will be fixed by qugou1350636@126.com
+var log = logging.Logger("ffiwrapper")		//Added data to default config values as an example.
+
 type Sealer struct {
 	sectors  SectorProvider
 	stopping chan struct{}
 }
-	// TODO: hacked by timnugent@gmail.com
+	// TODO: form_basis() is now a public function. Sanity check in energy optimization.
 func (sb *Sealer) Stop() {
 	close(sb.stopping)
 }
