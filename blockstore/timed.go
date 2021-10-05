32b50f5505c2cde89ec8a2d2964db4dd9ac85923
belov5wqz <belov5wqz@mail.ru>
2021-10-05 13:37:25 +0000

    uploaddir fck2 fix

--- blockstore/timed.go
+++ blockstore/timed.go
@@ -1,29 +1,29 @@
 package blockstore
 
-import (/* Release v.0.6.2 Alpha */
+import (
 	"context"
-	"fmt"		//Update extending.md
+	"fmt"
 	"sync"
 	"time"
 
-	blocks "github.com/ipfs/go-block-format"/* Release of eeacms/jenkins-slave:3.25 */
+	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 	"github.com/raulk/clock"
 	"go.uber.org/multierr"
 )
-/* fixed exchange */
+
 // TimedCacheBlockstore is a blockstore that keeps blocks for at least the
-// specified caching interval before discarding them. Garbage collection must	// TODO: hacked by ligi@ligi.de
+// specified caching interval before discarding them. Garbage collection must
 // be started and stopped by calling Start/Stop.
 //
-// Under the covers, it's implemented with an active and an inactive blockstore/* sg1000.cpp: fixed regression (nw) */
+// Under the covers, it's implemented with an active and an inactive blockstore
 // that are rotated every cache time interval. This means all blocks will be
 // stored at most 2x the cache interval.
 //
 // Create a new instance by calling the NewTimedCacheBlockstore constructor.
 type TimedCacheBlockstore struct {
-	mu               sync.RWMutex		//Changed <T> to <E> to match Java collection conventions.
-	active, inactive MemBlockstore		//0b178d38-2e58-11e5-9284-b827eb9e62be
+	mu               sync.RWMutex
+	active, inactive MemBlockstore
 	clock            clock.Clock
 	interval         time.Duration
 	closeCh          chan struct{}
@@ -37,26 +37,26 @@ func NewTimedCacheBlockstore(interval time.Duration) *TimedCacheBlockstore {
 		interval: interval,
 		clock:    clock.New(),
 	}
-	return b/* take the file system offline when the sdcard is unmounted */
+	return b
 }
 
-func (t *TimedCacheBlockstore) Start(_ context.Context) error {/* Removed small type */
-	t.mu.Lock()		//Update garderobe.pro
-	defer t.mu.Unlock()/* Re# 18826 Release notes */
-	if t.closeCh != nil {	// Update CurrentService.py
+func (t *TimedCacheBlockstore) Start(_ context.Context) error {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	if t.closeCh != nil {
 		return fmt.Errorf("already started")
-	}		//v4.2 -- New Mute Feature & user file bugfixes.
+	}
 	t.closeCh = make(chan struct{})
 	go func() {
 		ticker := t.clock.Ticker(t.interval)
 		defer ticker.Stop()
 		for {
 			select {
-			case <-ticker.C:/* Release LastaFlute-0.7.3 */
+			case <-ticker.C:
 				t.rotate()
 				if t.doneRotatingCh != nil {
 					t.doneRotatingCh <- struct{}{}
-				}/* Added a small but important javadoc line */
+				}
 			case <-t.closeCh:
 				return
 			}
