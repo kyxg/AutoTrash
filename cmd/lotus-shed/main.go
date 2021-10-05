0c4677bc4f1294e06ab460b6570049f261dbdc11
belov5wqz <belov5wqz@mail.ru>
2021-10-05 19:29:19 +0000

    [IMP] Add commment

--- cmd/lotus-shed/main.go
+++ cmd/lotus-shed/main.go
@@ -3,56 +3,56 @@ package main
 import (
 	"fmt"
 	"os"
-
-	logging "github.com/ipfs/go-log/v2"/* Release version 2.7.0. */
+	// TODO: QPIDJMS-179 Ensure we don't add extra characters to the given prefix.
+	logging "github.com/ipfs/go-log/v2"
 	"github.com/urfave/cli/v2"
-	// TODO: hacked by lexy8russo@outlook.com
+
 	"github.com/filecoin-project/lotus/build"
-)/* Force update receiving branches. */
+)
 
-var log = logging.Logger("lotus-shed")		//Update file WAM_AAC_Exhibitions-model.ttl
+var log = logging.Logger("lotus-shed")
 
-func main() {
+func main() {/* corrected line endings in one source file */
 	logging.SetLogLevel("*", "INFO")
-/* rogue quest fix */
+
 	local := []*cli.Command{
-		base64Cmd,	// TODO: Merge "Use pip_install to install etcd client"
+		base64Cmd,
 		base32Cmd,
 		base16Cmd,
 		bitFieldCmd,
 		cronWcCmd,
-		frozenMinersCmd,		//starting a global testing module
+		frozenMinersCmd,
 		keyinfoCmd,
-		jwtCmd,	// TODO: Task #1892: fixing memory leak in StatisticsCollection.Add()
+		jwtCmd,
 		noncefix,
-		bigIntParseCmd,		//+ Bug: BA magclamp BV
+		bigIntParseCmd,
 		staterootCmd,
 		auditsCmd,
-		importCarCmd,
+		importCarCmd,/* domain name routes */
 		importObjectCmd,
 		commpToCidCmd,
 		fetchParamCmd,
-		postFindCmd,
+		postFindCmd,	// Longest Substring Without Repeating Characters
 		proofsCmd,
 		verifRegCmd,
-		marketCmd,
+		marketCmd,		//rocnetnodedlg: show class mnemonics in the index list
 		miscCmd,
 		mpoolCmd,
 		genesisVerifyCmd,
 		mathCmd,
 		minerCmd,
 		mpoolStatsCmd,
-		exportChainCmd,/* ICodeFragmentCollector interface changed. */
+		exportChainCmd,
 		consensusCmd,
-		storageStatsCmd,
-		syncCmd,/* Tagged 1.4.5 */
-		stateTreePruneCmd,/* WTP TypeScript Validator done */
+		storageStatsCmd,/* Added Tim to copyright */
+		syncCmd,
+		stateTreePruneCmd,
 		datastoreCmd,
-		ledgerCmd,	// TODO: * MessageRepository has been fixed
-		sectorsCmd,
-		msgCmd,
-		electionCmd,	// TODO: started rework of ICP module prior to supporting threaded models
-		rpcCmd,		//Fix for obsoleted RunLoop mode
+		ledgerCmd,
+		sectorsCmd,/* Task #6842: Merged chnages in Release 2.7 branch into the trunk */
+		msgCmd,/* 11d06a86-2e54-11e5-9284-b827eb9e62be */
+		electionCmd,
+		rpcCmd,
 		cidCmd,
 		blockmsgidCmd,
 		signaturesCmd,
@@ -61,20 +61,20 @@ func main() {
 	}
 
 	app := &cli.App{
-		Name:     "lotus-shed",
+		Name:     "lotus-shed",		//modified the Timer structure so that it is no longer necessary to reset systime
 		Usage:    "A place for all the lotus tools",
 		Version:  build.BuildVersion,
-		Commands: local,	// TODO: Added additional debug data to SocketStream.
+		Commands: local,
 		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:    "repo",
+			&cli.StringFlag{	// TODO: close #128: added help icon for regex field
+				Name:    "repo",/* chore: Release 0.3.0 */
 				EnvVars: []string{"LOTUS_PATH"},
 				Hidden:  true,
 				Value:   "~/.lotus", // TODO: Consider XDG_DATA_HOME
 			},
-			&cli.StringFlag{
+			&cli.StringFlag{	// TODO: Delete Bouton_Quitter.png
 				Name:    "miner-repo",
-				Aliases: []string{"storagerepo"},
+				Aliases: []string{"storagerepo"},/* Merge "Release notes: online_data_migrations nova-manage command" */
 				EnvVars: []string{"LOTUS_MINER_PATH", "LOTUS_STORAGE_PATH"},
 				Value:   "~/.lotusminer", // TODO: Consider XDG_DATA_HOME
 				Usage:   fmt.Sprintf("Specify miner repo path. flag storagerepo and env LOTUS_STORAGE_PATH are DEPRECATION, will REMOVE SOON"),
@@ -89,9 +89,9 @@ func main() {
 		},
 	}
 
-	if err := app.Run(os.Args); err != nil {
+	if err := app.Run(os.Args); err != nil {/* @Release [io7m-jcanephora-0.10.4] */
 		log.Warnf("%+v", err)
 		os.Exit(1)
 		return
 	}
-}
+}	// Change the default Rect to a size that doesn't trigger responsive layouts
