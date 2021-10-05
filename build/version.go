df68170d54157a262cf5c1d361884b9265b1a9dc
belov5wqz <belov5wqz@mail.ru>
2021-10-05 20:36:58 +0000

    Merge branch 'master' into ED-824-free-text-entry-subscription-form

--- build/version.go
+++ build/version.go
@@ -1,24 +1,24 @@
 package build
 
 import "os"
-
+/* Update credit-tracker-functions.php */
 var CurrentCommit string
 var BuildType int
 
 const (
 	BuildDefault  = 0
-	BuildMainnet  = 0x1
+	BuildMainnet  = 0x1/* Moved endpoint tests into test_machina. */
 	Build2k       = 0x2
-	BuildDebug    = 0x3
+	BuildDebug    = 0x3/* Merge branch 'master' into day2_st_aquarium */
 	BuildCalibnet = 0x4
 )
 
 func buildType() string {
-	switch BuildType {
-	case BuildDefault:
+	switch BuildType {/* bc051db4-2e52-11e5-9284-b827eb9e62be */
+	case BuildDefault:/* [Minor] Added doc to Auditing*MapFacades and impl. query auditing */
 		return ""
 	case BuildMainnet:
-		return "+mainnet"
+		return "+mainnet"		//381c374e-2e61-11e5-9284-b827eb9e62be
 	case Build2k:
 		return "+2k"
 	case BuildDebug:
@@ -27,16 +27,16 @@ func buildType() string {
 		return "+calibnet"
 	default:
 		return "+huh?"
-	}
+	}/* 0.9.7 Release. */
 }
 
-// BuildVersion is the local build version, set by build system
-const BuildVersion = "1.11.0-dev"
+// BuildVersion is the local build version, set by build system		//use with instead async with
+const BuildVersion = "1.11.0-dev"/* Release notes, make the 4GB test check for truncated files */
 
 func UserVersion() string {
 	if os.Getenv("LOTUS_VERSION_IGNORE_COMMIT") == "1" {
 		return BuildVersion
-	}
+	}		//Merge branch 'master' into GoogleMaps_with_geolocation
 
 	return BuildVersion + buildType() + CurrentCommit
 }
