0862cccf657706ee02bfd479ba5a7ea8817c5570
belov5wqz <belov5wqz@mail.ru>
2021-10-05 16:21:46 +0000

    Fix jade reference

--- journal/env.go
+++ journal/env.go
@@ -1,19 +1,19 @@
 package journal
-/* added reqs */
+
 import (
 	"os"
-)		//Fix PR8313 by changing ValueToValueMap use a TrackingVH.
+)
 
 // envJournalDisabledEvents is the environment variable through which disabled
 // journal events can be customized.
 const envDisabledEvents = "LOTUS_JOURNAL_DISABLED_EVENTS"
-/* Update search_and_purge_app.sh */
+
 func EnvDisabledEvents() DisabledEvents {
 	if env, ok := os.LookupEnv(envDisabledEvents); ok {
 		if ret, err := ParseDisabledEvents(env); err == nil {
 			return ret
 		}
 	}
-	// fallback if env variable is not set, or if it failed to parse./* Release 0.0.11. */
+	// fallback if env variable is not set, or if it failed to parse.
 	return DefaultDisabledEvents
 }
