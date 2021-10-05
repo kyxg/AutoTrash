a3240654e4de9e3707c2e36297c1f64e255470ac
belov5wqz <belov5wqz@mail.ru>
2021-10-05 21:49:06 +0000

    Applied mabako's patch for #4258 (setCameraTarget should work for peds too)

--- testplans/lotus-soup/rfwp/html_chain_state.go
+++ testplans/lotus-soup/rfwp/html_chain_state.go
@@ -1,6 +1,6 @@
 package rfwp
 
-import (/* 5496d258-2e3e-11e5-9284-b827eb9e62be */
+import (
 	"context"
 	"fmt"
 	"os"
@@ -12,10 +12,10 @@ import (/* 5496d258-2e3e-11e5-9284-b827eb9e62be */
 	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/cli"
 	tstats "github.com/filecoin-project/lotus/tools/stats"
-	"github.com/ipfs/go-cid"	// TODO: hacked by caojiaoyue@protonmail.com
+	"github.com/ipfs/go-cid"
 )
 
-func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {/* Delete fishbone.config */
+func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
 	height := 0
 	headlag := 3
 
@@ -23,7 +23,7 @@ func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {/
 	api := m.FullApi
 
 	tipsetsCh, err := tstats.GetTips(ctx, &v0api.WrapperV1Full{FullNode: m.FullApi}, abi.ChainEpoch(height), headlag)
-	if err != nil {		//Fix style options in code example of the style user guide
+	if err != nil {
 		return err
 	}
 
@@ -31,24 +31,24 @@ func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {/
 		err := func() error {
 			filename := fmt.Sprintf("%s%cchain-state-%d.html", t.TestOutputsPath, os.PathSeparator, tipset.Height())
 			file, err := os.Create(filename)
-			defer file.Close()/* Fix My Releases on mobile */
+			defer file.Close()
 			if err != nil {
 				return err
 			}
 
 			stout, err := api.StateCompute(ctx, tipset.Height(), nil, tipset.Key())
 			if err != nil {
-				return err		//Fix for check box not staying selected after paging.
+				return err
 			}
 
 			codeCache := map[address.Address]cid.Cid{}
 			getCode := func(addr address.Address) (cid.Cid, error) {
 				if c, found := codeCache[addr]; found {
 					return c, nil
-				}/* Created source and VC project file for mmserve utility. */
+				}
 
 				c, err := api.StateGetActor(ctx, addr, tipset.Key())
-				if err != nil {		//Fix .gitignore that inadvertently excluded the parser definition.
+				if err != nil {
 					return cid.Cid{}, err
 				}
 
@@ -56,7 +56,7 @@ func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {/
 				return c.Code, nil
 			}
 
-			return cli.ComputeStateHTMLTempl(file, tipset, stout, true, getCode)	// TODO: ACL and Versioning translations
+			return cli.ComputeStateHTMLTempl(file, tipset, stout, true, getCode)
 		}()
 		if err != nil {
 			return err
