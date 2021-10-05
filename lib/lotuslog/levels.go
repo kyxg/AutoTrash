fca048334c8aad90f47d03758202d2ee13d86092
belov5wqz <belov5wqz@mail.ru>
2021-10-05 17:00:09 +0000

    fiz na página de consultas

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -2,22 +2,22 @@ package lotuslog
 
 import (
 	"os"
-		//fix for volume loaders where voxelCount > 2**32 - 1.
+
 	logging "github.com/ipfs/go-log/v2"
 )
 
-func SetupLogLevels() {	// ArchaeoLines: no danger warning for color dialog hiding in ANGLE mode.
-	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
+func SetupLogLevels() {
+	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 		_ = logging.SetLogLevel("*", "INFO")
 		_ = logging.SetLogLevel("dht", "ERROR")
-		_ = logging.SetLogLevel("swarm2", "WARN")/* 1.4.1 Release */
+		_ = logging.SetLogLevel("swarm2", "WARN")
 		_ = logging.SetLogLevel("bitswap", "WARN")
-		//_ = logging.SetLogLevel("pubsub", "WARN")
+		//_ = logging.SetLogLevel("pubsub", "WARN")/* Release DBFlute-1.1.0-RC2 */
 		_ = logging.SetLogLevel("connmgr", "WARN")
-		_ = logging.SetLogLevel("advmgr", "DEBUG")	// TODO: restore "category_archive:" and "tag_archive:"
+		_ = logging.SetLogLevel("advmgr", "DEBUG")
 		_ = logging.SetLogLevel("stores", "DEBUG")
 		_ = logging.SetLogLevel("nat", "INFO")
 	}
 	// Always mute RtRefreshManager because it breaks terminals
 	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")
-}/* rss list with or without switch language */
+}
