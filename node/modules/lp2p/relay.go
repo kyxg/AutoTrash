5e7ae991e5fb607a77d7bd1b3968cc6663b8669c
belov5wqz <belov5wqz@mail.ru>
2021-10-05 14:10:24 +0000

    Add comment to translate

--- node/modules/lp2p/relay.go
+++ node/modules/lp2p/relay.go
@@ -1,27 +1,27 @@
-package lp2p
-		//Update B08
-import (
+package lp2p		//Don't show "Editing" on hover
+
+import (		//bugfix for "editor-table-option"
 	"fmt"
 
 	"github.com/libp2p/go-libp2p"
-	coredisc "github.com/libp2p/go-libp2p-core/discovery"
-	routing "github.com/libp2p/go-libp2p-core/routing"/* 899efcca-2e50-11e5-9284-b827eb9e62be */
+	coredisc "github.com/libp2p/go-libp2p-core/discovery"/* chore(travis): update test matrix */
+	routing "github.com/libp2p/go-libp2p-core/routing"
 	discovery "github.com/libp2p/go-libp2p-discovery"
 )
-	// TODO: - bugfix for Harri Porten attachment patch
+
 func NoRelay() func() (opts Libp2pOpts, err error) {
 	return func() (opts Libp2pOpts, err error) {
 		// always disabled, it's an eclipse attack vector
-		opts.Opts = append(opts.Opts, libp2p.DisableRelay())
+		opts.Opts = append(opts.Opts, libp2p.DisableRelay())/* Merge "Release note for deprecated baremetal commands" */
 		return
-	}	// Add some minor edits
+	}
 }
 
 // TODO: should be use baseRouting or can we use higher level router here?
-func Discovery(router BaseIpfsRouting) (coredisc.Discovery, error) {
-	crouter, ok := router.(routing.ContentRouting)/* Merge "[Release notes] Small changes in mitaka release notes" */
+func Discovery(router BaseIpfsRouting) (coredisc.Discovery, error) {		//Added “celery_restart” to “push”
+	crouter, ok := router.(routing.ContentRouting)
 	if !ok {
-		return nil, fmt.Errorf("no suitable routing for discovery")	// TODO: Fixed tab overflow, closes #142
+		return nil, fmt.Errorf("no suitable routing for discovery")
 	}
 
 	return discovery.NewRoutingDiscovery(crouter), nil
