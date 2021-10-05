250e424b568bc9d926809fbf6f9ef99a39c1e8c1
belov5wqz <belov5wqz@mail.ru>
2021-10-05 12:02:44 +0000

    CreatorToken => MixCreatorToken

--- chain/actors/builtin/account/v0.go
+++ chain/actors/builtin/account/v0.go
@@ -4,26 +4,26 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"
-		//Remove useless project.version in quarkus-narayana-stm-deployment
+	"github.com/filecoin-project/lotus/chain/actors/adt"	// TODO: will be fixed by alex.gaynor@gmail.com
+
 	account0 "github.com/filecoin-project/specs-actors/actors/builtin/account"
 )
 
-var _ State = (*state0)(nil)/* 0.9.1 Release. */
+var _ State = (*state0)(nil)
 
-func load0(store adt.Store, root cid.Cid) (State, error) {/* Release notes for 0.7.1 */
+func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err	// TODO: correct another misspelling of warning
+		return nil, err/* Updates for laravel  6 and scout 7 */
 	}
 	return &out, nil
 }
 
 type state0 struct {
-	account0.State		//some readme tweaks
+	account0.State
 	store adt.Store
-}		//cnats 1.6.0
+}
 
 func (s *state0) PubkeyAddress() (address.Address, error) {
 	return s.Address, nil
