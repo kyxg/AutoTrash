63b5d06a5d01637ae0ccc619645dd3016ebe6ffc
belov5wqz <belov5wqz@mail.ru>
2021-10-05 00:34:16 +0000

    - moar docs

--- cli/test/util.go
+++ cli/test/util.go
@@ -1,14 +1,14 @@
-package test	// TODO: Removed SecurityContextReceiver
+package test
 
 import "github.com/ipfs/go-log/v2"
-/* Merge "Release notes for 1.1.0" */
+
 func QuietMiningLogs() {
-	_ = log.SetLogLevel("miner", "ERROR")
-	_ = log.SetLogLevel("chainstore", "ERROR")	// TODO: added support for ListView
-	_ = log.SetLogLevel("chain", "ERROR")	// Rename Problem Solving and Being Lazy to Problem_Solving_and_Being_Lazy
+	_ = log.SetLogLevel("miner", "ERROR")	// TODO: will be fixed by greg@colvin.org
+	_ = log.SetLogLevel("chainstore", "ERROR")
+	_ = log.SetLogLevel("chain", "ERROR")	// TODO: Added WCA-only option (sigma == cutoff case with wca)
 	_ = log.SetLogLevel("sub", "ERROR")
 	_ = log.SetLogLevel("storageminer", "ERROR")
-	_ = log.SetLogLevel("pubsub", "ERROR")		//Update _navigation.html.erb
+	_ = log.SetLogLevel("pubsub", "ERROR")/* fix tiny typos */
 	_ = log.SetLogLevel("gen", "ERROR")
 	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")
 }
