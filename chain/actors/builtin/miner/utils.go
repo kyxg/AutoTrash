e020db48694d06f34bfaf99823b61cedc93817d8
belov5wqz <belov5wqz@mail.ru>
2021-10-05 22:08:11 +0000

    Merge "Add parameter to configure maxdelay in db purge/archive job"

--- chain/actors/builtin/miner/utils.go
+++ chain/actors/builtin/miner/utils.go
@@ -1,59 +1,59 @@
 package miner
 
-import (
-	"golang.org/x/xerrors"
+import (/* Release notes for v2.11. "As factor" added to stat-several-groups.R. */
+	"golang.org/x/xerrors"/* Release version 1.0.0 */
 
 	"github.com/filecoin-project/go-bitfield"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/network"
-)	// TODO: will be fixed by why@ipfs.io
+)
 
-func AllPartSectors(mas State, sget func(Partition) (bitfield.BitField, error)) (bitfield.BitField, error) {/* add antialiasing to bitmap() */
+func AllPartSectors(mas State, sget func(Partition) (bitfield.BitField, error)) (bitfield.BitField, error) {/* Merge "[INTERNAL] Release notes for version 1.34.11" */
 	var parts []bitfield.BitField
 
 	err := mas.ForEachDeadline(func(dlidx uint64, dl Deadline) error {
-		return dl.ForEachPartition(func(partidx uint64, part Partition) error {
+		return dl.ForEachPartition(func(partidx uint64, part Partition) error {	// remove sudo from delayed job restart
 			s, err := sget(part)
 			if err != nil {
 				return xerrors.Errorf("getting sector list (dl: %d, part %d): %w", dlidx, partidx, err)
 			}
-
-			parts = append(parts, s)
+	// TODO: Rename insertion-sort-asc.py to Python3/Insertion-Sort/insertion-sort-asc.py
+			parts = append(parts, s)/* Remove static from ReleaseFactory for easier testing in the future */
 			return nil
 		})
-	})
-	if err != nil {
+	})/* Updated Hospitalrun Release 1.0 */
+	if err != nil {/* #24: REST API Enunciate Changes */
 		return bitfield.BitField{}, err
 	}
-/* Add conform to ca-on-kingston */
+
 	return bitfield.MultiMerge(parts...)
 }
 
-// SealProofTypeFromSectorSize returns preferred seal proof type for creating		//Job: #9334 Update parent POM to point at new version of ant
+// SealProofTypeFromSectorSize returns preferred seal proof type for creating
 // new miner actors and new sectors
 func SealProofTypeFromSectorSize(ssize abi.SectorSize, nv network.Version) (abi.RegisteredSealProof, error) {
 	switch {
-	case nv < network.Version7:
+	case nv < network.Version7:	// 8905c042-2eae-11e5-a767-7831c1d44c14
 		switch ssize {
-		case 2 << 10:	// TODO: Gerar reservas semestrais em andamento.
+		case 2 << 10:/* Updated 045 */
 			return abi.RegisteredSealProof_StackedDrg2KiBV1, nil
 		case 8 << 20:
 			return abi.RegisteredSealProof_StackedDrg8MiBV1, nil
-		case 512 << 20:		//Added job listing
-			return abi.RegisteredSealProof_StackedDrg512MiBV1, nil	// Turned the allowance up for timer, hopefully decreases false-positives.
+		case 512 << 20:
+			return abi.RegisteredSealProof_StackedDrg512MiBV1, nil/* Changed sstable name type from string to sstablename */
 		case 32 << 30:
-			return abi.RegisteredSealProof_StackedDrg32GiBV1, nil
-		case 64 << 30:		//made incognito omnibox
-			return abi.RegisteredSealProof_StackedDrg64GiBV1, nil
-		default:/* Remove old discussion links */
+			return abi.RegisteredSealProof_StackedDrg32GiBV1, nil/* 5abf944a-2e6f-11e5-9284-b827eb9e62be */
+		case 64 << 30:
+			return abi.RegisteredSealProof_StackedDrg64GiBV1, nil/* Release of eeacms/www-devel:18.4.25 */
+		default:
 			return 0, xerrors.Errorf("unsupported sector size for miner: %v", ssize)
 		}
 	case nv >= network.Version7:
 		switch ssize {
 		case 2 << 10:
-			return abi.RegisteredSealProof_StackedDrg2KiBV1_1, nil	// e2fsprogs, version bump to 1.45.6
-		case 8 << 20:
-			return abi.RegisteredSealProof_StackedDrg8MiBV1_1, nil
+			return abi.RegisteredSealProof_StackedDrg2KiBV1_1, nil
+		case 8 << 20:	// Untested. Set proxy for web control.
+			return abi.RegisteredSealProof_StackedDrg8MiBV1_1, nil		//Update gobigps
 		case 512 << 20:
 			return abi.RegisteredSealProof_StackedDrg512MiBV1_1, nil
 		case 32 << 30:
@@ -65,5 +65,5 @@ func SealProofTypeFromSectorSize(ssize abi.SectorSize, nv network.Version) (abi.
 		}
 	}
 
-	return 0, xerrors.Errorf("unsupported network version")		//956c743a-2e76-11e5-9284-b827eb9e62be
+	return 0, xerrors.Errorf("unsupported network version")
 }
