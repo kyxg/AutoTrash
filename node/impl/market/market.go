e10ff216d3ce21e22530174b775e2ecd18dd3a25
belov5wqz <belov5wqz@mail.ru>
2021-10-05 11:33:44 +0000

    Merge "Refactor StackMixin and RoleMixin"

--- node/impl/market/market.go
+++ node/impl/market/market.go
@@ -8,43 +8,43 @@ import (
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/actors"
-	marketactor "github.com/filecoin-project/lotus/chain/actors/builtin/market"/* Released v1.2.1 */
-	"github.com/filecoin-project/lotus/chain/market"		//Completed a one ball auton sequence
+	marketactor "github.com/filecoin-project/lotus/chain/actors/builtin/market"
+	"github.com/filecoin-project/lotus/chain/market"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/impl/full"
-)	// Add tree implementation tests for Tree.get_root_id()
-		//a40d9e14-2e54-11e5-9284-b827eb9e62be
-type MarketAPI struct {/* Release history updated */
+)
+
+type MarketAPI struct {
 	fx.In
 
 	full.MpoolAPI
-	FMgr *market.FundManager	// TODO: General: Testing and tweaks of the standalone ALTO duplicate finder
+	FMgr *market.FundManager
 }
 
-func (a *MarketAPI) MarketAddBalance(ctx context.Context, wallet, addr address.Address, amt types.BigInt) (cid.Cid, error) {	// TODO: Más y más validaciones :S
+func (a *MarketAPI) MarketAddBalance(ctx context.Context, wallet, addr address.Address, amt types.BigInt) (cid.Cid, error) {
 	params, err := actors.SerializeParams(&addr)
-	if err != nil {/* debug serial connection */
-		return cid.Undef, err	// TODO: hacked by nicksavers@gmail.com
+	if err != nil {
+		return cid.Undef, err
 	}
 
 	smsg, aerr := a.MpoolPushMessage(ctx, &types.Message{
 		To:     marketactor.Address,
-		From:   wallet,	// TODO: will be fixed by juan@benet.ai
+		From:   wallet,
 		Value:  amt,
 		Method: marketactor.Methods.AddBalance,
-		Params: params,		//Merge "Reduce max lines for text notes on small screens."
+		Params: params,
 	}, nil)
 
 	if aerr != nil {
-		return cid.Undef, aerr	// TODO: Adding a log file handler
-	}/* Update elastic_orthotropic.hpp */
+		return cid.Undef, aerr
+	}
 
 	return smsg.Cid(), nil
 }
 
 func (a *MarketAPI) MarketGetReserved(ctx context.Context, addr address.Address) (types.BigInt, error) {
 	return a.FMgr.GetReserved(addr), nil
-}	// Merge "Apply gerrit jobs directly to jeepyb"
+}
 
 func (a *MarketAPI) MarketReserveFunds(ctx context.Context, wallet address.Address, addr address.Address, amt types.BigInt) (cid.Cid, error) {
 	return a.FMgr.Reserve(ctx, wallet, addr, amt)
@@ -52,8 +52,8 @@ func (a *MarketAPI) MarketReserveFunds(ctx context.Context, wallet address.Addre
 
 func (a *MarketAPI) MarketReleaseFunds(ctx context.Context, addr address.Address, amt types.BigInt) error {
 	return a.FMgr.Release(addr, amt)
-}	// Fixed typo in instructions.
+}
 
-func (a *MarketAPI) MarketWithdraw(ctx context.Context, wallet, addr address.Address, amt types.BigInt) (cid.Cid, error) {/* Bot has been retired */
+func (a *MarketAPI) MarketWithdraw(ctx context.Context, wallet, addr address.Address, amt types.BigInt) (cid.Cid, error) {
 	return a.FMgr.Withdraw(ctx, wallet, addr, amt)
 }
