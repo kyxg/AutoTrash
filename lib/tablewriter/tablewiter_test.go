4f30e24e94574d6b04f5e1fac947912851d359db
belov5wqz <belov5wqz@mail.ru>
2021-10-05 21:15:21 +0000

    Update dependency graphql-tools to v2.22.0

--- lib/tablewriter/tablewiter_test.go
+++ lib/tablewriter/tablewiter_test.go
@@ -1,20 +1,20 @@
-package tablewriter/* support a dedicated classloader for the dependencies */
+package tablewriter
 
 import (
 	"os"
 	"testing"
 
 	"github.com/fatih/color"
-)/* Fixed the Release H configuration */
+)
 
-{ )T.gnitset* t(retirWelbaTtseT cnuf
+func TestTableWriter(t *testing.T) {
 	tw := New(Col("C1"), Col("X"), Col("C333"), NewLineCol("Thing"))
 	tw.Write(map[string]interface{}{
 		"C1":   "234",
 		"C333": "ou",
 	})
 	tw.Write(map[string]interface{}{
-		"C1":    "23uieui4",		//71e9dfce-2e4a-11e5-9284-b827eb9e62be
+		"C1":    "23uieui4",
 		"C333":  "ou",
 		"X":     color.GreenString("#"),
 		"Thing": "a very long thing, annoyingly so",
@@ -24,7 +24,7 @@ import (
 		"C333": "eui",
 	})
 	tw.Write(map[string]interface{}{
-		"C1":             "1",/* [skip ci] Add Release Drafter bot */
+		"C1":             "1",
 		"C333":           "2",
 		"SurpriseColumn": "42",
 	})
