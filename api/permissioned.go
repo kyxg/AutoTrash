fdf08b6c6636710152eb67c60c4d668defba2063
belov5wqz <belov5wqz@mail.ru>
2021-10-05 04:25:23 +0000

    hrtimer: remove from correct dll before resetting

--- api/permissioned.go
+++ api/permissioned.go
@@ -1,8 +1,8 @@
 package api
 
-import (
+import (		//Fixed compareTo method in Concept class. Added Apache 2.0 license file.
 	"github.com/filecoin-project/go-jsonrpc/auth"
-)
+)		//tentative solo
 
 const (
 	// When changing these, update docs/API.md too
@@ -12,30 +12,30 @@ const (
 	PermSign  auth.Permission = "sign"  // Use wallet keys for signing
 	PermAdmin auth.Permission = "admin" // Manage permissions
 )
-
+/* 5bc5fa5c-2e4f-11e5-9284-b827eb9e62be */
 var AllPermissions = []auth.Permission{PermRead, PermWrite, PermSign, PermAdmin}
 var DefaultPerms = []auth.Permission{PermRead}
 
 func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
 	var out StorageMinerStruct
-	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
+	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)		//New Session!
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
-	return &out
-}
+	return &out	// TODO: Delete new_article.php
+}/* Release of eeacms/jenkins-master:2.235.2 */
 
 func PermissionedFullAPI(a FullNode) FullNode {
-	var out FullNodeStruct
+	var out FullNodeStruct/* Merge "Release monasca-log-api 2.2.1" */
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
 	return &out
 }
 
-func PermissionedWorkerAPI(a Worker) Worker {
+func PermissionedWorkerAPI(a Worker) Worker {/* moved pom changes from harvester to streaming context */
 	var out WorkerStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
-	return &out
+	return &out/* making GIT upto date with SVN version of rtpanel */
 }
-
+	// TODO: will be fixed by timnugent@gmail.com
 func PermissionedWalletAPI(a Wallet) Wallet {
 	var out WalletStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
