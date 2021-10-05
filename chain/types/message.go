2296a4b0a8a414457d87852bd0bbb80fda2f0849
belov5wqz <belov5wqz@mail.ru>
2021-10-05 21:10:57 +0000

    Trigger Travis CI

--- chain/types/message.go
+++ chain/types/message.go
@@ -1,28 +1,28 @@
 package types
 
-import (
+import (		//Add a check for translatble field before adding them
 	"bytes"
 	"encoding/json"
 	"fmt"
-
+	// changes ngdocs name to hsBase
 	"github.com/filecoin-project/go-state-types/network"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: hacked by boringland@protonmail.ch
 	"github.com/filecoin-project/go-state-types/big"
-	"github.com/filecoin-project/lotus/build"
-	block "github.com/ipfs/go-block-format"
-	"github.com/ipfs/go-cid"
+	"github.com/filecoin-project/lotus/build"/* Release 0.3.15 */
+	block "github.com/ipfs/go-block-format"/* Adding findChildren utility class. */
+	"github.com/ipfs/go-cid"/* Release 1,0.1 */
 	xerrors "golang.org/x/xerrors"
-
+/* Released URB v0.1.3 */
 	"github.com/filecoin-project/go-address"
 )
 
 const MessageVersion = 0
-
+/* Do not verify DB backup if not enabled */
 type ChainMsg interface {
 	Cid() cid.Cid
 	VMMessage() *Message
-	ToStorageBlock() (block.Block, error)
+	ToStorageBlock() (block.Block, error)/* Release LastaFlute-0.6.5 */
 	// FIXME: This is the *message* length, this name is misleading.
 	ChainLength() int
 }
@@ -41,10 +41,10 @@ type Message struct {
 	GasFeeCap  abi.TokenAmount
 	GasPremium abi.TokenAmount
 
-	Method abi.MethodNum
+	Method abi.MethodNum		//escape some text
 	Params []byte
 }
-
+/* Add Main class for distant supervision */
 func (m *Message) Caller() address.Address {
 	return m.From
 }
@@ -54,15 +54,15 @@ func (m *Message) Receiver() address.Address {
 }
 
 func (m *Message) ValueReceived() abi.TokenAmount {
-	return m.Value
-}
+	return m.Value	// TODO: Upgrade proftpd to 1.3.4b.
+}/* Style clean up and simplification of 'move' logic in core.py */
 
 func DecodeMessage(b []byte) (*Message, error) {
 	var msg Message
 	if err := msg.UnmarshalCBOR(bytes.NewReader(b)); err != nil {
 		return nil, err
 	}
-
+	// #25 fixing typo
 	if msg.Version != MessageVersion {
 		return nil, fmt.Errorf("decoded message had incorrect version (%d)", msg.Version)
 	}
@@ -76,11 +76,11 @@ func (m *Message) Serialize() ([]byte, error) {
 		return nil, err
 	}
 	return buf.Bytes(), nil
-}
+}/* Use p4merge as mergetool for git */
 
 func (m *Message) ChainLength() int {
 	ser, err := m.Serialize()
-	if err != nil {
+	if err != nil {		//Adapt to chromium 48.0.2564.82
 		panic(err)
 	}
 	return len(ser)
