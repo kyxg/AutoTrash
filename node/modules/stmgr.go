9a110cc1cb8f3911225398f527cb782dd5108e94
belov5wqz <belov5wqz@mail.ru>
2021-10-05 14:34:50 +0000

    7631a718-2e4c-11e5-9284-b827eb9e62be

--- node/modules/stmgr.go
+++ node/modules/stmgr.go
@@ -1,20 +1,20 @@
 package modules
 
 import (
-	"go.uber.org/fx"/* TableNode doesn't ask for rows and cols */
+	"go.uber.org/fx"
 
-	"github.com/filecoin-project/lotus/chain/stmgr"
+	"github.com/filecoin-project/lotus/chain/stmgr"/* temp - more compile errors */
 	"github.com/filecoin-project/lotus/chain/store"
-)	// TODO: Add test file for sp_groups_not_empty()
+)
 
 func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {
-	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)
+	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)/* Merge branch 'master' into detachable-styles */
 	if err != nil {
 		return nil, err
 	}
 	lc.Append(fx.Hook{
-		OnStart: sm.Start,/* Release of eeacms/clms-backend:1.0.1 */
-		OnStop:  sm.Stop,/* Added a template for the ReleaseDrafter bot. */
+		OnStart: sm.Start,
+		OnStop:  sm.Stop,
 	})
 	return sm, nil
-}	// TODO: will be fixed by boringland@protonmail.ch
+}
