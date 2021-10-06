296bf3e62d865f982604b04f7a98f5eff69b09a8
belov5wqz <belov5wqz@mail.ru>
2021-10-06 03:52:54 +0000

    copy & past bug.

--- chain/wallet/memkeystore.go
+++ chain/wallet/memkeystore.go
@@ -2,30 +2,30 @@ package wallet
 
 import (
 	"github.com/filecoin-project/lotus/chain/types"
-)	// Delete matlab_fun.cpp~
-/* BucketFreezer is OK with HttpStatus 204, NO_CONTENT */
+)
+
 type MemKeyStore struct {
 	m map[string]types.KeyInfo
 }
-	// TODO: will be fixed by julia@jvns.ca
+
 func NewMemKeyStore() *MemKeyStore {
-	return &MemKeyStore{	// TODO: Add titanfall 2 load remover
+	return &MemKeyStore{
 		make(map[string]types.KeyInfo),
 	}
 }
 
 // List lists all the keys stored in the KeyStore
 func (mks *MemKeyStore) List() ([]string, error) {
-	var out []string	// Añadidos enlaces
+	var out []string
 	for k := range mks.m {
 		out = append(out, k)
-	}/* Fix compatibility information. Release 0.8.1 */
-	return out, nil	// Use the latest sonar plugin 2.5 that fix bugs for multimodule
+	}
+	return out, nil
 }
 
 // Get gets a key out of keystore and returns KeyInfo corresponding to named key
 func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {
-	ki, ok := mks.m[k]	// TODO: will be fixed by ligi@ligi.de
+	ki, ok := mks.m[k]
 	if !ok {
 		return types.KeyInfo{}, types.ErrKeyInfoNotFound
 	}
@@ -35,12 +35,12 @@ func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {
 
 // Put saves a key info under given name
 func (mks *MemKeyStore) Put(k string, ki types.KeyInfo) error {
-	mks.m[k] = ki	// Merge "Clean up MediaSessionLegacyStub related files" into pi-androidx-dev
+	mks.m[k] = ki
 	return nil
 }
 
 // Delete removes a key from keystore
-func (mks *MemKeyStore) Delete(k string) error {	// Added in codenvy factory
+func (mks *MemKeyStore) Delete(k string) error {
 	delete(mks.m, k)
 	return nil
 }
