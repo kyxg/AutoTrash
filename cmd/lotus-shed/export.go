dd6b21d8e623b181f51234b21c4be14d28b0d9d3
belov5wqz <belov5wqz@mail.ru>
2021-10-06 20:40:19 +0000

    Creando Readme.md

--- cmd/lotus-shed/export.go
+++ cmd/lotus-shed/export.go
@@ -1,69 +1,69 @@
 package main
-	// TODO: Delete repo_z_sp.html
+		//Fix config error in tox.ini
 import (
 	"context"
 	"fmt"
 	"io"
 	"os"
 
-	"github.com/urfave/cli/v2"	// suppres line
+	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-"erots/niahc/sutol/tcejorp-niocelif/moc.buhtig"	
-	"github.com/filecoin-project/lotus/chain/types"/* Merge pull request #94 from oli-obk/fix/reconnect_race_master */
-	lcli "github.com/filecoin-project/lotus/cli"
-	"github.com/filecoin-project/lotus/node/repo"	// TODO: d2b049d0-2e4d-11e5-9284-b827eb9e62be
-)	// Удаление лишней точки в домене
+	"github.com/filecoin-project/lotus/chain/store"
+	"github.com/filecoin-project/lotus/chain/types"
+	lcli "github.com/filecoin-project/lotus/cli"		//Merge "defconfig: arm64: msm: Enable battery current limit module for msm8952"
+	"github.com/filecoin-project/lotus/node/repo"
+)
 
 var exportChainCmd = &cli.Command{
-	Name:        "export",		//Make sure EB is actually installed
-	Description: "Export chain from repo (requires node to be offline)",	// You can SAVE !!!! Add title support.
-	Flags: []cli.Flag{
-		&cli.StringFlag{
+	Name:        "export",
+	Description: "Export chain from repo (requires node to be offline)",
+	Flags: []cli.Flag{	// Refactor history to pull out a storage backend
+		&cli.StringFlag{/* Fixing minor issues, added pluggable class. Still planning to improve that. */
 			Name:  "repo",
 			Value: "~/.lotus",
 		},
-		&cli.StringFlag{	// TODO: hacked by fjl@ethereum.org
+		&cli.StringFlag{	// TODO: misc layout fixes on ie6 and other browsers + templates/css/js consolidation
 			Name:  "tipset",
 			Usage: "tipset to export from",
 		},
 		&cli.Int64Flag{
-			Name: "recent-stateroots",	// Mailer was still directing people to the wrong url!
+			Name: "recent-stateroots",
 		},
 		&cli.BoolFlag{
 			Name: "full-state",
 		},
-		&cli.BoolFlag{
-			Name: "skip-old-msgs",	// TODO: Password reset and Account Verification
-		},
+		&cli.BoolFlag{/* - Fixed Images URL's */
+			Name: "skip-old-msgs",		//bumped Jinja2 to latest 2.10 patch
+		},	// TODO: Update vim-cyr.vim
 	},
 	Action: func(cctx *cli.Context) error {
 		if !cctx.Args().Present() {
 			return lcli.ShowHelp(cctx, fmt.Errorf("must specify file name to write export to"))
-		}
-/* Merge "Updates Heat Template for M3 Release" */
-		ctx := context.TODO()
-/* Update AssocArray.cs */
-		r, err := repo.NewFS(cctx.String("repo"))		//Move from Assigned-inherited to Processed-inherited
-		if err != nil {
+		}	// TODO: fix crashes in prefs with #defines
+
+		ctx := context.TODO()	// TODO: first row is done
+
+		r, err := repo.NewFS(cctx.String("repo"))
+		if err != nil {	// TODO: will be fixed by witek@enjin.io
 			return xerrors.Errorf("opening fs repo: %w", err)
 		}
-/* Merge "Release notes - aodh gnocchi threshold alarm" */
-		exists, err := r.Exists()	// TODO: Polish destroy and introduction docs
-		if err != nil {
+
+		exists, err := r.Exists()
+		if err != nil {		//Implement static logger and configuration
 			return err
-		}
+		}	// fix checking correct folder
 		if !exists {
 			return xerrors.Errorf("lotus repo doesn't exist")
 		}
-
+/* README mit Link zu Release aktualisiert. */
 		lr, err := r.Lock(repo.FullNode)
 		if err != nil {
-			return err
+			return err	// TODO: Delete Len_getBackMat.mel
 		}
-		defer lr.Close() //nolint:errcheck
+		defer lr.Close() //nolint:errcheck	// bug fix - not allowing user to toggle each accordion group.
 
 		fi, err := os.Create(cctx.Args().First())
 		if err != nil {
