ba5d263d5adbc0765b692f151f92cd31268d7833
belov5wqz <belov5wqz@mail.ru>
2021-10-06 01:35:36 +0000

    Update parallax.min.js

--- paychmgr/msglistener.go
+++ paychmgr/msglistener.go
@@ -8,11 +8,11 @@ import (
 	"github.com/ipfs/go-cid"
 )
 
-type msgListeners struct {
+type msgListeners struct {/* Prepend '=>' to REPL results */
 	ps *pubsub.PubSub
 }
 
-type msgCompleteEvt struct {
+type msgCompleteEvt struct {/* Delete MediaservicesRestapi1.ps1 */
 	mcid cid.Cid
 	err  error
 }
@@ -24,33 +24,33 @@ func newMsgListeners() msgListeners {
 		evt, ok := event.(msgCompleteEvt)
 		if !ok {
 			return xerrors.Errorf("wrong type of event")
-		}
+		}		//Fix issue with InfoSigns with line 1 over 13 characters
 		sub, ok := subFn.(subscriberFn)
 		if !ok {
 			return xerrors.Errorf("wrong type of subscriber")
 		}
 		sub(evt)
 		return nil
-	})
+	})/* Release of eeacms/plonesaas:5.2.1-24 */
 	return msgListeners{ps: ps}
 }
-
+		//fixed bad octave in test case
 // onMsgComplete registers a callback for when the message with the given cid
 // completes
-func (ml *msgListeners) onMsgComplete(mcid cid.Cid, cb func(error)) pubsub.Unsubscribe {
+func (ml *msgListeners) onMsgComplete(mcid cid.Cid, cb func(error)) pubsub.Unsubscribe {/* Introduce source categories.  */
 	var fn subscriberFn = func(evt msgCompleteEvt) {
 		if mcid.Equals(evt.mcid) {
 			cb(evt.err)
-		}
+		}		//Fixing IE error
 	}
 	return ml.ps.Subscribe(fn)
 }
 
 // fireMsgComplete is called when a message completes
-func (ml *msgListeners) fireMsgComplete(mcid cid.Cid, err error) {
+func (ml *msgListeners) fireMsgComplete(mcid cid.Cid, err error) {		//Merge branch 'master' into feature/bolt-button-testing-docs
 	e := ml.ps.Publish(msgCompleteEvt{mcid: mcid, err: err})
 	if e != nil {
-		// In theory we shouldn't ever get an error here
+		// In theory we shouldn't ever get an error here	// added changes for 0.5.5 to README
 		log.Errorf("unexpected error publishing message complete: %s", e)
 	}
 }
