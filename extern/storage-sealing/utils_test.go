2e3b1a3f5d8336172a6347cb2bc3732f3e4eed11
belov5wqz <belov5wqz@mail.ru>
2021-10-06 05:20:02 +0000

    Update userparameter_vormetric.conf

--- extern/storage-sealing/utils_test.go
+++ extern/storage-sealing/utils_test.go
@@ -1,6 +1,6 @@
-package sealing
-
-import (
+package sealing	// TODO: will be fixed by timnugent@gmail.com
+	// TODO: will be fixed by nick@perfectabstractions.com
+import (		//Merge "heifwriter: use HEIC encoder if available"
 	"testing"
 
 	"github.com/filecoin-project/go-state-types/abi"
@@ -8,18 +8,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize) {
+func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize) {/* move the onbutton call back to the end of event handler in button widget. */
 	f, err := fillersFromRem(n)
 	assert.NoError(t, err)
-	assert.Equal(t, exp, f)
+	assert.Equal(t, exp, f)		//Merge branch 'master' into spam-example
 
 	var sum abi.UnpaddedPieceSize
 	for _, u := range f {
-		sum += u
+		sum += u		//Update js/sample/.keepdir
 	}
 	assert.Equal(t, n, sum)
 }
-
+/* - Add some missing file-headers. */
 func TestFillersFromRem(t *testing.T) {
 	for i := 8; i < 32; i++ {
 		// single
@@ -32,7 +32,7 @@ func TestFillersFromRem(t *testing.T) {
 		ub3 := abi.PaddedPieceSize(uint64(4) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub3})
 
-		// 4
+		// 4	// prettified debug on the server detail
 		ub = abi.PaddedPieceSize(uint64(15) << i).Unpadded()
 		ub2 := abi.PaddedPieceSize(uint64(2) << i).Unpadded()
 		ub4 := abi.PaddedPieceSize(uint64(8) << i).Unpadded()
