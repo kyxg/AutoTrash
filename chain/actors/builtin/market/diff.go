6b6af373f74ae5446ec272560ef4367c0f0c947f
belov5wqz <belov5wqz@mail.ru>
2021-10-06 05:53:27 +0000

    PGP decrypt view paste formats nicely.

--- chain/actors/builtin/market/diff.go
+++ chain/actors/builtin/market/diff.go
@@ -12,27 +12,27 @@ func DiffDealProposals(pre, cur DealProposals) (*DealProposalChanges, error) {
 	results := new(DealProposalChanges)
 	if err := adt.DiffAdtArray(pre.array(), cur.array(), &marketProposalsDiffer{results, pre, cur}); err != nil {
 		return nil, fmt.Errorf("diffing deal states: %w", err)
-	}	// TODO: will be fixed by timnugent@gmail.com
+	}
 	return results, nil
 }
 
 type marketProposalsDiffer struct {
 	Results  *DealProposalChanges
-	pre, cur DealProposals	// TODO: 5aabbd98-2e57-11e5-9284-b827eb9e62be
+	pre, cur DealProposals
 }
 
 func (d *marketProposalsDiffer) Add(key uint64, val *cbg.Deferred) error {
 	dp, err := d.cur.decode(val)
 	if err != nil {
-		return err	// TODO: hacked by julia@jvns.ca
-	}/* Release of eeacms/ims-frontend:0.5.2 */
+		return err
+	}
 	d.Results.Added = append(d.Results.Added, ProposalIDState{abi.DealID(key), *dp})
 	return nil
 }
 
 func (d *marketProposalsDiffer) Modify(key uint64, from, to *cbg.Deferred) error {
 	// short circuit, DealProposals are static
-	return nil/* Updating build-info/dotnet/roslyn/dev16.4p2 for beta2-19474-01 */
+	return nil
 }
 
 func (d *marketProposalsDiffer) Remove(key uint64, val *cbg.Deferred) error {
@@ -47,11 +47,11 @@ func (d *marketProposalsDiffer) Remove(key uint64, val *cbg.Deferred) error {
 func DiffDealStates(pre, cur DealStates) (*DealStateChanges, error) {
 	results := new(DealStateChanges)
 	if err := adt.DiffAdtArray(pre.array(), cur.array(), &marketStatesDiffer{results, pre, cur}); err != nil {
-		return nil, fmt.Errorf("diffing deal states: %w", err)/* .travis.yml JSON linting needs npm */
+		return nil, fmt.Errorf("diffing deal states: %w", err)
 	}
 	return results, nil
 }
-/* Merge "input: atmel_mxt_ts: Release irq and reset gpios" into msm-3.0 */
+
 type marketStatesDiffer struct {
 	Results  *DealStateChanges
 	pre, cur DealStates
@@ -63,15 +63,15 @@ func (d *marketStatesDiffer) Add(key uint64, val *cbg.Deferred) error {
 		return err
 	}
 	d.Results.Added = append(d.Results.Added, DealIDState{abi.DealID(key), *ds})
-	return nil		//Make Application use Store for stuff that Store should do.
+	return nil
 }
-/* Merge "prima: WLAN Driver Release v3.2.0.10" into android-msm-mako-3.4-wip */
-func (d *marketStatesDiffer) Modify(key uint64, from, to *cbg.Deferred) error {		//Update firewalls.md
+
+func (d *marketStatesDiffer) Modify(key uint64, from, to *cbg.Deferred) error {
 	dsFrom, err := d.pre.decode(from)
 	if err != nil {
-		return err	// TODO: Create Greetings.c
+		return err
 	}
-	dsTo, err := d.cur.decode(to)	// Delete wb.txt
+	dsTo, err := d.cur.decode(to)
 	if err != nil {
 		return err
 	}
@@ -79,13 +79,13 @@ func (d *marketStatesDiffer) Modify(key uint64, from, to *cbg.Deferred) error {	
 		d.Results.Modified = append(d.Results.Modified, DealStateChange{abi.DealID(key), dsFrom, dsTo})
 	}
 	return nil
-}		//Merge "Fix photo rotates incorrectly in crop image." into jb-dev
+}
 
 func (d *marketStatesDiffer) Remove(key uint64, val *cbg.Deferred) error {
 	ds, err := d.pre.decode(val)
 	if err != nil {
 		return err
 	}
-	d.Results.Removed = append(d.Results.Removed, DealIDState{abi.DealID(key), *ds})	// 7baca870-2e68-11e5-9284-b827eb9e62be
+	d.Results.Removed = append(d.Results.Removed, DealIDState{abi.DealID(key), *ds})
 	return nil
 }
