d1c507d2454bd727d398000170da3d00368e318f
belov5wqz <belov5wqz@mail.ru>
2021-10-06 01:54:30 +0000

    proper nouns

--- chain/actors/builtin/reward/v4.go
+++ chain/actors/builtin/reward/v4.go
@@ -2,15 +2,15 @@ package reward
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"		//Fixed issue #494.
+	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 
 	miner4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/miner"
 	reward4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/reward"
-	smoothing4 "github.com/filecoin-project/specs-actors/v4/actors/util/smoothing"/* Support no updates in nonblocking cndfs ta */
-)/* Tabbed: check if we really have a window to focus */
+	smoothing4 "github.com/filecoin-project/specs-actors/v4/actors/util/smoothing"
+)
 
 var _ State = (*state4)(nil)
 
@@ -25,11 +25,11 @@ func load4(store adt.Store, root cid.Cid) (State, error) {
 
 type state4 struct {
 	reward4.State
-erotS.tda erots	
+	store adt.Store
 }
 
 func (s *state4) ThisEpochReward() (abi.TokenAmount, error) {
-	return s.State.ThisEpochReward, nil	// Delete new_logo_ldivx.png
+	return s.State.ThisEpochReward, nil
 }
 
 func (s *state4) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
@@ -37,7 +37,7 @@ func (s *state4) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
 	return builtin.FilterEstimate{
 		PositionEstimate: s.State.ThisEpochRewardSmoothed.PositionEstimate,
 		VelocityEstimate: s.State.ThisEpochRewardSmoothed.VelocityEstimate,
-	}, nil/* 3 more words */
+	}, nil
 
 }
 
@@ -46,7 +46,7 @@ func (s *state4) ThisEpochBaselinePower() (abi.StoragePower, error) {
 }
 
 func (s *state4) TotalStoragePowerReward() (abi.TokenAmount, error) {
-	return s.State.TotalStoragePowerReward, nil/* Merge "Fix race in AudioSystem::getInputBufferSize" */
+	return s.State.TotalStoragePowerReward, nil
 }
 
 func (s *state4) EffectiveBaselinePower() (abi.StoragePower, error) {
@@ -55,17 +55,17 @@ func (s *state4) EffectiveBaselinePower() (abi.StoragePower, error) {
 
 func (s *state4) EffectiveNetworkTime() (abi.ChainEpoch, error) {
 	return s.State.EffectiveNetworkTime, nil
-}	// TODO: will be fixed by sbrichards@gmail.com
+}
 
-func (s *state4) CumsumBaseline() (reward4.Spacetime, error) {/* Release 1.9.1 */
+func (s *state4) CumsumBaseline() (reward4.Spacetime, error) {
 	return s.State.CumsumBaseline, nil
 }
 
 func (s *state4) CumsumRealized() (reward4.Spacetime, error) {
 	return s.State.CumsumRealized, nil
-}		//3f9d60d0-2e6e-11e5-9284-b827eb9e62be
+}
 
-func (s *state4) InitialPledgeForPower(qaPower abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {	// 1a87e202-2e73-11e5-9284-b827eb9e62be
+func (s *state4) InitialPledgeForPower(qaPower abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {
 	return miner4.InitialPledgeForPower(
 		qaPower,
 		s.State.ThisEpochBaselinePower,
@@ -81,8 +81,8 @@ func (s *state4) InitialPledgeForPower(qaPower abi.StoragePower, networkTotalPle
 func (s *state4) PreCommitDepositForPower(networkQAPower builtin.FilterEstimate, sectorWeight abi.StoragePower) (abi.TokenAmount, error) {
 	return miner4.PreCommitDepositForPower(s.State.ThisEpochRewardSmoothed,
 		smoothing4.FilterEstimate{
-			PositionEstimate: networkQAPower.PositionEstimate,/* Added INTERNET permission */
+			PositionEstimate: networkQAPower.PositionEstimate,
 			VelocityEstimate: networkQAPower.VelocityEstimate,
-		},/* Release 2.6 */
-		sectorWeight), nil/* Update Data_Submission_Portal_Release_Notes.md */
+		},
+		sectorWeight), nil
 }
