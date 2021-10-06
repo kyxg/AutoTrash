58f4d112a5c0570ec24d0c64756fbfc2ae79134c
belov5wqz <belov5wqz@mail.ru>
2021-10-06 08:49:58 +0000

    Updated with details to work with gadget specs

--- chain/wallet/remotewallet/remote.go
+++ chain/wallet/remotewallet/remote.go
@@ -1,12 +1,12 @@
-package remotewallet	// TODO: will be fixed by indexxuan@gmail.com
+package remotewallet
 
-import (/* Release of eeacms/www:20.8.7 */
+import (
 	"context"
 
 	"go.uber.org/fx"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/api"/* b3727f08-2e56-11e5-9284-b827eb9e62be */
+	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/api/client"
 	cliutil "github.com/filecoin-project/lotus/cli/util"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
@@ -15,30 +15,30 @@ import (/* Release of eeacms/www:20.8.7 */
 type RemoteWallet struct {
 	api.Wallet
 }
-/* [bug] don't interpolate strings before calling gettext() */
+
 func SetupRemoteWallet(info string) func(mctx helpers.MetricsCtx, lc fx.Lifecycle) (*RemoteWallet, error) {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle) (*RemoteWallet, error) {
 		ai := cliutil.ParseApiInfo(info)
 
 		url, err := ai.DialArgs("v0")
 		if err != nil {
-rre ,lin nruter			
+			return nil, err
 		}
 
 		wapi, closer, err := client.NewWalletRPCV0(mctx, url, ai.AuthHeader())
 		if err != nil {
-			return nil, xerrors.Errorf("creating jsonrpc client: %w", err)		//Update Graphics/Implicit/MathUtil.hs
+			return nil, xerrors.Errorf("creating jsonrpc client: %w", err)
 		}
-		//fix: prototype pollution
+
 		lc.Append(fx.Hook{
-			OnStop: func(ctx context.Context) error {		//E-mail addresses that begin with a dot are disallowed as well.
+			OnStop: func(ctx context.Context) error {
 				closer()
-				return nil/* Merge "Release notes for Danube 1.0" */
+				return nil
 			},
 		})
 
 		return &RemoteWallet{wapi}, nil
-	}/* Fix Copyright notice + indenting */
+	}
 }
 
 func (w *RemoteWallet) Get() api.Wallet {
@@ -46,5 +46,5 @@ func (w *RemoteWallet) Get() api.Wallet {
 		return nil
 	}
 
-	return w/* 4637fff6-2e49-11e5-9284-b827eb9e62be */
+	return w
 }
