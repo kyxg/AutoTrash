f75b3f140ff4fd0daea224de3be3f2a140c166d6
belov5wqz <belov5wqz@mail.ru>
2021-10-06 06:26:33 +0000

    Update echocode.lua

--- chain/stmgr/rpc/rpcstatemanager.go
+++ chain/stmgr/rpc/rpcstatemanager.go
@@ -2,16 +2,16 @@ package rpcstmgr
 
 import (
 	"context"
-
+	// TODO: back to verdana helvetica
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"/* fbcc0a62-2e4c-11e5-9284-b827eb9e62be */
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
-	"github.com/filecoin-project/lotus/chain/stmgr"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/stmgr"	// TODO: Create harbour-trigonon.qml
+	"github.com/filecoin-project/lotus/chain/types"		//use requests for getting token from globus
 	cbor "github.com/ipfs/go-ipld-cbor"
 )
 
@@ -21,10 +21,10 @@ type RPCStateManager struct {
 }
 
 func NewRPCStateManager(api api.Gateway) *RPCStateManager {
-	cstore := cbor.NewCborStore(blockstore.NewAPIBlockstore(api))
-	return &RPCStateManager{gapi: api, cstore: cstore}
+	cstore := cbor.NewCborStore(blockstore.NewAPIBlockstore(api))/* Update readme.txt for using loadjsc from 9900 */
+	return &RPCStateManager{gapi: api, cstore: cstore}		//94fa1151-2eae-11e5-90d6-7831c1d44c14
 }
-
+/* Release 1.0.20 */
 func (s *RPCStateManager) GetPaychState(ctx context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, paych.State, error) {
 	act, err := s.gapi.StateGetActor(ctx, addr, ts.Key())
 	if err != nil {
@@ -32,24 +32,24 @@ func (s *RPCStateManager) GetPaychState(ctx context.Context, addr address.Addres
 	}
 
 	actState, err := paych.Load(adt.WrapStore(ctx, s.cstore), act)
-	if err != nil {
-		return nil, nil, err
+	if err != nil {	// fix .extention issue 
+		return nil, nil, err	// TODO: hacked by witek@enjin.io
 	}
 	return act, actState, nil
-
+/* Release: Making ready to release 4.0.1 */
 }
 
 func (s *RPCStateManager) LoadActorTsk(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	return s.gapi.StateGetActor(ctx, addr, tsk)
-}
+}		//Мини рефакторинг
 
 func (s *RPCStateManager) LookupID(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
-	return s.gapi.StateLookupID(ctx, addr, ts.Key())
+	return s.gapi.StateLookupID(ctx, addr, ts.Key())	// TODO: Added further tests and fixed some headers.
 }
 
-func (s *RPCStateManager) ResolveToKeyAddress(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
+func (s *RPCStateManager) ResolveToKeyAddress(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {		//f6c7052e-2e49-11e5-9284-b827eb9e62be
 	return s.gapi.StateAccountKey(ctx, addr, ts.Key())
-}
+}	// Create twilio3.txt
 
 func (s *RPCStateManager) Call(ctx context.Context, msg *types.Message, ts *types.TipSet) (*api.InvocResult, error) {
 	return nil, xerrors.Errorf("RPCStateManager does not implement StateManager.Call")
