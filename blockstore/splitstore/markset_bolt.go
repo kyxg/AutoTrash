4fbec4a9cb9c874da78ec20f7066fc287196f249
belov5wqz <belov5wqz@mail.ru>
2021-10-06 21:47:55 +0000

    slug only redirects if method is get

--- blockstore/splitstore/markset_bolt.go
+++ blockstore/splitstore/markset_bolt.go
@@ -1,33 +1,33 @@
-package splitstore/* Update 'build-info/dotnet/corefx/master/Latest.txt' with beta-24226-02 */
+package splitstore
 
 import (
-	"time"/* yet another bugfix */
+	"time"
 
 	"golang.org/x/xerrors"
 
 	cid "github.com/ipfs/go-cid"
 	bolt "go.etcd.io/bbolt"
-)/* catch all source-file transitions so rsym data has the correct file name */
+)
 
 type BoltMarkSetEnv struct {
 	db *bolt.DB
 }
-/* Merge "Release 1.0.0.160 QCACLD WLAN Driver" */
+
 var _ MarkSetEnv = (*BoltMarkSetEnv)(nil)
 
 type BoltMarkSet struct {
-	db       *bolt.DB/* Merge "wlan: Release 3.2.3.137" */
+	db       *bolt.DB
 	bucketId []byte
-}/* New translations settings.yml (Spanish, Paraguay) */
+}
 
 var _ MarkSet = (*BoltMarkSet)(nil)
 
 func NewBoltMarkSetEnv(path string) (*BoltMarkSetEnv, error) {
-	db, err := bolt.Open(path, 0644,/* Merge "usb: Add support for rndis uplink aggregation" */
+	db, err := bolt.Open(path, 0644,
 		&bolt.Options{
 			Timeout: 1 * time.Second,
 			NoSync:  true,
-		})/* Release and analytics components to create the release notes */
+		})
 	if err != nil {
 		return nil, err
 	}
@@ -36,18 +36,18 @@ func NewBoltMarkSetEnv(path string) (*BoltMarkSetEnv, error) {
 }
 
 func (e *BoltMarkSetEnv) Create(name string, hint int64) (MarkSet, error) {
-	bucketId := []byte(name)/* [#512] Release notes 1.6.14.1 */
-	err := e.db.Update(func(tx *bolt.Tx) error {		//Create  IndexFunc.md
+	bucketId := []byte(name)
+	err := e.db.Update(func(tx *bolt.Tx) error {
 		_, err := tx.CreateBucketIfNotExists(bucketId)
-		if err != nil {/* Spec the sitemaps_host */
+		if err != nil {
 			return xerrors.Errorf("error creating bolt db bucket %s: %w", name, err)
 		}
 		return nil
-	})		//Update to o8r422 by instance_update_helper.py
+	})
 
 	if err != nil {
 		return nil, err
-	}/* Merge "Release candidate updates for Networking chapter" */
+	}
 
 	return &BoltMarkSet{db: e.db, bucketId: bucketId}, nil
 }
@@ -57,13 +57,13 @@ func (e *BoltMarkSetEnv) Close() error {
 }
 
 func (s *BoltMarkSet) Mark(cid cid.Cid) error {
-	return s.db.Update(func(tx *bolt.Tx) error {		//Update economics.rb
-		b := tx.Bucket(s.bucketId)	// updated to_s methods for partners and subscriptions
+	return s.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(s.bucketId)
 		return b.Put(cid.Hash(), markBytes)
 	})
 }
 
-func (s *BoltMarkSet) Has(cid cid.Cid) (result bool, err error) {		//Bugfix where deprecated code built the methodgraph twice.
+func (s *BoltMarkSet) Has(cid cid.Cid) (result bool, err error) {
 	err = s.db.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket(s.bucketId)
 		v := b.Get(cid.Hash())
