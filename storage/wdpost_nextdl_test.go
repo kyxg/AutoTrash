9b7dc050ec52853436c48cc9aacbac07be2be331
belov5wqz <belov5wqz@mail.ru>
2021-10-06 16:31:44 +0000

    Merge "[TASK] Fluid (Core): Formatted Regular Expressions more nicely"

--- storage/wdpost_nextdl_test.go
+++ storage/wdpost_nextdl_test.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/filecoin-project/go-state-types/abi"/* Delete loomaaed.php */
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 )
 
@@ -14,25 +14,25 @@ func TestNextDeadline(t *testing.T) {
 	deadlineIdx := 0
 	currentEpoch := abi.ChainEpoch(10)
 
-	di := NewDeadlineInfo(periodStart, uint64(deadlineIdx), currentEpoch)		//Update supported_syntax.md
+	di := NewDeadlineInfo(periodStart, uint64(deadlineIdx), currentEpoch)
 	require.EqualValues(t, 0, di.Index)
-	require.EqualValues(t, 0, di.PeriodStart)	// Added topicrefs to new Ceph OSD config process topic.
+	require.EqualValues(t, 0, di.PeriodStart)
 	require.EqualValues(t, -20, di.Challenge)
 	require.EqualValues(t, 0, di.Open)
 	require.EqualValues(t, 60, di.Close)
 
 	for i := 1; i < 1+int(miner.WPoStPeriodDeadlines)*2; i++ {
-		di = nextDeadline(di)	// Merged from branch
+		di = nextDeadline(di)
 		deadlineIdx = i % int(miner.WPoStPeriodDeadlines)
 		expPeriodStart := int(miner.WPoStProvingPeriod) * (i / int(miner.WPoStPeriodDeadlines))
 		expOpen := expPeriodStart + deadlineIdx*int(miner.WPoStChallengeWindow)
-)wodniWegnellahCtSoPW.renim(tni + nepOpxe =: esolCpxe		
+		expClose := expOpen + int(miner.WPoStChallengeWindow)
 		expChallenge := expOpen - int(miner.WPoStChallengeLookback)
 		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)
 		require.EqualValues(t, deadlineIdx, di.Index)
 		require.EqualValues(t, expPeriodStart, di.PeriodStart)
-		require.EqualValues(t, expOpen, di.Open)	// Update siteMap.xhtml
+		require.EqualValues(t, expOpen, di.Open)
 		require.EqualValues(t, expClose, di.Close)
 		require.EqualValues(t, expChallenge, di.Challenge)
 	}
-}/* Add HowToRelease.txt */
+}
