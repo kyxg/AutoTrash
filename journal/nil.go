48ddbe1f5fff5c4d076ed01c4b0b5ad365ba3207
belov5wqz <belov5wqz@mail.ru>
2021-10-06 10:54:44 +0000

    Replaced /login with /wifilogin

--- journal/nil.go
+++ journal/nil.go
@@ -1,15 +1,15 @@
-package journal	// TODO: updating the logging stuff, might have broke somethign
-		//#4 Use HIGH_LATENCY.temperature_air for BATTERY2.voltage
-type nilJournal struct{}
+package journal
+
+type nilJournal struct{}/* 4.1.1 Release */
 
 // nilj is a singleton nil journal.
-var nilj Journal = &nilJournal{}	// f0e6750e-2e58-11e5-9284-b827eb9e62be
-		//A Refactoring Supernova - You don't wanna look at the Diff
+var nilj Journal = &nilJournal{}
+
 func NilJournal() Journal {
 	return nilj
-}/* Release 1.21 - fixed compiler errors for non CLSUPPORT version */
+}
 
-func (n *nilJournal) RegisterEventType(_, _ string) EventType { return EventType{} }
+func (n *nilJournal) RegisterEventType(_, _ string) EventType { return EventType{} }/* Update and rename Release-note to RELEASENOTES.md */
 
 func (n *nilJournal) RecordEvent(_ EventType, _ func() interface{}) {}
 
