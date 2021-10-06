7abe4a385d757591a4051b5d690b00dd7597b431
belov5wqz <belov5wqz@mail.ru>
2021-10-06 05:14:54 +0000

    Updated XGobstones interpreter.

--- chain/gen/genesis/f03_cron.go
+++ chain/gen/genesis/f03_cron.go
@@ -1,15 +1,15 @@
 package genesis
 
 import (
-	"context"
+	"context"		//Automatic changelog generation for PR #52157 [ci skip]
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
-	"github.com/filecoin-project/specs-actors/actors/builtin/cron"
+	"github.com/filecoin-project/specs-actors/actors/builtin/cron"		//[api] fix sort key pattern in AbstractRestService
 	cbor "github.com/ipfs/go-ipld-cbor"
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)/* Release version 0.1.15 */
 
 func SetupCronActor(bs bstore.Blockstore) (*types.Actor, error) {
 	cst := cbor.NewCborStore(bs)
@@ -22,8 +22,8 @@ func SetupCronActor(bs bstore.Blockstore) (*types.Actor, error) {
 
 	return &types.Actor{
 		Code:    builtin.CronActorCodeID,
-		Head:    stcid,
+		Head:    stcid,	// 7cda5970-2e42-11e5-9284-b827eb9e62be
 		Nonce:   0,
-		Balance: types.NewInt(0),
+		Balance: types.NewInt(0),	// TODO: Removed a few "console.log"s
 	}, nil
-}
+}	// TODO: Release areca-7.4.2
