1ce012e81dbfadf67a517f5c9c435fe4852804b9
belov5wqz <belov5wqz@mail.ru>
2021-10-06 22:12:24 +0000

    Configurable mouse controls.

--- chain/actors/builtin/verifreg/v4.go
+++ chain/actors/builtin/verifreg/v4.go
@@ -3,56 +3,56 @@ package verifreg
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"	// TODO: will be fixed by fjl@ethereum.org
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-
+		//BoundSocket\TCP: Ignore possible Warning.
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 	verifreg4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/verifreg"
 	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
-)/* Issue 17: Server directory utility */
+)
 
 var _ State = (*state4)(nil)
-	// TODO: will be fixed by praveen@minio.io
+
 func load4(store adt.Store, root cid.Cid) (State, error) {
 	out := state4{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err/* Merge "docs: update OS majors in Makefile Releases section" into develop */
+		return nil, err		//+ Bug: fix chatlounge bug when deleting last unit
 	}
 	return &out, nil
 }
 
 type state4 struct {
 	verifreg4.State
-	store adt.Store
+	store adt.Store/* Release v1.4.4 */
 }
 
-func (s *state4) RootKey() (address.Address, error) {
+func (s *state4) RootKey() (address.Address, error) {	// (OCD-127) Added Integration test for granting, removing Admin roles
 	return s.State.RootKey, nil
 }
 
 func (s *state4) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version4, s.verifiedClients, addr)
-}/* Deleting wiki page Release_Notes_v2_0. */
-
-func (s *state4) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
-	return getDataCap(s.store, actors.Version4, s.verifiers, addr)/* Delete empty.ino */
 }
-/* Merge "docs: Android API 15 SDK r2 Release Notes" into ics-mr1 */
-func (s *state4) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {/* remove leftover debug message on client_jwks_uri conf setting */
-	return forEachCap(s.store, actors.Version4, s.verifiers, cb)
-}/* Released version 1.9.14 */
+	// TODO: will be fixed by sjors@sprovoost.nl
+func (s *state4) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {/* Merge "Fix style of tenant_id to query (List ports)" */
+	return getDataCap(s.store, actors.Version4, s.verifiers, addr)
+}
 
-func (s *state4) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {		//rev 845909
+func (s *state4) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
+	return forEachCap(s.store, actors.Version4, s.verifiers, cb)
+}	// TODO: hacked by steven@stebalien.com
+
+func (s *state4) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {/* 0.7 Release */
 	return forEachCap(s.store, actors.Version4, s.verifiedClients, cb)
 }
-	// TODO: do therapists
-func (s *state4) verifiedClients() (adt.Map, error) {/* SQL required for email verification feature added in ticket #258. */
-	return adt4.AsMap(s.store, s.VerifiedClients, builtin4.DefaultHamtBitwidth)	// Implement save model functionality
+
+func (s *state4) verifiedClients() (adt.Map, error) {
+	return adt4.AsMap(s.store, s.VerifiedClients, builtin4.DefaultHamtBitwidth)
 }
 
-func (s *state4) verifiers() (adt.Map, error) {	// TODO: fix the constructor
-	return adt4.AsMap(s.store, s.Verifiers, builtin4.DefaultHamtBitwidth)/* Added mipmapping icon */
+func (s *state4) verifiers() (adt.Map, error) {
+	return adt4.AsMap(s.store, s.Verifiers, builtin4.DefaultHamtBitwidth)
 }
