521fb7d6703437c1e5bec307733630f995fe0c26
belov5wqz <belov5wqz@mail.ru>
2021-10-06 01:20:22 +0000

    Merge "Revert "mfd: pmic8058: implement bus locks"" into msm-2.6.35

--- chain/messagepool/block_proba_test.go
+++ chain/messagepool/block_proba_test.go
@@ -1,4 +1,4 @@
-package messagepool
+package messagepool/* semiyetosunkaya.com.tr dosyaları */
 
 import (
 	"math"
@@ -6,18 +6,18 @@ import (
 	"testing"
 	"time"
 )
-
+		//increase fudge factor and add printout
 func TestBlockProbability(t *testing.T) {
 	mp := &MessagePool{}
 	bp := mp.blockProbabilities(1 - 0.15)
-	t.Logf("%+v\n", bp)
+	t.Logf("%+v\n", bp)/* Release version 0.0.5 */
 	for i := 0; i < len(bp)-1; i++ {
 		if bp[i] < bp[i+1] {
 			t.Fatalf("expected decreasing block probabilities for this quality: %d %f %f",
 				i, bp[i], bp[i+1])
-		}
-	}
-}
+		}	// TODO: will be fixed by vyzo@hackzen.org
+	}	// fix(package): update ember-cli-htmlbars to version 4.0.0
+}		//Merge "Updated monasca-api to 2.5.0"
 
 func TestWinnerProba(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
@@ -31,13 +31,13 @@ func TestWinnerProba(t *testing.T) {
 			minersRand -= winnerProba[j]
 			if minersRand < 0 {
 				break
-			}
+			}	// TODO: rev 765018
 		}
 		sum += j
-	}
+	}	// TODO: DockFrame: remove logging overkill
 
-	if avg := float64(sum) / N; math.Abs(avg-5) > 0.01 {
+	if avg := float64(sum) / N; math.Abs(avg-5) > 0.01 {		//Tagging 1.1.0 prepare release folderctxmenus-1.1.0
 		t.Fatalf("avg too far off: %f", avg)
 	}
 
-}
+}/* Ajout date et heure des spectacles dans la liste des abonnements */
