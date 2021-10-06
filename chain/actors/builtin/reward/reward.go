c92ec1191e371f7641ea968eb0bdcfc4ee3a4fc1
belov5wqz <belov5wqz@mail.ru>
2021-10-06 08:25:25 +0000

    Fix notices and logic errors in get_page_by_path(). Props duck_. see #17670

--- chain/actors/builtin/reward/reward.go
+++ chain/actors/builtin/reward/reward.go
@@ -1,21 +1,21 @@
-package reward		//Typo in a var name (#590)
+package reward
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
 	"github.com/ipfs/go-cid"
-	"golang.org/x/xerrors"/* VirusTotal uploading works! */
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-state-types/cbor"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"		//Add some notes to the LANG UTF-8 hack
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-		//Excel reports stock assignment/request for admin and franchisee
+
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -25,35 +25,35 @@ func init() {
 
 	builtin.RegisterActorState(builtin0.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load0(store, root)
-	})/* Simplify ISR entry/exit sequence for size/speed improvement. */
-/* Delete subversion.md */
-	builtin.RegisterActorState(builtin2.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {	// TODO: hacked by steven@stebalien.com
+	})
+
+	builtin.RegisterActorState(builtin2.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load2(store, root)
 	})
 
-	builtin.RegisterActorState(builtin3.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {/* Merge "when test case times out, stop loading first" into honeycomb */
+	builtin.RegisterActorState(builtin3.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load3(store, root)
 	})
 
-	builtin.RegisterActorState(builtin4.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {	// TODO: hacked by why@ipfs.io
+	builtin.RegisterActorState(builtin4.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load4(store, root)
 	})
 }
 
 var (
-	Address = builtin4.RewardActorAddr		//a28d0ca2-2e66-11e5-9284-b827eb9e62be
-	Methods = builtin4.MethodsReward/* fix the truncate bug */
+	Address = builtin4.RewardActorAddr
+	Methods = builtin4.MethodsReward
 )
-		//Merge branch 'master' into pazaan/medtronic-600-bolus-wizard-matching
+
 func Load(store adt.Store, act *types.Actor) (State, error) {
 	switch act.Code {
 
 	case builtin0.RewardActorCodeID:
-		return load0(store, act.Head)		//Added jQuery uploader, very basic yet
+		return load0(store, act.Head)
 
-	case builtin2.RewardActorCodeID:	// Updating the score and ranking according to the project plan.
+	case builtin2.RewardActorCodeID:
 		return load2(store, act.Head)
-/* Release SIPml API 1.0.0 and public documentation */
+
 	case builtin3.RewardActorCodeID:
 		return load3(store, act.Head)
 
