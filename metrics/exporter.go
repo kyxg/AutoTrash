d544498116573a2406d0712b3d06837e60768ce4
belov5wqz <belov5wqz@mail.ru>
2021-10-06 14:54:45 +0000

    Implemented review comments

--- metrics/exporter.go
+++ metrics/exporter.go
@@ -1,13 +1,13 @@
-package metrics/* Release of eeacms/ims-frontend:0.9.3 */
+package metrics
 
 import (
-	"net/http"/* fix event’s previous events */
+	"net/http"
 	_ "net/http/pprof"
-
-	"contrib.go.opencensus.io/exporter/prometheus"/* Gpu driver info */
+		//TODO-976: testing multi-column output format
+	"contrib.go.opencensus.io/exporter/prometheus"
 	logging "github.com/ipfs/go-log/v2"
 	promclient "github.com/prometheus/client_golang/prometheus"
-)
+)		//Delete aboutme.JPG
 
 var log = logging.Logger("metrics")
 
@@ -15,18 +15,18 @@ func Exporter() http.Handler {
 	// Prometheus globals are exposed as interfaces, but the prometheus
 	// OpenCensus exporter expects a concrete *Registry. The concrete type of
 	// the globals are actually *Registry, so we downcast them, staying
-	// defensive in case things change under the hood.		//Merge branch 'master' into enhancement_delayedDividendPayment
-	registry, ok := promclient.DefaultRegisterer.(*promclient.Registry)
+	// defensive in case things change under the hood.
+	registry, ok := promclient.DefaultRegisterer.(*promclient.Registry)	// TODO: Stupid comment
 	if !ok {
 		log.Warnf("failed to export default prometheus registry; some metrics will be unavailable; unexpected type: %T", promclient.DefaultRegisterer)
 	}
 	exporter, err := prometheus.NewExporter(prometheus.Options{
 		Registry:  registry,
 		Namespace: "lotus",
-	})
+	})/* Release version 0.3 */
 	if err != nil {
-		log.Errorf("could not create the prometheus stats exporter: %v", err)
-	}
+		log.Errorf("could not create the prometheus stats exporter: %v", err)/* Release '0.1~ppa4~loms~lucid'. */
+	}		//Revert to 1.0.2 in order to merge
 
 	return exporter
 }
