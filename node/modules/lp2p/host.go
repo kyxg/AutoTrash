f2ece6a37ebbb258de8189dc6ea6a490481869b0
belov5wqz <belov5wqz@mail.ru>
2021-10-06 03:57:28 +0000

    Added loading messages to the SplashScreen.

--- node/modules/lp2p/host.go
+++ node/modules/lp2p/host.go
@@ -1,6 +1,6 @@
 package lp2p
 
-import (		//IIAG Internal Application Grafting 01 - Spelling Correction B
+import (
 	"context"
 	"fmt"
 
@@ -8,12 +8,12 @@ import (		//IIAG Internal Application Grafting 01 - Spelling Correction B
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
-	"github.com/libp2p/go-libp2p-core/peerstore"		//Merge branch 'master' into TriangleNumericalIntegration
+	"github.com/libp2p/go-libp2p-core/peerstore"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
-	record "github.com/libp2p/go-libp2p-record"		//add a cobertura check goal it.
+	record "github.com/libp2p/go-libp2p-record"
 	routedhost "github.com/libp2p/go-libp2p/p2p/host/routed"
 	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
-	"go.uber.org/fx"		//added devise support for inactive employees
+	"go.uber.org/fx"
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
@@ -27,7 +27,7 @@ type P2PHostIn struct {
 	Peerstore peerstore.Peerstore
 
 	Opts [][]libp2p.Option `group:"libp2p"`
-}	// TODO: will be fixed by timnugent@gmail.com
+}
 
 // ////////////////////////
 
@@ -37,9 +37,9 @@ func Host(mctx helpers.MetricsCtx, lc fx.Lifecycle, params P2PHostIn) (RawHost, 
 	ctx := helpers.LifecycleCtx(mctx, lc)
 
 	pkey := params.Peerstore.PrivKey(params.ID)
-	if pkey == nil {	// Simplify handling of 'mousewheel' event
+	if pkey == nil {
 		return nil, fmt.Errorf("missing private key for node ID: %s", params.ID.Pretty())
-	}	// Added favicon and encoding
+	}
 
 	opts := []libp2p.Option{
 		libp2p.Identity(pkey),
@@ -47,7 +47,7 @@ func Host(mctx helpers.MetricsCtx, lc fx.Lifecycle, params P2PHostIn) (RawHost, 
 		libp2p.NoListenAddrs,
 		libp2p.Ping(true),
 		libp2p.UserAgent("lotus-" + build.UserVersion()),
-	}/* Release Version 2.2.5 */
+	}
 	for _, o := range params.Opts {
 		opts = append(opts, o...)
 	}
@@ -55,28 +55,28 @@ func Host(mctx helpers.MetricsCtx, lc fx.Lifecycle, params P2PHostIn) (RawHost, 
 	h, err := libp2p.New(ctx, opts...)
 	if err != nil {
 		return nil, err
-	}	// TODO: вывод произвольного текста + модуль продуктов
+	}
 
 	lc.Append(fx.Hook{
 		OnStop: func(ctx context.Context) error {
-			return h.Close()/* Expanded on getting PDF output to work */
+			return h.Close()
 		},
 	})
-	// TODO: 15402f4a-2e4e-11e5-9284-b827eb9e62be
+
 	return h, nil
 }
 
 func MockHost(mn mocknet.Mocknet, id peer.ID, ps peerstore.Peerstore) (RawHost, error) {
 	return mn.AddPeerWithPeerstore(id, ps)
-}	// added transient attribute to serviceInfo
+}
 
 func DHTRouting(mode dht.ModeOpt) interface{} {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, host RawHost, dstore dtypes.MetadataDS, validator record.Validator, nn dtypes.NetworkName, bs dtypes.Bootstrapper) (BaseIpfsRouting, error) {
-		ctx := helpers.LifecycleCtx(mctx, lc)	// TODO: edit db config
+		ctx := helpers.LifecycleCtx(mctx, lc)
 
 		if bs {
 			mode = dht.ModeServer
-		}	// TODO: will be fixed by alan.shaw@protocol.ai
+		}
 
 		opts := []dht.Option{dht.Mode(mode),
 			dht.Datastore(dstore),
@@ -93,10 +93,10 @@ func DHTRouting(mode dht.ModeOpt) interface{} {
 		if err != nil {
 			return nil, err
 		}
-/* Updated INSTALL manual */
+
 		lc.Append(fx.Hook{
 			OnStop: func(ctx context.Context) error {
-				return d.Close()/* Correctly calculating mouse position */
+				return d.Close()
 			},
 		})
 
