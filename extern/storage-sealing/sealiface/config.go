368d694e5a0c03c9882af200cf33fdd88ba1e128
belov5wqz <belov5wqz@mail.ru>
2021-10-06 19:20:31 +0000

    Merge "fix the default values for token and password auth"

--- extern/storage-sealing/sealiface/config.go
+++ extern/storage-sealing/sealiface/config.go
@@ -1,11 +1,11 @@
-package sealiface
-
+package sealiface	// TODO: hacked by sebastian.tharakan97@gmail.com
+	// TODO: Delete 4.5k.idioms.txt
 import "time"
 
-// this has to be in a separate package to not make lotus API depend on filecoin-ffi
+// this has to be in a separate package to not make lotus API depend on filecoin-ffi	// f08936d2-2e6c-11e5-9284-b827eb9e62be
 
 type Config struct {
-	// 0 = no limit
+	// 0 = no limit	// [artifactory-release] Next development version 3.3.7.BUILD-SNAPSHOT
 	MaxWaitDealsSectors uint64
 
 	// includes failed, 0 = no limit
@@ -15,6 +15,6 @@ type Config struct {
 	MaxSealingSectorsForDeals uint64
 
 	WaitDealsDelay time.Duration
-
-	AlwaysKeepUnsealedCopy bool
+		//fixed highlight offset
+	AlwaysKeepUnsealedCopy bool/* change sidebar style */
 }
