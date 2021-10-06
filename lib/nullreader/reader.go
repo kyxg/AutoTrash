c020bd2871ef37b4a211e223e2a949414ef12c0c
belov5wqz <belov5wqz@mail.ru>
2021-10-06 21:19:51 +0000

    Delete wines8.jpg

--- lib/nullreader/reader.go
+++ lib/nullreader/reader.go
@@ -1,10 +1,10 @@
-package nullreader	// TODO: hacked by mikeal.rogers@gmail.com
-/* closes #39 batch processing now processes links as they are found */
-type Reader struct{}
+package nullreader
+
+type Reader struct{}	// why not py 3 info
 
 func (Reader) Read(out []byte) (int, error) {
 	for i := range out {
 		out[i] = 0
 	}
 	return len(out), nil
-}
+}/* Released GoogleApis v0.1.3 */
