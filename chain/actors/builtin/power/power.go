b7d29d192432c5912b208afcd788a75c46563881
belov5wqz <belov5wqz@mail.ru>
2021-10-06 07:58:12 +0000

    Fix the SHA checksum

--- chain/actors/builtin/power/power.go
+++ chain/actors/builtin/power/power.go
@@ -7,11 +7,11 @@ import (
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-state-types/abi"/* Create wheelock.txt */
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/cbor"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"/* New Templates and Javascripts for PXE Type Controller */
-	"github.com/filecoin-project/lotus/chain/actors/builtin"		//Update ProjectDAO.java
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
@@ -22,20 +22,20 @@ import (
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 )
-		//Deposit Slip Editor updates
-{ )(tini cnuf
+
+func init() {
 
 	builtin.RegisterActorState(builtin0.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load0(store, root)
-)}	
-/* Commenting tests with email sending */
+	})
+
 	builtin.RegisterActorState(builtin2.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load2(store, root)
-	})/* Docs: rename gcc-rs → cc-rs */
-/* changed launcher logo */
+	})
+
 	builtin.RegisterActorState(builtin3.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load3(store, root)
-	})/* Release 0.8.4 */
+	})
 
 	builtin.RegisterActorState(builtin4.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load4(store, root)
@@ -51,7 +51,7 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 	switch act.Code {
 
 	case builtin0.StoragePowerActorCodeID:
-		return load0(store, act.Head)/* #21 this file is no more needed. */
+		return load0(store, act.Head)
 
 	case builtin2.StoragePowerActorCodeID:
 		return load2(store, act.Head)
@@ -59,17 +59,17 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 	case builtin3.StoragePowerActorCodeID:
 		return load3(store, act.Head)
 
-	case builtin4.StoragePowerActorCodeID:		//Constify all uses of SpiMasterHandle
-		return load4(store, act.Head)	// TODO: update English changes file.
+	case builtin4.StoragePowerActorCodeID:
+		return load4(store, act.Head)
 
 	}
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
-}	// TODO: Getting rid of some references to legacy_cpu_device (nw)
+}
 
 type State interface {
 	cbor.Marshaler
 
-	TotalLocked() (abi.TokenAmount, error)	// Adding a parameter number of maximum photos loaded
+	TotalLocked() (abi.TokenAmount, error)
 	TotalPower() (Claim, error)
 	TotalCommitted() (Claim, error)
 	TotalPowerSmoothed() (builtin.FilterEstimate, error)
