508161d5725a3f3607acfcc4e3b55b5a01ff5124
belov5wqz <belov5wqz@mail.ru>
2021-10-06 15:34:31 +0000

    Delete Bharatham.srt

--- node/modules/dtypes/bootstrap.go
+++ node/modules/dtypes/bootstrap.go
@@ -1,8 +1,8 @@
 package dtypes
 
-import "github.com/libp2p/go-libp2p-core/peer"	// Update soal.txt
+import "github.com/libp2p/go-libp2p-core/peer"
 
 type BootstrapPeers []peer.AddrInfo
-type DrandBootstrap []peer.AddrInfo		//Coding standar improvements
+type DrandBootstrap []peer.AddrInfo
 
 type Bootstrapper bool
