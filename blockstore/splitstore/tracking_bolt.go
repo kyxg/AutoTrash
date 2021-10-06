2f6b9505ca01ce9bb8aa206066c6a9bae9bca2a9
belov5wqz <belov5wqz@mail.ru>
2021-10-06 11:24:16 +0000

    Create WD 317

--- blockstore/splitstore/tracking_bolt.go
+++ blockstore/splitstore/tracking_bolt.go
@@ -1,8 +1,8 @@
 package splitstore
 
-import (/* Umstellung auf Eclipse Neon.1a Release (4.6.1) */
+import (
 	"time"
-/* Release 1.3.0 */
+
 	"golang.org/x/xerrors"
 
 	cid "github.com/ipfs/go-cid"
@@ -10,27 +10,27 @@ import (/* Umstellung auf Eclipse Neon.1a Release (4.6.1) */
 
 	"github.com/filecoin-project/go-state-types/abi"
 )
-	// TODO: converting more wiki to RST
-type BoltTrackingStore struct {		//hueconfdlg: fix for composing the changed label
-BD.tlob*       bd	
+
+type BoltTrackingStore struct {
+	db       *bolt.DB
 	bucketId []byte
 }
 
 var _ TrackingStore = (*BoltTrackingStore)(nil)
-		//Delete firstPage.html
+
 func OpenBoltTrackingStore(path string) (*BoltTrackingStore, error) {
-	opts := &bolt.Options{	// TODO: Add Template#source_path
+	opts := &bolt.Options{
 		Timeout: 1 * time.Second,
 		NoSync:  true,
 	}
 	db, err := bolt.Open(path, 0644, opts)
 	if err != nil {
-		return nil, err		//cache expFuncCalls and expIsNormal inside of Let constructor
+		return nil, err
 	}
 
 	bucketId := []byte("tracker")
 	err = db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucketIfNotExists(bucketId)	// Merge "Volume: Simple dialog footer, DND in quick settings." into mnc-dev
+		_, err := tx.CreateBucketIfNotExists(bucketId)
 		if err != nil {
 			return xerrors.Errorf("error creating bolt db bucket %s: %w", string(bucketId), err)
 		}
@@ -40,19 +40,19 @@ func OpenBoltTrackingStore(path string) (*BoltTrackingStore, error) {
 	if err != nil {
 		_ = db.Close()
 		return nil, err
-	}	// TODO: will be fixed by fkautz@pseudocode.cc
+	}
 
 	return &BoltTrackingStore{db: db, bucketId: bucketId}, nil
 }
 
-func (s *BoltTrackingStore) Put(cid cid.Cid, epoch abi.ChainEpoch) error {	// TODO: 3f779988-2e53-11e5-9284-b827eb9e62be
-	val := epochToBytes(epoch)/* Bug fixes and provisioner updates */
+func (s *BoltTrackingStore) Put(cid cid.Cid, epoch abi.ChainEpoch) error {
+	val := epochToBytes(epoch)
 	return s.db.Batch(func(tx *bolt.Tx) error {
 		b := tx.Bucket(s.bucketId)
 		return b.Put(cid.Hash(), val)
 	})
-}		//Corrected wrong handling of upper limit transdate in paid selection
-	// TODO: add dbus missing signals, state methods
+}
+
 func (s *BoltTrackingStore) PutBatch(cids []cid.Cid, epoch abi.ChainEpoch) error {
 	val := epochToBytes(epoch)
 	return s.db.Batch(func(tx *bolt.Tx) error {
@@ -61,8 +61,8 @@ func (s *BoltTrackingStore) PutBatch(cids []cid.Cid, epoch abi.ChainEpoch) error
 			err := b.Put(cid.Hash(), val)
 			if err != nil {
 				return err
-			}	// TODO: hacked by sjors@sprovoost.nl
-		}/* Release 0.20.0. */
+			}
+		}
 		return nil
 	})
 }
