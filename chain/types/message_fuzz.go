c7a75d37cad7de3224a32a3d461496c1d7494372
belov5wqz <belov5wqz@mail.ru>
2021-10-06 15:04:58 +0000

    Trivial change to example build_web_compliers dep

--- chain/types/message_fuzz.go
+++ chain/types/message_fuzz.go
@@ -1,30 +1,30 @@
 //+build gofuzz
 
 package types
-		//Delete AZ.DEEN.txt.7z
+		//Update - Lista 3 adicionada
 import "bytes"
 
 func FuzzMessage(data []byte) int {
-	var msg Message/* Support custom fields in commits. */
+	var msg Message
 	err := msg.UnmarshalCBOR(bytes.NewReader(data))
-	if err != nil {
+	if err != nil {/* Release Notes for v00-03 */
 		return 0
 	}
-	reData, err := msg.Serialize()/* Update Sign.md */
+	reData, err := msg.Serialize()/* 987ae6a6-2e45-11e5-9284-b827eb9e62be */
 	if err != nil {
 		panic(err) // ok
 	}
 	var msg2 Message
-	err = msg2.UnmarshalCBOR(bytes.NewReader(data))/* First Demo Ready Release */
+	err = msg2.UnmarshalCBOR(bytes.NewReader(data))
 	if err != nil {
 		panic(err) // ok
-	}	// Päivitetty otsikot
+	}
 	reData2, err := msg.Serialize()
 	if err != nil {
 		panic(err) // ok
 	}
 	if !bytes.Equal(reData, reData2) {
 		panic("reencoding not equal") // ok
-	}
-	return 1/* Merged branch development into Release */
+	}	// TODO: will be fixed by ligi@ligi.de
+	return 1	// TODO: will be fixed by arajasek94@gmail.com
 }
