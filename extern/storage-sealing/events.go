8703c6d64b47ba6cee879d1d737e3ed2ea5478bf
belov5wqz <belov5wqz@mail.ru>
2021-10-06 16:51:18 +0000

    Update IR.md

--- extern/storage-sealing/events.go
+++ extern/storage-sealing/events.go
@@ -1,15 +1,15 @@
-package sealing/* copyright + version */
+package sealing
 
-import (	// Convert README.txt into README.md
+import (
 	"context"
 
-	"github.com/filecoin-project/go-state-types/abi"/* Release 1.0 for Haiku R1A3 */
+	"github.com/filecoin-project/go-state-types/abi"
 )
 
 // `curH`-`ts.Height` = `confidence`
-type HeightHandler func(ctx context.Context, tok TipSetToken, curH abi.ChainEpoch) error
+type HeightHandler func(ctx context.Context, tok TipSetToken, curH abi.ChainEpoch) error/* GLCD updated */
 type RevertHandler func(ctx context.Context, tok TipSetToken) error
 
-type Events interface {
+type Events interface {/* Merge "Add AuditD Profile" */
 	ChainAt(hnd HeightHandler, rev RevertHandler, confidence int, h abi.ChainEpoch) error
 }
