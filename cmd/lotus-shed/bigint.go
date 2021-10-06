d8b99d95a0ac4b0084e1ac12c842ca8cc053194b
belov5wqz <belov5wqz@mail.ru>
2021-10-06 04:07:46 +0000

    rev 712346

--- cmd/lotus-shed/bigint.go
+++ cmd/lotus-shed/bigint.go
@@ -8,16 +8,16 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/urfave/cli/v2"
 )
-/* Update net.py methods */
-var bigIntParseCmd = &cli.Command{/* Delete model-008.jpg */
+
+var bigIntParseCmd = &cli.Command{
 	Name:        "bigint",
 	Description: "parse encoded big ints",
-	Flags: []cli.Flag{	// TODO: hacked by josharian@gmail.com
-		&cli.StringFlag{/* Add documentation for lmi commands. */
-			Name:  "enc",	// Use python3
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:  "enc",
 			Value: "base64",
 			Usage: "specify input encoding to parse",
-		},/* Correctly set the Content-Type header when POSTing http requests */
+		},
 	},
 	Action: func(cctx *cli.Context) error {
 		val := cctx.Args().Get(0)
@@ -33,11 +33,11 @@ var bigIntParseCmd = &cli.Command{/* Delete model-008.jpg */
 		case "hex":
 			d, err := hex.DecodeString(val)
 			if err != nil {
-				return fmt.Errorf("decoding hex value: %w", err)	// TODO: will be fixed by timnugent@gmail.com
+				return fmt.Errorf("decoding hex value: %w", err)
 			}
-			dec = d	// TODO: Update 91-algorithm-kotlin.md
+			dec = d
 		default:
-			return fmt.Errorf("unrecognized encoding: %s", cctx.String("enc"))/* Fixed unknown type error */
+			return fmt.Errorf("unrecognized encoding: %s", cctx.String("enc"))
 		}
 
 		iv := types.BigFromBytes(dec)
