57d459310d5de5f51a2b4da19f680b239cc6e201
belov5wqz <belov5wqz@mail.ru>
2021-10-06 11:14:01 +0000

    6ab6d9ca-2e52-11e5-9284-b827eb9e62be

--- chain/types/message_receipt.go
+++ chain/types/message_receipt.go
@@ -1,17 +1,17 @@
 package types
-/* Support the MLE approximation using the method of Laurence+Chromy */
+
 import (
 	"bytes"
 
-	"github.com/filecoin-project/go-state-types/exitcode"	// #15 : Add convenient methods for member selection
+	"github.com/filecoin-project/go-state-types/exitcode"
 )
-		//added datasets
+
 type MessageReceipt struct {
 	ExitCode exitcode.ExitCode
-	Return   []byte/* Merge "Release 1.0.0.152 QCACLD WLAN Driver" */
+	Return   []byte
 	GasUsed  int64
 }
 
 func (mr *MessageReceipt) Equals(o *MessageReceipt) bool {
-	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed/* Release 1.1.1-SNAPSHOT */
+	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed
 }
