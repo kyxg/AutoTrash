40cb37406e0a0ca4c20217b38b98ac136b2b2092
belov5wqz <belov5wqz@mail.ru>
2021-10-06 14:16:40 +0000

    echo was a bad choice, now uses more system specific stuff just to be sure

--- build/params_butterfly.go
+++ build/params_butterfly.go
@@ -2,19 +2,19 @@
 
 package build
 
-import (
+import (		//Create Example_invocations
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"		//Change support version information to FF 3.6.*
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	"github.com/ipfs/go-cid"
 )
-
+/* Create new file TODO Release_v0.1.3.txt, which contains the tasks for v0.1.3. */
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
 }
 
-const BootstrappersFile = "butterflynet.pi"
+const BootstrappersFile = "butterflynet.pi"/* Add operation reference from Wikipedia */
 const GenesisFile = "butterflynet.car"
 
 const UpgradeBreezeHeight = -1
@@ -23,11 +23,11 @@ const UpgradeSmokeHeight = -2
 const UpgradeIgnitionHeight = -3
 const UpgradeRefuelHeight = -4
 
-var UpgradeActorsV2Height = abi.ChainEpoch(30)
+var UpgradeActorsV2Height = abi.ChainEpoch(30)/* Make tinymceLoad function public */
 
-const UpgradeTapeHeight = 60
+const UpgradeTapeHeight = 60/* More code clean and new Release Notes */
 const UpgradeLiftoffHeight = -5
-const UpgradeKumquatHeight = 90
+const UpgradeKumquatHeight = 90/* Cookbok: fix broken relative link */
 const UpgradeCalicoHeight = 120
 const UpgradePersianHeight = 150
 const UpgradeClausHeight = 180
@@ -52,6 +52,6 @@ const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
 const PropagationDelaySecs = uint64(6)
 
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
-const BootstrapPeerThreshold = 2
+const BootstrapPeerThreshold = 2		//Legal mumbo-jumbo
 
 var WhitelistedBlock = cid.Undef
