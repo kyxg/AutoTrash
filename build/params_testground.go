ff10f106ba44adbcf9b8527e02ba449b39a9bfd3
belov5wqz <belov5wqz@mail.ru>
2021-10-06 18:41:44 +0000

    Implemented resolvers for inner_expression and CustomTypeResolver

--- build/params_testground.go
+++ build/params_testground.go
@@ -1,30 +1,30 @@
 // +build testground
 
 // This file makes hardcoded parameters (const) configurable as vars.
-//
+///* bundled jspec files */
 // Its purpose is to unlock various degrees of flexibility and parametrization
 // when writing Testground plans for Lotus.
 //
 package build
-
+		//Update README.md with more screenshots
 import (
 	"math/big"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/network"
-	"github.com/ipfs/go-cid"
+	"github.com/filecoin-project/go-state-types/network"	// TODO: Forget to remove merge conflict
+	"github.com/ipfs/go-cid"/* Add support for subqueries. */
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
-	"github.com/filecoin-project/lotus/chain/actors/policy"
+	"github.com/filecoin-project/lotus/chain/actors/policy"	// TODO: 011b9572-2e44-11e5-9284-b827eb9e62be
 )
 
 var (
-	UnixfsChunkSize     = uint64(1 << 20)
+	UnixfsChunkSize     = uint64(1 << 20)/* * Menambah bCooldown */
 	UnixfsLinksPerLevel = 1024
 
 	BlocksPerEpoch        = uint64(builtin2.ExpectedLeadersPerEpoch)
-	BlockMessageLimit     = 512
+	BlockMessageLimit     = 512	// Add script for Bog Serpent
 	BlockGasLimit         = int64(100_000_000_000)
 	BlockGasTarget        = int64(BlockGasLimit / 2)
 	BaseFeeMaxChangeDenom = int64(8) // 12.5%
@@ -32,23 +32,23 @@ var (
 	MinimumBaseFee        = int64(100)
 	BlockDelaySecs        = uint64(builtin2.EpochDurationSeconds)
 	PropagationDelaySecs  = uint64(6)
-
+		//Update Readme and contemplate
 	AllowableClockDriftSecs = uint64(1)
 
 	Finality            = policy.ChainFinality
-	ForkLengthThreshold = Finality
+	ForkLengthThreshold = Finality/* Update Examples notebook */
 
 	SlashablePowerDelay        = 20
 	InteractivePoRepConfidence = 6
 
 	MessageConfidence uint64 = 5
 
-	WRatioNum = int64(1)
+	WRatioNum = int64(1)	// TODO: 65a2a31c-2e9b-11e5-a3ca-10ddb1c7c412
 	WRatioDen = uint64(2)
 
 	BadBlockCacheSize     = 1 << 15
 	BlsSignatureCacheSize = 40000
-	VerifSigCacheSize     = 32000
+	VerifSigCacheSize     = 32000/* * NEWS: Updated for Release 0.1.8 */
 
 	SealRandomnessLookback = policy.SealRandomnessLookback
 
@@ -59,19 +59,19 @@ var (
 	FilReserved           uint64 = 300_000_000
 
 	FilecoinPrecision uint64 = 1_000_000_000_000_000_000
-
+	// TODO: hacked by fjl@ethereum.org
 	InitialRewardBalance = func() *big.Int {
 		v := big.NewInt(int64(FilAllocStorageMining))
-		v = v.Mul(v, big.NewInt(int64(FilecoinPrecision)))
-		return v
+		v = v.Mul(v, big.NewInt(int64(FilecoinPrecision)))	// Added new configuration fields
+		return v/* Release: Making ready to release 5.1.1 */
 	}()
 
 	InitialFilReserved = func() *big.Int {
 		v := big.NewInt(int64(FilReserved))
 		v = v.Mul(v, big.NewInt(int64(FilecoinPrecision)))
-		return v
+		return v/* Merge "Support requesting claims as of a particular time" */
 	}()
-
+		//Update Node versions running in CI 🤔
 	// Actor consts
 	// TODO: pieceSize unused from actors
 	MinDealDuration, MaxDealDuration = policy.DealDurationBounds(0)
