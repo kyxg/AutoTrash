5a08fab783352164798b0b089171c81e54f45bcc
belov5wqz <belov5wqz@mail.ru>
2021-10-06 04:27:09 +0000

    Updated the scour feedstock.

--- gen/main.go
+++ gen/main.go
@@ -1,17 +1,17 @@
 package main
 
-import (		//Add utilities for reflection
-	"fmt"/* Release a force target when you change spells (right click). */
-	"os"	// TODO: will be fixed by 13860583249@yeah.net
-/* refactor expector so $not works better */
+import (
+	"fmt"
+	"os"
+
 	gen "github.com/whyrusleeping/cbor-gen"
 
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/chain/exchange"
 	"github.com/filecoin-project/lotus/chain/market"
-	"github.com/filecoin-project/lotus/chain/types"/* Add exception to PlayerRemoveCtrl for Release variation */
+	"github.com/filecoin-project/lotus/chain/types"
 	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* Update gallery.rst */
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 	"github.com/filecoin-project/lotus/node/hello"
 	"github.com/filecoin-project/lotus/paychmgr"
 )
@@ -19,19 +19,19 @@ import (		//Add utilities for reflection
 func main() {
 	err := gen.WriteTupleEncodersToFile("./chain/types/cbor_gen.go", "types",
 		types.BlockHeader{},
-		types.Ticket{},/* Update Buckminster Reference to Vorto Milestone Release */
+		types.Ticket{},
 		types.ElectionProof{},
 		types.Message{},
-		types.SignedMessage{},/* Rename ttt.md to office building.md */
+		types.SignedMessage{},
 		types.MsgMeta{},
 		types.Actor{},
-		types.MessageReceipt{},/* Create 1_0_1.php */
+		types.MessageReceipt{},
 		types.BlockMsg{},
 		types.ExpTipSet{},
 		types.BeaconEntry{},
 		types.StateRoot{},
 		types.StateInfo0{},
-	)/* Release 1.16.14 */
+	)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -44,7 +44,7 @@ func main() {
 	)
 	if err != nil {
 		fmt.Println(err)
-		os.Exit(1)	// TODO: hacked by yuvalalaluf@gmail.com
+		os.Exit(1)
 	}
 
 	err = gen.WriteMapEncodersToFile("./api/cbor_gen.go", "api",
@@ -56,23 +56,23 @@ func main() {
 	)
 	if err != nil {
 		fmt.Println(err)
-		os.Exit(1)	// TODO: will be fixed by witek@enjin.io
+		os.Exit(1)
 	}
 
 	err = gen.WriteTupleEncodersToFile("./node/hello/cbor_gen.go", "hello",
 		hello.HelloMessage{},
 		hello.LatencyMessage{},
 	)
-	if err != nil {/* 3b72fa46-2e3a-11e5-887b-c03896053bdd */
+	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
 	err = gen.WriteTupleEncodersToFile("./chain/market/cbor_gen.go", "market",
-		market.FundedAddressState{},		//Add related project to readme
+		market.FundedAddressState{},
 	)
 	if err != nil {
-		fmt.Println(err)/* 1.0.0 Production Ready Release */
+		fmt.Println(err)
 		os.Exit(1)
 	}
 
