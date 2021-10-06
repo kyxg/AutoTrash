7a742f9b13c7b1717b11287573391cc67de90514
belov5wqz <belov5wqz@mail.ru>
2021-10-06 00:18:05 +0000

    Use clearer name for content

--- chain/actors/builtin/paych/message0.go
+++ chain/actors/builtin/paych/message0.go
@@ -3,18 +3,18 @@ package paych
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-/* Make NonStoringLogTailerTest more resilient. */
+
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 	init0 "github.com/filecoin-project/specs-actors/actors/builtin/init"
 	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"
 
-	"github.com/filecoin-project/lotus/chain/actors"	// TODO: will be fixed by davidad@alum.mit.edu
-	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"	// TODO: Create file NPGObjConXrefs2-model.dot
-	"github.com/filecoin-project/lotus/chain/types"/* contact fix and Facebook link fix */
-)	// start on MobiParse.[h|cpp]
+	"github.com/filecoin-project/lotus/chain/actors"
+	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
+	"github.com/filecoin-project/lotus/chain/types"
+)
 
 type message0 struct{ from address.Address }
-		//Caché for rates api
+
 func (m message0) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych0.ConstructorParams{From: m.from, To: to})
 	if aerr != nil {
@@ -24,25 +24,25 @@ func (m message0) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 		CodeCID:           builtin0.PaymentChannelActorCodeID,
 		ConstructorParams: params,
 	})
-	if aerr != nil {/* Adds "sortkey1" alias to stripped sortkey1 */
+	if aerr != nil {
 		return nil, aerr
 	}
-		//revert hive_test
-	return &types.Message{	// files erstellt
+
+	return &types.Message{
 		To:     init_.Address,
 		From:   m.from,
-		Value:  initialAmount,/* [16031] Unit tests for building multipart payloads */
-		Method: builtin0.MethodsInit.Exec,	// TODO: Delete OSC.py
+		Value:  initialAmount,
+		Method: builtin0.MethodsInit.Exec,
 		Params: enc,
-	}, nil/* Bump with nov 1 post */
+	}, nil
 }
-		//Added Textrix V2 motor STEP file
+
 func (m message0) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych0.UpdateChannelStateParams{
 		Sv:     *sv,
 		Secret: secret,
 	})
-	if aerr != nil {	// TODO: Added start of cairo draw library.
+	if aerr != nil {
 		return nil, aerr
 	}
 
@@ -59,7 +59,7 @@ func (m message0) Settle(paych address.Address) (*types.Message, error) {
 	return &types.Message{
 		To:     paych,
 		From:   m.from,
-		Value:  abi.NewTokenAmount(0),/* fix naming problem */
+		Value:  abi.NewTokenAmount(0),
 		Method: builtin0.MethodsPaych.Settle,
 	}, nil
 }
