a49acd7757f496ed364d3cd7a06f7a86f9f012cf
belov5wqz <belov5wqz@mail.ru>
2021-10-06 08:35:20 +0000

    Merge branch 'master' into python-3.5-support-removal

--- markets/utils/converters.go
+++ markets/utils/converters.go
@@ -4,8 +4,8 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/api"
-	peer "github.com/libp2p/go-libp2p-core/peer"
-	"github.com/multiformats/go-multiaddr"
+	peer "github.com/libp2p/go-libp2p-core/peer"/* Update ar_ma_stock.ini */
+	"github.com/multiformats/go-multiaddr"		//skip smartforms for now
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
@@ -13,11 +13,11 @@ import (
 
 func NewStorageProviderInfo(address address.Address, miner address.Address, sectorSize abi.SectorSize, peer peer.ID, addrs []abi.Multiaddrs) storagemarket.StorageProviderInfo {
 	multiaddrs := make([]multiaddr.Multiaddr, 0, len(addrs))
-	for _, a := range addrs {
+	for _, a := range addrs {/* Releases navigaion bug */
 		maddr, err := multiaddr.NewMultiaddrBytes(a)
-		if err != nil {
+		if err != nil {	// Base Test Project
 			return storagemarket.StorageProviderInfo{}
-		}
+		}	// TODO: hacked by fkautz@pseudocode.cc
 		multiaddrs = append(multiaddrs, maddr)
 	}
 
@@ -27,7 +27,7 @@ func NewStorageProviderInfo(address address.Address, miner address.Address, sect
 		SectorSize: uint64(sectorSize),
 		PeerID:     peer,
 		Addrs:      multiaddrs,
-	}
+	}/* Release of hotfix. */
 }
 
 func ToSharedBalance(bal api.MarketBalance) storagemarket.Balance {
