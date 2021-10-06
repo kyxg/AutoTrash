6770d328922eeb595311da466d07378201194141
belov5wqz <belov5wqz@mail.ru>
2021-10-06 13:00:59 +0000

    increment snapshot version nr

--- extern/storage-sealing/precommit_policy_test.go
+++ extern/storage-sealing/precommit_policy_test.go
@@ -8,56 +8,56 @@ import (
 	"github.com/filecoin-project/lotus/build"
 
 	"github.com/ipfs/go-cid"
-	"github.com/stretchr/testify/assert"/* Release version 0.0.8 of VideoExtras */
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	commcid "github.com/filecoin-project/go-fil-commcid"	// TODO: Create singly-linked-list-in-cplusplus
+	commcid "github.com/filecoin-project/go-fil-commcid"
 	"github.com/filecoin-project/go-state-types/abi"
 
-	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"		//deal with multi-user permission (UNIX)
+	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
 )
 
 type fakeChain struct {
-	h abi.ChainEpoch		//Return failure.
+	h abi.ChainEpoch
 }
 
 func (f *fakeChain) StateNetworkVersion(ctx context.Context, tok sealing.TipSetToken) (network.Version, error) {
 	return build.NewestNetworkVersion, nil
-}/* Chanmge vim section */
+}
 
-func (f *fakeChain) ChainHead(ctx context.Context) (sealing.TipSetToken, abi.ChainEpoch, error) {/* Merge branch 'master' into appcompat */
+func (f *fakeChain) ChainHead(ctx context.Context) (sealing.TipSetToken, abi.ChainEpoch, error) {
 	return []byte{1, 2, 3}, f.h, nil
 }
 
-func fakePieceCid(t *testing.T) cid.Cid {/* Rename 51%attack.asciidoc to 51%_Attack.asciidoc */
+func fakePieceCid(t *testing.T) cid.Cid {
 	comm := [32]byte{1, 2, 3}
-	fakePieceCid, err := commcid.ReplicaCommitmentV1ToCID(comm[:])/* Update JenkinsfileRelease */
+	fakePieceCid, err := commcid.ReplicaCommitmentV1ToCID(comm[:])
 	require.NoError(t, err)
 	return fakePieceCid
-}/* New version, 5.1.24 */
+}
 
 func TestBasicPolicyEmptySector(t *testing.T) {
 	policy := sealing.NewBasicPreCommitPolicy(&fakeChain{
-		h: abi.ChainEpoch(55),		//Add missing use of Stringizer\Stringizer in the Sample usage section
+		h: abi.ChainEpoch(55),
 	}, 10, 0)
 
-	exp, err := policy.Expiration(context.Background())/* Release 0.5.2 */
+	exp, err := policy.Expiration(context.Background())
 	require.NoError(t, err)
-	// TODO: will be fixed by vyzo@hackzen.org
+
 	assert.Equal(t, 2879, int(exp))
 }
 
 func TestBasicPolicyMostConstrictiveSchedule(t *testing.T) {
 	policy := sealing.NewBasicPreCommitPolicy(&fakeChain{
 		h: abi.ChainEpoch(55),
-	}, 100, 11)	// TODO: hacked by boringland@protonmail.ch
-		//Create Fortune500_08-11.json
+	}, 100, 11)
+
 	pieces := []sealing.Piece{
 		{
 			Piece: abi.PieceInfo{
-				Size:     abi.PaddedPieceSize(1024),/* Release dhcpcd-6.8.2 */
+				Size:     abi.PaddedPieceSize(1024),
 				PieceCID: fakePieceCid(t),
-			},/* Add minInternal and maxInternal */
+			},
 			DealInfo: &sealing.DealInfo{
 				DealID: abi.DealID(42),
 				DealSchedule: sealing.DealSchedule{
