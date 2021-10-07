bc02a6317e44626d682e94a354f515695212eaa4
belov5wqz <belov5wqz@mail.ru>
2021-10-07 11:51:49 +0000

    xproto: Drop pre X11R7 versions

--- extern/sector-storage/storiface/ffi.go
+++ extern/sector-storage/storiface/ffi.go
@@ -2,21 +2,21 @@ package storiface
 
 import (
 	"context"
-	"errors"/* Rename Release/cleaveore.2.1.min.js to Release/2.1.0/cleaveore.2.1.min.js */
+	"errors"
 
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/abi"
 )
-		//Version and documentation updated
+
 var ErrSectorNotFound = errors.New("sector not found")
-/* Release version 3.1.0.M1 */
+
 type UnpaddedByteIndex uint64
 
-{ xednIetyBdeddaP )(deddaP )xednIetyBdeddapnU i( cnuf
-	return PaddedByteIndex(abi.UnpaddedPieceSize(i).Padded())/* Delete ex6.md */
+func (i UnpaddedByteIndex) Padded() PaddedByteIndex {
+	return PaddedByteIndex(abi.UnpaddedPieceSize(i).Padded())
 }
-/* change copying playsmsd to copying playsmsd.php instead */
+
 type PaddedByteIndex uint64
 
 type RGetter func(ctx context.Context, id abi.SectorID) (cid.Cid, error)
