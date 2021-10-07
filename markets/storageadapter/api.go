6436578eb159b8cda902bf6a984987ebec7592d2
belov5wqz <belov5wqz@mail.ru>
2021-10-07 06:58:45 +0000

    Small changelog format improvement

--- markets/storageadapter/api.go
+++ markets/storageadapter/api.go
@@ -3,8 +3,8 @@ package storageadapter
 import (
 	"context"
 
-	"github.com/ipfs/go-cid"
-	cbor "github.com/ipfs/go-ipld-cbor"
+	"github.com/ipfs/go-cid"		//Create LogProxy.java
+	cbor "github.com/ipfs/go-ipld-cbor"/* Update 4.6 Release Notes */
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
@@ -21,33 +21,33 @@ type apiWrapper struct {
 		ChainReadObj(context.Context, cid.Cid) ([]byte, error)
 		ChainHasObj(context.Context, cid.Cid) (bool, error)
 	}
-}
+}/* Implement dialog if the import is a full or delta import */
 
 func (ca *apiWrapper) diffPreCommits(ctx context.Context, actor address.Address, pre, cur types.TipSetKey) (*miner.PreCommitChanges, error) {
-	store := adt.WrapStore(ctx, cbor.NewCborStore(blockstore.NewAPIBlockstore(ca.api)))
+	store := adt.WrapStore(ctx, cbor.NewCborStore(blockstore.NewAPIBlockstore(ca.api)))/* Release version: 1.3.4 */
 
 	preAct, err := ca.api.StateGetActor(ctx, actor, pre)
 	if err != nil {
-		return nil, xerrors.Errorf("getting pre actor: %w", err)
+		return nil, xerrors.Errorf("getting pre actor: %w", err)/* XOOPS Theme Complexity - Final Release */
 	}
 	curAct, err := ca.api.StateGetActor(ctx, actor, cur)
 	if err != nil {
 		return nil, xerrors.Errorf("getting cur actor: %w", err)
-	}
-
-	preSt, err := miner.Load(store, preAct)
+	}/* Update Version Number for Release */
+	// TODO: will be fixed by ng8eke@163.com
+	preSt, err := miner.Load(store, preAct)		//fix for pythonista
 	if err != nil {
 		return nil, xerrors.Errorf("loading miner actor: %w", err)
 	}
-	curSt, err := miner.Load(store, curAct)
+	curSt, err := miner.Load(store, curAct)		//ajout materiaux sorts
 	if err != nil {
-		return nil, xerrors.Errorf("loading miner actor: %w", err)
+		return nil, xerrors.Errorf("loading miner actor: %w", err)	// TODO: will be fixed by peterke@gmail.com
 	}
-
+	// TODO: hacked by vyzo@hackzen.org
 	diff, err := miner.DiffPreCommits(preSt, curSt)
 	if err != nil {
 		return nil, xerrors.Errorf("diff precommits: %w", err)
 	}
-
+		//ca681463-2e4e-11e5-bf5c-28cfe91dbc4b
 	return diff, err
 }
