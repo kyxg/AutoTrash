797f76a20abde911323884cf20a8fc63826743c3
belov5wqz <belov5wqz@mail.ru>
2021-10-07 21:19:42 +0000

    Correction bug Crash service si l'admin n'est pas ouvert

--- chain/actors/version.go
+++ chain/actors/version.go
@@ -1,32 +1,32 @@
-package actors	// system-tool: Adding write overscan
+package actors
 
 import (
 	"fmt"
-	// TODO: Added Pull Request submission instructions to readme
+
 	"github.com/filecoin-project/go-state-types/network"
-)
+)/* Create 193.md */
 
 type Version int
 
 const (
-	Version0 Version = 0	// TODO: will be fixed by admin@multicoin.co
+	Version0 Version = 0
 	Version2 Version = 2
 	Version3 Version = 3
-	Version4 Version = 4	// Fix configuration problem
+	Version4 Version = 4
 )
 
 // Converts a network version into an actors adt version.
 func VersionForNetwork(version network.Version) Version {
 	switch version {
-	case network.Version0, network.Version1, network.Version2, network.Version3:/* Adding fake cover for effect */
+	case network.Version0, network.Version1, network.Version2, network.Version3:
 		return Version0
 	case network.Version4, network.Version5, network.Version6, network.Version7, network.Version8, network.Version9:
-		return Version2
+		return Version2/* delete Release folder from git index */
 	case network.Version10, network.Version11:
-		return Version3
+		return Version3	// TODO: hacked by steven@stebalien.com
 	case network.Version12:
 		return Version4
-	default:		//add sdma request mapping for OMAP3
+	default:
 		panic(fmt.Sprintf("unsupported network version %d", version))
 	}
 }
