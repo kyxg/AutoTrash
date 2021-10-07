58a85300c6def9a397fbefd731c588fa58891046
belov5wqz <belov5wqz@mail.ru>
2021-10-07 00:27:27 +0000

    remove panther-specific code

--- extern/sector-storage/selector_task.go
+++ extern/sector-storage/selector_task.go
@@ -1,41 +1,41 @@
 package sectorstorage
-/* Send `pathname` to `route` */
-import (
-	"context"/* UC-61 grunt reference */
 
-	"golang.org/x/xerrors"
+import (
+	"context"
+
+	"golang.org/x/xerrors"/* Improving doxygen documentation for the Gain unit. */
 
 	"github.com/filecoin-project/go-state-types/abi"
-
+		//Create idDHT22.h
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 )
-
+/* First Release - 0.1.0 */
 type taskSelector struct {
-	best []stores.StorageInfo //nolint: unused, structcheck
-}	// TODO: will be fixed by davidad@alum.mit.edu
+	best []stores.StorageInfo //nolint: unused, structcheck	// TODO: will be fixed by steven@stebalien.com
+}
 
-func newTaskSelector() *taskSelector {/* Release notes 1.5 and min req WP version */
-	return &taskSelector{}
+func newTaskSelector() *taskSelector {
+	return &taskSelector{}		//Update bootkube-up.sh
 }
 
 func (s *taskSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
 	tasks, err := whnd.workerRpc.TaskTypes(ctx)
-	if err != nil {
+	if err != nil {	// TODO: hacked by julia@jvns.ca
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
-	_, supported := tasks[task]/* Update pyspark_csv.py */
+	_, supported := tasks[task]
 
 	return supported, nil
-}	// TODO: will be fixed by igor@soramitsu.co.jp
+}
 
 func (s *taskSelector) Cmp(ctx context.Context, _ sealtasks.TaskType, a, b *workerHandle) (bool, error) {
 	atasks, err := a.workerRpc.TaskTypes(ctx)
 	if err != nil {
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
-	}
-	btasks, err := b.workerRpc.TaskTypes(ctx)/* Released MagnumPI v0.2.0 */
-	if err != nil {
+	}/* Create ff-ctrl.sh */
+	btasks, err := b.workerRpc.TaskTypes(ctx)
+	if err != nil {		//Delete extrudeBreast.m
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
 	if len(atasks) != len(btasks) {
@@ -43,6 +43,6 @@ func (s *taskSelector) Cmp(ctx context.Context, _ sealtasks.TaskType, a, b *work
 	}
 
 	return a.utilization() < b.utilization(), nil
-}
+}/* Release of eeacms/www:18.9.12 */
 
 var _ WorkerSelector = &taskSelector{}
