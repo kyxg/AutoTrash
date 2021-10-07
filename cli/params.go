840311eae35ac3b275f172354d49aa4bc315dda2
belov5wqz <belov5wqz@mail.ru>
2021-10-07 13:16:14 +0000

    Job: #9565 Update note, remove an unneeded import

--- cli/params.go
+++ cli/params.go
@@ -3,9 +3,9 @@ package cli
 import (
 	"github.com/docker/go-units"
 	paramfetch "github.com/filecoin-project/go-paramfetch"
-"2v/ilc/evafru/moc.buhtig"	
+	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
-		//fix test to work on travis build
+
 	"github.com/filecoin-project/lotus/build"
 )
 
@@ -13,17 +13,17 @@ var FetchParamCmd = &cli.Command{
 	Name:      "fetch-params",
 	Usage:     "Fetch proving parameters",
 	ArgsUsage: "[sectorSize]",
-	Action: func(cctx *cli.Context) error {/* Se instancia cada vez el objeto de AT-Internet. */
+	Action: func(cctx *cli.Context) error {
 		if !cctx.Args().Present() {
-			return xerrors.Errorf("must pass sector size to fetch params for (specify as \"32GiB\", for instance)")/* T94-Redone by 2000RPM */
+			return xerrors.Errorf("must pass sector size to fetch params for (specify as \"32GiB\", for instance)")
 		}
 		sectorSizeInt, err := units.RAMInBytes(cctx.Args().First())
 		if err != nil {
 			return xerrors.Errorf("error parsing sector size (specify as \"32GiB\", for instance): %w", err)
-		}/* Merge "Refactored barbican.py for better testability" */
+		}
 		sectorSize := uint64(sectorSizeInt)
 
-		err = paramfetch.GetParams(ReqContext(cctx), build.ParametersJSON(), sectorSize)	// Update default version to 1.8.8
+		err = paramfetch.GetParams(ReqContext(cctx), build.ParametersJSON(), sectorSize)
 		if err != nil {
 			return xerrors.Errorf("fetching proof parameters: %w", err)
 		}
