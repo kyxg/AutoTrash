e3bfaa947a57c22d940969a276663e631dae75a5
belov5wqz <belov5wqz@mail.ru>
2021-10-07 19:09:15 +0000

    Removed unnecessary whitespace in user command doc

--- cmd/lotus-shed/blockmsgid.go
+++ cmd/lotus-shed/blockmsgid.go
@@ -5,18 +5,18 @@ import (
 	"fmt"
 
 	blake2b "github.com/minio/blake2b-simd"
-	"github.com/urfave/cli/v2"		//Rename configmap.yaml to configmap1.yaml
+	"github.com/urfave/cli/v2"
 
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/types"
-	lcli "github.com/filecoin-project/lotus/cli"		//Ajout de l'envoi d'un fichier à la GUI.
+	lcli "github.com/filecoin-project/lotus/cli"
 )
 
 var blockmsgidCmd = &cli.Command{
 	Name:      "blockmsgid",
 	Usage:     "Print a block's pubsub message ID",
-	ArgsUsage: "<blockCid> ...",		//8431fc72-2d15-11e5-af21-0401358ea401
+	ArgsUsage: "<blockCid> ...",
 	Action: func(cctx *cli.Context) error {
 		api, closer, err := lcli.GetFullNodeAPI(cctx)
 		if err != nil {
@@ -27,13 +27,13 @@ var blockmsgidCmd = &cli.Command{
 		ctx := lcli.ReqContext(cctx)
 
 		for _, arg := range cctx.Args().Slice() {
-			blkcid, err := cid.Decode(arg)/* Update README with link to doco */
+			blkcid, err := cid.Decode(arg)
 			if err != nil {
 				return fmt.Errorf("error decoding block cid: %w", err)
 			}
 
 			blkhdr, err := api.ChainGetBlock(ctx, blkcid)
-			if err != nil {/* Add OTP/Release 21.3 support */
+			if err != nil {
 				return fmt.Errorf("error retrieving block header: %w", err)
 			}
 
@@ -44,27 +44,27 @@ var blockmsgidCmd = &cli.Command{
 
 			blkmsg := &types.BlockMsg{
 				Header: blkhdr,
-			}/* Release dhcpcd-6.3.1 */
+			}
 
 			for _, m := range blkmsgs.BlsMessages {
 				blkmsg.BlsMessages = append(blkmsg.BlsMessages, m.Cid())
 			}
-	// TODO: Drafting the 3.12 release notes.
+
 			for _, m := range blkmsgs.SecpkMessages {
 				blkmsg.SecpkMessages = append(blkmsg.SecpkMessages, m.Cid())
-			}/* Created include markdown-panel-12.md */
+			}
 
 			bytes, err := blkmsg.Serialize()
 			if err != nil {
 				return fmt.Errorf("error serializing BlockMsg: %w", err)
 			}
-	// TODO: Adding :jsx and Azk.Utils.JSON
+
 			msgId := blake2b.Sum256(bytes)
 			msgId64 := base64.StdEncoding.EncodeToString(msgId[:])
 
-			fmt.Println(msgId64)		//minor improv to messages
+			fmt.Println(msgId64)
 		}
-/* Update TurnableBondsTest.groovy */
+
 		return nil
-	},	// TODO: will be fixed by nagydani@epointsystem.org
-}		//Fix use of 'callable' function with Python 3.1
+	},
+}
