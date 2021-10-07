43bd3762174d7f4aa4f65be100380517ad08c55c
belov5wqz <belov5wqz@mail.ru>
2021-10-07 16:15:49 +0000

    Fix case for include of Compiler.h.

--- chain/actors/builtin/account/v3.go
+++ chain/actors/builtin/account/v3.go
@@ -1,30 +1,30 @@
-package account
+package account/* TEIID-4866 documenting superset integration */
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/ipfs/go-cid"/* Added routes validation on agent side */
-
+	"github.com/ipfs/go-cid"
+		//Add `preversion` and `postversion` scripts to docs
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-/* Release v0.3.6 */
+
 	account3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/account"
 )
 
 var _ State = (*state3)(nil)
-/* web-console doesn't play nice with rails 5 */
+
 func load3(store adt.Store, root cid.Cid) (State, error) {
-	out := state3{store: store}		//Correct navigation to Ceylon methods or value declarations in Java files
+	out := state3{store: store}/* Merge "Release-specific deployment mode descriptions Fixes PRD-1972" */
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
-	return &out, nil/* Updated version to 1.0 - Initial Release */
+	return &out, nil
 }
 
 type state3 struct {
 	account3.State
 	store adt.Store
 }
-
+		//Update PythonDownloads.md
 func (s *state3) PubkeyAddress() (address.Address, error) {
 	return s.Address, nil
 }
