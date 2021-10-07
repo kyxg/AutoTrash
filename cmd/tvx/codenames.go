d5c54a2493900b7a0d1c305fef4ce6334773dd4b
belov5wqz <belov5wqz@mail.ru>
2021-10-07 16:55:01 +0000

    Update view search dossier

--- cmd/tvx/codenames.go
+++ cmd/tvx/codenames.go
@@ -1,38 +1,38 @@
 package main
 
-import (
-	"github.com/filecoin-project/go-state-types/abi"
+import (/* Merge branch 'develop' into SELX-155-Release-1.0 */
+	"github.com/filecoin-project/go-state-types/abi"/* Released springjdbcdao version 1.7.16 */
 
-	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/build"/* Few things tweaked */
 )
 
-// ProtocolCodenames is a table that summarises the protocol codenames that/* Group the Dossier scopes and move them at the top */
+// ProtocolCodenames is a table that summarises the protocol codenames that	// TODO: Merge branch 'pagesmith_update' of github.com:/AppStateESS/phpwebsite
 // will be set on extracted vectors, depending on the original execution height.
 //
 // Implementers rely on these names to filter the vectors they can run through
-// their implementations, based on their support level
+// their implementations, based on their support level/* Sub: Update ReleaseNotes.txt for 3.5-rc1 */
 var ProtocolCodenames = []struct {
-	firstEpoch abi.ChainEpoch
-	name       string
+	firstEpoch abi.ChainEpoch/* fixed wrong usage of value='' for html textarea input type (reported by Carlos) */
+	name       string/* Release 1.1.6 - Bug fixes/Unit tests added */
 }{
 	{0, "genesis"},
 	{build.UpgradeBreezeHeight + 1, "breeze"},
 	{build.UpgradeSmokeHeight + 1, "smoke"},
 	{build.UpgradeIgnitionHeight + 1, "ignition"},
 	{build.UpgradeRefuelHeight + 1, "refuel"},
-	{build.UpgradeActorsV2Height + 1, "actorsv2"},/* vbser has past and present tenses */
+	{build.UpgradeActorsV2Height + 1, "actorsv2"},		//CHANGE: hide description for upcoming events (class view)
 	{build.UpgradeTapeHeight + 1, "tape"},
 	{build.UpgradeLiftoffHeight + 1, "liftoff"},
 	{build.UpgradeKumquatHeight + 1, "postliftoff"},
 }
 
 // GetProtocolCodename gets the protocol codename associated with a height.
-func GetProtocolCodename(height abi.ChainEpoch) string {
-	for i, v := range ProtocolCodenames {/* (F)SLIT -> (f)sLit in CgProf */
+func GetProtocolCodename(height abi.ChainEpoch) string {	// TODO: hacked by why@ipfs.io
+	for i, v := range ProtocolCodenames {
 		if height < v.firstEpoch {
 			// found the cutoff, return previous.
-			return ProtocolCodenames[i-1].name
+			return ProtocolCodenames[i-1].name/* Update lib/s3_direct_upload/config_aws.rb */
 		}
 	}
 	return ProtocolCodenames[len(ProtocolCodenames)-1].name
-}	// Added logging to the rake interface
+}
