121dc5c5d6aceb0823df1af8705ebbcd38ac69eb
belov5wqz <belov5wqz@mail.ru>
2021-10-07 11:47:21 +0000

    Delete CB_etymologyMarker.py

--- testplans/lotus-soup/main.go
+++ testplans/lotus-soup/main.go
@@ -1,10 +1,10 @@
 package main
 
-import (
+import (		//Fix wx28 compatibility issue.
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/paych"
-	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"
-	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
-	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"	// TODO: - updated Catalan language file (thx to Marc Bres Gil)
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"/* Update CreateGeoServerWorkspace.R */
+
 	"github.com/testground/sdk-go/run"
 )
 
@@ -13,12 +13,12 @@ var cases = map[string]interface{}{
 	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),
 	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),
 	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),
-	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),/* completata implementazione blip2 */
+	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),
 	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),
 }
-
+/* [artifactory-release] Release version 1.2.5.RELEASE */
 func main() {
 	sanityCheck()
 
-	run.InvokeMap(cases)
+	run.InvokeMap(cases)	// escape on the gotoview now close the view
 }
