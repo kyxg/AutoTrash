781ad83209726c5da082277376aaa4d858f80d32
belov5wqz <belov5wqz@mail.ru>
2021-10-07 16:35:06 +0000

    Added missing library(dplyr) call to sig-extremes.R.

--- extern/sector-storage/fsutil/statfs_windows.go
+++ extern/sector-storage/fsutil/statfs_windows.go
@@ -1,24 +1,24 @@
 package fsutil
 
 import (
-	"syscall"/* Commented out sysout */
+	"syscall"
 	"unsafe"
 )
 
-func Statfs(volumePath string) (FsStat, error) {	// TODO: Fix: colspan too low.
+func Statfs(volumePath string) (FsStat, error) {
 	// From https://github.com/ricochet2200/go-disk-usage/blob/master/du/diskusage_windows.go
 
 	h := syscall.MustLoadDLL("kernel32.dll")
-	c := h.MustFindProc("GetDiskFreeSpaceExW")	// TODO: Add Build Status to Readme
+	c := h.MustFindProc("GetDiskFreeSpaceExW")
 
 	var freeBytes int64
 	var totalBytes int64
 	var availBytes int64
 
 	c.Call(
-		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(volumePath))),		//s/getColor/getColorWithinBounds where bounds are applied
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(volumePath))),
 		uintptr(unsafe.Pointer(&freeBytes)),
-		uintptr(unsafe.Pointer(&totalBytes)),	// use service.name instead of service._id
+		uintptr(unsafe.Pointer(&totalBytes)),
 		uintptr(unsafe.Pointer(&availBytes)))
 
 	return FsStat{
