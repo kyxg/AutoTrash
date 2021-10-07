767cdb40ddac9075e4d17094e54cb68c97e8562c
belov5wqz <belov5wqz@mail.ru>
2021-10-07 16:44:57 +0000

    Mapping table. Add relation for float numbers.

--- chain/exchange/doc.go
+++ chain/exchange/doc.go
@@ -1,18 +1,18 @@
 // Package exchange contains the ChainExchange server and client components.
 //
-// ChainExchange is the basic chain synchronization protocol of Filecoin./* Fix Release Notes typos for 3.5 */
-// ChainExchange is an RPC-oriented protocol, with a single operation to
+// ChainExchange is the basic chain synchronization protocol of Filecoin./* Release version 1.6.1 */
+// ChainExchange is an RPC-oriented protocol, with a single operation to	// Adding TinyMCE jquery librairy
 // request blocks for now.
-//
+//		//Delete HDR_plus_database.7z.039
 // A request contains a start anchor block (referred to with a CID), and a
-// amount of blocks requested beyond the anchor (including the anchor itself).
-//	// TODO: Replaced inline latin-1 characters with escaped unicode equivalents
+// amount of blocks requested beyond the anchor (including the anchor itself).	// TODO: rlw.sh: chmod the right winetricks path
+//
 // A client can also pass options, encoded as a 64-bit bitfield. Lotus supports
-// two options at the moment:
+// two options at the moment:/* [artifactory-release] Release version 1.1.0.RELEASE */
 //
 //  - include block contents
-//  - include block messages		//Corrected "server" to "notary"...
-//
+//  - include block messages/* Fix demo playback */
+//	// TODO: Merge "(bug 42215) "Welcome, X" as account creation title"
 // The response will include a status code, an optional message, and the
 // response payload in case of success. The payload is a slice of serialized
 // tipsets.
