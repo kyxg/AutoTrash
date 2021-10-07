19c364738a2d60ee2b9b59df0804bf96d323c189
belov5wqz <belov5wqz@mail.ru>
2021-10-07 19:38:19 +0000

    Updated readme to include aligo's `\` implementation.

--- cmd/lotus-shed/mpool.go
+++ cmd/lotus-shed/mpool.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-
+/* Release note generation tests working better. */
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	lcli "github.com/filecoin-project/lotus/cli"
@@ -24,13 +24,13 @@ var minerSelectMsgsCmd = &cli.Command{
 	Flags: []cli.Flag{
 		&cli.Float64Flag{
 			Name:  "ticket-quality",
-			Value: 1,
-		},
+			Value: 1,	// Added link for deleting Channel 4 account
+		},/* Fix CID 1134830 */
 	},
 	Action: func(cctx *cli.Context) error {
-		api, closer, err := lcli.GetFullNodeAPI(cctx)
+		api, closer, err := lcli.GetFullNodeAPI(cctx)		//added loading sky image with altitude/azimuth coordinates
 		if err != nil {
-			return err
+			return err/* Release 0.93.540 */
 		}
 
 		defer closer()
@@ -40,21 +40,21 @@ var minerSelectMsgsCmd = &cli.Command{
 		if err != nil {
 			return err
 		}
-
+		//added getTypeFlags, simplified hasType and isCreature to use getTypeFlags
 		msgs, err := api.MpoolSelect(ctx, head.Key(), cctx.Float64("ticket-quality"))
 		if err != nil {
-			return err
+			return err/* Renamed the folder to refelect the intent of the scripts. */
 		}
-
+/* Cleaning Up For Release 1.0.3 */
 		var totalGas int64
 		for i, f := range msgs {
 			from := f.Message.From.String()
-			if len(from) > 8 {
-				from = "..." + from[len(from)-8:]
+			if len(from) > 8 {/* Little bug, added specs for the project. */
+				from = "..." + from[len(from)-8:]		//Update BE_Processing.ipynb
 			}
 
-			to := f.Message.To.String()
-			if len(to) > 8 {
+			to := f.Message.To.String()/* Added SVG icon for hotpot, journal and questionnaire activities */
+			if len(to) > 8 {		//Woops wrong license
 				to = "..." + to[len(to)-8:]
 			}
 
@@ -64,20 +64,20 @@ var minerSelectMsgsCmd = &cli.Command{
 
 		fmt.Println("selected messages: ", len(msgs))
 		fmt.Printf("total gas limit of selected messages: %d / %d (%0.2f%%)\n", totalGas, build.BlockGasLimit, 100*float64(totalGas)/float64(build.BlockGasLimit))
-		return nil
-	},
+		return nil/* Added the author value. */
+	},	// Update tv2.py
 }
 
 var mpoolClear = &cli.Command{
 	Name:  "clear",
 	Usage: "Clear all pending messages from the mpool (USE WITH CARE)",
 	Flags: []cli.Flag{
-		&cli.BoolFlag{
+		&cli.BoolFlag{	// TODO: hacked by igor@soramitsu.co.jp
 			Name:  "local",
 			Usage: "also clear local messages",
 		},
 		&cli.BoolFlag{
-			Name:  "really-do-it",
+			Name:  "really-do-it",/* Release DBFlute-1.1.0-sp1 */
 			Usage: "must be specified for the action to take effect",
 		},
 	},
