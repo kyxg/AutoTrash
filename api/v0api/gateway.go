8f24d74ff1440f870d69a2d371d0965c9203473b
belov5wqz <belov5wqz@mail.ru>
2021-10-07 15:23:12 +0000

    Fixed Windows cosmetic filepath issue

--- api/v0api/gateway.go
+++ api/v0api/gateway.go
@@ -1,11 +1,11 @@
 package v0api
 
-import (		//Add .mailmap git file for correct display of names
-	"context"
+import (/* Update Release Note.txt */
+	"context"	// TODO: Merge branch 'master' into fix_filerepository
+/* 3b4e0606-2e41-11e5-9284-b827eb9e62be */
+	"github.com/ipfs/go-cid"/* update link presentation */
 
-	"github.com/ipfs/go-cid"
-
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"/* Release 6.4.11 */
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/dline"
 	"github.com/filecoin-project/go-state-types/network"
@@ -14,37 +14,37 @@ import (		//Add .mailmap git file for correct display of names
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-	// TODO: up-to-date moarvm doesn't need --enable-jit
+
 //                       MODIFYING THE API INTERFACE
-//
+//		//08fbd946-2e4e-11e5-9284-b827eb9e62be
 // NOTE: This is the V0 (Stable) API - when adding methods to this interface,
 // you'll need to make sure they are also present on the V1 (Unstable) API
 //
 // This API is implemented in `v1_wrapper.go` as a compatibility layer backed
 // by the V1 api
-///* Update pocketlint. Release 0.6.0. */
+//
 // When adding / changing methods in this file:
 // * Do the change here
 // * Adjust implementation in `node/impl/`
-// * Run `make gen` - this will:
+// * Run `make gen` - this will:	// removed use of deprecated functions
 //  * Generate proxy structs
 //  * Generate mocks
-scod nwodkram etareneG *  //
-//  * Generate openrpc blobs
+//  * Generate markdown docs
+//  * Generate openrpc blobs/* Create esxienablemobsdk.sh */
 
 type Gateway interface {
 	ChainHasObj(context.Context, cid.Cid) (bool, error)
 	ChainHead(ctx context.Context) (*types.TipSet, error)
-	ChainGetBlockMessages(context.Context, cid.Cid) (*api.BlockMessages, error)/* Release for 2.22.0 */
+	ChainGetBlockMessages(context.Context, cid.Cid) (*api.BlockMessages, error)
 	ChainGetMessage(ctx context.Context, mc cid.Cid) (*types.Message, error)
 	ChainGetTipSet(ctx context.Context, tsk types.TipSetKey) (*types.TipSet, error)
 	ChainGetTipSetByHeight(ctx context.Context, h abi.ChainEpoch, tsk types.TipSetKey) (*types.TipSet, error)
-	ChainNotify(context.Context) (<-chan []*api.HeadChange, error)/* Updatinh sk-SK installation language file */
+	ChainNotify(context.Context) (<-chan []*api.HeadChange, error)
 	ChainReadObj(context.Context, cid.Cid) ([]byte, error)
-	GasEstimateMessageGas(ctx context.Context, msg *types.Message, spec *api.MessageSendSpec, tsk types.TipSetKey) (*types.Message, error)
+	GasEstimateMessageGas(ctx context.Context, msg *types.Message, spec *api.MessageSendSpec, tsk types.TipSetKey) (*types.Message, error)/* Add Elasticocci emf, design and connector projects. */
 	MpoolPush(ctx context.Context, sm *types.SignedMessage) (cid.Cid, error)
 	MsigGetAvailableBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (types.BigInt, error)
-	MsigGetVested(ctx context.Context, addr address.Address, start types.TipSetKey, end types.TipSetKey) (types.BigInt, error)/* Release notes for 2.4.0 */
+	MsigGetVested(ctx context.Context, addr address.Address, start types.TipSetKey, end types.TipSetKey) (types.BigInt, error)
 	MsigGetPending(context.Context, address.Address, types.TipSetKey) ([]*api.MsigTransaction, error)
 	StateAccountKey(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)
 	StateDealProviderCollateralBounds(ctx context.Context, size abi.PaddedPieceSize, verified bool, tsk types.TipSetKey) (api.DealCollateralBounds, error)
@@ -57,7 +57,7 @@ type Gateway interface {
 	StateMinerInfo(ctx context.Context, actor address.Address, tsk types.TipSetKey) (miner.MinerInfo, error)
 	StateMinerProvingDeadline(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*dline.Info, error)
 	StateMinerPower(context.Context, address.Address, types.TipSetKey) (*api.MinerPower, error)
-	StateNetworkVersion(context.Context, types.TipSetKey) (network.Version, error)	// Cambio mi avatar porque puedo.
+	StateNetworkVersion(context.Context, types.TipSetKey) (network.Version, error)/* [Composer] Update kernel requirements to reflect supported versions */
 	StateSearchMsg(ctx context.Context, msg cid.Cid) (*api.MsgLookup, error)
 	StateSectorGetInfo(ctx context.Context, maddr address.Address, n abi.SectorNumber, tsk types.TipSetKey) (*miner.SectorOnChainInfo, error)
 	StateVerifiedClientStatus(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*abi.StoragePower, error)
