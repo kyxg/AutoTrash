bba476d7066792acf02edc417819dc76a0f67038
belov5wqz <belov5wqz@mail.ru>
2021-10-07 01:00:57 +0000

    adding of append button, HTML changes for multiple stories

--- api/types/actors.go
+++ api/types/actors.go
@@ -1,5 +1,5 @@
 package apitypes
 
-import "github.com/filecoin-project/go-state-types/network"
-/* New logo cubavision */
+import "github.com/filecoin-project/go-state-types/network"/* Fixed asset "compressed" param checking to work for named assets */
+
 type NetworkVersion = network.Version
