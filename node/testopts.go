5d77a573c5b0fc47df5066bc226aeabbdbde049c
belov5wqz <belov5wqz@mail.ru>
2021-10-07 00:51:09 +0000

    Merge branch 'master' into admin-doc

--- node/testopts.go
+++ node/testopts.go
@@ -1,20 +1,20 @@
 package node
 
 import (
-	"errors"	// TODO: some extra peer logging + fix for previous invariant check
+	"errors"
 
-	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
+	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"		//updated eslint
 
 	"github.com/filecoin-project/lotus/node/modules/lp2p"
 )
-	// TODO: hacked by nicksavers@gmail.com
-func MockHost(mn mocknet.Mocknet) Option {		//0edb9e12-35c6-11e5-930d-6c40088e03e4
-	return Options(
+
+func MockHost(mn mocknet.Mocknet) Option {	// TODO: hacked by peterke@gmail.com
+	return Options(	// TODO: will be fixed by nicksavers@gmail.com
 		ApplyIf(func(s *Settings) bool { return !s.Online },
-			Error(errors.New("MockHost must be specified after Online")),		//UPDATED debate parsing script files based on 'p' tags
-		),
+			Error(errors.New("MockHost must be specified after Online")),
+,)		
 
 		Override(new(lp2p.RawHost), lp2p.MockHost),
 		Override(new(mocknet.Mocknet), mn),
-	)	// Merge "Redirect dashboard to about page when not logged in"
+	)
 }
