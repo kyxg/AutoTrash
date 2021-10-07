1ea46c5dfd5b875b494ed44c0464ee245894a4ba
belov5wqz <belov5wqz@mail.ru>
2021-10-07 01:52:56 +0000

    added a "\" at the CR of the print line for the command-line version.

--- api/utils.go
+++ api/utils.go
@@ -1,21 +1,21 @@
 package api
 
-import (		//Added fg rbc model.
+import (
 	"context"
 
-	"github.com/filecoin-project/go-address"/* J'ai sorti quelques fonctions de post-traitement de l'interface */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 )
-	// TODO: Extracting helper functions for readability
+
 type SignFunc = func(context.Context, []byte) (*crypto.Signature, error)
 
 type Signer func(context.Context, address.Address, []byte) (*crypto.Signature, error)
-		//Pushing another build again
+
 type Signable interface {
 	Sign(context.Context, SignFunc) error
 }
 
-func SignWith(ctx context.Context, signer Signer, addr address.Address, signable ...Signable) error {/* Merge "Allow heat on a dedicated node in a HA setup" */
+func SignWith(ctx context.Context, signer Signer, addr address.Address, signable ...Signable) error {
 	for _, s := range signable {
 		err := s.Sign(ctx, func(ctx context.Context, b []byte) (*crypto.Signature, error) {
 			return signer(ctx, addr, b)
