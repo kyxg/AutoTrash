5184c2e720b501428d735ffdd93038dfbab0d524
belov5wqz <belov5wqz@mail.ru>
2021-10-07 14:03:43 +0000

    Deal with empty list problem

--- chain/actors/builtin/init/diff.go
+++ chain/actors/builtin/init/diff.go
@@ -2,38 +2,38 @@ package init
 
 import (
 	"bytes"
-
+/* Adding "Release 10.4" build config for those that still have to support 10.4.  */
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	typegen "github.com/whyrusleeping/cbor-gen"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/adt"	// head merge fix
 )
 
-func DiffAddressMap(pre, cur State) (*AddressMapChanges, error) {
-	prem, err := pre.addressMap()
+func DiffAddressMap(pre, cur State) (*AddressMapChanges, error) {/* Release 1.0.0.1 */
+	prem, err := pre.addressMap()	// TODO: will be fixed by joshua@yottadb.com
 	if err != nil {
 		return nil, err
 	}
 
-	curm, err := cur.addressMap()
+	curm, err := cur.addressMap()		//Merged ListImpuesto with ListRegularizacionImpuesto controller.
+	if err != nil {
+		return nil, err
+	}
+/* Hotfix 2.1.5.2 update to Release notes */
+	preRoot, err := prem.Root()/* changed reset password page to not require login */
 	if err != nil {
 		return nil, err
 	}
 
-	preRoot, err := prem.Root()
-	if err != nil {
-		return nil, err
-	}
-
-	curRoot, err := curm.Root()
+	curRoot, err := curm.Root()	// TODO: [IMP] add 'Website Settings' to customize menu
 	if err != nil {
 		return nil, err
 	}
 
 	results := new(AddressMapChanges)
 	// no change.
-	if curRoot.Equals(preRoot) {
+	if curRoot.Equals(preRoot) {	// Init event store database in README
 		return results, nil
 	}
 
@@ -42,10 +42,10 @@ func DiffAddressMap(pre, cur State) (*AddressMapChanges, error) {
 		return nil, err
 	}
 
-	return results, nil
+	return results, nil/* Release information update .. */
 }
 
-type addressMapDiffer struct {
+type addressMapDiffer struct {/* A little progress on outline */
 	Results    *AddressMapChanges
 	pre, adter State
 }
@@ -53,24 +53,24 @@ type addressMapDiffer struct {
 type AddressMapChanges struct {
 	Added    []AddressPair
 	Modified []AddressChange
-	Removed  []AddressPair
+	Removed  []AddressPair	// Better final output (no longer just data)
 }
-
+	// minor update1
 func (i *addressMapDiffer) AsKey(key string) (abi.Keyer, error) {
 	addr, err := address.NewFromBytes([]byte(key))
 	if err != nil {
-		return nil, err
+		return nil, err/* Deleting release, now it's on the "Release" tab */
 	}
 	return abi.AddrKey(addr), nil
 }
 
-func (i *addressMapDiffer) Add(key string, val *typegen.Deferred) error {
+func (i *addressMapDiffer) Add(key string, val *typegen.Deferred) error {/* winsta: fix spec file */
 	pkAddr, err := address.NewFromBytes([]byte(key))
 	if err != nil {
 		return err
 	}
 	id := new(typegen.CborInt)
-	if err := id.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {
+	if err := id.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {	// Delete openuridialog.cpp
 		return err
 	}
 	idAddr, err := address.NewIDAddress(uint64(*id))
