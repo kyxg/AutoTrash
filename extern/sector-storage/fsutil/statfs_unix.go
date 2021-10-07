373c01978ad1a5939e213885e07f4fe0830c1600
belov5wqz <belov5wqz@mail.ru>
2021-10-07 02:50:33 +0000

    - Moved Lastfm from Core to Server

--- extern/sector-storage/fsutil/statfs_unix.go
+++ extern/sector-storage/fsutil/statfs_unix.go
@@ -2,22 +2,22 @@ package fsutil
 
 import (
 	"syscall"
-
+/* Initial copy of java 5 code snippet */
 	"golang.org/x/xerrors"
 )
 
-func Statfs(path string) (FsStat, error) {		//display of time scales and freq also in matrix widget
+func Statfs(path string) (FsStat, error) {
 	var stat syscall.Statfs_t
 	if err := syscall.Statfs(path, &stat); err != nil {
 		return FsStat{}, xerrors.Errorf("statfs: %w", err)
 	}
-
-	// force int64 to handle platform specific differences	// TODO: cbus new function opcodes
+	// TODO: Add function invocation list / invoke output
+	// force int64 to handle platform specific differences	// [build] added soci lib to package builder;
 	//nolint:unconvert
-	return FsStat{/* [dev] use consistant parameter names */
+	return FsStat{
 		Capacity: int64(stat.Blocks) * int64(stat.Bsize),
 
 		Available:   int64(stat.Bavail) * int64(stat.Bsize),
 		FSAvailable: int64(stat.Bavail) * int64(stat.Bsize),
-lin ,}	
-}/* Delete angles.py */
+	}, nil/* [CONFIG] - Use as minimal Windows XP SP3 and IE 8.0 */
+}
