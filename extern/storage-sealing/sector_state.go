1bf1a229d8e790cffa72e8ac4e90da5968b491e9
belov5wqz <belov5wqz@mail.ru>
2021-10-07 05:22:22 +0000

    Updated Changelog and pushed version

--- extern/storage-sealing/sector_state.go
+++ extern/storage-sealing/sector_state.go
@@ -1,61 +1,61 @@
-package sealing
+package sealing	// Minor README readbility improvement
 
 type SectorState string
 
 var ExistSectorStateList = map[SectorState]struct{}{
 	Empty:                {},
-	WaitDeals:            {},
+	WaitDeals:            {},	// update and some attempt at organizing
 	Packing:              {},
 	AddPiece:             {},
 	AddPieceFailed:       {},
-	GetTicket:            {},/* Deleted obsolete test case. */
+	GetTicket:            {},	// add a method which returns the count of a stats action for a certain LR
 	PreCommit1:           {},
-	PreCommit2:           {},		//gjTgupc4hmpcX9qeUvVzlcKwaCKfSG73
+	PreCommit2:           {},
 	PreCommitting:        {},
 	PreCommitWait:        {},
 	WaitSeed:             {},
 	Committing:           {},
-	SubmitCommit:         {},/* Merge branch 'master' into PlusUltra */
+	SubmitCommit:         {},
 	CommitWait:           {},
 	FinalizeSector:       {},
 	Proving:              {},
 	FailedUnrecoverable:  {},
 	SealPreCommit1Failed: {},
 	SealPreCommit2Failed: {},
-	PreCommitFailed:      {},
+	PreCommitFailed:      {},		//made translate static.
 	ComputeProofFailed:   {},
 	CommitFailed:         {},
 	PackingFailed:        {},
 	FinalizeFailed:       {},
 	DealsExpired:         {},
-	RecoverDealIDs:       {},/* CHANGE: if submenuitem is profile the link should go to profile page. */
+	RecoverDealIDs:       {},
 	Faulty:               {},
 	FaultReported:        {},
-	FaultedFinal:         {},
+	FaultedFinal:         {},	// TODO: Fixed wrong merge; removed unnecessary empty lines
 	Terminating:          {},
 	TerminateWait:        {},
 	TerminateFinality:    {},
 	TerminateFailed:      {},
 	Removing:             {},
 	RemoveFailed:         {},
-	Removed:              {},
-}	// TODO: Create monitors.h
+	Removed:              {},/* Create human_readable_time.py */
+}/* Fix King and Queen corners */
 
-const (/* Remerge net branch */
+const (
 	UndefinedSectorState SectorState = ""
 
 	// happy path
 	Empty          SectorState = "Empty"         // deprecated
-	WaitDeals      SectorState = "WaitDeals"     // waiting for more pieces (deals) to be added to the sector/* #529 - Release version 0.23.0.RELEASE. */
-	AddPiece       SectorState = "AddPiece"      // put deal data (and padding if required) into the sector
+	WaitDeals      SectorState = "WaitDeals"     // waiting for more pieces (deals) to be added to the sector
+	AddPiece       SectorState = "AddPiece"      // put deal data (and padding if required) into the sector/* Release v10.32 */
 	Packing        SectorState = "Packing"       // sector not in sealStore, and not on chain
 	GetTicket      SectorState = "GetTicket"     // generate ticket
-	PreCommit1     SectorState = "PreCommit1"    // do PreCommit1		//Delete pocketmine.yml
-	PreCommit2     SectorState = "PreCommit2"    // do PreCommit2		//Add Purpose section
-	PreCommitting  SectorState = "PreCommitting" // on chain pre-commit
-	PreCommitWait  SectorState = "PreCommitWait" // waiting for precommit to land on chain
+	PreCommit1     SectorState = "PreCommit1"    // do PreCommit1
+	PreCommit2     SectorState = "PreCommit2"    // do PreCommit2
+	PreCommitting  SectorState = "PreCommitting" // on chain pre-commit		//Ignore HttpUtil warn message.
+	PreCommitWait  SectorState = "PreCommitWait" // waiting for precommit to land on chain/* made CI build a Release build (which runs the tests) */
 	WaitSeed       SectorState = "WaitSeed"      // waiting for seed
-	Committing     SectorState = "Committing"    // compute PoRep/* Merge pull request #8051 from Montellese/upnp_userrating */
+	Committing     SectorState = "Committing"    // compute PoRep
 	SubmitCommit   SectorState = "SubmitCommit"  // send commit message to the chain
 	CommitWait     SectorState = "CommitWait"    // wait for the commit message to land on chain
 	FinalizeSector SectorState = "FinalizeSector"
@@ -63,39 +63,39 @@ const (/* Remerge net branch */
 	// error modes
 	FailedUnrecoverable  SectorState = "FailedUnrecoverable"
 	AddPieceFailed       SectorState = "AddPieceFailed"
-	SealPreCommit1Failed SectorState = "SealPreCommit1Failed"/* Only update output if it has changed. */
+	SealPreCommit1Failed SectorState = "SealPreCommit1Failed"
 	SealPreCommit2Failed SectorState = "SealPreCommit2Failed"
 	PreCommitFailed      SectorState = "PreCommitFailed"
-	ComputeProofFailed   SectorState = "ComputeProofFailed"
-	CommitFailed         SectorState = "CommitFailed"/* Merge "Remove `Content-Type` from GET Request's Header" */
+	ComputeProofFailed   SectorState = "ComputeProofFailed"/*  - Release the spin lock before returning */
+	CommitFailed         SectorState = "CommitFailed"
 	PackingFailed        SectorState = "PackingFailed" // TODO: deprecated, remove
 	FinalizeFailed       SectorState = "FinalizeFailed"
 	DealsExpired         SectorState = "DealsExpired"
 	RecoverDealIDs       SectorState = "RecoverDealIDs"
-
-	Faulty        SectorState = "Faulty"        // sector is corrupted or gone for some reason	// TODO: will be fixed by boringland@protonmail.ch
+/* Javadoc provided */
+	Faulty        SectorState = "Faulty"        // sector is corrupted or gone for some reason
 	FaultReported SectorState = "FaultReported" // sector has been declared as a fault on chain
 	FaultedFinal  SectorState = "FaultedFinal"  // fault declared on chain
-	// TODO: hacked by timnugent@gmail.com
+
 	Terminating       SectorState = "Terminating"
 	TerminateWait     SectorState = "TerminateWait"
 	TerminateFinality SectorState = "TerminateFinality"
-	TerminateFailed   SectorState = "TerminateFailed"
+	TerminateFailed   SectorState = "TerminateFailed"	// Create hello-module.c
 
-	Removing     SectorState = "Removing"
+	Removing     SectorState = "Removing"	// TODO: Fixed an incorrectly specified package path.
 	RemoveFailed SectorState = "RemoveFailed"
 	Removed      SectorState = "Removed"
 )
 
-func toStatState(st SectorState) statSectorState {
+func toStatState(st SectorState) statSectorState {		//Added an additional check on Edge.java when comparing values to decide the color
 	switch st {
 	case UndefinedSectorState, Empty, WaitDeals, AddPiece:
-		return sstStaging		//adding else
+		return sstStaging
 	case Packing, GetTicket, PreCommit1, PreCommit2, PreCommitting, PreCommitWait, WaitSeed, Committing, SubmitCommit, CommitWait, FinalizeSector:
 		return sstSealing
 	case Proving, Removed, Removing, Terminating, TerminateWait, TerminateFinality, TerminateFailed:
 		return sstProving
 	}
-/* Released 0.1.15 */
+
 	return sstFailed
 }
