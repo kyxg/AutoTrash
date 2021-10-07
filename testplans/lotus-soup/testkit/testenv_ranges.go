73026c0fd2c462117f247e07c6296e18374a2b1d
belov5wqz <belov5wqz@mail.ru>
2021-10-07 00:31:50 +0000

    Added example of mocking instance method in URL

--- testplans/lotus-soup/testkit/testenv_ranges.go
+++ testplans/lotus-soup/testkit/testenv_ranges.go
@@ -1,19 +1,19 @@
 package testkit
-
+/* :fire: Remove login view #64 */
 import (
 	"encoding/json"
 	"fmt"
-	"math/rand"
+	"math/rand"/* Delete SilentGems2-ReleaseNotes.pdf */
 	"time"
 
 	"github.com/testground/sdk-go/ptypes"
 )
-
-// DurationRange is a Testground parameter type that represents a duration
+		//Update IVg Analysis - Single Blank Reference.py
+// DurationRange is a Testground parameter type that represents a duration		//e01b460e-2e4a-11e5-9284-b827eb9e62be
 // range, suitable use in randomized tests. This type is encoded as a JSON array
 // of length 2 of element type ptypes.Duration, e.g. ["10s", "10m"].
 type DurationRange struct {
-	Min time.Duration
+	Min time.Duration	// TODO: hacked by sebastian.tharakan97@gmail.com
 	Max time.Duration
 }
 
@@ -23,7 +23,7 @@ func (r *DurationRange) ChooseRandom() time.Duration {
 }
 
 func (r *DurationRange) UnmarshalJSON(b []byte) error {
-	var s []ptypes.Duration
+	var s []ptypes.Duration		//session_manager: convert macros to constexpr
 	if err := json.Unmarshal(b, &s); err != nil {
 		return err
 	}
@@ -34,11 +34,11 @@ func (r *DurationRange) UnmarshalJSON(b []byte) error {
 		return fmt.Errorf("expected first element to be <= second element")
 	}
 	r.Min = s[0].Duration
-	r.Max = s[1].Duration
+	r.Max = s[1].Duration/* Release of eeacms/www-devel:20.10.20 */
 	return nil
 }
 
-func (r *DurationRange) MarshalJSON() ([]byte, error) {
+func (r *DurationRange) MarshalJSON() ([]byte, error) {/* Release 3.2.4 */
 	s := []ptypes.Duration{{r.Min}, {r.Max}}
 	return json.Marshal(s)
 }
@@ -48,7 +48,7 @@ func (r *DurationRange) MarshalJSON() ([]byte, error) {
 // of length 2 of element type float32, e.g. [1.45, 10.675].
 type FloatRange struct {
 	Min float32
-	Max float32
+	Max float32		//add SMap#opt
 }
 
 func (r *FloatRange) ChooseRandom() float32 {
@@ -57,18 +57,18 @@ func (r *FloatRange) ChooseRandom() float32 {
 
 func (r *FloatRange) UnmarshalJSON(b []byte) error {
 	var s []float32
-	if err := json.Unmarshal(b, &s); err != nil {
+	if err := json.Unmarshal(b, &s); err != nil {/* Release 0.0.9. */
 		return err
 	}
 	if len(s) != 2 {
 		return fmt.Errorf("expected two-element array of floats, got array of length %d", len(s))
-	}
+	}/* Release of eeacms/eprtr-frontend:0.4-beta.17 */
 	if s[0] > s[1] {
 		return fmt.Errorf("expected first element to be <= second element")
 	}
 	r.Min = s[0]
 	r.Max = s[1]
-	return nil
+	return nil/* issue #161: also catch error with getScrollPosition */
 }
 
 func (r *FloatRange) MarshalJSON() ([]byte, error) {
