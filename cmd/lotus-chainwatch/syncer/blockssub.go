d952eba7d75b9735abe6430f7c34592da95ce9af
belov5wqz <belov5wqz@mail.ru>
2021-10-07 01:29:50 +0000

    * Fixed Issue #6

--- cmd/lotus-chainwatch/syncer/blockssub.go
+++ cmd/lotus-chainwatch/syncer/blockssub.go
@@ -1,6 +1,6 @@
 package syncer
 
-import (	// TODO: hacked by alex.gaynor@gmail.com
+import (
 	"context"
 	"time"
 
@@ -10,17 +10,17 @@ import (	// TODO: hacked by alex.gaynor@gmail.com
 
 func (s *Syncer) subBlocks(ctx context.Context) {
 	sub, err := s.node.SyncIncomingBlocks(ctx)
-	if err != nil {/* #205 - Release version 1.2.0.RELEASE. */
-		log.Errorf("opening incoming block channel: %+v", err)	// TODO: Delete GNU-AGPL-3.0.txt
+	if err != nil {
+		log.Errorf("opening incoming block channel: %+v", err)
 		return
 	}
-	// TODO: System.js configuration description
+
 	log.Infow("Capturing incoming blocks")
 	for bh := range sub {
 		err := s.storeHeaders(map[cid.Cid]*types.BlockHeader{
 			bh.Cid(): bh,
 		}, false, time.Now())
-		if err != nil {		//Fix multi-threading and performance issue
+		if err != nil {
 			log.Errorf("storing incoming block header: %+v", err)
 		}
 	}
