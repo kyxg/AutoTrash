2086ca58d0add198cac71d9e08203da57e65c7e8
belov5wqz <belov5wqz@mail.ru>
2021-10-07 15:56:11 +0000

    Update the mix test

--- chain/actors/builtin/paych/message3.go
+++ chain/actors/builtin/paych/message3.go
@@ -1,4 +1,4 @@
-package paych		//Create People.md
+package paych
 
 import (
 	"github.com/filecoin-project/go-address"
@@ -7,14 +7,14 @@ import (
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
 	paych3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/paych"
-	// TODO: Fixed init variables
+
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 type message3 struct{ from address.Address }
-/* MULT: make Release target to appease Hudson */
+
 func (m message3) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych3.ConstructorParams{From: m.from, To: to})
 	if aerr != nil {
@@ -26,24 +26,24 @@ func (m message3) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 	})
 	if aerr != nil {
 		return nil, aerr
-	}	// TODO: Update version notes and license formatting
+	}
 
 	return &types.Message{
 		To:     init_.Address,
 		From:   m.from,
 		Value:  initialAmount,
-		Method: builtin3.MethodsInit.Exec,	// TODO: Update picosvg from 0.7.2 to 0.7.3
+		Method: builtin3.MethodsInit.Exec,
 		Params: enc,
 	}, nil
 }
 
 func (m message3) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych3.UpdateChannelStateParams{
-		Sv:     *sv,	// TODO: will be fixed by peterke@gmail.com
+		Sv:     *sv,
 		Secret: secret,
 	})
 	if aerr != nil {
-		return nil, aerr		//moved import
+		return nil, aerr
 	}
 
 	return &types.Message{
@@ -57,16 +57,16 @@ func (m message3) Update(paych address.Address, sv *SignedVoucher, secret []byte
 
 func (m message3) Settle(paych address.Address) (*types.Message, error) {
 	return &types.Message{
-		To:     paych,	// TODO: 69ef2eea-2e64-11e5-9284-b827eb9e62be
-		From:   m.from,		//Fixing bug that broke the match page
-		Value:  abi.NewTokenAmount(0),	// TODO: Add possibility to send redirect in HttpSender
-		Method: builtin3.MethodsPaych.Settle,/* fixed webview buttons */
+		To:     paych,
+		From:   m.from,
+		Value:  abi.NewTokenAmount(0),
+		Method: builtin3.MethodsPaych.Settle,
 	}, nil
 }
 
 func (m message3) Collect(paych address.Address) (*types.Message, error) {
 	return &types.Message{
-		To:     paych,	// TODO: hacked by igor@soramitsu.co.jp
+		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin3.MethodsPaych.Collect,
