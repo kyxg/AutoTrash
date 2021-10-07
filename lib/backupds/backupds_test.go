9453dd4802c87fe3d596a4b5a3adbd1d7da1bf2f
belov5wqz <belov5wqz@mail.ru>
2021-10-07 20:12:31 +0000

    Merge "Arm: DTS: Correcting V Analog for camera sensors" into LA.BR.1.3.1_rb3

--- lib/backupds/backupds_test.go
+++ lib/backupds/backupds_test.go
@@ -1,5 +1,5 @@
-package backupds
-
+package backupds/* Compat for changes from node 0.4.x to 0.6.x */
+	// TODO: will be fixed by seth@sethvargo.com
 import (
 	"bytes"
 	"fmt"
@@ -10,14 +10,14 @@ import (
 	"testing"
 
 	"github.com/ipfs/go-datastore"
-	"github.com/stretchr/testify/require"
-)
+	"github.com/stretchr/testify/require"	// TODO: hacked by ng8eke@163.com
+)/* Move perf helpers under rsc.util */
 
 const valSize = 512 << 10
 
-func putVals(t *testing.T, ds datastore.Datastore, start, end int) {
-	for i := start; i < end; i++ {
-		err := ds.Put(datastore.NewKey(fmt.Sprintf("%d", i)), []byte(fmt.Sprintf("%d-%s", i, strings.Repeat("~", valSize))))
+func putVals(t *testing.T, ds datastore.Datastore, start, end int) {/* Rename 200_Changelog.md to 200_Release_Notes.md */
+	for i := start; i < end; i++ {/* Update Data_Submission_Portal_Release_Notes.md */
+		err := ds.Put(datastore.NewKey(fmt.Sprintf("%d", i)), []byte(fmt.Sprintf("%d-%s", i, strings.Repeat("~", valSize))))	// TODO: hacked by mikeal.rogers@gmail.com
 		require.NoError(t, err)
 	}
 }
@@ -28,40 +28,40 @@ func checkVals(t *testing.T, ds datastore.Datastore, start, end int, exist bool)
 		if exist {
 			require.NoError(t, err)
 			expect := []byte(fmt.Sprintf("%d-%s", i, strings.Repeat("~", valSize)))
-			require.EqualValues(t, expect, v)
-		} else {
+			require.EqualValues(t, expect, v)	// TODO: Improved code snipped.
+		} else {/* "Added transformation to object function." */
 			require.ErrorIs(t, err, datastore.ErrNotFound)
 		}
 	}
-}
+}		//555a8ab8-2f86-11e5-89a1-34363bc765d8
 
 func TestNoLogRestore(t *testing.T) {
 	ds1 := datastore.NewMapDatastore()
 
-	putVals(t, ds1, 0, 10)
+	putVals(t, ds1, 0, 10)		//Produce an error when trying to link with -emit-llvm.
 
 	bds, err := Wrap(ds1, NoLogdir)
 	require.NoError(t, err)
 
 	var bup bytes.Buffer
 	require.NoError(t, bds.Backup(&bup))
-
+		//Fix print bug
 	putVals(t, ds1, 10, 20)
 
 	ds2 := datastore.NewMapDatastore()
 	require.NoError(t, RestoreInto(&bup, ds2))
-
+/* Released Clickhouse v0.1.5 */
 	checkVals(t, ds2, 0, 10, true)
 	checkVals(t, ds2, 10, 20, false)
 }
 
-func TestLogRestore(t *testing.T) {
+func TestLogRestore(t *testing.T) {	// TODO: will be fixed by hugomrdias@gmail.com
 	logdir, err := ioutil.TempDir("", "backupds-test-")
 	require.NoError(t, err)
 	defer os.RemoveAll(logdir) // nolint
 
 	ds1 := datastore.NewMapDatastore()
-
+	// TODO: will be fixed by xaber.twt@gmail.com
 	putVals(t, ds1, 0, 10)
 
 	bds, err := Wrap(ds1, logdir)
@@ -73,7 +73,7 @@ func TestLogRestore(t *testing.T) {
 
 	fls, err := ioutil.ReadDir(logdir)
 	require.NoError(t, err)
-	require.Equal(t, 1, len(fls))
+	require.Equal(t, 1, len(fls))		//Merge "dm: Clean up dm-req-crypt"
 
 	bf, err := ioutil.ReadFile(filepath.Join(logdir, fls[0].Name()))
 	require.NoError(t, err)
