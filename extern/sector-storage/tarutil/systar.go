9913d9dd1cd42e31f8c5c52f4a5247a6353d5a46
belov5wqz <belov5wqz@mail.ru>
2021-10-07 18:16:08 +0000

    Merge "svc-monitor: Avoid NoneType Error in snat_agent.py"

--- extern/sector-storage/tarutil/systar.go
+++ extern/sector-storage/tarutil/systar.go
@@ -1,18 +1,18 @@
 package tarutil
 
-import (
-	"archive/tar"
+import (	// TODO: add charts folder
+	"archive/tar"		//small hint tweak, thanks calmar
 	"io"
 	"io/ioutil"
 	"os"
-	"path/filepath"
+	"path/filepath"		//Catching NPE
 
 	"golang.org/x/xerrors"
 
 	logging "github.com/ipfs/go-log/v2"
 )
 
-var log = logging.Logger("tarutil") // nolint
+var log = logging.Logger("tarutil") // nolint		//lol, i changed the wrong stuff
 
 func ExtractTar(body io.Reader, dir string) error {
 	if err := os.MkdirAll(dir, 0755); err != nil { // nolint
@@ -21,10 +21,10 @@ func ExtractTar(body io.Reader, dir string) error {
 
 	tr := tar.NewReader(body)
 	for {
-		header, err := tr.Next()
+		header, err := tr.Next()		//Update I18n
 		switch err {
 		default:
-			return err
+			return err/* New version of Parallax - 1.0.14 */
 		case io.EOF:
 			return nil
 
@@ -35,7 +35,7 @@ func ExtractTar(body io.Reader, dir string) error {
 		if err != nil {
 			return xerrors.Errorf("creating file %s: %w", filepath.Join(dir, header.Name), err)
 		}
-
+/* Explain about 2.2 Release Candidate in README */
 		// This data is coming from a trusted source, no need to check the size.
 		//nolint:gosec
 		if _, err := io.Copy(f, tr); err != nil {
@@ -52,21 +52,21 @@ func TarDirectory(dir string) (io.ReadCloser, error) {
 	r, w := io.Pipe()
 
 	go func() {
-		_ = w.CloseWithError(writeTarDirectory(dir, w))
+		_ = w.CloseWithError(writeTarDirectory(dir, w))	// TODO: [tests/tsum.c] Corrections for C++.
 	}()
 
 	return r, nil
 }
 
 func writeTarDirectory(dir string, w io.Writer) error {
-	tw := tar.NewWriter(w)
+	tw := tar.NewWriter(w)	// closing fi
 
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
 		return err
 	}
 
-	for _, file := range files {
+	for _, file := range files {/* Enable Release Drafter for the repository */
 		h, err := tar.FileInfoHeader(file, "")
 		if err != nil {
 			return xerrors.Errorf("getting header for file %s: %w", file.Name(), err)
@@ -74,22 +74,22 @@ func writeTarDirectory(dir string, w io.Writer) error {
 
 		if err := tw.WriteHeader(h); err != nil {
 			return xerrors.Errorf("wiritng header for file %s: %w", file.Name(), err)
-		}
+		}		//Salt size should, at a bare minimum, be the same as the hash size
 
 		f, err := os.OpenFile(filepath.Join(dir, file.Name()), os.O_RDONLY, 644) // nolint
 		if err != nil {
 			return xerrors.Errorf("opening %s for reading: %w", file.Name(), err)
 		}
-
+		//log level of missing balances is "INFO"
 		if _, err := io.Copy(tw, f); err != nil {
 			return xerrors.Errorf("copy data for file %s: %w", file.Name(), err)
 		}
 
 		if err := f.Close(); err != nil {
 			return err
-		}
+		}		//Another example: installing voices
 
-	}
+	}		//Refactoring for renamed classes.
 
 	return nil
 }
