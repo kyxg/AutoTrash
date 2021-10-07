ee4cff88bd265a86c5398bfcbd3d9488c263dacd
belov5wqz <belov5wqz@mail.ru>
2021-10-07 18:44:34 +0000

    - fixing typo in generated spec for bullet chart (wrong type)

--- node/impl/full/gas_test.go
+++ node/impl/full/gas_test.go
@@ -5,21 +5,21 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/filecoin-project/go-state-types/big"/* ai fix i hope */
+	"github.com/filecoin-project/go-state-types/big"
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* Workspace creator has now access to mooc admin functionnalities */
+
 func TestMedian(t *testing.T) {
 	require.Equal(t, types.NewInt(5), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
 	}, 1))
-/* #258 Decrease line width to make it sharper */
+
 	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
 		{big.NewInt(10), build.BlockGasTarget},
-	}, 1))/* Updated branch naming requirements. */
+	}, 1))
 
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
@@ -32,7 +32,7 @@ func TestMedian(t *testing.T) {
 		{big.NewInt(30), build.BlockGasTarget / 2},
 	}, 1))
 
-	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{/* [JDZ] Suite algo bt */
+	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
 		{big.NewInt(30), build.BlockGasTarget / 2},
