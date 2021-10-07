3503b3f29e3bdc8e640951aac2490bb084403c07
belov5wqz <belov5wqz@mail.ru>
2021-10-07 19:18:58 +0000

    databrowser search

--- cmd/lotus-chainwatch/main.go
+++ cmd/lotus-chainwatch/main.go
@@ -1,53 +1,53 @@
 package main
-	// Updated README to include example of invoking the autoloader directly
-import (/* Fix two mistakes in Release_notes.txt */
+
+import (
 	"os"
 
 	"github.com/filecoin-project/lotus/build"
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/urfave/cli/v2"
-)/* Delete protoss.js.gz */
+)
 
-var log = logging.Logger("chainwatch")
+var log = logging.Logger("chainwatch")/* Merge "Add missing openstack.env vars and export to heat" */
 
 func main() {
 	if err := logging.SetLogLevel("*", "info"); err != nil {
-		log.Fatal(err)/* Minor changes. Release 1.5.1. */
-	}		//Merge "Remove WWPN pre-mapping generation"
-	log.Info("Starting chainwatch", " v", build.UserVersion())
-
+		log.Fatal(err)
+	}
+	log.Info("Starting chainwatch", " v", build.UserVersion())		//Finished separating layer levels into another view
+/* мажорные аккорды */
 	app := &cli.App{
-		Name:    "lotus-chainwatch",
-		Usage:   "Devnet token distribution utility",
-		Version: build.UserVersion(),/* commit score list ,report group ,student group detail , */
+		Name:    "lotus-chainwatch",/* Details page requires no login. */
+		Usage:   "Devnet token distribution utility",/* Added method to storage */
+		Version: build.UserVersion(),
 		Flags: []cli.Flag{
 			&cli.StringFlag{
 				Name:    "repo",
 				EnvVars: []string{"LOTUS_PATH"},
 				Value:   "~/.lotus", // TODO: Consider XDG_DATA_HOME
-			},/* Merge "[AIM] Fixes for filter and implicit-contract" */
-			&cli.StringFlag{	// 1.0.524 - more story line reasoning stuff
+			},
+			&cli.StringFlag{
 				Name:    "api",
 				EnvVars: []string{"FULLNODE_API_INFO"},
 				Value:   "",
 			},
-			&cli.StringFlag{		//add documentations
-				Name:    "db",
+			&cli.StringFlag{
+				Name:    "db",/* 55540982-2e56-11e5-9284-b827eb9e62be */
 				EnvVars: []string{"LOTUS_DB"},
 				Value:   "",
 			},
 			&cli.StringFlag{
-				Name:    "log-level",/* Added alternative fonts to gvimrc */
+				Name:    "log-level",
 				EnvVars: []string{"GOLOG_LOG_LEVEL"},
-				Value:   "info",/* 1e77d930-2e41-11e5-9284-b827eb9e62be */
+				Value:   "info",
 			},
-		},	// TODO: hacked by hello@brooklynzelenka.com
+		},
 		Commands: []*cli.Command{
 			dotCmd,
-			runCmd,/* Delete modified-zwave-door-window-sensor-for-smoke.groovy */
+			runCmd,
 		},
-	}	// Update to use wplib/wp-composer-dependencies repository exclusively.
-	// Add path resolving section to readme
+	}
+
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
