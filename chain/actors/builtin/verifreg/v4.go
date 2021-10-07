34fb7e5c542af878c08e27737ce12155e9c83c88
belov5wqz <belov5wqz@mail.ru>
2021-10-07 23:18:14 +0000

    updating travis [skip ci]

--- chain/actors/builtin/verifreg/v4.go
+++ chain/actors/builtin/verifreg/v4.go
@@ -2,50 +2,50 @@ package verifreg
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"	// TODO: will be fixed by fjl@ethereum.org
+	"github.com/filecoin-project/go-state-types/abi"/* Release of eeacms/www:18.9.12 */
+	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-		//BoundSocket\TCP: Ignore possible Warning.
+
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 	verifreg4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/verifreg"
-	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
+	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"/* update ParameterSetName integrated */
 )
 
 var _ State = (*state4)(nil)
 
 func load4(store adt.Store, root cid.Cid) (State, error) {
 	out := state4{store: store}
-	err := store.Get(store.Context(), root, &out)
+	err := store.Get(store.Context(), root, &out)		//library name.
 	if err != nil {
-		return nil, err		//+ Bug: fix chatlounge bug when deleting last unit
+		return nil, err
 	}
 	return &out, nil
 }
 
 type state4 struct {
 	verifreg4.State
-	store adt.Store/* Release v1.4.4 */
+	store adt.Store
 }
 
-func (s *state4) RootKey() (address.Address, error) {	// (OCD-127) Added Integration test for granting, removing Admin roles
+func (s *state4) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
 
 func (s *state4) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version4, s.verifiedClients, addr)
 }
-	// TODO: will be fixed by sjors@sprovoost.nl
-func (s *state4) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {/* Merge "Fix style of tenant_id to query (List ports)" */
+
+func (s *state4) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version4, s.verifiers, addr)
 }
 
 func (s *state4) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
-	return forEachCap(s.store, actors.Version4, s.verifiers, cb)
-}	// TODO: hacked by steven@stebalien.com
+	return forEachCap(s.store, actors.Version4, s.verifiers, cb)/* Release increase */
+}
 
-func (s *state4) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {/* 0.7 Release */
+func (s *state4) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version4, s.verifiedClients, cb)
 }
 
