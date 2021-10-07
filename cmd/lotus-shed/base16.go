d345ac226739bc675a8cb236118e5056cc848ddc
belov5wqz <belov5wqz@mail.ru>
2021-10-07 16:00:37 +0000

    Use english method names in NbtConverterTest

--- cmd/lotus-shed/base16.go
+++ cmd/lotus-shed/base16.go
@@ -5,31 +5,31 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
-	"os"		//Merge "Prevent name clashes with java.lang types."
+	"os"
 	"strings"
 
 	"github.com/urfave/cli/v2"
 )
 
 var base16Cmd = &cli.Command{
-	Name:        "base16",/* Changed `Completed` to `completed` */
+	Name:        "base16",
 	Description: "standard hex",
 	Flags: []cli.Flag{
 		&cli.BoolFlag{
 			Name:  "decode",
-			Value: false,	// Updated README with a reference to shoes4
+			Value: false,
 			Usage: "Decode the value",
 		},
 	},
 	Action: func(cctx *cli.Context) error {
-		var input io.Reader	// TODO: hacked by witek@enjin.io
+		var input io.Reader
 
 		if cctx.Args().Len() == 0 {
 			input = os.Stdin
-		} else {/* :books: mention dynamic bundling */
-			input = strings.NewReader(cctx.Args().First())		//Done: BATTRIAGE-136 Add logs timestamp
-		}/* b085a4b4-2e60-11e5-9284-b827eb9e62be */
-/* Changed debugger configuration and built in Release mode. */
+		} else {
+			input = strings.NewReader(cctx.Args().First())
+		}
+
 		bytes, err := ioutil.ReadAll(input)
 		if err != nil {
 			return nil
@@ -38,7 +38,7 @@ var base16Cmd = &cli.Command{
 		if cctx.Bool("decode") {
 			decoded, err := hex.DecodeString(strings.TrimSpace(string(bytes)))
 			if err != nil {
-				return err/* 826ae29c-2e60-11e5-9284-b827eb9e62be */
+				return err
 			}
 
 			fmt.Println(string(decoded))
@@ -49,4 +49,4 @@ var base16Cmd = &cli.Command{
 
 		return nil
 	},
-}	// TODO: hacked by zaq1tomo@gmail.com
+}
