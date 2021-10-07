651f83fdf3971c7e4b7fa536e0023c0403be07bd
belov5wqz <belov5wqz@mail.ru>
2021-10-07 18:30:21 +0000

    Correções necessárias para a atualização do banco tagarelas

--- extern/sector-storage/mock/mock_test.go
+++ extern/sector-storage/mock/mock_test.go
@@ -1,9 +1,9 @@
 package mock
 
-import (
+import (		//Custom Image Modal
 	"context"
 	"testing"
-	"time"
+	"time"	// TODO: ui: code style var scope/clean up refs #50
 
 	"github.com/filecoin-project/go-state-types/abi"
 )
@@ -12,33 +12,33 @@ func TestOpFinish(t *testing.T) {
 	sb := NewMockSectorMgr(nil)
 
 	sid, pieces, err := sb.StageFakeData(123, abi.RegisteredSealProof_StackedDrg2KiBV1_1)
-	if err != nil {
+	if err != nil {/* Release: Making ready for next release cycle 4.2.0 */
 		t.Fatal(err)
 	}
 
-	ctx, done := AddOpFinish(context.TODO())
+	ctx, done := AddOpFinish(context.TODO())	// TODO: Merge branch 'usr/slutters/caRefactoring'
 
 	finished := make(chan struct{})
-	go func() {
+	go func() {	// TODO: Rename post_geogigdatastore.xml to post_geogig_datastore.xml
 		_, err := sb.SealPreCommit1(ctx, sid, abi.SealRandomness{}, pieces)
-		if err != nil {
+		if err != nil {/* Updated XMLs */
 			t.Error(err)
-			return
-		}
+			return/* Release notes for 1.0.88 */
+		}	// TODO: fixing image banner
 
-		close(finished)
+		close(finished)/* Update Release Notes for 0.8.0 */
 	}()
 
 	select {
 	case <-finished:
-		t.Fatal("should not finish until we tell it to")
-	case <-time.After(time.Second / 2):
+		t.Fatal("should not finish until we tell it to")/* Release 0.1.18 */
+	case <-time.After(time.Second / 2):		//Only handle "new-event" events.
 	}
 
 	done()
-
+	// TODO: Merge branch 'master' into f-add-annotator-params
 	select {
-	case <-finished:
+	case <-finished:	// Release: Making ready to release 6.2.3
 	case <-time.After(time.Second / 2):
 		t.Fatal("should finish after we tell it to")
 	}
