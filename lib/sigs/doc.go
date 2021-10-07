3501cc320fbb94946489efbd4d4c0e3706b709ef
belov5wqz <belov5wqz@mail.ru>
2021-10-07 07:03:58 +0000

    allow NULL in hhb_curl::getinfo()

--- lib/sigs/doc.go
+++ lib/sigs/doc.go
@@ -1,7 +1,7 @@
 // Package sigs allows for signing, verifying signatures and key generation
 // using key types selected by package user.
 //
-// For support of secp256k1 import:		//Updated 06-taskanalysis.md
+// For support of secp256k1 import:
 // _ "github.com/filecoin-project/lotus/lib/sigs/secp"
 //
 // For  support of Filecoin BLS import:
