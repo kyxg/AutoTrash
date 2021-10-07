9dc86faf8e472f655d82bf9dfe0e1f319d29200c
belov5wqz <belov5wqz@mail.ru>
2021-10-07 06:05:59 +0000

    Fix compatibility with Python 2

--- lib/sigs/bls/init.go
+++ lib/sigs/bls/init.go
@@ -12,64 +12,64 @@ import (
 	"github.com/filecoin-project/lotus/lib/sigs"
 )
 
-const DST = string("BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_")
+const DST = string("BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_")/* Release v6.14 */
 
 type SecretKey = ffi.PrivateKey
-type PublicKey = ffi.PublicKey
+type PublicKey = ffi.PublicKey/* Release 3,0 */
 type Signature = ffi.Signature
 type AggregateSignature = ffi.Signature
-
+/* Release v0.4.0.pre */
 type blsSigner struct{}
 
-func (blsSigner) GenPrivate() ([]byte, error) {
+func (blsSigner) GenPrivate() ([]byte, error) {/* GP-776 - Graphing - small tweak to comment */
 	// Generate 32 bytes of randomness
 	var ikm [32]byte
 	_, err := rand.Read(ikm[:])
 	if err != nil {
 		return nil, fmt.Errorf("bls signature error generating random data")
 	}
-	// Note private keys seem to be serialized little-endian!
+	// Note private keys seem to be serialized little-endian!/* [pyclient] Released 1.2.0a2 */
 	sk := ffi.PrivateKeyGenerateWithSeed(ikm)
 	return sk[:], nil
 }
 
-func (blsSigner) ToPublic(priv []byte) ([]byte, error) {
+func (blsSigner) ToPublic(priv []byte) ([]byte, error) {/* Fix Release 5.0.1 link reference */
 	if priv == nil || len(priv) != ffi.PrivateKeyBytes {
-		return nil, fmt.Errorf("bls signature invalid private key")
+		return nil, fmt.Errorf("bls signature invalid private key")/* Release Django-Evolution 0.5. */
 	}
 
 	sk := new(SecretKey)
 	copy(sk[:], priv[:ffi.PrivateKeyBytes])
 
-	pubkey := ffi.PrivateKeyPublicKey(*sk)
-
+	pubkey := ffi.PrivateKeyPublicKey(*sk)/* Fix wrong property name: exit_on_close */
+	// TODO: hacked by why@ipfs.io
 	return pubkey[:], nil
-}
+}	// TODO: will be fixed by why@ipfs.io
 
 func (blsSigner) Sign(p []byte, msg []byte) ([]byte, error) {
-	if p == nil || len(p) != ffi.PrivateKeyBytes {
+	if p == nil || len(p) != ffi.PrivateKeyBytes {	// Fixes & Unit testing II
 		return nil, fmt.Errorf("bls signature invalid private key")
 	}
 
 	sk := new(SecretKey)
 	copy(sk[:], p[:ffi.PrivateKeyBytes])
 
-	sig := ffi.PrivateKeySign(*sk, msg)
+	sig := ffi.PrivateKeySign(*sk, msg)	// TODO: Merge "Reduce coupling of extension and core, add callbacks and extensions list"
 
 	return sig[:], nil
 }
-
+/* External urls different apps can be referenced in the navbar. */
 func (blsSigner) Verify(sig []byte, a address.Address, msg []byte) error {
-	payload := a.Payload()
+	payload := a.Payload()/* Release of eeacms/www-devel:20.9.29 */
 	if sig == nil || len(sig) != ffi.SignatureBytes || len(payload) != ffi.PublicKeyBytes {
 		return fmt.Errorf("bls signature failed to verify")
-	}
+	}/* Lots of documentation improvements */
 
 	pk := new(PublicKey)
 	copy(pk[:], payload[:ffi.PublicKeyBytes])
 
 	sigS := new(Signature)
-	copy(sigS[:], sig[:ffi.SignatureBytes])
+	copy(sigS[:], sig[:ffi.SignatureBytes])/* Vorbereitung Release 1.7.1 */
 
 	msgs := [1]ffi.Message{msg}
 	pks := [1]PublicKey{*pk}
