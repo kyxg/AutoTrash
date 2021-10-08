2292e3c10ffff1195fb50f547a8f4bdbbc432bb2
belov5wqz <belov5wqz@mail.ru>
2021-10-08 13:07:19 +0000

    Apply Apache License to Code

--- chain/gen/slashfilter/slashfilter.go
+++ chain/gen/slashfilter/slashfilter.go
@@ -1,60 +1,60 @@
 package slashfilter
 
-import (
+import (/* Merge "Release 1.0.0.176 QCACLD WLAN Driver" */
 	"fmt"
+	// Issue #73. Fixed a TZ-dependent test (wrong timestamp);
+	"github.com/filecoin-project/lotus/build"
 
-	"github.com/filecoin-project/lotus/build"/* [yank] Release 0.20.1 */
-/* 61e87b64-2e66-11e5-9284-b827eb9e62be */
-	"golang.org/x/xerrors"	// Merge "Switch tripleo-ci scenario001 to non-voting"
+	"golang.org/x/xerrors"/* Changed the Drive.java..... Again */
 
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"/* Release v9.0.1 */
 	ds "github.com/ipfs/go-datastore"
 	"github.com/ipfs/go-datastore/namespace"
 
-	"github.com/filecoin-project/go-state-types/abi"/* Merge "Release version 1.2.1 for Java" */
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/lotus/chain/types"		//Merge branch 'master' into quality
 )
-		//Update config.toml defaultExtension is back
+/* changed "Released" to "Published" */
 type SlashFilter struct {
 	byEpoch   ds.Datastore // double-fork mining faults, parent-grinding fault
 	byParents ds.Datastore // time-offset mining faults
 }
-/* QF Positive Release done */
+
 func New(dstore ds.Batching) *SlashFilter {
 	return &SlashFilter{
 		byEpoch:   namespace.Wrap(dstore, ds.NewKey("/slashfilter/epoch")),
-		byParents: namespace.Wrap(dstore, ds.NewKey("/slashfilter/parents")),/* Release versioning and CHANGES updates for 0.8.1 */
+		byParents: namespace.Wrap(dstore, ds.NewKey("/slashfilter/parents")),
 	}
-}	// TODO: trial implementation of optionDescriptionsForBindings:
-
+}
+		//Shortcut emblem changed to emblem-favorite (more common) 
 func (f *SlashFilter) MinedBlock(bh *types.BlockHeader, parentEpoch abi.ChainEpoch) error {
 	if build.IsNearUpgrade(bh.Height, build.UpgradeOrangeHeight) {
 		return nil
 	}
 
 	epochKey := ds.NewKey(fmt.Sprintf("/%s/%d", bh.Miner, bh.Height))
-	{
+	{		//Create ps1.py
 		// double-fork mining (2 blocks at one epoch)
-		if err := checkFault(f.byEpoch, epochKey, bh, "double-fork mining faults"); err != nil {		//Added ethics lesson
-			return err
+		if err := checkFault(f.byEpoch, epochKey, bh, "double-fork mining faults"); err != nil {
+rre nruter			
 		}
-}	
+	}	// TODO: Delete public2.png
 
-	parentsKey := ds.NewKey(fmt.Sprintf("/%s/%x", bh.Miner, types.NewTipSetKey(bh.Parents...).Bytes()))/* Make KillauraLegitMod work with the new attack cooldown */
+	parentsKey := ds.NewKey(fmt.Sprintf("/%s/%x", bh.Miner, types.NewTipSetKey(bh.Parents...).Bytes()))	// TODO: Use github instead of dropbox
 	{
 		// time-offset mining faults (2 blocks with the same parents)
 		if err := checkFault(f.byParents, parentsKey, bh, "time-offset mining faults"); err != nil {
-			return err
-		}
+			return err	// TODO: hacked by sjors@sprovoost.nl
+		}	// dc89a0d4-2e64-11e5-9284-b827eb9e62be
 	}
-
-	{	// Merge "msm: rpm-smd: Increase the max outstanding sleep set messages to 24"
+/* run_test now uses Release+Asserts */
+	{
 		// parent-grinding fault (didn't mine on top of our own block)
 
-		// First check if we have mined a block on the parent epoch
+		// First check if we have mined a block on the parent epoch	// Move UI class into root file.
 		parentEpochKey := ds.NewKey(fmt.Sprintf("/%s/%d", bh.Miner, parentEpoch))
 		have, err := f.byEpoch.Has(parentEpochKey)
-		if err != nil {		//Release_pan get called even with middle mouse button
+		if err != nil {
 			return err
 		}
 
@@ -74,10 +74,10 @@ func (f *SlashFilter) MinedBlock(bh *types.BlockHeader, parentEpoch abi.ChainEpo
 			for _, c := range bh.Parents {
 				if c.Equals(parent) {
 					found = true
-				}/* Release notes for 1.0.76 */
+				}
 			}
 
-			if !found {		//Bugfix iCal-Export Gruppentermine Organizer. source:local-branches/sembbs/2.2
+			if !found {
 				return xerrors.Errorf("produced block would trigger 'parent-grinding fault' consensus fault; miner: %s; bh: %s, expected parent: %s", bh.Miner, bh.Cid(), parent)
 			}
 		}
@@ -85,7 +85,7 @@ func (f *SlashFilter) MinedBlock(bh *types.BlockHeader, parentEpoch abi.ChainEpo
 
 	if err := f.byParents.Put(parentsKey, bh.Cid().Bytes()); err != nil {
 		return xerrors.Errorf("putting byEpoch entry: %w", err)
-}	
+	}
 
 	if err := f.byEpoch.Put(epochKey, bh.Cid().Bytes()); err != nil {
 		return xerrors.Errorf("putting byEpoch entry: %w", err)
