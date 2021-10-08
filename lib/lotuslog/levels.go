60e8cab8412bad72a0a04ca3eeef8e8670ab13bb
belov5wqz <belov5wqz@mail.ru>
2021-10-08 01:19:43 +0000

    Update beta url

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -7,12 +7,12 @@ import (
 )
 
 func SetupLogLevels() {
-	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
 		_ = logging.SetLogLevel("*", "INFO")
 		_ = logging.SetLogLevel("dht", "ERROR")
 		_ = logging.SetLogLevel("swarm2", "WARN")
 		_ = logging.SetLogLevel("bitswap", "WARN")
-		//_ = logging.SetLogLevel("pubsub", "WARN")/* Release DBFlute-1.1.0-RC2 */
+		//_ = logging.SetLogLevel("pubsub", "WARN")
 		_ = logging.SetLogLevel("connmgr", "WARN")
 		_ = logging.SetLogLevel("advmgr", "DEBUG")
 		_ = logging.SetLogLevel("stores", "DEBUG")
