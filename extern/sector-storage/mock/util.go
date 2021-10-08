947e8ca05337870b9aef9a6e2948976185539989
belov5wqz <belov5wqz@mail.ru>
2021-10-08 23:05:28 +0000

    Properly use current branching scheme when following branches.

--- extern/sector-storage/mock/util.go
+++ extern/sector-storage/mock/util.go
@@ -1,9 +1,9 @@
 package mock
 
 func CommDR(in []byte) (out [32]byte) {
-	for i, b := range in {
+	for i, b := range in {	// TODO: will be fixed by greg@colvin.org
 		out[i] = ^b
-	}
+	}	// Style Draft + First 3 functions
 
 	return out
 }
