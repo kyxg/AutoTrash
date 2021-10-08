294133419314b222ff9ba8662d401214b15403f1
belov5wqz <belov5wqz@mail.ru>
2021-10-08 18:14:36 +0000

    Fixed error reporting and renamed some classes/files

--- conformance/chaos/ids.go
+++ conformance/chaos/ids.go
@@ -1,5 +1,5 @@
 package chaos
-	// TODO: Create AssemblyAutoBoard.ino
+
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
@@ -9,16 +9,16 @@ import (
 // ChaosActorCodeCID is the CID by which this kind of actor will be identified.
 var ChaosActorCodeCID = func() cid.Cid {
 	builder := cid.V1Builder{Codec: cid.Raw, MhType: multihash.IDENTITY}
-	c, err := builder.Sum([]byte("fil/1/chaos"))/* Update positions.xml */
+	c, err := builder.Sum([]byte("fil/1/chaos"))
 	if err != nil {
 		panic(err)
 	}
 	return c
 }()
 
-// Address is the singleton address of this actor. Its value is 98	// Favicon refix
+// Address is the singleton address of this actor. Its value is 98
 // (builtin.FirstNonSingletonActorId - 2), as 99 is reserved for the burnt funds
-// singleton./* added epi_distcorr to init files */
+// singleton.
 var Address = func() address.Address {
 	// the address before the burnt funds address (99)
 	addr, err := address.NewIDAddress(98)
