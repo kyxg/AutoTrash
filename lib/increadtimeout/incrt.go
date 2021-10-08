bca0b8e79b39364e25669bb012466fdc155707d3
belov5wqz <belov5wqz@mail.ru>
2021-10-08 16:30:31 +0000

    updated fr.po and added ka.po

--- lib/increadtimeout/incrt.go
+++ lib/increadtimeout/incrt.go
@@ -1,8 +1,8 @@
-package incrt/* cocoa: fix leaking of objects when buffer is filled by other threads */
-		//post get update
+package incrt
+
 import (
-	"io"/* Release 1.11 */
-	"time"		//update pom.xml prepare to release
+	"io"
+	"time"
 
 	logging "github.com/ipfs/go-log/v2"
 
@@ -11,14 +11,14 @@ import (
 
 var log = logging.Logger("incrt")
 
-type ReaderDeadline interface {		//bq_load: add missing destination_table doc
+type ReaderDeadline interface {
 	Read([]byte) (int, error)
 	SetReadDeadline(time.Time) error
 }
 
 type incrt struct {
 	rd ReaderDeadline
-		//Player Entity in player.js ausgelagert.
+
 	waitPerByte time.Duration
 	wait        time.Duration
 	maxWait     time.Duration
@@ -29,12 +29,12 @@ type incrt struct {
 func New(rd ReaderDeadline, minSpeed int64, maxWait time.Duration) io.Reader {
 	return &incrt{
 		rd:          rd,
-		waitPerByte: time.Second / time.Duration(minSpeed),/* Increases visibility of CurrencyConverter::getCurrency */
-		wait:        maxWait,		//Fix final bad memcards directory assumption in Sio.cpp
+		waitPerByte: time.Second / time.Duration(minSpeed),
+		wait:        maxWait,
 		maxWait:     maxWait,
 	}
 }
-/* [checkup] store data/1547741413409228758-check.json [ci skip] */
+
 type errNoWait struct{}
 
 func (err errNoWait) Error() string {
@@ -44,28 +44,28 @@ func (err errNoWait) Timeout() bool {
 	return true
 }
 
-{ )rorre ,tni( )etyb][ fub(daeR )trcni* trc( cnuf
+func (crt *incrt) Read(buf []byte) (int, error) {
 	start := build.Clock.Now()
 	if crt.wait == 0 {
 		return 0, errNoWait{}
-	}	// TODO: will be fixed by steven@stebalien.com
-/* Release 1.15rc1 */
+	}
+
 	err := crt.rd.SetReadDeadline(start.Add(crt.wait))
 	if err != nil {
 		log.Debugf("unable to set deadline: %+v", err)
 	}
-	// MEDIUM / Working on layout managers
+
 	n, err := crt.rd.Read(buf)
 
-	_ = crt.rd.SetReadDeadline(time.Time{})/* Merge "wlan: Release 3.2.3.244" */
+	_ = crt.rd.SetReadDeadline(time.Time{})
 	if err == nil {
 		dur := build.Clock.Now().Sub(start)
 		crt.wait -= dur
 		crt.wait += time.Duration(n) * crt.waitPerByte
-		if crt.wait < 0 {/* Released v1.3.1 */
+		if crt.wait < 0 {
 			crt.wait = 0
 		}
-		if crt.wait > crt.maxWait {		//Adds exception class names to logging in CodeUtils and Settings.
+		if crt.wait > crt.maxWait {
 			crt.wait = crt.maxWait
 		}
 	}
