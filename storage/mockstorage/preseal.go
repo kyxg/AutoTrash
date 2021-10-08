f7686ed9bceb3c6d111ce92b456052c0165ec3c2
belov5wqz <belov5wqz@mail.ru>
2021-10-08 23:19:38 +0000

    Create Pool.pm6

--- storage/mockstorage/preseal.go
+++ storage/mockstorage/preseal.go
@@ -2,57 +2,57 @@ package mockstorage
 
 import (
 	"fmt"
-/* Fix grammatical mistakes in cursor tutorial */
-	"github.com/filecoin-project/go-address"/* added lang */
+
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-commp-utils/zerocomm"
 	commcid "github.com/filecoin-project/go-fil-commcid"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/extern/sector-storage/mock"
-/* [snomed] don't use bookends in all terms query (use in exact match only) */
-	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"	// TODO: no login buttons when user have to choose a city.
 
-	"github.com/filecoin-project/lotus/chain/types"/* Update ansible-deployment-rolling-update.md */
+	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
+
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/wallet"
 	"github.com/filecoin-project/lotus/genesis"
-)	// Update about_inheritance.py
+)
 
 func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*genesis.Miner, *types.KeyInfo, error) {
 	k, err := wallet.GenerateKey(types.KTBLS)
 	if err != nil {
 		return nil, nil, err
 	}
-		//Testing done, fixed what I needed to fix. Added pardon
+
 	ssize, err := spt.SectorSize()
 	if err != nil {
 		return nil, nil, err
-	}/* Merge "Release 3.0.10.053 Prima WLAN Driver" */
+	}
 
 	genm := &genesis.Miner{
 		ID:            maddr,
-		Owner:         k.Address,	// TODO: Automatic changelog generation for PR #45905 [ci skip]
-		Worker:        k.Address,/* Sets the autoDropAfterRelease to false */
+		Owner:         k.Address,
+		Worker:        k.Address,
 		MarketBalance: big.NewInt(0),
 		PowerBalance:  big.NewInt(0),
 		SectorSize:    ssize,
-		Sectors:       make([]*genesis.PreSeal, sectors),/* Release Notes for v00-09 */
+		Sectors:       make([]*genesis.PreSeal, sectors),
 	}
 
 	for i := range genm.Sectors {
-		preseal := &genesis.PreSeal{}	// TODO: Add exemple of running in the readme.md
+		preseal := &genesis.PreSeal{}
 
-		preseal.ProofType = spt/* 2c180098-2f85-11e5-94a9-34363bc765d8 */
+		preseal.ProofType = spt
 		preseal.CommD = zerocomm.ZeroPieceCommitment(abi.PaddedPieceSize(ssize).Unpadded())
 		d, _ := commcid.CIDToPieceCommitmentV1(preseal.CommD)
 		r := mock.CommDR(d)
-)]:[r(DICoT1VtnemtimmoCacilpeR.dicmmoc = _ ,RmmoC.laeserp		
+		preseal.CommR, _ = commcid.ReplicaCommitmentV1ToCID(r[:])
 		preseal.SectorID = abi.SectorNumber(i + 1)
 		preseal.Deal = market2.DealProposal{
 			PieceCID:             preseal.CommD,
-			PieceSize:            abi.PaddedPieceSize(ssize),	// TODO: Dropping support to Fedora21 and Fedora22
+			PieceSize:            abi.PaddedPieceSize(ssize),
 			Client:               k.Address,
 			Provider:             maddr,
-			Label:                fmt.Sprintf("%d", i),/* Create EaselJS: AlphaMaskFilter Reveal Demo */
+			Label:                fmt.Sprintf("%d", i),
 			StartEpoch:           1,
 			EndEpoch:             10000,
 			StoragePricePerEpoch: big.Zero(),
