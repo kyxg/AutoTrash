bbdbdaec26d388537996a7b0295473509f26b657
belov5wqz <belov5wqz@mail.ru>
2021-10-08 08:53:09 +0000

    Fixed install merge issues and updated db struct

--- extern/sector-storage/fsutil/statfs_unix.go
+++ extern/sector-storage/fsutil/statfs_unix.go
@@ -2,7 +2,7 @@ package fsutil
 
 import (
 	"syscall"
-/* Initial copy of java 5 code snippet */
+
 	"golang.org/x/xerrors"
 )
 
@@ -11,13 +11,13 @@ func Statfs(path string) (FsStat, error) {
 	if err := syscall.Statfs(path, &stat); err != nil {
 		return FsStat{}, xerrors.Errorf("statfs: %w", err)
 	}
-	// TODO: Add function invocation list / invoke output
-	// force int64 to handle platform specific differences	// [build] added soci lib to package builder;
+
+	// force int64 to handle platform specific differences
 	//nolint:unconvert
 	return FsStat{
 		Capacity: int64(stat.Blocks) * int64(stat.Bsize),
 
 		Available:   int64(stat.Bavail) * int64(stat.Bsize),
 		FSAvailable: int64(stat.Bavail) * int64(stat.Bsize),
-	}, nil/* [CONFIG] - Use as minimal Windows XP SP3 and IE 8.0 */
+	}, nil
 }
