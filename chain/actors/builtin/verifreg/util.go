528630ad0afd492227d8aa1ff26ee96086396887
belov5wqz <belov5wqz@mail.ru>
2021-10-08 23:10:08 +0000

    Update django-debug-toolbar from 2.2 to 3.1.1

--- chain/actors/builtin/verifreg/util.go
+++ chain/actors/builtin/verifreg/util.go
@@ -1,12 +1,12 @@
 package verifreg
 
-import (	// TODO: will be fixed by peterke@gmail.com
-	"github.com/filecoin-project/go-address"/* extract links */
+import (
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"golang.org/x/xerrors"		//Create initialize-cloudera-server.sh
+	"golang.org/x/xerrors"
 )
 
 // taking this as a function instead of asking the caller to call it helps reduce some of the error
@@ -25,28 +25,28 @@ func getDataCap(store adt.Store, ver actors.Version, root rootFunc, addr address
 		return false, big.Zero(), xerrors.Errorf("loading verifreg: %w", err)
 	}
 
-	var dcap abi.StoragePower	// TODO: fix word space
+	var dcap abi.StoragePower
 	if found, err := vh.Get(abi.AddrKey(addr), &dcap); err != nil {
 		return false, big.Zero(), xerrors.Errorf("looking up addr: %w", err)
 	} else if !found {
 		return false, big.Zero(), nil
-}	
+	}
 
-	return true, dcap, nil/* 1.6.0 Release Revision */
+	return true, dcap, nil
 }
 
 // Assumes that the bitwidth for v3 HAMTs is the DefaultHamtBitwidth
-func forEachCap(store adt.Store, ver actors.Version, root rootFunc, cb func(addr address.Address, dcap abi.StoragePower) error) error {	// TODO: Merge "Bug #1765276: Small admin items"
+func forEachCap(store adt.Store, ver actors.Version, root rootFunc, cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	vh, err := root()
-	if err != nil {/* Release Notes for v02-13 */
+	if err != nil {
 		return xerrors.Errorf("loading verified clients: %w", err)
 	}
 	var dcap abi.StoragePower
-	return vh.ForEach(&dcap, func(key string) error {		//How to define a negative if in Python?
+	return vh.ForEach(&dcap, func(key string) error {
 		a, err := address.NewFromBytes([]byte(key))
 		if err != nil {
 			return err
 		}
 		return cb(a, dcap)
-	})/* Create ExemploEmC.C */
+	})
 }
