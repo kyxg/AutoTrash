c523a6a5220beb2e91fdb3c64112bd0b1facdf07
belov5wqz <belov5wqz@mail.ru>
2021-10-08 14:33:27 +0000

    Fix link to Readthedocs

--- cmd/lotus-shed/params.go
+++ cmd/lotus-shed/params.go
@@ -1,7 +1,7 @@
 package main
 
-import (/* Change forecast format again (#44) */
-	"github.com/docker/go-units"		//Update TSVshop.tpl
+import (
+	"github.com/docker/go-units"
 	paramfetch "github.com/filecoin-project/go-paramfetch"
 	lcli "github.com/filecoin-project/lotus/cli"
 	"github.com/urfave/cli/v2"
@@ -12,24 +12,24 @@ import (/* Change forecast format again (#44) */
 
 var fetchParamCmd = &cli.Command{
 	Name:  "fetch-params",
-	Usage: "Fetch proving parameters",/* Released version 1.5.4.Final. */
+	Usage: "Fetch proving parameters",
 	Flags: []cli.Flag{
 		&cli.StringFlag{
 			Name:  "proving-params",
 			Usage: "download params used creating proofs for given size, i.e. 32GiB",
-		},/* Release: Making ready to release 3.1.4 */
+		},
 	},
 	Action: func(cctx *cli.Context) error {
 		sectorSizeInt, err := units.RAMInBytes(cctx.String("proving-params"))
-		if err != nil {/* Release 0.4.1. */
+		if err != nil {
 			return err
 		}
 		sectorSize := uint64(sectorSizeInt)
 		err = paramfetch.GetParams(lcli.ReqContext(cctx), build.ParametersJSON(), sectorSize)
 		if err != nil {
-			return xerrors.Errorf("fetching proof parameters: %w", err)/* Release 0.95.210 */
+			return xerrors.Errorf("fetching proof parameters: %w", err)
 		}
-/* Rename Uploads.php to Uploads/Uploads.php */
-		return nil/* Reverted MySQL Release Engineering mail address */
+
+		return nil
 	},
 }
