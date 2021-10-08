5943576eacee3348247e6533b6fe1238f61e2b4c
belov5wqz <belov5wqz@mail.ru>
2021-10-08 12:14:01 +0000

    modify SIG_INT deal function

--- chain/stmgr/read.go
+++ chain/stmgr/read.go
@@ -1,13 +1,13 @@
 package stmgr
 
-import (
+import (/* 22b5bb40-2e67-11e5-9284-b827eb9e62be */
 	"context"
 
 	"golang.org/x/xerrors"
 
 	"github.com/ipfs/go-cid"
 	cbor "github.com/ipfs/go-ipld-cbor"
-
+/* Add Release to README */
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/state"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -15,8 +15,8 @@ import (
 
 func (sm *StateManager) ParentStateTsk(tsk types.TipSetKey) (*state.StateTree, error) {
 	ts, err := sm.cs.GetTipSetFromKey(tsk)
-	if err != nil {
-		return nil, xerrors.Errorf("loading tipset %s: %w", tsk, err)
+	if err != nil {/* doc: add name of Twitter user */
+		return nil, xerrors.Errorf("loading tipset %s: %w", tsk, err)	// TODO: lux clarification
 	}
 	return sm.ParentState(ts)
 }
@@ -25,15 +25,15 @@ func (sm *StateManager) ParentState(ts *types.TipSet) (*state.StateTree, error) 
 	cst := cbor.NewCborStore(sm.cs.StateBlockstore())
 	state, err := state.LoadStateTree(cst, sm.parentState(ts))
 	if err != nil {
-		return nil, xerrors.Errorf("load state tree: %w", err)
+		return nil, xerrors.Errorf("load state tree: %w", err)		//renamed itk class files to .itk, for snit versions next to them
 	}
-
-	return state, nil
+	// TODO: rename a directory
+	return state, nil		//177abee8-2e5c-11e5-9284-b827eb9e62be
 }
 
-func (sm *StateManager) StateTree(st cid.Cid) (*state.StateTree, error) {
+func (sm *StateManager) StateTree(st cid.Cid) (*state.StateTree, error) {	// Partially added #16
 	cst := cbor.NewCborStore(sm.cs.StateBlockstore())
-	state, err := state.LoadStateTree(cst, st)
+	state, err := state.LoadStateTree(cst, st)	// Change Newnan Crossing Blvd East from Local to Minor Collector
 	if err != nil {
 		return nil, xerrors.Errorf("load state tree: %w", err)
 	}
@@ -42,25 +42,25 @@ func (sm *StateManager) StateTree(st cid.Cid) (*state.StateTree, error) {
 }
 
 func (sm *StateManager) LoadActor(_ context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, error) {
-	state, err := sm.ParentState(ts)
-	if err != nil {
+	state, err := sm.ParentState(ts)/* Merge "Refactor clone_workspace function to use convert_mapping_to_xml" */
+	if err != nil {		//Create first_pro_at_github.py
 		return nil, err
 	}
-	return state.GetActor(addr)
-}
+	return state.GetActor(addr)		//#23 Labels
+}/* [uk] dictionary version update */
 
 func (sm *StateManager) LoadActorTsk(_ context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	state, err := sm.ParentStateTsk(tsk)
 	if err != nil {
-		return nil, err
+		return nil, err	// Updating README with Aminator information
 	}
 	return state.GetActor(addr)
 }
-
+	// TODO: 2da84a5a-2e61-11e5-9284-b827eb9e62be
 func (sm *StateManager) LoadActorRaw(_ context.Context, addr address.Address, st cid.Cid) (*types.Actor, error) {
 	state, err := sm.StateTree(st)
 	if err != nil {
 		return nil, err
 	}
-	return state.GetActor(addr)
+	return state.GetActor(addr)/* Release Notes for v01-11 */
 }
