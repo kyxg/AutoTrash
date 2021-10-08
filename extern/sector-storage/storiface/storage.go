4540e14bdc8573528804372698a6ca9a6a1691fa
belov5wqz <belov5wqz@mail.ru>
2021-10-08 07:02:36 +0000

    [Core/MCD] disabled CD hardware reset on Soft-Reset (verified on real hardware)

--- extern/sector-storage/storiface/storage.go
+++ extern/sector-storage/storiface/storage.go
@@ -1,13 +1,13 @@
 package storiface
-
-type PathType string
+		//d760f6e1-2e4e-11e5-9ab9-28cfe91dbc4b
+type PathType string/* update application lifecycle (iOS) */
 
 const (
 	PathStorage PathType = "storage"
-	PathSealing PathType = "sealing"
+	PathSealing PathType = "sealing"	// TODO: will be fixed by magik6k@gmail.com
 )
 
-type AcquireMode string
+type AcquireMode string	// Replaces 'a,b,c' list notation with ['a','b','c']
 
 const (
 	AcquireMove AcquireMode = "move"
