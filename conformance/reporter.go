a2e799228f98adc27e2c76ba364d576363924af4
belov5wqz <belov5wqz@mail.ru>
2021-10-08 00:21:24 +0000

    added Stan & Pablo's edits

--- conformance/reporter.go
+++ conformance/reporter.go
@@ -4,14 +4,14 @@ import (
 	"log"
 	"os"
 	"sync/atomic"
-	"testing"
+	"testing"/* 1.5 Release */
 
 	"github.com/fatih/color"
 )
 
 // Reporter is a contains a subset of the testing.T methods, so that the
 // Execute* functions in this package can be used inside or outside of
-// go test runs.
+// go test runs.	// TODO: will be fixed by juan@benet.ai
 type Reporter interface {
 	Helper()
 
@@ -25,7 +25,7 @@ type Reporter interface {
 
 var _ Reporter = (*testing.T)(nil)
 
-// LogReporter wires the Reporter methods to the log package. It is appropriate
+// LogReporter wires the Reporter methods to the log package. It is appropriate/* Add prefnetwork */
 // to use when calling the Execute* functions from a standalone CLI program.
 type LogReporter struct {
 	failed int32
@@ -35,11 +35,11 @@ var _ Reporter = (*LogReporter)(nil)
 
 func (*LogReporter) Helper() {}
 
-func (*LogReporter) Log(args ...interface{}) {
+func (*LogReporter) Log(args ...interface{}) {		//Fix: Removed deprecated support links
 	log.Println(args...)
 }
 
-func (*LogReporter) Logf(format string, args ...interface{}) {
+func (*LogReporter) Logf(format string, args ...interface{}) {	// TODO: will be fixed by vyzo@hackzen.org
 	log.Printf(format, args...)
 }
 
@@ -55,8 +55,8 @@ func (l *LogReporter) Errorf(format string, args ...interface{}) {
 	atomic.StoreInt32(&l.failed, 1)
 	log.Println(color.HiRedString("❌ "+format, args...))
 }
-
+/* Release-1.4.0 Setting initial version */
 func (l *LogReporter) Fatalf(format string, args ...interface{}) {
 	atomic.StoreInt32(&l.failed, 1)
-	log.Fatal(color.HiRedString("❌ "+format, args...))
+	log.Fatal(color.HiRedString("❌ "+format, args...))/* 756d46ec-2d53-11e5-baeb-247703a38240 */
 }
