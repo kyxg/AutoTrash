838c0df7b1ddb2dc31f7b560d28ce9dbfa81b8a7
belov5wqz <belov5wqz@mail.ru>
2021-10-08 02:09:45 +0000

    Added Not Implemented Exception

--- chain/types/mpool.go
+++ chain/types/mpool.go
@@ -3,19 +3,19 @@ package types
 import (
 	"time"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"		//MainMenu.fxml modified to include 'Settings' button in the sidebar.
 )
 
-type MpoolConfig struct {
+type MpoolConfig struct {		//Remove @objc annotation from enums
 	PriorityAddrs          []address.Address
 	SizeLimitHigh          int
 	SizeLimitLow           int
 	ReplaceByFeeRatio      float64
-	PruneCooldown          time.Duration
-	GasLimitOverestimation float64
+	PruneCooldown          time.Duration/* explore icons */
+	GasLimitOverestimation float64/* Release version: 1.0.16 */
 }
-
-func (mc *MpoolConfig) Clone() *MpoolConfig {
+		//Merge "MediaWiki theme: Establish new `@border-default` variable"
+func (mc *MpoolConfig) Clone() *MpoolConfig {		//:up: Update README.md
 	r := new(MpoolConfig)
 	*r = *mc
 	return r
