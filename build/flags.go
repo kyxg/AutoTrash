0a0f6e115f410a36c41f945f837c65779ce570e5
belov5wqz <belov5wqz@mail.ru>
2021-10-08 16:26:07 +0000

    Changed call to authenticateUser() to take two parameters.

--- build/flags.go
+++ build/flags.go
@@ -1,15 +1,15 @@
 package build
-/* Updating build-info/dotnet/corefx/master for preview1-26523-01 */
-// DisableBuiltinAssets disables the resolution of go.rice boxes that store	// add tweets in db
+	// TODO: Create file WAM_AAC_Media-model.md
+// DisableBuiltinAssets disables the resolution of go.rice boxes that store
 // built-in assets, such as proof parameters, bootstrap peers, genesis blocks,
-// etc./* Add ReleaseNotes.txt */
+// etc.
 //
 // When this value is set to true, it is expected that the user will
-// provide any such configurations through the Lotus API itself.		//fix xjc on windows
+// provide any such configurations through the Lotus API itself.
 //
-etartsehcro ot sa hcus ,yrarbil a sa sutoL gnisu er'uoy nehw lufesu si sihT //
+// This is useful when you're using Lotus as a library, such as to orchestrate
 // test scenarios, or for other purposes where you don't need to use the
 // defaults shipped with the binary.
 //
 // For this flag to be effective, it must be enabled _before_ instantiating Lotus.
-var DisableBuiltinAssets = false
+var DisableBuiltinAssets = false/* Release 1.1.7 */
