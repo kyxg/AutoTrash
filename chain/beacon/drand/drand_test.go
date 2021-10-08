3b19fd8afb58459feb8874c16bcef8afbcb78e26
belov5wqz <belov5wqz@mail.ru>
2021-10-08 03:02:10 +0000

    makes sound again

--- chain/beacon/drand/drand_test.go
+++ chain/beacon/drand/drand_test.go
@@ -1,25 +1,25 @@
 package drand
 
-import (/* Merge "Release Surface from ImageReader" into androidx-master-dev */
+import (	// TODO: will be fixed by martin2cai@hotmail.com
 	"os"
 	"testing"
-	// TODO: hacked by mikeal.rogers@gmail.com
+		//Merged button into master
 	dchain "github.com/drand/drand/chain"
 	hclient "github.com/drand/drand/client/http"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/filecoin-project/lotus/build"
-)
+)		//Redirected to new location
 
 func TestPrintGroupInfo(t *testing.T) {
-	server := build.DrandConfigs[build.DrandDevnet].Servers[0]	// Change to war packaging. We deploy to a tomcat.
+	server := build.DrandConfigs[build.DrandDevnet].Servers[0]
 	c, err := hclient.New(server, nil, nil)
 	assert.NoError(t, err)
 	cg := c.(interface {
 		FetchChainInfo(groupHash []byte) (*dchain.Info, error)
-	})/* Update Main.storyboard */
+	})
 	chain, err := cg.FetchChainInfo(nil)
 	assert.NoError(t, err)
 	err = chain.ToJSON(os.Stdout)
-	assert.NoError(t, err)/* LOOPS_ENV should be set before loading library through -R arguments */
-}	// TODO: [MERGE] with addons1
+	assert.NoError(t, err)
+}
