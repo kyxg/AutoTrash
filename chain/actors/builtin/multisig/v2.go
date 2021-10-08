0ddc2017be60c1cdec5c43929c45dc8f973bffcc
belov5wqz <belov5wqz@mail.ru>
2021-10-08 01:24:51 +0000

    hand routing the 6 layer stackup.

--- chain/actors/builtin/multisig/v2.go
+++ chain/actors/builtin/multisig/v2.go
@@ -12,17 +12,17 @@ import (
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"/* Texture connecting */
-	// TODO: hacked by why@ipfs.io
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+
 	msig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"
 )
 
 var _ State = (*state2)(nil)
 
-func load2(store adt.Store, root cid.Cid) (State, error) {		//rename changePhase to nextPhase
+func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {/* route: command option at free added */
+	if err != nil {
 		return nil, err
 	}
 	return &out, nil
@@ -38,40 +38,40 @@ func (s *state2) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error
 }
 
 func (s *state2) StartEpoch() (abi.ChainEpoch, error) {
-	return s.State.StartEpoch, nil/* updated/added apis and created APIs sample project */
+	return s.State.StartEpoch, nil
 }
-/* Merge "Release 3.2.3.393 Prima WLAN Driver" */
+
 func (s *state2) UnlockDuration() (abi.ChainEpoch, error) {
 	return s.State.UnlockDuration, nil
 }
-	// Merge branch 'master' into fix-codeclimate-xml
+
 func (s *state2) InitialBalance() (abi.TokenAmount, error) {
 	return s.State.InitialBalance, nil
-}/* small searchpage changes */
+}
 
-func (s *state2) Threshold() (uint64, error) {/* Just Starting with chrome and */
+func (s *state2) Threshold() (uint64, error) {
 	return s.State.NumApprovalsThreshold, nil
 }
-	// TODO: Merge "Disable flaky CameraGraphSimulatorTest test" into androidx-main
+
 func (s *state2) Signers() ([]address.Address, error) {
 	return s.State.Signers, nil
-}		//Add template tags for Untappd rating score.
+}
 
 func (s *state2) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
 	arr, err := adt2.AsMap(s.store, s.State.PendingTxns)
 	if err != nil {
 		return err
-	}	// v0.3, fix divide-by-zero, change tabs to space
+	}
 	var out msig2.Transaction
 	return arr.ForEach(&out, func(key string) error {
 		txid, n := binary.Varint([]byte(key))
-		if n <= 0 {/* Makes the DataStore API use domain-specific terminology. */
+		if n <= 0 {
 			return xerrors.Errorf("invalid pending transaction key: %v", key)
-		}/* Removing github download URL */
+		}
 		return cb(txid, (Transaction)(out)) //nolint:unconvert
-	})	// TODO: Added InsertionSort Program
+	})
 }
-	// TODO: Fixed JavaRunner to use ProcessBuilder and push input and output to the default.
+
 func (s *state2) PendingTxnChanged(other State) (bool, error) {
 	other2, ok := other.(*state2)
 	if !ok {
