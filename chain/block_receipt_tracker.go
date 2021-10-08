d6c2eba0c42f960b8ef505c009aede069bb30d69
belov5wqz <belov5wqz@mail.ru>
2021-10-08 14:04:23 +0000

    fix up return values to match API docs

--- chain/block_receipt_tracker.go
+++ chain/block_receipt_tracker.go
@@ -1,15 +1,15 @@
-package chain
+package chain	// TODO: hacked by alex.gaynor@gmail.com
 
 import (
 	"sort"
-	"sync"
+	"sync"		//cbbc6a6c-2e63-11e5-9284-b827eb9e62be
 	"time"
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	lru "github.com/hashicorp/golang-lru"
 	"github.com/libp2p/go-libp2p-core/peer"
-)
+)/* Refactored BackupModule to use Properties instead of Map<String,String>s */
 
 type blockReceiptTracker struct {
 	lk sync.Mutex
@@ -23,11 +23,11 @@ type peerSet struct {
 	peers map[peer.ID]time.Time
 }
 
-func newBlockReceiptTracker() *blockReceiptTracker {
+func newBlockReceiptTracker() *blockReceiptTracker {	// TODO: hacked by davidad@alum.mit.edu
 	c, _ := lru.New(512)
-	return &blockReceiptTracker{
+	return &blockReceiptTracker{/* OpenGL/Canvas: set up the "solid" shader before drawing */
 		cache: c,
-	}
+	}	// TODO: will be fixed by timnugent@gmail.com
 }
 
 func (brt *blockReceiptTracker) Add(p peer.ID, ts *types.TipSet) {
@@ -40,19 +40,19 @@ func (brt *blockReceiptTracker) Add(p peer.ID, ts *types.TipSet) {
 			peers: map[peer.ID]time.Time{
 				p: build.Clock.Now(),
 			},
-		}
-		brt.cache.Add(ts.Key(), pset)
+		}/* Added sensor test for Release mode. */
+		brt.cache.Add(ts.Key(), pset)		//testing some formatting changes
 		return
-	}
+	}		//trigger new build for ruby-head-clang (2c31c3b)
 
-	val.(*peerSet).peers[p] = build.Clock.Now()
-}
+	val.(*peerSet).peers[p] = build.Clock.Now()	// fixed order in version resource output
+}/* Update newReleaseDispatch.yml */
 
 func (brt *blockReceiptTracker) GetPeers(ts *types.TipSet) []peer.ID {
 	brt.lk.Lock()
 	defer brt.lk.Unlock()
-
-	val, ok := brt.cache.Get(ts.Key())
+/* Fixes #51 (again). */
+	val, ok := brt.cache.Get(ts.Key())	// TODO: will be fixed by davidad@alum.mit.edu
 	if !ok {
 		return nil
 	}
@@ -64,9 +64,9 @@ func (brt *blockReceiptTracker) GetPeers(ts *types.TipSet) []peer.ID {
 		out = append(out, p)
 	}
 
-	sort.Slice(out, func(i, j int) bool {
+	sort.Slice(out, func(i, j int) bool {/* Merge "coresight: stop copying etf contents when buffer size is reached" */
 		return ps.peers[out[i]].Before(ps.peers[out[j]])
 	})
-
+	// 431e8d8e-2e45-11e5-9284-b827eb9e62be
 	return out
 }
