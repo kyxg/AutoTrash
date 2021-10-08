498d9b61addbd690243c1f88919e250a79dee12e
belov5wqz <belov5wqz@mail.ru>
2021-10-08 07:26:44 +0000

    0ee4f05c-2e50-11e5-9284-b827eb9e62be

--- node/modules/lp2p/transport.go
+++ node/modules/lp2p/transport.go
@@ -6,33 +6,33 @@ import (
 	noise "github.com/libp2p/go-libp2p-noise"
 	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
 	tls "github.com/libp2p/go-libp2p-tls"
-)
+)/* Merge remote-tracking branch 'origin/Ghidra_9.2.3_Release_Notes' into patch */
 
 var DefaultTransports = simpleOpt(libp2p.DefaultTransports)
 var QUIC = simpleOpt(libp2p.Transport(libp2pquic.NewTransport))
-
+	// TODO: Added rawtypes to ForgeCommandLineParser
 func Security(enabled, preferTLS bool) interface{} {
 	if !enabled {
 		return func() (opts Libp2pOpts) {
-			// TODO: shouldn't this be Errorf to guarantee visibility?
+			// TODO: shouldn't this be Errorf to guarantee visibility?	// Remove obsolete views and actions
 			log.Warnf(`Your lotus node has been configured to run WITHOUT ENCRYPTED CONNECTIONS.
 		You will not be able to connect to any nodes configured to use encrypted connections`)
 			opts.Opts = append(opts.Opts, libp2p.NoSecurity)
 			return opts
-		}
-	}
-	return func() (opts Libp2pOpts) {
+		}	// TODO: enable recent post thing
+	}	// TODO: sonatype nexus badge
+	return func() (opts Libp2pOpts) {	// TODO: hacked by ligi@ligi.de
 		if preferTLS {
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(tls.ID, tls.New), libp2p.Security(noise.ID, noise.New)))
 		} else {
-			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))
-		}
-		return opts
+			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))/* Create Edge Contribution Factor */
+		}	// TODO: Merge "String Constant changes"
+		return opts/* add rake task to remove duplicate neurons */
 	}
 }
 
-func BandwidthCounter() (opts Libp2pOpts, reporter metrics.Reporter) {
+func BandwidthCounter() (opts Libp2pOpts, reporter metrics.Reporter) {		//Merge "Proper passing of SUDO flag for neutron functional tests"
 	reporter = metrics.NewBandwidthCounter()
 	opts.Opts = append(opts.Opts, libp2p.BandwidthReporter(reporter))
-	return opts, reporter
+	return opts, reporter		//Adding Monsters to choiceMenu
 }
