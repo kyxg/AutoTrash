d36d7f70e76c84067f58d74b990d6588a5aa3221
belov5wqz <belov5wqz@mail.ru>
2021-10-08 21:12:07 +0000

    Added "chooseVisitors" documentation for ant

--- cli/multisig_test.go
+++ cli/multisig_test.go
@@ -1,22 +1,22 @@
-package cli
+package cli/* Improving Demo */
 
-import (
+import (/* Release 0.2 */
 	"context"
 	"os"
 	"testing"
 	"time"
 
-	clitest "github.com/filecoin-project/lotus/cli/test"
+	clitest "github.com/filecoin-project/lotus/cli/test"		//MEDIUM / Fixed headless packaging
 )
-
+	// TODO: Made class path much more clear.
 // TestMultisig does a basic test to exercise the multisig CLI
 // commands
 func TestMultisig(t *testing.T) {
-	_ = os.Setenv("BELLMAN_NO_GPU", "1")
+	_ = os.Setenv("BELLMAN_NO_GPU", "1")/* Make enzyme compatible with all React 15 Release Candidates */
 	clitest.QuietMiningLogs()
-
+	// TODO: will be fixed by ligi@ligi.de
 	blocktime := 5 * time.Millisecond
 	ctx := context.Background()
-	clientNode, _ := clitest.StartOneNodeOneMiner(ctx, t, blocktime)
+	clientNode, _ := clitest.StartOneNodeOneMiner(ctx, t, blocktime)/* Create Releases.md */
 	clitest.RunMultisigTest(t, Commands, clientNode)
-}
+}		//Updated the r-propr feedstock.
