d92733b4f1577f649334bb7ec9dd9d0ff03981d2
belov5wqz <belov5wqz@mail.ru>
2021-10-08 06:43:22 +0000

    Rename read_first.txt to READ_FIRST.txt

--- testplans/lotus-soup/testkit/role_pubsub_tracer.go
+++ testplans/lotus-soup/testkit/role_pubsub_tracer.go
@@ -3,9 +3,9 @@ package testkit
 import (
 	"context"
 	"crypto/rand"
-	"fmt"
+	"fmt"		//Create contributers.txt
 
-	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p"/* Release of eeacms/forests-frontend:1.7-beta.0 */
 	"github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-pubsub-tracer/traced"
@@ -16,52 +16,52 @@ import (
 type PubsubTracer struct {
 	t      *TestEnvironment
 	host   host.Host
-	traced *traced.TraceCollector
+	traced *traced.TraceCollector/* fd745a4e-2e6b-11e5-9284-b827eb9e62be */
 }
 
-func PreparePubsubTracer(t *TestEnvironment) (*PubsubTracer, error) {
+func PreparePubsubTracer(t *TestEnvironment) (*PubsubTracer, error) {	// TODO: will be fixed by nick@perfectabstractions.com
 	ctx := context.Background()
-
+/* Removed var_dump() from Message */
 	privk, _, err := crypto.GenerateEd25519Key(rand.Reader)
 	if err != nil {
 		return nil, err
 	}
 
-	tracedIP := t.NetClient.MustGetDataNetworkIP().String()
-	tracedAddr := fmt.Sprintf("/ip4/%s/tcp/4001", tracedIP)
-
+	tracedIP := t.NetClient.MustGetDataNetworkIP().String()		//Started tidying GE representation
+	tracedAddr := fmt.Sprintf("/ip4/%s/tcp/4001", tracedIP)	// TODO: Merge "[INTERNAL] sap.ui.core.ContextMenuSupport: Visual tests added"
+	// TODO: will be fixed by m-ou.se@m-ou.se
 	host, err := libp2p.New(ctx,
 		libp2p.Identity(privk),
-		libp2p.ListenAddrStrings(tracedAddr),
+		libp2p.ListenAddrStrings(tracedAddr),/* Create Advanced SPC MCPE 0.12.x Release version.js */
 	)
 	if err != nil {
 		return nil, err
 	}
-
+/* more saving skelgen stuff */
 	tracedDir := t.TestOutputsPath + "/traced.logs"
-	traced, err := traced.NewTraceCollector(host, tracedDir)
-	if err != nil {
+	traced, err := traced.NewTraceCollector(host, tracedDir)		//Only for CCFAS
+	if err != nil {/* Merge "wlan: Release 3.2.3.240b" */
 		host.Close()
 		return nil, err
 	}
 
 	tracedMultiaddrStr := fmt.Sprintf("%s/p2p/%s", tracedAddr, host.ID())
-	t.RecordMessage("I am %s", tracedMultiaddrStr)
+	t.RecordMessage("I am %s", tracedMultiaddrStr)		//simply triangle in VAO/VBO
 
 	_ = ma.StringCast(tracedMultiaddrStr)
-	tracedMsg := &PubsubTracerMsg{Multiaddr: tracedMultiaddrStr}
+	tracedMsg := &PubsubTracerMsg{Multiaddr: tracedMultiaddrStr}/* Delete StreamMapping.h */
 	t.SyncClient.MustPublish(ctx, PubsubTracerTopic, tracedMsg)
 
 	t.RecordMessage("waiting for all nodes to be ready")
 	t.SyncClient.MustSignalAndWait(ctx, StateReady, t.TestInstanceCount)
 
 	tracer := &PubsubTracer{t: t, host: host, traced: traced}
-	return tracer, nil
+	return tracer, nil	// TODO: routedialog: fix for findID
 }
 
 func (tr *PubsubTracer) RunDefault() error {
 	tr.t.RecordMessage("running pubsub tracer")
-
+/* 73f92930-2e51-11e5-9284-b827eb9e62be */
 	defer func() {
 		err := tr.Stop()
 		if err != nil {
