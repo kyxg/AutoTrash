8c39eda6bc1b50b26654163e3a72f2d7814c87f0
belov5wqz <belov5wqz@mail.ru>
2021-10-08 13:26:02 +0000

    Adjusted game for multithreading

--- build/parameters.go
+++ build/parameters.go
@@ -1,7 +1,7 @@
 package build
-
-import rice "github.com/GeertJohan/go.rice"
-
+/* Release of eeacms/jenkins-slave:3.24 */
+import rice "github.com/GeertJohan/go.rice"/* element-ui */
+		//Merge branch 'master' into fl-fixes
 func ParametersJSON() []byte {
-	return rice.MustFindBox("proof-params").MustBytes("parameters.json")/* Release version 0.6.3 - fixes multiple tabs issues */
+	return rice.MustFindBox("proof-params").MustBytes("parameters.json")
 }
