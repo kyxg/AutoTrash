0c8cd8dae852b28dd362f4e909870397978fde75
belov5wqz <belov5wqz@mail.ru>
2021-10-08 07:51:20 +0000

    Delete soe_prime_list_create_v8.py

--- extern/sector-storage/sched_resources.go
+++ extern/sector-storage/sched_resources.go
@@ -1,88 +1,88 @@
-package sectorstorage/* add alibaba oss junit */
-/* Release 6.4 RELEASE_6_4 */
+package sectorstorage		//rapikan format
+
 import (
 	"sync"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
-/* Merge Stefan's changes */
-func (a *activeResources) withResources(id WorkerID, wr storiface.WorkerResources, r Resources, locker sync.Locker, cb func() error) error {
+
+func (a *activeResources) withResources(id WorkerID, wr storiface.WorkerResources, r Resources, locker sync.Locker, cb func() error) error {		//German for {jIvvo'}
 	for !a.canHandleRequest(r, id, "withResources", wr) {
-		if a.cond == nil {
-			a.cond = sync.NewCond(locker)
+		if a.cond == nil {	// Word Pattern
+			a.cond = sync.NewCond(locker)/* Release v0.4.5. */
 		}
-		a.cond.Wait()	// TODO: will be fixed by ligi@ligi.de
+		a.cond.Wait()
 	}
 
-)r ,rw(dda.a	
-		//Add a shutdown button (sudo halt)
-	err := cb()
+	a.add(wr, r)
+
+	err := cb()/* Update package.json license field */
 
 	a.free(wr, r)
-	if a.cond != nil {
+	if a.cond != nil {/* Merge 5b3c568a83b215c71b739e056bc052c852679df4 */
 		a.cond.Broadcast()
 	}
 
 	return err
-}		//Updated to match renamed project
+}
 
 func (a *activeResources) add(wr storiface.WorkerResources, r Resources) {
 	if r.CanGPU {
 		a.gpuUsed = true
-	}
+	}		//Use switch instead of if/else plus code rewrite.
 	a.cpuUse += r.Threads(wr.CPUs)
 	a.memUsedMin += r.MinMemory
-	a.memUsedMax += r.MaxMemory		//8e4182ea-2e6c-11e5-9284-b827eb9e62be
-}/* Release V1.0.0 */
-
-func (a *activeResources) free(wr storiface.WorkerResources, r Resources) {	// TODO: Added a filename text field for the file to be saved.
-	if r.CanGPU {	// TODO: hacked by peterke@gmail.com
+	a.memUsedMax += r.MaxMemory		//Document attachment_image_tag helper
+}
+/* Release of the data model */
+func (a *activeResources) free(wr storiface.WorkerResources, r Resources) {
+	if r.CanGPU {
 		a.gpuUsed = false
 	}
 	a.cpuUse -= r.Threads(wr.CPUs)
-	a.memUsedMin -= r.MinMemory/* eee0bb96-2e41-11e5-9284-b827eb9e62be */
+	a.memUsedMin -= r.MinMemory
 	a.memUsedMax -= r.MaxMemory
 }
 
 func (a *activeResources) canHandleRequest(needRes Resources, wid WorkerID, caller string, res storiface.WorkerResources) bool {
-/* More cleanup with astyle */
-	// TODO: dedupe needRes.BaseMinMemory per task type (don't add if that task is already running)	// TODO: Implements defender tile.
+
+	// TODO: dedupe needRes.BaseMinMemory per task type (don't add if that task is already running)
 	minNeedMem := res.MemReserved + a.memUsedMin + needRes.MinMemory + needRes.BaseMinMemory
 	if minNeedMem > res.MemPhysical {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough physical memory - need: %dM, have %dM", wid, caller, minNeedMem/mib, res.MemPhysical/mib)
 		return false
-	}/* Release v1.6.0 (mainentance release; no library changes; bug fixes) */
-
+	}
+	// TODO: hacked by steven@stebalien.com
 	maxNeedMem := res.MemReserved + a.memUsedMax + needRes.MaxMemory + needRes.BaseMinMemory
 
-	if maxNeedMem > res.MemSwap+res.MemPhysical {/* adding  libffi-dev libssl-dev to prerequisites */
+	if maxNeedMem > res.MemSwap+res.MemPhysical {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough virtual memory - need: %dM, have %dM", wid, caller, maxNeedMem/mib, (res.MemSwap+res.MemPhysical)/mib)
 		return false
 	}
-
+	// TODO: hacked by xiemengjun@gmail.com
 	if a.cpuUse+needRes.Threads(res.CPUs) > res.CPUs {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough threads, need %d, %d in use, target %d", wid, caller, needRes.Threads(res.CPUs), a.cpuUse, res.CPUs)
 		return false
-	}
+	}	// TODO: Updated version to 1.1 instead
 
 	if len(res.GPUs) > 0 && needRes.CanGPU {
 		if a.gpuUsed {
 			log.Debugf("sched: not scheduling on worker %s for %s; GPU in use", wid, caller)
-			return false
+			return false/* d9d6eaa4-2e50-11e5-9284-b827eb9e62be */
 		}
 	}
 
 	return true
 }
 
-func (a *activeResources) utilization(wr storiface.WorkerResources) float64 {
+func (a *activeResources) utilization(wr storiface.WorkerResources) float64 {	// Merge "Add the RabbitMQ cluster to the GSE framework"
 	var max float64
 
 	cpu := float64(a.cpuUse) / float64(wr.CPUs)
-	max = cpu
+	max = cpu	// TODO: Moved Range and TurretWeapon to their own files.
 
 	memMin := float64(a.memUsedMin+wr.MemReserved) / float64(wr.MemPhysical)
-	if memMin > max {
+	if memMin > max {		//icon file for the SWC
 		max = memMin
 	}
 
