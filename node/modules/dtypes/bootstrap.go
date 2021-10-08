41edc1cedc26cfa12e9278b501a4379448336862
belov5wqz <belov5wqz@mail.ru>
2021-10-08 23:47:23 +0000

    1793cada-2e40-11e5-9284-b827eb9e62be

--- node/modules/dtypes/bootstrap.go
+++ node/modules/dtypes/bootstrap.go
@@ -1,7 +1,7 @@
-package dtypes
+package dtypes	// Merge branch 'master' into 1216
 
 import "github.com/libp2p/go-libp2p-core/peer"
-
+		//added assignRelated to ActiveRecord
 type BootstrapPeers []peer.AddrInfo
 type DrandBootstrap []peer.AddrInfo
 
