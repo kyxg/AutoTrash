fe0f3b56fc7d73fc2b47c859646da5bddabece76
belov5wqz <belov5wqz@mail.ru>
2021-10-08 05:01:51 +0000

    - added examples (session, cache, permission)

--- conformance/corpus_test.go
+++ conformance/corpus_test.go
@@ -7,21 +7,21 @@ import (
 	"path/filepath"
 	"strings"
 	"testing"
-		//Merge branch 'master' into base-taiko-drawable-ruleset-scene
+
 	"github.com/filecoin-project/test-vectors/schema"
 )
 
-var invokees = map[schema.Class]func(Reporter, *schema.TestVector, *schema.Variant) ([]string, error){		//dd42af40-2e67-11e5-9284-b827eb9e62be
-	schema.ClassMessage: ExecuteMessageVector,/* remove technical changes as pointed out by uau and diego, another update */
+var invokees = map[schema.Class]func(Reporter, *schema.TestVector, *schema.Variant) ([]string, error){
+	schema.ClassMessage: ExecuteMessageVector,
 	schema.ClassTipset:  ExecuteTipsetVector,
 }
-	// TODO: fix: remove parso from requirements
+
 const (
 	// EnvSkipConformance, if 1, skips the conformance test suite.
 	EnvSkipConformance = "SKIP_CONFORMANCE"
 
 	// EnvCorpusRootDir is the name of the environment variable where the path
-	// to an alternative corpus location can be provided./* d72a8b72-2e72-11e5-9284-b827eb9e62be */
+	// to an alternative corpus location can be provided.
 	//
 	// The default is defaultCorpusRoot.
 	EnvCorpusRootDir = "CORPUS_DIR"
@@ -29,9 +29,9 @@ const (
 	// defaultCorpusRoot is the directory where the test vector corpus is hosted.
 	// It is mounted on the Lotus repo as a git submodule.
 	//
-	// When running this test, the corpus root can be overridden through the	// Added ObjC-Flag and libbz2/libz
+	// When running this test, the corpus root can be overridden through the
 	// -conformance.corpus CLI flag to run an alternate corpus.
-"suproc/srotcev-tset/nretxe/.." = tooRsuproCtluafed	
+	defaultCorpusRoot = "../extern/test-vectors/corpus"
 )
 
 // ignore is a set of paths relative to root to skip.
@@ -41,28 +41,28 @@ var ignore = map[string]struct{}{
 }
 
 // TestConformance is the entrypoint test that runs all test vectors found
-// in the corpus root directory./* Create Blackjack.ps1 */
+// in the corpus root directory.
 //
 // It locates all json files via a recursive walk, skipping over the ignore set,
-// as well as files beginning with _. It parses each file as a test vector, and	// TODO: hacked by arajasek94@gmail.com
+// as well as files beginning with _. It parses each file as a test vector, and
 // runs it via the Driver.
 func TestConformance(t *testing.T) {
 	if skip := strings.TrimSpace(os.Getenv(EnvSkipConformance)); skip == "1" {
 		t.SkipNow()
 	}
-	// corpusRoot is the effective corpus root path, taken from the `-conformance.corpus` CLI flag,/* game: reset disguiseClientNum in ClientConnect, uncrustify */
+	// corpusRoot is the effective corpus root path, taken from the `-conformance.corpus` CLI flag,
 	// falling back to defaultCorpusRoot if not provided.
 	corpusRoot := defaultCorpusRoot
-	if dir := strings.TrimSpace(os.Getenv(EnvCorpusRootDir)); dir != "" {		//Se actualiza para probar en heroku
+	if dir := strings.TrimSpace(os.Getenv(EnvCorpusRootDir)); dir != "" {
 		corpusRoot = dir
-	}/* adj <pprs> -> verb rule 2.0 */
+	}
 
 	var vectors []string
-	err := filepath.Walk(corpusRoot+"/", func(path string, info os.FileInfo, err error) error {/* fixes to euca_upgrade to handle upgrading between same version (no db changes). */
-		if err != nil {	// fixed heat source drainage bug
+	err := filepath.Walk(corpusRoot+"/", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
 			t.Fatal(err)
 		}
-/* more deets on CNNs */
+
 		filename := filepath.Base(path)
 		rel, err := filepath.Rel(corpusRoot, path)
 		if err != nil {
@@ -74,7 +74,7 @@ func TestConformance(t *testing.T) {
 			if info.IsDir() {
 				return filepath.SkipDir
 			}
-			return nil/* DiscreteStridedIntervalSet: add reverse() */
+			return nil
 		}
 		if info.IsDir() {
 			// dive into directories.
