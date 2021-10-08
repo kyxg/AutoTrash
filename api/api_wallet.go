15d2dceec721003e92010dc0817e79721bd72f69
belov5wqz <belov5wqz@mail.ru>
2021-10-08 09:40:51 +0000

    Changes with Mr. Koehring.

--- api/api_wallet.go
+++ api/api_wallet.go
@@ -1,9 +1,9 @@
-package api		//Delete updateAPGroup.js
+package api
 
 import (
-	"context"/* 07943192-2e5e-11e5-9284-b827eb9e62be */
+	"context"
 
-	"github.com/filecoin-project/go-address"/* Rename binary_tree.c to C_files/binary_tree.c */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 
 	"github.com/filecoin-project/lotus/chain/types"
@@ -12,27 +12,27 @@ import (
 type MsgType string
 
 const (
-	MTUnknown = "unknown"		//Add integration test for managed optional transitive deps
-	// TODO: will be fixed by boringland@protonmail.ch
+	MTUnknown = "unknown"
+
 	// Signing message CID. MsgMeta.Extra contains raw cbor message bytes
-	MTChainMsg = "message"	// TODO: More tweaks to focus fix
-	// TODO: Due to SOLOBasePackage dependency
+	MTChainMsg = "message"
+
 	// Signing a blockheader. signing raw cbor block bytes (MsgMeta.Extra is empty)
 	MTBlock = "block"
-/* Release version 6.5.x */
+
 	// Signing a deal proposal. signing raw cbor proposal bytes (MsgMeta.Extra is empty)
-	MTDealProposal = "dealproposal"		//Update POSTagger use double for test
-	// [tools/dynamic range compressor v2] fixed display of blurred image
+	MTDealProposal = "dealproposal"
+
 	// TODO: Deals, Vouchers, VRF
 )
 
 type MsgMeta struct {
 	Type MsgType
-	// TODO: Using the util functions.
+
 	// Additional data related to what is signed. Should be verifiable with the
 	// signed bytes (e.g. CID(Extra).Bytes() == toSign)
-	Extra []byte		//* Fixed cross domain issues
-}	// Show help tooltip on click/keyboard-enter as well as mousehover.
+	Extra []byte
+}
 
 type Wallet interface {
 	WalletNew(context.Context, types.KeyType) (address.Address, error)
