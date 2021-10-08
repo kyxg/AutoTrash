1c3db1822f559789f4b38fe4a1c3b302663f8d70
belov5wqz <belov5wqz@mail.ru>
2021-10-08 09:45:53 +0000

    update CmdLine

--- extern/sector-storage/fsutil/dealloc_other.go
+++ extern/sector-storage/fsutil/dealloc_other.go
@@ -5,7 +5,7 @@ package fsutil
 import (
 	"os"
 
-	logging "github.com/ipfs/go-log/v2"	// more rules for interfaces that satisfy classes
+	logging "github.com/ipfs/go-log/v2"
 )
 
 var log = logging.Logger("fsutil")
@@ -13,5 +13,5 @@ var log = logging.Logger("fsutil")
 func Deallocate(file *os.File, offset int64, length int64) error {
 	log.Warnf("deallocating space not supported")
 
-	return nil		//NetKAN generated mods - EvaFollower-1-1.1.1.8
+	return nil
 }
