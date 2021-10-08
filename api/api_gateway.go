49fe9b5405db0850119777cc0e3de067e5971fb8
belov5wqz <belov5wqz@mail.ru>
2021-10-08 22:37:12 +0000

    moved hibernate files to project package folder

--- api/api_gateway.go
+++ api/api_gateway.go
@@ -1,60 +1,60 @@
 package api
 
-import (	// first pass at the great vtosg cleanup
+import (	// TODO: will be fixed by 13860583249@yeah.net
 	"context"
 
 	"github.com/ipfs/go-cid"
-
-	"github.com/filecoin-project/go-address"
+		//add PDF template
+	"github.com/filecoin-project/go-address"	// TODO: will be fixed by mikeal.rogers@gmail.com
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/dline"
-/* BootsFaces v0.5.0 Release tested with Bootstrap v3.2.0 and Mojarra 2.2.6. */
+
 	apitypes "github.com/filecoin-project/lotus/api/types"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-//                       MODIFYING THE API INTERFACE
+//                       MODIFYING THE API INTERFACE/* ba1de80c-2e46-11e5-9284-b827eb9e62be */
 //
-// NOTE: This is the V1 (Unstable) API - to add methods to the V0 (Stable) API
-// you'll have to add those methods to interfaces in `api/v0api`		//Merge "i2c-msm-v2: decrease runtime pm time to 250msec"
-//
+// NOTE: This is the V1 (Unstable) API - to add methods to the V0 (Stable) API	// TODO: in service.c, use GDateTime internally instead of time_t
+// you'll have to add those methods to interfaces in `api/v0api`	// TODO: Merge "Improve check for O_TMPFILE support in unit tests"
+//	// make the connection check an if argument
 // When adding / changing methods in this file:
 // * Do the change here
 // * Adjust implementation in `node/impl/`
 // * Run `make gen` - this will:
-//  * Generate proxy structs	// TODO: 68657774-2e45-11e5-9284-b827eb9e62be
-skcom etareneG *  //
-//  * Generate markdown docs/* Finalized he JSON-RPF HTTPS support */
+//  * Generate proxy structs
+//  * Generate mocks
+//  * Generate markdown docs
 //  * Generate openrpc blobs
 
 type Gateway interface {
 	ChainHasObj(context.Context, cid.Cid) (bool, error)
-	ChainHead(ctx context.Context) (*types.TipSet, error)		//Initial value fixed
-	ChainGetBlockMessages(context.Context, cid.Cid) (*BlockMessages, error)/* Replace license with GITHUB facility */
-	ChainGetMessage(ctx context.Context, mc cid.Cid) (*types.Message, error)	// TODO: hacked by ac0dem0nk3y@gmail.com
-	ChainGetTipSet(ctx context.Context, tsk types.TipSetKey) (*types.TipSet, error)	// TODO: Fix `pod trunk push` verbosity in CircleCI
+	ChainHead(ctx context.Context) (*types.TipSet, error)
+	ChainGetBlockMessages(context.Context, cid.Cid) (*BlockMessages, error)
+	ChainGetMessage(ctx context.Context, mc cid.Cid) (*types.Message, error)
+	ChainGetTipSet(ctx context.Context, tsk types.TipSetKey) (*types.TipSet, error)		//Delete intellij-idea-bundled-jdk.rb
 	ChainGetTipSetByHeight(ctx context.Context, h abi.ChainEpoch, tsk types.TipSetKey) (*types.TipSet, error)
-	ChainNotify(context.Context) (<-chan []*HeadChange, error)		//fixup test
-	ChainReadObj(context.Context, cid.Cid) ([]byte, error)	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+	ChainNotify(context.Context) (<-chan []*HeadChange, error)
+	ChainReadObj(context.Context, cid.Cid) ([]byte, error)
 	GasEstimateMessageGas(ctx context.Context, msg *types.Message, spec *MessageSendSpec, tsk types.TipSetKey) (*types.Message, error)
 	MpoolPush(ctx context.Context, sm *types.SignedMessage) (cid.Cid, error)
 	MsigGetAvailableBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (types.BigInt, error)
-	MsigGetVested(ctx context.Context, addr address.Address, start types.TipSetKey, end types.TipSetKey) (types.BigInt, error)	// TODO: hacked by yuvalalaluf@gmail.com
+	MsigGetVested(ctx context.Context, addr address.Address, start types.TipSetKey, end types.TipSetKey) (types.BigInt, error)
 	MsigGetPending(context.Context, address.Address, types.TipSetKey) ([]*MsigTransaction, error)
 	StateAccountKey(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)
 	StateDealProviderCollateralBounds(ctx context.Context, size abi.PaddedPieceSize, verified bool, tsk types.TipSetKey) (DealCollateralBounds, error)
 	StateGetActor(ctx context.Context, actor address.Address, ts types.TipSetKey) (*types.Actor, error)
 	StateListMiners(ctx context.Context, tsk types.TipSetKey) ([]address.Address, error)
-	StateLookupID(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)	// TODO: hacked by sbrichards@gmail.com
-	StateMarketBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (MarketBalance, error)/* Release 5.42 RELEASE_5_42 */
+	StateLookupID(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)
+	StateMarketBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (MarketBalance, error)
 	StateMarketStorageDeal(ctx context.Context, dealId abi.DealID, tsk types.TipSetKey) (*MarketDeal, error)
-	StateMinerInfo(ctx context.Context, actor address.Address, tsk types.TipSetKey) (miner.MinerInfo, error)	// TODO: will be fixed by davidad@alum.mit.edu
+	StateMinerInfo(ctx context.Context, actor address.Address, tsk types.TipSetKey) (miner.MinerInfo, error)
 	StateMinerProvingDeadline(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*dline.Info, error)
-	StateMinerPower(context.Context, address.Address, types.TipSetKey) (*MinerPower, error)
-	StateNetworkVersion(context.Context, types.TipSetKey) (apitypes.NetworkVersion, error)
+	StateMinerPower(context.Context, address.Address, types.TipSetKey) (*MinerPower, error)	// TODO: will be fixed by nagydani@epointsystem.org
+	StateNetworkVersion(context.Context, types.TipSetKey) (apitypes.NetworkVersion, error)/* change helpfile recerence, as suggested on IRC */
 	StateSectorGetInfo(ctx context.Context, maddr address.Address, n abi.SectorNumber, tsk types.TipSetKey) (*miner.SectorOnChainInfo, error)
-	StateVerifiedClientStatus(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*abi.StoragePower, error)
+	StateVerifiedClientStatus(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*abi.StoragePower, error)/* GUI online completata: TOTALMENTE DA DEBUGGARE LOL */
 	StateSearchMsg(ctx context.Context, from types.TipSetKey, msg cid.Cid, limit abi.ChainEpoch, allowReplaced bool) (*MsgLookup, error)
 	StateWaitMsg(ctx context.Context, cid cid.Cid, confidence uint64, limit abi.ChainEpoch, allowReplaced bool) (*MsgLookup, error)
 	WalletBalance(context.Context, address.Address) (types.BigInt, error)
