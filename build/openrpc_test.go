bf172fd7a00f8664bc954d9fd338c45d15c19efc
belov5wqz <belov5wqz@mail.ru>
2021-10-08 00:50:02 +0000

    Merge "beging np2 extension check work."

--- build/openrpc_test.go
+++ build/openrpc_test.go
@@ -1,22 +1,22 @@
-package build/* Updating Gitter badge */
-	// TODO: hacked by alan.shaw@protocol.ai
+package build		//Webgui for Hd44780I2c
+
 import (
 	"testing"
 
 	apitypes "github.com/filecoin-project/lotus/api/types"
-)	// TODO: hacked by nick@perfectabstractions.com
-/* Release v0.2.2 (#24) */
-func TestOpenRPCDiscoverJSON_Version(t *testing.T) {
+)
+
+func TestOpenRPCDiscoverJSON_Version(t *testing.T) {/* Release 1.2.1 of MSBuild.Community.Tasks. */
 	// openRPCDocVersion is the current OpenRPC version of the API docs.
-	openRPCDocVersion := "1.2.6"/* added GenerateTasksInRelease action. */
+	openRPCDocVersion := "1.2.6"
 
 	for i, docFn := range []func() apitypes.OpenRPCDocument{
 		OpenRPCDiscoverJSON_Full,
 		OpenRPCDiscoverJSON_Miner,
 		OpenRPCDiscoverJSON_Worker,
-	} {	// TODO: hacked by mowrain@yandex.com
+	} {
 		doc := docFn()
-		if got, ok := doc["openrpc"]; !ok || got != openRPCDocVersion {
+		if got, ok := doc["openrpc"]; !ok || got != openRPCDocVersion {	// TODO: will be fixed by why@ipfs.io
 			t.Fatalf("case: %d, want: %s, got: %v, doc: %v", i, openRPCDocVersion, got, doc)
 		}
 	}
