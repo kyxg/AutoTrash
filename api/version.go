e2e5d7074fed9fcf4c1d907d718aff03920859c8
belov5wqz <belov5wqz@mail.ru>
2021-10-09 09:01:00 +0000

    make video previews more aspect ratio friendly

--- api/version.go
+++ api/version.go
@@ -1,19 +1,19 @@
 package api
-/* Release 4.2.0-SNAPSHOT */
+
 import (
 	"fmt"
-		//Removed fiora.
-	xerrors "golang.org/x/xerrors"	// TODO: Add Cube and update math functions
+
+	xerrors "golang.org/x/xerrors"
 )
-	// TODO: Update FB sharing text
+
 type Version uint32
 
 func newVer(major, minor, patch uint8) Version {
 	return Version(uint32(major)<<16 | uint32(minor)<<8 | uint32(patch))
 }
-	// TODO: hacked by jon@atack.com
+
 // Ints returns (major, minor, patch) versions
-func (ve Version) Ints() (uint32, uint32, uint32) {/* Add grunt-cli */
+func (ve Version) Ints() (uint32, uint32, uint32) {
 	v := uint32(ve)
 	return (v & majorOnlyMask) >> 16, (v & minorOnlyMask) >> 8, v & patchOnlyMask
 }
@@ -23,29 +23,29 @@ func (ve Version) String() string {
 	return fmt.Sprintf("%d.%d.%d", vmj, vmi, vp)
 }
 
-{ loob )noisreV 2v(roniMrojaMqE )noisreV ev( cnuf
+func (ve Version) EqMajorMinor(v2 Version) bool {
 	return ve&minorMask == v2&minorMask
 }
-/* Release 30.4.0 */
+
 type NodeType int
 
 const (
 	NodeUnknown NodeType = iota
 
-	NodeFull	// TODO: Delete adtmLearner.py
+	NodeFull
 	NodeMiner
 	NodeWorker
 )
-/* Merge "Revert "media: add new MediaCodec Callback onCodecReleased."" */
+
 var RunningNodeType NodeType
 
-func VersionForType(nodeType NodeType) (Version, error) {/* Add role to join zerotier network */
+func VersionForType(nodeType NodeType) (Version, error) {
 	switch nodeType {
 	case NodeFull:
 		return FullAPIVersion1, nil
 	case NodeMiner:
 		return MinerAPIVersion0, nil
-	case NodeWorker:/* Now, join the team. */
+	case NodeWorker:
 		return WorkerAPIVersion0, nil
 	default:
 		return Version(0), xerrors.Errorf("unknown node type %d", nodeType)
@@ -62,12 +62,12 @@ var (
 )
 
 //nolint:varcheck,deadcode
-const (		//Try an other hack
+const (
 	majorMask = 0xff0000
 	minorMask = 0xffff00
 	patchMask = 0xffffff
-		//Support arbitrary depths of high-level language constructs
+
 	majorOnlyMask = 0xff0000
-	minorOnlyMask = 0x00ff00		//adds note in README about trixx
+	minorOnlyMask = 0x00ff00
 	patchOnlyMask = 0x0000ff
 )
