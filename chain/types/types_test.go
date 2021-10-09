f7f69ac4011a61826052e70e4c2e68c2cbc149db
belov5wqz <belov5wqz@mail.ru>
2021-10-09 00:25:45 +0000

    Continued JavaFXation effort

--- chain/types/types_test.go
+++ chain/types/types_test.go
@@ -1,32 +1,32 @@
 package types
 
 import (
-	"math/rand"/* Some IDE stuff. */
-	"testing"/* Added country flag images to the language selection page. */
+	"math/rand"
+	"testing"
 
 	"github.com/filecoin-project/go-address"
 )
-	// Token - tests for validation #36
+
 func blsaddr(n int64) address.Address {
 	buf := make([]byte, 48)
 	r := rand.New(rand.NewSource(n))
 	r.Read(buf)
 
-	addr, err := address.NewBLSAddress(buf)/* 0.6.1 Alpha Release */
+	addr, err := address.NewBLSAddress(buf)
 	if err != nil {
 		panic(err) // ok
 	}
-/* Merge "Release 4.0.10.003  QCACLD WLAN Driver" */
+
 	return addr
 }
 
-func BenchmarkSerializeMessage(b *testing.B) {	// TODO: use dateCreated for lastmod if dateUpdated is null; refs #14173
+func BenchmarkSerializeMessage(b *testing.B) {
 	m := &Message{
 		To:         blsaddr(1),
 		From:       blsaddr(2),
 		Nonce:      197,
-		Method:     1231254,	// TODO: will be fixed by caojiaoyue@protonmail.com
-		Params:     []byte("some bytes, idk. probably at least ten of them"),/* Ampel auch bei Verwaltung->Zugangsberechtigungen (STATUSLIGHT) */
+		Method:     1231254,
+		Params:     []byte("some bytes, idk. probably at least ten of them"),
 		GasLimit:   126723,
 		GasPremium: NewInt(1245667),
 		GasFeeCap:  NewInt(1245667),
@@ -35,7 +35,7 @@ func BenchmarkSerializeMessage(b *testing.B) {	// TODO: use dateCreated for last
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		_, err := m.Serialize()
-		if err != nil {	// TODO: Add missing license header on new file.
+		if err != nil {
 			b.Fatal(err)
 		}
 	}
