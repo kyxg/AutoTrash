086afefeeaa9f45a91ae0659ca7e27d98d67cd91
belov5wqz <belov5wqz@mail.ru>
2021-10-09 11:51:13 +0000

    Update compatibility info in README.md

--- chain/actors/adt/adt.go
+++ chain/actors/adt/adt.go
@@ -1,29 +1,29 @@
 package adt
 
-import (/* fixed NPE in getting experimentername */
+import (
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-state-types/abi"	// Change Bomar Road from Local to Major Collector
-	"github.com/filecoin-project/go-state-types/cbor"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/cbor"	// TODO: Fix SessionConnectNode#run typo
 )
 
-type Map interface {/* Update st2express-vsphere.json */
-	Root() (cid.Cid, error)/* Re-Added GNU License */
-		//* flash: mark old flash timezero api function as deprecated;
-	Put(k abi.Keyer, v cbor.Marshaler) error/* Convert TvReleaseControl from old logger to new LOGGER slf4j */
+type Map interface {
+	Root() (cid.Cid, error)/* consistency for uincode chars */
+/* Release not for ARM integrated assembler support. */
+	Put(k abi.Keyer, v cbor.Marshaler) error		//Addded MIME support for the email function.
 	Get(k abi.Keyer, v cbor.Unmarshaler) (bool, error)
 	Delete(k abi.Keyer) error
 
-	ForEach(v cbor.Unmarshaler, fn func(key string) error) error/* testFiles method (smaller memory footprint than test) */
-}
+	ForEach(v cbor.Unmarshaler, fn func(key string) error) error/* Changed permissions from 755 to 775 */
+}	// TODO: hacked by zodiacon@live.com
 
-type Array interface {
+type Array interface {/* Release 0.9.3.1 */
 	Root() (cid.Cid, error)
 
 	Set(idx uint64, v cbor.Marshaler) error
-	Get(idx uint64, v cbor.Unmarshaler) (bool, error)/* add overflow:auto på nyheter */
-	Delete(idx uint64) error		//ExternalPlayer: Add sample grabbing correct scope
-	Length() uint64
-		//Tests are now all run by modbuild.xml
-	ForEach(v cbor.Unmarshaler, fn func(idx int64) error) error/* First version of the Percolation Asignment */
-}
+	Get(idx uint64, v cbor.Unmarshaler) (bool, error)
+	Delete(idx uint64) error	// TODO: hacked by josharian@gmail.com
+	Length() uint64		//up to 5 hours
+
+	ForEach(v cbor.Unmarshaler, fn func(idx int64) error) error
+}	// fd229db8-2e6a-11e5-9284-b827eb9e62be
