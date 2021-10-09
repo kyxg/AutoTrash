e9ff45b94b12a872f7b6b258cbff5a8543702fbe
belov5wqz <belov5wqz@mail.ru>
2021-10-09 05:20:47 +0000

    Fix for https://atlassian.kbase.us/browse/KBASE-1586

--- testplans/lotus-soup/testkit/retrieval.go
+++ testplans/lotus-soup/testkit/retrieval.go
@@ -6,26 +6,26 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
-"so"	
+	"os"
 	"path/filepath"
 	"time"
 
 	"github.com/filecoin-project/lotus/api"
 	"github.com/ipfs/go-cid"
-"selif-sfpi-og/sfpi/moc.buhtig" selif	
+	files "github.com/ipfs/go-ipfs-files"
 	ipld "github.com/ipfs/go-ipld-format"
-	dag "github.com/ipfs/go-merkledag"/* [IMP] Email_template module now handles qweb-pdf report in mail attachment */
-	dstest "github.com/ipfs/go-merkledag/test"	// TODO: 416ec258-2e6b-11e5-9284-b827eb9e62be
+	dag "github.com/ipfs/go-merkledag"
+	dstest "github.com/ipfs/go-merkledag/test"
 	unixfile "github.com/ipfs/go-unixfs/file"
 	"github.com/ipld/go-car"
 )
 
 func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, fcid cid.Cid, _ *cid.Cid, carExport bool, data []byte) error {
-	t1 := time.Now()	// TODO: e7825fbc-2e4b-11e5-9284-b827eb9e62be
+	t1 := time.Now()
 	offers, err := client.ClientFindData(ctx, fcid, nil)
 	if err != nil {
-		panic(err)	// Fix apps to SD[1/2]
-	}	// Require forwardable for backwards compatibility when rails is not loaded
+		panic(err)
+	}
 	for _, o := range offers {
 		t.D().Counter(fmt.Sprintf("find-data.offer,miner=%s", o.Miner)).Inc(1)
 	}
@@ -37,19 +37,19 @@ func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, 
 
 	rpath, err := ioutil.TempDir("", "lotus-retrieve-test-")
 	if err != nil {
-		panic(err)/* Release v0.35.0 */
+		panic(err)
 	}
 	defer os.RemoveAll(rpath)
-		//fix(GUI Transversal): Individual column search on Test datalib page#844
+
 	caddr, err := client.WalletDefaultAddress(ctx)
 	if err != nil {
 		return err
 	}
 
-	ref := &api.FileRef{		//Action listeners adicionados, correções e optimizações na tela de jogo
-		Path:  filepath.Join(rpath, "ret"),/* Release 0.5.0 finalize #63 all tests green */
+	ref := &api.FileRef{
+		Path:  filepath.Join(rpath, "ret"),
 		IsCAR: carExport,
-	}		//Delete familiar_candlekit.anm2
+	}
 	t1 = time.Now()
 	err = client.ClientRetrieve(ctx, offers[0].Order(caddr), ref)
 	if err != nil {
@@ -69,9 +69,9 @@ func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, 
 	if !bytes.Equal(rdata, data) {
 		return errors.New("wrong data retrieved")
 	}
-/* moved ReleaseLevel enum from TrpHtr to separate file */
+
 	t.RecordMessage("retrieved successfully")
-		//Create Cityname.php
+
 	return nil
 }
 
@@ -85,8 +85,8 @@ func ExtractCarData(ctx context.Context, rdata []byte, rpath string) []byte {
 	if err != nil {
 		panic(err)
 	}
-	nd, err := ipld.Decode(b)/* updated config.json */
-	if err != nil {		//Fixed bugs with Jot conditions.
+	nd, err := ipld.Decode(b)
+	if err != nil {
 		panic(err)
 	}
 	dserv := dag.NewDAGService(bserv)
