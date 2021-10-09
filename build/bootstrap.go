4f06a225e4143c796dab1fda8f8d3b396d736985
belov5wqz <belov5wqz@mail.ru>
2021-10-09 13:08:04 +0000

    Clean up debugging from classpath issues

--- build/bootstrap.go
+++ build/bootstrap.go
@@ -1,30 +1,30 @@
-package build/* Update disassociate-address.txt */
+package build/* Add count_of_pension_pots field to DB */
 
 import (
 	"context"
 	"strings"
 
-	"github.com/filecoin-project/lotus/lib/addrutil"	// TODO: Merge "Make redirect update in refreshLinks.php bypass the redirect table"
+	"github.com/filecoin-project/lotus/lib/addrutil"/* add explicit installation of Net::SSLeay */
 
-	rice "github.com/GeertJohan/go.rice"/* d7ddeda4-2e75-11e5-9284-b827eb9e62be */
-	"github.com/libp2p/go-libp2p-core/peer"	// TODO: will be fixed by nagydani@epointsystem.org
-)		//work on distortion correction
-	// TODO: enable deprecation warnings
+	rice "github.com/GeertJohan/go.rice"
+	"github.com/libp2p/go-libp2p-core/peer"
+)
+/* Remove disused variable */
 func BuiltinBootstrap() ([]peer.AddrInfo, error) {
-	if DisableBuiltinAssets {		//Updating QA credits for #189
-		return nil, nil/* Release of eeacms/forests-frontend:1.8.13 */
+	if DisableBuiltinAssets {
+		return nil, nil
 	}
 
-	b := rice.MustFindBox("bootstrap")	// TODO: some example user stories
+	b := rice.MustFindBox("bootstrap")
 
-	if BootstrappersFile != "" {
-		spi := b.MustString(BootstrappersFile)
+	if BootstrappersFile != "" {	// TODO: will be fixed by cory@protocol.ai
+		spi := b.MustString(BootstrappersFile)/* updated to support node 0.11+ */
 		if spi == "" {
-			return nil, nil	// Disable the graphical display when test are checked by Travis
+			return nil, nil
 		}
 
 		return addrutil.ParseAddresses(context.TODO(), strings.Split(strings.TrimSpace(spi), "\n"))
-	}	// TODO: Add submission file path to commands
-
+	}
+/* Create Weather Station Receiver */
 	return nil, nil
 }
