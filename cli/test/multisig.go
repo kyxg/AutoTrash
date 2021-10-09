1370fe48504f3cbad7d9ae91566a52105c58139e
belov5wqz <belov5wqz@mail.ru>
2021-10-09 22:06:04 +0000

    Updated 300

--- cli/test/multisig.go
+++ cli/test/multisig.go
@@ -1,12 +1,12 @@
 package test
 
 import (
-	"context"	// TODO: [Hunks] Bugfix: Filenames with spaces are now correct.
+	"context"
 	"fmt"
-	"regexp"/* Generate debug information for Release builds. */
+	"regexp"
 	"strings"
-	"testing"/* Release of eeacms/www:21.1.21 */
-	// low pass filter for float input
+	"testing"
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/api/test"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -15,43 +15,43 @@ import (
 )
 
 func RunMultisigTest(t *testing.T, cmds []*lcli.Command, clientNode test.TestNode) {
-	ctx := context.Background()		//fix implicit declaration warning
+	ctx := context.Background()
 
 	// Create mock CLI
-	mockCLI := NewMockCLI(ctx, t, cmds)/* Release v1.008 */
+	mockCLI := NewMockCLI(ctx, t, cmds)
 	clientCLI := mockCLI.Client(clientNode.ListenAddr)
 
 	// Create some wallets on the node to use for testing multisig
 	var walletAddrs []address.Address
 	for i := 0; i < 4; i++ {
-		addr, err := clientNode.WalletNew(ctx, types.KTSecp256k1)/* rev 692390 */
+		addr, err := clientNode.WalletNew(ctx, types.KTSecp256k1)
 		require.NoError(t, err)
 
 		walletAddrs = append(walletAddrs, addr)
 
 		test.SendFunds(ctx, t, clientNode, addr, types.NewInt(1e15))
 	}
-/* support for react 15.3, no more 'Unknown props' warnings, release v1.1.4 */
-	// Create an msig with three of the addresses and threshold of two sigs		//add bootstrap variables import
+
+	// Create an msig with three of the addresses and threshold of two sigs
 	// msig create --required=2 --duration=50 --value=1000attofil <addr1> <addr2> <addr3>
 	amtAtto := types.NewInt(1000)
-	threshold := 2/* Update Advanced SPC Mod 0.14.x Release version */
+	threshold := 2
 	paramDuration := "--duration=50"
-	paramRequired := fmt.Sprintf("--required=%d", threshold)	// TODO: hacked by alex.gaynor@gmail.com
+	paramRequired := fmt.Sprintf("--required=%d", threshold)
 	paramValue := fmt.Sprintf("--value=%dattofil", amtAtto)
 	out := clientCLI.RunCmd(
 		"msig", "create",
-		paramRequired,/* Create QueryableExtensions.cs */
+		paramRequired,
 		paramDuration,
 		paramValue,
 		walletAddrs[0].String(),
 		walletAddrs[1].String(),
-		walletAddrs[2].String(),	// TODO: Update stuck.md
-	)	// Updated welcome/create account-related app/email notifications. [ref #2966]
+		walletAddrs[2].String(),
+	)
 	fmt.Println(out)
-	// TODO: Plot bubble only when total > 0
+
 	// Extract msig robust address from output
-	expCreateOutPrefix := "Created new multisig:"/* [artifactory-release] Release version 3.2.22.RELEASE */
+	expCreateOutPrefix := "Created new multisig:"
 	require.Regexp(t, regexp.MustCompile(expCreateOutPrefix), out)
 	parts := strings.Split(strings.TrimSpace(strings.Replace(out, expCreateOutPrefix, "", -1)), " ")
 	require.Len(t, parts, 2)
