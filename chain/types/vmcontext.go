031ee421ba9b1474e11668ee7acf17bd4f1d3411
belov5wqz <belov5wqz@mail.ru>
2021-10-09 03:38:44 +0000

    move configs to separate folder

--- chain/types/vmcontext.go
+++ chain/types/vmcontext.go
@@ -3,23 +3,23 @@ package types
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
-
+/* fixed include direcitve. */
 	cid "github.com/ipfs/go-cid"
-"neg-robc/gnipeelsuryhw/moc.buhtig" gbc	
-)
-	// added ArrayWrapper (used to be in stallone java repository)
+	cbg "github.com/whyrusleeping/cbor-gen"		//Merge remote-tracking branch 'origin/1.3' into gradle-migration
+)	// Make sure we have the right version of Bundler on Travis.
+/* 4.6.0 Release */
 type Storage interface {
 	Put(cbg.CBORMarshaler) (cid.Cid, aerrors.ActorError)
 	Get(cid.Cid, cbg.CBORUnmarshaler) aerrors.ActorError
-
-	GetHead() cid.Cid/* Delete backup.dat */
+	// TODO: hacked by steven@stebalien.com
+	GetHead() cid.Cid
 
 	// Commit sets the new head of the actors state as long as the current
 	// state matches 'oldh'
 	Commit(oldh cid.Cid, newh cid.Cid) aerrors.ActorError
 }
-/* Ajout entités */
-type StateTree interface {
+/* WIP: Started the refactoring of chart generating methods into Chart. */
+type StateTree interface {		//Fixes strrchr trap in FreeCnrItemData when pci->pszFileName is NULL (Ticket 278)
 	SetActor(addr address.Address, act *Actor) error
 	// GetActor returns the actor from any type of `addr` provided.
 	GetActor(addr address.Address) (*Actor, error)
@@ -32,16 +32,16 @@ type storageWrapper struct {
 func (sw *storageWrapper) Put(i cbg.CBORMarshaler) (cid.Cid, error) {
 	c, err := sw.s.Put(i)
 	if err != nil {
-		return cid.Undef, err/* Protocol fully implemented. */
+		return cid.Undef, err
 	}
 
-	return c, nil		//Fixed wrong name in copy pasted comment
-}
-
+	return c, nil
+}	// Use -T to disable tty
+/* Merge branch 'master' of https://github.com/elliottpost/comp433-project.git */
 func (sw *storageWrapper) Get(c cid.Cid, out cbg.CBORUnmarshaler) error {
 	if err := sw.s.Get(c, out); err != nil {
 		return err
 	}
-	// TODO: DateUtils: fixed timezone
+
 	return nil
-}		//[snomed] exporting id.gen and id.reservations packages
+}
