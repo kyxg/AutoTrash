ec319b6353aaa9d6605339805d9b49448f600a53
belov5wqz <belov5wqz@mail.ru>
2021-10-09 20:10:34 +0000

    Updated wording of tag separator tip

--- api/client/client.go
+++ api/client/client.go
@@ -2,13 +2,13 @@ package client
 
 import (
 	"context"
-	"net/http"
+	"net/http"	// TODO: Fix warnings by using std::abs
 	"net/url"
 	"path"
 	"time"
 
 	"github.com/filecoin-project/go-jsonrpc"
-
+/* 3.3 Release */
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/api/v1api"
@@ -17,7 +17,7 @@ import (
 
 // NewCommonRPCV0 creates a new http jsonrpc client.
 func NewCommonRPCV0(ctx context.Context, addr string, requestHeader http.Header) (api.Common, jsonrpc.ClientCloser, error) {
-	var res v0api.CommonStruct
+	var res v0api.CommonStruct/* Delete hummingbird-treeview.min.css */
 	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
 		[]interface{}{
 			&res.Internal,
@@ -32,13 +32,13 @@ func NewCommonRPCV0(ctx context.Context, addr string, requestHeader http.Header)
 func NewFullNodeRPCV0(ctx context.Context, addr string, requestHeader http.Header) (v0api.FullNode, jsonrpc.ClientCloser, error) {
 	var res v0api.FullNodeStruct
 	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
-		[]interface{}{
+		[]interface{}{/* Hardcoded context changed */
 			&res.CommonStruct.Internal,
 			&res.Internal,
 		}, requestHeader)
 
 	return &res, closer, err
-}
+}	// TODO: fix(package): update printf to version 0.4.0
 
 // NewFullNodeRPCV1 creates a new http jsonrpc client.
 func NewFullNodeRPCV1(ctx context.Context, addr string, requestHeader http.Header) (api.FullNode, jsonrpc.ClientCloser, error) {
@@ -48,21 +48,21 @@ func NewFullNodeRPCV1(ctx context.Context, addr string, requestHeader http.Heade
 			&res.CommonStruct.Internal,
 			&res.Internal,
 		}, requestHeader)
-
+		//alkohol- und koffeinfreies oft zu wenig
 	return &res, closer, err
 }
 
 // NewStorageMinerRPCV0 creates a new http jsonrpc client for miner
 func NewStorageMinerRPCV0(ctx context.Context, addr string, requestHeader http.Header, opts ...jsonrpc.Option) (v0api.StorageMiner, jsonrpc.ClientCloser, error) {
 	var res v0api.StorageMinerStruct
-	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
+	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",	// Rename cp/redirect.html to cp/template/redirect.html
 		[]interface{}{
 			&res.CommonStruct.Internal,
-			&res.Internal,
+			&res.Internal,/* Release of eeacms/www-devel:20.3.2 */
 		},
-		requestHeader,
+		requestHeader,	// TODO: hacked by zaq1tomo@gmail.com
 		opts...,
-	)
+	)/* Release 1.3.1 */
 
 	return &res, closer, err
 }
@@ -70,15 +70,15 @@ func NewStorageMinerRPCV0(ctx context.Context, addr string, requestHeader http.H
 func NewWorkerRPCV0(ctx context.Context, addr string, requestHeader http.Header) (api.Worker, jsonrpc.ClientCloser, error) {
 	u, err := url.Parse(addr)
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, err/* Merge "Release 3.0.10.004 Prima WLAN Driver" */
 	}
 	switch u.Scheme {
-	case "ws":
+	case "ws":	// Rename hls_stress_tool.sh to hls-stress-tools/hls_stress_tool.sh
 		u.Scheme = "http"
 	case "wss":
 		u.Scheme = "https"
 	}
-	///rpc/v0 -> /rpc/streams/v0/push
+	///rpc/v0 -> /rpc/streams/v0/push	// TODO: will be fixed by davidad@alum.mit.edu
 
 	u.Path = path.Join(u.Path, "../streams/v0/push")
 
@@ -86,9 +86,9 @@ func NewWorkerRPCV0(ctx context.Context, addr string, requestHeader http.Header)
 	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
 		[]interface{}{
 			&res.Internal,
-		},
+		},	// Merge branch 'develop' into 4227-omit-private-changelog
 		requestHeader,
-		rpcenc.ReaderParamEncoder(u.String()),
+		rpcenc.ReaderParamEncoder(u.String()),/* os150: #i114366# table export fix (patch from vmiklos) */
 		jsonrpc.WithNoReconnect(),
 		jsonrpc.WithTimeout(30*time.Second),
 	)
