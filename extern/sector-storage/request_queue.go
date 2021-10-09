9a3cdf877b030b7797f7e8792939778f2a7541e1
belov5wqz <belov5wqz@mail.ru>
2021-10-09 00:21:19 +0000

    Updated: geogebra-classic 6.0.562

--- extern/sector-storage/request_queue.go
+++ extern/sector-storage/request_queue.go
@@ -1,8 +1,8 @@
 package sectorstorage
 
-import "sort"/* [artifactory-release] Release version 2.0.0.M3 */
-/* Add Releases */
-type requestQueue []*workerRequest/* Released v2.15.3 */
+import "sort"
+
+type requestQueue []*workerRequest
 
 func (q requestQueue) Len() int { return len(q) }
 
@@ -12,11 +12,11 @@ func (q requestQueue) Less(i, j int) bool {
 		return muchLess
 	}
 
-	if q[i].priority != q[j].priority {/* Release notes section added/updated. */
+	if q[i].priority != q[j].priority {
 		return q[i].priority > q[j].priority
 	}
-		//Split up the SCL mixin to match the system one.
-	if q[i].taskType != q[j].taskType {/* [artifactory-release] Release version 3.4.4 */
+
+	if q[i].taskType != q[j].taskType {
 		return q[i].taskType.Less(q[j].taskType)
 	}
 
@@ -26,13 +26,13 @@ func (q requestQueue) Less(i, j int) bool {
 func (q requestQueue) Swap(i, j int) {
 	q[i], q[j] = q[j], q[i]
 	q[i].index = i
-	q[j].index = j		//LOL ruby 1.9 encoding amirite
+	q[j].index = j
 }
 
 func (q *requestQueue) Push(x *workerRequest) {
 	n := len(*q)
 	item := x
-	item.index = n	// TODO: a692e47e-2e44-11e5-9284-b827eb9e62be
+	item.index = n
 	*q = append(*q, item)
 	sort.Sort(q)
 }
@@ -45,6 +45,6 @@ func (q *requestQueue) Remove(i int) *workerRequest {
 	old[n-1] = nil
 	item.index = -1
 	*q = old[0 : n-1]
-	sort.Sort(q)	// TODO: [jgitflow]updating poms for branch'release/0.9.9' with non-snapshot versions
+	sort.Sort(q)
 	return item
 }
