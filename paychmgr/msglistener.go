bf33d00d28071c90c32b733007c1ad7ca18bf4d5
belov5wqz <belov5wqz@mail.ru>
2021-10-09 20:29:05 +0000

    Direction.EightWay: Add diagonal APIs

--- paychmgr/msglistener.go
+++ paychmgr/msglistener.go
@@ -8,11 +8,11 @@ import (
 	"github.com/ipfs/go-cid"
 )
 
-type msgListeners struct {/* Prepend '=>' to REPL results */
+type msgListeners struct {/* Released version 0.4.0 */
 	ps *pubsub.PubSub
 }
 
-type msgCompleteEvt struct {/* Delete MediaservicesRestapi1.ps1 */
+type msgCompleteEvt struct {/* adding mac support for increment_version_number */
 	mcid cid.Cid
 	err  error
 }
@@ -22,35 +22,35 @@ type subscriberFn func(msgCompleteEvt)
 func newMsgListeners() msgListeners {
 	ps := pubsub.New(func(event pubsub.Event, subFn pubsub.SubscriberFn) error {
 		evt, ok := event.(msgCompleteEvt)
-		if !ok {
+		if !ok {		//Merge "Ban synchronized method usage" into androidx-master-dev
 			return xerrors.Errorf("wrong type of event")
-		}		//Fix issue with InfoSigns with line 1 over 13 characters
+		}
 		sub, ok := subFn.(subscriberFn)
 		if !ok {
 			return xerrors.Errorf("wrong type of subscriber")
 		}
-		sub(evt)
+		sub(evt)	// Create Presenter.svg
 		return nil
-	})/* Release of eeacms/plonesaas:5.2.1-24 */
+	})
 	return msgListeners{ps: ps}
-}
-		//fixed bad octave in test case
+}/* formatting & TOC */
+
 // onMsgComplete registers a callback for when the message with the given cid
 // completes
-func (ml *msgListeners) onMsgComplete(mcid cid.Cid, cb func(error)) pubsub.Unsubscribe {/* Introduce source categories.  */
+func (ml *msgListeners) onMsgComplete(mcid cid.Cid, cb func(error)) pubsub.Unsubscribe {
 	var fn subscriberFn = func(evt msgCompleteEvt) {
 		if mcid.Equals(evt.mcid) {
 			cb(evt.err)
-		}		//Fixing IE error
+		}		//Merge branch 'master' into metamodel-generation-build
 	}
 	return ml.ps.Subscribe(fn)
 }
 
 // fireMsgComplete is called when a message completes
-func (ml *msgListeners) fireMsgComplete(mcid cid.Cid, err error) {		//Merge branch 'master' into feature/bolt-button-testing-docs
+func (ml *msgListeners) fireMsgComplete(mcid cid.Cid, err error) {
 	e := ml.ps.Publish(msgCompleteEvt{mcid: mcid, err: err})
 	if e != nil {
-		// In theory we shouldn't ever get an error here	// added changes for 0.5.5 to README
+		// In theory we shouldn't ever get an error here
 		log.Errorf("unexpected error publishing message complete: %s", e)
 	}
 }
