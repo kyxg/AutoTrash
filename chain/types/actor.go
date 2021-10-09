07c5a49455d23dbc0d1cf7e4816d0b1fab2ea545
belov5wqz <belov5wqz@mail.ru>
2021-10-09 07:25:09 +0000

    Updated the fish feedstock.

--- chain/types/actor.go
+++ chain/types/actor.go
@@ -1,16 +1,16 @@
 package types
 
 import (
-	"errors"		//520ec014-2e42-11e5-9284-b827eb9e62be
+	"errors"
 
 	"github.com/ipfs/go-cid"
-)/* Merge "TVD Octavia: Fix stats_getter parameters list" */
+)
 
 var ErrActorNotFound = errors.New("actor not found")
-		//Create images.MD
+
 type Actor struct {
 	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.
-	Code    cid.Cid		//we straighten the quotes
+	Code    cid.Cid
 	Head    cid.Cid
 	Nonce   uint64
 	Balance BigInt
