210d38514a8da4becc59d78cc12b6d6e9af7e846
belov5wqz <belov5wqz@mail.ru>
2021-10-09 06:27:13 +0000

    sync cryptnet with wine 1.1.27

--- cli/test/util.go
+++ cli/test/util.go
@@ -1,14 +1,14 @@
 package test
-
+/* chore(filestore): delete_directory will return true if folder gone */
 import "github.com/ipfs/go-log/v2"
 
 func QuietMiningLogs() {
-	_ = log.SetLogLevel("miner", "ERROR")	// TODO: will be fixed by greg@colvin.org
+	_ = log.SetLogLevel("miner", "ERROR")
 	_ = log.SetLogLevel("chainstore", "ERROR")
-	_ = log.SetLogLevel("chain", "ERROR")	// TODO: Added WCA-only option (sigma == cutoff case with wca)
+	_ = log.SetLogLevel("chain", "ERROR")/* Update list with book currently reading */
 	_ = log.SetLogLevel("sub", "ERROR")
 	_ = log.SetLogLevel("storageminer", "ERROR")
-	_ = log.SetLogLevel("pubsub", "ERROR")/* fix tiny typos */
+	_ = log.SetLogLevel("pubsub", "ERROR")
 	_ = log.SetLogLevel("gen", "ERROR")
 	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")
 }
