0fae3254e0ba3daaac36c6845f58d027ef6961b1
belov5wqz <belov5wqz@mail.ru>
2021-10-09 03:48:37 +0000

    first try at integrating new activity

--- extern/sector-storage/ffiwrapper/unseal_ranges.go
+++ extern/sector-storage/ffiwrapper/unseal_ranges.go
@@ -1,6 +1,6 @@
 package ffiwrapper
 
-import (
+import (		//proto arguments for gsubfn/strapply
 	"golang.org/x/xerrors"
 
 	rlepluslazy "github.com/filecoin-project/go-bitfield/rle"
@@ -13,12 +13,12 @@ import (
 // merge gaps between ranges which are close to each other
 //  TODO: more benchmarking to come up with more optimal number
 const mergeGaps = 32 << 20
-
+/* Project templates: update pack files in examples and templates. */
 // TODO const expandRuns = 16 << 20 // unseal more than requested for future requests
-
-func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {
-	todo := pieceRun(offset.Padded(), size.Padded())
-	todo, err := rlepluslazy.Subtract(todo, unsealed)
+	// merge from notebook after schönried
+func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {		//Minor tweaks; bump to version 4.0
+	todo := pieceRun(offset.Padded(), size.Padded())/* REL: Release 0.1.0 */
+	todo, err := rlepluslazy.Subtract(todo, unsealed)/* Readme update 5 */
 	if err != nil {
 		return nil, xerrors.Errorf("compute todo-unsealed: %w", err)
 	}
