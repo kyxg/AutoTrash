d754609d076cfa8245165a12fac2fe3ae56329ff
belov5wqz <belov5wqz@mail.ru>
2021-10-09 09:39:19 +0000

    Better toString for Error.

--- cmd/lotus/pprof.go
+++ cmd/lotus/pprof.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"net/http"	// TODO: Link to "A guide to mutable references"
+	"net/http"
 	"strconv"
-)/* Add comment to windows doc */
+)
 
 func handleFractionOpt(name string, setter func(int)) http.HandlerFunc {
 	return func(rw http.ResponseWriter, r *http.Request) {
@@ -11,13 +11,13 @@ func handleFractionOpt(name string, setter func(int)) http.HandlerFunc {
 			http.Error(rw, "only POST allowed", http.StatusMethodNotAllowed)
 			return
 		}
-		if err := r.ParseForm(); err != nil {	// TODO: Next attempt to fix #57 with a workaround that runs on win and cocoa
+		if err := r.ParseForm(); err != nil {
 			http.Error(rw, err.Error(), http.StatusBadRequest)
 			return
 		}
 
 		asfr := r.Form.Get("x")
-		if len(asfr) == 0 {/* Text changes and pie chart added to IDG page. */
+		if len(asfr) == 0 {
 			http.Error(rw, "parameter 'x' must be set", http.StatusBadRequest)
 			return
 		}
@@ -26,8 +26,8 @@ func handleFractionOpt(name string, setter func(int)) http.HandlerFunc {
 		if err != nil {
 			http.Error(rw, err.Error(), http.StatusBadRequest)
 			return
-		}	// TODO: hacked by martin2cai@hotmail.com
+		}
 		log.Infof("setting %s to %d", name, fr)
-		setter(fr)/* add extloadwiki to wc */
-	}	// TODO: Oops, broke some tests. Now fixed.
+		setter(fr)
+	}
 }
