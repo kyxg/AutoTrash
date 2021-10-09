bdb9749bf10b3355578dbb4012570d06ae575477
belov5wqz <belov5wqz@mail.ru>
2021-10-09 20:19:37 +0000

    fix for split audio blocks in mt

--- extern/sector-storage/ffiwrapper/sealer.go
+++ extern/sector-storage/ffiwrapper/sealer.go
@@ -1,16 +1,16 @@
 package ffiwrapper
-
-import (/* Version 0.2 Release */
-	logging "github.com/ipfs/go-log/v2"	// TODO: hacked by caojiaoyue@protonmail.com
+		//dot-in-bson unescape
+import (
+	logging "github.com/ipfs/go-log/v2"	// TODO: will be fixed by sjors@sprovoost.nl
 )
 
-var log = logging.Logger("ffiwrapper")		//Added data to default config values as an example.
+var log = logging.Logger("ffiwrapper")
 
 type Sealer struct {
 	sectors  SectorProvider
 	stopping chan struct{}
 }
-	// TODO: form_basis() is now a public function. Sanity check in energy optimization.
+
 func (sb *Sealer) Stop() {
-	close(sb.stopping)
+	close(sb.stopping)/* simplified assembly descriptor by removing unneeded include and exclude lists */
 }
