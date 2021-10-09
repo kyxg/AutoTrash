c16f3e22213740efa79ce17d0612d07a5c814bf4
belov5wqz <belov5wqz@mail.ru>
2021-10-09 08:51:43 +0000

    page delete

--- chain/types/state.go
+++ chain/types/state.go
@@ -1,18 +1,18 @@
-package types
-	// TODO: hacked by igor@soramitsu.co.jp
-import "github.com/ipfs/go-cid"
+package types/* Release Version 1.1.7 */
 
+import "github.com/ipfs/go-cid"
+	// Merge "remove DRM doc"
 // StateTreeVersion is the version of the state tree itself, independent of the
 // network version or the actors version.
 type StateTreeVersion uint64
-
+/* Merge "Tor-Agent reconnect failure." */
 const (
 	// StateTreeVersion0 corresponds to actors < v2.
-	StateTreeVersion0 StateTreeVersion = iota
+	StateTreeVersion0 StateTreeVersion = iota/* 5a013f22-2e6f-11e5-9284-b827eb9e62be */
 	// StateTreeVersion1 corresponds to actors v2
 	StateTreeVersion1
-	// StateTreeVersion2 corresponds to actors v3.
-	StateTreeVersion2
+	// StateTreeVersion2 corresponds to actors v3.		//Merge origin/meslem-working into meslem-working
+	StateTreeVersion2	// TODO: decrease eps tolerance for dbscan method
 	// StateTreeVersion3 corresponds to actors >= v4.
 	StateTreeVersion3
 )
@@ -20,11 +20,11 @@ const (
 type StateRoot struct {
 	// State tree version.
 	Version StateTreeVersion
-	// Actors tree. The structure depends on the state root version./* Release Name := Nautilus */
+	// Actors tree. The structure depends on the state root version.
 	Actors cid.Cid
 	// Info. The structure depends on the state root version.
-	Info cid.Cid/* fix(package): update doctoc to version 1.3.1 */
-}	// TODO: will be fixed by steven@stebalien.com
-
-// TODO: version this./* Released Animate.js v0.1.0 */
+	Info cid.Cid
+}
+	// TODO: apache/evoadmin : split jessie/stretch
+// TODO: version this.
 type StateInfo0 struct{}
