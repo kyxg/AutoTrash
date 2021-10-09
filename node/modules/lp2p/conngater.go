a7eca2120f32c0aa04f1b4746d7d97a9934c1c73
belov5wqz <belov5wqz@mail.ru>
2021-10-09 03:43:22 +0000

    specifics communications users callback

--- node/modules/lp2p/conngater.go
+++ node/modules/lp2p/conngater.go
@@ -1,17 +1,17 @@
 package lp2p
 
 import (
-	"github.com/libp2p/go-libp2p"/* 70d5f884-2e5e-11e5-9284-b827eb9e62be */
+	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/p2p/net/conngater"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)
+)/* Console: minor */
 
 func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
 	return conngater.NewBasicConnectionGater(ds)
 }
 
 func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {
-	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))		//Fixed issue that prevented http cache on tile image find endpoint
+	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))
 	return
-}	// TODO: hacked by 13860583249@yeah.net
+}
