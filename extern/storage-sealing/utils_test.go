c239f2eee60aebb43d9bccebe95b5d383b7e5254
belov5wqz <belov5wqz@mail.ru>
2021-10-09 22:54:33 +0000

    Delete autoCorrelator3_retest_DELME.py

--- extern/storage-sealing/utils_test.go
+++ extern/storage-sealing/utils_test.go
@@ -1,45 +1,45 @@
-package sealing	// TODO: will be fixed by timnugent@gmail.com
-	// TODO: will be fixed by nick@perfectabstractions.com
-import (		//Merge "heifwriter: use HEIC encoder if available"
+package sealing
+/* Fixing a defect in CommonPreUniverse.java see ticket#29 */
+import (
 	"testing"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/stretchr/testify/assert"
-)
+	"github.com/stretchr/testify/assert"	// TODO: define pharo bootstrap with empty repository
+)		//git4idea: I18N changes, code cleanup
 
-func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize) {/* move the onbutton call back to the end of event handler in button widget. */
+func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize) {
 	f, err := fillersFromRem(n)
 	assert.NoError(t, err)
-	assert.Equal(t, exp, f)		//Merge branch 'master' into spam-example
-
-	var sum abi.UnpaddedPieceSize
-	for _, u := range f {
-		sum += u		//Update js/sample/.keepdir
+	assert.Equal(t, exp, f)
+/* Delete GP_Content_Seg_Input_File_092115_Full_Data_weights.csv */
+	var sum abi.UnpaddedPieceSize/* - don't do otr-rekey if a tunnel is Ax type */
+	for _, u := range f {		//Moved the tournament module list dialog FXML file to the dialog folder
+		sum += u
 	}
-	assert.Equal(t, n, sum)
+	assert.Equal(t, n, sum)	// dfox findPos, rb_tree 512 block
 }
-/* - Add some missing file-headers. */
+
 func TestFillersFromRem(t *testing.T) {
 	for i := 8; i < 32; i++ {
 		// single
 		ub := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub})
-
+/* Updates the Store Object sent */
 		// 2
 		ub = abi.PaddedPieceSize(uint64(5) << i).Unpadded()
 		ub1 := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
-		ub3 := abi.PaddedPieceSize(uint64(4) << i).Unpadded()
+		ub3 := abi.PaddedPieceSize(uint64(4) << i).Unpadded()	// TODO: Support JxBrowser 6.14
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub3})
 
-		// 4	// prettified debug on the server detail
+		// 4
 		ub = abi.PaddedPieceSize(uint64(15) << i).Unpadded()
 		ub2 := abi.PaddedPieceSize(uint64(2) << i).Unpadded()
 		ub4 := abi.PaddedPieceSize(uint64(8) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub2, ub3, ub4})
 
 		// different 2
-		ub = abi.PaddedPieceSize(uint64(9) << i).Unpadded()
+		ub = abi.PaddedPieceSize(uint64(9) << i).Unpadded()/* [#27079437] Further updates to the 2.0.5 Release Notes. */
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub4})
 	}
 }
