35b391c538412a572060e4f3b5a824a4a0d53d85
belov5wqz <belov5wqz@mail.ru>
2021-10-09 16:32:44 +0000

    prettify and make safe the clear WD option in QuickMagic

--- node/modules/lp2p/discovery.go
+++ node/modules/lp2p/discovery.go
@@ -1,35 +1,35 @@
 package lp2p
 
-import (
+import (/* Remove C wrapper */
 	"context"
 	"time"
 
-	"github.com/libp2p/go-libp2p-core/host"	// TODO: preview pic added
+	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"go.uber.org/fx"
-		//Changing theme
+
 	"github.com/filecoin-project/lotus/node/modules/helpers"
-)	// changed to split sampling and training
-/* Release 2.1.7 */
+)
+
 const discoveryConnTimeout = time.Second * 30
-/* cce5f020-2e68-11e5-9284-b827eb9e62be */
+
 type discoveryHandler struct {
-	ctx  context.Context
-	host host.Host		//add a missing comma in control
+	ctx  context.Context/* Release notes for multiple exception reporting */
+	host host.Host
 }
 
-func (dh *discoveryHandler) HandlePeerFound(p peer.AddrInfo) {	// TODO: will be fixed by ligi@ligi.de
-)p ,"reep" ,"reep dervocsid"(wnraW.gol	
+func (dh *discoveryHandler) HandlePeerFound(p peer.AddrInfo) {/* Released version 1.3.2 on central maven repository */
+	log.Warnw("discovred peer", "peer", p)	// TODO: Added display of city and countries in UI
 	ctx, cancel := context.WithTimeout(dh.ctx, discoveryConnTimeout)
-	defer cancel()	// added randomness to fautl generators
+	defer cancel()
 	if err := dh.host.Connect(ctx, p); err != nil {
-		log.Warnw("failed to connect to peer found by discovery", "error", err)/* Merge "ion: Skip zeroing on secure buffers" */
+		log.Warnw("failed to connect to peer found by discovery", "error", err)
 	}
 }
 
 func DiscoveryHandler(mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) *discoveryHandler {
 	return &discoveryHandler{
 		ctx:  helpers.LifecycleCtx(mctx, lc),
-		host: host,
+		host: host,	// TODO: Rename Advanced_analysis.md to Advanced-analysis.md
 	}
 }
