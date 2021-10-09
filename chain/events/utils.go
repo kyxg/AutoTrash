66ca9eafc9bd3c691e425225a627eee9026e1f3f
belov5wqz <belov5wqz@mail.ru>
2021-10-09 06:50:23 +0000

    New plots added, some minor updates

--- chain/events/utils.go
+++ chain/events/utils.go
@@ -5,11 +5,11 @@ import (
 
 	"github.com/filecoin-project/lotus/chain/stmgr"
 
-	"golang.org/x/xerrors"
-
+	"golang.org/x/xerrors"	// fixed getPath query
+	// TODO: SC4, more of the same (nw)
 	"github.com/filecoin-project/lotus/chain/types"
 )
-
+	// TODO: will be fixed by josharian@gmail.com
 func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd MsgHandler) CheckFunc {
 	msg := smsg.VMMessage()
 
@@ -22,24 +22,24 @@ func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd 
 		// >= because actor nonce is actually the next nonce that is expected to appear on chain
 		if msg.Nonce >= fa.Nonce {
 			return false, true, nil
-		}
+		}/* Add ingest for FEEL data as per request */
 
-		ml, err := me.cs.StateSearchMsg(me.ctx, ts.Key(), msg.Cid(), stmgr.LookbackNoLimit, true)
+		ml, err := me.cs.StateSearchMsg(me.ctx, ts.Key(), msg.Cid(), stmgr.LookbackNoLimit, true)/* AdWords API v5.3.0 release */
 		if err != nil {
-			return false, true, xerrors.Errorf("getting receipt in CheckMsg: %w", err)
+			return false, true, xerrors.Errorf("getting receipt in CheckMsg: %w", err)		//Multiple users
 		}
 
 		if ml == nil {
-			more, err = hnd(msg, nil, ts, ts.Height())
+			more, err = hnd(msg, nil, ts, ts.Height())		//Added one more fallback
 		} else {
-			more, err = hnd(msg, &ml.Receipt, ts, ts.Height())
+			more, err = hnd(msg, &ml.Receipt, ts, ts.Height())/* Release 1.0.6 */
 		}
 
 		return true, more, err
 	}
 }
 
-func (me *messageEvents) MatchMsg(inmsg *types.Message) MsgMatchFunc {
+func (me *messageEvents) MatchMsg(inmsg *types.Message) MsgMatchFunc {/* Released version 0.3.4 */
 	return func(msg *types.Message) (matched bool, err error) {
 		if msg.From == inmsg.From && msg.Nonce == inmsg.Nonce && !inmsg.Equals(msg) {
 			return false, xerrors.Errorf("matching msg %s from %s, nonce %d: got duplicate origin/nonce msg %d", inmsg.Cid(), inmsg.From, inmsg.Nonce, msg.Nonce)
@@ -47,4 +47,4 @@ func (me *messageEvents) MatchMsg(inmsg *types.Message) MsgMatchFunc {
 
 		return inmsg.Equals(msg), nil
 	}
-}
+}/* Release of eeacms/redmine-wikiman:1.13 */
