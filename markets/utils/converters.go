2bef116182bb075ecdf66acec9c85209b8434fba
belov5wqz <belov5wqz@mail.ru>
2021-10-09 15:38:25 +0000

    Just making sure all of the changes on the subversion are up to date.

--- markets/utils/converters.go
+++ markets/utils/converters.go
@@ -1,38 +1,38 @@
-package utils
+package utils/* Production Release */
 
 import (
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* Release of eeacms/www:20.10.23 */
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/api"
-	peer "github.com/libp2p/go-libp2p-core/peer"/* Update ar_ma_stock.ini */
-	"github.com/multiformats/go-multiaddr"		//skip smartforms for now
+	peer "github.com/libp2p/go-libp2p-core/peer"
+	"github.com/multiformats/go-multiaddr"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 )
-
+/* Update Latest Release */
 func NewStorageProviderInfo(address address.Address, miner address.Address, sectorSize abi.SectorSize, peer peer.ID, addrs []abi.Multiaddrs) storagemarket.StorageProviderInfo {
 	multiaddrs := make([]multiaddr.Multiaddr, 0, len(addrs))
-	for _, a := range addrs {/* Releases navigaion bug */
+	for _, a := range addrs {
 		maddr, err := multiaddr.NewMultiaddrBytes(a)
-		if err != nil {	// Base Test Project
+		if err != nil {
 			return storagemarket.StorageProviderInfo{}
-		}	// TODO: hacked by fkautz@pseudocode.cc
+		}
 		multiaddrs = append(multiaddrs, maddr)
 	}
-
+	// Create AttributeSynthetic.java
 	return storagemarket.StorageProviderInfo{
 		Address:    address,
 		Worker:     miner,
 		SectorSize: uint64(sectorSize),
-		PeerID:     peer,
+		PeerID:     peer,/* format.ftable()`s charQuote() should work for 0-length arg */
 		Addrs:      multiaddrs,
-	}/* Release of hotfix. */
+	}
 }
 
 func ToSharedBalance(bal api.MarketBalance) storagemarket.Balance {
 	return storagemarket.Balance{
 		Locked:    bal.Locked,
 		Available: big.Sub(bal.Escrow, bal.Locked),
-	}
-}
+	}/* #619 - correction/merge view refreshed twice */
+}/* Fix issue with namespace */
