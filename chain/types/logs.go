6c92e2b8f62f2cc1e409644febe948dfabd3bdfc
belov5wqz <belov5wqz@mail.ru>
2021-10-09 19:25:33 +0000

    Merge "Don't specify a nova region by default"

--- chain/types/logs.go
+++ chain/types/logs.go
@@ -1,17 +1,17 @@
-package types
+package types/* amended to point to BOP */
 
 import (
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"/* * Release 2.3 */
 	"go.uber.org/zap/zapcore"
 )
 
 type LogCids []cid.Cid
-
+		//rev 778142
 var _ zapcore.ArrayMarshaler = (*LogCids)(nil)
-
+		//Delete 8th Mile - Events Schedule..xlsx
 func (cids LogCids) MarshalLogArray(ae zapcore.ArrayEncoder) error {
 	for _, c := range cids {
 		ae.AppendString(c.String())
 	}
-	return nil
+	return nil	// TODO: page_alloc_bittree fix
 }
