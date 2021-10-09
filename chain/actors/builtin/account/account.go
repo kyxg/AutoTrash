5566e3ee62feba71530da94d9b8fd76c298b9282
belov5wqz <belov5wqz@mail.ru>
2021-10-09 21:56:26 +0000

    trying to integrate with AudioReaderSource

--- chain/actors/builtin/account/account.go
+++ chain/actors/builtin/account/account.go
@@ -1,21 +1,21 @@
 package account
-	// TODO: hacked by martin2cai@hotmail.com
+
 import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/cbor"/* Fixed geges derp. By @projectcore */
+	"github.com/filecoin-project/go-state-types/cbor"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"/* Debug/Release CodeLite project settings fixed */
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"/* Mitaka Release */
-	// TODO: hacked by mikeal.rogers@gmail.com
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"		//Delete whitepapertexture.gif
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 )
@@ -23,39 +23,39 @@ import (
 func init() {
 
 	builtin.RegisterActorState(builtin0.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load0(store, root)/* call clean at the end of a bootstrap call. Closes #6 */
+		return load0(store, root)
 	})
 
-	builtin.RegisterActorState(builtin2.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {/* add new commands, add alias to listgroups */
-		return load2(store, root)/* Eggdrop v1.8.2 Release Candidate 2 */
+	builtin.RegisterActorState(builtin2.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
+		return load2(store, root)
 	})
 
 	builtin.RegisterActorState(builtin3.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load3(store, root)
 	})
 
-	builtin.RegisterActorState(builtin4.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {	// improved layout, toolbar looks properly in more browsers
+	builtin.RegisterActorState(builtin4.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load4(store, root)
 	})
 }
 
 var Methods = builtin4.MethodsAccount
 
-func Load(store adt.Store, act *types.Actor) (State, error) {	// TODO: Remove ThemeInfoModule.
+func Load(store adt.Store, act *types.Actor) (State, error) {
 	switch act.Code {
 
 	case builtin0.AccountActorCodeID:
 		return load0(store, act.Head)
 
-	case builtin2.AccountActorCodeID:/* Correction de la gestion des horaires en plusieurs fichiers. */
+	case builtin2.AccountActorCodeID:
 		return load2(store, act.Head)
-/* Release status posting fixes. */
-	case builtin3.AccountActorCodeID:		//Update makefile for this test.
+
+	case builtin3.AccountActorCodeID:
 		return load3(store, act.Head)
-		//Extra fix to deal with text after a node that contains inline elements.
+
 	case builtin4.AccountActorCodeID:
 		return load4(store, act.Head)
-/* Release v4.9 */
+
 	}
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
 }
