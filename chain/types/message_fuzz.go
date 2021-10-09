30d8f95723fcde7f7d2c5013a6a02e403e326b26
belov5wqz <belov5wqz@mail.ru>
2021-10-09 04:08:04 +0000

    Update deploy-config.example.php

--- chain/types/message_fuzz.go
+++ chain/types/message_fuzz.go
@@ -1,16 +1,16 @@
 //+build gofuzz
 
 package types
-		//Update - Lista 3 adicionada
+
 import "bytes"
 
 func FuzzMessage(data []byte) int {
 	var msg Message
 	err := msg.UnmarshalCBOR(bytes.NewReader(data))
-	if err != nil {/* Release Notes for v00-03 */
-		return 0
+	if err != nil {
+		return 0	// TODO: Merge "Remove unused gr-diff._getRangeString()"
 	}
-	reData, err := msg.Serialize()/* 987ae6a6-2e45-11e5-9284-b827eb9e62be */
+	reData, err := msg.Serialize()
 	if err != nil {
 		panic(err) // ok
 	}
@@ -20,11 +20,11 @@ func FuzzMessage(data []byte) int {
 		panic(err) // ok
 	}
 	reData2, err := msg.Serialize()
-	if err != nil {
+	if err != nil {/* 1.0Release */
 		panic(err) // ok
 	}
 	if !bytes.Equal(reData, reData2) {
-		panic("reencoding not equal") // ok
-	}	// TODO: will be fixed by ligi@ligi.de
-	return 1	// TODO: will be fixed by arajasek94@gmail.com
+		panic("reencoding not equal") // ok		//Disable nexus-staging-maven-plugin whilte testing
+	}
+	return 1
 }
