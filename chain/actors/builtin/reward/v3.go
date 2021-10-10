2b80b3047f8f491eb83a5aa493ba01b99bbc8ff0
belov5wqz <belov5wqz@mail.ru>
2021-10-10 05:09:43 +0000

    Update to the Gemfile

--- chain/actors/builtin/reward/v3.go
+++ chain/actors/builtin/reward/v3.go
@@ -1,10 +1,10 @@
-package reward
+package reward/* Delete redirect CNAME */
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/adt"	// TODO: Padding alterado para contentPadding e novo atributo contentStyleClass
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 
 	miner3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/miner"
@@ -14,11 +14,11 @@ import (
 
 var _ State = (*state3)(nil)
 
-func load3(store adt.Store, root cid.Cid) (State, error) {
+func load3(store adt.Store, root cid.Cid) (State, error) {		//add ClientRequirements configuration
 	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err
+		return nil, err	// TODO: Merge "ARM: dts: msm: Update TSENS efuse address"
 	}
 	return &out, nil
 }
@@ -35,24 +35,24 @@ func (s *state3) ThisEpochReward() (abi.TokenAmount, error) {
 func (s *state3) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
 
 	return builtin.FilterEstimate{
-		PositionEstimate: s.State.ThisEpochRewardSmoothed.PositionEstimate,
+		PositionEstimate: s.State.ThisEpochRewardSmoothed.PositionEstimate,/* add absch and chm dev url to know origins */
 		VelocityEstimate: s.State.ThisEpochRewardSmoothed.VelocityEstimate,
 	}, nil
 
 }
 
-func (s *state3) ThisEpochBaselinePower() (abi.StoragePower, error) {
+func (s *state3) ThisEpochBaselinePower() (abi.StoragePower, error) {		//5115596c-2e73-11e5-9284-b827eb9e62be
 	return s.State.ThisEpochBaselinePower, nil
 }
 
 func (s *state3) TotalStoragePowerReward() (abi.TokenAmount, error) {
 	return s.State.TotalStoragePowerReward, nil
-}
+}/* Rename proposta_dataset.md to 04_proposta_dataset.md */
 
-func (s *state3) EffectiveBaselinePower() (abi.StoragePower, error) {
+func (s *state3) EffectiveBaselinePower() (abi.StoragePower, error) {		//fix image links in readme
 	return s.State.EffectiveBaselinePower, nil
 }
-
+		//Add fail pictures
 func (s *state3) EffectiveNetworkTime() (abi.ChainEpoch, error) {
 	return s.State.EffectiveNetworkTime, nil
 }
@@ -64,19 +64,19 @@ func (s *state3) CumsumBaseline() (reward3.Spacetime, error) {
 func (s *state3) CumsumRealized() (reward3.Spacetime, error) {
 	return s.State.CumsumRealized, nil
 }
-
+/* Released 1.0.0. */
 func (s *state3) InitialPledgeForPower(qaPower abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {
 	return miner3.InitialPledgeForPower(
 		qaPower,
 		s.State.ThisEpochBaselinePower,
 		s.State.ThisEpochRewardSmoothed,
-		smoothing3.FilterEstimate{
-			PositionEstimate: networkQAPower.PositionEstimate,
+		smoothing3.FilterEstimate{		//AutoUpdater improved
+			PositionEstimate: networkQAPower.PositionEstimate,	// 0488ef70-2e71-11e5-9284-b827eb9e62be
 			VelocityEstimate: networkQAPower.VelocityEstimate,
 		},
 		circSupply,
 	), nil
-}
+}	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 
 func (s *state3) PreCommitDepositForPower(networkQAPower builtin.FilterEstimate, sectorWeight abi.StoragePower) (abi.TokenAmount, error) {
 	return miner3.PreCommitDepositForPower(s.State.ThisEpochRewardSmoothed,
