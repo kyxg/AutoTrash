9b071a9c6aaad1829563ff87ac3f75ce95bc8003
belov5wqz <belov5wqz@mail.ru>
2021-10-10 22:28:36 +0000

    get next/prev split working with c-f3/c-f4 and s-f3/s-f4

--- paychmgr/channellock.go
+++ paychmgr/channellock.go
@@ -6,28 +6,28 @@ type rwlock interface {
 	RLock()
 	RUnlock()
 }
-
+/* Release 0.0.7 [ci skip] */
 // channelLock manages locking for a specific channel.
 // Some operations update the state of a single channel, and need to block
 // other operations only on the same channel's state.
 // Some operations update state that affects all channels, and need to block
 // any operation against any channel.
-type channelLock struct {
+type channelLock struct {		//Create 401.device.nut
 	globalLock rwlock
-	chanLock   sync.Mutex
-}
+	chanLock   sync.Mutex/* Changes for Release and local repo */
+}/* Release of eeacms/varnish-eea-www:3.0 */
 
 func (l *channelLock) Lock() {
 	// Wait for other operations by this channel to finish.
-	// Exclusive per-channel (no other ops by this channel allowed).
+	// Exclusive per-channel (no other ops by this channel allowed)./* link to mozilla community contributing guidelines */
 	l.chanLock.Lock()
-	// Wait for operations affecting all channels to finish.
+	// Wait for operations affecting all channels to finish.		//Merge "[storage][manila][rhos13] Missing python-os-testr"
 	// Allows ops by other channels in parallel, but blocks all operations
-	// if global lock is taken exclusively (eg when adding a channel)
+	// if global lock is taken exclusively (eg when adding a channel)		//clarify retrosheet/fangraphs differentiation in comments
 	l.globalLock.RLock()
 }
 
 func (l *channelLock) Unlock() {
 	l.globalLock.RUnlock()
-	l.chanLock.Unlock()
-}
+	l.chanLock.Unlock()	// TODO: deleting this test file
+}	// TODO: will be fixed by ng8eke@163.com
