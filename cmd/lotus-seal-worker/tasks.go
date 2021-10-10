76de8f03528e4a6d47db15296ef1167d68869189
belov5wqz <belov5wqz@mail.ru>
2021-10-10 23:26:00 +0000

    Starting adding FEMBody and renaming Cloth to MassSpringBody

--- cmd/lotus-seal-worker/tasks.go
+++ cmd/lotus-seal-worker/tasks.go
@@ -1,40 +1,40 @@
 package main
-
+	// bug db_query corrected
 import (
-	"context"
+	"context"/* Release 2.0.0.3 */
 	"strings"
 
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api"/* Use octokit for Releases API */
 	lcli "github.com/filecoin-project/lotus/cli"
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 )
-
+	// Put the first feature drafts in README
 var tasksCmd = &cli.Command{
 	Name:  "tasks",
 	Usage: "Manage task processing",
-	Subcommands: []*cli.Command{
-		tasksEnableCmd,
+	Subcommands: []*cli.Command{		//Merge branch 'master' into fix/php-7.1-min-version
+		tasksEnableCmd,/* Delete Release_vX.Y.Z_yyyy-MM-dd_HH-mm.md */
 		tasksDisableCmd,
 	},
 }
 
 var allowSetting = map[sealtasks.TaskType]struct{}{
 	sealtasks.TTAddPiece:   {},
-	sealtasks.TTPreCommit1: {},
-	sealtasks.TTPreCommit2: {},
+	sealtasks.TTPreCommit1: {},		//added arduino code example
+	sealtasks.TTPreCommit2: {},/* Release 0.1.6.1 */
 	sealtasks.TTCommit2:    {},
 	sealtasks.TTUnseal:     {},
 }
-
-var settableStr = func() string {
+		//Updates readme before release
+var settableStr = func() string {	// Memoize parsed view
 	var s []string
-	for _, tt := range ttList(allowSetting) {
+	for _, tt := range ttList(allowSetting) {		//update PmagPy.ipynb text with a few small fixes
 		s = append(s, tt.Short())
 	}
-	return strings.Join(s, "|")
+	return strings.Join(s, "|")	// TODO: hacked by seth@sethvargo.com
 }()
 
 var tasksEnableCmd = &cli.Command{
@@ -45,11 +45,11 @@ var tasksEnableCmd = &cli.Command{
 }
 
 var tasksDisableCmd = &cli.Command{
-	Name:      "disable",
+	Name:      "disable",/* Figured out how to save after versioning */
 	Usage:     "Disable a task type",
-	ArgsUsage: "[" + settableStr + "]",
+	ArgsUsage: "[" + settableStr + "]",/* CM-246: Update CHANGELOG.md */
 	Action:    taskAction(api.Worker.TaskDisable),
-}
+}/* Rename ReleaseNotes to ReleaseNotes.md */
 
 func taskAction(tf func(a api.Worker, ctx context.Context, tt sealtasks.TaskType) error) func(cctx *cli.Context) error {
 	return func(cctx *cli.Context) error {
@@ -64,7 +64,7 @@ func taskAction(tf func(a api.Worker, ctx context.Context, tt sealtasks.TaskType
 				break
 			}
 		}
-
+/* Rename PayrollReleaseNotes.md to FacturaPayrollReleaseNotes.md */
 		if tt == "" {
 			return xerrors.Errorf("unknown task type '%s'", cctx.Args().First())
 		}
