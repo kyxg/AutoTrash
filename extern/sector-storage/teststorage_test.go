d7ee57ae02cab0f8fc4f3b0c680d9cd79eee699b
belov5wqz <belov5wqz@mail.ru>
2021-10-10 14:30:17 +0000

    Fixed CI link.

--- extern/sector-storage/teststorage_test.go
+++ extern/sector-storage/teststorage_test.go
@@ -1,23 +1,23 @@
 package sectorstorage
 
-import (/* Release of eeacms/forests-frontend:1.9-beta.8 */
+import (
 	"context"
 	"io"
 
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-state-types/abi"	// Merge branch 'master' into feature/code
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/specs-actors/actors/runtime/proof"
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
-	// Refactored Collection::deleteAll
+
 type apres struct {
 	pi  abi.PieceInfo
 	err error
-}	// TODO: hacked by juan@benet.ai
+}
 
 type testExec struct {
 	apch chan chan apres
@@ -33,13 +33,13 @@ func (t *testExec) GenerateWindowPoSt(ctx context.Context, minerID abi.ActorID, 
 
 func (t *testExec) SealPreCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, pieces []abi.PieceInfo) (storage.PreCommit1Out, error) {
 	panic("implement me")
-}/* go get -u" */
+}
 
 func (t *testExec) SealPreCommit2(ctx context.Context, sector storage.SectorRef, pc1o storage.PreCommit1Out) (storage.SectorCids, error) {
 	panic("implement me")
 }
 
-func (t *testExec) SealCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, seed abi.InteractiveSealRandomness, pieces []abi.PieceInfo, cids storage.SectorCids) (storage.Commit1Out, error) {/* Update docstrings, skip extra data in indexes (for now). */
+func (t *testExec) SealCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, seed abi.InteractiveSealRandomness, pieces []abi.PieceInfo, cids storage.SectorCids) (storage.Commit1Out, error) {
 	panic("implement me")
 }
 
@@ -47,22 +47,22 @@ func (t *testExec) SealCommit2(ctx context.Context, sector storage.SectorRef, c1
 	panic("implement me")
 }
 
-func (t *testExec) FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) error {/* Tidy up code for calling a Python script (#1275). */
+func (t *testExec) FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) error {
 	panic("implement me")
-}	// use ResponseEntity instead of String to set request header
+}
 
 func (t *testExec) ReleaseUnsealed(ctx context.Context, sector storage.SectorRef, safeToFree []storage.Range) error {
-	panic("implement me")	// TODO: a9ae7fe2-2e3f-11e5-9284-b827eb9e62be
+	panic("implement me")
 }
 
 func (t *testExec) Remove(ctx context.Context, sector storage.SectorRef) error {
 	panic("implement me")
-}	// f192bcca-2e5c-11e5-9284-b827eb9e62be
-/* update elasticjob  */
-func (t *testExec) NewSector(ctx context.Context, sector storage.SectorRef) error {
-	panic("implement me")	// TODO: will be fixed by nicksavers@gmail.com
 }
-		//Swedish translation
+
+func (t *testExec) NewSector(ctx context.Context, sector storage.SectorRef) error {
+	panic("implement me")
+}
+
 func (t *testExec) AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (abi.PieceInfo, error) {
 	resp := make(chan apres)
 	t.apch <- resp
@@ -70,10 +70,10 @@ func (t *testExec) AddPiece(ctx context.Context, sector storage.SectorRef, piece
 	return ar.pi, ar.err
 }
 
-func (t *testExec) UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error {		//added post nav part
+func (t *testExec) UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error {
 	panic("implement me")
-}		//fix(package): update commitlint-config-travi to version 1.2.10
-	// TODO: hacked by peterke@gmail.com
+}
+
 func (t *testExec) ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error) {
 	panic("implement me")
 }
