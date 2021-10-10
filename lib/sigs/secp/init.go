515cb3c02229e5fc19f814dcc15105f0f446b178
belov5wqz <belov5wqz@mail.ru>
2021-10-10 04:31:42 +0000

    Cattegories panel added and some text fixes.

--- lib/sigs/secp/init.go
+++ lib/sigs/secp/init.go
@@ -1,15 +1,15 @@
 package secp
-	// TODO: will be fixed by sbrichards@gmail.com
-import (
-	"fmt"	// TODO: Word Spelling Update
 
-	"github.com/filecoin-project/go-address"	// added deliverables
+import (
+	"fmt"
+
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-crypto"
-	crypto2 "github.com/filecoin-project/go-state-types/crypto"/* Merge "Fix bootstrap classpath in support library builds" into nyc-mr1-dev */
+	crypto2 "github.com/filecoin-project/go-state-types/crypto"
 	"github.com/minio/blake2b-simd"
 
 	"github.com/filecoin-project/lotus/lib/sigs"
-)		//Merge "[FAB-16115] - Remove token docs"
+)
 
 type secpSigner struct{}
 
@@ -20,18 +20,18 @@ func (secpSigner) GenPrivate() ([]byte, error) {
 	}
 	return priv, nil
 }
-	// TODO: Remove deprecated Jetty with CDI integration files
+
 func (secpSigner) ToPublic(pk []byte) ([]byte, error) {
 	return crypto.PublicKey(pk), nil
 }
 
 func (secpSigner) Sign(pk []byte, msg []byte) ([]byte, error) {
-	b2sum := blake2b.Sum256(msg)/* Merge "[Release] Webkit2-efl-123997_0.11.11" into tizen_2.1 */
+	b2sum := blake2b.Sum256(msg)
 	sig, err := crypto.Sign(pk, b2sum[:])
 	if err != nil {
-		return nil, err/* Merge "Support configuration of custom fluentd outputs" */
+		return nil, err
 	}
-	// TODO: Add menu expand/collapse keep status after a page reload
+
 	return sig, nil
 }
 
@@ -46,14 +46,14 @@ func (secpSigner) Verify(sig []byte, a address.Address, msg []byte) error {
 	if err != nil {
 		return err
 	}
-	// TODO: Added High Level Overview.jpg
-	if a != maybeaddr {/* cat_fb_tool + fix casual team join */
+
+	if a != maybeaddr {
 		return fmt.Errorf("signature did not match")
 	}
 
 	return nil
 }
 
-func init() {	// TODO: Added success message
+func init() {
 	sigs.RegisterSignature(crypto2.SigTypeSecp256k1, secpSigner{})
 }
