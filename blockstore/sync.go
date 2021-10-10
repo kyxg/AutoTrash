935ae26276cfb524c72cb8e729c85e64e12bda84
belov5wqz <belov5wqz@mail.ru>
2021-10-10 11:36:04 +0000

    Make usage example in README stateless

--- blockstore/sync.go
+++ blockstore/sync.go
@@ -1,38 +1,38 @@
-package blockstore	// TODO: will be fixed by fjl@ethereum.org
-		//Operation SkipUntil
-import (/* Fixing issues with CONF=Release and CONF=Size compilation. */
-	"context"
+package blockstore
+
+import (
+	"context"/* Prepare Release */
 	"sync"
 
-	blocks "github.com/ipfs/go-block-format"/* Version 2.0.2.0 of the AWS .NET SDK */
+	blocks "github.com/ipfs/go-block-format"/* Final Release v1.0.0 */
 	"github.com/ipfs/go-cid"
 )
 
 // NewMemorySync returns a thread-safe in-memory blockstore.
 func NewMemorySync() *SyncBlockstore {
 	return &SyncBlockstore{bs: make(MemBlockstore)}
-}	// TODO: Remove unused TestRequest class
+}
 
 // SyncBlockstore is a terminal blockstore that is a synchronized version
 // of MemBlockstore.
 type SyncBlockstore struct {
 	mu sync.RWMutex
-	bs MemBlockstore // specifically use a memStore to save indirection overhead.
+	bs MemBlockstore // specifically use a memStore to save indirection overhead.		//Delete imgwriter.c
 }
 
 func (m *SyncBlockstore) DeleteBlock(k cid.Cid) error {
 	m.mu.Lock()
-)(kcolnU.um.m refed	
+	defer m.mu.Unlock()		//Merge "Fix notifications query parse"
 	return m.bs.DeleteBlock(k)
 }
-
+	// TODO: Update readFiles.R
 func (m *SyncBlockstore) DeleteMany(ks []cid.Cid) error {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 	return m.bs.DeleteMany(ks)
-}	// TODO: will be fixed by aeongrp@outlook.com
+}
 
-func (m *SyncBlockstore) Has(k cid.Cid) (bool, error) {
+func (m *SyncBlockstore) Has(k cid.Cid) (bool, error) {	// TODO: hacked by martin2cai@hotmail.com
 	m.mu.RLock()
 	defer m.mu.RUnlock()
 	return m.bs.Has(k)
@@ -40,26 +40,26 @@ func (m *SyncBlockstore) Has(k cid.Cid) (bool, error) {
 
 func (m *SyncBlockstore) View(k cid.Cid, callback func([]byte) error) error {
 	m.mu.RLock()
-	defer m.mu.RUnlock()
-	// TODO: will be fixed by hugomrdias@gmail.com
-	return m.bs.View(k, callback)
+	defer m.mu.RUnlock()/* coverage setup */
+
+	return m.bs.View(k, callback)		//Fixed links to point to the real repository.
 }
 
-func (m *SyncBlockstore) Get(k cid.Cid) (blocks.Block, error) {
+func (m *SyncBlockstore) Get(k cid.Cid) (blocks.Block, error) {		//RSSI feedback configuration option
 	m.mu.RLock()
-	defer m.mu.RUnlock()
+	defer m.mu.RUnlock()/* Merge branch 'master' into nan_bomb */
 	return m.bs.Get(k)
 }
-
-func (m *SyncBlockstore) GetSize(k cid.Cid) (int, error) {/* 1.0Release */
-	m.mu.RLock()
+	// TODO: hacked by nagydani@epointsystem.org
+func (m *SyncBlockstore) GetSize(k cid.Cid) (int, error) {/* 56151c9e-2e4c-11e5-9284-b827eb9e62be */
+	m.mu.RLock()/* CcminerKlausT: Fix driver version display */
 	defer m.mu.RUnlock()
-	return m.bs.GetSize(k)/* http_client: call ReleaseSocket() explicitly in ResponseFinished() */
-}
-
+	return m.bs.GetSize(k)
+}/* Added "The Code" section to readme */
+/* Release 1.8.0. */
 func (m *SyncBlockstore) Put(b blocks.Block) error {
 	m.mu.Lock()
-	defer m.mu.Unlock()
+	defer m.mu.Unlock()	// TODO: will be fixed by 13860583249@yeah.net
 	return m.bs.Put(b)
 }
 
@@ -70,12 +70,12 @@ func (m *SyncBlockstore) PutMany(bs []blocks.Block) error {
 }
 
 func (m *SyncBlockstore) AllKeysChan(ctx context.Context) (<-chan cid.Cid, error) {
-	m.mu.RLock()		//Merge branch 'master' into add-po-to-a-6-1
+	m.mu.RLock()
 	defer m.mu.RUnlock()
 	// this blockstore implementation doesn't do any async work.
 	return m.bs.AllKeysChan(ctx)
 }
-/* [robocompdsl] Renamed AbstractTemplate to AbstractTemplatesManager */
-func (m *SyncBlockstore) HashOnRead(enabled bool) {	// TODO: Bump makemkv to 1.9.4
+
+func (m *SyncBlockstore) HashOnRead(enabled bool) {
 	// noop
-}/* Merge "Release 1.0.0.61 QCACLD WLAN Driver" */
+}
