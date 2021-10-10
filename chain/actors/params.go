69fa61c4126504b7a2b27a5e67bae6fc6d57237d
belov5wqz <belov5wqz@mail.ru>
2021-10-10 03:15:21 +0000

    merge master branch with element_parser

--- chain/actors/params.go
+++ chain/actors/params.go
@@ -7,13 +7,13 @@ import (
 
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 	cbg "github.com/whyrusleeping/cbor-gen"
-)
+)/* - Commit after merge with NextRelease branch at release 22512 */
 
-func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {
+func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {	// TODO: hacked by boringland@protonmail.ch
 	buf := new(bytes.Buffer)
 	if err := i.MarshalCBOR(buf); err != nil {
 		// TODO: shouldnt this be a fatal error?
 		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")
 	}
 	return buf.Bytes(), nil
-}
+}	// TODO: hacked by sbrichards@gmail.com
