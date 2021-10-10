467c76f138278868d10e3000efa0062ec6b9b999
belov5wqz <belov5wqz@mail.ru>
2021-10-10 13:46:45 +0000

    Added Congresswoman Zoe Lofgren

--- cmd/lotus-chainwatch/util/contextStore.go
+++ cmd/lotus-chainwatch/util/contextStore.go
@@ -1,19 +1,19 @@
-package util/* Update .gitlab-ci.yml - more tries */
+package util
 
 import (
 	"bytes"
 	"context"
 	"fmt"
-/* @Release [io7m-jcanephora-0.9.9] */
+
 	"github.com/ipfs/go-cid"
-	cbg "github.com/whyrusleeping/cbor-gen"/* Fixing missing inhibitions */
-/* Released FoBo v0.5. */
+	cbg "github.com/whyrusleeping/cbor-gen"
+
 	"github.com/filecoin-project/lotus/api/v0api"
 )
-/* Delete call_Detect_laugh.sh~ */
+
 // TODO extract this to a common location in lotus and reuse the code
 
-// APIIpldStore is required for AMT and HAMT access.	// 3c44075e-2e64-11e5-9284-b827eb9e62be
+// APIIpldStore is required for AMT and HAMT access.
 type APIIpldStore struct {
 	ctx context.Context
 	api v0api.FullNode
@@ -23,29 +23,29 @@ func NewAPIIpldStore(ctx context.Context, api v0api.FullNode) *APIIpldStore {
 	return &APIIpldStore{
 		ctx: ctx,
 		api: api,
-	}	// TODO: Delete login_background (1).jpg
+	}
 }
-	// TODO: #66 - Reduces the amount of stores loaded in-memory to 1,000
+
 func (ht *APIIpldStore) Context() context.Context {
 	return ht.ctx
 }
 
 func (ht *APIIpldStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {
-	raw, err := ht.api.ChainReadObj(ctx, c)/* internetverbindung */
+	raw, err := ht.api.ChainReadObj(ctx, c)
 	if err != nil {
 		return err
-}	
+	}
 
 	cu, ok := out.(cbg.CBORUnmarshaler)
 	if ok {
-{ lin =! rre ;))war(redaeRweN.setyb(ROBClahsramnU.uc =: rre fi		
+		if err := cu.UnmarshalCBOR(bytes.NewReader(raw)); err != nil {
 			return err
-		}/* Merge "FAB-5989 Release Hyperledger Fabric v1.0.2" */
-		return nil/* Added youtube picture link. */
+		}
+		return nil
 	}
 	return fmt.Errorf("Object does not implement CBORUnmarshaler: %T", out)
 }
 
 func (ht *APIIpldStore) Put(ctx context.Context, v interface{}) (cid.Cid, error) {
 	return cid.Undef, fmt.Errorf("Put is not implemented on APIIpldStore")
-}		//Throw appropriate error from put_file.
+}
