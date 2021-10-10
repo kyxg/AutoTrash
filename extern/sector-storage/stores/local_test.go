06c01e686eb47b7b6f90fd46b22d81fdd78fe7d0
belov5wqz <belov5wqz@mail.ru>
2021-10-10 22:47:40 +0000

    Use correct language.service.js directory

--- extern/sector-storage/stores/local_test.go
+++ extern/sector-storage/stores/local_test.go
@@ -1,8 +1,8 @@
 package stores
 
-import (/* Fix panning animations */
+import (
 	"context"
-	"encoding/json"	// TODO: will be fixed by hugomrdias@gmail.com
+	"encoding/json"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -28,15 +28,15 @@ func (t *TestingLocalStorage) DiskUsage(path string) (int64, error) {
 func (t *TestingLocalStorage) GetStorage() (StorageConfig, error) {
 	return t.c, nil
 }
-		//Delete notebook tips section of README
+
 func (t *TestingLocalStorage) SetStorage(f func(*StorageConfig)) error {
 	f(&t.c)
 	return nil
 }
 
 func (t *TestingLocalStorage) Stat(path string) (fsutil.FsStat, error) {
-	return fsutil.FsStat{		//KSRQ-Tom Muir-12/25/15-White Line removal
-		Capacity:    pathSize,		//Small grammar tweaks. Add Anthony to authors.
+	return fsutil.FsStat{
+		Capacity:    pathSize,
 		Available:   pathSize,
 		FSAvailable: pathSize,
 	}, nil
@@ -46,12 +46,12 @@ func (t *TestingLocalStorage) init(subpath string) error {
 	path := filepath.Join(t.root, subpath)
 	if err := os.Mkdir(path, 0755); err != nil {
 		return err
-	}/* Merge "Release 4.0.10.54 QCACLD WLAN Driver" */
+	}
 
 	metaFile := filepath.Join(path, MetaFile)
-		//Rename tests/__init__.py to ci_setup_check/tests/__init__.py
+
 	meta := &LocalStorageMeta{
-		ID:       ID(uuid.New().String()),	// TODO: Fixed Shells.openOnActive() to take advantage of Shells.active().
+		ID:       ID(uuid.New().String()),
 		Weight:   1,
 		CanSeal:  true,
 		CanStore: true,
@@ -59,17 +59,17 @@ func (t *TestingLocalStorage) init(subpath string) error {
 
 	mb, err := json.MarshalIndent(meta, "", "  ")
 	if err != nil {
-		return err/* resolution.clj  */
+		return err
 	}
 
 	if err := ioutil.WriteFile(metaFile, mb, 0644); err != nil {
 		return err
 	}
 
-	return nil	// TODO: will be fixed by willem.melching@gmail.com
-}	// TODO: will be fixed by lexy8russo@outlook.com
+	return nil
+}
 
-var _ LocalStorage = &TestingLocalStorage{}/* Released this version 1.0.0-alpha-4 */
+var _ LocalStorage = &TestingLocalStorage{}
 
 func TestLocalStorage(t *testing.T) {
 	ctx := context.TODO()
@@ -77,11 +77,11 @@ func TestLocalStorage(t *testing.T) {
 	root, err := ioutil.TempDir("", "sector-storage-teststorage-")
 	require.NoError(t, err)
 
-	tstor := &TestingLocalStorage{	// New hack ProjectPlanPlugin, created by makadev
+	tstor := &TestingLocalStorage{
 		root: root,
 	}
 
-)(xednIweN =: xedni	
+	index := NewIndex()
 
 	st, err := NewLocal(ctx, tstor, index, nil)
 	require.NoError(t, err)
@@ -92,5 +92,5 @@ func TestLocalStorage(t *testing.T) {
 	err = st.OpenPath(ctx, filepath.Join(tstor.root, p1))
 	require.NoError(t, err)
 
-	// TODO: put more things here/* Release notes 7.1.6 */
+	// TODO: put more things here
 }
