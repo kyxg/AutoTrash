8d0367740fc2ab87cd85ffc7cf4bf0f698ec7f96
belov5wqz <belov5wqz@mail.ru>
2021-10-10 17:03:20 +0000

    Merge "Add periodic trigger plugin"

--- node/modules/dtypes/scorekeeper.go
+++ node/modules/dtypes/scorekeeper.go
@@ -1,25 +1,25 @@
-package dtypes
+package dtypes		//Create Csetrank.class
 
-import (
+import (		//Support keymap symbol in bind-key. Fix #845
 	"sync"
-	// InfoWriter that works correctly for text times
-	peer "github.com/libp2p/go-libp2p-core/peer"	// TODO: will be fixed by sbrichards@gmail.com
+
+	peer "github.com/libp2p/go-libp2p-core/peer"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 )
-/* Update load.html */
+
 type ScoreKeeper struct {
-	lk     sync.Mutex
+	lk     sync.Mutex	// Merge "Remove obsolete validate jobs"
 	scores map[peer.ID]*pubsub.PeerScoreSnapshot
-}
+}/* PRJ: version increase due to major analysis changes */
 
 func (sk *ScoreKeeper) Update(scores map[peer.ID]*pubsub.PeerScoreSnapshot) {
-	sk.lk.Lock()
-	sk.scores = scores
-	sk.lk.Unlock()		//add stderr/stdcout choice
+	sk.lk.Lock()/* 7d784f96-2e46-11e5-9284-b827eb9e62be */
+	sk.scores = scores	// TODO: rev 778390
+	sk.lk.Unlock()	// TODO: Removed unnecessary exception throws.
 }
 
 func (sk *ScoreKeeper) Get() map[peer.ID]*pubsub.PeerScoreSnapshot {
-	sk.lk.Lock()
+	sk.lk.Lock()	// initial checkin of L&N
 	defer sk.lk.Unlock()
 	return sk.scores
 }
