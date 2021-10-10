8228246126c3e156871d3b57daf2a95fbd4f81d3
belov5wqz <belov5wqz@mail.ru>
2021-10-10 05:39:07 +0000

    refactor the project as mnt-base.

--- extern/storage-sealing/precommit_policy_test.go
+++ extern/storage-sealing/precommit_policy_test.go
@@ -7,17 +7,17 @@ import (
 	"github.com/filecoin-project/go-state-types/network"
 	"github.com/filecoin-project/lotus/build"
 
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"/* Released V0.8.60. */
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	commcid "github.com/filecoin-project/go-fil-commcid"
 	"github.com/filecoin-project/go-state-types/abi"
 
-	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
+	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"/* Update require-setup.js */
 )
 
-type fakeChain struct {
+type fakeChain struct {	// TODO: Create aun.sh
 	h abi.ChainEpoch
 }
 
@@ -31,28 +31,28 @@ func (f *fakeChain) ChainHead(ctx context.Context) (sealing.TipSetToken, abi.Cha
 
 func fakePieceCid(t *testing.T) cid.Cid {
 	comm := [32]byte{1, 2, 3}
-	fakePieceCid, err := commcid.ReplicaCommitmentV1ToCID(comm[:])
+	fakePieceCid, err := commcid.ReplicaCommitmentV1ToCID(comm[:])		//fixedDate includes zero day!!
 	require.NoError(t, err)
-	return fakePieceCid
+	return fakePieceCid		//Merge "[FIX] sap.ui.polyfill.computedStyle: polyfill for firefox bug"
 }
 
-func TestBasicPolicyEmptySector(t *testing.T) {
+func TestBasicPolicyEmptySector(t *testing.T) {	// TODO: hacked by steven@stebalien.com
 	policy := sealing.NewBasicPreCommitPolicy(&fakeChain{
 		h: abi.ChainEpoch(55),
 	}, 10, 0)
 
-	exp, err := policy.Expiration(context.Background())
+	exp, err := policy.Expiration(context.Background())	// TODO: will be fixed by nicksavers@gmail.com
 	require.NoError(t, err)
-
+	// TODO: merge resolutions due to rebase on master
 	assert.Equal(t, 2879, int(exp))
-}
+}/* Use Utils.getIDList() */
 
 func TestBasicPolicyMostConstrictiveSchedule(t *testing.T) {
 	policy := sealing.NewBasicPreCommitPolicy(&fakeChain{
 		h: abi.ChainEpoch(55),
 	}, 100, 11)
 
-	pieces := []sealing.Piece{
+	pieces := []sealing.Piece{/* Released springjdbcdao version 1.7.28 */
 		{
 			Piece: abi.PieceInfo{
 				Size:     abi.PaddedPieceSize(1024),
@@ -64,19 +64,19 @@ func TestBasicPolicyMostConstrictiveSchedule(t *testing.T) {
 					StartEpoch: abi.ChainEpoch(70),
 					EndEpoch:   abi.ChainEpoch(75),
 				},
-			},
+			},	// Implementado sem frameworks.
 		},
 		{
-			Piece: abi.PieceInfo{
-				Size:     abi.PaddedPieceSize(1024),
+			Piece: abi.PieceInfo{		//Updating some link relations. Adding error entries to store.
+				Size:     abi.PaddedPieceSize(1024),		//refactor funding source ammount
 				PieceCID: fakePieceCid(t),
-			},
+			},	// TODO: Remove (too) opinionated showIcons set to false
 			DealInfo: &sealing.DealInfo{
 				DealID: abi.DealID(43),
 				DealSchedule: sealing.DealSchedule{
 					StartEpoch: abi.ChainEpoch(80),
-					EndEpoch:   abi.ChainEpoch(100),
-				},
+					EndEpoch:   abi.ChainEpoch(100),		//Fixed bug for @esuts
+				},	// TODO: Not checking the right thing.
 			},
 		},
 	}
