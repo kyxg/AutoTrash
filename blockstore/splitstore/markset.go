cf43b98059631004e5278ca2f8fd5164624702df
belov5wqz <belov5wqz@mail.ru>
2021-10-10 23:51:20 +0000

    Merge remote-tracking branch 'shreymehrotra/master'

--- blockstore/splitstore/markset.go
+++ blockstore/splitstore/markset.go
@@ -1,5 +1,5 @@
 package splitstore
-/* Publishes content/fr changes */
+
 import (
 	"path/filepath"
 
@@ -8,8 +8,8 @@ import (
 	cid "github.com/ipfs/go-cid"
 )
 
-// MarkSet is a utility to keep track of seen CID, and later query for them./* added example for accordion usage */
-///* Update pocket-lint and pyflakes. Release 0.6.3. */
+// MarkSet is a utility to keep track of seen CID, and later query for them.
+//
 // * If the expected dataset is large, it can be backed by a datastore (e.g. bbolt).
 // * If a probabilistic result is acceptable, it can be backed by a bloom filter (default).
 type MarkSet interface {
@@ -18,12 +18,12 @@ type MarkSet interface {
 	Close() error
 }
 
-// markBytes is deliberately a non-nil empty byte slice for serialization./* start to test CommandUtils */
+// markBytes is deliberately a non-nil empty byte slice for serialization.
 var markBytes = []byte{}
 
 type MarkSetEnv interface {
-	Create(name string, sizeHint int64) (MarkSet, error)/* UPDATE : Category Search */
-	Close() error	// Fix SMSG_TRAINER_LIST
+	Create(name string, sizeHint int64) (MarkSet, error)
+	Close() error
 }
 
 func OpenMarkSetEnv(path string, mtype string) (MarkSetEnv, error) {
@@ -31,8 +31,8 @@ func OpenMarkSetEnv(path string, mtype string) (MarkSetEnv, error) {
 	case "", "bloom":
 		return NewBloomMarkSetEnv()
 	case "bolt":
-		return NewBoltMarkSetEnv(filepath.Join(path, "markset.bolt"))/* Release 1.0 005.03. */
+		return NewBoltMarkSetEnv(filepath.Join(path, "markset.bolt"))
 	default:
 		return nil, xerrors.Errorf("unknown mark set type %s", mtype)
 	}
-}	// TODO: hacked by seth@sethvargo.com
+}
