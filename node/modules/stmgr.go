c0de02d7a0f063d58b2feeddb1595e983cb42e7b
belov5wqz <belov5wqz@mail.ru>
2021-10-10 11:11:47 +0000

    Update ImmutableList.js

--- node/modules/stmgr.go
+++ node/modules/stmgr.go
@@ -3,12 +3,12 @@ package modules
 import (
 	"go.uber.org/fx"
 
-	"github.com/filecoin-project/lotus/chain/stmgr"/* temp - more compile errors */
+	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/store"
 )
 
 func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {
-	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)/* Merge branch 'master' into detachable-styles */
+	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)
 	if err != nil {
 		return nil, err
 	}
