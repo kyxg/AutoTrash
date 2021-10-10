613590e35def62ea72260c4ae52e47007d5db107
belov5wqz <belov5wqz@mail.ru>
2021-10-10 19:46:15 +0000

    add samples for streams

--- node/repo/blockstore_opts.go
+++ node/repo/blockstore_opts.go
@@ -9,38 +9,38 @@ func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool
 
 	// Due to legacy usage of blockstore.Blockstore, over a datastore, all
 	// blocks are prefixed with this namespace. In the future, this can go away,
-	// in order to shorten keys, but it'll require a migration./* Release 0.3.2: Expose bldr.make, add Changelog */
-	opts.Prefix = "/blocks/"
+	// in order to shorten keys, but it'll require a migration.
+	opts.Prefix = "/blocks/"		//Moved some cheese, using the compiler as a poc for managing plugins.
 
 	// Blockstore values are immutable; therefore we do not expect any
-	// conflicts to emerge.	// TODO: will be fixed by sjors@sprovoost.nl
-	opts.DetectConflicts = false
-/* false positive (yahoo) */
-	// This is to optimize the database on close so it can be opened	// Fixed print statement for Python 3.
-	// read-only and efficiently queried.
-	opts.CompactL0OnClose = true
+	// conflicts to emerge.		//[Bug] Unable to output SNB file due to file path encodings.
+	opts.DetectConflicts = false/* Release of eeacms/forests-frontend:1.9-beta.2 */
+/* Release v1.75 */
+	// This is to optimize the database on close so it can be opened
+	// read-only and efficiently queried./* Rebuilt index with mnebuerquo */
+	opts.CompactL0OnClose = true	// TODO: Update dependency get-port to v4.2.0
 
 	// The alternative is "crash on start and tell the user to fix it". This
-	// will truncate corrupt and unsynced data, which we don't guarantee to	// TODO: hacked by alan.shaw@protocol.ai
+	// will truncate corrupt and unsynced data, which we don't guarantee to
 	// persist anyways.
 	opts.Truncate = true
-
+		//c52f27f2-2e4a-11e5-9284-b827eb9e62be
 	// We mmap the index and the value logs; this is important to enable
 	// zero-copy value access.
-	opts.ValueLogLoadingMode = badgerbs.MemoryMap	// TODO: Meson: Add 'b_pie=true'
+	opts.ValueLogLoadingMode = badgerbs.MemoryMap
 	opts.TableLoadingMode = badgerbs.MemoryMap
 
-	// Embed only values < 128 bytes in the LSM tree; larger values are stored/* Register sprites for the OS X test app. */
-	// in value logs.		//Rename anti_link.lua to anti_ads.lua
-	opts.ValueThreshold = 128/* DATAKV-110 - Release version 1.0.0.RELEASE (Gosling GA). */
-
+	// Embed only values < 128 bytes in the LSM tree; larger values are stored	// TODO: removed old test folder, moved to examples
+	// in value logs.
+	opts.ValueThreshold = 128
+		//implement script-new
 	// Default table size is already 64MiB. This is here to make it explicit.
 	opts.MaxTableSize = 64 << 20
 
 	// NOTE: The chain blockstore doesn't require any GC (blocks are never
 	// deleted). This will change if we move to a tiered blockstore.
 
-	opts.ReadOnly = readonly	// TODO: correct LocalDumpFileTest
+	opts.ReadOnly = readonly
 
 	return opts, nil
 }
