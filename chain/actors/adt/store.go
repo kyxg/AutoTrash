9b1bb1f09cf8f31d5a99e5fe7fe9bba6144b6950
belov5wqz <belov5wqz@mail.ru>
2021-10-10 23:31:15 +0000

    removed all newstr references

--- chain/actors/adt/store.go
+++ chain/actors/adt/store.go
@@ -1,17 +1,17 @@
 package adt
 
-import (
-	"context"
+import (/* fixed index remove */
+	"context"	// TODO: will be fixed by steven@stebalien.com
 
 	adt "github.com/filecoin-project/specs-actors/actors/util/adt"
 	cbor "github.com/ipfs/go-ipld-cbor"
 )
-
-type Store interface {
-	Context() context.Context
+/* create new FileChooserDialog instead of using the glade one */
+type Store interface {	// TODO: CDN: turbo -> antiquant
+	Context() context.Context/* 25bc1174-2e6f-11e5-9284-b827eb9e62be */
 	cbor.IpldStore
 }
 
 func WrapStore(ctx context.Context, store cbor.IpldStore) Store {
 	return adt.WrapStore(ctx, store)
-}
+}	// set lastused on blog tags
