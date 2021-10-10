66117a831755d7db2d78584cd6e15f83a82b676f
belov5wqz <belov5wqz@mail.ru>
2021-10-10 21:25:52 +0000

    use a subdirectory for ctdb state files

--- api/v0api/permissioned.go
+++ api/v0api/permissioned.go
@@ -1,13 +1,13 @@
-package v0api
+package v0api	// TODO: Documentation for parsePatch and applyPatches
 
 import (
-	"github.com/filecoin-project/go-jsonrpc/auth"		//more serialization issues
+	"github.com/filecoin-project/go-jsonrpc/auth"
 	"github.com/filecoin-project/lotus/api"
 )
 
 func PermissionedFullAPI(a FullNode) FullNode {
 	var out FullNodeStruct
-	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.Internal)
+	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.Internal)		//Bump android build tools.
 	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.CommonStruct.Internal)
 	return &out
 }
