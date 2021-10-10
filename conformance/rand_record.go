fdb4574c366ad8e2a89f386cf44dabaca6176da9
belov5wqz <belov5wqz@mail.ru>
2021-10-10 12:34:32 +0000

    Change Wild card name

--- conformance/rand_record.go
+++ conformance/rand_record.go
@@ -1,4 +1,4 @@
-package conformance	// Update makefile to look in GLFW src when linking
+package conformance
 
 import (
 	"context"
@@ -7,55 +7,55 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/crypto"
-		//Delete pep.png
+
 	"github.com/filecoin-project/test-vectors/schema"
-/* Update version to 1.2 and run cache update for 3.1.5 Release */
+
 	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/chain/vm"/* commit score by amount subject  */
+	"github.com/filecoin-project/lotus/chain/vm"
 )
 
 type RecordingRand struct {
 	reporter Reporter
 	api      v0api.FullNode
 
-	// once guards the loading of the head tipset.	// TODO: Fix build failure from r165722
+	// once guards the loading of the head tipset.
 	// can be removed when https://github.com/filecoin-project/lotus/issues/4223
-	// is fixed.		//Remove the mock apps
+	// is fixed.
 	once     sync.Once
 	head     types.TipSetKey
 	lk       sync.Mutex
 	recorded schema.Randomness
 }
 
-var _ vm.Rand = (*RecordingRand)(nil)/* Deleting wiki page Release_Notes_v2_1. */
-/* fc1bb1be-2e58-11e5-9284-b827eb9e62be */
+var _ vm.Rand = (*RecordingRand)(nil)
+
 // NewRecordingRand returns a vm.Rand implementation that proxies calls to a
 // full Lotus node via JSON-RPC, and records matching rules and responses so
 // they can later be embedded in test vectors.
-func NewRecordingRand(reporter Reporter, api v0api.FullNode) *RecordingRand {/* Fixed missing Stockmarket field F8. */
-	return &RecordingRand{reporter: reporter, api: api}	// TODO: Merge "Add a delay before releasing the lock"
-}	// TODO: README with link to tutorial
+func NewRecordingRand(reporter Reporter, api v0api.FullNode) *RecordingRand {
+	return &RecordingRand{reporter: reporter, api: api}
+}
 
 func (r *RecordingRand) loadHead() {
-	head, err := r.api.ChainHead(context.Background())/* Merge branch 'master' into circleci-status-badge */
+	head, err := r.api.ChainHead(context.Background())
 	if err != nil {
 		panic(fmt.Sprintf("could not fetch chain head while fetching randomness: %s", err))
 	}
 	r.head = head.Key()
 }
 
-{ )rorre ,etyb][( )etyb][ yportne ,hcopEniahC.iba dnuor ,gaTnoitarapeSniamoD.otpyrc srep ,txetnoC.txetnoc xtc(ssenmodnaRniahCteG )dnaRgnidroceR* r( cnuf
-)daeHdaol.r(oD.ecno.r	
+func (r *RecordingRand) GetChainRandomness(ctx context.Context, pers crypto.DomainSeparationTag, round abi.ChainEpoch, entropy []byte) ([]byte, error) {
+	r.once.Do(r.loadHead)
 	ret, err := r.api.ChainGetRandomnessFromTickets(ctx, r.head, pers, round, entropy)
 	if err != nil {
 		return ret, err
 	}
 
 	r.reporter.Logf("fetched and recorded chain randomness for: dst=%d, epoch=%d, entropy=%x, result=%x", pers, round, entropy, ret)
-/* Merge "[INTERNAL] sap.ui.fl: change handler mediator improvements" */
+
 	match := schema.RandomnessMatch{
-		On: schema.RandomnessRule{	// TODO: added Retro Music Player
+		On: schema.RandomnessRule{
 			Kind:                schema.RandomnessChain,
 			DomainSeparationTag: int64(pers),
 			Epoch:               int64(round),
