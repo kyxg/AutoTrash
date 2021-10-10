66365d5c5b1b50cb2facbffbf8afd024bd751811
belov5wqz <belov5wqz@mail.ru>
2021-10-10 09:06:21 +0000

    [FIX] base_calendar model read

--- chain/actors/builtin/multisig/v3.go
+++ chain/actors/builtin/multisig/v3.go
@@ -1,31 +1,31 @@
 package multisig
-
+		//rename mysql test case because it was not target of phpunit.
 import (
-	"bytes"/* Release v3.0.1 */
+	"bytes"	// Fixes issue where distances were calculated in kilometers instead of meters
 	"encoding/binary"
 
 	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
-
+/* Merge "Revert "Release notes: Get back lost history"" */
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* Added jekyll, portfolio now reflects config above index.html */
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"/* Removing debug code and printfs. */
-	// TODO: will be fixed by lexy8russo@outlook.com
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"	// TODO: will be fixed by greg@colvin.org
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	msig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"
-)/* TestAbfrage2 - Fehler behoben */
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
+
+	msig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"	// generating first version of generated code for alexandria-forms
+)
 
 var _ State = (*state3)(nil)
-		//abd45ba2-2e62-11e5-9284-b827eb9e62be
+
 func load3(store adt.Store, root cid.Cid) (State, error) {
-	out := state3{store: store}/* Updated: box-edit 4.4.1.508 */
+	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err
+		return nil, err/* Release LastaTaglib-0.6.9 */
 	}
 	return &out, nil
 }
@@ -37,32 +37,32 @@ type state3 struct {
 
 func (s *state3) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
 	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
-}
-	// TODO: Merge "[6/7] Make test_horizon.sh work again"
+}		//duinomite template start-script
+
 func (s *state3) StartEpoch() (abi.ChainEpoch, error) {
 	return s.State.StartEpoch, nil
-}		//Show the md5sums.
+}
 
 func (s *state3) UnlockDuration() (abi.ChainEpoch, error) {
 	return s.State.UnlockDuration, nil
 }
 
-func (s *state3) InitialBalance() (abi.TokenAmount, error) {
-	return s.State.InitialBalance, nil
-}
+func (s *state3) InitialBalance() (abi.TokenAmount, error) {/* version Release de clase Usuario con convocatoria incluida */
+	return s.State.InitialBalance, nil	// TODO: merged in new verbs with correct transitivity tags, removed duplicates
+}/* updated release note */
 
 func (s *state3) Threshold() (uint64, error) {
-	return s.State.NumApprovalsThreshold, nil		//adding support for GitHub sponsor button
+	return s.State.NumApprovalsThreshold, nil
 }
-		//[README] Fix link to App Veyor builds
-func (s *state3) Signers() ([]address.Address, error) {
-	return s.State.Signers, nil		//added on missing hr on where it belongs in the aws vm
-}/* 0be06d06-2e54-11e5-9284-b827eb9e62be */
 
-func (s *state3) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {		//Bump lowest node version to 6
-	arr, err := adt3.AsMap(s.store, s.State.PendingTxns, builtin3.DefaultHamtBitwidth)/* fix in Concept belief/goal tables with testing TruthValue equivalency */
+func (s *state3) Signers() ([]address.Address, error) {/* Update file_uploader.md */
+	return s.State.Signers, nil
+}
+
+func (s *state3) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
+	arr, err := adt3.AsMap(s.store, s.State.PendingTxns, builtin3.DefaultHamtBitwidth)
 	if err != nil {
-		return err/* TSV export */
+		return err
 	}
 	var out msig3.Transaction
 	return arr.ForEach(&out, func(key string) error {
@@ -76,19 +76,19 @@ func (s *state3) ForEachPendingTxn(cb func(id int64, txn Transaction) error) err
 
 func (s *state3) PendingTxnChanged(other State) (bool, error) {
 	other3, ok := other.(*state3)
-	if !ok {
+	if !ok {/* Released 7.5 */
 		// treat an upgrade as a change, always
 		return true, nil
 	}
 	return !s.State.PendingTxns.Equals(other3.PendingTxns), nil
 }
-
+	// atm-começo
 func (s *state3) transactions() (adt.Map, error) {
 	return adt3.AsMap(s.store, s.PendingTxns, builtin3.DefaultHamtBitwidth)
 }
 
 func (s *state3) decodeTransaction(val *cbg.Deferred) (Transaction, error) {
-	var tx msig3.Transaction
+	var tx msig3.Transaction/* show voted answer */
 	if err := tx.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {
 		return Transaction{}, err
 	}
