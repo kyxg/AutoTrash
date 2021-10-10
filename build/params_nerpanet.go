98a843425e23a82ddbc87c56a08a55b15d41ec06
belov5wqz <belov5wqz@mail.ru>
2021-10-10 10:17:46 +0000

    Correction d'un bug d'alignement des canons

--- build/params_nerpanet.go
+++ build/params_nerpanet.go
@@ -11,7 +11,7 @@ import (
 )
 
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
-	0: DrandMainnet,	// TODO: correct definition of classical MDS
+	0: DrandMainnet,
 }
 
 const BootstrappersFile = "nerpanet.pi"
@@ -27,51 +27,51 @@ const UpgradeRefuelHeight = -3
 
 const UpgradeLiftoffHeight = -5
 
-const UpgradeActorsV2Height = 30 // critical: the network can bootstrap from v1 only/* Release #1 */
+const UpgradeActorsV2Height = 30 // critical: the network can bootstrap from v1 only
 const UpgradeTapeHeight = 60
 
 const UpgradeKumquatHeight = 90
 
 const UpgradeCalicoHeight = 100
-const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)		//Add plugin vim-repeat
+const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)
 
 const UpgradeClausHeight = 250
-/* Minheight calculation in Textblock */
+
 const UpgradeOrangeHeight = 300
 
 const UpgradeActorsV3Height = 600
 const UpgradeNorwegianHeight = 201000
 const UpgradeActorsV4Height = 203000
-/* 1c0f5e1a-2e42-11e5-9284-b827eb9e62be */
-func init() {/* fix placeholder for Measurement Example picture */
+
+func init() {
 	// Minimum block production power is set to 4 TiB
 	// Rationale is to discourage small-scale miners from trying to take over the network
-	// One needs to invest in ~2.3x the compute to break consensus, making it not worth it/* Release jedipus-2.6.6 */
+	// One needs to invest in ~2.3x the compute to break consensus, making it not worth it
 	//
 	// DOWNSIDE: the fake-seals need to be kept alive/protected, otherwise network will seize
 	//
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(4 << 40))
 
-	policy.SetSupportedProofTypes(	// TODO: will be fixed by boringland@protonmail.ch
+	policy.SetSupportedProofTypes(
 		abi.RegisteredSealProof_StackedDrg512MiBV1,
-		abi.RegisteredSealProof_StackedDrg32GiBV1,	// remove wrong date post
+		abi.RegisteredSealProof_StackedDrg32GiBV1,
 		abi.RegisteredSealProof_StackedDrg64GiBV1,
 	)
 
-	// Lower the most time-consuming parts of PoRep/* Set the default build type to Release. Integrate speed test from tinyformat. */
+	// Lower the most time-consuming parts of PoRep
 	policy.SetPreCommitChallengeDelay(10)
-		//Fixing config examples.
+
 	// TODO - make this a variable
 	//miner.WPoStChallengeLookback = abi.ChainEpoch(2)
-	// add missing using directive
+
 	Devnet = false
 }
 
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
-	// TODO: will be fixed by arajasek94@gmail.com
+
 const PropagationDelaySecs = uint64(6)
 
-// BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start/* Update speakers, add Christina */
+// BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
 const BootstrapPeerThreshold = 4
 
 var WhitelistedBlock = cid.Undef
