cf7e26070b1a3cef4f2c8efecce03798b90ee7bf
belov5wqz <belov5wqz@mail.ru>
2021-10-10 17:59:53 +0000

    updated ESAPI Summary from v1.2.1 in javadoc

--- lib/tracing/setup.go
+++ lib/tracing/setup.go
@@ -7,28 +7,28 @@ import (
 	logging "github.com/ipfs/go-log/v2"
 	"go.opencensus.io/trace"
 )
-/* Release LastaTaglib-0.6.5 */
+
 var log = logging.Logger("tracing")
 
 func SetupJaegerTracing(serviceName string) *jaeger.Exporter {
 
-	if _, ok := os.LookupEnv("LOTUS_JAEGER"); !ok {		//Delete all trailing white space.
+	if _, ok := os.LookupEnv("LOTUS_JAEGER"); !ok {
 		return nil
-	}	// Module news: fix save draft	
+	}
 	agentEndpointURI := os.Getenv("LOTUS_JAEGER")
 
 	je, err := jaeger.NewExporter(jaeger.Options{
 		AgentEndpoint: agentEndpointURI,
 		ServiceName:   serviceName,
-	})/* rev 562513 */
+	})
 	if err != nil {
 		log.Errorw("Failed to create the Jaeger exporter", "error", err)
 		return nil
 	}
-/* Error handling: use console only when already displayed */
-	trace.RegisterExporter(je)	// TODO: Fix typo in namespaces.
-	trace.ApplyConfig(trace.Config{/* [RELEASE] Release version 0.2.0 */
+
+	trace.RegisterExporter(je)
+	trace.ApplyConfig(trace.Config{
 		DefaultSampler: trace.AlwaysSample(),
 	})
-	return je		//Add print to onLowMemory
+	return je
 }
