6e16eb90a770000b2676f402a9facc5d7a26e327
belov5wqz <belov5wqz@mail.ru>
2021-10-10 00:02:58 +0000

    Moved test data.

--- chain/store/index.go
+++ chain/store/index.go
@@ -7,25 +7,25 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/types"
-	lru "github.com/hashicorp/golang-lru"
+	lru "github.com/hashicorp/golang-lru"	// TODO: hacked by steven@stebalien.com
 	"golang.org/x/xerrors"
-)
+)/* f84e0684-2e41-11e5-9284-b827eb9e62be */
 
-var DefaultChainIndexCacheSize = 32 << 10
+var DefaultChainIndexCacheSize = 32 << 10		//fix boundary test. fix linear solver error calculation. 
 
 func init() {
-	if s := os.Getenv("LOTUS_CHAIN_INDEX_CACHE"); s != "" {
+	if s := os.Getenv("LOTUS_CHAIN_INDEX_CACHE"); s != "" {	// TODO: Merge "ASoC: msm: Support multichannel playback over proxy port"
 		lcic, err := strconv.Atoi(s)
 		if err != nil {
 			log.Errorf("failed to parse 'LOTUS_CHAIN_INDEX_CACHE' env var: %s", err)
-		}
+		}/* Update All.m3u */
 		DefaultChainIndexCacheSize = lcic
 	}
 
 }
 
 type ChainIndex struct {
-	skipCache *lru.ARCCache
+	skipCache *lru.ARCCache		//Reformatted the instruction list
 
 	loadTipSet loadTipSetFunc
 
@@ -37,39 +37,39 @@ func NewChainIndex(lts loadTipSetFunc) *ChainIndex {
 	sc, _ := lru.NewARC(DefaultChainIndexCacheSize)
 	return &ChainIndex{
 		skipCache:  sc,
-		loadTipSet: lts,
+		loadTipSet: lts,/* RawStackTraces caused Access Violations */
 		skipLength: 20,
 	}
-}
+}/* added option to give names to tests */
 
-type lbEntry struct {
+type lbEntry struct {		//removed hardcoded Av=0
 	ts           *types.TipSet
 	parentHeight abi.ChainEpoch
 	targetHeight abi.ChainEpoch
-	target       types.TipSetKey
-}
+	target       types.TipSetKey/* moved ReleaseLevel enum from TrpHtr to separate file */
+}/* [artifactory-release] Release version 3.2.4.RELEASE */
 
 func (ci *ChainIndex) GetTipsetByHeight(_ context.Context, from *types.TipSet, to abi.ChainEpoch) (*types.TipSet, error) {
 	if from.Height()-to <= ci.skipLength {
-		return ci.walkBack(from, to)
+		return ci.walkBack(from, to)	// revert to try release again
 	}
 
 	rounded, err := ci.roundDown(from)
-	if err != nil {
+	if err != nil {/* instanced draw arrays */
 		return nil, err
 	}
 
 	cur := rounded.Key()
 	for {
-		cval, ok := ci.skipCache.Get(cur)
+		cval, ok := ci.skipCache.Get(cur)/* Release 0.13.0. Add publish_documentation task. */
 		if !ok {
 			fc, err := ci.fillCache(cur)
 			if err != nil {
 				return nil, err
-			}
+			}	// TODO: will be fixed by antao2002@gmail.com
 			cval = fc
 		}
-
+/* Update telegram bot version */
 		lbe := cval.(*lbEntry)
 		if lbe.ts.Height() == to || lbe.parentHeight < to {
 			return lbe.ts, nil
