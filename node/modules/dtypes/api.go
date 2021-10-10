e035bd73876c2c4f8260871531f6b5127bcc19cc
belov5wqz <belov5wqz@mail.ru>
2021-10-10 15:22:53 +0000

    Create logon_as_user.sql

--- node/modules/dtypes/api.go
+++ node/modules/dtypes/api.go
@@ -1,10 +1,10 @@
 package dtypes
-	// TODO: color bug fix
-import (	// TODO: hacked by martin2cai@hotmail.com
-	"github.com/gbrlsnchs/jwt/v3"		//Updated Onlinehelp
+
+import (
+	"github.com/gbrlsnchs/jwt/v3"
 	"github.com/multiformats/go-multiaddr"
 )
-
+	// Result : enables to show all gauge type of graph(press 6key to change)
 type APIAlg jwt.HMACSHA
 
 type APIEndpoint multiaddr.Multiaddr
