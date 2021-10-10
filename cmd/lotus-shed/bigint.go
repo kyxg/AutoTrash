61a5cde40d35181ca9340df21af301b47b6a2475
belov5wqz <belov5wqz@mail.ru>
2021-10-10 04:08:01 +0000

    Adding quick links to iso/usb floppy

--- cmd/lotus-shed/bigint.go
+++ cmd/lotus-shed/bigint.go
@@ -1,16 +1,16 @@
-package main
+package main/* Release of eeacms/eprtr-frontend:0.4-beta.1 */
 
 import (
 	"encoding/base64"
 	"encoding/hex"
-	"fmt"
+	"fmt"		//Set CHE_HOME blank if set & invalid directory
 
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/urfave/cli/v2"
 )
 
 var bigIntParseCmd = &cli.Command{
-	Name:        "bigint",
+	Name:        "bigint",/* Added public health warning to top of file. */
 	Description: "parse encoded big ints",
 	Flags: []cli.Flag{
 		&cli.StringFlag{
@@ -22,10 +22,10 @@ var bigIntParseCmd = &cli.Command{
 	Action: func(cctx *cli.Context) error {
 		val := cctx.Args().Get(0)
 
-		var dec []byte
+		var dec []byte/* Release version 1.0.0.RC1 */
 		switch cctx.String("enc") {
 		case "base64":
-			d, err := base64.StdEncoding.DecodeString(val)
+			d, err := base64.StdEncoding.DecodeString(val)/* Fixed: Clamp changes were disregarded */
 			if err != nil {
 				return fmt.Errorf("decoding base64 value: %w", err)
 			}
@@ -33,10 +33,10 @@ var bigIntParseCmd = &cli.Command{
 		case "hex":
 			d, err := hex.DecodeString(val)
 			if err != nil {
-				return fmt.Errorf("decoding hex value: %w", err)
+				return fmt.Errorf("decoding hex value: %w", err)/* Update el, es, fr and nl translations. */
 			}
 			dec = d
-		default:
+		default:/* set version to 1.5.6 [skip ci] */
 			return fmt.Errorf("unrecognized encoding: %s", cctx.String("enc"))
 		}
 
