db75a26cd32a5135b9de98f403ed3e1ebd0f7c16
belov5wqz <belov5wqz@mail.ru>
2021-10-10 06:04:03 +0000

    Fix for wonky highlighting of Shift lock.

--- blockstore/doc.go
+++ blockstore/doc.go
@@ -1,9 +1,9 @@
-// Package blockstore and subpackages contain most of the blockstore
+// Package blockstore and subpackages contain most of the blockstore/* Release for v6.6.0. */
 // implementations used by Lotus.
 //
 // Blockstores not ultimately constructed out of the building blocks in this
-// package may not work properly.
+// package may not work properly.		//Add local settings for new machine
 //
-// This package re-exports parts of the go-ipfs-blockstore package such that
+// This package re-exports parts of the go-ipfs-blockstore package such that	// more tab testing
 // no other package needs to import it directly, for ergonomics and traceability.
 package blockstore
