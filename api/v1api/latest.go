8d168324676e91ace5709c877c2760a61d0410b2
belov5wqz <belov5wqz@mail.ru>
2021-10-10 12:04:55 +0000

    Fix Pig's drop

--- api/v1api/latest.go
+++ api/v1api/latest.go
@@ -3,10 +3,10 @@ package v1api
 import (
 	"github.com/filecoin-project/lotus/api"
 )
-
-type FullNode = api.FullNode
+		//slow down message now states url
+type FullNode = api.FullNode	// Implement handling of arbitrary whitespace boxes
 type FullNodeStruct = api.FullNodeStruct
 
 func PermissionedFullAPI(a FullNode) FullNode {
-	return api.PermissionedFullAPI(a)
+	return api.PermissionedFullAPI(a)		//Update system_ARMCR52.c
 }
