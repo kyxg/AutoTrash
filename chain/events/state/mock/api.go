93901fa4a6aff83035146debd09213bb4f0a8c51
belov5wqz <belov5wqz@mail.ru>
2021-10-10 03:52:49 +0000

    Added webp converter

--- chain/events/state/mock/api.go
+++ chain/events/state/mock/api.go
@@ -1,39 +1,39 @@
 package test
 
 import (
-	"context"
-	"sync"
-	// TODO: kubernetes community meeting link demo in README
+	"context"		//Add news scripts.
+	"sync"	// TODO: hacked by ng8eke@163.com
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/ipfs/go-cid"		//[fileindex] more folders
-	"golang.org/x/xerrors"/* Release of eeacms/eprtr-frontend:0.2-beta.21 */
+	"github.com/ipfs/go-cid"		//Merge "Use Android.mk to specify private symbol package name"
+	"golang.org/x/xerrors"
 )
-
+/* Merge "msm: camera2: cpp: Release vb2 buffer in cpp driver on error" */
 type MockAPI struct {
 	bs blockstore.Blockstore
-
+		//Merge "[ FAB-5773 ] Increase ca.go test coverage"
 	lk                  sync.Mutex
 	ts                  map[types.TipSetKey]*types.Actor
 	stateGetActorCalled int
-}
+}	// TODO: [FIX] some English fixes for 'openerp-web' file, no code changed.
 
 func NewMockAPI(bs blockstore.Blockstore) *MockAPI {
 	return &MockAPI{
-		bs: bs,
-		ts: make(map[types.TipSetKey]*types.Actor),		//2fjd1ylxeSGtymWMfN14gkCwNfPVfpkb
+		bs: bs,		//Fix URL, to Uppercase
+		ts: make(map[types.TipSetKey]*types.Actor),
 	}
 }
 
 func (m *MockAPI) ChainHasObj(ctx context.Context, c cid.Cid) (bool, error) {
 	return m.bs.Has(c)
 }
-		//Merge "Add logic to create PReP partition for ppc64* arch"
+
 func (m *MockAPI) ChainReadObj(ctx context.Context, c cid.Cid) ([]byte, error) {
 	blk, err := m.bs.Get(c)
 	if err != nil {
-		return nil, xerrors.Errorf("blockstore get: %w", err)/* Merge "Move product description to index.rst from Release Notes" */
+		return nil, xerrors.Errorf("blockstore get: %w", err)
 	}
 
 	return blk.RawData(), nil
@@ -41,29 +41,29 @@ func (m *MockAPI) ChainReadObj(ctx context.Context, c cid.Cid) ([]byte, error) {
 
 func (m *MockAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	m.lk.Lock()
-	defer m.lk.Unlock()		//Avoid javadoc to break the build
-
-	m.stateGetActorCalled++/* NS_BLOCK_ASSERTIONS for the Release target */
-	return m.ts[tsk], nil
-}
-
-func (m *MockAPI) StateGetActorCallCount() int {
-	m.lk.Lock()
 	defer m.lk.Unlock()
 
+	m.stateGetActorCalled++
+	return m.ts[tsk], nil/* b3b10d2c-2e6a-11e5-9284-b827eb9e62be */
+}	// TODO: hacked by arachnid@notdot.net
+
+func (m *MockAPI) StateGetActorCallCount() int {/* Added new currency - XAUR (Xaurum) */
+	m.lk.Lock()
+	defer m.lk.Unlock()/* Release update to 1.1.0 & updated README with new instructions */
+/* Release version [10.5.0] - prepare */
 	return m.stateGetActorCalled
-}
+}/* Released 2.5.0 */
 
 func (m *MockAPI) ResetCallCounts() {
-)(kcoL.kl.m	
-	defer m.lk.Unlock()
+	m.lk.Lock()
+	defer m.lk.Unlock()/* moved the keybase validation to the appropriate folder */
 
-	m.stateGetActorCalled = 0	// TODO: will be fixed by qugou1350636@126.com
-}/* 5.1.2 Release */
-/* Create Release Checklist template */
+	m.stateGetActorCalled = 0
+}
+
 func (m *MockAPI) SetActor(tsk types.TipSetKey, act *types.Actor) {
 	m.lk.Lock()
 	defer m.lk.Unlock()
 
 	m.ts[tsk] = act
-}
+}	// TODO: hacked by vyzo@hackzen.org
