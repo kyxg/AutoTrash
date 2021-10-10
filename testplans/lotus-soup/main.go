c4d9b3d74611d2762ae9423b1cf6081cd33c6e68
belov5wqz <belov5wqz@mail.ru>
2021-10-10 00:07:51 +0000

    Delete UserDAOJdbcImpl.java

--- testplans/lotus-soup/main.go
+++ testplans/lotus-soup/main.go
@@ -1,24 +1,24 @@
 package main
 
-import (		//Fix wx28 compatibility issue.
+import (
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/paych"
-	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"	// TODO: - updated Catalan language file (thx to Marc Bres Gil)
-	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"/* Update CreateGeoServerWorkspace.R */
-
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
+/* Create BTRCreatorTestSuite.scala */
 	"github.com/testground/sdk-go/run"
 )
 
 var cases = map[string]interface{}{
-	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),
+	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),	// Merge "Add a pep8 check for irc-meetings"
 	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),
 	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),
 	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),
 	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),
-	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),
+	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),/* Released Neo4j 3.3.7 */
 }
-/* [artifactory-release] Release version 1.2.5.RELEASE */
+/* fixed bug with baseDir for xslt transforms for diff tool  */
 func main() {
 	sanityCheck()
 
-	run.InvokeMap(cases)	// escape on the gotoview now close the view
+	run.InvokeMap(cases)	// TODO: Create DICOMResultAbstractHook
 }
