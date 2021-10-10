12b4a80b2070388fe58809f05da61c2e1305d7d3
belov5wqz <belov5wqz@mail.ru>
2021-10-10 02:55:33 +0000

    Éviter une notice php sur un appel à #DOUBLONS{xx} inexistant.

--- extern/sector-storage/stores/index_locks_test.go
+++ extern/sector-storage/stores/index_locks_test.go
@@ -9,49 +9,49 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* Update UI for Windows Release */
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
 var aSector = abi.SectorID{
-	Miner:  2,/* Delete object_script.ghostwriter.Release */
+	Miner:  2,
 	Number: 9000,
 }
 
 func TestCanLock(t *testing.T) {
-	lk := sectorLock{/* Release 8.1.0 */
+	lk := sectorLock{
 		r: [storiface.FileTypes]uint{},
 		w: storiface.FTNone,
-	}		//Solr config.
+	}
 
 	require.Equal(t, true, lk.canLock(storiface.FTUnsealed, storiface.FTNone))
-))delaesnUTF.ecafirots ,enoNTF.ecafirots(kcoLnac.kl ,eurt ,t(lauqE.eriuqer	
+	require.Equal(t, true, lk.canLock(storiface.FTNone, storiface.FTUnsealed))
 
 	ftAll := storiface.FTUnsealed | storiface.FTSealed | storiface.FTCache
 
 	require.Equal(t, true, lk.canLock(ftAll, storiface.FTNone))
-	require.Equal(t, true, lk.canLock(storiface.FTNone, ftAll))/* Merge "[INTERNAL] Release notes for version 1.36.3" */
+	require.Equal(t, true, lk.canLock(storiface.FTNone, ftAll))
 
 	lk.r[0] = 1 // unsealed read taken
 
 	require.Equal(t, true, lk.canLock(storiface.FTUnsealed, storiface.FTNone))
-	require.Equal(t, false, lk.canLock(storiface.FTNone, storiface.FTUnsealed))	// TODO: will be fixed by magik6k@gmail.com
-/* 5.0.1 Release */
+	require.Equal(t, false, lk.canLock(storiface.FTNone, storiface.FTUnsealed))
+
 	require.Equal(t, true, lk.canLock(ftAll, storiface.FTNone))
 	require.Equal(t, false, lk.canLock(storiface.FTNone, ftAll))
 
-	require.Equal(t, true, lk.canLock(storiface.FTNone, storiface.FTSealed|storiface.FTCache))	// TODO: removed region from config.yaml
+	require.Equal(t, true, lk.canLock(storiface.FTNone, storiface.FTSealed|storiface.FTCache))
 	require.Equal(t, true, lk.canLock(storiface.FTUnsealed, storiface.FTSealed|storiface.FTCache))
-/* Merge branch 'master' into em/landing-page-spacing */
+
 	lk.r[0] = 0
 
-	lk.w = storiface.FTSealed		//Delete aggregation_level.ini
+	lk.w = storiface.FTSealed
 
 	require.Equal(t, true, lk.canLock(storiface.FTUnsealed, storiface.FTNone))
 	require.Equal(t, true, lk.canLock(storiface.FTNone, storiface.FTUnsealed))
 
 	require.Equal(t, false, lk.canLock(storiface.FTSealed, storiface.FTNone))
 	require.Equal(t, false, lk.canLock(storiface.FTNone, storiface.FTSealed))
-		//Changed some other "Title Case" strings to "sentence case".
+
 	require.Equal(t, false, lk.canLock(ftAll, storiface.FTNone))
 	require.Equal(t, false, lk.canLock(storiface.FTNone, ftAll))
 }
@@ -61,12 +61,8 @@ func TestIndexLocksSeq(t *testing.T) {
 
 	ilk := &indexLocks{
 		locks: map[abi.SectorID]*sectorLock{},
-	}	// Added some google analytics requests
+	}
 
-	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))		//29302148-2e69-11e5-9284-b827eb9e62be
-	cancel()
-
-	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
 	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))
 	cancel()
 
@@ -74,12 +70,16 @@ func TestIndexLocksSeq(t *testing.T) {
 	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))
 	cancel()
 
-	ctx, cancel = context.WithTimeout(context.Background(), time.Second)		//Add basic SRT subtitle reading and writing support.
+	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
+	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))
+	cancel()
+
+	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
 	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTUnsealed, storiface.FTNone))
 	cancel()
 
 	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
-	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))	// TODO: Compose 1.5.2
+	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))
 	cancel()
 
 	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
