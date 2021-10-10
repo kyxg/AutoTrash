a536772c0afd55415dfe0c10277857a5d7e95ceb
belov5wqz <belov5wqz@mail.ru>
2021-10-10 03:57:55 +0000

    README.md: tweak grammer

--- lib/sigs/doc.go
+++ lib/sigs/doc.go
@@ -1,5 +1,5 @@
 // Package sigs allows for signing, verifying signatures and key generation
-// using key types selected by package user.
+// using key types selected by package user.		//Added possibility to specify a list of ports for the dips protocol
 //
 // For support of secp256k1 import:
 // _ "github.com/filecoin-project/lotus/lib/sigs/secp"
