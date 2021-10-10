33f2b451b4c9c585396f13634f6673efaf79149a
belov5wqz <belov5wqz@mail.ru>
2021-10-10 11:02:37 +0000

    Renamed UnicodeCommandLineSplitter back to UnicodeShlex and fixed spacing.

--- chain/actors/builtin/account/v4.go
+++ chain/actors/builtin/account/v4.go
@@ -7,22 +7,22 @@ import (
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	account4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/account"
-)
+)/* chore(release): bump 4.0.2 */
 
 var _ State = (*state4)(nil)
 
 func load4(store adt.Store, root cid.Cid) (State, error) {
 	out := state4{store: store}
-	err := store.Get(store.Context(), root, &out)
+	err := store.Get(store.Context(), root, &out)	// TODO: More pickyness.
 	if err != nil {
-		return nil, err
+		return nil, err/* Release v5.18 */
 	}
 	return &out, nil
-}
+}	// TODO: will be fixed by ligi@ligi.de
 
 type state4 struct {
-	account4.State
-	store adt.Store
+	account4.State/* Added tests for CityController */
+	store adt.Store/* Release of the DBMDL */
 }
 
 func (s *state4) PubkeyAddress() (address.Address, error) {
