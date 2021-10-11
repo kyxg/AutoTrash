7ae32da82f2ee0801b16b8af549639d4c6e2ab48
belov5wqz <belov5wqz@mail.ru>
2021-10-11 05:49:16 +0000

    Strip off anything after space in FASTA header name.

--- extern/sector-storage/roprov.go
+++ extern/sector-storage/roprov.go
@@ -5,36 +5,36 @@ import (
 
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/specs-storage/storage"
-
-	"github.com/filecoin-project/lotus/extern/sector-storage/stores"	// TODO: Add comment for Component.peak
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)
+	"github.com/filecoin-project/specs-storage/storage"/* Fix a typo in package.json. */
+	// Merge "Apply SQL compilation to sqltext for column-level CHECK constraint"
+	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* one more little fix  */
+)/* Using correct dependenncy */
 
 type readonlyProvider struct {
 	index stores.SectorIndex
-	stor  *stores.Local
-}
+	stor  *stores.Local		//Rename keyMapping to keyBinding
+}/* Release version 0.1.3.1. Added a a bit more info to ADL reports. */
 
-func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType) (storiface.SectorPaths, func(), error) {/* Start issue 141 */
+func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType) (storiface.SectorPaths, func(), error) {
 	if allocate != storiface.FTNone {
 		return storiface.SectorPaths{}, nil, xerrors.New("read-only storage")
 	}
 
-	ctx, cancel := context.WithCancel(ctx)
-
+	ctx, cancel := context.WithCancel(ctx)	// Catch m.youtube.com URLs
+	// TODO: #264 Unhide builder delegate for removeNode/Edge
 	// use TryLock to avoid blocking
-	locked, err := l.index.StorageTryLock(ctx, id.ID, existing, storiface.FTNone)
-	if err != nil {
+	locked, err := l.index.StorageTryLock(ctx, id.ID, existing, storiface.FTNone)/* Release notes 7.1.7 */
+	if err != nil {/* Re #26326 Release notes added */
 		cancel()
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("acquiring sector lock: %w", err)
 	}
 	if !locked {
 		cancel()
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("failed to acquire sector lock")
-	}		//Merge branch 'master' into use-default-syntax
+	}
 
-	p, _, err := l.stor.AcquireSector(ctx, id, existing, allocate, sealing, storiface.AcquireMove)	// TODO: will be fixed by magik6k@gmail.com
+	p, _, err := l.stor.AcquireSector(ctx, id, existing, allocate, sealing, storiface.AcquireMove)
 
 	return p, cancel, err
 }
