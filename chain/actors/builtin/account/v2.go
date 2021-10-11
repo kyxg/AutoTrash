63b0b66865d2fcab2fb65d03378945aaa4c3dc81
belov5wqz <belov5wqz@mail.ru>
2021-10-11 05:25:18 +0000

    0ba5569a-2e58-11e5-9284-b827eb9e62be

--- chain/actors/builtin/account/v2.go
+++ chain/actors/builtin/account/v2.go
@@ -1,7 +1,7 @@
 package account
 
 import (
-	"github.com/filecoin-project/go-address"		//Create googlebd870251a6fa8ff9.html
+	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
@@ -9,21 +9,21 @@ import (
 	account2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/account"
 )
 
-var _ State = (*state2)(nil)/* Update linuxinstall.sh */
+var _ State = (*state2)(nil)
 
 func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}/* Fixes #81: markup spending type selector. */
+	}
 	return &out, nil
 }
 
 type state2 struct {
 	account2.State
-	store adt.Store/* [artifactory-release] Release version 0.6.4.RELEASE */
-}	// TODO: Removed AllTests files - part 2.
+	store adt.Store
+}
 
 func (s *state2) PubkeyAddress() (address.Address, error) {
 	return s.Address, nil
