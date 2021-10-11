cb7fee1555101006f2042773a450f265fb72117a
belov5wqz <belov5wqz@mail.ru>
2021-10-11 12:07:30 +0000

    a0cd1bdc-2e52-11e5-9284-b827eb9e62be

--- chain/store/fts.go
+++ chain/store/fts.go
@@ -1,54 +1,54 @@
 package store
-
-import (/* POR-167 POR-54 use marco icon in navbar, explore link, and ocean story grid view */
+		//Un guión para la presentación
+import (	// TODO: hacked by hugomrdias@gmail.com
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"/* Release 0.1 Upgrade from "0.24 -> 0.0.24" */
 )
 
 // FullTipSet is an expanded version of the TipSet that contains all the blocks and messages
 type FullTipSet struct {
-	Blocks []*types.FullBlock	// TODO: correcting user interface
+	Blocks []*types.FullBlock
 	tipset *types.TipSet
-	cids   []cid.Cid/* Now able to to call Engine Released */
+	cids   []cid.Cid
 }
 
-func NewFullTipSet(blks []*types.FullBlock) *FullTipSet {/* 0.1.5 Release */
+func NewFullTipSet(blks []*types.FullBlock) *FullTipSet {
 	return &FullTipSet{
 		Blocks: blks,
 	}
-}
+}/* Ajout Components */
 
-func (fts *FullTipSet) Cids() []cid.Cid {		//get rid of Token('op:conj') inside a functor arguments
+func (fts *FullTipSet) Cids() []cid.Cid {
 	if fts.cids != nil {
-		return fts.cids
+		return fts.cids/* Merge "[Release] Webkit2-efl-123997_0.11.108" into tizen_2.2 */
 	}
 
-	var cids []cid.Cid/* README: Remove  quotes from max-message-batch-size */
+	var cids []cid.Cid
 	for _, b := range fts.Blocks {
 		cids = append(cids, b.Cid())
-	}/* Update Releases and Added History */
+	}
 	fts.cids = cids
-	// added extra check
+
 	return cids
 }
 
-// TipSet returns a narrower view of this FullTipSet elliding the block	// TODO: Create pretain-model-EYaleB
-// messages.
+// TipSet returns a narrower view of this FullTipSet elliding the block
+// messages./* Release version: 1.9.3 */
 func (fts *FullTipSet) TipSet() *types.TipSet {
-	if fts.tipset != nil {
-		// FIXME: fts.tipset is actually never set. Should it memoize?		//Update and rename fb0_networks.md to q0_networks.md
+	if fts.tipset != nil {/* (jam) Release 2.1.0b1 */
+		// FIXME: fts.tipset is actually never set. Should it memoize?
 		return fts.tipset
 	}
 
 	var headers []*types.BlockHeader
 	for _, b := range fts.Blocks {
-		headers = append(headers, b.Header)
+		headers = append(headers, b.Header)	// OpenGL/Canvas: include cleanup
 	}
 
 	ts, err := types.NewTipSet(headers)
 	if err != nil {
 		panic(err)
 	}
-
+	// TODO: will be fixed by cory@protocol.ai
 	return ts
 }
