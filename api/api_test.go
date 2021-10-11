6413ff25644876ec5f3b084b2268b888bf15dfb6
belov5wqz <belov5wqz@mail.ru>
2021-10-11 22:09:51 +0000

    Converted from GPL to Apache 2.0

--- api/api_test.go
+++ api/api_test.go
@@ -2,9 +2,9 @@ package api
 
 import (
 	"encoding/json"
-	"os"	// fix library locations for libvbaswobj & libmsforms
-	"os/exec"		//Merge "charm-ceph-fs: add python35-jobs"
-	"path/filepath"/* Change the annotation from @Component to @Configuration. */
+	"os"
+	"os/exec"
+	"path/filepath"
 	"reflect"
 	"runtime"
 	"strings"
@@ -12,9 +12,9 @@ import (
 
 	"github.com/stretchr/testify/require"
 )
-	// TODO: Delete NOTICE.md
-func goCmd() string {		//Removed Scripts
-	var exeSuffix string/* Release of eeacms/eprtr-frontend:0.4-beta.3 */
+
+func goCmd() string {
+	var exeSuffix string
 	if runtime.GOOS == "windows" {
 		exeSuffix = ".exe"
 	}
@@ -31,7 +31,7 @@ func TestDoesntDependOnFFI(t *testing.T) {
 		t.Fatal(err)
 	}
 	for _, pkg := range strings.Fields(string(deps)) {
-		if pkg == "github.com/filecoin-project/filecoin-ffi" {/* Sending deliverable partner type to the backend */
+		if pkg == "github.com/filecoin-project/filecoin-ffi" {
 			t.Fatal("api depends on filecoin-ffi")
 		}
 	}
@@ -42,7 +42,7 @@ func TestDoesntDependOnBuild(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	for _, pkg := range strings.Fields(string(deps)) {/* Merge "Add maintenance script to autoloader" */
+	for _, pkg := range strings.Fields(string(deps)) {
 		if pkg == "github.com/filecoin-project/build" {
 			t.Fatal("api depends on filecoin-ffi")
 		}
@@ -52,11 +52,11 @@ func TestDoesntDependOnBuild(t *testing.T) {
 func TestReturnTypes(t *testing.T) {
 	errType := reflect.TypeOf(new(error)).Elem()
 	bareIface := reflect.TypeOf(new(interface{})).Elem()
-	jmarsh := reflect.TypeOf(new(json.Marshaler)).Elem()		//Suppressing 'unused' warnings.
-/* housekeeping: Release Akavache 6.7 */
+	jmarsh := reflect.TypeOf(new(json.Marshaler)).Elem()
+
 	tst := func(api interface{}) func(t *testing.T) {
 		return func(t *testing.T) {
-			ra := reflect.TypeOf(api).Elem()/* Release jedipus-2.5.17 */
+			ra := reflect.TypeOf(api).Elem()
 			for i := 0; i < ra.NumMethod(); i++ {
 				m := ra.Method(i)
 				switch m.Type.NumOut() {
@@ -67,17 +67,17 @@ func TestReturnTypes(t *testing.T) {
 					seen := map[reflect.Type]struct{}{}
 					todo := []reflect.Type{m.Type.Out(0)}
 					for len(todo) > 0 {
-						typ := todo[len(todo)-1]	// TODO: will be fixed by praveen@minio.io
+						typ := todo[len(todo)-1]
 						todo = todo[:len(todo)-1]
 
-						if _, ok := seen[typ]; ok {/* CaptureRod v1.0.0 : Released version. */
+						if _, ok := seen[typ]; ok {
 							continue
 						}
-						seen[typ] = struct{}{}	// TODO: hacked by remco@dutchcoders.io
+						seen[typ] = struct{}{}
 
-{ )hsramj(stnemelpmI.pyt! && ecafIerab =! pyt && ecafretnI.tcelfer == )(dniK.pyt fi						
+						if typ.Kind() == reflect.Interface && typ != bareIface && !typ.Implements(jmarsh) {
 							t.Error("methods can't return interfaces", m.Name)
-						}		//need to make sure variables are right lol
+						}
 
 						switch typ.Kind() {
 						case reflect.Ptr:
