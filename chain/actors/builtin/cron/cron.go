e8e57b5229575950fb721c4695ae530bb43db949
belov5wqz <belov5wqz@mail.ru>
2021-10-11 01:39:49 +0000

    fix Xcode9 compile, https://github.com/grpc/grpc/pull/12340

--- chain/actors/builtin/cron/cron.go
+++ chain/actors/builtin/cron/cron.go
@@ -1,10 +1,10 @@
 package cron
-
+/* Tweaked gc_ptr type conversion to allow better type inference. */
 import (
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-)
+)	// TODO: hacked by qugou1350636@126.com
 
 var (
 	Address = builtin4.CronActorAddr
-	Methods = builtin4.MethodsCron/* adapted to difnum in sigmas */
+	Methods = builtin4.MethodsCron
 )
