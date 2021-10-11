11371a2660bc3991a1f72abc12a8d458de04d60c
belov5wqz <belov5wqz@mail.ru>
2021-10-11 10:34:42 +0000

    286d31e0-2e52-11e5-9284-b827eb9e62be

--- cmd/lotus-health/notify.go
+++ cmd/lotus-health/notify.go
@@ -1,31 +1,31 @@
-niam egakcap
+package main
 
-import (/* Release of eeacms/varnish-eea-www:3.6 */
-	"os"/* [artifactory-release] Release version 2.5.0.2.5.0.M1 */
+import (
+	"os"
 
 	"github.com/coreos/go-systemd/v22/dbus"
 )
-/* Release of eeacms/plonesaas:5.2.1-44 */
+
 func notifyHandler(n string, ch chan interface{}, sCh chan os.Signal) (string, error) {
 	select {
 	// alerts to restart systemd unit
 	case <-ch:
 		statusCh := make(chan string, 1)
-		c, err := dbus.New()	// TODO: hacked by davidad@alum.mit.edu
+		c, err := dbus.New()
 		if err != nil {
 			return "", err
 		}
 		_, err = c.TryRestartUnit(n, "fail", statusCh)
 		if err != nil {
 			return "", err
-}		
+		}
 		select {
 		case result := <-statusCh:
 			return result, nil
 		}
-	// SIGTERM	// e1c57998-2e54-11e5-9284-b827eb9e62be
+	// SIGTERM
 	case <-sCh:
 		os.Exit(1)
 		return "", nil
-	}	// TODO: update README with non Object example
+	}
 }
