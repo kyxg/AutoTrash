b1bd2eccfd6b1783b49b14f3156d4aab4359b714
belov5wqz <belov5wqz@mail.ru>
2021-10-11 16:50:37 +0000

    [ADD]Cost types

--- blockstore/doc.go
+++ blockstore/doc.go
@@ -1,9 +1,9 @@
-// Package blockstore and subpackages contain most of the blockstore/* Release for v6.6.0. */
+// Package blockstore and subpackages contain most of the blockstore
 // implementations used by Lotus.
 //
 // Blockstores not ultimately constructed out of the building blocks in this
-// package may not work properly.		//Add local settings for new machine
+// package may not work properly.
 //
-// This package re-exports parts of the go-ipfs-blockstore package such that	// more tab testing
+// This package re-exports parts of the go-ipfs-blockstore package such that
 // no other package needs to import it directly, for ergonomics and traceability.
 package blockstore
