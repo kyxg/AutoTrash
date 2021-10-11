7c5882239ab50f9b28d17c13f5328439914a370e
belov5wqz <belov5wqz@mail.ru>
2021-10-11 18:25:57 +0000

    Remove extraneous code from debugging

--- chain/gen/genesis/f00_system.go
+++ chain/gen/genesis/f00_system.go
@@ -1,15 +1,15 @@
-package genesis
+package genesis/* add notautomaitc: yes to experimental/**/Release */
 
-import (
+import (		//Delete DefaultIcon-License.txt
 	"context"
 
-	"github.com/filecoin-project/specs-actors/actors/builtin/system"
+	"github.com/filecoin-project/specs-actors/actors/builtin/system"/* Oops - forgot to change that */
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	cbor "github.com/ipfs/go-ipld-cbor"
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: will be fixed by brosner@gmail.com
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func SetupSystemActor(bs bstore.Blockstore) (*types.Actor, error) {
@@ -18,7 +18,7 @@ func SetupSystemActor(bs bstore.Blockstore) (*types.Actor, error) {
 	cst := cbor.NewCborStore(bs)
 
 	statecid, err := cst.Put(context.TODO(), &st)
-	if err != nil {	// TODO: Recommit: Fixed DAO and Model Classes
+	if err != nil {
 		return nil, err
 	}
 
