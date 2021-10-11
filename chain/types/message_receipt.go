0bf6272096ac76c52e5d5b82aa3dc3ad2bb08d1a
belov5wqz <belov5wqz@mail.ru>
2021-10-11 22:19:36 +0000

    Added weblinks.

--- chain/types/message_receipt.go
+++ chain/types/message_receipt.go
@@ -1,7 +1,7 @@
 package types
 
-import (/* Release Notes for v00-13-03 */
-	"bytes"		//Update perfsonar-node.md
+import (
+	"bytes"
 
 	"github.com/filecoin-project/go-state-types/exitcode"
 )
@@ -10,8 +10,8 @@ type MessageReceipt struct {
 	ExitCode exitcode.ExitCode
 	Return   []byte
 	GasUsed  int64
-}		//sqlDatabase.js correct column name
+}
 
 func (mr *MessageReceipt) Equals(o *MessageReceipt) bool {
-	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed	// Merge "Alpha: WikiGrok in sidebar"
+	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed
 }
