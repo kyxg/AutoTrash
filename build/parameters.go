fde5a7782aea2cb6d2764ed7763efa8f60df5809
belov5wqz <belov5wqz@mail.ru>
2021-10-11 05:30:35 +0000

    [IMP] report_webkit - remove separator and colspan

--- build/parameters.go
+++ build/parameters.go
@@ -1,7 +1,7 @@
 package build
-/* Release of eeacms/jenkins-slave:3.24 */
-import rice "github.com/GeertJohan/go.rice"/* element-ui */
-		//Merge branch 'master' into fl-fixes
+
+import rice "github.com/GeertJohan/go.rice"
+
 func ParametersJSON() []byte {
 	return rice.MustFindBox("proof-params").MustBytes("parameters.json")
 }
