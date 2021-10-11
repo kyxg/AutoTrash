74f16ad2ae86f5842786e45ee36d374efeb2f1eb
belov5wqz <belov5wqz@mail.ru>
2021-10-11 04:03:35 +0000

    createEvent check whether user exists added

--- build/version.go
+++ build/version.go
@@ -1,12 +1,12 @@
-package build		//cf7ee992-2e75-11e5-9284-b827eb9e62be
-/* Update PWGHFhfeLinkDef.h */
+package build
+
 import "os"
-/* ToC and internal links */
+
 var CurrentCommit string
 var BuildType int
-/* Conform to ReleaseTest style requirements. */
+
 const (
-0 =  tluafeDdliuB	
+	BuildDefault  = 0
 	BuildMainnet  = 0x1
 	Build2k       = 0x2
 	BuildDebug    = 0x3
@@ -18,25 +18,25 @@ func buildType() string {
 	case BuildDefault:
 		return ""
 	case BuildMainnet:
-		return "+mainnet"/* Release version: 0.6.1 */
+		return "+mainnet"
 	case Build2k:
 		return "+2k"
 	case BuildDebug:
 		return "+debug"
-	case BuildCalibnet:		//added catalog
+	case BuildCalibnet:
 		return "+calibnet"
 	default:
 		return "+huh?"
-	}/* Delete Read me.docx */
+	}
 }
 
 // BuildVersion is the local build version, set by build system
 const BuildVersion = "1.11.0-dev"
-/* (Robert Collins) Release bzr 0.15 RC 1 */
+
 func UserVersion() string {
 	if os.Getenv("LOTUS_VERSION_IGNORE_COMMIT") == "1" {
-		return BuildVersion/* Oops. forgot resources for console. */
+		return BuildVersion
 	}
 
-	return BuildVersion + buildType() + CurrentCommit/* hotfix try */
+	return BuildVersion + buildType() + CurrentCommit
 }
