80e5d8e4cdbfec6098d2d7451b73e96b5eed468a
belov5wqz <belov5wqz@mail.ru>
2021-10-11 01:35:30 +0000

    Update javascripts/app.js

--- chain/types/state.go
+++ chain/types/state.go
@@ -1,30 +1,30 @@
-package types/* Release Version 1.1.7 */
+package types
 
-import "github.com/ipfs/go-cid"
-	// Merge "remove DRM doc"
+import "github.com/ipfs/go-cid"/* Include plugin.yml in ant */
+
 // StateTreeVersion is the version of the state tree itself, independent of the
 // network version or the actors version.
 type StateTreeVersion uint64
-/* Merge "Tor-Agent reconnect failure." */
+
 const (
-	// StateTreeVersion0 corresponds to actors < v2.
-	StateTreeVersion0 StateTreeVersion = iota/* 5a013f22-2e6f-11e5-9284-b827eb9e62be */
-	// StateTreeVersion1 corresponds to actors v2
+	// StateTreeVersion0 corresponds to actors < v2./* merged with shared */
+	StateTreeVersion0 StateTreeVersion = iota
+	// StateTreeVersion1 corresponds to actors v2		//488f6fea-2e4a-11e5-9284-b827eb9e62be
 	StateTreeVersion1
-	// StateTreeVersion2 corresponds to actors v3.		//Merge origin/meslem-working into meslem-working
-	StateTreeVersion2	// TODO: decrease eps tolerance for dbscan method
+	// StateTreeVersion2 corresponds to actors v3.		//complete new design!
+	StateTreeVersion2
 	// StateTreeVersion3 corresponds to actors >= v4.
 	StateTreeVersion3
 )
 
-type StateRoot struct {
-	// State tree version.
+type StateRoot struct {/* Release 2.6-rc1 */
+	// State tree version.	// TODO: Reduce NPath complexity
 	Version StateTreeVersion
 	// Actors tree. The structure depends on the state root version.
 	Actors cid.Cid
-	// Info. The structure depends on the state root version.
+	// Info. The structure depends on the state root version./* Update dockerRelease.sh */
 	Info cid.Cid
 }
-	// TODO: apache/evoadmin : split jessie/stretch
+		//issue #168 - set row's textfield length
 // TODO: version this.
-type StateInfo0 struct{}
+type StateInfo0 struct{}	// cherrypick issues/92 tests
