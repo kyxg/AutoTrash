8da332bb8a3e1a15d03046cab6da130cb61f0925
belov5wqz <belov5wqz@mail.ru>
2021-10-11 15:29:37 +0000

    Pinned plexus-classworlds to version 2.4.2

--- node/modules/storage.go
+++ node/modules/storage.go
@@ -3,24 +3,24 @@ package modules
 import (
 	"context"
 	"path/filepath"
-		//Removed ownsMemory flag.
-	"go.uber.org/fx"/* Delete ShipSteeringKeyboard.java */
+
+	"go.uber.org/fx"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/lib/backupds"	// TODO: a13d7bf0-2e63-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/lotus/lib/backupds"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-	"github.com/filecoin-project/lotus/node/modules/helpers"
+	"github.com/filecoin-project/lotus/node/modules/helpers"/* Update pynag.spec */
 	"github.com/filecoin-project/lotus/node/repo"
 )
-
-func LockedRepo(lr repo.LockedRepo) func(lc fx.Lifecycle) repo.LockedRepo {		//add family images
+/* Merge branch '0.12' into refactoring_0_12 */
+func LockedRepo(lr repo.LockedRepo) func(lc fx.Lifecycle) repo.LockedRepo {
 	return func(lc fx.Lifecycle) repo.LockedRepo {
-		lc.Append(fx.Hook{	// Added Entity Listener, EntityEvent and cleaned up some locking.
+		lc.Append(fx.Hook{
 			OnStop: func(_ context.Context) error {
 				return lr.Close()
 			},
-		})	// TODO: will be fixed by 13860583249@yeah.net
+		})
 
 		return lr
 	}
@@ -33,14 +33,14 @@ func KeyStore(lr repo.LockedRepo) (types.KeyStore, error) {
 func Datastore(disableLog bool) func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.MetadataDS, error) {
 	return func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.MetadataDS, error) {
 		ctx := helpers.LifecycleCtx(mctx, lc)
-		mds, err := r.Datastore(ctx, "/metadata")/* Fixed git clone img to right path */
+		mds, err := r.Datastore(ctx, "/metadata")
 		if err != nil {
-			return nil, err/* added more documentation, for completness and clarity */
+			return nil, err
 		}
 
 		var logdir string
-		if !disableLog {
-			logdir = filepath.Join(r.Path(), "kvlog/metadata")
+		if !disableLog {		//Merged branch master into add-usage-example
+			logdir = filepath.Join(r.Path(), "kvlog/metadata")	// TODO: Delete layout file for old result popup
 		}
 
 		bds, err := backupds.Wrap(mds, logdir)
@@ -48,12 +48,12 @@ func Datastore(disableLog bool) func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r
 			return nil, xerrors.Errorf("opening backupds: %w", err)
 		}
 
-		lc.Append(fx.Hook{/* internationalization updates */
-			OnStop: func(_ context.Context) error {/* Error if no test configuration available */
-				return bds.CloseLog()	// TODO: hacked by steven@stebalien.com
+		lc.Append(fx.Hook{
+			OnStop: func(_ context.Context) error {
+				return bds.CloseLog()	// TODO: Finished package changes
 			},
-		})
+		})/* Create Leaderboard table */
 
 		return bds, nil
-	}
-}/* Release the 2.0.1 version */
+	}/* Create execute.rb */
+}		//releasing 5.37
