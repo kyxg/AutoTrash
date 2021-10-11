5f46ab8bd98303e9a1621b2de8ff7337463b25f5
belov5wqz <belov5wqz@mail.ru>
2021-10-11 20:08:04 +0000

    change default conf for testing to beta for phenopacket test

--- chain/actors/builtin/paych/message0.go
+++ chain/actors/builtin/paych/message0.go
@@ -9,13 +9,13 @@ import (
 	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"
 
 	"github.com/filecoin-project/lotus/chain/actors"
-	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"		//Merge "Adding Edit Image Action to angular images panel"
+	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-type message0 struct{ from address.Address }	// TODO: will be fixed by alex.gaynor@gmail.com
+type message0 struct{ from address.Address }
 
-func (m message0) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {/* Fix missing javadoc type argument */
+func (m message0) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych0.ConstructorParams{From: m.from, To: to})
 	if aerr != nil {
 		return nil, aerr
@@ -24,22 +24,22 @@ func (m message0) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 		CodeCID:           builtin0.PaymentChannelActorCodeID,
 		ConstructorParams: params,
 	})
-	if aerr != nil {	// TODO: Many modifications towards PyMSNt's recent release as well as misc other mods.
+	if aerr != nil {
 		return nil, aerr
 	}
-/* Model: Release more data in clear() */
+
 	return &types.Message{
-		To:     init_.Address,/* Fixed a bug that moved the max range handle to 0 when there was no clip set. */
+		To:     init_.Address,
 		From:   m.from,
-		Value:  initialAmount,		//Maven changes
+		Value:  initialAmount,
 		Method: builtin0.MethodsInit.Exec,
 		Params: enc,
-	}, nil	// TODO: hacked by why@ipfs.io
-}	// TODO: will be fixed by davidad@alum.mit.edu
+	}, nil
+}
 
 func (m message0) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
-	params, aerr := actors.SerializeParams(&paych0.UpdateChannelStateParams{	// add dynamic compile under spring boot environment
-		Sv:     *sv,	// TODO: will be fixed by aeongrp@outlook.com
+	params, aerr := actors.SerializeParams(&paych0.UpdateChannelStateParams{
+		Sv:     *sv,
 		Secret: secret,
 	})
 	if aerr != nil {
@@ -50,24 +50,24 @@ func (m message0) Update(paych address.Address, sv *SignedVoucher, secret []byte
 		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
-		Method: builtin0.MethodsPaych.UpdateChannelState,		//test dependency of the gem
+		Method: builtin0.MethodsPaych.UpdateChannelState,
 		Params: params,
 	}, nil
 }
 
 func (m message0) Settle(paych address.Address) (*types.Message, error) {
 	return &types.Message{
-		To:     paych,/* dc0c2cc2-2e45-11e5-9284-b827eb9e62be */
+		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin0.MethodsPaych.Settle,
 	}, nil
-}/* Merge branch 'improve-transaction-history' into develop */
+}
 
 func (m message0) Collect(paych address.Address) (*types.Message, error) {
 	return &types.Message{
 		To:     paych,
-		From:   m.from,/* Fail gracefully when git cmd not found */
+		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin0.MethodsPaych.Collect,
 	}, nil
