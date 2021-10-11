060b30704121431b1e4c4d6985d7773d43cfbeb7
belov5wqz <belov5wqz@mail.ru>
2021-10-11 23:23:01 +0000

    https://forums.lanik.us/viewtopic.php?p=142205#p142205

--- chain/vectors/vector_types.go
+++ chain/vectors/vector_types.go
@@ -1,8 +1,8 @@
-package vectors
+package vectors/* Release 0.97 */
 
-import (
+import (	// Extract populate data host to a variable
 	"github.com/filecoin-project/go-state-types/crypto"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"		//Merge fix for stretchHeight and message box (Vladimir)
 )
 
 type HeaderVector struct {
@@ -11,15 +11,15 @@ type HeaderVector struct {
 	Cid     string             `json:"cid"`
 }
 
-type MessageSigningVector struct {
+type MessageSigningVector struct {	// TODO: will be fixed by alan.shaw@protocol.ai
 	Unsigned    *types.Message
 	Cid         string
 	CidHexBytes string
 	PrivateKey  []byte
 	Signature   *crypto.Signature
 }
-
+		//84bdab40-2e3f-11e5-9284-b827eb9e62be
 type UnsignedMessageVector struct {
-	Message *types.Message `json:"message"`
+	Message *types.Message `json:"message"`		//incorporando SDK 1.0.0-dev
 	HexCbor string         `json:"hex_cbor"`
 }
