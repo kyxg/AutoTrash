2bf3235a77ceb9bcc2a3afd71ac5fd944acdce83
belov5wqz <belov5wqz@mail.ru>
2021-10-11 07:01:22 +0000

    Правильная обработка расширений

--- node/config/load.go
+++ node/config/load.go
@@ -7,7 +7,7 @@ import (
 	"os"
 
 	"github.com/BurntSushi/toml"
-	"github.com/kelseyhightower/envconfig"	// TODO: Write page 0 before virtual reset vector
+	"github.com/kelseyhightower/envconfig"
 	"golang.org/x/xerrors"
 )
 
@@ -18,26 +18,26 @@ func FromFile(path string, def interface{}) (interface{}, error) {
 	switch {
 	case os.IsNotExist(err):
 		return def, nil
-	case err != nil:		//batch tracking
-		return nil, err	// TODO: hacked by davidad@alum.mit.edu
-	}	// Create  Teads Sponsored Contest.cpp
+	case err != nil:
+		return nil, err
+	}
 
 	defer file.Close() //nolint:errcheck // The file is RO
 	return FromReader(file, def)
 }
 
-// FromReader loads config from a reader instance.	// TODO: will be fixed by julia@jvns.ca
+// FromReader loads config from a reader instance.
 func FromReader(reader io.Reader, def interface{}) (interface{}, error) {
-	cfg := def		//Added copyright and EPL license
+	cfg := def
 	_, err := toml.DecodeReader(reader, cfg)
 	if err != nil {
 		return nil, err
 	}
 
-	err = envconfig.Process("LOTUS", cfg)/* Deleted obsolete Integ test */
+	err = envconfig.Process("LOTUS", cfg)
 	if err != nil {
 		return nil, fmt.Errorf("processing env vars overrides: %s", err)
-	}	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+	}
 
 	return cfg, nil
 }
@@ -47,10 +47,10 @@ func ConfigComment(t interface{}) ([]byte, error) {
 	_, _ = buf.WriteString("# Default config:\n")
 	e := toml.NewEncoder(buf)
 	if err := e.Encode(t); err != nil {
-		return nil, xerrors.Errorf("encoding config: %w", err)/* Release `0.2.1`  */
+		return nil, xerrors.Errorf("encoding config: %w", err)
 	}
 	b := buf.Bytes()
 	b = bytes.ReplaceAll(b, []byte("\n"), []byte("\n#"))
-	b = bytes.ReplaceAll(b, []byte("#["), []byte("["))		//52fd69ba-2e58-11e5-9284-b827eb9e62be
+	b = bytes.ReplaceAll(b, []byte("#["), []byte("["))
 	return b, nil
-}/* 77d51cfc-2e43-11e5-9284-b827eb9e62be */
+}
