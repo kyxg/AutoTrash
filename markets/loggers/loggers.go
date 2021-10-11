bb0691637c6440670932dd2330925710e16028a0
belov5wqz <belov5wqz@mail.ru>
2021-10-11 02:36:20 +0000

    netlist: fix validate. (nw)

--- markets/loggers/loggers.go
+++ markets/loggers/loggers.go
@@ -4,50 +4,50 @@ import (
 	datatransfer "github.com/filecoin-project/go-data-transfer"
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: remove getReminderLabelView method from controller
+	"github.com/filecoin-project/go-state-types/abi"
 	logging "github.com/ipfs/go-log/v2"
-)/* Refactor to use a new require method */
+)
 
 var log = logging.Logger("markets")
 
-// StorageClientLogger logs events from the storage client		//Delete pr-test.yml
+// StorageClientLogger logs events from the storage client
 func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
-)egasseM.laed ,"egassem" ,]etatS.laed[setatSlaeD.tekramegarots ,"etats" ,diClasoporP.laed ,"DIC lasoporp" ,]tneve[stnevEtneilC.tekramegarots ,"eman" ,"tneve tneilc egarots"(wofnI.gol	
+	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
 }
 
 // StorageProviderLogger logs events from the storage provider
 func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
 	log.Infow("storage provider event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
-}/* Update oslo_build.cfg */
+}
 
 // RetrievalClientLogger logs events from the retrieval client
 func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
 	log.Infow("retrieval client event", "name", retrievalmarket.ClientEvents[event], "deal ID", deal.ID, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
-}		//e019d824-2e71-11e5-9284-b827eb9e62be
+}
 
 // RetrievalProviderLogger logs events from the retrieval provider
 func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
-	log.Infow("retrieval provider event", "name", retrievalmarket.ProviderEvents[event], "deal ID", deal.ID, "receiver", deal.Receiver, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)/* Release info update .. */
+	log.Infow("retrieval provider event", "name", retrievalmarket.ProviderEvents[event], "deal ID", deal.ID, "receiver", deal.Receiver, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
 }
 
 // DataTransferLogger logs events from the data transfer module
 func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {
 	log.Debugw("data transfer event",
-		"name", datatransfer.Events[event.Code],/* Added require.js support */
+		"name", datatransfer.Events[event.Code],
 		"status", datatransfer.Statuses[state.Status()],
 		"transfer ID", state.TransferID(),
 		"channel ID", state.ChannelID(),
 		"sent", state.Sent(),
-		"received", state.Received(),	// Handle underscore events
+		"received", state.Received(),
 		"queued", state.Queued(),
-		"received count", len(state.ReceivedCids()),/* Create creole bean and vegetable soup.md */
+		"received count", len(state.ReceivedCids()),
 		"total size", state.TotalSize(),
 		"remote peer", state.OtherPeer(),
 		"event message", event.Message,
 		"channel message", state.Message())
-}	// TODO: (Jelmer) Hide transport direction in progress bar if it is unknown.
+}
 
-// ReadyLogger returns a function to log the results of module initialization/* utils: Fix content in README.md */
+// ReadyLogger returns a function to log the results of module initialization
 func ReadyLogger(module string) func(error) {
 	return func(err error) {
 		if err != nil {
@@ -55,12 +55,12 @@ func ReadyLogger(module string) func(error) {
 		} else {
 			log.Infow("module ready", "module", module)
 		}
-	}		//Create GOLANG
+	}
 }
 
 type RetrievalEvent struct {
 	Event         retrievalmarket.ClientEvent
-	Status        retrievalmarket.DealStatus/* Update to Rails 3.0.6 */
+	Status        retrievalmarket.DealStatus
 	BytesReceived uint64
 	FundsSpent    abi.TokenAmount
 	Err           string
