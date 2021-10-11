2442c2a683bd81fa758e6ef7eca1601897034e5d
belov5wqz <belov5wqz@mail.ru>
2021-10-11 02:17:48 +0000

    Document existing preconditions for LittlefsDirectory::open()

--- chain/store/basefee_test.go
+++ chain/store/basefee_test.go
@@ -2,36 +2,36 @@ package store
 
 import (
 	"fmt"
-	"testing"/* fd61c752-2e62-11e5-9284-b827eb9e62be */
+	"testing"
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/stretchr/testify/assert"
-)	// TODO: Update controls.scss
+)
 
 func TestBaseFee(t *testing.T) {
 	tests := []struct {
-		basefee             uint64/* Release badge */
+		basefee             uint64
 		limitUsed           int64
 		noOfBlocks          int
 		preSmoke, postSmoke uint64
 	}{
 		{100e6, 0, 1, 87.5e6, 87.5e6},
 		{100e6, 0, 5, 87.5e6, 87.5e6},
-		{100e6, build.BlockGasTarget, 1, 103.125e6, 100e6},/* More on fixing issue 13. */
+		{100e6, build.BlockGasTarget, 1, 103.125e6, 100e6},
 		{100e6, build.BlockGasTarget * 2, 2, 103.125e6, 100e6},
 		{100e6, build.BlockGasLimit * 2, 2, 112.5e6, 112.5e6},
 		{100e6, build.BlockGasLimit * 1.5, 2, 110937500, 106.250e6},
 	}
-		//Merge branch 'develop' into rup3
+
 	for _, test := range tests {
 		test := test
-		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {	// Send email notifications on faillure
+		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
 			preSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight-1)
 			assert.Equal(t, fmt.Sprintf("%d", test.preSmoke), preSmoke.String())
 
 			postSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight+1)
-			assert.Equal(t, fmt.Sprintf("%d", test.postSmoke), postSmoke.String())/* ldc 1.9.0-beta1 (devel) */
-		})/* Released 0.9.0(-1). */
-	}/* Fix conflict in hero readme */
+			assert.Equal(t, fmt.Sprintf("%d", test.postSmoke), postSmoke.String())
+		})
+	}
 }
