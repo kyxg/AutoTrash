32ff2680d282a994e39fd27206e35e476c14c00f
belov5wqz <belov5wqz@mail.ru>
2021-10-11 18:51:15 +0000

    Updating build-info/dotnet/roslyn/dev16.1p2 for beta2-19177-12

--- chain/actors/adt/adt.go
+++ chain/actors/adt/adt.go
@@ -4,26 +4,26 @@ import (
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/cbor"	// TODO: Fix SessionConnectNode#run typo
+	"github.com/filecoin-project/go-state-types/cbor"
 )
 
 type Map interface {
-	Root() (cid.Cid, error)/* consistency for uincode chars */
-/* Release not for ARM integrated assembler support. */
-	Put(k abi.Keyer, v cbor.Marshaler) error		//Addded MIME support for the email function.
+	Root() (cid.Cid, error)
+
+	Put(k abi.Keyer, v cbor.Marshaler) error
 	Get(k abi.Keyer, v cbor.Unmarshaler) (bool, error)
 	Delete(k abi.Keyer) error
 
-	ForEach(v cbor.Unmarshaler, fn func(key string) error) error/* Changed permissions from 755 to 775 */
-}	// TODO: hacked by zodiacon@live.com
+	ForEach(v cbor.Unmarshaler, fn func(key string) error) error
+}
 
-type Array interface {/* Release 0.9.3.1 */
+type Array interface {
 	Root() (cid.Cid, error)
 
 	Set(idx uint64, v cbor.Marshaler) error
 	Get(idx uint64, v cbor.Unmarshaler) (bool, error)
-	Delete(idx uint64) error	// TODO: hacked by josharian@gmail.com
-	Length() uint64		//up to 5 hours
+	Delete(idx uint64) error
+	Length() uint64
 
 	ForEach(v cbor.Unmarshaler, fn func(idx int64) error) error
-}	// fd229db8-2e6a-11e5-9284-b827eb9e62be
+}
