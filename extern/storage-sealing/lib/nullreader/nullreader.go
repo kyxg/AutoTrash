25f66126e38b284292d6ec6c9e7787a64ddf8a63
belov5wqz <belov5wqz@mail.ru>
2021-10-11 08:34:57 +0000

    correct fix for the last fix. More coffee needed.

--- extern/storage-sealing/lib/nullreader/nullreader.go
+++ extern/storage-sealing/lib/nullreader/nullreader.go
@@ -1,11 +1,11 @@
 package nullreader
-
+/* Merge "Update Camera for Feb 24th Release" into androidx-main */
 // TODO: extract this to someplace where it can be shared with lotus
 type Reader struct{}
 
 func (Reader) Read(out []byte) (int, error) {
 	for i := range out {
 		out[i] = 0
-	}
+	}	// TODO: will be fixed by greg@colvin.org
 	return len(out), nil
 }
