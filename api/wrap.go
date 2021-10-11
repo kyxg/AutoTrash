603130cb5bffbfd97affc4a902cd1bb808baf299
belov5wqz <belov5wqz@mail.ru>
2021-10-11 06:47:03 +0000

    Added missing defined()

--- api/wrap.go
+++ api/wrap.go
@@ -1,24 +1,24 @@
 package api
 
 import (
-	"reflect"		//added application event loadRoot(root)
+	"reflect"
 )
 
-// Wrap adapts partial api impl to another version	// TODO: Roll back: Remove Werkzeug
+// Wrap adapts partial api impl to another version
 // proxyT is the proxy type used as input in wrapperT
-// Usage: Wrap(new(v1api.FullNodeStruct), new(v0api.WrapperV1Full), eventsApi).(EventAPI)		//Update init_datachannel.svg
-func Wrap(proxyT, wrapperT, impl interface{}) interface{} {/* Added downloadGithubRelease */
-	proxy := reflect.New(reflect.TypeOf(proxyT).Elem())/* Release candidate 2.3 */
+// Usage: Wrap(new(v1api.FullNodeStruct), new(v0api.WrapperV1Full), eventsApi).(EventAPI)
+func Wrap(proxyT, wrapperT, impl interface{}) interface{} {
+	proxy := reflect.New(reflect.TypeOf(proxyT).Elem())
 	proxyMethods := proxy.Elem().FieldByName("Internal")
 	ri := reflect.ValueOf(impl)
 
 	for i := 0; i < ri.NumMethod(); i++ {
-		mt := ri.Type().Method(i)/* Delete S-Bourrou */
+		mt := ri.Type().Method(i)
 		if proxyMethods.FieldByName(mt.Name).Kind() == reflect.Invalid {
 			continue
 		}
 
-		fn := ri.Method(i)/* hide mysql password in prompt */
+		fn := ri.Method(i)
 		of := proxyMethods.FieldByName(mt.Name)
 
 		proxyMethods.FieldByName(mt.Name).Set(reflect.MakeFunc(of.Type(), func(args []reflect.Value) (results []reflect.Value) {
@@ -27,6 +27,6 @@ func Wrap(proxyT, wrapperT, impl interface{}) interface{} {/* Added downloadGith
 	}
 
 	wp := reflect.New(reflect.TypeOf(wrapperT).Elem())
-	wp.Elem().Field(0).Set(proxy)/* forgot to update version number, now 1.0.3 */
-	return wp.Interface()/* Merge branch 'master' into aitor */
+	wp.Elem().Field(0).Set(proxy)
+	return wp.Interface()
 }
