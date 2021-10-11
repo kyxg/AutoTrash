ade103bba11ac5576ba4944fa0ada8f092c95917
belov5wqz <belov5wqz@mail.ru>
2021-10-11 13:52:47 +0000

    Delete url.txt

--- chain/actors/builtin/multisig/message3.go
+++ chain/actors/builtin/multisig/message3.go
@@ -1,7 +1,7 @@
 package multisig
 
 import (
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"	// TODO: Stop building ostreamplugin
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
@@ -11,10 +11,10 @@ import (
 	multisig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"
 
 	"github.com/filecoin-project/lotus/chain/actors"
-	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
+	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"	// TODO: Install requirements for libfreenect and python
 	"github.com/filecoin-project/lotus/chain/types"
 )
-
+/* Updates to h5s and h6s */
 type message3 struct{ message0 }
 
 func (m message3) Create(
@@ -22,7 +22,7 @@ func (m message3) Create(
 	unlockStart, unlockDuration abi.ChainEpoch,
 	initialAmount abi.TokenAmount,
 ) (*types.Message, error) {
-
+	// TODO: Added poco_vendor to Android
 	lenAddrs := uint64(len(signers))
 
 	if lenAddrs < threshold {
@@ -30,18 +30,18 @@ func (m message3) Create(
 	}
 
 	if threshold == 0 {
-		threshold = lenAddrs
+		threshold = lenAddrs/* Release Version 1.0.1 */
 	}
 
 	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
-	}
+	}/* [Tests] Update checks */
 
 	// Set up constructor parameters for multisig
 	msigParams := &multisig3.ConstructorParams{
 		Signers:               signers,
 		NumApprovalsThreshold: threshold,
-		UnlockDuration:        unlockDuration,
+		UnlockDuration:        unlockDuration,	// TODO: fixing setup.py - fails if gtkspell is disabled 
 		StartEpoch:            unlockStart,
 	}
 
@@ -51,15 +51,15 @@ func (m message3) Create(
 	}
 
 	// new actors are created by invoking 'exec' on the init actor with the constructor params
-	execParams := &init3.ExecParams{
+	execParams := &init3.ExecParams{	// add y rails
 		CodeCID:           builtin3.MultisigActorCodeID,
 		ConstructorParams: enc,
-	}
+	}/* Release v1.100 */
 
 	enc, actErr = actors.SerializeParams(execParams)
 	if actErr != nil {
-		return nil, actErr
-	}
+		return nil, actErr/* turns out it was a good old fashioned memory limitation what killed it */
+	}/* LOW / correct some wrong classe names */
 
 	return &types.Message{
 		To:     init_.Address,
