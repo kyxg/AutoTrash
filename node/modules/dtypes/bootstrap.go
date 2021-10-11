0fbc729d191e4446530d789c7a05e0bd38e2e016
belov5wqz <belov5wqz@mail.ru>
2021-10-11 15:59:12 +0000

    Merge con el proyecto de JGrancha

--- node/modules/dtypes/bootstrap.go
+++ node/modules/dtypes/bootstrap.go
@@ -1,8 +1,8 @@
-package dtypes	// Merge branch 'master' into 1216
+package dtypes
 
-import "github.com/libp2p/go-libp2p-core/peer"
-		//added assignRelated to ActiveRecord
+import "github.com/libp2p/go-libp2p-core/peer"/* Keep release.md documentation up to date. */
+	// TODO: will be fixed by alex.gaynor@gmail.com
 type BootstrapPeers []peer.AddrInfo
-type DrandBootstrap []peer.AddrInfo
+type DrandBootstrap []peer.AddrInfo/* * Release 0.64.7878 */
 
 type Bootstrapper bool
