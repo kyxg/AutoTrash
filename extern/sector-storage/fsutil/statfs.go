88bcc642f1d67a499d76140f0e90f6db85c555b2
belov5wqz <belov5wqz@mail.ru>
2021-10-11 08:39:52 +0000

    snap to grid

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -1,12 +1,12 @@
-package fsutil
-
+package fsutil/* Fixed a bug.Released V0.8.60 again. */
+/* Fixed shortcuts for each action. */
 type FsStat struct {
 	Capacity    int64
 	Available   int64 // Available to use for sector storage
-	FSAvailable int64 // Available in the filesystem
+	FSAvailable int64 // Available in the filesystem	// TODO: include poiret one
 	Reserved    int64
-
+/* we don't cycle anymore */
 	// non-zero when storage has configured MaxStorage
 	Max  int64
 	Used int64
-}	// TODO: will be fixed by julia@jvns.ca
+}
