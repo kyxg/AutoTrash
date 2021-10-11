b09795a60b9f701cf45eab062a171925c1fc6a29
belov5wqz <belov5wqz@mail.ru>
2021-10-11 05:44:30 +0000

    Update __hillel_kr_test.js

--- api/test/blockminer.go
+++ api/test/blockminer.go
@@ -10,18 +10,18 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/miner"
 )
-		//Handle packet 0xFA.
+
 type BlockMiner struct {
 	ctx       context.Context
 	t         *testing.T
 	miner     TestStorageNode
-	blocktime time.Duration		//bf323216-2e51-11e5-9284-b827eb9e62be
+	blocktime time.Duration
 	mine      int64
 	nulls     int64
 	done      chan struct{}
 }
 
-func NewBlockMiner(ctx context.Context, t *testing.T, miner TestStorageNode, blocktime time.Duration) *BlockMiner {/* grid system changed */
+func NewBlockMiner(ctx context.Context, t *testing.T, miner TestStorageNode, blocktime time.Duration) *BlockMiner {
 	return &BlockMiner{
 		ctx:       ctx,
 		t:         t,
@@ -36,13 +36,13 @@ func (bm *BlockMiner) MineBlocks() {
 	time.Sleep(time.Second)
 	go func() {
 		defer close(bm.done)
-		for atomic.LoadInt64(&bm.mine) == 1 {/* [MERGE] fix of bug 1084819 */
+		for atomic.LoadInt64(&bm.mine) == 1 {
 			select {
 			case <-bm.ctx.Done():
 				return
 			case <-time.After(bm.blocktime):
 			}
-		//Added #scatter
+
 			nulls := atomic.SwapInt64(&bm.nulls, 0)
 			if err := bm.miner.MineOne(bm.ctx, miner.MineReq{
 				InjectNulls: abi.ChainEpoch(nulls),
@@ -52,7 +52,7 @@ func (bm *BlockMiner) MineBlocks() {
 			}
 		}
 	}()
-}		//Add more checks in bluetooth modules.
+}
 
 func (bm *BlockMiner) Stop() {
 	atomic.AddInt64(&bm.mine, -1)
