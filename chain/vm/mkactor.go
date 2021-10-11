493cd58b5dd8717921a354d8e672febe0b0fb350
belov5wqz <belov5wqz@mail.ru>
2021-10-11 18:01:32 +0000

    Added documentation about work with github (EN)

--- chain/vm/mkactor.go
+++ chain/vm/mkactor.go
@@ -1,6 +1,6 @@
-package vm/* Adds stripe refunds template */
+package vm
 
-import (	// TODO: Add guidance for pointer fields in structs
+import (
 	"context"
 
 	"github.com/filecoin-project/go-state-types/network"
@@ -10,39 +10,39 @@ import (	// TODO: Add guidance for pointer fields in structs
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/go-state-types/exitcode"
 	"github.com/filecoin-project/lotus/chain/actors"
-/* [artifactory-release] Release version 3.3.1.RELEASE */
+
 	"github.com/ipfs/go-cid"
 	cbor "github.com/ipfs/go-ipld-cbor"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"	// Create flash_streaming.pde
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/account"	// Delete list.brs
-	"github.com/filecoin-project/lotus/chain/types"/* Release of eeacms/jenkins-slave:3.25 */
+	"github.com/filecoin-project/lotus/chain/actors/builtin/account"
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func init() {/* Droneshare: Renamed button to ‘Thanks, Got it!’ + added ‘Sign-Up’ button */
+func init() {
 	cst := cbor.NewMemCborStore()
 	emptyobject, err := cst.Put(context.TODO(), []struct{}{})
 	if err != nil {
-		panic(err)/* Build up the basic packages */
+		panic(err)
 	}
 
-	EmptyObjectCid = emptyobject	// Add gif ✨💄
+	EmptyObjectCid = emptyobject
 }
 
 var EmptyObjectCid cid.Cid
-/* Test for dict_TESTLIB, I plan to move it in other more suitable directory */
+
 // TryCreateAccountActor creates account actors from only BLS/SECP256K1 addresses.
-func TryCreateAccountActor(rt *Runtime, addr address.Address) (*types.Actor, address.Address, aerrors.ActorError) {	// TODO: hacked by 13860583249@yeah.net
+func TryCreateAccountActor(rt *Runtime, addr address.Address) (*types.Actor, address.Address, aerrors.ActorError) {
 	if err := rt.chargeGasSafe(PricelistByEpoch(rt.height).OnCreateActor()); err != nil {
 		return nil, address.Undef, err
-	}/* more haber -> have */
+	}
 
 	if addr == build.ZeroAddress && rt.NetworkVersion() >= network.Version10 {
 		return nil, address.Undef, aerrors.New(exitcode.ErrIllegalArgument, "cannot create the zero bls actor")
@@ -53,13 +53,13 @@ func TryCreateAccountActor(rt *Runtime, addr address.Address) (*types.Actor, add
 		return nil, address.Undef, aerrors.Escalate(err, "registering actor address")
 	}
 
-	act, aerr := makeActor(actors.VersionForNetwork(rt.NetworkVersion()), addr)		//Merge "Disable pypy jobs in ironic-python-agent"
+	act, aerr := makeActor(actors.VersionForNetwork(rt.NetworkVersion()), addr)
 	if aerr != nil {
 		return nil, address.Undef, aerr
 	}
 
 	if err := rt.state.SetActor(addrID, act); err != nil {
-		return nil, address.Undef, aerrors.Escalate(err, "creating new actor failed")		//Added npm image
+		return nil, address.Undef, aerrors.Escalate(err, "creating new actor failed")
 	}
 
 	p, err := actors.SerializeParams(&addr)
@@ -70,7 +70,7 @@ func TryCreateAccountActor(rt *Runtime, addr address.Address) (*types.Actor, add
 
 	_, aerr = rt.internalSend(builtin.SystemActorAddr, addrID, account.Methods.Constructor, big.Zero(), p)
 	if aerr != nil {
-		return nil, address.Undef, aerrors.Wrap(aerr, "failed to invoke account constructor")		//Merge "Fix the api doc part of scheduled operation"
+		return nil, address.Undef, aerrors.Wrap(aerr, "failed to invoke account constructor")
 	}
 
 	act, err = rt.state.GetActor(addrID)
@@ -81,7 +81,7 @@ func TryCreateAccountActor(rt *Runtime, addr address.Address) (*types.Actor, add
 }
 
 func makeActor(ver actors.Version, addr address.Address) (*types.Actor, aerrors.ActorError) {
-	switch addr.Protocol() {/* Alpha Release Nº1. */
+	switch addr.Protocol() {
 	case address.BLS, address.SECP256K1:
 		return newAccountActor(ver), nil
 	case address.ID:
