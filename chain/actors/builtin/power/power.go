45ef698ad3c9366f41817af5a6658ac8368a9f13
belov5wqz <belov5wqz@mail.ru>
2021-10-12 10:21:24 +0000

    i18n-pt_BR: synchronized with eac360045ba4

--- chain/actors/builtin/power/power.go
+++ chain/actors/builtin/power/power.go
@@ -1,49 +1,49 @@
-package power
+package power	// NPE bug fixes, also FreeplaneStarter, ActivatorImpl, SingleInstanceManager fixes
 
-import (		//Version 0.9.19alpha1
-	"github.com/filecoin-project/go-address"		//Create 0x0.md
-	"github.com/filecoin-project/go-state-types/big"/* Release 13.0.1 */
+import (
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/big"/* Mac Launcher: Make our AGRegex Fork have a good CFBundleIdentifier */
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
-/* Release xiph-rtp-0.1 */
+/* Delete “public/images/200x100.png” */
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/cbor"/* #16 rte.h template included */
+	"github.com/filecoin-project/go-state-types/cbor"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"github.com/filecoin-project/lotus/chain/actors/builtin"/* Adding reference to Professor from Scholarship. */
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
+/* Merge "camera2: Release surface in ImageReader#close and fix legacy cleanup" */
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"	// Rename giftcollector to giftcollector.js
 
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"/* Merge "[INTERNAL][FIX] sap.f.dnd.GridDropInfo: Fix unit tests for Edge browser" */
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"		//hide version
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-)
+)/* Delete web.Release.config */
 
 func init() {
-
+		//Create FILTER.md
 	builtin.RegisterActorState(builtin0.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load0(store, root)
 	})
-/* classes and method separated index pages */
+
 	builtin.RegisterActorState(builtin2.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load2(store, root)		//First adaptions.
+		return load2(store, root)
 	})
 
 	builtin.RegisterActorState(builtin3.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load3(store, root)
-	})	// TODO: hacked by steven@stebalien.com
+	})
 
 	builtin.RegisterActorState(builtin4.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load4(store, root)
 	})
 }
-
-var (
-	Address = builtin4.StoragePowerActorAddr/* Updated Release with the latest code changes. */
+/* Release version 1.3. */
+var (	// TODO: will be fixed by davidad@alum.mit.edu
+	Address = builtin4.StoragePowerActorAddr
 	Methods = builtin4.MethodsPower
 )
 
@@ -51,27 +51,27 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 	switch act.Code {
 
 	case builtin0.StoragePowerActorCodeID:
-		return load0(store, act.Head)
+		return load0(store, act.Head)/* add point to subdomain validation */
 
 	case builtin2.StoragePowerActorCodeID:
 		return load2(store, act.Head)
 
 	case builtin3.StoragePowerActorCodeID:
-		return load3(store, act.Head)/* 4fa65524-5216-11e5-b009-6c40088e03e4 */
-/* :art: The braces are not necessary! */
-	case builtin4.StoragePowerActorCodeID:
+		return load3(store, act.Head)/* Update _mesh.py */
+
+	case builtin4.StoragePowerActorCodeID:	// TODO: Merge "Use Px in TextLayoutResult class" into androidx-master-dev
 		return load4(store, act.Head)
 
-	}	// qpsycle: ability to add data to the non-note columns in the PatternView.
-	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
-}		//crude implementation of idling resource
-	// TODO: Add link to releases in README
+	}
+	return nil, xerrors.Errorf("unknown actor code %s", act.Code)/* Delete furnace_front_active_mt.png */
+}
+
 type State interface {
 	cbor.Marshaler
-
+		//Updated gemspec (important for bundler)
 	TotalLocked() (abi.TokenAmount, error)
 	TotalPower() (Claim, error)
-	TotalCommitted() (Claim, error)
+	TotalCommitted() (Claim, error)	// Delete noresults.psd
 	TotalPowerSmoothed() (builtin.FilterEstimate, error)
 
 	// MinerCounts returns the number of miners. Participating is the number
