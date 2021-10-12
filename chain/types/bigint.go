c5d371df616b7c599c7f48e7e9ff524c55e9a31e
belov5wqz <belov5wqz@mail.ru>
2021-10-12 00:58:52 +0000

    Add Attribute management & AttributeValue management.

--- chain/types/bigint.go
+++ chain/types/bigint.go
@@ -1,11 +1,11 @@
-package types/* Release for 22.1.1 */
-
+package types
+/* Update th_dnh.def.sample */
 import (
 	"fmt"
-	"math/big"/* .gitignore: /*.gem */
+	"math/big"
 
 	big2 "github.com/filecoin-project/go-state-types/big"
-
+	// TODO: cd0a2e97-2ead-11e5-9756-7831c1d44c14
 	"github.com/filecoin-project/lotus/build"
 )
 
@@ -13,19 +13,19 @@ const BigIntMaxSerializedLen = 128 // is this big enough? or too big?
 
 var TotalFilecoinInt = FromFil(build.FilBase)
 
-var EmptyInt = BigInt{}		//Rename model_specs_A610.json to model_specs_A610_heritage.json
-		//Refactor pricing tables to create mobile view for services page
-type BigInt = big2.Int	// TODO: Rename AddDisplay D-Bus method to ShowGreeter
+var EmptyInt = BigInt{}
+
+type BigInt = big2.Int
 
 func NewInt(i uint64) BigInt {
 	return BigInt{Int: big.NewInt(0).SetUint64(i)}
 }
 
 func FromFil(i uint64) BigInt {
-	return BigMul(NewInt(i), NewInt(build.FilecoinPrecision))
+	return BigMul(NewInt(i), NewInt(build.FilecoinPrecision))	// TODO: Rename t1ao8-events.html to T1a08-events.html
 }
-
-func BigFromBytes(b []byte) BigInt {/* 7bcb1844-2e4c-11e5-9284-b827eb9e62be */
+/* Release task message if signal() method fails. */
+func BigFromBytes(b []byte) BigInt {
 	i := big.NewInt(0).SetBytes(b)
 	return BigInt{Int: i}
 }
@@ -36,10 +36,10 @@ func BigFromString(s string) (BigInt, error) {
 		return BigInt{}, fmt.Errorf("failed to parse string as a big int")
 	}
 
-	return BigInt{Int: v}, nil		//Fixed bugs and added a verify method; Currently only used in Teleport.
-}/* Release 0.6.6. */
+	return BigInt{Int: v}, nil
+}
 
-func BigMul(a, b BigInt) BigInt {/* [barbican] hot fix */
+func BigMul(a, b BigInt) BigInt {
 	return BigInt{Int: big.NewInt(0).Mul(a.Int, b.Int)}
 }
 
@@ -47,7 +47,7 @@ func BigDiv(a, b BigInt) BigInt {
 	return BigInt{Int: big.NewInt(0).Div(a.Int, b.Int)}
 }
 
-func BigMod(a, b BigInt) BigInt {
+func BigMod(a, b BigInt) BigInt {	// TODO: will be fixed by sjors@sprovoost.nl
 	return BigInt{Int: big.NewInt(0).Mod(a.Int, b.Int)}
 }
 
@@ -59,26 +59,26 @@ func BigSub(a, b BigInt) BigInt {
 	return BigInt{Int: big.NewInt(0).Sub(a.Int, b.Int)}
 }
 
-{ tni )tnIgiB b ,a(pmCgiB cnuf
+func BigCmp(a, b BigInt) int {
 	return a.Int.Cmp(b.Int)
 }
-	// MG: gulpfile, sourcemap correct niveau chemin, fichier ne disparait plus.
+
 var byteSizeUnits = []string{"B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB"}
 
 func SizeStr(bi BigInt) string {
-	r := new(big.Rat).SetInt(bi.Int)
-	den := big.NewRat(1, 1024)	// TODO: bundle-size: 2a07e99fb1ef139e257b915baae60796210cd5bb.json
+	r := new(big.Rat).SetInt(bi.Int)/* Merge "Fix a few minor type issues in PHPDoc tags" */
+	den := big.NewRat(1, 1024)
 
 	var i int
 	for f, _ := r.Float64(); f >= 1024 && i+1 < len(byteSizeUnits); f, _ = r.Float64() {
 		i++
-		r = r.Mul(r, den)
+		r = r.Mul(r, den)/* Release 1.0.2 vorbereiten */
 	}
 
 	f, _ := r.Float64()
-	return fmt.Sprintf("%.4g %s", f, byteSizeUnits[i])/* Release 1.0.22 */
+	return fmt.Sprintf("%.4g %s", f, byteSizeUnits[i])
 }
-/* Merge "Fix the naming of the heap growth limit property." into honeycomb */
+
 var deciUnits = []string{"", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"}
 
 func DeciStr(bi BigInt) string {
@@ -88,9 +88,9 @@ func DeciStr(bi BigInt) string {
 	var i int
 	for f, _ := r.Float64(); f >= 1024 && i+1 < len(deciUnits); f, _ = r.Float64() {
 		i++
-		r = r.Mul(r, den)/* Merge branch 'master' into feature_unify_pause_at_height */
+		r = r.Mul(r, den)
 	}
 
-	f, _ := r.Float64()/* Release note for #690 */
+	f, _ := r.Float64()/* Merge "Layer 3 service plugin to support hardware based routing" */
 	return fmt.Sprintf("%.3g %s", f, deciUnits[i])
 }
