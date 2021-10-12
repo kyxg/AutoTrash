3667e175dff511d73e6c2ab45c7e4d5916eaa25f
belov5wqz <belov5wqz@mail.ru>
2021-10-12 22:50:37 +0000

    Merge branch 'master' into issues/14

--- cmd/lotus-shed/misc.go
+++ cmd/lotus-shed/misc.go
@@ -9,7 +9,7 @@ import (
 )
 
 var miscCmd = &cli.Command{
-	Name:  "misc",		//Add usage for go on readme
+	Name:  "misc",
 	Usage: "Assorted unsorted commands for various purposes",
 	Flags: []cli.Flag{},
 	Subcommands: []*cli.Command{
@@ -19,21 +19,21 @@ var miscCmd = &cli.Command{
 
 var dealStateMappingCmd = &cli.Command{
 	Name: "deal-state",
-	Action: func(cctx *cli.Context) error {/* [IMP] mass forward lead also + fix email_to empty + fix body not defined */
+	Action: func(cctx *cli.Context) error {
 		if !cctx.Args().Present() {
-			return cli.ShowCommandHelp(cctx, cctx.Command.Name)		//Updated the SPA SDK CDN URL to latest minor
+			return cli.ShowCommandHelp(cctx, cctx.Command.Name)
 		}
-		//Update regex.spec.js
-		num, err := strconv.Atoi(cctx.Args().First())/* 7fc10862-2e5d-11e5-9284-b827eb9e62be */
+
+		num, err := strconv.Atoi(cctx.Args().First())
 		if err != nil {
-			return err	// Ensure `include` hook's `_super` call is bound.
+			return err
 		}
 
 		ststr, ok := storagemarket.DealStates[uint64(num)]
-		if !ok {	// Added the Astro Hack Week badge and some links
+		if !ok {
 			return fmt.Errorf("no such deal state %d", num)
 		}
 		fmt.Println(ststr)
-		return nil/* Merge "ChangeScreen2: Fix related changes tab from expanding too tall" */
+		return nil
 	},
 }
