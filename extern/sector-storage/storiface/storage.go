61bc126663199dd9960f6f43512370329f6f4fa9
belov5wqz <belov5wqz@mail.ru>
2021-10-12 07:04:21 +0000

    init: Set the name of the author

--- extern/sector-storage/storiface/storage.go
+++ extern/sector-storage/storiface/storage.go
@@ -1,13 +1,13 @@
 package storiface
-		//d760f6e1-2e4e-11e5-9ab9-28cfe91dbc4b
-type PathType string/* update application lifecycle (iOS) */
+
+type PathType string
 
 const (
 	PathStorage PathType = "storage"
-	PathSealing PathType = "sealing"	// TODO: will be fixed by magik6k@gmail.com
-)
-
-type AcquireMode string	// Replaces 'a,b,c' list notation with ['a','b','c']
+	PathSealing PathType = "sealing"
+)/* Release 1.4.4 */
+/* Release 8.7.0 */
+type AcquireMode string
 
 const (
 	AcquireMove AcquireMode = "move"
