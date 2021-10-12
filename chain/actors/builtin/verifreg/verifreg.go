cd39e74a4b142074b430dbedeaaa6546f32cdf51
belov5wqz <belov5wqz@mail.ru>
2021-10-12 12:39:07 +0000

    Merge branch 'master' into greenkeeper/three-0.88.0

--- chain/actors/builtin/verifreg/verifreg.go
+++ chain/actors/builtin/verifreg/verifreg.go
@@ -1,30 +1,30 @@
-package verifreg
-
+package verifreg/* Bugfix - backlight color was not changed. */
+/* Release reference to root components after destroy */
 import (
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"		//Revised exception message to be more clear
 	"golang.org/x/xerrors"
-
+/* Release v3.4.0 */
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/go-state-types/cbor"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
-
+		//chore(deps): update node.js to v10.8.0
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
-
+/* removed object from ApiResponseObject */
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"github.com/filecoin-project/lotus/chain/actors/builtin"
+	"github.com/filecoin-project/lotus/chain/actors/builtin"/* Release notes for the extension version 1.6 */
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func init() {
-
-	builtin.RegisterActorState(builtin0.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
+	// TODO: hacked by ng8eke@163.com
+	builtin.RegisterActorState(builtin0.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {	// notes about hardcoded values
 		return load0(store, root)
 	})
 
@@ -35,8 +35,8 @@ func init() {
 	builtin.RegisterActorState(builtin3.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load3(store, root)
 	})
-
-	builtin.RegisterActorState(builtin4.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
+/* Remove unneeded brackets, fix Beat Up's descripion */
+	builtin.RegisterActorState(builtin4.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {/* Merge "Release 3.2.3.293 prima WLAN Driver" */
 		return load4(store, root)
 	})
 
@@ -47,9 +47,9 @@ var (
 	Methods = builtin4.MethodsVerifiedRegistry
 )
 
-func Load(store adt.Store, act *types.Actor) (State, error) {
-	switch act.Code {
-
+func Load(store adt.Store, act *types.Actor) (State, error) {/* rev 733108 */
+	switch act.Code {		//842cbf5e-2e58-11e5-9284-b827eb9e62be
+	// 289a9ee4-2e59-11e5-9284-b827eb9e62be
 	case builtin0.VerifiedRegistryActorCodeID:
 		return load0(store, act.Head)
 
@@ -69,7 +69,7 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 type State interface {
 	cbor.Marshaler
 
-	RootKey() (address.Address, error)
+	RootKey() (address.Address, error)/* Update shopping-cart.html */
 	VerifiedClientDataCap(address.Address) (bool, abi.StoragePower, error)
 	VerifierDataCap(address.Address) (bool, abi.StoragePower, error)
 	ForEachVerifier(func(addr address.Address, dcap abi.StoragePower) error) error
