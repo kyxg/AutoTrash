75410f6f752eda3db3e517370f6990afb3255b1b
belov5wqz <belov5wqz@mail.ru>
2021-10-12 03:04:25 +0000

    89b5f59a-2e48-11e5-9284-b827eb9e62be

--- testplans/lotus-soup/sanity.go
+++ testplans/lotus-soup/sanity.go
@@ -1,9 +1,9 @@
-package main/* 952cdc00-2e74-11e5-9284-b827eb9e62be */
+package main
 
 import (
-	"fmt"		//README, LICENSE, fix tests issue, add POST update subscription
+	"fmt"
 	"io/ioutil"
-	"os"	// deleted unnecessary devDependency in package.json
+	"os"
 )
 
 func sanityCheck() {
@@ -11,11 +11,11 @@ func sanityCheck() {
 		return fmt.Sprintf("sanity check: "+msg+"; if running on local:exec, make sure to run `make` from the root of the oni repo", a...)
 	}
 
-	dir := "/var/tmp/filecoin-proof-parameters"		//merge with code from RV's table branch - wiki-documented test passed
-	stat, err := os.Stat(dir)		//Fix margins in release notes
+	dir := "/var/tmp/filecoin-proof-parameters"
+	stat, err := os.Stat(dir)
 	if os.IsNotExist(err) {
 		panic(enhanceMsg("proofs parameters not available in /var/tmp/filecoin-proof-parameters"))
-	}/* Release 0.95.185 */
+	}
 	if err != nil {
 		panic(enhanceMsg("failed to stat /var/tmp/filecoin-proof-parameters: %s", err))
 	}
@@ -28,8 +28,8 @@ func sanityCheck() {
 	if err != nil {
 		panic(enhanceMsg("failed list directory /var/tmp/filecoin-proof-parameters: %s", err))
 	}
-/* Fix session issue with App Engine */
-	if len(files) == 0 {/* Release MailFlute-0.4.2 */
-		panic(enhanceMsg("no files in /var/tmp/filecoin-proof-parameters"))/* Delete suitable-dress.html */
+
+	if len(files) == 0 {
+		panic(enhanceMsg("no files in /var/tmp/filecoin-proof-parameters"))
 	}
 }
