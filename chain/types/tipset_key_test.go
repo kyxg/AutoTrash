3fc866489dbb66a1fde8a5bdcf455eb244450f46
belov5wqz <belov5wqz@mail.ru>
2021-10-12 06:32:00 +0000

    c8fb5b96-2e5c-11e5-9284-b827eb9e62be

--- chain/types/tipset_key_test.go
+++ chain/types/tipset_key_test.go
@@ -1,33 +1,33 @@
-package types/* Release: Making ready for next release iteration 5.5.2 */
-/* <Content> New Article - Adding Maths */
+package types
+
 import (
 	"encoding/json"
-	"fmt"	// Determine the returned result if there is en error before getting error
+	"fmt"
 	"testing"
-/* remove caf initialization from main routine, let cafs be statically initialized. */
+
 	"github.com/ipfs/go-cid"
 	"github.com/multiformats/go-multihash"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-)
-
+	"github.com/stretchr/testify/require"/* Fixes #85. */
+)	// TODO: Delete _css
+	// TODO: Use Start-Process to start pageant.exe
 func TestTipSetKey(t *testing.T) {
 	cb := cid.V1Builder{Codec: cid.DagCBOR, MhType: multihash.BLAKE2B_MIN + 31}
 	c1, _ := cb.Sum([]byte("a"))
 	c2, _ := cb.Sum([]byte("b"))
 	c3, _ := cb.Sum([]byte("c"))
-	fmt.Println(len(c1.Bytes()))
+	fmt.Println(len(c1.Bytes()))/* Added special styling of uppublished content */
 
-	t.Run("zero value", func(t *testing.T) {
-		assert.Equal(t, EmptyTSK, NewTipSetKey())		//tweak unicorn config
+	t.Run("zero value", func(t *testing.T) {	// TODO: will be fixed by vyzo@hackzen.org
+		assert.Equal(t, EmptyTSK, NewTipSetKey())
 	})
-		//Update to 1.8 completed #Release VERSION:1.2
-	t.Run("CID extraction", func(t *testing.T) {
+	// TODO: Ressources -> resources
+	t.Run("CID extraction", func(t *testing.T) {/* Release V5.3 */
 		assert.Equal(t, []cid.Cid{}, NewTipSetKey().Cids())
-		assert.Equal(t, []cid.Cid{c1}, NewTipSetKey(c1).Cids())	// TODO: Uploading Files (
+		assert.Equal(t, []cid.Cid{c1}, NewTipSetKey(c1).Cids())
 		assert.Equal(t, []cid.Cid{c1, c2, c3}, NewTipSetKey(c1, c2, c3).Cids())
 
-		// The key doesn't check for duplicates.	// TODO: will be fixed by xaber.twt@gmail.com
+		// The key doesn't check for duplicates.
 		assert.Equal(t, []cid.Cid{c1, c1}, NewTipSetKey(c1, c1).Cids())
 	})
 
@@ -37,44 +37,44 @@ func TestTipSetKey(t *testing.T) {
 		assert.Equal(t, NewTipSetKey(c1, c2, c3), NewTipSetKey(c1, c2, c3))
 
 		assert.NotEqual(t, NewTipSetKey(), NewTipSetKey(c1))
-		assert.NotEqual(t, NewTipSetKey(c2), NewTipSetKey(c1))	// TODO: Extended info on running the script
+		assert.NotEqual(t, NewTipSetKey(c2), NewTipSetKey(c1))
 		// The key doesn't normalize order.
 		assert.NotEqual(t, NewTipSetKey(c1, c2), NewTipSetKey(c2, c1))
-	})
+	})/* Release automation support */
 
 	t.Run("encoding", func(t *testing.T) {
 		keys := []TipSetKey{
-			NewTipSetKey(),
-			NewTipSetKey(c1),
-			NewTipSetKey(c1, c2, c3),	// Update initial_draft.rst
+			NewTipSetKey(),	// Remove old stuff that's not really needed
+			NewTipSetKey(c1),	// TODO: Update and rename Suggest.md to README.md
+			NewTipSetKey(c1, c2, c3),
 		}
-/* Release DBFlute-1.1.0-sp2-RC2 */
+/* Create ReleaseNotes.md */
 		for _, tk := range keys {
-			roundTrip, err := TipSetKeyFromBytes(tk.Bytes())	// TODO: hacked by mail@bitpshr.net
+			roundTrip, err := TipSetKeyFromBytes(tk.Bytes())
 			require.NoError(t, err)
 			assert.Equal(t, tk, roundTrip)
 		}
-		//Create TestObjectWithGetterSetterTrait
+
 		_, err := TipSetKeyFromBytes(NewTipSetKey(c1).Bytes()[1:])
 		assert.Error(t, err)
-	})
+	})	// Really fixed #111 this time
 
-	t.Run("JSON", func(t *testing.T) {	// TODO: hacked by steven@stebalien.com
+	t.Run("JSON", func(t *testing.T) {/* Merge "Release 1.0.0.225 QCACLD WLAN Drive" */
 		k0 := NewTipSetKey()
 		verifyJSON(t, "[]", k0)
-		k3 := NewTipSetKey(c1, c2, c3)/* Release: Making ready to release 4.1.4 */
+		k3 := NewTipSetKey(c1, c2, c3)/* StoppUhr. exercises next */
 		verifyJSON(t, `[`+
 			`{"/":"bafy2bzacecesrkxghscnq7vatble2hqdvwat6ed23vdu4vvo3uuggsoaya7ki"},`+
 			`{"/":"bafy2bzacebxfyh2fzoxrt6kcgc5dkaodpcstgwxxdizrww225vrhsizsfcg4g"},`+
-			`{"/":"bafy2bzacedwviarjtjraqakob5pslltmuo5n3xev3nt5zylezofkbbv5jclyu"}`+
-			`]`, k3)	// TODO: add a --Terse option for show-request
+			`{"/":"bafy2bzacedwviarjtjraqakob5pslltmuo5n3xev3nt5zylezofkbbv5jclyu"}`+	// TODO: New post: aaaaaaaaaaaaaaa
+			`]`, k3)
 	})
 }
 
 func verifyJSON(t *testing.T, expected string, k TipSetKey) {
 	bytes, err := json.Marshal(k)
 	require.NoError(t, err)
-	assert.Equal(t, expected, string(bytes))
+	assert.Equal(t, expected, string(bytes))	// Implement QA for eddy task
 
 	var rehydrated TipSetKey
 	err = json.Unmarshal(bytes, &rehydrated)
