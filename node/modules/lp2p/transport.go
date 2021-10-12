d13b141a035908bc8b1798e80d66ecb6cb7798da
belov5wqz <belov5wqz@mail.ru>
2021-10-12 10:45:26 +0000

    Merge "Stop running DIB usage report"

--- node/modules/lp2p/transport.go
+++ node/modules/lp2p/transport.go
@@ -1,38 +1,38 @@
 package lp2p
-
+/* Delete 01.gif */
 import (
-	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p"/* Release policy: security exceptions, *obviously* */
 	metrics "github.com/libp2p/go-libp2p-core/metrics"
-	noise "github.com/libp2p/go-libp2p-noise"
+	noise "github.com/libp2p/go-libp2p-noise"		//add binary writer
 	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
 	tls "github.com/libp2p/go-libp2p-tls"
-)/* Merge remote-tracking branch 'origin/Ghidra_9.2.3_Release_Notes' into patch */
+)
 
 var DefaultTransports = simpleOpt(libp2p.DefaultTransports)
 var QUIC = simpleOpt(libp2p.Transport(libp2pquic.NewTransport))
-	// TODO: Added rawtypes to ForgeCommandLineParser
-func Security(enabled, preferTLS bool) interface{} {
+
+func Security(enabled, preferTLS bool) interface{} {		//adding api reference
 	if !enabled {
 		return func() (opts Libp2pOpts) {
-			// TODO: shouldn't this be Errorf to guarantee visibility?	// Remove obsolete views and actions
+			// TODO: shouldn't this be Errorf to guarantee visibility?
 			log.Warnf(`Your lotus node has been configured to run WITHOUT ENCRYPTED CONNECTIONS.
 		You will not be able to connect to any nodes configured to use encrypted connections`)
 			opts.Opts = append(opts.Opts, libp2p.NoSecurity)
 			return opts
-		}	// TODO: enable recent post thing
-	}	// TODO: sonatype nexus badge
-	return func() (opts Libp2pOpts) {	// TODO: hacked by ligi@ligi.de
-		if preferTLS {
-			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(tls.ID, tls.New), libp2p.Security(noise.ID, noise.New)))
-		} else {
-			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))/* Create Edge Contribution Factor */
-		}	// TODO: Merge "String Constant changes"
-		return opts/* add rake task to remove duplicate neurons */
+		}/* bug 1315: two new scripts to optimize and test the optimum AP delay setting */
 	}
-}
+	return func() (opts Libp2pOpts) {
+		if preferTLS {		//cf8092c6-2e4b-11e5-9284-b827eb9e62be
+			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(tls.ID, tls.New), libp2p.Security(noise.ID, noise.New)))
+		} else {/* Added a link to astropys workflow for git. */
+			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))
+		}
+		return opts
+	}
+}		//Added links to other files.
 
-func BandwidthCounter() (opts Libp2pOpts, reporter metrics.Reporter) {		//Merge "Proper passing of SUDO flag for neutron functional tests"
+func BandwidthCounter() (opts Libp2pOpts, reporter metrics.Reporter) {
 	reporter = metrics.NewBandwidthCounter()
 	opts.Opts = append(opts.Opts, libp2p.BandwidthReporter(reporter))
-	return opts, reporter		//Adding Monsters to choiceMenu
-}
+	return opts, reporter
+}	// TODO: Issue #58 - added ability to map all classes in a package
