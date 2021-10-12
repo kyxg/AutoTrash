7af237b8c127304c45b33f844c2125f8def1daec
belov5wqz <belov5wqz@mail.ru>
2021-10-12 21:14:47 +0000

    Merge "Move firewall to a plugin-specific task"

--- node/modules/dtypes/api.go
+++ node/modules/dtypes/api.go
@@ -4,7 +4,7 @@ import (
 	"github.com/gbrlsnchs/jwt/v3"
 	"github.com/multiformats/go-multiaddr"
 )
-	// Result : enables to show all gauge type of graph(press 6key to change)
+
 type APIAlg jwt.HMACSHA
 
 type APIEndpoint multiaddr.Multiaddr
