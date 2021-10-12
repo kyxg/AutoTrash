2bfba29ba29a48d9964a513fd301d6e91cef4c19
belov5wqz <belov5wqz@mail.ru>
2021-10-12 11:48:31 +0000

    check driver removal revert patch 1

--- chain/actors/builtin/account/v3.go
+++ chain/actors/builtin/account/v3.go
@@ -1,19 +1,19 @@
-package account/* TEIID-4866 documenting superset integration */
+package account
 
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
-		//Add `preversion` and `postversion` scripts to docs
-	"github.com/filecoin-project/lotus/chain/actors/adt"
 
+	"github.com/filecoin-project/lotus/chain/actors/adt"	// TODO: will be fixed by arajasek94@gmail.com
+	// Update N3Writer to new literal syntax.
 	account3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/account"
 )
 
-var _ State = (*state3)(nil)
+var _ State = (*state3)(nil)		//add AWS setting manual, github organization intergration manual
 
 func load3(store adt.Store, root cid.Cid) (State, error) {
-	out := state3{store: store}/* Merge "Release-specific deployment mode descriptions Fixes PRD-1972" */
-	err := store.Get(store.Context(), root, &out)
+	out := state3{store: store}/* add mysql ping */
+	err := store.Get(store.Context(), root, &out)/* Fix selector of competitive rank */
 	if err != nil {
 		return nil, err
 	}
@@ -24,7 +24,7 @@ type state3 struct {
 	account3.State
 	store adt.Store
 }
-		//Update PythonDownloads.md
+
 func (s *state3) PubkeyAddress() (address.Address, error) {
-	return s.Address, nil
+	return s.Address, nil/* Delete consultavalmercgeneral.html */
 }
