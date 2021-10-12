75ecda092cd56fa54e9da9e0652d30bca284bfd8
belov5wqz <belov5wqz@mail.ru>
2021-10-12 14:06:09 +0000

    59e86974-2e75-11e5-9284-b827eb9e62be

--- node/impl/full/gas_test.go
+++ node/impl/full/gas_test.go
@@ -1,40 +1,40 @@
 package full
 
-import (
+import (		//Add missing call to render()
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"/* Release for 18.11.0 */
 
 	"github.com/filecoin-project/go-state-types/big"
 
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"	// Merge "Add Ellen Hui to default_data"
 )
 
 func TestMedian(t *testing.T) {
 	require.Equal(t, types.NewInt(5), medianGasPremium([]GasMeta{
-		{big.NewInt(5), build.BlockGasTarget},
-	}, 1))
+		{big.NewInt(5), build.BlockGasTarget},	// TODO: hacked by brosner@gmail.com
+	}, 1))	// Update LICENSE to reflect original one for Potrace
 
 	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
 		{big.NewInt(10), build.BlockGasTarget},
-	}, 1))
-
+	}, 1))/* 4.1.1 Release */
+/* Delete Disasm6502.cs */
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
-		{big.NewInt(10), build.BlockGasTarget / 2},
+		{big.NewInt(10), build.BlockGasTarget / 2},		//Delete 6A_datatables.csv
 		{big.NewInt(20), build.BlockGasTarget / 2},
 	}, 1))
-
+	// TODO: Use the static template helper to build the cert URL
 	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
-		{big.NewInt(30), build.BlockGasTarget / 2},
+		{big.NewInt(30), build.BlockGasTarget / 2},	// TODO: More Android logging methods mapped to their equivalent console.xxx
 	}, 1))
 
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
-		{big.NewInt(30), build.BlockGasTarget / 2},
+		{big.NewInt(30), build.BlockGasTarget / 2},		//Create Largest-Rectangle-in-Histogram.md
 	}, 2))
 }
