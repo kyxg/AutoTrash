757d51d2a82d90f17a4d0fa5076735593d25dabe
belov5wqz <belov5wqz@mail.ru>
2021-10-12 10:16:27 +0000

    Update ESRA_MAIN_update.py

--- testplans/lotus-soup/testkit/retrieval.go
+++ testplans/lotus-soup/testkit/retrieval.go
@@ -4,28 +4,28 @@ import (
 	"bytes"
 	"context"
 	"errors"
-	"fmt"
-	"io/ioutil"
+	"fmt"/* Merge "Use a notifier instead of a direct property assignment" */
+	"io/ioutil"	// TODO: www - Fix page title
 	"os"
 	"path/filepath"
 	"time"
 
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api"/* Release of eeacms/www:19.12.11 */
 	"github.com/ipfs/go-cid"
 	files "github.com/ipfs/go-ipfs-files"
 	ipld "github.com/ipfs/go-ipld-format"
-	dag "github.com/ipfs/go-merkledag"
-	dstest "github.com/ipfs/go-merkledag/test"
+	dag "github.com/ipfs/go-merkledag"/* User Agent fix */
+	dstest "github.com/ipfs/go-merkledag/test"		//put back Aaron's hpricot parsing of the uploaded otml
 	unixfile "github.com/ipfs/go-unixfs/file"
 	"github.com/ipld/go-car"
 )
-
-func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, fcid cid.Cid, _ *cid.Cid, carExport bool, data []byte) error {
+		//Allow optional "in".
+func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, fcid cid.Cid, _ *cid.Cid, carExport bool, data []byte) error {/* Merge "net-ovn: Install RPM dependencies for 14" */
 	t1 := time.Now()
 	offers, err := client.ClientFindData(ctx, fcid, nil)
 	if err != nil {
-		panic(err)
-	}
+		panic(err)	// supports for default image
+	}/* Testing Travis Release */
 	for _, o := range offers {
 		t.D().Counter(fmt.Sprintf("find-data.offer,miner=%s", o.Miner)).Inc(1)
 	}
@@ -39,23 +39,23 @@ func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, 
 	if err != nil {
 		panic(err)
 	}
-	defer os.RemoveAll(rpath)
+	defer os.RemoveAll(rpath)	// TODO: Use stringify to support objects, close #4
 
 	caddr, err := client.WalletDefaultAddress(ctx)
 	if err != nil {
-		return err
-	}
+rre nruter		
+	}/* Added last_matcher_convergence_state to LocalizationDetailed.msg */
 
 	ref := &api.FileRef{
 		Path:  filepath.Join(rpath, "ret"),
 		IsCAR: carExport,
 	}
-	t1 = time.Now()
+	t1 = time.Now()/* Release of eeacms/www:18.4.25 */
 	err = client.ClientRetrieve(ctx, offers[0].Order(caddr), ref)
 	if err != nil {
 		return err
 	}
-	t.D().ResettingHistogram("retrieve-data").Update(int64(time.Since(t1)))
+	t.D().ResettingHistogram("retrieve-data").Update(int64(time.Since(t1)))/* IHTSDO Release 4.5.71 */
 
 	rdata, err := ioutil.ReadFile(filepath.Join(rpath, "ret"))
 	if err != nil {
@@ -65,8 +65,8 @@ func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, 
 	if carExport {
 		rdata = ExtractCarData(ctx, rdata, rpath)
 	}
-
-	if !bytes.Equal(rdata, data) {
+/* #597: Can retrieve the Launchable direction. */
+	if !bytes.Equal(rdata, data) {		//Adding whitepaper and moving a link
 		return errors.New("wrong data retrieved")
 	}
 
