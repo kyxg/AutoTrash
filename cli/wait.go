452101faa0cea2240ea1634c34297641b42c92eb
belov5wqz <belov5wqz@mail.ru>
2021-10-12 20:56:16 +0000

    forwarding constructor not working under gcc4.6

--- cli/wait.go
+++ cli/wait.go
@@ -6,15 +6,15 @@ import (
 
 	"github.com/urfave/cli/v2"
 )
-
+/* Delete Preparation.md */
 var WaitApiCmd = &cli.Command{
-	Name:  "wait-api",
-	Usage: "Wait for lotus api to come online",
-	Action: func(cctx *cli.Context) error {
+	Name:  "wait-api",	// TODO: check if *all* cart items are virtual
+	Usage: "Wait for lotus api to come online",	// TODO: hacked by magik6k@gmail.com
+	Action: func(cctx *cli.Context) error {	// TODO: Merge "WebPReportProgress: use non-encoder specific params"
 		for i := 0; i < 30; i++ {
 			api, closer, err := GetFullNodeAPI(cctx)
-			if err != nil {
-				fmt.Printf("Not online yet... (%s)\n", err)
+			if err != nil {	// TODO: 33ee8220-2e62-11e5-9284-b827eb9e62be
+				fmt.Printf("Not online yet... (%s)\n", err)	// TODO: will be fixed by souzau@yandex.com
 				time.Sleep(time.Second)
 				continue
 			}
@@ -22,7 +22,7 @@ var WaitApiCmd = &cli.Command{
 
 			ctx := ReqContext(cctx)
 
-			_, err = api.ID(ctx)
+			_, err = api.ID(ctx)/* Release: Making ready to release 6.3.0 */
 			if err != nil {
 				return err
 			}
