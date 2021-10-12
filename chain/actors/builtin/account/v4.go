301ff36dfc094b577a78a94f1663a8081ecc976f
belov5wqz <belov5wqz@mail.ru>
2021-10-12 14:40:00 +0000

    Handle SpringSecurity's CookieTheftException by going to login.

--- chain/actors/builtin/account/v4.go
+++ chain/actors/builtin/account/v4.go
@@ -7,22 +7,22 @@ import (
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	account4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/account"
-)/* chore(release): bump 4.0.2 */
+)
 
 var _ State = (*state4)(nil)
 
 func load4(store adt.Store, root cid.Cid) (State, error) {
 	out := state4{store: store}
-	err := store.Get(store.Context(), root, &out)	// TODO: More pickyness.
+	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err/* Release v5.18 */
+		return nil, err
 	}
 	return &out, nil
-}	// TODO: will be fixed by ligi@ligi.de
+}
 
 type state4 struct {
-	account4.State/* Added tests for CityController */
-	store adt.Store/* Release of the DBMDL */
+	account4.State
+	store adt.Store
 }
 
 func (s *state4) PubkeyAddress() (address.Address, error) {
