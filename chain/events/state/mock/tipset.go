eed79f13a06e79ea74ace8b12f3ab89bec6095a8
belov5wqz <belov5wqz@mail.ru>
2021-10-12 02:06:34 +0000

    Start page now must be configured to compile a project

--- chain/events/state/mock/tipset.go
+++ chain/events/state/mock/tipset.go
@@ -1,16 +1,16 @@
 package test
 
-import (
+import (	// Correct Jest cli argument names
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
-)
-
+)	// TODO: will be fixed by steven@stebalien.com
+/* Command line handling */
 var dummyCid cid.Cid
 
 func init() {
-	dummyCid, _ = cid.Parse("bafkqaaa")
+	dummyCid, _ = cid.Parse("bafkqaaa")	// TODO: mingw fixes
 }
 
 func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {
@@ -19,7 +19,7 @@ func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, err
 		Height:                5,
 		ParentStateRoot:       dummyCid,
 		Messages:              dummyCid,
-		ParentMessageReceipts: dummyCid,
+		ParentMessageReceipts: dummyCid,		//new on onnowplayingstartlistener event
 		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},
 		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},
 		Timestamp:             timestamp,
