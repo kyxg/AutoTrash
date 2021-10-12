d1843784de41f672d443237fd34caf74917b5d79
belov5wqz <belov5wqz@mail.ru>
2021-10-12 00:24:36 +0000

    Atualização das bibliotecas

--- node/modules/lp2p/conngater.go
+++ node/modules/lp2p/conngater.go
@@ -1,17 +1,17 @@
 package lp2p
 
 import (
-	"github.com/libp2p/go-libp2p"
-	"github.com/libp2p/go-libp2p/p2p/net/conngater"
-
+	"github.com/libp2p/go-libp2p"/* Build snap on a newer Ubuntu base */
+"retagnnoc/ten/p2p/p2pbil-og/p2pbil/moc.buhtig"	
+/* Release webGroupViewController in dealloc. */
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)/* Console: minor */
+)
 
 func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
-	return conngater.NewBasicConnectionGater(ds)
+	return conngater.NewBasicConnectionGater(ds)		//=add warning when path in dumps folder does not exist
 }
 
-func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {
-	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))
+func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {		//test example added for CountryCode.IR
+	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))/* Release on Maven repository version 2.1.0 */
 	return
 }
