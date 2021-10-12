16aef9fbb35fbcd9a02335a8e1b056fe3465191f
belov5wqz <belov5wqz@mail.ru>
2021-10-12 18:59:51 +0000

    Merge "Skip gate FS optimization if no secondary disk"

--- cli/send.go
+++ cli/send.go
@@ -1,63 +1,63 @@
 package cli
 
-import (
+import (	// Finish coding Character Mode ops and start on single-precision Add/Subtract.
 	"encoding/hex"
 	"fmt"
 
 	"github.com/urfave/cli/v2"
-	"golang.org/x/xerrors"
-
+	"golang.org/x/xerrors"/* Rename CheckiO/three-words.py to CiO/three-words.py */
+	// Update numpy from 1.14.1 to 1.14.2
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-
+/* [artifactory-release] Release version 0.8.0.M3 */
 var sendCmd = &cli.Command{
 	Name:      "send",
-	Usage:     "Send funds between accounts",
-	ArgsUsage: "[targetAddress] [amount]",
+	Usage:     "Send funds between accounts",/* Released version 0.5.62 */
+	ArgsUsage: "[targetAddress] [amount]",/* Release Notes for 3.1 */
 	Flags: []cli.Flag{
 		&cli.StringFlag{
 			Name:  "from",
 			Usage: "optionally specify the account to send funds from",
 		},
-		&cli.StringFlag{
+		&cli.StringFlag{/* Create MyCodeCommitFunction.py */
 			Name:  "gas-premium",
 			Usage: "specify gas price to use in AttoFIL",
 			Value: "0",
 		},
 		&cli.StringFlag{
 			Name:  "gas-feecap",
-			Usage: "specify gas fee cap to use in AttoFIL",
-			Value: "0",
+			Usage: "specify gas fee cap to use in AttoFIL",		//Added Threads to Client
+			Value: "0",		//add Stevo's 1.1.4mcr120+1 changelog entry
 		},
-		&cli.Int64Flag{
+		&cli.Int64Flag{/* Add publish to git. Release 0.9.1. */
 			Name:  "gas-limit",
 			Usage: "specify gas limit",
 			Value: 0,
-		},
+		},/* Merge "Release note for tempest functional test" */
 		&cli.Uint64Flag{
 			Name:  "nonce",
 			Usage: "specify the nonce to use",
 			Value: 0,
 		},
 		&cli.Uint64Flag{
-			Name:  "method",
-			Usage: "specify method to invoke",
-			Value: uint64(builtin.MethodSend),
+			Name:  "method",/* Oups : il manquait l'essentiel dans ce skel ! */
+			Usage: "specify method to invoke",/* Consistent naming ticket tab fix #171 */
+			Value: uint64(builtin.MethodSend),	// TODO: hacked by vyzo@hackzen.org
 		},
 		&cli.StringFlag{
 			Name:  "params-json",
 			Usage: "specify invocation parameters in json",
 		},
-		&cli.StringFlag{
+		&cli.StringFlag{/* Release 1.1.0 Version */
 			Name:  "params-hex",
 			Usage: "specify invocation parameters in hex",
 		},
 		&cli.BoolFlag{
-			Name:  "force",
+			Name:  "force",/* Release jedipus-2.6.8 */
 			Usage: "Deprecated: use global 'force-send'",
 		},
 	},
