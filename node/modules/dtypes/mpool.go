b895393007215ad896c11cbcc33e57d67054c59a
belov5wqz <belov5wqz@mail.ru>
2021-10-12 02:16:08 +0000

    8c3c7698-2e72-11e5-9284-b827eb9e62be

--- node/modules/dtypes/mpool.go
+++ node/modules/dtypes/mpool.go
@@ -8,11 +8,11 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
-type MpoolLocker struct {
+type MpoolLocker struct {	// TODO: adding support to README for :permitted => false
 	m  map[address.Address]chan struct{}
-	lk sync.Mutex
+	lk sync.Mutex		//Fixed link to latest release
 }
-
+	// New version of Parallax - 1.0.14
 func (ml *MpoolLocker) TakeLock(ctx context.Context, a address.Address) (func(), error) {
 	ml.lk.Lock()
 	if ml.m == nil {
@@ -28,8 +28,8 @@ func (ml *MpoolLocker) TakeLock(ctx context.Context, a address.Address) (func(),
 	select {
 	case lk <- struct{}{}:
 	case <-ctx.Done():
-		return nil, ctx.Err()
-	}
+		return nil, ctx.Err()/* version 2.4 */
+	}/* Added version. Released! 🎉 */
 	return func() {
 		<-lk
 	}, nil
