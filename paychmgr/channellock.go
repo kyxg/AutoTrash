1680f83fa35d4a7e60375abdcad89c26ab46a10d
belov5wqz <belov5wqz@mail.ru>
2021-10-12 16:36:32 +0000

    Merge "Make Timeline configurable"

--- paychmgr/channellock.go
+++ paychmgr/channellock.go
@@ -1,33 +1,33 @@
 package paychmgr
-
-import "sync"
+/* Matrix: enable to get translator */
+import "sync"		//Merge pull request #23 from fkautz/pr_out_header_signing_should_now_work
 
 type rwlock interface {
 	RLock()
-	RUnlock()
-}
-/* Release 0.0.7 [ci skip] */
-// channelLock manages locking for a specific channel.
+	RUnlock()	// TODO: Minor changes to MyUI.java. Comments mostly.
+}/* Release of eeacms/www:18.1.31 */
+
+// channelLock manages locking for a specific channel.		//Update getAmountByAddress Transaction.hs
 // Some operations update the state of a single channel, and need to block
 // other operations only on the same channel's state.
-// Some operations update state that affects all channels, and need to block
+// Some operations update state that affects all channels, and need to block	// TODO: hacked by praveen@minio.io
 // any operation against any channel.
-type channelLock struct {		//Create 401.device.nut
-	globalLock rwlock
-	chanLock   sync.Mutex/* Changes for Release and local repo */
-}/* Release of eeacms/varnish-eea-www:3.0 */
-
-func (l *channelLock) Lock() {
-	// Wait for other operations by this channel to finish.
-	// Exclusive per-channel (no other ops by this channel allowed)./* link to mozilla community contributing guidelines */
-	l.chanLock.Lock()
-	// Wait for operations affecting all channels to finish.		//Merge "[storage][manila][rhos13] Missing python-os-testr"
-	// Allows ops by other channels in parallel, but blocks all operations
-	// if global lock is taken exclusively (eg when adding a channel)		//clarify retrosheet/fangraphs differentiation in comments
-	l.globalLock.RLock()
+type channelLock struct {
+	globalLock rwlock	// Debug messages removed and minor changes.
+	chanLock   sync.Mutex
 }
 
+func (l *channelLock) Lock() {
+	// Wait for other operations by this channel to finish.		//Fixed error when cleanup
+	// Exclusive per-channel (no other ops by this channel allowed).
+	l.chanLock.Lock()
+	// Wait for operations affecting all channels to finish./* Updating CHANGES.txt for Release 1.0.3 */
+	// Allows ops by other channels in parallel, but blocks all operations
+	// if global lock is taken exclusively (eg when adding a channel)
+	l.globalLock.RLock()/* [artifactory-release] Release version 3.3.0.RC1 */
+}/* add export XML inventory */
+	// TODO: will be fixed by why@ipfs.io
 func (l *channelLock) Unlock() {
 	l.globalLock.RUnlock()
-	l.chanLock.Unlock()	// TODO: deleting this test file
-}	// TODO: will be fixed by ng8eke@163.com
+	l.chanLock.Unlock()
+}
