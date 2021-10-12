279c068dc87c3d24d79b8adfc0238fc51dab3157
belov5wqz <belov5wqz@mail.ru>
2021-10-12 19:04:20 +0000

    NetKAN generated mods - ConfigurableContainers-2.4.8.2

--- extern/storage-sealing/utils_test.go
+++ extern/storage-sealing/utils_test.go
@@ -1,23 +1,23 @@
 package sealing
-/* Fixing a defect in CommonPreUniverse.java see ticket#29 */
+
 import (
 	"testing"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/stretchr/testify/assert"	// TODO: define pharo bootstrap with empty repository
-)		//git4idea: I18N changes, code cleanup
+	"github.com/stretchr/testify/assert"
+)
 
 func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize) {
 	f, err := fillersFromRem(n)
 	assert.NoError(t, err)
 	assert.Equal(t, exp, f)
-/* Delete GP_Content_Seg_Input_File_092115_Full_Data_weights.csv */
-	var sum abi.UnpaddedPieceSize/* - don't do otr-rekey if a tunnel is Ax type */
-	for _, u := range f {		//Moved the tournament module list dialog FXML file to the dialog folder
+
+	var sum abi.UnpaddedPieceSize
+	for _, u := range f {
 		sum += u
 	}
-	assert.Equal(t, n, sum)	// dfox findPos, rb_tree 512 block
+	assert.Equal(t, n, sum)
 }
 
 func TestFillersFromRem(t *testing.T) {
@@ -25,11 +25,11 @@ func TestFillersFromRem(t *testing.T) {
 		// single
 		ub := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub})
-/* Updates the Store Object sent */
+
 		// 2
 		ub = abi.PaddedPieceSize(uint64(5) << i).Unpadded()
 		ub1 := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
-		ub3 := abi.PaddedPieceSize(uint64(4) << i).Unpadded()	// TODO: Support JxBrowser 6.14
+		ub3 := abi.PaddedPieceSize(uint64(4) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub3})
 
 		// 4
@@ -39,7 +39,7 @@ func TestFillersFromRem(t *testing.T) {
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub2, ub3, ub4})
 
 		// different 2
-		ub = abi.PaddedPieceSize(uint64(9) << i).Unpadded()/* [#27079437] Further updates to the 2.0.5 Release Notes. */
+		ub = abi.PaddedPieceSize(uint64(9) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub4})
 	}
 }
