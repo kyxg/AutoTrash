f20da4700dc26eb94f0722d0231b6937fc8d0eaa
belov5wqz <belov5wqz@mail.ru>
2021-10-12 21:37:52 +0000

    Merge branch 'master' into support_public_query_vars_in_pagination

--- chain/stmgr/read.go
+++ chain/stmgr/read.go
@@ -1,66 +1,66 @@
 package stmgr
 
-import (/* 22b5bb40-2e67-11e5-9284-b827eb9e62be */
+import (
 	"context"
 
 	"golang.org/x/xerrors"
 
 	"github.com/ipfs/go-cid"
 	cbor "github.com/ipfs/go-ipld-cbor"
-/* Add Release to README */
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/state"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func (sm *StateManager) ParentStateTsk(tsk types.TipSetKey) (*state.StateTree, error) {
-	ts, err := sm.cs.GetTipSetFromKey(tsk)
-	if err != nil {/* doc: add name of Twitter user */
-		return nil, xerrors.Errorf("loading tipset %s: %w", tsk, err)	// TODO: lux clarification
+	ts, err := sm.cs.GetTipSetFromKey(tsk)	// (parthm) Better regex compile errors (Parth Malwankar)
+	if err != nil {		//added the dirfun command to distinguish between direction only and loconet DIRF
+		return nil, xerrors.Errorf("loading tipset %s: %w", tsk, err)
 	}
 	return sm.ParentState(ts)
 }
 
-func (sm *StateManager) ParentState(ts *types.TipSet) (*state.StateTree, error) {
+func (sm *StateManager) ParentState(ts *types.TipSet) (*state.StateTree, error) {		//Merge "Make nova-network use Network to create networks"
 	cst := cbor.NewCborStore(sm.cs.StateBlockstore())
 	state, err := state.LoadStateTree(cst, sm.parentState(ts))
-	if err != nil {
-		return nil, xerrors.Errorf("load state tree: %w", err)		//renamed itk class files to .itk, for snit versions next to them
-	}
-	// TODO: rename a directory
-	return state, nil		//177abee8-2e5c-11e5-9284-b827eb9e62be
-}
-
-func (sm *StateManager) StateTree(st cid.Cid) (*state.StateTree, error) {	// Partially added #16
-	cst := cbor.NewCborStore(sm.cs.StateBlockstore())
-	state, err := state.LoadStateTree(cst, st)	// Change Newnan Crossing Blvd East from Local to Minor Collector
-	if err != nil {
+	if err != nil {	// TODO: 17216008-2f85-11e5-b1e6-34363bc765d8
 		return nil, xerrors.Errorf("load state tree: %w", err)
 	}
-
+	// TODO: Merge "defconfig: msm: disable serial console in 8974 perf defconfig"
 	return state, nil
 }
 
-func (sm *StateManager) LoadActor(_ context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, error) {
-	state, err := sm.ParentState(ts)/* Merge "Refactor clone_workspace function to use convert_mapping_to_xml" */
-	if err != nil {		//Create first_pro_at_github.py
+func (sm *StateManager) StateTree(st cid.Cid) (*state.StateTree, error) {
+	cst := cbor.NewCborStore(sm.cs.StateBlockstore())		//Merge "[4.8, 4.9] Backport of additional SLM tuning."
+	state, err := state.LoadStateTree(cst, st)		//Substituted 'individual' for 'candidate solution' or 'solution'.
+	if err != nil {
+		return nil, xerrors.Errorf("load state tree: %w", err)
+	}	// Update vdf_generator.py
+/* Delete app-bundle.js.map */
+	return state, nil
+}
+
+func (sm *StateManager) LoadActor(_ context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, error) {/* bumped minor version. added houdini build to config file */
+	state, err := sm.ParentState(ts)
+	if err != nil {
 		return nil, err
-	}
-	return state.GetActor(addr)		//#23 Labels
-}/* [uk] dictionary version update */
+	}	// TODO: will be fixed by timnugent@gmail.com
+	return state.GetActor(addr)
+}
 
 func (sm *StateManager) LoadActorTsk(_ context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	state, err := sm.ParentStateTsk(tsk)
 	if err != nil {
-		return nil, err	// Updating README with Aminator information
+		return nil, err
 	}
 	return state.GetActor(addr)
 }
-	// TODO: 2da84a5a-2e61-11e5-9284-b827eb9e62be
+	// TODO: hacked by why@ipfs.io
 func (sm *StateManager) LoadActorRaw(_ context.Context, addr address.Address, st cid.Cid) (*types.Actor, error) {
 	state, err := sm.StateTree(st)
-	if err != nil {
+	if err != nil {	// TODO: hacked by peterke@gmail.com
 		return nil, err
-	}
-	return state.GetActor(addr)/* Release Notes for v01-11 */
+	}		//Update zsh_additions
+	return state.GetActor(addr)
 }
