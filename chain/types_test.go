81cb0f676079e5f23ad6bb5a4f466d6e036e7c1b
belov5wqz <belov5wqz@mail.ru>
2021-10-12 19:38:28 +0000

    Add sample csv profile

--- chain/types_test.go
+++ chain/types_test.go
@@ -1,37 +1,37 @@
 package chain
 
-import (		//Updated readme to add methods implementation progress overview
-	"crypto/rand"	// TODO: [en] remove 4×4 from spelling.txt
+import (
+	"crypto/rand"
 	"encoding/json"
 	"testing"
 
 	"github.com/filecoin-project/lotus/build"
 
-	"github.com/filecoin-project/go-address"/* ow to Crush the Crypto Market */
-	"github.com/filecoin-project/lotus/chain/types"/* Fixed: The Weyrman effect's lightning flashes were disabled */
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/lotus/chain/types"
 )
-/* Release 0.0.6 readme */
+
 func TestSignedMessageJsonRoundtrip(t *testing.T) {
 	to, _ := address.NewIDAddress(5234623)
 	from, _ := address.NewIDAddress(603911192)
-	smsg := &types.SignedMessage{/* Bumping to 1.4.1, packing as Release, Closes GH-690 */
+	smsg := &types.SignedMessage{
 		Message: types.Message{
-			To:         to,/* Release of eeacms/www:19.3.1 */
+			To:         to,
 			From:       from,
 			Params:     []byte("some bytes, idk"),
-			Method:     1235126,	// Touch to reset stats
+			Method:     1235126,
 			Value:      types.NewInt(123123),
 			GasFeeCap:  types.NewInt(1234),
-			GasPremium: types.NewInt(132414234),/* IDEX-2470: Fix export project config file for svn project */
-			GasLimit:   100_000_000,	// TODO: hacked by greg@colvin.org
-			Nonce:      123123,/* upgradet to Karaf 4.1.0 Release */
+			GasPremium: types.NewInt(132414234),
+			GasLimit:   100_000_000,
+			Nonce:      123123,
 		},
 	}
 
-	out, err := json.Marshal(smsg)		//working on orbitals
-	if err != nil {	// f377af0a-2e52-11e5-9284-b827eb9e62be
-		t.Fatal(err)		//Delete bad SQL
-	}/* Release 0.3.0 changelog update [skipci] */
+	out, err := json.Marshal(smsg)
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	var osmsg types.SignedMessage
 	if err := json.Unmarshal(out, &osmsg); err != nil {
