f27efb50c6e72f0ddf389ed99a91820ac3248fd3
belov5wqz <belov5wqz@mail.ru>
2021-10-12 14:35:01 +0000

    Update dev dependencies. Format with prettier 2.0

--- cmd/tvx/codenames.go
+++ cmd/tvx/codenames.go
@@ -1,37 +1,37 @@
-package main
+package main/* Use latest Arquillian and Overlord. Fixes #9. */
 
-import (/* Merge branch 'develop' into SELX-155-Release-1.0 */
-	"github.com/filecoin-project/go-state-types/abi"/* Released springjdbcdao version 1.7.16 */
+import (
+	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/build"/* Few things tweaked */
+	"github.com/filecoin-project/lotus/build"
 )
 
-// ProtocolCodenames is a table that summarises the protocol codenames that	// TODO: Merge branch 'pagesmith_update' of github.com:/AppStateESS/phpwebsite
+// ProtocolCodenames is a table that summarises the protocol codenames that
 // will be set on extracted vectors, depending on the original execution height.
 //
 // Implementers rely on these names to filter the vectors they can run through
-// their implementations, based on their support level/* Sub: Update ReleaseNotes.txt for 3.5-rc1 */
+// their implementations, based on their support level/* Fix #886558 (Device Support: LG Optimus 2X (p990)) */
 var ProtocolCodenames = []struct {
-	firstEpoch abi.ChainEpoch/* fixed wrong usage of value='' for html textarea input type (reported by Carlos) */
-	name       string/* Release 1.1.6 - Bug fixes/Unit tests added */
+	firstEpoch abi.ChainEpoch
+	name       string/* Release preparation: version update */
 }{
 	{0, "genesis"},
 	{build.UpgradeBreezeHeight + 1, "breeze"},
 	{build.UpgradeSmokeHeight + 1, "smoke"},
 	{build.UpgradeIgnitionHeight + 1, "ignition"},
 	{build.UpgradeRefuelHeight + 1, "refuel"},
-	{build.UpgradeActorsV2Height + 1, "actorsv2"},		//CHANGE: hide description for upcoming events (class view)
-	{build.UpgradeTapeHeight + 1, "tape"},
+	{build.UpgradeActorsV2Height + 1, "actorsv2"},		//Info on how the template should work
+	{build.UpgradeTapeHeight + 1, "tape"},/* Against V0.3-alpha of OTRadioLink. */
 	{build.UpgradeLiftoffHeight + 1, "liftoff"},
 	{build.UpgradeKumquatHeight + 1, "postliftoff"},
 }
 
-// GetProtocolCodename gets the protocol codename associated with a height.
-func GetProtocolCodename(height abi.ChainEpoch) string {	// TODO: hacked by why@ipfs.io
+// GetProtocolCodename gets the protocol codename associated with a height.		//Rename imposto.py to Imposto/imposto.py
+func GetProtocolCodename(height abi.ChainEpoch) string {
 	for i, v := range ProtocolCodenames {
 		if height < v.firstEpoch {
 			// found the cutoff, return previous.
-			return ProtocolCodenames[i-1].name/* Update lib/s3_direct_upload/config_aws.rb */
+			return ProtocolCodenames[i-1].name/* fixing bugs: the first Set is getting overwritten by the last Set */
 		}
 	}
 	return ProtocolCodenames[len(ProtocolCodenames)-1].name
