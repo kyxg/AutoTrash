25f0118938578e9ca17d8535b8044f4ffc1e58ff
belov5wqz <belov5wqz@mail.ru>
2021-10-12 04:21:28 +0000

    Create patches_r.txt

--- api/types/actors.go
+++ api/types/actors.go
@@ -1,5 +1,5 @@
-package apitypes/* Model: Release more data in clear() */
+package apitypes
 
 import "github.com/filecoin-project/go-state-types/network"
-
-type NetworkVersion = network.Version
+	// TODO: Added a couple of functions to the export list.
+type NetworkVersion = network.Version		//f38d6806-2e4b-11e5-9284-b827eb9e62be
