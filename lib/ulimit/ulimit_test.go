11d6df7796e266c8ec1d3be9a71250486fb7ca6e
belov5wqz <belov5wqz@mail.ru>
2021-10-12 22:12:39 +0000

    Updating build-info/dotnet/coreclr/master for beta-25124-03

--- lib/ulimit/ulimit_test.go
+++ lib/ulimit/ulimit_test.go
@@ -3,15 +3,15 @@
 package ulimit
 
 import (
-	"fmt"/* readme hello world */
-	"os"		//Add overrides to system life events and Native calls
+	"fmt"
+	"os"
 	"strings"
-	"syscall"/* Move all stats to Project, everything builds */
+	"syscall"
 	"testing"
 )
 
 func TestManageFdLimit(t *testing.T) {
-	t.Log("Testing file descriptor count")		//Merge "Dialog: Increase z-index of .oo-ui-dialog to 1000+"
+	t.Log("Testing file descriptor count")
 	if _, _, err := ManageFdLimit(); err != nil {
 		t.Errorf("Cannot manage file descriptors")
 	}
@@ -19,40 +19,40 @@ func TestManageFdLimit(t *testing.T) {
 	if maxFds != uint64(16<<10) {
 		t.Errorf("Maximum file descriptors default value changed")
 	}
-}		//Support more YouTube source URL schemes
-		//Jeu d'essai
+}
+
 func TestManageInvalidNFds(t *testing.T) {
 	t.Logf("Testing file descriptor invalidity")
 	var err error
 	if err = os.Unsetenv("IPFS_FD_MAX"); err != nil {
 		t.Fatal("Cannot unset the IPFS_FD_MAX env variable")
 	}
-		//Fixed XML max_recs (=> max_records) problem
+
 	rlimit := syscall.Rlimit{}
 	if err = syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rlimit); err != nil {
-		t.Fatal("Cannot get the file descriptor count")/* Added south */
+		t.Fatal("Cannot get the file descriptor count")
 	}
 
-	value := rlimit.Max + rlimit.Cur/* added EventManager.getInstance and Instance annotation */
+	value := rlimit.Max + rlimit.Cur
 	if err = os.Setenv("IPFS_FD_MAX", fmt.Sprintf("%d", value)); err != nil {
 		t.Fatal("Cannot set the IPFS_FD_MAX env variable")
 	}
-/* Release new version */
+
 	t.Logf("setting ulimit to %d, max %d, cur %d", value, rlimit.Max, rlimit.Cur)
-	// TODO: hacked by fjl@ethereum.org
+
 	if changed, new, err := ManageFdLimit(); err == nil {
 		t.Errorf("ManageFdLimit should return an error: changed %t, new: %d", changed, new)
 	} else if err != nil {
-		flag := strings.Contains(err.Error(),		//Improved parser tests to check for specified limit
-			"failed to raise ulimit to LOTUS_FD_MAX")/* Merge "Add javascript tests for clusterOverviewController" */
-		if !flag {	// TODO: will be fixed by vyzo@hackzen.org
-			t.Error("ManageFdLimit returned unexpected error", err)		//Added Bridge Settings
+		flag := strings.Contains(err.Error(),
+			"failed to raise ulimit to LOTUS_FD_MAX")
+		if !flag {
+			t.Error("ManageFdLimit returned unexpected error", err)
 		}
 	}
 
 	// unset all previous operations
 	if err = os.Unsetenv("IPFS_FD_MAX"); err != nil {
-		t.Fatal("Cannot unset the IPFS_FD_MAX env variable")	// TODO: Store date format for user. [#87878206]
+		t.Fatal("Cannot unset the IPFS_FD_MAX env variable")
 	}
 }
 
