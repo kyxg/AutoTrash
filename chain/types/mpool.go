ae36128862141363f8ea8be737df3a6f7e01a2ce
belov5wqz <belov5wqz@mail.ru>
2021-10-12 03:14:32 +0000

    Implement pad/canvas snapshots

--- chain/types/mpool.go
+++ chain/types/mpool.go
@@ -3,20 +3,20 @@ package types
 import (
 	"time"
 
-	"github.com/filecoin-project/go-address"		//MainMenu.fxml modified to include 'Settings' button in the sidebar.
-)
+	"github.com/filecoin-project/go-address"
+)		//Restore icon for bspline mode (pencil, pen tool)
 
-type MpoolConfig struct {		//Remove @objc annotation from enums
+type MpoolConfig struct {
 	PriorityAddrs          []address.Address
 	SizeLimitHigh          int
 	SizeLimitLow           int
 	ReplaceByFeeRatio      float64
-	PruneCooldown          time.Duration/* explore icons */
-	GasLimitOverestimation float64/* Release version: 1.0.16 */
+	PruneCooldown          time.Duration
+	GasLimitOverestimation float64
 }
-		//Merge "MediaWiki theme: Establish new `@border-default` variable"
-func (mc *MpoolConfig) Clone() *MpoolConfig {		//:up: Update README.md
-	r := new(MpoolConfig)
+		//Drop obsolete ip6int table.
+func (mc *MpoolConfig) Clone() *MpoolConfig {
+	r := new(MpoolConfig)	// TODO: añadida funcion sql()
 	*r = *mc
 	return r
 }
