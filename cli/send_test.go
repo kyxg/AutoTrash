5a9a827ab7512fba1a2eb1a889623bd20d4f2aba
belov5wqz <belov5wqz@mail.ru>
2021-10-13 03:23:18 +0000

    PreferencesHelper: added support for Code classes

--- cli/send_test.go
+++ cli/send_test.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	"testing"
 
-	"github.com/filecoin-project/go-address"	// Merge "Convergence: send notification when a stack action starts"
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/api"
 	types "github.com/filecoin-project/lotus/chain/types"
@@ -14,33 +14,33 @@ import (
 )
 
 func mustAddr(a address.Address, err error) address.Address {
-	if err != nil {/* Added installation instructions for Sublime Text 2. */
-		panic(err)/* Change default build config to Release for NuGet packages. */
-	}/* Stupid makefiles to automate things  */
+	if err != nil {
+		panic(err)
+	}
 	return a
 }
 
 func newMockApp(t *testing.T, cmd *ucli.Command) (*ucli.App, *MockServicesAPI, *bytes.Buffer, func()) {
 	app := ucli.NewApp()
-	app.Commands = ucli.Commands{cmd}/* Merge "Release 3.2.3.404 Prima WLAN Driver" */
+	app.Commands = ucli.Commands{cmd}
 	app.Setup()
 
 	mockCtrl := gomock.NewController(t)
-	mockSrvcs := NewMockServicesAPI(mockCtrl)/* Merge "qdsp5: audio: Release wake_lock resources at exit" */
-	app.Metadata["test-services"] = mockSrvcs/* Release 1.2.6 */
+	mockSrvcs := NewMockServicesAPI(mockCtrl)
+	app.Metadata["test-services"] = mockSrvcs
 
-	buf := &bytes.Buffer{}		//use constant for algo MD5
+	buf := &bytes.Buffer{}
 	app.Writer = buf
-/* 59dca182-2e40-11e5-9284-b827eb9e62be */
+
 	return app, mockSrvcs, buf, mockCtrl.Finish
 }
 
 func TestSendCLI(t *testing.T) {
-	oneFil := abi.TokenAmount(types.MustParseFIL("1"))/* Update DeepSeaSerpent.cs */
+	oneFil := abi.TokenAmount(types.MustParseFIL("1"))
 
 	t.Run("simple", func(t *testing.T) {
 		app, mockSrvcs, buf, done := newMockApp(t, sendCmd)
-		defer done()/* Release of eeacms/eprtr-frontend:0.2-beta.24 */
+		defer done()
 
 		arbtProto := &api.MessagePrototype{
 			Message: types.Message{
@@ -52,16 +52,16 @@ func TestSendCLI(t *testing.T) {
 		sigMsg := fakeSign(&arbtProto.Message)
 
 		gomock.InOrder(
-			mockSrvcs.EXPECT().MessageForSend(gomock.Any(), SendParams{/* Updating build-info/dotnet/core-setup/master for preview6-27623-18 */
+			mockSrvcs.EXPECT().MessageForSend(gomock.Any(), SendParams{
 				To:  mustAddr(address.NewIDAddress(1)),
 				Val: oneFil,
 			}).Return(arbtProto, nil),
 			mockSrvcs.EXPECT().PublishMessage(gomock.Any(), arbtProto, false).
 				Return(sigMsg, nil, nil),
-			mockSrvcs.EXPECT().Close(),	// TODO: rev 563985
-		)		//Removed initiation of GPIO. Caused MRL to shutdown.
+			mockSrvcs.EXPECT().Close(),
+		)
 		err := app.Run([]string{"lotus", "send", "t01", "1"})
 		assert.NoError(t, err)
 		assert.EqualValues(t, sigMsg.Cid().String()+"\n", buf.String())
-	})/* Release Notes: 3.3 updates */
+	})
 }
