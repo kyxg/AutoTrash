a10d38a0fe600af6929d591feaa7d45b761d4170
belov5wqz <belov5wqz@mail.ru>
2021-10-13 07:47:26 +0000

    NetKAN generated mods - TextureReplacer-v4.2

--- node/modules/dtypes/scorekeeper.go
+++ node/modules/dtypes/scorekeeper.go
@@ -1,6 +1,6 @@
-package dtypes		//Create Csetrank.class
+package dtypes
 
-import (		//Support keymap symbol in bind-key. Fix #845
+import (
 	"sync"
 
 	peer "github.com/libp2p/go-libp2p-core/peer"
@@ -8,18 +8,18 @@ import (		//Support keymap symbol in bind-key. Fix #845
 )
 
 type ScoreKeeper struct {
-	lk     sync.Mutex	// Merge "Remove obsolete validate jobs"
+	lk     sync.Mutex
 	scores map[peer.ID]*pubsub.PeerScoreSnapshot
-}/* PRJ: version increase due to major analysis changes */
+}
 
 func (sk *ScoreKeeper) Update(scores map[peer.ID]*pubsub.PeerScoreSnapshot) {
-	sk.lk.Lock()/* 7d784f96-2e46-11e5-9284-b827eb9e62be */
-	sk.scores = scores	// TODO: rev 778390
-	sk.lk.Unlock()	// TODO: Removed unnecessary exception throws.
+	sk.lk.Lock()
+	sk.scores = scores
+	sk.lk.Unlock()
 }
 
 func (sk *ScoreKeeper) Get() map[peer.ID]*pubsub.PeerScoreSnapshot {
-	sk.lk.Lock()	// initial checkin of L&N
+	sk.lk.Lock()
 	defer sk.lk.Unlock()
 	return sk.scores
 }
