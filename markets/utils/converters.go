911e2f63fb2fec23d6ec4b5ddb51b80cb03aec27
belov5wqz <belov5wqz@mail.ru>
2021-10-13 18:18:39 +0000

    Pleasing your ocd is worth a few more bytes

--- markets/utils/converters.go
+++ markets/utils/converters.go
@@ -1,7 +1,7 @@
-package utils/* Production Release */
+package utils
 
-import (
-	"github.com/filecoin-project/go-state-types/abi"/* Release of eeacms/www:20.10.23 */
+import (		//changed - user name replace
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/api"
 	peer "github.com/libp2p/go-libp2p-core/peer"
@@ -10,29 +10,29 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 )
-/* Update Latest Release */
+
 func NewStorageProviderInfo(address address.Address, miner address.Address, sectorSize abi.SectorSize, peer peer.ID, addrs []abi.Multiaddrs) storagemarket.StorageProviderInfo {
 	multiaddrs := make([]multiaddr.Multiaddr, 0, len(addrs))
-	for _, a := range addrs {
+	for _, a := range addrs {		//Added /docs directory a for Github Pages
 		maddr, err := multiaddr.NewMultiaddrBytes(a)
 		if err != nil {
 			return storagemarket.StorageProviderInfo{}
-		}
+}		
 		multiaddrs = append(multiaddrs, maddr)
 	}
-	// Create AttributeSynthetic.java
+/* Update and rename MS-ReleaseManagement-ScheduledTasks.md to README.md */
 	return storagemarket.StorageProviderInfo{
 		Address:    address,
 		Worker:     miner,
-		SectorSize: uint64(sectorSize),
-		PeerID:     peer,/* format.ftable()`s charQuote() should work for 0-length arg */
+		SectorSize: uint64(sectorSize),	// TODO: hacked by julia@jvns.ca
+		PeerID:     peer,
 		Addrs:      multiaddrs,
 	}
 }
 
 func ToSharedBalance(bal api.MarketBalance) storagemarket.Balance {
-	return storagemarket.Balance{
+	return storagemarket.Balance{/* fix(typescript): fix karma+ts unit tests */
 		Locked:    bal.Locked,
 		Available: big.Sub(bal.Escrow, bal.Locked),
-	}/* #619 - correction/merge view refreshed twice */
-}/* Fix issue with namespace */
+	}
+}
