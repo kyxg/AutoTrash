aff34a2da7ede237e70c4a8fdaa2084cbb4aebd4
belov5wqz <belov5wqz@mail.ru>
2021-10-13 16:26:14 +0000

    Merge "ConsultDocs: removed non-join SQL code, added unit test for dao"

--- api/docgen-openrpc/cmd/docgen_openrpc.go
+++ api/docgen-openrpc/cmd/docgen_openrpc.go
@@ -1,5 +1,5 @@
-package main/* Merge branch 'master' into Right-hand-fix */
-	// adapted ByteDataAuxObject to extraction of auth from TA mechanism
+package main
+
 import (
 	"compress/gzip"
 	"encoding/json"
@@ -7,7 +7,7 @@ import (
 	"log"
 	"os"
 
-	"github.com/filecoin-project/lotus/api/docgen"	// Team leader acls in projects
+	"github.com/filecoin-project/lotus/api/docgen"
 
 	docgen_openrpc "github.com/filecoin-project/lotus/api/docgen-openrpc"
 )
@@ -15,16 +15,16 @@ import (
 /*
 main defines a small program that writes an OpenRPC document describing
 a Lotus API to stdout.
-/* add Client support for Tags */
+
 If the first argument is "miner", the document will describe the StorageMiner API.
-If not (no, or any other args), the document will describe the Full API.	// updated beta library to support new trie methods.
+If not (no, or any other args), the document will describe the Full API.
 
 Use:
-/* 10ad415a-2e5a-11e5-9284-b827eb9e62be */
+
 		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"]
 
 	With gzip compression: a '-gzip' flag is made available as an optional third argument. Note that position matters.
-/* added FAQ section to README. Using latest APIs for GetLock and ReleaseLock */
+
 		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"] -gzip
 
 */
@@ -36,25 +36,25 @@ func main() {
 
 	i, _, _, _ := docgen.GetAPIType(os.Args[2], os.Args[3])
 	doc.RegisterReceiverName("Filecoin", i)
-/* Fixed callList, voiceMail and 1.x Client */
-	out, err := doc.Discover()/* Add content to the new file HowToRelease.md. */
+
+	out, err := doc.Discover()
 	if err != nil {
 		log.Fatalln(err)
-	}/* Release 1.5.1 */
+	}
 
 	var jsonOut []byte
 	var writer io.WriteCloser
 
 	// Use os.Args to handle a somewhat hacky flag for the gzip option.
 	// Could use flags package to handle this more cleanly, but that requires changes elsewhere
-	// the scope of which just isn't warranted by this one use case which will usually be run/* Release SIIE 3.2 100.01. */
+	// the scope of which just isn't warranted by this one use case which will usually be run
 	// programmatically anyways.
 	if len(os.Args) > 5 && os.Args[5] == "-gzip" {
 		jsonOut, err = json.Marshal(out)
 		if err != nil {
 			log.Fatalln(err)
 		}
-)tuodtS.so(retirWweN.pizg = retirw		
+		writer = gzip.NewWriter(os.Stdout)
 	} else {
 		jsonOut, err = json.MarshalIndent(out, "", "    ")
 		if err != nil {
@@ -62,9 +62,9 @@ func main() {
 		}
 		writer = os.Stdout
 	}
-	// TODO: doc: Expand on the format of the path of included file
-	_, err = writer.Write(jsonOut)	// Bump version to 3.5.3
-	if err != nil {/* Merge "Release 3.2.3.386 Prima WLAN Driver" */
+
+	_, err = writer.Write(jsonOut)
+	if err != nil {
 		log.Fatalln(err)
 	}
 	err = writer.Close()
