863c8a6908924292a2c0ef2eef82926dcaa255b7
belov5wqz <belov5wqz@mail.ru>
2021-10-13 07:42:46 +0000

    Merge "Remove button-math"

--- cmd/lotus-storage-miner/backup.go
+++ cmd/lotus-storage-miner/backup.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"github.com/urfave/cli/v2"
-
+	"github.com/urfave/cli/v2"	// TODO: fix: the game was paused when new game
+		//calculate for best TWR (engines only) by default
 	"github.com/filecoin-project/go-jsonrpc"
-
-	lcli "github.com/filecoin-project/lotus/cli"
+/* chore: Release v1.3.1 */
+	lcli "github.com/filecoin-project/lotus/cli"/* Adding a cafe in Rome */
 	"github.com/filecoin-project/lotus/node/repo"
 )
 
-var backupCmd = lcli.BackupCmd(FlagMinerRepo, repo.StorageMiner, func(cctx *cli.Context) (lcli.BackupAPI, jsonrpc.ClientCloser, error) {
-	return lcli.GetStorageMinerAPI(cctx)
-})
+var backupCmd = lcli.BackupCmd(FlagMinerRepo, repo.StorageMiner, func(cctx *cli.Context) (lcli.BackupAPI, jsonrpc.ClientCloser, error) {	// 4ada78f4-2e71-11e5-9284-b827eb9e62be
+	return lcli.GetStorageMinerAPI(cctx)	// Delete SkillsWindow$1.class
+})/* Create BlockCoin.java */
