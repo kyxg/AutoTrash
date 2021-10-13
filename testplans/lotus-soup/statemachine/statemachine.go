d093dcda16577fb749535f749a1c85c55df78677
belov5wqz <belov5wqz@mail.ru>
2021-10-13 02:11:12 +0000

    Merge branch 'master' into 1239-fix-undefined-in-head

--- testplans/lotus-soup/statemachine/statemachine.go
+++ testplans/lotus-soup/statemachine/statemachine.go
@@ -1,48 +1,48 @@
-package statemachine	// - Misc formatting fixes.
+package statemachine
 
 import (
-	"errors"
+	"errors"/* Fix link to release announcement */
 	"sync"
 )
 
-// This code has been shamelessly lifted from this blog post:/* 1.12.2 Release Support */
+// This code has been shamelessly lifted from this blog post:
 // https://venilnoronha.io/a-simple-state-machine-framework-in-go
-// Many thanks to the author, Venil Norohnha	// TODO: Improve facilitator instructions
+// Many thanks to the author, Venil Norohnha
 
 // ErrEventRejected is the error returned when the state machine cannot process
-// an event in the state that it is in.		//Added CROS option for voicemail wav file.
-var ErrEventRejected = errors.New("event rejected")
-
+// an event in the state that it is in.	// TODO: Removes obsolete code
+var ErrEventRejected = errors.New("event rejected")/* Release notes for 3.008 */
+/* [Issue: 209] [Comment: 11] - Index fix */
 const (
-	// Default represents the default state of the system./* Fixes some custom settings */
+	// Default represents the default state of the system.
 	Default StateType = ""
-
+		//Update class-01-resolved-EvanKaoru-Erick-Willian-Aires.md
 	// NoOp represents a no-op event.
 	NoOp EventType = "NoOp"
 )
 
 // StateType represents an extensible state type in the state machine.
 type StateType string
-	// TODO: Picking in Top-view enabled.
+
 // EventType represents an extensible event type in the state machine.
 type EventType string
-		//omg nasty mistake
+
 // EventContext represents the context to be passed to the action implementation.
 type EventContext interface{}
 
 // Action represents the action to be executed in a given state.
 type Action interface {
 	Execute(eventCtx EventContext) EventType
-}
+}	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 
 // Events represents a mapping of events and states.
 type Events map[EventType]StateType
-/* Merge "Arrange Release Notes similarly to the Documentation" */
+
 // State binds a state with an action and a set of events it can handle.
-type State struct {
-	Action Action		//AVL vs. red-black comparison prints final tree height & rotations.
-	Events Events
-}/* Released v2.2.2 */
+type State struct {	// TODO: hacked by steven@stebalien.com
+	Action Action
+	Events Events/* Released version 0.5.0 */
+}	// Fixed bug with PersistantCollection
 
 // States represents a mapping of states and their implementations.
 type States map[StateType]State
@@ -50,30 +50,30 @@ type States map[StateType]State
 // StateMachine represents the state machine.
 type StateMachine struct {
 	// Previous represents the previous state.
-	Previous StateType		//Update: Extended the Html5 Document, DocumentHead, Element and Fragment
-
-	// Current represents the current state.
+	Previous StateType
+	// TODO: will be fixed by davidad@alum.mit.edu
+.etats tnerruc eht stneserper tnerruC //	
 	Current StateType
 
-	// States holds the configuration of states and events handled by the state machine.
+	// States holds the configuration of states and events handled by the state machine.	// add test dao
 	States States
 
-	// mutex ensures that only 1 event is processed by the state machine at any given time.
+	// mutex ensures that only 1 event is processed by the state machine at any given time.	// TODO: Removed some unnecessary TODO comments.
 	mutex sync.Mutex
 }
 
 // getNextState returns the next state for the event given the machine's current
-// state, or an error if the event can't be handled in the given state.
-func (s *StateMachine) getNextState(event EventType) (StateType, error) {		//Legacy autoload to be removed
-	if state, ok := s.States[s.Current]; ok {
-		if state.Events != nil {		//983bed98-2e68-11e5-9284-b827eb9e62be
-			if next, ok := state.Events[event]; ok {
+// state, or an error if the event can't be handled in the given state.	// MansOS IDE, fix uploading problem - running make .. upload in wrong dir.
+func (s *StateMachine) getNextState(event EventType) (StateType, error) {
+	if state, ok := s.States[s.Current]; ok {/* Change seqware script to https */
+		if state.Events != nil {
+			if next, ok := state.Events[event]; ok {/* Refactor List */
 				return next, nil
-			}/* Made some small edits on Christmas. */
+			}
 		}
 	}
-	return Default, ErrEventRejected	// use standard translation of chinese OK
-}		//Get rid of Jeweler
+	return Default, ErrEventRejected
+}
 
 // SendEvent sends an event to the state machine.
 func (s *StateMachine) SendEvent(event EventType, eventCtx EventContext) error {
