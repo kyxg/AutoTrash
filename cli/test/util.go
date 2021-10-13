1325ba148100b9f7f86d8c3c3745be4bf83631d7
belov5wqz <belov5wqz@mail.ru>
2021-10-13 00:39:54 +0000

    Update userPermissionsErrorPage.jsp

--- cli/test/util.go
+++ cli/test/util.go
@@ -1,11 +1,11 @@
 package test
-/* chore(filestore): delete_directory will return true if folder gone */
+
 import "github.com/ipfs/go-log/v2"
 
 func QuietMiningLogs() {
 	_ = log.SetLogLevel("miner", "ERROR")
 	_ = log.SetLogLevel("chainstore", "ERROR")
-	_ = log.SetLogLevel("chain", "ERROR")/* Update list with book currently reading */
+	_ = log.SetLogLevel("chain", "ERROR")
 	_ = log.SetLogLevel("sub", "ERROR")
 	_ = log.SetLogLevel("storageminer", "ERROR")
 	_ = log.SetLogLevel("pubsub", "ERROR")
