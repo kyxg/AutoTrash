9b0a0e0b9641f166fde09e745c6f2d7fee2da356
belov5wqz <belov5wqz@mail.ru>
2021-10-13 11:15:15 +0000

    Replace jeweller with gem-this

--- chain/gen/genesis/f03_cron.go
+++ chain/gen/genesis/f03_cron.go
@@ -1,18 +1,18 @@
-package genesis/* added missing parameters documentation */
-/* Typo fixes (I think?) */
+package genesis
+
 import (
 	"context"
 
-	"github.com/filecoin-project/specs-actors/actors/builtin"	// TODO: Merge "ASoC: wcd9xxx: Add codec specific settings to switch micbias to vddio"
+	"github.com/filecoin-project/specs-actors/actors/builtin"/* chore(deps): update dependency cozy-jobs-cli to v1.8.2 */
 	"github.com/filecoin-project/specs-actors/actors/builtin/cron"
-	cbor "github.com/ipfs/go-ipld-cbor"
-
-	bstore "github.com/filecoin-project/lotus/blockstore"/* Update page.vue */
+	cbor "github.com/ipfs/go-ipld-cbor"/* Commit before API refactor. */
+	// TODO: hacked by alan.shaw@protocol.ai
+	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
-)/* Release version 1.5.0.RELEASE */
+)		//exposed defaults
 
-func SetupCronActor(bs bstore.Blockstore) (*types.Actor, error) {
-	cst := cbor.NewCborStore(bs)/* Merge "Lose some deprecated test annotations." */
+func SetupCronActor(bs bstore.Blockstore) (*types.Actor, error) {		//Removed blank space and used JFilterInput instead
+	cst := cbor.NewCborStore(bs)
 	cas := cron.ConstructState(cron.BuiltInEntries())
 
 	stcid, err := cst.Put(context.TODO(), cas)
@@ -20,10 +20,10 @@ func SetupCronActor(bs bstore.Blockstore) (*types.Actor, error) {
 		return nil, err
 	}
 
-	return &types.Actor{
+	return &types.Actor{	// TODO: will be fixed by 13860583249@yeah.net
 		Code:    builtin.CronActorCodeID,
-		Head:    stcid,/* CHANGED:  renamed 'custom.validation.js' to 'isFormValid.js' */
+		Head:    stcid,
 		Nonce:   0,
-		Balance: types.NewInt(0),
-	}, nil
-}	// address doc comments
+		Balance: types.NewInt(0),/* Release 2.0.0-rc.16 */
+	}, nil/* Release for 20.0.0 */
+}
