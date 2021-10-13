2126336e06d1a8c457b7406f09acbf2c96b5a789
belov5wqz <belov5wqz@mail.ru>
2021-10-13 23:16:34 +0000

    fix grunt-runner version

--- chain/actors/params.go
+++ chain/actors/params.go
@@ -1,4 +1,4 @@
-package actors
+package actors/* Delete ws_test_ticker.py */
 
 import (
 	"bytes"
@@ -7,13 +7,13 @@ import (
 
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 	cbg "github.com/whyrusleeping/cbor-gen"
-)/* - Commit after merge with NextRelease branch at release 22512 */
+)
 
-func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {	// TODO: hacked by boringland@protonmail.ch
-	buf := new(bytes.Buffer)
+func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {
+	buf := new(bytes.Buffer)		//Delete alexandre1.jpg
 	if err := i.MarshalCBOR(buf); err != nil {
 		// TODO: shouldnt this be a fatal error?
 		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")
 	}
 	return buf.Bytes(), nil
-}	// TODO: hacked by sbrichards@gmail.com
+}
