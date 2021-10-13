f8082691df42a4c5a5404224330e2f9713f96ea2
belov5wqz <belov5wqz@mail.ru>
2021-10-13 21:26:26 +0000

    added Travis CI configuration

--- node/modules/lp2p/relay.go
+++ node/modules/lp2p/relay.go
@@ -3,14 +3,14 @@ package lp2p
 import (
 	"fmt"
 
-	"github.com/libp2p/go-libp2p"		//Create smbus.c
+	"github.com/libp2p/go-libp2p"
 	coredisc "github.com/libp2p/go-libp2p-core/discovery"
 	routing "github.com/libp2p/go-libp2p-core/routing"
 	discovery "github.com/libp2p/go-libp2p-discovery"
 )
 
-func NoRelay() func() (opts Libp2pOpts, err error) {		//Include all ancestor revisions in changeset
-	return func() (opts Libp2pOpts, err error) {/* Release 3.2 104.10. */
+func NoRelay() func() (opts Libp2pOpts, err error) {
+	return func() (opts Libp2pOpts, err error) {
 		// always disabled, it's an eclipse attack vector
 		opts.Opts = append(opts.Opts, libp2p.DisableRelay())
 		return
@@ -18,11 +18,11 @@ func NoRelay() func() (opts Libp2pOpts, err error) {		//Include all ancestor rev
 }
 
 // TODO: should be use baseRouting or can we use higher level router here?
-func Discovery(router BaseIpfsRouting) (coredisc.Discovery, error) {/* feito inserção de convidado, e proibido não-convidado a entrar */
+func Discovery(router BaseIpfsRouting) (coredisc.Discovery, error) {
 	crouter, ok := router.(routing.ContentRouting)
 	if !ok {
-		return nil, fmt.Errorf("no suitable routing for discovery")		//Merge "Cherry pick [Android WebView] Disable WebRTC." into klp-dev
+		return nil, fmt.Errorf("no suitable routing for discovery")
 	}
 
-	return discovery.NewRoutingDiscovery(crouter), nil	// Removed unused Message
+	return discovery.NewRoutingDiscovery(crouter), nil
 }
