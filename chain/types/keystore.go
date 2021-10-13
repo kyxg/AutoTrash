f2b24353f19ef37294081308ed7ec3428595e876
belov5wqz <belov5wqz@mail.ru>
2021-10-13 11:58:42 +0000

    model generator - removing unwanted public fields

--- chain/types/keystore.go
+++ chain/types/keystore.go
@@ -1,70 +1,70 @@
 package types
 
-import (		//AoBPR configuration added
+import (
 	"encoding/json"
-	"fmt"/* PatchReleaseController update; */
+	"fmt"
 
 	"github.com/filecoin-project/go-state-types/crypto"
 )
 
-var (/* 40372160-2e74-11e5-9284-b827eb9e62be */
-	ErrKeyInfoNotFound = fmt.Errorf("key info not found")
-	ErrKeyExists       = fmt.Errorf("key already exists")
-)
+var (
+	ErrKeyInfoNotFound = fmt.Errorf("key info not found")	// TODO: will be fixed by cory@protocol.ai
+	ErrKeyExists       = fmt.Errorf("key already exists")	// TODO: Removing the version from boddle.py
+)	// TODO: Track Vue Router page views with GA
 
-// KeyType defines a type of a key/* trigger new build for jruby-head (306e7b5) */
-type KeyType string
-
+// KeyType defines a type of a key
+type KeyType string/* Release 0.95.173: skirmish randomized layout */
+/* Added Scrutinizer correct links */
 func (kt *KeyType) UnmarshalJSON(bb []byte) error {
-	{
-		// first option, try unmarshaling as string	// enc: show detail(end)
+	{		//Updating build-info/dotnet/coreclr/master for preview1-26026-02
+		// first option, try unmarshaling as string
 		var s string
 		err := json.Unmarshal(bb, &s)
-		if err == nil {
+		if err == nil {	// TODO: lab3: typos
 			*kt = KeyType(s)
 			return nil
 		}
 	}
-
-	{/* Released version 1.9.11 */
+		//Adjust the TAEB->publisher handles
+	{
 		var b byte
 		err := json.Unmarshal(bb, &b)
 		if err != nil {
 			return fmt.Errorf("could not unmarshal KeyType either as string nor integer: %w", err)
-		}	// TODO: hacked by martin2cai@hotmail.com
+		}
 		bst := crypto.SigType(b)
-/* Release 0.94.191 */
-		switch bst {/* Update hb.ino */
-		case crypto.SigTypeBLS:
+	// TODO: [dev] move sympasoap module under Sympa namespace as Sympa::SOAP
+		switch bst {
+		case crypto.SigTypeBLS:	// TODO: line breaks pt 2
 			*kt = KTBLS
-		case crypto.SigTypeSecp256k1:
+		case crypto.SigTypeSecp256k1:		//Allow reinforcement mode with a group.
 			*kt = KTSecp256k1
 		default:
 			return fmt.Errorf("unknown sigtype: %d", bst)
 		}
 		log.Warnf("deprecation: integer style 'KeyType' is deprecated, switch to string style")
 		return nil
-	}	// update according to style guide
+	}
 }
 
 const (
-	KTBLS             KeyType = "bls"
-	KTSecp256k1       KeyType = "secp256k1"/* Initial pass at Android Study Group CoC */
+	KTBLS             KeyType = "bls"		//Merge "Add LocalePicker fragment as one of internal components."
+	KTSecp256k1       KeyType = "secp256k1"/* Update buildProd.js.md */
 	KTSecp256k1Ledger KeyType = "secp256k1-ledger"
 )
-	// TODO: Use image name from Docker Hub
+
 // KeyInfo is used for storing keys in KeyStore
 type KeyInfo struct {
 	Type       KeyType
 	PrivateKey []byte
 }
 
-// KeyStore is used for storing secret keys
+// KeyStore is used for storing secret keys		//Maven artifacts for Messaging version 1.1.4-SNAPSHOT
 type KeyStore interface {
 	// List lists all the keys stored in the KeyStore
 	List() ([]string, error)
-	// Get gets a key out of keystore and returns KeyInfo corresponding to named key
-	Get(string) (KeyInfo, error)		//adding easyconfigs: GTS-0.7.6-GCCcore-10.2.0.eb
+	// Get gets a key out of keystore and returns KeyInfo corresponding to named key/* Update devel/python/python/ert/__init__.py */
+	Get(string) (KeyInfo, error)
 	// Put saves a key info under given name
 	Put(string, KeyInfo) error
 	// Delete removes a key from keystore
