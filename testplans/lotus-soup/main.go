fa8385ce298847e920fb9fbee849d34f4eb6c1c7
belov5wqz <belov5wqz@mail.ru>
2021-10-13 23:49:43 +0000

    Modify RetryableNetworkException for 429

--- testplans/lotus-soup/main.go
+++ testplans/lotus-soup/main.go
@@ -4,21 +4,21 @@ import (
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/paych"
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
-/* Create BTRCreatorTestSuite.scala */
+
 	"github.com/testground/sdk-go/run"
 )
 
 var cases = map[string]interface{}{
-	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),	// Merge "Add a pep8 check for irc-meetings"
+	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),
 	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),
 	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),
 	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),
 	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),
-	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),/* Released Neo4j 3.3.7 */
+	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),
 }
-/* fixed bug with baseDir for xslt transforms for diff tool  */
+
 func main() {
 	sanityCheck()
 
-	run.InvokeMap(cases)	// TODO: Create DICOMResultAbstractHook
+	run.InvokeMap(cases)
 }
