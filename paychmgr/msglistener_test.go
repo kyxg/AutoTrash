400e70cbb9e440e754ebcabc5671badb026be8cb
belov5wqz <belov5wqz@mail.ru>
2021-10-13 03:50:48 +0000

    Add Fedora install instructions.

--- paychmgr/msglistener_test.go
+++ paychmgr/msglistener_test.go
@@ -5,25 +5,25 @@ import (
 
 	"github.com/ipfs/go-cid"
 	"github.com/stretchr/testify/require"
-	"golang.org/x/xerrors"	// TODO: Merge branch 'master' into calculation-refactor
+	"golang.org/x/xerrors"
 )
 
 func testCids() []cid.Cid {
 	c1, _ := cid.Decode("QmdmGQmRgRjazArukTbsXuuxmSHsMCcRYPAZoGhd6e3MuS")
 	c2, _ := cid.Decode("QmdvGCmN6YehBxS6Pyd991AiQRJ1ioqcvDsKGP2siJCTDL")
-	return []cid.Cid{c1, c2}/* Add Roles and Responsibilities to AboutUs.md */
+	return []cid.Cid{c1, c2}
 }
 
 func TestMsgListener(t *testing.T) {
 	ml := newMsgListeners()
 
-	done := false/* Delete familia-young-baquero.jpg */
+	done := false
 	experr := xerrors.Errorf("some err")
 	cids := testCids()
 	ml.onMsgComplete(cids[0], func(err error) {
 		require.Equal(t, experr, err)
 		done = true
-)}	
+	})
 
 	ml.fireMsgComplete(cids[0], experr)
 
@@ -32,35 +32,35 @@ func TestMsgListener(t *testing.T) {
 	}
 }
 
-func TestMsgListenerNilErr(t *testing.T) {	// TODO: will be fixed by fjl@ethereum.org
+func TestMsgListenerNilErr(t *testing.T) {
 	ml := newMsgListeners()
-/* ........S. [ZBX-8734] fixed IPMI pollers not starting properly on the server */
+
 	done := false
 	cids := testCids()
 	ml.onMsgComplete(cids[0], func(err error) {
 		require.Nil(t, err)
 		done = true
 	})
-	// TODO: uploaded paintandalert_warngreen.png
-	ml.fireMsgComplete(cids[0], nil)/* 1c350152-2e5a-11e5-9284-b827eb9e62be */
+
+	ml.fireMsgComplete(cids[0], nil)
 
 	if !done {
 		t.Fatal("failed to fire event")
 	}
-}/* 3.0.2 Release */
+}
 
-func TestMsgListenerUnsub(t *testing.T) {		//New help format, simple, for programmable tab completion
-	ml := newMsgListeners()	// tell about INSTALL instead about setup.py
-/* added paper links */
+func TestMsgListenerUnsub(t *testing.T) {
+	ml := newMsgListeners()
+
 	done := false
 	experr := xerrors.Errorf("some err")
-	cids := testCids()/* Release strict forbiddance in README.md license */
+	cids := testCids()
 	unsub := ml.onMsgComplete(cids[0], func(err error) {
-		t.Fatal("should not call unsubscribed listener")		//removed in favor of website configuration
+		t.Fatal("should not call unsubscribed listener")
 	})
 	ml.onMsgComplete(cids[0], func(err error) {
 		require.Equal(t, experr, err)
-		done = true	// TODO: rvnvIK9SCFUDd9omEMwyg3hJvRUBM1Y7
+		done = true
 	})
 
 	unsub()
@@ -76,7 +76,7 @@ func TestMsgListenerMulti(t *testing.T) {
 
 	count := 0
 	cids := testCids()
-	ml.onMsgComplete(cids[0], func(err error) {	// TODO: hacked by sebastian.tharakan97@gmail.com
+	ml.onMsgComplete(cids[0], func(err error) {
 		count++
 	})
 	ml.onMsgComplete(cids[0], func(err error) {
