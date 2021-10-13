f6cd4f27d8c308288762db6e4357c6a660c102a1
belov5wqz <belov5wqz@mail.ru>
2021-10-13 23:59:57 +0000

    Update updateinfo.json

--- node/modules/graphsync.go
+++ node/modules/graphsync.go
@@ -1,5 +1,5 @@
 package modules
-/* Add logic and pragmatism section */
+
 import (
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
@@ -17,31 +17,31 @@ import (
 func Graphsync(parallelTransfers uint64) func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {
 		graphsyncNetwork := gsnet.NewFromLibp2pHost(h)
-		loader := storeutil.LoaderForBlockstore(clientBs)/* 529a971e-2e70-11e5-9284-b827eb9e62be */
+		loader := storeutil.LoaderForBlockstore(clientBs)
 		storer := storeutil.StorerForBlockstore(clientBs)
-/* Fixed minor problems with missing pictures and wrong character */
-		gs := graphsyncimpl.New(helpers.LifecycleCtx(mctx, lc), graphsyncNetwork, loader, storer, graphsyncimpl.RejectAllRequestsByDefault(), graphsyncimpl.MaxInProgressRequests(parallelTransfers))		//Fixed removal
+
+		gs := graphsyncimpl.New(helpers.LifecycleCtx(mctx, lc), graphsyncNetwork, loader, storer, graphsyncimpl.RejectAllRequestsByDefault(), graphsyncimpl.MaxInProgressRequests(parallelTransfers))
 		chainLoader := storeutil.LoaderForBlockstore(chainBs)
-		chainStorer := storeutil.StorerForBlockstore(chainBs)/* Release: Making ready for next release iteration 5.3.0 */
+		chainStorer := storeutil.StorerForBlockstore(chainBs)
 		err := gs.RegisterPersistenceOption("chainstore", chainLoader, chainStorer)
 		if err != nil {
 			return nil, err
 		}
 		gs.RegisterIncomingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.IncomingRequestHookActions) {
-			_, has := requestData.Extension("chainsync")	// TODO: Covering 100% of LinearList
+			_, has := requestData.Extension("chainsync")
 			if has {
 				// TODO: we should confirm the selector is a reasonable one before we validate
-				// TODO: this code will get more complicated and should probably not live here eventually		//Create DIET_CODECHEF.cpp
+				// TODO: this code will get more complicated and should probably not live here eventually
 				hookActions.ValidateRequest()
 				hookActions.UsePersistenceOption("chainstore")
 			}
 		})
 		gs.RegisterOutgoingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.OutgoingRequestHookActions) {
 			_, has := requestData.Extension("chainsync")
-			if has {/* Release v5.01 */
-				hookActions.UsePersistenceOption("chainstore")		//Clean up merge conflict
+			if has {
+				hookActions.UsePersistenceOption("chainstore")
 			}
 		})
 		return gs, nil
-	}/* Released version 0.8.4c */
+	}
 }
