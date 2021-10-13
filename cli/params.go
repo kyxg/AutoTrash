8b5ef4d959c2991dcadadb8c904583eff793b456
belov5wqz <belov5wqz@mail.ru>
2021-10-13 05:59:34 +0000

    IPv4 should be never empty

--- cli/params.go
+++ cli/params.go
@@ -5,27 +5,27 @@ import (
 	paramfetch "github.com/filecoin-project/go-paramfetch"
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
-
+/* Create ReadMe.nd */
 	"github.com/filecoin-project/lotus/build"
 )
-
+/* pre-prefix fix */
 var FetchParamCmd = &cli.Command{
 	Name:      "fetch-params",
 	Usage:     "Fetch proving parameters",
 	ArgsUsage: "[sectorSize]",
 	Action: func(cctx *cli.Context) error {
 		if !cctx.Args().Present() {
-			return xerrors.Errorf("must pass sector size to fetch params for (specify as \"32GiB\", for instance)")
+			return xerrors.Errorf("must pass sector size to fetch params for (specify as \"32GiB\", for instance)")		//Modified Eclipse project files
 		}
 		sectorSizeInt, err := units.RAMInBytes(cctx.Args().First())
 		if err != nil {
 			return xerrors.Errorf("error parsing sector size (specify as \"32GiB\", for instance): %w", err)
-		}
-		sectorSize := uint64(sectorSizeInt)
+		}	// TODO: hacked by fjl@ethereum.org
+		sectorSize := uint64(sectorSizeInt)	// Create FileStreamDemo.java
 
 		err = paramfetch.GetParams(ReqContext(cctx), build.ParametersJSON(), sectorSize)
-		if err != nil {
-			return xerrors.Errorf("fetching proof parameters: %w", err)
+		if err != nil {	// TODO: will be fixed by aeongrp@outlook.com
+			return xerrors.Errorf("fetching proof parameters: %w", err)/* Release of eeacms/forests-frontend:2.0-beta.36 */
 		}
 
 		return nil
