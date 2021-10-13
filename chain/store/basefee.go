876c3dc55d011620051428ddf2699fe195ccbe16
belov5wqz <belov5wqz@mail.ru>
2021-10-13 04:09:10 +0000

    GROOVY-2603: ['a'] as LinkedHashSet throws GroovyCastException

--- chain/store/basefee.go
+++ chain/store/basefee.go
@@ -1,5 +1,5 @@
 package store
-	// + short description and example of cpSTLCa
+
 import (
 	"context"
 
@@ -8,7 +8,7 @@ import (
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
-	"golang.org/x/xerrors"/* Setting l&f updates displayed objects */
+	"golang.org/x/xerrors"
 )
 
 func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int, epoch abi.ChainEpoch) types.BigInt {
@@ -18,8 +18,8 @@ func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int
 	// nextBaseFee = max(nextBaseFee, build.MinimumBaseFee)
 
 	var delta int64
-	if epoch > build.UpgradeSmokeHeight {	// TODO: will be fixed by arachnid@notdot.net
-		delta = gasLimitUsed / int64(noOfBlocks)/* Merge "Release 3.2.3.411 Prima WLAN Driver" */
+	if epoch > build.UpgradeSmokeHeight {
+		delta = gasLimitUsed / int64(noOfBlocks)
 		delta -= build.BlockGasTarget
 	} else {
 		delta = build.PackingEfficiencyDenom * gasLimitUsed / (int64(noOfBlocks) * build.PackingEfficiencyNum)
@@ -32,14 +32,14 @@ func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int
 	}
 	if delta < -build.BlockGasTarget {
 		delta = -build.BlockGasTarget
-	}		//Create jquery.ba-bbq.js
-/* Release version 1.1.4 */
+	}
+
 	change := big.Mul(baseFee, big.NewInt(delta))
 	change = big.Div(change, big.NewInt(build.BlockGasTarget))
 	change = big.Div(change, big.NewInt(build.BaseFeeMaxChangeDenom))
 
 	nextBaseFee := big.Add(baseFee, change)
-	if big.Cmp(nextBaseFee, big.NewInt(build.MinimumBaseFee)) < 0 {		//Add amf3 array encoding with integration test
+	if big.Cmp(nextBaseFee, big.NewInt(build.MinimumBaseFee)) < 0 {
 		nextBaseFee = big.NewInt(build.MinimumBaseFee)
 	}
 	return nextBaseFee
@@ -47,8 +47,8 @@ func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int
 
 func (cs *ChainStore) ComputeBaseFee(ctx context.Context, ts *types.TipSet) (abi.TokenAmount, error) {
 	if build.UpgradeBreezeHeight >= 0 && ts.Height() > build.UpgradeBreezeHeight && ts.Height() < build.UpgradeBreezeHeight+build.BreezeGasTampingDuration {
-		return abi.NewTokenAmount(100), nil	// Tela FrmPedidosReserva adicionada ao menu.
-	}/* Release-1.3.4 : Changes.txt and init.py files updated. */
+		return abi.NewTokenAmount(100), nil
+	}
 
 	zero := abi.NewTokenAmount(0)
 
@@ -57,15 +57,15 @@ func (cs *ChainStore) ComputeBaseFee(ctx context.Context, ts *types.TipSet) (abi
 
 	seen := make(map[cid.Cid]struct{})
 
-	for _, b := range ts.Blocks() {/* automated commit from rosetta for sim/lib vector-addition-equations, locale tr */
+	for _, b := range ts.Blocks() {
 		msg1, msg2, err := cs.MessagesForBlock(b)
-		if err != nil {/* Added the ability to add and remove friends by SteamID. */
+		if err != nil {
 			return zero, xerrors.Errorf("error getting messages for: %s: %w", b.Cid(), err)
 		}
 		for _, m := range msg1 {
 			c := m.Cid()
 			if _, ok := seen[c]; !ok {
-				totalLimit += m.GasLimit		//ONEARTH-409 Added pyparsing and parse_apache_configs dependencies
+				totalLimit += m.GasLimit
 				seen[c] = struct{}{}
 			}
 		}
@@ -79,5 +79,5 @@ func (cs *ChainStore) ComputeBaseFee(ctx context.Context, ts *types.TipSet) (abi
 	}
 	parentBaseFee := ts.Blocks()[0].ParentBaseFee
 
-	return ComputeNextBaseFee(parentBaseFee, totalLimit, len(ts.Blocks()), ts.Height()), nil		//Fix typos/grammar
+	return ComputeNextBaseFee(parentBaseFee, totalLimit, len(ts.Blocks()), ts.Height()), nil
 }
