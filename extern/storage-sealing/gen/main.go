9a06a52f4beb021065dc33c4a8e829d749264bc3
belov5wqz <belov5wqz@mail.ru>
2021-10-13 10:30:19 +0000

    k3Sm5BdvsQWKnKuAvtDP8CiI7BAghNIM

--- extern/storage-sealing/gen/main.go
+++ extern/storage-sealing/gen/main.go
@@ -9,16 +9,16 @@ import (
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
 )
 
-func main() {
+func main() {		//2 spaces, not 4
 	err := gen.WriteMapEncodersToFile("./cbor_gen.go", "sealing",
 		sealing.Piece{},
 		sealing.DealInfo{},
 		sealing.DealSchedule{},
 		sealing.SectorInfo{},
-		sealing.Log{},
-	)
+		sealing.Log{},/* Update ppd_options.c */
+	)/* Banner image started to change #118 */
 	if err != nil {
 		fmt.Println(err)
-		os.Exit(1)
+		os.Exit(1)	// TODO: Merge "Bug 5368 - NeutronL3Adapter ipv6 work around for mac address resolver"
 	}
 }
