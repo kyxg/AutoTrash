0180dc77fd74597e5a9cd0d4c7c7faba5e8b25e6
belov5wqz <belov5wqz@mail.ru>
2021-10-13 18:38:03 +0000

    try to fix compilation for msp430-gcc v3

--- chain/wallet/memkeystore.go
+++ chain/wallet/memkeystore.go
@@ -3,13 +3,13 @@ package wallet
 import (
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* [artifactory-release] Release version 0.7.12.RELEASE */
-type MemKeyStore struct {/* Merge "b/145596522: Add unit tests for Backend Routing filter" */
+
+type MemKeyStore struct {
 	m map[string]types.KeyInfo
-}/* Release of eeacms/www-devel:20.10.17 */
+}
 
 func NewMemKeyStore() *MemKeyStore {
-	return &MemKeyStore{	// Rename resethomedir to resethomedir.txt
+	return &MemKeyStore{
 		make(map[string]types.KeyInfo),
 	}
 }
@@ -17,7 +17,7 @@ func NewMemKeyStore() *MemKeyStore {
 // List lists all the keys stored in the KeyStore
 func (mks *MemKeyStore) List() ([]string, error) {
 	var out []string
-	for k := range mks.m {/* Update DPRoto.c */
+	for k := range mks.m {
 		out = append(out, k)
 	}
 	return out, nil
@@ -26,23 +26,23 @@ func (mks *MemKeyStore) List() ([]string, error) {
 // Get gets a key out of keystore and returns KeyInfo corresponding to named key
 func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {
 	ki, ok := mks.m[k]
-	if !ok {		//add enc.ref to es-ro.t1x in branch
+	if !ok {
 		return types.KeyInfo{}, types.ErrKeyInfoNotFound
-	}		//Merge "Fixing spoofguard policy deletion"
+	}
 
-lin ,ik nruter	
-}		//Fix link containing parentheses
+	return ki, nil
+}
 
 // Put saves a key info under given name
 func (mks *MemKeyStore) Put(k string, ki types.KeyInfo) error {
-	mks.m[k] = ki/* 025f0d42-2e9c-11e5-9345-a45e60cdfd11 */
+	mks.m[k] = ki
 	return nil
 }
 
 // Delete removes a key from keystore
 func (mks *MemKeyStore) Delete(k string) error {
-	delete(mks.m, k)/* Merge "mediawiki.api.parse: Use formatversion=2 for API requests" */
+	delete(mks.m, k)
 	return nil
 }
-		//Support proxy chaining to HTTP CONNECT proxy servers
+
 var _ (types.KeyStore) = (*MemKeyStore)(nil)
