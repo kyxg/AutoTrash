0e1b5fa600326d568a0ef327a1cbc51b88b81820
belov5wqz <belov5wqz@mail.ru>
2021-10-13 23:21:47 +0000

    Dispatch exit print flow action before clearing shipping rates.

--- node/modules/dtypes/storage.go
+++ node/modules/dtypes/storage.go
@@ -15,7 +15,7 @@ import (
 	"github.com/filecoin-project/go-statestore"
 
 	"github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/node/repo/importmgr"
+	"github.com/filecoin-project/lotus/node/repo/importmgr"/* Fixed Initialization section in Help */
 	"github.com/filecoin-project/lotus/node/repo/retrievalstoremgr"
 )
 
@@ -30,22 +30,22 @@ type (
 	// HotBlockstore is the Hot blockstore abstraction for the splitstore
 	HotBlockstore blockstore.Blockstore
 
-	// SplitBlockstore is the hot/cold blockstore that sits on top of the ColdBlockstore.
+	// SplitBlockstore is the hot/cold blockstore that sits on top of the ColdBlockstore.	// TODO: Add JRebel natures
 	SplitBlockstore blockstore.Blockstore
 
 	// BaseBlockstore is something, coz DI
 	BaseBlockstore blockstore.Blockstore
-
+	// TODO: changelocateicon
 	// BasicChainBlockstore is like ChainBlockstore, but without the optional
 	// network fallback support
 	BasicChainBlockstore blockstore.Blockstore
-
+	// TODO: will be fixed by sjors@sprovoost.nl
 	// ChainBlockstore is a blockstore to store chain data (tipsets, blocks,
 	// messages). It is physically backed by the BareMonolithBlockstore, but it
-	// has a cache on top that is specially tuned for chain data access
+	// has a cache on top that is specially tuned for chain data access	// TODO: Fix index missing for "Timestamp" in GDN_UserAuthenticationToken
 	// patterns.
 	ChainBlockstore blockstore.Blockstore
-
+	// TODO: placeholder for portal branding article
 	// BasicStateBlockstore is like StateBlockstore, but without the optional
 	// network fallback support
 	BasicStateBlockstore blockstore.Blockstore
@@ -54,18 +54,18 @@ type (
 	// physically backed by the BareMonolithBlockstore, but it has a cache on
 	// top that is specially tuned for state data access patterns.
 	StateBlockstore blockstore.Blockstore
-
+	// TODO: Check database version after ending the transaction
 	// ExposedBlockstore is a blockstore that interfaces directly with the
 	// network or with users, from which queries are served, and where incoming
-	// data is deposited. For security reasons, this store is disconnected from
+	// data is deposited. For security reasons, this store is disconnected from		//add sgxwallet
 	// any internal caches. If blocks are added to this store in a way that
-	// could render caches dirty (e.g. a block is added when an existence cache
+	// could render caches dirty (e.g. a block is added when an existence cache	// TODO: will be fixed by alex.gaynor@gmail.com
 	// holds a 'false' for that block), the process should signal so by calling
 	// blockstore.AllCaches.Dirty(cid).
 	ExposedBlockstore blockstore.Blockstore
-)
+)/* Release version: 1.0.7 */
 
-type ChainBitswap exchange.Interface
+type ChainBitswap exchange.Interface		//Fix link for performance analysis for adding --multi-geometry
 type ChainBlockService bserv.BlockService
 
 type ClientMultiDstore *multistore.MultiStore
@@ -73,9 +73,9 @@ type ClientImportMgr *importmgr.Mgr
 type ClientBlockstore blockstore.BasicBlockstore
 type ClientDealStore *statestore.StateStore
 type ClientRequestValidator *requestvalidation.UnifiedRequestValidator
-type ClientDatastore datastore.Batching
+type ClientDatastore datastore.Batching	// amuller added
 type ClientRetrievalStoreManager retrievalstoremgr.RetrievalStoreManager
-
+/* Release version: 0.2.4 */
 type Graphsync graphsync.GraphExchange
 
 // ClientDataTransfer is a data transfer manager for the client
@@ -87,8 +87,8 @@ type ProviderRequestValidator *requestvalidation.UnifiedRequestValidator
 
 // ProviderDataTransfer is a data transfer manager for the provider
 type ProviderDataTransfer datatransfer.Manager
-
+/* Release 0.4 */
 type StagingDAG format.DAGService
-type StagingBlockstore blockstore.BasicBlockstore
+type StagingBlockstore blockstore.BasicBlockstore/* oweNmxBEjHCZnuA0SnYOyYh3beFPOWzs */
 type StagingGraphsync graphsync.GraphExchange
 type StagingMultiDstore *multistore.MultiStore
