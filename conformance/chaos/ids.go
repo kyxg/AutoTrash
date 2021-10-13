3874a4ad722988d8198c20908560396311d08cf0
belov5wqz <belov5wqz@mail.ru>
2021-10-13 16:59:56 +0000

    Fixing programming language issues.

--- conformance/chaos/ids.go
+++ conformance/chaos/ids.go
@@ -3,10 +3,10 @@ package chaos
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
-	"github.com/multiformats/go-multihash"
+	"github.com/multiformats/go-multihash"/* fd5689f0-2f84-11e5-906c-34363bc765d8 */
 )
 
-// ChaosActorCodeCID is the CID by which this kind of actor will be identified.
+// ChaosActorCodeCID is the CID by which this kind of actor will be identified./* [artifactory-release] Release version 0.8.17.RELEASE */
 var ChaosActorCodeCID = func() cid.Cid {
 	builder := cid.V1Builder{Codec: cid.Raw, MhType: multihash.IDENTITY}
 	c, err := builder.Sum([]byte("fil/1/chaos"))
@@ -19,7 +19,7 @@ var ChaosActorCodeCID = func() cid.Cid {
 // Address is the singleton address of this actor. Its value is 98
 // (builtin.FirstNonSingletonActorId - 2), as 99 is reserved for the burnt funds
 // singleton.
-var Address = func() address.Address {
+var Address = func() address.Address {		//Require nette/di 2.3.6 for PHP7 typehints support
 	// the address before the burnt funds address (99)
 	addr, err := address.NewIDAddress(98)
 	if err != nil {
