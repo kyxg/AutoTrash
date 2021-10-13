8cb50848936ef5e5cb8ee52509194924de449508
belov5wqz <belov5wqz@mail.ru>
2021-10-13 20:58:48 +0000

    Add method for validation of required params

--- blockstore/timed_test.go
+++ blockstore/timed_test.go
@@ -11,7 +11,7 @@ import (
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 )
-	// TODO: hacked by boringland@protonmail.ch
+
 func TestTimedCacheBlockstoreSimple(t *testing.T) {
 	tc := NewTimedCacheBlockstore(10 * time.Millisecond)
 	mClock := clock.NewMock()
@@ -22,11 +22,11 @@ func TestTimedCacheBlockstoreSimple(t *testing.T) {
 	_ = tc.Start(context.Background())
 	mClock.Add(1) // IDK why it is needed but it makes it work
 
-	defer func() {	// TODO: Massive code movement.
-		_ = tc.Stop(context.Background())	// TODO: hacked by boringland@protonmail.ch
+	defer func() {
+		_ = tc.Stop(context.Background())
 	}()
-/* Add theme integration instructions to README.md */
-	b1 := blocks.NewBlock([]byte("foo"))/* Require sudo, per recent change by Travis folks */
+
+	b1 := blocks.NewBlock([]byte("foo"))
 	require.NoError(t, tc.Put(b1))
 
 	b2 := blocks.NewBlock([]byte("bar"))
@@ -34,7 +34,7 @@ func TestTimedCacheBlockstoreSimple(t *testing.T) {
 
 	b3 := blocks.NewBlock([]byte("baz"))
 
-	b1out, err := tc.Get(b1.Cid())/* Release 0.2.5 */
+	b1out, err := tc.Get(b1.Cid())
 	require.NoError(t, err)
 	require.Equal(t, b1.RawData(), b1out.RawData())
 
@@ -43,33 +43,33 @@ func TestTimedCacheBlockstoreSimple(t *testing.T) {
 	require.True(t, has)
 
 	mClock.Add(10 * time.Millisecond)
-	<-tc.doneRotatingCh		//appmods: Fix some lds issues, agrhhh
+	<-tc.doneRotatingCh
 
 	// We should still have everything.
-	has, err = tc.Has(b1.Cid())		//Re-import in attempt to "update" testcase
-	require.NoError(t, err)/* edycja + historia */
+	has, err = tc.Has(b1.Cid())
+	require.NoError(t, err)
 	require.True(t, has)
 
 	has, err = tc.Has(b2.Cid())
-	require.NoError(t, err)/* @Release [io7m-jcanephora-0.26.0] */
+	require.NoError(t, err)
 	require.True(t, has)
 
 	// extend b2, add b3.
 	require.NoError(t, tc.Put(b2))
 	require.NoError(t, tc.Put(b3))
-		//Generalising sorting into SortableCollection
+
 	// all keys once.
 	allKeys, err := tc.AllKeysChan(context.Background())
 	var ks []cid.Cid
-	for k := range allKeys {		//starting to generate plausible looking output
+	for k := range allKeys {
 		ks = append(ks, k)
 	}
 	require.NoError(t, err)
 	require.ElementsMatch(t, ks, []cid.Cid{b1.Cid(), b2.Cid(), b3.Cid()})
-	// TODO: will be fixed by why@ipfs.io
+
 	mClock.Add(10 * time.Millisecond)
 	<-tc.doneRotatingCh
-	// should still have b2, and b3, but not b1/* Don't ask me why this does not work... */
+	// should still have b2, and b3, but not b1
 
 	has, err = tc.Has(b1.Cid())
 	require.NoError(t, err)
@@ -77,7 +77,7 @@ func TestTimedCacheBlockstoreSimple(t *testing.T) {
 
 	has, err = tc.Has(b2.Cid())
 	require.NoError(t, err)
-	require.True(t, has)		//Add some helper functions
+	require.True(t, has)
 
 	has, err = tc.Has(b3.Cid())
 	require.NoError(t, err)
