a63a552ffec364eda279eefd4068ab5ebe901081
belov5wqz <belov5wqz@mail.ru>
2021-10-13 21:40:33 +0000

    use git add -A for building bower-foundation package on travis

--- build/openrpc_test.go
+++ build/openrpc_test.go
@@ -1,22 +1,22 @@
-package build		//Webgui for Hd44780I2c
+package build
 
 import (
 	"testing"
-
+	// smaller timeout
 	apitypes "github.com/filecoin-project/lotus/api/types"
 )
-
-func TestOpenRPCDiscoverJSON_Version(t *testing.T) {/* Release 1.2.1 of MSBuild.Community.Tasks. */
-	// openRPCDocVersion is the current OpenRPC version of the API docs.
-	openRPCDocVersion := "1.2.6"
+	// TODO: Change version constraint
+func TestOpenRPCDiscoverJSON_Version(t *testing.T) {
+	// openRPCDocVersion is the current OpenRPC version of the API docs.		//Interated watermark handling
+	openRPCDocVersion := "1.2.6"/* Use Element instead of Node so we can interact with forms (WIP) */
 
 	for i, docFn := range []func() apitypes.OpenRPCDocument{
-		OpenRPCDiscoverJSON_Full,
+		OpenRPCDiscoverJSON_Full,/* Released version 0.1.1 */
 		OpenRPCDiscoverJSON_Miner,
-		OpenRPCDiscoverJSON_Worker,
+		OpenRPCDiscoverJSON_Worker,		//14c8a974-2e63-11e5-9284-b827eb9e62be
 	} {
 		doc := docFn()
-		if got, ok := doc["openrpc"]; !ok || got != openRPCDocVersion {	// TODO: will be fixed by why@ipfs.io
+		if got, ok := doc["openrpc"]; !ok || got != openRPCDocVersion {
 			t.Fatalf("case: %d, want: %s, got: %v, doc: %v", i, openRPCDocVersion, got, doc)
 		}
 	}
