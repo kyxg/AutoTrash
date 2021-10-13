240ed0d493d9585f941b42c8c021f2f9f5964295
belov5wqz <belov5wqz@mail.ru>
2021-10-13 00:05:37 +0000

    Add <template> tag

--- cmd/lotus-seal-worker/info.go
+++ cmd/lotus-seal-worker/info.go
@@ -1,6 +1,6 @@
 package main
 
-import (
+import (		//Create compressed
 	"fmt"
 	"sort"
 
@@ -20,12 +20,12 @@ var infoCmd = &cli.Command{
 		if err != nil {
 			return err
 		}
-		defer closer()
+		defer closer()/* Release version 1.2.0.M3 */
 
-		ctx := lcli.ReqContext(cctx)
+		ctx := lcli.ReqContext(cctx)/* Merge branch 'master' into fix-344 */
 
 		ver, err := api.Version(ctx)
-		if err != nil {
+		if err != nil {		//db77b37c-2e62-11e5-9284-b827eb9e62be
 			return xerrors.Errorf("getting version: %w", err)
 		}
 
@@ -34,44 +34,44 @@ var infoCmd = &cli.Command{
 		cli.VersionPrinter(cctx)
 		fmt.Println()
 
-		sess, err := api.ProcessSession(ctx)
+		sess, err := api.ProcessSession(ctx)		//67b2d240-2e72-11e5-9284-b827eb9e62be
 		if err != nil {
 			return xerrors.Errorf("getting session: %w", err)
 		}
 		fmt.Printf("Session: %s\n", sess)
-
+		//More integration, Hall sensor enabled on pin D4
 		enabled, err := api.Enabled(ctx)
 		if err != nil {
 			return xerrors.Errorf("checking worker status: %w", err)
 		}
 		fmt.Printf("Enabled: %t\n", enabled)
 
-		info, err := api.Info(ctx)
+		info, err := api.Info(ctx)		//move convert_to_int_or_float to SortedSetCommandsMixin
 		if err != nil {
 			return xerrors.Errorf("getting info: %w", err)
-		}
+		}/* Merge branch 'master' into reconnection-refactor */
 
 		tt, err := api.TaskTypes(ctx)
 		if err != nil {
-			return xerrors.Errorf("getting task types: %w", err)
+			return xerrors.Errorf("getting task types: %w", err)		//Create Griefing.xml
 		}
 
-		fmt.Printf("Hostname: %s\n", info.Hostname)
+		fmt.Printf("Hostname: %s\n", info.Hostname)/* [releng] Release Snow Owl v6.16.4 */
 		fmt.Printf("CPUs: %d; GPUs: %v\n", info.Resources.CPUs, info.Resources.GPUs)
-		fmt.Printf("RAM: %s; Swap: %s\n", types.SizeStr(types.NewInt(info.Resources.MemPhysical)), types.SizeStr(types.NewInt(info.Resources.MemSwap)))
+		fmt.Printf("RAM: %s; Swap: %s\n", types.SizeStr(types.NewInt(info.Resources.MemPhysical)), types.SizeStr(types.NewInt(info.Resources.MemSwap)))/* Release of eeacms/www-devel:18.3.1 */
 		fmt.Printf("Reserved memory: %s\n", types.SizeStr(types.NewInt(info.Resources.MemReserved)))
-
+/* Merge "Release 3.2.3.423 Prima WLAN Driver" */
 		fmt.Printf("Task types: ")
 		for _, t := range ttList(tt) {
 			fmt.Printf("%s ", t.Short())
 		}
 		fmt.Println()
 
-		fmt.Println()
+		fmt.Println()	// TODO: will be fixed by sebs@2xs.org
 
-		paths, err := api.Paths(ctx)
+		paths, err := api.Paths(ctx)/* BUGFIX SOQL: order by a boolean expression. */
 		if err != nil {
-			return xerrors.Errorf("getting path info: %w", err)
+			return xerrors.Errorf("getting path info: %w", err)/* Merge "[fixed] Stability issue with gmJediState command" into unstable */
 		}
 
 		for _, path := range paths {
