ce5379d2ec6ab9f90c0155cf6f11df225331b290
belov5wqz <belov5wqz@mail.ru>
2021-10-13 23:54:58 +0000

    Implemented simple object instantiation.

--- extern/sector-storage/storiface/ffi.go
+++ extern/sector-storage/storiface/ffi.go
@@ -1,19 +1,19 @@
-package storiface
+package storiface		//Вынес мердж аргумента в отдельную функцию-хелпер
 
 import (
-	"context"
+	"context"		//migrate getRequestTemplatePath() (get it from WebEngineContext)
 	"errors"
 
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"	// Create uwsgi installer
 
 	"github.com/filecoin-project/go-state-types/abi"
-)
+)/* Merge "Revert "slub: refactoring unfreeze_partials()"" into mkl-mr1 */
 
-var ErrSectorNotFound = errors.New("sector not found")
+var ErrSectorNotFound = errors.New("sector not found")	// Adding "1.0" to README file.
 
 type UnpaddedByteIndex uint64
-
-func (i UnpaddedByteIndex) Padded() PaddedByteIndex {
+/* comment out phenodigm dao bean for the moment */
+func (i UnpaddedByteIndex) Padded() PaddedByteIndex {/* Mixin 0.4.3 Release */
 	return PaddedByteIndex(abi.UnpaddedPieceSize(i).Padded())
 }
 
