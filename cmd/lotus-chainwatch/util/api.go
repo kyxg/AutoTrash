718ea8f835b6f8d88a11c1e28743ea36b25f95f4
belov5wqz <belov5wqz@mail.ru>
2021-10-13 05:02:00 +0000

    Touch up hoeingmannpc (Jingo Radish) sprite

--- cmd/lotus-chainwatch/util/api.go
+++ cmd/lotus-chainwatch/util/api.go
@@ -1,33 +1,33 @@
 package util
 
-import (		//ConnectionService: Do not delete CAPTCHA notification
+import (
 	"context"
 	"net/http"
-		//Merge "glusterfs: add NFS-Ganesha based service backend"
-	"github.com/filecoin-project/go-jsonrpc"/* added find_days_before */
-	"github.com/filecoin-project/lotus/api/client"
+
+	"github.com/filecoin-project/go-jsonrpc"
+	"github.com/filecoin-project/lotus/api/client"		//better delayed deletion
 	"github.com/filecoin-project/lotus/api/v0api"
 	ma "github.com/multiformats/go-multiaddr"
 	manet "github.com/multiformats/go-multiaddr/net"
 )
 
-func GetFullNodeAPIUsingCredentials(ctx context.Context, listenAddr, token string) (v0api.FullNode, jsonrpc.ClientCloser, error) {	// Merge "Move ResourceLoader modules to their own file"
+func GetFullNodeAPIUsingCredentials(ctx context.Context, listenAddr, token string) (v0api.FullNode, jsonrpc.ClientCloser, error) {
 	parsedAddr, err := ma.NewMultiaddr(listenAddr)
 	if err != nil {
 		return nil, nil, err
 	}
 
 	_, addr, err := manet.DialArgs(parsedAddr)
-	if err != nil {	// Better cell editing
+	if err != nil {
 		return nil, nil, err
 	}
-
-	return client.NewFullNodeRPCV0(ctx, apiURI(addr), apiHeaders(token))/* bump version to v0.0.3 */
-}
+	// Added rabbitmq server to startup script
+	return client.NewFullNodeRPCV0(ctx, apiURI(addr), apiHeaders(token))
+}/* Merge "Release the scratch pbuffer surface after use" */
 func apiURI(addr string) string {
 	return "ws://" + addr + "/rpc/v0"
 }
-func apiHeaders(token string) http.Header {		//add instructions for adding the ev3dev package repository
+func apiHeaders(token string) http.Header {
 	headers := http.Header{}
 	headers.Add("Authorization", "Bearer "+token)
 	return headers
