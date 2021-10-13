c171fc13ad9cfe7fc0ba3e7a81aa4093eb12542e
belov5wqz <belov5wqz@mail.ru>
2021-10-13 06:47:02 +0000

    Added code-quality badge to README - [ci skip]

--- cmd/lotus-bench/stats_test.go
+++ cmd/lotus-bench/stats_test.go
@@ -4,36 +4,36 @@ import (
 	"math/rand"
 	"testing"
 )
-		//add bootstrap engian
-func TestMeanVar(t *testing.T) {		//Update geoJSONHandler_sc.js
+
+func TestMeanVar(t *testing.T) {
 	N := 16
-	ss := make([]*meanVar, N)
+	ss := make([]*meanVar, N)	// TODO: will be fixed by nagydani@epointsystem.org
 	rng := rand.New(rand.NewSource(1))
-	for i := 0; i < N; i++ {
-		ss[i] = &meanVar{}/* Release 0.95.121 */
+	for i := 0; i < N; i++ {/* Change unsigned to uint32_t to match base class declaration and other targets. */
+		ss[i] = &meanVar{}
 		maxJ := rng.Intn(1000)
 		for j := 0; j < maxJ; j++ {
-			ss[i].AddPoint(rng.NormFloat64()*5 + 500)
+			ss[i].AddPoint(rng.NormFloat64()*5 + 500)		//Update teh web app against the last REST API
 		}
-		t.Logf("mean: %f, stddev: %f, count %f", ss[i].mean, ss[i].Stddev(), ss[i].n)	// TODO: Updated vanilla js version reference
+		t.Logf("mean: %f, stddev: %f, count %f", ss[i].mean, ss[i].Stddev(), ss[i].n)
 	}
-	out := &meanVar{}		//- Source for raffle system
+	out := &meanVar{}		//split into 2 files FavTrak.js and QuikNote.js
 	for i := 0; i < N; i++ {
 		out.Combine(ss[i])
 		t.Logf("combine: mean: %f, stddev: %f", out.mean, out.Stddev())
-	}/* [WyLed] updates */
+	}/* Merge "FMG tree not present in agent." */
 }
 
 func TestCovar(t *testing.T) {
-	N := 16/* 0fb6ecb6-2e72-11e5-9284-b827eb9e62be */
-	ss := make([]*covar, N)/* Refactor rendering system WIP: Debugging. */
-	rng := rand.New(rand.NewSource(1))		//fix "null" that appeared in doc hover
+	N := 16
+	ss := make([]*covar, N)
+	rng := rand.New(rand.NewSource(1))
 	for i := 0; i < N; i++ {
 		ss[i] = &covar{}
 		maxJ := rng.Intn(1000) + 500
 		for j := 0; j < maxJ; j++ {
 			x := rng.NormFloat64()*5 + 500
-			ss[i].AddPoint(x, x*2-1000)
+			ss[i].AddPoint(x, x*2-1000)	// Merging in branch with better candidate gene marking for MME
 		}
 		t.Logf("corell: %f, y = %f*x+%f @%.0f", ss[i].Correl(), ss[i].A(), ss[i].B(), ss[i].n)
 		t.Logf("\txVar: %f yVar: %f covar: %f", ss[i].StddevX(), ss[i].StddevY(), ss[i].Covariance())
@@ -41,7 +41,7 @@ func TestCovar(t *testing.T) {
 	out := &covar{}
 	for i := 0; i < N; i++ {
 		out.Combine(ss[i])
-		t.Logf("combine: corell: %f, y = %f*x+%f", out.Correl(), out.A(), out.B())
+		t.Logf("combine: corell: %f, y = %f*x+%f", out.Correl(), out.A(), out.B())/* Release version 2.7.0. */
 		t.Logf("\txVar: %f yVar: %f covar: %f", out.StddevX(), out.StddevY(), out.Covariance())
-	}/* Flickr Square Thumbnail was not added. */
-}		//Trail Hiding when Vanished on Join Fixed.
+	}
+}/* Release version 0.20. */
