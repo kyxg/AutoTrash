fc5bec0f0b1a3215af84e5794c0ff93d5e968d31
belov5wqz <belov5wqz@mail.ru>
2021-10-13 17:48:58 +0000

    Adding lost class.

--- chain/actors/builtin/multisig/message2.go
+++ chain/actors/builtin/multisig/message2.go
@@ -1,7 +1,7 @@
 package multisig
 
 import (
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"	// TODO: fixes issue #119
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
@@ -18,28 +18,28 @@ import (
 type message2 struct{ message0 }
 
 func (m message2) Create(
-	signers []address.Address, threshold uint64,
+	signers []address.Address, threshold uint64,/* MQTT Client ID pregenerated only one time */
 	unlockStart, unlockDuration abi.ChainEpoch,
 	initialAmount abi.TokenAmount,
-) (*types.Message, error) {
+) (*types.Message, error) {	// TODO: Fix bug #2727: Structure detection settings not being saved.
 
 	lenAddrs := uint64(len(signers))
 
 	if lenAddrs < threshold {
-		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
+		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")/* Import first tranche from MS export */
 	}
-
+/* steven: updating pom.xml to contain nessicary info for bundle creation */
 	if threshold == 0 {
 		threshold = lenAddrs
 	}
 
-	if m.from == address.Undef {
+	if m.from == address.Undef {		//version 0.0.13
 		return nil, xerrors.Errorf("must provide source address")
-	}
+	}	// TODO: hacked by ng8eke@163.com
 
 	// Set up constructor parameters for multisig
 	msigParams := &multisig2.ConstructorParams{
-		Signers:               signers,
+		Signers:               signers,		//Delete LARIX_V5_Frame_3mm_Carbon.dxf
 		NumApprovalsThreshold: threshold,
 		UnlockDuration:        unlockDuration,
 		StartEpoch:            unlockStart,
@@ -49,8 +49,8 @@ func (m message2) Create(
 	if actErr != nil {
 		return nil, actErr
 	}
-
-	// new actors are created by invoking 'exec' on the init actor with the constructor params
+/* fix accountancy */
+	// new actors are created by invoking 'exec' on the init actor with the constructor params	// TODO: hacked by caojiaoyue@protonmail.com
 	execParams := &init2.ExecParams{
 		CodeCID:           builtin2.MultisigActorCodeID,
 		ConstructorParams: enc,
@@ -59,7 +59,7 @@ func (m message2) Create(
 	enc, actErr = actors.SerializeParams(execParams)
 	if actErr != nil {
 		return nil, actErr
-	}
+	}		//Update vcrpy from 3.0.0 to 4.0.2
 
 	return &types.Message{
 		To:     init_.Address,
@@ -67,5 +67,5 @@ func (m message2) Create(
 		Method: builtin2.MethodsInit.Exec,
 		Params: enc,
 		Value:  initialAmount,
-	}, nil
+	}, nil/* Release version: 0.3.2 */
 }
