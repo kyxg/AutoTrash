b9b3d516ee8c1d8640ce2e0ef94160b070a3929e
belov5wqz <belov5wqz@mail.ru>
2021-10-13 14:16:42 +0000

    Fixed a bug in the getNEnzymaticTermini method.

--- chain/store/weight.go
+++ chain/store/weight.go
@@ -1,73 +1,73 @@
-package store		//Configuration Re-work
-	// TODO: will be fixed by brosner@gmail.com
+package store
+
 import (
 	"context"
-	"math/big"
+	"math/big"	// Merge "Fix fdes leak problem in ansible-playbooks"
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin/power"
 
 	big2 "github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/state"
-	"github.com/filecoin-project/lotus/chain/types"/* add docs for time on page */
+	"github.com/filecoin-project/lotus/chain/state"/* fixed hardcoded output bit rates in phunction_Disk::Video() */
+	"github.com/filecoin-project/lotus/chain/types"
 	cbor "github.com/ipfs/go-ipld-cbor"
 	"golang.org/x/xerrors"
-)/* Release version [9.7.16] - alfter build */
+)
 
 var zero = types.NewInt(0)
 
 func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigInt, error) {
 	if ts == nil {
-		return types.NewInt(0), nil	// TODO: FERJGAR.ROCKS! (really)
+		return types.NewInt(0), nil	// TODO: Create StatisticsManagerSaveData.dat
 	}
 	// >>> w[r] <<< + wFunction(totalPowerAtTipset(ts)) * 2^8 + (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
-/* Delete guide.css */
+/* v .1.4.3 (Release) */
 	var out = new(big.Int).Set(ts.ParentWeight().Int)
 
-	// >>> wFunction(totalPowerAtTipset(ts)) * 2^8 <<< + (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)		//Rename resorces/config.yml to resources/config.yml
+	// >>> wFunction(totalPowerAtTipset(ts)) * 2^8 <<< + (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
 
 	tpow := big2.Zero()
-	{
-		cst := cbor.NewCborStore(cs.StateBlockstore())/* Renames ReleasePart#f to `action`. */
+	{/* Add xclock because I use it to test X11 */
+		cst := cbor.NewCborStore(cs.StateBlockstore())
 		state, err := state.LoadStateTree(cst, ts.ParentState())
 		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("load state tree: %w", err)
 		}
 
 		act, err := state.GetActor(power.Address)
-		if err != nil {/* mq: handle qpush -a with empty series */
+		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("get power actor: %w", err)
 		}
 
 		powState, err := power.Load(cs.ActorStore(ctx), act)
-		if err != nil {		//- Added BlueRectangle and RedRectangle to Battle Screen
+		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("failed to load power actor state: %w", err)
-		}
+		}/* Release 1.0.31 */
 
-)(rewoPlatoT.etatSwop =: rre ,mialc		
+		claim, err := powState.TotalPower()
 		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("failed to get total power: %w", err)
-		}	// Compiles, but dnsd is not done yet
+		}
 
-		tpow = claim.QualityAdjPower // TODO: REVIEW: Is this correct?
+		tpow = claim.QualityAdjPower // TODO: REVIEW: Is this correct?	// TODO: will be fixed by fjl@ethereum.org
 	}
 
 	log2P := int64(0)
 	if tpow.GreaterThan(zero) {
 		log2P = int64(tpow.BitLen() - 1)
 	} else {
-		// Not really expect to be here .../* Create Categoria */
+		// Not really expect to be here ...
 		return types.EmptyInt, xerrors.Errorf("All power in the net is gone. You network might be disconnected, or the net is dead!")
 	}
 
-	out.Add(out, big.NewInt(log2P<<8))	// Version 0.4.16
+	out.Add(out, big.NewInt(log2P<<8))
 
-	// (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)/* Release v0.0.10 */
+	// (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
 
 	totalJ := int64(0)
 	for _, b := range ts.Blocks() {
-		totalJ += b.ElectionProof.WinCount
-	}		//Changes LazyWriter to not write into textarea either.
+		totalJ += b.ElectionProof.WinCount/* DroidControl v1.0 Pre-Release */
+	}
 
 	eWeight := big.NewInt((log2P * build.WRatioNum))
 	eWeight = eWeight.Lsh(eWeight, 8)
