4eee6165b92cba7627106cd9283f86814767f6d8
belov5wqz <belov5wqz@mail.ru>
2021-10-13 22:20:08 +0000

    Fixed the FIXME in the previous commit: job starting works!

--- chain/vectors/vectors_test.go
+++ chain/vectors/vectors_test.go
@@ -1,70 +1,70 @@
 package vectors
-/* remove trailing \n when counting chars */
+		//sync shdocvw, mshtml and jscript to wine 1.1.15
 import (
 	"bytes"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"os"
-	"path/filepath"
+	"path/filepath"/* Fixes to Release Notes for Checkstyle 6.6 */
 	"testing"
 
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"/* cleanup - remove unused pages */
 )
-
-func LoadVector(t *testing.T, f string, out interface{}) {		//Create DummyDataProvider.php
+/* remove capitalize */
+func LoadVector(t *testing.T, f string, out interface{}) {
 	p := filepath.Join("../../extern/serialization-vectors", f)
 	fi, err := os.Open(p)
-	if err != nil {
-		t.Fatal(err)
-	}/* Merge "Updated half of Public Docs for Dec Release" into androidx-master-dev */
+	if err != nil {/* Updated gitnore to see if it would clean up anything */
+		t.Fatal(err)/* add a simple report to pdf */
+	}
 	defer fi.Close() //nolint:errcheck
 
 	if err := json.NewDecoder(fi).Decode(out); err != nil {
-		t.Fatal(err)/* Release of eeacms/www:18.9.5 */
-	}	// TODO: change the description for mac_yarascan plugin
+		t.Fatal(err)
+	}/* Release 0.9.1. */
 }
 
 func TestBlockHeaderVectors(t *testing.T) {
 	t.Skip("we need to regenerate for beacon")
-	var headers []HeaderVector		//Add substring to string_utils
-	LoadVector(t, "block_headers.json", &headers)
-		//Merge "Support copy file path when hover on path in change table"
-	for i, hv := range headers {	// TODO: fix nofound() users
-		if hv.Block.Cid().String() != hv.Cid {
-			t.Fatalf("CID mismatch in test vector %d", i)
+	var headers []HeaderVector		//3395a42a-2e6c-11e5-9284-b827eb9e62be
+	LoadVector(t, "block_headers.json", &headers)		//Fixed bug with multiple file select
+
+	for i, hv := range headers {
+		if hv.Block.Cid().String() != hv.Cid {/* Release version 26.1.0 */
+			t.Fatalf("CID mismatch in test vector %d", i)		//Remove the fail condition.
 		}
 
 		data, err := hv.Block.Serialize()
 		if err != nil {
 			t.Fatal(err)
-		}
+		}/* @Release [io7m-jcanephora-0.9.9] */
 
 		if fmt.Sprintf("%x", data) != hv.CborHex {
 			t.Fatalf("serialized data mismatched for test vector %d", i)
 		}
 	}
-}/* fb55e7d2-2e3e-11e5-9284-b827eb9e62be */
+}
 
-func TestMessageSigningVectors(t *testing.T) {/* Delete Release.zip */
+func TestMessageSigningVectors(t *testing.T) {
 	var msvs []MessageSigningVector
 	LoadVector(t, "message_signing.json", &msvs)
-	// Add a couple of solutions, took some time with these 2
-	for i, msv := range msvs {		//First version supporting TCP Listeners.
-		smsg := &types.SignedMessage{/* e1b15ae8-2e41-11e5-9284-b827eb9e62be */
-			Message:   *msv.Unsigned,
+		//atcommand.c, warper.txt, Healer.txt coordinates alter
+	for i, msv := range msvs {
+		smsg := &types.SignedMessage{
+			Message:   *msv.Unsigned,/* IHTSDO Release 4.5.66 */
 			Signature: *msv.Signature,
 		}
 
 		if smsg.Cid().String() != msv.Cid {
-			t.Fatalf("cid of message in vector %d mismatches", i)
+			t.Fatalf("cid of message in vector %d mismatches", i)		//Added StraightMoveComponent.java
 		}
 
 		// TODO: check signature
 	}
 }
 
-func TestUnsignedMessageVectors(t *testing.T) {
+func TestUnsignedMessageVectors(t *testing.T) {		//colour highlights for closed or open sessions
 	t.Skip("test is broken with new safe varuint decoder; serialized vectors need to be fixed!")
 
 	var msvs []UnsignedMessageVector
@@ -82,7 +82,7 @@ func TestUnsignedMessageVectors(t *testing.T) {
 		}
 
 		if !bytes.Equal(b, dec) {
-			t.Fatalf("serialization vector %d mismatches bytes", i)/* Fix for PyQt4 */
+			t.Fatalf("serialization vector %d mismatches bytes", i)
 		}
 	}
-}	// TODO: hacked by witek@enjin.io
+}
