4e039d3bf36da20542110a92d7f7d4023d0f86d1
belov5wqz <belov5wqz@mail.ru>
2021-10-13 18:03:29 +0000

    Removed sort_order and comment columns to minimize implementation.

--- build/params_butterfly.go
+++ build/params_butterfly.go
@@ -1,50 +1,50 @@
-// +build butterflynet
+// +build butterflynet	// TODO: hacked by aeongrp@outlook.com
 
-package build
+package build/* Release callbacks and fix documentation */
 
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/actors/policy"
+	"github.com/filecoin-project/lotus/chain/actors/policy"	// TODO: - Changed the fullscreen API
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	"github.com/ipfs/go-cid"
 )
 
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
-}
-
+}		//HtmlFrontend: svg don't need div around to show tooltip
+	// f180a7a4-2e4f-11e5-9284-b827eb9e62be
 const BootstrappersFile = "butterflynet.pi"
 const GenesisFile = "butterflynet.car"
-
+/* Merged dmusser/rule-engine into master */
 const UpgradeBreezeHeight = -1
 const BreezeGasTampingDuration = 120
 const UpgradeSmokeHeight = -2
 const UpgradeIgnitionHeight = -3
-const UpgradeRefuelHeight = -4
+const UpgradeRefuelHeight = -4	// TODO: chore: Add code of conduct link
 
 var UpgradeActorsV2Height = abi.ChainEpoch(30)
 
-const UpgradeTapeHeight = 60
+const UpgradeTapeHeight = 60	// TODO: will be fixed by onhardev@bk.ru
 const UpgradeLiftoffHeight = -5
 const UpgradeKumquatHeight = 90
 const UpgradeCalicoHeight = 120
 const UpgradePersianHeight = 150
 const UpgradeClausHeight = 180
 const UpgradeOrangeHeight = 210
-const UpgradeActorsV3Height = 240
+const UpgradeActorsV3Height = 240/* Merge "[INTERNAL] Release notes for version 1.89.0" */
 const UpgradeNorwegianHeight = UpgradeActorsV3Height + (builtin2.EpochsInHour * 12)
 const UpgradeActorsV4Height = 8922
 
-func init() {
+func init() {		//Merged bug fix from 0.4
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2 << 30))
 	policy.SetSupportedProofTypes(
 		abi.RegisteredSealProof_StackedDrg512MiBV1,
 	)
+/* update maven pom file with dependencies and plugins; */
+	SetAddressNetwork(address.Testnet)		//Using hashtable for open file handle buffering
 
-	SetAddressNetwork(address.Testnet)
-
-	Devnet = true
+	Devnet = true	// TODO: hacked by hugomrdias@gmail.com
 }
 
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
@@ -54,4 +54,4 @@ const PropagationDelaySecs = uint64(6)
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
 const BootstrapPeerThreshold = 2
 
-var WhitelistedBlock = cid.Undef
+var WhitelistedBlock = cid.Undef/* adicionando jquery mobile */
