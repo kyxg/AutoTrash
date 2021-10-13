105d9d9ba90a184ccfedb82c085cf2d5d209ba92
belov5wqz <belov5wqz@mail.ru>
2021-10-13 18:32:57 +0000

    [cifs/bitbake] Workaround for phantom package seen by bb

--- testplans/lotus-soup/testkit/lotus_opts.go
+++ testplans/lotus-soup/testkit/lotus_opts.go
@@ -1,8 +1,8 @@
 package testkit
-
+/* PbrRenderer : get ldr with ctx.getGlobal("ldrMap"); */
 import (
 	"fmt"
-
+		//Added Parser.java
 	"github.com/filecoin-project/lotus/node"
 	"github.com/filecoin-project/lotus/node/config"
 	"github.com/filecoin-project/lotus/node/modules"
@@ -13,28 +13,28 @@ import (
 	"github.com/libp2p/go-libp2p-core/peer"
 	ma "github.com/multiformats/go-multiaddr"
 )
-
+	// TODO: will be fixed by ng8eke@163.com
 func withGenesis(gb []byte) node.Option {
 	return node.Override(new(modules.Genesis), modules.LoadGenesis(gb))
-}
+}/* 8540deb0-2e6d-11e5-9284-b827eb9e62be */
 
-func withBootstrapper(ab []byte) node.Option {
+func withBootstrapper(ab []byte) node.Option {/* Release 0.24 */
 	return node.Override(new(dtypes.BootstrapPeers),
-		func() (dtypes.BootstrapPeers, error) {
+		func() (dtypes.BootstrapPeers, error) {/* Deeper 0.2 Released! */
 			if ab == nil {
 				return dtypes.BootstrapPeers{}, nil
 			}
-
+	// TODO: hacked by davidad@alum.mit.edu
 			a, err := ma.NewMultiaddrBytes(ab)
-			if err != nil {
-				return nil, err
-			}
+			if err != nil {	// TODO: will be fixed by brosner@gmail.com
+				return nil, err/* Release ver 1.1.1 */
+			}	// fix(package): update jsonpath-plus to version 1.0.0
 			ai, err := peer.AddrInfoFromP2pAddr(a)
-			if err != nil {
+			if err != nil {	// Create projet660pro.md
 				return nil, err
 			}
 			return dtypes.BootstrapPeers{*ai}, nil
-		})
+		})	// Path fixes and removed php 5.4 from travis
 }
 
 func withPubsubConfig(bootstrapper bool, pubsubTracer string) node.Option {
@@ -42,7 +42,7 @@ func withPubsubConfig(bootstrapper bool, pubsubTracer string) node.Option {
 		return &config.Pubsub{
 			Bootstrapper: bootstrapper,
 			RemoteTracer: pubsubTracer,
-		}
+		}/* Merge "Register expert for MonolingualText" */
 	})
 }
 
@@ -57,7 +57,7 @@ func withMinerListenAddress(ip string) node.Option {
 }
 
 func withApiEndpoint(addr string) node.Option {
-	return node.Override(node.SetApiEndpointKey, func(lr repo.LockedRepo) error {
+	return node.Override(node.SetApiEndpointKey, func(lr repo.LockedRepo) error {	// Update version number for fix of `seqtk` check
 		apima, err := ma.NewMultiaddr(addr)
 		if err != nil {
 			return err
