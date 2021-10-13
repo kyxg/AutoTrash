40660c93b143468c4af5cc04af29e50d49103067
belov5wqz <belov5wqz@mail.ru>
2021-10-13 15:09:21 +0000

    Starting documenting changes in v2.0

--- conformance/rand_fixed.go
+++ conformance/rand_fixed.go
@@ -1,18 +1,18 @@
 package conformance
 
-import (
+import (	// TODO: "read multimeter" button
 	"context"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// ui: add secondary activity indicator and permalink buttons in liveview
 	"github.com/filecoin-project/go-state-types/crypto"
 
 	"github.com/filecoin-project/lotus/chain/vm"
 )
 
 type fixedRand struct{}
-
+/* save in progress work */
 var _ vm.Rand = (*fixedRand)(nil)
-
+/* Adding missing return on contentBean.setReleaseDate() */
 // NewFixedRand creates a test vm.Rand that always returns fixed bytes value
 // of utf-8 string 'i_am_random_____i_am_random_____'.
 func NewFixedRand() vm.Rand {
