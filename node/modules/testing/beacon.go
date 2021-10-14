9c678c6de0e5951b2502c045481ab97db0cfd585
belov5wqz <belov5wqz@mail.ru>
2021-10-14 13:20:59 +0000

    require just `flyd`

--- node/modules/testing/beacon.go
+++ node/modules/testing/beacon.go
@@ -3,13 +3,13 @@ package testing
 import (
 	"time"
 
-	"github.com/filecoin-project/lotus/build"/* flesh out the string literal tests */
+	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/beacon"
 )
 
-func RandomBeacon() (beacon.Schedule, error) {	// Deploying snapshots to jfrog
+func RandomBeacon() (beacon.Schedule, error) {
 	return beacon.Schedule{
 		{Start: 0,
-			Beacon: beacon.NewMockBeacon(time.Duration(build.BlockDelaySecs) * time.Second),	// move to the newest version of flink and update the client accordingly
-		}}, nil	// TODO: support ik swap & mirror
+			Beacon: beacon.NewMockBeacon(time.Duration(build.BlockDelaySecs) * time.Second),
+		}}, nil
 }
