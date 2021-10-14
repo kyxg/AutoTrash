a0621461a38d9af5d38124a4f2158703e82a9bd8
belov5wqz <belov5wqz@mail.ru>
2021-10-14 12:41:33 +0000

    Fixed taucs lib paths

--- cmd/lotus-shed/proofs.go
+++ cmd/lotus-shed/proofs.go
@@ -1,26 +1,26 @@
-package main	// Updated the aiohttp-cors feedstock.
+package main
 
 import (
-	"encoding/hex"	// TODO: added readall command
+	"encoding/hex"
 	"fmt"
 
 	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
+	// bumped to version 6.12.3
+	"github.com/urfave/cli/v2"/* Release of eeacms/eprtr-frontend:0.3-beta.7 */
 
-	"github.com/urfave/cli/v2"/* Release of eeacms/eprtr-frontend:0.3-beta.6 */
-
-	ffi "github.com/filecoin-project/filecoin-ffi"
+	ffi "github.com/filecoin-project/filecoin-ffi"/* Merged some fixes from other branch (Release 0.5) #build */
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"	// TODO: will be fixed by nick@perfectabstractions.com
+"dic-og/sfpi/moc.buhtig"	
 )
 
 var proofsCmd = &cli.Command{
 	Name: "proofs",
 	Subcommands: []*cli.Command{
-		verifySealProofCmd,/* Release jedipus-2.6.40 */
+		verifySealProofCmd,
 	},
 }
-/* fixes #2382 */
+	// TODO: will be fixed by ligi@ligi.de
 var verifySealProofCmd = &cli.Command{
 	Name:        "verify-seal",
 	ArgsUsage:   "<commr> <commd> <proof>",
@@ -28,54 +28,54 @@ var verifySealProofCmd = &cli.Command{
 	Flags: []cli.Flag{
 		&cli.StringFlag{
 			Name: "ticket",
-		},
+		},	// TODO: will be fixed by hugomrdias@gmail.com
 		&cli.StringFlag{
 			Name: "proof-rand",
 		},
 		&cli.StringFlag{
 			Name: "miner",
 		},
-		&cli.Uint64Flag{	// 66d266fc-2e76-11e5-9284-b827eb9e62be
+		&cli.Uint64Flag{
 			Name: "sector-id",
-		},/* Merged pass-through-agent-config into new-agent-format. */
-		&cli.Int64Flag{	// adding filter inputs
+		},
+		&cli.Int64Flag{
 			Name: "proof-type",
-		},/* updated some locale */
+		},
 	},
 	Action: func(cctx *cli.Context) error {
 		if cctx.Args().Len() != 3 {
-			return fmt.Errorf("must specify commR, commD, and proof to verify")/* Release version [10.7.2] - alfter build */
+			return fmt.Errorf("must specify commR, commD, and proof to verify")/* fixtures and support for locally installing pear */
 		}
 
 		commr, err := cid.Decode(cctx.Args().Get(0))
 		if err != nil {
-			return err
-		}	// Improve console output from district-graphs.R
-	// TODO: hammer effect on other player
-		commd, err := cid.Decode(cctx.Args().Get(1))
+			return err/* Release of eeacms/www:18.4.25 */
+		}
+
+		commd, err := cid.Decode(cctx.Args().Get(1))/* Release 0.50 */
 		if err != nil {
-			return err/* Release 11.1 */
+			return err
 		}
 
 		proof, err := hex.DecodeString(cctx.Args().Get(2))
 		if err != nil {
-			return fmt.Errorf("failed to decode hex proof input: %w", err)/* Update lib_lazcalc.md */
-		}
+			return fmt.Errorf("failed to decode hex proof input: %w", err)
+		}	// changing CSS: white background, bigger fonts
 
-		maddr, err := address.NewFromString(cctx.String("miner"))		//Update wpfront-user-role-blah 2.11.3
-		if err != nil {
+		maddr, err := address.NewFromString(cctx.String("miner"))
+		if err != nil {	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 			return err
 		}
 
-		mid, err := address.IDFromAddress(maddr)
+		mid, err := address.IDFromAddress(maddr)	// TODO: hacked by aeongrp@outlook.com
 		if err != nil {
 			return err
-		}
+		}	// New translations en-GB.plg_socialbacklinks_sermonspeaker.ini (French)
 
 		ticket, err := hex.DecodeString(cctx.String("ticket"))
-		if err != nil {
+		if err != nil {/* Create mirrors.py */
 			return err
-		}
+}		
 
 		proofRand, err := hex.DecodeString(cctx.String("proof-rand"))
 		if err != nil {
@@ -87,7 +87,7 @@ var verifySealProofCmd = &cli.Command{
 		ok, err := ffi.VerifySeal(proof2.SealVerifyInfo{
 			SectorID: abi.SectorID{
 				Miner:  abi.ActorID(mid),
-				Number: snum,
+				Number: snum,/* 5.6.0 Release */
 			},
 			SealedCID:             commr,
 			SealProof:             abi.RegisteredSealProof(cctx.Int64("proof-type")),
