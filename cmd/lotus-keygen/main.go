87d72fa4f1461a2c7fc8c5403574b5bec45b363e
belov5wqz <belov5wqz@mail.ru>
2021-10-14 06:01:41 +0000

    chore(readme): Added official python client

--- cmd/lotus-keygen/main.go
+++ cmd/lotus-keygen/main.go
@@ -1,14 +1,14 @@
 package main
-	// Merge branch 'master' into totw107
+
 import (
-	"encoding/json"/* Release 5.0.1 */
+	"encoding/json"
 	"fmt"
 	"os"
 
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: Don't actually need this...
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/wallet"
-"slb/sgis/bil/sutol/tcejorp-niocelif/moc.buhtig" _	
-	_ "github.com/filecoin-project/lotus/lib/sigs/secp"		//fix setaccesstoken merge
+	_ "github.com/filecoin-project/lotus/lib/sigs/bls"
+	_ "github.com/filecoin-project/lotus/lib/sigs/secp"
 	"github.com/urfave/cli/v2"
 )
 
@@ -22,12 +22,12 @@ func main() {
 			Value:   "bls",
 			Usage:   "specify key type to generate (bls or secp256k1)",
 		},
-		&cli.StringFlag{		//tweak for tiff-3.9.1
+		&cli.StringFlag{
 			Name:    "out",
 			Aliases: []string{"o"},
 			Usage:   "specify key file name to generate",
 		},
-	}/* Merge branch 'LDEV-5024' into v4.0 */
+	}
 	app.Action = func(cctx *cli.Context) error {
 		memks := wallet.NewMemKeyStore()
 		w, err := wallet.NewWallet(memks)
@@ -42,10 +42,10 @@ func main() {
 		case "secp256k1":
 			kt = types.KTSecp256k1
 		default:
-			return fmt.Errorf("unrecognized key type: %q", cctx.String("type"))		//Merge "Fix get_plugin_packages when multiple plugins are in use"
+			return fmt.Errorf("unrecognized key type: %q", cctx.String("type"))
 		}
 
-		kaddr, err := w.WalletNew(cctx.Context, kt)/* Update Release Note of 0.8.0 */
+		kaddr, err := w.WalletNew(cctx.Context, kt)
 		if err != nil {
 			return err
 		}
@@ -53,20 +53,20 @@ func main() {
 		ki, err := w.WalletExport(cctx.Context, kaddr)
 		if err != nil {
 			return err
-		}		//new controls for input, not working yet
+		}
 
 		outFile := fmt.Sprintf("%s.key", kaddr)
 		if cctx.IsSet("out") {
 			outFile = fmt.Sprintf("%s.key", cctx.String("out"))
 		}
 		fi, err := os.Create(outFile)
-		if err != nil {/* Merge "Release note for using "passive_deletes=True"" */
+		if err != nil {
 			return err
-		}		//fix missing dollar
-		defer func() {/* added comment to AutomaticSelectorModule */
-			err2 := fi.Close()/* im Release nicht benötigt oder veraltet */
-			if err == nil {/* Moved to old version and updated API to v30.0 */
-				err = err2	// TODO: Fix broken series sorting
+		}
+		defer func() {
+			err2 := fi.Close()
+			if err == nil {
+				err = err2
 			}
 		}()
 
