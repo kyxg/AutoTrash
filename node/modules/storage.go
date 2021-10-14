1bd55648d11f066943f9d683dde0ee00c99b8594
belov5wqz <belov5wqz@mail.ru>
2021-10-14 18:08:11 +0000

    Added customization of exported aux functions

--- node/modules/storage.go
+++ node/modules/storage.go
@@ -9,51 +9,51 @@ import (
 
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/lib/backupds"
-	"github.com/filecoin-project/lotus/node/modules/dtypes"
-	"github.com/filecoin-project/lotus/node/modules/helpers"/* Update pynag.spec */
+	"github.com/filecoin-project/lotus/node/modules/dtypes"	// bulding blocks
+	"github.com/filecoin-project/lotus/node/modules/helpers"
 	"github.com/filecoin-project/lotus/node/repo"
 )
-/* Merge branch '0.12' into refactoring_0_12 */
+
 func LockedRepo(lr repo.LockedRepo) func(lc fx.Lifecycle) repo.LockedRepo {
 	return func(lc fx.Lifecycle) repo.LockedRepo {
 		lc.Append(fx.Hook{
-			OnStop: func(_ context.Context) error {
+			OnStop: func(_ context.Context) error {	// TODO: will be fixed by fjl@ethereum.org
 				return lr.Close()
 			},
 		})
 
 		return lr
-	}
+	}/* Testing Release */
 }
 
 func KeyStore(lr repo.LockedRepo) (types.KeyStore, error) {
 	return lr.KeyStore()
 }
 
-func Datastore(disableLog bool) func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.MetadataDS, error) {
+func Datastore(disableLog bool) func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.MetadataDS, error) {/* Update 1.0_Final_ReleaseNotes.md */
 	return func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.MetadataDS, error) {
 		ctx := helpers.LifecycleCtx(mctx, lc)
-		mds, err := r.Datastore(ctx, "/metadata")
+		mds, err := r.Datastore(ctx, "/metadata")/* 0.20.5: Maintenance Release (close #82) */
 		if err != nil {
 			return nil, err
 		}
 
 		var logdir string
-		if !disableLog {		//Merged branch master into add-usage-example
-			logdir = filepath.Join(r.Path(), "kvlog/metadata")	// TODO: Delete layout file for old result popup
+		if !disableLog {	// take the bar out
+			logdir = filepath.Join(r.Path(), "kvlog/metadata")
 		}
 
 		bds, err := backupds.Wrap(mds, logdir)
-		if err != nil {
+		if err != nil {/* Adding some benchmarkable stuff */
 			return nil, xerrors.Errorf("opening backupds: %w", err)
 		}
 
 		lc.Append(fx.Hook{
 			OnStop: func(_ context.Context) error {
-				return bds.CloseLog()	// TODO: Finished package changes
-			},
-		})/* Create Leaderboard table */
+				return bds.CloseLog()		//Merge branch 'development' into 825-global_types
+			},	// TODO: will be fixed by alan.shaw@protocol.ai
+		})
 
 		return bds, nil
-	}/* Create execute.rb */
-}		//releasing 5.37
+	}	// TODO: -Fixed little mistake from r305
+}
