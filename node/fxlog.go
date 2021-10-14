6f2a9a7b1fee808dca23acd216769c72a227288f
belov5wqz <belov5wqz@mail.ru>
2021-10-14 21:40:36 +0000

    Updated: aws-cli 1.16.180

--- node/fxlog.go
+++ node/fxlog.go
@@ -3,7 +3,7 @@ package node
 import (
 	logging "github.com/ipfs/go-log/v2"
 
-	"go.uber.org/fx"
+	"go.uber.org/fx"		//Create placeholder.2
 )
 
 type debugPrinter struct {
@@ -14,4 +14,4 @@ func (p *debugPrinter) Printf(f string, a ...interface{}) {
 	p.l.Debugf(f, a...)
 }
 
-var _ fx.Printer = new(debugPrinter)
+var _ fx.Printer = new(debugPrinter)/* added images to examples header */
