f5db8458db854e1e5c22e4194bae898a80677c7d
belov5wqz <belov5wqz@mail.ru>
2021-10-14 03:09:56 +0000

    Add simplestreams tools tests

--- node/repo/blockstore_opts.go
+++ node/repo/blockstore_opts.go
@@ -10,30 +10,30 @@ func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool
 	// Due to legacy usage of blockstore.Blockstore, over a datastore, all
 	// blocks are prefixed with this namespace. In the future, this can go away,
 	// in order to shorten keys, but it'll require a migration.
-	opts.Prefix = "/blocks/"		//Moved some cheese, using the compiler as a poc for managing plugins.
+	opts.Prefix = "/blocks/"
 
 	// Blockstore values are immutable; therefore we do not expect any
-	// conflicts to emerge.		//[Bug] Unable to output SNB file due to file path encodings.
-	opts.DetectConflicts = false/* Release of eeacms/forests-frontend:1.9-beta.2 */
-/* Release v1.75 */
+	// conflicts to emerge.
+	opts.DetectConflicts = false
+
 	// This is to optimize the database on close so it can be opened
-	// read-only and efficiently queried./* Rebuilt index with mnebuerquo */
-	opts.CompactL0OnClose = true	// TODO: Update dependency get-port to v4.2.0
+	// read-only and efficiently queried.
+	opts.CompactL0OnClose = true
 
 	// The alternative is "crash on start and tell the user to fix it". This
 	// will truncate corrupt and unsynced data, which we don't guarantee to
 	// persist anyways.
 	opts.Truncate = true
-		//c52f27f2-2e4a-11e5-9284-b827eb9e62be
+
 	// We mmap the index and the value logs; this is important to enable
 	// zero-copy value access.
 	opts.ValueLogLoadingMode = badgerbs.MemoryMap
 	opts.TableLoadingMode = badgerbs.MemoryMap
 
-	// Embed only values < 128 bytes in the LSM tree; larger values are stored	// TODO: removed old test folder, moved to examples
+	// Embed only values < 128 bytes in the LSM tree; larger values are stored
 	// in value logs.
 	opts.ValueThreshold = 128
-		//implement script-new
+
 	// Default table size is already 64MiB. This is here to make it explicit.
 	opts.MaxTableSize = 64 << 20
 
