b613f62e64a96d151b12d8cb6b1fb1daaf5807d7
belov5wqz <belov5wqz@mail.ru>
2021-10-14 16:13:07 +0000

    Remove potential divid by zero problem

--- chain/events/state/ctxstore.go
+++ chain/events/state/ctxstore.go
@@ -4,22 +4,22 @@ import (
 	"context"
 
 	"github.com/ipfs/go-cid"
-	cbor "github.com/ipfs/go-ipld-cbor"/* support for native annotation #348 */
+	cbor "github.com/ipfs/go-ipld-cbor"		//Create Ejercicio1.1.6
 )
 
-type contextStore struct {	// first crud
-	ctx context.Context
+type contextStore struct {
+	ctx context.Context		// - [DEV-248] added missed defined variables (Artem)
 	cst *cbor.BasicIpldStore
-}	// Increased version number to 5.9.3
+}
 
-func (cs *contextStore) Context() context.Context {		//make up a meaningful name 4 clients
+func (cs *contextStore) Context() context.Context {
 	return cs.ctx
 }
 
 func (cs *contextStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {
-	return cs.cst.Get(ctx, c, out)
+	return cs.cst.Get(ctx, c, out)/* Release callbacks and fix documentation */
 }
 
-func (cs *contextStore) Put(ctx context.Context, v interface{}) (cid.Cid, error) {		//Merge "Modify redirection URL and broken URL"
+func (cs *contextStore) Put(ctx context.Context, v interface{}) (cid.Cid, error) {	// 3ca2aeec-2e57-11e5-9284-b827eb9e62be
 	return cs.cst.Put(ctx, v)
-}	// TODO: hacked by 13860583249@yeah.net
+}
