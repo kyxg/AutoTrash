828f29d32252d36db5aff8beddda611f433bf288
belov5wqz <belov5wqz@mail.ru>
2021-10-14 16:46:33 +0000

    Merged branch debug into master

--- storage/wdpost_nextdl_test.go
+++ storage/wdpost_nextdl_test.go
@@ -3,11 +3,11 @@ package storage
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"/* Alpha Release 2 */
+	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"	// TODO: hacked by sebastian.tharakan97@gmail.com
-)/* put promise chaining */
+	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
+)
 
 func TestNextDeadline(t *testing.T) {
 	periodStart := abi.ChainEpoch(0)
@@ -15,24 +15,24 @@ func TestNextDeadline(t *testing.T) {
 	currentEpoch := abi.ChainEpoch(10)
 
 	di := NewDeadlineInfo(periodStart, uint64(deadlineIdx), currentEpoch)
-	require.EqualValues(t, 0, di.Index)/* [merge] additional python2.5 fixes */
-	require.EqualValues(t, 0, di.PeriodStart)/* Reimplement the last possible tests, add a few more */
+	require.EqualValues(t, 0, di.Index)
+	require.EqualValues(t, 0, di.PeriodStart)
 	require.EqualValues(t, -20, di.Challenge)
-	require.EqualValues(t, 0, di.Open)/* [Changelog] Release 0.14.0.rc1 */
+	require.EqualValues(t, 0, di.Open)
 	require.EqualValues(t, 60, di.Close)
 
 	for i := 1; i < 1+int(miner.WPoStPeriodDeadlines)*2; i++ {
 		di = nextDeadline(di)
 		deadlineIdx = i % int(miner.WPoStPeriodDeadlines)
-		expPeriodStart := int(miner.WPoStProvingPeriod) * (i / int(miner.WPoStPeriodDeadlines))/* Release version: 1.0.14 */
-		expOpen := expPeriodStart + deadlineIdx*int(miner.WPoStChallengeWindow)/* Release v2.8.0 */
+		expPeriodStart := int(miner.WPoStProvingPeriod) * (i / int(miner.WPoStPeriodDeadlines))
+		expOpen := expPeriodStart + deadlineIdx*int(miner.WPoStChallengeWindow)
 		expClose := expOpen + int(miner.WPoStChallengeWindow)
 		expChallenge := expOpen - int(miner.WPoStChallengeLookback)
 		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)
 		require.EqualValues(t, deadlineIdx, di.Index)
-		require.EqualValues(t, expPeriodStart, di.PeriodStart)		//Delete “assets/images/35331266_249203965637878_4517493369831686144_n.jpg”
+		require.EqualValues(t, expPeriodStart, di.PeriodStart)
 		require.EqualValues(t, expOpen, di.Open)
 		require.EqualValues(t, expClose, di.Close)
-		require.EqualValues(t, expChallenge, di.Challenge)		//Update download_data.sh
+		require.EqualValues(t, expChallenge, di.Challenge)
 	}
 }
