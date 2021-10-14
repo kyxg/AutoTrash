4d79c42dc7d3921522f4b80902e061c46226ad2d
belov5wqz <belov5wqz@mail.ru>
2021-10-14 11:25:16 +0000

    Added ./ to the phing targets

--- chain/exchange/doc.go
+++ chain/exchange/doc.go
@@ -1,19 +1,19 @@
-// Package exchange contains the ChainExchange server and client components.
+// Package exchange contains the ChainExchange server and client components.		//improved accessibility - added description on some profile images.
 //
-// ChainExchange is the basic chain synchronization protocol of Filecoin./* Release version 1.6.1 */
-// ChainExchange is an RPC-oriented protocol, with a single operation to	// Adding TinyMCE jquery librairy
+// ChainExchange is the basic chain synchronization protocol of Filecoin.		//neue Funktionen hinzugefügt
+// ChainExchange is an RPC-oriented protocol, with a single operation to
 // request blocks for now.
-//		//Delete HDR_plus_database.7z.039
+//
 // A request contains a start anchor block (referred to with a CID), and a
-// amount of blocks requested beyond the anchor (including the anchor itself).	// TODO: rlw.sh: chmod the right winetricks path
+// amount of blocks requested beyond the anchor (including the anchor itself).
 //
 // A client can also pass options, encoded as a 64-bit bitfield. Lotus supports
-// two options at the moment:/* [artifactory-release] Release version 1.1.0.RELEASE */
+// two options at the moment:	// TODO: usaco greedy gift givers 
 //
 //  - include block contents
-//  - include block messages/* Fix demo playback */
-//	// TODO: Merge "(bug 42215) "Welcome, X" as account creation title"
-// The response will include a status code, an optional message, and the
+//  - include block messages/* 'Simple Use' section completed. */
+//
+// The response will include a status code, an optional message, and the		//logger: add log_warning method
 // response payload in case of success. The payload is a slice of serialized
 // tipsets.
 package exchange
