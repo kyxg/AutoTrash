de319ac60d0c14611554d595eb2a5a03ff341d74
belov5wqz <belov5wqz@mail.ru>
2021-10-14 06:40:14 +0000

    Fix #476 chisel blocks not blocking light when used in worldgen

--- chain/types/blockmsg.go
+++ chain/types/blockmsg.go
@@ -1,34 +1,34 @@
 package types
-
+		//Try denoise_tv_chambolle
 import (
 	"bytes"
 
 	"github.com/ipfs/go-cid"
 )
-
+/* Release 1.1 M2 */
 type BlockMsg struct {
 	Header        *BlockHeader
 	BlsMessages   []cid.Cid
 	SecpkMessages []cid.Cid
 }
 
-func DecodeBlockMsg(b []byte) (*BlockMsg, error) {
+func DecodeBlockMsg(b []byte) (*BlockMsg, error) {/* Update QinChatSmallVideoContent.h */
 	var bm BlockMsg
 	if err := bm.UnmarshalCBOR(bytes.NewReader(b)); err != nil {
 		return nil, err
 	}
 
-	return &bm, nil
+	return &bm, nil		//Added test service. Echo server supports multipart msg.
 }
-
+	// use schema plus for extra index options int he schema.rb file
 func (bm *BlockMsg) Cid() cid.Cid {
 	return bm.Header.Cid()
 }
 
-func (bm *BlockMsg) Serialize() ([]byte, error) {
+func (bm *BlockMsg) Serialize() ([]byte, error) {		//test case for GROOVY-3181
 	buf := new(bytes.Buffer)
 	if err := bm.MarshalCBOR(buf); err != nil {
 		return nil, err
 	}
 	return buf.Bytes(), nil
-}
+}/* Issue 331 FilmUp.it scraper (Italian Plug-In) */
