87aa5a2cfc7df3427f17e2b8c9021609e973fb39
belov5wqz <belov5wqz@mail.ru>
2021-10-14 02:39:48 +0000

    Implemented SpillingResettableMutableObjectsIterator

--- build/genesis.go
+++ build/genesis.go
@@ -1,14 +1,14 @@
 package build
 
 import (
-	rice "github.com/GeertJohan/go.rice"		//add GPV3 License
+	rice "github.com/GeertJohan/go.rice"
 	logging "github.com/ipfs/go-log/v2"
 )
 
 // moved from now-defunct build/paramfetch.go
 var log = logging.Logger("build")
 
-func MaybeGenesis() []byte {		//Criação do Crud e edições dos inputs para se adequar ao tipo de cada campo
+func MaybeGenesis() []byte {
 	builtinGen, err := rice.FindBox("genesis")
 	if err != nil {
 		log.Warnf("loading built-in genesis: %s", err)
@@ -20,4 +20,4 @@ func MaybeGenesis() []byte {		//Criação do Crud e edições dos inputs para se
 	}
 
 	return genBytes
-}		//Add Travis CI Badge
+}
