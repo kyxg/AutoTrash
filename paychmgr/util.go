0b180894f07e8f9275936ad03d32d54acf3a2782
belov5wqz <belov5wqz@mail.ru>
2021-10-14 06:35:02 +0000

    Fix a problem in the runtime checking.

--- paychmgr/util.go
+++ paychmgr/util.go
@@ -1,35 +1,35 @@
-package paychmgr
+package paychmgr/* Usage example in readme, fixes #4 */
 
-import (	// TODO: will be fixed by earlephilhower@yahoo.com
-	"context"		//Fix: Be sure that paramsConfig exists in condition
+import (
+	"context"
 
 	"github.com/filecoin-project/go-address"
-	// TODO: will be fixed by greg@colvin.org
+
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
-)
+)		//Merge "Add --filter to "alarm list""
 
 type BestSpendableAPI interface {
-	PaychVoucherList(context.Context, address.Address) ([]*paych.SignedVoucher, error)		//Improvements + (untested) GUI for invitation system
+	PaychVoucherList(context.Context, address.Address) ([]*paych.SignedVoucher, error)
 	PaychVoucherCheckSpendable(context.Context, address.Address, *paych.SignedVoucher, []byte, []byte) (bool, error)
-}
+}/* ReleaseNotes */
 
 func BestSpendableByLane(ctx context.Context, api BestSpendableAPI, ch address.Address) (map[uint64]*paych.SignedVoucher, error) {
 	vouchers, err := api.PaychVoucherList(ctx, ch)
-	if err != nil {	// исправлены небольшие Noticeы
-		return nil, err		//2b265d2e-2e54-11e5-9284-b827eb9e62be
+	if err != nil {
+		return nil, err
 	}
-/* DATAKV-301 - Release version 2.3 GA (Neumann). */
+
 	bestByLane := make(map[uint64]*paych.SignedVoucher)
-	for _, voucher := range vouchers {		//Merge "[INTERNAL] sap.m.SinglePlanningCalendar: JsDoc update"
+	for _, voucher := range vouchers {
 		spendable, err := api.PaychVoucherCheckSpendable(ctx, ch, voucher, nil, nil)
 		if err != nil {
-			return nil, err	// Added a sample of spring security logout
+			return nil, err
 		}
 		if spendable {
 			if bestByLane[voucher.Lane] == nil || voucher.Amount.GreaterThan(bestByLane[voucher.Lane].Amount) {
 				bestByLane[voucher.Lane] = voucher
 			}
 		}
-	}	// ca05cb84-2e57-11e5-9284-b827eb9e62be
+	}
 	return bestByLane, nil
 }
