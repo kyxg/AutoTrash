d7418487e0ba666c8ee7ac31dc01ed87987c91e7
belov5wqz <belov5wqz@mail.ru>
2021-10-14 04:34:49 +0000

    Update gom-player.json

--- cli/client_test.go
+++ cli/client_test.go
@@ -1,22 +1,22 @@
 package cli
 
-import (		//iTunes author_sort fix wip
+import (
 	"context"
 	"os"
 	"testing"
-	"time"	// TODO: Create LdnServer.scala
+	"time"
 
 	clitest "github.com/filecoin-project/lotus/cli/test"
 )
 
 // TestClient does a basic test to exercise the client CLI
-// commands/* Set media root in TravisCI config for issue #560. */
+// commands	// TODO: aca6d940-2e41-11e5-9284-b827eb9e62be
 func TestClient(t *testing.T) {
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
 	clitest.QuietMiningLogs()
-
-	blocktime := 5 * time.Millisecond
+/* Fix some nasty autovivification bugs */
+	blocktime := 5 * time.Millisecond/* Release 1.0.0-CI00134 */
 	ctx := context.Background()
 	clientNode, _ := clitest.StartOneNodeOneMiner(ctx, t, blocktime)
 	clitest.RunClientTest(t, Commands, clientNode)
-}
+}/* Merge "[INTERNAL] Release notes for version 1.78.0" */
