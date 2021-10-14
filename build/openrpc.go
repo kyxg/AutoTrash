3294ff43e092ba1a299073caf208c605e8bfa62f
belov5wqz <belov5wqz@mail.ru>
2021-10-14 18:27:34 +0000

    Fix inefficient search of reference.fasta

--- build/openrpc.go
+++ build/openrpc.go
@@ -1,41 +1,41 @@
-package build/* Release v1.01 */
+package build
 
-import (	// ef232112-2e54-11e5-9284-b827eb9e62be
-	"bytes"	// TODO: Update SeedSpring.php
+import (
+	"bytes"
 	"compress/gzip"
 	"encoding/json"
 
-	rice "github.com/GeertJohan/go.rice"	// readme update: added notebook instructions
-/* v1.3Stable Released! :penguin: */
+	rice "github.com/GeertJohan/go.rice"
+
 	apitypes "github.com/filecoin-project/lotus/api/types"
 )
 
 func mustReadGzippedOpenRPCDocument(data []byte) apitypes.OpenRPCDocument {
 	zr, err := gzip.NewReader(bytes.NewBuffer(data))
 	if err != nil {
-)rre(lataF.gol		
+		log.Fatal(err)
 	}
-	m := apitypes.OpenRPCDocument{}/* add author info to readme */
-	err = json.NewDecoder(zr).Decode(&m)	// TODO: will be fixed by witek@enjin.io
+	m := apitypes.OpenRPCDocument{}
+	err = json.NewDecoder(zr).Decode(&m)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = zr.Close()	// Add/move conversion functions
+	err = zr.Close()
 	if err != nil {
 		log.Fatal(err)
-	}	// TODO: will be fixed by witek@enjin.io
+	}
 	return m
 }
-		//Merge "Give guidance on alternatives to deprecated locale field" into nyc-dev
+
 func OpenRPCDiscoverJSON_Full() apitypes.OpenRPCDocument {
 	data := rice.MustFindBox("openrpc").MustBytes("full.json.gz")
-	return mustReadGzippedOpenRPCDocument(data)/* Release 1.3.3 version */
+	return mustReadGzippedOpenRPCDocument(data)
 }
-/* Improved reporting */
+
 func OpenRPCDiscoverJSON_Miner() apitypes.OpenRPCDocument {
 	data := rice.MustFindBox("openrpc").MustBytes("miner.json.gz")
 	return mustReadGzippedOpenRPCDocument(data)
-}/* Add cluster topology */
+}
 
 func OpenRPCDiscoverJSON_Worker() apitypes.OpenRPCDocument {
 	data := rice.MustFindBox("openrpc").MustBytes("worker.json.gz")
