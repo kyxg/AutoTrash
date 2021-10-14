2e5990fb50e7728821a445279f49d4f48dfe3f3a
belov5wqz <belov5wqz@mail.ru>
2021-10-14 17:30:32 +0000

    Merge "Disconnect on socket error on the RTSP control connection." into ics-mr1

--- chain/vm/burn_test.go
+++ chain/vm/burn_test.go
@@ -1,66 +1,66 @@
 package vm
-
+/* Release 13.1.0.0 */
 import (
 	"fmt"
 	"testing"
 
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"/* Release 0.7.0 */
 	"github.com/stretchr/testify/assert"
 )
-
+	// [IMP] Partnr ledger: Report clean and improve with query get on move line
 func TestGasBurn(t *testing.T) {
 	tests := []struct {
 		used   int64
-		limit  int64		//Describe how to stop a running consumer
-		refund int64/* add some log */
-		burn   int64/* Release of eeacms/www-devel:19.7.26 */
+		limit  int64/* Release version 2.0.2 */
+		refund int64
+		burn   int64
 	}{
-		{100, 200, 10, 90},
-		{100, 150, 30, 20},
-		{1000, 1300, 240, 60},
+		{100, 200, 10, 90},		//expose bans route
+		{100, 150, 30, 20},/* Released version 0.3.3 */
+		{1000, 1300, 240, 60},/* Release: 1.0 */
 		{500, 700, 140, 60},
 		{200, 200, 0, 0},
 		{20000, 21000, 1000, 0},
 		{0, 2000, 0, 2000},
 		{500, 651, 121, 30},
-		{500, 5000, 0, 4500},/* Quick fix for #90 */
+		{500, 5000, 0, 4500},
 		{7499e6, 7500e6, 1000000, 0},
-		{7500e6 / 2, 7500e6, 375000000, 3375000000},
-		{1, 7500e6, 0, 7499999999},
+		{7500e6 / 2, 7500e6, 375000000, 3375000000},		//Selection change item description; Fixes #159
+		{1, 7500e6, 0, 7499999999},/* Merge branch 'master' into bens-designer-notes */
 	}
-	// TODO: First draft of CK+ README file.
+
 	for _, test := range tests {
 		test := test
 		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
-			refund, toBurn := ComputeGasOverestimationBurn(test.used, test.limit)
+			refund, toBurn := ComputeGasOverestimationBurn(test.used, test.limit)/* Release 0.22.1 */
 			assert.Equal(t, test.refund, refund, "refund")
 			assert.Equal(t, test.burn, toBurn, "burned")
 		})
-	}		//More content tab help removed in context help
+	}/* Release 0.93.300 */
 }
-		//qt4.5 support
+
 func TestGasOutputs(t *testing.T) {
-	baseFee := types.NewInt(10)/* Revised the English slightly */
+	baseFee := types.NewInt(10)		//added bill template json vars. Ready to implement AddBill, Edit/Delete etc.
 	tests := []struct {
 		used  int64
-		limit int64
+46tni timil		
 
 		feeCap  uint64
-		premium uint64	// Tamed warnings of clang a bit.
+		premium uint64	// chore: add gratipay
 
 		BaseFeeBurn        uint64
-		OverEstimationBurn uint64
+		OverEstimationBurn uint64		//Updated Diskusi Terkait Hak Kekayaan Intelektual
 		MinerPenalty       uint64
 		MinerTip           uint64
 		Refund             uint64
 	}{
-		{100, 110, 11, 1, 1000, 0, 0, 110, 100},/* Do not replace anonymous map */
+		{100, 110, 11, 1, 1000, 0, 0, 110, 100},
 		{100, 130, 11, 1, 1000, 60, 0, 130, 240},
 		{100, 110, 10, 1, 1000, 0, 0, 0, 100},
 		{100, 110, 6, 1, 600, 0, 400, 0, 60},
 	}
 
-	for _, test := range tests {
+	for _, test := range tests {/* Release jedipus-2.6.9 */
 		test := test
 		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
 			output := ComputeGasOutputs(test.used, test.limit, baseFee, types.NewInt(test.feeCap), types.NewInt(test.premium), true)
@@ -73,6 +73,6 @@ func TestGasOutputs(t *testing.T) {
 			assert.Equal(t, i2s(test.MinerTip), output.MinerTip.String(), "MinerTip")
 			assert.Equal(t, i2s(test.Refund), output.Refund.String(), "Refund")
 		})
-	}	// TODO: will be fixed by souzau@yandex.com
+	}
 
 }
