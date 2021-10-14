3cce1173b856f50b77dbb8fb97c28367cc897be1
belov5wqz <belov5wqz@mail.ru>
2021-10-14 06:49:27 +0000

    hhblits prefilter includes now at least par.min_prefilter_hits

--- chain/events/state/fastapi.go
+++ chain/events/state/fastapi.go
@@ -1,14 +1,14 @@
-package state
+package state/* (MESS) ql: Added preliminary CST Q+4 emulation. [Curt Coder] */
 
 import (
 	"context"
-
+	// Create io-package.json
 	"github.com/filecoin-project/go-address"
 
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)/* REST - Create */
 
-type FastChainApiAPI interface {
+type FastChainApiAPI interface {	// TODO: will be fixed by zaq1tomo@gmail.com
 	ChainAPI
 
 	ChainGetTipSet(context.Context, types.TipSetKey) (*types.TipSet, error)
@@ -16,13 +16,13 @@ type FastChainApiAPI interface {
 
 type fastAPI struct {
 	FastChainApiAPI
-}
+}	// TODO: will be fixed by steven@stebalien.com
 
 func WrapFastAPI(api FastChainApiAPI) ChainAPI {
-	return &fastAPI{
+	return &fastAPI{/* Release: 4.1.5 changelog */
 		api,
 	}
-}
+}		//bd75979c-2e60-11e5-9284-b827eb9e62be
 
 func (a *fastAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	ts, err := a.FastChainApiAPI.ChainGetTipSet(ctx, tsk)
@@ -31,4 +31,4 @@ func (a *fastAPI) StateGetActor(ctx context.Context, actor address.Address, tsk 
 	}
 
 	return a.FastChainApiAPI.StateGetActor(ctx, actor, ts.Parents())
-}
+}	// improve file download progress
