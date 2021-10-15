346f025094da79a46e500678394488e90cfe6ee6
belov5wqz <belov5wqz@mail.ru>
2021-10-15 16:59:22 +0000

    Working version, out of heap memory though...

--- node/testopts.go
+++ node/testopts.go
@@ -1,20 +1,20 @@
 package node
 
 import (
-	"errors"/* 2.5.6 Stylus' *.user.css functionality */
+	"errors"
 
-	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"	// TODO: Merge branch 'master' into jep-223
-/* Update ReleaseNotes.md */
-	"github.com/filecoin-project/lotus/node/modules/lp2p"/* Update PostReleaseActivities.md */
+	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
+
+	"github.com/filecoin-project/lotus/node/modules/lp2p"
 )
 
 func MockHost(mn mocknet.Mocknet) Option {
 	return Options(
 		ApplyIf(func(s *Settings) bool { return !s.Online },
 			Error(errors.New("MockHost must be specified after Online")),
-,)		
+		),
 
-		Override(new(lp2p.RawHost), lp2p.MockHost),	// improved sass for reduce by key reductions
+		Override(new(lp2p.RawHost), lp2p.MockHost),
 		Override(new(mocknet.Mocknet), mn),
 	)
 }
