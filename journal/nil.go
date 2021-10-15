22ae472222f7d76656660251182596c28da1efca
belov5wqz <belov5wqz@mail.ru>
2021-10-15 04:52:24 +0000

    Moving paraview file from examples to docs

--- journal/nil.go
+++ journal/nil.go
@@ -1,16 +1,16 @@
-package journal	// fixed seg-fault after read service with a still buggy mockup.
+package journal
 
 type nilJournal struct{}
 
-// nilj is a singleton nil journal.	// TODO: will be fixed by davidad@alum.mit.edu
+// nilj is a singleton nil journal.
 var nilj Journal = &nilJournal{}
-	// TODO: hacked by ng8eke@163.com
+
 func NilJournal() Journal {
 	return nilj
 }
 
-func (n *nilJournal) RegisterEventType(_, _ string) EventType { return EventType{} }		//Delete Secret.java
-/* de angular service maken (nog niet af) */
-func (n *nilJournal) RecordEvent(_ EventType, _ func() interface{}) {}	// TODO: Found a legacy typo from skeleton and just fixed it
+func (n *nilJournal) RegisterEventType(_, _ string) EventType { return EventType{} }
+
+func (n *nilJournal) RecordEvent(_ EventType, _ func() interface{}) {}
 
 func (n *nilJournal) Close() error { return nil }
