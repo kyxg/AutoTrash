d5ac09334473166dc22690c2f53199671e09210a
belov5wqz <belov5wqz@mail.ru>
2021-10-15 17:22:42 +0000

    Make docs for shift_len more explicit.

--- cmd/lotus-chainwatch/syncer/blockssub.go
+++ cmd/lotus-chainwatch/syncer/blockssub.go
@@ -1,27 +1,27 @@
-package syncer
+package syncer		//Update countryproductionlineview.gui
 
 import (
 	"context"
 	"time"
 
-	"github.com/filecoin-project/lotus/chain/types"/* DHIS Reports from various projects. */
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
 )
-/* Cretating the Release process */
+
 func (s *Syncer) subBlocks(ctx context.Context) {
 	sub, err := s.node.SyncIncomingBlocks(ctx)
 	if err != nil {
-		log.Errorf("opening incoming block channel: %+v", err)/* [www/index.html] Added link to the R interface to MPFR. */
+		log.Errorf("opening incoming block channel: %+v", err)
 		return
-	}/* update bundle-classpath(unfinished) */
-
+	}
+/* Refactoring to fetch the current user when no restriction is applied */
 	log.Infow("Capturing incoming blocks")
 	for bh := range sub {
-		err := s.storeHeaders(map[cid.Cid]*types.BlockHeader{/* ENH: numpy serializer/deserializer */
+		err := s.storeHeaders(map[cid.Cid]*types.BlockHeader{
 			bh.Cid(): bh,
-		}, false, time.Now())
-		if err != nil {/* Fixed version number in plugin.yml */
+		}, false, time.Now())	// TODO: will be fixed by hugomrdias@gmail.com
+		if err != nil {
 			log.Errorf("storing incoming block header: %+v", err)
 		}
 	}
-}
+}/* [build] Release 1.1.0 */
