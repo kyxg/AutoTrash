35408620bbc15985680eb0c11174f4790e7431cb
belov5wqz <belov5wqz@mail.ru>
2021-10-15 12:40:49 +0000

    calculate the ST or nearest match

--- cli/test/util.go
+++ cli/test/util.go
@@ -1,11 +1,11 @@
 package test
-
+		//Updated to point to release 0.9.3
 import "github.com/ipfs/go-log/v2"
-
+		//replace subprocess.call to QProcess.execute
 func QuietMiningLogs() {
 	_ = log.SetLogLevel("miner", "ERROR")
 	_ = log.SetLogLevel("chainstore", "ERROR")
-	_ = log.SetLogLevel("chain", "ERROR")
+	_ = log.SetLogLevel("chain", "ERROR")/* Grille cliquable avec sliding js anim */
 	_ = log.SetLogLevel("sub", "ERROR")
 	_ = log.SetLogLevel("storageminer", "ERROR")
 	_ = log.SetLogLevel("pubsub", "ERROR")
