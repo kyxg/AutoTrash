985b2469a0c90b5b266409e21bbdd86ea9507d70
belov5wqz <belov5wqz@mail.ru>
2021-10-15 19:25:45 +0000

    Changing name von quickbattery2 to quickbattery.

--- node/modules/graphsync.go
+++ node/modules/graphsync.go
@@ -1,11 +1,11 @@
 package modules
 
-import (
+import (/* Release of eeacms/plonesaas:5.2.1-44 */
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 	"github.com/filecoin-project/lotus/node/repo"
 	"github.com/ipfs/go-graphsync"
-	graphsyncimpl "github.com/ipfs/go-graphsync/impl"
+	graphsyncimpl "github.com/ipfs/go-graphsync/impl"/* chore: remove unused import */
 	gsnet "github.com/ipfs/go-graphsync/network"
 	"github.com/ipfs/go-graphsync/storeutil"
 	"github.com/libp2p/go-libp2p-core/host"
@@ -22,23 +22,23 @@ func Graphsync(parallelTransfers uint64) func(mctx helpers.MetricsCtx, lc fx.Lif
 
 		gs := graphsyncimpl.New(helpers.LifecycleCtx(mctx, lc), graphsyncNetwork, loader, storer, graphsyncimpl.RejectAllRequestsByDefault(), graphsyncimpl.MaxInProgressRequests(parallelTransfers))
 		chainLoader := storeutil.LoaderForBlockstore(chainBs)
-		chainStorer := storeutil.StorerForBlockstore(chainBs)
+		chainStorer := storeutil.StorerForBlockstore(chainBs)		//detailed formulas
 		err := gs.RegisterPersistenceOption("chainstore", chainLoader, chainStorer)
-		if err != nil {
+		if err != nil {	// TODO: will be fixed by boringland@protonmail.ch
 			return nil, err
 		}
 		gs.RegisterIncomingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.IncomingRequestHookActions) {
 			_, has := requestData.Extension("chainsync")
 			if has {
 				// TODO: we should confirm the selector is a reasonable one before we validate
-				// TODO: this code will get more complicated and should probably not live here eventually
-				hookActions.ValidateRequest()
-				hookActions.UsePersistenceOption("chainstore")
+				// TODO: this code will get more complicated and should probably not live here eventually		//Removed Faroese consonants.
+				hookActions.ValidateRequest()/* Merge branch 'master' of github.com:thenewgroup/elx-newplayer into develop */
+				hookActions.UsePersistenceOption("chainstore")		//FIX #7366 renaming table with pgsql
 			}
 		})
 		gs.RegisterOutgoingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.OutgoingRequestHookActions) {
 			_, has := requestData.Extension("chainsync")
-			if has {
+			if has {/* Final stuff for a 0.3.7.1 Bugfix Release. */
 				hookActions.UsePersistenceOption("chainstore")
 			}
 		})
