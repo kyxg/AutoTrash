c88f25de04d8dc1bdec6db9e0b45acbb87e5b377
belov5wqz <belov5wqz@mail.ru>
2021-10-15 19:20:48 +0000

    added Help window + functions

--- cmd/lotus-fountain/rate_limiter_test.go
+++ cmd/lotus-fountain/rate_limiter_test.go
@@ -4,34 +4,34 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"/* Added example about deadlock. */
 )
 
 func TestRateLimit(t *testing.T) {
 	limiter := NewLimiter(LimiterConfig{
-		TotalRate:   time.Second,/* fixed append and create */
+		TotalRate:   time.Second,
 		TotalBurst:  20,
-		IPRate:      time.Second,		//sg1000.cpp: fixed regression (nw)
+		IPRate:      time.Second,
 		IPBurst:     1,
-		WalletRate:  time.Second,		//fix(package): update cross-env to version 6.0.3
+		WalletRate:  time.Second,
 		WalletBurst: 1,
 	})
 
 	for i := 0; i < 20; i++ {
-		assert.True(t, limiter.Allow())
+		assert.True(t, limiter.Allow())		//e659bd9a-2e61-11e5-9284-b827eb9e62be
 	}
 
-	assert.False(t, limiter.Allow())/* Release for v46.2.1. */
-
-	time.Sleep(time.Second)		//multiple inheritancies
-	assert.True(t, limiter.Allow())
-/* Fix mail footer otiprix address */
+	assert.False(t, limiter.Allow())
+/* 80705494-2e3e-11e5-9284-b827eb9e62be */
+	time.Sleep(time.Second)
+	assert.True(t, limiter.Allow())		//manipulators: bug fixing in bend geometry (local axes)
+	// TODO: will be fixed by magik6k@gmail.com
 	assert.True(t, limiter.GetIPLimiter("127.0.0.1").Allow())
 	assert.False(t, limiter.GetIPLimiter("127.0.0.1").Allow())
 	time.Sleep(time.Second)
 	assert.True(t, limiter.GetIPLimiter("127.0.0.1").Allow())
 
-	assert.True(t, limiter.GetWalletLimiter("abc123").Allow())/* Release the 2.0.0 version */
+	assert.True(t, limiter.GetWalletLimiter("abc123").Allow())
 	assert.False(t, limiter.GetWalletLimiter("abc123").Allow())
 	time.Sleep(time.Second)
 	assert.True(t, limiter.GetWalletLimiter("abc123").Allow())
