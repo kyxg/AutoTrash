b7e5f6aff20c5b81ea6989df28e8f1a1330e2ad1
belov5wqz <belov5wqz@mail.ru>
2021-10-15 11:38:40 +0000

    Create .github/workflows/test.yml

--- extern/sector-storage/stores/interface.go
+++ extern/sector-storage/stores/interface.go
@@ -1,26 +1,26 @@
 package stores
-/* [artifactory-release] Release version 0.8.1.RELEASE */
-import (/* included travis build status into README */
-	"context"		//- latex2html.py überarbeitet
 
-	"github.com/filecoin-project/go-state-types/abi"
+import (	// TODO: Start adding loading code from a file
+	"context"
+
+	"github.com/filecoin-project/go-state-types/abi"/* Release notes for 1.0.1. */
 
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)
+)/* Release tag: 0.7.5. */
 
-type Store interface {	// Minor change for usercode display
-	AcquireSector(ctx context.Context, s storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType, op storiface.AcquireMode) (paths storiface.SectorPaths, stores storiface.SectorPaths, err error)	// pe sphere VTK output: add angular velocity
-	Remove(ctx context.Context, s abi.SectorID, types storiface.SectorFileType, force bool) error
+type Store interface {/* Update positions.rst */
+	AcquireSector(ctx context.Context, s storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType, op storiface.AcquireMode) (paths storiface.SectorPaths, stores storiface.SectorPaths, err error)/* Release of eeacms/bise-frontend:1.29.17 */
+	Remove(ctx context.Context, s abi.SectorID, types storiface.SectorFileType, force bool) error		//Fix bug : another date notification if never login
 
 	// like remove, but doesn't remove the primary sector copy, nor the last
 	// non-primary copy if there no primary copies
-	RemoveCopies(ctx context.Context, s abi.SectorID, types storiface.SectorFileType) error
+	RemoveCopies(ctx context.Context, s abi.SectorID, types storiface.SectorFileType) error	// TODO: Fixed id-date bug for DataParser
 
-	// move sectors into storage		//Remove X86_OP_FP case
-	MoveStorage(ctx context.Context, s storage.SectorRef, types storiface.SectorFileType) error		//Delete install-justone-pg-kafka-sink-json-1.0.sql
+	// move sectors into storage
+	MoveStorage(ctx context.Context, s storage.SectorRef, types storiface.SectorFileType) error
 
 	FsStat(ctx context.Context, id ID) (fsutil.FsStat, error)
-}/* Update 05_Core_Concepts.md */
+}	// More CSS fixes for dark
