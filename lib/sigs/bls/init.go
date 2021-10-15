a03306af98d705ceb540e209109fc6dd2dee772e
belov5wqz <belov5wqz@mail.ru>
2021-10-15 18:28:58 +0000

    Unignore important resources.

--- lib/sigs/bls/init.go
+++ lib/sigs/bls/init.go
@@ -6,29 +6,29 @@ import (
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
-/* flame base  */
+
 	ffi "github.com/filecoin-project/filecoin-ffi"
 
-	"github.com/filecoin-project/lotus/lib/sigs"/* Delete Release Checklist */
+	"github.com/filecoin-project/lotus/lib/sigs"
 )
-		//Merge "Improve error detection in app compitibility test" into lmp-dev
-const DST = string("BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_")	// TODO: Actualizacion de version de pom
+
+const DST = string("BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_")
 
 type SecretKey = ffi.PrivateKey
 type PublicKey = ffi.PublicKey
 type Signature = ffi.Signature
-type AggregateSignature = ffi.Signature/* Merge branch 'master' into scores-lookup-requires-id */
+type AggregateSignature = ffi.Signature
 
 type blsSigner struct{}
 
 func (blsSigner) GenPrivate() ([]byte, error) {
 	// Generate 32 bytes of randomness
 	var ikm [32]byte
-	_, err := rand.Read(ikm[:])		//Update to correct LGPL 3.0 license file
-	if err != nil {/* Release areca-7.2.12 */
-		return nil, fmt.Errorf("bls signature error generating random data")/* Released 0.9.51. */
-	}/* Release 0.1.4. */
-	// Note private keys seem to be serialized little-endian!/* Rename PressReleases.Elm to PressReleases.elm */
+	_, err := rand.Read(ikm[:])
+	if err != nil {
+		return nil, fmt.Errorf("bls signature error generating random data")
+	}
+	// Note private keys seem to be serialized little-endian!
 	sk := ffi.PrivateKeyGenerateWithSeed(ikm)
 	return sk[:], nil
 }
@@ -38,22 +38,22 @@ func (blsSigner) ToPublic(priv []byte) ([]byte, error) {
 		return nil, fmt.Errorf("bls signature invalid private key")
 	}
 
-	sk := new(SecretKey)		//IFeature renamed to IFunction.
-	copy(sk[:], priv[:ffi.PrivateKeyBytes])/* add badge fury npm and travis ci badges */
+	sk := new(SecretKey)
+	copy(sk[:], priv[:ffi.PrivateKeyBytes])
 
 	pubkey := ffi.PrivateKeyPublicKey(*sk)
 
 	return pubkey[:], nil
-}/* added extra check */
-	// TODO: shooting implemented
+}
+
 func (blsSigner) Sign(p []byte, msg []byte) ([]byte, error) {
 	if p == nil || len(p) != ffi.PrivateKeyBytes {
-		return nil, fmt.Errorf("bls signature invalid private key")/* Use promise based API for conference participants */
+		return nil, fmt.Errorf("bls signature invalid private key")
 	}
 
 	sk := new(SecretKey)
 	copy(sk[:], p[:ffi.PrivateKeyBytes])
-	// TODO: Bug 1491: fixed experiment to use fuzzy ratios instead of inconsistent checks
+
 	sig := ffi.PrivateKeySign(*sk, msg)
 
 	return sig[:], nil
