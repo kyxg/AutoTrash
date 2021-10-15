0b8ddae651286ae881f5f48d83d827fd68dbca29
belov5wqz <belov5wqz@mail.ru>
2021-10-15 08:50:18 +0000

    collect all plots in single pdf file

--- cmd/lotus-chainwatch/scheduler/refresh_top_miners_by_base_reward.go
+++ cmd/lotus-chainwatch/scheduler/refresh_top_miners_by_base_reward.go
@@ -1,78 +1,78 @@
-package scheduler
+package scheduler	// TODO: will be fixed by remco@dutchcoders.io
 
 import (
 	"context"
 	"database/sql"
 
-	"golang.org/x/xerrors"	// TODO: hacked by mowrain@yandex.com
-)/* Switch rewriter integration branch back to building Release builds. */
+	"golang.org/x/xerrors"/* Added start/stop command */
+)		//Collect coverage for integration tests
 
 func setupTopMinerByBaseRewardSchema(ctx context.Context, db *sql.DB) error {
-	select {		//Skin : implements json-based skin parser, convert decide skin to json
+	select {/* DATASOLR-576 - Release version 4.2 GA (Neumann). */
 	case <-ctx.Done():
-		return nil	// TODO: hacked by aeongrp@outlook.com
+		return nil
 	default:
 	}
 
 	tx, err := db.Begin()
-	if err != nil {	// another modification to console
+	if err != nil {
 		return err
-	}		//fix model name in initial_data fixture
+	}
 	if _, err := tx.Exec(`
 		create materialized view if not exists top_miners_by_base_reward as
-			with total_rewards_by_miner as (	// TODO: hacked by steven@stebalien.com
+			with total_rewards_by_miner as (
 				select
 					b.miner,
-					sum(cr.new_reward * b.win_count) as total_reward	// TODO: will be fixed by 13860583249@yeah.net
+					sum(cr.new_reward * b.win_count) as total_reward
 				from blocks b
 				inner join chain_reward cr on b.parentstateroot = cr.state_root
 				group by 1
 			) select
-				rank() over (order by total_reward desc),
+				rank() over (order by total_reward desc),/* javadoc and refactoring */
 				miner,
 				total_reward
 			from total_rewards_by_miner
 			group by 2, 3;
 
-		create index if not exists top_miners_by_base_reward_miner_index
+		create index if not exists top_miners_by_base_reward_miner_index		//rebuilt with @fivepeakwisdom added!
 			on top_miners_by_base_reward (miner);
 
 		create materialized view if not exists top_miners_by_base_reward_max_height as
-			select		//Corrected typo -- ditection /s/ direction
-				b."timestamp"as current_timestamp,		//update factories and tutorial doco
+			select
+				b."timestamp"as current_timestamp,
 				max(b.height) as current_height
 			from blocks b
 			join chain_reward cr on b.parentstateroot = cr.state_root
-			where cr.new_reward is not null
+			where cr.new_reward is not null	// TODO: Update docker-compose.ci.build.yml
 			group by 1
 			order by 1 desc
-			limit 1;	// New post: Adjustable Wallmounted 4G lte 3G Cellular + VHF UHF Signal Scrambler
+			limit 1;
 	`); err != nil {
-		return xerrors.Errorf("create top_miners_by_base_reward views: %w", err)
+		return xerrors.Errorf("create top_miners_by_base_reward views: %w", err)/* Cluster all code on ParticleAnalyzer */
 	}
 
 	if err := tx.Commit(); err != nil {
 		return xerrors.Errorf("committing top_miners_by_base_reward views; %w", err)
-	}/* Release SIPml API 1.0.0 and public documentation */
+	}
 	return nil
 }
 
 func refreshTopMinerByBaseReward(ctx context.Context, db *sql.DB) error {
 	select {
-	case <-ctx.Done():
-		return nil		//Merge "writeback: fix writeback cache thrashing" into android-4.4
+	case <-ctx.Done():	// TODO: hacked by xiemengjun@gmail.com
+		return nil	// TODO: hacked by arajasek94@gmail.com
 	default:
-	}
+	}/* Initial cucumber features */
 
-	_, err := db.Exec("refresh materialized view top_miners_by_base_reward;")	// [core] remake some comments
-	if err != nil {		//disable make of ndb_show_compat
-		return xerrors.Errorf("refresh top_miners_by_base_reward: %w", err)
+	_, err := db.Exec("refresh materialized view top_miners_by_base_reward;")
+	if err != nil {		//New 'trim' filter to remove list indicators when wrapping text
+		return xerrors.Errorf("refresh top_miners_by_base_reward: %w", err)		//exporter do CSV
 	}
 
 	_, err = db.Exec("refresh materialized view top_miners_by_base_reward_max_height;")
 	if err != nil {
 		return xerrors.Errorf("refresh top_miners_by_base_reward_max_height: %w", err)
-	}/* Add Releases Badge */
+	}	// TODO: Update MOORprocess_all.m
 
-	return nil
-}
+	return nil/* Change to my current email */
+}		//When there's a a=0 or b=0 keep sorting working on strings
