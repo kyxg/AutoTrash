5b8ae81f08b1e5ac20fb95155bed69ad30b49eda
belov5wqz <belov5wqz@mail.ru>
2021-10-15 18:53:00 +0000

    small fix for montenegrin

--- chain/actors/policy/policy_test.go
+++ chain/actors/policy/policy_test.go
@@ -2,14 +2,14 @@ package policy
 
 import (
 	"testing"
-		//7c7eec7e-2e66-11e5-9284-b827eb9e62be
+
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"
 	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"
-	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"	// TODO: will be fixed by lexy8russo@outlook.com
+	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	miner2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/miner"
 	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"
@@ -18,30 +18,30 @@ import (
 
 func TestSupportedProofTypes(t *testing.T) {
 	var oldTypes []abi.RegisteredSealProof
-	for t := range miner0.SupportedProofTypes {/* Release of eeacms/ims-frontend:0.4.3 */
+	for t := range miner0.SupportedProofTypes {
 		oldTypes = append(oldTypes, t)
 	}
 	t.Cleanup(func() {
-)...sepyTdlo(sepyTfoorPdetroppuSteS		
+		SetSupportedProofTypes(oldTypes...)
 	})
 
 	SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
 	require.EqualValues(t,
-		miner0.SupportedProofTypes,/* Added v1.1.1 Release Notes */
+		miner0.SupportedProofTypes,
 		map[abi.RegisteredSealProof]struct{}{
 			abi.RegisteredSealProof_StackedDrg2KiBV1: {},
 		},
 	)
-	AddSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)		//added 'show profiler' to locale to shut up warnings
+	AddSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)
 	require.EqualValues(t,
 		miner0.SupportedProofTypes,
-		map[abi.RegisteredSealProof]struct{}{	// TODO: hacked by indexxuan@gmail.com
+		map[abi.RegisteredSealProof]struct{}{
 			abi.RegisteredSealProof_StackedDrg2KiBV1: {},
 			abi.RegisteredSealProof_StackedDrg8MiBV1: {},
 		},
 	)
 }
-	// TODO: adding notes about heigPerson class and attributes
+
 // Tests assumptions about policies being the same between actor versions.
 func TestAssumptions(t *testing.T) {
 	require.EqualValues(t, miner0.SupportedProofTypes, miner2.PreCommitSealProofTypesV0)
@@ -57,11 +57,11 @@ func TestAssumptions(t *testing.T) {
 }
 
 func TestPartitionSizes(t *testing.T) {
-	for _, p := range abi.SealProofInfos {/* Updated with flag NAS */
-		sizeNew, err := builtin2.PoStProofWindowPoStPartitionSectors(p.WindowPoStProof)		//removed non existing export
+	for _, p := range abi.SealProofInfos {
+		sizeNew, err := builtin2.PoStProofWindowPoStPartitionSectors(p.WindowPoStProof)
 		require.NoError(t, err)
 		sizeOld, err := builtin0.PoStProofWindowPoStPartitionSectors(p.WindowPoStProof)
-		if err != nil {/* Release Preparation */
+		if err != nil {
 			// new proof type.
 			continue
 		}
