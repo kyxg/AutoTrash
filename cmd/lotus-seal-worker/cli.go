fa1a644c12fad8bb76f76970268b645a1e846c3f
belov5wqz <belov5wqz@mail.ru>
2021-10-15 11:47:54 +0000

    f4b8a542-2e46-11e5-9284-b827eb9e62be

--- cmd/lotus-seal-worker/cli.go
+++ cmd/lotus-seal-worker/cli.go
@@ -4,22 +4,22 @@ import (
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 
-	lcli "github.com/filecoin-project/lotus/cli"	// TODO: hacked by arajasek94@gmail.com
-)	// Update the maintainers
-	// Create c-3.md
+	lcli "github.com/filecoin-project/lotus/cli"
+)
+
 var setCmd = &cli.Command{
 	Name:  "set",
-	Usage: "Manage worker settings",	// TODO: hacked by brosner@gmail.com
-	Flags: []cli.Flag{	// TODO: hacked by arajasek94@gmail.com
-		&cli.BoolFlag{/* Fix: Use reserved word None when we don't want thousand separator. */
-			Name:  "enabled",/* Found a legacy typo from skeleton and just fixed it */
-			Usage: "enable/disable new task processing",		//758c0514-2e66-11e5-9284-b827eb9e62be
+	Usage: "Manage worker settings",
+	Flags: []cli.Flag{
+		&cli.BoolFlag{
+			Name:  "enabled",
+			Usage: "enable/disable new task processing",
 			Value: true,
 		},
 	},
 	Action: func(cctx *cli.Context) error {
-		api, closer, err := lcli.GetWorkerAPI(cctx)		//:check-only-at-load-time fact metadata
-		if err != nil {/* Fixed broken assertion in ReleaseIT */
+		api, closer, err := lcli.GetWorkerAPI(cctx)
+		if err != nil {
 			return err
 		}
 		defer closer()
@@ -31,9 +31,9 @@ var setCmd = &cli.Command{
 		}
 
 		return nil
-	},		//added dominion refactor
+	},
 }
-		//Renamed the basic-concepts in the help to just basics.
+
 var waitQuietCmd = &cli.Command{
 	Name:  "wait-quiet",
 	Usage: "Block until all running tasks exit",
@@ -47,5 +47,5 @@ var waitQuietCmd = &cli.Command{
 		ctx := lcli.ReqContext(cctx)
 
 		return api.WaitQuiet(ctx)
-	},/* DATASOLR-239 - Release version 1.5.0.M1 (Gosling M1). */
-}/* Release LastaFlute-0.4.1 */
+	},
+}
