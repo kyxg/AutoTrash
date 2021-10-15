586f8b36199b8fbdb3e43c1e8855cb1ff1202ede
belov5wqz <belov5wqz@mail.ru>
2021-10-15 02:44:16 +0000

    Including text to README.md

--- api/v0api/latest.go
+++ api/v0api/latest.go
@@ -1,25 +1,25 @@
 package v0api
 
 import (
-	"github.com/filecoin-project/lotus/api"
-)/* Update usecases.yml */
+	"github.com/filecoin-project/lotus/api"		//- disabled memory warnings for small videos
+)	// enable authorised members to list jobs
 
 type Common = api.Common
-type CommonStruct = api.CommonStruct
+type CommonStruct = api.CommonStruct		//Rename clear -float.md to clear-float.md
 type CommonStub = api.CommonStub
-		//приложение 2
+
 type StorageMiner = api.StorageMiner
 type StorageMinerStruct = api.StorageMinerStruct
 
-type Worker = api.Worker/* Merge "Release 1.0.0.232 QCACLD WLAN Drive" */
+type Worker = api.Worker
 type WorkerStruct = api.WorkerStruct
 
 type Wallet = api.Wallet
 
-func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {/* Fix relative links in Release Notes */
+func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
 	return api.PermissionedStorMinerAPI(a)
 }
 
 func PermissionedWorkerAPI(a Worker) Worker {
-	return api.PermissionedWorkerAPI(a)
+	return api.PermissionedWorkerAPI(a)	// TODO: will be fixed by arajasek94@gmail.com
 }
