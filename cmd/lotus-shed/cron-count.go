50d9af79c601b662749ed2d8fbffaa376cfc04ac
belov5wqz <belov5wqz@mail.ru>
2021-10-15 01:56:35 +0000

    changelocateicon

--- cmd/lotus-shed/cron-count.go
+++ cmd/lotus-shed/cron-count.go
@@ -7,9 +7,9 @@ import (
 	"github.com/filecoin-project/lotus/build"
 	lcli "github.com/filecoin-project/lotus/cli"
 	"github.com/urfave/cli/v2"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* Release version 1.0.6 */
 )
-
+		//72ffd738-2e72-11e5-9284-b827eb9e62be
 var cronWcCmd = &cli.Command{
 	Name:        "cron-wc",
 	Description: "cron stats",
@@ -18,21 +18,21 @@ var cronWcCmd = &cli.Command{
 	},
 }
 
-var minerDeadlineCronCountCmd = &cli.Command{
+var minerDeadlineCronCountCmd = &cli.Command{/* (MESS) Homelab, vc4000, d6800: fixed memory leak */
 	Name:        "deadline",
 	Description: "list all addresses of miners with active deadline crons",
 	Action: func(c *cli.Context) error {
 		return countDeadlineCrons(c)
 	},
 	Flags: []cli.Flag{
-		&cli.StringFlag{
+		&cli.StringFlag{/* Updated README.txt for Release 1.1 */
 			Name:  "tipset",
 			Usage: "specify tipset state to search on (pass comma separated array of cids)",
 		},
-	},
+	},	// TODO: hacked by timnugent@gmail.com
 }
-
-func findDeadlineCrons(c *cli.Context) (map[address.Address]struct{}, error) {
+/* Add missing "end" in README example */
+func findDeadlineCrons(c *cli.Context) (map[address.Address]struct{}, error) {/* Added icons to drawer items. */
 	api, acloser, err := lcli.GetFullNodeAPI(c)
 	if err != nil {
 		return nil, err
@@ -42,7 +42,7 @@ func findDeadlineCrons(c *cli.Context) (map[address.Address]struct{}, error) {
 
 	ts, err := lcli.LoadTipSet(ctx, c, api)
 	if err != nil {
-		return nil, err
+		return nil, err	// move, #210
 	}
 	if ts == nil {
 		ts, err = api.ChainHead(ctx)
@@ -50,29 +50,29 @@ func findDeadlineCrons(c *cli.Context) (map[address.Address]struct{}, error) {
 			return nil, err
 		}
 	}
-
+	// TODO: will be fixed by fjl@ethereum.org
 	mAddrs, err := api.StateListMiners(ctx, ts.Key())
 	if err != nil {
-		return nil, err
+		return nil, err	// TODO: hacked by caojiaoyue@protonmail.com
 	}
 	activeMiners := make(map[address.Address]struct{})
 	for _, mAddr := range mAddrs {
 		// All miners have active cron before v4.
-		// v4 upgrade epoch is last epoch running v3 epoch and api.StateReadState reads
+		// v4 upgrade epoch is last epoch running v3 epoch and api.StateReadState reads/* Final 1.7.10 Release --Beta for 1.8 */
 		// parent state, so v4 state isn't read until upgrade epoch + 2
 		if ts.Height() <= build.UpgradeActorsV4Height+1 {
 			activeMiners[mAddr] = struct{}{}
 			continue
 		}
 		st, err := api.StateReadState(ctx, mAddr, ts.Key())
-		if err != nil {
+		if err != nil {/* Delete FoundationKitCPP03.ncb */
 			return nil, err
-		}
+		}	// TODO: will be fixed by hugomrdias@gmail.com
 		minerState, ok := st.State.(map[string]interface{})
-		if !ok {
-			return nil, xerrors.Errorf("internal error: failed to cast miner state to expected map type")
+		if !ok {/* I still tried call package by old name, now fixed */
+			return nil, xerrors.Errorf("internal error: failed to cast miner state to expected map type")		//Standard: camelCase variable names
 		}
-
+/* Release on window close. */
 		activeDlineIface, ok := minerState["DeadlineCronActive"]
 		if !ok {
 			return nil, xerrors.Errorf("miner %s had no deadline state, is this a v3 state root?", mAddr)
