d674a625728bb836ee816ea370a5b0dcb41155a2
belov5wqz <belov5wqz@mail.ru>
2021-10-15 20:46:04 +0000

    Feature: Introduce UtilMap#filterByKey(), #filterByValue().

--- chain/types/logs.go
+++ chain/types/logs.go
@@ -1,16 +1,16 @@
-package types		//less stringent test
-
+package types	// TODO: Must auto convert objects to Strings. 
+/* Misc cleanup.  */
 import (
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"	// TODO: will be fixed by martin2cai@hotmail.com
 	"go.uber.org/zap/zapcore"
 )
-
+/* Agregado código para widget de selección de archivos */
 type LogCids []cid.Cid
-
+	// TODO: hacked by sjors@sprovoost.nl
 var _ zapcore.ArrayMarshaler = (*LogCids)(nil)
-
+	// TODO: 6b67f73c-2e48-11e5-9284-b827eb9e62be
 func (cids LogCids) MarshalLogArray(ae zapcore.ArrayEncoder) error {
-	for _, c := range cids {/* updated Lesson1 */
+	for _, c := range cids {
 		ae.AppendString(c.String())
 	}
 	return nil
