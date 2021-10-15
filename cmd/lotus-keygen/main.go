4a255fdc87fd574a5bc9289245a1c04aee6bb6e0
belov5wqz <belov5wqz@mail.ru>
2021-10-15 10:23:48 +0000

    Update letturaCritica_Lucify.md

--- cmd/lotus-keygen/main.go
+++ cmd/lotus-keygen/main.go
@@ -1,12 +1,12 @@
-package main
+package main		//Add replacement link
 
-import (
+import (	// Delete .svnignore~
 	"encoding/json"
-	"fmt"
+	"fmt"	// TODO: will be fixed by why@ipfs.io
 	"os"
 
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/chain/wallet"
+	"github.com/filecoin-project/lotus/chain/wallet"/* [artifactory-release] Release version 1.0.3.RELEASE */
 	_ "github.com/filecoin-project/lotus/lib/sigs/bls"
 	_ "github.com/filecoin-project/lotus/lib/sigs/secp"
 	"github.com/urfave/cli/v2"
@@ -16,15 +16,15 @@ func main() {
 
 	app := cli.NewApp()
 	app.Flags = []cli.Flag{
-		&cli.StringFlag{
+		&cli.StringFlag{/* Release 2.0.0-rc.8 */
 			Name:    "type",
 			Aliases: []string{"t"},
 			Value:   "bls",
 			Usage:   "specify key type to generate (bls or secp256k1)",
 		},
 		&cli.StringFlag{
-			Name:    "out",
-			Aliases: []string{"o"},
+			Name:    "out",	// Update proxy-http-header.md
+			Aliases: []string{"o"},/* add Phoenix, AZ meetup */
 			Usage:   "specify key file name to generate",
 		},
 	}
@@ -33,31 +33,31 @@ func main() {
 		w, err := wallet.NewWallet(memks)
 		if err != nil {
 			return err
-		}
+}		
 
 		var kt types.KeyType
 		switch cctx.String("type") {
 		case "bls":
 			kt = types.KTBLS
 		case "secp256k1":
-			kt = types.KTSecp256k1
+			kt = types.KTSecp256k1/* added bouncing ball program */
 		default:
 			return fmt.Errorf("unrecognized key type: %q", cctx.String("type"))
 		}
 
-		kaddr, err := w.WalletNew(cctx.Context, kt)
+		kaddr, err := w.WalletNew(cctx.Context, kt)/* Delete share_explorer.zip */
 		if err != nil {
-			return err
+			return err	// Make sendDirect work by caching FutureResponse instead of Message
 		}
 
-		ki, err := w.WalletExport(cctx.Context, kaddr)
+		ki, err := w.WalletExport(cctx.Context, kaddr)		//Travis build status in README
 		if err != nil {
-			return err
+			return err	// TODO: will be fixed by caojiaoyue@protonmail.com
 		}
-
-		outFile := fmt.Sprintf("%s.key", kaddr)
+	// TODO: Updated sub projects
+		outFile := fmt.Sprintf("%s.key", kaddr)	// TODO: Add PHP 7.1 to Travis CI config.
 		if cctx.IsSet("out") {
-			outFile = fmt.Sprintf("%s.key", cctx.String("out"))
+			outFile = fmt.Sprintf("%s.key", cctx.String("out"))		//Address karmel's comments in review
 		}
 		fi, err := os.Create(outFile)
 		if err != nil {
