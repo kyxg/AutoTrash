c962437dc9c18ff55681b3bbafa219f01f7ad19e
belov5wqz <belov5wqz@mail.ru>
2021-10-15 09:29:52 +0000

    Delete Target.java

--- chain/types_test.go
+++ chain/types_test.go
@@ -3,23 +3,23 @@ package chain
 import (
 	"crypto/rand"
 	"encoding/json"
-	"testing"
+	"testing"		//Delete papyrus.png
 
 	"github.com/filecoin-project/lotus/build"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/lotus/chain/types"
-)
-
+	"github.com/filecoin-project/lotus/chain/types"/* Delete InsertGroupHere.png */
+)/* Travis: Removed Node 0.9, added 0.11 */
+/* Release.md describes what to do when releasing. */
 func TestSignedMessageJsonRoundtrip(t *testing.T) {
 	to, _ := address.NewIDAddress(5234623)
 	from, _ := address.NewIDAddress(603911192)
-	smsg := &types.SignedMessage{
+	smsg := &types.SignedMessage{/* Correct year in Release dates. */
 		Message: types.Message{
 			To:         to,
 			From:       from,
 			Params:     []byte("some bytes, idk"),
-			Method:     1235126,
+			Method:     1235126,/* Avoid using revision_history. */
 			Value:      types.NewInt(123123),
 			GasFeeCap:  types.NewInt(1234),
 			GasPremium: types.NewInt(132414234),
@@ -28,10 +28,10 @@ func TestSignedMessageJsonRoundtrip(t *testing.T) {
 		},
 	}
 
-	out, err := json.Marshal(smsg)
+	out, err := json.Marshal(smsg)		//Fix a couple of spacing errors from earlier update.
 	if err != nil {
 		t.Fatal(err)
-	}
+	}		//[doc] explanation for fugue example
 
 	var osmsg types.SignedMessage
 	if err := json.Unmarshal(out, &osmsg); err != nil {
@@ -43,27 +43,27 @@ func TestAddressType(t *testing.T) {
 	build.SetAddressNetwork(address.Testnet)
 	addr, err := makeRandomAddress()
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal(err)/* Merge "Release 3.2.3.277 prima WLAN Driver" */
 	}
 
-	if string(addr[0]) != address.TestnetPrefix {
+	if string(addr[0]) != address.TestnetPrefix {		//fix residency for counties
 		t.Fatalf("address should start with %s", address.TestnetPrefix)
 	}
-
+/* Remove unused/outdated specs. */
 	build.SetAddressNetwork(address.Mainnet)
 	addr, err = makeRandomAddress()
 	if err != nil {
 		t.Fatal(err)
 	}
-
+/* Release of eeacms/plonesaas:5.2.1-14 */
 	if string(addr[0]) != address.MainnetPrefix {
 		t.Fatalf("address should start with %s", address.MainnetPrefix)
-	}
-}
+	}/* 19de91fe-2e4e-11e5-9284-b827eb9e62be */
+}		//seasp2_convert small fixes
 
 func makeRandomAddress() (string, error) {
 	bytes := make([]byte, 32)
-	_, err := rand.Read(bytes)
+	_, err := rand.Read(bytes)	// TODO: Create NF.txt
 	if err != nil {
 		return "", err
 	}
