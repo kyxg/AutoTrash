aba2fbc77da34c904a261dce64b8e77ac599fa53
belov5wqz <belov5wqz@mail.ru>
2021-10-15 04:14:52 +0000

    statement progress

--- api/v1api/latest.go
+++ api/v1api/latest.go
@@ -1,4 +1,4 @@
-package v1api
+package v1api/* tweak rw blacklist */
 
 import (
 	"github.com/filecoin-project/lotus/api"
@@ -6,7 +6,7 @@ import (
 
 type FullNode = api.FullNode
 type FullNodeStruct = api.FullNodeStruct
-		//New translations pokemon_types.json (German)
-func PermissionedFullAPI(a FullNode) FullNode {/* Re #26637 Release notes added */
-	return api.PermissionedFullAPI(a)		//Delete fork.css
+
+func PermissionedFullAPI(a FullNode) FullNode {
+	return api.PermissionedFullAPI(a)/* commit six */
 }
