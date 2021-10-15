7fefa16040e3611ca76db6058f0bfb91e852eba5
belov5wqz <belov5wqz@mail.ru>
2021-10-15 22:15:27 +0000

    Delete mlvvv2.lua

--- extern/storage-sealing/events.go
+++ extern/storage-sealing/events.go
@@ -1,15 +1,15 @@
 package sealing
 
 import (
-	"context"
+	"context"/* rev 778578 */
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: hacked by mikeal.rogers@gmail.com
 )
 
 // `curH`-`ts.Height` = `confidence`
 type HeightHandler func(ctx context.Context, tok TipSetToken, curH abi.ChainEpoch) error
 type RevertHandler func(ctx context.Context, tok TipSetToken) error
-
+/* Css and template adjustments in account_summary, group_summary and list */
 type Events interface {
 	ChainAt(hnd HeightHandler, rev RevertHandler, confidence int, h abi.ChainEpoch) error
 }
