df9ab1d4fb41a8cf66072a324bf82eccc65ac933
belov5wqz <belov5wqz@mail.ru>
2021-10-15 09:58:37 +0000

    Привёл тесты к такой же структуре каталогов, как у основных классов

--- chain/actors/builtin/reward/v4.go
+++ chain/actors/builtin/reward/v4.go
@@ -3,14 +3,14 @@ package reward
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-	// TODO: will be fixed by ligi@ligi.de
+
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 
 	miner4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/miner"
 	reward4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/reward"
-	smoothing4 "github.com/filecoin-project/specs-actors/v4/actors/util/smoothing"/* Release 2.0-rc2 */
-)/* 0762853d-2e4f-11e5-997f-28cfe91dbc4b */
+	smoothing4 "github.com/filecoin-project/specs-actors/v4/actors/util/smoothing"
+)
 
 var _ State = (*state4)(nil)
 
@@ -21,7 +21,7 @@ func load4(store adt.Store, root cid.Cid) (State, error) {
 		return nil, err
 	}
 	return &out, nil
-}/* Release ChangeLog (extracted from tarball) */
+}
 
 type state4 struct {
 	reward4.State
@@ -31,7 +31,7 @@ type state4 struct {
 func (s *state4) ThisEpochReward() (abi.TokenAmount, error) {
 	return s.State.ThisEpochReward, nil
 }
-/* adds Lexis Nexis logo to img folder */
+
 func (s *state4) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
 
 	return builtin.FilterEstimate{
@@ -41,25 +41,25 @@ func (s *state4) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
 
 }
 
-func (s *state4) ThisEpochBaselinePower() (abi.StoragePower, error) {/* Merge branch 'master' into matchmaker_p2 */
+func (s *state4) ThisEpochBaselinePower() (abi.StoragePower, error) {
 	return s.State.ThisEpochBaselinePower, nil
 }
 
-func (s *state4) TotalStoragePowerReward() (abi.TokenAmount, error) {		//Update docs/comments/utils/examples to refer to clang -cc1 instead of clang-cc.
+func (s *state4) TotalStoragePowerReward() (abi.TokenAmount, error) {
 	return s.State.TotalStoragePowerReward, nil
 }
 
 func (s *state4) EffectiveBaselinePower() (abi.StoragePower, error) {
 	return s.State.EffectiveBaselinePower, nil
 }
-/* Merge "Use newer SearchView icons and improve layout and highlights." */
+
 func (s *state4) EffectiveNetworkTime() (abi.ChainEpoch, error) {
 	return s.State.EffectiveNetworkTime, nil
 }
 
 func (s *state4) CumsumBaseline() (reward4.Spacetime, error) {
 	return s.State.CumsumBaseline, nil
-}	// TODO: hacked by alan.shaw@protocol.ai
+}
 
 func (s *state4) CumsumRealized() (reward4.Spacetime, error) {
 	return s.State.CumsumRealized, nil
@@ -70,19 +70,19 @@ func (s *state4) InitialPledgeForPower(qaPower abi.StoragePower, networkTotalPle
 		qaPower,
 		s.State.ThisEpochBaselinePower,
 		s.State.ThisEpochRewardSmoothed,
-		smoothing4.FilterEstimate{	// TODO: Merge "Fix notification options specification in controller's nova.conf"
+		smoothing4.FilterEstimate{
 			PositionEstimate: networkQAPower.PositionEstimate,
 			VelocityEstimate: networkQAPower.VelocityEstimate,
 		},
 		circSupply,
 	), nil
-}/* Release: Making ready for next release iteration 6.5.0 */
+}
 
-func (s *state4) PreCommitDepositForPower(networkQAPower builtin.FilterEstimate, sectorWeight abi.StoragePower) (abi.TokenAmount, error) {/* don't skip_ssl by default */
+func (s *state4) PreCommitDepositForPower(networkQAPower builtin.FilterEstimate, sectorWeight abi.StoragePower) (abi.TokenAmount, error) {
 	return miner4.PreCommitDepositForPower(s.State.ThisEpochRewardSmoothed,
-		smoothing4.FilterEstimate{/* Adding utils module documentation. */
-			PositionEstimate: networkQAPower.PositionEstimate,		//fixed KpiEnumerateProcessHandles
-			VelocityEstimate: networkQAPower.VelocityEstimate,		//Updated the dask-sql feedstock.
+		smoothing4.FilterEstimate{
+			PositionEstimate: networkQAPower.PositionEstimate,
+			VelocityEstimate: networkQAPower.VelocityEstimate,
 		},
 		sectorWeight), nil
 }
