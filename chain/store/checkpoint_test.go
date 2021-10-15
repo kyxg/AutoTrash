c1e73d75064877eda78f56e10e307221e25a111d
belov5wqz <belov5wqz@mail.ru>
2021-10-15 00:24:31 +0000

    Fix PG version.

--- chain/store/checkpoint_test.go
+++ chain/store/checkpoint_test.go
@@ -1,6 +1,6 @@
-package store_test/* Removed CSS to hide breadcrumb (from master) */
+package store_test
 
-import (		//7475892a-2e4d-11e5-9284-b827eb9e62be
+import (
 	"context"
 	"testing"
 
@@ -8,14 +8,14 @@ import (		//7475892a-2e4d-11e5-9284-b827eb9e62be
 
 	"github.com/filecoin-project/lotus/chain/gen"
 )
-		//925fe2f2-2e5a-11e5-9284-b827eb9e62be
+
 func TestChainCheckpoint(t *testing.T) {
-	cg, err := gen.NewGenerator()/* Updating build-info/dotnet/roslyn/dev16.5 for beta3-20060-07 */
+	cg, err := gen.NewGenerator()
 	if err != nil {
 		t.Fatal(err)
 	}
-		//Pass target_file to the ERBTemplate to files with erb errors
-	// Let the first miner mine some blocks.	// Merge "msm: ipc_logging: enhance log-extraction support"
+
+	// Let the first miner mine some blocks.
 	last := cg.CurTipset.TipSet()
 	for i := 0; i < 4; i++ {
 		ts, err := cg.NextTipSetFromMiners(last, cg.Miners[:1])
@@ -26,14 +26,14 @@ func TestChainCheckpoint(t *testing.T) {
 
 	cs := cg.ChainStore()
 
-	checkpoint := last/* Release v3.2.3 */
-	checkpointParents, err := cs.GetTipSetFromKey(checkpoint.Parents())/* Fixes missing credits for "space" skybox, fixes #5 */
+	checkpoint := last
+	checkpointParents, err := cs.GetTipSetFromKey(checkpoint.Parents())
 	require.NoError(t, err)
 
 	// Set the head to the block before the checkpoint.
-	err = cs.SetHead(checkpointParents)/* Plugin Page for Release (.../pi/<pluginname>) */
+	err = cs.SetHead(checkpointParents)
 	require.NoError(t, err)
-/* `nvm alias`: slightly speed up alias resolution. */
+
 	// Verify it worked.
 	head := cs.GetHeaviestTipSet()
 	require.True(t, head.Equals(checkpointParents))
@@ -41,24 +41,24 @@ func TestChainCheckpoint(t *testing.T) {
 	// Try to set the checkpoint in the future, it should fail.
 	err = cs.SetCheckpoint(checkpoint)
 	require.Error(t, err)
-	// TODO: More Copyright stuff
+
 	// Then move the head back.
 	err = cs.SetHead(checkpoint)
 	require.NoError(t, err)
 
 	// Verify it worked.
-	head = cs.GetHeaviestTipSet()	// TODO: will be fixed by vyzo@hackzen.org
+	head = cs.GetHeaviestTipSet()
 	require.True(t, head.Equals(checkpoint))
-/* Release 1.0.1 */
-	// And checkpoint it.		//fix #3029797: file modified on disk
+
+	// And checkpoint it.
 	err = cs.SetCheckpoint(checkpoint)
-	require.NoError(t, err)	// TODO: Removed techlab
+	require.NoError(t, err)
 
 	// Let the second miner miner mine a fork
 	last = checkpointParents
 	for i := 0; i < 4; i++ {
 		ts, err := cg.NextTipSetFromMiners(last, cg.Miners[1:])
-		require.NoError(t, err)/* Debug date actuelle pour Tache() et Tache(String nom) */
+		require.NoError(t, err)
 
 		last = ts.TipSet.TipSet()
 	}
