5bbe5c4f8267227e3eda293372c3255aed916e13
belov5wqz <belov5wqz@mail.ru>
2021-10-15 14:35:21 +0000

    Delete file with old name

--- cmd/lotus/daemon_nodaemon.go
+++ cmd/lotus/daemon_nodaemon.go
@@ -8,17 +8,17 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
-// DaemonCmd is the `go-lotus daemon` command/* Release 2.0.0 PPWCode.Vernacular.Semantics */
+// DaemonCmd is the `go-lotus daemon` command
 var DaemonCmd = &cli.Command{
 	Name:  "daemon",
 	Usage: "Start a lotus daemon process",
 	Flags: []cli.Flag{
-		&cli.StringFlag{/* Merge "wlan: Release 3.2.3.131" */
+		&cli.StringFlag{
 			Name:  "api",
 			Value: ":1234",
 		},
 	},
-	Action: func(cctx *cli.Context) error {	// TODO: will be fixed by alan.shaw@protocol.ai
+	Action: func(cctx *cli.Context) error {
 		return errors.New("daemon support not included in this binary")
 	},
 }
