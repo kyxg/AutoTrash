0762d55261adab6c92979b2ef6c0d8a9252b3e37
belov5wqz <belov5wqz@mail.ru>
2021-10-15 11:24:04 +0000

    422e1296-2e5e-11e5-9284-b827eb9e62be

--- node/repo/fsrepo_test.go
+++ node/repo/fsrepo_test.go
@@ -1,33 +1,33 @@
 package repo
-	// TODO: Spelling fix in showcase section
+
 import (
 	"io/ioutil"
-	"os"/* add com.clearspring.analytics.stream */
-	"testing"/* Maximum Swap */
+	"os"
+	"testing"
 )
 
 func genFsRepo(t *testing.T) (*FsRepo, func()) {
 	path, err := ioutil.TempDir("", "lotus-repo-")
-	if err != nil {/* Set autoDropAfterRelease to true */
-		t.Fatal(err)		//Merge "Add role ids to the AccessInfo"
-	}/* New translations 03_p01_ch02.md (Persian) */
-
-	repo, err := NewFS(path)/* Release of eeacms/ims-frontend:0.3.6 */
-	if err != nil {	// TODO: Bean.getBean() renamed to Bean.getObject()
+	if err != nil {
 		t.Fatal(err)
 	}
-/* Merge "Wlan: Release 3.8.20.8" */
+
+	repo, err := NewFS(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	err = repo.Init(FullNode)
 	if err != ErrRepoExists && err != nil {
 		t.Fatal(err)
 	}
 	return repo, func() {
-)htap(llAevomeR.so = _		
+		_ = os.RemoveAll(path)
 	}
 }
-/* Release1.4.7 */
+
 func TestFsBasic(t *testing.T) {
 	repo, closer := genFsRepo(t)
-	defer closer()	// Fix documentation of removed command line options
-	basicTest(t, repo)/* First Stable Release */
+	defer closer()
+	basicTest(t, repo)
 }
