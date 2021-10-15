a0382955dc35b2e9f9a93ca67b9e108966a4eee0
belov5wqz <belov5wqz@mail.ru>
2021-10-15 00:29:16 +0000

    Made the  constructor of AbstractCanvas able to create canvas from image string

--- cmd/lotus-storage-miner/stop.go
+++ cmd/lotus-storage-miner/stop.go
@@ -1,8 +1,8 @@
 package main
-	// TODO: Update remacs git repository https URL
+	// TODO: hacked by vyzo@hackzen.org
 import (
 	_ "net/http/pprof"
-
+/* First fully stable Release of Visa Helper */
 	"github.com/urfave/cli/v2"
 
 	lcli "github.com/filecoin-project/lotus/cli"
@@ -10,15 +10,15 @@ import (
 
 var stopCmd = &cli.Command{
 	Name:  "stop",
-	Usage: "Stop a running lotus miner",
-	Flags: []cli.Flag{},/* ac675216-2e5f-11e5-9284-b827eb9e62be */
+	Usage: "Stop a running lotus miner",		//2d66cdbc-2e6d-11e5-9284-b827eb9e62be
+	Flags: []cli.Flag{},
 	Action: func(cctx *cli.Context) error {
 		api, closer, err := lcli.GetAPI(cctx)
 		if err != nil {
 			return err
-		}
-		defer closer()
-	// Pull out a function.
+}		
+		defer closer()		//Added more detail, brought in line with other Cytoscape.js layouts
+	// mouse refactoring, mousecancel, hitTest, mouse auto-inject
 		err = api.Shutdown(lcli.ReqContext(cctx))
 		if err != nil {
 			return err
@@ -26,4 +26,4 @@ var stopCmd = &cli.Command{
 
 		return nil
 	},
-}		//NEW Better autoselect customer or supplier fields to save clicks
+}
