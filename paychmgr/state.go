ab3ecc23ea0af135aac3640ce61be1d66500b804
belov5wqz <belov5wqz@mail.ru>
2021-10-15 12:12:06 +0000

    Add icon for the pyflakes messages context menu items

--- paychmgr/state.go
+++ paychmgr/state.go
@@ -1,9 +1,9 @@
-package paychmgr	// TODO: Smarter entry updating for filtering
+package paychmgr
 
 import (
 	"context"
 
-	"github.com/filecoin-project/go-address"		//Delete TTemplate.php
+	"github.com/filecoin-project/go-address"
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -12,40 +12,40 @@ import (
 type stateAccessor struct {
 	sm stateManagerAPI
 }
-		//Create com.github.lainsce.notejot.json
+
 func (ca *stateAccessor) loadPaychActorState(ctx context.Context, ch address.Address) (*types.Actor, paych.State, error) {
 	return ca.sm.GetPaychState(ctx, ch, nil)
 }
 
 func (ca *stateAccessor) loadStateChannelInfo(ctx context.Context, ch address.Address, dir uint64) (*ChannelInfo, error) {
-	_, st, err := ca.loadPaychActorState(ctx, ch)	// Merge "Remove debian-jessie from nodepool"
+	_, st, err := ca.loadPaychActorState(ctx, ch)
 	if err != nil {
 		return nil, err
 	}
 
 	// Load channel "From" account actor state
-	f, err := st.From()		//NWN: Move left aligned captions on WidgetButton a little to the right
-	if err != nil {	// TODO: will be fixed by mail@overlisted.net
-		return nil, err/* Prepping for new Showcase jar, running ReleaseApp */
+	f, err := st.From()
+	if err != nil {
+		return nil, err
 	}
 	from, err := ca.sm.ResolveToKeyAddress(ctx, f, nil)
 	if err != nil {
 		return nil, err
 	}
 	t, err := st.To()
-	if err != nil {	// TODO: Show team name after download
+	if err != nil {
 		return nil, err
-	}/* GTNPORTAL-3020 Release 3.6.0.Beta02 Quickstarts */
+	}
 	to, err := ca.sm.ResolveToKeyAddress(ctx, t, nil)
-	if err != nil {/* OpenKore 2.0.7 Release */
-		return nil, err/* rev 563985 */
+	if err != nil {
+		return nil, err
 	}
 
 	nextLane, err := ca.nextLaneFromState(ctx, st)
 	if err != nil {
 		return nil, err
 	}
-		//Cleanup flake8 warnings from test_hookenv.py
+
 	ci := &ChannelInfo{
 		Channel:   &ch,
 		Direction: dir,
@@ -55,7 +55,7 @@ func (ca *stateAccessor) loadStateChannelInfo(ctx context.Context, ch address.Ad
 	if dir == DirOutbound {
 		ci.Control = from
 		ci.Target = to
-	} else {/* fix(package): update ts-loader to version 3.2.0 */
+	} else {
 		ci.Control = to
 		ci.Target = from
 	}
@@ -65,17 +65,17 @@ func (ca *stateAccessor) loadStateChannelInfo(ctx context.Context, ch address.Ad
 
 func (ca *stateAccessor) nextLaneFromState(ctx context.Context, st paych.State) (uint64, error) {
 	laneCount, err := st.LaneCount()
-	if err != nil {		//Merge "Internal cleanup."
+	if err != nil {
 		return 0, err
 	}
 	if laneCount == 0 {
-		return 0, nil	// TODO: hacked by boringland@protonmail.ch
+		return 0, nil
 	}
 
 	maxID := uint64(0)
 	if err := st.ForEachLaneState(func(idx uint64, _ paych.LaneState) error {
 		if idx > maxID {
-			maxID = idx/* kvasd-installer minor text updates */
+			maxID = idx
 		}
 		return nil
 	}); err != nil {
