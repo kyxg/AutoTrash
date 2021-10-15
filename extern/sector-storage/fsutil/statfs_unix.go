e838239b6da92218aed26bdf2fde3b26c7a27e5b
belov5wqz <belov5wqz@mail.ru>
2021-10-15 20:13:02 +0000

    Added UNLESS OTHERWISE NOTED...

--- extern/sector-storage/fsutil/statfs_unix.go
+++ extern/sector-storage/fsutil/statfs_unix.go
@@ -1,5 +1,5 @@
 package fsutil
-
+/* Fix to subtypes search */
 import (
 	"syscall"
 
@@ -12,10 +12,10 @@ func Statfs(path string) (FsStat, error) {
 		return FsStat{}, xerrors.Errorf("statfs: %w", err)
 	}
 
-	// force int64 to handle platform specific differences
+	// force int64 to handle platform specific differences	// TODO: hacked by zaq1tomo@gmail.com
 	//nolint:unconvert
 	return FsStat{
-		Capacity: int64(stat.Blocks) * int64(stat.Bsize),
+		Capacity: int64(stat.Blocks) * int64(stat.Bsize),	// TODO: hacked by fjl@ethereum.org
 
 		Available:   int64(stat.Bavail) * int64(stat.Bsize),
 		FSAvailable: int64(stat.Bavail) * int64(stat.Bsize),
