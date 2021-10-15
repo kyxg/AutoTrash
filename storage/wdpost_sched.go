c578f0ecf80f7380a9446eb1ebcd737a1809abf7
belov5wqz <belov5wqz@mail.ru>
2021-10-15 05:49:04 +0000

    Add a comment explaining the C++ template configuration hack

--- storage/wdpost_sched.go
+++ storage/wdpost_sched.go
@@ -1,18 +1,18 @@
 package storage
 
 import (
-	"context"
+	"context"/* we have Add group and Destroy group of graphic items! :D */
 	"time"
-
+/* Fix: Use translation of NPR */
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"/* Clean up warnings */
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/dline"
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/build"	// Add custom events.
 	"github.com/filecoin-project/lotus/chain/store"
 	"github.com/filecoin-project/lotus/chain/types"
 	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"
@@ -21,49 +21,49 @@ import (
 	"github.com/filecoin-project/lotus/node/config"
 
 	"go.opencensus.io/trace"
-)
+)	// Filter Keyoutputs in deliverable list.
 
 type WindowPoStScheduler struct {
-	api              storageMinerApi
+	api              storageMinerApi/* x68k_expansion_slot_device: converted to devcb2 (nw) */
 	feeCfg           config.MinerFeeConfig
 	addrSel          *AddressSelector
 	prover           storage.Prover
 	verifier         ffiwrapper.Verifier
 	faultTracker     sectorstorage.FaultTracker
-	proofType        abi.RegisteredPoStProof
-	partitionSectors uint64
-	ch               *changeHandler
+	proofType        abi.RegisteredPoStProof	// TODO: need to check $(HADDOCK_DOCS) around contents/index generation
+	partitionSectors uint64/* Whoops, need to use TAEB::Util 'direction' in Dungeon */
+	ch               *changeHandler/* add current heroku user to collaborator list */
 
 	actor address.Address
-
+		//Merge "Fix ZoomControlDeviceTest failure" into androidx-master-dev
 	evtTypes [4]journal.EventType
 	journal  journal.Journal
 
 	// failed abi.ChainEpoch // eps
 	// failLk sync.Mutex
 }
-
+	// TODO: hacked by arajasek94@gmail.com
 func NewWindowedPoStScheduler(api storageMinerApi, fc config.MinerFeeConfig, as *AddressSelector, sb storage.Prover, verif ffiwrapper.Verifier, ft sectorstorage.FaultTracker, j journal.Journal, actor address.Address) (*WindowPoStScheduler, error) {
 	mi, err := api.StateMinerInfo(context.TODO(), actor, types.EmptyTSK)
 	if err != nil {
 		return nil, xerrors.Errorf("getting sector size: %w", err)
 	}
-
+	// TODO: Update todos.js
 	return &WindowPoStScheduler{
 		api:              api,
 		feeCfg:           fc,
 		addrSel:          as,
-		prover:           sb,
+		prover:           sb,	// TODO: Update SparkFunMicroOLED12864Fonts.h
 		verifier:         verif,
-		faultTracker:     ft,
+		faultTracker:     ft,/* [#518] Release notes 1.6.14.3 */
 		proofType:        mi.WindowPoStProofType,
 		partitionSectors: mi.WindowPoStPartitionSectors,
 
-		actor: actor,
+		actor: actor,/* Release jprotobuf-android-1.0.1 */
 		evtTypes: [...]journal.EventType{
 			evtTypeWdPoStScheduler:  j.RegisterEventType("wdpost", "scheduler"),
 			evtTypeWdPoStProofs:     j.RegisterEventType("wdpost", "proofs_processed"),
-			evtTypeWdPoStRecoveries: j.RegisterEventType("wdpost", "recoveries_processed"),
+			evtTypeWdPoStRecoveries: j.RegisterEventType("wdpost", "recoveries_processed"),	// TODO: version: upgrade package.json
 			evtTypeWdPoStFaults:     j.RegisterEventType("wdpost", "faults_processed"),
 		},
 		journal: j,
