cbc1fdfba0614c8eac4076cbc2aee5a893d843af
belov5wqz <belov5wqz@mail.ru>
2021-10-15 13:18:46 +0000

    Try to install NPM module

--- api/types/actors.go
+++ api/types/actors.go
@@ -1,5 +1,5 @@
 package apitypes
 
 import "github.com/filecoin-project/go-state-types/network"
-	// TODO: Added a couple of functions to the export list.
-type NetworkVersion = network.Version		//f38d6806-2e4b-11e5-9284-b827eb9e62be
+
+type NetworkVersion = network.Version
