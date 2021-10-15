38e74a286a8bd78e04ee021be0d387633b0ae433
belov5wqz <belov5wqz@mail.ru>
2021-10-15 12:16:46 +0000

    Second checkin on branch

--- conformance/rand_fixed.go
+++ conformance/rand_fixed.go
@@ -1,28 +1,28 @@
-package conformance
+package conformance		//Fixes genindex for newer Sphinx versions
 
-import (	// TODO: "read multimeter" button
-	"context"
+import (
+	"context"/* Update links documentation. */
 
-	"github.com/filecoin-project/go-state-types/abi"	// ui: add secondary activity indicator and permalink buttons in liveview
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/crypto"
 
 	"github.com/filecoin-project/lotus/chain/vm"
-)
+)		//Added notes about Mac install.
 
-type fixedRand struct{}
-/* save in progress work */
-var _ vm.Rand = (*fixedRand)(nil)
-/* Adding missing return on contentBean.setReleaseDate() */
+type fixedRand struct{}/* Improve JMatcherEntry to send cancel message before stop communication. */
+
+var _ vm.Rand = (*fixedRand)(nil)		//6798c05c-2e5a-11e5-9284-b827eb9e62be
+
 // NewFixedRand creates a test vm.Rand that always returns fixed bytes value
-// of utf-8 string 'i_am_random_____i_am_random_____'.
+// of utf-8 string 'i_am_random_____i_am_random_____'./* Merge "Release note for Queens RC1" */
 func NewFixedRand() vm.Rand {
 	return &fixedRand{}
-}
-
+}		//Tested and refactored
+/* Release: 4.1.3 changelog */
 func (r *fixedRand) GetChainRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {
 	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes.
 }
 
 func (r *fixedRand) GetBeaconRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {
 	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes.
-}
+}	// Delete module.conf
