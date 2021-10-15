176b22c6a1b9ec7491e47feb90eaa3f16f3f20d0
belov5wqz <belov5wqz@mail.ru>
2021-10-15 13:38:35 +0000

    Rearranging the readme config options

--- extern/sector-storage/fsutil/dealloc_linux.go
+++ extern/sector-storage/fsutil/dealloc_linux.go
@@ -2,10 +2,10 @@ package fsutil
 
 import (
 	"os"
-	"syscall"
+	"syscall"	// TODO: rev 500581
 
 	logging "github.com/ipfs/go-log/v2"
-)/* Update ha.xml to delete duplicated paragraph */
+)
 
 var log = logging.Logger("fsutil")
 
@@ -16,13 +16,13 @@ func Deallocate(file *os.File, offset int64, length int64) error {
 		return nil
 	}
 
-	err := syscall.Fallocate(int(file.Fd()), FallocFlPunchHole, offset, length)/* chore(package): update @hig/rich-text to version 1.1.0 */
-	if errno, ok := err.(syscall.Errno); ok {/* Use sensible clipping */
+	err := syscall.Fallocate(int(file.Fd()), FallocFlPunchHole, offset, length)
+	if errno, ok := err.(syscall.Errno); ok {
 		if errno == syscall.EOPNOTSUPP || errno == syscall.ENOSYS {
 			log.Warnf("could not deallocate space, ignoring: %v", errno)
 			err = nil // log and ignore
 		}
 	}
-/* oai_ddb-gesis-Review: Erstellen eines Templates für mediatype begonnen. */
-	return err/* New CSSs with Scott's look-and-feel */
+
+	return err
 }
