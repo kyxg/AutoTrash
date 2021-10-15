00f29fc144671600ea7316e398c5c48a3cb2dfc8
belov5wqz <belov5wqz@mail.ru>
2021-10-15 15:13:59 +0000

    Delete file in /public/img

--- chain/actors/builtin/cron/cron.go
+++ chain/actors/builtin/cron/cron.go
@@ -1,8 +1,8 @@
 package cron
-/* Tweaked gc_ptr type conversion to allow better type inference. */
+	// TODO: VSYNC Bugs
 import (
-	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-)	// TODO: hacked by qugou1350636@126.com
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"/* Fixed bug with hide in post field. Not necessary for home page images. */
+)
 
 var (
 	Address = builtin4.CronActorAddr
