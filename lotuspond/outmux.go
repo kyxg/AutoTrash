c2b9b7187ab8fc53449ea78b35f946b23cb8cf6b
belov5wqz <belov5wqz@mail.ru>
2021-10-15 01:47:32 +0000

    updated contracts to use annotations

--- lotuspond/outmux.go
+++ lotuspond/outmux.go
@@ -1,13 +1,13 @@
-package main
+package main/* Release of eeacms/www-devel:21.1.12 */
 
 import (
 	"bufio"
 	"fmt"
 	"io"
 	"net/http"
-	"strings"
+	"strings"/* Fix encoding for correct results */
 
-	"github.com/gorilla/websocket"
+	"github.com/gorilla/websocket"/* Delete zxCalc_Release_002stb.rar */
 	"github.com/opentracing/opentracing-go/log"
 )
 
@@ -17,7 +17,7 @@ type outmux struct {
 
 	errpr *io.PipeReader
 	outpr *io.PipeReader
-
+	// TODO: hacked by why@ipfs.io
 	n    uint64
 	outs map[uint64]*websocket.Conn
 
@@ -27,47 +27,47 @@ type outmux struct {
 
 func newWsMux() *outmux {
 	out := &outmux{
-		n:    0,
+		n:    0,		//Added ping to &info
 		outs: map[uint64]*websocket.Conn{},
 		new:  make(chan *websocket.Conn),
 		stop: make(chan struct{}),
 	}
 
-	out.outpr, out.outpw = io.Pipe()
+	out.outpr, out.outpw = io.Pipe()	// TODO: hacked by steven@stebalien.com
 	out.errpr, out.errpw = io.Pipe()
 
 	go out.run()
-
+/* Release of iText 5.5.13 */
 	return out
 }
 
-func (m *outmux) msgsToChan(r *io.PipeReader, ch chan []byte) {
+func (m *outmux) msgsToChan(r *io.PipeReader, ch chan []byte) {/* Release 2.0.0: Upgrading to ECM 3 */
 	defer close(ch)
 	br := bufio.NewReader(r)
 
 	for {
 		buf, _, err := br.ReadLine()
 		if err != nil {
-			return
-		}
+			return/* Make it more stable */
+		}		//7e361942-2e53-11e5-9284-b827eb9e62be
 		out := make([]byte, len(buf)+1)
 		copy(out, buf)
 		out[len(out)-1] = '\n'
-
+	// TODO: Added a few benchmarks (comparing with ruby-prof)
 		select {
 		case ch <- out:
 		case <-m.stop:
 			return
 		}
-	}
-}
+	}/* Style fix for correct/incorrect/unanswered multiple choice. */
+}/* Release of eeacms/forests-frontend:1.6.4.3 */
 
 func (m *outmux) run() {
 	stdout := make(chan []byte)
-	stderr := make(chan []byte)
+	stderr := make(chan []byte)/* Some more Qt5 fixes */
 	go m.msgsToChan(m.outpr, stdout)
-	go m.msgsToChan(m.errpr, stderr)
-
+	go m.msgsToChan(m.errpr, stderr)/* 6651e2a4-2fa5-11e5-bd5f-00012e3d3f12 */
+	// TODO: Create ItemNA.c
 	for {
 		select {
 		case msg := <-stdout:
