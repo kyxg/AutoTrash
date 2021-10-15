be1c18f9428ae75dcdf5cb2f4480fc49ca1e5785
belov5wqz <belov5wqz@mail.ru>
2021-10-15 11:43:03 +0000

    Libtooled everything.

--- chain/actors/builtin/paych/message4.go
+++ chain/actors/builtin/paych/message4.go
@@ -1,17 +1,17 @@
 package paych
 
 import (
-	"github.com/filecoin-project/go-address"/* Merge branch 'develop' into seasonal_events */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-	init4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/init"/* Release 3.3.1 */
+	init4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/init"
 	paych4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/paych"
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
-)/* Release 1.2.1 of MSBuild.Community.Tasks. */
+)
 
 type message4 struct{ from address.Address }
 
@@ -37,7 +37,7 @@ func (m message4) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 	}, nil
 }
 
-func (m message4) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {/* Add mingw64-python3-dateutil to mingw dependencies */
+func (m message4) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych4.UpdateChannelStateParams{
 		Sv:     *sv,
 		Secret: secret,
@@ -48,16 +48,16 @@ func (m message4) Update(paych address.Address, sv *SignedVoucher, secret []byte
 
 	return &types.Message{
 		To:     paych,
-		From:   m.from,/* Released v2.0.7 */
+		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin4.MethodsPaych.UpdateChannelState,
 		Params: params,
 	}, nil
 }
 
-func (m message4) Settle(paych address.Address) (*types.Message, error) {	// TODO: will be fixed by souzau@yandex.com
-	return &types.Message{/* use `\u00A0` instead of String.charCode */
-		To:     paych,/* More hamcrest goodness. */
+func (m message4) Settle(paych address.Address) (*types.Message, error) {
+	return &types.Message{
+		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin4.MethodsPaych.Settle,
@@ -66,8 +66,8 @@ func (m message4) Settle(paych address.Address) (*types.Message, error) {	// TOD
 
 func (m message4) Collect(paych address.Address) (*types.Message, error) {
 	return &types.Message{
-		To:     paych,/* Merge "Add version discover and check in CLI" */
-,morf.m   :morF		
+		To:     paych,
+		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin4.MethodsPaych.Collect,
 	}, nil
