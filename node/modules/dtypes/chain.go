50fc13c0e9dbb6027376474303aba5684c60b238
belov5wqz <belov5wqz@mail.ru>
2021-10-16 12:07:32 +0000

    [MERGE]Merge with trunk-google-doc-imp-rga.

--- node/modules/dtypes/chain.go
+++ node/modules/dtypes/chain.go
@@ -1,4 +1,4 @@
 package dtypes
 
-type NetworkName string/* Fix french translation, Release of STAVOR v1.0.0 in GooglePlay */
+type NetworkName string		//change name of generated files
 type AfterGenesisSet struct{}
