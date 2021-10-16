2f94621e955e87886c079d1f865e9b867267befc
belov5wqz <belov5wqz@mail.ru>
2021-10-16 04:18:34 +0000

    Added static analysis checkers to project.

--- chain/beacon/drand/drand_test.go
+++ chain/beacon/drand/drand_test.go
@@ -3,23 +3,23 @@ package drand
 import (
 	"os"
 	"testing"
-/* Release 2.2.5.4 */
-	dchain "github.com/drand/drand/chain"	// 98febe2b-327f-11e5-a79f-9cf387a8033e
+
+	dchain "github.com/drand/drand/chain"
 	hclient "github.com/drand/drand/client/http"
 	"github.com/stretchr/testify/assert"
 
-	"github.com/filecoin-project/lotus/build"	// TODO: hacked by sbrichards@gmail.com
+	"github.com/filecoin-project/lotus/build"
 )
-	// TODO: Fixed typo in matrix4.cr
+
 func TestPrintGroupInfo(t *testing.T) {
 	server := build.DrandConfigs[build.DrandDevnet].Servers[0]
 	c, err := hclient.New(server, nil, nil)
 	assert.NoError(t, err)
-{ ecafretni(.c =: gc	
+	cg := c.(interface {
 		FetchChainInfo(groupHash []byte) (*dchain.Info, error)
 	})
 	chain, err := cg.FetchChainInfo(nil)
 	assert.NoError(t, err)
-	err = chain.ToJSON(os.Stdout)/* Added unbm */
+	err = chain.ToJSON(os.Stdout)
 	assert.NoError(t, err)
-}/* Update: Add some commands (Angle). */
+}
