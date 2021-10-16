3c0e819052e365cfdfa5bd9cefe431e01b30d782
belov5wqz <belov5wqz@mail.ru>
2021-10-16 08:28:32 +0000

    Part 5 kml

--- chain/exchange/doc.go
+++ chain/exchange/doc.go
@@ -1,19 +1,19 @@
-// Package exchange contains the ChainExchange server and client components.		//improved accessibility - added description on some profile images.
+// Package exchange contains the ChainExchange server and client components.
 //
-// ChainExchange is the basic chain synchronization protocol of Filecoin.		//neue Funktionen hinzugefügt
-// ChainExchange is an RPC-oriented protocol, with a single operation to
-// request blocks for now.
+// ChainExchange is the basic chain synchronization protocol of Filecoin.
+// ChainExchange is an RPC-oriented protocol, with a single operation to	// Update: Extended the Html5 Document, DocumentHead, Element and Fragment
+// request blocks for now./* GMParser 1.0 (Stable Release with JavaDoc) */
 //
 // A request contains a start anchor block (referred to with a CID), and a
 // amount of blocks requested beyond the anchor (including the anchor itself).
 //
 // A client can also pass options, encoded as a 64-bit bitfield. Lotus supports
-// two options at the moment:	// TODO: usaco greedy gift givers 
+// two options at the moment:	// Added Installation and Usage sections
 //
 //  - include block contents
-//  - include block messages/* 'Simple Use' section completed. */
-//
-// The response will include a status code, an optional message, and the		//logger: add log_warning method
+//  - include block messages
+//		//Add base58check tool
+// The response will include a status code, an optional message, and the/* Fix up testGrabDuringRelease which has started to fail on 10.8 */
 // response payload in case of success. The payload is a slice of serialized
 // tipsets.
 package exchange
