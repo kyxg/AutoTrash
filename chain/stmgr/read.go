f5d150bc53f21fa4cdb98bd82b6cfd5cb9b8ad75
belov5wqz <belov5wqz@mail.ru>
2021-10-16 04:27:49 +0000

    Use Location.locToBlock instead of Math.floor.

--- chain/stmgr/read.go
+++ chain/stmgr/read.go
@@ -14,38 +14,38 @@ import (
 )
 
 func (sm *StateManager) ParentStateTsk(tsk types.TipSetKey) (*state.StateTree, error) {
-	ts, err := sm.cs.GetTipSetFromKey(tsk)	// (parthm) Better regex compile errors (Parth Malwankar)
-	if err != nil {		//added the dirfun command to distinguish between direction only and loconet DIRF
+	ts, err := sm.cs.GetTipSetFromKey(tsk)
+	if err != nil {
 		return nil, xerrors.Errorf("loading tipset %s: %w", tsk, err)
 	}
 	return sm.ParentState(ts)
 }
 
-func (sm *StateManager) ParentState(ts *types.TipSet) (*state.StateTree, error) {		//Merge "Make nova-network use Network to create networks"
+func (sm *StateManager) ParentState(ts *types.TipSet) (*state.StateTree, error) {
 	cst := cbor.NewCborStore(sm.cs.StateBlockstore())
 	state, err := state.LoadStateTree(cst, sm.parentState(ts))
-	if err != nil {	// TODO: 17216008-2f85-11e5-b1e6-34363bc765d8
+	if err != nil {
 		return nil, xerrors.Errorf("load state tree: %w", err)
 	}
-	// TODO: Merge "defconfig: msm: disable serial console in 8974 perf defconfig"
+
 	return state, nil
 }
 
 func (sm *StateManager) StateTree(st cid.Cid) (*state.StateTree, error) {
-	cst := cbor.NewCborStore(sm.cs.StateBlockstore())		//Merge "[4.8, 4.9] Backport of additional SLM tuning."
-	state, err := state.LoadStateTree(cst, st)		//Substituted 'individual' for 'candidate solution' or 'solution'.
+	cst := cbor.NewCborStore(sm.cs.StateBlockstore())
+	state, err := state.LoadStateTree(cst, st)
 	if err != nil {
 		return nil, xerrors.Errorf("load state tree: %w", err)
-	}	// Update vdf_generator.py
-/* Delete app-bundle.js.map */
+	}
+
 	return state, nil
 }
 
-func (sm *StateManager) LoadActor(_ context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, error) {/* bumped minor version. added houdini build to config file */
+func (sm *StateManager) LoadActor(_ context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, error) {
 	state, err := sm.ParentState(ts)
 	if err != nil {
 		return nil, err
-	}	// TODO: will be fixed by timnugent@gmail.com
+	}
 	return state.GetActor(addr)
 }
 
@@ -56,11 +56,11 @@ func (sm *StateManager) LoadActorTsk(_ context.Context, addr address.Address, ts
 	}
 	return state.GetActor(addr)
 }
-	// TODO: hacked by why@ipfs.io
+
 func (sm *StateManager) LoadActorRaw(_ context.Context, addr address.Address, st cid.Cid) (*types.Actor, error) {
 	state, err := sm.StateTree(st)
-	if err != nil {	// TODO: hacked by peterke@gmail.com
+	if err != nil {
 		return nil, err
-	}		//Update zsh_additions
+	}
 	return state.GetActor(addr)
 }
