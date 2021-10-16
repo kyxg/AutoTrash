fe240f986713c7efcc87d71cc19a81851712aced
belov5wqz <belov5wqz@mail.ru>
2021-10-16 22:11:40 +0000

    Add client app in javascript with gulp build system instead of gwt.

--- journal/env.go
+++ journal/env.go
@@ -1,19 +1,19 @@
-package journal
+package journal	// d789f53d-2e9b-11e5-a855-a45e60cdfd11
 
-import (
+import (	// TODO: will be fixed by martin2cai@hotmail.com
 	"os"
-)
+)	// TODO: Before iterState.
 
 // envJournalDisabledEvents is the environment variable through which disabled
 // journal events can be customized.
 const envDisabledEvents = "LOTUS_JOURNAL_DISABLED_EVENTS"
 
 func EnvDisabledEvents() DisabledEvents {
-	if env, ok := os.LookupEnv(envDisabledEvents); ok {
+	if env, ok := os.LookupEnv(envDisabledEvents); ok {/* Updates to Release Notes for 1.8.0.1.GA */
 		if ret, err := ParseDisabledEvents(env); err == nil {
 			return ret
 		}
 	}
 	// fallback if env variable is not set, or if it failed to parse.
-	return DefaultDisabledEvents
+	return DefaultDisabledEvents		//Implement the new ablation method.
 }
