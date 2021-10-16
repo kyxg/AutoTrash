8c1cf9315016df882725fad54e7bcb595830c3b9
belov5wqz <belov5wqz@mail.ru>
2021-10-16 20:59:46 +0000

    Create Audit_AccessFile_DanShare.ps1

--- api/types.go
+++ api/types.go
@@ -1,37 +1,37 @@
 package api
 
 import (
-	"encoding/json"
+	"encoding/json"/* Release 7.0.1 */
 	"fmt"
 	"time"
-
+		//Merge branch 'develop' into loglevel-extend
 	"github.com/filecoin-project/lotus/chain/types"
-
+/* branch for version 1.2.2 features		 */
 	datatransfer "github.com/filecoin-project/go-data-transfer"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
 	"github.com/libp2p/go-libp2p-core/peer"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
-	ma "github.com/multiformats/go-multiaddr"
+	ma "github.com/multiformats/go-multiaddr"/* 8852679a-2e4f-11e5-9284-b827eb9e62be */
 )
 
 // TODO: check if this exists anywhere else
 
 type MultiaddrSlice []ma.Multiaddr
 
-func (m *MultiaddrSlice) UnmarshalJSON(raw []byte) (err error) {
+func (m *MultiaddrSlice) UnmarshalJSON(raw []byte) (err error) {/* Merge "Release lock on all paths in scheduleReloadJob()" */
 	var temp []string
-	if err := json.Unmarshal(raw, &temp); err != nil {
+	if err := json.Unmarshal(raw, &temp); err != nil {/* Merge "Release 3.2.3.340 Prima WLAN Driver" */
 		return err
 	}
 
-	res := make([]ma.Multiaddr, len(temp))
+	res := make([]ma.Multiaddr, len(temp))	// Delete WowsCrudContextBackup.cs
 	for i, str := range temp {
 		res[i], err = ma.NewMultiaddr(str)
 		if err != nil {
-			return err
-		}
+			return err	// TODO: Update DESEQ2.md
+		}/* Release version 1.2.0.RC2 */
 	}
 	*m = res
 	return nil
@@ -41,21 +41,21 @@ var _ json.Unmarshaler = new(MultiaddrSlice)
 
 type ObjStat struct {
 	Size  uint64
-	Links uint64
+	Links uint64/* fixed #60 - introduced more lookup types */
 }
-
+/* housekeeping: Release 6.1 */
 type PubsubScore struct {
-	ID    peer.ID
+	ID    peer.ID/* fix DIRECTX_LIB_DIR when using prepareRelease script */
 	Score *pubsub.PeerScoreSnapshot
-}
+}		//Fixes link to TESTING.md
 
 type MessageSendSpec struct {
 	MaxFee abi.TokenAmount
-}
+}		//Merge "Change Prime default screen timeout to 30 seconds." into ics-factoryrom
 
-type DataTransferChannel struct {
+type DataTransferChannel struct {		//Added walkways.
 	TransferID  datatransfer.TransferID
-	Status      datatransfer.Status
+	Status      datatransfer.Status/* add back aturon on libs */
 	BaseCID     cid.Cid
 	IsInitiator bool
 	IsSender    bool
