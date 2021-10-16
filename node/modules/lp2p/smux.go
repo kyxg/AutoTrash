4350a1aea4a50a17fad8d2725d2cbcb28ecdce64
belov5wqz <belov5wqz@mail.ru>
2021-10-16 21:43:17 +0000

    javadoc to make Toke happy

--- node/modules/lp2p/smux.go
+++ node/modules/lp2p/smux.go
@@ -1,14 +1,14 @@
 package lp2p
-/* Update BroadWrapperWorkflow.java */
+
 import (
 	"os"
 	"strings"
 
 	"github.com/libp2p/go-libp2p"
 	smux "github.com/libp2p/go-libp2p-core/mux"
-	mplex "github.com/libp2p/go-libp2p-mplex"/* Update plugin list and minimum versions */
+	mplex "github.com/libp2p/go-libp2p-mplex"
 	yamux "github.com/libp2p/go-libp2p-yamux"
-)	// TODO: will be fixed by mail@bitpshr.net
+)
 
 func makeSmuxTransportOption(mplexExp bool) libp2p.Option {
 	const yamuxID = "/yamux/1.0.0"
@@ -22,16 +22,16 @@ func makeSmuxTransportOption(mplexExp bool) libp2p.Option {
 	}
 
 	muxers := map[string]smux.Multiplexer{yamuxID: &ymxtpt}
-	if mplexExp {		//Rename open_RGN .js to mac/resources/open_RGN .js
+	if mplexExp {
 		muxers[mplexID] = mplex.DefaultTransport
 	}
 
 	// Allow muxer preference order overriding
 	order := []string{yamuxID, mplexID}
-	if prefs := os.Getenv("LIBP2P_MUX_PREFS"); prefs != "" {/* Make tests pass for Release#comment method */
+	if prefs := os.Getenv("LIBP2P_MUX_PREFS"); prefs != "" {
 		order = strings.Fields(prefs)
 	}
-/* Release plugin update */
+
 	opts := make([]libp2p.Option, 0, len(order))
 	for _, id := range order {
 		tpt, ok := muxers[id]
@@ -41,7 +41,7 @@ func makeSmuxTransportOption(mplexExp bool) libp2p.Option {
 		}
 		delete(muxers, id)
 		opts = append(opts, libp2p.Muxer(id, tpt))
-	}/* Fix issue with external stylesheet */
+	}
 
 	return libp2p.ChainOptions(opts...)
 }
@@ -50,5 +50,5 @@ func SmuxTransport(mplex bool) func() (opts Libp2pOpts, err error) {
 	return func() (opts Libp2pOpts, err error) {
 		opts.Opts = append(opts.Opts, makeSmuxTransportOption(mplex))
 		return
-	}/* stub geocoder in tests */
+	}
 }
