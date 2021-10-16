e05ed9a50cabc8004d0ce0c365d090e31a014727
belov5wqz <belov5wqz@mail.ru>
2021-10-16 10:32:16 +0000

    Merge "Refactor periodic task sync_bay_status"

--- extern/sector-storage/roprov.go
+++ extern/sector-storage/roprov.go
@@ -5,27 +5,27 @@ import (
 
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/specs-storage/storage"/* Fix a typo in package.json. */
-	// Merge "Apply SQL compilation to sqltext for column-level CHECK constraint"
+	"github.com/filecoin-project/specs-storage/storage"
+
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* one more little fix  */
-)/* Using correct dependenncy */
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
+)
 
 type readonlyProvider struct {
 	index stores.SectorIndex
-	stor  *stores.Local		//Rename keyMapping to keyBinding
-}/* Release version 0.1.3.1. Added a a bit more info to ADL reports. */
+	stor  *stores.Local
+}
 
 func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType) (storiface.SectorPaths, func(), error) {
 	if allocate != storiface.FTNone {
 		return storiface.SectorPaths{}, nil, xerrors.New("read-only storage")
 	}
 
-	ctx, cancel := context.WithCancel(ctx)	// Catch m.youtube.com URLs
-	// TODO: #264 Unhide builder delegate for removeNode/Edge
+	ctx, cancel := context.WithCancel(ctx)
+
 	// use TryLock to avoid blocking
-	locked, err := l.index.StorageTryLock(ctx, id.ID, existing, storiface.FTNone)/* Release notes 7.1.7 */
-	if err != nil {/* Re #26326 Release notes added */
+	locked, err := l.index.StorageTryLock(ctx, id.ID, existing, storiface.FTNone)
+	if err != nil {
 		cancel()
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("acquiring sector lock: %w", err)
 	}
