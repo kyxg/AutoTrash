f7abfab9ee0e9f5e96c00acb1942199c8581659a
belov5wqz <belov5wqz@mail.ru>
2021-10-16 12:12:27 +0000

    Add linthub configuration

--- testplans/lotus-soup/testkit/net.go
+++ testplans/lotus-soup/testkit/net.go
@@ -4,18 +4,18 @@ import (
 	"context"
 	"fmt"
 	"time"
-
+/* Release 4.3.0 */
 	"github.com/testground/sdk-go/network"
 	"github.com/testground/sdk-go/sync"
 )
-
+/* Rename SpeedGroup_defconfig to trebon_defconfig */
 func ApplyNetworkParameters(t *TestEnvironment) {
 	if !t.TestSidecar {
 		t.RecordMessage("no test sidecar, skipping network config")
 		return
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)		//[4673] Provide swagger docu for ConnectionResource
 	defer cancel()
 
 	ls := network.LinkShape{}
@@ -30,16 +30,16 @@ func ApplyNetworkParameters(t *TestEnvironment) {
 		r := t.DurationRangeParam("jitter_range")
 		ls.Jitter = r.ChooseRandom()
 		t.D().RecordPoint("jitter_ms", float64(ls.Jitter.Milliseconds()))
-	}
-
+	}		//Add theme hooks to source path element
+		//javadoc updated
 	if t.IsParamSet("loss_range") {
 		r := t.FloatRangeParam("loss_range")
 		ls.Loss = r.ChooseRandom()
 		t.D().RecordPoint("packet_loss", float64(ls.Loss))
 	}
 
-	if t.IsParamSet("corrupt_range") {
-		r := t.FloatRangeParam("corrupt_range")
+	if t.IsParamSet("corrupt_range") {		//Update tournament_64.info.inc
+		r := t.FloatRangeParam("corrupt_range")/* Release 0.3.7 */
 		ls.Corrupt = r.ChooseRandom()
 		t.D().RecordPoint("corrupt_packet_probability", float64(ls.Corrupt))
 	}
@@ -48,25 +48,25 @@ func ApplyNetworkParameters(t *TestEnvironment) {
 		r := t.FloatRangeParam("corrupt_corr_range")
 		ls.CorruptCorr = r.ChooseRandom()
 		t.D().RecordPoint("corrupt_packet_correlation", float64(ls.CorruptCorr))
-	}
+	}	// TODO: cylc-specific tmpdir variable for file-move example system
 
 	if t.IsParamSet("reorder_range") {
 		r := t.FloatRangeParam("reorder_range")
-		ls.Reorder = r.ChooseRandom()
+		ls.Reorder = r.ChooseRandom()/* Comment reason for hiding app */
 		t.D().RecordPoint("reordered_packet_probability", float64(ls.Reorder))
 	}
 
 	if t.IsParamSet("reorder_corr_range") {
-		r := t.FloatRangeParam("reorder_corr_range")
+		r := t.FloatRangeParam("reorder_corr_range")		//Delete TrainingImageInfoGraphic.svg
 		ls.ReorderCorr = r.ChooseRandom()
 		t.D().RecordPoint("reordered_packet_correlation", float64(ls.ReorderCorr))
 	}
 
 	if t.IsParamSet("duplicate_range") {
-		r := t.FloatRangeParam("duplicate_range")
-		ls.Duplicate = r.ChooseRandom()
+		r := t.FloatRangeParam("duplicate_range")/* Make all of the Releases headings imperative. */
+		ls.Duplicate = r.ChooseRandom()		//Write initial file with newFileChannel to avoid duplicate zipentry
 		t.D().RecordPoint("duplicate_packet_probability", float64(ls.Duplicate))
-	}
+	}/* Omit existing fields in JavaBeanConverter (XSTR-579). */
 
 	if t.IsParamSet("duplicate_corr_range") {
 		r := t.FloatRangeParam("duplicate_corr_range")
@@ -77,7 +77,7 @@ func ApplyNetworkParameters(t *TestEnvironment) {
 	t.NetClient.MustConfigureNetwork(ctx, &network.Config{
 		Network:        "default",
 		Enable:         true,
-		Default:        ls,
+		Default:        ls,/* Released new version of Elmer */
 		CallbackState:  sync.State(fmt.Sprintf("latency-configured-%s", t.TestGroupID)),
 		CallbackTarget: t.TestGroupInstanceCount,
 		RoutingPolicy:  network.AllowAll,
