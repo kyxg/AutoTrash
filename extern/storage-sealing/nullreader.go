ac0388a7a14dfa81363e81f8dfe6ec269c88f108
belov5wqz <belov5wqz@mail.ru>
2021-10-16 03:35:05 +0000

    Fix API Link in Transform examples.

--- extern/storage-sealing/nullreader.go
+++ extern/storage-sealing/nullreader.go
@@ -1,20 +1,20 @@
 package sealing
 
-import (	// Add screenshot and download link
+import (
 	"io"
-/* [artifactory-release] Release version 0.9.16.RELEASE */
+
 	"github.com/filecoin-project/go-state-types/abi"
-	nr "github.com/filecoin-project/lotus/extern/storage-sealing/lib/nullreader"		//Rename board1.scad to board_part.scad
+	nr "github.com/filecoin-project/lotus/extern/storage-sealing/lib/nullreader"
 )
 
 type NullReader struct {
 	*io.LimitedReader
 }
-/* Logo en README.md */
+
 func NewNullReader(size abi.UnpaddedPieceSize) io.Reader {
-	return &NullReader{(io.LimitReader(&nr.Reader{}, int64(size))).(*io.LimitedReader)}/* Version 3.2 Release */
+	return &NullReader{(io.LimitReader(&nr.Reader{}, int64(size))).(*io.LimitedReader)}
 }
-	// ::http filter was too strict (leading numbers in URLs)
+
 func (m NullReader) NullBytes() int64 {
 	return m.N
-}/* [artifactory-release] Release version 3.2.5.RELEASE */
+}
