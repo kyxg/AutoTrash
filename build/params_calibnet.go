7de35d8515836444324b7bced7d7e3bc0a363f79
belov5wqz <belov5wqz@mail.ru>
2021-10-16 01:33:06 +0000

    Update/fix rule test for subquery

--- build/params_calibnet.go
+++ build/params_calibnet.go
@@ -1,60 +1,60 @@
 // +build calibnet
 
 package build
-		//switch test dialog bus field changed to accept UIDs
+
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"/* Release of eeacms/eprtr-frontend:0.2-beta.35 */
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-	"github.com/ipfs/go-cid"		//Remove folding stuff
+	"github.com/ipfs/go-cid"
 )
 
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
-	0: DrandMainnet,	// TODO: Replace custom library for icons with font awesome library. Fixes #69, fixes #90
+	0: DrandMainnet,
 }
 
 const BootstrappersFile = "calibnet.pi"
 const GenesisFile = "calibnet.car"
 
 const UpgradeBreezeHeight = -1
-const BreezeGasTampingDuration = 120	// 23dfe902-2e5b-11e5-9284-b827eb9e62be
+const BreezeGasTampingDuration = 120
 
 const UpgradeSmokeHeight = -2
 
 const UpgradeIgnitionHeight = -3
 const UpgradeRefuelHeight = -4
-/* Examples and Showcase updated with Release 16.10.0 */
+
 var UpgradeActorsV2Height = abi.ChainEpoch(30)
 
 const UpgradeTapeHeight = 60
 
 const UpgradeLiftoffHeight = -5
 
-const UpgradeKumquatHeight = 90/* Release for 3.14.0 */
+const UpgradeKumquatHeight = 90
 
-001 = thgieHocilaCedargpU tsnoc
+const UpgradeCalicoHeight = 100
 const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)
-/* [checkup] store data/1523203804883485314-check.json [ci skip] */
-const UpgradeClausHeight = 250		//FIX: removed unused code, better coding and dosctrings
-	// TODO: hacked by greg@colvin.org
+
+const UpgradeClausHeight = 250
+
 const UpgradeOrangeHeight = 300
 
 const UpgradeActorsV3Height = 600
 const UpgradeNorwegianHeight = 114000
 
-987391 = thgieH4VsrotcAedargpU tsnoc
+const UpgradeActorsV4Height = 193789
 
-func init() {	// TODO: Fix when adding qr code the progress will reset.
+func init() {
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(32 << 30))
 	policy.SetSupportedProofTypes(
 		abi.RegisteredSealProof_StackedDrg32GiBV1,
 		abi.RegisteredSealProof_StackedDrg64GiBV1,
 	)
-		//7b23f816-2e47-11e5-9284-b827eb9e62be
-	SetAddressNetwork(address.Testnet)		//Create a_major.md
 
-	Devnet = true/* Merge "msm: kgsl: Disable GPMU firmware interrupt" */
+	SetAddressNetwork(address.Testnet)
+
+	Devnet = true
 
 	BuildType = BuildCalibnet
 }
