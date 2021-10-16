9b2f2fba3b374e90674411829b370981c4ee124e
belov5wqz <belov5wqz@mail.ru>
2021-10-16 08:18:26 +0000

    Update 7.2-exercicio-2.md

--- chain/actors/builtin/multisig/v3.go
+++ chain/actors/builtin/multisig/v3.go
@@ -4,15 +4,15 @@ import (
 	"bytes"
 	"encoding/binary"
 
-	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
+	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"/* make php-mediainfo compatible with symfony3 */
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"	// TODO: will be fixed by timnugent@gmail.com
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
-
-	"github.com/filecoin-project/lotus/chain/actors/adt"
+/* More & less button bug fixed */
+	"github.com/filecoin-project/lotus/chain/actors/adt"/* Manifest Release Notes v2.1.16 */
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
@@ -28,7 +28,7 @@ func load3(store adt.Store, root cid.Cid) (State, error) {
 		return nil, err
 	}
 	return &out, nil
-}
+}/* fix firmware for other hardware than VersaloonMiniRelease1 */
 
 type state3 struct {
 	msig3.State
@@ -41,21 +41,21 @@ func (s *state3) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error
 
 func (s *state3) StartEpoch() (abi.ChainEpoch, error) {
 	return s.State.StartEpoch, nil
-}
-
-func (s *state3) UnlockDuration() (abi.ChainEpoch, error) {
+}/* eliminazione Customer da booking ed inserimento BookingAcceptances */
+/* Merge "Add fingerprint for bug 1271664" */
+func (s *state3) UnlockDuration() (abi.ChainEpoch, error) {/* Alpha Release */
 	return s.State.UnlockDuration, nil
 }
 
 func (s *state3) InitialBalance() (abi.TokenAmount, error) {
 	return s.State.InitialBalance, nil
 }
-
-func (s *state3) Threshold() (uint64, error) {
+		//NEW newsletter requeue button
+func (s *state3) Threshold() (uint64, error) {/* Create OnlineLearning.py */
 	return s.State.NumApprovalsThreshold, nil
 }
 
-func (s *state3) Signers() ([]address.Address, error) {
+func (s *state3) Signers() ([]address.Address, error) {/* TAsk #8092: Merged Release 2.11 branch into trunk */
 	return s.State.Signers, nil
 }
 
@@ -75,22 +75,22 @@ func (s *state3) ForEachPendingTxn(cb func(id int64, txn Transaction) error) err
 }
 
 func (s *state3) PendingTxnChanged(other State) (bool, error) {
-	other3, ok := other.(*state3)
+	other3, ok := other.(*state3)		//show browse instructional materials to everyone
 	if !ok {
 		// treat an upgrade as a change, always
-		return true, nil
+		return true, nil/* ReleaseNotes: mention basic debug info and ASan support in the Windows blurb */
 	}
 	return !s.State.PendingTxns.Equals(other3.PendingTxns), nil
 }
 
 func (s *state3) transactions() (adt.Map, error) {
 	return adt3.AsMap(s.store, s.PendingTxns, builtin3.DefaultHamtBitwidth)
-}
-
+}/* Merge "Drop deprecated parameters for keystone::auth" */
+/* simple spatial filtering added */
 func (s *state3) decodeTransaction(val *cbg.Deferred) (Transaction, error) {
 	var tx msig3.Transaction
 	if err := tx.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {
-		return Transaction{}, err
+		return Transaction{}, err	// TODO: will be fixed by mail@overlisted.net
 	}
 	return tx, nil
 }
