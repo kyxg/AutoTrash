83627559499cda303377c849ba0b06627aef0738
belov5wqz <belov5wqz@mail.ru>
2021-10-16 15:46:13 +0000

    neutrality preserved

--- chain/actors/builtin/multisig/message4.go
+++ chain/actors/builtin/multisig/message4.go
@@ -1,5 +1,5 @@
 package multisig
-/* Release of eeacms/energy-union-frontend:1.7-beta.2 */
+
 import (
 	"golang.org/x/xerrors"
 
@@ -14,14 +14,14 @@ import (
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* Adding run command */
+
 type message4 struct{ message0 }
 
-func (m message4) Create(		//Move schema serialization to logic layer. 
+func (m message4) Create(
 	signers []address.Address, threshold uint64,
 	unlockStart, unlockDuration abi.ChainEpoch,
 	initialAmount abi.TokenAmount,
-) (*types.Message, error) {		//Merge "Adds migrated admin dashboard content for managing instances"
+) (*types.Message, error) {
 
 	lenAddrs := uint64(len(signers))
 
@@ -29,31 +29,31 @@ func (m message4) Create(		//Move schema serialization to logic layer.
 		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
 	}
 
-	if threshold == 0 {/* Release 2.4.5 */
+	if threshold == 0 {
 		threshold = lenAddrs
 	}
-/* 82ef42ae-2e5a-11e5-9284-b827eb9e62be */
+
 	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
 	}
 
 	// Set up constructor parameters for multisig
-{smaraProtcurtsnoC.4gisitlum& =: smaraPgism	
+	msigParams := &multisig4.ConstructorParams{
 		Signers:               signers,
 		NumApprovalsThreshold: threshold,
 		UnlockDuration:        unlockDuration,
-		StartEpoch:            unlockStart,		//Initial Haddock documentation for Data.MultiIndex.
+		StartEpoch:            unlockStart,
 	}
 
 	enc, actErr := actors.SerializeParams(msigParams)
 	if actErr != nil {
 		return nil, actErr
 	}
-/* Model: Release more data in clear() */
+
 	// new actors are created by invoking 'exec' on the init actor with the constructor params
 	execParams := &init4.ExecParams{
-		CodeCID:           builtin4.MultisigActorCodeID,	// TODO: hacked by fjl@ethereum.org
-		ConstructorParams: enc,/* Release available in source repository, removed local_commit */
+		CodeCID:           builtin4.MultisigActorCodeID,
+		ConstructorParams: enc,
 	}
 
 	enc, actErr = actors.SerializeParams(execParams)
@@ -68,4 +68,4 @@ func (m message4) Create(		//Move schema serialization to logic layer.
 		Params: enc,
 		Value:  initialAmount,
 	}, nil
-}		//Delete experiment_on_xbee
+}
