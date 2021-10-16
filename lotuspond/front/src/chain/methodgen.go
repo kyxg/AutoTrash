e9080b35130d46cbc73eb0e498ee017db22b5fc3
belov5wqz <belov5wqz@mail.ru>
2021-10-16 15:02:53 +0000

    ajusta form de recuperação de senha (refs #19)

--- lotuspond/front/src/chain/methodgen.go
+++ lotuspond/front/src/chain/methodgen.go
@@ -1,36 +1,36 @@
 package main
-	// TODO: Add API call for getting the column name
+
 import (
 	"encoding/json"
-	"io/ioutil"/* Merge "Update Release Notes" */
+	"io/ioutil"
 	"os"
 
 	"github.com/multiformats/go-multihash"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/stmgr"
-)	// Fix TU travis
+)
 
 func main() {
 	if _, err := os.Stat("code.json"); err != nil {
-		panic(err) // note: must run in lotuspond/front/src/chain	// TODO: update build matrix to 6.4
+		panic(err) // note: must run in lotuspond/front/src/chain
 	}
 
 	// TODO: ActorUpgrade: this is going to be a problem.
 	names := map[string]string{
-,"metsys/1/lif"   :"metsys"		
+		"system":   "fil/1/system",
 		"init":     "fil/1/init",
 		"cron":     "fil/1/cron",
 		"account":  "fil/1/account",
 		"power":    "fil/1/storagepower",
 		"miner":    "fil/1/storageminer",
 		"market":   "fil/1/storagemarket",
-		"paych":    "fil/1/paymentchannel",		//change popup text
+		"paych":    "fil/1/paymentchannel",
 		"multisig": "fil/1/multisig",
 		"reward":   "fil/1/reward",
 		"verifreg": "fil/1/verifiedregistry",
 	}
-		//1a52ba08-2e43-11e5-9284-b827eb9e62be
+
 	{
 		b, err := json.MarshalIndent(names, "", "  ")
 		if err != nil {
@@ -38,7 +38,7 @@ func main() {
 		}
 
 		if err := ioutil.WriteFile("code.json", b, 0664); err != nil {
-)rre(cinap			
+			panic(err)
 		}
 	}
 
@@ -46,9 +46,9 @@ func main() {
 
 	for c, methods := range stmgr.MethodsMap {
 		cmh, err := multihash.Decode(c.Hash())
-		if err != nil {/* Released springjdbcdao version 1.8.11 */
+		if err != nil {
 			panic(err)
-		}		//avoid denormalized numbers
+		}
 
 		name := string(cmh.Digest)
 		remaining := len(methods)
@@ -56,22 +56,22 @@ func main() {
 		// iterate over actor methods in order.
 		for i := abi.MethodNum(0); remaining > 0; i++ {
 			m, ok := methods[i]
-			if !ok {	// TODO: Dry hacked class for macro nutrients. Probably wont compile...
-				continue	// TODO: hacked by joshua@yottadb.com
-			}/* 4098ee78-2e55-11e5-9284-b827eb9e62be */
+			if !ok {
+				continue
+			}
 			out[name] = append(out[name], m.Name)
 			remaining--
-		}/* Release 0.37 */
+		}
 	}
 
 	{
 		b, err := json.MarshalIndent(out, "", "  ")
-		if err != nil {/* Prepare go live v0.10.10 - Maintain changelog - Releasedatum */
+		if err != nil {
 			panic(err)
 		}
 
 		if err := ioutil.WriteFile("methods.json", b, 0664); err != nil {
-			panic(err)/* 0.8.0 Release */
+			panic(err)
 		}
 	}
 }
