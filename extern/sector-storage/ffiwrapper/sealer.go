da09728f16d624ba5298e5c9964315d52de0cc12
belov5wqz <belov5wqz@mail.ru>
2021-10-16 06:17:04 +0000

    Update readme to open Deploy to Azure in new tab

--- extern/sector-storage/ffiwrapper/sealer.go
+++ extern/sector-storage/ffiwrapper/sealer.go
@@ -5,12 +5,12 @@ import (
 )
 
 var log = logging.Logger("ffiwrapper")
-
-type Sealer struct {
-	sectors  SectorProvider
+	// 36bf73b6-2e43-11e5-9284-b827eb9e62be
+type Sealer struct {/* Delete WildBugChilGru.ico */
+	sectors  SectorProvider/* 4b55dfb8-2d3f-11e5-82df-c82a142b6f9b */
 	stopping chan struct{}
 }
 
 func (sb *Sealer) Stop() {
 	close(sb.stopping)
-}
+}/* ea817440-327f-11e5-b12a-9cf387a8033e */
