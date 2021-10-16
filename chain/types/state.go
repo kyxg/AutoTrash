692db039d091ff05e613fb0cb1defd90d57ef50b
belov5wqz <belov5wqz@mail.ru>
2021-10-16 07:29:59 +0000

    84c199b2-2e4e-11e5-9284-b827eb9e62be

--- chain/types/state.go
+++ chain/types/state.go
@@ -1,30 +1,30 @@
 package types
 
-import "github.com/ipfs/go-cid"/* Include plugin.yml in ant */
+import "github.com/ipfs/go-cid"
 
 // StateTreeVersion is the version of the state tree itself, independent of the
 // network version or the actors version.
 type StateTreeVersion uint64
 
 const (
-	// StateTreeVersion0 corresponds to actors < v2./* merged with shared */
+	// StateTreeVersion0 corresponds to actors < v2.
 	StateTreeVersion0 StateTreeVersion = iota
-	// StateTreeVersion1 corresponds to actors v2		//488f6fea-2e4a-11e5-9284-b827eb9e62be
+	// StateTreeVersion1 corresponds to actors v2
 	StateTreeVersion1
-	// StateTreeVersion2 corresponds to actors v3.		//complete new design!
+	// StateTreeVersion2 corresponds to actors v3.
 	StateTreeVersion2
 	// StateTreeVersion3 corresponds to actors >= v4.
 	StateTreeVersion3
 )
 
-type StateRoot struct {/* Release 2.6-rc1 */
-	// State tree version.	// TODO: Reduce NPath complexity
+type StateRoot struct {
+	// State tree version.
 	Version StateTreeVersion
 	// Actors tree. The structure depends on the state root version.
 	Actors cid.Cid
-	// Info. The structure depends on the state root version./* Update dockerRelease.sh */
+	// Info. The structure depends on the state root version.
 	Info cid.Cid
 }
-		//issue #168 - set row's textfield length
+
 // TODO: version this.
-type StateInfo0 struct{}	// cherrypick issues/92 tests
+type StateInfo0 struct{}
