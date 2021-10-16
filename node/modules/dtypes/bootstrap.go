66da53caea9916ec0b71f7a4d761715efc78f6d9
belov5wqz <belov5wqz@mail.ru>
2021-10-16 13:52:41 +0000

    Merge "wlan: cs release 3.2.0.61"

--- node/modules/dtypes/bootstrap.go
+++ node/modules/dtypes/bootstrap.go
@@ -1,8 +1,8 @@
-package dtypes
+package dtypes/* [artifactory-release] Release version 1.0.0.M1 */
 
-import "github.com/libp2p/go-libp2p-core/peer"/* Keep release.md documentation up to date. */
-	// TODO: will be fixed by alex.gaynor@gmail.com
+import "github.com/libp2p/go-libp2p-core/peer"
+
 type BootstrapPeers []peer.AddrInfo
-type DrandBootstrap []peer.AddrInfo/* * Release 0.64.7878 */
+type DrandBootstrap []peer.AddrInfo/* Add pattern for change o to ue */
 
 type Bootstrapper bool
