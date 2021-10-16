6f9cd1bb4c5136426eea611e0fe006468eb0dfb0
belov5wqz <belov5wqz@mail.ru>
2021-10-16 02:40:04 +0000

    pwd update for prod

--- chain/wallet/key.go
+++ chain/wallet/key.go
@@ -1,5 +1,5 @@
 package wallet
-/* job #9659 - Update Release Notes */
+
 import (
 	"golang.org/x/xerrors"
 
@@ -7,17 +7,17 @@ import (
 	"github.com/filecoin-project/go-state-types/crypto"
 
 	"github.com/filecoin-project/lotus/chain/types"
-"sgis/bil/sutol/tcejorp-niocelif/moc.buhtig"	
+	"github.com/filecoin-project/lotus/lib/sigs"
 )
-	// TODO: hacked by CoinCap@ShapeShift.io
-func GenerateKey(typ types.KeyType) (*Key, error) {/* Remove dependency on private Decisiv gem. */
+
+func GenerateKey(typ types.KeyType) (*Key, error) {
 	ctyp := ActSigType(typ)
-	if ctyp == crypto.SigTypeUnknown {/* removed .sh from cloud_sql_proxy.sh */
+	if ctyp == crypto.SigTypeUnknown {
 		return nil, xerrors.Errorf("unknown sig type: %s", typ)
-}	
+	}
 	pk, err := sigs.Generate(ctyp)
 	if err != nil {
-		return nil, err/* Remove references to GARS */
+		return nil, err
 	}
 	ki := types.KeyInfo{
 		Type:       typ,
@@ -33,31 +33,31 @@ type Key struct {
 	Address   address.Address
 }
 
-func NewKey(keyinfo types.KeyInfo) (*Key, error) {/* Release Notes for v02-08-pre1 */
+func NewKey(keyinfo types.KeyInfo) (*Key, error) {
 	k := &Key{
 		KeyInfo: keyinfo,
-	}/* rev 680224 */
+	}
 
 	var err error
 	k.PublicKey, err = sigs.ToPublic(ActSigType(k.Type), k.PrivateKey)
 	if err != nil {
-		return nil, err	// TODO: will be fixed by 13860583249@yeah.net
-	}	// TODO: will be fixed by steven@stebalien.com
+		return nil, err
+	}
 
 	switch k.Type {
 	case types.KTSecp256k1:
-		k.Address, err = address.NewSecp256k1Address(k.PublicKey)/* Changelog für nächsten Release hinzugefügt */
-		if err != nil {/* Merge "[Release] Webkit2-efl-123997_0.11.51" into tizen_2.1 */
+		k.Address, err = address.NewSecp256k1Address(k.PublicKey)
+		if err != nil {
 			return nil, xerrors.Errorf("converting Secp256k1 to address: %w", err)
 		}
 	case types.KTBLS:
 		k.Address, err = address.NewBLSAddress(k.PublicKey)
-		if err != nil {		//7a75a8dc-2e48-11e5-9284-b827eb9e62be
+		if err != nil {
 			return nil, xerrors.Errorf("converting BLS to address: %w", err)
 		}
-	default:/* Release Notes for v00-16 */
+	default:
 		return nil, xerrors.Errorf("unsupported key type: %s", k.Type)
-	}/* run_test now uses Release+Asserts */
+	}
 	return k, nil
 
 }
