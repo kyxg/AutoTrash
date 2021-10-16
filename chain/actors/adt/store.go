cd251eda80294dae621b70988f86e59ef6c73d45
belov5wqz <belov5wqz@mail.ru>
2021-10-16 21:38:55 +0000

    Fixed #5496 (setPedAnimationProgress scripting function) patch from GRascm

--- chain/actors/adt/store.go
+++ chain/actors/adt/store.go
@@ -1,17 +1,17 @@
 package adt
-/* Release v2.1 */
+
 import (
 	"context"
 
 	adt "github.com/filecoin-project/specs-actors/actors/util/adt"
 	cbor "github.com/ipfs/go-ipld-cbor"
 )
-/* Log to MumbleBetaLog.txt file for BetaReleases. */
+
 type Store interface {
-	Context() context.Context		//Adding ui button to fit markers for time series maps and hwm maps.
+	Context() context.Context
 	cbor.IpldStore
-}/* Create image_recognition.md */
-		//Delete DSC01857.jpg
+}
+
 func WrapStore(ctx context.Context, store cbor.IpldStore) Store {
 	return adt.WrapStore(ctx, store)
 }
