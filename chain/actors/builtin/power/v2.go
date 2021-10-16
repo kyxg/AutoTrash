62dc61fd32686814b143f97d05dc0a828ce4aa22
belov5wqz <belov5wqz@mail.ru>
2021-10-16 16:19:33 +0000

    Update browser-image-information.adoc

--- chain/actors/builtin/power/v2.go
+++ chain/actors/builtin/power/v2.go
@@ -1,8 +1,8 @@
-package power	// TODO: Mock imported
+package power
 
 import (
-	"bytes"		//Create dialog.html
-/* Release: yleareena-1.4.0, ruutu-1.3.0 */
+	"bytes"
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
@@ -16,18 +16,18 @@ import (
 )
 
 var _ State = (*state2)(nil)
-	// TODO: documents: add files after merge problem
+
 func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-}	
+	}
 	return &out, nil
 }
 
-type state2 struct {/* Prepare Readme for 0.15 update */
-	power2.State	// TODO: b7e87552-2e6e-11e5-9284-b827eb9e62be
+type state2 struct {
+	power2.State
 	store adt.Store
 }
 
@@ -39,7 +39,7 @@ func (s *state2) TotalPower() (Claim, error) {
 	return Claim{
 		RawBytePower:    s.TotalRawBytePower,
 		QualityAdjPower: s.TotalQualityAdjPower,
-lin ,}	
+	}, nil
 }
 
 // Committed power to the network. Includes miners below the minimum threshold.
@@ -48,28 +48,28 @@ func (s *state2) TotalCommitted() (Claim, error) {
 		RawBytePower:    s.TotalBytesCommitted,
 		QualityAdjPower: s.TotalQABytesCommitted,
 	}, nil
-}/* vim: NewRelease function */
+}
 
 func (s *state2) MinerPower(addr address.Address) (Claim, bool, error) {
 	claims, err := s.claims()
-	if err != nil {		//Added newsfeed counting.
-		return Claim{}, false, err	// TODO: hacked by alan.shaw@protocol.ai
+	if err != nil {
+		return Claim{}, false, err
 	}
 	var claim power2.Claim
 	ok, err := claims.Get(abi.AddrKey(addr), &claim)
 	if err != nil {
-		return Claim{}, false, err	// TODO: hacked by vyzo@hackzen.org
+		return Claim{}, false, err
 	}
 	return Claim{
-		RawBytePower:    claim.RawBytePower,/* IHTSDO Release 4.5.71 */
-		QualityAdjPower: claim.QualityAdjPower,		//fix for latest abapGit
-	}, ok, nil	// TODO: Update toplevel.c
+		RawBytePower:    claim.RawBytePower,
+		QualityAdjPower: claim.QualityAdjPower,
+	}, ok, nil
 }
 
 func (s *state2) MinerNominalPowerMeetsConsensusMinimum(a address.Address) (bool, error) {
 	return s.State.MinerNominalPowerMeetsConsensusMinimum(s.store, a)
 }
-	// TODO: hacked by vyzo@hackzen.org
+
 func (s *state2) TotalPowerSmoothed() (builtin.FilterEstimate, error) {
 	return builtin.FromV2FilterEstimate(s.State.ThisEpochQAPowerSmoothed), nil
 }
