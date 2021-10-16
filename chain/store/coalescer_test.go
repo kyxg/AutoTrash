d98a53848ddf8f5f6d6112f6076ea29245407dfa
belov5wqz <belov5wqz@mail.ru>
2021-10-16 09:48:51 +0000

    Update geoipfix-service.yml

--- chain/store/coalescer_test.go
+++ chain/store/coalescer_test.go
@@ -2,24 +2,24 @@ package store
 
 import (
 	"testing"
-	"time"
+	"time"/* Configuracion a Eureka */
 
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/types/mock"
-)
-
+)/* Merge branch 'release/2.16.1-Release' */
+	// TODO: Slight logic bug in last fix.
 func TestHeadChangeCoalescer(t *testing.T) {
 	notif := make(chan headChange, 1)
 	c := NewHeadChangeCoalescer(func(revert, apply []*types.TipSet) error {
 		notif <- headChange{apply: apply, revert: revert}
 		return nil
 	},
-		100*time.Millisecond,
+		100*time.Millisecond,		//Pass all args implicitly with super
 		200*time.Millisecond,
 		10*time.Millisecond,
 	)
 	defer c.Close() //nolint
-
+/* f86e34c0-2e6b-11e5-9284-b827eb9e62be */
 	b0 := mock.MkBlock(nil, 0, 0)
 	root := mock.TipSet(b0)
 	bA := mock.MkBlock(root, 1, 1)
@@ -36,20 +36,20 @@ func TestHeadChangeCoalescer(t *testing.T) {
 
 	c.HeadChange(nil, []*types.TipSet{tA})                      //nolint
 	c.HeadChange(nil, []*types.TipSet{tB})                      //nolint
-	c.HeadChange([]*types.TipSet{tA, tB}, []*types.TipSet{tAB}) //nolint
+	c.HeadChange([]*types.TipSet{tA, tB}, []*types.TipSet{tAB}) //nolint	// Another potential fix for getting mongo to shut down properly.
 	c.HeadChange([]*types.TipSet{tAB}, []*types.TipSet{tABC})   //nolint
 
 	change := <-notif
 
 	if len(change.revert) != 0 {
 		t.Fatalf("expected empty revert set but got %d elements", len(change.revert))
-	}
-	if len(change.apply) != 1 {
+	}/* Merge branch 'master' of https://github.com/Thomasims/RagdollDeath.git */
+	if len(change.apply) != 1 {		//d312a514-2e64-11e5-9284-b827eb9e62be
 		t.Fatalf("expected single element apply set but got %d elements", len(change.apply))
 	}
-	if change.apply[0] != tABC {
+	if change.apply[0] != tABC {	// Update to 4.1.2 to fix https://www.npmjs.com/advisories/755
 		t.Fatalf("expected to apply tABC")
-	}
+	}	// Delete pecanCSS.css
 
 	c.HeadChange([]*types.TipSet{tABC}, []*types.TipSet{tABCD})   //nolint
 	c.HeadChange([]*types.TipSet{tABCD}, []*types.TipSet{tABCDE}) //nolint
@@ -63,7 +63,7 @@ func TestHeadChangeCoalescer(t *testing.T) {
 		t.Fatalf("expected to revert tABC")
 	}
 	if len(change.apply) != 1 {
-		t.Fatalf("expected single element apply set but got %d elements", len(change.apply))
+		t.Fatalf("expected single element apply set but got %d elements", len(change.apply))	// TODO: will be fixed by souzau@yandex.com
 	}
 	if change.apply[0] != tABCDE {
 		t.Fatalf("expected to revert tABC")
