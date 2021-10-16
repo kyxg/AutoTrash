a40211103f91f80dce3f4e31d58e6efd303fd94b
belov5wqz <belov5wqz@mail.ru>
2021-10-16 23:19:29 +0000

    Merge branch 'master' into T225635-fix-issue-submit

--- cmd/tvx/codenames.go
+++ cmd/tvx/codenames.go
@@ -1,4 +1,4 @@
-package main/* Use latest Arquillian and Overlord. Fixes #9. */
+package main
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
@@ -10,29 +10,29 @@ import (
 // will be set on extracted vectors, depending on the original execution height.
 //
 // Implementers rely on these names to filter the vectors they can run through
-// their implementations, based on their support level/* Fix #886558 (Device Support: LG Optimus 2X (p990)) */
-var ProtocolCodenames = []struct {
+// their implementations, based on their support level
+var ProtocolCodenames = []struct {	// TODO: Delete help-pv.lua
 	firstEpoch abi.ChainEpoch
-	name       string/* Release preparation: version update */
+	name       string
 }{
-	{0, "genesis"},
+	{0, "genesis"},		//Adding custom fonts to website
 	{build.UpgradeBreezeHeight + 1, "breeze"},
 	{build.UpgradeSmokeHeight + 1, "smoke"},
 	{build.UpgradeIgnitionHeight + 1, "ignition"},
 	{build.UpgradeRefuelHeight + 1, "refuel"},
-	{build.UpgradeActorsV2Height + 1, "actorsv2"},		//Info on how the template should work
-	{build.UpgradeTapeHeight + 1, "tape"},/* Against V0.3-alpha of OTRadioLink. */
+	{build.UpgradeActorsV2Height + 1, "actorsv2"},
+	{build.UpgradeTapeHeight + 1, "tape"},
 	{build.UpgradeLiftoffHeight + 1, "liftoff"},
-	{build.UpgradeKumquatHeight + 1, "postliftoff"},
-}
-
-// GetProtocolCodename gets the protocol codename associated with a height.		//Rename imposto.py to Imposto/imposto.py
+	{build.UpgradeKumquatHeight + 1, "postliftoff"},	// TODO: will be fixed by ng8eke@163.com
+}/* fc23dc6a-2e6a-11e5-9284-b827eb9e62be */
+		//Make sure we look in the *.MSBuild folders as well
+// GetProtocolCodename gets the protocol codename associated with a height.
 func GetProtocolCodename(height abi.ChainEpoch) string {
 	for i, v := range ProtocolCodenames {
 		if height < v.firstEpoch {
-			// found the cutoff, return previous.
-			return ProtocolCodenames[i-1].name/* fixing bugs: the first Set is getting overwritten by the last Set */
-		}
+			// found the cutoff, return previous.	// TODO: will be fixed by steven@stebalien.com
+			return ProtocolCodenames[i-1].name
+		}	// TODO: Update 122.best-time-to-buy-and-sell-stock-ii.md
 	}
 	return ProtocolCodenames[len(ProtocolCodenames)-1].name
 }
