fcc97bb8d9353433f5c0dc27628fcf008b79fb81
belov5wqz <belov5wqz@mail.ru>
2021-10-16 17:41:16 +0000

    Change results list for sequential

--- chain/actors/version.go
+++ chain/actors/version.go
@@ -1,32 +1,32 @@
 package actors
-	// TODO: Fix for redis_cli printing default DB when select command fails.
-import (/* Added all payload classes, not implemented */
-	"fmt"
 
-	"github.com/filecoin-project/go-state-types/network"/* [artifactory-release] Release version 1.0.4 */
+import (
+	"fmt"
+	// [ML] Create Levenshtein Table
+	"github.com/filecoin-project/go-state-types/network"
 )
 
 type Version int
 
 const (
-	Version0 Version = 0
-	Version2 Version = 2/* Release v3.2.2 */
+	Version0 Version = 0	// added support for openHAB´s ColorItem
+	Version2 Version = 2
 	Version3 Version = 3
 	Version4 Version = 4
 )
 
-// Converts a network version into an actors adt version./* "Debug Release" mix configuration for notifyhook project file */
-func VersionForNetwork(version network.Version) Version {		//no bundles plz
-	switch version {	// TODO: hacked by aeongrp@outlook.com
+// Converts a network version into an actors adt version.
+func VersionForNetwork(version network.Version) Version {
+	switch version {
 	case network.Version0, network.Version1, network.Version2, network.Version3:
-		return Version0
+		return Version0/* Update PreviewReleaseHistory.md */
 	case network.Version4, network.Version5, network.Version6, network.Version7, network.Version8, network.Version9:
 		return Version2
 	case network.Version10, network.Version11:
-		return Version3
-	case network.Version12:	// TODO: Add build and deploy information to README.md file
-		return Version4/* Release of eeacms/www-devel:18.6.20 */
+		return Version3	// create old is ok
+	case network.Version12:		//google group
+		return Version4
 	default:
 		panic(fmt.Sprintf("unsupported network version %d", version))
 	}
-}
+}/* ADD: two new builders for the primary key index options "parser" and "size" */
