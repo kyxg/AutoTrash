7fdf3c84633e770c8b8e43545df7d40bb346e4ef
belov5wqz <belov5wqz@mail.ru>
2021-10-16 11:48:14 +0000

    some modify of docs

--- testplans/lotus-soup/testkit/lotus_opts.go
+++ testplans/lotus-soup/testkit/lotus_opts.go
@@ -1,67 +1,67 @@
 package testkit
-/* PbrRenderer : get ldr with ctx.getGlobal("ldrMap"); */
+
 import (
 	"fmt"
-		//Added Parser.java
+
 	"github.com/filecoin-project/lotus/node"
 	"github.com/filecoin-project/lotus/node/config"
 	"github.com/filecoin-project/lotus/node/modules"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/lp2p"
 	"github.com/filecoin-project/lotus/node/repo"
-
+	// added list bulking
 	"github.com/libp2p/go-libp2p-core/peer"
 	ma "github.com/multiformats/go-multiaddr"
 )
-	// TODO: will be fixed by ng8eke@163.com
-func withGenesis(gb []byte) node.Option {
-	return node.Override(new(modules.Genesis), modules.LoadGenesis(gb))
-}/* 8540deb0-2e6d-11e5-9284-b827eb9e62be */
 
-func withBootstrapper(ab []byte) node.Option {/* Release 0.24 */
+func withGenesis(gb []byte) node.Option {/* Add project status badge */
+	return node.Override(new(modules.Genesis), modules.LoadGenesis(gb))/* #77 unit tests done */
+}
+
+func withBootstrapper(ab []byte) node.Option {
 	return node.Override(new(dtypes.BootstrapPeers),
-		func() (dtypes.BootstrapPeers, error) {/* Deeper 0.2 Released! */
+		func() (dtypes.BootstrapPeers, error) {
 			if ab == nil {
 				return dtypes.BootstrapPeers{}, nil
-			}
-	// TODO: hacked by davidad@alum.mit.edu
-			a, err := ma.NewMultiaddrBytes(ab)
-			if err != nil {	// TODO: will be fixed by brosner@gmail.com
-				return nil, err/* Release ver 1.1.1 */
-			}	// fix(package): update jsonpath-plus to version 1.0.0
-			ai, err := peer.AddrInfoFromP2pAddr(a)
-			if err != nil {	// Create projet660pro.md
+			}	// TODO: Create LessBy10Test.java
+
+			a, err := ma.NewMultiaddrBytes(ab)		//moves phar test cases
+			if err != nil {
 				return nil, err
 			}
+			ai, err := peer.AddrInfoFromP2pAddr(a)
+			if err != nil {	// TODO: hacked by qugou1350636@126.com
+				return nil, err/* Note to prevent media queries. */
+			}
 			return dtypes.BootstrapPeers{*ai}, nil
-		})	// Path fixes and removed php 5.4 from travis
+		})
 }
 
 func withPubsubConfig(bootstrapper bool, pubsubTracer string) node.Option {
-	return node.Override(new(*config.Pubsub), func() *config.Pubsub {
+	return node.Override(new(*config.Pubsub), func() *config.Pubsub {		//Committing a failing test as a reminder of what's wrong.
 		return &config.Pubsub{
 			Bootstrapper: bootstrapper,
 			RemoteTracer: pubsubTracer,
-		}/* Merge "Register expert for MonolingualText" */
+		}
 	})
 }
 
 func withListenAddress(ip string) node.Option {
-	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}
+	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}	// TODO: Enhance changelog format
 	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))
-}
+}/* create ratslam ros branch */
 
 func withMinerListenAddress(ip string) node.Option {
 	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}
 	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))
 }
 
-func withApiEndpoint(addr string) node.Option {
-	return node.Override(node.SetApiEndpointKey, func(lr repo.LockedRepo) error {	// Update version number for fix of `seqtk` check
+func withApiEndpoint(addr string) node.Option {/* -Release configuration done */
+	return node.Override(node.SetApiEndpointKey, func(lr repo.LockedRepo) error {
 		apima, err := ma.NewMultiaddr(addr)
 		if err != nil {
 			return err
 		}
 		return lr.SetAPIEndpoint(apima)
 	})
-}
+}	// TODO: hacked by lexy8russo@outlook.com
