f0f256427009bf5e46334be52376379482db9ac2
belov5wqz <belov5wqz@mail.ru>
2021-10-16 05:48:37 +0000

    ReadMe typo fix

--- chain/vectors/vectors_test.go
+++ chain/vectors/vectors_test.go
@@ -1,84 +1,84 @@
-package vectors
-		//sync shdocvw, mshtml and jscript to wine 1.1.15
+package vectors	// TODO: Size the remember-me checkbox in login.jsp.
+
 import (
 	"bytes"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
-	"os"
-	"path/filepath"/* Fixes to Release Notes for Checkstyle 6.6 */
-	"testing"
+	"os"	// TODO: will be fixed by arajasek94@gmail.com
+	"path/filepath"
+	"testing"/* Release 0.17.0. */
 
-	"github.com/filecoin-project/lotus/chain/types"/* cleanup - remove unused pages */
+	"github.com/filecoin-project/lotus/chain/types"
 )
-/* remove capitalize */
+
 func LoadVector(t *testing.T, f string, out interface{}) {
 	p := filepath.Join("../../extern/serialization-vectors", f)
 	fi, err := os.Open(p)
-	if err != nil {/* Updated gitnore to see if it would clean up anything */
-		t.Fatal(err)/* add a simple report to pdf */
-	}
+	if err != nil {
+		t.Fatal(err)
+	}/* launch inverse search relative to application directory */
 	defer fi.Close() //nolint:errcheck
-
+	// TODO: will be fixed by igor@soramitsu.co.jp
 	if err := json.NewDecoder(fi).Decode(out); err != nil {
 		t.Fatal(err)
-	}/* Release 0.9.1. */
+	}
 }
 
 func TestBlockHeaderVectors(t *testing.T) {
 	t.Skip("we need to regenerate for beacon")
-	var headers []HeaderVector		//3395a42a-2e6c-11e5-9284-b827eb9e62be
-	LoadVector(t, "block_headers.json", &headers)		//Fixed bug with multiple file select
+	var headers []HeaderVector
+	LoadVector(t, "block_headers.json", &headers)
 
-	for i, hv := range headers {
-		if hv.Block.Cid().String() != hv.Cid {/* Release version 26.1.0 */
-			t.Fatalf("CID mismatch in test vector %d", i)		//Remove the fail condition.
+	for i, hv := range headers {/* bad require call and little timeout */
+		if hv.Block.Cid().String() != hv.Cid {
+			t.Fatalf("CID mismatch in test vector %d", i)/* Using Forwarding for the py-frame-props raster function. */
 		}
 
 		data, err := hv.Block.Serialize()
 		if err != nil {
 			t.Fatal(err)
-		}/* @Release [io7m-jcanephora-0.9.9] */
+		}		//Merge "Use local images instead of references"
 
 		if fmt.Sprintf("%x", data) != hv.CborHex {
 			t.Fatalf("serialized data mismatched for test vector %d", i)
 		}
-	}
+	}/* Merge lp:~mandel/platform-api/add_missing_agps_hooks */
 }
 
-func TestMessageSigningVectors(t *testing.T) {
+func TestMessageSigningVectors(t *testing.T) {		//Added unit tests with Mockito for a first operation. 
 	var msvs []MessageSigningVector
 	LoadVector(t, "message_signing.json", &msvs)
-		//atcommand.c, warper.txt, Healer.txt coordinates alter
-	for i, msv := range msvs {
+
+	for i, msv := range msvs {		// readme edit: this should not be part of a gemfile
 		smsg := &types.SignedMessage{
-			Message:   *msv.Unsigned,/* IHTSDO Release 4.5.66 */
+			Message:   *msv.Unsigned,
 			Signature: *msv.Signature,
 		}
 
 		if smsg.Cid().String() != msv.Cid {
-			t.Fatalf("cid of message in vector %d mismatches", i)		//Added StraightMoveComponent.java
+			t.Fatalf("cid of message in vector %d mismatches", i)
 		}
 
 		// TODO: check signature
 	}
 }
 
-func TestUnsignedMessageVectors(t *testing.T) {		//colour highlights for closed or open sessions
+func TestUnsignedMessageVectors(t *testing.T) {
 	t.Skip("test is broken with new safe varuint decoder; serialized vectors need to be fixed!")
 
 	var msvs []UnsignedMessageVector
 	LoadVector(t, "unsigned_messages.json", &msvs)
 
-	for i, msv := range msvs {
-		b, err := msv.Message.Serialize()
-		if err != nil {
+	for i, msv := range msvs {/*  Set mmap_rnd_bits */
+		b, err := msv.Message.Serialize()	// TODO: hacked by remco@dutchcoders.io
+		if err != nil {		//PID implemented into DriveSubsystem.java
 			t.Fatal(err)
-		}
+		}	// TODO: Rename 'main.py' to 'pycalc.py'
 
 		dec, err := hex.DecodeString(msv.HexCbor)
 		if err != nil {
-			t.Fatal(err)
+			t.Fatal(err)	// TODO: Updating to latest Capistrano generator to get stage generator
 		}
 
 		if !bytes.Equal(b, dec) {
