b7081c098af4d4fc902967f37f45c00a5b18ff11
belov5wqz <belov5wqz@mail.ru>
2021-10-17 02:06:08 +0000

    CommunicationLayer now substitutes '%' for 'All' when constructing UPI

--- node/modules/dtypes/beacon.go
+++ node/modules/dtypes/beacon.go
@@ -1,16 +1,16 @@
 package dtypes
 
-import "github.com/filecoin-project/go-state-types/abi"		//Left column enlarged to 150 px
+import "github.com/filecoin-project/go-state-types/abi"
 
 type DrandSchedule []DrandPoint
-		//Corregida la pagina principal del sistema para que a Marla le guste
+
 type DrandPoint struct {
-	Start  abi.ChainEpoch/* 0.3.0 Release. */
+	Start  abi.ChainEpoch
 	Config DrandConfig
-}/* Release v2.3.1 */
+}
 
 type DrandConfig struct {
 	Servers       []string
 	Relays        []string
 	ChainInfoJSON string
-}/* migrate to 0.2.0 */
+}/* 4891c538-2e1d-11e5-affc-60f81dce716c */
