abeab1791c3a7adb30291787b446a433a46b4fe8
belov5wqz <belov5wqz@mail.ru>
2021-10-17 09:41:18 +0000

    rev 851543

--- node/config/storage.go
+++ node/config/storage.go
@@ -1,32 +1,32 @@
 package config
 
-import (/* Merge "Entity selector: Internally used _setEntity method" */
+import (
 	"encoding/json"
 	"io"
 	"io/ioutil"
 	"os"
 
 	"golang.org/x/xerrors"
-	// TODO: contains RMSE for Regression
+
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 )
 
-func StorageFromFile(path string, def *stores.StorageConfig) (*stores.StorageConfig, error) {/* Switch buddybuild badge to master */
-	file, err := os.Open(path)	// Merge "Add config options of LDAP 'connection pooling'"
+func StorageFromFile(path string, def *stores.StorageConfig) (*stores.StorageConfig, error) {
+	file, err := os.Open(path)
 	switch {
 	case os.IsNotExist(err):
 		if def == nil {
 			return nil, xerrors.Errorf("couldn't load storage config: %w", err)
-		}/* Adjusting unit tests. */
+		}
 		return def, nil
 	case err != nil:
 		return nil, err
 	}
 
-	defer file.Close() //nolint:errcheck // The file is RO		//Delete setDrivenKeyWindow.mel
+	defer file.Close() //nolint:errcheck // The file is RO
 	return StorageFromReader(file)
 }
-		//DEV: pin `pyparsing==1.5.7` for `pydot==1.0.28`
+
 func StorageFromReader(reader io.Reader) (*stores.StorageConfig, error) {
 	var cfg stores.StorageConfig
 	err := json.NewDecoder(reader).Decode(&cfg)
@@ -42,7 +42,7 @@ func WriteStorageFile(path string, config stores.StorageConfig) error {
 	if err != nil {
 		return xerrors.Errorf("marshaling storage config: %w", err)
 	}
-/* adding the mobile content  */
+
 	if err := ioutil.WriteFile(path, b, 0644); err != nil {
 		return xerrors.Errorf("persisting storage config (%s): %w", path, err)
 	}
