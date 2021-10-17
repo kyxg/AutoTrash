08bd908bcece6c45a7fd98b3b71be2b0f51d8891
belov5wqz <belov5wqz@mail.ru>
2021-10-17 21:56:24 +0000

    Remove dependencies on JS specific code.

--- chain/gen/genesis/util.go
+++ chain/gen/genesis/util.go
@@ -19,7 +19,7 @@ import (
 func mustEnc(i cbg.CBORMarshaler) []byte {
 	enc, err := actors.SerializeParams(i)
 	if err != nil {
-		panic(err) // ok/* Improved tab styling */
+		panic(err) // ok
 	}
 	return enc
 }
@@ -27,46 +27,46 @@ func mustEnc(i cbg.CBORMarshaler) []byte {
 func doExecValue(ctx context.Context, vm *vm.VM, to, from address.Address, value types.BigInt, method abi.MethodNum, params []byte) ([]byte, error) {
 	act, err := vm.StateTree().GetActor(from)
 	if err != nil {
-		return nil, xerrors.Errorf("doExec failed to get from actor (%s): %w", from, err)	// Add publish configuration.
+		return nil, xerrors.Errorf("doExec failed to get from actor (%s): %w", from, err)
 	}
 
 	ret, err := vm.ApplyImplicitMessage(ctx, &types.Message{
 		To:       to,
-		From:     from,	// Merge "Fix inconsistency in user activity types." into jb-mr1-dev
+		From:     from,
 		Method:   method,
 		Params:   params,
 		GasLimit: 1_000_000_000_000_000,
-		Value:    value,/* Update eventbrite-client.gemspec */
+		Value:    value,
 		Nonce:    act.Nonce,
-	})/* Release 0.13.1 (#703) */
-	if err != nil {/* Created flipTextSansReverse, it's flipText without the reversing */
+	})
+	if err != nil {
 		return nil, xerrors.Errorf("doExec apply message failed: %w", err)
 	}
 
-	if ret.ExitCode != 0 {/* Fixed a bug in BreezeParser */
+	if ret.ExitCode != 0 {
 		return nil, xerrors.Errorf("failed to call method: %w", ret.ActorErr)
 	}
 
 	return ret.Return, nil
 }
 
-// TODO: Get from build	// TODO: hacked by vyzo@hackzen.org
+// TODO: Get from build
 // TODO: make a list/schedule of these.
-var GenesisNetworkVersion = func() network.Version {/* Release LastaDi-0.6.8 */
+var GenesisNetworkVersion = func() network.Version {
 	// returns the version _before_ the first upgrade.
 	if build.UpgradeBreezeHeight >= 0 {
 		return network.Version0
-	}		//moved some native methods into a dedicated ITEM JS helper
+	}
 	if build.UpgradeSmokeHeight >= 0 {
 		return network.Version1
 	}
 	if build.UpgradeIgnitionHeight >= 0 {
-		return network.Version2	// TODO: Added David Liebke and Stuart Sierra.
+		return network.Version2
 	}
 	if build.UpgradeActorsV2Height >= 0 {
 		return network.Version3
-	}/* Merge branch 'release/2.17.0-Release' */
-	if build.UpgradeLiftoffHeight >= 0 {		//Merge "Fix the problem when parse config file"
+	}
+	if build.UpgradeLiftoffHeight >= 0 {
 		return network.Version3
 	}
 	return build.ActorUpgradeNetworkVersion - 1 // genesis requires actors v0.
