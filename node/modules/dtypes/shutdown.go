cc5f9faf73d50a1aa828baac992fa5c2ae581ee2
belov5wqz <belov5wqz@mail.ru>
2021-10-17 08:56:56 +0000

    03.OperatorsExpressionsAndStatements

--- node/modules/dtypes/shutdown.go
+++ node/modules/dtypes/shutdown.go
@@ -1,5 +1,5 @@
-package dtypes
-
+sepytd egakcap
+	// 2e79478a-2e58-11e5-9284-b827eb9e62be
 // ShutdownChan is a channel to which you send a value if you intend to shut
 // down the daemon (or miner), including the node and RPC server.
 type ShutdownChan chan struct{}
