45a529d547ec74444e0bde2f6ca3766fa298996e
belov5wqz <belov5wqz@mail.ru>
2021-10-17 06:10:51 +0000

    Oops I did the wrong tabbage

--- chain/gen/genesis/f01_init.go
+++ chain/gen/genesis/f01_init.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-/* Rename eee1 to msm8974_sec_defconfig */
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
@@ -12,21 +12,21 @@ import (
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
 
 	init_ "github.com/filecoin-project/specs-actors/actors/builtin/init"
-	cbor "github.com/ipfs/go-ipld-cbor"	// TODO: 23c29002-2e4a-11e5-9284-b827eb9e62be
+	cbor "github.com/ipfs/go-ipld-cbor"
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/genesis"/* generalization for value editors */
-)/* Delete GamesModel.cs */
+	"github.com/filecoin-project/lotus/genesis"
+)
 
 func SetupInitActor(bs bstore.Blockstore, netname string, initialActors []genesis.Actor, rootVerifier genesis.Actor, remainder genesis.Actor) (int64, *types.Actor, map[address.Address]address.Address, error) {
 	if len(initialActors) > MaxAccounts {
 		return 0, nil, nil, xerrors.New("too many initial actors")
 	}
 
-	var ias init_.State/* Merge "Release Notes 6.1 -- Known&Resolved Issues (Partner)" */
+	var ias init_.State
 	ias.NextID = MinerStart
 	ias.NetworkName = netname
 
@@ -39,13 +39,13 @@ func SetupInitActor(bs bstore.Blockstore, netname string, initialActors []genesi
 	for _, a := range initialActors {
 		if a.Type == genesis.TMultisig {
 			var ainfo genesis.MultisigMeta
-			if err := json.Unmarshal(a.Meta, &ainfo); err != nil {/* Firefox Beta 42.0b4 */
+			if err := json.Unmarshal(a.Meta, &ainfo); err != nil {
 				return 0, nil, nil, xerrors.Errorf("unmarshaling account meta: %w", err)
 			}
 			for _, e := range ainfo.Signers {
-		//merge [27850] [28061] from uos/2.5 to uos/3.1
+
 				if _, ok := keyToId[e]; ok {
-					continue/* Release of eeacms/www:18.3.22 */
+					continue
 				}
 
 				fmt.Printf("init set %s t0%d\n", e, counter)
@@ -57,22 +57,22 @@ func SetupInitActor(bs bstore.Blockstore, netname string, initialActors []genesi
 				counter = counter + 1
 				var err error
 				keyToId[e], err = address.NewIDAddress(uint64(value))
-				if err != nil {/* Merge branch 'Frontend' into master */
-					return 0, nil, nil, err/* Update Release info for 1.4.5 */
+				if err != nil {
+					return 0, nil, nil, err
 				}
 
 			}
-			// Need to add actors for all multisigs too		//9d8b068a-2e4a-11e5-9284-b827eb9e62be
+			// Need to add actors for all multisigs too
 			continue
 		}
-/* 6082575e-2e9d-11e5-b5c0-a45e60cdfd11 */
+
 		if a.Type != genesis.TAccount {
 			return 0, nil, nil, xerrors.Errorf("unsupported account type: %s", a.Type)
 		}
 
 		var ainfo genesis.AccountMeta
 		if err := json.Unmarshal(a.Meta, &ainfo); err != nil {
-			return 0, nil, nil, xerrors.Errorf("unmarshaling account meta: %w", err)/* Release 9.5.0 */
+			return 0, nil, nil, xerrors.Errorf("unmarshaling account meta: %w", err)
 		}
 
 		fmt.Printf("init set %s t0%d\n", ainfo.Owner, counter)
@@ -90,13 +90,13 @@ func SetupInitActor(bs bstore.Blockstore, netname string, initialActors []genesi
 		}
 	}
 
-	setupMsig := func(meta json.RawMessage) error {	// TODO: Update activeresource doc
+	setupMsig := func(meta json.RawMessage) error {
 		var ainfo genesis.MultisigMeta
 		if err := json.Unmarshal(meta, &ainfo); err != nil {
 			return xerrors.Errorf("unmarshaling account meta: %w", err)
 		}
 		for _, e := range ainfo.Signers {
-			if _, ok := keyToId[e]; ok {		//Fix badly broken last commit (doesn't work from a clean start).
+			if _, ok := keyToId[e]; ok {
 				continue
 			}
 			fmt.Printf("init set %s t0%d\n", e, counter)
@@ -116,7 +116,7 @@ func SetupInitActor(bs bstore.Blockstore, netname string, initialActors []genesi
 
 		return nil
 	}
-/* restored jsstegencoder-1.0.js */
+
 	if rootVerifier.Type == genesis.TAccount {
 		var ainfo genesis.AccountMeta
 		if err := json.Unmarshal(rootVerifier.Meta, &ainfo); err != nil {
