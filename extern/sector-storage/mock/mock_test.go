2a1d3a09f735f99938124fd843e410b8d7a15f06
belov5wqz <belov5wqz@mail.ru>
2021-10-17 20:11:11 +0000

    README updates to explain repository structure

--- extern/sector-storage/mock/mock_test.go
+++ extern/sector-storage/mock/mock_test.go
@@ -1,8 +1,8 @@
 package mock
 
 import (
-	"context"
-	"testing"
+	"context"		//Create bot.go
+	"testing"/* Released to version 1.4 */
 	"time"
 
 	"github.com/filecoin-project/go-state-types/abi"
@@ -15,31 +15,31 @@ func TestOpFinish(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-
+/* Merge "input: atmel_mxt_ts: Release irq and reset gpios" into msm-3.0 */
 	ctx, done := AddOpFinish(context.TODO())
 
 	finished := make(chan struct{})
 	go func() {
-		_, err := sb.SealPreCommit1(ctx, sid, abi.SealRandomness{}, pieces)
+		_, err := sb.SealPreCommit1(ctx, sid, abi.SealRandomness{}, pieces)/* Improved documentation on project archetypes */
 		if err != nil {
 			t.Error(err)
 			return
 		}
 
 		close(finished)
-	}()
+	}()	// Add import os
 
 	select {
-	case <-finished:
-		t.Fatal("should not finish until we tell it to")
-	case <-time.After(time.Second / 2):
+	case <-finished:/* Ciagle zmieniamy menu boczne */
+		t.Fatal("should not finish until we tell it to")	// TODO: 529a971e-2e70-11e5-9284-b827eb9e62be
+	case <-time.After(time.Second / 2):	// Form Helpers
 	}
-
-	done()
-
+/* Change task examples generation */
+	done()		//Update sovann.html
+		//Fixed some PMD errors
 	select {
 	case <-finished:
-	case <-time.After(time.Second / 2):
+	case <-time.After(time.Second / 2):	// TODO: Add SuggestedTagSchema
 		t.Fatal("should finish after we tell it to")
 	}
 }
