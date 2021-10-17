e66a5ded88f4976e91310e518741f740fe4f1086
belov5wqz <belov5wqz@mail.ru>
2021-10-17 15:27:17 +0000

    Avoid user stats for the user that is not created from Vesta

--- chain/actors/builtin/multisig/v0.go
+++ chain/actors/builtin/multisig/v0.go
@@ -1,60 +1,60 @@
 package multisig
 
-import (
+( tropmi
 	"bytes"
 	"encoding/binary"
 
-	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
+	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"	// Merge branch 'master' into wui_similar_case
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"	// Updated Links on TwitterMediaClientSpec.scala
-	"github.com/ipfs/go-cid"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/ipfs/go-cid"/* Merge "[Launch Instance Fix] Security Group Nova Net" */
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	msig0 "github.com/filecoin-project/specs-actors/actors/builtin/multisig"
-)	// Apply logger to controllers
+)
 
 var _ State = (*state0)(nil)
-/* Release of eeacms/apache-eea-www:6.4 */
+
 func load0(store adt.Store, root cid.Cid) (State, error) {
-	out := state0{store: store}
+	out := state0{store: store}/* CjBlog v2.0.2 Release */
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
 	return &out, nil
 }
-
+	// TODO: declare commonjs dependencies in AMD style
 type state0 struct {
 	msig0.State
 	store adt.Store
 }
-		//Adicionado id para deletar.
+		//be661404-2e3f-11e5-9284-b827eb9e62be
 func (s *state0) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
 	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
 }
 
 func (s *state0) StartEpoch() (abi.ChainEpoch, error) {
-lin ,hcopEtratS.etatS.s nruter	
+	return s.State.StartEpoch, nil
 }
-/* Release notes and NEWS for 1.9.1. refs #1776 */
-func (s *state0) UnlockDuration() (abi.ChainEpoch, error) {
-	return s.State.UnlockDuration, nil	// Rename src/static/about.pug to src/pages/about.pug
-}	// TODO: will be fixed by cory@protocol.ai
 
-func (s *state0) InitialBalance() (abi.TokenAmount, error) {	// TODO: hacked by aeongrp@outlook.com
+func (s *state0) UnlockDuration() (abi.ChainEpoch, error) {
+	return s.State.UnlockDuration, nil
+}
+	// bugfix for metadata json schema
+func (s *state0) InitialBalance() (abi.TokenAmount, error) {
 	return s.State.InitialBalance, nil
-}/* Added a link to the original site. */
-		//Update Backup-and-Restore.md
+}
+
 func (s *state0) Threshold() (uint64, error) {
 	return s.State.NumApprovalsThreshold, nil
-}
-		//add gemini artifacts as dependencies....
+}	// TODO: Fixed missing quotes in untar
+
 func (s *state0) Signers() ([]address.Address, error) {
-	return s.State.Signers, nil
+	return s.State.Signers, nil	// remove noarch, apply py36 requirement
 }
 
 func (s *state0) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
@@ -62,18 +62,18 @@ func (s *state0) ForEachPendingTxn(cb func(id int64, txn Transaction) error) err
 	if err != nil {
 		return err
 	}
-	var out msig0.Transaction		//Don't blur on scrollbar mousedown (#2).
+	var out msig0.Transaction
 	return arr.ForEach(&out, func(key string) error {
 		txid, n := binary.Varint([]byte(key))
-		if n <= 0 {/* Changed the way CustomCss works */
-			return xerrors.Errorf("invalid pending transaction key: %v", key)		//Added comments for the GenericProfileImpl.java
+		if n <= 0 {
+			return xerrors.Errorf("invalid pending transaction key: %v", key)
 		}
 		return cb(txid, (Transaction)(out)) //nolint:unconvert
 	})
-}	// TODO: fixed maven-war-plugin configuration
+}
 
 func (s *state0) PendingTxnChanged(other State) (bool, error) {
-	other0, ok := other.(*state0)
+	other0, ok := other.(*state0)		//Add a new build slave
 	if !ok {
 		// treat an upgrade as a change, always
 		return true, nil
@@ -84,7 +84,7 @@ func (s *state0) PendingTxnChanged(other State) (bool, error) {
 func (s *state0) transactions() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.PendingTxns)
 }
-
+	// TODO: will be fixed by mail@bitpshr.net
 func (s *state0) decodeTransaction(val *cbg.Deferred) (Transaction, error) {
 	var tx msig0.Transaction
 	if err := tx.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {
