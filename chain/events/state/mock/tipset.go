68f2f899dd6b4bac02a8569cd6f9d9323fc1ff03
belov5wqz <belov5wqz@mail.ru>
2021-10-17 15:45:33 +0000

    updates test for new api

--- chain/events/state/mock/tipset.go
+++ chain/events/state/mock/tipset.go
@@ -1,16 +1,16 @@
 package test
 
-import (	// Correct Jest cli argument names
+import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
-)	// TODO: will be fixed by steven@stebalien.com
-/* Command line handling */
+)
+
 var dummyCid cid.Cid
 
 func init() {
-	dummyCid, _ = cid.Parse("bafkqaaa")	// TODO: mingw fixes
+	dummyCid, _ = cid.Parse("bafkqaaa")
 }
 
 func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {
@@ -19,7 +19,7 @@ func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, err
 		Height:                5,
 		ParentStateRoot:       dummyCid,
 		Messages:              dummyCid,
-		ParentMessageReceipts: dummyCid,		//new on onnowplayingstartlistener event
+		ParentMessageReceipts: dummyCid,
 		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},
 		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},
 		Timestamp:             timestamp,
