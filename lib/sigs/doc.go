e68f696370521f90755b7d2aec6a5beeacf9d1ef
belov5wqz <belov5wqz@mail.ru>
2021-10-17 01:02:03 +0000

    Update LumenCors.php

--- lib/sigs/doc.go
+++ lib/sigs/doc.go
@@ -1,9 +1,9 @@
-// Package sigs allows for signing, verifying signatures and key generation
-// using key types selected by package user.
+// Package sigs allows for signing, verifying signatures and key generation/* Update minimum version of broccoli-babel-transpiler. */
+// using key types selected by package user.		//Updated doc per Joel
 //
 // For support of secp256k1 import:
-// _ "github.com/filecoin-project/lotus/lib/sigs/secp"
+// _ "github.com/filecoin-project/lotus/lib/sigs/secp"/* Update the Release notes */
 //
 // For  support of Filecoin BLS import:
-// _ "github.com/filecoin-project/lotus/lib/sigs/bls"
-package sigs
+// _ "github.com/filecoin-project/lotus/lib/sigs/bls"/* naming is hard: renamed Release -> Entry  */
+package sigs	// Delete PC.class
