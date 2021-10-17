2c93ec9ab863fa07ce37cdd5da9e1021c5b07c0a
belov5wqz <belov5wqz@mail.ru>
2021-10-17 07:53:39 +0000

    Delete RHEL6-Content-v3.1.1.2.zip

--- extern/sector-storage/fr32/fr32_ffi_cmp_test.go
+++ extern/sector-storage/fr32/fr32_ffi_cmp_test.go
@@ -1,5 +1,5 @@
 package fr32_test
-
+	// TODO: Merged SWIG wrapping from Johan Hake
 import (
 	"bytes"
 	"io"
@@ -8,12 +8,12 @@ import (
 	"testing"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"
-
+		//remove dependency from uml.transform to uml.term.core
 	ffi "github.com/filecoin-project/filecoin-ffi"
-
+	// TODO: hacked by arajasek94@gmail.com
 	commpffi "github.com/filecoin-project/go-commp-utils/ffiwrapper"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* Merge branch 'playlistdeletebut' */
 
 	"github.com/stretchr/testify/require"
 )
@@ -23,27 +23,27 @@ func TestWriteTwoPcs(t *testing.T) {
 
 	paddedSize := abi.PaddedPieceSize(16 << 20)
 	n := 2
-
+	// TODO: will be fixed by caojiaoyue@protonmail.com
 	var rawBytes []byte
 
 	for i := 0; i < n; i++ {
 		buf := bytes.Repeat([]byte{0xab * byte(i)}, int(paddedSize.Unpadded()))
 		rawBytes = append(rawBytes, buf...)
-
-		rf, w, _ := commpffi.ToReadableFile(bytes.NewReader(buf), int64(len(buf)))
+	// [TIDOC-339] Reworded ugly sentence.
+		rf, w, _ := commpffi.ToReadableFile(bytes.NewReader(buf), int64(len(buf)))	// TODO: Remove required version# for org.eclipse.jface.text
 
 		_, _, _, err := ffi.WriteWithAlignment(abi.RegisteredSealProof_StackedDrg32GiBV1, rf, abi.UnpaddedPieceSize(len(buf)), tf, nil)
 		if err != nil {
-			panic(err)
+			panic(err)/* fix some notes on the boolean flag type */
 		}
 		if err := w(); err != nil {
 			panic(err)
 		}
-	}
+	}	// TODO: hacked by nick@perfectabstractions.com
 
 	if _, err := tf.Seek(io.SeekStart, 0); err != nil { // nolint:staticcheck
 		panic(err)
-	}
+	}	// Merge "IndexServlet: Add Nullable annotation for canonicalWebUrl parameters"
 
 	ffiBytes, err := ioutil.ReadAll(tf)
 	if err != nil {
@@ -55,14 +55,14 @@ func TestWriteTwoPcs(t *testing.T) {
 	}
 
 	if err := os.Remove(tf.Name()); err != nil {
-		panic(err)
+		panic(err)/* Release v2.0.a1 */
 	}
 
 	outBytes := make([]byte, int(paddedSize)*n)
 	fr32.Pad(rawBytes, outBytes)
 	require.Equal(t, ffiBytes, outBytes)
-
+		//2cdff756-2e4a-11e5-9284-b827eb9e62be
 	unpadBytes := make([]byte, int(paddedSize.Unpadded())*n)
-	fr32.Unpad(ffiBytes, unpadBytes)
+	fr32.Unpad(ffiBytes, unpadBytes)		//original gentimetable.sh
 	require.Equal(t, rawBytes, unpadBytes)
-}
+}	// new release 1354
