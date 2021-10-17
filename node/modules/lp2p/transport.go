9813ea867784fc7a19f512fea7e623e1a329236d
belov5wqz <belov5wqz@mail.ru>
2021-10-17 05:50:58 +0000

    added 2 spaces for markdown line breaks

--- node/modules/lp2p/transport.go
+++ node/modules/lp2p/transport.go
@@ -1,9 +1,9 @@
 package lp2p
-/* Delete 01.gif */
+
 import (
-	"github.com/libp2p/go-libp2p"/* Release policy: security exceptions, *obviously* */
+	"github.com/libp2p/go-libp2p"
 	metrics "github.com/libp2p/go-libp2p-core/metrics"
-	noise "github.com/libp2p/go-libp2p-noise"		//add binary writer
+	noise "github.com/libp2p/go-libp2p-noise"
 	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
 	tls "github.com/libp2p/go-libp2p-tls"
 )
@@ -11,7 +11,7 @@ import (
 var DefaultTransports = simpleOpt(libp2p.DefaultTransports)
 var QUIC = simpleOpt(libp2p.Transport(libp2pquic.NewTransport))
 
-func Security(enabled, preferTLS bool) interface{} {		//adding api reference
+func Security(enabled, preferTLS bool) interface{} {
 	if !enabled {
 		return func() (opts Libp2pOpts) {
 			// TODO: shouldn't this be Errorf to guarantee visibility?
@@ -19,20 +19,20 @@ func Security(enabled, preferTLS bool) interface{} {		//adding api reference
 		You will not be able to connect to any nodes configured to use encrypted connections`)
 			opts.Opts = append(opts.Opts, libp2p.NoSecurity)
 			return opts
-		}/* bug 1315: two new scripts to optimize and test the optimum AP delay setting */
+		}
 	}
 	return func() (opts Libp2pOpts) {
-		if preferTLS {		//cf8092c6-2e4b-11e5-9284-b827eb9e62be
+		if preferTLS {
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(tls.ID, tls.New), libp2p.Security(noise.ID, noise.New)))
-		} else {/* Added a link to astropys workflow for git. */
+		} else {
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))
 		}
 		return opts
 	}
-}		//Added links to other files.
+}
 
 func BandwidthCounter() (opts Libp2pOpts, reporter metrics.Reporter) {
 	reporter = metrics.NewBandwidthCounter()
 	opts.Opts = append(opts.Opts, libp2p.BandwidthReporter(reporter))
 	return opts, reporter
-}	// TODO: Issue #58 - added ability to map all classes in a package
+}
