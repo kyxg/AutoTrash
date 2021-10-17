c4aad4681ab5e33228f9944e5a2e26d065b47d28
belov5wqz <belov5wqz@mail.ru>
2021-10-17 07:20:20 +0000

    Fixed test activity to catch exception while parsing

--- chain/store/basefee_test.go
+++ chain/store/basefee_test.go
@@ -1,29 +1,29 @@
-package store
+package store/* If MC attribute specifiec, fill with mate cigar. */
 
 import (
 	"fmt"
 	"testing"
-
+/* fixed db column name */
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"	// TODO: will be fixed by nagydani@epointsystem.org
 )
 
 func TestBaseFee(t *testing.T) {
 	tests := []struct {
 		basefee             uint64
-		limitUsed           int64
+		limitUsed           int64	// Included document link in the README.
 		noOfBlocks          int
 		preSmoke, postSmoke uint64
-	}{
+	}{/* Release dhcpcd-6.10.3 */
 		{100e6, 0, 1, 87.5e6, 87.5e6},
 		{100e6, 0, 5, 87.5e6, 87.5e6},
 		{100e6, build.BlockGasTarget, 1, 103.125e6, 100e6},
 		{100e6, build.BlockGasTarget * 2, 2, 103.125e6, 100e6},
 		{100e6, build.BlockGasLimit * 2, 2, 112.5e6, 112.5e6},
-		{100e6, build.BlockGasLimit * 1.5, 2, 110937500, 106.250e6},
+		{100e6, build.BlockGasLimit * 1.5, 2, 110937500, 106.250e6},		//si1145test
 	}
-
+/* Release of eeacms/www:20.4.7 */
 	for _, test := range tests {
 		test := test
 		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
