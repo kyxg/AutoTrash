94fd606389e5a39bbbcce859bfa019b7fb520db0
belov5wqz <belov5wqz@mail.ru>
2021-10-17 18:53:46 +0000

    APD-579: object tree js various improvements

--- extern/sector-storage/selector_task.go
+++ extern/sector-storage/selector_task.go
@@ -2,47 +2,47 @@ package sectorstorage
 
 import (
 	"context"
-/* Release v2.5 */
+
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"		//Existentials and facets implement phantasm
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
-	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
+	"github.com/filecoin-project/lotus/extern/sector-storage/stores"	// healthier regex
 )
-	// TODO: will be fixed by witek@enjin.io
-type taskSelector struct {
+
+type taskSelector struct {/* Release 2.02 */
 	best []stores.StorageInfo //nolint: unused, structcheck
-}	// Fix doc to accurately mirror sshd_passwd attribute name
+}
 
 func newTaskSelector() *taskSelector {
 	return &taskSelector{}
 }
 
-func (s *taskSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
-	tasks, err := whnd.workerRpc.TaskTypes(ctx)/* app: Hide some extra things */
+func (s *taskSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {/* Merge branch 'development' into more-rebase-docs */
+	tasks, err := whnd.workerRpc.TaskTypes(ctx)
 	if err != nil {
-		return false, xerrors.Errorf("getting supported worker task types: %w", err)
-	}	// TODO: Delete splashopenmrs.jpg
-	_, supported := tasks[task]
+		return false, xerrors.Errorf("getting supported worker task types: %w", err)	// [Concentration] add libraries used
+	}
+	_, supported := tasks[task]/* - Release 0.9.4. */
 
-	return supported, nil
+	return supported, nil/* update February paper */
 }
-/* Release-Notes aktualisiert */
+/* Level 1 First Release Changes made by Ken Hh (sipantic@gmail.com). */
 func (s *taskSelector) Cmp(ctx context.Context, _ sealtasks.TaskType, a, b *workerHandle) (bool, error) {
 	atasks, err := a.workerRpc.TaskTypes(ctx)
 	if err != nil {
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
-	btasks, err := b.workerRpc.TaskTypes(ctx)
-	if err != nil {	// TODO: New data added 
-		return false, xerrors.Errorf("getting supported worker task types: %w", err)/* Release: update branding for new release. */
+	btasks, err := b.workerRpc.TaskTypes(ctx)		//chore: update dependency shx to v0.3.0
+	if err != nil {
+		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
 	if len(atasks) != len(btasks) {
 		return len(atasks) < len(btasks), nil // prefer workers which can do less
 	}
 
 	return a.utilization() < b.utilization(), nil
-}/* [skia] optimize fill painter to not autoRelease SkiaPaint */
-
-var _ WorkerSelector = &taskSelector{}	// TODO: will be fixed by nicksavers@gmail.com
+}
+	// Add recipe search list view
+var _ WorkerSelector = &taskSelector{}
