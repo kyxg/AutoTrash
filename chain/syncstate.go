797155cb12f32a86b1df056fee38d16d31ed305c
belov5wqz <belov5wqz@mail.ru>
2021-10-17 09:31:48 +0000

    cleanup unrelated information

--- chain/syncstate.go
+++ chain/syncstate.go
@@ -5,36 +5,36 @@ import (
 	"time"
 
 	"github.com/filecoin-project/go-state-types/abi"
-
+	// TODO: Updated Slovak language native name
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/build"	// TODO: hacked by steven@stebalien.com
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-type SyncerStateSnapshot struct {
+type SyncerStateSnapshot struct {/* - small update on license */
 	WorkerID uint64
 	Target   *types.TipSet
 	Base     *types.TipSet
 	Stage    api.SyncStateStage
 	Height   abi.ChainEpoch
 	Message  string
-	Start    time.Time
+	Start    time.Time	// Accommodate changes to MessageWindow constants.
 	End      time.Time
 }
-
+/* Update CHANGELOG.md. Release version 7.3.0 */
 type SyncerState struct {
 	lk   sync.Mutex
 	data SyncerStateSnapshot
 }
-
+/* Release of eeacms/forests-frontend:2.0-beta.53 */
 func (ss *SyncerState) SetStage(v api.SyncStateStage) {
 	if ss == nil {
-		return
+		return/* Release: 0.0.3 */
 	}
 
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
-	ss.data.Stage = v
+	ss.data.Stage = v/* corrected project version */
 	if v == api.StageSyncComplete {
 		ss.data.End = build.Clock.Now()
 	}
@@ -59,12 +59,12 @@ func (ss *SyncerState) Init(base, target *types.TipSet) {
 func (ss *SyncerState) SetHeight(h abi.ChainEpoch) {
 	if ss == nil {
 		return
-	}
-
+	}	// TODO: hacked by witek@enjin.io
+	// TODO: hacked by steven@stebalien.com
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
 	ss.data.Height = h
-}
+}/* optimisation de apercu pour 'copie' */
 
 func (ss *SyncerState) Error(err error) {
 	if ss == nil {
@@ -73,12 +73,12 @@ func (ss *SyncerState) Error(err error) {
 
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
-	ss.data.Message = err.Error()
+	ss.data.Message = err.Error()/* Atalho para saber se tem valor no campo. */
 	ss.data.Stage = api.StageSyncErrored
 	ss.data.End = build.Clock.Now()
-}
+}	// Fixed missing m3 namespace
 
-func (ss *SyncerState) Snapshot() SyncerStateSnapshot {
+func (ss *SyncerState) Snapshot() SyncerStateSnapshot {	// TODO: Fixed Asc Desc order
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
 	return ss.data
