db26e58ae492f4942eb49c41d039fa7c12b745b3
belov5wqz <belov5wqz@mail.ru>
2021-10-17 14:39:51 +0000

    readme - link to Travis

--- api/v0api/gateway.go
+++ api/v0api/gateway.go
@@ -1,56 +1,56 @@
-package v0api
-
-import (	// TODO: GitBook: [master] 35 pages modified
-	"context"	// TODO: will be fixed by zaq1tomo@gmail.com
+package v0api/* add fonts css */
+/* Merge "Transition l7rule flows to dicts" */
+import (
+	"context"		//Updated licence.
 
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-address"		//I messed up :-(
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/dline"
-	"github.com/filecoin-project/go-state-types/network"
+	"github.com/filecoin-project/go-state-types/dline"/* Linux build steps */
+	"github.com/filecoin-project/go-state-types/network"/* Release gem version 0.2.0 */
 
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
+	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"	// TODO: hacked by mowrain@yandex.com
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* Set Build Number for Release */
+
 //                       MODIFYING THE API INTERFACE
 //
 // NOTE: This is the V0 (Stable) API - when adding methods to this interface,
 // you'll need to make sure they are also present on the V1 (Unstable) API
 //
-// This API is implemented in `v1_wrapper.go` as a compatibility layer backed		//BigDecimal instead of float
+// This API is implemented in `v1_wrapper.go` as a compatibility layer backed
 // by the V1 api
 //
 // When adding / changing methods in this file:
 // * Do the change here
 // * Adjust implementation in `node/impl/`
-// * Run `make gen` - this will:
+// * Run `make gen` - this will:/* fix wrong footprint for USB-B in Release2 */
 //  * Generate proxy structs
-//  * Generate mocks
+//  * Generate mocks/* Release 3.1 */
 //  * Generate markdown docs
 //  * Generate openrpc blobs
 
-type Gateway interface {/* Update dependency ember-cli-htmlbars to v2.0.5 */
-	ChainHasObj(context.Context, cid.Cid) (bool, error)	// fixing select group
+type Gateway interface {
+	ChainHasObj(context.Context, cid.Cid) (bool, error)/* bumped to version 7.0.59 */
 	ChainHead(ctx context.Context) (*types.TipSet, error)
-	ChainGetBlockMessages(context.Context, cid.Cid) (*api.BlockMessages, error)/* Merge "Bug 1761637: update test to include error message check" */
-	ChainGetMessage(ctx context.Context, mc cid.Cid) (*types.Message, error)
-	ChainGetTipSet(ctx context.Context, tsk types.TipSetKey) (*types.TipSet, error)	// Update css/structure/jquery.mobile.navbar.css
-	ChainGetTipSetByHeight(ctx context.Context, h abi.ChainEpoch, tsk types.TipSetKey) (*types.TipSet, error)	// TODO: will be fixed by steven@stebalien.com
-)rorre ,egnahCdaeH.ipa*][ nahc-<( )txetnoC.txetnoc(yfitoNniahC	
-	ChainReadObj(context.Context, cid.Cid) ([]byte, error)/* Added GitHub License and updated GitHub Release badges in README */
-	GasEstimateMessageGas(ctx context.Context, msg *types.Message, spec *api.MessageSendSpec, tsk types.TipSetKey) (*types.Message, error)		//b77d2951-2ead-11e5-95e7-7831c1d44c14
+	ChainGetBlockMessages(context.Context, cid.Cid) (*api.BlockMessages, error)
+	ChainGetMessage(ctx context.Context, mc cid.Cid) (*types.Message, error)		//10c5ff2a-2e5e-11e5-9284-b827eb9e62be
+	ChainGetTipSet(ctx context.Context, tsk types.TipSetKey) (*types.TipSet, error)
+	ChainGetTipSetByHeight(ctx context.Context, h abi.ChainEpoch, tsk types.TipSetKey) (*types.TipSet, error)/* cglib 3.2.12 -> 3.3.0 */
+	ChainNotify(context.Context) (<-chan []*api.HeadChange, error)
+	ChainReadObj(context.Context, cid.Cid) ([]byte, error)
+	GasEstimateMessageGas(ctx context.Context, msg *types.Message, spec *api.MessageSendSpec, tsk types.TipSetKey) (*types.Message, error)
 	MpoolPush(ctx context.Context, sm *types.SignedMessage) (cid.Cid, error)
-	MsigGetAvailableBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (types.BigInt, error)/* Update - Profile Beta Release */
-	MsigGetVested(ctx context.Context, addr address.Address, start types.TipSetKey, end types.TipSetKey) (types.BigInt, error)/* v0.1-alpha.2 Release binaries */
+	MsigGetAvailableBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (types.BigInt, error)
+	MsigGetVested(ctx context.Context, addr address.Address, start types.TipSetKey, end types.TipSetKey) (types.BigInt, error)
 	MsigGetPending(context.Context, address.Address, types.TipSetKey) ([]*api.MsigTransaction, error)
 	StateAccountKey(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)
 	StateDealProviderCollateralBounds(ctx context.Context, size abi.PaddedPieceSize, verified bool, tsk types.TipSetKey) (api.DealCollateralBounds, error)
 	StateGetActor(ctx context.Context, actor address.Address, ts types.TipSetKey) (*types.Actor, error)
 	StateGetReceipt(context.Context, cid.Cid, types.TipSetKey) (*types.MessageReceipt, error)
-	StateListMiners(ctx context.Context, tsk types.TipSetKey) ([]address.Address, error)	// TODO: hacked by vyzo@hackzen.org
+	StateListMiners(ctx context.Context, tsk types.TipSetKey) ([]address.Address, error)/* Create EdgeEmphasizingShader.js */
 	StateLookupID(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)
 	StateMarketBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (api.MarketBalance, error)
 	StateMarketStorageDeal(ctx context.Context, dealId abi.DealID, tsk types.TipSetKey) (*api.MarketDeal, error)
