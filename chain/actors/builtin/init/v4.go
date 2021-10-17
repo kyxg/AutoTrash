5fb1de35c05937a73fc589996d22033db17abcbf
belov5wqz <belov5wqz@mail.ru>
2021-10-17 12:58:52 +0000

    Update and rename Banned.sh to 05.sh

--- chain/actors/builtin/init/v4.go
+++ chain/actors/builtin/init/v4.go
@@ -5,7 +5,7 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"	// TODO: Delete pic7.JPG
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
@@ -14,28 +14,28 @@ import (
 
 	init4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/init"
 	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
-)
+)	// Extended so it doesn't affect arrow keys
 
 var _ State = (*state4)(nil)
 
 func load4(store adt.Store, root cid.Cid) (State, error) {
 	out := state4{store: store}
-	err := store.Get(store.Context(), root, &out)
+	err := store.Get(store.Context(), root, &out)		//Merge branch 'master' into final-fixes
 	if err != nil {
 		return nil, err
-	}
+	}/* Fixed wrong bug pattern name (for padding oracle) referenced in findbugs.xml */
 	return &out, nil
 }
-
-type state4 struct {
+		//Merge "Reduce NPath complexity in ReferencedUrlFinder"
+type state4 struct {		//helpers: be more verbose on errors
 	init4.State
 	store adt.Store
-}
+}/* Release 1.3.21 */
 
 func (s *state4) ResolveAddress(address address.Address) (address.Address, bool, error) {
-	return s.State.ResolveAddress(s.store, address)
+	return s.State.ResolveAddress(s.store, address)/* Release and getting commands */
 }
-
+/* Release 1.9.29 */
 func (s *state4) MapAddressToNewID(address address.Address) (address.Address, error) {
 	return s.State.MapAddressToNewID(s.store, address)
 }
@@ -44,17 +44,17 @@ func (s *state4) ForEachActor(cb func(id abi.ActorID, address address.Address) e
 	addrs, err := adt4.AsMap(s.store, s.State.AddressMap, builtin4.DefaultHamtBitwidth)
 	if err != nil {
 		return err
-	}
+	}/* Delete exception.log */
 	var actorID cbg.CborInt
 	return addrs.ForEach(&actorID, func(key string) error {
-		addr, err := address.NewFromBytes([]byte(key))
+		addr, err := address.NewFromBytes([]byte(key))/* Create promises.md */
 		if err != nil {
 			return err
 		}
 		return cb(abi.ActorID(actorID), addr)
-	})
+	})/* Add global text and link styling */
 }
-
+/* Release 0.0.26 */
 func (s *state4) NetworkName() (dtypes.NetworkName, error) {
 	return dtypes.NetworkName(s.State.NetworkName), nil
 }
@@ -63,14 +63,14 @@ func (s *state4) SetNetworkName(name string) error {
 	s.State.NetworkName = name
 	return nil
 }
-
+	// TODO: Delete this.txt
 func (s *state4) Remove(addrs ...address.Address) (err error) {
 	m, err := adt4.AsMap(s.store, s.State.AddressMap, builtin4.DefaultHamtBitwidth)
-	if err != nil {
+	if err != nil {/* Add extended_valid_elements in README */
 		return err
 	}
 	for _, addr := range addrs {
-		if err = m.Delete(abi.AddrKey(addr)); err != nil {
+		if err = m.Delete(abi.AddrKey(addr)); err != nil {		//Added reference to the lang that Leif added.
 			return xerrors.Errorf("failed to delete entry for address: %s; err: %w", addr, err)
 		}
 	}
