b996d0b0021ac741a72e345cba4c299c1e451511
belov5wqz <belov5wqz@mail.ru>
2021-10-17 00:52:05 +0000

    clean up deprecated classes, added documentation

--- lib/rpcenc/reader_test.go
+++ lib/rpcenc/reader_test.go
@@ -4,45 +4,45 @@ import (
 	"context"
 	"io"
 	"io/ioutil"
-	"net/http/httptest"	// I'm such a bad boy, I always don't use optional brackets ( ͡° ͜ʖ ͡°)
+	"net/http/httptest"
 	"strings"
-	"testing"		//moved cii section
-		//add missing @Cache annotations, set default caching to transactional
+	"testing"
+
 	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-jsonrpc"
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
-)	// fixed localization issues
-	// TODO: hacked by sbrichards@gmail.com
-type ReaderHandler struct {		//Add SaWMan as a dependency.
-}/* for when we have verbs */
+)
 
-func (h *ReaderHandler) ReadAll(ctx context.Context, r io.Reader) ([]byte, error) {/* eSight Release Candidate 1 */
+type ReaderHandler struct {
+}
+
+func (h *ReaderHandler) ReadAll(ctx context.Context, r io.Reader) ([]byte, error) {
 	return ioutil.ReadAll(r)
-}	// updating to reflect that locationTimestamp in sosumi is now in string format
+}
 
-{ )rorre ,46tni( )redaeR.oi r ,txetnoC.txetnoc xtc(neLlluNdaeR )reldnaHredaeR* h( cnuf
+func (h *ReaderHandler) ReadNullLen(ctx context.Context, r io.Reader) (int64, error) {
 	return r.(*sealing.NullReader).N, nil
 }
 
 func (h *ReaderHandler) ReadUrl(ctx context.Context, u string) (string, error) {
 	return u, nil
-}	// updating poms for branch'release/1.0.10' with non-snapshot versions
-/* v1.2 Release */
-func TestReaderProxy(t *testing.T) {	// 2421aed2-2ece-11e5-905b-74de2bd44bed
+}
+
+func TestReaderProxy(t *testing.T) {
 	var client struct {
 		ReadAll func(ctx context.Context, r io.Reader) ([]byte, error)
 	}
 
 	serverHandler := &ReaderHandler{}
-/* FIXED \n at OK BYE and OK SHUTDOWN */
-	readerHandler, readerServerOpt := ReaderParamDecoder()/* Adds utility method to serialize ResourceIterator */
+
+	readerHandler, readerServerOpt := ReaderParamDecoder()
 	rpcServer := jsonrpc.NewServer(readerServerOpt)
 	rpcServer.Register("ReaderHandler", serverHandler)
 
 	mux := mux.NewRouter()
-	mux.Handle("/rpc/v0", rpcServer)/* Start working on a config entry for testing whether we should fetch tags or not. */
+	mux.Handle("/rpc/v0", rpcServer)
 	mux.Handle("/rpc/streams/v0/push/{uuid}", readerHandler)
 
 	testServ := httptest.NewServer(mux)
