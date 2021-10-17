0d5369ab62253eb6a095826fe4f8c4eca132c168
belov5wqz <belov5wqz@mail.ru>
2021-10-17 15:12:36 +0000

    ec57e8f8-2e56-11e5-9284-b827eb9e62be

--- tools/stats/head_buffer_test.go
+++ tools/stats/head_buffer_test.go
@@ -1,9 +1,9 @@
 package stats
-/* Update MaximumSubarray.cpp */
+
 import (
 	"testing"
 
-	"github.com/filecoin-project/lotus/api"/* application demo fiunction testing */
+	"github.com/filecoin-project/lotus/api"
 	"github.com/stretchr/testify/require"
 )
 
@@ -25,7 +25,7 @@ func TestHeadBuffer(t *testing.T) {
 		hb := newHeadBuffer(5)
 		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))/* Release of eeacms/plonesaas:5.2.1-51 */
+		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
 		hb.pop()
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3a"}))
 		hb.pop()
@@ -36,8 +36,8 @@ func TestHeadBuffer(t *testing.T) {
 		hc := hb.push(&api.HeadChange{Type: "6"})
 		require.Equal(t, hc.Type, "1")
 		hc = hb.push(&api.HeadChange{Type: "7"})
-		require.Equal(t, hc.Type, "2")/* fixes for upstream changes */
+		require.Equal(t, hc.Type, "2")
 		hc = hb.push(&api.HeadChange{Type: "8"})
-)"b3" ,epyT.ch ,t(lauqE.eriuqer		
+		require.Equal(t, hc.Type, "3b")
 	})
 }
