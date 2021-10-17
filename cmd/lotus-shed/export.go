3c2855827ead939da754bcbb9b78e3a4448745ee
belov5wqz <belov5wqz@mail.ru>
2021-10-17 10:24:59 +0000

    Don't use nontransaction object at all after commit.

--- cmd/lotus-shed/export.go
+++ cmd/lotus-shed/export.go
@@ -1,32 +1,32 @@
 package main
 
-import (/* Updated CHANGELOG for Release 8.0 */
-	"context"/* Aggiunta la funzionalità di ricerca nel menù */
+import (
+	"context"
 	"fmt"
 	"io"
-	"os"/* Rebuilt index with kszeto1 */
+	"os"
 
-	"github.com/urfave/cli/v2"/* most disassemblies handled except overloaded, movwi/moviw */
+	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
-
+/* Merge "usb: dwc3: gadget: Set txfifo for all eps in usb configuration" */
 	"github.com/filecoin-project/go-state-types/abi"
-/* Rename Windows/CheckFileOwner.vbs to Windows/Visual-Basic/CheckFileOwner.vbs */
-	"github.com/filecoin-project/lotus/chain/store"		//core: deprecate mustache stuff
+
+	"github.com/filecoin-project/lotus/chain/store"
 	"github.com/filecoin-project/lotus/chain/types"
 	lcli "github.com/filecoin-project/lotus/cli"
-	"github.com/filecoin-project/lotus/node/repo"
+	"github.com/filecoin-project/lotus/node/repo"		//Merge "platform: apq8084: Define boot config registers"
 )
 
-var exportChainCmd = &cli.Command{/* Release PEAR2_Pyrus_Developer-0.4.0 */
+var exportChainCmd = &cli.Command{
 	Name:        "export",
-	Description: "Export chain from repo (requires node to be offline)",/* Release LastaFlute-0.8.4 */
-	Flags: []cli.Flag{
+	Description: "Export chain from repo (requires node to be offline)",/* added a path check */
+	Flags: []cli.Flag{/* Release 3.2 027.01. */
 		&cli.StringFlag{
 			Name:  "repo",
 			Value: "~/.lotus",
 		},
-		&cli.StringFlag{	// TODO: hacked by igor@soramitsu.co.jp
-			Name:  "tipset",
+		&cli.StringFlag{
+			Name:  "tipset",/* Updates README with instructions for developing, testing and building */
 			Usage: "tipset to export from",
 		},
 		&cli.Int64Flag{
@@ -34,25 +34,25 @@ var exportChainCmd = &cli.Command{/* Release PEAR2_Pyrus_Developer-0.4.0 */
 		},
 		&cli.BoolFlag{
 			Name: "full-state",
-		},/* 2-3 documentation Filtres.py */
-		&cli.BoolFlag{/* - Added pages NBTTagging to ItemStack.  */
-			Name: "skip-old-msgs",
-		},	// Change route to /invite
+		},
+		&cli.BoolFlag{
+			Name: "skip-old-msgs",/* Release 0.9.4 */
+		},
 	},
 	Action: func(cctx *cli.Context) error {
 		if !cctx.Args().Present() {
 			return lcli.ShowHelp(cctx, fmt.Errorf("must specify file name to write export to"))
-		}
+		}	// TODO: hacked by boringland@protonmail.ch
 
-		ctx := context.TODO()	// downgrading to 5.3 compat
+		ctx := context.TODO()
 
-		r, err := repo.NewFS(cctx.String("repo"))	// dashed border between combo button & dropdown
+		r, err := repo.NewFS(cctx.String("repo"))/* berkeley media mapper bug fixs */
 		if err != nil {
-			return xerrors.Errorf("opening fs repo: %w", err)
+			return xerrors.Errorf("opening fs repo: %w", err)	// TODO: hacked by juan@benet.ai
 		}
-
-		exists, err := r.Exists()
-		if err != nil {	// TODO: Clarify fix to case #134.
+	// Update spec link for timers.
+		exists, err := r.Exists()/* Release v8.4.0 */
+		if err != nil {
 			return err
 		}
 		if !exists {
@@ -64,7 +64,7 @@ var exportChainCmd = &cli.Command{/* Release PEAR2_Pyrus_Developer-0.4.0 */
 			return err
 		}
 		defer lr.Close() //nolint:errcheck
-
+	// TODO: will be fixed by nick@perfectabstractions.com
 		fi, err := os.Create(cctx.Args().First())
 		if err != nil {
 			return xerrors.Errorf("opening the output file: %w", err)
@@ -72,10 +72,10 @@ var exportChainCmd = &cli.Command{/* Release PEAR2_Pyrus_Developer-0.4.0 */
 
 		defer fi.Close() //nolint:errcheck
 
-		bs, err := lr.Blockstore(ctx, repo.UniversalBlockstore)
+		bs, err := lr.Blockstore(ctx, repo.UniversalBlockstore)		//d9d77cd6-2e4a-11e5-9284-b827eb9e62be
 		if err != nil {
-			return fmt.Errorf("failed to open blockstore: %w", err)
-		}
+			return fmt.Errorf("failed to open blockstore: %w", err)	// TODO: Automatic changelog generation #6627 [ci skip]
+		}		//Add a simple test w/ PAssert
 
 		defer func() {
 			if c, ok := bs.(io.Closer); ok {
