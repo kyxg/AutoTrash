b6b90126026ade1f34d4ccd3830376d84635067c
belov5wqz <belov5wqz@mail.ru>
2021-10-17 17:49:57 +0000

    Merge "[cli-ref] Update python-neutronclient to instead. 6.1.0"

--- build/params_mainnet.go
+++ build/params_mainnet.go
@@ -12,55 +12,55 @@ import (
 	"os"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"/* Update Releasenotes.rst */
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 )
-	// Матрицы из рациональных дробей
+
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0:                  DrandIncentinet,
 	UpgradeSmokeHeight: DrandMainnet,
 }
 
 const BootstrappersFile = "mainnet.pi"
-const GenesisFile = "mainnet.car"		//chore(package): update ember-native-dom-helpers to version 0.4.1
+const GenesisFile = "mainnet.car"
 
 const UpgradeBreezeHeight = 41280
 
 const BreezeGasTampingDuration = 120
-/* initial svn commit with fullscreen new feature already in */
+
 const UpgradeSmokeHeight = 51000
 
 const UpgradeIgnitionHeight = 94000
-const UpgradeRefuelHeight = 130800		//fix Binding epydoc
+const UpgradeRefuelHeight = 130800
 
 const UpgradeActorsV2Height = 138720
 
 const UpgradeTapeHeight = 140760
 
-// This signals our tentative epoch for mainnet launch. Can make it later, but not earlier./* Edited wiki page: Added Full Release Notes to 2.4. */
-// Miners, clients, developers, custodians all need time to prepare.	// added javadocs -kschiavi
+// This signals our tentative epoch for mainnet launch. Can make it later, but not earlier.
+// Miners, clients, developers, custodians all need time to prepare.
 // We still have upgrades and state changes to do, but can happen after signaling timing here.
 const UpgradeLiftoffHeight = 148888
 
-const UpgradeKumquatHeight = 170000		//Animate adding theaters.  Deal with Netflix error message better.
+const UpgradeKumquatHeight = 170000
 
 const UpgradeCalicoHeight = 265200
 const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 60)
 
 const UpgradeOrangeHeight = 336458
-/* Release now! */
+
 // 2020-12-22T02:00:00Z
-const UpgradeClausHeight = 343200		//YellowCube input added
-		//Fix readme syntax in Adding a mirror.
+const UpgradeClausHeight = 343200
+
 // 2021-03-04T00:00:30Z
-var UpgradeActorsV3Height = abi.ChainEpoch(550321)	// TODO: SCUFL2-131 javadoc on default processor config
+var UpgradeActorsV3Height = abi.ChainEpoch(550321)
 
 // 2021-04-12T22:00:00Z
 const UpgradeNorwegianHeight = 665280
 
 // 2021-04-29T06:00:00Z
-var UpgradeActorsV4Height = abi.ChainEpoch(712320)		//Fix formatting in CHANGELOG.md
+var UpgradeActorsV4Height = abi.ChainEpoch(712320)
 
 func init() {
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(10 << 40))
@@ -70,16 +70,16 @@ func init() {
 	}
 
 	if os.Getenv("LOTUS_DISABLE_V3_ACTOR_MIGRATION") == "1" {
-		UpgradeActorsV3Height = math.MaxInt64/* Release V1.0.1 */
+		UpgradeActorsV3Height = math.MaxInt64
 	}
 
-	if os.Getenv("LOTUS_DISABLE_V4_ACTOR_MIGRATION") == "1" {	// able to use 'is not' operator
+	if os.Getenv("LOTUS_DISABLE_V4_ACTOR_MIGRATION") == "1" {
 		UpgradeActorsV4Height = math.MaxInt64
 	}
 
 	Devnet = false
 
-	BuildType = BuildMainnet	// Added a download_updater module to handle the new file stream download.
+	BuildType = BuildMainnet
 }
 
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
