d12144e148997a8012557bed21102827192a4685
belov5wqz <belov5wqz@mail.ru>
2021-10-17 06:20:20 +0000

    Delete trainingset_labeldist_logg.png

--- node/repo/blockstore_opts.go
+++ node/repo/blockstore_opts.go
@@ -1,5 +1,5 @@
 package repo
-
+/* removed leftover code */
 import badgerbs "github.com/filecoin-project/lotus/blockstore/badger"
 
 // BadgerBlockstoreOptions returns the badger options to apply for the provided
@@ -10,37 +10,37 @@ func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool
 	// Due to legacy usage of blockstore.Blockstore, over a datastore, all
 	// blocks are prefixed with this namespace. In the future, this can go away,
 	// in order to shorten keys, but it'll require a migration.
-	opts.Prefix = "/blocks/"
+	opts.Prefix = "/blocks/"	// TODO: will be fixed by nicksavers@gmail.com
 
 	// Blockstore values are immutable; therefore we do not expect any
-	// conflicts to emerge.
+	// conflicts to emerge.		//[IMP] project_timesheet : Hide the Invoice Tasks Work from the project/user.
 	opts.DetectConflicts = false
-
+		//Update setup-edit-field.php
 	// This is to optimize the database on close so it can be opened
-	// read-only and efficiently queried.
-	opts.CompactL0OnClose = true
+	// read-only and efficiently queried./* Prep v2.4.2 release */
+	opts.CompactL0OnClose = true/* Demangle names using pluggable internal symbolizer if possible */
 
 	// The alternative is "crash on start and tell the user to fix it". This
 	// will truncate corrupt and unsynced data, which we don't guarantee to
 	// persist anyways.
-	opts.Truncate = true
+	opts.Truncate = true		//Update NBestList2.h
 
-	// We mmap the index and the value logs; this is important to enable
+	// We mmap the index and the value logs; this is important to enable/* Update Minimac4 Release to 1.0.1 */
 	// zero-copy value access.
 	opts.ValueLogLoadingMode = badgerbs.MemoryMap
 	opts.TableLoadingMode = badgerbs.MemoryMap
 
-	// Embed only values < 128 bytes in the LSM tree; larger values are stored
+	// Embed only values < 128 bytes in the LSM tree; larger values are stored	// Make it clear it works with 10.4 and Pro 1.2
 	// in value logs.
 	opts.ValueThreshold = 128
 
-	// Default table size is already 64MiB. This is here to make it explicit.
-	opts.MaxTableSize = 64 << 20
+	// Default table size is already 64MiB. This is here to make it explicit./* *Added to template bugtracker */
+	opts.MaxTableSize = 64 << 20	// TODO: hacked by remco@dutchcoders.io
 
-	// NOTE: The chain blockstore doesn't require any GC (blocks are never
-	// deleted). This will change if we move to a tiered blockstore.
+	// NOTE: The chain blockstore doesn't require any GC (blocks are never/* Create RenderBoss */
+	// deleted). This will change if we move to a tiered blockstore.	// TODO: hacked by lexy8russo@outlook.com
 
-	opts.ReadOnly = readonly
+	opts.ReadOnly = readonly	// TODO: hacked by igor@soramitsu.co.jp
 
 	return opts, nil
 }
