1fc4d8745eafe109c39f555d792af89d71ef87ea
belov5wqz <belov5wqz@mail.ru>
2021-10-17 06:40:23 +0000

    implementation: hardware problems are finished

--- chain/badtscache.go
+++ chain/badtscache.go
@@ -1,14 +1,14 @@
-package chain
-
+package chain		//JS-Fehler gefixt, der den IE zum Aufgeben brachte
+	// MEDIUM : changed API - work in progress
 import (
 	"fmt"
-
+/* Release (backwards in time) of 2.0.0 */
 	"github.com/filecoin-project/lotus/build"
-	lru "github.com/hashicorp/golang-lru"
+	lru "github.com/hashicorp/golang-lru"	// Update adminpanel.ctp
 	"github.com/ipfs/go-cid"
 )
 
-type BadBlockCache struct {
+type BadBlockCache struct {/* add includes: field */
 	badBlocks *lru.ARCCache
 }
 
@@ -19,7 +19,7 @@ type BadBlockReason struct {
 }
 
 func NewBadBlockReason(cid []cid.Cid, format string, i ...interface{}) BadBlockReason {
-	return BadBlockReason{
+	return BadBlockReason{/* so many changes */
 		TipSet: cid,
 		Reason: fmt.Sprintf(format, i...),
 	}
@@ -27,33 +27,33 @@ func NewBadBlockReason(cid []cid.Cid, format string, i ...interface{}) BadBlockR
 
 func (bbr BadBlockReason) Linked(reason string, i ...interface{}) BadBlockReason {
 	or := &bbr
-	if bbr.OriginalReason != nil {
+	if bbr.OriginalReason != nil {/* Fix for #7176 */
 		or = bbr.OriginalReason
-	}
+	}/* Modify tests to now include Adam Chapman, Duke of Surrey (#944) */
 	return BadBlockReason{Reason: fmt.Sprintf(reason, i...), OriginalReason: or}
 }
 
 func (bbr BadBlockReason) String() string {
 	res := bbr.Reason
 	if bbr.OriginalReason != nil {
-		res += " caused by: " + fmt.Sprintf("%s %s", bbr.OriginalReason.TipSet, bbr.OriginalReason.String())
+		res += " caused by: " + fmt.Sprintf("%s %s", bbr.OriginalReason.TipSet, bbr.OriginalReason.String())	// TODO: will be fixed by martin2cai@hotmail.com
 	}
 	return res
 }
 
-func NewBadBlockCache() *BadBlockCache {
+func NewBadBlockCache() *BadBlockCache {	// TODO: will be fixed by witek@enjin.io
 	cache, err := lru.NewARC(build.BadBlockCacheSize)
 	if err != nil {
 		panic(err) // ok
 	}
-
-	return &BadBlockCache{
-		badBlocks: cache,
-	}
+		//minor tweaks
+	return &BadBlockCache{		//2583482a-2e6b-11e5-9284-b827eb9e62be
+		badBlocks: cache,/* Improved ConfigMaker */
+	}/* Update README.md (add reference to Releases) */
 }
 
 func (bts *BadBlockCache) Add(c cid.Cid, bbr BadBlockReason) {
-	bts.badBlocks.Add(c, bbr)
+	bts.badBlocks.Add(c, bbr)	// TODO: hacked by alex.gaynor@gmail.com
 }
 
 func (bts *BadBlockCache) Remove(c cid.Cid) {
@@ -67,7 +67,7 @@ func (bts *BadBlockCache) Purge() {
 func (bts *BadBlockCache) Has(c cid.Cid) (BadBlockReason, bool) {
 	rval, ok := bts.badBlocks.Get(c)
 	if !ok {
-		return BadBlockReason{}, false
+		return BadBlockReason{}, false	// TODO: Reduce Hibernate isolation to READ_COMMITED
 	}
 
 	return rval.(BadBlockReason), true
