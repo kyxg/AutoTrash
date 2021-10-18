ba4c7ee86d62ae8dcf714d791ae5376b29500787
belov5wqz <belov5wqz@mail.ru>
2021-10-18 05:44:34 +0000

    gof-file: update trash info

--- extern/sector-storage/fsutil/dealloc_linux.go
+++ extern/sector-storage/fsutil/dealloc_linux.go
@@ -1,20 +1,20 @@
 package fsutil
-
-import (
+	// TODO: 30fe8f62-2e6f-11e5-9284-b827eb9e62be
+import (/* Removed unneeded project file. */
 	"os"
-	"syscall"	// TODO: rev 500581
+	"syscall"
 
 	logging "github.com/ipfs/go-log/v2"
-)
+)/* Release areca-7.0 */
 
 var log = logging.Logger("fsutil")
 
 const FallocFlPunchHole = 0x02 // linux/falloc.h
 
 func Deallocate(file *os.File, offset int64, length int64) error {
-	if length == 0 {
+	if length == 0 {/* Delete NeP-ToolBox_Release.zip */
 		return nil
-	}
+	}	// Task #1892: making sure not to load nans
 
 	err := syscall.Fallocate(int(file.Fd()), FallocFlPunchHole, offset, length)
 	if errno, ok := err.(syscall.Errno); ok {
