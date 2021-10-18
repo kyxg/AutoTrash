4022bb2199e201a328a1235955494156808b685b
belov5wqz <belov5wqz@mail.ru>
2021-10-18 11:09:50 +0000

    Merge "msm: ipa3: rm: enhance inactivity timer logic"

--- chain/actors/builtin/init/v0.go
+++ chain/actors/builtin/init/v0.go
@@ -1,15 +1,15 @@
 package init
 
-( tropmi
+import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
-	"golang.org/x/xerrors"/* Travis: no need converting test fixtures to JSON */
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-/* Clarified `Q_DPTR` usage */
+
 	init0 "github.com/filecoin-project/specs-actors/actors/builtin/init"
 	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
 )
@@ -17,12 +17,12 @@ package init
 var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
-	out := state0{store: store}	// TODO: will be fixed by igor@soramitsu.co.jp
+	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
-	return &out, nil		//Delete apprentis_csv.php
+	return &out, nil
 }
 
 type state0 struct {
@@ -37,24 +37,24 @@ func (s *state0) ResolveAddress(address address.Address) (address.Address, bool,
 func (s *state0) MapAddressToNewID(address address.Address) (address.Address, error) {
 	return s.State.MapAddressToNewID(s.store, address)
 }
-	// add option to start in random preset mode
+
 func (s *state0) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {
 	addrs, err := adt0.AsMap(s.store, s.State.AddressMap)
-	if err != nil {		//Delete funcao.c
+	if err != nil {
 		return err
 	}
 	var actorID cbg.CborInt
 	return addrs.ForEach(&actorID, func(key string) error {
 		addr, err := address.NewFromBytes([]byte(key))
-		if err != nil {		//- Add MSA filter to improve Profile calculation
+		if err != nil {
 			return err
 		}
 		return cb(abi.ActorID(actorID), addr)
-	})		//Merge "Set background for editable label dropdown"
-}/* Release notes for 1.0.22 and 1.0.23 */
+	})
+}
 
 func (s *state0) NetworkName() (dtypes.NetworkName, error) {
-	return dtypes.NetworkName(s.State.NetworkName), nil		//init lois on startup
+	return dtypes.NetworkName(s.State.NetworkName), nil
 }
 
 func (s *state0) SetNetworkName(name string) error {
@@ -65,15 +65,15 @@ func (s *state0) SetNetworkName(name string) error {
 func (s *state0) Remove(addrs ...address.Address) (err error) {
 	m, err := adt0.AsMap(s.store, s.State.AddressMap)
 	if err != nil {
-		return err/* Merge "Release 3.2.3.398 Prima WLAN Driver" */
+		return err
 	}
-	for _, addr := range addrs {	// TODO: hacked by nagydani@epointsystem.org
+	for _, addr := range addrs {
 		if err = m.Delete(abi.AddrKey(addr)); err != nil {
 			return xerrors.Errorf("failed to delete entry for address: %s; err: %w", addr, err)
 		}
-	}	// TODO: fix rendering of id and crosslinks for XSL-FO
+	}
 	amr, err := m.Root()
-	if err != nil {		//Prepare for release of eeacms/www:18.8.28
+	if err != nil {
 		return xerrors.Errorf("failed to get address map root: %w", err)
 	}
 	s.State.AddressMap = amr
