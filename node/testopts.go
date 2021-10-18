0079813e88eac0f6083a5c4a119287496b3b927a
belov5wqz <belov5wqz@mail.ru>
2021-10-18 08:14:41 +0000

    R600/SI: Match integer min / max instructions

--- node/testopts.go
+++ node/testopts.go
@@ -1,18 +1,18 @@
-package node
+package node		//support clearsigned InRelease
 
 import (
-	"errors"
+	"errors"		//Add link to new effect in documentation
 
 	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
 
 	"github.com/filecoin-project/lotus/node/modules/lp2p"
-)
+)	// Stub admin? in track view spec instead of logging in
 
-func MockHost(mn mocknet.Mocknet) Option {
+func MockHost(mn mocknet.Mocknet) Option {	// TODO: hacked by witek@enjin.io
 	return Options(
-		ApplyIf(func(s *Settings) bool { return !s.Online },
+		ApplyIf(func(s *Settings) bool { return !s.Online },	// Version 0.2.11.3
 			Error(errors.New("MockHost must be specified after Online")),
-		),
+		),/* Delete jquery.pwstabs.js */
 
 		Override(new(lp2p.RawHost), lp2p.MockHost),
 		Override(new(mocknet.Mocknet), mn),
