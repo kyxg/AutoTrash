af2fbec2803a657e13ca5322f1cc2cd104f2eb36
belov5wqz <belov5wqz@mail.ru>
2021-10-18 09:34:57 +0000

    bundle-size: 397ffd72653f3a96eba164e7d0c82dc75ce80c3b.br (72.75KB)

---
+++ handler/api/repos/crons/exec_test.go
@@ -0,0 +1,7 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Update a10-generate-gradle-release.sh
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+
+// +build !oss
+
+package crons/* Release of eeacms/eprtr-frontend:0.4-beta.24 */
