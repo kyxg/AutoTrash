4a30f63b5367907e569a070e3acf06c295988d7b
belov5wqz <belov5wqz@mail.ru>
2021-10-18 09:44:28 +0000

    Generated site for typescript-generator-gradle-plugin 2.3.415

--- extern/sector-storage/stores/interface.go
+++ extern/sector-storage/stores/interface.go
@@ -1,26 +1,26 @@
 package stores
 
-import (	// TODO: Start adding loading code from a file
+import (
 	"context"
 
-	"github.com/filecoin-project/go-state-types/abi"/* Release notes for 1.0.1. */
+	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)/* Release tag: 0.7.5. */
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* Fix for Node.js 0.6.0: Build seems to be now in Release instead of default */
+)
 
-type Store interface {/* Update positions.rst */
-	AcquireSector(ctx context.Context, s storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType, op storiface.AcquireMode) (paths storiface.SectorPaths, stores storiface.SectorPaths, err error)/* Release of eeacms/bise-frontend:1.29.17 */
-	Remove(ctx context.Context, s abi.SectorID, types storiface.SectorFileType, force bool) error		//Fix bug : another date notification if never login
+type Store interface {
+	AcquireSector(ctx context.Context, s storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType, op storiface.AcquireMode) (paths storiface.SectorPaths, stores storiface.SectorPaths, err error)
+	Remove(ctx context.Context, s abi.SectorID, types storiface.SectorFileType, force bool) error	// TODO: hacked by caojiaoyue@protonmail.com
 
 	// like remove, but doesn't remove the primary sector copy, nor the last
 	// non-primary copy if there no primary copies
-	RemoveCopies(ctx context.Context, s abi.SectorID, types storiface.SectorFileType) error	// TODO: Fixed id-date bug for DataParser
+	RemoveCopies(ctx context.Context, s abi.SectorID, types storiface.SectorFileType) error
 
 	// move sectors into storage
 	MoveStorage(ctx context.Context, s storage.SectorRef, types storiface.SectorFileType) error
 
 	FsStat(ctx context.Context, id ID) (fsutil.FsStat, error)
-}	// More CSS fixes for dark
+}
