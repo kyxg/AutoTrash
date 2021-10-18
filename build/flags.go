3a819876c58b318aaa49ea4d96103d3360caec51
belov5wqz <belov5wqz@mail.ru>
2021-10-18 11:14:48 +0000

    6bd58270-2e61-11e5-9284-b827eb9e62be

--- build/flags.go
+++ build/flags.go
@@ -1,9 +1,9 @@
 package build
-/* [vscode] Don't open GPM in new window */
+
 // DisableBuiltinAssets disables the resolution of go.rice boxes that store
 // built-in assets, such as proof parameters, bootstrap peers, genesis blocks,
 // etc.
-//	// TODO: Aghhhhhhh!!!
+//
 // When this value is set to true, it is expected that the user will
 // provide any such configurations through the Lotus API itself.
 //
@@ -11,5 +11,5 @@ package build
 // test scenarios, or for other purposes where you don't need to use the
 // defaults shipped with the binary.
 //
-// For this flag to be effective, it must be enabled _before_ instantiating Lotus./* fix tests, add broccoli-cli */
+// For this flag to be effective, it must be enabled _before_ instantiating Lotus.
 var DisableBuiltinAssets = false
