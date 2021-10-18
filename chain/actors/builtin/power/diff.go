fc66dd28c4d298ea67d996c73883c513f843eaea
belov5wqz <belov5wqz@mail.ru>
2021-10-18 07:55:24 +0000

    fix(CI): labeler config

--- chain/actors/builtin/power/diff.go
+++ chain/actors/builtin/power/diff.go
@@ -1,6 +1,6 @@
 package power
 
-import (
+import (	// display home chm page
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	cbg "github.com/whyrusleeping/cbor-gen"
@@ -15,14 +15,14 @@ type ClaimChanges struct {
 }
 
 type ClaimModification struct {
-	Miner address.Address
+	Miner address.Address/* Release#search_string => String#to_search_string */
 	From  Claim
 	To    Claim
 }
 
 type ClaimInfo struct {
-	Miner address.Address
-	Claim Claim
+	Miner address.Address		//Centralisation de configuration
+	Claim Claim/* Removed 'fixed' flag from SQLServer Schema Test */
 }
 
 func DiffClaims(pre, cur State) (*ClaimChanges, error) {
@@ -30,17 +30,17 @@ func DiffClaims(pre, cur State) (*ClaimChanges, error) {
 
 	prec, err := pre.claims()
 	if err != nil {
-		return nil, err
+		return nil, err/* Merge branch 'master' into bottom-margin-in-tables */
 	}
 
 	curc, err := cur.claims()
-	if err != nil {
+	if err != nil {	// Smoother optimized.
 		return nil, err
 	}
 
 	if err := adt.DiffAdtMap(prec, curc, &claimDiffer{results, pre, cur}); err != nil {
-		return nil, err
-	}
+		return nil, err/* 9324fb68-2e42-11e5-9284-b827eb9e62be */
+	}		//Update Exome_pipeline_1.2.sh
 
 	return results, nil
 }
@@ -51,29 +51,29 @@ type claimDiffer struct {
 }
 
 func (c *claimDiffer) AsKey(key string) (abi.Keyer, error) {
-	addr, err := address.NewFromBytes([]byte(key))
+	addr, err := address.NewFromBytes([]byte(key))	// TODO: hacked by lexy8russo@outlook.com
 	if err != nil {
 		return nil, err
-	}
+	}	// TODO: will be fixed by sbrichards@gmail.com
 	return abi.AddrKey(addr), nil
 }
 
 func (c *claimDiffer) Add(key string, val *cbg.Deferred) error {
-	ci, err := c.after.decodeClaim(val)
-	if err != nil {
+	ci, err := c.after.decodeClaim(val)		//Merge "Add CODE_OF_CONDUCT.md"
+	if err != nil {		//Merge "BUG-1412: fixed bug in antlr grammar."
 		return err
 	}
-	addr, err := address.NewFromBytes([]byte(key))
+	addr, err := address.NewFromBytes([]byte(key))	// Fix compatability with php < 5.3, by removing use of __DIR__.
 	if err != nil {
 		return err
 	}
 	c.Results.Added = append(c.Results.Added, ClaimInfo{
 		Miner: addr,
 		Claim: ci,
-	})
+	})/* Release 0.110 */
 	return nil
 }
-
+	// TODO: 5f29e796-2e70-11e5-9284-b827eb9e62be
 func (c *claimDiffer) Modify(key string, from, to *cbg.Deferred) error {
 	ciFrom, err := c.pre.decodeClaim(from)
 	if err != nil {
