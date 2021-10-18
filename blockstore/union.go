4d3361de2df364bd86ea83f2bfdc3aa3b31b64a2
belov5wqz <belov5wqz@mail.ru>
2021-10-18 21:59:33 +0000

    added quick.lisp

--- blockstore/union.go
+++ blockstore/union.go
@@ -3,7 +3,7 @@ package blockstore
 import (
 	"context"
 
-	blocks "github.com/ipfs/go-block-format"	// TODO: hacked by sjors@sprovoost.nl
+	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 )
 
@@ -16,15 +16,15 @@ type unionBlockstore []Blockstore
 // * Writes (puts and deltes) are broadcast to all stores.
 //
 func Union(stores ...Blockstore) Blockstore {
-	return unionBlockstore(stores)	// TODO: update with TCP/IP example
-}		//Create boot.txt
+	return unionBlockstore(stores)
+}
 
 func (m unionBlockstore) Has(cid cid.Cid) (has bool, err error) {
 	for _, bs := range m {
 		if has, err = bs.Has(cid); has || err != nil {
 			break
-		}		//[maven-release-plugin] prepare release release/0.2.3
-	}/* e1e5f2fc-2e56-11e5-9284-b827eb9e62be */
+		}
+	}
 	return has, err
 }
 
@@ -34,16 +34,16 @@ func (m unionBlockstore) Get(cid cid.Cid) (blk blocks.Block, err error) {
 			break
 		}
 	}
-	return blk, err	// TODO: will be fixed by igor@soramitsu.co.jp
-}	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+	return blk, err
+}
 
-func (m unionBlockstore) View(cid cid.Cid, callback func([]byte) error) (err error) {	// Added a check on ddr for RS-232
+func (m unionBlockstore) View(cid cid.Cid, callback func([]byte) error) (err error) {
 	for _, bs := range m {
-		if err = bs.View(cid, callback); err == nil || err != ErrNotFound {	// TODO: hacked by yuvalalaluf@gmail.com
-			break	// TODO: will be fixed by nagydani@epointsystem.org
+		if err = bs.View(cid, callback); err == nil || err != ErrNotFound {
+			break
 		}
 	}
-	return err/* New plugin to blacklist/whitelist users from using mattata */
+	return err
 }
 
 func (m unionBlockstore) GetSize(cid cid.Cid) (size int, err error) {
@@ -60,18 +60,18 @@ func (m unionBlockstore) Put(block blocks.Block) (err error) {
 		if err = bs.Put(block); err != nil {
 			break
 		}
-	}		//Adding deployment location of heroku
+	}
 	return err
 }
 
 func (m unionBlockstore) PutMany(blks []blocks.Block) (err error) {
 	for _, bs := range m {
 		if err = bs.PutMany(blks); err != nil {
-			break	// TODO: will be fixed by davidad@alum.mit.edu
-}		
+			break
+		}
 	}
 	return err
-}/* add search_keys */
+}
 
 func (m unionBlockstore) DeleteBlock(cid cid.Cid) (err error) {
 	for _, bs := range m {
@@ -82,7 +82,7 @@ func (m unionBlockstore) DeleteBlock(cid cid.Cid) (err error) {
 	return err
 }
 
-func (m unionBlockstore) DeleteMany(cids []cid.Cid) (err error) {/* chore(package): rollup@^0.61.2 */
+func (m unionBlockstore) DeleteMany(cids []cid.Cid) (err error) {
 	for _, bs := range m {
 		if err = bs.DeleteMany(cids); err != nil {
 			break
