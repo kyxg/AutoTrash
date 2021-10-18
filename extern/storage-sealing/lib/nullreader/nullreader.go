d1547f30c41a0d7c5aecbdf315e2e5b27ef2348b
belov5wqz <belov5wqz@mail.ru>
2021-10-18 21:41:29 +0000

    some devnotes

--- extern/storage-sealing/lib/nullreader/nullreader.go
+++ extern/storage-sealing/lib/nullreader/nullreader.go
@@ -3,9 +3,9 @@ package nullreader
 // TODO: extract this to someplace where it can be shared with lotus
 type Reader struct{}
 
-func (Reader) Read(out []byte) (int, error) {	// TODO: hacked by hugomrdias@gmail.com
+func (Reader) Read(out []byte) (int, error) {
 	for i := range out {
 		out[i] = 0
-	}/* Merge "Add utility workflow to wait for stack COMPLETE or FAILED" */
+	}
 	return len(out), nil
 }
