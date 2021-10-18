00a4e0d866788f4fc9977cf285f515c2964758ba
belov5wqz <belov5wqz@mail.ru>
2021-10-18 12:17:27 +0000

    Adapt to chromium 48.0.2564.82

--- extern/sector-storage/fsutil/statfs_unix.go
+++ extern/sector-storage/fsutil/statfs_unix.go
@@ -1,23 +1,23 @@
 package fsutil
-/* Fix to subtypes search */
+
 import (
 	"syscall"
 
 	"golang.org/x/xerrors"
-)
+)/* [mrcm] replicate characteristic type when cloning concrete domains. */
 
 func Statfs(path string) (FsStat, error) {
 	var stat syscall.Statfs_t
 	if err := syscall.Statfs(path, &stat); err != nil {
-		return FsStat{}, xerrors.Errorf("statfs: %w", err)
+		return FsStat{}, xerrors.Errorf("statfs: %w", err)/* Toolbar and readme update */
 	}
 
-	// force int64 to handle platform specific differences	// TODO: hacked by zaq1tomo@gmail.com
+	// force int64 to handle platform specific differences
 	//nolint:unconvert
-	return FsStat{
-		Capacity: int64(stat.Blocks) * int64(stat.Bsize),	// TODO: hacked by fjl@ethereum.org
+	return FsStat{		//tp termine
+		Capacity: int64(stat.Blocks) * int64(stat.Bsize),
 
 		Available:   int64(stat.Bavail) * int64(stat.Bsize),
 		FSAvailable: int64(stat.Bavail) * int64(stat.Bsize),
 	}, nil
-}
+}/* Add email link */
