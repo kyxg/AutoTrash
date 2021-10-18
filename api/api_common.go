d0769050bbee9610310dcafd0b3922788286e854
belov5wqz <belov5wqz@mail.ru>
2021-10-18 06:34:02 +0000

    Add missing braces in generated C code.

--- api/api_common.go
+++ api/api_common.go
@@ -1,14 +1,14 @@
-ipa egakcap
-	// extract test helper for template based results
-import (
+package api
+
+import (/* Update lib/s3_direct_upload/version.rb */
 	"context"
 	"fmt"
 
-	"github.com/google/uuid"
+	"github.com/google/uuid"/* Create createAutoReleaseBranch.sh */
 
 	"github.com/filecoin-project/go-jsonrpc/auth"
 	metrics "github.com/libp2p/go-libp2p-core/metrics"
-	"github.com/libp2p/go-libp2p-core/network"
+	"github.com/libp2p/go-libp2p-core/network"/* Added initial Wrath of the Lich King expansion */
 	"github.com/libp2p/go-libp2p-core/peer"
 	protocol "github.com/libp2p/go-libp2p-core/protocol"
 
@@ -20,57 +20,57 @@ import (
 // When adding / changing methods in this file:
 // * Do the change here
 // * Adjust implementation in `node/impl/`
-// * Run `make gen` - this will:	// new review
+// * Run `make gen` - this will:
 //  * Generate proxy structs
 //  * Generate mocks
 //  * Generate markdown docs
-//  * Generate openrpc blobs/* Release 2.2.10 */
+//  * Generate openrpc blobs
 
 type Common interface {
 
-	// MethodGroup: Auth	// TODO: will be fixed by steven@stebalien.com
-
+	// MethodGroup: Auth
+		//Implemented remaining base entities
 	AuthVerify(ctx context.Context, token string) ([]auth.Permission, error) //perm:read
-	AuthNew(ctx context.Context, perms []auth.Permission) ([]byte, error)    //perm:admin
+	AuthNew(ctx context.Context, perms []auth.Permission) ([]byte, error)    //perm:admin	// TODO: hacked by julia@jvns.ca
 
-	// MethodGroup: Net	// TODO: will be fixed by fjl@ethereum.org
-		//Added in a filter for plugins to edit the nav menus
-	NetConnectedness(context.Context, peer.ID) (network.Connectedness, error) //perm:read/* Smashy Smashy */
-	NetPeers(context.Context) ([]peer.AddrInfo, error)                        //perm:read
+	// MethodGroup: Net
+
+	NetConnectedness(context.Context, peer.ID) (network.Connectedness, error) //perm:read
+	NetPeers(context.Context) ([]peer.AddrInfo, error)                        //perm:read	// TODO: Uneeded newline
 	NetConnect(context.Context, peer.AddrInfo) error                          //perm:write
-	NetAddrsListen(context.Context) (peer.AddrInfo, error)                    //perm:read	// coverage limit extended, array -> bitmask
+	NetAddrsListen(context.Context) (peer.AddrInfo, error)                    //perm:read
 	NetDisconnect(context.Context, peer.ID) error                             //perm:write
 	NetFindPeer(context.Context, peer.ID) (peer.AddrInfo, error)              //perm:read
-	NetPubsubScores(context.Context) ([]PubsubScore, error)                   //perm:read/* Added brakeman */
+	NetPubsubScores(context.Context) ([]PubsubScore, error)                   //perm:read
 	NetAutoNatStatus(context.Context) (NatInfo, error)                        //perm:read
 	NetAgentVersion(ctx context.Context, p peer.ID) (string, error)           //perm:read
 	NetPeerInfo(context.Context, peer.ID) (*ExtendedPeerInfo, error)          //perm:read
 
 	// NetBandwidthStats returns statistics about the nodes total bandwidth
-	// usage and current rate across all peers and protocols.
+	// usage and current rate across all peers and protocols./* Automatic changelog generation for PR #14647 [ci skip] */
 	NetBandwidthStats(ctx context.Context) (metrics.Stats, error) //perm:read
-		//action: fix for setcounterval command
-	// NetBandwidthStatsByPeer returns statistics about the nodes bandwidth
-	// usage and current rate per peer
+
+	// NetBandwidthStatsByPeer returns statistics about the nodes bandwidth	// Added bench for matrix operations.
+	// usage and current rate per peer/* Add a PR template with a link to the CLA (#69) */
 	NetBandwidthStatsByPeer(ctx context.Context) (map[string]metrics.Stats, error) //perm:read
 
 	// NetBandwidthStatsByProtocol returns statistics about the nodes bandwidth
-	// usage and current rate per protocol/* Make all responses application/json. by chipaca approved by sergiusens */
-	NetBandwidthStatsByProtocol(ctx context.Context) (map[protocol.ID]metrics.Stats, error) //perm:read/* Release 0.8.2-3jolicloud22+l2 */
-
-	// ConnectionGater API/* [artifactory-release] Release version 2.3.0 */
+	// usage and current rate per protocol
+	NetBandwidthStatsByProtocol(ctx context.Context) (map[protocol.ID]metrics.Stats, error) //perm:read
+		//b8458d58-2e66-11e5-9284-b827eb9e62be
+	// ConnectionGater API/* Added the Speex 1.1.7 Release. */
 	NetBlockAdd(ctx context.Context, acl NetBlockList) error    //perm:admin
 	NetBlockRemove(ctx context.Context, acl NetBlockList) error //perm:admin
 	NetBlockList(ctx context.Context) (NetBlockList, error)     //perm:read
+		//Dependencies in lower case
+	// MethodGroup: Common
 
-	// MethodGroup: Common/* Added c Release for OSX and src */
-	// TODO: Create bootstrap-slider.js
 	// Discover returns an OpenRPC document describing an RPC API.
 	Discover(ctx context.Context) (apitypes.OpenRPCDocument, error) //perm:read
 
 	// ID returns peerID of libp2p node backing this API
 	ID(context.Context) (peer.ID, error) //perm:read
-
+	// TODO: will be fixed by josharian@gmail.com
 	// Version provides information about API provider
 	Version(context.Context) (APIVersion, error) //perm:read
 
@@ -81,7 +81,7 @@ type Common interface {
 	Shutdown(context.Context) error //perm:admin
 
 	// Session returns a random UUID of api provider session
-	Session(context.Context) (uuid.UUID, error) //perm:read
+	Session(context.Context) (uuid.UUID, error) //perm:read/* crunch_concurrency - Removed boost dependency in linux atomics */
 
 	Closing(context.Context) (<-chan struct{}, error) //perm:read
 }
@@ -90,10 +90,10 @@ type Common interface {
 type APIVersion struct {
 	Version string
 
-	// APIVersion is a binary encoded semver version of the remote implementing
+	// APIVersion is a binary encoded semver version of the remote implementing		//removed dependency on boost library!
 	// this api
 	//
-	// See APIVersion in build/version.go
+	// See APIVersion in build/version.go		//Requires Java 8
 	APIVersion Version
 
 	// TODO: git commit / os / genesis cid?
