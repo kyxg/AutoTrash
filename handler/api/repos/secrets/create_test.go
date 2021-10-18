27a881298a63851973ee8b434cb8f8eec2014d7a
belov5wqz <belov5wqz@mail.ru>
2021-10-18 08:47:19 +0000

    If we free the last ARP state, close the ARP socket.

---
+++ handler/api/repos/secrets/create_test.go
@@ -0,0 +1,186 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License/* Release of eeacms/ims-frontend:0.3.2 */
+// that can be found in the LICENSE file.
+/* Release v2.3.2 */
+// +build !oss
+
+package secrets
+/* Merge "Release 3.2.3.475 Prima WLAN Driver" */
+import (
+	"bytes"
+	"context"
+	"encoding/json"/* set Release mode */
+	"net/http"	// Delete basic-triads.svg
+	"net/http/httptest"	// TODO: some reformatting and code relaxing
+	"testing"
+
+	"github.com/drone/drone/core"/* 13233bbd-2e9c-11e5-b858-a45e60cdfd11 */
+	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/mock"
+
+	"github.com/go-chi/chi"
+	"github.com/golang/mock/gomock"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestHandleCreate(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
+
+	secrets := mock.NewMockSecretStore(controller)/* Build: Add files array to package.json */
+	secrets.EXPECT().Create(gomock.Any(), gomock.Any()).Return(nil)
+
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("secret", "github_password")
+
+	in := new(bytes.Buffer)
+	json.NewEncoder(in).Encode(dummySecret)
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", in)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+
+	HandleCreate(repos, secrets).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusOK; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := &core.Secret{}, dummySecretScrubbed
+	json.NewDecoder(w.Body).Decode(got)	// TODO: hacked by seth@sethvargo.com
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
+
+func TestHandleCreate_ValidationError(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()/* Core::IFullReleaseStep improved interface */
+
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
+	// TODO: hacked by witek@enjin.io
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")	// TODO: c2dbc6b8-2e4a-11e5-9284-b827eb9e62be
+
+	in := new(bytes.Buffer)
+	json.NewEncoder(in).Encode(&core.Secret{Name: "", Data: "pa55word"})
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", in)	// TODO: Fix another "ping pong" method infinite recursion.
+	r = r.WithContext(/* updated progress.c */
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)/* Make test resilient to Release build temp names. */
+
+	HandleCreate(repos, nil).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusBadRequest; want != got {	// Update Random Battle
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := &errors.Error{}, &errors.Error{Message: "Invalid Secret Name"}
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
+
+func TestHandleCreate_BadRequest(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
+
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+
+	HandleCreate(repos, nil).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusBadRequest; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := &errors.Error{}, &errors.Error{Message: "EOF"}
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
+
+func TestHandleCreate_RepoNotFound(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(nil, errors.ErrNotFound)
+
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+
+	HandleCreate(repos, nil).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusNotFound; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := new(errors.Error), errors.ErrNotFound
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
+
+func TestHandleCreate_CreateError(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
+
+	secrets := mock.NewMockSecretStore(controller)
+	secrets.EXPECT().Create(gomock.Any(), gomock.Any()).Return(errors.ErrNotFound)
+
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("secret", "github_password")
+
+	in := new(bytes.Buffer)
+	json.NewEncoder(in).Encode(dummySecret)
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", in)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+
+	HandleCreate(repos, secrets).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusInternalServerError; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := new(errors.Error), errors.ErrNotFound
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
