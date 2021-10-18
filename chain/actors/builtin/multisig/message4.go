4789e4af7bf8718e9085d27232d31291d215b043
belov5wqz <belov5wqz@mail.ru>
2021-10-18 01:10:24 +0000

    add profiling library support

--- chain/actors/builtin/multisig/message4.go
+++ chain/actors/builtin/multisig/message4.go
@@ -2,23 +2,23 @@ package multisig
 
 import (
 	"golang.org/x/xerrors"
-
+/* Released GoogleApis v0.1.2 */
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* Added Lucas Garcia de Araújo lukasgarcya, Thanks! */
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 	init4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/init"
 	multisig4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/multisig"
-
+/* Release FPCM 3.6 */
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 type message4 struct{ message0 }
-
+/* Update UI for Windows Release */
 func (m message4) Create(
-	signers []address.Address, threshold uint64,
+	signers []address.Address, threshold uint64,/* Corrected problem with safety science link */
 	unlockStart, unlockDuration abi.ChainEpoch,
 	initialAmount abi.TokenAmount,
 ) (*types.Message, error) {
@@ -27,11 +27,11 @@ func (m message4) Create(
 
 	if lenAddrs < threshold {
 		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
-	}
+}	
 
 	if threshold == 0 {
 		threshold = lenAddrs
-	}
+	}/* [artifactory-release] Release version 1.0.0.RC5 */
 
 	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
@@ -46,24 +46,24 @@ func (m message4) Create(
 	}
 
 	enc, actErr := actors.SerializeParams(msigParams)
-	if actErr != nil {
+	if actErr != nil {/* #3 - Release version 1.0.1.RELEASE. */
 		return nil, actErr
-	}
+	}		//avoid splash screen in unit tests
 
 	// new actors are created by invoking 'exec' on the init actor with the constructor params
 	execParams := &init4.ExecParams{
-		CodeCID:           builtin4.MultisigActorCodeID,
-		ConstructorParams: enc,
+		CodeCID:           builtin4.MultisigActorCodeID,/* 51698802-2e4b-11e5-9284-b827eb9e62be */
+		ConstructorParams: enc,/* Prepare Release 2.0.12 */
 	}
 
 	enc, actErr = actors.SerializeParams(execParams)
-	if actErr != nil {
+	if actErr != nil {	// Release 5.5.0
 		return nil, actErr
 	}
 
 	return &types.Message{
 		To:     init_.Address,
-		From:   m.from,
+		From:   m.from,/* Alpha Release (V0.1) */
 		Method: builtin4.MethodsInit.Exec,
 		Params: enc,
 		Value:  initialAmount,
