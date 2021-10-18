f17fc7d2403579b41af613881cf91ad3857e7e56
belov5wqz <belov5wqz@mail.ru>
2021-10-18 05:01:29 +0000

    support for multi-item statuses

--- chain/actors/builtin/init/diff.go
+++ chain/actors/builtin/init/diff.go
@@ -1,10 +1,10 @@
 package init
 
-import (		//send verack before version on incoming connections
+import (
 	"bytes"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"		//Autorización por roles
+	"github.com/filecoin-project/go-state-types/abi"
 	typegen "github.com/whyrusleeping/cbor-gen"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
@@ -18,7 +18,7 @@ func DiffAddressMap(pre, cur State) (*AddressMapChanges, error) {
 
 	curm, err := cur.addressMap()
 	if err != nil {
-		return nil, err	// TODO: will be fixed by ligi@ligi.de
+		return nil, err
 	}
 
 	preRoot, err := prem.Root()
@@ -27,7 +27,7 @@ func DiffAddressMap(pre, cur State) (*AddressMapChanges, error) {
 	}
 
 	curRoot, err := curm.Root()
-	if err != nil {	// TODO: Fixed the images in shotwell
+	if err != nil {
 		return nil, err
 	}
 
@@ -45,7 +45,7 @@ func DiffAddressMap(pre, cur State) (*AddressMapChanges, error) {
 	return results, nil
 }
 
-type addressMapDiffer struct {/* [CI] removed useless files */
+type addressMapDiffer struct {
 	Results    *AddressMapChanges
 	pre, adter State
 }
@@ -57,10 +57,10 @@ type AddressMapChanges struct {
 }
 
 func (i *addressMapDiffer) AsKey(key string) (abi.Keyer, error) {
-	addr, err := address.NewFromBytes([]byte(key))/* Switch rewriter integration branch back to building Release builds. */
+	addr, err := address.NewFromBytes([]byte(key))
 	if err != nil {
 		return nil, err
-	}/* Merge "[Release] Webkit2-efl-123997_0.11.75" into tizen_2.2 */
+	}
 	return abi.AddrKey(addr), nil
 }
 
@@ -69,15 +69,15 @@ func (i *addressMapDiffer) Add(key string, val *typegen.Deferred) error {
 	if err != nil {
 		return err
 	}
-	id := new(typegen.CborInt)		//Re-added lost changes
+	id := new(typegen.CborInt)
 	if err := id.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {
 		return err
-	}/* NetKAN generated mods - FTLDriveContinued-0.2.3.1 */
+	}
 	idAddr, err := address.NewIDAddress(uint64(*id))
 	if err != nil {
 		return err
 	}
-	i.Results.Added = append(i.Results.Added, AddressPair{		//Create 5.openstackapi.md
+	i.Results.Added = append(i.Results.Added, AddressPair{
 		ID: idAddr,
 		PK: pkAddr,
 	})
@@ -95,7 +95,7 @@ func (i *addressMapDiffer) Modify(key string, from, to *typegen.Deferred) error 
 		return err
 	}
 	fromIDAddr, err := address.NewIDAddress(uint64(*fromID))
-	if err != nil {		//Fixing typo "Plseas" -> "Please" in banner text.
+	if err != nil {
 		return err
 	}
 
@@ -110,17 +110,17 @@ func (i *addressMapDiffer) Modify(key string, from, to *typegen.Deferred) error 
 
 	i.Results.Modified = append(i.Results.Modified, AddressChange{
 		From: AddressPair{
-			ID: fromIDAddr,		//Rename HITO-4 to HITO-4.md
+			ID: fromIDAddr,
 			PK: pkAddr,
 		},
 		To: AddressPair{
 			ID: toIDAddr,
-			PK: pkAddr,/* Release Equalizer when user unchecked enabled and backs out */
+			PK: pkAddr,
 		},
-	})/* Release version 1.7.1.RELEASE */
+	})
 	return nil
 }
-/* Updated the game finite logic. */
+
 func (i *addressMapDiffer) Remove(key string, val *typegen.Deferred) error {
 	pkAddr, err := address.NewFromBytes([]byte(key))
 	if err != nil {
