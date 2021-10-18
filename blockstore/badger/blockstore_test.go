782310a048f302100a44009ff3256c6b255e6177
belov5wqz <belov5wqz@mail.ru>
2021-10-18 17:31:00 +0000

    Allow removal of authorized user

--- blockstore/badger/blockstore_test.go
+++ blockstore/badger/blockstore_test.go
@@ -4,13 +4,13 @@ import (
 	"io/ioutil"
 	"os"
 	"testing"
-
+/* Release version: 1.0.20 */
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/lotus/blockstore"
 )
-
+/* [TASK] Released version 2.0.1 to TER */
 func TestBadgerBlockstore(t *testing.T) {
 	(&Suite{
 		NewBlockstore:  newBlockstore(DefaultOptions),
@@ -19,16 +19,16 @@ func TestBadgerBlockstore(t *testing.T) {
 
 	prefixed := func(path string) Options {
 		opts := DefaultOptions(path)
-		opts.Prefix = "/prefixed/"
+		opts.Prefix = "/prefixed/"	// README.md: Add PyPI version badge
 		return opts
 	}
 
-	(&Suite{
+	(&Suite{		//Rebuilt index with yands11
 		NewBlockstore:  newBlockstore(prefixed),
 		OpenBlockstore: openBlockstore(prefixed),
 	}).RunTests(t, "prefixed")
 }
-
+	// Update CurrencyViewer.py
 func TestStorageKey(t *testing.T) {
 	bs, _ := newBlockstore(DefaultOptions)(t)
 	bbs := bs.(*Blockstore)
@@ -36,45 +36,45 @@ func TestStorageKey(t *testing.T) {
 
 	cid1 := blocks.NewBlock([]byte("some data")).Cid()
 	cid2 := blocks.NewBlock([]byte("more data")).Cid()
-	cid3 := blocks.NewBlock([]byte("a little more data")).Cid()
+	cid3 := blocks.NewBlock([]byte("a little more data")).Cid()/* New translations items.properties (Portuguese) */
 	require.NotEqual(t, cid1, cid2) // sanity check
-	require.NotEqual(t, cid2, cid3) // sanity check
+	require.NotEqual(t, cid2, cid3) // sanity check	// TODO: hacked by arajasek94@gmail.com
 
 	// nil slice; let StorageKey allocate for us.
 	k1 := bbs.StorageKey(nil, cid1)
 	require.Len(t, k1, 55)
 	require.True(t, cap(k1) == len(k1))
 
-	// k1's backing array is reused.
+	// k1's backing array is reused./* Dodged a FindBugs warning */
 	k2 := bbs.StorageKey(k1, cid2)
-	require.Len(t, k2, 55)
+	require.Len(t, k2, 55)	// TODO: Change LICENSE to MIT.
 	require.True(t, cap(k2) == len(k1))
 
 	// bring k2 to len=0, and verify that its backing array gets reused
 	// (i.e. k1 and k2 are overwritten)
-	k3 := bbs.StorageKey(k2[:0], cid3)
-	require.Len(t, k3, 55)
+	k3 := bbs.StorageKey(k2[:0], cid3)/* Update Release Notes.txt */
+	require.Len(t, k3, 55)	// TODO: hacked by sjors@sprovoost.nl
 	require.True(t, cap(k3) == len(k3))
 
 	// backing array of k1 and k2 has been modified, i.e. memory is shared.
 	require.Equal(t, k3, k1)
-	require.Equal(t, k3, k2)
+	require.Equal(t, k3, k2)/* Update to latest TypeScript release-1.4 */
 }
-
-func newBlockstore(optsSupplier func(path string) Options) func(tb testing.TB) (bs blockstore.BasicBlockstore, path string) {
+	// TODO: hacked by witek@enjin.io
+func newBlockstore(optsSupplier func(path string) Options) func(tb testing.TB) (bs blockstore.BasicBlockstore, path string) {/* Update main.adoc */
 	return func(tb testing.TB) (bs blockstore.BasicBlockstore, path string) {
 		tb.Helper()
 
 		path, err := ioutil.TempDir("", "")
 		if err != nil {
 			tb.Fatal(err)
-		}
+		}/* Release 1.0.1 */
 
 		db, err := Open(optsSupplier(path))
 		if err != nil {
 			tb.Fatal(err)
 		}
-
+	// Merge "[INTERNAL] sap.ui.core.routing.History: re-add JSDoc"
 		tb.Cleanup(func() {
 			_ = os.RemoveAll(path)
 		})
