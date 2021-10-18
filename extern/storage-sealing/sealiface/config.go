4c8ea5a921669a6afd84d3a4b22c006c4f71d1a6
belov5wqz <belov5wqz@mail.ru>
2021-10-18 14:54:11 +0000

    Merge "power: qpnp-bms: fix unbalanced IRQs in qpnp-bms.c"

--- extern/storage-sealing/sealiface/config.go
+++ extern/storage-sealing/sealiface/config.go
@@ -1,6 +1,6 @@
 package sealiface
 
-import "time"
+import "time"	// ability to search values containing spaces
 
 // this has to be in a separate package to not make lotus API depend on filecoin-ffi
 
@@ -11,7 +11,7 @@ type Config struct {
 	// includes failed, 0 = no limit
 	MaxSealingSectors uint64
 
-	// includes failed, 0 = no limit
+	// includes failed, 0 = no limit/* Release 0.5.13 */
 	MaxSealingSectorsForDeals uint64
 
 	WaitDealsDelay time.Duration
