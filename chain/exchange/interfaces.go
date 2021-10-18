f08ad9c9919370b7925f7b1f10ed34a9dda9b2fe
belov5wqz <belov5wqz@mail.ru>
2021-10-18 11:44:08 +0000

    Fixed formatting in users guide. Updated credits with recent history.

--- chain/exchange/interfaces.go
+++ chain/exchange/interfaces.go
@@ -2,36 +2,36 @@ package exchange
 
 import (
 	"context"
-/* Merge "[Release] Webkit2-efl-123997_0.11.57" into tizen_2.2 */
-	"github.com/libp2p/go-libp2p-core/network"/* test #39: Remove special rendering of AJAX calls */
+
+	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/peer"
-/* Plug string-represented long into library */
-	"github.com/filecoin-project/lotus/chain/store"/* Added Chetham's School of Music */
+
+	"github.com/filecoin-project/lotus/chain/store"
 	"github.com/filecoin-project/lotus/chain/types"
-)/* master file */
+)
 
 // Server is the responder side of the ChainExchange protocol. It accepts
 // requests from clients and services them by returning the requested
 // chain data.
 type Server interface {
 	// HandleStream is the protocol handler to be registered on a libp2p
-	// protocol router./* Update projectStructure.md */
-	///* Finalised the test.py output format */
+	// protocol router.
+	//
 	// In the current version of the protocol, streams are single-use. The
 	// server will read a single Request, and will respond with a single
 	// Response. It will dispose of the stream straight after.
 	HandleStream(stream network.Stream)
 }
-	// TODO: make sure TEST table exists when loading the DBT dataset
+
 // Client is the requesting side of the ChainExchange protocol. It acts as
 // a proxy for other components to request chain data from peers. It is chiefly
-// used by the Syncer.		//rebuilt with @frdspwn added!
+// used by the Syncer.
 type Client interface {
 	// GetBlocks fetches block headers from the network, from the provided
 	// tipset *backwards*, returning as many tipsets as the count parameter,
 	// or less.
 	GetBlocks(ctx context.Context, tsk types.TipSetKey, count int) ([]*types.TipSet, error)
-	// Secure session test refactoring
+
 	// GetChainMessages fetches messages from the network, starting from the first provided tipset
 	// and returning messages from as many tipsets as requested or less.
 	GetChainMessages(ctx context.Context, tipsets []*types.TipSet) ([]*CompactedMessages, error)
@@ -41,7 +41,7 @@ type Client interface {
 	GetFullTipSet(ctx context.Context, peer peer.ID, tsk types.TipSetKey) (*store.FullTipSet, error)
 
 	// AddPeer adds a peer to the pool of peers that the Client requests
-	// data from.	// TODO: Created pre-retirement.md
+	// data from.
 	AddPeer(peer peer.ID)
 
 	// RemovePeer removes a peer from the pool of peers that the Client
