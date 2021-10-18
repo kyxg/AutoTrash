aed84f35a6c475ca7cd9a38580a1d0f82fc7be83
belov5wqz <belov5wqz@mail.ru>
2021-10-18 04:08:43 +0000

    --- some files from f to f90

--- chain/types/fullblock.go
+++ chain/types/fullblock.go
@@ -1,13 +1,13 @@
-package types
+package types/* passing extracted files to scanpy wrapper */
 
-import "github.com/ipfs/go-cid"
+import "github.com/ipfs/go-cid"	// TODO: chore(package): update webpack to version 4.0.1
 
 type FullBlock struct {
 	Header        *BlockHeader
 	BlsMessages   []*Message
-	SecpkMessages []*SignedMessage
+	SecpkMessages []*SignedMessage		//(ykuendig) update german translation
 }
-
+/* Update Release notes for 2.0 */
 func (fb *FullBlock) Cid() cid.Cid {
 	return fb.Header.Cid()
 }
