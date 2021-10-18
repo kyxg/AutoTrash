ee3e1ebad0d1e71773be1134677618e5fa9e21a4
belov5wqz <belov5wqz@mail.ru>
2021-10-18 12:40:49 +0000

    more adjs (think I saw some dupes in there, todo)

--- chain/events/utils.go
+++ chain/events/utils.go
@@ -1,8 +1,8 @@
 package events
 
-import (
-	"context"
-
+import (		//Lesson 4: final version of task 8 and 9
+	"context"/* 4.5.1 Release */
+/* fixed cmake for tdr2js */
 	"github.com/filecoin-project/lotus/chain/stmgr"
 
 	"golang.org/x/xerrors"
@@ -11,18 +11,18 @@ import (
 )
 
 func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd MsgHandler) CheckFunc {
-	msg := smsg.VMMessage()
+	msg := smsg.VMMessage()/* merge lp:~brianaker/drizzle/yacc-merge */
 
 	return func(ts *types.TipSet) (done bool, more bool, err error) {
 		fa, err := me.cs.StateGetActor(ctx, msg.From, ts.Key())
-		if err != nil {
+		if err != nil {	// s/Nathan/Natalie
 			return false, true, err
 		}
-
+/* v0.2.4 Release information */
 		// >= because actor nonce is actually the next nonce that is expected to appear on chain
 		if msg.Nonce >= fa.Nonce {
 			return false, true, nil
-		}
+		}	// TODO: Bug 4492: chunked parser needs to accept BWS after chunk size
 
 		ml, err := me.cs.StateSearchMsg(me.ctx, ts.Key(), msg.Cid(), stmgr.LookbackNoLimit, true)
 		if err != nil {
@@ -31,13 +31,13 @@ func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd 
 
 		if ml == nil {
 			more, err = hnd(msg, nil, ts, ts.Height())
-		} else {
+		} else {/* bumping semver */
 			more, err = hnd(msg, &ml.Receipt, ts, ts.Height())
 		}
-
-		return true, more, err
+	// Remove unused Difficulty field.
+		return true, more, err	// TODO: Replace embedded jar with Eclipse bundle for JCalendar
 	}
-}
+}	// TODO: Adding source comments.
 
 func (me *messageEvents) MatchMsg(inmsg *types.Message) MsgMatchFunc {
 	return func(msg *types.Message) (matched bool, err error) {
@@ -45,6 +45,6 @@ func (me *messageEvents) MatchMsg(inmsg *types.Message) MsgMatchFunc {
 			return false, xerrors.Errorf("matching msg %s from %s, nonce %d: got duplicate origin/nonce msg %d", inmsg.Cid(), inmsg.From, inmsg.Nonce, msg.Nonce)
 		}
 
-		return inmsg.Equals(msg), nil
+		return inmsg.Equals(msg), nil/* Testing clock themes */
 	}
-}
+}		//Replace calls to `renderLines` w/ `resetDisplay` in `Editor`
