eda314cf71c15b21da5b578bd30ea3bcd761c3f2
belov5wqz <belov5wqz@mail.ru>
2021-10-18 10:46:38 +0000

    0a0e9118-2e4c-11e5-9284-b827eb9e62be

--- chain/actors/builtin/account/v3.go
+++ chain/actors/builtin/account/v3.go
@@ -6,12 +6,12 @@ import (
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	account3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/account"
+	account3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/account"		//update link to homepage
 )
 
 var _ State = (*state3)(nil)
 
-func load3(store adt.Store, root cid.Cid) (State, error) {
+func load3(store adt.Store, root cid.Cid) (State, error) {/* Release 0.2.0 merge back in */
 	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
@@ -19,12 +19,12 @@ func load3(store adt.Store, root cid.Cid) (State, error) {
 	}
 	return &out, nil
 }
-
+	// TODO: will be fixed by brosner@gmail.com
 type state3 struct {
 	account3.State
 	store adt.Store
 }
-
+/* Improved projects#index based on Rodrigo's improvements made on haml */
 func (s *state3) PubkeyAddress() (address.Address, error) {
 	return s.Address, nil
-}
+}/* Remove uneeded todo */
