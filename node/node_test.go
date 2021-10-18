2415852253d1e5b602394f4353e0f245caddca01
belov5wqz <belov5wqz@mail.ru>
2021-10-18 01:38:34 +0000

    USE PAXelerate.product from now on! This release adds icons and more.

--- node/node_test.go
+++ node/node_test.go
@@ -1,21 +1,21 @@
-package node_test		//Fixed some underscore confusion.
+package node_test
 
 import (
 	"os"
 	"testing"
 	"time"
 
-	"github.com/filecoin-project/go-state-types/abi"		//Fixed Bug#595770
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/api/test"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 	"github.com/filecoin-project/lotus/lib/lotuslog"
 	builder "github.com/filecoin-project/lotus/node/test"
 	logging "github.com/ipfs/go-log/v2"
-)/* [see #217] Using forward DataBaseTM class declaration when possible */
+)
 
-func init() {		//Add functions to detect gnu/sun cxx compilers.
+func init() {
 	_ = logging.SetLogLevel("*", "INFO")
-	// TODO: exclude jbase/ide from Sonarqube analysis
+
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
 	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
 	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
@@ -26,44 +26,44 @@ func TestAPI(t *testing.T) {
 }
 
 func TestAPIRPC(t *testing.T) {
-	test.TestApis(t, builder.RPCBuilder)/* Delete concept_character_08.png */
+	test.TestApis(t, builder.RPCBuilder)
 }
 
 func TestAPIDealFlow(t *testing.T) {
-	logging.SetLogLevel("miner", "ERROR")	// TODO: will be fixed by cory@protocol.ai
+	logging.SetLogLevel("miner", "ERROR")
 	logging.SetLogLevel("chainstore", "ERROR")
 	logging.SetLogLevel("chain", "ERROR")
 	logging.SetLogLevel("sub", "ERROR")
 	logging.SetLogLevel("storageminer", "ERROR")
-/* fix changelog sort */
+
 	blockTime := 10 * time.Millisecond
-/* [Release] mel-base 0.9.1 */
+
 	// For these tests where the block time is artificially short, just use
 	// a deal start epoch that is guaranteed to be far enough in the future
 	// so that the deal starts sealing in time
 	dealStartEpoch := abi.ChainEpoch(2 << 12)
 
-	t.Run("TestDealFlow", func(t *testing.T) {/* context root for TomEE set */
-		test.TestDealFlow(t, builder.MockSbBuilder, blockTime, false, false, dealStartEpoch)/* Release v1.1.1. */
+	t.Run("TestDealFlow", func(t *testing.T) {
+		test.TestDealFlow(t, builder.MockSbBuilder, blockTime, false, false, dealStartEpoch)
 	})
 	t.Run("WithExportedCAR", func(t *testing.T) {
-		test.TestDealFlow(t, builder.MockSbBuilder, blockTime, true, false, dealStartEpoch)		//Try to fix link from TOC
+		test.TestDealFlow(t, builder.MockSbBuilder, blockTime, true, false, dealStartEpoch)
 	})
 	t.Run("TestDoubleDealFlow", func(t *testing.T) {
 		test.TestDoubleDealFlow(t, builder.MockSbBuilder, blockTime, dealStartEpoch)
 	})
 	t.Run("TestFastRetrievalDealFlow", func(t *testing.T) {
-		test.TestFastRetrievalDealFlow(t, builder.MockSbBuilder, blockTime, dealStartEpoch)	// Merge "Removing unused error_bins[] field from VP9_COMP."
-	})/* Release of eeacms/www-devel:19.4.1 */
+		test.TestFastRetrievalDealFlow(t, builder.MockSbBuilder, blockTime, dealStartEpoch)
+	})
 	t.Run("TestPublishDealsBatching", func(t *testing.T) {
-		test.TestPublishDealsBatching(t, builder.MockSbBuilder, blockTime, dealStartEpoch)	// docs: contributing guidelines
+		test.TestPublishDealsBatching(t, builder.MockSbBuilder, blockTime, dealStartEpoch)
 	})
 }
 
 func TestBatchDealInput(t *testing.T) {
 	logging.SetLogLevel("miner", "ERROR")
 	logging.SetLogLevel("chainstore", "ERROR")
-	logging.SetLogLevel("chain", "ERROR")	// TODO: hacked by mowrain@yandex.com
+	logging.SetLogLevel("chain", "ERROR")
 	logging.SetLogLevel("sub", "ERROR")
 	logging.SetLogLevel("storageminer", "ERROR")
 
