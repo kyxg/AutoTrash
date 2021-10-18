2c1adad5e98f30331a3c98f45648acd9fd64c7cb
belov5wqz <belov5wqz@mail.ru>
2021-10-18 18:28:30 +0000

    Adding user category to the database and links

---
+++ handler/api/repos/sign/sign_test.go
@@ -0,0 +1,5 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License/* Merge branch 'develop' into fix/snap-execstack-and-grpc */
+// that can be found in the LICENSE file.
+
+package sign
