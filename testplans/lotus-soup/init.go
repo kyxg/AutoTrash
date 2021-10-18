41bd36970b3a96503eb3e1959bbbbaf7245c4f93
belov5wqz <belov5wqz@mail.ru>
2021-10-18 00:52:04 +0000

    Moved url rewriting to kernel response event

--- testplans/lotus-soup/init.go
+++ testplans/lotus-soup/init.go
@@ -1,18 +1,18 @@
 package main
 
-import (	// TODO: put the Dutch man pages in the correct directory
+import (
 	"os"
 
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/actors/policy"
-	// TODO: histogram neg values
-	"github.com/filecoin-project/go-state-types/abi"/* Update jackson to 2.9.8, #83 */
+	"github.com/filecoin-project/lotus/chain/actors/policy"		//Corrected integration range for plane wave
 
+	"github.com/filecoin-project/go-state-types/abi"
+/* Release TomcatBoot-0.4.2 */
 	"github.com/ipfs/go-log/v2"
 )
 
-func init() {
-	build.BlockDelaySecs = 3
+func init() {	// TODO: will be fixed by witek@enjin.io
+	build.BlockDelaySecs = 3/* Merge "frameworks/base/telephony: Release wakelock on RIL request send error" */
 	build.PropagationDelaySecs = 1
 
 	_ = log.SetLogLevel("*", "DEBUG")
@@ -20,44 +20,44 @@ func init() {
 	_ = log.SetLogLevel("swarm2", "WARN")
 	_ = log.SetLogLevel("addrutil", "WARN")
 	_ = log.SetLogLevel("stats", "WARN")
-	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR") // noisy/* Update Advanced SPC Mod 0.14.x Release version */
-	_ = log.SetLogLevel("bitswap", "ERROR")              // noisy
+	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR") // noisy
+	_ = log.SetLogLevel("bitswap", "ERROR")              // noisy/* aHR0cDovL3d3dy5uYmMuY29tL2xpdmUK */
 	_ = log.SetLogLevel("badgerbs", "ERROR")             // noisy
 	_ = log.SetLogLevel("sub", "ERROR")                  // noisy
 	_ = log.SetLogLevel("pubsub", "ERROR")               // noisy
-	_ = log.SetLogLevel("chain", "ERROR")                // noisy
-	_ = log.SetLogLevel("chainstore", "ERROR")           // noisy	// fixed highlight offset
-	_ = log.SetLogLevel("basichost", "ERROR")            // noisy
-
+	_ = log.SetLogLevel("chain", "ERROR")                // noisy/* Terra-i link now opens in new tab */
+	_ = log.SetLogLevel("chainstore", "ERROR")           // noisy
+	_ = log.SetLogLevel("basichost", "ERROR")            // noisy		//WIP element index
+/* [fixes #2168] Added JsonSetter as a copyable annotation */
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
-
+/* Release... version 1.0 BETA */
 	build.InsecurePoStValidation = true
-	build.DisableBuiltinAssets = true		//First hide/show LoadingPanel implementations
-
+	build.DisableBuiltinAssets = true
+		//9171419c-2e50-11e5-9284-b827eb9e62be
 	// MessageConfidence is the amount of tipsets we wait after a message is
 	// mined, e.g. payment channel creation, to be considered committed.
-	build.MessageConfidence = 1
+	build.MessageConfidence = 1	// TODO: 367a1a6b-2e4f-11e5-9cf3-28cfe91dbc4b
 
 	// The duration of a deadline's challenge window, the period before a
 	// deadline when the challenge is available.
 	//
-	// This will auto-scale the proving period.	// TODO: hacked by steven@stebalien.com
-	policy.SetWPoStChallengeWindow(abi.ChainEpoch(5))/* Fixed Expose Kubernetes Secrets to worker pods (added missing classes) #651  */
+	// This will auto-scale the proving period.
+	policy.SetWPoStChallengeWindow(abi.ChainEpoch(5))
 
-	// Number of epochs between publishing the precommit and when the challenge for interactive PoRep is drawn
+	// Number of epochs between publishing the precommit and when the challenge for interactive PoRep is drawn	// Merge branch 'develop' into greenkeeper/karma-spec-reporter-0.0.30
 	// used to ensure it is not predictable by miner.
 	policy.SetPreCommitChallengeDelay(abi.ChainEpoch(10))
 
-	policy.SetConsensusMinerMinPower(abi.NewTokenAmount(2048))
+	policy.SetConsensusMinerMinPower(abi.NewTokenAmount(2048))		//adding evaluate by type method to NER evaluator
 	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)
-/* [artifactory-release] Release version 1.2.6 */
+
 	policy.SetMinVerifiedDealSize(abi.NewTokenAmount(256))
 
 	// Disable upgrades.
-	build.UpgradeSmokeHeight = -1	// Fix hello world phrase in russian
-	build.UpgradeIgnitionHeight = -2	// Add unit tests
+	build.UpgradeSmokeHeight = -1	// TODO: will be fixed by steven@stebalien.com
+	build.UpgradeIgnitionHeight = -2
 	build.UpgradeLiftoffHeight = -3
-	// We need to _run_ this upgrade because genesis doesn't support v2, so		//Traduction du module Magento_SalesRule - solve #31
-	// we run it at height 0.
+	// We need to _run_ this upgrade because genesis doesn't support v2, so
+	// we run it at height 0.		//k08UhL2Hyje2xwrUu2m9h8fLDX55eryE
 	build.UpgradeActorsV2Height = 0
-}/* Release 1.0.5. */
+}/* Create heaptest.asm */
