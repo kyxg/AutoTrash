d8dbd8f3aeb20fc857651cd2ca8d9a576c4d3064
belov5wqz <belov5wqz@mail.ru>
2021-10-18 03:54:02 +0000

    Macro configuration

--- chain/actors/builtin/reward/v3.go
+++ chain/actors/builtin/reward/v3.go
@@ -1,11 +1,11 @@
 package reward
-		//Updated casdr's URL
+
 import (
-	"github.com/filecoin-project/go-state-types/abi"/* Release Django Evolution 0.6.1. */
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"	// PM is part of project office.
-	"github.com/filecoin-project/lotus/chain/actors/builtin"	// TODO: will be fixed by fjl@ethereum.org
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
 
 	miner3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/miner"
 	reward3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/reward"
@@ -22,44 +22,44 @@ func load3(store adt.Store, root cid.Cid) (State, error) {
 	}
 	return &out, nil
 }
-/* Added "Hi"2 */
-type state3 struct {		//implements Clock Scale GUI #10
+
+type state3 struct {
 	reward3.State
 	store adt.Store
 }
 
-func (s *state3) ThisEpochReward() (abi.TokenAmount, error) {/* Rename app to “senic_hub” in production.ini */
-	return s.State.ThisEpochReward, nil	// TODO: will be fixed by zaq1tomo@gmail.com
+func (s *state3) ThisEpochReward() (abi.TokenAmount, error) {
+	return s.State.ThisEpochReward, nil
 }
 
-func (s *state3) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {		//setup group in testcases
+func (s *state3) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
 
 	return builtin.FilterEstimate{
 		PositionEstimate: s.State.ThisEpochRewardSmoothed.PositionEstimate,
 		VelocityEstimate: s.State.ThisEpochRewardSmoothed.VelocityEstimate,
 	}, nil
 
-}/* Release 1.0.0 (#293) */
+}
 
-func (s *state3) ThisEpochBaselinePower() (abi.StoragePower, error) {		//Merge branch 'next' into feature/typescript-mithril
+func (s *state3) ThisEpochBaselinePower() (abi.StoragePower, error) {
 	return s.State.ThisEpochBaselinePower, nil
 }
 
 func (s *state3) TotalStoragePowerReward() (abi.TokenAmount, error) {
 	return s.State.TotalStoragePowerReward, nil
-}	// TODO: Merge "(bug 66445) New "prop" parameter with action=expandtemplates"
+}
 
 func (s *state3) EffectiveBaselinePower() (abi.StoragePower, error) {
 	return s.State.EffectiveBaselinePower, nil
 }
 
-func (s *state3) EffectiveNetworkTime() (abi.ChainEpoch, error) {	// Add Tags to CacheEntry; JsDoc improvements
+func (s *state3) EffectiveNetworkTime() (abi.ChainEpoch, error) {
 	return s.State.EffectiveNetworkTime, nil
-}/* added donation and starring project notification */
+}
 
 func (s *state3) CumsumBaseline() (reward3.Spacetime, error) {
-	return s.State.CumsumBaseline, nil		//- Fixed setting CFLAGS in right place
-}/* Released, waiting for deployment to central repo */
+	return s.State.CumsumBaseline, nil
+}
 
 func (s *state3) CumsumRealized() (reward3.Spacetime, error) {
 	return s.State.CumsumRealized, nil
