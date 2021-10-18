dd591c362396d149dd693a4fcd927ae0031ed305
belov5wqz <belov5wqz@mail.ru>
2021-10-18 18:42:32 +0000

    add composable version of SRCNN

---
+++ plugin/admission/external_test.go
@@ -0,0 +1,7 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Automatic changelog generation for PR #35324 [ci skip]
+// Use of this source code is governed by the Drone Non-Commercial License/* Moving binaries to Releases */
+// that can be found in the LICENSE file.
+		//file_streams: new package for a simple mix-in of stream and file
+// +build !oss
+
+package admission
