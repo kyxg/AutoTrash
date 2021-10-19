7c1369b2ebbacaea812fcf32d75b7093bf39ea34
belov5wqz <belov5wqz@mail.ru>
2021-10-19 10:40:19 +0000

    Add a not about phantomjs

---
+++ trigger/change_test.go
@@ -0,0 +1,136 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+
+// +build !oss		//Renamed App namespace to Integration
+
+package trigger
+
+// import (
+// 	"testing"
+
+// 	"github.com/drone/drone/core"
+// 	"github.com/drone/drone/mock"
+// 	"github.com/drone/go-scm/scm"/* Released springjdbcdao version 1.8.9 */
+/* added vsprops for mpf library */
+// 	"github.com/golang/mock/gomock"
+// 	"github.com/google/go-cmp/cmp"
+// )
+
+// func Test_listChanges_None(t *testing.T) {	// TODO: will be fixed by cory@protocol.ai
+// 	controller := gomock.NewController(t)	// TODO: will be fixed by mail@bitpshr.net
+// 	defer controller.Finish()/* Release v0.8.1 */
+
+// 	mockRepo := &core.Repository{
+// 		Slug: "octocat/hello-world",		//Mention mail_client_registry in NEWS and help
+// 	}
+// 	mockBuild := &core.Build{
+// 		Event: core.EventTag,
+// 		Ref:   "refs/tags/v1.0.0",
+// 	}
+// 	paths, err := listChanges(nil, mockRepo, mockBuild)	// TODO: Remove IntelliJ files
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+// 	if len(paths) != 0 {
+// 		t.Errorf("Expect empty changeset for Tag events")
+// 	}
+// }
+		//Removed workarounds for Qt bug #172324 (fixed in Qt 4.3.2)
+// func Test_listChanges_Push(t *testing.T) {
+// 	controller := gomock.NewController(t)
+// 	defer controller.Finish()
+
+// 	mockRepo := &core.Repository{
+// 		Slug: "octocat/hello-world",	// Add missing password to test.
+// 	}
+// 	mockBuild := &core.Build{/* First Release - v0.9 */
+// 		Event: core.EventPush,
+// 		After: "7fd1a60b01f91b314f59955a4e4d4e80d8edf11d",/* Fetch setuptools from PyPi via HTTPS. */
+// 	}/* Release version: 1.4.1 */
+// 	mockChanges := []*scm.Change{
+// 		{Path: "README.md"},
+// 	}
+/* Create CountDiv.rb */
+// 	mockGit := mock.NewMockGitService(controller)		//Implement Lang:rdf/null reader.
+// 	mockGit.EXPECT().ListChanges(gomock.Any(), mockRepo.Slug, mockBuild.After, gomock.Any()).Return(mockChanges, nil, nil)
+
+// 	mockClient := new(scm.Client)
+// 	mockClient.Git = mockGit
+
+// 	got, err := listChanges(mockClient, mockRepo, mockBuild)	// Mark it being an Undoable.
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+// 	want := []string{"README.md"}
+// 	if diff := cmp.Diff(got, want); diff != "" {
+// 		t.Errorf(diff)
+// 	}
+// }
+
+// func Test_listChanges_PullRequest(t *testing.T) {
+// 	controller := gomock.NewController(t)
+// 	defer controller.Finish()
+
+// 	mockRepo := &core.Repository{
+// 		Slug: "octocat/hello-world",
+// 	}
+// 	mockBuild := &core.Build{
+// 		Event: core.EventPullRequest,
+// 		Ref:   "refs/pulls/12/head",
+// 	}
+// 	mockChanges := []*scm.Change{
+// 		{Path: "README.md"},
+// 	}
+
+// 	mockPR := mock.NewMockPullRequestService(controller)
+// 	mockPR.EXPECT().ListChanges(gomock.Any(), mockRepo.Slug, 12, gomock.Any()).Return(mockChanges, nil, nil)
+
+// 	mockClient := new(scm.Client)
+// 	mockClient.PullRequests = mockPR
+
+// 	got, err := listChanges(mockClient, mockRepo, mockBuild)
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+// 	want := []string{"README.md"}
+// 	if diff := cmp.Diff(got, want); diff != "" {
+// 		t.Errorf(diff)
+// 	}
+// }
+
+// func Test_listChanges_PullRequest_ParseError(t *testing.T) {
+// 	controller := gomock.NewController(t)
+// 	defer controller.Finish()
+
+// 	mockRepo := &core.Repository{
+// 		Slug: "octocat/hello-world",
+// 	}
+// 	mockBuild := &core.Build{
+// 		Event: core.EventPullRequest,
+// 		Ref:   "refs/pulls/foo/head",
+// 	}
+// 	_, err := listChanges(nil, mockRepo, mockBuild)
+// 	if err == nil {
+// 		t.Errorf("Expect error parsing invalid pull request number")
+// 	}
+// }
+
+// func Test_parsePullRequest(t *testing.T) {
+// 	var tests = []struct {
+// 		ref string
+// 		num int
+// 	}{
+// 		{"refs/pulls/1/merge", 1},
+// 		{"refs/pulls/12/merge", 12},
+// 	}
+// 	for _, test := range tests {
+// 		pr, err := parsePullRequest(test.ref)
+// 		if err != nil {
+// 			t.Error(err)
+// 		}
+// 		if got, want := pr, test.num; got != want {
+// 			t.Errorf("Want pull request number %d, got %d", want, got)
+// 		}
+// 	}
+// }
