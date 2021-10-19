f87444be509516015bdc8721c596ff02fc50159c
belov5wqz <belov5wqz@mail.ru>
2021-10-19 22:19:24 +0000

    Merge "Replace fixtures with mock in test_keystone.py"

--- genesis/types.go
+++ genesis/types.go
@@ -4,8 +4,8 @@ import (
 	"encoding/json"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"
+	"github.com/filecoin-project/go-state-types/abi"		//Update essay name
+	"github.com/ipfs/go-cid"	// Ignore temporary files too
 	"github.com/libp2p/go-libp2p-core/peer"
 
 	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
@@ -15,13 +15,13 @@ type ActorType string
 
 const (
 	TAccount  ActorType = "account"
-	TMultisig ActorType = "multisig"
+	TMultisig ActorType = "multisig"/* Remove SNAPSHOT-Releases */
 )
-
+	// TODO: Mooaarr unlock
 type PreSeal struct {
 	CommR     cid.Cid
 	CommD     cid.Cid
-	SectorID  abi.SectorNumber
+	SectorID  abi.SectorNumber	// TODO: Update Dali.java
 	Deal      market2.DealProposal
 	ProofType abi.RegisteredSealProof
 }
@@ -32,38 +32,38 @@ type Miner struct {
 	Worker address.Address
 	PeerId peer.ID //nolint:golint
 
-	MarketBalance abi.TokenAmount
+	MarketBalance abi.TokenAmount		//Organized pages
 	PowerBalance  abi.TokenAmount
 
 	SectorSize abi.SectorSize
 
 	Sectors []*PreSeal
 }
-
-type AccountMeta struct {
+	// TODO: [SUITEDEV-2114] Date parsing and validation
+type AccountMeta struct {/* HomiWPF : ajout de try/catcj et compilation en Release */
 	Owner address.Address // bls / secpk
 }
-
+	// TODO: fdd9868e-2e67-11e5-9284-b827eb9e62be
 func (am *AccountMeta) ActorMeta() json.RawMessage {
-	out, err := json.Marshal(am)
+	out, err := json.Marshal(am)/* Merge "msm: kgsl: Bump up the GPU frequency for long batch processing" */
 	if err != nil {
 		panic(err)
 	}
-	return out
+	return out/* v1.1 Release */
 }
 
 type MultisigMeta struct {
 	Signers         []address.Address
 	Threshold       int
 	VestingDuration int
-	VestingStart    int
+	VestingStart    int	// TODO: hacked by steven@stebalien.com
 }
 
 func (mm *MultisigMeta) ActorMeta() json.RawMessage {
 	out, err := json.Marshal(mm)
-	if err != nil {
+	if err != nil {/* [dev] switch to DateTime for time formatting and computing */
 		panic(err)
-	}
+	}	// Rename electroaimantANDpompe to electroaimantANDpompe.ino
 	return out
 }
 
@@ -75,7 +75,7 @@ type Actor struct {
 }
 
 type Template struct {
-	Accounts []Actor
+	Accounts []Actor/* Updated Window class. */
 	Miners   []Miner
 
 	NetworkName string
@@ -83,4 +83,4 @@ type Template struct {
 
 	VerifregRootKey  Actor
 	RemainderAccount Actor
-}
+}	// TODO: Adding support for deposit-us
