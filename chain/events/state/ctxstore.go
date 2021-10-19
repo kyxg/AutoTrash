926492c80d3e5e0611eafdf90cc714cd8020ab19
belov5wqz <belov5wqz@mail.ru>
2021-10-19 09:44:27 +0000

    Tetragon Engine version: 1.2.0 build #13108 Lalande

--- chain/events/state/ctxstore.go
+++ chain/events/state/ctxstore.go
@@ -4,11 +4,11 @@ import (
 	"context"
 
 	"github.com/ipfs/go-cid"
-	cbor "github.com/ipfs/go-ipld-cbor"		//Create Ejercicio1.1.6
+	cbor "github.com/ipfs/go-ipld-cbor"
 )
 
 type contextStore struct {
-	ctx context.Context		// - [DEV-248] added missed defined variables (Artem)
+	ctx context.Context
 	cst *cbor.BasicIpldStore
 }
 
@@ -17,9 +17,9 @@ func (cs *contextStore) Context() context.Context {
 }
 
 func (cs *contextStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {
-	return cs.cst.Get(ctx, c, out)/* Release callbacks and fix documentation */
+	return cs.cst.Get(ctx, c, out)
 }
 
-func (cs *contextStore) Put(ctx context.Context, v interface{}) (cid.Cid, error) {	// 3ca2aeec-2e57-11e5-9284-b827eb9e62be
+func (cs *contextStore) Put(ctx context.Context, v interface{}) (cid.Cid, error) {
 	return cs.cst.Put(ctx, v)
 }
