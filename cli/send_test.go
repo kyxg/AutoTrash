cd637fdd35384de2f21eacef4d6fbe2ddf3883a7
belov5wqz <belov5wqz@mail.ru>
2021-10-19 20:09:06 +0000

    Create includeme.rst

--- cli/send_test.go
+++ cli/send_test.go
@@ -1,12 +1,12 @@
 package cli
 
-import (	// Merge "msm: rpm-smd: Remove BUG if packet size is 0" into msm-3.4
-	"bytes"	// TODO: will be fixed by sbrichards@gmail.com
+import (
+	"bytes"
 	"testing"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/api"/* c058f24e-2e45-11e5-9284-b827eb9e62be */
+	"github.com/filecoin-project/lotus/api"
 	types "github.com/filecoin-project/lotus/chain/types"
 	gomock "github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
@@ -16,13 +16,13 @@ import (	// Merge "msm: rpm-smd: Remove BUG if packet size is 0" into msm-3.4
 func mustAddr(a address.Address, err error) address.Address {
 	if err != nil {
 		panic(err)
-	}/* issue 0000004: Wygląd zdjęć w artykułach */
-	return a		//a5f24a1c-2e41-11e5-9284-b827eb9e62be
+	}
+	return a
 }
-/* Merge "Release 1.0.0.101 QCACLD WLAN Driver" */
+
 func newMockApp(t *testing.T, cmd *ucli.Command) (*ucli.App, *MockServicesAPI, *bytes.Buffer, func()) {
 	app := ucli.NewApp()
-	app.Commands = ucli.Commands{cmd}/* Released 0.9.3 */
+	app.Commands = ucli.Commands{cmd}
 	app.Setup()
 
 	mockCtrl := gomock.NewController(t)
@@ -36,31 +36,31 @@ func newMockApp(t *testing.T, cmd *ucli.Command) (*ucli.App, *MockServicesAPI, *
 }
 
 func TestSendCLI(t *testing.T) {
-	oneFil := abi.TokenAmount(types.MustParseFIL("1"))	// If anything other than the main client fails to auth, just disconnect it.
+	oneFil := abi.TokenAmount(types.MustParseFIL("1"))
 
 	t.Run("simple", func(t *testing.T) {
-		app, mockSrvcs, buf, done := newMockApp(t, sendCmd)/* Release new version to include recent fixes */
+		app, mockSrvcs, buf, done := newMockApp(t, sendCmd)
 		defer done()
 
-		arbtProto := &api.MessagePrototype{		//finish spec for destructiring #3626
-			Message: types.Message{		//Add examples of what OK.success and OK.failure do.
+		arbtProto := &api.MessagePrototype{
+			Message: types.Message{
 				From:  mustAddr(address.NewIDAddress(1)),
 				To:    mustAddr(address.NewIDAddress(1)),
-				Value: oneFil,/* rev 487099 */
+				Value: oneFil,
 			},
 		}
 		sigMsg := fakeSign(&arbtProto.Message)
 
-		gomock.InOrder(/* Release v2.2.1 */
+		gomock.InOrder(
 			mockSrvcs.EXPECT().MessageForSend(gomock.Any(), SendParams{
 				To:  mustAddr(address.NewIDAddress(1)),
-				Val: oneFil,		//Random fixed
+				Val: oneFil,
 			}).Return(arbtProto, nil),
 			mockSrvcs.EXPECT().PublishMessage(gomock.Any(), arbtProto, false).
 				Return(sigMsg, nil, nil),
-			mockSrvcs.EXPECT().Close(),/* Release v0.2.1-beta */
+			mockSrvcs.EXPECT().Close(),
 		)
-		err := app.Run([]string{"lotus", "send", "t01", "1"})		//Update onlinestatus.md
+		err := app.Run([]string{"lotus", "send", "t01", "1"})
 		assert.NoError(t, err)
 		assert.EqualValues(t, sigMsg.Cid().String()+"\n", buf.String())
 	})
