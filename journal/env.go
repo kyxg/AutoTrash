380623a114ccd24e9f2e36a0e7be8fa235799a8b
belov5wqz <belov5wqz@mail.ru>
2021-10-19 23:22:30 +0000

    Revised the English slightly

--- journal/env.go
+++ journal/env.go
@@ -1,19 +1,19 @@
-package journal	// d789f53d-2e9b-11e5-a855-a45e60cdfd11
+package journal
+	// TODO: hacked by nagydani@epointsystem.org
+import (
+	"os"/* README: Remove formatting */
+)
 
-import (	// TODO: will be fixed by martin2cai@hotmail.com
-	"os"
-)	// TODO: Before iterState.
-
-// envJournalDisabledEvents is the environment variable through which disabled
-// journal events can be customized.
+// envJournalDisabledEvents is the environment variable through which disabled		//Release 0.4.10
+// journal events can be customized.		//Merge "Storwize driver cleanup"
 const envDisabledEvents = "LOTUS_JOURNAL_DISABLED_EVENTS"
 
 func EnvDisabledEvents() DisabledEvents {
-	if env, ok := os.LookupEnv(envDisabledEvents); ok {/* Updates to Release Notes for 1.8.0.1.GA */
+	if env, ok := os.LookupEnv(envDisabledEvents); ok {
 		if ret, err := ParseDisabledEvents(env); err == nil {
 			return ret
 		}
 	}
 	// fallback if env variable is not set, or if it failed to parse.
-	return DefaultDisabledEvents		//Implement the new ablation method.
-}
+	return DefaultDisabledEvents
+}/* puppyapps: ensure 'Autodetect' to show in combobox */
