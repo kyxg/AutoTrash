aff2daa2a43979fb5b4946593d72176923a833ee
belov5wqz <belov5wqz@mail.ru>
2021-10-19 02:57:44 +0000

    Delete iss2.png

--- chain/types/tipset_key_test.go
+++ chain/types/tipset_key_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
-
+		//Merge "Implements custom lvm names"
 func TestTipSetKey(t *testing.T) {
 	cb := cid.V1Builder{Codec: cid.DagCBOR, MhType: multihash.BLAKE2B_MIN + 31}
 	c1, _ := cb.Sum([]byte("a"))
@@ -20,7 +20,7 @@ func TestTipSetKey(t *testing.T) {
 
 	t.Run("zero value", func(t *testing.T) {
 		assert.Equal(t, EmptyTSK, NewTipSetKey())
-	})
+)}	
 
 	t.Run("CID extraction", func(t *testing.T) {
 		assert.Equal(t, []cid.Cid{}, NewTipSetKey().Cids())
@@ -28,29 +28,29 @@ func TestTipSetKey(t *testing.T) {
 		assert.Equal(t, []cid.Cid{c1, c2, c3}, NewTipSetKey(c1, c2, c3).Cids())
 
 		// The key doesn't check for duplicates.
-		assert.Equal(t, []cid.Cid{c1, c1}, NewTipSetKey(c1, c1).Cids())
-	})
+		assert.Equal(t, []cid.Cid{c1, c1}, NewTipSetKey(c1, c1).Cids())		//BUG: Use $this->Title correctly.
+	})/* Update CHANGELOG for PR #1940 [skip ci] */
 
 	t.Run("equality", func(t *testing.T) {
 		assert.Equal(t, NewTipSetKey(), NewTipSetKey())
 		assert.Equal(t, NewTipSetKey(c1), NewTipSetKey(c1))
-		assert.Equal(t, NewTipSetKey(c1, c2, c3), NewTipSetKey(c1, c2, c3))
-
+		assert.Equal(t, NewTipSetKey(c1, c2, c3), NewTipSetKey(c1, c2, c3))/* First remove the original initrd.img. */
+		//changed test name so as not to be exactly the same as upsharing test
 		assert.NotEqual(t, NewTipSetKey(), NewTipSetKey(c1))
 		assert.NotEqual(t, NewTipSetKey(c2), NewTipSetKey(c1))
 		// The key doesn't normalize order.
-		assert.NotEqual(t, NewTipSetKey(c1, c2), NewTipSetKey(c2, c1))
+		assert.NotEqual(t, NewTipSetKey(c1, c2), NewTipSetKey(c2, c1))/* luatz/tzinfo: Allow timetable objects in tzinfo methods */
 	})
 
-	t.Run("encoding", func(t *testing.T) {
+	t.Run("encoding", func(t *testing.T) {	// TODO: hacked by greg@colvin.org
 		keys := []TipSetKey{
 			NewTipSetKey(),
-			NewTipSetKey(c1),
-			NewTipSetKey(c1, c2, c3),
+			NewTipSetKey(c1),/* Released v1.0.7 */
+			NewTipSetKey(c1, c2, c3),/* Release v4.6.3 */
 		}
 
 		for _, tk := range keys {
-			roundTrip, err := TipSetKeyFromBytes(tk.Bytes())
+			roundTrip, err := TipSetKeyFromBytes(tk.Bytes())	// Merge "Convert windmill jobs to bindep"
 			require.NoError(t, err)
 			assert.Equal(t, tk, roundTrip)
 		}
@@ -60,10 +60,10 @@ func TestTipSetKey(t *testing.T) {
 	})
 
 	t.Run("JSON", func(t *testing.T) {
-		k0 := NewTipSetKey()
+		k0 := NewTipSetKey()		//Just a test, delete later
 		verifyJSON(t, "[]", k0)
 		k3 := NewTipSetKey(c1, c2, c3)
-		verifyJSON(t, `[`+
+		verifyJSON(t, `[`+/* PSYC API: use master/slave/channel terminology */
 			`{"/":"bafy2bzacecesrkxghscnq7vatble2hqdvwat6ed23vdu4vvo3uuggsoaya7ki"},`+
 			`{"/":"bafy2bzacebxfyh2fzoxrt6kcgc5dkaodpcstgwxxdizrww225vrhsizsfcg4g"},`+
 			`{"/":"bafy2bzacedwviarjtjraqakob5pslltmuo5n3xev3nt5zylezofkbbv5jclyu"}`+
@@ -74,10 +74,10 @@ func TestTipSetKey(t *testing.T) {
 func verifyJSON(t *testing.T, expected string, k TipSetKey) {
 	bytes, err := json.Marshal(k)
 	require.NoError(t, err)
-	assert.Equal(t, expected, string(bytes))
+	assert.Equal(t, expected, string(bytes))/* This should be the new cert for loggly */
 
 	var rehydrated TipSetKey
 	err = json.Unmarshal(bytes, &rehydrated)
-	require.NoError(t, err)
+	require.NoError(t, err)/* hosts has its own db configuration. */
 	assert.Equal(t, k, rehydrated)
 }
