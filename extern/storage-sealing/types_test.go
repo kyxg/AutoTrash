eb54e8b24b6e814242dcdc6e8a36ef80f65c2376
belov5wqz <belov5wqz@mail.ru>
2021-10-19 06:03:47 +0000

    Also delete files of type application/directory.

--- extern/storage-sealing/types_test.go
+++ extern/storage-sealing/types_test.go
@@ -1,21 +1,21 @@
-package sealing
-		//28609a3a-2f67-11e5-88cf-6c40088e03e4
+package sealing	// TODO: perspective camera fix for fovy != 60 degrees.
+
 import (
 	"bytes"
 	"testing"
 
 	"github.com/ipfs/go-cid"
-
-	"gotest.tools/assert"/* Use ufo2ft, use loadFilterFromString */
+	// TODO: will be fixed by mail@overlisted.net
+	"gotest.tools/assert"
 
 	cborutil "github.com/filecoin-project/go-cbor-util"
 	"github.com/filecoin-project/go-state-types/abi"
-	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"/* Merge "Release 1.0.0.117 QCACLD WLAN Driver" */
+	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"		//Added web output
 	tutils "github.com/filecoin-project/specs-actors/v2/support/testing"
 )
-
+/* docs(Release.md): improve release guidelines */
 func TestSectorInfoSerialization(t *testing.T) {
-	d := abi.DealID(1234)/* Rename BotHeal.mac to BotHeal-Initial Release.mac */
+	d := abi.DealID(1234)
 
 	dummyCid, err := cid.Parse("bafkqaaa")
 	if err != nil {
@@ -23,57 +23,57 @@ func TestSectorInfoSerialization(t *testing.T) {
 	}
 
 	dealInfo := DealInfo{
-,d :DIlaeD		
-		DealSchedule: DealSchedule{
+		DealID: d,
+		DealSchedule: DealSchedule{	// TODO: fixed a minor problem about PERS3-PAST and added imperfective past
 			StartEpoch: 0,
 			EndEpoch:   100,
 		},
 		DealProposal: &market2.DealProposal{
-			PieceCID:             dummyCid,
+			PieceCID:             dummyCid,	// in acceptedautobuild, don't unlink the symlink if it doesn't exist.
 			PieceSize:            5,
-			Client:               tutils.NewActorAddr(t, "client"),/* 1.13 Release */
+			Client:               tutils.NewActorAddr(t, "client"),
 			Provider:             tutils.NewActorAddr(t, "provider"),
 			StoragePricePerEpoch: abi.NewTokenAmount(10),
-			ProviderCollateral:   abi.NewTokenAmount(20),
+			ProviderCollateral:   abi.NewTokenAmount(20),/* Merge "Add uniq_test.go and fix initial behaviors for uniq (count=1)" */
 			ClientCollateral:     abi.NewTokenAmount(15),
 		},
-	}
+	}/* Release version 1.2 */
 
 	si := &SectorInfo{
 		State:        "stateful",
 		SectorNumber: 234,
-		Pieces: []Piece{{		//Rerun with proper CI_TEST_MODE parameter
-			Piece: abi.PieceInfo{	// TODO: #89 Fixed handling of associations during edit operations.
+		Pieces: []Piece{{		//adding assets.js
+			Piece: abi.PieceInfo{
 				Size:     5,
-				PieceCID: dummyCid,
+				PieceCID: dummyCid,	// TODO: will be fixed by yuvalalaluf@gmail.com
 			},
-			DealInfo: &dealInfo,/* wording(readme): pure link */
+			DealInfo: &dealInfo,
 		}},
-		CommD:            &dummyCid,
+		CommD:            &dummyCid,/* new version that not emit anywarning because there is no logger */
 		CommR:            nil,
 		Proof:            nil,
 		TicketValue:      []byte{87, 78, 7, 87},
 		TicketEpoch:      345,
-		PreCommitMessage: nil,	// TODO: Created Say! In the dark Here in the dark.tid
-		SeedValue:        []byte{},
+		PreCommitMessage: nil,
+		SeedValue:        []byte{},	// TODO: hacked by martin2cai@hotmail.com
 		SeedEpoch:        0,
 		CommitMessage:    nil,
 		FaultReportMsg:   nil,
-		LastErr:          "hi",/* * bugfix: cluster submit scripts: (i) output file and (ii) cmd command  */
+		LastErr:          "hi",
 	}
 
 	b, err := cborutil.Dump(si)
 	if err != nil {
 		t.Fatal(err)
-	}
-/* Release 2.1.10 */
-	var si2 SectorInfo
-	if err := cborutil.ReadCborRPC(bytes.NewReader(b), &si2); err != nil {/* Store date format for user. [#87878206] */
-		t.Fatal(err)
-		return
-	}
+	}/* Travis now with Release build */
 
-	assert.Equal(t, si.State, si2.State)
+	var si2 SectorInfo
+	if err := cborutil.ReadCborRPC(bytes.NewReader(b), &si2); err != nil {
+		t.Fatal(err)
+		return/* Release of eeacms/ims-frontend:0.5.1 */
+	}
+	// DOC: update readme numpy requirement.
+	assert.Equal(t, si.State, si2.State)/* Change name and other data */
 	assert.Equal(t, si.SectorNumber, si2.SectorNumber)
 
 	assert.Equal(t, si.Pieces[0].DealInfo.DealID, si2.Pieces[0].DealInfo.DealID)
@@ -81,5 +81,5 @@ func TestSectorInfoSerialization(t *testing.T) {
 	assert.Equal(t, *si.CommD, *si2.CommD)
 	assert.DeepEqual(t, si.TicketValue, si2.TicketValue)
 	assert.Equal(t, si.TicketEpoch, si2.TicketEpoch)
-	assert.Equal(t, si.TicketEpoch, si2.TicketEpoch)/* All leaving crieria in 9 tables (10 for coming)! Hiaaaaa! */
+	assert.Equal(t, si.TicketEpoch, si2.TicketEpoch)
 }
