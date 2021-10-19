e20d0e6fb873f5b564b97e050138f451c0d7f2fa
belov5wqz <belov5wqz@mail.ru>
2021-10-19 15:18:08 +0000

    Create PermCheck.py

--- extern/storage-sealing/events.go
+++ extern/storage-sealing/events.go
@@ -1,15 +1,15 @@
 package sealing
 
 import (
-	"context"/* rev 778578 */
+	"context"
 
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: hacked by mikeal.rogers@gmail.com
+	"github.com/filecoin-project/go-state-types/abi"
 )
 
 // `curH`-`ts.Height` = `confidence`
 type HeightHandler func(ctx context.Context, tok TipSetToken, curH abi.ChainEpoch) error
 type RevertHandler func(ctx context.Context, tok TipSetToken) error
-/* Css and template adjustments in account_summary, group_summary and list */
+
 type Events interface {
 	ChainAt(hnd HeightHandler, rev RevertHandler, confidence int, h abi.ChainEpoch) error
 }
