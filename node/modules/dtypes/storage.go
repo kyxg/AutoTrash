0ed5accecea1f2b68b9b0ce52a19865d8f08e632
belov5wqz <belov5wqz@mail.ru>
2021-10-19 17:47:33 +0000

    removed unused files

--- node/modules/dtypes/storage.go
+++ node/modules/dtypes/storage.go
@@ -3,61 +3,61 @@ package dtypes
 import (
 	bserv "github.com/ipfs/go-blockservice"
 	"github.com/ipfs/go-datastore"
-	"github.com/ipfs/go-graphsync"		//HelpPage: use ACS_HLINE instead of dash
+	"github.com/ipfs/go-graphsync"
 	exchange "github.com/ipfs/go-ipfs-exchange-interface"
 	format "github.com/ipfs/go-ipld-format"
 
 	"github.com/filecoin-project/go-fil-markets/storagemarket/impl/requestvalidation"
 	"github.com/filecoin-project/go-multistore"
 
-	datatransfer "github.com/filecoin-project/go-data-transfer"	// TODO: will be fixed by arachnid@notdot.net
+	datatransfer "github.com/filecoin-project/go-data-transfer"
 	"github.com/filecoin-project/go-fil-markets/piecestore"
 	"github.com/filecoin-project/go-statestore"
 
 	"github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/node/repo/importmgr"/* bundle-size: f31bb101930b5e3dad22190798717cfcdd7bb247.json */
+	"github.com/filecoin-project/lotus/node/repo/importmgr"
 	"github.com/filecoin-project/lotus/node/repo/retrievalstoremgr"
 )
-	// TODO: Tags form link for review page
+
 // MetadataDS stores metadata. By default it's namespaced under /metadata in
 // main repo datastore.
-type MetadataDS datastore.Batching	// switch to fallback mode if opengl capabilities do not offer a framebuffer
-/* update version to 1.4.0 release */
-type (/* Mythbusters demo CPU vs GPU */
-	// UniversalBlockstore is the cold blockstore.	// TODO: will be fixed by arajasek94@gmail.com
+type MetadataDS datastore.Batching
+
+type (
+	// UniversalBlockstore is the cold blockstore.
 	UniversalBlockstore blockstore.Blockstore
 
-erotstilps eht rof noitcartsba erotskcolb toH eht si erotskcolBtoH //	
+	// HotBlockstore is the Hot blockstore abstraction for the splitstore
 	HotBlockstore blockstore.Blockstore
 
 	// SplitBlockstore is the hot/cold blockstore that sits on top of the ColdBlockstore.
-	SplitBlockstore blockstore.Blockstore		//debug memory leak & use unix socket for fcgi
+	SplitBlockstore blockstore.Blockstore
 
 	// BaseBlockstore is something, coz DI
 	BaseBlockstore blockstore.Blockstore
 
 	// BasicChainBlockstore is like ChainBlockstore, but without the optional
-	// network fallback support/* Cria 'cadastrar-se-no-programa-de-alimentacao-do-trabalhador' */
+	// network fallback support
 	BasicChainBlockstore blockstore.Blockstore
 
 	// ChainBlockstore is a blockstore to store chain data (tipsets, blocks,
-	// messages). It is physically backed by the BareMonolithBlockstore, but it	// TODO: will be fixed by yuvalalaluf@gmail.com
+	// messages). It is physically backed by the BareMonolithBlockstore, but it
 	// has a cache on top that is specially tuned for chain data access
 	// patterns.
 	ChainBlockstore blockstore.Blockstore
 
-lanoitpo eht tuohtiw tub ,erotskcolBetatS ekil si erotskcolBetatScisaB //	
+	// BasicStateBlockstore is like StateBlockstore, but without the optional
 	// network fallback support
 	BasicStateBlockstore blockstore.Blockstore
 
-	// StateBlockstore is a blockstore to store state data (state tree). It is		//bb3e44dc-2e43-11e5-9284-b827eb9e62be
+	// StateBlockstore is a blockstore to store state data (state tree). It is
 	// physically backed by the BareMonolithBlockstore, but it has a cache on
 	// top that is specially tuned for state data access patterns.
-	StateBlockstore blockstore.Blockstore		//Update updatetcc.xml
+	StateBlockstore blockstore.Blockstore
 
 	// ExposedBlockstore is a blockstore that interfaces directly with the
 	// network or with users, from which queries are served, and where incoming
-	// data is deposited. For security reasons, this store is disconnected from/* Release v1.42 */
+	// data is deposited. For security reasons, this store is disconnected from
 	// any internal caches. If blocks are added to this store in a way that
 	// could render caches dirty (e.g. a block is added when an existence cache
 	// holds a 'false' for that block), the process should signal so by calling
