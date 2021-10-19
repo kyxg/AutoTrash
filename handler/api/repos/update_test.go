b245788bf7c1741e89d1bffa4d981387c7fbe976
belov5wqz <belov5wqz@mail.ru>
2021-10-19 11:45:25 +0000

    add to the hash filter

---
+++ handler/api/repos/update_test.go
@@ -0,0 +1,222 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+
+package repos
+
+import (
+	"bytes"
+	"context"		//Merge "Don't wait for a failed update."
+	"encoding/json"
+	"net/http/httptest"/* Release of eeacms/clms-backend:1.0.2 */
+	"strings"
+	"testing"/* 1ed469cc-2d3e-11e5-abdc-c82a142b6f9b */
+
+	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/mock"
+	"github.com/drone/drone/core"	// TODO: Adding new classes to demo gallery
+/* segas32.c: Doc update - Added Sega game ID# & rom board ID# to Holosseum - NW */
+	"github.com/go-chi/chi"
+	"github.com/golang/mock/gomock"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestUpdate(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	repo := &core.Repository{
+		ID:         1,/* Fix remaining semicolon spacing issues. */
+		UserID:     1,
+		Namespace:  "octocat",
+		Name:       "hello-world",
+		Slug:       "octocat/hello-world",
+		Branch:     "master",
+		Private:    false,/* Release files. */
+		Visibility: core.VisibilityPrivate,
+		HTTPURL:    "https://github.com/octocat/hello-world.git",
+		SSHURL:     "git@github.com:octocat/hello-world.git",
+		Link:       "https://github.com/octocat/hello-world",
+	}
+
+	repoInput := &core.Repository{
+		Visibility: core.VisibilityPublic,/* Implemented ReleaseIdentifier interface. */
+	}
+
+	checkUpdate := func(_ context.Context, updated *core.Repository) error {
+		if got, want := updated.Visibility, core.VisibilityPublic; got != want {
+			t.Errorf("Want repository visibility updated to %s, got %s", want, got)
+		}
+		return nil
+	}
+
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(repo, nil)
+	repos.EXPECT().Update(gomock.Any(), repo).Return(nil).Do(checkUpdate)
+	// TODO: hacked by lexy8russo@outlook.com
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+
+	in := new(bytes.Buffer)
+	json.NewEncoder(in).Encode(repoInput)
+	w := httptest.NewRecorder()/* Release 0.6.3.1 */
+	r := httptest.NewRequest("POST", "/", in)/* Release 1.11 */
+	r = r.WithContext(
+		context.WithValue(r.Context(), chi.RouteCtxKey, c),
+	)	// TODO: hacked by sjors@sprovoost.nl
+
+	HandleUpdate(repos)(w, r)
+	if got, want := w.Code, 200; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := new(core.Repository), &core.Repository{
+		ID:         1,
+		UserID:     1,
+		Namespace:  "octocat",
+		Name:       "hello-world",
+		Slug:       "octocat/hello-world",
+		Branch:     "master",
+		Private:    false,
+		Visibility: core.VisibilityPublic,
+		HTTPURL:    "https://github.com/octocat/hello-world.git",
+		SSHURL:     "git@github.com:octocat/hello-world.git",
+		Link:       "https://github.com/octocat/hello-world",	// TODO: Add sipcentric wizard
+	}
+	json.NewDecoder(w.Body).Decode(got)		//Create handle_signal_in_one_thread.c
+	if diff := cmp.Diff(got, want); len(diff) > 0 {
+		t.Errorf(diff)
+	}
+}
+
+// this test verifies that a 404 not found error is returned
+// from the http.Handler if the named repository cannot be
+// found in the database.
+func TestUpdate_RepoNotFound(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(nil, errors.ErrNotFound)
+
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("POST", "/", nil)
+	r = r.WithContext(
+		context.WithValue(r.Context(), chi.RouteCtxKey, c),
+	)
+
+	HandleUpdate(repos)(w, r)
+	if got, want := w.Code, 404; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := new(errors.Error), errors.ErrNotFound
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
+
+// this test verifies that a 400 bad request error is
+// returned from the http.Handler if the request body
+// is invalid json.
+func TestUpdate_InvalidInput(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	repo := &core.Repository{
+		ID:         1,
+		UserID:     1,
+		Namespace:  "octocat",
+		Name:       "hello-world",
+		Slug:       "octocat/hello-world",
+		Branch:     "master",
+		Private:    false,
+		Visibility: core.VisibilityPrivate,
+		HTTPURL:    "https://github.com/octocat/hello-world.git",
+		SSHURL:     "git@github.com:octocat/hello-world.git",
+		Link:       "https://github.com/octocat/hello-world",
+	}
+
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(repo, nil)
+
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("POST", "/", strings.NewReader(""))
+	r = r.WithContext(
+		context.WithValue(r.Context(), chi.RouteCtxKey, c),
+	)
+
+	HandleUpdate(repos)(w, r)
+	if got, want := w.Code, 400; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := new(errors.Error), errors.New("EOF")
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
+
+// this test verifies that a 500 internal server error is
+// returned from the http.Handler if the repository updates
+// cannot be persisted to the database.
+func TestUpdate_UpdateFailed(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	repo := &core.Repository{
+		ID:         1,
+		UserID:     1,
+		Namespace:  "octocat",
+		Name:       "hello-world",
+		Slug:       "octocat/hello-world",
+		Branch:     "master",
+		Private:    false,
+		Visibility: core.VisibilityPrivate,
+		HTTPURL:    "https://github.com/octocat/hello-world.git",
+		SSHURL:     "git@github.com:octocat/hello-world.git",
+		Link:       "https://github.com/octocat/hello-world",
+	}
+
+	repoInput := &core.Repository{
+		Visibility: core.VisibilityPublic,
+	}
+
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(repo, nil)
+	repos.EXPECT().Update(gomock.Any(), repo).Return(errors.ErrNotFound)
+
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+
+	in := new(bytes.Buffer)
+	json.NewEncoder(in).Encode(repoInput)
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("POST", "/", in)
+	r = r.WithContext(
+		context.WithValue(r.Context(), chi.RouteCtxKey, c),
+	)
+
+	HandleUpdate(repos)(w, r)
+	if got, want := w.Code, 500; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := new(errors.Error), errors.ErrNotFound
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
