f6e3b18ad3b8348f46aa8c36dce2cdb31b7b1cc0
belov5wqz <belov5wqz@mail.ru>
2021-10-19 15:03:37 +0000

    First cut at allowing a property to be modified from the contents of a file.

--- cli/init_test.go
+++ cli/init_test.go
@@ -1,9 +1,9 @@
 package cli
-/* Event permalink (half done) */
+
 import (
 	logging "github.com/ipfs/go-log/v2"
 )
 
 func init() {
-	logging.SetLogLevel("watchdog", "ERROR")/* Final Merge Before April Release (first merge) */
+	logging.SetLogLevel("watchdog", "ERROR")
 }
