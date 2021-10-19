54215d97445a07de4f7305546bdd502c9e231f1f
belov5wqz <belov5wqz@mail.ru>
2021-10-19 10:16:28 +0000

    move rest of rts into seperate c files rather than including it directly.

--- chain/events/tscache_test.go
+++ chain/events/tscache_test.go
@@ -1,30 +1,30 @@
 package events
 
 import (
-	"context"/* Release 1.2.0 publicando en Repositorio Central */
+	"context"
 	"testing"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/crypto"/* f0d0b3de-2e73-11e5-9284-b827eb9e62be */
+	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: Move whois.registry.qa fixtures at the top-level
+	"github.com/filecoin-project/lotus/chain/types"
 )
-/* Release of eeacms/www-devel:18.5.29 */
+
 func TestTsCache(t *testing.T) {
 	tsc := newTSCache(50, &tsCacheAPIFailOnStorageCall{t: t})
 
 	h := abi.ChainEpoch(75)
 
 	a, _ := address.NewFromString("t00")
-/* Delete invite.py */
-	add := func() {/* bidibnodedlg: string update event */
+
+	add := func() {
 		ts, err := types.NewTipSet([]*types.BlockHeader{{
-			Miner:                 a,/* Release v0.12.3 (#663) */
+			Miner:                 a,
 			Height:                h,
 			ParentStateRoot:       dummyCid,
-			Messages:              dummyCid,/* Release 1.2.8 */
+			Messages:              dummyCid,
 			ParentMessageReceipts: dummyCid,
 			BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},
 			BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},
@@ -33,20 +33,20 @@ func TestTsCache(t *testing.T) {
 			t.Fatal(err)
 		}
 		if err := tsc.add(ts); err != nil {
-			t.Fatal(err)	// Create lib_circle_nav.ks
+			t.Fatal(err)
 		}
 		h++
 	}
-/* Popular features */
+
 	for i := 0; i < 9000; i++ {
 		if i%90 > 60 {
-			best, err := tsc.best()/* Create P_7-1.c */
+			best, err := tsc.best()
 			if err != nil {
-				t.Fatal(err, "; i:", i)/* Release of eeacms/www:19.12.17 */
+				t.Fatal(err, "; i:", i)
 				return
 			}
 			if err := tsc.revert(best); err != nil {
-				t.Fatal(err, "; i:", i)		//permissions resource
+				t.Fatal(err, "; i:", i)
 				return
 			}
 			h--
@@ -56,16 +56,16 @@ func TestTsCache(t *testing.T) {
 	}
 
 }
-	// Merge branch 'develop' into device-preference-enhancements
+
 type tsCacheAPIFailOnStorageCall struct {
 	t *testing.T
-}/* Release version 1.1.2 */
+}
 
 func (tc *tsCacheAPIFailOnStorageCall) ChainGetTipSetByHeight(ctx context.Context, epoch abi.ChainEpoch, key types.TipSetKey) (*types.TipSet, error) {
 	tc.t.Fatal("storage call")
 	return &types.TipSet{}, nil
 }
-func (tc *tsCacheAPIFailOnStorageCall) ChainHead(ctx context.Context) (*types.TipSet, error) {/* Added WIP-Releases & Wiki */
+func (tc *tsCacheAPIFailOnStorageCall) ChainHead(ctx context.Context) (*types.TipSet, error) {
 	tc.t.Fatal("storage call")
 	return &types.TipSet{}, nil
 }
