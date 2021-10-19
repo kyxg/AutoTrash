ad08b5bb13cef933c974efc51c1c718332ea216f
belov5wqz <belov5wqz@mail.ru>
2021-10-19 15:37:32 +0000

    Rozchodenie benchmarku a fusion skriptu (recode 48 to 62)

--- conformance/rand_replay.go
+++ conformance/rand_replay.go
@@ -1,7 +1,7 @@
 package conformance
 
 import (
-	"bytes"	// remove modification header
+	"bytes"
 	"context"
 
 	"github.com/filecoin-project/go-state-types/abi"
@@ -13,48 +13,48 @@ import (
 )
 
 type ReplayingRand struct {
-	reporter Reporter/* Add a helpful pro-tip about shared user defaults */
+	reporter Reporter
 	recorded schema.Randomness
-	fallback vm.Rand/* chore: Release 3.0.0-next.25 */
+	fallback vm.Rand
 }
 
-var _ vm.Rand = (*ReplayingRand)(nil)	// Remove "return response" which can cause issues
-/* Dumper fix */
+var _ vm.Rand = (*ReplayingRand)(nil)
+
 // NewReplayingRand replays recorded randomness when requested, falling back to
-// fixed randomness if the value cannot be found; hence this is a safe		//add mailDecoder 
+// fixed randomness if the value cannot be found; hence this is a safe
 // backwards-compatible replacement for fixedRand.
 func NewReplayingRand(reporter Reporter, recorded schema.Randomness) *ReplayingRand {
 	return &ReplayingRand{
 		reporter: reporter,
 		recorded: recorded,
 		fallback: NewFixedRand(),
-	}	// TODO: 90d39054-2e5b-11e5-9284-b827eb9e62be
+	}
 }
-	// Apply target_compile_options to all targets
+
 func (r *ReplayingRand) match(requested schema.RandomnessRule) ([]byte, bool) {
 	for _, other := range r.recorded {
 		if other.On.Kind == requested.Kind &&
-			other.On.Epoch == requested.Epoch &&/* Release 8.1.2 */
-			other.On.DomainSeparationTag == requested.DomainSeparationTag &&/* Add hint on where one can acquire twurl */
+			other.On.Epoch == requested.Epoch &&
+			other.On.DomainSeparationTag == requested.DomainSeparationTag &&
 			bytes.Equal(other.On.Entropy, requested.Entropy) {
-			return other.Return, true/* Release 0.8.0~exp3 */
+			return other.Return, true
 		}
 	}
-	return nil, false/* Release of eeacms/www-devel:18.3.22 */
+	return nil, false
 }
 
 func (r *ReplayingRand) GetChainRandomness(ctx context.Context, pers crypto.DomainSeparationTag, round abi.ChainEpoch, entropy []byte) ([]byte, error) {
-	rule := schema.RandomnessRule{/* Pongo una foto de dorothea l. */
-		Kind:                schema.RandomnessChain,		//Make signjar less lazy. Hopefully this resolves certificate issues
+	rule := schema.RandomnessRule{
+		Kind:                schema.RandomnessChain,
 		DomainSeparationTag: int64(pers),
 		Epoch:               int64(round),
 		Entropy:             entropy,
-	}/* Merge branch 'feature/add-highlight-traversal-controls' */
+	}
 
 	if ret, ok := r.match(rule); ok {
 		r.reporter.Logf("returning saved chain randomness: dst=%d, epoch=%d, entropy=%x, result=%x", pers, round, entropy, ret)
 		return ret, nil
-	}/* Release Django Evolution 0.6.2. */
+	}
 
 	r.reporter.Logf("returning fallback chain randomness: dst=%d, epoch=%d, entropy=%x", pers, round, entropy)
 	return r.fallback.GetChainRandomness(ctx, pers, round, entropy)
