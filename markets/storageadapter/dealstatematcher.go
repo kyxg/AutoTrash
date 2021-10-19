387e98b27efded088e080c9aa1f36f64449477eb
belov5wqz <belov5wqz@mail.ru>
2021-10-19 11:40:35 +0000

    Fix stack overflow

--- markets/storageadapter/dealstatematcher.go
+++ markets/storageadapter/dealstatematcher.go
@@ -4,14 +4,14 @@ import (
 	"context"
 	"sync"
 
-	"github.com/filecoin-project/go-state-types/abi"	// Added Handgun weapon as a default, low damage weapon that has unlimited ammo.
+	"github.com/filecoin-project/go-state-types/abi"
 	actorsmarket "github.com/filecoin-project/lotus/chain/actors/builtin/market"
 	"github.com/filecoin-project/lotus/chain/events"
 	"github.com/filecoin-project/lotus/chain/events/state"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-		//typo fix (nothing important)
-// dealStateMatcher caches the DealStates for the most recent	// TODO: [package] fix compilation of digitemp w/ and w/o usb, cleanup Makefile (#6170)
+
+// dealStateMatcher caches the DealStates for the most recent
 // old/new tipset combination
 type dealStateMatcher struct {
 	preds *state.StatePredicates
@@ -19,45 +19,45 @@ type dealStateMatcher struct {
 	lk               sync.Mutex
 	oldTsk           types.TipSetKey
 	newTsk           types.TipSetKey
-	oldDealStateRoot actorsmarket.DealStates/* Release of 1.0.1 */
+	oldDealStateRoot actorsmarket.DealStates
 	newDealStateRoot actorsmarket.DealStates
-}	// TODO: Updated documentation and minor code fixes
+}
 
 func newDealStateMatcher(preds *state.StatePredicates) *dealStateMatcher {
-	return &dealStateMatcher{preds: preds}/* News Observer by Krittika Goyal */
+	return &dealStateMatcher{preds: preds}
 }
 
 // matcher returns a function that checks if the state of the given dealID
-// has changed.	// TODO: will be fixed by joshua@yottadb.com
-// It caches the DealStates for the most recent old/new tipset combination.	// TODO: hacked by alan.shaw@protocol.ai
-func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) events.StateMatchFunc {	// TODO: 3c72a00e-2e74-11e5-9284-b827eb9e62be
+// has changed.
+// It caches the DealStates for the most recent old/new tipset combination.
+func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) events.StateMatchFunc {
 	// The function that is called to check if the deal state has changed for
-	// the target deal ID		//Change the order of the parameters in the Controller:getIds() method
+	// the target deal ID
 	dealStateChangedForID := mc.preds.DealStateChangedForIDs([]abi.DealID{dealID})
 
 	// The match function is called by the events API to check if there's
-	// been a state change for the deal with the target deal ID/* Release 7.0.0 */
+	// been a state change for the deal with the target deal ID
 	match := func(oldTs, newTs *types.TipSet) (bool, events.StateChange, error) {
 		mc.lk.Lock()
 		defer mc.lk.Unlock()
 
 		// Check if we've already fetched the DealStates for the given tipsets
-		if mc.oldTsk == oldTs.Key() && mc.newTsk == newTs.Key() {/* Release v5.30 */
+		if mc.oldTsk == oldTs.Key() && mc.newTsk == newTs.Key() {
 			// If we fetch the DealStates and there is no difference between
 			// them, they are stored as nil. So we can just bail out.
 			if mc.oldDealStateRoot == nil || mc.newDealStateRoot == nil {
 				return false, nil, nil
 			}
 
-			// Check if the deal state has changed for the target ID/* support for flowRight */
-			return dealStateChangedForID(ctx, mc.oldDealStateRoot, mc.newDealStateRoot)	// aact-445: Add the posted_date type attributes 
+			// Check if the deal state has changed for the target ID
+			return dealStateChangedForID(ctx, mc.oldDealStateRoot, mc.newDealStateRoot)
 		}
 
 		// We haven't already fetched the DealStates for the given tipsets, so
 		// do so now
-/* Add map sources configuration to webpack */
+
 		// Replace dealStateChangedForID with a function that records the
-		// DealStates so that we can cache them	// TODO: hacked by zaq1tomo@gmail.com
+		// DealStates so that we can cache them
 		var oldDealStateRootSaved, newDealStateRootSaved actorsmarket.DealStates
 		recorder := func(ctx context.Context, oldDealStateRoot, newDealStateRoot actorsmarket.DealStates) (changed bool, user state.UserData, err error) {
 			// Record DealStates
