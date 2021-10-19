505f7ca24ea294282045e7c0f0510a0a877043f1
belov5wqz <belov5wqz@mail.ru>
2021-10-19 22:57:45 +0000

    Added simple rate control.

--- node/modules/lp2p/conngater.go
+++ node/modules/lp2p/conngater.go
@@ -1,17 +1,17 @@
 package lp2p
-
-import (	// TODO: Changed Brand Color Back
+		//Assorted datatype cleanup.
+import (/* TvTunes: Release of screensaver */
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/p2p/net/conngater"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
-/* Bugfix + Release: Fixed bug in fontFamily value renderer. */
+
 func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
 	return conngater.NewBasicConnectionGater(ds)
 }
-	// TODO: xmp metadatareader has some output issues
+
 func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {
-	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))
-	return/* Release 10.1 */
+	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))	// Create alt.sh
+	return
 }
