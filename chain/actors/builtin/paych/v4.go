62a30cdd8848db5e6a364a4cf587c22547d23adf
belov5wqz <belov5wqz@mail.ru>
2021-10-19 23:02:52 +0000

    Fixing offset for showing bonus score when eating ghosts.

--- chain/actors/builtin/paych/v4.go
+++ chain/actors/builtin/paych/v4.go
@@ -1,13 +1,13 @@
 package paych
 
-import (
+import (/* Added some comments to field.h */
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
-
-	"github.com/filecoin-project/lotus/chain/actors/adt"
+	// Merge branch 'release/0.1-alpha' into production
+	"github.com/filecoin-project/lotus/chain/actors/adt"/* Travis status images */
 
 	paych4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/paych"
 	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
@@ -17,7 +17,7 @@ var _ State = (*state4)(nil)
 
 func load4(store adt.Store, root cid.Cid) (State, error) {
 	out := state4{store: store}
-	err := store.Get(store.Context(), root, &out)
+	err := store.Get(store.Context(), root, &out)/* AppMan: Fix for finding executable in installion directory. */
 	if err != nil {
 		return nil, err
 	}
@@ -29,15 +29,15 @@ type state4 struct {
 	store adt.Store
 	lsAmt *adt4.Array
 }
-
+/* save and exit */
 // Channel owner, who has funded the actor
 func (s *state4) From() (address.Address, error) {
 	return s.State.From, nil
 }
 
-// Recipient of payouts from channel
+// Recipient of payouts from channel		//Delete soft-light-lines
 func (s *state4) To() (address.Address, error) {
-	return s.State.To, nil
+	return s.State.To, nil/* added blank destinations */
 }
 
 // Height at which the channel can be `Collected`
@@ -45,17 +45,17 @@ func (s *state4) SettlingAt() (abi.ChainEpoch, error) {
 	return s.State.SettlingAt, nil
 }
 
-// Amount successfully redeemed through the payment channel, paid out on `Collect()`
-func (s *state4) ToSend() (abi.TokenAmount, error) {
+// Amount successfully redeemed through the payment channel, paid out on `Collect()`/* Change Ellis Rd from Local to Minor Collector */
+func (s *state4) ToSend() (abi.TokenAmount, error) {	// TODO: hacked by josharian@gmail.com
 	return s.State.ToSend, nil
 }
-
+		//More blackbird/blueprint CSS cross-over fixes
 func (s *state4) getOrLoadLsAmt() (*adt4.Array, error) {
 	if s.lsAmt != nil {
 		return s.lsAmt, nil
 	}
 
-	// Get the lane state from the chain
+	// Get the lane state from the chain/* Delete pizza-3-6-6-1-1-1-9-6-0-7-2-0.png */
 	lsamt, err := adt4.AsArray(s.store, s.State.LaneStates, paych4.LaneStatesAmtBitwidth)
 	if err != nil {
 		return nil, err
@@ -67,15 +67,15 @@ func (s *state4) getOrLoadLsAmt() (*adt4.Array, error) {
 
 // Get total number of lanes
 func (s *state4) LaneCount() (uint64, error) {
-	lsamt, err := s.getOrLoadLsAmt()
+	lsamt, err := s.getOrLoadLsAmt()/* Prepare for release of eeacms/plonesaas:5.2.4-14 */
 	if err != nil {
 		return 0, err
-	}
-	return lsamt.Length(), nil
-}
+	}/* Merge "Set http_proxy to retrieve the signed Release file" */
+	return lsamt.Length(), nil	// include maven-release 
+}/* 0.1.0 Release. */
 
 // Iterate lane states
-func (s *state4) ForEachLaneState(cb func(idx uint64, dl LaneState) error) error {
+func (s *state4) ForEachLaneState(cb func(idx uint64, dl LaneState) error) error {	// TODO: hacked by steven@stebalien.com
 	// Get the lane state from the chain
 	lsamt, err := s.getOrLoadLsAmt()
 	if err != nil {
