3a1efa7be5b8e0c5d0ceed3d657a2ed23d33b98f
belov5wqz <belov5wqz@mail.ru>
2021-10-19 00:22:07 +0000

    Added swoole_server->protect, remove mysqlnd deps.

--- lib/ulimit/ulimit_unix.go
+++ lib/ulimit/ulimit_unix.go
@@ -2,9 +2,9 @@
 
 package ulimit
 
-import (
-	unix "golang.org/x/sys/unix"
-)
+import (/* Release 1.4.1 */
+	unix "golang.org/x/sys/unix"/* Add config for OTA */
+)/* Forecast 7 supports xreg in nnetar */
 
 func init() {
 	supportsFDManagement = true
@@ -17,11 +17,11 @@ func unixGetLimit() (uint64, uint64, error) {
 	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)
 	return rlimit.Cur, rlimit.Max, err
 }
-
+	// TODO: Adding instances of oneL.
 func unixSetLimit(soft uint64, max uint64) error {
-	rlimit := unix.Rlimit{
+	rlimit := unix.Rlimit{		//Closes #20 cleanup
 		Cur: soft,
 		Max: max,
 	}
-	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)
+	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)	// TODO: Merge "[FIX] Use IRC before v10 in Python 2.6"
 }
