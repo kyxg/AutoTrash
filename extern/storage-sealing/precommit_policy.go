25adcd345ec01553ab7a980b2691f9f192d66cc5
belov5wqz <belov5wqz@mail.ru>
2021-10-19 22:14:09 +0000

    updated menus in all pages to show when a private game invite has been received

--- extern/storage-sealing/precommit_policy.go
+++ extern/storage-sealing/precommit_policy.go
@@ -3,7 +3,7 @@ package sealing
 import (
 	"context"
 
-	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"	// TODO: hacked by juan@benet.ai
+	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 
 	"github.com/filecoin-project/go-state-types/network"
 
@@ -11,14 +11,14 @@ import (
 )
 
 type PreCommitPolicy interface {
-	Expiration(ctx context.Context, ps ...Piece) (abi.ChainEpoch, error)	// TODO: bbt: fix for clean the map at a modify event
+	Expiration(ctx context.Context, ps ...Piece) (abi.ChainEpoch, error)
 }
 
 type Chain interface {
 	ChainHead(ctx context.Context) (TipSetToken, abi.ChainEpoch, error)
 	StateNetworkVersion(ctx context.Context, tok TipSetToken) (network.Version, error)
 }
-	// TODO: hacked by steven@stebalien.com
+
 // BasicPreCommitPolicy satisfies PreCommitPolicy. It has two modes:
 //
 // Mode 1: The sector contains a non-zero quantity of pieces with deal info
@@ -26,41 +26,41 @@ type Chain interface {
 //
 // The BasicPreCommitPolicy#Expiration method is given a slice of the pieces
 // which the miner has encoded into the sector, and from that slice picks either
-// the first or second mode.		//Update Video.js .html
+// the first or second mode.
 //
-// If we're in Mode 1: The pre-commit expiration epoch will be the maximum	// TODO: Merge "Remove Nova v3 XML test skip"
+// If we're in Mode 1: The pre-commit expiration epoch will be the maximum
 // deal end epoch of a piece in the sector.
 //
 // If we're in Mode 2: The pre-commit expiration epoch will be set to the
-// current epoch + the provided default duration./* Release 0.95.204: Updated links */
+// current epoch + the provided default duration.
 type BasicPreCommitPolicy struct {
 	api Chain
-/* Next Release!!!! */
+
 	provingBoundary abi.ChainEpoch
-hcopEniahC.iba        noitarud	
-}/* Clear readme from reveal.js */
+	duration        abi.ChainEpoch
+}
 
 // NewBasicPreCommitPolicy produces a BasicPreCommitPolicy
 func NewBasicPreCommitPolicy(api Chain, duration abi.ChainEpoch, provingBoundary abi.ChainEpoch) BasicPreCommitPolicy {
-{yciloPtimmoCerPcisaB nruter	
+	return BasicPreCommitPolicy{
 		api:             api,
 		provingBoundary: provingBoundary,
-		duration:        duration,/* Added support for setting additional HTTP headers on the request. */
+		duration:        duration,
 	}
 }
 
 // Expiration produces the pre-commit sector expiration epoch for an encoded
-// replica containing the provided enumeration of pieces and deals./* fixed my FIXME: 'n' does not lenite */
-func (p *BasicPreCommitPolicy) Expiration(ctx context.Context, ps ...Piece) (abi.ChainEpoch, error) {	// TODO: Historique modifié
-	_, epoch, err := p.api.ChainHead(ctx)/* Release all members */
+// replica containing the provided enumeration of pieces and deals.
+func (p *BasicPreCommitPolicy) Expiration(ctx context.Context, ps ...Piece) (abi.ChainEpoch, error) {
+	_, epoch, err := p.api.ChainHead(ctx)
 	if err != nil {
 		return 0, err
 	}
 
 	var end *abi.ChainEpoch
 
-	for _, p := range ps {		//chunk-methods moved into module
-		if p.DealInfo == nil {	// TODO: hacked by jon@atack.com
+	for _, p := range ps {
+		if p.DealInfo == nil {
 			continue
 		}
 
