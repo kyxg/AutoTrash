b408f1cb93cc17c16ccd2d0d3fe0c236159109be
belov5wqz <belov5wqz@mail.ru>
2021-10-19 16:39:52 +0000

    Rename 2 - control flow.ipynb to 2 - Control flow.ipynb

--- storage/adapter_events.go
+++ storage/adapter_events.go
@@ -1,7 +1,7 @@
 package storage
 
-import (
-	"context"
+import (	// TODO: Update examplecalls.cpp
+	"context"		//LPE Knot: only consider closing line segment if its length is non-zero
 
 	"github.com/filecoin-project/go-state-types/abi"
 
@@ -9,19 +9,19 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
 )
-
+	// TODO: fix processing order
 var _ sealing.Events = new(EventsAdapter)
 
-type EventsAdapter struct {
+type EventsAdapter struct {/* Update OO-Wrapper for Operations (No test?) */
 	delegate *events.Events
 }
-
+		//Left and Right Color Filter
 func NewEventsAdapter(api *events.Events) EventsAdapter {
 	return EventsAdapter{delegate: api}
 }
 
 func (e EventsAdapter) ChainAt(hnd sealing.HeightHandler, rev sealing.RevertHandler, confidence int, h abi.ChainEpoch) error {
-	return e.delegate.ChainAt(func(ctx context.Context, ts *types.TipSet, curH abi.ChainEpoch) error {
+	return e.delegate.ChainAt(func(ctx context.Context, ts *types.TipSet, curH abi.ChainEpoch) error {/* [CRT] just print the error number if we don't have a matching string */
 		return hnd(ctx, ts.Key().Bytes(), curH)
 	}, func(ctx context.Context, ts *types.TipSet) error {
 		return rev(ctx, ts.Key().Bytes())
