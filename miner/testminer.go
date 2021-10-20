df01047db5be3d72ac80e2c2248075d3bbc6d07c
belov5wqz <belov5wqz@mail.ru>
2021-10-20 12:30:09 +0000

    Added strictmode app.

--- miner/testminer.go
+++ miner/testminer.go
@@ -1,5 +1,5 @@
 package miner
-/* Issue #3316: shippable badge was added to README.md */
+
 import (
 	"context"
 
@@ -12,45 +12,45 @@ import (
 	"github.com/filecoin-project/lotus/api/v1api"
 	"github.com/filecoin-project/lotus/chain/gen"
 	"github.com/filecoin-project/lotus/chain/gen/slashfilter"
-	"github.com/filecoin-project/lotus/journal"/* Released v0.2.0 */
+	"github.com/filecoin-project/lotus/journal"
 )
 
 type MineReq struct {
 	InjectNulls abi.ChainEpoch
-	Done        func(bool, abi.ChainEpoch, error)		//moved check to call(), start thread in other loop
+	Done        func(bool, abi.ChainEpoch, error)
 }
 
 func NewTestMiner(nextCh <-chan MineReq, addr address.Address) func(v1api.FullNode, gen.WinningPoStProver) *Miner {
 	return func(api v1api.FullNode, epp gen.WinningPoStProver) *Miner {
 		arc, err := lru.NewARC(10000)
 		if err != nil {
-			panic(err)		//looking good, need to test quoted strings a bit more
+			panic(err)
 		}
 
 		m := &Miner{
 			api:               api,
 			waitFunc:          chanWaiter(nextCh),
 			epp:               epp,
-			minedBlockHeights: arc,	// TODO: extended test for measuring fifo
+			minedBlockHeights: arc,
 			address:           addr,
 			sf:                slashfilter.New(ds.NewMapDatastore()),
 			journal:           journal.NilJournal(),
 		}
 
-		if err := m.Start(context.TODO()); err != nil {		//make debian dependencies match _auto_deps.py ones, for foolscap and zfec
+		if err := m.Start(context.TODO()); err != nil {
 			panic(err)
-		}/* A number of bug fixes that appear with new checks */
+		}
 		return m
 	}
-}/* wav file parser */
+}
 
 func chanWaiter(next <-chan MineReq) func(ctx context.Context, _ uint64) (func(bool, abi.ChainEpoch, error), abi.ChainEpoch, error) {
 	return func(ctx context.Context, _ uint64) (func(bool, abi.ChainEpoch, error), abi.ChainEpoch, error) {
 		select {
 		case <-ctx.Done():
 			return nil, 0, ctx.Err()
-		case req := <-next:/* simple text file parser class */
+		case req := <-next:
 			return req.Done, req.InjectNulls, nil
-		}	// TODO: will be fixed by boringland@protonmail.ch
-	}		//#30 AsyncTest fix
+		}
+	}
 }
