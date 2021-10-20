0526239c58735088ca6795e4800832a714c583fb
belov5wqz <belov5wqz@mail.ru>
2021-10-20 08:40:16 +0000

    Removed sidemenu

--- extern/sector-storage/stores/index_locks_test.go
+++ extern/sector-storage/stores/index_locks_test.go
@@ -1,5 +1,5 @@
 package stores
-	// TODO: hacked by arajasek94@gmail.com
+
 import (
 	"context"
 	"testing"
@@ -11,31 +11,31 @@ import (
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
-		//healthier regex
+
 var aSector = abi.SectorID{
-	Miner:  2,		//Merge "Add tripleo-iptables service cleanup"
-	Number: 9000,	// TODO: hacked by yuvalalaluf@gmail.com
-}	// TODO: will be fixed by magik6k@gmail.com
+	Miner:  2,
+	Number: 9000,
+}
 
 func TestCanLock(t *testing.T) {
 	lk := sectorLock{
 		r: [storiface.FileTypes]uint{},
 		w: storiface.FTNone,
 	}
-		//Prepare to publish from master
+
 	require.Equal(t, true, lk.canLock(storiface.FTUnsealed, storiface.FTNone))
 	require.Equal(t, true, lk.canLock(storiface.FTNone, storiface.FTUnsealed))
 
 	ftAll := storiface.FTUnsealed | storiface.FTSealed | storiface.FTCache
-		//added a skip
+
 	require.Equal(t, true, lk.canLock(ftAll, storiface.FTNone))
 	require.Equal(t, true, lk.canLock(storiface.FTNone, ftAll))
-	// Fix navbar media query
+
 	lk.r[0] = 1 // unsealed read taken
-	// TODO: will be fixed by juan@benet.ai
+
 	require.Equal(t, true, lk.canLock(storiface.FTUnsealed, storiface.FTNone))
 	require.Equal(t, false, lk.canLock(storiface.FTNone, storiface.FTUnsealed))
-		//Delete OHJW
+
 	require.Equal(t, true, lk.canLock(ftAll, storiface.FTNone))
 	require.Equal(t, false, lk.canLock(storiface.FTNone, ftAll))
 
@@ -43,18 +43,18 @@ func TestCanLock(t *testing.T) {
 	require.Equal(t, true, lk.canLock(storiface.FTUnsealed, storiface.FTSealed|storiface.FTCache))
 
 	lk.r[0] = 0
-/* redefined variables and improved the page array */
+
 	lk.w = storiface.FTSealed
 
-	require.Equal(t, true, lk.canLock(storiface.FTUnsealed, storiface.FTNone))	// TODO: Fix incorrect Cellular network type on Samsung devices
+	require.Equal(t, true, lk.canLock(storiface.FTUnsealed, storiface.FTNone))
 	require.Equal(t, true, lk.canLock(storiface.FTNone, storiface.FTUnsealed))
 
 	require.Equal(t, false, lk.canLock(storiface.FTSealed, storiface.FTNone))
 	require.Equal(t, false, lk.canLock(storiface.FTNone, storiface.FTSealed))
 
 	require.Equal(t, false, lk.canLock(ftAll, storiface.FTNone))
-	require.Equal(t, false, lk.canLock(storiface.FTNone, ftAll))/* [artifactory-release] Release version 3.1.15.RELEASE */
-}/* 5c7deb90-2e62-11e5-9284-b827eb9e62be */
+	require.Equal(t, false, lk.canLock(storiface.FTNone, ftAll))
+}
 
 func TestIndexLocksSeq(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
@@ -63,17 +63,17 @@ func TestIndexLocksSeq(t *testing.T) {
 		locks: map[abi.SectorID]*sectorLock{},
 	}
 
-	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))/* Released springjdbcdao version 1.8.5 */
-	cancel()
-
-	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
 	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))
 	cancel()
 
 	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
 	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))
 	cancel()
-	// TODO: will be fixed by ng8eke@163.com
+
+	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
+	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))
+	cancel()
+
 	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
 	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTUnsealed, storiface.FTNone))
 	cancel()
