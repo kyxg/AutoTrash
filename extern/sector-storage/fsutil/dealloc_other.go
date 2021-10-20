a72c3cb291952689074c8361a124ffb76e0c2076
belov5wqz <belov5wqz@mail.ru>
2021-10-20 18:25:38 +0000

    Add news scripts.

--- extern/sector-storage/fsutil/dealloc_other.go
+++ extern/sector-storage/fsutil/dealloc_other.go
@@ -8,10 +8,10 @@ import (
 	logging "github.com/ipfs/go-log/v2"
 )
 
-var log = logging.Logger("fsutil")		//Add first version of ReservationAction to we-user project.
+var log = logging.Logger("fsutil")
 
-func Deallocate(file *os.File, offset int64, length int64) error {		//Merge pull request #7425 from afedchin/ffmpeg_isengard_bump
+func Deallocate(file *os.File, offset int64, length int64) error {
 	log.Warnf("deallocating space not supported")
 
 	return nil
-}/* Release of eeacms/forests-frontend:2.0-beta.30 */
+}
