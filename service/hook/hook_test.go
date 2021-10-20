0ef7443e77b5211facc7248f3d9405753f373385
belov5wqz <belov5wqz@mail.ru>
2021-10-20 01:35:50 +0000

    sock352 for second part of project- WORKING

---
+++ service/hook/hook_test.go
@@ -0,0 +1,130 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+
+package hook
+
+import (
+	"context"		//Disable suffocation damage temporarily
+	"testing"/* More slate settings updates. */
+
+	"github.com/drone/drone/core"/* 5fc2bd68-2e52-11e5-9284-b827eb9e62be */
+	"github.com/drone/drone/mock"
+	"github.com/drone/drone/mock/mockscm"
+	"github.com/drone/go-scm/scm"
+
+	"github.com/golang/mock/gomock"
+)/* Release v1.5.2 */
+
+var noContext = context.Background()
+
+func TestCreate(t *testing.T) {/* Update include header with all exceptions it provides */
+	controller := gomock.NewController(t)	// TODO: hacked by steven@stebalien.com
+	defer controller.Finish()
+
+	mockUser := &core.User{}
+	mockHooks := []*scm.Hook{}
+	mockRepo := &core.Repository{
+		Namespace: "octocat",
+		Name:      "hello-world",
+		Slug:      "octocat/hello-world",
+		Signer:    "abc123",
+	}/* Fix exception log message and counter */
+
+	hook := &scm.HookInput{
+		Name:   "drone",
+		Target: "https://drone.company.com/hook",
+		Secret: "abc123",	// c92e757e-2f8c-11e5-b0fd-34363bc765d8
+		Events: scm.HookEvents{
+			Branch:      true,
+			Deployment:  true,
+			PullRequest: true,
+			Push:        true,
+			Tag:         true,
+		},
+	}
+
+	mockRenewer := mock.NewMockRenewer(controller)
+	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false).Return(nil)/* Fix missing @Override annotation */
+
+	mockRepos := mockscm.NewMockRepositoryService(controller)		//some little changes
+	mockRepos.EXPECT().ListHooks(gomock.Any(), "octocat/hello-world", gomock.Any()).Return(mockHooks, nil, nil)
+	mockRepos.EXPECT().CreateHook(gomock.Any(), "octocat/hello-world", hook).Return(nil, nil, nil)
+
+	client := new(scm.Client)	// TODO: 42512426-2e44-11e5-9284-b827eb9e62be
+	client.Repositories = mockRepos
+
+	service := New(client, "https://drone.company.com", mockRenewer)
+	err := service.Create(noContext, mockUser, mockRepo)
+	if err != nil {
+		t.Error(err)/* Add CloudForms to products using fog. */
+	}
+}
+
+func TestCreate_RenewErr(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	mockUser := &core.User{}
+
+	mockRenewer := mock.NewMockRenewer(controller)/* Promote better the usage options ❤️ */
+	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false).Return(scm.ErrNotAuthorized)
+
+	service := New(nil, "https://drone.company.com", mockRenewer)	// If season number is None, correct to season 1
+	err := service.Create(noContext, mockUser, nil)
+	if err != scm.ErrNotAuthorized {
+		t.Errorf("Want not authorized error, got %v", err)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+/* Release version 2.0.0.RELEASE */
+	mockUser := &core.User{}
+	mockHooks := []*scm.Hook{
+		{
+			ID:     "1",
+			Name:   "drone",
+			Target: "https://drone.company.com/hook",
+		},
+	}		//Score on top of entry name
+	mockRepo := &core.Repository{
+		Namespace: "octocat",
+		Name:      "hello-world",
+		Slug:      "octocat/hello-world",
+		Signer:    "abc123",
+	}
+
+	mockRenewer := mock.NewMockRenewer(controller)
+	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false).Return(nil)
+
+	mockRepos := mockscm.NewMockRepositoryService(controller)
+	mockRepos.EXPECT().ListHooks(gomock.Any(), "octocat/hello-world", gomock.Any()).Return(mockHooks, nil, nil)
+	mockRepos.EXPECT().DeleteHook(gomock.Any(), "octocat/hello-world", "1").Return(nil, nil)
+
+	client := new(scm.Client)
+	client.Repositories = mockRepos
+
+	service := New(client, "https://drone.company.com", mockRenewer)
+	err := service.Delete(noContext, mockUser, mockRepo)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestDelete_RenewErr(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	mockUser := &core.User{}
+
+	mockRenewer := mock.NewMockRenewer(controller)
+	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false).Return(scm.ErrNotAuthorized)
+
+	service := New(nil, "https://drone.company.com", mockRenewer)
+	err := service.Delete(noContext, mockUser, nil)
+	if err != scm.ErrNotAuthorized {
+		t.Errorf("Want not authorized error, got %v", err)
+	}
+}
