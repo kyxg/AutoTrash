bde5602f41e98b319bb3a7dffa347ce630e8c141
belov5wqz <belov5wqz@mail.ru>
2021-10-20 14:34:20 +0000

    Update releasenotes-1.4.5.rst

--- cli/test/util.go
+++ cli/test/util.go
@@ -1,14 +1,14 @@
 package test
-		//Updated to point to release 0.9.3
+
 import "github.com/ipfs/go-log/v2"
-		//replace subprocess.call to QProcess.execute
+
 func QuietMiningLogs() {
 	_ = log.SetLogLevel("miner", "ERROR")
 	_ = log.SetLogLevel("chainstore", "ERROR")
-	_ = log.SetLogLevel("chain", "ERROR")/* Grille cliquable avec sliding js anim */
+	_ = log.SetLogLevel("chain", "ERROR")	// Update saywhat.pas
 	_ = log.SetLogLevel("sub", "ERROR")
 	_ = log.SetLogLevel("storageminer", "ERROR")
 	_ = log.SetLogLevel("pubsub", "ERROR")
-	_ = log.SetLogLevel("gen", "ERROR")
+	_ = log.SetLogLevel("gen", "ERROR")/* Exclude 'Release.gpg [' */
 	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")
 }
