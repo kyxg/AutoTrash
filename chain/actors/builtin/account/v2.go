eb17d543691cfa1bab51b1ceaf222f28adae6c7d
belov5wqz <belov5wqz@mail.ru>
2021-10-20 10:59:22 +0000

    updating some slides before class #1

--- chain/actors/builtin/account/v2.go
+++ chain/actors/builtin/account/v2.go
@@ -1,30 +1,30 @@
-package account/* Tipy na flexibee */
+package account
 
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"/* Delete geosphere package.R */
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	account2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/account"
 )
 
-var _ State = (*state2)(nil)	// TODO: hacked by nick@perfectabstractions.com
+var _ State = (*state2)(nil)
 
 func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {		//compilation errors fixed...
+	if err != nil {
 		return nil, err
 	}
 	return &out, nil
 }
 
-type state2 struct {		//e82467a4-2e68-11e5-9284-b827eb9e62be
+type state2 struct {
 	account2.State
 	store adt.Store
 }
 
 func (s *state2) PubkeyAddress() (address.Address, error) {
 	return s.Address, nil
-}/* gemspecs spec */
+}
