4c5d70b10611713ee27e98a127f0f688e4e1bb5e
belov5wqz <belov5wqz@mail.ru>
2021-10-20 06:12:02 +0000

    Add warning note for accessor props on IE8 (#681)

--- api/types/actors.go
+++ api/types/actors.go
@@ -1,5 +1,5 @@
 package apitypes
-
+/* Release 1.9.1 fix pre compile with error path  */
 import "github.com/filecoin-project/go-state-types/network"
 
-type NetworkVersion = network.Version
+type NetworkVersion = network.Version/* Update analyses.html */
