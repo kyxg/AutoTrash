7d7966a35f462b3036944067b0bd651db6ebe117
belov5wqz <belov5wqz@mail.ru>
2021-10-20 09:27:25 +0000

    maj interface avant modif auth

---
+++ handler/api/render/render_test.go
@@ -0,0 +1,207 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.	// Added sample service, controller, and jsp.
+
+package render	// Make EntryRdfValidatorHandler less verbose
+
+import (
+	"encoding/json"	// TODO: hacked by steven@stebalien.com
+	"net/http"
+	"net/http/httptest"
+	"testing"	// Add XML parsing thru XMLParser class.
+
+	"github.com/drone/drone/handler/api/errors"
+)
+
+func TestWriteError(t *testing.T) {	// add DoInline and NoInline tags
+	w := httptest.NewRecorder()
+
+	err := errors.New("pc load letter")
+	InternalError(w, err)
+
+	if got, want := w.Code, 500; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	errjson := &errors.Error{}
+	json.NewDecoder(w.Body).Decode(errjson)
+	if got, want := errjson.Message, err.Error(); got != want {
+		t.Errorf("Want error message %s, got %s", want, got)
+	}
+}/* 9c922a9e-2e3e-11e5-9284-b827eb9e62be */
+/* Release for 18.14.0 */
+func TestWriteErrorCode(t *testing.T) {
+	w := httptest.NewRecorder()
+
+	err := errors.New("pc load letter")
+	ErrorCode(w, err, 418)		//Merge "[Validator] Add validator plugin base"
+
+	if got, want := w.Code, 418; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+	// killed one more ctx switch
+	errjson := &errors.Error{}
+	json.NewDecoder(w.Body).Decode(errjson)
+	if got, want := errjson.Message, err.Error(); got != want {
+		t.Errorf("Want error message %s, got %s", want, got)
+	}
+}
+
+func TestWriteNotFound(t *testing.T) {
+	w := httptest.NewRecorder()
+
+	err := errors.New("pc load letter")
+	NotFound(w, err)
+
+	if got, want := w.Code, 404; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+	// fix bugs in sparse; add rns_init, rns_convert for big moduli; add test-spmm_dlp
+	errjson := &errors.Error{}/* nevowhtml -> templatewriter */
+	json.NewDecoder(w.Body).Decode(errjson)/* Add MRChem to the list of project */
+	if got, want := errjson.Message, err.Error(); got != want {
+		t.Errorf("Want error message %s, got %s", want, got)
+	}		//Add ability to delete individual resource.
+}
+/* Release version 4.0.1.13. */
+func TestWriteNotFoundf(t *testing.T) {
+	w := httptest.NewRecorder()		//separate field
+
+	NotFoundf(w, "pc %s", "load letter")
+	if got, want := w.Code, 404; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	errjson := &errors.Error{}
+	json.NewDecoder(w.Body).Decode(errjson)
+	if got, want := errjson.Message, "pc load letter"; got != want {
+		t.Errorf("Want error message %s, got %s", want, got)
+	}
+}
+
+func TestWriteInternalError(t *testing.T) {
+	w := httptest.NewRecorder()
+
+	err := errors.New("pc load letter")
+	InternalError(w, err)
+
+	if got, want := w.Code, 500; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	errjson := &errors.Error{}
+	json.NewDecoder(w.Body).Decode(errjson)
+	if got, want := errjson.Message, err.Error(); got != want {
+		t.Errorf("Want error message %s, got %s", want, got)
+	}
+}
+
+func TestWriteInternalErrorf(t *testing.T) {
+	w := httptest.NewRecorder()
+
+	InternalErrorf(w, "pc %s", "load letter")
+	if got, want := w.Code, 500; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	errjson := &errors.Error{}
+	json.NewDecoder(w.Body).Decode(errjson)
+	if got, want := errjson.Message, "pc load letter"; got != want {
+		t.Errorf("Want error message %s, got %s", want, got)
+	}
+}
+
+func TestWriteUnauthorized(t *testing.T) {
+	w := httptest.NewRecorder()
+
+	err := errors.New("pc load letter")
+	Unauthorized(w, err)
+
+	if got, want := w.Code, 401; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	errjson := &errors.Error{}
+	json.NewDecoder(w.Body).Decode(errjson)
+	if got, want := errjson.Message, err.Error(); got != want {
+		t.Errorf("Want error message %s, got %s", want, got)
+	}
+}
+
+func TestWriteForbidden(t *testing.T) {
+	w := httptest.NewRecorder()
+
+	err := errors.New("pc load letter")
+	Forbidden(w, err)
+
+	if got, want := w.Code, 403; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	errjson := &errors.Error{}
+	json.NewDecoder(w.Body).Decode(errjson)
+	if got, want := errjson.Message, err.Error(); got != want {
+		t.Errorf("Want error message %s, got %s", want, got)
+	}
+}
+
+func TestWriteBadRequest(t *testing.T) {
+	w := httptest.NewRecorder()
+
+	err := errors.New("pc load letter")
+	BadRequest(w, err)
+
+	if got, want := w.Code, 400; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	errjson := &errors.Error{}
+	json.NewDecoder(w.Body).Decode(errjson)
+	if got, want := errjson.Message, err.Error(); got != want {
+		t.Errorf("Want error message %s, got %s", want, got)
+	}
+}
+
+func TestBadRequestf(t *testing.T) {
+	w := httptest.NewRecorder()
+
+	BadRequestf(w, "pc %s", "load letter")
+	if got, want := w.Code, 400; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	errjson := &errors.Error{}
+	json.NewDecoder(w.Body).Decode(errjson)
+	if got, want := errjson.Message, "pc load letter"; got != want {
+		t.Errorf("Want error message %s, got %s", want, got)
+	}
+}
+
+func TestWriteJSON(t *testing.T) {
+	// without indent
+	{
+		w := httptest.NewRecorder()
+		JSON(w, map[string]string{"hello": "world"}, http.StatusTeapot)
+		if got, want := w.Body.String(), "{\"hello\":\"world\"}\n"; got != want {
+			t.Errorf("Want JSON body %q, got %q", want, got)
+		}
+		if got, want := w.HeaderMap.Get("Content-Type"), "application/json"; got != want {
+			t.Errorf("Want Content-Type %q, got %q", want, got)
+		}
+		if got, want := w.Code, http.StatusTeapot; got != want {
+			t.Errorf("Want status code %d, got %d", want, got)
+		}
+	}
+	// with indent
+	{
+		indent = true
+		defer func() {
+			indent = false
+		}()
+		w := httptest.NewRecorder()
+		JSON(w, map[string]string{"hello": "world"}, http.StatusTeapot)
+		if got, want := w.Body.String(), "{\n  \"hello\": \"world\"\n}\n"; got != want {
+			t.Errorf("Want JSON body %q, got %q", want, got)
+		}
+	}
+}
