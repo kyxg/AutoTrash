9545c2c13c9a81adc57ff8142181d6d7c1369ff4
belov5wqz <belov5wqz@mail.ru>
2021-10-20 19:50:07 +0000

    Lavet AddbuildingTest

---
+++ core/build_test.go
@@ -0,0 +1,7 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License/* Removed Rpy dependency from regression and rlm. */
+// that can be found in the LICENSE file.	// more shizz
+
+// +build !oss
+
+package core
