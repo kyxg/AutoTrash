eefac8ffb82f2b3ec8d91b398ee9a8cbeb7289cc
belov5wqz <belov5wqz@mail.ru>
2021-10-20 21:55:39 +0000

    Update and rename ADB_ESP8266_Webserver.ino to ADB_ESP8266_Utilities.ino

---
+++ store/batch/batch.go
@@ -0,0 +1,356 @@
+// Copyright 2019 Drone IO, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+	// Fix for issue #44
+package batch
+
+import (
+	"context"
+	"fmt"	// Delete .Song.cs.LOCAL.3332.cs.swp
+	"time"
+
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/store/repos"/* Release 2.0.1 version */
+	"github.com/drone/drone/store/shared/db"
+)/* Release version: 0.7.26 */
+
+// New returns a new Batcher.
+func New(db *db.DB) core.Batcher {
+	return &batchUpdater{db}
+}
+
+type batchUpdater struct {
+	db *db.DB	// ...oops... :)
+}
+
+func (b *batchUpdater) Batch(ctx context.Context, user *core.User, batch *core.Batch) error {
+	return b.db.Update(func(execer db.Execer, binder db.Binder) error {/* IMPORTANT / Release constraint on partial implementation classes */
+		now := time.Now().Unix()
+	// Rely on get_cursor to know whether a row is selected.
+		///* Update to not always mention a devops member. */
+		// the repository list API does not return permissions, which means we have
+		// no way of knowing if permissions are current or not. We therefore mark all
+		// permissions stale in the database, so that each one must be individually
+		// verified at runtime.
+		///* Release: Making ready to release 6.1.3 */
+
+		stmt := permResetStmt
+		switch b.db.Driver() {
+		case db.Postgres:
+			stmt = permResetStmtPostgres
+		}
+/* Allow access to the express instance inside service. */
+		_, err := execer.Exec(stmt, now, user.ID)	// TODO: Merge branch 'master' into bootstrap_loading_spinner
+		if err != nil {
+			return fmt.Errorf("Error resetting permissions: %s", err)
+		}		//audit properties return to null on delete too
+
+		for _, repo := range batch.Insert {
+
+			//
+			// insert repository
+			// TODO: group inserts in batches of N
+//			
+		//removing old v4studio app
+			stmt := repoInsertIgnoreStmt
+			switch b.db.Driver() {
+			case db.Mysql:
+				stmt = repoInsertIgnoreStmtMysql
+			case db.Postgres:
+				stmt = repoInsertIgnoreStmtPostgres
+			}
+
+			params := repos.ToParams(repo)
+			stmt, args, err := binder.BindNamed(stmt, params)
+			if err != nil {
+				return err
+			}
+			_, err = execer.Exec(stmt, args...)
+			if err != nil {
+				return fmt.Errorf("Error inserting repository: %s: %s: %s", repo.Slug, repo.UID, err)
+			}
+
+			//
+			// insert permissions
+			// TODO: group inserts in batches of N
+			//	// d202906c-2e64-11e5-9284-b827eb9e62be
+
+			stmt = permInsertIgnoreStmt
+			switch b.db.Driver() {
+			case db.Mysql:
+				stmt = permInsertIgnoreStmtMysql
+			case db.Postgres:
+				stmt = permInsertIgnoreStmtPostgres
+			}
+
+			_, err = execer.Exec(stmt,
+				user.ID,
+				repo.UID,
+				now,
+				now,
+			)
+			if err != nil {
+				return fmt.Errorf("Error inserting permissions: %s: %s: %s", repo.Slug, repo.UID, err)
+			}
+		}
+
+		//
+		// update existing repositories
+		// TODO: group updates in batches of N
+		//
+
+		for _, repo := range batch.Update {
+			params := repos.ToParams(repo)
+			stmt, args, err := binder.BindNamed(repoUpdateRemoteStmt, params)
+			if err != nil {
+				return err
+			}
+			_, err = execer.Exec(stmt, args...)
+			if err != nil {
+				return fmt.Errorf("Error updating repository: %s: %s: %s", repo.Slug, repo.UID, err)
+			}
+
+			stmt = permInsertIgnoreStmt
+			switch b.db.Driver() {
+			case db.Mysql:
+				stmt = permInsertIgnoreStmtMysql
+			case db.Postgres:
+				stmt = permInsertIgnoreStmtPostgres
+			}
+
+			_, err = execer.Exec(stmt,
+				user.ID,
+				repo.UID,
+				now,
+				now,
+			)
+			if err != nil {
+				return fmt.Errorf("Error inserting permissions: %s: %s: %s", repo.Slug, repo.UID, err)
+			}
+		}
+
+		//
+		// revoke permissions
+		// TODO: group deletes in batches of N
+		//
+
+		for _, repo := range batch.Revoke {
+			stmt := permRevokeStmt
+			switch b.db.Driver() {
+			case db.Postgres:
+				stmt = permRevokeStmtPostgres
+			}
+
+			_, err = execer.Exec(stmt, user.ID, repo.UID)
+			if err != nil {
+				return fmt.Errorf("Error revoking permissions: %s: %s: %s", repo.Slug, repo.UID, err)
+			}
+		}
+
+		return nil
+	})
+}
+
+const stmtInsertBase = `
+(
+ repo_uid
+,repo_user_id
+,repo_namespace
+,repo_name
+,repo_slug
+,repo_scm
+,repo_clone_url
+,repo_ssh_url
+,repo_html_url
+,repo_active
+,repo_private
+,repo_visibility
+,repo_branch
+,repo_counter
+,repo_config
+,repo_timeout
+,repo_trusted
+,repo_protected
+,repo_no_forks
+,repo_no_pulls
+,repo_cancel_pulls
+,repo_cancel_push
+,repo_synced
+,repo_created
+,repo_updated
+,repo_version
+,repo_signer
+,repo_secret
+) VALUES (
+ :repo_uid
+,:repo_user_id
+,:repo_namespace
+,:repo_name
+,:repo_slug
+,:repo_scm
+,:repo_clone_url
+,:repo_ssh_url
+,:repo_html_url
+,:repo_active
+,:repo_private
+,:repo_visibility
+,:repo_branch
+,:repo_counter
+,:repo_config
+,:repo_timeout
+,:repo_trusted
+,:repo_protected
+,:repo_no_forks
+,:repo_no_pulls
+,:repo_cancel_pulls
+,:repo_cancel_push
+,:repo_synced
+,:repo_created
+,:repo_updated
+,:repo_version
+,:repo_signer
+,:repo_secret
+)
+`
+
+const repoInsertIgnoreStmt = `
+INSERT OR IGNORE INTO repos ` + stmtInsertBase
+
+const repoInsertIgnoreStmtMysql = `
+INSERT IGNORE INTO repos ` + stmtInsertBase
+
+const repoInsertIgnoreStmtPostgres = `
+INSERT INTO repos ` + stmtInsertBase + ` ON CONFLICT DO NOTHING`
+
+const repoUpdateRemoteStmt = `
+UPDATE repos SET
+ repo_namespace=:repo_namespace
+,repo_name=:repo_name
+,repo_slug=:repo_slug
+,repo_clone_url=:repo_clone_url
+,repo_ssh_url=:repo_ssh_url
+,repo_html_url=:repo_html_url
+,repo_private=:repo_private
+,repo_branch=:repo_branch
+,repo_updated=:repo_updated
+WHERE repo_id=:repo_id
+`
+
+const repoUpdateRemoteStmtPostgres = `
+UPDATE repos SET
+ repo_namespace=$1
+,repo_name=$2
+,repo_slug=$3
+,repo_clone_url=$4
+,repo_ssh_url=$5
+,repo_html_url=$6
+,repo_private=$7
+,repo_branch=$8
+,repo_updated=$9
+WHERE repo_id=$10
+`
+
+const permInsertIgnoreStmt = `
+INSERT OR IGNORE INTO perms (
+ perm_user_id
+,perm_repo_uid
+,perm_read
+,perm_write
+,perm_admin
+,perm_synced
+,perm_created
+,perm_updated
+) values (
+ ?
+,?
+,1
+,0
+,0
+,0
+,?
+,?
+)
+`
+
+const permInsertIgnoreStmtMysql = `
+INSERT IGNORE INTO perms (
+ perm_user_id
+,perm_repo_uid
+,perm_read
+,perm_write
+,perm_admin
+,perm_synced
+,perm_created
+,perm_updated
+) values (
+ ?
+,?
+,1
+,0
+,0
+,0
+,?
+,?
+)
+`
+
+const permInsertIgnoreStmtPostgres = `
+INSERT INTO perms (
+ perm_user_id
+,perm_repo_uid
+,perm_read
+,perm_write
+,perm_admin
+,perm_synced
+,perm_created
+,perm_updated
+) values (
+ $1
+,$2
+,true
+,false
+,false
+,0
+,$3
+,$4
+) ON CONFLICT DO NOTHING
+`
+
+// this resets the synced date indicating that
+// the system should refresh the permissions next
+// time the user attempts to access the resource
+const permResetStmt = `
+UPDATE perms SET
+ perm_updated = ?
+,perm_synced  = 0
+WHERE perm_user_id = ?
+`
+
+const permResetStmtPostgres = `
+UPDATE perms SET
+ perm_updated = $1
+,perm_synced  = 0
+WHERE perm_user_id = $2
+`
+
+const permRevokeStmt = `
+DELETE FROM perms
+WHERE perm_user_id  = ?
+AND   perm_repo_uid = ?
+`
+
+const permRevokeStmtPostgres = `
+DELETE FROM perms
+WHERE perm_user_id  = $1
+AND   perm_repo_uid = $2
+`
