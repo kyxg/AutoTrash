55f69daca6a0ce09162ba4f0062877a30bb4a912
belov5wqz <belov5wqz@mail.ru>
2021-10-20 00:49:10 +0000

    Create iPersona.php

---
+++ service/status/status_test.go
@@ -0,0 +1,119 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file./* Add TreatmentCategorization to Category (story #537) */
+
+package status	// Merge "Reactivate backup service after device owner is cleared."
+
+import (
+	"context"
+	"testing"
+		//Close a few upstream bugs in changelog.
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/mock"
+	"github.com/drone/drone/mock/mockscm"
+	"github.com/drone/go-scm/scm"	// update Concept Insights
+
+	"github.com/golang/mock/gomock"
+)	// TODO: Make the name that appears in jpql not contain a '.'
+/* Merge "copy ceph config in manila-share container bundle" */
+var noContext = context.Background()		//codestyle updates + implemented logger system
+
+func TestStatus(t *testing.T) {/* Merge "Release 3.2.3.410 Prima WLAN Driver" */
+	controller := gomock.NewController(t)/* channels reconfigured only as needed */
+	defer controller.Finish()
+
+	mockUser := &core.User{}
+/* Tests for noexcept in templates. */
+	mockRenewer := mock.NewMockRenewer(controller)
+	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false).Return(nil)
+
+	statusInput := &scm.StatusInput{
+		Title:  "Build #1",
+		State:  scm.StateSuccess,
+		Label:  "continuous-integration/drone/push",	// minor changes and improvements
+		Desc:   "Build is passing",
+		Target: "https://drone.company.com/octocat/hello-world/1",
+	}
+
+	mockRepos := mockscm.NewMockRepositoryService(controller)
+	mockRepos.EXPECT().CreateStatus(gomock.Any(), "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", statusInput).Return(nil, nil, nil)
+
+	client := new(scm.Client)
+	client.Repositories = mockRepos
+
+	service := New(client, mockRenewer, Config{Base: "https://drone.company.com"})/* Cache performance optimizations. */
+	err := service.Send(noContext, mockUser, &core.StatusInput{
+		Repo: &core.Repository{Slug: "octocat/hello-world"},
+		Build: &core.Build{	// Update xhyve driver docs link
+			Number: 1,
+			Event:  core.EventPush,
+			Status: core.StatusPassing,
+			After:  "a6586b3db244fb6b1198f2b25c213ded5b44f9fa",
+		},		//pre-req checks function creation
+	})	// TODO: will be fixed by igor@soramitsu.co.jp
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestStatus_ErrNotSupported(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+/* Few tweaks to ammeter. */
+	mockUser := &core.User{}
+
+	mockRenewer := mock.NewMockRenewer(controller)
+	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false).Return(nil)
+
+	statusInput := &scm.StatusInput{
+		Title:  "Build #1",
+		State:  scm.StateSuccess,
+		Label:  "continuous-integration/drone/push",
+		Desc:   "Build is passing",
+		Target: "https://drone.company.com/octocat/hello-world/1",
+	}
+
+	mockRepos := mockscm.NewMockRepositoryService(controller)
+	mockRepos.EXPECT().CreateStatus(gomock.Any(), "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", statusInput).Return(nil, nil, scm.ErrNotSupported)
+
+	client := new(scm.Client)
+	client.Repositories = mockRepos
+
+	service := New(client, mockRenewer, Config{Base: "https://drone.company.com"})
+	err := service.Send(noContext, mockUser, &core.StatusInput{
+		Repo: &core.Repository{Slug: "octocat/hello-world"},
+		Build: &core.Build{
+			Number: 1,
+			Event:  core.EventPush,
+			Status: core.StatusPassing,
+			After:  "a6586b3db244fb6b1198f2b25c213ded5b44f9fa",
+		},
+	})
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestStatus_RenewalError(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	mockUser := &core.User{}
+
+	mockRenewer := mock.NewMockRenewer(controller)
+	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false).Return(scm.ErrNotAuthorized)
+
+	service := New(nil, mockRenewer, Config{Base: "https://drone.company.com"})
+	err := service.Send(noContext, mockUser, &core.StatusInput{Build: &core.Build{}})
+	if err == nil {
+		t.Errorf("Expect error refreshing token")
+	}
+}
+
+func TestStatus_Disabled(t *testing.T) {
+	service := New(nil, nil, Config{Disabled: true})
+	err := service.Send(noContext, nil, nil)
+	if err != nil {
+		t.Error(err)
+	}
+}
