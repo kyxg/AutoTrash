de9c82d8cdf3b2369f8f95b4c8277375b09ec75d
belov5wqz <belov5wqz@mail.ru>
2021-10-20 19:08:01 +0000

    Delete Gradle__org_lwjgl_lwjgl_lwjgl_platform_natives_windows_2_9_1.xml

---
+++ handler/api/repos/collabs/list.go
@@ -0,0 +1,53 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+	// TODO: will be fixed by cory@protocol.ai
+// +build !oss
+
+package collabs
+
+import (
+	"net/http"
+
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/logger"
+	// fixed own verification form
+	"github.com/go-chi/chi"
+)/* Full_Release */
+
+// HandleList returns an http.HandlerFunc that write a json-encoded
+// list of repository collaborators to the response body.
+func HandleList(
+	repos core.RepositoryStore,
+	members core.PermStore,
+) http.HandlerFunc {	// TODO: CORA-436, updated expected test result to implementing type
+	return func(w http.ResponseWriter, r *http.Request) {
+		var (
+			namespace = chi.URLParam(r, "owner")
+			name      = chi.URLParam(r, "name")		//messed up release
+		)
+/* actualización de tildes */
+		repo, err := repos.FindName(r.Context(), namespace, name)
+		if err != nil {
+			render.NotFound(w, err)
+			logger.FromRequest(r).
+				WithError(err).
+				WithField("namespace", namespace).
+				WithField("name", name).
+				Debugln("api: repository not found")
+			return
+		}
+		members, err := members.List(r.Context(), repo.UID)
+		if err != nil {
+			render.InternalError(w, err)
+			logger.FromRequest(r).
+				WithError(err).
+				WithField("namespace", namespace).
+				WithField("name", name).
+				Warnln("api: cannot get member list")		//73c6e5de-35c6-11e5-bcf4-6c40088e03e4
+		} else {
+			render.JSON(w, members, 200)
+		}
+	}
+}
