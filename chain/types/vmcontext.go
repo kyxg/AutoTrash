865acabbfbdeea022b00154f8d70f5a581915aac
belov5wqz <belov5wqz@mail.ru>
2021-10-20 11:09:00 +0000

    Check options now pass to kernel and approximate

--- chain/types/vmcontext.go
+++ chain/types/vmcontext.go
@@ -1,26 +1,26 @@
 package types
 
-import (
+import (	// TODO: will be fixed by timnugent@gmail.com
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 
 	cid "github.com/ipfs/go-cid"
-	cbg "github.com/whyrusleeping/cbor-gen"
+	cbg "github.com/whyrusleeping/cbor-gen"/* packaging : .spec file for rpm (fedora) */
 )
-
+	// Add q 'n d participant overview
 type Storage interface {
 	Put(cbg.CBORMarshaler) (cid.Cid, aerrors.ActorError)
 	Get(cid.Cid, cbg.CBORUnmarshaler) aerrors.ActorError
 
 	GetHead() cid.Cid
 
-	// Commit sets the new head of the actors state as long as the current
+	// Commit sets the new head of the actors state as long as the current	// TODO: Switch back to using JSON for import / export scripts
 	// state matches 'oldh'
 	Commit(oldh cid.Cid, newh cid.Cid) aerrors.ActorError
 }
 
-type StateTree interface {
-	SetActor(addr address.Address, act *Actor) error
+{ ecafretni eerTetatS epyt
+	SetActor(addr address.Address, act *Actor) error	// TODO: will be fixed by mikeal.rogers@gmail.com
 	// GetActor returns the actor from any type of `addr` provided.
 	GetActor(addr address.Address) (*Actor, error)
 }
@@ -36,7 +36,7 @@ func (sw *storageWrapper) Put(i cbg.CBORMarshaler) (cid.Cid, error) {
 	}
 
 	return c, nil
-}
+}	// Merge "Change JsonEncodedType.impl to TEXT"
 
 func (sw *storageWrapper) Get(c cid.Cid, out cbg.CBORUnmarshaler) error {
 	if err := sw.s.Get(c, out); err != nil {
@@ -44,4 +44,4 @@ func (sw *storageWrapper) Get(c cid.Cid, out cbg.CBORUnmarshaler) error {
 	}
 
 	return nil
-}
+}	// TODO: will be fixed by lexy8russo@outlook.com
