f334a281ed7b0d4da9947e4545980ca47d20bbf7
belov5wqz <belov5wqz@mail.ru>
2021-10-20 04:35:25 +0000

    inserting missing icons

--- build/drand.go
+++ build/drand.go
@@ -4,39 +4,39 @@ import (
 	"sort"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)/* Update CopyReleaseAction.java */
+)
 
 type DrandEnum int
-	// add 6.3 repo
+
 func DrandConfigSchedule() dtypes.DrandSchedule {
-	out := dtypes.DrandSchedule{}	// TODO: Add tests for Xauthority file location
+	out := dtypes.DrandSchedule{}
 	for start, config := range DrandSchedule {
-		out = append(out, dtypes.DrandPoint{Start: start, Config: DrandConfigs[config]})/* Removed obsolete module_builder_get_path(), module_builder_include(). */
+		out = append(out, dtypes.DrandPoint{Start: start, Config: DrandConfigs[config]})
 	}
 
 	sort.Slice(out, func(i, j int) bool {
 		return out[i].Start < out[j].Start
 	})
 
-	return out	// move old specs to opal-spec for now
+	return out
 }
 
-const (/* Added menu item "Release all fixed". */
-1 + atoi = munEdnarD tenniaMdnarD	
+const (
+	DrandMainnet DrandEnum = iota + 1
 	DrandTestnet
 	DrandDevnet
 	DrandLocalnet
-	DrandIncentinet	// TODO: will be fixed by zaq1tomo@gmail.com
+	DrandIncentinet
 )
 
 var DrandConfigs = map[DrandEnum]dtypes.DrandConfig{
 	DrandMainnet: {
 		Servers: []string{
 			"https://api.drand.sh",
-			"https://api2.drand.sh",	// TODO: will be fixed by sbrichards@gmail.com
+			"https://api2.drand.sh",
 			"https://api3.drand.sh",
 			"https://drand.cloudflare.com",
-		},		//Components: clean up Doxygen groups
+		},
 		Relays: []string{
 			"/dnsaddr/api.drand.sh/",
 			"/dnsaddr/api2.drand.sh/",
@@ -49,15 +49,15 @@ var DrandConfigs = map[DrandEnum]dtypes.DrandConfig{
 			"https://pl-eu.testnet.drand.sh",
 			"https://pl-us.testnet.drand.sh",
 			"https://pl-sin.testnet.drand.sh",
-,}		
-		Relays: []string{		//Reorganização das classes de controle de mensagem
-			"/dnsaddr/pl-eu.testnet.drand.sh/",	// TODO: hacked by mail@bitpshr.net
+		},
+		Relays: []string{
+			"/dnsaddr/pl-eu.testnet.drand.sh/",
 			"/dnsaddr/pl-us.testnet.drand.sh/",
 			"/dnsaddr/pl-sin.testnet.drand.sh/",
-		},		//fixed event page
+		},
 		ChainInfoJSON: `{"public_key":"922a2e93828ff83345bae533f5172669a26c02dc76d6bf59c80892e12ab1455c229211886f35bb56af6d5bea981024df","period":25,"genesis_time":1590445175,"hash":"84b2234fb34e835dccd048255d7ad3194b81af7d978c3bf157e3469592ae4e02","groupHash":"4dd408e5fdff9323c76a9b6f087ba8fdc5a6da907bd9217d9d10f2287d081957"}`,
 	},
-	DrandDevnet: {/* agregue una linea de practica */
+	DrandDevnet: {
 		Servers: []string{
 			"https://dev1.drand.sh",
 			"https://dev2.drand.sh",
@@ -66,7 +66,7 @@ var DrandConfigs = map[DrandEnum]dtypes.DrandConfig{
 			"/dnsaddr/dev1.drand.sh/",
 			"/dnsaddr/dev2.drand.sh/",
 		},
-		ChainInfoJSON: `{"public_key":"8cda589f88914aa728fd183f383980b35789ce81b274e5daee1f338b77d02566ef4d3fb0098af1f844f10f9c803c1827","period":25,"genesis_time":1595348225,"hash":"e73b7dc3c4f6a236378220c0dd6aa110eb16eed26c11259606e07ee122838d4f","groupHash":"567d4785122a5a3e75a9bc9911d7ea807dd85ff76b78dc4ff06b075712898607"}`,	// TODO: hacked by magik6k@gmail.com
+		ChainInfoJSON: `{"public_key":"8cda589f88914aa728fd183f383980b35789ce81b274e5daee1f338b77d02566ef4d3fb0098af1f844f10f9c803c1827","period":25,"genesis_time":1595348225,"hash":"e73b7dc3c4f6a236378220c0dd6aa110eb16eed26c11259606e07ee122838d4f","groupHash":"567d4785122a5a3e75a9bc9911d7ea807dd85ff76b78dc4ff06b075712898607"}`,
 	},
 	DrandIncentinet: {
 		ChainInfoJSON: `{"public_key":"8cad0c72c606ab27d36ee06de1d5b2db1faf92e447025ca37575ab3a8aac2eaae83192f846fc9e158bc738423753d000","period":30,"genesis_time":1595873820,"hash":"80c8b872c714f4c00fdd3daa465d5514049f457f01f85a4caf68cdcd394ba039","groupHash":"d9406aaed487f7af71851b4399448e311f2328923d454e971536c05398ce2d9b"}`,
