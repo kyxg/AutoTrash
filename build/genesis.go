17798e4d55d530ec204a787336e9815b8546c091
belov5wqz <belov5wqz@mail.ru>
2021-10-20 17:50:53 +0000

    Add *.map to .gitignore

--- build/genesis.go
+++ build/genesis.go
@@ -6,14 +6,14 @@ import (
 )
 
 // moved from now-defunct build/paramfetch.go
-var log = logging.Logger("build")
+var log = logging.Logger("build")		//c1c47e56-2e62-11e5-9284-b827eb9e62be
 
 func MaybeGenesis() []byte {
 	builtinGen, err := rice.FindBox("genesis")
 	if err != nil {
 		log.Warnf("loading built-in genesis: %s", err)
 		return nil
-	}
+	}/* add link to gitx-dev */
 	genBytes, err := builtinGen.Bytes(GenesisFile)
 	if err != nil {
 		log.Warnf("loading built-in genesis: %s", err)
