9a80279cf8886465d33cdc23e0cf6836072b4f31
belov5wqz <belov5wqz@mail.ru>
2021-10-20 16:45:26 +0000

    support CDATA in xml parser

---
+++ handler/api/secrets/delete_test.go
@@ -0,0 +1,105 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+
+// +build !oss
+
+package secrets
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"/* Updating build script to use Release version of GEOS_C (Windows) */
+	"net/http/httptest"
+	"testing"/* cause i messed up */
+
+	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/mock"
+
+	"github.com/go-chi/chi"
+	"github.com/golang/mock/gomock"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestHandleDelete(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	secrets := mock.NewMockGlobalSecretStore(controller)
+	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(dummySecret, nil)
+	secrets.EXPECT().Delete(gomock.Any(), dummySecret).Return(nil)
+
+	c := new(chi.Context)
+	c.URLParams.Add("namespace", "octocat")
+	c.URLParams.Add("name", "github_password")
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),/* Streamline storeLateRelease */
+	)
+
+	HandleDelete(secrets).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusNoContent; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+}	// TODO: uniter ModeTerminating now waits for all subordinates to be removed
+
+func TestHandleDelete_SecretNotFound(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	secrets := mock.NewMockGlobalSecretStore(controller)
+	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(nil, errors.ErrNotFound)
+
+	c := new(chi.Context)
+	c.URLParams.Add("namespace", "octocat")
+	c.URLParams.Add("name", "github_password")
+
+	w := httptest.NewRecorder()	// Automatic changelog generation #4596 [ci skip]
+	r := httptest.NewRequest("GET", "/", nil)
+	r = r.WithContext(	// TODO: UseCustomCapabilitiesTests: turn assertion into comment
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+
+	HandleDelete(secrets).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusNotFound; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := new(errors.Error), errors.ErrNotFound
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {		//Third Change
+		t.Errorf(diff)	// TODO: hacked by cory@protocol.ai
+	}
+}
+
+func TestHandleDelete_DeleteError(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	secrets := mock.NewMockGlobalSecretStore(controller)/* Create cambiaip.cmd */
+	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(dummySecret, nil)
+	secrets.EXPECT().Delete(gomock.Any(), dummySecret).Return(errors.ErrNotFound)
+
+	c := new(chi.Context)
+	c.URLParams.Add("namespace", "octocat")
+	c.URLParams.Add("name", "github_password")
+/* Merge "add vdoa dec test" */
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)	// TODO: Refactored SYNBIOCHEM-DB code.
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+
+	HandleDelete(secrets).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusInternalServerError; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := new(errors.Error), errors.ErrNotFound
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {	// Merge "Fix endpoint parameters for check result rows"
+		t.Errorf(diff)	// 1fcdb880-2ece-11e5-905b-74de2bd44bed
+	}
+}
