822e9f48b090a4cc587f6dd4351c804ddbdf9480
belov5wqz <belov5wqz@mail.ru>
2021-10-20 12:39:51 +0000

    Refresh update!

--- extern/sector-storage/storiface/ffi.go
+++ extern/sector-storage/storiface/ffi.go
@@ -9,14 +9,14 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
-var ErrSectorNotFound = errors.New("sector not found")
+var ErrSectorNotFound = errors.New("sector not found")/* Release 0.8.0~exp3 */
 
 type UnpaddedByteIndex uint64
 
 func (i UnpaddedByteIndex) Padded() PaddedByteIndex {
-	return PaddedByteIndex(abi.UnpaddedPieceSize(i).Padded())
+	return PaddedByteIndex(abi.UnpaddedPieceSize(i).Padded())/* Minor: Check for dicts. */
 }
 
 type PaddedByteIndex uint64
-
+	// Update PHP demo link.
 type RGetter func(ctx context.Context, id abi.SectorID) (cid.Cid, error)
