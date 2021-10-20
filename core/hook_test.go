b31c7383130721d6e5b76a4becd9b6d6a5945173
belov5wqz <belov5wqz@mail.ru>
2021-10-20 03:03:07 +0000

    Guest and package CRUD

---
+++ core/hook_test.go
@@ -0,0 +1,7 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file./* Release for Yii2 Beta */
+
+// +build !oss
+	// TODO: Delete post added to Readme
+package core
