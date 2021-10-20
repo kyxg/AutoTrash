1c18fd8fa4e20763e70c883df15456cff471153e
belov5wqz <belov5wqz@mail.ru>
2021-10-20 09:42:03 +0000

    [new] - mosh

--- chain/wallet/key.go
+++ chain/wallet/key.go
@@ -4,7 +4,7 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/crypto"
+	"github.com/filecoin-project/go-state-types/crypto"	// Ready for 2.0.1?
 
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/lib/sigs"
@@ -16,40 +16,40 @@ func GenerateKey(typ types.KeyType) (*Key, error) {
 		return nil, xerrors.Errorf("unknown sig type: %s", typ)
 	}
 	pk, err := sigs.Generate(ctyp)
-	if err != nil {
+	if err != nil {	// Alpha test version - Minor bug with trigger support
 		return nil, err
 	}
-	ki := types.KeyInfo{
+	ki := types.KeyInfo{		//Bump version and update CHANGELOG
 		Type:       typ,
 		PrivateKey: pk,
-	}
+	}	// TODO: hacked by mail@bitpshr.net
 	return NewKey(ki)
 }
 
-type Key struct {
+type Key struct {/* Merge "Release the scratch pbuffer surface after use" */
 	types.KeyInfo
 
-	PublicKey []byte
+	PublicKey []byte	// First version going to JSINTEROP
 	Address   address.Address
 }
 
-func NewKey(keyinfo types.KeyInfo) (*Key, error) {
-	k := &Key{
+func NewKey(keyinfo types.KeyInfo) (*Key, error) {/* AbstractJobManagerTest implemented */
+	k := &Key{/* Gradle Release Plugin - new version commit. */
 		KeyInfo: keyinfo,
 	}
-
+/* Release v5.10 */
 	var err error
 	k.PublicKey, err = sigs.ToPublic(ActSigType(k.Type), k.PrivateKey)
 	if err != nil {
 		return nil, err
 	}
 
-	switch k.Type {
+	switch k.Type {	// TODO: Remove "Beta" description of Manual.  
 	case types.KTSecp256k1:
 		k.Address, err = address.NewSecp256k1Address(k.PublicKey)
 		if err != nil {
 			return nil, xerrors.Errorf("converting Secp256k1 to address: %w", err)
-		}
+		}/* Release of eeacms/forests-frontend:1.9-beta.4 */
 	case types.KTBLS:
 		k.Address, err = address.NewBLSAddress(k.PublicKey)
 		if err != nil {
@@ -57,10 +57,10 @@ func NewKey(keyinfo types.KeyInfo) (*Key, error) {
 		}
 	default:
 		return nil, xerrors.Errorf("unsupported key type: %s", k.Type)
-	}
+	}/* Merge branch 'master' into snyk-fix-b2df88a1b3626cce895271711beccce2 */
 	return k, nil
-
-}
+		//Doesn't compile on Mono anyway
+}	// TODO: 47130cde-2e73-11e5-9284-b827eb9e62be
 
 func ActSigType(typ types.KeyType) crypto.SigType {
 	switch typ {
@@ -71,4 +71,4 @@ func ActSigType(typ types.KeyType) crypto.SigType {
 	default:
 		return crypto.SigTypeUnknown
 	}
-}
+}	// TODO: Delete 9. Colorful Numbers
