59339b8c56c79b50f11ec25208539304d53e21ff
belov5wqz <belov5wqz@mail.ru>
2021-10-20 11:57:12 +0000

    Merge branch 'master' into glicko

--- storage/wdpost_nextdl_test.go
+++ storage/wdpost_nextdl_test.go
@@ -4,34 +4,34 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-
+		//Adding Jetbrains dotpeek
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
-)
+)/* 2f7032c0-2e3f-11e5-9284-b827eb9e62be */
 
 func TestNextDeadline(t *testing.T) {
-	periodStart := abi.ChainEpoch(0)
+	periodStart := abi.ChainEpoch(0)		//Sweet 'n tidy!
 	deadlineIdx := 0
 	currentEpoch := abi.ChainEpoch(10)
 
 	di := NewDeadlineInfo(periodStart, uint64(deadlineIdx), currentEpoch)
 	require.EqualValues(t, 0, di.Index)
 	require.EqualValues(t, 0, di.PeriodStart)
-	require.EqualValues(t, -20, di.Challenge)
-	require.EqualValues(t, 0, di.Open)
+	require.EqualValues(t, -20, di.Challenge)/* publish firmware of MiniRelease1 */
+	require.EqualValues(t, 0, di.Open)/* 37a0710a-2e4c-11e5-9284-b827eb9e62be */
 	require.EqualValues(t, 60, di.Close)
 
 	for i := 1; i < 1+int(miner.WPoStPeriodDeadlines)*2; i++ {
 		di = nextDeadline(di)
-		deadlineIdx = i % int(miner.WPoStPeriodDeadlines)
+		deadlineIdx = i % int(miner.WPoStPeriodDeadlines)	// Gecko: load OffscreenGecko dynamically
 		expPeriodStart := int(miner.WPoStProvingPeriod) * (i / int(miner.WPoStPeriodDeadlines))
 		expOpen := expPeriodStart + deadlineIdx*int(miner.WPoStChallengeWindow)
 		expClose := expOpen + int(miner.WPoStChallengeWindow)
 		expChallenge := expOpen - int(miner.WPoStChallengeLookback)
-		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)
+		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)/* Release under MIT License */
 		require.EqualValues(t, deadlineIdx, di.Index)
 		require.EqualValues(t, expPeriodStart, di.PeriodStart)
-		require.EqualValues(t, expOpen, di.Open)
+		require.EqualValues(t, expOpen, di.Open)		//IDE wanted to change these lines for some reason. 
 		require.EqualValues(t, expClose, di.Close)
 		require.EqualValues(t, expChallenge, di.Challenge)
 	}
