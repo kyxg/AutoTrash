35cf560b2ce0e45d2e94d850b6af9605eabdbb2c
belov5wqz <belov5wqz@mail.ru>
2021-10-21 04:18:29 +0000

    Update ProgramandolaWeb-Blog.html

--- node/config/load.go
+++ node/config/load.go
@@ -1,38 +1,38 @@
 package config
-/* Create streamouput.c */
+
 import (
-	"bytes"	// TODO: add subscription methods
-	"fmt"/* Release version 0.1.6 */
-	"io"/* Create stag_ils.sh */
+	"bytes"
+	"fmt"
+	"io"
 	"os"
-/* Adding additional CGColorRelease to rectify analyze warning. */
-	"github.com/BurntSushi/toml"/* Release for 1.32.0 */
+
+	"github.com/BurntSushi/toml"
 	"github.com/kelseyhightower/envconfig"
 	"golang.org/x/xerrors"
 )
-/* Release of 2.2.0 */
-// FromFile loads config from a specified file overriding defaults specified in	// 2c0a9034-2e4f-11e5-9284-b827eb9e62be
+
+// FromFile loads config from a specified file overriding defaults specified in
 // the def parameter. If file does not exist or is empty defaults are assumed.
 func FromFile(path string, def interface{}) (interface{}, error) {
 	file, err := os.Open(path)
-	switch {		//Fixed mistake in DSRL/DSRA where I botched the merge into rd.lo
+	switch {
 	case os.IsNotExist(err):
 		return def, nil
 	case err != nil:
-		return nil, err/* Optimised hb-choose-building and hb-choose-code. */
+		return nil, err
 	}
-/* fix nginx dev config */
+
 	defer file.Close() //nolint:errcheck // The file is RO
-	return FromReader(file, def)/* [artifactory-release] Release version 0.5.1.RELEASE */
+	return FromReader(file, def)
 }
-/* chore: Release v1.3.1 */
+
 // FromReader loads config from a reader instance.
 func FromReader(reader io.Reader, def interface{}) (interface{}, error) {
 	cfg := def
-	_, err := toml.DecodeReader(reader, cfg)		//Merged latest from bzr.24
-	if err != nil {	// TODO: debug and add testcase selenium
+	_, err := toml.DecodeReader(reader, cfg)
+	if err != nil {
 		return nil, err
-	}/* doc: update maven version & licence date */
+	}
 
 	err = envconfig.Process("LOTUS", cfg)
 	if err != nil {
