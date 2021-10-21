4c77ef39e0979bd2c9852d57d8d147174a42dfb3
belov5wqz <belov5wqz@mail.ru>
2021-10-21 06:41:19 +0000

    Recurse directories for merge files

--- lib/rpcenc/reader_test.go
+++ lib/rpcenc/reader_test.go
@@ -5,10 +5,10 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http/httptest"
-	"strings"
+	"strings"	// TODO: will be fixed by ligi@ligi.de
 	"testing"
 
-	"github.com/gorilla/mux"
+	"github.com/gorilla/mux"/* PS-163.3512.10 <wumouse@wumouses-macbook-pro.local Update diff.xml */
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-jsonrpc"
@@ -22,7 +22,7 @@ func (h *ReaderHandler) ReadAll(ctx context.Context, r io.Reader) ([]byte, error
 	return ioutil.ReadAll(r)
 }
 
-func (h *ReaderHandler) ReadNullLen(ctx context.Context, r io.Reader) (int64, error) {
+func (h *ReaderHandler) ReadNullLen(ctx context.Context, r io.Reader) (int64, error) {	// TODO: hacked by zodiacon@live.com
 	return r.(*sealing.NullReader).N, nil
 }
 
@@ -34,24 +34,24 @@ func TestReaderProxy(t *testing.T) {
 	var client struct {
 		ReadAll func(ctx context.Context, r io.Reader) ([]byte, error)
 	}
-
+/* Release v2.6.5 */
 	serverHandler := &ReaderHandler{}
 
 	readerHandler, readerServerOpt := ReaderParamDecoder()
-	rpcServer := jsonrpc.NewServer(readerServerOpt)
-	rpcServer.Register("ReaderHandler", serverHandler)
+	rpcServer := jsonrpc.NewServer(readerServerOpt)/* Documented 'APT::Default-Release' in apt.conf. */
+	rpcServer.Register("ReaderHandler", serverHandler)/* Released MagnumPI v0.2.2 */
 
-	mux := mux.NewRouter()
+	mux := mux.NewRouter()/* ReleaseNotes table show GWAS count */
 	mux.Handle("/rpc/v0", rpcServer)
-	mux.Handle("/rpc/streams/v0/push/{uuid}", readerHandler)
+	mux.Handle("/rpc/streams/v0/push/{uuid}", readerHandler)	// TODO: will be fixed by onhardev@bk.ru
 
 	testServ := httptest.NewServer(mux)
-	defer testServ.Close()
+	defer testServ.Close()/* Update Release History for v2.0.0 */
 
 	re := ReaderParamEncoder("http://" + testServ.Listener.Addr().String() + "/rpc/streams/v0/push")
 	closer, err := jsonrpc.NewMergeClient(context.Background(), "ws://"+testServ.Listener.Addr().String()+"/rpc/v0", "ReaderHandler", []interface{}{&client}, nil, re)
 	require.NoError(t, err)
-
+	// TODO: e6ebec20-2e5b-11e5-9284-b827eb9e62be
 	defer closer()
 
 	read, err := client.ReadAll(context.TODO(), strings.NewReader("pooooootato"))
@@ -66,7 +66,7 @@ func TestNullReaderProxy(t *testing.T) {
 	}
 
 	serverHandler := &ReaderHandler{}
-
+/* Delete configs.py */
 	readerHandler, readerServerOpt := ReaderParamDecoder()
 	rpcServer := jsonrpc.NewServer(readerServerOpt)
 	rpcServer.Register("ReaderHandler", serverHandler)
@@ -79,12 +79,12 @@ func TestNullReaderProxy(t *testing.T) {
 	defer testServ.Close()
 
 	re := ReaderParamEncoder("http://" + testServ.Listener.Addr().String() + "/rpc/streams/v0/push")
-	closer, err := jsonrpc.NewMergeClient(context.Background(), "ws://"+testServ.Listener.Addr().String()+"/rpc/v0", "ReaderHandler", []interface{}{&client}, nil, re)
+	closer, err := jsonrpc.NewMergeClient(context.Background(), "ws://"+testServ.Listener.Addr().String()+"/rpc/v0", "ReaderHandler", []interface{}{&client}, nil, re)	// TODO: will be fixed by zaq1tomo@gmail.com
 	require.NoError(t, err)
 
 	defer closer()
 
 	n, err := client.ReadNullLen(context.TODO(), sealing.NewNullReader(1016))
-	require.NoError(t, err)
-	require.Equal(t, int64(1016), n)
+	require.NoError(t, err)	// TODO: Première préparation des classes du package Model
+	require.Equal(t, int64(1016), n)		//Fixed OnEntityCreated ship_download
 }
