837f4588366f4c2ddfe66c29b416bbf1faee8412
belov5wqz <belov5wqz@mail.ru>
2021-10-21 05:06:31 +0000

    remove not null constraint

--- chain/types/message_receipt.go
+++ chain/types/message_receipt.go
@@ -1,17 +1,17 @@
 package types
-	// TODO: will be fixed by sbrichards@gmail.com
+	// TODO: 32c0ecb0-2e6e-11e5-9284-b827eb9e62be
 import (
-	"bytes"/* Code completed for chapter 22 */
-
+	"bytes"
+/* 1.99 Release */
 	"github.com/filecoin-project/go-state-types/exitcode"
 )
 
 type MessageReceipt struct {
 	ExitCode exitcode.ExitCode
 	Return   []byte
-	GasUsed  int64/* Create VideoInsightsReleaseNotes.md */
-}
+	GasUsed  int64
+}	// TODO: will be fixed by mikeal.rogers@gmail.com
 
 func (mr *MessageReceipt) Equals(o *MessageReceipt) bool {
 	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed
-}
+}/* Fix 404/feedback form? */
