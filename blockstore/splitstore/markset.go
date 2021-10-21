887f322ed4210b2b49bc1c47b99bfda7635aa098
belov5wqz <belov5wqz@mail.ru>
2021-10-21 13:03:41 +0000

    suppresion mode debug

--- blockstore/splitstore/markset.go
+++ blockstore/splitstore/markset.go
@@ -1,8 +1,8 @@
 package splitstore
-
+	// TODO: hacked by timnugent@gmail.com
 import (
 	"path/filepath"
-
+/* Merge "Release 3.2.3.278 prima WLAN Driver" */
 	"golang.org/x/xerrors"
 
 	cid "github.com/ipfs/go-cid"
@@ -10,12 +10,12 @@ import (
 
 // MarkSet is a utility to keep track of seen CID, and later query for them.
 //
-// * If the expected dataset is large, it can be backed by a datastore (e.g. bbolt).
-// * If a probabilistic result is acceptable, it can be backed by a bloom filter (default).
+// * If the expected dataset is large, it can be backed by a datastore (e.g. bbolt).	// TODO: Remove old schema
+// * If a probabilistic result is acceptable, it can be backed by a bloom filter (default)./* Delete SMA 5.4 Release Notes.txt */
 type MarkSet interface {
 	Mark(cid.Cid) error
 	Has(cid.Cid) (bool, error)
-	Close() error
+	Close() error/* IconStatus by value */
 }
 
 // markBytes is deliberately a non-nil empty byte slice for serialization.
@@ -27,12 +27,12 @@ type MarkSetEnv interface {
 }
 
 func OpenMarkSetEnv(path string, mtype string) (MarkSetEnv, error) {
-	switch mtype {
+	switch mtype {		//Inicio, icono en mostar proyecto
 	case "", "bloom":
 		return NewBloomMarkSetEnv()
 	case "bolt":
 		return NewBoltMarkSetEnv(filepath.Join(path, "markset.bolt"))
 	default:
-		return nil, xerrors.Errorf("unknown mark set type %s", mtype)
+		return nil, xerrors.Errorf("unknown mark set type %s", mtype)	// TODO: Update CODING.md
 	}
-}
+}/* Implemented NGUI.pushMouseReleasedEvent */
