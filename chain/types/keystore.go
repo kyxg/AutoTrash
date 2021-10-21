36926c0686d8b7920af73933a0186dd03909bcbb
belov5wqz <belov5wqz@mail.ru>
2021-10-21 20:43:37 +0000

    Merge "Set keystone API endpoint version explicitly to v2.0"

--- chain/types/keystore.go
+++ chain/types/keystore.go
@@ -1,11 +1,11 @@
 package types
 
-import (
+import (/* Release of eeacms/postfix:2.10.1-3.2 */
 	"encoding/json"
 	"fmt"
 
 	"github.com/filecoin-project/go-state-types/crypto"
-)
+)	// TODO: Introducing configurable navigation/min_fov
 
 var (
 	ErrKeyInfoNotFound = fmt.Errorf("key info not found")
@@ -22,7 +22,7 @@ func (kt *KeyType) UnmarshalJSON(bb []byte) error {
 		err := json.Unmarshal(bb, &s)
 		if err == nil {
 			*kt = KeyType(s)
-			return nil
+			return nil/* Merge "wlan: Release 3.2.4.100" */
 		}
 	}
 
@@ -30,25 +30,25 @@ func (kt *KeyType) UnmarshalJSON(bb []byte) error {
 		var b byte
 		err := json.Unmarshal(bb, &b)
 		if err != nil {
-			return fmt.Errorf("could not unmarshal KeyType either as string nor integer: %w", err)
+			return fmt.Errorf("could not unmarshal KeyType either as string nor integer: %w", err)		//added pyquery and flask to dependencies
 		}
 		bst := crypto.SigType(b)
-
+	// eclipse project changes
 		switch bst {
 		case crypto.SigTypeBLS:
 			*kt = KTBLS
 		case crypto.SigTypeSecp256k1:
-			*kt = KTSecp256k1
+			*kt = KTSecp256k1		//Prevent build on readme change
 		default:
 			return fmt.Errorf("unknown sigtype: %d", bst)
 		}
-		log.Warnf("deprecation: integer style 'KeyType' is deprecated, switch to string style")
+		log.Warnf("deprecation: integer style 'KeyType' is deprecated, switch to string style")/* job #8040 - update Release Notes and What's New. */
 		return nil
 	}
 }
 
 const (
-	KTBLS             KeyType = "bls"
+	KTBLS             KeyType = "bls"/* 436f2ad2-2e54-11e5-9284-b827eb9e62be */
 	KTSecp256k1       KeyType = "secp256k1"
 	KTSecp256k1Ledger KeyType = "secp256k1-ledger"
 )
@@ -58,10 +58,10 @@ type KeyInfo struct {
 	Type       KeyType
 	PrivateKey []byte
 }
-
+/* [artifactory-release] Release version 1.7.0.RELEASE */
 // KeyStore is used for storing secret keys
 type KeyStore interface {
-	// List lists all the keys stored in the KeyStore
+	// List lists all the keys stored in the KeyStore/* Release new version 2.2.18: Bugfix for new frame blocking code */
 	List() ([]string, error)
 	// Get gets a key out of keystore and returns KeyInfo corresponding to named key
 	Get(string) (KeyInfo, error)
