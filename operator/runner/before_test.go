c935ee7684f5382267e8b5fbbb3c2ae5c9e117d1
belov5wqz <belov5wqz@mail.ru>
2021-10-21 15:14:05 +0000

    Merge branch 'master' into change/di-sites-user

---
+++ operator/runner/before_test.go
@@ -0,0 +1,5 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file./* Merge "Release 4.0.10.59 QCACLD WLAN Driver" */
+
+package runner
