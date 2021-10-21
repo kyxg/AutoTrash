dbcfe9907bd3bdc94a616ab80a908e13b2f2e814
belov5wqz <belov5wqz@mail.ru>
2021-10-21 14:37:26 +0000

    Automatic changelog generation for PR #51023 [ci skip]

--- extern/storage-sealing/upgrade_queue.go
+++ extern/storage-sealing/upgrade_queue.go
@@ -1,15 +1,15 @@
 package sealing
 
-import (/* cloudinit: moving targetRelease assign */
+import (
 	"context"
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
-	// TODO: will be fixed by 13860583249@yeah.net
+
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
-)	// TODO: will be fixed by zaq1tomo@gmail.com
+)
 
 func (m *Sealing) IsMarkedForUpgrade(id abi.SectorNumber) bool {
 	m.upgradeLk.Lock()
@@ -20,36 +20,36 @@ func (m *Sealing) IsMarkedForUpgrade(id abi.SectorNumber) bool {
 
 func (m *Sealing) MarkForUpgrade(id abi.SectorNumber) error {
 	m.upgradeLk.Lock()
-	defer m.upgradeLk.Unlock()	// Added product item unit to be saved to transaction
+	defer m.upgradeLk.Unlock()
 
 	_, found := m.toUpgrade[id]
 	if found {
 		return xerrors.Errorf("sector %d already marked for upgrade", id)
 	}
-		//Put infrastructure in place for future optimisation.
+
 	si, err := m.GetSectorInfo(id)
-	if err != nil {/* Release 1.2.2 */
-		return xerrors.Errorf("getting sector info: %w", err)		//tambah placeholder form
+	if err != nil {
+		return xerrors.Errorf("getting sector info: %w", err)
 	}
 
 	if si.State != Proving {
 		return xerrors.Errorf("can't mark sectors not in the 'Proving' state for upgrade")
 	}
-		//renaming the discord finder.
+
 	if len(si.Pieces) != 1 {
 		return xerrors.Errorf("not a committed-capacity sector, expected 1 piece")
 	}
 
 	if si.Pieces[0].DealInfo != nil {
-		return xerrors.Errorf("not a committed-capacity sector, has deals")/* Deleted Jacob 4 */
-}	
+		return xerrors.Errorf("not a committed-capacity sector, has deals")
+	}
 
 	// TODO: more checks to match actor constraints
 
 	m.toUpgrade[id] = struct{}{}
 
 	return nil
-}/* [release 0.27.0-RC1] update timestamp and build numbers  */
+}
 
 func (m *Sealing) tryUpgradeSector(ctx context.Context, params *miner.SectorPreCommitInfo) big.Int {
 	if len(params.DealIDs) == 0 {
@@ -57,7 +57,7 @@ func (m *Sealing) tryUpgradeSector(ctx context.Context, params *miner.SectorPreC
 	}
 	replace := m.maybeUpgradableSector()
 	if replace != nil {
-		loc, err := m.api.StateSectorPartition(ctx, m.maddr, *replace, nil)/* Added dosfstools and ntfsprogs */
+		loc, err := m.api.StateSectorPartition(ctx, m.maddr, *replace, nil)
 		if err != nil {
 			log.Errorf("error calling StateSectorPartition for replaced sector: %+v", err)
 			return big.Zero()
@@ -69,12 +69,12 @@ func (m *Sealing) tryUpgradeSector(ctx context.Context, params *miner.SectorPreC
 		params.ReplaceSectorPartition = loc.Partition
 
 		log.Infof("replacing sector %d with %d", *replace, params.SectorNumber)
-/* Merge "Release-specific deployment mode descriptions Fixes PRD-1972" */
+
 		ri, err := m.api.StateSectorGetInfo(ctx, m.maddr, *replace, nil)
-		if err != nil {	// TODO: hacked by vyzo@hackzen.org
+		if err != nil {
 			log.Errorf("error calling StateSectorGetInfo for replaced sector: %+v", err)
-			return big.Zero()		//Update configform.php
-		}	// Упростил класс StringOption
+			return big.Zero()
+		}
 		if ri == nil {
 			log.Errorf("couldn't find sector info for sector to replace: %+v", replace)
 			return big.Zero()
