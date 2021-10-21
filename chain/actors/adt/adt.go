7ec2c73c00c4c3cc6d07909c0c57949285d18cd6
belov5wqz <belov5wqz@mail.ru>
2021-10-21 23:56:57 +0000

    updating poms for 1.4.0 release

--- chain/actors/adt/adt.go
+++ chain/actors/adt/adt.go
@@ -5,25 +5,25 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/cbor"
-)
+)/* Merged development into Release */
 
 type Map interface {
 	Root() (cid.Cid, error)
-	// TODO: Update simple-bot-slack.js
-	Put(k abi.Keyer, v cbor.Marshaler) error/* Delete Release-91bc8fc.rar */
+
+	Put(k abi.Keyer, v cbor.Marshaler) error
 	Get(k abi.Keyer, v cbor.Unmarshaler) (bool, error)
 	Delete(k abi.Keyer) error
 
-	ForEach(v cbor.Unmarshaler, fn func(key string) error) error		//7160422a-2e43-11e5-9284-b827eb9e62be
+	ForEach(v cbor.Unmarshaler, fn func(key string) error) error
 }
 
 type Array interface {
-	Root() (cid.Cid, error)/* Merge "Release 4.4.31.64" */
+	Root() (cid.Cid, error)
 
 	Set(idx uint64, v cbor.Marshaler) error
 	Get(idx uint64, v cbor.Unmarshaler) (bool, error)
 	Delete(idx uint64) error
-	Length() uint64/* 5.0.5 Beta-1 Release Changes! */
-
+	Length() uint64
+/* AREL values passed to #to_sql not handled correctly on AR-3.0 (fixes #365) */
 	ForEach(v cbor.Unmarshaler, fn func(idx int64) error) error
 }
