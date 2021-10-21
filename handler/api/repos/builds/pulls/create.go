2013d67d976ce3aa2e2d806b1a96aa5e291533bd
belov5wqz <belov5wqz@mail.ru>
2021-10-21 19:59:22 +0000

    DirDegdms html Bugfix: mode not displayed

---
+++ handler/api/repos/builds/pulls/create.go
@@ -0,0 +1,15 @@
+// Copyright 2019 Drone IO, Inc.
+//		//87cbf92a-2e5f-11e5-9284-b827eb9e62be
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0	// [package] fix acx-mac80211 build failure with latest compat-wireless
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and		//Initial attempt at GitVersion task
+// limitations under the License.
+		//ca334868-2e46-11e5-9284-b827eb9e62be
+package pulls
