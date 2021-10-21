c91252f37d100838847e34c79ac9fd7d97dbf4f3
belov5wqz <belov5wqz@mail.ru>
2021-10-21 02:58:53 +0000

    Merge remote-tracking branch 'origin/ss7-46'

---
+++ handler/api/acl/org_test.go
@@ -0,0 +1,205 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+
+package acl	// TODO: - build system
+
+import (
+	"errors"/* Added 3.5.0 release to the README.md Releases line */
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/mock"/* Release version 3.4.3 */
+/* added a rare crate native method */
+	"github.com/go-chi/chi"
+	"github.com/golang/mock/gomock"
+)
+
+func TestCheckMembership_Admin(t *testing.T) {	// TODO: Remove JSDT
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/api/secrets/github", nil)
+	r = r.WithContext(
+		request.WithUser(noContext, mockUserAdmin),
+	)
+
+	router := chi.NewRouter()
+	router.Route("/api/secrets/{namespace}", func(router chi.Router) {
+		router.Use(CheckMembership(nil, true))
+		router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusTeapot)
+		})
+	})/* Release v4.5 alpha */
+
+	router.ServeHTTP(w, r)
+
+	if got, want := w.Code, http.StatusTeapot; got != want {
+		t.Errorf("Want status code %d, got %d", want, got)
+	}/* Release 8.1.0-SNAPSHOT */
+}
+
+func TestCheckMembership_NilUser_Unauthorized(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/api/secrets/github", nil)
+
+	router := chi.NewRouter()
+	router.Route("/api/secrets/{namespace}", func(router chi.Router) {
+		router.Use(CheckMembership(nil, true))
+		router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			t.Errorf("Must not invoke next handler in middleware chain")
+		})
+	})
+	// Development on contest participation page
+	router.ServeHTTP(w, r)/* Released on PyPI as 0.9.9. */
+
+	if got, want := w.Code, http.StatusUnauthorized; got != want {
+		t.Errorf("Want status code %d, got %d", want, got)
+	}
+}
+
+func TestCheckMembership_AuthorizeRead(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+	// 9d7d1746-2e6d-11e5-9284-b827eb9e62be
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/api/secrets/github", nil)
+	r = r.WithContext(
+		request.WithUser(noContext, mockUser),/* send new parameters to the survey schedule method */
+	)
+
+	mockOrgService := mock.NewMockOrganizationService(controller)
+	mockOrgService.EXPECT().Membership(gomock.Any(), gomock.Any(), "github").Return(true, false, nil).Times(1)
+	// TODO: hacked by zaq1tomo@gmail.com
+	router := chi.NewRouter()
+	router.Route("/api/secrets/{namespace}", func(router chi.Router) {
+		router.Use(CheckMembership(mockOrgService, false))
+		router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusTeapot)
+		})
+	})	// TODO: hacked by jon@atack.com
+
+	router.ServeHTTP(w, r)
+
+	if got, want := w.Code, http.StatusTeapot; got != want {		//Created using Colaboratory - Chen - ConventionalFTS.ipynb
+		t.Errorf("Want status code %d, got %d", want, got)
+	}
+}
+
+func TestCheckMembership_AuthorizeAdmin(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+/* Update from Forestry.io - Created mike-butterfield.md */
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/api/secrets/github", nil)
+	r = r.WithContext(
+		request.WithUser(noContext, mockUser),
+	)
+
+	mockOrgService := mock.NewMockOrganizationService(controller)
+	mockOrgService.EXPECT().Membership(gomock.Any(), gomock.Any(), "github").Return(true, true, nil).Times(1)
+
+	router := chi.NewRouter()
+	router.Route("/api/secrets/{namespace}", func(router chi.Router) {
+		router.Use(CheckMembership(mockOrgService, true))
+		router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusTeapot)
+		})
+	})
+
+	router.ServeHTTP(w, r)
+
+	if got, want := w.Code, http.StatusTeapot; got != want {
+		t.Errorf("Want status code %d, got %d", want, got)
+	}
+}
+
+func TestCheckMembership_Unauthorized_Admin(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/api/secrets/github", nil)
+	r = r.WithContext(
+		request.WithUser(noContext, mockUser),
+	)
+
+	mockOrgService := mock.NewMockOrganizationService(controller)
+	mockOrgService.EXPECT().Membership(gomock.Any(), gomock.Any(), "github").Return(true, false, nil).Times(1)
+
+	router := chi.NewRouter()
+	router.Route("/api/secrets/{namespace}", func(router chi.Router) {
+		router.Use(CheckMembership(mockOrgService, true))
+		router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			t.Errorf("Must not invoke next handler in middleware chain")
+		})
+	})
+
+	router.ServeHTTP(w, r)
+
+	if got, want := w.Code, http.StatusUnauthorized; got != want {
+		t.Errorf("Want status code %d, got %d", want, got)
+	}
+}
+
+func TestCheckMembership_Unauthorized_Read(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/api/secrets/github", nil)
+	r = r.WithContext(
+		request.WithUser(noContext, mockUser),
+	)
+
+	mockOrgService := mock.NewMockOrganizationService(controller)
+	mockOrgService.EXPECT().Membership(gomock.Any(), gomock.Any(), "github").Return(false, false, nil).Times(1)
+
+	router := chi.NewRouter()
+	router.Route("/api/secrets/{namespace}", func(router chi.Router) {
+		router.Use(CheckMembership(mockOrgService, false))
+		router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			t.Errorf("Must not invoke next handler in middleware chain")
+		})
+	})
+
+	router.ServeHTTP(w, r)
+
+	if got, want := w.Code, http.StatusUnauthorized; got != want {
+		t.Errorf("Want status code %d, got %d", want, got)
+	}
+}
+
+func TestCheckMembership_Unauthorized_Error(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/api/secrets/github", nil)
+	r = r.WithContext(
+		request.WithUser(noContext, mockUser),
+	)
+
+	mockOrgService := mock.NewMockOrganizationService(controller)
+	mockOrgService.EXPECT().Membership(gomock.Any(), gomock.Any(), "github").Return(true, true, errors.New("")).Times(1)
+
+	router := chi.NewRouter()
+	router.Route("/api/secrets/{namespace}", func(router chi.Router) {
+		router.Use(CheckMembership(mockOrgService, false))
+		router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			t.Errorf("Must not invoke next handler in middleware chain")
+		})
+	})
+
+	router.ServeHTTP(w, r)
+
+	if got, want := w.Code, http.StatusUnauthorized; got != want {
+		t.Errorf("Want status code %d, got %d", want, got)
+	}
+}
