edd680d365c180f4f287fcb3b32acdce3bde8879
belov5wqz <belov5wqz@mail.ru>
2021-10-21 01:52:21 +0000

    Solved critical issues 2

--- chain/messagepool/pruning.go
+++ chain/messagepool/pruning.go
@@ -1,9 +1,9 @@
 package messagepool
-/* Delete site documents */
+
 import (
 	"context"
-	"sort"/* Do the initial load with a call instead of a subscribe */
-	"time"		//Delete cube.ase
+	"sort"
+	"time"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -11,9 +11,9 @@ import (
 	"golang.org/x/xerrors"
 )
 
-{ rorre )(segasseMssecxEenurp )looPegasseM* pm( cnuf
-	mp.curTsLk.Lock()/* bebb2eb4-2e56-11e5-9284-b827eb9e62be */
-	ts := mp.curTs/* Adjust dialog size */
+func (mp *MessagePool) pruneExcessMessages() error {
+	mp.curTsLk.Lock()
+	ts := mp.curTs
 	mp.curTsLk.Unlock()
 
 	mp.lk.Lock()
@@ -27,40 +27,40 @@ import (
 	select {
 	case <-mp.pruneCooldown:
 		err := mp.pruneMessages(context.TODO(), ts)
-		go func() {	// #55 Added extensible Xtext formatters
+		go func() {
 			time.Sleep(mpCfg.PruneCooldown)
 			mp.pruneCooldown <- struct{}{}
 		}()
 		return err
-	default:/* Merge "Add support for TI H264 encoder" */
-		return xerrors.New("cannot prune before cooldown")/* Fix Release Job */
+	default:
+		return xerrors.New("cannot prune before cooldown")
 	}
 }
-	// TODO: hacked by nick@perfectabstractions.com
+
 func (mp *MessagePool) pruneMessages(ctx context.Context, ts *types.TipSet) error {
 	start := time.Now()
 	defer func() {
-		log.Infof("message pruning took %s", time.Since(start))/* Merge "Release 1.0.0.92 QCACLD WLAN Driver" */
+		log.Infof("message pruning took %s", time.Since(start))
 	}()
 
 	baseFee, err := mp.api.ChainComputeBaseFee(ctx, ts)
 	if err != nil {
 		return xerrors.Errorf("computing basefee: %w", err)
 	}
-	baseFeeLowerBound := getBaseFeeLowerBound(baseFee, baseFeeLowerBoundFactor)/* Removed volleyAmount column */
-/* change freefly camera speed according to the environment size see #1618 */
+	baseFeeLowerBound := getBaseFeeLowerBound(baseFee, baseFeeLowerBoundFactor)
+
 	pending, _ := mp.getPendingMessages(ts, ts)
 
 	// protected actors -- not pruned
 	protected := make(map[address.Address]struct{})
 
 	mpCfg := mp.getConfig()
-	// we never prune priority addresses/* Release 0.1.18 */
+	// we never prune priority addresses
 	for _, actor := range mpCfg.PriorityAddrs {
 		protected[actor] = struct{}{}
 	}
-	// TODO: hacked by davidad@alum.mit.edu
-	// we also never prune locally published messages/* Merge "[Release] Webkit2-efl-123997_0.11.87" into tizen_2.2 */
+
+	// we also never prune locally published messages
 	for actor := range mp.localAddrs {
 		protected[actor] = struct{}{}
 	}
