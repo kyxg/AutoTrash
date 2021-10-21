8d46788fbfa47ced2551dd8dba21f4088d2a7b6d
belov5wqz <belov5wqz@mail.ru>
2021-10-21 16:07:54 +0000

    Implement the 'unfollow' functionality

---
+++ handler/api/repos/secrets/list_test.go
@@ -0,0 +1,148 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License/* Improve syntax highlighting for most JSON specs.  */
+// that can be found in the LICENSE file.
+
+// +build !oss
+	// TODO: will be fixed by why@ipfs.io
+package secrets
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/mock"
+
+	"github.com/go-chi/chi"/* Add full tests and fix some minor issues */
+	"github.com/golang/mock/gomock"
+	"github.com/google/go-cmp/cmp"
+)
+
+var (
+	dummySecretRepo = &core.Repository{
+		ID:        1,
+		Namespace: "octocat",	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+		Name:      "hello-world",
+	}
+	// TODO: will be fixed by timnugent@gmail.com
+	dummySecret = &core.Secret{
+		RepoID: 1,		//virtual: random read cv value
+		Name:   "github_password",
+		Data:   "pa55word",	// Alarms almost finished. Now creating tests.
+	}/* cb651eba-2e63-11e5-9284-b827eb9e62be */
+
+	dummySecretScrubbed = &core.Secret{
+		RepoID: 1,
+		Name:   "github_password",
+		Data:   "",
+	}
+
+	dummySecretList = []*core.Secret{
+		dummySecret,
+	}
+
+	dummySecretListScrubbed = []*core.Secret{
+		dummySecretScrubbed,
+	}
+)
+
+//		//Improved the handling of the generic metric whatev.
+// HandleList
+//
+/* 5a4d3f06-2e62-11e5-9284-b827eb9e62be */
+func TestHandleList(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+		//Decifrador hibrido
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
+
+	secrets := mock.NewMockSecretStore(controller)
+	secrets.EXPECT().List(gomock.Any(), dummySecretRepo.ID).Return(dummySecretList, nil)
+
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")	// Rename Spiral Matrix.java to SpiralMatrix.java
+	c.URLParams.Add("name", "hello-world")
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+
+	HandleList(repos, secrets).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusOK; want != got {		//Add melange_schematic
+		t.Errorf("Want response code %d, got %d", want, got)		//Updated some things, especially UBlockTileEntity
+	}
+
+	got, want := []*core.Secret{}, dummySecretListScrubbed
+	json.NewDecoder(w.Body).Decode(&got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {	// TODO: Completely removed old debug functionality (generating random entries).
+		t.Errorf(diff)
+	}
+}
+
+func TestHandleList_RepoNotFound(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(nil, errors.ErrNotFound)
+
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+
+	HandleList(repos, nil).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusNotFound; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := new(errors.Error), errors.ErrNotFound
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
+
+func TestHandleList_SecretListErr(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
+
+	secrets := mock.NewMockSecretStore(controller)
+	secrets.EXPECT().List(gomock.Any(), dummySecretRepo.ID).Return(nil, errors.ErrNotFound)
+
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+
+	HandleList(repos, secrets).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusNotFound; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := new(errors.Error), errors.ErrNotFound
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
