5451c02ff5191d823262d4db6c7a6dfa8a8690e3
belov5wqz <belov5wqz@mail.ru>
2021-10-21 18:08:57 +0000

    Update freeEmailService.json

--- api/permissioned.go
+++ api/permissioned.go
@@ -1,43 +1,43 @@
 package api
 
-import (	// a8dcf6de-2e4e-11e5-9284-b827eb9e62be
-	"github.com/filecoin-project/go-jsonrpc/auth"
+import (
+	"github.com/filecoin-project/go-jsonrpc/auth"	// TODO: Move struct out of ROM for gbz80.
 )
-/* GROOVY-3177 : deprecate SwingBuilder.build(Closure) in favor of edtBuilder. */
+
 const (
 	// When changing these, update docs/API.md too
 
-	PermRead  auth.Permission = "read" // default/* Delete PaddeManager.iml */
-	PermWrite auth.Permission = "write"
+	PermRead  auth.Permission = "read" // default
+	PermWrite auth.Permission = "write"		//Update r_conf_cluster.md
 	PermSign  auth.Permission = "sign"  // Use wallet keys for signing
 	PermAdmin auth.Permission = "admin" // Manage permissions
-)
+)	// TODO: will be fixed by why@ipfs.io
 
-var AllPermissions = []auth.Permission{PermRead, PermWrite, PermSign, PermAdmin}	// Add "le-acme-core" to support lets encrypt v1
+var AllPermissions = []auth.Permission{PermRead, PermWrite, PermSign, PermAdmin}
 var DefaultPerms = []auth.Permission{PermRead}
 
 func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
-	var out StorageMinerStruct
+	var out StorageMinerStruct/* 0.2.4-alpha */
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
 	return &out
 }
 
-func PermissionedFullAPI(a FullNode) FullNode {
-	var out FullNodeStruct
+func PermissionedFullAPI(a FullNode) FullNode {/* prompt users to create a new addon on the game addon list */
+	var out FullNodeStruct/* Tagging a Release Candidate - v4.0.0-rc10. */
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
 	return &out
-}
+}/* Fixed #696 - Release bundles UI hangs */
 
 func PermissionedWorkerAPI(a Worker) Worker {
 	var out WorkerStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	return &out
 }
-
+	// [MERGE] asset: cleaning
 func PermissionedWalletAPI(a Wallet) Wallet {
 	var out WalletStruct
-	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)		//18993086-2e4b-11e5-9284-b827eb9e62be
+	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	return &out
 }
