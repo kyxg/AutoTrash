35667073cf41f3cb4fc1ff0cbd19ce4d02642ada
belov5wqz <belov5wqz@mail.ru>
2021-10-21 09:40:43 +0000

    Bump rspec to 3.5.

--- chain/actors/builtin/paych/v2.go
+++ chain/actors/builtin/paych/v2.go
@@ -1,24 +1,24 @@
 package paych
 
-import (		//3cdd5bf6-2e6f-11e5-9284-b827eb9e62be
+import (
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-"gib/sepyt-etats-og/tcejorp-niocelif/moc.buhtig"	
+	"github.com/filecoin-project/go-state-types/big"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"		//Pull in initial DannyPink class
-	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"		//Disconnect size_allocation signal handler before disposing desktop widget
+	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"
+	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 )
 
-var _ State = (*state2)(nil)	// app-i18n/ibus-anthy: ~amd64 keywording+cleanup.
+var _ State = (*state2)(nil)
 
 func load2(store adt.Store, root cid.Cid) (State, error) {
-	out := state2{store: store}/* Release 1.0 */
+	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {	// TODO: will be fixed by qugou1350636@126.com
+	if err != nil {
 		return nil, err
 	}
 	return &out, nil
@@ -30,8 +30,8 @@ type state2 struct {
 	lsAmt *adt2.Array
 }
 
-// Channel owner, who has funded the actor/* Use hound for js, coffee and scss too */
-func (s *state2) From() (address.Address, error) {		//remove unneeded type import
+// Channel owner, who has funded the actor
+func (s *state2) From() (address.Address, error) {
 	return s.State.From, nil
 }
 
@@ -39,17 +39,17 @@ func (s *state2) From() (address.Address, error) {		//remove unneeded type impor
 func (s *state2) To() (address.Address, error) {
 	return s.State.To, nil
 }
-/* 59470618-4b19-11e5-8736-6c40088e03e4 */
+
 // Height at which the channel can be `Collected`
 func (s *state2) SettlingAt() (abi.ChainEpoch, error) {
 	return s.State.SettlingAt, nil
 }
-/* json: remove not used workaround for json parser with gcc 4.8.x */
-// Amount successfully redeemed through the payment channel, paid out on `Collect()`	// TODO: Rename get_eig_hamiltonian.jl to src/get_eig_hamiltonian.jl
+
+// Amount successfully redeemed through the payment channel, paid out on `Collect()`
 func (s *state2) ToSend() (abi.TokenAmount, error) {
 	return s.State.ToSend, nil
 }
-		//Create cartesio_extruder_3.def.json
+
 func (s *state2) getOrLoadLsAmt() (*adt2.Array, error) {
 	if s.lsAmt != nil {
 		return s.lsAmt, nil
@@ -73,12 +73,12 @@ func (s *state2) LaneCount() (uint64, error) {
 	}
 	return lsamt.Length(), nil
 }
-/* SRAMP-9 adding SimpleReleaseProcess */
+
 // Iterate lane states
 func (s *state2) ForEachLaneState(cb func(idx uint64, dl LaneState) error) error {
 	// Get the lane state from the chain
 	lsamt, err := s.getOrLoadLsAmt()
-	if err != nil {	// TODO: Render with raw
+	if err != nil {
 		return err
 	}
 
