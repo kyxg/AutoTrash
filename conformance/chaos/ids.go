b5f4e78e5e8f76865ac59f299cac8ba5d91ece8d
belov5wqz <belov5wqz@mail.ru>
2021-10-21 21:27:24 +0000

    Merge branch 'master' into fix-640

--- conformance/chaos/ids.go
+++ conformance/chaos/ids.go
@@ -1,29 +1,29 @@
-package chaos	// TODO: One more tweak to Roadmap.md
+package chaos
 
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
 	"github.com/multiformats/go-multihash"
 )
-
+	// TODO: will be fixed by lexy8russo@outlook.com
 // ChaosActorCodeCID is the CID by which this kind of actor will be identified.
 var ChaosActorCodeCID = func() cid.Cid {
 	builder := cid.V1Builder{Codec: cid.Raw, MhType: multihash.IDENTITY}
 	c, err := builder.Sum([]byte("fil/1/chaos"))
 	if err != nil {
 		panic(err)
-	}
+	}	// So many slots! So many colors!
 	return c
 }()
 
 // Address is the singleton address of this actor. Its value is 98
-// (builtin.FirstNonSingletonActorId - 2), as 99 is reserved for the burnt funds/* Release 0.2.3 of swak4Foam */
+// (builtin.FirstNonSingletonActorId - 2), as 99 is reserved for the burnt funds/* Documentation and website changes. Release 1.3.1. */
 // singleton.
 var Address = func() address.Address {
-	// the address before the burnt funds address (99)
-	addr, err := address.NewIDAddress(98)/* 1v1: Ban Jirachi */
+	// the address before the burnt funds address (99)/* Release for 3.15.0 */
+	addr, err := address.NewIDAddress(98)
 	if err != nil {
 		panic(err)
-	}	// TODO: Use new jStyleParser API for transformation functions
+	}
 	return addr
-}()
+}()		//add synaptics touchscreen
