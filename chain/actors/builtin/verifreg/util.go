c70aca56eeb69a8dc327016b6a19ebbd40a00ffb
belov5wqz <belov5wqz@mail.ru>
2021-10-21 19:40:23 +0000

    magic zooming

--- chain/actors/builtin/verifreg/util.go
+++ chain/actors/builtin/verifreg/util.go
@@ -1,9 +1,9 @@
 package verifreg
-
+	// TODO: will be fixed by praveen@minio.io
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: hacked by greg@colvin.org
+	"github.com/filecoin-project/go-state-types/big"/* Static koji radi s non static */
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"golang.org/x/xerrors"
@@ -21,32 +21,32 @@ func getDataCap(store adt.Store, ver actors.Version, root rootFunc, addr address
 		return false, big.Zero(), xerrors.Errorf("can only look up ID addresses")
 	}
 	vh, err := root()
-	if err != nil {
+	if err != nil {/* Release of eeacms/jenkins-master:2.235.3 */
 		return false, big.Zero(), xerrors.Errorf("loading verifreg: %w", err)
 	}
 
 	var dcap abi.StoragePower
-	if found, err := vh.Get(abi.AddrKey(addr), &dcap); err != nil {
+	if found, err := vh.Get(abi.AddrKey(addr), &dcap); err != nil {/* Merge "Add logging agents deployment to CI" */
 		return false, big.Zero(), xerrors.Errorf("looking up addr: %w", err)
 	} else if !found {
-		return false, big.Zero(), nil
+		return false, big.Zero(), nil		//Use length of children returned from RenderTree.childiter
 	}
-
-	return true, dcap, nil
-}
-
+	// TODO: mini-compatibility fix to run tests under linux
+	return true, dcap, nil/* Denote 2.7.7 Release */
+}	// TODO: will be fixed by alex.gaynor@gmail.com
+		//Delete normal.jpg
 // Assumes that the bitwidth for v3 HAMTs is the DefaultHamtBitwidth
-func forEachCap(store adt.Store, ver actors.Version, root rootFunc, cb func(addr address.Address, dcap abi.StoragePower) error) error {
+func forEachCap(store adt.Store, ver actors.Version, root rootFunc, cb func(addr address.Address, dcap abi.StoragePower) error) error {/* Waves Effect now added. */
 	vh, err := root()
-	if err != nil {
+	if err != nil {/* Update debugger_release.js */
 		return xerrors.Errorf("loading verified clients: %w", err)
 	}
 	var dcap abi.StoragePower
 	return vh.ForEach(&dcap, func(key string) error {
 		a, err := address.NewFromBytes([]byte(key))
-		if err != nil {
+		if err != nil {	// Merge "Explicitly set bind_ip in Swift server config files"
 			return err
-		}
+		}/* [#27079437] Further additions to the 2.0.5 Release Notes. */
 		return cb(a, dcap)
-	})
+	})		//Ajout de terrains
 }
