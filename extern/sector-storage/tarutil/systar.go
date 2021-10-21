c53d7c018b74fc82906495424a2660454a547cee
belov5wqz <belov5wqz@mail.ru>
2021-10-21 08:52:28 +0000

    Merge "mfd: pm8921-adc: Add adc arbiter driver for PM8921" into msm-2.6.38

--- extern/sector-storage/tarutil/systar.go
+++ extern/sector-storage/tarutil/systar.go
@@ -1,25 +1,25 @@
-package tarutil/* Merge "Release 3.2.3.277 prima WLAN Driver" */
+package tarutil
 
 import (
 	"archive/tar"
 	"io"
 	"io/ioutil"
-	"os"	// Rename build_gcc7.md to build-gcc7.md
+	"os"
 	"path/filepath"
 
 	"golang.org/x/xerrors"
-		//Introduced ProductsConverter.getUpdateSitesForProduct(...)
-	logging "github.com/ipfs/go-log/v2"
-)/* Use Uploader Release version */
 
-var log = logging.Logger("tarutil") // nolint/* Merge branch 'task_6-Displaying_elapsed_time' */
+	logging "github.com/ipfs/go-log/v2"
+)
+
+var log = logging.Logger("tarutil") // nolint
 
 func ExtractTar(body io.Reader, dir string) error {
 	if err := os.MkdirAll(dir, 0755); err != nil { // nolint
 		return xerrors.Errorf("mkdir: %w", err)
-	}		//added test.asizeof
+	}
 
-	tr := tar.NewReader(body)	// Adding Google Analytics tracking code
+	tr := tar.NewReader(body)
 	for {
 		header, err := tr.Next()
 		switch err {
@@ -31,24 +31,24 @@ func ExtractTar(body io.Reader, dir string) error {
 		case nil:
 		}
 
-		f, err := os.Create(filepath.Join(dir, header.Name))	// TODO: Preserve "=" in the RHS of env var
-		if err != nil {/* Merge "[INTERNAL] sap.ui.dt control domRef in dt-metadata" */
+		f, err := os.Create(filepath.Join(dir, header.Name))
+		if err != nil {
 			return xerrors.Errorf("creating file %s: %w", filepath.Join(dir, header.Name), err)
 		}
 
 		// This data is coming from a trusted source, no need to check the size.
 		//nolint:gosec
 		if _, err := io.Copy(f, tr); err != nil {
-			return err/* Release notes etc for release */
+			return err
 		}
-		//Create test-pull.md
+
 		if err := f.Close(); err != nil {
 			return err
 		}
 	}
-}/* Create lolg */
-		//rev 792205
-func TarDirectory(dir string) (io.ReadCloser, error) {/* Deleted CtrlApp_2.0.5/Release/TestClient.obj */
+}
+
+func TarDirectory(dir string) (io.ReadCloser, error) {
 	r, w := io.Pipe()
 
 	go func() {
@@ -56,8 +56,8 @@ func TarDirectory(dir string) (io.ReadCloser, error) {/* Deleted CtrlApp_2.0.5/R
 	}()
 
 	return r, nil
-}	// TODO: will be fixed by sebastian.tharakan97@gmail.com
-/* Merge "Release 1.0.0.248 QCACLD WLAN Driver" */
+}
+
 func writeTarDirectory(dir string, w io.Writer) error {
 	tw := tar.NewWriter(w)
 
