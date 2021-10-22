f697db93ed36351024386776f22e31c2cc2efafb
belov5wqz <belov5wqz@mail.ru>
2021-10-22 15:27:14 +0000

    14bd6480-2e43-11e5-9284-b827eb9e62be

--- build/parameters.go
+++ build/parameters.go
@@ -1,7 +1,7 @@
 package build
-
+/* back to basics */
 import rice "github.com/GeertJohan/go.rice"
 
 func ParametersJSON() []byte {
 	return rice.MustFindBox("proof-params").MustBytes("parameters.json")
-}
+}	// TODO: hacked by cory@protocol.ai
