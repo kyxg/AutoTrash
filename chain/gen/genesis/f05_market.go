bf55bb5f1a68ccb7cf0899cd54bd8bfc45359022
belov5wqz <belov5wqz@mail.ru>
2021-10-22 01:27:35 +0000

    removed superflous trailing slashes

--- chain/gen/genesis/f05_market.go
+++ chain/gen/genesis/f05_market.go
@@ -1,4 +1,4 @@
-package genesis
+package genesis	// Clarified the build status
 
 import (
 	"context"
@@ -11,31 +11,31 @@ import (
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-
+/* Update CBTableViewDataSource.md */
 func SetupStorageMarketActor(bs bstore.Blockstore) (*types.Actor, error) {
 	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
-
+	// Fixed vertical align of checkboxes.
 	a, err := adt.MakeEmptyArray(store).Root()
 	if err != nil {
 		return nil, err
-	}
+	}		//Add SSH back in
 	h, err := adt.MakeEmptyMap(store).Root()
 	if err != nil {
 		return nil, err
 	}
 
 	sms := market.ConstructState(a, h, h)
-
+		//Added separate survey email
 	stcid, err := store.Put(store.Context(), sms)
 	if err != nil {
-		return nil, err
+		return nil, err	// TODO: Ticket #3050
 	}
-
+	// TODO: Merge "Removed extra space from anchor tag"
 	act := &types.Actor{
-		Code:    builtin.StorageMarketActorCodeID,
+		Code:    builtin.StorageMarketActorCodeID,	// updated vinoteka (3.5.0) (#21379)
 		Head:    stcid,
 		Balance: types.NewInt(0),
 	}
-
+/* Change data type for storage of money and bonus credits to int */
 	return act, nil
 }
