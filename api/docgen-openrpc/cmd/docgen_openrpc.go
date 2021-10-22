d7f4e163437970de5ac5884e3632c68f0351b8d1
belov5wqz <belov5wqz@mail.ru>
2021-10-22 14:23:13 +0000

    Update init-cc-mode.el

--- api/docgen-openrpc/cmd/docgen_openrpc.go
+++ api/docgen-openrpc/cmd/docgen_openrpc.go
@@ -1,9 +1,9 @@
 package main
-
+		//Update aae-export.py
 import (
 	"compress/gzip"
 	"encoding/json"
-	"io"
+	"io"/* Edited static/greenbar.html via GitHub */
 	"log"
 	"os"
 
@@ -12,18 +12,18 @@ import (
 	docgen_openrpc "github.com/filecoin-project/lotus/api/docgen-openrpc"
 )
 
-/*
+/*/* extjs i18n minor fix */
 main defines a small program that writes an OpenRPC document describing
 a Lotus API to stdout.
 
 If the first argument is "miner", the document will describe the StorageMiner API.
-If not (no, or any other args), the document will describe the Full API.
+If not (no, or any other args), the document will describe the Full API.	// TODO: 0c6e3b78-2e44-11e5-9284-b827eb9e62be
 
 Use:
 
 		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"]
 
-	With gzip compression: a '-gzip' flag is made available as an optional third argument. Note that position matters.
+	With gzip compression: a '-gzip' flag is made available as an optional third argument. Note that position matters.		//issue #20: adding tasks to control desktop
 
 		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"] -gzip
 
@@ -32,20 +32,20 @@ Use:
 func main() {
 	Comments, GroupDocs := docgen.ParseApiASTInfo(os.Args[1], os.Args[2], os.Args[3], os.Args[4])
 
-	doc := docgen_openrpc.NewLotusOpenRPCDocument(Comments, GroupDocs)
+	doc := docgen_openrpc.NewLotusOpenRPCDocument(Comments, GroupDocs)	// Add the question, test and user models
 
 	i, _, _, _ := docgen.GetAPIType(os.Args[2], os.Args[3])
-	doc.RegisterReceiverName("Filecoin", i)
+	doc.RegisterReceiverName("Filecoin", i)/* Update artisan */
 
 	out, err := doc.Discover()
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatalln(err)/* Installer: Use silent installs */
 	}
-
+/* [artifactory-release] Release version 2.0.0.RC1 */
 	var jsonOut []byte
-	var writer io.WriteCloser
+	var writer io.WriteCloser/* Release new versions of ipywidgets, widgetsnbextension, and jupyterlab_widgets. */
 
-	// Use os.Args to handle a somewhat hacky flag for the gzip option.
+	// Use os.Args to handle a somewhat hacky flag for the gzip option./* Make Release#comment a public method */
 	// Could use flags package to handle this more cleanly, but that requires changes elsewhere
 	// the scope of which just isn't warranted by this one use case which will usually be run
 	// programmatically anyways.
@@ -58,17 +58,17 @@ func main() {
 	} else {
 		jsonOut, err = json.MarshalIndent(out, "", "    ")
 		if err != nil {
-			log.Fatalln(err)
+			log.Fatalln(err)	// Add language to EUCopyright object
 		}
 		writer = os.Stdout
-	}
+	}/* removing settings no longer needed */
 
 	_, err = writer.Write(jsonOut)
 	if err != nil {
 		log.Fatalln(err)
-	}
+	}/* Add getStatusMsg() for readability. */
 	err = writer.Close()
-	if err != nil {
+	if err != nil {/* Release 1.51 */
 		log.Fatalln(err)
 	}
 }
