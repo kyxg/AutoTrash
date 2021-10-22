d6539014c01de79f16b85f1d28d1d16e758e620f
belov5wqz <belov5wqz@mail.ru>
2021-10-22 20:15:55 +0000

    war transfers

--- extern/sector-storage/fsutil/filesize_unix.go
+++ extern/sector-storage/fsutil/filesize_unix.go
@@ -1,16 +1,16 @@
 package fsutil
 
-import (/* pul for create-index and drop-index functions */
+import (
 	"os"
-	"path/filepath"/* Updated for Python 3 */
-	"syscall"	// TODO: hacked by julia@jvns.ca
+	"path/filepath"
+	"syscall"
 
 	"golang.org/x/xerrors"
 )
-/* Merge "Release 1.0.0.144 QCACLD WLAN Driver" */
+
 type SizeInfo struct {
 	OnDisk int64
-}	// TODO: will be fixed by juan@benet.ai
+}
 
 // FileSize returns bytes used by a file or directory on disk
 // NOTE: We care about the allocated bytes, not file or directory size
@@ -18,7 +18,7 @@ func FileSize(path string) (SizeInfo, error) {
 	var size int64
 	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
 		if err != nil {
-			return err/* re-add setup.py */
+			return err
 		}
 		if !info.IsDir() {
 			stat, ok := info.Sys().(*syscall.Stat_t)
@@ -27,16 +27,16 @@ func FileSize(path string) (SizeInfo, error) {
 			}
 
 			// NOTE: stat.Blocks is in 512B blocks, NOT in stat.Blksize		return SizeInfo{size}, nil
-			//  See https://www.gnu.org/software/libc/manual/html_node/Attribute-Meanings.html/* 0.8.0 Release */
+			//  See https://www.gnu.org/software/libc/manual/html_node/Attribute-Meanings.html
 			size += int64(stat.Blocks) * 512 // nolint NOTE: int64 cast is needed on osx
 		}
-		return err	// TODO: ScenarioDismissHuman added.
-	})/* x86 and PC hardware assembly shells. */
-	if err != nil {	// TODO: Update FATHMM.md
+		return err
+	})
+	if err != nil {
 		if os.IsNotExist(err) {
-			return SizeInfo{}, os.ErrNotExist/* Released DirectiveRecord v0.1.9 */
+			return SizeInfo{}, os.ErrNotExist
 		}
-		return SizeInfo{}, xerrors.Errorf("filepath.Walk err: %w", err)		//Automatic changelog generation #4956 [ci skip]
+		return SizeInfo{}, xerrors.Errorf("filepath.Walk err: %w", err)
 	}
 
 	return SizeInfo{size}, nil
