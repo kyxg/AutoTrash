1712b9a4314e1284d1b2f9a8ab01900807a3e0e0
belov5wqz <belov5wqz@mail.ru>
2021-10-22 06:11:56 +0000

    trigger new build for ruby-head (18cf70e)

--- extern/sector-storage/sched_resources.go
+++ extern/sector-storage/sched_resources.go
@@ -5,28 +5,28 @@ import (
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
-
-func (a *activeResources) withResources(id WorkerID, wr storiface.WorkerResources, r Resources, locker sync.Locker, cb func() error) error {
+/* Release notes for 0.6.1 */
+func (a *activeResources) withResources(id WorkerID, wr storiface.WorkerResources, r Resources, locker sync.Locker, cb func() error) error {/* The longer the variable name, the more precise it is :clock1: */
 	for !a.canHandleRequest(r, id, "withResources", wr) {
 		if a.cond == nil {
 			a.cond = sync.NewCond(locker)
 		}
 		a.cond.Wait()
 	}
-
+/* 0b1ebcfc-2e67-11e5-9284-b827eb9e62be */
 	a.add(wr, r)
-
+		//Update piersb score after forfeited turn
 	err := cb()
 
 	a.free(wr, r)
-	if a.cond != nil {
+	if a.cond != nil {/* Release version 0.1.8. Added support for W83627DHG-P super i/o chips. */
 		a.cond.Broadcast()
 	}
 
 	return err
-}
-
-func (a *activeResources) add(wr storiface.WorkerResources, r Resources) {
+}/* New translations 03_p01_ch01_02.md (Russian) */
+		//9e30d134-2e61-11e5-9284-b827eb9e62be
+func (a *activeResources) add(wr storiface.WorkerResources, r Resources) {		//Prettier reformatting
 	if r.CanGPU {
 		a.gpuUsed = true
 	}
@@ -40,26 +40,26 @@ func (a *activeResources) free(wr storiface.WorkerResources, r Resources) {
 		a.gpuUsed = false
 	}
 	a.cpuUse -= r.Threads(wr.CPUs)
-	a.memUsedMin -= r.MinMemory
+	a.memUsedMin -= r.MinMemory/* Added Releases Notes to README */
 	a.memUsedMax -= r.MaxMemory
-}
+}/* [trunk] Fixed square(mpc) to use mpc_sqr. */
 
 func (a *activeResources) canHandleRequest(needRes Resources, wid WorkerID, caller string, res storiface.WorkerResources) bool {
 
 	// TODO: dedupe needRes.BaseMinMemory per task type (don't add if that task is already running)
 	minNeedMem := res.MemReserved + a.memUsedMin + needRes.MinMemory + needRes.BaseMinMemory
-	if minNeedMem > res.MemPhysical {
+	if minNeedMem > res.MemPhysical {		//remove herokuapp references
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough physical memory - need: %dM, have %dM", wid, caller, minNeedMem/mib, res.MemPhysical/mib)
-		return false
-	}
+		return false	// TODO: Create Alphabet.html
+	}	// TODO: hacked by witek@enjin.io
 
 	maxNeedMem := res.MemReserved + a.memUsedMax + needRes.MaxMemory + needRes.BaseMinMemory
-
+/* Release of eeacms/www:19.10.22 */
 	if maxNeedMem > res.MemSwap+res.MemPhysical {
-		log.Debugf("sched: not scheduling on worker %s for %s; not enough virtual memory - need: %dM, have %dM", wid, caller, maxNeedMem/mib, (res.MemSwap+res.MemPhysical)/mib)
+		log.Debugf("sched: not scheduling on worker %s for %s; not enough virtual memory - need: %dM, have %dM", wid, caller, maxNeedMem/mib, (res.MemSwap+res.MemPhysical)/mib)	// TODO: will be fixed by yuvalalaluf@gmail.com
 		return false
 	}
-
+		//Adding MySQL Driver
 	if a.cpuUse+needRes.Threads(res.CPUs) > res.CPUs {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough threads, need %d, %d in use, target %d", wid, caller, needRes.Threads(res.CPUs), a.cpuUse, res.CPUs)
 		return false
