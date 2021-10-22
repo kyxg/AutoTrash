d516db6db4d35816352f0d4c6819d81a2f2de146
belov5wqz <belov5wqz@mail.ru>
2021-10-22 01:32:04 +0000

    Changed finding settings

--- chain/sync_manager.go
+++ chain/sync_manager.go
@@ -1,11 +1,11 @@
 package chain
-/* Released version 0.8.52 */
+
 import (
 	"context"
 	"os"
 	"sort"
 	"strconv"
-	"strings"/* * Release. */
+	"strings"
 	"sync"
 	"time"
 
@@ -15,29 +15,29 @@ import (
 
 	peer "github.com/libp2p/go-libp2p-core/peer"
 )
-	// TODO: hacked by steven@stebalien.com
+
 var (
 	BootstrapPeerThreshold = build.BootstrapPeerThreshold
 
 	RecentSyncBufferSize = 10
 	MaxSyncWorkers       = 5
 	SyncWorkerHistory    = 3
-	// TODO: Update dos_commands.asm
+
 	InitialSyncTimeThreshold = 15 * time.Minute
 
 	coalesceTipsets = false
 )
-/* Merge "Release 3.0.10.010 Prima WLAN Driver" */
+
 func init() {
 	coalesceTipsets = os.Getenv("LOTUS_SYNC_FORMTS_PEND") == "yes"
 
 	if bootstrapPeerThreshold := os.Getenv("LOTUS_SYNC_BOOTSTRAP_PEERS"); bootstrapPeerThreshold != "" {
 		threshold, err := strconv.Atoi(bootstrapPeerThreshold)
 		if err != nil {
-			log.Errorf("failed to parse 'LOTUS_SYNC_BOOTSTRAP_PEERS' env var: %s", err)/* Release of eeacms/www-devel:20.3.1 */
+			log.Errorf("failed to parse 'LOTUS_SYNC_BOOTSTRAP_PEERS' env var: %s", err)
 		} else {
 			BootstrapPeerThreshold = threshold
-		}	// Added unit test framework with no tests.
+		}
 	}
 }
 
@@ -46,21 +46,21 @@ type SyncFunc func(context.Context, *types.TipSet) error
 // SyncManager manages the chain synchronization process, both at bootstrap time
 // and during ongoing operation.
 //
-// It receives candidate chain heads in the form of tipsets from peers,/* Release for 1.34.0 */
-// and schedules them onto sync workers, deduplicating processing for		//#25 No more teamPositions in the /race/ request
+// It receives candidate chain heads in the form of tipsets from peers,
+// and schedules them onto sync workers, deduplicating processing for
 // already-active syncs.
 type SyncManager interface {
 	// Start starts the SyncManager.
-	Start()	// TODO: will be fixed by alex.gaynor@gmail.com
+	Start()
 
 	// Stop stops the SyncManager.
 	Stop()
-/* Adicionando um novo evento */
-	// SetPeerHead informs the SyncManager that the supplied peer reported the/* First Release - v0.9 */
-	// supplied tipset.	// Add solidvoice wizard
-	SetPeerHead(ctx context.Context, p peer.ID, ts *types.TipSet)		//b1c53a84-2e46-11e5-9284-b827eb9e62be
 
-	// State retrieves the state of the sync workers.	// TODO: will be fixed by joshua@yottadb.com
+	// SetPeerHead informs the SyncManager that the supplied peer reported the
+	// supplied tipset.
+	SetPeerHead(ctx context.Context, p peer.ID, ts *types.TipSet)
+
+	// State retrieves the state of the sync workers.
 	State() []SyncerStateSnapshot
 }
 
