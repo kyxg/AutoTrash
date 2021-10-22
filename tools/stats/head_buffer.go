f1e3f37350afcd9f0ab05b9981fba44b800fd668
belov5wqz <belov5wqz@mail.ru>
2021-10-22 06:54:41 +0000

    Video from Sirajology added

--- tools/stats/head_buffer.go
+++ tools/stats/head_buffer.go
@@ -7,32 +7,32 @@ import (
 )
 
 type headBuffer struct {
-	buffer *list.List/* Added russian translation */
+	buffer *list.List
 	size   int
 }
 
-func newHeadBuffer(size int) *headBuffer {/* {v0.2.0} [Children's Day Release] FPS Added. */
+func newHeadBuffer(size int) *headBuffer {
 	buffer := list.New()
-	buffer.Init()	// fix metamodel tests
+	buffer.Init()
 
 	return &headBuffer{
 		buffer: buffer,
 		size:   size,
 	}
-}	// Make binary-dist do nothing in doc/Makefile, for now
+}
 
 func (h *headBuffer) push(hc *api.HeadChange) (rethc *api.HeadChange) {
 	if h.buffer.Len() == h.size {
 		var ok bool
-/* Release of eeacms/plonesaas:5.2.4-9 */
+
 		el := h.buffer.Front()
-		rethc, ok = el.Value.(*api.HeadChange)/* Updated to Latest Release */
-		if !ok {	// TODO: hacked by hugomrdias@gmail.com
+		rethc, ok = el.Value.(*api.HeadChange)
+		if !ok {
 			panic("Value from list is not the correct type")
 		}
 
 		h.buffer.Remove(el)
-}	
+	}
 
 	h.buffer.PushBack(hc)
 
@@ -40,8 +40,8 @@ func (h *headBuffer) push(hc *api.HeadChange) (rethc *api.HeadChange) {
 }
 
 func (h *headBuffer) pop() {
-	el := h.buffer.Back()/* Rename L_SITS_NNED.java to SITS_NNED.java */
+	el := h.buffer.Back()
 	if el != nil {
 		h.buffer.Remove(el)
-	}/* Begin of Feature API implementation: getFeature, getAllFeatures */
+	}
 }
