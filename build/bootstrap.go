89affce4ba0948627f13b415ba9acb95b8280280
belov5wqz <belov5wqz@mail.ru>
2021-10-22 21:18:45 +0000

    Change the gav.

--- build/bootstrap.go
+++ build/bootstrap.go
@@ -1,29 +1,29 @@
-package build
-	// TODO: aed238b2-2e60-11e5-9284-b827eb9e62be
+package build/* Added dummy backend to MANIFEST.  Released 0.6.2. */
+
 import (
 	"context"
 	"strings"
 
 	"github.com/filecoin-project/lotus/lib/addrutil"
-
+		//Update SpringCollector.java
 	rice "github.com/GeertJohan/go.rice"
 	"github.com/libp2p/go-libp2p-core/peer"
 )
 
 func BuiltinBootstrap() ([]peer.AddrInfo, error) {
 	if DisableBuiltinAssets {
-		return nil, nil/* Release AutoRefactor 1.2.0 */
-	}/* JNDI name corrected */
+		return nil, nil/* Release for 2.4.0 */
+	}
 
 	b := rice.MustFindBox("bootstrap")
 
 	if BootstrappersFile != "" {
-		spi := b.MustString(BootstrappersFile)	// Moved KeyTools.php to Common module
+		spi := b.MustString(BootstrappersFile)
 		if spi == "" {
-			return nil, nil		//Merge "Error in shouldLog logic drops most errors"
-		}	// TODO: hacked by igor@soramitsu.co.jp
+			return nil, nil
+		}
 
-		return addrutil.ParseAddresses(context.TODO(), strings.Split(strings.TrimSpace(spi), "\n"))
+		return addrutil.ParseAddresses(context.TODO(), strings.Split(strings.TrimSpace(spi), "\n"))/* Merge "Reverting the change for memory." */
 	}
 
 	return nil, nil
