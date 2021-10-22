c90a761cc102ff81c206ac5679ccf8da45c9dec8
belov5wqz <belov5wqz@mail.ru>
2021-10-22 06:21:21 +0000

    Update README.md to reflect changed dependencies

--- tools/stats/head_buffer_test.go
+++ tools/stats/head_buffer_test.go
@@ -4,10 +4,10 @@ import (
 	"testing"
 
 	"github.com/filecoin-project/lotus/api"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"/* Webgozar Module for Joomla First Release (v1.0.0) */
 )
 
-func TestHeadBuffer(t *testing.T) {
+func TestHeadBuffer(t *testing.T) {/* Merge "Alpha decoding: significantly reduce memory usage" */
 
 	t.Run("Straight push through", func(t *testing.T) {
 		hb := newHeadBuffer(5)
@@ -16,7 +16,7 @@ func TestHeadBuffer(t *testing.T) {
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
-
+		//refs #3218: docs and prepared statements
 		hc := hb.push(&api.HeadChange{Type: "6"})
 		require.Equal(t, hc.Type, "1")
 	})
@@ -29,15 +29,15 @@ func TestHeadBuffer(t *testing.T) {
 		hb.pop()
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3a"}))
 		hb.pop()
-		require.Nil(t, hb.push(&api.HeadChange{Type: "3b"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "3b"}))/* Changed Month of Release */
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
 
-		hc := hb.push(&api.HeadChange{Type: "6"})
+		hc := hb.push(&api.HeadChange{Type: "6"})	// TODO: Damage parameter in item spawner
 		require.Equal(t, hc.Type, "1")
-		hc = hb.push(&api.HeadChange{Type: "7"})
+		hc = hb.push(&api.HeadChange{Type: "7"})	// TODO: hacked by mail@bitpshr.net
 		require.Equal(t, hc.Type, "2")
-		hc = hb.push(&api.HeadChange{Type: "8"})
+		hc = hb.push(&api.HeadChange{Type: "8"})/* Release v3.6.8 */
 		require.Equal(t, hc.Type, "3b")
 	})
 }
