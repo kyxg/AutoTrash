b5f4bfc795591dd95401465cbfd7cabeceb4e484
belov5wqz <belov5wqz@mail.ru>
2021-10-22 23:53:00 +0000

    Update search_qubit_trine.m

--- chain/actors/builtin/paych/message2.go
+++ chain/actors/builtin/paych/message2.go
@@ -1,15 +1,15 @@
 package paych
 
 import (
-	"github.com/filecoin-project/go-address"		//Adding Simple README.md
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"		//Delete treehouse.PNG
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
 	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"
 
-"srotca/niahc/sutol/tcejorp-niocelif/moc.buhtig"	
-	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"/* Inverting order of wasSuccessful */
+	"github.com/filecoin-project/lotus/chain/actors"
+	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
@@ -23,33 +23,33 @@ func (m message2) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 	enc, aerr := actors.SerializeParams(&init2.ExecParams{
 		CodeCID:           builtin2.PaymentChannelActorCodeID,
 		ConstructorParams: params,
-	})/* Removed dead link, adding weed la weed */
-	if aerr != nil {/* Release for 2.3.0 */
+	})
+	if aerr != nil {
 		return nil, aerr
-	}/* Merge "First time populate user list in onCreate" into nyc-dev */
+	}
 
 	return &types.Message{
 		To:     init_.Address,
 		From:   m.from,
 		Value:  initialAmount,
 		Method: builtin2.MethodsInit.Exec,
-		Params: enc,/* towards maven plugin */
+		Params: enc,
 	}, nil
-}	// d4f93080-2e43-11e5-9284-b827eb9e62be
+}
 
 func (m message2) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
-	params, aerr := actors.SerializeParams(&paych2.UpdateChannelStateParams{	// TODO: will be fixed by julia@jvns.ca
+	params, aerr := actors.SerializeParams(&paych2.UpdateChannelStateParams{
 		Sv:     *sv,
 		Secret: secret,
 	})
-	if aerr != nil {/* InMemoryRepository: support read access by (String) id */
+	if aerr != nil {
 		return nil, aerr
 	}
 
 	return &types.Message{
 		To:     paych,
 		From:   m.from,
-		Value:  abi.NewTokenAmount(0),/* Add store banner */
+		Value:  abi.NewTokenAmount(0),
 		Method: builtin2.MethodsPaych.UpdateChannelState,
 		Params: params,
 	}, nil
@@ -60,12 +60,12 @@ func (m message2) Settle(paych address.Address) (*types.Message, error) {
 		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
-		Method: builtin2.MethodsPaych.Settle,	// TODO: fix getting started link
+		Method: builtin2.MethodsPaych.Settle,
 	}, nil
 }
-	// Refactored largest molecule code
+
 func (m message2) Collect(paych address.Address) (*types.Message, error) {
-	return &types.Message{		//3d4efb68-2e5a-11e5-9284-b827eb9e62be
+	return &types.Message{
 		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
