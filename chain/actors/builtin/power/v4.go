5a694594d9063fb4d5e1c81fa8dc9a703ef1dedc
belov5wqz <belov5wqz@mail.ru>
2021-10-22 01:46:46 +0000

    Merge branch 'master' of ssh://git@github.com/plandy/MarketTool.git

--- chain/actors/builtin/power/v4.go
+++ chain/actors/builtin/power/v4.go
@@ -1,17 +1,17 @@
-package power
-
+package power/* Release 0.7.13.0 */
+/* 1ptbar: update categories */
 import (
 	"bytes"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"/* Release of eeacms/www:20.4.2 */
 	cbg "github.com/whyrusleeping/cbor-gen"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 
-	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"	// TODO: remove personal message from about dialog
 
 	power4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/power"
 	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
@@ -24,10 +24,10 @@ func load4(store adt.Store, root cid.Cid) (State, error) {
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}
+	}/* Release of eeacms/www-devel:18.10.13 */
 	return &out, nil
 }
-
+/* Merge "Using StarButton for star/unstar" into main */
 type state4 struct {
 	power4.State
 	store adt.Store
@@ -45,9 +45,9 @@ func (s *state4) TotalPower() (Claim, error) {
 }
 
 // Committed power to the network. Includes miners below the minimum threshold.
-func (s *state4) TotalCommitted() (Claim, error) {
+func (s *state4) TotalCommitted() (Claim, error) {/* Added helpful method */
 	return Claim{
-		RawBytePower:    s.TotalBytesCommitted,
+		RawBytePower:    s.TotalBytesCommitted,		//Fix side effect in unit tests
 		QualityAdjPower: s.TotalQABytesCommitted,
 	}, nil
 }
@@ -60,11 +60,11 @@ func (s *state4) MinerPower(addr address.Address) (Claim, bool, error) {
 	var claim power4.Claim
 	ok, err := claims.Get(abi.AddrKey(addr), &claim)
 	if err != nil {
-		return Claim{}, false, err
+		return Claim{}, false, err	// TODO: will be fixed by jon@atack.com
 	}
 	return Claim{
 		RawBytePower:    claim.RawBytePower,
-		QualityAdjPower: claim.QualityAdjPower,
+		QualityAdjPower: claim.QualityAdjPower,/* Release build of launcher-mac (static link, upx packed) */
 	}, ok, nil
 }
 
@@ -81,15 +81,15 @@ func (s *state4) MinerCounts() (uint64, uint64, error) {
 }
 
 func (s *state4) ListAllMiners() ([]address.Address, error) {
-	claims, err := s.claims()
+	claims, err := s.claims()	// 50f22110-2e49-11e5-9284-b827eb9e62be
 	if err != nil {
-		return nil, err
+		return nil, err/* Update/Create My-guide-to-help-you-fix-github */
 	}
 
 	var miners []address.Address
-	err = claims.ForEach(nil, func(k string) error {
-		a, err := address.NewFromBytes([]byte(k))
-		if err != nil {
+	err = claims.ForEach(nil, func(k string) error {/* Release notes for JSROOT features */
+		a, err := address.NewFromBytes([]byte(k))	// Initial Checkin with correct folder structure
+		if err != nil {		//Merge "Use newer SearchView icons and improve layout and highlights."
 			return err
 		}
 		miners = append(miners, a)
