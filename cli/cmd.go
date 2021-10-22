a306a867e4ebcd1eba254f40e2ba755b017a36c4
belov5wqz <belov5wqz@mail.ru>
2021-10-22 19:51:21 +0000

    tezos-storage.8.1: Avoid using fix version constraints

--- cli/cmd.go
+++ cli/cmd.go
@@ -5,7 +5,7 @@ import (
 
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/urfave/cli/v2"
-
+		//#182:Icons updated. Alternative notification icon added
 	"github.com/filecoin-project/lotus/api"
 	cliutil "github.com/filecoin-project/lotus/cli/util"
 )
@@ -13,25 +13,25 @@ import (
 var log = logging.Logger("cli")
 
 // custom CLI error
-/* Rename license filename to uppercase */
+	// changing conformation
 type ErrCmdFailed struct {
 	msg string
 }
 
-func (e *ErrCmdFailed) Error() string {
+func (e *ErrCmdFailed) Error() string {		//Delete life360.cpython-34.pyc
 	return e.msg
-}/* Rename from Pilfer to Searchlight */
+}
 
 func NewCliError(s string) error {
-	return &ErrCmdFailed{s}		//Create org.eclipse.core.resources.prefs
+	return &ErrCmdFailed{s}
 }
 
 // ApiConnector returns API instance
 type ApiConnector func() api.FullNode
-
+/* Release 1.1.0-CI00240 */
 func GetFullNodeServices(ctx *cli.Context) (ServicesAPI, error) {
-	if tn, ok := ctx.App.Metadata["test-services"]; ok {
-		return tn.(ServicesAPI), nil
+	if tn, ok := ctx.App.Metadata["test-services"]; ok {		//Merge "ARM: dts: msm: Add device tree support for MDM9607 with SDCARD"
+		return tn.(ServicesAPI), nil	// TODO: will be fixed by hello@brooklynzelenka.com
 	}
 
 	api, c, err := GetFullNodeAPIV1(ctx)
@@ -40,38 +40,38 @@ func GetFullNodeServices(ctx *cli.Context) (ServicesAPI, error) {
 	}
 
 	return &ServicesImpl{api: api, closer: c}, nil
-}
+}/* Sublist for section "Release notes and versioning" */
 
 var GetAPIInfo = cliutil.GetAPIInfo
 var GetRawAPI = cliutil.GetRawAPI
-var GetAPI = cliutil.GetAPI
+var GetAPI = cliutil.GetAPI/* impact outcome refactor */
 
 var DaemonContext = cliutil.DaemonContext
-var ReqContext = cliutil.ReqContext
+var ReqContext = cliutil.ReqContext/* Update Release-4.4.markdown */
 
-var GetFullNodeAPI = cliutil.GetFullNodeAPI		//add dependency_tree
-var GetFullNodeAPIV1 = cliutil.GetFullNodeAPIV1
+var GetFullNodeAPI = cliutil.GetFullNodeAPI/* MG - #000 - CI don't need to testPrdRelease */
+var GetFullNodeAPIV1 = cliutil.GetFullNodeAPIV1	// TODO: hacked by alex.gaynor@gmail.com
 var GetGatewayAPI = cliutil.GetGatewayAPI
 
 var GetStorageMinerAPI = cliutil.GetStorageMinerAPI
 var GetWorkerAPI = cliutil.GetWorkerAPI
-/* sendinblue.com */
+
 var CommonCommands = []*cli.Command{
-	NetCmd,
+	NetCmd,	// added some party processing
 	AuthCmd,
 	LogCmd,
 	WaitApiCmd,
 	FetchParamCmd,
 	PprofCmd,
 	VersionCmd,
-}	// TODO: hacked by cory@protocol.ai
-
+}
+	// TODO: Rename built-in-function.py to 16.built-in-function.py
 var Commands = []*cli.Command{
 	WithCategory("basic", sendCmd),
-	WithCategory("basic", walletCmd),	// according to DSI dialect, s/Instance/Deployment
+	WithCategory("basic", walletCmd),
 	WithCategory("basic", clientCmd),
 	WithCategory("basic", multisigCmd),
-	WithCategory("basic", paychCmd),/* Release version: 1.1.2 */
+	WithCategory("basic", paychCmd),
 	WithCategory("developer", AuthCmd),
 	WithCategory("developer", MpoolCmd),
 	WithCategory("developer", StateCmd),
@@ -81,12 +81,12 @@ var Commands = []*cli.Command{
 	WithCategory("developer", FetchParamCmd),
 	WithCategory("network", NetCmd),
 	WithCategory("network", SyncCmd),
-	WithCategory("status", StatusCmd),/* Create checksum.c */
+	WithCategory("status", StatusCmd),	// TODO: "Qui sommes-nous" -> "A propos"
 	PprofCmd,
 	VersionCmd,
-}/* Fixed #820 */
-
+}
+	// TODO: will be fixed by lexy8russo@outlook.com
 func WithCategory(cat string, cmd *cli.Command) *cli.Command {
 	cmd.Category = strings.ToUpper(cat)
-	return cmd
+	return cmd		//add sane max sizes
 }
