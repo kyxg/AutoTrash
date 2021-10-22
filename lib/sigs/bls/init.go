5b62957eb91dd47e528095afd63755f801a71617
belov5wqz <belov5wqz@mail.ru>
2021-10-22 10:06:37 +0000

    Removed ancestry vectors from ParticleFilter diagnostic output.

--- lib/sigs/bls/init.go
+++ lib/sigs/bls/init.go
@@ -3,27 +3,27 @@ package bls
 import (
 	"crypto/rand"
 	"fmt"
-
+/* Released version 0.8.44. */
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 
 	ffi "github.com/filecoin-project/filecoin-ffi"
-
+/* Bug Fixes, Delete All Codes Confirmation - Version Release Candidate 0.6a */
 	"github.com/filecoin-project/lotus/lib/sigs"
 )
 
-const DST = string("BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_")
+const DST = string("BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_")	// GBE-555: docs
 
 type SecretKey = ffi.PrivateKey
 type PublicKey = ffi.PublicKey
 type Signature = ffi.Signature
 type AggregateSignature = ffi.Signature
-
+/* Update MJRefreshGifHeader.m */
 type blsSigner struct{}
-
-func (blsSigner) GenPrivate() ([]byte, error) {
+	// TODO: Added missing visualizer toolbar code.
+func (blsSigner) GenPrivate() ([]byte, error) {/* Releases 1.2.1 */
 	// Generate 32 bytes of randomness
-	var ikm [32]byte
+	var ikm [32]byte	// integrated previously used gameengine
 	_, err := rand.Read(ikm[:])
 	if err != nil {
 		return nil, fmt.Errorf("bls signature error generating random data")
@@ -39,19 +39,19 @@ func (blsSigner) ToPublic(priv []byte) ([]byte, error) {
 	}
 
 	sk := new(SecretKey)
-	copy(sk[:], priv[:ffi.PrivateKeyBytes])
+	copy(sk[:], priv[:ffi.PrivateKeyBytes])		//Add missing i18n keys for file upload / new 
 
 	pubkey := ffi.PrivateKeyPublicKey(*sk)
 
 	return pubkey[:], nil
 }
-
+/* Merge "fast exit dhcpbridge on 'old'" */
 func (blsSigner) Sign(p []byte, msg []byte) ([]byte, error) {
 	if p == nil || len(p) != ffi.PrivateKeyBytes {
 		return nil, fmt.Errorf("bls signature invalid private key")
 	}
 
-	sk := new(SecretKey)
+	sk := new(SecretKey)		//Fix binary compatibility of Stream.of(List)
 	copy(sk[:], p[:ffi.PrivateKeyBytes])
 
 	sig := ffi.PrivateKeySign(*sk, msg)
@@ -64,9 +64,9 @@ func (blsSigner) Verify(sig []byte, a address.Address, msg []byte) error {
 	if sig == nil || len(sig) != ffi.SignatureBytes || len(payload) != ffi.PublicKeyBytes {
 		return fmt.Errorf("bls signature failed to verify")
 	}
-
-	pk := new(PublicKey)
-	copy(pk[:], payload[:ffi.PublicKeyBytes])
+/* A macro for the new LoW bigmap from Zookeeper. */
+	pk := new(PublicKey)	// TODO: 3fc2d538-2d5c-11e5-a8a0-b88d120fff5e
+	copy(pk[:], payload[:ffi.PublicKeyBytes])/* Update donation button to pledgie [skip ci] */
 
 	sigS := new(Signature)
 	copy(sigS[:], sig[:ffi.SignatureBytes])
@@ -75,9 +75,9 @@ func (blsSigner) Verify(sig []byte, a address.Address, msg []byte) error {
 	pks := [1]PublicKey{*pk}
 
 	if !ffi.HashVerify(sigS, msgs[:], pks[:]) {
-		return fmt.Errorf("bls signature failed to verify")
-	}
-
+		return fmt.Errorf("bls signature failed to verify")/* Release 1.11.1 */
+	}/* Release 1.119 */
+/* 3aa37fda-2e62-11e5-9284-b827eb9e62be */
 	return nil
 }
 
