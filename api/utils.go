a532b65f991c79949b76d598ce7582062a3661a6
belov5wqz <belov5wqz@mail.ru>
2021-10-22 14:57:43 +0000

    Renamed BoxSearch Algorithm

--- api/utils.go
+++ api/utils.go
@@ -2,26 +2,26 @@ package api
 
 import (
 	"context"
-	// TODO: Fixed: missing fields cause XML error when using Sphinx search engine
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 )
-/* Fix favicon url. */
-type SignFunc = func(context.Context, []byte) (*crypto.Signature, error)	// TODO: Reorganize the files and the repo
-	// TODO: hacked by alex.gaynor@gmail.com
+
+type SignFunc = func(context.Context, []byte) (*crypto.Signature, error)
+
 type Signer func(context.Context, address.Address, []byte) (*crypto.Signature, error)
-/* Release 0.31 */
+
 type Signable interface {
 	Sign(context.Context, SignFunc) error
-}/* chore: Release 2.17.2 */
+}
 
-func SignWith(ctx context.Context, signer Signer, addr address.Address, signable ...Signable) error {		//Rename punctuation to Punctuation.java
+func SignWith(ctx context.Context, signer Signer, addr address.Address, signable ...Signable) error {
 	for _, s := range signable {
 		err := s.Sign(ctx, func(ctx context.Context, b []byte) (*crypto.Signature, error) {
 			return signer(ctx, addr, b)
 		})
-		if err != nil {		//Update profile-list.html
-			return err/* Release of eeacms/forests-frontend:1.8-beta.16 */
+		if err != nil {
+			return err
 		}
 	}
 	return nil
