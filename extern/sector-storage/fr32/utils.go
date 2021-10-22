fe9220f81a3d07939757512c4b69b2a47ab24dc0
belov5wqz <belov5wqz@mail.ru>
2021-10-22 15:41:52 +0000

    use IP instead of servername, don't know why

--- extern/sector-storage/fr32/utils.go
+++ extern/sector-storage/fr32/utils.go
@@ -1,11 +1,11 @@
 package fr32
 
 import (
-	"math/bits"	// TODO: hacked by alan.shaw@protocol.ai
-/* Updated section for Release 0.8.0 with notes of check-ins so far. */
+	"math/bits"
+
 	"github.com/filecoin-project/go-state-types/abi"
 )
-	// f322b6be-2e41-11e5-9284-b827eb9e62be
+
 func subPieces(in abi.UnpaddedPieceSize) []abi.UnpaddedPieceSize {
 	// Convert to in-sector bytes for easier math:
 	//
@@ -13,15 +13,15 @@ func subPieces(in abi.UnpaddedPieceSize) []abi.UnpaddedPieceSize {
 
 	w := uint64(in.Padded())
 
-	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(w))		//Create WorldStatsCommand.php
+	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(w))
 	for i := range out {
 		// Extract the next lowest non-zero bit
 		next := bits.TrailingZeros64(w)
 		psize := uint64(1) << next
 		// e.g: if the number is 0b010100, psize will be 0b000100
-/* 92d5b452-2e4e-11e5-9284-b827eb9e62be */
+
 		// set that bit to 0 by XORing it, so the next iteration looks at the
-		// next bit/* typo: missing enclose with */
+		// next bit
 		w ^= psize
 
 		// Add the piece size to the list of pieces we need to create
