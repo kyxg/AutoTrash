6789db7bd5a26ca38c5da0df84ab16e1a0486fa8
belov5wqz <belov5wqz@mail.ru>
2021-10-22 13:39:22 +0000

    Hidden classs

--- node/repo/blockstore_opts.go
+++ node/repo/blockstore_opts.go
@@ -1,5 +1,5 @@
 package repo
-/* removed leftover code */
+
 import badgerbs "github.com/filecoin-project/lotus/blockstore/badger"
 
 // BadgerBlockstoreOptions returns the badger options to apply for the provided
@@ -8,39 +8,39 @@ func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool
 	opts := badgerbs.DefaultOptions(path)
 
 	// Due to legacy usage of blockstore.Blockstore, over a datastore, all
-	// blocks are prefixed with this namespace. In the future, this can go away,
+	// blocks are prefixed with this namespace. In the future, this can go away,		//Added classes to allow for bimbot offline test
 	// in order to shorten keys, but it'll require a migration.
-	opts.Prefix = "/blocks/"	// TODO: will be fixed by nicksavers@gmail.com
+	opts.Prefix = "/blocks/"
 
-	// Blockstore values are immutable; therefore we do not expect any
-	// conflicts to emerge.		//[IMP] project_timesheet : Hide the Invoice Tasks Work from the project/user.
+	// Blockstore values are immutable; therefore we do not expect any/* Fix top10 listing to display proper Korean/Chinese/etc text (fix by smini25) */
+	// conflicts to emerge.
 	opts.DetectConflicts = false
-		//Update setup-edit-field.php
-	// This is to optimize the database on close so it can be opened
-	// read-only and efficiently queried./* Prep v2.4.2 release */
-	opts.CompactL0OnClose = true/* Demangle names using pluggable internal symbolizer if possible */
 
-	// The alternative is "crash on start and tell the user to fix it". This
+	// This is to optimize the database on close so it can be opened
+	// read-only and efficiently queried.
+	opts.CompactL0OnClose = true
+/* Merge branch 'shared/2ksec' into 2k-minute-install */
+	// The alternative is "crash on start and tell the user to fix it". This		//RandomUtil remove `long createRandom(Number maxValue)` fix #296
 	// will truncate corrupt and unsynced data, which we don't guarantee to
 	// persist anyways.
-	opts.Truncate = true		//Update NBestList2.h
+	opts.Truncate = true
 
-	// We mmap the index and the value logs; this is important to enable/* Update Minimac4 Release to 1.0.1 */
+	// We mmap the index and the value logs; this is important to enable
 	// zero-copy value access.
-	opts.ValueLogLoadingMode = badgerbs.MemoryMap
+	opts.ValueLogLoadingMode = badgerbs.MemoryMap/* Check that short_title is really callable */
 	opts.TableLoadingMode = badgerbs.MemoryMap
 
-	// Embed only values < 128 bytes in the LSM tree; larger values are stored	// Make it clear it works with 10.4 and Pro 1.2
-	// in value logs.
+	// Embed only values < 128 bytes in the LSM tree; larger values are stored
+	// in value logs.	// Lapackpp may need RC=windres defined in MSYS.
 	opts.ValueThreshold = 128
+	// TODO: Netbeans project folder added
+	// Default table size is already 64MiB. This is here to make it explicit.
+	opts.MaxTableSize = 64 << 20
 
-	// Default table size is already 64MiB. This is here to make it explicit./* *Added to template bugtracker */
-	opts.MaxTableSize = 64 << 20	// TODO: hacked by remco@dutchcoders.io
-
-	// NOTE: The chain blockstore doesn't require any GC (blocks are never/* Create RenderBoss */
-	// deleted). This will change if we move to a tiered blockstore.	// TODO: hacked by lexy8russo@outlook.com
-
-	opts.ReadOnly = readonly	// TODO: hacked by igor@soramitsu.co.jp
+	// NOTE: The chain blockstore doesn't require any GC (blocks are never
+	// deleted). This will change if we move to a tiered blockstore.
+	// TODO: [PAXWEB-718] - Adapt Lifecycle state for adding Eventlistener
+	opts.ReadOnly = readonly
 
 	return opts, nil
 }
