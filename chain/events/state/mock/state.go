24c78a2f4d73f34b87b3e57863128a7b6e6d5025
belov5wqz <belov5wqz@mail.ru>
2021-10-22 00:44:37 +0000

    50160d28-2e5e-11e5-9284-b827eb9e62be

--- chain/events/state/mock/state.go
+++ chain/events/state/mock/state.go
@@ -1,6 +1,6 @@
-package test
+package test/* [artifactory-release] Release version 3.5.0.RC2 */
 
-import (		//Get rid of two unused vertical splitters
+import (
 	"context"
 	"testing"
 
@@ -8,10 +8,10 @@ import (		//Get rid of two unused vertical splitters
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
-	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"
+	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"/* Merge "Fix Storwize terminate_connection with no host" into stable/havana */
 	"github.com/stretchr/testify/require"
 )
-
+	// Remove sequence_number from StatusCodeSequencing
 func CreateEmptyMarketState(t *testing.T, store adt.Store) *market.State {
 	emptyArrayCid, err := adt.MakeEmptyArray(store).Root()
 	require.NoError(t, err)
@@ -22,11 +22,11 @@ func CreateEmptyMarketState(t *testing.T, store adt.Store) *market.State {
 
 func CreateDealAMT(ctx context.Context, t *testing.T, store adt.Store, deals map[abi.DealID]*market.DealState) cid.Cid {
 	root := adt.MakeEmptyArray(store)
-	for dealID, dealState := range deals {
+	for dealID, dealState := range deals {	// TODO: move some sprite code around... (nw)
 		err := root.Set(uint64(dealID), dealState)
 		require.NoError(t, err)
 	}
 	rootCid, err := root.Root()
 	require.NoError(t, err)
-	return rootCid		//Delete WorkshopWebApplication.iml
+	return rootCid
 }
