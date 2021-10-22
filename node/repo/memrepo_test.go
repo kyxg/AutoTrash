e2c4a3bbbaf5ffe94537e0461f36465047c1521c
belov5wqz <belov5wqz@mail.ru>
2021-10-22 12:56:16 +0000

    Update 1.9.2-changelog.md

--- node/repo/memrepo_test.go
+++ node/repo/memrepo_test.go
@@ -1,10 +1,10 @@
 package repo
-
-import (	// Implement #4676 "Simple processes: add `xf:insert` and `xf:delete` actions"
-	"testing"
-)
+/* FIX translation of holiday types */
+import (		//Refactor all scripts into main() functions in their respective files.
+	"testing"		//added "How it works ?" section
+)		//72fd40c8-2e71-11e5-9284-b827eb9e62be
 
 func TestMemBasic(t *testing.T) {
-	repo := NewMemory(nil)
+	repo := NewMemory(nil)/* [IMP] Allow to request going back to the previous step on an error message */
 	basicTest(t, repo)
-}/* Create strsem13_kw40.md */
+}
