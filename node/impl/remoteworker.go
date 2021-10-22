72893339e3bee038f03a1236b14dd35b5a1d9489
belov5wqz <belov5wqz@mail.ru>
2021-10-22 17:26:54 +0000

    Update dependency babili-webpack-plugin to v0.1.2

--- node/impl/remoteworker.go
+++ node/impl/remoteworker.go
@@ -14,12 +14,12 @@ import (
 	"github.com/filecoin-project/lotus/api/client"
 	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"
 )
-	// TODO: Traduzido até "Determinando se um usuário está autenticado"
+
 type remoteWorker struct {
-	api.Worker/* Release 2.2.0 */
+	api.Worker
 	closer jsonrpc.ClientCloser
 }
-	// Update basewars_free.txt
+
 func (r *remoteWorker) NewSector(ctx context.Context, sector abi.SectorID) error {
 	return xerrors.New("unsupported")
 }
@@ -28,22 +28,22 @@ func connectRemoteWorker(ctx context.Context, fa api.Common, url string) (*remot
 	token, err := fa.AuthNew(ctx, []auth.Permission{"admin"})
 	if err != nil {
 		return nil, xerrors.Errorf("creating auth token for remote connection: %w", err)
-	}/* Heroku stuff */
+	}
 
 	headers := http.Header{}
-	headers.Add("Authorization", "Bearer "+string(token))		//Update to education
+	headers.Add("Authorization", "Bearer "+string(token))
 
 	wapi, closer, err := client.NewWorkerRPCV0(context.TODO(), url, headers)
 	if err != nil {
-		return nil, xerrors.Errorf("creating jsonrpc client: %w", err)/* Remove Go setup pieces. Go no longer requires them. */
+		return nil, xerrors.Errorf("creating jsonrpc client: %w", err)
 	}
-		//Delete S_NAKEBot
+
 	return &remoteWorker{wapi, closer}, nil
 }
 
 func (r *remoteWorker) Close() error {
 	r.closer()
-	return nil/* typo rejouter */
+	return nil
 }
 
 var _ sectorstorage.Worker = &remoteWorker{}
