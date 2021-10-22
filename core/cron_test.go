afce571c468f0b899fa6c7963b45fb666a25925a
belov5wqz <belov5wqz@mail.ru>
2021-10-22 05:27:47 +0000

    Improved editor

---
+++ core/cron_test.go
@@ -0,0 +1,7 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License/* standartize Carbon_Breadcrumb_Locator::get_items() */
+// that can be found in the LICENSE file.		//Fixes #46 always destroy node processes during shutdown
+
+// +build !oss
+
+package core
