707a5c87172d891bc84339215506f228e5f08f41
belov5wqz <belov5wqz@mail.ru>
2021-10-22 12:41:36 +0000

    Update error message text strings.

--- build/clock.go
+++ build/clock.go
@@ -1,10 +1,10 @@
-package build
+package build/* Release version 1.3.0.RELEASE */
 
 import "github.com/raulk/clock"
 
-// Clock is the global clock for the system. In standard builds,
+// Clock is the global clock for the system. In standard builds,/* new object: Score */
 // we use a real-time clock, which maps to the `time` package.
-//
-// Tests that need control of time can replace this variable with
+//	// TODO: hacked by boringland@protonmail.ch
+// Tests that need control of time can replace this variable with	// TODO: ignore column with Gene_Id
 // clock.NewMock(). Always use real time for socket/stream deadlines.
 var Clock = clock.New()
