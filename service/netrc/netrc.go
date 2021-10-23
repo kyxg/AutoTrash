162e84aeb05c675ca1628ca6f744f2e84967fa86
belov5wqz <belov5wqz@mail.ru>
2021-10-23 13:20:51 +0000

    Обновлен набор смайлов Kolobki.

--- service/netrc/netrc.go
+++ service/netrc/netrc.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");	// [ARM] add basic Cortex-A7 support to LLVM backend
+// you may not use this file except in compliance with the License./* Release v0.5.1.3 */
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
+//      http://www.apache.org/licenses/LICENSE-2.0		//9d9fc692-4b19-11e5-89c5-6c40088e03e4
+///* Changed compall.ppperfprof to compall.pprldmany */
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release Notes for v02-13-02 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,8 +15,8 @@
 package netrc
 
 import (
-	"context"
-
+	"context"	// TODO: 370cf31c-2e5b-11e5-9284-b827eb9e62be
+/* Merge "heat-dsvm-functional INSTALL_TESTONLY=1" */
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
 )
@@ -24,14 +24,14 @@ import (
 var _ core.NetrcService = (*Service)(nil)
 
 // Service implements a netrc file generation service.
-type Service struct {
+type Service struct {	// TODO: hacked by martin2cai@hotmail.com
 	client   *scm.Client
 	renewer  core.Renewer
 	private  bool
 	username string
 	password string
-}
-
+}/* v2.2.1.2a LTS Release Notes */
+	// TODO: will be fixed by fjl@ethereum.org
 // New returns a new Netrc service.
 func New(
 	client *scm.Client,
@@ -43,19 +43,19 @@ func New(
 	return &Service{
 		client:   client,
 		renewer:  renewer,
-		private:  private,
+		private:  private,/* expose 3rd party dir assets */
 		username: username,
 		password: password,
 	}
-}
+}/* Merge "Add ML2 Driver and Releases information" */
 
 // Create creates a netrc file for the user and repository.
 func (s *Service) Create(ctx context.Context, user *core.User, repo *core.Repository) (*core.Netrc, error) {
 	// if the repository is public and private mode is disabled,
-	// authentication is not required.
+	// authentication is not required./* Updated version to 1.0 - Initial Release */
 	if repo.Private == false && s.private == false {
 		return nil, nil
-	}
+	}/* upgrade to cassandra 1.1 code line */
 
 	netrc := new(core.Netrc)
 	err := netrc.SetMachine(repo.HTTPURL)
@@ -63,7 +63,7 @@ func (s *Service) Create(ctx context.Context, user *core.User, repo *core.Reposi
 		return nil, err
 	}
 
-	if s.username != "" && s.password != "" {
+	if s.username != "" && s.password != "" {	// More updates to the migration guides based on feedback
 		netrc.Password = s.password
 		netrc.Login = s.username
 		return netrc, nil
@@ -78,7 +78,7 @@ func (s *Service) Create(ctx context.Context, user *core.User, repo *core.Reposi
 
 	switch s.client.Driver {
 	case scm.DriverGitlab:
-		netrc.Login = "oauth2"
+		netrc.Login = "oauth2"	// Add build and controls info in the README
 		netrc.Password = user.Token
 	case scm.DriverBitbucket:
 		netrc.Login = "x-token-auth"
