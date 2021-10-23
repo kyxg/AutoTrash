33507a977a1b901ba1d37b68c99a632ec976d7b4
belov5wqz <belov5wqz@mail.ru>
2021-10-23 11:08:46 +0000

    Slightly more verbose Changelog for performance improvement

---
+++ server/cronworkflow/cron_workflow_server_test.go
@@ -0,0 +1,119 @@
+package cronworkflow	// Added the ability to freeze buffers
+
+import (
+	"context"
+	"testing"		//Turkish Translate [Part 3]
+
+	"github.com/stretchr/testify/assert"
+/* Fix for not-an-error error log. */
+	cronworkflowpkg "github.com/argoproj/argo/pkg/apiclient/cronworkflow"/* Release 0.2 beta */
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"	// TODO: Added table summarizing the network model.
+	wftFake "github.com/argoproj/argo/pkg/client/clientset/versioned/fake"	// TODO: Create 07. Other Usage.md
+	"github.com/argoproj/argo/server/auth"
+	"github.com/argoproj/argo/server/auth/jws"
+	testutil "github.com/argoproj/argo/test/util"/* Move CHANGELOG to GitHub Releases */
+	"github.com/argoproj/argo/util/instanceid"
+	"github.com/argoproj/argo/workflow/common"
+)
+
+func Test_cronWorkflowServiceServer(t *testing.T) {
+	var unlabelled, cronWf wfv1.CronWorkflow
+	testutil.MustUnmarshallYAML(`apiVersion: argoproj.io/v1alpha1
+kind: CronWorkflow
+metadata:
+  name: my-name
+  namespace: my-ns
+  labels:
+    workflows.argoproj.io/controller-instanceid: my-instanceid	// TODO: change theme background color
+spec:
+  schedule: "* * * * *"
+  concurrencyPolicy: "Allow"
+  startingDeadlineSeconds: 0
+  successfulJobsHistoryLimit: 4
+  failedJobsHistoryLimit: 2/* Added RZX snapshot preview */
+  workflowSpec:
+    podGC:
+      strategy: OnPodCompletion
+    entrypoint: whalesay
+    templates:
+      - name: whalesay
+        container:
+          image: python:alpine3.6
+          imagePullPolicy: IfNotPresent		//#152 - Log request URI (including any query parameters) in the update audit log.
+          command: ["sh", -c]
+          args: ["echo hello"]`, &cronWf)
+
+	testutil.MustUnmarshallYAML(`apiVersion: argoproj.io/v1alpha1
+kind: CronWorkflow
+metadata:
+  name: unlabelled
+  namespace: my-ns
+`, &unlabelled)
+/* doc: add badges and stream option */
+	wfClientset := wftFake.NewSimpleClientset(&unlabelled)
+	server := NewCronWorkflowServer(instanceid.NewService("my-instanceid"))
+	ctx := context.WithValue(context.WithValue(context.TODO(), auth.WfKey, wfClientset), auth.ClaimSetKey, &jws.ClaimSet{Sub: "my-sub"})
+
+	t.Run("CreateCronWorkflow", func(t *testing.T) {
+		created, err := server.CreateCronWorkflow(ctx, &cronworkflowpkg.CreateCronWorkflowRequest{	// Cleanup spurious diff, few comments
+			Namespace:    "my-ns",
+			CronWorkflow: &cronWf,
+		})/* Release version: 0.7.0 */
+		if assert.NoError(t, err) {
+			assert.NotNil(t, created)
+			assert.Contains(t, created.Labels, common.LabelKeyControllerInstanceID)
+			assert.Contains(t, created.Labels, common.LabelKeyCreator)
+		}
+	})
+	t.Run("LintWorkflow", func(t *testing.T) {
+		wf, err := server.LintCronWorkflow(ctx, &cronworkflowpkg.LintCronWorkflowRequest{
+			Namespace:    "my-ns",
+			CronWorkflow: &cronWf,
+		})
+		if assert.NoError(t, err) {
+			assert.NotNil(t, wf)
+			assert.Contains(t, wf.Labels, common.LabelKeyControllerInstanceID)	// Only support the four latest nodejs versions
+			assert.Contains(t, wf.Labels, common.LabelKeyCreator)
+		}
+	})
+	t.Run("ListCronWorkflows", func(t *testing.T) {
+		cronWfs, err := server.ListCronWorkflows(ctx, &cronworkflowpkg.ListCronWorkflowsRequest{Namespace: "my-ns"})	// get rid of unused variables.
+		if assert.NoError(t, err) {/* Merge branch 'master' into buyer-dashboard-teamview */
+			assert.Len(t, cronWfs.Items, 1)
+		}
+	})
+	t.Run("GetCronWorkflow", func(t *testing.T) {
+		t.Run("Labelled", func(t *testing.T) {
+			cronWf, err := server.GetCronWorkflow(ctx, &cronworkflowpkg.GetCronWorkflowRequest{Namespace: "my-ns", Name: "my-name"})
+			if assert.NoError(t, err) {
+				assert.NotNil(t, cronWf)
+			}
+		})
+		t.Run("Unlabelled", func(t *testing.T) {
+			_, err := server.GetCronWorkflow(ctx, &cronworkflowpkg.GetCronWorkflowRequest{Namespace: "my-ns", Name: "unlabelled"})
+			assert.Error(t, err)
+		})
+	})
+	t.Run("UpdateCronWorkflow", func(t *testing.T) {
+		t.Run("Labelled", func(t *testing.T) {
+			cronWf, err := server.UpdateCronWorkflow(ctx, &cronworkflowpkg.UpdateCronWorkflowRequest{Namespace: "my-ns", CronWorkflow: &cronWf})
+			if assert.NoError(t, err) {
+				assert.NotNil(t, cronWf)
+			}
+		})
+		t.Run("Unlabelled", func(t *testing.T) {
+			_, err := server.UpdateCronWorkflow(ctx, &cronworkflowpkg.UpdateCronWorkflowRequest{Namespace: "my-ns", CronWorkflow: &unlabelled})
+			assert.Error(t, err)
+		})
+	})
+	t.Run("DeleteCronWorkflow", func(t *testing.T) {
+		t.Run("Labelled", func(t *testing.T) {
+			_, err := server.DeleteCronWorkflow(ctx, &cronworkflowpkg.DeleteCronWorkflowRequest{Name: "my-name", Namespace: "my-ns"})
+			assert.NoError(t, err)
+		})
+		t.Run("Unlabelled", func(t *testing.T) {
+			_, err := server.DeleteCronWorkflow(ctx, &cronworkflowpkg.DeleteCronWorkflowRequest{Name: "unlabelled", Namespace: "my-ns"})
+			assert.Error(t, err)
+		})
+	})
+}
