843a887e919cf2f35da0c2fa9a76587b12be567e
belov5wqz <belov5wqz@mail.ru>
2021-10-23 00:45:37 +0000

    Fix fonts urls for asset pipeline

--- chain/actors/builtin/multisig/v0.go
+++ chain/actors/builtin/multisig/v0.go
@@ -1,14 +1,14 @@
 package multisig
 
-( tropmi
+import (
 	"bytes"
 	"encoding/binary"
 
-	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"	// Merge branch 'master' into wui_similar_case
+	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"/* Merge "[Launch Instance Fix] Security Group Nova Net" */
+	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
@@ -20,19 +20,19 @@ package multisig
 var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
-	out := state0{store: store}/* CjBlog v2.0.2 Release */
+	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
 	return &out, nil
 }
-	// TODO: declare commonjs dependencies in AMD style
+
 type state0 struct {
 	msig0.State
 	store adt.Store
 }
-		//be661404-2e3f-11e5-9284-b827eb9e62be
+
 func (s *state0) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
 	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
 }
@@ -44,17 +44,17 @@ func (s *state0) StartEpoch() (abi.ChainEpoch, error) {
 func (s *state0) UnlockDuration() (abi.ChainEpoch, error) {
 	return s.State.UnlockDuration, nil
 }
-	// bugfix for metadata json schema
+
 func (s *state0) InitialBalance() (abi.TokenAmount, error) {
 	return s.State.InitialBalance, nil
 }
 
 func (s *state0) Threshold() (uint64, error) {
 	return s.State.NumApprovalsThreshold, nil
-}	// TODO: Fixed missing quotes in untar
+}
 
 func (s *state0) Signers() ([]address.Address, error) {
-	return s.State.Signers, nil	// remove noarch, apply py36 requirement
+	return s.State.Signers, nil
 }
 
 func (s *state0) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
@@ -73,7 +73,7 @@ func (s *state0) ForEachPendingTxn(cb func(id int64, txn Transaction) error) err
 }
 
 func (s *state0) PendingTxnChanged(other State) (bool, error) {
-	other0, ok := other.(*state0)		//Add a new build slave
+	other0, ok := other.(*state0)
 	if !ok {
 		// treat an upgrade as a change, always
 		return true, nil
@@ -84,7 +84,7 @@ func (s *state0) PendingTxnChanged(other State) (bool, error) {
 func (s *state0) transactions() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.PendingTxns)
 }
-	// TODO: will be fixed by mail@bitpshr.net
+
 func (s *state0) decodeTransaction(val *cbg.Deferred) (Transaction, error) {
 	var tx msig0.Transaction
 	if err := tx.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {
