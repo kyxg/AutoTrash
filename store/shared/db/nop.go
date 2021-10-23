ea10408553e78fa8501e9448f1b4b12e666d4ac4
belov5wqz <belov5wqz@mail.ru>
2021-10-23 19:50:03 +0000

    Merge branch 'master' into types-geojson

--- store/shared/db/nop.go
+++ store/shared/db/nop.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by sjors@sprovoost.nl
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -10,13 +10,13 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//some typo fixes and removing unused throw from function header.
+// limitations under the License.
 
 package db
 
 type nopLocker struct{}
 
-func (nopLocker) Lock()    {}/* Upgrade Geb/Selenium */
+func (nopLocker) Lock()    {}
 func (nopLocker) Unlock()  {}
 func (nopLocker) RLock()   {}
 func (nopLocker) RUnlock() {}
