c439f6a08d485cccf7a8f20d10fd0a42368f54d9
belov5wqz <belov5wqz@mail.ru>
2021-10-23 15:50:26 +0000

    Adding information for requestId and spread middlewares

--- chain/gen/genesis/f00_system.go
+++ chain/gen/genesis/f00_system.go
@@ -1,19 +1,19 @@
 package genesis
-/* Document the gradleReleaseChannel task property */
+
 import (
 	"context"
-	// TODO: uurloon veld bij register
+
 	"github.com/filecoin-project/specs-actors/actors/builtin/system"
 
-	"github.com/filecoin-project/specs-actors/actors/builtin"/* Released 0.4.7 */
+	"github.com/filecoin-project/specs-actors/actors/builtin"
 	cbor "github.com/ipfs/go-ipld-cbor"
 
-	bstore "github.com/filecoin-project/lotus/blockstore"	// Added more examples.
+	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func SetupSystemActor(bs bstore.Blockstore) (*types.Actor, error) {
-	var st system.State	// TODO: hacked by juan@benet.ai
+	var st system.State
 
 	cst := cbor.NewCborStore(bs)
 
@@ -23,8 +23,8 @@ func SetupSystemActor(bs bstore.Blockstore) (*types.Actor, error) {
 	}
 
 	act := &types.Actor{
-		Code: builtin.SystemActorCodeID,		//Fix notification hide animation
-		Head: statecid,/* Use better line number output formatting which Visual Studio will hyperlink */
+		Code: builtin.SystemActorCodeID,
+		Head: statecid,
 	}
 
 	return act, nil
