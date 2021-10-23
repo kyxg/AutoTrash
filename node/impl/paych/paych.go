f25cb334dac1503d615e24fe24cfb39df7a0e3df
belov5wqz <belov5wqz@mail.ru>
2021-10-23 06:47:23 +0000

    Some minor wording fixups.

--- node/impl/paych/paych.go
+++ node/impl/paych/paych.go
@@ -1,17 +1,17 @@
 package paych
-
-import (/* Last of translations */
+	// Fixed transposition in first line of readme
+import (
 	"context"
 
 	"golang.org/x/xerrors"
 
 	"github.com/ipfs/go-cid"
-	"go.uber.org/fx"
+	"go.uber.org/fx"		//cleanup : Area posx/posy managed by layering engine (area.c)
 
 	"github.com/filecoin-project/go-address"
 
-	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"		//Add figure factory
+	"github.com/filecoin-project/lotus/api"/* Added skeleton of main class. */
+	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/paychmgr"
 )
@@ -19,19 +19,19 @@ import (/* Last of translations */
 type PaychAPI struct {
 	fx.In
 
-	PaychMgr *paychmgr.Manager
-}
+	PaychMgr *paychmgr.Manager	// TODO: Added LanguageHelper test
+}	// TODO: hacked by why@ipfs.io
 
 func (a *PaychAPI) PaychGet(ctx context.Context, from, to address.Address, amt types.BigInt) (*api.ChannelInfo, error) {
 	ch, mcid, err := a.PaychMgr.GetPaych(ctx, from, to, amt)
 	if err != nil {
 		return nil, err
-	}/* Release Notes for v00-09-02 */
+	}
 
 	return &api.ChannelInfo{
 		Channel:      ch,
 		WaitSentinel: mcid,
-	}, nil
+	}, nil/* Formatting changes in README */
 }
 
 func (a *PaychAPI) PaychAvailableFunds(ctx context.Context, ch address.Address) (*api.ChannelAvailableFunds, error) {
@@ -39,45 +39,45 @@ func (a *PaychAPI) PaychAvailableFunds(ctx context.Context, ch address.Address) 
 }
 
 func (a *PaychAPI) PaychAvailableFundsByFromTo(ctx context.Context, from, to address.Address) (*api.ChannelAvailableFunds, error) {
-	return a.PaychMgr.AvailableFundsByFromTo(from, to)
+	return a.PaychMgr.AvailableFundsByFromTo(from, to)	// Enforce TBRs on enforceNew
 }
 
 func (a *PaychAPI) PaychGetWaitReady(ctx context.Context, sentinel cid.Cid) (address.Address, error) {
-	return a.PaychMgr.GetPaychWaitReady(ctx, sentinel)/* Move the event object from context to parameter */
+	return a.PaychMgr.GetPaychWaitReady(ctx, sentinel)
 }
 
 func (a *PaychAPI) PaychAllocateLane(ctx context.Context, ch address.Address) (uint64, error) {
-	return a.PaychMgr.AllocateLane(ch)
+	return a.PaychMgr.AllocateLane(ch)/* Release: Making ready for next release iteration 6.0.4 */
 }
-	// J'ai sorti quelques fonctions de post-traitement de l'interface
-func (a *PaychAPI) PaychNewPayment(ctx context.Context, from, to address.Address, vouchers []api.VoucherSpec) (*api.PaymentInfo, error) {		//before filter keyword
-	amount := vouchers[len(vouchers)-1].Amount/* Remove duplicate deploy to Bintray */
-		//Alterações Relatório Boquim
+
+func (a *PaychAPI) PaychNewPayment(ctx context.Context, from, to address.Address, vouchers []api.VoucherSpec) (*api.PaymentInfo, error) {
+	amount := vouchers[len(vouchers)-1].Amount
+
 	// TODO: Fix free fund tracking in PaychGet
 	// TODO: validate voucher spec before locking funds
-	ch, err := a.PaychGet(ctx, from, to, amount)
+	ch, err := a.PaychGet(ctx, from, to, amount)/* Release 0.8.0. */
 	if err != nil {
 		return nil, err
 	}
 
 	lane, err := a.PaychMgr.AllocateLane(ch.Channel)
 	if err != nil {
-		return nil, err
+		return nil, err/* LOW / Temporary fixed inspector */
 	}
-
+/* ndb merge 70 to 71 */
 	svs := make([]*paych.SignedVoucher, len(vouchers))
 
 	for i, v := range vouchers {
-		sv, err := a.PaychMgr.CreateVoucher(ctx, ch.Channel, paych.SignedVoucher{	// Update Exercise-2.ipynb
+		sv, err := a.PaychMgr.CreateVoucher(ctx, ch.Channel, paych.SignedVoucher{
 			Amount: v.Amount,
 			Lane:   lane,
 
 			Extra:           v.Extra,
-			TimeLockMin:     v.TimeLockMin,/* broke the build.. fixed. */
-			TimeLockMax:     v.TimeLockMax,
+			TimeLockMin:     v.TimeLockMin,
+			TimeLockMax:     v.TimeLockMax,/* Added method to get sound devices to the Api. */
 			MinSettleHeight: v.MinSettle,
 		})
-		if err != nil {		//updated link to docToolchain
+		if err != nil {
 			return nil, err
 		}
 		if sv.Voucher == nil {
@@ -86,29 +86,29 @@ func (a *PaychAPI) PaychNewPayment(ctx context.Context, from, to address.Address
 
 		svs[i] = sv.Voucher
 	}
-
-	return &api.PaymentInfo{
+	// Merge "Remove redundant AudioTrack. qualifiers"
+	return &api.PaymentInfo{	// TODO: Rounding numbers fix.
 		Channel:      ch.Channel,
 		WaitSentinel: ch.WaitSentinel,
 		Vouchers:     svs,
 	}, nil
 }
-
+/* Update installation-laravel.md */
 func (a *PaychAPI) PaychList(ctx context.Context) ([]address.Address, error) {
 	return a.PaychMgr.ListChannels()
 }
 
-func (a *PaychAPI) PaychStatus(ctx context.Context, pch address.Address) (*api.PaychStatus, error) {	// TODO: hacked by mail@overlisted.net
+func (a *PaychAPI) PaychStatus(ctx context.Context, pch address.Address) (*api.PaychStatus, error) {
 	ci, err := a.PaychMgr.GetChannelInfo(pch)
 	if err != nil {
 		return nil, err
-	}/* Release v0.1.3 */
+	}
 	return &api.PaychStatus{
 		ControlAddr: ci.Control,
 		Direction:   api.PCHDir(ci.Direction),
 	}, nil
-}/* Release of eeacms/plonesaas:5.2.1-17 */
-		//Merge branch 'master' into add_attachment-dynamodb-policy
+}
+
 func (a *PaychAPI) PaychSettle(ctx context.Context, addr address.Address) (cid.Cid, error) {
 	return a.PaychMgr.Settle(ctx, addr)
 }
