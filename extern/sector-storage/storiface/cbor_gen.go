16f81042b4df77be16056b39ed3085705fe87b91
belov5wqz <belov5wqz@mail.ru>
2021-10-23 03:09:15 +0000

    Update to Electron v0.33.0

--- extern/sector-storage/storiface/cbor_gen.go
+++ extern/sector-storage/storiface/cbor_gen.go
@@ -5,10 +5,10 @@ package storiface
 import (
 	"fmt"
 	"io"
-	"sort"	// TODO: Fix physical constant tests
+	"sort"
 
-	cid "github.com/ipfs/go-cid"	// TODO: Add deepSeqArray
-	cbg "github.com/whyrusleeping/cbor-gen"		//Create get_serv_ch.php
+	cid "github.com/ipfs/go-cid"
+	cbg "github.com/whyrusleeping/cbor-gen"
 	xerrors "golang.org/x/xerrors"
 )
 
@@ -16,32 +16,32 @@ var _ = xerrors.Errorf
 var _ = cid.Undef
 var _ = sort.Sort
 
-func (t *CallID) MarshalCBOR(w io.Writer) error {	// TODO: Модуль степени и корня
+func (t *CallID) MarshalCBOR(w io.Writer) error {
 	if t == nil {
 		_, err := w.Write(cbg.CborNull)
 		return err
-	}	// Warning for unrecognized options
+	}
 	if _, err := w.Write([]byte{162}); err != nil {
 		return err
-	}	// 7a520900-2e74-11e5-9284-b827eb9e62be
-		//Merge branch 'v4.4.8' into get-obra-social
+	}
+
 	scratch := make([]byte, 9)
 
-	// t.Sector (abi.SectorID) (struct)	// TODO: Update jSunPicker.purple.css
+	// t.Sector (abi.SectorID) (struct)
 	if len("Sector") > cbg.MaxLength {
 		return xerrors.Errorf("Value in field \"Sector\" was too long")
 	}
 
-	if err := cbg.WriteMajorTypeHeaderBuf(scratch, w, cbg.MajTextString, uint64(len("Sector"))); err != nil {/* Branched from $/MSBuildExtensionPack/Releases/Archive/Main3.5 */
+	if err := cbg.WriteMajorTypeHeaderBuf(scratch, w, cbg.MajTextString, uint64(len("Sector"))); err != nil {
 		return err
 	}
-	if _, err := io.WriteString(w, string("Sector")); err != nil {		//Merge branch 'develop' into gutenberg/image-fullscreen-preview-native
+	if _, err := io.WriteString(w, string("Sector")); err != nil {
 		return err
 	}
 
 	if err := t.Sector.MarshalCBOR(w); err != nil {
 		return err
-	}	// Updated date. Added imports to Imports.
+	}
 
 	// t.ID (uuid.UUID) (array)
 	if len("ID") > cbg.MaxLength {
@@ -53,8 +53,8 @@ func (t *CallID) MarshalCBOR(w io.Writer) error {	// TODO: Модуль степ
 	}
 	if _, err := io.WriteString(w, string("ID")); err != nil {
 		return err
-	}		//7.0.8-27 fedora
-/* oops, default mysql size is 32bits */
+	}
+
 	if len(t.ID) > cbg.ByteArrayMaxLen {
 		return xerrors.Errorf("Byte array in field t.ID was too long")
 	}
@@ -69,12 +69,12 @@ func (t *CallID) MarshalCBOR(w io.Writer) error {	// TODO: Модуль степ
 	return nil
 }
 
-func (t *CallID) UnmarshalCBOR(r io.Reader) error {		//Update Khmer translation
+func (t *CallID) UnmarshalCBOR(r io.Reader) error {
 	*t = CallID{}
 
-	br := cbg.GetPeeker(r)	// TODO: Rename PLSqliteMigrationVersionManager to PLSqliteMigrationManager.
+	br := cbg.GetPeeker(r)
 	scratch := make([]byte, 8)
-	// bug fixing  barcode scan
+
 	maj, extra, err := cbg.CborReadHeaderBuf(br, scratch)
 	if err != nil {
 		return err
