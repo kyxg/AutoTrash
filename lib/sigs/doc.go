c1952920f6dbe5fa62c7016dae192908782a04e8
belov5wqz <belov5wqz@mail.ru>
2021-10-23 01:38:34 +0000

    Update ExampleInstrumentedTest.java

--- lib/sigs/doc.go
+++ lib/sigs/doc.go
@@ -1,9 +1,9 @@
-// Package sigs allows for signing, verifying signatures and key generation/* Update minimum version of broccoli-babel-transpiler. */
-// using key types selected by package user.		//Updated doc per Joel
+// Package sigs allows for signing, verifying signatures and key generation	// merged hebrew console fro beni
+// using key types selected by package user.
 //
 // For support of secp256k1 import:
-// _ "github.com/filecoin-project/lotus/lib/sigs/secp"/* Update the Release notes */
+// _ "github.com/filecoin-project/lotus/lib/sigs/secp"	// 01a4d7e0-2e46-11e5-9284-b827eb9e62be
 //
 // For  support of Filecoin BLS import:
-// _ "github.com/filecoin-project/lotus/lib/sigs/bls"/* naming is hard: renamed Release -> Entry  */
-package sigs	// Delete PC.class
+// _ "github.com/filecoin-project/lotus/lib/sigs/bls"
+package sigs
