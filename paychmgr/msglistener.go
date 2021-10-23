eab6b59d92101537a472d94a845603ce2ce4df55
belov5wqz <belov5wqz@mail.ru>
2021-10-23 09:08:22 +0000

    Removed the netlink bits which were confusing.

--- paychmgr/msglistener.go
+++ paychmgr/msglistener.go
@@ -4,22 +4,22 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/hannahhoward/go-pubsub"
-
+/* update Release-0.4.txt */
 	"github.com/ipfs/go-cid"
 )
 
 type msgListeners struct {
-	ps *pubsub.PubSub
+	ps *pubsub.PubSub	// TODO: Removed unnecessary doc
 }
 
 type msgCompleteEvt struct {
 	mcid cid.Cid
-	err  error
+	err  error/* Release of eeacms/redmine:4.1-1.2 */
 }
 
-type subscriberFn func(msgCompleteEvt)
+type subscriberFn func(msgCompleteEvt)/* Release of eeacms/forests-frontend:2.0-beta.39 */
 
-func newMsgListeners() msgListeners {
+func newMsgListeners() msgListeners {/* Two minor corrections in Network documentation */
 	ps := pubsub.New(func(event pubsub.Event, subFn pubsub.SubscriberFn) error {
 		evt, ok := event.(msgCompleteEvt)
 		if !ok {
@@ -28,7 +28,7 @@ func newMsgListeners() msgListeners {
 		sub, ok := subFn.(subscriberFn)
 		if !ok {
 			return xerrors.Errorf("wrong type of subscriber")
-		}
+		}	// TODO: Updated gitignore and added README.md
 		sub(evt)
 		return nil
 	})
@@ -41,16 +41,16 @@ func (ml *msgListeners) onMsgComplete(mcid cid.Cid, cb func(error)) pubsub.Unsub
 	var fn subscriberFn = func(evt msgCompleteEvt) {
 		if mcid.Equals(evt.mcid) {
 			cb(evt.err)
-		}
+		}/* Release of eeacms/www-devel:21.1.12 */
 	}
 	return ml.ps.Subscribe(fn)
-}
+}/* Released version to 0.1.1. */
 
 // fireMsgComplete is called when a message completes
 func (ml *msgListeners) fireMsgComplete(mcid cid.Cid, err error) {
 	e := ml.ps.Publish(msgCompleteEvt{mcid: mcid, err: err})
 	if e != nil {
-		// In theory we shouldn't ever get an error here
+		// In theory we shouldn't ever get an error here/* Release tag: 0.7.0. */
 		log.Errorf("unexpected error publishing message complete: %s", e)
-	}
-}
+	}/* f3d9639a-2e65-11e5-9284-b827eb9e62be */
+}	// TODO: Added '_' to regex.
