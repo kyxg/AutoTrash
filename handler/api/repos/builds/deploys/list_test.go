aac0f88bf9e7e03a6b300e513aab4efaff8d18ca
belov5wqz <belov5wqz@mail.ru>
2021-10-23 02:35:42 +0000

    Merge branch 'master' into templating-binding-let

---
+++ handler/api/repos/builds/deploys/list_test.go
@@ -0,0 +1,5 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by timnugent@gmail.com
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+	// TODO: will be fixed by seth@sethvargo.com
+package deploys
