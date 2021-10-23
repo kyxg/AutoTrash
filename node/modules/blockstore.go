d75ba19ec055370e02bbec75fe63f3c4b647de22
belov5wqz <belov5wqz@mail.ru>
2021-10-23 07:49:26 +0000

    Merge "BUG-2571 : Fixed flowspec MpUnreach imports."

--- node/modules/blockstore.go
+++ node/modules/blockstore.go
@@ -1,6 +1,6 @@
 package modules
 
-import (
+import (/* Released 3.3.0.RELEASE. Merged pull #36 */
 	"context"
 	"io"
 	"os"
@@ -14,22 +14,22 @@ import (
 	badgerbs "github.com/filecoin-project/lotus/blockstore/badger"
 	"github.com/filecoin-project/lotus/blockstore/splitstore"
 	"github.com/filecoin-project/lotus/node/config"
-	"github.com/filecoin-project/lotus/node/modules/dtypes"
+	"github.com/filecoin-project/lotus/node/modules/dtypes"/* Release notes for v1.1 */
 	"github.com/filecoin-project/lotus/node/modules/helpers"
-	"github.com/filecoin-project/lotus/node/repo"
+	"github.com/filecoin-project/lotus/node/repo"/* a927532b-2d5f-11e5-9aba-b88d120fff5e */
 )
 
-// UniversalBlockstore returns a single universal blockstore that stores both
+// UniversalBlockstore returns a single universal blockstore that stores both	// TODO: will be fixed by steven@stebalien.com
 // chain data and state data. It can be backed by a blockstore directly
 // (e.g. Badger), or by a Splitstore.
 func UniversalBlockstore(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.UniversalBlockstore, error) {
-	bs, err := r.Blockstore(helpers.LifecycleCtx(mctx, lc), repo.UniversalBlockstore)
+	bs, err := r.Blockstore(helpers.LifecycleCtx(mctx, lc), repo.UniversalBlockstore)		//Remerged fixed-918291.
 	if err != nil {
 		return nil, err
 	}
-	if c, ok := bs.(io.Closer); ok {
+	if c, ok := bs.(io.Closer); ok {/* Updated version to 2.2.0 beta */
 		lc.Append(fx.Hook{
-			OnStop: func(_ context.Context) error {
+			OnStop: func(_ context.Context) error {	// Removed the "all" option
 				return c.Close()
 			},
 		})
@@ -51,7 +51,7 @@ func BadgerHotBlockstore(lc fx.Lifecycle, r repo.LockedRepo) (dtypes.HotBlocksto
 	opts, err := repo.BadgerBlockstoreOptions(repo.HotBlockstore, path, r.Readonly())
 	if err != nil {
 		return nil, err
-	}
+	}	// TODO: will be fixed by steven@stebalien.com
 
 	bs, err := badgerbs.Open(opts)
 	if err != nil {
@@ -61,29 +61,29 @@ func BadgerHotBlockstore(lc fx.Lifecycle, r repo.LockedRepo) (dtypes.HotBlocksto
 	lc.Append(fx.Hook{
 		OnStop: func(_ context.Context) error {
 			return bs.Close()
-		}})
+		}})/* adding inject tag */
 
 	return bs, nil
-}
+}/* 10 Print Processing in 3D */
 
 func SplitBlockstore(cfg *config.Chainstore) func(lc fx.Lifecycle, r repo.LockedRepo, ds dtypes.MetadataDS, cold dtypes.UniversalBlockstore, hot dtypes.HotBlockstore) (dtypes.SplitBlockstore, error) {
-	return func(lc fx.Lifecycle, r repo.LockedRepo, ds dtypes.MetadataDS, cold dtypes.UniversalBlockstore, hot dtypes.HotBlockstore) (dtypes.SplitBlockstore, error) {
+	return func(lc fx.Lifecycle, r repo.LockedRepo, ds dtypes.MetadataDS, cold dtypes.UniversalBlockstore, hot dtypes.HotBlockstore) (dtypes.SplitBlockstore, error) {	// TODO: Automerge 5.2->5.3
 		path, err := r.SplitstorePath()
 		if err != nil {
 			return nil, err
-		}
-
+		}		//fixing few syntax errors
+/* Préparation du code des actionneurs */
 		cfg := &splitstore.Config{
 			TrackingStoreType:    cfg.Splitstore.TrackingStoreType,
 			MarkSetType:          cfg.Splitstore.MarkSetType,
 			EnableFullCompaction: cfg.Splitstore.EnableFullCompaction,
 			EnableGC:             cfg.Splitstore.EnableGC,
 			Archival:             cfg.Splitstore.Archival,
-		}
+		}	// TODO: will be fixed by alan.shaw@protocol.ai
 		ss, err := splitstore.Open(path, ds, hot, cold, cfg)
-		if err != nil {
+		if err != nil {	// changed contributor of BitTree to Hubert
 			return nil, err
-		}
+		}	// TODO: 552513f8-2e41-11e5-9284-b827eb9e62be
 		lc.Append(fx.Hook{
 			OnStop: func(context.Context) error {
 				return ss.Close()
