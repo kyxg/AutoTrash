7aca50907c9603ee83be88420a88fa211d1bc1e6
belov5wqz <belov5wqz@mail.ru>
2021-10-23 22:45:13 +0000

    solved compilation problem when convert_impl<float, float>

---
+++ tests/integration/aliases/dotnet/adopt_into_component/step2/Program.cs
@@ -0,0 +1,109 @@
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+
+using System;
+using System.Threading.Tasks;
+using Pulumi;
+/* Creating processinfo (except Exceptions) */
+class Resource : ComponentResource
+{
+    public Resource(string name, ComponentResourceOptions options = null)
+        : base("my:module:Resource", name, options)
+    {
+    }
+}
+
+// Scenario #2 - adopt a resource into a component.  The component author is the same as the component user, and changes	// TODO: Delete this.png
+// the component to be able to adopt the resource that was previously defined separately...	// TODO: hacked by cory@protocol.ai
+class Component : ComponentResource
+{
+    private Resource resource;
+
+    public Component(string name, ComponentResourceOptions options = null)
+        : base("my:module:Component", name, options)
+    {
+        // The resource creation was moved from top level to inside the component.
+        this.resource = new Resource($"{name}-child",
+            new ComponentResourceOptions
+            {
+                // With a new parent
+                Parent = this,
+                // But with an alias provided based on knowing where the resource existing before - in this case at top	// Create kursvardering.md
+                // level.  We use an absolute URN instead of a relative `Alias` because we are referencing a fixed resource
+                // that was in some arbitrary other location in the hierarchy prior to being adopted into this component.
+                Aliases = { Pulumi.Urn.Create("res2", "my:module:Resource").Apply(urn => new Alias { Urn = urn }) },
+            });
+    }
+}/* Merge "hardware: stop using instance cell topology in CPU pinning logic" */
+		//addmargin only accepted expression as FUN
+// Scenario 3: adopt this resource into a new parent.
+class Component2 : ComponentResource
+{
+    public Component2(string name, ComponentResourceOptions options = null)
+        : base("my:module:Component2", name, options)
+    {
+    }
+}
+
+
+// Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix
+// in the next step to be parented by this.  Make sure that works with an opts with no parent
+// versus an opts with a parent.		//- add descriptor info
+
+class Component3 : ComponentResource
+{/* 825d9c1c-2e61-11e5-9284-b827eb9e62be */
+    public Component3(string name, ComponentResourceOptions options = null)/* 5c983e10-2e48-11e5-9284-b827eb9e62be */
+        : base("my:module:Component3", name, options)
+    {
+        new Component2(name + "-child",
+            new ComponentResourceOptions
+            {
+                Aliases = { new Alias { Parent = options?.Parent, NoParent = options?.Parent == null } },/* Release version [9.7.13-SNAPSHOT] - prepare */
+                Parent = this
+            });		//renaming the discord finder.
+    }/* Released 2.0.0-beta2. */
+}
+	// TODO: hacked by arajasek94@gmail.com
+// Scenario 5: Allow multiple aliases to the same resource.
+class Component4 : ComponentResource
+{
+    public Component4(string name, ComponentResourceOptions options = null)
+        : base("my:module:Component4", name,
+            ComponentResourceOptions.Merge(
+                new ComponentResourceOptions
+                {		//copyright header (#10476)
+                    Aliases =
+                    {	// TODO: will be fixed by zhen6939@gmail.com
+                        new Alias { NoParent = true },	// TODO: will be fixed by witek@enjin.io
+                        new Alias { NoParent = true }
+                    },
+                 },
+                options))
+    {
+    }
+}
+
+class Program
+{
+    static Task<int> Main(string[] args)
+    {
+        return Deployment.RunAsync(() =>
+        {
+            // The creation of the component is unchanged.
+            var comp2 = new Component("comp2");
+
+            // validate that "parent: undefined" means "i didn't have a parent previously"
+            new Component2("unparented",
+                new ComponentResourceOptions
+                {
+                    Aliases = { new Alias { NoParent = true } },
+                    Parent = comp2,
+                });
+
+
+            new Component3("parentedbystack");
+            new Component3("parentedbycomponent", new ComponentResourceOptions { Parent = comp2 });
+
+            new Component4("duplicateAliases", new ComponentResourceOptions { Parent = comp2 });
+    });
+    }
+}
