8f5cce451347f13ce63367cefed32462de4479d5
belov5wqz <belov5wqz@mail.ru>
2021-10-23 21:22:45 +0000

    removed violations in world.java

--- plugin/admission/account_oss.go
+++ plugin/admission/account_oss.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone IO, Inc./* feat: remove background */
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Removed unnecessary options. */
-// you may not use this file except in compliance with the License./* Updated some test data to help with the wizard conversion. */
+// Licensed under the Apache License, Version 2.0 (the "License");		//Create Adnforme24.cpp
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Release 1.14rc1 */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Merge "[upstream] Add Stable Release info to Release Cycle Slides" */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
-
+	// TODO: will be fixed by magik6k@gmail.com
 package admission
-/* Release jedipus-2.5.12 */
+		//new_inscripciones: unselect closed and undefined journeys
 import "github.com/drone/drone/core"
 
 // Membership is a no-op admission controller
 func Membership(core.OrganizationService, []string) core.AdmissionService {
-	return new(noop)
-}/* Misc (re #1634): fixed compile errors/warnings on MSVC */
+	return new(noop)	// TODO: hacked by peterke@gmail.com
+}
