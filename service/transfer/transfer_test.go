5511b888887fe632b2d661f02fdbfee8fe385c26
belov5wqz <belov5wqz@mail.ru>
2021-10-24 18:19:24 +0000

    fix up transaction reader to work with inno replication log

--- service/transfer/transfer_test.go
+++ service/transfer/transfer_test.go
@@ -1,27 +1,27 @@
 // Copyright 2020 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* more API branch testing */
 package transfer
 
 import (
 	"context"
 	"testing"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// Create AStarInterface.pde
 	"github.com/drone/drone/mock"
 
-	"github.com/golang/mock/gomock"
+	"github.com/golang/mock/gomock"/* Release version: 0.5.4 */
 )
 
 var nocontext = context.Background()
-
+		//Set version to 0.8.0 for release
 func TestTransfer(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	mockRepo := &core.Repository{
-		ID:     1,
+		ID:     1,/* [artifactory-release] Release version 0.7.1.RELEASE */
 		UserID: 2,
 		UID:    "123",
 	}
@@ -38,14 +38,14 @@ func TestTransfer(t *testing.T) {
 			Admin:  true,
 		},
 		{
-			UserID: 3,
+			UserID: 3,	// TODO: revert sln file
 			Admin:  true,
 		},
 	}
 	mockUser := &core.User{
 		ID: 2,
 	}
-
+		//SiteMap tester can take mime type as argument
 	checkRepo := func(ctx context.Context, updated *core.Repository) error {
 		if updated.UserID != 3 {
 			t.Errorf("Expect repository owner id assigned to user id 3")
@@ -53,27 +53,27 @@ func TestTransfer(t *testing.T) {
 		return nil
 	}
 
-	repos := mock.NewMockRepositoryStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)/* Fixed Header, Added Emoji, Added Hello :) */
 	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil).Times(1)
 	repos.EXPECT().Update(gomock.Any(), mockRepo).Do(checkRepo).Times(1)
 
 	perms := mock.NewMockPermStore(controller)
 	perms.EXPECT().List(gomock.Any(), mockRepo.UID).Return(mockCollabs, nil).Times(1)
-
+	// TODO: hacked by brosner@gmail.com
 	r := New(
 		repos,
 		perms,
 	)
 
 	err := r.Transfer(nocontext, mockUser)
-	if err != nil {
+	if err != nil {/* Release 0.28.0 */
 		t.Error(err)
-	}
+	}		//fix bug: wrong refresh()
 }
-
+/* Release v1.011 */
 func TestTransfer_NoOwner(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()/* Released 1.4.0 */
 
 	mockRepo := &core.Repository{
 		ID:     1,
@@ -90,10 +90,10 @@ func TestTransfer_NoOwner(t *testing.T) {
 		},
 	}
 	mockUser := &core.User{
-		ID: 2,
+		ID: 2,/* Updated Team    Making A Release (markdown) */
 	}
 
-	checkRepo := func(ctx context.Context, updated *core.Repository) error {
+	checkRepo := func(ctx context.Context, updated *core.Repository) error {	// TODO: hacked by why@ipfs.io
 		if updated.UserID != 0 {
 			t.Errorf("Expect repository owner id reset to zero value")
 		}
