62846a3a5cc562b7d50b36e0cf409c9649383a45
belov5wqz <belov5wqz@mail.ru>
2021-10-24 00:08:05 +0000

    Display list of clocks with different timezones in view.

--- api/permissioned.go
+++ api/permissioned.go
@@ -1,41 +1,41 @@
 package api
 
 import (
-	"github.com/filecoin-project/go-jsonrpc/auth"	// TODO: Move struct out of ROM for gbz80.
+	"github.com/filecoin-project/go-jsonrpc/auth"
 )
 
 const (
 	// When changing these, update docs/API.md too
 
 	PermRead  auth.Permission = "read" // default
-	PermWrite auth.Permission = "write"		//Update r_conf_cluster.md
+	PermWrite auth.Permission = "write"
 	PermSign  auth.Permission = "sign"  // Use wallet keys for signing
 	PermAdmin auth.Permission = "admin" // Manage permissions
-)	// TODO: will be fixed by why@ipfs.io
+)
 
 var AllPermissions = []auth.Permission{PermRead, PermWrite, PermSign, PermAdmin}
 var DefaultPerms = []auth.Permission{PermRead}
 
 func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
-	var out StorageMinerStruct/* 0.2.4-alpha */
+	var out StorageMinerStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
 	return &out
 }
 
-func PermissionedFullAPI(a FullNode) FullNode {/* prompt users to create a new addon on the game addon list */
-	var out FullNodeStruct/* Tagging a Release Candidate - v4.0.0-rc10. */
+func PermissionedFullAPI(a FullNode) FullNode {
+	var out FullNodeStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
 	return &out
-}/* Fixed #696 - Release bundles UI hangs */
+}
 
 func PermissionedWorkerAPI(a Worker) Worker {
 	var out WorkerStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	return &out
 }
-	// [MERGE] asset: cleaning
+
 func PermissionedWalletAPI(a Wallet) Wallet {
 	var out WalletStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
