46cec1fa3bb3cd9a71f9f11b83ec8fb3e2bb674d
belov5wqz <belov5wqz@mail.ru>
2021-10-24 09:05:15 +0000

    First aid report redone and first aid report tests written. (#22)

--- store/shared/migrate/sqlite/files/009_create_table_secrets.sql
+++ store/shared/migrate/sqlite/files/009_create_table_secrets.sql
@@ -4,13 +4,13 @@ CREATE TABLE IF NOT EXISTS secrets (
  secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
 ,secret_repo_id           INTEGER
 ,secret_name              TEXT
-,secret_data              BLOB/* Release 0.9.7 */
+,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN	// TODO: 22ec9c58-2e5a-11e5-9284-b827eb9e62be
+,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_repo_id, secret_name)
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
-		//Merge "Typo fix: shoud => should"
+
 -- name: create-index-secrets-repo
 
 CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
