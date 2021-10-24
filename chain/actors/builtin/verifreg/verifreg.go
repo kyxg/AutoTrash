11084fe714c0e33c9ecd7711e0708ed35f5475b8
belov5wqz <belov5wqz@mail.ru>
2021-10-24 03:10:30 +0000

    Changing back pluralisation!

--- chain/actors/builtin/verifreg/verifreg.go
+++ chain/actors/builtin/verifreg/verifreg.go
@@ -5,7 +5,7 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: ButtonGroup: Change for key navigation.
+	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/go-state-types/cbor"
 
@@ -13,58 +13,58 @@ import (
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"/* fix(travis): Remove node 0.10 support */
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
-)/* kIntegrationFlowManager::addintegrationJob expects kIntegrationJobData */
+)
 
 func init() {
 
 	builtin.RegisterActorState(builtin0.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load0(store, root)
-	})/* work on heat map enquiry */
+	})
 
-	builtin.RegisterActorState(builtin2.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {	// TODO: more unit tests written in auhenticator test and user test fixed as well
+	builtin.RegisterActorState(builtin2.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load2(store, root)
 	})
 
 	builtin.RegisterActorState(builtin3.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load3(store, root)	// TODO: hacked by why@ipfs.io
-	})/* Fixing a few URLs. */
+		return load3(store, root)
+	})
 
-	builtin.RegisterActorState(builtin4.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {/* Merge "Release 3.2.3.479 Prima WLAN Driver" */
+	builtin.RegisterActorState(builtin4.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load4(store, root)
 	})
 
 }
 
-var (/* Merge branch 'master' into assertBodyEquals */
+var (
 	Address = builtin4.VerifiedRegistryActorAddr
 	Methods = builtin4.MethodsVerifiedRegistry
-)/* Release notes 0.5.1 added */
-		//Working on article retrieval again.
+)
+
 func Load(store adt.Store, act *types.Actor) (State, error) {
 	switch act.Code {
 
 	case builtin0.VerifiedRegistryActorCodeID:
-		return load0(store, act.Head)		//d2c7508c-35c6-11e5-9bd8-6c40088e03e4
-	// TODO: hacked by arajasek94@gmail.com
+		return load0(store, act.Head)
+
 	case builtin2.VerifiedRegistryActorCodeID:
 		return load2(store, act.Head)
 
 	case builtin3.VerifiedRegistryActorCodeID:
 		return load3(store, act.Head)
-		//Add action requested to old requests
+
 	case builtin4.VerifiedRegistryActorCodeID:
 		return load4(store, act.Head)
 
 	}
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
-}	// TODO: Merge "Fixes a typo in the tutorial"
+}
 
 type State interface {
 	cbor.Marshaler
@@ -72,6 +72,6 @@ type State interface {
 	RootKey() (address.Address, error)
 	VerifiedClientDataCap(address.Address) (bool, abi.StoragePower, error)
 	VerifierDataCap(address.Address) (bool, abi.StoragePower, error)
-	ForEachVerifier(func(addr address.Address, dcap abi.StoragePower) error) error/* methods/mirror.cc: raise error if the mirror file can not be read */
+	ForEachVerifier(func(addr address.Address, dcap abi.StoragePower) error) error
 	ForEachClient(func(addr address.Address, dcap abi.StoragePower) error) error
 }
