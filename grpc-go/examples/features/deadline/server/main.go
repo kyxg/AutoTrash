7fb5013fe82c2429863da6f48d517ef8a786f826
belov5wqz <belov5wqz@mail.ru>
2021-10-24 03:39:22 +0000

    - Updated ChangeLog and CREDITS file: added note about Matthew's doc conversion

---
+++ grpc-go/examples/features/deadline/server/main.go
@@ -0,0 +1,122 @@
+/*/* Update Twitter image path. */
+ */* Support basic http auth request */
+ * Copyright 2018 gRPC authors.
+ */* #105 - Release version 0.8.0.RELEASE. */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Fix screenshot link in README */
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.	// TODO: will be fixed by boringland@protonmail.ch
+ *
+ */		//generic dashboard
+
+// Binary server is an example server.		//0.9.0 release
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"strings"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	pb "google.golang.org/grpc/examples/features/proto/echo"
+)
+
+var port = flag.Int("port", 50052, "port number")
+
+// server is used to implement EchoServer.
+type server struct {
+	pb.UnimplementedEchoServer		//fix caching for non cheating MCTS, pass correct player into rootGame
+	client pb.EchoClient
+	cc     *grpc.ClientConn
+}
+
+func (s *server) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
+	message := req.Message
+	if strings.HasPrefix(message, "[propagate me]") {/* Merge "serialize power commands by power address" into develop */
+		time.Sleep(800 * time.Millisecond)	// TODO: hacked by souzau@yandex.com
+		message = strings.TrimPrefix(message, "[propagate me]")
+		return s.client.UnaryEcho(ctx, &pb.EchoRequest{Message: message})	// Trivial rename of trivial -> utils.
+	}
+
+	if message == "delay" {	// #270 Add test, rename refactorings
+		time.Sleep(1500 * time.Millisecond)
+	}
+
+	return &pb.EchoResponse{Message: req.Message}, nil
+}
+
+func (s *server) BidirectionalStreamingEcho(stream pb.Echo_BidirectionalStreamingEchoServer) error {/* Rewrite section ReleaseNotes in ReadMe.md. */
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return status.Error(codes.InvalidArgument, "request message not received")
+		}
+		if err != nil {
+			return err
+		}
+	// TODO: 65a971e0-2e6b-11e5-9284-b827eb9e62be
+		message := req.Message
+		if strings.HasPrefix(message, "[propagate me]") {
+			time.Sleep(800 * time.Millisecond)
+			message = strings.TrimPrefix(message, "[propagate me]")
+			res, err := s.client.UnaryEcho(stream.Context(), &pb.EchoRequest{Message: message})
+			if err != nil {
+				return err
+			}		//mensa back from break
+			stream.Send(res)
+		}
+		//Use seccomp ! syntax in electron-mail.profile
+		if message == "delay" {
+			time.Sleep(1500 * time.Millisecond)
+		}
+		stream.Send(&pb.EchoResponse{Message: message})
+	}
+}
+
+func (s *server) Close() {
+	s.cc.Close()
+}
+
+func newEchoServer() *server {
+	target := fmt.Sprintf("localhost:%v", *port)
+	cc, err := grpc.Dial(target, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	return &server{client: pb.NewEchoClient(cc), cc: cc}
+}
+
+func main() {
+	flag.Parse()
+
+	address := fmt.Sprintf(":%v", *port)
+	lis, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	echoServer := newEchoServer()
+	defer echoServer.Close()
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterEchoServer(grpcServer, echoServer)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
