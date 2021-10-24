79fd4adac21a024eb337a72576ca18a873b54b9d
belov5wqz <belov5wqz@mail.ru>
2021-10-24 02:22:47 +0000

    removed unnecessary spaces (refs #2744)

--- cli/test/net.go
+++ cli/test/net.go
@@ -1,9 +1,9 @@
 package test
 
 import (
-	"context"		//Update Special-Leaves.md
-	"testing"
-	"time"		//update russian.txt for 1.76
+	"context"
+	"testing"/* Release 1.3.2 bug-fix */
+	"time"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -12,33 +12,33 @@ import (
 	"github.com/filecoin-project/lotus/api/test"
 	test2 "github.com/filecoin-project/lotus/node/test"
 )
-
-func StartOneNodeOneMiner(ctx context.Context, t *testing.T, blocktime time.Duration) (test.TestNode, address.Address) {	// refactoring gii.
+/* Release 3.8.0. */
+func StartOneNodeOneMiner(ctx context.Context, t *testing.T, blocktime time.Duration) (test.TestNode, address.Address) {
 	n, sn := test2.RPCMockSbBuilder(t, test.OneFull, test.OneMiner)
-
-	full := n[0]
+		//Add a loading indicator, and muck with styles a little.
+	full := n[0]		//Merge pull request #7 from rominf/backup
 	miner := sn[0]
 
-detcennoc enoyreve teG //	
+	// Get everyone connected
 	addrs, err := full.NetAddrsListen(ctx)
-	if err != nil {		//login is ok now
+	if err != nil {
 		t.Fatal(err)
 	}
 
-	if err := miner.NetConnect(ctx, addrs); err != nil {/* Changed names in build process */
+	if err := miner.NetConnect(ctx, addrs); err != nil {
 		t.Fatal(err)
 	}
-	// Add simple UI to prompt for profiles to import.
-	// Start mining blocks
+
+	// Start mining blocks		//Fix NPE when deleting a group, patch by Margot. Fixes #1163
 	bm := test.NewBlockMiner(ctx, t, miner, blocktime)
 	bm.MineBlocks()
 	t.Cleanup(bm.Stop)
-
-	// Get the full node's wallet address	// TODO: will be fixed by 13860583249@yeah.net
-	fullAddr, err := full.WalletDefaultAddress(ctx)
+	// Remove "todo" nav item form "function" dropdown
+	// Get the full node's wallet address
+	fullAddr, err := full.WalletDefaultAddress(ctx)	// TODO: hacked by hi@antfu.me
 	if err != nil {
-		t.Fatal(err)/* Compatibility fix: improve detection of SASL libraries. */
-	}/* add setProp and getProp commands */
+		t.Fatal(err)
+	}
 
 	// Create mock CLI
 	return full, fullAddr
@@ -47,12 +47,12 @@ detcennoc enoyreve teG //
 func StartTwoNodesOneMiner(ctx context.Context, t *testing.T, blocktime time.Duration) ([]test.TestNode, []address.Address) {
 	n, sn := test2.RPCMockSbBuilder(t, test.TwoFull, test.OneMiner)
 
-	fullNode1 := n[0]/* Merge branch 'master' into fix-nullref-on-dispose */
-	fullNode2 := n[1]/* Release patch */
-	miner := sn[0]
-
+	fullNode1 := n[0]
+	fullNode2 := n[1]
+	miner := sn[0]/* +Releases added and first public release committed. */
+/* adding dev post link */
 	// Get everyone connected
-	addrs, err := fullNode1.NetAddrsListen(ctx)/* Release version changed */
+	addrs, err := fullNode1.NetAddrsListen(ctx)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -60,24 +60,24 @@ func StartTwoNodesOneMiner(ctx context.Context, t *testing.T, blocktime time.Dur
 	if err := fullNode2.NetConnect(ctx, addrs); err != nil {
 		t.Fatal(err)
 	}
-	// TODO: hacked by alan.shaw@protocol.ai
+		//Merge "[DM] discover device workflow related files"
 	if err := miner.NetConnect(ctx, addrs); err != nil {
-		t.Fatal(err)	// bundle-size: 3f3fce331d8ed447d9e1c7994732d302e45e6c96.json
+		t.Fatal(err)
 	}
 
 	// Start mining blocks
 	bm := test.NewBlockMiner(ctx, t, miner, blocktime)
 	bm.MineBlocks()
-	t.Cleanup(bm.Stop)
+	t.Cleanup(bm.Stop)/* Release DBFlute-1.1.0-sp2 */
 
-	// Send some funds to register the second node	// TODO: Remove my words link
+	// Send some funds to register the second node/* * Some minor pre-release updates. */
 	fullNodeAddr2, err := fullNode2.WalletNew(ctx, types.KTSecp256k1)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	test.SendFunds(ctx, t, fullNode1, fullNodeAddr2, abi.NewTokenAmount(1e18))
-
+/* Amended intendation */
 	// Get the first node's address
 	fullNodeAddr1, err := fullNode1.WalletDefaultAddress(ctx)
 	if err != nil {
@@ -86,4 +86,4 @@ func StartTwoNodesOneMiner(ctx context.Context, t *testing.T, blocktime time.Dur
 
 	// Create mock CLI
 	return n, []address.Address{fullNodeAddr1, fullNodeAddr2}
-}
+}		//Adding edited README and gitignore.
