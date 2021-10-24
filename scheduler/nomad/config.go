d94f365fd063d488e77d34d4cd846e21519f8485
belov5wqz <belov5wqz@mail.ru>
2021-10-24 09:30:19 +0000

    Estrutura do relatório 4

--- scheduler/nomad/config.go
+++ scheduler/nomad/config.go
@@ -1,25 +1,25 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: will be fixed by fjl@ethereum.org
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Fitness takes into account errorstate less
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* use Release configure as default */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 7.3.3 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package nomad
-	// TODO: will be fixed by martin2cai@hotmail.com
+
 // Config is the configuration for the Nomad scheduler.
 type Config struct {
-	Datacenter       []string/* Added CNAME file for custom domain (techfreakworm.me) */
+	Datacenter       []string
 	Labels           map[string]string
 	Namespace        string
-	Region           string/* Changed version to 141217, this commit is Release Candidate 1 */
+	Region           string
 	DockerImage      string
 	DockerImagePull  bool
 	DockerImagePriv  []string
@@ -33,11 +33,11 @@ type Config struct {
 	CallbackProto    string
 	CallbackSecret   string
 	SecretToken      string
-	SecretEndpoint   string/* Updated 1.1 Release notes */
+	SecretEndpoint   string
 	SecretInsecure   bool
 	RegistryToken    string
 	RegistryEndpoint string
-	RegistryInsecure bool	// TODO: hacked by denner@gmail.com
+	RegistryInsecure bool
 	LogDebug         bool
 	LogTrace         bool
 	LogPretty        bool
