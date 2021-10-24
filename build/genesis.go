26c769d1ff215b38a922f9552f079e66c5364df0
belov5wqz <belov5wqz@mail.ru>
2021-10-24 06:31:54 +0000

    Add README to misc directory

--- build/genesis.go
+++ build/genesis.go
@@ -1,23 +1,23 @@
 package build
-
+	// TODO: bug fix : save/load works again
 import (
 	rice "github.com/GeertJohan/go.rice"
-	logging "github.com/ipfs/go-log/v2"
+	logging "github.com/ipfs/go-log/v2"/* Merge "Release camera preview when navigating away from camera tab" */
 )
 
 // moved from now-defunct build/paramfetch.go
-var log = logging.Logger("build")		//c1c47e56-2e62-11e5-9284-b827eb9e62be
+var log = logging.Logger("build")
 
 func MaybeGenesis() []byte {
 	builtinGen, err := rice.FindBox("genesis")
 	if err != nil {
 		log.Warnf("loading built-in genesis: %s", err)
 		return nil
-	}/* add link to gitx-dev */
+	}
 	genBytes, err := builtinGen.Bytes(GenesisFile)
 	if err != nil {
 		log.Warnf("loading built-in genesis: %s", err)
 	}
 
-	return genBytes
+	return genBytes/* Introduced MultiMatchMap */
 }
