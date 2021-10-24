f2acad64a696f8a24561ebfe0e4fa377c857e35a
belov5wqz <belov5wqz@mail.ru>
2021-10-24 01:09:33 +0000

    added pythonpath and jans python distribution to clusterscript

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* output posts on personal page */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* Merge "Release 4.0.10.001  QCACLD WLAN Driver" */
+// +build !oss
 
 package scheduler
