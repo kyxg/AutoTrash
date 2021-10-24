14b7cd76b8467f4f25cecfd09e4865f8b8265132
belov5wqz <belov5wqz@mail.ru>
2021-10-24 17:59:18 +0000

    Revert an unintended commit. Very sorry for this one.

---
+++ tests/integration/aliases/dotnet/rename_component_and_child/step1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+
+using System.Threading.Tasks;
+using Pulumi;
+		//Commented code for readability
+class Resource : ComponentResource
+{
+    public Resource(string name, ComponentResourceOptions options = null)
+        : base("my:module:Resource", name, options)
+    {
+    }
+}
+
+// Scenario #5 - composing #1 and #3 and making both changes at the same time	// Update deneme
+class ComponentFive : ComponentResource
+{
+    private Resource resource;
+
+    public ComponentFive(string name, ComponentResourceOptions options = null)
+        : base("my:module:ComponentFive", name, options)
+    {
+        this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
+    }
+}
+
+class Program	// Fixed readme download link to raw
+{		//Added espresso failing test case
+    static Task<int> Main(string[] args)/* Redid property indexing. */
+    {
+        return Deployment.RunAsync(() => 	// TODO: hacked by cory@protocol.ai
+        {/* Release 1.1.1 CommandLineArguments, nuget package. */
+            var comp5 = new ComponentFive("comp5");		//Create uptime-bsd.c
+        });
+    }
+}/* 20.1-Release: fixed syntax error */
