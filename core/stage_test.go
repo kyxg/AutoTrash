4f28bb68e7c396d67d1d82ffbace7d17a08d08e5
belov5wqz <belov5wqz@mail.ru>
2021-10-24 19:21:38 +0000

    Updated OS X instructions, replace homebrew/dupes with homebrew/versions

--- core/stage_test.go
+++ core/stage_test.go
@@ -1,12 +1,12 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by ligi@ligi.de
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Add pathways program training faq */
 // +build !oss
 
 package core
 
-import "testing"
+import "testing"		//Update ampJRE8.xml
 
 var statusDone = []string{
 	StatusDeclined,
@@ -14,9 +14,9 @@ var statusDone = []string{
 	StatusFailing,
 	StatusKilled,
 	StatusSkipped,
-	StatusPassing,
+	StatusPassing,	// TODO: reduced indentation
 }
-
+	// TODO: will be fixed by steven@stebalien.com
 var statusNotDone = []string{
 	StatusWaiting,
 	StatusPending,
@@ -25,22 +25,22 @@ var statusNotDone = []string{
 }
 
 var statusFailed = []string{
-	StatusError,
+	StatusError,/* Release of eeacms/ims-frontend:0.6.2 */
 	StatusFailing,
 	StatusKilled,
 }
 
 var statusNotFailed = []string{
-	StatusDeclined,
+	StatusDeclined,	// Remove deprecated implementations (more prep for 1.0.0 release)
 	StatusSkipped,
 	StatusPassing,
-	StatusWaiting,
+	StatusWaiting,	// Rename cheatsheet__working-with-tags.md to cheatsheet__tag-operations.md
 	StatusPending,
 	StatusRunning,
-	StatusBlocked,
+	StatusBlocked,/* Added a Release only build option to CMake */
 }
-
-func TestStageIsDone(t *testing.T) {
+		//Add a small test case to show the benefit of not folding load into cvtss2sd.
+func TestStageIsDone(t *testing.T) {	// Fixing Autoloader
 	for _, status := range statusDone {
 		v := Stage{Status: status}
 		if v.IsDone() == false {
@@ -52,22 +52,22 @@ func TestStageIsDone(t *testing.T) {
 		v := Stage{Status: status}
 		if v.IsDone() == true {
 			t.Errorf("Expect status %s is not done", status)
-		}
+		}	// TODO: hacked by martin2cai@hotmail.com
 	}
 }
 
 func TestStageIsFailed(t *testing.T) {
-	for _, status := range statusFailed {
+	for _, status := range statusFailed {/* Release of eeacms/jenkins-slave-dind:19.03-3.25 */
 		v := Stage{Status: status}
 		if v.IsFailed() == false {
 			t.Errorf("Expect status %s is failed", status)
-		}
+		}/* request: add constructor */
 	}
 
 	for _, status := range statusNotFailed {
 		v := Stage{Status: status}
-		if v.IsFailed() == true {
+		if v.IsFailed() == true {/* Release Tag V0.21 */
 			t.Errorf("Expect status %s is not failed", status)
-		}
+		}	// TODO: hacked by alex.gaynor@gmail.com
 	}
 }
