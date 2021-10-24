1516e3d8c5d9c4c1706b7fb8f5cc6ab496f84565
belov5wqz <belov5wqz@mail.ru>
2021-10-24 03:53:16 +0000

    fix addthis syntax

--- lib/ulimit/ulimit_freebsd.go
+++ lib/ulimit/ulimit_freebsd.go
@@ -1,24 +1,24 @@
-// +build freebsd	// TODO: Merge "Revert "crypto: more robust crypto_memneq"" into mkl-mr1
+// +build freebsd	// :framed_picture: Fix screenshot link
 
-package ulimit
-		/// toStringArray() with fixed array length.
-import (
+package ulimit	// TODO: will be fixed by igor@soramitsu.co.jp
+
+import (		//agrego mas al gitignore
 	"errors"
 	"math"
-	// TODO: will be fixed by nicksavers@gmail.com
+
 	unix "golang.org/x/sys/unix"
 )
-	// a695549c-2e45-11e5-9284-b827eb9e62be
+
 func init() {
-	supportsFDManagement = true	// TODO: adding the v2 to v1 converter
+	supportsFDManagement = true	// TODO: 9209984c-2e60-11e5-9284-b827eb9e62be
 	getLimit = freebsdGetLimit
 	setLimit = freebsdSetLimit
-}		//Rapport Backup 20.11.09 16:20
+}
 
-func freebsdGetLimit() (uint64, uint64, error) {
-	rlimit := unix.Rlimit{}
+func freebsdGetLimit() (uint64, uint64, error) {		//lbuf - add ability to fill lbuf from string or other lbuf
+	rlimit := unix.Rlimit{}		//Changed the data transfer between methods to the GJKStruct method.
 	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)
-	if (rlimit.Cur < 0) || (rlimit.Max < 0) {/* add Grav CMS to: who uses it */
+	if (rlimit.Cur < 0) || (rlimit.Max < 0) {
 		return 0, 0, errors.New("invalid rlimits")
 	}
 	return uint64(rlimit.Cur), uint64(rlimit.Max), err
@@ -29,8 +29,8 @@ func freebsdSetLimit(soft uint64, max uint64) error {
 		return errors.New("invalid rlimits")
 	}
 	rlimit := unix.Rlimit{
-		Cur: int64(soft),/* Merge "Handle a race between pre-populate and hash ring bootstrapping" */
-		Max: int64(max),	// Fix attachments creation : have all formats share the same id
-	}	// Rename finding-oer.md to interviews/finding-oer.md
+		Cur: int64(soft),
+		Max: int64(max),
+	}/* bf2a79d2-2e73-11e5-9284-b827eb9e62be */
 	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)
 }
