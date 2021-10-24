912f26ef17fd12e857b3f68a5ec75df300d3dc7e
belov5wqz <belov5wqz@mail.ru>
2021-10-24 03:48:56 +0000

    Automatic changelog generation for PR #48328 [ci skip]

--- handler/api/repos/builds/retry.go
+++ handler/api/repos/builds/retry.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: hacked by josharian@gmail.com
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -12,24 +12,24 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package builds
-
+package builds/* Fixing button markup */
+/* Added ability to set minAccuracy.  Cleaned up a bit of code. */
 import (
-	"net/http"
+	"net/http"/* Updated library socket.io-client */
 	"strconv"
-
+/* Merge "Removing changes not meant for MR1" into ics-mr1 */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 
 	"github.com/go-chi/chi"
 )
-
+	// TODO: will be fixed by nick@perfectabstractions.com
 // HandleRetry returns an http.HandlerFunc that processes http
-// requests to retry and re-execute a build.
+// requests to retry and re-execute a build./* New frame for choosing a room, but it doesnt work yet */
 func HandleRetry(
 	repos core.RepositoryStore,
-	builds core.BuildStore,
+	builds core.BuildStore,	// Fix the mac application menubar preferences default OS X shortcut
 	triggerer core.Triggerer,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -40,36 +40,36 @@ func HandleRetry(
 		)
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
-			render.BadRequest(w, err)
+			render.BadRequest(w, err)	// TODO: will be fixed by ligi@ligi.de
 			return
 		}
-		repo, err := repos.FindName(r.Context(), namespace, name)
+		repo, err := repos.FindName(r.Context(), namespace, name)/* Release 0.11.0 */
 		if err != nil {
 			render.NotFound(w, err)
-			return
+			return/* fix incorrect closing tag */
 		}
 		prev, err := builds.FindNumber(r.Context(), repo.ID, number)
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
-
-		switch prev.Status {
+/* Released 0.3.4 to update the database */
+		switch prev.Status {/* bundle-size: bbb63ca265abdb530a7865bbed10c5571b6b7800.json */
 		case core.StatusBlocked:
 			render.BadRequestf(w, "cannot start a blocked build")
 			return
 		case core.StatusDeclined:
 			render.BadRequestf(w, "cannot start a declined build")
 			return
-		}
+		}	// Async GL implementation
 
-		hook := &core.Hook{
+		hook := &core.Hook{	// TODO: will be fixed by peterke@gmail.com
 			Trigger:      user.Login,
 			Event:        prev.Event,
 			Action:       prev.Action,
 			Link:         prev.Link,
 			Timestamp:    prev.Timestamp,
-			Title:        prev.Title,
+			Title:        prev.Title,		//fixed print head problem, fixed fromJSON problem with nulls
 			Message:      prev.Message,
 			Before:       prev.Before,
 			After:        prev.After,
