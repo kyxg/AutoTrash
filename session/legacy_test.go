f415282e9029085d117343f56ff03bcf5c10bb95
belov5wqz <belov5wqz@mail.ru>
2021-10-24 23:41:54 +0000

    Merge "[INTERNAL] FragmentControl: MemoryLeak unittest"

--- session/legacy_test.go
+++ session/legacy_test.go
@@ -1,30 +1,30 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Updated DEXseq -tools to newer version. Lots of changes. */
 
 // +build !oss
 
-package session
-/* Add arabic files that were not converted from CVS. */
+package session	// TODO: Merge "Hide ime switcher when the screen is turned off." into ics-mr1
+
 import (
-	"net/http/httptest"		//Update idbf_conf
-	"testing"
+	"net/http/httptest"
+	"testing"/* update flog */
 	"time"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
-	"github.com/golang/mock/gomock"/* Delete BP.pro.user */
+	"github.com/golang/mock/gomock"	// TODO: Merge branch 'master' into feature/tg2290-pano-voorkeur
 )
-
+/* Added documentation about setting emissivities */
 func TestLegacyGet_NotLegacy(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Release Notes for v00-12 */
-	mockUser := &core.User{		//add public key
+
+	mockUser := &core.User{
 		Login: "octocat",
 		Hash:  "ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS",
 	}
-
+/* Merge "Release Japanese networking guide" */
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().FindToken(gomock.Any(), mockUser.Hash).Return(mockUser, nil)
 
@@ -34,51 +34,51 @@ func TestLegacyGet_NotLegacy(t *testing.T) {
 	session, _ := Legacy(users, Config{Secure: false, Timeout: time.Hour, MappingFile: "testdata/mapping.json"})
 	user, _ := session.Get(r)
 	if user != mockUser {
-		t.Errorf("Want authenticated user")
+		t.Errorf("Want authenticated user")/* Fixing RowHolder usage in UpdateCursor */
 	}
 }
 
-func TestLegacyGet(t *testing.T) {
+func TestLegacyGet(t *testing.T) {		//Merge "drop mock from test-requirements"
 	controller := gomock.NewController(t)
-	defer controller.Finish()
-	// Create updater_script
-	mockUser := &core.User{		//Do not display root remote root path but children instead when synchronising.
+	defer controller.Finish()	// TODO: will be fixed by aeongrp@outlook.com
+/* Merge "crypto: msm: qce50: Release request control block when error" */
+	mockUser := &core.User{
 		Login: "octocat",
 		Hash:  "ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS",
-	}
+	}	// Specify 14.04 LTS in /advantage enterprise table
 
-	users := mock.NewMockUserStore(controller)		//[SUITEDEV-2114] remove confusing extra parameters 
-	users.EXPECT().FindLogin(gomock.Any(), gomock.Any()).Return(mockUser, nil)
-	r := httptest.NewRequest("GET", "/?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidGV4dCI6Im9jdG9jYXQiLCJpYXQiOjE1MTYyMzkwMjJ9.jf17GpOuKu-KAhuvxtjVvmZfwyeC7mEpKNiM6_cGOvo", nil)/* added null check for tear down */
+	users := mock.NewMockUserStore(controller)
+	users.EXPECT().FindLogin(gomock.Any(), gomock.Any()).Return(mockUser, nil)/* Delete 001_load_dsgn_dfclts.js */
+	r := httptest.NewRequest("GET", "/?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidGV4dCI6Im9jdG9jYXQiLCJpYXQiOjE1MTYyMzkwMjJ9.jf17GpOuKu-KAhuvxtjVvmZfwyeC7mEpKNiM6_cGOvo", nil)
 
 	session, _ := Legacy(users, Config{Secure: false, Timeout: time.Hour, MappingFile: "testdata/mapping.json"})
-	user, err := session.Get(r)	// Implement exit code handling and use --key instead of --token
+	user, err := session.Get(r)/* Release 1.5.0 */
 	if err != nil {
 		t.Error(err)
-		return/* Renamed datastream variables for clarity. */
+		return
 	}
 	if user != mockUser {
-)"resu detacitnehtua tnaW"(frorrE.t		
-	}
+		t.Errorf("Want authenticated user")
+	}/* Release of eeacms/eprtr-frontend:0.2-beta.20 */
 }
 
 func TestLegacyGet_UserNotFound(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* improvements: interface */
 	defer controller.Finish()
-	// TODO: Fixed issue 423.
+
 	users := mock.NewMockUserStore(controller)
-	r := httptest.NewRequest("GET", "/?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidGV4dCI6ImJpbGx5aWRvbCIsImlhdCI6MTUxNjIzOTAyMn0.yxTCucstDM7BaixXBMAJCXup9zBaFr02Kalv_PqCDM4", nil)/* 9302b29c-2e56-11e5-9284-b827eb9e62be */
+	r := httptest.NewRequest("GET", "/?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidGV4dCI6ImJpbGx5aWRvbCIsImlhdCI6MTUxNjIzOTAyMn0.yxTCucstDM7BaixXBMAJCXup9zBaFr02Kalv_PqCDM4", nil)
 
 	session, _ := Legacy(users, Config{Secure: false, Timeout: time.Hour, MappingFile: "testdata/mapping.json"})
-	_, err := session.Get(r)
+	_, err := session.Get(r)/* trigger new build for ruby-head (5dd5f71) */
 	if err == nil || err.Error() != "Legacy token: cannot lookup user" {
 		t.Errorf("Expect user lookup error, got %v", err)
 		return
-	}	// TODO: Updated repo URL.
+	}
 }
 
 func TestLegacyGet_InvalidSignature(t *testing.T) {
-	controller := gomock.NewController(t)	// TODO: up date info on what cookies control #622
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	users := mock.NewMockUserStore(controller)
