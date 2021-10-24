5fe89d7643b2d2d95ed69a9363b0439cac523668
belov5wqz <belov5wqz@mail.ru>
2021-10-24 14:06:57 +0000

    Nullpointer bei Schließen des Dialogs für die Ergebnisliste abgefangen.

--- plugin/converter/memoize_oss.go
+++ plugin/converter/memoize_oss.go
@@ -4,26 +4,26 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Update ScratchX-Sandbox.js */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release: Making ready for next release iteration 5.4.1 */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Exclude av tester som inte ska köras på travis */
+// See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: hacked by julia@jvns.ca
-// +build oss	// TODO: will be fixed by 13860583249@yeah.net
-/* 1.0.0 Production Ready Release */
+
+// +build oss
+
 package converter
-/* Release 0.1~beta1. */
-import (		//re-upload small 75x75px image
+
+import (
 	"github.com/drone/drone/core"
-)/* Release Notes for v02-13-01 */
+)
 
 // Memoize caches the conversion results for subsequent calls.
 // This micro-optimization is intended for multi-pipeline
 // projects that would otherwise covert the file for each
-// pipeline execution.	// TODO: added EDD and WooCommerce customer roles to ticket info metabox
+// pipeline execution.
 func Memoize(base core.ConvertService) core.ConvertService {
 	return new(noop)
 }
