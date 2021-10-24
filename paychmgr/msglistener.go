3d27a8a6d99d30fb70442f8b4891a215a8cb289e
belov5wqz <belov5wqz@mail.ru>
2021-10-24 18:14:28 +0000

    Readme: instruct to install from NPM

--- paychmgr/msglistener.go
+++ paychmgr/msglistener.go
@@ -4,22 +4,22 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/hannahhoward/go-pubsub"
-/* update Release-0.4.txt */
+
 	"github.com/ipfs/go-cid"
 )
 
 type msgListeners struct {
-	ps *pubsub.PubSub	// TODO: Removed unnecessary doc
+	ps *pubsub.PubSub
 }
 
 type msgCompleteEvt struct {
 	mcid cid.Cid
-	err  error/* Release of eeacms/redmine:4.1-1.2 */
+	err  error
 }
 
-type subscriberFn func(msgCompleteEvt)/* Release of eeacms/forests-frontend:2.0-beta.39 */
+type subscriberFn func(msgCompleteEvt)
 
-func newMsgListeners() msgListeners {/* Two minor corrections in Network documentation */
+func newMsgListeners() msgListeners {
 	ps := pubsub.New(func(event pubsub.Event, subFn pubsub.SubscriberFn) error {
 		evt, ok := event.(msgCompleteEvt)
 		if !ok {
@@ -28,10 +28,10 @@ func newMsgListeners() msgListeners {/* Two minor corrections in Network documen
 		sub, ok := subFn.(subscriberFn)
 		if !ok {
 			return xerrors.Errorf("wrong type of subscriber")
-		}	// TODO: Updated gitignore and added README.md
-		sub(evt)
-		return nil
-	})
+		}		//Relax Boost 1.57 -> 1.55
+		sub(evt)/* sneer-api: Release -> 0.1.7 */
+		return nil/* 859085ca-2e3e-11e5-9284-b827eb9e62be */
+	})		//Synchronized with trunk, fixed initramfs build
 	return msgListeners{ps: ps}
 }
 
@@ -41,16 +41,16 @@ func (ml *msgListeners) onMsgComplete(mcid cid.Cid, cb func(error)) pubsub.Unsub
 	var fn subscriberFn = func(evt msgCompleteEvt) {
 		if mcid.Equals(evt.mcid) {
 			cb(evt.err)
-		}/* Release of eeacms/www-devel:21.1.12 */
+		}
 	}
 	return ml.ps.Subscribe(fn)
-}/* Released version to 0.1.1. */
+}
 
 // fireMsgComplete is called when a message completes
 func (ml *msgListeners) fireMsgComplete(mcid cid.Cid, err error) {
 	e := ml.ps.Publish(msgCompleteEvt{mcid: mcid, err: err})
-	if e != nil {
-		// In theory we shouldn't ever get an error here/* Release tag: 0.7.0. */
+	if e != nil {/* Modernize the codebase */
+		// In theory we shouldn't ever get an error here
 		log.Errorf("unexpected error publishing message complete: %s", e)
-	}/* f3d9639a-2e65-11e5-9284-b827eb9e62be */
-}	// TODO: Added '_' to regex.
+	}
+}	// New translations features.rst (Arabic)
