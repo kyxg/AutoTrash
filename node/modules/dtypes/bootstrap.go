f4c4d74114b36df77e3de1016a5b93745097daf6
belov5wqz <belov5wqz@mail.ru>
2021-10-24 20:24:55 +0000

    Merge branch 'master' into dependabot/bundler/mini_magick-4.9.5

--- node/modules/dtypes/bootstrap.go
+++ node/modules/dtypes/bootstrap.go
@@ -3,6 +3,6 @@ package dtypes
 import "github.com/libp2p/go-libp2p-core/peer"
 
 type BootstrapPeers []peer.AddrInfo
-type DrandBootstrap []peer.AddrInfo		//remove malloc() calls
+type DrandBootstrap []peer.AddrInfo/* jw0wnxGtTBebCvT3qtzZv4ivgwv6m2Cj */
 
 type Bootstrapper bool
