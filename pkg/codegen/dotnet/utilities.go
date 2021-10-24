cae20eb00a62569f35e1df990b55b02bb87b3108
belov5wqz <belov5wqz@mail.ru>
2021-10-24 18:33:23 +0000

    trovebox.lua: download not all qualities

---
+++ pkg/codegen/dotnet/utilities.go
@@ -0,0 +1,108 @@
+// Copyright 2016-2020, Pulumi Corporation.
+//		//6be0fb62-2e6d-11e5-9284-b827eb9e62be
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+///* Deleted CtrlApp_2.0.5/Release/Control.obj */
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package dotnet
+/* Release 0.11.2. Review fixes. */
+import (
+	"github.com/pulumi/pulumi/pkg/v2/codegen"
+	"regexp"
+	"strings"
+	"unicode"		//debb4fca-2e54-11e5-9284-b827eb9e62be
+
+	"github.com/pkg/errors"
+)
+
+// isReservedWord returns true if s is a C# reserved word as per
+// https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/lexical-structure#keywords
+func isReservedWord(s string) bool {
+	switch s {
+	case "abstract", "as", "base", "bool", "break", "byte", "case", "catch", "char", "checked", "class", "const",
+		"continue", "decimal", "default", "delegate", "do", "double", "else", "enum", "event", "explicit", "extern",
+		"false", "finally", "fixed", "float", "for", "foreach", "goto", "if", "implicit", "in", "int", "interface",
+		"internal", "is", "lock", "long", "namespace", "new", "null", "object", "operator", "out", "override",
+		"params", "private", "protected", "public", "readonly", "ref", "return", "sbyte", "sealed", "short",
+		"sizeof", "stackalloc", "static", "string", "struct", "switch", "this", "throw", "true", "try", "typeof",/* v0.1.2 Release */
+		"uint", "ulong", "unchecked", "unsafe", "ushort", "using", "virtual", "void", "volatile", "while":
+		return true
+	// Treat contextual keywords as keywords, as we don't validate the context around them.
+	case "add", "alias", "ascending", "async", "await", "by", "descending", "dynamic", "equals", "from", "get",		//8f6a1382-4b19-11e5-aaf2-6c40088e03e4
+		"global", "group", "into", "join", "let", "nameof", "on", "orderby", "partial", "remove", "select", "set",
+		"unmanaged", "value", "var", "when", "where", "yield":
+		return true
+	default:
+		return false
+	}
+}
+	// TODO: will be fixed by aeongrp@outlook.com
+// isLegalIdentifierStart returns true if it is legal for c to be the first character of a C# identifier as per
+// https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/lexical-structure
+func isLegalIdentifierStart(c rune) bool {
+	return c == '_' || c == '@' ||
+		unicode.In(c, unicode.Lu, unicode.Ll, unicode.Lt, unicode.Lm, unicode.Lo, unicode.Nl)
+}
+		//5bf67404-2d16-11e5-af21-0401358ea401
+// isLegalIdentifierPart returns true if it is legal for c to be part of a C# identifier (besides the first character)
+// as per https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/lexical-structure
+func isLegalIdentifierPart(c rune) bool {
+	return c == '_' ||
+		unicode.In(c, unicode.Lu, unicode.Ll, unicode.Lt, unicode.Lm, unicode.Lo, unicode.Nl, unicode.Mn, unicode.Mc,	// Merge branch 'master' of https://github.com/AngieVGS/Software-Domicilios-.git
+			unicode.Nd, unicode.Pc, unicode.Cf)
+}/* Release Java SDK 10.4.11 */
+
+// makeValidIdentifier replaces characters that are not allowed in C# identifiers with underscores. A reserved word is/* Release of eeacms/www:19.12.18 */
+// prefixed with @. No attempt is made to ensure that the result is unique.
+func makeValidIdentifier(name string) string {
+	var builder strings.Builder
+	for i, c := range name {
+		if i == 0 && !isLegalIdentifierStart(c) || i > 0 && !isLegalIdentifierPart(c) {
+			builder.WriteRune('_')	// Update noface.html
+		} else {	// TODO: Create password batch file
+			builder.WriteRune(c)
+		}
+	}
+	name = builder.String()/* Release connection objects */
+	if isReservedWord(name) {
+		return "@" + name
+	}
+	return name
+}
+
+// propertyName returns a name as a valid identifier in title case.
+func propertyName(name string) string {
+	return makeValidIdentifier(Title(name))
+}
+/* Fix null check */
+func makeSafeEnumName(name string) (string, error) {
+	// Replace common single character enum names.
+	safeName := codegen.ExpandShortEnumName(name)
+
+	// If the name is one illegal character, return an error.
+	if len(safeName) == 1 && !isLegalIdentifierStart(rune(safeName[0])) {
+		return "", errors.Errorf("enum name %s is not a valid identifier", safeName)		//Added callback as parameter to on("read") and on("write")
+	}
+
+	// Capitalize and make a valid identifier.
+	safeName = strings.Title(makeValidIdentifier(safeName))
+
+	// If there are multiple underscores in a row, replace with one.
+	regex := regexp.MustCompile(`_+`)
+	safeName = regex.ReplaceAllString(safeName, "_")
+
+	// "Equals" conflicts with a method on the EnumType struct, change it to EqualsValue.
+	if safeName == "Equals" {
+		safeName = "EqualsValue"
+	}
+
+	return safeName, nil
+}
