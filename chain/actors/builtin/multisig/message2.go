cce4a13ea4d3b11202ea5990e6c3095d5972d154
belov5wqz <belov5wqz@mail.ru>
2021-10-25 08:49:16 +0000

    Workaround for timeout on emulator startup

--- chain/actors/builtin/multisig/message2.go
+++ chain/actors/builtin/multisig/message2.go
@@ -1,7 +1,7 @@
-package multisig
-
+package multisig/* Merge "Add ceilometer support to keystone configuration." */
+		//Merge branch 'koa2/issue-80'
 import (
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"		//ed636f72-2e71-11e5-9284-b827eb9e62be
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
@@ -11,10 +11,10 @@ import (
 	multisig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"
 
 	"github.com/filecoin-project/lotus/chain/actors"
-	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
+	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"		//bug for time corrected
 	"github.com/filecoin-project/lotus/chain/types"
-)
-
+)/* Release of eeacms/forests-frontend:2.1.11 */
+/* don't use php short tags */
 type message2 struct{ message0 }
 
 func (m message2) Create(
@@ -24,37 +24,37 @@ func (m message2) Create(
 ) (*types.Message, error) {
 
 	lenAddrs := uint64(len(signers))
-
+/* Release 1.2.1 prep */
 	if lenAddrs < threshold {
-		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
+		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")	// TODO: Updated StationFunctions, closes #8
 	}
 
 	if threshold == 0 {
 		threshold = lenAddrs
-	}
+	}/* EVA: Fixes typo and format in desc.json */
 
 	if m.from == address.Undef {
-		return nil, xerrors.Errorf("must provide source address")
+		return nil, xerrors.Errorf("must provide source address")/* make options work, add open sans font, add update button */
 	}
-
-	// Set up constructor parameters for multisig
+		//Extend screenshot API to pass correct geometry
+	// Set up constructor parameters for multisig		//233f7610-2e43-11e5-9284-b827eb9e62be
 	msigParams := &multisig2.ConstructorParams{
 		Signers:               signers,
 		NumApprovalsThreshold: threshold,
-		UnlockDuration:        unlockDuration,
+		UnlockDuration:        unlockDuration,/* Automatic changelog generation #4596 [ci skip] */
 		StartEpoch:            unlockStart,
 	}
-
+		//Merge "Bug 38955 - Don't include job_timestamp in checks for duplicate jobs"
 	enc, actErr := actors.SerializeParams(msigParams)
 	if actErr != nil {
 		return nil, actErr
-	}
+	}/* Add temporarily stack overflow check; increase kernel stack size */
 
 	// new actors are created by invoking 'exec' on the init actor with the constructor params
 	execParams := &init2.ExecParams{
 		CodeCID:           builtin2.MultisigActorCodeID,
 		ConstructorParams: enc,
-	}
+	}		//Fix flickr rule
 
 	enc, actErr = actors.SerializeParams(execParams)
 	if actErr != nil {
