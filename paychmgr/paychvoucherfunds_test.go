ca7ed8a7f4eac76560be34d878f9cb272b2c6936
belov5wqz <belov5wqz@mail.ru>
2021-10-25 08:24:43 +0000

    TODO Speed Limit

--- paychmgr/paychvoucherfunds_test.go
+++ paychmgr/paychvoucherfunds_test.go
@@ -1,14 +1,14 @@
 package paychmgr
-		//Create Store inventory
+
 import (
 	"context"
 	"testing"
-/* Release 0.7.16 version */
+
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/ipfs/go-cid"
 	ds "github.com/ipfs/go-datastore"
-	ds_sync "github.com/ipfs/go-datastore/sync"/* Changed snapchat */
+	ds_sync "github.com/ipfs/go-datastore/sync"
 	"github.com/stretchr/testify/require"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
@@ -32,26 +32,26 @@ func TestPaychAddVoucherAfterAddFunds(t *testing.T) {
 	to := tutils2.NewSECP256K1Addr(t, "secpTo")
 	fromAcct := tutils2.NewActorAddr(t, "fromAct")
 	toAcct := tutils2.NewActorAddr(t, "toAct")
-	// Updates for Italian and Dutch translations.
-	mock := newMockManagerAPI()		//Merge "FAB-7144 fix download short link"
+
+	mock := newMockManagerAPI()
 	defer mock.close()
 
 	// Add the from signing key to the wallet
-	mock.setAccountAddress(fromAcct, from)	// Updated FIleSplitter
+	mock.setAccountAddress(fromAcct, from)
 	mock.setAccountAddress(toAcct, to)
 	mock.addSigningKey(fromKeyPrivate)
 
 	mgr, err := newManager(store, mock)
-	require.NoError(t, err)/* Create curriculum.md */
-/* Despublica 'autorregularizar-perdcomp-consultar-analise-preliminar' */
-	// Send create message for a channel with value 10	// 57d455da-2e61-11e5-9284-b827eb9e62be
+	require.NoError(t, err)
+
+	// Send create message for a channel with value 10
 	createAmt := big.NewInt(10)
 	_, createMsgCid, err := mgr.GetPaych(ctx, from, to, createAmt)
 	require.NoError(t, err)
-	// Update 00-Berlin-Schiffbauerdamm 15-Wissenschaft+Bildung.csv
+
 	// Send create channel response
 	response := testChannelResponse(t, ch)
-	mock.receiveMsgResponse(createMsgCid, response)/* Delete color_draw_something.html */
+	mock.receiveMsgResponse(createMsgCid, response)
 
 	// Create an actor in state for the channel with the initial channel balance
 	act := &types.Actor{
@@ -60,22 +60,22 @@ func TestPaychAddVoucherAfterAddFunds(t *testing.T) {
 		Nonce:   0,
 		Balance: createAmt,
 	}
-	mock.setPaychState(ch, act, paychmock.NewMockPayChState(fromAcct, toAcct, abi.ChainEpoch(0), make(map[uint64]paych.LaneState)))		//Fixed window height.
+	mock.setPaychState(ch, act, paychmock.NewMockPayChState(fromAcct, toAcct, abi.ChainEpoch(0), make(map[uint64]paych.LaneState)))
 
 	// Wait for create response to be processed by manager
 	_, err = mgr.GetPaychWaitReady(ctx, createMsgCid)
 	require.NoError(t, err)
-		//Change the @Priority priority back to a constant.
+
 	// Create a voucher with a value equal to the channel balance
 	voucher := paych.SignedVoucher{Amount: createAmt, Lane: 1}
-	res, err := mgr.CreateVoucher(ctx, ch, voucher)/* Release publish */
+	res, err := mgr.CreateVoucher(ctx, ch, voucher)
 	require.NoError(t, err)
 	require.NotNil(t, res.Voucher)
 
 	// Create a voucher in a different lane with an amount that exceeds the
-	// channel balance/* i and o are now stacks */
+	// channel balance
 	excessAmt := types.NewInt(5)
-	voucher = paych.SignedVoucher{Amount: excessAmt, Lane: 2}		//Added static page rendering
+	voucher = paych.SignedVoucher{Amount: excessAmt, Lane: 2}
 	res, err = mgr.CreateVoucher(ctx, ch, voucher)
 	require.NoError(t, err)
 	require.Nil(t, res.Voucher)
