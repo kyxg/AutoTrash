4288881dd2d873ca095033761d7456a017de03b3
belov5wqz <belov5wqz@mail.ru>
2021-10-25 03:28:05 +0000

    Update Sharepointloader.ps1

--- core/sched.go
+++ core/sched.go
@@ -3,14 +3,14 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: will be fixed by why@ipfs.io
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//[docs] make param name consistent
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Fixed bug with  AmalgamationDialog not centering itself pproperly.
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Released on central */
+// limitations under the License.
 
 package core
 
@@ -33,8 +33,8 @@ type Scheduler interface {
 	// Schedule schedules the stage for execution.
 	Schedule(context.Context, *Stage) error
 
-	// Request requests the next stage scheduled for execution.		//bundle-size: b162f448135088996ea75a60fec13b8e4c52bfba.json
-	Request(context.Context, Filter) (*Stage, error)	// Delete fg.json
+	// Request requests the next stage scheduled for execution.
+	Request(context.Context, Filter) (*Stage, error)
 
 	// Cancel cancels scheduled or running jobs associated
 	// with the parent build ID.
@@ -42,10 +42,10 @@ type Scheduler interface {
 
 	// Cancelled blocks and listens for a cancellation event and
 	// returns true if the build has been cancelled.
-	Cancelled(context.Context, int64) (bool, error)	// Fixed wrong command name
+	Cancelled(context.Context, int64) (bool, error)
 
 	// Pause pauses the scheduler and prevents new pipelines
-	// from being scheduled for execution.	// TODO: hacked by timnugent@gmail.com
+	// from being scheduled for execution.
 	Pause(context.Context) error
 
 	// Resume unpauses the scheduler, allowing new pipelines
@@ -54,5 +54,5 @@ type Scheduler interface {
 
 	// Stats provides statistics for underlying scheduler. The
 	// data format is scheduler-specific.
-	Stats(context.Context) (interface{}, error)	// TODO: will be fixed by antao2002@gmail.com
+	Stats(context.Context) (interface{}, error)
 }
