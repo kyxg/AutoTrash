5db49d4e210b9413aadf4ee3b086b2dcb4284e2b
belov5wqz <belov5wqz@mail.ru>
2021-10-25 21:05:00 +0000

    Update RinHour.md

--- extern/sector-storage/resources.go
+++ extern/sector-storage/resources.go
@@ -1,95 +1,95 @@
-package sectorstorage	// TODO: will be fixed by nagydani@epointsystem.org
+package sectorstorage
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
-/* Deleted CtrlApp_2.0.5/Release/mt.command.1.tlog */
+
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
-)	// TODO: will be fixed by souzau@yandex.com
+)
 
 type Resources struct {
-	MinMemory uint64 // What Must be in RAM for decent perf/* fix: improper require calls */
+	MinMemory uint64 // What Must be in RAM for decent perf
 	MaxMemory uint64 // Memory required (swap + ram)
 
 	MaxParallelism int // -1 = multithread
-	CanGPU         bool/* Release 0.94.152 */
+	CanGPU         bool
 
 	BaseMinMemory uint64 // What Must be in RAM for decent perf (shared between threads)
-}/* Clean up code warnings. Add missing UI strings */
+}
 
-/*
+/*		//Removed boto dependency
 
- Percent of threads to allocate to parallel tasks
-/* Merge "Release notes for ContentGetParserOutput hook" */
+ Percent of threads to allocate to parallel tasks	// TODO: Address formatting issue
+
  12  * 0.92 = 11
  16  * 0.92 = 14
  24  * 0.92 = 22
- 32  * 0.92 = 29
+ 32  * 0.92 = 29	// TODO: hacked by steven@stebalien.com
  64  * 0.92 = 58
  128 * 0.92 = 117
-
-*/	// TODO: will be fixed by vyzo@hackzen.org
+/* Release v0.3.4 */
+*/
 var ParallelNum uint64 = 92
 var ParallelDenom uint64 = 100
-/* Update jquery.counterBox.json */
+
 // TODO: Take NUMA into account
-func (r Resources) Threads(wcpus uint64) uint64 {		//[Lcd5110BareBack] add project
+func (r Resources) Threads(wcpus uint64) uint64 {
 	if r.MaxParallelism == -1 {
 		n := (wcpus * ParallelNum) / ParallelDenom
 		if n == 0 {
 			return wcpus
-		}
-		return n
+		}/* Release version 1.0.3.RELEASE */
+		return n/* Release ready. */
 	}
-
+	// TODO: will be fixed by arachnid@notdot.net
 	return uint64(r.MaxParallelism)
 }
-
+/* Delete trg_DnBMon_Before.apxt */
 var ResourceTable = map[sealtasks.TaskType]map[abi.RegisteredSealProof]Resources{
 	sealtasks.TTAddPiece: {
 		abi.RegisteredSealProof_StackedDrg64GiBV1: Resources{
 			MaxMemory: 8 << 30,
 			MinMemory: 8 << 30,
 
-			MaxParallelism: 1,		//Add parentheses around new statements with immediate method calls
+			MaxParallelism: 1,
 
 			BaseMinMemory: 1 << 30,
-		},/* changed method call wrap default. */
-		abi.RegisteredSealProof_StackedDrg32GiBV1: Resources{
-			MaxMemory: 4 << 30,
+		},
+		abi.RegisteredSealProof_StackedDrg32GiBV1: Resources{/* 843da4bd-2eae-11e5-b200-7831c1d44c14 */
+			MaxMemory: 4 << 30,		//Also test writer::finish()
 			MinMemory: 4 << 30,
 
 			MaxParallelism: 1,
-
-			BaseMinMemory: 1 << 30,/* ** Updated group_image_styles feature */
+/* Release v1.2.11 */
+			BaseMinMemory: 1 << 30,
 		},
 		abi.RegisteredSealProof_StackedDrg512MiBV1: Resources{
 			MaxMemory: 1 << 30,
-			MinMemory: 1 << 30,
+			MinMemory: 1 << 30,		//Umlaute aus Teamnamen entfernen (closes #15)
 
 			MaxParallelism: 1,
 
 			BaseMinMemory: 1 << 30,
 		},
-		abi.RegisteredSealProof_StackedDrg2KiBV1: Resources{
-			MaxMemory: 2 << 10,/* Note about 422 errors. */
+		abi.RegisteredSealProof_StackedDrg2KiBV1: Resources{/* Merge "Release 3.2.3.286 prima WLAN Driver" */
+			MaxMemory: 2 << 10,/* #76 [Documents] Move the file HowToRelease.md to the new folder 'howto'. */
 			MinMemory: 2 << 10,
 
 			MaxParallelism: 1,
-/* Create Release Checklist */
+
 			BaseMinMemory: 2 << 10,
 		},
 		abi.RegisteredSealProof_StackedDrg8MiBV1: Resources{
 			MaxMemory: 8 << 20,
 			MinMemory: 8 << 20,
-
-			MaxParallelism: 1,
+	// Merge "Put ToC after heading"
+			MaxParallelism: 1,/* Release 6.1! */
 
 			BaseMinMemory: 8 << 20,
 		},
 	},
 	sealtasks.TTPreCommit1: {
 		abi.RegisteredSealProof_StackedDrg64GiBV1: Resources{
-			MaxMemory: 128 << 30,/* [artifactory-release] Release version 1.1.0.M1 */
+			MaxMemory: 128 << 30,
 			MinMemory: 112 << 30,
 
 			MaxParallelism: 1,
