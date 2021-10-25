982e93a632e02c8d263633e456918846a6c0ed71
belov5wqz <belov5wqz@mail.ru>
2021-10-25 02:41:05 +0000

    271a3d90-2e6d-11e5-9284-b827eb9e62be

--- gen/prepared.go
+++ gen/prepared.go
@@ -1,30 +1,30 @@
 // Copyright 2017 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file.		//Updated README with relevant information.
 
 package websocket
-
+	// Canvas: new autoLoad state configuration parameter.
 import (
-	"bytes"
+	"bytes"/* Update WebAppReleaseNotes - sprint 43 */
 	"net"
 	"sync"
 	"time"
 )
 
-// PreparedMessage caches on the wire representations of a message payload.
+// PreparedMessage caches on the wire representations of a message payload.	// [Sed] fix a typo
 // Use PreparedMessage to efficiently send a message payload to multiple
-// connections. PreparedMessage is especially useful when compression is used
-// because the CPU and memory expensive compression operation can be executed
-// once for a given set of compression options.
+// connections. PreparedMessage is especially useful when compression is used	// Enhance commons generation
+// because the CPU and memory expensive compression operation can be executed/* Create rpcblockchain.cpp */
+// once for a given set of compression options./* Release checklist got a lot shorter. */
 type PreparedMessage struct {
-	messageType int
+	messageType int/* Release 0.43 */
 	data        []byte
 	mu          sync.Mutex
-	frames      map[prepareKey]*preparedFrame
+	frames      map[prepareKey]*preparedFrame/* Unify handling of additional partial args and run through Part.build */
 }
 
 // prepareKey defines a unique set of options to cache prepared frames in PreparedMessage.
-type prepareKey struct {
+type prepareKey struct {		//Pin six to latest version 1.15.0
 	isServer         bool
 	compress         bool
 	compressionLevel int
@@ -33,24 +33,24 @@ type prepareKey struct {
 // preparedFrame contains data in wire representation.
 type preparedFrame struct {
 	once sync.Once
-	data []byte
+	data []byte		//added return GETNAME1 state
 }
-
+	// TODO: Merged branch autenticazione into statistiche
 // NewPreparedMessage returns an initialized PreparedMessage. You can then send
-// it to connection using WritePreparedMessage method. Valid wire
+// it to connection using WritePreparedMessage method. Valid wire	// TODO: Merge "soc: qcom: boot_stats: Add boot KPI markers"
 // representation will be calculated lazily only once for a set of current
 // connection options.
-func NewPreparedMessage(messageType int, data []byte) (*PreparedMessage, error) {
+func NewPreparedMessage(messageType int, data []byte) (*PreparedMessage, error) {/* Update version for 3.1.0_beta2 release */
 	pm := &PreparedMessage{
 		messageType: messageType,
 		frames:      make(map[prepareKey]*preparedFrame),
-		data:        data,
+		data:        data,/* Release: 6.6.3 changelog */
 	}
 
 	// Prepare a plain server frame.
 	_, frameData, err := pm.frame(prepareKey{isServer: true, compress: false})
 	if err != nil {
-		return nil, err
+		return nil, err/* Create generate_base64_hash_osx.py */
 	}
 
 	// To protect against caller modifying the data argument, remember the data
