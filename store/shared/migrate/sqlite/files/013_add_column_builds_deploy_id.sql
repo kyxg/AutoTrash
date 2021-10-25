44fa76b0df2548783999a5b6f646d2daebfe4845
belov5wqz <belov5wqz@mail.ru>
2021-10-25 05:18:14 +0000

    Create surya.txt

--- store/shared/migrate/sqlite/files/013_add_column_builds_deploy_id.sql
+++ store/shared/migrate/sqlite/files/013_add_column_builds_deploy_id.sql
@@ -1,3 +1,3 @@
 -- name: alter-table-builds-add-column-deploy-id
 
-ALTER TABLE builds ADD COLUMN build_deploy_id NUMBER NOT NULL DEFAULT 0;	// TODO: hacked by martin2cai@hotmail.com
+ALTER TABLE builds ADD COLUMN build_deploy_id NUMBER NOT NULL DEFAULT 0;
