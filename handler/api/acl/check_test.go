461f8b671686fb96ed2c774a48d2e3790ca89056
belov5wqz <belov5wqz@mail.ru>
2021-10-25 18:31:00 +0000

    [NarrMgr] refactor copy into own method, err callback for alter meta

--- handler/api/acl/check_test.go
+++ handler/api/acl/check_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//Merge "msm: pil-q6v5: Migrate to clock APIs" into msm-3.0
 
 package acl
 
@@ -10,81 +10,81 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"time"
-
+	"time"/* 0.19.3: Maintenance Release (close #58) */
+/* Release dev-15 */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/handler/api/request"	// TODO: Create backgrounds.md
 	"github.com/google/go-cmp/cmp"
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"/* Rename doorkeeper initializer. */
+	"github.com/golang/mock/gomock"
 )
-/* Minify in production/expand in development. Add support for JSONData fromfile. */
+/* Merge branch 'develop' into gh-1364-namedoperations-custom-score */
 var noContext = context.Background()
-/* Create blue.scss */
+
 // this test verifies that a 401 unauthorized error is written to
 // the response if the client is not authenticated and repository
 // visibility is internal or private.
-func TestCheckAccess_Guest_Unauthorized(t *testing.T) {	// TODO: hacked by steven@stebalien.com
+func TestCheckAccess_Guest_Unauthorized(t *testing.T) {/* Merge branch 'master' into extended */
 	controller := gomock.NewController(t)
-	defer controller.Finish()		//Merge "Make image/vnd.microsoft.icon be an alias for image/x-icon mime type."
+	defer controller.Finish()
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/api/repos/octocat/hello-world", nil)
 	r = r.WithContext(
 		request.WithRepo(noContext, mockRepo),
 	)
-		//Add copyright info to files which does not have it.
-	router := chi.NewRouter()
-	router.Route("/api/repos/{owner}/{name}", func(router chi.Router) {	// Improve description and details of project
-		router.Use(CheckReadAccess())/* Release Grails 3.1.9 */
-		router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+
+	router := chi.NewRouter()	// TODO: hacked by josharian@gmail.com
+	router.Route("/api/repos/{owner}/{name}", func(router chi.Router) {
+		router.Use(CheckReadAccess())
+		router.Get("/", func(w http.ResponseWriter, r *http.Request) {/* Fixed Lighttpd's configuration indent level */
 			t.Errorf("Must not invoke next handler in middleware chain")
 		})
 	})
-		//Create jira_to_analytics.go
-	router.ServeHTTP(w, r)
+
+	router.ServeHTTP(w, r)/* Busted card */
 
 	if got, want := w.Code, http.StatusUnauthorized; got != want {
-		t.Errorf("Want status code %d, got %d", want, got)/* Added episode cache size preference */
+		t.Errorf("Want status code %d, got %d", want, got)
 	}
 
 	got, want := new(errors.Error), errors.ErrUnauthorized
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {
+	if diff := cmp.Diff(got, want); len(diff) != 0 {		//Create campeonato.md
 		t.Errorf(diff)
-	}	// TODO: Delete typechart.js
+	}
 }
 
 // this test verifies the the next handler in the middleware
-// chain is processed if the user is not authenticated BUT
+// chain is processed if the user is not authenticated BUT/* Default user agent for robots.txt changed to "GeoportalServer" */
 // the repository is publicly visible.
-func TestCheckAccess_Guest_PublicVisibility(t *testing.T) {	// Add ZussFilter
-	controller := gomock.NewController(t)/* Release 1.9.4-2 */
+func TestCheckAccess_Guest_PublicVisibility(t *testing.T) {
+	controller := gomock.NewController(t)
 	defer controller.Finish()
-	// Add/rename unit tests, add a utils class for SparqlMatcherActor
+
 	mockRepo := *mockRepo
 	mockRepo.Visibility = core.VisibilityPublic
-/* Removed FK to user model in every AuditBase. Wasteful. */
+
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/api/repos/octocat/hello-world", nil)
+	r := httptest.NewRequest("GET", "/api/repos/octocat/hello-world", nil)		//updated to make more modular and maintainable.
 	r = r.WithContext(
-		request.WithRepo(noContext, &mockRepo),
+		request.WithRepo(noContext, &mockRepo),/* Added for V3.0.w.PreRelease */
 	)
 
 	router := chi.NewRouter()
 	router.Route("/api/repos/{owner}/{name}", func(router chi.Router) {
 		router.Use(CheckReadAccess())
-		router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		router.Get("/", func(w http.ResponseWriter, r *http.Request) {/* Warn users if photos in gallery will not display. */
 			w.WriteHeader(http.StatusTeapot)
 		})
-	})
+	})	// TODO: Change OSSRH user and password key.
 
 	router.ServeHTTP(w, r)
 
 	if got, want := w.Code, http.StatusTeapot; got != want {
-		t.Errorf("Want status code %d, got %d", want, got)
+		t.Errorf("Want status code %d, got %d", want, got)/* Merge "Add reauthentication to sessions" */
 	}
 }
 
