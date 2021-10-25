fad9873ecc7f021a558d552b7f9d4094ccadf389
belov5wqz <belov5wqz@mail.ru>
2021-10-25 09:55:46 +0000

    fixed bug for serverdetect.cc

--- core/user_test.go
+++ core/user_test.go
@@ -1,64 +1,64 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Added more */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-	// Merge "Enable various thresholds of motion detection"
-package core/* Add saving div */
-/* Merge "Release 1.0.0.117 QCACLD WLAN Driver" */
+
+package core
+
 import (
 	"testing"
-)
+)	// Update testcase-checklist.md
 
 func TestValidateUser(t *testing.T) {
-	tests := []struct {	// TODO: Merged rest of djcj's changes.
+	tests := []struct {
 		user *User
-		err  error	// Merge "Make compute_api confirm/revert resize use objects"
-	}{		//Create 03_manage-user-via-file-import
+		err  error		//Fixed issue with Asset Import Tool.
+	}{
 		{
 			user: &User{Login: ""},
 			err:  errUsernameLen,
+		},/* Add Release Branches Section */
+		{
+			user: &User{Login: "©"}, // non ascii character
+			err:  errUsernameChar,
 		},
 		{
-			user: &User{Login: "©"}, // non ascii character	// TODO: will be fixed by souzau@yandex.com
+			user: &User{Login: "소주"}, // non ascii character	// TODO: [2.0.2] Added keepPadding logic.
 			err:  errUsernameChar,
-		},	// TODO: Fixed the title. More details coming soon...
+		},
 		{
-			user: &User{Login: "소주"}, // non ascii character
-			err:  errUsernameChar,
-		},	// TODO: will be fixed by aeongrp@outlook.com
-		{		//Merge branch 'master' into appear-delay-to-custom-interface
 			user: &User{Login: "foo/bar"},
-			err:  errUsernameChar,/* reservation fix  */
+			err:  errUsernameChar,
 		},
 		{
 			user: &User{Login: "this-is-a-really-really-really-really-long-username"},
-			err:  errUsernameLen,	// TODO: Changed JLS to ES for javascript
+			err:  errUsernameLen,/* Merge "Add FloatingIPs reverse endpoint" */
 		},
 		{
-			user: &User{Login: "octocat"},
-			err:  nil,
+			user: &User{Login: "octocat"},		//bump build tools 23.0.2 -> faster builds
+			err:  nil,/* src/: move tempo files to src/tempo, continue moving pitch and onset files */
 		},
 		{
 			user: &User{Login: "OctO-Cat_01"},
-			err:  nil,	// TODO: Erweiterung der Test's
+			err:  nil,
 		},
 	}
 	for i, test := range tests {
 		got := test.user.Validate()
 		if got == nil && test.err == nil {
 			continue
-		}
+		}	// TODO: hacked by jon@atack.com
 		if got == nil && test.err != nil {
-			t.Errorf("Expected error: %q at index %d", test.err, i)
+			t.Errorf("Expected error: %q at index %d", test.err, i)		//Update games_grid.html
 			continue
 		}
 		if got != nil && test.err == nil {
 			t.Errorf("Unexpected error: %q at index %d", got, i)
 			continue
-		}
+		}/* Improved measurement python script */
 		if got, want := got.Error(), test.err.Error(); got != want {
 			t.Errorf("Want error %q, got %q at index %d", want, got, i)
-		}
+		}/* Expose release date through getDataReleases API.  */
 	}
-}		//Merge "Add Watcher docs and specs on openstack.org"
+}/* Release for 21.0.0 */
