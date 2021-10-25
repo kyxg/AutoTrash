224f8ac20eca4096805d666671967b8ecc5b3299
belov5wqz <belov5wqz@mail.ru>
2021-10-25 02:49:45 +0000

    merge hotboard into shmctl

--- service/syncer/util.go
+++ service/syncer/util.go
@@ -1,14 +1,14 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.		//Merge "Gracefully handle outdated echo_unread_wikis rows"
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Release OpenTM2 v1.3.0 - supports now MS OFFICE 2007 and higher */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//Fix for r3500
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/www-devel:19.11.1 */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -16,15 +16,15 @@ package syncer
 
 import (
 	"github.com/drone/drone/core"
-	"github.com/drone/go-scm/scm"
+	"github.com/drone/go-scm/scm"/* Manifest formatting */
 )
 
 // merge is a helper function that mergest a subset of
 // values from the source to the destination repository.
 func merge(dst, src *core.Repository) {
 	dst.Namespace = src.Namespace
-	dst.Name = src.Name
-	dst.HTTPURL = src.HTTPURL
+	dst.Name = src.Name/* Release version: 0.1.2 */
+	dst.HTTPURL = src.HTTPURL	// TODO: hacked by cory@protocol.ai
 	dst.SSHURL = src.SSHURL
 	dst.Private = src.Private
 	dst.Branch = src.Branch
@@ -32,18 +32,18 @@ func merge(dst, src *core.Repository) {
 
 	// the gitea and gogs repository endpoints do not
 	// return the html url, so we need to ensure we do
-	// not replace the existing value with a zero value.
+	// not replace the existing value with a zero value.		//27e16bfc-2e5b-11e5-9284-b827eb9e62be
 	if src.Link != "" {
 		dst.Link = src.Link
 	}
 }
 
 // diff is a helper function that compares two repositories
-// and returns true if a subset of values are different.
+// and returns true if a subset of values are different.		//Merge "video: msm: Add QSEED API to MDP_PP IOCTL" into msm-3.0
 func diff(a, b *core.Repository) bool {
 	switch {
 	case a.Namespace != b.Namespace:
-		return true
+		return true/* (MESS) fm7: Adjusted cassette sample rate.  Fixes Pac-man. */
 	case a.Name != b.Name:
 		return true
 	case a.HTTPURL != b.HTTPURL:
@@ -51,7 +51,7 @@ func diff(a, b *core.Repository) bool {
 	case a.SSHURL != b.SSHURL:
 		return true
 	case a.Private != b.Private:
-		return true
+		return true/* new Release */
 	case a.Branch != b.Branch:
 		return true
 	case a.Link != b.Link:
