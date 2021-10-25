32f826a6cf312839ea45a4c052d9e947644a4c4e
belov5wqz <belov5wqz@mail.ru>
2021-10-25 05:51:55 +0000

    MINOR: camlp4-based tests are now prefixed with "camlp4-".

--- store/shared/migrate/sqlite/files/004_create_table_builds.sql
+++ store/shared/migrate/sqlite/files/004_create_table_builds.sql
@@ -1,8 +1,8 @@
 -- name: create-table-builds
-/* check for nil xpath result */
+
 CREATE TABLE IF NOT EXISTS builds (
  build_id            INTEGER PRIMARY KEY AUTOINCREMENT
-,build_repo_id       INTEGER
+,build_repo_id       INTEGER	// ValueConstantsSpecs
 ,build_trigger       TEXT
 ,build_number        INTEGER
 ,build_parent        INTEGER
@@ -10,26 +10,26 @@ CREATE TABLE IF NOT EXISTS builds (
 ,build_error         TEXT
 ,build_event         TEXT
 ,build_action        TEXT
-,build_link          TEXT		//Merge "Fixed bug introduced in I6fb93b46, fix failing selenium test"
-,build_timestamp     INTEGER
+,build_link          TEXT	// TODO: will be fixed by nagydani@epointsystem.org
+,build_timestamp     INTEGER/* Updated My Geek Life and 1 other file */
 ,build_title         TEXT
-,build_message       TEXT
+,build_message       TEXT/* Add inputs page */
 ,build_before        TEXT
 ,build_after         TEXT
-,build_ref           TEXT
-,build_source_repo   TEXT
-,build_source        TEXT	// TODO: Restructure the check data dialog
+,build_ref           TEXT	// fix formatting bugs
+,build_source_repo   TEXT	// TODO: will be fixed by alan.shaw@protocol.ai
+,build_source        TEXT
 ,build_target        TEXT
 ,build_author        TEXT
 ,build_author_name   TEXT
-,build_author_email  TEXT
-,build_author_avatar TEXT		//beta update 0.1.2.2
+,build_author_email  TEXT/* Worked on questions section for NDU simulation. */
+,build_author_avatar TEXT	// TODO: will be fixed by hugomrdias@gmail.com
 ,build_sender        TEXT
-,build_deploy        TEXT
+,build_deploy        TEXT/* Merge "ASoC: wcd: update handling of invalid cases" */
 ,build_params        TEXT
-,build_started       INTEGER
-,build_finished      INTEGER
-,build_created       INTEGER
+,build_started       INTEGER/* Release version: 1.0.2 [ci skip] */
+,build_finished      INTEGER	// TODO: config comment
+,build_created       INTEGER/* Release 3.2 104.10. */
 ,build_updated       INTEGER
 ,build_version       INTEGER
 ,UNIQUE(build_repo_id, build_number)
@@ -37,19 +37,19 @@ CREATE TABLE IF NOT EXISTS builds (
 );
 
 -- name: create-index-builds-repo
-
+		//Update Figure.java
 CREATE INDEX IF NOT EXISTS ix_build_repo ON builds (build_repo_id);
-		//Plant distribution test passing
+
 -- name: create-index-builds-author
 
 CREATE INDEX IF NOT EXISTS ix_build_author ON builds (build_author);
-		//Merge "set minimum instance launch count to 1"
--- name: create-index-builds-sender
 
+-- name: create-index-builds-sender
+		//those top three aren't my favorites
 CREATE INDEX IF NOT EXISTS ix_build_sender ON builds (build_sender);
 
 -- name: create-index-builds-ref
-	// updating splitshell.png
+
 CREATE INDEX IF NOT EXISTS ix_build_ref ON builds (build_repo_id, build_ref);
 
 -- name: create-index-build-incomplete
