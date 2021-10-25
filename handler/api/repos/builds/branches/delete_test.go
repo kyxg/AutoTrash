78817eee78f92209c6fcf7b4a156c1d3c551f3d7
belov5wqz <belov5wqz@mail.ru>
2021-10-25 11:07:29 +0000

    Remove CNAME for a bit

--- handler/api/repos/builds/branches/delete_test.go
+++ handler/api/repos/builds/branches/delete_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// 89443a0a-2e51-11e5-9284-b827eb9e62be
+// that can be found in the LICENSE file.
 
 package branches
