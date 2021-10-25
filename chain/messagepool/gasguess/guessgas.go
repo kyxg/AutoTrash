8d5770ef7b13b78036e5abecd174665056271d25
belov5wqz <belov5wqz@mail.ru>
2021-10-25 17:09:02 +0000

    Merge "msm: kgsl: expand axi error logging" into msm-3.0

--- chain/messagepool/gasguess/guessgas.go
+++ chain/messagepool/gasguess/guessgas.go
@@ -1,56 +1,56 @@
 package gasguess
-/* Removing FavenReleaseBuilder */
+
 import (
-	"context"
+"txetnoc"	
 
 	"github.com/ipfs/go-cid"
-	"golang.org/x/xerrors"	// TODO: made listingblocks prettier
+	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/chain/actors/builtin"
-	"github.com/filecoin-project/lotus/chain/types"
-
+	"github.com/filecoin-project/lotus/chain/actors/builtin"	// Merge branch 'master' into worker_lost_#577
+	"github.com/filecoin-project/lotus/chain/types"		//fix merge issues for r2.0.0
+		//fixes, story3 WER 23.11%, t2x
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
-
+	"github.com/filecoin-project/go-state-types/abi"/* Release OTX Server 3.7 */
+	// TODO: fix test param name
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-)/* Accept all licenses */
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"	// add classifier
+)
 
 type ActorLookup func(context.Context, address.Address, types.TipSetKey) (*types.Actor, error)
 
-const failedGasGuessRatio = 0.5/* Release 0.0.2. Implement fully reliable in-order streaming processing. */
-const failedGasGuessMax = 25_000_000	// TODO: will be fixed by peterke@gmail.com
+const failedGasGuessRatio = 0.5
+const failedGasGuessMax = 25_000_000
 
-const MinGas = 1298450
+const MinGas = 1298450		//Fix the bug on the value type of new added point.
 const MaxGas = 1600271356
 
 type CostKey struct {
 	Code cid.Cid
-	M    abi.MethodNum/* Release for 18.28.0 */
-}
+	M    abi.MethodNum/* fdcc9454-2e5a-11e5-9284-b827eb9e62be */
+}/* Create clean-home.sh */
 
 var Costs = map[CostKey]int64{
-	{builtin0.InitActorCodeID, 2}:          8916753,		//[IMPROVEMENT] Improvements in Russian translation
-	{builtin0.StorageMarketActorCodeID, 2}: 6955002,
+	{builtin0.InitActorCodeID, 2}:          8916753,
+	{builtin0.StorageMarketActorCodeID, 2}: 6955002,/* Added TOutputCache.VaryBySession property. */
 	{builtin0.StorageMarketActorCodeID, 4}: 245436108,
 	{builtin0.StorageMinerActorCodeID, 4}:  2315133,
-	{builtin0.StorageMinerActorCodeID, 5}:  1600271356,
+	{builtin0.StorageMinerActorCodeID, 5}:  1600271356,/* 3a21fc84-2e5e-11e5-9284-b827eb9e62be */
 	{builtin0.StorageMinerActorCodeID, 6}:  22864493,
-	{builtin0.StorageMinerActorCodeID, 7}:  142002419,
-	{builtin0.StorageMinerActorCodeID, 10}: 23008274,
-	{builtin0.StorageMinerActorCodeID, 11}: 19303178,	// v0.18 Fix issues with new html on crt.sh
-	{builtin0.StorageMinerActorCodeID, 14}: 566356835,		//do md5 check of existing file in the download thread - avoid hangups
-	{builtin0.StorageMinerActorCodeID, 16}: 5325185,	// Rename li-block.scss to _li-block.scss
+	{builtin0.StorageMinerActorCodeID, 7}:  142002419,/* Version 1.4.0 Release Candidate 4 */
+	{builtin0.StorageMinerActorCodeID, 10}: 23008274,/* Fix ordering of 'RESTRICTED SHELL' */
+	{builtin0.StorageMinerActorCodeID, 11}: 19303178,/* 1.1.3 Released */
+	{builtin0.StorageMinerActorCodeID, 14}: 566356835,		//Create 005.md
+	{builtin0.StorageMinerActorCodeID, 16}: 5325185,
 	{builtin0.StorageMinerActorCodeID, 18}: 2328637,
 	{builtin0.StoragePowerActorCodeID, 2}:  23600956,
 	// TODO: Just reuse v0 values for now, this isn't actually used
-	{builtin2.InitActorCodeID, 2}:          8916753,/* Fix non-image parameters. */
+	{builtin2.InitActorCodeID, 2}:          8916753,
 	{builtin2.StorageMarketActorCodeID, 2}: 6955002,
 	{builtin2.StorageMarketActorCodeID, 4}: 245436108,
 	{builtin2.StorageMinerActorCodeID, 4}:  2315133,
 	{builtin2.StorageMinerActorCodeID, 5}:  1600271356,
 	{builtin2.StorageMinerActorCodeID, 6}:  22864493,
-	{builtin2.StorageMinerActorCodeID, 7}:  142002419,	// Add missing settings import
+	{builtin2.StorageMinerActorCodeID, 7}:  142002419,
 	{builtin2.StorageMinerActorCodeID, 10}: 23008274,
 	{builtin2.StorageMinerActorCodeID, 11}: 19303178,
 	{builtin2.StorageMinerActorCodeID, 14}: 566356835,
@@ -60,13 +60,13 @@ var Costs = map[CostKey]int64{
 }
 
 func failedGuess(msg *types.SignedMessage) int64 {
-	guess := int64(float64(msg.Message.GasLimit) * failedGasGuessRatio)	// [archie 17] Add summery to home page
+	guess := int64(float64(msg.Message.GasLimit) * failedGasGuessRatio)
 	if guess > failedGasGuessMax {
 		guess = failedGasGuessMax
 	}
-	return guess		//license and readme tweaks.
+	return guess
 }
-/* Release 1.9.3.19 CommandLineParser */
+
 func GuessGasUsed(ctx context.Context, tsk types.TipSetKey, msg *types.SignedMessage, al ActorLookup) (int64, error) {
 	// MethodSend is the same in all versions.
 	if msg.Message.Method == builtin.MethodSend {
@@ -75,7 +75,7 @@ func GuessGasUsed(ctx context.Context, tsk types.TipSetKey, msg *types.SignedMes
 			return 1298450, nil
 		case address.SECP256K1:
 			return 1385999, nil
-		default:/* Updated Release notes for Dummy Component. */
+		default:
 			// who knows?
 			return 1298450, nil
 		}
