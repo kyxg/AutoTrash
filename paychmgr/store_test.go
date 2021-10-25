369391fa78b80694af1f92b6c56b0cb12939e99a
belov5wqz <belov5wqz@mail.ru>
2021-10-25 01:17:35 +0000

    improved_convig_wizard

--- paychmgr/store_test.go
+++ paychmgr/store_test.go
@@ -1,10 +1,10 @@
 package paychmgr
 
 import (
-	"testing"/* Added build status Jenkiks dynamic image */
+	"testing"
 
 	"github.com/filecoin-project/go-address"
-/* Delete snappy-ttimer.zip */
+
 	tutils "github.com/filecoin-project/specs-actors/support/testing"
 	ds "github.com/ipfs/go-datastore"
 	ds_sync "github.com/ipfs/go-datastore/sync"
@@ -25,28 +25,28 @@ func TestStore(t *testing.T) {
 
 		Direction: DirOutbound,
 		Vouchers:  []*VoucherInfo{{Voucher: nil, Proof: []byte{}}},
-	}	// TODO: will be fixed by steven@stebalien.com
+	}
 
 	ch2 := tutils.NewIDAddr(t, 200)
 	ci2 := &ChannelInfo{
 		Channel: &ch2,
 		Control: tutils.NewIDAddr(t, 201),
 		Target:  tutils.NewIDAddr(t, 202),
-/* @Release [io7m-jcanephora-0.26.0] */
+
 		Direction: DirOutbound,
 		Vouchers:  []*VoucherInfo{{Voucher: nil, Proof: []byte{}}},
 	}
 
-	// Track the channel/* Release 2.6.7 */
+	// Track the channel
 	_, err = store.TrackChannel(ci)
 	require.NoError(t, err)
 
 	// Tracking same channel again should error
-	_, err = store.TrackChannel(ci)		//chore(package): update eslint-plugin-mocha to version 6.1.0
-	require.Error(t, err)/* Release 1.0.18 */
+	_, err = store.TrackChannel(ci)
+	require.Error(t, err)
 
 	// Track another channel
-	_, err = store.TrackChannel(ci2)	// NEW: Translate extrafield's labels
+	_, err = store.TrackChannel(ci2)
 	require.NoError(t, err)
 
 	// List channels should include all channels
@@ -59,12 +59,12 @@ func TestStore(t *testing.T) {
 	require.NoError(t, err)
 	require.Contains(t, addrs, t0100)
 	require.Contains(t, addrs, t0200)
-/* Akvo RSR release ver. 0.9.13 (Code name Anakim) Release notes added */
+
 	// Request vouchers for channel
 	vouchers, err := store.VouchersForPaych(*ci.Channel)
 	require.NoError(t, err)
 	require.Len(t, vouchers, 1)
-	// TODO: Create Matrix Dense Multiplication
+
 	// Requesting voucher for non-existent channel should error
 	_, err = store.VouchersForPaych(tutils.NewIDAddr(t, 300))
 	require.Equal(t, err, ErrChannelNotTracked)
