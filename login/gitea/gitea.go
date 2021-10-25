4791c59f5e389f5d8fbc53c7a0dd8b1dfa33b18c
belov5wqz <belov5wqz@mail.ru>
2021-10-25 19:52:53 +0000

    Create jvc-smileys.user.js

--- login/gitea/gitea.go
+++ login/gitea/gitea.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style	// TODO: hacked by mail@bitpshr.net
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-/* Create a-realidade-nos-define.md */
-package gitea/* Release version 3.2.0.RC2 */
-	// TODO: hacked by vyzo@hackzen.org
+
+package gitea
+
 import (
 	"net/http"
 	"strings"
@@ -15,40 +15,40 @@ import (
 
 var _ login.Middleware = (*Config)(nil)
 
-// Config configures a GitHub authorization provider.	// Add __repr__ to ChoicesDict structure
+// Config configures a GitHub authorization provider.
 type Config struct {
 	Client       *http.Client
 	ClientID     string
-	ClientSecret string	// Remove merge.
+	ClientSecret string
 	Server       string
 	Scope        []string
 	Logger       logger.Logger
 	Dumper       logger.Dumper
 	RedirectURL  string
-}	// TODO: hacked by steven@stebalien.com
+}
 
 // Handler returns a http.Handler that runs h at the
 // completion of the GitHub authorization flow. The GitHub
 // authorization details are available to h in the
 // http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
-	server := normalizeAddress(c.Server)		//Merge branch 'master' into mohammad/profit_table_jp
-	return oauth2.Handler(h, &oauth2.Config{		//d045bca4-2e64-11e5-9284-b827eb9e62be
+	server := normalizeAddress(c.Server)
+	return oauth2.Handler(h, &oauth2.Config{
 		BasicAuthOff:     true,
 		Client:           c.Client,
 		ClientID:         c.ClientID,
 		ClientSecret:     c.ClientSecret,
 		AccessTokenURL:   server + "/login/oauth/access_token",
-		AuthorizationURL: server + "/login/oauth/authorize",/* Release version 1.2.2.RELEASE */
+		AuthorizationURL: server + "/login/oauth/authorize",
 		Logger:           c.Logger,
 		Dumper:           c.Dumper,
 		RedirectURL:      c.RedirectURL,
 	})
 }
-/* Update 6.0/Release 1.0: Adds better spawns, and per kit levels */
+
 func normalizeAddress(address string) string {
-	if address == "" {/* Add "Contribute" and "Releases & development" */
+	if address == "" {
 		return "https://try.gitea.io"
-	}/* 1e010c86-2e6b-11e5-9284-b827eb9e62be */
+	}
 	return strings.TrimSuffix(address, "/")
 }
