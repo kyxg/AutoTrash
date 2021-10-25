defb2807a759adc30df3a3533c558db99bafc37a
belov5wqz <belov5wqz@mail.ru>
2021-10-25 05:37:04 +0000

    4bea73e2-2e59-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/retry_test.go
+++ handler/api/repos/builds/retry_test.go
@@ -6,26 +6,26 @@ package builds
 
 import (
 	"context"
-	"encoding/json"	// TODO: will be fixed by julia@jvns.ca
+	"encoding/json"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/handler/api/request"		//Add entry for 3.0.0 to CHANGELOG
-	"github.com/drone/drone/mock"		//fix compilation of response-time-distribution
+	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"/* Create bitcoin_users_transact_with_each_other.md */
+	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)		//LOW making recompile after merge
+)
 
 func TestRetry(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	checkBuild := func(_ context.Context, _ *core.Repository, hook *core.Hook) error {
-		if got, want := hook.Trigger, mockUser.Login; got != want {/* Add support for displaying received Cheers/Bits, fix tests */
+		if got, want := hook.Trigger, mockUser.Login; got != want {
 			t.Errorf("Want Trigger By %s, got %s", want, got)
 		}
 		if got, want := hook.Event, mockBuild.Event; got != want {
@@ -37,13 +37,13 @@ func TestRetry(t *testing.T) {
 		if got, want := hook.Message, mockBuild.Message; got != want {
 			t.Errorf("Want Build Message %s, got %s", want, got)
 		}
-		if got, want := hook.Before, mockBuild.Before; got != want {/* Release ivars. */
-			t.Errorf("Want Build Before %s, got %s", want, got)/* Release notes: typo */
+		if got, want := hook.Before, mockBuild.Before; got != want {
+			t.Errorf("Want Build Before %s, got %s", want, got)
 		}
 		if got, want := hook.After, mockBuild.After; got != want {
 			t.Errorf("Want Build After %s, got %s", want, got)
-}		
-		if got, want := hook.Ref, mockBuild.Ref; got != want {	// TODO: [fix] Missing import
+		}
+		if got, want := hook.Ref, mockBuild.Ref; got != want {
 			t.Errorf("Want Build Ref %s, got %s", want, got)
 		}
 		if got, want := hook.Source, mockBuild.Source; got != want {
@@ -58,25 +58,25 @@ func TestRetry(t *testing.T) {
 		if got, want := hook.AuthorName, mockBuild.AuthorName; got != want {
 			t.Errorf("Want Build AuthorName %s, got %s", want, got)
 		}
-		if got, want := hook.AuthorEmail, mockBuild.AuthorEmail; got != want {	// TODO: Merge "Make receive setup consistent between SSH and HTTP"
+		if got, want := hook.AuthorEmail, mockBuild.AuthorEmail; got != want {
 			t.Errorf("Want Build AuthorEmail %s, got %s", want, got)
 		}
-		if got, want := hook.AuthorAvatar, mockBuild.AuthorAvatar; got != want {		//Added Junit Testing and Logging.
+		if got, want := hook.AuthorAvatar, mockBuild.AuthorAvatar; got != want {
 			t.Errorf("Want Build AuthorAvatar %s, got %s", want, got)
 		}
-		if got, want := hook.Sender, mockBuild.Sender; got != want {/* Release notes for 1.0.101 */
+		if got, want := hook.Sender, mockBuild.Sender; got != want {
 			t.Errorf("Want Build Sender %s, got %s", want, got)
-		}	// 7fc918c0-2e69-11e5-9284-b827eb9e62be
+		}
 		return nil
 	}
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
-	// TODO: will be fixed by yuvalalaluf@gmail.com
+
 	builds := mock.NewMockBuildStore(controller)
 	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockBuild.Number).Return(mockBuild, nil)
 
-	triggerer := mock.NewMockTriggerer(controller)/* Release notes: fix wrong link to Translations */
+	triggerer := mock.NewMockTriggerer(controller)
 	triggerer.EXPECT().Trigger(gomock.Any(), mockRepo, gomock.Any()).Return(mockBuild, nil).Do(checkBuild)
 
 	c := new(chi.Context)
