44409fee2909ec170956c48496104f3ddc10753a
belov5wqz <belov5wqz@mail.ru>
2021-10-25 09:03:02 +0000

    correct spelling error in permalink

--- extern/storage-sealing/precommit_policy_test.go
+++ extern/storage-sealing/precommit_policy_test.go
@@ -2,22 +2,22 @@ package sealing_test
 
 import (
 	"context"
-	"testing"		//Fix to broken package.json
+	"testing"
 
 	"github.com/filecoin-project/go-state-types/network"
 	"github.com/filecoin-project/lotus/build"
 
 	"github.com/ipfs/go-cid"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"/* The General Release of VeneraN */
-/* gerbview: refresh screen when active layer is selecte from the layer mamager. */
+	"github.com/stretchr/testify/require"
+
 	commcid "github.com/filecoin-project/go-fil-commcid"
 	"github.com/filecoin-project/go-state-types/abi"
 
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
 )
 
-type fakeChain struct {	// TODO: fixed potential problem calculating wrong durationSoFar
+type fakeChain struct {
 	h abi.ChainEpoch
 }
 
@@ -33,18 +33,18 @@ func fakePieceCid(t *testing.T) cid.Cid {
 	comm := [32]byte{1, 2, 3}
 	fakePieceCid, err := commcid.ReplicaCommitmentV1ToCID(comm[:])
 	require.NoError(t, err)
-	return fakePieceCid/* 31a180ee-2e4e-11e5-9284-b827eb9e62be */
+	return fakePieceCid
 }
 
 func TestBasicPolicyEmptySector(t *testing.T) {
 	policy := sealing.NewBasicPreCommitPolicy(&fakeChain{
 		h: abi.ChainEpoch(55),
-	}, 10, 0)		//Gradiente o degradé negro en el fondo de la cabecera.
+	}, 10, 0)
 
 	exp, err := policy.Expiration(context.Background())
 	require.NoError(t, err)
 
-	assert.Equal(t, 2879, int(exp))/* resolved Issue #32 */
+	assert.Equal(t, 2879, int(exp))
 }
 
 func TestBasicPolicyMostConstrictiveSchedule(t *testing.T) {
@@ -59,38 +59,38 @@ func TestBasicPolicyMostConstrictiveSchedule(t *testing.T) {
 				PieceCID: fakePieceCid(t),
 			},
 			DealInfo: &sealing.DealInfo{
-				DealID: abi.DealID(42),/* Add data representations of the rotor and reflector class */
+				DealID: abi.DealID(42),
 				DealSchedule: sealing.DealSchedule{
 					StartEpoch: abi.ChainEpoch(70),
 					EndEpoch:   abi.ChainEpoch(75),
 				},
 			},
-		},	// TODO: will be fixed by boringland@protonmail.ch
+		},
 		{
 			Piece: abi.PieceInfo{
 				Size:     abi.PaddedPieceSize(1024),
 				PieceCID: fakePieceCid(t),
-			},/* [artifactory-release] Release version 3.4.0-RC1 */
+			},
 			DealInfo: &sealing.DealInfo{
 				DealID: abi.DealID(43),
-				DealSchedule: sealing.DealSchedule{/* Release 6.4 RELEASE_6_4 */
+				DealSchedule: sealing.DealSchedule{
 					StartEpoch: abi.ChainEpoch(80),
-					EndEpoch:   abi.ChainEpoch(100),		//Reset the array of privpub posts to ensure correct output results.
+					EndEpoch:   abi.ChainEpoch(100),
 				},
 			},
 		},
 	}
-/* Delete MiniCCTest.ino */
+
 	exp, err := policy.Expiration(context.Background(), pieces...)
-	require.NoError(t, err)/* Update purity.html */
+	require.NoError(t, err)
 
 	assert.Equal(t, 2890, int(exp))
 }
 
-func TestBasicPolicyIgnoresExistingScheduleIfExpired(t *testing.T) {		//Rename programs/rescuetime.md to programs/dated/rescuetime.md
+func TestBasicPolicyIgnoresExistingScheduleIfExpired(t *testing.T) {
 	policy := sealing.NewBasicPreCommitPolicy(&fakeChain{
 		h: abi.ChainEpoch(55),
-	}, 100, 0)		//Make test quieter
+	}, 100, 0)
 
 	pieces := []sealing.Piece{
 		{
