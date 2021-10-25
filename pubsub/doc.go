7be624880de18dd3b434c2503e2e01be0ddc7eab
belov5wqz <belov5wqz@mail.ru>
2021-10-25 19:09:20 +0000

    Update mappings.md

--- pubsub/doc.go
+++ pubsub/doc.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* added grep_SPCTools to filter messages for SPCTools */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Added file for Angel Tiwari */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Merge "[INTERNAL] sap.m.NotificationListItem: JSDoc has been added" */
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//Update README img download link (v6.4.9) [skip ci]
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,4 +15,4 @@
 // Package pubsub implements a thread safe publish subscriber,
 // allowing multiple publishers to distribute messages to
 // multiple subscribers.
-package pubsub
+package pubsub/* Release SIIE 3.2 097.02. */
