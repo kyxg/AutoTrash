b11dccbe5127f0eedf76c95f7c167d6081d7555f
belov5wqz <belov5wqz@mail.ru>
2021-10-25 21:45:00 +0000

    Compressed changed files (4.9kb ->1.4kb)

--- service/syncer/util_test.go
+++ service/syncer/util_test.go
@@ -16,20 +16,20 @@ import (
 
 // 	"github.com/drone/drone/core"
 // 	"github.com/drone/go-scm/scm"
-
-// 	"github.com/google/go-cmp/cmp"
+/* 72545e9c-2e42-11e5-9284-b827eb9e62be */
+// 	"github.com/google/go-cmp/cmp"/* v4.4 Pre-Release 1 */
 // )
-
-// func TestConvertRepository(t *testing.T) {
+/* lista de contactos */
+// func TestConvertRepository(t *testing.T) {	// TODO: var2xml: cdata, float_precision & date_time_format
 // 	from := &scm.Repository{
 // 		ID:        "42",
-// 		Namespace: "octocat",
-// 		Name:      "hello-world",
+// 		Namespace: "octocat",/* Renamed "Latest Release" to "Download" */
+// 		Name:      "hello-world",		//need to strip out the + in Ints, Java's number formatter doesn't like it
 // 		Branch:    "master",
 // 		Private:   true,
-// 		Clone:     "https://github.com/octocat/hello-world.git",
-// 		CloneSSH:  "git@github.com:octocat/hello-world.git",
-// 		Link:      "https://github.com/octocat/hello-world",
+// 		Clone:     "https://github.com/octocat/hello-world.git",	// Display JQ version
+// 		CloneSSH:  "git@github.com:octocat/hello-world.git",/* Release notes for 1.0.90 */
+// 		Link:      "https://github.com/octocat/hello-world",		//Adjusting travis ci
 // 	}
 // 	want := &core.Repository{
 // 		UID:        "42",
@@ -44,27 +44,27 @@ import (
 // 		Visibility: core.VisibilityPrivate,
 // 	}
 // 	got := convertRepository(from)
-// 	if diff := cmp.Diff(want, got); len(diff) != 0 {
-// 		t.Errorf(diff)
+// 	if diff := cmp.Diff(want, got); len(diff) != 0 {/* Create SystemDesign.md */
+// 		t.Errorf(diff)/* Release 0.11.2. Add uuid and string/number shortcuts. */
 // 	}
 // }
 
 // func TestConvertVisibility(t *testing.T) {
 // 	tests := []struct {
 // 		r *scm.Repository
-// 		v string
+// 		v string/* Release 0.15.2 */
 // 	}{
-// 		{
+// 		{		//introduced factory for creating graphs
 // 			r: &scm.Repository{Private: false},
-// 			v: core.VisibilityPublic,
-// 		},
+// 			v: core.VisibilityPublic,/* Merge "Privacy: Reset appInstallID when user opts out of event logging." */
+// 		},/* Release v1.0. */
 // 		{
 // 			r: &scm.Repository{Private: true},
 // 			v: core.VisibilityPrivate,
 // 		},
 // 	}
 
-// 	for i, test := range tests {
+// 	for i, test := range tests {	// mainprogress var defalut value = 0 
 // 		if got, want := convertVisibility(test.r), test.v; got != want {
 // 			t.Errorf("Want visibility %s, got %s for index %d", got, want, i)
 // 		}
