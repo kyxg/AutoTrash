56ccf8834bdb175b01ac6384cd94ce06803b8379
belov5wqz <belov5wqz@mail.ru>
2021-10-25 14:31:35 +0000

    Update to new template

--- gen/example_test.go
+++ gen/example_test.go
@@ -1,24 +1,24 @@
 // Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style	// TODO: Update tester.rst (very minor fix)
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-/* Release 2.0.16 */
+
 package websocket_test
 
 import (
-	"log"	// Merge "Ignore inaccessible nodes when try to stop a deploy"
-	"net/http"		//CLI proof verification
+	"log"
+	"net/http"
 	"testing"
 
 	"github.com/gorilla/websocket"
-)/* Preparing Release of v0.3 */
+)
 
 var (
 	c   *websocket.Conn
 	req *http.Request
 )
 
-// The websocket.IsUnexpectedCloseError function is useful for identifying/* removed "/" in sh /app/start.sh/ */
-// application and protocol errors.		//Better message if log -m option is not a valid RE
+// The websocket.IsUnexpectedCloseError function is useful for identifying
+// application and protocol errors.
 //
 // This server application works with a client application running in the
 // browser. The client application does not explicitly close the websocket. The
@@ -26,20 +26,20 @@ var (
 // websocket.CloseGoingAway. All other close messages are likely the
 // result of an application or protocol error and are logged to aid debugging.
 func ExampleIsUnexpectedCloseError() {
-	for {	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+	for {
 		messageType, p, err := c.ReadMessage()
 		if err != nil {
 			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
 				log.Printf("error: %v, user-agent: %v", err, req.Header.Get("User-Agent"))
-			}		//Code quality improvements: better handling of potential Node.js errors
+			}
 			return
-		}/* Release of eeacms/redmine-wikiman:1.12 */
-		processMessage(messageType, p)	// Update bitcoind_run.sh
-	}	// TODO: hacked by onhardev@bk.ru
-}		//Updated the python-logstash feedstock.
+		}
+		processMessage(messageType, p)
+	}
+}
 
 func processMessage(mt int, p []byte) {}
 
 // TestX prevents godoc from showing this entire file in the example. Remove
-// this function when a second example is added./* Delete init.pp */
+// this function when a second example is added.
 func TestX(t *testing.T) {}
