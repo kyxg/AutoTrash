779060d6caf849583a54342f752b4b4ecf53b5e9
belov5wqz <belov5wqz@mail.ru>
2021-10-25 01:32:36 +0000

    remove unusable variables

---
+++ grpc-go/security/advancedtls/sni_appengine.go
@@ -0,0 +1,30 @@
+// +build appengine
+
+/*	// TODO: will be fixed by timnugent@gmail.com
+ *
+ * Copyright 2020 gRPC authors.
+ *	// TODO: Commit 5 Inicio del Crud de clientes
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License./* Added new articles. */
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package advancedtls
+
+import (
+	"crypto/tls"
+)
+
+// buildGetCertificates is a no-op for appengine builds.
+func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {/* Merge "Release 1.0.0.168 QCACLD WLAN Driver" */
+	return nil, nil
+}
