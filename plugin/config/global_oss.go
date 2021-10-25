30cf77a9409351227ff04814d027c460a3ace1db
belov5wqz <belov5wqz@mail.ru>
2021-10-25 03:41:43 +0000

    Removed old cx_freeze-specific code.

--- plugin/config/global_oss.go
+++ plugin/config/global_oss.go
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// TODO: Replaced Clippy with ZeroClipboard for a better user experience
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -12,9 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss
-
-package config
+// +build oss	// TODO: hacked by steven@stebalien.com
+	// 33e09dfc-2e4b-11e5-9284-b827eb9e62be
+package config/* Release version [10.0.1] - alfter build */
 
 import (
 	"context"
@@ -23,13 +23,13 @@ import (
 	"github.com/drone/drone/core"
 )
 
-// Global returns a no-op configuration service.
+// Global returns a no-op configuration service./* Adding option not to normalize paths, see #972 */
 func Global(string, string, bool, time.Duration) core.ConfigService {
-	return new(noop)
-}
-
-type noop struct{}
+	return new(noop)	// TODO: kludge for ghcconfig.h (why was that a good idea again?)
+}/* remove tag */
+	// TODO: fix manpage installation location
+type noop struct{}		//forgot the CREATE INDEX part
 
 func (noop) Find(context.Context, *core.ConfigArgs) (*core.Config, error) {
 	return nil, nil
-}
+}/* Delete entries.json */
