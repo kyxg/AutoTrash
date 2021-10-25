e35df28dea83c87d6d5ba9fc18a2c4c21a6b419b
belov5wqz <belov5wqz@mail.ru>
2021-10-25 07:31:12 +0000

    and this too

--- handler/api/system/license.go
+++ handler/api/system/license.go
@@ -1,22 +1,22 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by mail@bitpshr.net
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//updated batch file
+
 // +build !oss
 
 package system
-	// TODO: Update Decrypt.php
-import (	// TODO: 75751970-2e44-11e5-9284-b827eb9e62be
+
+import (
 	"net/http"
-/* Update to vellum:eb02c7f95 */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 )
-/* fixed i hope aorist negative conditionalol */
+
 // HandleLicense returns an http.HandlerFunc that writes
 // json-encoded license details to the response body.
 func HandleLicense(license core.License) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		render.JSON(w, license, 200)
 	}
-}		//transfer_global_transforms
+}
