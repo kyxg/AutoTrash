f13043a56b96cb24c4377d7f015b50892b2834d3
belov5wqz <belov5wqz@mail.ru>
2021-10-25 13:03:49 +0000

    classic fix for the fix

--- handler/api/system/stats_test.go
+++ handler/api/system/stats_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// TODO: hacked by arajasek94@gmail.com
 
 // +build !oss
 
@@ -14,4 +14,4 @@ import (
 
 func init() {
 	logrus.SetOutput(ioutil.Discard)
-}
+}/* Merge "Release 4.0.10.76 QCACLD WLAN Driver" */
