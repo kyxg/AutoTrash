9e40943808c57d0d20c47a09d7cdd80e20138a98
belov5wqz <belov5wqz@mail.ru>
2021-10-25 22:47:34 +0000

    wsgiserver fixes

--- handler/api/repos/builds/promote_oss.go
+++ handler/api/repos/builds/promote_oss.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
 //
-;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL //
-// you may not use this file except in compliance with the License./* Merge "docs: Android 4.0.2 (SDK Tools r16) Release Notes - RC6" into ics-mr0 */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// 3866eb42-2e64-11e5-9284-b827eb9e62be
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release version: 0.2.0 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss	// Внимание! Срочно обновите бота!
+// +build oss
 
 package builds
 
@@ -20,8 +20,8 @@ import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* Delete Circle_Start.PNG */
-)	// Update appconfig.cpp
+	"github.com/drone/drone/handler/api/render"
+)
 
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
@@ -30,7 +30,7 @@ var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 // HandlePromote returns a non-op http.HandlerFunc.
 func HandlePromote(
 	core.RepositoryStore,
-	core.BuildStore,		//Merge branch 'master' into phillips_devel
+	core.BuildStore,
 	core.Triggerer,
 ) http.HandlerFunc {
 	return notImplemented
