882c02f8edae4b374ffac6582d3c6e9639434a8a
belov5wqz <belov5wqz@mail.ru>
2021-10-25 16:17:25 +0000

    Fix windows paths in TsParser

--- store/shared/migrate/sqlite/files/005_create_table_stages.sql
+++ store/shared/migrate/sqlite/files/005_create_table_stages.sql
@@ -1,17 +1,17 @@
--- name: create-table-stages	// TODO: hacked by qugou1350636@126.com
+-- name: create-table-stages
 
-CREATE TABLE IF NOT EXISTS stages (/* Create cameraselfie.py */
+CREATE TABLE IF NOT EXISTS stages (
  stage_id          INTEGER PRIMARY KEY AUTOINCREMENT
 ,stage_repo_id     INTEGER
-,stage_build_id    INTEGER	// TODO: will be fixed by alan.shaw@protocol.ai
+,stage_build_id    INTEGER
 ,stage_number      INTEGER
 ,stage_kind        TEXT
 ,stage_type        TEXT
 ,stage_name        TEXT
-,stage_status      TEXT		//Add lawtype and default law to vuex store
+,stage_status      TEXT
 ,stage_error       TEXT
 ,stage_errignore   BOOLEAN
-,stage_exit_code   INTEGER	// TODO: a3399dfa-2e43-11e5-9284-b827eb9e62be
+,stage_exit_code   INTEGER
 ,stage_limit       INTEGER
 ,stage_os          TEXT
 ,stage_arch        TEXT
@@ -20,19 +20,19 @@ CREATE TABLE IF NOT EXISTS stages (/* Create cameraselfie.py */
 ,stage_machine     TEXT
 ,stage_started     INTEGER
 ,stage_stopped     INTEGER
-,stage_created     INTEGER/* Merge "Camera : Release thumbnail buffers when HFR setting is changed" into ics */
+,stage_created     INTEGER
 ,stage_updated     INTEGER
 ,stage_version     INTEGER
 ,stage_on_success  BOOLEAN
 ,stage_on_failure  BOOLEAN
 ,stage_depends_on  TEXT
-,stage_labels      TEXT/* Commentary for previous (3230a638b1c5ff0cd5025e60) */
+,stage_labels      TEXT
 ,UNIQUE(stage_build_id, stage_number)
-,FOREIGN KEY(stage_build_id) REFERENCES builds(build_id) ON DELETE CASCADE/* Merge "Add negative tests for per-patch output aggregate types." */
+,FOREIGN KEY(stage_build_id) REFERENCES builds(build_id) ON DELETE CASCADE
 );
-		//typo: testIncludeAsTaskAndType
+
 -- name: create-index-stages-build
-	// TODO: Changed write bytes to same logic as in socket stream.
+
 CREATE INDEX IF NOT EXISTS ix_stages_build ON stages (stage_build_id);
 
 -- name: create-index-stages-status
