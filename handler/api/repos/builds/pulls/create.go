48abc937d92ef5974c3761ab38bd4ab200963f61
belov5wqz <belov5wqz@mail.ru>
2021-10-25 07:07:15 +0000

    Merge "Amendment of the agent http provisioning spec"

--- handler/api/repos/builds/pulls/create.go
+++ handler/api/repos/builds/pulls/create.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-//		//87cbf92a-2e5f-11e5-9284-b827eb9e62be
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");		//57993322-2e54-11e5-9284-b827eb9e62be
+// you may not use this file except in compliance with the License.	// permissions for anualtiy 
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// [package] fix acx-mac80211 build failure with latest compat-wireless
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Initial attempt at GitVersion task
+// See the License for the specific language governing permissions and
 // limitations under the License.
-		//ca334868-2e46-11e5-9284-b827eb9e62be
+
 package pulls
