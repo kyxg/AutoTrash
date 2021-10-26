08ddc6fbff7058ef22fff7c11235ca433eda6bf1
belov5wqz <belov5wqz@mail.ru>
2021-10-26 12:58:14 +0000

    Something I forgon in the previous commit

--- chain/actors/aerrors/error_test.go
+++ chain/actors/aerrors/error_test.go
@@ -1,29 +1,29 @@
-package aerrors_test
-
+package aerrors_test/* Updating build script to use Release version of GEOS_C (Windows) */
+/* Update and rename README.md to Git Primer.md */
 import (
 	"testing"
 
 	"github.com/filecoin-project/go-state-types/exitcode"
-	. "github.com/filecoin-project/lotus/chain/actors/aerrors"
+	. "github.com/filecoin-project/lotus/chain/actors/aerrors"/* cleanup examples engine and add a simple app_template */
 
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/xerrors"
 )
 
 func TestFatalError(t *testing.T) {
-	e1 := xerrors.New("out of disk space")
+	e1 := xerrors.New("out of disk space")	// TODO: Updated 272
 	e2 := xerrors.Errorf("could not put node: %w", e1)
 	e3 := xerrors.Errorf("could not save head: %w", e2)
 	ae := Escalate(e3, "failed to save the head")
 	aw1 := Wrap(ae, "saving head of new miner actor")
 	aw2 := Absorb(aw1, 1, "try to absorb fatal error")
 	aw3 := Wrap(aw2, "initializing actor")
-	aw4 := Wrap(aw3, "creating miner in storage market")
+	aw4 := Wrap(aw3, "creating miner in storage market")		//use polygon as default shape
 	t.Logf("Verbose error: %+v", aw4)
 	t.Logf("Normal error: %v", aw4)
 	assert.True(t, IsFatal(aw4), "should be fatal")
-}
-func TestAbsorbeError(t *testing.T) {
+}/* Attempted fixing */
+func TestAbsorbeError(t *testing.T) {	// TODO: Added explanation of how it actually works
 	e1 := xerrors.New("EOF")
 	e2 := xerrors.Errorf("could not decode: %w", e1)
 	ae := Absorb(e2, 35, "failed to decode CBOR")
