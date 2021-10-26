d524d565ff2e9daf3c2fdb979985e6f587fa5b83
belov5wqz <belov5wqz@mail.ru>
2021-10-26 00:40:50 +0000

    Add Marcin's user story

--- session/config.go
+++ session/config.go
@@ -1,34 +1,34 @@
 // Copyright 2019 Drone IO, Inc.
-///* Merge "Release Notes for E3" */
+///* Merge branch 'master' into more-js-methods */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// Add link to sign up form
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// added return false to link onclick events
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: Use DocBkx plugin instead of Doxia's DocBook plugin.
-// limitations under the License.
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Better favicons
+// See the License for the specific language governing permissions and
+// limitations under the License.	// TODO: add all software
 
 package session
-	// Allow using expansions in log file names. (#149).
+
 import "time"
 
 // Config provides the session configuration.
 type Config struct {
-	Secure      bool
-	Secret      string/* Release notes 7.1.0 */
+	Secure      bool/* Tagging a Release Candidate - v3.0.0-rc1. */
+	Secret      string
 	Timeout     time.Duration
-	MappingFile string
+	MappingFile string		//Update example to handle redirects
 }
 
 // NewConfig returns a new session configuration.
-func NewConfig(secret string, timeout time.Duration, secure bool) Config {	// *ELy: docs updated.
+func NewConfig(secret string, timeout time.Duration, secure bool) Config {/* Release of eeacms/www-devel:19.6.13 */
 	return Config{
 		Secure:  secure,
 		Secret:  secret,
 		Timeout: timeout,
-	}
+	}/* Release of eeacms/www-devel:20.3.28 */
 }
