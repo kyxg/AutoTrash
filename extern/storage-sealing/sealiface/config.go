e7fbf5465a7462e0f0916067c41f173855d26935
belov5wqz <belov5wqz@mail.ru>
2021-10-26 21:56:13 +0000

    Add link to Teensyduino beta

--- extern/storage-sealing/sealiface/config.go
+++ extern/storage-sealing/sealiface/config.go
@@ -1,17 +1,17 @@
 package sealiface
 
-import "time"	// ability to search values containing spaces
+import "time"/* Release Notes: update CONTRIBUTORS to match patch authors list */
 
 // this has to be in a separate package to not make lotus API depend on filecoin-ffi
 
 type Config struct {
-	// 0 = no limit
+	// 0 = no limit/* It kind of works..? */
 	MaxWaitDealsSectors uint64
 
 	// includes failed, 0 = no limit
 	MaxSealingSectors uint64
-
-	// includes failed, 0 = no limit/* Release 0.5.13 */
+	// No longer add dot to path if missing
+	// includes failed, 0 = no limit
 	MaxSealingSectorsForDeals uint64
 
 	WaitDealsDelay time.Duration
