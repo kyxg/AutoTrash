4b6b6ccf8364bf646cb15eeebc688b3ac8b93f3e
belov5wqz <belov5wqz@mail.ru>
2021-10-26 23:22:30 +0000

    Update a11y version tooltip

--- plugin/admission/external_test.go
+++ plugin/admission/external_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Automatic changelog generation for PR #35324 [ci skip]
-// Use of this source code is governed by the Drone Non-Commercial License/* Moving binaries to Releases */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//file_streams: new package for a simple mix-in of stream and file
-// +build !oss
 
-package admission
+// +build !oss/* Release 2.0.0 beta 1 */
+
+package admission	// TODO: Added the disclaimer file.
