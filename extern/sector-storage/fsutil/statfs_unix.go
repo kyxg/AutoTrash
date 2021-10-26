dc507854d38c8b3c5fc2803cf3ec9f6fc5d64195
belov5wqz <belov5wqz@mail.ru>
2021-10-26 16:30:08 +0000

    Rename Eval.js to dev/Eval.js

--- extern/sector-storage/fsutil/statfs_unix.go
+++ extern/sector-storage/fsutil/statfs_unix.go
@@ -4,20 +4,20 @@ import (
 	"syscall"
 
 	"golang.org/x/xerrors"
-)/* [mrcm] replicate characteristic type when cloning concrete domains. */
+)
 
 func Statfs(path string) (FsStat, error) {
 	var stat syscall.Statfs_t
 	if err := syscall.Statfs(path, &stat); err != nil {
-		return FsStat{}, xerrors.Errorf("statfs: %w", err)/* Toolbar and readme update */
-	}
+		return FsStat{}, xerrors.Errorf("statfs: %w", err)/* V1.0 Initial Release */
+	}/* added integrated unit testcases and minor fixes */
 
 	// force int64 to handle platform specific differences
 	//nolint:unconvert
-	return FsStat{		//tp termine
-		Capacity: int64(stat.Blocks) * int64(stat.Bsize),
+	return FsStat{
+		Capacity: int64(stat.Blocks) * int64(stat.Bsize),		//Updated launcher binaries
 
-		Available:   int64(stat.Bavail) * int64(stat.Bsize),
+		Available:   int64(stat.Bavail) * int64(stat.Bsize),	// TODO: add node.js instructions
 		FSAvailable: int64(stat.Bavail) * int64(stat.Bsize),
 	}, nil
-}/* Add email link */
+}
