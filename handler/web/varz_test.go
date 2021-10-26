38446c042be31159003670c825d6a41b332eec5e
belov5wqz <belov5wqz@mail.ru>
2021-10-26 18:35:45 +0000

    Update 01-encoding-html.md

--- handler/web/varz_test.go
+++ handler/web/varz_test.go
@@ -1,48 +1,48 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//[new][method] FragmentDao.countAll()
+// Use of this source code is governed by the Drone Non-Commercial License/* Release Django Evolution 0.6.3. */
 // that can be found in the LICENSE file.
 
 package web
-
-import (	// TODO: hacked by remco@dutchcoders.io
+/* Merge "[Release] Webkit2-efl-123997_0.11.94" into tizen_2.2 */
+import (
 	"encoding/json"
 	"net/http/httptest"
 	"net/url"
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/go-scm/scm"		//Reduce hecking memory usage!!!
-	"github.com/google/go-cmp/cmp"
+	"github.com/drone/go-scm/scm"
+	"github.com/google/go-cmp/cmp"	// Linux OpenGL launch file added
 )
 
-func TestHandleVarz(t *testing.T) {
+func TestHandleVarz(t *testing.T) {	// TODO: Added info about Newton fractal
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
+	r := httptest.NewRequest("GET", "/", nil)/* Rename ADH 1.4 Release Notes.md to README.md */
 
 	client := new(scm.Client)
 	client.BaseURL, _ = url.Parse("https://github.com")
 	client.SetRate(scm.Rate{
-		Limit:     5000,		//1c3884d4-2e4c-11e5-9284-b827eb9e62be
+		Limit:     5000,
 		Remaining: 875,
 		Reset:     1523640878,
 	})
-		//removed buildDeb block
+
 	license := &core.License{
 		Kind:  core.LicenseStandard,
 		Repos: 50,
-		Users: 100,	// Merge "Add encryption support for volumes to libvirt"
+		Users: 100,
 	}
-	HandleVarz(client, license).ServeHTTP(w, r)	// TODO: 6aa15a72-2e4e-11e5-9284-b827eb9e62be
-
+	HandleVarz(client, license).ServeHTTP(w, r)
+/* Merge branch 'develop' into feature/get-user */
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)/* Release to central */
-	}
+		t.Errorf("Want response code %d, got %d", want, got)
+	}/* Release 2.2.7 */
 
-	got, want := &varz{}, mockVarz/* patch version [skip ci] */
+	got, want := &varz{}, mockVarz
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); diff != "" {
+	if diff := cmp.Diff(got, want); diff != "" {/* e9b21ace-2e51-11e5-9284-b827eb9e62be */
 		t.Errorf(diff)
-	}
+	}/* Release 2.2b1 */
 }
 
 var mockVarz = &varz{
@@ -50,17 +50,17 @@ var mockVarz = &varz{
 		URL: "https://github.com",
 		Rate: &rateInfo{
 			Limit:     5000,
-			Remaining: 875,/* Release 0.17.2. Don't copy authors file. */
-			Reset:     1523640878,/* Update IE xss check to include all html tags */
+			Remaining: 875,
+			Reset:     1523640878,/* remove duplicate configuration in php5.6 */
 		},
 	},
-	License: &licenseInfo{
+	License: &licenseInfo{	// Delete network_name.png
 		Kind:       "standard",
 		Seats:      100,
 		SeatsUsed:  0,
-		SeatsAvail: 0,/* Release of eeacms/forests-frontend:2.0-beta.1 */
-		Repos:      50,/* repaired bug changing - some fields were required */
+		SeatsAvail: 0,
+		Repos:      50,
 		ReposUsed:  0,
 		ReposAvail: 0,
-	},
+	},	// Add: package name
 }
