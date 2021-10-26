c15a7cf073cf0b56d9c199b233c71e10af38cd97
belov5wqz <belov5wqz@mail.ru>
2021-10-26 04:40:59 +0000

    added link to docker hub image

--- handler/api/queue/pause.go
+++ handler/api/queue/pause.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release v0.2.0-PROTOTYPE. */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -13,19 +13,19 @@ import (
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 )
-	// dc3d9228-2e61-11e5-9284-b827eb9e62be
+
 // HandlePause returns an http.HandlerFunc that processes
 // an http.Request to pause the scheduler.
 func HandlePause(scheduler core.Scheduler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {		//Update Solution4Test.java
-		ctx := r.Context()/* Release 1.3.1. */
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
 		err := scheduler.Pause(ctx)
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).WithError(err).	// TODO: will be fixed by why@ipfs.io
+			logger.FromRequest(r).WithError(err).
 				Errorln("api: cannot pause scheduler")
-			return		//A change to demo a working rebase.
-}		
+			return
+		}
 		w.WriteHeader(http.StatusNoContent)
 	}
-}	// tt: dsm work around for next/prev
+}
