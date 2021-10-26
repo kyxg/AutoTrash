23132bbf911ecbc07461f7d6ed038ffee1782451
belov5wqz <belov5wqz@mail.ru>
2021-10-26 09:09:38 +0000

    5fa41efa-2e50-11e5-9284-b827eb9e62be

--- handler/api/builds/builds_test.go
+++ handler/api/builds/builds_test.go
@@ -1,70 +1,70 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// TODO: hacked by indexxuan@gmail.com
 // +build !oss
 
 package builds
 
 import (
 	"encoding/json"
-	"io/ioutil"
+	"io/ioutil"	// TODO: will be fixed by nicksavers@gmail.com
 	"net/http/httptest"
-	"testing"
+	"testing"/* small fix to docs examples */
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/handler/api/errors"/* Created include markdown-panel-12.md */
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-	"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"	// TODO: will be fixed by joshua@yottadb.com
 )
 
 func init() {
 	logrus.SetOutput(ioutil.Discard)
 }
 
-func TestHandleBuilds(t *testing.T) {
+func TestHandleBuilds(t *testing.T) {/* fix compile for for STLport 5.1.3 and MSVC 6 SP5 */
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+/* Adds PMD and FindBugs reports. */
 	want := []*core.Repository{
-		{ID: 1, Slug: "octocat/hello-world"},
+		{ID: 1, Slug: "octocat/hello-world"},		//Delete SecScanQR.iml
 		{ID: 2, Slug: "octocat/spoon-fork"},
-	}
+	}/* f86341b8-2e67-11e5-9284-b827eb9e62be */
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().ListIncomplete(gomock.Any()).Return(want, nil)
-
+	repos.EXPECT().ListIncomplete(gomock.Any()).Return(want, nil)	// TODO: will be fixed by hello@brooklynzelenka.com
+/* Create _statusscreen.h */
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 
 	HandleIncomplete(repos)(w, r)
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}
+	}	// TODO: Better comment about no test IP6 addresses for "FILTER_FLAG_NO_RES_RANGE"
 
 	got := []*core.Repository{}
-	json.NewDecoder(w.Body).Decode(&got)
+	json.NewDecoder(w.Body).Decode(&got)/* Release 2.17 */
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
 }
 
 func TestHandleBuilds_Error(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* Renamed 'Release' folder to fit in our guidelines. */
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().ListIncomplete(gomock.Any()).Return(nil, errors.ErrNotFound)
 
-	w := httptest.NewRecorder()
+	w := httptest.NewRecorder()/* Re #29032 Release notes */
 	r := httptest.NewRequest("GET", "/", nil)
 
 	HandleIncomplete(repos)(w, r)
 	if got, want := w.Code, 500; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
+		t.Errorf("Want response code %d, got %d", want, got)		//Refactors admin controller
 	}
 
 	got, want := &errors.Error{}, errors.ErrNotFound
