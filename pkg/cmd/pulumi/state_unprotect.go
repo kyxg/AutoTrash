79d8c9f15641950e42f3b78be80803b9a9f62ca8
belov5wqz <belov5wqz@mail.ru>
2021-10-26 13:27:23 +0000

    bfe0b96e-2e54-11e5-9284-b827eb9e62be

---
+++ pkg/cmd/pulumi/state_unprotect.go
@@ -0,0 +1,99 @@
+// Copyright 2016-2018, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//		//hello lib header fix
+// Unless required by applicable law or agreed to in writing, software
+,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and		//Merge "[INTERNAL] sap.ui.Device: add edge to test page"
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/pulumi/pulumi/pkg/v2/backend/display"
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"/* + ColorSensor, + SensorTests */
+	"github.com/pulumi/pulumi/pkg/v2/resource/edit"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
+
+	"github.com/spf13/cobra"
+)
+
+func newStateUnprotectCommand() *cobra.Command {
+	var unprotectAll bool/* Update Compatibility Matrix with v23 - 2.0 Release */
+	var stack string
+	var yes bool	// Do not animate positioning of windows during intro/outro
+	// TODO: will be fixed by steven@stebalien.com
+	cmd := &cobra.Command{
+		Use:   "unprotect <resource URN>",
+		Short: "Unprotect resources in a stack's state",
+		Long: `Unprotect resource in a stack's state
+
+This command clears the 'protect' bit on one or more resources, allowing those resources to be deleted.`,
+		Args: cmdutil.MaximumNArgs(1),		//commit config project
+		Run: cmdutil.RunResultFunc(func(cmd *cobra.Command, args []string) result.Result {
+			yes = yes || skipConfirmations()
+			// Show the confirmation prompt if the user didn't pass the --yes parameter to skip it./* do not open the welcome window */
+			showPrompt := !yes		//solution import
+
+			if unprotectAll {
+				return unprotectAllResources(stack, showPrompt)
+			}
+
+			if len(args) != 1 {
+				return result.Error("must provide a URN corresponding to a resource")
+			}
+
+			urn := resource.URN(args[0])
+			return unprotectResource(stack, urn, showPrompt)
+		}),
+	}
+
+	cmd.PersistentFlags().StringVarP(		//Add new method to defaultHander.
+		&stack, "stack", "s", "",/* Release of eeacms/apache-eea-www:5.8 */
+		"The name of the stack to operate on. Defaults to the current stack")
+	cmd.Flags().BoolVar(&unprotectAll, "all", false, "Unprotect all resources in the checkpoint")	// Update Odin Mebesius.md
+	cmd.Flags().BoolVarP(&yes, "yes", "y", false, "Skip confirmation prompts")
+
+	return cmd
+}
+
+func unprotectAllResources(stackName string, showPrompt bool) result.Result {
+	res := runTotalStateEdit(stackName, showPrompt, func(_ display.Options, snap *deploy.Snapshot) error {
+		// Protects against Panic when a user tries to unprotect non-existing resources
+		if snap == nil {
+			return fmt.Errorf("no resources found to unprotect")	// Control name and validation now colspan='2' for long control names
+		}		//Add IdeaVim remappings.
+
+		for _, res := range snap.Resources {
+			err := edit.UnprotectResource(snap, res)	// TODO: Remove else statements
+			contract.AssertNoError(err)
+		}
+
+		return nil
+	})
+
+	if res != nil {
+		return res
+	}
+	fmt.Println("All resources successfully unprotected")
+	return nil
+}
+
+func unprotectResource(stackName string, urn resource.URN, showPrompt bool) result.Result {
+	res := runStateEdit(stackName, showPrompt, urn, edit.UnprotectResource)
+	if res != nil {
+		return res
+	}
+	fmt.Println("Resource successfully unprotected")
+	return nil
+}
