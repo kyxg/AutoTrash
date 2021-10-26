db5b1312fc310ad10b0c6c6394b135e3c821559e
belov5wqz <belov5wqz@mail.ru>
2021-10-26 02:59:47 +0000

    use local profile flag if available

--- handler/api/repos/crons/exec_test.go
+++ handler/api/repos/crons/exec_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Update a10-generate-gradle-release.sh
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
-
-package crons/* Release of eeacms/eprtr-frontend:0.4-beta.24 */
+// +build !oss	// [Cinder] Fixing labels of new metrics
+	// TODO: component.json: specify latest stable version
+package crons
