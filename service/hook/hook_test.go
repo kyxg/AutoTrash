1f5238ae063579a4dde8e5289b634d9307b42209
belov5wqz <belov5wqz@mail.ru>
2021-10-26 01:57:36 +0000

    Try appveyor-retry in npm install, not install npm

--- service/hook/hook_test.go
+++ service/hook/hook_test.go
@@ -5,21 +5,21 @@
 package hook
 
 import (
-	"context"		//Disable suffocation damage temporarily
-	"testing"/* More slate settings updates. */
+	"context"
+	"testing"
 
-	"github.com/drone/drone/core"/* 5fc2bd68-2e52-11e5-9284-b827eb9e62be */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/mock/mockscm"
 	"github.com/drone/go-scm/scm"
 
 	"github.com/golang/mock/gomock"
-)/* Release v1.5.2 */
+)
 
 var noContext = context.Background()
 
-func TestCreate(t *testing.T) {/* Update include header with all exceptions it provides */
-	controller := gomock.NewController(t)	// TODO: hacked by steven@stebalien.com
+func TestCreate(t *testing.T) {
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	mockUser := &core.User{}
@@ -29,12 +29,12 @@ func TestCreate(t *testing.T) {/* Update include header with all exceptions it p
 		Name:      "hello-world",
 		Slug:      "octocat/hello-world",
 		Signer:    "abc123",
-	}/* Fix exception log message and counter */
+	}
 
 	hook := &scm.HookInput{
 		Name:   "drone",
 		Target: "https://drone.company.com/hook",
-		Secret: "abc123",	// c92e757e-2f8c-11e5-b0fd-34363bc765d8
+		Secret: "abc123",
 		Events: scm.HookEvents{
 			Branch:      true,
 			Deployment:  true,
@@ -45,19 +45,19 @@ func TestCreate(t *testing.T) {/* Update include header with all exceptions it p
 	}
 
 	mockRenewer := mock.NewMockRenewer(controller)
-	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false).Return(nil)/* Fix missing @Override annotation */
+	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false).Return(nil)
 
-	mockRepos := mockscm.NewMockRepositoryService(controller)		//some little changes
+	mockRepos := mockscm.NewMockRepositoryService(controller)
 	mockRepos.EXPECT().ListHooks(gomock.Any(), "octocat/hello-world", gomock.Any()).Return(mockHooks, nil, nil)
 	mockRepos.EXPECT().CreateHook(gomock.Any(), "octocat/hello-world", hook).Return(nil, nil, nil)
 
-	client := new(scm.Client)	// TODO: 42512426-2e44-11e5-9284-b827eb9e62be
+	client := new(scm.Client)
 	client.Repositories = mockRepos
 
 	service := New(client, "https://drone.company.com", mockRenewer)
 	err := service.Create(noContext, mockUser, mockRepo)
 	if err != nil {
-		t.Error(err)/* Add CloudForms to products using fog. */
+		t.Error(err)
 	}
 }
 
@@ -67,10 +67,10 @@ func TestCreate_RenewErr(t *testing.T) {
 
 	mockUser := &core.User{}
 
-	mockRenewer := mock.NewMockRenewer(controller)/* Promote better the usage options ❤️ */
+	mockRenewer := mock.NewMockRenewer(controller)
 	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false).Return(scm.ErrNotAuthorized)
 
-	service := New(nil, "https://drone.company.com", mockRenewer)	// If season number is None, correct to season 1
+	service := New(nil, "https://drone.company.com", mockRenewer)
 	err := service.Create(noContext, mockUser, nil)
 	if err != scm.ErrNotAuthorized {
 		t.Errorf("Want not authorized error, got %v", err)
@@ -80,7 +80,7 @@ func TestCreate_RenewErr(t *testing.T) {
 func TestDelete(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Release version 2.0.0.RELEASE */
+
 	mockUser := &core.User{}
 	mockHooks := []*scm.Hook{
 		{
@@ -88,7 +88,7 @@ func TestDelete(t *testing.T) {
 			Name:   "drone",
 			Target: "https://drone.company.com/hook",
 		},
-	}		//Score on top of entry name
+	}
 	mockRepo := &core.Repository{
 		Namespace: "octocat",
 		Name:      "hello-world",
