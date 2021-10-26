e036198a3f85b34d07738f891bf79a6d7f4f98e9
belov5wqz <belov5wqz@mail.ru>
2021-10-26 01:52:24 +0000

    add threading for scan

--- core/build_test.go
+++ core/build_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Removed Rpy dependency from regression and rlm. */
-// that can be found in the LICENSE file.	// more shizz
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: More fun with remote cameras and handling disconnection
+// that can be found in the LICENSE file.
 
 // +build !oss
-
+/* Release 0.8.3 Alpha */
 package core
