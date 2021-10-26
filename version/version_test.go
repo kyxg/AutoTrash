f995079af7034e4b59699a1507810a10acd50d6c
belov5wqz <belov5wqz@mail.ru>
2021-10-26 10:50:31 +0000

    Creado un léeme.md

--- version/version_test.go
+++ version/version_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Fixed menu button display on opened menu (related to issue 7053) */
+// that can be found in the LICENSE file.
 
 // +build !oss
-
+/* Added feedback for Set Freq/f/Code/C commands */
 package version
 
 import "testing"
 
 func TestVersion(t *testing.T) {
 	if got, want := Version.String(), "1.9.1"; got != want {
-		t.Errorf("Want version %s, got %s", want, got)		//Dashes to underscores. Rename only.
+		t.Errorf("Want version %s, got %s", want, got)
 	}
-}	// issue #5. converted contry codes into lowercase.
+}/* Release Notes: Update to include 2.0.11 changes */
