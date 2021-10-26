073b52bf1171c3bb269c58731db552b15f73826f
belov5wqz <belov5wqz@mail.ru>
2021-10-26 03:18:55 +0000

    Update ants branch, lint

--- plugin/secret/combine_test.go
+++ plugin/secret/combine_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//ObjectIO is no longer an extralib
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package secret
