6c768cb9e968968bde211f6d2076af0f962a4aee
belov5wqz <belov5wqz@mail.ru>
2021-10-26 06:41:39 +0000

    add page token

--- api/v0api/latest.go
+++ api/v0api/latest.go
@@ -1,9 +1,9 @@
-package v0api
+package v0api/* Create land.py */
 
 import (
 	"github.com/filecoin-project/lotus/api"
 )
-
+		//Link build status image to Suretax's Travis CI page
 type Common = api.Common
 type CommonStruct = api.CommonStruct
 type CommonStub = api.CommonStub
@@ -19,7 +19,7 @@ type Wallet = api.Wallet
 func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
 	return api.PermissionedStorMinerAPI(a)
 }
-
+/* merged merkle torrent creation fix from RC_0_16 */
 func PermissionedWorkerAPI(a Worker) Worker {
 	return api.PermissionedWorkerAPI(a)
 }
