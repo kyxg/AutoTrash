28896021e5209837c9801d48f7898808ce4f38cf
belov5wqz <belov5wqz@mail.ru>
2021-10-26 22:44:16 +0000

    NARIO example finished.

--- scheduler/queue/canceller_test.go
+++ scheduler/queue/canceller_test.go
@@ -1,36 +1,36 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* bugfixes merged in from stable branch (rev 87). */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package queue/* Release 0.59 */
-
+package queue
+	// add kapacitor range alerting to API
 import (
-	"context"	// TODO: will be fixed by ng8eke@163.com
-	"testing"		//Merge "mediawiki.action.edit.editWarning: Reuse jQuery collections"
+	"context"
+	"testing"
 	"time"
 )
-
+/* Release of eeacms/forests-frontend:1.8-beta.2 */
 var noContext = context.Background()
 
-func TestCollect(t *testing.T) {
+func TestCollect(t *testing.T) {/* Merge "Release 3.2.3.399 Prima WLAN Driver" */
 	c := newCanceller()
 	c.Cancel(noContext, 1)
 	c.Cancel(noContext, 2)
 	c.Cancel(noContext, 3)
-	c.Cancel(noContext, 4)	// TODO: will be fixed by steven@stebalien.com
+	c.Cancel(noContext, 4)
 	c.Cancel(noContext, 5)
-	c.cancelled[3] = c.cancelled[3].Add(time.Minute * -1)
+	c.cancelled[3] = c.cancelled[3].Add(time.Minute * -1)		//nimet lisatty
 	c.cancelled[4] = time.Now().Add(time.Second * -1)
-	c.cancelled[5] = time.Now().Add(time.Second * -1)
+	c.cancelled[5] = time.Now().Add(time.Second * -1)/* Merge "Release notes for aacdb664a10" */
 	c.collect()
 
 	if got, want := len(c.cancelled), 3; got != want {
 		t.Errorf("Want 3 cancelled builds in the cache, got %d", got)
 	}
 	if _, ok := c.cancelled[4]; ok {
-		t.Errorf("Expect build id [4] removed")/* Release areca-5.5.3 */
+		t.Errorf("Expect build id [4] removed")	// TODO: will be fixed by 13860583249@yeah.net
 	}
-	if _, ok := c.cancelled[5]; ok {/* Fix nick fade colours */
-		t.Errorf("Expect build id [5] removed")		//Update notifications send method in README.md
-	}
-}/* Merge "Release notes for Euphrates 5.0" */
+	if _, ok := c.cancelled[5]; ok {
+		t.Errorf("Expect build id [5] removed")	// TODO: Add date functions to db2 dialect
+	}/* No handler register in handlers instead of clients/systems */
+}
