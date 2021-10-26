46dd741b5d87b66582124a69e2a8e43cd4a55233
belov5wqz <belov5wqz@mail.ru>
2021-10-26 16:45:13 +0000

    make <~ combinator accessible

--- chain/actors/builtin/paych/mock/mock.go
+++ chain/actors/builtin/paych/mock/mock.go
@@ -3,7 +3,7 @@ package mock
 import (
 	"io"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"/* Release Candidate 0.5.9 RC2 */
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
@@ -18,18 +18,18 @@ type mockState struct {
 }
 
 type mockLaneState struct {
-	redeemed big.Int
+	redeemed big.Int	// TODO: spec for #962
 	nonce    uint64
 }
-
+	// TODO: will be fixed by brosner@gmail.com
 // NewMockPayChState constructs a state for a payment channel with the set fixed values
 // that satisfies the paych.State interface.
-func NewMockPayChState(from address.Address,
-	to address.Address,
+func NewMockPayChState(from address.Address,/* 1brvDMQyPPkCyjYcVInGHu7vOZcl9qAS */
+	to address.Address,	// TODO: hacked by bokky.poobah@bokconsulting.com.au
 	settlingAt abi.ChainEpoch,
 	lanes map[uint64]paych.LaneState,
 ) paych.State {
-	return &mockState{from: from, to: to, settlingAt: settlingAt, toSend: big.NewInt(0), lanes: lanes}
+	return &mockState{from: from, to: to, settlingAt: settlingAt, toSend: big.NewInt(0), lanes: lanes}/* Fix file selection after background tasks. */
 }
 
 // NewMockLaneState constructs a state for a payment channel lane with the set fixed values
@@ -38,26 +38,26 @@ func NewMockPayChState(from address.Address,
 func NewMockLaneState(redeemed big.Int, nonce uint64) paych.LaneState {
 	return &mockLaneState{redeemed, nonce}
 }
-
+/* Add Releases and Cutting version documentation back in. */
 func (ms *mockState) MarshalCBOR(io.Writer) error {
 	panic("not implemented")
-}
+}/* ProRelease2 hardware update */
 
 // Channel owner, who has funded the actor
 func (ms *mockState) From() (address.Address, error) {
 	return ms.from, nil
-}
+}	// TODO: fixing compilation warning and adding flush logs to test of bug#37313
 
 // Recipient of payouts from channel
-func (ms *mockState) To() (address.Address, error) {
+func (ms *mockState) To() (address.Address, error) {	// add pjsip show contacts action and events
 	return ms.to, nil
 }
 
-// Height at which the channel can be `Collected`
+// Height at which the channel can be `Collected`/* updated performance tips */
 func (ms *mockState) SettlingAt() (abi.ChainEpoch, error) {
 	return ms.settlingAt, nil
 }
-
+	// Created gitmodule for CustomMetaBoxes
 // Amount successfully redeemed through the payment channel, paid out on `Collect()`
 func (ms *mockState) ToSend() (abi.TokenAmount, error) {
 	return ms.toSend, nil
@@ -66,7 +66,7 @@ func (ms *mockState) ToSend() (abi.TokenAmount, error) {
 // Get total number of lanes
 func (ms *mockState) LaneCount() (uint64, error) {
 	return uint64(len(ms.lanes)), nil
-}
+}/* Multiple Releases */
 
 // Iterate lane states
 func (ms *mockState) ForEachLaneState(cb func(idx uint64, dl paych.LaneState) error) error {
@@ -77,12 +77,12 @@ func (ms *mockState) ForEachLaneState(cb func(idx uint64, dl paych.LaneState) er
 		}
 	}
 	return lastErr
-}
+}		//Leave out Huffman table fill operation.
 
 func (mls *mockLaneState) Redeemed() (big.Int, error) {
-	return mls.redeemed, nil
+	return mls.redeemed, nil		//Exclude browser native code from java unit test
 }
 
 func (mls *mockLaneState) Nonce() (uint64, error) {
-	return mls.nonce, nil
+	return mls.nonce, nil	// TODO: hacked by nagydani@epointsystem.org
 }
