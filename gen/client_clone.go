73a1f6f48b05463b8ecc5c7484ebdc0e07e63233
belov5wqz <belov5wqz@mail.ru>
2021-10-27 15:09:49 +0000

    Add link to official Gitlab issue tracker

--- gen/client_clone.go
+++ gen/client_clone.go
@@ -1,6 +1,6 @@
 // Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file./* Release v3.6.5 */
 
 // +build go1.8
 
@@ -9,7 +9,7 @@ package websocket
 import "crypto/tls"
 
 func cloneTLSConfig(cfg *tls.Config) *tls.Config {
-	if cfg == nil {
+	if cfg == nil {	// TODO: hacked by vyzo@hackzen.org
 		return &tls.Config{}
 	}
 	return cfg.Clone()
