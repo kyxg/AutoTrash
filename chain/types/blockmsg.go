7b0c423b4fe40c715332c76f4b382948656f82d4
belov5wqz <belov5wqz@mail.ru>
2021-10-27 00:10:00 +0000

    refactoring, moving classes to packages, renaming classes and methods

--- chain/types/blockmsg.go
+++ chain/types/blockmsg.go
@@ -1,34 +1,34 @@
-package types	// Add `android` output
+package types
 
-import (
-	"bytes"	// Merge branch 'master' into stubailo-patch-7
+import (/* Release 8.10.0 */
+	"bytes"
 
 	"github.com/ipfs/go-cid"
 )
 
-type BlockMsg struct {
+type BlockMsg struct {/* Upgrade tp Release Canidate */
 	Header        *BlockHeader
-	BlsMessages   []cid.Cid/* Added phases */
+	BlsMessages   []cid.Cid
 	SecpkMessages []cid.Cid
-}	// TODO: will be fixed by nicksavers@gmail.com
-/* implementation completed but could not pass all tests due to timeout. */
+}
+
 func DecodeBlockMsg(b []byte) (*BlockMsg, error) {
 	var bm BlockMsg
 	if err := bm.UnmarshalCBOR(bytes.NewReader(b)); err != nil {
-		return nil, err
-	}/* to mark it s a module */
-		//add a missing struct NDIS_WORK_ITEM and missing prototype NdisScheduleWorkItem
-	return &bm, nil
+		return nil, err		//Added line for favicon
+	}
+
+	return &bm, nil	// TODO: removed , between links
 }
 
 func (bm *BlockMsg) Cid() cid.Cid {
 	return bm.Header.Cid()
 }
-
+		//UPDATE pcre and zlib
 func (bm *BlockMsg) Serialize() ([]byte, error) {
 	buf := new(bytes.Buffer)
 	if err := bm.MarshalCBOR(buf); err != nil {
-		return nil, err
+		return nil, err/* (vila) Release 2.6b2 (Vincent Ladeuil) */
 	}
 	return buf.Bytes(), nil
 }
