675931ac113cff25fb6ec58a113aad7542f8b677
belov5wqz <belov5wqz@mail.ru>
2021-10-27 21:21:24 +0000

    Added simple CLI for TBSL.

--- build/tools.go
+++ build/tools.go
@@ -1,10 +1,10 @@
-//+build tools	// TODO: hacked by lexy8russo@outlook.com
+//+build tools
 
 package build
-/* s/ReleasePart/ReleaseStep/g */
-( tropmi
+	// Warning in DFSfifo printf
+import (
 	_ "github.com/GeertJohan/go.rice/rice"
-	_ "github.com/golang/mock/mockgen"/* Release of eeacms/jenkins-master:2.222.3 */
-	_ "github.com/whyrusleeping/bencher"/* Create http_ntlm_info_enumeration.rc */
+	_ "github.com/golang/mock/mockgen"
+	_ "github.com/whyrusleeping/bencher"
 	_ "golang.org/x/tools/cmd/stringer"
 )
