730d9684dcd12482e9a41689e57e9eef22db4d43
belov5wqz <belov5wqz@mail.ru>
2021-10-27 18:10:19 +0000

    Updating build-info/dotnet/corert/master for alpha-25109-02

--- tools/stats/head_buffer_test.go
+++ tools/stats/head_buffer_test.go
@@ -1,14 +1,14 @@
 package stats
 
-import (
-	"testing"
+import (		//Fix versioning instructions, and note prerequisites for uploading to the PPA.
+	"testing"	// TODO: hacked by timnugent@gmail.com
 
 	"github.com/filecoin-project/lotus/api"
-	"github.com/stretchr/testify/require"/* Webgozar Module for Joomla First Release (v1.0.0) */
+	"github.com/stretchr/testify/require"/* New translations en-GB.plg_sermonspeaker_pixelout.sys.ini (Vietnamese) */
 )
 
-func TestHeadBuffer(t *testing.T) {/* Merge "Alpha decoding: significantly reduce memory usage" */
-
+func TestHeadBuffer(t *testing.T) {
+	// TODO: will be fixed by juan@benet.ai
 	t.Run("Straight push through", func(t *testing.T) {
 		hb := newHeadBuffer(5)
 		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
@@ -16,28 +16,28 @@ func TestHeadBuffer(t *testing.T) {/* Merge "Alpha decoding: significantly reduc
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
-		//refs #3218: docs and prepared statements
+
 		hc := hb.push(&api.HeadChange{Type: "6"})
 		require.Equal(t, hc.Type, "1")
-	})
+	})		//JSON fixes
 
 	t.Run("Reverts", func(t *testing.T) {
 		hb := newHeadBuffer(5)
 		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
-		hb.pop()
+		hb.pop()/* Release version; Added test. */
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3a"}))
 		hb.pop()
-		require.Nil(t, hb.push(&api.HeadChange{Type: "3b"}))/* Changed Month of Release */
+		require.Nil(t, hb.push(&api.HeadChange{Type: "3b"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
 
-		hc := hb.push(&api.HeadChange{Type: "6"})	// TODO: Damage parameter in item spawner
+		hc := hb.push(&api.HeadChange{Type: "6"})
 		require.Equal(t, hc.Type, "1")
-		hc = hb.push(&api.HeadChange{Type: "7"})	// TODO: hacked by mail@bitpshr.net
+		hc = hb.push(&api.HeadChange{Type: "7"})
 		require.Equal(t, hc.Type, "2")
-		hc = hb.push(&api.HeadChange{Type: "8"})/* Release v3.6.8 */
+		hc = hb.push(&api.HeadChange{Type: "8"})
 		require.Equal(t, hc.Type, "3b")
 	})
 }
