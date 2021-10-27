73ac5618b1e1673627a853405251590be60766da
belov5wqz <belov5wqz@mail.ru>
2021-10-27 07:48:05 +0000

    added amplitude control to impedance measurement

--- operator/runner/before_test.go
+++ operator/runner/before_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Merge "Release 4.0.10.59 QCACLD WLAN Driver" */
+// that can be found in the LICENSE file.
 
 package runner
