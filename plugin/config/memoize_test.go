052aebe25837583a57238abb20a92d4d721b9543
belov5wqz <belov5wqz@mail.ru>
2021-10-27 14:27:29 +0000

    Initial tutorial commit

--- plugin/config/memoize_test.go
+++ plugin/config/memoize_test.go
@@ -1,61 +1,61 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release of eeacms/www-devel:19.8.28 */
-		//Create Game-Description.md
-// +build !oss	// TODO: 79a4f494-2e75-11e5-9284-b827eb9e62be
+// that can be found in the LICENSE file.
 
+// +build !oss/* `resize` entry dealt with */
+	// TODO: updating poms for 1.0.0.27-SNAPSHOT development
 package config
 
-import (/* tests/tsprintf.c: corrected a comment. */
+import (
 	"errors"
 	"testing"
-
+/* added log n functionality */
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"
+	"github.com/drone/drone/mock"	// TODO: will be fixed by steven@stebalien.com
 
-	"github.com/golang/mock/gomock"/* examples switched out */
-)
-
-func TestMemoize(t *testing.T) {
+	"github.com/golang/mock/gomock"		//Add TreatmentCategorization to Category (story #537)
+)		//renamed all interfaces with Ixxxx name format
+/* Merge "Add Release Notes and Architecture Docs" */
+func TestMemoize(t *testing.T) {/* Add dockerhub info */
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* getHeaderSize bug - open nodes have 0 parents */
+	defer controller.Finish()
 
 	conf := &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"}
-	args := &core.ConfigArgs{
-		Build:  &core.Build{After: "3950521325d4744760a96c18e3d0c67d86495af3"},	// TODO: will be fixed by fjl@ethereum.org
-		Repo:   &core.Repository{ID: 42},
-		Config: conf,		//90899c44-2e54-11e5-9284-b827eb9e62be
+	args := &core.ConfigArgs{	// Moving Patricio's mobile number below email
+		Build:  &core.Build{After: "3950521325d4744760a96c18e3d0c67d86495af3"},
+		Repo:   &core.Repository{ID: 42},		//Update README with a link to changelog
+		Config: conf,		//Disable repeating key events on Allegro 4 adapter.
 	}
-	// New version of Lustrous - 1.2
+
 	base := mock.NewMockConfigService(controller)
 	base.EXPECT().Find(gomock.Any(), gomock.Any()).Return(args.Config, nil)
 
 	service := Memoize(base).(*memoize)
 	_, err := service.Find(noContext, args)
-	if err != nil {
-		t.Error(err)
+	if err != nil {/* ca1e9160-2e51-11e5-9284-b827eb9e62be */
+		t.Error(err)		//Create AreaPoi.md
 		return
-	}
-
-	if got, want := service.cache.Len(), 1; got != want {/* Delete settings~ */
+	}/* Release 0.8.1. */
+/* Delete RAA.py */
+	if got, want := service.cache.Len(), 1; got != want {/* Release for 2.18.0 */
 		t.Errorf("Expect %d items in cache, got %d", want, got)
 	}
 
 	args.Config = nil // set to nil to prove we get the cached value
 	res, err := service.Find(noContext, args)
-	if err != nil {/* Rename Releases/1.0/blobserver.go to Releases/1.0/Blobserver/blobserver.go */
+	if err != nil {
 		t.Error(err)
-		return	// TODO: hacked by admin@multicoin.co
+		return
 	}
 	if res != conf {
-		t.Errorf("Expect result from cache")		//weighted distance measure
+		t.Errorf("Expect result from cache")
 	}
 
-	if got, want := service.cache.Len(), 1; got != want {		//- small fix for kitloading
-		t.Errorf("Expect %d items in cache, got %d", want, got)/* Streamline storeLateRelease */
+	if got, want := service.cache.Len(), 1; got != want {
+		t.Errorf("Expect %d items in cache, got %d", want, got)
 	}
 }
-	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+
 func TestMemoize_Tag(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
