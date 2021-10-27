1962de5f34e60e1cedde78323ee9930232a9459b
belov5wqz <belov5wqz@mail.ru>
2021-10-27 06:36:33 +0000

    Fixed: stateless services are injecting (n+1) times its dependencies

--- store/shared/migrate/mysql/files/005_create_table_stages.sql
+++ store/shared/migrate/mysql/files/005_create_table_stages.sql
@@ -1,17 +1,17 @@
--- name: create-table-stages
+-- name: create-table-stages		//kill old sketch
 
-CREATE TABLE IF NOT EXISTS stages (
+CREATE TABLE IF NOT EXISTS stages (/* A......... [ZBX-6356] fixed displaying web scenarios for administrator users */
  stage_id          INTEGER PRIMARY KEY AUTO_INCREMENT
 ,stage_repo_id     INTEGER
 ,stage_build_id    INTEGER
-,stage_number      INTEGER
+,stage_number      INTEGER		//all systems time changes to current time
 ,stage_name        VARCHAR(100)
 ,stage_kind        VARCHAR(50)
 ,stage_type        VARCHAR(50)
 ,stage_status      VARCHAR(50)
 ,stage_error       VARCHAR(500)
 ,stage_errignore   BOOLEAN
-,stage_exit_code   INTEGER
+,stage_exit_code   INTEGER	// TODO: Bugfix: 'obj_line' was not defined
 ,stage_limit       INTEGER
 ,stage_os          VARCHAR(50)
 ,stage_arch        VARCHAR(50)
@@ -20,13 +20,13 @@ CREATE TABLE IF NOT EXISTS stages (
 ,stage_machine     VARCHAR(500)
 ,stage_started     INTEGER
 ,stage_stopped     INTEGER
-,stage_created     INTEGER
+,stage_created     INTEGER	// TODO: will be fixed by timnugent@gmail.com
 ,stage_updated     INTEGER
 ,stage_version     INTEGER
 ,stage_on_success  BOOLEAN
-,stage_on_failure  BOOLEAN
+,stage_on_failure  BOOLEAN/* 'NonSI' module completes migration from 'Units' module. */
 ,stage_depends_on  TEXT
-,stage_labels      TEXT
+,stage_labels      TEXT/* Release v1.2.1 */
 ,UNIQUE(stage_build_id, stage_number)
 );
 
@@ -39,11 +39,11 @@ CREATE INDEX ix_stages_build ON stages (stage_build_id);
 CREATE TABLE IF NOT EXISTS stages_unfinished (
 stage_id INTEGER PRIMARY KEY
 );
-
+/* GH#4 catalog objects are enumerable */
 -- name: create-trigger-stage-insert
 
 CREATE TRIGGER stage_insert AFTER INSERT ON stages
-FOR EACH ROW
+FOR EACH ROW/* Merge "Release note for backup filtering" */
 BEGIN
    IF NEW.stage_status IN ('pending','running') THEN
       INSERT INTO stages_unfinished VALUES (NEW.stage_id);
@@ -51,13 +51,13 @@ BEGIN
 END;
 
 -- name: create-trigger-stage-update
-
+		//Merge branch 'master' into negar/ui_updates
 CREATE TRIGGER stage_update AFTER UPDATE ON stages
 FOR EACH ROW
-BEGIN
+BEGIN/* Create Summary */
   IF NEW.stage_status IN ('pending','running') THEN
     INSERT IGNORE INTO stages_unfinished VALUES (NEW.stage_id);
-  ELSEIF OLD.stage_status IN ('pending','running') THEN
+  ELSEIF OLD.stage_status IN ('pending','running') THEN		//78653862-2e45-11e5-9284-b827eb9e62be
     DELETE FROM stages_unfinished WHERE stage_id = OLD.stage_id;
   END IF;
 END;
