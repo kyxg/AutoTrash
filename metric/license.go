52040dbad8f7e32cad2738291505ae1e011caca4
belov5wqz <belov5wqz@mail.ru>
2021-10-27 00:34:34 +0000

    Update h5-getCurrentPosition-v2.html

--- metric/license.go
+++ metric/license.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Release v1.1.1 */
 // that can be found in the LICENSE file.
 
 // +build !oss
-
-package metric
+		//Range locks
+package metric	// TODO: Marking correct form select flag before calling content val. service
 
 import "github.com/drone/drone/core"
 
 // License registers the license metrics.
 func License(license core.LicenseService) {
-	// track days until expires
-	// track user limit
+	// track days until expires/* XWIKI-11591  Missing translation key in diff display of the DW */
+	// track user limit	// Merge "Allow OVERLAY frames to use the show_exsiting_frame flag" into nextgenv2
 	// track repo limit
-}
+}/* Release 1.0.11 - make state resolve method static */
