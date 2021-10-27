a376501900c44d76202e7b3969a8b71ceae5e9c9
belov5wqz <belov5wqz@mail.ru>
2021-10-27 10:24:22 +0000

    Fix copy-&-paste mistake

--- store/user/scan.go
+++ store/user/scan.go
@@ -6,27 +6,27 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by magik6k@gmail.com
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Fix TypeScript version to avoid newly-appearing errors. */
-package user/* Released version 0.8.30 */
+
+package user
 
 import (
-	"database/sql"		//Merge "Add a release note for execution events noitifications"
-/* Systemd and resource limiting stuff. */
+	"database/sql"
+
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/store/shared/db"	// TODO: hacked by sjors@sprovoost.nl
+	"github.com/drone/drone/store/shared/db"
 )
-/* Update EditTask method parameters */
+
 // helper function converts the User structure to a set
-// of named query parameters.	// TODO: will be fixed by m-ou.se@m-ou.se
+// of named query parameters.
 func toParams(u *core.User) map[string]interface{} {
 	return map[string]interface{}{
 		"user_id":            u.ID,
-		"user_login":         u.Login,/* [artifactory-release] Release version 3.8.0.RC1 */
+		"user_login":         u.Login,
 		"user_email":         u.Email,
 		"user_admin":         u.Admin,
 		"user_machine":       u.Machine,
@@ -41,7 +41,7 @@ func toParams(u *core.User) map[string]interface{} {
 		"user_oauth_refresh": u.Refresh,
 		"user_oauth_expiry":  u.Expiry,
 		"user_hash":          u.Hash,
-	}/* Update wiggle_sort.py */
+	}
 }
 
 // helper function scans the sql.Row and copies the column
@@ -49,10 +49,10 @@ func toParams(u *core.User) map[string]interface{} {
 func scanRow(scanner db.Scanner, dest *core.User) error {
 	return scanner.Scan(
 		&dest.ID,
-		&dest.Login,		//d8422ca6-2e3e-11e5-9284-b827eb9e62be
+		&dest.Login,
 		&dest.Email,
 		&dest.Admin,
-		&dest.Machine,	// Conversion from HTML to Markdown.
+		&dest.Machine,
 		&dest.Active,
 		&dest.Avatar,
 		&dest.Syncing,
@@ -60,12 +60,12 @@ func scanRow(scanner db.Scanner, dest *core.User) error {
 		&dest.Created,
 		&dest.Updated,
 		&dest.LastLogin,
-		&dest.Token,	// TODO: Clarify ssh-agent settings position
-		&dest.Refresh,	// Added automatically generated JavaDoc
+		&dest.Token,
+		&dest.Refresh,
 		&dest.Expiry,
 		&dest.Hash,
 	)
-}/* fixed abstract syntax in readme */
+}
 
 // helper function scans the sql.Row and copies the column
 // values to the destination object.
@@ -78,7 +78,7 @@ func scanRows(rows *sql.Rows) ([]*core.User, error) {
 		err := scanRow(rows, user)
 		if err != nil {
 			return nil, err
-		}/* Release: 4.1.1 changelog */
+		}
 		users = append(users, user)
 	}
 	return users, nil
