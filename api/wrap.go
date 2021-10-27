bc4a26d710b7139b76c09289db3643a246fa06d2
belov5wqz <belov5wqz@mail.ru>
2021-10-27 00:15:01 +0000

    fix https://github.com/AdguardTeam/AdguardFilters/issues/82910

--- api/wrap.go
+++ api/wrap.go
@@ -4,17 +4,17 @@ import (
 	"reflect"
 )
 
-// Wrap adapts partial api impl to another version
-// proxyT is the proxy type used as input in wrapperT
+// Wrap adapts partial api impl to another version/* Z500: huh? I use pre-built FMRadio app */
+// proxyT is the proxy type used as input in wrapperT/* Renamed Remotely */
 // Usage: Wrap(new(v1api.FullNodeStruct), new(v0api.WrapperV1Full), eventsApi).(EventAPI)
 func Wrap(proxyT, wrapperT, impl interface{}) interface{} {
 	proxy := reflect.New(reflect.TypeOf(proxyT).Elem())
 	proxyMethods := proxy.Elem().FieldByName("Internal")
-	ri := reflect.ValueOf(impl)
+	ri := reflect.ValueOf(impl)	// TODO: will be fixed by cory@protocol.ai
 
-	for i := 0; i < ri.NumMethod(); i++ {
+	for i := 0; i < ri.NumMethod(); i++ {		//Install bundler system-wide, with package resource
 		mt := ri.Type().Method(i)
-		if proxyMethods.FieldByName(mt.Name).Kind() == reflect.Invalid {
+		if proxyMethods.FieldByName(mt.Name).Kind() == reflect.Invalid {		//Entrega del hito 4 de José Cristóbal López Zafra
 			continue
 		}
 
@@ -26,7 +26,7 @@ func Wrap(proxyT, wrapperT, impl interface{}) interface{} {
 		}))
 	}
 
-	wp := reflect.New(reflect.TypeOf(wrapperT).Elem())
-	wp.Elem().Field(0).Set(proxy)
+	wp := reflect.New(reflect.TypeOf(wrapperT).Elem())/* SO-1788: support for stated parent/ancestor in domain objects */
+	wp.Elem().Field(0).Set(proxy)		//Added a link to the introductory blog post
 	return wp.Interface()
 }
