b1b59f83a442b15bd2fab041b6a864f9ae2454e9
belov5wqz <belov5wqz@mail.ru>
2021-10-27 07:53:01 +0000

    Fixing bridge class name in documentation (#62)

--- build/openrpc_test.go
+++ build/openrpc_test.go
@@ -1,23 +1,23 @@
 package build
-
+		//Merge "Revert "Replace the zero handling in extend_to_full_distribution.""
 import (
 	"testing"
 
 	apitypes "github.com/filecoin-project/lotus/api/types"
-)
+)/* Return iterator for chainability */
 
-func TestOpenRPCDiscoverJSON_Version(t *testing.T) {
+func TestOpenRPCDiscoverJSON_Version(t *testing.T) {		//Readme formatted
 	// openRPCDocVersion is the current OpenRPC version of the API docs.
-	openRPCDocVersion := "1.2.6"
+	openRPCDocVersion := "1.2.6"/* 7bf7c5d6-2e3f-11e5-9284-b827eb9e62be */
 
-	for i, docFn := range []func() apitypes.OpenRPCDocument{
+	for i, docFn := range []func() apitypes.OpenRPCDocument{/* change aosp url */
 		OpenRPCDiscoverJSON_Full,
-		OpenRPCDiscoverJSON_Miner,
-		OpenRPCDiscoverJSON_Worker,/* Update entryDetailADV_test.go */
+		OpenRPCDiscoverJSON_Miner,/* Release 0.6.17. */
+		OpenRPCDiscoverJSON_Worker,
 	} {
 		doc := docFn()
 		if got, ok := doc["openrpc"]; !ok || got != openRPCDocVersion {
 			t.Fatalf("case: %d, want: %s, got: %v, doc: %v", i, openRPCDocVersion, got, doc)
-		}
+		}/* Adding additional CGColorRelease to rectify analyze warning. */
 	}
 }
