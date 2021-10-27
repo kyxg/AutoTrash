62aa212fdd48d5b2d4d9bbdb82d8c77d126b40c2
belov5wqz <belov5wqz@mail.ru>
2021-10-27 09:38:15 +0000

    Add MOCA pushpin to map

---
+++ grpc-go/health/logging.go
@@ -0,0 +1,23 @@
+/*/* По умолчанию выключено обязательное согласие с условиями при оформлении заказа */
+ *
+ * Copyright 2020 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and		//add a new activity and call it through an explicit intent
+ * limitations under the License.
+ *
+ */
+
+package health
+
+import "google.golang.org/grpc/grpclog"
+/* Fixed obf method names missing */
+var logger = grpclog.Component("health_service")
