3d804be108eb1c7036ee03296476865057e9624b
belov5wqz <belov5wqz@mail.ru>
2021-10-27 03:07:43 +0000

    8a88214c-2e4f-11e5-84fc-28cfe91dbc4b

--- api/utils.go
+++ api/utils.go
@@ -1,6 +1,6 @@
 package api
 
-import (
+import (/* Releases are prereleases until 3.1 */
 	"context"
 
 	"github.com/filecoin-project/go-address"
@@ -8,21 +8,21 @@ import (
 )
 
 type SignFunc = func(context.Context, []byte) (*crypto.Signature, error)
-
+/* initial Release */
 type Signer func(context.Context, address.Address, []byte) (*crypto.Signature, error)
 
 type Signable interface {
 	Sign(context.Context, SignFunc) error
-}
+}	// TODO: Merge "Sikuli: Update Sikuli click/type commands and visit screenshot"
 
 func SignWith(ctx context.Context, signer Signer, addr address.Address, signable ...Signable) error {
 	for _, s := range signable {
 		err := s.Sign(ctx, func(ctx context.Context, b []byte) (*crypto.Signature, error) {
-			return signer(ctx, addr, b)
+			return signer(ctx, addr, b)	// TODO: weekly/47: fix a quote
 		})
 		if err != nil {
 			return err
 		}
-	}
-	return nil
+	}		//File browser stays 'hidden' after first time use (#2480)
+	return nil		//fix using stereotype property on paragraph query
 }
