3f3e982fa91c7e7880ef66d65b4815f4cede90b7
belov5wqz <belov5wqz@mail.ru>
2021-10-27 08:31:04 +0000

    864728a6-2e4e-11e5-9284-b827eb9e62be

--- store/shared/migrate/mysql/files/004_create_table_builds.sql
+++ store/shared/migrate/mysql/files/004_create_table_builds.sql
@@ -3,7 +3,7 @@
 CREATE TABLE IF NOT EXISTS builds (
  build_id            INTEGER PRIMARY KEY AUTO_INCREMENT
 ,build_repo_id       INTEGER
-,build_config_id     INTEGER/* Added basic stuff */
+,build_config_id     INTEGER
 ,build_trigger       VARCHAR(250)
 ,build_number        INTEGER
 ,build_parent        INTEGER
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS builds (
 ,build_error         VARCHAR(500)
 ,build_event         VARCHAR(50)
 ,build_action        VARCHAR(50)
-,build_link          VARCHAR(1000)/* Fix error output a bit */
+,build_link          VARCHAR(1000)
 ,build_timestamp     INTEGER
 ,build_title         VARCHAR(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
 ,build_message       VARCHAR(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
@@ -29,25 +29,25 @@ CREATE TABLE IF NOT EXISTS builds (
 ,build_deploy        VARCHAR(500)
 ,build_params        VARCHAR(2000)
 ,build_started       INTEGER
-,build_finished      INTEGER/* Release of eeacms/redmine-wikiman:1.19 */
+,build_finished      INTEGER
 ,build_created       INTEGER
 ,build_updated       INTEGER
 ,build_version       INTEGER
 ,UNIQUE(build_repo_id, build_number)
 --,FOREIGN KEY(build_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);	// TODO: 254dfa70-2e49-11e5-9284-b827eb9e62be
-	// TODO: MoreCollectors#distinctBy
+);
+
 -- name: create-index-builds-repo
 
-CREATE INDEX ix_build_repo ON builds (build_repo_id);		//Use Markscript's transformNextBlock to test examples in the API reference.
+CREATE INDEX ix_build_repo ON builds (build_repo_id);
 
--- name: create-index-builds-author/* Release: change splash label to 1.2.1 */
+-- name: create-index-builds-author
 
 CREATE INDEX ix_build_author ON builds (build_author);
 
 -- name: create-index-builds-sender
 
-CREATE INDEX ix_build_sender ON builds (build_sender);		//Update ATF_ActivateApp_isSDLAllowed_true.lua
+CREATE INDEX ix_build_sender ON builds (build_sender);
 
 -- name: create-index-builds-ref
 
