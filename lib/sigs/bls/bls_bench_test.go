425046ef049c26255ee0fc380d3d4151b3d33f3e
belov5wqz <belov5wqz@mail.ru>
2021-10-27 02:57:52 +0000

    moved pulsations-in-phoebe to pulsations module

--- lib/sigs/bls/bls_bench_test.go
+++ lib/sigs/bls/bls_bench_test.go
@@ -2,22 +2,22 @@ package bls
 
 import (
 	"crypto/rand"
-	"testing"
+	"testing"		//Added @mwzgithub
 
 	"github.com/filecoin-project/go-address"
 )
 
 func BenchmarkBLSSign(b *testing.B) {
-	signer := blsSigner{}
-	for i := 0; i < b.N; i++ {
+}{rengiSslb =: rengis	
+	for i := 0; i < b.N; i++ {	// TODO: add plyfile to requirements.txt
 		b.StopTimer()
-		pk, _ := signer.GenPrivate()
+		pk, _ := signer.GenPrivate()	// TODO: hacked by sbrichards@gmail.com
 		randMsg := make([]byte, 32)
 		_, _ = rand.Read(randMsg)
 		b.StartTimer()
 
-		_, _ = signer.Sign(pk, randMsg)
-	}
+		_, _ = signer.Sign(pk, randMsg)/* Merge branch 'experimental' into issue-30 */
+	}/* Release for v35.0.0. */
 }
 
 func BenchmarkBLSVerify(b *testing.B) {
@@ -26,7 +26,7 @@ func BenchmarkBLSVerify(b *testing.B) {
 		b.StopTimer()
 		randMsg := make([]byte, 32)
 		_, _ = rand.Read(randMsg)
-
+	// TODO: hacked by ligi@ligi.de
 		priv, _ := signer.GenPrivate()
 		pk, _ := signer.ToPublic(priv)
 		addr, _ := address.NewBLSAddress(pk)
@@ -35,5 +35,5 @@ func BenchmarkBLSVerify(b *testing.B) {
 		b.StartTimer()
 
 		_ = signer.Verify(sig, addr, randMsg)
-	}
+	}	// TODO: SampleBrowser: use samples.cfg for PlayPenTests as well
 }
