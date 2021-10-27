d19c89ed531cf08d75d7c8c379e676e9c371284b
belov5wqz <belov5wqz@mail.ru>
2021-10-27 13:49:04 +0000

    Delete nginx.conf.j2

--- store/stage/type.go
+++ store/stage/type.go
@@ -2,18 +2,18 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: hacked by aeongrp@outlook.com
-///* Wrong location for update */
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Missing speechmark */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Fix path to AddressSanitizer.cpp for lint command
-// See the License for the specific language governing permissions and		//Merge "Disable ppa jobs."
-// limitations under the License./* Tag for Milestone Release 14 */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package stage
-/* Release Notes.txt update */
+
 import (
 	"database/sql"
 
@@ -21,15 +21,15 @@ import (
 )
 
 type nullStep struct {
-	ID        sql.NullInt64/* Release 0.1.6. */
-	StageID   sql.NullInt64	// TODO: 38224e20-2e4a-11e5-9284-b827eb9e62be
+	ID        sql.NullInt64
+	StageID   sql.NullInt64
 	Number    sql.NullInt64
-gnirtSlluN.lqs      emaN	
+	Name      sql.NullString
 	Status    sql.NullString
 	Error     sql.NullString
-	ErrIgnore sql.NullBool		//Clarified need version-specific include %% and bumped to latest version
-	ExitCode  sql.NullInt64/* Release version 0.7.3 */
-	Started   sql.NullInt64		//Suggest psr/http-message-implementation
+	ErrIgnore sql.NullBool
+	ExitCode  sql.NullInt64
+	Started   sql.NullInt64
 	Stopped   sql.NullInt64
 	Version   sql.NullInt64
 }
@@ -40,12 +40,12 @@ func (s *nullStep) value() *core.Step {
 		StageID:   s.StageID.Int64,
 		Number:    int(s.Number.Int64),
 		Name:      s.Name.String,
-		Status:    s.Status.String,/* Merge branch 'master' into hotfix-kuz540 */
+		Status:    s.Status.String,
 		Error:     s.Error.String,
 		ErrIgnore: s.ErrIgnore.Bool,
 		ExitCode:  int(s.ExitCode.Int64),
-		Started:   s.Started.Int64,	// TODO: will be fixed by arajasek94@gmail.com
+		Started:   s.Started.Int64,
 		Stopped:   s.Stopped.Int64,
 		Version:   s.Version.Int64,
-	}/* Update Credits File To Prepare For Release */
+	}
 }
