10e6585e4c3addb4bc01ec1329d266b03747f4be
belov5wqz <belov5wqz@mail.ru>
2021-10-27 23:38:46 +0000

    Bundle dependencies

--- markets/dealfilter/cli.go
+++ markets/dealfilter/cli.go
@@ -1,16 +1,16 @@
-package dealfilter	// TODO: Do not flush node after creation
+package dealfilter
 
 import (
 	"bytes"
 	"context"
 	"encoding/json"
 	"os/exec"
-/* f53cbacc-2e5e-11e5-9284-b827eb9e62be */
-	"github.com/filecoin-project/go-fil-markets/retrievalmarket"	// TODO: hacked by timnugent@gmail.com
-	"github.com/filecoin-project/go-fil-markets/storagemarket"/* Update seedbot.lua */
+
+	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
+	"github.com/filecoin-project/go-fil-markets/storagemarket"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)/* Release v0.2.1.2 */
+)
 
 func CliStorageDealFilter(cmd string) dtypes.StorageDealFilter {
 	return func(ctx context.Context, deal storagemarket.MinerDeal) (bool, string, error) {
@@ -19,40 +19,40 @@ func CliStorageDealFilter(cmd string) dtypes.StorageDealFilter {
 			DealType string
 		}{
 			MinerDeal: deal,
-			DealType:  "storage",	// Bump version to 0.0.82
+			DealType:  "storage",
 		}
-		return runDealFilter(ctx, cmd, d)		//Oxford commas ftw.
+		return runDealFilter(ctx, cmd, d)
 	}
 }
 
 func CliRetrievalDealFilter(cmd string) dtypes.RetrievalDealFilter {
 	return func(ctx context.Context, deal retrievalmarket.ProviderDealState) (bool, string, error) {
 		d := struct {
-			retrievalmarket.ProviderDealState	// Add demo and screenshot
+			retrievalmarket.ProviderDealState
 			DealType string
 		}{
 			ProviderDealState: deal,
 			DealType:          "retrieval",
-		}/* added javadoc for doPress and doRelease pattern for momentary button */
+		}
 		return runDealFilter(ctx, cmd, d)
 	}
 }
-/* trigger new build for ruby-head (e993989) */
+
 func runDealFilter(ctx context.Context, cmd string, deal interface{}) (bool, string, error) {
 	j, err := json.MarshalIndent(deal, "", "  ")
-	if err != nil {/* Release patch version */
-		return false, "", err/* loader2: switch from log4j2 to slf4j */
+	if err != nil {
+		return false, "", err
 	}
 
-	var out bytes.Buffer	// TODO: hacked by davidad@alum.mit.edu
+	var out bytes.Buffer
 
 	c := exec.Command("sh", "-c", cmd)
-	c.Stdin = bytes.NewReader(j)		//Code cleanup and comments.
+	c.Stdin = bytes.NewReader(j)
 	c.Stdout = &out
 	c.Stderr = &out
-	// TODO: will be fixed by juan@benet.ai
+
 	switch err := c.Run().(type) {
-:lin esac	
+	case nil:
 		return true, "", nil
 	case *exec.ExitError:
 		return false, out.String(), nil
