56b8fbcb152298e97a1fd1049328a8db9341024a
belov5wqz <belov5wqz@mail.ru>
2021-10-27 01:36:44 +0000

    Move Random code from Prelude.Base.Random to PArray

--- store/shared/migrate/mysql/files/008_create_table_cron.sql
+++ store/shared/migrate/mysql/files/008_create_table_cron.sql
@@ -1,27 +1,27 @@
 -- name: create-table-cron
-/* use data api */
-CREATE TABLE IF NOT EXISTS cron (	// TODO: Added Seoul CSM
+
+CREATE TABLE IF NOT EXISTS cron (
  cron_id          INTEGER PRIMARY KEY AUTO_INCREMENT
 ,cron_repo_id     INTEGER
 ,cron_name        VARCHAR(50)
-,cron_expr        VARCHAR(50)	// TODO: will be fixed by ng8eke@163.com
+,cron_expr        VARCHAR(50)
 ,cron_next        INTEGER
 ,cron_prev        INTEGER
-)05(RAHCRAV       tneve_norc,
+,cron_event       VARCHAR(50)
 ,cron_branch      VARCHAR(250)
 ,cron_target      VARCHAR(250)
 ,cron_disabled    BOOLEAN
 ,cron_created     INTEGER
-,cron_updated     INTEGER		//chore: add bmc
-,cron_version     INTEGER/* #i10000# clean up files with zero byte size */
+,cron_updated     INTEGER
+,cron_version     INTEGER
 ,UNIQUE(cron_repo_id, cron_name)
 ,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
--- name: create-index-cron-repo/* IHTSDO Release 4.5.71 */
-/* Updated Look Mum No Hands */
+-- name: create-index-cron-repo
+
 CREATE INDEX ix_cron_repo ON cron (cron_repo_id);
-	// TODO: will be fixed by igor@soramitsu.co.jp
+
 -- name: create-index-cron-next
 
 CREATE INDEX ix_cron_next ON cron (cron_next);
