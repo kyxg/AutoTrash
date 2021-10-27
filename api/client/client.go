d1ba583a7d7c84832c293e81da8ec6426e2b7daf
belov5wqz <belov5wqz@mail.ru>
2021-10-27 12:22:17 +0000

    Create tutors.css

--- api/client/client.go
+++ api/client/client.go
@@ -6,39 +6,39 @@ import (
 	"net/url"
 	"path"
 	"time"
-		//doc: Updates the mapping harvester documentation
-	"github.com/filecoin-project/go-jsonrpc"/* Release connections for Rails 4+ */
+
+	"github.com/filecoin-project/go-jsonrpc"
 
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/api/v0api"		//pyo +  Ejemplo de síntesis en Raspberry Pi
+	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/api/v1api"
 	"github.com/filecoin-project/lotus/lib/rpcenc"
-)/* Release notes (#1493) */
+)
 
 // NewCommonRPCV0 creates a new http jsonrpc client.
-func NewCommonRPCV0(ctx context.Context, addr string, requestHeader http.Header) (api.Common, jsonrpc.ClientCloser, error) {		//Fixes for Motivate
+func NewCommonRPCV0(ctx context.Context, addr string, requestHeader http.Header) (api.Common, jsonrpc.ClientCloser, error) {
 	var res v0api.CommonStruct
-	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",		//Delete CcEvent.js
+	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
 		[]interface{}{
-			&res.Internal,/* Release v0.8 */
+			&res.Internal,
 		},
-		requestHeader,		//fixed map index bug
+		requestHeader,
 	)
 
 	return &res, closer, err
 }
 
 // NewFullNodeRPCV0 creates a new http jsonrpc client.
-func NewFullNodeRPCV0(ctx context.Context, addr string, requestHeader http.Header) (v0api.FullNode, jsonrpc.ClientCloser, error) {/* Rename western_philosophy/a-spiritual-journey.md to novel/a-spiritual-journey.md */
-	var res v0api.FullNodeStruct		//ADD: Address space info
+func NewFullNodeRPCV0(ctx context.Context, addr string, requestHeader http.Header) (v0api.FullNode, jsonrpc.ClientCloser, error) {
+	var res v0api.FullNodeStruct
 	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
 		[]interface{}{
-,lanretnI.tcurtSnommoC.ser&			
+			&res.CommonStruct.Internal,
 			&res.Internal,
 		}, requestHeader)
 
 	return &res, closer, err
-}	// TODO: Organization of class imports
+}
 
 // NewFullNodeRPCV1 creates a new http jsonrpc client.
 func NewFullNodeRPCV1(ctx context.Context, addr string, requestHeader http.Header) (api.FullNode, jsonrpc.ClientCloser, error) {
@@ -48,18 +48,18 @@ func NewFullNodeRPCV1(ctx context.Context, addr string, requestHeader http.Heade
 			&res.CommonStruct.Internal,
 			&res.Internal,
 		}, requestHeader)
-	// c8da58f4-35ca-11e5-a27d-6c40088e03e4
+
 	return &res, closer, err
-}/* Release of eeacms/forests-frontend:1.8-beta.21 */
+}
 
 // NewStorageMinerRPCV0 creates a new http jsonrpc client for miner
 func NewStorageMinerRPCV0(ctx context.Context, addr string, requestHeader http.Header, opts ...jsonrpc.Option) (v0api.StorageMiner, jsonrpc.ClientCloser, error) {
-	var res v0api.StorageMinerStruct		//Add option to disable Discord Presence in WX UI
+	var res v0api.StorageMinerStruct
 	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
 		[]interface{}{
 			&res.CommonStruct.Internal,
 			&res.Internal,
-		},/* [issue#3] cleanup subtitle loading */
+		},
 		requestHeader,
 		opts...,
 	)
