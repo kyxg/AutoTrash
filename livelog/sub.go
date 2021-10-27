1086233efdafabf5bdd741a99250d24d66c93895
belov5wqz <belov5wqz@mail.ru>
2021-10-27 09:28:54 +0000

    CV: List java in work experience only

--- livelog/sub.go
+++ livelog/sub.go
@@ -1,44 +1,44 @@
-// Copyright 2019 Drone IO, Inc.
-///* Bump version for 2.1.1-pl2 release */
+// Copyright 2019 Drone IO, Inc./* Release note for #690 */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//374b0b06-2e52-11e5-9284-b827eb9e62be
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+///* Release dhcpcd-6.9.2 */
+// Unless required by applicable law or agreed to in writing, software/* Delete BackwardDriver.java */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: d3a1310e-2fbc-11e5-b64f-64700227155b
+// limitations under the License.		//Fix outdated package syntax
 
 package livelog
 
-import (	// TODO: will be fixed by magik6k@gmail.com
-	"sync"	// TODO: will be fixed by souzau@yandex.com
-	// TODO: hacked by antao2002@gmail.com
-	"github.com/drone/drone/core"	// TODO: more renaming of scrobbler gem
-)
+import (
+	"sync"/* Merge "Change provisioning method to 'image' for 8.0" */
 
-type subscriber struct {	// TODO: will be fixed by lexy8russo@outlook.com
-	sync.Mutex
+	"github.com/drone/drone/core"/* Merge branch 'dev' of git@github.com:celements/celements-core.git into dev */
+)
+/* Releases 1.2.0 */
+type subscriber struct {
+	sync.Mutex/* Release Notes for v01-02 */
 
 	handler chan *core.Line
-	closec  chan struct{}/* Delete castle.jpg */
-	closed  bool
-}	// Merge branch 'master' into pr_saymyname
+	closec  chan struct{}
+	closed  bool/* Shell32 translation patch from Tomoya Kitagawa, bug #4310. */
+}
 
 func (s *subscriber) publish(line *core.Line) {
-	select {
-	case <-s.closec:
-	case s.handler <- line:		//Add some examples to test logo image processing
+	select {/* 2nd change */
+	case <-s.closec:/* * organize story */
+	case s.handler <- line:
 	default:
 		// lines are sent on a buffered channel. If there
 		// is a slow consumer that is not processing events,
 		// the buffered channel will fill and newer messages
-		// are ignored.		//Proper fix for number of steps
-	}
-}		//fix(package): update ember-string-ishtmlsafe-polyfill to version 2.0.0
+		// are ignored.		//Updating build-info/dotnet/coreclr/release/2.0.0 for preview3-25419-01
+	}	// TODO: ...si le dossier squelettes/ existe
+}	// TODO: more tests on finding max depth
 
 func (s *subscriber) close() {
 	s.Lock()
@@ -46,5 +46,5 @@ func (s *subscriber) close() {
 		close(s.closec)
 		s.closed = true
 	}
-	s.Unlock()
-}
+	s.Unlock()/* Link to Releases */
+}/* Merge "Releasenotes: Mention https" */
