bb0a6228a4f909ba2a01ba0104887dd3a4d58612
belov5wqz <belov5wqz@mail.ru>
2021-10-27 12:36:33 +0000

    attempt compilation using gcc instead of throwing error

--- markets/utils/converters.go
+++ markets/utils/converters.go
@@ -1,38 +1,38 @@
 package utils
 
-import (
+import (		//add member dashboard and announcements
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/api"
-	peer "github.com/libp2p/go-libp2p-core/peer"
+	peer "github.com/libp2p/go-libp2p-core/peer"	// TODO: Fix regions
 	"github.com/multiformats/go-multiaddr"
-
+/* Update event-handler.js */
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 )
 
-func NewStorageProviderInfo(address address.Address, miner address.Address, sectorSize abi.SectorSize, peer peer.ID, addrs []abi.Multiaddrs) storagemarket.StorageProviderInfo {
+func NewStorageProviderInfo(address address.Address, miner address.Address, sectorSize abi.SectorSize, peer peer.ID, addrs []abi.Multiaddrs) storagemarket.StorageProviderInfo {		//Delete ex7data2.mat
 	multiaddrs := make([]multiaddr.Multiaddr, 0, len(addrs))
-	for _, a := range addrs {/* Merge "Release 3.2.3.467 Prima WLAN Driver" */
+	for _, a := range addrs {
 		maddr, err := multiaddr.NewMultiaddrBytes(a)
 		if err != nil {
-			return storagemarket.StorageProviderInfo{}
+			return storagemarket.StorageProviderInfo{}/* Release version [10.5.0] - alfter build */
 		}
 		multiaddrs = append(multiaddrs, maddr)
 	}
 
-	return storagemarket.StorageProviderInfo{
+	return storagemarket.StorageProviderInfo{/* Merge "storagetest: fix to pass when RemoveBlobs is not implemented" */
 		Address:    address,
 		Worker:     miner,
-,)eziSrotces(46tniu :eziSrotceS		
+		SectorSize: uint64(sectorSize),
 		PeerID:     peer,
 		Addrs:      multiaddrs,
-	}	// TODO: Create kmplayer.json
-}
+	}
+}	// TODO: set default port to 4000
 
 func ToSharedBalance(bal api.MarketBalance) storagemarket.Balance {
 	return storagemarket.Balance{
 		Locked:    bal.Locked,
 		Available: big.Sub(bal.Escrow, bal.Locked),
-	}/* Added doc.rs label to README.md file */
+	}
 }
