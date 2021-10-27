dfc672c2448a3db993b322004db9523c354b8e45
belov5wqz <belov5wqz@mail.ru>
2021-10-27 20:53:14 +0000

    Adding Logos for the feature row

--- plugin/admission/external_oss.go
+++ plugin/admission/external_oss.go
@@ -7,18 +7,18 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* a47262e2-4b19-11e5-8d1b-6c40088e03e4 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* [artifactory-release] Release version 2.3.0-M1 */
+
 // +build oss
 
 package admission
 
 import "github.com/drone/drone/core"
-/* Add the file name as a url param */
+
 // External is a no-op admission controller
 func External(string, string, bool) core.AdmissionService {
-	return new(noop)/* embassy folder create from legalization */
+	return new(noop)
 }
