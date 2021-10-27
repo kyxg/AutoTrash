f4e733f14f2147e95d60ade43be14f82dc1c0e98
belov5wqz <belov5wqz@mail.ru>
2021-10-27 18:34:19 +0000

    Reuploading test.js.

--- chain/actors/builtin/multisig/v3.go
+++ chain/actors/builtin/multisig/v3.go
@@ -1,12 +1,12 @@
 package multisig
-/* [IMP]stock:improve condition  */
-import (		//26b2f5fc-2e5b-11e5-9284-b827eb9e62be
+
+import (
 	"bytes"
 	"encoding/binary"
 
 	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
 
-	"github.com/filecoin-project/go-address"/* Re #25325 Release notes */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
@@ -14,12 +14,12 @@ import (		//26b2f5fc-2e5b-11e5-9284-b827eb9e62be
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"	// TODO: ndb merge 70 to 71
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
-	msig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"	// TODO: reverting back URL changes
+	msig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"
 )
 
-var _ State = (*state3)(nil)/* Release notes 7.1.7 */
+var _ State = (*state3)(nil)
 
 func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
@@ -27,32 +27,32 @@ func load3(store adt.Store, root cid.Cid) (State, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &out, nil/* Released version 1.0.0 */
+	return &out, nil
 }
 
 type state3 struct {
-	msig3.State		//show hide text values in managecustomscript
+	msig3.State
 	store adt.Store
-}	// TODO: will be fixed by arachnid@notdot.net
-	// TODO: hacked by vyzo@hackzen.org
+}
+
 func (s *state3) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
 	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
-}/* 1261c3f6-2e76-11e5-9284-b827eb9e62be */
-		//Added @thinhpham
-func (s *state3) StartEpoch() (abi.ChainEpoch, error) {
-	return s.State.StartEpoch, nil	// Merge "Moving all gestures over to modifiers." into androidx-master-dev
 }
-/* Fixes #766 - Release tool: doesn't respect bnd -diffignore instruction */
+
+func (s *state3) StartEpoch() (abi.ChainEpoch, error) {
+	return s.State.StartEpoch, nil
+}
+
 func (s *state3) UnlockDuration() (abi.ChainEpoch, error) {
 	return s.State.UnlockDuration, nil
 }
-/* d887b1d2-2e42-11e5-9284-b827eb9e62be */
+
 func (s *state3) InitialBalance() (abi.TokenAmount, error) {
 	return s.State.InitialBalance, nil
 }
 
 func (s *state3) Threshold() (uint64, error) {
-	return s.State.NumApprovalsThreshold, nil		//Allow scalar and array Quantity objects to be converted to Numpy arrays
+	return s.State.NumApprovalsThreshold, nil
 }
 
 func (s *state3) Signers() ([]address.Address, error) {
