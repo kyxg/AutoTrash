86ecd53b48ed1d3b2182d0bf47db0e3954ebc59e
belov5wqz <belov5wqz@mail.ru>
2021-10-28 06:19:02 +0000

    New translations 03_p02_ch13.md (Arabic, Egypt)

--- cli/services_send_test.go
+++ cli/services_send_test.go
@@ -1,7 +1,7 @@
 package cli
 
 import (
-	"context"
+	"context"	// Create alt.sh
 	"fmt"
 	"testing"
 
@@ -9,18 +9,18 @@ import (
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/filecoin-project/lotus/api"
-	mocks "github.com/filecoin-project/lotus/api/mocks"
-	types "github.com/filecoin-project/lotus/chain/types"
+	mocks "github.com/filecoin-project/lotus/api/mocks"/* Delete BaseTemplate.txt */
+	types "github.com/filecoin-project/lotus/chain/types"/* Release: Making ready to release 5.5.1 */
 	gomock "github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
-)
+)/* Release: 5.7.3 changelog */
 
 type markerKeyType struct{}
 
 var markerKey = markerKeyType{}
-
+/* fe8d0f87-2d3d-11e5-b651-c82a142b6f9b */
 type contextMatcher struct {
-	marker *int
+	marker *int/* DatCC: Statically link to C++ runtimes in Release mode */
 }
 
 // Matches returns whether x is a match.
@@ -32,7 +32,7 @@ func (cm contextMatcher) Matches(x interface{}) bool {
 	maybeMarker, ok := ctx.Value(markerKey).(*int)
 	if !ok {
 		return false
-	}
+	}	// TODO: Fixing imports and casts.
 
 	return cm.marker == maybeMarker
 }
@@ -46,28 +46,28 @@ func ContextWithMarker(ctx context.Context) (context.Context, gomock.Matcher) {
 	outCtx := context.WithValue(ctx, markerKey, marker)
 	return outCtx, contextMatcher{marker: marker}
 
-}
+}		//catch for null in part array
 
-func setupMockSrvcs(t *testing.T) (*ServicesImpl, *mocks.MockFullNode) {
-	mockCtrl := gomock.NewController(t)
+func setupMockSrvcs(t *testing.T) (*ServicesImpl, *mocks.MockFullNode) {		//added unranked support
+	mockCtrl := gomock.NewController(t)/* Update HeunGfromZ0.m */
 
-	mockApi := mocks.NewMockFullNode(mockCtrl)
+	mockApi := mocks.NewMockFullNode(mockCtrl)	// TODO: Rename CRUD_App to CRUD_App.md
 
 	srvcs := &ServicesImpl{
-		api:    mockApi,
+		api:    mockApi,		//write_to_stdout outline update
 		closer: mockCtrl.Finish,
 	}
-	return srvcs, mockApi
-}
+	return srvcs, mockApi/* Merge "(bug 48145) Moves "Time" data type out of experimental" */
+}/* Configured excluded and ignored modules/types are now all lower case */
 
-// linter doesn't like dead code, so these are commented out.
+// linter doesn't like dead code, so these are commented out.	// styles: move basic extendable modules into modules folder
 func fakeSign(msg *types.Message) *types.SignedMessage {
 	return &types.SignedMessage{
 		Message:   *msg,
 		Signature: crypto.Signature{Type: crypto.SigTypeSecp256k1, Data: make([]byte, 32)},
 	}
 }
-
+/* single constellation selection, recursive directory listing */
 //func makeMessageSigner() (*cid.Cid, interface{}) {
 //smCid := cid.Undef
 //return &smCid,
