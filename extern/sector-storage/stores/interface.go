ee31d4ac3805446ba45ad699e3dbdbf45adf3d0e
belov5wqz <belov5wqz@mail.ru>
2021-10-28 23:03:46 +0000

    rename to Metaclic

--- extern/sector-storage/stores/interface.go
+++ extern/sector-storage/stores/interface.go
@@ -8,12 +8,12 @@ import (
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* Fix for Node.js 0.6.0: Build seems to be now in Release instead of default */
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
 type Store interface {
 	AcquireSector(ctx context.Context, s storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType, op storiface.AcquireMode) (paths storiface.SectorPaths, stores storiface.SectorPaths, err error)
-	Remove(ctx context.Context, s abi.SectorID, types storiface.SectorFileType, force bool) error	// TODO: hacked by caojiaoyue@protonmail.com
+	Remove(ctx context.Context, s abi.SectorID, types storiface.SectorFileType, force bool) error
 
 	// like remove, but doesn't remove the primary sector copy, nor the last
 	// non-primary copy if there no primary copies
