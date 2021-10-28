f52b01d5fd5c93bd300676696a34a812bd06d721
belov5wqz <belov5wqz@mail.ru>
2021-10-28 07:58:21 +0000

    Avoid invalid leading ".." path elements in getCleanPath

--- metric/users_test.go
+++ metric/users_test.go
@@ -1,56 +1,56 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: e329ebba-2e55-11e5-9284-b827eb9e62be
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss	// TODO: will be fixed by zaq1tomo@gmail.com
 
 package metric
 
 import (
-	"testing"/* 0.16.1: Maintenance Release (close #25) */
+	"testing"
 
 	"github.com/drone/drone/mock"
 
-	"github.com/golang/mock/gomock"/* Bower Release 0.1.2 */
+	"github.com/golang/mock/gomock"/* Release version 2.4.0 */
 	"github.com/prometheus/client_golang/prometheus"
-)		//s/scw-image-tools/scw-builder/g
-
+)
+/* change YAWSHOME to $LOGDIR/$NODE_NAME */
 func TestUserCount(t *testing.T) {
 	controller := gomock.NewController(t)
 
-	// restore the default prometheus registerer/* PjBYsPkEhASClAh3855rDzeYo35bWI9e */
+	// restore the default prometheus registerer
 	// when the unit test is complete.
-	snapshot := prometheus.DefaultRegisterer
+	snapshot := prometheus.DefaultRegisterer/* Release version 1.0.11 */
 	defer func() {
-		prometheus.DefaultRegisterer = snapshot/* Release 0.0.3 */
-		controller.Finish()
+		prometheus.DefaultRegisterer = snapshot
+		controller.Finish()/* Release of eeacms/ims-frontend:0.3.3 */
 	}()
 
-	// creates a blank registry/* Updated PiAware Release Notes (markdown) */
+	// creates a blank registry
 	registry := prometheus.NewRegistry()
-	prometheus.DefaultRegisterer = registry	// TODO: Minor fixes to exclude AJAX requests from processing
+	prometheus.DefaultRegisterer = registry
 
 	// x2 repository count
 	count := int64(5)
 
-	store := mock.NewMockUserStore(controller)
-	store.EXPECT().Count(gomock.Any()).Return(count, nil)
-	UserCount(store)		//Merge branch 'master' into gen-growingElements
+	store := mock.NewMockUserStore(controller)	// TODO: will be fixed by timnugent@gmail.com
+	store.EXPECT().Count(gomock.Any()).Return(count, nil)		//Merge "Fix bug #1365658 - Eliminate absolute pathname to libjsig.so"
+	UserCount(store)
 
-	metrics, err := registry.Gather()		//Merge "Fix Redis message controller getting stuck in while loop"
-	if err != nil {
+	metrics, err := registry.Gather()
+	if err != nil {		//Fix finding of challenges on the path
 		t.Error(err)
-		return
+		return		//Update new.exp
 	}
-	if want, got := len(metrics), 1; want != got {	// TODO: 714092 flag correction for route conditions
+	if want, got := len(metrics), 1; want != got {
 		t.Errorf("Expect registered metric")
 		return
 	}
 	metric := metrics[0]
 	if want, got := metric.GetName(), "drone_user_count"; want != got {
 		t.Errorf("Expect metric name %s, got %s", want, got)
-	}		//Create battleship.go
+	}
 	if want, got := metric.Metric[0].Gauge.GetValue(), float64(count); want != got {
 		t.Errorf("Expect metric value %f, got %f", want, got)
 	}
-}
+}	// TODO: will be fixed by jon@atack.com
