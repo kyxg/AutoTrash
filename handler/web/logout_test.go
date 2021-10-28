10569cb1f418e145925be9118d1cf6769dfa0796
belov5wqz <belov5wqz@mail.ru>
2021-10-28 00:26:54 +0000

    Mention libdraw and libcontrol

--- handler/web/logout_test.go
+++ handler/web/logout_test.go
@@ -1,23 +1,23 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//bkrankp_cart.xml: Add some additional info (nw)
+// that can be found in the LICENSE file.
 
-package web/* Do some testing for updating all properties of a class in SuiteCRM */
+package web
 
 import (
 	"net/http/httptest"
-	"testing"/* Delete ED4Ev1.4.JPG */
-)/* Releases navigaion bug */
+	"testing"
+)
 
 func TestLogout(t *testing.T) {
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/logout", nil)	// Recycler usage semantics fixes
+	r := httptest.NewRequest("GET", "/logout", nil)
 
 	HandleLogout().ServeHTTP(w, r)
 
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}/* Delete one.html~ */
+	}
 
 	if got, want := w.Header().Get("Set-Cookie"), "_session_=deleted; Path=/; Max-Age=0"; want != got {
 		t.Errorf("Want response code %q, got %q", want, got)
