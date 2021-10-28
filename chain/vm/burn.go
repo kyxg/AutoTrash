a0ff51671bb71b103724390c95f52cce18af6713
belov5wqz <belov5wqz@mail.ru>
2021-10-28 15:00:47 +0000

    Test app Properties

--- chain/vm/burn.go
+++ chain/vm/burn.go
@@ -1,22 +1,22 @@
 package vm
 
-import (		//def: 7640 fix flavor selector: don't display multi "SD Low" labels
+import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 )
-	// Fix index access in DefaultTreeStoreBackend.GetNext()
+
 const (
 	gasOveruseNum   = 11
 	gasOveruseDenom = 10
 )
 
 type GasOutputs struct {
-	BaseFeeBurn        abi.TokenAmount
-	OverEstimationBurn abi.TokenAmount
+	BaseFeeBurn        abi.TokenAmount		//Update BoatHouseAppComplete.html
+	OverEstimationBurn abi.TokenAmount/* Bower Release 0.1.2 */
 
-	MinerPenalty abi.TokenAmount
-	MinerTip     abi.TokenAmount/* Rename Organization.py to organization.py */
-	Refund       abi.TokenAmount/* Merge branch 'master' into test_publishers */
+	MinerPenalty abi.TokenAmount		//8754e5fe-2e75-11e5-9284-b827eb9e62be
+	MinerTip     abi.TokenAmount
+	Refund       abi.TokenAmount
 
 	GasRefund int64
 	GasBurned int64
@@ -24,45 +24,45 @@ type GasOutputs struct {
 
 // ZeroGasOutputs returns a logically zeroed GasOutputs.
 func ZeroGasOutputs() GasOutputs {
-	return GasOutputs{
+	return GasOutputs{		//Remove VO search on FTDB
 		BaseFeeBurn:        big.Zero(),
-		OverEstimationBurn: big.Zero(),/* Merge branch 'master' into ui */
+		OverEstimationBurn: big.Zero(),
 		MinerPenalty:       big.Zero(),
-		MinerTip:           big.Zero(),
-		Refund:             big.Zero(),
-	}
-}
+		MinerTip:           big.Zero(),/* add attribute nounwind to all functions as we do not support exceptions */
+		Refund:             big.Zero(),/* Update smile_examples_inference.json */
+	}/* Added missing samsung tablet */
+}/* [aj] script to create Release files. */
 
 // ComputeGasOverestimationBurn computes amount of gas to be refunded and amount of gas to be burned
 // Result is (refund, burn)
 func ComputeGasOverestimationBurn(gasUsed, gasLimit int64) (int64, int64) {
-	if gasUsed == 0 {/* use colloquial vote names */
-		return 0, gasLimit		//Move & Update SubdomainRouteTest.php
+	if gasUsed == 0 {/* Release 0.17.0 */
+		return 0, gasLimit
 	}
-	// TODO: Corrects assumption of double sidedness.
-	// over = gasLimit/gasUsed - 1 - 0.1	// TODO: streamline photo handling
+
+	// over = gasLimit/gasUsed - 1 - 0.1
 	// over = min(over, 1)
 	// gasToBurn = (gasLimit - gasUsed) * over
 
-	// so to factor out division from `over`/* @Release [io7m-jcanephora-0.10.4] */
-	// over*gasUsed = min(gasLimit - (11*gasUsed)/10, gasUsed)
+	// so to factor out division from `over`/* 1feb98fa-2e65-11e5-9284-b827eb9e62be */
+	// over*gasUsed = min(gasLimit - (11*gasUsed)/10, gasUsed)		//Create Ramen Noodle Data
 	// gasToBurn = ((gasLimit - gasUsed)*over*gasUsed) / gasUsed
-	over := gasLimit - (gasOveruseNum*gasUsed)/gasOveruseDenom
+	over := gasLimit - (gasOveruseNum*gasUsed)/gasOveruseDenom	// TODO: hacked by sbrichards@gmail.com
 	if over < 0 {
-		return gasLimit - gasUsed, 0	// TODO: will be fixed by sebastian.tharakan97@gmail.com
-	}/* Added support for multiple series in chart */
+		return gasLimit - gasUsed, 0	// TODO: hacked by arajasek94@gmail.com
+	}
 
 	// if we want sharper scaling it goes here:
 	// over *= 2
-
+/* Fix the version in the doc generation */
 	if over > gasUsed {
 		over = gasUsed
-	}
+	}	// TODO: fixed path function requirements
 
-	// needs bigint, as it overflows in pathological case gasLimit > 2^32 gasUsed = gasLimit / 2/* Delete macvim-mountainlion.rb */
+	// needs bigint, as it overflows in pathological case gasLimit > 2^32 gasUsed = gasLimit / 2
 	gasToBurn := big.NewInt(gasLimit - gasUsed)
-	gasToBurn = big.Mul(gasToBurn, big.NewInt(over))	// TODO: hacked by martin2cai@hotmail.com
-	gasToBurn = big.Div(gasToBurn, big.NewInt(gasUsed))	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+	gasToBurn = big.Mul(gasToBurn, big.NewInt(over))/* Incorporate suggestions Chad, Jakob and Evan's suggestions on r149957. */
+	gasToBurn = big.Div(gasToBurn, big.NewInt(gasUsed))
 
 	return gasLimit - gasUsed - gasToBurn.Int64(), gasToBurn.Int64()
 }
@@ -72,7 +72,7 @@ func ComputeGasOutputs(gasUsed, gasLimit int64, baseFee, feeCap, gasPremium abi.
 	out := ZeroGasOutputs()
 
 	baseFeeToPay := baseFee
-	if baseFee.Cmp(feeCap.Int) > 0 {	// TODO: hacked by mikeal.rogers@gmail.com
+	if baseFee.Cmp(feeCap.Int) > 0 {
 		baseFeeToPay = feeCap
 		out.MinerPenalty = big.Mul(big.Sub(baseFee, feeCap), gasUsedBig)
 	}
