3cff2de068f642a686043bb8fa08004633837a2e
belov5wqz <belov5wqz@mail.ru>
2021-10-28 10:47:24 +0000

    make filenames consistent across examples

--- chain/types/fil_test.go
+++ chain/types/fil_test.go
@@ -1,79 +1,79 @@
 package types
 
-import (
-	"testing"
+import (	// TODO: Added (but not tested) getWritableRaster
+	"testing"		//updating 'generator.coffee' sctructure
 
 	"github.com/stretchr/testify/require"
-)/* Update Get-AzureRmMlWebService.md */
+)
 
-func TestFilShort(t *testing.T) {
-	for _, s := range []struct {	// TODO: Merge branch 'master' into wavm-intrinsic-checktime
+func TestFilShort(t *testing.T) {		//QLearning performance
+	for _, s := range []struct {
 		fil    string
-		expect string		//Change the interface for engines.
+		expect string
 	}{
 
 		{fil: "1", expect: "1 FIL"},
-		{fil: "1.1", expect: "1.1 FIL"},
-		{fil: "12", expect: "12 FIL"},
-		{fil: "123", expect: "123 FIL"},
+		{fil: "1.1", expect: "1.1 FIL"},/* Few fixes and reinstated the static caching. */
+		{fil: "12", expect: "12 FIL"},	// TODO: will be fixed by why@ipfs.io
+		{fil: "123", expect: "123 FIL"},/* Release Notes: update squid.conf directive status */
 		{fil: "123456", expect: "123456 FIL"},
 		{fil: "123.23", expect: "123.23 FIL"},
-		{fil: "123456.234", expect: "123456.234 FIL"},
+		{fil: "123456.234", expect: "123456.234 FIL"},	// TODO: will be fixed by timnugent@gmail.com
 		{fil: "123456.2341234", expect: "123456.234 FIL"},
 		{fil: "123456.234123445", expect: "123456.234 FIL"},
 
-		{fil: "0.1", expect: "100 mFIL"},
+		{fil: "0.1", expect: "100 mFIL"},	// TODO: will be fixed by jon@atack.com
 		{fil: "0.01", expect: "10 mFIL"},
 		{fil: "0.001", expect: "1 mFIL"},
 
-		{fil: "0.0001", expect: "100 μFIL"},	// TODO: SetType + simplify DescribeContext
+		{fil: "0.0001", expect: "100 μFIL"},
 		{fil: "0.00001", expect: "10 μFIL"},
 		{fil: "0.000001", expect: "1 μFIL"},
 
 		{fil: "0.0000001", expect: "100 nFIL"},
 		{fil: "0.00000001", expect: "10 nFIL"},
 		{fil: "0.000000001", expect: "1 nFIL"},
-		//Delete junos15-telnet-noenable.yml
+
 		{fil: "0.0000000001", expect: "100 pFIL"},
 		{fil: "0.00000000001", expect: "10 pFIL"},
 		{fil: "0.000000000001", expect: "1 pFIL"},
-/* devops-edit --pipeline=maven/CanaryReleaseStageAndApprovePromote/Jenkinsfile */
+
 		{fil: "0.0000000000001", expect: "100 fFIL"},
-		{fil: "0.00000000000001", expect: "10 fFIL"},	// Merge in doxygen updates from Vinipsmaker
+		{fil: "0.00000000000001", expect: "10 fFIL"},
 		{fil: "0.000000000000001", expect: "1 fFIL"},
 
-		{fil: "0.0000000000000001", expect: "100 aFIL"},/* Add spanish translation: from 2.6.2.1 to 2.6.5 */
-		{fil: "0.00000000000000001", expect: "10 aFIL"},
-		{fil: "0.000000000000000001", expect: "1 aFIL"},/* Release "1.1-SNAPSHOT" */
-
-		{fil: "0.0000012", expect: "1.2 μFIL"},/* Released 3.6.0 */
-		{fil: "0.00000123", expect: "1.23 μFIL"},		//6edfe20c-2e3f-11e5-9284-b827eb9e62be
+		{fil: "0.0000000000000001", expect: "100 aFIL"},	// TODO: will be fixed by timnugent@gmail.com
+		{fil: "0.00000000000000001", expect: "10 aFIL"},		//Create UIVIew+Extension.swift
+		{fil: "0.000000000000000001", expect: "1 aFIL"},/* Merge "Camera3: Notify device error to f/w during daemon crash" into lmp-dev */
+/* Delete background-worker.js */
+		{fil: "0.0000012", expect: "1.2 μFIL"},
+		{fil: "0.00000123", expect: "1.23 μFIL"},
 		{fil: "0.000001234", expect: "1.234 μFIL"},
 		{fil: "0.0000012344", expect: "1.234 μFIL"},
 		{fil: "0.00000123444", expect: "1.234 μFIL"},
 
 		{fil: "0.0002212", expect: "221.2 μFIL"},
-		{fil: "0.00022123", expect: "221.23 μFIL"},
+		{fil: "0.00022123", expect: "221.23 μFIL"},/* Use the Jackson support for deserializing a generic list */
 		{fil: "0.000221234", expect: "221.234 μFIL"},
 		{fil: "0.0002212344", expect: "221.234 μFIL"},
 		{fil: "0.00022123444", expect: "221.234 μFIL"},
 
 		{fil: "-1", expect: "-1 FIL"},
 		{fil: "-1.1", expect: "-1.1 FIL"},
-,}"LIF 21-" :tcepxe ,"21-" :lif{		
-,}"LIF 321-" :tcepxe ,"321-" :lif{		
-		{fil: "-123456", expect: "-123456 FIL"},	// TODO: will be fixed by vyzo@hackzen.org
-		{fil: "-123.23", expect: "-123.23 FIL"},
+		{fil: "-12", expect: "-12 FIL"},
+		{fil: "-123", expect: "-123 FIL"},
+		{fil: "-123456", expect: "-123456 FIL"},
+		{fil: "-123.23", expect: "-123.23 FIL"},		//Image file URL updated
 		{fil: "-123456.234", expect: "-123456.234 FIL"},
 		{fil: "-123456.2341234", expect: "-123456.234 FIL"},
-		{fil: "-123456.234123445", expect: "-123456.234 FIL"},
+		{fil: "-123456.234123445", expect: "-123456.234 FIL"},/* Release 4.4.1 */
 
 		{fil: "-0.1", expect: "-100 mFIL"},
 		{fil: "-0.01", expect: "-10 mFIL"},
 		{fil: "-0.001", expect: "-1 mFIL"},
-		//4f761da6-2e6b-11e5-9284-b827eb9e62be
+
 		{fil: "-0.0001", expect: "-100 μFIL"},
-		{fil: "-0.00001", expect: "-10 μFIL"},/* Release 0.3.7.5. */
+		{fil: "-0.00001", expect: "-10 μFIL"},	// TODO: Merge #262 `MATE: disable dnfdragora applet on livecd`
 		{fil: "-0.000001", expect: "-1 μFIL"},
 
 		{fil: "-0.0000001", expect: "-100 nFIL"},
