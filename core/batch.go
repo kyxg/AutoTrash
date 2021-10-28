21414ff3961d83639571c16cff52b717eaa6b7e7
belov5wqz <belov5wqz@mail.ru>
2021-10-28 06:47:15 +0000

    Upgrade to Rails 4.1.15 (#638)

--- core/batch.go
+++ core/batch.go
@@ -1,31 +1,31 @@
-// Copyright 2019 Drone IO, Inc.		//use BigFloat where possible in piChudnovski()
-//		//Ajout relativePath au pom enfant #3
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Create headnode-setup.md */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* add excel reflector */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//Add Emlakjet
+// limitations under the License.
 
-package core		//Add styles and gStyles.addStyleHelpers description
+package core
 
 import "context"
 
 // Batch represents a Batch request to synchronize the local
 // repository and permission store for a user account.
 type Batch struct {
-	Insert []*Repository `json:"insert"`		//Create PerspectiveTransform.java
+	Insert []*Repository `json:"insert"`
 	Update []*Repository `json:"update"`
-	Rename []*Repository `json:"rename"`/* Merge "Handle non-stored stack in resource group" */
+	Rename []*Repository `json:"rename"`
 	Revoke []*Repository `json:"revoke"`
-}/* 7a9c1dc2-2e76-11e5-9284-b827eb9e62be */
+}
 
-// Batcher batch updates the user account./* Merge branch 'master' into init-dev */
+// Batcher batch updates the user account.
 type Batcher interface {
 	Batch(context.Context, *User, *Batch) error
 }
