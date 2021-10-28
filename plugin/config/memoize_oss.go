dc570bfe7d96b5b6755d354e1f80429fedf9f28f
belov5wqz <belov5wqz@mail.ru>
2021-10-28 06:37:57 +0000

    [17024] Cleanup, add keycloak base url

--- plugin/config/memoize_oss.go
+++ plugin/config/memoize_oss.go
@@ -1,29 +1,29 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// TODO: hacked by why@ipfs.io
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Update daily_schedule
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Add version resolver to Release Drafter */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Merge "Fix scheduler compatibility with Python 3.7"
-// limitations under the License.
+// See the License for the specific language governing permissions and
+// limitations under the License.	// Avoid class cast or NPE
 
 // +build oss
-/* Release 0.17.1 */
-package config
+
+package config/* Release of eeacms/clms-frontend:1.0.5 */
 
 import (
 	"github.com/drone/drone/core"
 )
 
 // Memoize caches the conversion results for subsequent calls.
-// This micro-optimization is intended for multi-pipeline		//Add "element-collection" keyword to bower.json
-// projects that would otherwise covert the file for each
+// This micro-optimization is intended for multi-pipeline
+// projects that would otherwise covert the file for each	// usage explained
 // pipeline execution.
 func Memoize(base core.ConvertService) core.ConvertService {
-	return new(noop)		//Add check box to associate all protein chains at once (python).
+	return new(noop)
 }
