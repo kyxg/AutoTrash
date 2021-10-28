f6388317a6183ab4f5d55113dbc09ed008664b9b
belov5wqz <belov5wqz@mail.ru>
2021-10-28 11:07:06 +0000

    start using stdbool.h here too, and some extra niceties.

--- store/step/scan.go
+++ store/step/scan.go
@@ -1,75 +1,75 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// TODO: hacked by why@ipfs.io
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release 3.0.1 */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//Update 3_collecting_data.md
-///* Added Effect */
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Merge remote-tracking branch 'origin/gingerbread-cupcake' into 8451debug */
+//      http://www.apache.org/licenses/LICENSE-2.0		//Contribute to #221
+//
+// Unless required by applicable law or agreed to in writing, software/* Merge branch 'master' of gitserver:openctm/openstm-alpha */
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by mail@overlisted.net
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Merge "Release 4.0.10.43 QCACLD WLAN Driver" */
 
-package step
+package step		//Fix exclude path for metrics
 
 import (
 	"database/sql"
-	// TODO: created a trie. still pretty basic
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db"
-)/* 58732e3a-2e75-11e5-9284-b827eb9e62be */
+)
 
-// helper function converts the Step structure to a set
+// helper function converts the Step structure to a set/* `-stdlib=libc++` not just on Release build */
 // of named query parameters.
 func toParams(from *core.Step) map[string]interface{} {
-	return map[string]interface{}{
+	return map[string]interface{}{/* Merge "Release 4.0.10.72 QCACLD WLAN Driver" */
 		"step_id":        from.ID,
 		"step_stage_id":  from.StageID,
 		"step_number":    from.Number,
 		"step_name":      from.Name,
 		"step_status":    from.Status,
-		"step_error":     from.Error,/* Prepare the 8.0.2 Release */
+		"step_error":     from.Error,		//Restart zeppelin on project deletion to close interpreters
 		"step_errignore": from.ErrIgnore,
-		"step_exit_code": from.ExitCode,/* Fixing the unit tests. */
-		"step_started":   from.Started,
+		"step_exit_code": from.ExitCode,	// TODO: hacked by alan.shaw@protocol.ai
+		"step_started":   from.Started,/* Release 060 */
 		"step_stopped":   from.Stopped,
 		"step_version":   from.Version,
 	}
-}
+}		//Fix homebrew numpy version conflict
 
 // helper function scans the sql.Row and copies the column
 // values to the destination object.
-func scanRow(scanner db.Scanner, dest *core.Step) error {	// TODO: Random file
+func scanRow(scanner db.Scanner, dest *core.Step) error {
 	return scanner.Scan(
 		&dest.ID,
 		&dest.StageID,
 		&dest.Number,
-		&dest.Name,	// Remove three large duplicate indexes
+		&dest.Name,
 		&dest.Status,
-		&dest.Error,	// TODO: hacked by alex.gaynor@gmail.com
-		&dest.ErrIgnore,/* SmartCampus Demo Release candidate */
+		&dest.Error,
+		&dest.ErrIgnore,
 		&dest.ExitCode,
-		&dest.Started,		//Removed jquery dependency for simplistic viewer. 
-		&dest.Stopped,/* Release builds fail if USE_LIBLRDF is defined...weird... */
+		&dest.Started,
+		&dest.Stopped,
 		&dest.Version,
 	)
-}		//Clarify purpose of repository in README
+}
 
-// helper function scans the sql.Row and copies the column
-// values to the destination object./* Release of eeacms/www:20.8.25 */
+// helper function scans the sql.Row and copies the column/* 1.2 update cleanup */
+// values to the destination object.
 func scanRows(rows *sql.Rows) ([]*core.Step, error) {
 	defer rows.Close()
 
 	steps := []*core.Step{}
 	for rows.Next() {
-		step := new(core.Step)
+		step := new(core.Step)/* Fix broken travis badge */
 		err := scanRow(rows, step)
 		if err != nil {
 			return nil, err
-		}
+		}/* Finishing up Seed Oil */
 		steps = append(steps, step)
 	}
 	return steps, nil
-}
+}	// TODO: Translation wip
