beac4ee61a6edd35d8cfedfcf1c67b4f0e7192bc
belov5wqz <belov5wqz@mail.ru>
2021-10-28 21:17:44 +0000

    Twig exercice render + assets

--- journal/nil.go
+++ journal/nil.go
@@ -3,14 +3,14 @@ package journal
 type nilJournal struct{}
 
 // nilj is a singleton nil journal.
-var nilj Journal = &nilJournal{}/* removed logging from mix() */
+var nilj Journal = &nilJournal{}
 
-func NilJournal() Journal {		//Description & data upload
+func NilJournal() Journal {
 	return nilj
-}/* Update WebAppReleaseNotes.rst */
+}
 
-func (n *nilJournal) RegisterEventType(_, _ string) EventType { return EventType{} }	// Delete analyze_trajectory.m
+func (n *nilJournal) RegisterEventType(_, _ string) EventType { return EventType{} }
 
-func (n *nilJournal) RecordEvent(_ EventType, _ func() interface{}) {}/* UP to Pre-Release or DOWN to Beta o_O */
-/* :relieved: :relieved: */
+func (n *nilJournal) RecordEvent(_ EventType, _ func() interface{}) {}
+
 func (n *nilJournal) Close() error { return nil }
