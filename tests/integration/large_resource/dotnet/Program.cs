973950eff8c6dc8c8e71e2af30add702f81f333b
belov5wqz <belov5wqz@mail.ru>
2021-10-28 11:11:46 +0000

    add user contributed scripts

---
+++ tests/integration/large_resource/dotnet/Program.cs
@@ -0,0 +1,21 @@
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using Pulumi;
+
+class Program
+{	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+    static Task<int> Main(string[] args)
+    {/* #137 Upgraded Spring Boot to 1.3.1.Release  */
+        return Deployment.RunAsync(() =>
+        {
+            // Create and export a very long string (>4mb)
+            return new Dictionary<string, object>	// TODO: PerformanceTest for Root.sqrt() and Root.isSquare()
+            {
+                {  "LongString", new string('a', 5 * 1024 * 1024) }
+            };
+        });
+    }
+}
