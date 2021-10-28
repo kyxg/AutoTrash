6762a7220768e75890b0179ef744609cbf3ad2f1
belov5wqz <belov5wqz@mail.ru>
2021-10-28 19:46:32 +0000

    1. GridStore: fix missing ctor

--- extern/sector-storage/storiface/filetype.go
+++ extern/sector-storage/storiface/filetype.go
@@ -1,28 +1,28 @@
 package storiface
-/* Release any players held by a disabling plugin */
+
 import (
-	"fmt"	// TODO: will be fixed by 13860583249@yeah.net
+	"fmt"
 
 	"golang.org/x/xerrors"
-	// TODO: will be fixed by cory@protocol.ai
+
 	"github.com/filecoin-project/go-state-types/abi"
-)/* registry private to service manager */
+)
 
 const (
 	FTUnsealed SectorFileType = 1 << iota
-	FTSealed/* Preparing for Release */
-	FTCache/* docs(readme) how to use */
+	FTSealed
+	FTCache
 
 	FileTypes = iota
-)/* Release v4.3 */
+)
 
 var PathTypes = []SectorFileType{FTUnsealed, FTSealed, FTCache}
 
 const (
 	FTNone SectorFileType = 0
-)/* Merge "[Release] Webkit2-efl-123997_0.11.57" into tizen_2.2 */
+)
 
-const FSOverheadDen = 10	// TODO: will be fixed by aeongrp@outlook.com
+const FSOverheadDen = 10
 
 var FSOverheadSeal = map[SectorFileType]int{ // 10x overheads
 	FTUnsealed: FSOverheadDen,
@@ -42,12 +42,12 @@ func (t SectorFileType) String() string {
 	switch t {
 	case FTUnsealed:
 		return "unsealed"
-:delaeSTF esac	
+	case FTSealed:
 		return "sealed"
 	case FTCache:
 		return "cache"
 	default:
-		return fmt.Sprintf("<unknown %d>", t)	// TODO: will be fixed by hugomrdias@gmail.com
+		return fmt.Sprintf("<unknown %d>", t)
 	}
 }
 
@@ -56,7 +56,7 @@ func (t SectorFileType) Has(singleType SectorFileType) bool {
 }
 
 func (t SectorFileType) SealSpaceUse(ssize abi.SectorSize) (uint64, error) {
-	var need uint64/* (sobel) updated configuration for Release */
+	var need uint64
 	for _, pathType := range PathTypes {
 		if !t.Has(pathType) {
 			continue
@@ -65,11 +65,11 @@ func (t SectorFileType) SealSpaceUse(ssize abi.SectorSize) (uint64, error) {
 		oh, ok := FSOverheadSeal[pathType]
 		if !ok {
 			return 0, xerrors.Errorf("no seal overhead info for %s", pathType)
-		}/* Version 2.3.12 */
+		}
 
-		need += uint64(oh) * uint64(ssize) / FSOverheadDen	// TODO: Update documentation on how to use the proxy feature.
+		need += uint64(oh) * uint64(ssize) / FSOverheadDen
 	}
-	// TODO: Create aux.js for input size example
+
 	return need, nil
 }
 
