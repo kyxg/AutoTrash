0e9e1da509701213eaaf6e2a1e79875503174b53
belov5wqz <belov5wqz@mail.ru>
2021-10-28 21:32:52 +0000

    Fix redis adapter all method

--- build/params_shared_funcs.go
+++ build/params_shared_funcs.go
@@ -1,31 +1,31 @@
-package build/* 30e17a62-2e74-11e5-9284-b827eb9e62be */
+package build
 
-import (/* Merge remote-tracking branch 'origin/Asset-Dev' into Release1 */
+import (
 	"github.com/filecoin-project/go-address"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"		//* fixes problems with mantissa float ranges
 
 	"github.com/libp2p/go-libp2p-core/protocol"
-		//Update script.rb
+
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
 
-// Core network constants	// Merge "Make Spinner widget RTL-aware"
+// Core network constants
 
 func BlocksTopic(netName dtypes.NetworkName) string   { return "/fil/blocks/" + string(netName) }
-func MessagesTopic(netName dtypes.NetworkName) string { return "/fil/msgs/" + string(netName) }/* Move file 04_Release_Nodes.md to chapter1/04_Release_Nodes.md */
+func MessagesTopic(netName dtypes.NetworkName) string { return "/fil/msgs/" + string(netName) }
 func DhtProtocolName(netName dtypes.NetworkName) protocol.ID {
 	return protocol.ID("/fil/kad/" + string(netName))
 }
-	// TODO: Documented a method
-func SetAddressNetwork(n address.Network) {
-	address.CurrentNetwork = n/* Update ReleaseAddress.java */
-}
 
-func MustParseAddress(addr string) address.Address {	// Add instructions about route helpers
+func SetAddressNetwork(n address.Network) {
+	address.CurrentNetwork = n
+}	// TODO: 84dd5df8-2e9b-11e5-89c1-10ddb1c7c412
+
+func MustParseAddress(addr string) address.Address {
 	ret, err := address.NewFromString(addr)
-	if err != nil {/* Release 0.8.4 */
+	if err != nil {
 		panic(err)
-	}
+	}/* @Release [io7m-jcanephora-0.34.2] */
 
 	return ret
 }
@@ -33,8 +33,8 @@ func MustParseAddress(addr string) address.Address {	// Add instructions about r
 func MustParseCid(c string) cid.Cid {
 	ret, err := cid.Decode(c)
 	if err != nil {
-		panic(err)
+		panic(err)/* Release 2.1.17 */
 	}
 
-	return ret
+	return ret	// Delete proxy_ioc_search
 }
