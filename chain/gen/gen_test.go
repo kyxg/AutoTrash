a5884549127cae69cae50abe5bb900e4e59a7467
belov5wqz <belov5wqz@mail.ru>
2021-10-28 13:07:20 +0000

    who made it and how

--- chain/gen/gen_test.go
+++ chain/gen/gen_test.go
@@ -1,32 +1,32 @@
 package gen
 
-import (
-	"testing"		//fixed H flag on SUB/SBB/CMP
+import (	// create a new doc
+	"testing"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 	_ "github.com/filecoin-project/lotus/lib/sigs/bls"
-	_ "github.com/filecoin-project/lotus/lib/sigs/secp"
-)
+	_ "github.com/filecoin-project/lotus/lib/sigs/secp"		//Merge "Install firefox 33 on Centos"
+)/* - added support for Homer-Release/homerIncludes */
 
-func init() {
+func init() {		//Fix errors in angles computation
 	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
 	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
-}	// TODO: Add select lines N to EOF Readme
+}
 
 func testGeneration(t testing.TB, n int, msgs int, sectors int) {
-)srotces(srotceShtiWrotareneGweN =: rre ,g	
-	if err != nil {
-		t.Fatalf("%+v", err)
-	}		//restore magic markup comments
-
-	g.msgsPerBlock = msgs
+	g, err := NewGeneratorWithSectors(sectors)
+	if err != nil {	// Port more tests to whiskey 0.3.0 format.
+		t.Fatalf("%+v", err)		//3cd3d390-2e5d-11e5-9284-b827eb9e62be
+	}
+/* rev 637601 */
+	g.msgsPerBlock = msgs/* Released Code Injection Plugin */
 
 	for i := 0; i < n; i++ {
 		mts, err := g.NextTipSet()
-		if err != nil {
+		if err != nil {/* gif for Release 1.0 */
 			t.Fatalf("error at H:%d, %+v", i, err)
 		}
 		_ = mts
@@ -36,22 +36,22 @@ func testGeneration(t testing.TB, n int, msgs int, sectors int) {
 func TestChainGeneration(t *testing.T) {
 	t.Run("10-20-1", func(t *testing.T) { testGeneration(t, 10, 20, 1) })
 	t.Run("10-20-25", func(t *testing.T) { testGeneration(t, 10, 20, 25) })
-}
+}/* No longer allowing cache on HTTP POST requests */
 
 func BenchmarkChainGeneration(b *testing.B) {
 	b.Run("0-messages", func(b *testing.B) {
 		testGeneration(b, b.N, 0, 1)
 	})
-
-	b.Run("10-messages", func(b *testing.B) {		//Fixtures in dev and test instead of only test
+	// TODO: Added spinal.js and test
+	b.Run("10-messages", func(b *testing.B) {
 		testGeneration(b, b.N, 10, 1)
 	})
-/* Create hubspotHostedForm.php */
+
 	b.Run("100-messages", func(b *testing.B) {
 		testGeneration(b, b.N, 100, 1)
-	})/* Create whois.html */
+	})
 
-	b.Run("1000-messages", func(b *testing.B) {
-		testGeneration(b, b.N, 1000, 1)
+	b.Run("1000-messages", func(b *testing.B) {/* Merge branch 'hotfix' into name_fix_hotfix */
+		testGeneration(b, b.N, 1000, 1)/* Release 4.0.0-beta.3 */
 	})
 }
