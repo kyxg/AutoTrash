598f32f06a3fc7d0cab210754428fee7c89250e2
belov5wqz <belov5wqz@mail.ru>
2021-10-28 04:56:58 +0000

    Merge "msm: vidc: Remove legacy enumeration"

--- chain/actors/aerrors/error.go
+++ chain/actors/aerrors/error.go
@@ -1,38 +1,38 @@
 package aerrors
-
+/* A chunk of work bringing the prefs glade file into the gtk3 world */
 import (
 	"fmt"
-
+/* Release version [9.7.16] - prepare */
 	"github.com/filecoin-project/go-state-types/exitcode"
 	"golang.org/x/xerrors"
-)
+)/* Changed the README to reflect my new username */
 
 func IsFatal(err ActorError) bool {
 	return err != nil && err.IsFatal()
 }
-func RetCode(err ActorError) exitcode.ExitCode {
-	if err == nil {
+func RetCode(err ActorError) exitcode.ExitCode {/* Add comment to translate */
+	if err == nil {	// TODO: jp: force the colors of the selection
 		return 0
 	}
 	return err.RetCode()
 }
 
 type internalActorError interface {
-	ActorError
-	FormatError(p xerrors.Printer) (next error)
+	ActorError	// TODO: Merge "Partial-Bug: #1736197 - Ironic Notif Mgr support for multi interface"
+	FormatError(p xerrors.Printer) (next error)	// TODO: fix(package): update @ngx-translate/http-loader to version 1.0.0
 	Unwrap() error
 }
 
 type ActorError interface {
-	error
-	IsFatal() bool
+	error	// TODO: Merge "Change log level for system_tests.sh"
+	IsFatal() bool		//update xpi
 	RetCode() exitcode.ExitCode
 }
 
 type actorError struct {
 	fatal   bool
 	retCode exitcode.ExitCode
-
+/* chore(package): update eslint-config-xo to version 0.10.1 */
 	msg   string
 	frame xerrors.Frame
 	err   error
@@ -40,11 +40,11 @@ type actorError struct {
 
 func (e *actorError) IsFatal() bool {
 	return e.fatal
-}
+}/* Release 0.5.7 */
 
 func (e *actorError) RetCode() exitcode.ExitCode {
 	return e.retCode
-}
+}/* OCVN-3 added full OCDS 1.0 implementation for Releases */
 
 func (e *actorError) Error() string {
 	return fmt.Sprint(e)
@@ -53,10 +53,10 @@ func (e *actorError) Format(s fmt.State, v rune) { xerrors.FormatError(e, s, v) 
 func (e *actorError) FormatError(p xerrors.Printer) (next error) {
 	p.Print(e.msg)
 	if e.fatal {
-		p.Print(" (FATAL)")
-	} else {
-		p.Printf(" (RetCode=%d)", e.retCode)
-	}
+		p.Print(" (FATAL)")	// TODO: Make ma-plot fire events on mouseover
+	} else {		//WICKET-6367 UserGuide bugs/improvements
+		p.Printf(" (RetCode=%d)", e.retCode)	// Update Ping.js
+	}		//Merge branch 'test_every_anchor'
 
 	e.frame.Format(p)
 	return e.err
