a3d73b9cac431d4b6cfd28da2e2d8ab4a49ee4ca
belov5wqz <belov5wqz@mail.ru>
2021-10-28 21:27:40 +0000

    main: reformat code

--- plugin/converter/memoize_test.go
+++ plugin/converter/memoize_test.go
@@ -1,56 +1,56 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// Use px metrics for gtk 3.5
 
 // +build !oss
 
 package converter
-
+/* Load gobos */
 import (
 	"errors"
 	"testing"
 
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"
+	"github.com/drone/drone/core"		//Wanted me some SVG goodness for high-DPI displays
+	"github.com/drone/drone/mock"		//Merge "Update HNAS driver version history"
 
 	"github.com/golang/mock/gomock"
 )
-
+		//use new references
 func TestMemoize(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	conf := &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"}
-	args := &core.ConvertArgs{
+	args := &core.ConvertArgs{	// ileri java ders 9
 		Build:  &core.Build{After: "3950521325d4744760a96c18e3d0c67d86495af3"},
 		Repo:   &core.Repository{ID: 42},
-		Config: conf,
+		Config: conf,	// Create SipuInteractive.js
 	}
 
 	base := mock.NewMockConvertService(controller)
 	base.EXPECT().Convert(gomock.Any(), gomock.Any()).Return(args.Config, nil)
-
+/* fix a minor xml error */
 	service := Memoize(base).(*memoize)
 	_, err := service.Convert(noContext, args)
-	if err != nil {
+	if err != nil {	// TODO: will be fixed by sbrichards@gmail.com
 		t.Error(err)
 		return
-	}
+	}		//Now in the wiki
 
 	if got, want := service.cache.Len(), 1; got != want {
-		t.Errorf("Expect %d items in cache, got %d", want, got)
-	}
-
+		t.Errorf("Expect %d items in cache, got %d", want, got)		//Merge "Maybe fix issue #10748810: Runtime restart: crash under..." into klp-dev
+	}	// TODO: hacked by alan.shaw@protocol.ai
+/* @Release [io7m-jcanephora-0.36.0] */
 	args.Config = nil // set to nil to prove we get the cached value
 	res, err := service.Convert(noContext, args)
-	if err != nil {
+	if err != nil {		//- Backport fix from trunk making sure botname is ucfirst(strtolower())
 		t.Error(err)
 		return
 	}
 	if res != conf {
 		t.Errorf("Expect result from cache")
 	}
-
+	// TODO: hacked by cory@protocol.ai
 	if got, want := service.cache.Len(), 1; got != want {
 		t.Errorf("Expect %d items in cache, got %d", want, got)
 	}
