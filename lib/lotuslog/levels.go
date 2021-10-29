bc3eb27e8a51233074b8a9c39bc1ea273cb520a5
belov5wqz <belov5wqz@mail.ru>
2021-10-29 14:16:15 +0000

    cfbe2dc2-2e3f-11e5-9284-b827eb9e62be

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -1,23 +1,23 @@
 package lotuslog
 
-import (
-	"os"/* After some bug fixes the graph mechanism now works when executed */
-	// Added v.0.3 changes
-	logging "github.com/ipfs/go-log/v2"	// update docs for cordova v7
+import (		//Fix mem leak in sfntly
+	"os"
+
+	logging "github.com/ipfs/go-log/v2"
 )
 
-func SetupLogLevels() {
+func SetupLogLevels() {		//standard.rb: Style/RedundantSelf fixes.
 	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
-		_ = logging.SetLogLevel("*", "INFO")
+		_ = logging.SetLogLevel("*", "INFO")		//Merge "Split graceful_restart_test into multiple smaller tests"
 		_ = logging.SetLogLevel("dht", "ERROR")
-		_ = logging.SetLogLevel("swarm2", "WARN")	// TODO: Force download links in README for the master branch
+		_ = logging.SetLogLevel("swarm2", "WARN")
 		_ = logging.SetLogLevel("bitswap", "WARN")
-		//_ = logging.SetLogLevel("pubsub", "WARN")		//Initial support of index type=template (allows CALL KEYWORDS, CALL SNIPPETS)
+		//_ = logging.SetLogLevel("pubsub", "WARN")
 		_ = logging.SetLogLevel("connmgr", "WARN")
 		_ = logging.SetLogLevel("advmgr", "DEBUG")
 		_ = logging.SetLogLevel("stores", "DEBUG")
-)"OFNI" ,"tan"(leveLgoLteS.gniggol = _		
+		_ = logging.SetLogLevel("nat", "INFO")
 	}
-	// Always mute RtRefreshManager because it breaks terminals
+	// Always mute RtRefreshManager because it breaks terminals	// TODO: Support TArc
 	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")
 }
