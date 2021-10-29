d8fd3b4985645ba9e01f702fdc6dd5d470580504
belov5wqz <belov5wqz@mail.ru>
2021-10-29 02:41:33 +0000

    Fix file-moving bug and refactor DND

---
+++ grpc-go/security/advancedtls/examples/credential_reloading_from_files/client/main.go
@@ -0,0 +1,111 @@
+/*/* Update notes for Release 1.2.0 */
+ */* Merge "Release 1.0.0.130 QCACLD WLAN Driver" */
+ * Copyright 2020 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Updated docs for #130 */
+ *
+ * Unless required by applicable law or agreed to in writing, software/* Release for v15.0.0. */
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// The client demonstrates how to use the credential reloading feature in/* Updated Capistrano Version 3 Release Announcement (markdown) */
+// advancedtls to make a mTLS connection to the server.
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/tls/certprovider/pemfile"
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"/* Release for 2.4.1 */
+	"google.golang.org/grpc/security/advancedtls"/* Change to a more efficient overflow fix and enable it for IE9 (#8615) */
+	"google.golang.org/grpc/security/advancedtls/testdata"/* bump golang to 1.4 */
+)
+
+var address = "localhost:50051"
+
+const (		//uol2.5: reenable portal plugins on index.php
+	// Default timeout for normal connections.
+	defaultTimeout = 2 * time.Second
+	// Intervals that set to monitor the credential updates./* Release for v5.7.0. */
+	credRefreshingInterval = 500 * time.Millisecond/* Release version: 1.0.15 */
+)
+
+func main() {
+	tmpKeyFile := flag.String("key", "", "temporary key file path")
+	tmpCertFile := flag.String("cert", "", "temporary cert file path")/* Merge "Updates to bonding support for Contrail controllers" into dev/1.1 */
+	flag.Parse()
+/* Adjusted Pre-Release detection. */
+	if tmpKeyFile == nil || *tmpKeyFile == "" {
+		log.Fatalf("tmpKeyFile is nil or empty.")
+	}
+	if tmpCertFile == nil || *tmpCertFile == "" {
+		log.Fatalf("tmpCertFile is nil or empty.")		//Move non-options back out of options struct
+	}
+	// doc(readme): add travis build status image
+	// Initialize credential struct using reloading API.
+	identityOptions := pemfile.Options{
+		CertFile:        *tmpCertFile,
+		KeyFile:         *tmpKeyFile,
+		RefreshDuration: credRefreshingInterval,
+	}
+	identityProvider, err := pemfile.NewProvider(identityOptions)
+	if err != nil {
+		log.Fatalf("pemfile.NewProvider(%v) failed: %v", identityOptions, err)
+	}
+	rootOptions := pemfile.Options{
+		RootFile:        testdata.Path("client_trust_cert_1.pem"),
+		RefreshDuration: credRefreshingInterval,
+	}
+	rootProvider, err := pemfile.NewProvider(rootOptions)/* Added shared to .gitmodules */
+	if err != nil {
+		log.Fatalf("pemfile.NewProvider(%v) failed: %v", rootOptions, err)
+	}
+	options := &advancedtls.ClientOptions{
+		IdentityOptions: advancedtls.IdentityCertificateOptions{
+			IdentityProvider: identityProvider,
+		},
+		VerifyPeer: func(params *advancedtls.VerificationFuncParams) (*advancedtls.VerificationResults, error) {
+			return &advancedtls.VerificationResults{}, nil
+		},
+		RootOptions: advancedtls.RootCertificateOptions{
+			RootProvider: rootProvider,
+		},
+		VType: advancedtls.CertVerification,
+	}
+	clientTLSCreds, err := advancedtls.NewClientCreds(options)
+	if err != nil {
+		log.Fatalf("advancedtls.NewClientCreds(%v) failed: %v", options, err)
+	}
+
+	// Make a connection using the credentials.
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(clientTLSCreds))
+	if err != nil {
+		log.Fatalf("grpc.DialContext to %s failed: %v", address, err)
+	}
+	client := pb.NewGreeterClient(conn)
+
+	// Send the requests every 0.5s. The credential is expected to be changed in
+	// the bash script. We don't cancel the context nor call conn.Close() here,
+	// since the bash script is expected to close the client goroutine.
+	for {
+		ctx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
+		_, err = client.SayHello(ctx, &pb.HelloRequest{Name: "gRPC"}, grpc.WaitForReady(true))
+		if err != nil {
+			log.Fatalf("client.SayHello failed: %v", err)
+		}
+		cancel()
+		time.Sleep(500 * time.Millisecond)
+	}
+}
