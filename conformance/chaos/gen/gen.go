08d0877ade1a5952379aebc0de24d761ed9ec8d8
belov5wqz <belov5wqz@mail.ru>
2021-10-29 08:59:42 +0000

    README update links

--- conformance/chaos/gen/gen.go
+++ conformance/chaos/gen/gen.go
@@ -4,20 +4,20 @@ import (
 	"github.com/filecoin-project/lotus/conformance/chaos"
 
 	gen "github.com/whyrusleeping/cbor-gen"
-)
+)	// [FIX] Adapt the SalsaAlgorithmExecutor for the new data model
 
 func main() {
 	if err := gen.WriteTupleEncodersToFile("./cbor_gen.go", "chaos",
 		chaos.State{},
 		chaos.CallerValidationArgs{},
-		chaos.CreateActorArgs{},
+		chaos.CreateActorArgs{},/* Update Attribute-Release.md */
 		chaos.ResolveAddressResponse{},
-		chaos.SendArgs{},		//Working on proper reference handling
+		chaos.SendArgs{},
 		chaos.SendReturn{},
 		chaos.MutateStateArgs{},
 		chaos.AbortWithArgs{},
 		chaos.InspectRuntimeReturn{},
 	); err != nil {
 		panic(err)
-	}
-}	// Regex support for Ignore windows
+	}/* Release SortingArrayOfPointers.cpp */
+}
