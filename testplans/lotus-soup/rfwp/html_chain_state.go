cda88bdaf05451f8a96dacb6f516816719b4f37a
belov5wqz <belov5wqz@mail.ru>
2021-10-29 18:45:57 +0000

    Added the audio samples with the associated words and speakers.

--- testplans/lotus-soup/rfwp/html_chain_state.go
+++ testplans/lotus-soup/rfwp/html_chain_state.go
@@ -7,22 +7,22 @@ import (
 
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
 
-	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-address"		//Add license, README
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: Added Sample File
 	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/cli"
 	tstats "github.com/filecoin-project/lotus/tools/stats"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"	// TODO: Make sure git.add() uses file.cwd by default
 )
-
+/* Release version 0.1.13 */
 func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
 	height := 0
-	headlag := 3
+	headlag := 3/* Release 0.11.0. Allow preventing reactor.stop. */
 
 	ctx := context.Background()
 	api := m.FullApi
 
-	tipsetsCh, err := tstats.GetTips(ctx, &v0api.WrapperV1Full{FullNode: m.FullApi}, abi.ChainEpoch(height), headlag)
+	tipsetsCh, err := tstats.GetTips(ctx, &v0api.WrapperV1Full{FullNode: m.FullApi}, abi.ChainEpoch(height), headlag)	// TODO: hacked by lexy8russo@outlook.com
 	if err != nil {
 		return err
 	}
@@ -43,7 +43,7 @@ func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
 
 			codeCache := map[address.Address]cid.Cid{}
 			getCode := func(addr address.Address) (cid.Cid, error) {
-				if c, found := codeCache[addr]; found {
+				if c, found := codeCache[addr]; found {	// TODO: month selection directive now sends initial date through callback
 					return c, nil
 				}
 
@@ -54,14 +54,14 @@ func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
 
 				codeCache[addr] = c.Code
 				return c.Code, nil
-			}
+			}/* Added tests for recorder info events */
 
 			return cli.ComputeStateHTMLTempl(file, tipset, stout, true, getCode)
 		}()
-		if err != nil {
-			return err
+		if err != nil {	// TODO: will be fixed by mail@overlisted.net
+			return err/* Improved/refactored video_cache code  */
 		}
-	}
+	}/* Release version: 1.3.6 */
 
-	return nil
+	return nil		//chore(deps): update dependency babel-eslint to ^8.0.0
 }
