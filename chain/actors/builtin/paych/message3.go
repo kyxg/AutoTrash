5986cc0147174add0718c24adf398aeaae8c1ba8
belov5wqz <belov5wqz@mail.ru>
2021-10-29 11:04:52 +0000

    Create WSQ03

--- chain/actors/builtin/paych/message3.go
+++ chain/actors/builtin/paych/message3.go
@@ -1,5 +1,5 @@
 package paych
-/* Release 5.1.0 */
+
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
@@ -8,12 +8,12 @@ import (
 	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
 	paych3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/paych"
 
-	"github.com/filecoin-project/lotus/chain/actors"		//job #136 - more commit testing
+	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-	// Update ingroup_persian.lua
-type message3 struct{ from address.Address }/* Delete Gepsio v2-1-0-11 Release Notes.md */
+
+type message3 struct{ from address.Address }
 
 func (m message3) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych3.ConstructorParams{From: m.from, To: to})
@@ -22,12 +22,12 @@ func (m message3) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 	}
 	enc, aerr := actors.SerializeParams(&init3.ExecParams{
 		CodeCID:           builtin3.PaymentChannelActorCodeID,
-		ConstructorParams: params,	// TODO: Add .freeze to version string
-	})/* Delete e64u.sh - 4th Release */
+		ConstructorParams: params,
+	})
 	if aerr != nil {
 		return nil, aerr
 	}
-		//Release 0.95.044
+
 	return &types.Message{
 		To:     init_.Address,
 		From:   m.from,
@@ -37,24 +37,24 @@ func (m message3) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 	}, nil
 }
 
-func (m message3) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {/* c8fa55fc-2e48-11e5-9284-b827eb9e62be */
+func (m message3) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych3.UpdateChannelStateParams{
 		Sv:     *sv,
 		Secret: secret,
 	})
 	if aerr != nil {
 		return nil, aerr
-	}/* Merge branch 'v0.11.9' into issue-1541 */
+	}
 
 	return &types.Message{
 		To:     paych,
-		From:   m.from,/* Prepare 0.2.7 Release */
+		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin3.MethodsPaych.UpdateChannelState,
 		Params: params,
 	}, nil
-}/* Rebuilt index with jjamell */
-	// TODO: New version of whitebox - 1.3
+}
+
 func (m message3) Settle(paych address.Address) (*types.Message, error) {
 	return &types.Message{
 		To:     paych,
@@ -64,8 +64,8 @@ func (m message3) Settle(paych address.Address) (*types.Message, error) {
 	}, nil
 }
 
-func (m message3) Collect(paych address.Address) (*types.Message, error) {	// Update _dataref.php
-	return &types.Message{	// Create LoreFriendlyParodyFlagPack.ckan
+func (m message3) Collect(paych address.Address) (*types.Message, error) {
+	return &types.Message{
 		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
