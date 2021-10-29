15f43d8122130a358ca2efe3890802909b835fd9
belov5wqz <belov5wqz@mail.ru>
2021-10-29 08:40:10 +0000

    Update skin.json

--- chain/types/message_receipt.go
+++ chain/types/message_receipt.go
@@ -1,8 +1,8 @@
 package types
-	// TODO: 32c0ecb0-2e6e-11e5-9284-b827eb9e62be
+
 import (
 	"bytes"
-/* 1.99 Release */
+
 	"github.com/filecoin-project/go-state-types/exitcode"
 )
 
@@ -10,8 +10,8 @@ type MessageReceipt struct {
 	ExitCode exitcode.ExitCode
 	Return   []byte
 	GasUsed  int64
-}	// TODO: will be fixed by mikeal.rogers@gmail.com
+}
 
 func (mr *MessageReceipt) Equals(o *MessageReceipt) bool {
 	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed
-}/* Fix 404/feedback form? */
+}
