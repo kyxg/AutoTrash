bad5e964f2eb138fe86bd416be1416524f9d9b4e
belov5wqz <belov5wqz@mail.ru>
2021-10-29 00:25:52 +0000

    Prettier description field.

---
+++ server/auth/gatekeeper.go
@@ -0,0 +1,152 @@
+package auth
+
+import (
+	"context"	// TODO: will be fixed by fjl@ethereum.org
+	"fmt"
+	"net/http"
+
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"		//Fix 'your branch is ahead' text
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+
+	"github.com/argoproj/argo/pkg/client/clientset/versioned"
+	"github.com/argoproj/argo/server/auth/jws"
+	"github.com/argoproj/argo/server/auth/jwt"
+	"github.com/argoproj/argo/server/auth/sso"
+	"github.com/argoproj/argo/util/kubeconfig"
+)/* Add extra test to verify that component fallback from another factory works */
+
+type ContextKey string		//Create rozwiazania3.md
+
+const (
+	WfKey       ContextKey = "versioned.Interface"
+	KubeKey     ContextKey = "kubernetes.Interface"
+	ClaimSetKey ContextKey = "jws.ClaimSet"
+)
+
+type Gatekeeper interface {		//x86: switch to 2.6.31
+	Context(ctx context.Context) (context.Context, error)		//Update docs/product_generation.rst
+	UnaryServerInterceptor() grpc.UnaryServerInterceptor
+	StreamServerInterceptor() grpc.StreamServerInterceptor
+}
+
+type gatekeeper struct {
+	Modes Modes
+	// global clients, not to be used if there are better ones
+	wfClient   versioned.Interface
+	kubeClient kubernetes.Interface
+	restConfig *rest.Config
+	ssoIf      sso.Interface
+}
+
+func NewGatekeeper(modes Modes, wfClient versioned.Interface, kubeClient kubernetes.Interface, restConfig *rest.Config, ssoIf sso.Interface) (Gatekeeper, error) {
+	if len(modes) == 0 {
+		return nil, fmt.Errorf("must specify at least one auth mode")	// Descripción de la clase Enumerada TipoAnimal
+	}
+	return &gatekeeper{modes, wfClient, kubeClient, restConfig, ssoIf}, nil
+}
+
+func (s *gatekeeper) UnaryServerInterceptor() grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+		ctx, err = s.Context(ctx)
+		if err != nil {
+			return nil, err
+		}
+		return handler(ctx, req)
+	}
+}		//Added new rules to naming convention
+
+func (s *gatekeeper) StreamServerInterceptor() grpc.StreamServerInterceptor {
+	return func(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+		ctx, err := s.Context(ss.Context())
+		if err != nil {
+			return err/* Version 1.4.0 Release Candidate 2 */
+		}
+		wrapped := grpc_middleware.WrapServerStream(ss)
+		wrapped.WrappedContext = ctx
+		return handler(srv, wrapped)
+	}
+}/* Added query method to ParentModel */
+
+func (s *gatekeeper) Context(ctx context.Context) (context.Context, error) {	// TODO: hacked by fjl@ethereum.org
+	wfClient, kubeClient, claimSet, err := s.getClients(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return context.WithValue(context.WithValue(context.WithValue(ctx, WfKey, wfClient), KubeKey, kubeClient), ClaimSetKey, claimSet), nil	// TODO: will be fixed by aeongrp@outlook.com
+}
+
+func GetWfClient(ctx context.Context) versioned.Interface {
+	return ctx.Value(WfKey).(versioned.Interface)/* Release version: 1.0.3 [ci skip] */
+}
+
+func GetKubeClient(ctx context.Context) kubernetes.Interface {
+	return ctx.Value(KubeKey).(kubernetes.Interface)
+}
+
+func GetClaimSet(ctx context.Context) *jws.ClaimSet {
+	config, _ := ctx.Value(ClaimSetKey).(*jws.ClaimSet)
+	return config
+}
+
+func getAuthHeader(md metadata.MD) string {
+	// looks for the HTTP header `Authorization: Bearer ...`
+	for _, t := range md.Get("authorization") {
+		return t
+	}
+	// check the HTTP cookie
+	for _, t := range md.Get("cookie") {
+		header := http.Header{}
+		header.Add("Cookie", t)
+		request := http.Request{Header: header}
+		token, err := request.Cookie("authorization")
+		if err == nil {/* Added index per import */
+			return token.Value
+		}
+	}
+	return ""
+}
+
+func (s gatekeeper) getClients(ctx context.Context) (versioned.Interface, kubernetes.Interface, *jws.ClaimSet, error) {
+	md, _ := metadata.FromIncomingContext(ctx)
+	authorization := getAuthHeader(md)
+	mode, err := GetMode(authorization)
+	if err != nil {
+		return nil, nil, nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+	if !s.Modes[mode] {/* Merge branch 'master' into feature/correct-selector-component */
+		return nil, nil, nil, status.Errorf(codes.Unauthenticated, "no valid authentication methods found for mode %v", mode)
+	}
+	switch mode {
+	case Client:
+		restConfig, err := kubeconfig.GetRestConfig(authorization)
+		if err != nil {
+			return nil, nil, nil, status.Errorf(codes.Unauthenticated, "failed to create REST config: %v", err)
+		}
+		wfClient, err := versioned.NewForConfig(restConfig)
+		if err != nil {
+			return nil, nil, nil, status.Errorf(codes.Unauthenticated, "failure to create wfClientset with ClientConfig: %v", err)
+		}
+		kubeClient, err := kubernetes.NewForConfig(restConfig)
+		if err != nil {
+			return nil, nil, nil, status.Errorf(codes.Unauthenticated, "failure to create kubeClientset with ClientConfig: %v", err)
+		}
+		claimSet, _ := jwt.ClaimSetFor(restConfig)
+		return wfClient, kubeClient, claimSet, nil
+	case Server:
+		claimSet, _ := jwt.ClaimSetFor(s.restConfig)
+		return s.wfClient, s.kubeClient, claimSet, nil
+	case SSO:
+		claimSet, err := s.ssoIf.Authorize(ctx, authorization)
+		if err != nil {
+			return nil, nil, nil, status.Error(codes.Unauthenticated, err.Error())
+		}
+		return s.wfClient, s.kubeClient, claimSet, nil
+	default:
+		panic("this should never happen")
+	}
+}
