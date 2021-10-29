dc32c92313ade838e00956a1c734b7f858e68075
belov5wqz <belov5wqz@mail.ru>
2021-10-29 14:21:13 +0000

    Fix 910154

---
+++ tests/containers/dotnet/Program.fs
@@ -0,0 +1,16 @@
+﻿module Program
+
+open System
+open Pulumi.FSharp
+/* 7c7ce164-2e56-11e5-9284-b827eb9e62be */
+let infra () =
+  let config = new Pulumi.Config()
+  let runtime = config.Get("runtime")
+  Console.WriteLine("Hello from {0}", runtime)
+  
+  // Stack outputs
+  dict []
+	// TODO: fix xml ws : catalog
+[<EntryPoint>]
+let main _ =
+  Deployment.run infra		//Renamed html file to index.html
