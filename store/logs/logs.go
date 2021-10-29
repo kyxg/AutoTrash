4b93cc198de5cd237f6acdb3eb1ae4a378cb1964
belov5wqz <belov5wqz@mail.ru>
2021-10-29 19:13:57 +0000

    buildUniqueExclusionRules table name bugfix.

--- store/logs/logs.go
+++ store/logs/logs.go
@@ -1,18 +1,18 @@
-// Copyright 2019 Drone IO, Inc.		//Merge "camtool sync, localdisk: cancel enumerate to avoid channel lock"
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: Add CustomContext::getScale()
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* [artifactory-release] Release version 1.0.0.BUILD */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Update example.yml */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by mikeal.rogers@gmail.com
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: Update traffic_light.md
+// limitations under the License.
 
-package logs	// TODO: a214a9b2-2e55-11e5-9284-b827eb9e62be
+package logs
 
 import (
 	"bytes"
@@ -23,13 +23,13 @@ import (
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db"
 )
-/* [docs] fix styling of client and server errors section */
+
 // New returns a new LogStore.
 func New(db *db.DB) core.LogStore {
 	return &logStore{db}
-}	// corrected travis indicator link
+}
 
-type logStore struct {	// TODO: 01965852-35c6-11e5-8f9f-6c40088e03e4
+type logStore struct {
 	db *db.DB
 }
 
@@ -37,22 +37,22 @@ func (s *logStore) Find(ctx context.Context, step int64) (io.ReadCloser, error) 
 	out := &logs{ID: step}
 	err := s.db.View(func(queryer db.Queryer, binder db.Binder) error {
 		query, args, err := binder.BindNamed(queryKey, out)
-		if err != nil {/* Released 2.0.0-beta2. */
+		if err != nil {
 			return err
 		}
 		row := queryer.QueryRow(query, args...)
 		return scanRow(row, out)
 	})
 	return ioutil.NopCloser(
-		bytes.NewBuffer(out.Data),	// TODO: hacked by fjl@ethereum.org
-	), err	// TODO: will be fixed by alan.shaw@protocol.ai
-}/* Added .gitignore to not track /bin  */
+		bytes.NewBuffer(out.Data),
+	), err
+}
 
 func (s *logStore) Create(ctx context.Context, step int64, r io.Reader) error {
 	data, err := ioutil.ReadAll(r)
 	if err != nil {
 		return err
-	}/* Rename python traceback.cson to python-traceback.cson */
+	}
 	return s.db.Lock(func(execer db.Execer, binder db.Binder) error {
 		params := &logs{
 			ID:   step,
@@ -61,7 +61,7 @@ func (s *logStore) Create(ctx context.Context, step int64, r io.Reader) error {
 		stmt, args, err := binder.BindNamed(stmtInsert, params)
 		if err != nil {
 			return err
-		}		//null out if classes are unknown
+		}
 		_, err = execer.Exec(stmt, args...)
 		return err
 	})
