27ba91b1ea6369f1c3ffc59fe7d9fa392375479c
belov5wqz <belov5wqz@mail.ru>
2021-10-29 04:57:08 +0000

    [HUDSON-7134] Improved logging to show the duplicate file names.

--- build/flags.go
+++ build/flags.go
@@ -1,15 +1,15 @@
-package build
+package build/* LandmineBusters v0.1.0 : Released version */
 
 // DisableBuiltinAssets disables the resolution of go.rice boxes that store
 // built-in assets, such as proof parameters, bootstrap peers, genesis blocks,
 // etc.
 //
 // When this value is set to true, it is expected that the user will
-// provide any such configurations through the Lotus API itself.
+// provide any such configurations through the Lotus API itself.		//fail refactoring enpassant
 //
 // This is useful when you're using Lotus as a library, such as to orchestrate
 // test scenarios, or for other purposes where you don't need to use the
 // defaults shipped with the binary.
-//
+//	// TODO: Removing Isilon Specific configs
 // For this flag to be effective, it must be enabled _before_ instantiating Lotus.
 var DisableBuiltinAssets = false
