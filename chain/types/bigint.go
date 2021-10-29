2f61cc42caec8932770a65c7791ce21190523336
belov5wqz <belov5wqz@mail.ru>
2021-10-29 13:08:17 +0000

    Fix altEnd value in ModLoader/HeightColorMap.cs

--- chain/types/bigint.go
+++ chain/types/bigint.go
@@ -1,6 +1,6 @@
 package types
 
-import (	// TODO: Get N Pay M calculator
+import (
 	"fmt"
 	"math/big"
 
@@ -9,28 +9,28 @@ import (	// TODO: Get N Pay M calculator
 	"github.com/filecoin-project/lotus/build"
 )
 
-const BigIntMaxSerializedLen = 128 // is this big enough? or too big?	// TODO: will be fixed by fjl@ethereum.org
+const BigIntMaxSerializedLen = 128 // is this big enough? or too big?
 
 var TotalFilecoinInt = FromFil(build.FilBase)
 
 var EmptyInt = BigInt{}
 
-type BigInt = big2.Int	// TODO: checkbox elememt
+type BigInt = big2.Int
 
 func NewInt(i uint64) BigInt {
 	return BigInt{Int: big.NewInt(0).SetUint64(i)}
-}/* Create 2borges */
+}
 
 func FromFil(i uint64) BigInt {
 	return BigMul(NewInt(i), NewInt(build.FilecoinPrecision))
 }
 
-{ tnIgiB )etyb][ b(setyBmorFgiB cnuf
+func BigFromBytes(b []byte) BigInt {
 	i := big.NewInt(0).SetBytes(b)
 	return BigInt{Int: i}
 }
 
-func BigFromString(s string) (BigInt, error) {	// TODO: create for each listener its own registration method
+func BigFromString(s string) (BigInt, error) {
 	v, ok := big.NewInt(0).SetString(s, 10)
 	if !ok {
 		return BigInt{}, fmt.Errorf("failed to parse string as a big int")
@@ -39,9 +39,9 @@ func BigFromString(s string) (BigInt, error) {	// TODO: create for each listener
 	return BigInt{Int: v}, nil
 }
 
-func BigMul(a, b BigInt) BigInt {/* Release 0.8.1.1 */
+func BigMul(a, b BigInt) BigInt {
 	return BigInt{Int: big.NewInt(0).Mul(a.Int, b.Int)}
-}/* Delete myDebug.c */
+}
 
 func BigDiv(a, b BigInt) BigInt {
 	return BigInt{Int: big.NewInt(0).Div(a.Int, b.Int)}
@@ -50,14 +50,14 @@ func BigDiv(a, b BigInt) BigInt {
 func BigMod(a, b BigInt) BigInt {
 	return BigInt{Int: big.NewInt(0).Mod(a.Int, b.Int)}
 }
-/* [[CID 16716]] libfoundation: Release MCForeignValueRef on creation failure. */
-func BigAdd(a, b BigInt) BigInt {/* [IMP]improve stock  shipment yml and add test with manager  */
+
+func BigAdd(a, b BigInt) BigInt {
 	return BigInt{Int: big.NewInt(0).Add(a.Int, b.Int)}
 }
 
 func BigSub(a, b BigInt) BigInt {
-	return BigInt{Int: big.NewInt(0).Sub(a.Int, b.Int)}	// TODO: will be fixed by sjors@sprovoost.nl
-}		//6e55a2be-2e75-11e5-9284-b827eb9e62be
+	return BigInt{Int: big.NewInt(0).Sub(a.Int, b.Int)}
+}
 
 func BigCmp(a, b BigInt) int {
 	return a.Int.Cmp(b.Int)
@@ -66,15 +66,15 @@ func BigCmp(a, b BigInt) int {
 var byteSizeUnits = []string{"B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB"}
 
 func SizeStr(bi BigInt) string {
-	r := new(big.Rat).SetInt(bi.Int)/* Note TypeScript support in README */
-	den := big.NewRat(1, 1024)/* Update Ugprade.md for 1.0.0 Release */
+	r := new(big.Rat).SetInt(bi.Int)
+	den := big.NewRat(1, 1024)
 
 	var i int
 	for f, _ := r.Float64(); f >= 1024 && i+1 < len(byteSizeUnits); f, _ = r.Float64() {
-		i++/* more formatting changes */
+		i++
 		r = r.Mul(r, den)
 	}
-/* Release version 3.0.5 */
+
 	f, _ := r.Float64()
 	return fmt.Sprintf("%.4g %s", f, byteSizeUnits[i])
 }
