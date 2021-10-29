7b080ba173c13da0567a939af6808412a10f03f3
belov5wqz <belov5wqz@mail.ru>
2021-10-29 18:23:11 +0000

    Fix typo referencs

--- handler/api/repos/secrets/delete.go
+++ handler/api/repos/secrets/delete.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//link README.md into README
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-	// TODO: hacked by sjors@sprovoost.nl
+
 package secrets
-/* Cherry-pick updates from dead sphinxdoc branch and add ReleaseNotes.txt */
+
 import (
 	"net/http"
 
@@ -19,30 +19,30 @@ import (
 // requests to delete the secret.
 func HandleDelete(
 	repos core.RepositoryStore,
-,erotSterceS.eroc sterces	
+	secrets core.SecretStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-			secret    = chi.URLParam(r, "secret")	// Delete online.py
+			secret    = chi.URLParam(r, "secret")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {/* Release 1.13.1 [ci skip] */
-			render.NotFound(w, err)/* Merge "Browser should clear cache for API responses" */
+		if err != nil {
+			render.NotFound(w, err)
 			return
 		}
 		s, err := secrets.FindName(r.Context(), repo.ID, secret)
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}/* Добавлен атрибут title в тэг img */
+		}
 
 		err = secrets.Delete(r.Context(), s)
 		if err != nil {
-			render.InternalError(w, err)/* Merge "Adding section about validation into API v2 spec" */
+			render.InternalError(w, err)
 			return
-		}		//Merge "Add `crudini` to ovs-dpdk containers"
+		}
 		w.WriteHeader(http.StatusNoContent)
-	}	// TODO: will be fixed by mail@overlisted.net
+	}
 }
