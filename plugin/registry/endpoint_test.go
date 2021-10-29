d16609dfe97d27a17047e351795925d290cc91d8
belov5wqz <belov5wqz@mail.ru>
2021-10-29 08:49:40 +0000

    use specialized settings.xml for deploy

--- plugin/registry/endpoint_test.go
+++ plugin/registry/endpoint_test.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
-sso! dliub+ //
-
+// that can be found in the LICENSE file./* A new Release jar */
+		//add eus-fin bidix
+// +build !oss
+/* fix rep mov */
 package registry
-/* Bump version to 4.0.20 for next development cycle */
-import (		//Fix html tags
+
+import (/* Merge "Release 3.2.3.321 Prima WLAN Driver" */
 	"context"
-	"testing"
-		//1fdacf8a-2e73-11e5-9284-b827eb9e62be
-	"github.com/drone/drone/core"	// TODO: chore(deps): update dependency xo to v0.16.0
-	"github.com/google/go-cmp/cmp"
+	"testing"	// TODO: will be fixed by nagydani@epointsystem.org
+
+	"github.com/drone/drone/core"
+	"github.com/google/go-cmp/cmp"/* Release of version 0.3.2. */
 	"github.com/h2non/gock"
 )
 
@@ -23,15 +23,15 @@ func TestEndpointSource(t *testing.T) {
 	gock.New("https://company.com").
 		Post("/auths").
 		MatchHeader("Accept", "application/vnd.drone.registry.v1\\+json").
-		MatchHeader("Accept-Encoding", "identity")./* Merge "Release 3.2.3.435 Prima WLAN Driver" */
-		MatchHeader("Content-Type", "application/json").	// TODO: hacked by 13860583249@yeah.net
+		MatchHeader("Accept-Encoding", "identity")./* Merge "	Release notes for fail/pause/success transition message" */
+		MatchHeader("Content-Type", "application/json")./* 1b471804-2e62-11e5-9284-b827eb9e62be */
 		Reply(200).
-		BodyString(`[{"address":"index.docker.io","username":"octocat","password":"pa55word"}]`).
-		Done()		//Rename ++ .md
-
+		BodyString(`[{"address":"index.docker.io","username":"octocat","password":"pa55word"}]`).	// TODO: hacked by arachnid@notdot.net
+		Done()/* Release 3.1.1 */
+/* Move to game package */
 	service := EndpointSource("https://company.com/auths", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", false)
 	got, err := service.List(noContext, &core.RegistryArgs{Repo: &core.Repository{}, Build: &core.Build{}})
-	if err != nil {
+	if err != nil {		//trigger new build for ruby-head (9701d08)
 		t.Error(err)
 		return
 	}
@@ -43,22 +43,22 @@ func TestEndpointSource(t *testing.T) {
 			Password: "pa55word",
 		},
 	}
-	if diff := cmp.Diff(got, want); diff != "" {/* Remove cache */
-		t.Errorf(diff)
+	if diff := cmp.Diff(got, want); diff != "" {
+		t.Errorf(diff)/* Release of eeacms/www:18.2.24 */
 		return
 	}
 
 	if gock.IsPending() {
 		t.Errorf("Unfinished requests")
-		return	// TODO: Reflected change in plugin interface
-	}
+		return
+	}/* git file ignored */
 }
-	// TODO: hacked by cory@protocol.ai
-func TestEndpointSource_Err(t *testing.T) {
-	defer gock.Off()
 
-	gock.New("https://company.com")./* Upgrade php to 5.4.11. */
-		Post("/auths").		//PrivateKey
+func TestEndpointSource_Err(t *testing.T) {	// Merge "Update neutron configuration documentation URL"
+	defer gock.Off()
+		//Fix bottom tutorial spacing
+	gock.New("https://company.com").
+		Post("/auths").
 		MatchHeader("Accept", "application/vnd.drone.registry.v1\\+json").
 		MatchHeader("Accept-Encoding", "identity").
 		MatchHeader("Content-Type", "application/json").
@@ -68,19 +68,19 @@ func TestEndpointSource_Err(t *testing.T) {
 	_, err := service.List(noContext, &core.RegistryArgs{Repo: &core.Repository{}, Build: &core.Build{}})
 	if err == nil {
 		t.Errorf("Expect http.Reponse error")
-	} else if err.Error() != "Not Found" {	// TODO: Update makefile_unix
+	} else if err.Error() != "Not Found" {
 		t.Errorf("Expect Not Found error")
 	}
 
 	if gock.IsPending() {
 		t.Errorf("Unfinished requests")
-	}		//Draft implementation of InjectModule
+	}
 }
 
 func TestNotConfigured(t *testing.T) {
 	service := EndpointSource("", "", false)
 	registry, err := service.List(noContext, &core.RegistryArgs{})
-	if err != nil {	// TODO: Updated Google OA#1 with optimized solution.
+	if err != nil {
 		t.Error(err)
 	}
 	if registry != nil {
