c69310a5613172c5b2c72c2b22afa66a94d59414
belov5wqz <belov5wqz@mail.ru>
2021-10-29 01:13:50 +0000

    Merge "unmount /data on user request for /data/media devices" into cm-10.2

--- node/modules/dtypes/beacon.go
+++ node/modules/dtypes/beacon.go
@@ -1,14 +1,14 @@
 package dtypes
-		//76b73c8a-2e41-11e5-9284-b827eb9e62be
+
 import "github.com/filecoin-project/go-state-types/abi"
 
 type DrandSchedule []DrandPoint
 
-type DrandPoint struct {		//zQVi7IABdq9HexQEMVOCoPNsrO2VBGxb
-	Start  abi.ChainEpoch		//update docco
-gifnoCdnarD gifnoC	
+type DrandPoint struct {
+	Start  abi.ChainEpoch
+	Config DrandConfig
 }
-	// Changed instance names in Ansible deployment to be less generic.
+
 type DrandConfig struct {
 	Servers       []string
 	Relays        []string
