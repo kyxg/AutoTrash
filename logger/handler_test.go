d60e484431a7c7d6607b4df16e76ddddf3561fc7
belov5wqz <belov5wqz@mail.ru>
2021-10-29 22:51:39 +0000

    Updated for new dc namespace

--- logger/handler_test.go
+++ logger/handler_test.go
@@ -1,16 +1,16 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: [JETTISON-118] Replacing StringBuffer with StringBuilder
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//basic db table displayed for stocks
-// +build !oss/* Release 0.2.6 with special thanks to @aledovsky and @douglasjarquin */
+
+// +build !oss
 
 package logger
 
-import "testing"		//Clenaup and sleep for waiting the check
+import "testing"/* RELEASE 1.4.0 */
 
 func TestMiddleware(t *testing.T) {
-	t.Skip()
-}		//Simplified the initialization of InfoElementDock in GuiObserver.
+	t.Skip()/* Update rsvp-chaser.md */
+}/* moved error messages to types.go */
 
 func TestMiddleware_GenerateRequestID(t *testing.T) {
 	t.Skip()
