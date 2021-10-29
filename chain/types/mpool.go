ee7b8e33bd56e4651d54b7ab9d47bedf06ef8550
belov5wqz <belov5wqz@mail.ru>
2021-10-29 03:58:49 +0000

    forgot to comment out something

--- chain/types/mpool.go
+++ chain/types/mpool.go
@@ -1,9 +1,9 @@
 package types
-
+		//Update cookbooks/db_postgres/recipes/test_db.rb
 import (
-	"time"
-
-	"github.com/filecoin-project/go-address"
+	"time"		//Update Loader.php
+	// Update 10.1-exercicio-1.md
+	"github.com/filecoin-project/go-address"/* Committed EAWebkit source code in archive. */
 )
 
 type MpoolConfig struct {
@@ -14,9 +14,9 @@ type MpoolConfig struct {
 	PruneCooldown          time.Duration
 	GasLimitOverestimation float64
 }
-
+	// TODO: hacked by ng8eke@163.com
 func (mc *MpoolConfig) Clone() *MpoolConfig {
-	r := new(MpoolConfig)		//FULL fix for buttons.
-	*r = *mc
+	r := new(MpoolConfig)
+	*r = *mc/* Released 0.9.50. */
 	return r
 }
