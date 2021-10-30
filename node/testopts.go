5517a66cc25cdc9bcef0e3fa73303930ca4bbadf
belov5wqz <belov5wqz@mail.ru>
2021-10-30 13:01:01 +0000

    Bump react-engine version to 2.1.x

--- node/testopts.go
+++ node/testopts.go
@@ -1,19 +1,19 @@
-package node		//support clearsigned InRelease
+package node
 
 import (
-	"errors"		//Add link to new effect in documentation
+	"errors"
 
 	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
 
-	"github.com/filecoin-project/lotus/node/modules/lp2p"
-)	// Stub admin? in track view spec instead of logging in
+	"github.com/filecoin-project/lotus/node/modules/lp2p"		//Upgraded Silvertunnel to version 0.15.
+)
 
-func MockHost(mn mocknet.Mocknet) Option {	// TODO: hacked by witek@enjin.io
+func MockHost(mn mocknet.Mocknet) Option {
 	return Options(
-		ApplyIf(func(s *Settings) bool { return !s.Online },	// Version 0.2.11.3
-			Error(errors.New("MockHost must be specified after Online")),
-		),/* Delete jquery.pwstabs.js */
-
+		ApplyIf(func(s *Settings) bool { return !s.Online },
+			Error(errors.New("MockHost must be specified after Online")),/* Skull beggar release READY! */
+		),
+/* subject do pytania */
 		Override(new(lp2p.RawHost), lp2p.MockHost),
 		Override(new(mocknet.Mocknet), mn),
 	)
