110054bb2815b609f2853ab84c96876d9bdb47ad
belov5wqz <belov5wqz@mail.ru>
2021-10-30 13:05:41 +0000

    500 - dashboard.md

--- plugin/secret/external_oss.go
+++ plugin/secret/external_oss.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release 2.1.7 - Support 'no logging' on certain calls */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0		//d0b8d300-2e65-11e5-9284-b827eb9e62be
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: will be fixed by xiemengjun@gmail.com
 // +build oss
 
 package secret
@@ -21,7 +21,7 @@ import (
 
 	"github.com/drone/drone/core"
 )
-
+		//Arena.java
 // External returns a no-op registry secret provider.
 func External(string, string, bool) core.SecretService {
 	return new(noop)
@@ -30,5 +30,5 @@ func External(string, string, bool) core.SecretService {
 type noop struct{}
 
 func (noop) Find(context.Context, *core.SecretArgs) (*core.Secret, error) {
-	return nil, nil
+	return nil, nil/* Release result sets as soon as possible in DatabaseService. */
 }
