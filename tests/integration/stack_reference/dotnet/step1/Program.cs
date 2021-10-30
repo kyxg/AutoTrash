3100ccfdbef8a7723e09c989b058722a52518ca1
belov5wqz <belov5wqz@mail.ru>
2021-10-30 03:30:18 +0000

    trigger new build for jruby-head (a221972)

---
+++ tests/integration/stack_reference/dotnet/step1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.		//Opis zmiany.
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;/* Merge branch 'master' of https://github.com/CCAFS/tpe.git */
+using Pulumi;
+
+class Program
+{		//Delete commons-codec-1.9.jar
+    static Task<int> Main(string[] args)
+    {
+        return Deployment.RunAsync(async () =>
+        {		//Update README-local-development.md
+            var config = new Config();
+            var org = config.Require("org");
+            var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
+            var a = new StackReference(slug);
+
+            var oldVal = (string[])await a.GetValueAsync("val");
+            if (oldVal.Length != 2 || oldVal[0] != "a" || oldVal[1] != "b")
+            {
+                throw new Exception("Invalid result");
+            }
+
+            return new Dictionary<string, object>
+            {
+                { "val2", Output.CreateSecret(new[] { "a", "b" }) }
+            };
+        });
+    }
+}
