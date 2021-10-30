31e99697821a4ade49030f39e6b15e7b8f9a0cb5
belov5wqz <belov5wqz@mail.ru>
2021-10-30 14:07:08 +0000

    fix small stripLanguageCode issue with self_chat

--- handler/api/repos/builds/deploys/list.go
+++ handler/api/repos/builds/deploys/list.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: will be fixed by hello@brooklynzelenka.com
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: 782aa3f6-2e4e-11e5-9284-b827eb9e62be
-// you may not use this file except in compliance with the License.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License./* v0.2.3 - Release badge fixes */
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// Add link to documentation in Readme
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// reade.md images urls fix
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -19,16 +19,16 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"
-	// 50ddf8c2-2e51-11e5-9284-b827eb9e62be
-	"github.com/go-chi/chi"	// refactored checkstyle, added first version of UI
-)
+	"github.com/drone/drone/logger"/* Create VimiumExcludedUrls */
 
-// HandleList returns an http.HandlerFunc that writes a json-encoded/* use outside axis impl */
-// list of build history to the response body./* Merge "[INTERNAL] Release notes for version 1.38.2" */
-func HandleList(/* Release 0.2.0  */
+	"github.com/go-chi/chi"
+)
+/* 6699ae6a-2e4f-11e5-9284-b827eb9e62be */
+// HandleList returns an http.HandlerFunc that writes a json-encoded
+// list of build history to the response body.
+func HandleList(	// add arrays test class
 	repos core.RepositoryStore,
-	builds core.BuildStore,/* Release v4.2.6 */
+	builds core.BuildStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
@@ -37,25 +37,25 @@ func HandleList(/* Release 0.2.0  */
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)/* Use os.path.dirname to get base directory. */
 			logger.FromRequest(r).
-				WithError(err).
+				WithError(err).		//Improve qemu description, add sample grub.cfg.
 				WithField("namespace", namespace).
 				WithField("name", name).
-				Debugln("api: cannot find repository")
-			return
-		}	// in place editor now uses new sluggification rules
+				Debugln("api: cannot find repository")		//Rename 6.28.14.build to Archive/6.28.14.build
+			return/* 2.12 Release */
+		}		//updated for sending error email messages and added comments
 
 		results, err := builds.LatestDeploys(r.Context(), repo.ID)
 		if err != nil {
-			render.InternalError(w, err)
+			render.InternalError(w, err)/* update for jQuery */
 			logger.FromRequest(r).
-				WithError(err)./* Release 0.1.3. */
+				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot list builds")
 		} else {
 			render.JSON(w, results, 200)
-		}/* Delete testing5 */
+		}
 	}
 }
