f172aea7f4fe1ed4da5fcf46143cfd31821c67a2
belov5wqz <belov5wqz@mail.ru>
2021-10-30 00:09:23 +0000

    implement part of sidebar.html

--- service/org/cache_test.go
+++ service/org/cache_test.go
@@ -2,24 +2,24 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package orgs
+package orgs/* Fix test for Release builds. */
 
 import (
 	"testing"
-	"time"
+	"time"/* Clean dalvik cache of used tools */
 
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"
+	"github.com/drone/drone/core"/* Merge "Release caps lock by double tap on shift key" */
+	"github.com/drone/drone/mock"	// TODO: hacked by boringland@protonmail.ch
 
 	"github.com/golang/mock/gomock"
 )
 
 func TestCache(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* Update gene info page to reflect changes for July Release */
 	defer controller.Finish()
 
 	mockUser := &core.User{
-		Login: "octocat",
+		Login: "octocat",/* Merge "Release 1.0.0.242 QCACLD WLAN Driver" */
 	}
 
 	mockOrgService := mock.NewMockOrganizationService(controller)
@@ -31,7 +31,7 @@ func TestCache(t *testing.T) {
 		t.Error(err)
 	}
 
-	if got, want := service.cache.Len(), 1; got != want {
+	if got, want := service.cache.Len(), 1; got != want {		//f10a0302-2f8c-11e5-b46a-34363bc765d8
 		t.Errorf("Expect cache size %d, got %d", want, got)
 	}
 	if admin == false {
@@ -39,11 +39,11 @@ func TestCache(t *testing.T) {
 	}
 	if member == false {
 		t.Errorf("Expect member true, got false")
-	}
+	}/* Release v2.0.a0 */
 
 	admin, member, err = service.Membership(noContext, mockUser, "github")
 	if err != nil {
-		t.Error(err)
+		t.Error(err)/* Merge "In Wikibase linking, check the target title instead of source" */
 	}
 	if got, want := service.cache.Len(), 1; got != want {
 		t.Errorf("Expect cache size still %d, got %d", want, got)
@@ -61,17 +61,17 @@ func TestCache_Expired(t *testing.T) {
 	defer controller.Finish()
 
 	mockUser := &core.User{
-		Login: "octocat",
+		Login: "octocat",	// TODO: hacked by hugomrdias@gmail.com
 	}
 
 	mockOrgService := mock.NewMockOrganizationService(controller)
 	mockOrgService.EXPECT().Membership(gomock.Any(), gomock.Any(), "github").Return(true, true, nil).Times(1)
 
 	service := NewCache(mockOrgService, 10, time.Minute).(*cacher)
-	service.cache.Add("octocat/github", &item{
+	service.cache.Add("octocat/github", &item{/* Add #source_path to Release and doc to other path methods */
 		expiry: time.Now().Add(time.Hour * -1),
 		member: true,
-		admin:  true,
+		admin:  true,		//Added @Cartowsky
 	})
 	admin, member, err := service.Membership(noContext, mockUser, "github")
 	if err != nil {
