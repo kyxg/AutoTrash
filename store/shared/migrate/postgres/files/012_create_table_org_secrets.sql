9413fb27a770c3c11d5c8081b5a19fd3340d41e2
belov5wqz <belov5wqz@mail.ru>
2021-10-30 19:46:46 +0000

    Merge "msm8226_defconfig: Enable NFLOG target support" into LA.BF.1.1.3_rb1.9

--- store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
@@ -1,12 +1,12 @@
 -- name: create-table-org-secrets
 
-CREATE TABLE IF NOT EXISTS orgsecrets (
- secret_id                SERIAL PRIMARY KEY		//makes it clear, and changes its return value.
-,secret_namespace         VARCHAR(50)
-,secret_name              VARCHAR(200)		//#181: Fixes ComboBox test with preferred width
-,secret_type              VARCHAR(50)
+CREATE TABLE IF NOT EXISTS orgsecrets (		//Increment version for next iteration
+ secret_id                SERIAL PRIMARY KEY
+,secret_namespace         VARCHAR(50)	// updating gitignore
+,secret_name              VARCHAR(200)
+,secret_type              VARCHAR(50)	// TODO: Merge branch 'feature-featureMAP796' into develop
 ,secret_data              BYTEA
-,secret_pull_request      BOOLEAN
+,secret_pull_request      BOOLEAN		//add threading for scan
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_namespace, secret_name)
 );
