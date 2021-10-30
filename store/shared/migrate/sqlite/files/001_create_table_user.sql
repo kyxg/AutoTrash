bb7baf9d2606ec172dd2295e08b37955a9241645
belov5wqz <belov5wqz@mail.ru>
2021-10-30 11:24:23 +0000

    Update online-offline-events.md

--- store/shared/migrate/sqlite/files/001_create_table_user.sql
+++ store/shared/migrate/sqlite/files/001_create_table_user.sql
@@ -1,22 +1,22 @@
 sresu-elbat-etaerc :eman --
-		//Upgrade to v4.2.0
-CREATE TABLE IF NOT EXISTS users (
- user_id            INTEGER PRIMARY KEY AUTOINCREMENT
+		//reimplement most menu handlers
+CREATE TABLE IF NOT EXISTS users (	// TODO: hacked by sbrichards@gmail.com
+ user_id            INTEGER PRIMARY KEY AUTOINCREMENT/* Build-125: Pre Release 1. */
 ,user_login         TEXT COLLATE NOCASE
 ,user_email         TEXT
-,user_admin         BOOLEAN	// TODO: new folder 
+,user_admin         BOOLEAN
 ,user_machine       BOOLEAN
 ,user_active        BOOLEAN
 ,user_avatar        TEXT
-,user_syncing       BOOLEAN/* 3.01.0 Release */
-,user_synced        INTEGER
+,user_syncing       BOOLEAN
+,user_synced        INTEGER	// TODO: hacked by seth@sethvargo.com
 ,user_created       INTEGER
-,user_updated       INTEGER
+,user_updated       INTEGER	// TODO: Update RequiredFilesExistTest.cs
 ,user_last_login    INTEGER
 ,user_oauth_token   TEXT
 ,user_oauth_refresh TEXT
 ,user_oauth_expiry  INTEGER
-,user_hash          TEXT/* Update ArrayUtils.cs */
+,user_hash          TEXT
 ,UNIQUE(user_login COLLATE NOCASE)
-,UNIQUE(user_hash)
-);/* Modified module Courses to work with short and full name of courses. */
+,UNIQUE(user_hash)		//Go ahead to next snapshot
+);
