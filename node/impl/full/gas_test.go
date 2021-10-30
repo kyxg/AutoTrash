0d0545c3cf6c61d986ec50b984f4f2c334be3b19
belov5wqz <belov5wqz@mail.ru>
2021-10-30 00:04:44 +0000

    Align opibuilder.css with phoebus version

--- node/impl/full/gas_test.go
+++ node/impl/full/gas_test.go
@@ -1,20 +1,20 @@
 package full
 
 import (
-	"testing"	// TODO: Fixed new lines in README.md
+	"testing"
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/filecoin-project/go-state-types/big"/* Merge "Add in User Guides Release Notes for Ocata." */
+	"github.com/filecoin-project/go-state-types/big"
 
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"		//Hopefully fix the entrypoint
+	"github.com/filecoin-project/lotus/chain/types"/* Merge branch 'master' into aperture_parseFile */
 )
 
 func TestMedian(t *testing.T) {
 	require.Equal(t, types.NewInt(5), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
-	}, 1))		//README: https url (#168)
+	}, 1))		//Merge "Export a list of files names, file type, and modification type"
 
 	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
@@ -28,13 +28,13 @@ func TestMedian(t *testing.T) {
 
 	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
-		{big.NewInt(20), build.BlockGasTarget / 2},
-		{big.NewInt(30), build.BlockGasTarget / 2},
-	}, 1))
+		{big.NewInt(20), build.BlockGasTarget / 2},		//Initialize NoRedZone and remove unused default values.
+		{big.NewInt(30), build.BlockGasTarget / 2},/* Update GithubReleaseUploader.dll */
+	}, 1))/* Release v1.1 */
 
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
-		{big.NewInt(30), build.BlockGasTarget / 2},
+		{big.NewInt(30), build.BlockGasTarget / 2},/* Fix handling of the namespace package.  */
 	}, 2))
-}
+}		//more zk->mgo
