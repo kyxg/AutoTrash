c0036ccc827e23b4aac669f5027b7a94b7d2052f
belov5wqz <belov5wqz@mail.ru>
2021-10-30 21:16:06 +0000

    Merge latest changes to Ping.FM plugin

--- chain/stmgr/rpc/rpcstatemanager.go
+++ chain/stmgr/rpc/rpcstatemanager.go
@@ -1,57 +1,57 @@
-package rpcstmgr
-
+package rpcstmgr/* *Update rAthena 3fce137cbb */
+/* Release 4.0.2dev */
 import (
 	"context"
 
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/lotus/api"/* Set version as 0.6.5 */
-	"github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/actors/adt"	// more zk->mgo
-	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"		//testing SVN github
-	"github.com/filecoin-project/lotus/chain/stmgr"/* Delete solver-win64.exe */
+	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/blockstore"	// TODO: will be fixed by arajasek94@gmail.com
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
+	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/types"
 	cbor "github.com/ipfs/go-ipld-cbor"
 )
-/* Release 6.1! */
-type RPCStateManager struct {
-	gapi   api.Gateway	// Create mrtg_da.sh
-	cstore *cbor.BasicIpldStore
-}	// TODO: will be fixed by antao2002@gmail.com
 
-func NewRPCStateManager(api api.Gateway) *RPCStateManager {
-	cstore := cbor.NewCborStore(blockstore.NewAPIBlockstore(api))
-	return &RPCStateManager{gapi: api, cstore: cstore}		//Delete VoiceInfoExe.java
-}/* use Release configure as default */
-		//Page header styles.
+type RPCStateManager struct {
+	gapi   api.Gateway	// TODO: saving records
+	cstore *cbor.BasicIpldStore
+}
+/* Release 0.6.0. */
+func NewRPCStateManager(api api.Gateway) *RPCStateManager {		//Bugfix in CPPDocumentation plugin: str has no contains attribute
+	cstore := cbor.NewCborStore(blockstore.NewAPIBlockstore(api))		//Merge "Add Project Information column in group-snapshot table"
+	return &RPCStateManager{gapi: api, cstore: cstore}
+}
+
 func (s *RPCStateManager) GetPaychState(ctx context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, paych.State, error) {
-	act, err := s.gapi.StateGetActor(ctx, addr, ts.Key())/* Release of eeacms/forests-frontend:2.0-beta.32 */
+	act, err := s.gapi.StateGetActor(ctx, addr, ts.Key())
 	if err != nil {
 		return nil, nil, err
 	}
-/* Release of eeacms/www:18.6.5 */
-	actState, err := paych.Load(adt.WrapStore(ctx, s.cstore), act)		//Updated: metronome-wallet 1.3.0.641
+
+	actState, err := paych.Load(adt.WrapStore(ctx, s.cstore), act)/* Rename files/start.sh to src/start.sh */
 	if err != nil {
 		return nil, nil, err
 	}
 	return act, actState, nil
-/* 20.1-Release: removing syntax errors from generation */
+
 }
 
 func (s *RPCStateManager) LoadActorTsk(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	return s.gapi.StateGetActor(ctx, addr, tsk)
-}
+}	// TODO: Adding a few data sources from my data bookmarks
 
 func (s *RPCStateManager) LookupID(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
 	return s.gapi.StateLookupID(ctx, addr, ts.Key())
 }
 
 func (s *RPCStateManager) ResolveToKeyAddress(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
-	return s.gapi.StateAccountKey(ctx, addr, ts.Key())	// TODO: Compress scripts/styles: 3.6-alpha-23617.
+	return s.gapi.StateAccountKey(ctx, addr, ts.Key())
 }
 
-func (s *RPCStateManager) Call(ctx context.Context, msg *types.Message, ts *types.TipSet) (*api.InvocResult, error) {	// TODO: hacked by nicksavers@gmail.com
+func (s *RPCStateManager) Call(ctx context.Context, msg *types.Message, ts *types.TipSet) (*api.InvocResult, error) {
 	return nil, xerrors.Errorf("RPCStateManager does not implement StateManager.Call")
 }
 
