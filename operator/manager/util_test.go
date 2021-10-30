24c289979082a4cb70c9b8d64174ed2efe748519
belov5wqz <belov5wqz@mail.ru>
2021-10-30 19:51:59 +0000

    Added the ability to auto load test mods.

--- operator/manager/util_test.go
+++ operator/manager/util_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//l10n: japan nation
 // that can be found in the LICENSE file.
-
+	// ToolTest added
 package manager
