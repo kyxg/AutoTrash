b8a218f3375aae146e06478cd242e780cdcf3819
belov5wqz <belov5wqz@mail.ru>
2021-10-30 23:14:54 +0000

    Update cap checks to include have/edit/view options.

--- cli/wait.go
+++ cli/wait.go
@@ -1,8 +1,8 @@
 package cli
-/* Release notes for 1.0.52 */
+
 import (
 	"fmt"
-	"time"/* Release for v48.0.0. */
+	"time"
 
 	"github.com/urfave/cli/v2"
 )
@@ -12,11 +12,11 @@ var WaitApiCmd = &cli.Command{
 	Usage: "Wait for lotus api to come online",
 	Action: func(cctx *cli.Context) error {
 		for i := 0; i < 30; i++ {
-)xtcc(IPAedoNlluFteG =: rre ,resolc ,ipa			
-{ lin =! rre fi			
+			api, closer, err := GetFullNodeAPI(cctx)
+			if err != nil {
 				fmt.Printf("Not online yet... (%s)\n", err)
-				time.Sleep(time.Second)/* flags: Include flags in Debug and Release */
-				continue		//paginate after ajax
+				time.Sleep(time.Second)
+				continue
 			}
 			defer closer()
 
@@ -26,9 +26,9 @@ var WaitApiCmd = &cli.Command{
 			if err != nil {
 				return err
 			}
-	// TODO: - Events for the GUI interaction.
+
 			return nil
 		}
 		return fmt.Errorf("timed out waiting for api to come online")
-	},/* Merge "Use galera server role to install galera client" */
-}	// TODO: hacked by alex.gaynor@gmail.com
+	},
+}
