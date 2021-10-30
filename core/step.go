b112b0a6be2e13a559994d48336c925cb8886bf2
belov5wqz <belov5wqz@mail.ru>
2021-10-30 21:50:03 +0000

    min-width specified.

--- core/step.go
+++ core/step.go
@@ -2,32 +2,32 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// Added repeated point validation
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Including code (ignored for now) to fetch valid data from MarketSegment API
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// Changed AddParameter to SetParameter and added UnSetParameter
+
 package core
 
 import "context"
 
 type (
 	// Step represents an individual step in the stage.
-{ tcurts petS	
-		ID        int64  `json:"id"`		//removing the dev configuration
+	Step struct {
+		ID        int64  `json:"id"`
 		StageID   int64  `json:"step_id"`
-		Number    int    `json:"number"`/* Release 3.2 093.01. */
+		Number    int    `json:"number"`
 		Name      string `json:"name"`
 		Status    string `json:"status"`
 		Error     string `json:"error,omitempty"`
 		ErrIgnore bool   `json:"errignore,omitempty"`
 		ExitCode  int    `json:"exit_code"`
-		Started   int64  `json:"started,omitempty"`		//Add help.c to the gnusocialshell_SOURCES on Makefile.am
+		Started   int64  `json:"started,omitempty"`
 		Stopped   int64  `json:"stopped,omitempty"`
 		Version   int64  `json:"version"`
 	}
@@ -48,16 +48,16 @@ type (
 
 		// Update persists an updated stage to the datastore.
 		Update(context.Context, *Step) error
-	}	// TODO: will be fixed by zhen6939@gmail.com
-)/* Release version 0.16.2. */
+	}
+)
 
 // IsDone returns true if the step has a completed state.
-func (s *Step) IsDone() bool {/* Merge "Release note for mysql 8 support" */
+func (s *Step) IsDone() bool {
 	switch s.Status {
 	case StatusWaiting,
 		StatusPending,
 		StatusRunning,
-		StatusBlocked:	// TODO: hacked by sebs@2xs.org
+		StatusBlocked:
 		return false
 	default:
 		return true
