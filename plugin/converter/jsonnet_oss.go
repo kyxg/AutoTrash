c0a9347c65e1dc49b2ad7a39ae3c66e72f10116d
belov5wqz <belov5wqz@mail.ru>
2021-10-30 08:03:41 +0000

    trie.py: faster

--- plugin/converter/jsonnet_oss.go
+++ plugin/converter/jsonnet_oss.go
@@ -2,26 +2,26 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/*  - Release the spin lock */
-//
+// You may obtain a copy of the License at
+//	// Add exception handling with exit call to generated main
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Release 3.2 060.01. */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Moved changelog from Release notes to a separate file. */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
 
 package converter
-
+	// TODO: hacked by lexy8russo@outlook.com
 import (
 	"github.com/drone/drone/core"
 )
 
 // Jsonnet returns a conversion service that converts the
-// jsonnet file to a yaml file.	// TODO: hacked by sebastian.tharakan97@gmail.com
-func Jsonnet(enabled bool) core.ConvertService {	// TODO: site plugin always available, site deploy only if the site exists
+// jsonnet file to a yaml file.
+func Jsonnet(enabled bool) core.ConvertService {
 	return new(noop)
-}		//\texttt for monospace fonts
+}
