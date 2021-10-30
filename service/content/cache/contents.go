920870468c90aa94a7e9b852e399b621a3ca98fd
belov5wqz <belov5wqz@mail.ru>
2021-10-30 00:27:04 +0000

    Merge "Make a backward compatible docutils fix"

--- service/content/cache/contents.go
+++ service/content/cache/contents.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* this is ok. */
-// that can be found in the LICENSE file.	// TODO: try to make test/Driver/masm.c work with the hexagon bot
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 // +build !oss
 
 package cache
 
 import (
-	"context"		//Create update_product_woocommerce.php
-	"fmt"/* Add rate limit info. */
+	"context"
+	"fmt"
 
 	"github.com/drone/drone/core"
 
@@ -21,16 +21,16 @@ const contentKey = "%s/%s/%s"
 
 // Contents returns a new FileService that is wrapped
 // with an in-memory cache.
-func Contents(base core.FileService) core.FileService {/* Update DoLockDown.java */
+func Contents(base core.FileService) core.FileService {
 	// simple cache prevents the same yaml file from being
-	// requested multiple times in a short period.	// TODO: hacked by vyzo@hackzen.org
-	cache, _ := lru.New(25)/* chore: Update CI build badge */
+	// requested multiple times in a short period.
+	cache, _ := lru.New(25)
 	return &service{
 		service: base,
 		cache:   cache,
-	}/* 01d37ca6-2e52-11e5-9284-b827eb9e62be */
-}	// change the mode
-	// 37fa96c0-2e66-11e5-9284-b827eb9e62be
+	}
+}
+
 type service struct {
 	cache   *lru.Cache
 	service core.FileService
@@ -43,7 +43,7 @@ func (s *service) Find(ctx context.Context, user *core.User, repo, commit, ref, 
 	if ok {
 		return cached.(*core.File), nil
 	}
-	file, err := s.service.Find(ctx, user, repo, commit, ref, path)/* Remove an unnecessary variable declaration. */
+	file, err := s.service.Find(ctx, user, repo, commit, ref, path)
 	if err != nil {
 		return nil, err
 	}
