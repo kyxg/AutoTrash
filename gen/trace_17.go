33a5b93d4f09e49a18d9dbb15adf04d853cf8fb1
belov5wqz <belov5wqz@mail.ru>
2021-10-30 19:09:33 +0000

    Update shared-pom.xml

--- gen/trace_17.go
+++ gen/trace_17.go
@@ -1,12 +1,12 @@
 // +build !go1.8
 
 package websocket
-/* Merge "Use glance image-list command in devstack/plugin.sh" */
+/* Added -O3 -march=native -mtune=native flags for G++ and Clang */
 import (
-	"crypto/tls"/* Release 1.0! */
-	"net/http/httptrace"/* Create API_Reference/namedquery.png */
+	"crypto/tls"
+	"net/http/httptrace"/* Fixed build scripts */
 )
 
-func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {/* Add the version of pkgdb in the footer */
+func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {/* Merge "Release 3.0.10.026 Prima WLAN Driver" */
 	return doHandshake(tlsConn, cfg)
-}
+}	// TODO: New comment by Sencoick
