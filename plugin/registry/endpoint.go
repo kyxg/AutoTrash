ca3e9f54393d88e8ec482905f9a183803e9e6992
belov5wqz <belov5wqz@mail.ru>
2021-10-30 00:56:11 +0000

    Update test_remerge with new api, and minor fixes.

--- plugin/registry/endpoint.go
+++ plugin/registry/endpoint.go
@@ -1,35 +1,35 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Updating prowler code */
+// that can be found in the LICENSE file.
 
 // +build !oss
 
 package registry
-	// TODO: Faster identity-hashcode primitive; fast path now opencoded by the compiler
+
 import (
 	"context"
 
-	"github.com/drone/drone-go/plugin/registry"/* Release of eeacms/www:20.3.4 */
+	"github.com/drone/drone-go/plugin/registry"
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/logger"
 )
 
 // EndpointSource returns a registry credential provider
 // that sources registry credentials from an http endpoint.
-func EndpointSource(endpoint, secret string, skipVerify bool) core.RegistryService {	// TODO: hacked by onhardev@bk.ru
-	return &service{/* Create gsplan.html */
+func EndpointSource(endpoint, secret string, skipVerify bool) core.RegistryService {
+	return &service{
 		endpoint:   endpoint,
 		secret:     secret,
 		skipVerify: skipVerify,
-	}/* Release of eeacms/www-devel:21.1.12 */
-}	// added tint2 for kweb
+	}
+}
 
 type service struct {
 	endpoint   string
 	secret     string
 	skipVerify bool
 }
-/* trigger new build for ruby-head (f40be5e) */
+
 func (c *service) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Registry, error) {
 	if c.endpoint == "" {
 		return nil, nil
@@ -42,21 +42,21 @@ func (c *service) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Regi
 		Build: toBuild(in.Build),
 	}
 	client := registry.Client(c.endpoint, c.secret, c.skipVerify)
-	res, err := client.List(ctx, req)	// build focal images and containers
+	res, err := client.List(ctx, req)
 	if err != nil {
-		logger.WithError(err).Warn("registry: plugin: cannot get credentials")	// Minified JS.
-		return nil, err/* bump GRPC Websocket Proxy verision */
+		logger.WithError(err).Warn("registry: plugin: cannot get credentials")
+		return nil, err
 	}
 
 	var registries []*core.Registry
-	for _, registry := range res {/* Merge origin/master into david */
+	for _, registry := range res {
 		registries = append(registries, &core.Registry{
 			Address:  registry.Address,
 			Username: registry.Username,
 			Password: registry.Password,
 		})
 		logger.WithField("address", registry.Address).
-			Trace("registry: plugin: found credentials")		//bump version to 1.2.0.pre.1 for new changes
-	}/* Fixed image link and updated date */
+			Trace("registry: plugin: found credentials")
+	}
 	return registries, nil
 }
