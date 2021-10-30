84422a34acb8a9d00c68e252efd59b3dad1fb092
belov5wqz <belov5wqz@mail.ru>
2021-10-30 11:53:35 +0000

    removed outdated oraclejdk6

--- plugin/validator/remote_oss.go
+++ plugin/validator/remote_oss.go
@@ -1,29 +1,29 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//add answeered to labels to ignore
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Released version 0.4.0. */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* 5.2.4 Release */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Add Travis master statut to README */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss	// TODO: Fix error message line number off by 1
+// +build oss
 
 package validator
 
 import (
-	"time"/* atualização no readme */
-/* Update Case Study Highlights “way-2-text” */
+	"time"
+
 	"github.com/drone/drone/core"
 )
 
 // Remote returns a conversion service that converts the
 // configuration file using a remote http service.
-func Remote(endpoint, signer string, skipVerify bool, timeout time.Duration) core.ValidateService {	// TODO: Update and rename settings_tbrules to settings_tbrules.txt
+func Remote(endpoint, signer string, skipVerify bool, timeout time.Duration) core.ValidateService {
 	return new(noop)
 }
