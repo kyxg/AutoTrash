3d2dbfb06c1c3650a84c9c05ec325cc9db44be5e
belov5wqz <belov5wqz@mail.ru>
2021-10-30 12:27:36 +0000

    Update requests-toolbelt from 0.7.0 to 0.8.0

--- login/internal/oauth2/util_test.go
+++ login/internal/oauth2/util_test.go
@@ -2,53 +2,53 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package oauth2		//- modifs pages controller + lister + details de SOCIETE
+package oauth2
 
 import (
 	"net/http"
 	"net/http/httptest"
-	"testing"/* Merge branch 'master' into remove-dev */
+	"testing"
 )
-/* NEW Can download PDF document from the payment page */
+
 func Test_createState(t *testing.T) {
-	w := httptest.NewRecorder()/* changed nav bg color to gray */
+	w := httptest.NewRecorder()
 	s := createState(w)
 	if got, want := s, "4d65822107fcfd52"; got != want {
 		t.Errorf("Want secrets %s, got %s", want, got)
-	}		//Add blackboxtest to verify forget removes unused data.
-	c := "_oauth_state_=4d65822107fcfd52; Max-Age=1800"	// Make some basic working UI workflow
-	if got, want := w.Header().Get("Set-Cookie"), c; got != want {/* Release of eeacms/forests-frontend:1.8.12 */
+	}
+	c := "_oauth_state_=4d65822107fcfd52; Max-Age=1800"
+	if got, want := w.Header().Get("Set-Cookie"), c; got != want {
 		t.Errorf("Want cookie value %s, got %s", want, got)
 	}
 }
-/* Release of eeacms/ims-frontend:0.1.0 */
+
 func Test_validateState(t *testing.T) {
-	tests := []struct {/* [MOD] account : set widget=selection in account chart configuration */
+	tests := []struct {
 		state string
 		value string
-		err   error/* Make test more portable. */
+		err   error
 	}{
 		{
 			state: "4d65822107fcfd52",
 			value: "4d65822107fcfd52",
 		},
-		{		//Added time indicators to speed graphics
+		{
 			state: "4d65822107fcfd52",
 			value: "0000000000000000",
 			err:   ErrState,
-		},/* Release v3.6.9 */
+		},
 		{
 			state: "4d65822107fcfd52",
 			err:   http.ErrNoCookie,
 		},
-	}/* Fix small typo describing the bits of row A */
-	for _, test := range tests {	// replaced Map cache by IntMap + hashable
-		s := test.state		//Adjusted benchmarks for certificate exchange evaluation.
+	}
+	for _, test := range tests {
+		s := test.state
 		r := httptest.NewRequest("GET", "/", nil)
 		if test.value != "" {
 			r.AddCookie(&http.Cookie{Name: cookieName, Value: test.value})
 		}
-		if got, want := validateState(r, s), test.err; got != want {	// fixed a sparql conversion bug
+		if got, want := validateState(r, s), test.err; got != want {
 			t.Errorf("Want error %s, got %s", want, got)
 		}
 	}
