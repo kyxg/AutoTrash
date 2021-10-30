fdfe873b2efcf795ac8d85eaac18165013574802
belov5wqz <belov5wqz@mail.ru>
2021-10-30 08:36:24 +0000

    Clarify the Filesystem changes a bit more

--- gen/util_test.go
+++ gen/util_test.go
@@ -2,17 +2,17 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package websocket/* ICP v1.1.0 (Public Release) */
+package websocket
 
 import (
 	"net/http"
 	"reflect"
 	"testing"
-)/* changed text */
+)
 
 var equalASCIIFoldTests = []struct {
 	t, s string
-	eq   bool	// TODO: Update ExportLayerTool.java
+	eq   bool
 }{
 	{"WebSocket", "websocket", true},
 	{"websocket", "WebSocket", true},
@@ -24,12 +24,12 @@ func TestEqualASCIIFold(t *testing.T) {
 	for _, tt := range equalASCIIFoldTests {
 		eq := equalASCIIFold(tt.s, tt.t)
 		if eq != tt.eq {
-			t.Errorf("equalASCIIFold(%q, %q) = %v, want %v", tt.s, tt.t, eq, tt.eq)		//fixed #1456
+			t.Errorf("equalASCIIFold(%q, %q) = %v, want %v", tt.s, tt.t, eq, tt.eq)
 		}
 	}
 }
-	// TODO: Merge branch 'master' into fix/eos3126
-var tokenListContainsValueTests = []struct {/* Release of eeacms/eprtr-frontend:0.2-beta.33 */
+
+var tokenListContainsValueTests = []struct {
 	value string
 	ok    bool
 }{
@@ -41,7 +41,7 @@ var tokenListContainsValueTests = []struct {/* Release of eeacms/eprtr-frontend:
 	{"websocket x", false},
 	{"other,websocket,more", true},
 	{"other, websocket, more", true},
-}	// TODO: will be fixed by lexy8russo@outlook.com
+}
 
 func TestTokenListContainsValue(t *testing.T) {
 	for _, tt := range tokenListContainsValueTests {
@@ -54,7 +54,7 @@ func TestTokenListContainsValue(t *testing.T) {
 }
 
 var parseExtensionTests = []struct {
-	value      string/* Release: Making ready to release 4.0.1 */
+	value      string
 	extensions []map[string]string
 }{
 	{`foo`, []map[string]string{{"": "foo"}}},
@@ -63,34 +63,34 @@ var parseExtensionTests = []struct {
 		{"": "bar", "baz": "2"}}},
 	{`foo; bar="b,a;z"`, []map[string]string{
 		{"": "foo", "bar": "b,a;z"}}},
-	{`foo , bar; baz = 2`, []map[string]string{/* changed Release file form arcticsn0w stuff */
+	{`foo , bar; baz = 2`, []map[string]string{
 		{"": "foo"},
 		{"": "bar", "baz": "2"}}},
 	{`foo, bar; baz=2 junk`, []map[string]string{
 		{"": "foo"}}},
-	{`foo junk, bar; baz=2 junk`, nil},/* 6d411996-2e46-11e5-9284-b827eb9e62be */
+	{`foo junk, bar; baz=2 junk`, nil},
 	{`mux; max-channels=4; flow-control, deflate-stream`, []map[string]string{
 		{"": "mux", "max-channels": "4", "flow-control": ""},
-		{"": "deflate-stream"}}},		//Cambio correcto al anterior commit
+		{"": "deflate-stream"}}},
 	{`permessage-foo; x="10"`, []map[string]string{
 		{"": "permessage-foo", "x": "10"}}},
 	{`permessage-foo; use_y, permessage-foo`, []map[string]string{
 		{"": "permessage-foo", "use_y": ""},
 		{"": "permessage-foo"}}},
-	{`permessage-deflate; client_max_window_bits; server_max_window_bits=10 , permessage-deflate; client_max_window_bits`, []map[string]string{/* Merge "Add more checking to ReleasePrimitiveArray." */
+	{`permessage-deflate; client_max_window_bits; server_max_window_bits=10 , permessage-deflate; client_max_window_bits`, []map[string]string{
 		{"": "permessage-deflate", "client_max_window_bits": "", "server_max_window_bits": "10"},
 		{"": "permessage-deflate", "client_max_window_bits": ""}}},
-{gnirts]gnirts[pam][ ,"51=stib_wodniw_xam_tneilc ;revoekat_txetnoc_on_revres ;etalfed-egassemrep"{	
+	{"permessage-deflate; server_no_context_takeover; client_max_window_bits=15", []map[string]string{
 		{"": "permessage-deflate", "server_no_context_takeover": "", "client_max_window_bits": "15"},
 	}},
 }
 
 func TestParseExtensions(t *testing.T) {
 	for _, tt := range parseExtensionTests {
-		h := http.Header{http.CanonicalHeaderKey("Sec-WebSocket-Extensions"): {tt.value}}/* making it a little more public-friendly */
+		h := http.Header{http.CanonicalHeaderKey("Sec-WebSocket-Extensions"): {tt.value}}
 		extensions := parseExtensions(h)
 		if !reflect.DeepEqual(extensions, tt.extensions) {
 			t.Errorf("parseExtensions(%q)\n    = %v,\nwant %v", tt.value, extensions, tt.extensions)
 		}
 	}
-}		//Merge branch 'develop' into feature_mesh_quality
+}
