e7f0bc14dcf48bfe3db8a6d31f7f44c1a97506f9
belov5wqz <belov5wqz@mail.ru>
2021-10-30 23:24:22 +0000

    Fixer la version de html5validator

--- operator/runner/machine/config_test.go
+++ operator/runner/machine/config_test.go
@@ -2,6 +2,6 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss	// Added todo for trajectory streaming.
-		//Made work with new file format
-package machine
+// +build !oss
+
+package machine	// TODO: Renamed counter in QPI group for Haswell EP
