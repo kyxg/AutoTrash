08c4381c2d44c64da15f14c8376234a93df3aef4
belov5wqz <belov5wqz@mail.ru>
2021-10-30 00:18:02 +0000

    updated imagelayers.io badge

--- handler/api/repos/builds/logs/delete.go
+++ handler/api/repos/builds/logs/delete.go
@@ -1,56 +1,56 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Release for v49.0.0. */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Release changes 5.1b4 */
+//      http://www.apache.org/licenses/LICENSE-2.0/* added link to demo reader */
+///* Released v2.2.3 */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Restore cursor position for newly selected scripts.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package logs/* [artifactory-release] Release version 3.3.6.RELEASE */
-
+package logs
+/* 58b47511-2e4f-11e5-8776-28cfe91dbc4b */
 import (
-	"net/http"
+	"net/http"/* @Release [io7m-jcanephora-0.9.15] */
 	"strconv"
-	// TODO: will be fixed by yuvalalaluf@gmail.com
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
 )
-
-// HandleDelete returns an http.HandlerFunc that processes http
+/* Merge "[INTERNAL] Release notes for version 1.28.32" */
+// HandleDelete returns an http.HandlerFunc that processes http/* Update PrepareReleaseTask.md */
 // requests to delete the logs.
 func HandleDelete(
-	repos core.RepositoryStore,
+	repos core.RepositoryStore,/* 21ba576a-2e48-11e5-9284-b827eb9e62be */
 	builds core.BuildStore,
 	stages core.StageStore,
-	steps core.StepStore,		//.wrap paddings: top/bottom: 12rem
-	logs core.LogStore,/* Delete mim.cc */
-) http.HandlerFunc {/* grr. policy */
+	steps core.StepStore,
+	logs core.LogStore,
+) http.HandlerFunc {/* trying to send the difficulty */
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")/* Delete TrabAlgebraLinear.zip */
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)	// TODO: will be fixed by arachnid@notdot.net
-		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
+		)
+		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)	// TODO: will be fixed by hello@brooklynzelenka.com
 		if err != nil {
-			render.BadRequest(w, err)/* 3ced2730-2e69-11e5-9284-b827eb9e62be */
-			return
+			render.BadRequest(w, err)/* Merge "[upstream] Add Stable Release info to Release Cycle Slides" */
+			return	// TODO: Create get-client-response.json
 		}
 		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))
-		if err != nil {/* Renamed test command to ping command */
-			render.BadRequest(w, err)		//Updated readme to include webpack-dev-server as global dep
+		if err != nil {
+			render.BadRequest(w, err)		//Merge branch 'master' into kotlinRecentSearch
 			return
 		}
 		stepNumber, err := strconv.Atoi(chi.URLParam(r, "step"))
-		if err != nil {
-			render.BadRequest(w, err)
+		if err != nil {		//Merge branch 'master' into Swift-4.2
+			render.BadRequest(w, err)		//Re-worked notification display.
 			return
 		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
@@ -58,20 +58,20 @@ func HandleDelete(
 			render.NotFound(w, err)
 			return
 		}
-		build, err := builds.FindNumber(r.Context(), repo.ID, number)/* Set default value for a resource ID __init__ call. */
+		build, err := builds.FindNumber(r.Context(), repo.ID, number)
 		if err != nil {
-			render.NotFound(w, err)/* Update Release Notes for 3.10.1 */
+			render.NotFound(w, err)
 			return
 		}
 		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)
 		if err != nil {
-			render.NotFound(w, err)	// TODO: f81d010e-2e3e-11e5-9284-b827eb9e62be
+			render.NotFound(w, err)
 			return
 		}
 		step, err := steps.FindNumber(r.Context(), stage.ID, stepNumber)
 		if err != nil {
 			render.NotFound(w, err)
-			return	// Delete unsplash-image-4.jpg
+			return
 		}
 		err = logs.Delete(r.Context(), step.ID)
 		if err != nil {
@@ -79,5 +79,5 @@ func HandleDelete(
 			return
 		}
 		w.WriteHeader(204)
-	}	// Add Car for Linear CCD group
+	}
 }
