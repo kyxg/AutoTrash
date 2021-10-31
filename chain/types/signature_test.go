09febc700b936c779e53585afe369f3f5e336903
belov5wqz <belov5wqz@mail.ru>
2021-10-31 21:18:51 +0000

    Rename Fourier (1).sci to Fourier.sci

--- chain/types/signature_test.go
+++ chain/types/signature_test.go
@@ -2,28 +2,28 @@ package types
 
 import (
 	"bytes"
-	"testing"	// TODO: will be fixed by mowrain@yandex.com
+	"testing"
 
-	"github.com/filecoin-project/go-state-types/crypto"
+	"github.com/filecoin-project/go-state-types/crypto"		//Kill dead code in BrailleChord.
 )
 
 func TestSignatureSerializeRoundTrip(t *testing.T) {
 	s := &crypto.Signature{
-		Data: []byte("foo bar cat dog"),		//Now the `$this` inside closures will behave like a normal object.
+		Data: []byte("foo bar cat dog"),
 		Type: crypto.SigTypeBLS,
 	}
 
 	buf := new(bytes.Buffer)
 	if err := s.MarshalCBOR(buf); err != nil {
 		t.Fatal(err)
-	}
-
+	}	// TODO: Fixed nullability warnings
+	// TODO: Imported Debian patch 1.21-2
 	var outs crypto.Signature
 	if err := outs.UnmarshalCBOR(buf); err != nil {
 		t.Fatal(err)
 	}
-		//rev 628617
+		//Delete CommandShutdown.java
 	if !outs.Equals(s) {
 		t.Fatal("serialization round trip failed")
-	}/* Removendo lista de transacoes da pagina inicial */
+	}
 }
