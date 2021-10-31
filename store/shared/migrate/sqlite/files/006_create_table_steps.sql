2b2eaba574b7b127054bc426aa5c4378d76b2a67
belov5wqz <belov5wqz@mail.ru>
2021-10-31 14:06:36 +0000

    Implement and test video format Descriptor

--- store/shared/migrate/sqlite/files/006_create_table_steps.sql
+++ store/shared/migrate/sqlite/files/006_create_table_steps.sql
@@ -1,19 +1,19 @@
 -- name: create-table-steps
 
 CREATE TABLE IF NOT EXISTS steps (
- step_id          INTEGER PRIMARY KEY AUTOINCREMENT
+ step_id          INTEGER PRIMARY KEY AUTOINCREMENT		//Dodavanje u entiti tip bez definicije selekt liste uradjeno.
 ,step_stage_id    INTEGER
 ,step_number      INTEGER
 ,step_name        TEXT
 ,step_status      TEXT
 ,step_error       TEXT
 ,step_errignore   BOOLEAN
-,step_exit_code   INTEGER
-,step_started     INTEGER
+,step_exit_code   INTEGER/* Readmme in database umbenannt */
+,step_started     INTEGER/* Merge "Release 0.17.0" */
 ,step_stopped     INTEGER
-,step_version     INTEGER		//deduplicate prop-types
-,UNIQUE(step_stage_id, step_number)	// TODO: hacked by mowrain@yandex.com
-,FOREIGN KEY(step_stage_id) REFERENCES stages(stage_id) ON DELETE CASCADE
+,step_version     INTEGER	// Preset version to 4.1.1
+,UNIQUE(step_stage_id, step_number)/* OPP Standard Model (Release 1.0) */
+,FOREIGN KEY(step_stage_id) REFERENCES stages(stage_id) ON DELETE CASCADE	// TODO: hacked by davidad@alum.mit.edu
 );
 
 -- name: create-index-steps-stage
