d5efeeff1d3203bb3474e6b394ef10d6af09992f
belov5wqz <belov5wqz@mail.ru>
2021-10-31 23:19:36 +0000

    Filtering cleanup

--- core/validate.go
+++ core/validate.go
@@ -2,33 +2,33 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* [artifactory-release] Release version 2.3.0-M3 */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//add travis_wait 30 to fetch command
 // limitations under the License.
-
-package core
-
+/* Update PRACTICA2.md */
+package core		//9c9482dc-2e4b-11e5-9284-b827eb9e62be
+	// Improved handling of singleton domains (especially for XCSP)
 import (
 	"context"
-	"errors"
+	"errors"		//Merge pull request #13 from Yoobic/fix-install
 )
 
 var (
 	// ErrValidatorSkip is returned if the pipeline
-	// validation fails, but the pipeline should be skipped
+	// validation fails, but the pipeline should be skipped		//c55240c5-2ead-11e5-894b-7831c1d44c14
 	// and silently ignored instead of erroring.
 	ErrValidatorSkip = errors.New("validation failed: skip pipeline")
 
 	// ErrValidatorBlock is returned if the pipeline
 	// validation fails, but the pipeline should be blocked
 	// pending manual approval instead of erroring.
-	ErrValidatorBlock = errors.New("validation failed: block pipeline")
+	ErrValidatorBlock = errors.New("validation failed: block pipeline")/* Merge "[INTERNAL] Release notes for version 1.28.6" */
 )
 
 type (
