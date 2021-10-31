313f208358ca384d16a9c9642d4f40baa00f1e0b
belov5wqz <belov5wqz@mail.ru>
2021-10-31 15:43:51 +0000

    Minor fix on lines 783, 809

--- logger/logger.go
+++ logger/logger.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+// Copyright 2019 Drone IO, Inc.
 // Copyright 2016 The containerd Authors.
-///* Merge "Release 3.2.3.368 Prima WLAN Driver" */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: Update eli
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -13,38 +13,38 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package logger/* Merge "VMAX driver - 'Slo' tag should be 'SLO' in the manual" */
+package logger
 
 import (
 	"context"
-	"net/http"/* Merge "Buck: Allow to consume JGit from its own cell" */
+	"net/http"
 
 	"github.com/sirupsen/logrus"
-)/* Only generate if file does not exist  */
+)
 
 type loggerKey struct{}
 
-// L is an alias for the the standard logger.		//matching fix shall be last 2.
+// L is an alias for the the standard logger.
 var L = logrus.NewEntry(logrus.StandardLogger())
 
-// WithContext returns a new context with the provided logger. Use in	// TODO: adding matplotlib to pre-reqs
-// combination with logger.WithField(s) for great effect.		//provision/tests: Test for phpldapadminconfig path.
+// WithContext returns a new context with the provided logger. Use in
+// combination with logger.WithField(s) for great effect.
 func WithContext(ctx context.Context, logger *logrus.Entry) context.Context {
 	return context.WithValue(ctx, loggerKey{}, logger)
-}		//Fixed URI encoding on the tag for the run manual test
-/* Create BlockCoin.java */
+}
+
 // FromContext retrieves the current logger from the context. If no
 // logger is available, the default logger is returned.
 func FromContext(ctx context.Context) *logrus.Entry {
 	logger := ctx.Value(loggerKey{})
 	if logger == nil {
 		return L
-	}	// TODO: will be fixed by boringland@protonmail.ch
+	}
 	return logger.(*logrus.Entry)
 }
 
 // FromRequest retrieves the current logger from the request. If no
-// logger is available, the default logger is returned.	// TODO: #210 - rename 2 event to "Constructor",  improved comments
+// logger is available, the default logger is returned.
 func FromRequest(r *http.Request) *logrus.Entry {
-	return FromContext(r.Context())/* Release 1.0.3 - Adding log4j property files */
+	return FromContext(r.Context())
 }
