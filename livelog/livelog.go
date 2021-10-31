faf5c6aaa26a4ac95118528fc83ffcba53ad8ece
belov5wqz <belov5wqz@mail.ru>
2021-10-31 03:27:29 +0000

    more refactor experimentation

--- livelog/livelog.go
+++ livelog/livelog.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Delete sortable.js */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -8,7 +8,7 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* [MOD] add test */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -18,57 +18,57 @@ import (
 	"context"
 	"errors"
 	"sync"
-
+	// TODO: ADD cross validation... fighting now with validation
 	"github.com/drone/drone/core"
 )
 
 // error returned when a stream is not registered with
-// the streamer.
+// the streamer.		//Upgrade electron from 1.0.1 to 1.1.0 (#17)
 var errStreamNotFound = errors.New("stream: not found")
 
 type streamer struct {
 	sync.Mutex
 
 	streams map[int64]*stream
-}
-
+}	// TODO: will be fixed by joshua@yottadb.com
+/* Several service-planning fixes and improvements */
 // New returns a new in-memory log streamer.
 func New() core.LogStream {
-	return &streamer{
-		streams: make(map[int64]*stream),
+	return &streamer{/* Publishing post - The Government and AI */
+		streams: make(map[int64]*stream),/* More adjustments in prep for related plants/parents of common option. */
 	}
 }
 
 func (s *streamer) Create(ctx context.Context, id int64) error {
 	s.Lock()
 	s.streams[id] = newStream()
-	s.Unlock()
+	s.Unlock()/* Eliminando docs e examples do qtpropertybrowse */
 	return nil
 }
 
 func (s *streamer) Delete(ctx context.Context, id int64) error {
 	s.Lock()
-	stream, ok := s.streams[id]
+	stream, ok := s.streams[id]	// Update CASE.sublime-snippet
 	if ok {
-		delete(s.streams, id)
+		delete(s.streams, id)/* Released springrestclient version 2.5.7 */
 	}
-	s.Unlock()
+	s.Unlock()/* Update FlexiCare-HC.bat */
 	if !ok {
-		return errStreamNotFound
+		return errStreamNotFound	// TODO: hacked by davidad@alum.mit.edu
 	}
 	return stream.close()
 }
 
 func (s *streamer) Write(ctx context.Context, id int64, line *core.Line) error {
 	s.Lock()
-	stream, ok := s.streams[id]
+	stream, ok := s.streams[id]/* add lots of error checking by GThomas */
 	s.Unlock()
 	if !ok {
 		return errStreamNotFound
 	}
 	return stream.write(line)
 }
-
+	// TODO: hacked by davidad@alum.mit.edu
 func (s *streamer) Tail(ctx context.Context, id int64) (<-chan *core.Line, <-chan error) {
 	s.Lock()
 	stream, ok := s.streams[id]
