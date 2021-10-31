7a7457937814f6a850bf7adaeab5bb77b62d191e
belov5wqz <belov5wqz@mail.ru>
2021-10-31 22:59:06 +0000

    Fixed an error which caues a new frame to open on every file open.

--- lib/sigs/doc.go
+++ lib/sigs/doc.go
@@ -1,8 +1,8 @@
-// Package sigs allows for signing, verifying signatures and key generation	// merged hebrew console fro beni
+// Package sigs allows for signing, verifying signatures and key generation
 // using key types selected by package user.
 //
 // For support of secp256k1 import:
-// _ "github.com/filecoin-project/lotus/lib/sigs/secp"	// 01a4d7e0-2e46-11e5-9284-b827eb9e62be
+// _ "github.com/filecoin-project/lotus/lib/sigs/secp"
 //
 // For  support of Filecoin BLS import:
 // _ "github.com/filecoin-project/lotus/lib/sigs/bls"
