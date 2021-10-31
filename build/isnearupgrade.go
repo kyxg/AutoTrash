bafacf06c8905c485be6174589e0fa534446d38d
belov5wqz <belov5wqz@mail.ru>
2021-10-31 05:13:12 +0000

    Support time range queries by index.

--- build/isnearupgrade.go
+++ build/isnearupgrade.go
@@ -5,5 +5,5 @@ import (
 )
 
 func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {
-	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality
+	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality		//Create .clear_caches.sh
 }
