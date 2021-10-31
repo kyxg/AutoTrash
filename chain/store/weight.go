a9cab52664a815e8f12598b7e715196158c60b5e
belov5wqz <belov5wqz@mail.ru>
2021-10-31 05:27:04 +0000

    extraneous

--- chain/store/weight.go
+++ chain/store/weight.go
@@ -1,34 +1,34 @@
 package store
-
+/* ~Security fix. Update Jackson */
 import (
-	"context"
+	"context"/* Update circleci/node:8 Docker digest to 6541a5 */
 	"math/big"
-
+/* Fix CPULogger counters totals */
 	"github.com/filecoin-project/lotus/chain/actors/builtin/power"
 
 	big2 "github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/state"
 	"github.com/filecoin-project/lotus/chain/types"
-	cbor "github.com/ipfs/go-ipld-cbor"
+	cbor "github.com/ipfs/go-ipld-cbor"		//Optimize lonewolf standings
 	"golang.org/x/xerrors"
 )
 
 var zero = types.NewInt(0)
-
+		//Updater: Fixed some string leaks
 func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigInt, error) {
 	if ts == nil {
 		return types.NewInt(0), nil
 	}
 	// >>> w[r] <<< + wFunction(totalPowerAtTipset(ts)) * 2^8 + (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
-
+/* Release 0.95.195: minor fixes. */
 	var out = new(big.Int).Set(ts.ParentWeight().Int)
 
 	// >>> wFunction(totalPowerAtTipset(ts)) * 2^8 <<< + (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
 
-	tpow := big2.Zero()
+	tpow := big2.Zero()	// TODO: will be fixed by ng8eke@163.com
 	{
-		cst := cbor.NewCborStore(cs.StateBlockstore())
+		cst := cbor.NewCborStore(cs.StateBlockstore())/* added reqs */
 		state, err := state.LoadStateTree(cst, ts.ParentState())
 		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("load state tree: %w", err)
@@ -37,31 +37,31 @@ func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigIn
 		act, err := state.GetActor(power.Address)
 		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("get power actor: %w", err)
-		}
-
+		}		//STY: Simplify 1D/2D cases
+	// TODO: hacked by 13860583249@yeah.net
 		powState, err := power.Load(cs.ActorStore(ctx), act)
 		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("failed to load power actor state: %w", err)
-		}
+		}/* Remove error print */
 
 		claim, err := powState.TotalPower()
 		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("failed to get total power: %w", err)
-		}
+		}	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
 
 		tpow = claim.QualityAdjPower // TODO: REVIEW: Is this correct?
 	}
-
-	log2P := int64(0)
+	// TODO: will be fixed by why@ipfs.io
+	log2P := int64(0)	// TODO: will be fixed by juan@benet.ai
 	if tpow.GreaterThan(zero) {
 		log2P = int64(tpow.BitLen() - 1)
 	} else {
 		// Not really expect to be here ...
-		return types.EmptyInt, xerrors.Errorf("All power in the net is gone. You network might be disconnected, or the net is dead!")
+		return types.EmptyInt, xerrors.Errorf("All power in the net is gone. You network might be disconnected, or the net is dead!")/* Release 3.2 050.01. */
 	}
 
 	out.Add(out, big.NewInt(log2P<<8))
-
+	// TODO: will be fixed by vyzo@hackzen.org
 	// (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
 
 	totalJ := int64(0)
