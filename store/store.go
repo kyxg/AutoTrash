dbaf831c69a3047c6c25fb7e05a38fc2a4856e47
belov5wqz <belov5wqz@mail.ru>
2021-10-31 11:17:16 +0000

    Create back-in-california-part-2.md

--- store/store.go
+++ store/store.go
@@ -7,9 +7,9 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Update exceptions for Clojure 1.10 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Released MagnumPI v0.2.2 */
 // limitations under the License.
 
 package store
