8416508398750684f954409f4e1f6d09e57bf6be
belov5wqz <belov5wqz@mail.ru>
2021-10-31 16:51:37 +0000

    Method divideTwo

---
+++ tests/integration/duplicate_urns/step2/index.ts
@@ -0,0 +1,19 @@
+// Copyright 2016-2018, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");/* Released 4.0.0.RELEASE */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software/* Updated 1 link from mitre.org to Releases page */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.	// Delete TSNE.lua
+
+import { Resource } from "./resource";
+
+// Setup for the next test.
+const a = new Resource("a", { state: 4 });
+
