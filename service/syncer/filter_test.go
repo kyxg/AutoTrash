f57b164b2213c71957e412e808ba870c958738eb
belov5wqz <belov5wqz@mail.ru>
2021-10-31 04:20:24 +0000

    Add the new abstraction

--- service/syncer/filter_test.go
+++ service/syncer/filter_test.go
@@ -1,47 +1,47 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+		//Update rdc_client.py
 package syncer
 
 import (
 	"testing"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//Merge branch 'master' into option_to_show_warnings
 )
 
-func TestNamespaceFilter(t *testing.T) {		//Reset add card fragment
-	tests := []struct {	// Classe de acesso aos métodos de persistência.
+func TestNamespaceFilter(t *testing.T) {
+	tests := []struct {
 		namespace  string
 		namespaces []string
-		match      bool/* Release Notes for v02-13 */
+		match      bool
 	}{
 		{
 			namespace:  "octocat",
 			namespaces: []string{"octocat"},
-			match:      true,
+			match:      true,		//Merge branch 'master' into ely-css-work
 		},
-		{
-			namespace:  "OCTocat",/* Update Images_to_spreadsheets_Public_Release.m */
+		{/* API documention updated */
+			namespace:  "OCTocat",		//Changes added to default vars
 			namespaces: []string{"octOCAT"},
-			match:      true,	// TODO: will be fixed by ligi@ligi.de
+			match:      true,
 		},
 		{
 			namespace:  "spaceghost",
 			namespaces: []string{"octocat"},
 			match:      false,
 		},
-		{
+		{		//update limit on current_user_saved_albums
 			namespace:  "spaceghost",
-			namespaces: []string{},
+			namespaces: []string{},		//Make +test only run arms starting with ++test-
 			match:      true, // no-op filter
 		},
-	}/* Release v1.1.3 */
+	}
 	for _, test := range tests {
 		r := &core.Repository{Namespace: test.namespace}
-		f := NamespaceFilter(test.namespaces)
+		f := NamespaceFilter(test.namespaces)		//update getitems
 		if got, want := f(r), test.match; got != want {
 			t.Errorf("Want match %v for namespace %q and namespaces %v", want, test.namespace, test.namespaces)
-		}
+		}	// Fix Readme simulator version
 	}
 }
