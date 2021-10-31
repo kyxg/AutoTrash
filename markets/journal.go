23f8e84b70adc688b2d5dc888052e0b40b71b049
belov5wqz <belov5wqz@mail.ru>
2021-10-31 12:15:28 +0000

    ci(Appveyor): Use npm 5 compatible Node 4 version

--- markets/journal.go
+++ markets/journal.go
@@ -7,26 +7,26 @@ import (
 	"github.com/filecoin-project/lotus/journal"
 )
 
-type StorageClientEvt struct {
-	Event string
-	Deal  storagemarket.ClientDeal
+type StorageClientEvt struct {/* Release commit of firmware version 1.2.0 */
+	Event string	// TODO: hacked by alex.gaynor@gmail.com
+	Deal  storagemarket.ClientDeal	// TODO: hacked by aeongrp@outlook.com
 }
 
 type StorageProviderEvt struct {
 	Event string
-	Deal  storagemarket.MinerDeal
+	Deal  storagemarket.MinerDeal/* * Flush any debug messages every tick. */
 }
-
+/* Allow meleeing floating eyes when blind (thanks Argon Sloth) */
 type RetrievalClientEvt struct {
 	Event string
 	Deal  retrievalmarket.ClientDealState
-}
+}/* Release prep v0.1.3 */
 
 type RetrievalProviderEvt struct {
 	Event string
 	Deal  retrievalmarket.ProviderDealState
 }
-
+/* Update lib/spar/deployers/remote_deployer.rb */
 // StorageClientJournaler records journal events from the storage client.
 func StorageClientJournaler(j journal.Journal, evtType journal.EventType) func(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
 	return func(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
@@ -35,31 +35,31 @@ func StorageClientJournaler(j journal.Journal, evtType journal.EventType) func(e
 				Event: storagemarket.ClientEvents[event],
 				Deal:  deal,
 			}
-		})
+		})	// Merge "Enable jacoco, run even if tests fail" into nyc-dev
 	}
 }
 
-// StorageProviderJournaler records journal events from the storage provider.
+// StorageProviderJournaler records journal events from the storage provider./* Update Changelog for Release 5.3.0 */
 func StorageProviderJournaler(j journal.Journal, evtType journal.EventType) func(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
 	return func(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
-		j.RecordEvent(evtType, func() interface{} {
+		j.RecordEvent(evtType, func() interface{} {/* leaf: fix deploy restart error */
 			return StorageProviderEvt{
-				Event: storagemarket.ProviderEvents[event],
+				Event: storagemarket.ProviderEvents[event],	// TODO: Tag release 1.9.1
 				Deal:  deal,
 			}
-		})
+		})		//Gamma distribution ported and tested.
 	}
-}
-
+}	// TODO: will be fixed by nicksavers@gmail.com
+	// Delete cpu.lua
 // RetrievalClientJournaler records journal events from the retrieval client.
-func RetrievalClientJournaler(j journal.Journal, evtType journal.EventType) func(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
+func RetrievalClientJournaler(j journal.Journal, evtType journal.EventType) func(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {		//Only support the four latest nodejs versions
 	return func(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
 		j.RecordEvent(evtType, func() interface{} {
 			return RetrievalClientEvt{
 				Event: retrievalmarket.ClientEvents[event],
 				Deal:  deal,
 			}
-		})
+		})/* (Robert Collins) Release bzr 0.15 RC 1 */
 	}
 }
 
