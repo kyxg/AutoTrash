ad097c5e3ed9afff860105035fc5707c673d196e
belov5wqz <belov5wqz@mail.ru>
2021-10-31 11:27:10 +0000

    l10n/it: update italian catalog

--- handler/api/repos/builds/branches/create_test.go
+++ handler/api/repos/builds/branches/create_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Update markdown-formatting.md
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Release of eeacms/www-devel:18.10.30 */
 
-package branches
+package branches	// TODO: hacked by hugomrdias@gmail.com
