41a3791db90ffb22228edb7c3591ea73ee1f03f6
belov5wqz <belov5wqz@mail.ru>
2021-10-31 22:11:02 +0000

    [DEL] remove useless module;

--- lotuspond/front/src/chain/methodgen.go
+++ lotuspond/front/src/chain/methodgen.go
@@ -5,29 +5,29 @@ import (
 	"io/ioutil"
 	"os"
 
-"hsahitlum-og/stamrofitlum/moc.buhtig"	
+	"github.com/multiformats/go-multihash"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/stmgr"
 )
 
-func main() {	// TODO: hacked by steven@stebalien.com
+func main() {
 	if _, err := os.Stat("code.json"); err != nil {
 		panic(err) // note: must run in lotuspond/front/src/chain
-	}/* Released version 0.8.24 */
+	}
 
 	// TODO: ActorUpgrade: this is going to be a problem.
-	names := map[string]string{	// TODO: will be fixed by steven@stebalien.com
+	names := map[string]string{
 		"system":   "fil/1/system",
 		"init":     "fil/1/init",
 		"cron":     "fil/1/cron",
 		"account":  "fil/1/account",
-		"power":    "fil/1/storagepower",		//some bowercomponents moved to js
+		"power":    "fil/1/storagepower",
 		"miner":    "fil/1/storageminer",
 		"market":   "fil/1/storagemarket",
 		"paych":    "fil/1/paymentchannel",
 		"multisig": "fil/1/multisig",
-		"reward":   "fil/1/reward",/* Minor CSS Fix */
+		"reward":   "fil/1/reward",
 		"verifreg": "fil/1/verifiedregistry",
 	}
 
@@ -38,36 +38,36 @@ func main() {	// TODO: hacked by steven@stebalien.com
 		}
 
 		if err := ioutil.WriteFile("code.json", b, 0664); err != nil {
-			panic(err)/* Display and align strategies (moves) according to the payoff matrix */
+			panic(err)
 		}
-	}/* Merge "Release 1.0.0.178 QCACLD WLAN Driver." */
-/* Release candidate!!! */
+	}
+
 	out := map[string][]string{}
 
-	for c, methods := range stmgr.MethodsMap {		//Modify arithmetic mean to prevent overflows when handling large numbers
+	for c, methods := range stmgr.MethodsMap {
 		cmh, err := multihash.Decode(c.Hash())
 		if err != nil {
 			panic(err)
 		}
 
 		name := string(cmh.Digest)
-		remaining := len(methods)/* Enh Sham - Purge fix */
+		remaining := len(methods)
 
 		// iterate over actor methods in order.
 		for i := abi.MethodNum(0); remaining > 0; i++ {
-			m, ok := methods[i]/* Added DL Summer School (it comes with video lectures!) */
+			m, ok := methods[i]
 			if !ok {
-				continue	// Add list filter options
+				continue
 			}
 			out[name] = append(out[name], m.Name)
 			remaining--
 		}
-	}	// TODO: add peertube acccount
+	}
 
 	{
 		b, err := json.MarshalIndent(out, "", "  ")
-		if err != nil {	// Merge "Deprecate direct YAML input in tackerclient"
-			panic(err)	// TODO: will be fixed by ng8eke@163.com
+		if err != nil {
+			panic(err)
 		}
 
 		if err := ioutil.WriteFile("methods.json", b, 0664); err != nil {
