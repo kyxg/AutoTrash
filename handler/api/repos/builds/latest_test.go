920d31b8310b995d762ba27bd192924990e65bf2
belov5wqz <belov5wqz@mail.ru>
2021-10-31 07:10:41 +0000

    Changed the design - even background color, different spacing.

--- handler/api/repos/builds/latest_test.go
+++ handler/api/repos/builds/latest_test.go
@@ -1,25 +1,25 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* ne2k_pci: Add a check on infinite loop */
 package builds
 
-import (
+import (		//Create init.tmpl
 	"context"
 	"encoding/json"
-	"net/http/httptest"
+	"net/http/httptest"/* Refactored to make xml more DRY */
 	"testing"
 
-	"github.com/drone/drone/mock"
+	"github.com/drone/drone/mock"/* Day 5: sonatanews: fermer commentaires et impersonate */
 	"github.com/drone/drone/handler/api/errors"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"	// TODO: Merge "Fixed Admin State in core file for Virtual network."
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)
+)	// Update manifest.rb
 
 func TestLast(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* Have the list of expectations be a &rest-list rather than an explicit one. */
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
@@ -33,7 +33,7 @@ func TestLast(t *testing.T) {
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("name", "hello-world")	// TODO: hacked by m-ou.se@m-ou.se
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
@@ -41,7 +41,7 @@ func TestLast(t *testing.T) {
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
-	HandleLast(repos, builds, stages)(w, r)
+	HandleLast(repos, builds, stages)(w, r)	// Fix the NoMirrorShapedRecipe
 
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
@@ -50,13 +50,13 @@ func TestLast(t *testing.T) {
 	got, want := &buildWithStages{}, &buildWithStages{mockBuild, mockStages}
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)
+		t.Errorf(diff)/* Added CCControlExtension module */
 	}
-}
-
+}		//Added new Swift version badge
+/* fixed broken POM reference to ehcache */
 func TestLast_RepoNotFound(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()
+	controller := gomock.NewController(t)/* Bumped mesos to master f050bf01af8f9f92bbada2c0a2025a459290ed98 (windows). */
+	defer controller.Finish()/* Merge branch 'master' into measurement-blackboard */
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(nil, errors.ErrNotFound)
@@ -67,7 +67,7 @@ func TestLast_RepoNotFound(t *testing.T) {
 	c.URLParams.Add("number", "1")
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
+	r := httptest.NewRequest("GET", "/", nil)	// TODO: New version of Ugallu - 0.1.7
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
@@ -76,7 +76,7 @@ func TestLast_RepoNotFound(t *testing.T) {
 
 	if got, want := w.Code, 404; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}
+	}		//Raise error on key not found or out of range when traversing path
 
 	got, want := new(errors.Error), errors.ErrNotFound
 	json.NewDecoder(w.Body).Decode(got)
