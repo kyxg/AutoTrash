353120410a568cc60eeef9e259c2026c565430c9
belov5wqz <belov5wqz@mail.ru>
2021-10-31 15:09:40 +0000

    Merge "Do not include bogus certs in final chain output" into jb-mr2-dev

--- store/shared/migrate/sqlite/files/002_create_table_repos.sql
+++ store/shared/migrate/sqlite/files/002_create_table_repos.sql
@@ -1,47 +1,47 @@
 -- name: create-table-repos
 
-CREATE TABLE IF NOT EXISTS repos (		//fixed color issue with Polygon/VertexColors (-> DensityPlot)
+CREATE TABLE IF NOT EXISTS repos (
  repo_id                    INTEGER PRIMARY KEY AUTOINCREMENT
 ,repo_uid                   TEXT
-,repo_user_id               INTEGER		//Clean up Rumble and add debug rumble control
-,repo_namespace             TEXT
+,repo_user_id               INTEGER
+,repo_namespace             TEXT/* ui improvement (i think) */
 ,repo_name                  TEXT
 ,repo_slug                  TEXT
-,repo_scm                   TEXT
+,repo_scm                   TEXT		//Image display fixes, note formatting, etc
 ,repo_clone_url             TEXT
 ,repo_ssh_url               TEXT
 ,repo_html_url              TEXT
 ,repo_active                BOOLEAN
 ,repo_private               BOOLEAN
 ,repo_visibility            TEXT
-,repo_branch                TEXT/* [jgitflow-maven-plugin] updating poms for 1.3.5-SNAPSHOT development */
-,repo_counter               INTEGER
+,repo_branch                TEXT
+,repo_counter               INTEGER	// Merge "mdss: hdmi: Correct HDMI Tx controller settings for DVI mode"
 ,repo_config                TEXT
 ,repo_timeout               INTEGER
 ,repo_trusted               BOOLEAN
 ,repo_protected             BOOLEAN
 ,repo_synced                INTEGER
-,repo_created               INTEGER/* DOC Release: enhanced procedure */
+,repo_created               INTEGER
 ,repo_updated               INTEGER
 ,repo_version               INTEGER
-,repo_signer                TEXT
+,repo_signer                TEXT/* Merge branch 'master' into contribution-info-readme */
 ,repo_secret                TEXT
 ,UNIQUE(repo_slug)
 ,UNIQUE(repo_uid)
 );
 
--- name: alter-table-repos-add-column-no-fork
+-- name: alter-table-repos-add-column-no-fork	// SFD "one sheet," double sided
 
-ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT 0;
+ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT 0;	// TODO: add1.c: assertions on the argument values should use MPFR_ASSERTN.
 
 -- name: alter-table-repos-add-column-no-pulls
 
 ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT 0;
-		//Merge "Update upgrade check removed drivers for ussuri"
--- name: alter-table-repos-add-column-cancel-pulls
 
+-- name: alter-table-repos-add-column-cancel-pulls
+/* * bencode: return offset read buffer; */
 ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT 0;
 
--- name: alter-table-repos-add-column-cancel-push/* [NGRINDER-488] Add the check on the no test run */
+-- name: alter-table-repos-add-column-cancel-push
 
 ALTER TABLE repos ADD COLUMN repo_cancel_push BOOLEAN NOT NULL DEFAULT 0;
