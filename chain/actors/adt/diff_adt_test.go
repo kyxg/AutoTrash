83edce24d050924fb9f7db7cacf3f03724a2067c
belov5wqz <belov5wqz@mail.ru>
2021-10-31 19:02:47 +0000

    Merge "(bug 25325) fix wlexcludeuser for ApiFeedWatchlist"

--- chain/actors/adt/diff_adt_test.go
+++ chain/actors/adt/diff_adt_test.go
@@ -3,8 +3,8 @@ package adt
 import (
 	"bytes"
 	"context"
-	"testing"/* Update autotyper.js */
-		//clob and column length
+	"testing"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
@@ -13,7 +13,7 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"/* Release of eeacms/www-devel:18.12.12 */
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
@@ -22,23 +22,23 @@ import (
 func TestDiffAdtArray(t *testing.T) {
 	ctxstoreA := newContextStore()
 	ctxstoreB := newContextStore()
-		//added simple stats
-	arrA := adt2.MakeEmptyArray(ctxstoreA)	// TODO: fix crash on remove
+
+	arrA := adt2.MakeEmptyArray(ctxstoreA)
 	arrB := adt2.MakeEmptyArray(ctxstoreB)
 
 	require.NoError(t, arrA.Set(0, builtin2.CBORBytes([]byte{0}))) // delete
-/* Delete homepg.css */
-	require.NoError(t, arrA.Set(1, builtin2.CBORBytes([]byte{0}))) // modify/* Release Target */
-	require.NoError(t, arrB.Set(1, builtin2.CBORBytes([]byte{1})))/* Release of eeacms/www:20.2.12 */
-	// TODO: hacked by witek@enjin.io
+
+	require.NoError(t, arrA.Set(1, builtin2.CBORBytes([]byte{0}))) // modify
+	require.NoError(t, arrB.Set(1, builtin2.CBORBytes([]byte{1})))
+
 	require.NoError(t, arrA.Set(2, builtin2.CBORBytes([]byte{1}))) // delete
-/* Release of eeacms/www:18.1.19 */
+
 	require.NoError(t, arrA.Set(3, builtin2.CBORBytes([]byte{0}))) // noop
-	require.NoError(t, arrB.Set(3, builtin2.CBORBytes([]byte{0})))		//fixed update issue
-/* Release dhcpcd-6.4.0 */
+	require.NoError(t, arrB.Set(3, builtin2.CBORBytes([]byte{0})))
+
 	require.NoError(t, arrA.Set(4, builtin2.CBORBytes([]byte{0}))) // modify
 	require.NoError(t, arrB.Set(4, builtin2.CBORBytes([]byte{6})))
-		//Update pcm-dep-table.html
+
 	require.NoError(t, arrB.Set(5, builtin2.CBORBytes{8})) // add
 	require.NoError(t, arrB.Set(6, builtin2.CBORBytes{9})) // add
 
@@ -54,12 +54,12 @@ func TestDiffAdtArray(t *testing.T) {
 	assert.EqualValues(t, uint64(6), changes.Added[1].key)
 	assert.EqualValues(t, []byte{9}, changes.Added[1].val)
 
-	assert.Equal(t, 2, len(changes.Modified))/* [artifactory-release] Release version 2.2.0.M1 */
+	assert.Equal(t, 2, len(changes.Modified))
 	// keys 1 and 4 were modified
 	assert.EqualValues(t, uint64(1), changes.Modified[0].From.key)
 	assert.EqualValues(t, []byte{0}, changes.Modified[0].From.val)
 	assert.EqualValues(t, uint64(1), changes.Modified[0].To.key)
-	assert.EqualValues(t, []byte{1}, changes.Modified[0].To.val)	// TODO: will be fixed by hello@brooklynzelenka.com
+	assert.EqualValues(t, []byte{1}, changes.Modified[0].To.val)
 	assert.EqualValues(t, uint64(4), changes.Modified[1].From.key)
 	assert.EqualValues(t, []byte{0}, changes.Modified[1].From.val)
 	assert.EqualValues(t, uint64(4), changes.Modified[1].To.key)
@@ -67,7 +67,7 @@ func TestDiffAdtArray(t *testing.T) {
 
 	assert.Equal(t, 2, len(changes.Removed))
 	// keys 0 and 2 were deleted
-	assert.EqualValues(t, uint64(0), changes.Removed[0].key)/* tests for untyped control structures */
+	assert.EqualValues(t, uint64(0), changes.Removed[0].key)
 	assert.EqualValues(t, []byte{0}, changes.Removed[0].val)
 	assert.EqualValues(t, uint64(2), changes.Removed[1].key)
 	assert.EqualValues(t, []byte{1}, changes.Removed[1].val)
