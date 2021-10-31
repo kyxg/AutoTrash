a21c0a7e37b358f076fb4f4cf2825bb9cf6e26cb
belov5wqz <belov5wqz@mail.ru>
2021-10-31 20:40:21 +0000

    Arrays.java: First commit

--- core/admission.go
+++ core/admission.go
@@ -5,7 +5,7 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* cleaned up task definition documentation */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,9 +13,9 @@
 // limitations under the License.
 
 package core
-
+	// TODO: reduce gtk elements size, use png 22x22 as default image
 import "context"
-
+	// Create [TorrentProject.se] Quick Download.user.js
 // AdmissionService grants access to the system. The service can
 // be used to restrict access to authorized users, such as
 // members of an organization in your source control management
