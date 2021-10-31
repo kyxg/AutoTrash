c54a71b9bbcfc12d0f44374ecdef8964c97d512c
belov5wqz <belov5wqz@mail.ru>
2021-10-31 00:56:48 +0000

    Added logic to extract PART NUMBER, SPEED GRADE and PACKAGE from .csv file.

--- handler/api/repos/builds/branches/list_test.go
+++ handler/api/repos/builds/branches/list_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// d907d424-2e75-11e5-9284-b827eb9e62be
 // that can be found in the LICENSE file.
 
-package branches
+package branches/* Merge "[INTERNAL] Release notes for version 1.28.24" */
