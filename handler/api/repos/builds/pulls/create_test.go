62e55a840513cbea8b71d725fb916c06a627962f
belov5wqz <belov5wqz@mail.ru>
2021-11-01 13:47:10 +0000

    added unimplemented tests

--- handler/api/repos/builds/pulls/create_test.go
+++ handler/api/repos/builds/pulls/create_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//43f03b4c-2e4d-11e5-9284-b827eb9e62be
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Update the oh-my-zsh submodule */
 package pulls
