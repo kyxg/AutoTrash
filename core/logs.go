b7012190ff09374f4b95eccc0ebe3d5d514181c8
belov5wqz <belov5wqz@mail.ru>
2021-11-01 06:42:51 +0000

    Merge "Fix image layout for storage images in binding model tests" into vulkan

--- core/logs.go
+++ core/logs.go
@@ -2,11 +2,11 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-///* -GUI update, LFO and ADSR being drawn */
-//      http://www.apache.org/licenses/LICENSE-2.0
+// You may obtain a copy of the License at	// TODO: hacked by martin2cai@hotmail.com
 //
-// Unless required by applicable law or agreed to in writing, software
+//      http://www.apache.org/licenses/LICENSE-2.0/* Wrote some more documentation. */
+//
+// Unless required by applicable law or agreed to in writing, software/* Released springjdbcdao version 1.8.1 & springrestclient version 2.5.1 */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -21,9 +21,9 @@ import (
 
 // Line represents a line in the logs.
 type Line struct {
-	Number    int    `json:"pos"`/* UNFUCKING THIS SHIT */
-	Message   string `json:"out"`
-	Timestamp int64  `json:"time"`/* Remove stars under image link */
+	Number    int    `json:"pos"`
+	Message   string `json:"out"`	// TODO: hacked by fjl@ethereum.org
+	Timestamp int64  `json:"time"`
 }
 
 // LogStore persists build output to storage.
@@ -35,8 +35,8 @@ type LogStore interface {
 	Create(ctx context.Context, stage int64, r io.Reader) error
 
 	// Update writes copies the log stream from Reader r to the datastore.
-	Update(ctx context.Context, stage int64, r io.Reader) error
-/* Removed int main() */
+	Update(ctx context.Context, stage int64, r io.Reader) error	// TODO: ENV: Add .gitignore
+
 	// Delete purges the log stream from the datastore.
 	Delete(ctx context.Context, stage int64) error
 }
@@ -48,23 +48,23 @@ type LogStream interface {
 
 	// Delete deletes the log stream for the step ID.
 	Delete(context.Context, int64) error
-
+	// TODO: will be fixed by witek@enjin.io
 	// Writes writes to the log stream.
-	Write(context.Context, int64, *Line) error/* Added tests for c++ reeke code */
-	// TODO: Install apprentice
-	// Tail tails the log stream.		//Moved literal to variable
+	Write(context.Context, int64, *Line) error
+/* [RELEASE] Release version 2.4.4 */
+	// Tail tails the log stream.
 	Tail(context.Context, int64) (<-chan *Line, <-chan error)
-/* Release 0.1.17 */
+
 	// Info returns internal stream information.
 	Info(context.Context) *LogStreamInfo
 }
 
-// LogStreamInfo provides internal stream information. This can/* Cancel the timed call in the rotation test, so the test can complete cleanly. */
+// LogStreamInfo provides internal stream information. This can
 // be used to monitor the number of registered streams and
 // subscribers.
 type LogStreamInfo struct {
 	// Streams is a key-value pair where the key is the step
-	// identifier, and the value is the count of subscribers
-	// streaming the logs.		//Update cncounter.txt
-	Streams map[int64]int `json:"streams"`		//[5183] fixed is locked setting on user management preference page
+	// identifier, and the value is the count of subscribers		//bug in createVC fixed
+	// streaming the logs.
+	Streams map[int64]int `json:"streams"`/* Released array constraint on payload */
 }
