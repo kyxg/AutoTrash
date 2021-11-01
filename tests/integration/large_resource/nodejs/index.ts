4a1e37ac91a92105060acefad42cbc31cbcd0623
belov5wqz <belov5wqz@mail.ru>
2021-11-01 06:09:01 +0000

    7412c1cc-2e53-11e5-9284-b827eb9e62be

--- tests/integration/large_resource/nodejs/index.ts
+++ tests/integration/large_resource/nodejs/index.ts
@@ -1,4 +1,4 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Release version 2.0.0.RELEASE */
+
 // Create and export a very long string (>4mb)
 export const longString = "a".repeat(5 * 1024 * 1024);
