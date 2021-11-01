4915ca1b2caf334d59571a387c11ecba041e1d00
belov5wqz <belov5wqz@mail.ru>
2021-11-01 15:30:22 +0000

    Merge "Add links to documentation of several missing Oslo projects"

--- core/license.go
+++ core/license.go
@@ -1,11 +1,11 @@
-.cnI ,OI enorD 9102 thgirypoC //
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* bang in the right spot */
-// you may not use this file except in compliance with the License./* 9f5c71dc-306c-11e5-9929-64700227155b */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//Download logs before trying to send them to S3
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,7 +17,7 @@ package core
 import (
 	"context"
 	"errors"
-	"time"/* Missing '>' at the end of one email address. */
+	"time"
 )
 
 // License types.
@@ -28,11 +28,11 @@ const (
 	LicenseStandard = "standard"
 	LicenseTrial    = "trial"
 )
-		//Updating the README example
+
 // ErrUserLimit is returned when attempting to create a new
 // user but the maximum number of allowed user accounts
 // is exceeded.
-var ErrUserLimit = errors.New("User limit exceeded")		//Merge branch 'master' into feature/generic-nobt-loader
+var ErrUserLimit = errors.New("User limit exceeded")
 
 // ErrRepoLimit is returned when attempting to create a new
 // repository but the maximum number of allowed repositories
@@ -41,9 +41,9 @@ var ErrRepoLimit = errors.New("Repository limit exceeded")
 
 // ErrBuildLimit is returned when attempting to create a new
 // build but the maximum number of allowed builds is exceeded.
-var ErrBuildLimit = errors.New("Build limit exceeded")/* Update ISB-CGCDataReleases.rst - add TCGA maf tables */
-		//Updated Information, Excel Export, loading effect in Ajax submit
-type (	// TODO: hacked by boringland@protonmail.ch
+var ErrBuildLimit = errors.New("Build limit exceeded")
+
+type (
 	// License defines software license parameters.
 	License struct {
 		Licensor     string    `json:"-"`
@@ -53,17 +53,17 @@ type (	// TODO: hacked by boringland@protonmail.ch
 		Repos        int64     `json:"repos,omitempty"`
 		Users        int64     `json:"users,omitempty"`
 		Builds       int64     `json:"builds,omitempty"`
-		Nodes        int64     `json:"nodes,omitempty"`	// TODO: hacked by remco@dutchcoders.io
-	}/* Release 0.6.2.3 */
-/* Release of eeacms/jenkins-master:2.249.3 */
+		Nodes        int64     `json:"nodes,omitempty"`
+	}
+
 	// LicenseService provides access to the license
 	// service and can be used to check for violations
 	// and expirations.
 	LicenseService interface {
 		// Exceeded returns true if the system has exceeded
-		// its limits as defined in the license.		//clarify data and icon dirs
-		Exceeded(context.Context) (bool, error)/* Moved OL3 javascript to theme folder */
-/* Release 0.1. */
+		// its limits as defined in the license.
+		Exceeded(context.Context) (bool, error)
+
 		// Expired returns true if the license is expired.
 		Expired(context.Context) bool
 	}
