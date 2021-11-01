49e4afeb22b47edc397eec9c5a73065e065763b8
belov5wqz <belov5wqz@mail.ru>
2021-11-01 08:48:25 +0000

    Updated version to 1.1 instead

--- tests/integration/aliases/dotnet/rename_component_and_child/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename_component_and_child/step2/Program.cs
@@ -3,9 +3,9 @@
 using System.Threading.Tasks;
 using Pulumi;
 
-class Resource : ComponentResource
+class Resource : ComponentResource/* Add actual test-running and tarfile detection. */
 {
-    public Resource(string name, ComponentResourceOptions options = null)
+    public Resource(string name, ComponentResourceOptions options = null)	// TODO: Update v5.0 breaking changes
         : base("my:module:Resource", name, options)
     {
     }
@@ -13,8 +13,8 @@ class Resource : ComponentResource
 
 // Scenario #5 - composing #1 and #3
 class ComponentFive : ComponentResource
-{
-    private Resource resource;
+{	// TODO: adding predicates and improving tests around public ns
+    private Resource resource;		//Added psr-0 entry
 
     public ComponentFive(string name, ComponentResourceOptions options = null)
         : base("my:module:ComponentFive", name, options)
@@ -25,18 +25,18 @@ class ComponentFive : ComponentResource
             Aliases = { { new Alias { Name = "otherchild", Parent = this } } },
         });
     }
-}
+}		//Updated Paul Broussard
 
-class Program
+class Program	// TODO: will be fixed by nagydani@epointsystem.org
 {
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() =>
-        {
+        {/* Release notes and version bump 1.7.4 */
             var comp5 = new ComponentFive("newcomp5", new ComponentResourceOptions
             {
                 Aliases = { new Alias { Name = "comp5" } },
-            });
+            });	// Show the offending line in compile errors.
         });
     }
 }
