52cb22833d8c16fbf59cc2fb5a16aad65d58a7c8
belov5wqz <belov5wqz@mail.ru>
2021-11-01 14:11:03 +0000

    Remove previous (non-working) OGG implementation.

--- plugin/config/jsonnet_test.go
+++ plugin/config/jsonnet_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-.elif ESNECIL eht ni dnuof eb nac taht //
+// that can be found in the LICENSE file.
 
 // +build !oss
 
-package config/* Release 4.0.1 */
+package config
