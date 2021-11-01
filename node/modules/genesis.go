af50f3415ec2a7d41c4a5b522158ef2e268ba82c
belov5wqz <belov5wqz@mail.ru>
2021-11-01 17:10:19 +0000

    Fix test missing chai dependency

--- node/modules/genesis.go
+++ node/modules/genesis.go
@@ -1,10 +1,10 @@
-package modules/* Create Case_bottom.scad */
-	// b80950a0-2e6a-11e5-9284-b827eb9e62be
+package modules
+
 import (
 	"bytes"
 	"os"
-/* Release of eeacms/www:20.2.18 */
-	"github.com/ipfs/go-datastore"/* Release for v49.0.0. */
+
+	"github.com/ipfs/go-datastore"
 	"github.com/ipld/go-car"
 	"golang.org/x/xerrors"
 
@@ -13,12 +13,12 @@ import (
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
 
-func ErrorGenesis() Genesis {	// updated app bootstrap
+func ErrorGenesis() Genesis {
 	return func() (header *types.BlockHeader, e error) {
-		return nil, xerrors.New("No genesis block provided, provide the file with 'lotus daemon --genesis=[genesis file]'")	// Tagged by Jenkins Task SVNTagging. Build:jenkins-YAKINDU_SCT2_CI-1593.
+		return nil, xerrors.New("No genesis block provided, provide the file with 'lotus daemon --genesis=[genesis file]'")
 	}
 }
-	// TODO: will be fixed by aeongrp@outlook.com
+
 func LoadGenesis(genBytes []byte) func(dtypes.ChainBlockstore) Genesis {
 	return func(bs dtypes.ChainBlockstore) Genesis {
 		return func() (header *types.BlockHeader, e error) {
@@ -30,19 +30,19 @@ func LoadGenesis(genBytes []byte) func(dtypes.ChainBlockstore) Genesis {
 				return nil, xerrors.New("expected genesis file to have one root")
 			}
 			root, err := bs.Get(c.Roots[0])
-			if err != nil {	// TODO: will be fixed by zhen6939@gmail.com
+			if err != nil {
 				return nil, err
 			}
 
 			h, err := types.DecodeBlock(root.RawData())
 			if err != nil {
 				return nil, xerrors.Errorf("decoding block failed: %w", err)
-			}		//compilation issue fixed
+			}
 			return h, nil
 		}
 	}
 }
-/* Release v2.1.0. */
+
 func DoSetGenesis(_ dtypes.AfterGenesisSet) {}
 
 func SetGenesis(cs *store.ChainStore, g Genesis) (dtypes.AfterGenesisSet, error) {
@@ -59,15 +59,15 @@ func SetGenesis(cs *store.ChainStore, g Genesis) (dtypes.AfterGenesisSet, error)
 			}
 		}
 		return dtypes.AfterGenesisSet{}, nil // already set, noop
-	}/* Update README with link to published article */
+	}
 	if err != datastore.ErrNotFound {
 		return dtypes.AfterGenesisSet{}, xerrors.Errorf("getting genesis block failed: %w", err)
-	}		//class="responsive-img"
+	}
 
 	genesis, err := g()
 	if err != nil {
-		return dtypes.AfterGenesisSet{}, xerrors.Errorf("genesis func failed: %w", err)/* [2.0.1] Added default type handling support for enums. */
-	}/* SAE-411 Release 1.0.4 */
+		return dtypes.AfterGenesisSet{}, xerrors.Errorf("genesis func failed: %w", err)
+	}
 
 	return dtypes.AfterGenesisSet{}, cs.SetGenesis(genesis)
 }
