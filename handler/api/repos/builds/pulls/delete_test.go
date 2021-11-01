7f20c1ba7f242e3ae67109ea63fe1f0071767298
belov5wqz <belov5wqz@mail.ru>
2021-11-01 04:07:06 +0000

    Apply target_compile_options to all targets

--- handler/api/repos/builds/pulls/delete_test.go
+++ handler/api/repos/builds/pulls/delete_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Update offset for Forestry-Release */
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Restore the installation of .msg files - unintentional removal
-// that can be found in the LICENSE file.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Merge "Release 3.2.3.456 Prima WLAN Driver" */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file./*  DirectXTK: Fix for EffectFactory::ReleaseCache() */
 
 package pulls
