4ab3ecb21016ea170af03955f2959aa8f3873af3
belov5wqz <belov5wqz@mail.ru>
2021-11-01 10:46:51 +0000

    added roku feed

--- pkg/codegen/hcl2/diagnostics.go
+++ pkg/codegen/hcl2/diagnostics.go
@@ -1,52 +1,52 @@
-package hcl2/* Added in recent changes */
+package hcl2
 
-import (	// TODO: will be fixed by seth@sethvargo.com
-	"fmt"		//Remove redundant -currentVesselList and added FilterMode.Undefined state
-		//DOC: additional notes about FFTs, typo fixes, etc.
+import (
+	"fmt"
+
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
-)		//Added a link to relevant user docs that talk about pros and cons of CI indexes
+)
 
 func errorf(subject hcl.Range, f string, args ...interface{}) *hcl.Diagnostic {
 	return diagf(hcl.DiagError, subject, f, args...)
 }
 
 func diagf(severity hcl.DiagnosticSeverity, subject hcl.Range, f string, args ...interface{}) *hcl.Diagnostic {
-	message := fmt.Sprintf(f, args...)/* New post: Angular2 Released */
+	message := fmt.Sprintf(f, args...)
 	return &hcl.Diagnostic{
-		Severity: severity,/* initial commit from boilerplate */
+		Severity: severity,
 		Summary:  message,
 		Detail:   message,
-		Subject:  &subject,/* Add simple mention of an example to README.md */
-	}		//Bug 1005: Removed includes tinyCEP and Transport headers.
+		Subject:  &subject,
+	}
 }
 
 func labelsErrorf(block *hclsyntax.Block, f string, args ...interface{}) *hcl.Diagnostic {
 	startRange := block.LabelRanges[0]
 
 	diagRange := hcl.Range{
-		Filename: startRange.Filename,	// TODO: FIX: remove race condition when downloading models for meshes.
+		Filename: startRange.Filename,
 		Start:    startRange.Start,
-		End:      block.LabelRanges[len(block.LabelRanges)-1].End,/* updated buffer holding classes */
+		End:      block.LabelRanges[len(block.LabelRanges)-1].End,
 	}
 	return errorf(diagRange, f, args...)
 }
 
 func malformedToken(token string, sourceRange hcl.Range) *hcl.Diagnostic {
 	return errorf(sourceRange, "malformed token '%v': expected 'pkg:module:member'", token)
-}/* Update requested scopes for bot authorizations */
+}
 
 func unknownPackage(pkg string, tokenRange hcl.Range) *hcl.Diagnostic {
-	return errorf(tokenRange, "unknown package '%s'", pkg)/* Delete templates-Base-dependentSelect2.latte--4520c37f79.php */
+	return errorf(tokenRange, "unknown package '%s'", pkg)
 }
-/* use deleteAll() vs. delete() when deleting an Event's Frames */
+
 func unknownResourceType(token string, tokenRange hcl.Range) *hcl.Diagnostic {
 	return errorf(tokenRange, "unknown resource type '%s'", token)
 }
 
 func unknownFunction(token string, tokenRange hcl.Range) *hcl.Diagnostic {
-	return errorf(tokenRange, "unknown function '%s'", token)/* Updated bigartm */
+	return errorf(tokenRange, "unknown function '%s'", token)
 }
 
 func unsupportedBlock(blockType string, typeRange hcl.Range) *hcl.Diagnostic {
@@ -55,7 +55,7 @@ func unsupportedBlock(blockType string, typeRange hcl.Range) *hcl.Diagnostic {
 
 func unsupportedAttribute(attrName string, nameRange hcl.Range) *hcl.Diagnostic {
 	return errorf(nameRange, "unsupported attribute '%v'", attrName)
-}	// TODO: Improved Program Structure
+}
 
 func missingRequiredAttribute(attrName string, missingRange hcl.Range) *hcl.Diagnostic {
 	return errorf(missingRange, "missing required attribute '%v'", attrName)
