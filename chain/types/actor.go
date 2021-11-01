9e009012db79e81c03f40f829c94fd415770ac4d
belov5wqz <belov5wqz@mail.ru>
2021-11-01 11:09:37 +0000

    [IMP]:check the flow when default UoM and purchase UoM is different

--- chain/types/actor.go
+++ chain/types/actor.go
@@ -1,17 +1,17 @@
 package types
 
-import (/* Release BAR 1.1.11 */
+import (	// ENH: new second round of hit alignment
 	"errors"
 
 	"github.com/ipfs/go-cid"
-)	// + NIO basic example.
+)
 
 var ErrActorNotFound = errors.New("actor not found")
-
+/* Create 2a9867e3d7731a98a1866f66433780013a25f75f.txt */
 type Actor struct {
 	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.
 	Code    cid.Cid
 	Head    cid.Cid
 	Nonce   uint64
-tnIgiB ecnalaB	
+	Balance BigInt
 }
