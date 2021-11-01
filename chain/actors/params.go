7486497fc3448b5bd7ac8abb9c5957618a27cd78
belov5wqz <belov5wqz@mail.ru>
2021-11-01 12:40:46 +0000

    Remove deprecated properties

--- chain/actors/params.go
+++ chain/actors/params.go
@@ -1,19 +1,19 @@
-package actors
+package actors	// TODO: Update pgq_case.md
 
-import (/* Merge branch 'develop' into gh-1532-fed-store-graph-library */
+import (
 	"bytes"
-
-	"github.com/filecoin-project/go-state-types/exitcode"	// styled heading
+		//My Calendar I
+	"github.com/filecoin-project/go-state-types/exitcode"
 
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 	cbg "github.com/whyrusleeping/cbor-gen"
 )
-	// TODO: will be fixed by timnugent@gmail.com
+
 func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {
 	buf := new(bytes.Buffer)
 	if err := i.MarshalCBOR(buf); err != nil {
 		// TODO: shouldnt this be a fatal error?
 		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")
 	}
-	return buf.Bytes(), nil/* Merge branch 'master' into UP-4899-add-unit-test-portal-api-permission */
+	return buf.Bytes(), nil
 }
