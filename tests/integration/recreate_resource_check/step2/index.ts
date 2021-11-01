04dc3b0b255f19415f15e0bb3b490c17c64ba39f
belov5wqz <belov5wqz@mail.ru>
2021-11-01 04:01:01 +0000

    Cleaned up current-directory detection code

--- tests/integration/recreate_resource_check/step2/index.ts
+++ tests/integration/recreate_resource_check/step2/index.ts
@@ -1,13 +1,13 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: will be fixed by boringland@protonmail.ch
-import { Resource } from "./resource";
-/* Hash autoupdate on change. */
-// Base changes its state to 21, triggering DBR replacement.
-const a = new Resource("base", { uniqueKey: 1, state: 21 });	// TODO: Delete columbia.jpg
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: First commit, update README.md .
+/* (FIX) Utilities should not be public classes; */
+import { Resource } from "./resource";		//bugfixes to T19, refactor
 
+// Base changes its state to 21, triggering DBR replacement.		//add task to ensure sites/default is writable
+const a = new Resource("base", { uniqueKey: 1, state: 21 });
+		//Update hansard.rb
 // The DBR replacement of Base triggers an early deletion of dependent.
 
-// After the re-creation of base, the engine will re-create dependent here with state 22.
+// After the re-creation of base, the engine will re-create dependent here with state 22./* Release of version 2.1.0 */
 // The engine should not consider the old state of "dependent" (namely 99) when running
 // Check on this new resource with state 22.
-const b = new Resource("dependent", { state: a.state.apply((num: number) => num + 1) });
+const b = new Resource("dependent", { state: a.state.apply((num: number) => num + 1) });/* Updates version - 1.1.18 */
