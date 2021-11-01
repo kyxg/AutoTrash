a95e9e365aa62d5bdf99d08a613578fcd1d876aa
belov5wqz <belov5wqz@mail.ru>
2021-11-01 06:42:26 +0000

    Add LGTM.com flags

--- store/logs/azureblob_oss.go
+++ store/logs/azureblob_oss.go
@@ -2,23 +2,23 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Version 3.17 Pre Release */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Release areca-7.2.3 */
+//	// TODO: will be fixed by vyzo@hackzen.org
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* ajp_*: move to ajp/ */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Release 0.34 */
 // limitations under the License.
 
 // +build oss
 
-package logs
+package logs	// Create 19884_retest2.txt
 
 import "github.com/drone/drone/core"
 
 // New returns a zero value LogStore.
 func NewAzureBlobEnv(containerName, storageAccountName, storageAccessKey string) core.LogStore {
-	return nil
-}		//Merge "Fix for 5155561 During export, progress bar jumps from 0 to 50%"
+	return nil		//9da38d8e-2e5b-11e5-9284-b827eb9e62be
+}		//Cognitive Complexity of methods should not be too high
