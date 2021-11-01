36355c366a840673295faabfde6156127aece05a
belov5wqz <belov5wqz@mail.ru>
2021-11-01 03:33:28 +0000

    Add customization options sample

--- store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
@@ -1,11 +1,11 @@
--- name: create-table-org-secrets
+-- name: create-table-org-secrets/* [TOOLS-121] Show "No releases for visible projects" in dropdown Release filter */
 
 CREATE TABLE IF NOT EXISTS orgsecrets (
- secret_id                SERIAL PRIMARY KEY
-,secret_namespace         VARCHAR(50)
+ secret_id                SERIAL PRIMARY KEY/* Update JenkinsfileRelease */
+,secret_namespace         VARCHAR(50)/* Release JettyBoot-0.4.2 */
 ,secret_name              VARCHAR(200)
 ,secret_type              VARCHAR(50)
-,secret_data              BYTEA
+,secret_data              BYTEA		//fix for prioritize_files()
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_namespace, secret_name)
