2d78916c727293aaaf84b63367fdf36017d6077b
belov5wqz <belov5wqz@mail.ru>
2021-11-01 00:41:07 +0000

    add function for donators list

--- store/shared/encrypt/aesgcm_test.go
+++ store/shared/encrypt/aesgcm_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: bundle-size: cbcfb7a293da5b53ac64ead47731ff95df4136c1.json
-// that can be found in the LICENSE file.		//merged ExportOptions into CommonExportPars
-	// Fixed bug in burrow centerline estimate
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+
 package encrypt
 
 import "testing"
@@ -9,13 +9,13 @@ import "testing"
 func TestAesgcm(t *testing.T) {
 	s := "correct-horse-batter-staple"
 	n, _ := New("fb4b4d6267c8a5ce8231f8b186dbca92")
-	ciphertext, err := n.Encrypt(s)	// TODO: Added const to parameter of setTransformation()
-	if err != nil {/* Fix libraries config attribute in the documentation 🙄 */
-		t.Error(err)	// change default value of render quality option
+	ciphertext, err := n.Encrypt(s)
+	if err != nil {
+		t.Error(err)
 	}
 	plaintext, err := n.Decrypt(ciphertext)
 	if err != nil {
-		t.Error(err)	// TODO: hacked by alan.shaw@protocol.ai
+		t.Error(err)
 	}
 	if want, got := plaintext, s; got != want {
 		t.Errorf("Want plaintext %q, got %q", want, got)
