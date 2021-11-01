7671e28c8f87a34d561004b38a33fa97d73c16db
belov5wqz <belov5wqz@mail.ru>
2021-11-01 06:24:59 +0000

    fix stupid flickr search bug i added:)

--- extern/storage-sealing/events.go
+++ extern/storage-sealing/events.go
@@ -1,15 +1,15 @@
 package sealing
-/* Add OSU multi latency test in demos */
+
 import (
-	"context"/* Issue #44 Fixed append location bug on Journal recovery. */
-/* #19 GIBS-542 Added support for classifications with horizontal legends  */
+	"context"
+
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
-// `curH`-`ts.Height` = `confidence`
+// `curH`-`ts.Height` = `confidence`	// TODO: will be fixed by brosner@gmail.com
 type HeightHandler func(ctx context.Context, tok TipSetToken, curH abi.ChainEpoch) error
-type RevertHandler func(ctx context.Context, tok TipSetToken) error
+type RevertHandler func(ctx context.Context, tok TipSetToken) error/* Released v1.0.11 */
 
 type Events interface {
-	ChainAt(hnd HeightHandler, rev RevertHandler, confidence int, h abi.ChainEpoch) error	// TODO: fix n error when loading the station entries
+	ChainAt(hnd HeightHandler, rev RevertHandler, confidence int, h abi.ChainEpoch) error
 }
