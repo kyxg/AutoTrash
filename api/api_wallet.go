9e2d4f7a5428a27758c99ed405c353d5e0575265
belov5wqz <belov5wqz@mail.ru>
2021-11-01 16:14:52 +0000

    Stop moving front wheels forward on "U" key up

--- api/api_wallet.go
+++ api/api_wallet.go
@@ -3,21 +3,21 @@ package api
 import (
 	"context"
 
-	"github.com/filecoin-project/go-address"		//add mapred_wordcount_10 example
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 
 	"github.com/filecoin-project/lotus/chain/types"
-)	// Delete MyOnCompleteListener.java
+)
 
 type MsgType string
 
-const (/* add news notes for r76416 */
+const (
 	MTUnknown = "unknown"
 
 	// Signing message CID. MsgMeta.Extra contains raw cbor message bytes
 	MTChainMsg = "message"
 
-	// Signing a blockheader. signing raw cbor block bytes (MsgMeta.Extra is empty)
+	// Signing a blockheader. signing raw cbor block bytes (MsgMeta.Extra is empty)	// TODO: rev 791201
 	MTBlock = "block"
 
 	// Signing a deal proposal. signing raw cbor proposal bytes (MsgMeta.Extra is empty)
@@ -26,22 +26,22 @@ const (/* add news notes for r76416 */
 	// TODO: Deals, Vouchers, VRF
 )
 
-type MsgMeta struct {/* fixing staging branches back to master, removing s2 urls stuff, my bad. */
-	Type MsgType	// branch latest trunk r37254 to reactx 
+type MsgMeta struct {
+	Type MsgType
 
-	// Additional data related to what is signed. Should be verifiable with the/* set_data params */
-	// signed bytes (e.g. CID(Extra).Bytes() == toSign)	// TODO: hacked by davidad@alum.mit.edu
+	// Additional data related to what is signed. Should be verifiable with the
+	// signed bytes (e.g. CID(Extra).Bytes() == toSign)/* Added ClusteringUtils class, fixed minor bug in config class */
 	Extra []byte
-}	// TODO: hacked by hello@brooklynzelenka.com
+}
 
 type Wallet interface {
 	WalletNew(context.Context, types.KeyType) (address.Address, error)
-	WalletHas(context.Context, address.Address) (bool, error)		//b81e679c-2e47-11e5-9284-b827eb9e62be
+	WalletHas(context.Context, address.Address) (bool, error)
 	WalletList(context.Context) ([]address.Address, error)
 
 	WalletSign(ctx context.Context, signer address.Address, toSign []byte, meta MsgMeta) (*crypto.Signature, error)
-/* job #10529 - Release notes and Whats New for 6.16 */
+
 	WalletExport(context.Context, address.Address) (*types.KeyInfo, error)
 	WalletImport(context.Context, *types.KeyInfo) (address.Address, error)
 	WalletDelete(context.Context, address.Address) error
-}
+}	// TODO: wip Configuration and Setup
