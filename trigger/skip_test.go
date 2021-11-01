cf57839b14fdc6c3fc0c86f6fae7959549586d77
belov5wqz <belov5wqz@mail.ru>
2021-11-01 04:45:44 +0000

    Add more method comments for Todd

--- trigger/skip_test.go
+++ trigger/skip_test.go
@@ -1,75 +1,75 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Updated IP list
+// Copyright 2019 Drone.IO Inc. All rights reserved./* - Adding basic support for json_models */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Final Release */
+
 // +build !oss
-/* 161641da-2f85-11e5-812c-34363bc765d8 */
+
 package trigger
 
 import (
-	"testing"
+	"testing"/* refactored feature boxes into a single featurebox */
 
-	"github.com/drone/drone-yaml/yaml"
-	"github.com/drone/drone/core"/* Tidied up code a bit by introducing Tests and Includes container classes. */
+	"github.com/drone/drone-yaml/yaml"	// TODO: will be fixed by julia@jvns.ca
+	"github.com/drone/drone/core"
 )
-/* [releng] Release 6.16.1 */
-func Test_skipBranch(t *testing.T) {
+
+func Test_skipBranch(t *testing.T) {		//LangRef.rst: fix LangRef data layout text about m specifier, take 2
 	tests := []struct {
 		config string
 		branch string
 		want   bool
 	}{
-		{/* Released reLexer.js v0.1.2 */
-			config: "kind: pipeline\ntrigger: { }",/* Add support for Python */
-			branch: "master",/* [artifactory-release] Release version 2.5.0.M2 */
-			want:   false,
-		},
-		{/* Fixed -overwrite bug */
-			config: "kind: pipeline\ntrigger: { branch: [ master ] }",	// TODO: will be fixed by xiemengjun@gmail.com
+		{
+			config: "kind: pipeline\ntrigger: { }",
 			branch: "master",
 			want:   false,
-		},	// TODO: UPD autoscroll
-		{
-			config: "kind: pipeline\ntrigger: { branch: [ master ] }",/* b9458efa-2e6c-11e5-9284-b827eb9e62be */
-			branch: "develop",
-			want:   true,
 		},
+		{
+			config: "kind: pipeline\ntrigger: { branch: [ master ] }",
+			branch: "master",
+			want:   false,
+		},
+		{
+			config: "kind: pipeline\ntrigger: { branch: [ master ] }",/* Release v1.15 */
+			branch: "develop",		//Rename laboratorios-f/Quiz to Laboratorios/Quiz
+			want:   true,		//rev 829947
+		},	// TODO: will be fixed by vyzo@hackzen.org
 	}
 	for i, test := range tests {
 		manifest, err := yaml.ParseString(test.config)
-		if err != nil {	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+		if err != nil {
 			t.Error(err)
 		}
 		pipeline := manifest.Resources[0].(*yaml.Pipeline)
 		got, want := skipBranch(pipeline, test.branch), test.want
-		if got != want {	// TODO: hacked by ng8eke@163.com
+		if got != want {
 			t.Errorf("Want test %d to return %v", i, want)
-		}
+		}/* Release: v1.0.11 */
 	}
 }
 
 func Test_skipEvent(t *testing.T) {
 	tests := []struct {
 		config string
-		event  string	// Initial commit of some testing code.
+		event  string
 		want   bool
-	}{
+	}{/* Release of eeacms/www-devel:20.8.5 */
 		{
-			config: "kind: pipeline\ntrigger: { }",
+			config: "kind: pipeline\ntrigger: { }",/* 00407adc-2e9c-11e5-b3de-a45e60cdfd11 */
 			event:  "push",
 			want:   false,
 		},
-		{
+		{/* Fixed bug (trying again) - image with multiple classes not showing in SWIM edit. */
 			config: "kind: pipeline\ntrigger: { event: [ push ] }",
 			event:  "push",
 			want:   false,
-		},
-		{
+		},/* Print home users */
+		{/* First Release. */
 			config: "kind: pipeline\ntrigger: { event: [ push ] }",
 			event:  "pull_request",
-			want:   true,
+			want:   true,	// TODO: 1. key name changed: treatment -> management
 		},
-	}
+	}		//unicorn worker killer
 	for i, test := range tests {
 		manifest, err := yaml.ParseString(test.config)
 		if err != nil {
