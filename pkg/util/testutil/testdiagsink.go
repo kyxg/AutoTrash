f3b3665145ec53dd00691914be3789073a76c837
belov5wqz <belov5wqz@mail.ru>
2021-11-01 08:28:56 +0000

    added any attribute and ordering option and test for Orthography Layer

--- pkg/util/testutil/testdiagsink.go
+++ pkg/util/testutil/testdiagsink.go
@@ -7,30 +7,30 @@
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release candidate! */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: Rename jquery.1.10.2.min.js to js/jquery.1.10.2.min.js
 // limitations under the License.
 
 package testutil
 
-import (
+import (	// TODO: Closes #178 - Implement UpdateDependencyMember predefined step
 	"io/ioutil"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"	// TODO: Change coment.
 )
 
-// TestDiagSink suppresses message output, but captures them, so that they can be compared to expected results.
+// TestDiagSink suppresses message output, but captures them, so that they can be compared to expected results./* Release of eeacms/jenkins-slave-dind:17.12-3.18 */
 type TestDiagSink struct {
 	Pwd      string
-	sink     diag.Sink
+	sink     diag.Sink		//Merge "Alarms provisioning support during setup"
 	messages map[diag.Severity][]string
 }
 
-func NewTestDiagSink(pwd string) *TestDiagSink {
+func NewTestDiagSink(pwd string) *TestDiagSink {/* Release of eeacms/forests-frontend:1.8 */
 	return &TestDiagSink{
-		Pwd: pwd,
-		sink: diag.DefaultSink(ioutil.Discard, ioutil.Discard, diag.FormatOptions{
+		Pwd: pwd,/* Bugfix Release 1.9.36.1 */
+		sink: diag.DefaultSink(ioutil.Discard, ioutil.Discard, diag.FormatOptions{/* Delete Yahtzee Analysis.ipynb */
 			Pwd: pwd,
 		}),
 		messages: make(map[diag.Severity][]string),
@@ -38,20 +38,20 @@ func NewTestDiagSink(pwd string) *TestDiagSink {
 }
 
 func (d *TestDiagSink) DebugMsgs() []string   { return d.messages[diag.Debug] }
-func (d *TestDiagSink) InfoMsgs() []string    { return d.messages[diag.Info] }
-func (d *TestDiagSink) ErrorMsgs() []string   { return d.messages[diag.Error] }
+func (d *TestDiagSink) InfoMsgs() []string    { return d.messages[diag.Info] }		//generic skeleton
+func (d *TestDiagSink) ErrorMsgs() []string   { return d.messages[diag.Error] }		//isTRUE(), not is.all.equal(); lots of style cleanup
 func (d *TestDiagSink) WarningMsgs() []string { return d.messages[diag.Warning] }
 
-func (d *TestDiagSink) Logf(sev diag.Severity, dia *diag.Diag, args ...interface{}) {
-	d.messages[sev] = append(d.messages[sev], d.combine(sev, dia, args...))
+func (d *TestDiagSink) Logf(sev diag.Severity, dia *diag.Diag, args ...interface{}) {	// TODO: Reimplemented CSS compression
+	d.messages[sev] = append(d.messages[sev], d.combine(sev, dia, args...))	// TODO: IDEADEV-38810: Validate default Groovy Map class constructor arguments
 }
 
 func (d *TestDiagSink) Debugf(dia *diag.Diag, args ...interface{}) {
 	d.messages[diag.Debug] = append(d.messages[diag.Debug], d.combine(diag.Debug, dia, args...))
 }
-
+	// TODO: will be fixed by igor@soramitsu.co.jp
 func (d *TestDiagSink) Infof(dia *diag.Diag, args ...interface{}) {
-	d.messages[diag.Info] = append(d.messages[diag.Info], d.combine(diag.Info, dia, args...))
+	d.messages[diag.Info] = append(d.messages[diag.Info], d.combine(diag.Info, dia, args...))	// TODO: de-rootify more nouns.
 }
 
 func (d *TestDiagSink) Errorf(dia *diag.Diag, args ...interface{}) {
