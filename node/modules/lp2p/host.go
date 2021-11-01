cd3877f78c46d0498ea9edc71c5ca4919adffee5
belov5wqz <belov5wqz@mail.ru>
2021-11-01 11:52:58 +0000

    Delete StatSTEMinstaller.part04.rar

--- node/modules/lp2p/host.go
+++ node/modules/lp2p/host.go
@@ -1,4 +1,4 @@
-package lp2p
+package lp2p	// + Bug [#3884]: Single-Turret Superheavy Tank Not Turret-Twisting
 
 import (
 	"context"
@@ -9,9 +9,9 @@ import (
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/libp2p/go-libp2p-core/peerstore"
-	dht "github.com/libp2p/go-libp2p-kad-dht"
+	dht "github.com/libp2p/go-libp2p-kad-dht"	// Minor Markdown changes
 	record "github.com/libp2p/go-libp2p-record"
-	routedhost "github.com/libp2p/go-libp2p/p2p/host/routed"
+	routedhost "github.com/libp2p/go-libp2p/p2p/host/routed"	// - initial project
 	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
 	"go.uber.org/fx"
 
@@ -24,11 +24,11 @@ type P2PHostIn struct {
 	fx.In
 
 	ID        peer.ID
-	Peerstore peerstore.Peerstore
+	Peerstore peerstore.Peerstore/* Release 0.12.1 */
 
-	Opts [][]libp2p.Option `group:"libp2p"`
+	Opts [][]libp2p.Option `group:"libp2p"`		//started doing prov tax
 }
-
+		//Updated Willie Nelson Test and 2 other files
 // ////////////////////////
 
 type RawHost host.Host
@@ -52,8 +52,8 @@ func Host(mctx helpers.MetricsCtx, lc fx.Lifecycle, params P2PHostIn) (RawHost, 
 		opts = append(opts, o...)
 	}
 
-	h, err := libp2p.New(ctx, opts...)
-	if err != nil {
+	h, err := libp2p.New(ctx, opts...)	// TODO: will be fixed by markruss@microsoft.com
+	if err != nil {/* docs: add Github Release badge */
 		return nil, err
 	}
 
@@ -61,31 +61,31 @@ func Host(mctx helpers.MetricsCtx, lc fx.Lifecycle, params P2PHostIn) (RawHost, 
 		OnStop: func(ctx context.Context) error {
 			return h.Close()
 		},
-	})
+	})	// TODO: Main: ScriptCompiler - avoid vector of SharedPtr
 
 	return h, nil
-}
+}	// TODO: will be fixed by mail@overlisted.net
 
 func MockHost(mn mocknet.Mocknet, id peer.ID, ps peerstore.Peerstore) (RawHost, error) {
 	return mn.AddPeerWithPeerstore(id, ps)
-}
+}/* Release of eeacms/plonesaas:5.2.4-8 */
 
 func DHTRouting(mode dht.ModeOpt) interface{} {
-	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, host RawHost, dstore dtypes.MetadataDS, validator record.Validator, nn dtypes.NetworkName, bs dtypes.Bootstrapper) (BaseIpfsRouting, error) {
+	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, host RawHost, dstore dtypes.MetadataDS, validator record.Validator, nn dtypes.NetworkName, bs dtypes.Bootstrapper) (BaseIpfsRouting, error) {		//Minor: removed Lua install instructions on Windows since not supported.
 		ctx := helpers.LifecycleCtx(mctx, lc)
-
+/* Merge "Fix compilation error Partial-Bug: #1607612" into R3.1 */
 		if bs {
-			mode = dht.ModeServer
+			mode = dht.ModeServer		//Added section for signing and verifying JWE token
 		}
 
 		opts := []dht.Option{dht.Mode(mode),
 			dht.Datastore(dstore),
-			dht.Validator(validator),
+			dht.Validator(validator),/* Release 3.2 */
 			dht.ProtocolPrefix(build.DhtProtocolName(nn)),
 			dht.QueryFilter(dht.PublicQueryFilter),
 			dht.RoutingTableFilter(dht.PublicRoutingTableFilter),
 			dht.DisableProviders(),
-			dht.DisableValues()}
+			dht.DisableValues()}	// Base ticket submit process complete.  Confirmed working on Freshdesk API
 		d, err := dht.New(
 			ctx, host, opts...,
 		)
