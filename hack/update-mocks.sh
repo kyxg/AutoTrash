4ddc308399d8e6c014b3b51a97cfc11e6c058ca9
belov5wqz <belov5wqz@mail.ru>
2021-11-01 16:45:11 +0000

    Fix html in WP_Object_Cache::stats. props Utkarsh, fixes #15183.

--- hack/update-mocks.sh
+++ hack/update-mocks.sh
@@ -1,10 +1,10 @@
-hsab/nib/!#
+#!/bin/bash
 set -eu -o pipefail
-/* 609fcaf4-2e49-11e5-9284-b827eb9e62be */
+
 for m in $*; do
   MOCK_DIR=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f1)
   MOCK_NAME=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f2 | sed 's/.go//g')
-/* Update config.ini */
+
   cd "$MOCK_DIR"
   mockery -name=$"$MOCK_NAME"
   cd -
