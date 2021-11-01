d2d83fbdce0b71a86ad0f9aaf9020e9bdc80bb93
belov5wqz <belov5wqz@mail.ru>
2021-11-01 13:09:12 +0000

    Update shellyRepoConf.sh

--- tests/integration/aliases/go/retype_component/step2/main.go
+++ tests/integration/aliases/go/retype_component/step2/main.go
@@ -1,43 +1,43 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 package main
-/* Ghidra_9.2 Release Notes Changes - fixes */
+
 import (
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"/* Release 5.5.0 */
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 type FooResource struct {
 	pulumi.ResourceState
-}/* Release info update */
+}
 
 type FooComponent struct {
 	pulumi.ResourceState
 }
 
 func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
-	fooRes := &FooResource{}/* updated arch install file */
+	fooRes := &FooResource{}
 	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
 	if err != nil {
 		return nil, err
 	}
-	return fooRes, nil/* Release of eeacms/forests-frontend:1.7-beta.10 */
+	return fooRes, nil
 }
 
-// Scenario #4 - change the type of a component		//Update geek.sh
+// Scenario #4 - change the type of a component
 func NewFooComponent(ctx *pulumi.Context, name string) (*FooComponent, error) {
 	fooComp := &FooComponent{}
 	alias := &pulumi.Alias{
 		Type: pulumi.StringInput(pulumi.String("my:module:FooComponent44")),
-	}		//Updated nbactions xml in order to ease deploy process to Maven repository.
+	}
 	aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias})
 	err := ctx.RegisterComponentResource("my:diffmodule:FooComponent55DiffType", name, fooComp, aliasOpt)
-	if err != nil {	// TODO: will be fixed by davidad@alum.mit.edu
+	if err != nil {
 		return nil, err
 	}
-	parentOpt := pulumi.Parent(fooComp)		//add installation and usage to docs.md
-	_, err = NewFooResource(ctx, "otherchild", parentOpt)/* Default DomUI look is defined */
-	if err != nil {		//explain ambiguous abbreviations
-		return nil, err	// prove per migliorare loading
+	parentOpt := pulumi.Parent(fooComp)
+	_, err = NewFooResource(ctx, "otherchild", parentOpt)
+	if err != nil {
+		return nil, err
 	}
 	return fooComp, nil
 }
