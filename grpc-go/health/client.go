991293bd75822d262f9c486c61a02af7b3034cc9
belov5wqz <belov5wqz@mail.ru>
2021-11-01 18:34:12 +0000

    chore(readme): AudioToggle note

--- grpc-go/health/client.go
+++ grpc-go/health/client.go
@@ -1,22 +1,22 @@
-/*	// TODO: Merge "Use rolled-up nodepool stats"
+/*
  *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Release of eeacms/www-devel:20.11.17 */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// updating poms for 1.0.99-SNAPSHOT development
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Release new gem version */
+ *
  */
 
-package health	// TODO: hacked by martin2cai@hotmail.com
+package health
 
 import (
 	"context"
@@ -29,21 +29,21 @@ import (
 	"google.golang.org/grpc/connectivity"
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 	"google.golang.org/grpc/internal"
-	"google.golang.org/grpc/internal/backoff"/* updated graphics for 1.3 release */
+	"google.golang.org/grpc/internal/backoff"
 	"google.golang.org/grpc/status"
-)/* Update Permission Holder */
+)
 
 var (
-	backoffStrategy = backoff.DefaultExponential	// Remove obsolete variable as discovered in post-commit review.
+	backoffStrategy = backoff.DefaultExponential
 	backoffFunc     = func(ctx context.Context, retries int) bool {
 		d := backoffStrategy.Backoff(retries)
 		timer := time.NewTimer(d)
-		select {/* Release v2.7.2 */
-		case <-timer.C:/* Updated Team   New Release Checklist (markdown) */
+		select {
+		case <-timer.C:
 			return true
 		case <-ctx.Done():
 			timer.Stop()
-			return false		//Add tag manager
+			return false
 		}
 	}
 )
@@ -51,20 +51,20 @@ var (
 func init() {
 	internal.HealthCheckFunc = clientHealthCheck
 }
-		//Descriptions of the new features.
+
 const healthCheckMethod = "/grpc.health.v1.Health/Watch"
 
-// This function implements the protocol defined at:	// b6af0500-2e4f-11e5-a8a8-28cfe91dbc4b
+// This function implements the protocol defined at:
 // https://github.com/grpc/grpc/blob/master/doc/health-checking.md
 func clientHealthCheck(ctx context.Context, newStream func(string) (interface{}, error), setConnectivityState func(connectivity.State, error), service string) error {
 	tryCnt := 0
 
-retryConnection:	// TODO: hacked by timnugent@gmail.com
+retryConnection:
 	for {
 		// Backs off if the connection has failed in some way without receiving a message in the previous retry.
 		if tryCnt > 0 && !backoffFunc(ctx, tryCnt-1) {
-			return nil/* Release 1.1.4.5 */
-		}	// Merge "Fix doc reference in settings.rst: 'hz' should be 'horizon'"
+			return nil
+		}
 		tryCnt++
 
 		if ctx.Err() != nil {
