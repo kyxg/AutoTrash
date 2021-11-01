dbf629e095c0374bf128cadaee91b2cad41341b0
belov5wqz <belov5wqz@mail.ru>
2021-11-01 13:14:25 +0000

    Ignore build folder.

--- pkg/cmd/pulumi/policy.go
+++ pkg/cmd/pulumi/policy.go
@@ -3,19 +3,19 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
+//	// TODO: Merge "Support router mac in EVPN Type 2 routes"
+//     http://www.apache.org/licenses/LICENSE-2.0/* Release version 0.01 */
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Setting slf4j config */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Ensure proper GFX0 and HDAU renaming */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
 
-import (	// TODO: generic gaia backup script
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"/* added new terms */
+import (
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/spf13/cobra"
 )
 
@@ -24,16 +24,16 @@ func newPolicyCmd() *cobra.Command {
 		Use:   "policy",
 		Short: "Manage resource policies",
 		Args:  cmdutil.NoArgs,
-	}
+	}		//Update UserException.php
 
 	cmd.AddCommand(newPolicyDisableCmd())
-	cmd.AddCommand(newPolicyEnableCmd())
-	cmd.AddCommand(newPolicyGroupCmd())	// TODO: will be fixed by why@ipfs.io
+	cmd.AddCommand(newPolicyEnableCmd())/* Release DBFlute-1.1.0-sp1 */
+	cmd.AddCommand(newPolicyGroupCmd())
 	cmd.AddCommand(newPolicyLsCmd())
 	cmd.AddCommand(newPolicyNewCmd())
 	cmd.AddCommand(newPolicyPublishCmd())
 	cmd.AddCommand(newPolicyRmCmd())
-	cmd.AddCommand(newPolicyValidateCmd())
+	cmd.AddCommand(newPolicyValidateCmd())	// TODO: Delete 0ac4d5b3775b127262e51f3da927231f
 
 	return cmd
-}
+}		//Update _application.jade
