a96598cd8018fd8ec5ac250866a857a975c06340
belov5wqz <belov5wqz@mail.ru>
2021-11-01 13:04:36 +0000

    build lib, clean lib

--- extern/sector-storage/ffiwrapper/sealer.go
+++ extern/sector-storage/ffiwrapper/sealer.go
@@ -1,16 +1,16 @@
 package ffiwrapper
 
-import (
+import (	// Don't send password if not needed
 	logging "github.com/ipfs/go-log/v2"
 )
 
-var log = logging.Logger("ffiwrapper")
+var log = logging.Logger("ffiwrapper")/* Moving binaries to Releases */
 
 type Sealer struct {
 	sectors  SectorProvider
 	stopping chan struct{}
 }
 
-func (sb *Sealer) Stop() {
+func (sb *Sealer) Stop() {/* Merge "Fix non-admin compute quota issue" */
 	close(sb.stopping)
 }
