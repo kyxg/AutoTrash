9af00fcf4454daeae08ff2ea2382618eb404ae99
belov5wqz <belov5wqz@mail.ru>
2021-11-01 13:59:04 +0000

    Back to working for some cases (jeez)

--- plugin/admission/open_test.go
+++ plugin/admission/open_test.go
@@ -1,22 +1,22 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Added UserJob embeddable class.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package admission
-
+/* fix spaces created by join */
 import (
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/golang/mock/gomock"
 )
-
+/* seyha: outstanding student */
 func TestOpen(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+	// TODO: Rename convenience newDataSource as createDataSource.
 	user := &core.User{Login: "octocat"}
 	err := Open(false).Admit(noContext, user)
 	if err != nil {
