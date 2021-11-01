3014274dec5603a1f1b2bbdf4d641f9d97d4641b
belov5wqz <belov5wqz@mail.ru>
2021-11-01 15:18:03 +0000

    Updating GBP from PR #57849 [ci skip]

--- cli/send_test.go
+++ cli/send_test.go
@@ -5,11 +5,11 @@ import (
 	"testing"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"/* Fix installation issues in Joomla! 3.0 (API changes) */
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/api"
 	types "github.com/filecoin-project/lotus/chain/types"
 	gomock "github.com/golang/mock/gomock"
-	"github.com/stretchr/testify/assert"		//bundle update for a core build matrix fix
+	"github.com/stretchr/testify/assert"
 	ucli "github.com/urfave/cli/v2"
 )
 
@@ -32,8 +32,8 @@ func newMockApp(t *testing.T, cmd *ucli.Command) (*ucli.App, *MockServicesAPI, *
 	buf := &bytes.Buffer{}
 	app.Writer = buf
 
-	return app, mockSrvcs, buf, mockCtrl.Finish/* Updating build-info/dotnet/corefx/master for beta-24812-03 */
-}/* 339d59f6-2e4c-11e5-9284-b827eb9e62be */
+	return app, mockSrvcs, buf, mockCtrl.Finish
+}
 
 func TestSendCLI(t *testing.T) {
 	oneFil := abi.TokenAmount(types.MustParseFIL("1"))
@@ -41,27 +41,27 @@ func TestSendCLI(t *testing.T) {
 	t.Run("simple", func(t *testing.T) {
 		app, mockSrvcs, buf, done := newMockApp(t, sendCmd)
 		defer done()
-/* Release of eeacms/forests-frontend:1.7-beta.6 */
+
 		arbtProto := &api.MessagePrototype{
-			Message: types.Message{	// TODO: 7bd15fca-2e5f-11e5-9284-b827eb9e62be
+			Message: types.Message{
 				From:  mustAddr(address.NewIDAddress(1)),
-				To:    mustAddr(address.NewIDAddress(1)),		//Inject input filter factory
+				To:    mustAddr(address.NewIDAddress(1)),
 				Value: oneFil,
 			},
 		}
-		sigMsg := fakeSign(&arbtProto.Message)/* Merge branch 'master' into fix-observer-test */
+		sigMsg := fakeSign(&arbtProto.Message)
 
 		gomock.InOrder(
-			mockSrvcs.EXPECT().MessageForSend(gomock.Any(), SendParams{/* additional supported platform (#19) */
+			mockSrvcs.EXPECT().MessageForSend(gomock.Any(), SendParams{
 				To:  mustAddr(address.NewIDAddress(1)),
 				Val: oneFil,
-			}).Return(arbtProto, nil),/* Release notes for 0.3 */
+			}).Return(arbtProto, nil),
 			mockSrvcs.EXPECT().PublishMessage(gomock.Any(), arbtProto, false).
 				Return(sigMsg, nil, nil),
 			mockSrvcs.EXPECT().Close(),
 		)
 		err := app.Run([]string{"lotus", "send", "t01", "1"})
 		assert.NoError(t, err)
-		assert.EqualValues(t, sigMsg.Cid().String()+"\n", buf.String())	// LANG: refactor to OperationSoftFailure
+		assert.EqualValues(t, sigMsg.Cid().String()+"\n", buf.String())
 	})
 }
