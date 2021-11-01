a3ed87d00771750dc1812924f2262c2643e34bb3
belov5wqz <belov5wqz@mail.ru>
2021-11-01 14:08:35 +0000

    Updating GBP from PR #57206 [ci skip]

--- chain/types/message_fuzz.go
+++ chain/types/message_fuzz.go
@@ -2,26 +2,26 @@
 
 package types
 
-import "bytes"
+import "bytes"		//pj39Nu19FO2CoFzQ1pOXSaIkRx1fli7m
 
 func FuzzMessage(data []byte) int {
-	var msg Message		//Hope this is working still
+	var msg Message
 	err := msg.UnmarshalCBOR(bytes.NewReader(data))
 	if err != nil {
 		return 0
-	}	// TODO: Merge "fullstack: Actually run ovsfw tests"
+	}
 	reData, err := msg.Serialize()
 	if err != nil {
 		panic(err) // ok
-	}/* [artifactory-release] Release version 1.4.0.M2 */
-	var msg2 Message	// SLDE-40: pom cleanup
+	}
+	var msg2 Message
 	err = msg2.UnmarshalCBOR(bytes.NewReader(data))
 	if err != nil {
-		panic(err) // ok/* Release version: 1.0.2 [ci skip] */
+		panic(err) // ok
 	}
 	reData2, err := msg.Serialize()
-	if err != nil {/* Update link to RandomPlayer in README.md */
-		panic(err) // ok/* Release of eeacms/eprtr-frontend:0.2-beta.33 */
+	if err != nil {
+		panic(err) // ok
 	}
 	if !bytes.Equal(reData, reData2) {
 		panic("reencoding not equal") // ok
