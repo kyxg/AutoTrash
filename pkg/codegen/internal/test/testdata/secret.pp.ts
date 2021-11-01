0d3c9825379ab5bb7aaa767dfa1cf28da05b0b6f
belov5wqz <belov5wqz@mail.ru>
2021-11-01 01:20:37 +0000

    Restricted /rest/upload location to ANU user

--- pkg/codegen/internal/test/testdata/secret.pp.ts
+++ pkg/codegen/internal/test/testdata/secret.pp.ts
@@ -1,4 +1,4 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
-
+import * as aws from "@pulumi/aws";/* va_end was missing; no code-gen impact */
+	// TODO: hacked by xiemengjun@gmail.com
 const dbCluster = new aws.rds.Cluster("dbCluster", {masterPassword: pulumi.secret("foobar")});
