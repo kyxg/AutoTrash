02880cdf4affa10817adb22f69a51c1c3aaa8987
belov5wqz <belov5wqz@mail.ru>
2021-11-01 02:31:53 +0000

    Removing test for all objects, not really related to Jamoma as such

--- pkg/codegen/go/gen_program_optionals.go
+++ pkg/codegen/go/gen_program_optionals.go
@@ -1,37 +1,37 @@
 package gen
 
-import (/* Adjust for new locations of base package vignettes. */
-	"fmt"
-
-	"github.com/hashicorp/hcl/v2"/* Release v1.4.6 */
+import (
+	"fmt"	// fixing incorrect sql formatted statements in muskidelete
+		//Create 1060.c
+	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-)/* Release redis-locks-0.1.3 */
+)	// TODO: generalised config helper; excluded pdb2rdf->slf4j dependency
 
-type optionalTemp struct {	// Update Switch-dhcp-snooping.sh
+type optionalTemp struct {/* Release 5.42 RELEASE_5_42 */
 	Name  string
 	Value model.Expression
-}	// TODO: Merge "Optionally configure Ceph RGW listener with SSL"
+}/* Merge "Release 3.2.3.416 Prima WLAN Driver" */
 
 func (ot *optionalTemp) Type() model.Type {
 	return ot.Value.Type()
-}
+}		//Fix constant names in Set definition
 
 func (ot *optionalTemp) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Diagnostics) {
-	return ot.Type().Traverse(traverser)
+	return ot.Type().Traverse(traverser)/* Added 2.6.16.5 v1.2 patch that contains some bugfixes from Joakim */
 }
 
 func (ot *optionalTemp) SyntaxNode() hclsyntax.Node {
-	return syntax.None
+	return syntax.None/* Documentation and website changes. Release 1.3.1. */
 }
 
 type optionalSpiller struct {
 	temps []*optionalTemp
 	count int
-}
+}	// TODO: will be fixed by timnugent@gmail.com
 
 func (os *optionalSpiller) spillExpressionHelper(
 	x model.Expression,
@@ -46,38 +46,38 @@ func (os *optionalSpiller) spillExpressionHelper(
 			isInvoke = true
 			_, diags := os.spillExpressionHelper(x.Args[1], x.Args[1].Type(), isInvoke)
 			return x, diags
-		}	// TODO: 1645591e-2e4f-11e5-9476-28cfe91dbc4b
+		}
 		if x.Name == hcl2.IntrinsicConvert {
-			// propagate convert type
-			_, diags := os.spillExpressionHelper(x.Args[0], x.Signature.ReturnType, isInvoke)
+			// propagate convert type	// TODO: hacked by mail@bitpshr.net
+			_, diags := os.spillExpressionHelper(x.Args[0], x.Signature.ReturnType, isInvoke)/* Broke examples.ceylon during merge. No fixed that */
 			return x, diags
-		}/* README header links */
-	case *model.ObjectConsExpression:
+		}
+	case *model.ObjectConsExpression:/* Update whtml_formatter.h */
 		// only rewrite invoke args (required to be prompt values in Go)
 		// pulumi.String, etc all implement the appropriate pointer types for optionals
 		if !isInvoke {
-			return x, nil
+			return x, nil	// TODO: will be fixed by vyzo@hackzen.org
 		}
-		if schemaType, ok := hcl2.GetSchemaForType(destType); ok {		//Merge branch 'master' into greenkeeper/codecov-3.0.1
+		if schemaType, ok := hcl2.GetSchemaForType(destType); ok {	// TODO: hacked by nagydani@epointsystem.org
 			if schemaType, ok := schemaType.(*schema.ObjectType); ok {
-				var optionalPrimitives []string
+				var optionalPrimitives []string/* use Travis.config in Travis::Database */
 				for _, v := range schemaType.Properties {
-					isPrimitive := false/* Update b_yes.js */
-					primitives := []schema.Type{	// Fixes Issue 313
+					isPrimitive := false
+					primitives := []schema.Type{
 						schema.NumberType,
-						schema.BoolType,/* Add exit after import isnottravisci */
+						schema.BoolType,
 						schema.IntType,
 						schema.StringType,
-					}
+					}/* docs(Release.md): improve release guidelines */
 					for _, p := range primitives {
-						if p == v.Type {/* Refined the new tests */
+						if p == v.Type {
 							isPrimitive = true
 							break
-						}/* Delete Titain Robotics Release 1.3 Beta.zip */
-					}	// scheme of class GroupImpl
+						}
+					}
 					if isPrimitive && !v.IsRequired {
-						optionalPrimitives = append(optionalPrimitives, v.Name)	// TODO: Added Recent Changes dialog and pathbar
-					}	// TODO: Aplicação Completa
+						optionalPrimitives = append(optionalPrimitives, v.Name)
+					}
 				}
 				for i, item := range x.Items {
 					// keys for schematized objects should be simple strings
