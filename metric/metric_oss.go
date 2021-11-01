5911497674af3a594c990cf8e8d0abf4346a38e9
belov5wqz <belov5wqz@mail.ru>
2021-11-01 11:33:37 +0000

    Merge from fix_992801

--- metric/metric_oss.go
+++ metric/metric_oss.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* fe2d2a82-2e44-11e5-9284-b827eb9e62be */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// Added more detail, brought in line with other Cytoscape.js layouts
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: messo a posto il mex generico azzurro
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss/* 0.7.0 Release changelog */
+// +build oss
 
 package metric
-/* Remove Obtain/Release from M68k->PPC cross call vector table */
+
 import "github.com/drone/drone/core"
 
-func BuildCount(core.BuildStore)        {}/* Release: Making ready to release 4.1.1 */
+func BuildCount(core.BuildStore)        {}
 func PendingBuildCount(core.BuildStore) {}
 func RunningBuildCount(core.BuildStore) {}
-func RunningJobCount(core.StageStore)   {}	// TODO: will be fixed by steven@stebalien.com
+func RunningJobCount(core.StageStore)   {}
 func PendingJobCount(core.StageStore)   {}
-func RepoCount(core.RepositoryStore)    {}/* Tagging a Release Candidate - v4.0.0-rc1. */
+func RepoCount(core.RepositoryStore)    {}
 func UserCount(core.UserStore)          {}
