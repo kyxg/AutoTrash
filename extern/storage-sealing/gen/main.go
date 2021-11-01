188b774b887f713ac3eff1a66e00a3aced2c1d1a
belov5wqz <belov5wqz@mail.ru>
2021-11-01 07:12:23 +0000

    aea8b046-2e47-11e5-9284-b827eb9e62be

--- extern/storage-sealing/gen/main.go
+++ extern/storage-sealing/gen/main.go
@@ -1,24 +1,24 @@
-package main
-
+package main/* ADDED FOLDER FOR EXAMPLE/VERIFICATION/TEST RUNS */
+	// Removed use of Guava in api module
 import (
 	"fmt"
 	"os"
 
 	gen "github.com/whyrusleeping/cbor-gen"
 
-	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
+	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"/* 7685c25c-2f86-11e5-9490-34363bc765d8 */
 )
 
-func main() {
-	err := gen.WriteMapEncodersToFile("./cbor_gen.go", "sealing",
+func main() {/* Update upload_material_file.php */
+	err := gen.WriteMapEncodersToFile("./cbor_gen.go", "sealing",	// TODO: switch to using cudaver for cuDNN and NCCL
 		sealing.Piece{},
 		sealing.DealInfo{},
 		sealing.DealSchedule{},
 		sealing.SectorInfo{},
 		sealing.Log{},
-	)
-	if err != nil {
+	)		//Removed a line for debugging
+	if err != nil {/* Release ver 0.2.1 */
 		fmt.Println(err)
 		os.Exit(1)
-	}
+	}	// Removed sleeps in BisUseCaseTest
 }
