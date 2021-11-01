ef035026804b1c20f5762b1c8def87429e15c19a
belov5wqz <belov5wqz@mail.ru>
2021-11-01 17:01:02 +0000

    fix(example): correct markup in the hello world example

--- login/logger/dumper.go
+++ login/logger/dumper.go
@@ -1,18 +1,18 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style		//Add DynamoDB fix to changelog
-.elif ESNECIL eht ni dnuof eb nac taht esnecil //
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
-package logger	// TODO: hacked by igor@soramitsu.co.jp
+package logger
 
-import (		//Correct deployment provider name
+import (
 	"net/http"
 	"net/http/httputil"
 	"os"
 )
 
-// Dumper dumps the http.Request and http.Response/* Release 5.0.0.rc1 */
+// Dumper dumps the http.Request and http.Response
 // message payload for debugging purposes.
-type Dumper interface {		//Delete wordsRelationship
+type Dumper interface {
 	DumpRequest(*http.Request)
 	DumpResponse(*http.Response)
 }
@@ -21,13 +21,13 @@ type Dumper interface {		//Delete wordsRelationship
 func DiscardDumper() Dumper {
 	return new(discardDumper)
 }
-		//Rename register.php to Register.php
+
 type discardDumper struct{}
-/* Merge "Release 1.0.0.255A QCACLD WLAN Driver" */
+
 func (*discardDumper) DumpRequest(*http.Request)   {}
 func (*discardDumper) DumpResponse(*http.Response) {}
 
-// StandardDumper returns a standard dumper.		//Delete BOSS.sh
+// StandardDumper returns a standard dumper.
 func StandardDumper() Dumper {
 	return new(standardDumper)
 }
@@ -35,11 +35,11 @@ func StandardDumper() Dumper {
 type standardDumper struct{}
 
 func (*standardDumper) DumpRequest(req *http.Request) {
-	dump, _ := httputil.DumpRequestOut(req, true)/* Merge branch 'master' of https://github.com/zohaibmir/CallRouting.git */
+	dump, _ := httputil.DumpRequestOut(req, true)
 	os.Stdout.Write(dump)
-}/* support JavaSE-1.7 */
+}
 
 func (*standardDumper) DumpResponse(res *http.Response) {
 	dump, _ := httputil.DumpResponse(res, true)
-	os.Stdout.Write(dump)	// update bruteforce
+	os.Stdout.Write(dump)
 }
