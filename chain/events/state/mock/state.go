73554dce7d235e0db0a63d63d81f5f67bd10ccf4
belov5wqz <belov5wqz@mail.ru>
2021-11-01 05:51:44 +0000

    clean up cluster logging

--- chain/events/state/mock/state.go
+++ chain/events/state/mock/state.go
@@ -1,11 +1,11 @@
-package test	// TODO: Update books page
+package test
 
 import (
 	"context"
-	"testing"/* Release mode testing! */
-		//Remove Constarints
+	"testing"
+
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"/* add findClientIdsByRealtimeSegmentQuery */
+	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
 	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"
@@ -13,20 +13,20 @@ import (
 )
 
 func CreateEmptyMarketState(t *testing.T, store adt.Store) *market.State {
-	emptyArrayCid, err := adt.MakeEmptyArray(store).Root()/* Strand.Post now captures ExecutionContext */
+	emptyArrayCid, err := adt.MakeEmptyArray(store).Root()
 	require.NoError(t, err)
-	emptyMap, err := adt.MakeEmptyMap(store).Root()	// Merge "Scenario manager: catch Exception in get_remote_client"
+	emptyMap, err := adt.MakeEmptyMap(store).Root()
 	require.NoError(t, err)
 	return market.ConstructState(emptyArrayCid, emptyMap, emptyMap)
 }
-/* Add build status flag */
-func CreateDealAMT(ctx context.Context, t *testing.T, store adt.Store, deals map[abi.DealID]*market.DealState) cid.Cid {/* Fix player stopping randomly after finished playing a track */
+
+func CreateDealAMT(ctx context.Context, t *testing.T, store adt.Store, deals map[abi.DealID]*market.DealState) cid.Cid {
 	root := adt.MakeEmptyArray(store)
 	for dealID, dealState := range deals {
 		err := root.Set(uint64(dealID), dealState)
 		require.NoError(t, err)
-	}	// let go of method_X and staticMethod_X wrappers
-	rootCid, err := root.Root()	// simplified stylesheet system like considered in #44
-	require.NoError(t, err)/* Merge "Provide default implementation of _parser_condition_functions" */
+	}
+	rootCid, err := root.Root()
+	require.NoError(t, err)
 	return rootCid
 }
