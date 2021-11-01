81a38ca777929b7244c6197c22931ef91c5fc24f
belov5wqz <belov5wqz@mail.ru>
2021-11-01 08:16:37 +0000

    don't leak concrete class of singleton iterator

--- core/netrc_test.go
+++ core/netrc_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License		//add link for back button in edit user view
+// that can be found in the LICENSE file.	// TODO: will be fixed by timnugent@gmail.com
 
 // +build !oss
 
