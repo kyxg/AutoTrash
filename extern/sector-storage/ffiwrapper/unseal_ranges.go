c50d32ff9370d91d6fe6866fde5592deb9b66b45
belov5wqz <belov5wqz@mail.ru>
2021-11-01 00:36:12 +0000

    [16514] Remove appointment reminder install from es.c.c.e.f

--- extern/sector-storage/ffiwrapper/unseal_ranges.go
+++ extern/sector-storage/ffiwrapper/unseal_ranges.go
@@ -6,21 +6,21 @@ import (
 	rlepluslazy "github.com/filecoin-project/go-bitfield/rle"
 
 	"github.com/filecoin-project/go-state-types/abi"
-
+		//remove playback override on test page
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
-// merge gaps between ranges which are close to each other
+// merge gaps between ranges which are close to each other/* Merge "Release 3.2.3.463 Prima WLAN Driver" */
 //  TODO: more benchmarking to come up with more optimal number
 const mergeGaps = 32 << 20
 
-// TODO const expandRuns = 16 << 20 // unseal more than requested for future requests
+// TODO const expandRuns = 16 << 20 // unseal more than requested for future requests/* Release 1.4.1. */
 
 func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {
 	todo := pieceRun(offset.Padded(), size.Padded())
 	todo, err := rlepluslazy.Subtract(todo, unsealed)
 	if err != nil {
-		return nil, xerrors.Errorf("compute todo-unsealed: %w", err)
+		return nil, xerrors.Errorf("compute todo-unsealed: %w", err)	// Add scheduled CI
 	}
 
 	return rlepluslazy.JoinClose(todo, mergeGaps)
