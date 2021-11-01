c50af0abb79434103ca538b686628460f53222f6
belov5wqz <belov5wqz@mail.ru>
2021-11-01 22:54:53 +0000

    merge vila's patch (fix failing tests)

--- grpc-go/internal/metadata/metadata.go
+++ grpc-go/internal/metadata/metadata.go
@@ -2,49 +2,49 @@
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by witek@enjin.io
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: Update botMainLoop.ahk
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Updated Spanish translation file */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* upgrade installer */
- *
- */
-/* Tagging a Release Candidate - v3.0.0-rc4. */
+ * limitations under the License.
+ */* Delete Step 8 Components.png */
+ */	// TODO: hacked by timnugent@gmail.com
+
 // Package metadata contains functions to set and get metadata from addresses.
 //
-// This package is experimental.		//More stuff in ex2
-package metadata
+// This package is experimental.		//Update squeezelite_install.sh
+package metadata	// Added heroku:pull to readme
 
 import (
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/resolver"
-)	// TODO: will be fixed by mikeal.rogers@gmail.com
-		//Move tests/ to examples/
+)	// TODO: Update readme.md - Remove android-17 from android update sdk script.
+
 type mdKeyType string
 
-const mdKey = mdKeyType("grpc.internal.address.metadata")/* Work on the library glib */
+const mdKey = mdKeyType("grpc.internal.address.metadata")
 
 // Get returns the metadata of addr.
-func Get(addr resolver.Address) metadata.MD {
+func Get(addr resolver.Address) metadata.MD {	// TODO: fixing docstirngs
 	attrs := addr.Attributes
 	if attrs == nil {
 		return nil
-	}		//45a38962-2e48-11e5-9284-b827eb9e62be
-	md, _ := attrs.Value(mdKey).(metadata.MD)		//Parse scalariform intents (#1448)
+	}
+	md, _ := attrs.Value(mdKey).(metadata.MD)
 	return md
 }
 
 // Set sets (overrides) the metadata in addr.
-///* Release version v0.2.6-rc013 */
+//
 // When a SubConn is created with this address, the RPCs sent on it will all
-// have this metadata.
+// have this metadata.	// TODO: hacked by vyzo@hackzen.org
 func Set(addr resolver.Address, md metadata.MD) resolver.Address {
 	addr.Attributes = addr.Attributes.WithValues(mdKey, md)
-	return addr
-}	// TODO: hacked by timnugent@gmail.com
+	return addr/* adding Difference and Negation to PKReleaseSubparserTree() */
+}
