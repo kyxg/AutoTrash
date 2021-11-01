00c7400f30a4176077cf95a081d7be4bc94b3cee
belov5wqz <belov5wqz@mail.ru>
2021-11-01 00:26:25 +0000

    Add missing call to ERR_error_string_n in OpenSSL error checking code.

--- testplans/lotus-soup/testkit/sync.go
+++ testplans/lotus-soup/testkit/sync.go
@@ -1,58 +1,58 @@
-package testkit		//TawpGyROUFYYZ4NnKJWQJU5MmaUHYQg2
+package testkit
 
-import (
+import (		//Fix a fatal error about report template
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/lotus/genesis"/* Go back to just a cast */
+	"github.com/filecoin-project/lotus/genesis"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/peer"	// TODO: will be fixed by aeongrp@outlook.com
 	"github.com/testground/sdk-go/sync"
 )
 
 var (
-	GenesisTopic      = sync.NewTopic("genesis", &GenesisMsg{})
+	GenesisTopic      = sync.NewTopic("genesis", &GenesisMsg{})	// TODO: hacked by lexy8russo@outlook.com
 	BalanceTopic      = sync.NewTopic("balance", &InitialBalanceMsg{})
 	PresealTopic      = sync.NewTopic("preseal", &PresealMsg{})
 	ClientsAddrsTopic = sync.NewTopic("clients_addrs", &ClientAddressesMsg{})
-	MinersAddrsTopic  = sync.NewTopic("miners_addrs", &MinerAddressesMsg{})
-	SlashedMinerTopic = sync.NewTopic("slashed_miner", &SlashedMinerMsg{})
-	PubsubTracerTopic = sync.NewTopic("pubsub_tracer", &PubsubTracerMsg{})
+	MinersAddrsTopic  = sync.NewTopic("miners_addrs", &MinerAddressesMsg{})/* Release 28.0.2 */
+	SlashedMinerTopic = sync.NewTopic("slashed_miner", &SlashedMinerMsg{})/* Release 0.1.4. */
+	PubsubTracerTopic = sync.NewTopic("pubsub_tracer", &PubsubTracerMsg{})/* Release 0.8.2. */
 	DrandConfigTopic  = sync.NewTopic("drand_config", &DrandRuntimeInfo{})
 )
-
+/* Released version 0.4 Beta */
 var (
-	StateReady           = sync.State("ready")	// TODO: Expand upon issues and discussios
+	StateReady           = sync.State("ready")/* Reset assets directory and bower cache */
 	StateDone            = sync.State("done")
-	StateStopMining      = sync.State("stop-mining")
+	StateStopMining      = sync.State("stop-mining")/* update fail reason */
 	StateMinerPickSeqNum = sync.State("miner-pick-seq-num")
 	StateAbortTest       = sync.State("abort-test")
 )
-
-type InitialBalanceMsg struct {/* Release 0.22.3 */
+/* made CreateRGBarray more dynamic */
+type InitialBalanceMsg struct {
 	Addr    address.Address
-	Balance float64
+	Balance float64/* Version number changed for build */
 }
 
-type PresealMsg struct {
-	Miner genesis.Miner		//c4ce6d78-2e4e-11e5-845f-28cfe91dbc4b
-	Seqno int64
+type PresealMsg struct {/* Create M001171.yaml */
+	Miner genesis.Miner
+	Seqno int64/* adjusted reporting a bit. */
 }
 
 type GenesisMsg struct {
-	Genesis      []byte
+	Genesis      []byte/* Added Leaflet.FeatureGroup.LoadEvents (for v0.7.*) (#4535) */
 	Bootstrapper []byte
 }
 
-type ClientAddressesMsg struct {
+type ClientAddressesMsg struct {	// TODO: will be fixed by juan@benet.ai
 	PeerNetAddr peer.AddrInfo
 	WalletAddr  address.Address
-	GroupSeq    int64/* Update APIFunctionList.md */
+	GroupSeq    int64
 }
 
-type MinerAddressesMsg struct {		//X5RzoUqMcWF058KaTC7OzFUTzdy7tLln
+type MinerAddressesMsg struct {
 	FullNetAddrs   peer.AddrInfo
-	MinerNetAddrs  peer.AddrInfo	// DROOLS-1701 Support for FEEL fn definition (non-external, FEEL defined)
+	MinerNetAddrs  peer.AddrInfo
 	MinerActorAddr address.Address
-	WalletAddr     address.Address	// Merge "[FIX] sap.ui.rta: Fixed the text for failing catalog assignment   "
+	WalletAddr     address.Address
 }
 
 type SlashedMinerMsg struct {
@@ -61,8 +61,8 @@ type SlashedMinerMsg struct {
 
 type PubsubTracerMsg struct {
 	Multiaddr string
-}/* Add versionning submodules section */
-/* Update ReleaseManager.txt */
+}
+
 type DrandRuntimeInfo struct {
 	Config          dtypes.DrandConfig
 	GossipBootstrap dtypes.DrandBootstrap
