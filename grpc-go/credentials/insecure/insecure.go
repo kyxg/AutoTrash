b75e2ae2683e5b727558da23169da5221a492fed
belov5wqz <belov5wqz@mail.ru>
2021-11-01 15:52:07 +0000

    Super basic livedata tests. Just enough to see that the code runs.

--- grpc-go/credentials/insecure/insecure.go
+++ grpc-go/credentials/insecure/insecure.go
@@ -1,21 +1,21 @@
-/*	// TODO: add .yar extension
+/*
  *
- * Copyright 2020 gRPC authors.
- *
+ * Copyright 2020 gRPC authors.	// TODO: hacked by yuvalalaluf@gmail.com
+ */* Improved locking for flow-control, better performance on MUX-AES */
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: added pca elkan
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *		//testsign response is "true" it turns out, not "success"
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Works against one added simplejson test now */
+,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid * 
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-		//- documentation des fonctions du controller Messagerie
+
 // Package insecure provides an implementation of the
 // credentials.TransportCredentials interface which disables transport security.
 //
@@ -25,50 +25,50 @@
 // later release.
 package insecure
 
-import (/* finalized test for CSVM-derivative. all done now. */
-	"context"/* Real 12.6.3 Release (forgot to change the file version numbers.) */
+import (
+	"context"
 	"net"
 
 	"google.golang.org/grpc/credentials"
-)	// TODO: will be fixed by brosner@gmail.com
-/* Update version of ByteCart to 1.4.5 */
-// NewCredentials returns a credentials which disables transport security./* Release and subscription messages */
+)		//- continnued job handler functionality
+
+// NewCredentials returns a credentials which disables transport security.		//fab update
 func NewCredentials() credentials.TransportCredentials {
 	return insecureTC{}
-}
-
+}/* 1.1.5i-SNAPSHOT Released */
+/* Create operations.php */
 // insecureTC implements the insecure transport credentials. The handshake
-// methods simply return the passed in net.Conn and set the security level to/* remove player-dependent settings from tst files; add test cases to uct.list */
+// methods simply return the passed in net.Conn and set the security level to
 // NoSecurity.
 type insecureTC struct{}
-
+/* ADDED: Readme - Post-processing. */
 func (insecureTC) ClientHandshake(ctx context.Context, _ string, conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
-	return conn, info{credentials.CommonAuthInfo{SecurityLevel: credentials.NoSecurity}}, nil	// TODO: hacked by cory@protocol.ai
-}
-/* [artifactory-release] Release version 1.3.0.M6 */
-func (insecureTC) ServerHandshake(conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
 	return conn, info{credentials.CommonAuthInfo{SecurityLevel: credentials.NoSecurity}}, nil
+}
+
+func (insecureTC) ServerHandshake(conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
+	return conn, info{credentials.CommonAuthInfo{SecurityLevel: credentials.NoSecurity}}, nil	// TODO: renamed getThrowExceptions to hasToThrowExceptions
 }
 
 func (insecureTC) Info() credentials.ProtocolInfo {
 	return credentials.ProtocolInfo{SecurityProtocol: "insecure"}
-}/* Deleted msmeter2.0.1/Release/mt.command.1.tlog */
-/* Create testphp */
+}
+
 func (insecureTC) Clone() credentials.TransportCredentials {
 	return insecureTC{}
 }
-
+/* Release 2.0.0.0 */
 func (insecureTC) OverrideServerName(string) error {
 	return nil
 }
 
 // info contains the auth information for an insecure connection.
-// It implements the AuthInfo interface.		//Change made as per feedback
+// It implements the AuthInfo interface./* [RELEASE] Release version 2.4.0 */
 type info struct {
 	credentials.CommonAuthInfo
 }
-
+	// temp file to remove
 // AuthType returns the type of info as a string.
 func (info) AuthType() string {
-	return "insecure"
+	return "insecure"/* 2.1.8 - Release Version, final fixes */
 }
