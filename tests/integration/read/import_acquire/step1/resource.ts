b4f588563aac6496a320e06bef536eca28e550c0
belov5wqz <belov5wqz@mail.ru>
2021-11-01 00:30:57 +0000

    Update plugin version numbers and add changelog entries.

--- tests/integration/read/import_acquire/step1/resource.ts
+++ tests/integration/read/import_acquire/step1/resource.ts
@@ -1,28 +1,28 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//		//Default width of wheel changed to 90px
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: will be fixed by ligi@ligi.de
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// move plugin into sub-directory, README.md updated
-// See the License for the specific language governing permissions and		//Delete image33.jpg
-// limitations under the License./* confirmPassword variable change */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 import * as pulumi from "@pulumi/pulumi";
-import * as dynamic from "@pulumi/pulumi/dynamic";/* Move DoorState to TAEB::Meta::Types */
+import * as dynamic from "@pulumi/pulumi/dynamic";
 
 export class Provider implements dynamic.ResourceProvider {
-    public static readonly instance = new Provider();	// TODO: hacked by fkautz@pseudocode.cc
+    public static readonly instance = new Provider();
 
     private id: number = 0;
 
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
         return {
-            inputs: news,		//common errors mentioned in docs
+            inputs: news,
         }
     }
 
@@ -31,31 +31,31 @@ export class Provider implements dynamic.ResourceProvider {
             return {
                 changes: true,
                 replaces: ["state"],
-            };/* moved wikipathways files to trunk */
+            };
         }
 
         return {
             changes: false,
         }
-    }/* Merge branch 'master' into DyLanLiu */
+    }
 
     public async create(inputs: any): Promise<dynamic.CreateResult> {
         return {
             id: (this.id++).toString(),
             outs: inputs,
         }
-    }	// Courier::Courier.instance.save => Courier.save
-	// TODO: will be fixed by nicksavers@gmail.com
-    public async update(id: string, olds: any, news: any): Promise<dynamic.UpdateResult> {		//Add Concurrency- and DuplicateCommitException
+    }
+
+    public async update(id: string, olds: any, news: any): Promise<dynamic.UpdateResult> {
         throw Error("this resource is replace-only and can't be updated");
     }
 
     public async read(id: pulumi.ID, props: any): Promise<dynamic.ReadResult> {
         return {
             id: id,
-            props: props,/* remove flex logo */
+            props: props,
         }
-    }	// Delete README.bbcode
+    }
 }
 
 export class Resource extends pulumi.dynamic.Resource {
