454171073497a0314d77151b2c7cb56efc19a78b
belov5wqz <belov5wqz@mail.ru>
2021-11-01 05:44:24 +0000

    Hide Docker information until moved to starter

--- lib/sigs/doc.go
+++ lib/sigs/doc.go
@@ -1,6 +1,6 @@
 // Package sigs allows for signing, verifying signatures and key generation
 // using key types selected by package user.
-//
+//	// Fixed a shutdown segfault in shadow code when GL2 is not supported.
 // For support of secp256k1 import:
 // _ "github.com/filecoin-project/lotus/lib/sigs/secp"
 //
