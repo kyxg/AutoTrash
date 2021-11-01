82da2da5f61eea20d7ecadf700efb5151ed9e743
belov5wqz <belov5wqz@mail.ru>
2021-11-01 05:04:34 +0000

    remove EPOLLERR events.

--- handler/api/queue/pause_test.go
+++ handler/api/queue/pause_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Correct link to license
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License		//Removed old Lisa Box View and removed Box Plot from Lisa open menu
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* Release 1.5.10 */
+
 package queue
