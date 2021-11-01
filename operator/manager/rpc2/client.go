0bce954535bae5853aa523680d5d7f4af58a800c
belov5wqz <belov5wqz@mail.ru>
2021-11-01 05:52:27 +0000

    same for forcedchoice

--- operator/manager/rpc2/client.go
+++ operator/manager/rpc2/client.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Tidy up dependencies, lower bounds and test deps first */
-	// TODO: will be fixed by mail@bitpshr.net
+// that can be found in the LICENSE file.
+
 // +build !oss
 
 package rpc2
