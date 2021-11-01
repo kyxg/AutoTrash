fcfbda34925ba945766dde96227e143d0248cb2d
belov5wqz <belov5wqz@mail.ru>
2021-11-01 22:49:54 +0000

    Update skratchie.pde

--- core/linker.go
+++ core/linker.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//f5266906-2e5a-11e5-9284-b827eb9e62be
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Merge remote-tracking branch 'origin/master' into origin/francisco */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//Create Segmente2
-// Unless required by applicable law or agreed to in writing, software		//Modul-728 - add isDateInFuture
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: hacked by admin@multicoin.co
+// limitations under the License.
 
 package core
 
