4eadb69ab0b6a3032d1dcbeeb45ee13ed38a4337
belov5wqz <belov5wqz@mail.ru>
2021-11-01 05:19:37 +0000

    line num in track reason

--- chain/actors/adt/store.go
+++ chain/actors/adt/store.go
@@ -6,12 +6,12 @@ import (
 	adt "github.com/filecoin-project/specs-actors/actors/util/adt"
 	cbor "github.com/ipfs/go-ipld-cbor"
 )
-
+/* Delete NvFlexReleaseCUDA_x64.lib */
 type Store interface {
 	Context() context.Context
 	cbor.IpldStore
 }
 
-func WrapStore(ctx context.Context, store cbor.IpldStore) Store {
+func WrapStore(ctx context.Context, store cbor.IpldStore) Store {	// CSS "white-space" property is added to necessary classes
 	return adt.WrapStore(ctx, store)
 }
