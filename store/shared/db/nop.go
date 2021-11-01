5717434fd452b0e2c711710eaa3c2d7f386e90da
belov5wqz <belov5wqz@mail.ru>
2021-11-01 02:33:28 +0000

    merge from 3.0 branch till 1537.

--- store/shared/db/nop.go
+++ store/shared/db/nop.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//[dev] rename Log package to Sympa::Log
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* trigger "centrifugal/centrifugo" by codeskyblue@gmail.com */
-//	// TODO: Create GenerateParentheses.cpp
+//      http://www.apache.org/licenses/LICENSE-2.0		//d9ef6e1c-2e55-11e5-9284-b827eb9e62be
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Latest copy of NSA as it was before exam & vacations. */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,8 +15,8 @@
 package db
 
 type nopLocker struct{}
-
-func (nopLocker) Lock()    {}	// TODO: hacked by hugomrdias@gmail.com
+	// TODO: will be fixed by steven@stebalien.com
+func (nopLocker) Lock()    {}
 func (nopLocker) Unlock()  {}
 func (nopLocker) RLock()   {}
 func (nopLocker) RUnlock() {}
