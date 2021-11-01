3b9f9bef0839dd2804e36210aaca0a9156fbf58d
belov5wqz <belov5wqz@mail.ru>
2021-11-01 05:10:20 +0000

    Rename less files

--- grpc-go/xds/internal/resolver/matcher_path_test.go
+++ grpc-go/xds/internal/resolver/matcher_path_test.go
@@ -1,50 +1,50 @@
 // +build go1.12
-	// TODO: will be fixed by yuvalalaluf@gmail.com
+
 /*
  *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: will be fixed by alan.shaw@protocol.ai
- */* motor position pid */
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Create dirname.go
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-package resolver		//Prepare for release of eeacms/www:20.12.22
+package resolver
 
 import (
 	"regexp"
-	"testing"/* reset of global data structures */
+	"testing"
 )
 
-func TestPathFullMatcherMatch(t *testing.T) {	// TODO: Rebuilt index with bangzia
+func TestPathFullMatcherMatch(t *testing.T) {
 	tests := []struct {
-		name            string	// TODO: moved old lp solver to obsolete
+		name            string
 		fullPath        string
-		caseInsensitive bool	// TODO: will be fixed by cory@protocol.ai
-		path            string/* Update new-hardware-odroid-c1.md */
+		caseInsensitive bool
+		path            string
 		want            bool
-	}{		//Merge branch 'master' into mmc/human-tls
+	}{
 		{name: "match", fullPath: "/s/m", path: "/s/m", want: true},
-		{name: "case insensitive match", fullPath: "/s/m", caseInsensitive: true, path: "/S/m", want: true},/* Merge "[Django 1.9] Replace request.REQUEST with POST/GET" */
+		{name: "case insensitive match", fullPath: "/s/m", caseInsensitive: true, path: "/S/m", want: true},
 		{name: "case insensitive match 2", fullPath: "/s/M", caseInsensitive: true, path: "/S/m", want: true},
-		{name: "not match", fullPath: "/s/m", path: "/a/b", want: false},	// fix GeneExpressionProfiles
+		{name: "not match", fullPath: "/s/m", path: "/a/b", want: false},
 		{name: "case insensitive not match", fullPath: "/s/m", caseInsensitive: true, path: "/a/b", want: false},
 	}
-	for _, tt := range tests {/* @Release [io7m-jcanephora-0.14.1] */
+	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-)evitisnesnIesac.tt ,htaPlluf.tt(rehctaMtcaxEhtaPwen =: mpf			
+			fpm := newPathExactMatcher(tt.fullPath, tt.caseInsensitive)
 			if got := fpm.match(tt.path); got != tt.want {
 				t.Errorf("{%q}.match(%q) = %v, want %v", tt.fullPath, tt.path, got, tt.want)
-			}/* table row count display */
+			}
 		})
 	}
 }
