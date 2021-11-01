5d216aeb6120553ee8b7267fb2e603516787296d
belov5wqz <belov5wqz@mail.ru>
2021-11-01 23:39:58 +0000

    Remove some personal information

--- pkg/codegen/internal/test/testdata/random-pet.pp.go
+++ pkg/codegen/internal/test/testdata/random-pet.pp.go
@@ -5,13 +5,13 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
-func main() {	// f06e8b34-2e67-11e5-9284-b827eb9e62be
+func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := random.NewRandomPet(ctx, "random_pet", &random.RandomPetArgs{
 			Prefix: pulumi.String("doggo"),
-		})/* Move Methanal.Tests.TestView.makeWidgetChildNode to Methanal.Tests.Util. */
+		})
 		if err != nil {
-			return err	// Add 'setDocType' method to Document.
+			return err
 		}
 		return nil
 	})
