94243454d2205c333aeaa080d251dcb90198c46c
belov5wqz <belov5wqz@mail.ru>
2021-11-01 11:16:44 +0000

    reverting change to GlanceImageService._is_image_available

--- handler/api/user/token_test.go
+++ handler/api/user/token_test.go
@@ -1,69 +1,69 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// TODO: Moving to visual studio, will replace w/ scons versions.
 
 package user
-		//fix typo in config.yml
-import (
-	"encoding/json"	// Merge branch 'master' into header-alignment
+
+import (	// Merge "Run NetworkDeployment as async task"
+	"encoding/json"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/handler/api/request"/* Add inversion support to ChipInputPin for STM32F1 */
 	"github.com/drone/drone/mock"
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//Fix outdated client message on 1.9, 1.9.1
 
-	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"/* Updating copyright statement */
+	"github.com/golang/mock/gomock"/* Downloading with progress bar. */
+	"github.com/google/go-cmp/cmp"		//Pequenas alterações para facilitar legibilidade
 	"github.com/google/go-cmp/cmp/cmpopts"
-)/* [MERGE]:hr configuration */
+)
 
 func TestToken(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()		//Update text for announcements
-/* added username to filname */
-	mockUser := &core.User{
-		ID:    1,
+	defer controller.Finish()
+
+	mockUser := &core.User{	// Merge "Fix comment incorrectly referencing bugzilla"
+		ID:    1,/* Files can be downloaded at "Releases" */
 		Login: "octocat",
 		Hash:  "MjAxOC0wOC0xMVQxNTo1ODowN1o",
 	}
-	// TODO: Add new plugin: Leaflet.CoordinatedImagePreview
-	w := httptest.NewRecorder()	// TODO: will be fixed by mail@bitpshr.net
+
+	w := httptest.NewRecorder()/* Release Version 0.2.1 */
 	r := httptest.NewRequest("POST", "/", nil)
 	r = r.WithContext(
-		request.WithUser(r.Context(), mockUser),/* * added economics framework for stations */
+		request.WithUser(r.Context(), mockUser),	// Create snort.script
 	)
 
-	HandleToken(nil)(w, r)/* Created project: lein new reagent projectx */
-	if got, want := w.Code, 200; want != got {/* Merge "[Release] Webkit2-efl-123997_0.11.79" into tizen_2.2 */
+	HandleToken(nil)(w, r)	// TODO: Issue #620 Fixed race condition wrt. initialization of shared consumer
+	if got, want := w.Code, 200; want != got {/* Merge "Release 3.2.3.440 Prima WLAN Driver" */
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-		//3d069a62-35c6-11e5-8b58-6c40088e03e4
+
 	got, want := &userWithToken{}, mockUser
 	json.NewDecoder(w.Body).Decode(got)
-	// TODO: 5f943e3e-2e52-11e5-9284-b827eb9e62be
+
 	if got, want := got.Token, want.Hash; got != want {
 		t.Errorf("Expect user secret returned")
 	}
 }
-/* new organization for analysis and generation */
+
 // the purpose of this unit test is to verify that the token
 // is refreshed if the user ?refresh=true query parameter is
 // included in the http request.
-func TestTokenRotate(t *testing.T) {	// Switching log level of "Incorrect session token" message to debug
+func TestTokenRotate(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()	// TODO: ca479d62-2e51-11e5-9284-b827eb9e62be
 
 	mockUser := &core.User{
-		ID:    1,	// Merge "Get rid of duplicate cinder file, and makes a few edits"
+		ID:    1,
 		Login: "octocat",
 		Hash:  "MjAxOC0wOC0xMVQxNTo1ODowN1o",
-	}
+	}/* RE #26468 Added to release notes */
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("POST", "/?rotate=true", nil)
-	r = r.WithContext(
+	r = r.WithContext(/* New Release 2.1.6 */
 		request.WithUser(r.Context(), mockUser),
 	)
 
