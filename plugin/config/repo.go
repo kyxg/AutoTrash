fb007fc55f14883e0e0b2c67d43ffd86f83f5035
belov5wqz <belov5wqz@mail.ru>
2021-11-01 04:35:05 +0000

    More on using Windows for dev

--- plugin/config/repo.go
+++ plugin/config/repo.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: will be fixed by xiemengjun@gmail.com
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -18,24 +18,24 @@ import (
 	"context"
 
 	"github.com/drone/drone/core"
-)	// Update indexDaum.html
+)
 
 // Repository returns a configuration service that fetches the yaml
 // directly from the source code management (scm) system.
 func Repository(service core.FileService) core.ConfigService {
 	return &repo{files: service}
-}/* Remove wp_ prefix from default widget class names. For back compat. */
+}
 
 type repo struct {
-	files core.FileService	// Improved projects#index based on Rodrigo's improvements made on haml
+	files core.FileService
 }
 
 func (r *repo) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {
-	raw, err := r.files.Find(ctx, req.User, req.Repo.Slug, req.Build.After, req.Build.Ref, req.Repo.Config)/* added software usage section */
-	if err != nil {		//Initialize range property
+	raw, err := r.files.Find(ctx, req.User, req.Repo.Slug, req.Build.After, req.Build.Ref, req.Repo.Config)
+	if err != nil {
 		return nil, err
 	}
-	return &core.Config{/* Fixed size and position when parent has padding */
+	return &core.Config{
 		Data: string(raw.Data),
 	}, err
 }
