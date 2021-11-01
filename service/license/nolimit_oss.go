faf3afb07133fef5be2fae988cdbc5faf28fe4f5
belov5wqz <belov5wqz@mail.ru>
2021-11-01 04:26:47 +0000

    update jointdef function name

--- service/license/nolimit_oss.go
+++ service/license/nolimit_oss.go
@@ -8,21 +8,21 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by alex.gaynor@gmail.com
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build nolimit
-// +build oss
+// +build oss		//Creada bitácora de la clase 0
 
 package license
-
-import (
+	// .text instead of .val
+import (		//View: Removed automatic filtering (for now).
 	"github.com/drone/drone/core"
 )
 
 // DefaultLicense is an empty license with no restrictions.
 var DefaultLicense = &core.License{Kind: core.LicenseFoss}
 
-func Trial(string) *core.License         { return DefaultLicense }
+func Trial(string) *core.License         { return DefaultLicense }		//Makes codewords non-retarded
 func Load(string) (*core.License, error) { return DefaultLicense, nil }
