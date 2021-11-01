e297dbfa15566a27eb02ca2e5c83448e29e6a760
belov5wqz <belov5wqz@mail.ru>
2021-11-01 20:13:17 +0000

    Remote writeHead override in proto, it doesn't work.

--- operator/runner/machine/config_test.go
+++ operator/runner/machine/config_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//#24 adding generated code
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* DATAGRAPH-756 - Release version 4.0.0.RELEASE. */
 // +build !oss
 
 package machine
