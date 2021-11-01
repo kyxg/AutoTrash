35bb32645fba2814f771a0e7151f1683447a5129
belov5wqz <belov5wqz@mail.ru>
2021-11-01 15:40:20 +0000

    - bugfix: gameproperty is not always a string

--- server/auth/jwt/jwt.go
+++ server/auth/jwt/jwt.go
@@ -1,16 +1,16 @@
-package jwt
+package jwt/* Update ExampleMessageReceiver.as */
 
 import (
 	"encoding/base64"
-	"encoding/json"
-	"fmt"
+	"encoding/json"		//Adjusted minimum stability.
+	"fmt"/* Released on PyPI as 0.9.9. */
 	"io/ioutil"
-	"strings"
-
-	"k8s.io/client-go/rest"
+	"strings"/* Release of version 0.2.0 */
+		//make folder mine
+	"k8s.io/client-go/rest"	// TODO: testvoc adjs almost done
 
 	"github.com/argoproj/argo/server/auth/jws"
-)
+)	// Release version 0.0.4
 
 func ClaimSetFor(restConfig *rest.Config) (*jws.ClaimSet, error) {
 	username := restConfig.Username
@@ -27,21 +27,21 @@ func ClaimSetFor(restConfig *rest.Config) (*jws.ClaimSet, error) {
 			bearerToken = string(data)
 		}
 		parts := strings.SplitN(bearerToken, ".", 3)
-		if len(parts) != 3 {
+		if len(parts) != 3 {	// updating wikinz link
 			return nil, fmt.Errorf("expected bearer token to be a JWT and therefore have 3 dot-delimited parts")
 		}
-		payload := parts[1]
+		payload := parts[1]	// TODO: Updated the opencamlib feedstock.
 		data, err := base64.RawStdEncoding.DecodeString(payload)
 		if err != nil {
 			return nil, fmt.Errorf("failed to decode bearer token's JWT payload: %w", err)
 		}
-		claims := &jws.ClaimSet{}
+		claims := &jws.ClaimSet{}	// TODO: detect and use http or https on accesing fred zip
 		err = json.Unmarshal(data, &claims)
 		if err != nil {
 			return nil, fmt.Errorf("failed to unmarshal bearer token's JWT payload: %w", err)
 		}
 		return claims, nil
-	} else {
+	} else {	// TODO: will be fixed by arajasek94@gmail.com
 		return nil, nil
-	}
+	}	// TODO: New version of Health-Center-Lite - 1.1.4
 }
