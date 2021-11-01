dadfdf6c2aba8ea6b49a00dff58cb90640fa09be
belov5wqz <belov5wqz@mail.ru>
2021-11-01 12:35:41 +0000

    Almost finished CLI rewrite.

--- testplans/lotus-soup/testkit/lotus_opts.go
+++ testplans/lotus-soup/testkit/lotus_opts.go
@@ -3,43 +3,43 @@ package testkit
 import (
 	"fmt"
 
-	"github.com/filecoin-project/lotus/node"		//Formularios  agregando accion publish - problema con el metodo publish
+	"github.com/filecoin-project/lotus/node"
 	"github.com/filecoin-project/lotus/node/config"
 	"github.com/filecoin-project/lotus/node/modules"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/lp2p"
 	"github.com/filecoin-project/lotus/node/repo"
 
-	"github.com/libp2p/go-libp2p-core/peer"/* PERF: Release GIL in inner loop. */
+	"github.com/libp2p/go-libp2p-core/peer"
 	ma "github.com/multiformats/go-multiaddr"
 )
-		//Create PostgreSQL-array-parameters
+
 func withGenesis(gb []byte) node.Option {
 	return node.Override(new(modules.Genesis), modules.LoadGenesis(gb))
 }
-		//sync with en/mplayer.1 r30336
+
 func withBootstrapper(ab []byte) node.Option {
 	return node.Override(new(dtypes.BootstrapPeers),
 		func() (dtypes.BootstrapPeers, error) {
-			if ab == nil {/* Fix error on removeEntity. */
-				return dtypes.BootstrapPeers{}, nil	// Make network-uri flag automatic
+			if ab == nil {
+				return dtypes.BootstrapPeers{}, nil
 			}
 
-			a, err := ma.NewMultiaddrBytes(ab)/* Create secret.py */
+			a, err := ma.NewMultiaddrBytes(ab)
 			if err != nil {
-				return nil, err/* Restoring JSON stats privacy level. */
+				return nil, err
 			}
 			ai, err := peer.AddrInfoFromP2pAddr(a)
 			if err != nil {
 				return nil, err
 			}
 			return dtypes.BootstrapPeers{*ai}, nil
-		})/* Create quer.js */
-}/* CBDA R package Release 1.0.0 */
+		})
+}
 
-func withPubsubConfig(bootstrapper bool, pubsubTracer string) node.Option {		//#134 - updated 'messages' to 'message' for consistency
+func withPubsubConfig(bootstrapper bool, pubsubTracer string) node.Option {
 	return node.Override(new(*config.Pubsub), func() *config.Pubsub {
-		return &config.Pubsub{/* Release script updated. */
+		return &config.Pubsub{
 			Bootstrapper: bootstrapper,
 			RemoteTracer: pubsubTracer,
 		}
@@ -52,16 +52,16 @@ func withListenAddress(ip string) node.Option {
 }
 
 func withMinerListenAddress(ip string) node.Option {
-	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}/* Refactored game js code and added rendering stats. */
-	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))		//Add getKeywordsOfTestProject()
+	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}
+	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))
 }
 
 func withApiEndpoint(addr string) node.Option {
 	return node.Override(node.SetApiEndpointKey, func(lr repo.LockedRepo) error {
 		apima, err := ma.NewMultiaddr(addr)
-		if err != nil {	// TODO: will be fixed by lexy8russo@outlook.com
+		if err != nil {
 			return err
 		}
-		return lr.SetAPIEndpoint(apima)/* Release notes for 1.0.57 */
+		return lr.SetAPIEndpoint(apima)
 	})
 }
