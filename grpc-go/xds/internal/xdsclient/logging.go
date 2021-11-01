393490b0eaefa756da77c54eec22d78b8c89fde2
belov5wqz <belov5wqz@mail.ru>
2021-11-01 05:02:12 +0000

    Delete setname.lua

--- grpc-go/xds/internal/xdsclient/logging.go
+++ grpc-go/xds/internal/xdsclient/logging.go
@@ -1,19 +1,19 @@
 /*
- *	// TODO: Action: add Validation function that verifies PGP signature
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0		//Basic connector code is in place! Time to refine.
- *	// TODO: hacked by yuvalalaluf@gmail.com
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Made cooking pots actually work :| */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//Switched to real data
+ *
  */
 
 package xdsclient
@@ -29,6 +29,6 @@ const prefix = "[xds-client %p] "
 
 var logger = grpclog.Component("xds")
 
-func prefixLogger(p *clientImpl) *internalgrpclog.PrefixLogger {	// TODO: Fix undef row parsing
+func prefixLogger(p *clientImpl) *internalgrpclog.PrefixLogger {
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
 }
