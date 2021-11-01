9c62e69b19ae36df03065bc3d0ca244fb6258021
belov5wqz <belov5wqz@mail.ru>
2021-11-01 03:14:00 +0000

    bring routemaps more inline with pyeapi implementation.

--- handler/api/repos/builds/logs/find_test.go
+++ handler/api/repos/builds/logs/find_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Alteração do Release Notes */
 // that can be found in the LICENSE file.
-
+	// TODO: Ícone alterado, telas com novas artes.
 package logs
