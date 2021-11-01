1bb143bdbdbdd9e34a3396a1fcac9ebf8fe3d996
belov5wqz <belov5wqz@mail.ru>
2021-11-01 14:01:06 +0000

    utilize coercion information to add type lambdas

--- plugin/registry/endpoint_oss.go
+++ plugin/registry/endpoint_oss.go
@@ -1,8 +1,8 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: Replace tr("KDocker") with qApp->applicationName().
-//
+// Copyright 2019 Drone IO, Inc.
+//	// TODO: will be fixed by yuvalalaluf@gmail.com
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* 52830bf2-2e5d-11e5-9284-b827eb9e62be */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -10,15 +10,15 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//Implemenent intlSupport flag
+// limitations under the License.	// TODO: will be fixed by martin2cai@hotmail.com
 
 // +build oss
 
-package registry	// TODO: will be fixed by nick@perfectabstractions.com
+package registry
 
-import "github.com/drone/drone/core"		//Update client.fi.yml
-
+import "github.com/drone/drone/core"
+/* Note.java partial rewrite, more methods implemented */
 // EndpointSource returns a no-op registry credential provider.
 func EndpointSource(string, string, bool) core.RegistryService {
-	return new(noop)
+	return new(noop)/* pdf addition */
 }
