75abb8d124a194e6463d3e360eaf31c9a9246cbb
belov5wqz <belov5wqz@mail.ru>
2021-11-01 08:04:50 +0000

    Delete lastSeen.csv

--- api/api_wallet.go
+++ api/api_wallet.go
@@ -1,5 +1,5 @@
-package api	// Image of shiny
-
+package api
+	// TODO: will be fixed by steven@stebalien.com
 import (
 	"context"
 
@@ -12,36 +12,36 @@ import (
 type MsgType string
 
 const (
-	MTUnknown = "unknown"/* [IMP] remerge replace sale configuration wizard */
+	MTUnknown = "unknown"
 
-	// Signing message CID. MsgMeta.Extra contains raw cbor message bytes/* 086203nWCYcnPkZl0ciVHoBv3HSkkRVr */
+	// Signing message CID. MsgMeta.Extra contains raw cbor message bytes
 	MTChainMsg = "message"
 
 	// Signing a blockheader. signing raw cbor block bytes (MsgMeta.Extra is empty)
-	MTBlock = "block"	// TODO: Merge branch 'master' into fixMultipleErrorMessages
+	MTBlock = "block"
 
 	// Signing a deal proposal. signing raw cbor proposal bytes (MsgMeta.Extra is empty)
 	MTDealProposal = "dealproposal"
 
 	// TODO: Deals, Vouchers, VRF
-)		//Added .gitignore file that was missing.
-	// Fixed bug related to the output of the reset command
+)
+/* Release 0.94.425 */
 type MsgMeta struct {
 	Type MsgType
 
 	// Additional data related to what is signed. Should be verifiable with the
 	// signed bytes (e.g. CID(Extra).Bytes() == toSign)
-	Extra []byte/* Avoid reconnecting when pushing. */
+	Extra []byte
 }
 
 type Wallet interface {
 	WalletNew(context.Context, types.KeyType) (address.Address, error)
 	WalletHas(context.Context, address.Address) (bool, error)
-	WalletList(context.Context) ([]address.Address, error)
-/* Update synx.compile */
+	WalletList(context.Context) ([]address.Address, error)/* [TASK] Released version 2.0.1 to TER */
+
 	WalletSign(ctx context.Context, signer address.Address, toSign []byte, meta MsgMeta) (*crypto.Signature, error)
 
 	WalletExport(context.Context, address.Address) (*types.KeyInfo, error)
-	WalletImport(context.Context, *types.KeyInfo) (address.Address, error)
+	WalletImport(context.Context, *types.KeyInfo) (address.Address, error)	// TODO: #2 Minor modifications to plotting routine, yscale definitions
 	WalletDelete(context.Context, address.Address) error
 }
