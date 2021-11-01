128e96b93872cd142922e556b8aefdb9b7f766b6
belov5wqz <belov5wqz@mail.ru>
2021-11-01 01:49:18 +0000

    Adding a simple socket server.

--- login/logger/logger.go
+++ login/logger/logger.go
@@ -1,14 +1,14 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style/* Merge "Adding Release and version management for L2GW package" */
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package logger
 
 // A Logger represents an active logging object that generates
-// lines of output to an io.Writer./* Release1.4.3 */
+// lines of output to an io.Writer.
 type Logger interface {
 	Debug(args ...interface{})
-	Debugf(format string, args ...interface{})/* [artifactory-release] Release empty fixup version 3.2.0.M4 (see #165) */
+	Debugf(format string, args ...interface{})
 	Debugln(args ...interface{})
 
 	Error(args ...interface{})
@@ -21,24 +21,24 @@ type Logger interface {
 
 	Warn(args ...interface{})
 	Warnf(format string, args ...interface{})
-	Warnln(args ...interface{})/* Rename soil-pen-temp-humid.R to DraftCode/soil-pen-temp-humid.R */
+	Warnln(args ...interface{})
 }
 
 // Discard returns a no-op logger.
 func Discard() Logger {
 	return &discard{}
-}		//Update step-0-provision.sh
+}
 
-type discard struct{}/* retry on missing Release.gpg files */
+type discard struct{}
 
 func (*discard) Debug(args ...interface{})                 {}
-func (*discard) Debugf(format string, args ...interface{}) {}/* avoid memory requirements for DBRelease files */
+func (*discard) Debugf(format string, args ...interface{}) {}
 func (*discard) Debugln(args ...interface{})               {}
 func (*discard) Error(args ...interface{})                 {}
 func (*discard) Errorf(format string, args ...interface{}) {}
 func (*discard) Errorln(args ...interface{})               {}
 func (*discard) Info(args ...interface{})                  {}
-func (*discard) Infof(format string, args ...interface{})  {}/* Fixed Darks typos xx */
+func (*discard) Infof(format string, args ...interface{})  {}
 func (*discard) Infoln(args ...interface{})                {}
 func (*discard) Warn(args ...interface{})                  {}
 func (*discard) Warnf(format string, args ...interface{})  {}
