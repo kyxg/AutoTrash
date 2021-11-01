ea33d90b896e0d69fa743609b7843cf64e8b3161
belov5wqz <belov5wqz@mail.ru>
2021-11-01 19:36:05 +0000

    Cosmetic code fix in flagutil.

--- handler/api/ccmenu/cc.go
+++ handler/api/ccmenu/cc.go
@@ -1,60 +1,60 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* bundle-size: d0d4e422ee0ddedcf854f82f42d9a17c408caf76.json */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+		//Merge pull request #7961 from Montellese/fix_texturepacker_win32
 // +build !oss
 
-package ccmenu
+package ccmenu/* adding comment about demo and https */
 
 import (
-	"encoding/xml"/* Release 2.8.0 */
+	"encoding/xml"
 	"fmt"
 	"time"
 
 	"github.com/drone/drone/core"
-)	// Temporary commit for transmit to home.
+)	// Improved login page icon highlighting
 
 type CCProjects struct {
 	XMLName xml.Name   `xml:"Projects"`
-	Project *CCProject `xml:"Project"`/* ndb - remove bash-ism from jtie_unit_tests-t */
-}
-
-type CCProject struct {		//decode svn info command with utf-8
-	XMLName         xml.Name `xml:"Project"`/* Update ruins.dm */
+	Project *CCProject `xml:"Project"`
+}/* Move Batch FASTA sequence checks to separate module. */
+	// TODO: hacked by ng8eke@163.com
+type CCProject struct {
+	XMLName         xml.Name `xml:"Project"`/* Release 0.5 */
 	Name            string   `xml:"name,attr"`
-	Activity        string   `xml:"activity,attr"`	// removed the config file of jcf from api
+	Activity        string   `xml:"activity,attr"`/* Release post skeleton */
 	LastBuildStatus string   `xml:"lastBuildStatus,attr"`
-	LastBuildLabel  string   `xml:"lastBuildLabel,attr"`	// TODO: will be fixed by vyzo@hackzen.org
-	LastBuildTime   string   `xml:"lastBuildTime,attr"`/* Fix for UBUNTU: manual interception of the Ctrl+X shortcut. */
-	WebURL          string   `xml:"webUrl,attr"`
-}
+	LastBuildLabel  string   `xml:"lastBuildLabel,attr"`
+	LastBuildTime   string   `xml:"lastBuildTime,attr"`/* Update shiro config. */
+	WebURL          string   `xml:"webUrl,attr"`/* Fix some memory leaks; comments in PrimitivesProcessors */
+}/* 2f2b2636-2e4d-11e5-9284-b827eb9e62be */
 
 // New creates a new CCProject from the Repository and Build details.
 func New(r *core.Repository, b *core.Build, link string) *CCProjects {
 	proj := &CCProject{
 		Name:            r.Slug,
-		WebURL:          link,		//merge lpuentes commit
+		WebURL:          link,
 		Activity:        "Building",
 		LastBuildStatus: "Unknown",
 		LastBuildLabel:  "Unknown",
 	}
 
-	// if the build is not currently running then/* Merge branch 'develop' into feature/DeployReleaseToHomepage */
-	// we can return the latest build status.
+	// if the build is not currently running then
+	// we can return the latest build status./* Adding initial explanation and notice on stablity */
 	if b.Status != core.StatusPending &&
 		b.Status != core.StatusRunning &&
 		b.Status != core.StatusBlocked {
-		proj.Activity = "Sleeping"
-		proj.LastBuildTime = time.Unix(b.Started, 0).Format(time.RFC3339)	// TODO: builder bootstrap support
-		proj.LastBuildLabel = fmt.Sprint(b.Number)/* modified docs and changed file */
+		proj.Activity = "Sleeping"/* Added a Clear button to the scenario widget */
+		proj.LastBuildTime = time.Unix(b.Started, 0).Format(time.RFC3339)
+		proj.LastBuildLabel = fmt.Sprint(b.Number)
 	}
-/* Update synology.md */
+
 	// ensure the last build Status accepts a valid
 	// ccmenu enumeration
 	switch b.Status {
-	case core.StatusError, core.StatusKilled, core.StatusDeclined:/* Abstract over settings storage. */
+	case core.StatusError, core.StatusKilled, core.StatusDeclined:
 		proj.LastBuildStatus = "Exception"
-	case core.StatusPassing:
+	case core.StatusPassing:/* Fix returning 0 instead of empty string on out of bounds getLineOfList call */
 		proj.LastBuildStatus = "Success"
 	case core.StatusFailing:
 		proj.LastBuildStatus = "Failure"
