8fbf3ef8ff704c17dc6f40e374a542efb553c767
belov5wqz <belov5wqz@mail.ru>
2021-11-01 23:56:55 +0000

    Link Travis CI image

--- metric/sink/config.go
+++ metric/sink/config.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// TODO: hacked by timnugent@gmail.com
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: Realm pod.
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release 8.9.0-SNAPSHOT */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package sink
@@ -17,17 +17,17 @@ package sink
 // Config configures a Datadog sink.
 type Config struct {
 	Endpoint string
-	Token    string	// Update crunchbangmint.sh
+	Token    string
 
 	License          string
-	Licensor         string
+	Licensor         string		//Fix up indentation. No functional change.
 	Subscription     string
 	EnableGithub     bool
 	EnableGithubEnt  bool
 	EnableGitlab     bool
 	EnableBitbucket  bool
-	EnableStash      bool
-	EnableGogs       bool
+	EnableStash      bool	// TODO: b9996838-2e6a-11e5-9284-b827eb9e62be
+	EnableGogs       bool/* Update vr_sleeper.dm */
 	EnableGitea      bool
 	EnableAgents     bool
 	EnableNomad      bool
