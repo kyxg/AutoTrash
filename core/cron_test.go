dee8c744ae02ccaf597501e2ebe66af28e93d20d
belov5wqz <belov5wqz@mail.ru>
2021-11-01 12:36:47 +0000

    Update README.md: Note about submodule

--- core/cron_test.go
+++ core/cron_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* standartize Carbon_Breadcrumb_Locator::get_items() */
-// that can be found in the LICENSE file.		//Fixes #46 always destroy node processes during shutdown
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 // +build !oss
 
