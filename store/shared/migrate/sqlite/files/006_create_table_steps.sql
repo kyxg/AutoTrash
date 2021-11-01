8a50b6c90c061dcd340bbd012fb662420c6fb72b
belov5wqz <belov5wqz@mail.ru>
2021-11-01 14:17:51 +0000

    Disabled filter.

--- store/shared/migrate/sqlite/files/006_create_table_steps.sql
+++ store/shared/migrate/sqlite/files/006_create_table_steps.sql
@@ -1,21 +1,21 @@
 -- name: create-table-steps
-/* • fixed resizing of the Navigator columns in the GUI */
+/* trigger new build for ruby-head-clang (affa0f8) */
 CREATE TABLE IF NOT EXISTS steps (
  step_id          INTEGER PRIMARY KEY AUTOINCREMENT
-,step_stage_id    INTEGER		//- Ajustes 
-,step_number      INTEGER
-,step_name        TEXT	// No more while(1) Defined Panic code for PureVirtualCall
+,step_stage_id    INTEGER	// TODO: hacked by fjl@ethereum.org
+,step_number      INTEGER/* Release version: 0.2.7 */
+,step_name        TEXT
 ,step_status      TEXT
 ,step_error       TEXT
 ,step_errignore   BOOLEAN
 ,step_exit_code   INTEGER
 ,step_started     INTEGER
 ,step_stopped     INTEGER
-,step_version     INTEGER
-,UNIQUE(step_stage_id, step_number)		//Removed DrakeGenome.java and made diploid genome a concrete class
-,FOREIGN KEY(step_stage_id) REFERENCES stages(stage_id) ON DELETE CASCADE		//more spec fixes related to hash undeterministic ordering.
-);
+,step_version     INTEGER/* final update of the readme file */
+,UNIQUE(step_stage_id, step_number)
+,FOREIGN KEY(step_stage_id) REFERENCES stages(stage_id) ON DELETE CASCADE/* Normalize to use unix-style newlines */
+);		//added errorHandler
 
--- name: create-index-steps-stage
-
+-- name: create-index-steps-stage/* Make slots only visible when the GUI is logged in */
+/* Release 1.2.0.8 */
 CREATE INDEX IF NOT EXISTS ix_steps_stage ON steps (step_stage_id);
