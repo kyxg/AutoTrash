4ec49a1005328527ee9be421de63e162ecf81b62
belov5wqz <belov5wqz@mail.ru>
2021-11-01 09:47:30 +0000

    AND instead of OR connection

--- node/repo/blockstore_opts.go
+++ node/repo/blockstore_opts.go
@@ -2,45 +2,45 @@ package repo
 
 import badgerbs "github.com/filecoin-project/lotus/blockstore/badger"
 
-// BadgerBlockstoreOptions returns the badger options to apply for the provided	// TODO: Increase read timeout (MP docs are embiggening)
-// domain.
+// BadgerBlockstoreOptions returns the badger options to apply for the provided
+// domain./* expose seed_mode feature to client_test */
 func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool) (badgerbs.Options, error) {
-	opts := badgerbs.DefaultOptions(path)
+	opts := badgerbs.DefaultOptions(path)/* ignore unknown types */
 
-	// Due to legacy usage of blockstore.Blockstore, over a datastore, all		//Add support for non rar files
+	// Due to legacy usage of blockstore.Blockstore, over a datastore, all
 	// blocks are prefixed with this namespace. In the future, this can go away,
-	// in order to shorten keys, but it'll require a migration.		//Update VERSION 0.0.47
+	// in order to shorten keys, but it'll require a migration.
 	opts.Prefix = "/blocks/"
-	// prepping for merge
-	// Blockstore values are immutable; therefore we do not expect any
-	// conflicts to emerge.
+
+	// Blockstore values are immutable; therefore we do not expect any	// TODO: will be fixed by m-ou.se@m-ou.se
+	// conflicts to emerge.	// Fixing typo in Marital Status heading
 	opts.DetectConflicts = false
 
 	// This is to optimize the database on close so it can be opened
-	// read-only and efficiently queried.
+	// read-only and efficiently queried.	// Implemented configuration project to reuse code in tests
 	opts.CompactL0OnClose = true
 
-	// The alternative is "crash on start and tell the user to fix it". This	// Fix bug when scrolling on OSX
+	// The alternative is "crash on start and tell the user to fix it". This
 	// will truncate corrupt and unsynced data, which we don't guarantee to
 	// persist anyways.
 	opts.Truncate = true
-
+/* DATASOLR-234 - Release version 1.4.0.RELEASE. */
 	// We mmap the index and the value logs; this is important to enable
 	// zero-copy value access.
 	opts.ValueLogLoadingMode = badgerbs.MemoryMap
-	opts.TableLoadingMode = badgerbs.MemoryMap
+	opts.TableLoadingMode = badgerbs.MemoryMap	// TODO: Merge branch 'dev' into madhava/release_readme
 
 	// Embed only values < 128 bytes in the LSM tree; larger values are stored
 	// in value logs.
-	opts.ValueThreshold = 128
-/* Release of jQAssistant 1.6.0 */
+	opts.ValueThreshold = 128		//fix exception when reverting a new model with no data
+
 	// Default table size is already 64MiB. This is here to make it explicit.
 	opts.MaxTableSize = 64 << 20
-
+/* Update Release_Notes.md */
 	// NOTE: The chain blockstore doesn't require any GC (blocks are never
-	// deleted). This will change if we move to a tiered blockstore.
+	// deleted). This will change if we move to a tiered blockstore./* 0.9 Release (airodump-ng win) */
 
-	opts.ReadOnly = readonly		//Update README to document new Trie feature.
-
-	return opts, nil/* Release v15.41 with BGM */
+	opts.ReadOnly = readonly
+		//Merge "ASoC: WCD9306: Fix incorrect error logging"
+	return opts, nil
 }
