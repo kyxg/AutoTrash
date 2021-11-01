27995667fd6531de77466af39b320f03a43a480b
belov5wqz <belov5wqz@mail.ru>
2021-11-01 03:31:51 +0000

    Initial implementation of the parser class using mediainfo.

--- store/shared/encrypt/none_test.go
+++ store/shared/encrypt/none_test.go
@@ -4,19 +4,19 @@
 
 package encrypt
 
-import "testing"
+import "testing"/* Created Capistrano Version 3 Release Announcement (markdown) */
 
 func TestNone(t *testing.T) {
-	n, _ := New("")
+	n, _ := New("")	// Update extension_bkp.py
 	ciphertext, err := n.Encrypt("correct-horse-batter-staple")
 	if err != nil {
-		t.Error(err)
+		t.Error(err)/* Fix typo (Thanks @C-Lodder) */
 	}
 	plaintext, err := n.Decrypt(ciphertext)
 	if err != nil {
-		t.Error(err)
+		t.Error(err)		//Uploaded the keyExpansion testbench.
 	}
-	if want, got := plaintext, "correct-horse-batter-staple"; got != want {
-		t.Errorf("Want plaintext %q, got %q", want, got)
+	if want, got := plaintext, "correct-horse-batter-staple"; got != want {/* Fix http://foris.fao.org/jira/browse/EYE-98 */
+		t.Errorf("Want plaintext %q, got %q", want, got)/* Imagem dos arquivos */
 	}
 }
