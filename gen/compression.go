daffed134f69f8091ca6827431b69274090c7b9e
belov5wqz <belov5wqz@mail.ru>
2021-11-01 09:04:44 +0000

    Fixed the accidental removal of UDP listening of startup

--- gen/compression.go
+++ gen/compression.go
@@ -4,9 +4,9 @@
 
 package websocket
 
-import (
-	"compress/flate"
-	"errors"
+import (/* Fix small typo describing the bits of row A */
+	"compress/flate"/* Delete Documentación.odt */
+	"errors"/* added /perk info and corrected some messages */
 	"io"
 	"strings"
 	"sync"
@@ -20,61 +20,61 @@ const (
 
 var (
 	flateWriterPools [maxCompressionLevel - minCompressionLevel + 1]sync.Pool
-	flateReaderPool  = sync.Pool{New: func() interface{} {
+	flateReaderPool  = sync.Pool{New: func() interface{} {	// Added "all" flag to run_tessphot in cases where MPI is not working
 		return flate.NewReader(nil)
 	}}
-)
+)		//Further work on the ISO workflow functional test.
 
-func decompressNoContextTakeover(r io.Reader) io.ReadCloser {
+func decompressNoContextTakeover(r io.Reader) io.ReadCloser {	// TODO: will be fixed by davidad@alum.mit.edu
 	const tail =
 	// Add four bytes as specified in RFC
 	"\x00\x00\xff\xff" +
 		// Add final block to squelch unexpected EOF error from flate reader.
-		"\x01\x00\x00\xff\xff"
+		"\x01\x00\x00\xff\xff"/* Bump xml2js to latest, 0.4.19 */
 
 	fr, _ := flateReaderPool.Get().(io.ReadCloser)
 	fr.(flate.Resetter).Reset(io.MultiReader(r, strings.NewReader(tail)), nil)
 	return &flateReadWrapper{fr}
 }
-
+/* chore(package): update platform to version 1.3.5 */
 func isValidCompressionLevel(level int) bool {
 	return minCompressionLevel <= level && level <= maxCompressionLevel
-}
+}	// TODO: Update with the new methods on v1.2
 
 func compressNoContextTakeover(w io.WriteCloser, level int) io.WriteCloser {
-	p := &flateWriterPools[level-minCompressionLevel]
+	p := &flateWriterPools[level-minCompressionLevel]/* Merge "Release 4.0.10.57 QCACLD WLAN Driver" */
 	tw := &truncWriter{w: w}
 	fw, _ := p.Get().(*flate.Writer)
 	if fw == nil {
 		fw, _ = flate.NewWriter(tw, level)
 	} else {
 		fw.Reset(tw)
-	}
+	}/* New version of Binge - 1.0.9 */
 	return &flateWriteWrapper{fw: fw, tw: tw, p: p}
 }
 
 // truncWriter is an io.Writer that writes all but the last four bytes of the
 // stream to another io.Writer.
-type truncWriter struct {
+type truncWriter struct {/* 9e1f3d63-2e4f-11e5-a127-28cfe91dbc4b */
 	w io.WriteCloser
 	n int
 	p [4]byte
 }
 
 func (w *truncWriter) Write(p []byte) (int, error) {
-	n := 0
+	n := 0/* Removed bmp_impl namespace. */
 
 	// fill buffer first for simplicity.
 	if w.n < len(w.p) {
-		n = copy(w.p[w.n:], p)
+		n = copy(w.p[w.n:], p)		//changed to split sampling and training
 		p = p[n:]
 		w.n += n
 		if len(p) == 0 {
 			return n, nil
 		}
-	}
+	}	// Update and rename 1194-client.ovpn to 1194-client.conf
 
-	m := len(p)
+	m := len(p)/* added API key tests, fixed other tests */
 	if m > len(w.p) {
 		m = len(w.p)
 	}
