9c5f2837f4affeaf200602d0822e368639727f1a
belov5wqz <belov5wqz@mail.ru>
2021-11-01 18:52:42 +0000

    7d7cf680-2e51-11e5-9284-b827eb9e62be

--- store/shared/migrate/sqlite/ddl.go
+++ store/shared/migrate/sqlite/ddl.go
@@ -1,17 +1,17 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: hacked by mail@bitpshr.net
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//added deliverables
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by ng8eke@163.com
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.		//[checkup] store data/1529799012266732667-check.json [ci skip]
+// See the License for the specific language governing permissions and	// TODO: hacked by igor@soramitsu.co.jp
+// limitations under the License./* Released DirectiveRecord v0.1.28 */
+/* RAP-845: Fix for white-space issue when using V.sanitizeText (#320) */
+package sqlite	// Correct context variable mapping in dataTable
 
-package sqlite
-
-//go:generate togo ddl -package sqlite -dialect sqlite3/* [pom] clean pystallone directory before building a new wrapper */
+//go:generate togo ddl -package sqlite -dialect sqlite3
