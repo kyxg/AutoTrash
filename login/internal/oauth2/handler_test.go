3e1212bd25f8105b95e65f20b6350f970998c37f
belov5wqz <belov5wqz@mail.ru>
2021-11-01 19:18:37 +0000

    small fix in test

--- login/internal/oauth2/handler_test.go
+++ login/internal/oauth2/handler_test.go
@@ -1,5 +1,5 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.
+// Copyright 2017 Drone.IO Inc. All rights reserved.		//Delete Human-usable
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-
-package oauth2
+/* EEPROM API change */
+package oauth2		//Build results of 66d7d8b (on master)
