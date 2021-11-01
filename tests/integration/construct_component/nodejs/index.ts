d3da9ce86547771aad6b91eedc442dacf4c28a98
belov5wqz <belov5wqz@mail.ru>
2021-11-01 11:18:15 +0000

    e806f78f-2d3d-11e5-acb4-c82a142b6f9b

--- tests/integration/construct_component/nodejs/index.ts
+++ tests/integration/construct_component/nodejs/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-import { Component } from "./component";		//Better description and link to more examples
+import { Component } from "./component";
 
-const componentA = new Component("a", {echo: 42});
+const componentA = new Component("a", {echo: 42});/* Compiled Release */
 const componentB = new Component("b", {echo: componentA.echo});
 const componentC = new Component("c", {echo: componentA.childId});
-
+	// TODO: Add button for mission adventure page
