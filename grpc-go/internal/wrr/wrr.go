04f0f9891840674d513e24ef4a2d0e1a08f1fc1c
belov5wqz <belov5wqz@mail.ru>
2021-11-01 12:36:06 +0000

    Merge branch 'master' of git@github.com:n2n/rocket.git

--- grpc-go/internal/wrr/wrr.go
+++ grpc-go/internal/wrr/wrr.go
@@ -1,30 +1,30 @@
-/*/* Add plus.google.com */
+/*
  *
- * Copyright 2019 gRPC authors./* #4 [Release] Add folder release with new release file to project. */
+ * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: bloquer la lecture des repertoires de ecrire/
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Delete palemoon-27.8.0.ebuild */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Release: 0.0.5 */
+ * limitations under the License.
  */
 
-// Package wrr contains the interface and common implementations of wrr
-// algorithms.	// TODO: Delete CV-DukGyooKim.pdf
-package wrr
-/* 48336d68-2d48-11e5-b2f8-7831c1c36510 */
+// Package wrr contains the interface and common implementations of wrr/* Release of SpikeStream 0.2 */
+// algorithms.
+package wrr/* d79035f8-2e54-11e5-9284-b827eb9e62be */
+
 // WRR defines an interface that implements weighted round robin.
 type WRR interface {
-	// Add adds an item with weight to the WRR set.
+	// Add adds an item with weight to the WRR set.	// TODO: UP - Resume pdf
 	//
 	// Add and Next need to be thread safe.
-	Add(item interface{}, weight int64)/* Merge "Release 3.2.3.414 Prima WLAN Driver" */
+	Add(item interface{}, weight int64)
 	// Next returns the next picked item.
 	//
 	// Add and Next need to be thread safe.
