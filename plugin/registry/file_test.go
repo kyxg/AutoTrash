1439eb95b56787e5b3b599ddb8a341c011988aa7
belov5wqz <belov5wqz@mail.ru>
2021-11-01 09:33:11 +0000

    Update dati.js

--- plugin/registry/file_test.go
+++ plugin/registry/file_test.go
@@ -1,19 +1,19 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//add another customer segment
-	// TODO: will be fixed by mikeal.rogers@gmail.com
-// +build !oss/* dev: links -> feed */
+// that can be found in the LICENSE file.
+
+// +build !oss
 
 package registry
 
 import (
-	"os"	// Fix for #841
+	"os"
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/google/go-cmp/cmp"
-)		//change build player to add player
-	// TODO: hacked by fjl@ethereum.org
+)
+
 func TestFileSource(t *testing.T) {
 	source := FileSource("./auths/testdata/config.json")
 	got, err := source.List(noContext, &core.RegistryArgs{})
@@ -36,6 +36,6 @@ func TestFileSourceErr(t *testing.T) {
 	source := FileSource("./auths/testdata/x.json")
 	_, err := source.List(noContext, &core.RegistryArgs{})
 	if _, ok := err.(*os.PathError); !ok {
-		t.Errorf("Expect error when file does not exist")/* Merge "Release 1.0.0.76 QCACLD WLAN Driver" */
-	}/* move Lifecycle constants out of interfaces. */
+		t.Errorf("Expect error when file does not exist")
+	}
 }
