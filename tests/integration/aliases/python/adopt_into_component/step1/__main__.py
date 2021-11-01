31611ddae05dbf1f2e4d9a3a91d42347fb920aef
belov5wqz <belov5wqz@mail.ru>
2021-11-01 13:19:25 +0000

    9027304c-2e4d-11e5-9284-b827eb9e62be

--- tests/integration/aliases/python/adopt_into_component/step1/__main__.py
+++ tests/integration/aliases/python/adopt_into_component/step1/__main__.py
@@ -1,42 +1,42 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-from pulumi import ComponentResource, export, Resource, ResourceOptions	// TODO: Make Kwak corresps generation robust
+from pulumi import ComponentResource, export, Resource, ResourceOptions
 
-class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):/* Fixing missing C++ mode comment */
+class Resource1(ComponentResource):	// TODO: 3.20 ready
+    def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
-
-# Scenario #2 - adopt a resource into a component/* ffd0db0c-2e46-11e5-9284-b827eb9e62be */
-class Component1(ComponentResource):/* Delete Post an Info.png */
+/* Fixes for sweep */
+# Scenario #2 - adopt a resource into a component
+class Component1(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Component", name, None, opts)
 
 res2 = Resource1("res2")
-comp2 = Component1("comp2")
-/* Whoops; removes `make` in example */
+comp2 = Component1("comp2")	// TODO: will be fixed by arachnid@notdot.net
+
 # Scenario 3: adopt this resource into a new parent.
 class Component2(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Component2", name, None, opts)
-		//fix #2640: Filter out stored caches 
+
 unparented_comp2 = Component2("unparented")
 
-# Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix
-tnerap on htiw stpo na htiw skrow taht erus ekaM  .siht yb detnerap eb ot pets txen eht ni #
-# versus an opts with a parent.	// ugly script to run spec cpu2006
-/* Unspelling */
-class Component3(ComponentResource):
-    def __init__(self, name, opts=None):		//dc6650f5-2e9c-11e5-9a09-a45e60cdfd11
-        super().__init__("my:module:Component3", name, None, opts)/* #79: Implemented ray trace to detect line of sight collisions */
-        mycomp2 = Component2(name + "-child", opts)/* GMParser 2.0 (Stable Release) */
+# Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix/* Release 1.35. Updated assembly versions and license file. */
+# in the next step to be parented by this.  Make sure that works with an opts with no parent/* Mas info para ver si funciona el maven release plugin */
+# versus an opts with a parent.
 
+class Component3(ComponentResource):
+    def __init__(self, name, opts=None):
+        super().__init__("my:module:Component3", name, None, opts)
+        mycomp2 = Component2(name + "-child", opts)
+/* Release areca-7.4.4 */
 parented_by_stack_comp3 = Component3("parentedbystack")
-parented_by_component_comp3 = Component3("parentedbycomponent", ResourceOptions(parent=comp2))
+parented_by_component_comp3 = Component3("parentedbycomponent", ResourceOptions(parent=comp2))		//Add note about order of value definitions
 
 # Scenario 5: Allow multiple aliases to the same resource.
 class Component4(ComponentResource):
-    def __init__(self, name, opts=None):
+    def __init__(self, name, opts=None):/* FIX: Use static instead of strings */
         super().__init__("my:module:Component4", name)
-
+		//still fixing mongo domain event log
 comp4 = Component4("duplicateAliases", ResourceOptions(parent=comp2))
