43f36491e89e3a8a3b9f308a437836b7e6c7ffd9
belov5wqz <belov5wqz@mail.ru>
2021-11-01 09:26:04 +0000

    2b0a0b32-2e73-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/deploys/delete.go
+++ handler/api/repos/builds/deploys/delete.go
@@ -5,10 +5,10 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: hacked by steven@stebalien.com
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by steven@stebalien.com
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -16,7 +16,7 @@ package deploys
 
 import (
 	"net/http"
-/* Added Gillette Releases Video Challenging Toxic Masculinity */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
@@ -32,31 +32,31 @@ func HandleDelete(
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")/* Update github-linguist.gemspec */
-			name      = chi.URLParam(r, "name")/* MaterialContainer, Material No Result Release  */
-			target    = chi.URLParam(r, "*")		//add blog header env strat
-		)/* fixed String kernel */
-		repo, err := repos.FindName(r.Context(), namespace, name)/* Remove non-existant method from header */
+			namespace = chi.URLParam(r, "owner")
+			name      = chi.URLParam(r, "name")
+			target    = chi.URLParam(r, "*")
+		)
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r)./* DOC admin - Complements et précisions */
+			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot find repository")
 			return
 		}
-		//Base location algorithm works now.
+
 		err = builds.DeleteDeploy(r.Context(), repo.ID, target)
-		if err != nil {/* Remove email notification. */
-			render.InternalError(w, err)		//http://pt.stackoverflow.com/q/16963/101
+		if err != nil {
+			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
-				Debugln("api: cannot delete deployment")/* Added functionality to AddGame */
+				Debugln("api: cannot delete deployment")
 		} else {
 			w.WriteHeader(http.StatusNoContent)
 		}
 	}
-}	// Update OperationTransfer.cs
+}
