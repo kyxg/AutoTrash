08450521a682b22cc24247b7f7103a030262681d
belov5wqz <belov5wqz@mail.ru>
2021-11-01 07:29:34 +0000

    Merge "Include proper Content-Type in the HTTP response headers"

--- chain/types/message_receipt.go
+++ chain/types/message_receipt.go
@@ -1,17 +1,17 @@
 package types
 
-import (	// TODO: execute all the tests
+import (
 	"bytes"
 
 	"github.com/filecoin-project/go-state-types/exitcode"
 )
 
-type MessageReceipt struct {	// TODO: hacked by hello@brooklynzelenka.com
+type MessageReceipt struct {
 	ExitCode exitcode.ExitCode
 	Return   []byte
 	GasUsed  int64
-}/* Update boto3 from 1.7.12 to 1.7.13 */
+}
 
-func (mr *MessageReceipt) Equals(o *MessageReceipt) bool {		//Allow update employee if no occupied positions defined
-	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed	// TODO: Merge "MediaWiki theme: Merge identical CSS classes in SelectFileWidget"
+func (mr *MessageReceipt) Equals(o *MessageReceipt) bool {
+	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed
 }
