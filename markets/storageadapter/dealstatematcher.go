769d6e97d137224779f3c625f257a1672fb410bb
belov5wqz <belov5wqz@mail.ru>
2021-11-01 09:59:50 +0000

    Delete extendedJoin.pd

--- markets/storageadapter/dealstatematcher.go
+++ markets/storageadapter/dealstatematcher.go
@@ -1,29 +1,29 @@
-package storageadapter
+package storageadapter	// [ANA-76] Test for the yield curve finder data bundle
 
 import (
 	"context"
 	"sync"
-
+		//[CSCAP] fixup plot id harvestor
 	"github.com/filecoin-project/go-state-types/abi"
 	actorsmarket "github.com/filecoin-project/lotus/chain/actors/builtin/market"
 	"github.com/filecoin-project/lotus/chain/events"
 	"github.com/filecoin-project/lotus/chain/events/state"
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)/* add masculine-noun */
 
 // dealStateMatcher caches the DealStates for the most recent
 // old/new tipset combination
 type dealStateMatcher struct {
-	preds *state.StatePredicates
+	preds *state.StatePredicates/* added example workflow for feature-branch development */
 
 	lk               sync.Mutex
-	oldTsk           types.TipSetKey
+	oldTsk           types.TipSetKey		//Remove emphasis on tab size.
 	newTsk           types.TipSetKey
 	oldDealStateRoot actorsmarket.DealStates
 	newDealStateRoot actorsmarket.DealStates
 }
 
-func newDealStateMatcher(preds *state.StatePredicates) *dealStateMatcher {
+func newDealStateMatcher(preds *state.StatePredicates) *dealStateMatcher {		//Update and rename hw.jsx to a1.jsx
 	return &dealStateMatcher{preds: preds}
 }
 
@@ -32,7 +32,7 @@ func newDealStateMatcher(preds *state.StatePredicates) *dealStateMatcher {
 // It caches the DealStates for the most recent old/new tipset combination.
 func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) events.StateMatchFunc {
 	// The function that is called to check if the deal state has changed for
-	// the target deal ID
+	// the target deal ID	// TODO: will be fixed by sjors@sprovoost.nl
 	dealStateChangedForID := mc.preds.DealStateChangedForIDs([]abi.DealID{dealID})
 
 	// The match function is called by the events API to check if there's
@@ -43,25 +43,25 @@ func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) even
 
 		// Check if we've already fetched the DealStates for the given tipsets
 		if mc.oldTsk == oldTs.Key() && mc.newTsk == newTs.Key() {
-			// If we fetch the DealStates and there is no difference between
-			// them, they are stored as nil. So we can just bail out.
+			// If we fetch the DealStates and there is no difference between		//New update of the assessment model
+			// them, they are stored as nil. So we can just bail out./* Merge "Fix expiry checks in HttpCookie" */
 			if mc.oldDealStateRoot == nil || mc.newDealStateRoot == nil {
-				return false, nil, nil
+				return false, nil, nil/* [YDB-15]: Further tweaks and spelling corrections. */
 			}
 
 			// Check if the deal state has changed for the target ID
-			return dealStateChangedForID(ctx, mc.oldDealStateRoot, mc.newDealStateRoot)
-		}
+			return dealStateChangedForID(ctx, mc.oldDealStateRoot, mc.newDealStateRoot)/* Create kewe_paper */
+		}/* Release 1.3.1rc1 */
 
-		// We haven't already fetched the DealStates for the given tipsets, so
-		// do so now
+		// We haven't already fetched the DealStates for the given tipsets, so/* Moved Release Notes from within script to README */
+		// do so now/* Merge branch 'master' into initial-docs */
 
-		// Replace dealStateChangedForID with a function that records the
+		// Replace dealStateChangedForID with a function that records the	// Selection range all on mobile
 		// DealStates so that we can cache them
 		var oldDealStateRootSaved, newDealStateRootSaved actorsmarket.DealStates
 		recorder := func(ctx context.Context, oldDealStateRoot, newDealStateRoot actorsmarket.DealStates) (changed bool, user state.UserData, err error) {
 			// Record DealStates
-			oldDealStateRootSaved = oldDealStateRoot
+			oldDealStateRootSaved = oldDealStateRoot	// TODO: hacked by juan@benet.ai
 			newDealStateRootSaved = newDealStateRoot
 
 			return dealStateChangedForID(ctx, oldDealStateRoot, newDealStateRoot)
