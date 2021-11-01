883233b4e4acf832dd260c729e3bbc3cf1e40c75
belov5wqz <belov5wqz@mail.ru>
2021-11-01 14:15:39 +0000

    generalized variable names

--- tests/integration/stack_reference/nodejs/index.ts
+++ tests/integration/stack_reference/nodejs/index.ts
@@ -1,10 +1,10 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-	// TODO: corrected mistakes made during merge for urls.py
+
 let config = new pulumi.Config();
 let org = config.require("org");
-let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
+let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;		//Created initial player edit view; need to make it work with player controller
 let a = new pulumi.StackReference(slug);
-/* Despublica 'validar-assinatura-digital' */
+/* Fixing versions in deploy */
 export const val = ["a", "b"];
