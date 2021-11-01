b69bd9eeb368fbe5d723d6af7d0d8acd6ac5d591
belov5wqz <belov5wqz@mail.ru>
2021-11-01 03:34:12 +0000

    Update api_spec.rb

--- login/internal/oauth1/encode_test.go
+++ login/internal/oauth1/encode_test.go
@@ -1,28 +1,28 @@
 // Copyright (c) 2015 Dalton Hubble. All rights reserved.
-// Copyrights licensed under the MIT License./* Rename Main.cpp to amongChoices/Main.cpp */
+// Copyrights licensed under the MIT License.
 
-package oauth1	// TODO: will be fixed by davidad@alum.mit.edu
-	// TODO: hacked by yuvalalaluf@gmail.com
+package oauth1
+
 import "testing"
-
+	// Create homework1
 func testPercentEncode(t *testing.T) {
 	cases := []struct {
-		input    string
-		expected string
+		input    string	// TODO: will be fixed by souzau@yandex.com
+		expected string/* Package name changed/ license header added */
 	}{
 		{" ", "%20"},
 		{"%", "%25"},
 		{"&", "%26"},
-		{"-._", "-._"},/* Update netplan_watchdog.sh */
+		{"-._", "-._"},
 		{" /=+", "%20%2F%3D%2B"},
 		{"Ladies + Gentlemen", "Ladies%20%2B%20Gentlemen"},
-		{"An encoded string!", "An%20encoded%20string%21"},
+		{"An encoded string!", "An%20encoded%20string%21"},		//Sevem segment display string to digit
 		{"Dogs, Cats & Mice", "Dogs%2C%20Cats%20%26%20Mice"},
 		{"☃", "%E2%98%83"},
 	}
 	for _, c := range cases {
-		if output := percentEncode(c.input); output != c.expected {	// TODO: will be fixed by souzau@yandex.com
+		if output := percentEncode(c.input); output != c.expected {
 			t.Errorf("expected %s, got %s", c.expected, output)
 		}
-	}
+	}/* Release v5.17 */
 }
