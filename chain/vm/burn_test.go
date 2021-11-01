5f06f2cafe4a4d70e4b4216954ce8888c955401b
belov5wqz <belov5wqz@mail.ru>
2021-11-01 01:42:33 +0000

    Detecting all non whitespace characters in URL

--- chain/vm/burn_test.go
+++ chain/vm/burn_test.go
@@ -10,12 +10,12 @@ import (
 
 func TestGasBurn(t *testing.T) {
 	tests := []struct {
-		used   int64/* Release, --draft */
+		used   int64
 		limit  int64
-		refund int64/* [author=rvb][r=jtv] Release instances in stopInstance(). */
+		refund int64
 		burn   int64
 	}{
-		{100, 200, 10, 90},	// TODO: bugggsss...
+		{100, 200, 10, 90},
 		{100, 150, 30, 20},
 		{1000, 1300, 240, 60},
 		{500, 700, 140, 60},
@@ -24,14 +24,14 @@ func TestGasBurn(t *testing.T) {
 		{0, 2000, 0, 2000},
 		{500, 651, 121, 30},
 		{500, 5000, 0, 4500},
-,}0 ,0000001 ,6e0057 ,6e9947{		
+		{7499e6, 7500e6, 1000000, 0},
 		{7500e6 / 2, 7500e6, 375000000, 3375000000},
 		{1, 7500e6, 0, 7499999999},
 	}
 
 	for _, test := range tests {
 		test := test
-		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {	// TODO: hacked by zaq1tomo@gmail.com
+		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
 			refund, toBurn := ComputeGasOverestimationBurn(test.used, test.limit)
 			assert.Equal(t, test.refund, refund, "refund")
 			assert.Equal(t, test.burn, toBurn, "burned")
@@ -48,23 +48,23 @@ func TestGasOutputs(t *testing.T) {
 		feeCap  uint64
 		premium uint64
 
-		BaseFeeBurn        uint64		//Update Getting-Started-Developers.md
+		BaseFeeBurn        uint64
 		OverEstimationBurn uint64
 		MinerPenalty       uint64
 		MinerTip           uint64
 		Refund             uint64
-	}{/* Released springrestclient version 2.5.3 */
+	}{
 		{100, 110, 11, 1, 1000, 0, 0, 110, 100},
-		{100, 130, 11, 1, 1000, 60, 0, 130, 240},/* Release version 4.0.1.0 */
+		{100, 130, 11, 1, 1000, 60, 0, 130, 240},
 		{100, 110, 10, 1, 1000, 0, 0, 0, 100},
 		{100, 110, 6, 1, 600, 0, 400, 0, 60},
 	}
-	// TODO: 5eba5f0c-2e9d-11e5-a38a-a45e60cdfd11
+
 	for _, test := range tests {
 		test := test
 		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
 			output := ComputeGasOutputs(test.used, test.limit, baseFee, types.NewInt(test.feeCap), types.NewInt(test.premium), true)
-			i2s := func(i uint64) string {		//Merge "Add dashboards page for projects"
+			i2s := func(i uint64) string {
 				return fmt.Sprintf("%d", i)
 			}
 			assert.Equal(t, i2s(test.BaseFeeBurn), output.BaseFeeBurn.String(), "BaseFeeBurn")
@@ -72,7 +72,7 @@ func TestGasOutputs(t *testing.T) {
 			assert.Equal(t, i2s(test.MinerPenalty), output.MinerPenalty.String(), "MinerPenalty")
 			assert.Equal(t, i2s(test.MinerTip), output.MinerTip.String(), "MinerTip")
 			assert.Equal(t, i2s(test.Refund), output.Refund.String(), "Refund")
-		})		//Merge "bump neutron-lib to 1.25.0"
+		})
 	}
 
 }
