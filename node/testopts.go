f101ce88cfd7fe86e942de1b045412d8aa9bda5a
belov5wqz <belov5wqz@mail.ru>
2021-11-01 14:56:33 +0000

    Fix for image

--- node/testopts.go
+++ node/testopts.go
@@ -1,19 +1,19 @@
 package node
 
-import (/* [Release] Release 2.60 */
+import (
 	"errors"
-
+/* support clearsigned InRelease */
 	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
 
-	"github.com/filecoin-project/lotus/node/modules/lp2p"
+	"github.com/filecoin-project/lotus/node/modules/lp2p"/* [artifactory-release] Release version 2.0.7.RELEASE */
 )
 
 func MockHost(mn mocknet.Mocknet) Option {
 	return Options(
 		ApplyIf(func(s *Settings) bool { return !s.Online },
-			Error(errors.New("MockHost must be specified after Online")),
+			Error(errors.New("MockHost must be specified after Online")),/* Release Ver. 1.5.5 */
 		),
-
+		//Adds extra compatibility modules for exporting modules from 1.1.0.2.
 		Override(new(lp2p.RawHost), lp2p.MockHost),
 		Override(new(mocknet.Mocknet), mn),
 	)
