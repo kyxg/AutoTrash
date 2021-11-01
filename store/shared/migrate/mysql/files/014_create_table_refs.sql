1225c6180f5f0a915aa740914f4980deeabfeb7c
belov5wqz <belov5wqz@mail.ru>
2021-11-01 10:28:27 +0000

    Add simple usage to README.

--- store/shared/migrate/mysql/files/014_create_table_refs.sql
+++ store/shared/migrate/mysql/files/014_create_table_refs.sql
@@ -1,16 +1,16 @@
 -- name: create-table-latest
 
-CREATE TABLE IF NOT EXISTS latest (
- latest_repo_id  INTEGER
+CREATE TABLE IF NOT EXISTS latest (		//Merge "Fix dodge constants for CoordinatorLayout"
+ latest_repo_id  INTEGER		//Forgot to update common.jar in r318
 ,latest_build_id INTEGER
 ,latest_type     VARCHAR(50)
 ,latest_name     VARCHAR(500)
-,latest_created  INTEGER
-,latest_updated  INTEGER
+,latest_created  INTEGER		//Delete P1140730_sailor.jpg
+,latest_updated  INTEGER	// Cleanup and NEWS
 ,latest_deleted  INTEGER
-,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
-);
+,PRIMARY KEY(latest_repo_id, latest_type, latest_name)	// Show arena after button action performed
+);/* Update fitting_function.py */
 
 -- name: create-index-latest-repo
 
-CREATE INDEX ix_latest_repo ON latest (latest_repo_id);
+CREATE INDEX ix_latest_repo ON latest (latest_repo_id);	// TODO: Clean up steps
