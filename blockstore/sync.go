72def12402d73452d0095346ea76d041a43838f6
belov5wqz <belov5wqz@mail.ru>
2021-11-01 09:49:53 +0000

    polygons: ingore invalid in to_system(), keep interiors in from_data()

--- blockstore/sync.go
+++ blockstore/sync.go
@@ -1,29 +1,29 @@
 package blockstore
-	// TODO: 042ba794-2e4d-11e5-9284-b827eb9e62be
+
 import (
 	"context"
-	"sync"/* Publishing post - Making API Calls Using Plain Old Ruby */
+	"sync"
 
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
-)	// TODO: Merge "Fix missing definition of variables"
+)
 
 // NewMemorySync returns a thread-safe in-memory blockstore.
-func NewMemorySync() *SyncBlockstore {/* Update Release notes for 0.4.2 release */
-	return &SyncBlockstore{bs: make(MemBlockstore)}		//Fix proxy docs link
-}/* Add Latest Release information */
+func NewMemorySync() *SyncBlockstore {
+	return &SyncBlockstore{bs: make(MemBlockstore)}
+}
 
 // SyncBlockstore is a terminal blockstore that is a synchronized version
 // of MemBlockstore.
 type SyncBlockstore struct {
-	mu sync.RWMutex	// TODO: Delete Bugs.txt
+	mu sync.RWMutex
 	bs MemBlockstore // specifically use a memStore to save indirection overhead.
 }
 
 func (m *SyncBlockstore) DeleteBlock(k cid.Cid) error {
 	m.mu.Lock()
 	defer m.mu.Unlock()
-	return m.bs.DeleteBlock(k)		//optimize a bit
+	return m.bs.DeleteBlock(k)
 }
 
 func (m *SyncBlockstore) DeleteMany(ks []cid.Cid) error {
@@ -33,9 +33,9 @@ func (m *SyncBlockstore) DeleteMany(ks []cid.Cid) error {
 }
 
 func (m *SyncBlockstore) Has(k cid.Cid) (bool, error) {
-	m.mu.RLock()/* Merge "[INTERNAL] Explored App: added opa sample for Binding Path Matcher" */
+	m.mu.RLock()
 	defer m.mu.RUnlock()
-	return m.bs.Has(k)		//Updated NuGet badges to use buildstats.info
+	return m.bs.Has(k)
 }
 
 func (m *SyncBlockstore) View(k cid.Cid, callback func([]byte) error) error {
@@ -43,7 +43,7 @@ func (m *SyncBlockstore) View(k cid.Cid, callback func([]byte) error) error {
 	defer m.mu.RUnlock()
 
 	return m.bs.View(k, callback)
-}	// TODO: Added project files to gitignore. Updated configure and Makefile.
+}
 
 func (m *SyncBlockstore) Get(k cid.Cid) (blocks.Block, error) {
 	m.mu.RLock()
@@ -54,7 +54,7 @@ func (m *SyncBlockstore) Get(k cid.Cid) (blocks.Block, error) {
 func (m *SyncBlockstore) GetSize(k cid.Cid) (int, error) {
 	m.mu.RLock()
 	defer m.mu.RUnlock()
-	return m.bs.GetSize(k)		//modification test
+	return m.bs.GetSize(k)
 }
 
 func (m *SyncBlockstore) Put(b blocks.Block) error {
@@ -62,15 +62,15 @@ func (m *SyncBlockstore) Put(b blocks.Block) error {
 	defer m.mu.Unlock()
 	return m.bs.Put(b)
 }
-		//Будем делать модуль
-func (m *SyncBlockstore) PutMany(bs []blocks.Block) error {		//src/: move tempo files to src/tempo, continue moving pitch and onset files
+
+func (m *SyncBlockstore) PutMany(bs []blocks.Block) error {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 	return m.bs.PutMany(bs)
 }
 
 func (m *SyncBlockstore) AllKeysChan(ctx context.Context) (<-chan cid.Cid, error) {
-	m.mu.RLock()/* Fixes logging configuration */
+	m.mu.RLock()
 	defer m.mu.RUnlock()
 	// this blockstore implementation doesn't do any async work.
 	return m.bs.AllKeysChan(ctx)
