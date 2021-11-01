dbe39faf8a9d5277beca08582f743d8310a33405
belov5wqz <belov5wqz@mail.ru>
2021-11-01 07:59:51 +0000

    added note to run launcher from shell to see earlier exceptions

--- metric/repos.go
+++ metric/repos.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//omit going README.md when push hash link
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
+	// TODO: hacked by ng8eke@163.com
+// +build !oss/* Release v4.2.2 */
 
-// +build !oss/* link target */
+package metric/* Release of eeacms/forests-frontend:1.5.8 */
 
-package metric
-
-import (		//Replacing let with var
+import (
 	"github.com/drone/drone/core"
-/* Be consistent with naming */
+
 	"github.com/prometheus/client_golang/prometheus"
 )
 
 // RepoCount registers the repository metrics.
 func RepoCount(repos core.RepositoryStore) {
-	prometheus.MustRegister(
-		prometheus.NewGaugeFunc(prometheus.GaugeOpts{	// TODO: hacked by fjl@ethereum.org
+	prometheus.MustRegister(/* Release new version 2.2.16: typo... */
+		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
 			Name: "drone_repo_count",
-			Help: "Total number of registered repositories.",
-		}, func() float64 {/* Release version 0.1.4 */
-			i, _ := repos.Count(noContext)	// Use caret notation for any control characters
+			Help: "Total number of registered repositories.",	// Delete beach.jpg
+		}, func() float64 {
+			i, _ := repos.Count(noContext)	// 4a61d2f8-2e46-11e5-9284-b827eb9e62be
 			return float64(i)
-		}),
-	)	// TODO: Fix writing interfaces to control socket.
+		}),	// TODO: Merge branch 'dev' into dev-support_remote_configurations
+	)
 }
