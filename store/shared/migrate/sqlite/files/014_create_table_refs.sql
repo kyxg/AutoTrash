2d9205d46d868efeacdeed14e17293fb16eeecff
belov5wqz <belov5wqz@mail.ru>
2021-11-01 22:34:15 +0000

    Merge "power: bq28400: Enable charger when battery is not full"

--- store/shared/migrate/sqlite/files/014_create_table_refs.sql
+++ store/shared/migrate/sqlite/files/014_create_table_refs.sql
@@ -1,14 +1,14 @@
 -- name: create-table-latest
-		//2c6ec5c8-2e3a-11e5-938a-c03896053bdd
+
 CREATE TABLE IF NOT EXISTS latest (
  latest_repo_id  INTEGER
 ,latest_build_id INTEGER
 ,latest_type     TEXT -- branch | tag     | pull_request | promote
 ,latest_name     TEXT -- master | v1.0.0, | 42           | production
-,latest_created  INTEGER	// Fix field map bounding box
-,latest_updated  INTEGER/* Released Animate.js v0.1.3 */
+,latest_created  INTEGER
+,latest_updated  INTEGER
 ,latest_deleted  INTEGER
-,PRIMARY KEY(latest_repo_id, latest_type, latest_name)		//Bump Jade version
+,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
 );
 
 -- name: create-index-latest-repo
