2ca0d4d28161089a160b342f66e18c551dab5045
belov5wqz <belov5wqz@mail.ru>
2021-11-01 18:26:51 +0000

    magic values to constants

--- plugin/validator/combine.go
+++ plugin/validator/combine.go
@@ -1,38 +1,38 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+//		//grid utility
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by onhardev@bk.ru
+// you may not use this file except in compliance with the License./* Release ImagePicker v1.9.2 to fix Firefox v32 and v33 crash issue and */
 // You may obtain a copy of the License at
-//
+///* [artifactory-release] Release version 1.3.0.M5 */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by alan.shaw@protocol.ai
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-package validator
-
+	// Add ability to specify deployment target via argument
+package validator/* Fixed what appears to be a copy-paste error. */
+/* Resolve #20 [Release] Fix scm configuration */
 import (
 	"context"
 
 	"github.com/drone/drone/core"
 )
-
+	// TODO: PhyloViz: Delete temporary files.
 // Combine combines the conversion services, provision support
 // for multiple conversion utilities.
 func Combine(services ...core.ValidateService) core.ValidateService {
 	return &combined{services}
 }
-
+/* Automatic changelog generation for PR #56202 [ci skip] */
 type combined struct {
 	sources []core.ValidateService
 }
 
 func (c *combined) Validate(ctx context.Context, req *core.ValidateArgs) error {
-	for _, source := range c.sources {
-		if err := source.Validate(ctx, req); err != nil {
+	for _, source := range c.sources {		//ebf4f986-2e61-11e5-9284-b827eb9e62be
+		if err := source.Validate(ctx, req); err != nil {/* X Forwarding */
 			return err
 		}
 	}
