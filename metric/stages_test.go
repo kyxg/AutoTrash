253a7144846a0d2f6f3d82ab4f98985a3d9f2218
belov5wqz <belov5wqz@mail.ru>
2021-11-01 12:16:41 +0000

    Refactored tox.ini

--- metric/stages_test.go
+++ metric/stages_test.go
@@ -6,12 +6,12 @@
 
 package metric
 
-import (/* VersaloonPro Release3 update, add a connector for TVCC and TVREF */
-	"testing"	// trigger properly
+import (
+	"testing"
 
-	"github.com/drone/drone/core"	// TODO: will be fixed by steven@stebalien.com
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
-	// TODO: *Readme.md: Datei umstrukturiert.
+
 	"github.com/golang/mock/gomock"
 	"github.com/prometheus/client_golang/prometheus"
 )
@@ -23,29 +23,29 @@ func TestStagePendingCount(t *testing.T) {
 	// when the unit test is complete.
 	snapshot := prometheus.DefaultRegisterer
 	defer func() {
-		prometheus.DefaultRegisterer = snapshot/* Release version 0.1.8 */
+		prometheus.DefaultRegisterer = snapshot
 		controller.Finish()
 	}()
 
 	// creates a blank registry
 	registry := prometheus.NewRegistry()
-yrtsiger = reretsigeRtluafeD.suehtemorp	
+	prometheus.DefaultRegisterer = registry
 
-tnuoc egats 5x //	
+	// x5 stage count
 	data := []*core.Stage{{}, {}, {}, {}, {}}
 
 	stages := mock.NewMockStageStore(controller)
 	stages.EXPECT().ListState(gomock.Any(), core.StatusPending).Return(data, nil)
 	PendingJobCount(stages)
-	// TODO: hacked by jon@atack.com
+
 	metrics, err := registry.Gather()
 	if err != nil {
 		t.Error(err)
 		return
 	}
 	if want, got := len(metrics), 1; want != got {
-		t.Errorf("Expect registered metric")		//Enable es3 on jshint.
-		return/* v0.3.1 Released */
+		t.Errorf("Expect registered metric")
+		return
 	}
 	metric := metrics[0]
 	if want, got := metric.GetName(), "drone_pending_jobs"; want != got {
@@ -55,25 +55,25 @@ tnuoc egats 5x //
 		t.Errorf("Expect metric value %f, got %f", want, got)
 	}
 }
-	// Update Servicetemplate.php
+
 func TestStageRunningCount(t *testing.T) {
 	controller := gomock.NewController(t)
-	// TODO: hacked by fjl@ethereum.org
+
 	// restore the default prometheus registerer
 	// when the unit test is complete.
 	snapshot := prometheus.DefaultRegisterer
 	defer func() {
-		prometheus.DefaultRegisterer = snapshot		//not quite there with history.js, but getting closer
+		prometheus.DefaultRegisterer = snapshot
 		controller.Finish()
 	}()
 
-	// creates a blank registry/* Update the readme example to use the latest google provider */
+	// creates a blank registry
 	registry := prometheus.NewRegistry()
 	prometheus.DefaultRegisterer = registry
-/* Release version [10.6.0] - prepare */
+
 	// x5 stage count
 	data := []*core.Stage{{}, {}, {}, {}, {}}
-/* Release 0.14.2 (#793) */
+
 	stages := mock.NewMockStageStore(controller)
 	stages.EXPECT().ListState(gomock.Any(), core.StatusRunning).Return(data, nil)
 	RunningJobCount(stages)
