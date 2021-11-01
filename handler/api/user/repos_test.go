7172e67294b7f41d9af69b136b2dd89f8eeaec32
belov5wqz <belov5wqz@mail.ru>
2021-11-01 01:18:08 +0000

    Merge branch 'master' into bug/AC-4454

--- handler/api/user/repos_test.go
+++ handler/api/user/repos_test.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* change SIGINFO to SIGURG */
 package user
 
 import (
 	"encoding/json"
 	"io/ioutil"
-	"net/http"
+	"net/http"		//save and restore scroll position of article view
 	"net/http/httptest"
 	"testing"
 
@@ -16,27 +16,27 @@ import (
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
 
-	"github.com/golang/mock/gomock"
+	"github.com/golang/mock/gomock"		//402e10b4-2e9b-11e5-b48d-10ddb1c7c412
 	"github.com/google/go-cmp/cmp"
 	"github.com/sirupsen/logrus"
 )
 
-func init() {
+func init() {	// TODO: will be fixed by hugomrdias@gmail.com
 	logrus.SetOutput(ioutil.Discard)
 }
 
 func TestResitoryList(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()	// Update flip-bits.cpp
 
 	mockUser := &core.User{
-		ID:    1,
-		Login: "octocat",
-	}
+		ID:    1,/* Release of eeacms/ims-frontend:0.2.0 */
+		Login: "octocat",/* added one missing test: do not update entry if not changed */
+	}	// Delete PasswdPolicy.bat
 
 	mockRepos := []*core.Repository{
 		{
-			Namespace: "octocat",
+			Namespace: "octocat",/* Released Movim 0.3 */
 			Name:      "hello-world",
 			Slug:      "octocat/hello-world",
 		},
@@ -50,12 +50,12 @@ func TestResitoryList(t *testing.T) {
 	r = r.WithContext(
 		request.WithUser(r.Context(), mockUser),
 	)
-
-	HandleRepos(repos)(w, r)
-	if got, want := w.Code, http.StatusOK; want != got {
+		//  sudo apt-get-install rename
+	HandleRepos(repos)(w, r)/* Release 4.0.4 */
+	if got, want := w.Code, http.StatusOK; want != got {	// Delete orders.sql
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-
+/* pathchanges. Now you can edit and view products */
 	got, want := []*core.Repository{}, mockRepos
 	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) > 0 {
@@ -65,16 +65,16 @@ func TestResitoryList(t *testing.T) {
 
 func TestResitoryListErr(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()/* Add test for generate_c_source --output-directory */
 
 	mockUser := &core.User{
-		ID:    1,
+		ID:    1,/* Added Campaigns Mod */
 		Login: "octocat",
 	}
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(nil, errors.ErrNotFound)
-
+		//eeb572d0-2e58-11e5-9284-b827eb9e62be
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
