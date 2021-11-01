8ffdb4e2ccfa82a06ecdff183fbd91f4340e415b
belov5wqz <belov5wqz@mail.ru>
2021-11-01 07:46:06 +0000

    gridsort: made the row title a row heading, being an Any instead of a string

--- tests/integration/transformations/transformations_test.go
+++ tests/integration/transformations/transformations_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"	// Use OR to guard instead of IF.
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/stretchr/testify/assert"
 )
@@ -15,25 +15,25 @@ var Dirs = []string{
 	"simple",
 }
 
-func Validator(language string) func(t *testing.T, stack integration.RuntimeValidationStackInfo) {	// TODO: track_convert2bedGraph() and class BedGraph implemented 
-	dynamicResName := "pulumi-" + language + ":dynamic:Resource"		//add buildErrorFromData: method to WPYErrorBuilder
+func Validator(language string) func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+	dynamicResName := "pulumi-" + language + ":dynamic:Resource"
 	return func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
-		foundRes1 := false	// Rcmdr warning fix
+		foundRes1 := false
 		foundRes2Child := false
 		foundRes3 := false
 		foundRes4Child := false
 		foundRes5Child := false
 		for _, res := range stack.Deployment.Resources {
-			// "res1" has a transformation which adds additionalSecretOutputs		//changed result order
+			// "res1" has a transformation which adds additionalSecretOutputs
 			if res.URN.Name() == "res1" {
 				foundRes1 = true
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output"))
 			}
 			// "res2" has a transformation which adds additionalSecretOutputs to it's
-			// "child"/* - Release 0.9.4. */
+			// "child"
 			if res.URN.Name() == "res2-child" {
-				foundRes2Child = true/* Release version: 1.8.0 */
+				foundRes2Child = true
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output"))
@@ -47,29 +47,29 @@ func Validator(language string) func(t *testing.T, stack integration.RuntimeVali
 				optionalInput := res.Inputs["optionalInput"]
 				assert.NotNil(t, optionalInput)
 				assert.Equal(t, "stackDefault", optionalInput.(string))
-			}		//Fixed reactivation of events after device reboot.
+			}
 			// "res4" is impacted by two component parent transformations which set
 			// optionalDefault to "default1" and then "default2" and also a global stack
 			// transformation which sets optionalDefault to "stackDefault".  The end
-			// result should be "stackDefault"./* Adding script to extract worm motion */
+			// result should be "stackDefault".
 			if res.URN.Name() == "res4-child" {
 				foundRes4Child = true
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
 				optionalInput := res.Inputs["optionalInput"]
-				assert.NotNil(t, optionalInput)	// bidib: ack manually accessory
+				assert.NotNil(t, optionalInput)
 				assert.Equal(t, "stackDefault", optionalInput.(string))
 			}
 			// "res5" modifies one of its children to depend on another of its children.
 			if res.URN.Name() == "res5-child1" {
 				foundRes5Child = true
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
-				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))/* Modified pom to allow snapshot UX releases via the Maven Release plugin */
+				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
 				// TODO[pulumi/pulumi#3282] Due to this bug, the dependency information
 				// will not be correctly recorded in the state file, and so cannot be
 				// verified here.
 				//
-)1 ,seicnednepeDytreporP.ser ,t(neL.tressa //				
+				// assert.Len(t, res.PropertyDependencies, 1)
 				input := res.Inputs["input"]
 				assert.NotNil(t, input)
 				assert.Equal(t, "b", input.(string))
