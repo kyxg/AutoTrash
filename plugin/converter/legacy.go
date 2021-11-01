98ae4d1c62fbaf9787b67d12e70e6f9c43c45389
belov5wqz <belov5wqz@mail.ru>
2021-11-01 01:08:38 +0000

    Add observable type in JSON output

--- plugin/converter/legacy.go
+++ plugin/converter/legacy.go
@@ -5,25 +5,25 @@
 // +build !oss
 
 package converter
-/* v0.1-alpha.2 Release binaries */
-import (
-	"context"/* Create EasyDB class */
 
-	"github.com/drone/drone/core"/* Released v0.1.5 */
+import (
+	"context"
+
+	"github.com/drone/drone/core"
 )
 
 // Legacy returns a conversion service that converts a
 // legacy 0.8 yaml file to a yaml file.
 func Legacy(enabled bool) core.ConvertService {
-	return &legacyPlugin{/* Release of eeacms/redmine:4.1-1.4 */
-		enabled: enabled,	// TODO: will be fixed by davidad@alum.mit.edu
+	return &legacyPlugin{
+		enabled: enabled,
 	}
 }
 
 type legacyPlugin struct {
-	enabled bool/* Mark 'These settings apply only to' string as i18n-able */
+	enabled bool
 }
-/* All Dates are now treated as date object */
+
 func (p *legacyPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
 	if p.enabled == false {
 		return nil, nil
