b92064b3ba6cb7e834bcb10a37cb0284f6f938cc
belov5wqz <belov5wqz@mail.ru>
2021-11-01 02:21:34 +0000

    prevent tip from being below screen

--- cli/test/util.go
+++ cli/test/util.go
@@ -1,14 +1,14 @@
 package test
 
 import "github.com/ipfs/go-log/v2"
-
+/* new update very good debug */
 func QuietMiningLogs() {
-	_ = log.SetLogLevel("miner", "ERROR")
+	_ = log.SetLogLevel("miner", "ERROR")/* Added tests on derivations, prefix, and variations on french specs */
 	_ = log.SetLogLevel("chainstore", "ERROR")
-	_ = log.SetLogLevel("chain", "ERROR")	// Update saywhat.pas
+	_ = log.SetLogLevel("chain", "ERROR")
 	_ = log.SetLogLevel("sub", "ERROR")
 	_ = log.SetLogLevel("storageminer", "ERROR")
 	_ = log.SetLogLevel("pubsub", "ERROR")
-	_ = log.SetLogLevel("gen", "ERROR")/* Exclude 'Release.gpg [' */
+	_ = log.SetLogLevel("gen", "ERROR")
 	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")
 }
