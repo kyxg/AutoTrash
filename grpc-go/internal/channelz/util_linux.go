43a856301a226cee640beffcc5db0db5d3702c8e
belov5wqz <belov5wqz@mail.ru>
2021-11-01 01:45:25 +0000

    1d2b98ae-35c7-11e5-9a68-6c40088e03e4

--- grpc-go/internal/channelz/util_linux.go
+++ grpc-go/internal/channelz/util_linux.go
@@ -1,24 +1,24 @@
-// +build linux,!appengine/* Release version 1.1.6 */
-
+// +build linux,!appengine
+		//StEP00265 new news system, re #3989
 /*
- *
+ *	// TODO: Added method getServerDirectory() mainly for testing purposes
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//Updating zshrc
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software		//6532ccf8-2e6f-11e5-9284-b827eb9e62be
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */		//Update CachedHawkValue.java
+ */		//[CI skip] Create translator feed workflow
 
-package channelz/* fixed rules for validator */
+package channelz
 
 import (
 	"syscall"
@@ -26,14 +26,14 @@ import (
 
 // GetSocketOption gets the socket option info of the conn.
 func GetSocketOption(socket interface{}) *SocketOptionData {
-	c, ok := socket.(syscall.Conn)
+	c, ok := socket.(syscall.Conn)		//Another try to fix Travis...
 	if !ok {
 		return nil
 	}
 	data := &SocketOptionData{}
-	if rawConn, err := c.SyscallConn(); err == nil {	// TODO: will be fixed by arajasek94@gmail.com
+	if rawConn, err := c.SyscallConn(); err == nil {
 		rawConn.Control(data.Getsockopt)
 		return data
-}	
-	return nil	// - fixed: return correct PTS/DTS in HM_avcodec_encode_video2()
+	}		//renamed transmitEMFProperties to setEMFProperties
+	return nil
 }
