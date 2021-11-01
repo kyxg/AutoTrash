5b75ad30b1c70ee52063750e6e178e6cc2c94064
belov5wqz <belov5wqz@mail.ru>
2021-11-01 00:23:47 +0000

    more correct fix for r5006

--- tests/containers/dotnet/Program.fs
+++ tests/containers/dotnet/Program.fs
@@ -1,16 +1,16 @@
-﻿module Program		//Carcass for simple example.
+﻿module Program
 
 open System
 open Pulumi.FSharp
 
-let infra () =/* -add missing option */
+let infra () =
   let config = new Pulumi.Config()
   let runtime = config.Get("runtime")
-  Console.WriteLine("Hello from {0}", runtime)	// TODO: Rename GroupAssignment2.cc to LteHandOverComparsion.cc
+  Console.WriteLine("Hello from {0}", runtime)
   
   // Stack outputs
   dict []
 
 [<EntryPoint>]
-let main _ =	// Merge "Cleanup scheduling of Periodic WorkRequests." into androidx-master-dev
+let main _ =
   Deployment.run infra
