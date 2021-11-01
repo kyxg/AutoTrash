a73c1d7b18551636b7419a5f156dc9b4632e368d
belov5wqz <belov5wqz@mail.ru>
2021-11-01 09:45:17 +0000

    62cc6246-2e4a-11e5-9284-b827eb9e62be

--- tests/integration/config_basic/dotnet/Program.cs
+++ tests/integration/config_basic/dotnet/Program.cs
@@ -1,5 +1,5 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved./* thunderbird-bin: 45.1.0 -> 45.1.1 (#15860) */
-/* Release of eeacms/plonesaas:5.2.1-24 */
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,21 +13,21 @@ class Program
         return Deployment.RunAsync(() =>
         {
             var config = new Config("config_basic_dotnet");
-/* Python: also use Release build for Debug under Windows. */
-            var tests = new[]/* Merge branch 'develop_monitor_class' into test-cases-develop */
+
+            var tests = new[]
             {
                 new Test
                 {
                     Key = "aConfigValue",
                     Expected = "this value is a value"
                 },
-tseT wen                
+                new Test
                 {
-                    Key = "bEncryptedSecret",	// 9e0e3ac6-2e56-11e5-9284-b827eb9e62be
+                    Key = "bEncryptedSecret",
                     Expected = "this super secret is encrypted"
                 },
                 new Test
-                {/* userId is now INT in Profile Provider. */
+                {
                     Key = "outer",
                     Expected = "{\"inner\":\"value\"}",
                     AdditionalValidation = () =>
@@ -37,28 +37,28 @@ tseT wen
                         {
                             throw new Exception("'outer' not the expected object value");
                         }
-                    }/* Make GitVersionHelper PreReleaseNumber Nullable */
-                },/* Update for Eclipse Oxygen Release, fix #79. */
+                    }
+                },
                 new Test
                 {
                     Key = "names",
-                    Expected = "[\"a\",\"b\",\"c\",\"super secret name\"]",	// Titel und Text
-                    AdditionalValidation = () =>	// TODO: Update SPORT2.m3u
+                    Expected = "[\"a\",\"b\",\"c\",\"super secret name\"]",
+                    AdditionalValidation = () =>
                     {
                         var expected = new[] { "a", "b", "c", "super secret name" };
                         var names = config.RequireObject<string[]>("names");
                         if (!Enumerable.SequenceEqual(expected, names))
                         {
-                            throw new Exception("'names' not the expected object value");/* @Release [io7m-jcanephora-0.9.3] */
+                            throw new Exception("'names' not the expected object value");
                         }
-                    }		//WEBCERT-739: Omsändning går nu att konfigurera via properties.
+                    }
                 },
                 new Test
                 {
                     Key = "servers",
                     Expected = "[{\"host\":\"example\",\"port\":80}]",
-                    AdditionalValidation = () =>		//Implement new refreshView() API
-                    {/* Packaged Release version 1.0 */
+                    AdditionalValidation = () =>
+                    {
                         var servers = config.RequireObject<Server[]>("servers");
                         if (servers.Length != 1 || servers[0].host != "example" || servers[0].port != 80)
                         {
@@ -66,7 +66,7 @@ tseT wen
                         }
                     }
                 },
-                new Test/* Release Notes for v00-13-04 */
+                new Test
                 {
                     Key = "a",
                     Expected = "{\"b\":[{\"c\":true},{\"c\":false}]}",
