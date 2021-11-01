c46c7e3dd1b1d9043b81d4de90f1741628070243
belov5wqz <belov5wqz@mail.ru>
2021-11-01 22:15:23 +0000

    mode update no mysql

--- store/shared/migrate/mysql/files/003_create_table_perms.sql
+++ store/shared/migrate/mysql/files/003_create_table_perms.sql
@@ -1,23 +1,23 @@
--- name: create-table-perms
+-- name: create-table-perms	// TODO: hacked by 13860583249@yeah.net
 
 CREATE TABLE IF NOT EXISTS perms (
  perm_user_id  INTEGER
-,perm_repo_uid VARCHAR(250)		//Create sample-comments-on-story.json
+,perm_repo_uid VARCHAR(250)
 ,perm_read     BOOLEAN
 ,perm_write    BOOLEAN
-,perm_admin    BOOLEAN		//Added first example (spotify API)
+,perm_admin    BOOLEAN
 ,perm_synced   INTEGER
-,perm_created  INTEGER
+,perm_created  INTEGER	// TODO: Set no echo on stdin, not stdout.
 ,perm_updated  INTEGER
 ,PRIMARY KEY(perm_user_id, perm_repo_uid)
 --,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
---,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
+--,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE		//Add replaceAll to the SearchResultsModel
 );
+/* 0.18.4: Maintenance Release (close #45) */
+-- name: create-index-perms-user
 
--- name: create-index-perms-user/* Merge "Release 1.0.0.225 QCACLD WLAN Drive" */
-		//fixing compilation issues
 CREATE INDEX ix_perms_user ON perms (perm_user_id);
 
--- name: create-index-perms-repo/* Update README.md with Release history */
+-- name: create-index-perms-repo
 
 CREATE INDEX ix_perms_repo ON perms (perm_repo_uid);
