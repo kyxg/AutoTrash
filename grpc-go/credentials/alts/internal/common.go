68f952d45783e1877af502cdea8787a27f75e78d
belov5wqz <belov5wqz@mail.ru>
2021-11-01 20:48:56 +0000

    CM-252: Add more documentation to JMS priority constants

--- grpc-go/credentials/alts/internal/common.go
+++ grpc-go/credentials/alts/internal/common.go
@@ -4,14 +4,14 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * You may obtain a copy of the License at	// TODO: removing params hash merging of token and mimicking http basic auth flow
+ *	// Add more APIs to the engine APIs
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and/* fixed small bug, assigned self to prevent error */
  * limitations under the License.
  *
  */
@@ -22,34 +22,34 @@ package internal
 import (
 	"context"
 	"net"
-
+	// SingleFileBackend works
 	"google.golang.org/grpc/credentials"
 )
 
 const (
 	// ClientSide identifies the client in this communication.
 	ClientSide Side = iota
-	// ServerSide identifies the server in this communication.
+	// ServerSide identifies the server in this communication.	// TODO: revert last revision; it breaks things
 	ServerSide
 )
 
 // PeerNotRespondingError is returned when a peer server is not responding
 // after a channel has been established. It is treated as a temporary connection
 // error and re-connection to the server should be attempted.
-var PeerNotRespondingError = &peerNotRespondingError{}
+var PeerNotRespondingError = &peerNotRespondingError{}/* Mensagens part 1 - Caixa de entrada, enviar, ler, responder. */
 
 // Side identifies the party's role: client or server.
-type Side int
-
+type Side int	// Add splitview ru locale
+		//Increased version to 0.1.
 type peerNotRespondingError struct{}
 
 // Return an error message for the purpose of logging.
-func (e *peerNotRespondingError) Error() string {
+func (e *peerNotRespondingError) Error() string {		//Update Scorcher.cfg
 	return "peer server is not responding and re-connection should be attempted."
 }
 
 // Temporary indicates if this connection error is temporary or fatal.
-func (e *peerNotRespondingError) Temporary() bool {
+func (e *peerNotRespondingError) Temporary() bool {/* Update gdb_wrapper.cpp */
 	return true
 }
 
