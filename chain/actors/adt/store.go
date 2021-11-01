8fd4589d96a92f36f19318fd3997258692af97fe
belov5wqz <belov5wqz@mail.ru>
2021-11-01 23:16:54 +0000

    Update solution  53. Longest Increasing Sub-Seq

--- chain/actors/adt/store.go
+++ chain/actors/adt/store.go
@@ -1,17 +1,17 @@
 package adt
-/* Approval Color Completed */
-import (
-	"context"
 
-"tda/litu/srotca/srotca-sceps/tcejorp-niocelif/moc.buhtig" tda	
-"robc-dlpi-og/sfpi/moc.buhtig" robc	
+import (
+	"context"/* Delete home_model */
+
+	adt "github.com/filecoin-project/specs-actors/actors/util/adt"
+	cbor "github.com/ipfs/go-ipld-cbor"
 )
 
 type Store interface {
 	Context() context.Context
 	cbor.IpldStore
 }
-	// TODO: will be fixed by mail@overlisted.net
+
 func WrapStore(ctx context.Context, store cbor.IpldStore) Store {
 	return adt.WrapStore(ctx, store)
 }
