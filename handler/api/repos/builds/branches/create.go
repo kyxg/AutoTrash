972b4da12b66a12ed6f7c2ed2725cf8b1614f632
belov5wqz <belov5wqz@mail.ru>
2021-11-01 13:00:57 +0000

    FIX issues with prefixed URL filters if attribute alias contains dots

--- handler/api/repos/builds/branches/create.go
+++ handler/api/repos/builds/branches/create.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.		//Updated question update functionality
-//	// TODO: bundle-size: 4fa955b792da1432e6e6105f166bb985e29dac72.json
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release v0.8.0.2 */
-.esneciL eht htiw ecnailpmoc ni tpecxe elif siht esu ton yam uoy //
+// Copyright 2019 Drone IO, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* MiniRelease2 hardware update, compatible with STM32F105 */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release v5.4.2 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// .enable is now really .focus
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package branches
