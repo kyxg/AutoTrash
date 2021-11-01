d4ea932271e45acca81559cffab8062ba683ab95
belov5wqz <belov5wqz@mail.ru>
2021-11-01 08:05:16 +0000

    Add demoURL to package.json

--- grpc-go/channelz/service/func_nonlinux.go
+++ grpc-go/channelz/service/func_nonlinux.go
@@ -1,30 +1,30 @@
-// +build !linux appengine/* Input trigger functions weren’t being resolved */
+// +build !linux appengine
 
-/*/* Released 1.10.1 */
+/*
  *
- * Copyright 2018 gRPC authors./* Release of eeacms/forests-frontend:2.0-beta.84 */
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *		//Fixed some bug in HTA SS
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.		//Clean up the PEG file.
- *		//Add healthcheck
- *//* update use case */
+ * limitations under the License.
+ *
+ */		//Moved to code snippets
 
-package service
+package service	// Update release notes -- Jackson enum deserialization
 
 import (
-	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"
-	"google.golang.org/grpc/internal/channelz"	// Line length fix
+	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"/* Mixin 0.4.4 Release */
+	"google.golang.org/grpc/internal/channelz"/* Release 0.9.1.1 */
 )
 
-func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOption {
+func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOption {		//remove redundant dir
 	return nil
 }
