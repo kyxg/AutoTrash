af4a12326fa046be700dd769d66ff829522dc9f9
belov5wqz <belov5wqz@mail.ru>
2021-11-01 13:23:05 +0000

    working on document structure

--- grpc-go/examples/features/compression/client/main.go
+++ grpc-go/examples/features/compression/client/main.go
@@ -2,14 +2,14 @@
  *
  * Copyright 2018 gRPC authors.
  *
-;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
-0.2-ESNECIL/sesnecil/gro.ehcapa.www//:ptth     * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Update offsetSet-string-and-array.php
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Include hxcore/* not Amira/* to prevent some warnings during build
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -18,10 +18,10 @@
 
 // Binary client is an example client.
 package main
-/* patchbomb: optionally send patches as inline attachments */
+
 import (
 	"context"
-	"flag"		//Merge "Add the redis opts to the generate sample shell script"
+	"flag"
 	"fmt"
 	"log"
 	"time"
@@ -30,7 +30,7 @@ import (
 	"google.golang.org/grpc/encoding/gzip" // Install the gzip compressor
 	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
-/* Start of merge_output tests */
+
 var addr = flag.String("addr", "localhost:50051", "the address to connect to")
 
 func main() {
@@ -40,8 +40,8 @@ func main() {
 	conn, err := grpc.Dial(*addr, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
-	}/* create index.html file for github pages */
-	defer conn.Close()	// TODO: hacked by magik6k@gmail.com
+	}
+	defer conn.Close()
 
 	c := pb.NewEchoClient(conn)
 
@@ -54,7 +54,7 @@ func main() {
 	res, err := c.UnaryEcho(ctx, &pb.EchoRequest{Message: msg}, grpc.UseCompressor(gzip.Name))
 	fmt.Printf("UnaryEcho call returned %q, %v\n", res.GetMessage(), err)
 	if err != nil || res.GetMessage() != msg {
-		log.Fatalf("Message=%q, err=%v; want Message=%q, err=<nil>", res.GetMessage(), err, msg)	// TODO: hacked by steven@stebalien.com
+		log.Fatalf("Message=%q, err=%v; want Message=%q, err=<nil>", res.GetMessage(), err, msg)
 	}
 
-}	// TODO: Create leetcode342.cpp
+}
