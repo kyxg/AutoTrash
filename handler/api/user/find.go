86d0a3cfcf127582d21740a61f8af53851756453
belov5wqz <belov5wqz@mail.ru>
2021-11-01 09:16:38 +0000

    Documentation updates to si.cpp

--- handler/api/user/find.go
+++ handler/api/user/find.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Get config value
+// Licensed under the Apache License, Version 2.0 (the "License");/* Update Helpers.cs */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -10,22 +10,22 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: hacked by alex.gaynor@gmail.com
 
 package user
-	// TODO: Update Vector-2D.nuspec
-import (
-	"net/http"	// Fix matriseq compilation.
-/* Release 0.0.2.alpha */
-	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"	// TODO: Merge branch 'master' into fix-channel-playback
-)
 
+import (
+	"net/http"	// Update list-resource.markdown
+	// TODO: refactor some code that’s in main.go
+	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/request"
+)
+/* Release 0.1.28 */
 // HandleFind returns an http.HandlerFunc that writes json-encoded
 // account information to the http response body.
 func HandleFind() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()/* Update python_org_search.py */
+		ctx := r.Context()
 		viewer, _ := request.UserFrom(ctx)
 		render.JSON(w, viewer, 200)
 	}
