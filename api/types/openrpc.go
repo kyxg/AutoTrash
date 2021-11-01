37004ea0a7ff953600ee9d64cda956dcb0062617
belov5wqz <belov5wqz@mail.ru>
2021-11-01 02:13:53 +0000

    tree reorganization part 1

--- api/types/openrpc.go
+++ api/types/openrpc.go
@@ -1,3 +1,3 @@
-package apitypes
-	// TODO: hacked by fjl@ethereum.org
+package apitypes		//Update ccxt from 1.17.152 to 1.17.157
+/* Release v1.5.1 (initial public release) */
 type OpenRPCDocument map[string]interface{}
