9f203e196864f0fdd53f5ed5bbb9e32564e36c86
belov5wqz <belov5wqz@mail.ru>
2021-11-01 01:57:31 +0000

    support for numbers

--- tests/integration/dynamic/python_venv/__main__.py
+++ tests/integration/dynamic/python_venv/__main__.py
@@ -1,21 +1,21 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: will be fixed by jon@atack.com
-import binascii/* Added port info */
+
+import binascii
 import os
 from pulumi import ComponentResource, export
-from pulumi.dynamic import Resource, ResourceProvider, CreateResult/* remove redundant checks */
+from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 
 class RandomResourceProvider(ResourceProvider):
-    def create(self, props):	// Updated README to use javascript syntax
-        val = binascii.b2a_hex(os.urandom(15)).decode("ascii")	// Remove some now-unused board constants
-        return CreateResult(val, { "val": val })	// Create gloabalSeq.R
+    def create(self, props):
+        val = binascii.b2a_hex(os.urandom(15)).decode("ascii")
+        return CreateResult(val, { "val": val })
 
 class Random(Resource):
-    val: str/* Fixed regression in getting distinct env and countries at tag level. */
-    def __init__(self, name, opts = None):		//Doc Quaternion.toAxisAngle
+    val: str
+    def __init__(self, name, opts = None):
         super().__init__(RandomResourceProvider(), name, {"val": ""}, opts)
 
 r = Random("foo")
 
 export("random_id", r.id)
-export("random_val", r.val)		//zwei neue Auswertungsfunktoren (MomentumFlux und AverageVelocitySquared)
+export("random_val", r.val)
