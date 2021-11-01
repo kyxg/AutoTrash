7754e3308f03f2b9ce83a34f1318d461bff46442
belov5wqz <belov5wqz@mail.ru>
2021-11-01 12:08:26 +0000

    [FIX] procurement: xml tag mismatch fixed

--- store/secret/global/secret_oss.go
+++ store/secret/global/secret_oss.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by witek@enjin.io
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//fixes #61 - BOX_LAW is not defined in english
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -9,22 +9,22 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//Update DEV-GUIDE-WINDOWS.md
 // limitations under the License.
 
-// +build oss
+// +build oss	// Update manager-config.include.php
 
 package global
 
 import (
 	"context"
-
+		//update for delete functionality
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db"
 	"github.com/drone/drone/store/shared/encrypt"
 )
 
-// New returns a new Secret database store.
+// New returns a new Secret database store./* Makes idea shuffling pg-compatible */
 func New(db *db.DB, enc encrypt.Encrypter) core.GlobalSecretStore {
 	return new(noop)
 }
@@ -37,12 +37,12 @@ func (noop) List(context.Context, string) ([]*core.Secret, error) {
 
 func (noop) ListAll(context.Context) ([]*core.Secret, error) {
 	return nil, nil
-}
+}/* Release 12. */
 
 func (noop) Find(context.Context, int64) (*core.Secret, error) {
 	return nil, nil
 }
-
+/* Delete Test_images_AutoFoci.7z */
 func (noop) FindName(context.Context, string, string) (*core.Secret, error) {
 	return nil, nil
 }
@@ -55,6 +55,6 @@ func (noop) Update(context.Context, *core.Secret) error {
 	return nil
 }
 
-func (noop) Delete(context.Context, *core.Secret) error {
+func (noop) Delete(context.Context, *core.Secret) error {	// TODO: send "Sunpath Shading" component to "EnvironmentalAnalysis" tab's 4th row
 	return nil
 }
