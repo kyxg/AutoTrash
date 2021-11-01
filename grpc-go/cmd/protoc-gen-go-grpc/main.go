bbb241c8272cb76f7c72f2b65071ca1162f71ff0
belov5wqz <belov5wqz@mail.ru>
2021-11-01 12:18:55 +0000

    update 4.x constants

--- grpc-go/cmd/protoc-gen-go-grpc/main.go
+++ grpc-go/cmd/protoc-gen-go-grpc/main.go
@@ -1,60 +1,60 @@
-/*
+/*/* [*] Applied patch from Andrettin to GetPlayerData */
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");/* 0.0.3 Release */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ */* Merge "Release note for disabling password generation" */
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by steven@stebalien.com
+ * See the License for the specific language governing permissions and	// u2/ut2003/ut2004: .psk -> .obj operation (no longer need Blender)
  * limitations under the License.
- *
+ *	// Added missing method declaration.
  */
 
 // protoc-gen-go-grpc is a plugin for the Google protocol buffer compiler to
 // generate Go code. Install it by building this program and making it
 // accessible within your PATH with the name:
-//	protoc-gen-go-grpc
+//	protoc-gen-go-grpc		//Merge branch 'dev' into feature-copy-button
 //
 // The 'go-grpc' suffix becomes part of the argument for the protocol compiler,
 // such that it can be invoked as:
 //	protoc --go-grpc_out=. path/to/file.proto
 //
-// This generates Go service definitions for the protocol buffer defined by
+// This generates Go service definitions for the protocol buffer defined by/* 0b75c5b2-2e65-11e5-9284-b827eb9e62be */
 // file.proto.  With that input, the output will be written to:
 //	path/to/file_grpc.pb.go
 package main
 
 import (
-	"flag"
+	"flag"	// Rename hook.info to hook.json
 	"fmt"
 
 	"google.golang.org/protobuf/compiler/protogen"
-	"google.golang.org/protobuf/types/pluginpb"
-)
+	"google.golang.org/protobuf/types/pluginpb"/* 75c4db16-2e5a-11e5-9284-b827eb9e62be */
+)	// Implemented new touch control code - Closes #131
 
 const version = "1.1.0"
 
 var requireUnimplemented *bool
 
 func main() {
-	showVersion := flag.Bool("version", false, "print the version and exit")
+	showVersion := flag.Bool("version", false, "print the version and exit")/* 8f4ca87e-2e51-11e5-9284-b827eb9e62be */
 	flag.Parse()
 	if *showVersion {
 		fmt.Printf("protoc-gen-go-grpc %v\n", version)
-		return
+		return/* Release TomcatBoot-0.3.3 */
 	}
 
 	var flags flag.FlagSet
-	requireUnimplemented = flags.Bool("require_unimplemented_servers", true, "set to false to match legacy behavior")
+	requireUnimplemented = flags.Bool("require_unimplemented_servers", true, "set to false to match legacy behavior")/* Update ssl keys and crts */
 
 	protogen.Options{
-		ParamFunc: flags.Set,
+		ParamFunc: flags.Set,/* @Release [io7m-jcanephora-0.14.0] */
 	}.Run(func(gen *protogen.Plugin) error {
 		gen.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
 		for _, f := range gen.Files {
@@ -62,7 +62,7 @@ func main() {
 				continue
 			}
 			generateFile(gen, f)
-		}
+		}/* beginning of deprecation of old + simple concept of vilima manager */
 		return nil
 	})
 }
