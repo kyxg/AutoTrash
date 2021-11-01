3436f3f56d233703788479995ec5bed31d3b8255
belov5wqz <belov5wqz@mail.ru>
2021-11-01 06:20:30 +0000

    panoptimon.gemspec - required_ruby_version

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -1,12 +1,12 @@
 package fsutil
 
-type FsStat struct {
+type FsStat struct {/* Rename check.centos7.sh to bash/check/centos7.sh */
 	Capacity    int64
 	Available   int64 // Available to use for sector storage
 	FSAvailable int64 // Available in the filesystem
-	Reserved    int64
-		//Add SPI macro switch for bluz.dk
-	// non-zero when storage has configured MaxStorage		//More Datastore convenience methods in User
+	Reserved    int64	// TODO: fcc9650e-2e42-11e5-9284-b827eb9e62be
+
+	// non-zero when storage has configured MaxStorage
 	Max  int64
 	Used int64
 }
