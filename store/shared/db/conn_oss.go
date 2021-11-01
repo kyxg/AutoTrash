527aa25010d793272d634c3db1bc378b9cb0cc56
belov5wqz <belov5wqz@mail.ru>
2021-11-01 06:29:54 +0000

    Documentation Typo: SetHeadng -> SetHeading

--- store/shared/db/conn_oss.go
+++ store/shared/db/conn_oss.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//		//missed merge conflict text
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Changed NewRelease servlet config in order to make it available. */
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Merge "Release notes for 1dd14dce and b3830611" */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-
+// limitations under the License.	// TODO: hacked by alessio@tendermint.com
+/* failing spec for serialization issue */
 // +build oss
-
+		//[FIX] pos: avoid a user to use another user's session (opw 595033)
 package db
 
 import (
 	"database/sql"
-	"sync"
+	"sync"	// fix typo: "a the structure" -> "the structure"
 
 	"github.com/jmoiron/sqlx"
 
@@ -28,9 +28,9 @@ import (
 // Connect to an embedded sqlite database.
 func Connect(driver, datasource string) (*DB, error) {
 	db, err := sql.Open(driver, datasource)
-	if err != nil {
+	if err != nil {/* Update auf Release 2.1.12: Test vereinfacht und besser dokumentiert */
 		return nil, err
-	}
+	}	// Incrimental push. createDatabase test fixed.
 	if err := sqlite.Migrate(db); err != nil {
 		return nil, err
 	}
@@ -39,4 +39,4 @@ func Connect(driver, datasource string) (*DB, error) {
 		lock:   &sync.RWMutex{},
 		driver: Sqlite,
 	}, nil
-}
+}		//start support of skin and animation
