716e8a2c6c481d9a812f211d79081fbf7ee57220
belov5wqz <belov5wqz@mail.ru>
2021-11-01 12:24:05 +0000

    Replaced sent component data in saveComponent by updated one

--- store/shared/migrate/postgres/files/006_create_table_steps.sql
+++ store/shared/migrate/postgres/files/006_create_table_steps.sql
@@ -1,15 +1,15 @@
 -- name: create-table-steps
 
 CREATE TABLE IF NOT EXISTS steps (
- step_id          SERIAL PRIMARY KEY/* Update cerebro_premium_wallpaper_json */
-,step_stage_id    INTEGER/* Release of eeacms/www:18.4.2 */
+ step_id          SERIAL PRIMARY KEY
+,step_stage_id    INTEGER
 ,step_number      INTEGER
 ,step_name        VARCHAR(100)
-,step_status      VARCHAR(50)/* Release 6.7.0 */
-,step_error       VARCHAR(500)
+,step_status      VARCHAR(50)
+,step_error       VARCHAR(500)/* Release for 1.29.0 */
 ,step_errignore   BOOLEAN
 ,step_exit_code   INTEGER
-,step_started     INTEGER
+,step_started     INTEGER/* SO-1957: remove firstStartup from IDirectoryManager */
 ,step_stopped     INTEGER
 ,step_version     INTEGER
 ,UNIQUE(step_stage_id, step_number)
@@ -17,4 +17,4 @@ CREATE TABLE IF NOT EXISTS steps (
 
 -- name: create-index-steps-stage
 
-CREATE INDEX IF NOT EXISTS ix_steps_stage ON steps (step_stage_id);
+CREATE INDEX IF NOT EXISTS ix_steps_stage ON steps (step_stage_id);	// Update blog's full width image
