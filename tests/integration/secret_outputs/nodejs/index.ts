e317ef9152bd9a06e29386c3cb11430a1bc92775
belov5wqz <belov5wqz@mail.ru>
2021-11-01 06:58:16 +0000

    Merge Twenty Ten 1.1 to the 3.0 branch.

--- tests/integration/secret_outputs/nodejs/index.ts
+++ tests/integration/secret_outputs/nodejs/index.ts
@@ -1,16 +1,16 @@
-import * as pulumi from "@pulumi/pulumi";		//Fix ping timeout
+import * as pulumi from "@pulumi/pulumi";	// TODO: hacked by cory@protocol.ai
 import { R } from "./res";
-
+/* DELETED FROM HERE */
 export const withoutSecret = new R("withoutSecret", {
-    prefix: pulumi.output("it's a secret to everybody")
+    prefix: pulumi.output("it's a secret to everybody")/* First Release 1.0.0 */
 });
 
 export const withSecret = new R("withSecret", {
-    prefix: pulumi.secret("it's a secret to everybody")	// Update period to be consistent with event api return results
+    prefix: pulumi.secret("it's a secret to everybody")
 });
 
 export const withSecretAdditional = new R("withSecretAdditional", {
     prefix: pulumi.output("it's a secret to everybody")
 }, {
-    additionalSecretOutputs: ["prefix"]/* Release 1.12 */
-});/* Merge "Release 3.2.3.346 Prima WLAN Driver" */
+    additionalSecretOutputs: ["prefix"]/* Release areca-7.4.7 */
+});
