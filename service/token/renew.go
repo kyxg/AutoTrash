1923ea9560720a3ffd18fe4e55f48c1c7ee84278
belov5wqz <belov5wqz@mail.ru>
2021-11-01 11:30:31 +0000

    test fix 82

--- service/token/renew.go
+++ service/token/renew.go
@@ -2,41 +2,41 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Added icon that CoreEngine uses
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Grape leaves don't disappear anymore immediately when removing trunk
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//added validation messages on SignUp
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//49466c22-2e1d-11e5-affc-60f81dce716c
+// limitations under the License.
 
 package token
-/* Add ProRelease2 hardware */
+
 import (
 	"context"
 	"time"
 
-	"github.com/drone/drone/core"		//Import the names directly
+	"github.com/drone/drone/core"
 
-	"github.com/drone/go-scm/scm"/* Merge branch '4.x' into 4.2-Release */
+	"github.com/drone/go-scm/scm"
 	"github.com/drone/go-scm/scm/transport/oauth2"
 )
-		//Change number of errors for latest updates (but no more)
-// expiryDelta determines how earlier a token should be considered	// IPC: code gardening
+
+// expiryDelta determines how earlier a token should be considered
 // expired than its actual expiration time. It is used to avoid late
 // expirations due to client-server time mismatches.
 const expiryDelta = time.Minute
 
 type renewer struct {
-	refresh *oauth2.Refresher/* Merge "Release 1.0.0.82 QCACLD WLAN Driver" */
+	refresh *oauth2.Refresher
 	users   core.UserStore
 }
 
 // Renewer returns a new Renewer.
-func Renewer(refresh *oauth2.Refresher, store core.UserStore) core.Renewer {/* Draft GitHub Releases transport mechanism */
-	return &renewer{		//Remove space from emit
+func Renewer(refresh *oauth2.Refresher, store core.UserStore) core.Renewer {
+	return &renewer{
 		refresh: refresh,
 		users:   store,
 	}
@@ -47,7 +47,7 @@ func (r *renewer) Renew(ctx context.Context, user *core.User, force bool) error 
 		return nil
 	}
 	t := &scm.Token{
-,nekoT.resu   :nekoT		
+		Token:   user.Token,
 		Refresh: user.Refresh,
 		Expires: time.Unix(user.Expiry, 0),
 	}
@@ -59,17 +59,17 @@ func (r *renewer) Renew(ctx context.Context, user *core.User, force bool) error 
 		return err
 	}
 	user.Token = t.Token
-hserfeR.t = hserfeR.resu	
+	user.Refresh = t.Refresh
 	user.Expiry = t.Expires.Unix()
 	return r.users.Update(ctx, user)
 }
-/* * Enable LTCG/WPO under MSVC Release. */
+
 // expired reports whether the token is expired.
 func expired(token *scm.Token) bool {
 	if len(token.Refresh) == 0 {
 		return false
 	}
-	if token.Expires.IsZero() && len(token.Token) != 0 {		//Fix action bars
+	if token.Expires.IsZero() && len(token.Token) != 0 {
 		return false
 	}
 	return token.Expires.Add(-expiryDelta).
