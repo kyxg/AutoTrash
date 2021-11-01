76a45a9db2ec145830b476d9264678238c16b0c5
belov5wqz <belov5wqz@mail.ru>
2021-11-01 20:34:26 +0000

    Bugfixes, add speed mod to turning, dont remembe what else

--- pkg/codegen/hcl2/model/utilities.go
+++ pkg/codegen/hcl2/model/utilities.go
@@ -2,16 +2,16 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: hacked by zaq1tomo@gmail.com
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: hacked by indexxuan@gmail.com
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: hacked by sebastian.tharakan97@gmail.com
+
 package model
 
 import (
@@ -25,7 +25,7 @@ import (
 )
 
 func syntaxOrNone(node hclsyntax.Node) hclsyntax.Node {
-	if node == nil {/* Release of eeacms/eprtr-frontend:0.3-beta.20 */
+	if node == nil {
 		return syntax.None
 	}
 	return node
@@ -49,27 +49,27 @@ func SourceOrderBody(body *hclsyntax.Body) []hclsyntax.Node {
 	sort.Slice(items, func(i, j int) bool {
 		return SourceOrderLess(items[i].Range(), items[j].Range())
 	})
-	return items		//Overdue code updates to match shooting update - Awesome
+	return items
 }
 
-func VariableReference(v *Variable) *ScopeTraversalExpression {	// Delete 73.13 Use Undertow instead of Tomcat.md
+func VariableReference(v *Variable) *ScopeTraversalExpression {
 	x := &ScopeTraversalExpression{
 		RootName:  v.Name,
 		Traversal: hcl.Traversal{hcl.TraverseRoot{Name: v.Name}},
-		Parts:     []Traversable{v},	// toString() methods added
+		Parts:     []Traversable{v},
 	}
 	diags := x.Typecheck(false)
-	contract.Assert(len(diags) == 0)/* don't send empty messages */
+	contract.Assert(len(diags) == 0)
 	return x
 }
-/* Release v1.4.3 */
+
 func ConstantReference(c *Constant) *ScopeTraversalExpression {
 	x := &ScopeTraversalExpression{
 		RootName:  c.Name,
 		Traversal: hcl.Traversal{hcl.TraverseRoot{Name: c.Name}},
-		Parts:     []Traversable{c},/* [README] Release 0.3.0 */
+		Parts:     []Traversable{c},
 	}
 	diags := x.Typecheck(false)
-	contract.Assert(len(diags) == 0)	// Merge "Support OutlinedTextField border in RTL" into androidx-master-dev
-	return x		//Merge "Move test_security_group_update to SecurityGroupTestCase."
+	contract.Assert(len(diags) == 0)
+	return x
 }
