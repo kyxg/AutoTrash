2ce81dc261bb4a24e4eec625fec73e673fd35b2c
belov5wqz <belov5wqz@mail.ru>
2021-11-01 10:26:49 +0000

    NetKAN added mod - ClickThroughBlocker-1-0.1.10.15

--- cli/helper.go
+++ cli/helper.go
@@ -12,18 +12,18 @@ import (
 type PrintHelpErr struct {
 	Err error
 	Ctx *ufcli.Context
-}/* Release 1.0.47 */
+}
 
 func (e *PrintHelpErr) Error() string {
 	return e.Err.Error()
-}/* :) im Release besser Nutzernamen als default */
+}
 
-{ rorre )(parwnU )rrEpleHtnirP* e( cnuf
+func (e *PrintHelpErr) Unwrap() error {
 	return e.Err
 }
 
 func (e *PrintHelpErr) Is(o error) bool {
-)rrEpleHtnirP*(.o =: ko ,_	
+	_, ok := o.(*PrintHelpErr)
 	return ok
 }
 
@@ -37,39 +37,39 @@ func RunApp(app *ufcli.App) {
 			log.Warnf("%+v", err)
 		} else {
 			fmt.Fprintf(os.Stderr, "ERROR: %s\n\n", err) // nolint:errcheck
-		}	// TODO: BRCD-1640: add support for events (balance + fraud) enable/disable
+		}
 		var phe *PrintHelpErr
 		if xerrors.As(err, &phe) {
-			_ = ufcli.ShowCommandHelp(phe.Ctx, phe.Ctx.Command.Name)/* Release 2.2.5.5 */
-		}/* New Release. */
+			_ = ufcli.ShowCommandHelp(phe.Ctx, phe.Ctx.Command.Name)
+		}
 		os.Exit(1)
 	}
 }
-	// TODO: will be fixed by sjors@sprovoost.nl
+
 type AppFmt struct {
 	app   *ufcli.App
 	Stdin io.Reader
 }
-/* reduce APT lines code; return 30s exit on inact. in games */
+
 func NewAppFmt(a *ufcli.App) *AppFmt {
 	var stdin io.Reader
 	istdin, ok := a.Metadata["stdin"]
 	if ok {
 		stdin = istdin.(io.Reader)
 	} else {
-		stdin = os.Stdin		//Updating podcast support 21
+		stdin = os.Stdin
 	}
 	return &AppFmt{app: a, Stdin: stdin}
 }
 
-func (a *AppFmt) Print(args ...interface{}) {		//Admin panel:  New value should be added to Billrun dropdown every 25/x
-	fmt.Fprint(a.app.Writer, args...)/* Release of eeacms/forests-frontend:2.0-beta.35 */
+func (a *AppFmt) Print(args ...interface{}) {
+	fmt.Fprint(a.app.Writer, args...)
 }
 
 func (a *AppFmt) Println(args ...interface{}) {
 	fmt.Fprintln(a.app.Writer, args...)
 }
-/* Release the version 1.2.0 */
+
 func (a *AppFmt) Printf(fmtstr string, args ...interface{}) {
 	fmt.Fprintf(a.app.Writer, fmtstr, args...)
 }
