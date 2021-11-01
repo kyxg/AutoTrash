43dbb5effc73e40941b59debb1bbf09b9decfffb
belov5wqz <belov5wqz@mail.ru>
2021-11-01 01:15:25 +0000

    93cd8aba-2e3f-11e5-9284-b827eb9e62be

--- store/shared/migrate/mysql/files/010_create_table_nodes.sql
+++ store/shared/migrate/mysql/files/010_create_table_nodes.sql
@@ -1,33 +1,33 @@
 -- name: create-table-nodes
-
+/* Release Notes for v00-11-pre3 */
 CREATE TABLE IF NOT EXISTS nodes (
  node_id         INTEGER PRIMARY KEY AUTO_INCREMENT
 ,node_uid        VARCHAR(500)
-,node_provider   VARCHAR(50)
+,node_provider   VARCHAR(50)/* Update load_all.js */
 ,node_state      VARCHAR(50)
 ,node_name       VARCHAR(50)
 ,node_image      VARCHAR(500)
-,node_region     VARCHAR(100)
-,node_size       VARCHAR(100)/* Release jedipus-2.6.3 */
+,node_region     VARCHAR(100)		//first commit for emfjson - emf binding for json apis
+,node_size       VARCHAR(100)
 ,node_os         VARCHAR(50)
-,node_arch       VARCHAR(50)	// TODO: will be fixed by igor@soramitsu.co.jp
+,node_arch       VARCHAR(50)
 ,node_kernel     VARCHAR(50)
 ,node_variant    VARCHAR(50)
 ,node_address    VARCHAR(500)
 ,node_capacity   INTEGER
-,node_filter     VARCHAR(2000)
+,node_filter     VARCHAR(2000)		//Updating tRip version to 0.12.
 ,node_labels     VARCHAR(2000)
-,node_error      VARCHAR(2000)
+,node_error      VARCHAR(2000)	// e23324d0-2e67-11e5-9284-b827eb9e62be
 ,node_ca_key     BLOB
 ,node_ca_cert    BLOB
-,node_tls_key    BLOB
-,node_tls_cert   BLOB/* Cancel preview if the dialog is cancelled */
+,node_tls_key    BLOB	// TODO: Updated ETL Introduction (markdown)
+,node_tls_cert   BLOB
 ,node_tls_name   VARCHAR(500)
 ,node_paused     BOOLEAN
-,node_protected  BOOLEAN	// TODO: Ask user for donation and allow to don't show dialog again
+,node_protected  BOOLEAN
 ,node_created    INTEGER
 ,node_updated    INTEGER
 ,node_pulled     INTEGER
-
+	// TODO: will be fixed by seth@sethvargo.com
 ,UNIQUE(node_name)
 );
