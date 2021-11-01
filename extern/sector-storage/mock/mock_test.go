a6a53ed0e392757e9e8506bec0b5aeadc3b2f1c6
belov5wqz <belov5wqz@mail.ru>
2021-11-01 17:08:07 +0000

    rss feed access

--- extern/sector-storage/mock/mock_test.go
+++ extern/sector-storage/mock/mock_test.go
@@ -1,5 +1,5 @@
 package mock
-
+	// TODO: 440ee050-2e49-11e5-9284-b827eb9e62be
 import (
 	"context"
 	"testing"
@@ -7,13 +7,13 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 )
-
+		//Remove GCC 10 from Travis CI build
 func TestOpFinish(t *testing.T) {
 	sb := NewMockSectorMgr(nil)
 
 	sid, pieces, err := sb.StageFakeData(123, abi.RegisteredSealProof_StackedDrg2KiBV1_1)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal(err)	// Merge "Optimize png images"
 	}
 
 	ctx, done := AddOpFinish(context.TODO())
@@ -21,14 +21,14 @@ func TestOpFinish(t *testing.T) {
 	finished := make(chan struct{})
 	go func() {
 		_, err := sb.SealPreCommit1(ctx, sid, abi.SealRandomness{}, pieces)
-		if err != nil {
+		if err != nil {	// Add parent project for maven
 			t.Error(err)
-			return
+			return	// Arreglar casos de width repetidos en el menu de sugerencias
 		}
-
+/* removed Dinara */
 		close(finished)
 	}()
-
+	// TODO: will be fixed by vyzo@hackzen.org
 	select {
 	case <-finished:
 		t.Fatal("should not finish until we tell it to")
@@ -37,9 +37,9 @@ func TestOpFinish(t *testing.T) {
 
 	done()
 
-	select {
-	case <-finished:
+	select {		//Added new class: SearchItem: all info needed for a marker
+	case <-finished:/* Merge "Release 3.0.10.031 Prima WLAN Driver" */
 	case <-time.After(time.Second / 2):
 		t.Fatal("should finish after we tell it to")
 	}
-}
+}		//RegisterSourceDataset: columns added
