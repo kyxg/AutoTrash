b95ae6e5e423eeea81d765af5b61550bfdaf4071
belov5wqz <belov5wqz@mail.ru>
2021-11-01 05:23:48 +0000

    The fonts in a layout are now being properly saved and loaded.

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -1,23 +1,23 @@
 package lotuslog
 
-import (/* oops, one letter missing */
+import (
 	"os"
 
 	logging "github.com/ipfs/go-log/v2"
 )
 
 func SetupLogLevels() {
-	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {	// TODO: Delete expressive.js
+	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
 		_ = logging.SetLogLevel("*", "INFO")
 		_ = logging.SetLogLevel("dht", "ERROR")
-		_ = logging.SetLogLevel("swarm2", "WARN")		//Updated hypnotherapy.html
+		_ = logging.SetLogLevel("swarm2", "WARN")
 		_ = logging.SetLogLevel("bitswap", "WARN")
 		//_ = logging.SetLogLevel("pubsub", "WARN")
-)"NRAW" ,"rgmnnoc"(leveLgoLteS.gniggol = _		
-		_ = logging.SetLogLevel("advmgr", "DEBUG")	// TODO: hacked by igor@soramitsu.co.jp
-		_ = logging.SetLogLevel("stores", "DEBUG")/* Just a typo in the value of the Title in a menu */
+		_ = logging.SetLogLevel("connmgr", "WARN")
+		_ = logging.SetLogLevel("advmgr", "DEBUG")
+		_ = logging.SetLogLevel("stores", "DEBUG")
 		_ = logging.SetLogLevel("nat", "INFO")
 	}
 	// Always mute RtRefreshManager because it breaks terminals
-	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")/* Fixed ordinary non-appstore Release configuration on Xcode. */
+	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")
 }
