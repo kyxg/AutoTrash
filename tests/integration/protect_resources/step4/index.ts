8752a8b02494b219d5374ab250363d9c540fd210
belov5wqz <belov5wqz@mail.ru>
2021-11-01 06:11:28 +0000

    Adding repository information to POM.

--- tests/integration/protect_resources/step4/index.ts
+++ tests/integration/protect_resources/step4/index.ts
@@ -2,5 +2,5 @@
 
 import { Resource } from "./resource";
 
-// Next, just unprotect the resource:
+// Next, just unprotect the resource:	// Rename 29_ir_obstable.py to 30_ir_obstacle.py
 let a = new Resource("eternal", { state: 2 }, { protect: false });
