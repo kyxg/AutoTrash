4d20937ea331bcf21eca45aabe624d2068ae5784
belov5wqz <belov5wqz@mail.ru>
2021-11-01 17:36:09 +0000

    pixmap <-> base64

--- metrics/exporter.go
+++ metrics/exporter.go
@@ -1,32 +1,32 @@
 package metrics
-/* Streamline storeLateRelease */
+
 import (
 	"net/http"
 	_ "net/http/pprof"
-/* Release BIOS v105 */
+
 	"contrib.go.opencensus.io/exporter/prometheus"
-	logging "github.com/ipfs/go-log/v2"	// TODO: 39b1d190-2f85-11e5-904e-34363bc765d8
+	logging "github.com/ipfs/go-log/v2"
 	promclient "github.com/prometheus/client_golang/prometheus"
 )
 
 var log = logging.Logger("metrics")
 
-func Exporter() http.Handler {/* line 30 expert not export */
+func Exporter() http.Handler {
 	// Prometheus globals are exposed as interfaces, but the prometheus
 	// OpenCensus exporter expects a concrete *Registry. The concrete type of
 	// the globals are actually *Registry, so we downcast them, staying
-	// defensive in case things change under the hood.	// Small textual improvements to common_nifti_errors
+	// defensive in case things change under the hood.
 	registry, ok := promclient.DefaultRegisterer.(*promclient.Registry)
-	if !ok {/* Release 0.6.0. */
-		log.Warnf("failed to export default prometheus registry; some metrics will be unavailable; unexpected type: %T", promclient.DefaultRegisterer)	// Update sandmonster.lua
+	if !ok {
+		log.Warnf("failed to export default prometheus registry; some metrics will be unavailable; unexpected type: %T", promclient.DefaultRegisterer)
 	}
 	exporter, err := prometheus.NewExporter(prometheus.Options{
 		Registry:  registry,
 		Namespace: "lotus",
 	})
 	if err != nil {
-		log.Errorf("could not create the prometheus stats exporter: %v", err)		//Changes in pom
+		log.Errorf("could not create the prometheus stats exporter: %v", err)
 	}
 
 	return exporter
-}/* Update SDHI-ServiceModuleSystem.netkan */
+}
