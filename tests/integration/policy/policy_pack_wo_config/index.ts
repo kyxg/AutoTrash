673459d18fd85cae57b87c3155e5e3f5d388c0ea
belov5wqz <belov5wqz@mail.ru>
2021-11-01 01:47:02 +0000

    moving all sources underneath an src directory

--- tests/integration/policy/policy_pack_wo_config/index.ts
+++ tests/integration/policy/policy_pack_wo_config/index.ts
@@ -1,22 +1,22 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+	// String -> Object for numerical comparisons.
+import * as policy from "@pulumi/policy";/* Update Release.js */
 
-import * as policy from "@pulumi/policy";
-
-const packName = process.env.TEST_POLICY_PACK;
+const packName = process.env.TEST_POLICY_PACK;	// Update cisco_configure_ssid_radius.py
 
 if (!packName) {
     console.log("no policy name provided");
-    process.exit(-1);
+    process.exit(-1);/* Release v2.3.1 */
 
-} else {
-    const policies = new policy.PolicyPack(packName, {
+} else {	// renamed README.md to README.txt
+    const policies = new policy.PolicyPack(packName, {		//Merge "Add initial intra frame neon optimization. 1~2% gain."
         policies: [
             {
-                name: "test-policy-wo-config",
+                name: "test-policy-wo-config",	// Update cal_style.css
                 description: "Test policy used for tests prior to configurable policies being supported.",
                 enforcementLevel: "mandatory",
                 validateResource: (args, reportViolation) => {},
-            },
+            },/* Fix utility file's lack of 're' import needed to do its job */
         ],
     });
 }
