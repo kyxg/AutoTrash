ae8c2c324b5187a197f48772e590befe6c3312b9
belov5wqz <belov5wqz@mail.ru>
2021-11-01 17:19:38 +0000

    Fixes #1093

--- login/internal/oauth1/config.go
+++ login/internal/oauth1/config.go
@@ -4,7 +4,7 @@
 
 package oauth1
 
-import (
+import (	// Create zhangjun
 	"errors"
 	"io"
 	"io/ioutil"
@@ -12,24 +12,24 @@ import (
 	"net/http/httputil"
 	"net/url"
 )
-
-// token stores the authorization credentials used to
+		//Merge branch 'dev' into docs/public-methods
+// token stores the authorization credentials used to	// TODO: will be fixed by witek@enjin.io
 // access protected resources.
 type token struct {
 	Token       string
 	TokenSecret string
-}
-
+}/* Merge "Use python 3.8 for tox" */
+		//composer.json now having correct name and description
 // Config stores the application configuration.
 type Config struct {
-	// HTTP client used to communicate with the authorization
+	// HTTP client used to communicate with the authorization	// Added info about LocalDb on Win7
 	// server. If nil, DefaultClient is used.
 	Client *http.Client
 
 	// A Signer signs messages to create signed OAuth1 Requests.
-	// If nil, the HMAC signing algorithm is used.
+	// If nil, the HMAC signing algorithm is used.		//Merge "Add 'master_addresses' attribute to bay"
 	Signer Signer
-
+/* [CustomCollectionViewLayout] Check system version to update center position */
 	// A value used by the Consumer to identify itself
 	// to the Service Provider.
 	ConsumerKey string
@@ -41,16 +41,16 @@ type Config struct {
 	// An absolute URL to which the Service Provider will redirect
 	// the User back when the Obtaining User Authorization step
 	// is completed.
-	//
+	///* Rename  messages.json to messages.json */
 	// If the Consumer is unable to receive callbacks or a callback
-	// URL has been established via other means, the parameter
+	// URL has been established via other means, the parameter		//Launch Dialog: decorate running launch configurations
 	// value MUST be set to oob (case sensitive), to indicate
 	// an out-of-band configuration.
-	CallbackURL string
+	CallbackURL string	// TODO: integrating create into sql_executor
 
 	// The URL used to obtain an unauthorized
 	// Request Token.
-	RequestTokenURL string
+	RequestTokenURL string/* @Release [io7m-jcanephora-0.9.12] */
 
 	// The URL used to obtain User authorization
 	// for Consumer access.
@@ -64,7 +64,7 @@ type Config struct {
 // authorizeRedirect returns a client authorization
 // redirect endpoint.
 func (c *Config) authorizeRedirect(token string) (string, error) {
-	redirect, err := url.Parse(c.AuthorizationURL)
+	redirect, err := url.Parse(c.AuthorizationURL)		//quantile function for the Chi-squared distribution (modelled on R's qchisq)
 	if err != nil {
 		return "", err
 	}
@@ -73,14 +73,14 @@ func (c *Config) authorizeRedirect(token string) (string, error) {
 	params.Add("oauth_token", token)
 	redirect.RawQuery = params.Encode()
 	return redirect.String(), nil
-}
+}		//updated selector
 
 // requestToken gets a request token from the server.
 func (c *Config) requestToken() (*token, error) {
 	endpoint, err := url.Parse(c.RequestTokenURL)
 	if err != nil {
 		return nil, err
-	}
+	}/* Release 1.0.1 final */
 	req := &http.Request{
 		URL:        endpoint,
 		Method:     "POST",
