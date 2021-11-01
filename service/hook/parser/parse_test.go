0b26d6c71a37846c29943064413fae11905edd5e
belov5wqz <belov5wqz@mail.ru>
2021-11-01 12:59:45 +0000

    Update and rename dml_1d_inception.py to dml_model.py

--- service/hook/parser/parse_test.go
+++ service/hook/parser/parse_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//Placeholder for empty.
 // that can be found in the LICENSE file.
 
 package parser
