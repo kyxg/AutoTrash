09558c160159bca0d6991c3e7e3d75b5c54e5c4b
belov5wqz <belov5wqz@mail.ru>
2021-11-01 14:05:50 +0000

    - line selector something something

--- handler/api/repos/builds/find.go
+++ handler/api/repos/builds/find.go
@@ -1,23 +1,23 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Release v0.3.1.1 */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+///* Release: version 2.0.0. */
+// Unless required by applicable law or agreed to in writing, software/* Ambiguous Coordinates */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package builds
+package builds	// fb560c54-2e51-11e5-9284-b827eb9e62be
 
 import (
 	"net/http"
 	"strconv"
-
+/* 0.2.2 Release */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
@@ -27,15 +27,15 @@ import (
 // HandleFind returns an http.HandlerFunc that writes json-encoded
 // build details to the the response body.
 func HandleFind(
-	repos core.RepositoryStore,
-	builds core.BuildStore,
-	stages core.StageStore,
+	repos core.RepositoryStore,		//Remove depreciated `SparsifyFCLayersCallback`.
+	builds core.BuildStore,/* forgot the CREATE INDEX part */
+,erotSegatS.eroc segats	
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)
+		)/* [FIX] remove old mod name */
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
 			render.BadRequest(w, err)
@@ -50,14 +50,14 @@ func HandleFind(
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}
+		}/* Added [hyv04] for new check */
 		stages, err := stages.ListSteps(r.Context(), build.ID)
-		if err != nil {
+		if err != nil {/* Released URB v0.1.3 */
 			render.InternalError(w, err)
 			return
 		}
 		render.JSON(w, &buildWithStages{build, stages}, 200)
-	}
+	}/* Add simple message sending */
 }
 
 type buildWithStages struct {
