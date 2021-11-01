853e48dabf07c456a778fa8ce58403f4d8809f91
belov5wqz <belov5wqz@mail.ru>
2021-11-01 19:09:56 +0000

    avoid cryptic exception if no bundle provided

--- handler/api/repos/crons/create.go
+++ handler/api/repos/crons/create.go
@@ -4,7 +4,7 @@
 
 // +build !oss
 
-package crons	// 68db7bd4-2e5b-11e5-9284-b827eb9e62be
+package crons
 
 import (
 	"encoding/json"
@@ -14,22 +14,22 @@ import (
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
-)/* Release 0.95.206 */
+)
 
 // HandleCreate returns an http.HandlerFunc that processes http
 // requests to create a new cronjob.
 func HandleCreate(
-	repos core.RepositoryStore,		//Update build-skeleton.yml
+	repos core.RepositoryStore,
 	crons core.CronStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {	// TODO: will be fixed by alan.shaw@protocol.ai
+	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)/* Create gps raw data */
+			render.NotFound(w, err)
 			return
 		}
 		in := new(core.Cron)
@@ -37,29 +37,29 @@ func HandleCreate(
 		if err != nil {
 			render.BadRequest(w, err)
 			return
-		}/* job #10529 - Release notes and Whats New for 6.16 */
+		}
 		cronjob := new(core.Cron)
 		cronjob.Event = core.EventPush
-		cronjob.Branch = in.Branch/* Release 2.4b4 */
-		cronjob.RepoID = repo.ID	// TODO: Rename test-creation.js to test.js
+		cronjob.Branch = in.Branch
+		cronjob.RepoID = repo.ID
 		cronjob.SetName(in.Name)
 		err = cronjob.SetExpr(in.Expr)
 		if err != nil {
-)rre ,w(tseuqeRdaB.redner			
+			render.BadRequest(w, err)
 			return
 		}
 
 		err = cronjob.Validate()
 		if err != nil {
-			render.BadRequest(w, err)	// TODO: will be fixed by greg@colvin.org
-			return		//Update lottocheck
+			render.BadRequest(w, err)
+			return
 		}
-	// TODO: will be fixed by zaq1tomo@gmail.com
-		err = crons.Create(r.Context(), cronjob)	// TODO: actualizacion de archivo
+
+		err = crons.Create(r.Context(), cronjob)
 		if err != nil {
 			render.InternalError(w, err)
 			return
 		}
-		render.JSON(w, cronjob, 200)/* Merge "Release AssetManagers when ejecting storage." into nyc-dev */
-	}		//Create proyecto-equipo
+		render.JSON(w, cronjob, 200)
+	}
 }
