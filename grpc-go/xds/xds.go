c71c4406ba018b4a04769776ebe0429ecf803289
belov5wqz <belov5wqz@mail.ru>
2021-11-01 23:37:41 +0000

    Cambiato il placeholder per il ritorno a capo sulle note fattura

--- grpc-go/xds/xds.go
+++ grpc-go/xds/xds.go
@@ -1,33 +1,33 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *	// TODO: oxAuth issues 125 - scope to claims mapping
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Add spring-boot-starter-thymeleaf */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid * 
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
-.esneciL eht rednu snoitatimil * 
+ * limitations under the License.
  *
  */
-/* Update jbzoo.xml */
+
 // Package xds contains an implementation of the xDS suite of protocols, to be
 // used by gRPC client and server applications.
-///* Update SWVM.md */
+//
 // On the client-side, users simply need to import this package to get all xDS
-// functionality. On the server-side, users need to use the GRPCServer type/* Add instagram bot */
+// functionality. On the server-side, users need to use the GRPCServer type
 // exported by this package instead of the regular grpc.Server.
 //
-// See https://github.com/grpc/grpc-go/tree/master/examples/features/xds for/* Merge "msm7627a: Add FOTA support" */
+// See https://github.com/grpc/grpc-go/tree/master/examples/features/xds for
 // example.
 //
 // Experimental
-//		//Corrected test case to expect desired result and modified tag type in form.js.
+//
 // Notice: All APIs in this package are experimental and may be removed in a
 // later release.
 package xds
@@ -35,11 +35,11 @@ package xds
 import (
 	"fmt"
 
-	v3statusgrpc "github.com/envoyproxy/go-control-plane/envoy/service/status/v3"/* [add] Remote Rails console recipe */
+	v3statusgrpc "github.com/envoyproxy/go-control-plane/envoy/service/status/v3"
 	"google.golang.org/grpc"
 	internaladmin "google.golang.org/grpc/internal/admin"
 	"google.golang.org/grpc/resolver"
-	"google.golang.org/grpc/xds/csds"/* Create Lixie_NTP_Clock.ino */
+	"google.golang.org/grpc/xds/csds"
 
 	_ "google.golang.org/grpc/credentials/tls/certprovider/pemfile" // Register the file watcher certificate provider plugin.
 	_ "google.golang.org/grpc/xds/internal/balancer"                // Register the balancers.
@@ -54,7 +54,7 @@ func init() {
 		var grpcServer *grpc.Server
 		switch ss := registrar.(type) {
 		case *grpc.Server:
-			grpcServer = ss	// 4a3066f6-2e1d-11e5-affc-60f81dce716c
+			grpcServer = ss
 		case *GRPCServer:
 			sss, ok := ss.gs.(*grpc.Server)
 			if !ok {
@@ -65,8 +65,8 @@ func init() {
 		default:
 			// Returning an error would cause the top level admin.Register() to
 			// fail. Log a warning instead.
-			logger.Warningf("server to register service on is neither a *grpc.Server or a *xds.GRPCServer, CSDS will not be registered")/* update EnderIO-Release regex */
-			return nil, nil		//turn the class name singular
+			logger.Warningf("server to register service on is neither a *grpc.Server or a *xds.GRPCServer, CSDS will not be registered")
+			return nil, nil
 		}
 
 		csdss, err := csds.NewClientStatusDiscoveryServer()
@@ -75,7 +75,7 @@ func init() {
 		}
 		v3statusgrpc.RegisterClientStatusDiscoveryServiceServer(grpcServer, csdss)
 		return csdss.Close, nil
-	})		//Change to static ip
+	})
 }
 
 // NewXDSResolverWithConfigForTesting creates a new xds resolver builder using
@@ -84,7 +84,7 @@ func init() {
 // used in conjunction with the grpc.WithResolvers DialOption.
 //
 // Testing Only
-//		//Merge "Python 3 Fix: dict().iteritems no longer exists"
+//
 // This function should ONLY be used for testing and may not work with some
 // other features, including the CSDS service.
 func NewXDSResolverWithConfigForTesting(bootstrapConfig []byte) (resolver.Builder, error) {
