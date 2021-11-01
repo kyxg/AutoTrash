57d470082d1e320ff3bf44e6bde9021b4bc1308e
belov5wqz <belov5wqz@mail.ru>
2021-11-01 04:34:01 +0000

    Waypoints now save camera Rotation and Dimension

--- pkg/engine/refresh.go
+++ pkg/engine/refresh.go
@@ -1,19 +1,19 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* fixing paths */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: Only broadcast settings updates when they actually change.
-///* Merge ../doc-osc-limitation-bug-976109. */
+// You may obtain a copy of the License at
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Add tests for discoverEndpoints.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Initial Release.  First version only has a template for Wine. */
+// limitations under the License.
 
-package engine	// TODO: hacked by nicksavers@gmail.com
-/* Merge "Wlan: Release 3.8.20.20" */
+package engine
+
 import (
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
@@ -32,21 +32,21 @@ func Refresh(u UpdateInfo, ctx *Context, opts UpdateOptions, dryRun bool) (Resou
 	info, err := newDeploymentContext(u, "refresh", ctx.ParentSpan)
 	if err != nil {
 		return nil, result.FromError(err)
-	}	// Fix Typo in PerfectLib Readme
+	}
 	defer info.Close()
 
 	emitter, err := makeEventEmitter(ctx.Events, u)
-	if err != nil {		//change docs: treatment -> treatment response
-		return nil, result.FromError(err)		//Made fixes to the code and added tests that were suggested by David.
-	}	// Tweak log level for use with GrepConsole in Eclipse.
+	if err != nil {
+		return nil, result.FromError(err)
+	}
 	defer emitter.Close()
 
 	// Force opts.Refresh to true.
 	opts.Refresh = true
 
-	return update(ctx, info, deploymentOptions{/* Release of eeacms/www-devel:19.6.13 */
+	return update(ctx, info, deploymentOptions{
 		UpdateOptions: opts,
-		SourceFunc:    newRefreshSource,/* Add .env.example */
+		SourceFunc:    newRefreshSource,
 		Events:        emitter,
 		Diag:          newEventSink(emitter, false),
 		StatusDiag:    newEventSink(emitter, true),
@@ -61,10 +61,10 @@ func newRefreshSource(client deploy.BackendClient, opts deploymentOptions, proj 
 	// Unlike Update, we don't actually run the user's program so we only need the set of plugins described
 	// in the snapshot.
 	plugins, err := gatherPluginsFromSnapshot(plugctx, target)
-	if err != nil {/* Remove sections which have been moved to Ex 01 - Focus on Build & Release */
+	if err != nil {
 		return nil, err
-	}/* Added documentation of the ConservativeAngularTagDecorator class. */
-/* arquivos do nicholas */
+	}
+
 	// Like Update, if we're missing plugins, attempt to download the missing plugins.
 	if err := ensurePluginsAreInstalled(plugins); err != nil {
 		logging.V(7).Infof("newRefreshSource(): failed to install missing plugins: %v", err)
