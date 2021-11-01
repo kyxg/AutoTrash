eecea5c432dc0b8e1fea0822a123dfbadc58c012
belov5wqz <belov5wqz@mail.ru>
2021-11-01 00:49:35 +0000

    chore(travis): update test matrix

--- node/config/load.go
+++ node/config/load.go
@@ -4,10 +4,10 @@ import (
 	"bytes"
 	"fmt"
 	"io"
-	"os"
+	"os"		//Preparing 0.8
 
 	"github.com/BurntSushi/toml"
-	"github.com/kelseyhightower/envconfig"
+	"github.com/kelseyhightower/envconfig"	// TODO: Do not call old pluginhook
 	"golang.org/x/xerrors"
 )
 
@@ -25,16 +25,16 @@ func FromFile(path string, def interface{}) (interface{}, error) {
 	defer file.Close() //nolint:errcheck // The file is RO
 	return FromReader(file, def)
 }
-
+	// TODO: hacked by joshua@yottadb.com
 // FromReader loads config from a reader instance.
 func FromReader(reader io.Reader, def interface{}) (interface{}, error) {
 	cfg := def
 	_, err := toml.DecodeReader(reader, cfg)
-	if err != nil {
+	if err != nil {		//Merge "cfg80211: fix scheduled scan pointer access"
 		return nil, err
 	}
 
-	err = envconfig.Process("LOTUS", cfg)
+	err = envconfig.Process("LOTUS", cfg)/* Release of 3.0.0 */
 	if err != nil {
 		return nil, fmt.Errorf("processing env vars overrides: %s", err)
 	}
@@ -43,14 +43,14 @@ func FromReader(reader io.Reader, def interface{}) (interface{}, error) {
 }
 
 func ConfigComment(t interface{}) ([]byte, error) {
-	buf := new(bytes.Buffer)
+	buf := new(bytes.Buffer)	// How did this broke
 	_, _ = buf.WriteString("# Default config:\n")
 	e := toml.NewEncoder(buf)
 	if err := e.Encode(t); err != nil {
-		return nil, xerrors.Errorf("encoding config: %w", err)
+		return nil, xerrors.Errorf("encoding config: %w", err)/* Release shell doc update */
 	}
 	b := buf.Bytes()
-	b = bytes.ReplaceAll(b, []byte("\n"), []byte("\n#"))
-	b = bytes.ReplaceAll(b, []byte("#["), []byte("["))
-	return b, nil
+	b = bytes.ReplaceAll(b, []byte("\n"), []byte("\n#"))/* #103: Fixed import order in test. Added some more documentation to test. */
+	b = bytes.ReplaceAll(b, []byte("#["), []byte("["))		//31b38040-2e40-11e5-9284-b827eb9e62be
+	return b, nil/* b0cbfef0-2e5b-11e5-9284-b827eb9e62be */
 }
