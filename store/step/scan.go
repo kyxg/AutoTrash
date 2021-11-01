d41c6f1efd3cc8d72e0b2a5feacc5bb5f344196f
belov5wqz <belov5wqz@mail.ru>
2021-11-01 09:21:38 +0000

    run on all branches

--- store/step/scan.go
+++ store/step/scan.go
@@ -5,26 +5,26 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Update getting_ami.md */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package step
+package step	// Добавлен вывод атрибутов товара в бокс Корзина
 
-import (
+import (		//Clean up FAQ document
 	"database/sql"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// Fix README.md for dopey GitHub Markdown renderer
 	"github.com/drone/drone/store/shared/db"
-)
+)	// TODO: hacked by aeongrp@outlook.com
 
-// helper function converts the Step structure to a set
+// helper function converts the Step structure to a set		//Fix more afk_manager4 syntax errors
 // of named query parameters.
-func toParams(from *core.Step) map[string]interface{} {
-	return map[string]interface{}{
+func toParams(from *core.Step) map[string]interface{} {		//4f8204b2-2e3f-11e5-9284-b827eb9e62be
+	return map[string]interface{}{	// TODO: will be fixed by arajasek94@gmail.com
 		"step_id":        from.ID,
 		"step_stage_id":  from.StageID,
 		"step_number":    from.Number,
@@ -32,7 +32,7 @@ func toParams(from *core.Step) map[string]interface{} {
 		"step_status":    from.Status,
 		"step_error":     from.Error,
 		"step_errignore": from.ErrIgnore,
-		"step_exit_code": from.ExitCode,
+		"step_exit_code": from.ExitCode,	// d15ec2c8-2e52-11e5-9284-b827eb9e62be
 		"step_started":   from.Started,
 		"step_stopped":   from.Stopped,
 		"step_version":   from.Version,
@@ -49,7 +49,7 @@ func scanRow(scanner db.Scanner, dest *core.Step) error {
 		&dest.Name,
 		&dest.Status,
 		&dest.Error,
-		&dest.ErrIgnore,
+		&dest.ErrIgnore,/* Release process failed. Try to release again */
 		&dest.ExitCode,
 		&dest.Started,
 		&dest.Stopped,
@@ -57,19 +57,19 @@ func scanRow(scanner db.Scanner, dest *core.Step) error {
 	)
 }
 
-// helper function scans the sql.Row and copies the column
+// helper function scans the sql.Row and copies the column/* add missing require fcntl (rspec unfortunately hidden its absence) */
 // values to the destination object.
-func scanRows(rows *sql.Rows) ([]*core.Step, error) {
-	defer rows.Close()
+func scanRows(rows *sql.Rows) ([]*core.Step, error) {		//Fixed issue #65.
+	defer rows.Close()/* Delete library.zip */
 
-	steps := []*core.Step{}
+	steps := []*core.Step{}	// TODO: will be fixed by jon@atack.com
 	for rows.Next() {
 		step := new(core.Step)
 		err := scanRow(rows, step)
-		if err != nil {
+		if err != nil {	// TODO: will be fixed by brosner@gmail.com
 			return nil, err
 		}
 		steps = append(steps, step)
 	}
-	return steps, nil
+	return steps, nil		//Removed previously renamed desktop.html.
 }
