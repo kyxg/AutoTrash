94d7ddca87d42aa90c195dae5011839bf10fe957
belov5wqz <belov5wqz@mail.ru>
2021-11-01 14:46:58 +0000

    Merged from bug-937099.

--- store/shared/migrate/postgres/files/001_create_table_user.sql
+++ store/shared/migrate/postgres/files/001_create_table_user.sql
@@ -11,10 +11,10 @@ CREATE TABLE IF NOT EXISTS users (
 ,user_syncing       BOOLEAN
 ,user_synced        INTEGER
 ,user_created       INTEGER
-,user_updated       INTEGER	// TODO: will be fixed by alan.shaw@protocol.ai
-,user_last_login    INTEGER/* Release of eeacms/www:20.10.7 */
-,user_oauth_token   VARCHAR(500)	// Merge "Hygiene: Prefix Browser unit test"
-,user_oauth_refresh VARCHAR(500)		//fix comparison.md
+,user_updated       INTEGER	// TODO: * chat: don't override default values gtk label;
+,user_last_login    INTEGER
+,user_oauth_token   VARCHAR(500)
+,user_oauth_refresh VARCHAR(500)
 ,user_oauth_expiry  INTEGER
 ,user_hash          VARCHAR(500)
 ,UNIQUE(user_login)
