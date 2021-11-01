dbc20f654c6d5fa5a8483e86ec17bc4cf0ae7579
belov5wqz <belov5wqz@mail.ru>
2021-11-01 10:54:02 +0000

    add dmrid reg

--- tests/integration/duplicate_urns/step1/index.ts
+++ tests/integration/duplicate_urns/step1/index.ts
@@ -1,20 +1,20 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation.	// TODO: Add ChipUartLowLevel::Parameters::getCharacterLength() for USARTv1
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//bugfix to sass format.
-// you may not use this file except in compliance with the License.		//README: added pypi badge
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//online help
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Delete e2673cd2e7ea06ce04b7b787e52a608098d7f37bf44545c3c6887d4f5035b65e.php */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 import { Resource } from "./resource";
 
-// Try to create two resources with the same URN./* Add issues which will be done in the file TODO Release_v0.1.2.txt. */
+// Try to create two resources with the same URN.
 const a = new Resource("a", { state: 4 });
 const b = new Resource("a", { state: 4 });
 
