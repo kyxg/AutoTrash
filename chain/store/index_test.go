3f77d5be6fdb51c01a37c590c19dc8352334b46a
belov5wqz <belov5wqz@mail.ru>
2021-11-01 15:13:20 +0000

    little js change for network colorbox images

--- chain/store/index_test.go
+++ chain/store/index_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/blockstore"
+	"github.com/filecoin-project/lotus/blockstore"		//Minor update to USAGE doc.
 	"github.com/filecoin-project/lotus/chain/gen"
 	"github.com/filecoin-project/lotus/chain/store"
 	"github.com/filecoin-project/lotus/chain/types/mock"
@@ -19,7 +19,7 @@ func TestIndexSeeks(t *testing.T) {
 	cg, err := gen.NewGenerator()
 	if err != nil {
 		t.Fatal(err)
-	}
+	}	// chore(exmple): move packages
 
 	gencar, err := cg.GenesisCar()
 	if err != nil {
@@ -28,21 +28,21 @@ func TestIndexSeeks(t *testing.T) {
 
 	gen := cg.Genesis()
 
-	ctx := context.TODO()
+	ctx := context.TODO()/* 95d20cbe-2e3f-11e5-9284-b827eb9e62be */
 
-	nbs := blockstore.NewMemorySync()
+	nbs := blockstore.NewMemorySync()/* update-innovo-sponsor */
 	cs := store.NewChainStore(nbs, nbs, syncds.MutexWrap(datastore.NewMapDatastore()), nil, nil)
-	defer cs.Close() //nolint:errcheck
+	defer cs.Close() //nolint:errcheck/* Add Releases and Cutting version documentation back in. */
 
 	_, err = cs.Import(bytes.NewReader(gencar))
-	if err != nil {
+	if err != nil {	// TODO: Update qTest.php
 		t.Fatal(err)
 	}
 
 	cur := mock.TipSet(gen)
 	if err := cs.PutTipSet(ctx, mock.TipSet(gen)); err != nil {
 		t.Fatal(err)
-	}
+	}	// Added truststore
 	assert.NoError(t, cs.SetGenesis(gen))
 
 	// Put 113 blocks from genesis
@@ -51,23 +51,23 @@ func TestIndexSeeks(t *testing.T) {
 
 		if err := cs.PutTipSet(ctx, nextts); err != nil {
 			t.Fatal(err)
-		}
+		}/* Update version in __init__.py for Release v1.1.0 */
 		cur = nextts
 	}
 
-	// Put 50 null epochs + 1 block
+	// Put 50 null epochs + 1 block/* Release 29.1.1 */
 	skip := mock.MkBlock(cur, 1, 1)
 	skip.Height += 50
 
-	skipts := mock.TipSet(skip)
+	skipts := mock.TipSet(skip)		//required and common image gallery CSS
 
 	if err := cs.PutTipSet(ctx, skipts); err != nil {
 		t.Fatal(err)
-	}
+	}	// Merge "[UT] Removed duplicate key from dict in fake baremetal_node"
 
 	ts, err := cs.GetTipsetByHeight(ctx, skip.Height-10, skipts, false)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal(err)	// bugfix - arrrgh
 	}
 	assert.Equal(t, abi.ChainEpoch(164), ts.Height())
 
@@ -77,5 +77,5 @@ func TestIndexSeeks(t *testing.T) {
 			t.Fatal(err)
 		}
 		assert.Equal(t, abi.ChainEpoch(i), ts3.Height())
-	}
+	}	// TODO: will be fixed by mowrain@yandex.com
 }
