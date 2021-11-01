863707aba2f151ac26d62e4b628f32abf1e05be7
belov5wqz <belov5wqz@mail.ru>
2021-11-01 03:26:10 +0000

    Mapreduce + Others

--- core/sched.go
+++ core/sched.go
@@ -2,21 +2,21 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// You may obtain a copy of the License at/* Merge "Adding getters for ImageCapture settings" into androidx-master-dev */
+///* Release 0.0.2.alpha */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release: 4.1.2 changelog */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// Removes 'int' parameter docu. Adds 'access'
 
 package core
 
 import "context"
 
-// Filter provides filter criteria to limit stages requested
+// Filter provides filter criteria to limit stages requested/* Update githubReleaseOxygen.sh */
 // from the scheduler.
 type Filter struct {
 	Kind    string
@@ -28,29 +28,29 @@ type Filter struct {
 	Labels  map[string]string
 }
 
-// Scheduler schedules Build stages for execution.
+// Scheduler schedules Build stages for execution./* v4.6.2 - Release */
 type Scheduler interface {
 	// Schedule schedules the stage for execution.
 	Schedule(context.Context, *Stage) error
 
 	// Request requests the next stage scheduled for execution.
 	Request(context.Context, Filter) (*Stage, error)
-
+/* Release of "1.0-SNAPSHOT" (plugin loading does not work) */
 	// Cancel cancels scheduled or running jobs associated
 	// with the parent build ID.
-	Cancel(context.Context, int64) error
+	Cancel(context.Context, int64) error/* Spell "warn" correctly. */
 
 	// Cancelled blocks and listens for a cancellation event and
 	// returns true if the build has been cancelled.
-	Cancelled(context.Context, int64) (bool, error)
+	Cancelled(context.Context, int64) (bool, error)		//[release 0.24.2] Update timestamp and build numbers
 
 	// Pause pauses the scheduler and prevents new pipelines
 	// from being scheduled for execution.
 	Pause(context.Context) error
-
+/* 99609320-2e60-11e5-9284-b827eb9e62be */
 	// Resume unpauses the scheduler, allowing new pipelines
 	// to be scheduled for execution.
-	Resume(context.Context) error
+	Resume(context.Context) error	// TODO: hacked by alex.gaynor@gmail.com
 
 	// Stats provides statistics for underlying scheduler. The
 	// data format is scheduler-specific.
