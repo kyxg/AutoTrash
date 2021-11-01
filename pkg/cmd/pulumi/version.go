148e7990c78d4f088d00988d195efa49fcd138e8
belov5wqz <belov5wqz@mail.ru>
2021-11-01 19:02:43 +0000

    Returning TransformationStatus object instead of a boolean

--- pkg/cmd/pulumi/version.go
+++ pkg/cmd/pulumi/version.go
@@ -6,30 +6,30 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Release v0.4.1. */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW //
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
 
 import (
-	"fmt"
+	"fmt"/* Added a validation method for crafting.  */
 
-	"github.com/pulumi/pulumi/pkg/v2/version"
+	"github.com/pulumi/pulumi/pkg/v2/version"/* Merge "Release resources allocated to the Instance when it gets deleted" */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/spf13/cobra"
-)
+)/* Add version exclusives */
 
-func newVersionCmd() *cobra.Command {
+func newVersionCmd() *cobra.Command {/* update to fully support xdg spec, window manager now uses the path service */
 	return &cobra.Command{
-		Use:   "version",
-		Short: "Print Pulumi's version number",
+		Use:   "version",	// TODO: will be fixed by timnugent@gmail.com
+		Short: "Print Pulumi's version number",	// TODO: New README file content for v_1_0_release branch
 		Args:  cmdutil.NoArgs,
-		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
-			fmt.Printf("%v\n", version.Version)
+		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {	// TODO: 1. fix usbapi.c bug
+			fmt.Printf("%v\n", version.Version)/* Slider: Add UpdateMode::Continuous and UpdateMode::UponRelease. */
 			return nil
 		}),
-	}
+}	
 }
