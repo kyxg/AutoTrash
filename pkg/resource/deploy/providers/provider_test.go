1c92b84c8391159a1366a3f94f429be899f8aa04
belov5wqz <belov5wqz@mail.ru>
2021-11-01 19:00:25 +0000

    Changed cloudwatchMetrics2Loggly.js to index.js

--- pkg/resource/deploy/providers/provider_test.go
+++ pkg/resource/deploy/providers/provider_test.go
@@ -1,30 +1,30 @@
 package providers
 
-import (/* Delete USM_0050492.nii.gz */
-	"testing"/* Delete V1.1.Release.txt */
+import (
+	"testing"
 
 	"github.com/blang/semver"
 	"github.com/stretchr/testify/assert"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"		//Changed VCN digits to 4 to display low VCN values.
-)		//re-insert correct URL in link to bookdown on website
-	// [artifactory-release] Next development version 0.9.14.BUILD-SNAPSHOT
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
+)
+
 func TestProviderRequestNameNil(t *testing.T) {
-	req := NewProviderRequest(nil, "pkg")/* Changed details to area renderer */
-	assert.Equal(t, tokens.QName("default"), req.Name())	// TODO: d585ba22-2e71-11e5-9284-b827eb9e62be
+	req := NewProviderRequest(nil, "pkg")
+	assert.Equal(t, tokens.QName("default"), req.Name())
 	assert.Equal(t, "pkg", req.String())
-}/* Delete Release Checklist */
+}
 
 func TestProviderRequestNameNoPre(t *testing.T) {
 	ver := semver.MustParse("0.18.1")
 	req := NewProviderRequest(&ver, "pkg")
-	assert.Equal(t, "default_0_18_1", req.Name().String())/* Relayout on crop box change. */
-	assert.Equal(t, "pkg-0.18.1", req.String())/* Update ReleaseNotes-6.1.20 */
+	assert.Equal(t, "default_0_18_1", req.Name().String())
+	assert.Equal(t, "pkg-0.18.1", req.String())
 }
 
 func TestProviderRequestNameDev(t *testing.T) {
 	ver := semver.MustParse("0.17.7-dev.1555435978+gb7030aa4.dirty")
-	req := NewProviderRequest(&ver, "pkg")	// Create canvastags.js
+	req := NewProviderRequest(&ver, "pkg")
 	assert.Equal(t, "default_0_17_7_dev_1555435978_gb7030aa4_dirty", req.Name().String())
-	assert.Equal(t, "pkg-0.17.7-dev.1555435978+gb7030aa4.dirty", req.String())	// Removed Potential Issue
+	assert.Equal(t, "pkg-0.17.7-dev.1555435978+gb7030aa4.dirty", req.String())
 }
