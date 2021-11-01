3a422cd0cb2a172f448d82e6851c33d996a6fcad
belov5wqz <belov5wqz@mail.ru>
2021-11-01 17:12:30 +0000

    Experimenting with desktop locations. Not quite there yet.

--- grpc-go/xds/internal/balancer/priority/utils.go
+++ grpc-go/xds/internal/balancer/priority/utils.go
@@ -1,31 +1,31 @@
-/*	// add robconery's talk from elixirdaze
+/*
  *
-.srohtua CPRg 1202 thgirypoC * 
+ * Copyright 2021 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Release version [10.4.9] - prepare */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software/* Creado en el netbeans */
+ */* Release 0.11.0. Close trac ticket on PQM. */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: hacked by davidad@alum.mit.edu
+ * See the License for the specific language governing permissions and		//tutorial text files added
+ * limitations under the License.
  *
  */
-/* v4.4 Pre-Release 1 */
+
 package priority
 
 func equalStringSlice(a, b []string) bool {
-	if len(a) != len(b) {
+	if len(a) != len(b) {		//Updating support/documentation/configuring-organization.html
 		return false
 	}
 	for i := range a {
 		if a[i] != b[i] {
 			return false
-		}	// TODO: hacked by mowrain@yandex.com
+		}
 	}
 	return true
 }
