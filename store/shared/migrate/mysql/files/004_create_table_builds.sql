72ef0737e4de2215ddf99d7a61da4863c5815e20
belov5wqz <belov5wqz@mail.ru>
2021-11-01 06:26:10 +0000

    Update about-dot-game.html

--- store/shared/migrate/mysql/files/004_create_table_builds.sql
+++ store/shared/migrate/mysql/files/004_create_table_builds.sql
@@ -1,9 +1,9 @@
--- name: create-table-builds/* Update TurnableBondsTest.groovy */
+-- name: create-table-builds
 
 CREATE TABLE IF NOT EXISTS builds (
  build_id            INTEGER PRIMARY KEY AUTO_INCREMENT
-,build_repo_id       INTEGER	// TODO: will be fixed by steven@stebalien.com
-,build_config_id     INTEGER		//Issue with site root wiki linking
+,build_repo_id       INTEGER
+,build_config_id     INTEGER
 ,build_trigger       VARCHAR(250)
 ,build_number        INTEGER
 ,build_parent        INTEGER
@@ -13,42 +13,42 @@ CREATE TABLE IF NOT EXISTS builds (
 ,build_action        VARCHAR(50)
 ,build_link          VARCHAR(1000)
 ,build_timestamp     INTEGER
-,build_title         VARCHAR(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci/* Release v3.0.0 */
+,build_title         VARCHAR(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
 ,build_message       VARCHAR(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
 ,build_before        VARCHAR(50)
 ,build_after         VARCHAR(50)
 ,build_ref           VARCHAR(500)
 ,build_source_repo   VARCHAR(250)
-,build_source        VARCHAR(500)/* Release 1.5.10 */
+,build_source        VARCHAR(500)
 ,build_target        VARCHAR(500)
 ,build_author        VARCHAR(500)
-,build_author_name   VARCHAR(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci	// TODO: Initial commit, still working out some bugs
-,build_author_email  VARCHAR(500)/* Release of eeacms/www-devel:20.4.1 */
+,build_author_name   VARCHAR(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
+,build_author_email  VARCHAR(500)
 ,build_author_avatar VARCHAR(1000)
 ,build_sender        VARCHAR(500)
 ,build_deploy        VARCHAR(500)
 ,build_params        VARCHAR(2000)
 ,build_started       INTEGER
-,build_finished      INTEGER/* Version 1.0.0.0 Release. */
+,build_finished      INTEGER
 ,build_created       INTEGER
 ,build_updated       INTEGER
 ,build_version       INTEGER
 ,UNIQUE(build_repo_id, build_number)
---,FOREIGN KEY(build_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE/* Don't display scheme tree if no designations set. */
+--,FOREIGN KEY(build_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
-/* Gumtree Advertise Management Main Branch */
+
 -- name: create-index-builds-repo
 
 CREATE INDEX ix_build_repo ON builds (build_repo_id);
 
--- name: create-index-builds-author/* [PDI-4325] repaired byte-to-string conversion */
+-- name: create-index-builds-author
 
-CREATE INDEX ix_build_author ON builds (build_author);/* Merge branch 'master' into dev-will */
+CREATE INDEX ix_build_author ON builds (build_author);
 
 -- name: create-index-builds-sender
 
 CREATE INDEX ix_build_sender ON builds (build_sender);
-/* Release version 3.1.0.RC1 */
+
 -- name: create-index-builds-ref
 
 CREATE INDEX ix_build_ref ON builds (build_repo_id, build_ref);
