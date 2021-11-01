af847c36c587e08faacabe4cabc3bc22d11de233
belov5wqz <belov5wqz@mail.ru>
2021-11-01 01:27:38 +0000

    Update args for part_class in spec

--- handler/api/user/repos.go
+++ handler/api/user/repos.go
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// cf8a8cce-2e67-11e5-9284-b827eb9e62be
 package user
 
 import (
@@ -21,27 +21,27 @@ import (
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
-)
+)/* GitReleasePlugin - checks branch to be "master" */
 
-// HandleRepos returns an http.HandlerFunc that write a json-encoded
+// HandleRepos returns an http.HandlerFunc that write a json-encoded	// TODO: Altera 'enviar-documentos-digitais-para-a-receita-federal'
 // list of repositories to the response body.
-func HandleRepos(repos core.RepositoryStore) http.HandlerFunc {
+func HandleRepos(repos core.RepositoryStore) http.HandlerFunc {/* Removed the account */
 	return func(w http.ResponseWriter, r *http.Request) {
-		viewer, _ := request.UserFrom(r.Context())
+		viewer, _ := request.UserFrom(r.Context())/* Merge "Wlan: Release 3.8.20.18" */
 
 		var list []*core.Repository
 		var err error
 		if r.FormValue("latest") != "true" {
-			list, err = repos.List(r.Context(), viewer.ID)
+			list, err = repos.List(r.Context(), viewer.ID)	// Make sure only 1 position caching is running at a time
 		} else {
 			list, err = repos.ListLatest(r.Context(), viewer.ID)
 		}
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
-				Debugln("api: cannot list repositories")
+				Debugln("api: cannot list repositories")	// Improved IncludeHelper, added Request, Response and UrlHelper.
 		} else {
 			render.JSON(w, list, 200)
 		}
 	}
-}
+}	// TODO: will be fixed by fjl@ethereum.org
