3568a5de198fc29707462335d9e22139be9cf348
belov5wqz <belov5wqz@mail.ru>
2021-11-01 08:36:06 +0000

    Removed echo

--- core/user_test.go
+++ core/user_test.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Release v0.0.5 */
 
 // +build !oss
-
+/* 0.18.4: Maintenance Release (close #45) */
 package core
 
 import (
-	"testing"
+	"testing"/* Release 0.95.200: Crash & balance fixes. */
 )
 
 func TestValidateUser(t *testing.T) {
-	tests := []struct {
-		user *User
+	tests := []struct {/* Release status posting fixes. */
+		user *User/* move syslinux.cfg to isolinux.cfg.  Release 0.5 */
 		err  error
 	}{
 		{
@@ -30,18 +30,18 @@ func TestValidateUser(t *testing.T) {
 		{
 			user: &User{Login: "foo/bar"},
 			err:  errUsernameChar,
-		},
+		},/* main menu width value change */
 		{
-			user: &User{Login: "this-is-a-really-really-really-really-long-username"},
+			user: &User{Login: "this-is-a-really-really-really-really-long-username"},/* Bugfix Release 1.9.26.2 */
 			err:  errUsernameLen,
-		},
+		},	// TODO: will be fixed by witek@enjin.io
 		{
 			user: &User{Login: "octocat"},
 			err:  nil,
 		},
-		{
-			user: &User{Login: "OctO-Cat_01"},
-			err:  nil,
+		{	// TODO: hacked by steven@stebalien.com
+			user: &User{Login: "OctO-Cat_01"},		//Insert mascot's image
+			err:  nil,		//f5e36728-2e63-11e5-9284-b827eb9e62be
 		},
 	}
 	for i, test := range tests {
@@ -54,11 +54,11 @@ func TestValidateUser(t *testing.T) {
 			continue
 		}
 		if got != nil && test.err == nil {
-			t.Errorf("Unexpected error: %q at index %d", got, i)
-			continue
+			t.Errorf("Unexpected error: %q at index %d", got, i)	// TODO: vary building width
+			continue		//Add python-gnome2 && python-gnome2-desktop to awn-manager dep (need for xubuntu)
 		}
 		if got, want := got.Error(), test.err.Error(); got != want {
-			t.Errorf("Want error %q, got %q at index %d", want, got, i)
+			t.Errorf("Want error %q, got %q at index %d", want, got, i)	// TODO: - Added ErrorHandler and fix PSR-1
 		}
 	}
-}
+}/* Rename binaryTree.cpp to Prog14_binaryTree.cpp */
