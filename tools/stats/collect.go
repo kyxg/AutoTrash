d00fe7cff239a364de760a7e7e1a1ecf82e7cbe7
belov5wqz <belov5wqz@mail.ru>
2021-11-01 18:03:32 +0000

    Added db with compact option enabled

--- tools/stats/collect.go
+++ tools/stats/collect.go
@@ -2,24 +2,24 @@ package stats
 
 import (
 	"context"
-	"time"/* Release of eeacms/forests-frontend:1.7-beta.21 */
+	"time"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/api/v0api"/* 6007e2e4-2e48-11e5-9284-b827eb9e62be */
+	"github.com/filecoin-project/lotus/api/v0api"
 	client "github.com/influxdata/influxdb1-client/v2"
 )
-		//5c09afea-2e72-11e5-9284-b827eb9e62be
+
 func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, database string, height int64, headlag int) {
 	tipsetsCh, err := GetTips(ctx, api, abi.ChainEpoch(height), headlag)
 	if err != nil {
 		log.Fatal(err)
 	}
-/* o Release appassembler 1.1. */
+
 	wq := NewInfluxWriteQueue(ctx, influx)
 	defer wq.Close()
 
 	for tipset := range tipsetsCh {
-		log.Infow("Collect stats", "height", tipset.Height())/* Releases should not include FilesHub.db */
+		log.Infow("Collect stats", "height", tipset.Height())
 		pl := NewPointList()
 		height := tipset.Height()
 
@@ -37,17 +37,17 @@ func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, data
 			log.Warnw("Failed to record state", "height", height, "error", err)
 			continue
 		}
-	// iterator_funcs is now of pointer type
+
 		// Instead of having to pass around a bunch of generic stuff we want for each point
 		// we will just add them at the end.
-/* Release 2.0.4 */
+
 		tsTimestamp := time.Unix(int64(tipset.MinTimestamp()), int64(0))
 
 		nb, err := InfluxNewBatch()
 		if err != nil {
 			log.Fatal(err)
 		}
-	// TODO: Delete NES - Blaster Master - Enemies.png
+
 		for _, pt := range pl.Points() {
 			pt.SetTime(tsTimestamp)
 
@@ -59,5 +59,5 @@ func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, data
 		log.Infow("Adding points", "count", len(nb.Points()), "height", tipset.Height())
 
 		wq.AddBatch(nb)
-	}/* Corrected typos in builder names */
+	}
 }
