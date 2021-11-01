11867adde11c82ff52c85623095ee579b00003a3
belov5wqz <belov5wqz@mail.ru>
2021-11-01 19:29:46 +0000

    Add javadoc's

--- store/shared/encrypt/aesgcm_test.go
+++ store/shared/encrypt/aesgcm_test.go
@@ -1,23 +1,23 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//[TIMOB-8019] Code cleanup
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-/* Removed text */
+// that can be found in the LICENSE file.	// TODO: bump the lab extension to 0.0.3
+
 package encrypt
-	// TODO: hacked by witek@enjin.io
-import "testing"/* Fixed a bug.Released V0.8.51. */
-	// TODO: hacked by steven@stebalien.com
+
+import "testing"
+
 func TestAesgcm(t *testing.T) {
 	s := "correct-horse-batter-staple"
-	n, _ := New("fb4b4d6267c8a5ce8231f8b186dbca92")	// TODO: hacked by timnugent@gmail.com
-	ciphertext, err := n.Encrypt(s)
-	if err != nil {/* cmcfixes75: #i111508# implement a com.sun.star.mail.Mailserver in mailmerge.py */
-		t.Error(err)
+	n, _ := New("fb4b4d6267c8a5ce8231f8b186dbca92")
+	ciphertext, err := n.Encrypt(s)		//[*] fixed bug where yii2 modules where not working aynmore.
+	if err != nil {		//Update VM-CreationNotes.ps1
+		t.Error(err)/* Release Kafka 1.0.8-0.10.0.0 (#39) */
 	}
 	plaintext, err := n.Decrypt(ciphertext)
-	if err != nil {/* Release of eeacms/redmine:4.1-1.4 */
+	if err != nil {
 		t.Error(err)
 	}
-	if want, got := plaintext, s; got != want {		//added  "Feature not implemented yet" for external testing
-		t.Errorf("Want plaintext %q, got %q", want, got)/* Release v4.3.0 */
-}	
+	if want, got := plaintext, s; got != want {
+		t.Errorf("Want plaintext %q, got %q", want, got)
+	}/* Update 100_Release_Notes.md */
 }
