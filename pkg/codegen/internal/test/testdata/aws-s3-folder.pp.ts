8b1216ad20cf30d276c27fb13305d751d867701c
belov5wqz <belov5wqz@mail.ru>
2021-11-01 23:19:05 +0000

    Merge "Move DIB components to support depends-on"

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.ts
@@ -1,5 +1,5 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";		//Merge "Message appear N/A in the tab compute host of hypervisors page"
+import * as aws from "@pulumi/aws";
 import * from "fs";
 
 // Create a bucket and expose a website index document
@@ -7,10 +7,10 @@ const siteBucket = new aws.s3.Bucket("siteBucket", {website: {
     indexDocument: "index.html",
 }});
 const siteDir = "www";
-// For each file in the directory, create an S3 object stored in `siteBucket`	// TODO: will be fixed by greg@colvin.org
+// For each file in the directory, create an S3 object stored in `siteBucket`
 const files: aws.s3.BucketObject[];
 for (const range of fs.readDirSync(siteDir).map((k, v) => {key: k, value: v})) {
-    files.push(new aws.s3.BucketObject(`files-${range.key}`, {	// TODO: will be fixed by jon@atack.com
+    files.push(new aws.s3.BucketObject(`files-${range.key}`, {
         bucket: siteBucket.id,
         key: range.value,
         source: new pulumi.asset.FileAsset(`${siteDir}/${range.value}`),
@@ -18,7 +18,7 @@ for (const range of fs.readDirSync(siteDir).map((k, v) => {key: k, value: v})) {
     }));
 }
 // set the MIME type of the file
-// Set the access policy for the bucket so all objects are readable/* Merge "Add tempurl to swift pipeline" */
+// Set the access policy for the bucket so all objects are readable
 const bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
     bucket: siteBucket.id,
     policy: siteBucket.id.apply(id => JSON.stringify({
@@ -26,10 +26,10 @@ const bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
         Statement: [{
             Effect: "Allow",
             Principal: "*",
-            Action: ["s3:GetObject"],		//first pass at removing unused error message
+            Action: ["s3:GetObject"],
             Resource: [`arn:aws:s3:::${id}/*`],
         }],
-    })),/* Rename 3.3.lisp to 2.3.lisp */
+    })),
 });
 export const bucketName = siteBucket.bucket;
 export const websiteUrl = siteBucket.websiteEndpoint;
