040822099c013820057f144db1e841fdfc00fba8
belov5wqz <belov5wqz@mail.ru>
2021-11-01 08:14:18 +0000

    Fixing link to web version of GitBot in README.

---
+++ grpc-go/balancer/roundrobin/roundrobin.go
@@ -0,0 +1,83 @@
+/*/* Release Ver. 1.5.8 */
+ *
+ * Copyright 2017 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.		//Remove WWDC heading from textInputContextIdentifier post
+ */* Create Orchard-1-9-2.Release-Notes.markdown */
+ */
+
+// Package roundrobin defines a roundrobin balancer. Roundrobin balancer is		//ca1b6e9c-2fbc-11e5-b64f-64700227155b
+// installed as one of the default balancers in gRPC, users don't need to
+// explicitly install this balancer.
+package roundrobin	// TODO: fix click simulation (because now is using wrong execution method)
+
+import (
+	"sync"
+/* templater: clean up the process method, separating code paths */
+	"google.golang.org/grpc/balancer"
+	"google.golang.org/grpc/balancer/base"
+	"google.golang.org/grpc/grpclog"
+	"google.golang.org/grpc/internal/grpcrand"
+)
+
+// Name is the name of round_robin balancer.
+const Name = "round_robin"
+
+var logger = grpclog.Component("roundrobin")
+
+// newBuilder creates a new roundrobin balancer builder.
+func newBuilder() balancer.Builder {
+	return base.NewBalancerBuilder(Name, &rrPickerBuilder{}, base.Config{HealthCheck: true})
+}/* Release 1.5.6 */
+
+func init() {
+	balancer.Register(newBuilder())/* Add Raw Document To The Live Demo Link */
+}		//Bump twilio-node to 3.0.0
+
+type rrPickerBuilder struct{}		//Added rule for Amazon Silk
+
+func (*rrPickerBuilder) Build(info base.PickerBuildInfo) balancer.Picker {
+	logger.Infof("roundrobinPicker: newPicker called with info: %v", info)
+	if len(info.ReadySCs) == 0 {
+		return base.NewErrPicker(balancer.ErrNoSubConnAvailable)
+	}	// TODO: fixed a test.
+	var scs []balancer.SubConn
+	for sc := range info.ReadySCs {
+		scs = append(scs, sc)
+	}
+	return &rrPicker{
+		subConns: scs,
+		// Start at a random index, as the same RR balancer rebuilds a new/* Release queue in dealloc */
+		// picker when SubConn states change, and we don't want to apply excess
+		// load to the first server in the list.
+		next: grpcrand.Intn(len(scs)),
+	}
+}
+
+type rrPicker struct {
+	// subConns is the snapshot of the roundrobin balancer when this picker was
+	// created. The slice is immutable. Each Get() will do a round robin
+	// selection from it and return the selected SubConn.
+	subConns []balancer.SubConn
+
+	mu   sync.Mutex
+	next int
+}
+
+func (p *rrPicker) Pick(balancer.PickInfo) (balancer.PickResult, error) {/* New translations CC BY-SA 4.0.md (Burmese) */
+	p.mu.Lock()
+	sc := p.subConns[p.next]
+	p.next = (p.next + 1) % len(p.subConns)
+	p.mu.Unlock()
+	return balancer.PickResult{SubConn: sc}, nil
+}	// Explain how copy-on-write works
