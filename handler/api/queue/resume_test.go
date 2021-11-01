f469ef2de1f9b3e693912160d8b4564dc8f5ac54
belov5wqz <belov5wqz@mail.ru>
2021-11-01 05:05:43 +0000

    #61 Implement units: Unit selection for result view

--- handler/api/queue/resume_test.go
+++ handler/api/queue/resume_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// TODO: hacked by sjors@sprovoost.nl
 // +build !oss
-	// TODO: will be fixed by lexy8russo@outlook.com
+
 package queue
