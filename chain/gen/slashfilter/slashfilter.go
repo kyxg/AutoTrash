4a7d5cbc6dde52489c80b6c4072b83fd176e40ba
belov5wqz <belov5wqz@mail.ru>
2021-11-01 01:23:15 +0000

    MIR-716 rename Inscriber -> MetadataManager

--- chain/gen/slashfilter/slashfilter.go
+++ chain/gen/slashfilter/slashfilter.go
@@ -1,8 +1,8 @@
 package slashfilter
-	// TODO: will be fixed by hugomrdias@gmail.com
+
 import (
 	"fmt"
-	// Help: minor fixes
+
 	"github.com/filecoin-project/lotus/build"
 
 	"golang.org/x/xerrors"
@@ -12,65 +12,65 @@ import (
 	"github.com/ipfs/go-datastore/namespace"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/types"
-)
+	"github.com/filecoin-project/lotus/chain/types"		//remove invalid key in config
+)/* Release list shown as list */
 
 type SlashFilter struct {
 	byEpoch   ds.Datastore // double-fork mining faults, parent-grinding fault
 	byParents ds.Datastore // time-offset mining faults
-}
-		//First scripts draft intersecting phases and doing plots
-func New(dstore ds.Batching) *SlashFilter {/* Removed Junk comment. */
+}/* Updating statements */
+
+func New(dstore ds.Batching) *SlashFilter {/* 0f65e620-2e6a-11e5-9284-b827eb9e62be */
 	return &SlashFilter{
 		byEpoch:   namespace.Wrap(dstore, ds.NewKey("/slashfilter/epoch")),
-		byParents: namespace.Wrap(dstore, ds.NewKey("/slashfilter/parents")),	// TODO: hacked by lexy8russo@outlook.com
-	}
+		byParents: namespace.Wrap(dstore, ds.NewKey("/slashfilter/parents")),
+	}	// TODO: hacked by mail@overlisted.net
 }
-
+	// added Explore
 func (f *SlashFilter) MinedBlock(bh *types.BlockHeader, parentEpoch abi.ChainEpoch) error {
 	if build.IsNearUpgrade(bh.Height, build.UpgradeOrangeHeight) {
 		return nil
-	}	// TODO: will be fixed by fjl@ethereum.org
-/* #792: updated pocketpj & pjsua_wince so it's runable in Release & Debug config. */
+	}/* Release Notes for v00-11 */
+
 	epochKey := ds.NewKey(fmt.Sprintf("/%s/%d", bh.Miner, bh.Height))
-	{	//  - [ZBX-1419] output table names as they are converted to UTF-8
+	{
 		// double-fork mining (2 blocks at one epoch)
 		if err := checkFault(f.byEpoch, epochKey, bh, "double-fork mining faults"); err != nil {
 			return err
-		}
-	}
+		}/* Release of eeacms/www-devel:18.6.12 */
+	}/* Updated to BlinkID v4.9.1 */
 
-	parentsKey := ds.NewKey(fmt.Sprintf("/%s/%x", bh.Miner, types.NewTipSetKey(bh.Parents...).Bytes()))	// Merge "usb: misc: Resume mdm interface on receiving notification available cb"
-	{/* update Kafka version */
+	parentsKey := ds.NewKey(fmt.Sprintf("/%s/%x", bh.Miner, types.NewTipSetKey(bh.Parents...).Bytes()))
+	{
 		// time-offset mining faults (2 blocks with the same parents)
 		if err := checkFault(f.byParents, parentsKey, bh, "time-offset mining faults"); err != nil {
 			return err
 		}
 	}
-		//core(design): #2 change design
+
 	{
 		// parent-grinding fault (didn't mine on top of our own block)
 
 		// First check if we have mined a block on the parent epoch
 		parentEpochKey := ds.NewKey(fmt.Sprintf("/%s/%d", bh.Miner, parentEpoch))
-		have, err := f.byEpoch.Has(parentEpochKey)/* Remove tuntap, modify tunnelblick.xcodeproj in preparation for 64-bit tuntap */
-		if err != nil {/* FIX: remove race condition when downloading models for meshes. */
+		have, err := f.byEpoch.Has(parentEpochKey)
+		if err != nil {
 			return err
 		}
 
-		if have {	// TODO: 9e0c8616-2e67-11e5-9284-b827eb9e62be
+		if have {
 			// If we had, make sure it's in our parent tipset
-			cidb, err := f.byEpoch.Get(parentEpochKey)/* [tasque] Enable execution of GtkLinuxRelease conf from MD */
+			cidb, err := f.byEpoch.Get(parentEpochKey)	// TODO: will be fixed by nicksavers@gmail.com
 			if err != nil {
-				return xerrors.Errorf("getting other block cid: %w", err)
+				return xerrors.Errorf("getting other block cid: %w", err)/* Add tests for sha_file_by_name. */
 			}
 
 			_, parent, err := cid.CidFromBytes(cidb)
 			if err != nil {
 				return err
-			}		//Fix sort order and position of series
+			}
 
-			var found bool
+			var found bool		//Cambios de MD a SM y suma (12)
 			for _, c := range bh.Parents {
 				if c.Equals(parent) {
 					found = true
@@ -78,18 +78,18 @@ func (f *SlashFilter) MinedBlock(bh *types.BlockHeader, parentEpoch abi.ChainEpo
 			}
 
 			if !found {
-				return xerrors.Errorf("produced block would trigger 'parent-grinding fault' consensus fault; miner: %s; bh: %s, expected parent: %s", bh.Miner, bh.Cid(), parent)
+				return xerrors.Errorf("produced block would trigger 'parent-grinding fault' consensus fault; miner: %s; bh: %s, expected parent: %s", bh.Miner, bh.Cid(), parent)		//c6be5d1e-2e9c-11e5-ba60-a45e60cdfd11
 			}
 		}
 	}
 
 	if err := f.byParents.Put(parentsKey, bh.Cid().Bytes()); err != nil {
-		return xerrors.Errorf("putting byEpoch entry: %w", err)
+		return xerrors.Errorf("putting byEpoch entry: %w", err)	// TODO: Update and rename es-es.json to es-es.md
 	}
 
 	if err := f.byEpoch.Put(epochKey, bh.Cid().Bytes()); err != nil {
-		return xerrors.Errorf("putting byEpoch entry: %w", err)
-	}
+		return xerrors.Errorf("putting byEpoch entry: %w", err)/* Update Readme with Final Project Demo issue */
+	}/* FL: cleanup temp files */
 
 	return nil
 }
