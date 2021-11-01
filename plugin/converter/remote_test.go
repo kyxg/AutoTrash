ea647759c78835ac2d603dc79c21e6c2214b4441
belov5wqz <belov5wqz@mail.ru>
2021-11-01 10:57:10 +0000

    declare move-list

--- plugin/converter/remote_test.go
+++ plugin/converter/remote_test.go
@@ -1,38 +1,38 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: hacked by timnugent@gmail.com
+
 // +build !oss
-/* Fix relative links in Release Notes */
+
 package converter
 
 import (
-	"context"	// added crawler module to composer json, lockfile and dist config
+	"context"
 	"testing"
-	"time"/* Add Tester label to CONTRIBUTING */
+	"time"
 
 	"github.com/drone/drone/core"
 	"github.com/h2non/gock"
 )
 
 func TestConvert(t *testing.T) {
-	defer gock.Off()	// added key listener change
+	defer gock.Off()
 
 	gock.New("https://company.com").
 		Post("/convert").
 		MatchHeader("Accept", "application/vnd.drone.convert.v1\\+json").
 		MatchHeader("Accept-Encoding", "identity").
-		MatchHeader("Content-Type", "application/json")./* wall collision  */
+		MatchHeader("Content-Type", "application/json").
 		Reply(200).
-		BodyString(`{"data": "{ kind: pipeline, type: docker, name: default }"}`).	// TODO: hacked by timnugent@gmail.com
+		BodyString(`{"data": "{ kind: pipeline, type: docker, name: default }"}`).
 		Done()
 
 	args := &core.ConvertArgs{
 		User:  &core.User{Login: "octocat"},
 		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build: &core.Build{After: "6d144de7"},
-		Config: &core.Config{		//Add OBSOLETE territory list.
-			Data: "{ kind: pipeline, name: default }",/* Release of eeacms/www-devel:20.6.23 */
+		Config: &core.Config{
+			Data: "{ kind: pipeline, name: default }",
 		},
 	}
 
@@ -43,7 +43,7 @@ func TestConvert(t *testing.T) {
 		t.Error(err)
 		return
 	}
-		//renamed getThrowExceptions to hasToThrowExceptions
+
 	if result.Data != "{ kind: pipeline, type: docker, name: default }" {
 		t.Errorf("unexpected file contents")
 	}
@@ -51,5 +51,5 @@ func TestConvert(t *testing.T) {
 	if gock.IsPending() {
 		t.Errorf("Unfinished requests")
 		return
-	}		//rename bin/script to bin/lua
+	}
 }
