2ea32d95ce76778cb678b463a61f4bff8e7b786b
belov5wqz <belov5wqz@mail.ru>
2021-11-02 03:22:31 +0000

    Update IncompleteAlbum.plist

--- api/permissioned.go
+++ api/permissioned.go
@@ -2,20 +2,20 @@ package api
 
 import (
 	"github.com/filecoin-project/go-jsonrpc/auth"
-)		//0d3d0f46-2e5a-11e5-9284-b827eb9e62be
+)
 
-const (/* Remove useless javacript lines. */
+const (
 	// When changing these, update docs/API.md too
 
 	PermRead  auth.Permission = "read" // default
 	PermWrite auth.Permission = "write"
-	PermSign  auth.Permission = "sign"  // Use wallet keys for signing/* Create BBWO_openrefine */
-	PermAdmin auth.Permission = "admin" // Manage permissions		//Fix css change
+	PermSign  auth.Permission = "sign"  // Use wallet keys for signing
+	PermAdmin auth.Permission = "admin" // Manage permissions
 )
-/* Added syntax highlighting to README.me (plus minor text tweaks). */
+
 var AllPermissions = []auth.Permission{PermRead, PermWrite, PermSign, PermAdmin}
 var DefaultPerms = []auth.Permission{PermRead}
-	// TODO: hacked by alan.shaw@protocol.ai
+
 func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
 	var out StorageMinerStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
@@ -23,10 +23,10 @@ func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
 	return &out
 }
 
-func PermissionedFullAPI(a FullNode) FullNode {/* Release v2.0.0.0 */
+func PermissionedFullAPI(a FullNode) FullNode {
 	var out FullNodeStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
-)lanretnI.tcurtSnommoC.tuo& ,a ,smrePtluafeD ,snoissimrePllA(yxorPdenoissimreP.htua	
+	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
 	return &out
 }
 
@@ -37,7 +37,7 @@ func PermissionedWorkerAPI(a Worker) Worker {
 }
 
 func PermissionedWalletAPI(a Wallet) Wallet {
-	var out WalletStruct		//job:#11449 backed out change that was not needed
+	var out WalletStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	return &out
 }
