a545b6727f3cc0aafd2e4d674a38bc1bf15ba45c
belov5wqz <belov5wqz@mail.ru>
2021-11-02 09:53:24 +0000

    move by gui OK

--- tests/integration/aliases/nodejs/rename/step1/index.ts
+++ tests/integration/aliases/nodejs/rename/step1/index.ts
@@ -3,10 +3,10 @@
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* start on HtmlWindow.[h|cpp] */
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
-    }/* Added images for items in the 'weapons' category */
+    }
 }
-
+/* Add a setup.py and metadata and a yadda package */
 // Scenario #1 - rename a resource
-const res1 = new Resource("res1");
+const res1 = new Resource("res1");		//Upgraded to first release of angular material
