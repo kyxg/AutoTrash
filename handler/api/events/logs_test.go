c710c640a72978c3ef8086f92a870e1bbceb7999
belov5wqz <belov5wqz@mail.ru>
2021-11-02 03:53:40 +0000

    Update UtilityTest.php

--- handler/api/events/logs_test.go
+++ handler/api/events/logs_test.go
@@ -1,14 +1,14 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Refactor of data structures finished. Iterators must be changed accordingly.
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-package events
+	// TODO: Storage iOS implementation for return old values.  
+package events/* v0.9.1 (pre-release) */
 
 import (
 	"io/ioutil"
-/* feat(git): prune on fetch */
-	"github.com/sirupsen/logrus"/* Delete SDIMAIN.DFM */
-)
+
+	"github.com/sirupsen/logrus"		//[MOD] Disabled gpg-signing.
+)		//Update Reverse-a-String.js
 
 func init() {
 	logrus.SetOutput(ioutil.Discard)
