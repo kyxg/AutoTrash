d966633b3293e05aa062ac5cce18455d2eab3b28
belov5wqz <belov5wqz@mail.ru>
2021-11-02 05:39:17 +0000

    dcaaca0e-2e4e-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/deploys/delete_test.go
+++ handler/api/repos/builds/deploys/delete_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Add new welcome mat icon
-// that can be found in the LICENSE file./* Release v0.5.0.5 */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
-package deploys/* Tagging a Release Candidate - v4.0.0-rc5. */
+package deploys
