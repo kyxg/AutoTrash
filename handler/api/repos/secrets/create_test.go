df9e26e8635c2643cd3c62a7e80b3d4023c176d5
belov5wqz <belov5wqz@mail.ru>
2021-11-02 03:20:07 +0000

    Write XML file location at end of test run.

--- handler/api/repos/secrets/create_test.go
+++ handler/api/repos/secrets/create_test.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: lib/utils: moved isaac cspring here
-// that can be found in the LICENSE file./* Fix first message styling */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 // +build !oss
 
 package secrets
 
-import (	// TODO: hacked by steven@stebalien.com
+import (
 	"bytes"
 	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"testing"/* Release note wiki for v1.0.13 */
+	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"	// TODO: ef40fe30-4b19-11e5-bd9e-6c40088e03e4
+	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)		//50f1b2f0-2e41-11e5-9284-b827eb9e62be
+)
 
 func TestHandleCreate(t *testing.T) {
 	controller := gomock.NewController(t)
@@ -29,36 +29,36 @@ func TestHandleCreate(t *testing.T) {
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
-	// Protect resolvePath from EMFILE errors
+
 	secrets := mock.NewMockSecretStore(controller)
 	secrets.EXPECT().Create(gomock.Any(), gomock.Any()).Return(nil)
 
-	c := new(chi.Context)		//Create Contoh DMTS
+	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")	// Test commit #3
-	c.URLParams.Add("secret", "github_password")/* fix search page results returned */
+	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("secret", "github_password")
 
 	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(dummySecret)
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", in)
-	r = r.WithContext(/* starting over with bootstrap theme */
+	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)/* Release of eeacms/www:18.7.25 */
+	)
 
-	HandleCreate(repos, secrets).ServeHTTP(w, r)/* Updated welcome/create account-related app/email notifications. [ref #2966] */
+	HandleCreate(repos, secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-		//Geo/WGS84: add missing include
-	got, want := &core.Secret{}, dummySecretScrubbed		//9d06f594-2e53-11e5-9284-b827eb9e62be
+
+	got, want := &core.Secret{}, dummySecretScrubbed
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
 }
-/* CORA-110 Records being created can never have incoming links */
+
 func TestHandleCreate_ValidationError(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
