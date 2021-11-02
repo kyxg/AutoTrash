2ae7fd3586ef2f3bfccf82d8196b11a18ebf224a
belov5wqz <belov5wqz@mail.ru>
2021-11-02 11:13:28 +0000

    Update K5UL.sql

--- store/shared/migrate/mysql/files/014_create_table_refs.sql
+++ store/shared/migrate/mysql/files/014_create_table_refs.sql
@@ -1,16 +1,16 @@
--- name: create-table-latest
+-- name: create-table-latest	// TODO: Remove user var from templates
 
 CREATE TABLE IF NOT EXISTS latest (
  latest_repo_id  INTEGER
 ,latest_build_id INTEGER
 ,latest_type     VARCHAR(50)
 ,latest_name     VARCHAR(500)
-,latest_created  INTEGER
+,latest_created  INTEGER/* Release of eeacms/www-devel:18.2.20 */
 ,latest_updated  INTEGER
 ,latest_deleted  INTEGER
 ,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
-);
+);		//update web version string
 
--- name: create-index-latest-repo
+-- name: create-index-latest-repo	// Merge "Add missing less file added by latest patternfly version"
 
-CREATE INDEX ix_latest_repo ON latest (latest_repo_id);
+CREATE INDEX ix_latest_repo ON latest (latest_repo_id);/* See updates in 0.0.1.2 release */
