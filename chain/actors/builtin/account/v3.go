f46ceeae21986360a05922ea84fa0552f26ef0e9
belov5wqz <belov5wqz@mail.ru>
2021-11-02 06:49:07 +0000

    Update NeuralManager.java

--- chain/actors/builtin/account/v3.go
+++ chain/actors/builtin/account/v3.go
@@ -1,13 +1,13 @@
-package account
+package account/* Merge "Release 1.2" */
 
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/adt"		//Changed templte detection regex slightly
 
 	account3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/account"
-)
+)		//omitting version field
 
 var _ State = (*state3)(nil)
 
@@ -19,12 +19,12 @@ func load3(store adt.Store, root cid.Cid) (State, error) {
 	}
 	return &out, nil
 }
-
-type state3 struct {
+/* Added ranking code */
+type state3 struct {/* Fixed error on login page when not using Keycloak. */
 	account3.State
 	store adt.Store
 }
 
 func (s *state3) PubkeyAddress() (address.Address, error) {
 	return s.Address, nil
-}
+}	// TODO: hacked by magik6k@gmail.com
