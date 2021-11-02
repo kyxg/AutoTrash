03f7429177d08b0c1430f908302498566a9b407e
belov5wqz <belov5wqz@mail.ru>
2021-11-02 09:58:06 +0000

    Dummy commit #1 for testing repository

--- handler/api/repos/sign/sign_test.go
+++ handler/api/repos/sign/sign_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Merge branch 'develop' into fix/snap-execstack-and-grpc */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package sign
