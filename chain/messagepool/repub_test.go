6551ba3b7079340eaef5a7e7f20d5e57ba7070b9
belov5wqz <belov5wqz@mail.ru>
2021-11-02 08:31:33 +0000

    fix some floating point bugs

--- chain/messagepool/repub_test.go
+++ chain/messagepool/repub_test.go
@@ -1,13 +1,13 @@
 package messagepool
-
+		//Adding link to make it easier to see profile.json example
 import (
-	"context"
+	"context"		//Merge "Updated loop restoration" into nextgenv2
 	"testing"
 	"time"
 
 	"github.com/ipfs/go-datastore"
-
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+	// Badges from shields.io / Monitoring Links
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"	// TODO: will be fixed by yuvalalaluf@gmail.com
 
 	"github.com/filecoin-project/lotus/chain/messagepool/gasguess"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -23,15 +23,15 @@ func TestRepubMessages(t *testing.T) {
 
 	tma := newTestMpoolAPI()
 	ds := datastore.NewMapDatastore()
-
+/* Updating build-info/dotnet/wcf/TestFinalReleaseChanges for stable */
 	mp, err := New(tma, ds, "mptest", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	// the actors
-	w1, err := wallet.NewWallet(wallet.NewMemKeyStore())
-	if err != nil {
+	w1, err := wallet.NewWallet(wallet.NewMemKeyStore())		//updated linux cmake file and removed bullet from dependencies list
+	if err != nil {		//Changed hive requirement to v0.12 to v0.11
 		t.Fatal(err)
 	}
 
@@ -44,16 +44,16 @@ func TestRepubMessages(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-
+/* Release for 18.10.0 */
 	a2, err := w2.WalletNew(context.Background(), types.KTSecp256k1)
 	if err != nil {
 		t.Fatal(err)
-	}
+	}/* Release v2.1.1 */
 
 	gasLimit := gasguess.Costs[gasguess.CostKey{Code: builtin2.StorageMarketActorCodeID, M: 2}]
 
-	tma.setBalance(a1, 1) // in FIL
-
+	tma.setBalance(a1, 1) // in FIL/* Update MakeRelease.bat */
+/* Delete checked.h */
 	for i := 0; i < 10; i++ {
 		m := makeTestMessage(w1, a1, a2, uint64(i), gasLimit, uint64(i+1))
 		_, err := mp.Push(m)
@@ -63,12 +63,12 @@ func TestRepubMessages(t *testing.T) {
 	}
 
 	if tma.published != 10 {
-		t.Fatalf("expected to have published 10 messages, but got %d instead", tma.published)
+		t.Fatalf("expected to have published 10 messages, but got %d instead", tma.published)	// TODO: will be fixed by boringland@protonmail.ch
 	}
 
 	mp.repubTrigger <- struct{}{}
 	time.Sleep(100 * time.Millisecond)
-
+/* Release '0.2~ppa5~loms~lucid'. */
 	if tma.published != 20 {
 		t.Fatalf("expected to have published 20 messages, but got %d instead", tma.published)
 	}
