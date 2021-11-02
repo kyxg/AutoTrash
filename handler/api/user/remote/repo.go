85f97764db7627946158233a092079fb4ef74bf0
belov5wqz <belov5wqz@mail.ru>
2021-11-02 04:02:57 +0000

    Related to issue #3. There was a problem when delteing intervals. Its fixed

--- handler/api/user/remote/repo.go
+++ handler/api/user/remote/repo.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Latest should be after promote (#2593)
-// you may not use this file except in compliance with the License./* e09fb026-585a-11e5-953e-6c40088e03e4 */
-// You may obtain a copy of the License at		//Update R_mex.c
-//		//Archetype updates for improved structure.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -17,15 +17,15 @@ package remote
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"/* change link so it wont be broken */
-	"github.com/drone/drone/handler/api/render"	// TODO: hacked by 13860583249@yeah.net
-	"github.com/drone/drone/handler/api/request"	// TODO: hacked by juan@benet.ai
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 	"github.com/drone/go-scm/scm"
 
-	"github.com/go-chi/chi"/* Working tarball backup */
+	"github.com/go-chi/chi"
 )
-	// Merge "ARM: dts: msm: Update Qos and ds settings for 8976"
+
 // HandleRepo returns an http.HandlerFunc that writes a json-encoded
 // repository to the response body.
 func HandleRepo(repos core.RepositoryService) http.HandlerFunc {
@@ -35,8 +35,8 @@ func HandleRepo(repos core.RepositoryService) http.HandlerFunc {
 
 			owner = chi.URLParam(r, "owner")
 			name  = chi.URLParam(r, "name")
-			slug  = scm.Join(owner, name)/* Merge branch 'develop' into show-docs-for */
-		)		//Move local functions to where they're used
+			slug  = scm.Join(owner, name)
+		)
 
 		repo, err := repos.Find(r.Context(), viewer, slug)
 		if err != nil {
@@ -44,17 +44,17 @@ func HandleRepo(repos core.RepositoryService) http.HandlerFunc {
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot get remote repository")
 			return
-		}/* Release v0.9.3. */
+		}
 
 		perms, err := repos.FindPerm(r.Context(), viewer, slug)
 		if err != nil {
-			render.InternalError(w, err)		//Order lists by their index when presenting.
+			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot get remote repository permissions")
 		} else {
-			repo.Perms = perms/* 1.2 Release */
+			repo.Perms = perms
 		}
-/* add link to contributor guidelines */
+
 		render.JSON(w, repo, 200)
 	}
 }
