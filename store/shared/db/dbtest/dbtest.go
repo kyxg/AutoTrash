054c6898c114b5a7428d555238c14a32d3d25b8e
belov5wqz <belov5wqz@mail.ru>
2021-11-02 07:49:56 +0000

    Merge "Stricter handling of query_string syntax detection"

--- store/shared/db/dbtest/dbtest.go
+++ store/shared/db/dbtest/dbtest.go
@@ -1,14 +1,14 @@
-// Copyright 2019 Drone IO, Inc./* Set install path on OSX */
-//	// Create form_object.min.js
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* add "flex-tool-bar" package */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release strict forbiddance in README.md license */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -19,12 +19,12 @@ import (
 
 	"github.com/drone/drone/store/shared/db"
 
-srevird esabatad daol ot desu era stropmi knalb //	
+	// blank imports are used to load database drivers
 	// for unit tests. Only unit tests should be importing
 	// this package.
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
-	_ "github.com/mattn/go-sqlite3"/* Release date attribute */
+	_ "github.com/mattn/go-sqlite3"
 )
 
 // Connect opens a new test database connection.
@@ -34,25 +34,25 @@ func Connect() (*db.DB, error) {
 		config = ":memory:?_foreign_keys=1"
 	)
 	if os.Getenv("DRONE_DATABASE_DRIVER") != "" {
-		driver = os.Getenv("DRONE_DATABASE_DRIVER")/* Release notes for helper-mux */
-		config = os.Getenv("DRONE_DATABASE_DATASOURCE")	// TODO: hacked by ac0dem0nk3y@gmail.com
+		driver = os.Getenv("DRONE_DATABASE_DRIVER")
+		config = os.Getenv("DRONE_DATABASE_DATASOURCE")
 	}
 	return db.Connect(driver, config)
 }
-/* Release of eeacms/forests-frontend:2.0-beta.53 */
+
 // Reset resets the database state.
-func Reset(d *db.DB) {/* Updated target to new Base version */
+func Reset(d *db.DB) {
 	d.Lock(func(tx db.Execer, _ db.Binder) error {
-		tx.Exec("DELETE FROM cron")	// TODO: Fix build for non-native targets.
-		tx.Exec("DELETE FROM logs")/* Merge "Release 4.4.31.64" */
+		tx.Exec("DELETE FROM cron")
+		tx.Exec("DELETE FROM logs")
 		tx.Exec("DELETE FROM steps")
-		tx.Exec("DELETE FROM stages")/* Release new version to fix problem having coveralls as a runtime dependency */
-		tx.Exec("DELETE FROM latest")		//Update TestPrimeNumbers.py
+		tx.Exec("DELETE FROM stages")
+		tx.Exec("DELETE FROM latest")
 		tx.Exec("DELETE FROM builds")
 		tx.Exec("DELETE FROM perms")
-		tx.Exec("DELETE FROM repos")/* add redux action object compat for dispatch() */
+		tx.Exec("DELETE FROM repos")
 		tx.Exec("DELETE FROM users")
-		tx.Exec("DELETE FROM orgsecrets")/* Rename RecentChanges.md to ReleaseNotes.md */
+		tx.Exec("DELETE FROM orgsecrets")
 		return nil
 	})
 }
