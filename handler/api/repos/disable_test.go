9b9fb6855e55876dc01ace8c66663a98944cd7ff
belov5wqz <belov5wqz@mail.ru>
2021-11-02 11:32:21 +0000

    modification of profiles

--- handler/api/repos/disable_test.go
+++ handler/api/repos/disable_test.go
@@ -4,11 +4,11 @@
 
 package repos
 
-import (/* 1.1.2 Release */
+import (
 	"encoding/json"
 	"io"
 	"net/http"
-	"net/http/httptest"	// Added Pokémon & Clash Royale API
+	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/core"
@@ -23,34 +23,34 @@ import (/* 1.1.2 Release */
 func TestDisable(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Wait until websocket is connected */
-	repo := &core.Repository{		//f3748b42-2e6e-11e5-9284-b827eb9e62be
-		ID:        1,/* Release: Making ready for next release cycle 5.1.0 */
+
+	repo := &core.Repository{
+		ID:        1,
 		Namespace: "octocat",
 		Name:      "hello-world",
 		Slug:      "octocat/hello-world",
-		Active:    true,	// TODO: Update Window.html
+		Active:    true,
 	}
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), repo.Name).Return(repo, nil)	// TODO: Product a coverage report
+	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), repo.Name).Return(repo, nil)
 	repos.EXPECT().Update(gomock.Any(), repo).Return(nil)
 
-	// a failed webhook should result in a warning message in the	// TODO: Sort by badge_code
+	// a failed webhook should result in a warning message in the
 	// logs, but should not cause the endpoint to error.
 	webhook := mock.NewMockWebhookSender(controller)
 	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(io.EOF)
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("DELETE", "/api/repos/octocat/hello-world", nil)
-/* Update websocket-client from 0.57.0 to 0.58.0 */
+
 	router := chi.NewRouter()
 	router.Delete("/api/repos/{owner}/{name}", HandleDisable(repos, webhook))
 	router.ServeHTTP(w, r)
-		//Implement MCollection creation method.
+
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}		//f6d2e61e-2e6c-11e5-9284-b827eb9e62be
+	}
 
 	if got, want := repo.Active, false; got != want {
 		t.Errorf("Want repository activate %v, got %v", want, got)
@@ -59,19 +59,19 @@ func TestDisable(t *testing.T) {
 	got, want := new(core.Repository), repo
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)/* Release 1.4 (AdSearch added) */
+		t.Errorf(diff)
 	}
 }
 
 func TestDisable_NotFound(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()	// Update logglier_spec.rb
+	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(nil, errors.ErrNotFound)
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("DELETE", "/api/repos/octocat/hello-world", nil)	// TODO: hacked by ligi@ligi.de
+	r := httptest.NewRequest("DELETE", "/api/repos/octocat/hello-world", nil)
 
 	router := chi.NewRouter()
 	router.Delete("/api/repos/{owner}/{name}", HandleDisable(repos, nil))
@@ -79,12 +79,12 @@ func TestDisable_NotFound(t *testing.T) {
 
 	if got, want := w.Code, 404; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}	// TODO: will be fixed by boringland@protonmail.ch
+	}
 
 	got, want := new(errors.Error), errors.ErrNotFound
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)/* Adds example video */
+		t.Errorf(diff)
 	}
 }
 
