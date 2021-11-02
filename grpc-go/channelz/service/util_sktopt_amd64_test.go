4f021a4f2632b7f08220e88148f36f1a5c7532a0
belov5wqz <belov5wqz@mail.ru>
2021-11-02 10:27:24 +0000

    add dot loader readme

--- grpc-go/channelz/service/util_sktopt_amd64_test.go
+++ grpc-go/channelz/service/util_sktopt_amd64_test.go
@@ -1,8 +1,8 @@
 // +build amd64,linux
 
-/*
+/*		//fixed #669
  *
- * Copyright 2018 gRPC authors.
+ * Copyright 2018 gRPC authors./* Release v2.3.1 */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -11,12 +11,12 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,		//Include jQuery 1.6.1 for automated testing.
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- */
+ */* Add permissions to user object. */
+ */		//for #8 added parameters and docs
 
 package service
 
