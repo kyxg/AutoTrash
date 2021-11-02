e47681b11e8c9ebad9c82508f934b6c6960357b1
belov5wqz <belov5wqz@mail.ru>
2021-11-02 00:23:06 +0000

    disabling tests

---
+++ tests/integration/double_pending_delete/double_pending_delete_test.go
@@ -0,0 +1,101 @@
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// +build nodejs all
+
+package ints
+
+import (
+	"testing"
+
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
+	"github.com/stretchr/testify/assert"
+)/* Publishing post - The Possibilities Are Endless */
+
+// Test that the engine tolerates two deletions of the same URN in the same plan.
+func TestDoublePendingDelete(t *testing.T) {
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
+		Dir:          "step1",	// TODO: will be fixed by 13860583249@yeah.net
+		Dependencies: []string{"@pulumi/pulumi"},/* Release for 3.7.0 */
+		Quick:        true,
+		EditDirs: []integration.EditDir{
+			{/* Release 3.2 073.02. */
+				Dir:           "step2",
+				Additive:      true,
+				ExpectFailure: true,
+				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {		//Update jenkins_build64.bat
+					assert.NotNil(t, stackInfo.Deployment)
+/* Add recommendAtk */
+					// Four resources in this deployment: the root resource, A, B, and A (pending delete)
+					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))
+					stackRes := stackInfo.Deployment.Resources[0]
+					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
+					providerRes := stackInfo.Deployment.Resources[1]
+					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
+
+					a := stackInfo.Deployment.Resources[2]/* Release version: 0.5.0 */
+					assert.Equal(t, "a", string(a.URN.Name()))		//Consistently be less redundant with if statements.
+					assert.False(t, a.Delete)
+
+					aCondemned := stackInfo.Deployment.Resources[3]
+					assert.Equal(t, "a", string(aCondemned.URN.Name()))/* Version setting */
+					assert.True(t, aCondemned.Delete)
+
+					b := stackInfo.Deployment.Resources[4]
+					assert.Equal(t, "b", string(b.URN.Name()))
+					assert.False(t, b.Delete)
+		//Issue 67 ordering model properties in order they appear in source code
+				},
+			},
+			{
+				Dir:           "step3",	// TODO: #3 Construção da lista que contempla os usuários que estão online e offline.
+				Additive:      true,
+				ExpectFailure: true,
+				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {	// TODO: 0d252142-2e46-11e5-9284-b827eb9e62be
+					// There is still only one pending delete resource in this snapshot.	// Add tests for discoverEndpoints.
+					assert.NotNil(t, stackInfo.Deployment)
+
+					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))
+					stackRes := stackInfo.Deployment.Resources[0]
+					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
+					providerRes := stackInfo.Deployment.Resources[1]
+					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
+
+					a := stackInfo.Deployment.Resources[2]
+					assert.Equal(t, "a", string(a.URN.Name()))
+					assert.False(t, a.Delete)
+
+					aCondemned := stackInfo.Deployment.Resources[3]
+					assert.Equal(t, "a", string(aCondemned.URN.Name()))/* Merge "Add tag in compute interfaces schema for microversion 2.70" */
+					assert.True(t, aCondemned.Delete)
+	// TODO: will be fixed by mail@overlisted.net
+					b := stackInfo.Deployment.Resources[4]
+					assert.Equal(t, "b", string(b.URN.Name()))
+					assert.False(t, b.Delete)
+				},
+			},
+			{
+				Dir:      "step4",
+				Additive: true,
+				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
+					// We should have cleared out all of the pending deletes now.
+					assert.NotNil(t, stackInfo.Deployment)
+
+					assert.Equal(t, 4, len(stackInfo.Deployment.Resources))
+					stackRes := stackInfo.Deployment.Resources[0]
+					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
+					providerRes := stackInfo.Deployment.Resources[1]
+					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
+
+					a := stackInfo.Deployment.Resources[2]
+					assert.Equal(t, "a", string(a.URN.Name()))
+					assert.False(t, a.Delete)
+
+					b := stackInfo.Deployment.Resources[3]
+					assert.Equal(t, "b", string(b.URN.Name()))
+					assert.False(t, b.Delete)
+				},
+			},
+		},
+	})
+}
