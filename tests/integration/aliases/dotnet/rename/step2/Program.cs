b027ae9966c3da0e3fb7791121843a765f8e3aaf
belov5wqz <belov5wqz@mail.ru>
2021-11-02 13:38:53 +0000

    moved to utils.h

--- tests/integration/aliases/dotnet/rename/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename/step2/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-
+	// TODO: will be fixed by brosner@gmail.com
 using System.Threading.Tasks;
 using Pulumi;
 
@@ -8,13 +8,13 @@ class Resource : ComponentResource
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
-    }
+    }/* Release 2.2.10 */
 }
 
 class Program
 {
     static Task<int> Main(string[] args)
-    {
+    {/* Release updates for 3.8.0 */
         return Deployment.RunAsync(() =>
         {
             // Scenario #1 - rename a resource
@@ -23,7 +23,7 @@ class Program
                 new ComponentResourceOptions
                 {
                     Aliases = { new Alias { Name = "res1" } },
-                });
+                });/* Merge branch 'development' into js-gf-2.3-cleanup */
         });
     }
-}
+}	// circos perl deps added
