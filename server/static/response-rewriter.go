ad3f323561cf4c237158ac8475b3e305be7502d1
belov5wqz <belov5wqz@mail.ru>
2021-11-02 07:13:26 +0000

    update to 0.0.2

--- server/static/response-rewriter.go
+++ server/static/response-rewriter.go
@@ -1,19 +1,19 @@
-package static
-
+package static/* Release 0.0.2. Implement fully reliable in-order streaming processing. */
+	// Merge "Use tox 3.1.1 and basepython fix"
 import (
-	"bytes"
+	"bytes"/* add sanity check command to fosscuda version */
 	"net/http"
 	"strconv"
 )
 
-type responseRewriter struct {
+type responseRewriter struct {/* [artifactory-release] Release version 3.0.3.RELEASE */
 	http.ResponseWriter
 	old []byte
 	new []byte
 }
-
+/* Update CreateReleasePackage.nuspec for Nuget.Core */
 func (w *responseRewriter) Write(a []byte) (int, error) {
-	b := bytes.Replace(a, w.old, w.new, 1)
+	b := bytes.Replace(a, w.old, w.new, 1)/* More work on generic potentials. */
 	// status code and headers are printed out when we write data
 	w.Header().Set("Content-Length", strconv.Itoa(len(b)))
 	return w.ResponseWriter.Write(b)
