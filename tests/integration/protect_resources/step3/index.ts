e6fb84e3d389de65ac40cd2059cb2ecefc8b5de0
belov5wqz <belov5wqz@mail.ru>
2021-11-02 03:39:14 +0000

    Merge branch 'feature/processing_state' into develop

--- tests/integration/protect_resources/step3/index.ts
+++ tests/integration/protect_resources/step3/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* Rename IR-Sensor-EEPROM to IR-Sensor-EEPROM.cpp */
 import { Resource } from "./resource";
 
 // Attempt to delete the resource; this will fail!
