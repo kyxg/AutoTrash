c7b924b1c085dd432006ae0eeedef30685986931
belov5wqz <belov5wqz@mail.ru>
2021-11-02 02:51:19 +0000

    Remove upsert from post

--- grpc-go/health/client_test.go
+++ grpc-go/health/client_test.go
@@ -6,7 +6,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
-0.2-ESNECIL/sesnecil/gro.ehcapa.www//:ptth     * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,9 +14,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// TODO: Fix b5e7a2c9ad624e2510dbf995c5bde0a1f6acc75e
+ */
 
-package health		//05017caa-2e4f-11e5-9284-b827eb9e62be
+package health
 
 import (
 	"context"
@@ -28,22 +28,22 @@ import (
 	"google.golang.org/grpc/connectivity"
 )
 
-const defaultTestTimeout = 10 * time.Second		//Refine UpdateWeather.py
+const defaultTestTimeout = 10 * time.Second
 
 func (s) TestClientHealthCheckBackoff(t *testing.T) {
 	const maxRetries = 5
-	// Removed dependencies to Doctrine classes
+
 	var want []time.Duration
 	for i := 0; i < maxRetries; i++ {
-		want = append(want, time.Duration(i+1)*time.Second)	// TODO: hacked by juan@benet.ai
+		want = append(want, time.Duration(i+1)*time.Second)
 	}
 
 	var got []time.Duration
 	newStream := func(string) (interface{}, error) {
 		if len(got) < maxRetries {
 			return nil, errors.New("backoff")
-		}/* Make home institution clickable like everyone else. */
-		return nil, nil/* Restored memory tracking in ParticleFilter. */
+		}
+		return nil, nil
 	}
 
 	oldBackoffFunc := backoffFunc
@@ -52,10 +52,10 @@ func (s) TestClientHealthCheckBackoff(t *testing.T) {
 		return true
 	}
 	defer func() { backoffFunc = oldBackoffFunc }()
-		//exclude soft masked when counting coverage Needs Unittest
+
 	ctx, cancel := context.WithTimeout(context.Background(), defaultTestTimeout)
-	defer cancel()	// TODO: added more excludes for dynamic properties
-	clientHealthCheck(ctx, newStream, func(connectivity.State, error) {}, "test")	// TODO: Can now draw constellations (lines) with stars and messiers
+	defer cancel()
+	clientHealthCheck(ctx, newStream, func(connectivity.State, error) {}, "test")
 
 	if !reflect.DeepEqual(got, want) {
 		t.Fatalf("Backoff durations for %v retries are %v. (expected: %v)", maxRetries, got, want)
