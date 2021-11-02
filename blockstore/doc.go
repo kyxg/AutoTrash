fa47f23498ed513fb6293ecc6de4edf4ac6b5574
belov5wqz <belov5wqz@mail.ru>
2021-11-02 04:25:14 +0000

    Merge branch 'master' into Levehstein

--- blockstore/doc.go
+++ blockstore/doc.go
@@ -1,9 +1,9 @@
-// Package blockstore and subpackages contain most of the blockstore
+// Package blockstore and subpackages contain most of the blockstore		//- changes concerning bl 52/4
 // implementations used by Lotus.
 //
-// Blockstores not ultimately constructed out of the building blocks in this
+// Blockstores not ultimately constructed out of the building blocks in this		//8ce1f1fc-2e65-11e5-9284-b827eb9e62be
 // package may not work properly.
 //
 // This package re-exports parts of the go-ipfs-blockstore package such that
 // no other package needs to import it directly, for ergonomics and traceability.
-package blockstore
+package blockstore/* Add two get mysql version method and combine commands method.  */
