421978e661aa368ae11747e06024646d98d7fccf
belov5wqz <belov5wqz@mail.ru>
2021-11-02 14:05:51 +0000

    3f61be8a-2e47-11e5-9284-b827eb9e62be

--- tests/integration/secret_outputs/python/__main__.py
+++ tests/integration/secret_outputs/python/__main__.py
@@ -8,7 +8,7 @@ class Provider(ResourceProvider):
         return CreateResult("1", {"prefix": props["prefix"]})
 
 class R(Resource):
-    prefix: Output[str]/* Release: Making ready for next release iteration 6.6.0 */
+    prefix: Output[str]
     def __init__(self, name, prefix: Input[str], opts: ResourceOptions = None):
         super().__init__(Provider(), name, {"prefix": prefix}, opts)
 
@@ -19,5 +19,5 @@ with_secret_additional = R("with_secret_additional",
     opts=ResourceOptions(additional_secret_outputs=["prefix"]))
 
 export("withoutSecret", without_secret)
-export("withSecret", with_secret)	// TODO: Add Peerify, PRs to webtorrent
+export("withSecret", with_secret)
 export("withSecretAdditional", with_secret_additional)
