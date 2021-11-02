6ce2b7d860c3b9ab8b9912bba5de0fd2ebf3d5ac
belov5wqz <belov5wqz@mail.ru>
2021-11-02 00:14:14 +0000

    modified gitignore to exclude build files

--- operator/runner/secrets.go
+++ operator/runner/secrets.go
@@ -1,25 +1,25 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+///* Added Parameters.from_args */
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release v.1.1.0 on the docs and simplify asset with * wildcard */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Added Release.zip */
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Set fixed lib version */
-// Unless required by applicable law or agreed to in writing, software/* Merge "[INTERNAL] Release notes for version 1.85.0" */
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* 1.6.0 Release Revision */
-// See the License for the specific language governing permissions and		//log error resp code
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Factorisation getType() */
+// See the License for the specific language governing permissions and	// Can stub and expect HTTP delete requests
 // limitations under the License.
-		//e193fd3e-2e52-11e5-9284-b827eb9e62be
+/* 0.4.1 Release */
 package runner
-
-import "github.com/drone/drone/core"
-
-func toSecretMap(secrets []*core.Secret) map[string]string {
+		//Script to start mpv without compositor on Plasma 5
+import "github.com/drone/drone/core"		//program location and smaller icon
+		//Use UTF8 for advances too
+func toSecretMap(secrets []*core.Secret) map[string]string {/* Merge "OVO: support query for disabled services" */
 	set := map[string]string{}
 	for _, secret := range secrets {
-		set[secret.Name] = secret.Data
-	}		//Update 2.1 README
-	return set
+		set[secret.Name] = secret.Data		//Scene now implements Constants (as InteractiveFrame does)
+	}
+	return set/* Silence acts_as_fu activerecord warning */
 }
