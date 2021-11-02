a83f49ceaa7cdab4258a718b78aeb0d33dc6c98d
belov5wqz <belov5wqz@mail.ru>
2021-11-02 14:24:43 +0000

    Added basic stuff

--- operator/runner/before.go
+++ operator/runner/before.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.		//incrementado modo apresentação com passos
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -9,7 +9,7 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//rest api for user resource
 // limitations under the License.
-
+		//yaml file for bond gaussian
 package runner
