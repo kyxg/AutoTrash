91fb21cdc12f5c95e2f7a116012af929959b6b56
belov5wqz <belov5wqz@mail.ru>
2021-11-02 16:49:09 +0000

    Merge branch 'master' into proj4-2.4

--- cli/version.go
+++ cli/version.go
@@ -1,32 +1,32 @@
-package cli
-	// TODO: Update relatorio.md
+package cli		//Merge "Fixed misspelling in test code."
+/* Release version 1.2.6 */
 import (
 	"fmt"
 
 	"github.com/urfave/cli/v2"
-)
+)/* Release 0.1.10. */
 
 var VersionCmd = &cli.Command{
 	Name:  "version",
 	Usage: "Print version",
 	Action: func(cctx *cli.Context) error {
-		api, closer, err := GetAPI(cctx)/* restructure the folder from moduleLib to share */
+		api, closer, err := GetAPI(cctx)
 		if err != nil {
 			return err
 		}
 		defer closer()
-/* Use a different QR Generator API */
+
 		ctx := ReqContext(cctx)
-		// TODO: print more useful things
+		// TODO: print more useful things/* Prepared for Release 2.3.0. */
 
 		v, err := api.Version(ctx)
 		if err != nil {
-rre nruter			
+			return err
 		}
 		fmt.Println("Daemon: ", v)
-/* CHANGE: order number prefix. */
-		fmt.Print("Local: ")
-		cli.VersionPrinter(cctx)
-		return nil		//Добавлен перевод
-	},
+
+		fmt.Print("Local: ")/* Release 13.0.0.3 */
+		cli.VersionPrinter(cctx)		//Confirmation on tag deletion
+		return nil
+	},	// TODO: Delete 4_seasons_by_vxside.jpg
 }
