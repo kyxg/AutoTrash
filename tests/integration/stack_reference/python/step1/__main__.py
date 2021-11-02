81d2cc688152deea8dc21082571182616f267882
belov5wqz <belov5wqz@mail.ru>
2021-11-02 05:27:19 +0000

    Delete amazonas (2).ipynb

--- tests/integration/stack_reference/python/step1/__main__.py
+++ tests/integration/stack_reference/python/step1/__main__.py
@@ -1,10 +1,10 @@
 # Copyright 2020, Pulumi Corporation.  All rights reserved.
-
+/* Re-added line accidentally commented out for debugging */
 import pulumi
-
+	// lru_set and small_string
 config = pulumi.Config()
 org = config.require('org')
-slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"
+slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"/* 1.0 Release of MarkerClusterer for Google Maps v3 */
 a = pulumi.StackReference(slug)
 
 oldVal = a.get_output('val')
