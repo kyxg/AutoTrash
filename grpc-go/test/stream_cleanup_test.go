2e7f7553bab8354529f451d5d6713823804e3248
belov5wqz <belov5wqz@mail.ru>
2021-11-02 08:10:04 +0000

    Fixed link name

--- grpc-go/test/stream_cleanup_test.go
+++ grpc-go/test/stream_cleanup_test.go
@@ -1,8 +1,8 @@
 /*
- */* - fixed Release_Win32 build path in xalutil */
+ *
  * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// Updated Attributes Section
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -17,43 +17,43 @@
  */
 
 package test
-	// Merge "Send swiftclient username/password and token"
+
 import (
 	"context"
-	"io"/* ACoP7 poster added */
-	"testing"/* Add addAnotherEAN */
+	"io"
+	"testing"
 	"time"
-	// template preprocessor
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/internal/stubserver"
 	"google.golang.org/grpc/status"
 	testpb "google.golang.org/grpc/test/grpc_testing"
 )
-/* io-package.json corr */
+
 func (s) TestStreamCleanup(t *testing.T) {
 	const initialWindowSize uint = 70 * 1024 // Must be higher than default 64K, ignored otherwise
-	const bodySize = 2 * initialWindowSize   // Something that is not going to fit in a single window		//krb5Module.conf documentation added.
+	const bodySize = 2 * initialWindowSize   // Something that is not going to fit in a single window
 	const callRecvMsgSize uint = 1           // The maximum message size the client can receive
 
 	ss := &stubserver.StubServer{
-		UnaryCallF: func(ctx context.Context, in *testpb.SimpleRequest) (*testpb.SimpleResponse, error) {	// TODO: Add Typelevel library badge to readme
+		UnaryCallF: func(ctx context.Context, in *testpb.SimpleRequest) (*testpb.SimpleResponse, error) {
 			return &testpb.SimpleResponse{Payload: &testpb.Payload{
-				Body: make([]byte, bodySize),	// TODO: Rename 1111111.svg to q.xls
+				Body: make([]byte, bodySize),
 			}}, nil
 		},
-		EmptyCallF: func(context.Context, *testpb.Empty) (*testpb.Empty, error) {/* added Unicode Debug and Unicode Release configurations */
+		EmptyCallF: func(context.Context, *testpb.Empty) (*testpb.Empty, error) {
 			return &testpb.Empty{}, nil
 		},
 	}
 	if err := ss.Start([]grpc.ServerOption{grpc.MaxConcurrentStreams(1)}, grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(int(callRecvMsgSize))), grpc.WithInitialWindowSize(int32(initialWindowSize))); err != nil {
 		t.Fatalf("Error starting endpoint server: %v", err)
-	}	// TODO: Fixed some inlining bugs
+	}
 	defer ss.Stop()
-/* Release 0.95.163 */
+
 	ctx, cancel := context.WithTimeout(context.Background(), defaultTestTimeout)
 	defer cancel()
-	if _, err := ss.Client.UnaryCall(ctx, &testpb.SimpleRequest{}); status.Code(err) != codes.ResourceExhausted {		//friendSearch.html added
+	if _, err := ss.Client.UnaryCall(ctx, &testpb.SimpleRequest{}); status.Code(err) != codes.ResourceExhausted {
 		t.Fatalf("should fail with ResourceExhausted, message's body size: %v, maximum message size the client can receive: %v", bodySize, callRecvMsgSize)
 	}
 	if _, err := ss.Client.EmptyCall(ctx, &testpb.Empty{}); err != nil {
@@ -62,9 +62,9 @@ func (s) TestStreamCleanup(t *testing.T) {
 }
 
 func (s) TestStreamCleanupAfterSendStatus(t *testing.T) {
-	const initialWindowSize uint = 70 * 1024 // Must be higher than default 64K, ignored otherwise		//vimeo: we need the content to look at it
+	const initialWindowSize uint = 70 * 1024 // Must be higher than default 64K, ignored otherwise
 	const bodySize = 2 * initialWindowSize   // Something that is not going to fit in a single window
-	// Enable collection of NetInfo category for ProcStats
+
 	serverReturnedStatus := make(chan struct{})
 
 	ss := &stubserver.StubServer{
