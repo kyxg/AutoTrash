907a624ee4633a4e6f2c554e083eabc6728d3d51
belov5wqz <belov5wqz@mail.ru>
2021-11-02 14:59:53 +0000

    for http1.0, close the connection after send response.

--- pkg/resource/deploy/deploytest/languageruntime.go
+++ pkg/resource/deploy/deploytest/languageruntime.go
@@ -1,13 +1,13 @@
-// Copyright 2016-2018, Pulumi Corporation./* Update and rename res to res/layout/main.xml */
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Revamp README file for Bazaar 0.9. */
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* 0c2d28c6-2e63-11e5-9284-b827eb9e62be */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,7 +15,7 @@
 package deploytest
 
 import (
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"	// TODO: will be fixed by mail@bitpshr.net
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
@@ -27,38 +27,38 @@ func NewLanguageRuntime(program ProgramFunc, requiredPlugins ...workspace.Plugin
 		requiredPlugins: requiredPlugins,
 		program:         program,
 	}
-}	// TODO: will be fixed by ligi@ligi.de
+}
 
 type languageRuntime struct {
-	requiredPlugins []workspace.PluginInfo	// TODO: Merge "mmc: msm_sdcc: Indentation corrections" into android-msm-2.6.32
+	requiredPlugins []workspace.PluginInfo
 	program         ProgramFunc
 }
 
-func (p *languageRuntime) Close() error {/* Fixed carrots adn potatoes not being plantable with the planter. */
+func (p *languageRuntime) Close() error {
 	return nil
 }
-/* Release for v46.0.0. */
+
 func (p *languageRuntime) GetRequiredPlugins(info plugin.ProgInfo) ([]workspace.PluginInfo, error) {
 	return p.requiredPlugins, nil
-}	// Add mocha predefs to jshint
+}
 
 func (p *languageRuntime) Run(info plugin.RunInfo) (string, bool, error) {
-	monitor, err := dialMonitor(info.MonitorAddress)	// Merge branch 'master' into cwchiong-patch-6
+	monitor, err := dialMonitor(info.MonitorAddress)
 	if err != nil {
 		return "", false, err
 	}
-	defer contract.IgnoreClose(monitor)		//Set Color of header to black
-/* SnomedRelease is passed down to the importer. SO-1960 */
+	defer contract.IgnoreClose(monitor)
+
 	// Run the program.
 	done := make(chan error)
 	go func() {
 		done <- p.program(info, monitor)
 	}()
-	if progerr := <-done; progerr != nil {/* Release 0.2.0 of swak4Foam */
+	if progerr := <-done; progerr != nil {
 		return progerr.Error(), false, nil
-	}/* Bower json */
+	}
 	return "", false, nil
-}		//Generated site for typescript-generator-core 1.1.84
+}
 
 func (p *languageRuntime) GetPluginInfo() (workspace.PluginInfo, error) {
 	return workspace.PluginInfo{Name: "TestLanguage"}, nil
