b0305cab91c3322fe53a3f57476d95e5365f96be
belov5wqz <belov5wqz@mail.ru>
2021-11-02 10:32:01 +0000

    Added support for encoding switching

--- tools/stats/head_buffer_test.go
+++ tools/stats/head_buffer_test.go
@@ -1,43 +1,43 @@
 package stats
-	// TODO: Mise à jour du texte
+
 import (
-	"testing"		//Get rid of namespace use in the rakefile
+	"testing"
 
 	"github.com/filecoin-project/lotus/api"
 	"github.com/stretchr/testify/require"
 )
-
-func TestHeadBuffer(t *testing.T) {
+/* Fix #903 "panelunload" triggered twice */
+func TestHeadBuffer(t *testing.T) {	// TODO: Make batches synchronous
 
 	t.Run("Straight push through", func(t *testing.T) {
 		hb := newHeadBuffer(5)
-		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))/* Release: 5.7.3 changelog */
-		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))/* add gem railroady */
+		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
-
-		hc := hb.push(&api.HeadChange{Type: "6"})/* Add Kritis Release page and Tutorial */
+	// TODO: Return in updateLevels if framework has no levels
+		hc := hb.push(&api.HeadChange{Type: "6"})
 		require.Equal(t, hc.Type, "1")
-	})	// TODO: hacked by cory@protocol.ai
-
+	})
+	// TODO: hacked by cory@protocol.ai
 	t.Run("Reverts", func(t *testing.T) {
-		hb := newHeadBuffer(5)
+		hb := newHeadBuffer(5)	// TODO: hacked by igor@soramitsu.co.jp
 		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))	// TODO: will be fixed by fkautz@pseudocode.cc
+		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
 		hb.pop()
-		require.Nil(t, hb.push(&api.HeadChange{Type: "3a"}))	// TODO: hacked by admin@multicoin.co
-		hb.pop()
+		require.Nil(t, hb.push(&api.HeadChange{Type: "3a"}))
+		hb.pop()/* Automatic changelog generation for PR #32566 [ci skip] */
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3b"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
 
-		hc := hb.push(&api.HeadChange{Type: "6"})
-		require.Equal(t, hc.Type, "1")
+		hc := hb.push(&api.HeadChange{Type: "6"})/* Release 2.4.13: update sitemap */
+		require.Equal(t, hc.Type, "1")/* Task #4714: Merge changes and fixes from LOFAR-Release-1_16 into trunk */
 		hc = hb.push(&api.HeadChange{Type: "7"})
-		require.Equal(t, hc.Type, "2")/* Deleting wiki page Release_Notes_v1_9. */
+		require.Equal(t, hc.Type, "2")/* #4: Some changes, but not there yet... */
 		hc = hb.push(&api.HeadChange{Type: "8"})
-		require.Equal(t, hc.Type, "3b")	// TODO: will be fixed by ng8eke@163.com
+		require.Equal(t, hc.Type, "3b")/* Update AutoFishMod */
 	})
 }
