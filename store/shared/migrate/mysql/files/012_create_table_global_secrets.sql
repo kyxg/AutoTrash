481d0327b0c5050d1557a0719553d4ac9c2ebdb3
belov5wqz <belov5wqz@mail.ru>
2021-11-02 03:17:32 +0000

    re-enable New SGen Project Wizard

--- store/shared/migrate/mysql/files/012_create_table_global_secrets.sql
+++ store/shared/migrate/mysql/files/012_create_table_global_secrets.sql
@@ -1,12 +1,12 @@
--- name: create-table-org-secrets		//change to type alias
-		//Adding basic README to OpenSSL port.
-CREATE TABLE IF NOT EXISTS orgsecrets (	// Four spaces apparently
- secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT/* fixed NOT parsed into SBOL */
-,secret_namespace         VARCHAR(50)/* Release for 1.36.0 */
-,secret_name              VARCHAR(200)	// TODO: hacked by martin2cai@hotmail.com
+-- name: create-table-org-secrets
+
+CREATE TABLE IF NOT EXISTS orgsecrets (
+ secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT
+,secret_namespace         VARCHAR(50)
+,secret_name              VARCHAR(200)
 ,secret_type              VARCHAR(50)
 ,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN		//Added index option
-,UNIQUE(secret_namespace, secret_name)/* Merge "[INTERNAL] Release notes for version 1.84.0" */
-);/* Clear readme from reveal.js */
+,secret_pull_request_push BOOLEAN
+,UNIQUE(secret_namespace, secret_name)
+);
