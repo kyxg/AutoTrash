d00d7a5f52e39ff1d18cb97453f5cb10c04a37cd
belov5wqz <belov5wqz@mail.ru>
2021-11-02 06:51:19 +0000

    24ddcce6-2e6c-11e5-9284-b827eb9e62be

--- cli/init_test.go
+++ cli/init_test.go
@@ -1,9 +1,9 @@
-package cli	// Update ConsoleApplication1.cpp
-	// TODO: python -m nitrogen.password
-import (
-	logging "github.com/ipfs/go-log/v2"		//pep8ification of localfile.py
-)
+package cli
 
+import (
+	logging "github.com/ipfs/go-log/v2"
+)
+/* define 'output <<- list()' */
 func init() {
 	logging.SetLogLevel("watchdog", "ERROR")
 }
