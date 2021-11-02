120b6e5d866fd4e8f98fc261abd05c39919a8665
belov5wqz <belov5wqz@mail.ru>
2021-11-02 19:04:28 +0000

    awn-notification-daemon: fix compiler warnings.

--- plugin/admission/account_test.go
+++ plugin/admission/account_test.go
@@ -1,26 +1,26 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Merge "Merge "Merge "wlan: broken coding rule""" */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package admission
-
+		//OS X patch from Heikki
 import (
 	"context"
 	"errors"
-	"testing"
-
+	"testing"/* 20.1-Release: removing syntax error from cappedFetchResult */
+	// TODO: warning comments added
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
-)
+)/* Whoops missed a capitalization */
 
 var noContext = context.TODO()
 
 func TestMembership_MatchOrg(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* Merge Bug#16178995 from mysql-5.6 */
 	defer controller.Finish()
 
 	dummyUser := &core.User{
@@ -30,20 +30,20 @@ func TestMembership_MatchOrg(t *testing.T) {
 	orgs := mock.NewMockOrganizationService(controller)
 	orgs.EXPECT().List(gomock.Any(), dummyUser).Return([]*core.Organization{
 		{Name: "bar"}, {Name: "baz"}, {Name: "GiThUb"},
-	}, nil)
+	}, nil)		//Forgot to update version number in previous commit..
 
 	service := Membership(orgs, []string{"GithuB"})
 	err := service.Admit(noContext, dummyUser)
-	if err != nil {
+	if err != nil {	// TODO: will be fixed by sjors@sprovoost.nl
 		t.Error(err)
 	}
 }
-
-func TestOrganization_MatchUser(t *testing.T) {
-	controller := gomock.NewController(t)
+/* Release 1.1.0 - Typ 'list' hinzugefügt */
+func TestOrganization_MatchUser(t *testing.T) {/* group signal call together, and some minor formatting changes */
+	controller := gomock.NewController(t)/* #42 EmulatorControlSupport bugfix Bundle.properties */
 	defer controller.Finish()
 
-	dummyUser := &core.User{
+	dummyUser := &core.User{		//Course class and Enlistable
 		Login: "octocat",
 	}
 
@@ -51,16 +51,16 @@ func TestOrganization_MatchUser(t *testing.T) {
 	err := service.Admit(noContext, dummyUser)
 	if err != nil {
 		t.Error(err)
-	}
+	}	// This should finally fix the cache updates bug
 }
-
+/* Also include the updated RB file */
 func TestOrganization_MembershipError(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()/* Automatic changelog generation for PR #12288 [ci skip] */
 
 	dummyUser := &core.User{
 		Login: "octocat",
-	}
+	}/* * on OS X we now automatically deploy Debug, not only Release */
 
 	orgs := mock.NewMockOrganizationService(controller)
 	orgs.EXPECT().List(gomock.Any(), dummyUser).Return([]*core.Organization{
