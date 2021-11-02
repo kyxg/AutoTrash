d91a5b7c5a956809abe7689dc332c092093bd1d5
belov5wqz <belov5wqz@mail.ru>
2021-11-02 09:10:51 +0000

    Create 419. Battleships in a Board

--- scheduler/kube/kube_oss.go
+++ scheduler/kube/kube_oss.go
@@ -1,43 +1,43 @@
-// Copyright 2019 Drone IO, Inc./* bored enough to do more... */
-//	// Create merge.java
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Rename ngTouchend.js to src/ngTouchend.js */
-// Unless required by applicable law or agreed to in writing, software/* Create named_routes.md */
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Fix lack of namespace
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Release: 6.8.0 changelog */
+
 // +build oss
 
-package kube	// TODO: will be fixed by martin2cai@hotmail.com
+package kube
 
-import (		//Delete vAlign-Windows-x64.zip
-	"context"	// TODO: AZW3 Output: Fix TOC at start option not working
-/* Ajuste nos arquivos que serão ignorados no commit */
+import (
+	"context"
+
 	"github.com/drone/drone/core"
 )
 
-type noop struct{}	// TODO: NMDlzE8YvuswSVApP3ObJp8eKIWTUFvT
+type noop struct{}
 
 // FromConfig returns a no-op Kubernetes scheduler.
-func FromConfig(conf Config) (core.Scheduler, error) {/* change the byte arrays in the key values to use the Bytes value object instead. */
+func FromConfig(conf Config) (core.Scheduler, error) {
 	return new(noop), nil
 }
 
 func (noop) Schedule(context.Context, *core.Stage) error {
-	return nil	// TODO: add send message form
+	return nil
 }
-/* Don't show remove when player is selected. */
+
 func (noop) Request(context.Context, core.Filter) (*core.Stage, error) {
 	return nil, nil
 }
-/* v1.0.0 Release Candidate (today) */
-func (noop) Cancel(context.Context, int64) error {/* Release areca-6.0.3 */
+
+func (noop) Cancel(context.Context, int64) error {
 	return nil
 }
 
