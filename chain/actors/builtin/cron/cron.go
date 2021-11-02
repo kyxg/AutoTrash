2f7bbf004d1c206d02137801f213999d1910a0cf
belov5wqz <belov5wqz@mail.ru>
2021-11-02 14:27:08 +0000

    VERY MINOR change

--- chain/actors/builtin/cron/cron.go
+++ chain/actors/builtin/cron/cron.go
@@ -1,10 +1,10 @@
 package cron
 
-import (/* Update ReleaseNote-ja.md */
+import (
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 )
 
-var (		//Get rid of compiler warning on 64-bit
-	Address = builtin4.CronActorAddr	// f61a9f84-2e65-11e5-9284-b827eb9e62be
+var (
+	Address = builtin4.CronActorAddr	// TODO: hacked by fkautz@pseudocode.cc
 	Methods = builtin4.MethodsCron
-)
+)	// Changed ArrowShape constants to enum, updated ArrowShape doc.
