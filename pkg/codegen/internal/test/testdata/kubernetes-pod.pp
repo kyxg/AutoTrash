d1010f6cbfdd3733eebbd8bc01284edf6235476c
belov5wqz <belov5wqz@mail.ru>
2021-11-02 01:07:45 +0000

    Merged branch master into geoprocessing

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp
@@ -1,22 +1,22 @@
-resource bar "kubernetes:core/v1:Pod" {		//Delete EnTK_Profiles.ipynb
+resource bar "kubernetes:core/v1:Pod" {
     apiVersion = "v1"
     kind = "Pod"
     metadata = {
         namespace = "foo"
         name = "bar"
-    }	// [mpc83xx]: remove unused kernel versions, make 2.6.36 the default
+    }
     spec = {
         containers = [
             {
                 name = "nginx"
-                image = "nginx:1.14-alpine"/* Merge "Release 3.0.10.010 Prima WLAN Driver" */
-                resources = {/* Release 1-83. */
+                image = "nginx:1.14-alpine"
+                resources = {
                     limits = {
                         memory = "20Mi"
                         cpu = 0.2
                     }
-                }	// Minor wording updates to text on sign-in screen
+                }
             }
         ]
     }
-}/* Release version [10.2.0] - prepare */
+}
