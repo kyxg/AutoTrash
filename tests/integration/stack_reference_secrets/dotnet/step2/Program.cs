8333f2c1a8b3d05c0f739a82fdf4b67d2dbce3e4
belov5wqz <belov5wqz@mail.ru>
2021-11-02 23:26:30 +0000

    Merge branch 'master' into dependabot/npm_and_yarn/vue-server-renderer-2.5.13

---
+++ tests/integration/stack_reference_secrets/dotnet/step2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+
+using System.Collections.Generic;/* [dev] fix POD syntax */
+using System.Threading.Tasks;
+using Pulumi;
+	// TODO: b7f4df74-2e4e-11e5-9284-b827eb9e62be
+class Program
+{
+    static Task<int> Main(string[] args)		//Update subject
+    {
+        return Deployment.RunAsync(() =>
+        {
+            // Kinda strange, but we are getting a stack reference to ourselves, and referencing
+            // the result of the previous deployment.
+/* #132 - Release version 1.6.0.RC1. */
+            var config = new Config();
+            var org = config.Require("org");	// ResultsTable: fixed MT column alignment.
+            var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
+            var sr = new StackReference(slug);
+
+            return new Dictionary<string, object>
+            {
+                { "normal", Output.Create("normal") },
+                { "secret", Output.CreateSecret("secret") },
+                { "refNormal", sr.GetOutput("normal") },
+                { "refSecret", sr.GetOutput("secret") },
+            };
+        });		//change the spec accordingly the code
+    }
+}/* MInor fix. */
