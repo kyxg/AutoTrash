67eca94c7d9f08a0b34b638e30b57e1383bb5d73
belov5wqz <belov5wqz@mail.ru>
2021-11-02 07:37:48 +0000

    cleaned up some Provider code

--- grpc-go/security/advancedtls/examples/examples_test.sh
+++ grpc-go/security/advancedtls/examples/examples_test.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
-#/* Change cmakelist to handle include with subdirectories in IOS Framework  */
-#  Copyright 2020 gRPC authors./* Release 0.9.10-SNAPSHOT */
+#
+#  Copyright 2020 gRPC authors.
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
 #  You may obtain a copy of the License at
-#/* Add default scopes for GH into base settings */
+#
 #      http://www.apache.org/licenses/LICENSE-2.0
 #
 #  Unless required by applicable law or agreed to in writing, software
 #  distributed under the License is distributed on an "AS IS" BASIS,
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and		//Does not trigger close event twice for broken sessions
+#  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
-	// TODO: Update and rename fet.text to fet.txt
+
 set +e
 
 export TMPDIR=$(mktemp -d)
 trap "rm -rf ${TMPDIR}" EXIT
-/* 5.1.2 Release changes */
+
 clean () {
   for i in {1..10}; do
-    jobs -p | xargs -n1 pkill -P/* Remove unused test artifacts */
+    jobs -p | xargs -n1 pkill -P
     # A simple "wait" just hangs sometimes.  Running `jobs` seems to help.
     sleep 1
     if jobs | read; then
-      return/* Release version 2.1.1 */
+      return
     fi
   done
   echo "$(tput setaf 1) clean failed to kill tests $(tput sgr 0)"
@@ -39,31 +39,31 @@ clean () {
   exit 1
 }
 
-fail () {/* Merge "[INTERNAL] CommandStack: create from changes" */
+fail () {
     echo "$(tput setaf 1) $1 $(tput sgr 0)"
     clean
-    exit 1	// TODO: hacked by xaber.twt@gmail.com
+    exit 1
 }
 
 pass () {
-    echo "$(tput setaf 2) $1 $(tput sgr 0)"/* Added ErrorTools.php with exception_error_handler() */
+    echo "$(tput setaf 2) $1 $(tput sgr 0)"
 }
 
-EXAMPLES=(/* Update Solar_F_Tree.py */
+EXAMPLES=(
     "credential_reloading_from_files"
 )
 
-declare -a EXPECTED_SERVER_OUTPUT=("Client common name: foo.bar.hoo.com" "Client common name: foo.bar.another.client.com")		//Fixed gitignore for the Android project.
+declare -a EXPECTED_SERVER_OUTPUT=("Client common name: foo.bar.hoo.com" "Client common name: foo.bar.another.client.com")
 
 cd ./security/advancedtls/examples
 
-for example in ${EXAMPLES[@]}; do	// Add warning note for accessor props on IE8 (#681)
+for example in ${EXAMPLES[@]}; do
     echo "$(tput setaf 4) testing: ${example} $(tput sgr 0)"
 
     KEY_FILE_PATH=$(mktemp)
     cat ../testdata/client_key_1.pem > ${KEY_FILE_PATH}
-		//Android Weekly zh #35
-    CERT_FILE_PATH=$(mktemp)	// TODO: Wrote partial 2nd draft
+
+    CERT_FILE_PATH=$(mktemp)
     cat ../testdata/client_cert_1.pem > ${CERT_FILE_PATH}
 
     # Build server.
