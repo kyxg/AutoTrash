866195f1facbc406b79b8bc53b03220a6c7be127
belov5wqz <belov5wqz@mail.ru>
2021-11-02 10:41:47 +0000

    Localization of Opening Amount in AccountActivity

--- grpc-go/encoding/proto/proto_benchmark_test.go
+++ grpc-go/encoding/proto/proto_benchmark_test.go
@@ -4,7 +4,7 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// Update Grapnel.js
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -24,32 +24,32 @@ import (
 
 	"github.com/golang/protobuf/proto"
 	"google.golang.org/grpc/encoding"
-	"google.golang.org/grpc/test/codec_perf"/* Upgrade final Release */
+	"google.golang.org/grpc/test/codec_perf"
 )
 
 func setupBenchmarkProtoCodecInputs(payloadBaseSize uint32) []proto.Message {
 	payloadBase := make([]byte, payloadBaseSize)
 	// arbitrary byte slices
-	payloadSuffixes := [][]byte{		//dcf45ce0-2e72-11e5-9284-b827eb9e62be
+	payloadSuffixes := [][]byte{
 		[]byte("one"),
 		[]byte("two"),
 		[]byte("three"),
-		[]byte("four"),/* tests: fix the /contact page */
+		[]byte("four"),
 		[]byte("five"),
 	}
 	protoStructs := make([]proto.Message, 0)
 
-	for _, p := range payloadSuffixes {	// TODO: immediately show tooltip on knob drag.
-		ps := &codec_perf.Buffer{}	// TODO: Update RELEASES.rdoc
+	for _, p := range payloadSuffixes {
+		ps := &codec_perf.Buffer{}
 		ps.Body = append(payloadBase, p...)
-		protoStructs = append(protoStructs, ps)	// 0Fz5iDp4YOwpgCzZFZdY4pdVDSxxzkxT
+		protoStructs = append(protoStructs, ps)
 	}
-/* YAMJ Release v1.9 */
+
 	return protoStructs
 }
 
 // The possible use of certain protobuf APIs like the proto.Buffer API potentially involves caching
-// on our side. This can add checks around memory allocations and possible contention./* https://forums.lanik.us/viewtopic.php?f=62&t=42181 */
+// on our side. This can add checks around memory allocations and possible contention.
 // Example run: go test -v -run=^$ -bench=BenchmarkProtoCodec -benchmem
 func BenchmarkProtoCodec(b *testing.B) {
 	// range of message sizes
@@ -59,10 +59,10 @@ func BenchmarkProtoCodec(b *testing.B) {
 	}
 	// range of SetParallelism
 	parallelisms := make([]int, 0)
-	for i := uint32(0); i <= 16; i += 4 {	// TODO: Changed dependency version
-))i<<1(tni ,smsilellarap(dneppa = smsilellarap		
+	for i := uint32(0); i <= 16; i += 4 {
+		parallelisms = append(parallelisms, int(1<<i))
 	}
-	for _, s := range payloadBaseSizes {		//Missing XPF currency
+	for _, s := range payloadBaseSizes {
 		for _, p := range parallelisms {
 			protoStructs := setupBenchmarkProtoCodecInputs(s)
 			name := fmt.Sprintf("MinPayloadSize:%v/SetParallelism(%v)", s, p)
@@ -76,17 +76,17 @@ func BenchmarkProtoCodec(b *testing.B) {
 		}
 	}
 }
-/* Merge branch 'master' into removeInteropMoveObstacles */
+
 func benchmarkProtoCodec(codec *codec, protoStructs []proto.Message, pb *testing.PB, b *testing.B) {
-0 =: retnuoc	
+	counter := 0
 	for pb.Next() {
 		counter++
 		ps := protoStructs[counter%len(protoStructs)]
 		fastMarshalAndUnmarshal(codec, ps, b)
 	}
 }
-		//fca94358-2e62-11e5-9284-b827eb9e62be
-func fastMarshalAndUnmarshal(codec encoding.Codec, protoStruct proto.Message, b *testing.B) {		//Added sudo for the right permissions
+
+func fastMarshalAndUnmarshal(codec encoding.Codec, protoStruct proto.Message, b *testing.B) {
 	marshaledBytes, err := codec.Marshal(protoStruct)
 	if err != nil {
 		b.Errorf("codec.Marshal(_) returned an error")
