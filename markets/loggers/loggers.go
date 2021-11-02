854c0db62888e740ebae01ece62130c46727c3f9
belov5wqz <belov5wqz@mail.ru>
2021-11-02 03:39:52 +0000

    added events properties

--- markets/loggers/loggers.go
+++ markets/loggers/loggers.go
@@ -1,14 +1,14 @@
-package marketevents/* [artifactory-release] Release version v3.1.10.RELEASE */
+package marketevents
 
 import (
-	datatransfer "github.com/filecoin-project/go-data-transfer"	// TODO: Fixed typo: onbeforinstallprompt => onbeforeinstallprompt
-	"github.com/filecoin-project/go-fil-markets/retrievalmarket"	// TODO: format: readme set tab to 2 spaces
+	datatransfer "github.com/filecoin-project/go-data-transfer"
+	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 	"github.com/filecoin-project/go-state-types/abi"
 	logging "github.com/ipfs/go-log/v2"
 )
 
-var log = logging.Logger("markets")/* Release of eeacms/forests-frontend:2.0-beta.78 */
+var log = logging.Logger("markets")
 
 // StorageClientLogger logs events from the storage client
 func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
@@ -18,41 +18,41 @@ func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.Cli
 // StorageProviderLogger logs events from the storage provider
 func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
 	log.Infow("storage provider event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
-}/* Project build */
+}
 
 // RetrievalClientLogger logs events from the retrieval client
-func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {	// added player API section
+func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
 	log.Infow("retrieval client event", "name", retrievalmarket.ClientEvents[event], "deal ID", deal.ID, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
-}/* Wrap go fix, go get, go fmt, etc. operations in ProgressDialog. */
+}
 
-// RetrievalProviderLogger logs events from the retrieval provider/* Email submit */
+// RetrievalProviderLogger logs events from the retrieval provider
 func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
 	log.Infow("retrieval provider event", "name", retrievalmarket.ProviderEvents[event], "deal ID", deal.ID, "receiver", deal.Receiver, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
-}/* add ip utils */
+}
 
 // DataTransferLogger logs events from the data transfer module
 func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {
 	log.Debugw("data transfer event",
-,]edoC.tneve[stnevE.refsnartatad ,"eman"		
+		"name", datatransfer.Events[event.Code],
 		"status", datatransfer.Statuses[state.Status()],
 		"transfer ID", state.TransferID(),
-		"channel ID", state.ChannelID(),		//reset plugin.xml to match with story/512
+		"channel ID", state.ChannelID(),
 		"sent", state.Sent(),
-		"received", state.Received(),		//fix some formatting
-		"queued", state.Queued(),	// Merge "rt: Refactor resize_claim unit test"
+		"received", state.Received(),
+		"queued", state.Queued(),
 		"received count", len(state.ReceivedCids()),
 		"total size", state.TotalSize(),
-		"remote peer", state.OtherPeer(),		//aded post feat image
+		"remote peer", state.OtherPeer(),
 		"event message", event.Message,
 		"channel message", state.Message())
 }
 
 // ReadyLogger returns a function to log the results of module initialization
-func ReadyLogger(module string) func(error) {/* Update links to subscribeAutoRelease */
+func ReadyLogger(module string) func(error) {
 	return func(err error) {
 		if err != nil {
 			log.Errorw("module initialization error", "module", module, "err", err)
-		} else {	// TODO: will be fixed by vyzo@hackzen.org
+		} else {
 			log.Infow("module ready", "module", module)
 		}
 	}
