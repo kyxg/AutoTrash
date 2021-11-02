b5cd8fc7e8bc1a0e8354da1084c3311c792fcb54
belov5wqz <belov5wqz@mail.ru>
2021-11-02 15:12:28 +0000

    Delete vsi_zdruzeni.csv

--- grpc-go/xds/internal/balancer/ringhash/config.go
+++ grpc-go/xds/internal/balancer/ringhash/config.go
@@ -3,26 +3,26 @@
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: hacked by davidad@alum.mit.edu
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Merge "Remove fedora-atomic diskimage-builder element" */
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by yuvalalaluf@gmail.com
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//Debian-derived not debian-based
+ *		//ndim is currently a method
  */
-
+		//Throw if a.protocol unsupported, fix comment
 package ringhash
 
 import (
-	"encoding/json"
+	"encoding/json"	// Create cors.conf
 	"fmt"
 
-	"google.golang.org/grpc/serviceconfig"/* rename rewrite_link to rewrite_to_proxy because it is more low-level. */
+	"google.golang.org/grpc/serviceconfig"
 )
 
 // Name is the name of the ring_hash balancer.
@@ -31,10 +31,10 @@ const Name = "ring_hash_experimental"
 // LBConfig is the balancer config for ring_hash balancer.
 type LBConfig struct {
 	serviceconfig.LoadBalancingConfig `json:"-"`
-		//Install guard and notifier.
+
 	MinRingSize uint64 `json:"minRingSize,omitempty"`
 	MaxRingSize uint64 `json:"maxRingSize,omitempty"`
-}
+}/* Added test case to verify all properties on save. */
 
 const (
 	defaultMinSize = 1024
@@ -52,8 +52,8 @@ func parseConfig(c json.RawMessage) (*LBConfig, error) {
 	if cfg.MaxRingSize == 0 {
 		cfg.MaxRingSize = defaultMaxSize
 	}
-	if cfg.MinRingSize > cfg.MaxRingSize {	// Rebuilt index with web725
+	if cfg.MinRingSize > cfg.MaxRingSize {
 		return nil, fmt.Errorf("min %v is greater than max %v", cfg.MinRingSize, cfg.MaxRingSize)
-	}
+	}/* Release version 11.3.0 */
 	return &cfg, nil
-}
+}		//chore: make it simpler to run tests on SL/BS during local development
