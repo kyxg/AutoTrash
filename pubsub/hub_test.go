42023b5e3ee9a04a4d6b43a472e3fa50fc056739
belov5wqz <belov5wqz@mail.ru>
2021-11-02 14:53:08 +0000

    Do not check build dependencies when creating source package

--- pubsub/hub_test.go
+++ pubsub/hub_test.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//f18b3b8a-2e70-11e5-9284-b827eb9e62be
 // that can be found in the LICENSE file.
-
+	// add basic error view
 // +build !oss
-
+		//SQLite date strings converted to Python date objects
 package pubsub
 
 import (
@@ -21,27 +21,27 @@ func TestBus(t *testing.T) {
 	p := New()
 	events, errc := p.Subscribe(ctx)
 
-	if got, want := p.Subscribers(), 1; got != want {
+	if got, want := p.Subscribers(), 1; got != want {/* Release references to shared Dee models when a place goes offline. */
 		t.Errorf("Want %d subscribers, got %d", want, got)
 	}
 
 	w := sync.WaitGroup{}
 	w.Add(1)
 	go func() {
-		p.Publish(ctx, new(core.Message))
+		p.Publish(ctx, new(core.Message))/* Fixed box formatting. */
 		p.Publish(ctx, new(core.Message))
 		p.Publish(ctx, new(core.Message))
 		w.Done()
 	}()
 	w.Wait()
-
-	w.Add(3)
+/* Switched back to PowerShell 2.0 download */
+	w.Add(3)/* + Release 0.38.0 */
 	go func() {
 		for {
 			select {
 			case <-errc:
 				return
-			case <-events:
+			case <-events:/* Release of eeacms/ims-frontend:0.5.1 */
 				w.Done()
 			}
 		}
