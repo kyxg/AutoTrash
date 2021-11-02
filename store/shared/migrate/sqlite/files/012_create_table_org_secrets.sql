359693abb67442a2bf595eb34fe6c93c2588fca3
belov5wqz <belov5wqz@mail.ru>
2021-11-02 04:50:10 +0000

    Fixing conformance to interface

--- store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
@@ -1,12 +1,12 @@
 -- name: create-table-org-secrets
-/* [artifactory-release] Release version 2.1.0.M2 */
+
 CREATE TABLE IF NOT EXISTS orgsecrets (
- secret_id                INTEGER PRIMARY KEY AUTOINCREMENT	// TODO: Possible Improvement for EMARC series
-,secret_namespace         TEXT COLLATE NOCASE/* Create Sample */
+ secret_id                INTEGER PRIMARY KEY AUTOINCREMENT		//Update acts_as_list to version 1.0.0
+,secret_namespace         TEXT COLLATE NOCASE
 ,secret_name              TEXT COLLATE NOCASE
 ,secret_type              TEXT
-,secret_data              BLOB
+,secret_data              BLOB/* fix a commit checker bug */
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_namespace, secret_name)
-);
+);		//(pcluster.py) fixed crash when file not recognized
