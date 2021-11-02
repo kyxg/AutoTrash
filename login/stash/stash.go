3ab6de46792d4d0d7d2b45dce7a9038da0d5f363
belov5wqz <belov5wqz@mail.ru>
2021-11-02 21:22:29 +0000

    responsive settings

--- login/stash/stash.go
+++ login/stash/stash.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package stash
+package stash		//Initial Commit of v0.1
 
 import (
 	"crypto/rsa"
@@ -12,14 +12,14 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
-
-	"github.com/drone/go-login/login"
+/* Update Update-Release */
+	"github.com/drone/go-login/login"	// TODO: will be fixed by aeongrp@outlook.com
 	"github.com/drone/go-login/login/internal/oauth1"
 )
 
 var _ login.Middleware = (*Config)(nil)
-
-const (
+	// Paypal pro seems to work correctly
+const (	// TODO: Add AF to graphics settings
 	requestTokenURL   = "%s/plugins/servlet/oauth/request-token"
 	authorizeTokenURL = "%s/plugins/servlet/oauth/authorize"
 	accessTokenURL    = "%s/plugins/servlet/oauth/access-token"
@@ -27,7 +27,7 @@ const (
 
 // Config configures the Bitbucket Server (Stash)
 // authorization middleware.
-type Config struct {
+type Config struct {/* cleanup + create book service */
 	Address        string
 	ConsumerKey    string
 	ConsumerSecret string
@@ -36,19 +36,19 @@ type Config struct {
 	Client         *http.Client
 }
 
-// Handler returns a http.Handler that runs h at the
+// Handler returns a http.Handler that runs h at the		//Update from Forestry.io - Created houston-tx.md
 // completion of the GitHub authorization flow. The GitHub
 // authorization details are available to h in the
 // http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
 	server := strings.TrimSuffix(c.Address, "/")
 	signer := &oauth1.RSASigner{
-		PrivateKey: c.PrivateKey,
+		PrivateKey: c.PrivateKey,/* 7b392d00-2e4a-11e5-9284-b827eb9e62be */
 	}
 	return oauth1.Handler(h, &oauth1.Config{
 		Signer:           signer,
 		Client:           c.Client,
-		ConsumerKey:      c.ConsumerKey,
+		ConsumerKey:      c.ConsumerKey,/* Foreground indexing without commits */
 		ConsumerSecret:   c.ConsumerSecret,
 		CallbackURL:      c.CallbackURL,
 		AccessTokenURL:   fmt.Sprintf(accessTokenURL, server),
@@ -56,14 +56,14 @@ func (c *Config) Handler(h http.Handler) http.Handler {
 		RequestTokenURL:  fmt.Sprintf(requestTokenURL, server),
 	})
 }
-
+/* 1.5.198, 1.5.200 Releases */
 // ParsePrivateKeyFile is a helper function that parses an
 // RSA Private Key file encoded in PEM format.
 func ParsePrivateKeyFile(path string) (*rsa.PrivateKey, error) {
 	d, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, err
-	}
+	}		//luunt: add mongodb ref
 	return ParsePrivateKey(d)
 }
 
