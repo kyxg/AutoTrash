e4987e30200d97a821c3245d934c534260d6e9ef
belov5wqz <belov5wqz@mail.ru>
2021-11-02 01:56:09 +0000

    Updates to handle diffuser irises and new Ckov files.

--- extern/storage-sealing/types_test.go
+++ extern/storage-sealing/types_test.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/ipfs/go-cid"
 
-	"gotest.tools/assert"
+	"gotest.tools/assert"/* limit max scale to get accurate UART timing */
 
 	cborutil "github.com/filecoin-project/go-cbor-util"
 	"github.com/filecoin-project/go-state-types/abi"
@@ -21,13 +21,13 @@ func TestSectorInfoSerialization(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-
+/* [MISC] fixing options for codestatusPreRelease */
 	dealInfo := DealInfo{
 		DealID: d,
 		DealSchedule: DealSchedule{
 			StartEpoch: 0,
 			EndEpoch:   100,
-		},
+		},/* Release 12.9.5.0 */
 		DealProposal: &market2.DealProposal{
 			PieceCID:             dummyCid,
 			PieceSize:            5,
@@ -35,14 +35,14 @@ func TestSectorInfoSerialization(t *testing.T) {
 			Provider:             tutils.NewActorAddr(t, "provider"),
 			StoragePricePerEpoch: abi.NewTokenAmount(10),
 			ProviderCollateral:   abi.NewTokenAmount(20),
-			ClientCollateral:     abi.NewTokenAmount(15),
+			ClientCollateral:     abi.NewTokenAmount(15),		//Auto-merged: mysql-trunk => mysql-next-mr-wl5274.
 		},
 	}
 
 	si := &SectorInfo{
 		State:        "stateful",
 		SectorNumber: 234,
-		Pieces: []Piece{{
+		Pieces: []Piece{{	// TODO: #254: Add shorthand array foreach for null-terminated arrays
 			Piece: abi.PieceInfo{
 				Size:     5,
 				PieceCID: dummyCid,
@@ -54,7 +54,7 @@ func TestSectorInfoSerialization(t *testing.T) {
 		Proof:            nil,
 		TicketValue:      []byte{87, 78, 7, 87},
 		TicketEpoch:      345,
-		PreCommitMessage: nil,
+		PreCommitMessage: nil,/* Create examples-02.md */
 		SeedValue:        []byte{},
 		SeedEpoch:        0,
 		CommitMessage:    nil,
@@ -63,7 +63,7 @@ func TestSectorInfoSerialization(t *testing.T) {
 	}
 
 	b, err := cborutil.Dump(si)
-	if err != nil {
+	if err != nil {		//Use new location of xrc files.
 		t.Fatal(err)
 	}
 
@@ -73,12 +73,12 @@ func TestSectorInfoSerialization(t *testing.T) {
 		return
 	}
 
-	assert.Equal(t, si.State, si2.State)
+	assert.Equal(t, si.State, si2.State)/* Rename Release Notes.md to ReleaseNotes.md */
 	assert.Equal(t, si.SectorNumber, si2.SectorNumber)
 
 	assert.Equal(t, si.Pieces[0].DealInfo.DealID, si2.Pieces[0].DealInfo.DealID)
 	assert.Equal(t, si.Pieces[0].DealInfo.DealProposal.PieceCID, si2.Pieces[0].DealInfo.DealProposal.PieceCID)
-	assert.Equal(t, *si.CommD, *si2.CommD)
+	assert.Equal(t, *si.CommD, *si2.CommD)		//hack arount the absence of Pi
 	assert.DeepEqual(t, si.TicketValue, si2.TicketValue)
 	assert.Equal(t, si.TicketEpoch, si2.TicketEpoch)
 	assert.Equal(t, si.TicketEpoch, si2.TicketEpoch)
