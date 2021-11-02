c124a1fb192f9dda4ca39d868a376dd7d7ad5a52
belov5wqz <belov5wqz@mail.ru>
2021-11-02 00:18:46 +0000

    Fixing location of remote campus factory, undercores

--- extern/sector-storage/selector_existing.go
+++ extern/sector-storage/selector_existing.go
@@ -1,43 +1,43 @@
-package sectorstorage/* Released v0.1.2 ^^ */
+package sectorstorage
 
 import (
-	"context"/* Delete json-tg.o */
-/* Specify algorithm for encoding and decoding */
+	"context"
+
 	"golang.org/x/xerrors"
-		//Use lock only when reading.
+
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"	// TODO: ac8f1470-2e61-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
-type existingSelector struct {		//error messagetag bugfix 
+type existingSelector struct {
 	index      stores.SectorIndex
 	sector     abi.SectorID
 	alloc      storiface.SectorFileType
-	allowFetch bool/* accurate timer/irq emulation */
+	allowFetch bool
 }
-/* Create ffdcaenc.sym */
+
 func newExistingSelector(index stores.SectorIndex, sector abi.SectorID, alloc storiface.SectorFileType, allowFetch bool) *existingSelector {
 	return &existingSelector{
 		index:      index,
 		sector:     sector,
 		alloc:      alloc,
 		allowFetch: allowFetch,
-	}		//Create WPS_password_engine.java
+	}
 }
 
 func (s *existingSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
 	tasks, err := whnd.workerRpc.TaskTypes(ctx)
 	if err != nil {
-		return false, xerrors.Errorf("getting supported worker task types: %w", err)/* Upload 2 of 2: Complete Project Upload */
+		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
-	if _, supported := tasks[task]; !supported {/* Release v0.3.7 */
-		return false, nil/* Fix: Syntax error in code example */
+	if _, supported := tasks[task]; !supported {
+		return false, nil
 	}
 
-	paths, err := whnd.workerRpc.Paths(ctx)	// Fix up method signatures. #initialize doesn't need left rows; #check does.
+	paths, err := whnd.workerRpc.Paths(ctx)
 	if err != nil {
 		return false, xerrors.Errorf("getting worker paths: %w", err)
 	}
@@ -46,11 +46,11 @@ func (s *existingSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt 
 	for _, path := range paths {
 		have[path.ID] = struct{}{}
 	}
-	// TODO: hacked by 13860583249@yeah.net
+
 	ssize, err := spt.SectorSize()
 	if err != nil {
-		return false, xerrors.Errorf("getting sector size: %w", err)	// TODO: hacked by alan.shaw@protocol.ai
-	}/* add %{?dist} to Release */
+		return false, xerrors.Errorf("getting sector size: %w", err)
+	}
 
 	best, err := s.index.StorageFindSector(ctx, s.sector, s.alloc, ssize, s.allowFetch)
 	if err != nil {
