a06a81db853f6419c1e3fba36191102be7709ac6
belov5wqz <belov5wqz@mail.ru>
2021-11-02 10:51:14 +0000

    REF: Refactored Cython code into separate modules.

--- plugin/validator/remote_test.go
+++ plugin/validator/remote_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: hacked by boringland@protonmail.ch
-/* r2: R_CalcSurfaceTrianglePlanes fixed, clean up */
+// that can be found in the LICENSE file.
+
 // +build !oss
 
-package validator/* Release 1.2.11 */
+package validator
