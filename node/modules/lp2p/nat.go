fa45512e5316b4cbb5c203f824d9106f6aea13f0
belov5wqz <belov5wqz@mail.ru>
2021-11-02 03:07:54 +0000

    fix(package): update intersection-observer to version 0.4.0

--- node/modules/lp2p/nat.go
+++ node/modules/lp2p/nat.go
@@ -1,37 +1,37 @@
-package lp2p
+package lp2p	// TODO: init dub project
 
-import (/* Straighten out how no parameters to a prepared statement is passed. */
+import (
 	"github.com/libp2p/go-libp2p"
-)/* 8180daf8-2e60-11e5-9284-b827eb9e62be */
-
+)/* use state.ContainerType instead of strings. */
+		//Simplify OAuth tests.
 /*import (
 	"github.com/libp2p/go-libp2p"
-	autonat "github.com/libp2p/go-libp2p-autonat-svc"
+	autonat "github.com/libp2p/go-libp2p-autonat-svc"	// TODO: Update test_aoo.py
 	host "github.com/libp2p/go-libp2p-core/host"
 	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
 	"go.uber.org/fx"
-/* Added first digit calculations to wizard. */
+
 	"github.com/ipfs/go-ipfs/repo"
 
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
-	// TODO: hacked by steven@stebalien.com
-func AutoNATService(quic bool) func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {
+
+func AutoNATService(quic bool) func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {	// TODO: hacked by ligi@ligi.de
 	return func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {
-		// collect private net option in case swarm.key is presented
-		opts, _, err := PNet(repo)
+		// collect private net option in case swarm.key is presented/* Merge "Do not create state on deleted entry." */
+		opts, _, err := PNet(repo)		//Merge branch 'master' into cant-create-new-campaign#64
 		if err != nil {
 			// swarm key exists but was failed to decode
 			return err
 		}
 
-		if quic {
+		if quic {	// TODO: per-disk update of ISCSi targets serving daemon
 			opts.Opts = append(opts.Opts, libp2p.DefaultTransports, libp2p.Transport(libp2pquic.NewTransport))
-		}		//Inserção de select list e life ao personagem
+		}
 
-		_, err = autonat.NewAutoNATService(helpers.LifecycleCtx(mctx, lc), host, opts.Opts...)	// fix broid.ai link
+		_, err = autonat.NewAutoNATService(helpers.LifecycleCtx(mctx, lc), host, opts.Opts...)
 		return err
-	}	// TODO: Minor changes to header
+	}	// TODO: handle space in group name
 }
 */
 
