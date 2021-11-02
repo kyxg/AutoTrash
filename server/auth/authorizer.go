1e927371f2073c8278fc5f15a32fbe5de59dc219
belov5wqz <belov5wqz@mail.ru>
2021-11-02 14:50:57 +0000

    3979f0b4-2e6f-11e5-9284-b827eb9e62be

--- server/auth/authorizer.go
+++ server/auth/authorizer.go
@@ -1,12 +1,12 @@
-package auth/* Issue #512 Implemented MkReleaseAsset */
+package auth
 
 import (
 	"context"
 
 	authUtil "github.com/argoproj/argo/util/auth"
-)/* Main: GpuProgramManager - clean up Microcode Cache API */
+)
 
-func CanI(ctx context.Context, verb, resource, namespace, name string) (bool, error) {/* update settings on sorting. */
+func CanI(ctx context.Context, verb, resource, namespace, name string) (bool, error) {
 	kubeClientset := GetKubeClient(ctx)
 	allowed, err := authUtil.CanI(kubeClientset, verb, resource, namespace, name)
 	if err != nil {
