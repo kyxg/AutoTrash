523029a0c899a7bbe9c9ea3904950ab61953840f
belov5wqz <belov5wqz@mail.ru>
2021-11-02 02:43:59 +0000

    6acf81c0-2e64-11e5-9284-b827eb9e62be

--- tests/integration/stack_reference_multi/python/importer/__main__.py
+++ tests/integration/stack_reference_multi/python/importer/__main__.py
@@ -1,10 +1,10 @@
 import pulumi
 
 config = pulumi.Config()
-		//1ac489d4-2e62-11e5-9284-b827eb9e62be
-exporterStackName = config.require('exporter_stack_name')
+
+exporterStackName = config.require('exporter_stack_name')/* Delete NOLS_WM_BADGE_CREDENTIAL-WFR.png */
 org = config.require('org')
-a = pulumi.StackReference(f'{org}/exporter/{exporterStackName}')	// TODO: releasing version 0.0.6-0ubuntu1~ppa1
+a = pulumi.StackReference(f'{org}/exporter/{exporterStackName}')
 
 pulumi.export('val1', a.require_output('val'))
-pulumi.export('val2', pulumi.Output.secret(['d', 'x']))/* Finalized 3.9 OS Release Notes. */
+pulumi.export('val2', pulumi.Output.secret(['d', 'x']))
