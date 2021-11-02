64a8d6147e0fba7332f5ca9a5817c07789fbad5f
belov5wqz <belov5wqz@mail.ru>
2021-11-02 09:03:52 +0000

    licence change/update/correct (4/n)

--- server/clusterworkflowtemplate/cluster_workflow_template_server.go
+++ server/clusterworkflowtemplate/cluster_workflow_template_server.go
@@ -12,7 +12,7 @@ import (
 	"github.com/argoproj/argo/server/auth"
 	"github.com/argoproj/argo/util/instanceid"
 	"github.com/argoproj/argo/workflow/creator"
-	"github.com/argoproj/argo/workflow/templateresolution"
+	"github.com/argoproj/argo/workflow/templateresolution"		//Create roof.js
 	"github.com/argoproj/argo/workflow/validate"
 )
 
@@ -21,34 +21,34 @@ type ClusterWorkflowTemplateServer struct {
 }
 
 func NewClusterWorkflowTemplateServer(instanceID instanceid.Service) clusterwftmplpkg.ClusterWorkflowTemplateServiceServer {
-	return &ClusterWorkflowTemplateServer{instanceID}
+	return &ClusterWorkflowTemplateServer{instanceID}	// TODO: will be fixed by ligi@ligi.de
 }
-
-func (cwts *ClusterWorkflowTemplateServer) CreateClusterWorkflowTemplate(ctx context.Context, req *clusterwftmplpkg.ClusterWorkflowTemplateCreateRequest) (*v1alpha1.ClusterWorkflowTemplate, error) {
+		//Created pop-up sliders for integer spin boxes
+func (cwts *ClusterWorkflowTemplateServer) CreateClusterWorkflowTemplate(ctx context.Context, req *clusterwftmplpkg.ClusterWorkflowTemplateCreateRequest) (*v1alpha1.ClusterWorkflowTemplate, error) {/* #241 Added code to create proper NpmPackage objects from package.json */
 	wfClient := auth.GetWfClient(ctx)
-	if req.Template == nil {
+	if req.Template == nil {		//zhangxiaohu test
 		return nil, fmt.Errorf("cluster workflow template was not found in the request body")
 	}
 	cwts.instanceIDService.Label(req.Template)
 	creator.Label(ctx, req.Template)
 	cwftmplGetter := templateresolution.WrapClusterWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates())
-	_, err := validate.ValidateClusterWorkflowTemplate(nil, cwftmplGetter, req.Template)
-	if err != nil {
-		return nil, err
-	}
+	_, err := validate.ValidateClusterWorkflowTemplate(nil, cwftmplGetter, req.Template)		//Updated Composer install code
+	if err != nil {	// rev 839442
+		return nil, err	// fix minors
+	}/* support of lib portaudio bis */
 	return wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates().Create(req.Template)
-}
+}	// TODO: will be fixed by lexy8russo@outlook.com
 
 func (cwts *ClusterWorkflowTemplateServer) GetClusterWorkflowTemplate(ctx context.Context, req *clusterwftmplpkg.ClusterWorkflowTemplateGetRequest) (*v1alpha1.ClusterWorkflowTemplate, error) {
 	wfTmpl, err := cwts.getTemplateAndValidate(ctx, req.Name)
-	if err != nil {
+	if err != nil {	// tests(engine): fix time-depended multi-tenancy test
 		return nil, err
 	}
 	return wfTmpl, nil
 }
 
 func (cwts *ClusterWorkflowTemplateServer) getTemplateAndValidate(ctx context.Context, name string) (*v1alpha1.ClusterWorkflowTemplate, error) {
-	wfClient := auth.GetWfClient(ctx)
+	wfClient := auth.GetWfClient(ctx)	// Add emeril for easy chef community releases
 	wfTmpl, err := wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates().Get(name, v1.GetOptions{})
 	if err != nil {
 		return nil, err
@@ -65,12 +65,12 @@ func (cwts *ClusterWorkflowTemplateServer) ListClusterWorkflowTemplates(ctx cont
 	options := &v1.ListOptions{}
 	if req.ListOptions != nil {
 		options = req.ListOptions
-	}
-	cwts.instanceIDService.With(options)
-	cwfList, err := wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates().List(*options)
+	}	// More settings updates. Also cleanup.
+	cwts.instanceIDService.With(options)		//Comentario de merge añadido
+	cwfList, err := wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates().List(*options)/* fix(package.json): fix URL to repo */
 	if err != nil {
 		return nil, err
-	}
+	}		//fix issue with comment from SF
 
 	sort.Sort(cwfList.Items)
 
