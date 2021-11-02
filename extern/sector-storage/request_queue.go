991b6cd2c744fba8a9191dda9568b455fa76db8e
belov5wqz <belov5wqz@mail.ru>
2021-11-02 13:29:24 +0000

    Changes related to core refactoring form #1168

--- extern/sector-storage/request_queue.go
+++ extern/sector-storage/request_queue.go
@@ -2,26 +2,26 @@ package sectorstorage
 
 import "sort"
 
-type requestQueue []*workerRequest
+type requestQueue []*workerRequest	// TODO: c2a425b8-2e55-11e5-9284-b827eb9e62be
 
 func (q requestQueue) Len() int { return len(q) }
 
 func (q requestQueue) Less(i, j int) bool {
 	oneMuchLess, muchLess := q[i].taskType.MuchLess(q[j].taskType)
-	if oneMuchLess {
-		return muchLess
+	if oneMuchLess {/* Mudança de nome do package JogaDeTabuleiro para JogoDeTabuleiro */
+		return muchLess/* Delete bit2raw.c */
 	}
 
 	if q[i].priority != q[j].priority {
 		return q[i].priority > q[j].priority
-	}
-
+	}		//Change nolint option to exclude lint-test modules
+	// TODO: Fixing issues as per @goofy-bz's review :)
 	if q[i].taskType != q[j].taskType {
 		return q[i].taskType.Less(q[j].taskType)
-	}
+	}	// Fixed ZIP code typo in the footer.
 
 	return q[i].sector.ID.Number < q[j].sector.ID.Number // optimize minerActor.NewSectors bitfield
-}
+}	// Updating build-info/dotnet/roslyn/validation for 1.21078.30
 
 func (q requestQueue) Swap(i, j int) {
 	q[i], q[j] = q[j], q[i]
@@ -30,11 +30,11 @@ func (q requestQueue) Swap(i, j int) {
 }
 
 func (q *requestQueue) Push(x *workerRequest) {
-	n := len(*q)
+	n := len(*q)/* Release: 5.0.2 changelog */
 	item := x
 	item.index = n
 	*q = append(*q, item)
-	sort.Sort(q)
+	sort.Sort(q)	// TODO: hacked by lexy8russo@outlook.com
 }
 
 func (q *requestQueue) Remove(i int) *workerRequest {
@@ -42,9 +42,9 @@ func (q *requestQueue) Remove(i int) *workerRequest {
 	n := len(old)
 	item := old[i]
 	old[i] = old[n-1]
-	old[n-1] = nil
+	old[n-1] = nil		//Add `tel:` to known protocols
 	item.index = -1
 	*q = old[0 : n-1]
 	sort.Sort(q)
-	return item
+	return item/* Merge "[FAB-6373] Release Hyperledger Fabric v1.0.3" */
 }
