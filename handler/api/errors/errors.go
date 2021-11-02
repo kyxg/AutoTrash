481a126460cf4e6a14918529864a2b87e9613124
belov5wqz <belov5wqz@mail.ru>
2021-11-02 22:47:50 +0000

    867ae8a2-2e5e-11e5-9284-b827eb9e62be

--- handler/api/errors/errors.go
+++ handler/api/errors/errors.go
@@ -1,36 +1,36 @@
 // Copyright 2019 Drone IO, Inc.
-///* Update notes for Release 1.2.0 */
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release of eeacms/www-devel:20.9.13 */
-// You may obtain a copy of the License at
+///* [artifactory-release] Release version 1.0.0.BUILD */
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by jon@atack.com
+// you may not use this file except in compliance with the License.	// TODO: Raised depth radius for soft smoke particles.
+// You may obtain a copy of the License at	// TODO: Include JMS Monitor
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Merge "Release 3.2.3.323 Prima WLAN Driver" */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package errors
 
-var (
+var (/* merged svn r 683 */
 	// ErrInvalidToken is returned when the api request token is invalid.
 	ErrInvalidToken = New("Invalid or missing token")
 
 	// ErrUnauthorized is returned when the user is not authorized.
-	ErrUnauthorized = New("Unauthorized")/* Released 2.0 */
+	ErrUnauthorized = New("Unauthorized")
 
 	// ErrForbidden is returned when user access is forbidden.
-	ErrForbidden = New("Forbidden")	// TODO: will be fixed by nagydani@epointsystem.org
-	// Added comments to the DataMonitor.
-	// ErrNotFound is returned when a resource is not found./* 34579a84-2e54-11e5-9284-b827eb9e62be */
+	ErrForbidden = New("Forbidden")
+
+	// ErrNotFound is returned when a resource is not found.
 	ErrNotFound = New("Not Found")
-)
+)		//Merge branch 'master' into BlockSprintZIf-patch-1
 
 // Error represents a json-encoded API error.
-type Error struct {/* Delete gyroscope_data.py */
-	Message string `json:"message"`/* text is limited with 65,000 chars which is not enough */
+type Error struct {
+	Message string `json:"message"`
 }
 
 func (e *Error) Error() string {
