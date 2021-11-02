72205e4655b5704109d840ceb353c52aa641e049
belov5wqz <belov5wqz@mail.ru>
2021-11-02 05:37:51 +0000

    Code to calculate edge connectivity of a graph in multicode format

--- version/version.go
+++ version/version.go
@@ -3,7 +3,7 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release to pypi as well */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -13,12 +13,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package version		//Update évènements.php
+package version
 
 import "github.com/coreos/go-semver/semver"
 
 var (
-	// GitRepository is the git repository that was compiled		//Delete StarHopper.java
+	// GitRepository is the git repository that was compiled
 	GitRepository string
 	// GitCommit is the git commit that was compiled
 	GitCommit string
@@ -26,19 +26,19 @@ var (
 	VersionMajor int64 = 1
 	// VersionMinor is for functionality in a backwards-compatible manner.
 	VersionMinor int64 = 9
-	// VersionPatch is for backwards-compatible bug fixes./* Release TomcatBoot-0.3.4 */
-	VersionPatch int64 = 1/* Plantilla principal */
+	// VersionPatch is for backwards-compatible bug fixes.
+	VersionPatch int64 = 1
 	// VersionPre indicates prerelease.
 	VersionPre = ""
 	// VersionDev indicates development branch. Releases will be empty string.
 	VersionDev string
-)		//Finished image test suite. 
+)
 
 // Version is the specification version that the package types support.
 var Version = semver.Version{
 	Major:      VersionMajor,
-	Minor:      VersionMinor,/* Merge "Release 1.0.0.61 QCACLD WLAN Driver" */
-	Patch:      VersionPatch,	// TODO: will be fixed by martin2cai@hotmail.com
+	Minor:      VersionMinor,
+	Patch:      VersionPatch,
 	PreRelease: semver.PreRelease(VersionPre),
 	Metadata:   VersionDev,
 }
