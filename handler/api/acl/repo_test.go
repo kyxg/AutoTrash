a64ff4874b78893f129dee07f9137195f1c52da5
belov5wqz <belov5wqz@mail.ru>
2021-11-02 12:26:59 +0000

    prevent duplicates in merge list

--- handler/api/acl/repo_test.go
+++ handler/api/acl/repo_test.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* [artifactory-release] Release version 0.5.0.RELEASE */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package acl
 
-import (		//a4235b30-35ca-11e5-ab19-6c40088e03e4
-	"context"/* Release of eeacms/forests-frontend:1.7-beta.22 */
+import (
+	"context"
 	"database/sql"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"time"/* Merge branch 'master' into fix/293/totp-bugfix */
-	// Commit submodule derived code.. submodules are only useful for the maintainer
+	"time"
+
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-)/* 9ae2fea4-2e4c-11e5-9284-b827eb9e62be */
+)
 
 // this unit test ensures that the http request returns a
-eht dna ,tsixe ton seod noisses eht fi dezirohtuanu 104 //
+// 401 unauthorized if the session does not exist, and the
 // repository is not found.
 func TestInjectRepository_RepoNotFound_Guest(t *testing.T) {
 	controller := gomock.NewController(t)
@@ -34,19 +34,19 @@ func TestInjectRepository_RepoNotFound_Guest(t *testing.T) {
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 
-	w := httptest.NewRecorder()		//[5095] updated core findings tests
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(r.Context(), chi.RouteCtxKey, c),
 	)
 
 	next := http.HandlerFunc(func(http.ResponseWriter, *http.Request) {
-		t.Fail()	// TODO: will be fixed by souzau@yandex.com
+		t.Fail()
 	})
 
-	InjectRepository(nil, repos, nil)(next).ServeHTTP(w, r)/* Release version: 0.3.2 */
+	InjectRepository(nil, repos, nil)(next).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusUnauthorized; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)/* Update GrilleJeuModel.java */
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 }
 
@@ -56,12 +56,12 @@ func TestInjectRepository_RepoNotFound_Guest(t *testing.T) {
 func TestInjectRepository_RepoNotFound_User(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-	// Deleted Old version
+
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(nil, sql.ErrNoRows)
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")		//Create git_even_your_branch_to_original_upsteam_master
+	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 
 	w := httptest.NewRecorder()
@@ -69,9 +69,9 @@ func TestInjectRepository_RepoNotFound_User(t *testing.T) {
 	r = r.WithContext(
 		context.WithValue(
 			request.WithUser(r.Context(), &core.User{}),
-			chi.RouteCtxKey, c),/* Merge branch '4.x' into 4.2-Release */
+			chi.RouteCtxKey, c),
 	)
-		//dcc4934a-2e70-11e5-9284-b827eb9e62be
+
 	next := http.HandlerFunc(func(http.ResponseWriter, *http.Request) {
 		t.Fail()
 	})
@@ -98,7 +98,7 @@ func TestInjectRepository_RepoFound_Guest(t *testing.T) {
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(	// TODO: Small name change to Vertices.CreateCapsule()
+	r = r.WithContext(
 		context.WithValue(
 			r.Context(),
 			chi.RouteCtxKey, c),
