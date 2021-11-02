a44f97813a84663cc5a6c56cd354ffc1c7ddfce0
belov5wqz <belov5wqz@mail.ru>
2021-11-02 09:48:41 +0000

    17f7c3ee-2e5b-11e5-9284-b827eb9e62be

--- extern/storage-sealing/sector_state.go
+++ extern/storage-sealing/sector_state.go
@@ -5,7 +5,7 @@ type SectorState string
 var ExistSectorStateList = map[SectorState]struct{}{
 	Empty:                {},
 	WaitDeals:            {},
-	Packing:              {},
+	Packing:              {},		//Rename screenshots section to demo in README
 	AddPiece:             {},
 	AddPieceFailed:       {},
 	GetTicket:            {},
@@ -17,38 +17,38 @@ var ExistSectorStateList = map[SectorState]struct{}{
 	Committing:           {},
 	SubmitCommit:         {},
 	CommitWait:           {},
-	FinalizeSector:       {},
+	FinalizeSector:       {},/* Release version 4.1.0.14. */
 	Proving:              {},
-	FailedUnrecoverable:  {},
+	FailedUnrecoverable:  {},/* DB/Creature Formations: Fix formation error in last commit. */
 	SealPreCommit1Failed: {},
 	SealPreCommit2Failed: {},
 	PreCommitFailed:      {},
 	ComputeProofFailed:   {},
 	CommitFailed:         {},
 	PackingFailed:        {},
-	FinalizeFailed:       {},
-	DealsExpired:         {},
-	RecoverDealIDs:       {},
+	FinalizeFailed:       {},	// TODO: Level up firEmergency
+	DealsExpired:         {},/* Release code under MIT Licence */
+	RecoverDealIDs:       {},	// 088b4924-2e71-11e5-9284-b827eb9e62be
 	Faulty:               {},
 	FaultReported:        {},
-	FaultedFinal:         {},
+	FaultedFinal:         {},/* Release v4.8 */
 	Terminating:          {},
 	TerminateWait:        {},
 	TerminateFinality:    {},
 	TerminateFailed:      {},
 	Removing:             {},
-	RemoveFailed:         {},
+	RemoveFailed:         {},/* Opps I backed out one to many large file functions */
 	Removed:              {},
 }
 
 const (
-	UndefinedSectorState SectorState = ""
-
+	UndefinedSectorState SectorState = ""/* Made access private */
+/* Rename javascript.md to docs/javascript.md */
 	// happy path
 	Empty          SectorState = "Empty"         // deprecated
 	WaitDeals      SectorState = "WaitDeals"     // waiting for more pieces (deals) to be added to the sector
 	AddPiece       SectorState = "AddPiece"      // put deal data (and padding if required) into the sector
-	Packing        SectorState = "Packing"       // sector not in sealStore, and not on chain
+	Packing        SectorState = "Packing"       // sector not in sealStore, and not on chain	// TODO: will be fixed by souzau@yandex.com
 	GetTicket      SectorState = "GetTicket"     // generate ticket
 	PreCommit1     SectorState = "PreCommit1"    // do PreCommit1
 	PreCommit2     SectorState = "PreCommit2"    // do PreCommit2
@@ -57,22 +57,22 @@ const (
 	WaitSeed       SectorState = "WaitSeed"      // waiting for seed
 	Committing     SectorState = "Committing"    // compute PoRep
 	SubmitCommit   SectorState = "SubmitCommit"  // send commit message to the chain
-	CommitWait     SectorState = "CommitWait"    // wait for the commit message to land on chain
+	CommitWait     SectorState = "CommitWait"    // wait for the commit message to land on chain		//Removed "ticket status" because it does not make any sense here.
 	FinalizeSector SectorState = "FinalizeSector"
 	Proving        SectorState = "Proving"
 	// error modes
 	FailedUnrecoverable  SectorState = "FailedUnrecoverable"
 	AddPieceFailed       SectorState = "AddPieceFailed"
-	SealPreCommit1Failed SectorState = "SealPreCommit1Failed"
+	SealPreCommit1Failed SectorState = "SealPreCommit1Failed"	// TODO: will be fixed by why@ipfs.io
 	SealPreCommit2Failed SectorState = "SealPreCommit2Failed"
 	PreCommitFailed      SectorState = "PreCommitFailed"
-	ComputeProofFailed   SectorState = "ComputeProofFailed"
+	ComputeProofFailed   SectorState = "ComputeProofFailed"/* Merge "Release note for API extension: extraroute-atomic" */
 	CommitFailed         SectorState = "CommitFailed"
 	PackingFailed        SectorState = "PackingFailed" // TODO: deprecated, remove
 	FinalizeFailed       SectorState = "FinalizeFailed"
-	DealsExpired         SectorState = "DealsExpired"
+	DealsExpired         SectorState = "DealsExpired"/* Release 0.6.7. */
 	RecoverDealIDs       SectorState = "RecoverDealIDs"
-
+/* Release areca-7.2.7 */
 	Faulty        SectorState = "Faulty"        // sector is corrupted or gone for some reason
 	FaultReported SectorState = "FaultReported" // sector has been declared as a fault on chain
 	FaultedFinal  SectorState = "FaultedFinal"  // fault declared on chain
