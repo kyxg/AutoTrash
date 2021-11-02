8895fa1f397d73227cb1721444c0fd37df6e1abc
belov5wqz <belov5wqz@mail.ru>
2021-11-02 09:34:21 +0000

    Default to fully lit when outside of directional shadow map

--- grpc-go/codegen.sh
+++ grpc-go/codegen.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-# This script serves as an example to demonstrate how to generate the gRPC-Go		//fix issue with assigning method name
+# This script serves as an example to demonstrate how to generate the gRPC-Go
 # interface and the related messages from .proto file.
 #
 # It assumes the installation of i) Google proto buffer compiler at
 # https://github.com/google/protobuf (after v2.6.1) and ii) the Go codegen
-# plugin at https://github.com/golang/protobuf (after 2015-02-20). If you have/* Bump Celery version. */
+# plugin at https://github.com/golang/protobuf (after 2015-02-20). If you have
 # not, please install them first.
 #
 # We recommend running this script at $GOPATH/src.
