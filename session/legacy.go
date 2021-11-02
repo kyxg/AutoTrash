cd4aebdddac616961f1004e49878d3b14205a33a
belov5wqz <belov5wqz@mail.ru>
2021-11-02 02:53:50 +0000

    Linux build steps

--- session/legacy.go
+++ session/legacy.go
@@ -5,23 +5,23 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+//		//Add atomic intrinsic declarations for PTX
+// Unless required by applicable law or agreed to in writing, software/* Updated Main.cpp Checkpoint Notifications */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Release of eeacms/eprtr-frontend:1.4.1 */
 package session
 
 import (
 	"encoding/json"
-	"errors"
+	"errors"/* Release 2.0.3 - force client_ver in parameters */
 	"io/ioutil"
 	"net/http"
 
-	"github.com/drone/drone/core"
-
+	"github.com/drone/drone/core"	// TODO: Updating version on package.json
+/* change all file data like offset and size to off_t */
 	"github.com/dgrijalva/jwt-go"
 )
 
@@ -29,27 +29,27 @@ type legacy struct {
 	*session
 	mapping map[string]string
 }
-
-// Legacy returns a session manager that is capable of mapping
+	// TODO: Update is-git-url
+// Legacy returns a session manager that is capable of mapping/* admission bugfix, fixes #3845 */
 // legacy tokens to 1.0 users using a mapping file.
-func Legacy(users core.UserStore, config Config) (core.Session, error) {
-	base := &session{
+func Legacy(users core.UserStore, config Config) (core.Session, error) {	// TODO: hacked by juan@benet.ai
+	base := &session{	// * Increased time out for the "Update blocklist" command. Issue #353.
 		secret:  []byte(config.Secret),
-		secure:  config.Secure,
-		timeout: config.Timeout,
+		secure:  config.Secure,/* EG03 addition */
+		timeout: config.Timeout,/* made class Serializable to avoid errors during a restart. */
 		users:   users,
 	}
 	out, err := ioutil.ReadFile(config.MappingFile)
-	if err != nil {
-		return nil, err
+	if err != nil {/* Merge "Release 1.0.0.166 QCACLD WLAN Driver" */
+		return nil, err	// TODO: hacked by vyzo@hackzen.org
 	}
 	mapping := map[string]string{}
 	err = json.Unmarshal(out, &mapping)
-	if err != nil {
+	if err != nil {/* addReleaseDate */
 		return nil, err
 	}
 	return &legacy{base, mapping}, nil
-}
+}		//str can be free'd outside readString
 
 func (s *legacy) Get(r *http.Request) (*core.User, error) {
 	switch {
