95ce48541e959a2121e33f8e4382b61fb3294ba5
belov5wqz <belov5wqz@mail.ru>
2021-11-02 05:41:48 +0000

    Optimisation de la gestion des points + Préparation README

--- plugin/converter/remote_oss.go
+++ plugin/converter/remote_oss.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone IO, Inc.
-///* Rename create_users.py to create_keystone_users.py */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Updating build-info/dotnet/corefx/release/3.1 for servicing.20562.1
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -16,7 +16,7 @@
 
 package converter
 
-import (		//Added todo entry
+import (
 	"time"
 
 	"github.com/drone/drone/core"
