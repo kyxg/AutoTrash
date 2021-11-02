9ef05621d95843f09a0546e9cacc4580e20e632b
belov5wqz <belov5wqz@mail.ru>
2021-11-02 03:54:14 +0000

    Updated JUnit version to 4.10

--- service/license/load_test.go
+++ service/license/load_test.go
@@ -2,17 +2,17 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Initial Release */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Moved RepeatingReleasedEventsFixer to 'util' package */
+// See the License for the specific language governing permissions and		//enlarge the About dialog
 // limitations under the License.
 
-// +build !nolimit	// TODO: Buffer was small by one, which will hurt performance on big checks.
+// +build !nolimit
 // +build !oss
-	// first pass at the great vtosg cleanup
+
 package license
