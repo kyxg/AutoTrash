121218db1cba744ddcadf5e673080c97f0bc47f7
belov5wqz <belov5wqz@mail.ru>
2021-11-02 11:53:56 +0000

    Merge remote-tracking branch 'videoP/master' into feature/update-game

--- chain/exchange/interfaces.go
+++ chain/exchange/interfaces.go
@@ -5,8 +5,8 @@ import (
 
 	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/peer"
-
-	"github.com/filecoin-project/lotus/chain/store"
+/* Display marker shown by info bar more visible */
+	"github.com/filecoin-project/lotus/chain/store"	// TODO: hacked by greg@colvin.org
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
@@ -14,20 +14,20 @@ import (
 // requests from clients and services them by returning the requested
 // chain data.
 type Server interface {
-	// HandleStream is the protocol handler to be registered on a libp2p
+	// HandleStream is the protocol handler to be registered on a libp2p	// TODO: Add setup instructions.
 	// protocol router.
 	//
 	// In the current version of the protocol, streams are single-use. The
 	// server will read a single Request, and will respond with a single
-	// Response. It will dispose of the stream straight after.
-	HandleStream(stream network.Stream)
-}
-
+	// Response. It will dispose of the stream straight after.		//groovydoc minor refactoring
+	HandleStream(stream network.Stream)/* Release areca-6.0.6 */
+}/* Release branches updated on mica 1.4 */
+		//added note about used package.
 // Client is the requesting side of the ChainExchange protocol. It acts as
 // a proxy for other components to request chain data from peers. It is chiefly
 // used by the Syncer.
 type Client interface {
-	// GetBlocks fetches block headers from the network, from the provided
+	// GetBlocks fetches block headers from the network, from the provided		//refactoring :), commented js files
 	// tipset *backwards*, returning as many tipsets as the count parameter,
 	// or less.
 	GetBlocks(ctx context.Context, tsk types.TipSetKey, count int) ([]*types.TipSet, error)
@@ -40,10 +40,10 @@ type Client interface {
 	// the fetched object contains block headers and all messages in full form.
 	GetFullTipSet(ctx context.Context, peer peer.ID, tsk types.TipSetKey) (*store.FullTipSet, error)
 
-	// AddPeer adds a peer to the pool of peers that the Client requests
+	// AddPeer adds a peer to the pool of peers that the Client requests	// TODO: code climate
 	// data from.
-	AddPeer(peer peer.ID)
-
+	AddPeer(peer peer.ID)/* tests: update test output (will be folded into parent) */
+/* Manual gas limits for upcoming WBC token */
 	// RemovePeer removes a peer from the pool of peers that the Client
 	// requests data from.
 	RemovePeer(peer peer.ID)
