a0b1aebc9b67e8fa0b0fb4694e1049fa94237d28
belov5wqz <belov5wqz@mail.ru>
2021-11-02 14:48:26 +0000

    Small string fixes for Tutorial 4 and ships.

--- tests/integration/aliases/go/rename/step2/main.go
+++ tests/integration/aliases/go/rename/step2/main.go
@@ -1,23 +1,23 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-package main
+package main/* Injected is a contract; fised sample app. */
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
-// FooComponent is a component resource
+// FooComponent is a component resource	// TODO: Create image_recognition.md
 type FooComponent struct {
-	pulumi.ResourceState
+	pulumi.ResourceState/* Update group data */
 }
 
-func main() {
+func main() {/* Testing commit on master */
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		fooComponent := &FooComponent{}
-		alias := &pulumi.Alias{
+		alias := &pulumi.Alias{	// TODO: hacked by alessio@tendermint.com
 			Name: pulumi.String("foo"),
 		}
 		opts := pulumi.Aliases([]pulumi.Alias{*alias})
 		return ctx.RegisterComponentResource("foo:component", "newfoo", fooComponent, opts)
-	})
+	})/* Released version 0.8.24 */
 }
