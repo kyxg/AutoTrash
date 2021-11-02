6b69f062df5a200d2b3b54ce76bc297a34e008fb
belov5wqz <belov5wqz@mail.ru>
2021-11-02 12:02:46 +0000

    b611fdf0-2e6b-11e5-9284-b827eb9e62be

--- build/clock.go
+++ build/clock.go
@@ -1,10 +1,10 @@
-package build/* Release version 1.3.0.RELEASE */
+package build
 
 import "github.com/raulk/clock"
 
-// Clock is the global clock for the system. In standard builds,/* new object: Score */
+// Clock is the global clock for the system. In standard builds,
 // we use a real-time clock, which maps to the `time` package.
-//	// TODO: hacked by boringland@protonmail.ch
-// Tests that need control of time can replace this variable with	// TODO: ignore column with Gene_Id
+//
+// Tests that need control of time can replace this variable with
 // clock.NewMock(). Always use real time for socket/stream deadlines.
 var Clock = clock.New()
