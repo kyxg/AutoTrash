de350177bbf06101ef6e5bb29af8fd1a783ed39d
belov5wqz <belov5wqz@mail.ru>
2021-11-02 08:55:21 +0000

    2b131c38-2e53-11e5-9284-b827eb9e62be

--- core/trigger_test.go
+++ core/trigger_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//* Use getClass() for get Class loader
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// TODO: hacked by greg@colvin.org
 // +build !oss
 
-package core/* Nov 1 - 6:56 */
+package core
