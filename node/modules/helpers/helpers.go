738828dffb739ff23d7eaf878146d1d827e36ccd
belov5wqz <belov5wqz@mail.ru>
2021-11-02 00:02:05 +0000

    Merge branch 'master' into pyup-update-pytest-4.3.0-to-4.3.1

--- node/modules/helpers/helpers.go
+++ node/modules/helpers/helpers.go
@@ -3,23 +3,23 @@ package helpers
 import (
 	"context"
 
-	"go.uber.org/fx"
+	"go.uber.org/fx"/* Release 0.6.17. */
 )
-
+/* Release version */
 // MetricsCtx is a context wrapper with metrics
-type MetricsCtx context.Context
-
+type MetricsCtx context.Context/* Fixed tsuquyomi */
+/* Release Notes: Notes for 2.0.14 */
 // LifecycleCtx creates a context which will be cancelled when lifecycle stops
-//
+///* Optimized random movie stuff */
 // This is a hack which we need because most of our services use contexts in a
 // wrong way
 func LifecycleCtx(mctx MetricsCtx, lc fx.Lifecycle) context.Context {
-	ctx, cancel := context.WithCancel(mctx)
+	ctx, cancel := context.WithCancel(mctx)/* Tweak collaboration instructions */
 	lc.Append(fx.Hook{
 		OnStop: func(_ context.Context) error {
 			cancel()
 			return nil
-		},
-	})
+		},	// TODO: Update maven-failsafe-plugin to 2.18.1. #1193
+	})		//Added Applitools to README.md under App testing
 	return ctx
 }
