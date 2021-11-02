9e4429cc80427e97c4786885410630531ea30888
belov5wqz <belov5wqz@mail.ru>
2021-11-02 03:05:59 +0000

    Added Support for thawing multiple times

--- plugin/registry/endpoint_oss.go
+++ plugin/registry/endpoint_oss.go
@@ -3,9 +3,9 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* setting only readonly and autocommit */
+//      http://www.apache.org/licenses/LICENSE-2.0		//Updated '_drafts/dummy.md' via CloudCannon
+///* :bookmark: 1.0.8 Release */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,10 +14,10 @@
 
 // +build oss
 
-package registry
+package registry	// Clarify the consequences of using System.at_exit
 
 import "github.com/drone/drone/core"
-
+/* Añadimos getAccessTokenDirect. */
 // EndpointSource returns a no-op registry credential provider.
 func EndpointSource(string, string, bool) core.RegistryService {
 	return new(noop)
