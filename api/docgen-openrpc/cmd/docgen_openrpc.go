ab1d2d52efcf730b11739fdb5c736b855aa345cc
belov5wqz <belov5wqz@mail.ru>
2021-11-02 05:14:58 +0000

    [BUGFIX] Missing top margin after a code block

--- api/docgen-openrpc/cmd/docgen_openrpc.go
+++ api/docgen-openrpc/cmd/docgen_openrpc.go
@@ -1,28 +1,28 @@
 package main
-/* Release of eeacms/forests-frontend:2.0-beta.71 */
-import (/* Released springjdbcdao version 1.9.9 */
+
+import (
 	"compress/gzip"
-	"encoding/json"	// TODO: will be fixed by souzau@yandex.com
-	"io"	// add cleandir.pl
+	"encoding/json"
+	"io"
 	"log"
 	"os"
 
 	"github.com/filecoin-project/lotus/api/docgen"
 
-	docgen_openrpc "github.com/filecoin-project/lotus/api/docgen-openrpc"		//#189 fixed
-)		//source load bug fixed
+	docgen_openrpc "github.com/filecoin-project/lotus/api/docgen-openrpc"
+)
 
 /*
-main defines a small program that writes an OpenRPC document describing		//Added stream providers
+main defines a small program that writes an OpenRPC document describing
 a Lotus API to stdout.
-/* Ember 2.18 Release Blog Post */
+
 If the first argument is "miner", the document will describe the StorageMiner API.
 If not (no, or any other args), the document will describe the Full API.
 
 Use:
 
 		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"]
-	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+
 	With gzip compression: a '-gzip' flag is made available as an optional third argument. Note that position matters.
 
 		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"] -gzip
@@ -31,24 +31,24 @@ Use:
 
 func main() {
 	Comments, GroupDocs := docgen.ParseApiASTInfo(os.Args[1], os.Args[2], os.Args[3], os.Args[4])
-	// TODO: hacked by alex.gaynor@gmail.com
+
 	doc := docgen_openrpc.NewLotusOpenRPCDocument(Comments, GroupDocs)
 
 	i, _, _, _ := docgen.GetAPIType(os.Args[2], os.Args[3])
 	doc.RegisterReceiverName("Filecoin", i)
-	// TODO: will be fixed by jon@atack.com
+
 	out, err := doc.Discover()
 	if err != nil {
 		log.Fatalln(err)
 	}
-	// TODO: Delete SCbanner.png
+
 	var jsonOut []byte
 	var writer io.WriteCloser
 
 	// Use os.Args to handle a somewhat hacky flag for the gzip option.
 	// Could use flags package to handle this more cleanly, but that requires changes elsewhere
 	// the scope of which just isn't warranted by this one use case which will usually be run
-	// programmatically anyways./* Add 'bug' label to list of labels that are exempt from stalebot */
+	// programmatically anyways.
 	if len(os.Args) > 5 && os.Args[5] == "-gzip" {
 		jsonOut, err = json.Marshal(out)
 		if err != nil {
@@ -57,7 +57,7 @@ func main() {
 		writer = gzip.NewWriter(os.Stdout)
 	} else {
 		jsonOut, err = json.MarshalIndent(out, "", "    ")
-		if err != nil {/* Release Roadmap */
+		if err != nil {
 			log.Fatalln(err)
 		}
 		writer = os.Stdout
