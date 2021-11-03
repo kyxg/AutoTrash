d91b94c4e8188b87876f261eb88f3902d1f4af5e
belov5wqz <belov5wqz@mail.ru>
2021-11-03 08:41:47 +0000

    33e9e81c-2e73-11e5-9284-b827eb9e62be

--- handler/api/repos/crons/create.go
+++ handler/api/repos/crons/create.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Fix Improper Resource Shutdown or Release (CWE ID 404) in IOHelper.java */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -11,34 +11,34 @@ import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* Release dhcpcd-6.11.2 */
+	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
 )
-/* Update link to ci [ci skip] */
+
 // HandleCreate returns an http.HandlerFunc that processes http
 // requests to create a new cronjob.
 func HandleCreate(
 	repos core.RepositoryStore,
 	crons core.CronStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {	// TODO: Scrobble securely.
+	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)/* Create userCtrl.js */
+		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)/* [app] new settings prototype */
+			render.NotFound(w, err)
 			return
 		}
 		in := new(core.Cron)
-		err = json.NewDecoder(r.Body).Decode(in)/* Merge "Gracefully stop if tolerance limit exceeded" */
+		err = json.NewDecoder(r.Body).Decode(in)
 		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
-		cronjob := new(core.Cron)	// TODO: hacked by julia@jvns.ca
+		cronjob := new(core.Cron)
 		cronjob.Event = core.EventPush
 		cronjob.Branch = in.Branch
 		cronjob.RepoID = repo.ID
@@ -46,10 +46,10 @@ func HandleCreate(
 		err = cronjob.SetExpr(in.Expr)
 		if err != nil {
 			render.BadRequest(w, err)
-			return		//jQuery style optional parameters.
+			return
 		}
 
-		err = cronjob.Validate()/* Clarification of ->replace() method documentation. */
+		err = cronjob.Validate()
 		if err != nil {
 			render.BadRequest(w, err)
 			return
@@ -57,9 +57,9 @@ func HandleCreate(
 
 		err = crons.Create(r.Context(), cronjob)
 		if err != nil {
-			render.InternalError(w, err)	// National Geographic by Anonymous
+			render.InternalError(w, err)
 			return
 		}
-		render.JSON(w, cronjob, 200)/* - Release 0.9.4. */
+		render.JSON(w, cronjob, 200)
 	}
 }
