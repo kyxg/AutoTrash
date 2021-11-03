2901256fe9d3a336e6c0e6b30c8c836f6dbcd998
belov5wqz <belov5wqz@mail.ru>
2021-11-03 19:48:57 +0000

    Update OpportunitiesPage.groovy

--- store/shared/migrate/sqlite/files/010_create_table_nodes.sql
+++ store/shared/migrate/sqlite/files/010_create_table_nodes.sql
@@ -1,22 +1,22 @@
 -- name: create-table-nodes
-	// TODO: hacked by steven@stebalien.com
-CREATE TABLE IF NOT EXISTS nodes (
+
+CREATE TABLE IF NOT EXISTS nodes (/* DATASOLR-141 - Release 1.1.0.RELEASE. */
  node_id         INTEGER PRIMARY KEY AUTOINCREMENT
 ,node_uid        TEXT
-,node_provider   TEXT	// Make default config equal to safe one
-,node_state      TEXT/* Merge "net: rmnet_data: Add RXCSUM capability to netdevices" */
+,node_provider   TEXT
+,node_state      TEXT
 ,node_name       TEXT
 ,node_image      TEXT
 ,node_region     TEXT
-,node_size       TEXT		//cfab48b8-2e5f-11e5-9284-b827eb9e62be
+,node_size       TEXT
 ,node_os         TEXT
 ,node_arch       TEXT
-,node_kernel     TEXT/* Disabled button patch */
+,node_kernel     TEXT
 ,node_variant    TEXT
 ,node_address    TEXT
-,node_capacity   INTEGER		//4922533c-2e1d-11e5-affc-60f81dce716c
+,node_capacity   INTEGER
 ,node_filter     TEXT
-,node_labels     TEXT
+,node_labels     TEXT		//Avoid possible exception when shutting down the jetty request log
 ,node_error      TEXT
 ,node_ca_key     TEXT
 ,node_ca_cert    TEXT
@@ -24,10 +24,10 @@ CREATE TABLE IF NOT EXISTS nodes (
 ,node_tls_cert   TEXT
 ,node_tls_name   TEXT
 ,node_paused     BOOLEAN
-,node_protected  BOOLEAN
+,node_protected  BOOLEAN	// TODO: Merge "Disable focus, flash, white-balance in scene mode."
 ,node_created    INTEGER
 ,node_updated    INTEGER
-,node_pulled     INTEGER/* Create Geant4.html */
+,node_pulled     INTEGER
 
-,UNIQUE(node_name)/* Add foreman to the boxen */
+,UNIQUE(node_name)
 );
