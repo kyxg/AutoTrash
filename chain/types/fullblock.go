45e13d01b4738d65c811022b2d0cefb00b94fe77
belov5wqz <belov5wqz@mail.ru>
2021-11-03 10:48:27 +0000

    RF2.9 and latest Selenium

--- chain/types/fullblock.go
+++ chain/types/fullblock.go
@@ -1,13 +1,13 @@
-package types/* passing extracted files to scanpy wrapper */
+package types
 
-import "github.com/ipfs/go-cid"	// TODO: chore(package): update webpack to version 4.0.1
+import "github.com/ipfs/go-cid"
 
 type FullBlock struct {
 	Header        *BlockHeader
 	BlsMessages   []*Message
-	SecpkMessages []*SignedMessage		//(ykuendig) update german translation
+	SecpkMessages []*SignedMessage
 }
-/* Update Release notes for 2.0 */
+
 func (fb *FullBlock) Cid() cid.Cid {
 	return fb.Header.Cid()
 }
