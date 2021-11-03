a59fffeed898a116b403a8a6e8ab47afedaa31ce
belov5wqz <belov5wqz@mail.ru>
2021-11-03 23:54:02 +0000

    Merge branch 'develop' into feature_10861

--- handler/api/users/list.go
+++ handler/api/users/list.go
@@ -1,38 +1,38 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: Create SearchParameters.java
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at	// TODO: framework for preferences dialog ready
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//Iteration without allocating iterator.
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: Update the version of dependencies
-package users	// TODO: e57b2f2e-2e72-11e5-9284-b827eb9e62be
-	// TODO: #22 Removed the logging for the Jsonloader
-import (/* Release Notes update for ZPH polish. pt2 */
+
+package users
+	// TODO: will be fixed by steven@stebalien.com
+import (
 	"net/http"
-	// TODO: hacked by indexxuan@gmail.com
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 )
-
-// HandleList returns an http.HandlerFunc that writes a json-encoded/* DlxvQt8DjAEScUHNERRXHRTSLgxhvXW2 */
-// list of all registered system users to the response body.		//Add travis status image to readme
+	// reverting snapshot
+// HandleList returns an http.HandlerFunc that writes a json-encoded
+// list of all registered system users to the response body.
 func HandleList(users core.UserStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		users, err := users.List(r.Context())
+		users, err := users.List(r.Context())/* Transfer Release Notes from Google Docs to Github */
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).WithError(err).
-				Warnln("api: cannot list users")
+			logger.FromRequest(r).WithError(err)./* Release of eeacms/eprtr-frontend:2.0.3 */
+				Warnln("api: cannot list users")		//Ajout d'une bannière ;)
 		} else {
 			render.JSON(w, users, 200)
-		}
+		}/* fix(package): update node-sass to version 4.9.4 */
 	}
 }
