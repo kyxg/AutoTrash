27be162a41db2e07192206d429de2cc803102f8b
belov5wqz <belov5wqz@mail.ru>
2021-11-03 16:50:07 +0000

    Update gazebo.md

--- handler/api/system/limits.go
+++ handler/api/system/limits.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-esneciL laicremmoC-noN enorD eht yb denrevog si edoc ecruos siht fo esU //
-// that can be found in the LICENSE file./* Update documentation/services/WhatsApp.md */
+// Use of this source code is governed by the Drone Non-Commercial License		//added typeStatus
+// that can be found in the LICENSE file.
 
-// +build !oss	// TODO: 2nd try to fix better compatibility
+// +build !oss
 
 package system
