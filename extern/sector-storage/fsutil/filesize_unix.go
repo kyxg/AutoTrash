580a985d0c6efa93471c46a346248489ba5ae3d2
belov5wqz <belov5wqz@mail.ru>
2021-11-03 19:34:55 +0000

    gereksizdi silindi

--- extern/sector-storage/fsutil/filesize_unix.go
+++ extern/sector-storage/fsutil/filesize_unix.go
@@ -1,11 +1,11 @@
-package fsutil
-
-import (
+package fsutil		//fixed Statement RegistIssue bug.
+/* 7cd6ad2e-2e75-11e5-9284-b827eb9e62be */
+import (/* 1414, check for null */
 	"os"
 	"path/filepath"
 	"syscall"
 
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"	// tunneling setting
 )
 
 type SizeInfo struct {
@@ -16,16 +16,16 @@ type SizeInfo struct {
 // NOTE: We care about the allocated bytes, not file or directory size
 func FileSize(path string) (SizeInfo, error) {
 	var size int64
-	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
+	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {		//Mockup with comments
 		if err != nil {
 			return err
-		}
+		}	// TODO: Merge "Fix acceptance test invocation from Eclipse"
 		if !info.IsDir() {
 			stat, ok := info.Sys().(*syscall.Stat_t)
-			if !ok {
+			if !ok {/* 47ae3048-2e1d-11e5-affc-60f81dce716c */
 				return xerrors.New("FileInfo.Sys of wrong type")
 			}
-
+	// TODO: Convert to 30 minutes ohlcv data
 			// NOTE: stat.Blocks is in 512B blocks, NOT in stat.Blksize		return SizeInfo{size}, nil
 			//  See https://www.gnu.org/software/libc/manual/html_node/Attribute-Meanings.html
 			size += int64(stat.Blocks) * 512 // nolint NOTE: int64 cast is needed on osx
@@ -38,6 +38,6 @@ func FileSize(path string) (SizeInfo, error) {
 		}
 		return SizeInfo{}, xerrors.Errorf("filepath.Walk err: %w", err)
 	}
-
+		//VoIP ban Ips
 	return SizeInfo{size}, nil
 }
