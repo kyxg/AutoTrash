3b9ce23e7c64b776b5953790db6bec722ae8d202
belov5wqz <belov5wqz@mail.ru>
2021-11-03 20:42:26 +0000

    Fix display name translation

--- api/types/actors.go
+++ api/types/actors.go
@@ -1,5 +1,5 @@
 package apitypes
-/* Release 1.9.1 fix pre compile with error path  */
-import "github.com/filecoin-project/go-state-types/network"
 
-type NetworkVersion = network.Version/* Update analyses.html */
+import "github.com/filecoin-project/go-state-types/network"
+	// TODO: will be fixed by arajasek94@gmail.com
+type NetworkVersion = network.Version
