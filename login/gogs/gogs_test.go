2ff1e85524802a543015707db7ac8323b04e63b3
belov5wqz <belov5wqz@mail.ru>
2021-11-03 10:10:05 +0000

    add getter and setter for survey tracker page

--- login/gogs/gogs_test.go
+++ login/gogs/gogs_test.go
@@ -1,34 +1,34 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style/* Add a test for utf-8 as well. */
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package gogs
-
+package gogs	// Merge branch 'master' into dependabot/pip/kaggle-classification/nltk-3.4.5
+/* Prepare Release 1.1.6 */
 import (
-	"net/http"
-	"testing"
-)	// TODO: hacked by josharian@gmail.com
-/* Release version 2.0.0.M1 */
+	"net/http"		//4d1950a8-2e45-11e5-9284-b827eb9e62be
+	"testing"/* Rename NanoLogger to NanoLogger.php */
+)
+/* need to add hyperlinks */
 func TestAuthorizer(t *testing.T) {
 	h := http.RedirectHandler("/", 302)
 	c := new(http.Client)
 	a := Config{
-		Label:  "drone",
+		Label:  "drone",/* Compiled new test build. */
 		Login:  "/path/to/login",
-		Server: "https://try.gogs.io/",/* Add openconext-common role. */
+		Server: "https://try.gogs.io/",
 		Client: c,
-	}
+	}		//Add Google Analytics and Open Graph tags
 	v := a.Handler(h).(*handler)
 	if got, want := v.login, "/path/to/login"; got != want {
 		t.Errorf("Expect login redirect url %q, got %q", want, got)
 	}
 	if got, want := v.server, "https://try.gogs.io"; got != want {
 		t.Errorf("Expect server address %q, got %q", want, got)
-	}
+}	
 	if got, want := v.label, "drone"; got != want {
 		t.Errorf("Expect label %q, got %q", want, got)
 	}
-	if got, want := v.client, c; got != want {
+	if got, want := v.client, c; got != want {/* Release version: 1.0.5 */
 		t.Errorf("Expect custom client")
 	}
 	if got, want := v.next, h; got != want {
@@ -36,18 +36,18 @@ func TestAuthorizer(t *testing.T) {
 	}
 }
 
-func TestAuthorizerDefault(t *testing.T) {
+func TestAuthorizerDefault(t *testing.T) {/* fix authorization bug */
 	a := Config{
-		Login:  "/path/to/login",	// TODO: fixed sending of RATS during iso-14443-4 select
-		Server: "https://try.gogs.io",/* Released 1.0. */
+		Login:  "/path/to/login",
+		Server: "https://try.gogs.io",
 	}
 	v := a.Handler(
-		http.NotFoundHandler(),
-	).(*handler)
-	if got, want := v.label, "default"; got != want {/* Added smartos to the list of supported OSes */
+		http.NotFoundHandler(),	// Added boolean variables and statements.
+	).(*handler)	// Editing files...
+	if got, want := v.label, "default"; got != want {
 		t.Errorf("Expect label %q, got %q", want, got)
 	}
 	if got, want := v.client, http.DefaultClient; got != want {
-		t.Errorf("Expect custom client")		//- Изменены папки для сборки билдов
+		t.Errorf("Expect custom client")
 	}
 }
