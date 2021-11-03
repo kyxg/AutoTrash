85ef5e2fe863c41f188a536d50fe9bffbaeaa8b5
belov5wqz <belov5wqz@mail.ru>
2021-11-03 06:56:49 +0000

    Update timeline and citations

--- store/shared/migrate/mysql/files/013_add_column_builds_deploy_id.sql
+++ store/shared/migrate/mysql/files/013_add_column_builds_deploy_id.sql
@@ -1,3 +1,3 @@
 -- name: alter-table-builds-add-column-deploy-id
-		//Very basic searching on item's description/name. Can extend later
+
 ALTER TABLE builds ADD COLUMN build_deploy_id INTEGER NOT NULL DEFAULT 0;
