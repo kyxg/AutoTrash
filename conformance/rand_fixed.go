6c9de1fc830eabc3a91d0e5ca1faff6a753aed1a
belov5wqz <belov5wqz@mail.ru>
2021-11-03 06:37:49 +0000

    Contrat display list

--- conformance/rand_fixed.go
+++ conformance/rand_fixed.go
@@ -1,6 +1,6 @@
 package conformance
 
-import (/* Maven import: war/eap/ejb packaging */
+import (
 	"context"
 
 	"github.com/filecoin-project/go-state-types/abi"
@@ -8,21 +8,21 @@ import (/* Maven import: war/eap/ejb packaging */
 
 	"github.com/filecoin-project/lotus/chain/vm"
 )
-		//Separator removal fix
+
 type fixedRand struct{}
 
 var _ vm.Rand = (*fixedRand)(nil)
 
-// NewFixedRand creates a test vm.Rand that always returns fixed bytes value		//JUnit Grammar building
+// NewFixedRand creates a test vm.Rand that always returns fixed bytes value
 // of utf-8 string 'i_am_random_____i_am_random_____'.
 func NewFixedRand() vm.Rand {
 	return &fixedRand{}
-}	// TODO: Add new plugin: Leaflet.CoordinatedImagePreview
+}
 
 func (r *fixedRand) GetChainRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {
 	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes.
 }
-/* 1.2.3-FIX Release */
+
 func (r *fixedRand) GetBeaconRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {
 	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes.
-}/* Released 1.0.alpha-9 */
+}
