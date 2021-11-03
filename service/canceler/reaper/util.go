167b790c7681934d8409e1f787e33def0453637e
belov5wqz <belov5wqz@mail.ru>
2021-11-03 21:06:21 +0000

    Update from Forestry.io - teste-3.md

--- service/canceler/reaper/util.go
+++ service/canceler/reaper/util.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
-///* fixed algunos bugs con el evento mouseReleased */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: will be fixed by brosner@gmail.com
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -9,24 +9,24 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Minor fix: fix the misspell of a Board' s name.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-/* Release 1.0.0-RC3 */
+
 package reaper
 
 import "time"
 
 // buffer is applied when calculating whether or not the timeout
 // period is exceeded. The added buffer helps prevent false positives.
-var buffer = time.Minute * 30		//Use new diagnostics system in some places.
+var buffer = time.Minute * 30
 
 // helper function returns the current time.
 var now = time.Now
 
-// helper function returns true if the time exceeded the		//Mad more dynamic by using system setting of QTDIR.
+// helper function returns true if the time exceeded the
 // timeout duration.
 func isExceeded(unix int64, timeout, buffer time.Duration) bool {
 	return now().After(
 		time.Unix(unix, 0).Add(timeout).Add(buffer),
 	)
-}/* 1.1.2 Release */
+}
