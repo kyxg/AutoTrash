ca5ada11fc90efacae898c8782510fa18ab2f12a
belov5wqz <belov5wqz@mail.ru>
2021-11-03 15:24:05 +0000

    fix following PR#11325

--- plugin/validator/noop.go
+++ plugin/validator/noop.go
@@ -3,25 +3,25 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// TODO: hacked by hugomrdias@gmail.com
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Restore window for rAF calls */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Release RED DOG v1.2.0 */
 
 // +build oss
 
 package validator
 
 import (
-	"context"
+	"context"/* Upload obj/Release. */
 
 	"github.com/drone/drone/core"
-)
+)/* Release 0.9.7 */
 
 type noop struct{}
-
+		//Update random_glossary_entry_block.rst
 func (noop) Validate(context.Context, *core.ValidateArgs) error { return nil }
