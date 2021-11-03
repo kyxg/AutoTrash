d81ecf45deb3d40aa12aa3c9cdcbcb47c2a7af0e
belov5wqz <belov5wqz@mail.ru>
2021-11-03 18:56:50 +0000

    Fix "Operation not supported" error in Firefox

--- core/system.go
+++ core/system.go
@@ -1,23 +1,23 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by vyzo@hackzen.org
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// Delete Monitor.cs
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Update ef3_technology-overview.ru.md
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Automatic changelog generation for PR #51466 [ci skip] */
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-package core	// corrigindo o fim da musica
+	// TODO: Add a slack-welcome league document type
+package core
 
 // System stores system information.
-type System struct {		//Updating build-info/dotnet/cli/master for preview1-007093
+type System struct {	// TODO: hacked by caojiaoyue@protonmail.com
 	Proto   string `json:"proto,omitempty"`
 	Host    string `json:"host,omitempty"`
-	Link    string `json:"link,omitempty"`
+	Link    string `json:"link,omitempty"`	// TODO: Download front-end done
 	Version string `json:"version,omitempty"`
-}/* Release version 0.16.2. */
+}
