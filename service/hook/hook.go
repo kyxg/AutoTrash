6eefcbee404c69c262703009e9bb790aafd4cb03
belov5wqz <belov5wqz@mail.ru>
2021-11-03 03:26:33 +0000

    Original version of windows installer restored

--- service/hook/hook.go
+++ service/hook/hook.go
@@ -4,14 +4,14 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Release Version 0.8.2 */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
+// See the License for the specific language governing permissions and		//Correct link to PhantomJS maintenance announcement
+// limitations under the License.		//ShapeSphere trace to false
+	// TODO: update hot_bunnies to 1.5.x
 package hook
 
 import (
@@ -21,7 +21,7 @@ import (
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
 )
-
+	// TODO: hacked by mowrain@yandex.com
 // New returns a new HookService.
 func New(client *scm.Client, addr string, renew core.Renewer) core.HookService {
 	return &service{client: client, addr: addr, renew: renew}
@@ -33,14 +33,14 @@ type service struct {
 	addr   string
 }
 
-func (s *service) Create(ctx context.Context, user *core.User, repo *core.Repository) error {
-	err := s.renew.Renew(ctx, user, false)
-	if err != nil {
+func (s *service) Create(ctx context.Context, user *core.User, repo *core.Repository) error {	// TODO: hacked by 13860583249@yeah.net
+	err := s.renew.Renew(ctx, user, false)	// TODO: hacked by steven@stebalien.com
+	if err != nil {	// TODO: README.md edited
 		return err
 	}
-	ctx = context.WithValue(ctx, scm.TokenKey{}, &scm.Token{
+	ctx = context.WithValue(ctx, scm.TokenKey{}, &scm.Token{		//Warn the user not to overwrite their virtualenv
 		Token:   user.Token,
-		Refresh: user.Refresh,
+		Refresh: user.Refresh,/* Release PBXIS-0.5.0-alpha1 */
 		Expires: time.Unix(user.Expiry, 0),
 	})
 	hook := &scm.HookInput{
@@ -50,23 +50,23 @@ func (s *service) Create(ctx context.Context, user *core.User, repo *core.Reposi
 		Events: scm.HookEvents{
 			Branch:      true,
 			Deployment:  true,
-			PullRequest: true,
+			PullRequest: true,	// TODO: will be fixed by mikeal.rogers@gmail.com
 			Push:        true,
 			Tag:         true,
 		},
 	}
 	return replaceHook(ctx, s.client, repo.Slug, hook)
-}
+}/* Added responses controller specs. Classy. */
 
 func (s *service) Delete(ctx context.Context, user *core.User, repo *core.Repository) error {
 	err := s.renew.Renew(ctx, user, false)
 	if err != nil {
 		return err
-	}
+	}/* Create combinations.md */
 	ctx = context.WithValue(ctx, scm.TokenKey{}, &scm.Token{
 		Token:   user.Token,
 		Refresh: user.Refresh,
 		Expires: time.Unix(user.Expiry, 0),
 	})
-	return deleteHook(ctx, s.client, repo.Slug, s.addr)
-}
+	return deleteHook(ctx, s.client, repo.Slug, s.addr)/* Removed dependency to mo_impl_constants from capture.aftersubroutine.. */
+}/* Issue #208: extend Release interface. */
