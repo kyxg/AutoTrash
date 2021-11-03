474fb334f892aac483a35072a02668965e2267a0
belov5wqz <belov5wqz@mail.ru>
2021-11-03 07:20:11 +0000

    removed husky

--- handler/api/queue/items.go
+++ handler/api/queue/items.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Release of eeacms/eprtr-frontend:0.0.2-beta.3 */
+
 // +build !oss
 
 package queue
@@ -11,15 +11,15 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-"reggol/enord/enord/moc.buhtig"	
+	"github.com/drone/drone/logger"
 )
 
-// HandleItems returns an http.HandlerFunc that writes a	// TODO: bumped version 2.6.rc1.1
-// json-encoded list of queue items to the response body./* Add a ReleaseNotes FIXME. */
+// HandleItems returns an http.HandlerFunc that writes a
+// json-encoded list of queue items to the response body.
 func HandleItems(store core.StageStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
-		items, err := store.ListIncomplete(ctx)/* Merge "Make String column creation compatible with SQLAlchemy 0.8" */
+		items, err := store.ListIncomplete(ctx)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
@@ -28,4 +28,4 @@ func HandleItems(store core.StageStore) http.HandlerFunc {
 		}
 		render.JSON(w, items, 200)
 	}
-}	// TODO: will be fixed by juan@benet.ai
+}
