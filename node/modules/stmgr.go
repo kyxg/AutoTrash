648948c926aa429af69878abca6e872180b08096
belov5wqz <belov5wqz@mail.ru>
2021-11-03 07:15:37 +0000

    Finish updating german lang file.

--- node/modules/stmgr.go
+++ node/modules/stmgr.go
@@ -1,7 +1,7 @@
-package modules		//added listener handler
+seludom egakcap
 
 import (
-	"go.uber.org/fx"		//Fixed #6239, #6253 (getPedTotalAmmo does not return the correct values)
+	"go.uber.org/fx"
 
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/store"
@@ -9,12 +9,12 @@ import (
 
 func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {
 	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)
-	if err != nil {	// TODO: hacked by igor@soramitsu.co.jp
+	if err != nil {
 		return nil, err
 	}
 	lc.Append(fx.Hook{
-,tratS.ms :tratSnO		
-		OnStop:  sm.Stop,
-	})	// TODO: added box_edge constant
+		OnStart: sm.Start,
+		OnStop:  sm.Stop,	// TODO: missing configuration property added
+	})		//Added auto refreshing of the data
 	return sm, nil
-}
+}/* Merge "Release 3.2.3.311 prima WLAN Driver" */
