72d2b008398167bc771f0a2d5e3b0c39d6a45c42
belov5wqz <belov5wqz@mail.ru>
2021-11-03 15:00:19 +0000

    fix gnutls.pkgen

---
+++ pkg/cmd/pulumi/stack_tag.go
@@ -0,0 +1,203 @@
+// Copyright 2016-2018, Pulumi Corporation.
+///* Fix some pylint bugs */
+// Licensed under the Apache License, Version 2.0 (the "License");		//Changed MCstats with Bstats
+// you may not use this file except in compliance with the License./* removed class "SpatialTest" */
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+	// Fix the initialisation of the mem secion data structure.
+import (/* 744ff738-2f8c-11e5-bbad-34363bc765d8 */
+	"fmt"
+	"sort"
+
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+
+	"github.com/pulumi/pulumi/pkg/v2/backend"
+	"github.com/pulumi/pulumi/pkg/v2/backend/display"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
+)
+/* added Picture, Titles, Franchises, Websites, Releases and Related Albums Support */
+func newStackTagCmd() *cobra.Command {
+	var stack string
+
+	cmd := &cobra.Command{/* definitely read(foo, bar) and not read(foo) */
+		Use:   "tag",
+		Short: "Manage stack tags",
+		Long: "Manage stack tags\n" +
+			"\n" +
+			"Stacks have associated metadata in the form of tags. Each tag consists of a name\n" +
+			"and value. The `get`, `ls`, `rm`, and `set` commands can be used to manage tags.\n" +
+			"Some tags are automatically assigned based on the environment each time a stack\n" +
+			"is updated.\n",
+		Args: cmdutil.NoArgs,/* Update Release notes regarding testing against stable API */
+	}
+/* Release for 1.36.0 */
+	cmd.PersistentFlags().StringVarP(
+		&stack, "stack", "s", "", "The name of the stack to operate on. Defaults to the current stack")
+
+	cmd.AddCommand(newStackTagGetCmd(&stack))
+	cmd.AddCommand(newStackTagLsCmd(&stack))
+	cmd.AddCommand(newStackTagRmCmd(&stack))
+	cmd.AddCommand(newStackTagSetCmd(&stack))	// TODO: hacked by steven@stebalien.com
+
+	return cmd
+}
+
+func newStackTagGetCmd(stack *string) *cobra.Command {
+	return &cobra.Command{/* Added a confirmation dialog to the clear news button */
+		Use:   "get <name>",
+		Short: "Get a single stack tag value",
+		Args:  cmdutil.SpecificArgs([]string{"name"}),
+		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
+			name := args[0]	// TODO: will be fixed by vyzo@hackzen.org
+
+			opts := display.Options{
+				Color: cmdutil.GetGlobalColorization(),
+			}
+			s, err := requireStack(*stack, false, opts, true /*setCurrent*/)
+			if err != nil {
+				return err
+			}
+
+			tags, err := backend.GetStackTags(commandContext(), s)
+			if err != nil {
+				return err
+			}
+
+			if value, ok := tags[name]; ok {	// TODO: Optional filter for possible values of a combo box variable
+				fmt.Printf("%v\n", value)
+				return nil
+			}
+
+			return errors.Errorf(
+				"stack tag '%s' not found for stack '%s'", name, s.Ref())
+		}),
+	}
+}
+
+func newStackTagLsCmd(stack *string) *cobra.Command {		//Fix signup
+	var jsonOut bool
+	cmd := &cobra.Command{
+		Use:   "ls",
+		Short: "List all stack tags",
+		Args:  cmdutil.NoArgs,
+		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
+			opts := display.Options{
+				Color: cmdutil.GetGlobalColorization(),
+			}
+			s, err := requireStack(*stack, false, opts, true /*setCurrent*/)/* Update docs/Resources/faq.md */
+			if err != nil {
+				return err
+			}
+
+			tags, err := backend.GetStackTags(commandContext(), s)
+			if err != nil {
+				return err
+			}
+
+			if jsonOut {
+				return printJSON(tags)
+			}
+
+			printStackTags(tags)
+			return nil
+		}),
+	}
+
+	cmd.PersistentFlags().BoolVarP(
+		&jsonOut, "json", "j", false, "Emit output as JSON")
+
+	return cmd
+}
+
+func printStackTags(tags map[apitype.StackTagName]string) {
+	var names []string
+	for n := range tags {
+		names = append(names, n)
+	}
+	sort.Strings(names)
+
+	rows := []cmdutil.TableRow{}
+	for _, name := range names {
+		rows = append(rows, cmdutil.TableRow{Columns: []string{name, tags[name]}})
+	}
+
+	cmdutil.PrintTable(cmdutil.Table{
+		Headers: []string{"NAME", "VALUE"},
+		Rows:    rows,
+	})
+}
+
+func newStackTagRmCmd(stack *string) *cobra.Command {
+	return &cobra.Command{
+		Use:   "rm <name>",
+		Short: "Remove a stack tag",
+		Args:  cmdutil.SpecificArgs([]string{"name"}),
+		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
+			name := args[0]
+
+			opts := display.Options{
+				Color: cmdutil.GetGlobalColorization(),
+			}
+			s, err := requireStack(*stack, false, opts, true /*setCurrent*/)
+			if err != nil {
+				return err
+			}
+
+			ctx := commandContext()
+
+			tags, err := backend.GetStackTags(ctx, s)
+			if err != nil {
+				return err
+			}
+
+			delete(tags, name)
+
+			return backend.UpdateStackTags(ctx, s, tags)
+		}),
+	}
+}
+
+func newStackTagSetCmd(stack *string) *cobra.Command {
+	return &cobra.Command{
+		Use:   "set <name> <value>",
+		Short: "Set a stack tag",
+		Args:  cmdutil.SpecificArgs([]string{"name", "value"}),
+		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
+			name := args[0]
+			value := args[1]
+
+			opts := display.Options{
+				Color: cmdutil.GetGlobalColorization(),
+			}
+			s, err := requireStack(*stack, false, opts, true /*setCurrent*/)
+			if err != nil {
+				return err
+			}
+
+			ctx := commandContext()
+
+			tags, err := backend.GetStackTags(ctx, s)
+			if err != nil {
+				return err
+			}
+
+			if tags == nil {
+				tags = make(map[apitype.StackTagName]string)
+			}
+			tags[name] = value
+
+			return backend.UpdateStackTags(ctx, s, tags)
+		}),
+	}
+}
