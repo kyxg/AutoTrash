e8d47e77b26ab22ac75bd0a17fade849ca9ee81a
belov5wqz <belov5wqz@mail.ru>
2021-11-03 17:36:31 +0000

    Add HPC Fellow job

--- handler/api/repos/builds/deploys/list_test.go
+++ handler/api/repos/builds/deploys/list_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by timnugent@gmail.com
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License		//Create ryourmsg.lua
 // that can be found in the LICENSE file.
-	// TODO: will be fixed by seth@sethvargo.com
+
 package deploys
