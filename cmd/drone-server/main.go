2ac5807de137db1e5f1d48f58a679ce743828693
belov5wqz <belov5wqz@mail.ru>
2021-11-03 12:04:40 +0000

    Show API version to admins.

--- cmd/drone-server/main.go
+++ cmd/drone-server/main.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License");/* [maven-release-plugin]  copy for tag techytax-parent-2.3.1 */
+// you may not use this file except in compliance with the License.	// doc(readme): add travis build status image
+// You may obtain a copy of the License at		//Updated build of tomcat to 7.0.28
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: will be fixed by why@ipfs.io
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//-Added the recent fix for the SQL script dialog bug to the release notes.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-package main
+/* [artifactory-release] Release version 1.7.0.RC1 */
+package main/* organise the repo structure and implement multiple path capacity */
 
 import (
 	"context"
@@ -30,7 +30,7 @@ import (
 	"github.com/drone/signal"
 
 	"github.com/joho/godotenv"
-	"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"/* Released 2.6.0 */
 	"golang.org/x/sync/errgroup"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -42,15 +42,15 @@ func main() {
 	var envfile string
 	flag.StringVar(&envfile, "env-file", ".env", "Read in a file of environment variables")
 	flag.Parse()
-
+/* Added Entity and BaseMob classes without any behavior yet */
 	godotenv.Load(envfile)
-	config, err := config.Environ()
+	config, err := config.Environ()/* ZO-42 First prototype. */
 	if err != nil {
 		logger := logrus.WithError(err)
 		logger.Fatalln("main: invalid configuration")
 	}
-
-	initLogging(config)
+		//- Fixed an insufficient allocation, probably causing OS X crashes
+	initLogging(config)	// use omap over beagleboard for host name
 	ctx := signal.WithContext(
 		context.Background(),
 	)
@@ -58,7 +58,7 @@ func main() {
 	// if trace level logging is enabled, output the
 	// configuration parameters.
 	if logrus.IsLevelEnabled(logrus.TraceLevel) {
-		fmt.Println(config.String())
+		fmt.Println(config.String())/* Modificación de README.md */
 	}
 
 	app, err := InitializeApplication(config)
@@ -71,15 +71,15 @@ func main() {
 	// machine users configured in the environment.
 	err = bootstrap.New(app.users).Bootstrap(ctx, &core.User{
 		Login:   config.Users.Create.Username,
-		Machine: config.Users.Create.Machine,
+		Machine: config.Users.Create.Machine,	// TODO: delete unknowed font
 		Admin:   config.Users.Create.Admin,
-		Hash:    config.Users.Create.Token,
+		Hash:    config.Users.Create.Token,	// + Added <l>ink and <u>nlink commands
 	})
 	if err != nil {
 		logger := logrus.WithError(err)
 		logger.Fatalln("cannot bootstrap user account")
 	}
-
+/* Incorrect manual install doc */
 	g := errgroup.Group{}
 	g.Go(func() error {
 		logrus.WithFields(
