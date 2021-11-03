5e507198bdce6722df760c358e070d928eadca89
belov5wqz <belov5wqz@mail.ru>
2021-11-03 07:29:40 +0000

    Update boto3 from 1.7.27 to 1.7.28

---
+++ tests/integration/aliases/dotnet/rename_component/step1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved./* Update travis build for XUnit 2 */
+/* Batch Script for new Release */
+using System.Threading.Tasks;
+using Pulumi;
+/* Merge "msm: mdss: force HW reprogram when ROI changes mixer layout" */
+class Resource : ComponentResource
+{
+    public Resource(string name, ComponentResourceOptions options = null)
+        : base("my:module:Resource", name, options)/* Create FacturaReleaseNotes.md */
+    {
+    }/* [MERGE] move menu 'Automated Actions' to Administration/Customization */
+}
+
+// Scenario #3 - rename a component (and all it's children)
+class ComponentThree : ComponentResource
+{
+    private Resource resource1;
+    private Resource resource2;
+
+    public ComponentThree(string name, ComponentResourceOptions options = null)		//move `import msgpack` into function
+        : base("my:module:ComponentThree", name, options)
+    {
+        // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit	// Manua production using ZIP
+        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.	// TODO: will be fixed by alex.gaynor@gmail.com
+        this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });/* Release of eeacms/www:18.5.8 */
+        this.resource2 = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
+    }
+}
+
+class Program
+{
+    static Task<int> Main(string[] args)
+{    
+        return Deployment.RunAsync(() => 
+        {
+            var comp3 = new ComponentThree("comp3");
+        });/* Adding politicstext.properties for Napoleonic Empires. (veqryn) */
+    }
+}
