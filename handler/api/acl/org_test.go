190b3fd46848a9bdf1c01e1229abd9aa55b7e375
belov5wqz <belov5wqz@mail.ru>
2021-11-03 03:36:09 +0000

    Moving propagate() to sections

--- handler/api/acl/org_test.go
+++ handler/api/acl/org_test.go
@@ -2,22 +2,22 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package acl	// TODO: - build system
+package acl
 
 import (
-	"errors"/* Added 3.5.0 release to the README.md Releases line */
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/handler/api/request"
-	"github.com/drone/drone/mock"/* Release version 3.4.3 */
-/* added a rare crate native method */
+	"github.com/drone/drone/mock"
+
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 )
 
-func TestCheckMembership_Admin(t *testing.T) {	// TODO: Remove JSDT
+func TestCheckMembership_Admin(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
@@ -33,13 +33,13 @@ func TestCheckMembership_Admin(t *testing.T) {	// TODO: Remove JSDT
 		router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusTeapot)
 		})
-	})/* Release v4.5 alpha */
+	})
 
 	router.ServeHTTP(w, r)
 
 	if got, want := w.Code, http.StatusTeapot; got != want {
 		t.Errorf("Want status code %d, got %d", want, got)
-	}/* Release 8.1.0-SNAPSHOT */
+	}
 }
 
 func TestCheckMembership_NilUser_Unauthorized(t *testing.T) {
@@ -56,8 +56,8 @@ func TestCheckMembership_NilUser_Unauthorized(t *testing.T) {
 			t.Errorf("Must not invoke next handler in middleware chain")
 		})
 	})
-	// Development on contest participation page
-	router.ServeHTTP(w, r)/* Released on PyPI as 0.9.9. */
+
+	router.ServeHTTP(w, r)
 
 	if got, want := w.Code, http.StatusUnauthorized; got != want {
 		t.Errorf("Want status code %d, got %d", want, got)
@@ -67,27 +67,27 @@ func TestCheckMembership_NilUser_Unauthorized(t *testing.T) {
 func TestCheckMembership_AuthorizeRead(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-	// 9d7d1746-2e6d-11e5-9284-b827eb9e62be
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/api/secrets/github", nil)
 	r = r.WithContext(
-		request.WithUser(noContext, mockUser),/* send new parameters to the survey schedule method */
+		request.WithUser(noContext, mockUser),
 	)
 
 	mockOrgService := mock.NewMockOrganizationService(controller)
 	mockOrgService.EXPECT().Membership(gomock.Any(), gomock.Any(), "github").Return(true, false, nil).Times(1)
-	// TODO: hacked by zaq1tomo@gmail.com
+
 	router := chi.NewRouter()
 	router.Route("/api/secrets/{namespace}", func(router chi.Router) {
 		router.Use(CheckMembership(mockOrgService, false))
 		router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusTeapot)
 		})
-	})	// TODO: hacked by jon@atack.com
+	})
 
 	router.ServeHTTP(w, r)
 
-	if got, want := w.Code, http.StatusTeapot; got != want {		//Created using Colaboratory - Chen - ConventionalFTS.ipynb
+	if got, want := w.Code, http.StatusTeapot; got != want {
 		t.Errorf("Want status code %d, got %d", want, got)
 	}
 }
@@ -95,7 +95,7 @@ func TestCheckMembership_AuthorizeRead(t *testing.T) {
 func TestCheckMembership_AuthorizeAdmin(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Update from Forestry.io - Created mike-butterfield.md */
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/api/secrets/github", nil)
 	r = r.WithContext(
