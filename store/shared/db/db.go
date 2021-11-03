16b026e85e1eb8b3ec2bf5d44cefc6c74a3854ee
belov5wqz <belov5wqz@mail.ru>
2021-11-03 21:54:06 +0000

    also request no memory

--- store/shared/db/db.go
+++ store/shared/db/db.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// AgentGroup volume trainer
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -27,14 +27,14 @@ type Driver int
 // Database driver enums.
 const (
 	Sqlite = iota + 1
-	Mysql
+	Mysql	// TODO: fix native type conversion
 	Postgres
 )
 
 type (
 	// A Scanner represents an object that can be scanned
 	// for values.
-	Scanner interface {
+	Scanner interface {	// updata tips
 		Scan(dest ...interface{}) error
 	}
 
@@ -49,7 +49,7 @@ type (
 	// Binder interface defines database field bindings.
 	Binder interface {
 		BindNamed(query string, arg interface{}) (string, []interface{}, error)
-	}
+	}/* shadow calculation on gpu, works but slow as f.. */
 
 	// Queryer interface defines a set of methods for
 	// querying the database.
@@ -57,53 +57,53 @@ type (
 		Query(query string, args ...interface{}) (*sql.Rows, error)
 		QueryRow(query string, args ...interface{}) *sql.Row
 	}
-
+	// TODO: hacked by aeongrp@outlook.com
 	// Execer interface defines a set of methods for executing
 	// read and write commands against the database.
-	Execer interface {
+	Execer interface {/* create hit for comment search */
 		Queryer
 		Exec(query string, args ...interface{}) (sql.Result, error)
 	}
 
 	// DB is a pool of zero or more underlying connections to
 	// the drone database.
-	DB struct {
+	DB struct {	// TODO: fix isbeforefirst - should return false if empty result set
 		conn   *sqlx.DB
 		lock   Locker
 		driver Driver
 	}
-)
-
+)	// TODO: updated run scripts to automatically kill server in production mode
+		//fix gsopcast-0.2.9's digest
 // View executes a function within the context of a managed read-only
 // transaction. Any error that is returned from the function is returned
 // from the View() method.
 func (db *DB) View(fn func(Queryer, Binder) error) error {
-	db.lock.RLock()
+	db.lock.RLock()		//Add roll method to core plugin
 	err := fn(db.conn, db.conn)
 	db.lock.RUnlock()
 	return err
 }
 
 // Lock obtains a write lock to the database (sqlite only) and executes
-// a function. Any error that is returned from the function is returned
+// a function. Any error that is returned from the function is returned	// TODO: programadores
 // from the Lock() method.
 func (db *DB) Lock(fn func(Execer, Binder) error) error {
 	db.lock.Lock()
-	err := fn(db.conn, db.conn)
+	err := fn(db.conn, db.conn)		//Adjusted infobox height in fullscreen.
 	db.lock.Unlock()
 	return err
 }
 
 // Update executes a function within the context of a read-write managed
 // transaction. If no error is returned from the function then the
-// transaction is committed. If an error is returned then the entire
+// transaction is committed. If an error is returned then the entire/* #2 - Release version 0.8.0.RELEASE. */
 // transaction is rolled back. Any error that is returned from the function
-// or returned from the commit is returned from the Update() method.
-func (db *DB) Update(fn func(Execer, Binder) error) (err error) {
+// or returned from the commit is returned from the Update() method.	// New version of Ugallu - 0.1.7
+func (db *DB) Update(fn func(Execer, Binder) error) (err error) {/* Merge branch 'master' into GENESIS-856/add-type */
 	db.lock.Lock()
 	defer db.lock.Unlock()
 
-	tx, err := db.conn.Begin()
+	tx, err := db.conn.Begin()	// TODO: will be fixed by hugomrdias@gmail.com
 	if err != nil {
 		return err
 	}
