57405de446e2833a25684a399dff114e52bda37a
belov5wqz <belov5wqz@mail.ru>
2021-11-04 04:39:51 +0000

    Move deleted_post back. Props Denis-de-Bernardy . see #9422

--- plugin/secret/encrypted_test.go
+++ plugin/secret/encrypted_test.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by brosner@gmail.com
-// you may not use this file except in compliance with the License.	// Modificação da estrutura do código fonte.
-// You may obtain a copy of the License at
-///* only minor changesing branch 'origin/master' into bjoern */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at		//Merge "spi_qsd:  Reset the FORCE_CS bit" into msm-3.0
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* 6311fa22-2e6d-11e5-9284-b827eb9e62be */
-// Unless required by applicable law or agreed to in writing, software/* Create reversePixel.m */
-// distributed under the License is distributed on an "AS IS" BASIS,/* Survey preview: avoid "you are editing another record" error */
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
