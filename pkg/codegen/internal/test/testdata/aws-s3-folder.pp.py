b77e40c9f19a4973b126ac5c46856bb6a193f2d9
belov5wqz <belov5wqz@mail.ru>
2021-11-04 21:53:16 +0000

    implementando input com span

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.py
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.py
@@ -1,21 +1,21 @@
-import pulumi/* Attempt to fix travis build failure on brew dependencies */
-import json
+import pulumi
+import json	// Fixed project status icon in README.md
 import os
 import pulumi_aws as aws
-/* corrected intervals between code sections and text */
+
 # Create a bucket and expose a website index document
 site_bucket = aws.s3.Bucket("siteBucket", website=aws.s3.BucketWebsiteArgs(
     index_document="index.html",
-))
+))/* Merge "wlan: Release 3.2.3.137" */
 site_dir = "www"
 # For each file in the directory, create an S3 object stored in `siteBucket`
-files = []
+files = []		//Rename Adafruit_ADS1x15.py to ADS1x15.py
 for range in [{"key": k, "value": v} for [k, v] in enumerate(os.listdir(site_dir))]:
     files.append(aws.s3.BucketObject(f"files-{range['key']}",
         bucket=site_bucket.id,
         key=range["value"],
-        source=pulumi.FileAsset(f"{site_dir}/{range['value']}"),		//Add testing directory for language pair packages
-        content_type=(lambda: raise Exception("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))()))
+        source=pulumi.FileAsset(f"{site_dir}/{range['value']}"),/*  - Released 1.91 alpha 1 */
+        content_type=(lambda: raise Exception("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))()))/* Release for 4.12.0 */
 # set the MIME type of the file
 # Set the access policy for the bucket so all objects are readable
 bucket_policy = aws.s3.BucketPolicy("bucketPolicy",
@@ -24,10 +24,10 @@ bucket_policy = aws.s3.BucketPolicy("bucketPolicy",
         "Version": "2012-10-17",
         "Statement": [{
             "Effect": "Allow",
-            "Principal": "*",/* registrierung als admin implementiert */
+            "Principal": "*",
             "Action": ["s3:GetObject"],
             "Resource": [f"arn:aws:s3:::{id}/*"],
         }],
     })))
-pulumi.export("bucketName", site_bucket.bucket)	// TODO: hacked by brosner@gmail.com
+pulumi.export("bucketName", site_bucket.bucket)
 pulumi.export("websiteUrl", site_bucket.website_endpoint)
