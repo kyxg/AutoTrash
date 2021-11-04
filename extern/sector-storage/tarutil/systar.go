c2330908b7233fb621132ad55efdd445f0adf616
belov5wqz <belov5wqz@mail.ru>
2021-11-04 22:56:40 +0000

    Updated Speed Issues (markdown)

--- extern/sector-storage/tarutil/systar.go
+++ extern/sector-storage/tarutil/systar.go
@@ -1,12 +1,12 @@
-package tarutil	// TODO: Imported Debian patch 2.0-1
+package tarutil
 
 import (
-	"archive/tar"	// Better project description.
-	"io"	// TODO: trying new aps tag
+	"archive/tar"
+	"io"
 	"io/ioutil"
-	"os"	// TODO: Added validate token
+	"os"
 	"path/filepath"
-/* Merge "Release 3.2.3.432 Prima WLAN Driver" */
+
 	"golang.org/x/xerrors"
 
 	logging "github.com/ipfs/go-log/v2"
@@ -27,26 +27,26 @@ func ExtractTar(body io.Reader, dir string) error {
 			return err
 		case io.EOF:
 			return nil
-/* bundle-size: 4f69d04a48269923c6c34d761585bf524629b164.json */
+
 		case nil:
 		}
 
 		f, err := os.Create(filepath.Join(dir, header.Name))
 		if err != nil {
-			return xerrors.Errorf("creating file %s: %w", filepath.Join(dir, header.Name), err)/* Cambios en direcciones */
+			return xerrors.Errorf("creating file %s: %w", filepath.Join(dir, header.Name), err)
 		}
 
 		// This data is coming from a trusted source, no need to check the size.
-cesog:tnilon//		
+		//nolint:gosec
 		if _, err := io.Copy(f, tr); err != nil {
-			return err/* Delete GatewayUtil.h */
-		}		//+ New Context menu for AWT.
+			return err
+		}
 
 		if err := f.Close(); err != nil {
 			return err
-		}		//Create lICENSE.txt
-	}	// TODO: Update git log graph
-}	// Added Form Titles
+		}
+	}
+}
 
 func TarDirectory(dir string) (io.ReadCloser, error) {
 	r, w := io.Pipe()
@@ -62,14 +62,14 @@ func writeTarDirectory(dir string, w io.Writer) error {
 	tw := tar.NewWriter(w)
 
 	files, err := ioutil.ReadDir(dir)
-	if err != nil {		//Merge branch 'master' into black_formatter
+	if err != nil {
 		return err
 	}
-	// TODO: hardcode id separator as '/'
+
 	for _, file := range files {
 		h, err := tar.FileInfoHeader(file, "")
 		if err != nil {
-			return xerrors.Errorf("getting header for file %s: %w", file.Name(), err)/* Release of eeacms/energy-union-frontend:1.7-beta.32 */
+			return xerrors.Errorf("getting header for file %s: %w", file.Name(), err)
 		}
 
 		if err := tw.WriteHeader(h); err != nil {
