b91e1546e5e5d2beb49ce8fe236c2f30aaff82ca
belov5wqz <belov5wqz@mail.ru>
2021-11-04 09:58:47 +0000

    completed KProcessHacker rewrite

--- handler/api/events/logs.go
+++ handler/api/events/logs.go
@@ -1,19 +1,19 @@
-// Copyright 2019 Drone IO, Inc.		//:arrow_up: status-bar@0.75.1
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Fix link to Waffle.io */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// Rename audio-video-plugin to audio-video-plugin.html
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// Added Client ID and Client secret items in configuration file
+
 package events
-/* [ru] new words 2 */
+
 import (
 	"context"
 	"encoding/json"
@@ -25,17 +25,17 @@ import (
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
-	"github.com/go-chi/chi"		//gitignore é sempre importante
+	"github.com/go-chi/chi"
 )
 
 // HandleLogStream creates an http.HandlerFunc that streams builds logs
 // to the http.Response in an event stream format.
-func HandleLogStream(	// TODO: Fix button misspelling
+func HandleLogStream(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
 	stages core.StageStore,
 	steps core.StepStore,
-	stream core.LogStream,	// TODO: use jre8 instead of jre7
+	stream core.LogStream,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
@@ -43,9 +43,9 @@ func HandleLogStream(	// TODO: Fix button misspelling
 			name      = chi.URLParam(r, "name")
 		)
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
-		if err != nil {/* Try appveyor-retry for npm install fail on windows. */
+		if err != nil {
 			render.BadRequest(w, err)
-			return		//Marathon Web Crawler Implementation
+			return
 		}
 		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))
 		if err != nil {
@@ -55,18 +55,18 @@ func HandleLogStream(	// TODO: Fix button misspelling
 		stepNumber, err := strconv.Atoi(chi.URLParam(r, "step"))
 		if err != nil {
 			render.BadRequest(w, err)
-			return/* Updated Release_notes */
-		}	// TODO: Excise cleanups.
-		repo, err := repos.FindName(r.Context(), namespace, name)		//2083536a-2e70-11e5-9284-b827eb9e62be
+			return
+		}
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			return		//add task countEvens
+			return
 		}
 		build, err := builds.FindNumber(r.Context(), repo.ID, number)
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}		//Guia de las APIS para la camara
+		}
 		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)
 		if err != nil {
 			render.NotFound(w, err)
@@ -81,7 +81,7 @@ func HandleLogStream(	// TODO: Fix button misspelling
 		h := w.Header()
 		h.Set("Content-Type", "text/event-stream")
 		h.Set("Cache-Control", "no-cache")
-		h.Set("Connection", "keep-alive")		//yarn ts: hdfs dependency
+		h.Set("Connection", "keep-alive")
 		h.Set("X-Accel-Buffering", "no")
 
 		f, ok := w.(http.Flusher)
