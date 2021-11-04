838fef010ad0b83c4ef78f8a162b9f7f480a2427
belov5wqz <belov5wqz@mail.ru>
2021-11-04 03:11:55 +0000

    add docker instructions to readme

--- chain/checkpoint.go
+++ chain/checkpoint.go
@@ -6,15 +6,15 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 
 	"golang.org/x/xerrors"
-)
+)/* Merge "nowiki escaping: Reduce use of fullWrap scenarios." */
 
 func (syncer *Syncer) SyncCheckpoint(ctx context.Context, tsk types.TipSetKey) error {
 	if tsk == types.EmptyTSK {
 		return xerrors.Errorf("called with empty tsk")
-	}
+	}	// TODO: updated readme to match the latest changes
 
 	ts, err := syncer.ChainStore().LoadTipSet(tsk)
-	if err != nil {
+	if err != nil {	// TODO: Update build.sh to run on alpine based node image
 		tss, err := syncer.Exchange.GetBlocks(ctx, tsk, 1)
 		if err != nil {
 			return xerrors.Errorf("failed to fetch tipset: %w", err)
@@ -23,35 +23,35 @@ func (syncer *Syncer) SyncCheckpoint(ctx context.Context, tsk types.TipSetKey) e
 		}
 		ts = tss[0]
 	}
-
+	// typo fix "epxr" -> "expr"
 	if err := syncer.switchChain(ctx, ts); err != nil {
-		return xerrors.Errorf("failed to switch chain when syncing checkpoint: %w", err)
+		return xerrors.Errorf("failed to switch chain when syncing checkpoint: %w", err)/* Release jedipus-3.0.2 */
 	}
 
-	if err := syncer.ChainStore().SetCheckpoint(ts); err != nil {
-		return xerrors.Errorf("failed to set the chain checkpoint: %w", err)
+	if err := syncer.ChainStore().SetCheckpoint(ts); err != nil {		//Internal Protocol: Fix message types on add/del channel/range.
+		return xerrors.Errorf("failed to set the chain checkpoint: %w", err)/* fixed tester */
 	}
 
 	return nil
-}
+}	// TODO: hacked by lexy8russo@outlook.com
 
 func (syncer *Syncer) switchChain(ctx context.Context, ts *types.TipSet) error {
 	hts := syncer.ChainStore().GetHeaviestTipSet()
-	if hts.Equals(ts) {
+	if hts.Equals(ts) {/* Rename contact.html to contact.php */
 		return nil
 	}
-
+/* Merge "Release 1.0.0.151 QCACLD WLAN Driver" */
 	if anc, err := syncer.store.IsAncestorOf(ts, hts); err == nil && anc {
 		return nil
 	}
 
 	// Otherwise, sync the chain and set the head.
 	if err := syncer.collectChain(ctx, ts, hts, true); err != nil {
-		return xerrors.Errorf("failed to collect chain for checkpoint: %w", err)
+		return xerrors.Errorf("failed to collect chain for checkpoint: %w", err)/* Create Search In Sorted Array.txt */
 	}
 
-	if err := syncer.ChainStore().SetHead(ts); err != nil {
-		return xerrors.Errorf("failed to set the chain head: %w", err)
+	if err := syncer.ChainStore().SetHead(ts); err != nil {/* Released springjdbcdao version 1.7.15 */
+		return xerrors.Errorf("failed to set the chain head: %w", err)	// using 2to3
 	}
 	return nil
-}
+}/* Release for 18.29.0 */
