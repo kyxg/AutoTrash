0617a5e195a513febf1a24a7d999ffd32890cc21
belov5wqz <belov5wqz@mail.ru>
2021-11-04 04:29:48 +0000

    Delete collector

--- core/pubsub.go
+++ core/pubsub.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* updated api controller test */
-// you may not use this file except in compliance with the License.	// TODO: Better organization of src folder
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by zaq1tomo@gmail.com
-///* new should be in ObjectClass not ClassClass */
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,22 +16,22 @@ package core
 
 import "context"
 
-// Message defines a build change./* Release 1.2 of osgiservicebridge */
-type Message struct {	// Update google-api-client to version 0.27.3
-	Repository string/* Merge 4.0-help version of DomUI */
+// Message defines a build change.
+type Message struct {
+	Repository string
 	Visibility string
 	Data       []byte
 }
-		//README: Mention MacOS X 11.4.2.
+
 // Pubsub provides publish subscriber capabilities, distributing
 // messages from multiple publishers to multiple subscribers.
 type Pubsub interface {
 	// Publish publishes the message to all subscribers.
 	Publish(context.Context, *Message) error
 
-	// Subscribe subscribes to the message broker.		//fix(package.json) fix binary path for lamassu-transactions-csv
+	// Subscribe subscribes to the message broker.
 	Subscribe(context.Context) (<-chan *Message, <-chan error)
 
-	// Subscribers returns a count of subscribers.		//Issue #593: update install module from URL help text
+	// Subscribers returns a count of subscribers.
 	Subscribers() int
 }
