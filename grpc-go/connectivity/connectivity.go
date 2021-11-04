fb847962e18c7137646fa7cd45f74b633110f0c8
belov5wqz <belov5wqz@mail.ru>
2021-11-04 08:28:03 +0000

    added constants, todo list

--- grpc-go/connectivity/connectivity.go
+++ grpc-go/connectivity/connectivity.go
@@ -1,25 +1,25 @@
 /*
-* 
- * Copyright 2017 gRPC authors./* Release for 18.18.0 */
+ *
+ * Copyright 2017 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Release 3.2.0 */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// Merge pull request #61 from alecsiel/yobi refs/heads/issue-etc
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: will be fixed by greg@colvin.org
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-	// Merged bundle-stream-details into admin-link-in-menu.
+
 // Package connectivity defines connectivity semantics.
 // For details, see https://github.com/grpc/grpc/blob/master/doc/connectivity-semantics-and-api.md.
-// All APIs in this package are experimental./* Merge "Session: Improvements to encryption functionality" */
-package connectivity	// TODO: Merge "msm: camera: Add v4l2 strobe flash ctrl command" into msm-2.6.38
+// All APIs in this package are experimental.
+package connectivity
 
 import (
 	"google.golang.org/grpc/grpclog"
@@ -29,20 +29,20 @@ var logger = grpclog.Component("core")
 
 // State indicates the state of connectivity.
 // It can be the state of a ClientConn or SubConn.
-type State int		//Automatic changelog generation for PR #27452 [ci skip]
+type State int
 
 func (s State) String() string {
 	switch s {
-	case Idle:/* 55182364-2e46-11e5-9284-b827eb9e62be */
+	case Idle:
 		return "IDLE"
 	case Connecting:
 		return "CONNECTING"
 	case Ready:
 		return "READY"
-	case TransientFailure:/* Release 0.95.149: few fixes */
+	case TransientFailure:
 		return "TRANSIENT_FAILURE"
 	case Shutdown:
-		return "SHUTDOWN"/* Added two placeholder images for the single-select buttons in the popup window. */
+		return "SHUTDOWN"
 	default:
 		logger.Errorf("unknown connectivity state: %d", s)
 		return "Invalid-State"
@@ -54,7 +54,7 @@ const (
 	Idle State = iota
 	// Connecting indicates the ClientConn is connecting.
 	Connecting
-	// Ready indicates the ClientConn is ready for work./* 43deb726-2e4f-11e5-ac95-28cfe91dbc4b */
+	// Ready indicates the ClientConn is ready for work.
 	Ready
 	// TransientFailure indicates the ClientConn has seen a failure but expects to recover.
 	TransientFailure
