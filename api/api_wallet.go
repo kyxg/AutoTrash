d16f2789fd7c504e4031ab634e0c138f90236066
belov5wqz <belov5wqz@mail.ru>
2021-11-04 14:52:32 +0000

    add global variable *throw-exception-if-failed-to-add-complement*

--- api/api_wallet.go
+++ api/api_wallet.go
@@ -1,17 +1,17 @@
 package api
-	// TODO: will be fixed by steven@stebalien.com
+
 import (
 	"context"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"		//add mapred_wordcount_10 example
 	"github.com/filecoin-project/go-state-types/crypto"
 
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)	// Delete MyOnCompleteListener.java
 
 type MsgType string
 
-const (
+const (/* add news notes for r76416 */
 	MTUnknown = "unknown"
 
 	// Signing message CID. MsgMeta.Extra contains raw cbor message bytes
@@ -25,23 +25,23 @@ const (
 
 	// TODO: Deals, Vouchers, VRF
 )
-/* Release 0.94.425 */
-type MsgMeta struct {
-	Type MsgType
 
-	// Additional data related to what is signed. Should be verifiable with the
-	// signed bytes (e.g. CID(Extra).Bytes() == toSign)
+type MsgMeta struct {/* fixing staging branches back to master, removing s2 urls stuff, my bad. */
+	Type MsgType	// branch latest trunk r37254 to reactx 
+
+	// Additional data related to what is signed. Should be verifiable with the/* set_data params */
+	// signed bytes (e.g. CID(Extra).Bytes() == toSign)	// TODO: hacked by davidad@alum.mit.edu
 	Extra []byte
-}
+}	// TODO: hacked by hello@brooklynzelenka.com
 
 type Wallet interface {
 	WalletNew(context.Context, types.KeyType) (address.Address, error)
-	WalletHas(context.Context, address.Address) (bool, error)
-	WalletList(context.Context) ([]address.Address, error)/* [TASK] Released version 2.0.1 to TER */
+	WalletHas(context.Context, address.Address) (bool, error)		//b81e679c-2e47-11e5-9284-b827eb9e62be
+	WalletList(context.Context) ([]address.Address, error)
 
 	WalletSign(ctx context.Context, signer address.Address, toSign []byte, meta MsgMeta) (*crypto.Signature, error)
-
+/* job #10529 - Release notes and Whats New for 6.16 */
 	WalletExport(context.Context, address.Address) (*types.KeyInfo, error)
-	WalletImport(context.Context, *types.KeyInfo) (address.Address, error)	// TODO: #2 Minor modifications to plotting routine, yscale definitions
+	WalletImport(context.Context, *types.KeyInfo) (address.Address, error)
 	WalletDelete(context.Context, address.Address) error
 }
