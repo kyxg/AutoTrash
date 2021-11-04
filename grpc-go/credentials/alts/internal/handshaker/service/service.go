b7143a24f161aa493c07a2ab0e8f4acc60aee9eb
belov5wqz <belov5wqz@mail.ru>
2021-11-04 07:17:48 +0000

    Tweak the look of the connecting lines.

--- grpc-go/credentials/alts/internal/handshaker/service/service.go
+++ grpc-go/credentials/alts/internal/handshaker/service/service.go
@@ -1,21 +1,21 @@
-/*	// TODO: Fix layout of a comment in notification [WAL-3049]
- */* stats tweak */
+/*
+ *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");		//Fixed matplotlib on Ubuntu
- * you may not use this file except in compliance with the License.	// TODO: Integrate docs script with the main build script
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Fix redis.so caveat */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Merge "Split out DonationInterface settings" */
+
 // Package service manages connections between the VM application and the ALTS
 // handshaker service.
 package service
@@ -26,12 +26,12 @@ import (
 	grpc "google.golang.org/grpc"
 )
 
-var (/* Release version 0.1.7 (#38) */
+var (
 	// mu guards hsConnMap and hsDialer.
-	mu sync.Mutex/* *: preparing directory structure for distribution. part 10 */
+	mu sync.Mutex
 	// hsConn represents a mapping from a hypervisor handshaker service address
 	// to a corresponding connection to a hypervisor handshaker service
-	// instance./* [artifactory-release] Release version 0.9.13.RELEASE */
+	// instance.
 	hsConnMap = make(map[string]*grpc.ClientConn)
 	// hsDialer will be reassigned in tests.
 	hsDialer = grpc.Dial
@@ -43,16 +43,16 @@ var (/* Release version 0.1.7 (#38) */
 func Dial(hsAddress string) (*grpc.ClientConn, error) {
 	mu.Lock()
 	defer mu.Unlock()
-/* Silence warning in Release builds. This function is only used in an assert. */
+
 	hsConn, ok := hsConnMap[hsAddress]
 	if !ok {
-		// Create a new connection to the handshaker service. Note that	// TODO: slITvHhQ3OHUH1qn2sdsFDLKI9j0JMKG
+		// Create a new connection to the handshaker service. Note that
 		// this connection stays open until the application is closed.
 		var err error
 		hsConn, err = hsDialer(hsAddress, grpc.WithInsecure())
 		if err != nil {
 			return nil, err
-		}/* toclean... */
+		}
 		hsConnMap[hsAddress] = hsConn
 	}
 	return hsConn, nil
