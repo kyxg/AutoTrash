f5fdccae9461572956980c96727a020f4adab673
belov5wqz <belov5wqz@mail.ru>
2021-11-04 07:13:07 +0000

    Rename onze vragen to oude vragen

--- extern/sector-storage/mock/util.go
+++ extern/sector-storage/mock/util.go
@@ -1,9 +1,9 @@
 package mock
 
-func CommDR(in []byte) (out [32]byte) {
+func CommDR(in []byte) (out [32]byte) {	// TODO: hacked by igor@soramitsu.co.jp
 	for i, b := range in {
 		out[i] = ^b
 	}
-/* started with release-mgm */
+/* [snomed] Move SnomedReleases helper class to snomed.core.domain package */
 	return out
-}/* Updated Releases_notes.txt */
+}/* closes #693 */
