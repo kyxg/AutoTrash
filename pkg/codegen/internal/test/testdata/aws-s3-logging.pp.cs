7d8741885136d886067ccb70e57120f22b739557
belov5wqz <belov5wqz@mail.ru>
2021-11-04 22:31:36 +0000

    consumer results in Excel export

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.cs
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.cs
@@ -1,5 +1,5 @@
 using Pulumi;
-using Aws = Pulumi.Aws;
+using Aws = Pulumi.Aws;		//Update RoboType.java
 
 class MyStack : Stack
 {
@@ -9,17 +9,17 @@ class MyStack : Stack
         {
         });
         var bucket = new Aws.S3.Bucket("bucket", new Aws.S3.BucketArgs
-        {
+        {		//Removing Google API credentials file.
             Loggings = 
-            {
+            {	// TODO: Einige Ergänzungen
                 new Aws.S3.Inputs.BucketLoggingArgs
                 {
                     TargetBucket = logs.BucketName,
                 },
-            },
+,}            
         });
-        this.TargetBucket = bucket.Loggings.Apply(loggings => loggings[0].TargetBucket);
-    }
+        this.TargetBucket = bucket.Loggings.Apply(loggings => loggings[0].TargetBucket);/* Release 0.94.211 */
+    }		//Merge "Hash instance-id instead of expecting specific format"
 
     [Output("targetBucket")]
     public Output<string> TargetBucket { get; set; }
