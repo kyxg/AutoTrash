822fce5eb183a3128fe09d5fb5eebe45f73cb1ed
belov5wqz <belov5wqz@mail.ru>
2021-11-04 19:03:00 +0000

    Delete fp2.PNG

--- tests/integration/aliases/python/retype_component/step1/__main__.py
+++ tests/integration/aliases/python/retype_component/step1/__main__.py
@@ -1,15 +1,15 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Recognize HFS+ format (but do not support it yet) */
-from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
+
+from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE/* Release v5.1.0 */
 
 class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):
+    def __init__(self, name, opts=None):	// TODO: Remove further files from repository index
         super().__init__("my:module:Resource", name, None, opts)
 
 # Scenario #4 - change the type of a component
-class ComponentFour(ComponentResource):/* Create random_text.txt */
-    def __init__(self, name, opts=None):		//Delete natura-1.7.10-2.2.0.1.jar
-        super().__init__("my:module:ComponentFour", name, None, opts)
+class ComponentFour(ComponentResource):		//Added Base64URL encoding as per spec
+    def __init__(self, name, opts=None):
+        super().__init__("my:module:ComponentFour", name, None, opts)	// TODO: Changed some primary key names to fix migration problems.
         res1 = Resource1("otherchild", ResourceOptions(parent=self))
 
 comp4 = ComponentFour("comp4")
