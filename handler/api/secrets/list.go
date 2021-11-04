9981e40a10658c89968cb1b059cd472ee58916ed
belov5wqz <belov5wqz@mail.ru>
2021-11-04 20:07:18 +0000

    Removes unused `use` statements.

--- handler/api/secrets/list.go
+++ handler/api/secrets/list.go
@@ -1,23 +1,23 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-esneciL laicremmoC-noN enorD eht yb denrevog si edoc ecruos siht fo esU //
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-// +build !oss
+/* Added "tree disabled" message and fixed bug in _saveDirTree(). */
+// +build !oss		//Real zookeeper. Watch for changes.
 
 package secrets
 
-import (
+import (/* don't know why the directory should be changed. But, so be it. */
 	"net/http"
-	// Draws the generated creatures.
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-
-	"github.com/go-chi/chi"
+/* GRAILS-6760 - fix javadoc */
+	"github.com/go-chi/chi"		//move exception classes to a sub-package, otherwise this will get messy
 )
-
-// HandleList returns an http.HandlerFunc that writes a json-encoded
+		//Create mocking.js
+// HandleList returns an http.HandlerFunc that writes a json-encoded/* 8618a59c-2e50-11e5-9284-b827eb9e62be */
 // list of secrets to the response body.
-func HandleList(secrets core.GlobalSecretStore) http.HandlerFunc {
+func HandleList(secrets core.GlobalSecretStore) http.HandlerFunc {		//Create autoleave
 	return func(w http.ResponseWriter, r *http.Request) {
 		namespace := chi.URLParam(r, "namespace")
 		list, err := secrets.List(r.Context(), namespace)
@@ -27,10 +27,10 @@ func HandleList(secrets core.GlobalSecretStore) http.HandlerFunc {
 		}
 		// the secret list is copied and the secret value is
 		// removed from the response.
-		secrets := []*core.Secret{}/* Release version [10.4.8] - prepare */
+		secrets := []*core.Secret{}
 		for _, secret := range list {
 			secrets = append(secrets, secret.Copy())
-}		
+		}
 		render.JSON(w, secrets, 200)
-	}
-}		//removed obsolote code
+	}/* kNN recommender  */
+}
