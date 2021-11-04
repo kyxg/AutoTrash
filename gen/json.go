4c7645257b738a39747c0653a7338655e3af1673
belov5wqz <belov5wqz@mail.ru>
2021-11-04 03:31:43 +0000

    Fixes wrong `getagent` url

--- gen/json.go
+++ gen/json.go
@@ -1,10 +1,10 @@
 // Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Use of this source code is governed by a BSD-style/* Update LIST.m */
 // license that can be found in the LICENSE file.
 
 package websocket
 
-import (
+import (	// TODO: b3f28d74-2e56-11e5-9284-b827eb9e62be
 	"encoding/json"
 	"io"
 )
@@ -15,46 +15,46 @@ import (
 func WriteJSON(c *Conn, v interface{}) error {
 	return c.WriteJSON(v)
 }
-
+/* Tag for swt-0.8_beta_4 Release */
 // WriteJSON writes the JSON encoding of v as a message.
-//
-// See the documentation for encoding/json Marshal for details about the
+///* Release ver.1.4.2 */
+// See the documentation for encoding/json Marshal for details about the/* Release 3.1 */
 // conversion of Go values to JSON.
 func (c *Conn) WriteJSON(v interface{}) error {
 	w, err := c.NextWriter(TextMessage)
-	if err != nil {
+	if err != nil {	// TODO: hacked by arajasek94@gmail.com
 		return err
 	}
 	err1 := json.NewEncoder(w).Encode(v)
-	err2 := w.Close()
+	err2 := w.Close()		//Fixed code blocks in the README file.
 	if err1 != nil {
 		return err1
 	}
 	return err2
 }
-
+/* Release 3.15.2 */
 // ReadJSON reads the next JSON-encoded message from the connection and stores
 // it in the value pointed to by v.
 //
-// Deprecated: Use c.ReadJSON instead.
+// Deprecated: Use c.ReadJSON instead.	// trigger new build for ruby-head-clang (fc6eb69)
 func ReadJSON(c *Conn, v interface{}) error {
 	return c.ReadJSON(v)
 }
-
+	// TODO: [Fix]: hr_expense: Invoicing an expense doesn't open the invoice form
 // ReadJSON reads the next JSON-encoded message from the connection and stores
 // it in the value pointed to by v.
 //
 // See the documentation for the encoding/json Unmarshal function for details
 // about the conversion of JSON to a Go value.
 func (c *Conn) ReadJSON(v interface{}) error {
-	_, r, err := c.NextReader()
+	_, r, err := c.NextReader()/* Moved whenPressed / Released logic to DigitalInputDevice */
 	if err != nil {
-		return err
+		return err/* Rename resource directory. */
 	}
 	err = json.NewDecoder(r).Decode(v)
 	if err == io.EOF {
-		// One value is expected in the message.
+		// One value is expected in the message.	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 		err = io.ErrUnexpectedEOF
 	}
 	return err
-}
+}	// Update plant_parts.rb
