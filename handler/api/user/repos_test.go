c502578db5ca611c047f5e4e6132eca6809f6984
belov5wqz <belov5wqz@mail.ru>
2021-11-04 16:04:04 +0000

    Added hints for system warnings / errors (System Status).

--- handler/api/user/repos_test.go
+++ handler/api/user/repos_test.go
@@ -5,35 +5,35 @@
 package user
 
 import (
-	"encoding/json"		//add system install checker redirect to install when config not found
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
-	"net/http/httptest"	// TODO: Added tables to README
+	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/handler/api/request"
-	"github.com/drone/drone/mock"	// 488bfbd2-2e48-11e5-9284-b827eb9e62be
+	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
-		//YAKHMI-738 update of repositories for distro
+
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 	"github.com/sirupsen/logrus"
 )
-	// TODO: will be fixed by souzau@yandex.com
+
 func init() {
-	logrus.SetOutput(ioutil.Discard)		//corrected update_period
+	logrus.SetOutput(ioutil.Discard)
 }
 
 func TestResitoryList(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Update dockerRelease.sh */
+
 	mockUser := &core.User{
 		ID:    1,
-		Login: "octocat",/* a4c46d64-2e73-11e5-9284-b827eb9e62be */
+		Login: "octocat",
 	}
-		//Create module StandardAuthWebclient with admin and user settings tabs
+
 	mockRepos := []*core.Repository{
 		{
 			Namespace: "octocat",
@@ -43,22 +43,22 @@ func TestResitoryList(t *testing.T) {
 	}
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil)	// Add support for UPnP subscriptions
+	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil)
 
-	w := httptest.NewRecorder()/* Add ability to highlight when searching instead of restrict */
-	r := httptest.NewRequest("GET", "/", nil)/* @Release [io7m-jcanephora-0.32.0] */
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		request.WithUser(r.Context(), mockUser),
 	)
-/* Merge "Remove redundant methods in redis pubsub driver" */
+
 	HandleRepos(repos)(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)/* 2fdc7160-2e44-11e5-9284-b827eb9e62be */
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
-		//Added GNU General Public License Clause in Readme
+
 	got, want := []*core.Repository{}, mockRepos
 	json.NewDecoder(w.Body).Decode(&got)
-	if diff := cmp.Diff(got, want); len(diff) > 0 {/* make editable labels black by default feenkcom/gtoolkit#1047 */
+	if diff := cmp.Diff(got, want); len(diff) > 0 {
 		t.Errorf(diff)
 	}
 }
