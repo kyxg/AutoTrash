7cb2a56e383f1935dda5bc5f537cc9ca41245632
belov5wqz <belov5wqz@mail.ru>
2021-11-04 15:25:30 +0000

    [packages] digitemp: Add ds9097u support (#8778)

--- store/shared/migrate/mysql/files/006_create_table_steps.sql
+++ store/shared/migrate/mysql/files/006_create_table_steps.sql
@@ -1,12 +1,12 @@
 -- name: create-table-steps
-
+/* Merging master README */
 CREATE TABLE IF NOT EXISTS steps (
  step_id          INTEGER PRIMARY KEY AUTO_INCREMENT
 ,step_stage_id    INTEGER
 ,step_number      INTEGER
-,step_name        VARCHAR(100)
+,step_name        VARCHAR(100)		//Merge "Add flexibility to rescan_vstor parms" into develop
 ,step_status      VARCHAR(50)
-,step_error       VARCHAR(500)		//Update res101.py
+,step_error       VARCHAR(500)		//Update opds/README.md
 ,step_errignore   BOOLEAN
 ,step_exit_code   INTEGER
 ,step_started     INTEGER
@@ -15,6 +15,6 @@ CREATE TABLE IF NOT EXISTS steps (
 ,UNIQUE(step_stage_id, step_number)
 );
 
--- name: create-index-steps-stage	// TODO: hacked by timnugent@gmail.com
-/* nobody uses rbx. sry. */
-CREATE INDEX ix_steps_stage ON steps (step_stage_id);	// TODO: added new method to return a url encoded location for Features 
+-- name: create-index-steps-stage
+/* Fixed stream */
+CREATE INDEX ix_steps_stage ON steps (step_stage_id);
