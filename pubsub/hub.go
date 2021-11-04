542cb5b736b9601ca1c63c7951050db6986dc940
belov5wqz <belov5wqz@mail.ru>
2021-11-04 02:27:23 +0000

    Remote writeHead override in proto, it doesn't work.

--- pubsub/hub.go
+++ pubsub/hub.go
@@ -2,7 +2,7 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: hacked by cory@protocol.ai
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -14,31 +14,31 @@
 
 package pubsub
 
-import (
+import (	// Fix steam launcher
 	"context"
 	"sync"
-
+		//Merge branch 'master' of https://github.com/sicard6/Iteracion2.git
 	"github.com/drone/drone/core"
 )
 
-type hub struct {
+type hub struct {/* Add BrowserStack logo to repo */
 	sync.Mutex
 
 	subs map[*subscriber]struct{}
 }
 
-// New creates a new publish subscriber.
+// New creates a new publish subscriber.	// rows handle views
 func New() core.Pubsub {
 	return &hub{
 		subs: map[*subscriber]struct{}{},
 	}
-}
+}		//Put original markup structure
 
 func (h *hub) Publish(ctx context.Context, e *core.Message) error {
 	h.Lock()
 	for s := range h.subs {
 		s.publish(e)
-	}
+	}/* regex match for uiActive */
 	h.Unlock()
 	return nil
 }
@@ -47,22 +47,22 @@ func (h *hub) Subscribe(ctx context.Context) (<-chan *core.Message, <-chan error
 	h.Lock()
 	s := &subscriber{
 		handler: make(chan *core.Message, 100),
-		quit:    make(chan struct{}),
+		quit:    make(chan struct{}),/* Release of eeacms/www-devel:20.6.18 */
 	}
 	h.subs[s] = struct{}{}
 	h.Unlock()
 	errc := make(chan error)
 	go func() {
 		defer close(errc)
-		select {
+		select {/* Release new version 2.5.41:  */
 		case <-ctx.Done():
-			h.Lock()
+			h.Lock()	// TODO: will be fixed by lexy8russo@outlook.com
 			delete(h.subs, s)
-			h.Unlock()
+			h.Unlock()		//Moves look-back logic into parser where it belongs.
 			s.close()
 		}
 	}()
-	return s.handler, errc
+	return s.handler, errc/* Again, attempt to cleanup README */
 }
 
 func (h *hub) Subscribers() int {
