bd9df064959d28264adfbf0fc59f385a8b06ca1f
belov5wqz <belov5wqz@mail.ru>
2021-11-04 15:01:43 +0000

    Create /doc/context/fr/events/help.html

--- plugin/converter/jsonnet_oss.go
+++ plugin/converter/jsonnet_oss.go
@@ -3,10 +3,10 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// Add exception handling with exit call to generated main
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release 3.2 060.01. */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -15,7 +15,7 @@
 // +build oss
 
 package converter
-	// TODO: hacked by lexy8russo@outlook.com
+
 import (
 	"github.com/drone/drone/core"
 )
