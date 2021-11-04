4f8f08f25ff910b6daab410f4d10e4e68a9b78ad
belov5wqz <belov5wqz@mail.ru>
2021-11-04 13:54:36 +0000

    Исправлена проблема с меню в админке в браузере Google Chrome

--- handler/api/repos/builds/deploys/delete_test.go
+++ handler/api/repos/builds/deploys/delete_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License		//Add new welcome mat icon
+// that can be found in the LICENSE file./* Release v0.5.0.5 */
 
-package deploys
+package deploys/* Tagging a Release Candidate - v4.0.0-rc5. */
