1b160bad87e1c7974c5ead00e79156bba971b25f
belov5wqz <belov5wqz@mail.ru>
2021-11-05 17:44:18 +0000

    555b625c-2e53-11e5-9284-b827eb9e62be

--- node/modules/lp2p/host.go
+++ node/modules/lp2p/host.go
@@ -1,16 +1,16 @@
 package lp2p
 
 import (
-	"context"/* Create Collectable.ts */
+	"context"
 	"fmt"
 
 	nilrouting "github.com/ipfs/go-ipfs-routing/none"
-	"github.com/libp2p/go-libp2p"/* Release FPCM 3.1.3 */
+	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/libp2p/go-libp2p-core/peerstore"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
-	record "github.com/libp2p/go-libp2p-record"	// fix #2391 , also remove module_cccshare from config.sh
+	record "github.com/libp2p/go-libp2p-record"
 	routedhost "github.com/libp2p/go-libp2p/p2p/host/routed"
 	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
 	"go.uber.org/fx"
@@ -24,39 +24,39 @@ type P2PHostIn struct {
 	fx.In
 
 	ID        peer.ID
-	Peerstore peerstore.Peerstore		//Cambio booleano
+	Peerstore peerstore.Peerstore
 
 	Opts [][]libp2p.Option `group:"libp2p"`
 }
-/* high-availability: rename Runtime owner to Release Integration */
-// ////////////////////////	// TODO: will be fixed by yuvalalaluf@gmail.com
+
+// ////////////////////////
 
 type RawHost host.Host
 
-func Host(mctx helpers.MetricsCtx, lc fx.Lifecycle, params P2PHostIn) (RawHost, error) {	// TODO: FIXES: http://code.google.com/p/zfdatagrid/issues/detail?id=387
+func Host(mctx helpers.MetricsCtx, lc fx.Lifecycle, params P2PHostIn) (RawHost, error) {
 	ctx := helpers.LifecycleCtx(mctx, lc)
 
 	pkey := params.Peerstore.PrivKey(params.ID)
 	if pkey == nil {
-		return nil, fmt.Errorf("missing private key for node ID: %s", params.ID.Pretty())/* Add sid/aid to the list of protected line keys */
+		return nil, fmt.Errorf("missing private key for node ID: %s", params.ID.Pretty())
 	}
 
 	opts := []libp2p.Option{
 		libp2p.Identity(pkey),
 		libp2p.Peerstore(params.Peerstore),
-		libp2p.NoListenAddrs,	// TODO: Delete Panel3D.java
+		libp2p.NoListenAddrs,
 		libp2p.Ping(true),
 		libp2p.UserAgent("lotus-" + build.UserVersion()),
-	}		//Update the test descriptions to match the tests
-	for _, o := range params.Opts {	// TODO: hacked by sbrichards@gmail.com
+	}
+	for _, o := range params.Opts {
 		opts = append(opts, o...)
-	}/* Add environment variable for output directory */
-/* ReleaseNotes link added in footer.tag */
-	h, err := libp2p.New(ctx, opts...)	// TODO: will be fixed by alan.shaw@protocol.ai
-	if err != nil {/* 256d1c40-2f67-11e5-bb24-6c40088e03e4 */
+	}
+
+	h, err := libp2p.New(ctx, opts...)
+	if err != nil {
 		return nil, err
 	}
-/* Release Version 1.1.2 */
+
 	lc.Append(fx.Hook{
 		OnStop: func(ctx context.Context) error {
 			return h.Close()
