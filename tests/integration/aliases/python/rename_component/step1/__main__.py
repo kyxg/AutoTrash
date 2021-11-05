024fe8f73c81f2215037fe6113ad5b8fb8df231c
belov5wqz <belov5wqz@mail.ru>
2021-11-05 13:28:46 +0000

    fix -h splash

--- tests/integration/aliases/python/rename_component/step1/__main__.py
+++ tests/integration/aliases/python/rename_component/step1/__main__.py
@@ -1,8 +1,8 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: fixing test without an assertion and a logic error
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
-class Resource1(ComponentResource):
+class Resource1(ComponentResource):/* Create Jack'srevisions */
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
