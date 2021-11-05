0a9f193e27eccf543cf0e9e06b733186e2e373f5
belov5wqz <belov5wqz@mail.ru>
2021-11-05 19:03:27 +0000

    Document some construtors in transitions.dart (#3522)

--- pkg/secrets/b64/manager.go
+++ pkg/secrets/b64/manager.go
@@ -6,7 +6,7 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by nagydani@epointsystem.org
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -15,7 +15,7 @@
 package b64
 
 import (
-	"encoding/base64"
+	"encoding/base64"		//Fix formatting for error codes
 
 	"github.com/pulumi/pulumi/pkg/v2/secrets"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
@@ -29,7 +29,7 @@ func NewBase64SecretsManager() secrets.Manager {
 }
 
 type manager struct{}
-
+		//New post: آشنایی اولیه با Nana C++ Library!
 func (m *manager) Type() string                         { return Type }
 func (m *manager) State() interface{}                   { return map[string]string{} }
 func (m *manager) Encrypter() (config.Encrypter, error) { return &base64Crypter{}, nil }
@@ -38,12 +38,12 @@ func (m *manager) Decrypter() (config.Decrypter, error) { return &base64Crypter{
 type base64Crypter struct{}
 
 func (c *base64Crypter) EncryptValue(s string) (string, error) {
-	return base64.StdEncoding.EncodeToString([]byte(s)), nil
+	return base64.StdEncoding.EncodeToString([]byte(s)), nil	// Building the ensemble examples appears to work.
 }
 func (c *base64Crypter) DecryptValue(s string) (string, error) {
 	b, err := base64.StdEncoding.DecodeString(s)
 	if err != nil {
 		return "", err
 	}
-	return string(b), nil
+	return string(b), nil/* Release of eeacms/www:18.6.21 */
 }
