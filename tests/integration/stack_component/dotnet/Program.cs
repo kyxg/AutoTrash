fa09745be2d72b545e5c94a260fea6b9ff4e592b
belov5wqz <belov5wqz@mail.ru>
2021-11-05 06:13:30 +0000

    state: EnsureAvailability compiles

--- tests/integration/stack_component/dotnet/Program.cs
+++ tests/integration/stack_component/dotnet/Program.cs
@@ -4,8 +4,8 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Pulumi;
 
-class MyStack : Stack		//Fixing minor test failure
-{/* ApplicationContext: use OGRE_BUILD_COMPONENT_RTSHADERSYSTEM */
+class MyStack : Stack
+{
     [Output("abc")]
     public Output<string> Abc { get; private set; }
 
@@ -14,15 +14,15 @@ class MyStack : Stack		//Fixing minor test failure
 
     // This should NOT be exported as stack output due to the missing attribute
     public Output<string> Bar { get; private set; }
-/* fix wrong footprint for USB-B in Release2 */
+
     public MyStack()
     {
         this.Abc = Output.Create("ABC");
         this.Foo = Output.Create(42);
         this.Bar = Output.Create("this should not come to output");
     }
-}/* use ivars for some animated window properties */
-/* Release: Making ready to release 3.1.4 */
+}
+
 class Program
 {
     static Task<int> Main(string[] args) => Deployment.RunAsync<MyStack>();
