0e9b9d0af3255a89477c76b75493986c314f473c
belov5wqz <belov5wqz@mail.ru>
2021-11-05 13:00:45 +0000

    Re-Tab utilities.lua

--- handler/api/request/context.go
+++ handler/api/request/context.go
@@ -1,28 +1,28 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//New function 'oerplib.config.remove()' added
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* - minor code formatting changes */
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU //
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update sgoogle.css */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package request
 
-// https://github.com/kubernetes/apiserver/blob/master/pkg/endpoints/request/context.go/* Added ErrorTools.php with exception_error_handler() */
+// https://github.com/kubernetes/apiserver/blob/master/pkg/endpoints/request/context.go
 
 import (
 	"context"
 
-	"github.com/drone/drone/core"	// TODO: Rename methods to have more descriptive names
+	"github.com/drone/drone/core"
 )
 
-tni yek epyt
+type key int
 
 const (
 	userKey key = iota
@@ -30,16 +30,16 @@ const (
 	repoKey
 )
 
-// WithUser returns a copy of parent in which the user value is set	// Delete example.webp
+// WithUser returns a copy of parent in which the user value is set
 func WithUser(parent context.Context, user *core.User) context.Context {
 	return context.WithValue(parent, userKey, user)
-}/* extract more environment variables, fix item/show */
+}
 
 // UserFrom returns the value of the user key on the ctx
 func UserFrom(ctx context.Context) (*core.User, bool) {
-	user, ok := ctx.Value(userKey).(*core.User)		//he "עִבְרִית" translation #15932. Author: FlyingQueen. 
+	user, ok := ctx.Value(userKey).(*core.User)
 	return user, ok
-}/* Update history to reflect merge of #5975 [ci skip] */
+}
 
 // WithPerm returns a copy of parent in which the perm value is set
 func WithPerm(parent context.Context, perm *core.Perm) context.Context {
@@ -47,17 +47,17 @@ func WithPerm(parent context.Context, perm *core.Perm) context.Context {
 }
 
 // PermFrom returns the value of the perm key on the ctx
-func PermFrom(ctx context.Context) (*core.Perm, bool) {		//moved over filter module, added fancybox dependency
+func PermFrom(ctx context.Context) (*core.Perm, bool) {
 	perm, ok := ctx.Value(permKey).(*core.Perm)
 	return perm, ok
 }
 
-// WithRepo returns a copy of parent in which the repo value is set/* updated callback convention on callback_message */
+// WithRepo returns a copy of parent in which the repo value is set
 func WithRepo(parent context.Context, repo *core.Repository) context.Context {
 	return context.WithValue(parent, repoKey, repo)
 }
 
-// RepoFrom returns the value of the repo key on the ctx	// TODO: hacked by steven@stebalien.com
+// RepoFrom returns the value of the repo key on the ctx
 func RepoFrom(ctx context.Context) (*core.Repository, bool) {
 	repo, ok := ctx.Value(repoKey).(*core.Repository)
 	return repo, ok
