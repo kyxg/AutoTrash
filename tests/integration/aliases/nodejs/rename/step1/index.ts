629e38f9a44b3539cfed84011177ccd7364b3412
belov5wqz <belov5wqz@mail.ru>
2021-11-05 18:53:13 +0000

    When a dime is inserted the display shows $0.10

--- tests/integration/aliases/nodejs/rename/step1/index.ts
+++ tests/integration/aliases/nodejs/rename/step1/index.ts
@@ -1,12 +1,12 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Update angular-initial-value.js */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* start on HtmlWindow.[h|cpp] */
         super("my:module:Resource", name, {}, opts);
-    }
+    }/* Added images for items in the 'weapons' category */
 }
-	// TODO: will be fixed by mail@overlisted.net
+
 // Scenario #1 - rename a resource
 const res1 = new Resource("res1");
