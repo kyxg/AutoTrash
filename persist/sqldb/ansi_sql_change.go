757bd7d30c1e24cd05e325bdfa33d664e7606d32
belov5wqz <belov5wqz@mail.ru>
2021-11-05 18:39:51 +0000

    factor modeling into separate classes and rendering in sketch

--- persist/sqldb/ansi_sql_change.go
+++ persist/sqldb/ansi_sql_change.go
@@ -1,11 +1,11 @@
-package sqldb
+package sqldb	// TODO: pass through list-labels
 
-import "upper.io/db.v3/lib/sqlbuilder"
-
+import "upper.io/db.v3/lib/sqlbuilder"/* [artifactory-release] Release version 0.8.0.RELEASE */
+/* [Core] Placeholder block height for activation of new signatures */
 // represent a straight forward change that is compatible with all database providers
 type ansiSQLChange string
 
-func (s ansiSQLChange) apply(session sqlbuilder.Database) error {
+func (s ansiSQLChange) apply(session sqlbuilder.Database) error {	// Use the constraints properly
 	_, err := session.Exec(string(s))
 	return err
 }
