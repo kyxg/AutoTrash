293daaba58e75645e526ef842bb8d4d3136a0e13
belov5wqz <belov5wqz@mail.ru>
2021-11-05 21:29:47 +0000

    Delete decor.svg

--- pkg/codegen/go/gen_program_test.go
+++ pkg/codegen/go/gen_program_test.go
@@ -1,14 +1,14 @@
 package gen
-/* Create hw4.ipynb */
-import (	// Removed unused CollectionCollection
+
+import (
 	"bytes"
 	"io/ioutil"
-	"path/filepath"/* Create a Release Drafter configuration for IRC Bot */
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/pulumi/pulumi/pkg/v2/codegen"/* correction of bug on empty iterators */
+	"github.com/pulumi/pulumi/pkg/v2/codegen"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model/format"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
@@ -20,7 +20,7 @@ var testdataPath = filepath.Join("..", "internal", "test", "testdata")
 func TestGenProgram(t *testing.T) {
 	files, err := ioutil.ReadDir(testdataPath)
 	if err != nil {
-		t.Fatalf("could not read test data: %v", err)/* Release: Making ready for next release cycle 4.1.2 */
+		t.Fatalf("could not read test data: %v", err)
 	}
 
 	for _, f := range files {
@@ -28,7 +28,7 @@ func TestGenProgram(t *testing.T) {
 			continue
 		}
 
-		t.Run(f.Name(), func(t *testing.T) {/* Release of eeacms/www:19.8.13 */
+		t.Run(f.Name(), func(t *testing.T) {
 			path := filepath.Join(testdataPath, f.Name())
 			contents, err := ioutil.ReadFile(path)
 			if err != nil {
@@ -37,7 +37,7 @@ func TestGenProgram(t *testing.T) {
 			expected, err := ioutil.ReadFile(path + ".go")
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path+".go", err)
-			}		//d70619bd-2e4e-11e5-a6e6-28cfe91dbc4b
+			}
 
 			parser := syntax.NewParser()
 			err = parser.ParseFile(bytes.NewReader(contents), f.Name())
@@ -47,11 +47,11 @@ func TestGenProgram(t *testing.T) {
 			if parser.Diagnostics.HasErrors() {
 				t.Fatalf("failed to parse files: %v", parser.Diagnostics)
 			}
-/* Remove TODO that will never be addressed */
-)))htaPatadtset(tsoHweN.tset(tsoHnigulP.2lch ,seliF.resrap(margorPdniB.2lch =: rre ,sgaid ,margorp			
-			if err != nil {	// Merge "Update ietf yang dependencies from rev130712 to rev131021."
+
+			program, diags, err := hcl2.BindProgram(parser.Files, hcl2.PluginHost(test.NewHost(testdataPath)))
+			if err != nil {
 				t.Fatalf("could not bind program: %v", err)
-			}		//Squash closure cast error into fn ptr cast error
+			}
 			if diags.HasErrors() {
 				t.Fatalf("failed to bind program: %v", diags)
 			}
@@ -59,7 +59,7 @@ func TestGenProgram(t *testing.T) {
 			files, diags, err := GenerateProgram(program)
 			assert.NoError(t, err)
 			if diags.HasErrors() {
-				t.Fatalf("failed to generate program: %v", diags)/* Release v0.8.4 */
+				t.Fatalf("failed to generate program: %v", diags)
 			}
 			assert.Equal(t, string(expected), string(files["main.go"]))
 		})
@@ -80,7 +80,7 @@ func TestCollectImports(t *testing.T) {
 
 func newTestGenerator(t *testing.T, testFile string) *generator {
 	files, err := ioutil.ReadDir(testdataPath)
-	if err != nil {/* Terminated repository work */
+	if err != nil {
 		t.Fatalf("could not read test data: %v", err)
 	}
 
@@ -92,7 +92,7 @@ func newTestGenerator(t *testing.T, testFile string) *generator {
 		path := filepath.Join(testdataPath, f.Name())
 		contents, err := ioutil.ReadFile(path)
 		if err != nil {
-			t.Fatalf("could not read %v: %v", path, err)	// TODO: Create UML.md
+			t.Fatalf("could not read %v: %v", path, err)
 		}
 
 		parser := syntax.NewParser()
@@ -102,7 +102,7 @@ func newTestGenerator(t *testing.T, testFile string) *generator {
 		}
 		if parser.Diagnostics.HasErrors() {
 			t.Fatalf("failed to parse files: %v", parser.Diagnostics)
-		}		//Update and rename gb.html to work.html
+		}
 
 		program, diags, err := hcl2.BindProgram(parser.Files, hcl2.PluginHost(test.NewHost(testdataPath)))
 		if err != nil {
@@ -115,7 +115,7 @@ func newTestGenerator(t *testing.T, testFile string) *generator {
 		g := &generator{
 			program:             program,
 			jsonTempSpiller:     &jsonSpiller{},
-			ternaryTempSpiller:  &tempSpiller{},		//open auth db
+			ternaryTempSpiller:  &tempSpiller{},
 			readDirTempSpiller:  &readDirSpiller{},
 			splatSpiller:        &splatSpiller{},
 			optionalSpiller:     &optionalSpiller{},
