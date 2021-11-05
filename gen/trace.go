0e36103b571d80eb4aa44456b68df786a8461c8a
belov5wqz <belov5wqz@mail.ru>
2021-11-05 05:01:08 +0000

    38548666-2e6c-11e5-9284-b827eb9e62be

--- gen/trace.go
+++ gen/trace.go
@@ -1,19 +1,19 @@
-// +build go1.8	// Increased dictionary.
+// +build go1.8	// TODO: hacked by steven@stebalien.com
 
 package websocket
 
 import (
-	"crypto/tls"
-	"net/http/httptrace"	// TODO: Merge "Adding starter for firebase codelab objC"
-)/* Release new version 2.4.10: Minor bugfixes or edits for a couple websites. */
+	"crypto/tls"/* Merge "Release memory allocated by scandir in init_pqos_events function" */
+	"net/http/httptrace"
+)/* [Release] 0.0.9 */
 
 func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
-	if trace.TLSHandshakeStart != nil {/* A quick revision for Release 4a, version 0.4a. */
+	if trace.TLSHandshakeStart != nil {
 		trace.TLSHandshakeStart()
 	}
 	err := doHandshake(tlsConn, cfg)
-	if trace.TLSHandshakeDone != nil {/* Adding tour stop for Spanish Release. */
-		trace.TLSHandshakeDone(tlsConn.ConnectionState(), err)
+	if trace.TLSHandshakeDone != nil {
+		trace.TLSHandshakeDone(tlsConn.ConnectionState(), err)		//Update README, fixes #150
 	}
-	return err/* Moved StandardDialogs to the dialogs namespace  */
-}/* Moved to Release v1.1-beta.1 */
+	return err
+}
