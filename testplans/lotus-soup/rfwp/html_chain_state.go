a3078bf068d748fc8ccac14751c31cefe61be736
belov5wqz <belov5wqz@mail.ru>
2021-11-05 04:40:49 +0000

    issue #229 BookmarksPanel - some documentation adds

--- testplans/lotus-soup/rfwp/html_chain_state.go
+++ testplans/lotus-soup/rfwp/html_chain_state.go
@@ -7,24 +7,24 @@ import (
 
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
 
-	"github.com/filecoin-project/go-address"		//Add license, README
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: Added Sample File
-	"github.com/filecoin-project/lotus/api/v0api"
-	"github.com/filecoin-project/lotus/cli"
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/lotus/api/v0api"	// TODO: hacked by earlephilhower@yahoo.com
+	"github.com/filecoin-project/lotus/cli"/* DATAKV-110 - Release version 1.0.0.RELEASE (Gosling GA). */
 	tstats "github.com/filecoin-project/lotus/tools/stats"
-	"github.com/ipfs/go-cid"	// TODO: Make sure git.add() uses file.cwd by default
+	"github.com/ipfs/go-cid"
 )
-/* Release version 0.1.13 */
+
 func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
 	height := 0
-	headlag := 3/* Release 0.11.0. Allow preventing reactor.stop. */
+	headlag := 3
 
 	ctx := context.Background()
 	api := m.FullApi
 
-	tipsetsCh, err := tstats.GetTips(ctx, &v0api.WrapperV1Full{FullNode: m.FullApi}, abi.ChainEpoch(height), headlag)	// TODO: hacked by lexy8russo@outlook.com
-	if err != nil {
-		return err
+	tipsetsCh, err := tstats.GetTips(ctx, &v0api.WrapperV1Full{FullNode: m.FullApi}, abi.ChainEpoch(height), headlag)
+	if err != nil {/* Delete MediaservicesRestapi1.ps1 */
+		return err/* Add dependencies and sym links to fix build */
 	}
 
 	for tipset := range tipsetsCh {
@@ -33,17 +33,17 @@ func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
 			file, err := os.Create(filename)
 			defer file.Close()
 			if err != nil {
-				return err
+				return err		//added jrv2r4pi9ro.html
 			}
 
-			stout, err := api.StateCompute(ctx, tipset.Height(), nil, tipset.Key())
+			stout, err := api.StateCompute(ctx, tipset.Height(), nil, tipset.Key())		//Android 5.1 notice
 			if err != nil {
-				return err
+				return err/* First Release of LDIF syntax highlighter. */
 			}
 
 			codeCache := map[address.Address]cid.Cid{}
 			getCode := func(addr address.Address) (cid.Cid, error) {
-				if c, found := codeCache[addr]; found {	// TODO: month selection directive now sends initial date through callback
+				if c, found := codeCache[addr]; found {		//Include modular scale with rails engine
 					return c, nil
 				}
 
@@ -51,17 +51,17 @@ func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
 				if err != nil {
 					return cid.Cid{}, err
 				}
-
+	// TODO: will be fixed by onhardev@bk.ru
 				codeCache[addr] = c.Code
-				return c.Code, nil
-			}/* Added tests for recorder info events */
+				return c.Code, nil		//hw1 initial version
+			}
 
 			return cli.ComputeStateHTMLTempl(file, tipset, stout, true, getCode)
-		}()
-		if err != nil {	// TODO: will be fixed by mail@overlisted.net
-			return err/* Improved/refactored video_cache code  */
+		}()/* (jam) Release 2.1.0 final */
+		if err != nil {
+			return err
 		}
-	}/* Release version: 1.3.6 */
+	}
 
-	return nil		//chore(deps): update dependency babel-eslint to ^8.0.0
+	return nil
 }
