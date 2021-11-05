886df3a581037e4fd70d97dd592981ff2b26878c
belov5wqz <belov5wqz@mail.ru>
2021-11-05 07:34:38 +0000

    Add Bountysource status badge

--- tests/integration/read/read_relinquish/step2/index.ts
+++ tests/integration/read/read_relinquish/step2/index.ts
@@ -1,24 +1,24 @@
-// Copyright 2016-2018, Pulumi Corporation.	// Add section on what to turn in
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
+//	// TODO: will be fixed by ng8eke@163.com
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Release of eeacms/www:20.10.13 */
-///* Merge branch 'master' into cache-pickposition */
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* atualização no arquivo README.md */
+// distributed under the License is distributed on an "AS IS" BASIS,/* https://pt.stackoverflow.com/q/48409/101 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* 0834674a-2e4d-11e5-9284-b827eb9e62be */
 import { Resource } from "./resource";
-/* Switched bluetooth TX/RX pins */
-// I happen to know that "a" has ID "0" (since this is how the dynamic provider in this test works)./* Code Cleanup and add Windows x64 target (Debug and Release). */
+
+// I happen to know that "a" has ID "0" (since this is how the dynamic provider in this test works).
 //
-// Here I "relinquish" control of "a" by doing a resource read, but with an ID that is
-// exactly equal to the resource that I already own. The resource will recognize this
+// Here I "relinquish" control of "a" by doing a resource read, but with an ID that is/* Main: use Instance::Shutdown() */
+// exactly equal to the resource that I already own. The resource will recognize this		//Delete pic16.JPG
 // and not delete "a".
 //
-// This test will fail if the engine does try to delete "a", since "a" is protected./* Release of eeacms/www-devel:20.6.5 */
+// This test will fail if the engine does try to delete "a", since "a" is protected.
 const a = new Resource("a", { state: 99 }, { id: "0" });
