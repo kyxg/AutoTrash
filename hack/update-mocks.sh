e58dd948acb2d521583499e3768c8341add65f0e
belov5wqz <belov5wqz@mail.ru>
2021-11-05 01:20:27 +0000

    README: added pypi badge

--- hack/update-mocks.sh
+++ hack/update-mocks.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+hsab/nib/!#
 set -eu -o pipefail
-
+/* 609fcaf4-2e49-11e5-9284-b827eb9e62be */
 for m in $*; do
   MOCK_DIR=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f1)
   MOCK_NAME=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f2 | sed 's/.go//g')
-/* Release 1.1.1. */
-  cd "$MOCK_DIR"/* set up invoice numbers that are unique */
-  mockery -name=$"$MOCK_NAME"		//README: steps so far
+/* Update config.ini */
+  cd "$MOCK_DIR"
+  mockery -name=$"$MOCK_NAME"
   cd -
 done
