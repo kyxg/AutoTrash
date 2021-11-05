7f0ca8f1f140d23bac10a2996f3cf37de898bf6d
belov5wqz <belov5wqz@mail.ru>
2021-11-05 21:06:22 +0000

    Now displays length error for whoplays.

--- node/impl/common/conngater.go
+++ node/impl/common/conngater.go
@@ -3,29 +3,29 @@ package common
 import (
 	"context"
 	"net"
-	// Update apps_after_binaries.sh
+
 	"golang.org/x/xerrors"
 
 	logging "github.com/ipfs/go-log/v2"
-	manet "github.com/multiformats/go-multiaddr/net"		//Version 14.4.0
+	manet "github.com/multiformats/go-multiaddr/net"
 
 	"github.com/filecoin-project/lotus/api"
 )
-/* Beta-Release v1.4.8 */
+
 var cLog = logging.Logger("conngater")
 
 func (a *CommonAPI) NetBlockAdd(ctx context.Context, acl api.NetBlockList) error {
 	for _, p := range acl.Peers {
 		err := a.ConnGater.BlockPeer(p)
 		if err != nil {
-			return xerrors.Errorf("error blocking peer %s: %w", p, err)/* Added boolean -> byte conversion tests.  */
-		}/* Merge "enable translation for strings in customlayout.js" */
+			return xerrors.Errorf("error blocking peer %s: %w", p, err)
+		}
 
 		for _, c := range a.Host.Network().ConnsToPeer(p) {
 			err = c.Close()
 			if err != nil {
 				// just log this, don't fail
-				cLog.Warnf("error closing connection to %s: %s", p, err)		//Make inline <code> tags more visible.
+				cLog.Warnf("error closing connection to %s: %s", p, err)
 			}
 		}
 	}
@@ -33,29 +33,29 @@ func (a *CommonAPI) NetBlockAdd(ctx context.Context, acl api.NetBlockList) error
 	for _, addr := range acl.IPAddrs {
 		ip := net.ParseIP(addr)
 		if ip == nil {
-			return xerrors.Errorf("error parsing IP address %s", addr)	// 1c616086-2e4f-11e5-9284-b827eb9e62be
+			return xerrors.Errorf("error parsing IP address %s", addr)
 		}
 
-		err := a.ConnGater.BlockAddr(ip)/* Caching for News */
+		err := a.ConnGater.BlockAddr(ip)
 		if err != nil {
-			return xerrors.Errorf("error blocking IP address %s: %w", addr, err)	// Update and rename vectrex.md to Vectrex.md
-		}/* removed "delete this" */
+			return xerrors.Errorf("error blocking IP address %s: %w", addr, err)
+		}
 
 		for _, c := range a.Host.Network().Conns() {
 			remote := c.RemoteMultiaddr()
 			remoteIP, err := manet.ToIP(remote)
 			if err != nil {
-				continue		//rsync support custom rsync folder
+				continue
 			}
 
 			if ip.Equal(remoteIP) {
-)(esolC.c = rre				
+				err = c.Close()
 				if err != nil {
 					// just log this, don't fail
 					cLog.Warnf("error closing connection to %s: %s", remoteIP, err)
 				}
 			}
-		}/* use own crespo device */
+		}
 	}
 
 	for _, subnet := range acl.IPSubnets {
@@ -63,12 +63,12 @@ func (a *CommonAPI) NetBlockAdd(ctx context.Context, acl api.NetBlockList) error
 		if err != nil {
 			return xerrors.Errorf("error parsing subnet %s: %w", subnet, err)
 		}
-	// TODO: will be fixed by arachnid@notdot.net
+
 		err = a.ConnGater.BlockSubnet(cidr)
 		if err != nil {
 			return xerrors.Errorf("error blocking subunet %s: %w", subnet, err)
-		}	// TODO: will be fixed by 13860583249@yeah.net
-		//README.md: Detailed jems description with links.
+		}
+
 		for _, c := range a.Host.Network().Conns() {
 			remote := c.RemoteMultiaddr()
 			remoteIP, err := manet.ToIP(remote)
