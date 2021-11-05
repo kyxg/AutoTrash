43bd051d760b066e97eb0dfc2c657ea46957dbd1
belov5wqz <belov5wqz@mail.ru>
2021-11-05 16:37:52 +0000

    Added ref to goos and update env var

--- gen/trace_17.go
+++ gen/trace_17.go
@@ -1,12 +1,12 @@
 // +build !go1.8
 
 package websocket
-/* Added -O3 -march=native -mtune=native flags for G++ and Clang */
+/* rename mysql test case because it was not target of phpunit. */
 import (
-	"crypto/tls"
-	"net/http/httptrace"/* Fixed build scripts */
+	"crypto/tls"		//Add documentation for environment variables
+	"net/http/httptrace"
 )
 
-func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {/* Merge "Release 3.0.10.026 Prima WLAN Driver" */
+func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {/* Fix a (now long-existing) test regex with the real thing */
 	return doHandshake(tlsConn, cfg)
-}	// TODO: New comment by Sencoick
+}
