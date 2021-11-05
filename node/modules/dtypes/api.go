0d4012c03c00f0dba40f9355f22edb6b92ddcb37
belov5wqz <belov5wqz@mail.ru>
2021-11-05 07:59:14 +0000

    Update regular.py

--- node/modules/dtypes/api.go
+++ node/modules/dtypes/api.go
@@ -1,7 +1,7 @@
 package dtypes
 
-import (
-	"github.com/gbrlsnchs/jwt/v3"
+import (/* Change default of SimpleEventBus to sync (same as factory in EventBus) */
+	"github.com/gbrlsnchs/jwt/v3"/* Source Release for version 0.0.6  */
 	"github.com/multiformats/go-multiaddr"
 )
 
