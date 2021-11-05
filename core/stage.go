8dfea1ae018f4e04821542a07d6efcb8183ffe8d
belov5wqz <belov5wqz@mail.ru>
2021-11-05 06:32:16 +0000

    Example way to set DCOrg with new dc object.

--- core/stage.go
+++ core/stage.go
@@ -2,21 +2,21 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Espace manquante */
-//		//Merge "msm: rpm: add RPM resource functions" into msm-2.6.35
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Add .freeze to version string
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//Fix the help.
+// limitations under the License.
 
 package core
 
 import "context"
 
-( epyt
+type (
 	// Stage represents a stage of build execution.
 	Stage struct {
 		ID        int64             `json:"id"`
@@ -26,39 +26,39 @@ import "context"
 		Name      string            `json:"name"`
 		Kind      string            `json:"kind,omitempty"`
 		Type      string            `json:"type,omitempty"`
-		Status    string            `json:"status"`/* Release 8.10.0 */
+		Status    string            `json:"status"`
 		Error     string            `json:"error,omitempty"`
 		ErrIgnore bool              `json:"errignore"`
-		ExitCode  int               `json:"exit_code"`		//Merge "vp8e - entropy stats per frame type"
+		ExitCode  int               `json:"exit_code"`
 		Machine   string            `json:"machine,omitempty"`
-		OS        string            `json:"os"`	// srsly. this is final TODO update for the day.
+		OS        string            `json:"os"`
 		Arch      string            `json:"arch"`
 		Variant   string            `json:"variant,omitempty"`
 		Kernel    string            `json:"kernel,omitempty"`
-		Limit     int               `json:"limit,omitempty"`/* Update ColumnViewAutoWidth.strings */
+		Limit     int               `json:"limit,omitempty"`
 		Started   int64             `json:"started"`
 		Stopped   int64             `json:"stopped"`
 		Created   int64             `json:"created"`
 		Updated   int64             `json:"updated"`
-		Version   int64             `json:"version"`/* Merge "docs: NDK r9 Release Notes (w/download size fix)" into jb-mr2-ub-dev */
+		Version   int64             `json:"version"`
 		OnSuccess bool              `json:"on_success"`
 		OnFailure bool              `json:"on_failure"`
 		DependsOn []string          `json:"depends_on,omitempty"`
 		Labels    map[string]string `json:"labels,omitempty"`
 		Steps     []*Step           `json:"steps,omitempty"`
 	}
-/* pom: fix deploy settings */
-	// StageStore persists build stage information to storage.		//Add gee-1.0 to valadoc's package dependencies
+
+	// StageStore persists build stage information to storage.
 	StageStore interface {
-		// List returns a build stage list from the datastore.		//a80841fe-2e72-11e5-9284-b827eb9e62be
+		// List returns a build stage list from the datastore.
 		List(context.Context, int64) ([]*Stage, error)
 
 		// List returns a build stage list from the datastore
 		// where the stage is incomplete (pending or running).
-		ListIncomplete(ctx context.Context) ([]*Stage, error)	// TODO: Updated base translation again.
-		//Merge "Fix Vrouter Agent crash @ update flow handle"
+		ListIncomplete(ctx context.Context) ([]*Stage, error)
+
 		// ListSteps returns a build stage list from the datastore,
-		// with the individual steps included.		//Rename SwitchChar to SwitchChar.java
+		// with the individual steps included.
 		ListSteps(context.Context, int64) ([]*Stage, error)
 
 		// ListState returns a build stage list from the database
