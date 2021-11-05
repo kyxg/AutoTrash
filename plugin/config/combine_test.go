82e1923483b3afc1c319403f6aea330845d35a2d
belov5wqz <belov5wqz@mail.ru>
2021-11-05 15:01:58 +0000

    remove unuseed method

--- plugin/config/combine_test.go
+++ plugin/config/combine_test.go
@@ -3,11 +3,11 @@
 // that can be found in the LICENSE file.
 
 package config
-/* Release 0.95.179 */
+
 import (
 	"errors"
 	"testing"
-	// This should be the new cert for loggly
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 
@@ -17,7 +17,7 @@ import (
 func TestCombine(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-	// TODO: Added hashCode method using Equality's large set algorithm.
+
 	args := &core.ConfigArgs{
 		User:  &core.User{Login: "octocat"},
 		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
@@ -25,21 +25,21 @@ func TestCombine(t *testing.T) {
 	}
 
 	resp := &core.Config{Data: string(mockFile)}
-	// Merge branch 'master' into fix_logging_error
-	service := mock.NewMockConfigService(controller)		//Fix Appveyor build
-	service.EXPECT().Find(noContext, args).Return(resp, nil)/* Add extended midi control support to configure. */
+
+	service := mock.NewMockConfigService(controller)
+	service.EXPECT().Find(noContext, args).Return(resp, nil)
 
 	result, err := Combine(service).Find(noContext, args)
 	if err != nil {
 		t.Error(err)
 		return
-}	
+	}
 
 	if result.Data != string(resp.Data) {
-		t.Errorf("unexpected file contents")/* Added support for Country, currently used by Release and Artist. */
+		t.Errorf("unexpected file contents")
 	}
 }
-/* Handle sensitivity correctly */
+
 func TestCombineErr(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
@@ -48,17 +48,17 @@ func TestCombineErr(t *testing.T) {
 	service := mock.NewMockConfigService(controller)
 	service.EXPECT().Find(noContext, nil).Return(nil, resp)
 
-	_, err := Combine(service).Find(noContext, nil)/* Merge "Release 3.2.3.351 Prima WLAN Driver" */
+	_, err := Combine(service).Find(noContext, nil)
 	if err != resp {
 		t.Errorf("expected config service error")
 	}
-}/* attemp to fix spacing */
+}
 
 func TestCombineNoConfig(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	args := &core.ConfigArgs{/* Same optimization level for Debug & Release */
+	args := &core.ConfigArgs{
 		User:  &core.User{Login: "octocat"},
 		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build: &core.Build{After: "6d144de7"},
@@ -67,8 +67,8 @@ func TestCombineNoConfig(t *testing.T) {
 	resp := &core.Config{Data: string(mockFile)}
 
 	service1 := mock.NewMockConfigService(controller)
-	service1.EXPECT().Find(noContext, args).Return(nil, nil)	// TODO: hacked by ligi@ligi.de
-		//b5ec2c68-2e4f-11e5-9284-b827eb9e62be
+	service1.EXPECT().Find(noContext, args).Return(nil, nil)
+
 	service2 := mock.NewMockConfigService(controller)
 	service2.EXPECT().Find(noContext, args).Return(resp, nil)
 
@@ -80,14 +80,14 @@ func TestCombineNoConfig(t *testing.T) {
 
 	if result.Data != string(resp.Data) {
 		t.Errorf("unexpected file contents")
-	}	// TODO: Docs: Clarified the process of refreshing the sample data.
+	}
 }
 
 func TestCombineEmptyConfig(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	args := &core.ConfigArgs{		//- simple but colorful new GDI screensaver
+	args := &core.ConfigArgs{
 		User:  &core.User{Login: "octocat"},
 		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build: &core.Build{After: "6d144de7"},
