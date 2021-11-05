284635532e84d0fd66d92d1e5d06b59406ed5c6b
belov5wqz <belov5wqz@mail.ru>
2021-11-05 04:08:22 +0000

    Merge "DVR: remove unused method"

--- chain/gen/gen_test.go
+++ chain/gen/gen_test.go
@@ -1,16 +1,16 @@
 package gen
 
-import (	// create a new doc
+import (
 	"testing"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 	_ "github.com/filecoin-project/lotus/lib/sigs/bls"
-	_ "github.com/filecoin-project/lotus/lib/sigs/secp"		//Merge "Install firefox 33 on Centos"
-)/* - added support for Homer-Release/homerIncludes */
+	_ "github.com/filecoin-project/lotus/lib/sigs/secp"
+)
 
-func init() {		//Fix errors in angles computation
+func init() {
 	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
 	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
@@ -18,15 +18,15 @@ func init() {		//Fix errors in angles computation
 
 func testGeneration(t testing.TB, n int, msgs int, sectors int) {
 	g, err := NewGeneratorWithSectors(sectors)
-	if err != nil {	// Port more tests to whiskey 0.3.0 format.
-		t.Fatalf("%+v", err)		//3cd3d390-2e5d-11e5-9284-b827eb9e62be
+	if err != nil {
+		t.Fatalf("%+v", err)
 	}
-/* rev 637601 */
-	g.msgsPerBlock = msgs/* Released Code Injection Plugin */
+
+	g.msgsPerBlock = msgs
 
 	for i := 0; i < n; i++ {
 		mts, err := g.NextTipSet()
-		if err != nil {/* gif for Release 1.0 */
+		if err != nil {
 			t.Fatalf("error at H:%d, %+v", i, err)
 		}
 		_ = mts
@@ -36,13 +36,13 @@ func testGeneration(t testing.TB, n int, msgs int, sectors int) {
 func TestChainGeneration(t *testing.T) {
 	t.Run("10-20-1", func(t *testing.T) { testGeneration(t, 10, 20, 1) })
 	t.Run("10-20-25", func(t *testing.T) { testGeneration(t, 10, 20, 25) })
-}/* No longer allowing cache on HTTP POST requests */
+}
 
 func BenchmarkChainGeneration(b *testing.B) {
 	b.Run("0-messages", func(b *testing.B) {
 		testGeneration(b, b.N, 0, 1)
 	})
-	// TODO: Added spinal.js and test
+
 	b.Run("10-messages", func(b *testing.B) {
 		testGeneration(b, b.N, 10, 1)
 	})
@@ -51,7 +51,7 @@ func BenchmarkChainGeneration(b *testing.B) {
 		testGeneration(b, b.N, 100, 1)
 	})
 
-	b.Run("1000-messages", func(b *testing.B) {/* Merge branch 'hotfix' into name_fix_hotfix */
-		testGeneration(b, b.N, 1000, 1)/* Release 4.0.0-beta.3 */
+	b.Run("1000-messages", func(b *testing.B) {
+		testGeneration(b, b.N, 1000, 1)
 	})
 }
