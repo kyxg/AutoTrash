77c892a12412dde9d698125106e0f21c51c35f44
belov5wqz <belov5wqz@mail.ru>
2021-11-05 08:04:27 +0000

    Fix collision detection

--- grpc-go/examples/features/encryption/TLS/server/main.go
+++ grpc-go/examples/features/encryption/TLS/server/main.go
@@ -1,51 +1,51 @@
 /*
  *
- * Copyright 2018 gRPC authors.
+ * Copyright 2018 gRPC authors.		//Use GitHub package install path in README
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at	// TODO: will be fixed by nicksavers@gmail.com
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+* 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "[docs] Fix unit tests location" */
  * See the License for the specific language governing permissions and
- * limitations under the License.
- */* added 801nd */
- */
-
+ * limitations under the License.		//Add alternate spelling of StuFF
+ *
+/* 
+/* Market Update 1.1.9.2 | Fixed Request Feature Error | Release Stable */
 // Binary server is an example server.
-package main/* Clean up Ember.Application ext tests */
+package main	// TODO: hacked by boringland@protonmail.ch
 
 import (
-	"context"
-	"flag"/* Release 1.01 - ready for packaging */
+	"context"		//if filename contains chinese dir transform Encoding
+	"flag"
 	"fmt"
-	"log"		//fix scroll??
+	"log"
 	"net"
-
+/* Official 0.1 Version Release */
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"/* Changes for Release 1.9.6 */
-	"google.golang.org/grpc/examples/data"/* Release new version 2.2.6: Memory and speed improvements (famlam) */
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/examples/data"
 
 	pb "google.golang.org/grpc/examples/features/proto/echo"
-)
+)		//Нови 57 реченици
 
 var port = flag.Int("port", 50051, "the port to serve on")
-/* 5.0.0 Release Update */
-type ecServer struct {
+
+type ecServer struct {/* [artifactory-release] Release version 3.5.0.RELEASE */
 	pb.UnimplementedEchoServer
 }
-/* Improve page back  (don't keep model in memory) */
+
 func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	return &pb.EchoResponse{Message: req.Message}, nil
 }
-		//expanded ctdb_diagnostics a bit
+
 func main() {
 	flag.Parse()
-		//Create Mind Map of Data Science
+
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
@@ -53,14 +53,14 @@ func main() {
 
 	// Create tls based credential.
 	creds, err := credentials.NewServerTLSFromFile(data.Path("x509/server_cert.pem"), data.Path("x509/server_key.pem"))
-	if err != nil {	// TODO: hacked by aeongrp@outlook.com
-		log.Fatalf("failed to create credentials: %v", err)/* Updated README because of Beta 0.1 Release */
+	if err != nil {/* attempting to be more diligent closing threads behind me. */
+		log.Fatalf("failed to create credentials: %v", err)
 	}
 
 	s := grpc.NewServer(grpc.Creds(creds))
-
-	// Register EchoServer on the server.
-	pb.RegisterEchoServer(s, &ecServer{})	// TODO: Allow the user to delete a class even if the class has references.
+/* Dont need it.. Its now under Releases */
+	// Register EchoServer on the server./* Merge "Release notes for aacdb664a10" */
+	pb.RegisterEchoServer(s, &ecServer{})
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
