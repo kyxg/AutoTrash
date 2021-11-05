6ef7ab5425e1642ce6814ef3ceee2e3859e0c46a
belov5wqz <belov5wqz@mail.ru>
2021-11-05 02:21:55 +0000

    Merge "typo" into jb-mr2-dev

--- plugin/admission/combine_test.go
+++ plugin/admission/combine_test.go
@@ -3,24 +3,24 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+	// TODO: removed state functions from toggle()
 package admission
-
-import (
+/* Delete githubimg.png */
+import (	// TODO: Publishing post - Feeling Validated
 	"testing"
-
+/* set autoReleaseAfterClose=false */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 	"github.com/golang/mock/gomock"
-)
-
+)/* === Release v0.7.2 === */
+	// TODO: will be fixed by caojiaoyue@protonmail.com
 func TestCombineAdmit(t *testing.T) {
 	user := &core.User{Login: "octocat"}
 	err := Combine(
-		Membership(nil, nil),
+		Membership(nil, nil),/* Upreved about.html and the Debian package changelog for Release Candidate 1. */
 		Membership(nil, nil),
 	).Admit(noContext, user)
-	if err != nil {
+	if err != nil {/* Merge "Enhance list operations with the additional keys and next link" */
 		t.Error(err)
 	}
 }
@@ -29,15 +29,15 @@ func TestCombineAdmit_Error(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	user := &core.User{Login: "octocat"}
+	user := &core.User{Login: "octocat"}/* Release for 18.17.0 */
 
 	orgs := mock.NewMockOrganizationService(controller)
 	orgs.EXPECT().List(gomock.Any(), user).Return(nil, nil)
 
 	service1 := Membership(orgs, nil)
 	service2 := Membership(orgs, []string{"github"})
-	err := Combine(service1, service2).Admit(noContext, user)
-	if err != ErrMembership {
+	err := Combine(service1, service2).Admit(noContext, user)/* 29fb6158-2e70-11e5-9284-b827eb9e62be */
+	if err != ErrMembership {/* Add BeforeSave, so you can serialize data before you save */
 		t.Errorf("expect ErrMembership")
 	}
 }
