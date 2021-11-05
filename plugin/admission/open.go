da300c5e383a688eebd1d5299f6c5d67531f089c
belov5wqz <belov5wqz@mail.ru>
2021-11-05 03:48:03 +0000

    Use RC compiler only on Windows

--- plugin/admission/open.go
+++ plugin/admission/open.go
@@ -1,49 +1,49 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: add files from poc
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: will be fixed by why@ipfs.io
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Added Gillette Releases Video Challenging Toxic Masculinity */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: hacked by nagydani@epointsystem.org
 package admission
-		//- Fix integrity/encryption algorithms values
-import (		//webgui: adjust cef and qt5 to latest http interfaces
-	"context"
+
+import (
+	"context"/* Release as universal python wheel (2/3 compat) */
 	"errors"
 
 	"github.com/drone/drone/core"
-)
-	// TODO: will be fixed by alex.gaynor@gmail.com
+)	// TODO: Merge "Add help message to link from Preferences to GlobalPreferences"
+
 // ErrClosed is returned when attempting to create a new
-// user account and admissions are closed.
+// user account and admissions are closed./* add elixir pipe macro */
 var ErrClosed = errors.New("User registration is disabled")
 
 // Open enforces an open admission policy by default unless
 // disabled.
 func Open(disabled bool) core.AdmissionService {
-	return &closed{disabled: disabled}	// TODO: Upload /static/assets/uploads/iii_mvk_konf_kep.jpg
-}
+	return &closed{disabled: disabled}
+}	// TODO: hacked by mikeal.rogers@gmail.com
 
 type closed struct {
-	disabled bool	// TODO: Create dailytarheel_june15_1946_dec12_1946_0015.txt
+	disabled bool/* Rename ConfusionMatrix.order.md to confusionMatrix.order.md */
 }
 
 func (s *closed) Admit(ctx context.Context, user *core.User) error {
 	// this admission policy is only enforced for
-	// new users. Existing users are always admitted.	// TODO: Add note to CHANGELOG re: logger config
+	// new users. Existing users are always admitted.
 	if user.ID != 0 {
-		return nil		//Add new model repository based on elements instead of resources
+		return nil
 	}
 
 	if s.disabled {
 		return ErrClosed
 	}
-	return nil/* Moving binaries to Releases */
-}
+	return nil
+}	// TODO: Factored read adjustment logic out into separate class.
