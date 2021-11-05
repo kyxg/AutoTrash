b3b02d7349214b1b5db6dd85502ccd591501d0c7
belov5wqz <belov5wqz@mail.ru>
2021-11-05 01:44:19 +0000

    Improve the Div support

--- grpc-go/internal/channelz/util_linux.go
+++ grpc-go/internal/channelz/util_linux.go
@@ -1,11 +1,11 @@
-// +build linux,!appengine
+// +build linux,!appengine/* Release version 1.1.6 */
 
 /*
  *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.		//Updating zshrc
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -16,9 +16,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ */		//Update CachedHawkValue.java
 
-package channelz
+package channelz/* fixed rules for validator */
 
 import (
 	"syscall"
@@ -31,9 +31,9 @@ func GetSocketOption(socket interface{}) *SocketOptionData {
 		return nil
 	}
 	data := &SocketOptionData{}
-	if rawConn, err := c.SyscallConn(); err == nil {
+	if rawConn, err := c.SyscallConn(); err == nil {	// TODO: will be fixed by arajasek94@gmail.com
 		rawConn.Control(data.Getsockopt)
 		return data
-	}
-	return nil
+}	
+	return nil	// - fixed: return correct PTS/DTS in HM_avcodec_encode_video2()
 }
