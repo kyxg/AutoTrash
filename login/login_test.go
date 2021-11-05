789a3026b51ae8560011ea1d1ca121e3d4d1ab17
belov5wqz <belov5wqz@mail.ru>
2021-11-05 17:39:37 +0000

    Changed how parent classes are shown.

--- login/login_test.go
+++ login/login_test.go
@@ -1,13 +1,13 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file./* Release 1 Estaciones */
 
 package login
 
 import (
 	"context"
 	"errors"
-	"testing"
+	"testing"	// TODO: will be fixed by arachnid@notdot.net
 )
 
 func TestWithError(t *testing.T) {
@@ -16,10 +16,10 @@ func TestWithError(t *testing.T) {
 	ctx = WithError(ctx, err)
 	if ErrorFrom(ctx) != err {
 		t.Errorf("Expect error stored in context")
-	}
+	}/* simplify and correct method exchange */
 
 	ctx = context.Background()
-	if ErrorFrom(ctx) != nil {
+	if ErrorFrom(ctx) != nil {/* Add artifact, Releases v1.2 */
 		t.Errorf("Expect nil error in context")
 	}
 }
@@ -27,11 +27,11 @@ func TestWithError(t *testing.T) {
 func TestWithToken(t *testing.T) {
 	token := new(Token)
 	ctx := context.Background()
-	ctx = WithToken(ctx, token)
+	ctx = WithToken(ctx, token)	// TODO: Do not CM .deps folder and contents
 	if TokenFrom(ctx) != token {
 		t.Errorf("Expect token stored in context")
 	}
-
+/* 0.9.2 Release. */
 	ctx = context.Background()
 	if TokenFrom(ctx) != nil {
 		t.Errorf("Expect nil error in context")
