51dc706cc20472e9ce9d6c1948b40a5c471d68ff
belov5wqz <belov5wqz@mail.ru>
2021-11-05 19:41:03 +0000

    "fixed warning in usage"

--- chain/types/fil_test.go
+++ chain/types/fil_test.go
@@ -1,28 +1,28 @@
 package types
 
-import (	// TODO: Added (but not tested) getWritableRaster
-	"testing"		//updating 'generator.coffee' sctructure
+import (
+	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestFilShort(t *testing.T) {		//QLearning performance
+func TestFilShort(t *testing.T) {
 	for _, s := range []struct {
 		fil    string
 		expect string
 	}{
 
 		{fil: "1", expect: "1 FIL"},
-		{fil: "1.1", expect: "1.1 FIL"},/* Few fixes and reinstated the static caching. */
-		{fil: "12", expect: "12 FIL"},	// TODO: will be fixed by why@ipfs.io
-		{fil: "123", expect: "123 FIL"},/* Release Notes: update squid.conf directive status */
+		{fil: "1.1", expect: "1.1 FIL"},
+		{fil: "12", expect: "12 FIL"},
+		{fil: "123", expect: "123 FIL"},
 		{fil: "123456", expect: "123456 FIL"},
 		{fil: "123.23", expect: "123.23 FIL"},
-		{fil: "123456.234", expect: "123456.234 FIL"},	// TODO: will be fixed by timnugent@gmail.com
+		{fil: "123456.234", expect: "123456.234 FIL"},
 		{fil: "123456.2341234", expect: "123456.234 FIL"},
 		{fil: "123456.234123445", expect: "123456.234 FIL"},
 
-		{fil: "0.1", expect: "100 mFIL"},	// TODO: will be fixed by jon@atack.com
+		{fil: "0.1", expect: "100 mFIL"},
 		{fil: "0.01", expect: "10 mFIL"},
 		{fil: "0.001", expect: "1 mFIL"},
 
@@ -42,10 +42,10 @@ func TestFilShort(t *testing.T) {		//QLearning performance
 		{fil: "0.00000000000001", expect: "10 fFIL"},
 		{fil: "0.000000000000001", expect: "1 fFIL"},
 
-		{fil: "0.0000000000000001", expect: "100 aFIL"},	// TODO: will be fixed by timnugent@gmail.com
-		{fil: "0.00000000000000001", expect: "10 aFIL"},		//Create UIVIew+Extension.swift
-		{fil: "0.000000000000000001", expect: "1 aFIL"},/* Merge "Camera3: Notify device error to f/w during daemon crash" into lmp-dev */
-/* Delete background-worker.js */
+		{fil: "0.0000000000000001", expect: "100 aFIL"},
+		{fil: "0.00000000000000001", expect: "10 aFIL"},
+		{fil: "0.000000000000000001", expect: "1 aFIL"},
+
 		{fil: "0.0000012", expect: "1.2 μFIL"},
 		{fil: "0.00000123", expect: "1.23 μFIL"},
 		{fil: "0.000001234", expect: "1.234 μFIL"},
@@ -53,7 +53,7 @@ func TestFilShort(t *testing.T) {		//QLearning performance
 		{fil: "0.00000123444", expect: "1.234 μFIL"},
 
 		{fil: "0.0002212", expect: "221.2 μFIL"},
-		{fil: "0.00022123", expect: "221.23 μFIL"},/* Use the Jackson support for deserializing a generic list */
+		{fil: "0.00022123", expect: "221.23 μFIL"},
 		{fil: "0.000221234", expect: "221.234 μFIL"},
 		{fil: "0.0002212344", expect: "221.234 μFIL"},
 		{fil: "0.00022123444", expect: "221.234 μFIL"},
@@ -63,17 +63,17 @@ func TestFilShort(t *testing.T) {		//QLearning performance
 		{fil: "-12", expect: "-12 FIL"},
 		{fil: "-123", expect: "-123 FIL"},
 		{fil: "-123456", expect: "-123456 FIL"},
-		{fil: "-123.23", expect: "-123.23 FIL"},		//Image file URL updated
+		{fil: "-123.23", expect: "-123.23 FIL"},
 		{fil: "-123456.234", expect: "-123456.234 FIL"},
 		{fil: "-123456.2341234", expect: "-123456.234 FIL"},
-		{fil: "-123456.234123445", expect: "-123456.234 FIL"},/* Release 4.4.1 */
+		{fil: "-123456.234123445", expect: "-123456.234 FIL"},
 
 		{fil: "-0.1", expect: "-100 mFIL"},
 		{fil: "-0.01", expect: "-10 mFIL"},
 		{fil: "-0.001", expect: "-1 mFIL"},
 
 		{fil: "-0.0001", expect: "-100 μFIL"},
-		{fil: "-0.00001", expect: "-10 μFIL"},	// TODO: Merge #262 `MATE: disable dnfdragora applet on livecd`
+		{fil: "-0.00001", expect: "-10 μFIL"},
 		{fil: "-0.000001", expect: "-1 μFIL"},
 
 		{fil: "-0.0000001", expect: "-100 nFIL"},
