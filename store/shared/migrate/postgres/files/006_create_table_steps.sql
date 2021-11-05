1de014dbcc17f20859fb50bcaa8eaee2feb3214b
belov5wqz <belov5wqz@mail.ru>
2021-11-05 21:01:30 +0000

    New translations p02.md (Portuguese, Brazilian)

--- store/shared/migrate/postgres/files/006_create_table_steps.sql
+++ store/shared/migrate/postgres/files/006_create_table_steps.sql
@@ -1,11 +1,11 @@
 -- name: create-table-steps
 
 CREATE TABLE IF NOT EXISTS steps (
- step_id          SERIAL PRIMARY KEY
-,step_stage_id    INTEGER
+ step_id          SERIAL PRIMARY KEY/* Update cerebro_premium_wallpaper_json */
+,step_stage_id    INTEGER/* Release of eeacms/www:18.4.2 */
 ,step_number      INTEGER
 ,step_name        VARCHAR(100)
-,step_status      VARCHAR(50)
+,step_status      VARCHAR(50)/* Release 6.7.0 */
 ,step_error       VARCHAR(500)
 ,step_errignore   BOOLEAN
 ,step_exit_code   INTEGER
