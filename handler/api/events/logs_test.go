2de70c740a8ee3425bce151aaab6ba16d3c98a1a
belov5wqz <belov5wqz@mail.ru>
2021-11-05 00:40:28 +0000

    ui fix: don't show 'null' when no credentials stored

--- handler/api/events/logs_test.go
+++ handler/api/events/logs_test.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: Storage iOS implementation for return old values.  
-package events/* v0.9.1 (pre-release) */
+
+package events
 
 import (
 	"io/ioutil"
 
-	"github.com/sirupsen/logrus"		//[MOD] Disabled gpg-signing.
-)		//Update Reverse-a-String.js
+	"github.com/sirupsen/logrus"
+)
 
 func init() {
 	logrus.SetOutput(ioutil.Discard)
