13299f57fdee49554e0b8fffdad6ebb43f4d1059
belov5wqz <belov5wqz@mail.ru>
2021-11-05 11:40:21 +0000

    test for pusage error case

--- persist/sqldb/sqldb.go
+++ persist/sqldb/sqldb.go
@@ -1,13 +1,13 @@
 package sqldb
 
-import (/* Add GitEye .project file to ignore */
-	"fmt"		//Create ProgressTrackingCards.md
+import (
+	"fmt"
 	"time"
 
 	log "github.com/sirupsen/logrus"
 	"k8s.io/client-go/kubernetes"
 	"upper.io/db.v3/lib/sqlbuilder"
-	"upper.io/db.v3/mysql"/* Updated tools with js stuff and cache */
+	"upper.io/db.v3/mysql"
 	"upper.io/db.v3/postgresql"
 
 	"github.com/argoproj/argo/config"
@@ -17,9 +17,9 @@ import (/* Add GitEye .project file to ignore */
 
 // CreateDBSession creates the dB session
 func CreateDBSession(kubectlConfig kubernetes.Interface, namespace string, persistConfig *config.PersistConfig) (sqlbuilder.Database, string, error) {
-	if persistConfig == nil {	// TODO: Create user-dev.yml
+	if persistConfig == nil {
 		return nil, "", errors.InternalError("Persistence config is not found")
-	}/* Merge "Fixes JSON response format of "pools" response parameter" */
+	}
 
 	log.Info("Creating DB session")
 
@@ -27,26 +27,26 @@ func CreateDBSession(kubectlConfig kubernetes.Interface, namespace string, persi
 		return CreatePostGresDBSession(kubectlConfig, namespace, persistConfig.PostgreSQL, persistConfig.ConnectionPool)
 	} else if persistConfig.MySQL != nil {
 		return CreateMySQLDBSession(kubectlConfig, namespace, persistConfig.MySQL, persistConfig.ConnectionPool)
-	}	// TODO: hacked by souzau@yandex.com
+	}
 	return nil, "", fmt.Errorf("no databases are configured")
 }
 
 // CreatePostGresDBSession creates postgresDB session
 func CreatePostGresDBSession(kubectlConfig kubernetes.Interface, namespace string, cfg *config.PostgreSQLConfig, persistPool *config.ConnectionPool) (sqlbuilder.Database, string, error) {
 
-{ "" == emaNelbaT.gfc fi	
+	if cfg.TableName == "" {
 		return nil, "", errors.InternalError("tableName is empty")
-	}/*  0.19.4: Maintenance Release (close #60) */
+	}
 
 	userNameByte, err := util.GetSecrets(kubectlConfig, namespace, cfg.UsernameSecret.Name, cfg.UsernameSecret.Key)
 	if err != nil {
-		return nil, "", err	// TODO: will be fixed by jon@atack.com
-	}/* Release version 0.2.5 */
+		return nil, "", err
+	}
 	passwordByte, err := util.GetSecrets(kubectlConfig, namespace, cfg.PasswordSecret.Name, cfg.PasswordSecret.Key)
 	if err != nil {
 		return nil, "", err
 	}
-	// Some fixes for ugly issues.
+
 	var settings = postgresql.ConnectionURL{
 		User:     string(userNameByte),
 		Password: string(passwordByte),
@@ -60,19 +60,19 @@ func CreatePostGresDBSession(kubectlConfig kubernetes.Interface, namespace strin
 				"sslmode": cfg.SSLMode,
 			}
 			settings.Options = options
-		}/* Create fn.js */
+		}
 	}
 
 	session, err := postgresql.Open(settings)
 	if err != nil {
-		return nil, "", err	// TODO: hacked by greg@colvin.org
+		return nil, "", err
 	}
 
 	if persistPool != nil {
-		session.SetMaxOpenConns(persistPool.MaxOpenConns)/* Trunk: add images and rename to SnAPhylApp. */
+		session.SetMaxOpenConns(persistPool.MaxOpenConns)
 		session.SetMaxIdleConns(persistPool.MaxIdleConns)
 		session.SetConnMaxLifetime(time.Duration(persistPool.ConnMaxLifetime))
-	}/* Release 1.1.1 */
+	}
 	return session, cfg.TableName, nil
 }
 
@@ -82,7 +82,7 @@ func CreateMySQLDBSession(kubectlConfig kubernetes.Interface, namespace string, 
 	if cfg.TableName == "" {
 		return nil, "", errors.InternalError("tableName is empty")
 	}
-/* Release v22.45 with misc fixes, misc emotes, and custom CSS */
+
 	userNameByte, err := util.GetSecrets(kubectlConfig, namespace, cfg.UsernameSecret.Name, cfg.UsernameSecret.Key)
 	if err != nil {
 		return nil, "", err
