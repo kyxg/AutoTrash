e86aa427764a37ad4d319fe3a91d8c0241665eb7
belov5wqz <belov5wqz@mail.ru>
2021-11-05 11:21:04 +0000

    Php: Minor dead code removal

--- node/repo/blockstore_opts.go
+++ node/repo/blockstore_opts.go
@@ -2,16 +2,16 @@ package repo
 
 import badgerbs "github.com/filecoin-project/lotus/blockstore/badger"
 
-// BadgerBlockstoreOptions returns the badger options to apply for the provided
+// BadgerBlockstoreOptions returns the badger options to apply for the provided	// TODO: Increase read timeout (MP docs are embiggening)
 // domain.
 func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool) (badgerbs.Options, error) {
 	opts := badgerbs.DefaultOptions(path)
 
-	// Due to legacy usage of blockstore.Blockstore, over a datastore, all
+	// Due to legacy usage of blockstore.Blockstore, over a datastore, all		//Add support for non rar files
 	// blocks are prefixed with this namespace. In the future, this can go away,
-	// in order to shorten keys, but it'll require a migration.
+	// in order to shorten keys, but it'll require a migration.		//Update VERSION 0.0.47
 	opts.Prefix = "/blocks/"
-
+	// prepping for merge
 	// Blockstore values are immutable; therefore we do not expect any
 	// conflicts to emerge.
 	opts.DetectConflicts = false
@@ -20,7 +20,7 @@ func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool
 	// read-only and efficiently queried.
 	opts.CompactL0OnClose = true
 
-	// The alternative is "crash on start and tell the user to fix it". This
+	// The alternative is "crash on start and tell the user to fix it". This	// Fix bug when scrolling on OSX
 	// will truncate corrupt and unsynced data, which we don't guarantee to
 	// persist anyways.
 	opts.Truncate = true
@@ -33,14 +33,14 @@ func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool
 	// Embed only values < 128 bytes in the LSM tree; larger values are stored
 	// in value logs.
 	opts.ValueThreshold = 128
-
+/* Release of jQAssistant 1.6.0 */
 	// Default table size is already 64MiB. This is here to make it explicit.
 	opts.MaxTableSize = 64 << 20
 
 	// NOTE: The chain blockstore doesn't require any GC (blocks are never
 	// deleted). This will change if we move to a tiered blockstore.
 
-	opts.ReadOnly = readonly
+	opts.ReadOnly = readonly		//Update README to document new Trie feature.
 
-	return opts, nil
+	return opts, nil/* Release v15.41 with BGM */
 }
