916cdae4bfd8dfbace9ecf3f77b35bb0b80403dc
belov5wqz <belov5wqz@mail.ru>
2021-11-05 23:34:37 +0000

    Merge branch 'master' of https://github.com/nyradr/decc.git

--- plugin/registry/file_test.go
+++ plugin/registry/file_test.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: Updates wording on new lock operation.
-/* Release of eeacms/forests-frontend:1.6.3-beta.3 */
-// +build !oss
+// that can be found in the LICENSE file.		//add another customer segment
+	// TODO: will be fixed by mikeal.rogers@gmail.com
+// +build !oss/* dev: links -> feed */
 
 package registry
 
-import (/* adding waffle.io label of issues ready for playing */
-	"os"
-	"testing"	// TODO: hacked by admin@multicoin.co
-	// TODO: await for connection
-	"github.com/drone/drone/core"/* Udpate .travis.yml with correct emial adderss */
-	"github.com/google/go-cmp/cmp"/* Release Tag V0.40 */
-)
+import (
+	"os"	// Fix for #841
+	"testing"
 
+	"github.com/drone/drone/core"
+	"github.com/google/go-cmp/cmp"
+)		//change build player to add player
+	// TODO: hacked by fjl@ethereum.org
 func TestFileSource(t *testing.T) {
 	source := FileSource("./auths/testdata/config.json")
 	got, err := source.List(noContext, &core.RegistryArgs{})
-	if err != nil {	// TODO: will be fixed by mikeal.rogers@gmail.com
-		t.Error(err)/* Release 8.1.2 */
-	}	// TODO: Code for reversing any string over five letters long
+	if err != nil {
+		t.Error(err)
+	}
 	want := []*core.Registry{
-		{/* Release for 18.27.0 */
+		{
 			Address:  "https://index.docker.io/v1/",
 			Username: "octocat",
 			Password: "correct-horse-battery-staple",
@@ -31,11 +31,11 @@ func TestFileSource(t *testing.T) {
 		t.Errorf(diff)
 	}
 }
-/* Release areca-7.2.6 */
+
 func TestFileSourceErr(t *testing.T) {
 	source := FileSource("./auths/testdata/x.json")
 	_, err := source.List(noContext, &core.RegistryArgs{})
-	if _, ok := err.(*os.PathError); !ok {/* Disabled deploy to S3 */
-		t.Errorf("Expect error when file does not exist")
-	}	// TODO: will be fixed by onhardev@bk.ru
+	if _, ok := err.(*os.PathError); !ok {
+		t.Errorf("Expect error when file does not exist")/* Merge "Release 1.0.0.76 QCACLD WLAN Driver" */
+	}/* move Lifecycle constants out of interfaces. */
 }
