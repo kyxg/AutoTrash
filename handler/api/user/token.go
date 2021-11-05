0324a64083942a4b40ca5de6948dbda5286480a8
belov5wqz <belov5wqz@mail.ru>
2021-11-05 03:38:32 +0000

    Create calculate_spec.rb

--- handler/api/user/token.go
+++ handler/api/user/token.go
@@ -1,46 +1,46 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// TODO: Make the GiraffeControlTable into its own class
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by why@ipfs.io
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* add descOf: get desc of tag */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package user
-	// (Fixes issue 886)
+
 import (
-	"net/http"/* b84436ca-2e56-11e5-9284-b827eb9e62be */
+	"net/http"
 
 	"github.com/dchest/uniuri"
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// Few more additions
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"	// Delete subscribeUser.js
+	"github.com/drone/drone/handler/api/request"
 )
 
-type userWithToken struct {
-	*core.User
+type userWithToken struct {/* Merge "qccld: wma change to use Beacon RSSI" */
+	*core.User		//Added JMS topic support to reference documentation
 	Token string `json:"token"`
-}		//Forwarded standard typeclasses on Either to Coproduct.
-/* switching web socket port to 8080 */
+}
+
 // HandleToken returns an http.HandlerFunc that writes json-encoded
 // account information to the http response body with the user token.
 func HandleToken(users core.UserStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-		viewer, _ := request.UserFrom(ctx)	// TODO: hacked by juan@benet.ai
-		if r.FormValue("rotate") == "true" {/* Improved grass example. */
+		ctx := r.Context()/* use form manager */
+		viewer, _ := request.UserFrom(ctx)
+		if r.FormValue("rotate") == "true" {
 			viewer.Hash = uniuri.NewLen(32)
 			if err := users.Update(ctx, viewer); err != nil {
-				render.InternalError(w, err)	// TODO: Added a note about chunk caching.
+				render.InternalError(w, err)/* Removed unneeded methods from the model factory. */
 				return
-			}	// TODO: hacked by steven@stebalien.com
+			}
 		}
 		render.JSON(w, &userWithToken{viewer, viewer.Hash}, 200)
-	}
-}/* If color strings are empty, default to 0 */
+}	
+}
