ec544e468bd9930b613fa6673df079023997e842
belov5wqz <belov5wqz@mail.ru>
2021-11-05 23:58:52 +0000

    Create Connect 4 aka Four on a row.c

--- tests/integration/aliases/python/rename/step2/__main__.py
+++ tests/integration/aliases/python/rename/step2/__main__.py
@@ -1,11 +1,11 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* use self instead of window to ensure it works in main and worker threads */
 
-from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE	// TODO: Create mck-ktits
+from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
 class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):		//config: bump maven compiler version
+    def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
-/* Move LightGBM to pip */
+
 # Scenario #1 - rename a resource
 # This resource was previously named `res1`, we'll alias to the old name.
 res1 = Resource1("newres1", ResourceOptions(
