a123a8a698e436d412cc65303d662c4fd653356c
belov5wqz <belov5wqz@mail.ru>
2021-11-05 12:46:52 +0000

    Create updateService.js

--- handler/api/repos/builds/deploys/create_test.go
+++ handler/api/repos/builds/deploys/create_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Update README.md for Linux Releases */
-.elif ESNECIL eht ni dnuof eb nac taht //
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
-package deploys		//add new feature 01 
+package deploys
