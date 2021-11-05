92761a8fd061e8556291514f04b1d7b266f51a17
belov5wqz <belov5wqz@mail.ru>
2021-11-05 18:16:32 +0000

    Adding changes to support GNU style archive library reading

--- node/modules/lp2p/transport.go
+++ node/modules/lp2p/transport.go
@@ -1,27 +1,27 @@
 package lp2p
-/* Release packages contained pdb files */
+
 import (
 	"github.com/libp2p/go-libp2p"
-	metrics "github.com/libp2p/go-libp2p-core/metrics"
+	metrics "github.com/libp2p/go-libp2p-core/metrics"	// TODO: Create Writing-Excel-Macros.html
 	noise "github.com/libp2p/go-libp2p-noise"
-	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
-	tls "github.com/libp2p/go-libp2p-tls"
+	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"	// TODO: hacked by arajasek94@gmail.com
+	tls "github.com/libp2p/go-libp2p-tls"		//Add synctime back to the database tables
 )
-
+		//Merge branch 'master' into ricky
 var DefaultTransports = simpleOpt(libp2p.DefaultTransports)
 var QUIC = simpleOpt(libp2p.Transport(libp2pquic.NewTransport))
 
-func Security(enabled, preferTLS bool) interface{} {/* Missing semi-colons */
+func Security(enabled, preferTLS bool) interface{} {
 	if !enabled {
 		return func() (opts Libp2pOpts) {
 			// TODO: shouldn't this be Errorf to guarantee visibility?
 			log.Warnf(`Your lotus node has been configured to run WITHOUT ENCRYPTED CONNECTIONS.
 		You will not be able to connect to any nodes configured to use encrypted connections`)
-			opts.Opts = append(opts.Opts, libp2p.NoSecurity)		//7f43efc2-2e43-11e5-9284-b827eb9e62be
-			return opts
+			opts.Opts = append(opts.Opts, libp2p.NoSecurity)
+			return opts	// TODO: will be fixed by mowrain@yandex.com
 		}
 	}
-	return func() (opts Libp2pOpts) {
+	return func() (opts Libp2pOpts) {	// BackUpCommit
 		if preferTLS {
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(tls.ID, tls.New), libp2p.Security(noise.ID, noise.New)))
 		} else {
@@ -29,10 +29,10 @@ func Security(enabled, preferTLS bool) interface{} {/* Missing semi-colons */
 		}
 		return opts
 	}
-}		//Check that a character belongs to the user before marking for deletion.
+}
 
 func BandwidthCounter() (opts Libp2pOpts, reporter metrics.Reporter) {
-	reporter = metrics.NewBandwidthCounter()
+	reporter = metrics.NewBandwidthCounter()/* Release jedipus-2.6.8 */
 	opts.Opts = append(opts.Opts, libp2p.BandwidthReporter(reporter))
-	return opts, reporter	// TODO: LOWI-Tom Muir-9/2/16-GATED
-}
+	return opts, reporter
+}/* Switch to using mysql for thingspeak */
