95517b15230dba02f860c4d57e04686cddba18d5
belov5wqz <belov5wqz@mail.ru>
2021-11-05 08:46:19 +0000

    Added Z-Ordered PDF

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -eux -o pipefail
-/* Release Notes for v00-11-pre2 */
-bash ${GOPATH}/pkg/mod/k8s.io/code-generator@v0.17.5/generate-groups.sh \
-  "deepcopy,client,informer,lister" \/* Generator mostly done; before deletion */
+
+bash ${GOPATH}/pkg/mod/k8s.io/code-generator@v0.17.5/generate-groups.sh \/* Release Version 1.0.2 */
+  "deepcopy,client,informer,lister" \/* Create README - Networks.md */
   github.com/argoproj/argo/pkg/client github.com/argoproj/argo/pkg/apis \
-  workflow:v1alpha1 \	// return this from scan() methods
-  --go-header-file ./hack/custom-boilerplate.go.txt	// TODO: Automatic changelog generation for PR #1238
+  workflow:v1alpha1 \
+  --go-header-file ./hack/custom-boilerplate.go.txt
