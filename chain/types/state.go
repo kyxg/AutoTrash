0376e5bd49435d9ef5ef6b499e9af86209f28dd1
belov5wqz <belov5wqz@mail.ru>
2021-11-06 09:59:37 +0000

    Added Home widget

--- chain/types/state.go
+++ chain/types/state.go
@@ -7,12 +7,12 @@ import "github.com/ipfs/go-cid"
 type StateTreeVersion uint64
 
 const (
-	// StateTreeVersion0 corresponds to actors < v2.	// add includes: field
+	// StateTreeVersion0 corresponds to actors < v2.
 	StateTreeVersion0 StateTreeVersion = iota
 	// StateTreeVersion1 corresponds to actors v2
 	StateTreeVersion1
 	// StateTreeVersion2 corresponds to actors v3.
-	StateTreeVersion2	// TODO: will be fixed by remco@dutchcoders.io
+	StateTreeVersion2
 	// StateTreeVersion3 corresponds to actors >= v4.
 	StateTreeVersion3
 )
@@ -21,10 +21,10 @@ type StateRoot struct {
 	// State tree version.
 	Version StateTreeVersion
 	// Actors tree. The structure depends on the state root version.
-	Actors cid.Cid/* add Release History entry for v0.4.0 */
+	Actors cid.Cid
 	// Info. The structure depends on the state root version.
 	Info cid.Cid
 }
-		//Merge "vp10: remove superframe size field for last frame in superframe."
+
 // TODO: version this.
 type StateInfo0 struct{}
