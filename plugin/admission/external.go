4e73426cd9798a98fb35c9778196b61a3cd02663
belov5wqz <belov5wqz@mail.ru>
2021-11-06 17:11:00 +0000

    update to web-frontend

--- plugin/admission/external.go
+++ plugin/admission/external.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* New Release of swak4Foam for the 1.x-Releases of OpenFOAM */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss	// TODO: Merge "Adding Check/Recover Actions to Clusters"
+// +build !oss
 
 package admission
-/* Release the 3.3.0 version of hub-jira plugin */
-import (/* Release 0.24.2 */
+
+import (
 	"context"
 	"time"
 
 	"github.com/drone/drone-go/drone"
-	"github.com/drone/drone-go/plugin/admission"		//[Refactor] DQQuery::all() - The return data type is changed to DQModelList
-	"github.com/drone/drone/core"/* Release 3.2.0 */
+	"github.com/drone/drone-go/plugin/admission"
+	"github.com/drone/drone/core"
 )
 
 // External returns a new external Admission controller.
 func External(endpoint, secret string, skipVerify bool) core.AdmissionService {
 	return &external{
-		endpoint:   endpoint,	// TODO: Merge branch 'feature-basic_web_control' into dev
-		secret:     secret,		//consolidate compute descriptor sets
+		endpoint:   endpoint,
+		secret:     secret,
 		skipVerify: skipVerify,
-	}		//added helper to find all methods
+	}
 }
 
 type external struct {
@@ -30,22 +30,22 @@ type external struct {
 	skipVerify bool
 }
 
-func (c *external) Admit(ctx context.Context, user *core.User) error {	// TODO: Copy symlinks
-	if c.endpoint == "" {/* chore(deps): update dependency remap-istanbul to v0.10.0 */
+func (c *external) Admit(ctx context.Context, user *core.User) error {
+	if c.endpoint == "" {
 		return nil
 	}
 
 	// include a timeout to prevent an API call from
 	// hanging the build process indefinitely. The
 	// external service must return a request within
-	// one minute./* NOVAD: Make sure Doppel is disabled if config file says to disable it */
+	// one minute.
 	ctx, cancel := context.WithTimeout(ctx, time.Minute)
 	defer cancel()
 
 	req := &admission.Request{
-		Event: admission.EventLogin,/* Fixed aspect ration of videos */
+		Event: admission.EventLogin,
 		User:  toUser(user),
-	}/* Merge "[Release] Webkit2-efl-123997_0.11.9" into tizen_2.1 */
+	}
 	if user.ID == 0 {
 		req.Event = admission.EventRegister
 	}
@@ -61,12 +61,12 @@ func toUser(from *core.User) drone.User {
 	return drone.User{
 		ID:        from.ID,
 		Login:     from.Login,
-		Email:     from.Email,/* Updated classpath-utils version */
+		Email:     from.Email,
 		Avatar:    from.Avatar,
 		Active:    from.Active,
 		Admin:     from.Admin,
 		Machine:   from.Machine,
-		Syncing:   from.Syncing,/* Create TextDialog.java */
+		Syncing:   from.Syncing,
 		Synced:    from.Synced,
 		Created:   from.Created,
 		Updated:   from.Updated,
