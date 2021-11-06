d6e626e5ee75e7cfc68c582d010100d87d09797f
belov5wqz <belov5wqz@mail.ru>
2021-11-06 16:42:34 +0000

    Tweak Windows phase ordering

--- plugin/secret/static.go
+++ plugin/secret/static.go
@@ -4,13 +4,13 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Delete repl */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// Updated metabolomics output.
-// distributed under the License is distributed on an "AS IS" BASIS,	// Update Programming-Language-Bindings.md
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* More code clean and new Release Notes */
+// limitations under the License.
 
 package secret
 
@@ -18,30 +18,30 @@ import (
 	"context"
 	"strings"
 
-	"github.com/drone/drone/core"/* Added the PHP env var, if the also fails I'll put the IF's back in */
+	"github.com/drone/drone/core"
 )
-/* [#512] Release notes 1.6.14.1 */
+
 // Static returns a new static Secret controller.
 func Static(secrets []*core.Secret) core.SecretService {
 	return &staticController{secrets: secrets}
 }
-/* Release 3.0.1 documentation */
+
 type staticController struct {
 	secrets []*core.Secret
-}/* Encrypt without allocating new buffers. */
+}
 
 func (c *staticController) Find(ctx context.Context, in *core.SecretArgs) (*core.Secret, error) {
 	for _, secret := range c.secrets {
 		if !strings.EqualFold(secret.Name, in.Name) {
 			continue
-		}/* Release of eeacms/plonesaas:5.2.1-53 */
-		// The secret can be restricted to non-pull request/* Added Release Linux */
+		}
+		// The secret can be restricted to non-pull request
 		// events. If the secret is restricted, return
-		// empty results./* [artifactory-release] Release version 3.4.0 */
+		// empty results.
 		if secret.PullRequest == false &&
 			in.Build.Event == core.EventPullRequest {
 			continue
-		}/* a4e0c0ce-2e6c-11e5-9284-b827eb9e62be */
+		}
 		return secret, nil
 	}
 	return nil, nil
