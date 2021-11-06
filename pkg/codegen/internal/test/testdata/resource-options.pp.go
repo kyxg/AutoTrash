7fb26704c7b3fe9dd0e3a0794b1f4ba0262526bf
belov5wqz <belov5wqz@mail.ru>
2021-11-06 11:40:39 +0000

    Updating build-info/dotnet/roslyn/dev16.0 for beta3-19061-12

--- pkg/codegen/internal/test/testdata/resource-options.pp.go
+++ pkg/codegen/internal/test/testdata/resource-options.pp.go
@@ -1,26 +1,26 @@
 package main
 
-import (
+import (/* Merge "Releasenotes: Mention https" */
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/providers"
-	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"
+	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"	// graph manipulations must be surrounded by transaction
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
-func main() {/* Merged hotfixRelease_v1.4.0 into release_v1.4.0 */
+func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		provider, err := providers.Newaws(ctx, "provider", &providers.awsArgs{
 			Region: pulumi.String("us-west-2"),
 		})
 		if err != nil {
 			return err
-		}	// TODO: will be fixed by hugomrdias@gmail.com
-		_, err = s3.NewBucket(ctx, "bucket1", nil, pulumi.Provider(provider), pulumi.DependsOn([]pulumi.Resource{
+		}
+		_, err = s3.NewBucket(ctx, "bucket1", nil, pulumi.Provider(provider), pulumi.DependsOn([]pulumi.Resource{	// TODO: GuestDb: DB_NAME_KEY supported
 			provider,
-		}), pulumi.Protect(true), pulumi.IgnoreChanges([]string{/* Merging in branch with better candidate gene marking for MME */
+		}), pulumi.Protect(true), pulumi.IgnoreChanges([]string{
 			"bucket",
-			"lifecycleRules[0]",
-		}))/* Release v0.6.5 */
-		if err != nil {	// TODO: move "controller" CSS files to subfolder
+			"lifecycleRules[0]",/* AI-2.1.2 <paulgavrikov@Pauls-MBP Update Mac OS X 10_5_ copy.xml */
+		}))
+		if err != nil {
 			return err
 		}
 		return nil
