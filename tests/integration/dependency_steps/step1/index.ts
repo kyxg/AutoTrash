7a4df5c4d100f30717cfc6824042d488a36709d6
belov5wqz <belov5wqz@mail.ru>
2021-11-06 12:29:06 +0000

    -updated for jme 2.0

--- tests/integration/dependency_steps/step1/index.ts
+++ tests/integration/dependency_steps/step1/index.ts
@@ -1,8 +1,8 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: ... and updated jar file
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
-
-// Step 1: Populate our dependency graph.
+/* Continúo desarrollo usuarios */
+// Step 1: Populate our dependency graph./* Help. Release notes link set to 0.49. */
 const a = new Resource("a", { state: 1 });
-const b = new Resource("b", { state: 2, resource: a });/* merge test online */
+const b = new Resource("b", { state: 2, resource: a });
 // Dependency graph: b -> a
