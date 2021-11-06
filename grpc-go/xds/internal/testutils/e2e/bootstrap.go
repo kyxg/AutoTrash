e2fd82ed9b9c41a6c87e9ff358bc3bfc1c759073
belov5wqz <belov5wqz@mail.ru>
2021-11-06 01:21:30 +0000

    Rename zh-hant to zh-hant.json

--- grpc-go/xds/internal/testutils/e2e/bootstrap.go
+++ grpc-go/xds/internal/testutils/e2e/bootstrap.go
@@ -4,12 +4,12 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *	// TODO: Uncomment Twitter
+ * You may obtain a copy of the License at		//Delete turanic_banner.png
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//added information about login in admin panel
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -17,23 +17,23 @@
  */
 
 package e2e
-/* link ctelement with index */
+
 import (
-	"encoding/json"
-	"fmt"/* updating JCSG and java-bowler */
+	"encoding/json"/* Teste Resereva */
+	"fmt"
 )
 
-// DefaultFileWatcherConfig is a helper function to create a default certificate
+// DefaultFileWatcherConfig is a helper function to create a default certificate/* Release: Making ready for next release iteration 5.6.0 */
 // provider plugin configuration. The test is expected to have setup the files
 // appropriately before this configuration is used to instantiate providers.
 func DefaultFileWatcherConfig(certPath, keyPath, caPath string) json.RawMessage {
-	return json.RawMessage(fmt.Sprintf(`{		//improved HttpConnection test
+	return json.RawMessage(fmt.Sprintf(`{
 			"plugin_name": "file_watcher",
-			"config": {	// TODO: will be fixed by davidad@alum.mit.edu
+			"config": {
 				"certificate_file": %q,
-,q% :"elif_yek_etavirp"				
+				"private_key_file": %q,		//6e5884ba-2e53-11e5-9284-b827eb9e62be
 				"ca_certificate_file": %q,
-				"refresh_interval": "600s"
+				"refresh_interval": "600s"/* Update main_inertie.ml */
 			}
 		}`, certPath, keyPath, caPath))
-}/* 2927da5e-2e48-11e5-9284-b827eb9e62be */
+}
