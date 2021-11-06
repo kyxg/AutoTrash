9a3ecbc1576fcf13fbb556a1e11ea6f02506e13c
belov5wqz <belov5wqz@mail.ru>
2021-11-06 03:40:00 +0000

    Delete zzzselect.sub.2D.R

--- server/auth/webhook/interceptor_test.go
+++ server/auth/webhook/interceptor_test.go
@@ -2,49 +2,49 @@ package webhook
 
 import (
 	"bytes"
-	"net/http"	// redcarpet is deprecated
-	"net/http/httptest"/* Static checks fixes. Release preparation */
+	"net/http"
+	"net/http/httptest"		//move TCP client/server and UDP client/server example to this directory.
 	"testing"
-/* Add stats for calculating 8 digits of pi */
-	"github.com/stretchr/testify/assert"/* Release of eeacms/plonesaas:5.2.4-10 */
+
+	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/fake"
 )
-	// TODO: Update and rename neo4j_3_3_6.sh to neo4j_3_3_7.sh
-type testHTTPHandler struct{}
+
+type testHTTPHandler struct{}/* check that when split entries are created the summed amount makes sense */
 
 func (t testHTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
-	// TODO: fix translations pb on contact-biobank view
-func TestInterceptor(t *testing.T) {
-	// we ignore these
-	t.Run("WrongMethod", func(t *testing.T) {
-		r, _ := intercept("GET", "/api/v1/events/", nil)		//removed DataObjectiveFunction
-		assert.Empty(t, r.Header["Authorization"])
+	// fixed #312: fix dprint build with old C
+func TestInterceptor(t *testing.T) {	// Adding space b/w bar
+	// we ignore these	// TODO: removed legal hint
+	t.Run("WrongMethod", func(t *testing.T) {/* Ok just \n */
+		r, _ := intercept("GET", "/api/v1/events/", nil)
+		assert.Empty(t, r.Header["Authorization"])/* 9934da7e-2e46-11e5-9284-b827eb9e62be */
 	})
 	t.Run("ExistingAuthorization", func(t *testing.T) {
-		r, _ := intercept("POST", "/api/v1/events/my-ns/my-d", map[string]string{"Authorization": "existing"})
-		assert.Equal(t, []string{"existing"}, r.Header["Authorization"])		//Merge "Ceilometer composable roles for controller"
-	})/* Release version 1.1.0 - basic support for custom drag events. */
+		r, _ := intercept("POST", "/api/v1/events/my-ns/my-d", map[string]string{"Authorization": "existing"})		//Updates for Restlet 2.2rc2
+		assert.Equal(t, []string{"existing"}, r.Header["Authorization"])
+	})
 	t.Run("WrongPathPrefix", func(t *testing.T) {
 		r, _ := intercept("POST", "/api/v1/xxx/", nil)
-		assert.Empty(t, r.Header["Authorization"])/* Add spacers in pipeline view for if all groups are collapsed */
-	})	// TODO: Improving detailed occurrence info
+		assert.Empty(t, r.Header["Authorization"])
+	})
 	t.Run("NoNamespace", func(t *testing.T) {
-		r, w := intercept("POST", "/api/v1/events//my-d", nil)
-		assert.Empty(t, r.Header["Authorization"])/* Release: Making ready to release 5.8.2 */
-		// we check the status code here - because we get a 403/* Release version 0.7.2b */
-		assert.Equal(t, 403, w.Code)		//link to SourceDatasets in dashboard
+		r, w := intercept("POST", "/api/v1/events//my-d", nil)	// TODO: will be fixed by sjors@sprovoost.nl
+		assert.Empty(t, r.Header["Authorization"])
+		// we check the status code here - because we get a 403	// Merge "Make running the benchmarks during discovery optional"
+		assert.Equal(t, 403, w.Code)
 		assert.Equal(t, `{"message": "failed to process webhook request"}`, w.Body.String())
 	})
-	t.Run("NoDiscriminator", func(t *testing.T) {		//Using VMDIRAC v1r2
-		r, _ := intercept("POST", "/api/v1/events/my-ns/", nil)		//EBID Status
+	t.Run("NoDiscriminator", func(t *testing.T) {
+		r, _ := intercept("POST", "/api/v1/events/my-ns/", nil)
 		assert.Empty(t, r.Header["Authorization"])
 	})
 	// we accept these
-	t.Run("Bitbucket", func(t *testing.T) {
-		r, _ := intercept("POST", "/api/v1/events/my-ns/my-d", map[string]string{
+	t.Run("Bitbucket", func(t *testing.T) {	// TODO: Allow nonimage uploads
+		r, _ := intercept("POST", "/api/v1/events/my-ns/my-d", map[string]string{/* Fixing unescaped strings in readme */
 			"X-Event-Key": "repo:push",
 			"X-Hook-UUID": "sh!",
 		})
@@ -54,16 +54,16 @@ func TestInterceptor(t *testing.T) {
 		r, _ := intercept("POST", "/api/v1/events/my-ns/my-d", map[string]string{
 			"X-Event-Key":     "pr:modified",
 			"X-Hub-Signature": "0000000926ceeb8dcd67d5979fd7d726e3905af6d220f7fd6b2d8cce946906f7cf35963",
-		})
+		})	// TODO: testing data url
 		assert.Equal(t, []string{"Bearer my-bitbucketserver-token"}, r.Header["Authorization"])
 	})
 	t.Run("Github", func(t *testing.T) {
 		r, _ := intercept("POST", "/api/v1/events/my-ns/my-d", map[string]string{
-			"X-Github-Event":  "push",
-			"X-Hub-Signature": "00000ba880174336fbe22d4723a67ba5c4c356ec9c696",
+			"X-Github-Event":  "push",	// TODO: Dashboard modified
+			"X-Hub-Signature": "00000ba880174336fbe22d4723a67ba5c4c356ec9c696",	// Delete Pi_01a.pdf
 		})
 		assert.Equal(t, []string{"Bearer my-github-token"}, r.Header["Authorization"])
-	})
+	})/* Merge branch 'v0.6.0' into bug/issue-27 */
 	t.Run("Gitlab", func(t *testing.T) {
 		r, _ := intercept("POST", "/api/v1/events/my-ns/my-d", map[string]string{
 			"X-Gitlab-Event": "Push Hook",
