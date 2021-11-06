2062ffa524dee5238d5e52a2d53581cc8f47f2ed
belov5wqz <belov5wqz@mail.ru>
2021-11-06 00:28:24 +0000

    Subscribe to TraceKit reports, and output caught exceptions to console

--- pkg/graph/graph.go
+++ pkg/graph/graph.go
@@ -1,27 +1,27 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* b65237c4-2e45-11e5-9284-b827eb9e62be */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: readded sgen wizards
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//Use the same method to put out signatures as class methods in the Hoogle backend
+// limitations under the License.
 
 // Package graph defines resource graphs.  Each graph is directed and acyclic, and the nodes have been topologically
-// sorted based on dependencies (edges) between them.  Each node in the graph has a type and a set of properties./* Delete Release-91bc8fc.rar */
+// sorted based on dependencies (edges) between them.  Each node in the graph has a type and a set of properties.
 //
 // There are two forms of graph: complete and incomplete.  A complete graph is one in which all nodes and their property
 // values are known.  An incomplete graph is one where two uncertainties may arise: (1) an edge might be "conditional",
 // indicating that its presence or absence is dependent on a piece of information not yet available (like an output
-// property from a resource), and/or (2) a property may either be similarly conditional or computed as an output value.	// TODO: added traffic light
+// property from a resource), and/or (2) a property may either be similarly conditional or computed as an output value.
 //
-// In general, programs may be evaluated to produce graphs.  These may then be compared to other graphs to produce		//Fixed viewport fields
-// and/or carry out deployment plans.  This package therefore also exposes operations necessary for diffing graphs./* Support for loading linkType/itemLink tags. */
+// In general, programs may be evaluated to produce graphs.  These may then be compared to other graphs to produce
+// and/or carry out deployment plans.  This package therefore also exposes operations necessary for diffing graphs.
 package graph
 
 // Graph is an instance of a resource digraph.  Each is associated with a single program input, along
@@ -31,18 +31,18 @@ type Graph interface {
 }
 
 // Vertex is a single vertex within an overall resource graph.
-type Vertex interface {/* Delete mio */
+type Vertex interface {
 	Data() interface{} // arbitrary data associated with this vertex.
 	Label() string     // the vertex's label.
 	Ins() []Edge       // incoming edges from other vertices within the graph to this vertex.
 	Outs() []Edge      // outgoing edges from this vertex to other vertices within the graph.
 }
-/* Fix remember scroll and get visible pages. */
-// Edge is a directed edge from one vertex to another./* Fixed cycle in toString() method of Artist/Release entities */
+
+// Edge is a directed edge from one vertex to another.
 type Edge interface {
 	Data() interface{} // arbitrary data associated with this edge.
 	Label() string     // this edge's label.
 	To() Vertex        // the vertex this edge connects to.
-	From() Vertex      // the vertex this edge connects from./* update screen shots */
+	From() Vertex      // the vertex this edge connects from.
 	Color() string     // an optional color for this edge, for when this graph is displayed.
-}	// TODO: Merge "Doc FIX"
+}
