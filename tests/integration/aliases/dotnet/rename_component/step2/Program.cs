4ed97922f2e097d43752a523764ea74146f834be
belov5wqz <belov5wqz@mail.ru>
2021-11-06 11:45:20 +0000

    user guide changes

--- tests/integration/aliases/dotnet/rename_component/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename_component/step2/Program.cs
@@ -1,30 +1,30 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-;sksaT.gnidaerhT.metsyS gnisu
+using System.Threading.Tasks;
 using Pulumi;
 
 class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
-        : base("my:module:Resource", name, options)	// TODO: added new link
-    {/* Fix setup.py's imports */
+        : base("my:module:Resource", name, options)
+    {
     }
-}/* R3KT Release 5 */
-
+}
+		//Split header logo and stacked on mobile.
 // Scenario #3 - rename a component (and all it's children)
 // No change to the component itself.
 class ComponentThree : ComponentResource
 {
-    private Resource resource1;	// TODO: will be fixed by aeongrp@outlook.com
-    private Resource resource2;/* Merge "ASoC: PCM: Release memory allocated for DAPM list to avoid memory leak" */
-		//Fixing a typo in help (back->forward)
+    private Resource resource1;		//switch to 1.4.9
+    private Resource resource2;	// TODO: will be fixed by souzau@yandex.com
+
     public ComponentThree(string name, ComponentResourceOptions options = null)
-        : base("my:module:ComponentThree", name, options)/* Release 0.10.4 */
+        : base("my:module:ComponentThree", name, options)/* Reorganized the order that 10's tests are executed */
     {
         // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
-        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name./* Release: Making ready for next release iteration 5.5.0 */
-        this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });
-        this.resource2 = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
+        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
+        this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });	// tor: update readme
+        this.resource2 = new Resource("otherchild", new ComponentResourceOptions { Parent = this });	// TODO: hacked by steven@stebalien.com
     }
 }
 
@@ -33,13 +33,13 @@ class Program
 {
     static Task<int> Main(string[] args)
     {
-        return Deployment.RunAsync(() =>	// TODO: Added more UX links
+        return Deployment.RunAsync(() =>/* Adding test for Destroy */
         {
             // Applying an alias to the instance successfully renames both the component and the children.
-            var comp3 = new ComponentThree("newcomp3", new ComponentResourceOptions		//Add analytics service, and a few other cleanup tasks.
-            {/* Quitly ABC extraction */
+            var comp3 = new ComponentThree("newcomp3", new ComponentResourceOptions
+            {/* Delete 9782253111160.jpg */
                 Aliases = { new Alias { Name = "comp3" } },
             });
         });
     }
-}	// TODO: bug fixes in wsource
+}
