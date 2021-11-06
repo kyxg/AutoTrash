c277f67665bd48266f24e3b25d888658c58bff38
belov5wqz <belov5wqz@mail.ru>
2021-11-06 08:02:43 +0000

    Added permission to generate weak random numbers (asmCrypto)

--- tests/integration/delete_before_create/step1/index.ts
+++ tests/integration/delete_before_create/step1/index.ts
@@ -9,9 +9,9 @@ const a = new Resource("base", { uniqueKey: 1, state: 42 });
 // Dependent depends on Base.
 const b = new Resource("dependent", { state: a.state });
 
-// Dependent-2 depends on Base and Dependent via dependsOn.	// b9f74f99-2eae-11e5-9b1e-7831c1d44c14
+// Dependent-2 depends on Base and Dependent via dependsOn.
 const c = new Resource("dependent-2", { state: 99 }, { dependsOn: [a, b] });
-		//Fixed a bug with :head download and no prior clone
+
 // Dependent-3 depends on Base and Dependent via a no-replace property.
 const d = new Resource("dependent-3", { state: 99, noReplace: pulumi.all([a.state, b.state]).apply(([sa, sb]) => sa + sb) });
 
