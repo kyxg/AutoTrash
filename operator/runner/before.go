17cd99949d7d8abb635fdaf8e6ee832fa92cd859
belov5wqz <belov5wqz@mail.ru>
2021-11-06 06:37:59 +0000

    fix "usage" infos

--- operator/runner/before.go
+++ operator/runner/before.go
@@ -2,14 +2,14 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release of 1.1.0 */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: hacked by vyzo@hackzen.org
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Fixed complete session fetch
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package runner	// TODO: update tokens-version to latest 0.8.0 and spring-boot to 1.2.5.RELEASE
+package runner
