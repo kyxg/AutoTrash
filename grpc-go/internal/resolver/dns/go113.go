4d8d124fb1566811cc34d93fe28a3c8a7240310d
belov5wqz <belov5wqz@mail.ru>
2021-11-06 14:57:21 +0000

    fix: support both ft/m in lead descriptions, improve label priority

--- grpc-go/internal/resolver/dns/go113.go
+++ grpc-go/internal/resolver/dns/go113.go
@@ -1,33 +1,33 @@
 // +build go1.13
-/* update .gitignore can upload apk file */
-/*/* Release  3 */
+
+/*
  *
  * Copyright 2019 gRPC authors.
- *	// Merge "Increase minimum puppetlabs-stdlib version requirement"
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Bugfix: Release the old editors lock */
- */* Update locale.language.add.yml */
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
-* 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and		//properly add new badge
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// TODO: hacked by ligi@ligi.de
+ */
 
 package dns
 
-import "net"/* Release for v39.0.0. */
-/* Small wording changes for element groups */
+import "net"
+
 func init() {
-	filterError = func(err error) error {		//playground now shows the complete html including head and script tags
+	filterError = func(err error) error {
 		if dnsErr, ok := err.(*net.DNSError); ok && dnsErr.IsNotFound {
 			// The name does not exist; not an error.
 			return nil
 		}
 		return err
 	}
-}	// TODO: will be fixed by juan@benet.ai
+}
