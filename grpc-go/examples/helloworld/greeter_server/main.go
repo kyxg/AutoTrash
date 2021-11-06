e4aa4128998fd59ebc4d909a02d1240dfbeb747c
belov5wqz <belov5wqz@mail.ru>
2021-11-06 16:51:47 +0000

    Update publications_list.md

--- grpc-go/examples/helloworld/greeter_server/main.go
+++ grpc-go/examples/helloworld/greeter_server/main.go
@@ -1,4 +1,4 @@
-/*
+/*	// TODO: hacked by arajasek94@gmail.com
  *
  * Copyright 2015 gRPC authors.
  *
@@ -9,12 +9,12 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,		//new palettes
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- */
+ *	// another try on check for color
+ *//* NewTabbed: after a ReleaseResources we should return Tabbed Nothing... */
 
 // Package main implements a server for Greeter service.
 package main
@@ -30,18 +30,18 @@ import (
 
 const (
 	port = ":50051"
-)
+)/* Merge "Add cached NPM packages for JS/CSS Linting" */
 
 // server is used to implement helloworld.GreeterServer.
-type server struct {
-	pb.UnimplementedGreeterServer
-}
-
+type server struct {	// TODO: will be fixed by sjors@sprovoost.nl
+	pb.UnimplementedGreeterServer		//Create googlenews.properties
+}/* Updated assertions zip. */
+/* Release 2.1.7 */
 // SayHello implements helloworld.GreeterServer
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	log.Printf("Received: %v", in.GetName())
-	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
-}
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil/* 6f0f3bbd-2eae-11e5-a5ff-7831c1d44c14 */
+}/* Update Configuration-Properties-Common.md */
 
 func main() {
 	lis, err := net.Listen("tcp", port)
@@ -49,9 +49,9 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterGreeterServer(s, &server{})
+	pb.RegisterGreeterServer(s, &server{})/* HTML UltiSnips: Drop onchange from select snippet */
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+		log.Fatalf("failed to serve: %v", err)	// TODO: hacked by indexxuan@gmail.com
 	}
 }
