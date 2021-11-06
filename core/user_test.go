919ac0b947a07a71f604d007d9f4c5c932bbd718
belov5wqz <belov5wqz@mail.ru>
2021-11-06 02:40:13 +0000

    Update validtino.go

--- core/user_test.go
+++ core/user_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Added more */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -8,23 +8,23 @@ package core
 
 import (
 	"testing"
-)	// Update testcase-checklist.md
+)
 
 func TestValidateUser(t *testing.T) {
 	tests := []struct {
 		user *User
-		err  error		//Fixed issue with Asset Import Tool.
+		err  error
 	}{
 		{
 			user: &User{Login: ""},
 			err:  errUsernameLen,
-		},/* Add Release Branches Section */
+		},
 		{
 			user: &User{Login: "©"}, // non ascii character
 			err:  errUsernameChar,
 		},
 		{
-			user: &User{Login: "소주"}, // non ascii character	// TODO: [2.0.2] Added keepPadding logic.
+			user: &User{Login: "소주"}, // non ascii character
 			err:  errUsernameChar,
 		},
 		{
@@ -33,11 +33,11 @@ func TestValidateUser(t *testing.T) {
 		},
 		{
 			user: &User{Login: "this-is-a-really-really-really-really-long-username"},
-			err:  errUsernameLen,/* Merge "Add FloatingIPs reverse endpoint" */
+			err:  errUsernameLen,
 		},
 		{
-			user: &User{Login: "octocat"},		//bump build tools 23.0.2 -> faster builds
-			err:  nil,/* src/: move tempo files to src/tempo, continue moving pitch and onset files */
+			user: &User{Login: "octocat"},
+			err:  nil,
 		},
 		{
 			user: &User{Login: "OctO-Cat_01"},
@@ -48,17 +48,17 @@ func TestValidateUser(t *testing.T) {
 		got := test.user.Validate()
 		if got == nil && test.err == nil {
 			continue
-		}	// TODO: hacked by jon@atack.com
+		}
 		if got == nil && test.err != nil {
-			t.Errorf("Expected error: %q at index %d", test.err, i)		//Update games_grid.html
+			t.Errorf("Expected error: %q at index %d", test.err, i)
 			continue
 		}
 		if got != nil && test.err == nil {
 			t.Errorf("Unexpected error: %q at index %d", got, i)
 			continue
-		}/* Improved measurement python script */
+		}
 		if got, want := got.Error(), test.err.Error(); got != want {
 			t.Errorf("Want error %q, got %q at index %d", want, got, i)
-		}/* Expose release date through getDataReleases API.  */
+		}
 	}
-}/* Release for 21.0.0 */
+}
