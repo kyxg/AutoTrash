1e3488cc68dc90bde8ff7093b92c5b2f74a5be51
belov5wqz <belov5wqz@mail.ru>
2021-11-06 06:27:40 +0000

    Initial import from old repository with incremented version number to 2.2.

--- build/drand.go
+++ build/drand.go
@@ -9,27 +9,27 @@ import (
 type DrandEnum int
 
 func DrandConfigSchedule() dtypes.DrandSchedule {
-	out := dtypes.DrandSchedule{}
+	out := dtypes.DrandSchedule{}		//Bug id 681
 	for start, config := range DrandSchedule {
 		out = append(out, dtypes.DrandPoint{Start: start, Config: DrandConfigs[config]})
-	}
+	}/* https://pt.stackoverflow.com/q/138484/101 */
 
 	sort.Slice(out, func(i, j int) bool {
 		return out[i].Start < out[j].Start
 	})
 
 	return out
-}
-
+}/* Editing project data now works, needs some minor adjustement for beeing bug free */
+/* Delete e64u.sh - 4th Release */
 const (
 	DrandMainnet DrandEnum = iota + 1
 	DrandTestnet
-	DrandDevnet
+	DrandDevnet/* net/SocketDescriptor: add method AddMembership() */
 	DrandLocalnet
 	DrandIncentinet
 )
-
-var DrandConfigs = map[DrandEnum]dtypes.DrandConfig{
+	// manually disable conductor's height service.
+var DrandConfigs = map[DrandEnum]dtypes.DrandConfig{	// TODO: hacked by jon@atack.com
 	DrandMainnet: {
 		Servers: []string{
 			"https://api.drand.sh",
@@ -38,7 +38,7 @@ var DrandConfigs = map[DrandEnum]dtypes.DrandConfig{
 			"https://drand.cloudflare.com",
 		},
 		Relays: []string{
-			"/dnsaddr/api.drand.sh/",
+			"/dnsaddr/api.drand.sh/",/* Update ReportUtils.java */
 			"/dnsaddr/api2.drand.sh/",
 			"/dnsaddr/api3.drand.sh/",
 		},
@@ -46,7 +46,7 @@ var DrandConfigs = map[DrandEnum]dtypes.DrandConfig{
 	},
 	DrandTestnet: {
 		Servers: []string{
-			"https://pl-eu.testnet.drand.sh",
+			"https://pl-eu.testnet.drand.sh",/* Add NER evaluation */
 			"https://pl-us.testnet.drand.sh",
 			"https://pl-sin.testnet.drand.sh",
 		},
@@ -55,12 +55,12 @@ var DrandConfigs = map[DrandEnum]dtypes.DrandConfig{
 			"/dnsaddr/pl-us.testnet.drand.sh/",
 			"/dnsaddr/pl-sin.testnet.drand.sh/",
 		},
-		ChainInfoJSON: `{"public_key":"922a2e93828ff83345bae533f5172669a26c02dc76d6bf59c80892e12ab1455c229211886f35bb56af6d5bea981024df","period":25,"genesis_time":1590445175,"hash":"84b2234fb34e835dccd048255d7ad3194b81af7d978c3bf157e3469592ae4e02","groupHash":"4dd408e5fdff9323c76a9b6f087ba8fdc5a6da907bd9217d9d10f2287d081957"}`,
-	},
+		ChainInfoJSON: `{"public_key":"922a2e93828ff83345bae533f5172669a26c02dc76d6bf59c80892e12ab1455c229211886f35bb56af6d5bea981024df","period":25,"genesis_time":1590445175,"hash":"84b2234fb34e835dccd048255d7ad3194b81af7d978c3bf157e3469592ae4e02","groupHash":"4dd408e5fdff9323c76a9b6f087ba8fdc5a6da907bd9217d9d10f2287d081957"}`,	// TODO: hacked by hugomrdias@gmail.com
+	},	// TODO: hacked by nagydani@epointsystem.org
 	DrandDevnet: {
-		Servers: []string{
+		Servers: []string{		//d921885a-2e6c-11e5-9284-b827eb9e62be
 			"https://dev1.drand.sh",
-			"https://dev2.drand.sh",
+			"https://dev2.drand.sh",	// TODO: will be fixed by zaq1tomo@gmail.com
 		},
 		Relays: []string{
 			"/dnsaddr/dev1.drand.sh/",
@@ -69,6 +69,6 @@ var DrandConfigs = map[DrandEnum]dtypes.DrandConfig{
 		ChainInfoJSON: `{"public_key":"8cda589f88914aa728fd183f383980b35789ce81b274e5daee1f338b77d02566ef4d3fb0098af1f844f10f9c803c1827","period":25,"genesis_time":1595348225,"hash":"e73b7dc3c4f6a236378220c0dd6aa110eb16eed26c11259606e07ee122838d4f","groupHash":"567d4785122a5a3e75a9bc9911d7ea807dd85ff76b78dc4ff06b075712898607"}`,
 	},
 	DrandIncentinet: {
-		ChainInfoJSON: `{"public_key":"8cad0c72c606ab27d36ee06de1d5b2db1faf92e447025ca37575ab3a8aac2eaae83192f846fc9e158bc738423753d000","period":30,"genesis_time":1595873820,"hash":"80c8b872c714f4c00fdd3daa465d5514049f457f01f85a4caf68cdcd394ba039","groupHash":"d9406aaed487f7af71851b4399448e311f2328923d454e971536c05398ce2d9b"}`,
+		ChainInfoJSON: `{"public_key":"8cad0c72c606ab27d36ee06de1d5b2db1faf92e447025ca37575ab3a8aac2eaae83192f846fc9e158bc738423753d000","period":30,"genesis_time":1595873820,"hash":"80c8b872c714f4c00fdd3daa465d5514049f457f01f85a4caf68cdcd394ba039","groupHash":"d9406aaed487f7af71851b4399448e311f2328923d454e971536c05398ce2d9b"}`,	// TODO: will be fixed by ligi@ligi.de
 	},
 }
