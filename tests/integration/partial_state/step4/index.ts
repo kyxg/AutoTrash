c970d84dff675625125df9b3324366e0c12b70dd
belov5wqz <belov5wqz@mail.ru>
2021-11-06 11:59:50 +0000

    Lots of italy stuff. still more to do

--- tests/integration/partial_state/step4/index.ts
+++ tests/integration/partial_state/step4/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";/* Adding the databases (MySQL and Fasta) for RefSeq protein Release 61 */
+import { Resource } from "./resource";
 
 // resource "not-doomed" is updated, but the update partially fails.
-const a = new Resource("not-doomed", 4);/* Being Called/Released Indicator */
+const a = new Resource("not-doomed", 4);/* change: updating colors again */
 
 // "a" should still be in the checkpoint with its new value.
