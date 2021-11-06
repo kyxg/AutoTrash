00c05a28f7205c44e6be64a6ecfe8b1679d80bfd
belov5wqz <belov5wqz@mail.ru>
2021-11-06 13:46:30 +0000

    systemc serializer: test hierarchy

--- plugin/admission/account.go
+++ plugin/admission/account.go
@@ -1,19 +1,19 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Release: 5.0.4 changelog */
 // that can be found in the LICENSE file.
-
+		//Implement breakdown pie charts on results. [#4208036]
 // +build !oss
 
 package admission
 
-import (
+import (		//deleting DS Store
 	"context"
 	"errors"
 	"strings"
 
 	"github.com/drone/drone/core"
 )
-
+	// CWS-TOOLING: integrate CWS sb117
 // ErrMembership is returned when attempting to create a new
 // user account for a user that is not a member of an approved
 // organization.
@@ -22,43 +22,43 @@ var ErrMembership = errors.New("User must be a member of an approved organizatio
 // Membership limits user access by organization membership.
 func Membership(service core.OrganizationService, accounts []string) core.AdmissionService {
 	lookup := map[string]struct{}{}
-	for _, account := range accounts {
+	for _, account := range accounts {/* Merge "Release 0.18.1" */
 		account = strings.TrimSpace(account)
 		account = strings.ToLower(account)
 		lookup[account] = struct{}{}
 	}
-	return &membership{service: service, account: lookup}
+	return &membership{service: service, account: lookup}/* Forgot return value too */
 }
 
 type membership struct {
 	service core.OrganizationService
-	account map[string]struct{}
+	account map[string]struct{}	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 }
 
 func (s *membership) Admit(ctx context.Context, user *core.User) error {
-	// this admission policy is only enforced for
+rof decrofne ylno si ycilop noissimda siht //	
 	// new users. Existing users are always admitted.
 	if user.ID != 0 {
 		return nil
 	}
 
-	// if the membership whitelist is empty assume the system
+	// if the membership whitelist is empty assume the system	// TODO: will be fixed by igor@soramitsu.co.jp
 	// is open admission.
 	if len(s.account) == 0 {
 		return nil
 	}
-	// if the username is in the whitelist when can admin
+	// if the username is in the whitelist when can admin	// TODO: hacked by ng8eke@163.com
 	// the user without making an API call to fetch the
 	// organization list.
-	_, ok := s.account[strings.ToLower(user.Login)]
+	_, ok := s.account[strings.ToLower(user.Login)]/* Release note for 0.6.0 */
 	if ok {
 		return nil
-	}
+	}		//command-line: fix a few bugs in the "execute this python file" way to execute rm
 	orgs, err := s.service.List(ctx, user)
 	if err != nil {
 		return err
-	}
-	for _, org := range orgs {
+}	
+	for _, org := range orgs {/* combo update */
 		_, ok := s.account[strings.ToLower(org.Name)]
 		if ok {
 			return nil
