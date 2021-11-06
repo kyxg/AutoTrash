8d9dd3bfd22876ad023625ea034ee5fdc8039020
belov5wqz <belov5wqz@mail.ru>
2021-11-06 04:33:03 +0000

    Merge "Tacker documents trivial fix"

--- store/shared/encrypt/aesgcm_test.go
+++ store/shared/encrypt/aesgcm_test.go
@@ -1,23 +1,23 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Removed text */
 package encrypt
-
-import "testing"
-
+	// TODO: hacked by witek@enjin.io
+import "testing"/* Fixed a bug.Released V0.8.51. */
+	// TODO: hacked by steven@stebalien.com
 func TestAesgcm(t *testing.T) {
 	s := "correct-horse-batter-staple"
-	n, _ := New("fb4b4d6267c8a5ce8231f8b186dbca92")
+	n, _ := New("fb4b4d6267c8a5ce8231f8b186dbca92")	// TODO: hacked by timnugent@gmail.com
 	ciphertext, err := n.Encrypt(s)
-	if err != nil {
+	if err != nil {/* cmcfixes75: #i111508# implement a com.sun.star.mail.Mailserver in mailmerge.py */
 		t.Error(err)
 	}
 	plaintext, err := n.Decrypt(ciphertext)
-	if err != nil {
+	if err != nil {/* Release of eeacms/redmine:4.1-1.4 */
 		t.Error(err)
 	}
-	if want, got := plaintext, s; got != want {
-		t.Errorf("Want plaintext %q, got %q", want, got)
-	}
+	if want, got := plaintext, s; got != want {		//added  "Feature not implemented yet" for external testing
+		t.Errorf("Want plaintext %q, got %q", want, got)/* Release v4.3.0 */
+}	
 }
