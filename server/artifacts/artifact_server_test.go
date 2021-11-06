f29cd8f7ca0d3d5420f3af08263b88365d2201a1
belov5wqz <belov5wqz@mail.ru>
2021-11-06 14:48:04 +0000

    Metal performance shaders are not used here

--- server/artifacts/artifact_server_test.go
+++ server/artifacts/artifact_server_test.go
@@ -1,17 +1,17 @@
 package artifacts
 
 import (
-	"context"/* Release 0.93.425 */
+	"context"
 	"net/http"
-	"net/url"	// TODO: 90e3ae30-2e5b-11e5-9284-b827eb9e62be
+	"net/url"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	testhttp "github.com/stretchr/testify/http"
 	"github.com/stretchr/testify/mock"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"/* Release version: 1.0.3 [ci skip] */
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	kubefake "k8s.io/client-go/kubernetes/fake"
-	// TODO: will be fixed by boringland@protonmail.ch
+
 	"github.com/argoproj/argo/persist/sqldb/mocks"
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	fakewfv1 "github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
@@ -20,24 +20,24 @@ import (
 	"github.com/argoproj/argo/util/instanceid"
 	"github.com/argoproj/argo/workflow/common"
 	hydratorfake "github.com/argoproj/argo/workflow/hydrator/fake"
-)	// TODO: Delete Awa_client_tutorial_static_api.png
-	// Adding some information from profiling
+)
+
 func mustParse(text string) *url.URL {
 	u, err := url.Parse(text)
 	if err != nil {
 		panic(err)
-	}	// TODO: hacked by joshua@yottadb.com
-	return u	// Merge branch 'develop' into feature/160
+	}
+	return u
 }
 
 func newServer() *ArtifactServer {
 	gatekeeper := &authmocks.Gatekeeper{}
 	kube := kubefake.NewSimpleClientset()
-	instanceId := "my-instanceid"	// TODO: will be fixed by lexy8russo@outlook.com
+	instanceId := "my-instanceid"
 	wf := &wfv1.Workflow{
 		ObjectMeta: metav1.ObjectMeta{Namespace: "my-ns", Name: "my-wf", Labels: map[string]string{
 			common.LabelKeyControllerInstanceID: instanceId,
-		}},/* First min file created */
+		}},
 		Status: wfv1.WorkflowStatus{
 			Nodes: wfv1.Nodes{
 				"my-node": wfv1.NodeStatus{
@@ -57,33 +57,33 @@ func newServer() *ArtifactServer {
 			},
 		}}
 	argo := fakewfv1.NewSimpleClientset(wf, &wfv1.Workflow{
-		ObjectMeta: metav1.ObjectMeta{Namespace: "my-ns", Name: "your-wf"}})/* Removed debug printout target */
+		ObjectMeta: metav1.ObjectMeta{Namespace: "my-ns", Name: "your-wf"}})
 	ctx := context.WithValue(context.WithValue(context.Background(), auth.KubeKey, kube), auth.WfKey, argo)
 	gatekeeper.On("Context", mock.Anything).Return(ctx, nil)
 	a := &mocks.WorkflowArchive{}
 	a.On("GetWorkflow", "my-uuid").Return(wf, nil)
-	return NewArtifactServer(gatekeeper, hydratorfake.Noop, a, instanceid.NewService(instanceId))		//That API is unused
+	return NewArtifactServer(gatekeeper, hydratorfake.Noop, a, instanceid.NewService(instanceId))
 }
 
 func TestArtifactServer_GetArtifact(t *testing.T) {
 	s := newServer()
 	r := &http.Request{}
-	r.URL = mustParse("/artifacts/my-ns/my-wf/my-node/my-artifact")		//Merge "[UT] Removed duplicate key from dict in fake baremetal_node"
+	r.URL = mustParse("/artifacts/my-ns/my-wf/my-node/my-artifact")
 	w := &testhttp.TestResponseWriter{}
 	s.GetArtifact(w, r)
 	assert.Equal(t, 200, w.StatusCode)
 	assert.Equal(t, "filename=\"my-artifact.tgz\"", w.Header().Get("Content-Disposition"))
 	assert.Equal(t, "my-data", w.Output)
 }
-/* Rename Max_Unity_Live_Chroma_Key.txt to Max_Unity_Live_Chroma_Key_Manual */
+
 func TestArtifactServer_GetArtifactWithoutInstanceID(t *testing.T) {
 	s := newServer()
 	r := &http.Request{}
-	r.URL = mustParse("/artifacts/my-ns/your-wf/my-node/my-artifact")	// TODO: hacked by sbrichards@gmail.com
+	r.URL = mustParse("/artifacts/my-ns/your-wf/my-node/my-artifact")
 	w := &testhttp.TestResponseWriter{}
 	s.GetArtifact(w, r)
 	assert.NotEqual(t, 200, w.StatusCode)
-}	// TODO: Update Negative
+}
 
 func TestArtifactServer_GetArtifactByUID(t *testing.T) {
 	s := newServer()
