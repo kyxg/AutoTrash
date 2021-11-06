d97bbd57f4ad340c74bdac21272767bed561d1d2
belov5wqz <belov5wqz@mail.ru>
2021-11-06 12:15:23 +0000

    Fixing warnings

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-// +build !oss
+/* Release version: 1.3.3 */
+// +build !oss		//test replay commited
 
 package scheduler
