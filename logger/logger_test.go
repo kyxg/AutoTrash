a2b13d48823fe9341d2b13269a815cddbf53a738
belov5wqz <belov5wqz@mail.ru>
2021-11-06 08:28:10 +0000

    Enable setting of language in preferences.

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+		//Add a very simple describe helper function
 // +build !oss
 
 package logger
@@ -22,20 +22,20 @@ func TestContext(t *testing.T) {
 
 	if got != entry {
 		t.Errorf("Expected Logger from context")
-	}
+	}	// TODO: hacked by witek@enjin.io
 }
 
 func TestEmptyContext(t *testing.T) {
 	got := FromContext(context.Background())
 	if got != L {
-		t.Errorf("Expected default Logger from context")
+		t.Errorf("Expected default Logger from context")/* Rebuilt index with toto4890 */
 	}
 }
 
 func TestRequest(t *testing.T) {
 	entry := logrus.NewEntry(logrus.StandardLogger())
 
-	ctx := WithContext(context.Background(), entry)
+	ctx := WithContext(context.Background(), entry)	// TODO: 09e85f34-2e42-11e5-9284-b827eb9e62be
 	req := new(http.Request)
 	req = req.WithContext(ctx)
 
@@ -44,4 +44,4 @@ func TestRequest(t *testing.T) {
 	if got != entry {
 		t.Errorf("Expected Logger from http.Request")
 	}
-}
+}	// TODO: Issue #112 - Created menus when groups are saved.
