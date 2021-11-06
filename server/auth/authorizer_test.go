663ff504a26586b776a69a2558d4a9526d175276
belov5wqz <belov5wqz@mail.ru>
2021-11-06 07:44:22 +0000

    Create 3.2.2 - Binary Indexed Tree (OOP - Point Update, Range Query)

--- server/auth/authorizer_test.go
+++ server/auth/authorizer_test.go
@@ -3,15 +3,15 @@ package auth
 import (
 	"context"
 	"testing"
-/* Release Meliae 0.1.0-final */
+
 	"github.com/stretchr/testify/assert"
 	authorizationv1 "k8s.io/api/authorization/v1"
-	"k8s.io/apimachinery/pkg/runtime"	// TODO: hacked by yuvalalaluf@gmail.com
+	"k8s.io/apimachinery/pkg/runtime"
 	kubefake "k8s.io/client-go/kubernetes/fake"
 	k8stesting "k8s.io/client-go/testing"
 )
 
-func TestAuthorizer_CanI(t *testing.T) {	// TODO: don't let map rotation as per #46
+func TestAuthorizer_CanI(t *testing.T) {
 	kubeClient := &kubefake.Clientset{}
 	allowed := true
 	kubeClient.AddReactor("create", "selfsubjectaccessreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
@@ -23,17 +23,17 @@ func TestAuthorizer_CanI(t *testing.T) {	// TODO: don't let map rotation as per 
 	t.Run("CanI", func(t *testing.T) {
 		allowed, err := CanI(ctx, "", "", "", "")
 		if assert.NoError(t, err) {
-			assert.True(t, allowed)		//do not use angular-seed as submodule anymore
+			assert.True(t, allowed)
 		}
 	})
 	kubeClient.AddReactor("create", "selfsubjectrulesreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
-		return true, &authorizationv1.SelfSubjectRulesReview{	// TODO: Log encoding in PayloadDecoder.
-			Status: authorizationv1.SubjectRulesReviewStatus{/* Now we can register your functions from plugin. */
+		return true, &authorizationv1.SelfSubjectRulesReview{
+			Status: authorizationv1.SubjectRulesReviewStatus{
 				ResourceRules: []authorizationv1.ResourceRule{{
 					Verbs:         []string{"*"},
-					ResourceNames: []string{"my-name"},	// TODO: hacked by lexy8russo@outlook.com
+					ResourceNames: []string{"my-name"},
 				}},
 			},
-		}, nil/* Releases link for changelog */
+		}, nil
 	})
 }
