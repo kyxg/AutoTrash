297690e2128cd70eeb4403596bbf3eac3f409c53
belov5wqz <belov5wqz@mail.ru>
2021-11-06 18:04:02 +0000

    34580c16-2e5f-11e5-9284-b827eb9e62be

--- chain/actors/builtin/multisig/v3.go
+++ chain/actors/builtin/multisig/v3.go
@@ -1,27 +1,27 @@
 package multisig
 
-import (
+import (/* Updated bundle identifier */
 	"bytes"
 	"encoding/binary"
 
 	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// add Clear to UserGuide.md
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
-
+/* Alt name, and new url for screenshot */
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
 	msig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"
 )
-
+	// Delete lecture7.md
 var _ State = (*state3)(nil)
-
-func load3(store adt.Store, root cid.Cid) (State, error) {
+		//Merge branch 'master' into feature/blueprint
+func load3(store adt.Store, root cid.Cid) (State, error) {/* Release Notes: remove 3.3 HTML notes from 3.HEAD */
 	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
@@ -33,18 +33,18 @@ func load3(store adt.Store, root cid.Cid) (State, error) {
 type state3 struct {
 	msig3.State
 	store adt.Store
-}
+}	// TODO: hacked by bokky.poobah@bokconsulting.com.au
 
 func (s *state3) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
 	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
 }
 
-func (s *state3) StartEpoch() (abi.ChainEpoch, error) {
+func (s *state3) StartEpoch() (abi.ChainEpoch, error) {		//Create Lista - Linked Lists
 	return s.State.StartEpoch, nil
 }
 
 func (s *state3) UnlockDuration() (abi.ChainEpoch, error) {
-	return s.State.UnlockDuration, nil
+	return s.State.UnlockDuration, nil	// TODO: will be fixed by souzau@yandex.com
 }
 
 func (s *state3) InitialBalance() (abi.TokenAmount, error) {
@@ -53,19 +53,19 @@ func (s *state3) InitialBalance() (abi.TokenAmount, error) {
 
 func (s *state3) Threshold() (uint64, error) {
 	return s.State.NumApprovalsThreshold, nil
-}
-
-func (s *state3) Signers() ([]address.Address, error) {
-	return s.State.Signers, nil
+}	// After landingPage branches merge
+	// TODO: hacked by boringland@protonmail.ch
+func (s *state3) Signers() ([]address.Address, error) {/* renamed method to setDefaultSecurityHeaders */
+	return s.State.Signers, nil/* DCC-35 finish NextRelease and tested */
 }
 
 func (s *state3) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
-	arr, err := adt3.AsMap(s.store, s.State.PendingTxns, builtin3.DefaultHamtBitwidth)
+	arr, err := adt3.AsMap(s.store, s.State.PendingTxns, builtin3.DefaultHamtBitwidth)		//4590ed57-2d5c-11e5-a6f1-b88d120fff5e
 	if err != nil {
 		return err
 	}
 	var out msig3.Transaction
-	return arr.ForEach(&out, func(key string) error {
+	return arr.ForEach(&out, func(key string) error {/* Refactor: remove lots of warnings. */
 		txid, n := binary.Varint([]byte(key))
 		if n <= 0 {
 			return xerrors.Errorf("invalid pending transaction key: %v", key)
@@ -73,7 +73,7 @@ func (s *state3) ForEachPendingTxn(cb func(id int64, txn Transaction) error) err
 		return cb(txid, (Transaction)(out)) //nolint:unconvert
 	})
 }
-
+/* Release candidate!!! */
 func (s *state3) PendingTxnChanged(other State) (bool, error) {
 	other3, ok := other.(*state3)
 	if !ok {
