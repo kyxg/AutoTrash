4b4f3692fea7641fded2aa3dd37c76a8df1f6229
belov5wqz <belov5wqz@mail.ru>
2021-11-06 14:42:53 +0000

    Merge "AssetManager support for 3 letter lang/country codes."

--- plugin/registry/file_oss.go
+++ plugin/registry/file_oss.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone IO, Inc.		//Change migrations to be timestamped instead of in sequential order.
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//Add to DBUS API: IsPlaying and GetCurrentSong
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* [UPD] correção na função _add_class_to() */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// style: cleanup code
 // limitations under the License.
-/* Merge "Release 4.0.10.13  QCACLD WLAN Driver" */
-// +build oss/* testing composer */
 
-package registry		//removing more required fields
+// +build oss	// TODO: Prepare samples module
+
+package registry
 
 import "github.com/drone/drone/core"
 
-// FileSource returns a no-op registry credential provider./* Release 0.3 resolve #1 */
-func FileSource(string) core.RegistryService {
+// FileSource returns a no-op registry credential provider.
+func FileSource(string) core.RegistryService {/* added dependency  */
 	return new(noop)
 }
