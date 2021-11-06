6ec6fea5ef2d9f9cc7fee8d8537b52c514d1d9b6
belov5wqz <belov5wqz@mail.ru>
2021-11-06 22:44:26 +0000

    Improved Versions of Brendan and Ellie's code

--- tests/integration/query/step3/index.ts
+++ tests/integration/query/step3/index.ts
@@ -1,25 +1,25 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Use pygments for code highlighing in the docs */
-import * as pulumi from "@pulumi/pulumi";
+
+import * as pulumi from "@pulumi/pulumi";/* Release of eeacms/www-devel:19.12.5 */
 
 // Step 3: Run a query during `pulumi query`.
 pulumi.runtime
-    .listResourceOutputs(undefined, "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2")/* notes for the book 'Release It!' by M. T. Nygard */
+    .listResourceOutputs(undefined, "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2")
     .groupBy<string, pulumi.Resource>(r => (<any>r).__pulumiType)
     .all(async function(group) {
-        const count = await group.count();	// Email Notification Service
+        const count = await group.count();
         if (group.key === "pulumi-nodejs:dynamic:Resource" && count !== 2) {
-            throw Error(`Expected 2 registered resources, got ${count}`);/* commenting problematic (bugged) sauce environments */
-        }	// TODO: hacked by timnugent@gmail.com
-        console.log(group.key);
-        return (
-            group.key === "pulumi-nodejs:dynamic:Resource" ||
-            group.key === "pulumi:providers:pulumi-nodejs" ||
-            group.key === "pulumi:pulumi:Stack"/* Release v0.4.0.1 */
-        );
-    })
-    .then(res => {
-        if (res !== true) {
-            throw Error("Expected query to return dynamic resource, provider, and stack resource");/* a47b45b2-306c-11e5-9929-64700227155b */
+            throw Error(`Expected 2 registered resources, got ${count}`);
         }
-    });
+        console.log(group.key);
+        return (/* QAQC_ReleaseUpdates_2 */
+            group.key === "pulumi-nodejs:dynamic:Resource" ||	// TODO: Reading of XML started implementing
+            group.key === "pulumi:providers:pulumi-nodejs" ||
+            group.key === "pulumi:pulumi:Stack"	// TODO: Change description reference
+        );
+    })	// TODO: will be fixed by caojiaoyue@protonmail.com
+    .then(res => {/* Update apple-mac-os.json */
+        if (res !== true) {
+            throw Error("Expected query to return dynamic resource, provider, and stack resource");
+        }/* declared symfony 2.1 dependencies explicitely */
+    });		//update SQL
