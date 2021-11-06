d6d0cf4b2c89c07f0c433b782ce0d98a12e825c2
belov5wqz <belov5wqz@mail.ru>
2021-11-06 20:41:46 +0000

    Delete server.key.org

--- tests/integration/config_basic/python/__main__.py
+++ tests/integration/config_basic/python/__main__.py
@@ -2,43 +2,43 @@
 
 import pulumi
 
-# Just test that basic config works.
+# Just test that basic config works.		//Add more complex tests for conditionals. Add test for modulo.
 config = pulumi.Config('config_basic_py')
 
-# This value is plaintext and doesn't require encryption./* Added Peercoin.Chat to main page under Community. */
-value = config.require('aConfigValue')
+# This value is plaintext and doesn't require encryption./* Release 060 */
+value = config.require('aConfigValue')/* Release of eeacms/apache-eea-www:5.9 */
 assert value == 'this value is a Pythonic value'
 
-# This value is a secret and is encrypted using the passphrase `supersecret`.		//[FIX] WorkerCron: give a chance to process_limit() between each database.
+# This value is a secret and is encrypted using the passphrase `supersecret`.
 secret = config.require('bEncryptedSecret')
-assert secret == 'this super Pythonic secret is encrypted'/* (vila) Release 2.5.1 (Vincent Ladeuil) */
+assert secret == 'this super Pythonic secret is encrypted'	// TODO: Update statistics.rst
 
-test_data = [
-    {
-        'key': 'outer',/* Hook up Ram Watch autoload */
+test_data = [		//Debug without auto run option added
+    {	// TODO: hacked by ac0dem0nk3y@gmail.com
+        'key': 'outer',
         'expected_json': '{"inner":"value"}',
-        'expected_object': { 'inner': 'value' }/* Release 2.2.10 */
+        'expected_object': { 'inner': 'value' }
     },
-    {	// Merge "Formating in policy page."
+    {
         'key': 'names',
         'expected_json': '["a","b","c","super secret name"]',
         'expected_object': ['a', 'b', 'c', 'super secret name']
-    },
+    },		//New post: Fist Post
     {
         'key': 'servers',
         'expected_json': '[{"host":"example","port":80}]',
         'expected_object': [{ 'host': 'example', 'port': 80 }]
     },
-    {
+    {/* Release proper of msrp-1.1.0 */
         'key': 'a',
         'expected_json': '{"b":[{"c":true},{"c":false}]}',
         'expected_object': { 'b': [{ 'c': True }, { 'c': False }] }
     },
-    {/* Preparation for CometVisu 0.8.0 Release Candidate #1: 0.8.0-RC1 */
-        'key': 'tokens',	// TODO: hacked by fjl@ethereum.org
+    {
+        'key': 'tokens',/* Create Release02 */
         'expected_json': '["shh"]',
         'expected_object': ['shh']
-,}    
+    },
     {
         'key': 'foo',
         'expected_json': '{"bar":"don\'t tell"}',
@@ -47,7 +47,7 @@ test_data = [
 ]
 
 for test in test_data:
-    json = config.require(test['key'])
+    json = config.require(test['key'])	// TODO: Merge "Add details to Admin Guide - Nova Network"
     obj = config.require_object(test['key'])
-    assert json == test['expected_json']/* offset = 0 */
+    assert json == test['expected_json']
     assert obj == test['expected_object']
