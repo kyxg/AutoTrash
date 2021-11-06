e12752db4cf634bd04d795c5605be26e9186eb2f
belov5wqz <belov5wqz@mail.ru>
2021-11-06 22:06:31 +0000

    Merge "heat client expects a string for the template not json"

--- tests/integration/protect_resources/step2/index.ts
+++ tests/integration/protect_resources/step2/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";/* Release version 2.8.0 */
 
-// Now, simply update the resource; this should work fine:		//Merge "Removing firing of spurious scroll accesibility events."
-let a = new Resource("eternal", { state: 2 }, { protect: true });
+// Now, simply update the resource; this should work fine:
+let a = new Resource("eternal", { state: 2 }, { protect: true });	// TODO: hacked by ng8eke@163.com
