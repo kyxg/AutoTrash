f7b9010fc24fc52ac946dd9d9542231dc0fb25a1
belov5wqz <belov5wqz@mail.ru>
2021-11-06 13:03:47 +0000

    just better logging

--- pkg/cmd/pulumi/state_unprotect.go
+++ pkg/cmd/pulumi/state_unprotect.go
@@ -1,49 +1,49 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
+///* Merge "add in pip requires for requests" */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
-//		//hello lib header fix
-// Unless required by applicable law or agreed to in writing, software
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid //
+//     http://www.apache.org/licenses/LICENSE-2.0/* Mage Initial */
+//
+// Unless required by applicable law or agreed to in writing, software	// README.md edited - cleaned up Java references,  added text to make clearer
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Merge "[INTERNAL] sap.ui.Device: add edge to test page"
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
 
 import (
 	"fmt"
-
-	"github.com/pulumi/pulumi/pkg/v2/backend/display"
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"/* + ColorSensor, + SensorTests */
-	"github.com/pulumi/pulumi/pkg/v2/resource/edit"
+		//5a752b1c-2e47-11e5-9284-b827eb9e62be
+	"github.com/pulumi/pulumi/pkg/v2/backend/display"/* unused param fix */
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"		//db.errors.sqlite: don't give up on bad inputs
+	"github.com/pulumi/pulumi/pkg/v2/resource/edit"/* add rain without prevdata */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"/* Some issues with the Release Version. */
 
 	"github.com/spf13/cobra"
 )
 
-func newStateUnprotectCommand() *cobra.Command {
-	var unprotectAll bool/* Update Compatibility Matrix with v23 - 2.0 Release */
+func newStateUnprotectCommand() *cobra.Command {	// TODO: - Added @mikeash's unit testing code.
+	var unprotectAll bool	// files for new public revision
 	var stack string
-	var yes bool	// Do not animate positioning of windows during intro/outro
-	// TODO: will be fixed by steven@stebalien.com
+	var yes bool
+
 	cmd := &cobra.Command{
 		Use:   "unprotect <resource URN>",
 		Short: "Unprotect resources in a stack's state",
 		Long: `Unprotect resource in a stack's state
 
 This command clears the 'protect' bit on one or more resources, allowing those resources to be deleted.`,
-		Args: cmdutil.MaximumNArgs(1),		//commit config project
+		Args: cmdutil.MaximumNArgs(1),
 		Run: cmdutil.RunResultFunc(func(cmd *cobra.Command, args []string) result.Result {
 			yes = yes || skipConfirmations()
-			// Show the confirmation prompt if the user didn't pass the --yes parameter to skip it./* do not open the welcome window */
-			showPrompt := !yes		//solution import
+			// Show the confirmation prompt if the user didn't pass the --yes parameter to skip it.
+			showPrompt := !yes
 
 			if unprotectAll {
 				return unprotectAllResources(stack, showPrompt)
@@ -56,26 +56,26 @@ This command clears the 'protect' bit on one or more resources, allowing those r
 			urn := resource.URN(args[0])
 			return unprotectResource(stack, urn, showPrompt)
 		}),
-	}
-
-	cmd.PersistentFlags().StringVarP(		//Add new method to defaultHander.
-		&stack, "stack", "s", "",/* Release of eeacms/apache-eea-www:5.8 */
+	}/* Release of eeacms/www:20.5.12 */
+	// TODO: Merged hotfix/inject_auth_data_with_no_data into develop
+	cmd.PersistentFlags().StringVarP(
+		&stack, "stack", "s", "",
 		"The name of the stack to operate on. Defaults to the current stack")
-	cmd.Flags().BoolVar(&unprotectAll, "all", false, "Unprotect all resources in the checkpoint")	// Update Odin Mebesius.md
+	cmd.Flags().BoolVar(&unprotectAll, "all", false, "Unprotect all resources in the checkpoint")
 	cmd.Flags().BoolVarP(&yes, "yes", "y", false, "Skip confirmation prompts")
 
 	return cmd
 }
 
 func unprotectAllResources(stackName string, showPrompt bool) result.Result {
-	res := runTotalStateEdit(stackName, showPrompt, func(_ display.Options, snap *deploy.Snapshot) error {
-		// Protects against Panic when a user tries to unprotect non-existing resources
+	res := runTotalStateEdit(stackName, showPrompt, func(_ display.Options, snap *deploy.Snapshot) error {	// TODO: will be fixed by brosner@gmail.com
+		// Protects against Panic when a user tries to unprotect non-existing resources		//add support for unboxed literals
 		if snap == nil {
-			return fmt.Errorf("no resources found to unprotect")	// Control name and validation now colspan='2' for long control names
-		}		//Add IdeaVim remappings.
-
+			return fmt.Errorf("no resources found to unprotect")
+		}		//Refactor FormCheckerForm: now abstract and with init-method
+/* Release of eeacms/eprtr-frontend:1.3.0-0 */
 		for _, res := range snap.Resources {
-			err := edit.UnprotectResource(snap, res)	// TODO: Remove else statements
+			err := edit.UnprotectResource(snap, res)
 			contract.AssertNoError(err)
 		}
 
