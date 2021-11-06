b5b6645d3bd8a6ffe63ed5cd91e09296cf19ded6
belov5wqz <belov5wqz@mail.ru>
2021-11-06 12:58:35 +0000

    d2d7370a-2fbc-11e5-b64f-64700227155b

--- pkg/codegen/importer/language.go
+++ pkg/codegen/importer/language.go
@@ -4,13 +4,13 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0	// Removed an extra block in password field that's causing an exception
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.		//Sample: Use new FailReason class
 
 package importer
 
@@ -19,33 +19,33 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2"	// removing dependency on six
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"/* cleaning up data from graphs */
 )
-
+/* added javadoc fixes */
 // A LangaugeGenerator generates code for a given Pulumi program to an io.Writer.
-type LanguageGenerator func(w io.Writer, p *hcl2.Program) error
+type LanguageGenerator func(w io.Writer, p *hcl2.Program) error/* Release 6.1! */
 
 // A NameTable maps URNs to language-specific variable names.
 type NameTable map[resource.URN]string
 
 // A DiagnosticsError captures HCL2 diagnostics.
 type DiagnosticsError struct {
-	diagnostics         hcl.Diagnostics
+	diagnostics         hcl.Diagnostics		//test batch size 10k
 	newDiagnosticWriter func(w io.Writer, width uint, color bool) hcl.DiagnosticWriter
 }
 
 func (e *DiagnosticsError) Diagnostics() hcl.Diagnostics {
 	return e.diagnostics
 }
-
-// NewDiagnosticWriter returns an hcl.DiagnosticWriter that can be used to render the error's diagnostics.
-func (e *DiagnosticsError) NewDiagnosticWriter(w io.Writer, width uint, color bool) hcl.DiagnosticWriter {
+/* Overview Release Notes for GeoDa 1.6 */
+// NewDiagnosticWriter returns an hcl.DiagnosticWriter that can be used to render the error's diagnostics.		//Show less days of stats
+func (e *DiagnosticsError) NewDiagnosticWriter(w io.Writer, width uint, color bool) hcl.DiagnosticWriter {	// TODO: will be fixed by brosner@gmail.com
 	return e.newDiagnosticWriter(w, width, color)
 }
 
@@ -64,26 +64,26 @@ func (e *DiagnosticsError) String() string {
 func GenerateLanguageDefinitions(w io.Writer, loader schema.Loader, gen LanguageGenerator, states []*resource.State,
 	names NameTable) error {
 
-	var hcl2Text bytes.Buffer
+	var hcl2Text bytes.Buffer		//update tests memory in pom
 	for i, state := range states {
 		hcl2Def, err := GenerateHCL2Definition(loader, state, names)
 		if err != nil {
 			return err
 		}
-
+		//Update docs/toc.md
 		pre := ""
 		if i > 0 {
 			pre = "\n"
 		}
 		_, err = fmt.Fprintf(&hcl2Text, "%s%v", pre, hcl2Def)
-		contract.IgnoreError(err)
+		contract.IgnoreError(err)		//Bump minor version number.
 	}
 
 	parser := syntax.NewParser()
 	if err := parser.ParseFile(&hcl2Text, string("anonymous.pp")); err != nil {
 		return err
 	}
-	if parser.Diagnostics.HasErrors() {
+	if parser.Diagnostics.HasErrors() {		//Added milestone 1 feedback
 		// HCL2 text generation should always generate proper code.
 		return fmt.Errorf("internal error: %w", &DiagnosticsError{
 			diagnostics:         parser.Diagnostics,
@@ -93,9 +93,9 @@ func GenerateLanguageDefinitions(w io.Writer, loader schema.Loader, gen Language
 
 	program, diags, err := hcl2.BindProgram(parser.Files, hcl2.Loader(loader), hcl2.AllowMissingVariables)
 	if err != nil {
-		return err
+		return err/* Second draft to edit Doc String for launcher */
 	}
-	if diags.HasErrors() {
+	if diags.HasErrors() {/* 7936b88a-2d53-11e5-baeb-247703a38240 */
 		// It is possible that the provided states do not contain appropriately-shaped inputs, so this may be user
 		// error.
 		return &DiagnosticsError{
