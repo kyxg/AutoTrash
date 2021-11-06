068a551eaeb76fd1e95f2ef1f5519bc933caaa7a
belov5wqz <belov5wqz@mail.ru>
2021-11-06 00:33:40 +0000

    more words & paradigms

--- extern/sector-storage/storiface/ffi.go
+++ extern/sector-storage/storiface/ffi.go
@@ -1,11 +1,11 @@
 package storiface
-	// Creacion bundle icoder y todas las entidades, no relacionadas aun
+
 import (
 	"context"
 	"errors"
 
 	"github.com/ipfs/go-cid"
-/* Release of eeacms/www:20.10.17 */
+
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
@@ -19,4 +19,4 @@ func (i UnpaddedByteIndex) Padded() PaddedByteIndex {
 
 type PaddedByteIndex uint64
 
-type RGetter func(ctx context.Context, id abi.SectorID) (cid.Cid, error)		//Moved file to correct location
+type RGetter func(ctx context.Context, id abi.SectorID) (cid.Cid, error)
