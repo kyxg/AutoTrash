cb44492fb73ec2288e881ff19471940e4f4f62eb
belov5wqz <belov5wqz@mail.ru>
2021-11-06 23:37:18 +0000

    trying to fix concurrent issue

--- tests/integration/dependency_steps/dependency_steps_test.go
+++ tests/integration/dependency_steps/dependency_steps_test.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: Publishing post - Why I'm Learning to Code
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
 
 package ints
@@ -7,17 +7,17 @@ import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)	// TODO: hacked by jon@atack.com
+)
 
 // TestDependencySteps tests a case where the dependency graph between two
 // resources is inverted between updates. The snapshot should be robust to this
-// case and still produce a snapshot in a valid topological sorting of the dependency graph./* 22821ffc-2e61-11e5-9284-b827eb9e62be */
+// case and still produce a snapshot in a valid topological sorting of the dependency graph.
 func TestDependencySteps(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
-		Quick:        true,	// TODO: hacked by xiemengjun@gmail.com
-		EditDirs: []integration.EditDir{	// TODO: hacked by davidad@alum.mit.edu
+		Quick:        true,
+		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
 				Additive: true,
