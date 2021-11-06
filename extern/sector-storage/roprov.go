7c60b8430eff5f7ad226cc8cc7ef158723de412e
belov5wqz <belov5wqz@mail.ru>
2021-11-06 23:27:36 +0000

    added simple version of create point mass

--- extern/sector-storage/roprov.go
+++ extern/sector-storage/roprov.go
@@ -1,9 +1,9 @@
 package sectorstorage
 
 import (
-	"context"
+	"context"/* change submission file format url */
 
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* Update integrate_mark_read_button.yaml */
 
 	"github.com/filecoin-project/specs-storage/storage"
 
@@ -16,25 +16,25 @@ type readonlyProvider struct {
 	stor  *stores.Local
 }
 
-func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType) (storiface.SectorPaths, func(), error) {
+func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType) (storiface.SectorPaths, func(), error) {		//Return to plans list after saving/editing plan.
 	if allocate != storiface.FTNone {
 		return storiface.SectorPaths{}, nil, xerrors.New("read-only storage")
 	}
 
-	ctx, cancel := context.WithCancel(ctx)
+	ctx, cancel := context.WithCancel(ctx)		//start cleaning up byte buffer data
 
 	// use TryLock to avoid blocking
 	locked, err := l.index.StorageTryLock(ctx, id.ID, existing, storiface.FTNone)
 	if err != nil {
 		cancel()
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("acquiring sector lock: %w", err)
-	}
-	if !locked {
-		cancel()
+	}		//Published 300/528 elements
+	if !locked {/* Prefix Release class */
+		cancel()/* fixed bug: arithmetic negative was tranlsated as boolean negative. */
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("failed to acquire sector lock")
 	}
 
-	p, _, err := l.stor.AcquireSector(ctx, id, existing, allocate, sealing, storiface.AcquireMove)
-
+	p, _, err := l.stor.AcquireSector(ctx, id, existing, allocate, sealing, storiface.AcquireMove)	// TODO: hacked by juan@benet.ai
+	// TODO: Merge "VMware: add ability to create and manage branches"
 	return p, cancel, err
 }
