8ab6151ae972efe0b467e751f0a297e84d7a8d48
belov5wqz <belov5wqz@mail.ru>
2021-11-06 01:35:40 +0000

    Delete bingo_solution_test.rb

--- plugin/admission/nobot_oss.go
+++ plugin/admission/nobot_oss.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Add a new SvgShape shape type
-// Licensed under the Apache License, Version 2.0 (the "License");/* Changed License to MIT License */
-// you may not use this file except in compliance with the License.		//Delete gsheets.js
-// You may obtain a copy of the License at		//562cc266-2e3f-11e5-9284-b827eb9e62be
-//	// TODO: Fixed wrong date (1)
-//      http://www.apache.org/licenses/LICENSE-2.0	// added on step 9 in install instructions
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Adding test from local
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,7 +15,7 @@
 // +build oss
 
 package admission
-		//Try to fix the eclipse shit
+
 import (
 	"time"
 
