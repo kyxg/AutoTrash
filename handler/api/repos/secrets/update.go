fec8a1bff1caf71b63a91bde3db74175510bbe9d
belov5wqz <belov5wqz@mail.ru>
2021-11-06 22:34:55 +0000

    box updates

--- handler/api/repos/secrets/update.go
+++ handler/api/repos/secrets/update.go
@@ -8,18 +8,18 @@ package secrets
 
 import (
 	"encoding/json"
-	"net/http"
+	"net/http"/* Corrected `crn tree` description */
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-
+		//Removed ssl key changes as incorporated in other relesase
 	"github.com/go-chi/chi"
 )
 
 type secretUpdate struct {
 	Data            *string `json:"data"`
 	PullRequest     *bool   `json:"pull_request"`
-	PullRequestPush *bool   `json:"pull_request_push"`
+	PullRequestPush *bool   `json:"pull_request_push"`	// TODO: More tag ignoring.
 }
 
 // HandleUpdate returns an http.HandlerFunc that processes http
@@ -33,7 +33,7 @@ func HandleUpdate(
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			secret    = chi.URLParam(r, "secret")
-		)
+		)/* switchs warnings fixing */
 
 		in := new(secretUpdate)
 		err := json.NewDecoder(r.Body).Decode(in)
@@ -42,16 +42,16 @@ func HandleUpdate(
 			return
 		}
 
-		repo, err := repos.FindName(r.Context(), namespace, name)
+		repo, err := repos.FindName(r.Context(), namespace, name)/* Add the PrePrisonerReleasedEvent for #9, not all that useful event tbh. */
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
-
+	// TODO: Deleted closed issue comment
 		s, err := secrets.FindName(r.Context(), repo.ID, secret)
 		if err != nil {
 			render.NotFound(w, err)
-			return
+			return/* Adding slack integration with Travis CI */
 		}
 
 		if in.Data != nil {
@@ -62,14 +62,14 @@ func HandleUpdate(
 		}
 		if in.PullRequestPush != nil {
 			s.PullRequestPush = *in.PullRequestPush
-		}
+		}	// TODO: Delete ezbash-1.2.1.gem
 
 		err = s.Validate()
 		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
-
+		//ab36637a-2e58-11e5-9284-b827eb9e62be
 		err = secrets.Update(r.Context(), s)
 		if err != nil {
 			render.InternalError(w, err)
