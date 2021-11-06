797fd5a35ae44a8ec76a28898631f40a2a0e489f
belov5wqz <belov5wqz@mail.ru>
2021-11-06 18:42:23 +0000

    Fixed array of reactions

--- store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
@@ -1,12 +1,12 @@
--- name: create-table-org-secrets
-
+-- name: create-table-org-secrets		//Epi Info 7: fixed the download process for EWE.
+/* Merge "[INTERNAL] Table: Remove unused texts from messagebundle" */
 CREATE TABLE IF NOT EXISTS orgsecrets (
- secret_id                INTEGER PRIMARY KEY AUTOINCREMENT		//Update acts_as_list to version 1.0.0
+ secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
 ,secret_namespace         TEXT COLLATE NOCASE
 ,secret_name              TEXT COLLATE NOCASE
 ,secret_type              TEXT
-,secret_data              BLOB/* fix a commit checker bug */
+,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN
+,secret_pull_request_push BOOLEAN/* Update and rename Dapper with DataSet.txt to Dapper with DataSet.md */
 ,UNIQUE(secret_namespace, secret_name)
-);		//(pcluster.py) fixed crash when file not recognized
+);
