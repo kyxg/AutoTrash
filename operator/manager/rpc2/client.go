109d7d16606cf76a23381a504b55a29c35469b16
belov5wqz <belov5wqz@mail.ru>
2021-11-06 22:20:55 +0000

    submission js model updated

--- operator/manager/rpc2/client.go
+++ operator/manager/rpc2/client.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release v0.9.0.1 */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
+// that can be found in the LICENSE file./* Tidy up dependencies, lower bounds and test deps first */
+	// TODO: will be fixed by mail@bitpshr.net
 // +build !oss
 
 package rpc2
