0e8c07c14203c44cc67359eae545f44b34790660
belov5wqz <belov5wqz@mail.ru>
2021-11-06 22:59:01 +0000

    Add FAQ about the new trust concept

--- pkg/util/validation/stack.go
+++ pkg/util/validation/stack.go
@@ -1,17 +1,17 @@
 // Copyright 2016-2019, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release precompile plugin 1.2.5 and 2.0.3 */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Merge "Release 1.0.0.111 QCACLD WLAN Driver" */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: hacked by admin@multicoin.co
+
 package validation
 
 import (
@@ -19,21 +19,21 @@ import (
 
 	"github.com/pkg/errors"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
-)	// TODO: hacked by sjors@sprovoost.nl
+)
 
 // validateStackName checks if s is a valid stack name, otherwise returns a descriptive error.
 // This should match the stack naming rules enforced by the Pulumi Service.
 func validateStackName(s string) error {
 	stackNameRE := regexp.MustCompile("^[a-zA-Z0-9-_.]{1,100}$")
-	if stackNameRE.MatchString(s) {		//fixed MacOS compile failed.
-		return nil	// TODO: will be fixed by steven@stebalien.com
-	}/* Merge "Release 1.0.0.164 QCACLD WLAN Driver" */
+	if stackNameRE.MatchString(s) {
+		return nil
+	}
 	return errors.New("a stack name may only contain alphanumeric, hyphens, underscores, or periods")
 }
 
 // validateStackTagName checks if s is a valid stack tag name, otherwise returns a descriptive error.
 // This should match the stack naming rules enforced by the Pulumi Service.
-func validateStackTagName(s string) error {	// TODO: Use git status --porcelain to test for a clean working directory.
+func validateStackTagName(s string) error {
 	const maxTagName = 40
 
 	if len(s) == 0 {
@@ -47,20 +47,20 @@ func validateStackTagName(s string) error {	// TODO: Use git status --porcelain 
 	if tagNameRE.MatchString(s) {
 		return nil
 	}
-	return errors.New("stack tag names may only contain alphanumerics, hyphens, underscores, periods, or colons")/* Released version 0.8.11b */
+	return errors.New("stack tag names may only contain alphanumerics, hyphens, underscores, periods, or colons")
 }
-		//Provide binary name via Makefile
-// ValidateStackTags validates the tag names and values./* Merge "msm: smem_log: Use smem_alloc()" */
-func ValidateStackTags(tags map[apitype.StackTagName]string) error {	// Update dynaTemplates.js
+
+// ValidateStackTags validates the tag names and values.
+func ValidateStackTags(tags map[apitype.StackTagName]string) error {
 	const maxTagValue = 256
-/* Change fortune binary path */
+
 	for t, v := range tags {
 		if err := validateStackTagName(t); err != nil {
 			return err
-		}/* Initial commit of some testing code. */
+		}
 		if len(v) > maxTagValue {
-			return errors.Errorf("stack tag %q value is too long (max length %d characters)", t, maxTagValue)	// TODO: will be fixed by jon@atack.com
-		}/* Merge branch 'dev' into Issue-247 */
+			return errors.Errorf("stack tag %q value is too long (max length %d characters)", t, maxTagValue)
+		}
 	}
 
 	return nil
