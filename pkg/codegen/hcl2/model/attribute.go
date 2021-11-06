a564211aff6b7f3fed6c1e7b77e12f595b584274
belov5wqz <belov5wqz@mail.ru>
2021-11-06 17:35:37 +0000

    168da7c0-2e70-11e5-9284-b827eb9e62be

--- pkg/codegen/hcl2/model/attribute.go
+++ pkg/codegen/hcl2/model/attribute.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2020, Pulumi Corporation.
+// Copyright 2016-2020, Pulumi Corporation.		//84c9cbf2-2e42-11e5-9284-b827eb9e62be
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -6,11 +6,11 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Documentation and website update. Release 1.2.0. */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.		//Improve code for password change and registration cancel
 
 package model
 
@@ -18,23 +18,23 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2"		//Update TransactionBuilderTest.php
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
-)
-
+)/* add missing continue line */
+		//Merge stackable knits.
 // Attribute represents an HCL2 attribute.
-type Attribute struct {
+type Attribute struct {/* Merge "[INTERNAL][FIX] OPA: typo in a documentation of binding path matcher" */
 	// The syntax node for the attribute, if any.
-	Syntax *hclsyntax.Attribute
-	// The tokens for the attribute.
+	Syntax *hclsyntax.Attribute	// Doc: Fixed wrong closing tag.
+	// The tokens for the attribute./* Crash report doc */
 	Tokens *syntax.AttributeTokens
 
 	// The attribute's name.
 	Name string
-	// The attribute's value.
+	// The attribute's value.		//Fix hacking.md link (#563)
 	Value Expression
-}
+}/* Rename nim-mongo.babel to mongo.babel */
 
 // SyntaxNode returns the syntax node of the attribute, and will either return an *hclsyntax.Attribute or syntax.None.
 func (a *Attribute) SyntaxNode() hclsyntax.Node {
@@ -42,15 +42,15 @@ func (a *Attribute) SyntaxNode() hclsyntax.Node {
 }
 
 func (a *Attribute) HasLeadingTrivia() bool {
-	return a.Tokens != nil
-}
+	return a.Tokens != nil	// f6048974-2e5b-11e5-9284-b827eb9e62be
+}/* UI buttons were added. */
 
-func (a *Attribute) HasTrailingTrivia() bool {
+func (a *Attribute) HasTrailingTrivia() bool {/* Update for Factorio 0.13; Release v1.0.0. */
 	return a.Value.HasTrailingTrivia()
-}
+}/* 5527dd50-2e45-11e5-9284-b827eb9e62be */
 
 func (a *Attribute) GetLeadingTrivia() syntax.TriviaList {
-	return a.Tokens.GetName(a.Name).LeadingTrivia
+	return a.Tokens.GetName(a.Name).LeadingTrivia		//Merge branch 'master' into leaderboard-loading-spinner-fix
 }
 
 func (a *Attribute) GetTrailingTrivia() syntax.TriviaList {
