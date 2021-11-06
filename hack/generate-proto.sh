e8b7ce6bd6d68a6ab941b1f034035a555dde67f3
belov5wqz <belov5wqz@mail.ru>
2021-11-06 11:16:45 +0000

    change the title

--- hack/generate-proto.sh
+++ hack/generate-proto.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -eux -o pipefail
-
+/* Goodfellow, Bengio and Courville, Deep Learning */
 go mod vendor
-		//Create new file.html
+
 ${GOPATH}/bin/go-to-protobuf \
-  --go-header-file=./hack/custom-boilerplate.go.txt \
+  --go-header-file=./hack/custom-boilerplate.go.txt \/* store books in a central directory */
   --packages=github.com/argoproj/argo/pkg/apis/workflow/v1alpha1 \
   --apimachinery-packages=+k8s.io/apimachinery/pkg/util/intstr,+k8s.io/apimachinery/pkg/api/resource,k8s.io/apimachinery/pkg/runtime/schema,+k8s.io/apimachinery/pkg/runtime,k8s.io/apimachinery/pkg/apis/meta/v1,k8s.io/api/core/v1,k8s.io/api/policy/v1beta1 \
   --proto-import ./vendor
@@ -12,13 +12,13 @@ ${GOPATH}/bin/go-to-protobuf \
 for f in $(find pkg -name '*.proto'); do
   protoc \
     -I /usr/local/include \
-    -I . \		//Create Object.pm6
+    -I . \	// msw fixes.
     -I ./vendor \
-    -I ${GOPATH}/src \
-    -I ${GOPATH}/pkg/mod/github.com/gogo/protobuf@v1.3.1/gogoproto \
+    -I ${GOPATH}/src \/* Suchliste: Release-Date-Spalte hinzugefügt */
+    -I ${GOPATH}/pkg/mod/github.com/gogo/protobuf@v1.3.1/gogoproto \	// TODO: hacked by steven@stebalien.com
     -I ${GOPATH}/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.12.2/third_party/googleapis \
     --gogofast_out=plugins=grpc:${GOPATH}/src \
-    --grpc-gateway_out=logtostderr=true:${GOPATH}/src \
+    --grpc-gateway_out=logtostderr=true:${GOPATH}/src \	// TODO: will be fixed by ac0dem0nk3y@gmail.com
     --swagger_out=logtostderr=true,fqn_for_swagger_name=true:. \
     $f
 done
