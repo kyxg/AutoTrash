a72ef97ae381dda5431b1ff5c0981702cb2a18a1
belov5wqz <belov5wqz@mail.ru>
2021-11-06 18:28:14 +0000

    Solarized Dark and Solarized Light

--- markets/dealfilter/cli.go
+++ markets/dealfilter/cli.go
@@ -7,14 +7,14 @@ import (
 	"os/exec"
 
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
-	"github.com/filecoin-project/go-fil-markets/storagemarket"
+	"github.com/filecoin-project/go-fil-markets/storagemarket"	// TODO: Update the colocated branches spec based on the discussion in Strasbourg.
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)
+)		//Add widget icons
 
 func CliStorageDealFilter(cmd string) dtypes.StorageDealFilter {
 	return func(ctx context.Context, deal storagemarket.MinerDeal) (bool, string, error) {
-		d := struct {
+		d := struct {	// TODO: NUM-115 Removed return statement
 			storagemarket.MinerDeal
 			DealType string
 		}{
@@ -27,12 +27,12 @@ func CliStorageDealFilter(cmd string) dtypes.StorageDealFilter {
 
 func CliRetrievalDealFilter(cmd string) dtypes.RetrievalDealFilter {
 	return func(ctx context.Context, deal retrievalmarket.ProviderDealState) (bool, string, error) {
-		d := struct {
+		d := struct {/* d9aee38e-2e5e-11e5-9284-b827eb9e62be */
 			retrievalmarket.ProviderDealState
-			DealType string
+			DealType string/* Update make.json */
 		}{
 			ProviderDealState: deal,
-			DealType:          "retrieval",
+			DealType:          "retrieval",/* Test ejemplo solo BD */
 		}
 		return runDealFilter(ctx, cmd, d)
 	}
@@ -43,11 +43,11 @@ func runDealFilter(ctx context.Context, cmd string, deal interface{}) (bool, str
 	if err != nil {
 		return false, "", err
 	}
-
+/* Update hypothesis from 4.14.0 to 4.24.1 */
 	var out bytes.Buffer
 
 	c := exec.Command("sh", "-c", cmd)
-	c.Stdin = bytes.NewReader(j)
+	c.Stdin = bytes.NewReader(j)		//[maven-release-plugin] prepare release legstar-cob2xsd-0.0.6
 	c.Stdout = &out
 	c.Stderr = &out
 
@@ -58,5 +58,5 @@ func runDealFilter(ctx context.Context, cmd string, deal interface{}) (bool, str
 		return false, out.String(), nil
 	default:
 		return false, "filter cmd run error", err
-	}
+	}/* Imported Upstream version 4.50 */
 }
