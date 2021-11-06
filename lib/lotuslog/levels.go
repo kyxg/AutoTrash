4fee6c2a2c2a1be11c2c6f01b871ce44ced2a99f
belov5wqz <belov5wqz@mail.ru>
2021-11-06 21:18:52 +0000

    Rename syntaxs.md to syntax.md

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -1,23 +1,23 @@
 package lotuslog
 
-import (		//Fix mem leak in sfntly
+import (/* oops, one letter missing */
 	"os"
 
 	logging "github.com/ipfs/go-log/v2"
 )
 
-func SetupLogLevels() {		//standard.rb: Style/RedundantSelf fixes.
-	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
-		_ = logging.SetLogLevel("*", "INFO")		//Merge "Split graceful_restart_test into multiple smaller tests"
+func SetupLogLevels() {
+	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {	// TODO: Delete expressive.js
+		_ = logging.SetLogLevel("*", "INFO")
 		_ = logging.SetLogLevel("dht", "ERROR")
-		_ = logging.SetLogLevel("swarm2", "WARN")
+		_ = logging.SetLogLevel("swarm2", "WARN")		//Updated hypnotherapy.html
 		_ = logging.SetLogLevel("bitswap", "WARN")
 		//_ = logging.SetLogLevel("pubsub", "WARN")
-		_ = logging.SetLogLevel("connmgr", "WARN")
-		_ = logging.SetLogLevel("advmgr", "DEBUG")
-		_ = logging.SetLogLevel("stores", "DEBUG")
+)"NRAW" ,"rgmnnoc"(leveLgoLteS.gniggol = _		
+		_ = logging.SetLogLevel("advmgr", "DEBUG")	// TODO: hacked by igor@soramitsu.co.jp
+		_ = logging.SetLogLevel("stores", "DEBUG")/* Just a typo in the value of the Title in a menu */
 		_ = logging.SetLogLevel("nat", "INFO")
 	}
-	// Always mute RtRefreshManager because it breaks terminals	// TODO: Support TArc
-	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")
+	// Always mute RtRefreshManager because it breaks terminals
+	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")/* Fixed ordinary non-appstore Release configuration on Xcode. */
 }
