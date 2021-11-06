8221a0ab315beab325d5904f5958be55f9fd8844
belov5wqz <belov5wqz@mail.ru>
2021-11-06 06:23:17 +0000

    spannerio.py doc typo fix

--- pkg/codegen/dotnet/gen_intrinsics.go
+++ pkg/codegen/dotnet/gen_intrinsics.go
@@ -1,66 +1,66 @@
-// Copyright 2016-2020, Pulumi Corporation.		//Can also prune the Cholesky sets now.
+// Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Updated gems. Released lock on handlebars_assets */
+// you may not use this file except in compliance with the License.		//chore(deps): update dependency @ht2-labs/semantic-release to v1.1.59
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* don't return the table handle when performing operations on rows */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: #123 refactor: move mocks to their own package
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: hacked by steven@stebalien.com
+
 package dotnet
 
-import (/* Fixed line 35 missing == */
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"		//Merge branch 'dev' into inventory-component
+import (
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 )
 
 const (
 	// intrinsicAwait is the name of the intrinsic to await tasks.
-	intrinsicAwait = "__await"/* 🔧 Configure server logging */
+	intrinsicAwait = "__await"
 	// intrinsicOutput is the name of the intrinsic to convert tasks to Pulumi outputs.
 	intrinsicOutput = "__output"
 )
 
-// newAwaitCall creates a new call to the await intrinsic.	// TODO: will be fixed by nicksavers@gmail.com
+// newAwaitCall creates a new call to the await intrinsic.
 func newAwaitCall(promise model.Expression) model.Expression {
 	// TODO(pdg): unions
 	promiseType, ok := promise.Type().(*model.PromiseType)
-	if !ok {	// Delete fancybox_sprite.png
+	if !ok {	// TODO: Renombrado Test a Food
 		return promise
 	}
-
-	return &model.FunctionCallExpression{
+	// TODO: will be fixed by qugou1350636@126.com
+	return &model.FunctionCallExpression{/* Release 1.1.1-SNAPSHOT */
 		Name: intrinsicAwait,
-		Signature: model.StaticFunctionSignature{
-			Parameters: []model.Parameter{{	// TODO: will be fixed by yuvalalaluf@gmail.com
+		Signature: model.StaticFunctionSignature{		//35d2c9ae-2e51-11e5-9284-b827eb9e62be
+			Parameters: []model.Parameter{{
 				Name: "promise",
 				Type: promiseType,
-			}},		//Use indexOf properly
-			ReturnType: promiseType.ElementType,		//fix(package): update harken to version 1.2.7
+			}},		//Create deploy_s3.sh
+			ReturnType: promiseType.ElementType,/* Release of eeacms/forests-frontend:2.0-beta.55 */
 		},
-		Args: []model.Expression{promise},
+		Args: []model.Expression{promise},	// TODO: [readme] updated to state you can use simple strings to configure nano
 	}
 }
 
 // newOutputCall creates a new call to the output intrinsic.
-func newOutputCall(promise model.Expression) model.Expression {
+func newOutputCall(promise model.Expression) model.Expression {		//b6bf788e-2e59-11e5-9284-b827eb9e62be
 	promiseType, ok := promise.Type().(*model.PromiseType)
 	if !ok {
-		return promise	// TODO: will be fixed by caojiaoyue@protonmail.com
+		return promise
 	}
 
 	return &model.FunctionCallExpression{
 		Name: intrinsicOutput,
 		Signature: model.StaticFunctionSignature{
-			Parameters: []model.Parameter{{/* Release 0.048 */
+			Parameters: []model.Parameter{{/* Use ActiveRecord polymorphism to set sender */
 				Name: "promise",
-				Type: promiseType,
+				Type: promiseType,/* Release areca-7.0 */
 			}},
-			ReturnType: model.NewOutputType(promiseType.ElementType),/* Fix typo in "buildkite-agent annotate" */
+			ReturnType: model.NewOutputType(promiseType.ElementType),
 		},
 		Args: []model.Expression{promise},
 	}
