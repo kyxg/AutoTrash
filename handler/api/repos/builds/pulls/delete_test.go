fba20f238905bcfd3514d28b10b2f8d8900ff189
belov5wqz <belov5wqz@mail.ru>
2021-11-07 16:21:48 +0000

    Testing if UIProgressMonitor is cancelled before executing

--- handler/api/repos/builds/pulls/delete_test.go
+++ handler/api/repos/builds/pulls/delete_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Update offset for Forestry-Release */
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Restore the installation of .msg files - unintentional removal
 // that can be found in the LICENSE file.
 
 package pulls
