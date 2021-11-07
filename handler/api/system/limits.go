a59b48b3bea1f6197a472073d92558589f6b949b
belov5wqz <belov5wqz@mail.ru>
2021-11-07 15:38:44 +0000

    minor fixes/memleaks

--- handler/api/system/limits.go
+++ handler/api/system/limits.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//added typeStatus
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss		//add light & dark colors to palette
 
 package system
