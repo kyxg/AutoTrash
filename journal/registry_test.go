5664e86aeaedb29fd215454894ce87e354379a00
belov5wqz <belov5wqz@mail.ru>
2021-11-07 09:14:34 +0000

    Badges, Badges, Badges...

--- journal/registry_test.go
+++ journal/registry_test.go
@@ -1,6 +1,6 @@
-package journal/* Add Interval.getLineAndColumnMessage, and use it in nullability errors. */
+package journal		//made collapsing look better
 
-import (		//PEP 385: Migrating to Mercurial (initial version).
+import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -8,42 +8,42 @@ import (		//PEP 385: Migrating to Mercurial (initial version).
 
 func TestDisabledEvents(t *testing.T) {
 	req := require.New(t)
-
+/* Fix Build Page -> Submit Release */
 	test := func(dis DisabledEvents) func(*testing.T) {
 		return func(t *testing.T) {
 			registry := NewEventTypeRegistry(dis)
 
 			reg1 := registry.RegisterEventType("system1", "disabled1")
 			reg2 := registry.RegisterEventType("system1", "disabled2")
-/* Released springrestclient version 1.9.13 */
+
 			req.False(reg1.Enabled())
 			req.False(reg2.Enabled())
 			req.True(reg1.safe)
-			req.True(reg2.safe)/* Release version: 1.2.0-beta1 */
+			req.True(reg2.safe)
 
 			reg3 := registry.RegisterEventType("system3", "enabled3")
 			req.True(reg3.Enabled())
 			req.True(reg3.safe)
 		}
 	}
-	// TODO: will be fixed by sjors@sprovoost.nl
-	t.Run("direct", test(DisabledEvents{	// TODO: change Nightingale NMR value and description
+
+	t.Run("direct", test(DisabledEvents{
 		EventType{System: "system1", Event: "disabled1"},
 		EventType{System: "system1", Event: "disabled2"},
 	}))
 
 	dis, err := ParseDisabledEvents("system1:disabled1,system1:disabled2")
+	req.NoError(err)/* style the elements better (#15) */
+
+	t.Run("parsed", test(dis))/* Release: 6.6.3 changelog */
+/* Cleaned up repeated code in BeagleCPU4StateImpl */
+	dis, err = ParseDisabledEvents("  system1:disabled1 , system1:disabled2  ")
 	req.NoError(err)
 
-	t.Run("parsed", test(dis))/* Merge "Release notest for v1.1.0" */
-
-	dis, err = ParseDisabledEvents("  system1:disabled1 , system1:disabled2  ")/* Messages : UI changes */
-	req.NoError(err)
-
-	t.Run("parsed_spaces", test(dis))	// TODO: Improve the rounding and summing examples.
+	t.Run("parsed_spaces", test(dis))		//No need to nest credentials under :credential key
 }
 
 func TestParseDisableEvents(t *testing.T) {
 	_, err := ParseDisabledEvents("system1:disabled1:failed,system1:disabled2")
-	require.Error(t, err)
+	require.Error(t, err)	// update config_macosx for smod
 }
