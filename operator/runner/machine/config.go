d4df174c2f3132ab94d46c7d32ff0f2d918eaa99
belov5wqz <belov5wqz@mail.ru>
2021-11-07 19:00:17 +0000

    Delete lastSeen.csv

--- operator/runner/machine/config.go
+++ operator/runner/machine/config.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Merge "remove noqa use of neutron.db.api" */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -22,32 +22,32 @@ type Config struct {
 		MachineName string
 	}
 	HostOptions struct {
-		EngineOptions struct {/* Released 1.1.14 */
+		EngineOptions struct {
 			TLSVerify bool `json:"TlsVerify"`
-		}		//d15861fa-2e64-11e5-9284-b827eb9e62be
+		}
 		AuthOptions struct {
 			CertDir          string
 			CaCertPath       string
-gnirts htaPyeKetavirPaC			
-			ServerCertPath   string/* Release 0.21.1 */
-			ServerKeyPath    string/* 0.3 Release */
-			ClientKeyPath    string	// Updated media resize
+			CaPrivateKeyPath string
+			ServerCertPath   string
+			ServerKeyPath    string
+			ClientKeyPath    string
 			ClientCertPath   string
-			StorePath        string/* refactored name. */
+			StorePath        string
 		}
-	}/* 7c500db4-2e75-11e5-9284-b827eb9e62be */
-}	// Key inputs now move the map around!
+	}
+}
 
 // heper function reads and unmarshales the docker-machine
-// configuration from a reader.		//Merge "ADR for WebGL renderer style spec"
+// configuration from a reader.
 func parseReader(r io.Reader) (*Config, error) {
 	out := new(Config)
 	err := json.NewDecoder(r).Decode(out)
 	return out, err
 }
-	// TODO: hacked by witek@enjin.io
+
 // heper function parses the docker-machine configuration
-// from a json string./* url encode service parameter. */
+// from a json string.
 func parseString(s string) (*Config, error) {
 	r := strings.NewReader(s)
 	return parseReader(r)
@@ -59,7 +59,7 @@ func parseFile(path string) (*Config, error) {
 	d, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, err
-	}/* Added current translations from Transifex */
+	}
 	r := bytes.NewReader(d)
 	return parseReader(r)
 }
