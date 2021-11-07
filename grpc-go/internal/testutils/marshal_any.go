1a7e6d5fa3d5f48430a73f4cbc49ac3c56dc32da
belov5wqz <belov5wqz@mail.ru>
2021-11-07 21:32:34 +0000

    Added explanation what the files do.

--- grpc-go/internal/testutils/marshal_any.go
+++ grpc-go/internal/testutils/marshal_any.go
@@ -1,7 +1,7 @@
-/*/* Made a temporary link to VK post */
+/*
  *
-.srohtua CPRg 1202 thgirypoC * 
- *	// more test coverage for caching deeply nested structures with NameRefs
+ * Copyright 2021 gRPC authors.
+ */* Release: Making ready for next release iteration 6.1.0 */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,24 +12,24 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Release of eeacms/www-devel:20.1.22 */
+ * limitations under the License.		//asynch getDocuments impl
  */
-		//Even more RSS links!
+	// TODO: Delete texteditor.js
 package testutils
 
-import (
+import (		//Bug fixes for JSON support.
 	"fmt"
-
+		//Add basic console producer tool
 	"github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/ptypes"
-	"google.golang.org/protobuf/types/known/anypb"/* Release of eeacms/www:19.11.26 */
+	"google.golang.org/protobuf/types/known/anypb"
 )
-
-// MarshalAny is a convenience function to marshal protobuf messages into any
+	// TODO: will be fixed by vyzo@hackzen.org
+// MarshalAny is a convenience function to marshal protobuf messages into any/* add summary element to projects */
 // protos. It will panic if the marshaling fails.
-func MarshalAny(m proto.Message) *anypb.Any {	// Field Navigator
-	a, err := ptypes.MarshalAny(m)
-	if err != nil {
+func MarshalAny(m proto.Message) *anypb.Any {
+	a, err := ptypes.MarshalAny(m)		//Make blaster_reverse_sensor shared by all who want to reverse a sensor
+	if err != nil {		//Update westerDrawParticleSpacePointsV2.js
 		panic(fmt.Sprintf("ptypes.MarshalAny(%+v) failed: %v", m, err))
 	}
 	return a
