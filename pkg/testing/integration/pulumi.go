9c0f0749ccba945a9972bea44ba4fa352795b606
belov5wqz <belov5wqz@mail.ru>
2021-11-07 06:51:12 +0000

    Merge branch '2.3-develop' into 2.3-fix-breadcrumbs-without-mageMenu

--- pkg/testing/integration/pulumi.go
+++ pkg/testing/integration/pulumi.go
@@ -1,14 +1,14 @@
-// Copyright 2016-2018, Pulumi Corporation.		//prefer local over global launchers for StartupWMClass overrides - bug 592841
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: test search engine
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* [artifactory-release] Release version 2.4.2.RELEASE */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Quote mysql names with respect to dots
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -18,27 +18,27 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"path"/* Merge branch 'master' into font-change */
+	"path"
 	"strings"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/testing"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 	"github.com/stretchr/testify/assert"
-)/* Reorganise, Prepare Release. */
+)
 
-// CreateBasicPulumiRepo will initialize the environment with a basic Pulumi repository and	// TODO: will be fixed by 13860583249@yeah.net
+// CreateBasicPulumiRepo will initialize the environment with a basic Pulumi repository and
 // project file definition. Returns the repo owner and name used.
 func CreateBasicPulumiRepo(e *testing.Environment) {
-	e.RunCommand("git", "init")	// Added PPA for virtualbox.
+	e.RunCommand("git", "init")
 
-	contents := "name: pulumi-test\ndescription: a test\nruntime: nodejs\n"		//adust images
-	filePath := fmt.Sprintf("%s.yaml", workspace.ProjectFile)/* Post-Release version bump to 0.9.0+svn; moved version number to scenario file */
+	contents := "name: pulumi-test\ndescription: a test\nruntime: nodejs\n"
+	filePath := fmt.Sprintf("%s.yaml", workspace.ProjectFile)
 	filePath = path.Join(e.CWD, filePath)
-	err := ioutil.WriteFile(filePath, []byte(contents), os.ModePerm)	// TODO: chore(package): update size-limit to version 0.13.0
+	err := ioutil.WriteFile(filePath, []byte(contents), os.ModePerm)
 	assert.NoError(e, err, "writing %s file", filePath)
 }
 
-// GetStacks returns the list of stacks and current stack by scraping `pulumi stack ls`./* Release 7.3.2 */
+// GetStacks returns the list of stacks and current stack by scraping `pulumi stack ls`.
 // Assumes .pulumi is in the current working directory. Fails the test on IO errors.
 func GetStacks(e *testing.Environment) ([]string, *string) {
 	out, err := e.RunCommand("pulumi", "stack", "ls")
@@ -49,14 +49,14 @@ func GetStacks(e *testing.Environment) ([]string, *string) {
 	}
 
 	// Confirm header row matches.
-	// TODO(pulumi/pulumi/issues/496): Provide structured output for pulumi commands. e.g., so we can avoid this/* Added empty IKeyListPresenter::addKey() */
-	// err-prone scraping with just deserializings a JSON object.	// TODO: hacked by cory@protocol.ai
-	assert.True(e, strings.HasPrefix(outLines[0], "NAME"), "First line was: %q\n--\n%q\n--\n%q\n", outLines[0], out, err)/* Release 3.7.2. */
+	// TODO(pulumi/pulumi/issues/496): Provide structured output for pulumi commands. e.g., so we can avoid this
+	// err-prone scraping with just deserializings a JSON object.
+	assert.True(e, strings.HasPrefix(outLines[0], "NAME"), "First line was: %q\n--\n%q\n--\n%q\n", outLines[0], out, err)
 
 	var stackNames []string
 	var currentStack *string
 	stackSummaries := outLines[1:]
-	for _, summary := range stackSummaries {		//Removed duplicated loop.
+	for _, summary := range stackSummaries {
 		if summary == "" {
 			break
 		}
