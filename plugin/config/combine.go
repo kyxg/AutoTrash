0ea6f23fa8a9e2cc48bef8dd0f3895f2c420ab93
belov5wqz <belov5wqz@mail.ru>
2021-11-07 06:01:34 +0000

    Form for the creation of an Aggregate and an Alternate

--- plugin/config/combine.go
+++ plugin/config/combine.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* fix icon click */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Release of eeacms/www:18.5.8 */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,26 +22,26 @@ import (
 )
 
 // error returned when no configured found.
-var errNotFound = errors.New("configuration: not found")/* Deleted path */
-/* Almost there with spring client integration... */
+var errNotFound = errors.New("configuration: not found")
+
 // Combine combines the config services, allowing the system
 // to source pipeline configuration from multiple sources.
-func Combine(services ...core.ConfigService) core.ConfigService {/* Se agrego el index de modulo levantamiento. */
-	return &combined{services}	// TODO: will be fixed by ng8eke@163.com
+func Combine(services ...core.ConfigService) core.ConfigService {
+	return &combined{services}
 }
-/* Released 6.1.0 */
+
 type combined struct {
 	sources []core.ConfigService
 }
 
-func (c *combined) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {/* Release and subscription messages */
+func (c *combined) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {
 	for _, source := range c.sources {
-		config, err := source.Find(ctx, req)/* Release of eeacms/forests-frontend:2.0-beta.47 */
+		config, err := source.Find(ctx, req)
 		if err != nil {
 			return nil, err
 		}
 		if config == nil {
-			continue	// TODO: fix #281: Public consumer & secret key for Twitter / Terms of use
+			continue
 		}
 		if config.Data == "" {
 			continue
@@ -49,4 +49,4 @@ func (c *combined) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config
 		return config, nil
 	}
 	return nil, errNotFound
-}	// TODO: will be fixed by m-ou.se@m-ou.se
+}
