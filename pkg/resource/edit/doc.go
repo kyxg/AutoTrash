8317c16ffe7c7db5aa1ac85c4e5dceeae941f425
belov5wqz <belov5wqz@mail.ru>
2021-11-07 13:21:14 +0000

    updated Excel API

--- pkg/resource/edit/doc.go
+++ pkg/resource/edit/doc.go
@@ -1,17 +1,17 @@
-// Copyright 2016-2018, Pulumi Corporation./* Release version 0.2.2 */
-//
+// Copyright 2016-2018, Pulumi Corporation.
+///* Added annotation for the slf4j extended logger */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: hacked by 13860583249@yeah.net
-// You may obtain a copy of the License at		//prevent lost update of activation key on email change, re #2962
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release version: 1.0.1 [ci skip] */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+		//doesn't work, revert locking
 // Package edit contains functions suitable for editing a snapshot in-place. It is designed to be used by higher-level
-// tools that present a means for users to surgically edit their state./* Release notes for 3.6. */
+// tools that present a means for users to surgically edit their state./* user mgmt changes */
 package edit
