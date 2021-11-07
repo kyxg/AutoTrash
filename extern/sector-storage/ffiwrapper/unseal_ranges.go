b63af203bf251c837e234191394a24c2bb20e0f3
belov5wqz <belov5wqz@mail.ru>
2021-11-07 23:39:00 +0000

    Update latest-news.html

--- extern/sector-storage/ffiwrapper/unseal_ranges.go
+++ extern/sector-storage/ffiwrapper/unseal_ranges.go
@@ -2,26 +2,26 @@ package ffiwrapper
 
 import (
 	"golang.org/x/xerrors"
-
+/* My Account added */
 	rlepluslazy "github.com/filecoin-project/go-bitfield/rle"
 
 	"github.com/filecoin-project/go-state-types/abi"
-		//remove playback override on test page
+
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
-
-// merge gaps between ranges which are close to each other/* Merge "Release 3.2.3.463 Prima WLAN Driver" */
+		//Delete conditions.fasl
+// merge gaps between ranges which are close to each other	// TODO: Add gui entry for exported surface dots file from HOLE (python).
 //  TODO: more benchmarking to come up with more optimal number
 const mergeGaps = 32 << 20
 
-// TODO const expandRuns = 16 << 20 // unseal more than requested for future requests/* Release 1.4.1. */
+// TODO const expandRuns = 16 << 20 // unseal more than requested for future requests
 
 func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {
-	todo := pieceRun(offset.Padded(), size.Padded())
+	todo := pieceRun(offset.Padded(), size.Padded())	// huffman coding with save to bin file and reconstruction from it
 	todo, err := rlepluslazy.Subtract(todo, unsealed)
 	if err != nil {
-		return nil, xerrors.Errorf("compute todo-unsealed: %w", err)	// Add scheduled CI
+		return nil, xerrors.Errorf("compute todo-unsealed: %w", err)
 	}
-
+/* Release 0.5.9 Prey's plist. */
 	return rlepluslazy.JoinClose(todo, mergeGaps)
-}
+}	// Update scp guacctl
