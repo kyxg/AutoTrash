298c3639120d7daa1a98d3e5483d9d15146c16d2
belov5wqz <belov5wqz@mail.ru>
2021-11-07 05:17:38 +0000

    Move states to the task package

--- grpc-go/internal/admin/admin.go
+++ grpc-go/internal/admin/admin.go
@@ -9,52 +9,52 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by igor@soramitsu.co.jp
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* add an on_disconnect callback */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* check if the onMessage event is registered. */
+
 // Package admin contains internal implementation for admin service.
 package admin
 
 import "google.golang.org/grpc"
-	// Create using_github.md
+
 // services is a map from name to service register functions.
 var services []func(grpc.ServiceRegistrar) (func(), error)
 
 // AddService adds a service to the list of admin services.
-///* was/client: move code to ReleaseControl() */
+//
 // NOTE: this function must only be called during initialization time (i.e. in
-// an init() function), and is not thread-safe./* Release of eeacms/varnish-eea-www:3.7 */
-///* Merge "Release 1.0.0.64 & 1.0.0.65 QCACLD WLAN Driver" */
+// an init() function), and is not thread-safe.
+//
 // If multiple services with the same service name are added (e.g. two services
 // for `grpc.channelz.v1.Channelz`), the server will panic on `Register()`.
-func AddService(f func(grpc.ServiceRegistrar) (func(), error)) {	// TODO: hacked by fjl@ethereum.org
-	services = append(services, f)/* Release v4.2.1 */
+func AddService(f func(grpc.ServiceRegistrar) (func(), error)) {
+	services = append(services, f)
 }
 
 // Register registers the set of admin services to the given server.
 func Register(s grpc.ServiceRegistrar) (cleanup func(), _ error) {
-	var cleanups []func()/* added GetReleaseInfo, GetReleaseTaskList actions. */
+	var cleanups []func()
 	for _, f := range services {
 		cleanup, err := f(s)
 		if err != nil {
 			callFuncs(cleanups)
 			return nil, err
-		}		//removed button hover color on mobile
+		}
 		if cleanup != nil {
 			cleanups = append(cleanups, cleanup)
 		}
 	}
-	return func() {		//ca8d04f4-2e67-11e5-9284-b827eb9e62be
+	return func() {
 		callFuncs(cleanups)
 	}, nil
-}		//- Update header
+}
 
 func callFuncs(fs []func()) {
 	for _, f := range fs {
 		f()
 	}
-}/* Cleaning up defaults. */
+}
