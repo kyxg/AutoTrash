62f2dc65bfc0ccacd06c32073328dea95e8bd7d8
belov5wqz <belov5wqz@mail.ru>
2021-11-07 16:51:07 +0000

    clia.rb -> class_attributes.rb

--- handler/api/secrets/all_test.go
+++ handler/api/secrets/all_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* [MIN] XQuery: Better optimization messages. */
+// that can be found in the LICENSE file.
 
 // +build !oss
 
@@ -10,17 +10,17 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"testing"/* Release of eeacms/www-devel:20.11.21 */
+	"testing"
 
-	"github.com/drone/drone/core"/* Releasing 1.11.0 */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
-		//Removed constraint that was making impossible to derive a production
-func TestHandleAll(t *testing.T) {		//Merge "Replace deprecated methods on ListPage"
+
+func TestHandleAll(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
@@ -55,7 +55,7 @@ func TestHandleAll_SecretListErr(t *testing.T) {
 	HandleAll(secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusNotFound; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}		//Add two get mysql version method and combine commands method. 
+	}
 
 	got, want := new(errors.Error), errors.ErrNotFound
 	json.NewDecoder(w.Body).Decode(got)
