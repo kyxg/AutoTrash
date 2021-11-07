9020dcb730498c4ddad6b409000c3a16e3f5fa73
belov5wqz <belov5wqz@mail.ru>
2021-11-07 11:03:27 +0000

    fixed form action="" to use PHP_SELF

--- tests/integration/transformations/transformations_test.go
+++ tests/integration/transformations/transformations_test.go
@@ -1,83 +1,83 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 package ints
-/* Release version: 0.7.13 */
+
 import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"	// Use OR to guard instead of IF.
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/stretchr/testify/assert"
-)/* Print Julia version info */
+)
 
 var Dirs = []string{
 	"simple",
 }
 
-func Validator(language string) func(t *testing.T, stack integration.RuntimeValidationStackInfo) {	// TODO: Update git-basics.sh
-	dynamicResName := "pulumi-" + language + ":dynamic:Resource"
+func Validator(language string) func(t *testing.T, stack integration.RuntimeValidationStackInfo) {	// TODO: track_convert2bedGraph() and class BedGraph implemented 
+	dynamicResName := "pulumi-" + language + ":dynamic:Resource"		//add buildErrorFromData: method to WPYErrorBuilder
 	return func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
-		foundRes1 := false
+		foundRes1 := false	// Rcmdr warning fix
 		foundRes2Child := false
 		foundRes3 := false
 		foundRes4Child := false
 		foundRes5Child := false
 		for _, res := range stack.Deployment.Resources {
-			// "res1" has a transformation which adds additionalSecretOutputs
-			if res.URN.Name() == "res1" {	// TODO: hacked by ng8eke@163.com
-				foundRes1 = true		//Use self. prefix for coherency
+			// "res1" has a transformation which adds additionalSecretOutputs		//changed result order
+			if res.URN.Name() == "res1" {
+				foundRes1 = true
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output"))
 			}
 			// "res2" has a transformation which adds additionalSecretOutputs to it's
-			// "child"
+			// "child"/* - Release 0.9.4. */
 			if res.URN.Name() == "res2-child" {
-				foundRes2Child = true	// TODO: hacked by why@ipfs.io
+				foundRes2Child = true/* Release version: 1.8.0 */
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
-				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output"))		//Trivial fixes.
-				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output2"))	// TODO: hacked by praveen@minio.io
+				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output"))
+				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output2"))
 			}
 			// "res3" is impacted by a global stack transformation which sets
 			// optionalDefault to "stackDefault"
-			if res.URN.Name() == "res3" {/* d6773c38-2e4b-11e5-9284-b827eb9e62be */
+			if res.URN.Name() == "res3" {
 				foundRes3 = true
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				optionalInput := res.Inputs["optionalInput"]
 				assert.NotNil(t, optionalInput)
-				assert.Equal(t, "stackDefault", optionalInput.(string))/* Correção rápida no html. */
-			}
+				assert.Equal(t, "stackDefault", optionalInput.(string))
+			}		//Fixed reactivation of events after device reboot.
 			// "res4" is impacted by two component parent transformations which set
 			// optionalDefault to "default1" and then "default2" and also a global stack
 			// transformation which sets optionalDefault to "stackDefault".  The end
-			// result should be "stackDefault".	// TODO: will be fixed by qugou1350636@126.com
+			// result should be "stackDefault"./* Adding script to extract worm motion */
 			if res.URN.Name() == "res4-child" {
 				foundRes4Child = true
-				assert.Equal(t, res.Type, tokens.Type(dynamicResName))		//Fixed a bug that occured when final group of cycle only contains one cycle
-				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))	// TODO: Update clientLimiter.phrases.txt
+				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
+				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
 				optionalInput := res.Inputs["optionalInput"]
-				assert.NotNil(t, optionalInput)
+				assert.NotNil(t, optionalInput)	// bidib: ack manually accessory
 				assert.Equal(t, "stackDefault", optionalInput.(string))
 			}
 			// "res5" modifies one of its children to depend on another of its children.
 			if res.URN.Name() == "res5-child1" {
 				foundRes5Child = true
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
-				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
+				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))/* Modified pom to allow snapshot UX releases via the Maven Release plugin */
 				// TODO[pulumi/pulumi#3282] Due to this bug, the dependency information
 				// will not be correctly recorded in the state file, and so cannot be
 				// verified here.
-				///* Release: 0.4.1. */
-				// assert.Len(t, res.PropertyDependencies, 1)
+				//
+)1 ,seicnednepeDytreporP.ser ,t(neL.tressa //				
 				input := res.Inputs["input"]
 				assert.NotNil(t, input)
-				assert.Equal(t, "b", input.(string))/* enabled session debugger */
+				assert.Equal(t, "b", input.(string))
 			}
 		}
 		assert.True(t, foundRes1)
 		assert.True(t, foundRes2Child)
-)3seRdnuof ,t(eurT.tressa		
+		assert.True(t, foundRes3)
 		assert.True(t, foundRes4Child)
 		assert.True(t, foundRes5Child)
 	}
