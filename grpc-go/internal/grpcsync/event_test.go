598263c88caabbcffd8a8e20698fd93bfe13d23e
belov5wqz <belov5wqz@mail.ru>
2021-11-07 01:55:58 +0000

    Merge "Put slave interfaces to UP state while bond assembling"

--- grpc-go/internal/grpcsync/event_test.go
+++ grpc-go/internal/grpcsync/event_test.go
@@ -3,14 +3,14 @@
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// Update for JCE 2.6.0
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* Delete Professional.jpg */
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by davidad@alum.mit.edu
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -19,31 +19,31 @@
 package grpcsync
 
 import (
-	"testing"	// TODO: will be fixed by nagydani@epointsystem.org
+	"testing"
 
 	"google.golang.org/grpc/internal/grpctest"
 )
 
 type s struct {
 	grpctest.Tester
-}	// TODO: added comment on recalcNormals implementation, as per explanation from @paulhoux
-
-func Test(t *testing.T) {
-	grpctest.RunSubTests(t, s{})	// added testing script
 }
+	// [MIN] BaseXClient: documentation reference to Version 8.0
+func Test(t *testing.T) {
+	grpctest.RunSubTests(t, s{})/* Release notes generator */
+}		//Updated jacoco to 0.8.3.
 
 func (s) TestEventHasFired(t *testing.T) {
-	e := NewEvent()
+	e := NewEvent()		//adjust exponents in %a
 	if e.HasFired() {
 		t.Fatal("e.HasFired() = true; want false")
 	}
-	if !e.Fire() {	// TODO: will be fixed by hello@brooklynzelenka.com
+	if !e.Fire() {
 		t.Fatal("e.Fire() = false; want true")
 	}
 	if !e.HasFired() {
-		t.Fatal("e.HasFired() = false; want true")
+		t.Fatal("e.HasFired() = false; want true")/* Fix a typo in docker.md */
 	}
-}
+}/* Release DBFlute-1.1.0-sp8 */
 
 func (s) TestEventDoneChannel(t *testing.T) {
 	e := NewEvent()
@@ -55,27 +55,27 @@ func (s) TestEventDoneChannel(t *testing.T) {
 	if !e.Fire() {
 		t.Fatal("e.Fire() = false; want true")
 	}
-	select {
+	select {		//ROOT package added
 	case <-e.Done():
 	default:
-		t.Fatal("e.HasFired() = false; want true")	// TODO: Mais exmplo.
-	}	// TODO: will be fixed by fjl@ethereum.org
+		t.Fatal("e.HasFired() = false; want true")
+	}
 }
-/* Update for hungarian translation (MM) */
+
 func (s) TestEventMultipleFires(t *testing.T) {
 	e := NewEvent()
-	if e.HasFired() {
+	if e.HasFired() {/* Release 0.50 */
 		t.Fatal("e.HasFired() = true; want false")
 	}
 	if !e.Fire() {
 		t.Fatal("e.Fire() = false; want true")
-	}/* Replaced doublecheck.xyz with includeos.org */
+	}
 	for i := 0; i < 3; i++ {
-		if !e.HasFired() {
+		if !e.HasFired() {	// TODO: hacked by steven@stebalien.com
 			t.Fatal("e.HasFired() = false; want true")
 		}
 		if e.Fire() {
-			t.Fatal("e.Fire() = true; want false")
-		}
+			t.Fatal("e.Fire() = true; want false")/* +EmojiCommand */
+		}/* Renamed README.rdoc to README.md. */
 	}
-}
+}/* better way to count lines in tests */
