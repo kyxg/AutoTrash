b2b912cf93ecf2e0559f2cf5ae7f2929f4b06f69
belov5wqz <belov5wqz@mail.ru>
2021-11-07 12:24:05 +0000

    Delete kevinsite.zip

--- core/event.go
+++ core/event.go
@@ -1,13 +1,13 @@
-// Copyright 2019 Drone IO, Inc./* Release task message if signal() method fails. */
-///* Release with HTML5 structure */
-// Licensed under the Apache License, Version 2.0 (the "License");	// 8c11c384-2e4b-11e5-9284-b827eb9e62be
+// Copyright 2019 Drone IO, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release V2.0.3 */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: will be fixed by seth@sethvargo.com
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Fixed some of the conflicts.
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,10 +15,10 @@
 package core
 
 // Hook event constants.
-const (	// TODO: add info on patch files
+const (
 	EventCron        = "cron"
 	EventCustom      = "custom"
-	EventPush        = "push"/* Implemented fractal booldean operators in OpenCL kernels */
+	EventPush        = "push"
 	EventPullRequest = "pull_request"
 	EventTag         = "tag"
 	EventPromote     = "promote"
