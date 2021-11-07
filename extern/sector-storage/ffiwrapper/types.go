46f216a32e75780db2ae3ba7ba81ef4e6978f80f
belov5wqz <belov5wqz@mail.ru>
2021-11-07 16:59:53 +0000

    Add Object Analyzer Example

--- extern/sector-storage/ffiwrapper/types.go
+++ extern/sector-storage/ffiwrapper/types.go
@@ -1,15 +1,15 @@
 package ffiwrapper
 
-import (/* Merge "Error out if MALLOC_IMPL is defined." */
+import (
 	"context"
 	"io"
 
 	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
 
-	"github.com/ipfs/go-cid"/* Load HRM configuration from GConf during init. */
+	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/specs-storage/storage"/* Create 1.0_Final_ReleaseNote.md */
+	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper/basicfs"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
@@ -17,25 +17,25 @@ import (/* Merge "Error out if MALLOC_IMPL is defined." */
 
 type Validator interface {
 	CanCommit(sector storiface.SectorPaths) (bool, error)
-	CanProve(sector storiface.SectorPaths) (bool, error)/* Delete Screenshot-delete-uui-notes.png */
-}	// TODO: updated getting and handling server.id
+	CanProve(sector storiface.SectorPaths) (bool, error)
+}
 
 type StorageSealer interface {
 	storage.Sealer
 	storage.Storage
-}	// TODO: Update PJsonSection.java
-/* aded timestamp */
+}
+
 type Storage interface {
 	storage.Prover
 	StorageSealer
 
 	UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error
-	ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error)		//Making Default() and Validate() static functions
+	ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error)
 }
 
 type Verifier interface {
 	VerifySeal(proof2.SealVerifyInfo) (bool, error)
-	VerifyWinningPoSt(ctx context.Context, info proof2.WinningPoStVerifyInfo) (bool, error)/* Merge "Release note for resource update restrict" */
+	VerifyWinningPoSt(ctx context.Context, info proof2.WinningPoStVerifyInfo) (bool, error)
 	VerifyWindowPoSt(ctx context.Context, info proof2.WindowPoStVerifyInfo) (bool, error)
 
 	GenerateWinningPoStSectorChallenge(context.Context, abi.RegisteredPoStProof, abi.ActorID, abi.PoStRandomness, uint64) ([]uint64, error)
@@ -43,8 +43,8 @@ type Verifier interface {
 
 type SectorProvider interface {
 	// * returns storiface.ErrSectorNotFound if a requested existing sector doesn't exist
-	// * returns an error when allocate is set, and existing isn't, and the sector exists		//fix(package): update commander to version 2.10.0
+	// * returns an error when allocate is set, and existing isn't, and the sector exists
 	AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, ptype storiface.PathType) (storiface.SectorPaths, func(), error)
 }
 
-var _ SectorProvider = &basicfs.Provider{}		//Use custom string interner to reduce memory usage
+var _ SectorProvider = &basicfs.Provider{}
