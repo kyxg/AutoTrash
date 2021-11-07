8ac52fa47f6faa88970ea1271bbd5a9c8478f190
belov5wqz <belov5wqz@mail.ru>
2021-11-07 15:43:57 +0000

    Update: version 1.1

--- tests/integration/delete_before_create/step4/index.ts
+++ tests/integration/delete_before_create/step4/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: hacked by nagydani@epointsystem.org
+
 import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";
+import { Resource } from "./resource";	// TODO: hacked by steven@stebalien.com
 
 // Setup for the next test.
-const a = new Resource("base", { uniqueKey: 1, state: 100 });	// TODO: avatar_rick
-const b = new Resource("base-2", { uniqueKey: 2, state: 100 });/* Merge "Enable java test as voting on monasca-api" */
+const a = new Resource("base", { uniqueKey: 1, state: 100 });	// TODO: hacked by boringland@protonmail.ch
+const b = new Resource("base-2", { uniqueKey: 2, state: 100 });
 const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });
