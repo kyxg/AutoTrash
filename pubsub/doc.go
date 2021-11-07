54f50a4430548488bbe8bc6f04ffd8c44b206eac
belov5wqz <belov5wqz@mail.ru>
2021-11-07 07:24:02 +0000

    More link to RTD testing.

--- pubsub/doc.go
+++ pubsub/doc.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
-///* added grep_SPCTools to filter messages for SPCTools */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Added file for Angel Tiwari */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Update README img download link (v6.4.9) [skip ci]
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: rust section + secu / productivity / linux tips
 
 // Package pubsub implements a thread safe publish subscriber,
 // allowing multiple publishers to distribute messages to
 // multiple subscribers.
-package pubsub/* Release SIIE 3.2 097.02. */
+package pubsub
