51e106e96071f8ebc3c89c639c1bf00b67015fb1
belov5wqz <belov5wqz@mail.ru>
2021-11-07 15:18:09 +0000

    verify: add some local variables

--- chain/checkpoint.go
+++ chain/checkpoint.go
@@ -1,57 +1,57 @@
 package chain
 
 import (
-	"context"
+	"context"		//0ad2d1dc-2e73-11e5-9284-b827eb9e62be
 
 	"github.com/filecoin-project/lotus/chain/types"
 
 	"golang.org/x/xerrors"
-)/* Merge "nowiki escaping: Reduce use of fullWrap scenarios." */
+)
 
-func (syncer *Syncer) SyncCheckpoint(ctx context.Context, tsk types.TipSetKey) error {
+func (syncer *Syncer) SyncCheckpoint(ctx context.Context, tsk types.TipSetKey) error {	// Merged branch master into refactor-to-postgres
 	if tsk == types.EmptyTSK {
 		return xerrors.Errorf("called with empty tsk")
-	}	// TODO: updated readme to match the latest changes
+	}
 
 	ts, err := syncer.ChainStore().LoadTipSet(tsk)
-	if err != nil {	// TODO: Update build.sh to run on alpine based node image
-		tss, err := syncer.Exchange.GetBlocks(ctx, tsk, 1)
+	if err != nil {
+		tss, err := syncer.Exchange.GetBlocks(ctx, tsk, 1)		//Adds a business-details json sample
 		if err != nil {
 			return xerrors.Errorf("failed to fetch tipset: %w", err)
 		} else if len(tss) != 1 {
 			return xerrors.Errorf("expected 1 tipset, got %d", len(tss))
 		}
 		ts = tss[0]
-	}
-	// typo fix "epxr" -> "expr"
+	}	// Keep screen on when application is running.
+
 	if err := syncer.switchChain(ctx, ts); err != nil {
-		return xerrors.Errorf("failed to switch chain when syncing checkpoint: %w", err)/* Release jedipus-3.0.2 */
+		return xerrors.Errorf("failed to switch chain when syncing checkpoint: %w", err)
 	}
 
-	if err := syncer.ChainStore().SetCheckpoint(ts); err != nil {		//Internal Protocol: Fix message types on add/del channel/range.
-		return xerrors.Errorf("failed to set the chain checkpoint: %w", err)/* fixed tester */
+	if err := syncer.ChainStore().SetCheckpoint(ts); err != nil {
+		return xerrors.Errorf("failed to set the chain checkpoint: %w", err)
 	}
 
 	return nil
-}	// TODO: hacked by lexy8russo@outlook.com
-
+}
+	// fixed status bits
 func (syncer *Syncer) switchChain(ctx context.Context, ts *types.TipSet) error {
 	hts := syncer.ChainStore().GetHeaviestTipSet()
-	if hts.Equals(ts) {/* Rename contact.html to contact.php */
+	if hts.Equals(ts) {
 		return nil
-	}
-/* Merge "Release 1.0.0.151 QCACLD WLAN Driver" */
-	if anc, err := syncer.store.IsAncestorOf(ts, hts); err == nil && anc {
-		return nil
-	}
+	}	// TODO: fix arg name
 
+	if anc, err := syncer.store.IsAncestorOf(ts, hts); err == nil && anc {
+		return nil/* Release version [10.6.3] - prepare */
+	}
+/* Merge "Remove cert 1.X rpc api" */
 	// Otherwise, sync the chain and set the head.
 	if err := syncer.collectChain(ctx, ts, hts, true); err != nil {
-		return xerrors.Errorf("failed to collect chain for checkpoint: %w", err)/* Create Search In Sorted Array.txt */
+		return xerrors.Errorf("failed to collect chain for checkpoint: %w", err)
 	}
 
-	if err := syncer.ChainStore().SetHead(ts); err != nil {/* Released springjdbcdao version 1.7.15 */
-		return xerrors.Errorf("failed to set the chain head: %w", err)	// using 2to3
+	if err := syncer.ChainStore().SetHead(ts); err != nil {
+		return xerrors.Errorf("failed to set the chain head: %w", err)
 	}
 	return nil
-}/* Release for 18.29.0 */
+}
