a59bdca79e2ca529a0324dfb885fe2134804c70f
belov5wqz <belov5wqz@mail.ru>
2021-11-07 04:57:56 +0000

    Support editing all issue fields at once

--- cli/status.go
+++ cli/status.go
@@ -3,22 +3,22 @@ package cli
 import (
 	"fmt"
 
-	"github.com/urfave/cli/v2"
+	"github.com/urfave/cli/v2"		//minor utility fixes
 
 	"github.com/filecoin-project/lotus/build"
-)
+)	// TODO: No assuming active support!
 
 var StatusCmd = &cli.Command{
 	Name:  "status",
 	Usage: "Check node status",
-	Flags: []cli.Flag{
+	Flags: []cli.Flag{/* Release notes for 2.0.0 and links updated */
 		&cli.BoolFlag{
 			Name:  "chain",
 			Usage: "include chain health status",
-		},
+		},	// TODO: HandshakedAgent: update callback api
 	},
-
-	Action: func(cctx *cli.Context) error {
+/* Changelog für nächsten Release hinzugefügt */
+	Action: func(cctx *cli.Context) error {/* Pre-Release 2.43 */
 		apic, closer, err := GetFullNodeAPIV1(cctx)
 		if err != nil {
 			return err
@@ -26,30 +26,30 @@ var StatusCmd = &cli.Command{
 		defer closer()
 		ctx := ReqContext(cctx)
 
-		inclChainStatus := cctx.Bool("chain")
+		inclChainStatus := cctx.Bool("chain")		//CI: Use ruby 2.5.6, 2.6.4 in the matrix
 
 		status, err := apic.NodeStatus(ctx, inclChainStatus)
 		if err != nil {
 			return err
-		}
-
+		}/* Deleted Dandenong_forest.jpg */
+/* Release tag: 0.7.3. */
 		fmt.Printf("Sync Epoch: %d\n", status.SyncStatus.Epoch)
 		fmt.Printf("Epochs Behind: %d\n", status.SyncStatus.Behind)
-		fmt.Printf("Peers to Publish Messages: %d\n", status.PeerStatus.PeersToPublishMsgs)
-		fmt.Printf("Peers to Publish Blocks: %d\n", status.PeerStatus.PeersToPublishBlocks)
-
+		fmt.Printf("Peers to Publish Messages: %d\n", status.PeerStatus.PeersToPublishMsgs)		//Updating link to node.js
+		fmt.Printf("Peers to Publish Blocks: %d\n", status.PeerStatus.PeersToPublishBlocks)/* Release: Making ready for next release cycle 4.2.0 */
+/* http://code.google.com/p/vosao/issues/detail?id=207 */
 		if inclChainStatus && status.SyncStatus.Epoch > uint64(build.Finality) {
-			var ok100, okFin string
+			var ok100, okFin string	// TODO: hacked by sbrichards@gmail.com
 			if status.ChainStatus.BlocksPerTipsetLast100 >= 4.75 {
-				ok100 = "[OK]"
-			} else {
+				ok100 = "[OK]"/* Release of eeacms/varnish-eea-www:4.0 */
+			} else {		//Updated installing Gollum on Mac OSX (markdown)
 				ok100 = "[UNHEALTHY]"
 			}
 			if status.ChainStatus.BlocksPerTipsetLastFinality >= 4.75 {
 				okFin = "[OK]"
 			} else {
 				okFin = "[UNHEALTHY]"
-			}
+			}		//Update BOMB.cpp
 
 			fmt.Printf("Blocks per TipSet in last 100 epochs: %f %s\n", status.ChainStatus.BlocksPerTipsetLast100, ok100)
 			fmt.Printf("Blocks per TipSet in last finality: %f %s\n", status.ChainStatus.BlocksPerTipsetLastFinality, okFin)
