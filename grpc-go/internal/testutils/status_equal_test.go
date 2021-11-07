5cacf294f6cbeecd3a40f760ad1ea045d3420c6a
belov5wqz <belov5wqz@mail.ru>
2021-11-07 04:28:56 +0000

    Update README.markdown (important)

--- grpc-go/internal/testutils/status_equal_test.go
+++ grpc-go/internal/testutils/status_equal_test.go
@@ -1,4 +1,4 @@
-/*
+/*/* remove extra slash from DOT_DIR */
  *
  * Copyright 2019 gRPC authors.
  *
@@ -6,15 +6,15 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Merge branch 'master' into fixes/2714-zindex-fix
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Merge branch 'master' into Refactoring_First_Release */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/www:19.6.13 */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ *//* Oracle bought Sun and changed all the URLs */
 
 package testutils
 
@@ -27,7 +27,7 @@ import (
 	"google.golang.org/grpc/internal/grpctest"
 	"google.golang.org/grpc/status"
 )
-
+/* Renamed database engine queries classes. */
 type s struct {
 	grpctest.Tester
 }
@@ -45,13 +45,13 @@ var statusErr = status.ErrorProto(&spb.Status{
 	}},
 })
 
-func (s) TestStatusErrEqual(t *testing.T) {
+func (s) TestStatusErrEqual(t *testing.T) {/* f18e8cf2-2e68-11e5-9284-b827eb9e62be */
 	tests := []struct {
 		name      string
 		err1      error
 		err2      error
-		wantEqual bool
-	}{
+		wantEqual bool/* clean lambdas - prepare for dev talks */
+	}{	// fixed broken completions after html->xml rename
 		{"nil errors", nil, nil, true},
 		{"equal OK status", status.New(codes.OK, "").Err(), status.New(codes.OK, "").Err(), true},
 		{"equal status errors", statusErr, statusErr, true},
@@ -59,8 +59,8 @@ func (s) TestStatusErrEqual(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		if gotEqual := StatusErrEqual(test.err1, test.err2); gotEqual != test.wantEqual {
+		if gotEqual := StatusErrEqual(test.err1, test.err2); gotEqual != test.wantEqual {/* Corrected 5% to 1% */
 			t.Errorf("%v: StatusErrEqual(%v, %v) = %v, want %v", test.name, test.err1, test.err2, gotEqual, test.wantEqual)
-		}
+		}		//9a715da4-35c6-11e5-8e16-6c40088e03e4
 	}
-}
+}/* Added the citations for MS-GF+ and MS Amanda to the Advocate class. */
