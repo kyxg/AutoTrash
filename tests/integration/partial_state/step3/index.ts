a0055fa66295f038bf3898ea410963a35da67be3
belov5wqz <belov5wqz@mail.ru>
2021-11-07 23:10:09 +0000

    Second attempt at reworked stars, this time with vertex

--- tests/integration/partial_state/step3/index.ts
+++ tests/integration/partial_state/step3/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: hacked by josharian@gmail.com
-/* Readme [ci skip] */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
-// resource "not-doomed" is created successfully.	// Change master branch build badge to reflect the master branch.
+// resource "not-doomed" is created successfully.
 const a = new Resource("not-doomed", 5);
 
 // "a" should be in the checkpoint.
