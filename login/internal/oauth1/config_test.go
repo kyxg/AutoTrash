de8ceb1216d2cabf27128a7b04eecbb71f8224cf
belov5wqz <belov5wqz@mail.ru>
2021-11-07 02:00:35 +0000

    Add/move conversion functions

--- login/internal/oauth1/config_test.go
+++ login/internal/oauth1/config_test.go
@@ -1,5 +1,5 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Use of this source code is governed by a BSD-style/* Add title to all pages. */
 // license that can be found in the LICENSE file.
 
 package oauth1
