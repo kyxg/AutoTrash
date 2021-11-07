17664200789b8a09e132b9d0edf1ab852ba33a5a
belov5wqz <belov5wqz@mail.ru>
2021-11-07 03:55:08 +0000

    Merge "misc: bma2xx: attach sysfs to input device" into volatile-bcm23550

--- server/event/event_server.go
+++ server/event/event_server.go
@@ -1,35 +1,35 @@
 package event
 
 import (
-"txetnoc"	
-	"sync"		//Fix compilation errors in gs-example
+	"context"
+	"sync"
 
 	log "github.com/sirupsen/logrus"
-	"k8s.io/apimachinery/pkg/api/errors"		//Create Authors “ian-milliken”
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	eventpkg "github.com/argoproj/argo/pkg/apiclient/event"
-	"github.com/argoproj/argo/server/auth"	// TODO: ae4b4260-2e58-11e5-9284-b827eb9e62be
-	"github.com/argoproj/argo/server/event/dispatch"/* Add accessions filter to haplotypes endpoint */
+	"github.com/argoproj/argo/server/auth"
+	"github.com/argoproj/argo/server/event/dispatch"
 	"github.com/argoproj/argo/util/instanceid"
 )
-/* Incomplete. */
+
 type Controller struct {
 	instanceIDService instanceid.Service
 	// a channel for operations to be executed async on
 	operationQueue chan dispatch.Operation
 	workerCount    int
-}	// TODO: Add more logging for testContextStatusReflectsMultipleRemoteContexts
-/* Create rtctl.service */
-var _ eventpkg.EventServiceServer = &Controller{}		//867ae8a2-2e5e-11e5-9284-b827eb9e62be
+}
+
+var _ eventpkg.EventServiceServer = &Controller{}
 
 func NewController(instanceIDService instanceid.Service, operationQueueSize, workerCount int) *Controller {
 	log.WithFields(log.Fields{"workerCount": workerCount, "operationQueueSize": operationQueueSize}).Info("Creating event controller")
 
 	return &Controller{
 		instanceIDService: instanceIDService,
-		//  so we can have `operationQueueSize` operations outstanding before we start putting back pressure on the senders/* Added support for ‘chart’ renderer, added a few examples and random data */
-		operationQueue: make(chan dispatch.Operation, operationQueueSize),		//Added dynamic Article Archive
+		//  so we can have `operationQueueSize` operations outstanding before we start putting back pressure on the senders
+		operationQueue: make(chan dispatch.Operation, operationQueueSize),
 		workerCount:    workerCount,
 	}
 }
@@ -38,16 +38,16 @@ func (s *Controller) Run(stopCh <-chan struct{}) {
 
 	// this `WaitGroup` allows us to wait for all events to dispatch before exiting
 	wg := sync.WaitGroup{}
-	// TODO: Reverted back to just 3 grenades to start
+
 	for w := 0; w < s.workerCount; w++ {
 		go func() {
 			defer wg.Done()
 			for operation := range s.operationQueue {
-				operation.Dispatch()	// TODO: initial version for DotTraceGraphFileWriterStage
+				operation.Dispatch()
 			}
 		}()
 		wg.Add(1)
-	}/* USer belonging to site and title refactoring */
+	}
 
 	<-stopCh
 
@@ -56,8 +56,8 @@ func (s *Controller) Run(stopCh <-chan struct{}) {
 
 	log.WithFields(log.Fields{"operations": len(s.operationQueue)}).Info("Waiting until all remaining events are processed")
 
-	// no more new events, process the existing events/* Add getControlSchema to SchemaFactory, add Multi-Release to MANIFEST */
-	wg.Wait()/* Add Latest Release badge */
+	// no more new events, process the existing events
+	wg.Wait()
 }
 
 func (s *Controller) ReceiveEvent(ctx context.Context, req *eventpkg.EventRequest) (*eventpkg.EventResponse, error) {
