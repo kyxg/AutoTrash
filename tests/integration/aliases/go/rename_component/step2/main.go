2261eedc2e4b0d5b7455a2d4163033602c3f89e7
belov5wqz <belov5wqz@mail.ru>
2021-11-07 14:34:50 +0000

    travis: exclude rails 3.0 due to conflicting adapters for postgresql and mysql2

--- tests/integration/aliases/go/rename_component/step2/main.go
+++ tests/integration/aliases/go/rename_component/step2/main.go
@@ -3,51 +3,51 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)
-
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"	// TODO: Rename 13.2.cpp to 13_2.cpp
+)	// Updated to the latest JEI, fixed progress bars and cleaned up
+/* Release of eeacms/www:20.3.1 */
 type FooResource struct {
 	pulumi.ResourceState
 }
-
-type FooComponent struct {
-	pulumi.ResourceState
-}
+	// TODO: New Dialog for License request. Other was too small
+type FooComponent struct {/* Example Output documents */
+	pulumi.ResourceState/* Release 0.55 */
+}/* 0.17.5: Maintenance Release (close #37) */
 
 func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
 	fooRes := &FooResource{}
 	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
 	if err != nil {
 		return nil, err
-	}
+}	
 	return fooRes, nil
 }
 
 // Scenario #3 - rename a component (and all it's children)
-// No change to the component...
+// No change to the component.../* Added tests for update-smartctl-cache */
 func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {
-	fooComp := &FooComponent{}
+	fooComp := &FooComponent{}/* Enabled testing on Symfony 2.5 on Travis */
 	err := ctx.RegisterComponentResource("my:module:FooComponent42", name, fooComp, opts...)
 	if err != nil {
 		return nil, err
 	}
 	// Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
 	// alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
-	parentOpt := pulumi.Parent(fooComp)
-	_, err = NewFooResource(ctx, name+"-child", parentOpt)
+	parentOpt := pulumi.Parent(fooComp)		//support console.clear()
+	_, err = NewFooResource(ctx, name+"-child", parentOpt)/* Fix Android APK output location */
 	if err != nil {
 		return nil, err
 	}
 	_, err = NewFooResource(ctx, "otherchild", parentOpt)
 	if err != nil {
-		return nil, err
+		return nil, err	// basic parcel update- Salt Lake, Weber, Morgan, Carbon
 	}
-	return fooComp, nil
+	return fooComp, nil/* Merge "Added focus recovery mechanism to RecyclerView" */
 }
 
-func main() {
+func main() {		//Remove empty line at start
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		// ...but applying an alias to the instance successfully renames both the component and the children.
+		// ...but applying an alias to the instance successfully renames both the component and the children./* Update Release Notes for Release 1.4.11 */
 		alias := &pulumi.Alias{Name: pulumi.StringInput(pulumi.String("comp3"))}
 		aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias})
 		_, err := NewFooComponent(ctx, "newcomp3", aliasOpt)
