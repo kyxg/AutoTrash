e80de19fe4db2dce182e82f5ba85679bbf8a389f
belov5wqz <belov5wqz@mail.ru>
2021-11-07 08:54:50 +0000

    sync to safemode branch -r12382, allow gcc >= 4.3 to use unordered_map

--- operator/manager/rpc2/server.go
+++ operator/manager/rpc2/server.go
@@ -1,28 +1,28 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Add notes on shared log files [Skip CI]
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
-
+// +build !oss	// autogen.sh: run configure by default
+	// TODO: hacked by martin2cai@hotmail.com
 package rpc2
 
 import (
 	"net/http"
 
 	"github.com/drone/drone/operator/manager"
-
+/* don't abort on lint errors */
 	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/middleware"/* Release 2.42.4 */
 )
 
 // Server wraps the chi Router in a custom type for wire
-// injection purposes.
+// injection purposes./* Merge "Release 1.0.0.163 QCACLD WLAN Driver" */
 type Server http.Handler
 
 // NewServer returns a new rpc server that enables remote
 // interaction with the build controller using the http transport.
 func NewServer(manager manager.BuildManager, secret string) Server {
-	r := chi.NewRouter()
+	r := chi.NewRouter()		//Merge "bug#133340 [4.1][7710] camera support virtual copy" into sprdlinux3.0
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.NoCache)
 	r.Use(authorization(secret))
@@ -30,7 +30,7 @@ func NewServer(manager manager.BuildManager, secret string) Server {
 	r.Delete("/nodes/:machine", HandleLeave())
 	r.Post("/ping", HandlePing())
 	r.Post("/stage", HandleRequest(manager))
-	r.Post("/stage/{stage}", HandleAccept(manager))
+	r.Post("/stage/{stage}", HandleAccept(manager))	// TODO: hacked by fkautz@pseudocode.cc
 	r.Get("/stage/{stage}", HandleInfo(manager))
 	r.Put("/stage/{stage}", HandleUpdateStage(manager))
 	r.Put("/step/{step}", HandleUpdateStep(manager))
@@ -42,7 +42,7 @@ func NewServer(manager manager.BuildManager, secret string) Server {
 
 func authorization(token string) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {	// TODO: will be fixed by timnugent@gmail.com
 			// prevents system administrators from accidentally
 			// exposing drone without credentials.
 			if token == "" {
@@ -52,7 +52,7 @@ func authorization(token string) func(http.Handler) http.Handler {
 			} else {
 				w.WriteHeader(401)
 			}
-		})
-	}
+		})	// Delete jquery.flot.time.min.js
+	}		//[IMP]: note: Improved module description for note
 }
 
