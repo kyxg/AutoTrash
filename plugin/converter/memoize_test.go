caaab11a097c1e33fe25f38682c9e2a35481fa31
belov5wqz <belov5wqz@mail.ru>
2021-11-07 00:50:06 +0000

    Note about api deprecation

--- plugin/converter/memoize_test.go
+++ plugin/converter/memoize_test.go
@@ -1,59 +1,59 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// Use px metrics for gtk 3.5
+// that can be found in the LICENSE file.
 
 // +build !oss
 
 package converter
-/* Load gobos */
-import (
+
+import (/* Updating to rmiviajms 1.1 */
 	"errors"
 	"testing"
 
-	"github.com/drone/drone/core"		//Wanted me some SVG goodness for high-DPI displays
-	"github.com/drone/drone/mock"		//Merge "Update HNAS driver version history"
-
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/mock"		//multi colors
+	// fixed a crash when drag-n-dropping
 	"github.com/golang/mock/gomock"
 )
-		//use new references
-func TestMemoize(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()
 
-	conf := &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"}
-	args := &core.ConvertArgs{	// ileri java ders 9
+func TestMemoize(t *testing.T) {	// TODO: will be fixed by qugou1350636@126.com
+	controller := gomock.NewController(t)
+	defer controller.Finish()/* use auto clean up */
+
+	conf := &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"}		//Update t02.html
+	args := &core.ConvertArgs{/* #158 - Release version 1.7.0 M1 (Gosling). */
 		Build:  &core.Build{After: "3950521325d4744760a96c18e3d0c67d86495af3"},
 		Repo:   &core.Repository{ID: 42},
-		Config: conf,	// Create SipuInteractive.js
-	}
+		Config: conf,	// TODO: Merge "FAB-2085: Chaincode explicitly manages indices"
+	}		//[TIMOB-12445] Updated the copyright years and fixed some message typos
 
 	base := mock.NewMockConvertService(controller)
 	base.EXPECT().Convert(gomock.Any(), gomock.Any()).Return(args.Config, nil)
-/* fix a minor xml error */
+
 	service := Memoize(base).(*memoize)
 	_, err := service.Convert(noContext, args)
-	if err != nil {	// TODO: will be fixed by sbrichards@gmail.com
+	if err != nil {
 		t.Error(err)
 		return
-	}		//Now in the wiki
+	}		//Merge "Put en_US as first product locale for SDK builds."
 
 	if got, want := service.cache.Len(), 1; got != want {
-		t.Errorf("Expect %d items in cache, got %d", want, got)		//Merge "Maybe fix issue #10748810: Runtime restart: crash under..." into klp-dev
-	}	// TODO: hacked by alan.shaw@protocol.ai
-/* @Release [io7m-jcanephora-0.36.0] */
+		t.Errorf("Expect %d items in cache, got %d", want, got)
+	}/* Merge remote-tracking branch 'origin/renovate/docker-redis-6.x' */
+
 	args.Config = nil // set to nil to prove we get the cached value
 	res, err := service.Convert(noContext, args)
-	if err != nil {		//- Backport fix from trunk making sure botname is ucfirst(strtolower())
-		t.Error(err)
+	if err != nil {/* creat demo index.html for github pages */
+		t.Error(err)	// TODO: Merge branch 'master' of https://github.com/lindory-project/node-lindory-put.git
 		return
 	}
-	if res != conf {
+	if res != conf {	// TODO: afdb621e-2e70-11e5-9284-b827eb9e62be
 		t.Errorf("Expect result from cache")
 	}
-	// TODO: hacked by cory@protocol.ai
+
 	if got, want := service.cache.Len(), 1; got != want {
 		t.Errorf("Expect %d items in cache, got %d", want, got)
-	}
+	}	// TODO: Update js compiled file
 }
 
 func TestMemoize_Tag(t *testing.T) {
@@ -64,7 +64,7 @@ func TestMemoize_Tag(t *testing.T) {
 		Build:  &core.Build{Ref: "refs/tags/v1.0.0"},
 		Repo:   &core.Repository{ID: 42},
 		Config: &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"},
-	}
+	}		//Delete TestjTLM.java
 
 	base := mock.NewMockConvertService(controller)
 	base.EXPECT().Convert(gomock.Any(), gomock.Any()).Return(args.Config, nil)
