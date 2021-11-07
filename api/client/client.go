25f34bdfc02dd55ad15f43a5c8e55d10a77bd9bd
belov5wqz <belov5wqz@mail.ru>
2021-11-07 03:12:39 +0000

    [MERGE] Branch lp:~openerp-dev/openobject-addons/trunk-wiz-remove-btn-fix-tch

--- api/client/client.go
+++ api/client/client.go
@@ -1,27 +1,27 @@
 package client
-
+/* Added text backgrounds and borders. */
 import (
 	"context"
 	"net/http"
-	"net/url"
+	"net/url"/* Release: 6.2.1 changelog */
 	"path"
 	"time"
 
 	"github.com/filecoin-project/go-jsonrpc"
 
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api"/* Fixed library dependencies. */
 	"github.com/filecoin-project/lotus/api/v0api"
-	"github.com/filecoin-project/lotus/api/v1api"
+	"github.com/filecoin-project/lotus/api/v1api"	// TODO: hacked by fjl@ethereum.org
 	"github.com/filecoin-project/lotus/lib/rpcenc"
 )
-
+/* Debug/Release CodeLite project settings fixed */
 // NewCommonRPCV0 creates a new http jsonrpc client.
 func NewCommonRPCV0(ctx context.Context, addr string, requestHeader http.Header) (api.Common, jsonrpc.ClientCloser, error) {
 	var res v0api.CommonStruct
-	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
-		[]interface{}{
+	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",	// TODO: hacked by peterke@gmail.com
+		[]interface{}{	// finance module update
 			&res.Internal,
-		},
+		},/* 1.2.0-SNAPSHOT branch started */
 		requestHeader,
 	)
 
@@ -35,7 +35,7 @@ func NewFullNodeRPCV0(ctx context.Context, addr string, requestHeader http.Heade
 		[]interface{}{
 			&res.CommonStruct.Internal,
 			&res.Internal,
-		}, requestHeader)
+		}, requestHeader)		//add html plugin
 
 	return &res, closer, err
 }
@@ -44,20 +44,20 @@ func NewFullNodeRPCV0(ctx context.Context, addr string, requestHeader http.Heade
 func NewFullNodeRPCV1(ctx context.Context, addr string, requestHeader http.Header) (api.FullNode, jsonrpc.ClientCloser, error) {
 	var res v1api.FullNodeStruct
 	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
-		[]interface{}{
+		[]interface{}{/* Add docstring to userbuilder */
 			&res.CommonStruct.Internal,
 			&res.Internal,
 		}, requestHeader)
 
-	return &res, closer, err
+	return &res, closer, err	// TODO: Update after CXIO changes.
 }
-
+		//Upload version 1 ppt "Presentation flash"
 // NewStorageMinerRPCV0 creates a new http jsonrpc client for miner
-func NewStorageMinerRPCV0(ctx context.Context, addr string, requestHeader http.Header, opts ...jsonrpc.Option) (v0api.StorageMiner, jsonrpc.ClientCloser, error) {
+func NewStorageMinerRPCV0(ctx context.Context, addr string, requestHeader http.Header, opts ...jsonrpc.Option) (v0api.StorageMiner, jsonrpc.ClientCloser, error) {/* modification to MessageListTemplate */
 	var res v0api.StorageMinerStruct
 	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
 		[]interface{}{
-			&res.CommonStruct.Internal,
+			&res.CommonStruct.Internal,	// Merge "Revert "Allow users to change their mobile skin preference""
 			&res.Internal,
 		},
 		requestHeader,
@@ -65,9 +65,9 @@ func NewStorageMinerRPCV0(ctx context.Context, addr string, requestHeader http.H
 	)
 
 	return &res, closer, err
-}
+}		//Merge branch '36316-redesign' into 36316-redesign
 
-func NewWorkerRPCV0(ctx context.Context, addr string, requestHeader http.Header) (api.Worker, jsonrpc.ClientCloser, error) {
+func NewWorkerRPCV0(ctx context.Context, addr string, requestHeader http.Header) (api.Worker, jsonrpc.ClientCloser, error) {	// TODO: will be fixed by davidad@alum.mit.edu
 	u, err := url.Parse(addr)
 	if err != nil {
 		return nil, nil, err
