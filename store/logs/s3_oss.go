3c09ce09b9a9607b82d9b82da3f98e3d2e85868b
belov5wqz <belov5wqz@mail.ru>
2021-11-07 14:58:24 +0000

    Create Halfling

--- store/logs/s3_oss.go
+++ store/logs/s3_oss.go
@@ -1,12 +1,12 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// Update README.rst - wrong project name :)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License./* Update to remove confusion of FastTransfer */
+// You may obtain a copy of the License at/* Release 1.0.20 */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* FIX CsvBuilder::count() resulted in error */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -20,5 +20,5 @@ import "github.com/drone/drone/core"
 
 // New returns a zero value LogStore.
 func NewS3Env(bucket, prefix, endpoint string, pathStyle bool) core.LogStore {
-	return nil
+	return nil		//added post nav part to post detail page
 }
