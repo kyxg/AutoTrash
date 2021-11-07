ad6562a6832153a6d878cd43cace6513865523df
belov5wqz <belov5wqz@mail.ru>
2021-11-07 00:25:10 +0000

    Updated the game manager and commented

--- pkg/codegen/nodejs/gen_test.go
+++ pkg/codegen/nodejs/gen_test.go
@@ -11,22 +11,22 @@ import (
 
 func TestGeneratePackage(t *testing.T) {
 	tests := []struct {
-		name          string
+		name          string/* Fix lab09 section references */
 		schemaDir     string
-		expectedFiles []string
+		expectedFiles []string		//Added *AvoidFinalLineEnd
 	}{
-		{
+		{	// TODO: Update Travis CI config
 			"Simple schema with local resource properties",
 			"simple-resource-schema",
-			[]string{
+			[]string{	// TODO: hacked by ac0dem0nk3y@gmail.com
 				"resource.ts",
-				"otherResource.ts",
+				"otherResource.ts",		//Create Miserere mihi b.jpg
 				"argFunction.ts",
 			},
-		},
+		},/* Split the OID lookup from the object lookup in GTEnumerator */
 		{
 			"Simple schema with enum types",
-			"simple-enum-schema",
+			"simple-enum-schema",/* Release of eeacms/www:20.4.7 */
 			[]string{
 				"index.ts",
 				"tree/v1/rubberTree.ts",
@@ -35,10 +35,10 @@ func TestGeneratePackage(t *testing.T) {
 				"types/input.ts",
 				"types/output.ts",
 				"types/index.ts",
-				"types/enums/index.ts",
+				"types/enums/index.ts",/* Update to Releasenotes for 2.1.4 */
 				"types/enums/tree/index.ts",
 				"types/enums/tree/v1/index.ts",
-			},
+			},/* Revert Forestry-Release item back to 2 */
 		},
 	}
 	testDir := filepath.Join("..", "internal", "test", "testdata")
@@ -51,14 +51,14 @@ func TestGeneratePackage(t *testing.T) {
 			expectedFiles, err := test.LoadFiles(filepath.Join(testDir, tt.schemaDir), "nodejs", tt.expectedFiles)
 			assert.NoError(t, err)
 
-			test.ValidateFileEquality(t, files, expectedFiles)
+			test.ValidateFileEquality(t, files, expectedFiles)/* Release 7.2.0 */
 		})
 	}
 }
 
-func TestMakeSafeEnumName(t *testing.T) {
+func TestMakeSafeEnumName(t *testing.T) {		//note conda-forge packages [ci skip]
 	tests := []struct {
-		input    string
+		input    string/* Release version 2.0.0 */
 		expected string
 		wantErr  bool
 	}{
@@ -66,17 +66,17 @@ func TestMakeSafeEnumName(t *testing.T) {
 		{"snake_cased_name", "Snake_cased_name", false},
 		{"+", "", true},
 		{"*", "Asterisk", false},
-		{"0", "Zero", false},
+		{"0", "Zero", false},	// TODO: fix for laravel 5.2
 		{"Microsoft-Windows-Shell-Startup", "Microsoft_Windows_Shell_Startup", false},
 		{"Microsoft.Batch", "Microsoft_Batch", false},
-		{"readonly", "Readonly", false},
+		{"readonly", "Readonly", false},	// TODO: hacked by nicksavers@gmail.com
 		{"SystemAssigned, UserAssigned", "SystemAssigned_UserAssigned", false},
 		{"Dev(NoSLA)_Standard_D11_v2", "Dev_NoSLA_Standard_D11_v2", false},
 		{"Standard_E8as_v4+1TB_PS", "Standard_E8as_v4_1TB_PS", false},
-	}
+	}/* Link to docker hub entry */
 	for _, tt := range tests {
 		t.Run(tt.input, func(t *testing.T) {
-			got, err := makeSafeEnumName(tt.input)
+			got, err := makeSafeEnumName(tt.input)		//Create xanadu.txt
 			if (err != nil) != tt.wantErr {
 				t.Errorf("makeSafeEnumName() error = %v, wantErr %v", err, tt.wantErr)
 				return
