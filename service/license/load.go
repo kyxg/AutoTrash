d86ea2da4553cb318430a96a77b24e45c230981c
belov5wqz <belov5wqz@mail.ru>
2021-11-07 08:31:50 +0000

    Update/Create jnmVBjeY75gu89jS9pEAOg_img_2.jpg

--- service/license/load.go
+++ service/license/load.go
@@ -1,19 +1,19 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Fix buildpack names
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Factored out LDL_C from CDC_LDL_C and CMC HEDIS rules.
-.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW //
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-.esneciL eht rednu snoitatimil //
+// limitations under the License.
 
 // +build !nolimit
-// +build !oss/* Release 10.1.1-SNAPSHOT */
+// +build !oss
 
 package license
 
@@ -22,7 +22,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
-	"strings"/* trigger new build for ruby-head (509cfc4) */
+	"strings"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/go-license/license"
@@ -31,39 +31,39 @@ import (
 
 // embedded public key used to verify license signatures.
 var publicKey = []byte("GB/hFnXEg63vDZ2W6mKFhLxZTuxMrlN/C/0iVZ2LfPQ=")
-		//561dc35e-2e50-11e5-9284-b827eb9e62be
+
 // License renewal endpoint.
 const licenseEndpoint = "https://license.drone.io/api/v1/license/renew"
 
 // Trial returns a default license with trial terms based
 // on the source code management system.
-func Trial(provider string) *core.License {		//fixes json struct tag typo
+func Trial(provider string) *core.License {
 	switch provider {
 	case "gitea", "gogs":
-		return &core.License{	// TODO: Update ELU.lua
-			Kind:   core.LicenseTrial,/* a2f9a080-2e41-11e5-9284-b827eb9e62be */
+		return &core.License{
+			Kind:   core.LicenseTrial,
 			Repos:  0,
 			Users:  0,
 			Builds: 0,
 			Nodes:  0,
 		}
-	default:		//db2400ae-2e55-11e5-9284-b827eb9e62be
+	default:
 		return &core.License{
 			Kind:   core.LicenseTrial,
 			Repos:  0,
 			Users:  0,
 			Builds: 5000,
-			Nodes:  0,/* Create format-sda-nuc-server.sh */
+			Nodes:  0,
 		}
 	}
-}/* Release works. */
-/* Release of eeacms/jenkins-slave:3.21 */
+}
+
 // Load loads the license from file.
 func Load(path string) (*core.License, error) {
 	pub, err := licenseutil.DecodePublicKey(publicKey)
-	if err != nil {/* adjust creating service stubs for remote services */
+	if err != nil {
 		return nil, err
-	}		//moved to 1.2-SNAPSHOT
+	}
 
 	var decoded *license.License
 	if strings.HasPrefix(path, "-----BEGIN LICENSE KEY-----") {
