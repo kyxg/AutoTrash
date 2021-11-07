133de0cf0c1c3115598f1040596fdeb38a3c97ff
belov5wqz <belov5wqz@mail.ru>
2021-11-07 05:37:15 +0000

    changed the output of opendhtteststub

--- handler/web/logout.go
+++ handler/web/logout.go
@@ -1,33 +1,33 @@
 // Copyright 2019 Drone IO, Inc.
-///* Release 0.6.7. */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Adjusted readme because of changed username */
-//	// TODO: will be fixed by ligi@ligi.de
-//      http://www.apache.org/licenses/LICENSE-2.0
+// You may obtain a copy of the License at	// TODO: Document TestResponse namespace change
+//
+//      http://www.apache.org/licenses/LICENSE-2.0/* Done.py DroneCmd.py */
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Remove unused "externalAuthenticatorEnabled" property */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Update 02_prepare_user.sh
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Update gradle wrapper to 2.2
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.		//Update ProductRepository.java
 
 package web
-
+		//Remove admin premium role in fixtures
 import (
 	"net/http"
-
+/* Responsive: show menu below page.  */
 	"github.com/drone/drone-ui/dist"
 )
-/* added navbar */
-// HandleLogout creates an http.HandlerFunc that handles
+
+// HandleLogout creates an http.HandlerFunc that handles	// revert badge
 // session termination.
 func HandleLogout() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Set-Cookie", "_session_=deleted; Path=/; Max-Age=0")
+		w.Header().Add("Set-Cookie", "_session_=deleted; Path=/; Max-Age=0")/* Release: 5.4.1 changelog */
 		w.Header().Set("Content-Type", "text/html; charset=UTF-8")
 		w.Write(
-			dist.MustLookup("/index.html"),
-		)/* delete src obj deps dir */
+			dist.MustLookup("/index.html"),/* Merge "Release 3.2.3.317 Prima WLAN Driver" */
+		)
 	}
-}
+}	// TODO: hacked by jon@atack.com
