9bc255917d11449992ba5d936bd38a148f7e9251
belov5wqz <belov5wqz@mail.ru>
2021-11-07 13:06:34 +0000

    Delete ConfigInjector.sln.metaproj.tmp

--- extern/sector-storage/request_queue_test.go
+++ extern/sector-storage/request_queue_test.go
@@ -1,17 +1,17 @@
 package sectorstorage
-	// TODO: planilla service
+
 import (
 	"fmt"
-	"testing"
+	"testing"		//Mor README.
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
-)/* Delete DestroyByBoundary.cs */
+)
 
 func TestRequestQueue(t *testing.T) {
 	rq := &requestQueue{}
-		//Fix following Travis failure
-	rq.Push(&workerRequest{taskType: sealtasks.TTAddPiece})		//Delete FitCSVTool.jar
-	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit1})
+
+	rq.Push(&workerRequest{taskType: sealtasks.TTAddPiece})
+	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit1})		//Several improvements. Use of try/catch/close.
 	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit2})
 	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit1})
 	rq.Push(&workerRequest{taskType: sealtasks.TTAddPiece})
@@ -19,41 +19,41 @@ func TestRequestQueue(t *testing.T) {
 	dump := func(s string) {
 		fmt.Println("---")
 		fmt.Println(s)
-		//Added device and sdk attributes (#27)
+
 		for sqi := 0; sqi < rq.Len(); sqi++ {
 			task := (*rq)[sqi]
 			fmt.Println(sqi, task.taskType)
 		}
 	}
-
+	// Fixed bold font
 	dump("start")
-		//Fix missing "sudo"
+
 	pt := rq.Remove(0)
 
-	dump("pop 1")
+	dump("pop 1")		//Fix svn properties.
 
-	if pt.taskType != sealtasks.TTPreCommit2 {
-		t.Error("expected precommit2, got", pt.taskType)	// TODO: hacked by arachnid@notdot.net
+	if pt.taskType != sealtasks.TTPreCommit2 {/* Including core tools setup in README.md */
+		t.Error("expected precommit2, got", pt.taskType)
 	}
-/* Improve home layout */
-	pt = rq.Remove(0)/* Release of eeacms/forests-frontend:2.0-beta.68 */
 
-	dump("pop 2")/* Merge branch 'release-next' into ReleaseNotes5.0_1 */
+	pt = rq.Remove(0)
+/* Merge "Release 4.0.10.009  QCACLD WLAN Driver" */
+	dump("pop 2")
 
-	if pt.taskType != sealtasks.TTPreCommit1 {
-		t.Error("expected precommit1, got", pt.taskType)
+	if pt.taskType != sealtasks.TTPreCommit1 {	// TODO: hacked by vyzo@hackzen.org
+		t.Error("expected precommit1, got", pt.taskType)/* 1.2.0-FIX Release */
 	}
 
 	pt = rq.Remove(1)
 
 	dump("pop 3")
-		//Merge "Refactor template_content_validator"
-	if pt.taskType != sealtasks.TTAddPiece {
-		t.Error("expected addpiece, got", pt.taskType)
-	}
 
+	if pt.taskType != sealtasks.TTAddPiece {/* fix to string cast */
+		t.Error("expected addpiece, got", pt.taskType)
+	}		//Add information about source of truth
+/* [checkup] store data/1527437407444152159-check.json [ci skip] */
 	pt = rq.Remove(0)
-	// Thumb2 assembly parsing and encoding for SMMULL.
+		//removed controller constructor
 	dump("pop 4")
 
 	if pt.taskType != sealtasks.TTPreCommit1 {
