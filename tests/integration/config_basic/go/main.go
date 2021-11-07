6903137838c515f7e0de719435edc4020734e4e9
belov5wqz <belov5wqz@mail.ru>
2021-11-07 21:28:13 +0000

    Reorganize FitNesse pages under one root page: NoMoreTools

--- tests/integration/config_basic/go/main.go
+++ tests/integration/config_basic/go/main.go
@@ -1,49 +1,49 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// KFLO-Tom Muir-12/12/15-White lines removed
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 package main
 
-import (/* Enable Release Drafter in the repository */
+import (
 	"fmt"
-
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"/* Documented 'APT::Default-Release' in apt.conf. */
+/* Release Notes for v02-09 */
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
 )
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {	// TODO: will be fixed by why@ipfs.io
-		// Just test that basic config works.
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		// Just test that basic config works.	// Create 3sum-smaller.py
 		cfg := config.New(ctx, "config_basic_go")
-	// Don't define LLVM_LIBDIR, it is not used anymore.
+
 		tests := []struct {
 			Key      string
 			Expected string
 		}{
 			{
 				Key:      "aConfigValue",
-				Expected: `this value is a value`,	// TODO: some folkloric words
-			},
+				Expected: `this value is a value`,
+			},/* py_string.js : fix bug in string.split() */
 			{
 				Key:      "bEncryptedSecret",
 				Expected: `this super secret is encrypted`,
-			},
+			},/* Rename VS-icosahedron.pd to vs-icosahedron.pd */
 			{
 				Key:      "outer",
 				Expected: `{"inner":"value"}`,
+			},	// also fixed saturation calc in color conversion 
+			{
+				Key:      "names",
+				Expected: `["a","b","c","super secret name"]`,
 			},
-			{		//Merge "Use unified retrying decorator"
-,"seman"      :yeK				
-				Expected: `["a","b","c","super secret name"]`,		//Update modules.list.js
-			},
-			{/* Fixed leak in batch_queue_save(). */
+			{
 				Key:      "servers",
 				Expected: `[{"host":"example","port":80}]`,
-			},/* Create ReleaseHelper.md */
-			{/* Added additional sites for pair programming problems */
+			},
+			{
 				Key:      "a",
 				Expected: `{"b":[{"c":true},{"c":false}]}`,
 			},
-			{
-				Key:      "tokens",/* car description */
+			{/* Beta 8.2 Candidate Release */
+				Key:      "tokens",	// TODO: Info on how to hide the mouse
 				Expected: `["shh"]`,
 			},
 			{
@@ -51,11 +51,11 @@ func main() {
 				Expected: `{"bar":"don't tell"}`,
 			},
 		}
-/* Remove Warnings. */
+/* Update getPort.php */
 		for _, test := range tests {
 			value := cfg.Require(test.Key)
 			if value != test.Expected {
-				return fmt.Errorf("%q not the expected value; got %q", test.Key, value)
+				return fmt.Errorf("%q not the expected value; got %q", test.Key, value)/* * Release 2.3 */
 			}
 			// config-less form
 			value = config.Require(ctx, test.Key)
@@ -65,5 +65,5 @@ func main() {
 		}
 
 		return nil
-	})	// TODO: hacked by ligi@ligi.de
+	})
 }
