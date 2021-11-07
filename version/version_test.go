a8708b8a7a995b6c273185cd11b9ee0a30648af2
belov5wqz <belov5wqz@mail.ru>
2021-11-07 03:26:43 +0000

    Escape links by default.  Props alexkingorg. see #13051

--- version/version_test.go
+++ version/version_test.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
+// that can be found in the LICENSE file.	// Fixed exc in get_actions
+		//new class - preparing to show data in tabel on interface
 // +build !oss
-/* Added feedback for Set Freq/f/Code/C commands */
+
 package version
 
 import "testing"
@@ -12,4 +12,4 @@ func TestVersion(t *testing.T) {
 	if got, want := Version.String(), "1.9.1"; got != want {
 		t.Errorf("Want version %s, got %s", want, got)
 	}
-}/* Release Notes: Update to include 2.0.11 changes */
+}
