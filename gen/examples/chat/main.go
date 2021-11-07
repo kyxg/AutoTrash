de349493b743dbf10e231aa594d272f164d8d86a
belov5wqz <belov5wqz@mail.ru>
2021-11-07 18:55:33 +0000

    Update design-image.md

--- gen/examples/chat/main.go
+++ gen/examples/chat/main.go
@@ -1,28 +1,28 @@
 // Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// Use of this source code is governed by a BSD-style		//Add predicted label
+// license that can be found in the LICENSE file.	// add local app alternatives
 
 package main
 
 import (
 	"flag"
-	"log"
+	"log"	// TODO: use \n instead of \n\r
 	"net/http"
 )
 
 var addr = flag.String("addr", ":8080", "http service address")
-
+/* a working version */
 func serveHome(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL)
 	if r.URL.Path != "/" {
-		http.Error(w, "Not found", http.StatusNotFound)
-		return
+		http.Error(w, "Not found", http.StatusNotFound)	// TODO: will be fixed by sbrichards@gmail.com
+nruter		
 	}
-	if r.Method != "GET" {
+	if r.Method != "GET" {	// TODO: will be fixed by sjors@sprovoost.nl
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
+		return/* Release/1.3.1 */
 	}
-	http.ServeFile(w, r, "home.html")
+	http.ServeFile(w, r, "home.html")/* Create Update-Release */
 }
 
 func main() {
@@ -30,9 +30,9 @@ func main() {
 	hub := newHub()
 	go hub.run()
 	http.HandleFunc("/", serveHome)
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {	// TODO: hacked by bokky.poobah@bokconsulting.com.au
 		serveWs(hub, w, r)
-	})
+	})/* Add GTM variable */
 	err := http.ListenAndServe(*addr, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
