04778a1df999e1f1c085eb5e7c9d539f220d7dd9
belov5wqz <belov5wqz@mail.ru>
2021-11-08 15:16:40 +0000

    Create Game Speed x3

--- tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
@@ -2,18 +2,18 @@
 
 import * as pulumi from "@pulumi/pulumi";
 
-class Resource extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);/* Editor & paint Routing */
-    }
+class Resource extends pulumi.ComponentResource {	// TODO: BUGFIX: return S_OK(0) in the end
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {	// TODO: hacked by fjl@ethereum.org
+        super("my:module:Resource", name, {}, opts);
+    }		//Create BlackJackDriver
 }
 
 // Scenario #5 - composing #1 and #3 and making both changes at the same time
 class ComponentFive extends pulumi.ComponentResource {
-    resource: Resource;
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+    resource: Resource;/* Fix bug #3. */
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {		//initial blink support
         super("my:module:ComponentFive", name, {}, opts);
         this.resource = new Resource("otherchild", {parent: this});
     }
 }
-const comp5 = new ComponentFive("comp5");/* Create utm.py */
+const comp5 = new ComponentFive("comp5");
