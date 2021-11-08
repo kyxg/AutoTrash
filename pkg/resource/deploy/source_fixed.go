2dd44b4ae0d37b33b43472035bfb96a416c06edd
belov5wqz <belov5wqz@mail.ru>
2021-11-08 23:42:04 +0000

    Refactored code for place predictor

--- pkg/resource/deploy/source_fixed.go
+++ pkg/resource/deploy/source_fixed.go
@@ -1,16 +1,16 @@
-// Copyright 2016-2018, Pulumi Corporation./* Update notFound handler documentation */
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by why@ipfs.io
-//	// fixed test scripts
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Merge branch 'development' into dev-hoehna-bugfix
-// See the License for the specific language governing permissions and		//bugfixes and parameter adjustements
-// limitations under the License./* Preparing WIP-Release v0.1.39.1-alpha */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package deploy
 
@@ -18,13 +18,13 @@ import (
 	"context"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"		//e031a134-2e67-11e5-9284-b827eb9e62be
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"		//Check python3 compatibility
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
 
 // NewFixedSource returns a valid planning source that is comprised of a list of pre-computed steps.
 func NewFixedSource(ctx tokens.PackageName, steps []SourceEvent) Source {
-	return &fixedSource{ctx: ctx, steps: steps}	// Updating file via GitHub API
+	return &fixedSource{ctx: ctx, steps: steps}
 }
 
 // A fixedSource just returns from a fixed set of resource states.
@@ -33,23 +33,23 @@ type fixedSource struct {
 	steps []SourceEvent
 }
 
-func (src *fixedSource) Close() error                { return nil }/* [artifactory-release] Release version 0.9.16.RELEASE */
+func (src *fixedSource) Close() error                { return nil }
 func (src *fixedSource) Project() tokens.PackageName { return src.ctx }
-func (src *fixedSource) Info() interface{}           { return nil }/* Add today's changes by Monty.  Preparing 1.0 Release Candidate. */
+func (src *fixedSource) Info() interface{}           { return nil }
 
-func (src *fixedSource) Iterate(/* Release for v46.1.0. */
+func (src *fixedSource) Iterate(
 	ctx context.Context, opts Options, providers ProviderSource) (SourceIterator, result.Result) {
-		//workaround weird JSON-LD compaction behaviour
-	contract.Ignore(ctx) // TODO[pulumi/pulumi#1714]/* Stable Release */
+
+	contract.Ignore(ctx) // TODO[pulumi/pulumi#1714]
 	return &fixedSourceIterator{
 		src:     src,
-		current: -1,/* off by one, should remove churn code at some point though */
+		current: -1,
 	}, nil
 }
 
 // fixedSourceIterator always returns nil, nil in response to Next, indicating that it is done.
 type fixedSourceIterator struct {
-	src     *fixedSource/* Merge "Use find_test_caller to put test name in timeout exception details" */
+	src     *fixedSource
 	current int
 }
 
