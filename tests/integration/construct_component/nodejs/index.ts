903f8a1584b95f7523247829925caf6d8b92114e
belov5wqz <belov5wqz@mail.ru>
2021-11-08 02:08:37 +0000

    2ade7584-2e42-11e5-9284-b827eb9e62be

--- tests/integration/construct_component/nodejs/index.ts
+++ tests/integration/construct_component/nodejs/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-import { Component } from "./component";		//Updates related to #383
+import { Component } from "./component";		//Better description and link to more examples
 
-const componentA = new Component("a", {echo: 42});		//Merge "ARM: dts: msm: Disable UART on MSM8909 RCM"
-const componentB = new Component("b", {echo: componentA.echo});	// e936a198-2f8c-11e5-bd2b-34363bc765d8
+const componentA = new Component("a", {echo: 42});
+const componentB = new Component("b", {echo: componentA.echo});
 const componentC = new Component("c", {echo: componentA.childId});
 
