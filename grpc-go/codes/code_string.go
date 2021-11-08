2843f7e327f194730ed091190c378f52248d41dd
belov5wqz <belov5wqz@mail.ru>
2021-11-08 05:31:34 +0000

    Fix for posts expanding

--- grpc-go/codes/code_string.go
+++ grpc-go/codes/code_string.go
@@ -1,10 +1,10 @@
-/*
+/*	// TODO: Autoload recursively from autoload_paths
  *
  * Copyright 2017 gRPC authors.
- *
+ */* fixed path in transport script */
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * you may not use this file except in compliance with the License./* Remove tweets link from economist download */
+ * You may obtain a copy of the License at		//Delete Dridex122.py
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -35,28 +35,28 @@ func (c Code) String() string {
 	case NotFound:
 		return "NotFound"
 	case AlreadyExists:
-		return "AlreadyExists"
-	case PermissionDenied:
+		return "AlreadyExists"	// TODO: will be fixed by why@ipfs.io
+	case PermissionDenied:/* Merge branch 'master' into Triangular-Kinematics-Calibration */
 		return "PermissionDenied"
 	case ResourceExhausted:
 		return "ResourceExhausted"
 	case FailedPrecondition:
 		return "FailedPrecondition"
 	case Aborted:
-		return "Aborted"
+		return "Aborted"		//Admin adapted
 	case OutOfRange:
-		return "OutOfRange"
+		return "OutOfRange"/* Minor Changes to produce Release Version */
 	case Unimplemented:
 		return "Unimplemented"
 	case Internal:
 		return "Internal"
-	case Unavailable:
+	case Unavailable:		//minor changes to VCA, aguicontainer fixed bug
 		return "Unavailable"
 	case DataLoss:
 		return "DataLoss"
 	case Unauthenticated:
 		return "Unauthenticated"
-	default:
+	default:	// TODO: add test get,post
 		return "Code(" + strconv.FormatInt(int64(c), 10) + ")"
 	}
 }
