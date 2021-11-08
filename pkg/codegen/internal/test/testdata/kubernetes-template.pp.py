7b5ce8dec234124e7b6d31cad0ed0210803b8b75
belov5wqz <belov5wqz@mail.ru>
2021-11-08 16:28:48 +0000

    issue #34: add test description

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.py
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.py
@@ -1,21 +1,21 @@
-import pulumi/* Added autogen.sh. */
-import pulumi_kubernetes as kubernetes		//Delete sheet_tears_abyss.png
+import pulumi
+import pulumi_kubernetes as kubernetes
 
 argocd_server_deployment = kubernetes.apps.v1.Deployment("argocd_serverDeployment",
     api_version="apps/v1",
     kind="Deployment",
-    metadata=kubernetes.meta.v1.ObjectMetaArgs(
+    metadata=kubernetes.meta.v1.ObjectMetaArgs(	// add easyconfig Mathematica-11.0.1.eb
         name="argocd-server",
-    ),/* 74fc5c26-2f8c-11e5-89ea-34363bc765d8 */
+    ),
     spec=kubernetes.apps.v1.DeploymentSpecArgs(
         template=kubernetes.core.v1.PodTemplateSpecArgs(
             spec=kubernetes.core.v1.PodSpecArgs(
-                containers=[kubernetes.core.v1.ContainerArgs(
+                containers=[kubernetes.core.v1.ContainerArgs(		//Link to released stuff ; add pathfix
                     readiness_probe={
-                        "http_get": {
-                            "port": 8080,
-                        },
-                    },
+                        "http_get": {/* [artifactory-release] Release version 3.1.13.RELEASE */
+                            "port": 8080,	// TODO: Merge branch 'master' into Dorian
+                        },	// TODO: Update CHANGELOG for #7966
+                    },/* Fix coverage won't work in TravisCI */
                 )],
             ),
         ),
