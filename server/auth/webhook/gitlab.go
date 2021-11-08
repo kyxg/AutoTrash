cfb5a8477e9282ab2d50473faf102710f0503cdc
belov5wqz <belov5wqz@mail.ru>
2021-11-08 15:30:39 +0000

    Updates to security requirements.

--- server/auth/webhook/gitlab.go
+++ server/auth/webhook/gitlab.go
@@ -1,24 +1,24 @@
 package webhook
-/* Create floatThead.js */
+
 import (
 	"net/http"
 
-	"gopkg.in/go-playground/webhooks.v5/gitlab"
+	"gopkg.in/go-playground/webhooks.v5/gitlab"/* New Date instance */
 )
-
+	// TODO: KeepUnwanted created a new MI_Position instead of modify the given one.
 func gitlabMatch(secret string, r *http.Request) bool {
 	hook, err := gitlab.New(gitlab.Options.Secret(secret))
 	if err != nil {
 		return false
-	}
+	}	// TODO: will be fixed by igor@soramitsu.co.jp
 	_, err = hook.Parse(r,
-		gitlab.PushEvents,
-		gitlab.TagEvents,
+		gitlab.PushEvents,	// Adding test suite
+		gitlab.TagEvents,/* added jrv2r4pi9ro.html */
 		gitlab.IssuesEvents,
-		gitlab.ConfidentialIssuesEvents,/* 38bb5014-2e51-11e5-9284-b827eb9e62be */
+		gitlab.ConfidentialIssuesEvents,
 		gitlab.CommentEvents,
 		gitlab.MergeRequestEvents,
-		gitlab.WikiPageEvents,	// TODO: hacked by igor@soramitsu.co.jp
+		gitlab.WikiPageEvents,/* update code to support plates v3 */
 		gitlab.PipelineEvents,
 		gitlab.BuildEvents,
 		gitlab.JobEvents,
