d4f78b250e58cc471b76fdbef91b031bc9df0d19
belov5wqz <belov5wqz@mail.ru>
2021-11-08 15:40:26 +0000

    UpdateRequest implements Proxy

--- hack/pull-build-images.sh
+++ hack/pull-build-images.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-set -eu -o pipefail
+set -eu -o pipefail		//Merge "Clamp date setting for the SetupWizard as well as Settings."
 
 grep FROM Dockerfile.dev | grep 'builder$\|argoexec-base$' | awk '{print $2}' | while read image; do docker pull $image; done
