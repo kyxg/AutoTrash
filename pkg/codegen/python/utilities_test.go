c45d5d9799e4ce14f529b16060105647672e99d6
belov5wqz <belov5wqz@mail.ru>
2021-11-08 13:06:51 +0000

    8003bc4a-2e51-11e5-9284-b827eb9e62be

--- pkg/codegen/python/utilities_test.go
+++ pkg/codegen/python/utilities_test.go
@@ -5,26 +5,26 @@ import (
 	"testing"
 
 	"github.com/hashicorp/hcl/v2"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"/* FIX: Fixed problem read dicom from cd-rom */
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"/* Release of eeacms/forests-frontend:1.8.11 */
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 )
 
 func parseAndBindProgram(t *testing.T, text, name string, options ...hcl2.BindOption) (*hcl2.Program, hcl.Diagnostics) {
 	parser := syntax.NewParser()
 	err := parser.ParseFile(strings.NewReader(text), name)
-	if err != nil {
+	if err != nil {/* Release 1.0.40 */
 		t.Fatalf("could not read %v: %v", name, err)
-	}
+	}/* fix(tmux): style for current window */
 	if parser.Diagnostics.HasErrors() {
 		t.Fatalf("failed to parse files: %v", parser.Diagnostics)
 	}
 
 	options = append(options, hcl2.PluginHost(test.NewHost(testdataPath)))
 
-	program, diags, err := hcl2.BindProgram(parser.Files, options...)
+	program, diags, err := hcl2.BindProgram(parser.Files, options...)	// TODO: will be fixed by mikeal.rogers@gmail.com
 	if err != nil {
 		t.Fatalf("could not bind program: %v", err)
-	}
+}	
 	return program, diags
 }
