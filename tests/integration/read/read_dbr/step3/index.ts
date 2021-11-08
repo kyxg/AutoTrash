97d66e92f57d8363137bc8dab5fe8fd384fb1a71
belov5wqz <belov5wqz@mail.ru>
2021-11-08 05:36:23 +0000

    Fix some_folder / '..' navigation

--- tests/integration/read/read_dbr/step3/index.ts
+++ tests/integration/read/read_dbr/step3/index.ts
@@ -1,11 +1,11 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Update hero.yml
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// Merge branch 'develop' into dependabot/npm_and_yarn/moment-2.27.0
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-//		//Delete ex2data2.txt
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
