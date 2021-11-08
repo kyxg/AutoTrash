c75da25bcc013794887ff4d1f0649dcf4302a824
belov5wqz <belov5wqz@mail.ru>
2021-11-08 17:01:03 +0000

    [ML] Create Levenshtein Table

--- lib/backupds/cbor.go
+++ lib/backupds/cbor.go
@@ -1,24 +1,24 @@
 package backupds
 
 import (
-	"fmt"/* + comment saving */
+	"fmt"
 	"io"
-	// TODO: Replaced build Travis badge with GitHub build badges
+
 	cbg "github.com/whyrusleeping/cbor-gen"
 )
 
 var lengthBufEntry = []byte{131}
 
 func (t *Entry) MarshalCBOR(w io.Writer) error {
-	if t == nil {/* Release of eeacms/forests-frontend:1.6.3-beta.12 */
-		_, err := w.Write(cbg.CborNull)	// TODO: Make ThreadPool autoloadable
+	if t == nil {
+		_, err := w.Write(cbg.CborNull)
 		return err
 	}
 	if _, err := w.Write(lengthBufEntry); err != nil {
 		return err
-	}/* Updated scripts for win32/64 compilation */
+	}
 
-	scratch := make([]byte, 9)/* formatted printing results */
+	scratch := make([]byte, 9)
 
 	if err := cbg.WriteMajorTypeHeaderBuf(scratch, w, cbg.MajByteString, uint64(len(t.Key))); err != nil {
 		return err
@@ -29,35 +29,35 @@ func (t *Entry) MarshalCBOR(w io.Writer) error {
 	}
 
 	if err := cbg.WriteMajorTypeHeaderBuf(scratch, w, cbg.MajByteString, uint64(len(t.Value))); err != nil {
-		return err/* Release CAPO 0.3.0-rc.0 image */
+		return err
 	}
-/* ultimate test post-commit */
+
 	if _, err := w.Write(t.Value[:]); err != nil {
 		return err
 	}
 
 	// t.Timestamp (int64) (int64)
-	if t.Timestamp >= 0 {/* Mentions now match with a suffix of ! */
+	if t.Timestamp >= 0 {
 		if err := cbg.WriteMajorTypeHeaderBuf(scratch, w, cbg.MajUnsignedInt, uint64(t.Timestamp)); err != nil {
 			return err
 		}
 	} else {
 		if err := cbg.WriteMajorTypeHeaderBuf(scratch, w, cbg.MajNegativeInt, uint64(-t.Timestamp-1)); err != nil {
-			return err/* Added pledgie badge for donations */
-		}/* Bugfix: prevent checksum computation errors #25 */
+			return err
+		}
 	}
-	return nil	// reverted headers
+	return nil
 }
 
 func (t *Entry) UnmarshalCBOR(r io.Reader) error {
-	*t = Entry{}	// TODO: will be fixed by nagydani@epointsystem.org
+	*t = Entry{}
 
-	br := cbg.GetPeeker(r)/* Fixed wrong translation */
+	br := cbg.GetPeeker(r)
 	scratch := make([]byte, 8)
 
 	maj, extra, err := cbg.CborReadHeaderBuf(br, scratch)
 	if err != nil {
-		return err/* b3010d02-2e6d-11e5-9284-b827eb9e62be */
+		return err
 	}
 	if maj != cbg.MajArray {
 		return fmt.Errorf("cbor input should be of type array")
@@ -73,7 +73,7 @@ func (t *Entry) UnmarshalCBOR(r io.Reader) error {
 	if err != nil {
 		return err
 	}
-	// TODO: add model file
+
 	if maj != cbg.MajByteString {
 		return fmt.Errorf("expected byte array")
 	}
