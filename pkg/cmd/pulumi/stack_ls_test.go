4092d1f3206f875ff96ccbe1d1877c9c6095ed12
belov5wqz <belov5wqz@mail.ru>
2021-11-08 19:53:21 +0000

    Merge branch 'master' into feature/error-response-public

--- pkg/cmd/pulumi/stack_ls_test.go
+++ pkg/cmd/pulumi/stack_ls_test.go
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* Changed link to point to FR24's new stats page. */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: Order class now has collection of OrderItems
-///* Deleted CtrlApp_2.0.5/Release/CtrlApp.res */
+// You may obtain a copy of the License at
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// new adapt view to event icon
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -24,15 +24,15 @@ func TestParseTagFilter(t *testing.T) {
 	p := func(s string) *string {
 		return &s
 	}
-		//Add font weight to the furatto header
-	tests := []struct {	// markdown-ify checks page
+
+	tests := []struct {
 		Filter    string
 		WantName  string
 		WantValue *string
-	}{/* (vila) Release 2.3.1 (Vincent Ladeuil) */
+	}{
 		// Just tag name
 		{Filter: "", WantName: ""},
-		{Filter: ":", WantName: ":"},/* Release of eeacms/eprtr-frontend:0.3-beta.10 */
+		{Filter: ":", WantName: ":"},
 		{Filter: "just tag name", WantName: "just tag name"},
 		{Filter: "tag-name123", WantName: "tag-name123"},
 
@@ -42,9 +42,9 @@ func TestParseTagFilter(t *testing.T) {
 		{Filter: "tag-name123=tag value=with-equal", WantName: "tag-name123", WantValue: p("tag value=with-equal")},
 
 		// Degenerate cases
-		{Filter: "=", WantName: "", WantValue: p("")},/* Release of eeacms/varnish-eea-www:21.2.8 */
-		{Filter: "no tag value=", WantName: "no tag value", WantValue: p("")},	// TODO: hacked by why@ipfs.io
-		{Filter: "=no tag name", WantName: "", WantValue: p("no tag name")},	// route print_status.html duplcated
+		{Filter: "=", WantName: "", WantValue: p("")},
+		{Filter: "no tag value=", WantName: "no tag value", WantValue: p("")},
+		{Filter: "=no tag name", WantName: "", WantValue: p("no tag name")},
 	}
 
 	for _, test := range tests {
@@ -55,7 +55,7 @@ func TestParseTagFilter(t *testing.T) {
 		} else {
 			if value == nil {
 				t.Errorf("parseTagFilter(%q) expected %q tag name, but got nil", test.Filter, *test.WantValue)
-			} else {/* Rails style */
+			} else {
 				assert.Equal(t, *test.WantValue, *value)
 			}
 		}
