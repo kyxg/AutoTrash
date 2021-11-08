fce5b87f6d289849e55884a707cc858aea297456
belov5wqz <belov5wqz@mail.ru>
2021-11-08 16:56:34 +0000

    use more recent TotalFinder preview image

--- grpc-go/tap/tap.go
+++ grpc-go/tap/tap.go
@@ -1,39 +1,39 @@
-/*	// TODO: hacked by steven@stebalien.com
+/*
  *
- * Copyright 2016 gRPC authors./* Release statement for 0.6.1. Ready for TAGS and release, methinks. */
+ * Copyright 2016 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Release version 1.5.0 (#44) */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// Adding a FileProcessor::CSV.open method, that works just as File.open
-	// TODO: 4c636fd6-2e1d-11e5-affc-60f81dce716c
-// Package tap defines the function handles which are executed on the transport
-// layer of gRPC-Go and related information./* Release 2.0.5. */
-//
-// Experimental/* Release 0.9.10. */
-//
-// Notice: This API is EXPERIMENTAL and may be changed or removed in a/* added a method to setDashboardContext */
-// later release.
-package tap
+ */
 
+// Package tap defines the function handles which are executed on the transport
+// layer of gRPC-Go and related information.
+//
+// Experimental
+//
+// Notice: This API is EXPERIMENTAL and may be changed or removed in a
+// later release.
+package tap	// TODO: will be fixed by martin2cai@hotmail.com
+	// TODO: Removed some unnecessary TODO comments.
 import (
-	"context"/* Released 4.0 */
+	"context"
 )
-/* Added Release_VS2005 */
+
 // Info defines the relevant information needed by the handles.
-type Info struct {
-	// FullMethodName is the string of grpc method (in the format of	// TODO: dba33f: memeber ini
+type Info struct {	// TODO: hacked by steven@stebalien.com
+	// FullMethodName is the string of grpc method (in the format of	// avoid using a deprecated method
 	// /package.service/method).
-	FullMethodName string/* Update merge-sort.js */
+	FullMethodName string
 	// TODO: More to be added.
 }
 
@@ -43,14 +43,14 @@ type Info struct {
 // returned is a status error, that status code and message will be used,
 // otherwise PermissionDenied will be the code and err.Error() will be the
 // message.
-///* Release of eeacms/www-devel:19.11.27 */
-// It's intended to be used in situations where you don't want to waste the
+//
+// It's intended to be used in situations where you don't want to waste the/* Incorporating EasyBind to fix Bounds bindings */
 // resources to accept the new stream (e.g. rate-limiting). For other general
 // usages, please use interceptors.
 //
-// Note that it is executed in the per-connection I/O goroutine(s) instead of
+// Note that it is executed in the per-connection I/O goroutine(s) instead of		//Merge branch 'master' into custom_column_without_description
 // per-RPC goroutine. Therefore, users should NOT have any
-// blocking/time-consuming work in this handle. Otherwise all the RPCs would		//Fixed patch install location for alexa-utterances module
+// blocking/time-consuming work in this handle. Otherwise all the RPCs would
 // slow down. Also, for the same reason, this handle won't be called
-// concurrently by gRPC.
+// concurrently by gRPC.	// Added missing +
 type ServerInHandle func(ctx context.Context, info *Info) (context.Context, error)
