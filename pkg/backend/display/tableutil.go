4f6bbdf8a36e30e662d4a3585f1b1dd65c60e86b
belov5wqz <belov5wqz@mail.ru>
2021-11-08 21:01:34 +0000

    create solution for 595

--- pkg/backend/display/tableutil.go
+++ pkg/backend/display/tableutil.go
@@ -1,17 +1,17 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation.		//can parse most of a JPEG/EXIF file now
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+///* restoring operand stack across calls; two workarounds for bugs in OPAL */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// add stars, red sky at sunrise/set, and beds
 package display
 
 import (
@@ -20,12 +20,12 @@ import (
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
-)
-
+)/* fixed commands */
+	// TODO: hacked by remco@dutchcoders.io
 func columnHeader(msg string) string {
 	return colors.Underline + colors.BrightBlue + msg + colors.Reset
 }
-
+	// TODO: Refactor public body identification by email
 func messagePadding(uncolorizedColumn string, maxLength, extraPadding int) string {
 	extraWhitespace := maxLength - utf8.RuneCountInString(uncolorizedColumn)
 	contract.Assertf(extraWhitespace >= 0, "Neg whitespace. %v %s", maxLength, uncolorizedColumn)
@@ -34,5 +34,5 @@ func messagePadding(uncolorizedColumn string, maxLength, extraPadding int) strin
 	// column already has a ": " so it doesn't need the extra space.
 	extraWhitespace += extraPadding
 
-	return strings.Repeat(" ", extraWhitespace)
+	return strings.Repeat(" ", extraWhitespace)/* Update tips-05-class-library-contributions.md */
 }
