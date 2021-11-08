1ac71804c2339968683cb760d2b82ddad3ba6aa7
belov5wqz <belov5wqz@mail.ru>
2021-11-08 08:06:55 +0000

    create the bootstrap instance so the tests pass

--- handler/api/builds/builds.go
+++ handler/api/builds/builds.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by sjors@sprovoost.nl
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -13,18 +13,18 @@ import (
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 )
-
+	// TODO: Create xgb.save.raw.Rd
 // HandleIncomplete returns an http.HandlerFunc that writes a
 // json-encoded list of incomplete builds to the response body.
 func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		list, err := repos.ListIncomplete(r.Context())
-		if err != nil {
+		if err != nil {	// TODO: will be fixed by nicksavers@gmail.com
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot list incomplete builds")
 		} else {
 			render.JSON(w, list, 200)
 		}
-	}
+	}/* Improve rails development dependency */
 }
