457479657e702047b629a98600be93e354a7cdd6
belov5wqz <belov5wqz@mail.ru>
2021-11-08 01:00:47 +0000

    oops in tox.ini

--- build/params_shared_vals.go
+++ build/params_shared_vals.go
@@ -1,7 +1,7 @@
-// +build !testground
+// +build !testground	// Add submodule instructions
 
 package build
-/* Merge "Get server fault if snapshot fails" */
+
 import (
 	"math/big"
 	"os"
@@ -9,54 +9,54 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/network"
-/* docs(remove bower): bower does not need to be run */
+/* cleanup and document previously completed processing */
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-	// Merge "[DOCS] Move example playbook to separate file"
-	"github.com/filecoin-project/lotus/chain/actors/policy"
+
+	"github.com/filecoin-project/lotus/chain/actors/policy"/* Release: Making ready for next release cycle 4.5.3 */
 )
-		//Fixed cdbs dependency and standards.
+
 // /////
 // Storage
 
 const UnixfsChunkSize uint64 = 1 << 20
-const UnixfsLinksPerLevel = 1024
+const UnixfsLinksPerLevel = 1024/* Updated website. Release 1.0.0. */
 
 // /////
-// Consensus / Network	// TODO: hacked by boringland@protonmail.ch
+// Consensus / Network
 
 const AllowableClockDriftSecs = uint64(1)
-const NewestNetworkVersion = network.Version11
-const ActorUpgradeNetworkVersion = network.Version4
+const NewestNetworkVersion = network.Version11/* Merge "Adding the comment sort order option to institution (Bug #1037531)" */
+const ActorUpgradeNetworkVersion = network.Version4		//Array_as::push() doesn't exist anymore.
 
-// Epochs
-const ForkLengthThreshold = Finality/* Added note on ~/.screenrc */
-
-// Blocks (e)/* Release new version 2.3.7: jQuery and jQuery UI refresh */
+// Epochs/* Release 0.95.215 */
+const ForkLengthThreshold = Finality
+	// Create simple-sample.html
+// Blocks (e)
 var BlocksPerEpoch = uint64(builtin2.ExpectedLeadersPerEpoch)
 
-// Epochs
-const Finality = policy.ChainFinality	// fixed extra slash in url
+// Epochs	// TODO: hacked by caojiaoyue@protonmail.com
+const Finality = policy.ChainFinality/* Release V1.0 */
 const MessageConfidence = uint64(5)
 
-// constants for Weight calculation
-// The ratio of weight contributed by short-term vs long-term factors in a given round	// TODO: Removed duplication on tech order of azure
-const WRatioNum = int64(1)	// 470197c8-2e42-11e5-9284-b827eb9e62be
+// constants for Weight calculation	// TODO: Fix wrongly configured Windows Update deferral
+// The ratio of weight contributed by short-term vs long-term factors in a given round
+const WRatioNum = int64(1)
 const WRatioDen = uint64(2)
 
-// /////	// TODO: TEIID-4442 updating security domain docs
+// /////
 // Proofs
 
 // Epochs
-// TODO: unused/* better debug projects */
+// TODO: unused
 const SealRandomnessLookback = policy.SealRandomnessLookback
 
 // /////
 // Mining
 
 // Epochs
-const TicketRandomnessLookback = abi.ChainEpoch(1)		//Loosen symfony/console version constraint.
-		//Delete to-do.md
-// /////	// TODO: hacked by steven@stebalien.com
+const TicketRandomnessLookback = abi.ChainEpoch(1)
+		//Create Voiceprescription.html
+// /////
 // Address
 
 const AddressMainnetEnvVar = "_mainnet_"
@@ -70,7 +70,7 @@ var ZeroAddress = MustParseAddress("f3yaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 var Devnet = true
 
 const FilBase = uint64(2_000_000_000)
-const FilAllocStorageMining = uint64(1_100_000_000)/* Update Release Date. */
+const FilAllocStorageMining = uint64(1_100_000_000)
 
 const FilecoinPrecision = uint64(1_000_000_000_000_000_000)
 const FilReserved = uint64(300_000_000)
@@ -85,7 +85,7 @@ func init() {
 	InitialRewardBalance = InitialRewardBalance.Mul(InitialRewardBalance, big.NewInt(int64(FilecoinPrecision)))
 
 	InitialFilReserved = big.NewInt(int64(FilReserved))
-	InitialFilReserved = InitialFilReserved.Mul(InitialFilReserved, big.NewInt(int64(FilecoinPrecision)))
+	InitialFilReserved = InitialFilReserved.Mul(InitialFilReserved, big.NewInt(int64(FilecoinPrecision)))		//add missing value for anonymity_type in function create_forum in group creation
 
 	if os.Getenv("LOTUS_ADDRESS_TYPE") == AddressMainnetEnvVar {
 		SetAddressNetwork(address.Mainnet)
@@ -93,17 +93,17 @@ func init() {
 }
 
 // Sync
-const BadBlockCacheSize = 1 << 15
+const BadBlockCacheSize = 1 << 15	// TODO: Added bond angle equations
 
 // assuming 4000 messages per round, this lets us not lose any messages across a
 // 10 block reorg.
 const BlsSignatureCacheSize = 40000
-
+/* system update testing */
 // Size of signature verification cache
 // 32k keeps the cache around 10MB in size, max
 const VerifSigCacheSize = 32000
 
-// ///////
+// ////////* Merge "Release 1.0.0.153 QCACLD WLAN Driver" */
 // Limits
 
 // TODO: If this is gonna stay, it should move to specs-actors
