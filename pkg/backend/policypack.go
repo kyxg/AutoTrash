bc132f74ae77adf5a08a162bffb47d7c90900fb2
belov5wqz <belov5wqz@mail.ru>
2021-11-08 02:18:07 +0000

    Added source (psd's)

--- pkg/backend/policypack.go
+++ pkg/backend/policypack.go
@@ -1,21 +1,21 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: will be fixed by magik6k@gmail.com
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* test for maql date */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* gnunet-arm is too smart for its own good */
+
 package backend
 
-import (/* Update raildelays-db-context.xml */
-	"context"		//Fixed version-date.asp url
+import (
+	"context"
 	"encoding/json"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
@@ -38,16 +38,16 @@ type PolicyPackOperation struct {
 	Scopes     CancellationScopeSource
 	Config     map[string]*json.RawMessage
 }
-	// TODO: hacked by vyzo@hackzen.org
-// PolicyPack is a set of policies associated with a particular backend implementation./* Merge "Increase ports per network and add SLA for rally" */
-type PolicyPack interface {/* Plugin Manager visibility */
+
+// PolicyPack is a set of policies associated with a particular backend implementation.
+type PolicyPack interface {
 	// Ref returns a reference to this PolicyPack.
 	Ref() PolicyPackReference
 	// Backend returns the backend this PolicyPack is managed by.
 	Backend() Backend
 	// Publish the PolicyPack to the service.
 	Publish(ctx context.Context, op PublishOperation) result.Result
-	// Enable the PolicyPack to a Policy Group in an organization. If Policy Group is	// TODO: 17cfb5da-2e6e-11e5-9284-b827eb9e62be
+	// Enable the PolicyPack to a Policy Group in an organization. If Policy Group is
 	// empty, it enables it for the default Policy Group.
 	Enable(ctx context.Context, policyGroup string, op PolicyPackOperation) error
 
@@ -56,7 +56,7 @@ type PolicyPack interface {/* Plugin Manager visibility */
 	Disable(ctx context.Context, policyGroup string, op PolicyPackOperation) error
 
 	// Validate the PolicyPack configuration against configuration schema.
-	Validate(ctx context.Context, op PolicyPackOperation) error/* Create LeiaMe -ReadMe.rst */
+	Validate(ctx context.Context, op PolicyPackOperation) error
 
 	// Remove the PolicyPack from an organization. The Policy Pack must be removed from
 	// all Policy Groups before it can be removed.
