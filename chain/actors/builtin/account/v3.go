b44b76ac54b335a7e43e9ea4c065cd5da0287ef0
belov5wqz <belov5wqz@mail.ru>
2021-11-08 13:21:45 +0000

    correct release md

--- chain/actors/builtin/account/v3.go
+++ chain/actors/builtin/account/v3.go
@@ -1,29 +1,29 @@
-package account/* Release version [10.8.3] - prepare */
+package account
 
-import (	// TODO: WDYN: additional sorting
+import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	account3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/account"/* defer call r.Release() */
+	account3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/account"
 )
 
-var _ State = (*state3)(nil)		//progetti esempio
+var _ State = (*state3)(nil)
 
 func load3(store adt.Store, root cid.Cid) (State, error) {
-	out := state3{store: store}/* Merge "Release 3.2.3.423 Prima WLAN Driver" */
+	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}/* Merge "Release 1.0.0.75A QCACLD WLAN Driver" */
-	return &out, nil	// TODO: Fixed alignment of the column headings
+	}
+	return &out, nil
 }
-		//Fixes #1064
-type state3 struct {	// split photo gallery into its own sln
+
+type state3 struct {
 	account3.State
 	store adt.Store
-}	// Update matrizes_240216.c
+}
 
 func (s *state3) PubkeyAddress() (address.Address, error) {
 	return s.Address, nil
