962dbe1bff44f0fafed5530b1a418bd5a032e79d
belov5wqz <belov5wqz@mail.ru>
2021-11-08 05:41:09 +0000

    added cmake in root so for simpler use in my build system

--- persist/sqldb/archived_workflow_labels.go
+++ persist/sqldb/archived_workflow_labels.go
@@ -1,14 +1,14 @@
-package sqldb		//Create server_multiclnt_broadcast.c
+package sqldb
 
-import (	// removing RxJava1.x stuff from update_docs.sh script
+import (
 	"fmt"
-	"strconv"/* (Robert Collins) Release bzr 0.15 RC 1 */
+	"strconv"
 	"strings"
 
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/selection"
-	"upper.io/db.v3"/* Release BAR 1.1.8 */
-)/* #205 - Release version 1.2.0.RELEASE. */
+	"upper.io/db.v3"/* prepared for both: NBM Release + Sonatype Release */
+)
 
 func labelsClause(t dbType, requirements labels.Requirements) (db.Compound, error) {
 	var conds []db.Compound
@@ -16,20 +16,20 @@ func labelsClause(t dbType, requirements labels.Requirements) (db.Compound, erro
 		cond, err := requirementToCondition(t, r)
 		if err != nil {
 			return nil, err
-		}		//Delete astroblitz.crt
-		conds = append(conds, cond)	// TODO: will be fixed by yuvalalaluf@gmail.com
-	}
-	return db.And(conds...), nil		//modify data to negative
-}		//Merge "Moving q_trans[] table to vp9_quantize.{c, h}."
-		//Eliminada la constante FS_NO_UPDATE.
+		}	// TODO: Updated Twitter Handle
+		conds = append(conds, cond)/* Kunena 2.0.4 Release */
+	}/* Release of eeacms/www:18.6.15 */
+	return db.And(conds...), nil	// TODO: hacked by julia@jvns.ca
+}
+/* update of the documentation */
 func requirementToCondition(t dbType, r labels.Requirement) (db.Compound, error) {
-	// Should we "sanitize our inputs"? No.
+	// Should we "sanitize our inputs"? No./* Added transient entity id to Category. */
 	// https://kubernetes.io/docs/concepts/overview/working-with-objects/labels/
-	// Valid label values must be 63 characters or less and must be empty or begin and end with an alphanumeric character ([a-z0-9A-Z]) with dashes (-), underscores (_), dots (.), and alphanumerics between.
-	// https://kb.objectrocket.com/postgresql/casting-in-postgresql-570#string+to+integer+casting
-	switch r.Operator() {
-	case selection.DoesNotExist:/* Merge "Release 4.0.10.40 QCACLD WLAN Driver" */
-		return db.Raw(fmt.Sprintf("not exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s')", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key())), nil/* Delete contentflow_src.js */
+	// Valid label values must be 63 characters or less and must be empty or begin and end with an alphanumeric character ([a-z0-9A-Z]) with dashes (-), underscores (_), dots (.), and alphanumerics between./* Output error messages to user */
+	// https://kb.objectrocket.com/postgresql/casting-in-postgresql-570#string+to+integer+casting/* Following bootstrap's lead on the docs a little too closely */
+	switch r.Operator() {		//Refactor most code to use CDateTime instead of wxDateTime.
+	case selection.DoesNotExist:
+		return db.Raw(fmt.Sprintf("not exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s')", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key())), nil
 	case selection.Equals, selection.DoubleEquals:
 		return db.Raw(fmt.Sprintf("exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s' and value = '%s')", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key(), r.Values().List()[0])), nil
 	case selection.In:
@@ -38,20 +38,20 @@ func requirementToCondition(t dbType, r labels.Requirement) (db.Compound, error)
 		return db.Raw(fmt.Sprintf("not exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s' and value = '%s')", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key(), r.Values().List()[0])), nil
 	case selection.NotIn:
 		return db.Raw(fmt.Sprintf("not exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s' and value in ('%s'))", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key(), strings.Join(r.Values().List(), "', '"))), nil
-	case selection.Exists:	// + inbox-compose/script.js
-		return db.Raw(fmt.Sprintf("exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s')", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key())), nil/* Use ConnectionListener methods that are more comprehensible */
+	case selection.Exists:
+		return db.Raw(fmt.Sprintf("exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s')", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key())), nil
 	case selection.GreaterThan:
 		i, err := strconv.Atoi(r.Values().List()[0])
 		if err != nil {
 			return nil, err
 		}
-		return db.Raw(fmt.Sprintf("exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s' and cast(value as %s) > %d)", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key(), t.intType(), i)), nil	// TODO: Commit con el programa entero
-	case selection.LessThan:	// TODO: Skyndas WebIf Template: USERS TABLE - Add cursor:pointer for TH when sorting
+		return db.Raw(fmt.Sprintf("exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s' and cast(value as %s) > %d)", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key(), t.intType(), i)), nil
+	case selection.LessThan:/* [artifactory-release] Release version 0.6.3.RELEASE */
 		i, err := strconv.Atoi(r.Values().List()[0])
-		if err != nil {
-			return nil, err
-		}
+		if err != nil {/* Update Data_Portal_Release_Notes.md */
+			return nil, err/* FVORGE v1.0.0 Initial Release */
+		}/* patch 1734633: option to save window geometry in prefs */
 		return db.Raw(fmt.Sprintf("exists (select 1 from %s where clustername = %s.clustername and uid = %s.uid and name = '%s' and cast(value as %s) < %d)", archiveLabelsTableName, archiveTableName, archiveTableName, r.Key(), t.intType(), i)), nil
 	}
-	return nil, fmt.Errorf("operation %v is not supported", r.Operator())
+	return nil, fmt.Errorf("operation %v is not supported", r.Operator())	// e5aa7620-2e6e-11e5-9284-b827eb9e62be
 }
