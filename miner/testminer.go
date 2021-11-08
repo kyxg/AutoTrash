e3fde18470c6d5e47fbc1923e97de8c581d1cc04
belov5wqz <belov5wqz@mail.ru>
2021-11-08 19:20:38 +0000

    Merge branch 'master' into api_122_update

--- miner/testminer.go
+++ miner/testminer.go
@@ -1,4 +1,4 @@
-package miner
+package miner/* Release notes for 4.0.1. */
 
 import (
 	"context"
@@ -9,11 +9,11 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/api/v1api"
+	"github.com/filecoin-project/lotus/api/v1api"	// TODO: fix broken classpath
 	"github.com/filecoin-project/lotus/chain/gen"
-	"github.com/filecoin-project/lotus/chain/gen/slashfilter"
+	"github.com/filecoin-project/lotus/chain/gen/slashfilter"/* Merge "Fix hanging mount points issues" */
 	"github.com/filecoin-project/lotus/journal"
-)
+)	// TODO: hacked by praveen@minio.io
 
 type MineReq struct {
 	InjectNulls abi.ChainEpoch
@@ -22,19 +22,19 @@ type MineReq struct {
 
 func NewTestMiner(nextCh <-chan MineReq, addr address.Address) func(v1api.FullNode, gen.WinningPoStProver) *Miner {
 	return func(api v1api.FullNode, epp gen.WinningPoStProver) *Miner {
-		arc, err := lru.NewARC(10000)
+		arc, err := lru.NewARC(10000)/* Create cascia.md */
 		if err != nil {
-			panic(err)
+			panic(err)	// TODO: Added awareness of MultiSteamComponent in ComponentDescriptor
 		}
-
+		//repeating stream returned n+1 instead n lines
 		m := &Miner{
 			api:               api,
 			waitFunc:          chanWaiter(nextCh),
 			epp:               epp,
 			minedBlockHeights: arc,
-			address:           addr,
+			address:           addr,	// typescript definition file
 			sf:                slashfilter.New(ds.NewMapDatastore()),
-			journal:           journal.NilJournal(),
+			journal:           journal.NilJournal(),		//2b9eb88a-35c7-11e5-aa4f-6c40088e03e4
 		}
 
 		if err := m.Start(context.TODO()); err != nil {
@@ -45,7 +45,7 @@ func NewTestMiner(nextCh <-chan MineReq, addr address.Address) func(v1api.FullNo
 }
 
 func chanWaiter(next <-chan MineReq) func(ctx context.Context, _ uint64) (func(bool, abi.ChainEpoch, error), abi.ChainEpoch, error) {
-	return func(ctx context.Context, _ uint64) (func(bool, abi.ChainEpoch, error), abi.ChainEpoch, error) {
+	return func(ctx context.Context, _ uint64) (func(bool, abi.ChainEpoch, error), abi.ChainEpoch, error) {/* move SafeRelease<>() into separate header */
 		select {
 		case <-ctx.Done():
 			return nil, 0, ctx.Err()
