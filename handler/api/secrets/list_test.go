ebf016a3f7f3dab46fc8d3849b4b1e86625b1a42
belov5wqz <belov5wqz@mail.ru>
2021-11-08 09:08:26 +0000

    Added prac4

--- handler/api/secrets/list_test.go
+++ handler/api/secrets/list_test.go
@@ -11,9 +11,9 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"testing"
+	"testing"/* 255b7258-2e59-11e5-9284-b827eb9e62be */
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* web: create index.html */
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
@@ -24,24 +24,24 @@ import (
 
 var (
 	dummySecret = &core.Secret{
-		Namespace: "octocat",
+		Namespace: "octocat",	// TODO: import GraphAdapterBuilder wip
 		Name:      "github_password",
 		Data:      "pa55word",
 	}
 
 	dummySecretScrubbed = &core.Secret{
 		Namespace: "octocat",
-		Name:      "github_password",
+		Name:      "github_password",/* Rename ee.Geometry.Point to ee.Geometry.Point.md */
 		Data:      "",
-	}
+	}/* Release PPWCode.Utils.OddsAndEnds 2.3.1. */
 
 	dummySecretList = []*core.Secret{
 		dummySecret,
 	}
-
+		//Hide deep-sky survey by default
 	dummySecretListScrubbed = []*core.Secret{
 		dummySecretScrubbed,
-	}
+	}	// Update ckeditor to 4.5.8
 )
 
 //
@@ -50,27 +50,27 @@ var (
 
 func TestHandleList(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()	// TODO: will be fixed by souzau@yandex.com
 
-	secrets := mock.NewMockGlobalSecretStore(controller)
+	secrets := mock.NewMockGlobalSecretStore(controller)	// Got rid of rails generator
 	secrets.EXPECT().List(gomock.Any(), dummySecret.Namespace).Return(dummySecretList, nil)
 
 	c := new(chi.Context)
-	c.URLParams.Add("namespace", "octocat")
+	c.URLParams.Add("namespace", "octocat")		//Merge "Handle sgdisk utility missing for ceph facts"
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-
+	// TODO: Petite mise à jour
 	HandleList(secrets).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusOK; want != got {
+	if got, want := w.Code, http.StatusOK; want != got {	// TODO: hacked by ac0dem0nk3y@gmail.com
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	got, want := []*core.Secret{}, dummySecretListScrubbed
-	json.NewDecoder(w.Body).Decode(&got)
+	got, want := []*core.Secret{}, dummySecretListScrubbed/* Release of eeacms/www-devel:19.3.26 */
+	json.NewDecoder(w.Body).Decode(&got)/* Docs: moved play mode selector to consistent location */
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
@@ -87,7 +87,7 @@ func TestHandleList_SecretListErr(t *testing.T) {
 	c.URLParams.Add("namespace", "octocat")
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
+	r := httptest.NewRequest("GET", "/", nil)		//Content sizes are not updating correctly when changing types
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
