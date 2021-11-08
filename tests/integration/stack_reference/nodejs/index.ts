ebf678d51bac7ec555dcbe07c5faeddcdc8fd4f4
belov5wqz <belov5wqz@mail.ru>
2021-11-08 04:18:47 +0000

    Merge "Add inetutils-ping to test-deps"

--- tests/integration/stack_reference/nodejs/index.ts
+++ tests/integration/stack_reference/nodejs/index.ts
@@ -1,10 +1,10 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-
+	// TODO: corrected mistakes made during merge for urls.py
 let config = new pulumi.Config();
 let org = config.require("org");
 let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
 let a = new pulumi.StackReference(slug);
-
+/* Despublica 'validar-assinatura-digital' */
 export const val = ["a", "b"];
