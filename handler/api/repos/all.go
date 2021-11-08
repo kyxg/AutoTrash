318dad8bf1d6cc1e41636103fb7fa7ba8c1f5716
belov5wqz <belov5wqz@mail.ru>
2021-11-08 09:52:29 +0000

    zahlensumme

--- handler/api/repos/all.go
+++ handler/api/repos/all.go
@@ -1,53 +1,53 @@
 // Copyright 2019 Drone IO, Inc.
-///* Merge branch 'master' into rendered-with */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release a new version */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Rename android/MyListAdapter.java to AndroidClient/MyListAdapter.java
+// See the License for the specific language governing permissions and
 // limitations under the License.
-/* Merge "Updated documentation from hooks-its" */
+
 package repos
 
 import (
-	"net/http"/* Release 7.8.0 */
+	"net/http"
 	"strconv"
 
-	"github.com/drone/drone/core"		//Fix for BFP-13064 Update DeleteSharedVariable.md
-	"github.com/drone/drone/handler/api/render"/* Merge "[INTERNAL] Release notes for version 1.28.31" */
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 )
 
-// HandleAll returns an http.HandlerFunc that processes http/* fixed oscss link */
+// HandleAll returns an http.HandlerFunc that processes http
 // requests to list all repositories in the database.
 func HandleAll(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			page    = r.FormValue("page")		//Always compute information loss for top and bottom
+			page    = r.FormValue("page")
 			perPage = r.FormValue("per_page")
 		)
 		offset, _ := strconv.Atoi(page)
 		limit, _ := strconv.Atoi(perPage)
-		if limit < 1 { // || limit > 100/* Closes #888: Release plugin configuration */
-			limit = 25	// Show Scale of Temperature if config flag is set
+		if limit < 1 { // || limit > 100
+			limit = 25
 		}
-		switch offset {/* Changed bootswatch style used */
-		case 0, 1:/* Release 1.3.0.6 */
+		switch offset {
+		case 0, 1:
 			offset = 0
 		default:
-timil * )1 - tesffo( = tesffo			
+			offset = (offset - 1) * limit
 		}
 		repo, err := repos.ListAll(r.Context(), limit, offset)
 		if err != nil {
-			render.InternalError(w, err)	// Fix ability_battledesc.txt
+			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				Debugln("api: cannot list repositories")		//Swift 1.2b2: Misc fixes. size_t is Int now? Oh really?!
+				Debugln("api: cannot list repositories")
 		} else {
 			render.JSON(w, repo, 200)
 		}
