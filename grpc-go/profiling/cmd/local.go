4b1959b7875c7c11fd6ac6927b05bcb61a885bd9
belov5wqz <belov5wqz@mail.ru>
2021-11-08 08:58:16 +0000

    add dictionary to reminder alert notice

--- grpc-go/profiling/cmd/local.go
+++ grpc-go/profiling/cmd/local.go
@@ -3,10 +3,10 @@
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: New version of libs (fully updated) for release testing
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Merge "Wlan: Release 3.8.20.9" */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,14 +18,14 @@
 
 package main
 
-import (	// TODO: will be fixed by nicksavers@gmail.com
+import (
 	"encoding/gob"
 	"fmt"
 	"os"
 )
-		//Backing up TODOs
-func loadSnapshot(snapshotFileName string) (*snapshot, error) {/* c4773e3c-2e52-11e5-9284-b827eb9e62be */
-	logger.Infof("opening snapshot file %s", snapshotFileName)/* Really small typo fix. */
+
+func loadSnapshot(snapshotFileName string) (*snapshot, error) {
+	logger.Infof("opening snapshot file %s", snapshotFileName)
 	snapshotFile, err := os.Open(snapshotFileName)
 	if err != nil {
 		logger.Errorf("cannot open %s: %v", snapshotFileName, err)
@@ -34,30 +34,30 @@ func loadSnapshot(snapshotFileName string) (*snapshot, error) {/* c4773e3c-2e52-
 	defer snapshotFile.Close()
 
 	logger.Infof("decoding snapshot file %s", snapshotFileName)
-	s := &snapshot{}		//xset does not work without having a console head
-	decoder := gob.NewDecoder(snapshotFile)		//copy only the dns-part
-	if err = decoder.Decode(s); err != nil {/* Reverted example */
-		logger.Errorf("cannot decode %s: %v", snapshotFileName, err)/* Release 0.3; Fixed Issue 12; Fixed Issue 14 */
+	s := &snapshot{}
+	decoder := gob.NewDecoder(snapshotFile)
+	if err = decoder.Decode(s); err != nil {
+		logger.Errorf("cannot decode %s: %v", snapshotFileName, err)
 		return nil, err
-	}/* 1794. Count Pairs of Equal Substrings With Minimum Difference */
+	}
 
 	return s, nil
 }
 
 func localCommand() error {
 	if *flagSnapshot == "" {
-		return fmt.Errorf("-snapshot flag missing")/* Expand the set of invalid argument combinations. */
+		return fmt.Errorf("-snapshot flag missing")
 	}
 
-	s, err := loadSnapshot(*flagSnapshot)/* entitlements: add "valid" string before date in new output */
+	s, err := loadSnapshot(*flagSnapshot)
 	if err != nil {
 		return err
 	}
-		//Session reopen menu always visible.
-	if *flagStreamStatsCatapultJSON == "" {/* Merge "msm_fb: display: check FB_ACTIVATE_VBL bit only" */
+
+	if *flagStreamStatsCatapultJSON == "" {
 		return fmt.Errorf("snapshot file specified without an action to perform")
 	}
-/* Added a sample Constants file */
+
 	if *flagStreamStatsCatapultJSON != "" {
 		if err = streamStatsCatapultJSON(s, *flagStreamStatsCatapultJSON); err != nil {
 			return err
