60fa71a7a9e80d2e57f4255db6d223ddfdcdb34b
belov5wqz <belov5wqz@mail.ru>
2021-11-08 19:48:31 +0000

    Delete app_one_method.py

--- tests/integration/stack_reference_multi/python/importer/__main__.py
+++ tests/integration/stack_reference_multi/python/importer/__main__.py
@@ -1,10 +1,10 @@
 import pulumi
 
 config = pulumi.Config()
-
+		//1ac489d4-2e62-11e5-9284-b827eb9e62be
 exporterStackName = config.require('exporter_stack_name')
 org = config.require('org')
-a = pulumi.StackReference(f'{org}/exporter/{exporterStackName}')
+a = pulumi.StackReference(f'{org}/exporter/{exporterStackName}')	// TODO: releasing version 0.0.6-0ubuntu1~ppa1
 
-pulumi.export('val1', a.require_output('val'))		//Fix library import issues
-pulumi.export('val2', pulumi.Output.secret(['d', 'x']))
+pulumi.export('val1', a.require_output('val'))
+pulumi.export('val2', pulumi.Output.secret(['d', 'x']))/* Finalized 3.9 OS Release Notes. */
