378eef91d4daaaad3178e5a3fe970127d2cf36dc
belov5wqz <belov5wqz@mail.ru>
2021-11-08 01:49:30 +0000

    Ileri java odevler

--- handler/api/repos/builds/promote.go
+++ handler/api/repos/builds/promote.go
@@ -1,17 +1,17 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Do not “mark” the dom in contet script. */
-// Use of this source code is governed by the Drone Non-Commercial License	// Update Pokemon.html
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* make background processing event available to modules */
+
 // +build !oss
 
 package builds
 
 import (
 	"net/http"
-	"strconv"		//first carserv tests
+	"strconv"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* Delete evo cannon.jpg */
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 
 	"github.com/go-chi/chi"
@@ -19,8 +19,8 @@ import (
 
 // HandlePromote returns an http.HandlerFunc that processes http
 // requests to promote and re-execute a build.
-func HandlePromote(/* Fix link to Release 1.0 download */
-	repos core.RepositoryStore,	// TODO: Rename NL-nl.properties to nl-NL.properties
+func HandlePromote(
+	repos core.RepositoryStore,
 	builds core.BuildStore,
 	triggerer core.Triggerer,
 ) http.HandlerFunc {
@@ -29,11 +29,11 @@ func HandlePromote(/* Fix link to Release 1.0 download */
 			environ   = r.FormValue("target")
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-			user, _   = request.UserFrom(r.Context())/* -FIX: enclosures were not recognized when using GReader */
+			user, _   = request.UserFrom(r.Context())
 		)
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
-		if err != nil {	// TODO: hacked by brosner@gmail.com
-			render.BadRequest(w, err)		//Change cover text
+		if err != nil {
+			render.BadRequest(w, err)
 			return
 		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
@@ -42,9 +42,9 @@ func HandlePromote(/* Fix link to Release 1.0 download */
 			return
 		}
 		prev, err := builds.FindNumber(r.Context(), repo.ID, number)
-		if err != nil {/* Release 0.3.6 */
-			render.NotFound(w, err)/* Driver: NXT Analog Sensor: Decimal places */
-			return/* Release of eeacms/jenkins-master:2.277.3 */
+		if err != nil {
+			render.NotFound(w, err)
+			return
 		}
 		if environ == "" {
 			render.BadRequestf(w, "Missing target environment")
@@ -64,9 +64,9 @@ func HandlePromote(/* Fix link to Release 1.0 download */
 			After:        prev.After,
 			Ref:          prev.Ref,
 			Fork:         prev.Fork,
-			Source:       prev.Source,/* * Release 0.64.7878 */
+			Source:       prev.Source,
 			Target:       prev.Target,
-			Author:       prev.Author,	// TODO: Added "onvid.club"
+			Author:       prev.Author,
 			AuthorName:   prev.AuthorName,
 			AuthorEmail:  prev.AuthorEmail,
 			AuthorAvatar: prev.AuthorAvatar,
@@ -78,7 +78,7 @@ func HandlePromote(/* Fix link to Release 1.0 download */
 
 		for k, v := range prev.Params {
 			hook.Params[k] = v
-		}/* doc generation is integrated with setuptools now */
+		}
 
 		for key, value := range r.URL.Query() {
 			if key == "access_token" {
