9df7966edd28cf764ddbac3ce9bb9d00b150cdeb
belov5wqz <belov5wqz@mail.ru>
2021-11-08 23:56:26 +0000

    :wrench: ajouter signes de + et de x

--- store/shared/db/db_test.go
+++ store/shared/db/db_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// Delete CamdRAED.py
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
