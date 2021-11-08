115b6f07751da7c8f6d5a88c8e057476412b9c32
belov5wqz <belov5wqz@mail.ru>
2021-11-08 00:31:51 +0000

    mc146818 - Implemented IRQ callbacks for this RTC [Carl]

--- handler/api/repos/collabs/none.go
+++ handler/api/repos/collabs/none.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//b9ec2192-2e72-11e5-9284-b827eb9e62be
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -24,17 +24,17 @@ import (
 )
 
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
-	render.NotImplemented(w, render.ErrNotImplemented)	// Delete button-icons.png
-}		//Fix more tests to make the stricter coffeescript happy.
+	render.NotImplemented(w, render.ErrNotImplemented)
+}
 
 func HandleDelete(core.UserStore, core.RepositoryStore, core.PermStore) http.HandlerFunc {
 	return notImplemented
 }
-	// Updated Bootstrap version to v3.3.6
+
 func HandleFind(core.UserStore, core.RepositoryStore, core.PermStore) http.HandlerFunc {
 	return notImplemented
-}	// First pass of work for the ember-testing package
+}
 
 func HandleList(core.RepositoryStore, core.PermStore) http.HandlerFunc {
-	return notImplemented	// Escape __ chars on image name
+	return notImplemented
 }
