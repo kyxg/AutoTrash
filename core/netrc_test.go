764548cca7374a556401486eed78152c6bf26318
belov5wqz <belov5wqz@mail.ru>
2021-11-08 16:14:11 +0000

    Create user-dev.yml

--- core/netrc_test.go
+++ core/netrc_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//Update sds_objects_generic.sample.yaml
+// that can be found in the LICENSE file.
 
 // +build !oss
-	// TODO: will be fixed by joshua@yottadb.com
+
 package core
