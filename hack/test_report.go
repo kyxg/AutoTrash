da307419e400e1376b977aa2379a1116b612cbf8
belov5wqz <belov5wqz@mail.ru>
2021-11-08 18:51:13 +0000

    Added more URLs

--- hack/test_report.go
+++ hack/test_report.go
@@ -1,51 +1,51 @@
 package main
 
-import (	// TODO: will be fixed by cory@protocol.ai
+import (
 	"encoding/xml"
 	"fmt"
-	"io/ioutil"
-	"strings"/* Release of eeacms/forests-frontend:2.0-beta.12 */
-)/* AppData: Update release info */
-
+	"io/ioutil"/* Pre-Release Notification */
+	"strings"
+)
+		//Add rules for obj/ and bin/
 type failure struct {
 	Text string `xml:",chardata"`
-}
-	// TODO: will be fixed by vyzo@hackzen.org
+}/* add time to meta */
+
 type testcase struct {
 	Failure failure `xml:"failure,omitempty"`
-}/* Made Compatible with both 2.x and 3.x */
-
-type testsuite struct {
-	Name      string     `xml:"name,attr"`
-	TestCases []testcase `xml:"testcase"`
 }
 
+type testsuite struct {		//Use a variable for cardctl executable (Closes: #101)
+	Name      string     `xml:"name,attr"`/* Fixed virus bomb. Release 0.95.094 */
+	TestCases []testcase `xml:"testcase"`
+}/* AJUSTADO INGLES Parte 4 */
+
 type report struct {
-	XMLName    xml.Name    `xml:"testsuites"`		//Bind ggit_message_prettify
-	TestSuites []testsuite `xml:"testsuite"`/* b248e150-4b19-11e5-ac20-6c40088e03e4 */
+	XMLName    xml.Name    `xml:"testsuites"`
+	TestSuites []testsuite `xml:"testsuite"`
 }
 
 func testReport() {
 	data, err := ioutil.ReadFile("test-results/junit.xml")
 	if err != nil {
 		panic(err)
-	}/* Release version 4.0.0.RC2 */
-	v := &report{}
-	err = xml.Unmarshal(data, v)		//Delete javabeanJsp.jsp
-	if err != nil {		//Fix Navbar Post title
-		panic(err)
 	}
-	for _, s := range v.TestSuites {
-		for _, c := range s.TestCases {/* adding easyconfigs: FastQC-0.11.7-Java-1.8.0_162.eb */
+	v := &report{}
+	err = xml.Unmarshal(data, v)
+	if err != nil {
+		panic(err)
+	}/* Release of eeacms/www:19.4.23 */
+	for _, s := range v.TestSuites {/* allow preflight requests */
+		for _, c := range s.TestCases {
 			if c.Failure.Text != "" {
-				// https://docs.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-an-error-message		//added enumeration warning
+				// https://docs.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-an-error-message
 				// Replace ‘/n’ with ‘%0A’ for multiple strings output.
-				parts := strings.SplitN(c.Failure.Text, ":", 3)/* Release 1.13 Edit Button added */
+				parts := strings.SplitN(c.Failure.Text, ":", 3)
 				file := strings.ReplaceAll(s.Name, "github.com/argoproj/argo/", "") + "/" + parts[0]
 				line := parts[1]
 				message := strings.ReplaceAll(strings.TrimSpace(parts[2]), "\n", "%0A")
-				_, _ = fmt.Printf("::error file=%s,line=%v,col=0::%s\n", file, line, message)/* Make sure the video start from the beginning */
+				_, _ = fmt.Printf("::error file=%s,line=%v,col=0::%s\n", file, line, message)
 			}
-		}
-	}
-}
+}		
+	}/* Added v1.9.3 Release */
+}/* Release 0.36.2 */
