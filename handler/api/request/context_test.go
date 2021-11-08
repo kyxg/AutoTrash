f899adec4df068bee4c1caa0ccf41bf92845efa4
belov5wqz <belov5wqz@mail.ru>
2021-11-08 23:22:13 +0000

    [build] remove jcl and log4j log output from index.tests

--- handler/api/request/context_test.go
+++ handler/api/request/context_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Updated links to new website and documentation */
-package request/* [Updated installation steps to use installer] */
+
+package request
