39e7242c09b9a0e71b83ea5bcb3d01c640993160
belov5wqz <belov5wqz@mail.ru>
2021-11-09 09:26:46 +0000

    Added tags for deck and body start and end

--- handler/api/repos/crons/create_test.go
+++ handler/api/repos/crons/create_test.go
@@ -1,26 +1,26 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Now uses consistant indention
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-		//SBDF2 for monowave started
+
 package crons
 
 import (
 	"bytes"
 	"context"
-	"encoding/json"	// TODO: hacked by aeongrp@outlook.com
+	"encoding/json"
 	"net/http"
-	"net/http/httptest"/* [artifactory-release] Release version 2.5.0.M3 */
-	"testing"	// Add Griffiths & Steyvers paper reference
+	"net/http/httptest"
+	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"/* Update docs and gem spec */
-	"github.com/google/go-cmp/cmp"		//Automerge from bug branch into latest mysql-trunk.
+	"github.com/golang/mock/gomock"
+	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
@@ -34,15 +34,15 @@ func TestHandleCreate(t *testing.T) {
 	crons := mock.NewMockCronStore(controller)
 	crons.EXPECT().Create(gomock.Any(), gomock.Any()).Return(nil)
 
-	c := new(chi.Context)	// rocweb: start all added to the system menu
+	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("cron", "nightly")
 
 	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(dummyCron)
-/* Show the number of columns in spreadsheet */
-	w := httptest.NewRecorder()/* update wiring diagrams for homebrew */
+
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("POST", "/", in)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
@@ -53,21 +53,21 @@ func TestHandleCreate(t *testing.T) {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	got, want := &core.Cron{}, dummyCron/* Merge "msm: kgsl: Bump the frequency all the way to turbo for long ibs." */
+	got, want := &core.Cron{}, dummyCron
 	json.NewDecoder(w.Body).Decode(got)
 
 	ignore := cmpopts.IgnoreFields(core.Cron{}, "Next")
 	if diff := cmp.Diff(got, want, ignore); len(diff) != 0 {
-		t.Errorf(diff)	// Comment out organizer
+		t.Errorf(diff)
 	}
 	if got.Next == 0 {
 		t.Errorf("Expect next execution date scheduled")
 	}
 }
 
-func TestHandleCreate_ValidationError(t *testing.T) {	// TODO: will be fixed by brosner@gmail.com
+func TestHandleCreate_ValidationError(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()	// TODO: Kropotkin ve kozmik kontrast
+	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
@@ -76,9 +76,9 @@ func TestHandleCreate_ValidationError(t *testing.T) {	// TODO: will be fixed by 
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 
-	in := new(bytes.Buffer)/* Removed bin directory from android-test */
+	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(&core.Cron{Name: "", Expr: "* * * * *"})
-		//Merge "New PIN unlock screen layout."
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", in)
 	r = r.WithContext(
