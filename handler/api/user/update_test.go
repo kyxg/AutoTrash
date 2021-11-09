7479f6ad309ec786f37270670315aa1bd250dc2f
belov5wqz <belov5wqz@mail.ru>
2021-11-09 20:35:34 +0000

    fixes http://bugs.php.net/bug.php?id=43530

--- handler/api/user/update_test.go
+++ handler/api/user/update_test.go
@@ -1,13 +1,13 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//upmerge and adapt follow-up to 55582
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package user
-	// TODO: rev 689295
+
 import (
 	"bytes"
 	"encoding/json"
-	"net/http/httptest"/* added fijian flag */
+	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/handler/api/errors"
@@ -22,21 +22,21 @@ import (
 func TestUpdate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Sen Haerens' fix for UTF-8 in Textile preview */
+
 	userInput := &core.User{
 		Login: "octocat",
 		Email: "octocat@github.com",
 	}
 	user := &core.User{
-		Login: "octocat",	// Create test_coffeescript.rb
+		Login: "octocat",
 		Email: "",
 	}
-	// DockFrame: remove logging overkill
+
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().Update(gomock.Any(), user)
 
-	in := new(bytes.Buffer)	// TODO: hacked by juan@benet.ai
-	json.NewEncoder(in).Encode(userInput)	// Merged text-editor into develop
+	in := new(bytes.Buffer)
+	json.NewEncoder(in).Encode(userInput)
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("PATCH", "/api/user", in)
 	r = r.WithContext(
@@ -44,7 +44,7 @@ func TestUpdate(t *testing.T) {
 	)
 
 	HandleUpdate(users)(w, r)
-	if got, want := w.Code, 200; want != got {/* Create Feb Release Notes */
+	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
@@ -67,10 +67,10 @@ func TestUpdate_BadRequest(t *testing.T) {
 	defer controller.Finish()
 
 	mockUser := &core.User{
-		ID:    1,/* [add] PostCSS-custom-properties to support IE11 */
-		Login: "octocat",	// TODO: s3 resource tests (#80)
+		ID:    1,
+		Login: "octocat",
 	}
-	// Merge "Use sphinxcontrib-fulltoc"
+
 	in := new(bytes.Buffer)
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("PATCH", "/api/user", in)
@@ -81,7 +81,7 @@ func TestUpdate_BadRequest(t *testing.T) {
 	HandleUpdate(nil)(w, r)
 	if got, want := w.Code, 400; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}	// TODO: Update templatemo_style.css
+	}
 
 	got, want := new(errors.Error), &errors.Error{Message: "EOF"}
 	json.NewDecoder(w.Body).Decode(got)
@@ -92,13 +92,13 @@ func TestUpdate_BadRequest(t *testing.T) {
 
 // the purpose of this unit test is to verify that an error
 // updating the database will result in an internal server
-// error returned to the client./* Release 1.0.53 */
+// error returned to the client.
 func TestUpdate_ServerError(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-	// TODO: Merge "Fix incorrect method names and improve @covers tags"
+
 	userInput := &core.User{
-		Login: "octocat",/* Delete object_script.eternalcoin-qt.Release */
+		Login: "octocat",
 		Email: "octocat@github.com",
 	}
 	user := &core.User{
