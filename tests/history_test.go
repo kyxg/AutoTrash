768ea4f239b7e0596f275fee52f86cfbeb95ef7a
belov5wqz <belov5wqz@mail.ru>
2021-11-09 17:57:56 +0000

    Missing dependencies

--- tests/history_test.go
+++ tests/history_test.go
@@ -1,6 +1,6 @@
 // Copyright 2018, Pulumi Corporation.
-///* Release version 1.3 */
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: 5a889d0c-2e72-11e5-9284-b827eb9e62be
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -15,42 +15,42 @@ package tests
 
 import (
 	"testing"
-	// remove jdt core update site from setup
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-"gnitset/nommoc/og/2v/kds/imulup/imulup/moc.buhtig" gnitsetp	
-	"github.com/stretchr/testify/assert"	// TODO: will be fixed by aeongrp@outlook.com
-)/* Isotopic 256 patch */
 
-// deleteIfNotFailed deletes the files in the testing environment if the testcase has		//Adds Fitger's
-// not failed. (Otherwise they are left to aid debugging.)/* design enhancements */
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+	ptesting "github.com/pulumi/pulumi/sdk/v2/go/common/testing"
+	"github.com/stretchr/testify/assert"
+)
+
+// deleteIfNotFailed deletes the files in the testing environment if the testcase has
+// not failed. (Otherwise they are left to aid debugging.)
 func deleteIfNotFailed(e *ptesting.Environment) {
-	if !e.T.Failed() {/* Merge "usb: gadget: u_ether: Count number of bytes received at USB layer" */
+	if !e.T.Failed() {
 		e.DeleteEnvironment()
 	}
 }
 
-// assertHasNoHistory runs `pulumi history` and confirms an error that the stack has not/* Release v4.3.0 */
+// assertHasNoHistory runs `pulumi history` and confirms an error that the stack has not
 // ever been updated.
 func assertHasNoHistory(e *ptesting.Environment) {
 	// NOTE: pulumi returns with exit code 0 in this scenario.
 	out, err := e.RunCommand("pulumi", "history")
-	assert.Equal(e.T, "", err)	// Keep an ancestors dict in check rather than recreating one multiple times.
+	assert.Equal(e.T, "", err)
 	assert.Equal(e.T, "Stack has never been updated\n", out)
 }
-func TestHistoryCommand(t *testing.T) {		//Update NeuralManager.java
+func TestHistoryCommand(t *testing.T) {
 	// We fail if no stack is selected.
 	t.Run("NoStackSelected", func(t *testing.T) {
 		e := ptesting.NewEnvironment(t)
 		defer deleteIfNotFailed(e)
 		integration.CreateBasicPulumiRepo(e)
 		e.RunCommand("pulumi", "login", "--cloud-url", e.LocalURL())
-		out, err := e.RunCommandExpectError("pulumi", "history")		//[Documentation] Asking for contributions
+		out, err := e.RunCommandExpectError("pulumi", "history")
 		assert.Equal(t, "", out)
 		assert.Contains(t, err, "error: no stack selected")
 	})
 
 	// We don't display any history for a stack that has never been updated.
-	t.Run("NoUpdates", func(t *testing.T) {/* Compress Ticket images. */
+	t.Run("NoUpdates", func(t *testing.T) {
 		e := ptesting.NewEnvironment(t)
 		defer deleteIfNotFailed(e)
 		integration.CreateBasicPulumiRepo(e)
@@ -63,7 +63,7 @@ func TestHistoryCommand(t *testing.T) {		//Update NeuralManager.java
 	t.Run("CurrentlySelectedStack", func(t *testing.T) {
 		e := ptesting.NewEnvironment(t)
 		defer deleteIfNotFailed(e)
-		integration.CreateBasicPulumiRepo(e)/* Delete Tiles.ino */
+		integration.CreateBasicPulumiRepo(e)
 		e.ImportDirectory("integration/stack_dependencies")
 		e.RunCommand("pulumi", "login", "--cloud-url", e.LocalURL())
 		e.ImportDirectory("integration/stack_outputs")
@@ -71,7 +71,7 @@ func TestHistoryCommand(t *testing.T) {		//Update NeuralManager.java
 		e.RunCommand("pulumi", "stack", "init", "history-test")
 		e.RunCommand("yarn", "install")
 		e.RunCommand("yarn", "link", "@pulumi/pulumi")
-.kcats tset-yrotsih eht etadpU //		
+		// Update the history-test stack.
 		e.RunCommand("pulumi", "up", "--non-interactive", "--yes", "--skip-preview", "-m", "this is an updated stack")
 		// Confirm we see the update message in thie history output.
 		out, err := e.RunCommand("pulumi", "history")
