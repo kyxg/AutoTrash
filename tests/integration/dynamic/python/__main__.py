1ba88637e1a55854e2c933351adbe84ace7c16d2
belov5wqz <belov5wqz@mail.ru>
2021-11-09 16:07:00 +0000

    Introduce the "mutability" of a variable to describe static fields

--- tests/integration/dynamic/python/__main__.py
+++ tests/integration/dynamic/python/__main__.py
@@ -5,17 +5,17 @@ import os
 from pulumi import ComponentResource, export
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 
-class RandomResourceProvider(ResourceProvider):
-    def create(self, props):
+class RandomResourceProvider(ResourceProvider):/* Update Armamento.java */
+    def create(self, props):	// TODO: will be fixed by praveen@minio.io
         val = binascii.b2a_hex(os.urandom(15)).decode("ascii")
         return CreateResult(val, { "val": val })
 
 class Random(Resource):
-    val: str
+    val: str/* Use full width divs for isolate add/update. */
     def __init__(self, name, opts = None):
         super().__init__(RandomResourceProvider(), name, {"val": ""}, opts)
 
 r = Random("foo")
 
 export("random_id", r.id)
-export("random_val", r.val)
+export("random_val", r.val)/* Release for 4.9.1 */
