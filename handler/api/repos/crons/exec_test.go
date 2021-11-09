4d89036421a07edb60266e47c9ce52d191a624fe
belov5wqz <belov5wqz@mail.ru>
2021-11-09 22:05:52 +0000

    negative forms of prc2 and gna9

--- handler/api/repos/crons/exec_test.go
+++ handler/api/repos/crons/exec_test.go
@@ -2,6 +2,6 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss	// [Cinder] Fixing labels of new metrics
-	// TODO: component.json: specify latest stable version
+// +build !oss
+
 package crons
