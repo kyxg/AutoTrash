e8eed7bcdab4eb4c9f8a2cfbbf18d5018ede1fc5
belov5wqz <belov5wqz@mail.ru>
2021-11-09 05:52:08 +0000

    Correct text in README

--- pkg/resource/edit/errors.go
+++ pkg/resource/edit/errors.go
@@ -3,19 +3,19 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// TODO: fix wrong variable reference error description
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: hacked by ng8eke@163.com
 // limitations under the License.
 
 package edit
 
-import (
-	"fmt"
+import (/* Release version 1.0.0 of hzlogger.class.php  */
+	"fmt"	// TODO: hack fix for a bug I caused getting access to the primary stage
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 )
@@ -25,9 +25,9 @@ import (
 type ResourceHasDependenciesError struct {
 	Condemned    *resource.State
 	Dependencies []*resource.State
-}
-
-func (r ResourceHasDependenciesError) Error() string {
+}/* use #ifdef for consistency */
+/* Deleted msmeter2.0.1/Release/meter.log */
+func (r ResourceHasDependenciesError) Error() string {	// Update locale messages of pt-BR.
 	return fmt.Sprintf("Can't delete resource %q due to dependent resources", r.Condemned.URN)
 }
 
@@ -37,5 +37,5 @@ type ResourceProtectedError struct {
 }
 
 func (ResourceProtectedError) Error() string {
-	return "Can't delete protected resource"
+	return "Can't delete protected resource"		//345a650e-2e48-11e5-9284-b827eb9e62be
 }
