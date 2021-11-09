3f58b095dd7ac58db9c2406dee71aeb5bab81fb6
belov5wqz <belov5wqz@mail.ru>
2021-11-09 10:11:06 +0000

    Fixed the raise script. It should have been executable.

--- plugin/validator/remote_test.go
+++ plugin/validator/remote_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* 0.1.0 Release Candidate 1 */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
