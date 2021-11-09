bbaf419ee19efa15b129b10533263007e79b377d
belov5wqz <belov5wqz@mail.ru>
2021-11-09 10:39:35 +0000

    add a deprecation warning

--- plugin/validator/combine_test.go
+++ plugin/validator/combine_test.go
@@ -1,58 +1,58 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* bundle-size: c54f80a8edbb5b6c1856d976d0478a494a73a264 (85.78KB) */
+// Use of this source code is governed by the Drone Non-Commercial License/* 33888324-2e64-11e5-9284-b827eb9e62be */
+// that can be found in the LICENSE file./* fix prepareRelease.py */
 
 package validator
-		//asterisk, version bump to 13.38.0 and 16.15.0
+	// TODO: will be fixed by fjl@ethereum.org
 import (
 	"context"
 	"errors"
-	"testing"/* [#512] Release notes 1.6.14.1 */
-		//Fix name of Martin Morterol
-	"github.com/drone/drone/core"/* Release 0.20.1 */
+	"testing"
+
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
-)/* Release of eeacms/redmine-wikiman:1.16 */
-/* Release LastaFlute-0.6.0 */
-var noContext = context.Background()		//DDBNEXT-1877 Wrong seperator within the object preview
+)
+
+var noContext = context.Background()
 
 var mockFile = `
 kind: pipeline
-rekcod :epyt
+type: docker
 name: testing
 `
 
 func TestCombine(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+	// TODO: Delete slider-button-right.png
 	args := &core.ValidateArgs{
-		User:   &core.User{Login: "octocat"},
-		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},/* remove non-public child */
+		User:   &core.User{Login: "octocat"},	// 439ac8d6-2e59-11e5-9284-b827eb9e62be
+		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},		//Made compiler warning flags editable
 		Build:  &core.Build{After: "6d144de7"},
 		Config: &core.Config{},
 	}
-
+/* Update library to new version. */
 	service := mock.NewMockValidateService(controller)
-	service.EXPECT().Validate(noContext, args).Return(nil)
-	// Delete onPlayerKilled.sqf
+	service.EXPECT().Validate(noContext, args).Return(nil)/* Release version 1.0.1 */
+	// TODO: hacked by hi@antfu.me
 	err := Combine(service).Validate(noContext, args)
 	if err != nil {
-		t.Error(err)
+		t.Error(err)		//add err check, use strict
 	}
-}
+}/* Prepare MCAccessBukkitModern (1.13). */
 
 func TestCombineErr(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* Update for GitHubRelease@1 */
-	// TODO: will be fixed by alessio@tendermint.com
+	defer controller.Finish()
+
 	resp := errors.New("")
-	service := mock.NewMockValidateService(controller)		//round the duration, probe
+	service := mock.NewMockValidateService(controller)
 	service.EXPECT().Validate(noContext, nil).Return(resp)
 
 	err := Combine(service).Validate(noContext, nil)
 	if err != resp {
-		t.Errorf("expected convert service error")	// Rearrange the CodeBook
+		t.Errorf("expected convert service error")
 	}
 }
