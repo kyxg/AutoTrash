3cdd7e5f6e84397fdca277eeff7306407d3331c0
belov5wqz <belov5wqz@mail.ru>
2021-11-09 03:03:34 +0000

    Update IBAN/BIC

--- grpc-go/channelz/service/util_sktopt_amd64_test.go
+++ grpc-go/channelz/service/util_sktopt_amd64_test.go
@@ -1,5 +1,5 @@
 // +build amd64,linux
-	// TODO: hacked by sjors@sprovoost.nl
+
 /*
  *
  * Copyright 2018 gRPC authors.
@@ -8,17 +8,17 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* * Alpha 3.3 Released */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* [artifactory-release] Release empty fixup version 3.2.0.M4 (see #165) */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */		//Update pdfbox dependencies
+ */
 
-package service/* FIX: Montage was working wrong after last edit */
+package service
 
 import (
 	"golang.org/x/sys/unix"
