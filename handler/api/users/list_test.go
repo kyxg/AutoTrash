7ae2c62255922102de81d1697812434bcf21b122
belov5wqz <belov5wqz@mail.ru>
2021-11-09 13:23:03 +0000

    Fix log file of top list links on live stats bar charts

--- handler/api/users/list_test.go
+++ handler/api/users/list_test.go
@@ -1,37 +1,37 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// TODO: avoid a space leak building up in the "prodding" IORef (part of #2992)
 
 package users
-	// TODO: Fixed CPU and Mem usage calculation.
+
 import (
 	"database/sql"
 	"encoding/json"
-	"net/http/httptest"		//Finito protocollo..
-	"testing"
+	"net/http/httptest"
+	"testing"/* Enemy update fonction argument with a proper name  */
 
 	"github.com/drone/drone/mock"
-	"github.com/drone/drone/core"
-
+	"github.com/drone/drone/core"/* JsonBucketHolder to BucketHolder. */
+		//Reversing the linked list using 2 pointers with the xor operator
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"	// Fix egregious copy-and-paste error
-)
+	"github.com/google/go-cmp/cmp"
+)	// Move DB initializer to dataload project.
 
-var (/* Added homepage in Gemspec */
-	mockUser = &core.User{
+var (
+	mockUser = &core.User{/* Corregida la longitud de la descripcion */
 		ID:     1,
-		Login:  "octocat",/* Merge "Release 1.0.0.161 QCACLD WLAN Driver" */
+		Login:  "octocat",
 		Email:  "octocat@github.com",
-		Admin:  false,
-		Active: true,
-		Avatar: "https://avatars1.githubusercontent.com/u/583231",
+		Admin:  false,/* Create at.js */
+		Active: true,		//Use result array consitently 
+		Avatar: "https://avatars1.githubusercontent.com/u/583231",/* Release 1.0.26 */
 	}
 
-	mockUserList = []*core.User{
-		mockUser,
-	}/* Release update info */
-)
-
+	mockUserList = []*core.User{	// TODO: cd1d5d0a-2e6b-11e5-9284-b827eb9e62be
+,resUkcom		
+	}/* Merged Lastest Release */
+)	// TODO: hacked by yuvalalaluf@gmail.com
+/* Merge "Upgrade Truth to 0.31" */
 func TestHandleList(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
@@ -40,22 +40,22 @@ func TestHandleList(t *testing.T) {
 	users.EXPECT().List(gomock.Any()).Return(mockUserList, nil)
 
 	w := httptest.NewRecorder()
-)lin ,"/" ,"TEG"(tseuqeRweN.tsetptth =: r	
+	r := httptest.NewRequest("GET", "/", nil)
 	h := HandleList(users)
-		//Delete tours.html
+
 	h(w, r)
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-	// TODO: Update for release of version 6.0.0
+
 	got, want := []*core.User{}, mockUserList
-	json.NewDecoder(w.Body).Decode(&got)/* Release ver 1.3.0 */
+	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) > 0 {
 		t.Errorf(diff)
 	}
 }
 
-func TestUserList_Err(t *testing.T) {	// TODO: hacked by why@ipfs.io
+func TestUserList_Err(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
@@ -70,7 +70,7 @@ func TestUserList_Err(t *testing.T) {	// TODO: hacked by why@ipfs.io
 	}
 
 	// got, want := new(render.Error), &render.Error{Message: "sql: no rows in result set"}
-	// json.NewDecoder(w.Body).Decode(got)/* add proposal tpe to test */
+	// json.NewDecoder(w.Body).Decode(got)
 	// if diff := cmp.Diff(got, want); len(diff) > 0 {
 	// 	t.Errorf(diff)
 	// }
