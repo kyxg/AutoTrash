25e6f16c8b70f1506cc639ded9c3620ffef82b60
belov5wqz <belov5wqz@mail.ru>
2021-11-09 11:22:43 +0000

    removed bug from check cycles

--- core/trigger_test.go
+++ core/trigger_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//* Use getClass() for get Class loader
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: hacked by greg@colvin.org
+
 // +build !oss
 
 package core
