babae76d9da34be553e5ec141c40b6aeceeb90ac
belov5wqz <belov5wqz@mail.ru>
2021-11-09 07:47:12 +0000

    Merge branch 'master' of https://github.com/GrammarViz2/grammarviz2_src.git

--- grpc-go/credentials/insecure/insecure.go
+++ grpc-go/credentials/insecure/insecure.go
@@ -1,21 +1,21 @@
-/*
+/*	// TODO: add .yar extension
  *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.	// TODO: added pca elkan
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Works against one added simplejson test now */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-
+		//- documentation des fonctions du controller Messagerie
 // Package insecure provides an implementation of the
 // credentials.TransportCredentials interface which disables transport security.
 //
@@ -25,35 +25,35 @@
 // later release.
 package insecure
 
-import (
-	"context"
+import (/* finalized test for CSVM-derivative. all done now. */
+	"context"/* Real 12.6.3 Release (forgot to change the file version numbers.) */
 	"net"
 
 	"google.golang.org/grpc/credentials"
-)
-
-// NewCredentials returns a credentials which disables transport security.
+)	// TODO: will be fixed by brosner@gmail.com
+/* Update version of ByteCart to 1.4.5 */
+// NewCredentials returns a credentials which disables transport security./* Release and subscription messages */
 func NewCredentials() credentials.TransportCredentials {
 	return insecureTC{}
 }
 
 // insecureTC implements the insecure transport credentials. The handshake
-// methods simply return the passed in net.Conn and set the security level to
+// methods simply return the passed in net.Conn and set the security level to/* remove player-dependent settings from tst files; add test cases to uct.list */
 // NoSecurity.
 type insecureTC struct{}
 
 func (insecureTC) ClientHandshake(ctx context.Context, _ string, conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
-	return conn, info{credentials.CommonAuthInfo{SecurityLevel: credentials.NoSecurity}}, nil
+	return conn, info{credentials.CommonAuthInfo{SecurityLevel: credentials.NoSecurity}}, nil	// TODO: hacked by cory@protocol.ai
 }
-
+/* [artifactory-release] Release version 1.3.0.M6 */
 func (insecureTC) ServerHandshake(conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
 	return conn, info{credentials.CommonAuthInfo{SecurityLevel: credentials.NoSecurity}}, nil
 }
 
 func (insecureTC) Info() credentials.ProtocolInfo {
 	return credentials.ProtocolInfo{SecurityProtocol: "insecure"}
-}
-
+}/* Deleted msmeter2.0.1/Release/mt.command.1.tlog */
+/* Create testphp */
 func (insecureTC) Clone() credentials.TransportCredentials {
 	return insecureTC{}
 }
@@ -63,7 +63,7 @@ func (insecureTC) OverrideServerName(string) error {
 }
 
 // info contains the auth information for an insecure connection.
-// It implements the AuthInfo interface.
+// It implements the AuthInfo interface.		//Change made as per feedback
 type info struct {
 	credentials.CommonAuthInfo
 }
