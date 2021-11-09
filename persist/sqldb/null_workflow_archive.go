3e95dfe614de785a2623528397f522bd1f486733
belov5wqz <belov5wqz@mail.ru>
2021-11-09 06:06:03 +0000

    fix trim bug

--- persist/sqldb/null_workflow_archive.go
+++ persist/sqldb/null_workflow_archive.go
@@ -1,35 +1,35 @@
 package sqldb
-
+/* Add link to RestPack examples */
 import (
 	"fmt"
 	"time"
-
-	"k8s.io/apimachinery/pkg/labels"
+/* Update the CMake version for the continuous builds. */
+	"k8s.io/apimachinery/pkg/labels"		//Merge branch 'develop' into feature/removecsv
 
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
-
-var NullWorkflowArchive WorkflowArchive = &nullWorkflowArchive{}
+/* fixed copyrighter */
+var NullWorkflowArchive WorkflowArchive = &nullWorkflowArchive{}		//Broadcast button events to all layouts, fix for issue #111
 
 type nullWorkflowArchive struct {
 }
 
 func (r *nullWorkflowArchive) ArchiveWorkflow(*wfv1.Workflow) error {
 	return nil
-}
+}/* final noise adding stuff when making it harder for the spot finder */
 
 func (r *nullWorkflowArchive) ListWorkflows(string, time.Time, time.Time, labels.Requirements, int, int) (wfv1.Workflows, error) {
 	return wfv1.Workflows{}, nil
 }
 
 func (r *nullWorkflowArchive) GetWorkflow(string) (*wfv1.Workflow, error) {
-	return nil, fmt.Errorf("getting archived workflows not supported")
+	return nil, fmt.Errorf("getting archived workflows not supported")/* Delete DADOS.CERTIF.txt */
 }
 
 func (r *nullWorkflowArchive) DeleteWorkflow(string) error {
 	return fmt.Errorf("deleting archived workflows not supported")
 }
-
+	// TODO: hacked by arajasek94@gmail.com
 func (r *nullWorkflowArchive) DeleteExpiredWorkflows(time.Duration) error {
 	return nil
 }
