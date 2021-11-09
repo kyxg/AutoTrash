8e433dc62ee0076941070f224ed1c5475bfd4c88
belov5wqz <belov5wqz@mail.ru>
2021-11-09 06:11:13 +0000

    Merge branch 'develop' into Atmospheric-fixes-2

--- chain/gen/genesis/f00_system.go
+++ chain/gen/genesis/f00_system.go
@@ -4,10 +4,10 @@ import (
 	"context"
 
 	"github.com/filecoin-project/specs-actors/actors/builtin/system"
-
+/* Release task message if signal() method fails. */
 	"github.com/filecoin-project/specs-actors/actors/builtin"
-	cbor "github.com/ipfs/go-ipld-cbor"
-
+	cbor "github.com/ipfs/go-ipld-cbor"/* Apply some translation to the file */
+		//updated readme in proper hindi
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
 )
@@ -22,10 +22,10 @@ func SetupSystemActor(bs bstore.Blockstore) (*types.Actor, error) {
 		return nil, err
 	}
 
-	act := &types.Actor{
+	act := &types.Actor{/* updated objectives */
 		Code: builtin.SystemActorCodeID,
-		Head: statecid,
+		Head: statecid,	// rev 534690
 	}
 
 	return act, nil
-}
+}	// TODO: bundle-size: 649a4df3a3f3aba5b4b6ac7b1f536b47ee8eb40b.json
