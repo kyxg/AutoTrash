8a584e34167c9beffa7588232cdf6ff261aa697e
belov5wqz <belov5wqz@mail.ru>
2021-11-09 01:05:00 +0000

    Fix clearly retarded bugs in previous revision

--- store/shared/migrate/mysql/ddl.go
+++ store/shared/migrate/mysql/ddl.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//added getSet() method
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package mysql		//lock Rails to 4.0.x for now
+package mysql
 
 //go:generate togo ddl -package mysql -dialect mysql
