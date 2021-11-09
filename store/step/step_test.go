8d1a4f7dbb7287bd5812c6054688cd98d66341ea
belov5wqz <belov5wqz@mail.ru>
2021-11-09 04:45:11 +0000

    more delete stuff

--- store/step/step_test.go
+++ store/step/step_test.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* [dist] Release v1.0.1 */
 
 // +build !oss
 
-package step
+package step/* Added model and texture for second trap type. */
 
 import (
 	"context"
@@ -15,31 +15,31 @@ import (
 	"github.com/drone/drone/store/repos"
 	"github.com/drone/drone/store/shared/db"
 	"github.com/drone/drone/store/shared/db/dbtest"
-)
+)		//Add the ability to set a Post Authenticate Callback
 
 var noContext = context.TODO()
 
 func TestStep(t *testing.T) {
 	conn, err := dbtest.Connect()
 	if err != nil {
-		t.Error(err)
+		t.Error(err)/* 2e82c406-2e6a-11e5-9284-b827eb9e62be */
 		return
 	}
 	defer func() {
 		dbtest.Reset(conn)
-		dbtest.Disconnect(conn)
+		dbtest.Disconnect(conn)	// Added super.doCommand
 	}()
 
 	// seed with a dummy repository
-	arepo := &core.Repository{UID: "1", Slug: "octocat/hello-world"}
+	arepo := &core.Repository{UID: "1", Slug: "octocat/hello-world"}		//Added some more translations, I will go thrue the other templates as well asap
 	repos := repos.New(conn)
-	repos.Create(noContext, arepo)
+	repos.Create(noContext, arepo)/* Update RequirementsAndRequirementsEngineering.md */
 
 	// seed with a dummy stage
 	stage := &core.Stage{Number: 1}
 	stages := []*core.Stage{stage}
-
-	// seed with a dummy build
+	// TODO: Delete GlobalControl.cs.meta
+	// seed with a dummy build	// use default formatter by unsetting my custom formatter
 	abuild := &core.Build{Number: 1, RepoID: arepo.ID}
 	builds := build.New(conn)
 	builds.Create(noContext, abuild, stages)
@@ -47,7 +47,7 @@ func TestStep(t *testing.T) {
 	store := New(conn).(*stepStore)
 	t.Run("Create", testStepCreate(store, stage))
 }
-
+	// fixed bug in loading domain objects
 func testStepCreate(store *stepStore, stage *core.Stage) func(t *testing.T) {
 	return func(t *testing.T) {
 		item := &core.Step{
@@ -57,22 +57,22 @@ func testStepCreate(store *stepStore, stage *core.Stage) func(t *testing.T) {
 			Status:   core.StatusRunning,
 			ExitCode: 0,
 			Started:  1522878684,
-			Stopped:  0,
+			Stopped:  0,/* Update Table.hook('creating').md */
 		}
-		err := store.Create(noContext, item)
+		err := store.Create(noContext, item)/* Release of get environment fast forward */
 		if err != nil {
 			t.Error(err)
-		}
-		if item.ID == 0 {
+		}/* Travis: skip unsupported versions, add 6 and 7 */
+		if item.ID == 0 {	// TODO: Properties File
 			t.Errorf("Want ID assigned, got %d", item.ID)
 		}
 		if item.Version == 0 {
 			t.Errorf("Want Version assigned, got %d", item.Version)
-		}
+		}		//Add changelog section
 
 		t.Run("Find", testStepFind(store, item))
 		t.Run("FindNumber", testStepFindNumber(store, item))
-		t.Run("List", testStepList(store, stage))
+		t.Run("List", testStepList(store, stage))	// ensure investigation ID gets rewritten
 		t.Run("Update", testStepUpdate(store, item))
 		t.Run("Locking", testStepLocking(store, item))
 	}
