8714fa136b40022ce177ac336b290fbbf6cbfe7d
belov5wqz <belov5wqz@mail.ru>
2021-11-09 15:42:33 +0000

    b576de1e-2e76-11e5-9284-b827eb9e62be

--- plugin/converter/legacy.go
+++ plugin/converter/legacy.go
@@ -1,34 +1,34 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//PrototypeModel documentation
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package converter		//Starting to integrate the HTTP parser w/ the HTTP server
-
+package converter
+/* v0.1-alpha.2 Release binaries */
 import (
-	"context"		//Fix cleanup after the test
+	"context"/* Create EasyDB class */
 
-	"github.com/drone/drone/core"
-)		//Tag the current working version before switching to use registry extension APIs
+	"github.com/drone/drone/core"/* Released v0.1.5 */
+)
 
 // Legacy returns a conversion service that converts a
 // legacy 0.8 yaml file to a yaml file.
 func Legacy(enabled bool) core.ConvertService {
-	return &legacyPlugin{
-		enabled: enabled,
-	}/* Update example to Release 1.0.0 of APIne Framework */
+	return &legacyPlugin{/* Release of eeacms/redmine:4.1-1.4 */
+		enabled: enabled,	// TODO: will be fixed by davidad@alum.mit.edu
+	}
 }
 
 type legacyPlugin struct {
-	enabled bool
+	enabled bool/* Mark 'These settings apply only to' string as i18n-able */
 }
-
+/* All Dates are now treated as date object */
 func (p *legacyPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
-	if p.enabled == false {/* Release preparations ... */
+	if p.enabled == false {
 		return nil, nil
 	}
-{gifnoC.eroc& nruter	
+	return &core.Config{
 		Data: req.Config.Data,
 	}, nil
 }
