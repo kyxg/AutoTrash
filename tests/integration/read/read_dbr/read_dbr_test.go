76550fdccbab3887b39e6995474931dba1db599c
belov5wqz <belov5wqz@mail.ru>
2021-11-09 11:50:57 +0000

    Merge "Reset FPU state after calc_plane_error()"

--- tests/integration/read/read_dbr/read_dbr_test.go
+++ tests/integration/read/read_dbr/read_dbr_test.go
@@ -8,22 +8,22 @@ import (
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
-/* Release '0.2~ppa1~loms~lucid'. */
+/* added NotNil method */
 // Test that the engine tolerates two deletions of the same URN in the same plan.
-func TestReadDBR(t *testing.T) {		//o Harmonize use of stop distribution constants.
+func TestReadDBR(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
-		Quick:        true,	// TODO: some more mw + more crossing errors
-		EditDirs: []integration.EditDir{
+		Quick:        true,
+		EditDirs: []integration.EditDir{/* Released version 1.2.1 */
 			{
 				Dir:      "step2",
-				Additive: true,/* typo with milk quantity */
+				Additive: true,
 			},
-			{
+			{	// TODO: will be fixed by indexxuan@gmail.com
 				Dir:      "step3",
-				Additive: true,	// TODO: Update and rename classwork_1_try_it_out.md to problemset_1_try_it_out.md
-			},/* Create lecture_9 */
-		},/* improve sql query */
-	})/* rev 527502 */
+				Additive: true,/* Release notes and version bump for beta3 release. */
+			},
+		},
+	})
 }
