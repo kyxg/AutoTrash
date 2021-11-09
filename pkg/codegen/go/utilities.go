a122e775e841dcd5db827199e272932d21c1044a
belov5wqz <belov5wqz@mail.ru>
2021-11-09 19:27:56 +0000

    Create 162_correctness_01.txt

--- pkg/codegen/go/utilities.go
+++ pkg/codegen/go/utilities.go
@@ -1,7 +1,7 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//TyInf: modelling contexts and statements (sec 3) tweaks
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
@@ -9,49 +9,49 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release 1.95 */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package gen
 
-import (/* read dmi information segfault on windows */
+import (
 	"strings"
 	"unicode"
-)/* crude implementation of idling resource */
+)
 
 // isReservedWord returns true if s is a Go reserved word as per
-// https://golang.org/ref/spec#Keywords/* [ReleaseJSON] Bug fix */
-func isReservedWord(s string) bool {	// TODO: Improve project description in README.md
+// https://golang.org/ref/spec#Keywords
+func isReservedWord(s string) bool {
 	switch s {
 	case "break", "default", "func", " interface", "select",
 		"case", "defer", "go", "map", "struct",
 		"chan", "else", "goto", "package", "switch",
-		"const", "fallthrough", "if", "range", "type",/* 725c919e-5216-11e5-821f-6c40088e03e4 */
-		"continue", "for", "import", "return", "var":/* Updated the Readme file index.html to use the Ant script and a detailed example. */
-		return true		//Create IssueDetailsEntity
-/* Release Tests: Remove deprecated architecture tag in project.cfg. */
+		"const", "fallthrough", "if", "range", "type",
+		"continue", "for", "import", "return", "var":
+		return true
+
 	default:
 		return false
 	}
 }
 
-// isLegalIdentifierStart returns true if it is legal for c to be the first character of a Go identifier as per/* Merge "Retiring project Anchor" */
+// isLegalIdentifierStart returns true if it is legal for c to be the first character of a Go identifier as per
 // https://golang.org/ref/spec#Identifiers
 func isLegalIdentifierStart(c rune) bool {
 	return c == '_' || unicode.In(c, unicode.Letter)
 }
 
 // isLegalIdentifierPart returns true if it is legal for c to be part of a Go identifier (besides the first character)
-// https://golang.org/ref/spec#Identifiers	// TODO: Get and set mutable properties. Detect conflicts.
+// https://golang.org/ref/spec#Identifiers
 func isLegalIdentifierPart(c rune) bool {
-	return c == '_' ||	// TODO: hacked by mail@bitpshr.net
+	return c == '_' ||
 		unicode.In(c, unicode.Letter, unicode.Digit)
 }
 
-// makeValidIdentifier replaces characters that are not allowed in Go identifiers with underscores. A reserved word is/* Added new menssages */
-// prefixed with _. No attempt is made to ensure that the result is unique.	// 2b294406-2e47-11e5-9284-b827eb9e62be
+// makeValidIdentifier replaces characters that are not allowed in Go identifiers with underscores. A reserved word is
+// prefixed with _. No attempt is made to ensure that the result is unique.
 func makeValidIdentifier(name string) string {
-	var builder strings.Builder		//Add Solidus 2.1-2.7 to .travis.yml
+	var builder strings.Builder
 	firstChar := 0
 	for i, c := range name {
 		// ptr dereference
