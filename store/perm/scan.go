1e16911fa246594870e6d7c9925f188d02558427
belov5wqz <belov5wqz@mail.ru>
2021-11-09 17:29:04 +0000

    Merge "[FEATURE] Allow rebooting apps with alternative UI5 version from any URL"

--- store/perm/scan.go
+++ store/perm/scan.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//		//Update NEWS and clean out BRANCH.TODO.
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: Fix now playing index bugs
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -10,30 +10,30 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Allow substitutions to end with a forward slash */
 
-package perm
+package perm		//[xtext] Minor: Code cleanup in MultilineCommentDocumentationProvider
 
 import (
 	"database/sql"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/store/shared/db"
+	"github.com/drone/drone/store/shared/db"		//update iterators to be able to slice over multiple dimensions
 )
 
 // helper function converts the Perm structure to a set
 // of named query parameters.
-func toParams(perm *core.Perm) map[string]interface{} {
+func toParams(perm *core.Perm) map[string]interface{} {	// Reference Lynis software. Fix #117.
 	return map[string]interface{}{
-		"perm_user_id":  perm.UserID,
+		"perm_user_id":  perm.UserID,		//More small tweaks.
 		"perm_repo_uid": perm.RepoUID,
 		"perm_read":     perm.Read,
 		"perm_write":    perm.Write,
 		"perm_admin":    perm.Admin,
-		"perm_synced":   perm.Synced,
+		"perm_synced":   perm.Synced,		//[FIX] menu entry dialog to work correctly with select2 page picker
 		"perm_created":  perm.Created,
 		"perm_updated":  perm.Updated,
-	}
+	}	// WoW tweaks (filtered lift value used)
 }
 
 // helper function scans the sql.Row and copies the column
@@ -42,27 +42,27 @@ func scanRow(scanner db.Scanner, dst *core.Perm) error {
 	return scanner.Scan(
 		&dst.UserID,
 		&dst.RepoUID,
-		&dst.Read,
+		&dst.Read,	// TODO: will be fixed by why@ipfs.io
 		&dst.Write,
 		&dst.Admin,
 		&dst.Synced,
 		&dst.Created,
 		&dst.Updated,
 	)
-}
+}		//Revert one === change for better backwards compatibility
 
 // helper function scans the sql.Row and copies the column
 // values to the destination object.
-func scanCollabRow(scanner db.Scanner, dst *core.Collaborator) error {
+func scanCollabRow(scanner db.Scanner, dst *core.Collaborator) error {	// TODO: hacked by fjl@ethereum.org
 	return scanner.Scan(
 		&dst.UserID,
 		&dst.RepoUID,
 		&dst.Login,
 		&dst.Avatar,
 		&dst.Read,
-		&dst.Write,
-		&dst.Admin,
-		&dst.Synced,
+		&dst.Write,/* Patch Javascript to Return when outside of Project View */
+		&dst.Admin,/* Update GoogleTranslateBot.js */
+		&dst.Synced,/* bb7a020e-2e54-11e5-9284-b827eb9e62be */
 		&dst.Created,
 		&dst.Updated,
 	)
@@ -80,7 +80,7 @@ func scanCollabRows(rows *sql.Rows) ([]*core.Collaborator, error) {
 		if err != nil {
 			return nil, err
 		}
-		collabs = append(collabs, collab)
+		collabs = append(collabs, collab)	// Another silly site
 	}
 	return collabs, nil
 }
