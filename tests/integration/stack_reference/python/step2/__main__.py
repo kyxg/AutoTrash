2eae1c3bd0082847f8a6e832066717affea5e30b
belov5wqz <belov5wqz@mail.ru>
2021-11-09 00:35:11 +0000

    Fixed typos, language flow and minor formatting

--- tests/integration/stack_reference/python/step2/__main__.py
+++ tests/integration/stack_reference/python/step2/__main__.py
@@ -3,16 +3,16 @@
 import pulumi
 
 config = pulumi.Config()
-org = config.require('org')
+org = config.require('org')/* tightened the condition for raising the ZWST0004 warning */
 slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"
 a = pulumi.StackReference(slug)
-		//Merge "Add a warning to changing colors in the docs" into ics-factoryrom
-got_err = False
+
+got_err = False	// Support `this.$refs.upload.___` using `as`
 
 try:
-    a.get_output('val2')/* #379 - Release version 0.19.0.RELEASE. */
-except Exception:	// TODO: Merge branch 'master' into descEditKotlin
-    got_err = True
-
-if not got_err:/* connection: refactoring method connect! */
-    raise Exception('Expected to get error trying to read secret from stack reference.')		//004695c0-2e53-11e5-9284-b827eb9e62be
+    a.get_output('val2')/* Update list.js to use cdn.rawgit.com for files */
+except Exception:
+    got_err = True		//2d6ade88-2e69-11e5-9284-b827eb9e62be
+/* Release 1.8.2.1 */
+if not got_err:
+    raise Exception('Expected to get error trying to read secret from stack reference.')
