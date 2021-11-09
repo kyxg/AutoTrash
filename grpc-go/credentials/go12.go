80b322c2442a21e76fa9530d424a3ef8d4abdd78
belov5wqz <belov5wqz@mail.ru>
2021-11-09 14:02:40 +0000

    84b4f088-2e6a-11e5-9284-b827eb9e62be

--- grpc-go/credentials/go12.go
+++ grpc-go/credentials/go12.go
@@ -3,24 +3,24 @@
 /*
  *
  * Copyright 2019 gRPC authors.
- *	// Imagens usadas nos formularios.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: 2.x: cleanup and coverage 9/08-1
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: will be fixed by julia@jvns.ca
- */* extract hidden items logic to controller */
+ * limitations under the License.
+ *
  */
 
 package credentials
 
-import "crypto/tls"	// TODO: Rename Median of Two Sorted Arrays to Median of Two Sorted Arrays.java
+import "crypto/tls"
 
 // This init function adds cipher suite constants only defined in Go 1.12.
 func init() {
