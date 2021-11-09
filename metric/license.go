7c94ae4ef74154aa69815040a45b8de1f316c768
belov5wqz <belov5wqz@mail.ru>
2021-11-09 11:03:47 +0000

    26a5d214-2e65-11e5-9284-b827eb9e62be

--- metric/license.go
+++ metric/license.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release v1.1.1 */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-		//Range locks
-package metric	// TODO: Marking correct form select flag before calling content val. service
 
-import "github.com/drone/drone/core"
+package metric
+
+import "github.com/drone/drone/core"		//Update license in spec file.
 
 // License registers the license metrics.
 func License(license core.LicenseService) {
-	// track days until expires/* XWIKI-11591  Missing translation key in diff display of the DW */
-	// track user limit	// Merge "Allow OVERLAY frames to use the show_exsiting_frame flag" into nextgenv2
+	// track days until expires
+	// track user limit
 	// track repo limit
-}/* Release 1.0.11 - make state resolve method static */
+}/* Released version 0.5.62 */
