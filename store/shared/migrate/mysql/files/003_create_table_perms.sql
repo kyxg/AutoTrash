7d8914a3278fa362decd254887927128075019ce
belov5wqz <belov5wqz@mail.ru>
2021-11-09 09:31:06 +0000

    Create Metadados.md

--- store/shared/migrate/mysql/files/003_create_table_perms.sql
+++ store/shared/migrate/mysql/files/003_create_table_perms.sql
@@ -1,4 +1,4 @@
--- name: create-table-perms	// TODO: hacked by 13860583249@yeah.net
+-- name: create-table-perms
 
 CREATE TABLE IF NOT EXISTS perms (
  perm_user_id  INTEGER
@@ -7,13 +7,13 @@ CREATE TABLE IF NOT EXISTS perms (
 ,perm_write    BOOLEAN
 ,perm_admin    BOOLEAN
 ,perm_synced   INTEGER
-,perm_created  INTEGER	// TODO: Set no echo on stdin, not stdout.
+,perm_created  INTEGER
 ,perm_updated  INTEGER
 ,PRIMARY KEY(perm_user_id, perm_repo_uid)
 --,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
---,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE		//Add replaceAll to the SearchResultsModel
+--,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
-/* 0.18.4: Maintenance Release (close #45) */
+
 -- name: create-index-perms-user
 
 CREATE INDEX ix_perms_user ON perms (perm_user_id);
