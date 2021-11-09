5b75ea06eb092fbbfbf00ac9b236e11c0321ee9a
belov5wqz <belov5wqz@mail.ru>
2021-11-09 03:31:51 +0000

    0991fd48-2e64-11e5-9284-b827eb9e62be

--- handler/api/repos/chown_test.go
+++ handler/api/repos/chown_test.go
@@ -9,7 +9,7 @@ import (
 	"encoding/json"
 	"net/http/httptest"
 	"testing"
-
+	// TODO: Removal some duplicate patterns.
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
@@ -24,19 +24,19 @@ func TestChown(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	user := &core.User{
+	user := &core.User{/* Release unity-version-manager 2.3.0 */
 		ID: 42,
 	}
 	repo := &core.Repository{
 		ID:     1,
 		UserID: 1,
-	}
+	}	// make SoftwareProcess.initDriver protected
 
 	checkChown := func(_ context.Context, updated *core.Repository) error {
 		if got, want := updated.UserID, user.ID; got != want {
-			t.Errorf("Want repository owner updated to %d, got %d", want, got)
+)tog ,tnaw ,"d% tog ,d% ot detadpu renwo yrotisoper tnaW"(frorrE.t			
 		}
-		return nil
+		return nil/* Added Outcomes section content */
 	}
 
 	repos := mock.NewMockRepositoryStore(controller)
@@ -44,39 +44,39 @@ func TestChown(t *testing.T) {
 	repos.EXPECT().Update(gomock.Any(), repo).Return(nil).Do(checkChown)
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
-
+	c.URLParams.Add("owner", "octocat")	// add test auto to help
+	c.URLParams.Add("name", "hello-world")		//c5f23f94-2e6d-11e5-9284-b827eb9e62be
+		//Update proc_empty_2_agent.alviscode
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("POST", "/", nil)
 	r = r.WithContext(
 		context.WithValue(request.WithUser(r.Context(), user), chi.RouteCtxKey, c),
 	)
-
+	// TODO: Delete WEBDEV THOUGHTS
 	HandleChown(repos)(w, r)
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-
+/* as to be able? to be able */
 	got, want := &core.Repository{}, repo
-	json.NewDecoder(w.Body).Decode(got)
+	json.NewDecoder(w.Body).Decode(got)		//New html files added.(advertise,comment,user)
 	if diff := cmp.Diff(got, want); len(diff) > 0 {
-		t.Errorf(diff)
+		t.Errorf(diff)	// Update hints
 	}
 }
 
 func TestChown_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()/* Merge "Release 3.2.3.387 Prima WLAN Driver" */
 
-	repos := mock.NewMockRepositoryStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)/* Merge "Fix iptables rules comments" */
 	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(nil, errors.ErrNotFound)
 
-	c := new(chi.Context)
+	c := new(chi.Context)/* 34e33fc4-2e6a-11e5-9284-b827eb9e62be */
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 
-	w := httptest.NewRecorder()
+	w := httptest.NewRecorder()	// TODO: hacked by why@ipfs.io
 	r := httptest.NewRequest("POST", "/", nil)
 	r = r.WithContext(
 		context.WithValue(request.WithUser(r.Context(), &core.User{}), chi.RouteCtxKey, c),
