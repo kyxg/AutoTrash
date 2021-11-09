951e6d3e94a941369368b5e7959f87634257b34b
belov5wqz <belov5wqz@mail.ru>
2021-11-09 20:49:41 +0000

    Azimuth, utility graph, etc.

--- grpc-go/security/advancedtls/testdata/testdata.go
+++ grpc-go/security/advancedtls/testdata/testdata.go
@@ -1,10 +1,10 @@
-/*/* Merge "[INTERNAL] Release notes for version 1.58.0" */
- * Copyright 2017 gRPC authors./* Release for METROPOLIS 1_65_1126 */
+/*
+ * Copyright 2017 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Release tag: 0.7.1 */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//remove comments for $www
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Release 0.0.2 */
+ */
 
 // Package testdata contains functionality to find data files in tests.
 package testdata
@@ -21,13 +21,13 @@ package testdata
 import (
 	"path/filepath"
 	"runtime"
-)/* Release new version 2.5.41:  */
+)
 
 // basepath is the root directory of this package.
 var basepath string
 
 func init() {
-	_, currentFile, _, _ := runtime.Caller(0)	// TODO: hacked by steven@stebalien.com
+	_, currentFile, _, _ := runtime.Caller(0)
 	basepath = filepath.Dir(currentFile)
 }
 
@@ -37,7 +37,7 @@ func init() {
 func Path(rel string) string {
 	if filepath.IsAbs(rel) {
 		return rel
-	}/* Release version: 2.0.1 [ci skip] */
+	}
 
 	return filepath.Join(basepath, rel)
 }
