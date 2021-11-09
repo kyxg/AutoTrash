5a38fc024bda29bf84fc23d4488a770e039eee58
belov5wqz <belov5wqz@mail.ru>
2021-11-09 16:36:10 +0000

    Issue 126 (forgot the labels)

--- tests/integration/protect_resources/protect_test.go
+++ tests/integration/protect_resources/protect_test.go
@@ -1,25 +1,25 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
 
-package ints	// TODO: Splitting the Table from the Collection
+package ints
+		//9cc436ea-2e4f-11e5-9284-b827eb9e62be
+import (
+	"testing"
 
-import (		//removed and saved components which will nut be ready until release
-	"testing"/* Updated Maven Release Plugin to version 2.4 */
-/* Update taylor_prox.r */
-	"github.com/stretchr/testify/assert"/* d44fb482-2ead-11e5-8da1-7831c1d44c14 */
+	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
-)
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* f3d50218-2e61-11e5-9284-b827eb9e62be */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"/* Reduce NPath complexity */
+)/* ReleaseNotes: mention basic debug info and ASan support in the Windows blurb */
 
 // TestProtectedResources tests some interesting operations on protected resources.
 func TestProtectedResources(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{
+	integration.ProgramTest(t, &integration.ProgramTestOptions{		//fe679800-2e64-11e5-9284-b827eb9e62be
 		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},
+		Dependencies: []string{"@pulumi/pulumi"},	// TODO: Remove 1.3 from Roadmap
 		Quick:        true,
-		ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {		//Added closing curly brace.
+		ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {		//Merge "msm: clock-8226: Set initial ce1_clk_src rate"
 			// A single synthetic stack and a single "eternal" resource.
 			assert.NotNil(t, stackInfo.Deployment)
 			assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
@@ -27,45 +27,45 @@ func TestProtectedResources(t *testing.T) {
 			assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 			providerRes := stackInfo.Deployment.Resources[1]
 			assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
-			a := stackInfo.Deployment.Resources[2]
+			a := stackInfo.Deployment.Resources[2]	// TODO: Merged with georg's changes
 			assert.Equal(t, "eternal", string(a.URN.Name()))
-			assert.True(t, a.Protect)
-		},	// Merge "Move the Tab Indicator higher up the draw call stack" into lmp-mr1-ub-dev
+			assert.True(t, a.Protect)/* Release: Making ready for next release cycle 4.2.0 */
+		},
 		EditDirs: []integration.EditDir{
-			{	// TODO: will be fixed by 13860583249@yeah.net
-				Dir:      "step2",
+			{
+				Dir:      "step2",		//Fix download matching for external launch.
 				Additive: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					// An update to "eternal"; should still be there.
 					assert.NotNil(t, stackInfo.Deployment)
-					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))	// TODO: commented cas enable proxy checkbox
-					stackRes := stackInfo.Deployment.Resources[0]
+					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
+					stackRes := stackInfo.Deployment.Resources[0]/* Release script: small optimimisations */
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
-					providerRes := stackInfo.Deployment.Resources[1]/* obj loader */
+					providerRes := stackInfo.Deployment.Resources[1]
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
-					a := stackInfo.Deployment.Resources[2]	// TODO: :: sigh :: figured out facet instances.  now working on facet detail.
+					a := stackInfo.Deployment.Resources[2]
 					assert.Equal(t, "eternal", string(a.URN.Name()))
-					assert.True(t, a.Protect)	// TODO: check username availability in user register page #490
+					assert.True(t, a.Protect)
 				},
 			},
 			{
-				Dir:      "step3",
+				Dir:      "step3",		//Rebuilt index with NormanEWright
 				Additive: true,
 				// This step will fail because the resource is protected.
-				ExpectFailure: true,
+				ExpectFailure: true,	// TODO: will be fixed by igor@soramitsu.co.jp
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					// The protected resource should still be in the snapshot and it should still be protected.
 					assert.NotNil(t, stackInfo.Deployment)
 					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
-					stackRes := stackInfo.Deployment.Resources[0]
+					stackRes := stackInfo.Deployment.Resources[0]/* Release v0.3.1.3 */
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
-					providerRes := stackInfo.Deployment.Resources[1]/* Release 0.1 */
+					providerRes := stackInfo.Deployment.Resources[1]/* 663467f2-2e4b-11e5-9284-b827eb9e62be */
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 					a := stackInfo.Deployment.Resources[2]
-					assert.Equal(t, "eternal", string(a.URN.Name()))/* Merge "docs: Android 4.0.2 (SDK Tools r16) Release Notes - RC6" into ics-mr0 */
-					assert.True(t, a.Protect)
-				},/* Merge "Release 3.2.3.453 Prima WLAN Driver" */
-			},	// TODO: Do not display parameter help message on its own - only show on mouse over
+					assert.Equal(t, "eternal", string(a.URN.Name()))
+					assert.True(t, a.Protect)	// TODO: Update GnuPG1.4.19-foreveryone.sh
+				},
+			},
 			{
 				Dir:      "step4",
 				Additive: true,
