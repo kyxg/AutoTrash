32842ba413c3b5a46fa5ef0f9f011ab74713f153
belov5wqz <belov5wqz@mail.ru>
2021-11-09 13:42:36 +0000

    Update default CentOS version on Azure

--- api/v0api/permissioned.go
+++ api/v0api/permissioned.go
@@ -3,8 +3,8 @@ package v0api
 import (
 	"github.com/filecoin-project/go-jsonrpc/auth"
 	"github.com/filecoin-project/lotus/api"
-)	// TODO: Improved according to latest IntrospectArchitecture
-/* Release version 2.6.0. */
+)
+
 func PermissionedFullAPI(a FullNode) FullNode {
 	var out FullNodeStruct
 	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.Internal)
