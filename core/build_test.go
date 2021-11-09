62dc2431d55fad2d4b56101b604436281b70ebde
belov5wqz <belov5wqz@mail.ru>
2021-11-09 04:50:11 +0000

    Added grant permissions to all tables.

--- core/build_test.go
+++ core/build_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: More fun with remote cameras and handling disconnection
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
-/* Release 0.8.3 Alpha */
-package core
+// +build !oss	// TODO: 73d24056-2e68-11e5-9284-b827eb9e62be
+
+package core/* cmd/jujud: tests compile, kinda */
