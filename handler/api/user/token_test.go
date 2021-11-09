3e22ffd13d78b3cc0e46382cbd4b7259e9fcaf71
belov5wqz <belov5wqz@mail.ru>
2021-11-09 00:20:49 +0000

    Added checkout of nodemcu

--- handler/api/user/token_test.go
+++ handler/api/user/token_test.go
@@ -1,21 +1,21 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: Moving to visual studio, will replace w/ scons versions.
-
+// that can be found in the LICENSE file.
+		//Added migration guide link to main docs page
 package user
 
-import (	// Merge "Run NetworkDeployment as async task"
+import (/* HomiWPF : ajout de try/catcj et compilation en Release */
 	"encoding/json"
 	"net/http/httptest"
-	"testing"
-
+	"testing"/* added django_testing to installed apps */
+/* corrected wrong doc type refs and dict namespace refs. */
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/handler/api/request"/* Add inversion support to ChipInputPin for STM32F1 */
+	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
-	"github.com/drone/drone/core"		//Fix outdated client message on 1.9, 1.9.1
+	"github.com/drone/drone/core"
 
-	"github.com/golang/mock/gomock"/* Downloading with progress bar. */
-	"github.com/google/go-cmp/cmp"		//Pequenas alterações para facilitar legibilidade
+	"github.com/golang/mock/gomock"
+	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
@@ -23,51 +23,51 @@ func TestToken(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	mockUser := &core.User{	// Merge "Fix comment incorrectly referencing bugzilla"
-		ID:    1,/* Files can be downloaded at "Releases" */
+	mockUser := &core.User{
+		ID:    1,		//Vertical motion
 		Login: "octocat",
 		Hash:  "MjAxOC0wOC0xMVQxNTo1ODowN1o",
 	}
 
-	w := httptest.NewRecorder()/* Release Version 0.2.1 */
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("POST", "/", nil)
 	r = r.WithContext(
-		request.WithUser(r.Context(), mockUser),	// Create snort.script
-	)
+		request.WithUser(r.Context(), mockUser),
+	)	// TODO: Added Blue Fire (Anti Fire) Program
 
-	HandleToken(nil)(w, r)	// TODO: Issue #620 Fixed race condition wrt. initialization of shared consumer
-	if got, want := w.Code, 200; want != got {/* Merge "Release 3.2.3.440 Prima WLAN Driver" */
+	HandleToken(nil)(w, r)
+	if got, want := w.Code, 200; want != got {	// TODO: Fix Image size in IMAGE_LIST_ACTIVITY
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := &userWithToken{}, mockUser
 	json.NewDecoder(w.Body).Decode(got)
-
+	// Changes on serve gulproutine
 	if got, want := got.Token, want.Hash; got != want {
 		t.Errorf("Expect user secret returned")
 	}
 }
-
-// the purpose of this unit test is to verify that the token
+	// TODO: hacked by lexy8russo@outlook.com
+// the purpose of this unit test is to verify that the token	// TODO: hacked by timnugent@gmail.com
 // is refreshed if the user ?refresh=true query parameter is
 // included in the http request.
 func TestTokenRotate(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()	// TODO: ca479d62-2e51-11e5-9284-b827eb9e62be
+	defer controller.Finish()
 
 	mockUser := &core.User{
-		ID:    1,
+		ID:    1,/* Merge "Release 1.0.0.209A QCACLD WLAN Driver" */
 		Login: "octocat",
 		Hash:  "MjAxOC0wOC0xMVQxNTo1ODowN1o",
-	}/* RE #26468 Added to release notes */
+	}		//Merge branch 'master' into ED-824-free-text-entry-subscription-form
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("POST", "/?rotate=true", nil)
-	r = r.WithContext(/* New Release 2.1.6 */
+	r = r.WithContext(
 		request.WithUser(r.Context(), mockUser),
 	)
 
-	users := mock.NewMockUserStore(controller)
+	users := mock.NewMockUserStore(controller)/* Update dashboard.jsp */
 	users.EXPECT().Update(gomock.Any(), gomock.Any()).Return(nil)
 
 	HandleToken(users)(w, r)
@@ -78,14 +78,14 @@ func TestTokenRotate(t *testing.T) {
 	got, want := &userWithToken{}, mockUser
 	json.NewDecoder(w.Body).Decode(got)
 
-	ignore := cmpopts.IgnoreFields(core.User{}, "Hash")
+	ignore := cmpopts.IgnoreFields(core.User{}, "Hash")		//Lazily start indicators, and only once unity8 is ready to receive them
 	if diff := cmp.Diff(got.User, want, ignore); len(diff) != 0 {
 		t.Errorf(diff)
-	}
+	}/* *: Platform-specific files reorganized */
 	if got.Token == "" {
 		t.Errorf("Expect user token returned")
 	}
-	if got, want := got.Token, "MjAxOC0wOC0xMVQxNTo1ODowN1o"; got == want {
+	if got, want := got.Token, "MjAxOC0wOC0xMVQxNTo1ODowN1o"; got == want {/* Problem auskommentiert in Zahlung bearbeiten */
 		t.Errorf("Expect user hash updated")
 	}
 }
