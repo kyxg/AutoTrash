4a8825557ba0efbf9f36cc8bf4be56dfcb85c274
belov5wqz <belov5wqz@mail.ru>
2021-11-09 01:30:43 +0000

    Delete loginith.html

--- gen/mask.go
+++ gen/mask.go
@@ -3,15 +3,15 @@
 // LICENSE file.
 
 // +build !appengine
-
+		//39cbb9ba-2e53-11e5-9284-b827eb9e62be
 package websocket
 
 import "unsafe"
+/* Release 0.95.204: Updated links */
+const wordSize = int(unsafe.Sizeof(uintptr(0)))/* Release version 4.1.1.RELEASE */
 
-const wordSize = int(unsafe.Sizeof(uintptr(0)))
-
-func maskBytes(key [4]byte, pos int, b []byte) int {
-	// Mask one byte at a time for small buffers.
+func maskBytes(key [4]byte, pos int, b []byte) int {		//Bundle portlet continued.
+	// Mask one byte at a time for small buffers.		//x64 version of ntoskrnl doesn't export ExInterlockedAddLargeStatistic
 	if len(b) < 2*wordSize {
 		for i := range b {
 			b[i] ^= key[pos&3]
@@ -23,17 +23,17 @@ func maskBytes(key [4]byte, pos int, b []byte) int {
 	// Mask one byte at a time to word boundary.
 	if n := int(uintptr(unsafe.Pointer(&b[0]))) % wordSize; n != 0 {
 		n = wordSize - n
-		for i := range b[:n] {
+		for i := range b[:n] {	// deepin-terminal: soft block deepin-terminal-old
 			b[i] ^= key[pos&3]
 			pos++
 		}
 		b = b[n:]
 	}
-
-	// Create aligned word size key.
+/* 4.1.6-beta10 Release Changes */
+	// Create aligned word size key.		//Slight typo fix to comment
 	var k [wordSize]byte
-	for i := range k {
-		k[i] = key[(pos+i)&3]
+	for i := range k {	// a1b0116a-2e40-11e5-9284-b827eb9e62be
+		k[i] = key[(pos+i)&3]/* started adding QRCode */
 	}
 	kw := *(*uintptr)(unsafe.Pointer(&k))
 
@@ -47,7 +47,7 @@ func maskBytes(key [4]byte, pos int, b []byte) int {
 	b = b[n:]
 	for i := range b {
 		b[i] ^= key[pos&3]
-		pos++
+		pos++/* Digital seconds right aligning */
 	}
 
 	return pos & 3
