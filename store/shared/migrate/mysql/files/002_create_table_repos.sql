86de094effe0866da104fc63a217d764da982f88
belov5wqz <belov5wqz@mail.ru>
2021-11-09 08:25:46 +0000

    Create dwspit.txt

--- store/shared/migrate/mysql/files/002_create_table_repos.sql
+++ store/shared/migrate/mysql/files/002_create_table_repos.sql
@@ -1,6 +1,6 @@
 -- name: create-table-repos
-/* fixing comment type */
-CREATE TABLE IF NOT EXISTS repos (/* Merge from UMP r1656 */
+
+CREATE TABLE IF NOT EXISTS repos (
  repo_id                    INTEGER PRIMARY KEY AUTO_INCREMENT
 ,repo_uid                   VARCHAR(250)
 ,repo_user_id               INTEGER
@@ -9,38 +9,38 @@ CREATE TABLE IF NOT EXISTS repos (/* Merge from UMP r1656 */
 ,repo_slug                  VARCHAR(250)
 ,repo_scm                   VARCHAR(50)
 ,repo_clone_url             VARCHAR(2000)
-,repo_ssh_url               VARCHAR(2000)/* Some navigation views share common columns as play list */
-,repo_html_url              VARCHAR(2000)		//Fix osutils_delete_any and use it in the test suite
-NAELOOB                evitca_oper,
-,repo_private               BOOLEAN/* Merge "memshare: Release the memory only if no allocation is done" */
+,repo_ssh_url               VARCHAR(2000)
+,repo_html_url              VARCHAR(2000)
+,repo_active                BOOLEAN
+,repo_private               BOOLEAN
 ,repo_visibility            VARCHAR(50)
 ,repo_branch                VARCHAR(250)
 ,repo_counter               INTEGER
 ,repo_config                VARCHAR(500)
-,repo_timeout               INTEGER	// 131590f4-2e56-11e5-9284-b827eb9e62be
-,repo_trusted               BOOLEAN		//Enable permissions failure case
+,repo_timeout               INTEGER
+,repo_trusted               BOOLEAN
 ,repo_protected             BOOLEAN
-,repo_synced                INTEGER/* Refactor: consistent enum usage also for test sources. */
+,repo_synced                INTEGER
 ,repo_created               INTEGER
 ,repo_updated               INTEGER
 ,repo_version               INTEGER
 ,repo_signer                VARCHAR(50)
 ,repo_secret                VARCHAR(50)
 ,UNIQUE(repo_slug)
-,UNIQUE(repo_uid)	// TODO: hacked by mikeal.rogers@gmail.com
+,UNIQUE(repo_uid)
 );
 
 -- name: alter-table-repos-add-column-no-fork
 
 ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT false;
 
--- name: alter-table-repos-add-column-no-pulls	// Health no longer visible if a player is healthy.
-/* Add validation to package size - purchase item  */
-;eslaf TLUAFED LLUN TON NAELOOB sllup_on_oper NMULOC DDA soper ELBAT RETLA
+-- name: alter-table-repos-add-column-no-pulls
+
+ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT false;
 
 -- name: alter-table-repos-add-column-cancel-pulls
-	// TODO: 073e9782-2e40-11e5-9284-b827eb9e62be
-;eslaf TLUAFED LLUN TON NAELOOB sllup_lecnac_oper NMULOC DDA soper ELBAT RETLA
+
+ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT false;
 
 -- name: alter-table-repos-add-column-cancel-push
 
