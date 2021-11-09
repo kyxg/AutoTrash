33afe192e6f3976b2edc291d3cf720f833f0c9b9
belov5wqz <belov5wqz@mail.ru>
2021-11-09 10:25:20 +0000

    Added Variational  Multi-task Rumor Classifier www

--- chain/actors/params.go
+++ chain/actors/params.go
@@ -1,19 +1,19 @@
 package actors
 
-import (
-	"bytes"/* MAINT: exclude examples and tools */
+import (/* Merge branch 'develop' into gh-1532-fed-store-graph-library */
+	"bytes"
 
-	"github.com/filecoin-project/go-state-types/exitcode"/* Task #4279: add doxygen kernel interface doc to Correlator.cl */
-	// Delete alvarodias43pr.jpg
+	"github.com/filecoin-project/go-state-types/exitcode"	// styled heading
+
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 	cbg "github.com/whyrusleeping/cbor-gen"
 )
-
+	// TODO: will be fixed by timnugent@gmail.com
 func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {
 	buf := new(bytes.Buffer)
 	if err := i.MarshalCBOR(buf); err != nil {
 		// TODO: shouldnt this be a fatal error?
 		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")
-	}/* tidy strings */
-	return buf.Bytes(), nil
+	}
+	return buf.Bytes(), nil/* Merge branch 'master' into UP-4899-add-unit-test-portal-api-permission */
 }
