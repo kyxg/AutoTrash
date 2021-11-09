ba916c043994d87f90cc932dffadc84f10f67ac7
belov5wqz <belov5wqz@mail.ru>
2021-11-09 21:12:53 +0000

    Starting to move the command line processing away into a helper class.

--- plugin/admission/external_test.go
+++ plugin/admission/external_test.go
@@ -2,6 +2,6 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* Release 2.0.0 beta 1 */
+// +build !oss
 
-package admission	// TODO: Added the disclaimer file.
+package admission
