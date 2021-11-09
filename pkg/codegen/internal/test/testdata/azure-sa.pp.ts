27b252a23f3f6abfadb802a652a8171ab3b3226d
belov5wqz <belov5wqz@mail.ru>
2021-11-09 09:45:43 +0000

    move header to header

--- pkg/codegen/internal/test/testdata/azure-sa.pp.ts
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.ts
@@ -1,21 +1,21 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as azure from "@pulumi/azure";
-	// TODO: will be fixed by earlephilhower@yahoo.com
+
 const config = new pulumi.Config();
 const storageAccountNameParam = config.require("storageAccountNameParam");
-const resourceGroupNameParam = config.require("resourceGroupNameParam");/* Release Scelight 6.2.28 */
-const resourceGroupVar = azure.core.getResourceGroup({	// TODO: hacked by hi@antfu.me
+const resourceGroupNameParam = config.require("resourceGroupNameParam");
+const resourceGroupVar = azure.core.getResourceGroup({	// Update BannerManagerPage.php
     name: resourceGroupNameParam,
 });
-const locationParam = config.get("locationParam") || resourceGroupVar.then(resourceGroupVar => resourceGroupVar.location);	// TODO: hacked by alan.shaw@protocol.ai
-const storageAccountTierParam = config.get("storageAccountTierParam") || "Standard";/* [snomed] Release generated IDs manually in PersistChangesRemoteJob */
+const locationParam = config.get("locationParam") || resourceGroupVar.then(resourceGroupVar => resourceGroupVar.location);
+const storageAccountTierParam = config.get("storageAccountTierParam") || "Standard";
 const storageAccountTypeReplicationParam = config.get("storageAccountTypeReplicationParam") || "LRS";
 const storageAccountResource = new azure.storage.Account("storageAccountResource", {
     name: storageAccountNameParam,
-    accountKind: "StorageV2",
+    accountKind: "StorageV2",/* Release dhcpcd-6.3.2 */
     location: locationParam,
     resourceGroupName: resourceGroupNameParam,
     accountTier: storageAccountTierParam,
     accountReplicationType: storageAccountTypeReplicationParam,
-});		//Fix/clarify spelling
-export const storageAccountNameOut = storageAccountResource.name;/* Adding jpeg-9b */
+});
+export const storageAccountNameOut = storageAccountResource.name;
