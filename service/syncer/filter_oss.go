3f362774b611ac6e91c60ce8c7b4b251bd15bf81
belov5wqz <belov5wqz@mail.ru>
2021-11-09 06:35:21 +0000

    Fix typo previous commit

--- service/syncer/filter_oss.go
+++ service/syncer/filter_oss.go
@@ -1,33 +1,33 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by why@ipfs.io
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Release of eeacms/energy-union-frontend:1.7-beta.10 */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* [TOOLS-94] Releases should be from the filtered projects */
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: Update unresponsive-header.jsx
 
-// +build oss/* Merge "[Release] Webkit2-efl-123997_0.11.38" into tizen_2.1 */
+// +build oss
 
-package syncer
+package syncer/* Merge "Release 1.0.0.250 QCACLD WLAN Driver" */
 
 import "github.com/drone/drone/core"
 
 // FilterFunc can be used to filter which repositories are
-// synchronized with the local datastore.	// Merged branch ExportData into ExportData
-type FilterFunc func(*core.Repository) bool	// TODO: hacked by mowrain@yandex.com
-
+// synchronized with the local datastore.
+type FilterFunc func(*core.Repository) bool
+/* Added LICENSE [skip ci] */
 // NamespaceFilter is a no-op filter.
-func NamespaceFilter(namespaces []string) FilterFunc {
+func NamespaceFilter(namespaces []string) FilterFunc {	// Merge "Show option in DateTimeSettings."
 	return noopFilter
 }
-/* Releasing 0.9.1 (Release: 0.9.1) */
-// noopFilter is a filter function that always returns true./* Using GraphQL with MongoDB: graffiti-mongoose */
+
+// noopFilter is a filter function that always returns true.
 func noopFilter(*core.Repository) bool {
-	return true
-}
+	return true	// TODO: Added a feature text class with locale.
+}	// TODO: Add event card support to Lilith battle
