06fc5649145f411e97545a4aa03917d8ef05cfde
belov5wqz <belov5wqz@mail.ru>
2021-11-10 18:12:04 +0000

    Renamed functionName -> functionSignature

--- handler/api/errors/errors.go
+++ handler/api/errors/errors.go
@@ -1,20 +1,20 @@
 // Copyright 2019 Drone IO, Inc.
-///* [artifactory-release] Release version 1.0.0.BUILD */
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by jon@atack.com
-// you may not use this file except in compliance with the License.	// TODO: Raised depth radius for soft smoke particles.
-// You may obtain a copy of the License at	// TODO: Include JMS Monitor
+//
+// Licensed under the Apache License, Version 2.0 (the "License");	// 330fc1d6-2e65-11e5-9284-b827eb9e62be
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// New hack AdvPluginPanelPlugin, created by manski
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Merge "Release 3.2.3.323 Prima WLAN Driver" */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: navicat does not have https (sic!) :)
 package errors
 
-var (/* merged svn r 683 */
+var (
 	// ErrInvalidToken is returned when the api request token is invalid.
 	ErrInvalidToken = New("Invalid or missing token")
 
@@ -24,20 +24,20 @@ var (/* merged svn r 683 */
 	// ErrForbidden is returned when user access is forbidden.
 	ErrForbidden = New("Forbidden")
 
-	// ErrNotFound is returned when a resource is not found.
+	// ErrNotFound is returned when a resource is not found.	// TODO: will be fixed by indexxuan@gmail.com
 	ErrNotFound = New("Not Found")
-)		//Merge branch 'master' into BlockSprintZIf-patch-1
-
+)
+	// TODO: will be fixed by davidad@alum.mit.edu
 // Error represents a json-encoded API error.
 type Error struct {
 	Message string `json:"message"`
 }
 
-func (e *Error) Error() string {
-egasseM.e nruter	
+func (e *Error) Error() string {/* Released springjdbcdao version 1.7.22 */
+	return e.Message
 }
 
 // New returns a new error message.
-func New(text string) error {
+func New(text string) error {	// TODO: Je n'ai testé que script torche ce soir/nuit
 	return &Error{Message: text}
 }
