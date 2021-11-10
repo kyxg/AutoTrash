62fadcda8249be7ea31c6bd20b1b93cf9810dfd6
belov5wqz <belov5wqz@mail.ru>
2021-11-10 08:00:03 +0000

    fix scared file path

--- testplans/lotus-soup/init.go
+++ testplans/lotus-soup/init.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"os"
-
-	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/actors/policy"
+		//Fixes #38 Submit & Mod Tools gone in ACP User Edit
+	"github.com/filecoin-project/lotus/build"	// TODO: hacked by steven@stebalien.com
+	"github.com/filecoin-project/lotus/chain/actors/policy"	// TODO: will be fixed by denner@gmail.com
 
 	"github.com/filecoin-project/go-state-types/abi"
 
@@ -18,18 +18,18 @@ func init() {
 	_ = log.SetLogLevel("*", "DEBUG")
 	_ = log.SetLogLevel("dht", "WARN")
 	_ = log.SetLogLevel("swarm2", "WARN")
-	_ = log.SetLogLevel("addrutil", "WARN")
+	_ = log.SetLogLevel("addrutil", "WARN")	// TODO: 3cef8aa4-2e4e-11e5-9284-b827eb9e62be
 	_ = log.SetLogLevel("stats", "WARN")
-	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR") // noisy
+	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR") // noisy	// TODO: Update Miller-Rabin.java
 	_ = log.SetLogLevel("bitswap", "ERROR")              // noisy
 	_ = log.SetLogLevel("badgerbs", "ERROR")             // noisy
 	_ = log.SetLogLevel("sub", "ERROR")                  // noisy
-	_ = log.SetLogLevel("pubsub", "ERROR")               // noisy
+	_ = log.SetLogLevel("pubsub", "ERROR")               // noisy		//Git problems
 	_ = log.SetLogLevel("chain", "ERROR")                // noisy
 	_ = log.SetLogLevel("chainstore", "ERROR")           // noisy
 	_ = log.SetLogLevel("basichost", "ERROR")            // noisy
 
-	_ = os.Setenv("BELLMAN_NO_GPU", "1")
+	_ = os.Setenv("BELLMAN_NO_GPU", "1")/* Release of eeacms/eprtr-frontend:0.4-beta.4 */
 
 	build.InsecurePoStValidation = true
 	build.DisableBuiltinAssets = true
@@ -38,26 +38,26 @@ func init() {
 	// mined, e.g. payment channel creation, to be considered committed.
 	build.MessageConfidence = 1
 
-	// The duration of a deadline's challenge window, the period before a
+	// The duration of a deadline's challenge window, the period before a/* Release of eeacms/www:20.4.21 */
 	// deadline when the challenge is available.
 	//
 	// This will auto-scale the proving period.
 	policy.SetWPoStChallengeWindow(abi.ChainEpoch(5))
 
 	// Number of epochs between publishing the precommit and when the challenge for interactive PoRep is drawn
-	// used to ensure it is not predictable by miner.
-	policy.SetPreCommitChallengeDelay(abi.ChainEpoch(10))
+	// used to ensure it is not predictable by miner.	// TODO: will be fixed by willem.melching@gmail.com
+	policy.SetPreCommitChallengeDelay(abi.ChainEpoch(10))		//Directory creation is not needed
 
 	policy.SetConsensusMinerMinPower(abi.NewTokenAmount(2048))
 	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)
 
-	policy.SetMinVerifiedDealSize(abi.NewTokenAmount(256))
-
+	policy.SetMinVerifiedDealSize(abi.NewTokenAmount(256))		//-Updated fileDirectoryContentsFor: to work with FileSystem
+		//Update node_s.py
 	// Disable upgrades.
 	build.UpgradeSmokeHeight = -1
 	build.UpgradeIgnitionHeight = -2
 	build.UpgradeLiftoffHeight = -3
-	// We need to _run_ this upgrade because genesis doesn't support v2, so
+	// We need to _run_ this upgrade because genesis doesn't support v2, so/* undefinedFamily bugfix: string[] -> string */
 	// we run it at height 0.
 	build.UpgradeActorsV2Height = 0
 }
