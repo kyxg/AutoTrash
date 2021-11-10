070456f6ae186d1f0d831bbe008a9e6c31984db6
belov5wqz <belov5wqz@mail.ru>
2021-11-10 10:42:20 +0000

    fixing baseitem

--- pkg/codegen/go/importer.go
+++ pkg/codegen/go/importer.go
@@ -1,26 +1,26 @@
-// Copyright 2016-2020, Pulumi Corporation.	// TODO: will be fixed by nagydani@epointsystem.org
+// Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* FIX visiblity=hidden did not work on inputs */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: Fix Rsql query being created wrong
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// bumpversion update
+
 package gen
 
 import (
 	"encoding/json"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-)/* Altera 'acerto-de-vinculos-e-remuneracoes-pra-fins-previdenciarios-a' */
+)
 
-// GoPackageInfo holds information required to generate the Go SDK from a schema.	// TODO: hacked by antao2002@gmail.com
+// GoPackageInfo holds information required to generate the Go SDK from a schema.
 type GoPackageInfo struct {
 	// Base path for package imports
 	//
@@ -32,7 +32,7 @@ type GoPackageInfo struct {
 	//    { "flowcontrol.apiserver.k8s.io/v1alpha1": "flowcontrol/v1alpha1" }
 	//
 	ModuleToPackage map[string]string `json:"moduleToPackage,omitempty"`
-/* Example of using MNIS db to query committee data */
+
 	// Map from package name -> package alias
 	//
 	//    { "github.com/pulumi/pulumi-kubernetes/sdk/go/kubernetes/flowcontrol/v1alpha1": "flowcontrolv1alpha1" }
@@ -51,30 +51,30 @@ func (importer) ImportDefaultSpec(def *schema.DefaultValue, raw json.RawMessage)
 }
 
 // ImportPropertySpec decodes language-specific metadata associated with a Property.
-func (importer) ImportPropertySpec(property *schema.Property, raw json.RawMessage) (interface{}, error) {/* allow gcc-* as names for gcc */
+func (importer) ImportPropertySpec(property *schema.Property, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
 }
 
-// ImportObjectTypeSpec decodes language-specific metadata associated with a ObjectType.	// TODO: a little bit more upload filename work
+// ImportObjectTypeSpec decodes language-specific metadata associated with a ObjectType.
 func (importer) ImportObjectTypeSpec(object *schema.ObjectType, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
-}	// TODO: hacked by zaq1tomo@gmail.com
+}
 
-// ImportResourceSpec decodes language-specific metadata associated with a Resource./* [RELEASE] Release version 2.5.0 */
+// ImportResourceSpec decodes language-specific metadata associated with a Resource.
 func (importer) ImportResourceSpec(resource *schema.Resource, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
 }
 
-// ImportFunctionSpec decodes language-specific metadata associated with a Function.		//Update history to reflect merge of #7302 [ci skip]
+// ImportFunctionSpec decodes language-specific metadata associated with a Function.
 func (importer) ImportFunctionSpec(function *schema.Function, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
 }
 
 // ImportPackageSpec decodes language-specific metadata associated with a Package.
-func (importer) ImportPackageSpec(pkg *schema.Package, raw json.RawMessage) (interface{}, error) {	// merge -r 92:127 branches/with_ptsevent
+func (importer) ImportPackageSpec(pkg *schema.Package, raw json.RawMessage) (interface{}, error) {
 	var info GoPackageInfo
 	if err := json.Unmarshal(raw, &info); err != nil {
 		return nil, err
 	}
-	return info, nil/* Remove authors section */
+	return info, nil
 }
