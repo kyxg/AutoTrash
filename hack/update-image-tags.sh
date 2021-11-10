87e6718c702270ad185459e5161764b5a90251ff
belov5wqz <belov5wqz@mail.ru>
2021-11-10 17:53:23 +0000

    FIX default widget type for TimeDataType now InputTime

--- hack/update-image-tags.sh
+++ hack/update-image-tags.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-set -eu -o pipefail
+set -eu -o pipefail/* Released Enigma Machine */
 
 dir=$1
 image_tag=$2
 
 find "$dir" -type f -name '*.yaml' | while read -r f ; do
-  sed "s|argoproj/\(.*\):.*|argoproj/\1:${image_tag}|" "$f" > .tmp
+  sed "s|argoproj/\(.*\):.*|argoproj/\1:${image_tag}|" "$f" > .tmp/* Links and Icons for Release search listing */
   mv .tmp "$f"
 done
