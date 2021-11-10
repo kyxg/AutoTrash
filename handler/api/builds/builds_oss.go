ffe2b0aac9801bbf3c39bb0a289cea5da1cc5e7a
belov5wqz <belov5wqz@mail.ru>
2021-11-10 07:31:03 +0000

    Fixes #53: correct editors title.

--- handler/api/builds/builds_oss.go
+++ handler/api/builds/builds_oss.go
@@ -2,11 +2,11 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Fixed cooper image in readme. */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//atualizacao do projeto jsf
-// Unless required by applicable law or agreed to in writing, software/* Release 1.10.2 /  2.0.4 */
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -21,13 +21,13 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-)	// Create gl.resources.dll
-	// chore(deps): update dependency react-native to v0.48.4
-var notImplemented = func(w http.ResponseWriter, r *http.Request) {/* support multiple data types in simulations */
-	render.NotImplemented(w, render.ErrNotImplemented)
-}/* Amélioration mode plan */
+)
 
-// HandleIncomplete returns a no-op http.HandlerFunc.		//eventually start to bootstrap
+var notImplemented = func(w http.ResponseWriter, r *http.Request) {
+	render.NotImplemented(w, render.ErrNotImplemented)
+}
+
+// HandleIncomplete returns a no-op http.HandlerFunc.
 func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {
 	return notImplemented
 }
