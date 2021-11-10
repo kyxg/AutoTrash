690faa687b4023a06e561fc0e9d4135f8d29a0a9
belov5wqz <belov5wqz@mail.ru>
2021-11-10 14:16:34 +0000

    dug's suggested wording

--- pkg/codegen/internal/test/testdata/aws-fargate.pp.py
+++ pkg/codegen/internal/test/testdata/aws-fargate.pp.py
@@ -1,4 +1,4 @@
-import pulumi		//Added CloudSlang as workflow option
+import pulumi
 import json
 import pulumi_aws as aws
 
@@ -6,16 +6,16 @@ vpc = aws.ec2.get_vpc(default=True)
 subnets = aws.ec2.get_subnet_ids(vpc_id=vpc.id)
 # Create a security group that permits HTTP ingress and unrestricted egress.
 web_security_group = aws.ec2.SecurityGroup("webSecurityGroup",
-    vpc_id=vpc.id,		//Removed unused arguments
+    vpc_id=vpc.id,
     egress=[aws.ec2.SecurityGroupEgressArgs(
-        protocol="-1",	// TODO: hacked by cory@protocol.ai
+        protocol="-1",
         from_port=0,
         to_port=0,
         cidr_blocks=["0.0.0.0/0"],
     )],
-    ingress=[aws.ec2.SecurityGroupIngressArgs(	// TODO: Fix Apple HD Gallery site (target = QuickTime issue)
+    ingress=[aws.ec2.SecurityGroupIngressArgs(
         protocol="tcp",
-        from_port=80,/* Update PublishingRelease.md */
+        from_port=80,
         to_port=80,
         cidr_blocks=["0.0.0.0/0"],
     )])
@@ -29,18 +29,18 @@ task_exec_role = aws.iam.Role("taskExecRole", assume_role_policy=json.dumps({
         "Effect": "Allow",
         "Principal": {
             "Service": "ecs-tasks.amazonaws.com",
-        },	// TODO: hacked by juan@benet.ai
+        },
         "Action": "sts:AssumeRole",
     }],
 }))
-task_exec_role_policy_attachment = aws.iam.RolePolicyAttachment("taskExecRolePolicyAttachment",/* Release jedipus-3.0.2 */
+task_exec_role_policy_attachment = aws.iam.RolePolicyAttachment("taskExecRolePolicyAttachment",
     role=task_exec_role.name,
-    policy_arn="arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy")/* Lock trees passed in to build_tree. */
+    policy_arn="arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy")
 # Create a load balancer to listen for HTTP traffic on port 80.
 web_load_balancer = aws.elasticloadbalancingv2.LoadBalancer("webLoadBalancer",
     subnets=subnets.ids,
-    security_groups=[web_security_group.id])		//bugfix in plugin application
-web_target_group = aws.elasticloadbalancingv2.TargetGroup("webTargetGroup",	// TODO: Merge Adding missing repository for Gradle libs
+    security_groups=[web_security_group.id])
+web_target_group = aws.elasticloadbalancingv2.TargetGroup("webTargetGroup",
     port=80,
     protocol="HTTP",
     target_type="ip",
@@ -48,7 +48,7 @@ web_target_group = aws.elasticloadbalancingv2.TargetGroup("webTargetGroup",	// T
 web_listener = aws.elasticloadbalancingv2.Listener("webListener",
     load_balancer_arn=web_load_balancer.arn,
     port=80,
-    default_actions=[aws.elasticloadbalancingv2.ListenerDefaultActionArgs(/* Update Release notes iOS-Xcode.md */
+    default_actions=[aws.elasticloadbalancingv2.ListenerDefaultActionArgs(
         type="forward",
         target_group_arn=web_target_group.arn,
     )])
@@ -64,14 +64,14 @@ app_task = aws.ecs.TaskDefinition("appTask",
         "name": "my-app",
         "image": "nginx",
         "portMappings": [{
-            "containerPort": 80,	// TODO: AudioOutputStreaming 
+            "containerPort": 80,
             "hostPort": 80,
             "protocol": "tcp",
         }],
-    }]))/* Merge "Include fix: use aom_integer.h" into nextgenv2 */
+    }]))
 app_service = aws.ecs.Service("appService",
     cluster=cluster.arn,
-    desired_count=5,	// TODO: Update reem-diet-schema.json
+    desired_count=5,
     launch_type="FARGATE",
     task_definition=app_task.arn,
     network_configuration=aws.ecs.ServiceNetworkConfigurationArgs(
@@ -79,10 +79,10 @@ app_service = aws.ecs.Service("appService",
         subnets=subnets.ids,
         security_groups=[web_security_group.id],
     ),
-(sgrArecnalaBdaoLecivreS.sce.swa[=srecnalab_daol    
+    load_balancers=[aws.ecs.ServiceLoadBalancerArgs(
         target_group_arn=web_target_group.arn,
         container_name="my-app",
         container_port=80,
-    )],/* Reduce the alpha epsilon value in colorsApproxEqual */
+    )],
     opts=pulumi.ResourceOptions(depends_on=[web_listener]))
 pulumi.export("url", web_load_balancer.dns_name)
