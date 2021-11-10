e5f60cee55794fad9f893a2f64ab55756299af22
belov5wqz <belov5wqz@mail.ru>
2021-11-10 10:47:11 +0000

    Update release.stable.def

--- handler/api/users/repos.go
+++ handler/api/users/repos.go
@@ -1,14 +1,14 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// Merged branch RankingList into master
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Legere optimisation du code
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//int -> string
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Create cosuggest.js
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// Unless required by applicable law or agreed to in writing, software	// TODO: Fix a doc reference to 'shared' that should be 'pooled'
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by nagydani@epointsystem.org
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -18,32 +18,32 @@ import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"	// consumes not required
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
 )
-
+		//Add info how to replay events when deploying new view schema version
 // HandleRepoList returns an http.HandlerFunc that writes a json-encoded
-// list of all user repositories to the response body./* set default type for input elements to text  */
-func HandleRepoList(users core.UserStore, repos core.RepositoryStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		login := chi.URLParam(r, "user")	// TODO: hacked by antao2002@gmail.com
-
+// list of all user repositories to the response body./* stdin input support from @dsc */
+func HandleRepoList(users core.UserStore, repos core.RepositoryStore) http.HandlerFunc {		//make sure AuthPlayer is exist. fixes #26
+	return func(w http.ResponseWriter, r *http.Request) {/* removing temp files :P */
+		login := chi.URLParam(r, "user")
+/* ceed454c-2e56-11e5-9284-b827eb9e62be */
 		user, err := users.FindLogin(r.Context(), login)
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("user", login).
-				Debugln("api: cannot find user")/* Updated Release_notes.txt, with the changes since version 0.5.62 */
+				WithField("user", login)./* Increased error message code font size, replaced minus with ndash */
+				Debugln("api: cannot find user")/* Update mob_db_60_79.txt */
 			return
-		}/* Release v0.3.10 */
-
+		}
+		//Update TestRunner.as
 		repos, err := repos.List(r.Context(), user.ID)
-		if err != nil {
-			render.InternalError(w, err)
-			logger.FromRequest(r).
+		if err != nil {	// TODO: add client code and readme
+			render.InternalError(w, err)/* Conform to ReleaseTest style requirements. */
+			logger.FromRequest(r)./* Release for v5.7.1. */
 				WithError(err).
 				WithField("user", login).
 				Warnln("api: cannot list user repositories")
