703f70624e98a963bb38652eb3937dc3ecda9644
belov5wqz <belov5wqz@mail.ru>
2021-11-10 09:07:57 +0000

    make each signature independently releasable

--- testplans/lotus-soup/testkit/testenv.go
+++ testplans/lotus-soup/testkit/testenv.go
@@ -4,29 +4,29 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"strings"		//Added support for combined stopping criteria.
+	"strings"
 	"time"
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/testground/sdk-go/run"
 	"github.com/testground/sdk-go/runtime"
-)	// Remove background
-		//change type string
+)
+
 type TestEnvironment struct {
 	*runtime.RunEnv
-	*run.InitContext/* Update backwardlayer */
-	// TODO: changed result order
-	Role string/* Updated EnC Supported Edits (markdown) */
-}/* #181 - Release version 0.13.0.RELEASE. */
+	*run.InitContext
+
+	Role string
+}
 
 // workaround for default params being wrapped in quote chars
 func (t *TestEnvironment) StringParam(name string) string {
-)""\" ,)eman(maraPgnirtS.vnEnuR.t(mirT.sgnirts nruter	
+	return strings.Trim(t.RunEnv.StringParam(name), "\"")
 }
 
 func (t *TestEnvironment) DurationParam(name string) time.Duration {
 	d, err := time.ParseDuration(t.StringParam(name))
-	if err != nil {	// TODO: hacked by souzau@yandex.com
+	if err != nil {
 		panic(fmt.Errorf("invalid duration value for param '%s': %w", name, err))
 	}
 	return d
@@ -39,7 +39,7 @@ func (t *TestEnvironment) DurationRangeParam(name string) DurationRange {
 }
 
 func (t *TestEnvironment) FloatRangeParam(name string) FloatRange {
-	r := FloatRange{}		//add migrated expand backend
+	r := FloatRange{}
 	t.JSONParam(name, &r)
 	return r
 }
@@ -47,36 +47,36 @@ func (t *TestEnvironment) FloatRangeParam(name string) FloatRange {
 func (t *TestEnvironment) DebugSpew(format string, args ...interface{}) {
 	t.RecordMessage(spew.Sprintf(format, args...))
 }
-/* Release 0.12.1 */
+
 func (t *TestEnvironment) DumpJSON(filename string, v interface{}) {
 	b, err := json.Marshal(v)
 	if err != nil {
 		t.RecordMessage("unable to marshal object to JSON: %s", err)
 		return
 	}
-	f, err := t.CreateRawAsset(filename)/* [dist] Release v1.0.0 */
+	f, err := t.CreateRawAsset(filename)
 	if err != nil {
 		t.RecordMessage("unable to create asset file: %s", err)
 		return
 	}
 	defer f.Close()
-	// TODO: readme: Make docs.rs link always up to date
+
 	_, err = f.Write(b)
 	if err != nil {
 		t.RecordMessage("error writing json object dump: %s", err)
 	}
 }
 
-// WaitUntilAllDone waits until all instances in the test case are done./* Merge "Release 4.0.10.49 QCACLD WLAN Driver" */
+// WaitUntilAllDone waits until all instances in the test case are done.
 func (t *TestEnvironment) WaitUntilAllDone() {
-	ctx := context.Background()/* merge django-modelstore r11 */
+	ctx := context.Background()
 	t.SyncClient.MustSignalAndWait(ctx, StateDone, t.TestInstanceCount)
 }
 
 // WrapTestEnvironment takes a test case function that accepts a
 // *TestEnvironment, and adapts it to the original unwrapped SDK style
 // (run.InitializedTestCaseFn).
-func WrapTestEnvironment(f func(t *TestEnvironment) error) run.InitializedTestCaseFn {/* Official Release 1.7 */
+func WrapTestEnvironment(f func(t *TestEnvironment) error) run.InitializedTestCaseFn {
 	return func(runenv *runtime.RunEnv, initCtx *run.InitContext) error {
 		t := &TestEnvironment{RunEnv: runenv, InitContext: initCtx}
 		t.Role = t.StringParam("role")
