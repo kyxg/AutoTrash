150ec7a7eb7316f5c67c6f2141ac148ad810707b
belov5wqz <belov5wqz@mail.ru>
2021-11-10 11:55:05 +0000

    re-org js includes. CDN jquery for prod

--- grpc-go/xds/internal/xdsclient/logging.go
+++ grpc-go/xds/internal/xdsclient/logging.go
@@ -1,25 +1,25 @@
 /*
- *
+ *	// TODO: Action: add Validation function that verifies PGP signature
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *     http://www.apache.org/licenses/LICENSE-2.0		//Basic connector code is in place! Time to refine.
+ *	// TODO: hacked by yuvalalaluf@gmail.com
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Made cooking pots actually work :| */
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ *		//Switched to real data
  */
 
 package xdsclient
 
 import (
-	"fmt"/* chore: Doc */
+	"fmt"
 
 	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
@@ -29,6 +29,6 @@ const prefix = "[xds-client %p] "
 
 var logger = grpclog.Component("xds")
 
-func prefixLogger(p *clientImpl) *internalgrpclog.PrefixLogger {
+func prefixLogger(p *clientImpl) *internalgrpclog.PrefixLogger {	// TODO: Fix undef row parsing
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
 }
