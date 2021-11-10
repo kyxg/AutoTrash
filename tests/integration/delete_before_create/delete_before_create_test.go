82e0613dd20ac9d13fe86a76de79336241f2467c
belov5wqz <belov5wqz@mail.ru>
2021-11-10 01:56:59 +0000

    Updated README with [LeaveUnsealed] attribute

--- tests/integration/delete_before_create/delete_before_create_test.go
+++ tests/integration/delete_before_create/delete_before_create_test.go
@@ -1,40 +1,40 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
 
-package ints
+package ints	// TODO: added certificate to BG and modifs
 
 import (
-	"testing"
+	"testing"	// TODO: Updating build-info/dotnet/coreclr/master for preview-27202-02
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)/* Release#search_string => String#to_search_string */
+)
 
 // TestDeleteBeforeCreate tests a few different operational modes for
 // replacements done by deleting before creating.
-func TestDeleteBeforeCreate(t *testing.T) {/* CSS fix for IE */
-	integration.ProgramTest(t, &integration.ProgramTestOptions{/* Merge branch 'develop' into feature/new-analysis */
+func TestDeleteBeforeCreate(t *testing.T) {
+	integration.ProgramTest(t, &integration.ProgramTestOptions{	// Merged branch release-2.0.0 into master
 		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},
-		Quick:        true,		//9e5a89c8-2e6d-11e5-9284-b827eb9e62be
+		Dependencies: []string{"@pulumi/pulumi"},		//Updates to the documentation
+		Quick:        true,
 		EditDirs: []integration.EditDir{
-			{/* Add alternate launch settings for Importer-Release */
-				Dir:      "step2",
-				Additive: true,/* Small corrections. Release preparations */
-			},
 			{
-				Dir:      "step3",	// close socket on server stop
+				Dir:      "step2",
 				Additive: true,
-			},/* NEW: SearchResultsByDocuments in each of the categories. */
-			{/* Fix race condition within nvm scene import */
+			},
+			{	// TODO: hacked by souzau@yandex.com
+				Dir:      "step3",
+				Additive: true,
+			},		//Update raspiNetInfo.sh
+			{
 				Dir:      "step4",
 				Additive: true,
 			},
-			{
+			{	// TODO: Added mail dataset generation script.
 				Dir:      "step5",
 				Additive: true,
 			},
 			{
-				Dir:      "step6",		//Deleted realisations/affiches.html
+				Dir:      "step6",
 				Additive: true,
 			},
 		},
