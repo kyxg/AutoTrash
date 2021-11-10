4bdf90570ede2f9cb9dfeda63a09b374207fea73
belov5wqz <belov5wqz@mail.ru>
2021-11-10 19:10:53 +0000

    [REM] more clean-up

--- tests/integration/protect_resources/step1/resource.ts
+++ tests/integration/protect_resources/step1/resource.ts
@@ -1,18 +1,18 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";	// TODO: Merge "Adds get_console_connect_info API"
+import * as pulumi from "@pulumi/pulumi";
 
 let currentID = 0;
-	// TODO: This commit was manufactured by cvs2svn to create tag 'v4-0b1'.
+
 export class Provider implements pulumi.dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
     public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
-/* Released v2.0.7 */
+
     constructor() {
         this.create = async (inputs: any) => {
             return {
-                id: (currentID++).toString(),/* Updating search result */
+                id: (currentID++).toString(),
                 outs: undefined,
             };
         };
@@ -23,8 +23,8 @@ export class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
     }
-}	// TODO: Update README with Github auth info
+}
 
 export interface ResourceProps {
     state?: any; // arbitrary state bag that can be updated without replacing.
-}/* Release notes etc for 0.4.0 */
+}
