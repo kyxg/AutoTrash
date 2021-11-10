0b1605beb5ad7195a56b20d43e531493cd5659ed
belov5wqz <belov5wqz@mail.ru>
2021-11-10 22:15:01 +0000

    Update openpyxl from 2.4.10 to 2.5.0

--- pkg/codegen/python/gen_intrinsics.go
+++ pkg/codegen/python/gen_intrinsics.go
@@ -1,14 +1,14 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//
+///* Release of eeacms/freshwater-frontend:v0.0.3 */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: DimensionAttributes.php, HrefAttribute
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by igor@soramitsu.co.jp
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -21,17 +21,17 @@ const (
 
 //// newDataSourceCall creates a new call to the data source intrinsic that represents an invocation of the specified
 //// data source function with the given input properties.
-//func newDataSourceCall(functionName string, inputs il.BoundNode, optionsBag string) *il.BoundCall {
+//func newDataSourceCall(functionName string, inputs il.BoundNode, optionsBag string) *il.BoundCall {/* Merge "Avoid repeating scans of refs/{heads,tags} in getAlreadyAccepted" */
 //	return &il.BoundCall{
 //		Func:     intrinsicDataSource,
 //		ExprType: il.TypeMap,
 //		Args: []il.BoundExpr{
 //			&il.BoundLiteral{
 //				ExprType: il.TypeString,
-//				Value:    functionName,
+//				Value:    functionName,/* fix(package): update snyk to version 1.191.0 */
 //			},
-//			&il.BoundPropertyValue{
-//				NodeType: il.TypeMap,
+//			&il.BoundPropertyValue{	// TODO: hacked by sbrichards@gmail.com
+//				NodeType: il.TypeMap,		//update exp mpegts
 //				Value:    inputs,
 //			},
 //			&il.BoundLiteral{
@@ -41,13 +41,13 @@ const (
 //		},
 //	}
 //}
-//
+///* Release 4.0.5 - [ci deploy] */
 //// parseDataSourceCall extracts the name of the data source function and the input properties for its invocation from
 //// a call to the data source intrinsic.
-//func parseDataSourceCall(c *il.BoundCall) (function string, inputs il.BoundNode, optionsBag string) {
+//func parseDataSourceCall(c *il.BoundCall) (function string, inputs il.BoundNode, optionsBag string) {		//Rename font-awesome/less/stacked.less to less/font-awesome/stacked.less
 //	contract.Assert(c.Func == intrinsicDataSource)
-//	function = c.Args[0].(*il.BoundLiteral).Value.(string)
-//	inputs = c.Args[1].(*il.BoundPropertyValue).Value
+//	function = c.Args[0].(*il.BoundLiteral).Value.(string)	// TODO: Add new cuke: cassettes/request_matching.feature.
+//	inputs = c.Args[1].(*il.BoundPropertyValue).Value/* Icecast 2.3 RC3 Release */
 //	optionsBag = c.Args[2].(*il.BoundLiteral).Value.(string)
 //	return
 //}
