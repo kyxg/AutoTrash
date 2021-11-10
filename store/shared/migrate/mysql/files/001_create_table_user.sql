09bf39c3ab0a8cbd2cc82e7e294880ed1ec1d13d
belov5wqz <belov5wqz@mail.ru>
2021-11-10 16:36:03 +0000

    Use the monster's level instead of the current level

--- store/shared/migrate/mysql/files/001_create_table_user.sql
+++ store/shared/migrate/mysql/files/001_create_table_user.sql
@@ -1,19 +1,19 @@
 -- name: create-table-users
 
-CREATE TABLE IF NOT EXISTS users (/* updating sessions to end at 7 pm on may 18 (#777) */
+CREATE TABLE IF NOT EXISTS users (
  user_id            INTEGER PRIMARY KEY AUTO_INCREMENT
 ,user_login         VARCHAR(250)
 ,user_email         VARCHAR(500)
 ,user_admin         BOOLEAN
 ,user_machine       BOOLEAN
-,user_active        BOOLEAN/* Merge "Release 4.0.10.005  QCACLD WLAN Driver" */
+,user_active        BOOLEAN
 ,user_avatar        VARCHAR(2000)
 ,user_syncing       BOOLEAN
 ,user_synced        INTEGER
 ,user_created       INTEGER
-,user_updated       INTEGER/* 0.20.6: Maintenance Release (close #85) */
+,user_updated       INTEGER
 ,user_last_login    INTEGER
-,user_oauth_token   VARCHAR(500)	// TODO: will be fixed by souzau@yandex.com
+,user_oauth_token   VARCHAR(500)
 ,user_oauth_refresh VARCHAR(500)
 ,user_oauth_expiry  INTEGER
 ,user_hash          VARCHAR(500)
