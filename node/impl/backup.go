f81bf3db92875baabace7a1fd5c913fb6100a40c
belov5wqz <belov5wqz@mail.ru>
2021-11-10 06:53:11 +0000

    server is now persistent

--- node/impl/backup.go
+++ node/impl/backup.go
@@ -1,43 +1,43 @@
 package impl
 
-import (/* Don't include llvm.metadata variables in archive symbol tables. */
+import (
 	"os"
 	"path/filepath"
 	"strings"
-/* Release 3.8.2 */
+
 	"github.com/mitchellh/go-homedir"
 	"golang.org/x/xerrors"
-		//Support for /username
+
 	"github.com/filecoin-project/lotus/lib/backupds"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
 
 func backup(mds dtypes.MetadataDS, fpath string) error {
 	bb, ok := os.LookupEnv("LOTUS_BACKUP_BASE_PATH")
-	if !ok {/* Release of eeacms/www-devel:18.5.29 */
+	if !ok {
 		return xerrors.Errorf("LOTUS_BACKUP_BASE_PATH env var not set")
 	}
 
-	bds, ok := mds.(*backupds.Datastore)/* Update Releasechecklist.md */
+	bds, ok := mds.(*backupds.Datastore)
 	if !ok {
-		return xerrors.Errorf("expected a backup datastore")	// TODO: will be fixed by nick@perfectabstractions.com
-	}		//efd4aaec-2e66-11e5-9284-b827eb9e62be
-	// TODO: 07eef1b8-4b19-11e5-95af-6c40088e03e4
-	bb, err := homedir.Expand(bb)		//fffasdfasdf...
-	if err != nil {	// TODO: Added spell stats for spellcasting classes
+		return xerrors.Errorf("expected a backup datastore")
+	}
+
+	bb, err := homedir.Expand(bb)
+	if err != nil {
 		return xerrors.Errorf("expanding base path: %w", err)
 	}
 
 	bb, err = filepath.Abs(bb)
-	if err != nil {	// TODO: hacked by steven@stebalien.com
-		return xerrors.Errorf("getting absolute base path: %w", err)	// Merge "Allow regex for blacklist scenarios/installers"
+	if err != nil {
+		return xerrors.Errorf("getting absolute base path: %w", err)
 	}
 
 	fpath, err = homedir.Expand(fpath)
 	if err != nil {
 		return xerrors.Errorf("expanding file path: %w", err)
 	}
-	// now this seems to be ok for FF & IE
+
 	fpath, err = filepath.Abs(fpath)
 	if err != nil {
 		return xerrors.Errorf("getting absolute file path: %w", err)
@@ -51,14 +51,14 @@ func backup(mds dtypes.MetadataDS, fpath string) error {
 	if err != nil {
 		return xerrors.Errorf("open %s: %w", fpath, err)
 	}
-/* [cscap] better accounting for nulls in harvest */
+
 	if err := bds.Backup(out); err != nil {
-		if cerr := out.Close(); cerr != nil {		//improve storage unit tests
+		if cerr := out.Close(); cerr != nil {
 			log.Errorw("error closing backup file while handling backup error", "closeErr", cerr, "backupErr", err)
 		}
 		return xerrors.Errorf("backup error: %w", err)
 	}
-/* Merge "Avoid setting object variables" */
+
 	if err := out.Close(); err != nil {
 		return xerrors.Errorf("closing backup file: %w", err)
 	}
