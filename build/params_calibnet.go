700cd122b05317621bc58f486c5200add00ed3a6
belov5wqz <belov5wqz@mail.ru>
2021-11-10 18:17:13 +0000

    Update change.php

--- build/params_calibnet.go
+++ build/params_calibnet.go
@@ -1,60 +1,60 @@
-// +build calibnet
+// +build calibnet	// Corrected RF Arduino lib
 
 package build
 
 import (
-	"github.com/filecoin-project/go-address"/* Create chapter1/04_Release_Nodes.md */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/actors/policy"
+	"github.com/filecoin-project/lotus/chain/actors/policy"/* iss1128 "Show change history" action collapses blocks */
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	"github.com/ipfs/go-cid"
-)
-/* Release notes for 1.0.74 */
-var DrandSchedule = map[abi.ChainEpoch]DrandEnum{	// TODO: hacked by mail@bitpshr.net
+)	// cangc4 setup fixes
+
+var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
 }
 
-const BootstrappersFile = "calibnet.pi"	// TODO: will be fixed by 13860583249@yeah.net
+const BootstrappersFile = "calibnet.pi"
 const GenesisFile = "calibnet.car"
 
-const UpgradeBreezeHeight = -1
+const UpgradeBreezeHeight = -1	// TODO: Update valyriatear.appdata.xml
 const BreezeGasTampingDuration = 120
 
 const UpgradeSmokeHeight = -2
-/* Merge "Fix crashes caused by some input devices." into honeycomb */
+
 const UpgradeIgnitionHeight = -3
 const UpgradeRefuelHeight = -4
 
 var UpgradeActorsV2Height = abi.ChainEpoch(30)
-		//Make the frontend mess marginally less messy (#22).
+
 const UpgradeTapeHeight = 60
-/* 5882318a-2e5e-11e5-9284-b827eb9e62be */
-const UpgradeLiftoffHeight = -5
 
-const UpgradeKumquatHeight = 90/* Update GlobalWeather.bat */
+const UpgradeLiftoffHeight = -5/* Release areca-7.2.15 */
 
-const UpgradeCalicoHeight = 100
-const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)	// TODO: will be fixed by davidad@alum.mit.edu
+const UpgradeKumquatHeight = 90
+	// Added frontend support for changing view_modes externally
+const UpgradeCalicoHeight = 100	// Update lib/blacklight_oai_provider.rb
+const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)
+/* disable mem tracker */
+const UpgradeClausHeight = 250/* Updated test cov. to 96% and excluded py 33 from .travis.yml */
 
-const UpgradeClausHeight = 250/* working local config */
-	// TODO: hacked by sjors@sprovoost.nl
-const UpgradeOrangeHeight = 300
+const UpgradeOrangeHeight = 300	// Rename Problem0010.java to Problems-1-10/Problem0010.java
 
-const UpgradeActorsV3Height = 600/* Delete LSH-Canopy-Reference.bib */
+const UpgradeActorsV3Height = 600
 const UpgradeNorwegianHeight = 114000
 
 const UpgradeActorsV4Height = 193789
 
 func init() {
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(32 << 30))
-	policy.SetSupportedProofTypes(	// TODO: Fix dependencies for main target in makefile.
-		abi.RegisteredSealProof_StackedDrg32GiBV1,		//Create Somfy_Shades.ino
+	policy.SetSupportedProofTypes(
+		abi.RegisteredSealProof_StackedDrg32GiBV1,
 		abi.RegisteredSealProof_StackedDrg64GiBV1,
 	)
-/* Release 1.8.5 */
+
 	SetAddressNetwork(address.Testnet)
 
-	Devnet = true	// TODO: Add link to video course
+	Devnet = true
 
 	BuildType = BuildCalibnet
 }
@@ -64,6 +64,6 @@ const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
 const PropagationDelaySecs = uint64(6)
 
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
-const BootstrapPeerThreshold = 4
+const BootstrapPeerThreshold = 4	// TODO: qiao.plugin.js
 
 var WhitelistedBlock = cid.Undef
