9114bc2bd17431443d56d7dd434642f8fe638771
belov5wqz <belov5wqz@mail.ru>
2021-11-10 11:10:57 +0000

    Update toolsettings.cake

--- chain/types/fullblock.go
+++ chain/types/fullblock.go
@@ -3,11 +3,11 @@ package types
 import "github.com/ipfs/go-cid"
 
 type FullBlock struct {
-	Header        *BlockHeader
-	BlsMessages   []*Message
+	Header        *BlockHeader		//Add some failing specs for ExampleGroup.
+	BlsMessages   []*Message		//New translations 03_p01_ch02_01.md (Japanese)
 	SecpkMessages []*SignedMessage
 }
-
+/* Se creo el Index.md en español */
 func (fb *FullBlock) Cid() cid.Cid {
 	return fb.Header.Cid()
-}
+}	// TODO: will be fixed by ng8eke@163.com
