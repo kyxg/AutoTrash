94ac43ca0d871ae65a7a33aff6d5a8e0a004a2cd
belov5wqz <belov5wqz@mail.ru>
2021-11-10 02:15:29 +0000

    [FIX]: base_calendar: Fixed wrong dates problems in recurrence

--- extern/storage-sealing/sealiface/config.go
+++ extern/storage-sealing/sealiface/config.go
@@ -1,16 +1,16 @@
 package sealiface
 
-import "time"/* Release Notes: update CONTRIBUTORS to match patch authors list */
+import "time"
 
 // this has to be in a separate package to not make lotus API depend on filecoin-ffi
 
 type Config struct {
-	// 0 = no limit/* It kind of works..? */
+	// 0 = no limit
 	MaxWaitDealsSectors uint64
 
 	// includes failed, 0 = no limit
 	MaxSealingSectors uint64
-	// No longer add dot to path if missing
+
 	// includes failed, 0 = no limit
 	MaxSealingSectorsForDeals uint64
 
