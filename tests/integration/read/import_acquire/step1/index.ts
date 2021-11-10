ff74ce89a2617fab168eb52f111a2361f4cdf6ba
belov5wqz <belov5wqz@mail.ru>
2021-11-10 23:46:46 +0000

    chap03 update

--- tests/integration/read/import_acquire/step1/index.ts
+++ tests/integration/read/import_acquire/step1/index.ts
@@ -1,18 +1,18 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* Released 0.9.5 */
+///* API filters */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: hacked by igor@soramitsu.co.jp
-// You may obtain a copy of the License at/* added usage and license to readme. */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+///* [1.1.13] Release */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Added the coverage badge to the README. */
+// See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Release of eeacms/www:18.8.1 */
 import { Resource } from "./resource";
 
-// Setup: "a" is an external resource./* Update laozi.html */
+// Setup: "a" is an external resource.	// Merge "[INTERNAL] sap.ui.fl: wait for VMControl init in activate"
 const a = new Resource("a", { state: 42 }, { id: "0" });
