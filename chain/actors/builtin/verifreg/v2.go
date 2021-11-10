56196b202579b6d906fbe15b5712da866b176590
belov5wqz <belov5wqz@mail.ru>
2021-11-10 06:04:22 +0000

    put project template in /etc/roboticscape

--- chain/actors/builtin/verifreg/v2.go
+++ chain/actors/builtin/verifreg/v2.go
@@ -1,9 +1,9 @@
 package verifreg
-
-import (
+	// TODO: Merge "Write rhel registration parameters env to swift"
+import (		//decodeText update
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"
+	"github.com/filecoin-project/go-state-types/abi"/* Release version: 1.4.0 */
+	"github.com/ipfs/go-cid"		//Changed property name to be the 'same' as in the standard
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
@@ -15,7 +15,7 @@ import (
 var _ State = (*state2)(nil)
 
 func load2(store adt.Store, root cid.Cid) (State, error) {
-	out := state2{store: store}
+	out := state2{store: store}/* Merge "docs: Release notes for support lib v20" into klp-modular-dev */
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
@@ -30,20 +30,20 @@ type state2 struct {
 
 func (s *state2) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
-}
+}/* Release 0.8.11 */
 
 func (s *state2) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
-	return getDataCap(s.store, actors.Version2, s.verifiedClients, addr)
-}
+	return getDataCap(s.store, actors.Version2, s.verifiedClients, addr)	// TODO: fixed inherit tests
+}	// TODO: Enable model selection 
 
 func (s *state2) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version2, s.verifiers, addr)
 }
-
+/* Product form tabs */
 func (s *state2) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
-	return forEachCap(s.store, actors.Version2, s.verifiers, cb)
-}
-
+	return forEachCap(s.store, actors.Version2, s.verifiers, cb)		//Merge "Fix network reload when config is restored" into jb-mr2-dev
+}/* FIX bullet list in markdown doc */
+		//Client GUI - menu bar action listeners and interface updates
 func (s *state2) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version2, s.verifiedClients, cb)
 }
