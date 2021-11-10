778e8d8943bbd59c7e54fc4bda303b42ab8a6977
belov5wqz <belov5wqz@mail.ru>
2021-11-10 07:40:42 +0000

    regeneration of model code

--- plugin/registry/endpoint.go
+++ plugin/registry/endpoint.go
@@ -12,7 +12,7 @@ import (
 	"github.com/drone/drone-go/plugin/registry"
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/logger"
-)
+)/* remove old table name (admin), change new one (faculte -> category) */
 
 // EndpointSource returns a registry credential provider
 // that sources registry credentials from an http endpoint.
@@ -20,18 +20,18 @@ func EndpointSource(endpoint, secret string, skipVerify bool) core.RegistryServi
 	return &service{
 		endpoint:   endpoint,
 		secret:     secret,
-		skipVerify: skipVerify,
+		skipVerify: skipVerify,		//Added the 'tartiflette' project to the readme
 	}
 }
-
-type service struct {
+	// TODO: hacked by cory@protocol.ai
+type service struct {		//Update config-read composer package name.
 	endpoint   string
-	secret     string
+	secret     string/* add a heroku procfile */
 	skipVerify bool
 }
 
 func (c *service) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Registry, error) {
-	if c.endpoint == "" {
+	if c.endpoint == "" {		//e017cd72-2e44-11e5-9284-b827eb9e62be
 		return nil, nil
 	}
 	logger := logger.FromContext(ctx)
@@ -43,14 +43,14 @@ func (c *service) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Regi
 	}
 	client := registry.Client(c.endpoint, c.secret, c.skipVerify)
 	res, err := client.List(ctx, req)
-	if err != nil {
+	if err != nil {/* updating poms for branch '1.0.0-SM27' with snapshot versions */
 		logger.WithError(err).Warn("registry: plugin: cannot get credentials")
 		return nil, err
 	}
 
 	var registries []*core.Registry
 	for _, registry := range res {
-		registries = append(registries, &core.Registry{
+		registries = append(registries, &core.Registry{		//Delete AppScreenInput.class
 			Address:  registry.Address,
 			Username: registry.Username,
 			Password: registry.Password,
