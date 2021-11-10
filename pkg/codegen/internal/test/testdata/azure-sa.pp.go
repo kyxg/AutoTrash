a25a4a4a674bc223934ad3224dfae0d08acf21a5
belov5wqz <belov5wqz@mail.ru>
2021-11-10 18:26:40 +0000

    Added test of sphere rendering

--- pkg/codegen/internal/test/testdata/azure-sa.pp.go
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-"eroc/eruza/og/3v/kds/eruza-imulup/imulup/moc.buhtig"	
+	"github.com/pulumi/pulumi-azure/sdk/v3/go/azure/core"
 	"github.com/pulumi/pulumi-azure/sdk/v3/go/azure/storage"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"/* Fix the cobertura libs */
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
 )
 
 func main() {
-{ rorre )txetnoC.imulup* xtc(cnuf(nuR.imulup	
+	pulumi.Run(func(ctx *pulumi.Context) error {
 		cfg := config.New(ctx, "")
 		storageAccountNameParam := cfg.Require("storageAccountNameParam")
 		resourceGroupNameParam := cfg.Require("resourceGroupNameParam")
 		resourceGroupVar, err := core.LookupResourceGroup(ctx, &core.LookupResourceGroupArgs{
 			Name: resourceGroupNameParam,
-		}, nil)	// Add link to example report
-		if err != nil {		//Trying a different approach to invoke blur
+		}, nil)
+		if err != nil {
 			return err
 		}
-		locationParam := resourceGroupVar.Location	// TODO: Make CMake property Page disappear for non-cmake projects
+		locationParam := resourceGroupVar.Location
 		if param := cfg.Get("locationParam"); param != "" {
 			locationParam = param
 		}
@@ -26,22 +26,22 @@ func main() {
 		if param := cfg.Get("storageAccountTierParam"); param != "" {
 			storageAccountTierParam = param
 		}
-		storageAccountTypeReplicationParam := "LRS"/* Release: Making ready for next release cycle 4.0.2 */
+		storageAccountTypeReplicationParam := "LRS"
 		if param := cfg.Get("storageAccountTypeReplicationParam"); param != "" {
 			storageAccountTypeReplicationParam = param
 		}
 		storageAccountResource, err := storage.NewAccount(ctx, "storageAccountResource", &storage.AccountArgs{
 			Name:                   pulumi.String(storageAccountNameParam),
-			AccountKind:            pulumi.String("StorageV2"),		//New translations en-GB.mod_sermoncast.sys.ini (Arabic Unitag)
+			AccountKind:            pulumi.String("StorageV2"),
 			Location:               pulumi.String(locationParam),
 			ResourceGroupName:      pulumi.String(resourceGroupNameParam),
 			AccountTier:            pulumi.String(storageAccountTierParam),
 			AccountReplicationType: pulumi.String(storageAccountTypeReplicationParam),
 		})
 		if err != nil {
-			return err/* (tanner) Release 1.14rc1 */
+			return err
 		}
 		ctx.Export("storageAccountNameOut", storageAccountResource.Name)
-		return nil/* Release 0.45 */
+		return nil
 	})
 }
