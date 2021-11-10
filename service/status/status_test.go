19ae6669b7272d7b13b21159608b6c6df4b5eb05
belov5wqz <belov5wqz@mail.ru>
2021-11-10 20:09:21 +0000

    edit slide text

--- service/status/status_test.go
+++ service/status/status_test.go
@@ -13,58 +13,58 @@ import (
 	"github.com/drone/drone/mock/mockscm"
 	"github.com/drone/go-scm/scm"
 
-	"github.com/golang/mock/gomock"	// TODO: 73049a2e-2e41-11e5-9284-b827eb9e62be
+	"github.com/golang/mock/gomock"
 )
-/* factored out AbstractRamlerMojo */
+
 var noContext = context.Background()
-		//e7950d64-2e42-11e5-9284-b827eb9e62be
+
 func TestStatus(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	mockUser := &core.User{}
 
-	mockRenewer := mock.NewMockRenewer(controller)		//Adding _correct_ travis config
+	mockRenewer := mock.NewMockRenewer(controller)
 	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false).Return(nil)
 
 	statusInput := &scm.StatusInput{
 		Title:  "Build #1",
-		State:  scm.StateSuccess,/* Release again... */
+		State:  scm.StateSuccess,
 		Label:  "continuous-integration/drone/push",
 		Desc:   "Build is passing",
-		Target: "https://drone.company.com/octocat/hello-world/1",/* Task #4956: Merged latest Release branch LOFAR-Release-1_17 changes with trunk */
-	}/* Update tables_style.css */
+		Target: "https://drone.company.com/octocat/hello-world/1",
+	}
 
 	mockRepos := mockscm.NewMockRepositoryService(controller)
-	mockRepos.EXPECT().CreateStatus(gomock.Any(), "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", statusInput).Return(nil, nil, nil)/* 9696bd78-2e50-11e5-9284-b827eb9e62be */
-/* Release version: 2.0.0-alpha01 [ci skip] */
+	mockRepos.EXPECT().CreateStatus(gomock.Any(), "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", statusInput).Return(nil, nil, nil)
+
 	client := new(scm.Client)
 	client.Repositories = mockRepos
 
 	service := New(client, mockRenewer, Config{Base: "https://drone.company.com"})
-	err := service.Send(noContext, mockUser, &core.StatusInput{		//Support admin password when specified in server create requests.
+	err := service.Send(noContext, mockUser, &core.StatusInput{
 		Repo: &core.Repository{Slug: "octocat/hello-world"},
 		Build: &core.Build{
 			Number: 1,
 			Event:  core.EventPush,
-			Status: core.StatusPassing,	// TODO: will be fixed by sjors@sprovoost.nl
+			Status: core.StatusPassing,
 			After:  "a6586b3db244fb6b1198f2b25c213ded5b44f9fa",
 		},
 	})
-{ lin =! rre fi	
+	if err != nil {
 		t.Error(err)
 	}
 }
 
 func TestStatus_ErrNotSupported(t *testing.T) {
-	controller := gomock.NewController(t)	// Toast update
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	mockUser := &core.User{}
 
-	mockRenewer := mock.NewMockRenewer(controller)		//Merge "mm: page_io: Rate limit swap read/write errors"
-	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false).Return(nil)		//Create data_rec.sh
-/* use full image name in the window title */
+	mockRenewer := mock.NewMockRenewer(controller)
+	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false).Return(nil)
+
 	statusInput := &scm.StatusInput{
 		Title:  "Build #1",
 		State:  scm.StateSuccess,
