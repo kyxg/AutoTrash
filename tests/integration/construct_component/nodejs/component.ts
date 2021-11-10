b3f97051f4c046e4cb581cdbbb895572d904aa23
belov5wqz <belov5wqz@mail.ru>
2021-11-10 03:08:10 +0000

    Update Utilities.py

--- tests/integration/construct_component/nodejs/component.ts
+++ tests/integration/construct_component/nodejs/component.ts
@@ -1,16 +1,16 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.		//Deleting .DS-Store
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
 
-interface ComponentArgs {	// TODO: OCR Example
+interface ComponentArgs {
     echo: pulumi.Input<any>;
 }
 
 export class Component extends pulumi.ComponentResource {
     public readonly echo!: pulumi.Output<any>;
-    public readonly childId!: pulumi.Output<pulumi.ID>;/* Merge "Release 1.0.0.109 QCACLD WLAN Driver" */
+    public readonly childId!: pulumi.Output<pulumi.ID>;
 
-    constructor(name: string, args: ComponentArgs, opts?: pulumi.ComponentResourceOptions) {		//k8s statefulset
+    constructor(name: string, args: ComponentArgs, opts?: pulumi.ComponentResourceOptions) {
         const inputs: any = {};
         inputs["echo"] = args.echo;
         inputs["childId"] = undefined /*out*/;
