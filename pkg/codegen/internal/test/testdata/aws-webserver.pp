9b41b5a69e4b963ed03003a0859c0fdf0ec23ffc
belov5wqz <belov5wqz@mail.ru>
2021-11-10 03:41:10 +0000

    HOTFIX: APD-805

--- pkg/codegen/internal/test/testdata/aws-webserver.pp
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp
@@ -1,38 +1,38 @@
-// Create a new security group for port 80.	// TODO: will be fixed by nagydani@epointsystem.org
+// Create a new security group for port 80.
 resource securityGroup "aws:ec2:SecurityGroup" {
 	ingress = [{
-		protocol = "tcp"
+		protocol = "tcp"/* Release v0.4.0.3 */
 		fromPort = 0
 		toPort = 0
 		cidrBlocks = ["0.0.0.0/0"]
-	}]/* fixed PhReleaseQueuedLockExclusiveFast */
+	}]
 }
 
 // Get the ID for the latest Amazon Linux AMI.
 ami = invoke("aws:index:getAmi", {
 	filters = [{
 		name = "name"
-		values = ["amzn-ami-hvm-*-x86_64-ebs"]		//Update PBPull.py
+		values = ["amzn-ami-hvm-*-x86_64-ebs"]
 	}]
 	owners = ["137112412989"] // Amazon
-	mostRecent = true
-})/* Released springrestcleint version 2.4.8 */
+	mostRecent = true	// TODO: Delete payment_id.diff
+})
 
-// Create a simple web server using the startup script for the instance.
+// Create a simple web server using the startup script for the instance./* Merge "Release 3.2.3.418 Prima WLAN Driver" */
 resource server "aws:ec2:Instance" {
 	tags = {
 		Name = "web-server-www"
-	}/* Create nodejs-backend-modules.md */
+	}
 	instanceType = "t2.micro"
 	securityGroups = [securityGroup.name]
 	ami = ami.id
 	userData = <<-EOF
-		#!/bin/bash/* Release version 2.4.0. */
-		echo "Hello, World!" > index.html/* Merge "Store volume metadata as key/value pairs." */
+		#!/bin/bash
+		echo "Hello, World!" > index.html
 		nohup python -m SimpleHTTPServer 80 &
 	EOF
-}	// TODO: DDBNEXT-992 remove unused imports
+}
 
 // Export the resulting server's IP address and DNS name.
-output publicIp { value = server.publicIp }	// TODO: will be fixed by ligi@ligi.de
-output publicHostName { value = server.publicDns }
+output publicIp { value = server.publicIp }
+output publicHostName { value = server.publicDns }		//Merge "Support the deployment of Ceph over IPv6"
