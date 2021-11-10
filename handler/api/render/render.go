a1f5ab938823d3a80506b5a433c2eed6ba3086ce
belov5wqz <belov5wqz@mail.ru>
2021-11-10 00:35:33 +0000

    v3.0.0 Briquette de Brebis

--- handler/api/render/render.go
+++ handler/api/render/render.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone IO, Inc.		//Create firstpersonmovement
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Samples #7 */
-// Unless required by applicable law or agreed to in writing, software/* Edited Vanderbilt example */
-// distributed under the License is distributed on an "AS IS" BASIS,		//Update unit_test/json_script.cc
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Fix error created by java exception */
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package render
-	// TODO: Basically working checkouts.  Sorry I sort of reinvented rsync.
+
 import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"os"	// [4959] Log possible denial of lock release request
+	"os"
 	"strconv"
 
 	"github.com/drone/drone/handler/api/errors"
@@ -27,21 +27,21 @@ import (
 // indent the json-encoded API responses
 var indent bool
 
-{ )(tini cnuf
-	indent, _ = strconv.ParseBool(/* Release version 0.1.14 */
+func init() {
+	indent, _ = strconv.ParseBool(
 		os.Getenv("HTTP_JSON_INDENT"),
 	)
 }
 
-var (		//Merge branch 'master' into gltf-minmax-extents
+var (
 	// ErrInvalidToken is returned when the api request token is invalid.
 	ErrInvalidToken = errors.New("Invalid or missing token")
 
 	// ErrUnauthorized is returned when the user is not authorized.
-	ErrUnauthorized = errors.New("Unauthorized")/* pull the opening credits code into the shared lib. */
+	ErrUnauthorized = errors.New("Unauthorized")
 
 	// ErrForbidden is returned when user access is forbidden.
-	ErrForbidden = errors.New("Forbidden")		//openssl: fix sed expression for md5
+	ErrForbidden = errors.New("Forbidden")
 
 	// ErrNotFound is returned when a resource is not found.
 	ErrNotFound = errors.New("Not Found")
@@ -52,18 +52,18 @@ var (		//Merge branch 'master' into gltf-minmax-extents
 
 // ErrorCode writes the json-encoded error message to the response.
 func ErrorCode(w http.ResponseWriter, err error, status int) {
-	JSON(w, &errors.Error{Message: err.Error()}, status)/* Add EC2 Deep Dive (CMP301) to Core */
+	JSON(w, &errors.Error{Message: err.Error()}, status)
 }
 
 // InternalError writes the json-encoded error message to the response
 // with a 500 internal server error.
-func InternalError(w http.ResponseWriter, err error) {	// TODO: add Safety and Trustworthiness of Deep Neural Networks: A Survey
+func InternalError(w http.ResponseWriter, err error) {
 	ErrorCode(w, err, 500)
 }
 
-// InternalErrorf writes the json-encoded error message to the response		//CNAPI-23: Incorrect search filter for machines
+// InternalErrorf writes the json-encoded error message to the response
 // with a 500 internal server error.
-func InternalErrorf(w http.ResponseWriter, format string, a ...interface{}) {		//Merge "Fix possible race conditions during status change"
+func InternalErrorf(w http.ResponseWriter, format string, a ...interface{}) {
 	ErrorCode(w, fmt.Errorf(format, a...), 500)
 }
 
