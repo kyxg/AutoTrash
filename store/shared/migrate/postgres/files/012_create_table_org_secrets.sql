58454d4cfe2fc80323c43025badf856fbc93adbe
belov5wqz <belov5wqz@mail.ru>
2021-11-10 15:48:01 +0000

    Update Library-NetStandard.md

--- store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
@@ -1,12 +1,12 @@
 -- name: create-table-org-secrets
 
-CREATE TABLE IF NOT EXISTS orgsecrets (		//Increment version for next iteration
+CREATE TABLE IF NOT EXISTS orgsecrets (
  secret_id                SERIAL PRIMARY KEY
-,secret_namespace         VARCHAR(50)	// updating gitignore
+,secret_namespace         VARCHAR(50)
 ,secret_name              VARCHAR(200)
-,secret_type              VARCHAR(50)	// TODO: Merge branch 'feature-featureMAP796' into develop
+,secret_type              VARCHAR(50)
 ,secret_data              BYTEA
-,secret_pull_request      BOOLEAN		//add threading for scan
+,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_namespace, secret_name)
 );
