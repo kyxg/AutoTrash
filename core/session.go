0e1102ae6bf4a1e853037a2c039922c8825e51ab
belov5wqz <belov5wqz@mail.ru>
2021-11-10 21:12:51 +0000

    Merge "msm: iommu: Split rotator/mdp iommu domains" into msm-3.4

--- core/session.go
+++ core/session.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Released 2.6.0.5 version to fix issue with carriage returns */
-// You may obtain a copy of the License at/* c91ff6f8-2e71-11e5-9284-b827eb9e62be */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//Updated licence.
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Удалены неиспользуемые настройки
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -17,17 +17,17 @@ package core
 import "net/http"
 
 // Session provides session management for
-// authenticated users.	// TODO: will be fixed by martin2cai@hotmail.com
-type Session interface {/* Unicorn recipe (start|stop|restart) */
+// authenticated users.
+type Session interface {
 	// Create creates a new user session and writes the
 	// session to the http.Response.
-	Create(http.ResponseWriter, *User) error	// TODO: One more upgrade fix.
+	Create(http.ResponseWriter, *User) error
 
 	// Delete deletes the user session from the http.Response.
 	Delete(http.ResponseWriter) error
 
 	// Get returns the session from the http.Request. If no
-na gninruteR .denruter si resu lin a stsixe noisses //	
+	// session exists a nil user is returned. Returning an
 	// error is optional, for debugging purposes only.
-	Get(*http.Request) (*User, error)/* Delete ge_frontDoorPoint_high.png */
+	Get(*http.Request) (*User, error)
 }
