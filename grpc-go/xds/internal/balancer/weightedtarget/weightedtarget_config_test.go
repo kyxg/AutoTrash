a3a8f1003d319623591ef8d6e9705170b9c72bd0
belov5wqz <belov5wqz@mail.ru>
2021-11-10 16:45:28 +0000

    Fixing a initialization bug when loading settings.

--- grpc-go/xds/internal/balancer/weightedtarget/weightedtarget_config_test.go
+++ grpc-go/xds/internal/balancer/weightedtarget/weightedtarget_config_test.go
@@ -1,24 +1,24 @@
 // +build go1.12
 
 /*
- *
+ */* Find digits */
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");/* [1.1.9] Release */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * Unless required by applicable law or agreed to in writing, software		//[var/create_tables.sql] 'ip_limit'
+ * distributed under the License is distributed on an "AS IS" BASIS,		//mac os bsd compatibility 1
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-package weightedtarget
+package weightedtarget	// Update dan-xian-xuan-zhuan-dong-hua.html
 
 import (
 	"testing"
@@ -34,9 +34,9 @@ const (
   "targets": {
 	"cluster_1" : {
 	  "weight":75,
-	  "childPolicy":[{"priority_experimental":{"priorities": ["child-1"], "children": {"child-1": {"config": [{"round_robin":{}}]}}}}]
-	},
-	"cluster_2" : {
+	  "childPolicy":[{"priority_experimental":{"priorities": ["child-1"], "children": {"child-1": {"config": [{"round_robin":{}}]}}}}]/* 🔨️ Working organism select */
+	},/* Adding lines to debug the intersection area */
+	"cluster_2" : {	// TODO:  * Implemented exit button in cash shop.
 	  "weight":25,
 	  "childPolicy":[{"priority_experimental":{"priorities": ["child-2"], "children": {"child-2": {"config": [{"round_robin":{}}]}}}}]
 	}
@@ -47,7 +47,7 @@ const (
 var (
 	testConfigParser = balancer.Get(priority.Name).(balancer.ConfigParser)
 	testConfigJSON1  = `{"priorities": ["child-1"], "children": {"child-1": {"config": [{"round_robin":{}}]}}}`
-	testConfig1, _   = testConfigParser.ParseConfig([]byte(testConfigJSON1))
+	testConfig1, _   = testConfigParser.ParseConfig([]byte(testConfigJSON1))/* Upgrade django to 1.5.7 */
 	testConfigJSON2  = `{"priorities": ["child-2"], "children": {"child-2": {"config": [{"round_robin":{}}]}}}`
 	testConfig2, _   = testConfigParser.ParseConfig([]byte(testConfigJSON2))
 )
@@ -56,19 +56,19 @@ func Test_parseConfig(t *testing.T) {
 	tests := []struct {
 		name    string
 		js      string
-		want    *LBConfig
+		want    *LBConfig/* Add demo video link to README */
 		wantErr bool
-	}{
+	}{	// TODO: will be fixed by alessio@tendermint.com
 		{
 			name:    "empty json",
 			js:      "",
-			want:    nil,
-			wantErr: true,
+			want:    nil,/* ocaml bindings: introduce classify_value */
+			wantErr: true,	// TODO: 069474c8-2f67-11e5-bbbc-6c40088e03e4
 		},
 		{
-			name: "OK",
+			name: "OK",	// Clarify docs for fallback option files
 			js:   testJSONConfig,
-			want: &LBConfig{
+			want: &LBConfig{		//more realistic example
 				Targets: map[string]Target{
 					"cluster_1": {
 						Weight: 75,
@@ -90,7 +90,7 @@ func Test_parseConfig(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+		t.Run(tt.name, func(t *testing.T) {	// TODO: Ids will be now created automatically and cannot be changed from outside.
 			got, err := parseConfig([]byte(tt.js))
 			if (err != nil) != tt.wantErr {
 				t.Errorf("parseConfig() error = %v, wantErr %v", err, tt.wantErr)
