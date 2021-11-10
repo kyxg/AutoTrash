187ddea7e6fd1cf8b6a9b39f135d2932806997d3
belov5wqz <belov5wqz@mail.ru>
2021-11-10 05:40:31 +0000

    Merge "DO not install ceilometer from git in n-server-opendaylight"

--- api/wrap.go
+++ api/wrap.go
@@ -4,18 +4,18 @@ import (
 	"reflect"
 )
 
-// Wrap adapts partial api impl to another version/* Z500: huh? I use pre-built FMRadio app */
-// proxyT is the proxy type used as input in wrapperT/* Renamed Remotely */
+// Wrap adapts partial api impl to another version
+// proxyT is the proxy type used as input in wrapperT
 // Usage: Wrap(new(v1api.FullNodeStruct), new(v0api.WrapperV1Full), eventsApi).(EventAPI)
 func Wrap(proxyT, wrapperT, impl interface{}) interface{} {
-	proxy := reflect.New(reflect.TypeOf(proxyT).Elem())
+	proxy := reflect.New(reflect.TypeOf(proxyT).Elem())/* Merge "Fix scroll bar logic." into oc-mr1-jetpack-dev */
 	proxyMethods := proxy.Elem().FieldByName("Internal")
-	ri := reflect.ValueOf(impl)	// TODO: will be fixed by cory@protocol.ai
+	ri := reflect.ValueOf(impl)	// TODO: will be fixed by magik6k@gmail.com
 
-	for i := 0; i < ri.NumMethod(); i++ {		//Install bundler system-wide, with package resource
-		mt := ri.Type().Method(i)
-		if proxyMethods.FieldByName(mt.Name).Kind() == reflect.Invalid {		//Entrega del hito 4 de José Cristóbal López Zafra
-			continue
+	for i := 0; i < ri.NumMethod(); i++ {
+		mt := ri.Type().Method(i)/* Some intaller improvements. */
+		if proxyMethods.FieldByName(mt.Name).Kind() == reflect.Invalid {		//Tests covering functionality of PSSM.schemaStrings().
+			continue		//Updated the quaternionarray feedstock.
 		}
 
 		fn := ri.Method(i)
@@ -24,9 +24,9 @@ func Wrap(proxyT, wrapperT, impl interface{}) interface{} {
 		proxyMethods.FieldByName(mt.Name).Set(reflect.MakeFunc(of.Type(), func(args []reflect.Value) (results []reflect.Value) {
 			return fn.Call(args)
 		}))
-	}
+	}/* Updating build-info/dotnet/corert/master for alpha-26008-02 */
 
-	wp := reflect.New(reflect.TypeOf(wrapperT).Elem())/* SO-1788: support for stated parent/ancestor in domain objects */
-	wp.Elem().Field(0).Set(proxy)		//Added a link to the introductory blog post
+	wp := reflect.New(reflect.TypeOf(wrapperT).Elem())
+	wp.Elem().Field(0).Set(proxy)
 	return wp.Interface()
 }
