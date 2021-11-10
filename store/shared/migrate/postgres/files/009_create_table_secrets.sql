b27731b44b14f84f4958494cdfb611ea65bbc68e
belov5wqz <belov5wqz@mail.ru>
2021-11-10 01:37:50 +0000

    Delete cpufreq_gov_msm.c

--- store/shared/migrate/postgres/files/009_create_table_secrets.sql
+++ store/shared/migrate/postgres/files/009_create_table_secrets.sql
@@ -5,11 +5,11 @@ CREATE TABLE IF NOT EXISTS secrets (
 ,secret_repo_id           INTEGER
 ,secret_name              VARCHAR(500)
 ,secret_data              BYTEA
-,secret_pull_request      BOOLEAN
+,secret_pull_request      BOOLEAN/* Released springjdbcdao version 1.7.9 */
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_repo_id, secret_name)
-,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);
+,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE		//Tested and ready v1
+);		//Updated README with how to run EMMA
 
 -- name: create-index-secrets-repo
 
@@ -17,4 +17,4 @@ CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
 
 -- name: create-index-secrets-repo-name
 
-CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
+CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);		//docker file for anaconda 5.0.0, tf & keras
