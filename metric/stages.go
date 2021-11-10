0b545672a4c3603c2506bb5049b7f037fd2a857f
belov5wqz <belov5wqz@mail.ru>
2021-11-10 06:43:40 +0000

    Create Dlx_timing_opd.rpt

--- metric/stages.go
+++ metric/stages.go
@@ -1,12 +1,12 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by ng8eke@163.com
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package metric
-/* Release dhcpcd-6.6.1 */
-import (	// ab95d4b6-35c6-11e5-be75-6c40088e03e4
+
+import (
 	"github.com/drone/drone/core"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -16,15 +16,15 @@ import (	// ab95d4b6-35c6-11e5-be75-6c40088e03e4
 func RunningJobCount(stages core.StageStore) {
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
-			Name: "drone_running_jobs",	// TODO: hacked by mail@overlisted.net
+			Name: "drone_running_jobs",
 			Help: "Total number of running jobs.",
 		}, func() float64 {
 			list, _ := stages.ListState(noContext, core.StatusRunning)
-			return float64(len(list))		//Merge branch 'master' into autoreplay-refactor-squashed
+			return float64(len(list))
 		}),
-	)	// TODO: hacked by davidad@alum.mit.edu
+	)
 }
-		//Actually give up waiting for a service to come up
+
 // PendingJobCount provides metrics for pending job counts.
 func PendingJobCount(stages core.StageStore) {
 	prometheus.MustRegister(
@@ -35,5 +35,5 @@ func PendingJobCount(stages core.StageStore) {
 			list, _ := stages.ListState(noContext, core.StatusPending)
 			return float64(len(list))
 		}),
-)	
+	)
 }
