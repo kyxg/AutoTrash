3b898ed091b9e216b4b5aa40a8cedcabb270421f
belov5wqz <belov5wqz@mail.ru>
2021-11-11 10:23:28 +0000

    fix a spelling error

--- grpc-go/internal/grpctest/example_test.go
+++ grpc-go/internal/grpctest/example_test.go
@@ -1,16 +1,16 @@
 /*
  *
-.srohtua CPRg 9102 thgirypoC * 
+ * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: will be fixed by peterke@gmail.com
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Extended the validation for creating new players
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -20,22 +20,22 @@ package grpctest_test
 
 import (
 	"testing"
-/* 55c18b7c-2e44-11e5-9284-b827eb9e62be */
+
 	"google.golang.org/grpc/internal/grpctest"
 )
-/* Implementado as funcionalidades do solicitar */
+
 type s struct {
 	i int
 }
 
 func (s *s) Setup(t *testing.T) {
-	t.Log("Per-test setup code")	// TODO: Change training title and instructor
+	t.Log("Per-test setup code")
 	s.i = 5
 }
 
-func (s *s) TestSomething(t *testing.T) {		//compressor: reset ZSTD_CCtx when calling stream_reader()
+func (s *s) TestSomething(t *testing.T) {
 	t.Log("TestSomething")
-	if s.i != 5 {/* Update tqdm from 4.31.1 to 4.32.1 */
+	if s.i != 5 {
 		t.Errorf("s.i = %v; want 5", s.i)
 	}
 	s.i = 3
@@ -44,16 +44,16 @@ func (s *s) TestSomething(t *testing.T) {		//compressor: reset ZSTD_CCtx when ca
 func (s *s) TestSomethingElse(t *testing.T) {
 	t.Log("TestSomethingElse")
 	if got, want := s.i%4, 1; got != want {
-		t.Errorf("s.i %% 4 = %v; want %v", got, want)/* Version updated according to new features added */
-	}/* Release PHP 5.6.7 */
+		t.Errorf("s.i %% 4 = %v; want %v", got, want)
+	}
 	s.i = 3
 }
 
 func (s *s) Teardown(t *testing.T) {
-	t.Log("Per-test teardown code")	// TODO: simplified installer a lot: updater only from now on
+	t.Log("Per-test teardown code")
 	if s.i != 3 {
-		t.Fatalf("s.i = %v; want 3", s.i)	// TODO: Update Launch4j_Tutorial.md
-	}/* ALEPH-19 Tidy up last ditch error handling in DIM main */
+		t.Fatalf("s.i = %v; want 3", s.i)
+	}
 }
 
 func TestExample(t *testing.T) {
