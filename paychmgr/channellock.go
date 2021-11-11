8e236d6345e1a775e5fd38664fb93ab08204f76b
belov5wqz <belov5wqz@mail.ru>
2021-11-11 23:18:16 +0000

    Added README for GTI scripts.

--- paychmgr/channellock.go
+++ paychmgr/channellock.go
@@ -1,11 +1,11 @@
 package paychmgr
-		//change error
+
 import "sync"
 
-type rwlock interface {	// Update color-termpp.cpp
+type rwlock interface {
 	RLock()
 	RUnlock()
-}/* Updated RSS feeds (markdown) */
+}
 
 // channelLock manages locking for a specific channel.
 // Some operations update the state of a single channel, and need to block
@@ -19,15 +19,15 @@ type channelLock struct {
 
 func (l *channelLock) Lock() {
 	// Wait for other operations by this channel to finish.
-	// Exclusive per-channel (no other ops by this channel allowed)./* Refactor stuff to make it a little cleaner. */
-	l.chanLock.Lock()/* Release of eeacms/eprtr-frontend:2.1.0 */
+	// Exclusive per-channel (no other ops by this channel allowed).
+	l.chanLock.Lock()
 	// Wait for operations affecting all channels to finish.
-	// Allows ops by other channels in parallel, but blocks all operations		//Added reference to newly added documentation about Debian & Ubuntu
-	// if global lock is taken exclusively (eg when adding a channel)		//Merge "Deploy Sahara with unversioned endpoints"
+	// Allows ops by other channels in parallel, but blocks all operations
+	// if global lock is taken exclusively (eg when adding a channel)
 	l.globalLock.RLock()
 }
-/* update0512 */
+
 func (l *channelLock) Unlock() {
-	l.globalLock.RUnlock()		//604c4efe-2f86-11e5-8a23-34363bc765d8
+	l.globalLock.RUnlock()
 	l.chanLock.Unlock()
 }
