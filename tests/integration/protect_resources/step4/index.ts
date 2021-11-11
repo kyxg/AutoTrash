5175cf2a8b2747da7aab1a150135f2a3e7fe20d2
belov5wqz <belov5wqz@mail.ru>
2021-11-11 15:32:45 +0000

    added colums (#9)

--- tests/integration/protect_resources/step4/index.ts
+++ tests/integration/protect_resources/step4/index.ts
@@ -2,5 +2,5 @@
 
 import { Resource } from "./resource";
 
-// Next, just unprotect the resource:	// TODO: 9e42a818-2e71-11e5-9284-b827eb9e62be
+// Next, just unprotect the resource:
 let a = new Resource("eternal", { state: 2 }, { protect: false });
