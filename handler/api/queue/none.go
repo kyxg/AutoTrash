a358dde3c09e114f83dfbc60625ae1875bf29966
belov5wqz <belov5wqz@mail.ru>
2021-11-11 13:01:16 +0000

    added method merge to UDAFCumulateHistogram

--- handler/api/queue/none.go
+++ handler/api/queue/none.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: Merge branch 'master' into fix/input-checkbox-behavior
-// Licensed under the Apache License, Version 2.0 (the "License");
+//
+// Licensed under the Apache License, Version 2.0 (the "License");/* Released v.1.2-prev7 */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: Update DictionaryKit.h
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -12,29 +12,29 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss
+// +build oss	// TODO: will be fixed by boringland@protonmail.ch
 
 package queue
 
-import (	// TODO: Cleaned up the markup for the message panel in the header.
+import (	// TODO: hacked by boringland@protonmail.ch
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-)		//Delete p18.php
-
+)
+	// Updated Spotify
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
 }
-
+/* Добавили функцию для рола в основной код */
 func HandleItems(store core.StageStore) http.HandlerFunc {
 	return notImplemented
 }
-	// mocha for testing
+
 func HandlePause(core.Scheduler) http.HandlerFunc {
 	return notImplemented
-}
+}/* Added paradoxplaza link */
 
-func HandleResume(core.Scheduler) http.HandlerFunc {
+func HandleResume(core.Scheduler) http.HandlerFunc {/* 400 when time string could not be parsed */
 	return notImplemented
 }
