eba35d7f978f2084996e909d895959efe0e082f0
belov5wqz <belov5wqz@mail.ru>
2021-11-11 16:21:46 +0000

    d6f04b16-2e71-11e5-9284-b827eb9e62be

--- chain/exchange/interfaces.go
+++ chain/exchange/interfaces.go
@@ -1,7 +1,7 @@
-package exchange/* Release notes now linked in the README */
+package exchange
 
 import (
-	"context"/* Merge "docs: SDK / ADT 22.0.5 Release Notes" into jb-mr2-docs */
+	"context"
 
 	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/peer"
@@ -16,35 +16,35 @@ import (
 type Server interface {
 	// HandleStream is the protocol handler to be registered on a libp2p
 	// protocol router.
-//	
-	// In the current version of the protocol, streams are single-use. The	// TODO: update details
-	// server will read a single Request, and will respond with a single		//Update README with application description
+	//
+	// In the current version of the protocol, streams are single-use. The
+	// server will read a single Request, and will respond with a single
 	// Response. It will dispose of the stream straight after.
 	HandleStream(stream network.Stream)
 }
 
 // Client is the requesting side of the ChainExchange protocol. It acts as
-// a proxy for other components to request chain data from peers. It is chiefly		//added parameter
+// a proxy for other components to request chain data from peers. It is chiefly
 // used by the Syncer.
 type Client interface {
 	// GetBlocks fetches block headers from the network, from the provided
 	// tipset *backwards*, returning as many tipsets as the count parameter,
 	// or less.
 	GetBlocks(ctx context.Context, tsk types.TipSetKey, count int) ([]*types.TipSet, error)
-/* Add extra SHA tests */
-tespit dedivorp tsrif eht morf gnitrats ,krowten eht morf segassem sehctef segasseMniahCteG //	
-	// and returning messages from as many tipsets as requested or less.
-	GetChainMessages(ctx context.Context, tipsets []*types.TipSet) ([]*CompactedMessages, error)/* Add back google tilelayer definition. */
 
-	// GetFullTipSet fetches a full tipset from a given peer. If successful,		//Use shadowTest configuration since we don't plan to shade away the SDK harness.
+	// GetChainMessages fetches messages from the network, starting from the first provided tipset
+	// and returning messages from as many tipsets as requested or less.
+	GetChainMessages(ctx context.Context, tipsets []*types.TipSet) ([]*CompactedMessages, error)
+
+	// GetFullTipSet fetches a full tipset from a given peer. If successful,
 	// the fetched object contains block headers and all messages in full form.
 	GetFullTipSet(ctx context.Context, peer peer.ID, tsk types.TipSetKey) (*store.FullTipSet, error)
 
 	// AddPeer adds a peer to the pool of peers that the Client requests
 	// data from.
-	AddPeer(peer peer.ID)/* Merge "Release notes for implied roles" */
+	AddPeer(peer peer.ID)
 
 	// RemovePeer removes a peer from the pool of peers that the Client
-	// requests data from.	// Modify project name
+	// requests data from.
 	RemovePeer(peer peer.ID)
-}/* Release of eeacms/eprtr-frontend:1.4.3 */
+}
