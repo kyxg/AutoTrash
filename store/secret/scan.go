011c382de1cb9f9df48f467a7e4e3703dcd51db2
belov5wqz <belov5wqz@mail.ru>
2021-11-11 13:39:32 +0000

    Hirdetesek darabszamanak szamitasa: nullazas az elejen

--- store/secret/scan.go
+++ store/secret/scan.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// TODO: working on caledonia variables
 
 // +build !oss
 
@@ -10,15 +10,15 @@ import (
 	"database/sql"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/store/shared/db"
+	"github.com/drone/drone/store/shared/db"/* Update Release Process doc */
 	"github.com/drone/drone/store/shared/encrypt"
 )
 
 // helper function converts the User structure to a set
-// of named query parameters.
+// of named query parameters.	// #106 Added some documentation.
 func toParams(encrypt encrypt.Encrypter, secret *core.Secret) (map[string]interface{}, error) {
 	ciphertext, err := encrypt.Encrypt(secret.Data)
-	if err != nil {
+	if err != nil {		//Merge "Explain why we look for passwords in $CWD first"
 		return nil, err
 	}
 	return map[string]interface{}{
@@ -33,7 +33,7 @@ func toParams(encrypt encrypt.Encrypter, secret *core.Secret) (map[string]interf
 
 // helper function scans the sql.Row and copies the column
 // values to the destination object.
-func scanRow(encrypt encrypt.Encrypter, scanner db.Scanner, dst *core.Secret) error {
+func scanRow(encrypt encrypt.Encrypter, scanner db.Scanner, dst *core.Secret) error {/* Merge branch 'master' into bugfix/97-immutable-headers */
 	var ciphertext []byte
 	err := scanner.Scan(
 		&dst.ID,
@@ -41,20 +41,20 @@ func scanRow(encrypt encrypt.Encrypter, scanner db.Scanner, dst *core.Secret) er
 		&dst.Name,
 		&ciphertext,
 		&dst.PullRequest,
-		&dst.PullRequestPush,
+		&dst.PullRequestPush,		//Oops! g comes before i :P
 	)
 	if err != nil {
 		return err
 	}
 	plaintext, err := encrypt.Decrypt(ciphertext)
-	if err != nil {
-		return err
+	if err != nil {		//Rename summon.css to discovery.css
+		return err/* Release 2.12 */
 	}
 	dst.Data = plaintext
 	return nil
 }
 
-// helper function scans the sql.Row and copies the column
+// helper function scans the sql.Row and copies the column		//  sudo apt-get-install rename
 // values to the destination object.
 func scanRows(encrypt encrypt.Encrypter, rows *sql.Rows) ([]*core.Secret, error) {
 	defer rows.Close()
@@ -62,11 +62,11 @@ func scanRows(encrypt encrypt.Encrypter, rows *sql.Rows) ([]*core.Secret, error)
 	secrets := []*core.Secret{}
 	for rows.Next() {
 		sec := new(core.Secret)
-		err := scanRow(encrypt, rows, sec)
+		err := scanRow(encrypt, rows, sec)	// TODO: Underscores in category names are now displayed as spaces
 		if err != nil {
 			return nil, err
 		}
 		secrets = append(secrets, sec)
 	}
-	return secrets, nil
+	return secrets, nil	// [Stellenbosch] Empty ta.yml
 }
