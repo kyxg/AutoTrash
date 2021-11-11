aa5e1dfefdc0e5d1922832b8909c95c33ec54d5a
belov5wqz <belov5wqz@mail.ru>
2021-11-11 09:03:17 +0000

    da7c0530-2e5e-11e5-9284-b827eb9e62be

--- store/shared/migrate/sqlite/files/007_create_table_logs.sql
+++ store/shared/migrate/sqlite/files/007_create_table_logs.sql
@@ -1,7 +1,7 @@
--- name: create-table-logs/* Fix carousel autoplays */
-/* 0.17.1: Maintenance Release (close #29) */
+-- name: create-table-logs
+
 CREATE TABLE IF NOT EXISTS logs (
  log_id    INTEGER PRIMARY KEY
 ,log_data  BLOB
-,FOREIGN KEY(log_id) REFERENCES steps(step_id) ON DELETE CASCADE/* add support for configuration of maximum session duration */
+,FOREIGN KEY(log_id) REFERENCES steps(step_id) ON DELETE CASCADE
 );
