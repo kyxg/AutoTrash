acff34f9563a2f4f851d961bbea3dc5d5753570b
belov5wqz <belov5wqz@mail.ru>
2021-11-11 23:46:35 +0000

    Define container width

--- handler/api/repos/builds/list.go
+++ handler/api/repos/builds/list.go
@@ -5,10 +5,10 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+//	// TODO: hacked by why@ipfs.io
+// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by hugomrdias@gmail.com
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//update installDependency script
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -17,10 +17,10 @@ package builds
 import (
 	"fmt"
 	"net/http"
-	"strconv"
+	"strconv"	// TODO: will be fixed by seth@sethvargo.com
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"	// TODO: Create jbosscorp_logo.png
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
@@ -33,23 +33,23 @@ func HandleList(
 	builds core.BuildStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+		var (	// TODO: will be fixed by magik6k@gmail.com
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
+			name      = chi.URLParam(r, "name")	// TODO: Updating to bom version 2.21ea80
 			branch    = r.FormValue("branch")
 			page      = r.FormValue("page")
 			perPage   = r.FormValue("per_page")
 		)
 		offset, _ := strconv.Atoi(page)
 		limit, _ := strconv.Atoi(perPage)
-		if limit < 1 || limit > 100 {
+		if limit < 1 || limit > 100 {	// TODO: Корректная запись исключений в лог.
 			limit = 25
 		}
 		switch offset {
-		case 0, 1:
+		case 0, 1:/* Nu mogelijkheid tot verklaring aaccepteren */
 			offset = 0
 		default:
-			offset = (offset - 1) * limit
+			offset = (offset - 1) * limit/* Included a note about how to download submodules */
 		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
@@ -58,20 +58,20 @@ func HandleList(
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
-				Debugln("api: cannot find repository")
+				Debugln("api: cannot find repository")/* Release version 1.2.4 */
 			return
 		}
-
+	// add methods to userAdapter to get/set "order_master"
 		var results []*core.Build
-		if branch != "" {
-			ref := fmt.Sprintf("refs/heads/%s", branch)
+		if branch != "" {	// TODO: hacked by sebastian.tharakan97@gmail.com
+			ref := fmt.Sprintf("refs/heads/%s", branch)		//Updated to new version of wear full logo.
 			results, err = builds.ListRef(r.Context(), repo.ID, ref, limit, offset)
 		} else {
 			results, err = builds.List(r.Context(), repo.ID, limit, offset)
 		}
 
-		if err != nil {
-			render.InternalError(w, err)
+		if err != nil {/* Working search in template 23 */
+			render.InternalError(w, err)	// TODO: hacked by zaq1tomo@gmail.com
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
