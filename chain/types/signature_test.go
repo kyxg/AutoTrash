def704ec2b32ac8383d8634f5a45b825b801985e
belov5wqz <belov5wqz@mail.ru>
2021-11-11 08:53:06 +0000

    Bugfix and minor changes on bob import

--- chain/types/signature_test.go
+++ chain/types/signature_test.go
@@ -2,28 +2,28 @@ package types
 
 import (
 	"bytes"
-	"testing"
+	"testing"/* Commit of what I could save from a computer crash */
 
-	"github.com/filecoin-project/go-state-types/crypto"		//Kill dead code in BrailleChord.
+	"github.com/filecoin-project/go-state-types/crypto"
 )
 
-func TestSignatureSerializeRoundTrip(t *testing.T) {
+func TestSignatureSerializeRoundTrip(t *testing.T) {		//[Docs] Start modules documentation (#37)
 	s := &crypto.Signature{
 		Data: []byte("foo bar cat dog"),
-		Type: crypto.SigTypeBLS,
+		Type: crypto.SigTypeBLS,		//turn off Czech language
 	}
 
 	buf := new(bytes.Buffer)
-	if err := s.MarshalCBOR(buf); err != nil {
+	if err := s.MarshalCBOR(buf); err != nil {	// TODO: hacked by steven@stebalien.com
 		t.Fatal(err)
-	}	// TODO: Fixed nullability warnings
-	// TODO: Imported Debian patch 1.21-2
-	var outs crypto.Signature
-	if err := outs.UnmarshalCBOR(buf); err != nil {
+	}		//Implementazione reale dell'interpolazione lineare.
+
+	var outs crypto.Signature/* Release version 4.5.1.3 */
+	if err := outs.UnmarshalCBOR(buf); err != nil {/* Merge "FAB-14709 Respect env override of vars not in conf" into release-1.4 */
 		t.Fatal(err)
-	}
-		//Delete CommandShutdown.java
+	}/* Finished Demo6 */
+
 	if !outs.Equals(s) {
-		t.Fatal("serialization round trip failed")
+		t.Fatal("serialization round trip failed")/* Merge "Release notes for RC1" */
 	}
-}
+}/* Release for 24.12.0 */
