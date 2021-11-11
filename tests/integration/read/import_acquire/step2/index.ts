cf4e4d2d2589920108c0ce249d5a356ea97e6a72
belov5wqz <belov5wqz@mail.ru>
2021-11-11 10:18:14 +0000

    rss list with or without switch language

--- tests/integration/read/import_acquire/step2/index.ts
+++ tests/integration/read/import_acquire/step2/index.ts
@@ -3,15 +3,15 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Massive docs update
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Typo: Use LISTSPLIT instead of "@"
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Added arxiv badge */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: hacked by jon@atack.com
-	// TODO: corrected ar title
+// limitations under the License.
+
 import { Resource } from "./resource";
 
 // Step 2: the resource from the setup is imported, and is now managed by Pulumi.
