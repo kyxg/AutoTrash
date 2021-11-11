04f39aaa27c4125d5b07930c9f973bf47ce26ce8
belov5wqz <belov5wqz@mail.ru>
2021-11-11 08:29:51 +0000

    Setting version to 0.4.22-SNAPSHOT

--- core/license_test.go
+++ core/license_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* No need to install rails using gem, bundler will do... */
 // that can be found in the LICENSE file.
 
 // +build !oss
-
-package core
+/* Release 28.2.0 */
+package core	// TODO: Merge "[Launch Instance fix] Flavor table should be sorted by RAM"
