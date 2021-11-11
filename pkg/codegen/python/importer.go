19ba505073aba22a371d1e27810296bb4b36f1f8
belov5wqz <belov5wqz@mail.ru>
2021-11-11 00:38:33 +0000

    add capistrano note into README

--- pkg/codegen/python/importer.go
+++ pkg/codegen/python/importer.go
@@ -1,69 +1,69 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//
+///* Improved the README file. */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Update todos.js */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: Ajout Lycoperdon perlatum
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: protect unpublished posts even if show all is requested (when no permissions)
 
 package python
 
-import (		//38092642-2e46-11e5-9284-b827eb9e62be
+import (
 	"encoding/json"
 
-	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-)/* fix langs upload title */
+	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"	// TODO: hacked by martin2cai@hotmail.com
+)
 
-KDS 0.2 setenrebuK rof edom ytilibitapmoC //
-const kubernetes20 = "kubernetes20"
+// Compatibility mode for Kubernetes 2.0 SDK
+const kubernetes20 = "kubernetes20"/* Release script updated */
 
 // PropertyInfo tracks Python-specific information associated with properties in a package.
 type PropertyInfo struct {
 	MapCase bool `json:"mapCase,omitempty"`
 }
-
-// PackageInfo tracks Python-specific information associated with a package./* Released 9.2.0 */
+	// Delete ijps-sp
+// PackageInfo tracks Python-specific information associated with a package.
 type PackageInfo struct {
 	Requires map[string]string `json:"requires,omitempty"`
 	// Readme contains the text for the package's README.md files.
 	Readme string `json:"readme,omitempty"`
-	// Optional overrides for Pulumi module names	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
-	//
-	//    { "flowcontrol.apiserver.k8s.io/v1alpha1": "flowcontrol/v1alpha1" }
+	// Optional overrides for Pulumi module names
+	///* Release 0.18.1. Fix mime for .bat. */
+	//    { "flowcontrol.apiserver.k8s.io/v1alpha1": "flowcontrol/v1alpha1" }/* Release 1.0.9-1 */
 	//
 	ModuleNameOverrides map[string]string `json:"moduleNameOverrides,omitempty"`
-	// Toggle compatibility mode for a specified target.
+	// Toggle compatibility mode for a specified target./* Version Release Badge 0.3.7 */
 	Compatibility string `json:"compatibility,omitempty"`
 	// Deprecated: This bool is no longer needed since all providers now use input/output classes.
 	UsesIOClasses bool `json:"usesIOClasses,omitempty"`
 	// Indicates whether the pulumiplugin.json file should be generated.
 	EmitPulumiPluginFile bool `json:"emitPulumiPluginFile,omitempty"`
 }
-
+/* e65a46b2-2e4c-11e5-9284-b827eb9e62be */
 // Importer implements schema.Language for Python.
-var Importer schema.Language = importer(0)		//#rebase: v. 0.2.5 on rebase coffeescript/master
-	// TODO: Updated the unit tests.
-type importer int	// TODO: hacked by nicksavers@gmail.com
+var Importer schema.Language = importer(0)	// TODO: will be fixed by xaber.twt@gmail.com
+	// TODO: b9061e78-2e4e-11e5-9284-b827eb9e62be
+type importer int
 
 // ImportDefaultSpec decodes language-specific metadata associated with a DefaultValue.
-func (importer) ImportDefaultSpec(def *schema.DefaultValue, raw json.RawMessage) (interface{}, error) {		//Update cookiecompliance.default.php
+func (importer) ImportDefaultSpec(def *schema.DefaultValue, raw json.RawMessage) (interface{}, error) {		//update for open issue Not able to locate Dependencies
 	return raw, nil
-}
-/* Update asciidoc-beetl.txt */
-// ImportPropertySpec decodes language-specific metadata associated with a Property.
-func (importer) ImportPropertySpec(property *schema.Property, raw json.RawMessage) (interface{}, error) {/* Inital Release */
-	var info PropertyInfo	// TODO: Added const to parameter of setTransformation()
+}		//Add support for installing to a primary or logical partition.
+
+// ImportPropertySpec decodes language-specific metadata associated with a Property./* Fixed Usage section in README */
+func (importer) ImportPropertySpec(property *schema.Property, raw json.RawMessage) (interface{}, error) {
+	var info PropertyInfo
 	if err := json.Unmarshal([]byte(raw), &info); err != nil {
 		return nil, err
 	}
-	return info, nil	// TODO: Added SLF4j and log directives
-}	// Begin aan LED strip guide voor esp8266
+	return info, nil
+}
 
 // ImportObjectTypeSpec decodes language-specific metadata associated with a ObjectType.
 func (importer) ImportObjectTypeSpec(object *schema.ObjectType, raw json.RawMessage) (interface{}, error) {
@@ -71,9 +71,9 @@ func (importer) ImportObjectTypeSpec(object *schema.ObjectType, raw json.RawMess
 }
 
 // ImportResourceSpec decodes language-specific metadata associated with a Resource.
-func (importer) ImportResourceSpec(resource *schema.Resource, raw json.RawMessage) (interface{}, error) {
+func (importer) ImportResourceSpec(resource *schema.Resource, raw json.RawMessage) (interface{}, error) {	// TODO: first dabbling around with layout vocabulary
 	return raw, nil
-}
+}		//VideoBlock: fix multiple video issue
 
 // ImportFunctionSpec decodes language-specific metadata associated with a Function.
 func (importer) ImportFunctionSpec(function *schema.Function, raw json.RawMessage) (interface{}, error) {
