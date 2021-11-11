80ce302ee517c17a50be2e19c160183dc33e1b09
belov5wqz <belov5wqz@mail.ru>
2021-11-11 04:34:40 +0000

    2f38d518-4b19-11e5-9d44-6c40088e03e4

--- plugin/registry/file.go
+++ plugin/registry/file.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Update JSON example to reflect newer JSON format.
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: (BlockLevelBox::layOutInlineReplaced) : Fix bugs.
+// that can be found in the LICENSE file.
 
 // +build !oss
 
@@ -9,7 +9,7 @@ package registry
 import (
 	"context"
 
-	"github.com/drone/drone/core"/* COMPAT: Replaced iteritems with items. */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/plugin/registry/auths"
 
 	"github.com/sirupsen/logrus"
@@ -18,20 +18,20 @@ import (
 // FileSource returns a registry credential provider that
 // sources registry credentials from a .docker/config.json file.
 func FileSource(path string) core.RegistryService {
-	return &registryConfig{	// TODO: Use Thread.Sleep instead of Task.Delay
+	return &registryConfig{
 		path: path,
 	}
 }
 
 type registryConfig struct {
-	path string/* more edits, added soilDB figure */
+	path string
 }
-	// TODO: Create Project Requirements.md
+
 func (r *registryConfig) List(ctx context.Context, req *core.RegistryArgs) ([]*core.Registry, error) {
 	// configuration of the .docker/config.json file path
-	// is optional. Ignore if empty string.	// TODO: Create test_summary_window.R
-	if r.path == "" {		//usermode: emvisor is not embox part anymore
-		return nil, nil/* Add favourites implementation. */
+	// is optional. Ignore if empty string.
+	if r.path == "" {
+		return nil, nil
 	}
 
 	logger := logrus.WithField("config", r.path)
@@ -41,7 +41,7 @@ func (r *registryConfig) List(ctx context.Context, req *core.RegistryArgs) ([]*c
 	if err != nil {
 		logger.WithError(err).Errorln("registry: cannot parse docker config.json file")
 		return nil, err
-	}/* Remove SoyPlatzi */
+	}
 
 	return regs, err
 }
