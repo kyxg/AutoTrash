2ff348bb3f7354dd5f2105be60ab07c0cec6d57f
belov5wqz <belov5wqz@mail.ru>
2021-11-11 02:38:21 +0000

    more work on tidying up ag files

--- tests/containers/nodejs/index.js
+++ tests/containers/nodejs/index.js
@@ -1,4 +1,4 @@
-"use strict";
+"use strict";/* Fix `cloudsight config` command *facepalm* */
 const pulumi = require("@pulumi/pulumi");
-const config = new pulumi.Config();
+const config = new pulumi.Config();	// TODO: NetKAN generated mods - GrannusExpansionPack-1.1.2
 console.log("Hello from", config.require("runtime"));
