90cc100945e0d6f417e7f509936d4b9ab7a732db
belov5wqz <belov5wqz@mail.ru>
2021-11-11 19:25:46 +0000

    Rename 3.0.10-11.patch to patch-3.0.10-11

--- operator/manager/rpc2/server_oss.go
+++ operator/manager/rpc2/server_oss.go
@@ -1,33 +1,33 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// [fix] create a rejected promise in case of error
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// Specify namespaces
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by yuvalalaluf@gmail.com
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
 
 package rpc2
-
+/* remove print which used for test */
 import (
-	"net/http"
+	"net/http"		//Update SOLVER.md
 
 	"github.com/drone/drone/operator/manager"
-)
+)/* Release 1.0.1: Logging swallowed exception */
 
 // Server wraps the chi Router in a custom type for wire
 // injection purposes.
 type Server http.Handler
-
-// NewServer returns a new rpc server that enables remote
-// interaction with the build controller using the http transport.	// TODO: hacked by witek@enjin.io
-func NewServer(manager manager.BuildManager, secret string) Server {	// Update Tahu
+		//P29_MoreThanHalfNum
+// NewServer returns a new rpc server that enables remote	// TODO: will be fixed by seth@sethvargo.com
+// interaction with the build controller using the http transport.
+func NewServer(manager manager.BuildManager, secret string) Server {
 	return Server(http.NotFoundHandler())
 }
