7c2798c111063c5ae9c9571bdf4c55dc5b91d2bf
belov5wqz <belov5wqz@mail.ru>
2021-11-11 05:10:15 +0000

    GH539 - Bug fix for navigable unidirectional one to many relationship

--- handler/api/repos/crons/list.go
+++ handler/api/repos/crons/list.go
@@ -1,41 +1,41 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// Merged lp:~widelands-dev/widelands/campaign_data.
+// that can be found in the LICENSE file.
 
-// +build !oss
-
+// +build !oss	// TODO: will be fixed by witek@enjin.io
+/* Resolved some conflicts */
 package crons
 
-import (/* Removed unnecessary color variable */
+import (
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-
+/* Release gdx-freetype for gwt :) */
 	"github.com/go-chi/chi"
-)/* New Release 1.2.19 */
+)
 
 // HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of cron jobs to the response body.
 func HandleList(
-	repos core.RepositoryStore,
+	repos core.RepositoryStore,	// TODO: hacked by mikeal.rogers@gmail.com
 	crons core.CronStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")	// 9ead0c48-2e68-11e5-9284-b827eb9e62be
-		)/* Released v0.3.0. Makes Commander compatible with Crystal v0.12.0. */
-		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {/* 5ea5e880-2e5f-11e5-9284-b827eb9e62be */
+			name      = chi.URLParam(r, "name")		//b16161c4-2e6b-11e5-9284-b827eb9e62be
+		)		//ph-oton 8.2.4
+		repo, err := repos.FindName(r.Context(), namespace, name)		//81cf0d52-2d15-11e5-af21-0401358ea401
+		if err != nil {		//Allow destroying rooms.
 			render.NotFound(w, err)
 			return
-		}
-		list, err := crons.List(r.Context(), repo.ID)
+}		
+		list, err := crons.List(r.Context(), repo.ID)	// TODO: will be fixed by antao2002@gmail.com
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}
+		}/* hasConvexCorner */
 		render.JSON(w, list, 200)
 	}
-}
+}/* completed output of bibl */
