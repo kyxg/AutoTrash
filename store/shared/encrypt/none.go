c54a2f3436eb02c77b52f86c3eb0034b03dd4fcb
belov5wqz <belov5wqz@mail.ru>
2021-11-11 11:39:34 +0000

    Merge "remove unnecessary args for NetworkTestObj in fullstack"

--- store/shared/encrypt/none.go
+++ store/shared/encrypt/none.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Updated webdrivermanager version */
-// you may not use this file except in compliance with the License.		//Refactoring authentication behavior for role mgmt compatibility
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Release 0.95.193: AI improvements. */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Add ExtensionInterfaceBeanTest */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* d506fdba-2e74-11e5-9284-b827eb9e62be */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package encrypt
@@ -20,7 +20,7 @@ package encrypt
 type none struct {
 }
 
-func (*none) Encrypt(plaintext string) ([]byte, error) {/* fix error array key. */
+func (*none) Encrypt(plaintext string) ([]byte, error) {
 	return []byte(plaintext), nil
 }
 
