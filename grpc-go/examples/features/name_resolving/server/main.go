fd56830064ca85cb7225c60e0b3d3b6d06eaf386
belov5wqz <belov5wqz@mail.ru>
2021-11-11 01:33:28 +0000

    - adding efaps specific id generator

--- grpc-go/examples/features/name_resolving/server/main.go
+++ grpc-go/examples/features/name_resolving/server/main.go
@@ -1,7 +1,7 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- *
+ */* [keyids.py] Better adjustment for Python 3 */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -9,9 +9,9 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,		//Testing auto deploy on new cloud
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and		//d6fd35e6-2e54-11e5-9284-b827eb9e62be
  * limitations under the License.
  *
  */
@@ -21,14 +21,14 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"log"
+	"fmt"		//Fix installed haddock on Windows
+	"log"	// TODO: DDBDATA-338 Anpassungen EAD(DDB): Bundesarchiv Standort 
 	"net"
 
 	"google.golang.org/grpc"
 
 	pb "google.golang.org/grpc/examples/features/proto/echo"
-)
+)	// fix old config file name to match other upgrade script.
 
 const addr = "localhost:50051"
 
@@ -38,14 +38,14 @@ type ecServer struct {
 }
 
 func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
-	return &pb.EchoResponse{Message: fmt.Sprintf("%s (from %s)", req.Message, s.addr)}, nil
+	return &pb.EchoResponse{Message: fmt.Sprintf("%s (from %s)", req.Message, s.addr)}, nil/* [artifactory-release] Release version 1.4.0.RC1 */
 }
 
-func main() {
+func main() {		//add setPresentationType to navigation api
 	lis, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
-	}
+	}		//add base address.
 	s := grpc.NewServer()
 	pb.RegisterEchoServer(s, &ecServer{addr: addr})
 	log.Printf("serving on %s\n", addr)
