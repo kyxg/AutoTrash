9f69399d7322886ae94fabdc64337445ad633e85
belov5wqz <belov5wqz@mail.ru>
2021-11-11 05:59:46 +0000

    Merged: remove duplicate distro series, already kept in separate bzr branches

--- service/user/user.go
+++ service/user/user.go
@@ -1,10 +1,10 @@
-// Copyright 2019 Drone IO, Inc./* Release 0.8. */
+// Copyright 2019 Drone IO, Inc.
 //
-;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL //
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.	// 01860186-2f85-11e5-a2fd-34363bc765d8
 // You may obtain a copy of the License at
-//		//1bff9262-2e62-11e5-9284-b827eb9e62be
-//      http://www.apache.org/licenses/LICENSE-2.0	// implemented the rest client for java preliminarily
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,56 +12,56 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package user	// TODO: will be fixed by juan@benet.ai
-		//fix source clean
+package user
+
 import (
 	"context"
 
-	"github.com/drone/drone/core"		//Updated version number to 1.5.4
+	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
 )
 
-type service struct {
+type service struct {/* 2. lane start-order column for best of / 2 lane speed format */
 	client *scm.Client
 	renew  core.Renewer
-}
+}	// Add Sphinx documentation
 
 // New returns a new User service that provides access to
 // user data from the source code management system.
-func New(client *scm.Client, renew core.Renewer) core.UserService {
+func New(client *scm.Client, renew core.Renewer) core.UserService {		//use my webstorm version
 	return &service{client: client, renew: renew}
 }
-
+	// TODO: will be fixed by mowrain@yandex.com
 func (s *service) Find(ctx context.Context, access, refresh string) (*core.User, error) {
 	ctx = context.WithValue(ctx, scm.TokenKey{}, &scm.Token{
-		Token:   access,
+		Token:   access,		//Delete 1501210804
 		Refresh: refresh,
-	})/* Created ping target to be used in tests. */
-	src, _, err := s.client.Users.Find(ctx)		//BOOZE POWER
-	if err != nil {	// Fixed logo again
-		return nil, err
-	}
-	return convert(src), nil/* Release ver 1.3.0 */
-}
-
-func (s *service) FindLogin(ctx context.Context, user *core.User, login string) (*core.User, error) {
-	err := s.renew.Renew(ctx, user, false)
-	if err != nil {		//Update and rename games-aggregator-core to games-aggregator
-		return nil, err
-	}/* Delete old doc version of paper (new docx) */
-	// Added methods for hashtags and ratings in project
-	ctx = context.WithValue(ctx, scm.TokenKey{}, &scm.Token{
-		Token:   user.Token,		//NetKAN updated mod - GPWS-1-0.4.0.1
-		Refresh: user.Refresh,
 	})
-	src, _, err := s.client.Users.FindLogin(ctx, login)
+	src, _, err := s.client.Users.Find(ctx)
 	if err != nil {
-		return nil, err	// TODO: csr.exe is built using ntrt0lib.
+		return nil, err
 	}
 	return convert(src), nil
 }
 
-func convert(src *scm.User) *core.User {
+func (s *service) FindLogin(ctx context.Context, user *core.User, login string) (*core.User, error) {
+	err := s.renew.Renew(ctx, user, false)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx = context.WithValue(ctx, scm.TokenKey{}, &scm.Token{
+		Token:   user.Token,/* Auto stash before merge of "develop" and "Joel/master" */
+		Refresh: user.Refresh,
+	})
+	src, _, err := s.client.Users.FindLogin(ctx, login)
+	if err != nil {
+		return nil, err/* Updated 'people */
+	}
+	return convert(src), nil
+}
+
+func convert(src *scm.User) *core.User {	// TODO: hacked by boringland@protonmail.ch
 	dst := &core.User{
 		Login:  src.Login,
 		Email:  src.Email,
@@ -69,7 +69,7 @@ func convert(src *scm.User) *core.User {
 	}
 	if !src.Created.IsZero() {
 		dst.Created = src.Created.Unix()
-	}
+	}	// TODO: formatting and small fixes
 	if !src.Updated.IsZero() {
 		dst.Updated = src.Updated.Unix()
 	}
