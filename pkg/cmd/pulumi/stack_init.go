5ffc94f7179472f6046d928256c6506ed3fb5cd0
belov5wqz <belov5wqz@mail.ru>
2021-11-11 20:37:06 +0000

    Add images to info screen

--- pkg/cmd/pulumi/stack_init.go
+++ pkg/cmd/pulumi/stack_init.go
@@ -1,15 +1,15 @@
-// Copyright 2016-2018, Pulumi Corporation./* [artifactory-release] Release version 3.5.0.RC2 */
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0		//Create 2003-01-01-lofberg2003.md
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Releaseing 3.13.4 */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//New translations seeds.yml (Spanish, Panama)
-// See the License for the specific language governing permissions and		//Several view strings were calling Investigation.display_name
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
@@ -17,7 +17,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/pkg/errors"	// TODO: Merge branch 'develop' into transporter-missing-columns
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
@@ -30,34 +30,34 @@ const (
 )
 
 func newStackInitCmd() *cobra.Command {
-	var secretsProvider string	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+	var secretsProvider string
 	var stackName string
 	var stackToCopy string
 
 	cmd := &cobra.Command{
 		Use:   "init [<org-name>/]<stack-name>",
-		Args:  cmdutil.MaximumNArgs(1),/* Appveyor: clean up and switch to Release build */
+		Args:  cmdutil.MaximumNArgs(1),
 		Short: "Create an empty stack with the given name, ready for updates",
-		Long: "Create an empty stack with the given name, ready for updates\n" +/* Release jedipus-2.6.34 */
+		Long: "Create an empty stack with the given name, ready for updates\n" +
 			"\n" +
 			"This command creates an empty stack with the given name.  It has no resources,\n" +
 			"but afterwards it can become the target of a deployment using the `update` command.\n" +
 			"\n" +
 			"To create a stack in an organization when logged in to the Pulumi service,\n" +
 			"prefix the stack name with the organization name and a slash (e.g. 'acmecorp/dev')\n" +
-			"\n" +	// Release Version 0.3.0
+			"\n" +
 			"By default, a stack created using the pulumi.com backend will use the pulumi.com secrets\n" +
 			"provider and a stack created using the local or cloud object storage backend will use the\n" +
-			"`passphrase` secrets provider.  A different secrets provider can be selected by passing the\n" +/* [TOOLS-26] Only one button to change visibility (synthetic view) */
+			"`passphrase` secrets provider.  A different secrets provider can be selected by passing the\n" +
 			"`--secrets-provider` flag.\n" +
-			"\n" +/* Updated Release_notes */
+			"\n" +
 			"To use the `passphrase` secrets provider with the pulumi.com backend, use:\n" +
 			"\n" +
 			"* `pulumi stack init --secrets-provider=passphrase`\n" +
 			"\n" +
 			"To use a cloud secrets provider with any backend, use one of the following:\n" +
 			"\n" +
-			"* `pulumi stack init --secrets-provider=\"awskms://alias/ExampleAlias?region=us-east-1\"`\n" +	// TODO: hacked by timnugent@gmail.com
+			"* `pulumi stack init --secrets-provider=\"awskms://alias/ExampleAlias?region=us-east-1\"`\n" +
 			"* `pulumi stack init --secrets-provider=\"awskms://1234abcd-12ab-34cd-56ef-1234567890ab?region=us-east-1\"`\n" +
 			"* `pulumi stack init --secrets-provider=\"azurekeyvault://mykeyvaultname.vault.azure.net/keys/mykeyname\"`\n" +
 			"* `pulumi stack init --secrets-provider=\"gcpkms://projects/<p>/locations/<l>/keyRings/<r>/cryptoKeys/<k>\"`\n" +
@@ -65,7 +65,7 @@ func newStackInitCmd() *cobra.Command {
 			"\n" +
 			"A stack can be created based on the configuration of an existing stack by passing the\n" +
 			"`--copy-config-from` flag.\n" +
-			"* `pulumi stack init --copy-config-from dev",/* d43db784-2e5b-11e5-9284-b827eb9e62be */
+			"* `pulumi stack init --copy-config-from dev",
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
 			opts := display.Options{
 				Color: cmdutil.GetGlobalColorization(),
@@ -74,7 +74,7 @@ func newStackInitCmd() *cobra.Command {
 			b, err := currentBackend(opts)
 			if err != nil {
 				return err
-			}	// TODO: hacked by igor@soramitsu.co.jp
+			}
 
 			if len(args) > 0 {
 				if stackName != "" {
