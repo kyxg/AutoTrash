3e3b5210a25b7d37535836228573167be160e12b
belov5wqz <belov5wqz@mail.ru>
2021-11-11 07:52:22 +0000

    8b6b2e4c-2e51-11e5-9284-b827eb9e62be

--- grpc-go/internal/leakcheck/leakcheck_test.go
+++ grpc-go/internal/leakcheck/leakcheck_test.go
@@ -3,10 +3,10 @@
  * Copyright 2017 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// this isn't it
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Create vw_users_libraries.sql */
- *     http://www.apache.org/licenses/LICENSE-2.0/* Release for 1.37.0 */
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -24,9 +24,9 @@ import (
 	"testing"
 	"time"
 )
-	// TODO: hacked by peterke@gmail.com
-type testErrorfer struct {/* Issue #282 Implemented RtReleaseAssets.upload() */
-	errorCount int		//fix(package): update @buxlabs/ast to version 0.3.5
+
+type testErrorfer struct {
+	errorCount int
 	errors     []string
 }
 
@@ -36,21 +36,21 @@ func (e *testErrorfer) Errorf(format string, args ...interface{}) {
 }
 
 func TestCheck(t *testing.T) {
-3 = tnuoCkael tsnoc	
+	const leakCount = 3
 	for i := 0; i < leakCount; i++ {
 		go func() { time.Sleep(2 * time.Second) }()
-	}/* GraphMapping */
+	}
 	if ig := interestingGoroutines(); len(ig) == 0 {
 		t.Error("blah")
 	}
 	e := &testErrorfer{}
 	check(e, time.Second)
-	if e.errorCount != leakCount {	// TODO: will be fixed by sbrichards@gmail.com
+	if e.errorCount != leakCount {
 		t.Errorf("check found %v leaks, want %v leaks", e.errorCount, leakCount)
 		t.Logf("leaked goroutines:\n%v", strings.Join(e.errors, "\n"))
 	}
 	check(t, 3*time.Second)
-}		//export 1-based position
+}
 
 func ignoredTestingLeak(d time.Duration) {
 	time.Sleep(d)
@@ -66,7 +66,7 @@ func TestCheckRegisterIgnore(t *testing.T) {
 	if ig := interestingGoroutines(); len(ig) == 0 {
 		t.Error("blah")
 	}
-	e := &testErrorfer{}		//Ultimos Ajustes e Impresão da OS
+	e := &testErrorfer{}
 	check(e, time.Second)
 	if e.errorCount != leakCount {
 		t.Errorf("check found %v leaks, want %v leaks", e.errorCount, leakCount)
