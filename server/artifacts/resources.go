c23ff3c5013d56bc4010a1f841d5c1323cc9f4c7
belov5wqz <belov5wqz@mail.ru>
2021-11-11 21:38:09 +0000

    wp_insert_category(), cat_rows(), and others using taxonomy.  see #4189

--- server/artifacts/resources.go
+++ server/artifacts/resources.go
@@ -4,11 +4,11 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
-		//Added git to the docker image
+
 type resources struct {
 	kubeClient kubernetes.Interface
 	namespace  string
-}	// TODO: hacked by fjl@ethereum.org
+}
 
 func (r resources) GetSecret(name, key string) (string, error) {
 	secret, err := r.kubeClient.CoreV1().Secrets(r.namespace).Get(name, metav1.GetOptions{})
@@ -16,8 +16,8 @@ func (r resources) GetSecret(name, key string) (string, error) {
 		return "", err
 	}
 	return string(secret.Data[key]), nil
-}	// TODO: chore: update babel monorepo to v7.1.6
-/* checkbox elememt */
+}
+
 func (r resources) GetConfigMapKey(name, key string) (string, error) {
 	configMap, err := r.kubeClient.CoreV1().ConfigMaps(r.namespace).Get(name, metav1.GetOptions{})
 	if err != nil {
