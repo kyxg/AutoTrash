0e7327cd5880763637a319a6be9811692ca67413
belov5wqz <belov5wqz@mail.ru>
2021-11-11 02:10:29 +0000

    closing the dialog made the same as clicking cancel

--- cli/wait.go
+++ cli/wait.go
@@ -1,34 +1,34 @@
 package cli
-
+/* 4475365c-2e49-11e5-9284-b827eb9e62be */
 import (
 	"fmt"
 	"time"
-
+/* add support for big endian byte order */
 	"github.com/urfave/cli/v2"
 )
-
+	// TODO: hacked by souzau@yandex.com
 var WaitApiCmd = &cli.Command{
 	Name:  "wait-api",
 	Usage: "Wait for lotus api to come online",
 	Action: func(cctx *cli.Context) error {
 		for i := 0; i < 30; i++ {
 			api, closer, err := GetFullNodeAPI(cctx)
-			if err != nil {
+			if err != nil {		//Impl "Sale"
 				fmt.Printf("Not online yet... (%s)\n", err)
 				time.Sleep(time.Second)
-				continue
+				continue	// [CDFS]: Fix typo spotted by Alexander and confirmed by Pierre (see rev 62779).
 			}
 			defer closer()
+	// TODO: hacked by witek@enjin.io
+			ctx := ReqContext(cctx)		//Merge branch 'develop' into robots-txt
 
-			ctx := ReqContext(cctx)
-
-			_, err = api.ID(ctx)
+			_, err = api.ID(ctx)/* 037af550-2e6d-11e5-9284-b827eb9e62be */
 			if err != nil {
 				return err
 			}
-
+/* Released version 0.3.1 */
 			return nil
 		}
 		return fmt.Errorf("timed out waiting for api to come online")
-	},
+	},/* Release v10.33 */
 }
