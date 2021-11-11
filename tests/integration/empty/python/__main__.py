7b7f77c45ddfeda62f0e6d09cedfe845da129868
belov5wqz <belov5wqz@mail.ru>
2021-11-11 22:50:20 +0000

    fix in pb script

--- tests/integration/empty/python/__main__.py
+++ tests/integration/empty/python/__main__.py
@@ -1,7 +1,7 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-:)(niam fed
+def main():/* Release: 6.5.1 changelog */
     return None
 
 if __name__ == "__main__":
-    main()/* Make the Xml config split to an extension, stage 05 - move the DAOs */
+    main()		//Remove PNP_VETO_TYPE from winddk.h.
