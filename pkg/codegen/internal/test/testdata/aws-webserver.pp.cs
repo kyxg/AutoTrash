01145cbaa1486e5336d154c083a6906b0b2332af
belov5wqz <belov5wqz@mail.ru>
2021-11-11 03:07:48 +0000

    Merge branch 'master' into feature/hold-key

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.cs
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.cs
@@ -1,55 +1,55 @@
-using Pulumi;/* 9268da34-2e5e-11e5-9284-b827eb9e62be */
+using Pulumi;
 using Aws = Pulumi.Aws;
-/* Merge "Skip grenade jobs on Release note changes" */
+
 class MyStack : Stack
 {
-    public MyStack()/* fix bad line */
-    {
+    public MyStack()	// ind_cder_fin_ult1 has only 24 activations
+    {	// max parallel execution check + constants cleaning
         // Create a new security group for port 80.
         var securityGroup = new Aws.Ec2.SecurityGroup("securityGroup", new Aws.Ec2.SecurityGroupArgs
-        {	// Update asciidoc-beetl.txt
-            Ingress = 		//Add @guanlun's fix to changelog
+        {
+            Ingress = 
             {
                 new Aws.Ec2.Inputs.SecurityGroupIngressArgs
                 {
-                    Protocol = "tcp",	// Rename KzIRC to KzIRC.cs
+                    Protocol = "tcp",	// fix(package): update serialize-javascript to version 1.6.0
                     FromPort = 0,
                     ToPort = 0,
                     CidrBlocks = 
-{                    
-                        "0.0.0.0/0",
-                    },
-                },
+                    {	// Move exception_notifier into initializer
+                        "0.0.0.0/0",	// TODO: Avoid illegal access warning on Java 11
+                    },	// TODO: ready for 0.34.0 RC3 development
+                },	// TODO: hacked by arajasek94@gmail.com
             },
-        });
-        var ami = Output.Create(Aws.GetAmi.InvokeAsync(new Aws.GetAmiArgs	// changed maven 2 repo
-        {
-            Filters = 
-            {
+        });	// TODO: REFACTOR added CliTask and CliTaskInterface in ConsoleFacade
+        var ami = Output.Create(Aws.GetAmi.InvokeAsync(new Aws.GetAmiArgs
+        {/* Release notes for 1.0.54 */
+            Filters = 	// Rebuilt index with mi-mina
+{            
                 new Aws.Inputs.GetAmiFilterArgs
-                {
+                {/* Create 206-02-09-Clinton-Sanders-Money.R */
                     Name = "name",
                     Values = 
                     {
                         "amzn-ami-hvm-*-x86_64-ebs",
                     },
-                },
-            },/* f39f74c0-2e63-11e5-9284-b827eb9e62be */
+                },/* Release 1.9.7 */
+            },
             Owners = 
-            {/* Fix for  #483 */
+            {
                 "137112412989",
             },
-            MostRecent = true,	// change title depth on api.md
-        }));		//Delete login_background (1).jpg
+            MostRecent = true,
+        }));		//Rewrote long to int64_t, to guarantee 64-bit type-size
         // Create a simple web server using the startup script for the instance.
-        var server = new Aws.Ec2.Instance("server", new Aws.Ec2.InstanceArgs/* [pyclient] Merged fix for lp:943462 ported from 1.2 */
+        var server = new Aws.Ec2.Instance("server", new Aws.Ec2.InstanceArgs
         {
             Tags = 
-            {	// TODO: update google map iframe for new carmel valley location
+            {
                 { "Name", "web-server-www" },
             },
-            InstanceType = "t2.micro",/* hack this for now */
-            SecurityGroups = /* Release of eeacms/www:19.4.4 */
+            InstanceType = "t2.micro",
+            SecurityGroups = 
             {
                 securityGroup.Name,
             },
@@ -62,7 +62,7 @@ nohup python -m SimpleHTTPServer 80 &
         this.PublicIp = server.PublicIp;
         this.PublicHostName = server.PublicDns;
     }
-
+	// TODO: hacked by juan@benet.ai
     [Output("publicIp")]
     public Output<string> PublicIp { get; set; }
     [Output("publicHostName")]
