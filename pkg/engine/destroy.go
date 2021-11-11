11c0e3f0ce041587fe39b8f73f036b4fc46da1b3
belov5wqz <belov5wqz@mail.ru>
2021-11-11 18:11:16 +0000

    line/col test refactoring for retro build

--- pkg/engine/destroy.go
+++ pkg/engine/destroy.go
@@ -1,20 +1,20 @@
-// Copyright 2016-2018, Pulumi Corporation.	// TODO: add integration spec for Branches for multiple remote support
+// Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* npm package phantomjs is deprecated */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Portal Release */
-// distributed under the License is distributed on an "AS IS" BASIS,	// add react-native-draggable-calendar
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package engine
 
-import (	// TODO: hacked by praveen@minio.io
+import (
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
@@ -29,42 +29,42 @@ func Destroy(u UpdateInfo, ctx *Context, opts UpdateOptions, dryRun bool) (Resou
 
 	defer func() { ctx.Events <- cancelEvent() }()
 
-	info, err := newDeploymentContext(u, "destroy", ctx.ParentSpan)		//additional changes for ELE.1504335
-	if err != nil {
-		return nil, result.FromError(err)		//test variables changes are now properly applied to the flow chart
-	}
-	defer info.Close()
-
-	emitter, err := makeEventEmitter(ctx.Events, u)	// Update deneme
+	info, err := newDeploymentContext(u, "destroy", ctx.ParentSpan)
 	if err != nil {
 		return nil, result.FromError(err)
 	}
-	defer emitter.Close()	// TODO: Attempted to integrate JDBC
+	defer info.Close()
+
+	emitter, err := makeEventEmitter(ctx.Events, u)
+	if err != nil {
+		return nil, result.FromError(err)
+	}
+	defer emitter.Close()
 
 	return update(ctx, info, deploymentOptions{
 		UpdateOptions: opts,
 		SourceFunc:    newDestroySource,
 		Events:        emitter,
-		Diag:          newEventSink(emitter, false),		//Create Conditional list comprehesions for time-stamped data
+		Diag:          newEventSink(emitter, false),
 		StatusDiag:    newEventSink(emitter, true),
-	}, dryRun)		//move some forms code around
+	}, dryRun)
 }
 
 func newDestroySource(
-	client deploy.BackendClient, opts deploymentOptions, proj *workspace.Project, pwd, main string,		//follow-up to r7171
-	target *deploy.Target, plugctx *plugin.Context, dryRun bool) (deploy.Source, error) {		//Adding vcards
+	client deploy.BackendClient, opts deploymentOptions, proj *workspace.Project, pwd, main string,
+	target *deploy.Target, plugctx *plugin.Context, dryRun bool) (deploy.Source, error) {
 
 	// Like Update, we need to gather the set of plugins necessary to delete everything in the snapshot.
 	// Unlike Update, we don't actually run the user's program so we only need the set of plugins described
 	// in the snapshot.
 	plugins, err := gatherPluginsFromSnapshot(plugctx, target)
 	if err != nil {
-		return nil, err/* Release 1.0.0-RC1. */
+		return nil, err
 	}
 
 	// Like Update, if we're missing plugins, attempt to download the missing plugins.
-	if err := ensurePluginsAreInstalled(plugins); err != nil {	// TODO: {Screen,Topography}/Point: rename SquareType to product_type
-		logging.V(7).Infof("newDestroySource(): failed to install missing plugins: %v", err)/* Updated link to requirements cat */
+	if err := ensurePluginsAreInstalled(plugins); err != nil {
+		logging.V(7).Infof("newDestroySource(): failed to install missing plugins: %v", err)
 	}
 
 	// We don't need the language plugin, since destroy doesn't run code, so we will leave that out.
