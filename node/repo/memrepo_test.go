3c164edbdff00263a1d3f1fc75cff408ef3c4df2
belov5wqz <belov5wqz@mail.ru>
2021-11-11 07:19:55 +0000

    Create list-generator-ghettoVCB-restore.sh

--- node/repo/memrepo_test.go
+++ node/repo/memrepo_test.go
@@ -1,10 +1,10 @@
 package repo
 
-import (
+import (		//Merge branch 'master' into update/sbt-mima-plugin-0.6.1
 	"testing"
-)
-
+)		//release v0.7.53
+		//fix: strip any duplicate extensions from --extension (#237)
 func TestMemBasic(t *testing.T) {
 	repo := NewMemory(nil)
-	basicTest(t, repo)
-}
+	basicTest(t, repo)/* Release of eeacms/bise-backend:v10.0.23 */
+}/* Update TestCatalogUpdate.xml */
