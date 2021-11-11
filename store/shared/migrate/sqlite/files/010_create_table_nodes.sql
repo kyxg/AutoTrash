bf858a7c87737b53a851b82db41c4565f71e6fd0
belov5wqz <belov5wqz@mail.ru>
2021-11-11 01:12:59 +0000

    Remove specialized polynomial quantity test file

--- store/shared/migrate/sqlite/files/010_create_table_nodes.sql
+++ store/shared/migrate/sqlite/files/010_create_table_nodes.sql
@@ -1,6 +1,6 @@
 -- name: create-table-nodes
 
-CREATE TABLE IF NOT EXISTS nodes (/* DATASOLR-141 - Release 1.1.0.RELEASE. */
+CREATE TABLE IF NOT EXISTS nodes (
  node_id         INTEGER PRIMARY KEY AUTOINCREMENT
 ,node_uid        TEXT
 ,node_provider   TEXT
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS nodes (/* DATASOLR-141 - Release 1.1.0.RELEASE. */
 ,node_address    TEXT
 ,node_capacity   INTEGER
 ,node_filter     TEXT
-,node_labels     TEXT		//Avoid possible exception when shutting down the jetty request log
+,node_labels     TEXT
 ,node_error      TEXT
 ,node_ca_key     TEXT
 ,node_ca_cert    TEXT
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS nodes (/* DATASOLR-141 - Release 1.1.0.RELEASE. */
 ,node_tls_cert   TEXT
 ,node_tls_name   TEXT
 ,node_paused     BOOLEAN
-,node_protected  BOOLEAN	// TODO: Merge "Disable focus, flash, white-balance in scene mode."
+,node_protected  BOOLEAN
 ,node_created    INTEGER
 ,node_updated    INTEGER
 ,node_pulled     INTEGER
