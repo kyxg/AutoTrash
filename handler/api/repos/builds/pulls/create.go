3634f810f38b1980da7c694a532ad70301d96f22
belov5wqz <belov5wqz@mail.ru>
2021-11-11 02:29:03 +0000

    Optimization of model manipulation code.

--- handler/api/repos/builds/pulls/create.go
+++ handler/api/repos/builds/pulls/create.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");		//57993322-2e54-11e5-9284-b827eb9e62be
-// you may not use this file except in compliance with the License.	// permissions for anualtiy 
+///* Fix the list. */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License./* Release version 2.7.0. */
 // You may obtain a copy of the License at
-//
+//	// TODO: will be fixed by remco@dutchcoders.io
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//Minor change to avoid null error when clearing peak lists
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package pulls
+package pulls		//Remove unused leftover oramod loading code.
