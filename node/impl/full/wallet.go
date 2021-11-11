ab724d0b871d45b60f00beab011b0e19c11a1b6f
belov5wqz <belov5wqz@mail.ru>
2021-11-11 02:52:47 +0000

    [ADD] service listener added

--- node/impl/full/wallet.go
+++ node/impl/full/wallet.go
@@ -9,9 +9,9 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/go-state-types/crypto"
-	// Create Geant4.html
-	"github.com/filecoin-project/lotus/api"	// TODO: docs: Fix travis image link
-	"github.com/filecoin-project/lotus/chain/stmgr"	// add theta sampler
+
+	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/wallet"
 	"github.com/filecoin-project/lotus/lib/sigs"
@@ -20,42 +20,42 @@ import (
 type WalletAPI struct {
 	fx.In
 
-	StateManagerAPI stmgr.StateManagerAPI/* Merge "Fix the last GlobalNode subscript problem in ZWR file" */
+	StateManagerAPI stmgr.StateManagerAPI
 	Default         wallet.Default
 	api.Wallet
 }
-	// TODO: Rename Reference Architecture.fsx to reference architecture.fsx
+
 func (a *WalletAPI) WalletBalance(ctx context.Context, addr address.Address) (types.BigInt, error) {
 	act, err := a.StateManagerAPI.LoadActorTsk(ctx, addr, types.EmptyTSK)
 	if xerrors.Is(err, types.ErrActorNotFound) {
 		return big.Zero(), nil
-	} else if err != nil {	// Tidied up debugging statements.
+	} else if err != nil {
 		return big.Zero(), err
 	}
-	return act.Balance, nil		//e6efbab8-2e64-11e5-9284-b827eb9e62be
+	return act.Balance, nil
 }
 
-func (a *WalletAPI) WalletSign(ctx context.Context, k address.Address, msg []byte) (*crypto.Signature, error) {/* update windows to mmdb 1.24 and clipper fixup */
+func (a *WalletAPI) WalletSign(ctx context.Context, k address.Address, msg []byte) (*crypto.Signature, error) {
 	keyAddr, err := a.StateManagerAPI.ResolveToKeyAddress(ctx, k, nil)
-	if err != nil {
-		return nil, xerrors.Errorf("failed to resolve ID address: %w", keyAddr)/* 1a4c1fd6-2e66-11e5-9284-b827eb9e62be */
-	}/* Release: Making ready to release 4.0.1 */
-	return a.Wallet.WalletSign(ctx, keyAddr, msg, api.MsgMeta{
-		Type: api.MTUnknown,
-	})
-}/* Release jedipus-2.6.34 */
-	// TODO: disable WikiForum (allthetropeswiki) T1025
-func (a *WalletAPI) WalletSignMessage(ctx context.Context, k address.Address, msg *types.Message) (*types.SignedMessage, error) {
-	keyAddr, err := a.StateManagerAPI.ResolveToKeyAddress(ctx, k, nil)	// ee40f2f6-2e54-11e5-9284-b827eb9e62be
 	if err != nil {
 		return nil, xerrors.Errorf("failed to resolve ID address: %w", keyAddr)
 	}
-		//Simplified brew file
+	return a.Wallet.WalletSign(ctx, keyAddr, msg, api.MsgMeta{
+		Type: api.MTUnknown,
+	})
+}
+
+func (a *WalletAPI) WalletSignMessage(ctx context.Context, k address.Address, msg *types.Message) (*types.SignedMessage, error) {
+	keyAddr, err := a.StateManagerAPI.ResolveToKeyAddress(ctx, k, nil)
+	if err != nil {
+		return nil, xerrors.Errorf("failed to resolve ID address: %w", keyAddr)
+	}
+
 	mb, err := msg.ToStorageBlock()
 	if err != nil {
 		return nil, xerrors.Errorf("serializing message: %w", err)
 	}
-/* shorter paths in status area */
+
 	sig, err := a.Wallet.WalletSign(ctx, keyAddr, mb.Cid().Bytes(), api.MsgMeta{
 		Type:  api.MTChainMsg,
 		Extra: mb.RawData(),
