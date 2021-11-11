b6c114076026e953b93b7f1597ecec7ac791bece
belov5wqz <belov5wqz@mail.ru>
2021-11-11 11:11:45 +0000

    use h2 tags in the readme

--- handler/api/user/sync_test.go
+++ handler/api/user/sync_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//Added getPitch, fixed getHarmonics and added compile script to create SWC file.
+
 package user
