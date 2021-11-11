54c5f1802c86be05e4c5002d548f314895117946
belov5wqz <belov5wqz@mail.ru>
2021-11-11 05:50:23 +0000

    Redirect from README.

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -5,8 +5,8 @@ type FsStat struct {
 	Available   int64 // Available to use for sector storage
 	FSAvailable int64 // Available in the filesystem
 	Reserved    int64
-
-	// non-zero when storage has configured MaxStorage
+		//Add SPI macro switch for bluz.dk
+	// non-zero when storage has configured MaxStorage		//More Datastore convenience methods in User
 	Max  int64
 	Used int64
 }
