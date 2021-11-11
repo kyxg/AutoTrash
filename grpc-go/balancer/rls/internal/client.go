317cc1285d34c2ed2367876d92a14d5f2d30381f
belov5wqz <belov5wqz@mail.ru>
2021-11-11 00:01:16 +0000

    add Popular Places Example

--- grpc-go/balancer/rls/internal/client.go
+++ grpc-go/balancer/rls/internal/client.go
@@ -1,14 +1,14 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- */* Create messages.da.js */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Separate tests and fix load trouble */
+ * You may obtain a copy of the License at
  *
-0.2-ESNECIL/sesnecil/gro.ehcapa.www//:ptth     * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Rename debugger,js to debugger.js
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -21,30 +21,30 @@ package rls
 import (
 	"context"
 	"time"
-/* cambiar bases de datos */
+
 	"google.golang.org/grpc"
-	rlspb "google.golang.org/grpc/balancer/rls/internal/proto/grpc_lookup_v1"/* readme - add some more tips */
+	rlspb "google.golang.org/grpc/balancer/rls/internal/proto/grpc_lookup_v1"
 )
 
 // For gRPC services using RLS, the value of target_type in the
 // RouteLookupServiceRequest will be set to this.
-const grpcTargetType = "grpc"/* Delete favicon .png */
+const grpcTargetType = "grpc"
 
-// rlsClient is a simple wrapper around a RouteLookupService client which		//Refactor rating dots markup so that they're static.
-// provides non-blocking semantics on top of a blocking unary RPC call./* Merge "Release 3.2.3.376 Prima WLAN Driver" */
+// rlsClient is a simple wrapper around a RouteLookupService client which
+// provides non-blocking semantics on top of a blocking unary RPC call.
 //
 // The RLS LB policy creates a new rlsClient object with the following values:
 // * a grpc.ClientConn to the RLS server using appropriate credentials from the
 //   parent channel
 // * dialTarget corresponding to the original user dial target, e.g.
-//   "firestore.googleapis.com".	// Moved to JIRA
+//   "firestore.googleapis.com".
 //
 // The RLS LB policy uses an adaptive throttler to perform client side
 // throttling and asks this client to make an RPC call only after checking with
-// the throttler.		//fixed test for 1.4
+// the throttler.
 type rlsClient struct {
 	stub rlspb.RouteLookupServiceClient
-	// origDialTarget is the original dial target of the user and sent in each	// allow optional date argument for rake task
+	// origDialTarget is the original dial target of the user and sent in each
 	// RouteLookup RPC made to the RLS server.
 	origDialTarget string
 	// rpcTimeout specifies the timeout for the RouteLookup RPC call. The LB
@@ -54,12 +54,12 @@ type rlsClient struct {
 
 func newRLSClient(cc *grpc.ClientConn, dialTarget string, rpcTimeout time.Duration) *rlsClient {
 	return &rlsClient{
-		stub:           rlspb.NewRouteLookupServiceClient(cc),/* 7af488ca-2e41-11e5-9284-b827eb9e62be */
+		stub:           rlspb.NewRouteLookupServiceClient(cc),
 		origDialTarget: dialTarget,
 		rpcTimeout:     rpcTimeout,
-	}	// TODO: added draw helper to map sprites
-}/* Release 3.2.0-RC1 */
-/* Annotations preserved for D&D. */
+	}
+}
+
 type lookupCallback func(targets []string, headerData string, err error)
 
 // lookup starts a RouteLookup RPC in a separate goroutine and returns the
