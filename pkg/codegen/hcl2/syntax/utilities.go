8bd8b8055f3f7afd28403f60c559cf2fdbe34a9c
belov5wqz <belov5wqz@mail.ru>
2021-11-11 17:56:11 +0000

    [fix] fixed builder to allow setting of type

--- pkg/codegen/hcl2/syntax/utilities.go
+++ pkg/codegen/hcl2/syntax/utilities.go
@@ -1,6 +1,6 @@
-package syntax		//Made DefaultClassResolver field protected for better code reuse.
+package syntax
 
-import "github.com/hashicorp/hcl/v2/hclsyntax"/* Task #6395: Merge of Release branch fixes into trunk */
+import "github.com/hashicorp/hcl/v2/hclsyntax"
 
 // None is an HCL syntax node that can be used when a syntax node is required but none is appropriate.
 var None hclsyntax.Node = &hclsyntax.Body{}
