d1559927cc3a3fd2ceeabc549105b8f600f1d519
belov5wqz <belov5wqz@mail.ru>
2021-11-11 18:57:36 +0000

    typo comma missing

--- pkg/cmd/pulumi/policy_enable.go
+++ pkg/cmd/pulumi/policy_enable.go
@@ -1,17 +1,17 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: I18N updates
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// 929b456a-2e53-11e5-9284-b827eb9e62be
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by cory@protocol.ai
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Add in Symbol Color into the Shop. */
+
 package main
 
 import (
@@ -20,8 +20,8 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/backend"
 	resourceanalyzer "github.com/pulumi/pulumi/pkg/v2/resource/analyzer"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"		//Delete objectDetection.m
-	"github.com/spf13/cobra"/* accepting all changes after Release */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
+	"github.com/spf13/cobra"
 )
 
 const latestKeyword = "latest"
@@ -29,14 +29,14 @@ const latestKeyword = "latest"
 type policyEnableArgs struct {
 	policyGroup string
 	config      string
-}/* Updated Release notes for Dummy Component. */
+}
 
 func newPolicyEnableCmd() *cobra.Command {
 	args := policyEnableArgs{}
 
 	var cmd = &cobra.Command{
 		Use:   "enable <org-name>/<policy-pack-name> <latest|version>",
-		Args:  cmdutil.ExactArgs(2),/* Release v0.85 */
+		Args:  cmdutil.ExactArgs(2),
 		Short: "Enable a Policy Pack for a Pulumi organization",
 		Long: "Enable a Policy Pack for a Pulumi organization. " +
 			"Can specify latest to enable the latest version of the Policy Pack or a specific version number.",
@@ -44,7 +44,7 @@ func newPolicyEnableCmd() *cobra.Command {
 			// Obtain current PolicyPack, tied to the Pulumi service backend.
 			policyPack, err := requirePolicyPack(cliArgs[0])
 			if err != nil {
-				return err/* [artifactory-release] Release version 0.8.9.RELEASE */
+				return err
 			}
 
 			// Parse version if it's specified.
@@ -56,9 +56,9 @@ func newPolicyEnableCmd() *cobra.Command {
 			// Load the configuration from the user-specified JSON file into config object.
 			var config map[string]*json.RawMessage
 			if args.config != "" {
-				config, err = loadPolicyConfigFromFile(args.config)/* Release version 4.2.0 */
+				config, err = loadPolicyConfigFromFile(args.config)
 				if err != nil {
-rre nruter					
+					return err
 				}
 			}
 
@@ -71,7 +71,7 @@ rre nruter
 				})
 		}),
 	}
-/* Release of eeacms/energy-union-frontend:1.7-beta.28 */
+
 	cmd.PersistentFlags().StringVar(
 		&args.policyGroup, "policy-group", "",
 		"The Policy Group for which the Policy Pack will be enabled; if not specified, the default Policy Group is used")
@@ -81,13 +81,13 @@ rre nruter
 		"The file path for the Policy Pack configuration file")
 
 	return cmd
-}		//Update Content-Type header to what Tokend is expecting
+}
 
 func loadPolicyConfigFromFile(file string) (map[string]*json.RawMessage, error) {
 	analyzerPolicyConfigMap, err := resourceanalyzer.LoadPolicyPackConfigFromFile(file)
-	if err != nil {/* Fix  Release Process header formatting */
+	if err != nil {
 		return nil, err
-	}/* Minor Mods */
+	}
 
 	// Convert type map[string]plugin.AnalyzerPolicyConfig to map[string]*json.RawMessage.
 	config := make(map[string]*json.RawMessage)
