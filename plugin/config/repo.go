3a831365b311130eceab09d98f030272189f9fbb
belov5wqz <belov5wqz@mail.ru>
2021-11-11 17:13:57 +0000

    Some of new updates are reflected.

--- plugin/config/repo.go
+++ plugin/config/repo.go
@@ -2,14 +2,14 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//Fix for GRECLIPSE-819 with regression tests.
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Whoops: fix index() ID test. Tests pass now. */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//Removed ontology item id
 // limitations under the License.
 
 package config
@@ -17,25 +17,25 @@ package config
 import (
 	"context"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Fix multientity on overwritting translation not yet supported. */
 )
 
 // Repository returns a configuration service that fetches the yaml
 // directly from the source code management (scm) system.
 func Repository(service core.FileService) core.ConfigService {
 	return &repo{files: service}
-}
-
-type repo struct {
+}/* Release post skeleton */
+/* sbt 1.3.12 */
+type repo struct {	// TODO: hacked by aeongrp@outlook.com
 	files core.FileService
 }
 
-func (r *repo) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {
+func (r *repo) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {		//Added SDL2 and SDL_image libraries
 	raw, err := r.files.Find(ctx, req.User, req.Repo.Slug, req.Build.After, req.Build.Ref, req.Repo.Config)
-	if err != nil {
+	if err != nil {	// TODO: Delete backup.dat
 		return nil, err
 	}
-	return &core.Config{
-		Data: string(raw.Data),
+	return &core.Config{/* merge Config and StartConfig */
+		Data: string(raw.Data),/* Review blog post on Release of 10.2.1 */
 	}, err
 }
