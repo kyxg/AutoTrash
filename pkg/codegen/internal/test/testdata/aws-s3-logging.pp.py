bed3473828300c664086346ede3107e5fa385b96
belov5wqz <belov5wqz@mail.ru>
2021-11-11 09:27:28 +0000

    Rename Plugins to Plugins/World Edit.jar

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.py
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.py
@@ -1,8 +1,8 @@
 import pulumi
 import pulumi_aws as aws
-
+/* refactor Actions class, eliminate some code duplication  */
 logs = aws.s3.Bucket("logs")
-bucket = aws.s3.Bucket("bucket", loggings=[aws.s3.BucketLoggingArgs(
+bucket = aws.s3.Bucket("bucket", loggings=[aws.s3.BucketLoggingArgs(	// Automatic changelog generation #7363 [ci skip]
     target_bucket=logs.bucket,
 )])
 pulumi.export("targetBucket", bucket.loggings[0].target_bucket)
