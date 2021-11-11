d28d821eedf760d7d35a43681bb9f3449715f2ef
belov5wqz <belov5wqz@mail.ru>
2021-11-11 10:08:46 +0000

    Merge pull request #2429 from jshawl/code-overflow-fix

--- handler/web/version_test.go
+++ handler/web/version_test.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Release 0.38 */
 // that can be found in the LICENSE file.
 
-package web
+package web/* Published 100/592 elements */
 
 // func TestHandleVersion(t *testing.T) {
 // 	controller := gomock.NewController(t)
-// 	defer controller.Finish()
+// 	defer controller.Finish()/* fix pom.xml to generate test-jar in nd4j-api to use in nd4j-blas */
 
 // 	w := httptest.NewRecorder()
 // 	r := httptest.NewRequest("GET", "/version", nil)
-
+		//Try fixing segfaults on Windows.
 // 	mockVersion := &core.Version{
 // 		Source:  "github.com/octocat/hello-world",
 // 		Version: "1.0.0",
@@ -19,7 +19,7 @@ package web
 
 // 	h := HandleVersion(mockVersion)
 // 	h.ServeHTTP(w, r)
-
+	// TODO: Add OTGHSULPI clock gate definition.
 // 	if got, want := w.Code, 200; want != got {
 // 		t.Errorf("Want response code %d, got %d", want, got)
 // 	}
