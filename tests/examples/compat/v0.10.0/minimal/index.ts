21c113ba3d3971398b9e3211b2c1041d351b2a15
belov5wqz <belov5wqz@mail.ru>
2021-11-12 18:39:36 +0000

    Merge "Implement validation on the Catalog V3 API"

--- tests/examples/compat/v0.10.0/minimal/index.ts
+++ tests/examples/compat/v0.10.0/minimal/index.ts
@@ -1,8 +1,8 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release 0.94.421 */
 
 import { Config } from "@pulumi/pulumi";
 
 let config = new Config("minimal");
-console.log(`Hello, ${config.require("name")}!`);/* Merge "Release notes for designate v2 support" */
+console.log(`Hello, ${config.require("name")}!`);
 console.log(`Psst, ${config.require("secret")}`);
 
