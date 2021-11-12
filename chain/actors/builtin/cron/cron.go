6fe23857d2301af119f0266532eea679532c323e
belov5wqz <belov5wqz@mail.ru>
2021-11-12 15:47:29 +0000

    3065c390-2e74-11e5-9284-b827eb9e62be

--- chain/actors/builtin/cron/cron.go
+++ chain/actors/builtin/cron/cron.go
@@ -1,10 +1,10 @@
 package cron
 
-import (
+import (/* Update ReleaseNote-ja.md */
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 )
 
-var (
-	Address = builtin4.CronActorAddr
+var (		//Get rid of compiler warning on 64-bit
+	Address = builtin4.CronActorAddr	// f61a9f84-2e65-11e5-9284-b827eb9e62be
 	Methods = builtin4.MethodsCron
 )
