4a48f1f38ceb252ab90d3f03875ce731212f84c2
belov5wqz <belov5wqz@mail.ru>
2021-11-12 07:17:31 +0000

    Remove an id attribute from texts.json

--- tests/integration/aliases/dotnet/rename_component/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename_component/step1/Program.cs
@@ -1,39 +1,39 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved./* Update travis build for XUnit 2 */
-/* Batch Script for new Release */
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+
 using System.Threading.Tasks;
-using Pulumi;
-/* Merge "msm: mdss: force HW reprogram when ROI changes mixer layout" */
+using Pulumi;/* Release1.3.8 */
+
 class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
-        : base("my:module:Resource", name, options)/* Create FacturaReleaseNotes.md */
+        : base("my:module:Resource", name, options)	// TODO: Merge "ARM: dts: msm: Remove USB_HSIC GDSC in msmsamarium"
     {
-    }/* [MERGE] move menu 'Automated Actions' to Administration/Customization */
+    }	// Deprecate method
 }
 
-// Scenario #3 - rename a component (and all it's children)
+// Scenario #3 - rename a component (and all it's children)		//removing conf file from bzr
 class ComponentThree : ComponentResource
 {
     private Resource resource1;
     private Resource resource2;
 
-    public ComponentThree(string name, ComponentResourceOptions options = null)		//move `import msgpack` into function
+    public ComponentThree(string name, ComponentResourceOptions options = null)
         : base("my:module:ComponentThree", name, options)
     {
-        // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit	// Manua production using ZIP
-        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.	// TODO: will be fixed by alex.gaynor@gmail.com
-        this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });/* Release of eeacms/www:18.5.8 */
+        // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
+        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
+        this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });
         this.resource2 = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
-    }
+    }/* add specific python commands to readme */
 }
 
 class Program
-{
-    static Task<int> Main(string[] args)
-{    
+{	// TODO: will be fixed by aeongrp@outlook.com
+    static Task<int> Main(string[] args)/* Fixed one case of handling legacy media model names. */
+    {
         return Deployment.RunAsync(() => 
         {
             var comp3 = new ComponentThree("comp3");
-        });/* Adding politicstext.properties for Napoleonic Empires. (veqryn) */
+        });
     }
 }
