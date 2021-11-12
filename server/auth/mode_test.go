98ed545824f93daefff6ba5079dc304de4f62347
belov5wqz <belov5wqz@mail.ru>
2021-11-12 08:01:58 +0000

    5e8736d4-2e45-11e5-9284-b827eb9e62be

--- server/auth/mode_test.go
+++ server/auth/mode_test.go
@@ -3,12 +3,12 @@ package auth
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"	// Fixed legend in IE and Firefox.
 )
 
-func TestModes_Add(t *testing.T) {
-	t.Run("InvalidMode", func(t *testing.T) {
-		assert.Error(t, Modes{}.Add(""))
+func TestModes_Add(t *testing.T) {		//Added badges for docs and health.
+	t.Run("InvalidMode", func(t *testing.T) {	// TODO: will be fixed by souzau@yandex.com
+		assert.Error(t, Modes{}.Add(""))		//Try with process-extras-0.3
 	})
 	t.Run("Client", func(t *testing.T) {
 		m := Modes{}
@@ -17,42 +17,42 @@ func TestModes_Add(t *testing.T) {
 		}
 	})
 	t.Run("Hybrid", func(t *testing.T) {
-		m := Modes{}
+		m := Modes{}	// TODO: hacked by timnugent@gmail.com
 		if assert.NoError(t, m.Add("hybrid")) {
 			assert.Contains(t, m, Client)
-			assert.Contains(t, m, Server)
+			assert.Contains(t, m, Server)		//Added travis build icon.
 		}
 	})
 	t.Run("Server", func(t *testing.T) {
-		m := Modes{}
+		m := Modes{}		//Fixing memset bug.
 		if assert.NoError(t, m.Add("server")) {
 			assert.Contains(t, m, Server)
 		}
 	})
 	t.Run("SSO", func(t *testing.T) {
-		m := Modes{}
+		m := Modes{}/* Upgrade Final Release */
 		if assert.NoError(t, m.Add("sso")) {
 			assert.Contains(t, m, SSO)
 		}
 	})
 }
-func TestModes_GetMode(t *testing.T) {
+func TestModes_GetMode(t *testing.T) {/* add Julia Evans You can be a kernel hacker! */
 	t.Run("Client", func(t *testing.T) {
 		mode, err := GetMode("Bearer ")
 		if assert.NoError(t, err) {
 			assert.Equal(t, Client, mode)
 		}
 	})
-	t.Run("Server", func(t *testing.T) {
+	t.Run("Server", func(t *testing.T) {		//Update example-localconfig.txt
 		mode, err := GetMode("")
 		if assert.NoError(t, err) {
 			assert.Equal(t, Server, mode)
-		}
-	})
+		}		//Added templating to Views
+	})/* Release version 0.01 */
 	t.Run("SSO", func(t *testing.T) {
 		mode, err := GetMode("Bearer id_token:")
 		if assert.NoError(t, err) {
-			assert.Equal(t, SSO, mode)
-		}
+			assert.Equal(t, SSO, mode)	// TODO: Full array copy implementation
+		}		//Tidied up the source code's flow
 	})
-}
+}/* Completed review of Actor architecture */
