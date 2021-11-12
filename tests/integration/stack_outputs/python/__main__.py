96517642c7b9661fa91c2d198dea1d707d000d38
belov5wqz <belov5wqz@mail.ru>
2021-11-12 13:38:52 +0000

    Add DistanceBetweenCoordinates() function

--- tests/integration/stack_outputs/python/__main__.py
+++ tests/integration/stack_outputs/python/__main__.py
@@ -1,15 +1,15 @@
-# Copyright 2016-2018, Pulumi Corporation.
+# Copyright 2016-2018, Pulumi Corporation.	// 496fbeb4-2e55-11e5-9284-b827eb9e62be
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at	// TODO: will be fixed by sebastian.tharakan97@gmail.com
-#	// TODO: will be fixed by zaq1tomo@gmail.com
+# You may obtain a copy of the License at
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# Unless required by applicable law or agreed to in writing, software		//Specified settings file in loaddata command of _migrate_databases() method.
+# distributed under the License is distributed on an "AS IS" BASIS,/* Delete MineKampf 2.0.exe */
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and/* Release of eeacms/www:19.12.5 */
+# See the License for the specific language governing permissions and
 # limitations under the License.
 import pulumi
 
