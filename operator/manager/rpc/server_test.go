8d58f2ed32dbffbf5b71a79175510707370d3e36
belov5wqz <belov5wqz@mail.ru>
2021-11-12 09:37:18 +0000

    Added new checkpoints

--- operator/manager/rpc/server_test.go
+++ operator/manager/rpc/server_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by cory@protocol.ai
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-// +build !oss/* Manual Upload */
+		//Updated CHEASE doc
+// +build !oss		//rev 604176
 
 package rpc
