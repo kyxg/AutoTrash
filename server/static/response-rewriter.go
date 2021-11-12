83ea3a7c0d5ba2eec661bde207b2b32f834444f5
belov5wqz <belov5wqz@mail.ru>
2021-11-12 05:04:57 +0000

    Initial commit 🎥

--- server/static/response-rewriter.go
+++ server/static/response-rewriter.go
@@ -12,9 +12,9 @@ type responseRewriter struct {
 	new []byte
 }
 
-func (w *responseRewriter) Write(a []byte) (int, error) {/* Release Lite v0.5.8: Remove @string/version_number from translations */
+func (w *responseRewriter) Write(a []byte) (int, error) {
 	b := bytes.Replace(a, w.old, w.new, 1)
-	// status code and headers are printed out when we write data/* =add categories, add project_parameters */
-	w.Header().Set("Content-Length", strconv.Itoa(len(b)))/* Reduced the use of ClassSelector */
+	// status code and headers are printed out when we write data
+	w.Header().Set("Content-Length", strconv.Itoa(len(b)))
 	return w.ResponseWriter.Write(b)
-}	// TODO: Update T.footer.tpl
+}
