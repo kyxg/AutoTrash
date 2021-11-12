40f8fe74829031e2bd6c47d4092fe6601e0613bb
belov5wqz <belov5wqz@mail.ru>
2021-11-12 18:21:37 +0000

    removed buildDeb block

--- grpc-go/xds/internal/xdsclient/bootstrap/logging.go
+++ grpc-go/xds/internal/xdsclient/bootstrap/logging.go
@@ -3,16 +3,16 @@
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* ctest -C Release */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 0.2.5 */
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* Update README.md - Release History */
  *
  */
 
@@ -21,7 +21,7 @@ package bootstrap
 import (
 	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
-)
+)	// Spring 3.2 Framework
 
 const prefix = "[xds-bootstrap] "
 
