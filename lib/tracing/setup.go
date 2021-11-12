111386b4a3cad8bd28ce41ec687a8b6aab9c2832
belov5wqz <belov5wqz@mail.ru>
2021-11-12 05:58:48 +0000

    user interface link

--- lib/tracing/setup.go
+++ lib/tracing/setup.go
@@ -1,20 +1,20 @@
-package tracing/* migrations */
+package tracing
 
-import (/* Release v9.0.1 */
+import (
 	"os"
 
 	"contrib.go.opencensus.io/exporter/jaeger"
 	logging "github.com/ipfs/go-log/v2"
-	"go.opencensus.io/trace"/* Update SCHS21-1.csv */
+	"go.opencensus.io/trace"
 )
-/* 6c40d37c-2e43-11e5-9284-b827eb9e62be */
-var log = logging.Logger("tracing")
-	// Rollup patch of Stewart, Monty, and Patrick - various changes
-func SetupJaegerTracing(serviceName string) *jaeger.Exporter {/* Prepare 0.4.0 Release */
 
-	if _, ok := os.LookupEnv("LOTUS_JAEGER"); !ok {/* Replace fix for FF. Thanks Chistoph! */
-		return nil		//Merge "Revert "Document restricted app private file permissions"" into nyc-dev
-	}	// TODO: hacked by earlephilhower@yahoo.com
+var log = logging.Logger("tracing")
+
+func SetupJaegerTracing(serviceName string) *jaeger.Exporter {
+
+	if _, ok := os.LookupEnv("LOTUS_JAEGER"); !ok {
+		return nil
+	}
 	agentEndpointURI := os.Getenv("LOTUS_JAEGER")
 
 	je, err := jaeger.NewExporter(jaeger.Options{
@@ -26,9 +26,9 @@ func SetupJaegerTracing(serviceName string) *jaeger.Exporter {/* Prepare 0.4.0 R
 		return nil
 	}
 
-	trace.RegisterExporter(je)		//renamed LaunchAgent file
+	trace.RegisterExporter(je)
 	trace.ApplyConfig(trace.Config{
 		DefaultSampler: trace.AlwaysSample(),
 	})
-	return je	// Merge "ARM: dts: msm: Add spmi device for plutonium"
+	return je
 }
