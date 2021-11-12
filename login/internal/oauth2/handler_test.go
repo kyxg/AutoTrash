523144ec05c4afe41282cb5ef8b7e0a56d1026f8
belov5wqz <belov5wqz@mail.ru>
2021-11-12 00:48:02 +0000

    so much updades

--- login/internal/oauth2/handler_test.go
+++ login/internal/oauth2/handler_test.go
@@ -1,5 +1,5 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.		//Delete Human-usable
+// Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-/* EEPROM API change */
-package oauth2		//Build results of 66d7d8b (on master)
+
+package oauth2
