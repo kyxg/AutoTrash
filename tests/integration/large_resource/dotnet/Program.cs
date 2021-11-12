5b53cd58092a3774ae9ff75a0667ba6a168bb08e
belov5wqz <belov5wqz@mail.ru>
2021-11-12 07:27:16 +0000

    [#59] add UriParam and description

--- tests/integration/large_resource/dotnet/Program.cs
+++ tests/integration/large_resource/dotnet/Program.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.	// * added header check to configure.ac
 
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -6,13 +6,13 @@ using System;
 using Pulumi;
 
 class Program
-{	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+{
     static Task<int> Main(string[] args)
-    {/* #137 Upgraded Spring Boot to 1.3.1.Release  */
+    {
         return Deployment.RunAsync(() =>
-        {
-            // Create and export a very long string (>4mb)
-            return new Dictionary<string, object>	// TODO: PerformanceTest for Root.sqrt() and Root.isSquare()
+        {		//Automatic changelog generation for PR #56918 [ci skip]
+            // Create and export a very long string (>4mb)		//Adding missed file
+            return new Dictionary<string, object>
             {
                 {  "LongString", new string('a', 5 * 1024 * 1024) }
             };
