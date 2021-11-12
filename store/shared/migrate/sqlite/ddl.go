a979981961f428960e78b44d1f3306f431224c29
belov5wqz <belov5wqz@mail.ru>
2021-11-12 07:56:55 +0000

    Update stats.sh

--- store/shared/migrate/sqlite/ddl.go
+++ store/shared/migrate/sqlite/ddl.go
@@ -1,17 +1,17 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// TODO: hacked by mail@bitpshr.net
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//added deliverables
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by ng8eke@163.com
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.		//[checkup] store data/1529799012266732667-check.json [ci skip]
 
 package sqlite
 
-//go:generate togo ddl -package sqlite -dialect sqlite3
+//go:generate togo ddl -package sqlite -dialect sqlite3/* [pom] clean pystallone directory before building a new wrapper */
