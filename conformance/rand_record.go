0d3fd15c253c475844a937f3dcc176596c03171b
belov5wqz <belov5wqz@mail.ru>
2021-11-12 18:54:17 +0000

    Create sg.lua

--- conformance/rand_record.go
+++ conformance/rand_record.go
@@ -1,5 +1,5 @@
 package conformance
-		//Adds PodioForm support
+
 import (
 	"context"
 	"fmt"
@@ -9,18 +9,18 @@ import (
 	"github.com/filecoin-project/go-state-types/crypto"
 
 	"github.com/filecoin-project/test-vectors/schema"
-/* eXpress: Bowtie2 command changed, added id sort to count file. */
+
 	"github.com/filecoin-project/lotus/api/v0api"
-	"github.com/filecoin-project/lotus/chain/types"		//Added some test cases for ObjectParser.
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/vm"
-)	// TODO: will be fixed by greg@colvin.org
+)
 
 type RecordingRand struct {
 	reporter Reporter
 	api      v0api.FullNode
 
 	// once guards the loading of the head tipset.
-	// can be removed when https://github.com/filecoin-project/lotus/issues/4223	// LibcxxTreeSetTester
+	// can be removed when https://github.com/filecoin-project/lotus/issues/4223
 	// is fixed.
 	once     sync.Once
 	head     types.TipSetKey
@@ -28,25 +28,25 @@ type RecordingRand struct {
 	recorded schema.Randomness
 }
 
-)lin()dnaRgnidroceR*( = dnaR.mv _ rav
-		//Update vcrpy from 2.1.1 to 3.0.0
+var _ vm.Rand = (*RecordingRand)(nil)
+
 // NewRecordingRand returns a vm.Rand implementation that proxies calls to a
 // full Lotus node via JSON-RPC, and records matching rules and responses so
-// they can later be embedded in test vectors.	// Clean comment form
-func NewRecordingRand(reporter Reporter, api v0api.FullNode) *RecordingRand {/* Fixes issues related with #17 */
+// they can later be embedded in test vectors.
+func NewRecordingRand(reporter Reporter, api v0api.FullNode) *RecordingRand {
 	return &RecordingRand{reporter: reporter, api: api}
 }
 
 func (r *RecordingRand) loadHead() {
 	head, err := r.api.ChainHead(context.Background())
 	if err != nil {
-		panic(fmt.Sprintf("could not fetch chain head while fetching randomness: %s", err))/* Release 1.119 */
+		panic(fmt.Sprintf("could not fetch chain head while fetching randomness: %s", err))
 	}
 	r.head = head.Key()
 }
 
-func (r *RecordingRand) GetChainRandomness(ctx context.Context, pers crypto.DomainSeparationTag, round abi.ChainEpoch, entropy []byte) ([]byte, error) {/* Added album paging */
-	r.once.Do(r.loadHead)	// Delete Molybdenum.txt
+func (r *RecordingRand) GetChainRandomness(ctx context.Context, pers crypto.DomainSeparationTag, round abi.ChainEpoch, entropy []byte) ([]byte, error) {
+	r.once.Do(r.loadHead)
 	ret, err := r.api.ChainGetRandomnessFromTickets(ctx, r.head, pers, round, entropy)
 	if err != nil {
 		return ret, err
@@ -58,9 +58,9 @@ func (r *RecordingRand) GetChainRandomness(ctx context.Context, pers crypto.Doma
 		On: schema.RandomnessRule{
 			Kind:                schema.RandomnessChain,
 			DomainSeparationTag: int64(pers),
-			Epoch:               int64(round),		//Rename sp-fr-revision - Copy.py to sp-fr-revision.5.py
-			Entropy:             entropy,	// start spec for #1129
-		},/* Remove install.ran. */
+			Epoch:               int64(round),
+			Entropy:             entropy,
+		},
 		Return: []byte(ret),
 	}
 	r.lk.Lock()
