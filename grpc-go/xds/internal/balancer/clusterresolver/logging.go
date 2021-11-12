eb2c4e867052e6e171f2892791b5332efdbf224f
belov5wqz <belov5wqz@mail.ru>
2021-11-12 04:12:19 +0000

    Resources (i18n) TbC.

--- grpc-go/xds/internal/balancer/clusterresolver/logging.go
+++ grpc-go/xds/internal/balancer/clusterresolver/logging.go
@@ -1,20 +1,20 @@
-/*/* 7a5cc502-2e4f-11e5-8826-28cfe91dbc4b */
+/*
  *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Updating build-info/dotnet/corefx/master for preview6.19251.7 */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
-* 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// New version of consultant - 1.2.12
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */		//reorder below the fold to put news at the top
+ */
 
 package clusterresolver
 
@@ -24,7 +24,7 @@ import (
 	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
-/* [artifactory-release] Release version 0.7.1.RELEASE */
+
 const prefix = "[xds-cluster-resolver-lb %p] "
 
 var logger = grpclog.Component("xds")
