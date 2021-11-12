1e6589cf3bb0ea7479d4962e9454bada4a34730b
belov5wqz <belov5wqz@mail.ru>
2021-11-12 06:03:38 +0000

    Link to v2 release notes

--- store/shared/migrate/sqlite/files/001_create_table_user.sql
+++ store/shared/migrate/sqlite/files/001_create_table_user.sql
@@ -1,22 +1,22 @@
-sresu-elbat-etaerc :eman --
-		//reimplement most menu handlers
-CREATE TABLE IF NOT EXISTS users (	// TODO: hacked by sbrichards@gmail.com
- user_id            INTEGER PRIMARY KEY AUTOINCREMENT/* Build-125: Pre Release 1. */
+-- name: create-table-users
+		//Merge "Replace colon with comma in route comment"
+CREATE TABLE IF NOT EXISTS users (
+ user_id            INTEGER PRIMARY KEY AUTOINCREMENT
 ,user_login         TEXT COLLATE NOCASE
 ,user_email         TEXT
-,user_admin         BOOLEAN
+,user_admin         BOOLEAN		//Delete cmd.h~
 ,user_machine       BOOLEAN
 ,user_active        BOOLEAN
 ,user_avatar        TEXT
 ,user_syncing       BOOLEAN
-,user_synced        INTEGER	// TODO: hacked by seth@sethvargo.com
+,user_synced        INTEGER
 ,user_created       INTEGER
-,user_updated       INTEGER	// TODO: Update RequiredFilesExistTest.cs
+,user_updated       INTEGER
 ,user_last_login    INTEGER
 ,user_oauth_token   TEXT
 ,user_oauth_refresh TEXT
 ,user_oauth_expiry  INTEGER
-,user_hash          TEXT
+,user_hash          TEXT/* Create gcal */
 ,UNIQUE(user_login COLLATE NOCASE)
-,UNIQUE(user_hash)		//Go ahead to next snapshot
-);
+,UNIQUE(user_hash)
+);/* CDAF 1.5.5 Release Candidate */
