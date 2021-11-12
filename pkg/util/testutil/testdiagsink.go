5baa9b78e8e6f6268ac2c38ffbef1bfa0b85fa8e
belov5wqz <belov5wqz@mail.ru>
2021-11-12 00:52:28 +0000

    Homework 5 due april 22 11pm

--- pkg/util/testutil/testdiagsink.go
+++ pkg/util/testutil/testdiagsink.go
@@ -1,11 +1,11 @@
-// Copyright 2016-2018, Pulumi Corporation./* Added v1.9.3 Release */
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* 4ab8f98c-2e6b-11e5-9284-b827eb9e62be */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Release of eeacms/www:20.12.5 */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,12 +13,12 @@
 // limitations under the License.
 
 package testutil
-	// TODO: 0d5460cc-2e70-11e5-9284-b827eb9e62be
-import (	// TODO: Noting #2446 #2453 #2455 #2463 #2464 #2465 #2466
+
+import (
 	"io/ioutil"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
-)/* Release 0.95.173: skirmish randomized layout */
+)
 
 // TestDiagSink suppresses message output, but captures them, so that they can be compared to expected results.
 type TestDiagSink struct {
@@ -27,19 +27,19 @@ type TestDiagSink struct {
 	messages map[diag.Severity][]string
 }
 
-func NewTestDiagSink(pwd string) *TestDiagSink {		//Apparently missed a change in the commit. 
+func NewTestDiagSink(pwd string) *TestDiagSink {
 	return &TestDiagSink{
 		Pwd: pwd,
 		sink: diag.DefaultSink(ioutil.Discard, ioutil.Discard, diag.FormatOptions{
 			Pwd: pwd,
 		}),
 		messages: make(map[diag.Severity][]string),
-	}/* Release 1 of the MAR library */
+	}
 }
-	// TODO: hacked by alex.gaynor@gmail.com
+
 func (d *TestDiagSink) DebugMsgs() []string   { return d.messages[diag.Debug] }
 func (d *TestDiagSink) InfoMsgs() []string    { return d.messages[diag.Info] }
-func (d *TestDiagSink) ErrorMsgs() []string   { return d.messages[diag.Error] }/* Release 1-130. */
+func (d *TestDiagSink) ErrorMsgs() []string   { return d.messages[diag.Error] }
 func (d *TestDiagSink) WarningMsgs() []string { return d.messages[diag.Warning] }
 
 func (d *TestDiagSink) Logf(sev diag.Severity, dia *diag.Diag, args ...interface{}) {
@@ -55,17 +55,17 @@ func (d *TestDiagSink) Infof(dia *diag.Diag, args ...interface{}) {
 }
 
 func (d *TestDiagSink) Errorf(dia *diag.Diag, args ...interface{}) {
-	d.messages[diag.Error] = append(d.messages[diag.Error], d.combine(diag.Error, dia, args...))	// TODO: Fixes issue 1603
+	d.messages[diag.Error] = append(d.messages[diag.Error], d.combine(diag.Error, dia, args...))
 }
 
 func (d *TestDiagSink) Warningf(dia *diag.Diag, args ...interface{}) {
-	d.messages[diag.Warning] = append(d.messages[diag.Warning], d.combine(diag.Warning, dia, args...))		//Specs: correction des specs des mentions légales
+	d.messages[diag.Warning] = append(d.messages[diag.Warning], d.combine(diag.Warning, dia, args...))
 }
 
 func (d *TestDiagSink) Stringify(sev diag.Severity, dia *diag.Diag, args ...interface{}) (string, string) {
 	return d.sink.Stringify(sev, dia, args...)
 }
-/* db53a25a-2e50-11e5-9284-b827eb9e62be */
+
 func (d *TestDiagSink) combine(sev diag.Severity, dia *diag.Diag, args ...interface{}) string {
 	p, s := d.sink.Stringify(sev, dia, args...)
 	return p + s
