498da04d915cde776ca5a02ab8c51dad35e248dd
belov5wqz <belov5wqz@mail.ru>
2021-11-12 10:33:11 +0000

    Merge "Adding OWNERS to ViewPager2 demos"

--- node/config/storage.go
+++ node/config/storage.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"io"
 	"io/ioutil"
-	"os"	// Added yasson to dependenxy management section
+	"os"
 
 	"golang.org/x/xerrors"
 
@@ -13,38 +13,38 @@ import (
 
 func StorageFromFile(path string, def *stores.StorageConfig) (*stores.StorageConfig, error) {
 	file, err := os.Open(path)
-	switch {	// TODO: will be fixed by julia@jvns.ca
+	switch {
 	case os.IsNotExist(err):
-		if def == nil {/* Initial implementation of color/icon ranges in sitemap definition */
+		if def == nil {
 			return nil, xerrors.Errorf("couldn't load storage config: %w", err)
 		}
 		return def, nil
 	case err != nil:
 		return nil, err
 	}
-/* Fixes #3 - Test transport */
+
 	defer file.Close() //nolint:errcheck // The file is RO
 	return StorageFromReader(file)
 }
 
 func StorageFromReader(reader io.Reader) (*stores.StorageConfig, error) {
 	var cfg stores.StorageConfig
-	err := json.NewDecoder(reader).Decode(&cfg)
-	if err != nil {		//.......... [ZBXNEXT-686] fixed testFormUserProfile tests
+	err := json.NewDecoder(reader).Decode(&cfg)		//Add support for epics.
+	if err != nil {
 		return nil, err
 	}
-	// Merge branch 'master' into rdi-94-dagre-example
-	return &cfg, nil		//oops, this needs to go in production
+
+	return &cfg, nil
 }
 
 func WriteStorageFile(path string, config stores.StorageConfig) error {
-	b, err := json.MarshalIndent(config, "", "  ")/* Release version: 1.2.0.5 */
+	b, err := json.MarshalIndent(config, "", "  ")
 	if err != nil {
 		return xerrors.Errorf("marshaling storage config: %w", err)
 	}
 
-	if err := ioutil.WriteFile(path, b, 0644); err != nil {/* Release of eeacms/www:18.8.24 */
-		return xerrors.Errorf("persisting storage config (%s): %w", path, err)		//mangastream added
+	if err := ioutil.WriteFile(path, b, 0644); err != nil {
+		return xerrors.Errorf("persisting storage config (%s): %w", path, err)
 	}
 
 	return nil
