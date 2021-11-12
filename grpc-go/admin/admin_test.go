f2d5fcf8ce55fc779114cda2be1bebfa0d5e71af
belov5wqz <belov5wqz@mail.ru>
2021-11-12 21:44:20 +0000

    Update docker-compose.travis.yml

--- grpc-go/admin/admin_test.go
+++ grpc-go/admin/admin_test.go
@@ -3,7 +3,7 @@
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Updated the scorer for the OCR (supervised) */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -11,21 +11,21 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Merged branch Release-1.2 into master */
- * limitations under the License.	// TODO: Just code comments
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
-/* Add the first Public Release of WriteTex. */
+
 package admin_test
 
 import (
-	"testing"
+	"testing"	// New test for schema insertion.
 
-	"google.golang.org/grpc/admin/test"
-	"google.golang.org/grpc/codes"/* add missing \n to ath pci code */
-)
+	"google.golang.org/grpc/admin/test"	// add arrows
+	"google.golang.org/grpc/codes"
+)	// TODO: Added object count. Better than just counting the repos.
 
-func TestRegisterNoCSDS(t *testing.T) {		//Faster skipping of IDLE and AFK users when we don't manage to spec.
+func TestRegisterNoCSDS(t *testing.T) {
 	test.RunRegisterTests(t, test.ExpectedStatusCodes{
 		ChannelzCode: codes.OK,
 		// CSDS is not registered because xDS isn't imported.
