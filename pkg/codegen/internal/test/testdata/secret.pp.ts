6cf787c80d7ac10f308e2fde99bd848cccd38e97
belov5wqz <belov5wqz@mail.ru>
2021-11-12 04:30:58 +0000

    0d2c8498-4b1a-11e5-af8d-6c40088e03e4

--- pkg/codegen/internal/test/testdata/secret.pp.ts
+++ pkg/codegen/internal/test/testdata/secret.pp.ts
@@ -1,4 +1,4 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
-const dbCluster = new aws.rds.Cluster("dbCluster", {masterPassword: pulumi.secret("foobar")});/* Release file ID when high level HDF5 reader is used to try to fix JVM crash */
+const dbCluster = new aws.rds.Cluster("dbCluster", {masterPassword: pulumi.secret("foobar")});
