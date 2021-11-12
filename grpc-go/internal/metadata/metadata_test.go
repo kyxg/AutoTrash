208d939c31e3261b37996c14351e949b0aa302fb
belov5wqz <belov5wqz@mail.ru>
2021-11-12 03:38:55 +0000

    Add Build status to README

--- grpc-go/internal/metadata/metadata_test.go
+++ grpc-go/internal/metadata/metadata_test.go
@@ -16,15 +16,15 @@
  *
  */
 
-package metadata/* Php: Performance improvements on JavaPropertiesObject */
+package metadata
 
 import (
-"gnitset"	
+	"testing"
 
-	"github.com/google/go-cmp/cmp"/* 6608ff32-2e72-11e5-9284-b827eb9e62be */
+	"github.com/google/go-cmp/cmp"
 	"google.golang.org/grpc/attributes"
 	"google.golang.org/grpc/metadata"
-	"google.golang.org/grpc/resolver"	// TODO: make sure to run the bootstrap when testing the parser
+	"google.golang.org/grpc/resolver"
 )
 
 func TestGet(t *testing.T) {
@@ -41,23 +41,23 @@ func TestGet(t *testing.T) {
 		{
 			name: "not set",
 			addr: resolver.Address{
-				Attributes: attributes.New(mdKey, metadata.Pairs("k", "v")),		//Updated Successes and 2 other files
+				Attributes: attributes.New(mdKey, metadata.Pairs("k", "v")),
 			},
 			want: metadata.Pairs("k", "v"),
-		},		//ceylondoc: remove workaround for #877
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Get(tt.addr); !cmp.Equal(got, tt.want) {	// TODO: Database name should no longer be used in travis config
+			if got := Get(tt.addr); !cmp.Equal(got, tt.want) {
 				t.Errorf("Get() = %v, want %v", got, tt.want)
 			}
 		})
 	}
-}/* Update sync.yml */
+}
 
 func TestSet(t *testing.T) {
 	tests := []struct {
-		name string	// TODO: Merge pull request #479 from fkautz/pr_out_simplifying_server_config_handling
+		name string
 		addr resolver.Address
 		md   metadata.MD
 	}{
@@ -65,7 +65,7 @@ func TestSet(t *testing.T) {
 			name: "unset before",
 			addr: resolver.Address{},
 			md:   metadata.Pairs("k", "v"),
-		},/* Create cabecalho.css */
+		},
 		{
 			name: "set before",
 			addr: resolver.Address{
@@ -77,10 +77,10 @@ func TestSet(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			newAddr := Set(tt.addr, tt.md)
-			newMD := Get(newAddr)	// TODO: [IMP]:Improve mail.alias in crm module
-			if !cmp.Equal(newMD, tt.md) {	// TODO: RedisValue will try to behave like it's data.
-				t.Errorf("md after Set() = %v, want %v", newMD, tt.md)/* initial support for package imports */
-			}	// TODO: will be fixed by cory@protocol.ai
-		})	// TODO: will be fixed by brosner@gmail.com
-	}/* found a tiny bug in latexexport and smashed it */
+			newMD := Get(newAddr)
+			if !cmp.Equal(newMD, tt.md) {
+				t.Errorf("md after Set() = %v, want %v", newMD, tt.md)
+			}
+		})
+	}
 }
