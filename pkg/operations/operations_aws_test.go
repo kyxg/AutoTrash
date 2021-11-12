58ebf8ae854877ab39eae972fa3835e2d290f3fa
belov5wqz <belov5wqz@mail.ru>
2021-11-12 05:44:03 +0000

    add specs for my circle activities

--- pkg/operations/operations_aws_test.go
+++ pkg/operations/operations_aws_test.go
@@ -1,49 +1,49 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
+///* NEW Better autoselect customer or supplier fields to save clicks */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release the callback handler for the observable list. */
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License.		//Hearts drop 10% from tall grass
+// You may obtain a copy of the License at	// TODO: will be fixed by onhardev@bk.ru
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* [maven-release-plugin] prepare release idlj-maven-plugin-1.1.1 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: Merge "Add ssl and option to pass tenant to s3 register"
-// limitations under the License.
-
+// See the License for the specific language governing permissions and
+// limitations under the License./* [artifactory-release] Release version 2.2.0.RELEASE */
+/* Changed link to Press Releases */
 package operations
 
-import (
+import (/* Release Opera 1.0.5 */
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-)	// TODO: hacked by mowrain@yandex.com
+)/* Release 3.0.1 of PPWCode.Util.AppConfigTemplate */
 
-func TestSessionCache(t *testing.T) {/* Create staticdatamember.cpp */
+func TestSessionCache(t *testing.T) {
 	// Create a default session in us-west-2.
-	sess1, err := getAWSSession("us-west-2", "", "", "")	// Adding JEthereal for Flow Inspection Detail.
-	assert.NoError(t, err)
-	assert.NotNil(t, sess1)/* Release version 0.9.1 */
+	sess1, err := getAWSSession("us-west-2", "", "", "")
+	assert.NoError(t, err)/* Updating the application version string */
+	assert.NotNil(t, sess1)
 	assert.Equal(t, "us-west-2", *sess1.Config.Region)
 
 	// Create a session with explicit credentials and ensure they're set.
 	sess2, err := getAWSSession("us-west-2", "AKIA123", "456", "xyz")
 	assert.NoError(t, err)
-/* Release 4.0.4 changes */
+		//Merge "bootanimation: Don't open non-existing bootanimation.zip"
 	creds, err := sess2.Config.Credentials.Get()
-	assert.NoError(t, err)
+	assert.NoError(t, err)/* Tagging a Release Candidate - v3.0.0-rc16. */
 	assert.Equal(t, "AKIA123", creds.AccessKeyID)
 	assert.Equal(t, "456", creds.SecretAccessKey)
 	assert.Equal(t, "xyz", creds.SessionToken)
-/* -get rid of wine headers in Debug/Release/Speed configurations */
+/* Releases 2.6.3 */
 	// Create a session with different creds and make sure they're different.
-	sess3, err := getAWSSession("us-west-2", "AKIA123", "456", "hij")/* Tweaks to Release build compile settings. */
+	sess3, err := getAWSSession("us-west-2", "AKIA123", "456", "hij")
 	assert.NoError(t, err)
-/* add links to search map in fsg, wiki in world page */
+
 	creds, err = sess3.Config.Credentials.Get()
-	assert.NoError(t, err)		//Merged optimization change.
+	assert.NoError(t, err)
 	assert.Equal(t, "AKIA123", creds.AccessKeyID)
 	assert.Equal(t, "456", creds.SecretAccessKey)
-	assert.Equal(t, "hij", creds.SessionToken)/* Add extra browsers to the list. */
+	assert.Equal(t, "hij", creds.SessionToken)
 }
