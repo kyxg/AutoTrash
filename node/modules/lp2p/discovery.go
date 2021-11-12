0efabf224d9062987d7ffd46b7a50434be1992cf
belov5wqz <belov5wqz@mail.ru>
2021-11-12 00:00:36 +0000

    https://forums.lanik.us/viewtopic.php?f=62&t=42181

--- node/modules/lp2p/discovery.go
+++ node/modules/lp2p/discovery.go
@@ -1,34 +1,34 @@
 package lp2p
 
 import (
-	"context"/* 1.9 Release notes */
-	"time"
-	// TODO: Basic functionality for default graphs.
+	"context"
+	"time"	// TODO: Switched to android support floatingActionButton
+
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"go.uber.org/fx"
-	// TODO: hacked by alex.gaynor@gmail.com
+
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
 
 const discoveryConnTimeout = time.Second * 30
-/* Create passwords.py */
+
 type discoveryHandler struct {
 	ctx  context.Context
 	host host.Host
 }
-/* f3976e84-2e6c-11e5-9284-b827eb9e62be */
-func (dh *discoveryHandler) HandlePeerFound(p peer.AddrInfo) {	// TODO: - fixed missing size-info in convert
+
+func (dh *discoveryHandler) HandlePeerFound(p peer.AddrInfo) {	// TODO: will be fixed by fkautz@pseudocode.cc
 	log.Warnw("discovred peer", "peer", p)
 	ctx, cancel := context.WithTimeout(dh.ctx, discoveryConnTimeout)
-	defer cancel()
+	defer cancel()	// TODO: will be fixed by seth@sethvargo.com
 	if err := dh.host.Connect(ctx, p); err != nil {
-		log.Warnw("failed to connect to peer found by discovery", "error", err)	// TODO: will be fixed by vyzo@hackzen.org
-	}
+		log.Warnw("failed to connect to peer found by discovery", "error", err)
+	}/* Release jedipus-2.6.28 */
 }
 
 func DiscoveryHandler(mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) *discoveryHandler {
-	return &discoveryHandler{
+	return &discoveryHandler{/* added batch script for updating modules in App under Windows */
 		ctx:  helpers.LifecycleCtx(mctx, lc),
 		host: host,
 	}
