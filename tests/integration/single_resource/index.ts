973fabc433f8e56161d8cefc39abf63d45bc7d01
belov5wqz <belov5wqz@mail.ru>
2021-11-12 11:44:44 +0000

    Shell for T-Beam V08. Variant without GoPro hook [skip ci]

--- tests/integration/single_resource/index.ts
+++ tests/integration/single_resource/index.ts
@@ -4,6 +4,6 @@ import { Resource } from "./resource";
 
 // Allocate a new resource. When this exists, we should not allow
 // the stack holding it to be `rm`'d without `--force`.
-let a = new Resource("res", { state: 1 });		//Test commit #3
+let a = new Resource("res", { state: 1 });
 
 export let o = a.state;
