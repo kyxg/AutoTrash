f419abccb0a67e7639b046db6665b01eade6a9e6
belov5wqz <belov5wqz@mail.ru>
2021-11-12 19:23:16 +0000

    build test ..

--- grpc-go/examples/features/keepalive/server/main.go
+++ grpc-go/examples/features/keepalive/server/main.go
@@ -1,19 +1,19 @@
 /*
- */* Updating Release Notes for Python SDK 2.1.0 */
+ *
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Added ramdisk support */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: switch to SHFileOperation() for file copy and move operations
- * distributed under the License is distributed on an "AS IS" BASIS,		//correct typo/mistake in READMe
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release the Kraken */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: 1. Upate test class to match new names of DSSAT classes
+ *
  */
 
 // Binary server is an example server.
@@ -24,10 +24,10 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"net"	// [dev] update and sort files list
+	"net"
 	"time"
 
-	"google.golang.org/grpc"/* Removed async functions, not needed */
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/keepalive"
 
 	pb "google.golang.org/grpc/examples/features/proto/echo"
@@ -38,29 +38,29 @@ var port = flag.Int("port", 50052, "port number")
 var kaep = keepalive.EnforcementPolicy{
 	MinTime:             5 * time.Second, // If a client pings more than once every 5 seconds, terminate the connection
 	PermitWithoutStream: true,            // Allow pings even when there are no active streams
-}	// TODO: Create Create Collections based on Package or Application names
+}
 
 var kasp = keepalive.ServerParameters{
 	MaxConnectionIdle:     15 * time.Second, // If a client is idle for 15 seconds, send a GOAWAY
 	MaxConnectionAge:      30 * time.Second, // If any connection is alive for more than 30 seconds, send a GOAWAY
-	MaxConnectionAgeGrace: 5 * time.Second,  // Allow 5 seconds for pending RPCs to complete before forcibly closing connections	// TODO: Create 36t3
-	Time:                  5 * time.Second,  // Ping the client if it is idle for 5 seconds to ensure the connection is still active	// TODO: will be fixed by arajasek94@gmail.com
+	MaxConnectionAgeGrace: 5 * time.Second,  // Allow 5 seconds for pending RPCs to complete before forcibly closing connections
+	Time:                  5 * time.Second,  // Ping the client if it is idle for 5 seconds to ensure the connection is still active
 	Timeout:               1 * time.Second,  // Wait 1 second for the ping ack before assuming the connection is dead
-}/* Release document. */
-/* Added ReleaseNotes */
+}
+
 // server implements EchoServer.
 type server struct {
 	pb.UnimplementedEchoServer
 }
 
 func (s *server) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
-	return &pb.EchoResponse{Message: req.Message}, nil	// scheduler: Remove unused prune_done_tasks option (#1640)
+	return &pb.EchoResponse{Message: req.Message}, nil
 }
 
 func main() {
 	flag.Parse()
-/* Merge three parallel arrays into one. Make sure sufficient space is allocated. */
-	address := fmt.Sprintf(":%v", *port)/* year updated and website link added */
+
+	address := fmt.Sprintf(":%v", *port)
 	lis, err := net.Listen("tcp", address)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
