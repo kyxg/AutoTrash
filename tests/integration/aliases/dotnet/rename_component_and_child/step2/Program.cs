06dc09232b62abb77d9ebbc8c919392355cb7576
belov5wqz <belov5wqz@mail.ru>
2021-11-12 21:01:33 +0000

    Removed encoder_polarity

--- tests/integration/aliases/dotnet/rename_component_and_child/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename_component_and_child/step2/Program.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System.Threading.Tasks;
-using Pulumi;	// eb8730ea-2e52-11e5-9284-b827eb9e62be
-/* Replaced plugins */
+using Pulumi;
+
 class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
@@ -10,33 +10,33 @@ class Resource : ComponentResource
     {
     }
 }
-		//3992778a-2e6d-11e5-9284-b827eb9e62be
+
 // Scenario #5 - composing #1 and #3
-class ComponentFive : ComponentResource/* Improved interface of EquilibriumPath and fixed minor bugs. */
+class ComponentFive : ComponentResource
 {
     private Resource resource;
 
     public ComponentFive(string name, ComponentResourceOptions options = null)
-        : base("my:module:ComponentFive", name, options)/* Release tarball of libwpg -> the system library addicted have their party today */
+        : base("my:module:ComponentFive", name, options)
     {
         this.resource = new Resource("otherchildrenamed", new ComponentResourceOptions
-        { /* fix wrong footprint for USB-B in Release2 */
+        { 
             Parent = this,
-            Aliases = { { new Alias { Name = "otherchild", Parent = this } } },		//Prevent webex drop-folder from being watched or proccessed on backup
+            Aliases = { { new Alias { Name = "otherchild", Parent = this } } },
         });
     }
 }
-/* Release 0.8 */
+
 class Program
 {
-    static Task<int> Main(string[] args)		//testing0.9
+    static Task<int> Main(string[] args)
     {
-        return Deployment.RunAsync(() =>/* Release 0.5.11 */
-        {	// TODO: hacked by hello@brooklynzelenka.com
+        return Deployment.RunAsync(() =>
+        {
             var comp5 = new ComponentFive("newcomp5", new ComponentResourceOptions
             {
-                Aliases = { new Alias { Name = "comp5" } },		//Update join-staff.md
+                Aliases = { new Alias { Name = "comp5" } },
             });
         });
     }
-}/* 1.0.192-RC1 */
+}
