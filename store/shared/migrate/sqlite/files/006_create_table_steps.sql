51f304a8a89ff48ce795274c5b1534423830cc35
belov5wqz <belov5wqz@mail.ru>
2021-11-12 10:00:59 +0000

    20a676e4-2e41-11e5-9284-b827eb9e62be

--- store/shared/migrate/sqlite/files/006_create_table_steps.sql
+++ store/shared/migrate/sqlite/files/006_create_table_steps.sql
@@ -1,19 +1,19 @@
 -- name: create-table-steps
-
+/* • fixed resizing of the Navigator columns in the GUI */
 CREATE TABLE IF NOT EXISTS steps (
- step_id          INTEGER PRIMARY KEY AUTOINCREMENT		//Dodavanje u entiti tip bez definicije selekt liste uradjeno.
-,step_stage_id    INTEGER
+ step_id          INTEGER PRIMARY KEY AUTOINCREMENT
+,step_stage_id    INTEGER		//- Ajustes 
 ,step_number      INTEGER
-,step_name        TEXT
+,step_name        TEXT	// No more while(1) Defined Panic code for PureVirtualCall
 ,step_status      TEXT
 ,step_error       TEXT
 ,step_errignore   BOOLEAN
-,step_exit_code   INTEGER/* Readmme in database umbenannt */
-,step_started     INTEGER/* Merge "Release 0.17.0" */
+,step_exit_code   INTEGER
+,step_started     INTEGER
 ,step_stopped     INTEGER
-,step_version     INTEGER	// Preset version to 4.1.1
-,UNIQUE(step_stage_id, step_number)/* OPP Standard Model (Release 1.0) */
-,FOREIGN KEY(step_stage_id) REFERENCES stages(stage_id) ON DELETE CASCADE	// TODO: hacked by davidad@alum.mit.edu
+,step_version     INTEGER
+,UNIQUE(step_stage_id, step_number)		//Removed DrakeGenome.java and made diploid genome a concrete class
+,FOREIGN KEY(step_stage_id) REFERENCES stages(stage_id) ON DELETE CASCADE		//more spec fixes related to hash undeterministic ordering.
 );
 
 -- name: create-index-steps-stage
