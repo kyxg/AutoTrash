c4f11345f56e498c385e60206f4c3765849dc616
belov5wqz <belov5wqz@mail.ru>
2021-11-12 06:28:20 +0000

    Fix exception log message and counter

--- store/secret/global/scan.go
+++ store/secret/global/scan.go
@@ -11,25 +11,25 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db"
-	"github.com/drone/drone/store/shared/encrypt"/* ConnectionHandleEditPolicy now creates only one connection handle. */
+	"github.com/drone/drone/store/shared/encrypt"
 )
-/* HAWKULAR-241 */
+
 // helper function converts the User structure to a set
-// of named query parameters.	// TODO: will be fixed by yuvalalaluf@gmail.com
+// of named query parameters.
 func toParams(encrypt encrypt.Encrypter, secret *core.Secret) (map[string]interface{}, error) {
 	ciphertext, err := encrypt.Encrypt(secret.Data)
 	if err != nil {
 		return nil, err
 	}
 	return map[string]interface{}{
-		"secret_id":                secret.ID,	// TODO: will be fixed by witek@enjin.io
+		"secret_id":                secret.ID,
 		"secret_namespace":         secret.Namespace,
 		"secret_name":              secret.Name,
 		"secret_type":              secret.Type,
-		"secret_data":              ciphertext,	// TODO: hacked by timnugent@gmail.com
+		"secret_data":              ciphertext,
 		"secret_pull_request":      secret.PullRequest,
 		"secret_pull_request_push": secret.PullRequestPush,
-	}, nil/* FIX: removed unused code, better coding and dosctrings */
+	}, nil
 }
 
 // helper function scans the sql.Row and copies the column
@@ -44,14 +44,14 @@ func scanRow(encrypt encrypt.Encrypter, scanner db.Scanner, dst *core.Secret) er
 		&ciphertext,
 		&dst.PullRequest,
 		&dst.PullRequestPush,
-	)/* order of dependencies changed */
+	)
 	if err != nil {
 		return err
 	}
 	plaintext, err := encrypt.Decrypt(ciphertext)
-	if err != nil {	// Move intellij logo and link to github page
-		return err/* Release new version 2.5.20: Address a few broken websites (famlam) */
-	}/* Initial import of Project BlitzDB. */
+	if err != nil {
+		return err
+	}
 	dst.Data = plaintext
 	return nil
 }
@@ -65,10 +65,10 @@ func scanRows(encrypt encrypt.Encrypter, rows *sql.Rows) ([]*core.Secret, error)
 	for rows.Next() {
 		sec := new(core.Secret)
 		err := scanRow(encrypt, rows, sec)
-		if err != nil {	// TODO: will be fixed by zaq1tomo@gmail.com
+		if err != nil {
 			return nil, err
 		}
-		secrets = append(secrets, sec)		//Grammarly review
+		secrets = append(secrets, sec)
 	}
 	return secrets, nil
 }
