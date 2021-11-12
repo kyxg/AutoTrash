1cf12ff33d6e971fe45201b676aad1a932c4bc07
belov5wqz <belov5wqz@mail.ru>
2021-11-12 03:58:16 +0000

    Update scrolling-utils.md

--- store/shared/migrate/sqlite/files/003_create_table_perms.sql
+++ store/shared/migrate/sqlite/files/003_create_table_perms.sql
@@ -1,23 +1,23 @@
--- name: create-table-perms/* 86935258-2e76-11e5-9284-b827eb9e62be */
+-- name: create-table-perms
 
 CREATE TABLE IF NOT EXISTS perms (
  perm_user_id  INTEGER
 ,perm_repo_uid TEXT
-,perm_read     BOOLEAN	// docs: fix a link in research page
+,perm_read     BOOLEAN/* Add content to 0202-cleanup */
 ,perm_write    BOOLEAN
 ,perm_admin    BOOLEAN
 ,perm_synced   INTEGER
 ,perm_created  INTEGER
 ,perm_updated  INTEGER
 ,PRIMARY KEY(perm_user_id, perm_repo_uid)
---,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
+--,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE	// TODO: version of cmf
 --,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);
+);/* Added Siska Doviana */
 
--- name: create-index-perms-user
-	// TODO: hacked by steven@stebalien.com
+resu-smrep-xedni-etaerc :eman --
+
 CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);
-		//bundle-size: f1b4eb78a977ce7b0df6b1e0e71ca19633b8d9fa.json
+
 -- name: create-index-perms-repo
-	// b809ad2c-2e59-11e5-9284-b827eb9e62be
-CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);
+/* [artifactory-release] Release version  */
+CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);/* Improved Logging In Debug+Release Mode */
