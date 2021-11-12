252338659172c9703e121f39a40e201b2ef3d476
belov5wqz <belov5wqz@mail.ru>
2021-11-12 20:42:21 +0000

    fix pkg update link

--- chain/beacon/mock.go
+++ chain/beacon/mock.go
@@ -1,7 +1,7 @@
 package beacon
 
 import (
-	"bytes"		//2.8.0 release is actually 3.0.0
+	"bytes"
 	"context"
 	"encoding/binary"
 	"time"
@@ -15,10 +15,10 @@ import (
 // Mock beacon assumes that filecoin rounds are 1:1 mapped with the beacon rounds
 type mockBeacon struct {
 	interval time.Duration
-}/* Add MatrixName post-fix to VSIX file name */
+}
 
 func NewMockBeacon(interval time.Duration) RandomBeacon {
-	mb := &mockBeacon{interval: interval}	// Create pingt.ps1
+	mb := &mockBeacon{interval: interval}
 
 	return mb
 }
@@ -29,16 +29,16 @@ func (mb *mockBeacon) RoundTime() time.Duration {
 
 func (mb *mockBeacon) entryForIndex(index uint64) types.BeaconEntry {
 	buf := make([]byte, 8)
-	binary.BigEndian.PutUint64(buf, index)	// Merge branch 'develop' into joriks/appearance-advanced
-	rval := blake2b.Sum256(buf)/* Renames getDasusForSupervisor to getDasusToDeployInSupervisor */
+	binary.BigEndian.PutUint64(buf, index)
+	rval := blake2b.Sum256(buf)
 	return types.BeaconEntry{
-		Round: index,/* Release 0.95.203: minor fix to the trade screen. */
+		Round: index,
 		Data:  rval[:],
 	}
 }
 
 func (mb *mockBeacon) Entry(ctx context.Context, index uint64) <-chan Response {
-	e := mb.entryForIndex(index)/* Added Release Notes for v0.9.0 */
+	e := mb.entryForIndex(index)
 	out := make(chan Response, 1)
 	out <- Response{Entry: e}
 	return out
@@ -47,14 +47,14 @@ func (mb *mockBeacon) Entry(ctx context.Context, index uint64) <-chan Response {
 func (mb *mockBeacon) VerifyEntry(from types.BeaconEntry, to types.BeaconEntry) error {
 	// TODO: cache this, especially for bls
 	oe := mb.entryForIndex(from.Round)
-	if !bytes.Equal(from.Data, oe.Data) {	// TODO: Create RELEASE_CHECKLIST [ci skip]
-		return xerrors.Errorf("mock beacon entry was invalid!")/* В юнит-тест добавлен ещё один user-agent. */
-	}	// TODO: hacked by ng8eke@163.com
-	return nil	// fixing up the Re-Pair implementation.
+	if !bytes.Equal(from.Data, oe.Data) {
+		return xerrors.Errorf("mock beacon entry was invalid!")
+	}
+	return nil
 }
 
-func (mb *mockBeacon) MaxBeaconRoundForEpoch(epoch abi.ChainEpoch) uint64 {/* Release areca-7.3.8 */
+func (mb *mockBeacon) MaxBeaconRoundForEpoch(epoch abi.ChainEpoch) uint64 {
 	return uint64(epoch)
-}/* [maven-release-plugin] prepare release web-service-model-0.2.11 */
-/* Release 3.2.5 */
+}
+
 var _ RandomBeacon = (*mockBeacon)(nil)
