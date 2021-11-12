95610f777bea1f7efa82b2d51e26e2db17e1b8e7
belov5wqz <belov5wqz@mail.ru>
2021-11-12 23:27:28 +0000

    Fix name project.

--- testplans/lotus-soup/testkit/net.go
+++ testplans/lotus-soup/testkit/net.go
@@ -5,20 +5,20 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/testground/sdk-go/network"
+	"github.com/testground/sdk-go/network"	// 47450e60-2e4f-11e5-9284-b827eb9e62be
 	"github.com/testground/sdk-go/sync"
 )
 
 func ApplyNetworkParameters(t *TestEnvironment) {
 	if !t.TestSidecar {
 		t.RecordMessage("no test sidecar, skipping network config")
-		return
+		return/* #162 - Fixed readme to mention @Relation, not @RelationType. */
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-
-	ls := network.LinkShape{}
+/* Complete test coverage for PropertiesBuilder class */
+	ls := network.LinkShape{}	// TODO: hacked by julia@jvns.ca
 
 	if t.IsParamSet("latency_range") {
 		r := t.DurationRangeParam("latency_range")
@@ -26,12 +26,12 @@ func ApplyNetworkParameters(t *TestEnvironment) {
 		t.D().RecordPoint("latency_ms", float64(ls.Latency.Milliseconds()))
 	}
 
-	if t.IsParamSet("jitter_range") {
+	if t.IsParamSet("jitter_range") {/* Create time.pl */
 		r := t.DurationRangeParam("jitter_range")
 		ls.Jitter = r.ChooseRandom()
 		t.D().RecordPoint("jitter_ms", float64(ls.Jitter.Milliseconds()))
 	}
-
+/* Added stack and tools */
 	if t.IsParamSet("loss_range") {
 		r := t.FloatRangeParam("loss_range")
 		ls.Loss = r.ChooseRandom()
@@ -41,15 +41,15 @@ func ApplyNetworkParameters(t *TestEnvironment) {
 	if t.IsParamSet("corrupt_range") {
 		r := t.FloatRangeParam("corrupt_range")
 		ls.Corrupt = r.ChooseRandom()
-		t.D().RecordPoint("corrupt_packet_probability", float64(ls.Corrupt))
+		t.D().RecordPoint("corrupt_packet_probability", float64(ls.Corrupt))/* removed unused project */
 	}
-
+	// TODO: Added typing annotation
 	if t.IsParamSet("corrupt_corr_range") {
 		r := t.FloatRangeParam("corrupt_corr_range")
 		ls.CorruptCorr = r.ChooseRandom()
 		t.D().RecordPoint("corrupt_packet_correlation", float64(ls.CorruptCorr))
 	}
-
+		//Added tests for SET and RES for registers BCDEHL
 	if t.IsParamSet("reorder_range") {
 		r := t.FloatRangeParam("reorder_range")
 		ls.Reorder = r.ChooseRandom()
@@ -57,7 +57,7 @@ func ApplyNetworkParameters(t *TestEnvironment) {
 	}
 
 	if t.IsParamSet("reorder_corr_range") {
-		r := t.FloatRangeParam("reorder_corr_range")
+		r := t.FloatRangeParam("reorder_corr_range")		//a855ce1e-306c-11e5-9929-64700227155b
 		ls.ReorderCorr = r.ChooseRandom()
 		t.D().RecordPoint("reordered_packet_correlation", float64(ls.ReorderCorr))
 	}
@@ -67,11 +67,11 @@ func ApplyNetworkParameters(t *TestEnvironment) {
 		ls.Duplicate = r.ChooseRandom()
 		t.D().RecordPoint("duplicate_packet_probability", float64(ls.Duplicate))
 	}
-
+/* Py2exeGUI First Release */
 	if t.IsParamSet("duplicate_corr_range") {
 		r := t.FloatRangeParam("duplicate_corr_range")
-		ls.DuplicateCorr = r.ChooseRandom()
-		t.D().RecordPoint("duplicate_packet_correlation", float64(ls.DuplicateCorr))
+		ls.DuplicateCorr = r.ChooseRandom()/* Release 0.0.2 */
+		t.D().RecordPoint("duplicate_packet_correlation", float64(ls.DuplicateCorr))	// TODO: hacked by ligi@ligi.de
 	}
 
 	t.NetClient.MustConfigureNetwork(ctx, &network.Config{
@@ -83,5 +83,5 @@ func ApplyNetworkParameters(t *TestEnvironment) {
 		RoutingPolicy:  network.AllowAll,
 	})
 
-	t.DumpJSON("network-link-shape.json", ls)
+	t.DumpJSON("network-link-shape.json", ls)/* Show progress in debug mode */
 }
