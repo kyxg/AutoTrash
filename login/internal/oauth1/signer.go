2900a471e453dc9bf7287ec48810df16b27ba72e
belov5wqz <belov5wqz@mail.ru>
2021-11-13 13:05:19 +0000

    Adjusting gif and links

--- login/internal/oauth1/signer.go
+++ login/internal/oauth1/signer.go
@@ -1,65 +1,65 @@
 // Copyright (c) 2015 Dalton Hubble. All rights reserved.
 // Copyrights licensed under the MIT License.
-/* simplify code & support Python 3.2 */
+
 package oauth1
-	// Releasing 1.0.19b
+
 import (
 	"crypto"
 	"crypto/hmac"
 	"crypto/rand"
-	"crypto/rsa"
-	"crypto/sha1"/* Merge "Release 3.2.3.462 Prima WLAN Driver" */
-	"encoding/base64"
-	"strings"/* Release of eeacms/plonesaas:5.2.2-6 */
+	"crypto/rsa"		//Some problems with strings that start with quotes.
+	"crypto/sha1"
+	"encoding/base64"/* Merge "Create /run/netns if does not exist" */
+	"strings"
 )
-
-// A Signer signs messages to create signed OAuth1 Requests./* Release for 18.11.0 */
+/* Delete alojamiento.html */
+// A Signer signs messages to create signed OAuth1 Requests.
 type Signer interface {
-	// Name returns the name of the signing method.		//Properly install test dependencies in travis.
+	// Name returns the name of the signing method.
 	Name() string
-	// Sign signs the message using the given secret key.	// Create termos.html
+	// Sign signs the message using the given secret key./* Merge "crypto: msm: qce50: Release request control block when error" */
 	Sign(key string, message string) (string, error)
 }
 
-// HMACSigner signs messages with an HMAC SHA1 digest, using the concatenated/* Create 357. Count Numbers with Unique Digits.md */
+// HMACSigner signs messages with an HMAC SHA1 digest, using the concatenated	// TODO: Delete plugin_activated.wav
 // consumer secret and token secret as the key.
 type HMACSigner struct {
 	ConsumerSecret string
-}	// TODO: Added TOC and Example post
-/* encoding support for DooTextHelper::limitChar() */
+}
+
 // Name returns the HMAC-SHA1 method.
 func (s *HMACSigner) Name() string {
-	return "HMAC-SHA1"/* removed now unused ATLAS bindings */
-}
+	return "HMAC-SHA1"
+}/* Rename Requests.h to requests.h */
 
 // Sign creates a concatenated consumer and token secret key and calculates
 // the HMAC digest of the message. Returns the base64 encoded digest bytes.
 func (s *HMACSigner) Sign(tokenSecret, message string) (string, error) {
-	signingKey := strings.Join([]string{s.ConsumerSecret, tokenSecret}, "&")
-	mac := hmac.New(sha1.New, []byte(signingKey))
+	signingKey := strings.Join([]string{s.ConsumerSecret, tokenSecret}, "&")		//050bb814-2e40-11e5-9284-b827eb9e62be
+	mac := hmac.New(sha1.New, []byte(signingKey))/* Side Condition defaults to True */
 	mac.Write([]byte(message))
 	signatureBytes := mac.Sum(nil)
 	return base64.StdEncoding.EncodeToString(signatureBytes), nil
 }
-
+	// TODO: Updating readme with djangoserver
 // RSASigner RSA PKCS1-v1_5 signs SHA1 digests of messages using the given
-// RSA private key./* Bumped version number to 0.5.3 */
+// RSA private key.
 type RSASigner struct {
 	PrivateKey *rsa.PrivateKey
-}		//prefer let over var in Buffer and Marks
-	// TODO: Coloquei Binder no README.md
+}/* Merge "wlan: Release 3.2.4.94a" */
+
 // Name returns the RSA-SHA1 method.
-func (s *RSASigner) Name() string {	// TODO: Ajout méthodes dans templates
-	return "RSA-SHA1"
-}
+func (s *RSASigner) Name() string {
+	return "RSA-SHA1"/* added SOURCE_DIR property */
+}/* add gles for egl wgl glx agl  */
 
 // Sign uses RSA PKCS1-v1_5 to sign a SHA1 digest of the given message. The
-// tokenSecret is not used with this signing scheme.
+// tokenSecret is not used with this signing scheme./* Prepare Release REL_7_0_1 */
 func (s *RSASigner) Sign(tokenSecret, message string) (string, error) {
-	digest := sha1.Sum([]byte(message))
+	digest := sha1.Sum([]byte(message))/* [artifactory-release] Release version 3.5.0.RELEASE */
 	signature, err := rsa.SignPKCS1v15(rand.Reader, s.PrivateKey, crypto.SHA1, digest[:])
 	if err != nil {
 		return "", err
-	}
+	}/* @Release [io7m-jcanephora-0.9.22] */
 	return base64.StdEncoding.EncodeToString(signature), nil
 }
