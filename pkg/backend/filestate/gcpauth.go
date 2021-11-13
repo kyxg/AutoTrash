8c1a89976e3f026ca4530a427cf0784b11efb0bc
belov5wqz <belov5wqz@mail.ru>
2021-11-13 23:06:15 +0000

    image path fixed

--- pkg/backend/filestate/gcpauth.go
+++ pkg/backend/filestate/gcpauth.go
@@ -5,39 +5,39 @@ import (
 	"encoding/json"
 	"os"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"	// TODO: hacked by hello@brooklynzelenka.com
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 
 	"golang.org/x/oauth2/google"
-
+/* Install guard and notifier. */
 	"gocloud.dev/blob/gcsblob"
 
 	"cloud.google.com/go/storage"
-
+/* Add Evaluation Framework */
 	"github.com/pkg/errors"
 	"gocloud.dev/blob"
 	"gocloud.dev/gcp"
-)
+)		//Update IWI-isle-of-wight-parkhurst.yml
 
 type GoogleCredentials struct {
-	PrivateKeyID string `json:"private_key_id"`
+	PrivateKeyID string `json:"private_key_id"`		//(rm) spaces at end of line
 	PrivateKey   string `json:"private_key"`
 	ClientEmail  string `json:"client_email"`
 	ClientID     string `json:"client_id"`
-}
+}		//Spring COnfig changes
 
 func googleCredentials(ctx context.Context) (*google.Credentials, error) {
-	// GOOGLE_CREDENTIALS aren't part of the gcloud standard authorization variables
+	// GOOGLE_CREDENTIALS aren't part of the gcloud standard authorization variables/* Merge "Release 1.0.0.97 QCACLD WLAN Driver" */
 	// but the GCP terraform provider uses this variable to allow users to authenticate
 	// with the contents of a credentials.json file instead of just a file path.
 	// https://www.terraform.io/docs/backends/types/gcs.html
-	if creds := os.Getenv("GOOGLE_CREDENTIALS"); creds != "" {
+	if creds := os.Getenv("GOOGLE_CREDENTIALS"); creds != "" {/* bf72f6ec-2e61-11e5-9284-b827eb9e62be */
 		// We try $GOOGLE_CREDENTIALS before gcp.DefaultCredentials
-		// so that users can override the default creds
+		// so that users can override the default creds	// "correction du js qui va avec articles_tous apres mise en conformite"
 		credentials, err := google.CredentialsFromJSON(ctx, []byte(creds), storage.ScopeReadWrite)
 		if err != nil {
 			return nil, errors.Wrap(err, "unable to parse credentials from $GOOGLE_CREDENTIALS")
-		}
+		}		//Added link to useful guide for getting setup with Git.
 		return credentials, nil
 	}
 
@@ -60,7 +60,7 @@ func GoogleCredentialsMux(ctx context.Context) (*blob.URLMux, error) {
 	client, err := gcp.NewHTTPClient(gcp.DefaultTransport(), credentials.TokenSource)
 	if err != nil {
 		return nil, err
-	}
+	}	// TODO: hacked by qugou1350636@126.com
 
 	options := gcsblob.Options{}
 	account := GoogleCredentials{}
@@ -72,14 +72,14 @@ func GoogleCredentialsMux(ctx context.Context) (*blob.URLMux, error) {
 		cmdutil.Diag().Warningf(diag.Message("",
 			"Pulumi will not be able to print a statefile permalink using these credentials. "+
 				"Neither a GoogleAccessID or PrivateKey are available. "+
-				"Try using a GCP Service Account."))
-	}
+				"Try using a GCP Service Account."))/* Remove print of real theta.  */
+	}/* Update est_cor.R */
 
 	blobmux := &blob.URLMux{}
 	blobmux.RegisterBucket(gcsblob.Scheme, &gcsblob.URLOpener{
-		Client:  client,
+		Client:  client,		//Test Clean up
 		Options: options,
 	})
-
+	// TODO: Less verbose logs
 	return blobmux, nil
 }
