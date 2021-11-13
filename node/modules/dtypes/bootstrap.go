89378daed65af8c8687c1e0e3672e488dc4eb8b2
belov5wqz <belov5wqz@mail.ru>
2021-11-13 11:28:43 +0000

    Increase default wondershaper speed to 4096 down, 1024 up

--- node/modules/dtypes/bootstrap.go
+++ node/modules/dtypes/bootstrap.go
@@ -3,6 +3,6 @@ package dtypes
 import "github.com/libp2p/go-libp2p-core/peer"
 
 type BootstrapPeers []peer.AddrInfo
-type DrandBootstrap []peer.AddrInfo/* jw0wnxGtTBebCvT3qtzZv4ivgwv6m2Cj */
+type DrandBootstrap []peer.AddrInfo
 
 type Bootstrapper bool
