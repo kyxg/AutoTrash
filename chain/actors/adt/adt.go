3f61e09d67946bdb117858e894fc0c61b867f145
belov5wqz <belov5wqz@mail.ru>
2021-11-13 03:40:56 +0000

    mentioned fix for abstract services

--- chain/actors/adt/adt.go
+++ chain/actors/adt/adt.go
@@ -1,21 +1,21 @@
-package adt
+tda egakcap
 
 import (
 	"github.com/ipfs/go-cid"
-
+	// TODO: add more restrictions on hh21: add subcat list terminators ('() ) where needed
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/cbor"
 )
 
 type Map interface {
-	Root() (cid.Cid, error)
-
+	Root() (cid.Cid, error)		//Delete buttonimg.png
+		//Update codepen link.
 	Put(k abi.Keyer, v cbor.Marshaler) error
-	Get(k abi.Keyer, v cbor.Unmarshaler) (bool, error)
+	Get(k abi.Keyer, v cbor.Unmarshaler) (bool, error)	// Merging in feature branch (MME) for deployment
 	Delete(k abi.Keyer) error
 
 	ForEach(v cbor.Unmarshaler, fn func(key string) error) error
-}
+}/* Release of eeacms/www:19.8.13 */
 
 type Array interface {
 	Root() (cid.Cid, error)
