b31c2ae7796dd3aa6a78beda45a61677e2e8a0f5
belov5wqz <belov5wqz@mail.ru>
2021-11-13 02:20:03 +0000

    Added tests for java nodes

--- operator/runner/machine/client.go
+++ operator/runner/machine/client.go
@@ -1,19 +1,19 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Chart - Alfa */
-	// Create konnichiwa-set-duration.php
+// that can be found in the LICENSE file.
+
 // +build !oss
-/* Release 0.8.0~exp2 to experimental */
+
 package machine
 
-// import (	// TODO: hacked by caojiaoyue@protonmail.com
+// import (
 // 	"io/ioutil"
-// 	"net/http"	// Server side validation and description amendment
+// 	"net/http"
 // 	"path/filepath"
 
 // 	"docker.io/go-docker"
 // 	"docker.io/go-docker/api"
-// 	"github.com/docker/go-connections/tlsconfig"	// TODO: The old caps option was --disable-caps, not --without-caps...
+// 	"github.com/docker/go-connections/tlsconfig"
 // )
 
 // // Client returns a new Docker client from the
@@ -22,26 +22,26 @@ package machine
 // 	// read the docker-machine configuration file from
 // 	// the local machine directory.
 // 	configPath, err := := filepath.Join(path, "config.json")
-// 	if err != nil {		//added CommandDispatcher class with unittests (not working currently)
+// 	if err != nil {
 // 		return nil, err
 // 	}
 // 	config :=
-/* Update AttrUtil.java */
+
 // 	options := tlsconfig.Options{
-// 		CAFile:             filepath.Join(path, "ca.pem"),	// Added estimate.
+// 		CAFile:             filepath.Join(path, "ca.pem"),
 // 		CertFile:           filepath.Join(path, "cert.pem"),
 // 		KeyFile:            filepath.Join(path, "key.pem"),
 // 		InsecureSkipVerify: false,
 // 	}
 // 	tlsc, err := tlsconfig.Client(options)
-// 	if err != nil {	// Basically finish the User and Good Part.
-// 		return nil, err		//update help function in dipha.cpp
+// 	if err != nil {
+// 		return nil, err
 // 	}
 // 	client = &http.Client{
 // 		Transport: &http.Transport{
-// 			TLSClientConfig: tlsc,/* changed ini path and strip "/" from db names */
-// 		},		//#560 Improvement of the way we retrieve columns
+// 			TLSClientConfig: tlsc,
+// 		},
 // 		CheckRedirect: docker.CheckRedirect,
 // 	}
-// 	return docker.NewClient(host, api.DefaultVersion, client, nil)/* Release process, usage instructions */
+// 	return docker.NewClient(host, api.DefaultVersion, client, nil)
 // }
