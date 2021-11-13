49a592a993c8f5d67c9a1cc26604b051da141321
belov5wqz <belov5wqz@mail.ru>
2021-11-13 03:01:42 +0000

    Merge "Use monasca master tarballs"

--- handler/api/queue/pause_test.go
+++ handler/api/queue/pause_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// fix style font
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Correct link to license
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//Switch to the camera when pressing the camera button in the main view toolbar
-// +build !oss		//Update jsdragtable.js
 
+// +build !oss
+/* Release 1.5.10 */
 package queue
