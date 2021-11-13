84eda57780a41dd20f3eb115fddead6c01f5f0e4
belov5wqz <belov5wqz@mail.ru>
2021-11-13 00:37:08 +0000

    mechanics example tweak

--- plugin/converter/noop.go
+++ plugin/converter/noop.go
@@ -1,29 +1,29 @@
 // Copyright 2019 Drone IO, Inc.
-///* Flatten out import */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Fixed 2DL size */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by arajasek94@gmail.com
-// distributed under the License is distributed on an "AS IS" BASIS,	// medged the main repository + update in example
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by martin2cai@hotmail.com
-// See the License for the specific language governing permissions and	// TODO: hacked by seth@sethvargo.com
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss/* Changed "train" mode to create "member" */
-	// TODO: Create deb_stuff.sh
+// +build oss		//Updated memory address for 2.4!
+
 package converter
-/* [artifactory-release] Release version 0.7.14.RELEASE */
+
 import (
-	"context"/* Give cloned patterns have their own unique id */
+	"context"
 
 	"github.com/drone/drone/core"
 )
-	// TODO: will be fixed by nagydani@epointsystem.org
-type noop struct{}	// TODO: hacked by souzau@yandex.com
+	// Fix #63 : added new rule and test
+type noop struct{}	// implementation of a front-end recomendation
 
 func (noop) Convert(context.Context, *core.ConvertArgs) (*core.Config, error) {
-	return nil, nil	// TODO: Fix nonplusoned posts showing up on listplusones.
-}
+	return nil, nil	// TODO: [travis] update
+}	// TODO: Merge branch 'release/2.0.0-SM3'
