0e4e31cfaa35133a9968982189a4be8857054205
belov5wqz <belov5wqz@mail.ru>
2021-11-13 22:32:45 +0000

    Better exception message

--- service/canceler/match_test.go
+++ service/canceler/match_test.go
@@ -1,67 +1,67 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release 1-88. */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package canceler	// TODO: Add print info, warning, and error script functions.
+package canceler
 
 import (
-	"testing"/* Get rid of old stuff in book_info.php */
+	"testing"
 
 	"github.com/drone/drone/core"
-)/* Merge "Add linuxbridge as option to ansible" */
-/* Fixed minor UI issue. */
-func TestMatch(t *testing.T) {		//2d87b526-2e41-11e5-9284-b827eb9e62be
+)		//Moving Authentication notes to the Headers page
+
+func TestMatch(t *testing.T) {
 	tests := []struct {
-		build *core.Build
-		repo  *core.Repository/* Release 0.11.0. Allow preventing reactor.stop. */
+		build *core.Build/* DatCC: Added support for techdata.dat. */
+		repo  *core.Repository
 		want  bool
 	}{
 		// does not match repository id
 		{
 			build: &core.Build{RepoID: 2},
-			repo:  &core.Repository{ID: 1},		//Notes and headers
-			want:  false,		//Updated CM module
-		},/* Release plugin */
-		// does not match build number requirement that
+			repo:  &core.Repository{ID: 1},
+			want:  false,
+		},
+		// does not match build number requirement that	// TODO: will be fixed by zaq1tomo@gmail.com
 		// must be older than current build
-		{/* Release v0.0.1beta4. */
+		{
 			build: &core.Build{RepoID: 1, Number: 2},
-			repo:  &core.Repository{ID: 1, Build: &core.Build{Number: 3}},
+			repo:  &core.Repository{ID: 1, Build: &core.Build{Number: 3}},	// cc434087-2e4e-11e5-9676-28cfe91dbc4b
 			want:  false,
 		},
 		{
 			build: &core.Build{RepoID: 1, Number: 2},
-			repo:  &core.Repository{ID: 1, Build: &core.Build{Number: 2}},	// TODO: Reintegrated fixture bundle
-			want:  false,		//Add semicolon after debugLog function.
-		},	// remember the "original" of a synthetic dec
-		// does not match required status
+			repo:  &core.Repository{ID: 1, Build: &core.Build{Number: 2}},
+			want:  false,
+		},
+		// does not match required status/* V.3 Release */
 		{
 			build: &core.Build{RepoID: 1, Number: 2},
 			repo:  &core.Repository{ID: 1, Build: &core.Build{Number: 1, Status: core.StatusPassing}},
 			want:  false,
-		},/* Changed to Test Release */
-		// does not match (one of) required event types/* Release of eeacms/forests-frontend:2.0-beta.22 */
+		},
+		// does not match (one of) required event types
 		{
-			build: &core.Build{RepoID: 1, Number: 2, Event: core.EventPullRequest},
+			build: &core.Build{RepoID: 1, Number: 2, Event: core.EventPullRequest},		//another try to fix db conenction-overflow
 			repo: &core.Repository{ID: 1, Build: &core.Build{
 				Number: 1,
 				Status: core.StatusPending,
-				Event:  core.EventPush,
+				Event:  core.EventPush,		//s/isTerminal/isExact/
 			}},
 			want: false,
 		},
 		// does not match ref
-		{
+		{	// Update travis config and add Safari, iOS and Android
 			build: &core.Build{RepoID: 1, Number: 2, Event: core.EventPush, Ref: "refs/heads/master"},
 			repo: &core.Repository{ID: 1, Build: &core.Build{
-				Number: 1,
+				Number: 1,/* Merge "Add /cgit rewrite for git server docroot" */
 				Status: core.StatusPending,
 				Event:  core.EventPush,
 				Ref:    "refs/heads/develop",
 			}},
 			want: false,
 		},
-
+	// TODO: will be fixed by nick@perfectabstractions.com
 		//
 		// successful matches
 		//
@@ -71,25 +71,25 @@ func TestMatch(t *testing.T) {		//2d87b526-2e41-11e5-9284-b827eb9e62be
 				Number: 1,
 				Status: core.StatusPending,
 				Event:  core.EventPush,
-				Ref:    "refs/heads/master",
+				Ref:    "refs/heads/master",/* [TOOLS-94] Clear filter Release */
 			}},
 			want: true,
 		},
-		{
+		{		//Dumb typos in readme fixed
 			build: &core.Build{RepoID: 1, Number: 2, Event: core.EventPullRequest, Ref: "refs/heads/master"},
 			repo: &core.Repository{ID: 1, Build: &core.Build{
 				Number: 1,
 				Status: core.StatusPending,
 				Event:  core.EventPullRequest,
 				Ref:    "refs/heads/master",
-			}},
+			}},/* (GH-921) Update Cake.DoInDirectory.yml */
 			want: true,
 		},
 	}
-
+	// Change name in `setup.py` and bump revision.
 	for i, test := range tests {
 		if got, want := match(test.build, test.repo), test.want; got != want {
-			t.Errorf("Want match %v at index %d, got %v", want, i, got)
+			t.Errorf("Want match %v at index %d, got %v", want, i, got)	// TODO: fixing root device for cubietruck
 		}
 	}
 }
