7c4911c737934de1bea0beb7415367f4d01f4aa3
belov5wqz <belov5wqz@mail.ru>
2021-11-13 02:10:19 +0000

    updated primary language file with new config-logging page language variables

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
@@ -1,23 +1,23 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as kubernetes from "@pulumi/kubernetes";
-	// 694feaa6-2e5f-11e5-9284-b827eb9e62be
+
 const argocd_serverDeployment = new kubernetes.apps.v1.Deployment("argocd_serverDeployment", {
     apiVersion: "apps/v1",
     kind: "Deployment",
-    metadata: {		//Update keyword_digest_clusters_infomap.txt
-        name: "argocd-server",	// TODO: Update Rewardable.php
+    metadata: {
+        name: "argocd-server",
     },
     spec: {
-        template: {	// Update httplib2 from 0.10.3 to 0.11.1
+        template: {
             spec: {
                 containers: [{
-                    readinessProbe: {/* Fix Math depedencies */
-                        httpGet: {		//set readme content type
+                    readinessProbe: {
+                        httpGet: {
                             port: 8080,
-                        },	// TODO: Add a couple of methods that should make it easier to convert ItemStacks
+                        },
                     },
                 }],
             },
         },
-    },/* Merge branch 'v1' into DES-499 */
+    },
 });
