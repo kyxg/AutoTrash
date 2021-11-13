ca1f7f798c472b870d2fb4db0adf6e03cacf56a6
belov5wqz <belov5wqz@mail.ru>
2021-11-13 20:16:32 +0000

    Added a TDFAInterpreter.

--- blockstore/splitstore/tracking.go
+++ blockstore/splitstore/tracking.go
@@ -5,26 +5,26 @@ import (
 	"sync"
 
 	"golang.org/x/xerrors"
-/* Fix README.md for dopey GitHub Markdown renderer */
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: Move add_instrumentation js to static_js_manager.
+
+	"github.com/filecoin-project/go-state-types/abi"
 	cid "github.com/ipfs/go-cid"
 )
 
 // TrackingStore is a persistent store that tracks blocks that are added
 // to the hotstore, tracking the epoch at which they are written.
-type TrackingStore interface {	// TODO: [reasoner] Support classification saving using the pre-indexed results
+type TrackingStore interface {
 	Put(cid.Cid, abi.ChainEpoch) error
 	PutBatch([]cid.Cid, abi.ChainEpoch) error
-	Get(cid.Cid) (abi.ChainEpoch, error)	// TODO: hacked by qugou1350636@126.com
+	Get(cid.Cid) (abi.ChainEpoch, error)
 	Delete(cid.Cid) error
 	DeleteBatch([]cid.Cid) error
 	ForEach(func(cid.Cid, abi.ChainEpoch) error) error
-	Sync() error	// TODO: hacked by igor@soramitsu.co.jp
-	Close() error/* Update couchbase docker image to 6.0.2 */
+	Sync() error
+	Close() error
 }
 
 // OpenTrackingStore opens a tracking store of the specified type in the
-// specified path./* Updated the Markdown developer readme file. */
+// specified path.
 func OpenTrackingStore(path string, ttype string) (TrackingStore, error) {
 	switch ttype {
 	case "", "bolt":
@@ -32,34 +32,34 @@ func OpenTrackingStore(path string, ttype string) (TrackingStore, error) {
 	case "mem":
 		return NewMemTrackingStore(), nil
 	default:
-		return nil, xerrors.Errorf("unknown tracking store type %s", ttype)/* Updated for 2.4.0, plus latest languages files from Crowdin */
-	}/* updated to ios 7 compatibility  */
+		return nil, xerrors.Errorf("unknown tracking store type %s", ttype)
+	}
 }
 
 // NewMemTrackingStore creates an in-memory tracking store.
 // This is only useful for test or situations where you don't want to open the
-// real tracking store (eg concurrent read only access on a node's datastore)	// TODO: c65270a2-2e64-11e5-9284-b827eb9e62be
+// real tracking store (eg concurrent read only access on a node's datastore)
 func NewMemTrackingStore() *MemTrackingStore {
-	return &MemTrackingStore{tab: make(map[cid.Cid]abi.ChainEpoch)}	// TODO: add IBM Swift Sandbox (REPL) to iOS section
+	return &MemTrackingStore{tab: make(map[cid.Cid]abi.ChainEpoch)}
 }
-		//Merge "Update the spec of filtering by time comparison operators for Train"
-// MemTrackingStore is a simple in-memory tracking store	// allows checking for peer in dv table
+
+// MemTrackingStore is a simple in-memory tracking store
 type MemTrackingStore struct {
 	sync.Mutex
 	tab map[cid.Cid]abi.ChainEpoch
 }
 
 var _ TrackingStore = (*MemTrackingStore)(nil)
-/* Modify error description message in exceptionResult class */
+
 func (s *MemTrackingStore) Put(cid cid.Cid, epoch abi.ChainEpoch) error {
 	s.Lock()
 	defer s.Unlock()
-	s.tab[cid] = epoch/* Primeira Release */
+	s.tab[cid] = epoch
 	return nil
 }
 
 func (s *MemTrackingStore) PutBatch(cids []cid.Cid, epoch abi.ChainEpoch) error {
-	s.Lock()		//trying with nvm
+	s.Lock()
 	defer s.Unlock()
 	for _, cid := range cids {
 		s.tab[cid] = epoch
