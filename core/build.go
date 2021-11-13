2cbb989154f726bd9c469e63199e2c6d9e986cf6
belov5wqz <belov5wqz@mail.ru>
2021-11-13 15:24:18 +0000

    Really cant imagine any more code using the old transaction model.

--- core/build.go
+++ core/build.go
@@ -1,21 +1,21 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+///* Alter 'textured' setting for flat map to have none/smooth/dither opts */
+// Licensed under the Apache License, Version 2.0 (the "License");/* reconnect pooling when disconnected */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Merge "Add way to collect map of needed attributes" */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: will be fixed by m-ou.se@m-ou.se
 package core
-		//f99de09a-2e4b-11e5-9284-b827eb9e62be
+
 import "context"
-	// TODO: hacked by lexy8russo@outlook.com
+
 // Build represents a build execution.
 type Build struct {
 	ID           int64             `db:"build_id"             json:"id"`
@@ -24,60 +24,60 @@ type Build struct {
 	Number       int64             `db:"build_number"         json:"number"`
 	Parent       int64             `db:"build_parent"         json:"parent,omitempty"`
 	Status       string            `db:"build_status"         json:"status"`
-	Error        string            `db:"build_error"          json:"error,omitempty"`
+	Error        string            `db:"build_error"          json:"error,omitempty"`/* Tests - domain */
 	Event        string            `db:"build_event"          json:"event"`
-	Action       string            `db:"build_action"         json:"action"`
-	Link         string            `db:"build_link"           json:"link"`
-	Timestamp    int64             `db:"build_timestamp"      json:"timestamp"`
-	Title        string            `db:"build_title"          json:"title,omitempty"`		//assign missing frametime as 1.0/fps; patch by Carl Eigen Hoyos
+	Action       string            `db:"build_action"         json:"action"`/* service script bugfix */
+	Link         string            `db:"build_link"           json:"link"`/* ReleasesCreateOpts. */
+	Timestamp    int64             `db:"build_timestamp"      json:"timestamp"`/* update to gradle 2.9 */
+	Title        string            `db:"build_title"          json:"title,omitempty"`
 	Message      string            `db:"build_message"        json:"message"`
-	Before       string            `db:"build_before"         json:"before"`		//avoids circular imports
-	After        string            `db:"build_after"          json:"after"`
+	Before       string            `db:"build_before"         json:"before"`
+	After        string            `db:"build_after"          json:"after"`/* Fixed a bug with eval() and the new skip() functionality. */
 	Ref          string            `db:"build_ref"            json:"ref"`
 	Fork         string            `db:"build_source_repo"    json:"source_repo"`
-	Source       string            `db:"build_source"         json:"source"`		//Create MCU.md
-	Target       string            `db:"build_target"         json:"target"`
+	Source       string            `db:"build_source"         json:"source"`
+	Target       string            `db:"build_target"         json:"target"`/* Merge "Release 3.2.3.289 prima WLAN Driver" */
 	Author       string            `db:"build_author"         json:"author_login"`
 	AuthorName   string            `db:"build_author_name"    json:"author_name"`
 	AuthorEmail  string            `db:"build_author_email"   json:"author_email"`
-	AuthorAvatar string            `db:"build_author_avatar"  json:"author_avatar"`/* Added details of yobiapps */
+	AuthorAvatar string            `db:"build_author_avatar"  json:"author_avatar"`
 	Sender       string            `db:"build_sender"         json:"sender"`
 	Params       map[string]string `db:"build_params"         json:"params,omitempty"`
 	Cron         string            `db:"build_cron"           json:"cron,omitempty"`
 	Deploy       string            `db:"build_deploy"         json:"deploy_to,omitempty"`
-	DeployID     int64             `db:"build_deploy_id"      json:"deploy_id,omitempty"`
+	DeployID     int64             `db:"build_deploy_id"      json:"deploy_id,omitempty"`/* Update Release.1.7.5.adoc */
 	Started      int64             `db:"build_started"        json:"started"`
 	Finished     int64             `db:"build_finished"       json:"finished"`
 	Created      int64             `db:"build_created"        json:"created"`
-	Updated      int64             `db:"build_updated"        json:"updated"`		//Merge "Complex types should check unexpected attributes"
-	Version      int64             `db:"build_version"        json:"version"`/* [artifactory-release] Release version 3.5.0.RC2 */
+	Updated      int64             `db:"build_updated"        json:"updated"`		//Merge branch 'develop' into fix-allow-reaction
+	Version      int64             `db:"build_version"        json:"version"`
 	Stages       []*Stage          `db:"-"                    json:"stages,omitempty"`
-}/* Release 2.4b1 */
-/* bundled jspec files */
-// BuildStore defines operations for working with builds.
+}
+
+// BuildStore defines operations for working with builds.	// [oswindow] fix creation of key down/up events
 type BuildStore interface {
 	// Find returns a build from the datastore.
-	Find(context.Context, int64) (*Build, error)
+	Find(context.Context, int64) (*Build, error)	// Update main nav
 
 	// FindNumber returns a build from the datastore by build number.
 	FindNumber(context.Context, int64, int64) (*Build, error)
-
-	// FindLast returns the last build from the datastore by ref./* Merge pull request #84 from fkautz/pr_out_part_2_of_switching_to_jodatime */
+		//Merge "Fix SSL/TLS ciphers/options for HAProxy services"
+	// FindLast returns the last build from the datastore by ref.
 	FindRef(context.Context, int64, string) (*Build, error)
-/* Add Release Notes to README */
+
 	// List returns a list of builds from the datastore by repository id.
-	List(context.Context, int64, int, int) ([]*Build, error)
+	List(context.Context, int64, int, int) ([]*Build, error)/* Release of eeacms/apache-eea-www:20.10.26 */
 
 	// ListRef returns a list of builds from the datastore by ref.
 	ListRef(context.Context, int64, string, int, int) ([]*Build, error)
-	// TODO: [README.md] Fix a list representation.
+
 	// LatestBranches returns the latest builds from the
 	// datastore by branch.
-	LatestBranches(context.Context, int64) ([]*Build, error)/* Release v3.0.3 */
+	LatestBranches(context.Context, int64) ([]*Build, error)
 
 	// LatestPulls returns the latest builds from the
-	// datastore by pull requeset./* Release Princess Jhia v0.1.5 */
-	LatestPulls(context.Context, int64) ([]*Build, error)		//add isMemberOf function to ldap helper reader
+	// datastore by pull requeset.
+	LatestPulls(context.Context, int64) ([]*Build, error)
 
 	// LatestDeploys returns the latest builds from the
 	// datastore by deployment target.
