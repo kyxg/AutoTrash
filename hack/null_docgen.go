d5e1eb5f94c54115e7466f294115841c17a4aca5
belov5wqz <belov5wqz@mail.ru>
2021-11-13 17:30:10 +0000

    Added a default implementation of NetcodeConfiguration.

--- hack/null_docgen.go
+++ hack/null_docgen.go
@@ -1,7 +1,7 @@
 // +build fields
 
 package main
-
+	// bug: fix escaping character and long line paragraph break
 func generateDocs() {
 	panic("hack package was built with 'fields' tag; doc generation code was not included")
 }
