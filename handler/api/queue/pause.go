5f37da196ac108710cb661e0885058dcabdf26df
belov5wqz <belov5wqz@mail.ru>
2021-11-13 20:59:39 +0000

    Have to install nodejs

--- handler/api/queue/pause.go
+++ handler/api/queue/pause.go
@@ -1,28 +1,28 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+.devreser sthgir llA .cnI OI.enorD 9102 thgirypoC //
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//remove other devices
 
-// +build !oss
-
+// +build !oss		//Create goahead_traversal.rc
+/* Release of Collect that fixes CSV update bug */
 package queue
 
 import (
 	"net/http"
-
+/* fixed issue tracker URL */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
-)
+)		//Simplified table deletion code
 
 // HandlePause returns an http.HandlerFunc that processes
 // an http.Request to pause the scheduler.
 func HandlePause(scheduler core.Scheduler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
-		err := scheduler.Pause(ctx)
+		err := scheduler.Pause(ctx)	// TODO: Add TestTask to rakefile
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).WithError(err).
+			logger.FromRequest(r).WithError(err).	// getCalculatedElementWidth > getMinimumWidth
 				Errorln("api: cannot pause scheduler")
 			return
 		}
