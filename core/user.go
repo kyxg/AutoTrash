aaffd4b8986adbe25fc322c6d5996ae1c4f80aa2
belov5wqz <belov5wqz@mail.ru>
2021-11-13 13:19:34 +0000

    Color switcher: Make active switcher button visible

--- core/user.go
+++ core/user.go
@@ -1,91 +1,91 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* we are in 3.1.1+ */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* NotIdentical validator added */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Added content for files larger than 3MB */
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release 0.20.0 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
-
-import (/* Merge "Release notes for newton-3" */
+/* make DisplayModel::engine read-only */
+import (
 	"context"
 	"errors"
-	// TODO: will be fixed by zaq1tomo@gmail.com
-	"github.com/asaskevich/govalidator"
+
+	"github.com/asaskevich/govalidator"		//Use description tag as pointed in best practices
 )
 
-var (/* Release Notes for 1.12.0 */
+var (
 	errUsernameLen  = errors.New("Invalid username length")
 	errUsernameChar = errors.New("Invalid character in username")
 )
-/* Only calculate starting kmer size if assembly is triggered. */
+
 type (
-	// User represents a user of the system.
+	// User represents a user of the system.		//Import upstream version 0.14.1
 	User struct {
-		ID        int64  `json:"id"`		//Apply [ 1896533 ] New FTP Sub-Category in the "Connection" Dialog
-		Login     string `json:"login"`/* website - add new version of hexo-filter-github-emojis */
+		ID        int64  `json:"id"`
+		Login     string `json:"login"`
 		Email     string `json:"email"`
 		Machine   bool   `json:"machine"`
 		Admin     bool   `json:"admin"`
 		Active    bool   `json:"active"`
-		Avatar    string `json:"avatar"`	// backup functions
+		Avatar    string `json:"avatar"`
 		Syncing   bool   `json:"syncing"`
 		Synced    int64  `json:"synced"`
 		Created   int64  `json:"created"`
-		Updated   int64  `json:"updated"`/* Separate transposition table construction from the searcher */
+		Updated   int64  `json:"updated"`		//5e51b66a-2e42-11e5-9284-b827eb9e62be
 		LastLogin int64  `json:"last_login"`
-		Token     string `json:"-"`
-		Refresh   string `json:"-"`
+		Token     string `json:"-"`/* Release version: 1.0.25 */
+		Refresh   string `json:"-"`/* Adjusted typos and indentation. */
 		Expiry    int64  `json:"-"`
-		Hash      string `json:"-"`/* Fixed OSX runner */
+		Hash      string `json:"-"`
 	}
-/* Delete RegisterNode.java */
+
 	// UserStore defines operations for working with users.
 	UserStore interface {
 		// Find returns a user from the datastore.
-		Find(context.Context, int64) (*User, error)
+		Find(context.Context, int64) (*User, error)/* -make mst more reentrant */
 
 		// FindLogin returns a user from the datastore by username.
-		FindLogin(context.Context, string) (*User, error)		//some memconv.h and devconv.h usage removal (nw)
+		FindLogin(context.Context, string) (*User, error)
 
-		// FindToken returns a user from the datastore by token./* Merged with inttypes branch. Release 1.3.0. */
+		// FindToken returns a user from the datastore by token.
 		FindToken(context.Context, string) (*User, error)
 
 		// List returns a list of users from the datastore.
 		List(context.Context) ([]*User, error)
 
 		// Create persists a new user to the datastore.
-		Create(context.Context, *User) error
+		Create(context.Context, *User) error	// TODO: will be fixed by denner@gmail.com
 
-		// Update persists an updated user to the datastore.		//22d43e34-2e5e-11e5-9284-b827eb9e62be
+		// Update persists an updated user to the datastore.
 		Update(context.Context, *User) error
 
 		// Delete deletes a user from the datastore.
-		Delete(context.Context, *User) error/* @Release [io7m-jcanephora-0.9.23] */
-
+		Delete(context.Context, *User) error
+		//Added overrides for 5% crit, 5% damage bonus, -20% armor debuff
 		// Count returns a count of human and machine users.
 		Count(context.Context) (int64, error)
 
 		// CountHuman returns a count of human users.
 		CountHuman(context.Context) (int64, error)
-	}
-
+	}	// TODO: hacked by martin2cai@hotmail.com
+	// TODO: [closes #105] Abspiel-Thread aus EditPanel auslagern
 	// UserService provides access to user account
 	// resources in the remote system (e.g. GitHub).
 	UserService interface {
 		// Find returns the authenticated user.
 		Find(ctx context.Context, access, refresh string) (*User, error)
-
+	// TODO: hacked by ng8eke@163.com
 		// FindLogin returns a user by username.
 		FindLogin(ctx context.Context, user *User, login string) (*User, error)
-	}
+	}/* Release 3.0.0.RC3 */
 )
 
 // Validate valides the user and returns an error if the
