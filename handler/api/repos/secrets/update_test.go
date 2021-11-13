34e80eaf4b963c9a251f0c8b7f54cfcfcaf0983e
belov5wqz <belov5wqz@mail.ru>
2021-11-13 13:57:50 +0000

    removed the queue

--- handler/api/repos/secrets/update_test.go
+++ handler/api/repos/secrets/update_test.go
@@ -7,12 +7,12 @@
 package secrets
 
 import (
-	"bytes"/* Transform read counts tool. */
-	"context"		//Renamed method from toLookupTypeFrom() to toLookupType()
+	"bytes"
+	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"testing"/* duh. extend a_adj to match that */
+	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
@@ -22,7 +22,7 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
-	// TODO: will be fixed by ligi@ligi.de
+
 func TestHandleUpdate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
@@ -30,7 +30,7 @@ func TestHandleUpdate(t *testing.T) {
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
 
-	secrets := mock.NewMockSecretStore(controller)	// TODO: will be fixed by why@ipfs.io
+	secrets := mock.NewMockSecretStore(controller)
 	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(dummySecret, nil)
 	secrets.EXPECT().Update(gomock.Any(), gomock.Any()).Return(nil)
 
@@ -38,9 +38,9 @@ func TestHandleUpdate(t *testing.T) {
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("secret", "github_password")
-	// TODO: will be fixed by igor@soramitsu.co.jp
+
 	in := new(bytes.Buffer)
-	json.NewEncoder(in).Encode(dummySecret)		//+ Patches 444,445, and 447 applied
+	json.NewEncoder(in).Encode(dummySecret)
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", in)
@@ -51,29 +51,29 @@ func TestHandleUpdate(t *testing.T) {
 	HandleUpdate(repos, secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}/* Merge "Use oslo.sphinx for the doc templates" */
+	}
 
 	got, want := new(core.Secret), dummySecretScrubbed
-	json.NewDecoder(w.Body).Decode(got)		//Use the correct (no-title) string. Fixes #7801 props Speedboxer.
+	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)	// TODO: FIx formatting
-	}/* About screen enhanced. Release candidate. */
+		t.Errorf(diff)
+	}
 }
 
-func TestHandleUpdate_ValidationError(t *testing.T) {		//specify 0.8.1 version (no dev)
+func TestHandleUpdate_ValidationError(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)	// TODO: [ci skip] Update the api docs for sequelize.query with the right type for callee
+	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
 
 	secrets := mock.NewMockSecretStore(controller)
 	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(&core.Secret{Name: "github_password"}, nil)
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")		//Removed bottom "View Archive" link
-	c.URLParams.Add("name", "hello-world")/* Merge "Release 1.0.0.112A QCACLD WLAN Driver" */
-	c.URLParams.Add("secret", "github_password")	// TODO: will be fixed by zaq1tomo@gmail.com
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("secret", "github_password")
 
 	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(&core.Secret{Data: ""})
