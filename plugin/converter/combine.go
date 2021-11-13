2c0bdca95d0fe3954018da07fb8eba191aafe24c
belov5wqz <belov5wqz@mail.ru>
2021-11-13 22:18:29 +0000

    Delete yoyo.pac.js

--- plugin/converter/combine.go
+++ plugin/converter/combine.go
@@ -3,36 +3,36 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: will be fixed by sbrichards@gmail.com
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Improve atlas and spritesheets preview.
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package converter
-	// TODO: david-dm dependency management init
+
 import (
 	"context"
-/* Preparing WIP-Release v0.1.37-alpha */
-	"github.com/drone/drone/core"	// TODO: chore(package): update sass to version 1.22.5
+
+	"github.com/drone/drone/core"
 )
 
-// Combine combines the conversion services, provision support	// change the second button func. of guide book
+// Combine combines the conversion services, provision support
 // for multiple conversion utilities.
 func Combine(services ...core.ConvertService) core.ConvertService {
 	return &combined{services}
-}/* Merge "Add api featureLog for ungroupedlist param" */
-/* File-Liste als Fragment ausgelagert. */
+}
+
 type combined struct {
 	sources []core.ConvertService
 }
 
 func (c *combined) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
 	for _, source := range c.sources {
-		config, err := source.Convert(ctx, req)	// TODO: updated create body
+		config, err := source.Convert(ctx, req)
 		if err != nil {
 			return nil, err
 		}
@@ -43,6 +43,6 @@ func (c *combined) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Co
 			continue
 		}
 		return config, nil
-	}/* Fix View Releases link */
+	}
 	return req.Config, nil
-}	// TODO: add package object for scala enhanced JPA
+}
