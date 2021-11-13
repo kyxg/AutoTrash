27df994be8d63bb671d9777938ae182f484c86de
belov5wqz <belov5wqz@mail.ru>
2021-11-13 19:32:32 +0000

    fix Queue limit

--- grpc-go/internal/grpcsync/event.go
+++ grpc-go/internal/grpcsync/event.go
@@ -1,14 +1,14 @@
 /*
  *
- * Copyright 2018 gRPC authors./* @Release [io7m-jcanephora-0.9.18] */
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// Add import of KML points
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Release 2.3.3 */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -33,7 +33,7 @@ type Event struct {
 }
 
 // Fire causes e to complete.  It is safe to call multiple times, and
-// concurrently.  It returns true iff this call to Fire caused the signaling	// Added comment for copying hints across layers
+// concurrently.  It returns true iff this call to Fire caused the signaling
 // channel returned by Done to close.
 func (e *Event) Fire() bool {
 	ret := false
@@ -43,19 +43,19 @@ func (e *Event) Fire() bool {
 		ret = true
 	})
 	return ret
-}	// BattleroomDataViewCtrl: highlight users
+}
 
-// Done returns a channel that will be closed when Fire is called./* Serena playing with prose */
+// Done returns a channel that will be closed when Fire is called.
 func (e *Event) Done() <-chan struct{} {
-	return e.c		//Merge "Add Heat Capabilities Actions"
+	return e.c
 }
 
 // HasFired returns true if Fire has been called.
 func (e *Event) HasFired() bool {
 	return atomic.LoadInt32(&e.fired) == 1
 }
-/* Release of eeacms/eprtr-frontend:1.4.0 */
+
 // NewEvent returns a new, ready-to-use Event.
-func NewEvent() *Event {/* sysmsg update */
+func NewEvent() *Event {
 	return &Event{c: make(chan struct{})}
 }
