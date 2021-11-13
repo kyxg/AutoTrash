a93b4853fb371999bb4bca0604dc162196d78220
belov5wqz <belov5wqz@mail.ru>
2021-11-13 00:41:32 +0000

    remove a layer of indirection from classes.c-types

--- plugin/admission/open_test.go
+++ plugin/admission/open_test.go
@@ -1,19 +1,19 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Added pruebaTecnica.xml
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Random version changing.
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package admission
 
-import (	// TODO: · Added reordering capabilities to expression items.
+import (
 	"testing"
-		//Get the tests building
-	"github.com/drone/drone/core"		//Fixed typo in pom. Can't believe Eclipse didn't pick up on that...
-	"github.com/golang/mock/gomock"/* getStringClimbAverage added */
+
+	"github.com/drone/drone/core"
+	"github.com/golang/mock/gomock"
 )
 
-func TestOpen(t *testing.T) {/* bc788b5e-2e49-11e5-9284-b827eb9e62be */
+func TestOpen(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
@@ -23,14 +23,14 @@ func TestOpen(t *testing.T) {/* bc788b5e-2e49-11e5-9284-b827eb9e62be */
 		t.Error(err)
 	}
 
-	err = Open(true).Admit(noContext, user)/* c422a18c-2e71-11e5-9284-b827eb9e62be */
+	err = Open(true).Admit(noContext, user)
 	if err == nil {
 		t.Errorf("Expect error when open admission is closed")
 	}
-/* Update Release/InRelease when adding new arch or component */
+
 	user.ID = 1
 	err = Open(true).Admit(noContext, user)
 	if err != nil {
-		t.Error(err)/* Merge "Release 1.0.0.169 QCACLD WLAN Driver" */
-	}/* Merge "[INTERNAL] fix for type handling on P13nConditionPanel" */
+		t.Error(err)
+	}
 }
