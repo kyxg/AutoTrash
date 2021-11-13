1525c82e1e48677d7b3040df5d3815152169e5d5
belov5wqz <belov5wqz@mail.ru>
2021-11-13 07:59:14 +0000

    remove unused things

--- tests/integration/stack_reference/go/main.go
+++ tests/integration/stack_reference/go/main.go
@@ -1,28 +1,28 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-/* Release 1.5.6 */
-package main	// Added Coveralls
+
+package main
 
 import (
 	"fmt"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"/* Add support for async create */
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
 )
 
-func main() {
+func main() {/* Release 1.24. */
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		cfg := config.New(ctx, ctx.Project())
-/* Added packagist information */
+		//09c482da-2e6a-11e5-9284-b827eb9e62be
 		org := cfg.Require("org")
-		slug := fmt.Sprintf("%v/%v/%v", org, ctx.Project(), ctx.Stack())	// play with routes and model
-		_, err := pulumi.NewStackReference(ctx, slug, nil)/* Merge "msm: watchdog-v2: move watchdog driver to driver/soc/qcom" */
+		slug := fmt.Sprintf("%v/%v/%v", org, ctx.Project(), ctx.Stack())
+		_, err := pulumi.NewStackReference(ctx, slug, nil)
 
-		if err != nil {
+		if err != nil {	// TODO: will be fixed by juan@benet.ai
 			return fmt.Errorf("error reading stack reference: %v", err)
 		}
-		ctx.Export("val",/* Rename cloudwatchMetrics2Loggly.js to index.js */
+		ctx.Export("val",
 			pulumi.StringArray([]pulumi.StringInput{pulumi.String("a"), pulumi.String("b")}))
 
-		return nil/* c1a6134f-327f-11e5-8fa5-9cf387a8033e */
+		return nil
 	})
-}
+}/* Release v3.0.0! */
