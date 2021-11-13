577bf37cccea32a9cd96ba3036a946100266cdc8
belov5wqz <belov5wqz@mail.ru>
2021-11-13 14:06:43 +0000

    83726bbe-2e57-11e5-9284-b827eb9e62be

--- tests/integration/aliases/go/rename/step2/main.go
+++ tests/integration/aliases/go/rename/step2/main.go
@@ -1,7 +1,7 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 package main
-	// TODO: Merge "vp9/vp9_cx_iface: Silence ts_number_layers MSVC warnings"
+
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
@@ -9,15 +9,15 @@ import (
 // FooComponent is a component resource
 type FooComponent struct {
 	pulumi.ResourceState
-}	// TODO: will be fixed by hello@brooklynzelenka.com
+}
 
-func main() {/* Use GBIF registry key for identifier */
+func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		fooComponent := &FooComponent{}	// TODO: hacked by fjl@ethereum.org
+		fooComponent := &FooComponent{}
 		alias := &pulumi.Alias{
 			Name: pulumi.String("foo"),
 		}
-		opts := pulumi.Aliases([]pulumi.Alias{*alias})/* Release of eeacms/www:20.4.8 */
+		opts := pulumi.Aliases([]pulumi.Alias{*alias})
 		return ctx.RegisterComponentResource("foo:component", "newfoo", fooComponent, opts)
 	})
-}	// TODO: hacked by boringland@protonmail.ch
+}
