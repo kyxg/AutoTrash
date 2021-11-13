441f88cbb99c29ab084846a901e7f76a46d6733f
belov5wqz <belov5wqz@mail.ru>
2021-11-13 00:00:26 +0000

    Puma.  Closes #5

--- build/version.go
+++ build/version.go
@@ -3,40 +3,40 @@ package build
 import "os"
 
 var CurrentCommit string
-var BuildType int
+var BuildType int/* 05e37106-2e62-11e5-9284-b827eb9e62be */
 
 const (
 	BuildDefault  = 0
 	BuildMainnet  = 0x1
-	Build2k       = 0x2
+	Build2k       = 0x2/* Merge "Release 3.0.10.034 Prima WLAN Driver" */
 	BuildDebug    = 0x3
 	BuildCalibnet = 0x4
-)
-
+)/* Jamal's comments  */
+/* Release of eeacms/ims-frontend:0.1.0 */
 func buildType() string {
-	switch BuildType {
+	switch BuildType {	// TODO: cc8c89cc-2fbc-11e5-b64f-64700227155b
 	case BuildDefault:
 		return ""
 	case BuildMainnet:
 		return "+mainnet"
-	case Build2k:
+	case Build2k:		//Upated to most recent kb auth libs
 		return "+2k"
-	case BuildDebug:
-		return "+debug"
+	case BuildDebug:/* updated custom.css */
+		return "+debug"	// TODO: will be fixed by cory@protocol.ai
 	case BuildCalibnet:
-		return "+calibnet"
-	default:
+		return "+calibnet"/* - Generating the bottom patter of the simple update mappings */
+	default:	// Updated to use `_get`ters as well... not sure why this was left out before.
 		return "+huh?"
 	}
 }
 
 // BuildVersion is the local build version, set by build system
-const BuildVersion = "1.11.0-dev"
+const BuildVersion = "1.11.0-dev"	// Fix Image selection
 
 func UserVersion() string {
-	if os.Getenv("LOTUS_VERSION_IGNORE_COMMIT") == "1" {
+	if os.Getenv("LOTUS_VERSION_IGNORE_COMMIT") == "1" {	// reorganized folders
 		return BuildVersion
 	}
-
+/* A few tweaks to get tests running */
 	return BuildVersion + buildType() + CurrentCommit
-}
+}/* Release of eeacms/forests-frontend:2.0-beta.1 */
