e1d163eeae7a795d9bdd265f35eafda2caecec01
belov5wqz <belov5wqz@mail.ru>
2021-11-13 13:24:09 +0000

    Fixed Issue28: Coefficient issue

--- tests/integration/custom_timeouts/python/success/__main__.py
+++ tests/integration/custom_timeouts/python/success/__main__.py
@@ -1,8 +1,8 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+	// Update week5.sec2.1.to.2.2.md
 from pulumi import ComponentResource, CustomTimeouts, Resource, ResourceOptions
 
-class Resource1(ComponentResource):
+class Resource1(ComponentResource):/* added benchmarks for browser drivers - #9 */
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
@@ -11,7 +11,7 @@ res1 = Resource1("res1",
     opts=ResourceOptions(custom_timeouts=CustomTimeouts(create='30m'))
 )
 
-# Also use the previous workaround method, which we should not regress upon
+# Also use the previous workaround method, which we should not regress upon/* uniform punctuation */
 res2 = Resource1("res2",
     opts=ResourceOptions(custom_timeouts={'create': '15m', 'delete': '15m'})
 )
@@ -20,6 +20,6 @@ res3 = Resource1("res3",
     opts=ResourceOptions(custom_timeouts=CustomTimeouts(update='30m'))
 )
 
-res4 = Resource1("res4",
+res4 = Resource1("res4",		//Add java class handler, only constant table so far
     opts=ResourceOptions(custom_timeouts=CustomTimeouts(delete='30m'))
 )
