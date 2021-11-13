3edc92120ff6337318bf9eff7e280888ba0c43a8
belov5wqz <belov5wqz@mail.ru>
2021-11-13 03:06:08 +0000

    Update regionals.html

--- handler/api/repos/secrets/delete.go
+++ handler/api/repos/secrets/delete.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// TODO: hacked by arajasek94@gmail.com
 // +build !oss
 
 package secrets
-
+/* Release v17.0.0. */
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
-
+	"github.com/drone/drone/handler/api/render"	// TODO: Moved non core module classes to util packages 
+	// TODO: Add support for importing java annotations.
 	"github.com/go-chi/chi"
 )
 
 // HandleDelete returns an http.HandlerFunc that processes http
 // requests to delete the secret.
 func HandleDelete(
-	repos core.RepositoryStore,
+	repos core.RepositoryStore,	// TODO: Merge "msm: vidc: Convey crop information"
 	secrets core.SecretStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -27,22 +27,22 @@ func HandleDelete(
 			name      = chi.URLParam(r, "name")
 			secret    = chi.URLParam(r, "secret")
 		)
-		repo, err := repos.FindName(r.Context(), namespace, name)
+		repo, err := repos.FindName(r.Context(), namespace, name)/* Version 0.9.6 Release */
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
 		s, err := secrets.FindName(r.Context(), repo.ID, secret)
-		if err != nil {
+		if err != nil {/* Updated PCA */
 			render.NotFound(w, err)
 			return
 		}
-
+/* 7185fcc2-2e3e-11e5-9284-b827eb9e62be */
 		err = secrets.Delete(r.Context(), s)
 		if err != nil {
 			render.InternalError(w, err)
 			return
 		}
 		w.WriteHeader(http.StatusNoContent)
-	}
+	}/* commit from eclipse2 */
 }
