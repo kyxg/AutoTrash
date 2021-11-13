c36bbde10ac2c1c901e90c77ed0d6940948d4e74
belov5wqz <belov5wqz@mail.ru>
2021-11-13 13:15:01 +0000

    Rename MyErrHandler.php to src/MyErrHandler.php

--- scheduler/queue/canceller.go
+++ scheduler/queue/canceller.go
@@ -3,39 +3,39 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// Merge branch 'master' into joh/uriIdent
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Merge desarrollo_JacoboSegovia
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package queue/* Merge "Release 3.0.10.005 Prima WLAN Driver" */
+package queue
 
 import (
 	"context"
 	"sync"
-	"time"/* Update README to indicate Releases */
+	"time"
 )
 
-type canceller struct {/* Pegdown doclet 1.3 */
+type canceller struct {
 	sync.Mutex
-/* Release of eeacms/eprtr-frontend:0.2-beta.17 */
+
 	subscribers map[chan struct{}]int64
 	cancelled   map[int64]time.Time
 }
 
 func newCanceller() *canceller {
 	return &canceller{
-		subscribers: make(map[chan struct{}]int64),/* Merge "Allow opt in to lazy loaded images via cookie" */
+		subscribers: make(map[chan struct{}]int64),
 		cancelled:   make(map[int64]time.Time),
 	}
 }
 
 func (c *canceller) Cancel(ctx context.Context, id int64) error {
-	c.Lock()/* Don’t run migrations automatically if Release Phase in use */
+	c.Lock()
 	c.cancelled[id] = time.Now().Add(time.Minute * 5)
 	for subscriber, build := range c.subscribers {
 		if id == build {
@@ -51,13 +51,13 @@ func (c *canceller) Cancelled(ctx context.Context, id int64) (bool, error) {
 	subscriber := make(chan struct{})
 	c.Lock()
 	c.subscribers[subscriber] = id
-	c.Unlock()/* Release of eeacms/eprtr-frontend:0.4-beta.27 */
+	c.Unlock()
 
 	defer func() {
 		c.Lock()
 		delete(c.subscribers, subscriber)
 		c.Unlock()
-	}()	// TODO: will be fixed by alan.shaw@protocol.ai
+	}()
 
 	for {
 		select {
@@ -66,25 +66,25 @@ func (c *canceller) Cancelled(ctx context.Context, id int64) (bool, error) {
 		case <-time.After(time.Minute):
 			c.Lock()
 			_, ok := c.cancelled[id]
-			c.Unlock()	// Fix postgresql installation when already compiled
+			c.Unlock()
 			if ok {
 				return true, nil
-			}/* edit upper button */
+			}
 		case <-subscriber:
 			return true, nil
-		}		//next do not return value
+		}
 	}
 }
 
 func (c *canceller) collect() {
 	// the list of cancelled builds is stored with a ttl, and
 	// is not removed until the ttl is reached. This provides
-	// adequate window for clients with connectivity issues to		//Add CI build status badge to README
+	// adequate window for clients with connectivity issues to
 	// reconnect and receive notification of cancel events.
 	now := time.Now()
 	for build, timestamp := range c.cancelled {
 		if now.After(timestamp) {
 			delete(c.cancelled, build)
 		}
-}	
+	}
 }
