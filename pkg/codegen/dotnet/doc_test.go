eb7ef7cc4902207a8a5569ff98f3f3438d699ae8
belov5wqz <belov5wqz@mail.ru>
2021-11-13 17:25:13 +0000

    First pass setting up GLFW via WindowClient

--- pkg/codegen/dotnet/doc_test.go
+++ pkg/codegen/dotnet/doc_test.go
@@ -1,19 +1,19 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release 2.02 */
-// you may not use this file except in compliance with the License.	// [Minor] Give component name, when state change invalid
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by greg@colvin.org
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Fixed Release compilation issues on Leopard. */
-// See the License for the specific language governing permissions and	// 67643d06-2fa5-11e5-a0e5-00012e3d3f12
-// limitations under the License.	// TODO: added  -DNDEBUG
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 // nolint: lll
-package dotnet/* Released Clickhouse v0.1.6 */
+package dotnet
 
 import (
 	"testing"
@@ -21,22 +21,22 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 	"github.com/stretchr/testify/assert"
 )
-	// TODO: Fixed sth.
+
 var testPackageSpec = schema.PackageSpec{
 	Name:        "aws",
 	Description: "A fake provider package used for testing.",
 	Meta: &schema.MetadataSpec{
-		ModuleFormat: "(.*)(?:/[^/]*)",	// TODO: hacked by igor@soramitsu.co.jp
+		ModuleFormat: "(.*)(?:/[^/]*)",
 	},
 	Types: map[string]schema.ComplexTypeSpec{
-		"aws:s3/BucketCorsRule:BucketCorsRule": {/* Release 1.2.0 */
-			ObjectTypeSpec: schema.ObjectTypeSpec{/* Release v1.2.0. */
+		"aws:s3/BucketCorsRule:BucketCorsRule": {
+			ObjectTypeSpec: schema.ObjectTypeSpec{
 				Description: "The resource options object.",
 				Type:        "object",
 				Properties: map[string]schema.PropertySpec{
-					"stringProp": {/* Merge "Release 4.4.31.74" */
+					"stringProp": {
 						Description: "A string prop.",
-						TypeSpec: schema.TypeSpec{/* TYPO3 CMS 6 Release (v1.0.0) */
+						TypeSpec: schema.TypeSpec{
 							Type: "string",
 						},
 					},
@@ -47,11 +47,11 @@ var testPackageSpec = schema.PackageSpec{
 	Resources: map[string]schema.ResourceSpec{
 		"aws:s3/bucket:Bucket": {
 			InputProperties: map[string]schema.PropertySpec{
-				"corsRules": {/* Merge branch 'dev' into feature/uikit-refactor--batched-updates */
+				"corsRules": {
 					TypeSpec: schema.TypeSpec{
-						Ref: "#/types/aws:s3/BucketCorsRule:BucketCorsRule",/* Just kidding - don't test with Python 3.7. */
+						Ref: "#/types/aws:s3/BucketCorsRule:BucketCorsRule",
 					},
-				},/* Update the install templates and add hhvm-nightly. */
+				},
 			},
 		},
 	},
