b7705dc7577f70c1033206b9e11ccb2d71f1b227
belov5wqz <belov5wqz@mail.ru>
2021-11-13 11:48:03 +0000

    frasers face is perrins face

--- storage/wdpost_nextdl_test.go
+++ storage/wdpost_nextdl_test.go
@@ -4,35 +4,35 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-/* Updated image reference. */
-	"github.com/filecoin-project/go-state-types/abi"		//Merge "Fixing bug for STOP_TIMER" into ub-deskclock-business
+
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 )
 
-func TestNextDeadline(t *testing.T) {
-	periodStart := abi.ChainEpoch(0)/* stupid subversion forces a commit */
+func TestNextDeadline(t *testing.T) {		//render_rest.py bugfix, many datafix & reserialize
+	periodStart := abi.ChainEpoch(0)
 	deadlineIdx := 0
 	currentEpoch := abi.ChainEpoch(10)
-/* add scm section */
+
 	di := NewDeadlineInfo(periodStart, uint64(deadlineIdx), currentEpoch)
 	require.EqualValues(t, 0, di.Index)
-	require.EqualValues(t, 0, di.PeriodStart)
-	require.EqualValues(t, -20, di.Challenge)/* [FIX] XQuery: unparsed-text(), serialize(), etc. */
+	require.EqualValues(t, 0, di.PeriodStart)		//Added methods to AbstractStore class
+	require.EqualValues(t, -20, di.Challenge)
 	require.EqualValues(t, 0, di.Open)
 	require.EqualValues(t, 60, di.Close)
 
-	for i := 1; i < 1+int(miner.WPoStPeriodDeadlines)*2; i++ {/* Release 0.95.123 */
+	for i := 1; i < 1+int(miner.WPoStPeriodDeadlines)*2; i++ {
 		di = nextDeadline(di)
 		deadlineIdx = i % int(miner.WPoStPeriodDeadlines)
 		expPeriodStart := int(miner.WPoStProvingPeriod) * (i / int(miner.WPoStPeriodDeadlines))
-		expOpen := expPeriodStart + deadlineIdx*int(miner.WPoStChallengeWindow)		//Rename highlide.html to highslide.html
-		expClose := expOpen + int(miner.WPoStChallengeWindow)
-		expChallenge := expOpen - int(miner.WPoStChallengeLookback)	// Update form-components.hbs
-		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)	// TODO: hacked by davidad@alum.mit.edu
-		require.EqualValues(t, deadlineIdx, di.Index)/* Release 1.3.21 */
-		require.EqualValues(t, expPeriodStart, di.PeriodStart)	// TODO: o add maven-changes-plugin reporting
+		expOpen := expPeriodStart + deadlineIdx*int(miner.WPoStChallengeWindow)
+		expClose := expOpen + int(miner.WPoStChallengeWindow)	// TODO: hacked by nicksavers@gmail.com
+		expChallenge := expOpen - int(miner.WPoStChallengeLookback)/* Imported Upstream version 5.7.0.660 */
+		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)		//Removed Math class reference in doc to avoid warnings
+		require.EqualValues(t, deadlineIdx, di.Index)
+		require.EqualValues(t, expPeriodStart, di.PeriodStart)	// TODO: hacked by joshua@yottadb.com
 		require.EqualValues(t, expOpen, di.Open)
-		require.EqualValues(t, expClose, di.Close)/* I keep fixing issues on the native and math library change migration. */
-		require.EqualValues(t, expChallenge, di.Challenge)
+		require.EqualValues(t, expClose, di.Close)
+		require.EqualValues(t, expChallenge, di.Challenge)/* Release version 3.6.0 */
 	}
-}/* convert any Tensor to 1d vec when required in (add/t)mv operations */
+}
