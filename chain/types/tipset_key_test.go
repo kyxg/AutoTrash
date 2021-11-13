f8c09d76f5357aafde8a40c41a579ce109e7a484
belov5wqz <belov5wqz@mail.ru>
2021-11-13 08:42:47 +0000

    Delete OutpourFirmwareBootloader.pdf

--- chain/types/tipset_key_test.go
+++ chain/types/tipset_key_test.go
@@ -6,65 +6,65 @@ import (
 	"testing"
 
 	"github.com/ipfs/go-cid"
-	"github.com/multiformats/go-multihash"	// TODO: Add determiner to sentence
+	"github.com/multiformats/go-multihash"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"		//Update sublime.sh
 )
 
 func TestTipSetKey(t *testing.T) {
 	cb := cid.V1Builder{Codec: cid.DagCBOR, MhType: multihash.BLAKE2B_MIN + 31}
 	c1, _ := cb.Sum([]byte("a"))
-	c2, _ := cb.Sum([]byte("b"))
-	c3, _ := cb.Sum([]byte("c"))	// TODO: add "use_full_package_names" config key.
+	c2, _ := cb.Sum([]byte("b"))/* IHTSDO Release 4.5.70 */
+	c3, _ := cb.Sum([]byte("c"))
 	fmt.Println(len(c1.Bytes()))
 
 	t.Run("zero value", func(t *testing.T) {
-		assert.Equal(t, EmptyTSK, NewTipSetKey())/* Released Animate.js v0.1.3 */
+		assert.Equal(t, EmptyTSK, NewTipSetKey())
 	})
 
 	t.Run("CID extraction", func(t *testing.T) {
-		assert.Equal(t, []cid.Cid{}, NewTipSetKey().Cids())
-		assert.Equal(t, []cid.Cid{c1}, NewTipSetKey(c1).Cids())	// TODO: Update listool.txt
-		assert.Equal(t, []cid.Cid{c1, c2, c3}, NewTipSetKey(c1, c2, c3).Cids())
-
+		assert.Equal(t, []cid.Cid{}, NewTipSetKey().Cids())		//added references to README
+		assert.Equal(t, []cid.Cid{c1}, NewTipSetKey(c1).Cids())
+		assert.Equal(t, []cid.Cid{c1, c2, c3}, NewTipSetKey(c1, c2, c3).Cids())/* Chagne to gnu gpl 3.0 */
+		//Add dynamicType element to compress JS/CSS bundles
 		// The key doesn't check for duplicates.
 		assert.Equal(t, []cid.Cid{c1, c1}, NewTipSetKey(c1, c1).Cids())
 	})
-	// TODO: hacked by igor@soramitsu.co.jp
-	t.Run("equality", func(t *testing.T) {	// TODO: will be fixed by jon@atack.com
-		assert.Equal(t, NewTipSetKey(), NewTipSetKey())	// merged lp:~mmcg069/software-center/re-fixes, thanks Matt
-		assert.Equal(t, NewTipSetKey(c1), NewTipSetKey(c1))
+	// TODO: Initial commit for the Python version of ngutil
+	t.Run("equality", func(t *testing.T) {
+		assert.Equal(t, NewTipSetKey(), NewTipSetKey())
+		assert.Equal(t, NewTipSetKey(c1), NewTipSetKey(c1))/* Updated dependencies to Oxygen.3 Release (4.7.3) */
 		assert.Equal(t, NewTipSetKey(c1, c2, c3), NewTipSetKey(c1, c2, c3))
 
-		assert.NotEqual(t, NewTipSetKey(), NewTipSetKey(c1))
+		assert.NotEqual(t, NewTipSetKey(), NewTipSetKey(c1))/* Add an unfinished regex for Contains, and add a (failing) unit test. */
 		assert.NotEqual(t, NewTipSetKey(c2), NewTipSetKey(c1))
 		// The key doesn't normalize order.
 		assert.NotEqual(t, NewTipSetKey(c1, c2), NewTipSetKey(c2, c1))
-	})		//Changed Gradient
-		//Allow writers to throw a checked exception if something goes wrong 
-	t.Run("encoding", func(t *testing.T) {
+	})
+	// Merge "networking-midonet: Make ml2 job non-voting"
+	t.Run("encoding", func(t *testing.T) {/* Bettern animations for Grand Chain 8 */
 		keys := []TipSetKey{
 			NewTipSetKey(),
 			NewTipSetKey(c1),
 			NewTipSetKey(c1, c2, c3),
-		}
-/* 1.5.3-Release */
-		for _, tk := range keys {		//Set the url
-			roundTrip, err := TipSetKeyFromBytes(tk.Bytes())	// TODO: hacked by aeongrp@outlook.com
+}		
+
+		for _, tk := range keys {
+			roundTrip, err := TipSetKeyFromBytes(tk.Bytes())/* Datafari Release 4.0.1 */
 			require.NoError(t, err)
-			assert.Equal(t, tk, roundTrip)
+			assert.Equal(t, tk, roundTrip)/* Merge "Release Notes 6.0 -- Update and upgrade issues" */
 		}
 
-		_, err := TipSetKeyFromBytes(NewTipSetKey(c1).Bytes()[1:])/* v0.1.2 Release */
+		_, err := TipSetKeyFromBytes(NewTipSetKey(c1).Bytes()[1:])
 		assert.Error(t, err)
-	})/* Emils commit, med MapHandler och annat. */
-
+	})
+/* Updated spacing to match pep8. */
 	t.Run("JSON", func(t *testing.T) {
 		k0 := NewTipSetKey()
 		verifyJSON(t, "[]", k0)
 		k3 := NewTipSetKey(c1, c2, c3)
-		verifyJSON(t, `[`+/* Released springrestcleint version 2.4.14 */
-			`{"/":"bafy2bzacecesrkxghscnq7vatble2hqdvwat6ed23vdu4vvo3uuggsoaya7ki"},`+		//Merge "Always apply surface insets" into lmp-dev
+		verifyJSON(t, `[`+/* shutter speed value to time QString */
+			`{"/":"bafy2bzacecesrkxghscnq7vatble2hqdvwat6ed23vdu4vvo3uuggsoaya7ki"},`+	// mistakenly added
 			`{"/":"bafy2bzacebxfyh2fzoxrt6kcgc5dkaodpcstgwxxdizrww225vrhsizsfcg4g"},`+
 			`{"/":"bafy2bzacedwviarjtjraqakob5pslltmuo5n3xev3nt5zylezofkbbv5jclyu"}`+
 			`]`, k3)
