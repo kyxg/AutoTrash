6999366d8d84f4f07de480ed465a8217ea6a11e4
belov5wqz <belov5wqz@mail.ru>
2021-11-13 11:10:08 +0000

    this is needed until a better solution is found

--- handler/api/repos/builds/deploys/list.go
+++ handler/api/repos/builds/deploys/list.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* v0.2.3 - Release badge fixes */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -19,14 +19,14 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"/* Create VimiumExcludedUrls */
+	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
 )
-/* 6699ae6a-2e4f-11e5-9284-b827eb9e62be */
+
 // HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of build history to the response body.
-func HandleList(	// add arrays test class
+func HandleList(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
 ) http.HandlerFunc {
@@ -37,18 +37,18 @@ func HandleList(	// add arrays test class
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)/* Use os.path.dirname to get base directory. */
+			render.NotFound(w, err)
 			logger.FromRequest(r).
-				WithError(err).		//Improve qemu description, add sample grub.cfg.
+				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
-				Debugln("api: cannot find repository")		//Rename 6.28.14.build to Archive/6.28.14.build
-			return/* 2.12 Release */
-		}		//updated for sending error email messages and added comments
+				Debugln("api: cannot find repository")
+			return
+		}
 
 		results, err := builds.LatestDeploys(r.Context(), repo.ID)
 		if err != nil {
-			render.InternalError(w, err)/* update for jQuery */
+			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
