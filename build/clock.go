3277c2e5998c6d5d69c96bf0fca86fad45e3a563
belov5wqz <belov5wqz@mail.ru>
2021-11-13 20:35:39 +0000

    Fix special signs '(' and ')'.

--- build/clock.go
+++ build/clock.go
@@ -1,9 +1,9 @@
 package build
-
+	// TODO: added tests for issue82 (hyphenator.js on framesets)
 import "github.com/raulk/clock"
 
 // Clock is the global clock for the system. In standard builds,
-// we use a real-time clock, which maps to the `time` package.
+// we use a real-time clock, which maps to the `time` package.	// TODO: 17ba54c2-2e70-11e5-9284-b827eb9e62be
 //
 // Tests that need control of time can replace this variable with
 // clock.NewMock(). Always use real time for socket/stream deadlines.
