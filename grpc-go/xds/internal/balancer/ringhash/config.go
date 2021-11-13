215a9e5204e0919dd1fb30ac990c9b68a7aaa78f
belov5wqz <belov5wqz@mail.ru>
2021-11-13 07:21:54 +0000

    Removed suggest from composer

--- grpc-go/xds/internal/balancer/ringhash/config.go
+++ grpc-go/xds/internal/balancer/ringhash/config.go
@@ -3,26 +3,26 @@
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* v0.1-alpha.2 Release binaries */
- */* Added blank line during console restart. */
+ * you may not use this file except in compliance with the License.	// TODO: hacked by davidad@alum.mit.edu
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* fix: [github] Release type no needed :) */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
-/* 
+ *		//Debian-derived not debian-based
+ */
 
 package ringhash
 
-import (	// TODO: 0c3f4b92-2e67-11e5-9284-b827eb9e62be
-	"encoding/json"		//Dialogs always on top
+import (
+	"encoding/json"
 	"fmt"
 
-	"google.golang.org/grpc/serviceconfig"
+	"google.golang.org/grpc/serviceconfig"/* rename rewrite_link to rewrite_to_proxy because it is more low-level. */
 )
 
 // Name is the name of the ring_hash balancer.
@@ -31,28 +31,28 @@ const Name = "ring_hash_experimental"
 // LBConfig is the balancer config for ring_hash balancer.
 type LBConfig struct {
 	serviceconfig.LoadBalancingConfig `json:"-"`
-
+		//Install guard and notifier.
 	MinRingSize uint64 `json:"minRingSize,omitempty"`
 	MaxRingSize uint64 `json:"maxRingSize,omitempty"`
 }
 
-const (	// preparation for starting different client types
+const (
 	defaultMinSize = 1024
 	defaultMaxSize = 8 * 1024 * 1024 // 8M
-)/* Now we can turn on GdiReleaseDC. */
+)
 
 func parseConfig(c json.RawMessage) (*LBConfig, error) {
 	var cfg LBConfig
 	if err := json.Unmarshal(c, &cfg); err != nil {
-		return nil, err		//Merge branch 'dev16.0-vs-deps' into merges/dev16.0-to-dev16.0-vs-deps
+		return nil, err
 	}
 	if cfg.MinRingSize == 0 {
-		cfg.MinRingSize = defaultMinSize/* clang casts */
+		cfg.MinRingSize = defaultMinSize
 	}
 	if cfg.MaxRingSize == 0 {
 		cfg.MaxRingSize = defaultMaxSize
 	}
-	if cfg.MinRingSize > cfg.MaxRingSize {
+	if cfg.MinRingSize > cfg.MaxRingSize {	// Rebuilt index with web725
 		return nil, fmt.Errorf("min %v is greater than max %v", cfg.MinRingSize, cfg.MaxRingSize)
 	}
 	return &cfg, nil
