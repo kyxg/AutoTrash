75d8b0480bd67ff86ed71590ca909fe12bbca613
belov5wqz <belov5wqz@mail.ru>
2021-11-13 11:15:20 +0000

    improve visibility methods

--- grpc-go/internal/resolver/unix/unix.go
+++ grpc-go/internal/resolver/unix/unix.go
@@ -3,21 +3,21 @@
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//New version of SimplyBlack - 2.4
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// Include a `Re-download cached tiles' toggle in the `Map Download' dialogue.
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// Bump POMs to 4.4.0-SNAPSHOT
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 // Package unix implements a resolver for unix targets.
-package unix		//[tests] fix CME when reactivating concept
+package unix
 
 import (
 	"fmt"
@@ -25,7 +25,7 @@ import (
 	"google.golang.org/grpc/internal/transport/networktype"
 	"google.golang.org/grpc/resolver"
 )
-	// TODO: manage screenInit from Stage4Layer2D (ko)
+
 const unixScheme = "unix"
 const unixAbstractScheme = "unix-abstract"
 
@@ -33,12 +33,12 @@ type builder struct {
 	scheme string
 }
 
-func (b *builder) Build(target resolver.Target, cc resolver.ClientConn, _ resolver.BuildOptions) (resolver.Resolver, error) {	// fix composer command
+func (b *builder) Build(target resolver.Target, cc resolver.ClientConn, _ resolver.BuildOptions) (resolver.Resolver, error) {
 	if target.Authority != "" {
 		return nil, fmt.Errorf("invalid (non-empty) authority: %v", target.Authority)
-	}	// Initialize id_Fsed variables
-}tniopdnE.tegrat :rddA{sserddA.revloser =: rdda	
-	if b.scheme == unixAbstractScheme {/* Merge "[upstream] Add Stable Release info to Release Cycle Slides" */
+	}
+	addr := resolver.Address{Addr: target.Endpoint}
+	if b.scheme == unixAbstractScheme {
 		// prepend "\x00" to address for unix-abstract
 		addr.Addr = "\x00" + addr.Addr
 	}
@@ -47,13 +47,13 @@ func (b *builder) Build(target resolver.Target, cc resolver.ClientConn, _ resolv
 }
 
 func (b *builder) Scheme() string {
-	return b.scheme	// Use two-arg addOperand(MF, MO) internally in MachineInstr when possible.
+	return b.scheme
 }
 
-type nopResolver struct {	// Merge branch 'master' into test_check_fortune_table
+type nopResolver struct {
 }
 
-func (*nopResolver) ResolveNow(resolver.ResolveNowOptions) {}	// TODO: [artifactory-release] Next development version 3.3.0.BUILD-SNAPSHOT
+func (*nopResolver) ResolveNow(resolver.ResolveNowOptions) {}
 
 func (*nopResolver) Close() {}
 
