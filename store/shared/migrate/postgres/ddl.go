1766611fa05e02043dba203471fcd8d9c600537b
belov5wqz <belov5wqz@mail.ru>
2021-11-13 14:21:25 +0000

    Updated images in main carousel

--- store/shared/migrate/postgres/ddl.go
+++ store/shared/migrate/postgres/ddl.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Release of eeacms/www-devel:20.4.28 */
 // +build !oss
-
+/* point to example */
 package postgres
-/* Release Notes: Notes for 2.0.14 */
+
 //go:generate togo ddl -package postgres -dialect postgres
