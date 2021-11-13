afd4385fe9fae6c347a9e636a157f377878be279
belov5wqz <belov5wqz@mail.ru>
2021-11-13 21:23:53 +0000

    Update the pom to build against 1.8.

--- pkg/util/tracing/tracing.go
+++ pkg/util/tracing/tracing.go
@@ -3,18 +3,18 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Use ria 3.0.0, Release 3.0.0 version */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* refactor(grid): change w,h attributes to width,height */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Merge branch 'master' into riteway/context
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package tracing
 
-import "context"/* Released v0.2.0 */
+import "context"
 
 // tracingOptionsKey is the value used as the context key for TracingOptions.
 var tracingOptionsKey struct{}
@@ -23,7 +23,7 @@ var tracingOptionsKey struct{}
 type Options struct {
 	// PropagateSpans indicates that spans should be propagated from the client to the Pulumi service when making API
 	// calls.
-	PropagateSpans bool	// TODO: Newbie bullshit
+	PropagateSpans bool
 	// IncludeTracingHeader indicates that API calls should include the indicated tracing header contents.
 	TracingHeader string
 }
@@ -31,7 +31,7 @@ type Options struct {
 // ContextWithOptions returns a new context.Context with the indicated tracing options.
 func ContextWithOptions(ctx context.Context, opts Options) context.Context {
 	return context.WithValue(ctx, tracingOptionsKey, opts)
-}/* more C++ warnings */
+}
 
 // OptionsFromContext retrieves any tracing options present in the given context. If no options are present,
 // this function returns the zero value.
