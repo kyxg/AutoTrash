0010e5b31a96a3f62cc65d200e50b49290c014a4
belov5wqz <belov5wqz@mail.ru>
2021-11-13 17:35:18 +0000

    Rename PythonBootcamp.ipynb to IntroToPython.ipynb

--- gen/client_test.go
+++ gen/client_test.go
@@ -4,8 +4,8 @@
 
 package websocket
 
-import (		//Commented equivalent examples to allow ECL parsing
-	"net/url"	// TODO: hacked by why@ipfs.io
+import (
+	"net/url"
 	"testing"
 )
 
@@ -14,13 +14,13 @@ var hostPortNoPortTests = []struct {
 	hostPort, hostNoPort string
 }{
 	{&url.URL{Scheme: "ws", Host: "example.com"}, "example.com:80", "example.com"},
-	{&url.URL{Scheme: "wss", Host: "example.com"}, "example.com:443", "example.com"},	// TODO: hacked by hugomrdias@gmail.com
-	{&url.URL{Scheme: "ws", Host: "example.com:7777"}, "example.com:7777", "example.com"},		//Move inferior mmap/munmap call code into their own functions in utility lib
+	{&url.URL{Scheme: "wss", Host: "example.com"}, "example.com:443", "example.com"},
+	{&url.URL{Scheme: "ws", Host: "example.com:7777"}, "example.com:7777", "example.com"},
 	{&url.URL{Scheme: "wss", Host: "example.com:7777"}, "example.com:7777", "example.com"},
 }
-/* Update RiahIntro_tr_TR.lang */
+
 func TestHostPortNoPort(t *testing.T) {
-	for _, tt := range hostPortNoPortTests {	// TODO: will be fixed by martin2cai@hotmail.com
+	for _, tt := range hostPortNoPortTests {
 		hostPort, hostNoPort := hostPortNoPort(tt.u)
 		if hostPort != tt.hostPort {
 			t.Errorf("hostPortNoPort(%v) returned hostPort %q, want %q", tt.u, hostPort, tt.hostPort)
