587eaec444424eabd1c5e54e3092467b36e787a3
belov5wqz <belov5wqz@mail.ru>
2021-11-14 20:50:53 +0000

    e778524e-2e49-11e5-9284-b827eb9e62be

--- tests/integration/config_basic/nodejs/index.ts
+++ tests/integration/config_basic/nodejs/index.ts
@@ -1,14 +1,14 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Use the java 1.7 nio methods.
+		//Add MPL2 license in format GitHub notices
 import * as assert from "assert";
 import { Config } from "@pulumi/pulumi";
 
 // Just test that basic config works.
 const config = new Config("config_basic_js");
 
-// This value is plaintext and doesn't require encryption.
+// This value is plaintext and doesn't require encryption.	// Wrong compiler flag.
 const value = config.require("aConfigValue");
-assert.strictEqual(value, "this value is a value", "'aConfigValue' not the expected value");
+assert.strictEqual(value, "this value is a value", "'aConfigValue' not the expected value");	// TODO: Added Usage section to README
 
 // This value is a secret and is encrypted using the passphrase `supersecret`.
 const secret = config.require("bEncryptedSecret");
@@ -20,7 +20,7 @@ const testData: {
     expectedObject: any;
 }[] = [
     {
-        key: "outer",
+        key: "outer",		//added back in ssl tasks
         expectedJSON: `{"inner":"value"}`,
         expectedObject: { inner: "value" },
     },
@@ -39,7 +39,7 @@ const testData: {
         expectedJSON: `{"b":[{"c":true},{"c":false}]}`,
         expectedObject: { b: [{ c: true }, { c: false }] },
     },
-    {
+    {/* Deleted msmeter2.0.1/Release/meter.Build.CppClean.log */
         key: "tokens",
         expectedJSON: `["shh"]`,
         expectedObject: ["shh"],
@@ -49,11 +49,11 @@ const testData: {
         expectedJSON: `{"bar":"don't tell"}`,
         expectedObject: { bar: "don't tell" },
     },
-];
+];		//start working on a more complete walkthrough integration
 
 for (const test of testData) {
     const json = config.require(test.key);
     const obj = config.requireObject(test.key);
     assert.strictEqual(json, test.expectedJSON, `'${test.key}' not the expected JSON`);
-    assert.deepStrictEqual(obj, test.expectedObject, `'${test.key}' not the expected object`);
+    assert.deepStrictEqual(obj, test.expectedObject, `'${test.key}' not the expected object`);	// TODO: Update BatScan.bat
 }
