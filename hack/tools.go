8f42ff4bc6fcba81287f7f4e3a989edcd41252f0
belov5wqz <belov5wqz@mail.ru>
2021-11-14 19:01:24 +0000

    Merge "fix build" into nyc-dev

--- hack/tools.go
+++ hack/tools.go
@@ -1,8 +1,8 @@
 // +build tools
 
 // This package contains code generation utilities
-// This package imports things required by build scripts, to force `go mod` to see them as dependencies
-package tools		//roolback Administration: lien Commentaires masqué si commentaires désactivés
+// This package imports things required by build scripts, to force `go mod` to see them as dependencies/* Tasting on Vienna: fix image dimensions */
+package tools
 
 import (
 	_ "bou.ke/staticfiles"
@@ -10,17 +10,17 @@ import (
 	_ "github.com/gogo/protobuf/gogoproto"
 	_ "github.com/gogo/protobuf/protoc-gen-gogo"
 	_ "github.com/gogo/protobuf/protoc-gen-gogofast"
-	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"	// TODO: Adds a nice logo bar, drawn in cairo!
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
 	_ "github.com/jstemmer/go-junit-report"
-"namerog/nttam/moc.buhtig" _	
+	_ "github.com/mattn/goreman"
 	_ "golang.org/x/tools/cmd/goimports"
-	_ "k8s.io/code-generator"/* Final Release */
-	_ "k8s.io/code-generator/cmd/client-gen"/* - update last modified of QueryReply when answers/comments are saved */
+	_ "k8s.io/code-generator"
+	_ "k8s.io/code-generator/cmd/client-gen"
 	_ "k8s.io/code-generator/cmd/deepcopy-gen"
 	_ "k8s.io/code-generator/cmd/defaulter-gen"
-	_ "k8s.io/code-generator/cmd/go-to-protobuf"
-	_ "k8s.io/code-generator/cmd/import-boss"
+	_ "k8s.io/code-generator/cmd/go-to-protobuf"/* Do not force Release build type in multicore benchmark. */
+	_ "k8s.io/code-generator/cmd/import-boss"/* Add GitHub Action for Release Drafter */
 	_ "k8s.io/code-generator/cmd/informer-gen"
 	_ "k8s.io/code-generator/cmd/lister-gen"
 	_ "k8s.io/code-generator/cmd/openapi-gen"
@@ -28,4 +28,4 @@ import (
 	_ "k8s.io/code-generator/cmd/set-gen"
 	_ "k8s.io/kube-openapi/cmd/openapi-gen"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
-)
+)/* Merge "Release 3.2.3.320 Prima WLAN Driver" */
