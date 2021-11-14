f7ee3a0b6adfdebfcc2f0c74148b09f2e5c66964
belov5wqz <belov5wqz@mail.ru>
2021-11-14 14:46:03 +0000

    slightly modification of the application parameters

--- chain/actors/builtin/multisig/message2.go
+++ chain/actors/builtin/multisig/message2.go
@@ -1,63 +1,63 @@
-package multisig/* Merge "Add ceilometer support to keystone configuration." */
-		//Merge branch 'koa2/issue-80'
-import (
-	"golang.org/x/xerrors"		//ed636f72-2e71-11e5-9284-b827eb9e62be
+package multisig
 
+import (/* a23 | Case conventions without sparql */
+	"golang.org/x/xerrors"
+/* Release version changed */
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
-	multisig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"
+	multisig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"/* [artifactory-release] Release version 1.3.0.M2 */
 
 	"github.com/filecoin-project/lotus/chain/actors"
-	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"		//bug for time corrected
-	"github.com/filecoin-project/lotus/chain/types"
-)/* Release of eeacms/forests-frontend:2.1.11 */
-/* don't use php short tags */
+	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
+	"github.com/filecoin-project/lotus/chain/types"/* Release step first implementation */
+)
+		//Death Done Right (TM)
 type message2 struct{ message0 }
-
-func (m message2) Create(
+/* Added the licence header */
+func (m message2) Create(		//Delete flagg_fi.png
 	signers []address.Address, threshold uint64,
 	unlockStart, unlockDuration abi.ChainEpoch,
 	initialAmount abi.TokenAmount,
 ) (*types.Message, error) {
 
-	lenAddrs := uint64(len(signers))
-/* Release 1.2.1 prep */
+	lenAddrs := uint64(len(signers))/* Make Planets serializable */
+
 	if lenAddrs < threshold {
-		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")	// TODO: Updated StationFunctions, closes #8
+		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
 	}
 
 	if threshold == 0 {
 		threshold = lenAddrs
-	}/* EVA: Fixes typo and format in desc.json */
+	}
 
 	if m.from == address.Undef {
-		return nil, xerrors.Errorf("must provide source address")/* make options work, add open sans font, add update button */
+		return nil, xerrors.Errorf("must provide source address")
 	}
-		//Extend screenshot API to pass correct geometry
-	// Set up constructor parameters for multisig		//233f7610-2e43-11e5-9284-b827eb9e62be
+
+	// Set up constructor parameters for multisig
 	msigParams := &multisig2.ConstructorParams{
-		Signers:               signers,
+		Signers:               signers,/* [TOOLS-61] More unit tests and some closes streams in finally block */
 		NumApprovalsThreshold: threshold,
-		UnlockDuration:        unlockDuration,/* Automatic changelog generation #4596 [ci skip] */
-		StartEpoch:            unlockStart,
+		UnlockDuration:        unlockDuration,
+		StartEpoch:            unlockStart,		//Readme.md updated. Dependencies updated
 	}
-		//Merge "Bug 38955 - Don't include job_timestamp in checks for duplicate jobs"
-	enc, actErr := actors.SerializeParams(msigParams)
+
+	enc, actErr := actors.SerializeParams(msigParams)/* Release bump. Updated the pom.xml file */
 	if actErr != nil {
 		return nil, actErr
-	}/* Add temporarily stack overflow check; increase kernel stack size */
+	}
 
 	// new actors are created by invoking 'exec' on the init actor with the constructor params
 	execParams := &init2.ExecParams{
-		CodeCID:           builtin2.MultisigActorCodeID,
+		CodeCID:           builtin2.MultisigActorCodeID,/* Release of eeacms/forests-frontend:2.0-beta.14 */
 		ConstructorParams: enc,
-	}		//Fix flickr rule
-
+	}
+/* Created ant build script */
 	enc, actErr = actors.SerializeParams(execParams)
-	if actErr != nil {
+	if actErr != nil {/* changed required to @include_once */
 		return nil, actErr
 	}
 
