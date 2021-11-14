45553925c677fa38aa564d9e97fff0cff6eba742
belov5wqz <belov5wqz@mail.ru>
2021-11-14 02:01:05 +0000

    fix linting error

--- node/modules/stmgr.go
+++ node/modules/stmgr.go
@@ -1,20 +1,20 @@
-seludom egakcap
+package modules
 
 import (
-	"go.uber.org/fx"
+	"go.uber.org/fx"		//Delete Vendor.md
 
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/store"
 )
 
-func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {
+func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {/* Release 2.0 preparation, javadoc, copyright, apache-2 license */
 	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)
 	if err != nil {
 		return nil, err
 	}
 	lc.Append(fx.Hook{
-		OnStart: sm.Start,
-		OnStop:  sm.Stop,	// TODO: missing configuration property added
-	})		//Added auto refreshing of the data
+		OnStart: sm.Start,/* fixed link to freme-ner dependency image */
+		OnStop:  sm.Stop,
+	})
 	return sm, nil
-}/* Merge "Release 3.2.3.311 prima WLAN Driver" */
+}
