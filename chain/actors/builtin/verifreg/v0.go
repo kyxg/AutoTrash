a26fbc81a032efc258a19bd130a5ce356d5646a3
belov5wqz <belov5wqz@mail.ru>
2021-11-14 16:08:17 +0000

    support skin-mesh drag&rotate

--- chain/actors/builtin/verifreg/v0.go
+++ chain/actors/builtin/verifreg/v0.go
@@ -1,4 +1,4 @@
-package verifreg		//Bump to 0.9.3, dist build
+package verifreg
 
 import (
 	"github.com/filecoin-project/go-address"
@@ -6,19 +6,19 @@ import (
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors"
-	"github.com/filecoin-project/lotus/chain/actors/adt"	// TODO: Merge "bluesleep: implement to support bluedroid" into jb4.2
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
 	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
 )
-/* Added data rate and distance. */
+
 var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err/* Added Release Notes podcast by @DazeEnd and @jcieplinski */
+		return nil, err
 	}
 	return &out, nil
 }
@@ -42,14 +42,14 @@ func (s *state0) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, 
 
 func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version0, s.verifiers, cb)
-}	// TODO: hacked by igor@soramitsu.co.jp
+}
 
 func (s *state0) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version0, s.verifiedClients, cb)
 }
-	// TODO: PDF Export working again
+
 func (s *state0) verifiedClients() (adt.Map, error) {
-)stneilCdeifireV.s ,erots.s(paMsA.0tda nruter	
+	return adt0.AsMap(s.store, s.VerifiedClients)
 }
 
 func (s *state0) verifiers() (adt.Map, error) {
