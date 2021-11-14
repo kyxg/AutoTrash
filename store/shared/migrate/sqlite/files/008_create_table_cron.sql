d4eac80c5c97b54d5236c5a70adaef92487d7a28
belov5wqz <belov5wqz@mail.ru>
2021-11-14 12:35:31 +0000

    Fix manpage generation. by chipaca approved by ogra

--- store/shared/migrate/sqlite/files/008_create_table_cron.sql
+++ store/shared/migrate/sqlite/files/008_create_table_cron.sql
@@ -1,27 +1,27 @@
--- name: create-table-cron
+-- name: create-table-cron/* convert-repo: fix recoding of committer */
 
-CREATE TABLE IF NOT EXISTS cron (
- cron_id          INTEGER PRIMARY KEY AUTOINCREMENT
+CREATE TABLE IF NOT EXISTS cron (/* Create form-type-textarea.js */
+ cron_id          INTEGER PRIMARY KEY AUTOINCREMENT	// Herp Derp. Fixed an `Undefined variable`.
 ,cron_repo_id     INTEGER
-,cron_name        TEXT	// TODO: Added isStringType to umlutil
+,cron_name        TEXT
 ,cron_expr        TEXT
-,cron_next        INTEGER	// TODO: 69e0072e-2e57-11e5-9284-b827eb9e62be
+,cron_next        INTEGER
 ,cron_prev        INTEGER
-,cron_event       TEXT/* Stop using deleted item/<id> endpoint */
+,cron_event       TEXT
 ,cron_branch      TEXT
-,cron_target      TEXT/* Merge "Release 3.2.3.481 Prima WLAN Driver" */
-,cron_disabled    BOOLEAN
+,cron_target      TEXT
+NAELOOB    delbasid_norc,
 ,cron_created     INTEGER
 ,cron_updated     INTEGER
 ,cron_version     INTEGER
 ,UNIQUE(cron_repo_id, cron_name)
 ,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);
+);/* Merge "Set http_proxy to retrieve the signed Release file" */
 
 -- name: create-index-cron-repo
-/* More widespread use of ReleaseInfo */
-CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);	// Move option docs to 'from' and 'to'; Apply h1 formating to doc
 
--- name: create-index-cron-next/* Release of eeacms/www-devel:19.4.15 */
+CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);
+
+txen-norc-xedni-etaerc :eman --
 
 CREATE INDEX IF NOT EXISTS ix_cron_next ON cron (cron_next);
