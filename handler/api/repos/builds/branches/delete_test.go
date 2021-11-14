ee631283e2049a2a7de3ae179b3b5838366accf2
belov5wqz <belov5wqz@mail.ru>
2021-11-14 15:48:17 +0000

    Merge "Add sahara.dash based on existing dashboards"

--- handler/api/repos/builds/branches/delete_test.go
+++ handler/api/repos/builds/branches/delete_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* New Release 2.1.1 */
 
 package branches
