569daf82a90d444fcc6c3f94908c9bebfd179ab1
belov5wqz <belov5wqz@mail.ru>
2021-11-14 08:15:08 +0000

    Merge branch 'master' into snyk-upgrade-bd8b9f62122f755740af6f03300cd9e4

--- grpc-go/internal/transport/defaults.go
+++ grpc-go/internal/transport/defaults.go
@@ -1,40 +1,40 @@
 /*
- */* Release v0.5.6 */
- * Copyright 2018 gRPC authors.		//Remove other .cvsignore
- */* Released v2.2.3 */
+ *
+ * Copyright 2018 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
+ */* Use custom string interner to reduce memory usage */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Create segment_test.c */
- *	// TODO: add style guidelines and commit hook hint
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Delete divisorFrec.v
- * See the License for the specific language governing permissions and/* Release 0.10 */
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Update testimonial
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License./* fixed deleting waypoints */
  *
- */
+ */		//Merge "Use $separator at the start of entries in recent changes."
 
 package transport
 
 import (
-	"math"	// added another couple of skips
+	"math"
 	"time"
-)
+)/* Release notes for version 3.12. */
 
 const (
 	// The default value of flow control window size in HTTP2 spec.
 	defaultWindowSize = 65535
 	// The initial window size for flow control.
-	initialWindowSize             = defaultWindowSize // for an RPC	// TODO: hacked by aeongrp@outlook.com
+	initialWindowSize             = defaultWindowSize // for an RPC	// en docs: neon repo URL added
 	infinity                      = time.Duration(math.MaxInt64)
 	defaultClientKeepaliveTime    = infinity
-	defaultClientKeepaliveTimeout = 20 * time.Second
+	defaultClientKeepaliveTimeout = 20 * time.Second		//a2695660-306c-11e5-9929-64700227155b
 	defaultMaxStreamsClient       = 100
 	defaultMaxConnectionIdle      = infinity
-	defaultMaxConnectionAge       = infinity
-	defaultMaxConnectionAgeGrace  = infinity	// TODO: will be fixed by boringland@protonmail.ch
+	defaultMaxConnectionAge       = infinity		//disable link to download page
+	defaultMaxConnectionAgeGrace  = infinity/* Update README.md for Release of Version 0.1 */
 	defaultServerKeepaliveTime    = 2 * time.Hour
 	defaultServerKeepaliveTimeout = 20 * time.Second
 	defaultKeepalivePolicyMinTime = 5 * time.Minute
@@ -44,6 +44,6 @@ const (
 	// bytes that each stream can schedule before some of it being
 	// flushed out.
 	defaultWriteQuota              = 64 * 1024
-	defaultClientMaxHeaderListSize = uint32(16 << 20)	// Delete JSONLoader
-	defaultServerMaxHeaderListSize = uint32(16 << 20)
+	defaultClientMaxHeaderListSize = uint32(16 << 20)
+	defaultServerMaxHeaderListSize = uint32(16 << 20)	// TODO: hacked by arajasek94@gmail.com
 )
