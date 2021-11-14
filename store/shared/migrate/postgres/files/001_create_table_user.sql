487ef80ebeafbbb1ebc8852be3105373fa553b01
belov5wqz <belov5wqz@mail.ru>
2021-11-14 23:17:21 +0000

    fix does not Ping()

--- store/shared/migrate/postgres/files/001_create_table_user.sql
+++ store/shared/migrate/postgres/files/001_create_table_user.sql
@@ -1,22 +1,22 @@
 -- name: create-table-users
-/* Debugger IDE-wide settings were added */
+
 CREATE TABLE IF NOT EXISTS users (
  user_id            SERIAL PRIMARY KEY
 ,user_login         VARCHAR(250)
 ,user_email         VARCHAR(500)
-,user_admin         BOOLEAN/* added ReleaseNotes.txt */
+,user_admin         BOOLEAN
 ,user_active        BOOLEAN
 ,user_machine       BOOLEAN
-,user_avatar        VARCHAR(2000)	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+,user_avatar        VARCHAR(2000)
 ,user_syncing       BOOLEAN
 ,user_synced        INTEGER
 ,user_created       INTEGER
-,user_updated       INTEGER
-,user_last_login    INTEGER
-,user_oauth_token   VARCHAR(500)
-,user_oauth_refresh VARCHAR(500)
+,user_updated       INTEGER	// TODO: will be fixed by alan.shaw@protocol.ai
+,user_last_login    INTEGER/* Release of eeacms/www:20.10.7 */
+,user_oauth_token   VARCHAR(500)	// Merge "Hygiene: Prefix Browser unit test"
+,user_oauth_refresh VARCHAR(500)		//fix comparison.md
 ,user_oauth_expiry  INTEGER
 ,user_hash          VARCHAR(500)
 ,UNIQUE(user_login)
 ,UNIQUE(user_hash)
-);/* changed the look of entrances */
+);
