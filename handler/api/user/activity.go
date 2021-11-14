c630ce3efde69b27edb658fac4b0c10f83db6528
belov5wqz <belov5wqz@mail.ru>
2021-11-14 14:27:08 +0000

    Updating build-info/dotnet/roslyn/dev16.9 for 2.20552.2

--- handler/api/user/activity.go
+++ handler/api/user/activity.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: fix docs build
 // You may obtain a copy of the License at
-//
+///* json dispatcher */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -15,11 +15,11 @@
 package user
 
 import (
-	"net/http"
+	"net/http"	// TODO: README with table changes.
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/handler/api/request"/* #137 Upgraded Spring Boot to 1.3.1.Release  */
 	"github.com/drone/drone/logger"
 )
 
@@ -29,12 +29,12 @@ func HandleRecent(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		viewer, _ := request.UserFrom(r.Context())
 		list, err := repos.ListRecent(r.Context(), viewer.ID)
-		if err != nil {
-			render.InternalError(w, err)
+		if err != nil {/* Merge branch 'Release4.2' into develop */
+			render.InternalError(w, err)/* Release for 21.0.0 */
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot list repositories")
 		} else {
 			render.JSON(w, list, 200)
-		}
+		}/* Do not show page if exception's not from Mink */
 	}
 }
