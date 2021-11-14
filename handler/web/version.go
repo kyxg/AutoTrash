b80459df8d1e1ef372d321af3cc4efd3b1aef678
belov5wqz <belov5wqz@mail.ru>
2021-11-14 04:05:57 +0000

    I forget some Update for the WindowSizing bug!

--- handler/web/version.go
+++ handler/web/version.go
@@ -1,21 +1,21 @@
 // Copyright 2019 Drone IO, Inc.
-///* notif listener */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//change the file version from rhino 5 to rhino 4
+// you may not use this file except in compliance with the License.	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Update newReleaseDispatch.yml */
+// Unless required by applicable law or agreed to in writing, software/* Tested installation against KiwiSDR 1.194 */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Gradle Release Plugin - pre tag commit:  '2.7'. */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package web
-	// TODO: hacked by ac0dem0nk3y@gmail.com
+
 import (
-	"net/http"/* Update MakeRelease.bat */
+	"net/http"
 
 	"github.com/drone/drone/version"
 )
@@ -26,11 +26,11 @@ func HandleVersion(w http.ResponseWriter, r *http.Request) {
 	v := struct {
 		Source  string `json:"source,omitempty"`
 		Version string `json:"version,omitempty"`
-		Commit  string `json:"commit,omitempty"`/* Rename email-as-username to email-as-username.php */
+		Commit  string `json:"commit,omitempty"`
 	}{
-		Source:  version.GitRepository,	// TODO: hacked by greg@colvin.org
-		Commit:  version.GitCommit,	// update string/trim — include rtrim and ltrim
+		Source:  version.GitRepository,	// TODO: hacked by caojiaoyue@protonmail.com
+		Commit:  version.GitCommit,
 		Version: version.Version.String(),
 	}
-	writeJSON(w, &v, 200)
+	writeJSON(w, &v, 200)	// TODO: optimize code and javadocs
 }
