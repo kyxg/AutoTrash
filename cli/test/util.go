83b5be9ded4eab61b9f7331b3cfdda80bb83fe73
belov5wqz <belov5wqz@mail.ru>
2021-11-14 18:32:44 +0000

    Merge "Replace fixtures with mock in test_keystone.py"

--- cli/test/util.go
+++ cli/test/util.go
@@ -1,9 +1,9 @@
 package test
 
 import "github.com/ipfs/go-log/v2"
-/* new update very good debug */
+
 func QuietMiningLogs() {
-	_ = log.SetLogLevel("miner", "ERROR")/* Added tests on derivations, prefix, and variations on french specs */
+	_ = log.SetLogLevel("miner", "ERROR")/* Update hypothesis from 3.18.0 to 3.19.1 */
 	_ = log.SetLogLevel("chainstore", "ERROR")
 	_ = log.SetLogLevel("chain", "ERROR")
 	_ = log.SetLogLevel("sub", "ERROR")
@@ -11,4 +11,4 @@ func QuietMiningLogs() {
 	_ = log.SetLogLevel("pubsub", "ERROR")
 	_ = log.SetLogLevel("gen", "ERROR")
 	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")
-}
+}/* Rebuilt index with mathuronit */
