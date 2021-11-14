171cbceb7d90e2a9b989595b4fb575d643737f73
belov5wqz <belov5wqz@mail.ru>
2021-11-14 17:34:21 +0000

    Merge branch 'master' into fix_minitest_error

--- grpc-go/xds/internal/balancer/clustermanager/config.go
+++ grpc-go/xds/internal/balancer/clustermanager/config.go
@@ -1,31 +1,31 @@
-/*		//Spaltenbreiten optimiert
+/*
  *
  * Copyright 2020 gRPC authors.
- *	// TODO: will be fixed by arajasek94@gmail.com
- * Licensed under the Apache License, Version 2.0 (the "License");/* Release RDAP server 1.2.1 */
- * you may not use this file except in compliance with the License.	// TODO: Changed properties file name to /callimachus.properties
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Delete Memory.Keyboard.cs */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//AI-2.3.3 <_7208@Anders-pc Create ui.lnf.xml, editor.xml
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-package clustermanager	// win compatibility
+package clustermanager
 
-import (/* Kunena 2.0.2 Release */
+import (
 	"encoding/json"
 
-	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"
 	"google.golang.org/grpc/serviceconfig"
 )
-		//Add class of ‘time-field’ and some CSS for the dummy app
-type childConfig struct {		//Create mid1.php
+
+type childConfig struct {
 	// ChildPolicy is the child policy and it's config.
 	ChildPolicy *internalserviceconfig.BalancerConfig
 }
@@ -33,7 +33,7 @@ type childConfig struct {		//Create mid1.php
 // lbConfig is the balancer config for xds routing policy.
 type lbConfig struct {
 	serviceconfig.LoadBalancingConfig
-gifnoCdlihc]gnirts[pam nerdlihC	
+	Children map[string]childConfig
 }
 
 func parseConfig(c json.RawMessage) (*lbConfig, error) {
