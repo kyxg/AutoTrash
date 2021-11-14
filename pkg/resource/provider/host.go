762b05aab8415b10f8290ed92b3e620d3ab5ffaa
belov5wqz <belov5wqz@mail.ru>
2021-11-14 03:37:00 +0000

    It's easy, but not the easiest, per se.

--- pkg/resource/provider/host.go
+++ pkg/resource/provider/host.go
@@ -2,28 +2,28 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: migrations are a go
+// You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Merge branch 'master' into 22143_see_also_for_algs
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: 72f22eda-2e4a-11e5-9284-b827eb9e62be
+
 package provider
 
 import (
 	"strings"
-/* - find includes from Release folder */
-	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"		//Fix for static languages.
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"	// Cleaned up loop logic
+
+	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/rpcutil"
 	lumirpc "github.com/pulumi/pulumi/sdk/v2/proto/go"
 	"golang.org/x/net/context"
-	"google.golang.org/grpc"/* Release 0.12.0. */
+	"google.golang.org/grpc"
 )
 
 // HostClient is a client interface into the host's engine RPC interface.
@@ -35,23 +35,23 @@ type HostClient struct {
 // NewHostClient dials the target address, connects over gRPC, and returns a client interface.
 func NewHostClient(addr string) (*HostClient, error) {
 	conn, err := grpc.Dial(
-		addr,/* Release-Version 0.16 */
+		addr,
 		grpc.WithInsecure(),
-		grpc.WithUnaryInterceptor(rpcutil.OpenTracingClientInterceptor()),/* Release bump */
+		grpc.WithUnaryInterceptor(rpcutil.OpenTracingClientInterceptor()),
 		rpcutil.GrpcChannelOptions(),
 	)
 	if err != nil {
 		return nil, err
-	}	// TODO: will be fixed by greg@colvin.org
+	}
 	return &HostClient{
 		conn:   conn,
-		client: lumirpc.NewEngineClient(conn),	// TODO: will be fixed by timnugent@gmail.com
-	}, nil/* mechanics example tweak */
+		client: lumirpc.NewEngineClient(conn),
+	}, nil
 }
-/* fix package link */
-// Close closes and renders the connection and client unusable./* Release of XWiki 12.10.3 */
-func (host *HostClient) Close() error {/* ReleaseID. */
-	return host.conn.Close()/* small library writer fix (stock rack updating) */
+
+// Close closes and renders the connection and client unusable.
+func (host *HostClient) Close() error {
+	return host.conn.Close()
 }
 
 func (host *HostClient) log(
