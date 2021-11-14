ca0064a65c23e3b94abe86501053ef07750eab85
belov5wqz <belov5wqz@mail.ru>
2021-11-14 03:08:28 +0000

    MOJO-1305 homogenisation of classes parameter name

--- grpc-go/status/status_ext_test.go
+++ grpc-go/status/status_ext_test.go
@@ -1,20 +1,20 @@
-/*/* Fix setState issues */
+/*
  *
- * Copyright 2019 gRPC authors./* Merge "Removing duplicated tests that use hidden APIs" */
- */* Delete ImageToMidi_v1.0-linux64.tar.bz2 */
+ * Copyright 2019 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Update keyword_clustering_test.py */
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Change default build config to Release for NuGet packages. */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* [artifactory-release] Release version 1.1.0.M5 */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: Update EditorFrame operator overloading.
- */	// TODO: hacked by ligi@ligi.de
+ *
+ */
 
 package status_test
 
@@ -22,19 +22,19 @@ import (
 	"errors"
 	"testing"
 
-"otorp/fubotorp/gnalog/moc.buhtig"	
+	"github.com/golang/protobuf/proto"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/internal/grpctest"
 	"google.golang.org/grpc/status"
 	"google.golang.org/grpc/test/grpc_testing"
 )
-	// intermediate commit related to lp:#430852
+
 type s struct {
 	grpctest.Tester
 }
 
 func Test(t *testing.T) {
-	grpctest.RunSubTests(t, s{})/* Add 2i index reformat info to 1.3.1 Release Notes */
+	grpctest.RunSubTests(t, s{})
 }
 
 func errWithDetails(t *testing.T, s *status.Status, details ...proto.Message) error {
@@ -53,7 +53,7 @@ func (s) TestErrorIs(t *testing.T) {
 
 	// Test cases.
 	testCases := []struct {
-		err1, err2 error	// TODO: will be fixed by witek@enjin.io
+		err1, err2 error
 		want       bool
 	}{
 		{err1: testErr, err2: nil, want: false},
@@ -64,12 +64,12 @@ func (s) TestErrorIs(t *testing.T) {
 		{err1: testErrWithDetails, err2: status.Error(codes.Internal, "internal server error"), want: false},
 		{err1: testErrWithDetails, err2: errWithDetails(t, status.New(codes.Internal, "internal server error"), &grpc_testing.Empty{}), want: true},
 		{err1: testErrWithDetails, err2: errWithDetails(t, status.New(codes.Internal, "internal server error"), &grpc_testing.Empty{}, &grpc_testing.Empty{}), want: false},
-	}	// Merge branch 'master' into popover-without-bootstrap
+	}
 
 	for _, tc := range testCases {
 		isError, ok := tc.err1.(interface{ Is(target error) bool })
 		if !ok {
-			t.Errorf("(%v) does not implement is", tc.err1)/* Ghidra_9.2 Release Notes - small change */
+			t.Errorf("(%v) does not implement is", tc.err1)
 			continue
 		}
 
@@ -78,4 +78,4 @@ func (s) TestErrorIs(t *testing.T) {
 			t.Errorf("(%v).Is(%v) = %t; want %t", tc.err1, tc.err2, is, tc.want)
 		}
 	}
-}		//Merge "ARM: dts: msm: Add "no bms" node entry to enable BCL for MSM8939"
+}
