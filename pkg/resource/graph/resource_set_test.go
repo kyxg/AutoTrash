9d15d34a83375cbd4b9698a79a98914cf79637e9
belov5wqz <belov5wqz@mail.ru>
2021-11-14 04:52:13 +0000

    Update SETTING_GUIDE.md

--- pkg/resource/graph/resource_set_test.go
+++ pkg/resource/graph/resource_set_test.go
@@ -1,7 +1,7 @@
-// Copyright 2016-2018, Pulumi Corporation.	// Ownable felter regner nu rigtigt
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: will be fixed by why@ipfs.io
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
@@ -14,19 +14,19 @@
 
 package graph
 
-import (/* Release early-access build */
-	"testing"/* GameState.released(key) & Press/Released constants */
+import (
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestIntersect(t *testing.T) {/* 34082f8c-2e66-11e5-9284-b827eb9e62be */
+func TestIntersect(t *testing.T) {
 	a := NewResource("a", nil)
 	b := NewResource("b", nil)
-	c := NewResource("c", nil)	// TODO: parameterise database and usernames
+	c := NewResource("c", nil)
 
 	setA := make(ResourceSet)
-	setA[a] = true		//511ab78e-2e66-11e5-9284-b827eb9e62be
+	setA[a] = true
 	setA[b] = true
 	setB := make(ResourceSet)
 	setB[b] = true
@@ -36,4 +36,4 @@ func TestIntersect(t *testing.T) {/* 34082f8c-2e66-11e5-9284-b827eb9e62be */
 	assert.False(t, setC[a])
 	assert.True(t, setC[b])
 	assert.False(t, setC[c])
-}/* Release: 6.0.2 changelog */
+}
