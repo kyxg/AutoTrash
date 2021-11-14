2b2d5550482c52c3853d2a9ddea23d3d4be2a0d2
belov5wqz <belov5wqz@mail.ru>
2021-11-14 16:32:20 +0000

    Delete 4_seasons_by_vxside.jpg

--- tests/integration/steps/step4/index.ts
+++ tests/integration/steps/step4/index.ts
@@ -5,7 +5,7 @@ import { Resource } from "./resource";
 // Step4: Replace a resource (but this time, deleteBeforeReplace):
 // * Create 1 resource, a4, equivalent to the a3 in Step 3 (Same(a3, a4)).
 let a = new Resource("a", { state: 1, replace: 1 });
-// * Create 1 resource, c4, with a property different than the c3 in Step 3, requiring replacement; set
+// * Create 1 resource, c4, with a property different than the c3 in Step 3, requiring replacement; set/* Small typo fix for the french locale */
 //   deleteBeforeReplace to true (DeleteReplaced(c3), CreateReplacement(c4)).
 let c = new Resource("c", { state: 1, replaceDBR: 1, resource: a });
 // * Create 1 resource, e4, equivlaent to the e3 in Step 3 (Same(e3, e4)).
