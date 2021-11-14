fcd6500ca50331d54a64de6d9e594be90b6b0448
belov5wqz <belov5wqz@mail.ru>
2021-11-14 12:40:46 +0000

    Automatic update for release 1.2.4

--- store/repos/scan_test.go
+++ store/repos/scan_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// TODO: Fix generation of unit testing libs
 package repos
