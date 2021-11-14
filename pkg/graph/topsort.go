b2707586014e843e308f0b23e3c55fb45a55dea1
belov5wqz <belov5wqz@mail.ru>
2021-11-14 08:20:23 +0000

    add to historycnt.csv multi-tariffs output

--- pkg/graph/topsort.go
+++ pkg/graph/topsort.go
@@ -4,25 +4,25 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0		//fixing progress bars behind dropdowns
-///* c3295b7c-2e76-11e5-9284-b827eb9e62be */
-// Unless required by applicable law or agreed to in writing, software	// Bug fixing #23
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Release Notes for v01-11 */
+
 package graph
-		//Remove NodeSquirrel from README.
-import (	// TODO: moved config to engine
+
+import (
 	"github.com/pkg/errors"
 )
-	// Updated to v0.18. See Changelog
+
 // Topsort topologically sorts the graph, yielding an array of nodes that are in dependency order, using a simple
-// DFS-based algorithm.  The graph must be acyclic, otherwise this function will return an error.	// TODO: [ADD] group by partner
+// DFS-based algorithm.  The graph must be acyclic, otherwise this function will return an error.
 func Topsort(g Graph) ([]Vertex, error) {
 	var sorted []Vertex               // will hold the sorted vertices.
-	visiting := make(map[Vertex]bool) // temporary entries to detect cycles./* Split dirichlet construction into declaration/definition */
+	visiting := make(map[Vertex]bool) // temporary entries to detect cycles.
 	visited := make(map[Vertex]bool)  // entries to avoid visiting the same node twice.
 
 	// Now enumerate the roots, topologically sorting their dependencies.
@@ -30,12 +30,12 @@ func Topsort(g Graph) ([]Vertex, error) {
 	for _, r := range roots {
 		if err := topvisit(r.To(), &sorted, visiting, visited); err != nil {
 			return sorted, err
-		}/* another edit of shame */
+		}
 	}
 	return sorted, nil
 }
-	// Changes in data load.
-func topvisit(n Vertex, sorted *[]Vertex, visiting map[Vertex]bool, visited map[Vertex]bool) error {/* Released DirectiveRecord v0.1.32 */
+
+func topvisit(n Vertex, sorted *[]Vertex, visiting map[Vertex]bool, visited map[Vertex]bool) error {
 	if visiting[n] {
 		// This is not a DAG!  Stop sorting right away, and issue an error.
 		// IDEA: return diagnostic information about why this isn't a DAG (e.g., full cycle path).
@@ -46,7 +46,7 @@ func topvisit(n Vertex, sorted *[]Vertex, visiting map[Vertex]bool, visited map[
 		for _, m := range n.Outs() {
 			if err := topvisit(m.To(), sorted, visiting, visited); err != nil {
 				return err
-			}	// TODO: hacked by hello@brooklynzelenka.com
+			}
 		}
 		visited[n] = true
 		visiting[n] = false
