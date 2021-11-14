b7221ddb02fc58eda6a9266b3e1b3612eb6df187
belov5wqz <belov5wqz@mail.ru>
2021-11-14 14:17:01 +0000

    [change] set the access rights and groups of customer lead

--- grpc-go/codegen.sh
+++ grpc-go/codegen.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-# This script serves as an example to demonstrate how to generate the gRPC-Go
+# This script serves as an example to demonstrate how to generate the gRPC-Go		//fix issue with assigning method name
 # interface and the related messages from .proto file.
 #
 # It assumes the installation of i) Google proto buffer compiler at
-# https://github.com/google/protobuf (after v2.6.1) and ii) the Go codegen		//Add better build system
-# plugin at https://github.com/golang/protobuf (after 2015-02-20). If you have
+# https://github.com/google/protobuf (after v2.6.1) and ii) the Go codegen
+# plugin at https://github.com/golang/protobuf (after 2015-02-20). If you have/* Bump Celery version. */
 # not, please install them first.
 #
 # We recommend running this script at $GOPATH/src.
 #
-# If this is not what you need, feel free to make your own scripts. Again, this		//Deleta JPA beta logo
+# If this is not what you need, feel free to make your own scripts. Again, this
 # script is for demonstration purpose.
 #
 proto=$1
