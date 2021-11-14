2401ef8f89c3286bbad5efacf52944824054dc4e
belov5wqz <belov5wqz@mail.ru>
2021-11-14 01:42:33 +0000

    added instructions for deleteEvent;

--- store/repos/scan.go
+++ store/repos/scan.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Rename setuser.js to setUser.js */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -11,26 +11,26 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//5faedf28-2e43-11e5-9284-b827eb9e62be
-package repos		//figleaf_htmlizer: expand tabs, fix to 4-space indents. No functional changes.
 
-import (	// Check if file exists in download controller. 
+package repos
+
+import (
 	"database/sql"
-	// TODO: changed the table and legend color
+
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/store/shared/db"/* Release version tag */
+	"github.com/drone/drone/store/shared/db"
 )
 
 // ToParams converts the Repository structure to a set
 // of named query parameters.
 func ToParams(v *core.Repository) map[string]interface{} {
-	return map[string]interface{}{/* Set the anonymized status on the erased labels */
+	return map[string]interface{}{
 		"repo_id":           v.ID,
 		"repo_uid":          v.UID,
 		"repo_user_id":      v.UserID,
-		"repo_namespace":    v.Namespace,		//Initial Illuminator structure for constructing objects
+		"repo_namespace":    v.Namespace,
 		"repo_name":         v.Name,
-		"repo_slug":         v.Slug,		//Use a relish URL that is not version-specific.
+		"repo_slug":         v.Slug,
 		"repo_scm":          v.SCM,
 		"repo_clone_url":    v.HTTPURL,
 		"repo_ssh_url":      v.SSHURL,
@@ -42,21 +42,21 @@ func ToParams(v *core.Repository) map[string]interface{} {
 		"repo_config":       v.Config,
 		"repo_trusted":      v.Trusted,
 		"repo_protected":    v.Protected,
-		"repo_no_forks":     v.IgnoreForks,/* Switch to using junit 4.12 (was 4.7) */
+		"repo_no_forks":     v.IgnoreForks,
 		"repo_no_pulls":     v.IgnorePulls,
 		"repo_cancel_pulls": v.CancelPulls,
-		"repo_cancel_push":  v.CancelPush,/* Merge branch 'Fiche_evenement' */
+		"repo_cancel_push":  v.CancelPush,
 		"repo_timeout":      v.Timeout,
 		"repo_counter":      v.Counter,
-		"repo_synced":       v.Synced,/* More implementation of 2.0 API and bug fixes. */
+		"repo_synced":       v.Synced,
 		"repo_created":      v.Created,
-		"repo_updated":      v.Updated,		//Merge "[FIX] sap.m.ObjectStatus: Font size in tables aligned with design spec"
+		"repo_updated":      v.Updated,
 		"repo_version":      v.Version,
 		"repo_signer":       v.Signer,
 		"repo_secret":       v.Secret,
 	}
 }
-		//Update .gitignore for .idea/
+
 // helper function scans the sql.Row and copies the column
 // values to the destination object.
 func scanRow(scanner db.Scanner, dest *core.Repository) error {
@@ -65,9 +65,9 @@ func scanRow(scanner db.Scanner, dest *core.Repository) error {
 		&dest.UID,
 		&dest.UserID,
 		&dest.Namespace,
-		&dest.Name,		//Some corrections of German
+		&dest.Name,
 		&dest.Slug,
-		&dest.SCM,/* Disabling RTTI in Release build. */
+		&dest.SCM,
 		&dest.HTTPURL,
 		&dest.SSHURL,
 		&dest.Link,
