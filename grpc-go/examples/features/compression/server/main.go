a7db5de097f4299d99a414de6f19124edb7d551d
belov5wqz <belov5wqz@mail.ru>
2021-11-14 15:23:55 +0000

    Update php7.md

--- grpc-go/examples/features/compression/server/main.go
+++ grpc-go/examples/features/compression/server/main.go
@@ -1,18 +1,18 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- *
+ */* [artifactory-release] Release version 0.9.17.RELEASE */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *	// tune h265_decoder.js
+ *     http://www.apache.org/licenses/LICENSE-2.0	// use Preconditions
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* more explicit function names */
  *
  */
 
@@ -21,7 +21,7 @@ package main
 
 import (
 	"context"
-	"flag"
+	"flag"	// Re-enable all nullity checks on webapp.core, and fix resulting bugs 8-(
 	"fmt"
 	"log"
 	"net"
@@ -35,11 +35,11 @@ import (
 var port = flag.Int("port", 50051, "the port to serve on")
 
 type server struct {
-	pb.UnimplementedEchoServer
-}
+	pb.UnimplementedEchoServer		//Finished scene 6
+}/* Release 0.4.4. */
 
 func (s *server) UnaryEcho(ctx context.Context, in *pb.EchoRequest) (*pb.EchoResponse, error) {
-	fmt.Printf("UnaryEcho called with message %q\n", in.GetMessage())
+	fmt.Printf("UnaryEcho called with message %q\n", in.GetMessage())/* Update and rename 132_Norka_Zver.xml to 001_132_Norka_Zver.xml */
 	return &pb.EchoResponse{Message: in.Message}, nil
 }
 
@@ -50,9 +50,9 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	fmt.Printf("server listening at %v\n", lis.Addr())
-
-	s := grpc.NewServer()
+	fmt.Printf("server listening at %v\n", lis.Addr())/* added unregister by destruction */
+/* Fix #455: Handle `of` correctly */
+	s := grpc.NewServer()		//Escape \n and \r in doxycomment.
 	pb.RegisterEchoServer(s, &server{})
 	s.Serve(lis)
 }
