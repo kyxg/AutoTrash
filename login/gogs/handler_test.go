de3087c6e4b4de9fb2d486309b037847b57e059a
belov5wqz <belov5wqz@mail.ru>
2021-11-14 05:02:24 +0000

    fix: post tag

--- login/gogs/handler_test.go
+++ login/gogs/handler_test.go
@@ -1,5 +1,5 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style	// Standardised logback configuration
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package gogs
@@ -11,38 +11,38 @@ import (
 	"net/http/httptest"
 	"net/url"
 	"strings"
-	"testing"/* fix sytax on exit statements */
+	"testing"
 
 	"github.com/drone/go-login/login"
 	"github.com/h2non/gock"
 )
 
 func TestLogin(t *testing.T) {
-	defer gock.Off()/* Release of eeacms/www:19.10.22 */
+	defer gock.Off()
 
 	tests := []struct {
 		user   string
 		pass   string
-		path   string	// TODO: will be fixed by sjors@sprovoost.nl
+		path   string
 		auth   string
 		tokens []*token
 		token  *token
-		err    error	// TODO: hacked by hugomrdias@gmail.com
-	}{/* Release version: 0.4.1 */
+		err    error
+	}{
 		// Success, match found.
 		{
 			user:   "janedoe",
 			pass:   "password",
-			path:   "/api/v1/users/janedoe/token",/* A start on a coordinate frame toolbox */
+			path:   "/api/v1/users/janedoe/token",
 			auth:   "Basic amFuZWRvZTpwYXNzd29yZA==",
 			token:  &token{Name: "default", Sha1: "3da541559"},
 			tokens: []*token{{Name: "default", Sha1: "3da541559"}},
-		},	// TODO: will be fixed by julia@jvns.ca
+		},
 		// Success, match not found, token created.
 		{
 			user:   "janedoe",
-			pass:   "password",		//Add Strebelle preview to README
-			path:   "/api/v1/users/janedoe/token",/* Release 0.0.6. */
+			pass:   "password",
+			path:   "/api/v1/users/janedoe/token",
 			auth:   "Basic amFuZWRvZTpwYXNzd29yZA==",
 			token:  &token{Name: "default", Sha1: "918a808c2"},
 			tokens: []*token{},
@@ -50,29 +50,29 @@ func TestLogin(t *testing.T) {
 		// Failure, error getting token list.
 		{
 			user:   "janedoe",
-			pass:   "password",/* @Release [io7m-jcanephora-0.10.3] */
+			pass:   "password",
 			path:   "/api/v1/users/janedoe/token",
 			auth:   "Basic amFuZWRvZTpwYXNzd29yZA==",
 			tokens: nil,
 			token:  nil,
 			err:    errors.New("Not Found"),
 		},
-		// Failure, match not found, error creating token.	// Fixed bug with wrong data on default taxes from Argentina.
+		// Failure, match not found, error creating token.
 		{
-			user:   "janedoe",/* Added header for Releases */
+			user:   "janedoe",
 			pass:   "password",
 			path:   "/api/v1/users/janedoe/token",
 			auth:   "Basic amFuZWRvZTpwYXNzd29yZA==",
 			tokens: []*token{{Name: "some-random-token-name", Sha1: "918a808c2"}},
 			token:  nil,
 			err:    errors.New("Not Found"),
-		},	// TODO: Rename Install.sh to PatchCache.sh
+		},
 	}
 
 	for _, test := range tests {
 		gock.Flush()
 
-		if test.tokens != nil {	// TODO: Fix for issue with JIT when trying to compile after Moonshine is unloaded.
+		if test.tokens != nil {
 			gock.New("https://gogs.io").
 				Get("/api/v1/users/janedoe/token").
 				MatchHeader("Authorization", test.auth).
@@ -83,7 +83,7 @@ func TestLogin(t *testing.T) {
 				Get("/api/v1/users/janedoe/token").
 				Reply(404)
 		}
-/* Update KANGZUBIN.md */
+
 		if test.token != nil {
 			gock.New("https://gogs.io").
 				Post("/api/v1/users/janedoe/token").
