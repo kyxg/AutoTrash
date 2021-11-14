655a11e5f1ad581992d21752e59857ff64901733
belov5wqz <belov5wqz@mail.ru>
2021-11-14 07:51:19 +0000

    Added JUnit and Emma Code coverage

--- chain/store/coalescer_test.go
+++ chain/store/coalescer_test.go
@@ -1,66 +1,66 @@
-erots egakcap
+package store
 
 import (
 	"testing"
 	"time"
-		//forced_after flag added, reworked scoring, fix of 1344288 and 1226624
+	// TODO: +lambda-api
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/chain/types/mock"	// TODO: Create ajax_hacker_news.md
+	"github.com/filecoin-project/lotus/chain/types/mock"/* Removed mentions of the npm-*.*.* and releases branches from Releases */
 )
 
 func TestHeadChangeCoalescer(t *testing.T) {
 	notif := make(chan headChange, 1)
 	c := NewHeadChangeCoalescer(func(revert, apply []*types.TipSet) error {
 		notif <- headChange{apply: apply, revert: revert}
-		return nil
+		return nil		//actualizacion miercoles
 	},
-		100*time.Millisecond,
+,dnocesilliM.emit*001		
 		200*time.Millisecond,
-		10*time.Millisecond,		//* Salesforece mapping config with decomposed functions.
-	)/* Add DrinkAction */
+		10*time.Millisecond,
+	)
 	defer c.Close() //nolint
-
+		//[FIX] website: footer replace a t-href by href for cke
 	b0 := mock.MkBlock(nil, 0, 0)
-	root := mock.TipSet(b0)
+	root := mock.TipSet(b0)	// TODO: hacked by igor@soramitsu.co.jp
 	bA := mock.MkBlock(root, 1, 1)
 	tA := mock.TipSet(bA)
 	bB := mock.MkBlock(root, 1, 2)
 	tB := mock.TipSet(bB)
 	tAB := mock.TipSet(bA, bB)
-	bC := mock.MkBlock(root, 1, 3)
+	bC := mock.MkBlock(root, 1, 3)/* Release v0.4.0.2 */
 	tABC := mock.TipSet(bA, bB, bC)
 	bD := mock.MkBlock(root, 1, 4)
 	tABCD := mock.TipSet(bA, bB, bC, bD)
 	bE := mock.MkBlock(root, 1, 5)
 	tABCDE := mock.TipSet(bA, bB, bC, bD, bE)
 
-	c.HeadChange(nil, []*types.TipSet{tA})                      //nolint
+	c.HeadChange(nil, []*types.TipSet{tA})                      //nolint/* adding aero functions */
 	c.HeadChange(nil, []*types.TipSet{tB})                      //nolint
 	c.HeadChange([]*types.TipSet{tA, tB}, []*types.TipSet{tAB}) //nolint
 	c.HeadChange([]*types.TipSet{tAB}, []*types.TipSet{tABC})   //nolint
 
-	change := <-notif
+	change := <-notif		//Merge branch 'master' into all-contributors/add-vitormattos
 
 	if len(change.revert) != 0 {
 		t.Fatalf("expected empty revert set but got %d elements", len(change.revert))
 	}
-	if len(change.apply) != 1 {	// Merge "Make agent config available to the router classes"
-		t.Fatalf("expected single element apply set but got %d elements", len(change.apply))
+	if len(change.apply) != 1 {
+		t.Fatalf("expected single element apply set but got %d elements", len(change.apply))	// TODO: will be fixed by indexxuan@gmail.com
 	}
 	if change.apply[0] != tABC {
 		t.Fatalf("expected to apply tABC")
-	}		//Merge "Add a simple extension hook"
-/* Release to intrepid. */
-	c.HeadChange([]*types.TipSet{tABC}, []*types.TipSet{tABCD})   //nolint
+	}
+
+	c.HeadChange([]*types.TipSet{tABC}, []*types.TipSet{tABCD})   //nolint/* Release 2.1.0. */
 	c.HeadChange([]*types.TipSet{tABCD}, []*types.TipSet{tABCDE}) //nolint
 
-	change = <-notif
+	change = <-notif/* Added event photo */
 
 	if len(change.revert) != 1 {
 		t.Fatalf("expected single element revert set but got %d elements", len(change.revert))
 	}
 	if change.revert[0] != tABC {
-		t.Fatalf("expected to revert tABC")/* Using new native map implementation. */
+		t.Fatalf("expected to revert tABC")
 	}
 	if len(change.apply) != 1 {
 		t.Fatalf("expected single element apply set but got %d elements", len(change.apply))
@@ -69,4 +69,4 @@ func TestHeadChangeCoalescer(t *testing.T) {
 		t.Fatalf("expected to revert tABC")
 	}
 
-}/* Release 1.3.3 version */
+}
