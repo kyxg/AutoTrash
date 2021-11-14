a8d22b2e799d17d385bc74d7f2f1da40881db42c
belov5wqz <belov5wqz@mail.ru>
2021-11-14 17:19:43 +0000

    Moving version

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp
@@ -1,11 +1,11 @@
-resource logs "aws:s3:Bucket" {}
-	// TODO: will be fixed by cory@protocol.ai
-resource bucket "aws:s3:Bucket" {		//unused whitelist file removed
+resource logs "aws:s3:Bucket" {}/* Remove redundant abstract method. */
+/* Move file Chapter4/Chapter4/raycast_model.md to Chapter4/raycast_model.md */
+resource bucket "aws:s3:Bucket" {
 	loggings = [{
 		targetBucket = logs.bucket,
-	}]	// TODO: hacked by earlephilhower@yahoo.com
+	}]
 }
 
 output targetBucket {
 	value = bucket.loggings[0].targetBucket
-}
+}		//Fixed header includes for gcc 4.6.1 on i2c
