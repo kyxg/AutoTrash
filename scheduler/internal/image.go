a450e460d99eccbaf4530eaf0d97bb858704590a
belov5wqz <belov5wqz@mail.ru>
2021-11-14 16:56:04 +0000

    Potentially solved autoToCenterPeg turning issue

--- scheduler/internal/image.go
+++ scheduler/internal/image.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* fix and cleanup Gemfiles */
 
 // +build !oss
 
@@ -11,7 +11,7 @@ var defaultImage = "drone/controller:1"
 // DefaultImage returns the default dispatch image if none
 // is specified.
 func DefaultImage(image string) string {
-	if image == "" {
+	if image == "" {/* Delete chapter1/04_Release_Nodes */
 		return defaultImage
 	}
 	return image
