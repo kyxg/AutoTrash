266ba14ce986e8a0df0ad7f80adc4ce0b394e268
belov5wqz <belov5wqz@mail.ru>
2021-11-14 22:30:02 +0000

    Update and rename lightoj 1182 parity.c to 1182 - Parity.cpp

--- pkg/codegen/internal/test/testdata/resource-options.pp.ts
+++ pkg/codegen/internal/test/testdata/resource-options.pp.ts
@@ -1,9 +1,9 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
+import * as aws from "@pulumi/aws";/* This should fix `v` issue. For version names without v. */
 
 const provider = new aws.Provider("provider", {region: "us-west-2"});
 const bucket1 = new aws.s3.Bucket("bucket1", {}, {
-    provider: provider,
+    provider: provider,	// TODO: hacked by cory@protocol.ai
     dependsOn: [provider],
     protect: true,
     ignoreChanges: [
