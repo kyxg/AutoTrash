c59c59ff05e6ee61498f77eb6c9350cda10b0f49
belov5wqz <belov5wqz@mail.ru>
2021-11-14 04:24:45 +0000

    92cd33b0-2e65-11e5-9284-b827eb9e62be

--- store/shared/migrate/mysql/files/009_create_table_secrets.sql
+++ store/shared/migrate/mysql/files/009_create_table_secrets.sql
@@ -1,20 +1,20 @@
--- name: create-table-secrets
+-- name: create-table-secrets/* KRIHS Version Release */
 
-CREATE TABLE IF NOT EXISTS secrets (
- secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT
+CREATE TABLE IF NOT EXISTS secrets (	// TODO: hacked by fkautz@pseudocode.cc
+ secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT	// updated main and neural net wrapper
 ,secret_repo_id           INTEGER
 ,secret_name              VARCHAR(500)
 ,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_repo_id, secret_name)
-,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
+,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE/* Release workloop event source when stopping. */
 );
 
 -- name: create-index-secrets-repo
 
 CREATE INDEX ix_secret_repo ON secrets (secret_repo_id);
-
+/* Release version 1.4.5. */
 -- name: create-index-secrets-repo-name
-
+/* Build Release 2.0.5 */
 CREATE INDEX ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
