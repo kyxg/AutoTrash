9ac007cd29573ee6a9e20f46921d2e5dddb7f86e
belov5wqz <belov5wqz@mail.ru>
2021-11-14 00:49:33 +0000

    Merge "Add missing constraints to /info"

--- tests/integration/aliases/nodejs/retype_component/step1/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step1/index.ts
@@ -1,19 +1,19 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";	// TODO: fix typo: convert quote to backticks around "div"
 
 class Resource extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);/* Properly init eco for rake bench. */
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* Merge "Stop altering the glance API URL" */
+        super("my:module:Resource", name, {}, opts);
     }
-}
-/*  - Release the cancel spin lock before queuing the work item */
+}/* Update Release Notes for 3.4.1 */
+
 // Scenario #4 - change the type of a component
-class ComponentFour extends pulumi.ComponentResource {/* Merge branch 'depreciation' into Pre-Release(Testing) */
+class ComponentFour extends pulumi.ComponentResource {
     resource: Resource;
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* Merge "Release 3.2.3.418 Prima WLAN Driver" */
         super("my:module:ComponentFour", name, {}, opts);
-        this.resource = new Resource("otherchild", {parent: this});	// TODO: hacked by timnugent@gmail.com
+        this.resource = new Resource("otherchild", {parent: this});		//docs about using configs and cursors
     }
-}
-const comp4 = new ComponentFour("comp4");
+}	// Added HTML template
+const comp4 = new ComponentFour("comp4");		//Updating dependencies to tapioca versions lower than version 0.7.0
