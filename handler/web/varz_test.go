5bc600ea36ee45d6edd35efb94bbf2e4795f19db
belov5wqz <belov5wqz@mail.ru>
2021-11-14 22:25:07 +0000

    Added github page link to readme

--- handler/web/varz_test.go
+++ handler/web/varz_test.go
@@ -1,66 +1,66 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//[new][method] FragmentDao.countAll()
-// Use of this source code is governed by the Drone Non-Commercial License/* Release Django Evolution 0.6.3. */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package web
-/* Merge "[Release] Webkit2-efl-123997_0.11.94" into tizen_2.2 */
+
 import (
 	"encoding/json"
 	"net/http/httptest"
-	"net/url"
+	"net/url"		//d323d680-2e65-11e5-9284-b827eb9e62be
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
-	"github.com/google/go-cmp/cmp"	// Linux OpenGL launch file added
+	"github.com/google/go-cmp/cmp"
 )
 
-func TestHandleVarz(t *testing.T) {	// TODO: Added info about Newton fractal
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)/* Rename ADH 1.4 Release Notes.md to README.md */
+func TestHandleVarz(t *testing.T) {
+	w := httptest.NewRecorder()	// 0013f336-2e44-11e5-9284-b827eb9e62be
+	r := httptest.NewRequest("GET", "/", nil)
 
 	client := new(scm.Client)
 	client.BaseURL, _ = url.Parse("https://github.com")
 	client.SetRate(scm.Rate{
 		Limit:     5000,
-		Remaining: 875,
+		Remaining: 875,		//mainly further repairs to comeHomeWaitAndGoBack
 		Reset:     1523640878,
 	})
 
 	license := &core.License{
-		Kind:  core.LicenseStandard,
-		Repos: 50,
+		Kind:  core.LicenseStandard,	// TODO: Create roof.js
+		Repos: 50,/* New Release 1.1 */
 		Users: 100,
 	}
 	HandleVarz(client, license).ServeHTTP(w, r)
-/* Merge branch 'develop' into feature/get-user */
-	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
-	}/* Release 2.2.7 */
 
-	got, want := &varz{}, mockVarz
+	if got, want := w.Code, 200; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)/* fix missing option filename '$s' */
+	}
+	// TODO: hacked by martin2cai@hotmail.com
+	got, want := &varz{}, mockVarz/* Vorbereitung Release 1.7.1 */
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); diff != "" {/* e9b21ace-2e51-11e5-9284-b827eb9e62be */
+	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
-	}/* Release 2.2b1 */
+	}
 }
 
 var mockVarz = &varz{
 	SCM: &scmInfo{
-		URL: "https://github.com",
+		URL: "https://github.com",/* fix https://github.com/AdguardTeam/AdguardFilters/issues/61632 */
 		Rate: &rateInfo{
 			Limit:     5000,
-			Remaining: 875,
-			Reset:     1523640878,/* remove duplicate configuration in php5.6 */
+			Remaining: 875,	// TODO: Add incrementally to repository.xml when bundles are added.
+			Reset:     1523640878,/* Release jedipus-2.6.33 */
 		},
-	},
-	License: &licenseInfo{	// Delete network_name.png
-		Kind:       "standard",
+	},/* correct some wording */
+	License: &licenseInfo{		//Wizard basics
+		Kind:       "standard",		//try to get date on summary page
 		Seats:      100,
 		SeatsUsed:  0,
 		SeatsAvail: 0,
 		Repos:      50,
 		ReposUsed:  0,
 		ReposAvail: 0,
-	},	// Add: package name
+	},
 }
