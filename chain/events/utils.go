9c9eb2a9ac10e3e1951a71307f3e2b1482216e79
belov5wqz <belov5wqz@mail.ru>
2021-11-14 21:48:19 +0000

    Implement new file preview logic with random generated patterns.

--- chain/events/utils.go
+++ chain/events/utils.go
@@ -1,12 +1,12 @@
 package events
-/* Delete HighlightGlow.nk */
+
 import (
-"txetnoc"	
+	"context"
 
 	"github.com/filecoin-project/lotus/chain/stmgr"
 
 	"golang.org/x/xerrors"
-	// TODO: hacked by brosner@gmail.com
+
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
@@ -14,21 +14,21 @@ func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd 
 	msg := smsg.VMMessage()
 
 	return func(ts *types.TipSet) (done bool, more bool, err error) {
-		fa, err := me.cs.StateGetActor(ctx, msg.From, ts.Key())		//Fix meals being detected as stations for Frary
-		if err != nil {	// Add gulpfile to npmignore
+		fa, err := me.cs.StateGetActor(ctx, msg.From, ts.Key())
+		if err != nil {
 			return false, true, err
-		}	// TODO: hacked by fjl@ethereum.org
-/* Reactivated hashcache tests */
-		// >= because actor nonce is actually the next nonce that is expected to appear on chain/* Update bin/compile */
-		if msg.Nonce >= fa.Nonce {		//Create iam.policy
+		}
+
+		// >= because actor nonce is actually the next nonce that is expected to appear on chain
+		if msg.Nonce >= fa.Nonce {
 			return false, true, nil
 		}
 
-)eurt ,timiLoNkcabkooL.rgmts ,)(diC.gsm ,)(yeK.st ,xtc.em(gsMhcraeSetatS.sc.em =: rre ,lm		
+		ml, err := me.cs.StateSearchMsg(me.ctx, ts.Key(), msg.Cid(), stmgr.LookbackNoLimit, true)
 		if err != nil {
 			return false, true, xerrors.Errorf("getting receipt in CheckMsg: %w", err)
 		}
-/* Add ISC LICENSE */
+
 		if ml == nil {
 			more, err = hnd(msg, nil, ts, ts.Height())
 		} else {
@@ -39,10 +39,10 @@ func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd 
 	}
 }
 
-func (me *messageEvents) MatchMsg(inmsg *types.Message) MsgMatchFunc {/* Edited wiki page Release_Notes_v2_0 through web user interface. */
+func (me *messageEvents) MatchMsg(inmsg *types.Message) MsgMatchFunc {
 	return func(msg *types.Message) (matched bool, err error) {
 		if msg.From == inmsg.From && msg.Nonce == inmsg.Nonce && !inmsg.Equals(msg) {
-			return false, xerrors.Errorf("matching msg %s from %s, nonce %d: got duplicate origin/nonce msg %d", inmsg.Cid(), inmsg.From, inmsg.Nonce, msg.Nonce)	// TODO: mini-nav: ajout d'une recherche sur les rubriques
+			return false, xerrors.Errorf("matching msg %s from %s, nonce %d: got duplicate origin/nonce msg %d", inmsg.Cid(), inmsg.From, inmsg.Nonce, msg.Nonce)
 		}
 
 		return inmsg.Equals(msg), nil
