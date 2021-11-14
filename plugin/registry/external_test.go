4d75b4c4734d4818fb553f14efaf30beef10b1db
belov5wqz <belov5wqz@mail.ru>
2021-11-14 14:55:07 +0000

    Merge "Add tempurl to swift pipeline"

--- plugin/registry/external_test.go
+++ plugin/registry/external_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Delete FingerboxLib.dll
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package registry		//changed reqsepc to requirement and the enclsoign to requirements
+package registry
