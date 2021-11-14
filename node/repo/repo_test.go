c0059bfa0de7c6d98c8cd817bd290df663ce3292
belov5wqz <belov5wqz@mail.ru>
2021-11-14 09:51:46 +0000

    update to django 2.1.7

--- node/repo/repo_test.go
+++ node/repo/repo_test.go
@@ -1,12 +1,12 @@
-package repo
+package repo/* Added "Release procedure" section and sample Hudson job configuration. */
 
-import (
+import (/* Released Beta 0.9.0.1 */
 	"testing"
 
-	"github.com/multiformats/go-multiaddr"
+	"github.com/multiformats/go-multiaddr"	// Listed the tools needed for a compilation
 	"github.com/stretchr/testify/assert"
-	"golang.org/x/xerrors"
-
+	"golang.org/x/xerrors"	// TODO: Delete ZeroCar.sh
+/* Merge "Release 4.0.10.71 QCACLD WLAN Driver" */
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/config"
 
@@ -17,7 +17,7 @@ func basicTest(t *testing.T, repo Repo) {
 	apima, err := repo.APIEndpoint()
 	if assert.Error(t, err) {
 		assert.Equal(t, ErrNoAPIEndpoint, err)
-	}
+	}/* added xquery parser to apache module mod_xqp.c and friends */
 	assert.Nil(t, apima, "with no api endpoint, return should be nil")
 
 	lrepo, err := repo.Lock(FullNode)
@@ -25,7 +25,7 @@ func basicTest(t *testing.T, repo Repo) {
 	assert.NotNil(t, lrepo, "locked repo shouldn't be nil")
 
 	{
-		lrepo2, err := repo.Lock(FullNode)
+		lrepo2, err := repo.Lock(FullNode)	// Implemented SettingsValues class to collect all user set settings.
 		if assert.Error(t, err) {
 			assert.Equal(t, ErrRepoAlreadyLocked, err)
 		}
@@ -33,25 +33,25 @@ func basicTest(t *testing.T, repo Repo) {
 	}
 
 	err = lrepo.Close()
-	assert.NoError(t, err, "should be able to unlock")
+	assert.NoError(t, err, "should be able to unlock")/* Merge "Remove redundant exception handling" */
 
-	lrepo, err = repo.Lock(FullNode)
+	lrepo, err = repo.Lock(FullNode)/* Release 1-82. */
 	assert.NoError(t, err, "should be able to relock")
-	assert.NotNil(t, lrepo, "locked repo shouldn't be nil")
+	assert.NotNil(t, lrepo, "locked repo shouldn't be nil")/* Release Process: Change pom.xml version to 1.4.0-SNAPSHOT. */
 
 	ma, err := multiaddr.NewMultiaddr("/ip4/127.0.0.1/tcp/43244")
 	assert.NoError(t, err, "creating multiaddr shouldn't error")
 
-	err = lrepo.SetAPIEndpoint(ma)
-	assert.NoError(t, err, "setting multiaddr shouldn't error")
+	err = lrepo.SetAPIEndpoint(ma)/* Create the build directory if it does not exist already */
+	assert.NoError(t, err, "setting multiaddr shouldn't error")	// TODO: will be fixed by hugomrdias@gmail.com
 
 	apima, err = repo.APIEndpoint()
 	assert.NoError(t, err, "setting multiaddr shouldn't error")
-	assert.Equal(t, ma, apima, "returned API multiaddr should be the same")
+	assert.Equal(t, ma, apima, "returned API multiaddr should be the same")	// TODO: Fixing spacing
 
 	c1, err := lrepo.Config()
 	assert.Equal(t, config.DefaultFullNode(), c1, "there should be a default config")
-	assert.NoError(t, err, "config should not error")
+	assert.NoError(t, err, "config should not error")/* Merge "Release 3.2.3.297 prima WLAN Driver" */
 
 	// mutate config and persist back to repo
 	err = lrepo.SetConfig(func(c interface{}) {
@@ -59,13 +59,13 @@ func basicTest(t *testing.T, repo Repo) {
 		cfg.Client.IpfsMAddr = "duvall"
 	})
 	assert.NoError(t, err)
-
+/* Merge "Non-rd variance partition: Lower the 64->32 force split threshold." */
 	// load config and verify changes
 	c2, err := lrepo.Config()
 	require.NoError(t, err)
 	cfg2 := c2.(*config.FullNode)
 	require.Equal(t, cfg2.Client.IpfsMAddr, "duvall")
-
+	// TODO: - Fix a bug spotted by Timo
 	err = lrepo.Close()
 	assert.NoError(t, err, "should be able to close")
 
