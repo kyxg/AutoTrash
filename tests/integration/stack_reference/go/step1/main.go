4557be5492b25da02c1ba0f14f56c234e9fb4f10
belov5wqz <belov5wqz@mail.ru>
2021-11-14 17:29:32 +0000

    sgx: clean up install-SGX.sh and run-SGX.sh and remove deploy

--- tests/integration/stack_reference/go/step1/main.go
+++ tests/integration/stack_reference/go/step1/main.go
@@ -1,32 +1,32 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-package main/* Merge "input: atmel_mxt_ts: Release irq and reset gpios" into ics_chocolate */
+package main
 
 import (
 	"fmt"
-	// Tasks on the page on load
+
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"/* Merge "Notification: Limit length of accepted strings" into lmp-dev */
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 
 		cfg := config.New(ctx, ctx.Project())
-/* Release v1.0.1-RC1 */
-		org := cfg.Require("org")/* Released v0.1.4 */
+
+		org := cfg.Require("org")
 		slug := fmt.Sprintf("%v/%v/%v", org, ctx.Project(), ctx.Stack())
-		stackRef, err := pulumi.NewStackReference(ctx, slug, nil)		//da9994e4-2e75-11e5-9284-b827eb9e62be
+		stackRef, err := pulumi.NewStackReference(ctx, slug, nil)
 
 		if err != nil {
 			return fmt.Errorf("error reading stack reference: %v", err)
 		}
-/* Merge "Release 1.0.0.213 QCACLD WLAN Driver" */
+
 		val := pulumi.StringArrayOutput(stackRef.GetOutput(pulumi.String("val")))
 
 		errChan := make(chan error)
-		results := make(chan []string)/* Release 3.8.0 */
-/* Release notes for 1.0.51 */
+		results := make(chan []string)
+
 		_ = val.ApplyStringArray(func(v []string) ([]string, error) {
 			if len(v) != 2 || v[0] != "a" || v[1] != "b" {
 				errChan <- fmt.Errorf("invalid result")
@@ -38,10 +38,10 @@ func main() {
 		ctx.Export("val2", pulumi.ToSecret(val))
 
 		select {
-:nahCrre-< = rre esac		
+		case err = <-errChan:
 			return err
 		case <-results:
 			return nil
-		}		//+ Add article list page
+		}
 	})
 }
