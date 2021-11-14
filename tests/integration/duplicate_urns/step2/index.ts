0371083514fb7d111840f922d8960ce78080c880
belov5wqz <belov5wqz@mail.ru>
2021-11-14 07:41:21 +0000

    Implement dictionary to js object compiler.

--- tests/integration/duplicate_urns/step2/index.ts
+++ tests/integration/duplicate_urns/step2/index.ts
@@ -1,16 +1,16 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Released 4.0.0.RELEASE */
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License./* [ixp4xx] fix avila-wdt compilation failure (#6948) */
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Updated 1 link from mitre.org to Releases page */
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Added windows project for asyn directory
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// Delete TSNE.lua
+// limitations under the License.
 
 import { Resource } from "./resource";
 
