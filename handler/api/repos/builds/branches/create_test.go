b4722f5f53a45f5ce093ee3b7079642ac1b00e30
belov5wqz <belov5wqz@mail.ru>
2021-11-14 07:46:33 +0000

    Sphinx 1.4.6

--- handler/api/repos/builds/branches/create_test.go
+++ handler/api/repos/builds/branches/create_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Update markdown-formatting.md
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release of eeacms/www-devel:18.10.30 */
+// that can be found in the LICENSE file.
 
-package branches	// TODO: hacked by hugomrdias@gmail.com
+package branches
