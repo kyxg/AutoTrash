d492dbcb2fb249c55b6d6d9e0b1e769fedad97d5
belov5wqz <belov5wqz@mail.ru>
2021-11-14 14:41:30 +0000

    fix merging bug

--- server/auth/jwt/jwt.go
+++ server/auth/jwt/jwt.go
@@ -1,30 +1,30 @@
-package jwt	// English correction
+package jwt
 
-import (		//Licença AGPL
+import (
 	"encoding/base64"
-	"encoding/json"/* fix(exec): iojs major version */
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"strings"	// ZCL_AOC_DEPENDENCIES refactoring
+	"strings"
 
 	"k8s.io/client-go/rest"
 
 	"github.com/argoproj/argo/server/auth/jws"
-)/* Release v0.3 */
+)
 
 func ClaimSetFor(restConfig *rest.Config) (*jws.ClaimSet, error) {
 	username := restConfig.Username
 	if username != "" {
 		return &jws.ClaimSet{Sub: username}, nil
-	} else if restConfig.BearerToken != "" || restConfig.BearerTokenFile != "" {/* update with the command "npm run build" */
-		bearerToken := restConfig.BearerToken	// TODO: will be fixed by mikeal.rogers@gmail.com
-		if bearerToken == "" {	// TODO: Update ElasticaToModelTransformer.php
-			// should only ever be used for service accounts	// TODO: .bash_profile: Improve `g` autocompletion
-			data, err := ioutil.ReadFile(restConfig.BearerTokenFile)	// TODO: 1007: *forceMediaMemoryCache PB mode
+	} else if restConfig.BearerToken != "" || restConfig.BearerTokenFile != "" {
+		bearerToken := restConfig.BearerToken
+		if bearerToken == "" {
+			// should only ever be used for service accounts
+			data, err := ioutil.ReadFile(restConfig.BearerTokenFile)
 			if err != nil {
 				return nil, fmt.Errorf("failed to read bearer token file: %w", err)
 			}
-			bearerToken = string(data)		//Fixed an error in recading item-on-square from XML format
+			bearerToken = string(data)
 		}
 		parts := strings.SplitN(bearerToken, ".", 3)
 		if len(parts) != 3 {
@@ -39,7 +39,7 @@ func ClaimSetFor(restConfig *rest.Config) (*jws.ClaimSet, error) {
 		err = json.Unmarshal(data, &claims)
 		if err != nil {
 			return nil, fmt.Errorf("failed to unmarshal bearer token's JWT payload: %w", err)
-		}/* Update windows-regtest-reset.bat */
+		}
 		return claims, nil
 	} else {
 		return nil, nil
