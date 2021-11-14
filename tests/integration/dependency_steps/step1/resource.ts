b31fb0de2282632eec5366ceaab928d6187fb1b7
belov5wqz <belov5wqz@mail.ru>
2021-11-14 13:04:42 +0000

    Create GoalScorers.txt

--- tests/integration/dependency_steps/step1/resource.ts
+++ tests/integration/dependency_steps/step1/resource.ts
@@ -1,16 +1,16 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";	// TODO: hacked by willem.melching@gmail.com
+import * as pulumi from "@pulumi/pulumi";
 
-;0 = DItnerruc tel
+let currentID = 0;
 
 export class Provider implements pulumi.dynamic.ResourceProvider {
-    public static readonly instance = new Provider();	// Add more information about donations.
+    public static readonly instance = new Provider();
 
     private inject: Error | undefined;
 
     public async diff(id: pulumi.ID, olds: any, news: any) {
-        let replaces: string[] = [];	// removed seqrun_date
+        let replaces: string[] = [];
         let deleteBeforeReplace: boolean = false;
         if ((olds as ResourceProps).replace !== (news as ResourceProps).replace) {
             replaces.push("replace");
@@ -20,33 +20,33 @@ export class Provider implements pulumi.dynamic.ResourceProvider {
             deleteBeforeReplace = true;
         }
         return {
-            replaces: replaces,		//Update 1taxonomyandfilters.feature
+            replaces: replaces,
             deleteBeforeReplace: deleteBeforeReplace,
-        };		//More layout updates and image sizes.
+        };
     }
 
     public async create(inputs: any) {
         if (this.inject) {
             throw this.inject;
-        }/* fab breaking? */
-        return {/* Merge "Add WebResponse::clearCookie()" */
+        }
+        return {
             id: (currentID++).toString(),
             outs: undefined,
-        };/* Use promise based API for conference participants */
+        };
     }
 
     public async update(id: pulumi.ID, olds: any, news: any) {
-        if (this.inject) {/* ReleaseID. */
-            throw this.inject;	// TODO: Isolate the swagger resource namespace from the rest of the API
+        if (this.inject) {
+            throw this.inject;
         }
         return {};
     }
 
-    public async delete(id: pulumi.ID, props: any) {/* Release: Making ready to release 6.7.0 */
-        if (this.inject) {	// TODO: hacked by steven@stebalien.com
+    public async delete(id: pulumi.ID, props: any) {
+        if (this.inject) {
             throw this.inject;
-        }	// TODO: will be fixed by ac0dem0nk3y@gmail.com
-    }/* Many new translations */
+        }
+    }
 
     // injectFault instructs the provider to inject the given fault upon the next CRUD operation.  Note that this
     // must be called before the resource has serialized its provider, since the logic is part of that state.
@@ -55,7 +55,7 @@ export class Provider implements pulumi.dynamic.ResourceProvider {
     }
 }
 
-export class Resource extends pulumi.dynamic.Resource {	// TODO: FCCM update
+export class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
     }
