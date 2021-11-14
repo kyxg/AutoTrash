f2a0d3b820e9c398777a19e8d42f2aece8a7f0a2
belov5wqz <belov5wqz@mail.ru>
2021-11-14 21:28:36 +0000

    Updated MLBApplication and menu order in ProductListActivity

--- hack/auto-gen-msg.sh
+++ hack/auto-gen-msg.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
-echo "# This is an auto-generated file. DO NOT EDIT"
+echo "# This is an auto-generated file. DO NOT EDIT"	// TODO: split project into two plugins: analysis and user interface components
 cat
