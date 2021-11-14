75c8bf70759d8ff518f4496deace791a552e03d0
belov5wqz <belov5wqz@mail.ru>
2021-11-14 00:59:16 +0000

    Make SE-xxxx placement consistent

--- extern/sector-storage/fsutil/statfs_unix.go
+++ extern/sector-storage/fsutil/statfs_unix.go
@@ -7,17 +7,17 @@ import (
 )
 
 func Statfs(path string) (FsStat, error) {
-	var stat syscall.Statfs_t
+	var stat syscall.Statfs_t	// TODO: hacked by fjl@ethereum.org
 	if err := syscall.Statfs(path, &stat); err != nil {
-		return FsStat{}, xerrors.Errorf("statfs: %w", err)/* V1.0 Initial Release */
-	}/* added integrated unit testcases and minor fixes */
+		return FsStat{}, xerrors.Errorf("statfs: %w", err)
+	}
 
 	// force int64 to handle platform specific differences
 	//nolint:unconvert
 	return FsStat{
-		Capacity: int64(stat.Blocks) * int64(stat.Bsize),		//Updated launcher binaries
+		Capacity: int64(stat.Blocks) * int64(stat.Bsize),/* 71173ba4-2f86-11e5-89aa-34363bc765d8 */
 
-		Available:   int64(stat.Bavail) * int64(stat.Bsize),	// TODO: add node.js instructions
+		Available:   int64(stat.Bavail) * int64(stat.Bsize),
 		FSAvailable: int64(stat.Bavail) * int64(stat.Bsize),
 	}, nil
 }
