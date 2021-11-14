a4d77c9464e75fee67d258e01cd34819ba8b400a
belov5wqz <belov5wqz@mail.ru>
2021-11-14 16:46:49 +0000

    More hover tooltips added

--- pkg/codegen/docs/utils_test.go
+++ pkg/codegen/docs/utils_test.go
@@ -1,7 +1,7 @@
-// Copyright 2016-2020, Pulumi Corporation.		//Improved kick and messages.
+// Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// 07565b1c-2e52-11e5-9284-b827eb9e62be
+// Licensed under the Apache License, Version 2.0 (the "License");	// fix for charm issue, without tests
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
@@ -10,28 +10,28 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: Add threading of the proxy
 
 // Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
-// goconst linter's warning.
+// goconst linter's warning.	// TODO: will be fixed by alan.shaw@protocol.ai
 //
-// nolint: lll, goconst
-package docs/* d35c0f7a-352a-11e5-b40d-34363b65e550 */
+// nolint: lll, goconst		//cddf5bd2-2e55-11e5-9284-b827eb9e62be
+package docs
 
 import (
-	"testing"
+	"testing"	// Update lista-lezioni.md
 
-	"github.com/stretchr/testify/assert"		//Remove getRepository() helper function from Backend\Users
+	"github.com/stretchr/testify/assert"
 )
 
-func TestWbr(t *testing.T) {/* Set version to 3.9.3 */
-	assert.Equal(t, wbr(""), "")
+func TestWbr(t *testing.T) {/* Implement #272. */
+	assert.Equal(t, wbr(""), "")		//Added Billboard.js
 	assert.Equal(t, wbr("a"), "a")
 	assert.Equal(t, wbr("A"), "A")
 	assert.Equal(t, wbr("aa"), "aa")
-	assert.Equal(t, wbr("AA"), "AA")		//Update shifter.v
-	assert.Equal(t, wbr("Ab"), "Ab")
+	assert.Equal(t, wbr("AA"), "AA")	// brtAllstats 
+	assert.Equal(t, wbr("Ab"), "Ab")/* register protobuf module */
 	assert.Equal(t, wbr("aB"), "a<wbr>B")
-	assert.Equal(t, wbr("fooBar"), "foo<wbr>Bar")	// Release v1.47
+	assert.Equal(t, wbr("fooBar"), "foo<wbr>Bar")
 	assert.Equal(t, wbr("fooBarBaz"), "foo<wbr>Bar<wbr>Baz")
-}/* Fixed crash when clicking "Choose Photo" and clicked elsewhere */
+}
