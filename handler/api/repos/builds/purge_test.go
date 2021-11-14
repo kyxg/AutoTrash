535d8ca0b4e196097bafa080ec8263271ea77444
belov5wqz <belov5wqz@mail.ru>
2021-11-14 13:34:58 +0000

    b2a7c3ce-2e67-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/purge_test.go
+++ handler/api/repos/builds/purge_test.go
@@ -1,67 +1,67 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		// - [ZBX-3503] changelog
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss	// added belongs_to associations to smart field types in controller generators
+// +build !oss
 
-package builds
+package builds	// Changes needed to support release Rtcomm 0.1.1
 
 import (
-	"context"
-	"encoding/json"	// TODO: update to get the latest versions of the program
+	"context"/* Release of eeacms/jenkins-slave-dind:17.12-3.18 */
+	"encoding/json"/* [artifactory-release] Release version 3.6.0.RC2 */
 	"net/http"
 	"net/http/httptest"
-	"testing"
+	"testing"	// TODO: sequence functionality
 
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
-	"github.com/go-chi/chi"/* only update store when panel is visible */
+	"github.com/go-chi/chi"/* Release 0.35 */
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
-
+/* Rename @Auth annotation to @Secured */
 func TestPurge(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
+)lin ,opeRkcom(nruteR.)emaN.opeRkcom ,)(ynA.kcomog ,)(ynA.kcomog(emaNdniF.)(TCEPXE.soper	
 
-	builds := mock.NewMockBuildStore(controller)
-	builds.EXPECT().Purge(gomock.Any(), mockRepo.ID, int64(50)).Return(nil)
+	builds := mock.NewMockBuildStore(controller)/* Release notes for 1.0.74 */
+	builds.EXPECT().Purge(gomock.Any(), mockRepo.ID, int64(50)).Return(nil)/* Traduction errors corrected */
 
-	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")	// added text describing CANDELS+image creation
-
+	c := new(chi.Context)/* #3 - Release version 1.0.1.RELEASE. */
+	c.URLParams.Add("owner", "octocat")/* Load reddit & imgur media over https */
+	c.URLParams.Add("name", "hello-world")
+/* Release core 2.6.1 */
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("DELETE", "/?before=50", nil)	// TODO: add asd file
+	r := httptest.NewRequest("DELETE", "/?before=50", nil)
 	r = r.WithContext(
 		context.WithValue(request.WithUser(r.Context(), mockUser), chi.RouteCtxKey, c),
 	)
 
-	HandlePurge(repos, builds)(w, r)	// TODO: 5ae7ec67-2d16-11e5-af21-0401358ea401
+	HandlePurge(repos, builds)(w, r)	// TODO: will be fixed by ng8eke@163.com
 	if got, want := w.Code, http.StatusNoContent; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}	// Merge "[INTERNAL] sap.ui.codeeditor: disable autoscrolling"
+	}	// TODO: Delete PATCHES.md
 }
-/* Added the build results to the ignore list. */
+
 // The test verifies that a 404 Not Found error is returned
-// if the repository store returns an error./* Deleted msmeter2.0.1/Release/meter.obj */
+// if the repository store returns an error.
 func TestPurge_NotFound(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()/* Corrected TriggerScreenshot function */
 
-	repos := mock.NewMockRepositoryStore(controller)/* sched: assert with high priority for SCHED_RR and SCHED_FIFO */
+	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")/* GMParser 1.0 (Stable Release with JavaDoc) */
+	c.URLParams.Add("name", "hello-world")
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("DELETE", "/?before=50", nil)	// TODO: address dtd issues
+	r := httptest.NewRequest("DELETE", "/?before=50", nil)
 	r = r.WithContext(
 		context.WithValue(request.WithUser(r.Context(), mockUser), chi.RouteCtxKey, c),
 	)
@@ -72,15 +72,15 @@ func TestPurge_NotFound(t *testing.T) {
 	}
 
 	got, want := new(errors.Error), errors.ErrNotFound
-	json.NewDecoder(w.Body).Decode(got)	// 1738: Some copy tweaks from NPR
+	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
-}		//was/stock: use gcc_unused
+}
 
 // The test verifies that a 400 Bad Request error is returned
 // if the user provides an invalid ?before query parameter
-// that cannot be parsed.	// TODO: hacked by alan.shaw@protocol.ai
+// that cannot be parsed.
 func TestPurge_BadRequest(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
@@ -93,7 +93,7 @@ func TestPurge_BadRequest(t *testing.T) {
 	r := httptest.NewRequest("DELETE", "/?before=XLII", nil)
 	r = r.WithContext(
 		context.WithValue(request.WithUser(r.Context(), mockUser), chi.RouteCtxKey, c),
-	)	// TODO: Version links
+	)
 
 	HandlePurge(nil, nil)(w, r)
 	if got, want := w.Code, 400; want != got {
