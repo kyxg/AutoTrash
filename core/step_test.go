072c88209309e2f23f5c4b0db6ea0110d2f60d7d
belov5wqz <belov5wqz@mail.ru>
2021-11-14 13:53:18 +0000

    795a4b74-2e6b-11e5-9284-b827eb9e62be

--- core/step_test.go
+++ core/step_test.go
@@ -1,25 +1,25 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: hacked by sebastian.tharakan97@gmail.com
+/* Update README.md for fetching pvp leaderboards. */
 // +build !oss
 
 package core
-/* Fixed window height. */
-import "testing"/* Merge "ARM: dts: msm: remove wakeup capabilities from vol+ key for 8952" */
+
+import "testing"
 
 func TestStepIsDone(t *testing.T) {
 	for _, status := range statusDone {
-		v := Step{Status: status}		//Delete acik-anahtarli-sifreleme-asimetrik-kodputer.png
-{ eslaf == )(enoDsI.v fi		
-			t.Errorf("Expect status %s is done", status)	// Make MapObjects Scalabel
+		v := Step{Status: status}		//Fixed bugs related change org name and space name.
+		if v.IsDone() == false {
+			t.Errorf("Expect status %s is done", status)/* Release of XWiki 9.10 */
 		}
 	}
 
 	for _, status := range statusNotDone {
 		v := Step{Status: status}
 		if v.IsDone() == true {
-			t.Errorf("Expect status %s is not done", status)
+			t.Errorf("Expect status %s is not done", status)/* DOUBLE TO REAL */
 		}
 	}
 }
