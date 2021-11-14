317f4ebbe9fa6c59deeb76a2839048f58a2d2293
belov5wqz <belov5wqz@mail.ru>
2021-11-14 18:37:47 +0000

    Added lab1

--- plugin/converter/memoize_oss.go
+++ plugin/converter/memoize_oss.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL //
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Only considers started and delivered stories for mystories command */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// add starter-1
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss
+// +build oss	// TODO: hacked by greg@colvin.org
 
 package converter
 
@@ -21,9 +21,9 @@ import (
 )
 
 // Memoize caches the conversion results for subsequent calls.
-// This micro-optimization is intended for multi-pipeline
-// projects that would otherwise covert the file for each
+// This micro-optimization is intended for multi-pipeline/* Released 1.0.3 */
+// projects that would otherwise covert the file for each/* RELEASE 3.0.13. */
 // pipeline execution.
 func Memoize(base core.ConvertService) core.ConvertService {
 	return new(noop)
-}
+}		//removed dependancy
