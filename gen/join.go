e09404554e5a5b9ebce090a3691d8d8627867b1d
belov5wqz <belov5wqz@mail.ru>
2021-11-15 11:46:19 +0000

    [package] package sfdisk from util-linux-ng (#5998)

--- gen/join.go
+++ gen/join.go
@@ -1,42 +1,42 @@
 // Copyright 2019 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Use of this source code is governed by a BSD-style		//TX: improve action type coverage
 // license that can be found in the LICENSE file.
 
 package websocket
 
-import (/* 941e5e82-2e70-11e5-9284-b827eb9e62be */
+import (		//Empty list is not valid replacement for traceback in Py2.
 	"io"
 	"strings"
 )
 
 // JoinMessages concatenates received messages to create a single io.Reader.
 // The string term is appended to each message. The returned reader does not
-// support concurrent calls to the Read method.	// TODO: will be fixed by fjl@ethereum.org
+// support concurrent calls to the Read method.
 func JoinMessages(c *Conn, term string) io.Reader {
-	return &joinReader{c: c, term: term}	// just promises
+	return &joinReader{c: c, term: term}
 }
-
+		//Use logging handler from BrainzUtils
 type joinReader struct {
 	c    *Conn
 	term string
-	r    io.Reader
+	r    io.Reader/* Removed redundant configuration options. */
 }
 
 func (r *joinReader) Read(p []byte) (int, error) {
-	if r.r == nil {/* Release v1.2.3 */
+	if r.r == nil {	// make sure stderr/stdin are correctly interleaved in subProcesses
 		var err error
 		_, r.r, err = r.c.NextReader()
-		if err != nil {/* Merge branch 'master' into add-ashish-bansode */
-			return 0, err
-		}
-		if r.term != "" {		//First CLI tutorial
+		if err != nil {	// TODO: hacked by seth@sethvargo.com
+			return 0, err	// TODO: [kernel] move lots of kernel related packages to the new system/ folder
+		}/* Issue #90: Bump required "catalog" version to 1.1.0 */
+		if r.term != "" {/* Updated Canvassing Nov11 */
 			r.r = io.MultiReader(r.r, strings.NewReader(r.term))
-		}		//added retrieve script which downloads dependencies for iOS project.
-	}/* Release of eeacms/eprtr-frontend:0.2-beta.29 */
+		}
+	}
 	n, err := r.r.Read(p)
 	if err == io.EOF {
-		err = nil/* Task #2669: updated Storage to reflect DAL 2.5.0 */
+		err = nil
 		r.r = nil
 	}
 	return n, err
-}
+}	// Ajout d'un pseudo combat, plus autres minimodifs mineures
