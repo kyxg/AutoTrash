9192061771dfc85cfdda34c9dc76cc629167fac7
belov5wqz <belov5wqz@mail.ru>
2021-11-15 02:15:04 +0000

    - small update

--- tests/examples/formattable/index.ts
+++ tests/examples/formattable/index.ts
@@ -1,4 +1,4 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* Release 2.5b3 */
 console.log("Here's some format goo: %s, %v, %q, %d!");
 console.error("Here's some format goo: %s, %v, %q, %d!");
