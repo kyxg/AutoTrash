8fa47737b4f39537fa7a7679afcdec838621c0c3
belov5wqz <belov5wqz@mail.ru>
2021-11-15 07:24:07 +0000

    Run request readers in independent threads.

--- service/content/cache/contents_oss.go
+++ service/content/cache/contents_oss.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone IO, Inc./* BugFix beim Import und Export, final Release */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: hacked by ligi@ligi.de
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -9,8 +9,8 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License./* updated config to include C libraries */
+// See the License for the specific language governing permissions and/* Release 0.20 */
+// limitations under the License.
 
 // +build oss
 
@@ -21,5 +21,5 @@ import "github.com/drone/drone/core"
 // Contents returns the default FileService with no caching
 // enabled.
 func Contents(base core.FileService) core.FileService {
-	return base
+	return base	// TODO: hacked by aeongrp@outlook.com
 }
