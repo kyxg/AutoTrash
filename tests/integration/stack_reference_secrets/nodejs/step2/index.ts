e5ca46d67d51096060a1b02073391ab943e28217
belov5wqz <belov5wqz@mail.ru>
2021-11-15 00:34:18 +0000

    Fix build #3

--- tests/integration/stack_reference_secrets/nodejs/step2/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step2/index.ts
@@ -1,12 +1,12 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* Release 0.14.2 (#793) */
 
 export const normal = pulumi.output("normal");
 export const secret = pulumi.secret("secret");
-		//Added null subtitles event to clear subtitles when media changed
+
 // Kinda strange, but we are getting a stack reference to ourselves, and refercing the result of the /previous/
-// deployment.
-const org = new pulumi.Config().require("org");		//Merge "[FAB-7766] Document on CouchDB (fix links)"
-const project = pulumi.getProject();/* Include LICENSE in gem. */
+// deployment./* Release v0.32.1 (#455) */
+const org = new pulumi.Config().require("org");
+const project = pulumi.getProject();
 const stack = pulumi.getStack();
 const sr = new pulumi.StackReference(`${org}/${project}/${stack}`);
 
