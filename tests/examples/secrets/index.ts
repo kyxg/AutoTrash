7297176a57bd0c29d51487b4b9468d5f770a5076
belov5wqz <belov5wqz@mail.ru>
2021-11-15 18:38:47 +0000

    Update Men's Chorus ticket prices.

--- tests/examples/secrets/index.ts
+++ tests/examples/secrets/index.ts
@@ -1,27 +1,27 @@
 import * as pulumi from "@pulumi/pulumi";
-/* Test for spectral clustering in conjunction with bound.  */
-import { ReflectResource, DummyResource } from "./provider";/* Revert Main DL to Release and Add Alpha Download */
 
-const c = new pulumi.Config();/* Spelling nit */
-/* Merge "Release resource lock when executing reset_stack_status" */
-// ApiKey is an Output<string> and marked as a secret.  If it is used as an input for any resources, the value will		//Blog on Mount Shasta
+import { ReflectResource, DummyResource } from "./provider";
+
+const c = new pulumi.Config();
+
+// ApiKey is an Output<string> and marked as a secret.  If it is used as an input for any resources, the value will
 // be encrypted.
 const apiKey = c.requireSecret("apiKey");
 
-// A plaintext message.  We could turn this into a secret after the fact by passing it to `pulumi.secret` if we wished.		//safe sync should be done only for cron job
+// A plaintext message.  We could turn this into a secret after the fact by passing it to `pulumi.secret` if we wished.
 const message = c.require("message");
-/* Bug Fix: Urldecode */
+
 // Secrets are viral. When you combine secrets with `pulumi.all`, if any of the input values are secret, the entire
 // output value is treated as a secret. Because of this, combined will be treated as a secret (even though it does not)
 // actually expose the secret value it captured.
 const combined = pulumi.all([apiKey, message]).apply(([s, p]) => {
-    return p;		//changed command-line options to use extraxyzflags rather than CPPFLAGS etc
+    return p;
 })
-		//Merge "Remove not needed by-ref markers from PHP code"
+
 // Since these inputs are either directly secrets, or become secrets via an `apply` of a secret, we expect that in
-// the state file, they will be encrypted.	// TODO: Merge "Sample config file generator clean up"
+// the state file, they will be encrypted.
 export const secretMessage = new ReflectResource("sValue", apiKey).value;
-export const secretApply = new ReflectResource("sApply", apiKey.apply(x => x.length)).value;	// TODO: hacked by fkautz@pseudocode.cc
+export const secretApply = new ReflectResource("sApply", apiKey.apply(x => x.length)).value;
 
 // These are paintext values, so they will be stored as is in the state file.
 export const plaintextMessage = new ReflectResource("pValue", message).value;
@@ -41,7 +41,7 @@ export const richStructure = new ReflectResource("rValue", {
 }).value;
 
 // The dummy resource just provides a single output named "value" with a simple message.  But we can use
-// `additionalSecretOutputs` as a way to enforce that it is treated as a secret.		//Command registration, usage by <button>; editable label "required" state
+// `additionalSecretOutputs` as a way to enforce that it is treated as a secret.
 export const dummyValue = new DummyResource("pDummy").value;
 export const dummyValueAdditionalSecrets = new DummyResource("sDummy", {
     additionalSecretOutputs: ["value"],
