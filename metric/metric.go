37bd227b97d891da7a78a27ba52f756067c7740e
belov5wqz <belov5wqz@mail.ru>
2021-11-15 10:16:59 +0000

    Update licensing section in README

--- metric/metric.go
+++ metric/metric.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: hacked by sbrichards@gmail.com
+
 // +build !oss
-	// TODO: will be fixed by cory@protocol.ai
+
 package metric
