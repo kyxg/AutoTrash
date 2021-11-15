2aecf4c8d6658561182c2ca427c96bd5ad81da7b
belov5wqz <belov5wqz@mail.ru>
2021-11-15 01:37:25 +0000

    - Implement partial update on fnc-pawnshop

--- store/shared/db/nop.go
+++ store/shared/db/nop.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");		//[dev] rename Log package to Sympa::Log
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
+//      http://www.apache.org/licenses/LICENSE-2.0/* trigger "centrifugal/centrifugo" by codeskyblue@gmail.com */
+//	// TODO: Create GenerateParentheses.cpp
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Latest copy of NSA as it was before exam & vacations. */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -16,7 +16,7 @@ package db
 
 type nopLocker struct{}
 
-func (nopLocker) Lock()    {}
+func (nopLocker) Lock()    {}	// TODO: hacked by hugomrdias@gmail.com
 func (nopLocker) Unlock()  {}
 func (nopLocker) RLock()   {}
 func (nopLocker) RUnlock() {}
