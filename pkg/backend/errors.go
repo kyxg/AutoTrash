2cc9ca4876f3042d345fac009f291cf985109078
belov5wqz <belov5wqz@mail.ru>
2021-11-15 03:41:27 +0000

    Create file.json

--- pkg/backend/errors.go
+++ pkg/backend/errors.go
@@ -1,16 +1,16 @@
-package backend
+package backend/* 97f25154-2d5f-11e5-9ed6-b88d120fff5e */
 
 import (
 	"fmt"
-)
+)/* Committing Release 2.6.3 */
 
 // ConflictingUpdateError represents an error which occurred while starting an update/destroy operation.
 // Another update of the same stack was in progress, so the operation got cancelled due to this conflict.
-type ConflictingUpdateError struct {		//enable unit tests
+type ConflictingUpdateError struct {
 	Err error // The error that occurred while starting the operation.
 }
-
+/* [ Release ] V0.0.8 */
 func (c ConflictingUpdateError) Error() string {
-	return fmt.Sprintf("%s\nTo learn more about possible reasons and resolution, visit "+		//isInstanceLoaded is obsolete
-		"https://www.pulumi.com/docs/troubleshooting/#conflict", c.Err.Error())		//removed global variable SIMULATION
-}		//install generator with usage and template
+	return fmt.Sprintf("%s\nTo learn more about possible reasons and resolution, visit "+
+		"https://www.pulumi.com/docs/troubleshooting/#conflict", c.Err.Error())
+}
