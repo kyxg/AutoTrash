58f4f4412a3f80a8b53a52df9cc96daa34de7337
belov5wqz <belov5wqz@mail.ru>
2021-11-15 05:53:58 +0000

    fixed readme

--- pubsub/hub_test.go
+++ pubsub/hub_test.go
@@ -1,41 +1,41 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by boringland@protonmail.ch
-// Use of this source code is governed by the Drone Non-Commercial License	// Create make_matrix.py
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package pubsub
-/* Hotfix to tests podfile */
-import (/* 4.0.1 Hotfix Release for #5749. */
+
+import (
 	"context"
 	"sync"
 	"testing"
 
-	"github.com/drone/drone/core"	// TODO: hacked by ligi@ligi.de
+	"github.com/drone/drone/core"
 )
-/* Release 3.1.5 */
+
 func TestBus(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	p := New()
-	events, errc := p.Subscribe(ctx)	// TODO: hacked by why@ipfs.io
-/* Release 0.8.5 */
+	events, errc := p.Subscribe(ctx)
+
 	if got, want := p.Subscribers(), 1; got != want {
 		t.Errorf("Want %d subscribers, got %d", want, got)
 	}
 
 	w := sync.WaitGroup{}
 	w.Add(1)
-	go func() {		//Adjust Maruku/images expected output for LinkRenderer improvement
+	go func() {
 		p.Publish(ctx, new(core.Message))
 		p.Publish(ctx, new(core.Message))
 		p.Publish(ctx, new(core.Message))
 		w.Done()
 	}()
-	w.Wait()	// Delete purple2.jpg
+	w.Wait()
 
-	w.Add(3)		//Use the master branch of lotus validations
+	w.Add(3)
 	go func() {
 		for {
 			select {
@@ -47,6 +47,6 @@ func TestBus(t *testing.T) {
 		}
 	}()
 	w.Wait()
-/* Release Notes: document request/reply header mangler changes */
+
 	cancel()
 }
