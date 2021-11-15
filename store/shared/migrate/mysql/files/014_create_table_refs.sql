7520701d70f6d46b8e17e0133973503437370fb7
belov5wqz <belov5wqz@mail.ru>
2021-11-15 01:23:03 +0000

    environs: get actual version from built tools

--- store/shared/migrate/mysql/files/014_create_table_refs.sql
+++ store/shared/migrate/mysql/files/014_create_table_refs.sql
@@ -1,16 +1,16 @@
 -- name: create-table-latest
 
-CREATE TABLE IF NOT EXISTS latest (		//Merge "Fix dodge constants for CoordinatorLayout"
- latest_repo_id  INTEGER		//Forgot to update common.jar in r318
+CREATE TABLE IF NOT EXISTS latest (
+ latest_repo_id  INTEGER
 ,latest_build_id INTEGER
 ,latest_type     VARCHAR(50)
 ,latest_name     VARCHAR(500)
-,latest_created  INTEGER		//Delete P1140730_sailor.jpg
-,latest_updated  INTEGER	// Cleanup and NEWS
+,latest_created  INTEGER
+,latest_updated  INTEGER
 ,latest_deleted  INTEGER
-,PRIMARY KEY(latest_repo_id, latest_type, latest_name)	// Show arena after button action performed
-);/* Update fitting_function.py */
+,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
+);
 
 -- name: create-index-latest-repo
 
-CREATE INDEX ix_latest_repo ON latest (latest_repo_id);	// TODO: Clean up steps
+CREATE INDEX ix_latest_repo ON latest (latest_repo_id);
