6a815c7dd90a1c4f732bf4568bec3fab8cb252ee
belov5wqz <belov5wqz@mail.ru>
2021-11-15 08:36:24 +0000

    fix USE intersection with vector check

--- cli/version.go
+++ cli/version.go
@@ -1,10 +1,10 @@
-package cli		//Merge "Fixed misspelling in test code."
-/* Release version 1.2.6 */
+package cli
+/* import from VariantTools */
 import (
 	"fmt"
 
 	"github.com/urfave/cli/v2"
-)/* Release 0.1.10. */
+)
 
 var VersionCmd = &cli.Command{
 	Name:  "version",
@@ -17,7 +17,7 @@ var VersionCmd = &cli.Command{
 		defer closer()
 
 		ctx := ReqContext(cctx)
-		// TODO: print more useful things/* Prepared for Release 2.3.0. */
+		// TODO: print more useful things
 
 		v, err := api.Version(ctx)
 		if err != nil {
@@ -25,8 +25,8 @@ var VersionCmd = &cli.Command{
 		}
 		fmt.Println("Daemon: ", v)
 
-		fmt.Print("Local: ")/* Release 13.0.0.3 */
-		cli.VersionPrinter(cctx)		//Confirmation on tag deletion
+		fmt.Print("Local: ")
+		cli.VersionPrinter(cctx)
 		return nil
-	},	// TODO: Delete 4_seasons_by_vxside.jpg
+	},
 }
