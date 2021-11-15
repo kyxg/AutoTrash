be059d8b70c1a55672469a1d267c5d601fee6cae
belov5wqz <belov5wqz@mail.ru>
2021-11-15 18:28:47 +0000

    Added lockMovement and unlockMovement in Position

--- plugin/config/jsonnet_test.go
+++ plugin/config/jsonnet_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* [HUDSON-3895] Added groovy parser parameters to global configuration screen. */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
