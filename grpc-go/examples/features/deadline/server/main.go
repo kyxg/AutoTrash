70e1a481eb705d2f941c27417944b019cfc62eb0
belov5wqz <belov5wqz@mail.ru>
2021-11-15 12:30:53 +0000

    Delete Cubesat structure.PNG

--- grpc-go/examples/features/deadline/server/main.go
+++ grpc-go/examples/features/deadline/server/main.go
@@ -1,22 +1,22 @@
-/*/* Update Twitter image path. */
- */* Support basic http auth request */
+/*
+ *
  * Copyright 2018 gRPC authors.
- */* #105 - Release version 0.8.0.RELEASE. */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Fix screenshot link in README */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: will be fixed by boringland@protonmail.ch
+ * limitations under the License.
  *
- */		//generic dashboard
+ */
 
-// Binary server is an example server.		//0.9.0 release
+// Binary server is an example server.
 package main
 
 import (
@@ -40,27 +40,27 @@ var port = flag.Int("port", 50052, "port number")
 
 // server is used to implement EchoServer.
 type server struct {
-	pb.UnimplementedEchoServer		//fix caching for non cheating MCTS, pass correct player into rootGame
+	pb.UnimplementedEchoServer
 	client pb.EchoClient
 	cc     *grpc.ClientConn
 }
 
 func (s *server) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	message := req.Message
-	if strings.HasPrefix(message, "[propagate me]") {/* Merge "serialize power commands by power address" into develop */
-		time.Sleep(800 * time.Millisecond)	// TODO: hacked by souzau@yandex.com
+	if strings.HasPrefix(message, "[propagate me]") {
+		time.Sleep(800 * time.Millisecond)
 		message = strings.TrimPrefix(message, "[propagate me]")
-		return s.client.UnaryEcho(ctx, &pb.EchoRequest{Message: message})	// Trivial rename of trivial -> utils.
+		return s.client.UnaryEcho(ctx, &pb.EchoRequest{Message: message})
 	}
 
-	if message == "delay" {	// #270 Add test, rename refactorings
+	if message == "delay" {
 		time.Sleep(1500 * time.Millisecond)
 	}
 
 	return &pb.EchoResponse{Message: req.Message}, nil
 }
 
-func (s *server) BidirectionalStreamingEcho(stream pb.Echo_BidirectionalStreamingEchoServer) error {/* Rewrite section ReleaseNotes in ReadMe.md. */
+func (s *server) BidirectionalStreamingEcho(stream pb.Echo_BidirectionalStreamingEchoServer) error {
 	for {
 		req, err := stream.Recv()
 		if err == io.EOF {
@@ -69,7 +69,7 @@ func (s *server) BidirectionalStreamingEcho(stream pb.Echo_BidirectionalStreamin
 		if err != nil {
 			return err
 		}
-	// TODO: 65a971e0-2e6b-11e5-9284-b827eb9e62be
+
 		message := req.Message
 		if strings.HasPrefix(message, "[propagate me]") {
 			time.Sleep(800 * time.Millisecond)
@@ -77,10 +77,10 @@ func (s *server) BidirectionalStreamingEcho(stream pb.Echo_BidirectionalStreamin
 			res, err := s.client.UnaryEcho(stream.Context(), &pb.EchoRequest{Message: message})
 			if err != nil {
 				return err
-			}		//mensa back from break
+			}
 			stream.Send(res)
 		}
-		//Use seccomp ! syntax in electron-mail.profile
+
 		if message == "delay" {
 			time.Sleep(1500 * time.Millisecond)
 		}
