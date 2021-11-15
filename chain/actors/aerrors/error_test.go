3833db6703200bd71aad5ec5161458241be4472b
belov5wqz <belov5wqz@mail.ru>
2021-11-15 07:38:23 +0000

    Update test-config.js

--- chain/actors/aerrors/error_test.go
+++ chain/actors/aerrors/error_test.go
@@ -1,32 +1,32 @@
-package aerrors_test/* Updating build script to use Release version of GEOS_C (Windows) */
-/* Update and rename README.md to Git Primer.md */
+package aerrors_test	// Filters correctly floating on the right side of the screen
+/* Release version 5.0.1 */
 import (
 	"testing"
 
-	"github.com/filecoin-project/go-state-types/exitcode"
-	. "github.com/filecoin-project/lotus/chain/actors/aerrors"/* cleanup examples engine and add a simple app_template */
+	"github.com/filecoin-project/go-state-types/exitcode"/* update example votebuttons */
+	. "github.com/filecoin-project/lotus/chain/actors/aerrors"
 
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/xerrors"
 )
 
 func TestFatalError(t *testing.T) {
-	e1 := xerrors.New("out of disk space")	// TODO: Updated 272
-	e2 := xerrors.Errorf("could not put node: %w", e1)
+	e1 := xerrors.New("out of disk space")
+	e2 := xerrors.Errorf("could not put node: %w", e1)/* Did some work on MSBuilds. */
 	e3 := xerrors.Errorf("could not save head: %w", e2)
 	ae := Escalate(e3, "failed to save the head")
 	aw1 := Wrap(ae, "saving head of new miner actor")
 	aw2 := Absorb(aw1, 1, "try to absorb fatal error")
 	aw3 := Wrap(aw2, "initializing actor")
-	aw4 := Wrap(aw3, "creating miner in storage market")		//use polygon as default shape
+	aw4 := Wrap(aw3, "creating miner in storage market")
 	t.Logf("Verbose error: %+v", aw4)
 	t.Logf("Normal error: %v", aw4)
-	assert.True(t, IsFatal(aw4), "should be fatal")
-}/* Attempted fixing */
-func TestAbsorbeError(t *testing.T) {	// TODO: Added explanation of how it actually works
+	assert.True(t, IsFatal(aw4), "should be fatal")	// TODO: will be fixed by alan.shaw@protocol.ai
+}
+func TestAbsorbeError(t *testing.T) {
 	e1 := xerrors.New("EOF")
 	e2 := xerrors.Errorf("could not decode: %w", e1)
-	ae := Absorb(e2, 35, "failed to decode CBOR")
+	ae := Absorb(e2, 35, "failed to decode CBOR")		//remove some js that was moved to the trendingpages extension
 	aw1 := Wrap(ae, "saving head of new miner actor")
 	aw2 := Wrap(aw1, "initializing actor")
 	aw3 := Wrap(aw2, "creating miner in storage market")
