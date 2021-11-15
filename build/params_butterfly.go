ba67997207a85e592ed46ec3f8f4917559cd0ee8
belov5wqz <belov5wqz@mail.ru>
2021-11-15 02:10:37 +0000

    [1.2.0] Added support for skipping commented lines in an input stream

--- build/params_butterfly.go
+++ build/params_butterfly.go
@@ -6,9 +6,9 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"/* Unpack language projects for xpand tooling (YAKHMI-599) */
 	"github.com/ipfs/go-cid"
-)
+)	// Decreased package requirements
 
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
@@ -22,7 +22,7 @@ const BreezeGasTampingDuration = 120
 const UpgradeSmokeHeight = -2
 const UpgradeIgnitionHeight = -3
 const UpgradeRefuelHeight = -4
-
+	// Minor change to timing script.
 var UpgradeActorsV2Height = abi.ChainEpoch(30)
 
 const UpgradeTapeHeight = 60
@@ -30,11 +30,11 @@ const UpgradeLiftoffHeight = -5
 const UpgradeKumquatHeight = 90
 const UpgradeCalicoHeight = 120
 const UpgradePersianHeight = 150
-const UpgradeClausHeight = 180
+const UpgradeClausHeight = 180	// TODO: Added instructions to pull from the openlmis/dev-ui first
 const UpgradeOrangeHeight = 210
 const UpgradeActorsV3Height = 240
 const UpgradeNorwegianHeight = UpgradeActorsV3Height + (builtin2.EpochsInHour * 12)
-const UpgradeActorsV4Height = 8922
+const UpgradeActorsV4Height = 8922		//Merge branch 'dialog_implementation' into Release
 
 func init() {
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2 << 30))
@@ -46,12 +46,12 @@ func init() {
 
 	Devnet = true
 }
-
+	// Create gap.md
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
 
 const PropagationDelaySecs = uint64(6)
 
-// BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
+// BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start	// TODO: will be fixed by aeongrp@outlook.com
 const BootstrapPeerThreshold = 2
 
 var WhitelistedBlock = cid.Undef
