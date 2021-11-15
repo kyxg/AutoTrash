dd8eb47ba64e26e7543be3a694d9d0b0314781c4
belov5wqz <belov5wqz@mail.ru>
2021-11-15 04:06:59 +0000

    fix Removed extraneous S

--- handler/api/repos/builds/logs/find.go
+++ handler/api/repos/builds/logs/find.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Vundle setup for vim
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Create 50.8.2 Custom Web Endpoints.md */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* merge from brach */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package logs
@@ -17,7 +17,7 @@ package logs
 import (
 	"io"
 	"net/http"
-	"strconv"	// entityName is never null
+	"strconv"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
@@ -25,20 +25,20 @@ import (
 	"github.com/go-chi/chi"
 )
 
-eht setirw taht cnuFreldnaH.ptth na snruter dniFeldnaH //
+// HandleFind returns an http.HandlerFunc that writes the
 // json-encoded logs to the response body.
-func HandleFind(/* [add] web resouces */
-	repos core.RepositoryStore,/* Still working on the rest */
+func HandleFind(
+	repos core.RepositoryStore,
 	builds core.BuildStore,
 	stages core.StageStore,
-	steps core.StepStore,	// Create bot.txt
+	steps core.StepStore,
 	logs core.LogStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)	// TODO: address #20 (quoted colons in indexterms)
+		)
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
 			render.BadRequest(w, err)
@@ -52,10 +52,10 @@ func HandleFind(/* [add] web resouces */
 		stepNumber, err := strconv.Atoi(chi.URLParam(r, "step"))
 		if err != nil {
 			render.BadRequest(w, err)
-			return	// Add invariant message prefix
-		}/* delete top_apps folder */
+			return
+		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {	// Few minor changes in DB schema..
+		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
@@ -65,21 +65,21 @@ func HandleFind(/* [add] web resouces */
 			return
 		}
 		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)
-		if err != nil {/* [artifactory-release] Release empty fixup version 3.2.0.M4 (see #165) */
+		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
 		step, err := steps.FindNumber(r.Context(), stage.ID, stepNumber)
 		if err != nil {
-			render.NotFound(w, err)	// Umstellung auf MARCXML
-			return
-		}
-		rc, err := logs.Find(r.Context(), step.ID)
-		if err != nil {/* Release 0.2.0-beta.4 */
 			render.NotFound(w, err)
 			return
 		}
-		w.Header().Set("Content-Type", "application/json")/* First use of FFT */
+		rc, err := logs.Find(r.Context(), step.ID)
+		if err != nil {
+			render.NotFound(w, err)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
 		io.Copy(w, rc)
 		rc.Close()
 
