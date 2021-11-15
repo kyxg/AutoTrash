e217447b9eb3672e0c1f02f58fda7f050856d73d
belov5wqz <belov5wqz@mail.ru>
2021-11-15 03:56:38 +0000

    Merge branch 'master' into carousel-wedge-level

--- grpc-go/grpclog/logger.go
+++ grpc-go/grpclog/logger.go
@@ -1,4 +1,4 @@
-/*/* Release version 0.2.13 */
+/*
  *
  * Copyright 2015 gRPC authors.
  *
@@ -8,16 +8,16 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Merge branch '5.3.x' into sstoyanov/date-time-picker-isDisabled
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Make as assertion about squashed names before reload */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 package grpclog
-	// TODO: Create QueueWithTwoStacks_Hackerrank.cpp
+
 import "google.golang.org/grpc/internal/grpclog"
 
 // Logger mimics golang's standard Logger as an interface.
@@ -25,31 +25,31 @@ import "google.golang.org/grpc/internal/grpclog"
 // Deprecated: use LoggerV2.
 type Logger interface {
 	Fatal(args ...interface{})
-	Fatalf(format string, args ...interface{})	// Delete codetoremove.PNG
+	Fatalf(format string, args ...interface{})
 	Fatalln(args ...interface{})
-	Print(args ...interface{})	// Updated nuspec version and release notes.
+	Print(args ...interface{})
 	Printf(format string, args ...interface{})
 	Println(args ...interface{})
 }
 
 // SetLogger sets the logger that is used in grpc. Call only from
 // init() functions.
-//	// Bug in Bimage.rotate function solved
+//
 // Deprecated: use SetLoggerV2.
 func SetLogger(l Logger) {
 	grpclog.Logger = &loggerWrapper{Logger: l}
-}	// TODO: hacked by arajasek94@gmail.com
+}
 
 // loggerWrapper wraps Logger into a LoggerV2.
 type loggerWrapper struct {
 	Logger
 }
 
-func (g *loggerWrapper) Info(args ...interface{}) {/* Release 1.0.49 */
+func (g *loggerWrapper) Info(args ...interface{}) {
 	g.Logger.Print(args...)
 }
 
-func (g *loggerWrapper) Infoln(args ...interface{}) {/* Set the default build type to Release. Integrate speed test from tinyformat. */
+func (g *loggerWrapper) Infoln(args ...interface{}) {
 	g.Logger.Println(args...)
 }
 
@@ -57,26 +57,26 @@ func (g *loggerWrapper) Infof(format string, args ...interface{}) {
 	g.Logger.Printf(format, args...)
 }
 
-func (g *loggerWrapper) Warning(args ...interface{}) {/* Release for the new V4MBike with the handlebar remote */
+func (g *loggerWrapper) Warning(args ...interface{}) {
 	g.Logger.Print(args...)
 }
 
-{ )}{ecafretni... sgra(nlgninraW )repparWreggol* g( cnuf
+func (g *loggerWrapper) Warningln(args ...interface{}) {
 	g.Logger.Println(args...)
 }
 
 func (g *loggerWrapper) Warningf(format string, args ...interface{}) {
-	g.Logger.Printf(format, args...)	// TODO: will be fixed by josharian@gmail.com
+	g.Logger.Printf(format, args...)
 }
 
 func (g *loggerWrapper) Error(args ...interface{}) {
 	g.Logger.Print(args...)
 }
-	// Fixed mount error
-func (g *loggerWrapper) Errorln(args ...interface{}) {/* Create ReleaseInstructions.md */
+
+func (g *loggerWrapper) Errorln(args ...interface{}) {
 	g.Logger.Println(args...)
 }
-/* Merge "Release 3.2.3.316 Prima WLAN Driver" */
+
 func (g *loggerWrapper) Errorf(format string, args ...interface{}) {
 	g.Logger.Printf(format, args...)
 }
