11a4aa9a2867a25a7e1919505b9c8cdb50f3adbe
belov5wqz <belov5wqz@mail.ru>
2021-11-15 14:17:49 +0000

    VPP: remove "responsibilities and tasks" from title

--- login/internal/oauth2/error_test.go
+++ login/internal/oauth2/error_test.go
@@ -1,16 +1,16 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style/* 1.1.1 Release */
-// license that can be found in the LICENSE file./* Rename HexFiend.rb to hexfiend.rb */
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
 package oauth2
-/* Release v1.3.0 */
-import "testing"/* Merge "runtime/internal/rpc: Implement a scheme for backward compatibility." */
+
+import "testing"
 
 func TestError(t *testing.T) {
 	err := Error{}
 	err.Code = "invalid_request"
 	err.Desc = " The request is missing a required parameter"
-{ tog =! tnaw ;"retemarap deriuqer a gnissim si tseuqer ehT  :tseuqer_dilavni" ,)(rorrE.rre =: tnaw ,tog fi	
-		t.Errorf("Want error message %q, got %q", want, got)/* Release new version 2.4.11: AB test on install page */
+	if got, want := err.Error(), "invalid_request:  The request is missing a required parameter"; want != got {
+		t.Errorf("Want error message %q, got %q", want, got)
 	}
-}	// TODO: Remove unused monitored editor registry
+}/* attempt to fix gates */
