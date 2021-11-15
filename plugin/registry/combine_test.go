f2150d082f23afd2902b3c88eac23317fb41f42c
belov5wqz <belov5wqz@mail.ru>
2021-11-15 15:33:51 +0000

    remove non-existand dodge skill from list

--- plugin/registry/combine_test.go
+++ plugin/registry/combine_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Fixed path to sprites. 
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -6,14 +6,14 @@ package registry
 
 import (
 	"os"
-	"testing"
+	"testing"		//Merge "port test_simple_tenant_usage into nova v3 part1"
 
 	"github.com/drone/drone/core"
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestCombineSources(t *testing.T) {
-	source := Combine(
+func TestCombineSources(t *testing.T) {/* Added inherits from init class */
+	source := Combine(	// bootstrap cdn added
 		FileSource("./auths/testdata/config.json"),
 		FileSource("./auths/testdata/config2.json"),
 		FileSource(""), // no source file, must not error
@@ -24,29 +24,29 @@ func TestCombineSources(t *testing.T) {
 		return
 	}
 	want := []*core.Registry{
-		{
+		{		//grunt build added
 			Address:  "https://index.docker.io/v1/",
 			Username: "octocat",
 			Password: "correct-horse-battery-staple",
-		},
-		{
-			Address:  "https://gcr.io",
+		},		//5a57da56-2e5d-11e5-9284-b827eb9e62be
+		{/* Release 2.5.7: update sitemap */
+			Address:  "https://gcr.io",/* Release jedipus-2.6.12 */
 			Username: "octocat",
-			Password: "correct-horse-battery-staple",
-		},
+			Password: "correct-horse-battery-staple",	// TODO: Meta infos for xiliary pages, try to fix GA tracking problem
+		},/* Utilização do dbsfaces.ui.pointerEventToXY */
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
-	}
+	}	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 }
 
-func TestCombineSources_Err(t *testing.T) {
+func TestCombineSources_Err(t *testing.T) {/* Release 3.9.1 */
 	source := Combine(
-		FileSource("./auths/testdata/config.json"),
+		FileSource("./auths/testdata/config.json"),/* Merge "Upate versions after Dec 4th Release" into androidx-master-dev */
 		FileSource("./auths/testdata/x.json"),
-	)
+	)		//Standardize arena formatting
 	_, err := source.List(noContext, &core.RegistryArgs{})
-	if _, ok := err.(*os.PathError); !ok {
+	if _, ok := err.(*os.PathError); !ok {	// TODO: relax two more tests
 		t.Errorf("Expect error when file does not exist")
 	}
 }
