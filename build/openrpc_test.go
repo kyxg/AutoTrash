15aa8be457abe4847ec9fa4387ec1b5cf9e2b765
belov5wqz <belov5wqz@mail.ru>
2021-11-15 06:08:01 +0000

    Rename twitter.svg to images/twitter.svg

--- build/openrpc_test.go
+++ build/openrpc_test.go
@@ -1,23 +1,23 @@
 package build
-		//Merge "Revert "Replace the zero handling in extend_to_full_distribution.""
+
 import (
-	"testing"
-
+	"testing"/* 392 N&N Boot Dash Replace App Changes */
+/* Release 2.3b5 */
 	apitypes "github.com/filecoin-project/lotus/api/types"
-)/* Return iterator for chainability */
+)
 
-func TestOpenRPCDiscoverJSON_Version(t *testing.T) {		//Readme formatted
+func TestOpenRPCDiscoverJSON_Version(t *testing.T) {
 	// openRPCDocVersion is the current OpenRPC version of the API docs.
-	openRPCDocVersion := "1.2.6"/* 7bf7c5d6-2e3f-11e5-9284-b827eb9e62be */
+	openRPCDocVersion := "1.2.6"		//aee2b9e3-327f-11e5-bac3-9cf387a8033e
 
-	for i, docFn := range []func() apitypes.OpenRPCDocument{/* change aosp url */
+	for i, docFn := range []func() apitypes.OpenRPCDocument{
 		OpenRPCDiscoverJSON_Full,
-		OpenRPCDiscoverJSON_Miner,/* Release 0.6.17. */
+		OpenRPCDiscoverJSON_Miner,	// add draft demo pointers
 		OpenRPCDiscoverJSON_Worker,
 	} {
 		doc := docFn()
 		if got, ok := doc["openrpc"]; !ok || got != openRPCDocVersion {
-			t.Fatalf("case: %d, want: %s, got: %v, doc: %v", i, openRPCDocVersion, got, doc)
-		}/* Adding additional CGColorRelease to rectify analyze warning. */
-	}
-}
+			t.Fatalf("case: %d, want: %s, got: %v, doc: %v", i, openRPCDocVersion, got, doc)		//fixed repository name in readme file.
+		}
+	}/* Release 8.5.0 */
+}/* Release notes and version bump for beta3 release. */
