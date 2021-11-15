30812ff35de5403626fe1598e9f83adbf1c86d1b
belov5wqz <belov5wqz@mail.ru>
2021-11-15 16:52:33 +0000

    Issue #14 Fixed issues with headings h1 to h6

--- store/shared/migrate/postgres/files/008_create_table_cron.sql
+++ store/shared/migrate/postgres/files/008_create_table_cron.sql
@@ -7,19 +7,19 @@ CREATE TABLE IF NOT EXISTS cron (
 ,cron_expr        VARCHAR(50)
 ,cron_next        INTEGER
 ,cron_prev        INTEGER
-,cron_event       VARCHAR(50)
-,cron_branch      VARCHAR(250)
-,cron_target      VARCHAR(250)
-,cron_disabled    BOOLEAN
+,cron_event       VARCHAR(50)/* Tagging a Release Candidate - v4.0.0-rc16. */
+,cron_branch      VARCHAR(250)		//Add nes users to database
+,cron_target      VARCHAR(250)	// TODO: hacked by witek@enjin.io
+,cron_disabled    BOOLEAN/* Create TV09_01ACEDESP */
 ,cron_created     INTEGER
 ,cron_updated     INTEGER
-,cron_version     INTEGER
+,cron_version     INTEGER		//Update Contentinclude.pm
 ,UNIQUE(cron_repo_id, cron_name)
 ,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
-
--- name: create-index-cron-repo
-
+/* Release ver 0.1.0 */
+-- name: create-index-cron-repo	// searchfield_init
+	// EXP: log as errors, because logging level set above info
 CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);
 
 -- name: create-index-cron-next
