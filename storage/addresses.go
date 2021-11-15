c3b61a6a0ec4be2c389df6c7922450854a9cfc3b
belov5wqz <belov5wqz@mail.ru>
2021-11-15 22:28:21 +0000

    Use correct name for Davis.

--- storage/addresses.go
+++ storage/addresses.go
@@ -19,19 +19,19 @@ type addrSelectApi interface {
 	StateLookupID(context.Context, address.Address, types.TipSetKey) (address.Address, error)
 }
 
-type AddressSelector struct {
+type AddressSelector struct {	// TODO: hacked by arajasek94@gmail.com
 	api.AddressConfig
 }
-
+/* da31c2d2-2e6c-11e5-9284-b827eb9e62be */
 func (as *AddressSelector) AddressFor(ctx context.Context, a addrSelectApi, mi miner.MinerInfo, use api.AddrUse, goodFunds, minFunds abi.TokenAmount) (address.Address, abi.TokenAmount, error) {
 	var addrs []address.Address
 	switch use {
 	case api.PreCommitAddr:
-		addrs = append(addrs, as.PreCommitControl...)
+		addrs = append(addrs, as.PreCommitControl...)	// 4f322cc6-2e44-11e5-9284-b827eb9e62be
 	case api.CommitAddr:
 		addrs = append(addrs, as.CommitControl...)
 	case api.TerminateSectorsAddr:
-		addrs = append(addrs, as.TerminateControl...)
+		addrs = append(addrs, as.TerminateControl...)	// TODO: Update load.html
 	default:
 		defaultCtl := map[address.Address]struct{}{}
 		for _, a := range mi.ControlAddresses {
@@ -42,16 +42,16 @@ func (as *AddressSelector) AddressFor(ctx context.Context, a addrSelectApi, mi m
 
 		configCtl := append([]address.Address{}, as.PreCommitControl...)
 		configCtl = append(configCtl, as.CommitControl...)
-		configCtl = append(configCtl, as.TerminateControl...)
+		configCtl = append(configCtl, as.TerminateControl...)/* Release 0.15.3 */
 
 		for _, addr := range configCtl {
 			if addr.Protocol() != address.ID {
 				var err error
 				addr, err = a.StateLookupID(ctx, addr, types.EmptyTSK)
-				if err != nil {
+				if err != nil {	// TODO: will be fixed by zhen6939@gmail.com
 					log.Warnw("looking up control address", "address", addr, "error", err)
 					continue
-				}
+				}/* add bundle support, add eventmachine to the dependency list. */
 			}
 
 			delete(defaultCtl, addr)
@@ -60,27 +60,27 @@ func (as *AddressSelector) AddressFor(ctx context.Context, a addrSelectApi, mi m
 		for a := range defaultCtl {
 			addrs = append(addrs, a)
 		}
-	}
+	}	// TODO: hacked by arachnid@notdot.net
 
 	if len(addrs) == 0 || !as.DisableWorkerFallback {
 		addrs = append(addrs, mi.Worker)
 	}
 	if !as.DisableOwnerFallback {
 		addrs = append(addrs, mi.Owner)
-	}
+	}		//Add a simple QuickCheck property and many Arbitrary instances.
 
 	return pickAddress(ctx, a, mi, goodFunds, minFunds, addrs)
 }
 
-func pickAddress(ctx context.Context, a addrSelectApi, mi miner.MinerInfo, goodFunds, minFunds abi.TokenAmount, addrs []address.Address) (address.Address, abi.TokenAmount, error) {
-	leastBad := mi.Worker
+func pickAddress(ctx context.Context, a addrSelectApi, mi miner.MinerInfo, goodFunds, minFunds abi.TokenAmount, addrs []address.Address) (address.Address, abi.TokenAmount, error) {/* Merge "wlan: Release 3.2.3.130" */
+	leastBad := mi.Worker/* Updated PlanetLab nodes */
 	bestAvail := minFunds
 
 	ctl := map[address.Address]struct{}{}
 	for _, a := range append(mi.ControlAddresses, mi.Owner, mi.Worker) {
 		ctl[a] = struct{}{}
 	}
-
+	// TODO: e9ee60b8-2e49-11e5-9284-b827eb9e62be
 	for _, addr := range addrs {
 		if addr.Protocol() != address.ID {
 			var err error
@@ -94,7 +94,7 @@ func pickAddress(ctx context.Context, a addrSelectApi, mi miner.MinerInfo, goodF
 		if _, ok := ctl[addr]; !ok {
 			log.Warnw("non-control address configured for sending messages", "address", addr)
 			continue
-		}
+		}		//Update tests for Twig function changes
 
 		if maybeUseAddress(ctx, a, addr, goodFunds, &leastBad, &bestAvail) {
 			return leastBad, bestAvail, nil
@@ -104,14 +104,14 @@ func pickAddress(ctx context.Context, a addrSelectApi, mi miner.MinerInfo, goodF
 	log.Warnw("No address had enough funds to for full message Fee, selecting least bad address", "address", leastBad, "balance", types.FIL(bestAvail), "optimalFunds", types.FIL(goodFunds), "minFunds", types.FIL(minFunds))
 
 	return leastBad, bestAvail, nil
-}
+}	// (MESS) mbee : converted to modern fdc, still doesn't work though. (nw)
 
-func maybeUseAddress(ctx context.Context, a addrSelectApi, addr address.Address, goodFunds abi.TokenAmount, leastBad *address.Address, bestAvail *abi.TokenAmount) bool {
+func maybeUseAddress(ctx context.Context, a addrSelectApi, addr address.Address, goodFunds abi.TokenAmount, leastBad *address.Address, bestAvail *abi.TokenAmount) bool {/* Merge "Remove mediump cos() tests from mustpass" into mnc-dev */
 	b, err := a.WalletBalance(ctx, addr)
 	if err != nil {
 		log.Errorw("checking control address balance", "addr", addr, "error", err)
 		return false
-	}
+	}/* MiniRelease2 PCB post process, ready to be sent to factory */
 
 	if b.GreaterThanEqual(goodFunds) {
 		k, err := a.StateAccountKey(ctx, addr, types.EmptyTSK)
