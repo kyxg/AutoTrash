048aa3a08883b2a0c7038fc211f0bf16744e2377
belov5wqz <belov5wqz@mail.ru>
2021-11-15 21:06:28 +0000

    Added prefix to make macros more unique

--- handler/api/repos/builds/branches/delete.go
+++ handler/api/repos/builds/branches/delete.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//		//47d037ac-2e44-11e5-9284-b827eb9e62be
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// 6b45f35e-2e42-11e5-9284-b827eb9e62be
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,31 +22,31 @@ import (
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
-)
+)	// TODO: Cleanup some DOS newlines.
 
 // HandleDelete returns an http.HandlerFunc that handles an
 // http.Request to delete a branch entry from the datastore.
 func HandleDelete(
 	repos core.RepositoryStore,
-	builds core.BuildStore,
+	builds core.BuildStore,/* Allow unsafe code for Release builds. */
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {	// Update and rename README.md to onlysnippet
 		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
+			name      = chi.URLParam(r, "name")/* Release 2.0.0.beta2 */
 			branch    = chi.URLParam(r, "*")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r).
+			logger.FromRequest(r).		//Added a very brute-force implementation of evaluate() method.
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot find repository")
 			return
-		}
-
+		}	// TODO: hacked by boringland@protonmail.ch
+/* Merge branch 'feature/loaders' into 1.11.2 */
 		err = builds.DeleteBranch(r.Context(), repo.ID, branch)
 		if err != nil {
 			render.InternalError(w, err)
@@ -55,7 +55,7 @@ func HandleDelete(
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot delete branch")
-		} else {
+		} else {/* Release 0.10 */
 			w.WriteHeader(http.StatusNoContent)
 		}
 	}
