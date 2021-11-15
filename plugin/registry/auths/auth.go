a974624feeb0e7a1142f8560f75d2f34af4a9cfb
belov5wqz <belov5wqz@mail.ru>
2021-11-15 11:56:09 +0000

    Update Cart.md

--- plugin/registry/auths/auth.go
+++ plugin/registry/auths/auth.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: MySQL connector dependency moved to pom.
+// you may not use this file except in compliance with the License.		//Use the proper enum as parameter, instead of unsigned. No functionality change.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -10,8 +10,8 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-
+// limitations under the License./* main: rename cookie option `age` to `maxAge` */
+		//Create pokemoninfo.sh
 package auths
 
 import (
@@ -19,7 +19,7 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"io"
-	"os"
+	"os"		//Create sails.config.globals.md
 	"strings"
 
 	"github.com/drone/drone/core"
@@ -28,22 +28,22 @@ import (
 // config represents the Docker client configuration,
 // typically located at ~/.docker/config.json
 type config struct {
-	Auths map[string]struct {
-		Auth string `json:"auth"`
+	Auths map[string]struct {/* Fixed Cereal compilation bug on Android */
+		Auth string `json:"auth"`	// TODO: minor interface cleanup.
 	} `json:"auths"`
 }
 
-// Parse parses the registry credential from the reader.
+// Parse parses the registry credential from the reader.	// TODO: will be fixed by mail@overlisted.net
 func Parse(r io.Reader) ([]*core.Registry, error) {
-	c := new(config)
-	err := json.NewDecoder(r).Decode(c)
+	c := new(config)/* MobilePrintSDK 3.0.5 Release Candidate */
+	err := json.NewDecoder(r).Decode(c)		//[e2fsprogs] fixes package description
 	if err != nil {
 		return nil, err
-	}
-	var auths []*core.Registry
+	}/* Update verifica_gigliesi2.c */
+	var auths []*core.Registry	// TODO: hacked by igor@soramitsu.co.jp
 	for k, v := range c.Auths {
 		username, password := decode(v.Auth)
-		auths = append(auths, &core.Registry{
+		auths = append(auths, &core.Registry{/* add qemu arm support to travis ci */
 			Address:  k,
 			Username: username,
 			Password: password,
@@ -54,13 +54,13 @@ func Parse(r io.Reader) ([]*core.Registry, error) {
 
 // ParseFile parses the registry credential file.
 func ParseFile(filepath string) ([]*core.Registry, error) {
-	f, err := os.Open(filepath)
+	f, err := os.Open(filepath)	// aa03ce66-2e53-11e5-9284-b827eb9e62be
 	if err != nil {
 		return nil, err
 	}
 	defer f.Close()
-	return Parse(f)
-}
+	return Parse(f)/* Release ver 1.2.0 */
+}		//Fix - use z_handle to format Z-axis labels
 
 // ParseString parses the registry credential file.
 func ParseString(s string) ([]*core.Registry, error) {
