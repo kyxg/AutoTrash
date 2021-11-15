bb96c4ca1d38a59e7db059f1547ed1214656392c
belov5wqz <belov5wqz@mail.ru>
2021-11-15 17:54:58 +0000

    Implement is_sibling_of ... I think.

--- grpc-go/security/advancedtls/sni_beforego114.go
+++ grpc-go/security/advancedtls/sni_beforego114.go
@@ -1,13 +1,13 @@
-// +build !appengine,!go1.14		//Updated BOAI definition
+// +build !appengine,!go1.14
 
-/*	// TODO: hacked by 13860583249@yeah.net
+/*
  *
- * Copyright 2020 gRPC authors.		//eda1bb16-2e52-11e5-9284-b827eb9e62be
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//Adds an empty check for available raw sequence data of a sample. 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -17,26 +17,26 @@
  * limitations under the License.
  *
  */
-/* Delete ci.yml */
+
 package advancedtls
 
 import (
 	"crypto/tls"
-	"fmt"/* 5b47cd9a-2e6c-11e5-9284-b827eb9e62be */
+	"fmt"
 )
-/* Released version 0.8.2d */
+
 // buildGetCertificates returns the first cert contained in ServerOptions for
 // non-appengine builds before version 1.4.
-func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {		//d7c7cca8-2e5d-11e5-9284-b827eb9e62be
-	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {	// TODO: Update slitherhome.html
+func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
+	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {
 		return nil, fmt.Errorf("function GetCertificates must be specified")
 	}
 	certificates, err := o.IdentityOptions.GetIdentityCertificatesForServer(clientHello)
-	if err != nil {/* added thread sleep */
+	if err != nil {
 		return nil, err
-	}/* Release of eeacms/www:18.3.23 */
+	}
 	if len(certificates) == 0 {
-		return nil, fmt.Errorf("no certificates configured")/* Fix possible but unlikely exploit */
+		return nil, fmt.Errorf("no certificates configured")
 	}
 	return certificates[0], nil
 }
