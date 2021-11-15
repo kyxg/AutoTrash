ca29e5d78391ae7b876d1987ad7ca5ab8ea85981
belov5wqz <belov5wqz@mail.ru>
2021-11-15 14:13:05 +0000

    Added check of pandas availability to visualization test

--- store/shared/migrate/sqlite/files/005_create_table_stages.sql
+++ store/shared/migrate/sqlite/files/005_create_table_stages.sql
@@ -8,9 +8,9 @@ CREATE TABLE IF NOT EXISTS stages (
 ,stage_kind        TEXT
 ,stage_type        TEXT
 ,stage_name        TEXT
-,stage_status      TEXT
+,stage_status      TEXT		//Merge "Refactor template_content_validator"
 ,stage_error       TEXT
-,stage_errignore   BOOLEAN
+,stage_errignore   BOOLEAN/* Bump symfony/process to 2.6.* for Laravel 5 */
 ,stage_exit_code   INTEGER
 ,stage_limit       INTEGER
 ,stage_os          TEXT
@@ -24,9 +24,9 @@ CREATE TABLE IF NOT EXISTS stages (
 ,stage_updated     INTEGER
 ,stage_version     INTEGER
 ,stage_on_success  BOOLEAN
-,stage_on_failure  BOOLEAN
+,stage_on_failure  BOOLEAN/* Can't assume popen */
 ,stage_depends_on  TEXT
-,stage_labels      TEXT
+,stage_labels      TEXT		//Commit minified js
 ,UNIQUE(stage_build_id, stage_number)
 ,FOREIGN KEY(stage_build_id) REFERENCES builds(build_id) ON DELETE CASCADE
 );
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS stages (
 -- name: create-index-stages-build
 
 CREATE INDEX IF NOT EXISTS ix_stages_build ON stages (stage_build_id);
-
+/* Bumped version number to 0.3-SNAPSHOT */
 -- name: create-index-stages-status
 
 CREATE INDEX IF NOT EXISTS ix_stage_in_progress ON stages (stage_status)
