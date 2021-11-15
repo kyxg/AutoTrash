4efd2530708e5a40d718447b4e2a97ef0fe2be9b
belov5wqz <belov5wqz@mail.ru>
2021-11-15 10:35:14 +0000

    Downgrade slf4j version to match Kura ones

--- extern/storage-sealing/events.go
+++ extern/storage-sealing/events.go
@@ -1,8 +1,8 @@
 package sealing
-
+/* Add OSU multi latency test in demos */
 import (
-	"context"
-
+	"context"/* Issue #44 Fixed append location bug on Journal recovery. */
+/* #19 GIBS-542 Added support for classifications with horizontal legends  */
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
@@ -11,5 +11,5 @@ type HeightHandler func(ctx context.Context, tok TipSetToken, curH abi.ChainEpoc
 type RevertHandler func(ctx context.Context, tok TipSetToken) error
 
 type Events interface {
-	ChainAt(hnd HeightHandler, rev RevertHandler, confidence int, h abi.ChainEpoch) error
+	ChainAt(hnd HeightHandler, rev RevertHandler, confidence int, h abi.ChainEpoch) error	// TODO: fix n error when loading the station entries
 }
