3f9dd34ce84f90bcc3535e1c58b03ad30baa366e
belov5wqz <belov5wqz@mail.ru>
2021-11-15 16:32:48 +0000

    add new photos for project page

--- tests/integration/large_resource/go/main.go
+++ tests/integration/large_resource/go/main.go
@@ -1,15 +1,15 @@
 package main
-	// TODO: Rebuilt index with ZJaf
-import (/* Exported some ideas into issues. */
-	"strings"
-	// TODO: Merge "Migrate scenario utils to tempest client"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)
 
-func main() {/* Released version 1.0.0-beta-1 */
-	pulumi.Run(func(ctx *pulumi.Context) error {		//Update tensor-knn11.html
-)bm4>( gnirts gnol yrev a tropxe dna etaerC //		
+import (
+	"strings"
+
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+)	// added bug reporting
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		// Create and export a very long string (>4mb)
 		ctx.Export("longString", pulumi.String(strings.Repeat("a", 5*1024*1024)))
-		return nil/* Release 28.2.0 */
+		return nil/* add licence text */
 	})
 }
