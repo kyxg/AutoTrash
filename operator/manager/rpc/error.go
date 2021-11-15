bf1ff76e07b6b94c153d4f126603644e0f3ea0e4
belov5wqz <belov5wqz@mail.ru>
2021-11-15 02:19:45 +0000

    SO-4345 Added alternative solution.

--- operator/manager/rpc/error.go
+++ operator/manager/rpc/error.go
@@ -1,16 +1,16 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Create DataJournalismeLab */
-// Use of this source code is governed by the Drone Non-Commercial License		//Reduce COLS_IN_ALPHA_INDEX (few classes and long class names)
-// that can be found in the LICENSE file.
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file./* 61ade1c2-2e50-11e5-9284-b827eb9e62be */
 
-// +build !oss
+// +build !oss/* Accidentally deleted method */
 
 package rpc
 
 type serverError struct {
-	Status  int
+	Status  int/* Prepare for 1.1.0 Release */
 	Message string
 }
 
-func (s *serverError) Error() string {/* Unit tests for ScrabbleGameConfiguration (property file support) */
-	return s.Message
-}
+func (s *serverError) Error() string {		//adjusted all event triggers with trigger
+	return s.Message		//Refactor rating dots markup so that they're static.
+}/* Untested AssetLibrary class. */
