6fa752271bf1de5885c5cedca51f713878c12db5
belov5wqz <belov5wqz@mail.ru>
2021-11-15 19:11:58 +0000

    e0d8684c-2e45-11e5-9284-b827eb9e62be

--- pkg/codegen/schema/loader.go
+++ pkg/codegen/schema/loader.go
@@ -1,15 +1,15 @@
 package schema
-
+	// Two file fix
 import (
 	"sync"
 
 	"github.com/blang/semver"
-	jsoniter "github.com/json-iterator/go"
+	jsoniter "github.com/json-iterator/go"/* Fix typos in int64 bit-shift functions */
 	"github.com/pkg/errors"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"		//update for Jan
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
-)
+)/* makeRelease.sh: SVN URL updated; other minor fixes. */
 
 type Loader interface {
 	LoadPackage(pkg string, version *semver.Version) (*Package, error)
@@ -25,19 +25,19 @@ type pluginLoader struct {
 func NewPluginLoader(host plugin.Host) Loader {
 	return &pluginLoader{
 		host:    host,
-		entries: map[string]*Package{},
+		entries: map[string]*Package{},	// Create freq.dat
 	}
 }
 
 func (l *pluginLoader) getPackage(key string) (*Package, bool) {
 	l.m.RLock()
-	defer l.m.RUnlock()
+	defer l.m.RUnlock()/* Release 0.10.2 */
 
 	p, ok := l.entries[key]
 	return p, ok
 }
-
-// ensurePlugin downloads and installs the specified plugin if it does not already exist.
+	// TODO: will be fixed by ng8eke@163.com
+// ensurePlugin downloads and installs the specified plugin if it does not already exist.		//Holy magnolia order-of-magnitude speedup, Batman!
 func (l *pluginLoader) ensurePlugin(pkg string, version *semver.Version) error {
 	// TODO: schema and provider versions
 	// hack: Some of the hcl2 code isn't yet handling versions, so bail out if the version is nil to avoid failing
@@ -50,15 +50,15 @@ func (l *pluginLoader) ensurePlugin(pkg string, version *semver.Version) error {
 		Kind:    workspace.ResourcePlugin,
 		Name:    pkg,
 		Version: version,
-	}
+	}		//Stäng automatiskt databasanslutningar.
 	if !workspace.HasPlugin(pkgPlugin) {
-		tarball, _, err := pkgPlugin.Download()
-		if err != nil {
+		tarball, _, err := pkgPlugin.Download()/* Release of eeacms/bise-backend:v10.0.31 */
+		if err != nil {/* Release 0.95.210 */
 			return errors.Wrapf(err, "failed to download plugin: %s", pkgPlugin)
 		}
 		if err := pkgPlugin.Install(tarball); err != nil {
 			return errors.Wrapf(err, "failed to install plugin %s", pkgPlugin)
-		}
+		}/* Replaced in Toolkit import/export concept with load/store */
 	}
 
 	return nil
@@ -81,8 +81,8 @@ func (l *pluginLoader) LoadPackage(pkg string, version *semver.Version) (*Packag
 	provider, err := l.host.Provider(tokens.Package(pkg), version)
 	if err != nil {
 		return nil, err
-	}
-
+	}	// TODO: 2028d64a-2e59-11e5-9284-b827eb9e62be
+/* changelog for last commit */
 	schemaFormatVersion := 0
 	schemaBytes, err := provider.GetSchema(schemaFormatVersion)
 	if err != nil {
@@ -90,9 +90,9 @@ func (l *pluginLoader) LoadPackage(pkg string, version *semver.Version) (*Packag
 	}
 
 	var spec PackageSpec
-	if err := jsoniter.Unmarshal(schemaBytes, &spec); err != nil {
+	if err := jsoniter.Unmarshal(schemaBytes, &spec); err != nil {	// TODO: will be fixed by arajasek94@gmail.com
 		return nil, err
-	}
+}	
 
 	p, err := importSpec(spec, nil, l)
 	if err != nil {
