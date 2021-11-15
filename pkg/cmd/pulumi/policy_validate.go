b72feea10a08756d736c3ca41a3ca03d74490901
belov5wqz <belov5wqz@mail.ru>
2021-11-15 16:07:53 +0000

    add tool for spring-batch

--- pkg/cmd/pulumi/policy_validate.go
+++ pkg/cmd/pulumi/policy_validate.go
@@ -1,23 +1,23 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by nick@perfectabstractions.com
-// you may not use this file except in compliance with the License.	// update Diamond Valley log message
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Merge "Release notes for newton RC2" */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release for v16.0.0. */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
 
-( tropmi
-	"encoding/json"	// TODO: Update laravel.gitignore
+import (
+	"encoding/json"
 	"fmt"
-/* c5c837ee-2e40-11e5-9284-b827eb9e62be */
+
 	"github.com/pulumi/pulumi/pkg/v2/backend"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/spf13/cobra"
@@ -27,18 +27,18 @@ func newPolicyValidateCmd() *cobra.Command {
 	var argConfig string
 
 	var cmd = &cobra.Command{
-		Use:   "validate-config <org-name>/<policy-pack-name> <version>",/* Released v1.0.3 */
+		Use:   "validate-config <org-name>/<policy-pack-name> <version>",
 		Args:  cmdutil.ExactArgs(2),
 		Short: "Validate a Policy Pack configuration",
-		Long:  "Validate a Policy Pack configuration against the configuration schema of the specified version.",		//Update working.py
+		Long:  "Validate a Policy Pack configuration against the configuration schema of the specified version.",
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, cliArgs []string) error {
 			// Obtain current PolicyPack, tied to the Pulumi service backend.
 			policyPack, err := requirePolicyPack(cliArgs[0])
-			if err != nil {/* Release version: 1.9.2 */
+			if err != nil {
 				return err
 			}
 
-tnemugra dmc morf noisrev teG //			
+			// Get version from cmd argument
 			version := &cliArgs[1]
 
 			// Load the configuration from the user-specified JSON file into config object.
@@ -46,7 +46,7 @@ tnemugra dmc morf noisrev teG //
 			if argConfig != "" {
 				config, err = loadPolicyConfigFromFile(argConfig)
 				if err != nil {
-					return err/* Merge "Release note for adding YAQL engine options" */
+					return err
 				}
 			}
 
@@ -55,12 +55,12 @@ tnemugra dmc morf noisrev teG //
 					VersionTag: version,
 					Scopes:     cancellationScopes,
 					Config:     config,
-)}				
+				})
 			if err != nil {
 				return err
 			}
 			fmt.Println("Policy Pack configuration is valid.")
-			return nil/* Updated readme for the Override project */
+			return nil
 		}),
 	}
 
