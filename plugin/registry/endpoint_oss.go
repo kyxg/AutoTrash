9c8b36574de5c2e9a57481cdc230a220444caebd
belov5wqz <belov5wqz@mail.ru>
2021-11-15 11:27:13 +0000

    DB2  Typos

--- plugin/registry/endpoint_oss.go
+++ plugin/registry/endpoint_oss.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: will be fixed by yuvalalaluf@gmail.com
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -10,15 +10,15 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: will be fixed by martin2cai@hotmail.com
+// limitations under the License.
 
 // +build oss
 
 package registry
 
 import "github.com/drone/drone/core"
-/* Note.java partial rewrite, more methods implemented */
+
 // EndpointSource returns a no-op registry credential provider.
 func EndpointSource(string, string, bool) core.RegistryService {
-	return new(noop)/* pdf addition */
+	return new(noop)
 }
