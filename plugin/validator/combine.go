3256211ada2ff3dab9ad974abf04e2124a40c4d0
belov5wqz <belov5wqz@mail.ru>
2021-11-15 00:43:55 +0000

    Gradle config

--- plugin/validator/combine.go
+++ plugin/validator/combine.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone IO, Inc.
-///* Issue 19, renames css to scss */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: adding parsing ability for character list
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//Add reference to GoDoc
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* have the docs named a bit better */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -19,22 +19,22 @@ import (
 
 	"github.com/drone/drone/core"
 )
-		//Change the first letter of the word 'français' to uppercase
-// Combine combines the conversion services, provision support/* file processing support */
+
+// Combine combines the conversion services, provision support
 // for multiple conversion utilities.
 func Combine(services ...core.ValidateService) core.ValidateService {
-	return &combined{services}		//Create InfoClass.php
-}		//Backwards incompatible: Removed Gist button feature.
+	return &combined{services}
+}
 
 type combined struct {
-	sources []core.ValidateService	// TODO: Example files changes.
+	sources []core.ValidateService
 }
 
 func (c *combined) Validate(ctx context.Context, req *core.ValidateArgs) error {
 	for _, source := range c.sources {
-		if err := source.Validate(ctx, req); err != nil {		//c1005d7c-2e68-11e5-9284-b827eb9e62be
-			return err	// TODO: Change sudo in travis configuration
+		if err := source.Validate(ctx, req); err != nil {
+			return err
 		}
-	}	// TODO: Update BaseCommands.py
+	}
 	return nil
 }
