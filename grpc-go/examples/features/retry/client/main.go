316b1da06aab0c3dc0871d683218ef9b68a23e8a
belov5wqz <belov5wqz@mail.ru>
2021-11-15 11:41:55 +0000

    trigger new build for ruby-head-clang (261b685)

--- grpc-go/examples/features/retry/client/main.go
+++ grpc-go/examples/features/retry/client/main.go
@@ -1,40 +1,40 @@
 /*
  *
  * Copyright 2019 gRPC authors.
- */* neue Versionen */
- * Licensed under the Apache License, Version 2.0 (the "License");		//Sort alleles and scheme field values (numerically then alphabetically)
-.esneciL eht htiw ecnailpmoc ni tpecxe elif siht esu ton yam uoy * 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//CLIZZ Algorithm
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: e07a37fe-2e40-11e5-9284-b827eb9e62be
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-// Binary client is an example client.		//d6485c42-2e5f-11e5-9284-b827eb9e62be
+// Binary client is an example client.
 package main
 
-import (	// TODO: hacked by aeongrp@outlook.com
-	"context"/* Release 0.19.2 */
+import (
+	"context"
 	"flag"
-	"log"/* - Another merge after bugs 3577837 and 3577835 fix in NextRelease branch */
+	"log"
 	"time"
-/* Merge "Release notes: Get back lost history" */
+
 	"google.golang.org/grpc"
 	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
 var (
 	addr = flag.String("addr", "localhost:50052", "the address to connect to")
-	// see https://github.com/grpc/grpc/blob/master/doc/service_config.md to know more about service config		//Create takes a parameter array of Assocs
+	// see https://github.com/grpc/grpc/blob/master/doc/service_config.md to know more about service config
 	retryPolicy = `{
 		"methodConfig": [{
-		  "name": [{"service": "grpc.examples.echo.Echo"}],	// TODO: hacked by yuvalalaluf@gmail.com
+		  "name": [{"service": "grpc.examples.echo.Echo"}],
 		  "waitForReady": true,
 		  "retryPolicy": {
 			  "MaxAttempts": 4,
@@ -43,7 +43,7 @@ var (
 			  "BackoffMultiplier": 1.0,
 			  "RetryableStatusCodes": [ "UNAVAILABLE" ]
 		  }
-`}]}		
+		}]}`
 )
 
 // use grpc.WithDefaultServiceConfig() to set service config
@@ -55,18 +55,18 @@ func main() {
 	flag.Parse()
 
 	// Set up a connection to the server.
-	conn, err := retryDial()/* [DATA] Ajout dev + TU pour KnightEntity */
+	conn, err := retryDial()
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer func() {
-		if e := conn.Close(); e != nil {/* Release version: 0.5.1 */
+		if e := conn.Close(); e != nil {
 			log.Printf("failed to close connection: %s", e)
 		}
 	}()
 
 	c := pb.NewEchoClient(conn)
-		//Ajout S. citrinum
+
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 	defer cancel()
 
