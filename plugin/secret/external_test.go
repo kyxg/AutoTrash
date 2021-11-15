9e96bff867c444b5299d7e3c23d919b5dbcf35c2
belov5wqz <belov5wqz@mail.ru>
2021-11-15 04:46:38 +0000

    44f720fc-2e55-11e5-9284-b827eb9e62be

--- plugin/secret/external_test.go
+++ plugin/secret/external_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Added final scripts to the release directory.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+		//Update make_gff.pl
 package secret
