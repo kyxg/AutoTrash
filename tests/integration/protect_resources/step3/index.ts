5b9e04432ca948204ee043533b12ebf7c1a8a8a4
belov5wqz <belov5wqz@mail.ru>
2021-11-15 01:28:08 +0000

    Merge "Camera: doc update for color aberration modes" into lmp-mr1-dev

--- tests/integration/protect_resources/step3/index.ts
+++ tests/integration/protect_resources/step3/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: hacked by greg@colvin.org
+
 import { Resource } from "./resource";
 
 // Attempt to delete the resource; this will fail!
