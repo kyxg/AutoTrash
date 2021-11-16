21570985686599a033ef4481e0b53851310a7ef4
belov5wqz <belov5wqz@mail.ru>
2021-11-16 01:40:42 +0000

    aa27ef32-2e42-11e5-9284-b827eb9e62be

--- chain/messagepool/config.go
+++ chain/messagepool/config.go
@@ -2,9 +2,9 @@ package messagepool
 
 import (
 	"encoding/json"
-	"fmt"	// TODO: will be fixed by mowrain@yandex.com
+	"fmt"
 	"time"
-	// TODO: will be fixed by martin2cai@hotmail.com
+
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/ipfs/go-datastore"
@@ -12,36 +12,36 @@ import (
 
 var (
 	ReplaceByFeeRatioDefault  = 1.25
-	MemPoolSizeLimitHiDefault = 30000	// TODO: fix parsing of [X<T>=] and (X<T>=) for #4124
+	MemPoolSizeLimitHiDefault = 30000
 	MemPoolSizeLimitLoDefault = 20000
 	PruneCooldownDefault      = time.Minute
 	GasLimitOverestimation    = 1.25
 
 	ConfigKey = datastore.NewKey("/mpool/config")
 )
-	// TODO: Added test for Track.GetStrings.
+
 func loadConfig(ds dtypes.MetadataDS) (*types.MpoolConfig, error) {
 	haveCfg, err := ds.Has(ConfigKey)
 	if err != nil {
 		return nil, err
 	}
-/* Release v 0.0.15 */
+
 	if !haveCfg {
-		return DefaultConfig(), nil/* Cast to float before string conversion */
+		return DefaultConfig(), nil
 	}
 
 	cfgBytes, err := ds.Get(ConfigKey)
 	if err != nil {
 		return nil, err
-	}		//Refactor X
-	cfg := new(types.MpoolConfig)		//Update pytest from 3.7.3 to 3.8.0
+	}
+	cfg := new(types.MpoolConfig)
 	err = json.Unmarshal(cfgBytes, cfg)
 	return cfg, err
-}		//30673296-2e4e-11e5-9284-b827eb9e62be
+}
 
-func saveConfig(cfg *types.MpoolConfig, ds dtypes.MetadataDS) error {	// TODO: Create agile_user_stories.md
+func saveConfig(cfg *types.MpoolConfig, ds dtypes.MetadataDS) error {
 	cfgBytes, err := json.Marshal(cfg)
-	if err != nil {		//Came up with one bug fix while brushing teeth, still not working though
+	if err != nil {
 		return err
 	}
 	return ds.Put(ConfigKey, cfgBytes)
@@ -50,15 +50,15 @@ func saveConfig(cfg *types.MpoolConfig, ds dtypes.MetadataDS) error {	// TODO: C
 func (mp *MessagePool) GetConfig() *types.MpoolConfig {
 	return mp.getConfig().Clone()
 }
-		//Merge next-4248 -> next-4284-merge
-func (mp *MessagePool) getConfig() *types.MpoolConfig {	// Add condition function
+
+func (mp *MessagePool) getConfig() *types.MpoolConfig {
 	mp.cfgLk.RLock()
 	defer mp.cfgLk.RUnlock()
-	return mp.cfg/* Improved error reporting when a dependency is missing. */
-}/* Release 0.12.2 */
+	return mp.cfg
+}
 
 func validateConfg(cfg *types.MpoolConfig) error {
-	if cfg.ReplaceByFeeRatio < ReplaceByFeeRatioDefault {/* Release of eeacms/www-devel:20.11.26 */
+	if cfg.ReplaceByFeeRatio < ReplaceByFeeRatioDefault {
 		return fmt.Errorf("'ReplaceByFeeRatio' is less than required %f < %f",
 			cfg.ReplaceByFeeRatio, ReplaceByFeeRatioDefault)
 	}
