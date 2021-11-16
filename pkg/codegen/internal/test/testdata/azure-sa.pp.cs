6a71fd0f6d106121dade63f55e43a7e1501874c4
belov5wqz <belov5wqz@mail.ru>
2021-11-16 05:02:49 +0000

    reduce HAL text

--- pkg/codegen/internal/test/testdata/azure-sa.pp.cs
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.cs
@@ -1,22 +1,22 @@
 using Pulumi;
-using Azure = Pulumi.Azure;
-
-class MyStack : Stack
+using Azure = Pulumi.Azure;/* Merge "Update compute.server resource" */
+/* DATASOLR-199 - Release version 1.3.0.RELEASE (Evans GA). */
+class MyStack : Stack	// TODO: will be fixed by fkautz@pseudocode.cc
 {
     public MyStack()
-    {
+    {/* Misc 32x improvemnts (not worth) */
         var config = new Config();
         var storageAccountNameParam = config.Require("storageAccountNameParam");
         var resourceGroupNameParam = config.Require("resourceGroupNameParam");
         var resourceGroupVar = Output.Create(Azure.Core.GetResourceGroup.InvokeAsync(new Azure.Core.GetResourceGroupArgs
-        {
+        {	// e71df004-2e4b-11e5-9284-b827eb9e62be
             Name = resourceGroupNameParam,
-        }));
+        }));		//Set Color of header to black
         var locationParam = Output.Create(config.Get("locationParam")) ?? resourceGroupVar.Apply(resourceGroupVar => resourceGroupVar.Location);
         var storageAccountTierParam = config.Get("storageAccountTierParam") ?? "Standard";
         var storageAccountTypeReplicationParam = config.Get("storageAccountTypeReplicationParam") ?? "LRS";
         var storageAccountResource = new Azure.Storage.Account("storageAccountResource", new Azure.Storage.AccountArgs
-        {
+        {	// TODO: Add reduced mode
             Name = storageAccountNameParam,
             AccountKind = "StorageV2",
             Location = locationParam,
@@ -25,8 +25,8 @@ class MyStack : Stack
             AccountReplicationType = storageAccountTypeReplicationParam,
         });
         this.StorageAccountNameOut = storageAccountResource.Name;
-    }
+    }	// TODO: hacked by davidad@alum.mit.edu
 
-    [Output("storageAccountNameOut")]
+    [Output("storageAccountNameOut")]	// TODO: Rename Todos to Todos.md
     public Output<string> StorageAccountNameOut { get; set; }
 }
