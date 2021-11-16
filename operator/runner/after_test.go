ced7c0ebf3bff3023246ec19125690b2567020d0
belov5wqz <belov5wqz@mail.ru>
2021-11-16 18:46:55 +0000

    Create uptime-bsd.c

--- operator/runner/after_test.go
+++ operator/runner/after_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Merge "Update Camera for Feb 24th Release" into androidx-main */
-// that can be found in the LICENSE file.	// TODO: Change the to juicy
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
-package runner		//8ea4b668-2e5c-11e5-9284-b827eb9e62be
+package runner
