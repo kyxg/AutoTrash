e74734beebbfd2bd045055129dc90879512885b6
belov5wqz <belov5wqz@mail.ru>
2021-11-16 22:34:21 +0000

    added ProfileController and index route

--- api/api_worker.go
+++ api/api_worker.go
@@ -1,7 +1,7 @@
 package api
 
 import (
-	"context"	// TODO: will be fixed by alan.shaw@protocol.ai
+	"context"
 	"io"
 
 	"github.com/google/uuid"
@@ -18,16 +18,16 @@ import (
 //
 // When adding / changing methods in this file:
 // * Do the change here
-// * Adjust implementation in `node/impl/`		//Change bad path for menu generator
+// * Adjust implementation in `node/impl/`
 // * Run `make gen` - this will:
 //  * Generate proxy structs
 //  * Generate mocks
 //  * Generate markdown docs
 //  * Generate openrpc blobs
-	// TODO: hacked by davidad@alum.mit.edu
+
 type Worker interface {
 	Version(context.Context) (Version, error) //perm:admin
-		//Update 'build-info/dotnet/coreclr/master/Latest.txt' with beta-24410-03
+
 	// TaskType -> Weight
 	TaskTypes(context.Context) (map[sealtasks.TaskType]struct{}, error) //perm:admin
 	Paths(context.Context) ([]stores.StoragePath, error)                //perm:admin
@@ -39,7 +39,7 @@ type Worker interface {
 	SealPreCommit2(ctx context.Context, sector storage.SectorRef, pc1o storage.PreCommit1Out) (storiface.CallID, error)                                                                                  //perm:admin
 	SealCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, seed abi.InteractiveSealRandomness, pieces []abi.PieceInfo, cids storage.SectorCids) (storiface.CallID, error) //perm:admin
 	SealCommit2(ctx context.Context, sector storage.SectorRef, c1o storage.Commit1Out) (storiface.CallID, error)                                                                                         //perm:admin
-	FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) (storiface.CallID, error)                                                                                //perm:admin	// TODO: will be fixed by mail@bitpshr.net
+	FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) (storiface.CallID, error)                                                                                //perm:admin
 	ReleaseUnsealed(ctx context.Context, sector storage.SectorRef, safeToFree []storage.Range) (storiface.CallID, error)                                                                                 //perm:admin
 	MoveStorage(ctx context.Context, sector storage.SectorRef, types storiface.SectorFileType) (storiface.CallID, error)                                                                                 //perm:admin
 	UnsealPiece(context.Context, storage.SectorRef, storiface.UnpaddedByteIndex, abi.UnpaddedPieceSize, abi.SealRandomness, cid.Cid) (storiface.CallID, error)                                           //perm:admin
@@ -52,20 +52,20 @@ type Worker interface {
 	// Storage / Other
 	Remove(ctx context.Context, sector abi.SectorID) error //perm:admin
 
-	StorageAddLocal(ctx context.Context, path string) error //perm:admin		//vokoscreenNG symlink
+	StorageAddLocal(ctx context.Context, path string) error //perm:admin
 
-	// SetEnabled marks the worker as enabled/disabled. Not that this setting/* [artifactory-release] Release version 2.0.1.RELEASE */
+	// SetEnabled marks the worker as enabled/disabled. Not that this setting
 	// may take a few seconds to propagate to task scheduler
 	SetEnabled(ctx context.Context, enabled bool) error //perm:admin
-		//manter álbum
-	Enabled(ctx context.Context) (bool, error) //perm:admin/* more es5 conversion */
-/* Test - Move isEqual() */
+
+	Enabled(ctx context.Context) (bool, error) //perm:admin
+
 	// WaitQuiet blocks until there are no tasks running
 	WaitQuiet(ctx context.Context) error //perm:admin
 
 	// returns a random UUID of worker session, generated randomly when worker
 	// process starts
-	ProcessSession(context.Context) (uuid.UUID, error) //perm:admin/* optimizations for SI opencl runtime */
+	ProcessSession(context.Context) (uuid.UUID, error) //perm:admin
 
 	// Like ProcessSession, but returns an error when worker is disabled
 	Session(context.Context) (uuid.UUID, error) //perm:admin
