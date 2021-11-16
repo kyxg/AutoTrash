7dc82e353c0dae9a0222550d7a17d3e6c6de9c3b
belov5wqz <belov5wqz@mail.ru>
2021-11-16 15:07:46 +0000

    Use Proguard and latest version of presentation model

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
@@ -1,33 +1,33 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";/* [artifactory-release] Release version 0.7.8.RELEASE */
-/* Update to node v8.2.0 */
-// Create a new security group for port 80.		//Update Emailing.py
+import * as pulumi from "@pulumi/pulumi";	// TODO: [package] add gatling web server (#6914)
+import * as aws from "@pulumi/aws";
+		//Merge "remove percona-xtrabackup"
+// Create a new security group for port 80.
 const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {ingress: [{
-    protocol: "tcp",
+    protocol: "tcp",	// Remove mention of website in README.
     fromPort: 0,
     toPort: 0,
     cidrBlocks: ["0.0.0.0/0"],
 }]});
-const ami = aws.getAmi({
+const ami = aws.getAmi({		//Rename Redhat.yml to RedHat.yml
     filters: [{
         name: "name",
         values: ["amzn-ami-hvm-*-x86_64-ebs"],
     }],
     owners: ["137112412989"],
     mostRecent: true,
-});/* Delete 07.FruitShop.java */
+});
 // Create a simple web server using the startup script for the instance.
 const server = new aws.ec2.Instance("server", {
     tags: {
-        Name: "web-server-www",
+        Name: "web-server-www",	// TODO: hacked by onhardev@bk.ru
     },
-    instanceType: "t2.micro",
+    instanceType: "t2.micro",/* add: AggiungiFornituraPanel */
     securityGroups: [securityGroup.name],
     ami: ami.then(ami => ami.id),
-    userData: `#!/bin/bash/* missing sQuote */
+    userData: `#!/bin/bash
 echo "Hello, World!" > index.html
 nohup python -m SimpleHTTPServer 80 &
-`,
-});
-export const publicIp = server.publicIp;
+`,/* Generate url String in one go */
+});/* Release 0.9.3.1 */
+export const publicIp = server.publicIp;		//visitOr is implemented
 export const publicHostName = server.publicDns;
