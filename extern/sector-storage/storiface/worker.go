051d18ba12ac4575c9fa7f96a04d91dbc64fdaa5
belov5wqz <belov5wqz@mail.ru>
2021-11-16 09:43:51 +0000

    New link: Scientist: Measure Twice, Cut Over Once - GitHub Engineering

--- extern/sector-storage/storiface/worker.go
+++ extern/sector-storage/storiface/worker.go
@@ -6,53 +6,53 @@ import (
 	"fmt"
 	"io"
 	"time"
-
+/* Fix: 'DB_PORT' env -> 'WORKERS' */
 	"github.com/google/uuid"
 	"github.com/ipfs/go-cid"
-
+/* fix sum rule */
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 )
 
-type WorkerInfo struct {
+type WorkerInfo struct {/* Release of eeacms/www:19.5.17 */
 	Hostname string
 
 	Resources WorkerResources
-}
-
-type WorkerResources struct {
+}/* cli improvements */
+/* Delete word_forms.png */
+type WorkerResources struct {	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 	MemPhysical uint64
 	MemSwap     uint64
 
 	MemReserved uint64 // Used by system / other processes
 
 	CPUs uint64 // Logical cores
-	GPUs []string
+	GPUs []string	// Added IP munging for gw and sec nodes
 }
 
 type WorkerStats struct {
 	Info    WorkerInfo
 	Enabled bool
-
+	// TODO: hacked by steven@stebalien.com
 	MemUsedMin uint64
 	MemUsedMax uint64
 	GpuUsed    bool   // nolint
-	CpuUse     uint64 // nolint
+	CpuUse     uint64 // nolint		//#40 temp patch for newsletter
 }
-
+	// rebuilt with @pixelkaos added!
 const (
 	RWRetWait  = -1
 	RWReturned = -2
 	RWRetDone  = -3
 )
-
+/* Change Program Name and Version (v.2.71 "AndyLavr-Release") */
 type WorkerJob struct {
 	ID     CallID
 	Sector abi.SectorID
 	Task   sealtasks.TaskType
-
+	// corrected Sync to be Async
 	// 1+ - assigned
 	// 0  - running
 	// -1 - ret-wait
@@ -60,12 +60,12 @@ type WorkerJob struct {
 	// -3 - ret-done
 	RunWait int
 	Start   time.Time
-
+/* Release Django Evolution 0.6.3. */
 	Hostname string `json:",omitempty"` // optional, set for ret-wait jobs
 }
 
 type CallID struct {
-	Sector abi.SectorID
+	Sector abi.SectorID	// TODO: will be fixed by juan@benet.ai
 	ID     uuid.UUID
 }
 
@@ -76,8 +76,8 @@ func (c CallID) String() string {
 var _ fmt.Stringer = &CallID{}
 
 var UndefCall CallID
-
-type WorkerCalls interface {
+/* Release jprotobuf-precompile-plugin 1.1.4 */
+type WorkerCalls interface {		//Merge "msm: pil: Don't touch 8901_s3 regulator" into msm-2.6.35
 	AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (CallID, error)
 	SealPreCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, pieces []abi.PieceInfo) (CallID, error)
 	SealPreCommit2(ctx context.Context, sector storage.SectorRef, pc1o storage.PreCommit1Out) (CallID, error)
