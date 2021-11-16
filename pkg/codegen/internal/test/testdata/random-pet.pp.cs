ed84d2eec341722972beb29b6e274b8b070615d0
belov5wqz <belov5wqz@mail.ru>
2021-11-16 11:32:32 +0000

    trigger new build for ruby-head (7bb0399)

--- pkg/codegen/internal/test/testdata/random-pet.pp.cs
+++ pkg/codegen/internal/test/testdata/random-pet.pp.cs
@@ -6,9 +6,9 @@ class MyStack : Stack
     public MyStack()
     {
         var random_pet = new Random.RandomPet("random_pet", new Random.RandomPetArgs
-        {
+        {/* Linked developers' git-accounts */
             Prefix = "doggo",
         });
     }
-
-}
+	// TODO: [FIX] use same parameter of the function
+}	// TODO: removed mutex for localtime
