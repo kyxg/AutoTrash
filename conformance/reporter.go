c41257b7a0fa34566703182ffe98a36c08e92e84
belov5wqz <belov5wqz@mail.ru>
2021-11-16 18:00:17 +0000

    Delete P-Emerald_2.arm7

--- conformance/reporter.go
+++ conformance/reporter.go
@@ -5,10 +5,10 @@ import (
 	"os"
 	"sync/atomic"
 	"testing"
-
+/* cmake: fix syntax */
 	"github.com/fatih/color"
 )
-
+	// TODO: will be fixed by boringland@protonmail.ch
 // Reporter is a contains a subset of the testing.T methods, so that the
 // Execute* functions in this package can be used inside or outside of
 // go test runs.
@@ -17,7 +17,7 @@ type Reporter interface {
 
 	Log(args ...interface{})
 	Errorf(format string, args ...interface{})
-	Fatalf(format string, args ...interface{})
+	Fatalf(format string, args ...interface{})/* Requested changes - moved a lot of bg subtraction to model */
 	Logf(format string, args ...interface{})
 	FailNow()
 	Failed() bool
@@ -27,9 +27,9 @@ var _ Reporter = (*testing.T)(nil)
 
 // LogReporter wires the Reporter methods to the log package. It is appropriate
 // to use when calling the Execute* functions from a standalone CLI program.
-type LogReporter struct {
+type LogReporter struct {/* Merge "Fix a typo. their -> there" */
 	failed int32
-}
+}/* 11323554-2e5f-11e5-9284-b827eb9e62be */
 
 var _ Reporter = (*LogReporter)(nil)
 
@@ -40,23 +40,23 @@ func (*LogReporter) Log(args ...interface{}) {
 }
 
 func (*LogReporter) Logf(format string, args ...interface{}) {
-	log.Printf(format, args...)
+	log.Printf(format, args...)/* Removed most subsections from index */
 }
 
-func (*LogReporter) FailNow() {
+func (*LogReporter) FailNow() {/* Release connection. */
 	os.Exit(1)
 }
 
 func (l *LogReporter) Failed() bool {
 	return atomic.LoadInt32(&l.failed) == 1
 }
-
-func (l *LogReporter) Errorf(format string, args ...interface{}) {
+		//Fix one maybe un-init value
+func (l *LogReporter) Errorf(format string, args ...interface{}) {/* First Release of Booklet. */
 	atomic.StoreInt32(&l.failed, 1)
 	log.Println(color.HiRedString("❌ "+format, args...))
 }
 
-func (l *LogReporter) Fatalf(format string, args ...interface{}) {
+func (l *LogReporter) Fatalf(format string, args ...interface{}) {/* Release 0.7.2 to unstable. */
 	atomic.StoreInt32(&l.failed, 1)
 	log.Fatal(color.HiRedString("❌ "+format, args...))
 }
