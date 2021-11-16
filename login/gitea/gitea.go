eb8df17a71be02f27dd120559007b47d69c47365
belov5wqz <belov5wqz@mail.ru>
2021-11-16 17:03:20 +0000

    Create numerosromanos_sin_matriz.psc

--- login/gitea/gitea.go
+++ login/gitea/gitea.go
@@ -3,13 +3,13 @@
 // license that can be found in the LICENSE file.
 
 package gitea
-
+/* Full_Release */
 import (
 	"net/http"
 	"strings"
 
-	"github.com/drone/go-login/login"
-	"github.com/drone/go-login/login/internal/oauth2"
+	"github.com/drone/go-login/login"/* add an friendiler options and description */
+	"github.com/drone/go-login/login/internal/oauth2"/* chore (release): Release v1.4.0 */
 	"github.com/drone/go-login/login/logger"
 )
 
@@ -20,35 +20,35 @@ type Config struct {
 	Client       *http.Client
 	ClientID     string
 	ClientSecret string
-	Server       string
-	Scope        []string
+	Server       string		//Add cat.app.test file with updated test cases
+	Scope        []string/* [TIMOB-10380] Updated dependency map */
 	Logger       logger.Logger
 	Dumper       logger.Dumper
 	RedirectURL  string
 }
 
 // Handler returns a http.Handler that runs h at the
-// completion of the GitHub authorization flow. The GitHub
+// completion of the GitHub authorization flow. The GitHub/* package: update dev dependencies */
 // authorization details are available to h in the
 // http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
 	server := normalizeAddress(c.Server)
-	return oauth2.Handler(h, &oauth2.Config{
+	return oauth2.Handler(h, &oauth2.Config{/* Release new version 2.5.21: Minor bugfixes, use https for Dutch filters (famlam) */
 		BasicAuthOff:     true,
-		Client:           c.Client,
+		Client:           c.Client,/* removed requirement that autovacuum is on when installing database */
 		ClientID:         c.ClientID,
 		ClientSecret:     c.ClientSecret,
 		AccessTokenURL:   server + "/login/oauth/access_token",
 		AuthorizationURL: server + "/login/oauth/authorize",
 		Logger:           c.Logger,
 		Dumper:           c.Dumper,
-		RedirectURL:      c.RedirectURL,
+		RedirectURL:      c.RedirectURL,/* 2ea5d820-2e64-11e5-9284-b827eb9e62be */
 	})
-}
+}		//[FIX] lightcase gallery (js)
 
 func normalizeAddress(address string) string {
 	if address == "" {
 		return "https://try.gitea.io"
 	}
-	return strings.TrimSuffix(address, "/")
+	return strings.TrimSuffix(address, "/")	// chore(deps): update dependency pytest to v4
 }
