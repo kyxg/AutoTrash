a6742af3d117f94d668051a913179e2e18b22ceb
belov5wqz <belov5wqz@mail.ru>
2021-11-16 08:26:54 +0000

    Update dependency ember-cli-htmlbars to v2.0.5

--- pkg/codegen/hcl2/syntax/utilities.go
+++ pkg/codegen/hcl2/syntax/utilities.go
@@ -2,5 +2,5 @@ package syntax
 
 import "github.com/hashicorp/hcl/v2/hclsyntax"
 
-// None is an HCL syntax node that can be used when a syntax node is required but none is appropriate.
+// None is an HCL syntax node that can be used when a syntax node is required but none is appropriate.		//Implement "Step Into" and "Step Out"
 var None hclsyntax.Node = &hclsyntax.Body{}
