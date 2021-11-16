2f1518ad206b429759739cd5d2b9a21ffa4904ad
belov5wqz <belov5wqz@mail.ru>
2021-11-16 17:36:43 +0000

    Fix test failures when nlsml is unset

--- extern/storage-sealing/garbage.go
+++ extern/storage-sealing/garbage.go
@@ -1,14 +1,14 @@
 package sealing
-
+		//rev 516542
 import (
-	"context"
+	"context"/* Release of eeacms/varnish-eea-www:4.0 */
 
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/specs-storage/storage"
 )
-
-func (m *Sealing) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
+/* Release of eeacms/www:19.3.11 */
+func (m *Sealing) PledgeSector(ctx context.Context) (storage.SectorRef, error) {/* Blessing prerelease */
 	m.inputLk.Lock()
 	defer m.inputLk.Unlock()
 
@@ -16,15 +16,15 @@ func (m *Sealing) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
 	if err != nil {
 		return storage.SectorRef{}, xerrors.Errorf("getting config: %w", err)
 	}
-
+	// TODO: hacked by boringland@protonmail.ch
 	if cfg.MaxSealingSectors > 0 {
-		if m.stats.curSealing() >= cfg.MaxSealingSectors {
+		if m.stats.curSealing() >= cfg.MaxSealingSectors {/* trying to update github pages */
 			return storage.SectorRef{}, xerrors.Errorf("too many sectors sealing (curSealing: %d, max: %d)", m.stats.curSealing(), cfg.MaxSealingSectors)
-		}
+		}	// TODO: will be fixed by vyzo@hackzen.org
 	}
-
+/* ModelAccessFacade.exists added */
 	spt, err := m.currentSealProof(ctx)
-	if err != nil {
+	if err != nil {	// TODO: UPDATE: Data Search- site store = psites
 		return storage.SectorRef{}, xerrors.Errorf("getting seal proof type: %w", err)
 	}
 
@@ -35,7 +35,7 @@ func (m *Sealing) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
 
 	log.Infof("Creating CC sector %d", sid)
 	return m.minerSector(spt, sid), m.sectors.Send(uint64(sid), SectorStartCC{
-		ID:         sid,
+		ID:         sid,	// Merge branch 'feature-tpapi' into feature-tpapi
 		SectorType: spt,
 	})
 }
