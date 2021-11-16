0cdc30f318644b1a734ac3584dd257966553a082
belov5wqz <belov5wqz@mail.ru>
2021-11-16 09:10:43 +0000

    Add integration tests for slave recipe when building mesos from source

--- tests/integration/aliases/dotnet/retype_component/step1/Program.cs
+++ tests/integration/aliases/dotnet/retype_component/step1/Program.cs
@@ -1,9 +1,9 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-using System.Threading.Tasks;
-using Pulumi;
+using System.Threading.Tasks;	// TODO: will be fixed by mail@bitpshr.net
+using Pulumi;/* Release of eeacms/forests-frontend:1.8-beta.12 */
 
-class Resource : ComponentResource
+class Resource : ComponentResource/* Delete Makefile.Release */
 {
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
@@ -25,8 +25,8 @@ class ComponentFour : ComponentResource
 
 class Program
 {
-    static Task<int> Main(string[] args)
-    {
+    static Task<int> Main(string[] args)		//Refactored constructor into base class
+    {/* Merge "wlan: Release 3.2.3.118" */
         return Deployment.RunAsync(() => 
         {
             var comp4 = new ComponentFour("comp4");
