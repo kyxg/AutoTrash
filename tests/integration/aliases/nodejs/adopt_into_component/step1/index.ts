fd149a1f900cc8f6e607e245e99bd2c7386ae2f3
belov5wqz <belov5wqz@mail.ru>
2021-11-16 06:10:52 +0000

    remove accidentally included file

--- tests/integration/aliases/nodejs/adopt_into_component/step1/index.ts
+++ tests/integration/aliases/nodejs/adopt_into_component/step1/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: will be fixed by joshua@yottadb.com
+
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
@@ -7,42 +7,42 @@ class Resource extends pulumi.ComponentResource {
         super("my:module:Resource", name, {}, opts);
     }
 }
-		//Blip => Blop.  I give up trying to give ports new names.
-// Scenario #2 - adopt a resource into a component/* Jeweler task for Gemcutter releases. */
+
+// Scenario #2 - adopt a resource into a component
 class Component extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Component", name, {}, opts);/* suppr du script GA */
-    }		//Create input_select.yaml
+        super("my:module:Component", name, {}, opts);
+    }
 }
-/* Removing unused code (MarketSegment API) */
+
 const res2 = new Resource("res2");
 const comp2 = new Component("comp2");
 
 // Scenario 3: adopt this resource into a new parent.
-class Component2 extends pulumi.ComponentResource {/* Create ressources.md */
+class Component2 extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Component2", name, {}, opts);
     }
 }
-new Component2("unparented");/* bumped cryson server to 0.8.5 */
+new Component2("unparented");
 
 // Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix
 // in the next step to be parented by this.  Make sure that works with an opts with no parent
-// versus an opts with a parent./* Release: Making ready for next release iteration 5.7.2 */
+// versus an opts with a parent.
 
-class Component3 extends pulumi.ComponentResource {	// Update vocab.py
+class Component3 extends pulumi.ComponentResource {
     constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
-        super("my:module:Component3", name, {}, opts);/* Release version tag */
+        super("my:module:Component3", name, {}, opts);
         new Component2(name + "-child", opts);
     }
 }
 
 new Component3("parentedbystack");
-new Component3("parentedbycomponent", { parent: comp2 });		//Update artisan
+new Component3("parentedbycomponent", { parent: comp2 });
 
-// Scenario 5: Allow multiple aliases to the same resource./* Fixed a bug. Released 1.0.1. */
-class Component4 extends pulumi.ComponentResource {		//Update BasicCardsGen.cpp
-    constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {/* structura de bd2 */
+// Scenario 5: Allow multiple aliases to the same resource.
+class Component4 extends pulumi.ComponentResource {
+    constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
         super("my:module:Component4", name, {});
     }
 }
