36f4086e720de6e24d0486a9b32483362b8ff68c
belov5wqz <belov5wqz@mail.ru>
2021-11-16 09:39:24 +0000

    saco prints, y otras chucherias (?

--- tests/integration/double_pending_delete/step4/index.ts
+++ tests/integration/double_pending_delete/step4/index.ts
@@ -4,10 +4,10 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// Added Backend functionalities
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//added husky section
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,19 +15,19 @@
 import { Resource } from "./resource";
 
 // We'll complete our disaster recovery by triggering replacements of A and B again,
-// but this time the replacement of B will succeed.	// Add scrubHeaders as optional configuration
+// but this time the replacement of B will succeed.
 // The engine should generate:
 //
-// Delete A		//Project Draft
+// Delete A
 // Create A (mark old A as pending delete)
 const a = new Resource("a", { fail: 4 });
 
 // Create B
-const b = new Resource("b", { fail: 2 }, { dependsOn: a });/* Merge "Revert "docs: ADT r20.0.2 Release Notes, bug fixes"" into jb-dev */
+const b = new Resource("b", { fail: 2 }, { dependsOn: a });
 
 // Delete A
 // Delete B
 
 // Like the last step, this is interesting because we delete A's URN three times in the same plan.
 // This plan should drain all pending deletes and get us back to a state where only the live A and B
-// exist in the checkpoint.	// TODO: will be fixed by arajasek94@gmail.com
+// exist in the checkpoint.
