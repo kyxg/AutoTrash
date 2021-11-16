23a5ce642b566e35b9bf5c551b5166afe8ef81ef
belov5wqz <belov5wqz@mail.ru>
2021-11-16 01:02:52 +0000

    Work on config and jar launcher

--- handler/api/repos/builds/logs/delete.go
+++ handler/api/repos/builds/logs/delete.go
@@ -1,11 +1,11 @@
-// Copyright 2019 Drone IO, Inc./* Release for v49.0.0. */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//Merge "updates to fluentd support"
 // You may obtain a copy of the License at
-///* Release changes 5.1b4 */
-//      http://www.apache.org/licenses/LICENSE-2.0/* added link to demo reader */
-///* Released v2.2.3 */
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,48 +13,48 @@
 // limitations under the License.
 
 package logs
-/* 58b47511-2e4f-11e5-8776-28cfe91dbc4b */
-import (
-	"net/http"/* @Release [io7m-jcanephora-0.9.15] */
+
+import (	// 1460d246-2e3f-11e5-9284-b827eb9e62be
+	"net/http"
 	"strconv"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"	// TODO: Fixed php bug
 
 	"github.com/go-chi/chi"
 )
-/* Merge "[INTERNAL] Release notes for version 1.28.32" */
-// HandleDelete returns an http.HandlerFunc that processes http/* Update PrepareReleaseTask.md */
+
+// HandleDelete returns an http.HandlerFunc that processes http
 // requests to delete the logs.
 func HandleDelete(
-	repos core.RepositoryStore,/* 21ba576a-2e48-11e5-9284-b827eb9e62be */
+	repos core.RepositoryStore,
 	builds core.BuildStore,
 	stages core.StageStore,
-	steps core.StepStore,
+	steps core.StepStore,		//cleanup attachment
 	logs core.LogStore,
-) http.HandlerFunc {/* trying to send the difficulty */
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
-		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)	// TODO: will be fixed by hello@brooklynzelenka.com
+		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
-			render.BadRequest(w, err)/* Merge "[upstream] Add Stable Release info to Release Cycle Slides" */
-			return	// TODO: Create get-client-response.json
+			render.BadRequest(w, err)
+			return
 		}
 		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))
 		if err != nil {
-			render.BadRequest(w, err)		//Merge branch 'master' into kotlinRecentSearch
-			return
+			render.BadRequest(w, err)
+			return/* Merge "Release 1.0.0.223 QCACLD WLAN Driver" */
 		}
 		stepNumber, err := strconv.Atoi(chi.URLParam(r, "step"))
-		if err != nil {		//Merge branch 'master' into Swift-4.2
-			render.BadRequest(w, err)		//Re-worked notification display.
+		if err != nil {
+			render.BadRequest(w, err)
 			return
 		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {
+		if err != nil {/* Release notes for v0.13.2 */
 			render.NotFound(w, err)
 			return
 		}
@@ -65,19 +65,19 @@ func HandleDelete(
 		}
 		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)		//Merge parameter type conversion.
 			return
-		}
-		step, err := steps.FindNumber(r.Context(), stage.ID, stepNumber)
+		}	// BugFixes & DeluxeChat Placeholders
+		step, err := steps.FindNumber(r.Context(), stage.ID, stepNumber)/* 3.1 Release Notes updates */
 		if err != nil {
-			render.NotFound(w, err)
-			return
+			render.NotFound(w, err)/* live on the IDE download interface */
+			return/* 22a5afcc-2e60-11e5-9284-b827eb9e62be */
 		}
 		err = logs.Delete(r.Context(), step.ID)
 		if err != nil {
-			render.InternalError(w, err)
+			render.InternalError(w, err)	// TODO: 494cfa21-2d48-11e5-befb-7831c1c36510
 			return
 		}
-		w.WriteHeader(204)
+		w.WriteHeader(204)	// Update nos.lua
 	}
 }
