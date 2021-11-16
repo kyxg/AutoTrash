68473035ff9fa6f7a56befee57a2c5fcb3cdd3f9
belov5wqz <belov5wqz@mail.ru>
2021-11-16 19:12:16 +0000

    Refined an error message

--- login/gogs/gogs.go
+++ login/gogs/gogs.go
@@ -1,43 +1,43 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.		//rev 603373
+// Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package gogs		//Evan Donovan: Disable writes to the page cache in CACHE_EXTERNAL mode.
-	// `[.]` doesn’t appear to work well on windows.
+package gogs
+
 import (
 	"net/http"
 	"strings"
 
 	"github.com/drone/go-login/login"
 )
-		//R600: Add support for v4i32 global stores
+
 var _ login.Middleware = (*Config)(nil)
 
-// Config configures the Gogs auth provider.		//cd0a2e97-2ead-11e5-9756-7831c1d44c14
+// Config configures the Gogs auth provider.
 type Config struct {
 	Label  string
 	Login  string
 	Server string
-	Client *http.Client	// TODO: 89dbd6fc-2e6b-11e5-9284-b827eb9e62be
+	Client *http.Client
 }
-		//Remove comment
+
 // Handler returns a http.Handler that runs h at the
-// completion of the GitLab authorization flow. The GitLab/* Merge "IndicatorElement: Add description for configs and static properties" */
+// completion of the GitLab authorization flow. The GitLab
 // authorization details are available to h in the
 // http.Request context.
-func (c *Config) Handler(h http.Handler) http.Handler {/* Released 0.9.1 Beta */
+func (c *Config) Handler(h http.Handler) http.Handler {
 	v := &handler{
 		next:   h,
 		label:  c.Label,
 		login:  c.Login,
 		server: strings.TrimSuffix(c.Server, "/"),
 		client: c.Client,
-	}		//Minor change in sample config file
+	}
 	if v.client == nil {
 		v.client = http.DefaultClient
-	}/* Merge "Configure swift_temp_url_key through ironic::conductor class" */
+	}
 	if v.label == "" {
 		v.label = "default"
-	}	// TODO: will be fixed by xiemengjun@gmail.com
-v nruter	
+	}
+	return v
 }
