af8e2ac3328cf9bb428cdc0f6b45c74aeecfd4b8
belov5wqz <belov5wqz@mail.ru>
2021-11-16 01:21:09 +0000

    Create index.hyperesources

--- tests/integration/partial_values/nodejs/index.ts
+++ tests/integration/partial_values/nodejs/index.ts
@@ -1,30 +1,30 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* modaldialoginstance.dart edited online with Bitbucket */
-import * as assert from "assert";
-import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";	// TODO: hacked by alan.shaw@protocol.ai
 
+import * as assert from "assert";
+import * as pulumi from "@pulumi/pulumi";	// TODO: hacked by witek@enjin.io
+import { Resource } from "./resource";
+		//Validates presence of image1 in job
 const unknown = <any>pulumi.output(pulumi.runtime.isDryRun() ? { __pulumiUnknown: true } : "foo");
 
 let a = new Resource("res", {
     foo: "foo",
     bar: { value: "foo", unknown },
-    baz: [ "foo", unknown ],
+    baz: [ "foo", unknown ],/* Release preparation. */
 });
-/* Instructions for skipping license header check. */
+
 export let o = Promise.all([
-    (<any>a.foo).isKnown,
-    (<any>a.bar.value).isKnown,/* faster than set, slower than list */
+    (<any>a.foo).isKnown,/* Release Notes for v01-13 */
+    (<any>a.bar.value).isKnown,	// fix inverted calculation for original timezone -> utc
     (<any>a.bar.unknown).isKnown,
-    (<any>a.baz[0]).isKnown,/* Release of eeacms/www-devel:19.10.10 */
+    (<any>a.baz[0]).isKnown,/* Release: Making ready for next release iteration 5.5.2 */
     (<any>a.baz[1]).isKnown,
-]).then(([r1, r2, r3, r4, r5]) => {/* Adding Sherbert */
-    assert.equal(r1, true);/* obsoletes ICON geloescht */
-    assert.equal(r2, true);
-    assert.equal(r3, !pulumi.runtime.isDryRun());	// TODO: hacked by ligi@ligi.de
-    assert.equal(r4, true);
+]).then(([r1, r2, r3, r4, r5]) => {
+    assert.equal(r1, true);
+    assert.equal(r2, true);/* set leak detection output for maven tests */
+    assert.equal(r3, !pulumi.runtime.isDryRun());
+    assert.equal(r4, true);	// par naming & design
     assert.equal(r5, !pulumi.runtime.isDryRun());
 
     console.log("ok");
-    return "checked";		//added poi link, small corrections
-});		//Rename ngTouchend.js to src/ngTouchend.js
+    return "checked";
+});
