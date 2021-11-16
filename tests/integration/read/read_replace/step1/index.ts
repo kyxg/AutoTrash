53cb004b059ff99ac22853d339d97633afa688b9
belov5wqz <belov5wqz@mail.ru>
2021-11-16 16:14:52 +0000

    Update etudiant.php

--- tests/integration/read/read_replace/step1/index.ts
+++ tests/integration/read/read_replace/step1/index.ts
@@ -1,10 +1,10 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
+///* Remove help notes from the ReleaseNotes. */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Release of eeacms/bise-frontend:1.29.15 */
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0		//Maven deploy to repo fix
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,9 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";	// TODO: Island generation working better, still with noticeable lag spikes
 
-// Setup: Resource A is external, Resource B is not.
+// Setup: Resource A is external, Resource B is not.		//removing one >
 const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
-const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
-	// TODO: hacked by alan.shaw@protocol.ai
+const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});/* Released eshop-1.0.0.FINAL */
+
