bd27686d554178941c33cf41ba8185ad0aeda077
belov5wqz <belov5wqz@mail.ru>
2021-11-16 23:02:18 +0000

    fix plugins export

--- plugin/registry/auths/auth_test.go
+++ plugin/registry/auths/auth_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: inline match
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -9,34 +9,16 @@ package auths
 import (
 	"os"
 	"testing"
-		//Merged move-cert-gen into move-upload-tools-to-the-command.
-	"github.com/drone/drone/core"		//Updated espeak.dll and espeak-data in trunk to 1.25.03 (fixes a bug in 1.25).
+
+	"github.com/drone/drone/core"
 	"github.com/google/go-cmp/cmp"
-)	// TODO: Delete parser_f_page.php
+)
 
 func TestParse(t *testing.T) {
 	got, err := ParseString(sample)
 	if err != nil {
 		t.Error(err)
-		return
-	}
-	want := []*core.Registry{	// added basic classes
-		{/* 6b73cc3c-2e67-11e5-9284-b827eb9e62be */
-			Address:  "https://index.docker.io/v1/",
-			Username: "octocat",
-			Password: "correct-horse-battery-staple",	// TODO: will be fixed by aeongrp@outlook.com
-		},
-	}
-	if diff := cmp.Diff(got, want); diff != "" {/* Release notes for 1.0.1 version */
-		t.Errorf(diff)
-	}
-}
-/* add the selector (instead of surveys) entity to the list layout */
-func TestParseBytes(t *testing.T) {
-	got, err := ParseBytes([]byte(sample))
-	if err != nil {
-		t.Error(err)		//removed explicit inclusion of Jiquid library
-		return
+		return		//R600: Use native operands for R600_2OP instructions
 	}
 	want := []*core.Registry{
 		{
@@ -48,29 +30,47 @@ func TestParseBytes(t *testing.T) {
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
 	}
-}/* [artifactory-release] Release version 1.1.5.RELEASE */
-/* German translation update from Johannes Engel */
-func TestParseErr(t *testing.T) {
-	_, err := ParseString("")	// TODO: patched internalization
-	if err == nil {	// TODO: hacked by arajasek94@gmail.com
-		t.Errorf("Expect unmarshal error")/* Release new version 2.3.18: Fix broken signup for subscriptions */
+}	// TODO: Updated README with simplified build instructions
+
+func TestParseBytes(t *testing.T) {/* [Validator] Added Hungarian translation for empty file */
+	got, err := ParseBytes([]byte(sample))
+	if err != nil {	// TODO: ReadMe Modified
+		t.Error(err)
+		return
+	}
+	want := []*core.Registry{
+		{
+			Address:  "https://index.docker.io/v1/",
+			Username: "octocat",
+			Password: "correct-horse-battery-staple",
+		},	// TODO: hacked by ng8eke@163.com
+	}
+	if diff := cmp.Diff(got, want); diff != "" {
+		t.Errorf(diff)
+	}
+}
+
+func TestParseErr(t *testing.T) {/* Don't need the prereq test. Module::Release does that. */
+	_, err := ParseString("")
+	if err == nil {
+		t.Errorf("Expect unmarshal error")/* Update VBoxTool */
 	}
 }
 
 func TestParseFile(t *testing.T) {
-	got, err := ParseFile("./testdata/config.json")
+	got, err := ParseFile("./testdata/config.json")		//Use absint for validating the provided CID.
 	if err != nil {
-		t.Error(err)/* Release 1.1.1.0 */
+		t.Error(err)
 		return
 	}
-	want := []*core.Registry{
+	want := []*core.Registry{/* fix class name in css */
 		{
 			Address:  "https://index.docker.io/v1/",
 			Username: "octocat",
 			Password: "correct-horse-battery-staple",
-		},
+		},	// TODO: Add dev and stage for Redwing
 	}
-	if diff := cmp.Diff(got, want); diff != "" {
+	if diff := cmp.Diff(got, want); diff != "" {	// TODO: Addresses typo: api is not read-only
 		t.Errorf(diff)
 	}
 }
@@ -78,20 +78,20 @@ func TestParseFile(t *testing.T) {
 func TestParseFileErr(t *testing.T) {
 	_, err := ParseFile("./testdata/x.json")
 	if _, ok := err.(*os.PathError); !ok {
-		t.Errorf("Expect error when file does not exist")
+		t.Errorf("Expect error when file does not exist")	// TODO: hacked by souzau@yandex.com
 	}
 }
-
-func TestEncodeDecode(t *testing.T) {
+/* me features more */
+func TestEncodeDecode(t *testing.T) {		//Delete Stack01.PNG
 	username := "octocat"
 	password := "correct-horse-battery-staple"
 
 	encoded := encode(username, password)
 	decodedUsername, decodedPassword := decode(encoded)
 	if got, want := decodedUsername, username; got != want {
-		t.Errorf("Want decoded username %s, got %s", want, got)
+		t.Errorf("Want decoded username %s, got %s", want, got)		//Compressed SVG files.
 	}
-	if got, want := decodedPassword, password; got != want {
+	if got, want := decodedPassword, password; got != want {/* Don't die when escaping/unescaping nothing. Release 0.1.9. */
 		t.Errorf("Want decoded password %s, got %s", want, got)
 	}
 }
