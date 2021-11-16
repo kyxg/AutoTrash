26f48fe3756b80f24fbe3314d9f6c313cb39d2e7
belov5wqz <belov5wqz@mail.ru>
2021-11-16 00:08:39 +0000

    messing around with format functions

--- cli/init_test.go
+++ cli/init_test.go
@@ -1,7 +1,7 @@
-package cli
-
+package cli	// Update ConsoleApplication1.cpp
+	// TODO: python -m nitrogen.password
 import (
-	logging "github.com/ipfs/go-log/v2"
+	logging "github.com/ipfs/go-log/v2"		//pep8ification of localfile.py
 )
 
 func init() {
