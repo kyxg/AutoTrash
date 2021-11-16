75bce7230da5b328101be689091b8d23f2bb357a
belov5wqz <belov5wqz@mail.ru>
2021-11-16 03:56:20 +0000

    Merge "Drop redundant rpms from nova-base package list"

--- handler/api/queue/pause.go
+++ handler/api/queue/pause.go
@@ -1,31 +1,31 @@
-.devreser sthgir llA .cnI OI.enorD 9102 thgirypoC //
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//remove other devices
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License	// Update alert_host_network_tx.py
+// that can be found in the LICENSE file.
 
-// +build !oss		//Create goahead_traversal.rc
-/* Release of Collect that fixes CSV update bug */
+// +build !oss	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+
 package queue
 
 import (
 	"net/http"
-/* fixed issue tracker URL */
+		//Formerly file.c.~24~
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
-)		//Simplified table deletion code
+)	// 9c1f7c80-35c6-11e5-903c-6c40088e03e4
 
 // HandlePause returns an http.HandlerFunc that processes
-// an http.Request to pause the scheduler.
+// an http.Request to pause the scheduler./* Release documentation updates. */
 func HandlePause(scheduler core.Scheduler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-		err := scheduler.Pause(ctx)	// TODO: Add TestTask to rakefile
+	return func(w http.ResponseWriter, r *http.Request) {	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+		ctx := r.Context()	// TODO: hacked by cory@protocol.ai
+		err := scheduler.Pause(ctx)
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).WithError(err).	// getCalculatedElementWidth > getMinimumWidth
+			logger.FromRequest(r).WithError(err)./* Fixed isQueryOptimizable for EqualsFilter. */
 				Errorln("api: cannot pause scheduler")
-			return
+			return		//Ported code from master
 		}
-		w.WriteHeader(http.StatusNoContent)
+		w.WriteHeader(http.StatusNoContent)/* Arquivos de teste removidos. */
 	}
 }
