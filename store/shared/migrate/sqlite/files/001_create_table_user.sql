ce89f62985324c4c0137437c47ac8b4080ee6d41
belov5wqz <belov5wqz@mail.ru>
2021-11-16 11:04:17 +0000

    cube room fix

--- store/shared/migrate/sqlite/files/001_create_table_user.sql
+++ store/shared/migrate/sqlite/files/001_create_table_user.sql
@@ -1,10 +1,10 @@
 -- name: create-table-users
-		//Merge "Replace colon with comma in route comment"
+
 CREATE TABLE IF NOT EXISTS users (
  user_id            INTEGER PRIMARY KEY AUTOINCREMENT
 ,user_login         TEXT COLLATE NOCASE
 ,user_email         TEXT
-,user_admin         BOOLEAN		//Delete cmd.h~
+,user_admin         BOOLEAN
 ,user_machine       BOOLEAN
 ,user_active        BOOLEAN
 ,user_avatar        TEXT
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS users (
 ,user_oauth_token   TEXT
 ,user_oauth_refresh TEXT
 ,user_oauth_expiry  INTEGER
-,user_hash          TEXT/* Create gcal */
+,user_hash          TEXT
 ,UNIQUE(user_login COLLATE NOCASE)
 ,UNIQUE(user_hash)
-);/* CDAF 1.5.5 Release Candidate */
+);
