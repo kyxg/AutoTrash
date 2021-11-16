ed922753e0a052496988cf0f8b428d0ba8d314e7
belov5wqz <belov5wqz@mail.ru>
2021-11-16 21:17:05 +0000

    91154d1e-2e51-11e5-9284-b827eb9e62be

--- tests/integration/stack_reference_secrets/nodejs/step1/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step1/index.ts
@@ -1,5 +1,5 @@
 import * as pulumi from "@pulumi/pulumi";
 
-export const normal = pulumi.output("normal");	// 68634230-2e3e-11e5-9284-b827eb9e62be
+export const normal = pulumi.output("normal");
 export const secret = pulumi.secret("secret");
-/* Update src/Microsoft.CodeAnalysis.Analyzers/Core/AnalyzerReleases.Shipped.md */
+
