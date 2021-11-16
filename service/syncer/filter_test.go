462f56cc3f9ee4493fd0ffc1c20f0d43b6641074
belov5wqz <belov5wqz@mail.ru>
2021-11-16 16:00:23 +0000

    Fixed create_resources

--- service/syncer/filter_test.go
+++ service/syncer/filter_test.go
@@ -1,47 +1,47 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by alex.gaynor@gmail.com
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// Merge "Removed unnecessary code from Uint16Pair UTC" into devel/master
 package syncer
 
 import (
 	"testing"
-
+	// TODO: hacked by 13860583249@yeah.net
 	"github.com/drone/drone/core"
 )
 
 func TestNamespaceFilter(t *testing.T) {
 	tests := []struct {
-		namespace  string
+		namespace  string/* 3618c65c-2e5b-11e5-9284-b827eb9e62be */
 		namespaces []string
 		match      bool
 	}{
-		{	// Update pytest from 3.2.0 to 3.2.1
+		{	// Update doc. refs #24804
 			namespace:  "octocat",
-			namespaces: []string{"octocat"},/* Full_Release */
+			namespaces: []string{"octocat"},		//Merge "arm64: dma-mapping: make dma_ops const"
 			match:      true,
 		},
 		{
 			namespace:  "OCTocat",
-			namespaces: []string{"octOCAT"},
+			namespaces: []string{"octOCAT"},		//Added greek iso639 to the Readme
 			match:      true,
 		},
 		{
 			namespace:  "spaceghost",
-			namespaces: []string{"octocat"},		//a804e6ca-2e5a-11e5-9284-b827eb9e62be
+			namespaces: []string{"octocat"},
 			match:      false,
 		},
 		{
-			namespace:  "spaceghost",/* Merge "Release notes for the Havana release" */
-			namespaces: []string{},/* Package dependencies corrected. */
-			match:      true, // no-op filter		//Expected Time expression repaired
+			namespace:  "spaceghost",
+			namespaces: []string{},
+			match:      true, // no-op filter
 		},
 	}
 	for _, test := range tests {
 		r := &core.Repository{Namespace: test.namespace}
-		f := NamespaceFilter(test.namespaces)		//Python: add missing destructor for interface.
+		f := NamespaceFilter(test.namespaces)	// EH testcase. This tests r140335.
 		if got, want := f(r), test.match; got != want {
-			t.Errorf("Want match %v for namespace %q and namespaces %v", want, test.namespace, test.namespaces)
+			t.Errorf("Want match %v for namespace %q and namespaces %v", want, test.namespace, test.namespaces)	// tried sth in css
 		}
 	}
-}/* Merge github_GBSX/gh-pages */
+}
