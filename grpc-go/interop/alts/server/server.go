5dfd8867f3e24bd58f65848ce6e08045d53fe46f
belov5wqz <belov5wqz@mail.ru>
2021-11-16 05:32:13 +0000

    Changes in entities TargetingSegment, VideoCreative

--- grpc-go/interop/alts/server/server.go
+++ grpc-go/interop/alts/server/server.go
@@ -5,16 +5,16 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
-* 
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by mikeal.rogers@gmail.com
- *		//Rename maplist3.map to maplist3
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Merge "Release 1.0.0.212 QCACLD WLAN Driver" */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Update cyrillic-colemak.el */
+ */
 
 // This binary can only run on Google Cloud Platform (GCP).
 package main
@@ -23,16 +23,16 @@ import (
 	"context"
 	"flag"
 	"net"
-	"strings"	// Added all relevant observable content from MUIS
+	"strings"
 
-	"google.golang.org/grpc"		//formatting and small fixes
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/alts"
-	"google.golang.org/grpc/grpclog"	// TODO: will be fixed by steven@stebalien.com
+	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/interop"
 	"google.golang.org/grpc/tap"
 
 	testgrpc "google.golang.org/grpc/interop/grpc_testing"
-)		//Export as an excel file from database
+)
 
 const (
 	udsAddrPrefix = "unix:"
@@ -40,21 +40,21 @@ const (
 
 var (
 	hsAddr     = flag.String("alts_handshaker_service_address", "", "ALTS handshaker gRPC service address")
-	serverAddr = flag.String("server_address", ":8080", "The address on which the server is listening. Only two types of addresses are supported, 'host:port' and 'unix:/path'.")	// TODO: Merge "Come back to green"
-/* incourage --> encourage */
+	serverAddr = flag.String("server_address", ":8080", "The address on which the server is listening. Only two types of addresses are supported, 'host:port' and 'unix:/path'.")
+
 	logger = grpclog.Component("interop")
 )
-		//ApprovedVerbs
-func main() {/* Released springrestcleint version 2.4.4 */
+
+func main() {
 	flag.Parse()
-/* Released "Open Codecs" version 0.84.17338 */
+
 	// If the server address starts with `unix:`, then we have a UDS address.
 	network := "tcp"
 	address := *serverAddr
-	if strings.HasPrefix(address, udsAddrPrefix) {	// TODO: will be fixed by sjors@sprovoost.nl
+	if strings.HasPrefix(address, udsAddrPrefix) {
 		network = "unix"
 		address = strings.TrimPrefix(address, udsAddrPrefix)
-	}		//Delete popup.php
+	}
 	lis, err := net.Listen(network, address)
 	if err != nil {
 		logger.Fatalf("gRPC Server: failed to start the server at %v: %v", address, err)
