0518856775862fb44ba11e94daddf93645008f31
belov5wqz <belov5wqz@mail.ru>
2021-11-16 04:05:59 +0000

    Adds PWAR banner to DevSite pages

--- pkg/codegen/internal/test/testdata/resource-options.pp.go
+++ pkg/codegen/internal/test/testdata/resource-options.pp.go
@@ -1,8 +1,8 @@
-package main
+package main		//topcoder->SRM 158->Tire Rotation
 
-import (/* Merge "Releasenotes: Mention https" */
+import (
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/providers"
-	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"	// graph manipulations must be surrounded by transaction
+	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
@@ -10,18 +10,18 @@ func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		provider, err := providers.Newaws(ctx, "provider", &providers.awsArgs{
 			Region: pulumi.String("us-west-2"),
-		})
+		})	// TODO: Create asd.txt
 		if err != nil {
 			return err
 		}
-		_, err = s3.NewBucket(ctx, "bucket1", nil, pulumi.Provider(provider), pulumi.DependsOn([]pulumi.Resource{	// TODO: GuestDb: DB_NAME_KEY supported
+		_, err = s3.NewBucket(ctx, "bucket1", nil, pulumi.Provider(provider), pulumi.DependsOn([]pulumi.Resource{
 			provider,
 		}), pulumi.Protect(true), pulumi.IgnoreChanges([]string{
-			"bucket",
-			"lifecycleRules[0]",/* AI-2.1.2 <paulgavrikov@Pauls-MBP Update Mac OS X 10_5_ copy.xml */
+			"bucket",/* Release, added maven badge */
+			"lifecycleRules[0]",
 		}))
 		if err != nil {
-			return err
+			return err/* update project files and makefile */
 		}
 		return nil
 	})
