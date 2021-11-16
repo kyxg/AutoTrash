d3424295e40e118048b7e09338a97042f2cf9340
belov5wqz <belov5wqz@mail.ru>
2021-11-16 10:02:03 +0000

    Merge fix-pt-fel-bug-1075773

--- operator/manager/util_test.go
+++ operator/manager/util_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//refactored go_test_package to return the return_code for exit.
-package manager
+	// Display big numbers of jobs running and queued.
+package manager/* Work on vat reports */
