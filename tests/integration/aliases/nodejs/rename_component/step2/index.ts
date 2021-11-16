fd47a30ab991f1a31a3b7715e5200c8540199f2e
belov5wqz <belov5wqz@mail.ru>
2021-11-16 19:21:03 +0000

    adjust for new project

--- tests/integration/aliases/nodejs/rename_component/step2/index.ts
+++ tests/integration/aliases/nodejs/rename_component/step2/index.ts
@@ -1,27 +1,27 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Release areca-6.0.5 */
+
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);
+        super("my:module:Resource", name, {}, opts);		//Delete time_graph.md
     }
 }
 
-// Scenario #3 - rename a component (and all it's children)/* Fix Issue # 39. Only use URI regex once. */
-// No change to the component...
+// Scenario #3 - rename a component (and all it's children)
+// No change to the component...		//Manual merge of New to Master
 class ComponentThree extends pulumi.ComponentResource {
-    resource1: Resource;/* Merge "ASoC: wcd: enable impedance detection." */
+    resource1: Resource;
     resource2: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:ComponentThree", name, {}, opts);
+        super("my:module:ComponentThree", name, {}, opts);/* 2a3a75e6-2e70-11e5-9284-b827eb9e62be */
         // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
         // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
         this.resource1 = new Resource(`${name}-child`, { parent: this });
         this.resource2 = new Resource("otherchild", { parent: this });
     }
-}
-// ...but applying an alias to the instance successfully renames both the component and the children./* #6 [Documentation] Update the documentation to reflect the new enhancements. */
+}/* Update eml.R */
+// ...but applying an alias to the instance successfully renames both the component and the children./* v0.2.4 Release information */
 const comp3 = new ComponentThree("newcomp3", {
-    aliases: [{ name: "comp3" }],		//enable extensions on shortwikiwiki per req T2797
+    aliases: [{ name: "comp3" }],
 });
