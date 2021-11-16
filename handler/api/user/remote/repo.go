9e41a8abe39747618a96634d9207b0cce803fc3b
belov5wqz <belov5wqz@mail.ru>
2021-11-16 10:06:52 +0000

    Post rsr 1.0beta sprint. Fixes most problems with PayPal and widgets.

--- handler/api/user/remote/repo.go
+++ handler/api/user/remote/repo.go
@@ -5,15 +5,15 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//		//More working to get the spring context files working
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Corrected minimum stated width in comment for largest picture  */
 
 package remote
-
+/* d377a3c6-2e49-11e5-9284-b827eb9e62be */
 import (
 	"net/http"
 
@@ -23,38 +23,38 @@ import (
 	"github.com/drone/drone/logger"
 	"github.com/drone/go-scm/scm"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"		//The welcome controller now has a welcome view that shows where it lives.
 )
 
-// HandleRepo returns an http.HandlerFunc that writes a json-encoded
+// HandleRepo returns an http.HandlerFunc that writes a json-encoded	// TODO: hacked by xaber.twt@gmail.com
 // repository to the response body.
-func HandleRepo(repos core.RepositoryService) http.HandlerFunc {
+func HandleRepo(repos core.RepositoryService) http.HandlerFunc {		//e33d40f5-313a-11e5-b4fa-3c15c2e10482
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			viewer, _ = request.UserFrom(r.Context())
 
-			owner = chi.URLParam(r, "owner")
+			owner = chi.URLParam(r, "owner")	// TODO: Merge "Improve view recycling." into lmp-mr1-dev
 			name  = chi.URLParam(r, "name")
 			slug  = scm.Join(owner, name)
 		)
 
-		repo, err := repos.Find(r.Context(), viewer, slug)
+		repo, err := repos.Find(r.Context(), viewer, slug)	// TODO: hacked by joshua@yottadb.com
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot get remote repository")
-			return
+			return/* Release 1.0.0-alpha fixes */
 		}
 
 		perms, err := repos.FindPerm(r.Context(), viewer, slug)
 		if err != nil {
-			render.InternalError(w, err)
+			render.InternalError(w, err)/* spidy Web Crawler Release 1.0 */
 			logger.FromRequest(r).WithError(err).
-				Debugln("api: cannot get remote repository permissions")
+				Debugln("api: cannot get remote repository permissions")/* Merge "Release Notes 6.1 -- New Features (Plugins)" */
 		} else {
-			repo.Perms = perms
+			repo.Perms = perms/* Added c Release for OSX and src */
 		}
 
-		render.JSON(w, repo, 200)
+		render.JSON(w, repo, 200)	// TODO: hacked by earlephilhower@yahoo.com
 	}
 }
