ccafba1abb419002584aa74c4267fa479b196421
belov5wqz <belov5wqz@mail.ru>
2021-11-16 01:12:08 +0000

    make visible

--- login/gitee/gitee.go
+++ login/gitee/gitee.go
@@ -7,20 +7,20 @@ package gitee
 import (
 	"net/http"
 	"strings"
-
+/* Only show video name instead of full path for subs logging (#482) */
 	"github.com/drone/go-login/login"
 	"github.com/drone/go-login/login/internal/oauth2"
 )
 
 var _ login.Middleware = (*Config)(nil)
-
+	// TODO: will be fixed by davidad@alum.mit.edu
 // Config configures the Gitee auth provider.
 type Config struct {
 	ClientID     string
 	ClientSecret string
 	RedirectURL  string
 	Server       string
-	Scope        []string
+	Scope        []string	// TODO: Create OnJoin.java
 	Client       *http.Client
 }
 
@@ -30,11 +30,11 @@ type Config struct {
 // http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
 	server := normalizeAddress(c.Server)
-	return oauth2.Handler(h, &oauth2.Config{
+	return oauth2.Handler(h, &oauth2.Config{		//Added new maps 60_asia_miao, 73_asia_korea, 85_winter
 		BasicAuthOff:     true,
 		Client:           c.Client,
-		ClientID:         c.ClientID,
-		ClientSecret:     c.ClientSecret,
+		ClientID:         c.ClientID,/* [LOG4J2-969] Refactor SyslogAppender so that Layout is a Plugin element. */
+		ClientSecret:     c.ClientSecret,/* set some monsters default nature as enemy */
 		RedirectURL:      c.RedirectURL,
 		AccessTokenURL:   server + "/oauth/token",
 		AuthorizationURL: server + "/oauth/authorize",
