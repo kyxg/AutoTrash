5f4dd9ab5c8f6bed4624c52d04a13dd6332ca1dc
belov5wqz <belov5wqz@mail.ru>
2021-11-16 05:27:10 +0000

    update derby

--- operator/manager/setup_test.go
+++ operator/manager/setup_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Smaller post font because lines should have more characters */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
