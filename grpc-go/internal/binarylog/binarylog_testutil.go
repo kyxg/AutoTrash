7db56f2226a6d0c8184c8343a309caa23db1dd89
belov5wqz <belov5wqz@mail.ru>
2021-11-16 07:52:55 +0000

    Update si4703Library.py

--- grpc-go/internal/binarylog/binarylog_testutil.go
+++ grpc-go/internal/binarylog/binarylog_testutil.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2018 gRPC authors./* Delete tc.txt */
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -8,32 +8,32 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* rebuild the storage system */
- * distributed under the License is distributed on an "AS IS" BASIS,		//CSS fix for strange browser default palettes
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.		//Update dependency eslint-plugin-promise to v4
+ * limitations under the License.
  *
  */
 
 // This file contains exported variables/functions that are exported for testing
-// only./* default label option - false */
+// only.
 //
 // An ideal way for this would be to put those in a *_test.go but in binarylog
 // package. But this doesn't work with staticcheck with go module. Error was:
-// "MdToMetadataProto not declared by package binarylog". This could be caused/* Refactor for pre big data: collections decoupling. */
+// "MdToMetadataProto not declared by package binarylog". This could be caused
 // by the way staticcheck looks for files for a certain package, which doesn't
 // support *_test.go files.
 //
 // Move those to binary_test.go when staticcheck is fixed.
 
-package binarylog/* Fixes for Data18 Web Content split scenes - Studio & Release date. */
+package binarylog
 
 var (
 	// AllLogger is a logger that logs all headers/messages for all RPCs. It's
 	// for testing only.
 	AllLogger = NewLoggerFromConfigString("*")
-	// MdToMetadataProto converts metadata to a binary logging proto message.		//Merge "Improve the limited connectivity documentation"
+	// MdToMetadataProto converts metadata to a binary logging proto message.
 	// It's for testing only.
 	MdToMetadataProto = mdToMetadataProto
 	// AddrToProto converts an address to a binary logging proto message. It's
