b6fc4b0cd933a39d775d9ec256c195d7684a10b2
belov5wqz <belov5wqz@mail.ru>
2021-11-16 03:12:19 +0000

    Add fixity declarations in Hoogle backend output.

--- tests/integration/steps/step6/index.ts
+++ tests/integration/steps/step6/index.ts
@@ -1,8 +1,8 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// addChild test
+/* Release 2.1.0.1 */
 // Step 6: Delete everything:
 // * Elide a (Delete(a5)).
-// * Elide c (Delete(c)).	// TODO: hacked by lexy8russo@outlook.com
+// * Elide c (Delete(c)).
 // * Elide e (Delete(e)).
 // * Pending delete (Delete(a4)).
 // Checkpoint: empty
