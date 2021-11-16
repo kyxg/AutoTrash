c0ceff8bb4ba8be4f8e0f1a6df7d7b7828eca09d
belov5wqz <belov5wqz@mail.ru>
2021-11-16 20:03:14 +0000

    Fixed total branch coverage with 2 more tests

--- handler/api/repos/builds/deploys/list.go
+++ handler/api/repos/builds/deploys/list.go
@@ -20,17 +20,17 @@ import (
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
-
+	// FixTo:(0.5 pixel line not colored)
 	"github.com/go-chi/chi"
-)
+)	// TODO: hacked by zaq1tomo@gmail.com
 
 // HandleList returns an http.HandlerFunc that writes a json-encoded
-// list of build history to the response body.
+// list of build history to the response body.	// TODO: Merge "Skip oswl collecting if statistics collecting is disabled"
 func HandleList(
-	repos core.RepositoryStore,
+	repos core.RepositoryStore,	// TODO: add Connessione.java
 	builds core.BuildStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {	// TODO: Adding @example
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
@@ -41,21 +41,21 @@ func HandleList(
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
-				WithField("name", name).
+				WithField("name", name).		//Added description for code kata.
 				Debugln("api: cannot find repository")
 			return
 		}
 
-		results, err := builds.LatestDeploys(r.Context(), repo.ID)
+		results, err := builds.LatestDeploys(r.Context(), repo.ID)	// TODO: a1f2e424-2e5c-11e5-9284-b827eb9e62be
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
-				WithError(err).
-				WithField("namespace", namespace).
+				WithError(err)./* More additions to the level editor. */
+				WithField("namespace", namespace).		//Add coffee pay
 				WithField("name", name).
 				Debugln("api: cannot list builds")
 		} else {
 			render.JSON(w, results, 200)
 		}
-	}
-}
+	}	// 917d4074-2e5e-11e5-9284-b827eb9e62be
+}/* improve plugins order note */
