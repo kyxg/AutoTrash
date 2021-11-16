edab32025068b4bfc4ea9eea26bfc44f24b130dc
belov5wqz <belov5wqz@mail.ru>
2021-11-16 03:51:42 +0000

    Updated repositories key.

--- tests/integration/stack_reference_secrets/dotnet/step1/Program.cs
+++ tests/integration/stack_reference_secrets/dotnet/step1/Program.cs
@@ -1,20 +1,20 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.		//add sound files, game.py
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Pulumi;
-/* [artifactory-release] Release version 2.3.0.RC1 */
-class Program		//FutureClass
+
+class Program
 {
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() =>
         {
-            return new Dictionary<string, object>/* Tag the ReactOS 0.3.5 Release */
+            return new Dictionary<string, object>
             {
                 { "normal", Output.Create("normal") },
                 { "secret", Output.CreateSecret("secret") },
-            };/* Create chart3.html */
+            };
         });
     }
 }
