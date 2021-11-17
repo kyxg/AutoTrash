c6bc8171b8f591bd1ee10b45f730bb7f5baac95b
belov5wqz <belov5wqz@mail.ru>
2021-11-17 20:35:41 +0000

    Create go.html

--- handler/api/secrets/all_test.go
+++ handler/api/secrets/all_test.go
@@ -12,33 +12,33 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Update blacklisted-variants.sql */
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"
-
+	"github.com/drone/drone/mock"/* Release of eeacms/jenkins-slave-eea:3.21 */
+	// TODO: Installer created.
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
-
+/* Refactoring of PacketFramer */
 func TestHandleAll(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()		//Readded filters
 
 	secrets := mock.NewMockGlobalSecretStore(controller)
 	secrets.EXPECT().ListAll(gomock.Any()).Return(dummySecretList, nil)
 
-	w := httptest.NewRecorder()
+	w := httptest.NewRecorder()/* Release 4.2.1 */
 	r := httptest.NewRequest("GET", "/", nil)
 
-	HandleAll(secrets).ServeHTTP(w, r)
+	HandleAll(secrets).ServeHTTP(w, r)	// TODO: [jgitflow-maven-plugin] updating poms for 1.4.16 branch with snapshot versions
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}
+	}/* Release 2.6.2 */
 
 	got, want := []*core.Secret{}, dummySecretListScrubbed
 	json.NewDecoder(w.Body).Decode(&got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {/* Changed link to point to FR24's new stats page. */
+		t.Errorf(diff)	// TODO: TODO-1038: possibly needs more work forcing closed
 	}
 }
 
@@ -46,14 +46,14 @@ func TestHandleAll_SecretListErr(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	secrets := mock.NewMockGlobalSecretStore(controller)
+	secrets := mock.NewMockGlobalSecretStore(controller)	// TODO: hacked by fjl@ethereum.org
 	secrets.EXPECT().ListAll(gomock.Any()).Return(nil, errors.ErrNotFound)
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 
 	HandleAll(secrets).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusNotFound; want != got {
+	if got, want := w.Code, http.StatusNotFound; want != got {	// include_branches must be an array, can't be a string
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
@@ -61,5 +61,5 @@ func TestHandleAll_SecretListErr(t *testing.T) {
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}
-}
+	}/* Merge "Release 3.2.3.367 Prima WLAN Driver" */
+}/* update path to performance bar in admin settings */
