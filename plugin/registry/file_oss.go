1c5ebaa6ef5e2652683ba5ae67fb9f280bf7885c
belov5wqz <belov5wqz@mail.ru>
2021-11-17 08:53:18 +0000

    fd1622de-2e4e-11e5-840a-28cfe91dbc4b

--- plugin/registry/file_oss.go
+++ plugin/registry/file_oss.go
@@ -2,23 +2,23 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Add to DBUS API: IsPlaying and GetCurrentSong
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// style: cleanup code
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss	// TODO: Prepare samples module
+// +build oss
 
 package registry
 
 import "github.com/drone/drone/core"
 
 // FileSource returns a no-op registry credential provider.
-func FileSource(string) core.RegistryService {/* added dependency  */
+func FileSource(string) core.RegistryService {
 	return new(noop)
 }
