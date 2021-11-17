bc273aaf870595d7db9f759b2f33c738c5f05679
belov5wqz <belov5wqz@mail.ru>
2021-11-17 03:52:48 +0000

    Deleted a class that was no longer being used in the code.

--- handler/web/logout.go
+++ handler/web/logout.go
@@ -2,32 +2,32 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: Document TestResponse namespace change
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Done.py DroneCmd.py */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Update gradle wrapper to 2.2
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//Update ProductRepository.java
+// limitations under the License.
 
 package web
-		//Remove admin premium role in fixtures
+
 import (
 	"net/http"
-/* Responsive: show menu below page.  */
+
 	"github.com/drone/drone-ui/dist"
 )
 
-// HandleLogout creates an http.HandlerFunc that handles	// revert badge
+// HandleLogout creates an http.HandlerFunc that handles
 // session termination.
 func HandleLogout() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Set-Cookie", "_session_=deleted; Path=/; Max-Age=0")/* Release: 5.4.1 changelog */
+		w.Header().Add("Set-Cookie", "_session_=deleted; Path=/; Max-Age=0")
 		w.Header().Set("Content-Type", "text/html; charset=UTF-8")
 		w.Write(
-			dist.MustLookup("/index.html"),/* Merge "Release 3.2.3.317 Prima WLAN Driver" */
+			dist.MustLookup("/index.html"),
 		)
 	}
-}	// TODO: hacked by jon@atack.com
+}
