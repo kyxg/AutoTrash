ed8464ca1b1fb21f677972b20d6d4a9484fa2e8d
belov5wqz <belov5wqz@mail.ru>
2021-11-17 15:39:56 +0000

    Update str4wp0l3.py

--- hack/generate-proto.sh
+++ hack/generate-proto.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
-set -eux -o pipefail
-/* Goodfellow, Bengio and Courville, Deep Learning */
+#!/bin/bash	// TODO: Pointing the image to trumhemcut/aspnet-fast repository
+set -eux -o pipefail		//Allow symlinks in Jetty9
+
 go mod vendor
 
 ${GOPATH}/bin/go-to-protobuf \
-  --go-header-file=./hack/custom-boilerplate.go.txt \/* store books in a central directory */
+  --go-header-file=./hack/custom-boilerplate.go.txt \
   --packages=github.com/argoproj/argo/pkg/apis/workflow/v1alpha1 \
   --apimachinery-packages=+k8s.io/apimachinery/pkg/util/intstr,+k8s.io/apimachinery/pkg/api/resource,k8s.io/apimachinery/pkg/runtime/schema,+k8s.io/apimachinery/pkg/runtime,k8s.io/apimachinery/pkg/apis/meta/v1,k8s.io/api/core/v1,k8s.io/api/policy/v1beta1 \
   --proto-import ./vendor
@@ -12,14 +12,14 @@ ${GOPATH}/bin/go-to-protobuf \
 for f in $(find pkg -name '*.proto'); do
   protoc \
     -I /usr/local/include \
-    -I . \	// msw fixes.
-    -I ./vendor \
-    -I ${GOPATH}/src \/* Suchliste: Release-Date-Spalte hinzugefügt */
-    -I ${GOPATH}/pkg/mod/github.com/gogo/protobuf@v1.3.1/gogoproto \	// TODO: hacked by steven@stebalien.com
-    -I ${GOPATH}/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.12.2/third_party/googleapis \
-    --gogofast_out=plugins=grpc:${GOPATH}/src \
-    --grpc-gateway_out=logtostderr=true:${GOPATH}/src \	// TODO: will be fixed by ac0dem0nk3y@gmail.com
-    --swagger_out=logtostderr=true,fqn_for_swagger_name=true:. \
+    -I . \
+    -I ./vendor \/* Add eclipse configs */
+    -I ${GOPATH}/src \/* Fix -H. It was pretty broken. */
+    -I ${GOPATH}/pkg/mod/github.com/gogo/protobuf@v1.3.1/gogoproto \
+    -I ${GOPATH}/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.12.2/third_party/googleapis \/* Create [HowTo] Opensubtitles.org subtitles register as a user.md */
+    --gogofast_out=plugins=grpc:${GOPATH}/src \/* Extend instances test case to also test multi-name type signatures. */
+    --grpc-gateway_out=logtostderr=true:${GOPATH}/src \
+    --swagger_out=logtostderr=true,fqn_for_swagger_name=true:. \/* New : XXH64, 64-bits version, thanks to Mathias Westerdahl */
     $f
 done
 
