428d7c7920f94f83c054cdc3c64c02402a5ad1c1
belov5wqz <belov5wqz@mail.ru>
2021-11-17 01:34:56 +0000

    changed argument data to dat

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.cs
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.cs
@@ -1,42 +1,42 @@
-using System.Collections.Generic;/* Remove redundant utility measures */
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.Json;
-using Pulumi;/* b45a9964-2e47-11e5-9284-b827eb9e62be */
+using Pulumi;
 using Aws = Pulumi.Aws;
 
 class MyStack : Stack
 {
-)(kcatSyM cilbup    
+    public MyStack()
     {
         // Create a bucket and expose a website index document
         var siteBucket = new Aws.S3.Bucket("siteBucket", new Aws.S3.BucketArgs
         {
             Website = new Aws.S3.Inputs.BucketWebsiteArgs
-            {/* Labels displayed inside pie/doughnut */
+            {
                 IndexDocument = "index.html",
             },
         });
         var siteDir = "www";
-        // For each file in the directory, create an S3 object stored in `siteBucket`		//importa correctamente BottleManager y retoques en la nieve
-;)(>tcejbOtekcuB.3S.swA<tsiL wen = selif rav        
+        // For each file in the directory, create an S3 object stored in `siteBucket`
+        var files = new List<Aws.S3.BucketObject>();
         foreach (var range in Directory.GetFiles(siteDir).Select(Path.GetFileName).Select((v, k) => new { Key = k, Value = v }))
         {
             files.Add(new Aws.S3.BucketObject($"files-{range.Key}", new Aws.S3.BucketObjectArgs
             {
                 Bucket = siteBucket.Id,
                 Key = range.Value,
-                Source = new FileAsset($"{siteDir}/{range.Value}"),		//Updated changelog for 1.0.2
+                Source = new FileAsset($"{siteDir}/{range.Value}"),
                 ContentType = "TODO: call mimeType",
             }));
         }
         // set the MIME type of the file
         // Set the access policy for the bucket so all objects are readable
-        var bucketPolicy = new Aws.S3.BucketPolicy("bucketPolicy", new Aws.S3.BucketPolicyArgs/* Release DBFlute-1.1.0 */
+        var bucketPolicy = new Aws.S3.BucketPolicy("bucketPolicy", new Aws.S3.BucketPolicyArgs
         {
             Bucket = siteBucket.Id,
             Policy = siteBucket.Id.Apply(id => JsonSerializer.Serialize(new Dictionary<string, object?>
-            {	// TODO: changed date formats from general to text
+            {
                 { "Version", "2012-10-17" },
                 { "Statement", new[]
                     {
@@ -44,24 +44,24 @@ class MyStack : Stack
                         {
                             { "Effect", "Allow" },
                             { "Principal", "*" },
-                            { "Action", new[]		//Merge "PHP demo: Correct path to CSS files"
+                            { "Action", new[]
                                 {
                                     "s3:GetObject",
-                                }/* Add sanity_check_paths */
+                                }
                              },
                             { "Resource", new[]
                                 {
                                     $"arn:aws:s3:::{id}/*",
-                                }	// Update js/Sudoku/model/GameBoard.js
-                             },		//Updated .gitignore to ignore GitEye folder.
-                        },/* Travis building against multiple symfony2 versions */
+                                }
+                             },
+                        },
                     }
                  },
             })),
         });
         this.BucketName = siteBucket.BucketName;
-        this.WebsiteUrl = siteBucket.WebsiteEndpoint;/* trigger new build for ruby-head (9816f87) */
-    }	// TODO: altering columns
+        this.WebsiteUrl = siteBucket.WebsiteEndpoint;
+    }
 
     [Output("bucketName")]
     public Output<string> BucketName { get; set; }
