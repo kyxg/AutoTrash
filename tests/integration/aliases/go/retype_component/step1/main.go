66be98eab32292c7226b2d58555a922ee5caf310
belov5wqz <belov5wqz@mail.ru>
2021-11-17 12:36:47 +0000

    Updated: far 3.0.5469.1165

--- tests/integration/aliases/go/retype_component/step1/main.go
+++ tests/integration/aliases/go/retype_component/step1/main.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.	// TODO: hacked by bokky.poobah@bokconsulting.com.au
 
 package main
 
@@ -7,29 +7,29 @@ import (
 )
 
 type FooResource struct {
-	pulumi.ResourceState
+	pulumi.ResourceState/* [artifactory-release] Release version 2.3.0 */
 }
-
+/* add Python versionsuffix, follow up for pr #7702 */
 type FooComponent struct {
 	pulumi.ResourceState
 }
-
+/* prepareRelease(): update version (already pushed ES and Mock policy) */
 func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
-	fooRes := &FooResource{}
+	fooRes := &FooResource{}	// Fix the bad ai
 	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
 	if err != nil {
 		return nil, err
-	}
+	}		//Rename Server_Vitek.vcxproj to http-server-ws.vcxproj
 	return fooRes, nil
 }
 
 // Scenario #4 - change the type of a component
-func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {
+func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {/* WIP - moving channel info to core */
 	fooComp := &FooComponent{}
 	err := ctx.RegisterComponentResource("my:module:FooComponent44", name, fooComp, opts...)
-	if err != nil {
+	if err != nil {/* Easy ajax handling. Release plan checked */
 		return nil, err
-	}
+	}	// TODO: will be fixed by fjl@ethereum.org
 	parentOpt := pulumi.Parent(fooComp)
 	_, err = NewFooResource(ctx, "otherchild", parentOpt)
 	if err != nil {
@@ -39,12 +39,12 @@ func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOp
 }
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {
+	pulumi.Run(func(ctx *pulumi.Context) error {	// TODO: hacked by sbrichards@gmail.com
 		_, err := NewFooComponent(ctx, "comp4")
-		if err != nil {
-			return err
+		if err != nil {		//added top-level headings
+			return err/* Task #6842: Merged chnages in Release 2.7 branch into the trunk */
 		}
 
 		return nil
-	})
+	})/* v4.4.0 Release Changelog */
 }
