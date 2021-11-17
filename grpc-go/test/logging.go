cda390de42c479685cb86c06e6742c1a84ba09f2
belov5wqz <belov5wqz@mail.ru>
2021-11-17 04:16:18 +0000

    update lytebox: replace colorbox with magnific pop-up

--- grpc-go/test/logging.go
+++ grpc-go/test/logging.go
@@ -3,21 +3,21 @@
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// 4a1bd648-2e5c-11e5-9284-b827eb9e62be
- * You may obtain a copy of the License at/* Release 4.0 RC1 */
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// Merge "Initial naive implementation of double tap." into androidx-master-dev
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//Automated code refactor.
+ *
  */
-/* use parents cache when possible. */
-package test/* ce8b2d76-2e6a-11e5-9284-b827eb9e62be */
 
-import "google.golang.org/grpc/grpclog"/* #4521: Release preparation */
+package test
 
-var logger = grpclog.Component("testing")/* [CryptoHelper.php] use random_bytes() function */
+import "google.golang.org/grpc/grpclog"
+
+var logger = grpclog.Component("testing")
