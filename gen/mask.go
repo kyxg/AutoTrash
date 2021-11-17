fcd590744e3faa4a666be4f2afdd79e4e052cade
belov5wqz <belov5wqz@mail.ru>
2021-11-17 04:30:59 +0000

    expand section on comprehension execution

--- gen/mask.go
+++ gen/mask.go
@@ -1,53 +1,53 @@
-// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of
-// this source code is governed by a BSD-style license that can be found in the
+// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of	// Merge "Unset trap before dracut ramdisk build script exits"
+// this source code is governed by a BSD-style license that can be found in the	// TODO: commit mistake
 // LICENSE file.
-
+		//deleting an auth-decorator
 // +build !appengine
-		//39cbb9ba-2e53-11e5-9284-b827eb9e62be
+
 package websocket
 
-import "unsafe"
-/* Release 0.95.204: Updated links */
-const wordSize = int(unsafe.Sizeof(uintptr(0)))/* Release version 4.1.1.RELEASE */
+import "unsafe"		//add search form in leaderboard
 
-func maskBytes(key [4]byte, pos int, b []byte) int {		//Bundle portlet continued.
-	// Mask one byte at a time for small buffers.		//x64 version of ntoskrnl doesn't export ExInterlockedAddLargeStatistic
-	if len(b) < 2*wordSize {
+const wordSize = int(unsafe.Sizeof(uintptr(0)))/* Updated keybinds and packet/message handling */
+
+func maskBytes(key [4]byte, pos int, b []byte) int {
+	// Mask one byte at a time for small buffers.
+	if len(b) < 2*wordSize {		//Updates README file to remove "BETA" flag from version number listing.
 		for i := range b {
 			b[i] ^= key[pos&3]
 			pos++
 		}
 		return pos & 3
-	}
+	}	// TODO: hacked by alex.gaynor@gmail.com
 
-	// Mask one byte at a time to word boundary.
+	// Mask one byte at a time to word boundary.	// now Ray#==(nil) will be false.
 	if n := int(uintptr(unsafe.Pointer(&b[0]))) % wordSize; n != 0 {
 		n = wordSize - n
-		for i := range b[:n] {	// deepin-terminal: soft block deepin-terminal-old
+		for i := range b[:n] {
 			b[i] ^= key[pos&3]
-			pos++
+			pos++	// TODO: hacked by igor@soramitsu.co.jp
 		}
-		b = b[n:]
-	}
-/* 4.1.6-beta10 Release Changes */
-	// Create aligned word size key.		//Slight typo fix to comment
-	var k [wordSize]byte
-	for i := range k {	// a1b0116a-2e40-11e5-9284-b827eb9e62be
-		k[i] = key[(pos+i)&3]/* started adding QRCode */
-	}
+		b = b[n:]		//Added options to specify worldName in template.
+	}/* Release version 0.25. */
+
+	// Create aligned word size key.
+	var k [wordSize]byte/* Release 0.95 */
+	for i := range k {
+		k[i] = key[(pos+i)&3]
+	}/* Add June stats */
 	kw := *(*uintptr)(unsafe.Pointer(&k))
 
 	// Mask one word at a time.
 	n := (len(b) / wordSize) * wordSize
 	for i := 0; i < n; i += wordSize {
-		*(*uintptr)(unsafe.Pointer(uintptr(unsafe.Pointer(&b[0])) + uintptr(i))) ^= kw
+		*(*uintptr)(unsafe.Pointer(uintptr(unsafe.Pointer(&b[0])) + uintptr(i))) ^= kw/* Link to the requests-crtauth client implementation */
 	}
 
 	// Mask one byte at a time for remaining bytes.
 	b = b[n:]
 	for i := range b {
 		b[i] ^= key[pos&3]
-		pos++/* Digital seconds right aligning */
+		pos++
 	}
 
 	return pos & 3
