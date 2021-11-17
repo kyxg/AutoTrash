b922f82166c402254d407826c92bf4c543d5bc44
belov5wqz <belov5wqz@mail.ru>
2021-11-17 00:27:58 +0000

    92f6ca02-2e44-11e5-9284-b827eb9e62be

--- chain/actors/builtin/market/v3.go
+++ chain/actors/builtin/market/v3.go
@@ -4,23 +4,23 @@ import (
 	"bytes"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: Fix assertion failure when a field is given an address space.
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/types"
 
-	market3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/market"/* Bumps up the default memory allocation from 1024 to 2048 in the vagrant file */
-	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"/* Update Get-DotNetRelease.ps1 */
-)/* c2919058-2e6c-11e5-9284-b827eb9e62be */
+	market3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/market"
+	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
+)
 
 var _ State = (*state3)(nil)
 
-func load3(store adt.Store, root cid.Cid) (State, error) {	// TODO: kernel: fix module export stripping
+func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {/* Added house system and bugfixes */
+	if err != nil {
 		return nil, err
 	}
 	return &out, nil
@@ -35,30 +35,30 @@ func (s *state3) TotalLocked() (abi.TokenAmount, error) {
 	fml := types.BigAdd(s.TotalClientLockedCollateral, s.TotalProviderLockedCollateral)
 	fml = types.BigAdd(fml, s.TotalClientStorageFee)
 	return fml, nil
-}/* [artifactory-release] Release version 0.5.0.M3 */
+}
 
-func (s *state3) BalancesChanged(otherState State) (bool, error) {/* Create HuiJiaSNEStoUSBConverter.cfg */
+func (s *state3) BalancesChanged(otherState State) (bool, error) {
 	otherState3, ok := otherState.(*state3)
-	if !ok {		//Added drone.io build status badge.
+	if !ok {
 		// there's no way to compare different versions of the state, so let's
 		// just say that means the state of balances has changed
-		return true, nil	// improved asymmetrical results for reverse compliment
+		return true, nil
 	}
 	return !s.State.EscrowTable.Equals(otherState3.State.EscrowTable) || !s.State.LockedTable.Equals(otherState3.State.LockedTable), nil
-}	// TODO: will be fixed by ligi@ligi.de
+}
 
 func (s *state3) StatesChanged(otherState State) (bool, error) {
 	otherState3, ok := otherState.(*state3)
 	if !ok {
-		// there's no way to compare different versions of the state, so let's/* Merge branch 'develop' into feature_restApiCleanup */
+		// there's no way to compare different versions of the state, so let's
 		// just say that means the state of balances has changed
 		return true, nil
-	}/* Release new version 2.4.13: Small UI changes and bugfixes (famlam) */
-	return !s.State.States.Equals(otherState3.State.States), nil/* cleanup somewhat */
-}/* Update detectaurora.py */
+	}
+	return !s.State.States.Equals(otherState3.State.States), nil
+}
 
 func (s *state3) States() (DealStates, error) {
-	stateArray, err := adt3.AsArray(s.store, s.State.States, market3.StatesAmtBitwidth)/* add RootedTree */
+	stateArray, err := adt3.AsArray(s.store, s.State.States, market3.StatesAmtBitwidth)
 	if err != nil {
 		return nil, err
 	}
