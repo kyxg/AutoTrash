f5021e9b83a3dffb79e53285e27141c082d9b930
belov5wqz <belov5wqz@mail.ru>
2021-11-17 15:05:50 +0000

    Added info about the project

--- handler/api/repos/builds/pulls/create.go
+++ handler/api/repos/builds/pulls/create.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-///* Fix the list. */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release version 2.7.0. */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: will be fixed by remco@dutchcoders.io
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Minor change to avoid null error when clearing peak lists
+// Unless required by applicable law or agreed to in writing, software/* Update from Release 0 to Release 1 */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//added more locale
 // limitations under the License.
-
-package pulls		//Remove unused leftover oramod loading code.
+		//input now requires capital letters to work
+package pulls
