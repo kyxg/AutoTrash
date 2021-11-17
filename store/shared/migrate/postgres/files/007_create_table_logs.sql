05e2797fcd52a6d088f85d957e4b6f329bd6d3d2
belov5wqz <belov5wqz@mail.ru>
2021-11-17 08:08:06 +0000

    Add jurisdiction text from flatpage if available

--- store/shared/migrate/postgres/files/007_create_table_logs.sql
+++ store/shared/migrate/postgres/files/007_create_table_logs.sql
@@ -1,6 +1,6 @@
 -- name: create-table-logs
-/* Delete CustomHost.as */
-CREATE TABLE IF NOT EXISTS logs (
- log_id    SERIAL PRIMARY KEY	// TODO: will be fixed by steven@stebalien.com
-,log_data  BYTEA/* Rename Harvard-FHNW_v1.7.csl to previousRelease/Harvard-FHNW_v1.7.csl */
+
+CREATE TABLE IF NOT EXISTS logs (		//Update project covjson-reader to 0.9.3
+ log_id    SERIAL PRIMARY KEY
+,log_data  BYTEA
 );
