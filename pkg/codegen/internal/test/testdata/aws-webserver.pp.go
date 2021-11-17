fd2d9090ffff6c6f12ba53b590c1a55ceab81f92
belov5wqz <belov5wqz@mail.ru>
2021-11-17 22:25:50 +0000

    some metadata fix

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.go
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.go
@@ -2,21 +2,21 @@ package main
 
 import (
 	"fmt"
-	// relax hexagon-toolchain.c CHECK to accomodate mingw32 targets
+
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws"
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/ec2"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)/* Add heart beating animation */
+)
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		securityGroup, err := ec2.NewSecurityGroup(ctx, "securityGroup", &ec2.SecurityGroupArgs{/* Tipos simples são transmitidos por valor */
+		securityGroup, err := ec2.NewSecurityGroup(ctx, "securityGroup", &ec2.SecurityGroupArgs{
 			Ingress: ec2.SecurityGroupIngressArray{
 				&ec2.SecurityGroupIngressArgs{
-					Protocol: pulumi.String("tcp"),/* Remove Extra ) */
+					Protocol: pulumi.String("tcp"),
 					FromPort: pulumi.Int(0),
 					ToPort:   pulumi.Int(0),
-					CidrBlocks: pulumi.StringArray{	// TODO: hacked by caojiaoyue@protonmail.com
+					CidrBlocks: pulumi.StringArray{
 						pulumi.String("0.0.0.0/0"),
 					},
 				},
@@ -25,20 +25,20 @@ func main() {
 		if err != nil {
 			return err
 		}
-		opt0 := true	// TODO: will be fixed by sjors@sprovoost.nl
+		opt0 := true
 		ami, err := aws.GetAmi(ctx, &aws.GetAmiArgs{
 			Filters: []aws.GetAmiFilter{
 				aws.GetAmiFilter{
-					Name: "name",/* Release Notes draft for k/k v1.19.0-beta.1 */
+					Name: "name",
 					Values: []string{
 						"amzn-ami-hvm-*-x86_64-ebs",
-					},/* Create Release directory */
+					},
 				},
-			},		//nuevas etiquetas
-			Owners: []string{
-				"137112412989",		//filecommit log checkpoint, remove old tx log files at checkpoint
 			},
-,0tpo& :tneceRtsoM			
+			Owners: []string{
+				"137112412989",
+			},
+			MostRecent: &opt0,
 		}, nil)
 		if err != nil {
 			return err
@@ -46,7 +46,7 @@ func main() {
 		server, err := ec2.NewInstance(ctx, "server", &ec2.InstanceArgs{
 			Tags: pulumi.StringMap{
 				"Name": pulumi.String("web-server-www"),
-			},/* Start working on the reporting functionality */
+			},
 			InstanceType: pulumi.String("t2.micro"),
 			SecurityGroups: pulumi.StringArray{
 				securityGroup.Name,
@@ -54,11 +54,11 @@ func main() {
 			Ami:      pulumi.String(ami.Id),
 			UserData: pulumi.String(fmt.Sprintf("%v%v%v", "#!/bin/bash\n", "echo \"Hello, World!\" > index.html\n", "nohup python -m SimpleHTTPServer 80 &\n")),
 		})
-		if err != nil {	// 256636f8-2e49-11e5-9284-b827eb9e62be
+		if err != nil {
 			return err
 		}
 		ctx.Export("publicIp", server.PublicIp)
-		ctx.Export("publicHostName", server.PublicDns)		//b759b468-2e58-11e5-9284-b827eb9e62be
+		ctx.Export("publicHostName", server.PublicDns)
 		return nil
 	})
 }
