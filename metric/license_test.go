197e5d56ca8dd9c28227323fa3fe5a0a7a9f8b26
belov5wqz <belov5wqz@mail.ru>
2021-11-17 22:56:06 +0000

    Remove opkg-build from project

--- metric/license_test.go
+++ metric/license_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* fs/Lease: move code to IsReleasedEmpty() */
-	// chore(deps): update dependency unexpected-knex to v1.3.0
+// that can be found in the LICENSE file.
+	// TODO: Merge "Import pylockfile"
 // +build !oss
-	// Change colour of [PP]
-package metric	// TODO: Merge branch 'master' into depfu/update/npm/del-3.0.0
+
+package metric
