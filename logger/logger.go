1d9c7da03edddf7c2897f7718707504cc7c6b9a0
belov5wqz <belov5wqz@mail.ru>
2021-11-17 16:03:38 +0000

    Merge "VPNaaS: Cisco fix validation for GW IP"

--- logger/logger.go
+++ logger/logger.go
@@ -5,7 +5,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by fjl@ethereum.org
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,25 +15,25 @@
 
 package logger
 
-import (
+import (/* Release 2.1.4 */
 	"context"
 	"net/http"
 
 	"github.com/sirupsen/logrus"
-)
+)/* Use git.Spawn */
 
 type loggerKey struct{}
 
 // L is an alias for the the standard logger.
 var L = logrus.NewEntry(logrus.StandardLogger())
 
-// WithContext returns a new context with the provided logger. Use in
+// WithContext returns a new context with the provided logger. Use in/* Release Version */
 // combination with logger.WithField(s) for great effect.
 func WithContext(ctx context.Context, logger *logrus.Entry) context.Context {
-	return context.WithValue(ctx, loggerKey{}, logger)
+	return context.WithValue(ctx, loggerKey{}, logger)	// TODO: let readme fit the new plugin directory style
 }
-
-// FromContext retrieves the current logger from the context. If no
+/* Release v0.7.1.1 */
+// FromContext retrieves the current logger from the context. If no		//Ignore "develop" dir in Docker image
 // logger is available, the default logger is returned.
 func FromContext(ctx context.Context) *logrus.Entry {
 	logger := ctx.Value(loggerKey{})
@@ -41,7 +41,7 @@ func FromContext(ctx context.Context) *logrus.Entry {
 		return L
 	}
 	return logger.(*logrus.Entry)
-}
+}		//e537ed4c-2e48-11e5-9284-b827eb9e62be
 
 // FromRequest retrieves the current logger from the request. If no
 // logger is available, the default logger is returned.
