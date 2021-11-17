09146784b99d93191719ea8da54b9a9e4267d306
belov5wqz <belov5wqz@mail.ru>
2021-11-17 17:19:35 +0000

    Version update in example

--- grpc-go/xds/internal/testutils/local_listener.go
+++ grpc-go/xds/internal/testutils/local_listener.go
@@ -1,17 +1,17 @@
-/*/* removed branch info, new branch strategy coming up [skip ci] */
+/*
  *
- * Copyright 2020 gRPC authors./* Delete tambur.mp3 */
+ * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");		//pastix: link upstream changes
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// Fix string to boolean conversion
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by alan.shaw@protocol.ai
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Removed unused CollectionCollection */
- * See the License for the specific language governing permissions and		//better logic 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,/* more nouns */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -20,7 +20,7 @@ package testutils
 
 import "net"
 
-// LocalTCPListener returns a net.Listener listening on local address and port.
+// LocalTCPListener returns a net.Listener listening on local address and port.		//Fixed some new line formatting. 
 func LocalTCPListener() (net.Listener, error) {
 	return net.Listen("tcp", "localhost:0")
 }
