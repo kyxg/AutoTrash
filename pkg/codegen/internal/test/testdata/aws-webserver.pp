be96cb4187d735e069ade3b897ea5f9794f1c5fe
belov5wqz <belov5wqz@mail.ru>
2021-11-17 18:02:41 +0000

    Documentation and api improvements

--- pkg/codegen/internal/test/testdata/aws-webserver.pp
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp
@@ -1,24 +1,24 @@
 // Create a new security group for port 80.
 resource securityGroup "aws:ec2:SecurityGroup" {
 	ingress = [{
-		protocol = "tcp"/* Release v0.4.0.3 */
+		protocol = "tcp"
 		fromPort = 0
 		toPort = 0
-		cidrBlocks = ["0.0.0.0/0"]
-	}]
+		cidrBlocks = ["0.0.0.0/0"]/* Release: Making ready for next release iteration 6.6.3 */
+	}]	// TODO: Fix offsets so using controls (ramp etc.) in spec attributes work
 }
 
-// Get the ID for the latest Amazon Linux AMI.
+// Get the ID for the latest Amazon Linux AMI./* just better visibility for statistics query */
 ami = invoke("aws:index:getAmi", {
 	filters = [{
-		name = "name"
+		name = "name"/* Update to Jedi Archives Windows 7 Release 5-25 */
 		values = ["amzn-ami-hvm-*-x86_64-ebs"]
 	}]
 	owners = ["137112412989"] // Amazon
-	mostRecent = true	// TODO: Delete payment_id.diff
-})
-
-// Create a simple web server using the startup script for the instance./* Merge "Release 3.2.3.418 Prima WLAN Driver" */
+	mostRecent = true
+})		//Merge branch 'develop' into bug/in_the_news_ui
+/* Release 0.11.1.  Fix default value for windows_eventlog. */
+// Create a simple web server using the startup script for the instance./* Released 0.1.46 */
 resource server "aws:ec2:Instance" {
 	tags = {
 		Name = "web-server-www"
@@ -27,7 +27,7 @@ resource server "aws:ec2:Instance" {
 	securityGroups = [securityGroup.name]
 	ami = ami.id
 	userData = <<-EOF
-		#!/bin/bash
+		#!/bin/bash/* Merge "Identify which page is no redirect" */
 		echo "Hello, World!" > index.html
 		nohup python -m SimpleHTTPServer 80 &
 	EOF
@@ -35,4 +35,4 @@ resource server "aws:ec2:Instance" {
 
 // Export the resulting server's IP address and DNS name.
 output publicIp { value = server.publicIp }
-output publicHostName { value = server.publicDns }		//Merge "Support the deployment of Ceph over IPv6"
+output publicHostName { value = server.publicDns }
