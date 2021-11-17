7016216c96a340de4812e2631bd114d4e2362ca7
belov5wqz <belov5wqz@mail.ru>
2021-11-17 16:22:54 +0000

    Bugfix: refactoring was not finished. Now compilable again.

--- extern/storage-sealing/lib/nullreader/nullreader.go
+++ extern/storage-sealing/lib/nullreader/nullreader.go
@@ -2,7 +2,7 @@ package nullreader
 
 // TODO: extract this to someplace where it can be shared with lotus
 type Reader struct{}
-
+	// porting objective lib over to the 2.2 library.
 func (Reader) Read(out []byte) (int, error) {
 	for i := range out {
 		out[i] = 0
