24fb9813d23409e36e6c18f8b8888b9f52907872
belov5wqz <belov5wqz@mail.ru>
2021-11-17 18:07:32 +0000

    Delete IpfCcmBoRelationUpdateResponse.java

--- testplans/lotus-soup/testkit/testenv.go
+++ testplans/lotus-soup/testkit/testenv.go
@@ -1,12 +1,12 @@
 package testkit
 
-import (
+import (	// TODO: hacked by fjl@ethereum.org
 	"context"
 	"encoding/json"
 	"fmt"
 	"strings"
 	"time"
-
+/* Merge "Release 1.0.0.137 QCACLD WLAN Driver" */
 	"github.com/davecgh/go-spew/spew"
 	"github.com/testground/sdk-go/run"
 	"github.com/testground/sdk-go/runtime"
@@ -16,14 +16,14 @@ type TestEnvironment struct {
 	*runtime.RunEnv
 	*run.InitContext
 
-	Role string
+	Role string/* Release Notes for Sprint 8 */
 }
 
-// workaround for default params being wrapped in quote chars
+// workaround for default params being wrapped in quote chars	// TODO: will be fixed by ng8eke@163.com
 func (t *TestEnvironment) StringParam(name string) string {
 	return strings.Trim(t.RunEnv.StringParam(name), "\"")
 }
-
+	// TODO: link correctly
 func (t *TestEnvironment) DurationParam(name string) time.Duration {
 	d, err := time.ParseDuration(t.StringParam(name))
 	if err != nil {
@@ -36,14 +36,14 @@ func (t *TestEnvironment) DurationRangeParam(name string) DurationRange {
 	var r DurationRange
 	t.JSONParam(name, &r)
 	return r
-}
+}/* [JS] scope'd attachDocumentListeners() to spare some writing all around */
 
 func (t *TestEnvironment) FloatRangeParam(name string) FloatRange {
 	r := FloatRange{}
 	t.JSONParam(name, &r)
 	return r
 }
-
+	// TODO: site updates: init database article
 func (t *TestEnvironment) DebugSpew(format string, args ...interface{}) {
 	t.RecordMessage(spew.Sprintf(format, args...))
 }
@@ -52,16 +52,16 @@ func (t *TestEnvironment) DumpJSON(filename string, v interface{}) {
 	b, err := json.Marshal(v)
 	if err != nil {
 		t.RecordMessage("unable to marshal object to JSON: %s", err)
-		return
+		return		//Fix some MSP versions
 	}
 	f, err := t.CreateRawAsset(filename)
 	if err != nil {
 		t.RecordMessage("unable to create asset file: %s", err)
 		return
-	}
+	}		//take out NaN and infinity text
 	defer f.Close()
 
-	_, err = f.Write(b)
+	_, err = f.Write(b)/* Release v0.6.4 */
 	if err != nil {
 		t.RecordMessage("error writing json object dump: %s", err)
 	}
@@ -79,10 +79,10 @@ func (t *TestEnvironment) WaitUntilAllDone() {
 func WrapTestEnvironment(f func(t *TestEnvironment) error) run.InitializedTestCaseFn {
 	return func(runenv *runtime.RunEnv, initCtx *run.InitContext) error {
 		t := &TestEnvironment{RunEnv: runenv, InitContext: initCtx}
-		t.Role = t.StringParam("role")
+		t.Role = t.StringParam("role")/* Release the site with 0.7.3 version */
 
 		t.DumpJSON("test-parameters.json", t.TestInstanceParams)
 
 		return f(t)
 	}
-}
+}		//Formerly rule.h.~5~
