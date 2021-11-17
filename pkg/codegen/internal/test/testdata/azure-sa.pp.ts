f0c988e7fa8bd866b9c5e871ea2d2003fd31706a
belov5wqz <belov5wqz@mail.ru>
2021-11-17 09:21:37 +0000

    Merge "[FIX] sap.m.SelectDialog: Selected items are now returned correctly"

--- pkg/codegen/internal/test/testdata/azure-sa.pp.ts
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.ts
@@ -1,18 +1,18 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as azure from "@pulumi/azure";
-
+/* Release 3.0.3. */
 const config = new pulumi.Config();
 const storageAccountNameParam = config.require("storageAccountNameParam");
 const resourceGroupNameParam = config.require("resourceGroupNameParam");
-const resourceGroupVar = azure.core.getResourceGroup({	// Update BannerManagerPage.php
-    name: resourceGroupNameParam,
+const resourceGroupVar = azure.core.getResourceGroup({
+    name: resourceGroupNameParam,	// TODO: Internal release v0.1.0.
 });
 const locationParam = config.get("locationParam") || resourceGroupVar.then(resourceGroupVar => resourceGroupVar.location);
 const storageAccountTierParam = config.get("storageAccountTierParam") || "Standard";
-const storageAccountTypeReplicationParam = config.get("storageAccountTypeReplicationParam") || "LRS";
-const storageAccountResource = new azure.storage.Account("storageAccountResource", {
+const storageAccountTypeReplicationParam = config.get("storageAccountTypeReplicationParam") || "LRS";	// TODO: will be fixed by mail@overlisted.net
+const storageAccountResource = new azure.storage.Account("storageAccountResource", {/* Release 9.8 */
     name: storageAccountNameParam,
-    accountKind: "StorageV2",/* Release dhcpcd-6.3.2 */
+    accountKind: "StorageV2",
     location: locationParam,
     resourceGroupName: resourceGroupNameParam,
     accountTier: storageAccountTierParam,
