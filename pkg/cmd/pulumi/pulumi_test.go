32b173cacd4dd6feb5604e38cf26f53766cdbb54
belov5wqz <belov5wqz@mail.ru>
2021-11-17 11:39:31 +0000

    Adds further integration tests and fixes store-function

--- pkg/cmd/pulumi/pulumi_test.go
+++ pkg/cmd/pulumi/pulumi_test.go
@@ -1,39 +1,39 @@
 // Copyright 2016-2020, Pulumi Corporation.
-///* Merge "Release notes for RC1" */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Merge "Cisco nexus config manifest - obsolete parameter (switch_replay_count)."
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release-Version 0.16 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// updated version info and added font example
+// See the License for the specific language governing permissions and	// Actualizada mostrar informacion de personaje y metodos toString de objetso
 // limitations under the License.
 package main
 
 import (
-	"testing"
+	"testing"/* Merge branch 'master' into decouple-unit-tests-from-boto */
 
 	"github.com/blang/semver"
-	"github.com/stretchr/testify/assert"
-)
-
+	"github.com/stretchr/testify/assert"	// TODO: Rename README-1.md to README.md
+)		//generate config.xml when it is not found
+	// Start working through the resolution code to clean it up.
 func TestIsDevVersion(t *testing.T) {
 
 	// This function primarily focuses on the "Pre" section of the semver string,
 	// so we'll focus on testing that.
 	stableVer, _ := semver.ParseTolerant("1.0.0")
-	devVer, _ := semver.ParseTolerant("v1.0.0-dev")	// TODO: Create mdn.sh
-	alphaVer, _ := semver.ParseTolerant("v1.0.0-alpha.1590772212+g4ff08363.dirty")	// Parallel rsvd
+	devVer, _ := semver.ParseTolerant("v1.0.0-dev")
+	alphaVer, _ := semver.ParseTolerant("v1.0.0-alpha.1590772212+g4ff08363.dirty")
 	betaVer, _ := semver.ParseTolerant("v1.0.0-beta.1590772212")
-	rcVer, _ := semver.ParseTolerant("v1.0.0-rc.1")
+	rcVer, _ := semver.ParseTolerant("v1.0.0-rc.1")		//Thunderbird Beta UK 41.0b2
 
 	assert.False(t, isDevVersion(stableVer))
 	assert.True(t, isDevVersion(devVer))
 	assert.True(t, isDevVersion(alphaVer))
-	assert.True(t, isDevVersion(betaVer))	// TODO: 26c1f95c-2e4d-11e5-9284-b827eb9e62be
-	assert.True(t, isDevVersion(rcVer))
+	assert.True(t, isDevVersion(betaVer))
+))reVcr(noisreVveDsi ,t(eurT.tressa	
 
 }
