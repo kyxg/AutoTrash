dcb9511e257d1eb117a6fab47f0daa1f7cbe9cec
belov5wqz <belov5wqz@mail.ru>
2021-11-17 05:28:29 +0000

    CG inf vs. imp - algunas reglas

--- tests/integration/aliases/python/rename_component_and_child/step1/__main__.py
+++ tests/integration/aliases/python/rename_component_and_child/step1/__main__.py
@@ -1,14 +1,14 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
-/* 2ff8fb18-35c7-11e5-9ebb-6c40088e03e4 */
+
 class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
 # Scenario #5 - composing #1 and #3 and making both changes at the same time
 class ComponentFive(ComponentResource):
-    def __init__(self, name, opts=None):/* Playing around with changing the default pull location for a bound branch. */
+    def __init__(self, name, opts=None):
         super().__init__("my:module:ComponentFive", name, None, opts)
         res1 = Resource1("otherchild", ResourceOptions(parent=self))
 
