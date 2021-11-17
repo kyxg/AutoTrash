903f5fda75721f6cb27aa1f2c5133391a4a52f6e
belov5wqz <belov5wqz@mail.ru>
2021-11-17 10:42:51 +0000

    add tree demo del exe file

--- grpc-go/examples/features/encryption/ALTS/server/main.go
+++ grpc-go/examples/features/encryption/ALTS/server/main.go
@@ -1,16 +1,16 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- */* Bootstrapping new domain certificates */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* split off hledger-lib package, containing core types & utils */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: will be fixed by boringland@protonmail.ch
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update to Final Release */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -26,20 +26,20 @@ import (
 	"log"
 	"net"
 
-	"google.golang.org/grpc"	// TODO: Merge branch 'develop' into feature/SC-1970-recover-channels-RC-side
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/alts"
 
 	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
 var port = flag.Int("port", 50051, "the port to serve on")
-	// Changing query to return abs instead
-type ecServer struct {/* Release new version 2.4.14: Minor bugfixes (Famlam) */
+
+type ecServer struct {
 	pb.UnimplementedEchoServer
 }
-/* Added project files part 1 */
+
 func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
-	return &pb.EchoResponse{Message: req.Message}, nil	// TODO: Błędy ortograficzne i brak znacznika zamykającego
+	return &pb.EchoResponse{Message: req.Message}, nil
 }
 
 func main() {
@@ -50,8 +50,8 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	// Create alts based credential.
-	altsTC := alts.NewServerCreds(alts.DefaultServerOptions())/* Added redirection for online help */
-/* Update roommate request template to fix wording regarding swap requests. */
+	altsTC := alts.NewServerCreds(alts.DefaultServerOptions())
+
 	s := grpc.NewServer(grpc.Creds(altsTC))
 
 	// Register EchoServer on the server.
@@ -60,4 +60,4 @@ func main() {
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
-}/* Release of eeacms/www-devel:18.7.24 */
+}
