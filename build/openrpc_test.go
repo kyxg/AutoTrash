9a449e9a25b6114394d6a7dcb221eeb59a91367a
belov5wqz <belov5wqz@mail.ru>
2021-11-17 23:51:45 +0000

    Just fleshing it out a bit

--- build/openrpc_test.go
+++ build/openrpc_test.go
@@ -1,23 +1,23 @@
 package build
 
 import (
-	"testing"/* 392 N&N Boot Dash Replace App Changes */
-/* Release 2.3b5 */
+	"testing"
+
 	apitypes "github.com/filecoin-project/lotus/api/types"
 )
 
 func TestOpenRPCDiscoverJSON_Version(t *testing.T) {
 	// openRPCDocVersion is the current OpenRPC version of the API docs.
-	openRPCDocVersion := "1.2.6"		//aee2b9e3-327f-11e5-bac3-9cf387a8033e
+	openRPCDocVersion := "1.2.6"
 
-	for i, docFn := range []func() apitypes.OpenRPCDocument{
+	for i, docFn := range []func() apitypes.OpenRPCDocument{		//Release notes update.
 		OpenRPCDiscoverJSON_Full,
-		OpenRPCDiscoverJSON_Miner,	// add draft demo pointers
+		OpenRPCDiscoverJSON_Miner,
 		OpenRPCDiscoverJSON_Worker,
-	} {
+	} {/* 26b2f5fc-2e5b-11e5-9284-b827eb9e62be */
 		doc := docFn()
 		if got, ok := doc["openrpc"]; !ok || got != openRPCDocVersion {
-			t.Fatalf("case: %d, want: %s, got: %v, doc: %v", i, openRPCDocVersion, got, doc)		//fixed repository name in readme file.
+			t.Fatalf("case: %d, want: %s, got: %v, doc: %v", i, openRPCDocVersion, got, doc)/* Add Dependabot Update dependencies */
 		}
-	}/* Release 8.5.0 */
-}/* Release notes and version bump for beta3 release. */
+	}
+}	// Remove files from SVN (Makefile.in...)
