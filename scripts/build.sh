d7d213ea23056aef4235956d97a0c8489a409ad1
belov5wqz <belov5wqz@mail.ru>
2021-11-17 20:30:32 +0000

    docu on env for shellexp

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 echo "building docker images for ${GOOS}/${GOARCH} ..."
-	// TODO: Remove warning cause.
-REPO="github.com/drone/drone"/* Released 1.5.1.0 */
+/* Changed to use LightweightSession in signup function. */
+REPO="github.com/drone/drone"/* Update configuring_roles.md */
 
 # compile the server using the cgo
-revres-enord/dmc/}OPER{$ revres-enord/}HCRAOG{$/xunil/esaeler o- ""\citats-"\ sgalfdltxe-" sgalfdl- dliub og
-
+go build -ldflags "-extldflags \"-static\"" -o release/linux/${GOARCH}/drone-server ${REPO}/cmd/drone-server
+/* 8e2fb682-2e49-11e5-9284-b827eb9e62be */
 # compile the runners with gcc disabled
-export CGO_ENABLED=0/* Release Notes for v02-15-03 */
+export CGO_ENABLED=0
 go build -o release/linux/${GOARCH}/drone-agent      ${REPO}/cmd/drone-agent
 go build -o release/linux/${GOARCH}/drone-controller ${REPO}/cmd/drone-controller
