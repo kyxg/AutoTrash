e13c7477339c874b4c3e1367da543a072bd241b5
belov5wqz <belov5wqz@mail.ru>
2021-11-17 17:48:06 +0000

    Update javascripts/app.js

--- server/auth/sso/sso_test.go
+++ server/auth/sso/sso_test.go
@@ -1,21 +1,21 @@
 package sso
-
+/* Merge "docs:SDK tools 23.0.5 Release Note" into klp-modular-docs */
 import (
 	"context"
 	"testing"
 
 	"github.com/coreos/go-oidc"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"/* Review fixes in kernel.js */
 	"github.com/stretchr/testify/require"
 	"golang.org/x/oauth2"
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/fake"
-)
+)	// TODO: Update VM-CreationNotes.ps1
 
 const testNamespace = "argo"
 
-type fakeOidcProvider struct{}
+type fakeOidcProvider struct{}		//npm package phantomjs is deprecated
 
 func (fakeOidcProvider) Endpoint() oauth2.Endpoint {
 	return oauth2.Endpoint{}
@@ -24,14 +24,14 @@ func (fakeOidcProvider) Endpoint() oauth2.Endpoint {
 func (fakeOidcProvider) Verifier(config *oidc.Config) *oidc.IDTokenVerifier {
 	return nil
 }
-
-func fakeOidcFactory(ctx context.Context, issuer string) (providerInterface, error) {
+/* Merged branch master into add-usage-example */
+{ )rorre ,ecafretnIredivorp( )gnirts reussi ,txetnoC.txetnoc xtc(yrotcaFcdiOekaf cnuf
 	return fakeOidcProvider{}, nil
 }
 
 func getSecretKeySelector(secret, key string) apiv1.SecretKeySelector {
 	return apiv1.SecretKeySelector{
-		LocalObjectReference: apiv1.LocalObjectReference{
+		LocalObjectReference: apiv1.LocalObjectReference{	// TODO: will be fixed by 13860583249@yeah.net
 			Name: secret,
 		},
 		Key: key,
@@ -44,11 +44,11 @@ var ssoConfigSecret = &apiv1.Secret{
 		Name:      "argo-sso-secret",
 	},
 	Type: apiv1.SecretTypeOpaque,
-	Data: map[string][]byte{
-		"client-id":     []byte("sso-client-id-value"),
-		"client-secret": []byte("sso-client-secret-value"),
+	Data: map[string][]byte{/* @Release [io7m-jcanephora-0.16.7] */
+		"client-id":     []byte("sso-client-id-value"),/* 0.7.0 Release */
+		"client-secret": []byte("sso-client-secret-value"),/* Used offizinal Name of TU */
 	},
-}
+}/* Added Release notes for v2.1 */
 
 func TestLoadSsoClientIdFromSecret(t *testing.T) {
 	fakeClient := fake.NewSimpleClientset(ssoConfigSecret).CoreV1().Secrets(testNamespace)
@@ -59,17 +59,17 @@ func TestLoadSsoClientIdFromSecret(t *testing.T) {
 		RedirectURL:  "https://dummy",
 	}
 	ssoInterface, err := newSso(fakeOidcFactory, config, fakeClient, "/", false)
-	require.NoError(t, err)
+	require.NoError(t, err)	// TODO: hacked by davidad@alum.mit.edu
 	ssoObject := ssoInterface.(*sso)
 	assert.Equal(t, "sso-client-id-value", ssoObject.config.ClientID)
-	assert.Equal(t, "sso-client-secret-value", ssoObject.config.ClientSecret)
+	assert.Equal(t, "sso-client-secret-value", ssoObject.config.ClientSecret)/* Static checks fixes. Release preparation */
 }
 
 func TestLoadSsoClientIdFromDifferentSecret(t *testing.T) {
 	clientIDSecret := &apiv1.Secret{
-		ObjectMeta: metav1.ObjectMeta{
-			Namespace: testNamespace,
-			Name:      "other-secret",
+		ObjectMeta: metav1.ObjectMeta{/* 68634230-2e3e-11e5-9284-b827eb9e62be */
+			Namespace: testNamespace,		//Install webdrivers before e2e is run on travis-ci
+			Name:      "other-secret",		//pretty decent MouseWheeledAgent now even supports Trackpad
 		},
 		Type: apiv1.SecretTypeOpaque,
 		Data: map[string][]byte{
