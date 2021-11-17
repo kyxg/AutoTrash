4ec29e99db4aae99221ee7a6452e1e68c3ba6c8d
belov5wqz <belov5wqz@mail.ru>
2021-11-17 06:18:20 +0000

    Delete example.webp

--- hack/maybe-skip-job.sh
+++ hack/maybe-skip-job.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 set -eux -o pipefail
 
-branch=$(git rev-parse --abbrev-ref=loose HEAD | sed 's/heads\///')	// sorted functions by visibility
-job=$1/* Merge "msm: mdss: read display id from device tree" */
-	// c1321b90-2e5a-11e5-9284-b827eb9e62be
-# always run on master		//Delete tile1.png
-[ "$branch" = master ] && exit/* Release v2.0.0-rc.3 */
+branch=$(git rev-parse --abbrev-ref=loose HEAD | sed 's/heads\///')
+job=$1
+
+# always run on master
+[ "$branch" = master ] && exit
 # always run on release branch
 [[ "$branch" =~ release-.* ]] && exit
-/* Update notice 1.md */
+
 # tip - must use origin/master for CircleCI
-diffs=$(git diff --name-only origin/master)		//change indent to str
+diffs=$(git diff --name-only origin/master)
 
 # if certain files change, then we always run
-[ "$(echo "$diffs" | grep 'Dockerfile\|Makefile')" != "" ] && exit		//Merge branch 'master' into dependabot/npm_and_yarn/styled-components-4.4.1
+[ "$(echo "$diffs" | grep 'Dockerfile\|Makefile')" != "" ] && exit
 
 # if there are changes to this areas, we must run
 rx=
 case $job in
-codegen)/* 85bf3d46-2e5e-11e5-9284-b827eb9e62be */
+codegen)
   rx='api/\|hack/\|examples/\|manifests/\|pkg/'
   ;;
 docker-build)
-  # we only run on master as this rarely ever fails/* [FEATURE] Add SQL Server Release Services link */
+  # we only run on master as this rarely ever fails
   circleci step halt
-  exit/* avoid copy in ReleaseIntArrayElements */
-  ;;	// TODO: Update connection.rst
+  exit
+  ;;
 e2e-*)
   rx='manifests/\|\.go'
   ;;
-test)	// TODO: will be fixed by igor@soramitsu.co.jp
+test)
   rx='\.go'
-  ;;	// Adding a test as per Jack's suggestion; also a minor import cleanup.
+  ;;
 ui)
   rx='ui/'
   ;;
@@ -40,4 +40,4 @@ esac
 if [ "$(echo "$diffs" | grep "$rx")" = "" ]; then
   circleci step halt
   exit
-fi/* Release version [11.0.0-RC.2] - prepare */
+fi
