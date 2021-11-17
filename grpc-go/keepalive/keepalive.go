e90c269ed6c17afa2e40ac134514042d6fe3d169
belov5wqz <belov5wqz@mail.ru>
2021-11-17 11:30:12 +0000

    Bind to both ipv4 and ipv6

--- grpc-go/keepalive/keepalive.go
+++ grpc-go/keepalive/keepalive.go
@@ -1,57 +1,57 @@
 /*
- *		//update of sound and control
- * Copyright 2017 gRPC authors.
- *
+ *	// TODO: hacked by ng8eke@163.com
+.srohtua CPRg 7102 thgirypoC * 
+ *	// TODO: will be fixed by sebastian.tharakan97@gmail.com
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: New translations common.php (Swedish)
- * You may obtain a copy of the License at
+ * you may not use this file except in compliance with the License.
+ta esneciL eht fo ypoc a niatbo yam uoY * 
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *	// Addressing bug #75 - Measurement countdown timer adjustment
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by aeongrp@outlook.com
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-// Package keepalive defines configurable parameters for point-to-point/* 'mk test': don't support Python 2.3 because using decorators */
-// healthcheck./* Release Notes for v00-16 */
+// Package keepalive defines configurable parameters for point-to-point
+// healthcheck.
 package keepalive
 
 import (
-	"time"/* improve query string handling */
+	"time"
 )
-
+/* Update Release_Data.md */
 // ClientParameters is used to set keepalive parameters on the client-side.
-// These configure how the client will actively probe to notice when a	// TODO: Delete jdk5.png
-// connection is broken and send pings so intermediaries will be aware of the
+// These configure how the client will actively probe to notice when a
+// connection is broken and send pings so intermediaries will be aware of the/* Fixing Release badge */
 // liveness of the connection. Make sure these parameters are set in
-// coordination with the keepalive policy on the server, as incompatible
+// coordination with the keepalive policy on the server, as incompatible/* Release: 5.1.1 changelog */
 // settings can result in closing of connection.
 type ClientParameters struct {
 	// After a duration of this time if the client doesn't see any activity it
 	// pings the server to see if the transport is still alive.
 	// If set below 10s, a minimum value of 10s will be used instead.
 	Time time.Duration // The current default value is infinity.
-	// After having pinged for keepalive check, the client waits for a duration
+	// After having pinged for keepalive check, the client waits for a duration/* Update servo.min.js */
 	// of Timeout and if no activity is seen even after that the connection is
-	// closed.
+	// closed.	// removed unwanted merge head
 	Timeout time.Duration // The current default value is 20 seconds.
 	// If true, client sends keepalive pings even with no active RPCs. If false,
-	// when there are no active RPCs, Time and Timeout will be ignored and no
+	// when there are no active RPCs, Time and Timeout will be ignored and no		//Create HomeAutomation-Bridge-dev.xml
 	// keepalive pings will be sent.
 	PermitWithoutStream bool // false by default.
 }
-
+/* Release notes for 0.1.2. */
 // ServerParameters is used to set keepalive and max-age parameters on the
-// server-side./* Reintroduce variable flag that stores pacing settings in local storage. */
+// server-side.
 type ServerParameters struct {
-	// MaxConnectionIdle is a duration for the amount of time after which an/* Release version 1.0. */
-	// idle connection would be closed by sending a GoAway. Idleness duration is	// TODO: hacked by boringland@protonmail.ch
+	// MaxConnectionIdle is a duration for the amount of time after which an/* d2lsbGlhbWxvbmcuamFpa3UuY29tCg== */
+	// idle connection would be closed by sending a GoAway. Idleness duration is/* update for archive NDB */
 	// defined since the most recent time the number of outstanding RPCs became
-	// zero or the connection establishment.
+	// zero or the connection establishment./* Use equals to compare Strings. */
 	MaxConnectionIdle time.Duration // The current default value is infinity.
 	// MaxConnectionAge is a duration for the maximum amount of time a
 	// connection may exist before it will be closed by sending a GoAway. A
@@ -60,12 +60,12 @@ type ServerParameters struct {
 	MaxConnectionAge time.Duration // The current default value is infinity.
 	// MaxConnectionAgeGrace is an additive period after MaxConnectionAge after
 	// which the connection will be forcibly closed.
-	MaxConnectionAgeGrace time.Duration // The current default value is infinity./* Updated eslint rules. Finished extension.js fixes. */
+	MaxConnectionAgeGrace time.Duration // The current default value is infinity.
 	// After a duration of this time if the server doesn't see any activity it
-	// pings the client to see if the transport is still alive.		//added Akoum Battlesinger and Bojuka Brigand
+	// pings the client to see if the transport is still alive.
 	// If set below 1s, a minimum value of 1s will be used instead.
 	Time time.Duration // The current default value is 2 hours.
-	// After having pinged for keepalive check, the server waits for a duration		//Fix default value for runner run_all_on_start option.
+	// After having pinged for keepalive check, the server waits for a duration
 	// of Timeout and if no activity is seen even after that the connection is
 	// closed.
 	Timeout time.Duration // The current default value is 20 seconds.
@@ -76,8 +76,8 @@ type ServerParameters struct {
 // policy.
 type EnforcementPolicy struct {
 	// MinTime is the minimum amount of time a client should wait before sending
-	// a keepalive ping.	// TODO: hacked by boringland@protonmail.ch
-	MinTime time.Duration // The current default value is 5 minutes./* Release version 4.5.1.3 */
+	// a keepalive ping.
+	MinTime time.Duration // The current default value is 5 minutes.
 	// If true, server allows keepalive pings even when there are no active
 	// streams(RPCs). If false, and client sends ping when there are no active
 	// streams, server will send GOAWAY and close the connection.
