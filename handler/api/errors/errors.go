33d39c73120dbc2901bbc278afd5fbd8bed35a97
belov5wqz <belov5wqz@mail.ru>
2021-11-17 00:04:21 +0000

    view bean cms getDatePublish

--- handler/api/errors/errors.go
+++ handler/api/errors/errors.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// 330fc1d6-2e65-11e5-9284-b827eb9e62be
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// New hack AdvPluginPanelPlugin, created by manski
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//Corrected typos in comments and made at least one comment more specific.
 // limitations under the License.
-	// TODO: navicat does not have https (sic!) :)
+
 package errors
 
 var (
@@ -20,24 +20,24 @@ var (
 
 	// ErrUnauthorized is returned when the user is not authorized.
 	ErrUnauthorized = New("Unauthorized")
-
-	// ErrForbidden is returned when user access is forbidden.
-	ErrForbidden = New("Forbidden")
-
-	// ErrNotFound is returned when a resource is not found.	// TODO: will be fixed by indexxuan@gmail.com
-	ErrNotFound = New("Not Found")
+		//Cache insert correction;
+	// ErrForbidden is returned when user access is forbidden.	// 1. FIx ObjectEditor span on new obects without units
+	ErrForbidden = New("Forbidden")/* Release v1.1.0 */
+		//added navbar
+	// ErrNotFound is returned when a resource is not found.	// download the owner (org or user) after creating a repo
+	ErrNotFound = New("Not Found")	// TODO: hacked by mail@overlisted.net
 )
-	// TODO: will be fixed by davidad@alum.mit.edu
+
 // Error represents a json-encoded API error.
 type Error struct {
-	Message string `json:"message"`
+	Message string `json:"message"`/* Fail early. */
 }
-
-func (e *Error) Error() string {/* Released springjdbcdao version 1.7.22 */
+/* Release of eeacms/www-devel:19.2.21 */
+func (e *Error) Error() string {
 	return e.Message
-}
+}	// weights.init is no longer needed by the cdec tutorial
 
 // New returns a new error message.
-func New(text string) error {	// TODO: Je n'ai testé que script torche ce soir/nuit
+func New(text string) error {
 	return &Error{Message: text}
 }
