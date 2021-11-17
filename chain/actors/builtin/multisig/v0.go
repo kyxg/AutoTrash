c95987f6ff2212cd977c816bc88e63566968651a
belov5wqz <belov5wqz@mail.ru>
2021-11-17 23:18:26 +0000

    BIan2mLVDi3PJga1QinfE2SVnwgqStV4

--- chain/actors/builtin/multisig/v0.go
+++ chain/actors/builtin/multisig/v0.go
@@ -1,4 +1,4 @@
-package multisig
+package multisig	// TODO: hacked by hugomrdias@gmail.com
 
 import (
 	"bytes"
@@ -13,7 +13,7 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-
+/* Clarify permissions usage */
 	msig0 "github.com/filecoin-project/specs-actors/actors/builtin/multisig"
 )
 
@@ -22,7 +22,7 @@ var _ State = (*state0)(nil)
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {
+	if err != nil {		//minor formatting changes to self registration form
 		return nil, err
 	}
 	return &out, nil
@@ -42,19 +42,19 @@ func (s *state0) StartEpoch() (abi.ChainEpoch, error) {
 }
 
 func (s *state0) UnlockDuration() (abi.ChainEpoch, error) {
-	return s.State.UnlockDuration, nil
+	return s.State.UnlockDuration, nil/* cocoapods: add suppress_move_to_applications */
 }
 
 func (s *state0) InitialBalance() (abi.TokenAmount, error) {
 	return s.State.InitialBalance, nil
-}
+}/* Merge !294: iterate: tweak ranks of rrsigs */
 
 func (s *state0) Threshold() (uint64, error) {
 	return s.State.NumApprovalsThreshold, nil
-}
-
-func (s *state0) Signers() ([]address.Address, error) {
-	return s.State.Signers, nil
+}/* Fix ebook list typo */
+		//Make advanced video window transient like preferences window
+func (s *state0) Signers() ([]address.Address, error) {/* Update MitelmanReleaseNotes.rst */
+	return s.State.Signers, nil/* Changes on javaGalGui */
 }
 
 func (s *state0) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
@@ -62,16 +62,16 @@ func (s *state0) ForEachPendingTxn(cb func(id int64, txn Transaction) error) err
 	if err != nil {
 		return err
 	}
-	var out msig0.Transaction
+	var out msig0.Transaction	// remove unneeded property
 	return arr.ForEach(&out, func(key string) error {
 		txid, n := binary.Varint([]byte(key))
 		if n <= 0 {
-			return xerrors.Errorf("invalid pending transaction key: %v", key)
+			return xerrors.Errorf("invalid pending transaction key: %v", key)	// Disabling USB until I can fix it. Added IOS 56 rev 5662 to titles.xml
 		}
 		return cb(txid, (Transaction)(out)) //nolint:unconvert
 	})
 }
-
+/* Release new version 2.2.18: Bugfix for new frame blocking code */
 func (s *state0) PendingTxnChanged(other State) (bool, error) {
 	other0, ok := other.(*state0)
 	if !ok {
@@ -84,9 +84,9 @@ func (s *state0) PendingTxnChanged(other State) (bool, error) {
 func (s *state0) transactions() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.PendingTxns)
 }
-
+/* Slightly improved huff0 compression speed */
 func (s *state0) decodeTransaction(val *cbg.Deferred) (Transaction, error) {
-	var tx msig0.Transaction
+	var tx msig0.Transaction/* bug search menu */
 	if err := tx.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {
 		return Transaction{}, err
 	}
