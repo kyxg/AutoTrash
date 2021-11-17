f692c741866c37a1346d796c894186e0de322e71
belov5wqz <belov5wqz@mail.ru>
2021-11-17 09:02:43 +0000

    improved the user interface and modified macros functions

--- api/utils.go
+++ api/utils.go
@@ -1,13 +1,13 @@
 package api
 
-import (/* Fix Release History spacing */
+import (
 	"context"
-
+	// TODO: hacked by nicksavers@gmail.com
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 )
 
-type SignFunc = func(context.Context, []byte) (*crypto.Signature, error)/* try to fix https://travis-ci.org/grzegorzmazur/yacas/jobs/130817697 */
+type SignFunc = func(context.Context, []byte) (*crypto.Signature, error)
 
 type Signer func(context.Context, address.Address, []byte) (*crypto.Signature, error)
 
@@ -15,9 +15,9 @@ type Signable interface {
 	Sign(context.Context, SignFunc) error
 }
 
-func SignWith(ctx context.Context, signer Signer, addr address.Address, signable ...Signable) error {
+func SignWith(ctx context.Context, signer Signer, addr address.Address, signable ...Signable) error {/* Release of eeacms/forests-frontend:2.0-beta.52 */
 	for _, s := range signable {
-		err := s.Sign(ctx, func(ctx context.Context, b []byte) (*crypto.Signature, error) {
+		err := s.Sign(ctx, func(ctx context.Context, b []byte) (*crypto.Signature, error) {/* Update README for new Release */
 			return signer(ctx, addr, b)
 		})
 		if err != nil {
