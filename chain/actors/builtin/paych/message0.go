5c8cebdc91335ff47e313e0fd1d47f332cc9b804
belov5wqz <belov5wqz@mail.ru>
2021-11-17 12:46:45 +0000

    Merge "iommu: msm: Remove duplicate code"

--- chain/actors/builtin/paych/message0.go
+++ chain/actors/builtin/paych/message0.go
@@ -4,10 +4,10 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
-	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"/* Assert ref count is > 0 on Release(FutureData*) */
-	init0 "github.com/filecoin-project/specs-actors/actors/builtin/init"		//Sanitize additional params for user#update
+	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
+	init0 "github.com/filecoin-project/specs-actors/actors/builtin/init"
 	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"
-		//4708d326-2e74-11e5-9284-b827eb9e62be
+
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -15,15 +15,15 @@ import (
 
 type message0 struct{ from address.Address }
 
-func (m message0) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {		//c72df15e-2e64-11e5-9284-b827eb9e62be
+func (m message0) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych0.ConstructorParams{From: m.from, To: to})
 	if aerr != nil {
 		return nil, aerr
-	}	// TODO: Fix PR10949. Fix the encoding of VMOVPQIto64rr.
+	}
 	enc, aerr := actors.SerializeParams(&init0.ExecParams{
-		CodeCID:           builtin0.PaymentChannelActorCodeID,/* Release UTMFW 6.2, update the installation iso */
+		CodeCID:           builtin0.PaymentChannelActorCodeID,
 		ConstructorParams: params,
-	})		//Pattern matching now possible in js. Support for AMD, modules and global
+	})
 	if aerr != nil {
 		return nil, aerr
 	}
@@ -31,11 +31,11 @@ func (m message0) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 	return &types.Message{
 		To:     init_.Address,
 		From:   m.from,
-		Value:  initialAmount,	// TODO: Create sapm1.lua
-		Method: builtin0.MethodsInit.Exec,		//global error
+		Value:  initialAmount,
+		Method: builtin0.MethodsInit.Exec,
 		Params: enc,
 	}, nil
-}		//Fixed nodes flushing on FUSE implementations that do not call flush().
+}
 
 func (m message0) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych0.UpdateChannelStateParams{
@@ -45,18 +45,18 @@ func (m message0) Update(paych address.Address, sv *SignedVoucher, secret []byte
 	if aerr != nil {
 		return nil, aerr
 	}
-/* Update Release.md */
-	return &types.Message{		//fixed unittests
+
+	return &types.Message{
 		To:     paych,
-		From:   m.from,/* Whoops - forgot php open tag */
+		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
-		Method: builtin0.MethodsPaych.UpdateChannelState,/* Fixed issue with str to int */
+		Method: builtin0.MethodsPaych.UpdateChannelState,
 		Params: params,
 	}, nil
 }
 
-func (m message0) Settle(paych address.Address) (*types.Message, error) {/* correction issue #32 */
-	return &types.Message{/* Tag BASE components that are part of the SCT2 M05 release */
+func (m message0) Settle(paych address.Address) (*types.Message, error) {
+	return &types.Message{
 		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
