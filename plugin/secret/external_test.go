a7eb066981d2da5fba3580a6e5262b1aa9821434
belov5wqz <belov5wqz@mail.ru>
2021-11-17 05:09:11 +0000

    Small changes to parameters

--- plugin/secret/external_test.go
+++ plugin/secret/external_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Added final scripts to the release directory.
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-		//Update make_gff.pl
+// that can be found in the LICENSE file./* 0.19.6: Maintenance Release (close #70) */
+
 package secret
