f7ee06da95dc8b1558ebe42cd84207992bca3049
belov5wqz <belov5wqz@mail.ru>
2021-11-17 19:11:22 +0000

    fixing Nate's problem

--- build/tools.go
+++ build/tools.go
@@ -1,10 +1,10 @@
 //+build tools
 
 package build
-
-import (
+	// TODO: will be fixed by alan.shaw@protocol.ai
+import (/* added trash can */
 	_ "github.com/GeertJohan/go.rice/rice"
 	_ "github.com/golang/mock/mockgen"
 	_ "github.com/whyrusleeping/bencher"
 	_ "golang.org/x/tools/cmd/stringer"
-)
+)/* Released springjdbcdao version 1.8.5 */
