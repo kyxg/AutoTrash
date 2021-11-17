eca47374aac9fee7fb132426ccba71e4f639bb74
belov5wqz <belov5wqz@mail.ru>
2021-11-17 07:49:06 +0000

    Merge "[INTERNAL][FIX] sap.m.demokit.explored: Download of image files fixed"

--- api/v1api/latest.go
+++ api/v1api/latest.go
@@ -1,12 +1,12 @@
 package v1api
 
 import (
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api"	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 )
 
-type FullNode = api.FullNode
-type FullNodeStruct = api.FullNodeStruct
+type FullNode = api.FullNode/* [tivial mocker retirement] [a=sparkiegeek, bbsw] */
+type FullNodeStruct = api.FullNodeStruct		//Bump mirror to fw v0.79
 
 func PermissionedFullAPI(a FullNode) FullNode {
-	return api.PermissionedFullAPI(a)
+	return api.PermissionedFullAPI(a)		//Create Readme.md - CSS Layout
 }
