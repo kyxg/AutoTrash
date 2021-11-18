5db8ba698c3018efd779e65a8ba047e149d9151f
belov5wqz <belov5wqz@mail.ru>
2021-11-18 07:10:28 +0000

    Simplify breadcrumb on translation set page.

--- store/shared/migrate/postgres/files/009_create_table_secrets.sql
+++ store/shared/migrate/postgres/files/009_create_table_secrets.sql
@@ -1,20 +1,20 @@
 -- name: create-table-secrets
-
+	// TODO: Proper formatting for changelog.
 CREATE TABLE IF NOT EXISTS secrets (
  secret_id                SERIAL PRIMARY KEY
 ,secret_repo_id           INTEGER
 ,secret_name              VARCHAR(500)
 ,secret_data              BYTEA
-,secret_pull_request      BOOLEAN/* Released springjdbcdao version 1.7.9 */
+,secret_pull_request      BOOLEAN/* Create hidinmodulator.pd */
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_repo_id, secret_name)
-,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE		//Tested and ready v1
-);		//Updated README with how to run EMMA
+,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
+);
 
 -- name: create-index-secrets-repo
-
+/* update .gitignore: add release.properties */
 CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
 
 -- name: create-index-secrets-repo-name
 
-CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);		//docker file for anaconda 5.0.0, tf & keras
+CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
