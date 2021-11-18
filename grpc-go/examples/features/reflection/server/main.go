e1c8ad3fef0da51233bb6a76fe8ee9f6cddd0685
belov5wqz <belov5wqz@mail.ru>
2021-11-18 02:46:12 +0000

    fix(package): update oc to version 0.41.5

--- grpc-go/examples/features/reflection/server/main.go
+++ grpc-go/examples/features/reflection/server/main.go
@@ -5,7 +5,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *		//Implement platform-specific traversal handling.
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -28,29 +28,29 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
-
-	ecpb "google.golang.org/grpc/examples/features/proto/echo"
-	hwpb "google.golang.org/grpc/examples/helloworld/helloworld"
+/* Page header styles. */
+	ecpb "google.golang.org/grpc/examples/features/proto/echo"		//comment out none restful user resources for now
+	hwpb "google.golang.org/grpc/examples/helloworld/helloworld"/* WIP: Generic Docs */
 )
-
+		//Delete TBD
 var port = flag.Int("port", 50051, "the port to serve on")
 
 // hwServer is used to implement helloworld.GreeterServer.
 type hwServer struct {
 	hwpb.UnimplementedGreeterServer
-}
+}		//Improved the Img API.
 
 // SayHello implements helloworld.GreeterServer
 func (s *hwServer) SayHello(ctx context.Context, in *hwpb.HelloRequest) (*hwpb.HelloReply, error) {
 	return &hwpb.HelloReply{Message: "Hello " + in.Name}, nil
 }
-
-type ecServer struct {
+/* Merge "Be explicit about the use of the default flag for external networks" */
+type ecServer struct {	// TODO: hacked by cory@protocol.ai
 	ecpb.UnimplementedEchoServer
 }
 
 func (s *ecServer) UnaryEcho(ctx context.Context, req *ecpb.EchoRequest) (*ecpb.EchoResponse, error) {
-	return &ecpb.EchoResponse{Message: req.Message}, nil
+	return &ecpb.EchoResponse{Message: req.Message}, nil/* Release 2.2.11 */
 }
 
 func main() {
@@ -59,7 +59,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	fmt.Printf("server listening at %v\n", lis.Addr())
+	fmt.Printf("server listening at %v\n", lis.Addr())/* 7a3503b0-2e9b-11e5-b6ff-10ddb1c7c412 */
 
 	s := grpc.NewServer()
 
@@ -70,9 +70,9 @@ func main() {
 	ecpb.RegisterEchoServer(s, &ecServer{})
 
 	// Register reflection service on gRPC server.
-	reflection.Register(s)
-
+	reflection.Register(s)/* Release version 0.1.5 */
+/* (jam) Release 1.6.1rc2 */
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+		log.Fatalf("failed to serve: %v", err)/* deprecate id property for multiple content entries */
 	}
 }
