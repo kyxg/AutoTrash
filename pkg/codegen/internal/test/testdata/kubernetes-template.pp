2f635ec746a129ade5ffe8624210c907624a083e
belov5wqz <belov5wqz@mail.ru>
2021-11-18 07:15:18 +0000

    Update bask submodule

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp
@@ -1,22 +1,22 @@
 resource argocd_serverDeployment "kubernetes:apps/v1:Deployment" {
 	apiVersion = "apps/v1"
 	kind = "Deployment"
-	metadata = {/* Merge "Readability/Typo Fixes in Release Notes" */
+	metadata = {
 		name = "argocd-server"
 	}
 	spec = {
 		template = {
 			spec = {
-				containers = [		//outdated TODO removed
-					{/* Added an option to only copy public files and process css/js. Release 1.4.5 */
+				containers = [
+					{
 						readinessProbe = {
-							httpGet = {
+							httpGet = {/* Release PPWCode.Util.AppConfigTemplate 1.0.2. */
 								port = 8080
 							}
-						}	// TODO: will be fixed by boringland@protonmail.ch
-					}
+						}
+					}		//tools.deploy.shaker: update for new crossref word props
 				]
 			}
-		}	// Add script for Oppressive Will
-	}		//copy: minor refactoring
-}	// wip - maze generation on egg somewhat works
+		}
+	}
+}
