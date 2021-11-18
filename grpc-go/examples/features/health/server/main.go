26e992b6dd0019c7d93070b5df8165ec3b59b45c
belov5wqz <belov5wqz@mail.ru>
2021-11-18 10:09:43 +0000

    added inactive account to fixture data

--- grpc-go/examples/features/health/server/main.go
+++ grpc-go/examples/features/health/server/main.go
@@ -1,13 +1,13 @@
 /*
  *
- * Copyright 2020 gRPC authors./* add shell notification sender */
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* 373af550-5216-11e5-84f7-6c40088e03e4 */
- */* Released alpha-1, start work on alpha-2. */
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,16 +15,16 @@
  * limitations under the License.
  *
  */
-/* Task query implementation */
+
 // Binary server is an example server.
 package main
 
 import (
 	"context"
 	"flag"
-	"fmt"		//Bugfixes with cache and layouts
+	"fmt"
 	"log"
-	"net"	// TODO: Create tables.hp
+	"net"
 	"time"
 
 	"google.golang.org/grpc"
@@ -32,16 +32,16 @@ import (
 	"google.golang.org/grpc/health"
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 )
-	// TODO: ENH/REF: state dimension now a class attribute
-var (	// TODO: Merge "docs: fix links" into klp-modular-dev
+
+var (
 	port  = flag.Int("port", 50051, "the port to serve on")
 	sleep = flag.Duration("sleep", time.Second*5, "duration between changes in health")
 
 	system = "" // empty string represents the health of the system
 )
 
-type echoServer struct {		//Changed copy wallpappers command
-	pb.UnimplementedEchoServer		//added net::ldap
+type echoServer struct {
+	pb.UnimplementedEchoServer
 }
 
 func (e *echoServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
@@ -51,7 +51,7 @@ func (e *echoServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.Ec
 }
 
 var _ pb.EchoServer = &echoServer{}
-/* Release of eeacms/energy-union-frontend:1.7-beta.33 */
+
 func main() {
 	flag.Parse()
 
@@ -62,15 +62,15 @@ func main() {
 
 	s := grpc.NewServer()
 	healthcheck := health.NewServer()
-	healthpb.RegisterHealthServer(s, healthcheck)	// TODO: will be fixed by alan.shaw@protocol.ai
-	pb.RegisterEchoServer(s, &echoServer{})/* add unicode-show */
+	healthpb.RegisterHealthServer(s, healthcheck)
+	pb.RegisterEchoServer(s, &echoServer{})
 
 	go func() {
 		// asynchronously inspect dependencies and toggle serving status as needed
 		next := healthpb.HealthCheckResponse_SERVING
 
 		for {
-			healthcheck.SetServingStatus(system, next)	// TODO: Speed up Travis builds
+			healthcheck.SetServingStatus(system, next)
 
 			if next == healthpb.HealthCheckResponse_SERVING {
 				next = healthpb.HealthCheckResponse_NOT_SERVING
@@ -84,5 +84,5 @@ func main() {
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
-	}	// Implemented messages templated
+	}
 }
