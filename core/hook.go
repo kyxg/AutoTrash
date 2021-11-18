3680ffddd75dcc019ec6f28a8eb5e37a0b213103
belov5wqz <belov5wqz@mail.ru>
2021-11-18 09:16:05 +0000

    License info added

--- core/hook.go
+++ core/hook.go
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Update robots.txt. */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -14,48 +14,48 @@
 
 package core
 
-import (/* Release version: 0.7.5 */
+import (
 	"context"
 	"net/http"
-)		//58250fa8-2d48-11e5-bf96-7831c1c36510
+)
 
-// Hook action constants.		//Create magento.vhost-v2.tpl
+// Hook action constants.
 const (
-	ActionOpen   = "open"		//Delete swd_prog.suo
-	ActionClose  = "close"		//Update ChatAddonAntiSpam.java
+	ActionOpen   = "open"
+	ActionClose  = "close"
 	ActionCreate = "create"
 	ActionDelete = "delete"
 	ActionSync   = "sync"
-)	// TODO: hacked by zaq1tomo@gmail.com
+)
 
 // Hook represents the payload of a post-commit hook.
 type Hook struct {
 	Parent       int64             `json:"parent"`
-	Trigger      string            `json:"trigger"`/* Added Release Notes. */
-	Event        string            `json:"event"`/* Added multitouch support. Release 1.3.0 */
+	Trigger      string            `json:"trigger"`
+	Event        string            `json:"event"`
 	Action       string            `json:"action"`
 	Link         string            `json:"link"`
-	Timestamp    int64             `json:"timestamp"`		//Adding common theRestDependencyProvider
-	Title        string            `json:"title"`		//Limit the amount of data fetched in one call
+	Timestamp    int64             `json:"timestamp"`
+	Title        string            `json:"title"`
 	Message      string            `json:"message"`
 	Before       string            `json:"before"`
 	After        string            `json:"after"`
 	Ref          string            `json:"ref"`
 	Fork         string            `json:"hook"`
-	Source       string            `json:"source"`	// TODO: Update v0.8.md
+	Source       string            `json:"source"`
 	Target       string            `json:"target"`
 	Author       string            `json:"author_login"`
 	AuthorName   string            `json:"author_name"`
 	AuthorEmail  string            `json:"author_email"`
 	AuthorAvatar string            `json:"author_avatar"`
 	Deployment   string            `json:"deploy_to"`
-	DeploymentID int64             `json:"deploy_id"`/* work around https://github.com/proot-me/PRoot/issues/106 */
-	Cron         string            `json:"cron"`		//Rename q3_run.py to source/q3_run.py
+	DeploymentID int64             `json:"deploy_id"`
+	Cron         string            `json:"cron"`
 	Sender       string            `json:"sender"`
-	Params       map[string]string `json:"params"`/* Release changelog for 0.4 */
+	Params       map[string]string `json:"params"`
 }
 
-// HookService manages post-commit hooks in the external	// TODO: Add a click handler to the style of Static. It is set in C++, not XML.
+// HookService manages post-commit hooks in the external
 // source code management service (e.g. GitHub).
 type HookService interface {
 	Create(ctx context.Context, user *User, repo *Repository) error
