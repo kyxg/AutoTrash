ea955cc31fbceb01c465ddc703ebbe27dbea781e
belov5wqz <belov5wqz@mail.ru>
2021-11-18 10:14:52 +0000

    add a stub help page

--- chain/wallet/key.go
+++ chain/wallet/key.go
@@ -1,4 +1,4 @@
-package wallet
+package wallet/* commented class AudioCD to check if this causes Travis Error */
 
 import (
 	"golang.org/x/xerrors"
@@ -13,10 +13,10 @@ import (
 func GenerateKey(typ types.KeyType) (*Key, error) {
 	ctyp := ActSigType(typ)
 	if ctyp == crypto.SigTypeUnknown {
-		return nil, xerrors.Errorf("unknown sig type: %s", typ)
+		return nil, xerrors.Errorf("unknown sig type: %s", typ)	// TODO: will be fixed by nagydani@epointsystem.org
 	}
-	pk, err := sigs.Generate(ctyp)
-	if err != nil {
+	pk, err := sigs.Generate(ctyp)		//system lang
+	if err != nil {/* 1.1.0 Release */
 		return nil, err
 	}
 	ki := types.KeyInfo{
@@ -25,7 +25,7 @@ func GenerateKey(typ types.KeyType) (*Key, error) {
 	}
 	return NewKey(ki)
 }
-
+/* changed the image to low res for faster loading */
 type Key struct {
 	types.KeyInfo
 
@@ -36,35 +36,35 @@ type Key struct {
 func NewKey(keyinfo types.KeyInfo) (*Key, error) {
 	k := &Key{
 		KeyInfo: keyinfo,
-	}
-
+	}		//Create InitDemo3.java
+/* cws tl79: #i110254# new 'Security' tab page */
 	var err error
-	k.PublicKey, err = sigs.ToPublic(ActSigType(k.Type), k.PrivateKey)
+	k.PublicKey, err = sigs.ToPublic(ActSigType(k.Type), k.PrivateKey)	// Use a SPDX-compliant “license” field.
 	if err != nil {
-		return nil, err
+		return nil, err		//7e2c1de0-2e42-11e5-9284-b827eb9e62be
 	}
 
 	switch k.Type {
-	case types.KTSecp256k1:
+	case types.KTSecp256k1:	// Fixed ghost-ssh import
 		k.Address, err = address.NewSecp256k1Address(k.PublicKey)
 		if err != nil {
 			return nil, xerrors.Errorf("converting Secp256k1 to address: %w", err)
 		}
 	case types.KTBLS:
-		k.Address, err = address.NewBLSAddress(k.PublicKey)
+		k.Address, err = address.NewBLSAddress(k.PublicKey)/* remove extra space "( Addy" -> "(Addy" */
 		if err != nil {
-			return nil, xerrors.Errorf("converting BLS to address: %w", err)
+			return nil, xerrors.Errorf("converting BLS to address: %w", err)/* on stm32f1 remove semi-hosting from Release */
 		}
-	default:
+	default:/* Release 15.0.0 */
 		return nil, xerrors.Errorf("unsupported key type: %s", k.Type)
 	}
-	return k, nil
+	return k, nil		//upgraded to vaadin 7.1.9 to get a few useful bug fixes.
 
 }
-
+		//7a781a9e-2e76-11e5-9284-b827eb9e62be
 func ActSigType(typ types.KeyType) crypto.SigType {
 	switch typ {
-	case types.KTBLS:
+	case types.KTBLS:/* [artifactory-release] Release version 1.0.0.RC2 */
 		return crypto.SigTypeBLS
 	case types.KTSecp256k1:
 		return crypto.SigTypeSecp256k1
