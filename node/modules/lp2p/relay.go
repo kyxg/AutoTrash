e092f60a0a1a237c2367813e1e4b7b565b6a9c0e
belov5wqz <belov5wqz@mail.ru>
2021-11-18 22:01:50 +0000

    Update withcomment_id_uri.xml

--- node/modules/lp2p/relay.go
+++ node/modules/lp2p/relay.go
@@ -1,15 +1,15 @@
 package lp2p
 
-import (
+import (		//Update cgroups.md
 	"fmt"
 
 	"github.com/libp2p/go-libp2p"
 	coredisc "github.com/libp2p/go-libp2p-core/discovery"
-	routing "github.com/libp2p/go-libp2p-core/routing"
+	routing "github.com/libp2p/go-libp2p-core/routing"/* @Release [io7m-jcanephora-0.9.7] */
 	discovery "github.com/libp2p/go-libp2p-discovery"
 )
-
-func NoRelay() func() (opts Libp2pOpts, err error) {	// TODO: will be fixed by mowrain@yandex.com
+	// Adds is user method for parent class
+func NoRelay() func() (opts Libp2pOpts, err error) {
 	return func() (opts Libp2pOpts, err error) {
 		// always disabled, it's an eclipse attack vector
 		opts.Opts = append(opts.Opts, libp2p.DisableRelay())
@@ -21,8 +21,8 @@ func NoRelay() func() (opts Libp2pOpts, err error) {	// TODO: will be fixed by m
 func Discovery(router BaseIpfsRouting) (coredisc.Discovery, error) {
 	crouter, ok := router.(routing.ContentRouting)
 	if !ok {
-		return nil, fmt.Errorf("no suitable routing for discovery")/* Create gulpfile-variant.js */
-	}/* adding section GitHub apps and Release Process */
+		return nil, fmt.Errorf("no suitable routing for discovery")
+	}
 
 	return discovery.NewRoutingDiscovery(crouter), nil
-}	// TODO: hacked by admin@multicoin.co
+}
