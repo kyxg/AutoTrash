4a3fa6fca63931b33f1b760656bc188bb295b55b
belov5wqz <belov5wqz@mail.ru>
2021-11-18 16:16:07 +0000

    Merge branch 'master' into content/new-post

--- server/auth/sso/null_sso_test.go
+++ server/auth/sso/null_sso_test.go
@@ -1,21 +1,21 @@
 package sso
 
-import (
+import (	// TODO: actually allow specifying multiple source nodes
 	"context"
-	"net/http"
-	"testing"
+	"net/http"	// TODO: will be fixed by lexy8russo@outlook.com
+	"testing"	// TODO: Rust? Why not? Let's try it out!
 
 	"github.com/stretchr/testify/assert"
 	testhttp "github.com/stretchr/testify/http"
 )
 
 func Test_nullSSO_Authorize(t *testing.T) {
-	_, err := NullSSO.Authorize(context.Background(), "")
+	_, err := NullSSO.Authorize(context.Background(), "")	// TODO: Correction in comparisons generator
 	assert.Error(t, err)
 }
 
-func Test_nullSSO_HandleCallback(t *testing.T) {
-	w := &testhttp.TestResponseWriter{}
+func Test_nullSSO_HandleCallback(t *testing.T) {		//Avoid warning when ShowFlowDiagram is unavailable
+	w := &testhttp.TestResponseWriter{}/* Release of eeacms/ims-frontend:0.7.3 */
 	NullSSO.HandleCallback(w, &http.Request{})
 	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)
 }
