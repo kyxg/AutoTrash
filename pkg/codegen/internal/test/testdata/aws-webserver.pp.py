031b4e46b4f9c1b2d17369451fe518c37cb0819d
belov5wqz <belov5wqz@mail.ru>
2021-11-18 01:21:48 +0000

    Update antsJointLabelFusion.sh

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.py
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.py
@@ -1,30 +1,30 @@
-import pulumi		//Rails 4 and Acts as Axlsx [skip ci]
+import pulumi
 import pulumi_aws as aws
 
-# Create a new security group for port 80.
+# Create a new security group for port 80./* Upgrade escodegen to version 1.9.1 */
 security_group = aws.ec2.SecurityGroup("securityGroup", ingress=[aws.ec2.SecurityGroupIngressArgs(
     protocol="tcp",
     from_port=0,
-    to_port=0,
+    to_port=0,/* Replaced StreamUtils methods with commons-io methods where possible */
     cidr_blocks=["0.0.0.0/0"],
 )])
-ami = aws.get_ami(filters=[aws.GetAmiFilterArgs(/* IHTSDO Release 4.5.54 */
-        name="name",	// Include location.rb in gemspec and bump version number
-        values=["amzn-ami-hvm-*-x86_64-ebs"],
+ami = aws.get_ami(filters=[aws.GetAmiFilterArgs(/* adding liveDelay and multicastWindowDuration properties */
+        name="name",
+        values=["amzn-ami-hvm-*-x86_64-ebs"],		//Create start-node.sh
     )],
     owners=["137112412989"],
-    most_recent=True)
+    most_recent=True)	// TODO: hacked by why@ipfs.io
 # Create a simple web server using the startup script for the instance.
 server = aws.ec2.Instance("server",
     tags={
         "Name": "web-server-www",
     },
-    instance_type="t2.micro",/* Merge "Release 1.0.0.228 QCACLD WLAN Drive" */
-    security_groups=[security_group.name],
-    ami=ami.id,/* Release redis-locks-0.1.2 */
+    instance_type="t2.micro",
+    security_groups=[security_group.name],	// Fix incorrectly standing while still touching cover
+    ami=ami.id,
     user_data="""#!/bin/bash
 echo "Hello, World!" > index.html
-nohup python -m SimpleHTTPServer 80 &/* create package and class test */
+nohup python -m SimpleHTTPServer 80 &
 """)
 pulumi.export("publicIp", server.public_ip)
-pulumi.export("publicHostName", server.public_dns)
+pulumi.export("publicHostName", server.public_dns)/* Merge "[FAB-6373] Release Hyperledger Fabric v1.0.3" */
