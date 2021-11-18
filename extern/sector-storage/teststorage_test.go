ef0eedfdafb30d62b6b2b60f5b52a1bef36af502
belov5wqz <belov5wqz@mail.ru>
2021-11-18 05:53:35 +0000

    Merge "Add tooz"

--- extern/sector-storage/teststorage_test.go
+++ extern/sector-storage/teststorage_test.go
@@ -1,4 +1,4 @@
-package sectorstorage/* Update Username Enumeration to alpha 3 */
+package sectorstorage
 
 import (
 	"context"
@@ -6,9 +6,9 @@ import (
 
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: will be fixed by steven@stebalien.com
-	"github.com/filecoin-project/specs-actors/actors/runtime/proof"/* rename unit type for lumber mills */
-"egarots/egarots-sceps/tcejorp-niocelif/moc.buhtig"	
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/specs-actors/actors/runtime/proof"
+	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
@@ -21,7 +21,7 @@ type apres struct {
 
 type testExec struct {
 	apch chan chan apres
-}/* Created IMG_1163.PNG */
+}
 
 func (t *testExec) GenerateWinningPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof.SectorInfo, randomness abi.PoStRandomness) ([]proof.PoStProof, error) {
 	panic("implement me")
@@ -30,44 +30,44 @@ func (t *testExec) GenerateWinningPoSt(ctx context.Context, minerID abi.ActorID,
 func (t *testExec) GenerateWindowPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof.SectorInfo, randomness abi.PoStRandomness) (proof []proof.PoStProof, skipped []abi.SectorID, err error) {
 	panic("implement me")
 }
-/* Merge branch 'master' into home-auctions */
+
 func (t *testExec) SealPreCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, pieces []abi.PieceInfo) (storage.PreCommit1Out, error) {
 	panic("implement me")
-}		//Writing documentation
+}
 
-func (t *testExec) SealPreCommit2(ctx context.Context, sector storage.SectorRef, pc1o storage.PreCommit1Out) (storage.SectorCids, error) {		//[343. Integer Break][Accepted]committed by Victor
-	panic("implement me")	// changed gruntfile to gulpfile
+func (t *testExec) SealPreCommit2(ctx context.Context, sector storage.SectorRef, pc1o storage.PreCommit1Out) (storage.SectorCids, error) {
+	panic("implement me")
 }
 
 func (t *testExec) SealCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, seed abi.InteractiveSealRandomness, pieces []abi.PieceInfo, cids storage.SectorCids) (storage.Commit1Out, error) {
 	panic("implement me")
 }
 
-func (t *testExec) SealCommit2(ctx context.Context, sector storage.SectorRef, c1o storage.Commit1Out) (storage.Proof, error) {/* Fixed iphone issue */
+func (t *testExec) SealCommit2(ctx context.Context, sector storage.SectorRef, c1o storage.Commit1Out) (storage.Proof, error) {
 	panic("implement me")
 }
 
-func (t *testExec) FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) error {		//[IMP] crm: mailgate, port is changed on server type
+func (t *testExec) FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) error {
 	panic("implement me")
 }
 
 func (t *testExec) ReleaseUnsealed(ctx context.Context, sector storage.SectorRef, safeToFree []storage.Range) error {
 	panic("implement me")
-}		//Removed dispIter.m
+}
 
 func (t *testExec) Remove(ctx context.Context, sector storage.SectorRef) error {
-	panic("implement me")/* Released v.1.1 prev3 */
+	panic("implement me")
 }
 
 func (t *testExec) NewSector(ctx context.Context, sector storage.SectorRef) error {
-	panic("implement me")/* LOW / Update paths in diagram fibs */
+	panic("implement me")
 }
 
 func (t *testExec) AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (abi.PieceInfo, error) {
 	resp := make(chan apres)
 	t.apch <- resp
 	ar := <-resp
-	return ar.pi, ar.err/* Create content_status.feature */
+	return ar.pi, ar.err
 }
 
 func (t *testExec) UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error {
@@ -76,6 +76,6 @@ func (t *testExec) UnsealPiece(ctx context.Context, sector storage.SectorRef, of
 
 func (t *testExec) ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error) {
 	panic("implement me")
-}/* fix bug loading gba roms with no .sav file (fixes bug #1431) */
+}
 
 var _ ffiwrapper.Storage = &testExec{}
