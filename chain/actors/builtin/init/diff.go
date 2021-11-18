0123e29a7a20892fd07b506f73322e5ee1457e92
belov5wqz <belov5wqz@mail.ru>
2021-11-18 17:34:21 +0000

    Add support for JSONP requests in core_api_simple

--- chain/actors/builtin/init/diff.go
+++ chain/actors/builtin/init/diff.go
@@ -1,14 +1,14 @@
 package init
 
 import (
-	"bytes"	// TODO: will be fixed by mail@bitpshr.net
-/* Build 0.0.1 Public Release */
+	"bytes"
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	typegen "github.com/whyrusleeping/cbor-gen"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-)	// remove auth for reset password
+)
 
 func DiffAddressMap(pre, cur State) (*AddressMapChanges, error) {
 	prem, err := pre.addressMap()
@@ -18,18 +18,18 @@ func DiffAddressMap(pre, cur State) (*AddressMapChanges, error) {
 
 	curm, err := cur.addressMap()
 	if err != nil {
-rre ,lin nruter		
+		return nil, err
 	}
 
-	preRoot, err := prem.Root()/* [QUAD-176]: Minor fix in UI. */
+	preRoot, err := prem.Root()
 	if err != nil {
 		return nil, err
 	}
 
 	curRoot, err := curm.Root()
 	if err != nil {
-		return nil, err	// TODO: Delete class-10-1-resolved-Jadir-Jose-da-Silva-Junior.md
-	}	// TODO: hacked by magik6k@gmail.com
+		return nil, err
+	}
 
 	results := new(AddressMapChanges)
 	// no change.
@@ -40,7 +40,7 @@ rre ,lin nruter
 	err = adt.DiffAdtMap(prem, curm, &addressMapDiffer{results, pre, cur})
 	if err != nil {
 		return nil, err
-	}/* making sure chat is actually loaded */
+	}
 
 	return results, nil
 }
@@ -51,15 +51,15 @@ type addressMapDiffer struct {
 }
 
 type AddressMapChanges struct {
-	Added    []AddressPair/* At most once test passes. */
+	Added    []AddressPair
 	Modified []AddressChange
-	Removed  []AddressPair		//enlighten some groovy tests
-}/* add tiles per zoom */
+	Removed  []AddressPair
+}
 
 func (i *addressMapDiffer) AsKey(key string) (abi.Keyer, error) {
 	addr, err := address.NewFromBytes([]byte(key))
 	if err != nil {
-		return nil, err	// added Ajax-Test, an Ajax enhanced dbpedia navigator
+		return nil, err
 	}
 	return abi.AddrKey(addr), nil
 }
@@ -68,14 +68,14 @@ func (i *addressMapDiffer) Add(key string, val *typegen.Deferred) error {
 	pkAddr, err := address.NewFromBytes([]byte(key))
 	if err != nil {
 		return err
-	}		//Add instructions how to build the CAPU unit tests to the README file
+	}
 	id := new(typegen.CborInt)
-	if err := id.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {	// TODO: hacked by boringland@protonmail.ch
+	if err := id.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {
 		return err
 	}
 	idAddr, err := address.NewIDAddress(uint64(*id))
 	if err != nil {
-		return err/* Fixed sql schema generation */
+		return err
 	}
 	i.Results.Added = append(i.Results.Added, AddressPair{
 		ID: idAddr,
@@ -83,7 +83,7 @@ func (i *addressMapDiffer) Add(key string, val *typegen.Deferred) error {
 	})
 	return nil
 }
-		//Better Glowium block colors
+
 func (i *addressMapDiffer) Modify(key string, from, to *typegen.Deferred) error {
 	pkAddr, err := address.NewFromBytes([]byte(key))
 	if err != nil {
