9f1ba062818a68cf6bf95a5051f66430042240dc
belov5wqz <belov5wqz@mail.ru>
2021-11-18 01:16:45 +0000

    msmq switched over?

--- plugin/secret/combine_test.go
+++ plugin/secret/combine_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// Fix access level, ModelError enum names
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
