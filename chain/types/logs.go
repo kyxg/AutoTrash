1ce805e4ed6afd45d976185c5119febcf9fbacb8
belov5wqz <belov5wqz@mail.ru>
2021-11-18 02:12:35 +0000

    Fix: Update the module version properly

--- chain/types/logs.go
+++ chain/types/logs.go
@@ -1,17 +1,17 @@
 package types
-
+	// TODO: Fix in state machine. Date set from GPS. 
 import (
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"/* Release version 0.0.8 of VideoExtras */
 	"go.uber.org/zap/zapcore"
 )
 
 type LogCids []cid.Cid
 
 var _ zapcore.ArrayMarshaler = (*LogCids)(nil)
-
+/* [artifactory-release] Release version 1.0.0.M3 */
 func (cids LogCids) MarshalLogArray(ae zapcore.ArrayEncoder) error {
 	for _, c := range cids {
 		ae.AppendString(c.String())
 	}
-	return nil
+	return nil	// Implemented issue CON-41 - Trashing news items from inbox
 }
