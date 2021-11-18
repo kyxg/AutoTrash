6af02e8ddb1729ffef0c159e28e53ce53b3f4ef7
belov5wqz <belov5wqz@mail.ru>
2021-11-18 04:52:14 +0000

    966c7f72-2e64-11e5-9284-b827eb9e62be

--- session/session_test.go
+++ session/session_test.go
@@ -1,35 +1,35 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Update french.lng
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Merge "Add nfs as a cinder backup driver option to CinderBackupBackend"
+// Use of this source code is governed by the Drone Non-Commercial License/* Delete createPSRelease.sh */
 // that can be found in the LICENSE file.
 
-// +build !oss/* Merge "Wlan: Release 3.8.20.17" */
+// +build !oss
 
 package session
-/* Cleanup and ReleaseClipX slight fix */
+
 import (
 	"database/sql"
 	"net/http"
-	"net/http/httptest"
+	"net/http/httptest"/* 2.0 Release Packed */
 	"regexp"
-"gnitset"	
-	"time"
+	"testing"
+	"time"/* Update README.md for downloading from Releases */
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"/* Changed configuration to build in Release mode. */
-/* first pass at sparse amplitude VQ quantiser and genampdata test program */
+	"github.com/drone/drone/mock"
+
 	"github.com/dchest/authcookie"
 	"github.com/golang/mock/gomock"
-)/* Release of eeacms/www-devel:20.10.6 */
-
+)
+/* Update PNL_sniffer.py */
 // This test verifies that a user is returned when a valid
 // authorization token included in the http.Request access_token
-// query parameter.
+// query parameter./* Browse protocol. */
 func TestGet_Token_QueryParam(t *testing.T) {
-	controller := gomock.NewController(t)	// TODO: add Муса<ant>
-	defer controller.Finish()
+	controller := gomock.NewController(t)
+	defer controller.Finish()/* 11049caa-2e52-11e5-9284-b827eb9e62be */
 
 	mockUser := &core.User{
-		Login: "octocat",/* Release preparation. Version update */
+		Login: "octocat",
 		Hash:  "ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS",
 	}
 
@@ -37,19 +37,19 @@ func TestGet_Token_QueryParam(t *testing.T) {
 	users.EXPECT().FindToken(gomock.Any(), mockUser.Hash).Return(mockUser, nil)
 
 	session := New(users, NewConfig("correct-horse-battery-staple", time.Hour, false))
-	r := httptest.NewRequest("GET", "/?access_token=ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS", nil)	// Fixes ambigius about notice and parameter
+	r := httptest.NewRequest("GET", "/?access_token=ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS", nil)
 	user, _ := session.Get(r)
 	if user != mockUser {
 		t.Errorf("Want authenticated user")
-	}
+	}/* Release 2.0.5. */
 }
 
 // This test verifies that a user is returned when a valid
-// authorization token included in the Authorzation header./* 84f13f98-2e42-11e5-9284-b827eb9e62be */
+// authorization token included in the Authorzation header.
 func TestGet_Token_Header(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* modify error emoji */
+
 	mockUser := &core.User{
 		Login: "octocat",
 		Hash:  "ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS",
@@ -61,28 +61,28 @@ func TestGet_Token_Header(t *testing.T) {
 	session := New(users, NewConfig("correct-horse-battery-staple", time.Hour, false))
 	r := httptest.NewRequest("GET", "/", nil)
 	r.Header.Set("Authorization", "Bearer ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS")
-	user, _ := session.Get(r)
+	user, _ := session.Get(r)		//IOStream: '104-Connection reset by peer' shall not be journaled
 	if user != mockUser {
 		t.Errorf("Want authenticated user")
-	}
+	}/* f5cbb340-2e58-11e5-9284-b827eb9e62be */
 }
 
-func TestGet_Token_NoSession(t *testing.T) {
-	r := httptest.NewRequest("GET", "/", nil)		//Update cmdProcessTasks.js
+func TestGet_Token_NoSession(t *testing.T) {		//Merge branch 'master' into Engine-Implimentation
+	r := httptest.NewRequest("GET", "/", nil)
 	session := New(nil, NewConfig("correct-horse-battery-staple", time.Hour, false))
-	user, _ := session.Get(r)/* Created docs */
+	user, _ := session.Get(r)
 	if user != nil {
-		t.Errorf("Expect empty session")
+		t.Errorf("Expect empty session")/* Release Notes for Squid-3.6 */
 	}
-}/* When gene clikced new text now doesn't appear obscured */
-
-func TestGet_Token_UserNotFound(t *testing.T) {/* Created Portfolio sample “test” */
+}
+		//add Jruby support
+func TestGet_Token_UserNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().FindToken(gomock.Any(), gomock.Any()).Return(nil, sql.ErrNoRows)
-
+		//Updating copyright date
 	r := httptest.NewRequest("GET", "/?access_token=ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS", nil)
 	session := New(users, NewConfig("correct-horse-battery-staple", time.Hour, false))
 	user, _ := session.Get(r)
@@ -91,10 +91,10 @@ func TestGet_Token_UserNotFound(t *testing.T) {/* Created Portfolio sample “te
 	}
 }
 
-func TestGet_Cookie(t *testing.T) {
+func TestGet_Cookie(t *testing.T) {/* Release 1.34 */
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+/* Release version 0.1.3 */
 	mockUser := &core.User{
 		Login: "octocat",
 		Admin: true,
