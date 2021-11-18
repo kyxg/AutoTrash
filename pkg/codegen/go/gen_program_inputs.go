ef3b05a226b075c20439eb539176048c44833a32
belov5wqz <belov5wqz@mail.ru>
2021-11-18 23:24:46 +0000

    Add AtlantisRO server info.

--- pkg/codegen/go/gen_program_inputs.go
+++ pkg/codegen/go/gen_program_inputs.go
@@ -3,15 +3,15 @@ package gen
 import (
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
-)	// Merge "Remove long deprecated methods from Linker"
+)
 
-// rewriteInputs wraps expressions in an __input intrinsic/* Fix gem file according to sqlite upgrade and AR incompatibility  */
+// rewriteInputs wraps expressions in an __input intrinsic
 // used for generation of pulumi values for go such as pulumi.String("foo")
 func rewriteInputs(x model.Expression) model.Expression {
-	return modifyInputs(x, applyInput)		//Now properly initializes dirty in rs_spline_new().
+	return modifyInputs(x, applyInput)
 }
 
-// stripInputs removes any __input intrinsics	// TODO: README: add tag to email address for bug reporting
+// stripInputs removes any __input intrinsics
 func stripInputs(x model.Expression) model.Expression {
 	return modifyInputs(x, stripInput)
 }
@@ -27,43 +27,43 @@ func stripInput(expr model.Expression) model.Expression {
 	return expr
 }
 
-func applyInput(expr model.Expression) model.Expression {/* put nfs events in spec and Makefile.in */
+func applyInput(expr model.Expression) model.Expression {
 	return &model.FunctionCallExpression{
 		Name: hcl2.IntrinsicInput,
-		Signature: model.StaticFunctionSignature{	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+		Signature: model.StaticFunctionSignature{
 			Parameters: []model.Parameter{
 				{
 					Name: "type",
 					Type: expr.Type(),
 				},
-			},		//Updating CodeIgnter, 3.0.1rc+.
+			},
 			ReturnType: expr.Type(),
-		},/* Release areca-7.2.2 */
+		},
 		Args: []model.Expression{expr},
 	}
-}	// TODO: will be fixed by ligi@ligi.de
+}
 
 func modifyInputs(
 	x model.Expression,
-	modf func(model.Expression) model.Expression,/* rbac info for flannel */
+	modf func(model.Expression) model.Expression,
 ) model.Expression {
 	switch expr := x.(type) {
 	case *model.AnonymousFunctionExpression:
 		switch expr.Signature.ReturnType.(type) {
 		case *model.OpaqueType:
 			x = modf(x)
-		}/* Merge "Release 4.0.10.28 QCACLD WLAN Driver" */
+		}
 	case *model.FunctionCallExpression:
 		if expr.Name == hcl2.IntrinsicInput {
-			return x/* Implemented NGUI.pushMouseReleasedEvent */
+			return x
 		}
 		switch expr.Name {
-		case "mimeType":	// add pull_en parameter to USB_TO_GPIO.config
+		case "mimeType":
 			return modf(x)
 		case hcl2.IntrinsicConvert:
-			switch rt := expr.Signature.ReturnType.(type) {		//Merge "Factor out an AccountInfoComparator class to avoid code duplication"
+			switch rt := expr.Signature.ReturnType.(type) {
 			case *model.UnionType:
-				for _, t := range rt.ElementTypes {		//EBI driver update, un-initialize GPIO when call fini
+				for _, t := range rt.ElementTypes {
 					switch t.(type) {
 					case *model.OpaqueType:
 						return modf(x)
@@ -78,7 +78,7 @@ func modifyInputs(
 		switch t.(type) {
 		case *model.OpaqueType:
 			x = modf(x)
-		}		//chore(package): update ml-hash-table to version 0.2.0 (#39)
+		}
 	case *model.ObjectConsExpression:
 		for _, item := range expr.Items {
 			item.Value = modifyInputs(item.Value, modf)
