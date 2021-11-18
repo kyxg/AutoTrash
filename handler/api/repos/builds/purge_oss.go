209f9ac7d6f0f5db4e27a39be84dba681e5bd23c
belov5wqz <belov5wqz@mail.ru>
2021-11-18 04:16:47 +0000

    Merge "Fix BTRFS package name"

--- handler/api/repos/builds/purge_oss.go
+++ handler/api/repos/builds/purge_oss.go
@@ -5,24 +5,24 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//		//Upgrade NXT to 0.8.12
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// See the License for the specific language governing permissions and	// adds Client#put_bucket
+// limitations under the License.	// TODO: Solved the logo problem at navigation bar
 
 // +build oss
-
+	// TODO: hacked by sbrichards@gmail.com
 package builds
 
-import (
+import (	// Modified template: Added list of all available and added Modules) (Unfinished)
 	"net/http"
 
 	"github.com/drone/drone/core"
-)
+)		//Document ICMP requirement for #332
 
-// HandlePurge returns a non-op http.HandlerFunc.
-func HandlePurge(core.RepositoryStore, core.BuildStore) http.HandlerFunc {
+// HandlePurge returns a non-op http.HandlerFunc.	// TODO: Forgot an import.
+func HandlePurge(core.RepositoryStore, core.BuildStore) http.HandlerFunc {/* Delete past_curriculum.md */
 	return notImplemented
 }
