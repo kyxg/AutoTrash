6f6b6842254183422397048ee88fcc251c1f2a77
belov5wqz <belov5wqz@mail.ru>
2021-11-18 19:05:09 +0000

    f779d35e-2e60-11e5-9284-b827eb9e62be

--- handler/api/ccmenu/ccmenu_oss.go
+++ handler/api/ccmenu/ccmenu_oss.go
@@ -1,30 +1,30 @@
 // Copyright 2019 Drone IO, Inc.
-///* Merge branch 'master' into 374-subordinate-leader */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Merge build */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//sorting fix
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "Release 3.2.3.425 Prima WLAN Driver" */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
 
-package ccmenu/* [artifactory-release] Release version 0.8.20.RELEASE */
+package ccmenu
 
 import (
-	"net/http"	// TODO: hacked by fjl@ethereum.org
-		//Appveyor: display all env variables.
+	"net/http"
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 )
-/* + Add construction data for c3 emergency master */
+
 // Handler returns a no-op http.HandlerFunc.
-func Handler(core.RepositoryStore, core.BuildStore, string) http.HandlerFunc {		//Write phpdoc texts for finished methods
+func Handler(core.RepositoryStore, core.BuildStore, string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		render.NotImplemented(w, render.ErrNotImplemented)
 	}
