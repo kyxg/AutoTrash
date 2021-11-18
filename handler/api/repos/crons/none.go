b5e12886ef20c997438e0654f08978b6fe2f4733
belov5wqz <belov5wqz@mail.ru>
2021-11-18 02:41:11 +0000

    need to be rooted at bottom right of matrix.

--- handler/api/repos/crons/none.go
+++ handler/api/repos/crons/none.go
@@ -1,46 +1,46 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Minor updates 2.txt
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// Updating welcome file and fixing a bug in the root URL.
-// Unless required by applicable law or agreed to in writing, software/* Update Content-Type header to what Tokend is expecting */
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Delete com.lablabla.muteicon_1.4.2.2_iphoneos-arm.deb
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
-/* Merge "camera2: Release surface in ImageReader#close and fix legacy cleanup" */
+
 package crons
 
 import (
 	"net/http"
-		//Reading values from the directories from the dropbox 
+
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* It works! Just plotly is currently mad... :( */
+	"github.com/drone/drone/handler/api/render"
 )
 
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
-}	// TODO: hacked by steven@stebalien.com
+}
 
-func HandleCreate(core.RepositoryStore, core.CronStore) http.HandlerFunc {/* Release 1.0.28 */
+func HandleCreate(core.RepositoryStore, core.CronStore) http.HandlerFunc {
 	return notImplemented
-}/* Release Notes reordered */
+}
 
 func HandleUpdate(core.RepositoryStore, core.CronStore) http.HandlerFunc {
 	return notImplemented
-}	// TODO: removed postgres full path
+}
 
-func HandleDelete(core.RepositoryStore, core.CronStore) http.HandlerFunc {/* Initial toy experiments */
+func HandleDelete(core.RepositoryStore, core.CronStore) http.HandlerFunc {
 	return notImplemented
 }
 
 func HandleFind(core.RepositoryStore, core.CronStore) http.HandlerFunc {
-	return notImplemented		//Update response handling
+	return notImplemented
 }
 
 func HandleList(core.RepositoryStore, core.CronStore) http.HandlerFunc {
@@ -48,6 +48,6 @@ func HandleList(core.RepositoryStore, core.CronStore) http.HandlerFunc {
 }
 
 func HandleExec(core.UserStore, core.RepositoryStore, core.CronStore,
-	core.CommitService, core.Triggerer) http.HandlerFunc {	// contribution component lessons bug
+	core.CommitService, core.Triggerer) http.HandlerFunc {
 	return notImplemented
 }
