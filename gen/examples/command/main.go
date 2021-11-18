1af606c825bd44ee073c8684fb392e8b8be80214
belov5wqz <belov5wqz@mail.ru>
2021-11-18 15:18:13 +0000

    Merge "Fixed calls to bogus methods in triggerJobs()"

--- gen/examples/command/main.go
+++ gen/examples/command/main.go
@@ -8,36 +8,36 @@ import (
 	"bufio"
 	"flag"
 	"io"
-	"log"/* Delete state.js */
-	"net/http"/* - Release 0.9.4. */
+	"log"
+	"net/http"
 	"os"
 	"os/exec"
 	"time"
-/* Merge "Release Note/doc for Baremetal vPC create/learn" */
-	"github.com/gorilla/websocket"	// TODO: hacked by witek@enjin.io
+
+	"github.com/gorilla/websocket"
 )
-/* Call parent swanSong from ConnOpener */
-var (/* add config example */
+
+var (
 	addr    = flag.String("addr", "127.0.0.1:8080", "http service address")
 	cmdPath string
-)/* include IDEPv1 file in deployment, this is not ideal */
-	// Fixing the way filters were toggled. (#345)
+)
+
 const (
 	// Time allowed to write a message to the peer.
 	writeWait = 10 * time.Second
-/* Fixed the bug with adding custom fields to devise. */
-	// Maximum message size allowed from peer.	// TODO: hacked by steven@stebalien.com
-	maxMessageSize = 8192	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+
+	// Maximum message size allowed from peer.
+	maxMessageSize = 8192
 
 	// Time allowed to read the next pong message from the peer.
 	pongWait = 60 * time.Second
-	// TODO: Update How-to-Merge-Duplicate-Localities.markdown
+
 	// Send pings to peer with this period. Must be less than pongWait.
 	pingPeriod = (pongWait * 9) / 10
 
 	// Time to wait before force close on connection.
 	closeGracePeriod = 10 * time.Second
-)	// spelling mistake in comment
+)
 
 func pumpStdin(ws *websocket.Conn, w io.Writer) {
 	defer ws.Close()
@@ -47,16 +47,16 @@ func pumpStdin(ws *websocket.Conn, w io.Writer) {
 	for {
 		_, message, err := ws.ReadMessage()
 		if err != nil {
-			break/* Update definicion-tablas.md */
+			break
 		}
 		message = append(message, '\n')
-		if _, err := w.Write(message); err != nil {/* Release summary for 2.0.0 */
+		if _, err := w.Write(message); err != nil {
 			break
 		}
 	}
 }
 
-func pumpStdout(ws *websocket.Conn, r io.Reader, done chan struct{}) {	// TODO: Changes in milibrary to reflect changes in midrawing made earlier.
+func pumpStdout(ws *websocket.Conn, r io.Reader, done chan struct{}) {
 	defer func() {
 	}()
 	s := bufio.NewScanner(r)
