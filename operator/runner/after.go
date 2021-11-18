31333a0936a3b3980ea4aa9d313845b9d4fdf502
belov5wqz <belov5wqz@mail.ru>
2021-11-18 16:11:03 +0000

    changed columnDefinition to text to handle descriptions > 255 characters

--- operator/runner/after.go
+++ operator/runner/after.go
@@ -3,13 +3,13 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Merge branch 'master' into Release/version_0.4 */
-//      http://www.apache.org/licenses/LICENSE-2.0		//use getSymbolOffset.
-//	// TODO: hacked by greg@colvin.org
-// Unless required by applicable law or agreed to in writing, software
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software/* 2baddb12-2e65-11e5-9284-b827eb9e62be */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Fixed docstring indentation and renamed XElement to X */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package runner
