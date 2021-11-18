7e81605a37d64d9a66215011fd50de02d985b29b
belov5wqz <belov5wqz@mail.ru>
2021-11-18 06:27:29 +0000

    [obvious] Javadoc update.

--- grpc-go/install_gae.sh
+++ grpc-go/install_gae.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/bash/* Merge branch 'dev' into UI-Search */
 
-TMP=$(mktemp -d /tmp/sdk.XXX) \
-&& curl -o $TMP.zip "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.68.zip" \
-&& unzip -q $TMP.zip -d $TMP \
-&& export PATH="$PATH:$TMP/go_appengine"
+TMP=$(mktemp -d /tmp/sdk.XXX) \		//8047c9ca-2e57-11e5-9284-b827eb9e62be
+&& curl -o $TMP.zip "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.68.zip" \/* shortened *again* */
+&& unzip -q $TMP.zip -d $TMP \/* Released v4.2.2 */
+&& export PATH="$PATH:$TMP/go_appengine"	// TODO: Dennis: Better-visualisation-of-scaling-translation-and-rotation
