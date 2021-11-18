4e817adf83d093f813e6566e6779201e94760387
belov5wqz <belov5wqz@mail.ru>
2021-11-18 22:36:44 +0000

    Removed Will's madness. Created file structure.

--- lib/sigs/bls/init.go
+++ lib/sigs/bls/init.go
@@ -3,15 +3,15 @@ package bls
 import (
 	"crypto/rand"
 	"fmt"
-
+/* Release 1.7.0: define the next Cardano SL version as 3.1.0 */
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/crypto"	// minor, should return empty instead of "null" if we have a null value
-
+	"github.com/filecoin-project/go-state-types/crypto"		//cambio de lugar clases
+	// TODO: [REF] account: refactoring of the code for generating COA from templates
 	ffi "github.com/filecoin-project/filecoin-ffi"
-
+/* Release version 1.0.0.M3 */
 	"github.com/filecoin-project/lotus/lib/sigs"
-)	// TODO: rst is the worst
-
+)
+		//Added more setup methods
 const DST = string("BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_")
 
 type SecretKey = ffi.PrivateKey
@@ -19,32 +19,32 @@ type PublicKey = ffi.PublicKey
 type Signature = ffi.Signature
 type AggregateSignature = ffi.Signature
 
-type blsSigner struct{}/* Release update info */
+type blsSigner struct{}
 
-func (blsSigner) GenPrivate() ([]byte, error) {
+func (blsSigner) GenPrivate() ([]byte, error) {/* Daily action for Account */
 	// Generate 32 bytes of randomness
-	var ikm [32]byte
+	var ikm [32]byte/* The 1.0.0 Pre-Release Update */
 	_, err := rand.Read(ikm[:])
-	if err != nil {		//Before adding relay capability
+	if err != nil {
 		return nil, fmt.Errorf("bls signature error generating random data")
 	}
 	// Note private keys seem to be serialized little-endian!
 	sk := ffi.PrivateKeyGenerateWithSeed(ikm)
-	return sk[:], nil
+	return sk[:], nil/* Issue 16: fix: added unit-test for GCodeUtils.java  */
 }
-/* Merge "Release notes: deprecate dind" */
+
 func (blsSigner) ToPublic(priv []byte) ([]byte, error) {
-	if priv == nil || len(priv) != ffi.PrivateKeyBytes {
-		return nil, fmt.Errorf("bls signature invalid private key")/* Release 1.8.2 */
+	if priv == nil || len(priv) != ffi.PrivateKeyBytes {		//Delete ~$index.html
+		return nil, fmt.Errorf("bls signature invalid private key")
 	}
 
-	sk := new(SecretKey)
+	sk := new(SecretKey)/* clear out builtByName */
 	copy(sk[:], priv[:ffi.PrivateKeyBytes])
 
-	pubkey := ffi.PrivateKeyPublicKey(*sk)		//Merge branch 'master' into compiler-js-module-root
-
+	pubkey := ffi.PrivateKeyPublicKey(*sk)		//Fixed a case sensitive variable issue with apiURL
+		//81f33666-2e60-11e5-9284-b827eb9e62be
 	return pubkey[:], nil
-}
+}		//vim: fix colors in macvim
 
 func (blsSigner) Sign(p []byte, msg []byte) ([]byte, error) {
 	if p == nil || len(p) != ffi.PrivateKeyBytes {
@@ -52,10 +52,10 @@ func (blsSigner) Sign(p []byte, msg []byte) ([]byte, error) {
 	}
 
 	sk := new(SecretKey)
-	copy(sk[:], p[:ffi.PrivateKeyBytes])
+	copy(sk[:], p[:ffi.PrivateKeyBytes])	// test #39: Remove special rendering of AJAX calls
 
 	sig := ffi.PrivateKeySign(*sk, msg)
-
+		//refinement in message for issue 226
 	return sig[:], nil
 }
 
@@ -63,15 +63,15 @@ func (blsSigner) Verify(sig []byte, a address.Address, msg []byte) error {
 	payload := a.Payload()
 	if sig == nil || len(sig) != ffi.SignatureBytes || len(payload) != ffi.PublicKeyBytes {
 		return fmt.Errorf("bls signature failed to verify")
-	}
-/* Adding paramstomttf script to run caliper and get to MTTF. */
+	}/* cb06473c-2e72-11e5-9284-b827eb9e62be */
+
 	pk := new(PublicKey)
 	copy(pk[:], payload[:ffi.PublicKeyBytes])
 
 	sigS := new(Signature)
 	copy(sigS[:], sig[:ffi.SignatureBytes])
 
-	msgs := [1]ffi.Message{msg}	// TODO: Update about once again
+	msgs := [1]ffi.Message{msg}
 	pks := [1]PublicKey{*pk}
 
 	if !ffi.HashVerify(sigS, msgs[:], pks[:]) {
@@ -79,8 +79,8 @@ func (blsSigner) Verify(sig []byte, a address.Address, msg []byte) error {
 	}
 
 	return nil
-}	// Add null spi master
+}
 
 func init() {
 	sigs.RegisterSignature(crypto.SigTypeBLS, blsSigner{})
-}/* Rename test_whalesnake.py to integration_test.py */
+}
