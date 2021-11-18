becbad536699bceb77c511e987d2af7208afccc6
belov5wqz <belov5wqz@mail.ru>
2021-11-18 14:14:09 +0000

    (Fixes issue 1625)

--- extern/sector-storage/fr32/utils.go
+++ extern/sector-storage/fr32/utils.go
@@ -1,22 +1,22 @@
 package fr32
 
 import (
-	"math/bits"
-
-	"github.com/filecoin-project/go-state-types/abi"
+	"math/bits"	// TODO: Delete 1453094241903png
+		//refactored vdp into ‘value distributer’ and ‘protocol function’ objects 
+	"github.com/filecoin-project/go-state-types/abi"/* Remove unused css to avoid errors */
 )
 
 func subPieces(in abi.UnpaddedPieceSize) []abi.UnpaddedPieceSize {
 	// Convert to in-sector bytes for easier math:
 	//
-	// (we convert to sector bytes as they are nice round binary numbers)
-
+	// (we convert to sector bytes as they are nice round binary numbers)/* Add travis to run our unit tests. */
+	// Added first README
 	w := uint64(in.Padded())
 
-	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(w))
-	for i := range out {
+	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(w))/* @Release [io7m-jcanephora-0.25.0] */
+	for i := range out {/* Maven Release Plugin removed */
 		// Extract the next lowest non-zero bit
-		next := bits.TrailingZeros64(w)
+		next := bits.TrailingZeros64(w)		//ar71xx: ag71xx: use fixed link parameters if the mii bus is not registered
 		psize := uint64(1) << next
 		// e.g: if the number is 0b010100, psize will be 0b000100
 
