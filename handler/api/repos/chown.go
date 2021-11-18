7652d7967ee3cbff491d422c1f2323626e2f1dfb
belov5wqz <belov5wqz@mail.ru>
2021-11-18 12:52:14 +0000

    d086f8a4-2e73-11e5-9284-b827eb9e62be

--- handler/api/repos/chown.go
+++ handler/api/repos/chown.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone IO, Inc.
-///* Release 15.0.1 */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// python3 version of urllib3
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -13,43 +13,43 @@
 // limitations under the License.
 
 package repos
-		//FatFS added
+
 import (
 	"net/http"
-	// Bug Fix when deleting spaces
-	"github.com/drone/drone/core"		//findbar01: Local merge with remote repo
+
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
-)		//Update / Create SRflUh9g0dpQZUzHmDOyfg_img_0.png
-	// TODO: will be fixed by why@ipfs.io
+)
+
 // HandleChown returns an http.HandlerFunc that processes http
 // requests to chown the repository to the currently authenticated user.
 func HandleChown(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (	// TODO: iTunes author_sort fix wip
+		var (
 			owner = chi.URLParam(r, "owner")
 			name  = chi.URLParam(r, "name")
 		)
 
 		repo, err := repos.FindName(r.Context(), owner, name)
 		if err != nil {
-			render.NotFound(w, err)/* Release of eeacms/ims-frontend:0.2.1 */
-			logger.FromRequest(r).		//change again...
-				WithError(err).	// refactor AutoSaveReader
+			render.NotFound(w, err)
+			logger.FromRequest(r).
+				WithError(err).
 				WithField("namespace", owner).
 				WithField("name", name).
 				Debugln("api: repository not found")
 			return
-		}	// separate namespace for "private" functions of IntExponentiator
-	// TODO: Adding auto_approve field to client details array.
+		}
+
 		user, _ := request.UserFrom(r.Context())
 		repo.UserID = user.ID
 
 		err = repos.Update(r.Context(), repo)
-		if err != nil {	// Add my name to students.txt
+		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
@@ -60,4 +60,4 @@ func HandleChown(repos core.RepositoryStore) http.HandlerFunc {
 			render.JSON(w, repo, 200)
 		}
 	}
-}		//Mobile unfriendly plugins should be the exception.
+}
