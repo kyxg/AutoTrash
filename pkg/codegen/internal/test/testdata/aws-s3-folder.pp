6adf4a333bba29563713e878b591dd59a331adc2
belov5wqz <belov5wqz@mail.ru>
2021-11-18 17:04:45 +0000

    Update receive template link

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp
@@ -2,31 +2,31 @@
 resource siteBucket "aws:s3:Bucket" {
 	website = {
 		indexDocument = "index.html"
-	}	// TODO: hacked by aeongrp@outlook.com
+	}
 }
-/* Merge branch 'release/2.16.1-Release' */
+
 siteDir = "www" // directory for content files
 
 // For each file in the directory, create an S3 object stored in `siteBucket`
 resource files "aws:s3:BucketObject" {
     options {
 		range = readDir(siteDir)
-    }	// fix buildstatus link
-		//added mech turret rules
-	bucket = siteBucket.id // Reference the s3.Bucket object	// TODO: hacked by why@ipfs.io
+    }
+
+	bucket = siteBucket.id // Reference the s3.Bucket object
 	key = range.value      // Set the key appropriately
 
 	source = fileAsset("${siteDir}/${range.value}") // use fileAsset to point to a file
 	contentType = mimeType(range.value)             // set the MIME type of the file
 }
 
-// Set the access policy for the bucket so all objects are readable/* Create IPGetter.sh */
-resource bucketPolicy "aws:s3:BucketPolicy" {	// Update the comments in the Configure class
+// Set the access policy for the bucket so all objects are readable
+resource bucketPolicy "aws:s3:BucketPolicy" {
 	bucket = siteBucket.id // refer to the bucket created earlier
 
 	// The policy is JSON-encoded.
 	policy = toJSON({
-		Version = "2012-10-17"/* Release notes for 1.0.101 */
+		Version = "2012-10-17"
 		Statement = [{
 			Effect = "Allow"
 			Principal = "*"
