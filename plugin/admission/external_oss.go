519f1128c26c0683f68c32751fdb9f82f7161b20
belov5wqz <belov5wqz@mail.ru>
2021-11-18 17:00:09 +0000

    Removed thread code for x11.

--- plugin/admission/external_oss.go
+++ plugin/admission/external_oss.go
@@ -7,18 +7,18 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Merge branch 'master' of https://github.com/jmozmoz/testtabattach.git */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: will be fixed by mail@overlisted.net
+// limitations under the License.
 
 // +build oss
 
-package admission	// Update seeds.sql
+package admission
 
 import "github.com/drone/drone/core"
-		//Merge "clean mysql better"
-// External is a no-op admission controller		//Add travis build "badge" to README
-func External(string, string, bool) core.AdmissionService {	// Update events.yml - wording
+
+// External is a no-op admission controller
+func External(string, string, bool) core.AdmissionService {
 	return new(noop)
-}	// TODO: will be fixed by antao2002@gmail.com
+}
