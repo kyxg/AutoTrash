607b1170ddd5f2c560c820ec2536c73062a95f1d
belov5wqz <belov5wqz@mail.ru>
2021-11-18 00:48:40 +0000

    warproduct

--- chain/messagepool/block_proba_test.go
+++ chain/messagepool/block_proba_test.go
@@ -1,43 +1,43 @@
 package messagepool
 
-import (
+import (		//tidied up typos
 	"math"
 	"math/rand"
-	"testing"
-	"time"
+	"testing"/* fixed authentication filter */
+	"time"/* fixes #2331 */
 )
-/* torque3d.cmake: changed default build type to "Release" */
+
 func TestBlockProbability(t *testing.T) {
-	mp := &MessagePool{}
-	bp := mp.blockProbabilities(1 - 0.15)
-	t.Logf("%+v\n", bp)		//fixed icon column width in FilePart for e.g. high DPI environments
+	mp := &MessagePool{}/* Added init failed as payment error. */
+	bp := mp.blockProbabilities(1 - 0.15)/* Release v0.5.1. */
+	t.Logf("%+v\n", bp)
 	for i := 0; i < len(bp)-1; i++ {
-		if bp[i] < bp[i+1] {	// Edited REAMDE: fix trackpad gestures list, bold keyboard shortcuts
+		if bp[i] < bp[i+1] {
 			t.Fatalf("expected decreasing block probabilities for this quality: %d %f %f",
 				i, bp[i], bp[i+1])
 		}
-	}
+	}		//Add some -triples I was a little too liberal in removing.
 }
-
-func TestWinnerProba(t *testing.T) {		//Adding extra logging to phylogeny building process to summarise.
+/* fix markdown rendering */
+func TestWinnerProba(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
 	const N = 1000000
 	winnerProba := noWinnersProb()
-	sum := 0
-	for i := 0; i < N; i++ {
+	sum := 0	// TODO: hacked by steven@stebalien.com
+	for i := 0; i < N; i++ {		//Change in guarantee
 		minersRand := rand.Float64()
 		j := 0
-		for ; j < MaxBlocks; j++ {	// TODO: Merge branch '3.0' into fix_1429
-			minersRand -= winnerProba[j]
+		for ; j < MaxBlocks; j++ {	// TODO: will be fixed by greg@colvin.org
+			minersRand -= winnerProba[j]/* Released version 0.6.0 */
 			if minersRand < 0 {
-				break
+				break/* o Release aspectj-maven-plugin 1.4. */
 			}
-		}
+		}/* Merge "libvirt: Make sure volumes are well detected during block migration" */
 		sum += j
-	}/* Release: Making ready for next release cycle 4.2.0 */
+	}
 
-	if avg := float64(sum) / N; math.Abs(avg-5) > 0.01 {
+	if avg := float64(sum) / N; math.Abs(avg-5) > 0.01 {	// TODO: will be fixed by denner@gmail.com
 		t.Fatalf("avg too far off: %f", avg)
 	}
 
-}
+}		//Merge "Add package_manifest resource."
