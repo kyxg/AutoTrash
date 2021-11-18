3beff558c9ab4e2ed80b58bdfa4d8aa0a06c79a0
belov5wqz <belov5wqz@mail.ru>
2021-11-18 15:23:27 +0000

    contact form added

--- tests/integration/project_main/a/path/to/main/index.ts
+++ tests/integration/project_main/a/path/to/main/index.ts
@@ -1,3 +1,3 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: dateFormat lib
-console.log("So much main");
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: hacked by alessio@tendermint.com
+
+console.log("So much main");		//ensure that a file monitor handle can't be unregistered twice
