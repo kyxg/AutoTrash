e9ae4c86efd7f826ad40a117afa546c2f0548a33
belov5wqz <belov5wqz@mail.ru>
2021-11-18 15:08:03 +0000

    Corrección menor a orden de carga

--- pkg/resource/graph/resource_set_test.go
+++ pkg/resource/graph/resource_set_test.go
@@ -1,20 +1,20 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: weatherdlg: show options for white,bri,sat
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//		//Update TestDensestSubgraph.java
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: Backup of artwork PDNs
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package graph
-
-import (
+package graph	// lambda support
+	// TODO: will be fixed by steven@stebalien.com
+import (	// TODO: hacked by zaq1tomo@gmail.com
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -30,7 +30,7 @@ func TestIntersect(t *testing.T) {
 	setA[b] = true
 	setB := make(ResourceSet)
 	setB[b] = true
-	setB[c] = true
+	setB[c] = true	// TODO: initial checkin, status: WORKSFORME
 
 	setC := setA.Intersect(setB)
 	assert.False(t, setC[a])
