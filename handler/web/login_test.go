0be268237b6c59fb6e5b92eedd63b26fb22526b6
belov5wqz <belov5wqz@mail.ru>
2021-11-18 11:17:07 +0000

    Be paranoid and unlink build/bin before creating a new symlink

--- handler/web/login_test.go
+++ handler/web/login_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by nick@perfectabstractions.com
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
