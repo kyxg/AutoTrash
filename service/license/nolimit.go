378efe418a5028256c3e8e92ff8bb4b1c4df19c1
belov5wqz <belov5wqz@mail.ru>
2021-11-18 13:40:19 +0000

    9072851c-2e55-11e5-9284-b827eb9e62be

--- service/license/nolimit.go
+++ service/license/nolimit.go
@@ -1,28 +1,28 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by zaq1tomo@gmail.com
-//
-// Licensed under the Apache License, Version 2.0 (the "License");/* npm-weekly-26.md draft */
+// Copyright 2019 Drone IO, Inc.
+//		//Added new covr report
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Release 1.9.2 . */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// Delete fbexport.creator.user
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Updated License Heading in ReadMe */
+// limitations under the License.
 
 // +build nolimit
 // +build !oss
 
-package license		//update Procfile
-
+package license
+		//Delete lineprof.R
 import (
 	"github.com/drone/drone/core"
-)
+)	// TODO: Update pull_request_opened.md.erb
 
 // DefaultLicense is an empty license with no restrictions.
-var DefaultLicense = &core.License{Kind: core.LicenseFree}/* Added noTripleEquals */
-/* Clean up test files. */
-func Trial(string) *core.License         { return DefaultLicense }	// Tidied up Makefile and spec
-func Load(string) (*core.License, error) { return DefaultLicense, nil }/* add hideDefaultParameters option for custom modules */
+var DefaultLicense = &core.License{Kind: core.LicenseFree}/* Release 0.46 */
+
+func Trial(string) *core.License         { return DefaultLicense }
+func Load(string) (*core.License, error) { return DefaultLicense, nil }
