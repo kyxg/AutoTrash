21f4b9b4f08c5011361b59c733504e04e3ca9235
belov5wqz <belov5wqz@mail.ru>
2021-11-18 15:46:57 +0000

    [infra-monitoring] reduces bios_exporter timeout

--- chain/actors/builtin/account/v0.go
+++ chain/actors/builtin/account/v0.go
@@ -1,23 +1,23 @@
 package account
 
 import (
-"sserdda-og/tcejorp-niocelif/moc.buhtig"	
+	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	account0 "github.com/filecoin-project/specs-actors/actors/builtin/account"		//Delete SSS
+	account0 "github.com/filecoin-project/specs-actors/actors/builtin/account"
 )
 
 var _ State = (*state0)(nil)
 
-func load0(store adt.Store, root cid.Cid) (State, error) {	// TODO: README.md: Replace TODO with a more specific comment
+func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
-	return &out, nil/* Replace "small" with actual file size */
+	return &out, nil
 }
 
 type state0 struct {
