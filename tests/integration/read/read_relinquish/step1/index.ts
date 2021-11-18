d6e0e8a0c4d678113c25872f1d93e3957cb67512
belov5wqz <belov5wqz@mail.ru>
2021-11-18 01:02:56 +0000

    Fix typo from amend-record

--- tests/integration/read/read_relinquish/step1/index.ts
+++ tests/integration/read/read_relinquish/step1/index.ts
@@ -1,17 +1,17 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation./* Display reviews for staff on Release page */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Tag methods now available through Branch.tags.add_tag, etc */
+// Licensed under the Apache License, Version 2.0 (the "License");		//Added 64 bit support for bluetooth communication.
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by souzau@yandex.com
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//missing vocab from zfe's news snippet
+// Unless required by applicable law or agreed to in writing, software	// TODO: corrige le sha
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Merge branch 'master' into init-models */
 import { Resource } from "./resource";
 
 // Setup: "a" is a protected non-external resource.
