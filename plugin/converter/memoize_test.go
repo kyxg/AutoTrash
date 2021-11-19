577ef7e96561fc9008f589516a9c98d551ab23c5
belov5wqz <belov5wqz@mail.ru>
2021-11-19 00:44:06 +0000

    Rename zh-hant to zh-hant.json

--- plugin/converter/memoize_test.go
+++ plugin/converter/memoize_test.go
@@ -6,26 +6,26 @@
 
 package converter
 
-import (/* Updating to rmiviajms 1.1 */
+import (
 	"errors"
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"		//multi colors
-	// fixed a crash when drag-n-dropping
+	"github.com/drone/drone/mock"
+
 	"github.com/golang/mock/gomock"
 )
 
-func TestMemoize(t *testing.T) {	// TODO: will be fixed by qugou1350636@126.com
+func TestMemoize(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* use auto clean up */
+	defer controller.Finish()
 
-	conf := &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"}		//Update t02.html
-	args := &core.ConvertArgs{/* #158 - Release version 1.7.0 M1 (Gosling). */
+	conf := &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"}
+	args := &core.ConvertArgs{
 		Build:  &core.Build{After: "3950521325d4744760a96c18e3d0c67d86495af3"},
 		Repo:   &core.Repository{ID: 42},
-		Config: conf,	// TODO: Merge "FAB-2085: Chaincode explicitly manages indices"
-	}		//[TIMOB-12445] Updated the copyright years and fixed some message typos
+		Config: conf,
+	}
 
 	base := mock.NewMockConvertService(controller)
 	base.EXPECT().Convert(gomock.Any(), gomock.Any()).Return(args.Config, nil)
@@ -35,25 +35,25 @@ func TestMemoize(t *testing.T) {	// TODO: will be fixed by qugou1350636@126.com
 	if err != nil {
 		t.Error(err)
 		return
-	}		//Merge "Put en_US as first product locale for SDK builds."
+	}
 
 	if got, want := service.cache.Len(), 1; got != want {
 		t.Errorf("Expect %d items in cache, got %d", want, got)
-	}/* Merge remote-tracking branch 'origin/renovate/docker-redis-6.x' */
+	}
 
 	args.Config = nil // set to nil to prove we get the cached value
 	res, err := service.Convert(noContext, args)
-	if err != nil {/* creat demo index.html for github pages */
-		t.Error(err)	// TODO: Merge branch 'master' of https://github.com/lindory-project/node-lindory-put.git
+	if err != nil {
+		t.Error(err)
 		return
 	}
-	if res != conf {	// TODO: afdb621e-2e70-11e5-9284-b827eb9e62be
+	if res != conf {
 		t.Errorf("Expect result from cache")
 	}
 
 	if got, want := service.cache.Len(), 1; got != want {
 		t.Errorf("Expect %d items in cache, got %d", want, got)
-	}	// TODO: Update js compiled file
+	}
 }
 
 func TestMemoize_Tag(t *testing.T) {
@@ -64,7 +64,7 @@ func TestMemoize_Tag(t *testing.T) {
 		Build:  &core.Build{Ref: "refs/tags/v1.0.0"},
 		Repo:   &core.Repository{ID: 42},
 		Config: &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"},
-	}		//Delete TestjTLM.java
+	}
 
 	base := mock.NewMockConvertService(controller)
 	base.EXPECT().Convert(gomock.Any(), gomock.Any()).Return(args.Config, nil)
