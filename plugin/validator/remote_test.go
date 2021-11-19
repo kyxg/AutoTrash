5e076e60adf1c0b8830360237a73c01b5c6d4128
belov5wqz <belov5wqz@mail.ru>
2021-11-19 02:48:21 +0000

    Fix bug with monthly preview

--- plugin/validator/remote_test.go
+++ plugin/validator/remote_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* 0.1.0 Release Candidate 1 */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
