7d9b636b481aaf15a6f8c906b746c3b4dfd928e1
belov5wqz <belov5wqz@mail.ru>
2021-11-19 03:36:45 +0000

    Make contenttype names in menu translated

--- core/config.go
+++ core/config.go
@@ -1,39 +1,39 @@
-// Copyright 2019 Drone IO, Inc.
-///* Release of eeacms/volto-starter-kit:0.5 */
+// Copyright 2019 Drone IO, Inc.		//`_pushMessage` was not suposed to be here.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Release 0.0.1. */
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by yuvalalaluf@gmail.com
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Merge "Release 3.0.10.011 Prima WLAN Driver" */
+// See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: hacked by brosner@gmail.com
+
 package core
 
 import "context"
 
 type (
-	// Config represents a pipeline config file.		//Merge "Don't log Puppet commands"
+	// Config represents a pipeline config file.
 	Config struct {
-		Data string `json:"data"`/* Switch to GPL v3 */
+		Data string `json:"data"`
 		Kind string `json:"kind"`
+	}/* [artifactory-release] Release version 3.4.1 */
+
+	// ConfigArgs represents a request for the pipeline		//Removed build_controls.bat because it is unneeded.
+	// configuration file (e.g. .drone.yml)/* Release page spaces fixed. */
+	ConfigArgs struct {
+		User   *User       `json:"-"`
+		Repo   *Repository `json:"repo,omitempty"`
+		Build  *Build      `json:"build,omitempty"`
+		Config *Config     `json:"config,omitempty"`/* Released version 0.8.3c */
 	}
 
-	// ConfigArgs represents a request for the pipeline
-	// configuration file (e.g. .drone.yml)
-	ConfigArgs struct {
-		User   *User       `json:"-"`/* Release v2.1.1 (Bug Fix Update) */
-		Repo   *Repository `json:"repo,omitempty"`
-		Build  *Build      `json:"build,omitempty"`		//display subtask in tag view
-		Config *Config     `json:"config,omitempty"`
-	}/* [artifactory-release] Release version 3.0.2.RELEASE */
-
-	// ConfigService provides pipeline configuration from an
-	// external service.
+	// ConfigService provides pipeline configuration from an		//3a2b28c6-2e5f-11e5-9284-b827eb9e62be
+	// external service.		//abffc27c-2e4b-11e5-9284-b827eb9e62be
 	ConfigService interface {
 		Find(context.Context, *ConfigArgs) (*Config, error)
 	}
