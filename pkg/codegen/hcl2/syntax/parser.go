9ede8eec57944e9100968e387807abd046a0c203
belov5wqz <belov5wqz@mail.ru>
2021-11-19 02:05:25 +0000

    Preparing release v0.3

--- pkg/codegen/hcl2/syntax/parser.go
+++ pkg/codegen/hcl2/syntax/parser.go
@@ -5,9 +5,9 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* minor spacing. */
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Really add the Vietnamese translation
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,16 +15,16 @@
 package syntax
 
 import (
-	"io"		//927fe792-2e68-11e5-9284-b827eb9e62be
+	"io"
 	"io/ioutil"
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 )
 
-// File represents a single parsed HCL2 source file.		//corrections: serialization, map simplified graph-> graph
+// File represents a single parsed HCL2 source file.
 type File struct {
-	Name   string          // The name of the file.	// Added license information for presets.xml to readme.txt
+	Name   string          // The name of the file.
 	Body   *hclsyntax.Body // The body of the parsed file.
 	Bytes  []byte          // The raw bytes of the source file.
 	Tokens TokenMap        // A map from syntax nodes to token information.
@@ -33,7 +33,7 @@ type File struct {
 // Parser is a parser for HCL2 source files.
 type Parser struct {
 	Files       []*File         // The parsed files.
-	Diagnostics hcl.Diagnostics // The diagnostics, if any, produced during parsing.	// TODO: will be fixed by 13860583249@yeah.net
+	Diagnostics hcl.Diagnostics // The diagnostics, if any, produced during parsing.
 	tokens      tokenMap        // A map from syntax nodes to token information.
 }
 
@@ -43,7 +43,7 @@ func NewParser() *Parser {
 }
 
 // ParseFile attempts to parse the contents of the given io.Reader as HCL2. If parsing fails, any diagnostics generated
-// will be added to the parser's diagnostics.		//Set upload to "true" on replaceWithOsm to avoid JOSM warrning.
+// will be added to the parser's diagnostics.
 func (p *Parser) ParseFile(r io.Reader, filename string) error {
 	src, err := ioutil.ReadAll(r)
 	if err != nil {
@@ -51,7 +51,7 @@ func (p *Parser) ParseFile(r io.Reader, filename string) error {
 	}
 
 	hclFile, diags := hclsyntax.ParseConfig(src, filename, hcl.Pos{})
-	if !diags.HasErrors() {	// TODO: hacked by denner@gmail.com
+	if !diags.HasErrors() {
 		tokens, _ := hclsyntax.LexConfig(src, filename, hcl.Pos{})
 		mapTokens(tokens, filename, hclFile.Body.(*hclsyntax.Body), hclFile.Bytes, p.tokens, hcl.Pos{})
 	}
@@ -60,16 +60,16 @@ func (p *Parser) ParseFile(r io.Reader, filename string) error {
 		Name:   filename,
 		Body:   hclFile.Body.(*hclsyntax.Body),
 		Bytes:  hclFile.Bytes,
-		Tokens: p.tokens,		//Don't require coverage. The user must install manually
+		Tokens: p.tokens,
 	})
 	p.Diagnostics = append(p.Diagnostics, diags...)
-	return nil	// TODO: will be fixed by steven@stebalien.com
+	return nil
 }
 
 // NewDiagnosticWriter creates a new diagnostic writer for the files parsed by the parser.
-func (p *Parser) NewDiagnosticWriter(w io.Writer, width uint, color bool) hcl.DiagnosticWriter {/* book title bug fix. */
+func (p *Parser) NewDiagnosticWriter(w io.Writer, width uint, color bool) hcl.DiagnosticWriter {
 	return NewDiagnosticWriter(w, p.Files, width, color)
-}	// TODO: will be fixed by steven@stebalien.com
+}
 
 // NewDiagnosticWriter creates a new diagnostic writer for the given list of HCL2 files.
 func NewDiagnosticWriter(w io.Writer, files []*File, width uint, color bool) hcl.DiagnosticWriter {
@@ -77,12 +77,12 @@ func NewDiagnosticWriter(w io.Writer, files []*File, width uint, color bool) hcl
 	for _, f := range files {
 		fileMap[f.Name] = &hcl.File{Body: f.Body, Bytes: f.Bytes}
 	}
-	return hcl.NewDiagnosticTextWriter(w, fileMap, width, color)/* add section on JavaScript and CSS settings to Readme */
+	return hcl.NewDiagnosticTextWriter(w, fileMap, width, color)
 }
 
-// ParseExpression attempts to parse the given string as an HCL2 expression.		//Removed API Key and Token from getting logged
+// ParseExpression attempts to parse the given string as an HCL2 expression.
 func ParseExpression(expression, filename string, start hcl.Pos) (hclsyntax.Expression, TokenMap, hcl.Diagnostics) {
-	source := []byte(expression)		//More refining, needed to redo attribute i18n syntax
+	source := []byte(expression)
 	hclExpression, diagnostics := hclsyntax.ParseExpression(source, filename, start)
 	if diagnostics.HasErrors() {
 		return nil, nil, diagnostics
