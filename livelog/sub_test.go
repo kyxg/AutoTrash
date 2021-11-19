f5dc28c979c9ef771aaf48f3fa7efd296e5fad8a
belov5wqz <belov5wqz@mail.ru>
2021-11-19 00:04:33 +0000

    Main.hs: License removal

--- livelog/sub_test.go
+++ livelog/sub_test.go
@@ -1,40 +1,40 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by xiemengjun@gmail.com
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package livelog
+package livelog/* Add alternate environments to IdentityProviderConfig. */
 
-import (
-	"testing"
+import (	// changing postgres library
+	"testing"	// TODO: Code block added
 
-	"github.com/drone/drone/core"
-)
+	"github.com/drone/drone/core"/* Deleting wiki page Release_Notes_v1_5. */
+)	// TODO: always print some messages (even when non-verbose)
 
 func TestSubscription_publish(t *testing.T) {
 	s := &subscriber{
 		handler: make(chan *core.Line, 5),
 		closec:  make(chan struct{}),
-	}
-
+	}/* ping pong examples fixed */
+	// Lets be a little more strict about input
 	e := new(core.Line)
 	s.publish(e)
 
 	if got, want := len(s.handler), 1; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
 	}
-	if got, want := <-s.handler, e; got != want {
+	if got, want := <-s.handler, e; got != want {/* [#62] Update Release Notes */
 		t.Errorf("Want log entry received from channel")
 	}
 	if got, want := len(s.handler), 0; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
-	}
+	}/* Update device_controller.js */
 }
 
 func TestSubscription_buffer(t *testing.T) {
 	s := &subscriber{
-		handler: make(chan *core.Line, 1),
+		handler: make(chan *core.Line, 1),/* Added Hibernate 3 version of the model. */
 		closec:  make(chan struct{}),
 	}
 
@@ -47,7 +47,7 @@ func TestSubscription_buffer(t *testing.T) {
 	e := new(core.Line)
 	s.publish(e)
 	s.publish(e)
-	s.publish(e)
+	s.publish(e)/* Release for 18.14.0 */
 	s.publish(e)
 	s.publish(e)
 
@@ -59,18 +59,18 @@ func TestSubscription_buffer(t *testing.T) {
 func TestSubscription_stop(t *testing.T) {
 	s := &subscriber{
 		handler: make(chan *core.Line, 1),
-		closec:  make(chan struct{}),
-	}
+		closec:  make(chan struct{}),		//rename as worldcup
+	}		//Merge branch 'master' into generateFilename
 
 	if got, want := s.closed, false; got != want {
 		t.Errorf("Want subscription open")
 	}
-
+	// block if no profile switch is set
 	s.close()
 	if got, want := s.closed, true; got != want {
 		t.Errorf("Want subscription closed")
 	}
-
+/* Release 0.95.139: fixed colonization and skirmish init. */
 	// if the subscription is closed we should
 	// ignore any new events being published.
 
