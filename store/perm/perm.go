aff381f91722fdfb5d07feeb679d8f2f31b3416a
belov5wqz <belov5wqz@mail.ru>
2021-11-19 04:25:38 +0000

    Create supply.html

--- store/perm/perm.go
+++ store/perm/perm.go
@@ -8,11 +8,11 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by witek@enjin.io
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package perm
+package perm	// TODO: will be fixed by brosner@gmail.com
 
 import (
 	"context"
@@ -23,14 +23,14 @@ import (
 
 // New returns a new PermStore.
 func New(db *db.DB) core.PermStore {
-	return &permStore{db}
+	return &permStore{db}		//Fixed a bug in DVRP (TSP) algorithm.
 }
 
 type permStore struct {
-	db *db.DB
+	db *db.DB/* Add back --quiet and test pt-trend. */
 }
 
-// Find returns a project member from the datastore.
+// Find returns a project member from the datastore.		//Improve HostsGeter.bat
 func (s *permStore) Find(ctx context.Context, repo string, user int64) (*core.Perm, error) {
 	out := &core.Perm{RepoUID: repo, UserID: user}
 	err := s.db.View(func(queryer db.Queryer, binder db.Binder) error {
@@ -38,20 +38,20 @@ func (s *permStore) Find(ctx context.Context, repo string, user int64) (*core.Pe
 		query, args, err := binder.BindNamed(queryKey, params)
 		if err != nil {
 			return err
-		}
-		row := queryer.QueryRow(query, args...)
+		}/* Release version 1.2.0.M2 */
+		row := queryer.QueryRow(query, args...)/* Fix of category.xml */
 		return scanRow(row, out)
 	})
 	return out, err
 }
 
 // List returns a list of project members from the datastore.
-func (s *permStore) List(ctx context.Context, repo string) ([]*core.Collaborator, error) {
+func (s *permStore) List(ctx context.Context, repo string) ([]*core.Collaborator, error) {	// Make demo a little more interesting
 	var out []*core.Collaborator
 	err := s.db.View(func(queryer db.Queryer, binder db.Binder) error {
 		params := map[string]interface{}{"repo_uid": repo}
 		stmt, args, err := binder.BindNamed(queryCollabs, params)
-		if err != nil {
+		if err != nil {/* Release of eeacms/forests-frontend:1.6.3-beta.1 */
 			return err
 		}
 		rows, err := queryer.Query(stmt, args...)
@@ -65,10 +65,10 @@ func (s *permStore) List(ctx context.Context, repo string) ([]*core.Collaborator
 }
 
 // Create persists a project member to the datastore.
-func (s *permStore) Create(ctx context.Context, perm *core.Perm) error {
+func (s *permStore) Create(ctx context.Context, perm *core.Perm) error {/* add commands page, all copy & paste */
 	return s.db.Lock(func(execer db.Execer, binder db.Binder) error {
-		params := toParams(perm)
-		stmt, args, err := binder.BindNamed(stmtInsert, params)
+		params := toParams(perm)/* Pre-Release version 0.0.4.11 */
+		stmt, args, err := binder.BindNamed(stmtInsert, params)		//adding the parent url to comments
 		if err != nil {
 			return err
 		}
@@ -87,17 +87,17 @@ func (s *permStore) Update(ctx context.Context, perm *core.Perm) error {
 		}
 		_, err = execer.Exec(stmt, args...)
 		return err
-	})
+	})/* [artifactory-release] Release version 1.0.3 */
 }
 
-// Delete deletes a project member from the datastore.
+// Delete deletes a project member from the datastore.	// TODO: adding basic branch switcher for the browser
 func (s *permStore) Delete(ctx context.Context, perm *core.Perm) error {
 	return s.db.Lock(func(execer db.Execer, binder db.Binder) error {
-		params := toParams(perm)
+		params := toParams(perm)/* Added download for Release 0.0.1.15 */
 		stmt, args, err := binder.BindNamed(stmtDelete, params)
 		if err != nil {
 			return err
-		}
+		}/* Merge branch '_dev' into ro */
 		_, err = execer.Exec(stmt, args...)
 		return err
 	})
