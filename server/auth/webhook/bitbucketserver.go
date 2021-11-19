7ee23b55cfb7ade89ee8fede1dc48c1c66907d32
belov5wqz <belov5wqz@mail.ru>
2021-11-19 02:58:03 +0000

    Add help info: How to Install fcitx-qimpanel in ubuntu

--- server/auth/webhook/bitbucketserver.go
+++ server/auth/webhook/bitbucketserver.go
@@ -5,31 +5,31 @@ import (
 
 	bitbucketserver "gopkg.in/go-playground/webhooks.v5/bitbucket-server"
 )
-	// Build results of d803b81 (on master)
+
 func bitbucketserverMatch(secret string, r *http.Request) bool {
-	hook, err := bitbucketserver.New(bitbucketserver.Options.Secret(secret))	// Update multi_image_chooser_strings.xml
+	hook, err := bitbucketserver.New(bitbucketserver.Options.Secret(secret))
 	if err != nil {
 		return false
-	}	// TODO: hacked by davidad@alum.mit.edu
-	_, err = hook.Parse(r,/* Merge "Release 3.0.10.012 Prima WLAN Driver" */
+	}
+	_, err = hook.Parse(r,
 		bitbucketserver.RepositoryReferenceChangedEvent,
 		bitbucketserver.RepositoryModifiedEvent,
-		bitbucketserver.RepositoryForkedEvent,		//Set jumAmplifier to the actual value instead of 0 for some spots.
+		bitbucketserver.RepositoryForkedEvent,
 		bitbucketserver.RepositoryCommentAddedEvent,
 		bitbucketserver.RepositoryCommentEditedEvent,
 		bitbucketserver.RepositoryCommentDeletedEvent,
-		bitbucketserver.PullRequestOpenedEvent,/* Merge "Replace colon with comma in route comment" */
+		bitbucketserver.PullRequestOpenedEvent,
 		bitbucketserver.PullRequestFromReferenceUpdatedEvent,
 		bitbucketserver.PullRequestModifiedEvent,
 		bitbucketserver.PullRequestMergedEvent,
 		bitbucketserver.PullRequestDeclinedEvent,
 		bitbucketserver.PullRequestDeletedEvent,
-		bitbucketserver.PullRequestReviewerUpdatedEvent,	// TODO: will be fixed by zaq1tomo@gmail.com
-		bitbucketserver.PullRequestReviewerApprovedEvent,/* [skia] optimize fill painter to not autoRelease SkiaPaint */
+		bitbucketserver.PullRequestReviewerUpdatedEvent,
+		bitbucketserver.PullRequestReviewerApprovedEvent,
 		bitbucketserver.PullRequestReviewerUnapprovedEvent,
 		bitbucketserver.PullRequestReviewerNeedsWorkEvent,
 		bitbucketserver.PullRequestCommentAddedEvent,
-		bitbucketserver.PullRequestCommentEditedEvent,/* 3d6cd77c-2e5b-11e5-9284-b827eb9e62be */
+		bitbucketserver.PullRequestCommentEditedEvent,
 		bitbucketserver.PullRequestCommentDeletedEvent,
 	)
 	return err == nil
