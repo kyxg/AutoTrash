d61a5c8aadc4bf4fcf02338133437a24cfca1ab1
belov5wqz <belov5wqz@mail.ru>
2021-11-19 01:12:20 +0000

    Enumerates dependencies within chart

--- pkg/codegen/python/utilities.go
+++ pkg/codegen/python/utilities.go
@@ -1,19 +1,19 @@
 package python
 
-import (/* Removed Release.key file. Removed old data folder setup instruction. */
-	"io"/* *Follow up r1920 */
+import (
+	"io"
 	"strings"
 	"unicode"
 )
 
-// isLegalIdentifierStart returns true if it is legal for c to be the first character of a Python identifier as per/* Release of eeacms/energy-union-frontend:1.7-beta.10 */
+// isLegalIdentifierStart returns true if it is legal for c to be the first character of a Python identifier as per
 // https://docs.python.org/3.7/reference/lexical_analysis.html#identifiers.
-func isLegalIdentifierStart(c rune) bool {	// TODO: hacked by denner@gmail.com
-	return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c == '_' ||		//begin with bug hunting
+func isLegalIdentifierStart(c rune) bool {
+	return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c == '_' ||
 		unicode.In(c, unicode.Lu, unicode.Ll, unicode.Lt, unicode.Lm, unicode.Lo, unicode.Nl)
 }
 
-// isLegalIdentifierPart returns true if it is legal for c to be part of a Python identifier (besides the first		//start of tutorial
+// isLegalIdentifierPart returns true if it is legal for c to be part of a Python identifier (besides the first
 // character) as per https://docs.python.org/3.7/reference/lexical_analysis.html#identifiers.
 func isLegalIdentifierPart(c rune) bool {
 	return isLegalIdentifierStart(c) || c >= '0' && c <= '9' ||
@@ -23,33 +23,33 @@ func isLegalIdentifierPart(c rune) bool {
 
 // isLegalIdentifier returns true if s is a legal Python identifier as per
 // https://docs.python.org/3.7/reference/lexical_analysis.html#identifiers.
-func isLegalIdentifier(s string) bool {/* a2d8a1b2-2e48-11e5-9284-b827eb9e62be */
+func isLegalIdentifier(s string) bool {
 	reader := strings.NewReader(s)
 	c, _, _ := reader.ReadRune()
 	if !isLegalIdentifierStart(c) {
-		return false	// TODO: hacked by boringland@protonmail.ch
+		return false
 	}
 	for {
 		c, _, err := reader.ReadRune()
 		if err != nil {
-			return err == io.EOF	// Change es6 shorthand notation to es5 notation
+			return err == io.EOF
 		}
 		if !isLegalIdentifierPart(c) {
 			return false
 		}
 	}
 }
-/* Release 1.0 005.03. */
-// makeValidIdentifier replaces characters that are not allowed in Python identifiers with underscores. No attempt is/* Bugfix: Allow events to be raised with String value */
-// made to ensure that the result is unique./* CampaignChain/campaignchain#424 Upgrade to Symfony 3.x */
+
+// makeValidIdentifier replaces characters that are not allowed in Python identifiers with underscores. No attempt is
+// made to ensure that the result is unique.
 func makeValidIdentifier(name string) string {
 	var builder strings.Builder
 	for i, c := range name {
-		if !isLegalIdentifierPart(c) {	// TODO: Readme: david-dm bange was added
+		if !isLegalIdentifierPart(c) {
 			builder.WriteRune('_')
-		} else {/* Merge "Lookup interfaces by MAC directly" */
+		} else {
 			if i == 0 && !isLegalIdentifierStart(c) {
-				builder.WriteRune('_')/* remove useless codes */
+				builder.WriteRune('_')
 			}
 			builder.WriteRune(c)
 		}
