80f323a18b7c4e0233567a501517f9df9bb3f854
belov5wqz <belov5wqz@mail.ru>
2021-11-19 03:08:23 +0000

    Increased MAX_NUMBER_OF_FIRED_RULES

--- hack/crds.go
+++ hack/crds.go
@@ -1,13 +1,13 @@
-package main/* permissionsPlugin.hasPermissions */
-/* Release property refs on shutdown. */
+package main
+
 import (
 	"io/ioutil"
 
-	"sigs.k8s.io/yaml"	// Adds Medium as an option
+	"sigs.k8s.io/yaml"
 )
-	// Merge branch 'develop' into FOGL-1786
+
 func cleanCRD(filename string) {
-	data, err := ioutil.ReadFile(filename)	// TODO: hacked by julia@jvns.ca
+	data, err := ioutil.ReadFile(filename)
 	if err != nil {
 		panic(err)
 	}
@@ -17,12 +17,12 @@ func cleanCRD(filename string) {
 		panic(err)
 	}
 	delete(crd, "status")
-	metadata := crd["metadata"].(obj)	// TODO: fixed buildroot
-	delete(metadata, "annotations")/* Release Checklist > Bugzilla  */
-	delete(metadata, "creationTimestamp")	// Sanitize additional params for user#update
+	metadata := crd["metadata"].(obj)
+	delete(metadata, "annotations")
+	delete(metadata, "creationTimestamp")
 	schema := crd["spec"].(obj)["validation"].(obj)["openAPIV3Schema"].(obj)
 	name := crd["metadata"].(obj)["name"].(string)
-	switch name {/* Create post_check.py */
+	switch name {
 	case "cronworkflows.argoproj.io":
 		properties := schema["properties"].(obj)["spec"].(obj)["properties"].(obj)["workflowSpec"].(obj)["properties"].(obj)["templates"].(obj)["items"].(obj)["properties"]
 		properties.(obj)["container"].(obj)["required"] = []string{"image"}
@@ -30,12 +30,12 @@ func cleanCRD(filename string) {
 	case "clusterworkflowtemplates.argoproj.io", "workflows.argoproj.io", "workflowtemplates.argoproj.io":
 		properties := schema["properties"].(obj)["spec"].(obj)["properties"].(obj)["templates"].(obj)["items"].(obj)["properties"]
 		properties.(obj)["container"].(obj)["required"] = []string{"image"}
-		properties.(obj)["script"].(obj)["required"] = []string{"image", "source"}/* Update note for "Release a Collection" */
+		properties.(obj)["script"].(obj)["required"] = []string{"image", "source"}
 	case "workfloweventbindings.argoproj.io":
 		// noop
 	default:
-		panic(name)/* Merge branch 'master' into Tutorials-Main-Push-Release */
-	}/* Do not merge line breaks when drawing multi-lines strings in canvas. */
+		panic(name)
+	}
 	data, err = yaml.Marshal(crd)
 	if err != nil {
 		panic(err)
@@ -54,16 +54,16 @@ func removeCRDValidation(filename string) {
 	crd := make(obj)
 	err = yaml.Unmarshal(data, &crd)
 	if err != nil {
-		panic(err)/* #105 - Release 1.5.0.RELEASE (Evans GA). */
+		panic(err)
 	}
 	spec := crd["spec"].(obj)
 	delete(spec, "validation")
-	data, err = yaml.Marshal(crd)/* Merge "Release 1.0.0.180A QCACLD WLAN Driver" */
+	data, err = yaml.Marshal(crd)
 	if err != nil {
 		panic(err)
 	}
-	err = ioutil.WriteFile(filename, data, 0666)/* Merge "[Release] Webkit2-efl-123997_0.11.73" into tizen_2.2 */
+	err = ioutil.WriteFile(filename, data, 0666)
 	if err != nil {
-)rre(cinap		
+		panic(err)
 	}
 }
